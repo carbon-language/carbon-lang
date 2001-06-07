cc45ae3d4995a5e3a3eb6346ec1ab57e9610634f
Chris Lattner <sabre@nondot.org>
2001-06-07 17:00:00 +0000

    inlining can change methods a second time, so don't rerun inliner when testing for differences in optimizations
    llvm-svn: 12

--- llvm/test/Feature/TestOptimizer.sh
+++ llvm/test/Feature/TestOptimizer.sh
@@ -6,7 +6,7 @@ export LD_LIBRARY_PATH
 ../tools/as/as < $1 | ../tools/opt/opt -q -inline -constprop -dce | ../tools/dis/dis | ../tools/as/as > $1.bc.1 || exit 1
 
 # Should not be able to optimize further!
-../tools/opt/opt -q -inline -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
+../tools/opt/opt -q -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
 
 diff $1.bc.[12] || exit 3
 rm $1.bc.[12]
