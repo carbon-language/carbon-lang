f124461c303f2a465d3c7e4e47f8d6fc81ac8b77
Chris Lattner <sabre@nondot.org>
2001-06-27 23:24:22 +0000

    Filter out some more stuff
    llvm-svn: 77

--- llvm/getsomesrcs.sh
+++ llvm/getsomesrcs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script prints out some of the source files that are useful when 
 # editing.  I use this like this: xemacs `./getsomesrcs.sh` &
-./getsrcs.sh | grep -v Assembly | grep -v Byte | grep -v \\.ll | grep -v tools | grep -v Makefile | grep -v Opt | grep -v CommandLi | grep -v String | grep -v DataType | grep -v '/i[^/]*$' | grep -v SlotCalcul | grep -v VMCore | grep -v Holder
+./getsrcs.sh | grep -v Assembly | grep -v Byte | grep -v \\.ll | grep -v tools | grep -v Makefile | grep -v Opt | grep -v llvm/Tools | grep -v '/i[^/]*$' | grep -v VMCore | grep -v Holder | grep -v Analysis | grep -v html
 
 
