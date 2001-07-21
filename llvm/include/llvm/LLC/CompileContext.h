ad5ac0fab403e3e97c1bb8930b54b30d44401424
Vikram S. Adve <vadve@cs.uiuc.edu>
2001-07-21 12:39:30 +0000

    CompileContext and options class for the llc compiler.
    llvm-svn: 227

---
+++ llvm/include/llvm/LLC/CompileContext.h
@@ -0,0 +1,69 @@
+// $Id$ -*-c++-*-
+//***************************************************************************
+// class CompileContext
+// 
+// Purpose:
+//	Holds the common option and target information for a compilation run.
+// 
+// History:
+//     07/15/01 - vadve - Created
+//
+//**************************************************************************/
+
+#ifndef LLVM_LLC_COMPILECONTEXT_H
+#define LLVM_LLC_COMPILECONTEXT_H
+
+//************************** System Include Files **************************/
+
+#include <string>
+
+//*************************** User Include Files ***************************/
+
+#include "llvm/Codegen/Sparc.h"
+#include "llvm/LLC/LLCOptions.h"
+
+//************************** Forward Declarations **************************/
+
+class ProgramOptions;
+class TargetMachine;
+
+
+//---------------------------------------------------------------------------
+// class CompileContext
+//---------------------------------------------------------------------------
+
+class CompileContext: public Unique
+{
+private:
+  LLCOptions*		options;
+  TargetMachine*	targetMachine;
+  
+public:
+  /*ctor*/		CompileContext	(int argc, const char **argv, const char** envp);
+  /*dtor*/ virtual	~CompileContext	();
+  
+  const LLCOptions&	getOptions	() const { return *options; }
+  
+  const TargetMachine&	getTarget	() const { return *targetMachine; }
+  TargetMachine&	getTarget	()	 { return *targetMachine; }
+};
+
+
+inline
+CompileContext::CompileContext(int argc, const char **argv, const char** envp)
+{
+  options = new LLCOptions(argc, argv, envp);
+  targetMachine = new UltraSparc;
+}
+
+
+inline
+CompileContext::~CompileContext()
+{
+  delete options;
+  delete targetMachine;
+}
+
+//**************************************************************************/
+
+#endif
