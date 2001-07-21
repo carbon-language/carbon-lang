863bb601ce18a909e1759d7b4db58f9ad84c8dac
Chris Lattner <sabre@nondot.org>
2001-07-21 19:43:08 +0000

    Filter out the sparc.burm.c file
    llvm-svn: 249

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # This is useful because it prints out all of the source files.  Useful for
 # greps.
-find . -name \*.\[chyl\]\* | grep -v Lexer.cpp | grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | grep -v '\.ll$' | grep -v test | grep -v .flc
+find . -name \*.\[chyl\]\* | grep -v Lexer.cpp | grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | grep -v '\.ll$' | grep -v test | grep -v .flc | grep -v Sparc.burm.c
+
