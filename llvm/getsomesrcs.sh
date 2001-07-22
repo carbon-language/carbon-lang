2ed3326123ff73719dd94833486bf1c3cc7ccd6a
Chris Lattner <sabre@nondot.org>
2001-07-22 05:45:46 +0000

    Make it pickier
    llvm-svn: 270

--- llvm/getsomesrcs.sh
+++ llvm/getsomesrcs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This script prints out some of the source files that are useful when 
 # editing.  I use this like this: xemacs `./getsomesrcs.sh` &
-./getsrcs.sh | grep -v Assembly | grep -v Byte | grep -v \\.ll | grep -v tools | grep -v Makefile | grep -v Opt | grep -v llvm/Tools | grep -v '/i[^/]*$' | grep -v VMCore | grep -v Holder | grep -v Analysis | grep -v html
+./getsrcs.sh | grep -v Assembly | grep -v Byte | grep -v \\.ll | grep -v Optimization | grep -v llvm/Tools | grep -v '/i[^/]*$' | grep -v Holder | grep -v Analysis | grep -v html | grep include/llvm
 
 
