c0a673f13396e352eb1b969ad3110222e325118d
Chris Lattner <sabre@nondot.org>
2001-09-07 22:58:50 +0000

    Executables all live in a nice centralized location now
    llvm-svn: 500

--- llvm/test/Feature/TestAsmDisasm.sh
+++ llvm/test/Feature/TestAsmDisasm.sh
@@ -4,15 +4,21 @@
 
 rm -f test.bc.temp[12]
 
-LD_LIBRARY_PATH=../lib/Assembly/Parser/Debug:../lib/Assembly/Writer/Debug:../lib/Analysis/Debug:../lib/VMCore/Debug:../lib/Bytecode/Writer/Debug:../lib/Bytecode/Reader/Debug:../lib/Optimizations/Debug
+LD_LIBRARY_PATH=../lib/Debug
 export LD_LIBRARY_PATH
+
+AS=../tools/Debug/as
+DIS=../tools/Debug/dis
+export AS
+export DIS
+
 
 # Two full cycles are needed for bitwise stability
 
-../tools/as/as   < $1      > $1.bc.1 || exit 1
-../tools/dis/dis < $1.bc.1 > $1.ll.1 || exit 2
-../tools/as/as   < $1.ll.1 > $1.bc.2 || exit 3
-../tools/dis/dis < $1.bc.2 > $1.ll.2 || exit 4
+$AS  < $1      > $1.bc.1 || exit 1
+$DIS < $1.bc.1 > $1.ll.1 || exit 2
+$AS  < $1.ll.1 > $1.bc.2 || exit 3
+$DIS < $1.bc.2 > $1.ll.2 || exit 4
 
 diff $1.ll.[12] || exit 7
 
--- llvm/test/Feature/TestOptimizer.sh
+++ llvm/test/Feature/TestOptimizer.sh
@@ -1,22 +1,28 @@
 #!/bin/sh
-LD_LIBRARY_PATH=../lib/Assembly/Parser/Debug:../lib/Assembly/Writer/Debug:../lib/Analysis/Debug:../lib/VMCore/Debug:../lib/Bytecode/Writer/Debug:../lib/Bytecode/Reader/Debug:../lib/Optimizations/Debug
+LD_LIBRARY_PATH=../lib/Debug
 export LD_LIBRARY_PATH
 
+AS=../tools/Debug/as
+DIS=../tools/Debug/dis
+OPT=../tools/Debug/opt
+export AS
+export DIS
+export OPT
 
-../tools/as/as < $1 | ../tools/opt/opt -q -inline -dce -constprop -dce | ../tools/dis/dis | ../tools/as/as > $1.bc.1 || exit 1
+$AS < $1 | $OPT -q -inline -dce -constprop -dce | $DIS | $AS > $1.bc.1 || exit 1
 
 # Should not be able to optimize further!
-../tools/opt/opt -q -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
+$OPT -q -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
 
-../tools/dis/dis < $1.bc.1 > $1.ll.1 || exit 3
-../tools/dis/dis < $1.bc.2 > $1.ll.2 || exit 3
+$DIS < $1.bc.1 > $1.ll.1 || exit 3
+$DIS < $1.bc.2 > $1.ll.2 || exit 3
 diff $1.ll.[12] || exit 3
 
 # Try out SCCP
-../tools/as/as < $1 | ../tools/opt/opt -q -inline -dce -sccp -dce | ../tools/dis/dis | ../tools/as/as > $1.bc.3 || exit 1
+$AS < $1 | $OPT -q -inline -dce -sccp -dce | $DIS | $AS > $1.bc.3 || exit 1
 
 # Should not be able to optimize further!
-#../tools/opt/opt -q -sccp -dce < $1.bc.3 > $1.bc.4 || exit 2
+#$OPT -q -sccp -dce < $1.bc.3 > $1.bc.4 || exit 2
 
 #diff $1.bc.[34] || exit 3
 rm $1.bc.[123] $1.ll.[12]
