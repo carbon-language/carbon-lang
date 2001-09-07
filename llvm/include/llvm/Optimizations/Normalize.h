2833dd3fed0cb3ca48a7fbd77408a84418d2552e
Chris Lattner <sabre@nondot.org>
2001-09-07 17:38:10 +0000

    Follow the golden rule of the coding standards guide: Make the code look like the rest of the system.
    llvm-svn: 476

--- llvm/include/llvm/Optimizations/Normalize.h
+++ llvm/include/llvm/Optimizations/Normalize.h
@@ -1,36 +1,19 @@
-// $Id$ -*-c++-*-
-//***************************************************************************
-// File:
-//	Normalize.h
-// 
-// Purpose:
-//	Transformations to normalize LLVM code to simplify later passes:
-//	-- Insert loads of constants that are arguments to PHI
-//	   in the appropriate predecessor basic block.
-//	
-// History:
-//	8/25/01	 -  Vikram Adve  -  Created
-//**************************************************************************/
+//===-- Normalize.h - Functions that normalize code for the BE ---*- C++ -*--=//
+//
+// This file defines a family of transformations to normalize LLVM code for the
+// code generation passes, so that the back end doesn't have to worry about
+// annoying details.
+//
+//===----------------------------------------------------------------------===//
 
 #ifndef LLVM_OPT_NORMALIZE_H
 #define LLVM_OPT_NORMALIZE_H
 
-//************************** System Include Files ***************************/
-
-
-//*************************** User Include Files ***************************/
-
-
-//************************* Forward Declarations ***************************/
-
 class Method;
 
-//************************** External Functions ****************************/
-
-
-void	NormalizePhiConstantArgs	(Method* method);
-
-
-//**************************************************************************/
+// NormalizePhiConstantArgs - Insert loads of constants that are arguments to
+// PHI in the appropriate predecessor basic block.
+//
+void NormalizePhiConstantArgs(Method *M);
 
 #endif LLVM_OPT_NORMALIZE_H
