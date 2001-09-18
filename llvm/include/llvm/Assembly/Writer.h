0d4a68f8aa9d9a9d6775917796ad0859416ada7d
Vikram S. Adve <vadve@cs.uiuc.edu>
2001-09-18 12:43:15 +0000

    Moved debugging interfaces for class Value to Value.h.
    llvm-svn: 604

--- llvm/include/llvm/Assembly/Writer.h
+++ llvm/include/llvm/Assembly/Writer.h
@@ -89,21 +89,4 @@ inline ostream &operator<<(ostream &o, const Type *T) {
   return o << T->getDescription();
 }
 
-inline ostream &operator<<(ostream &o, const Value *I) {
-  switch (I->getValueType()) {
-  case Value::TypeVal:        return o << I->castTypeAsserting();
-  case Value::ConstantVal:    WriteToAssembly((const ConstPoolVal*)I, o); break;
-  case Value::MethodArgumentVal: return o <<I->getType() << " " << I->getName();
-  case Value::InstructionVal: WriteToAssembly((const Instruction *)I, o); break;
-  case Value::BasicBlockVal:  WriteToAssembly((const BasicBlock  *)I, o); break;
-  case Value::MethodVal:      WriteToAssembly((const Method      *)I, o); break;
-  case Value::GlobalVal:      WriteToAssembly((const GlobalVariable*)I,o);break;
-  case Value::ModuleVal:      WriteToAssembly((const Module      *)I, o); break;
-  default: return o << "<unknown value type: " << I->getValueType() << ">";
-  }
-  return o;
-}
-
-void DebugValue(const Value *V);
-
 #endif
