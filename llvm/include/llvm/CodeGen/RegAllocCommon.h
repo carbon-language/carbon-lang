d89425bb586acb4fe9990d722a61d96d2e7ccdb1
Chris Lattner <sabre@nondot.org>
2001-09-19 16:26:10 +0000

    Change debug info from #define to command line option
    llvm-svn: 646

--- llvm/include/llvm/CodeGen/RegAllocCommon.h
+++ llvm/include/llvm/CodeGen/RegAllocCommon.h
@@ -1,10 +1,21 @@
+
+#include "llvm/Support/CommandLine.h"
+
 #ifndef REG_ALLOC_COMMON_H
 #define  REG_ALLOC_COMMON_H
 
+// THIS IS NOW SPECIFIED VIA A COMMANDLINE ARGUMENT TO LLC
+//
 // set DEBUG_RA for printing out debug messages
 // if DEBUG_RA is 1 normal output messages
 // if DEBUG_RA is 2 extensive debug info for each instr
 
-#define DEBUG_RA (0)
+enum RegAllocDebugLevel_t {
+  RA_DEBUG_None    = 0,
+  RA_DEBUG_Normal  = 1,
+  RA_DEBUG_Verbose = 2,
+};
+
+extern cl::Enum<RegAllocDebugLevel_t> DEBUG_RA;
 
 #endif
