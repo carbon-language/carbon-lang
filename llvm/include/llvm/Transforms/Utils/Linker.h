8015f958c6973160ae2ed26a2449258f7880e823
Chris Lattner <sabre@nondot.org>
2001-10-13 16:57:49 +0000

    New module linking functionality prototype
    llvm-svn: 789

---
+++ llvm/include/llvm/Transforms/Utils/Linker.h
@@ -0,0 +1,20 @@
+//===- llvm/Transforms/Linker.h - Module Linker Interface --------*- C++ -*--=//
+//
+// This file defines the interface to the module linker.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMATIONS_LINKER_H
+#define LLVM_TRANSFORMATIONS_LINKER_H
+
+#include <string>
+class Module;
+
+// LinkModules - This function links two modules together, with the resulting
+// left module modified to be the composite of the two input modules.  If an
+// error occurs, true is returned and ErrorMsg (if not null) is set to indicate
+// the problem.
+//
+bool LinkModules(Module *Dest, const Module *Src, string *ErrorMsg = 0);
+
+#endif
