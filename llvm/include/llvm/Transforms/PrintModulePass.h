fe2f987d5eb4a2a13a31901ba22fa8542d96e364
Vikram S. Adve <vadve@cs.uiuc.edu>
2001-10-18 13:47:49 +0000

    Add option to print as bytecode instead of assembly.
    llvm-svn: 887

--- llvm/include/llvm/Transforms/PrintModulePass.h
+++ llvm/include/llvm/Transforms/PrintModulePass.h
@@ -10,14 +10,17 @@
 
 #include "llvm/Transforms/Pass.h"
 #include "llvm/Assembly/Writer.h"
+#include "llvm/Bytecode/Writer.h"
 
 class PrintModulePass : public Pass {
   string Banner;          // String to print before each method
   ostream *Out;           // ostream to print on
   bool DeleteStream;      // Delete the ostream in our dtor?
+  bool PrintAsBytecode;   // Print as bytecode rather than assembly?
 public:
-  inline PrintModulePass(const string &B, ostream *o = &cout, bool DS = false) 
-    : Banner(B), Out(o), DeleteStream(DS) {}
+  inline PrintModulePass(const string &B, ostream *o = &cout, bool DS = false,
+                         bool printAsBytecode = false)
+    : Banner(B), Out(o), DeleteStream(DS), PrintAsBytecode(printAsBytecode) {}
 
   ~PrintModulePass() {
     if (DeleteStream) delete Out;
@@ -27,7 +30,17 @@ public:
   // it's processed.
   //
   bool doPerMethodWork(Method *M) {
-    (*Out) << Banner << M;
+    if (! PrintAsBytecode)
+      (*Out) << Banner << M;
+    return false;
+  }
+  
+  // doPassFinalization - Virtual method overriden by subclasses to do any post
+  // processing needed after all passes have run.
+  //
+  bool doPassFinalization(Module *M) {
+    if (PrintAsBytecode)
+      WriteBytecodeToFile(M, *Out);
     return false;
   }
 };
