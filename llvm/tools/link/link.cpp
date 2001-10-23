ebaa78852c0831492755d66557d83d3e118e7c79
Chris Lattner <sabre@nondot.org>
2001-10-23 20:44:55 +0000

    Add '-v' option to enable verbose output from the linker
    llvm-svn: 966

--- llvm/tools/link/link.cpp
+++ llvm/tools/link/link.cpp
@@ -4,6 +4,9 @@
 // This utility may be invoked in the following manner:
 //  link a.bc b.bc c.bc -o x.bc
 //
+// Alternatively, this can be used as an 'ar' tool as well.  If invoked as
+// either 'ar' or 'llvm-ar', it accepts a 'cr' parameter as well.
+//
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Transforms/Linker.h"
@@ -21,6 +24,7 @@ cl::StringList InputFilenames("", "Load <arg> files, linking them together",
 			      cl::OneOrMore);
 cl::String OutputFilename("o", "Override output filename", cl::NoFlags, "-");
 cl::Flag   Force         ("f", "Overwrite output files", cl::NoFlags, false);
+cl::Flag   Verbose       ("v", "Print information about actions taken");
 cl::Flag   DumpAsm       ("d", "Print assembly as linked", cl::Hidden, false);
 
 
@@ -28,6 +32,10 @@ int main(int argc, char **argv) {
   cl::ParseCommandLineOptions(argc, argv, " llvm linker\n");
   assert(InputFilenames.size() > 0 && "OneOrMore is not working");
 
+  // TODO: TEST argv[0]
+  
+
+  if (Verbose) cerr << "Loading '" << InputFilenames[0] << "'\n";
   std::auto_ptr<Module> Composite(ParseBytecodeFile(InputFilenames[0]));
   if (Composite.get() == 0) {
     cerr << "Error opening bytecode file: '" << InputFilenames[0] << "'\n";
@@ -35,12 +43,15 @@ int main(int argc, char **argv) {
   }
 
   for (unsigned i = 1; i < InputFilenames.size(); ++i) {
+  if (Verbose) cerr << "Loading '" << InputFilenames[i] << "'\n";
     auto_ptr<Module> M(ParseBytecodeFile(InputFilenames[i]));
     if (M.get() == 0) {
       cerr << "Error opening bytecode file: '" << InputFilenames[i] << "'\n";
       return 1;
     }
-    
+
+    if (Verbose) cerr << "Linking in '" << InputFilenames[i] << "'\n";
+
     string ErrorMessage;
     if (LinkModules(Composite.get(), M.get(), &ErrorMessage)) {
       cerr << "Error linking in '" << InputFilenames[i] << "': "
@@ -62,6 +73,7 @@ int main(int argc, char **argv) {
     }
   }
 
+  if (Verbose) cerr << "Writing bytecode...\n";
   WriteBytecodeToFile(Composite.get(), *Out);
 
   if (Out != &cout) delete Out;
