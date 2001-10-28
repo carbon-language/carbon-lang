328c9afafe9274a40c5a989a07659e151eb2c34a
Chris Lattner <sabre@nondot.org>
2001-10-28 22:40:59 +0000

    Remove unneccesary function prototypes
    llvm-svn: 1014

--- llvm/include/llvm/Transforms/Utils/Linker.h
+++ llvm/include/llvm/Transforms/Utils/Linker.h
@@ -9,8 +9,6 @@
 
 #include <string>
 class Module;
-class Type;
-class Value;
 
 
 // LinkModules - This function links two modules together, with the resulting
@@ -18,20 +16,7 @@ class Value;
 // error occurs, true is returned and ErrorMsg (if not null) is set to indicate
 // the problem.
 //
-bool    LinkModules(Module *Dest, const Module *Src, string *ErrorMsg = 0);
-
-
-// MangleTypeName - Implement a consistent name-mangling scheme for
-//                  a given type.
-// 
-string	MangleTypeName(const Type* type);
-
-
-// MangleName - implement a consistent name-mangling scheme for all
-// externally visible (i.e., global) objects.
-// privateName should be unique within the module.
-// 
-string  MangleName(const string& privateName, const Value* V);
-
+bool LinkModules(Module *Dest, const Module *Src, string *ErrorMsg = 0);
 
 #endif
+
