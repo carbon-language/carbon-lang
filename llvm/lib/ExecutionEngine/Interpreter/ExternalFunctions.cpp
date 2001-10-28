9cd1451b893e47dcb085998a2f9b4cd8c1d88163
Chris Lattner <sabre@nondot.org>
2001-10-28 22:38:22 +0000

    Fix some illegal uses of the ## operator
    llvm-svn: 1013

--- llvm/lib/ExecutionEngine/Interpreter/ExternalFunctions.cpp
+++ llvm/lib/ExecutionEngine/Interpreter/ExternalFunctions.cpp
@@ -151,11 +151,10 @@ GenericValue lle_X_printString(MethodType *M, const vector<GenericValue> &ArgVal
 
 // Implement 'void print<TYPE>(X)' for each primitive type or pointer type
 #define PRINT_TYPE_FUNC(TYPENAME,TYPEID) \
-  GenericValue lle_X_print##TYPENAME##(MethodType *M,\
-                                       const vector<GenericValue> &ArgVal) {\
+  GenericValue lle_X_print##TYPENAME(MethodType *M,\
+                                     const vector<GenericValue> &ArgVal) {\
     assert(ArgVal.size() == 1 && "generic print only takes one argument!");\
-    assert(M->getParamTypes()[0].get()->getPrimitiveID()\
-           == Type::##TYPEID##);\
+    assert(M->getParamTypes()[0].get()->getPrimitiveID() == Type::##TYPEID);\
     Interpreter::printValue(M->getParamTypes()[0], ArgVal[0]);\
     return GenericValue();\
   }
