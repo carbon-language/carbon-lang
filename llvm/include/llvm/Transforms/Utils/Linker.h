e3b575497d0bc777f178591bba02f0d4a0ecaaf4
Vikram S. Adve <vadve@cs.uiuc.edu>
2001-10-28 21:23:44 +0000

    Add support for name mangling.
    llvm-svn: 998

--- llvm/include/llvm/Transforms/Utils/Linker.h
+++ llvm/include/llvm/Transforms/Utils/Linker.h
@@ -9,12 +9,29 @@
 
 #include <string>
 class Module;
+class Type;
+class Value;
+
 
 // LinkModules - This function links two modules together, with the resulting
 // left module modified to be the composite of the two input modules.  If an
 // error occurs, true is returned and ErrorMsg (if not null) is set to indicate
 // the problem.
 //
-bool LinkModules(Module *Dest, const Module *Src, string *ErrorMsg = 0);
+bool    LinkModules(Module *Dest, const Module *Src, string *ErrorMsg = 0);
+
+
+// MangleTypeName - Implement a consistent name-mangling scheme for
+//                  a given type.
+// 
+string	MangleTypeName(const Type* type);
+
+
+// MangleName - implement a consistent name-mangling scheme for all
+// externally visible (i.e., global) objects.
+// privateName should be unique within the module.
+// 
+string  MangleName(const string& privateName, const Value* V);
+
 
 #endif
