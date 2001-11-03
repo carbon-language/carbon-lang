cf5befef639e02c59fc02725998d3908d089aaca
Chris Lattner <sabre@nondot.org>
2001-11-03 19:49:54 +0000

    Expose method to merge identically named methods
    llvm-svn: 1109

--- llvm/include/llvm/Transforms/IPO.h
+++ llvm/include/llvm/Transforms/IPO.h
@@ -14,6 +14,12 @@ public:
 
   inline CleanupGCCOutput() : Malloc(0), Free(0) {}
 
+  // PatchUpMethodReferences - This is a part of the functionality exported by
+  // the CleanupGCCOutput pass.  This causes functions with different signatures
+  // to be linked together if they have the same name.
+  //
+  static bool PatchUpMethodReferences(Module *M);
+
   // doPassInitialization - For this pass, it removes global symbol table
   // entries for primitive types.  These are never used for linking in GCC and
   // they make the output uglier to look at, so we nuke them.
