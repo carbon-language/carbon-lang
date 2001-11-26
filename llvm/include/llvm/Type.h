7e26de46a3c272f6815b00177a6de34b6ea8f983
Chris Lattner <sabre@nondot.org>
2001-11-26 16:47:10 +0000

    New Method isLosslesslyConvertableTo
    llvm-svn: 1330

--- llvm/include/llvm/Type.h
+++ llvm/include/llvm/Type.h
@@ -141,6 +141,11 @@ public:
   //
   inline bool isRecursive() const { return Recursive; }
 
+  // isLosslesslyConvertableTo - Return true if this type can be converted to
+  // 'Ty' without any reinterpretation of bits.  For example, uint to int.
+  //
+  bool isLosslesslyConvertableTo(const Type *Ty) const;
+
   //===--------------------------------------------------------------------===//
   // Type Iteration support
   //
