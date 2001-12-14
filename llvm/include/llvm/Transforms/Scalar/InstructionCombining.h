64a028b47a6c8a10e8a0e39e5292e9201e7c846b
Chris Lattner <sabre@nondot.org>
2001-12-14 16:52:09 +0000

    Initial checkin of instcombine
    llvm-svn: 1479

---
+++ llvm/include/llvm/Transforms/Scalar/InstructionCombining.h
@@ -0,0 +1,27 @@
+//===- llvm/Transforms/Scalar/InstructionCombining.h -------------*- C++ -*--=//
+//
+// InstructionCombining - Combine instructions to form fewer, simple
+//   instructions.  This pass does not modify the CFG, and has a tendancy to
+//   make instructions dead, so a subsequent DCE pass is useful.
+//
+// This pass combines things like:
+//    %Y = add int 1, %X
+//    %Z = add int 1, %Y
+// into:
+//    %Z = add int 2, %X
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_SCALAR_INSTRUCTIONCOMBINING_H
+#define LLVM_TRANSFORMS_SCALAR_INSTRUCTIONCOMBINING_H
+
+#include "llvm/Pass.h"
+
+struct InstructionCombining : public Pass {
+  static bool doit(Method *M);
+  static bool CombineInstruction(Instruction *I);
+
+  virtual bool doPerMethodWork(Method *M) { return doit(M); }
+};
+
+#endif
