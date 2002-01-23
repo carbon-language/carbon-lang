7297fdeaa545d4aae3814e9ea716f3caee11d494
Chris Lattner <sabre@nondot.org>
2002-01-23 18:04:21 +0000

    Change test scripts so that they do not stop on failure, they just print an error message
    llvm-svn: 1563

--- llvm/test/Feature/TestAsmDisasm.sh
+++ llvm/test/Feature/TestAsmDisasm.sh
@@ -4,26 +4,28 @@
 
 rm -f test.bc.temp[12]
 
-LD_LIBRARY_PATH=../lib/Debug:/usr/dcs/software/evaluation/encap/gcc-3.0.2/lib/
+LD_LIBRARY_PATH=../../lib/Debug
 export LD_LIBRARY_PATH
 
-AS=../tools/Debug/as
-DIS=../tools/Debug/dis
-export AS
-export DIS
+AS=../../tools/Debug/as
+DIS=../../tools/Debug/dis
 
+echo "======== Running assembler/disassembler test on $1"
 
 # Two full cycles are needed for bitwise stability
+(
+  $AS  < $1      > $1.bc.1 || exit 1
+  $DIS < $1.bc.1 > $1.ll.1 || exit 2
+  $AS  < $1.ll.1 > $1.bc.2 || exit 3
+  $DIS < $1.bc.2 > $1.ll.2 || exit 4
 
-$AS  < $1      > $1.bc.1 || exit 1
-$DIS < $1.bc.1 > $1.ll.1 || exit 2
-$AS  < $1.ll.1 > $1.bc.2 || exit 3
-$DIS < $1.bc.2 > $1.ll.2 || exit 4
+  diff $1.ll.[12] || exit 7
 
-diff $1.ll.[12] || exit 7
+  # FIXME: When we sort things correctly and deterministically, we can
+  # reenable this
+  #diff $1.bc.[12] || exit 8
 
-# FIXME: When we sort things correctly and deterministically, we can reenable this
-#diff $1.bc.[12] || exit 8
-
-rm $1.[bl][cl].[12]
+  rm $1.[bl][cl].[12]
+  touch Output/$1.asmdis
+) || ./Failure.sh "$1 ASM/DIS"
 
--- llvm/test/Feature/TestOptimizer.sh
+++ llvm/test/Feature/TestOptimizer.sh
@@ -1,30 +1,33 @@
 #!/bin/sh
-LD_LIBRARY_PATH=../lib/Debug:/usr/dcs/software/evaluation/encap/gcc-3.0.2/lib/
+LD_LIBRARY_PATH=../../lib/Debug
 export LD_LIBRARY_PATH
 
-AS=../tools/Debug/as
-DIS=../tools/Debug/dis
-OPT=../tools/Debug/opt
-export AS
-export DIS
-export OPT
+AS=../../tools/Debug/as
+DIS=../../tools/Debug/dis
+OPT=../../tools/Debug/opt
 
-$AS < $1 | $OPT -q -inline -dce -constprop -dce | $DIS | $AS > $1.bc.1 || exit 1
+echo "======== Running optimizier test on $1"
 
-# Should not be able to optimize further!
-$OPT -q -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
+(
+  $AS < $1 | $OPT -q -inline -dce -constprop -dce |$DIS| $AS > $1.bc.1 || exit 1
 
-$DIS < $1.bc.1 > $1.ll.1 || exit 3
-$DIS < $1.bc.2 > $1.ll.2 || exit 3
-diff $1.ll.[12] || exit 3
+  # Should not be able to optimize further!
+  $OPT -q -constprop -dce < $1.bc.1 > $1.bc.2 || exit 2
 
-# Try out SCCP & CleanGCC
-$AS < $1 | $OPT -q -inline -dce -cleangcc -sccp -dce | $DIS | $AS > $1.bc.3 || exit 1
+  $DIS < $1.bc.1 > $1.ll.1 || exit 3
+  $DIS < $1.bc.2 > $1.ll.2 || exit 3
+  diff $1.ll.[12] || exit 3
 
-# Should not be able to optimize further!
-$OPT -q -sccp -dce < $1.bc.3 > $1.bc.4 || exit 2
-$DIS < $1.bc.3 > $1.ll.3 || exit 3
-$DIS < $1.bc.4 > $1.ll.4 || exit 3
-diff $1.ll.[34] || exit 3
-rm $1.bc.[1234] $1.ll.[1234]
+  # Try out SCCP & CleanGCC
+  $AS < $1 | $OPT -q -inline -dce -cleangcc -sccp -dce \
+           | $DIS | $AS > $1.bc.3 || exit 1
 
+  # Should not be able to optimize further!
+  $OPT -q -sccp -dce < $1.bc.3 > $1.bc.4 || exit 2
+  $DIS < $1.bc.3 > $1.ll.3 || exit 3
+  $DIS < $1.bc.4 > $1.ll.4 || exit 3
+  diff $1.ll.[34] || exit 3
+  rm $1.bc.[1234] $1.ll.[1234]
+  
+  touch Output/$1.opt  # Success!
+)|| ./Failure.sh "$1 Optimizer"
