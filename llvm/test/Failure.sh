a14d807fe0c5640351c92552edf7a43cf97e6041
Chris Lattner <sabre@nondot.org>
2002-01-23 18:03:49 +0000

    Initial checking of Failure.sh
    llvm-svn: 1562

---
+++ llvm/test/Failure.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Failure.sh - This little script is used by the makefile to print out errors
+# when they occur.
+
+echo
+echo "******************** TEST '$1' FAILED! ********************"
+echo
