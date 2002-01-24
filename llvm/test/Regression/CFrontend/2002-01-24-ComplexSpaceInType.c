735d4f15b813ef1c1ad0274dc1aded524340233b
Chris Lattner <sabre@nondot.org>
2002-01-24 20:45:45 +0000

    New testcase to ensure GCC doesn't emit type identifiers with spaces in them
    llvm-svn: 1580

---
+++ llvm/test/Regression/CFrontend/2002-01-24-ComplexSpaceInType.c
@@ -0,0 +1,9 @@
+// This caused generation of the following type name:
+//   %Array = uninitialized global [10 x %complex int]
+//
+// which caused problems because of the space int the complex int type
+//
+
+struct { int X, Y; } Array[10];
+
+void foo() {}
