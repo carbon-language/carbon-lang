4b361376f02e060ad77c4deb2280f20bdd503821
Chris Lattner <sabre@nondot.org>
2002-02-03 07:52:58 +0000

    Switch from MachineCodeForVMInstr model that is built into the VMCore library to an annotation based MAchineCodeForInstruction model
    Instruction.cpp now has 0 lines of code generation related code in it.
    
    llvm-svn: 1658

--- llvm/lib/VMCore/Instruction.cpp
+++ llvm/lib/VMCore/Instruction.cpp
@@ -7,18 +7,11 @@
 #include "llvm/Instruction.h"
 #include "llvm/Method.h"
 #include "llvm/SymbolTable.h"
-#include "llvm/CodeGen/MachineInstr.h"
 
 Instruction::Instruction(const Type *ty, unsigned it, const std::string &Name) 
-  : User(ty, Value::InstructionVal, Name), 
-    machineInstrVec(new MachineCodeForVMInstr) {
+  : User(ty, Value::InstructionVal, Name) {
   Parent = 0;
   iType = it;
-}
-
-Instruction::~Instruction() {
-  assert(getParent() == 0 && "Instruction still embedded in basic block!");
-  delete machineInstrVec;
 }
 
 // Specialize setName to take care of symbol table majik
@@ -31,24 +24,4 @@ void Instruction::setName(const std::string &name, SymbolTable *ST) {
     PP->getSymbolTable()->remove(this);
   Value::setName(name);
   if (PP && hasName()) PP->getSymbolTableSure()->insert(this);
-}
-
-void Instruction::addMachineInstruction(MachineInstr* minstr) {
-  machineInstrVec->push_back(minstr);
-}
-
-#if 0
-// Dont make this inline because you would need to include
-// MachineInstr.h in Instruction.h, which creates a circular
-// sequence of forward declarations.  Trying to fix that will
-// cause a serious circularity in link order.
-// 
-const std::vector<Value*> &Instruction::getTempValuesForMachineCode() const {
-  return machineInstrVec->getTempValues();
-}
-#endif
-
-void Instruction::dropAllReferences() {
-  machineInstrVec->dropAllReferences();
-  User::dropAllReferences();
 }
