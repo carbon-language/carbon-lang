621457a34bee33cf312a52caf2c6c547f1c5b933
Chris Lattner <sabre@nondot.org>
2002-02-03 07:47:05 +0000

    Add #include that is no longer implicitly included
    llvm-svn: 1651

--- llvm/lib/Target/TargetSchedInfo.cpp
+++ llvm/lib/Target/TargetSchedInfo.cpp
@@ -6,6 +6,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Target/MachineSchedInfo.h"
+#include "llvm/Target/TargetMachine.h"
 
 // External object describing the machine instructions
 // Initialized only when the TargetMachine class is created
