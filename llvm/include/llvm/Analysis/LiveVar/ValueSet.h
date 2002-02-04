d3972903b1b758419f4e9d463bd2cc537c06c31d
Chris Lattner <sabre@nondot.org>
2002-02-04 16:33:18 +0000

    eliminate the add and remove methods, clients must use insert and erase directly.
    llvm-svn: 1686

--- llvm/include/llvm/Analysis/LiveVar/ValueSet.h
+++ llvm/include/llvm/Analysis/LiveVar/ValueSet.h
@@ -9,35 +9,19 @@
 #define VALUE_SET_H
 
 class Value;
-#include "Support/HashExtras.h"
-#include <ext/hash_set>
+#include <set>
 
 //------------------- Class Definition for ValueSet --------------------------
 
-void printValue( const Value *const v);  // func to print a Value 
+void printValue( const Value *v);  // func to print a Value 
 
-
-
-class ValueSet : public std::hash_set<const Value *> {
- public:
-  inline void add(const Value *const  val) 
-    { assert( val ); insert(val);}      // for adding a live variable to set
-
-  inline void remove(const Value *const  val) 
-    { assert( val ); erase(val); }      // for removing a live var from set
-
+struct ValueSet : public std::set<const Value*> {
   bool setUnion( const ValueSet *const set1);     // for performing set union
   void setSubtract( const ValueSet *const set1);  // for performing set diff
-
  
   void setDifference( const ValueSet *const set1, const ValueSet *const set2); 
  
   void printSet() const;                // for printing a live variable set
 };
-
-
-
-
-
 
 #endif
