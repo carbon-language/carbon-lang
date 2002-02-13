8996df4a7e46eca6dc598e6ee8ecdec92963252a
Chris Lattner <sabre@nondot.org>
2002-02-13 20:25:16 +0000

    New testcase for fixed bug
    llvm-svn: 1751

---
+++ llvm/test/Regression/CFrontend/2002-02-13-ConditionalInCall.c
@@ -0,0 +1,9 @@
+/* Test problem where bad code was generated with a ?: statement was 
+   in a function call argument */
+
+void foo(int, double, float);
+
+void bar(int x) {
+  foo(x, x ? 1.0 : 12.5, 1.0f);
+}
+
