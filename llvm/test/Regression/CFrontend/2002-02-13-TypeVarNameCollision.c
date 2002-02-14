49242560a423efa0ba3622576c9e4ba3128cf51e
Chris Lattner <sabre@nondot.org>
2002-02-14 05:41:15 +0000

    New testcase for fixed GCC bug
    llvm-svn: 1755

---
+++ llvm/test/Regression/CFrontend/2002-02-13-TypeVarNameCollision.c
@@ -0,0 +1,14 @@
+/* This testcase causes a symbol table collision.  Type names and variable 
+ * names should be in distinct namespaces
+ */
+
+typedef struct foo {
+	int X, Y;
+} FOO;
+
+static FOO foo[100];
+
+int test() {
+	return foo[4].Y;
+}
+
