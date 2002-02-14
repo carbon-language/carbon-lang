feeeffa1709ca71e543373c1d8efa7e04d209cc4
Chris Lattner <sabre@nondot.org>
2002-02-14 00:18:44 +0000

    NEw testcase for bugfix
    llvm-svn: 1753

---
+++ llvm/test/Regression/CFrontend/2002-02-13-UnnamedLocal.c
@@ -0,0 +1,19 @@
+/* Testcase for a problem where GCC allocated xqic to a register,
+ * and did not have a VAR_DECL that explained the stack slot to LLVM.
+ * Now the LLVM code synthesizes a stack slot if one is presented that
+ * has not been previously recognized.  This is where alloca's named 
+ * 'local' come from now. 
+ */
+
+typedef struct {
+  short x;
+} foostruct;
+
+int foo(foostruct ic);
+
+void test() {
+  foostruct xqic;
+  foo(xqic);
+}
+
+
