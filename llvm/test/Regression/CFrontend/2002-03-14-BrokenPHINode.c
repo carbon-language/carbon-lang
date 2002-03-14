724d872ca7e8b7b0ed63bcb3e236521fd5995a36
Chris Lattner <sabre@nondot.org>
2002-03-14 19:40:44 +0000

    New test case for long standing bug that was fixed
    llvm-svn: 1873

---
+++ llvm/test/Regression/CFrontend/2002-03-14-BrokenPHINode.c
@@ -0,0 +1,17 @@
+/* GCC was generating PHI nodes with an arity < #pred of the basic block the
+ * PHI node lived in.  This was breaking LLVM because the number of entries
+ * in a PHI node must equal the number of predecessors for a basic block.
+ */
+
+int trys(char *s, int x)
+{
+  int asa;
+  double Val;
+  int LLS;
+  if (x) {
+    asa = LLS + asa;
+  } else {
+  }
+  return asa+(int)Val;
+}
+
