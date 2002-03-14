dace30004cfb066e30564fdca26ea29d2a4752a4
Chris Lattner <sabre@nondot.org>
2002-03-14 19:27:43 +0000

    New testcase
    llvm-svn: 1871

---
+++ llvm/test/Regression/CFrontend/2002-03-14-BrokenSSA.c
@@ -0,0 +1,15 @@
+/* This code used to break GCC's SSA computation code.  It would create
+   uses of B & C that are not dominated by their definitions.  See:
+   http://gcc.gnu.org/ml/gcc/2002-03/msg00697.html
+ */
+int bar();
+int foo()
+{
+  int a,b,c;
+
+  a = b + c;
+  b = bar();
+  c = bar();
+  return a + b + c;
+}
+
