71dfc02de9b972470f141cb4119eb462d3ef3b9b
Chris Lattner <sabre@nondot.org>
2002-04-08 21:52:58 +0000

    s/Method/Function Trim #includes
    llvm-svn: 2170

--- llvm/include/llvm/Assembly/PrintModulePass.h
+++ llvm/include/llvm/Assembly/PrintModulePass.h
@@ -11,7 +11,6 @@
 #define LLVM_ASSEMBLY_PRINTMODULEPASS_H
 
 #include "llvm/Pass.h"
-#include "llvm/Assembly/Writer.h"
 #include <iostream>
 
 class PrintModulePass : public Pass {
@@ -32,25 +31,25 @@ public:
   }
 };
 
-class PrintMethodPass : public MethodPass {
+class PrintFunctionPass : public MethodPass {
   std::string Banner;     // String to print before each method
   std::ostream *Out;      // ostream to print on
   bool DeleteStream;      // Delete the ostream in our dtor?
 public:
-  inline PrintMethodPass(const std::string &B, std::ostream *o = &std::cout,
-                         bool DS = false)
+  inline PrintFunctionPass(const std::string &B, std::ostream *o = &std::cout,
+                           bool DS = false)
     : Banner(B), Out(o), DeleteStream(DS) {
   }
   
-  inline ~PrintMethodPass() {
+  inline ~PrintFunctionPass() {
     if (DeleteStream) delete Out;
   }
   
   // runOnMethod - This pass just prints a banner followed by the method as
   // it's processed.
   //
-  bool runOnMethod(Method *M) {
-    (*Out) << Banner << M;
+  bool runOnMethod(Function *F) {
+    (*Out) << Banner << F;
     return false;
   }
 };
