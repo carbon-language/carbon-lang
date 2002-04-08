fec8df44bad5a96b34537b4f02e66e8a305fb8c4
Chris Lattner <sabre@nondot.org>
2002-04-08 19:32:51 +0000

    Checkin old testcase that is newly fixed.
    llvm-svn: 2161

---
+++ llvm/test/Regression/CFrontend/2002-03-12-StructInitialize.c
@@ -0,0 +1,12 @@
+
+typedef struct Connection_Type {
+   long    to;
+   char    type[10];
+   long    length;
+} Connection;
+
+Connection link[3]
+= { {1, "link1", 10},
+    {2, "link2", 20},
+    {3, "link3", 30} };
+
