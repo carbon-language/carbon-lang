0fda698670d274d5745d117a4738b4ff523b1aa7
Chris Lattner <sabre@nondot.org>
2002-04-10 17:43:40 +0000

    New testcase for structure valued parameters.
    llvm-svn: 2220

---
+++ llvm/test/Regression/CFrontend/2002-04-10-StructParameters.c
@@ -0,0 +1,23 @@
+
+typedef struct {
+  char p;
+  short q;
+  char r;
+  int X;
+  short Y, Z;
+  int Q;
+} foo;
+
+int test(foo X, float);
+int testE(char,short,char,int,int,float);
+void test3(foo *X) {
+  X->q = 1;
+}
+
+void test2(foo Y) {
+  testE(Y.p, Y.q, Y.r, Y.X, Y.Y, 0.1f);
+  test(Y, 0.1f);
+  test2(Y);
+  test3(&Y);
+}
+
