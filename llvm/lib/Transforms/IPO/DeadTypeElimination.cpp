011ab80ed61b5c8fdff7b154fd74f7fd9090f0d5
Chris Lattner <sabre@nondot.org>
2002-04-10 20:33:11 +0000

    * Add documentation * Split the CleanGCC pass into two passes, a global pass and an IP pass.   Before it was just a global pass, but it did illegal things to the   module, which broke other passes that were being scheduled with it by   gccld.
    llvm-svn: 2224

--- llvm/lib/Transforms/IPO/DeadTypeElimination.cpp
+++ llvm/lib/Transforms/IPO/DeadTypeElimination.cpp
@@ -1,4 +1,4 @@
-//===- CleanupGCCOutput.cpp - Cleanup GCC Output ----------------------------=//
+//===- CleanupGCCOutput.cpp - Cleanup GCC Output --------------------------===//
 //
 // This pass is used to cleanup the output of GCC.  GCC's output is
 // unneccessarily gross for a couple of reasons. This pass does the following
@@ -6,6 +6,8 @@
 //
 // * Eliminate names for GCC types that we know can't be needed by the user.
 // * Eliminate names for types that are unused in the entire translation unit
+// * Fix various problems that we might have in PHI nodes and casts
+// * Link uses of 'void %foo(...)' to 'void %foo(sometypes)'
 //
 // Note:  This code produces dead declarations, it is a good idea to run DCE
 //        after this pass.
@@ -60,193 +62,10 @@ namespace {
   };
 }
 
-
-
-// ConvertCallTo - Convert a call to a varargs function with no arg types
-// specified to a concrete nonvarargs function.
-//
-static void ConvertCallTo(CallInst *CI, Function *Dest) {
-  const FunctionType::ParamTypes &ParamTys =
-    Dest->getFunctionType()->getParamTypes();
-  BasicBlock *BB = CI->getParent();
-
-  // Get an iterator to where we want to insert cast instructions if the
-  // argument types don't agree.
-  //
-  BasicBlock::iterator BBI = find(BB->begin(), BB->end(), CI);
-  assert(BBI != BB->end() && "CallInst not in parent block?");
-
-  assert(CI->getNumOperands()-1 == ParamTys.size()&&
-         "Function calls resolved funny somehow, incompatible number of args");
-
-  vector<Value*> Params;
-
-  // Convert all of the call arguments over... inserting cast instructions if
-  // the types are not compatible.
-  for (unsigned i = 1; i < CI->getNumOperands(); ++i) {
-    Value *V = CI->getOperand(i);
-
-    if (V->getType() != ParamTys[i-1]) { // Must insert a cast...
-      Instruction *Cast = new CastInst(V, ParamTys[i-1]);
-      BBI = BB->getInstList().insert(BBI, Cast)+1;
-      V = Cast;
-    }
-
-    Params.push_back(V);
-  }
-
-  // Replace the old call instruction with a new call instruction that calls
-  // the real function.
-  //
-  ReplaceInstWithInst(BB->getInstList(), BBI, new CallInst(Dest, Params));
+Pass *createCleanupGCCOutputPass() {
+  return new CleanupGCCOutput();
 }
 
-
-// PatchUpFunctionReferences - Go over the functions that are in the module and
-// look for functions that have the same name.  More often than not, there will
-// be things like:
-//    void "foo"(...)
-//    void "foo"(int, int)
-// because of the way things are declared in C.  If this is the case, patch
-// things up.
-//
-static bool PatchUpFunctionReferences(Module *M) {
-  SymbolTable *ST = M->getSymbolTable();
-  if (!ST) return false;
-
-  std::map<string, vector<Function*> > Functions;
-
-  // Loop over the entries in the symbol table. If an entry is a func pointer,
-  // then add it to the Functions map.  We do a two pass algorithm here to avoid
-  // problems with iterators getting invalidated if we did a one pass scheme.
-  //
-  for (SymbolTable::iterator I = ST->begin(), E = ST->end(); I != E; ++I)
-    if (const PointerType *PT = dyn_cast<PointerType>(I->first))
-      if (isa<FunctionType>(PT->getElementType())) {
-        SymbolTable::VarMap &Plane = I->second;
-        for (SymbolTable::type_iterator PI = Plane.begin(), PE = Plane.end();
-             PI != PE; ++PI) {
-          const string &Name = PI->first;
-          Functions[Name].push_back(cast<Function>(PI->second));          
-        }
-      }
-
-  bool Changed = false;
-
-  // Now we have a list of all functions with a particular name.  If there is
-  // more than one entry in a list, merge the functions together.
-  //
-  for (std::map<string, vector<Function*> >::iterator I = Functions.begin(), 
-         E = Functions.end(); I != E; ++I) {
-    vector<Function*> &Functions = I->second;
-    Function *Implementation = 0;     // Find the implementation
-    Function *Concrete = 0;
-    for (unsigned i = 0; i < Functions.size(); ) {
-      if (!Functions[i]->isExternal()) {  // Found an implementation
-        assert(Implementation == 0 && "Multiple definitions of the same"
-               " function. Case not handled yet!");
-        Implementation = Functions[i];
-      } else {
-        // Ignore functions that are never used so they don't cause spurious
-        // warnings... here we will actually DCE the function so that it isn't
-        // used later.
-        //
-        if (Functions[i]->use_size() == 0) {
-          M->getFunctionList().remove(Functions[i]);
-          delete Functions[i];
-          Functions.erase(Functions.begin()+i);
-          Changed = true;
-          continue;
-        }
-      }
-      
-      if (Functions[i] && (!Functions[i]->getFunctionType()->isVarArg())) {
-        if (Concrete) {  // Found two different functions types.  Can't choose
-          Concrete = 0;
-          break;
-        }
-        Concrete = Functions[i];
-      }
-      ++i;
-    }
-
-    if (Functions.size() > 1) {         // Found a multiply defined function...
-      // We should find exactly one non-vararg function definition, which is
-      // probably the implementation.  Change all of the function definitions
-      // and uses to use it instead.
-      //
-      if (!Concrete) {
-        cerr << "Warning: Found functions types that are not compatible:\n";
-        for (unsigned i = 0; i < Functions.size(); ++i) {
-          cerr << "\t" << Functions[i]->getType()->getDescription() << " %"
-               << Functions[i]->getName() << "\n";
-        }
-        cerr << "  No linkage of functions named '" << Functions[0]->getName()
-             << "' performed!\n";
-      } else {
-        for (unsigned i = 0; i < Functions.size(); ++i)
-          if (Functions[i] != Concrete) {
-            Function *Old = Functions[i];
-            const FunctionType *OldMT = Old->getFunctionType();
-            const FunctionType *ConcreteMT = Concrete->getFunctionType();
-            bool Broken = false;
-
-            assert(Old->getReturnType() == Concrete->getReturnType() &&
-                   "Differing return types not handled yet!");
-            assert(OldMT->getParamTypes().size() <=
-                   ConcreteMT->getParamTypes().size() &&
-                   "Concrete type must have more specified parameters!");
-
-            // Check to make sure that if there are specified types, that they
-            // match...
-            //
-            for (unsigned i = 0; i < OldMT->getParamTypes().size(); ++i)
-              if (OldMT->getParamTypes()[i] != ConcreteMT->getParamTypes()[i]) {
-                cerr << "Parameter types conflict for" << OldMT
-                     << " and " << ConcreteMT;
-                Broken = true;
-              }
-            if (Broken) break;  // Can't process this one!
-
-
-            // Attempt to convert all of the uses of the old function to the
-            // concrete form of the function.  If there is a use of the fn
-            // that we don't understand here we punt to avoid making a bad
-            // transformation.
-            //
-            // At this point, we know that the return values are the same for
-            // our two functions and that the Old function has no varargs fns
-            // specified.  In otherwords it's just <retty> (...)
-            //
-            for (unsigned i = 0; i < Old->use_size(); ) {
-              User *U = *(Old->use_begin()+i);
-              if (CastInst *CI = dyn_cast<CastInst>(U)) {
-                // Convert casts directly
-                assert(CI->getOperand(0) == Old);
-                CI->setOperand(0, Concrete);
-                Changed = true;
-              } else if (CallInst *CI = dyn_cast<CallInst>(U)) {
-                // Can only fix up calls TO the argument, not args passed in.
-                if (CI->getCalledValue() == Old) {
-                  ConvertCallTo(CI, Concrete);
-                  Changed = true;
-                } else {
-                  cerr << "Couldn't cleanup this function call, must be an"
-                       << " argument or something!" << CI;
-                  ++i;
-                }
-              } else {
-                cerr << "Cannot convert use of function: " << U << "\n";
-                ++i;
-              }
-            }
-          }
-        }
-    }
-  }
-
-  return Changed;
-}
 
 
 // ShouldNukSymtabEntry - Return true if this module level symbol table entry
@@ -279,13 +98,6 @@ bool CleanupGCCOutput::doInitialization(Module *M) {
 
   if (M->hasSymbolTable()) {
     SymbolTable *ST = M->getSymbolTable();
-
-    // Go over the functions that are in the module and look for methods that
-    // have the same name.  More often than not, there will be things like:
-    // void "foo"(...)  and void "foo"(int, int) because of the way things are
-    // declared in C.  If this is the case, patch things up.
-    //
-    Changed |= PatchUpFunctionReferences(M);
 
     // Check the symbol table for superfluous type entries...
     //
@@ -434,10 +246,10 @@ static inline void RefactorPredecessor(BasicBlock *BB, BasicBlock *Pred) {
 }
 
 
-// fixLocalProblems - Loop through the function and fix problems with the PHI
-// nodes in the current function.  The problem is that PHI nodes might exist
-// with multiple entries for the same predecessor.  GCC sometimes generates code
-// that looks like this:
+// runOnMethod - Loop through the function and fix problems with the PHI nodes
+// in the current function.  The problem is that PHI nodes might exist with
+// multiple entries for the same predecessor.  GCC sometimes generates code that
+// looks like this:
 //
 //  bb7:  br bool %cond1004, label %bb8, label %bb8
 //  bb8: %reg119 = phi uint [ 0, %bb7 ], [ 1, %bb7 ]
@@ -450,7 +262,7 @@ static inline void RefactorPredecessor(BasicBlock *BB, BasicBlock *Pred) {
 //  bb8: %reg119 = phi uint [ 0, %bbX ], [ 1, %bb7 ]
 //
 //
-static bool fixLocalProblems(Function *M) {
+bool CleanupGCCOutput::runOnMethod(Function *M) {
   bool Changed = false;
   // Don't use iterators because invalidation gets messy...
   for (unsigned MI = 0; MI < M->size(); ++MI) {
@@ -480,18 +292,8 @@ static bool fixLocalProblems(Function *M) {
   return Changed;
 }
 
-
-
-
-// runOnFunction - This method simplifies the specified function hopefully.
-//
-bool CleanupGCCOutput::runOnMethod(Function *F) {
-  return fixLocalProblems(F);
-}
-
 bool CleanupGCCOutput::doFinalization(Module *M) {
   bool Changed = false;
-  
 
   if (M->hasSymbolTable()) {
     SymbolTable *ST = M->getSymbolTable();
@@ -523,7 +325,203 @@ bool CleanupGCCOutput::doFinalization(Module *M) {
   return Changed;
 }
 
-Pass *createCleanupGCCOutputPass() {
-  return new CleanupGCCOutput();
+
+//===----------------------------------------------------------------------===//
+//
+// FunctionResolvingPass - Go over the functions that are in the module and
+// look for functions that have the same name.  More often than not, there will
+// be things like:
+//    void "foo"(...)
+//    void "foo"(int, int)
+// because of the way things are declared in C.  If this is the case, patch
+// things up.
+//
+//===----------------------------------------------------------------------===//
+
+namespace {
+  struct FunctionResolvingPass : public Pass {
+    bool run(Module *M);
+  };
 }
 
+// ConvertCallTo - Convert a call to a varargs function with no arg types
+// specified to a concrete nonvarargs function.
+//
+static void ConvertCallTo(CallInst *CI, Function *Dest) {
+  const FunctionType::ParamTypes &ParamTys =
+    Dest->getFunctionType()->getParamTypes();
+  BasicBlock *BB = CI->getParent();
+
+  // Get an iterator to where we want to insert cast instructions if the
+  // argument types don't agree.
+  //
+  BasicBlock::iterator BBI = find(BB->begin(), BB->end(), CI);
+  assert(BBI != BB->end() && "CallInst not in parent block?");
+
+  assert(CI->getNumOperands()-1 == ParamTys.size()&&
+         "Function calls resolved funny somehow, incompatible number of args");
+
+  vector<Value*> Params;
+
+  // Convert all of the call arguments over... inserting cast instructions if
+  // the types are not compatible.
+  for (unsigned i = 1; i < CI->getNumOperands(); ++i) {
+    Value *V = CI->getOperand(i);
+
+    if (V->getType() != ParamTys[i-1]) { // Must insert a cast...
+      Instruction *Cast = new CastInst(V, ParamTys[i-1]);
+      BBI = BB->getInstList().insert(BBI, Cast)+1;
+      V = Cast;
+    }
+
+    Params.push_back(V);
+  }
+
+  // Replace the old call instruction with a new call instruction that calls
+  // the real function.
+  //
+  ReplaceInstWithInst(BB->getInstList(), BBI, new CallInst(Dest, Params));
+}
+
+
+bool FunctionResolvingPass::run(Module *M) {
+  SymbolTable *ST = M->getSymbolTable();
+  if (!ST) return false;
+
+  std::map<string, vector<Function*> > Functions;
+
+  // Loop over the entries in the symbol table. If an entry is a func pointer,
+  // then add it to the Functions map.  We do a two pass algorithm here to avoid
+  // problems with iterators getting invalidated if we did a one pass scheme.
+  //
+  for (SymbolTable::iterator I = ST->begin(), E = ST->end(); I != E; ++I)
+    if (const PointerType *PT = dyn_cast<PointerType>(I->first))
+      if (isa<FunctionType>(PT->getElementType())) {
+        SymbolTable::VarMap &Plane = I->second;
+        for (SymbolTable::type_iterator PI = Plane.begin(), PE = Plane.end();
+             PI != PE; ++PI) {
+          const string &Name = PI->first;
+          Functions[Name].push_back(cast<Function>(PI->second));          
+        }
+      }
+
+  bool Changed = false;
+
+  // Now we have a list of all functions with a particular name.  If there is
+  // more than one entry in a list, merge the functions together.
+  //
+  for (std::map<string, vector<Function*> >::iterator I = Functions.begin(), 
+         E = Functions.end(); I != E; ++I) {
+    vector<Function*> &Functions = I->second;
+    Function *Implementation = 0;     // Find the implementation
+    Function *Concrete = 0;
+    for (unsigned i = 0; i < Functions.size(); ) {
+      if (!Functions[i]->isExternal()) {  // Found an implementation
+        assert(Implementation == 0 && "Multiple definitions of the same"
+               " function. Case not handled yet!");
+        Implementation = Functions[i];
+      } else {
+        // Ignore functions that are never used so they don't cause spurious
+        // warnings... here we will actually DCE the function so that it isn't
+        // used later.
+        //
+        if (Functions[i]->use_size() == 0) {
+          M->getFunctionList().remove(Functions[i]);
+          delete Functions[i];
+          Functions.erase(Functions.begin()+i);
+          Changed = true;
+          continue;
+        }
+      }
+      
+      if (Functions[i] && (!Functions[i]->getFunctionType()->isVarArg())) {
+        if (Concrete) {  // Found two different functions types.  Can't choose
+          Concrete = 0;
+          break;
+        }
+        Concrete = Functions[i];
+      }
+      ++i;
+    }
+
+    if (Functions.size() > 1) {         // Found a multiply defined function...
+      // We should find exactly one non-vararg function definition, which is
+      // probably the implementation.  Change all of the function definitions
+      // and uses to use it instead.
+      //
+      if (!Concrete) {
+        cerr << "Warning: Found functions types that are not compatible:\n";
+        for (unsigned i = 0; i < Functions.size(); ++i) {
+          cerr << "\t" << Functions[i]->getType()->getDescription() << " %"
+               << Functions[i]->getName() << "\n";
+        }
+        cerr << "  No linkage of functions named '" << Functions[0]->getName()
+             << "' performed!\n";
+      } else {
+        for (unsigned i = 0; i < Functions.size(); ++i)
+          if (Functions[i] != Concrete) {
+            Function *Old = Functions[i];
+            const FunctionType *OldMT = Old->getFunctionType();
+            const FunctionType *ConcreteMT = Concrete->getFunctionType();
+            bool Broken = false;
+
+            assert(Old->getReturnType() == Concrete->getReturnType() &&
+                   "Differing return types not handled yet!");
+            assert(OldMT->getParamTypes().size() <=
+                   ConcreteMT->getParamTypes().size() &&
+                   "Concrete type must have more specified parameters!");
+
+            // Check to make sure that if there are specified types, that they
+            // match...
+            //
+            for (unsigned i = 0; i < OldMT->getParamTypes().size(); ++i)
+              if (OldMT->getParamTypes()[i] != ConcreteMT->getParamTypes()[i]) {
+                cerr << "Parameter types conflict for" << OldMT
+                     << " and " << ConcreteMT;
+                Broken = true;
+              }
+            if (Broken) break;  // Can't process this one!
+
+
+            // Attempt to convert all of the uses of the old function to the
+            // concrete form of the function.  If there is a use of the fn
+            // that we don't understand here we punt to avoid making a bad
+            // transformation.
+            //
+            // At this point, we know that the return values are the same for
+            // our two functions and that the Old function has no varargs fns
+            // specified.  In otherwords it's just <retty> (...)
+            //
+            for (unsigned i = 0; i < Old->use_size(); ) {
+              User *U = *(Old->use_begin()+i);
+              if (CastInst *CI = dyn_cast<CastInst>(U)) {
+                // Convert casts directly
+                assert(CI->getOperand(0) == Old);
+                CI->setOperand(0, Concrete);
+                Changed = true;
+              } else if (CallInst *CI = dyn_cast<CallInst>(U)) {
+                // Can only fix up calls TO the argument, not args passed in.
+                if (CI->getCalledValue() == Old) {
+                  ConvertCallTo(CI, Concrete);
+                  Changed = true;
+                } else {
+                  cerr << "Couldn't cleanup this function call, must be an"
+                       << " argument or something!" << CI;
+                  ++i;
+                }
+              } else {
+                cerr << "Cannot convert use of function: " << U << "\n";
+                ++i;
+              }
+            }
+          }
+        }
+    }
+  }
+
+  return Changed;
+}
+
+Pass *createFunctionResolvingPass() {
+  return new FunctionResolvingPass();
+}
