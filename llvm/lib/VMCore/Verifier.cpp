0e851daf531ce97dcf296328abefdd5662879927
Chris Lattner <sabre@nondot.org>
2002-04-18 20:37:37 +0000

    * Convert the verifier to use an InstVisitor to be better structured * Implement checking that a binary operator's two sides are the same type * Actually check that an instruction does not have a name if it has a void   return type.
    llvm-svn: 2305

--- llvm/lib/VMCore/Verifier.cpp
+++ llvm/lib/VMCore/Verifier.cpp
@@ -9,17 +9,16 @@
 //  . There are no duplicated names in a symbol table... ie there !exist a val
 //    with the same name as something in the symbol table, but with a different
 //    address as what is in the symbol table...
-//  . Both of a binary operator's parameters are the same type
+//  * Both of a binary operator's parameters are the same type
 //  . Verify that arithmetic and other things are only performed on first class
 //    types.  No adding structures or arrays.
 //  . All of the constants in a switch statement are of the correct type
 //  . The code is in valid SSA form
 //  . It should be illegal to put a label into any other type (like a structure)
 //    or to return one. [except constant arrays!]
-//  . Right now 'add bool 0, 0' is valid.  This isn't particularly good.
 //  * Only phi nodes can be self referential: 'add int %0, %0 ; <int>:0' is bad
 //  * PHI nodes must have an entry for each predecessor, with no extras.
-//  * All basic blocks should only end with terminator insts, not contain them
+//  . All basic blocks should only end with terminator insts, not contain them
 //  * The entry node to a function must not have predecessors
 //  * All Instructions must be embeded into a basic block
 //  . Verify that none of the Value getType()'s are null.
@@ -45,136 +44,66 @@
 #include "llvm/Argument.h"
 #include "llvm/SymbolTable.h"
 #include "llvm/Support/CFG.h"
+#include "llvm/Support/InstVisitor.h"
 #include "Support/STLExtras.h"
 #include <algorithm>
 
-#if 0
-#define t(x) (1 << (unsigned)Type::x)
-#define SignedIntegralTypes (t(SByteTyID) | t(ShortTyID) |  \
-                             t(IntTyID)   | t(LongTyID))
-static long UnsignedIntegralTypes = t(UByteTyID) | t(UShortTyID) | 
-                                          t(UIntTyID)  | t(ULongTyID);
-static const long FloatingPointTypes    = t(FloatTyID) | t(DoubleTyID);
+namespace {  // Anonymous namespace for class
 
-static const long IntegralTypes = SignedIntegralTypes | UnsignedIntegralTypes;
+  struct Verifier : public MethodPass, InstVisitor<Verifier> {
+    bool Broken;
 
-static long ValidTypes[Type::FirstDerivedTyID] = {
-  [(unsigned)Instruction::UnaryOps::Not] t(BoolTyID),
-  //[Instruction::UnaryOps::Add] = IntegralTypes,
-  //  [Instruction::Sub] = IntegralTypes,
-};
-#undef t
-#endif
+    Verifier() : Broken(false) {}
 
-// CheckFailed - A check failed, so print out the condition and the message that
-// failed.  This provides a nice place to put a breakpoint if you want to see
-// why something is not correct.
-//
-static inline void CheckFailed(const char *Cond, const std::string &Message,
-                               const Value *V1 = 0, const Value *V2 = 0) {
-  std::cerr << Message << "\n";
-  if (V1) { std::cerr << V1 << "\n"; }
-  if (V2) { std::cerr << V2 << "\n"; }
+    bool doInitialization(Module *M) {
+      verifySymbolTable(M->getSymbolTable());
+      return false;
+    }
+
+    bool runOnMethod(Function *F) {
+      visit(F);
+      return false;
+    }
+
+    // Verification methods...
+    void verifySymbolTable(SymbolTable *ST);
+    void visitFunction(Function *F);
+    void visitBasicBlock(BasicBlock *BB);
+    void visitPHINode(PHINode *PN);
+    void visitBinaryOperator(BinaryOperator *B);
+    void visitInstruction(Instruction *I);
+
+    // CheckFailed - A check failed, so print out the condition and the message
+    // that failed.  This provides a nice place to put a breakpoint if you want
+    // to see why something is not correct.
+    //
+    inline void CheckFailed(const char *Cond, const std::string &Message,
+                            const Value *V1 = 0, const Value *V2 = 0) {
+      std::cerr << Message << "\n";
+      if (V1) { std::cerr << V1 << "\n"; }
+      if (V2) { std::cerr << V2 << "\n"; }
+      Broken = true;
+    }
+  };
 }
 
 // Assert - We know that cond should be true, if not print an error message.
 #define Assert(C, M) \
-  do { if (!(C)) { CheckFailed(#C, M); Broken = true; } } while (0)
+  do { if (!(C)) { CheckFailed(#C, M); } } while (0)
 #define Assert1(C, M, V1) \
-  do { if (!(C)) { CheckFailed(#C, M, V1); Broken = true; } } while (0)
+  do { if (!(C)) { CheckFailed(#C, M, V1); } } while (0)
 #define Assert2(C, M, V1, V2) \
-  do { if (!(C)) { CheckFailed(#C, M, V1, V2); Broken = true; } } while (0)
+  do { if (!(C)) { CheckFailed(#C, M, V1, V2); } } while (0)
 
-
-// verifyInstruction - Verify that a non-terminator instruction is well formed.
-//
-static bool verifyInstruction(const Instruction *I) {
-  bool Broken = false;
-  assert(I->getParent() && "Instruction not embedded in basic block!");
-  Assert1(!isa<TerminatorInst>(I),
-          "Terminator instruction found embedded in basic block!\n", I);
-
-  // Check that all uses of the instruction, if they are instructions
-  // themselves, actually have parent basic blocks.
-  //
-  for (User::use_const_iterator UI = I->use_begin(), UE = I->use_end();
-       UI != UE; ++UI) {
-    if (Instruction *Used = dyn_cast<Instruction>(*UI))
-      Assert2(Used->getParent() != 0, "Instruction referencing instruction not"
-              " embeded in a basic block!", I, Used);
-  }
-
-  // Check that PHI nodes look ok
-  if (const PHINode *PN = dyn_cast<PHINode>(I)) {
-    std::vector<const BasicBlock*> Preds(pred_begin(I->getParent()),
-                                         pred_end(I->getParent()));
-    // Loop over all of the incoming values, make sure that there are
-    // predecessors for each one...
-    //
-    for (unsigned i = 0, e = PN->getNumIncomingValues(); i != e; ++i) {
-      const BasicBlock *BB = PN->getIncomingBlock(i);
-      std::vector<const BasicBlock*>::iterator PI =
-        find(Preds.begin(), Preds.end(), BB);
-      Assert2(PI != Preds.end(), "PHI node has entry for basic block that"
-              " is not a predecessor!", PN, BB);
-      if (PI != Preds.end()) Preds.erase(PI);
-    }
-
-    // There should be no entries left in the predecessor list...
-    for (std::vector<const BasicBlock*>::iterator I = Preds.begin(),
-           E = Preds.end(); I != E; ++I)
-      Assert2(0, "PHI node does not have entry for a predecessor basic block!",
-              PN, *I);
-  } else {
-    // Check that non-phi nodes are not self referential...
-    for (Value::use_const_iterator UI = I->use_begin(), UE = I->use_end();
-         UI != UE; ++UI)
-      Assert1(*UI != (const User*)I,
-              "Only PHI nodes may reference their own value!", I);
-  }
-
-  return Broken;
-}
-
-
-// verifyBasicBlock - Verify that a basic block is well formed...
-//
-static bool verifyBasicBlock(const BasicBlock *BB) {
-  bool Broken = false;
-
-  // Verify all instructions, except the terminator...
-  Broken |= reduce_apply_bool(BB->begin(), BB->end()-1, verifyInstruction);
-
-  Assert1(BB->getTerminator(), "Basic Block does not have terminator!\n", BB);
-
-  // Check that the terminator is ok as well...
-  if (BB->getTerminator() && isa<ReturnInst>(BB->getTerminator())) {
-    const Instruction *I = BB->getTerminator();
-    const Function *F = I->getParent()->getParent();
-    if (I->getNumOperands() == 0)
-      Assert1(F->getReturnType() == Type::VoidTy,
-              "Function returns no value, but ret instruction found that does!",
-              I);
-    else
-      Assert2(F->getReturnType() == I->getOperand(0)->getType(),
-              "Function return type does not match operand "
-              "type of return inst!", I, F->getReturnType());
-  }
-
-
-  return Broken;
-}
 
 // verifySymbolTable - Verify that a function or module symbol table is ok
 //
-static bool verifySymbolTable(const SymbolTable *ST) {
-  if (ST == 0) return false;
-  bool Broken = false;
+void Verifier::verifySymbolTable(SymbolTable *ST) {
+  if (ST == 0) return;   // No symbol table to process
 
   // Loop over all of the types in the symbol table...
-  for (SymbolTable::const_iterator TI = ST->begin(), TE = ST->end();
-       TI != TE; ++TI)
-    for (SymbolTable::type_const_iterator I = TI->second.begin(),
+  for (SymbolTable::iterator TI = ST->begin(), TE = ST->end(); TI != TE; ++TI)
+    for (SymbolTable::type_iterator I = TI->second.begin(),
            E = TI->second.end(); I != E; ++I) {
       Value *V = I->second;
 
@@ -184,17 +113,14 @@ static bool verifySymbolTable(const SymbolTable *ST) {
       Assert1(V->getType() != Type::VoidTy,
               "Values with void type are not allowed to have names!\n", V);
     }
-
-  return Broken;
 }
 
-// verifyFunction - Verify that a function is ok.  Return true if not so that
-// verifyModule and direct clients of the verifyFunction function are correctly
-// informed.
+
+// visitFunction - Verify that a function is ok.
 //
-bool verifyFunction(const Function *F) {
-  if (F->isExternal()) return false;  // Can happen if called by verifyModule
-  bool Broken = verifySymbolTable(F->getSymbolTable());
+void Verifier::visitFunction(Function *F) {
+  if (F->isExternal()) return;
+  verifySymbolTable(F->getSymbolTable());
 
   // Check linkage of function...
   Assert1(!F->isExternal() || F->hasExternalLinkage(),
@@ -218,34 +144,125 @@ bool verifyFunction(const Function *F) {
   }
 
   // Check the entry node
-  const BasicBlock *Entry = F->getEntryNode();
+  BasicBlock *Entry = F->getEntryNode();
   Assert1(pred_begin(Entry) == pred_end(Entry),
           "Entry block to function must not have predecessors!", Entry);
-
-  Broken |= reduce_apply_bool(F->begin(), F->end(), verifyBasicBlock);
-  return Broken;
 }
 
 
-namespace {  // Anonymous namespace for class
-  struct VerifierPass : public MethodPass {
+// verifyBasicBlock - Verify that a basic block is well formed...
+//
+void Verifier::visitBasicBlock(BasicBlock *BB) {
+  Assert1(BB->getTerminator(), "Basic Block does not have terminator!\n", BB);
 
-    bool doInitialization(Module *M) {
-      verifySymbolTable(M->getSymbolTable());
-      return false;
-    }
-    bool runOnMethod(Function *F) { verifyFunction(F); return false; }
-  };
+  // Check that the terminator is ok as well...
+  if (BB->getTerminator() && isa<ReturnInst>(BB->getTerminator())) {
+    Instruction *I = BB->getTerminator();
+    Function *F = I->getParent()->getParent();
+    if (I->getNumOperands() == 0)
+      Assert1(F->getReturnType() == Type::VoidTy,
+              "Function returns no value, but ret instruction found that does!",
+              I);
+    else
+      Assert2(F->getReturnType() == I->getOperand(0)->getType(),
+              "Function return type does not match operand "
+              "type of return inst!", I, F->getReturnType());
+  }
 }
+
+
+// visitPHINode - Ensure that a PHI node is well formed.
+void Verifier::visitPHINode(PHINode *PN) {
+  std::vector<BasicBlock*> Preds(pred_begin(PN->getParent()),
+                                 pred_end(PN->getParent()));
+  // Loop over all of the incoming values, make sure that there are
+  // predecessors for each one...
+  //
+  for (unsigned i = 0, e = PN->getNumIncomingValues(); i != e; ++i) {
+    // Make sure all of the incoming values are the right types...
+    Assert2(PN->getType() == PN->getIncomingValue(i)->getType(),
+            "PHI node argument type does not agree with PHI node type!",
+            PN, PN->getIncomingValue(i));
+
+    BasicBlock *BB = PN->getIncomingBlock(i);
+    std::vector<BasicBlock*>::iterator PI =
+      find(Preds.begin(), Preds.end(), BB);
+    Assert2(PI != Preds.end(), "PHI node has entry for basic block that"
+            " is not a predecessor!", PN, BB);
+    if (PI != Preds.end()) Preds.erase(PI);
+  }
+  
+  // There should be no entries left in the predecessor list...
+  for (std::vector<BasicBlock*>::iterator I = Preds.begin(),
+         E = Preds.end(); I != E; ++I)
+    Assert2(0, "PHI node does not have entry for a predecessor basic block!",
+            PN, *I);
+
+  visitInstruction(PN);
+}
+
+
+// visitBinaryOperator - Check that both arguments to the binary operator are
+// of the same type!
+//
+void Verifier::visitBinaryOperator(BinaryOperator *B) {
+  Assert2(B->getOperand(0)->getType() == B->getOperand(1)->getType(),
+          "Both operands to a binary operator are not of the same type!",
+          B->getOperand(0), B->getOperand(1));
+
+  visitInstruction(B);
+}
+
+
+// verifyInstruction - Verify that a non-terminator instruction is well formed.
+//
+void Verifier::visitInstruction(Instruction *I) {
+  assert(I->getParent() && "Instruction not embedded in basic block!");
+
+  // Check that all uses of the instruction, if they are instructions
+  // themselves, actually have parent basic blocks.  If the use is not an
+  // instruction, it is an error!
+  //
+  for (User::use_iterator UI = I->use_begin(), UE = I->use_end();
+       UI != UE; ++UI) {
+    Assert1(isa<Instruction>(*UI), "Use of instruction is not an instruction!",
+            *UI);
+    if (Instruction *Used = dyn_cast<Instruction>(*UI))
+      Assert2(Used->getParent() != 0, "Instruction referencing instruction not"
+              " embeded in a basic block!", I, Used);
+  }
+
+  if (!isa<PHINode>(I)) {   // Check that non-phi nodes are not self referential
+    for (Value::use_iterator UI = I->use_begin(), UE = I->use_end();
+         UI != UE; ++UI)
+      Assert1(*UI != (User*)I,
+              "Only PHI nodes may reference their own value!", I);
+  }
+
+  Assert1(I->getType() != Type::VoidTy || !I->hasName(),
+          "Instruction has a name, but provides a void value!", I);
+}
+
+
+//===----------------------------------------------------------------------===//
+//  Implement the public interfaces to this file...
+//===----------------------------------------------------------------------===//
 
 Pass *createVerifierPass() {
-  return new VerifierPass();
+  return new Verifier();
+}
+
+bool verifyFunction(const Function *F) {
+  Verifier V;
+  V.visit((Function*)F);
+  return V.Broken;
 }
 
 // verifyModule - Check a module for errors, printing messages on stderr.
 // Return true if the module is corrupt.
 //
 bool verifyModule(const Module *M) {
-  return verifySymbolTable(M->getSymbolTable()) |
-         reduce_apply_bool(M->begin(), M->end(), verifyFunction);
+  Verifier V;
+  V.run((Module*)M);
+  return V.Broken;
 }
