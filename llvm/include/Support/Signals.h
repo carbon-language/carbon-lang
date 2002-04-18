67f206acc7640f983ceaa092229696e3f75a6d57
Chris Lattner <sabre@nondot.org>
2002-04-18 19:53:34 +0000

    New api for signal handling for LLVM tools
    llvm-svn: 2301

---
+++ llvm/include/Support/Signals.h
@@ -0,0 +1,18 @@
+//===- Support/Signals.h - Signal Handling support ---------------*- C++ -*--=//
+//
+// This file defines some helpful functions for dealing with the possibility of
+// unix signals occuring while your program is running.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_SIGNALS_H
+#define SUPPORT_SIGNALS_H
+
+#include <string>
+
+// RemoveFileOnSignal - This function registers signal handlers to ensure that
+// if a signal gets delivered that the named file is removed.
+//
+void RemoveFileOnSignal(const std::string &Filename);
+
+#endif
