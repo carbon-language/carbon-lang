1c2329ade33de1693492f97a1e4e26df6d2d78cc
Chris Lattner <sabre@nondot.org>
2002-04-18 19:53:53 +0000

    Add new api for basic signal handling for tools
    llvm-svn: 2302

---
+++ llvm/support/lib/Support/Signals.cpp
@@ -0,0 +1,53 @@
+//===- Signals.cpp - Signal Handling support ------------------------------===//
+//
+// This file defines some helpful functions for dealing with the possibility of
+// unix signals occuring while your program is running.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/Signals.h"
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+#include <cstdio>
+#include <signal.h>
+
+static vector<string> FilesToRemove;
+
+// IntSigs - Signals that may interrupt the program at any time.
+static const int IntSigs[] = {
+  SIGHUP, SIGINT, SIGQUIT, SIGKILL, SIGPIPE, SIGTERM, SIGUSR1, SIGUSR2
+};
+static const int *IntSigsEnd = IntSigs + sizeof(IntSigs)/sizeof(IntSigs[0]);
+
+// KillSigs - Signals that are synchronous with the program that will cause it
+// to die.
+static const int KillSigs[] = {
+  SIGILL, SIGTRAP, SIGABRT, SIGEMT, SIGFPE,
+  SIGBUS, SIGSEGV, SIGSYS, SIGXCPU, SIGXFSZ
+};
+static const int *KillSigsEnd = KillSigs + sizeof(KillSigs)/sizeof(KillSigs[0]);
+
+
+// SignalHandler - The signal handler that runs...
+static void SignalHandler(int Sig) {
+  while (!FilesToRemove.empty()) {
+    std::remove(FilesToRemove.back().c_str());
+    FilesToRemove.pop_back();
+  }
+
+  if (find(IntSigs, IntSigsEnd, Sig) != IntSigsEnd)
+    exit(1);   // If this is an interrupt signal, exit the program
+
+  // Otherwise if it is a fault (like SEGV) reissue the signal to die...
+}
+
+static void RegisterHandler(int Signal) { signal(Signal, SignalHandler); }
+
+// RemoveFileOnSignal - The public API
+void RemoveFileOnSignal(const string &Filename) {
+  FilesToRemove.push_back(Filename);
+
+  for_each(IntSigs, IntSigsEnd, RegisterHandler);
+  for_each(KillSigs, KillSigsEnd, RegisterHandler);
+}
