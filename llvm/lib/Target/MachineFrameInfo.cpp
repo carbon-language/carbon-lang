b63c4886c492b928f5f4029b5bb596e590733e4c
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-04-25 04:35:27 +0000

    Implementation of class MachineFrameInfo.
    llvm-svn: 2313

---
+++ llvm/lib/Target/MachineFrameInfo.cpp
@@ -0,0 +1,50 @@
+// $Id$ -*-c++-*-
+//***************************************************************************
+// File:
+//	MachineFrameInfo.cpp
+// 
+// Purpose:
+//	Interface to layout of stack frame on target machine.
+//      Most functions of class MachineFrameInfo have to be machine-specific
+//      so there is little code here.
+// 
+// History:
+//	 4/17/02	 -  Vikram Adve  -  Created
+//**************************************************************************/
+
+
+#include "llvm/Target/MachineFrameInfo.h"
+#include "llvm/CodeGen/MachineCodeForMethod.h"
+
+
+int
+MachineFrameInfo::getIncomingArgOffset(MachineCodeForMethod& mcInfo,
+                                       unsigned argNum) const
+{
+  assert(argsOnStackHaveFixedSize()); 
+  
+  unsigned relativeOffset = argNum * getSizeOfEachArgOnStack();
+  bool growUp;                          // do args grow up or down
+  int firstArg = getFirstIncomingArgOffset(mcInfo, growUp);
+  int offset = growUp? firstArg + relativeOffset 
+                     : firstArg - relativeOffset; 
+  return offset; 
+}
+
+
+int
+MachineFrameInfo::getOutgoingArgOffset(MachineCodeForMethod& mcInfo,
+                                       unsigned argNum) const
+{
+  assert(argsOnStackHaveFixedSize()); 
+  assert(((int) argNum - this->getNumFixedOutgoingArgs())
+         <= (int) mcInfo.getMaxOptionalNumArgs());
+  
+  unsigned relativeOffset = argNum * getSizeOfEachArgOnStack();
+  bool growUp;                          // do args grow up or down
+  int firstArg = getFirstOutgoingArgOffset(mcInfo, growUp);
+  int offset = growUp? firstArg + relativeOffset 
+                     : firstArg - relativeOffset; 
+  
+  return offset; 
+}
