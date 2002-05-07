18409a4fb7f88527bd722ba9cb2ebd4d0bd27531
Chris Lattner <sabre@nondot.org>
2002-05-07 18:34:57 +0000

    Update header after moving file
    llvm-svn: 2522

--- llvm/include/llvm/Transforms/Utils/Linker.h
+++ llvm/include/llvm/Transforms/Utils/Linker.h
@@ -1,15 +1,14 @@
-//===- llvm/Transforms/Linker.h - Module Linker Interface --------*- C++ -*--=//
+//===- llvm/Transforms/Utils/Linker.h - Module Linker Interface --*- C++ -*--=//
 //
 // This file defines the interface to the module linker.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_TRANSFORMATIONS_LINKER_H
-#define LLVM_TRANSFORMATIONS_LINKER_H
+#ifndef LLVM_TRANSFORMATIONS_UTILS_LINKER_H
+#define LLVM_TRANSFORMATIONS_UTILS_LINKER_H
 
 #include <string>
 class Module;
-
 
 // LinkModules - This function links two modules together, with the resulting
 // left module modified to be the composite of the two input modules.  If an
