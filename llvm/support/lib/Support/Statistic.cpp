9eb00525486071acd23b28f6ee481aa56813084a
Chris Lattner <sabre@nondot.org>
2002-05-10 15:36:46 +0000

    Initial checkin of Statistic class.
    llvm-svn: 2598

---
+++ llvm/support/lib/Support/Statistic.cpp
@@ -0,0 +1,31 @@
+//===-- StatisticReporter.cpp - Easy way to expose stats information -------==//
+//
+// This file implements the 'Statistic' class, which is designed to be an easy
+// way to expose various success metrics from passes.  These statistics are
+// printed at the end of a run, when the -stats command line option is enabled
+// on the command line.
+//
+// This is useful for reporting information like the number of instructions
+// simplified, optimized or removed by various transformations, like this:
+//
+// static Statistic<> NumInstEliminated("GCSE - Number of instructions killed");
+//
+// Later, in the code: ++NumInstEliminated;
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/StatisticReporter.h"
+#include "Support/CommandLine.h"
+#include <iostream>
+
+static cl::Flag Enabled("stats", "Enable statistics output from program");
+
+// Print information when destroyed, iff command line option is specified
+void StatisticBase::destroy() const {
+  if (Enabled && hasSomeData()) {
+    std::cerr.width(7);
+    printValue(std::cerr);
+    std::cerr.width(0);
+    std::cerr << "\t" << Name << "\n";
+  }
+}
