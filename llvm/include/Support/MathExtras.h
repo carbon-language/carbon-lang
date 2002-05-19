84dc8633e81b44b4a3097fc057453d26d457543a
Chris Lattner <sabre@nondot.org>
2002-05-19 21:19:55 +0000

    Cleanups, rename IsPowerOf2 to isPowerOf2
    llvm-svn: 2662

--- llvm/include/Support/MathExtras.h
+++ llvm/include/Support/MathExtras.h
@@ -1,38 +1,29 @@
-// $Id$ -*-c++-*-
-//***************************************************************************
-// File:
-//	MathExtras.h
-// 
-// Purpose:
-//	
-// History:
-//	8/25/01	 -  Vikram Adve  -  Created
-//**************************************************************************/
+//===-- Support/MathExtras.h - Useful math functions -------------*- C++ -*--=//
+//
+// This file contains some functions that are useful for math stuff.
+//
+//===----------------------------------------------------------------------===//
 
-#ifndef LLVM_SUPPORT_MATH_EXTRAS_H
-#define LLVM_SUPPORT_MATH_EXTRAS_H
+#ifndef SUPPORT_MATH_EXTRAS_H
+#define SUPPORT_MATH_EXTRAS_H
 
 #include <Support/DataTypes.h>
 
-inline unsigned
-log2(uint64_t C)
-{
+inline unsigned log2(uint64_t C) {
   unsigned getPow;
-  for (getPow = 0; C > 1; getPow++)
-    C = C >> 1;
+  for (getPow = 0; C > 1; ++getPow)
+    C >>= 1;
   return getPow;
 }
 
-inline bool
-IsPowerOf2(int64_t C, unsigned& getPow)
-{
-  if (C < 0)
-    C = -C;
-  bool isPowerOf2 = C > 0 && (C == (C & ~(C - 1)));
-  if (isPowerOf2)
+inline bool isPowerOf2(int64_t C, unsigned &getPow) {
+  if (C < 0) C = -C;
+  if (C > 0 && C == (C & ~(C - 1))) {
     getPow = log2(C);
-  
-  return isPowerOf2;
+    return true;
+  }
+
+  return false;
 }
 
-#endif /*LLVM_SUPPORT_MATH_EXTRAS_H*/
+#endif
