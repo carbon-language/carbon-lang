88a600a5fde371b6fa9a9aba23891b983efbcfa3
Chris Lattner <sabre@nondot.org>
2002-05-23 20:58:45 +0000

    New testcase, to be inspected by hand
    llvm-svn: 2739

---
+++ llvm/test/Regression/CFrontend/2002-05-23-StaticValues.c
@@ -0,0 +1,13 @@
+/* Make sure the frontend is correctly marking static stuff as internal! */
+
+int X;
+static int Y = 12;
+
+static void foo(int Z) {
+  Y = Z;
+}
+
+void *test() {
+	foo(12);
+	return &Y;
+}
