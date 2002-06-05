0a5aca621a2f16afa8e3e2e6808706dbf1c56a68
Chris Lattner <sabre@nondot.org>
2002-06-05 17:55:27 +0000

    Fix const problems
    llvm-svn: 2760

--- llvm/support/lib/Support/NameMangling.cpp
+++ llvm/support/lib/Support/NameMangling.cpp
@@ -17,15 +17,15 @@ string MangleTypeName(const Type *Ty) {
   if (Ty->isPrimitiveType()) {
     const string &longName = Ty->getDescription();
     return string(longName.c_str(), (longName.length() < 2) ? 1 : 2);
-  } else if (PointerType *PTy = dyn_cast<PointerType>(Ty)) {
+  } else if (const PointerType *PTy = dyn_cast<PointerType>(Ty)) {
     mangledName = string("P_" + MangleTypeName(PTy->getElementType()));
-  } else if (StructType *STy = dyn_cast<StructType>(Ty)) {
+  } else if (const StructType *STy = dyn_cast<StructType>(Ty)) {
     mangledName = string("S_");
     for (unsigned i=0; i < STy->getNumContainedTypes(); ++i)
       mangledName += MangleTypeName(STy->getContainedType(i));
-  } else if (ArrayType *ATy = dyn_cast<ArrayType>(Ty)) {
+  } else if (const ArrayType *ATy = dyn_cast<ArrayType>(Ty)) {
     mangledName = string("A_" +MangleTypeName(ATy->getElementType()));
-  } else if (FunctionType *FTy = dyn_cast<FunctionType>(Ty)) {
+  } else if (const FunctionType *FTy = dyn_cast<FunctionType>(Ty)) {
     mangledName = string("M_") + MangleTypeName(FTy->getReturnType());
     for (unsigned i = 1; i < FTy->getNumContainedTypes(); ++i)
       mangledName += string(MangleTypeName(FTy->getContainedType(i)));
