af26b39bc540e70ef46e0ac225b70d16ced78576
Anand Shukla <ashukla@cs.uiuc.edu>
2002-06-25 21:12:25 +0000

    added include<iostream> for cerr
    llvm-svn: 2793

--- llvm/lib/Transforms/LevelRaise.cpp
+++ llvm/lib/Transforms/LevelRaise.cpp
@@ -18,6 +18,7 @@
 #include "Support/STLExtras.h"
 #include "Support/StatisticReporter.h"
 #include <algorithm>
+#include <iostream>
 using std::cerr;
 
 static Statistic<> NumLoadStorePeepholes("raise\t\t- Number of load/store peepholes");
