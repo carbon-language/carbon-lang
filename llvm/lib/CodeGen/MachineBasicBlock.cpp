f0b84cefb0b11df43906bf00ebed7b40d05f4b6c
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-07-08 23:01:11 +0000

    Implementation of class MachineCodeForBasicBlock. Moved here from MachineInstr.cpp to make it an annotation on BasicBlock.
    llvm-svn: 2827

---
+++ llvm/lib/CodeGen/MachineBasicBlock.cpp
@@ -0,0 +1,28 @@
+//===-- llvm/CodeGen/MachineCodeForBasicBlock.cpp ---------------*- C++ -*--=//
+// 
+// Purpose:
+//   Collect the sequence of machine instructions for a basic block.
+//===---------------------------------------------------------------------===//
+
+#include "llvm/CodeGen/MachineCodeForBasicBlock.h"
+
+AnnotationID MCFBB_AID(
+             AnnotationManager::getID("CodeGen::MachineCodeForBasicBlock"));
+
+static Annotation *CreateMCFBB(AnnotationID AID, const Annotable *, void *) {
+  assert(AID == MCFBB_AID);
+  return new MachineCodeForBasicBlock();  // Invoke constructor!
+}
+
+// Register the annotation with the annotation factory
+static struct MCFBBInitializer {
+  MCFBBInitializer() {
+    AnnotationManager::registerAnnotationFactory(MCFBB_AID, &CreateMCFBB);
+  }
+} RegisterCreateMCFBB;
+
+
+MachineCodeForBasicBlock::MachineCodeForBasicBlock()
+  : Annotation(MCFBB_AID)
+{}
+
