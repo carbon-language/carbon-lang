86fa226049a9d0df7096f7779a4a7dc5da0bcf12
Chris Lattner <sabre@nondot.org>
2002-07-23 19:48:52 +0000

    Prepare the file to become IPO.h
    llvm-svn: 3029

--- llvm/include/llvm/Transforms/IPO.h
+++ llvm/include/llvm/Transforms/IPO.h
@@ -1,29 +1,21 @@
-//===- llvm/Transforms/CleanupGCCOutput.h - Cleanup GCC Output ---*- C++ -*--=//
+//===- llvm/Transforms/IPO.h - Interprocedural Optimiations -----*- C++ -*-===//
 //
-// These passes are used to cleanup the output of GCC.  GCC's output is
-// unneccessarily gross for a couple of reasons. This pass does the following
-// things to try to clean it up:
-//
-// * Eliminate names for GCC types that we know can't be needed by the user.
-// * Eliminate names for types that are unused in the entire translation unit
-// * Fix various problems that we might have in PHI nodes and casts
-// * Link uses of 'void %foo(...)' to 'void %foo(sometypes)'
-//
-// Note:  This code produces dead declarations, it is a good idea to run DCE
-//        after this pass.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_TRANSFORMS_CLEANUPGCCOUTPUT_H
-#define LLVM_TRANSFORMS_CLEANUPGCCOUTPUT_H
+#ifndef LLVM_TRANSFORMS_IPO_H
+#define LLVM_TRANSFORMS_IPO_H
 
 class Pass;
 
-// CleanupGCCOutputPass - Perform all of the function body transformations.
+//===----------------------------------------------------------------------===//
+// createDeadTypeEliminationPass - Return a new pass that eliminates symbol
+// table entries for types that are never used.
 //
-Pass *createCleanupGCCOutputPass();
+Pass *createDeadTypeEliminationPass();
 
 
+//===----------------------------------------------------------------------===//
 // FunctionResolvingPass - Go over the functions that are in the module and
 // look for functions that have the same name.  More often than not, there will
 // be things like:
