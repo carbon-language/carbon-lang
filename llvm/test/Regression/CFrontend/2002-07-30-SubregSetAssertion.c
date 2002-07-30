56ea9cd7c248d014b86ba82f364943939c9f60d0
Chris Lattner <sabre@nondot.org>
2002-07-30 06:07:16 +0000

    Simplify testcase a bit
    llvm-svn: 3150

--- llvm/test/Regression/CFrontend/2002-07-30-SubregSetAssertion.c
+++ llvm/test/Regression/CFrontend/2002-07-30-SubregSetAssertion.c
@@ -1,13 +1,9 @@
 
 union X {
-  //char C;
-  //int A;
   void *B;
 };
 
-union X foo(union X A) {
-	//A.C = 123;
-	//A.A = 39249;
-	A.B = (void*)123040123321;
-	return A;
+union X void foo() {
+	union X A;
+	A.B = (void*)123
 }
