8858f0272de053489d80fd8081e9838a4b0459e2
Chris Lattner <sabre@nondot.org>
2002-08-19 21:48:09 +0000

    New testcase
    llvm-svn: 3390

---
+++ llvm/test/Regression/CFrontend/2002-08-19-RecursiveLocals.c
@@ -0,0 +1,16 @@
+/* This testcase doesn't actually test a bug, it's just the result of me 
+ * figuring out the syntax for forward declaring a static variable. */
+struct list {
+  int x;
+  struct list *Next;
+};
+
+static struct list B;  /* Forward declare static */
+static struct list A = { 7, &B };
+static struct list B = { 8, &A };
+
+extern struct list D;  /* forward declare normal var */
+
+struct list C = { 7, &D };
+struct list D = { 8, &C };
+
