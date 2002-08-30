f7e2daf5a42461c52f7f428ebb05063d3c009005
Chris Lattner <sabre@nondot.org>
2002-08-30 22:51:08 +0000

    Fix comments.
    llvm-svn: 3546

--- llvm/include/llvm/Analysis/Verifier.h
+++ llvm/include/llvm/Analysis/Verifier.h
@@ -1,7 +1,8 @@
 //===-- llvm/Analysis/Verifier.h - Module Verifier ---------------*- C++ -*-==//
 //
 // This file defines the function verifier interface, that can be used for some
-// sanity checking of input to the system.
+// sanity checking of input to the system, and for checking that transformations
+// haven't done something bad.
 //
 // Note that this does not provide full 'java style' security and verifications,
 // instead it just tries to ensure that code is well formed.
@@ -23,7 +24,8 @@ class Function;
 Pass *createVerifierPass();
 
 // verifyModule - Check a module for errors, printing messages on stderr.
-// Return true if the module is corrupt.
+// Return true if the module is corrupt.  This should only be used for
+// debugging, because it plays games with PassManagers and stuff.
 //
 bool verifyModule(const Module &M);
 
