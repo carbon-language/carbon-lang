bc56c86ca480497c307e6f140ced54fd9bb48fa5
Chris Lattner <sabre@nondot.org>
2002-08-31 00:28:25 +0000

    Convert cwriter to be a pass.
    llvm-svn: 3557

--- llvm/include/llvm/Assembly/CWriter.h
+++ llvm/include/llvm/Assembly/CWriter.h
@@ -1,21 +1,15 @@
-//===-- llvm/CGen/Writer.h - Printer for VM assembly files -------*- C++ -*--=//
+//===-- llvm/Assembly/CWriter.h - C Printer for LLVM programs --*- C++ -*--===//
 //
-// This functionality is implemented by the lib/CWriter library.
-// This library is used to print C language files to an iostream. 
-//
+// This functionality is implemented by the lib/CWriter library.  This library
+// is used to print C language files to an iostream.
 //
 //===----------------------------------------------------------------------===//
 
-#ifndef LLVM_C_WRITER_H
-#define LLVM_C_WRITER_H
-
-class Module;
+#ifndef LLVM_ASSEMBLY_CWRITER_H
+#define LLVM_ASSEMBLY_CWRITER_H
 
 #include <iosfwd>
-
-void WriteToC(const Module  *Module, std::ostream &o);
+class Pass;
+Pass *createWriteToCPass(std::ostream &o);
 
 #endif
-
-
-
