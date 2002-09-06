b6aae148d21c1749b08abb0327d7ac45e978492b
Chris Lattner <sabre@nondot.org>
2002-09-06 21:04:22 +0000

    Remove obsolete script
    llvm-svn: 3602

--- llvm/getsomesrcs.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# This script prints out some of the source files that are useful when 
-# editing.  I use this like this: xemacs `./getsomesrcs.sh` &
-./getsrcs.sh | grep -v Assembly | grep -v Byte | grep -v \\.ll | grep -v Optimization | grep -v llvm/Support | grep -v llvm/CodeGen | grep -v '/i[^/]*$' | grep -v Holder | grep -v Analysis | grep -v html | grep include/llvm
-
-
