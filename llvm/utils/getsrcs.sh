88cb597cbc98efa9c853ef80ca206a363592a0a7
Chris Lattner <sabre@nondot.org>
2002-09-06 21:04:36 +0000

    Fix script due to new www/ top level directory
    llvm-svn: 3603

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # This is useful because it prints out all of the source files.  Useful for
 # greps.
-find docs include lib tools -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' | grep -v '\.dir$' | grep -v docs/doxygen | grep -v include/boost
+find www include lib tools -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' | grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost
 
