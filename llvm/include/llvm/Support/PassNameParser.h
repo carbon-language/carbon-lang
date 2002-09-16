4b775b27638eaba95d3896b9c391d91b29cbe5ab
Vikram S. Adve <vadve@cs.uiuc.edu>
2002-09-16 16:01:41 +0000

    *** empty log message ***
    llvm-svn: 3749

--- llvm/include/llvm/Support/PassNameParser.h
+++ llvm/include/llvm/Support/PassNameParser.h
@@ -47,7 +47,8 @@ public:
     // Ignore non-selectable and non-constructible passes!  Ignore
     // non-optimizations.
     return P->getPassArgument() == 0 ||
-          (P->getNormalCtor() == 0 && P->getDataCtor() == 0) ||
+          (P->getNormalCtor() == 0 && P->getDataCtor() == 0 &&
+           P->getTargetCtor() == 0) ||
           ignorablePassImpl(P);
   }
 
