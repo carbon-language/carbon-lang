9f48a00e43c9c217b4612c2e94132c136aa3ceb9
Chris Lattner <sabre@nondot.org>
2002-09-24 00:08:37 +0000

    Add new BreakCriticalEdges pass
    llvm-svn: 3902

--- llvm/include/llvm/Transforms/Scalar.h
+++ llvm/include/llvm/Transforms/Scalar.h
@@ -175,6 +175,16 @@ Pass *createCorrelatedExpressionEliminationPass();
 //
 Pass *createCFGSimplificationPass();
 
+//===----------------------------------------------------------------------===//
+//
+// BreakCriticalEdges pass - Break all of the critical edges in the CFG by
+// inserting a dummy basic block.  This pass may be "required" by passes that
+// cannot deal with critical edges.  For this usage, the structure type is
+// forward declared.  This pass obviously invalidates the CFG, but can update
+// forward dominator (set, immediate dominators, and tree) information.
+//
+class BreakCriticalEdges;
+Pass *createBreakCriticalEdgesPass();
 
 //===----------------------------------------------------------------------===//
 // These two passes convert malloc and free instructions to and from %malloc &
