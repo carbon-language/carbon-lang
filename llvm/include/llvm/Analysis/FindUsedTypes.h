fc75a8552d3b5d4b7c95a56bab90ead937374940
Chris Lattner <sabre@nondot.org>
2002-09-24 00:07:17 +0000

    Make users of FindUsedTypes not have problems with linkage.  This fixes cwriter.
    llvm-svn: 3899

--- llvm/include/llvm/Analysis/FindUsedTypes.h
+++ llvm/include/llvm/Analysis/FindUsedTypes.h
@@ -15,33 +15,38 @@ class Type;
 class FindUsedTypes : public Pass {
   std::set<const Type *> UsedTypes;
 public:
-  // getTypes - After the pass has been run, return the set containing all of
-  // the types used in the module.
-  //
+  /// getTypes - After the pass has been run, return the set containing all of
+  /// the types used in the module.
+  ///
   const std::set<const Type *> &getTypes() const { return UsedTypes; }
 
-  // Print the types found in the module.  If the optional Module parameter is
-  // passed in, then the types are printed symbolically if possible, using the
-  // symbol table from the module.
-  //
+  /// Print the types found in the module.  If the optional Module parameter is
+  /// passed in, then the types are printed symbolically if possible, using the
+  /// symbol table from the module.
+  ///
   void print(std::ostream &o, const Module *M) const;
 
 private:
-  // IncorporateType - Incorporate one type and all of its subtypes into the
-  // collection of used types.
-  //
+  /// IncorporateType - Incorporate one type and all of its subtypes into the
+  /// collection of used types.
+  ///
   void IncorporateType(const Type *Ty);
 
 public:
-  // run - This incorporates all types used by the specified module
-  //
+  /// run - This incorporates all types used by the specified module
   bool run(Module &M);
 
-  // getAnalysisUsage - Of course, we provide ourself...
-  //
+  /// getAnalysisUsage - We do not modify anything.
   virtual void getAnalysisUsage(AnalysisUsage &AU) const {
     AU.setPreservesAll();
   }
+
+  // stub - dummy function, just ignore it
+  static void stub();
 };
+
+// Make sure that any clients of this file link in PostDominators.cpp
+static IncludeFile
+FIND_USED_TYPES_INCLUDE_FILE((void*)&FindUsedTypes::stub);
 
 #endif
