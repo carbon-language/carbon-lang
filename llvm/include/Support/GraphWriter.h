e515976a116ee72ab57397b76385f3883830dd77
Chris Lattner <sabre@nondot.org>
2002-10-16 01:34:18 +0000

    * Significantly refactor GraphWriter into a class.
    llvm-svn: 4194

--- llvm/include/Support/GraphWriter.h
+++ llvm/include/Support/GraphWriter.h
@@ -49,74 +49,102 @@ namespace DOT {  // Private functions...
 }
 
 template<typename GraphType>
-std::ostream &WriteGraph(std::ostream &O, const GraphType &G) {
-  typedef DOTGraphTraits<GraphType>  DOTTraits;
-  typedef GraphTraits<GraphType>     GTraits;
-  typedef typename GTraits::NodeType NodeType;
-  typedef typename GTraits::nodes_iterator node_iterator;
+class GraphWriter {
+  std::ostream &O;
+  const GraphType &G;
 
-  O << "digraph foo {\n";        // Graph name doesn't matter
-  std::string GraphName = DOTTraits::getGraphName(G);
-  if (!GraphName.empty())
-    O << "\tlabel=\"" << DOT::EscapeString(GraphName) << "\";\n";
-  O << DOTTraits::getGraphProperties(G);
-  O << "\n";
+  typedef DOTGraphTraits<GraphType>           DOTTraits;
+  typedef GraphTraits<GraphType>              GTraits;
+  typedef typename GTraits::NodeType          NodeType;
+  typedef typename GTraits::nodes_iterator    node_iterator;
+  typedef typename GTraits::ChildIteratorType child_iterator;
+public:
+  GraphWriter(std::ostream &o, const GraphType &g,
+              const std::string &Name) : O(o), G(g) {
+    if (Name.empty())
+      O << "digraph foo {\n";        // Graph name doesn't matter
+    else
+      O << "digraph " << Name << " {\n";
 
-  // Loop over the graph in DFO, printing it out...
-  for (node_iterator I = GTraits::nodes_begin(G), E = GTraits::nodes_end(G);
-       I != E; ++I) {
-    NodeType *Node = &*I;
+    std::string GraphName = DOTTraits::getGraphName(G);
+    if (!GraphName.empty())
+      O << "\tlabel=\"" << DOT::EscapeString(GraphName) << "\";\n";
+    O << DOTTraits::getGraphProperties(G);
+    O << "\n";
 
+    writeNodes();
+  }
+
+  ~GraphWriter() {
+    // Finish off the graph
+    O << "}\n";
+  }
+
+  void writeNodes() {
+    // Loop over the graph, printing it out...
+    for (node_iterator I = GTraits::nodes_begin(G), E = GTraits::nodes_end(G);
+         I != E; ++I)
+      writeNode(&*I);
+  }
+
+  void writeNode(NodeType *Node) {
     std::string NodeAttributes = DOTTraits::getNodeAttributes(Node);
-
+      
     O << "\tNode" << (void*)Node << " [shape=record,";
     if (!NodeAttributes.empty()) O << NodeAttributes << ",";
     O << "label=\"{"
       << DOT::EscapeString(DOTTraits::getNodeLabel(Node, G));
     
     // Print out the fields of the current node...
-    typename GTraits::ChildIteratorType EI = GTraits::child_begin(Node);
-    typename GTraits::ChildIteratorType EE = GTraits::child_end(Node);
+    child_iterator EI = GTraits::child_begin(Node);
+    child_iterator EE = GTraits::child_end(Node);
     if (EI != EE) {
       O << "|{";
-
+      
       for (unsigned i = 0; EI != EE && i != 64; ++EI, ++i) {
         if (i) O << "|";
         O << "<g" << i << ">" << DOTTraits::getEdgeSourceLabel(Node, EI);
       }
-
+      
       if (EI != EE)
         O << "|truncated...";
       O << "}";
     }
     O << "}\"];\n";   // Finish printing the "node" line
-
+    
     // Output all of the edges now
     EI = GTraits::child_begin(Node);
-    for (unsigned i = 0; EI != EE && i != 64; ++EI, ++i) {
-      if (NodeType *TargetNode = *EI) {
-        O << "\tNode" << (void*)Node << ":g" << i << " -> Node"
-          << (void*)TargetNode;
-        if (DOTTraits::edgeTargetsEdgeSource(Node, EI)) {
-          typename GTraits::ChildIteratorType TargetIt =
-            DOTTraits::getEdgeTarget(Node, EI);
-          // Figure out which edge this targets...
-          unsigned Offset = std::distance(GTraits::child_begin(TargetNode),
-                                          TargetIt);
-          if (Offset > 64) Offset = 64;  // Targetting the trancated part?
-          O << ":g" << Offset;        
-        }
-        
-        std::string EdgeAttributes = DOTTraits::getEdgeAttributes(Node, EI);
-        if (!EdgeAttributes.empty())
-          O << "[" << EdgeAttributes << "]";
-        O << ";\n";
-      }
-    }
+    for (unsigned i = 0; EI != EE && i != 64; ++EI, ++i)
+      writeEdge(Node, i, EI);
   }
 
-  // Finish off the graph
-  O << "}\n";
+  void writeEdge(NodeType *Node, unsigned edgeidx, child_iterator EI) {
+    if (NodeType *TargetNode = *EI) {
+      O << "\tNode" << (void*)Node << ":g" << edgeidx << " -> Node"
+        << (void*)TargetNode;
+      if (DOTTraits::edgeTargetsEdgeSource(Node, EI)) {
+        child_iterator TargetIt = DOTTraits::getEdgeTarget(Node, EI);
+
+        // Figure out which edge this targets...
+        unsigned Offset = std::distance(GTraits::child_begin(TargetNode),
+                                        TargetIt);
+        if (Offset > 64) Offset = 64;  // Targetting the truncated part?
+        O << ":g" << Offset;        
+      }
+      
+      std::string EdgeAttributes = DOTTraits::getEdgeAttributes(Node, EI);
+      if (!EdgeAttributes.empty())
+        O << "[" << EdgeAttributes << "]";
+      O << ";\n";
+    }
+  }
+};
+
+template<typename GraphType>
+std::ostream &WriteGraph(std::ostream &O, const GraphType &G,
+                         const std::string &Name = "") {
+  // Start the graph emission process...
+  GraphWriter<GraphType> W(O, G, Name);
   return O;
 }
 
