45bb78fc066593409798d542ad8f8eb80b6f4117
Chris Lattner <sabre@nondot.org>
2002-10-23 01:11:51 +0000

    Minor cleanups
    llvm-svn: 4267

--- llvm/include/llvm/CodeGen/StackSlots.h
+++ llvm/include/llvm/CodeGen/StackSlots.h
@@ -1,16 +1,15 @@
 //===-- llvm/CodeGen/StackSots.h -------------------------------*- C++ -*--===//
 //
-// External interface to stack-slots pass that enters 2 empty slots
-// at the top of each function stack
+// External interface to stack-slots pass that enters 2 empty slots at the top
+// of each function stack
 //
 //===----------------------------------------------------------------------===//
 
 #ifndef LLVM_CODEGEN_STACKSLOTS_H
 #define LLVM_CODEGEN_STACKSLOTS_H
 
-class TargetMachine;
 class Pass;
-
-Pass *createStackSlotsPass(TargetMachine &Target);
+class TargetMachine;
+Pass *createStackSlotsPass(const TargetMachine &TM);
 
 #endif
