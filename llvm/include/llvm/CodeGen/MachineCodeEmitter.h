b0a750cf3939a096570c4afc05fd2de264344186
Chris Lattner <sabre@nondot.org>
2002-12-02 21:21:36 +0000

    Initial version of MachineCodeEmitter interface: empty
    llvm-svn: 4864

---
+++ llvm/include/llvm/CodeGen/MachineCodeEmitter.h
@@ -0,0 +1,20 @@
+//===-- llvm/CodeGen/MachineCodeEmitter.h - Code emission -------*- C++ -*-===//
+//
+// This file defines an abstract interface that is used by the machine code
+// emission framework to output the code.  This allows machine code emission to
+// be seperated from concerns such as resolution of call targets, and where the
+// machine code will be written (memory or disk, f.e.).
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CODEGEN_MACHINE_CODE_EMITTER_H
+#define LLVM_CODEGEN_MACHINE_CODE_EMITTER_H
+
+struct MachineCodeEmitter {
+
+
+  
+
+};
+
+#endif
