2e6f17f13625394f5fcab05366e41de7de5d75fa
Chris Lattner <sabre@nondot.org>
2002-12-16 23:36:57 +0000

    Build add instructions of the correct form!
    llvm-svn: 5090

--- llvm/lib/Target/X86/InstSelectSimple.cpp
+++ llvm/lib/Target/X86/InstSelectSimple.cpp
@@ -599,7 +599,7 @@ ISel::visitCallInst (CallInst & CI)
 
   // Adjust the stack by `bytesPushed' amount if non-zero
   if (bytesPushed > 0)
-    BuildMI (BB, X86::ADDri32, 2).addReg(X86::ESP).addZImm(bytesPushed);
+    BuildMI (BB, X86::ADDri32,2,X86::ESP).addReg(X86::ESP).addZImm(bytesPushed);
 
   // If there is a return value, scavenge the result from the location the call
   // leaves it in...
