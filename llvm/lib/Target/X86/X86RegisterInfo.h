4997a840bc6e9c050601bc63b2ecab3d483bc409
Chris Lattner <sabre@nondot.org>
2002-12-25 05:06:43 +0000

    Adjustments to match new simpler spill interface
    llvm-svn: 5147

--- llvm/lib/Target/X86/X86RegisterInfo.h
+++ llvm/lib/Target/X86/X86RegisterInfo.h
@@ -14,27 +14,23 @@ class Type;
 struct X86RegisterInfo : public MRegisterInfo {
   X86RegisterInfo();
 
-  MachineBasicBlock::iterator
-  storeReg2RegOffset(MachineBasicBlock &MBB,
-                     MachineBasicBlock::iterator MBBI,
-                     unsigned DestReg, unsigned SrcReg, 
-                     unsigned ImmOffset, unsigned dataSize) const;
+  void storeReg2RegOffset(MachineBasicBlock &MBB,
+			  MachineBasicBlock::iterator &MBBI,
+			  unsigned SrcReg, unsigned DestReg, unsigned ImmOffset,
+			  const TargetRegisterClass *RC) const;
 
-  MachineBasicBlock::iterator
-  loadRegOffset2Reg(MachineBasicBlock &MBB,
-                    MachineBasicBlock::iterator MBBI,
-                    unsigned DestReg, unsigned SrcReg,
-                    unsigned ImmOffset, unsigned dataSize) const;
+  void loadRegOffset2Reg(MachineBasicBlock &MBB,
+			 MachineBasicBlock::iterator &MBBI,
+			 unsigned DestReg, unsigned SrcReg, unsigned ImmOffset,
+			 const TargetRegisterClass *RC) const;
 
-  MachineBasicBlock::iterator
-  moveReg2Reg(MachineBasicBlock &MBB,
-              MachineBasicBlock::iterator MBBI,
-              unsigned DestReg, unsigned SrcReg, unsigned dataSize) const;
+  void moveReg2Reg(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
+		   unsigned DestReg, unsigned SrcReg,
+		   const TargetRegisterClass *RC) const;
 
-  MachineBasicBlock::iterator
-  moveImm2Reg(MachineBasicBlock &MBB,
-              MachineBasicBlock::iterator MBBI,
-              unsigned DestReg, unsigned Imm, unsigned dataSize) const;
+  void moveImm2Reg(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
+		   unsigned DestReg, unsigned Imm,
+		   const TargetRegisterClass *RC) const;
 
   unsigned getFramePointer() const;
   unsigned getStackPointer() const;
