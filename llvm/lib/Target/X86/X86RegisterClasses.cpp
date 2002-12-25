c9f3bbb5d290b38cd4ef107f1ca4ab5b27280e9b
Chris Lattner <sabre@nondot.org>
2002-12-25 05:08:03 +0000

    Initial support for FP registers
    llvm-svn: 5149

--- llvm/lib/Target/X86/X86RegisterClasses.cpp
+++ llvm/lib/Target/X86/X86RegisterClasses.cpp
@@ -9,6 +9,9 @@
 #include "llvm/Type.h"
 #include "X86.h"
 
+//===----------------------------------------------------------------------===//
+//   8 Bit Integer Registers
+//
 namespace {
   const unsigned ByteRegClassRegs[] = {
 #define R8(ENUM, NAME, FLAGS, TSFLAGS, ALIAS_SET) X86::ENUM,
@@ -18,8 +21,8 @@ namespace {
   TargetRegisterClass X86ByteRegisterClassInstance(1, ByteRegClassRegs,
  ByteRegClassRegs+sizeof(ByteRegClassRegs)/sizeof(ByteRegClassRegs[0]));
 
-//
-//
+//===----------------------------------------------------------------------===//
+//   16 Bit Integer Registers
 //
   const unsigned ShortRegClassRegs[] = {
 #define R16(ENUM, NAME, FLAGS, TSFLAGS, ALIAS_SET) X86::ENUM,
@@ -29,10 +32,9 @@ namespace {
   TargetRegisterClass X86ShortRegisterClassInstance(2, ShortRegClassRegs,
       ShortRegClassRegs+sizeof(ShortRegClassRegs)/sizeof(ShortRegClassRegs[0]));
 
+//===----------------------------------------------------------------------===//
+//   32 Bit Integer Registers
 //
-//
-//
-
   const unsigned IntRegClassRegs[] = {
 #define R32(ENUM, NAME, FLAGS, TSFLAGS, ALIAS_SET) X86::ENUM,
 #include "X86RegisterInfo.def"
@@ -41,10 +43,25 @@ namespace {
   TargetRegisterClass X86IntRegisterClassInstance(4, IntRegClassRegs,
       IntRegClassRegs+sizeof(IntRegClassRegs)/sizeof(IntRegClassRegs[0]));
 
+//===----------------------------------------------------------------------===//
+//   Pseudo Floating Point Registers
+//
+  const unsigned PFPRegClassRegs[] = {
+#define PFP(ENUM, NAME, FLAGS, TSFLAGS, ALIAS_SET) X86::ENUM,
+#include "X86RegisterInfo.def"
+  };
+
+  TargetRegisterClass X86FPRegisterClassInstance(10, PFPRegClassRegs,
+      PFPRegClassRegs+sizeof(PFPRegClassRegs)/sizeof(PFPRegClassRegs[0]));
+
+//===----------------------------------------------------------------------===//
+// Register class array...
+//
   const TargetRegisterClass * const X86RegClasses[] = {
     &X86ByteRegisterClassInstance,
     &X86ShortRegisterClassInstance,
-    &X86IntRegisterClassInstance
+    &X86IntRegisterClassInstance,
+    &X86FPRegisterClassInstance,
   };
 }
 
@@ -72,26 +89,23 @@ X86RegisterInfo::X86RegisterInfo()
 
 
 
-const TargetRegisterClass* X86RegisterInfo::getRegClassForType(const Type* Ty)
-  const {
+const TargetRegisterClass*
+X86RegisterInfo::getRegClassForType(const Type* Ty) const {
   switch (Ty->getPrimitiveID()) {
+  default:                assert(0 && "Invalid type to getClass!");
   case Type::BoolTyID:
   case Type::SByteTyID:
   case Type::UByteTyID:   return &X86ByteRegisterClassInstance;
   case Type::ShortTyID:
   case Type::UShortTyID:  return &X86ShortRegisterClassInstance;
-  case Type::LongTyID:    // None of these are handled yet!
-  case Type::ULongTyID:  // FIXME: Treat these like ints, this is bogus!
+  case Type::LongTyID:    // FIXME: Longs are not handled yet!
+  case Type::ULongTyID:   // FIXME: Treat these like ints, this is bogus!
     
   case Type::IntTyID:
   case Type::UIntTyID:
   case Type::PointerTyID: return &X86IntRegisterClassInstance;
     
   case Type::FloatTyID:
-  case Type::DoubleTyID:
-    
-  default:
-    assert(0 && "Invalid type to getClass!");
-    return 0;  // not reached
+  case Type::DoubleTyID: return &X86FPRegisterClassInstance;
   }
 }
