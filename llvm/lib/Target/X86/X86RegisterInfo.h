e6fb194716db4caf29077794a5f750239c1ec781
Chris Lattner <sabre@nondot.org>
2002-12-28 20:32:54 +0000

    Changes to match new MRegisterInfo api
    llvm-svn: 5187

--- llvm/lib/Target/X86/X86RegisterInfo.h
+++ llvm/lib/Target/X86/X86RegisterInfo.h
@@ -1,4 +1,4 @@
-//===- X86RegisterInfo.h - X86 Register Information Impl ----------*-C++-*-===//
+//===- X86RegisterInfo.h - X86 Register Information Impl --------*- C++ -*-===//
 //
 // This file contains the X86 implementation of the MRegisterInfo class.
 //
@@ -14,38 +14,39 @@ class Type;
 struct X86RegisterInfo : public MRegisterInfo {
   X86RegisterInfo();
 
-  void storeReg2RegOffset(MachineBasicBlock &MBB,
-			  MachineBasicBlock::iterator &MBBI,
-			  unsigned SrcReg, unsigned DestReg, unsigned ImmOffset,
-			  const TargetRegisterClass *RC) const;
-
-  void loadRegOffset2Reg(MachineBasicBlock &MBB,
-			 MachineBasicBlock::iterator &MBBI,
-			 unsigned DestReg, unsigned SrcReg, unsigned ImmOffset,
-			 const TargetRegisterClass *RC) const;
-
-  void moveReg2Reg(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
-		   unsigned DestReg, unsigned SrcReg,
-		   const TargetRegisterClass *RC) const;
-
-  void moveImm2Reg(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
-		   unsigned DestReg, unsigned Imm,
-		   const TargetRegisterClass *RC) const;
-
-  unsigned getFramePointer() const;
-  unsigned getStackPointer() const;
-
   const unsigned* getCalleeSaveRegs() const;
-  const unsigned* getCallerSaveRegs() const;
-
-  void emitPrologue(MachineFunction &MF, unsigned numBytes) const;
-  void emitEpilogue(MachineBasicBlock &MBB, unsigned numBytes) const;
 
   /// Returns register class appropriate for input SSA register
   /// 
   const TargetRegisterClass *getClassForReg(unsigned Reg) const;
-
   const TargetRegisterClass* getRegClassForType(const Type* Ty) const;
+
+  /// Code Generation virtual methods...
+  void storeRegToStackSlot(MachineBasicBlock &MBB,
+			   MachineBasicBlock::iterator &MBBI,
+			   unsigned SrcReg, int FrameIndex,
+			   const TargetRegisterClass *RC) const;
+
+  void loadRegFromStackSlot(MachineBasicBlock &MBB,
+			    MachineBasicBlock::iterator &MBBI,
+			    unsigned DestReg, int FrameIndex,
+			    const TargetRegisterClass *RC) const;
+  
+  void copyRegToReg(MachineBasicBlock &MBB, MachineBasicBlock::iterator &MBBI,
+		   unsigned DestReg, unsigned SrcReg,
+		   const TargetRegisterClass *RC) const;
+
+  void eliminateCallFramePseudoInstr(MachineFunction &MF,
+				     MachineBasicBlock &MBB,
+				     MachineBasicBlock::iterator &I) const;
+
+  void eliminateFrameIndex(MachineFunction &MF,
+			   MachineBasicBlock::iterator &II) const;
+
+  void processFunctionBeforeFrameFinalized(MachineFunction &MF) const;
+
+  void emitPrologue(MachineFunction &MF) const;
+  void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const;
 };
 
 #endif
