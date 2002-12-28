343ad0d65ca75470a274a04105e428f4daae1463
Chris Lattner <sabre@nondot.org>
2002-12-28 20:03:01 +0000

    Initial check in of MachineFunctionPass.h
    llvm-svn: 5162

---
+++ llvm/include/llvm/CodeGen/MachineFunctionPass.h
@@ -0,0 +1,33 @@
+//===-- MachineFunctionPass.h - Pass for MachineFunctions --------*-C++ -*-===//
+//
+// This file defines the MachineFunctionPass class.  MachineFunctionPass's are
+// just FunctionPass's, except they operate on machine code as part of a code
+// generator.  Because they operate on machine code, not the LLVM
+// representation, MachineFunctionPass's are not allowed to modify the LLVM
+// representation.  Due to this limitation, the MachineFunctionPass class takes
+// care of declaring that no LLVM passes are invalidated.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CODEGEN_MACHINE_FUNCTION_PASS_H
+#define LLVM_CODEGEN_MACHINE_FUNCTION_PASS_H
+
+#include "llvm/Pass.h"
+#include "llvm/CodeGen/MachineFunction.h"
+
+struct MachineFunctionPass : public FunctionPass {
+
+  /// runOnMachineFunction - This method must be overloaded to perform the
+  /// desired machine code transformation or analysis.
+  ///
+  virtual bool runOnMachineFunction(MachineFunction &MF) = 0;
+
+  // FIXME: This pass should declare that the pass does not invalidate any LLVM
+  // passes.
+
+  virtual bool runOnFunction(Function &F) {
+    return runOnMachineFunction(MachineFunction::get(&F));
+  }
+};
+
+#endif
