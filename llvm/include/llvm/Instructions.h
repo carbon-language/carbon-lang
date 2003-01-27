6ffcdb5befb9ab19aba77bd6f4ee57adb90bd5f0
Chris Lattner <sabre@nondot.org>
2003-01-27 22:08:52 +0000

    New helper header
    llvm-svn: 5423

---
+++ llvm/include/llvm/Instructions.h
@@ -0,0 +1,18 @@
+//===-- llvm/Instructions.h - Instruction subclass definitions --*- C++ -*-===//
+//
+// This file exposes the class definitions of all of the subclasses of the
+// Instruction class.  This is meant to be an easy way to get access to all
+// instruction subclasses.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_INSTRUCTIONS_H
+#define LLVM_INSTRUCTIONS_H
+
+#include "llvm/iTerminators.h"   // Terminator instructions
+#include "llvm/iPHINode.h"       // The PHI node instruction
+#include "llvm/iOperators.h"     // Binary operator instructions
+#include "llvm/iMemory.h"        // Memory related instructions
+#include "llvm/iOther.h"         // Everything else
+
+#endif
