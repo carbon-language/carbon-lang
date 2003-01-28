aa6b2bfd782f869d4b84bd51c704ebaf9d4cecab
Chris Lattner <sabre@nondot.org>
2003-01-28 20:01:16 +0000

    Initial checking: defult the make_vector function
    llvm-svn: 5424

---
+++ llvm/include/Support/VectorExtras.h
@@ -0,0 +1,29 @@
+//===-- VectorExtras.h - Helper functions for std::vector -------*- C++ -*-===//
+//
+// This file contains helper functions which are useful for working with the
+// std::vector class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_VECTOREXTRAS_H
+#define SUPPORT_VECTOREXTRAS_H
+
+#include <cstdarg>
+
+/// make_vector - Helper function which is useful for building temporary vectors
+/// to pass into type construction of CallInst ctors.  This turns a null
+/// terminated list of pointers (or other value types) into a real live vector.
+///
+template<typename T>
+inline std::vector<T> make_vector(T A, ...) {
+  va_list Args;
+  va_start(Args, A);
+  std::vector<T> Result;
+  Result.push_back(A);
+  while (T Val = va_arg(Args, T))
+    Result.push_back(Val);
+  va_end(Args);
+  return Result;
+}
+
+#endif
