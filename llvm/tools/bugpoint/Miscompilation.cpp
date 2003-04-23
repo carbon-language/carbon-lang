1b35bee37cee4885635493240449b3af072893d7
Chris Lattner <sabre@nondot.org>
2003-04-23 20:41:18 +0000

    Print where reference output goes
    llvm-svn: 5891

--- llvm/tools/bugpoint/Miscompilation.cpp
+++ llvm/tools/bugpoint/Miscompilation.cpp
@@ -43,7 +43,7 @@ bool BugDriver::debugMiscompilation() {
     std::cout << "Generating reference output from raw program...";
     Output = executeProgram("bugpoint.reference.out");
     CreatedOutput = true;
-    std::cout << " done!\n";
+    std::cout << " done! Reference output is: bugpoint.reference.out.\n";
   } else if (diffProgram(Output)) {
     std::cout << "\n*** Input program does not match reference diff!\n"
 	      << "    Must be problem with input source!\n";
