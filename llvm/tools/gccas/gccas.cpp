e22031edfe2974dbd16e17d97a3834112b67a926
Chris Lattner <sabre@nondot.org>
2003-05-02 18:19:05 +0000

    Add an instcombine pass before levelraise
    llvm-svn: 5980

--- llvm/tools/gccas/gccas.cpp
+++ llvm/tools/gccas/gccas.cpp
@@ -70,6 +70,7 @@ void AddConfiguredTransformationPasses(PassManager &PM) {
   addPass(PM, createVerifierPass());             // Verify that input is correct
   addPass(PM, createDeadInstEliminationPass());  // Remove Dead code/vars
   addPass(PM, createRaiseAllocationsPass());     // call %malloc -> malloc inst
+  addPass(PM, createInstructionCombiningPass()); // Cleanup code for raise
   addPass(PM, createIndVarSimplifyPass());       // Simplify indvars
   addPass(PM, createRaisePointerReferencesPass());// Recover type information
   addPass(PM, createInstructionCombiningPass()); // Combine silly seq's
