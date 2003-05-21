33ae3b4ab47a776bc615a4a060fa827156f7e682
Chris Lattner <sabre@nondot.org>
2003-05-21 23:01:50 +0000

    New testcase
    llvm-svn: 6278

---
+++ llvm/test/Regression/C++Frontend/2003-05-21-UnionBitfields.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <math.h>
+
+int target_isinf(double x) {
+  union {
+    double d;
+    struct {
+      unsigned mantissa2;
+      unsigned mantissa1 : 20;
+      unsigned exponent  : 11;
+      unsigned sign      :  1;
+    } big_endian;
+  } u;
+
+  u.d = x;
+  return (u.big_endian.exponent == 2047 && u.big_endian.mantissa1 == 0 && u.big_endian.mantissa2 == 0);
+}
+
+int main() {
+  printf("%d %d\n", target_isinf(1234.42), target_isinf(INFINITY));
+  return 0;
+}
