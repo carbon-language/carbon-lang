d10f1b56c6ec09585fad3f6d2a1110478ebb5349
Chris Lattner <sabre@nondot.org>
2003-05-27 21:43:14 +0000

    Update to match the reality that is now.
    llvm-svn: 6362

--- llvm/runtime/libtrace/tracelib.c
+++ llvm/runtime/libtrace/tracelib.c
@@ -10,7 +10,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <stdint.h>
 
 /*===---------------------------------------------------------------------=====
  * HASH FUNCTIONS
