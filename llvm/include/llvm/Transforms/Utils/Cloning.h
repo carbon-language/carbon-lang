2ca11ed5981a44c353d6de511eae4a841e645d9f
Chris Lattner <sabre@nondot.org>
2003-05-29 15:06:40 +0000

    Doxygenify comments
    llvm-svn: 6393

--- llvm/include/llvm/Transforms/Utils/Cloning.h
+++ llvm/include/llvm/Transforms/Utils/Cloning.h
@@ -68,28 +68,27 @@ inline Function *CloneFunction(const Function *F) {
   return CloneFunction(F, ValueMap);
 }
 
-// Clone OldFunc into NewFunc, transforming the old arguments into references to
-// ArgMap values.  Note that if NewFunc already has basic blocks, the ones
-// cloned into it will be added to the end of the function.  This function fills
-// in a list of return instructions, and can optionally append the specified
-// suffix to all values cloned.
-//
+/// Clone OldFunc into NewFunc, transforming the old arguments into references
+/// to ArgMap values.  Note that if NewFunc already has basic blocks, the ones
+/// cloned into it will be added to the end of the function.  This function
+/// fills in a list of return instructions, and can optionally append the
+/// specified suffix to all values cloned.
+///
 void CloneFunctionInto(Function *NewFunc, const Function *OldFunc,
                        std::map<const Value*, Value*> &ValueMap,
                        std::vector<ReturnInst*> &Returns,
                        const char *NameSuffix = "");
 
 
-// InlineFunction - This function forcibly inlines the called function into the
-// basic block of the caller.  This returns true if it is not possible to inline
-// this call.  The program is still in a well defined state if this occurs 
-// though.
-//
-// Note that this only does one level of inlining.  For example, if the 
-// instruction 'call B' is inlined, and 'B' calls 'C', then the call to 'C' now 
-// exists in the instruction stream.  Similiarly this will inline a recursive
-// function by one level.
-//
+/// InlineFunction - This function inlines the called function into the basic
+/// block of the caller.  This returns true if it is not possible to inline this
+/// call.  The program is still in a well defined state if this occurs though.
+///
+/// Note that this only does one level of inlining.  For example, if the 
+/// instruction 'call B' is inlined, and 'B' calls 'C', then the call to 'C' now
+/// exists in the instruction stream.  Similiarly this will inline a recursive
+/// function by one level.
+///
 bool InlineFunction(CallInst *C);
 
 #endif
