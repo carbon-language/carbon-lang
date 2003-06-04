44b2d7a56ff2a1d7db22bbcb35a4f8331b13fd48
Brian Gaeke <gaeke@uiuc.edu>
2003-06-04 22:02:47 +0000

    Add file comment. Include <vector> and <string>.  Update include guards to reflect file's current location.  Add definition of class MappingInfo.
    llvm-svn: 6616

--- llvm/lib/Target/Sparc/MappingInfo.h
+++ llvm/lib/Target/Sparc/MappingInfo.h
@@ -1,11 +1,38 @@
-#ifndef LLVM_CODEGEN_MAPPINGINFO_H
-#define LLVM_CODEGEN_MAPPINGINFO_H
+//===- llvm/Reoptimizer/Mapping/MappingInfo.h ------------------*- C++ -*--=////
+//
+// Data structures to support the Reoptimizer's Instruction-to-MachineInstr
+// mapping information gatherer.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_REOPTIMIZER_MAPPING_MAPPINGINFO_H
+#define LLVM_REOPTIMIZER_MAPPING_MAPPINGINFO_H
 
 #include <iosfwd>
+#include <vector>
+#include <string>
 class Pass;
 
-Pass *MappingInfoForFunction(std::ostream &out);
+Pass *getMappingInfoCollector(std::ostream &out);
+
+class MappingInfo {
+  class byteVector : public std::vector <unsigned char> {
+  public:
+	void dumpAssembly (std::ostream &Out);
+  };
+  std::string comment;
+  std::string symbolPrefix;
+  unsigned functionNumber;
+  byteVector bytes;
+public:
+  void outByte (unsigned char b) { bytes.push_back (b); }
+  MappingInfo (std::string _comment, std::string _symbolPrefix,
+	           unsigned _functionNumber) : comment(_comment),
+        	   symbolPrefix(_symbolPrefix), functionNumber(_functionNumber) { }
+  void dumpAssembly (std::ostream &Out);
+  unsigned char *getBytes (unsigned int &length) {
+	length = bytes.size(); return &bytes[0];
+  }
+};
 
 #endif
-
-
