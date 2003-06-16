492aea746a8a07134ace3a417ab702c6da069a76
Chris Lattner <sabre@nondot.org>
2003-06-16 14:04:18 +0000

    Minor tweak to make testcase less simple
    llvm-svn: 6710

--- llvm/test/Regression/C++Frontend/2003-06-16-InvalidInitializer.c
+++ llvm/test/Regression/C++Frontend/2003-06-16-InvalidInitializer.c
@@ -1,7 +1,8 @@
 typedef struct {
     char *auth_pwfile;
+    int x;
 } auth_config_rec;
 
-void *Ptr = &((auth_config_rec*)0)->auth_pwfile;
+void *Ptr = &((auth_config_rec*)0)->x;
 
 int main() { return 0; }
