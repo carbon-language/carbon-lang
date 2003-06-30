e17afeb1b4c3de92e0352e69fae644b03f189e4b
John Criswell <criswell@uiuc.edu>
2003-06-30 22:13:48 +0000

    Adding in sample project tree.
    llvm-svn: 7018

---
+++ llvm/projects/sample/include/sample.h
@@ -0,0 +1,8 @@
+/*
+ * File: sample.h
+ *
+ *	This is a sample header file that is global to the entire project.
+ *	It is located here so that everyone will find it.
+ */
+extern int compute_sample (int a);
+
---
+++ llvm/projects/sample/tools/sample/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+#include "sample.h"
+
+int
+main (int argc, char ** argv)
+{
+	printf ("%d\n", compute_sample (5));
+	exit (0);
+}
+
