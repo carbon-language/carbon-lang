7dee443c1cffdb701c61d44735723752e0fec2ff
Misha Brukman <brukman+llvm@gmail.com>
2003-07-02 17:53:19 +0000

    * If compiling on X86 or Sparc, automagically enable the JIT for that arch * Setting ENABLE_X86_JIT or ENABLE_SPARC_JIT on the `make' command-line will   force the inclusion of that JIT on a different architecture * If neither JIT is enabled (e.g., compiling on a different architecture), the   -march option will not be available to LLI. * As a side effect of the $ARCH variable, the Sparc LLI can now link just a bit   faster by not including the x86 library.
    llvm-svn: 7070

--- llvm/lib/ExecutionEngine/JIT/JIT.cpp
+++ llvm/lib/ExecutionEngine/JIT/JIT.cpp
@@ -14,24 +14,31 @@
 // FIXME: REMOVE THIS
 #include "llvm/PassManager.h"
 
-namespace {
-  enum ArchName { nojit, x86, Sparc };
+#if !defined(ENABLE_X86_JIT) && !defined(ENABLE_SPARC_JIT)
+#define NO_JITS_ENABLED
+#endif
 
+namespace {
+  enum ArchName { x86, Sparc };
+
+#ifndef NO_JITS_ENABLED
   cl::opt<ArchName>
   Arch("march", cl::desc("Architecture to JIT to:"), cl::Prefix,
-       cl::values(clEnumVal(x86, "  IA-32 (pentium and above)"),
-#if defined(sparc) || defined(__sparc__) || defined(__sparcv9)
+       cl::values(
+#ifdef ENABLE_X86_JIT
+                  clEnumVal(x86, "  IA-32 (Pentium and above)"),
+#endif
+#ifdef ENABLE_SPARC_JIT
                   clEnumValN(Sparc, "sparc", "  Sparc-V9"),
 #endif
                   0),
-#if defined(i386) || defined(__i386__) || defined(__x86__)
+#if defined(ENABLE_X86_JIT)
   cl::init(x86)
-#elif defined(sparc) || defined(__sparc__) || defined(__sparcv9)
+#elif defined(ENABLE_SPARC_JIT)
   cl::init(Sparc)
-#else
-  cl::init(nojit)
 #endif
        );
+#endif /* NO_JITS_ENABLED */
 }
 
 /// createJIT - Create an return a new JIT compiler if there is one available
@@ -43,20 +50,24 @@ ExecutionEngine *ExecutionEngine::createJIT(Module *M, unsigned Config) {
 
   // Allow a command-line switch to override what *should* be the default target
   // machine for this platform. This allows for debugging a Sparc JIT on X86 --
-  // our X86 machines are much faster at recompiling LLVM and linking lli.
+  // our X86 machines are much faster at recompiling LLVM and linking LLI.
+#ifdef NO_JITS_ENABLED
+  return 0;
+#endif
+
   switch (Arch) {
+#ifdef ENABLE_X86_JIT
   case x86:
     TargetMachineAllocator = allocateX86TargetMachine;
     break;
-#if defined(sparc) || defined(__sparc__) || defined(__sparcv9)
+#endif
+#ifdef ENABLE_SPARC_JIT
   case Sparc:
     TargetMachineAllocator = allocateSparcTargetMachine;
     break;
 #endif
   default:
     assert(0 && "-march flag not supported on this host!");
-  case nojit:
-    return 0;
   }
 
   // Allocate a target...
@@ -75,7 +86,7 @@ VM::VM(Module *M, TargetMachine *tm) : ExecutionEngine(M), TM(*tm) {
 
   setupPassManager();
 
-#if defined(sparc) || defined(__sparc__) || defined(__sparcv9)
+#ifdef ENABLE_SPARC_JIT
   // THIS GOES BEYOND UGLY HACKS
   if (TM.getName() == "UltraSparc-Native") {
     extern Pass *createPreSelectionPass(TargetMachine &TM);
