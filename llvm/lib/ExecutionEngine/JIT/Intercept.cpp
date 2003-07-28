ee79899ce56ee6a2035e3509ab2c8884ee7f6a1f
Misha Brukman <brukman+llvm@gmail.com>
2003-07-28 19:07:30 +0000

    Clean up code dealing with RTLD_SELF differences on Sparc and X86.
    llvm-svn: 7362

--- llvm/lib/ExecutionEngine/JIT/Intercept.cpp
+++ llvm/lib/ExecutionEngine/JIT/Intercept.cpp
@@ -54,11 +54,12 @@ void *VM::getPointerToNamedFunction(const std::string &Name) {
   if (Name == "atexit") return (void*)&jit_atexit;
 
   // If it's an external function, look it up in the process image...
-#if defined(i386) || defined(__i386__) || defined(__x86__)
-  void *Ptr = dlsym(0, Name.c_str());
-#elif defined(sparc) || defined(__sparc__) || defined(__sparcv9)
-  void *Ptr = dlsym(RTLD_SELF, Name.c_str());
+  // On Sparc, RTLD_SELF is already defined and it's not zero
+  // Linux/x86 wants to use a 0, other systems may differ
+#ifndef RTLD_SELF
+#define RTLD_SELF 0
 #endif
+  void *Ptr = dlsym(RTLD_SELF, Name.c_str());
   if (Ptr == 0) {
     std::cerr << "WARNING: Cannot resolve fn '" << Name
 	      << "' using a dummy noop function instead!\n";
@@ -67,4 +68,3 @@ void *VM::getPointerToNamedFunction(const std::string &Name) {
   
   return Ptr;
 }
-
