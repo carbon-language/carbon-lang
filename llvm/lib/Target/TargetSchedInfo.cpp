f7586ceeb2103b8735dead013f61cb3c8b71c78a
Misha Brukman <brukman+llvm@gmail.com>
2003-08-05 00:02:06 +0000

    Reformatted code to match the prevalent LLVM style; fit code into 80 columns.
    llvm-svn: 7586

--- llvm/lib/Target/TargetSchedInfo.cpp
+++ llvm/lib/Target/TargetSchedInfo.cpp
@@ -23,15 +23,14 @@ RUConflict(const std::vector<resourceId_t>& fromRVec,
   unsigned fN = fromRVec.size(), tN = toRVec.size(); 
   unsigned fi = 0, ti = 0;
 
-  while (fi < fN && ti < tN)
-    {
-      if (fromRVec[fi] < toRVec[ti])
-	++fi;
-      else if (toRVec[ti] < fromRVec[fi])
-	++ti;
-      else
-	return true;
-    }
+  while (fi < fN && ti < tN) {
+    if (fromRVec[fi] < toRVec[ti])
+      ++fi;
+    else if (toRVec[ti] < fromRVec[fi])
+      ++ti;
+    else
+      return true;
+  }
   return false;
 }
 
@@ -45,24 +44,21 @@ ComputeMinGap(const InstrRUsage &fromRU,
   if (fromRU.numBubbles > 0)
     minGap = fromRU.numBubbles;
   
-  if (minGap < fromRU.numCycles)
-    {
-      // only need to check from cycle `minGap' onwards
-      for (cycles_t gap=minGap; gap <= fromRU.numCycles-1; gap++)
-	{
-	  // check if instr. #2 can start executing `gap' cycles after #1
-	  // by checking for resource conflicts in each overlapping cycle
-	  cycles_t numOverlap =std::min(fromRU.numCycles - gap, toRU.numCycles);
-	  for (cycles_t c = 0; c <= numOverlap-1; c++)
-	    if (RUConflict(fromRU.resourcesByCycle[gap + c],
-			   toRU.resourcesByCycle[c]))
-	      {
-		// conflict found so minGap must be more than `gap'
-		minGap = gap+1;
-		break;
-	      }
-	}
+  if (minGap < fromRU.numCycles) {
+    // only need to check from cycle `minGap' onwards
+    for (cycles_t gap=minGap; gap <= fromRU.numCycles-1; gap++) {
+      // check if instr. #2 can start executing `gap' cycles after #1
+      // by checking for resource conflicts in each overlapping cycle
+      cycles_t numOverlap =std::min(fromRU.numCycles - gap, toRU.numCycles);
+      for (cycles_t c = 0; c <= numOverlap-1; c++)
+        if (RUConflict(fromRU.resourcesByCycle[gap + c],
+                       toRU.resourcesByCycle[c])) {
+          // conflict found so minGap must be more than `gap'
+          minGap = gap+1;
+          break;
+        }
     }
+  }
   
   return minGap;
 }
@@ -157,12 +153,11 @@ TargetSchedInfo::computeIssueGaps(const std::vector<InstrRUsage>&
   // 
   int classPairGaps[numSchedClasses][numSchedClasses];
   for (InstrSchedClass fromSC=0; fromSC < numSchedClasses; fromSC++)
-    for (InstrSchedClass toSC=0; toSC < numSchedClasses; toSC++)
-      {
-	int classPairGap = ComputeMinGap(instrRUForClasses[fromSC],
-					 instrRUForClasses[toSC]);
-	classPairGaps[fromSC][toSC] = classPairGap; 
-      }
+    for (InstrSchedClass toSC=0; toSC < numSchedClasses; toSC++) {
+      int classPairGap = ComputeMinGap(instrRUForClasses[fromSC],
+                                       instrRUForClasses[toSC]);
+      classPairGaps[fromSC][toSC] = classPairGap; 
+    }
 
   // Now, for each pair of instructions, use the class pair gap if both
   // instructions have identical resource usage as their respective classes.
@@ -171,20 +166,18 @@ TargetSchedInfo::computeIssueGaps(const std::vector<InstrRUsage>&
   longestIssueConflict = 0;
 
   for (MachineOpCode fromOp=0; fromOp < numOpCodes; fromOp++)
-    for (MachineOpCode toOp=0; toOp < numOpCodes; toOp++)
-      {
-        int instrPairGap = 
-          (instrRUsages[fromOp].sameAsClass && instrRUsages[toOp].sameAsClass)
-          ? classPairGaps[getSchedClass(fromOp)][getSchedClass(toOp)]
-          : ComputeMinGap(instrRUsages[fromOp], instrRUsages[toOp]);
+    for (MachineOpCode toOp=0; toOp < numOpCodes; toOp++) {
+      int instrPairGap = 
+        (instrRUsages[fromOp].sameAsClass && instrRUsages[toOp].sameAsClass)
+        ? classPairGaps[getSchedClass(fromOp)][getSchedClass(toOp)]
+        : ComputeMinGap(instrRUsages[fromOp], instrRUsages[toOp]);
 
-        if (instrPairGap > 0)
-          {
-            this->setGap(instrPairGap, fromOp, toOp);
-            conflictLists[fromOp].push_back(toOp);
-            longestIssueConflict=std::max(longestIssueConflict, instrPairGap);
-          }
+      if (instrPairGap > 0) {
+        this->setGap(instrPairGap, fromOp, toOp);
+        conflictLists[fromOp].push_back(toOp);
+        longestIssueConflict=std::max(longestIssueConflict, instrPairGap);
       }
+    }
 }
 
 
@@ -194,12 +187,11 @@ void InstrRUsage::setTo(const InstrClassRUsage& classRU) {
   breaksGroup   = classRU.breaksGroup; 
   numBubbles    = classRU.numBubbles;
   
-  for (unsigned i=0; i < classRU.numSlots; i++)
-    {
-      unsigned slot = classRU.feasibleSlots[i];
-      assert(slot < feasibleSlots.size() && "Invalid slot specified!");
-      this->feasibleSlots[slot] = true;
-    }
+  for (unsigned i=0; i < classRU.numSlots; i++) {
+    unsigned slot = classRU.feasibleSlots[i];
+    assert(slot < feasibleSlots.size() && "Invalid slot specified!");
+    this->feasibleSlots[slot] = true;
+  }
   
   numCycles   = classRU.totCycles;
   resourcesByCycle.resize(this->numCycles);
@@ -209,10 +201,10 @@ void InstrRUsage::setTo(const InstrClassRUsage& classRU) {
 	 c < NC; c++)
       this->resourcesByCycle[c].push_back(classRU.V[i].resourceId);
   
-  // Sort each resource usage vector by resourceId_t to speed up conflict checking
+  // Sort each resource usage vector by resourceId_t to speed up conflict
+  // checking
   for (unsigned i=0; i < this->resourcesByCycle.size(); i++)
     sort(resourcesByCycle[i].begin(), resourcesByCycle[i].end());
-  
 }
 
 // Add the extra resource usage requirements specified in the delta.
@@ -226,29 +218,27 @@ void InstrRUsage::addUsageDelta(const InstrRUsageDelta &delta) {
   // resize the resources vector if more cycles are specified
   unsigned maxCycles = this->numCycles;
   maxCycles = std::max(maxCycles, delta.startCycle + abs(NC) - 1);
-  if (maxCycles > this->numCycles)
-    {
-      this->resourcesByCycle.resize(maxCycles);
-      this->numCycles = maxCycles;
-    }
+  if (maxCycles > this->numCycles) {
+    this->resourcesByCycle.resize(maxCycles);
+    this->numCycles = maxCycles;
+  }
     
   if (NC >= 0)
     for (unsigned c=delta.startCycle, last=c+NC-1; c <= last; c++)
       this->resourcesByCycle[c].push_back(delta.resourceId);
   else
     // Remove the resource from all NC cycles.
-    for (unsigned c=delta.startCycle, last=(c-NC)-1; c <= last; c++)
-      {
-	// Look for the resource backwards so we remove the last entry
-	// for that resource in each cycle.
-	std::vector<resourceId_t>& rvec = this->resourcesByCycle[c];
-	int r;
-	for (r = rvec.size() - 1; r >= 0; r--)
-	  if (rvec[r] == delta.resourceId)
-	    {// found last entry for the resource
-	      rvec.erase(rvec.begin() + r);
-	      break;
-	    }
-	assert(r >= 0 && "Resource to remove was unused in cycle c!");
-      }
+    for (unsigned c=delta.startCycle, last=(c-NC)-1; c <= last; c++) {
+      // Look for the resource backwards so we remove the last entry
+      // for that resource in each cycle.
+      std::vector<resourceId_t>& rvec = this->resourcesByCycle[c];
+      int r;
+      for (r = rvec.size() - 1; r >= 0; r--)
+        if (rvec[r] == delta.resourceId) {
+          // found last entry for the resource
+          rvec.erase(rvec.begin() + r);
+          break;
+        }
+      assert(r >= 0 && "Resource to remove was unused in cycle c!");
+    }
 }
