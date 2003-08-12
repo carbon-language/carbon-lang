9f09954df9cd0ef68f311a1bc3363cd82dad524b
Chris Lattner <sabre@nondot.org>
2003-08-12 05:11:12 +0000

    Make LOC count more accurate by not including the tablegen lexer/parser, any .inc files, which are generated by tablegen, nor .lo files which sometimes get picked up from libtool
    llvm-svn: 7770

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 # This is useful because it prints out all of the source files.  Useful for
 # greps.
-find www include lib tools utils -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' | grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost | grep -v /Burg/
+find www include lib tools utils -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | \
+       grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | \
+       grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' |\
+       grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost | \
+       grep -v /Burg/ | grep -v '\.lo' | grep -v '\.inc$' | \
+       grep -v TableGen/FileParser.cpp | grep -v TableGen/FileParser.h
 
