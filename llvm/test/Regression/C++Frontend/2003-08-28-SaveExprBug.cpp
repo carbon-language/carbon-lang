55ab0f57db9453b120ad720bce828519e3804c06
Chris Lattner <sabre@nondot.org>
2003-08-29 00:03:45 +0000

    New testcase, not only possible of causing compilation failures, but could also cause miscompilations!
    llvm-svn: 8198

---
+++ llvm/test/Regression/C++Frontend/2003-08-28-SaveExprBug.cpp
@@ -0,0 +1,22 @@
+
+char* eback();
+
+template<typename foo>
+struct basic_filebuf {
+  char *instancevar;
+ 
+  void callee() {
+    instancevar += eback() != eback();
+  }
+
+  void caller();
+};
+
+
+template<typename _CharT>
+void basic_filebuf<_CharT>::caller() {
+  callee();
+}
+
+
+template class basic_filebuf<char>;
