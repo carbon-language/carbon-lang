965dd49815c61e0b1a9e71d989dfe780a03c59bd
Chris Lattner <sabre@nondot.org>
2003-08-29 00:29:55 +0000

    NEw testcase
    llvm-svn: 8199

---
+++ llvm/test/Regression/C++Frontend/2003-08-28-ForwardType.cpp
@@ -0,0 +1,20 @@
+
+// Default placement versions of operator new.
+inline void* operator new(unsigned, void* __p) throw();
+
+
+template<typename _CharT>
+struct stdio_filebuf 
+{  stdio_filebuf();
+  
+};
+
+extern stdio_filebuf<char> buf_cout;
+
+void foo() {
+  // Create stream buffers for the standard streams and use
+  // those buffers without destroying and recreating the
+  // streams.
+  new (&buf_cout) stdio_filebuf<char>();
+
+}
