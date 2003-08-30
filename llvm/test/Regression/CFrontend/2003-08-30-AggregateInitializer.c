44e57999607b2ad966efb7186fc7375eca4c267e
Chris Lattner <sabre@nondot.org>
2003-08-30 20:57:49 +0000

    New, HARD, bitfield testcase
    llvm-svn: 8227

---
+++ llvm/test/Regression/CFrontend/2003-08-30-AggregateInitializer.c
@@ -0,0 +1,15 @@
+
+struct istruct {
+  unsigned char C;
+};
+
+struct foo {
+  unsigned int I:1;
+  struct istruct J;
+  unsigned char L[1];
+  unsigned int K:1;
+};
+
+struct foo F = { 1, { 7 }, { 123 } , 1 };
+
+
