63bb3005c078033e8165d44e4ddbeb957a56b1ba
Chris Lattner <sabre@nondot.org>
2003-08-31 06:53:05 +0000

    New testcase for annoying structure layout stuff!
    llvm-svn: 8252

---
+++ llvm/test/Regression/C++Frontend/2003-08-31-StructLayout.cpp
@@ -0,0 +1,14 @@
+// There is a HOLE in the derived2 object due to not wanting to place the two 
+// baseclass instances at the same offset!
+
+struct baseclass {};
+
+class derived1 : public baseclass {
+  void * NodePtr;
+};
+
+class derived2 : public baseclass {
+  derived1 current;
+};
+
+derived2 RI;
