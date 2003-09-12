46e18d437e93158bb25e46c93f2c62c934042a2b
Misha Brukman <brukman+llvm@gmail.com>
2003-09-12 20:42:57 +0000

    Bugpoint has the ability of generating a plethora of core files, so to avoid filling up the disk, set the max core file size to 0.
    llvm-svn: 8503

--- llvm/tools/bugpoint/bugpoint.cpp
+++ llvm/tools/bugpoint/bugpoint.cpp
@@ -9,6 +9,8 @@
 #include "BugDriver.h"
 #include "llvm/Support/PassNameParser.h"
 #include "Support/CommandLine.h"
+#include "Config/unistd.h"
+#include <sys/resource.h>
 
 static cl::list<std::string>
 InputFilenames(cl::Positional, cl::OneOrMore,
@@ -26,6 +28,16 @@ int main(int argc, char **argv) {
   BugDriver D(argv[0]);
   if (D.addSources(InputFilenames)) return 1;
   D.addPasses(PassList.begin(), PassList.end());
+
+  // Bugpoint has the ability of generating a plethora of core files, so to
+  // avoid filling up the disk, set the max core file size to 0.
+  struct rlimit rlim;
+  rlim.rlim_cur = rlim.rlim_max = 0;
+  int res = setrlimit(RLIMIT_CORE, &rlim);
+  if (res < 0) {
+    // setrlimit() may have failed, but we're not going to let that stop us
+    perror("setrlimit: RLIMIT_CORE");
+  }
 
   return D.run();
 }
