cd1d0e31f5addab2461985bce17ec33d4be8a60c
Misha Brukman <brukman+llvm@gmail.com>
2003-09-22 23:44:13 +0000

    * No longer initialize the module, a subclass is more suitable to do this   because it can add a module ID which we do not have at this time. * Check to see if the module has been initialized when materializing it.
    llvm-svn: 8674

--- llvm/lib/VMCore/ModuleProvider.cpp
+++ llvm/lib/VMCore/ModuleProvider.cpp
@@ -9,20 +9,20 @@
 
 /// ctor - always have a valid Module
 ///
-AbstractModuleProvider::AbstractModuleProvider() {
-  M = new Module("");
-}
+AbstractModuleProvider::AbstractModuleProvider() : TheModule(0) { }
 
 /// dtor - when we leave, we take our Module with us
 ///
 AbstractModuleProvider::~AbstractModuleProvider() {
-  delete M;
+  delete TheModule;
 }
 
 /// materializeFunction - make sure the given function is fully read.
 ///
 void AbstractModuleProvider::materializeModule() {
-  for (Module::iterator i = M->begin(), e = M->end(); i != e; ++i) {
+  if (!TheModule) return;
+
+  for (Module::iterator i = TheModule->begin(), e = TheModule->end();
+       i != e; ++i)
     materializeFunction(i);
-  }
 }
