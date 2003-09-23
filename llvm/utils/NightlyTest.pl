8560a069f8bde9ed3e0d67ed4d3f38de4d2f1a02
Chris Lattner <sabre@nondot.org>
2003-09-23 22:02:01 +0000

    Fix bug in previous checkin
    llvm-svn: 8694

--- llvm/utils/NightlyTest.pl
+++ llvm/utils/NightlyTest.pl
@@ -198,7 +198,7 @@ my $BuildWallTime = GetRegexNum "^real", 1, "([0-9.]+)","$Prefix-Build-Log.txt";
 
 my $BuildError = "";
 if (`grep '^gmake[^:]*: .*Error' $Prefix-Build-Log.txt | wc -l` + 0 ||
-    `grep '^gmake: \*\*\*.*Stop.`+0) {
+    `grep '^gmake: \*\*\*.*Stop.' $Prefix-Build-Log.txt | wc -l`+0) {
   $BuildError = "<h3><font color='red'>Build error: compilation " .
                 "<a href=\"$DATE-Build-Log.txt\">aborted</a></font></h3>";
 }
