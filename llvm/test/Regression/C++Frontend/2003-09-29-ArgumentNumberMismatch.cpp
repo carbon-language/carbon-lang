ff06e88ce9d0b0fb0584c5c4640a4a591ea4ce6a
Chris Lattner <sabre@nondot.org>
2003-09-29 21:53:04 +0000

    Simplify test, include problem analysis
    llvm-svn: 8747

--- llvm/test/Regression/C++Frontend/2003-09-29-ArgumentNumberMismatch.cpp
+++ llvm/test/Regression/C++Frontend/2003-09-29-ArgumentNumberMismatch.cpp
@@ -1,11 +1,15 @@
+// Non-POD classes cannot be passed into a function by component, because their
+// dtors must be run.  Instead, pass them in by reference.  The C++ front-end
+// was mistakenly "thinking" that 'foo' took a structure by component.
+
 struct C {
         int A, B;
         ~C() {}
-
-	void operator^(C b) const { }
 };
 
+void foo(C b);
+
 void test(C *P) {
-        *P ^ *P;
+	foo(*P);
 }
 
