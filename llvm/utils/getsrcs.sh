909a1abadeda1403ac5bda989f4f91538eae6b16
Chris Lattner <sabre@nondot.org>
2003-10-01 21:12:46 +0000

    Make sure to include the "support" directory
    llvm-svn: 8806

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is useful because it prints out all of the source files.  Useful for
 # greps.
-find www include lib tools utils -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | \
+find support www include lib tools utils -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | \
        grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | \
        grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' |\
        grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost | \
