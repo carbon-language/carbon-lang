e2a812447639d3a5ca012834e804ce5320be3ea2
Brian Gaeke <gaeke@uiuc.edu>
2003-10-10 16:55:42 +0000

    Add my abstracted dynamic linker support files.
    llvm-svn: 9008

---
+++ llvm/include/Support/DynamicLinker.h
@@ -0,0 +1,29 @@
+//===-- DynamicLinker.h - System-indep. DynamicLinker interface -*- C++ -*-===//
+//
+// Lightweight interface to dynamic library linking and loading, and dynamic
+// symbol lookup functionality, in whatever form the operating system
+// provides it.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef SUPPORT_DYNAMICLINKER_H
+#define SUPPORT_DYNAMICLINKER_H
+
+#include <string>
+
+/// LinkDynamicObject - Load the named file as a dynamic library
+/// and link it with the currently running process. Returns false
+/// on success, true if there is an error (and sets ErrorMessage
+/// if it is not NULL). Analogous to dlopen().
+///
+bool LinkDynamicObject (const char *filename, std::string *ErrorMessage);
+
+/// GetAddressOfSymbol - Returns the address of the named symbol in
+/// the currently running process, as reported by the dynamic linker,
+/// or NULL if the symbol does not exist or some other error has
+/// occurred.
+/// 
+void *GetAddressOfSymbol (const char *symbolName);
+void *GetAddressOfSymbol (const std::string &symbolName);
+
+#endif // SUPPORT_DYNAMICLINKER_H
---
+++ llvm/lib/Support/DynamicLinker.cpp
@@ -0,0 +1,42 @@
+//===-- DynamicLinker.cpp - Implement DynamicLinker interface -------------===//
+//
+// Lightweight interface to dynamic library linking and loading, and dynamic
+// symbol lookup functionality, in whatever form the operating system
+// provides it.
+//
+// Possible future extensions include support for the HPUX shl_load()
+// interface, the Mac OS X NSLinkModule() interface, and the Windows
+// LoadLibrary() interface.
+//
+// Note that we assume that if dlopen() is available, then dlsym() is too.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Support/DynamicLinker.h"
+#include "Config/dlfcn.h"
+#include <cassert>
+
+bool LinkDynamicObject (const char *filename, std::string *ErrorMessage) {
+#if defined (HAVE_DLOPEN)
+  if (dlopen (filename, RTLD_NOW | RTLD_GLOBAL) == 0) {
+    if (ErrorMessage) *ErrorMessage = dlerror ();
+    return true;
+  }
+  return false;
+#else
+  assert (0 && "Dynamic object linking not implemented for this platform");
+#endif
+}
+
+void *GetAddressOfSymbol (const char *symbolName) {
+#if defined (HAVE_DLOPEN)
+  return dlsym (RTLD_DEFAULT, symbolName);
+#else
+  assert (0 && "Dynamic symbol lookup not implemented for this platform");
+#endif
+}
+
+// soft, cushiony C++ interface.
+void *GetAddressOfSymbol (const std::string &symbolName) {
+  return GetAddressOfSymbol (symbolName.c_str ());
+}
