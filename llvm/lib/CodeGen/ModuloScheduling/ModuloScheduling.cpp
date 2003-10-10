80f283b316c12e15ba3db8b92e9b32b56b25406e
Misha Brukman <brukman+llvm@gmail.com>
2003-10-10 17:41:32 +0000

    * Doxygenified comments * Wrap code at 80 columns * Ordered includes according to LLVM style guide
    llvm-svn: 9020

--- llvm/lib/CodeGen/ModuloScheduling/ModuloScheduling.cpp
+++ llvm/lib/CodeGen/ModuloScheduling/ModuloScheduling.cpp
@@ -1,14 +1,13 @@
 //===-- ModuloScheduling.cpp - Software Pipeling Approach - SMS --*- C++ -*--=//
 //
 // The is a software pipelining pass based on the Swing Modulo Scheduling
-// alogrithm (SMS).
+// algorithm (SMS).
 //
 //===----------------------------------------------------------------------===//
 
 #include "ModuloSchedGraph.h"
-
-#include "llvm/Pass.h"
 #include "llvm/Function.h"
+#include "llvm/Pass.h"
 
 namespace {
   
@@ -18,18 +17,19 @@ namespace {
     virtual bool runOnFunction(Function &F);
   };
 
-  RegisterOpt<ModuloScheduling> X("modulo-sched", "Modulo Scheduling/Software Pipelining");
+  RegisterOpt<ModuloScheduling> X("modulo-sched",
+                                  "Modulo Scheduling/Software Pipelining");
 }
 
-//Create Modulo Scheduling Pass
+/// Create Modulo Scheduling Pass
+/// 
 Pass *createModuloSchedPass() {
   return new ModuloScheduling(); 
 }
 
-//ModuloScheduling::runOnFunction - Main transformation entry point.
+/// ModuloScheduling::runOnFunction - main transformation entry point
+///
 bool ModuloScheduling::runOnFunction(Function &F) {
   bool Changed = false;
-
   return Changed;
 }
-
