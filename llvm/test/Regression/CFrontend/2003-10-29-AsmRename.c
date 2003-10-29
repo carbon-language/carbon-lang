cbeda5ad9bd4d720febc367c686f6f96a83a6c8e
Chris Lattner <sabre@nondot.org>
2003-10-29 19:18:48 +0000

    New testcase
    llvm-svn: 9587

---
+++ llvm/test/Regression/CFrontend/2003-10-29-AsmRename.c
@@ -0,0 +1,20 @@
+
+struct foo { int X; };
+struct bar { int Y; };
+
+extern int Func(struct foo*) __asm__("Func64");
+extern int Func64(struct bar*);
+
+int Func(struct foo *F) {
+  return 1;
+}
+
+int Func64(struct bar* B) {
+  return 0;
+}
+
+
+int test() {
+  Func(0);    /* should be renamed to call Func64 */
+  Func64(0);
+}
