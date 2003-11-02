22c19cdf99a3f66be3f1dce1cceda28c17b2f30e
Chris Lattner <sabre@nondot.org>
2003-11-02 00:22:27 +0000

    new testcase, for PR 81
    llvm-svn: 9644

---
+++ llvm/test/Regression/CFrontend/2003-11-01-C99-CompoundLiteral.c
@@ -0,0 +1,6 @@
+typedef struct { int foo; } spinlock_t;
+typedef struct wait_queue_head_t { spinlock_t lock; } wait_queue_head_t;
+void call_usermodehelper(void) { 
+  struct wait_queue_head_t work = { lock: (spinlock_t) { 0 }, }; 
+}
+
