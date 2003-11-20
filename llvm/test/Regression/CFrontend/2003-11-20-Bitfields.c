f636784489022f4d4cf80480998ad3d4e5e61fe3
Chris Lattner <sabre@nondot.org>
2003-11-20 19:05:21 +0000

    New testcase for PR141
    llvm-svn: 10112

---
+++ llvm/test/Regression/CFrontend/2003-11-20-Bitfields.c
@@ -0,0 +1,10 @@
+struct face_cachel {
+  unsigned int reverse :1;
+  unsigned char font_specified[1];
+};
+
+void
+ensure_face_cachel_contains_charset (struct face_cachel *cachel) {
+  cachel->font_specified[0] = 0;
+}
+
