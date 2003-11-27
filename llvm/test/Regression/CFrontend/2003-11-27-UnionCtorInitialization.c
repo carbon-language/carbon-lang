9201586a98687562c8ff3838d9ced243ab06267b
Chris Lattner <sabre@nondot.org>
2003-11-27 21:25:07 +0000

    New testcase for PR158
    llvm-svn: 10246

---
+++ llvm/test/Regression/CFrontend/2003-11-27-UnionCtorInitialization.c
@@ -0,0 +1,14 @@
+struct i387_soft_struct {
+ long cwd;
+ long twd;
+ long fip;
+};
+union i387_union {
+ struct i387_soft_struct soft;
+};
+struct thread_struct {
+ union i387_union i387;
+};
+void _init_task_union(void) {
+  struct thread_struct thread = (struct thread_struct) { {{0}} };
+}
