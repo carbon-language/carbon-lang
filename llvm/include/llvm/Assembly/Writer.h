4ace9279aa3353fcce474a04113b84346b9c02e8
Brian Gaeke <gaeke@uiuc.edu>
2004-01-08 22:21:59 +0000

    Fix typos. Rewrite head-of-file comment.
    llvm-svn: 10719

--- llvm/include/llvm/Assembly/Writer.h
+++ llvm/include/llvm/Assembly/Writer.h
@@ -1,4 +1,4 @@
-//===-- llvm/Assembly/Writer.h - Printer for VM assembly files --*- C++ -*-===//
+//===-- llvm/Assembly/Writer.h - Printer for LLVM assembly files --*- C++ -*-=//
 // 
 //                     The LLVM Compiler Infrastructure
 //
@@ -7,16 +7,13 @@
 // 
 //===----------------------------------------------------------------------===//
 //
-// This functionality is implemented by the lib/Assembly/Writer library.
-// This library is used to print VM assembly language files to an iostream. It
-// can print VM code at a variety of granularities, ranging from a whole class
-// down to an individual instruction.  This makes it useful for debugging.
-//
-// This file also defines functions that allow it to output files that a program
-// called VCG can read.
+// This functionality is implemented by lib/VMCore/AsmWriter.cpp.
+// This library is used to print LLVM assembly language files to an iostream. It
+// can print LLVM code at a variety of granularities, including Modules,
+// BasicBlocks, and Instructions.  This makes it useful for debugging.
 //
 // This library uses the Analysis library to figure out offsets for
-// variables in the method tables...
+// variables in the method tables.
 //
 //===----------------------------------------------------------------------===//
 
@@ -32,16 +29,16 @@ class Module;
 class Value;
 
 // WriteTypeSymbolic - This attempts to write the specified type as a symbolic
-// type, iff there is an entry in the modules symbol table for the specified
-// type or one of it's component types.  This is slower than a simple x << Type;
+// type, iff there is an entry in the Module's symbol table for the specified
+// type or one of its component types.  This is slower than a simple x << Type;
 //
 std::ostream &WriteTypeSymbolic(std::ostream &, const Type *, const Module *M);
 
 // WriteAsOperand - Write the name of the specified value out to the specified
 // ostream.  This can be useful when you just want to print int %reg126, not the
 // whole instruction that generated it.  If you specify a Module for context,
-// then even constants get pretty printed (for example the type of a null 
-// pointer is printed symbolically).
+// then even constants get pretty-printed; for example, the type of a null 
+// pointer is printed symbolically.
 //
 std::ostream &WriteAsOperand(std::ostream &, const Value *, bool PrintTy = true,
                              bool PrintName = true, const Module *Context = 0);
