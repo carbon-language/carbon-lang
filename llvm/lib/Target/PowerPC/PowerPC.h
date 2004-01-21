b14a3f73cd8a666a56b9abd68f30e7e3bd35a983
Brian Gaeke <gaeke@uiuc.edu>
2004-01-21 21:13:19 +0000

    Import of skeletal PowerPC backend I have had laying around for months...
    llvm-svn: 10937

---
+++ llvm/lib/Target/PowerPC/PowerPC.h
@@ -0,0 +1,27 @@
+//===-- PowerPC.h - Top-level interface for PowerPC representation -*- C++ -*-//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This file contains the entry points for global functions defined in the LLVM
+// PowerPC back-end.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef TARGET_POWERPC_H
+#define TARGET_POWERPC_H
+
+// Defines symbolic names for PowerPC registers.  This defines a mapping from
+// register name to register number.
+//
+#include "PowerPCGenRegisterNames.inc"
+
+// Defines symbolic names for the PowerPC instructions.
+//
+#include "PowerPCGenInstrNames.inc"
+
+#endif
---
+++ llvm/lib/Target/PowerPC/PowerPCTargetMachine.cpp
@@ -0,0 +1,53 @@
+//===-- PowerPCTargetMachine.cpp - Define TargetMachine for PowerPC -------===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+// 
+//
+//===----------------------------------------------------------------------===//
+
+#include "PowerPCTargetMachine.h"
+#include "llvm/Module.h"
+#include "llvm/PassManager.h"
+#include "llvm/Target/TargetMachineImpls.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/Passes.h"
+
+namespace llvm {
+
+// allocatePowerPCTargetMachine - Allocate and return a subclass of 
+// TargetMachine that implements the PowerPC backend.
+//
+TargetMachine *allocatePowerPCTargetMachine(const Module &M,
+                                            IntrinsicLowering *IL) {
+  return new PowerPCTargetMachine(M, IL);
+}
+
+/// PowerPCTargetMachine ctor - Create an ILP32 architecture model
+///
+PowerPCTargetMachine::PowerPCTargetMachine(const Module &M,
+                                           IntrinsicLowering *IL)
+  : TargetMachine("PowerPC", IL, true, 4, 4, 4, 4, 4),
+    FrameInfo(TargetFrameInfo::StackGrowsDown, 8, 4) {
+}
+
+// addPassesToEmitAssembly - We currently use all of the same passes as the JIT
+// does to emit statically compiled machine code.
+bool PowerPCTargetMachine::addPassesToEmitAssembly(PassManager &PM,
+					       std::ostream &Out) {
+  return true;
+}
+
+/// addPassesToJITCompile - Add passes to the specified pass manager to
+/// implement a fast dynamic compiler for this target.  Return true if this is
+/// not supported for this target.
+///
+bool PowerPCTargetMachine::addPassesToJITCompile(FunctionPassManager &PM) {
+  return true;
+}
+
+} // end namespace llvm
