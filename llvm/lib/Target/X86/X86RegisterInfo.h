01dded2e698be440be28905b0b020e296318e1ac
Chris Lattner <sabre@nondot.org>
2004-02-17 05:54:57 +0000

    Change to match the newer, simpler, interface
    llvm-svn: 11525

--- llvm/lib/Target/X86/X86RegisterInfo.h
+++ llvm/lib/Target/X86/X86RegisterInfo.h
@@ -42,10 +42,15 @@ struct X86RegisterInfo : public X86GenRegisterInfo {
 		   unsigned DestReg, unsigned SrcReg,
 		   const TargetRegisterClass *RC) const;
 
-  virtual bool canFoldMemoryOperand(MachineInstr* MI, unsigned i) const;
+  /// foldMemoryOperand - If this target supports it, fold a load or store of
+  /// the specified stack slot into the specified machine instruction for the
+  /// specified operand.  If this is possible, the target should perform the
+  /// folding and return true, otherwise it should return false.  If it folds
+  /// the instruction, it is likely that the MachineInstruction the iterator
+  /// references has been changed.
+  virtual bool foldMemoryOperand(MachineBasicBlock::iterator &MI,unsigned OpNum,
+                                 int FrameIndex) const;
 
-  virtual int foldMemoryOperand(MachineInstr* MI, unsigned i,
-                                int FrameIndex) const;
 
   void eliminateCallFramePseudoInstr(MachineFunction &MF,
                                      MachineBasicBlock &MBB,
