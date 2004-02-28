370c58a44a8450b45ebb2d16aff55b3ca8534e02
Misha Brukman <brukman+llvm@gmail.com>
2004-02-28 03:33:30 +0000

    New Function-level transformation utils.
    llvm-svn: 11938

---
+++ llvm/include/llvm/Transforms/Utils/FunctionUtils.h
@@ -0,0 +1,28 @@
+//===-- Transform/Utils/FunctionUtils.h - Function Utils --------*- C++ -*-===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This family of functions perform manipulations on functions.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TRANSFORMS_UTILS_FUNCTION_H
+#define LLVM_TRANSFORMS_UTILS_FUNCTION_H
+
+namespace llvm {
+
+class Function;
+class Loop;
+
+/// ExtractLoop - rip out a natural loop into a new function
+///
+Function* ExtractLoop(Loop *L);
+
+}
+
+#endif
