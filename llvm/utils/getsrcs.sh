bf3c8763c02bdaf5f0f7d680ad29995c4cc85bc4
Chris Lattner <sabre@nondot.org>
2004-04-18 18:26:38 +0000

    Spiff is no longer in our tree
    llvm-svn: 13041

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -5,6 +5,6 @@ find docs include lib tools utils projects -name \*.\[cdhyl\]\* | grep -v Lexer.
        grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | \
        grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' |\
        grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost | \
-       grep -v /Burg/ | grep -v /Spiff/ | grep -v '\.lo' | grep -v '\.inc$' | grep -v '\.libs' | \
+       grep -v /Burg/ | grep -v '\.lo' | grep -v '\.inc$' | grep -v '\.libs' | \
        grep -v TableGen/FileParser.cpp | grep -v TableGen/FileParser.h
 
