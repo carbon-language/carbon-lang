e7bda9c0226b5b585914517cbb4f2e8d9a58990b
Chris Lattner <sabre@nondot.org>
2004-05-27 17:58:45 +0000

    doxygen no longer builds into the docs tree, and we no longer have boost in the tree
    llvm-svn: 13838

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -4,7 +4,7 @@
 find docs include lib tools utils projects -name \*.\[cdhyl\]\* | grep -v Lexer.cpp | \
        grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | \
        grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' |\
-       grep -v '\.dir$' | grep -v www/docs/doxygen | grep -v include/boost | \
+       grep -v '\.dir$' | grep -v '\.la$' | \
        grep -v /Burg/ | grep -v '\.lo' | grep -v '\.inc$' | grep -v '\.libs' | \
        grep -v TableGen/FileParser.cpp | grep -v TableGen/FileParser.h
 
