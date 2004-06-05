307e1df0d1dfa1da979820148b7255e5b262d1d7
Chris Lattner <sabre@nondot.org>
2004-06-05 17:44:48 +0000

    Fix GCC warning
    llvm-svn: 14045

--- llvm/lib/VMCore/Verifier.cpp
+++ llvm/lib/VMCore/Verifier.cpp
@@ -319,7 +319,7 @@ void Verifier::visitBasicBlock(BasicBlock &BB) {
     std::vector<BasicBlock*> Preds(pred_begin(&BB), pred_end(&BB));
     std::sort(Preds.begin(), Preds.end());
     PHINode *PN; 
-    for (BasicBlock::iterator I = BB.begin(); PN = dyn_cast<PHINode>(I); ++I) {
+    for (BasicBlock::iterator I = BB.begin(); (PN = dyn_cast<PHINode>(I));++I) {
 
       // Ensure that PHI nodes have at least one entry!
       Assert1(PN->getNumIncomingValues() != 0,
