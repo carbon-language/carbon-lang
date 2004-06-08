79dd36055545e9dc048c13bc8a833ca763687e07
Chris Lattner <sabre@nondot.org>
2004-06-08 07:11:14 +0000

    New testcase for PR355
    llvm-svn: 14062

---
+++ llvm/test/Regression/C++Frontend/2004-06-08-LateTemplateInstantiation.cpp
@@ -0,0 +1,17 @@
+
+
+template<typename Ty>
+struct normal_iterator {  
+  int FIELD;
+};
+
+void foo(normal_iterator<int>);
+normal_iterator<int> baz();
+
+void bar() {
+  foo(baz());
+}
+
+void *bar2() {
+  return (void*)foo;
+}
