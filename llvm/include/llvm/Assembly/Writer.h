904b345c71c4dd323bc8eb180a532aab2982fd7c
Reid Spencer <rspencer@reidspencer.com>
2004-07-04 10:59:05 +0000

    Add a new, compatible, interface function for writing types as operands. This is necessary because Types are no longer Values.
    llvm-svn: 14598

--- llvm/include/llvm/Assembly/Writer.h
+++ llvm/include/llvm/Assembly/Writer.h
@@ -40,6 +40,9 @@ std::ostream &WriteTypeSymbolic(std::ostream &, const Type *, const Module *M);
 std::ostream &WriteAsOperand(std::ostream &, const Value *, bool PrintTy = true,
                              bool PrintName = true, const Module *Context = 0);
 
+std::ostream &WriteAsOperand(std::ostream&, const Type*, bool PrintTy = true,
+	                     bool PrintName = true, const Module* Context = 0);
+
 } // End llvm namespace
 
 #endif
