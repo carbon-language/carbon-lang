99dcad4584c266e12033e0720afcbe1b5cfedfdb
Chris Lattner <sabre@nondot.org>
2004-07-11 01:04:33 +0000

    Implicitly getting a new option by linking to support.o instead of support.a is a bad idea.  Make tools that want the option #include PluginSupport.h explicitly.
    llvm-svn: 14738

--- llvm/lib/Support/PluginLoader.cpp
+++ llvm/lib/Support/PluginLoader.cpp
@@ -7,34 +7,19 @@
 // 
 //===----------------------------------------------------------------------===//
 //
-// This file implements the -load <plugin> command line option processor.  When
-// linked into a program, this new command line option is available that allows
-// users to load shared objects into the running program.
-//
-// Note that there are no symbols exported by the .o file generated for this
-// .cpp file.  Because of this, a program must link against support.o instead of
-// support.a: otherwise this translation unit will not be included.
+// This file implements the -load <plugin> command line option handler.
 //
 //===----------------------------------------------------------------------===//
 
+#define DONT_GET_PLUGIN_LOADER_OPTION
+#include "Support/PluginLoader.h"
 #include "Support/DynamicLinker.h"
-#include "Support/CommandLine.h"
-#include "Config/config.h"
 #include <iostream>
 using namespace llvm;
 
-namespace {
-  struct PluginLoader {
-    void operator=(const std::string &Filename) {
-      std::string ErrorMessage;
-      if (LinkDynamicObject (Filename.c_str (), &ErrorMessage))
-        std::cerr << "Error opening '" << Filename << "': " << ErrorMessage
-                  << "\n  -load request ignored.\n";	
-    }
-  };
+void PluginLoader::operator=(const std::string &Filename) {
+  std::string ErrorMessage;
+  if (LinkDynamicObject(Filename.c_str(), &ErrorMessage))
+    std::cerr << "Error opening '" << Filename << "': " << ErrorMessage
+              << "\n  -load request ignored.\n";	
 }
-
-// This causes operator= above to be invoked for every -load option.
-static cl::opt<PluginLoader, false, cl::parser<std::string> >
-LoadOpt("load", cl::ZeroOrMore, cl::value_desc("plugin" SHLIBEXT),
-        cl::desc("Load the specified plugin"));
