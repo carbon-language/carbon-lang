3adf84ba0d89edee9ae350b6cfb1b2f8d773338a
Misha Brukman <brukman+llvm@gmail.com>
2004-07-16 20:51:55 +0000

    Implement PowerPCInstrInfo::isMoveInstr(), patch by Nate Begeman
    llvm-svn: 14898

--- llvm/lib/Target/PowerPC/PowerPCInstrInfo.cpp
+++ llvm/lib/Target/PowerPC/PowerPCInstrInfo.cpp
@@ -15,8 +15,37 @@
 #include "PowerPC.h"
 #include "llvm/CodeGen/MachineInstrBuilder.h"
 #include "PowerPCGenInstrInfo.inc"
+#include <iostream>
 using namespace llvm;
 
 PowerPCInstrInfo::PowerPCInstrInfo()
   : TargetInstrInfo(PowerPCInsts, sizeof(PowerPCInsts)/sizeof(PowerPCInsts[0])){
+}
+
+bool PowerPCInstrInfo::isMoveInstr(const MachineInstr& MI,
+                               unsigned& sourceReg,
+                               unsigned& destReg) const {
+  MachineOpCode oc = MI.getOpcode();
+  if (oc == PPC32::OR) {
+      assert(MI.getNumOperands() == 3 &&
+             MI.getOperand(0).isRegister() &&
+             MI.getOperand(1).isRegister() &&
+             MI.getOperand(2).isRegister() &&
+             "invalid register-register int move instruction");
+      if (MI.getOperand(1).getReg() == MI.getOperand(2).getReg()) {
+        sourceReg = MI.getOperand(1).getReg();
+        destReg = MI.getOperand(0).getReg();
+        return true;
+      }
+  }
+  else if (oc == PPC32::FMR) {
+      assert(MI.getNumOperands() == 2 &&
+             MI.getOperand(0).isRegister() &&
+             MI.getOperand(1).isRegister() &&
+             "invalid register-register fp move instruction");
+      sourceReg = MI.getOperand(1).getReg();
+      destReg = MI.getOperand(0).getReg();
+      return true;
+  }
+  return false;
 }
