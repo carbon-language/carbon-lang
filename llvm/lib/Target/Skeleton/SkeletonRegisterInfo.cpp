ed85542ad7322d872930a93b35aac3fda80541c1
Chris Lattner <sabre@nondot.org>
2004-07-16 07:11:15 +0000

    Initial checkin of the rest of the skeleton target
    llvm-svn: 14874

---
+++ llvm/lib/Target/Skeleton/SkeletonRegisterInfo.cpp
@@ -0,0 +1,94 @@
+//===- SkeletonRegisterInfo.cpp - Skeleton Register Information -*- C++ -*-===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This file contains the Skeleton implementation of the MRegisterInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Skeleton.h"
+#include "SkeletonRegisterInfo.h"
+#include "llvm/Type.h"
+using namespace llvm;
+
+SkeletonRegisterInfo::SkeletonRegisterInfo()
+  : SkeletonGenRegisterInfo(Skeleton::ADJCALLSTACKDOWN,
+                           Skeleton::ADJCALLSTACKUP) {}
+
+int SkeletonRegisterInfo::storeRegToStackSlot(MachineBasicBlock &MBB,
+                                              MachineBasicBlock::iterator MBBI,
+                                              unsigned SrcReg, int FrameIdx,
+                                          const TargetRegisterClass *RC) const {
+  abort();
+  return -1;
+}
+
+int SkeletonRegisterInfo::loadRegFromStackSlot(MachineBasicBlock &MBB,
+                                               MachineBasicBlock::iterator MBBI,
+                                               unsigned DestReg, int FrameIdx,
+                                          const TargetRegisterClass *RC) const {
+  abort();
+  return -1;
+}
+
+int SkeletonRegisterInfo::copyRegToReg(MachineBasicBlock &MBB,
+                                       MachineBasicBlock::iterator MBBI,
+                                       unsigned DestReg, unsigned SrcReg,
+                                       const TargetRegisterClass *RC) const {
+  abort();
+  return -1;
+}
+
+void SkeletonRegisterInfo::
+eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB,
+                              MachineBasicBlock::iterator I) const {
+  abort();
+}
+
+void SkeletonRegisterInfo::eliminateFrameIndex(MachineFunction &MF,
+                                        MachineBasicBlock::iterator II) const {
+  abort();
+}
+
+void SkeletonRegisterInfo::
+processFunctionBeforeFrameFinalized(MachineFunction &MF) const {
+  abort();
+}
+
+void SkeletonRegisterInfo::emitPrologue(MachineFunction &MF) const {
+  abort();
+}
+
+void SkeletonRegisterInfo::emitEpilogue(MachineFunction &MF,
+                                        MachineBasicBlock &MBB) const {
+  abort();
+}
+
+
+#include "SkeletonGenRegisterInfo.inc"
+
+const TargetRegisterClass*
+SkeletonRegisterInfo::getRegClassForType(const Type* Ty) const {
+  switch (Ty->getTypeID()) {
+  case Type::LongTyID:
+  case Type::ULongTyID: assert(0 && "Long values can't fit in registers!");
+  default:              assert(0 && "Invalid type to getClass!");
+  case Type::BoolTyID:
+  case Type::SByteTyID:
+  case Type::UByteTyID:
+  case Type::ShortTyID:
+  case Type::UShortTyID:
+  case Type::IntTyID:
+  case Type::UIntTyID:
+  case Type::PointerTyID: return &GPRCInstance;
+    
+  case Type::FloatTyID:
+  case Type::DoubleTyID: return &FPRCInstance;
+  }
+}
+
---
+++ llvm/lib/Target/Skeleton/SkeletonTargetMachine.cpp
@@ -0,0 +1,58 @@
+//===-- SkeletonTargetMachine.cpp - Define TargetMachine for Skeleton -----===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by the LLVM research group and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+// 
+//
+//===----------------------------------------------------------------------===//
+
+#include "SkeletonTargetMachine.h"
+#include "Skeleton.h"
+#include "llvm/Module.h"
+#include "llvm/PassManager.h"
+#include "llvm/Target/TargetOptions.h"
+#include "llvm/Target/TargetMachineRegistry.h"
+#include "llvm/CodeGen/MachineFunction.h"
+#include "llvm/CodeGen/Passes.h"
+using namespace llvm;
+
+namespace {
+  // Register the target.
+  RegisterTarget<SkeletonTargetMachine> X("skeleton",
+                                          "  Target Skeleton (unusable)");
+}
+
+/// SkeletonTargetMachine ctor - Create an ILP32 architecture model
+///
+SkeletonTargetMachine::SkeletonTargetMachine(const Module &M,
+                                           IntrinsicLowering *IL)
+  : TargetMachine("Skeleton", IL, true, 4, 4, 4, 4, 4),
+    FrameInfo(TargetFrameInfo::StackGrowsDown, 8, -4), JITInfo(*this) {
+}
+
+/// addPassesToEmitAssembly - Add passes to the specified pass manager
+/// to implement a static compiler for this target.
+///
+bool SkeletonTargetMachine::addPassesToEmitAssembly(PassManager &PM,
+					       std::ostream &Out) {
+  // <insert instruction selector passes here>
+  PM.add(createRegisterAllocator());
+  PM.add(createPrologEpilogCodeInserter());
+  // <insert assembly code output passes here>
+  PM.add(createMachineCodeDeleter());
+  return true; // change to `return false' when this actually works.
+}
+
+/// addPassesToJITCompile - Add passes to the specified pass manager to
+/// implement a fast dynamic compiler for this target.
+///
+void SkeletonJITInfo::addPassesToJITCompile(FunctionPassManager &PM) {
+  // <insert instruction selector passes here>
+  PM.add(createRegisterAllocator());
+  PM.add(createPrologEpilogCodeInserter());
+}
+
