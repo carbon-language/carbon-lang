3074d3cfe7b0a4c712dae696b09fe29ec93f18dc
Reid Spencer <rspencer@reidspencer.com>
2004-07-18 00:37:35 +0000

    bug 122: - Correct isa<Constant> for GlobalValue subclass
    llvm-svn: 14949

--- llvm/lib/Target/SparcV9/LiveVar/ValueSet.cpp
+++ llvm/lib/Target/SparcV9/LiveVar/ValueSet.cpp
@@ -17,7 +17,7 @@ std::ostream &operator<<(std::ostream &O, RAV V) { // func to print a Value
   const Value &v = V.V;
   if (v.hasName())
     return O << (void*)&v << "(" << v.getName() << ") ";
-  else if (isa<Constant>(v))
+  else if (isa<Constant>(v) && !isa<GlobalValue>(v))
     return O << (void*)&v << "(" << v << ") ";
   else
     return O << (void*)&v << " ";
