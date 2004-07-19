874ffdd5928e2743871229f430d1c4d945ccb3db
Chris Lattner <sabre@nondot.org>
2004-07-19 06:00:17 +0000

    Fix bugpoint miscompilation support on OS/X
    Patch contributed by the fabulous Nate Begeman.
    
    llvm-svn: 14994

--- llvm/lib/Support/ToolRunner.cpp
+++ llvm/lib/Support/ToolRunner.cpp
@@ -406,7 +406,9 @@ int GCC::MakeSharedObject(const std::string &InputFile, FileType fileType,
 #elif (defined(__POWERPC__) || defined(__ppc__)) && defined(__APPLE__)
     "-dynamiclib",               // `-dynamiclib' for MacOS X/PowerPC
     "-fno-common",               // allow global vars w/o initializers to live
-                                 // in data segment, rather than generating blocks
+                                 // in data segment, rather than generating
+    "dynamic_lookup",            // blocks. dynamic_lookup requires that you set
+                                 // MACOSX_DEPLOYMENT_TARGET=10.3 in your env.
 #else
     "-shared",                   // `-shared' for Linux/X86, maybe others
 #endif
