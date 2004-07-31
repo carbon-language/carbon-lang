bb635a27a41bf07f3dfeffc2fecd9fdbb72345f1
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-07-31 10:05:44 +0000

    Align breaks.
    llvm-svn: 15371

--- llvm/lib/Target/X86/X86InstrInfo.cpp
+++ llvm/lib/Target/X86/X86InstrInfo.cpp
@@ -51,16 +51,16 @@ X86InstrInfo::reverseBranchCondition(MachineBasicBlock::iterator MI) const {
   unsigned ROpcode;
   switch (Opcode) {
   case X86::JB:  ROpcode = X86::JAE; break;
-  case X86::JAE: ROpcode = X86::JB; break;
+  case X86::JAE: ROpcode = X86::JB;  break;
   case X86::JE:  ROpcode = X86::JNE; break;
-  case X86::JNE: ROpcode = X86::JE; break;
-  case X86::JBE: ROpcode = X86::JA; break;
+  case X86::JNE: ROpcode = X86::JE;  break;
+  case X86::JBE: ROpcode = X86::JA;  break;
   case X86::JA:  ROpcode = X86::JBE; break;
   case X86::JS:  ROpcode = X86::JNS; break;
-  case X86::JNS: ROpcode = X86::JS; break;
+  case X86::JNS: ROpcode = X86::JS;  break;
   case X86::JL:  ROpcode = X86::JGE; break;
-  case X86::JGE: ROpcode = X86::JL; break;
-  case X86::JLE: ROpcode = X86::JG; break;
+  case X86::JGE: ROpcode = X86::JL;  break;
+  case X86::JLE: ROpcode = X86::JG;  break;
   case X86::JG:  ROpcode = X86::JLE; break;
   default:
     assert(0 && "Cannot reverse unconditional branches!");
