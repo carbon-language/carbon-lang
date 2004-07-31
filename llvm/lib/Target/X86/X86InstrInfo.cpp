fcef7655fa2ec2a040412d64a64a1a1d1da8c74b
Chris Lattner <sabre@nondot.org>
2004-07-31 09:53:31 +0000

    Add breaks
    llvm-svn: 15365

--- llvm/lib/Target/X86/X86InstrInfo.cpp
+++ llvm/lib/Target/X86/X86InstrInfo.cpp
@@ -50,18 +50,18 @@ X86InstrInfo::reverseBranchCondition(MachineBasicBlock::iterator MI) const {
   assert(isBranch(Opcode) && "MachineInstr must be a branch");
   unsigned ROpcode;
   switch (Opcode) {
-  case X86::JB:  ROpcode = X86::JAE;
-  case X86::JAE: ROpcode = X86::JB;
-  case X86::JE:  ROpcode = X86::JNE;
-  case X86::JNE: ROpcode = X86::JE;
-  case X86::JBE: ROpcode = X86::JA;
-  case X86::JA:  ROpcode = X86::JBE;
-  case X86::JS:  ROpcode = X86::JNS;
-  case X86::JNS: ROpcode = X86::JS;
-  case X86::JL:  ROpcode = X86::JGE;
-  case X86::JGE: ROpcode = X86::JL;
-  case X86::JLE: ROpcode = X86::JG;
-  case X86::JG:  ROpcode = X86::JLE;
+  case X86::JB:  ROpcode = X86::JAE; break;
+  case X86::JAE: ROpcode = X86::JB; break;
+  case X86::JE:  ROpcode = X86::JNE; break;
+  case X86::JNE: ROpcode = X86::JE; break;
+  case X86::JBE: ROpcode = X86::JA; break;
+  case X86::JA:  ROpcode = X86::JBE; break;
+  case X86::JS:  ROpcode = X86::JNS; break;
+  case X86::JNS: ROpcode = X86::JS; break;
+  case X86::JL:  ROpcode = X86::JGE; break;
+  case X86::JGE: ROpcode = X86::JL; break;
+  case X86::JLE: ROpcode = X86::JG; break;
+  case X86::JG:  ROpcode = X86::JLE; break;
   default:
     assert(0 && "Cannot reverse unconditional branches!");
   }
