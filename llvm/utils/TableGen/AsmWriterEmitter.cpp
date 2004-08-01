f40e4ebf198e13a6405c6e26b15825d8c9ba294e
Chris Lattner <sabre@nondot.org>
2004-08-01 08:55:34 +0000

    Be picky
    llvm-svn: 15400

--- llvm/utils/TableGen/AsmWriterEmitter.cpp
+++ llvm/utils/TableGen/AsmWriterEmitter.cpp
@@ -75,8 +75,12 @@ void AsmWriterEmitter::run(std::ostream &O) {
 
           // If this is a two-address instruction and we are not accessing the
           // 0th operand, remove an operand.
-          if (I->second.isTwoAddress && OpNo != 0)
+          if (I->second.isTwoAddress && OpNo != 0) {
+            if (OpNo == 1)
+              throw "Should refer to operand #0 instead of #1 for two-address"
+                    " instruction '" + I->first + "'!";
             --OpNo;
+          }
 
           O << ";  printOperand(MI->getOperand(" << OpNo << "), MVT::"
             << getName(I->second.OperandList[OpNo].Ty) << "); O ";
