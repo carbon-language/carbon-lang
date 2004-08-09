920ae9524df8f60994ff29de13f0e7d5dcd1d3e2
Misha Brukman <brukman+llvm@gmail.com>
2004-08-09 19:10:43 +0000

    Use the current target name instead of a ClassPrefix.
    llvm-svn: 15585

--- llvm/utils/TableGen/CodeEmitterGen.cpp
+++ llvm/utils/TableGen/CodeEmitterGen.cpp
@@ -14,21 +14,21 @@
 //===----------------------------------------------------------------------===//
 
 #include "CodeEmitterGen.h"
+#include "CodeGenTarget.h"
 #include "Record.h"
 #include "Support/Debug.h"
 using namespace llvm;
 
 void CodeEmitterGen::run(std::ostream &o) {
+  CodeGenTarget Target;
   std::vector<Record*> Insts = Records.getAllDerivedDefinitions("Instruction");
 
   EmitSourceFileHeader("Machine Code Emitter", o);
 
   std::string Namespace = Insts[0]->getValueAsString("Namespace") + "::";
-  std::string ClassName = Insts[0]->getValueAsString("ClassPrefix") + 
-    "CodeEmitter::";
 
   //const std::string &Namespace = Inst->getValue("Namespace")->getName();
-  o << "unsigned " << ClassName
+  o << "unsigned " << Target.getName() << "CodeEmitter::"
     << "getBinaryCodeForInstr(MachineInstr &MI) {\n"
     << "  unsigned Value = 0;\n"
     << "  DEBUG(std::cerr << MI);\n"
