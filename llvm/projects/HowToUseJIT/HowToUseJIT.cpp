f56b69211077a1bb023a9c8d45ad63f2e6850acc
Chris Lattner <sabre@nondot.org>
2004-08-15 23:21:54 +0000

    Simplify code, make it print the constructed module before it is run.
    llvm-svn: 15792

--- llvm/projects/HowToUseJIT/HowToUseJIT.cpp
+++ llvm/projects/HowToUseJIT/HowToUseJIT.cpp
@@ -10,8 +10,6 @@
 //  This small program provides an example of how to quickly build a small
 //  module with two functions and execute it with the JIT. 
 // 
-//===------------------------------------------------------------------------===
-
 // Goal: 
 //  The goal of this snippet is to create in the memory
 //  the LLVM module consisting of two functions as follow:
@@ -34,118 +32,78 @@
 //   conflict of temporary function name with some real
 //   existing function name?
 // 
+//===----------------------------------------------------------------------===//
 
-#include <iostream>
-
-#include <llvm/Module.h>
-#include <llvm/DerivedTypes.h>
-#include <llvm/Constants.h>
-#include <llvm/Instructions.h>
-#include <llvm/ModuleProvider.h>
-
+#include "llvm/Module.h"
+#include "llvm/Constants.h"
+#include "llvm/Type.h"
+#include "llvm/Instructions.h"
+#include "llvm/ModuleProvider.h"
 #include "llvm/ExecutionEngine/ExecutionEngine.h"
 #include "llvm/ExecutionEngine/GenericValue.h"
-
-
+#include <iostream>
 using namespace llvm;
 
 int main() {
-
   // Create some module to put our function into it.
   Module *M = new Module("test");
 
+  // Create the add1 function entry and insert this entry into module M.  The
+  // function will have a return type of "int" and take an argument of "int".
+  // The '0' terminates the list of argument types.
+  Function *Add1F = M->getOrInsertFunction("add1", Type::IntTy, Type::IntTy, 0);
 
-  // We are about to create the add1 function:
-  Function *Add1F;
-
-  {
-    // first create type for the single argument of add1 function: 
-    // the type is 'int ()'
-    std::vector<const Type*> ArgT(1);
-    ArgT[0] = Type::IntTy;
-
-    // now create full type of the add1 function:
-    FunctionType *Add1T = FunctionType::get(Type::IntTy, // type of result
-                                            ArgT,
-                                            /*not vararg*/false);
- 
-    // Now create the add1 function entry and 
-    // insert this entry into module M
-    // (By passing a module as the last parameter to the Function constructor,
-    // it automatically gets appended to the Module.)
-    Add1F = new Function(Add1T, 
-                         Function::ExternalLinkage, // maybe too much
-                         "add1", M);
-
-    // Add a basic block to the function... (again, it automatically inserts
-    // because of the last argument.)
-    BasicBlock *BB = new BasicBlock("EntryBlock of add1 function", Add1F);
+  // Add a basic block to the function. As before, it automatically inserts
+  // because of the last argument.
+  BasicBlock *BB = new BasicBlock("EntryBlock", Add1F);
   
-    // Get pointers to the constant `1'...
-    Value *One = ConstantSInt::get(Type::IntTy, 1);
+  // Get pointers to the constant `1'.
+  Value *One = ConstantSInt::get(Type::IntTy, 1);
 
-    // Get pointers to the integer argument of the add1 function...
-    assert(Add1F->abegin() != Add1F->aend()); // Make sure there's an arg
-    Argument &ArgX = Add1F->afront();  // Get the arg
+  // Get pointers to the integer argument of the add1 function...
+  assert(Add1F->abegin() != Add1F->aend()); // Make sure there's an arg
+  Argument *ArgX = Add1F->abegin();  // Get the arg
+  ArgX->setName("AnArg");            // Give it a nice symbolic name for fun.
 
-    // Create the add instruction... does not insert...
-    Instruction *Add = BinaryOperator::create(Instruction::Add, One, &ArgX,
-                                              "addresult");
+  // Create the add instruction, inserting it into the end of BB.
+  Instruction *Add = BinaryOperator::createAdd(One, ArgX, "addresult", BB);
   
-    // explicitly insert it into the basic block...
-    BB->getInstList().push_back(Add);
-  
-    // Create the return instruction and add it to the basic block
-    BB->getInstList().push_back(new ReturnInst(Add));
+  // Create the return instruction and add it to the basic block
+  new ReturnInst(Add, BB);
 
-    // function add1 is ready
-  }
+  // Now, function add1 is ready.
 
 
-  // now we going to create function `foo':
-  Function *FooF;
+  // Now we going to create function `foo', which returns an int and takes no
+  // arguments.
+  Function *FooF = M->getOrInsertFunction("foo", Type::IntTy, 0);
 
-  {  
-    // Create the foo function type:
-    FunctionType *FooT = 
-      FunctionType::get(Type::IntTy, // result has type: 'int ()'
-                        std::vector<const Type*>(), // no arguments
-                        /*not vararg*/false);
+  // Add a basic block to the FooF function.
+  BB = new BasicBlock("EntryBlock", FooF);
+
+  // Get pointers to the constant `10'.
+  Value *Ten = ConstantSInt::get(Type::IntTy, 10);
+
+  // Pass Ten to the call call:
+  std::vector<Value*> Params;
+  Params.push_back(Ten);
+  CallInst * Add1CallRes = new CallInst(Add1F, Params, "add1", BB);
     
-    // create the entry for function `foo' and insert
-    // this entry into module M:
-    FooF = 
-      new Function(FooT, 
-                   Function::ExternalLinkage, // too wide?
-                   "foo", M);
-    
-    // Add a basic block to the FooF function...
-    BasicBlock *BB = new BasicBlock("EntryBlock of add1 function", FooF);
+  // Create the return instruction and add it to the basic block.
+  new ReturnInst(Add1CallRes, BB);
 
-    // Get pointers to the constant `10'...
-    Value *Ten = ConstantSInt::get(Type::IntTy, 10);
-
-    // Put the argument Ten on stack and make call:
-    // ...
-    std::vector<Value*> Params;
-    Params.push_back(Ten);
-    CallInst * Add1CallRes = new CallInst(Add1F, Params, "add1", BB);
-    
-    // Create the return instruction and add it to the basic block
-    BB->getInstList().push_back(new ReturnInst(Add1CallRes));
-    
-  }
-
-  // Now we going to create JIT ??
+  // Now we create the JIT.
   ExistingModuleProvider* MP = new ExistingModuleProvider(M);
-  ExecutionEngine* EE = ExecutionEngine::create( MP, true );
+  ExecutionEngine* EE = ExecutionEngine::create(MP, true);
+
+  std::cout << "We just constructed this LLVM module:\n\n" << *M;
+  std::cout << "\n\nRunning foo: " << std::flush;
 
   // Call the `foo' function with no arguments:
   std::vector<GenericValue> noargs;
   GenericValue gv = EE->runFunction(FooF, noargs);
 
-  // import result of execution:
-  std::cout << "Result: " << gv.IntVal << std:: endl;
-
+  // Import result of execution:
+  std::cout << "Result: " << gv.IntVal << "\n";
   return 0;
 }
