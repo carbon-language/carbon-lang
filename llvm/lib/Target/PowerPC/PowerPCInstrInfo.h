d61283f347091aa702e12fbb334035c65897129d
Misha Brukman <brukman+llvm@gmail.com>
2004-08-17 05:00:46 +0000

    The PowerPCInstrInfo class has gone away.
    llvm-svn: 15853

--- llvm/lib/Target/PowerPC/PowerPCInstrInfo.h
+++ llvm/lib/Target/PowerPC/PowerPCInstrInfo.h
@@ -15,7 +15,6 @@
 #define POWERPC_INSTRUCTIONINFO_H
 
 #include "PowerPC.h"
-#include "PowerPCRegisterInfo.h"
 #include "llvm/Target/TargetInstrInfo.h"
 
 namespace llvm {
@@ -61,39 +60,6 @@ namespace PPCII {
 		Vpr = 22
 	};
 }
-
-class PowerPCInstrInfo : public TargetInstrInfo {
-  const PowerPCRegisterInfo RI;
-  bool is64bit;
-public:
-  PowerPCInstrInfo(bool is64b);
-
-  /// getRegisterInfo - TargetInstrInfo is a superset of MRegister info.  As
-  /// such, whenever a client has an instance of instruction info, it should
-  /// always be able to get register info as well (through this method).
-  ///
-  virtual const MRegisterInfo &getRegisterInfo() const { return RI; }
-
-  //
-  // Return true if the instruction is a register to register move and
-  // leave the source and dest operands in the passed parameters.
-  //
-  virtual bool isMoveInstr(const MachineInstr& MI,
-                           unsigned& sourceReg,
-                           unsigned& destReg) const;
-
-  static unsigned invertPPCBranchOpcode(unsigned Opcode) {
-    switch (Opcode) {
-    default: assert(0 && "Unknown PPC branch opcode!");
-    case PPC::BEQ: return PPC::BNE;
-    case PPC::BNE: return PPC::BEQ;
-    case PPC::BLT: return PPC::BGE;
-    case PPC::BGE: return PPC::BLT;
-    case PPC::BGT: return PPC::BLE;
-    case PPC::BLE: return PPC::BGT;
-    } 
-  }
-};
 
 }
 
