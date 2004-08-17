32ac814f295803ec33f348a5e4a05e1358ddc144
Chris Lattner <sabre@nondot.org>
2004-08-17 19:17:10 +0000

    Add an overload
    llvm-svn: 15889

--- llvm/include/Support/MathExtras.h
+++ llvm/include/Support/MathExtras.h
@@ -29,6 +29,13 @@ inline unsigned log2(uint64_t C) {
   return getPow;
 }
 
+inline unsigned log2(unsigned C) {
+  unsigned getPow;
+  for (getPow = 0; C > 1; ++getPow)
+    C >>= 1;
+  return getPow;
+}
+
 inline bool isPowerOf2(int64_t C, unsigned &getPow) {
   if (C < 0) C = -C;
   if (C > 0 && C == (C & ~(C - 1))) {
