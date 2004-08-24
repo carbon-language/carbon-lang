ad4c8e330f90b672248713eb74aeec0376a0a2ec
Reid Spencer <rspencer@reidspencer.com>
2004-08-24 17:42:33 +0000

    Add the examples directory to these scripts.
    llvm-svn: 16030

--- llvm/utils/getsrcs.sh
+++ llvm/utils/getsrcs.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is useful because it prints out all of the source files.  Useful for
 # greps.
-find docs include lib tools utils projects -name \*.\[cdhylt\]\* | grep -v Lexer.cpp | \
+find docs include lib tools utils examples projects -name \*.\[cdhylt\]\* | grep -v Lexer.cpp | \
        grep -v llvmAsmParser.cpp | grep -v llvmAsmParser.h | grep -v '~$' | \
        grep -v '\.ll$' | grep -v .flc | grep -v Sparc.burm.c | grep -v '\.d$' |\
        grep -v '\.dir$' | grep -v '\.la$' | \
