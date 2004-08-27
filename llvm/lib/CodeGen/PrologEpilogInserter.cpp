c561fc37319ba8d8ac620ca10d4252e002c28910
Nate Begeman <natebegeman@mac.com>
2004-08-27 16:48:24 +0000

    Back out change to divide getSpillSize by 8 until I figure out why it breaks x86, which has register sizes in bits.
    llvm-svn: 16073

--- llvm/lib/CodeGen/PrologEpilogInserter.cpp
+++ llvm/lib/CodeGen/PrologEpilogInserter.cpp
@@ -181,11 +181,11 @@ void PEI::calculateCallerSavedRegisters(MachineFunction &Fn) {
     int FrameIdx;
     if (FixedSlot == FixedSpillSlots+NumFixedSpillSlots) {
       // Nope, just spill it anywhere convenient.
-      FrameIdx = FFI->CreateStackObject(RegInfo->getSpillSize(Reg)/8,
+      FrameIdx = FFI->CreateStackObject(RegInfo->getSpillSize(Reg),
                                         RegInfo->getSpillAlignment(Reg)/8);
     } else {
       // Spill it to the stack where we must.
-      FrameIdx = FFI->CreateFixedObject(RegInfo->getSpillSize(Reg)/8,
+      FrameIdx = FFI->CreateFixedObject(RegInfo->getSpillSize(Reg),
                                         FixedSlot->second);
     }
     StackSlots.push_back(FrameIdx);
