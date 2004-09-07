d7eeb3ae282250690ba28cad12052d2b855b2bb3
Reid Spencer <rspencer@reidspencer.com>
2004-09-07 16:00:48 +0000

    - Perform all validity checks before doing anything. - Use the -I parameter to aclocal to tell it where the autoconf/m4   directory is located so it can find needed autoconf functions
    llvm-svn: 16225

--- llvm/autoconf/AutoRegen.sh
+++ llvm/autoconf/AutoRegen.sh
@@ -4,18 +4,19 @@ die () {
 	exit 1
 }
 test -d autoconf && test -f autoconf/configure.ac && cd autoconf
-[ -f configure.ac ] || die "Can't find 'autoconf' dir; please cd into it first"
-echo "Regenerating aclocal.m4 with aclocal"
-aclocal || die "aclocal failed"
+test -f configure.ac || die "Can't find 'autoconf' dir; please cd into it first"
 autoconf --version | egrep '2\.5[0-9]' > /dev/null
 if test $? -ne 0
 then
 	die "Your autoconf was not detected as being 2.5x"
 fi
 echo "Note: Warnings about 'AC_CONFIG_SUBDIRS: you should use literals' are ok"
+echo "Regenerating aclocal.m4 with aclocal"
+cwd=`pwd`
+aclocal -I $cwd/m4 || die "aclocal failed"
 echo "Regenerating configure with autoconf 2.5x"
 autoconf -o ../configure configure.ac || die "autoconf failed"
 cd ..
 echo "Regenerating config.h.in with autoheader 2.5x"
-autoheader -I autoconf autoconf/configure.ac || die "autoheader failed"
+autoheader -I autoconf -I autoconf/m4 autoconf/configure.ac || die "autoheader failed"
 exit 0
