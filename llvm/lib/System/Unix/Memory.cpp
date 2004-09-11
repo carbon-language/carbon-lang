2896c95bca59cf2ca53fc532a8c6ab8a7d5e4752
Reid Spencer <rspencer@reidspencer.com>
2004-09-11 04:57:25 +0000

    Provide a generic Unix implementation of the Memory concept.
    llvm-svn: 16291

---
+++ llvm/lib/System/Unix/Memory.cpp
@@ -0,0 +1,20 @@
+//===- Unix/Memory.cpp - Generic UNIX System Configuration ------*- C++ -*-===//
+// 
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Reid Spencer and is distributed under the 
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This file defines some functions for various memory management utilities.
+//
+//===----------------------------------------------------------------------===//
+
+#include "Unix.h"
+
+namespace llvm {
+
+}
+
+// vim: sw=2 smartindent smarttab tw=80 autoindent expandtab
