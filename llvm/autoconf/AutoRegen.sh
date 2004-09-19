eb61eb2e32880d83e40ba4b95480ae9b6607631e
Reid Spencer <rspencer@reidspencer.com>
2004-09-19 22:30:53 +0000

    Make the "Warning" notice a LOT more prominent.
    llvm-svn: 16414

--- llvm/autoconf/AutoRegen.sh
+++ llvm/autoconf/AutoRegen.sh
@@ -21,7 +21,16 @@ libtool --version | grep '1.5.10' > /dev/null
 if test $? -ne 0 ; then
   die "Your libtool was not detected as being 1.5.10"
 fi
-echo "Note: Warnings about 'AC_CONFIG_SUBDIRS: you should use literals' are ok"
+echo ""
+echo "### NOTE: ############################################################"
+echo "### If you get *any* warnings from autoconf below other than warnings"
+echo "### about 'AC_CONFIG_SUBDIRS: you should use literals', you MUST fix"
+echo "### the scripts in the m4 directory because there are future forward"
+echo "### compatibility or platform support issues at risk. Please do NOT"
+echo "### commit any configure.ac or configure script that was generated "
+echo "### with warnings present."
+echo "######################################################################"
+echo ""
 echo "Regenerating aclocal.m4 with aclocal"
 cwd=`pwd`
 aclocal --force -I $cwd/m4 || die "aclocal failed"
