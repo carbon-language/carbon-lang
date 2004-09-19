2aeccd0f1c93048e4def8ced6c9e16eaff60e03e
Reid Spencer <rspencer@reidspencer.com>
2004-09-19 22:14:24 +0000

    Add in version dependency checks on all the tools we depend on. This is necessary to ensure that a consistent configuration is created on each platform. Certain definitions we use (like m4/libtool.m4) require certain versions of the tools and we can no longer be slack about this or we'll have problems with mis-configured builds.
    llvm-svn: 16412

--- llvm/autoconf/AutoRegen.sh
+++ llvm/autoconf/AutoRegen.sh
@@ -5,17 +5,28 @@ die () {
 }
 test -d autoconf && test -f autoconf/configure.ac && cd autoconf
 test -f configure.ac || die "Can't find 'autoconf' dir; please cd into it first"
-autoconf --version | egrep '2\.5[0-9]' > /dev/null
-if test $? -ne 0
-then
-	die "Your autoconf was not detected as being 2.5x"
+autoconf --version | egrep '2\.59' > /dev/null
+if test $? -ne 0 ; then
+  die "Your autoconf was not detected as being 2.59"
+fi
+aclocal --version | egrep '1\.9\.1' > /dev/null
+if test $? -ne 0 ; then
+  die "Your aclocal was not detected as being 1.9.1"
+fi
+autoheader --version | egrep '2\.59' > /dev/null
+if test $? -ne 0 ; then
+  die "Your autoheader was not detected as being 2.59"
+fi
+libtool --version | grep '1.5.10' > /dev/null
+if test $? -ne 0 ; then
+  die "Your libtool was not detected as being 1.5.10"
 fi
 echo "Note: Warnings about 'AC_CONFIG_SUBDIRS: you should use literals' are ok"
 echo "Regenerating aclocal.m4 with aclocal"
 cwd=`pwd`
-aclocal -I $cwd/m4 || die "aclocal failed"
+aclocal --force -I $cwd/m4 || die "aclocal failed"
 echo "Regenerating configure with autoconf 2.5x"
-autoconf -o ../configure configure.ac || die "autoconf failed"
+autoconf --force --warnings=all -o ../configure configure.ac || die "autoconf failed"
 cd ..
 echo "Regenerating config.h.in with autoheader 2.5x"
 autoheader -I autoconf -I autoconf/m4 autoconf/configure.ac || die "autoheader failed"
