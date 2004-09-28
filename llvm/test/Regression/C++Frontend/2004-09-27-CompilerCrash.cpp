02060ae68e14888274c46a2e3fb3e0a132c363ad
Chris Lattner <sabre@nondot.org>
2004-09-28 02:29:40 +0000

    New testcase that crashes the C++ FE, encountered while working on PR445
    llvm-svn: 16532

---
+++ llvm/test/Regression/C++Frontend/2004-09-27-CompilerCrash.cpp
@@ -0,0 +1,17 @@
+
+
+struct Pass {} ;
+template<typename PassName>
+Pass *callDefaultCtor() { return new PassName(); }
+
+void foo(Pass *(*C)());
+
+#include <bits/c++config.h>
+#include <bits/stringfwd.h>
+#include <bits/char_traits.h>
+#include <memory>       // For allocator.
+#include <bits/basic_string.h>
+
+bool foo(std::string &X) {
+  return X.empty();
+}
