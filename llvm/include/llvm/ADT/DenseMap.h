aba8cf29ce08c3b12ad316791f9353b0e6d11ea5
Alkis Evlogimenos <alkis@evlogimenos.com>
2004-09-30 21:39:47 +0000

    Remove whitespace from the end of the line.
    llvm-svn: 16624

--- llvm/include/llvm/ADT/DenseMap.h
+++ llvm/include/llvm/ADT/DenseMap.h
@@ -1,10 +1,10 @@
 //===- llvm/ADT/DenseMap.h - A dense map implmentation ----------*- C++ -*-===//
-// 
+//
 //                     The LLVM Compiler Infrastructure
 //
 // This file was developed by the LLVM research group and is distributed under
 // the University of Illinois Open Source License. See LICENSE.TXT for details.
-// 
+//
 //===----------------------------------------------------------------------===//
 //
 // This file implements a dense map. A dense map template takes two
