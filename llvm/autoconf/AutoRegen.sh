f9fdfa7aa5401d1b041b7fb743a44cb84ef6716a
Reid Spencer <rspencer@reidspencer.com>
2004-10-08 05:33:35 +0000

    Add the --with-automake option to AutoRegen.sh and provide the automake version of the configure script. This is an early commit of the automake support so that automake support can be tested on multiple platforms. Many additional Makefile.am need to be added to LLVM before this is of any use. Please wait until automake support is announced on llvmdev list before using the --with-automake option.
    llvm-svn: 16837

--- llvm/autoconf/AutoRegen.sh
+++ llvm/autoconf/AutoRegen.sh
@@ -3,13 +3,22 @@ die () {
 	echo "$@" 1>&2
 	exit 1
 }
-test -d autoconf && test -f autoconf/configure.ac && cd autoconf
-test -f configure.ac || die "Can't find 'autoconf' dir; please cd into it first"
+if test "$1" = --with-automake ; then
+  outfile=configure_am
+  configfile=configure.am
+  with_automake=1
+else
+  outfile=configure
+  configfile=configure.ac
+  with_automake=0
+fi
+test -d autoconf && test -f autoconf/$configfile && cd autoconf
+test -f $configfile || die "Can't find 'autoconf' dir; please cd into it first"
 autoconf --version | egrep '2\.59' > /dev/null
 if test $? -ne 0 ; then
   die "Your autoconf was not detected as being 2.59"
 fi
-aclocal --version | egrep '1\.9\.1' > /dev/null
+aclocal --version | egrep '1\.9\.2' > /dev/null
 if test $? -ne 0 ; then
   die "Your aclocal was not detected as being 1.9.1"
 fi
@@ -21,22 +30,40 @@ libtool --version | grep '1.5.10' > /dev/null
 if test $? -ne 0 ; then
   die "Your libtool was not detected as being 1.5.10"
 fi
+if test $with_automake -eq 1 ; then
+  automake --version | grep 'automake.*1.9.2' > /dev/null
+  if test $? -ne 0 ; then
+    die "Your automake was not detected as being 1.9.2"
+  fi
+fi
 echo ""
 echo "### NOTE: ############################################################"
 echo "### If you get *any* warnings from autoconf below you MUST fix the"
 echo "### scripts in the m4 directory because there are future forward"
 echo "### compatibility or platform support issues at risk. Please do NOT"
-echo "### commit any configure.ac or configure script that was generated "
-echo "### with warnings present. You should get just three 'Regenerating..'"
-echo "### lines."
+echo "### commit any configure script that was generated with warnings"
+echo "### present. You should get just three 'Regenerating..' lines."
 echo "######################################################################"
 echo ""
 echo "Regenerating aclocal.m4 with aclocal"
 cwd=`pwd`
+if test $with_automake -eq 1 ; then
+  cp configure.ac .configure.ac.save
+  cp configure.am configure.ac
+  cp configure.am ../configure.ac
+fi
 aclocal --force -I $cwd/m4 || die "aclocal failed"
-echo "Regenerating configure with autoconf 2.5x"
-autoconf --force --warnings=all -o ../configure configure.ac || die "autoconf failed"
+echo "Regenerating configure with autoconf 2.59"
+autoconf --force --warnings=all -o ../$outfile $configfile || die "autoconf failed"
 cd ..
-echo "Regenerating config.h.in with autoheader 2.5x"
-autoheader -I autoconf -I autoconf/m4 autoconf/configure.ac || die "autoheader failed"
+echo "Regenerating config.h.in with autoheader 2.59"
+autoheader -I autoconf -I autoconf/m4 autoconf/$configfile || die "autoheader failed"
+if test $with_automake -eq 1 ; then
+  echo "Regenerating makefiles with automake 1.9.2"
+  cp autoconf/aclocal.m4 .
+  automake --foreign --add-missing --copy
+  rm configure.ac
+  cd $cwd
+  cp .configure.ac.save configure.ac
+fi
 exit 0
