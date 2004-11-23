be39b8f541dde1d19ac88275b1ba028088a0f2f2
Chris Lattner <sabre@nondot.org>
2004-11-23 20:37:41 +0000

    Get rid of flags that are dead
    llvm-svn: 18169

--- llvm/lib/Target/PowerPC/PowerPCInstrInfo.h
+++ llvm/lib/Target/PowerPC/PowerPCInstrInfo.h
@@ -18,49 +18,38 @@
 #include "llvm/Target/TargetInstrInfo.h"
 
 namespace llvm {
-
-namespace PPCII {
-	enum {
-		ArgCountShift = 0,
-		ArgCountMask = 7,
-		
-		Arg0TypeShift = 3,
-		Arg1TypeShift = 8,
-		Arg2TypeShift = 13,
-		Arg3TypeShift = 18,
-		Arg4TypeShift = 23,
-		VMX = 1<<28,
-		PPC64 = 1<<29,
-		ArgTypeMask = 31
-	};
-	
-	enum {
-		None = 0,
-		Gpr = 1,
-		Gpr0 = 2,
-		Simm16 = 3,
-		Zimm16 = 4,
-		PCRelimm24 = 5,
-		Imm24 = 6,
-		Imm5 = 7,
-		PCRelimm14 = 8,
-		Imm14 = 9,
-		Imm2 = 10,
-		Crf = 11,
-		Imm3 = 12,
-		Imm1 = 13,
-		Fpr = 14,
-		Imm4 = 15,
-		Imm8 = 16,
-		Disimm16 = 17,
-		Disimm14 = 18,
-		Spr = 19,
-		Sgr = 20,
-		Imm15 = 21,
-		Vpr = 22
-	};
-}
-
+  namespace PPCII {
+    enum {
+      VMX   = 1 << 0,
+      PPC64 = 1 << 1,
+    };
+  
+    enum {
+      None = 0,
+      Gpr = 1,
+      Gpr0 = 2,
+      Simm16 = 3,
+      Zimm16 = 4,
+      PCRelimm24 = 5,
+      Imm24 = 6,
+      Imm5 = 7,
+      PCRelimm14 = 8,
+      Imm14 = 9,
+      Imm2 = 10,
+      Crf = 11,
+      Imm3 = 12,
+      Imm1 = 13,
+      Fpr = 14,
+      Imm4 = 15,
+      Imm8 = 16,
+      Disimm16 = 17,
+      Disimm14 = 18,
+      Spr = 19,
+      Sgr = 20,
+      Imm15 = 21,
+      Vpr = 22
+    };
+  }
 }
 
 #endif
