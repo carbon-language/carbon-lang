94336bd4c7027699440e1c41fcce2b71c220dca0
Chris Lattner <sabre@nondot.org>
2004-11-27 20:31:59 +0000

    New testcase
    llvm-svn: 18284

---
+++ llvm/test/Regression/C++Frontend/2004-11-27-EmitsUnusedInlineFunctions.cpp
@@ -0,0 +1,7 @@
+// The C++ front-end was emitting WAY too many inline functions.  This test
+// verifies that it does not emit the body of getchar, because it is not used.
+// This corresponds to PR459
+
+// RUN: %llvmgxx %s -S -o - | not grep '^int .getchar'
+
+#include <stdio.h>
