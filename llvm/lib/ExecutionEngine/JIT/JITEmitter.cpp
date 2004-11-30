56bac306839e01a945b2881a4ace6856c8bfd917
Chris Lattner <sabre@nondot.org>
2004-11-30 17:41:49 +0000

    Fix the JIT when being used from llvm-db
    llvm-svn: 18391

--- llvm/lib/ExecutionEngine/JIT/JITEmitter.cpp
+++ llvm/lib/ExecutionEngine/JIT/JITEmitter.cpp
@@ -275,7 +275,7 @@ void *JITEmitter::getPointerToGlobal(GlobalValue *V, void *Reference,
   void *ResultPtr = TheJIT->getPointerToGlobalIfAvailable(F);
   if (ResultPtr) return ResultPtr;
 
-  if (F->hasExternalLinkage()) {
+  if (F->hasExternalLinkage() && F->isExternal()) {
     // If this is an external function pointer, we can force the JIT to
     // 'compile' it, which really just adds it to the map.
     if (DoesntNeedStub)
