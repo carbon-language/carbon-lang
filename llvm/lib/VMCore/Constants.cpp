744d95e8c74257c12b34ea0528362a6442199c40
Reid Spencer <rspencer@reidspencer.com>
2004-12-07 07:06:47 +0000

    Fix some comments.
    llvm-svn: 18596

--- llvm/lib/VMCore/Constants.cpp
+++ llvm/lib/VMCore/Constants.cpp
@@ -440,8 +440,9 @@ bool ConstantFP::isValueValidForType(const Type *Ty, double Val) {
   default:
     return false;         // These can't be represented as floating point!
 
-    // TODO: Figure out how to test if a double can be cast to a float!
   case Type::FloatTyID:
+    // Since we're passed a double but the ConstantFP is of type float, make
+    // sure that the double value is in the range of a float
     return isinf(Val) || isnan(Val) || (Val >= -HUGE_VALF && Val <= HUGE_VALF);
     
   case Type::DoubleTyID:
