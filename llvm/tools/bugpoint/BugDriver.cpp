08b4bd74961c890b3b06627044f953ce6cc7ce3f
Reid Spencer <rspencer@reidspencer.com>
2004-12-20 19:16:12 +0000

    Allow compilers that can't distinguish between a class instantiation and the declaration of a function to compile this.
    llvm-svn: 19073

--- llvm/tools/bugpoint/BugDriver.cpp
+++ llvm/tools/bugpoint/BugDriver.cpp
@@ -175,7 +175,8 @@ bool BugDriver::run() {
 
   // Make sure the reference output file gets deleted on exit from this
   // function, if appropriate.
-  FileRemover RemoverInstance(sys::Path(ReferenceOutputFile), CreatedOutput);
+  sys::Path ROF(ReferenceOutputFile);
+  FileRemover RemoverInstance(ROF, CreatedOutput);
 
   // Diff the output of the raw program against the reference output.  If it
   // matches, then we have a miscompilation bug.
