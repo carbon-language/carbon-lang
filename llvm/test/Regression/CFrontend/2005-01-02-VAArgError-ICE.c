588d9471f329bf9f1924250ea9771a3a66ada05c
Chris Lattner <sabre@nondot.org>
2005-01-03 04:39:16 +0000

    new testcase for PR481
    llvm-svn: 19270

---
+++ llvm/test/Regression/CFrontend/2005-01-02-VAArgError-ICE.c
@@ -0,0 +1,10 @@
+// This file is erroneous, but should not cause the compiler to ICE.
+// PR481
+// RUN: %llvmgcc %s -S -o /dev/null 2>&1 | not grep 'internal compiler error'
+
+#include <stdarg.h>
+int flags(int a, int b, ...) {
+        va_list         args;
+        va_start(args,a);       // not the last named arg
+        foo(args);
+}
