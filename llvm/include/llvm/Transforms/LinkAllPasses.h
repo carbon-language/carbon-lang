70e3e10aa4c9908e664e48c2dc7ad1b9b1a723b4
Jeff Cohen <jeffc@jolt-lang.org>
2005-01-07 07:44:02 +0000

    Correct indentation levels
    llvm-svn: 19322

--- llvm/include/llvm/Transforms/LinkAllPasses.h
+++ llvm/include/llvm/Transforms/LinkAllPasses.h
@@ -1,6 +1,6 @@
 //===- llvm/Transforms/LinkAllPasses.h - Reference All Passes ---*- C++ -*-===//
 // 
-//                     The LLVM Compiler Infrastructure
+//           The LLVM Compiler Infrastructure
 //
 // This file was developed by Jeff Cohen and is distributed under the
 // University of Illinois Open Source License. See LICENSE.TXT for details.
@@ -23,79 +23,79 @@
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/UnifyFunctionExitNodes.h"
 
-// Trying not to include <windows.h>, though maybe we should...  Problem is,
+// Trying not to include <windows.h>, though maybe we should... Problem is,
 // it pollutes the global namespace in some really nasty ways.
 extern "C" __declspec(dllimport) void* __stdcall GetCurrentProcess();
 
 namespace {
-    struct ForceLinking {
-        ForceLinking() {
-            // We must reference the passes in such a way that VC++ will not
-            // delete it all as dead code, even with whole program optimization,
-            // yet is effectively a NO-OP.  As the compiler isn't smart enough
-            // to know that GetCurrentProcess() never returns
-            // INVALID_HANDLE_VALUE, this will do the job.
-            if (GetCurrentProcess() != (void *) -1)
-                return;
+  struct ForceLinking {
+    ForceLinking() {
+      // We must reference the passes in such a way that VC++ will not
+      // delete it all as dead code, even with whole program optimization,
+      // yet is effectively a NO-OP. As the compiler isn't smart enough
+      // to know that GetCurrentProcess() never returns
+      // INVALID_HANDLE_VALUE, this will do the job.
+      if (GetCurrentProcess() != (void *) -1)
+        return;
 
-            std::vector<llvm::BasicBlock*> bbv;
+      std::vector<llvm::BasicBlock*> bbv;
 
-            (void) llvm::createAggressiveDCEPass();
-            (void) llvm::createArgumentPromotionPass();
-            (void) llvm::createBlockProfilerPass();
-            (void) llvm::createBreakCriticalEdgesPass();
-            (void) llvm::createCFGSimplificationPass();
-            (void) llvm::createCombineBranchesPass();
-            (void) llvm::createConstantMergePass();
-            (void) llvm::createConstantPropagationPass();
-            (void) llvm::createCorrelatedExpressionEliminationPass();
-            (void) llvm::createDeadArgEliminationPass();
-            (void) llvm::createDeadCodeEliminationPass();
-            (void) llvm::createDeadInstEliminationPass();
-            (void) llvm::createDeadStoreEliminationPass();
-            (void) llvm::createDeadTypeEliminationPass();
-            (void) llvm::createEdgeProfilerPass();
-            (void) llvm::createEmitFunctionTablePass();
-            (void) llvm::createFunctionInliningPass();
-            (void) llvm::createFunctionProfilerPass();
-            (void) llvm::createFunctionResolvingPass();
-            (void) llvm::createGCSEPass();
-            (void) llvm::createGlobalDCEPass();
-            (void) llvm::createGlobalOptimizerPass();
-            (void) llvm::createIPConstantPropagationPass();
-            (void) llvm::createIPSCCPPass();
-            (void) llvm::createIndVarSimplifyPass();
-            (void) llvm::createInstructionCombiningPass();
-            (void) llvm::createInternalizePass();
-            (void) llvm::createLICMPass();
-            (void) llvm::createLoopInstrumentationPass();
-            (void) llvm::createLoopSimplifyPass();
-            (void) llvm::createLoopStrengthReducePass();
-            (void) llvm::createLoopUnrollPass();
-            (void) llvm::createLoopUnswitchPass();
-            (void) llvm::createLowerAllocationsPass();
-            (void) llvm::createLowerConstantExpressionsPass();
-            (void) llvm::createLowerGCPass();
-            (void) llvm::createLowerInvokePass();
-            (void) llvm::createLowerPackedPass();
-            (void) llvm::createLowerSetJmpPass();
-            (void) llvm::createLowerSwitchPass();
-            (void) llvm::createProfilePathsPass();
-            (void) llvm::createPromoteMemoryToRegister();
-            (void) llvm::createPruneEHPass();
-            (void) llvm::createRaiseAllocationsPass();
-            (void) llvm::createRaisePointerReferencesPass();
-            (void) llvm::createReassociatePass();
-            (void) llvm::createSCCPPass();
-            (void) llvm::createScalarReplAggregatesPass();
-            (void) llvm::createSingleLoopExtractorPass();
-            (void) llvm::createTailCallEliminationPass();
-            (void) llvm::createTailDuplicationPass();
-            (void) llvm::createTraceBasicBlockPass();
-            (void) llvm::createTraceValuesPassForBasicBlocks();
-            (void) llvm::createTraceValuesPassForFunction();
-        }
-    } X;
+      (void) llvm::createAggressiveDCEPass();
+      (void) llvm::createArgumentPromotionPass();
+      (void) llvm::createBlockProfilerPass();
+      (void) llvm::createBreakCriticalEdgesPass();
+      (void) llvm::createCFGSimplificationPass();
+      (void) llvm::createCombineBranchesPass();
+      (void) llvm::createConstantMergePass();
+      (void) llvm::createConstantPropagationPass();
+      (void) llvm::createCorrelatedExpressionEliminationPass();
+      (void) llvm::createDeadArgEliminationPass();
+      (void) llvm::createDeadCodeEliminationPass();
+      (void) llvm::createDeadInstEliminationPass();
+      (void) llvm::createDeadStoreEliminationPass();
+      (void) llvm::createDeadTypeEliminationPass();
+      (void) llvm::createEdgeProfilerPass();
+      (void) llvm::createEmitFunctionTablePass();
+      (void) llvm::createFunctionInliningPass();
+      (void) llvm::createFunctionProfilerPass();
+      (void) llvm::createFunctionResolvingPass();
+      (void) llvm::createGCSEPass();
+      (void) llvm::createGlobalDCEPass();
+      (void) llvm::createGlobalOptimizerPass();
+      (void) llvm::createIPConstantPropagationPass();
+      (void) llvm::createIPSCCPPass();
+      (void) llvm::createIndVarSimplifyPass();
+      (void) llvm::createInstructionCombiningPass();
+      (void) llvm::createInternalizePass();
+      (void) llvm::createLICMPass();
+      (void) llvm::createLoopInstrumentationPass();
+      (void) llvm::createLoopSimplifyPass();
+      (void) llvm::createLoopStrengthReducePass();
+      (void) llvm::createLoopUnrollPass();
+      (void) llvm::createLoopUnswitchPass();
+      (void) llvm::createLowerAllocationsPass();
+      (void) llvm::createLowerConstantExpressionsPass();
+      (void) llvm::createLowerGCPass();
+      (void) llvm::createLowerInvokePass();
+      (void) llvm::createLowerPackedPass();
+      (void) llvm::createLowerSetJmpPass();
+      (void) llvm::createLowerSwitchPass();
+      (void) llvm::createProfilePathsPass();
+      (void) llvm::createPromoteMemoryToRegister();
+      (void) llvm::createPruneEHPass();
+      (void) llvm::createRaiseAllocationsPass();
+      (void) llvm::createRaisePointerReferencesPass();
+      (void) llvm::createReassociatePass();
+      (void) llvm::createSCCPPass();
+      (void) llvm::createScalarReplAggregatesPass();
+      (void) llvm::createSingleLoopExtractorPass();
+      (void) llvm::createTailCallEliminationPass();
+      (void) llvm::createTailDuplicationPass();
+      (void) llvm::createTraceBasicBlockPass();
+      (void) llvm::createTraceValuesPassForBasicBlocks();
+      (void) llvm::createTraceValuesPassForFunction();
+    }
+  } X;
 };
 
 #endif // _MSC_VER
