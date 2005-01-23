7e264b2ec60edc42ab741de0186bd6468ec3e28e
Chris Lattner <sabre@nondot.org>
2005-01-23 03:30:39 +0000

    Remove two dead methods and improve the comments for DiffFilesWithTolerance. Also, make DiffFilesWithTolerance take sys::Path objects instead of std::strings.
    llvm-svn: 19770

--- llvm/include/llvm/Support/FileUtilities.h
+++ llvm/include/llvm/Support/FileUtilities.h
@@ -19,34 +19,18 @@
 
 namespace llvm {
 
-/// DiffFiles - Compare the two files specified, returning true if they are
-/// different or if there is a file error.  If you specify a string to fill in
-/// for the error option, it will set the string to an error message if an error
-/// occurs, allowing the caller to distinguish between a failed diff and a file
-/// system error.
-///
-bool DiffFiles(const std::string &FileA, const std::string &FileB,
-               std::string *Error = 0);
-
-/// DiffFilesWithTolerance - Compare the two files specified, returning 0 if the
-/// files match, 1 if they are different, and 2 if there is a file error.  This
-/// function differs from DiffFiles in that you can specify an absolete and
-/// relative FP error that is allowed to exist.  If you specify a string to fill
-/// in for the error option, it will set the string to an error message if an
-/// error occurs, allowing the caller to distinguish between a failed diff and a
-/// file system error.
-///
-int DiffFilesWithTolerance(const std::string &FileA, const std::string &FileB,
-                           double AbsTol, double RelTol,
-                           std::string *Error = 0);
+  /// DiffFilesWithTolerance - Compare the two files specified, returning 0 if
+  /// the files match, 1 if they are different, and 2 if there is a file error.
+  /// This function allows you to specify an absolete and relative FP error that
+  /// is allowed to exist.  If you specify a string to fill in for the error
+  /// option, it will set the string to an error message if an error occurs, or
+  /// if the files are different.
+  ///
+  int DiffFilesWithTolerance(const sys::Path &FileA, const sys::Path &FileB,
+                             double AbsTol, double RelTol,
+                             std::string *Error = 0);
 
 
-/// MoveFileOverIfUpdated - If the file specified by New is different than Old,
-/// or if Old does not exist, move the New file over the Old file.  Otherwise,
-/// remove the New file.
-///
-void MoveFileOverIfUpdated(const std::string &New, const std::string &Old);
- 
   /// FileRemover - This class is a simple object meant to be stack allocated.
   /// If an exception is thrown from a region, the object removes the filename
   /// specified (if deleteIt is true).
