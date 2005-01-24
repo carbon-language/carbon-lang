3c12772190aa7af702c003397197cd090555f4ac
Andrew Lenharth <andrewl@lenharth.org>
2005-01-24 18:45:41 +0000

    include prune and JIT prune
    llvm-svn: 19814

--- llvm/lib/Target/Alpha/AlphaTargetMachine.cpp
+++ llvm/lib/Target/Alpha/AlphaTargetMachine.cpp
@@ -1,4 +1,4 @@
-//===-- AlphaTargetMachine.cpp - Define TargetMachine for Alpha -------===//
+//===-- AlphaTargetMachine.cpp - Define TargetMachine for Alpha -----------===//
 // 
 //                     The LLVM Compiler Infrastructure
 //
@@ -12,14 +12,10 @@
 
 #include "Alpha.h"
 #include "AlphaTargetMachine.h"
-#include "llvm/Module.h"
-#include "llvm/CodeGen/IntrinsicLowering.h"
-#include "llvm/CodeGen/MachineFunction.h"
 #include "llvm/CodeGen/Passes.h"
 #include "llvm/Target/TargetOptions.h"
 #include "llvm/Target/TargetMachineRegistry.h"
 #include "llvm/Transforms/Scalar.h"
-#include "llvm/Support/CommandLine.h"
 #include <iostream>
 using namespace llvm;
 
@@ -31,7 +27,6 @@ namespace {
 AlphaTargetMachine::AlphaTargetMachine( const Module &M, IntrinsicLowering *IL)
   : TargetMachine("alpha", IL, true), 
     FrameInfo(TargetFrameInfo::StackGrowsDown, 8, 0) //TODO: check these
-    //JITInfo(*this)
 {}
 
 bool AlphaTargetMachine::addPassesToEmitMachineCode(FunctionPassManager &PM,
@@ -57,8 +52,6 @@ bool AlphaTargetMachine::addPassesToEmitAssembly(PassManager &PM,
   // FIXME: Implement the switch instruction in the instruction selector!
   PM.add(createLowerSwitchPass());
 
-  PM.add(createLowerConstantExpressionsPass());
-
   // Make sure that no unreachable blocks are instruction selected.
   PM.add(createUnreachableBlockEliminationPass());
 
@@ -82,30 +75,3 @@ bool AlphaTargetMachine::addPassesToEmitAssembly(PassManager &PM,
   PM.add(createMachineCodeDeleter());
   return false;
 }
-
-//void AlphaJITInfo::addPassesToJITCompile(FunctionPassManager &PM) {
-//   // FIXME: Implement efficient support for garbage collection intrinsics.
-//   PM.add(createLowerGCPass());
-
-//   // FIXME: Implement the invoke/unwind instructions!
-//   PM.add(createLowerInvokePass());
-
-//   // FIXME: Implement the switch instruction in the instruction selector!
-//   PM.add(createLowerSwitchPass());
-
-//   PM.add(createLowerConstantExpressionsPass());
-
-//   // Make sure that no unreachable blocks are instruction selected.
-//   PM.add(createUnreachableBlockEliminationPass());
-
-//   PM.add(createPPC32ISelSimple(TM));
-//   PM.add(createRegisterAllocator());
-//   PM.add(createPrologEpilogCodeInserter());
-
-//   // Must run branch selection immediately preceding the asm printer
-//   PM.add(createPPCBranchSelectionPass());
-
-//   if (PrintMachineCode)
-//     PM.add(createMachineFunctionPrinterPass(&std::cerr));
-//}
-
