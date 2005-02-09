159485ff1bd18b13c41153dcc55d709f6a381d07
Chris Lattner <sabre@nondot.org>
2005-02-09 17:45:03 +0000

    Fix test/Regression/Assembler/2005-02-09-AsmWriterStoreBug.ll
    llvm-svn: 20089

--- llvm/lib/VMCore/AsmWriter.cpp
+++ llvm/lib/VMCore/AsmWriter.cpp
@@ -1161,7 +1161,7 @@ void AssemblyWriter::printInstruction(const Instruction &I) {
 
     // Shift Left & Right print both types even for Ubyte LHS, and select prints
     // types even if all operands are bools.
-    if (isa<ShiftInst>(I) || isa<SelectInst>(I)) {
+    if (isa<ShiftInst>(I) || isa<SelectInst>(I) || isa<StoreInst>(I)) {
       PrintAllTypes = true;
     } else {
       for (unsigned i = 1, E = I.getNumOperands(); i != E; ++i) {
