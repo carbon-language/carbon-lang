1a9cbe67ed112eae9867c169473b2e992dd60dab
Chris Lattner <sabre@nondot.org>
2005-02-15 05:18:05 +0000

    New testcase for PR510.
    llvm-svn: 20189

---
+++ llvm/test/Regression/C++Frontend/2005-02-14-BitFieldOffset.cpp
@@ -0,0 +1,11 @@
+// RUN: %llvmgxx %s -S -o - | not grep 'int 6'
+
+struct QVectorTypedData {
+    int size;
+    unsigned int sharable : 1;
+    unsigned short array[1];
+};
+
+void foo(QVectorTypedData *X) {
+  X->array[0] = 123;
+}
