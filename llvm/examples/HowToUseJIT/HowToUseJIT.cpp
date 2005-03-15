222dcd44ff19540223b7cfeab2d3271bc6065de2
Alkis Evlogimenos <alkis@evlogimenos.com>
2005-03-15 07:20:55 +0000

    Stop using abegin and aend.
    llvm-svn: 20610

--- llvm/examples/HowToUseJIT/HowToUseJIT.cpp
+++ llvm/examples/HowToUseJIT/HowToUseJIT.cpp
@@ -61,8 +61,8 @@ int main() {
   Value *One = ConstantSInt::get(Type::IntTy, 1);
 
   // Get pointers to the integer argument of the add1 function...
-  assert(Add1F->abegin() != Add1F->aend()); // Make sure there's an arg
-  Argument *ArgX = Add1F->abegin();  // Get the arg
+  assert(Add1F->arg_begin() != Add1F->arg_end()); // Make sure there's an arg
+  Argument *ArgX = Add1F->arg_begin();  // Get the arg
   ArgX->setName("AnArg");            // Give it a nice symbolic name for fun.
 
   // Create the add instruction, inserting it into the end of BB.
