451fa3268599b80d0c07fb739b3b056141cd4a30
Chris Lattner <sabre@nondot.org>
2005-03-15 06:29:12 +0000

    avoid varialbe name collisions
    llvm-svn: 20606

--- llvm/lib/Analysis/DataStructure/EquivClassGraphs.cpp
+++ llvm/lib/Analysis/DataStructure/EquivClassGraphs.cpp
@@ -311,7 +311,8 @@ processSCC(DSGraph &FG, std::vector<DSGraph*> &Stack, unsigned &NextID,
   Stack.push_back(&FG);
 
   // The edges out of the current node are the call site targets...
-  for (DSGraph::fc_iterator CI = FG.fc_begin(), E = FG.fc_end(); CI != E; ++CI){
+  for (DSGraph::fc_iterator CI = FG.fc_begin(), CE = FG.fc_end();
+       CI != CE; ++CI) {
     Instruction *Call = CI->getCallSite().getInstruction();
 
     // Loop over all of the actually called functions...
@@ -375,7 +376,7 @@ void EquivClassGraphs::processGraph(DSGraph &G) {
   // Else we need to inline some callee graph.  Visit all call sites.
   // The edges out of the current node are the call site targets...
   unsigned i = 0;
-  for (DSGraph::fc_iterator CI = G.fc_begin(), E = G.fc_end(); CI != E;
+  for (DSGraph::fc_iterator CI = G.fc_begin(), CE = G.fc_end(); CI != CE;
        ++CI, ++i) {
     const DSCallSite &CS = *CI;
     Instruction *TheCall = CS.getCallSite().getInstruction();
