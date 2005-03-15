d3d086d6794e901bde43b1c1a374a4b1d571d7e7
Chris Lattner <sabre@nondot.org>
2005-03-15 22:51:40 +0000

    consolidate LinkLibraries into LinkItems
    llvm-svn: 20629

--- llvm/lib/Linker/LinkItems.cpp
+++ llvm/lib/Linker/LinkItems.cpp
@@ -47,3 +47,60 @@ Linker::LinkInItems(const ItemList& Items) {
 
   return false;
 }
+
+
+/// LinkInLibrary - links one library into the HeadModule.
+///
+bool Linker::LinkInLibrary(const std::string& Lib) {
+  // Determine where this library lives.
+  sys::Path Pathname = FindLib(Lib);
+  if (Pathname.isEmpty())
+    return warning("Cannot find library '" + Lib + "'");
+
+  // If its an archive, try to link it in
+  if (Pathname.isArchive()) {
+    if (LinkInArchive(Pathname))
+      return error("Cannot link archive '" + Pathname.toString() + "'");
+  } else if (Pathname.isBytecodeFile()) {
+    // LLVM ".so" file.
+    if (LinkInFile(Pathname))
+      return error("Cannot link file '" + Pathname.toString() + "'");
+
+  } else if (Pathname.isDynamicLibrary()) {
+    return warning("Library '" + Lib + "' is a native dynamic library.");
+  } else {
+    return warning("Supposed library '" + Lib + "' isn't a library.");
+  }
+  return false;
+}
+
+/// LinkLibraries - takes the specified library files and links them into the
+/// main bytecode object file.
+///
+/// Inputs:
+///  Libraries  - The list of libraries to link into the module.
+///
+/// Return value:
+///  FALSE - No error.
+///  TRUE  - Error.
+///
+bool Linker::LinkInLibraries(const std::vector<std::string> &Libraries) {
+
+  // Process the set of libraries we've been provided.
+  for (unsigned i = 0; i < Libraries.size(); ++i)
+    if (LinkInLibrary(Libraries[i]))
+      return true;
+
+  // At this point we have processed all the libraries provided to us. Since
+  // we have an aggregated module at this point, the dependent libraries in
+  // that module should also be aggregated with duplicates eliminated. This is
+  // now the time to process the dependent libraries to resolve any remaining
+  // symbols.
+  const Module::LibraryListType& DepLibs = Composite->getLibraries();
+  for (Module::LibraryListType::const_iterator I = DepLibs.begin(), 
+      E = DepLibs.end(); I != E; ++I)
+    if (LinkInLibrary(*I)) 
+      return true;
+
+  return false;
+}
