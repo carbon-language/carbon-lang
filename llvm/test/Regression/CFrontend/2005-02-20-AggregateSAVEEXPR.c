9f680da78472d31706922579c6db5f47d90f5f99
John Criswell <criswell@uiuc.edu>
2005-04-01 23:16:23 +0000

    For Sparc, this xfails.  I don't think sparc has the C99 complex data type support.
    llvm-svn: 20993

--- llvm/test/Regression/CFrontend/2005-02-20-AggregateSAVEEXPR.c
+++ llvm/test/Regression/CFrontend/2005-02-20-AggregateSAVEEXPR.c
@@ -1,4 +1,12 @@
 // RUN: %llvmgcc %s -o /dev/null -S
+// Note:
+//  We fail this on SparcV9 because the C library seems to be missing complex.h
+//  and the corresponding C99 complex support.
+//
+//  We could modify the test to use only GCC extensions, but I don't know if
+//  that would change the nature of the test.
+//
+// XFAIL: sparcv9
 
 #include <complex.h>
 
