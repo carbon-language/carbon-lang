2de306ba8362a48c8bfc61393401efa4a63943d2
Chris Lattner <sabre@nondot.org>
2005-04-10 03:18:18 +0000

    make this harder
    llvm-svn: 21201

--- llvm/test/Regression/CFrontend/2005-04-09-ComplexOps.c
+++ llvm/test/Regression/CFrontend/2005-04-09-ComplexOps.c
@@ -4,3 +4,6 @@
 #define I 1.0iF
 
 double __complex test(double X) { return ~-(X*I); }
+
+_Bool EQ(double __complex A, double __complex B) { return A == B; }
+_Bool NE(double __complex A, double __complex B) { return A != B; }
