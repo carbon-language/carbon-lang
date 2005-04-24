3e9634eaa0b68d66e1d17b31d9083f7c6b9ab022
Misha Brukman <brukman+llvm@gmail.com>
2005-04-24 17:09:19 +0000

    elisp code to help with LLVM code standards compliance
    llvm-svn: 21497

---
+++ llvm/utils/emacs/emacs.el
@@ -0,0 +1,12 @@
+;; LLVM coding style guidelines in emacs
+;; Maintainer: LLVM Team, http://llvm.cs.uiuc.edu
+;; Modified:   2005-04-24
+
+;; Max 80 cols per line, indent by two spaces, no tabs.
+;; Apparently, this does not affect tabs in Makefiles.
+(custom-set-variables
+  '(fill-column 80)
+  '(c++-indent-level 2)
+  '(c-basic-offset 2)
+  '(indent-tabs-mode nil))
+
