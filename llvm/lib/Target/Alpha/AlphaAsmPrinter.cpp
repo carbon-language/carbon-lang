96db51a3b6dfc28e6ed644630baf749acdb2c926
Andrew Lenharth <andrewl@lenharth.org>
2005-06-27 16:29:54 +0000

    make constant pool labels local
    llvm-svn: 22294

--- llvm/lib/Target/Alpha/AlphaAsmPrinter.cpp
+++ llvm/lib/Target/Alpha/AlphaAsmPrinter.cpp
@@ -135,7 +135,7 @@ void AlphaAsmPrinter::printOp(const MachineOperand &MO, bool IsCallOp) {
   }
 
   case MachineOperand::MO_ConstantPoolIndex:
-    O << "CPI" << CurrentFnName << "_" << MO.getConstantPoolIndex();
+    O << "$CPI" << CurrentFnName << "_" << MO.getConstantPoolIndex();
     return;
 
   case MachineOperand::MO_ExternalSymbol:
@@ -226,7 +226,7 @@ void AlphaAsmPrinter::printConstantPool(MachineConstantPool *MCP) {
   for (unsigned i = 0, e = CP.size(); i != e; ++i) {
     //    SwitchSection(O, "section .rodata, \"dr\"");
     emitAlignment(TD.getTypeAlignmentShift(CP[i]->getType()));
-    O << "CPI" << CurrentFnName << "_" << i << ":\t\t\t\t\t" << CommentString
+    O << "$CPI" << CurrentFnName << "_" << i << ":\t\t\t\t\t" << CommentString
       << *CP[i] << "\n";
     emitGlobalConstant(CP[i]);
   }
