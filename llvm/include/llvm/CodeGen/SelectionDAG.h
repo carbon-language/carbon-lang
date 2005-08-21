f4dcbc0b9808d8d21128fe70ae79c51b7b33b08d
Chris Lattner <sabre@nondot.org>
2005-08-21 22:30:42 +0000

    add a method
    llvm-svn: 22958

--- llvm/include/llvm/CodeGen/SelectionDAG.h
+++ llvm/include/llvm/CodeGen/SelectionDAG.h
@@ -234,6 +234,9 @@ public:
                     SDOperand Op5);
   void SelectNodeTo(SDNode *N, MVT::ValueType VT1, MVT::ValueType VT2,
                     unsigned TargetOpc, SDOperand Op1, SDOperand Op2);
+  void SelectNodeTo(SDNode *N, MVT::ValueType VT1, MVT::ValueType VT2,
+                    unsigned TargetOpc, SDOperand Op1, SDOperand Op2,
+                    SDOperand Op3);
   
   SDOperand getTargetNode(unsigned Opcode, MVT::ValueType VT,
                           SDOperand Op1) {
