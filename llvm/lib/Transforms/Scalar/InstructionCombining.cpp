7fde91e365fa8f172144043251f76bb89ead10c5
Chris Lattner <sabre@nondot.org>
2005-10-17 17:56:38 +0000

    Oops, X+0.0 isn't foldable, but X+-0.0 is.
    llvm-svn: 23772

--- llvm/lib/Transforms/Scalar/InstructionCombining.cpp
+++ llvm/lib/Transforms/Scalar/InstructionCombining.cpp
@@ -694,11 +694,12 @@ Instruction *InstCombiner::visitAdd(BinaryOperator &I) {
       return ReplaceInstUsesWith(I, RHS);
 
     // X + 0 --> X
-    // NOTE: -0 + +0 = +0 in non-default rounding modes.  When we support them
-    // we must disable this.  Note that 0.0-0.0 = -0.0, so this doesn't hold
-    // for SUB.
-    if (RHSC->isNullValue())
+    if (!I.getType()->isFloatingPoint()) { // NOTE: -0 + +0 = +0.
+      if (RHSC->isNullValue())
+        return ReplaceInstUsesWith(I, LHS);
+    } else if (cast<ConstantFP>(RHSC)->isExactlyValue(-0.0)) {
       return ReplaceInstUsesWith(I, LHS);
+    }
 
     // X + (signbit) --> X ^ signbit
     if (ConstantInt *CI = dyn_cast<ConstantInt>(RHSC)) {
