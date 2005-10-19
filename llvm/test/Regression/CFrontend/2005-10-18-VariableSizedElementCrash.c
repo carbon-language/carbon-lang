38d7f2be4c264864173097b242ce323904898905
Chris Lattner <sabre@nondot.org>
2005-10-19 00:52:21 +0000

    Testcase that crashes llvmgcc in type layout
    llvm-svn: 23806

---
+++ llvm/test/Regression/CFrontend/2005-10-18-VariableSizedElementCrash.c
@@ -0,0 +1,9 @@
+// RUN: %llvmgcc %s -S -o -
+// XFAIL: *
+int sub1(int i, char *pi) {
+  typedef int foo[i];
+  struct bar {foo f1; int f2:3; int f3:4} *p = (struct bar *) pi;
+  xxx(p->f1);  
+  return p->f3;
+}
+
