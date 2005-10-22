711a5fe009959f3826ddc73342eddd7107367088
Chris Lattner <sabre@nondot.org>
2005-10-22 22:00:45 +0000

    document this as experimental
    llvm-svn: 23883

--- llvm/tools/llc/llc.cpp
+++ llvm/tools/llc/llc.cpp
@@ -67,7 +67,7 @@ FileType("filetype", cl::init(TargetMachine::AssemblyFile),
        clEnumValN(TargetMachine::AssemblyFile,    "asm",
                   "  Emit an assembly ('.s') file"),
        clEnumValN(TargetMachine::ObjectFile,    "obj",
-                  "  Emit a native object ('.o') file"),
+                  "  Emit a native object ('.o') file [experimental]"),
        clEnumValN(TargetMachine::DynamicLibrary, "dynlib",
                   "  Emit a native dynamic library ('.so') file"),
        clEnumValEnd));
