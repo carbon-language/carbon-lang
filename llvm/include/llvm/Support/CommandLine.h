b92e9c57d57b44db6d9be85d79b9ede2c99b4f65
Jeff Cohen <jeffc@jolt-lang.org>
2005-10-23 15:22:50 +0000

    Work around GCC's dislike of attributes on function definitions.
    llvm-svn: 23896

--- llvm/include/llvm/Support/CommandLine.h
+++ llvm/include/llvm/Support/CommandLine.h
@@ -334,9 +334,14 @@ public:
   }
 };
 
+// Silly GCC doesn't allow attributes on a function definition.
 template<class DataType>
 ValuesClass<DataType> values(const char *Arg, DataType Val, const char *Desc,
-                             ...) END_WITH_NULL {
+                             ...) END_WITH_NULL;
+
+template<class DataType>
+ValuesClass<DataType> values(const char *Arg, DataType Val, const char *Desc,
+                             ...) {
     va_list ValueArgs;
     va_start(ValueArgs, Desc);
     ValuesClass<DataType> Vals(Arg, Val, Desc, ValueArgs);
