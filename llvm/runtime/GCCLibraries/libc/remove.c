62ef23839c0d0c56029a4b4c437fdeb63a447670
John Criswell <criswell@uiuc.edu>
2005-11-28 15:49:15 +0000

    Add the remove() function from the C library.
    llvm-svn: 24498

---
+++ llvm/runtime/GCCLibraries/libc/remove.c
@@ -0,0 +1,46 @@
+//===-- remove.c - The remove function for the LLVM libc Library --*- C -*-===//
+// 
+// A lot of this code is ripped gratuitously from glibc and libiberty.
+//
+//===----------------------------------------------------------------------===//
+
+/* ANSI C `remove' function to delete a file or directory.  POSIX.1 version.
+   Copyright (C) 1995,96,97,2002 Free Software Foundation, Inc.
+   This file is part of the GNU C Library.
+
+   The GNU C Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2.1 of the License, or (at your option) any later version.
+
+   The GNU C Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with the GNU C Library; if not, write to the Free
+   Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA
+   02111-1307 USA.  */
+
+#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int
+remove (const char * file)
+{
+  int save;
+
+  save = errno;
+  if (rmdir (file) == 0)
+    return 0;
+  else if (errno == ENOTDIR && unlink (file) == 0)
+    {
+      errno = (save);
+      return 0;
+    }
+
+  return -1;
+}
+
