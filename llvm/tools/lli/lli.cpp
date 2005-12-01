ef2d8ca0b6f1e28779d3f67e695835301740720d
Chris Lattner <sabre@nondot.org>
2005-12-01 22:48:23 +0000

    If a module has a main, but it is defined externally, refuse to run it.
    Attempting to run it will find lli's main, which isn't the desired effect.
    
    llvm-svn: 24569

--- llvm/tools/lli/lli.cpp
+++ llvm/tools/lli/lli.cpp
@@ -85,7 +85,7 @@ int main(int argc, char **argv, char * const *envp) {
     // EnvVars to determine envp.
     //
     Function *Fn = MP->getModule()->getMainFunction();
-    if (!Fn) {
+    if (!Fn || Fn->isExternal()) {
       std::cerr << "'main' function not found in module.\n";
       return -1;
     }
