7b52a923b8b42e8e6c1b1dfade2ec7d1369604aa
Jim Laskey <jlaskey@mac.com>
2005-12-21 19:48:16 +0000

    Start of Dwarf framework.
    llvm-svn: 24914

---
+++ llvm/lib/CodeGen/DwarfWriter.cpp
@@ -0,0 +1,15 @@
+//===-- llvm/CodeGen/DwarfWriter.cpp - Dwarf Framework ----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by James M. Laskey and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains support for writing dwarf debug info into asm files.
+//
+//===----------------------------------------------------------------------===//
+
+
+#include "llvm/CodeGen/DwarfWriter.h"
