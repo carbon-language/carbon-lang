60c1a120190abf4662bb37ba04a831f4ef8da518
Reid Spencer <rspencer@reidspencer.com>
2005-12-30 09:07:29 +0000

    Revert removal of std:: prefixes and addtion of "using namespace std;". This violates the LLVM coding standards.
    llvm-svn: 25050

--- llvm/tools/llvm-prof/llvm-prof.cpp
+++ llvm/tools/llvm-prof/llvm-prof.cpp
@@ -26,14 +26,13 @@
 #include <set>
 
 using namespace llvm;
-using namespace std;
 
 namespace {
-  cl::opt<string>
+  cl::opt<std::string>
   BytecodeFile(cl::Positional, cl::desc("<program bytecode file>"),
                cl::Required);
 
-  cl::opt<string>
+  cl::opt<std::string>
   ProfileDataFile(cl::Positional, cl::desc("<llvmprof.out file>"),
                   cl::Optional, cl::init("llvmprof.out"));
 
@@ -50,31 +49,31 @@ namespace {
 // PairSecondSort - A sorting predicate to sort by the second element of a pair.
 template<class T>
 struct PairSecondSortReverse
-  : public binary_function<pair<T, unsigned>,
-                                pair<T, unsigned>, bool> {
-  bool operator()(const pair<T, unsigned> &LHS,
-                  const pair<T, unsigned> &RHS) const {
+  : public std::binary_function<std::pair<T, unsigned>,
+                                std::pair<T, unsigned>, bool> {
+  bool operator()(const std::pair<T, unsigned> &LHS,
+                  const std::pair<T, unsigned> &RHS) const {
     return LHS.second > RHS.second;
   }
 };
 
 namespace {
   class ProfileAnnotator : public AssemblyAnnotationWriter {
-    map<const Function  *, unsigned> &FuncFreqs;
-    map<const BasicBlock*, unsigned> &BlockFreqs;
-    map<ProfileInfoLoader::Edge, unsigned> &EdgeFreqs;
+    std::map<const Function  *, unsigned> &FuncFreqs;
+    std::map<const BasicBlock*, unsigned> &BlockFreqs;
+    std::map<ProfileInfoLoader::Edge, unsigned> &EdgeFreqs;
   public:
-    ProfileAnnotator(map<const Function  *, unsigned> &FF,
-                     map<const BasicBlock*, unsigned> &BF,
-                     map<ProfileInfoLoader::Edge, unsigned> &EF)
+    ProfileAnnotator(std::map<const Function  *, unsigned> &FF,
+                     std::map<const BasicBlock*, unsigned> &BF,
+                     std::map<ProfileInfoLoader::Edge, unsigned> &EF)
       : FuncFreqs(FF), BlockFreqs(BF), EdgeFreqs(EF) {}
 
-    virtual void emitFunctionAnnot(const Function *F, ostream &OS) {
+    virtual void emitFunctionAnnot(const Function *F, std::ostream &OS) {
       OS << ";;; %" << F->getName() << " called " << FuncFreqs[F]
          << " times.\n;;;\n";
     }
     virtual void emitBasicBlockStartAnnot(const BasicBlock *BB,
-                                          ostream &OS) {
+                                          std::ostream &OS) {
       if (BlockFreqs.empty()) return;
       if (unsigned Count = BlockFreqs[BB])
         OS << "\t;;; Basic block executed " << Count << " times.\n";
@@ -82,18 +81,18 @@ namespace {
         OS << "\t;;; Never executed!\n";
     }
 
-    virtual void emitBasicBlockEndAnnot(const BasicBlock *BB, ostream &OS){
+    virtual void emitBasicBlockEndAnnot(const BasicBlock *BB, std::ostream &OS){
       if (EdgeFreqs.empty()) return;
 
       // Figure out how many times each successor executed.
-      vector<pair<const BasicBlock*, unsigned> > SuccCounts;
+      std::vector<std::pair<const BasicBlock*, unsigned> > SuccCounts;
       const TerminatorInst *TI = BB->getTerminator();
 
-      map<ProfileInfoLoader::Edge, unsigned>::iterator I =
-        EdgeFreqs.lower_bound(make_pair(const_cast<BasicBlock*>(BB), 0U));
+      std::map<ProfileInfoLoader::Edge, unsigned>::iterator I =
+        EdgeFreqs.lower_bound(std::make_pair(const_cast<BasicBlock*>(BB), 0U));
       for (; I != EdgeFreqs.end() && I->first.first == BB; ++I)
         if (I->second)
-          SuccCounts.push_back(make_pair(TI->getSuccessor(I->first.second),
+          SuccCounts.push_back(std::make_pair(TI->getSuccessor(I->first.second),
                                               I->second));
       if (!SuccCounts.empty()) {
         OS << "\t;;; Out-edge counts:";
@@ -113,25 +112,26 @@ int main(int argc, char **argv) {
     sys::PrintStackTraceOnErrorSignal();
 
     // Read in the bytecode file...
-    string ErrorMessage;
+    std::string ErrorMessage;
     Module *M = ParseBytecodeFile(BytecodeFile, &ErrorMessage);
     if (M == 0) {
-      cerr << argv[0] << ": " << BytecodeFile << ": " << ErrorMessage << "\n";
+      std::cerr << argv[0] << ": " << BytecodeFile << ": " 
+        << ErrorMessage << "\n";
       return 1;
     }
 
     // Read the profiling information
     ProfileInfoLoader PI(argv[0], ProfileDataFile, *M);
 
-    map<const Function  *, unsigned> FuncFreqs;
-    map<const BasicBlock*, unsigned> BlockFreqs;
-    map<ProfileInfoLoader::Edge, unsigned> EdgeFreqs;
+    std::map<const Function  *, unsigned> FuncFreqs;
+    std::map<const BasicBlock*, unsigned> BlockFreqs;
+    std::map<ProfileInfoLoader::Edge, unsigned> EdgeFreqs;
 
     // Output a report. Eventually, there will be multiple reports selectable on
     // the command line, for now, just keep things simple.
 
     // Emit the most frequent function table...
-    vector<pair<Function*, unsigned> > FunctionCounts;
+    std::vector<std::pair<Function*, unsigned> > FunctionCounts;
     PI.getFunctionCounts(FunctionCounts);
     FuncFreqs.insert(FunctionCounts.begin(), FunctionCounts.end());
 
@@ -143,41 +143,41 @@ int main(int argc, char **argv) {
     for (unsigned i = 0, e = FunctionCounts.size(); i != e; ++i)
       TotalExecutions += FunctionCounts[i].second;
 
-    cout << "===" << string(73, '-') << "===\n"
+    std::cout << "===" << std::string(73, '-') << "===\n"
               << "LLVM profiling output for execution";
-    if (PI.getNumExecutions() != 1) cout << "s";
-    cout << ":\n";
+    if (PI.getNumExecutions() != 1) std::cout << "s";
+    std::cout << ":\n";
 
     for (unsigned i = 0, e = PI.getNumExecutions(); i != e; ++i) {
-      cout << "  ";
-      if (e != 1) cout << i+1 << ". ";
-      cout << PI.getExecution(i) << "\n";
+      std::cout << "  ";
+      if (e != 1) std::cout << i+1 << ". ";
+      std::cout << PI.getExecution(i) << "\n";
     }
 
-    cout << "\n===" << string(73, '-') << "===\n";
-    cout << "Function execution frequencies:\n\n";
+    std::cout << "\n===" << std::string(73, '-') << "===\n";
+    std::cout << "Function execution frequencies:\n\n";
 
     // Print out the function frequencies...
-    cout << " ##   Frequency\n";
+    std::cout << " ##   Frequency\n";
     for (unsigned i = 0, e = FunctionCounts.size(); i != e; ++i) {
       if (FunctionCounts[i].second == 0) {
-        cout << "\n  NOTE: " << e-i << " function" <<
+        std::cout << "\n  NOTE: " << e-i << " function" <<
                (e-i-1 ? "s were" : " was") << " never executed!\n";
         break;
       }
 
-      cout << setw(3) << i+1 << ". " 
-        << setw(5) << FunctionCounts[i].second << "/"
+      std::cout << std::setw(3) << i+1 << ". " 
+        << std::setw(5) << FunctionCounts[i].second << "/"
         << TotalExecutions << " "
         << FunctionCounts[i].first->getName().c_str() << "\n";
     }
 
-    set<Function*> FunctionsToPrint;
+    std::set<Function*> FunctionsToPrint;
 
     // If we have block count information, print out the LLVM module with
     // frequency annotations.
     if (PI.hasAccurateBlockCounts()) {
-      vector<pair<BasicBlock*, unsigned> > Counts;
+      std::vector<std::pair<BasicBlock*, unsigned> > Counts;
       PI.getBlockCounts(Counts);
 
       TotalExecutions = 0;
@@ -188,20 +188,20 @@ int main(int argc, char **argv) {
       sort(Counts.begin(), Counts.end(),
                 PairSecondSortReverse<BasicBlock*>());
 
-      cout << "\n===" << string(73, '-') << "===\n";
-      cout << "Top 20 most frequently executed basic blocks:\n\n";
+      std::cout << "\n===" << std::string(73, '-') << "===\n";
+      std::cout << "Top 20 most frequently executed basic blocks:\n\n";
 
       // Print out the function frequencies...
-      cout <<" ##      %% \tFrequency\n";
+      std::cout <<" ##      %% \tFrequency\n";
       unsigned BlocksToPrint = Counts.size();
       if (BlocksToPrint > 20) BlocksToPrint = 20;
       for (unsigned i = 0; i != BlocksToPrint; ++i) {
         if (Counts[i].second == 0) break;
         Function *F = Counts[i].first->getParent();
-        cout << setw(3) << i+1 << ". " 
-          << setw(5) << setprecision(2) 
+        std::cout << std::setw(3) << i+1 << ". " 
+          << std::setw(5) << std::setprecision(2) 
           << Counts[i].second/(double)TotalExecutions*100 << "% "
-          << setw(5) << Counts[i].second << "/"
+          << std::setw(5) << Counts[i].second << "/"
           << TotalExecutions << "\t"
           << F->getName().c_str() << "() - "
           << Counts[i].first->getName().c_str() << "\n";
@@ -212,31 +212,31 @@ int main(int argc, char **argv) {
     }
 
     if (PI.hasAccurateEdgeCounts()) {
-      vector<pair<ProfileInfoLoader::Edge, unsigned> > Counts;
+      std::vector<std::pair<ProfileInfoLoader::Edge, unsigned> > Counts;
       PI.getEdgeCounts(Counts);
       EdgeFreqs.insert(Counts.begin(), Counts.end());
     }
 
     if (PrintAnnotatedLLVM || PrintAllCode) {
-      cout << "\n===" << string(73, '-') << "===\n";
-      cout << "Annotated LLVM code for the module:\n\n";
+      std::cout << "\n===" << std::string(73, '-') << "===\n";
+      std::cout << "Annotated LLVM code for the module:\n\n";
 
       ProfileAnnotator PA(FuncFreqs, BlockFreqs, EdgeFreqs);
 
       if (FunctionsToPrint.empty() || PrintAllCode)
-        M->print(cout, &PA);
+        M->print(std::cout, &PA);
       else
         // Print just a subset of the functions...
-        for (set<Function*>::iterator I = FunctionsToPrint.begin(),
+        for (std::set<Function*>::iterator I = FunctionsToPrint.begin(),
                E = FunctionsToPrint.end(); I != E; ++I)
-          (*I)->print(cout, &PA);
+          (*I)->print(std::cout, &PA);
     }
 
     return 0;
-  } catch (const string& msg) {
-    cerr << argv[0] << ": " << msg << "\n";
+  } catch (const std::string& msg) {
+    std::cerr << argv[0] << ": " << msg << "\n";
   } catch (...) {
-    cerr << argv[0] << ": Unexpected unknown exception occurred.\n";
+    std::cerr << argv[0] << ": Unexpected unknown exception occurred.\n";
   }
   return 1;
 }
