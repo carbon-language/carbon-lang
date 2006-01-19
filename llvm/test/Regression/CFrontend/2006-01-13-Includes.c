800ad5287cccb171a0d662124a565862a2ad2ee2
Chris Lattner <sabre@nondot.org>
2006-01-19 06:49:46 +0000

    make this work for Reid, who renamed his CVS module dir :)
    llvm-svn: 25443

--- llvm/test/Regression/CFrontend/2006-01-13-Includes.c
+++ llvm/test/Regression/CFrontend/2006-01-13-Includes.c
@@ -1,5 +1,4 @@
-// RUN: %llvmgcc %s -g -S -o - | gccas | llvm-dis | grep "llvm/test/Regression/CFrontend"
-
+// RUN: %llvmgcc %s -g -S -o - | gccas | llvm-dis | grep "test/Regression/CFrontend"
 // PR676
 
 #include <stdio.h>
