8ca86aa91868905e59dc39bc90ac2c45f02cecda
Jeff Cohen <jeffc@jolt-lang.org>
2006-01-20 04:34:45 +0000

    Update Windows version of config.h to note availability of floor/floorf/ceil/ceilf. Also pulled Windows-specific modifications to config.h.in to a separate file, so that changes to it trigger regeneration of config.h (which doesn't happen when the custom build step for it changes).
    llvm-svn: 25477

---
+++ llvm/win32/config.h
@@ -0,0 +1,24 @@
+/* This file is appended to config.h.in to form the Windows version of
+ * config.h */
+
+#define PACKAGE_NAME "LLVM (win32 vc7.1)" 
+#define PACKAGE_VERSION 1.4 
+#define HAVE_WINDOWS_H 1 
+#define HAVE_LIMITS_H 1 
+#define HAVE_SYS_STAT_H 1 
+#define HAVE_STDLIB_H 1 
+#define HAVE_STDIO_H 1 
+#define HAVE_STRING_H 1 
+#define SHLIBEXT ".lib" 
+#define error_t int 
+#define HAVE_ERRNO_H 1 
+#define LTDL_DLOPEN_DEPLIBS 1 
+#define HAVE_CEIL 1 
+#define HAVE_CEILF 1 
+#define HAVE_FLOOR 1 
+#define HAVE_FLOORF 1 
+#define LTDL_OBJDIR "_libs" 
+#define LTDL_SHLIBPATH_VAR "PATH" 
+#define LTDL_SHLIB_EXT ".dll" 
+#define LTDL_SYSSEARCHPATH "" 
+#define LLVM_ON_WIN32 1 
