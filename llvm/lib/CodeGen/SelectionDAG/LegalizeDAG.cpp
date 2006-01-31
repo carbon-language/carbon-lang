e9721b29849d16ce20c8839376413fcabed46c38
Chris Lattner <sabre@nondot.org>
2006-01-31 05:04:52 +0000

    Only insert an AND when converting from BR_COND to BRCC if needed.
    llvm-svn: 25832

--- llvm/lib/CodeGen/SelectionDAG/LegalizeDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeDAG.cpp
@@ -703,7 +703,9 @@ SDOperand SelectionDAGLegalize::LegalizeOp(SDOperand Op) {
       } else {
         // Make sure the condition is either zero or one.  It may have been
         // promoted from something else.
-        Tmp2 = DAG.getZeroExtendInReg(Tmp2, MVT::i1);
+        unsigned NumBits = MVT::getSizeInBits(Tmp2.getValueType());
+        if (!TLI.MaskedValueIsZero(Tmp2, (~0ULL >> (64-NumBits))^1))
+          Tmp2 = DAG.getZeroExtendInReg(Tmp2, MVT::i1);
         
         Result = DAG.getNode(ISD::BR_CC, MVT::Other, Tmp1, 
                              DAG.getCondCode(ISD::SETNE), Tmp2,
