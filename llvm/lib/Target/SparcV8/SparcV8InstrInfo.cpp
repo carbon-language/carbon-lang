d7d98611ca05136c9096a9c58e95115b8f190c91
Chris Lattner <sabre@nondot.org>
2006-02-03 06:44:54 +0000

    Implement isLoadFromStackSlot and isStoreToStackSlot
    llvm-svn: 25932

--- llvm/lib/Target/SparcV8/SparcV8InstrInfo.cpp
+++ llvm/lib/Target/SparcV8/SparcV8InstrInfo.cpp
@@ -57,3 +57,41 @@ bool SparcV8InstrInfo::isMoveInstr(const MachineInstr &MI,
   }
   return false;
 }
+
+/// isLoadFromStackSlot - If the specified machine instruction is a direct
+/// load from a stack slot, return the virtual or physical register number of
+/// the destination along with the FrameIndex of the loaded stack slot.  If
+/// not, return 0.  This predicate must return 0 if the instruction has
+/// any side effects other than loading from the stack slot.
+unsigned SparcV8InstrInfo::isLoadFromStackSlot(MachineInstr *MI,
+                                               int &FrameIndex) const {
+  if (MI->getOpcode() == V8::LDri ||
+      MI->getOpcode() == V8::LDFri ||
+      MI->getOpcode() == V8::LDDFri) {
+    if (MI->getOperand(1).isFrameIndex() && MI->getOperand(2).isImmediate() &&
+        MI->getOperand(2).getImmedValue() == 0) {
+      FrameIndex = MI->getOperand(1).getFrameIndex();
+      return MI->getOperand(0).getReg();
+    }
+  }
+  return 0;
+}
+
+/// isStoreToStackSlot - If the specified machine instruction is a direct
+/// store to a stack slot, return the virtual or physical register number of
+/// the source reg along with the FrameIndex of the loaded stack slot.  If
+/// not, return 0.  This predicate must return 0 if the instruction has
+/// any side effects other than storing to the stack slot.
+unsigned SparcV8InstrInfo::isStoreToStackSlot(MachineInstr *MI,
+                                              int &FrameIndex) const {
+  if (MI->getOpcode() == V8::STri ||
+      MI->getOpcode() == V8::STFri ||
+      MI->getOpcode() == V8::STDFri) {
+    if (MI->getOperand(0).isFrameIndex() && MI->getOperand(1).isImmediate() &&
+        MI->getOperand(1).getImmedValue() == 0) {
+      FrameIndex = MI->getOperand(0).getFrameIndex();
+      return MI->getOperand(2).getReg();
+    }
+  }
+  return 0;
+}
