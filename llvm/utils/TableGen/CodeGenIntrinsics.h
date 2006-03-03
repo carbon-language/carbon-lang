f6e846eee81f6d1abab1077825a60ab19dd11645
Chris Lattner <sabre@nondot.org>
2006-03-03 06:13:41 +0000

    Fix pasteo
    llvm-svn: 26499

--- llvm/utils/TableGen/CodeGenIntrinsics.h
+++ llvm/utils/TableGen/CodeGenIntrinsics.h
@@ -2,7 +2,7 @@
 //
 //                     The LLVM Compiler Infrastructure
 //
-// This file was developed by the LLVM research group and is distributed under
+// This file was developed by Chris Lattner and is distributed under
 // the University of Illinois Open Source License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
