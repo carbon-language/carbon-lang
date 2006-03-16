cce5eef4316f2a35022f48a28153848e42e82f32
Chris Lattner <sabre@nondot.org>
2006-03-16 18:47:51 +0000

    New testcase, the new CFE compiles this into insertelement instructions, the old one crashes.
    llvm-svn: 26794

---
+++ llvm/test/Regression/CFrontend/2006-03-16-VectorCtor.c
@@ -0,0 +1,11 @@
+// Passes with the new CFE.
+// RUN: %llvmgcc %s -S -o -
+// XFAIL: *
+
+typedef int v4si __attribute__ ((__vector_size__ (16)));
+void test(v4si *P, v4si *Q, float X) {
+  *P = (v4si){ X, X, X, X } * *Q;
+}
+
+v4si G = (v4si){ 0.1, 1.2, 4.2, 17.2 };
+
