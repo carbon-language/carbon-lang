9573f0e5fdd117b8125cbafa2e5cfb530de952ab
Reid Spencer <rspencer@reidspencer.com>
2006-03-23 23:04:50 +0000

    A little script to find LLVM symbols. Ideally this would use c++filt for both input and output to render everything in C++, but that will be the next patch. For now, it just runs nm and greps each .o or .a file for the desired symbol.
    llvm-svn: 27003

---
+++ llvm/utils/findsym.pl
@@ -0,0 +1,34 @@
+#!/usr/bin/perl -w
+#
+# Program:  findsym.pl
+#
+# Synopsis: Generate a list of the libraries in which a symbol is defined or
+#           referenced.
+#
+# Syntax:   GenLibDeps.pl <directory_with_libraries_in_it> <symbol>
+#
+
+# Give first option a name.
+my $Directory = $ARGV[0];
+my $Symbol = $ARGV[1];
+
+
+# Open the directory and read its contents, sorting by name and differentiating
+# by whether its a library (.a) or an object file (.o)
+opendir DIR,$Directory;
+my @files = readdir DIR;
+closedir DIR;
+@objects = grep(/l?i?b?LLVM.*\.[oa]$/,sort(@files));
+
+# Gather definitions from the libraries
+foreach $lib (@objects) {
+  my $head = 0;
+  open SYMS, 
+    "nm $Directory/$lib | grep '$Symbol' | sort --key=3 | uniq |";
+  while (<SYMS>) {
+    if (!$head) { print "$lib:\n"; $head = 1; }
+    chomp($_);
+    print "  $_\n";
+  }
+  close SYMS;
+}
