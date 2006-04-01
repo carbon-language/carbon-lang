1b2436a624695c567b8766a4a008b6bfdd4e6c50
Chris Lattner <sabre@nondot.org>
2006-04-01 23:17:11 +0000

    add valuemapper support for inline asm
    llvm-svn: 27332

--- llvm/lib/Transforms/Utils/ValueMapper.cpp
+++ llvm/lib/Transforms/Utils/ValueMapper.cpp
@@ -24,7 +24,7 @@ Value *llvm::MapValue(const Value *V, std::map<const Value*, Value*> &VM) {
 
   // Global values do not need to be seeded into the ValueMap if they are using
   // the identity mapping.
-  if (isa<GlobalValue>(V))
+  if (isa<GlobalValue>(V) || isa<InlineAsm>(V))
     return VMSlot = const_cast<Value*>(V);
 
   if (Constant *C = const_cast<Constant*>(dyn_cast<Constant>(V))) {
