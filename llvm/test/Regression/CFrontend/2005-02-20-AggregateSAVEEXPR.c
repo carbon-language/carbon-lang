5197dfadf282fde02c20d1a31ebff71c6369d8df
Chris Lattner <sabre@nondot.org>
2006-04-20 17:43:41 +0000

    Fails with all sparcs
    llvm-svn: 27887

--- llvm/test/Regression/CFrontend/2005-02-20-AggregateSAVEEXPR.c
+++ llvm/test/Regression/CFrontend/2005-02-20-AggregateSAVEEXPR.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc %s -o /dev/null -S
 // Note:
-//  We fail this on SparcV9 because the C library seems to be missing complex.h
+//  We fail this on Sparc because the C library seems to be missing complex.h
 //  and the corresponding C99 complex support.
 //
 //  We could modify the test to use only GCC extensions, but I don't know if
