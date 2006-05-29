4967fa722df3f2b4a63d00f469d2f100c7c0d0bb
Reid Spencer <rspencer@reidspencer.com>
2006-05-29 18:06:28 +0000

    Fix file header comment.
    llvm-svn: 28540

--- llvm/tools/llvm2cpp/llvm2cpp.cpp
+++ llvm/tools/llvm2cpp/llvm2cpp.cpp
@@ -2,8 +2,8 @@
 //
 //                     The LLVM Compiler Infrastructure
 //
-// This file was developed by the LLVM research group and is distributed under
-// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// This file was developed by Reid Spencer and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
 //
