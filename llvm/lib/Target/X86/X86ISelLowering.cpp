2b2c1be49c3c79baa576e7414bfc0f10427ccecf
Evan Cheng <evan.cheng@apple.com>
2006-06-01 05:53:27 +0000

    Typos
    llvm-svn: 28617

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -380,7 +380,7 @@ static unsigned AddLiveIn(MachineFunction &MF, unsigned PReg,
 static void
 HowToPassCCCArgument(MVT::ValueType ObjectVT, unsigned NumXMMRegs,
                      unsigned &ObjSize, unsigned &ObjXMMRegs) {
-  NumXMMRegs = 0;
+  ObjXMMRegs = 0;
 
   switch (ObjectVT) {
   default: assert(0 && "Unhandled argument type!");
@@ -784,8 +784,8 @@ HowToPassFastCCArgument(MVT::ValueType ObjectVT,
                         unsigned &ObjSize, unsigned &ObjIntRegs,
                         unsigned &ObjXMMRegs) {
   ObjSize = 0;
-  NumIntRegs = 0;
-  NumXMMRegs = 0;
+  ObjIntRegs = 0;
+  ObjXMMRegs = 0;
 
   switch (ObjectVT) {
   default: assert(0 && "Unhandled argument type!");
