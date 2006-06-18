a88966fd5e37862a33201995f7572f52b1d8aab6
Rafael Espindola <rafael.espindola@gmail.com>
2006-06-18 00:08:07 +0000

    initial implementation of ARMRegisterInfo::eliminateFrameIndex fixes test/Regression/CodeGen/ARM/ret_arg5.ll
    llvm-svn: 28854

--- llvm/lib/Target/ARM/ARMRegisterInfo.cpp
+++ llvm/lib/Target/ARM/ARMRegisterInfo.cpp
@@ -77,7 +77,29 @@ eliminateCallFramePseudoInstr(MachineFunction &MF, MachineBasicBlock &MBB,
 
 void
 ARMRegisterInfo::eliminateFrameIndex(MachineBasicBlock::iterator II) const {
-  assert(0 && "Not Implemented");
+  MachineInstr &MI = *II;
+  MachineBasicBlock &MBB = *MI.getParent();
+  MachineFunction &MF = *MBB.getParent();
+
+  assert (MI.getOpcode() == ARM::movrr);
+
+  unsigned FrameIdx = 1;
+
+  int FrameIndex = MI.getOperand(FrameIdx).getFrameIndex();
+
+  int Offset = MF.getFrameInfo()->getObjectOffset(FrameIndex);
+
+  unsigned StackSize = MF.getFrameInfo()->getStackSize();
+
+  Offset += StackSize;
+
+  // Insert a set of r12 with the full address
+  // r12 = r13 + offset
+  MachineBasicBlock *MBB2 = MI.getParent();
+  BuildMI(*MBB2, II, ARM::addri, 2, ARM::R12).addReg(ARM::R13).addImm(Offset);
+
+  // Replace the FrameIndex with r12
+  MI.getOperand(FrameIdx).ChangeToRegister(ARM::R12);
 }
 
 void ARMRegisterInfo::
