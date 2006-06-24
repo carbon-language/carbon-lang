4da3a353b6b71ddc28c229d77bae8ea1d93ff07e
Chris Lattner <sabre@nondot.org>
2006-06-24 21:29:50 +0000

    Fix off-by-two error printing diagnostics
    llvm-svn: 38565

--- clang/Driver/clang.cpp
+++ clang/Driver/clang.cpp
@@ -148,7 +148,7 @@ void DiagnosticPrinterSTDERR::HandleDiagnostic(Diagnostic::Level Level,
     // Compute the column number.  Rewind from the current position to the start
     // of the line.
     ColNo = SourceMgr.getColumnNumber(Pos);
-    LineStart = FilePos-ColNo-1;  // Column # is 1-based
+    LineStart = FilePos-ColNo+1;  // Column # is 1-based
   
     // Compute the line end.  Scan forward from the error position to the end of
     // the line.
