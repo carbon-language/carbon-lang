370c135dceb3660404a1da1b629a976f1ced1ccd
Chris Lattner <sabre@nondot.org>
2006-07-11 04:03:32 +0000

    improve comment
    llvm-svn: 38696

--- clang/include/clang/Lex/MacroExpander.h
+++ clang/include/clang/Lex/MacroExpander.h
@@ -86,7 +86,8 @@ public:
   /// off the expansion stack, return false and set RanOffEnd to true.
   /// Otherwise, return true if we know for sure that the next token returned
   /// will not be a '(' token.  Return false if it is a '(' token or if we are
-  /// not sure.
+  /// not sure.  This is used when determining whether to expand a function-like
+  /// macro.
   bool NextTokenIsKnownNotLParen(bool &RanOffEnd) const;
   
   MacroInfo &getMacro() const { return Macro; }
