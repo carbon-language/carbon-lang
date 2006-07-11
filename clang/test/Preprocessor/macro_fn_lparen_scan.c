b63d9de243dddbb7240f7786a29612842d56f531
Chris Lattner <sabre@nondot.org>
2006-07-11 05:03:43 +0000

    new tests
    llvm-svn: 38698

--- clang/test/Preprocessor/macro_fn_lparen_scan.c
+++ clang/test/Preprocessor/macro_fn_lparen_scan.c
@@ -1,5 +1,7 @@
 // RUN: clang -E %s | grep 'noexp: foo y' &&
-// RUN: clang -E %s | grep 'expand: abc'
+// RUN: clang -E %s | grep 'expand: abc' &&
+// RUN: clang -E %s | grep 'noexp2: foo nonexp' &&
+// RUN: clang -E %s | grep 'expand2: abc'
 
 #define A foo
 #define foo() abc
@@ -8,9 +10,18 @@
 // This should not expand to abc, because the foo macro isn't followed by (.
 noexp: X
 
-#undef X
 
 // This should expand to abc.
+#undef X
 #define X A ()
 expand: X
+
+
+// This should be 'foo nonexp'
+noexp2: A nonexp
+
+// This should expand
+expand2: A (
+)
+
 
