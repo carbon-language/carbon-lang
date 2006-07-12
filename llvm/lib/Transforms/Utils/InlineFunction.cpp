b3c64f7ab38182823722cdf389891db4861d9584
Chris Lattner <sabre@nondot.org>
2006-07-12 21:37:11 +0000

    Handle instructions in the map, but that map to a null pointer. This unbreaks smg2000.
    llvm-svn: 29127

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -157,7 +157,8 @@ static void UpdateCallGraphAfterInlining(const Function *Caller,
     const Instruction *OrigCall = I->first.getInstruction();
     
     std::map<const Value*, Value*>::iterator VMI = ValueMap.find(OrigCall);
-    if (VMI != ValueMap.end()) { // Only copy the edge if the call was inlined!
+    // Only copy the edge if the call was inlined!
+    if (VMI != ValueMap.end() && VMI->second) {
       // If the call was inlined, but then constant folded, there is no edge to
       // add.  Check for this case.
       if (Instruction *NewCall = dyn_cast<Instruction>(VMI->second))
