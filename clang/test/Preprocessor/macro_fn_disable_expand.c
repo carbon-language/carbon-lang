b563379d78dc69c289315f3b6e35ea99439367c6
Chris Lattner <sabre@nondot.org>
2006-07-19 06:34:41 +0000

    Make this testcase more interesting, actually add a RUN line.
    llvm-svn: 38738

--- clang/test/Preprocessor/macro_fn_disable_expand.c
+++ clang/test/Preprocessor/macro_fn_disable_expand.c
@@ -1,5 +1,11 @@
-// clang %s -E | grep 'bar foo (2)'
+// RUN: clang %s -E | grep 'bar foo (2)' &&
+// RUN: clang %s -E | grep 'm(ABCD)'
 
 #define foo(x) bar x
 foo(foo) (2)
+
+
+#define m(a) a(w)
+#define w ABCD
+m(m)   // m(ABCD)
 
