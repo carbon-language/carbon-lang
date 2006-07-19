e11dd370ec63e67e094d80631f66c9dce4d56d63
Chris Lattner <sabre@nondot.org>
2006-07-19 08:01:28 +0000

    new testcase
    llvm-svn: 38741

---
+++ clang/test/Preprocessor/macro_paste_hashhash.c
@@ -0,0 +1,7 @@
+// RUN: clang -E %s | grep '^"x ## y";$'
+#define hash_hash # ## # 
+#define mkstr(a) # a 
+#define in_between(a) mkstr(a) 
+#define join(c, d) in_between(c hash_hash d) 
+join(x, y);
+
