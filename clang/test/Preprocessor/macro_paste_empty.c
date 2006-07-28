b1d2594456b044d2a55de8d5e427721c0191d953
Chris Lattner <sabre@nondot.org>
2006-07-28 05:13:36 +0000

    Add test for pasting empty formals
    llvm-svn: 38763

---
+++ clang/test/Preprocessor/macro_paste_empty.c
@@ -0,0 +1,13 @@
+// RUN: clang -E %s | grep 'a:Y' &&
+// RUN: clang -E %s | grep 'b:Y' &&
+// RUN: clang -E %s | grep 'c:YY'
+
+#define FOO(X) X ## Y
+a:FOO()
+
+#define FOO2(X) Y ## X
+b:FOO2()
+
+#define FOO3(X) X ## Y ## X ## Y ## X ## X
+c:FOO3()
+
