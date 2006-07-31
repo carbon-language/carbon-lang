8f5dcd716ad8db3ed49799342b0887333f45cbdf
Chris Lattner <sabre@nondot.org>
2006-07-31 17:31:48 +0000

    testcase for PR854.
    llvm-svn: 29412

---
+++ llvm/test/Regression/CFrontend/2006-07-31-PR854.c
@@ -0,0 +1,11 @@
+// RUN: llvm-gcc %s -S -o -
+// PR854
+  struct kernel_symbol {
+    unsigned long value;
+  };
+  unsigned long loops_per_jiffy = (1<<12);
+  static const char __kstrtab_loops_per_jiffy[]
+__attribute__((section("__ksymtab_strings"))) = "loops_per_jiffy";
+  static const struct kernel_symbol __ksymtab_loops_per_jiffy
+__attribute__((__used__)) __attribute__((section("__ksymtab"))) = { (unsigned
+long)&loops_per_jiffy, __kstrtab_loops_per_jiffy };
