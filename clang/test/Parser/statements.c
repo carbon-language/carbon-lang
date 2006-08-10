905caf37a07dcd9dea0d57827ef67ae788b7236b
Chris Lattner <sabre@nondot.org>
2006-08-10 04:59:23 +0000

    add test2
    llvm-svn: 38845

--- clang/test/Parser/statements.c
+++ clang/test/Parser/statements.c
@@ -4,3 +4,15 @@ int test1() {
   { ; {  ;;}} ;;
 }
 
+int test2() {
+  if (0) { if (1) {} } else { }
+
+  do { } while (0); 
+  
+  while (0) while(0) do ; while(0);
+
+  for (0;0;0)
+    for (;;)
+      for (9;0;2)
+        ;
+}
