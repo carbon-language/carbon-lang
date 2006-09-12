2655c59a7bf672b8f0c886ec8e6c3407c55e65d8
Chris Lattner <sabre@nondot.org>
2006-09-12 16:28:14 +0000

    Testcase that crashes the C++ FE.
    llvm-svn: 30273

---
+++ llvm/test/Regression/C++Frontend/2006-09-12-OpaqueStructCrash.cpp
@@ -0,0 +1,28 @@
+// RUN: llvm-g++ -O3 -S -o - %s
+
+struct A {
+   virtual ~A();
+};
+
+template <typename Ty>
+struct B : public A {
+   ~B () { delete [] val; }
+private:
+     Ty* val;
+};
+
+template <typename Ty>
+struct C : public A {
+   C ();
+   ~C ();
+};
+
+template <typename Ty>
+struct D : public A {
+     D () {}
+   private:
+     B<C<Ty> > blocks;
+};
+
+template class D<double>;
+
