3bbf8848558738fd0d6a2e62a9d73d9cc30d3751
Jim Laskey <jlaskey@mac.com>
2006-09-27 16:55:19 +0000

    Make sure C++ protection shows up in debug info
    llvm-svn: 30626

---
+++ llvm/test/Regression/C++Frontend/2006-09-27-Debug-Protection.cpp
@@ -0,0 +1,13 @@
+// RUN: %llvmgxx -O0 -emit-llvm -S -g -o - %s | grep 'uint 1,' &&
+// RUN: %llvmgxx -O0 -emit-llvm -S -g -o - %s | grep 'uint 2,'
+
+class A {
+public:
+  int x;
+protected:
+  int y;
+private:
+  int z;
+};
+
+A a;
