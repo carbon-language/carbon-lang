f66ab9f6f13091d01a3468329267e2999627ddc4
Chris Lattner <sabre@nondot.org>
2006-10-31 06:15:14 +0000

    new testcase
    llvm-svn: 31307

---
+++ llvm/test/Regression/C++Frontend/2006-10-30-ClassBitfield.cpp
@@ -0,0 +1,16 @@
+// RUN: %llvmgxx %s -emit-llvm -S -o -
+// PR954
+
+struct _Refcount_Base   {
+  unsigned long _M_ref_count;
+  int _M_ref_count_lock;
+  _Refcount_Base() : _M_ref_count(0) {}
+};
+
+struct _Rope_RopeRep : public _Refcount_Base 
+{
+public:
+  int _M_tag:8; 
+};
+
+int foo(_Rope_RopeRep* r) { return r->_M_tag; }
