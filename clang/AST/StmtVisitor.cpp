6fef2fb277da6808ac880e9c0cc8914e15434d62
Chris Lattner <sabre@nondot.org>
2006-11-04 20:49:30 +0000

    Add a new StmtNodes.def file as a catalog of all the Stmt's.  This helps eliminate repetitive code.
    llvm-svn: 39117

--- clang/AST/StmtVisitor.cpp
+++ clang/AST/StmtVisitor.cpp
@@ -20,30 +20,8 @@ StmtVisitor::~StmtVisitor() {
   // Out-of-line virtual dtor.
 }
 
-#define DELEGATE_VISITOR(FROM, TO) \
+// Implement all of the delegation visitor methods.
+#define STMT(FROM, TO) \
   void StmtVisitor::Visit##FROM(FROM *Node) { Visit##TO(Node); }
+#include "clang/AST/StmtNodes.def"
 
-DELEGATE_VISITOR(Expr, Stmt)
-
-// Stmt subclasses to Stmt.
-DELEGATE_VISITOR(CompoundStmt, Stmt)
-DELEGATE_VISITOR(IfStmt      , Stmt)
-DELEGATE_VISITOR(WhileStmt   , Stmt)
-DELEGATE_VISITOR(DoStmt      , Stmt)
-DELEGATE_VISITOR(ForStmt     , Stmt)
-DELEGATE_VISITOR(ReturnStmt  , Stmt)
-
-// Expr subclasses to Expr.
-DELEGATE_VISITOR(DeclRefExpr          , Expr)
-DELEGATE_VISITOR(IntegerConstant      , Expr)
-DELEGATE_VISITOR(FloatingConstant     , Expr)
-DELEGATE_VISITOR(StringExpr           , Expr)
-DELEGATE_VISITOR(ParenExpr            , Expr)
-DELEGATE_VISITOR(UnaryOperator        , Expr)
-DELEGATE_VISITOR(SizeOfAlignOfTypeExpr, Expr)
-DELEGATE_VISITOR(ArraySubscriptExpr   , Expr)
-DELEGATE_VISITOR(CallExpr             , Expr)
-DELEGATE_VISITOR(MemberExpr           , Expr)
-DELEGATE_VISITOR(CastExpr             , Expr)
-DELEGATE_VISITOR(BinaryOperator       , Expr)
-DELEGATE_VISITOR(ConditionalOperator  , Expr)
