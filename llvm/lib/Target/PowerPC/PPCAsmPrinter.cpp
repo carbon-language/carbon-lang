30055b920804ca8c9d66388fdefd21a4c32d0afa
Chris Lattner <sabre@nondot.org>
2006-11-16 21:45:30 +0000

    fix a regression that I introduced.  stdu should scale the offset by 4 before printing it.
    llvm-svn: 31791

--- llvm/lib/Target/PowerPC/PPCAsmPrinter.cpp
+++ llvm/lib/Target/PowerPC/PPCAsmPrinter.cpp
@@ -127,7 +127,16 @@ namespace {
       O << (unsigned short)MI->getOperand(OpNo).getImmedValue();
     }
     void printS16X4ImmOperand(const MachineInstr *MI, unsigned OpNo) {
-      O << (short)(MI->getOperand(OpNo).getImmedValue()*4);
+      if (MI->getOperand(OpNo).isImmediate()) {
+        O << (short)(MI->getOperand(OpNo).getImmedValue()*4);
+      } else {
+        O << "lo16(";
+        printOp(MI->getOperand(OpNo));
+        if (TM.getRelocationModel() == Reloc::PIC_)
+          O << "-\"L" << getFunctionNumber() << "$pb\")";
+        else
+          O << ')';
+      }
     }
     void printBranchOperand(const MachineInstr *MI, unsigned OpNo) {
       // Branches can take an immediate operand.  This is used by the branch
