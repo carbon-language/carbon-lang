e6b2c29c270d203df06b62ba7ce2146f355e85c8
Chris Lattner <sabre@nondot.org>
2006-12-01 18:04:08 +0000

    enable this now that codegen support is in place
    llvm-svn: 32076

--- llvm/test/Regression/CFrontend/extern-weak.c
+++ llvm/test/Regression/CFrontend/extern-weak.c
@@ -1,7 +1,5 @@
-// RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep extern_weak
-
-// TODO: Enable this when darwin gets weak support:
-// : %llvmgcc -O3 -S -o - -emit-llvm | llvm-as | llc
+// RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep extern_weak &&
+// RUN: %llvmgcc -O3 -S -o - -emit-llvm | llvm-as | llc
 
 #ifdef __APPLE_CC__
 void foo() __attribute__((weak_import));
