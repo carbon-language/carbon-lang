fb6ce95002f737671a20133fed344e066d93128a
Reid Spencer <rspencer@reidspencer.com>
2006-12-03 16:40:08 +0000

    Use weak_import on linux, weak everywhere else. Makes test pass on Linux. Checking for __APPLE_CC__ is useless as this just checks to see if the compiler is llvm-gcc, which we know it is.
    llvm-svn: 32158

--- llvm/test/Regression/CFrontend/extern-weak.c
+++ llvm/test/Regression/CFrontend/extern-weak.c
@@ -1,7 +1,7 @@
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep extern_weak &&
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm | llvm-as | llc
 
-#ifdef __APPLE_CC__
+#ifndef __linux__
 void foo() __attribute__((weak_import));
 #else
 void foo() __attribute__((weak));
