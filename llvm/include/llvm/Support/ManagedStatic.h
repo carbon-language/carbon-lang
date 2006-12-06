b1a268f08f55ba341e63a1d741b5d1a21bd9d0f7
Chris Lattner <sabre@nondot.org>
2006-12-06 01:01:14 +0000

    add a helper to call llvm_shutdown()
    llvm-svn: 32249

--- llvm/include/llvm/Support/ManagedStatic.h
+++ llvm/include/llvm/Support/ManagedStatic.h
@@ -74,6 +74,14 @@ public:
 /// llvm_shutdown - Deallocate and destroy all ManagedStatic variables.
 void llvm_shutdown();
 
+  
+/// llvm_shutdown_obj - This is a simple helper class that calls
+/// llvm_shutdown() when it is destroyed.
+struct llvm_shutdown_obj {
+  llvm_shutdown_obj() {}
+  ~llvm_shutdown_obj() { llvm_shutdown(); }
+};
+  
 }
 
 #endif
