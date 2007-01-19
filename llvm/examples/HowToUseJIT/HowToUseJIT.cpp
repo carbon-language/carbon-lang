727f31b2cdebca8a05e5095517fe97dee8b1ab57
Reid Spencer <rspencer@reidspencer.com>
2007-01-19 22:45:05 +0000

    Add a #include to resolve IntegerType class.
    llvm-svn: 33384

--- llvm/examples/HowToUseJIT/HowToUseJIT.cpp
+++ llvm/examples/HowToUseJIT/HowToUseJIT.cpp
@@ -36,7 +36,7 @@
 
 #include "llvm/Module.h"
 #include "llvm/Constants.h"
-#include "llvm/Type.h"
+#include "llvm/DerivedTypes.h"
 #include "llvm/Instructions.h"
 #include "llvm/ModuleProvider.h"
 #include "llvm/ExecutionEngine/JIT.h"
