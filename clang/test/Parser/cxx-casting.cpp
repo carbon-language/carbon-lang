3e30f7c70f43835adac87c655c09db901663232b
Chris Lattner <sabre@nondot.org>
2007-01-23 06:12:15 +0000

    add a testcase for c++ casting operators, by Bill
    llvm-svn: 39285

---
+++ clang/test/Parser/cxx-casting.cpp
@@ -0,0 +1,31 @@
+// RUN: clang -fsyntax-only %s
+
+char *const_cast_test(const char *var)
+{
+  return const_cast<char*>(var);
+}
+
+#if 0
+// FIXME: Uncomment when C++ is supported more.
+struct A {
+  virtual ~A() {}
+};
+
+struct B : public A {
+};
+
+struct B *dynamic_cast_test(struct A *a)
+{
+  return dynamic_cast<struct B*>(a);
+}
+#endif
+
+char *reinterpret_cast_test()
+{
+  return reinterpret_cast<char*>(0xdeadbeef);
+}
+
+double static_cast_test(int i)
+{
+  return static_cast<double>(i);
+}
