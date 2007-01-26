a426fcd6814c56d7da38daebc0ccdc4aecfc2759
Devang Patel <dpatel@apple.com>
2007-01-26 18:17:59 +0000

    Order createSimplifyLibCallsPass() after a set of function level passes so that these function passes are managed by CallGraphPassManager.
    llvm-svn: 33540

--- llvm/tools/gccas/gccas.cpp
+++ llvm/tools/gccas/gccas.cpp
@@ -97,7 +97,6 @@ void AddConfiguredTransformationPasses(PassManager &PM) {
 
   if (!DisableInline)
     addPass(PM, createFunctionInliningPass());   // Inline small functions
-  addPass(PM, createSimplifyLibCallsPass());     // Library Call Optimizations
   addPass(PM, createArgumentPromotionPass());    // Scalarize uninlined fn args
 
   addPass(PM, createRaisePointerReferencesPass());// Recover type information
@@ -129,6 +128,7 @@ void AddConfiguredTransformationPasses(PassManager &PM) {
   addPass(PM, createDeadStoreEliminationPass()); // Delete dead stores
   addPass(PM, createAggressiveDCEPass());        // SSA based 'Aggressive DCE'
   addPass(PM, createCFGSimplificationPass());    // Merge & remove BBs
+  addPass(PM, createSimplifyLibCallsPass());     // Library Call Optimizations
   addPass(PM, createDeadTypeEliminationPass());  // Eliminate dead types
   addPass(PM, createConstantMergePass());        // Merge dup global constants
 }
