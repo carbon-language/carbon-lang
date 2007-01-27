2ee13a0881fecc5f4ef79759a485ed5b04f6ed87
Bill Wendling <isanbard@gmail.com>
2007-01-27 02:53:50 +0000

    The TargetELFWriterInfo class holds target-specific information for the ELF writer.
    llvm-svn: 33570

---
+++ llvm/include/llvm/Target/TargetELFWriterInfo.h
@@ -0,0 +1,43 @@
+//===-- llvm/Target/TargetELFWriterInfo.h - ELF Writer Info -----*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Bill Wendling and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the TargetELFWriterInfo class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_TARGET_TARGETELFWRITERINFO_H
+#define LLVM_TARGET_TARGETELFWRITERINFO_H
+
+namespace llvm {
+
+  class MachineBasicBlock;
+
+  //===--------------------------------------------------------------------===//
+  //                          TargetELFWriterInfo
+  //===--------------------------------------------------------------------===//
+
+  class TargetELFWriterInfo {
+    // EMachine - This field is the target specific value to emit as the
+    // e_machine member of the ELF header.
+    unsigned short EMachine;
+  public:
+    enum MachineType {
+      NoMachine,
+      EM_386 = 3
+    };
+
+    TargetELFWriterInfo(MachineType machine) : EMachine(machine) {}
+    virtual ~TargetELFWriterInfo() {}
+
+    unsigned short getEMachine() const { return EMachine; }
+  };
+
+} // end llvm namespace
+
+#endif // LLVM_TARGET_TARGETELFWRITERINFO_H
