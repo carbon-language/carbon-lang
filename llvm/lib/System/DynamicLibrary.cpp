104fc7f823cdb2ad258c0ad9b51daa03383496ca
Chris Lattner <sabre@nondot.org>
2007-02-01 04:57:00 +0000

    silence warning
    llvm-svn: 33746

--- llvm/lib/System/DynamicLibrary.cpp
+++ llvm/lib/System/DynamicLibrary.cpp
@@ -49,6 +49,7 @@ static inline void check_ltdl_initialization() {
   static bool did_initialize_ltdl = false;
   if (!did_initialize_ltdl) {
     int Err = lt_dlinit();
+    Err = Err; // Silence warning.
     assert(0 == Err && "Can't init the ltdl library");
     did_initialize_ltdl = true;
   }
