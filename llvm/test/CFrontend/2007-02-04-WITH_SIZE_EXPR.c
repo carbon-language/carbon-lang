0132cdda10ba469ae77b550fd979e8be13e11490
Chris Lattner <sabre@nondot.org>
2007-02-05 03:56:59 +0000

    new testcase for 1174, currently xfailed
    llvm-svn: 33898

---
+++ llvm/test/CFrontend/2007-02-04-WITH_SIZE_EXPR.c
@@ -0,0 +1,22 @@
+// RUN: %llvmgcc %s -O3 -S -o - -emit-llvm
+// PR1174
+// XFAIL: *
+
+void zzz (char *s1, char *s2, int len, int *q)
+{
+  int z = 5;
+  unsigned int i,  b;
+  struct { char a[z]; } x;
+          
+  for (i = 0; i < len; i++)
+    s1[i] = s2[i];
+
+  b = z & 0x3;
+
+  len += (b == 0 ? 0 : 1) + z;
+    
+  *q = len;
+
+  foo (x, x);
+}
+
