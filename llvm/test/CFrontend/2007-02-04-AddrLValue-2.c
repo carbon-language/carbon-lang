d282d49e781e967fac443cd4151819bf0fa6ad02
Anton Korobeynikov <asl@math.spbu.ru>
2007-02-05 02:28:56 +0000

    Test for PR1173
    llvm-svn: 33895

---
+++ llvm/test/CFrontend/2007-02-04-AddrLValue-2.c
@@ -0,0 +1,13 @@
+// RUN: %llvmgcc %s -O3 -S -o - -emit-llvm
+// PR1173
+
+struct S { char s; };
+struct T { struct S t; };
+
+struct S *const p = &((struct T * const) (0x4000))->t;
+
+void
+foo (void)
+{
+  p->s = 0;
+}
