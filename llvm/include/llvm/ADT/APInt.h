c43c1402eff1df31b315c173e2700734d3377933
Zhou Sheng <zhousheng00@gmail.com>
2007-02-06 05:59:47 +0000

    As Reid suggested, fixed some problems.
    llvm-svn: 33954

--- llvm/include/llvm/ADT/APInt.h
+++ llvm/include/llvm/ADT/APInt.h
@@ -34,8 +34,10 @@ namespace llvm {
 ///
 /// @brief Class for arbitrary precision integers.
 ///
+/// Note: In this class, all bit/byte/word positions are zero-based.
+///
 class APInt {
-  /// Friend Functions of APInt Declared here. For detailed comments,
+  /// Friend Functions of APInt declared here. For detailed comments,
   /// see bottom of this file.
   friend bool isIntN(unsigned N, const APInt& APIVal);
   friend APInt ByteSwap(const APInt& APIVal);
@@ -47,21 +49,21 @@ class APInt {
   bool isSigned;         ///< The sign flag for this APInt.
 
   /// This union is used to store the integer value. When the
-  /// integer bit-width <= 64, it is used as an uint64_t; 
-  /// otherwise it uses an uint64_t array.
+  /// integer bit-width <= 64, it uses VAL; 
+  /// otherwise it uses the pVal.
   union {
     uint64_t VAL;    ///< Used to store the <= 64 bits integer value.
     uint64_t *pVal;  ///< Used to store the >64 bits integer value.
   };
 
-  /// This enum is just used to hold constant we needed for APInt.
+  /// This enum is just used to hold a constant we needed for APInt.
   enum {
     APINT_BITS_PER_WORD = sizeof(uint64_t) * 8
   };
 
-  /// @returns the number of words to hold the integer value of this APInt.
   /// Here one word's bitwidth equals to that of uint64_t.
-  /// @brief Get the number of the words.
+  /// @returns the number of words to hold the integer value of this APInt.
+  /// @brief Get the number of words.
   inline unsigned numWords() const {
     return bitsnum < 1 ? 0 : (bitsnum + APINT_BITS_PER_WORD - 1) /
                              APINT_BITS_PER_WORD;
@@ -73,23 +75,19 @@ class APInt {
   { return bitsnum <= APINT_BITS_PER_WORD; }
 
   /// @returns the word position for the specified bit position.
-  /// Note: the bitPosition and the return value are zero-based.
   static inline unsigned whichWord(unsigned bitPosition)
   { return bitPosition / APINT_BITS_PER_WORD; }
 
   /// @returns the byte position for the specified bit position.
-  /// Note: the bitPosition and the return value are zero-based.
   static inline unsigned whichByte(unsigned bitPosition);
 
   /// @returns the bit position in a word for the specified bit position 
   /// in APInt.
-  /// Note: the bitPosition and the return value are zero-based.
   static inline unsigned whichBit(unsigned bitPosition)
   { return bitPosition % APINT_BITS_PER_WORD; }
 
   /// @returns a uint64_t type integer with just bit position at
   /// "whichBit(bitPosition)" setting, others zero.
-  /// Note: the bitPosition and the return value are zero-based.
   static inline uint64_t maskBit(unsigned bitPosition)
   { return (static_cast<uint64_t>(1)) << whichBit(bitPosition); }
 
@@ -102,223 +100,193 @@ class APInt {
   }
 
   /// @returns the corresponding word for the specified bit position.
-  /// Note: the bitPosition is zero-based.
-  inline uint64_t& getWord(unsigned bitPosition);
+  inline uint64_t& getWord(unsigned bitPosition)
+  { return isSingleWord() ? VAL : pVal[whichWord(bitPosition)]; }
 
   /// @returns the corresponding word for the specified bit position.
   /// This is a constant version.
-  /// Note: the bitPosition is zero-based.
-  inline uint64_t getWord(unsigned bitPosition) const;
-
-  /// mul_1 - This function performs the multiplication operation on a
-  /// large integer (represented as a integer array) and a uint64_t integer.
-  /// @returns the carry of the multiplication.
-  static uint64_t mul_1(uint64_t dest[], uint64_t x[], 
-                        unsigned len, uint64_t y);
-
-  /// mul - This function performs the multiplication operation on two large
-  /// integers (represented as integer arrays).
-  static void mul(uint64_t dest[], uint64_t x[], unsigned xlen,
-                  uint64_t y[], unsigned ylen);
-
-  /// add_1 - This function performs the addition operation on a large integer
-  /// and a uint64_t integer.
-  /// @returns the carry of the addtion.
-  static uint64_t add_1(uint64_t dest[], uint64_t x[], 
-                        unsigned len, uint64_t y);
-
-  /// add - This function performs the addtion operation on two large integers.
-  static uint64_t add(uint64_t dest[], uint64_t x[], 
-                      uint64_t y[], unsigned len);
-
-  /// sub_1 - This function performs the subtraction operation on a large 
-  /// integer and a uint64_t integer.
-  static uint64_t sub_1(uint64_t x[], unsigned len, uint64_t y);
-
-  /// sub - This function performs the subtraction operation on two large 
-  /// integers.
-  static uint64_t sub(uint64_t dest[], uint64_t x[], 
-                      uint64_t y[], unsigned len);
-
-  /// unitDiv - This function divides uint64_t N by unsigned D.
-  /// @returns (remainder << 32) + quotient.
-  /// @assumes (N >> 32) < D.
-  static uint64_t unitDiv(uint64_t N, unsigned D);
-
-  /// subMul - This function subtract x[len-1 : 0] * y from 
-  /// dest[offset+len-1 : offset].
-  /// @returns the most significant word of the product, minus borrow-out from
-  /// the subtraction.
-  static unsigned subMul(unsigned dest[], unsigned offset, 
-                         unsigned x[], unsigned len, unsigned y);
-
-  /// div - This function divides the large integer zds[] by y[].
-  /// The remainder ends up in zds[ny-1 : 0].
-  /// The quotient ends up in zds[ny : nx].
-  /// @assumes nx > ny and (int)y[ny-1] < 0.
-  static void div(unsigned zds[], unsigned nx, unsigned y[], unsigned ny);
-
-  /// lshift - This function shifts x[len-1 : 0] by shiftAmt, and store the 
-  /// "len" least significant words of the result in 
-  /// dest[d_offset+len-1 : d_offset].
-  /// @returns the bits shifted out from the most significant digit.
-  static uint64_t lshift(uint64_t dest[], unsigned d_offset, 
-                         uint64_t x[], unsigned len, unsigned shiftAmt);
+  inline uint64_t getWord(unsigned bitPosition) const
+  { return isSingleWord() ? VAL : pVal[whichWord(bitPosition)]; }
 
 public:
-  /// Create a new APInt of numBits bit-width, and initalized as val.
+  /// @brief Create a new APInt of numBits bit-width, and initialized as val.
   APInt(uint64_t val = 0, unsigned numBits = APINT_BITS_PER_WORD, 
         bool sign = false);
 
-  /// Create a new APInt of numBits bit-width, and initalized as bigVal[].
+  /// @brief Create a new APInt of numBits bit-width, and initialized as 
+  /// bigVal[].
   APInt(unsigned numBits, uint64_t bigVal[], bool sign = false);
 
-  /// Create a new APInt by translating the string represented integer value.
+  /// @brief Create a new APInt by translating the string represented 
+  /// integer value.
   APInt(std::string& Val, uint8_t radix = 10, bool sign = false);
 
-  /// Copy Constructor.
+  /// @brief Copy Constructor.
   APInt(const APInt& API);
 
-  /// Destructor.
+  /// @brief Destructor.
   ~APInt();
 
-  /// @brief Copy assignment operator. Create a new object from the given
-  /// APInt one by initialization.
+  /// @brief Copy assignment operator. 
   APInt& operator=(const APInt& RHS);
 
-  /// @brief Assignment operator. Assigns a common case integer value to 
-  /// the APInt.
+  /// Assigns an integer value to the APInt.
+  /// @brief Assignment operator. 
   APInt& operator=(uint64_t RHS);
 
-  /// @brief Postfix increment operator. Increments the APInt by one.
+  /// Increments the APInt by one.
+  /// @brief Postfix increment operator.
   const APInt operator++(int);
 
-  /// @brief Prefix increment operator. Increments the APInt by one.
+  /// Increments the APInt by one.
+  /// @brief Prefix increment operator.
   APInt& operator++();
 
-  /// @brief Postfix decrement operator. Decrements the APInt by one.
+  /// Decrements the APInt by one.
+  /// @brief Postfix decrement operator. 
   const APInt operator--(int);
 
-  /// @brief Prefix decrement operator. Decrements the APInt by one.
+  /// Decrements the APInt by one.
+  /// @brief Prefix decrement operator. 
   APInt& operator--();
 
-  /// @brief Bitwise AND assignment operator. Performs bitwise AND operation on
-  /// this APInt and the given APInt& RHS, assigns the result to this APInt.
+  /// Performs bitwise AND operation on this APInt and the given APInt& RHS, 
+  /// assigns the result to this APInt.
+  /// @brief Bitwise AND assignment operator. 
   APInt& operator&=(const APInt& RHS);
 
-  /// @brief Bitwise OR assignment operator. Performs bitwise OR operation on 
-  /// this APInt and the given APInt& RHS, assigns the result to this APInt.
+  /// Performs bitwise OR operation on this APInt and the given APInt& RHS, 
+  /// assigns the result to this APInt.
+  /// @brief Bitwise OR assignment operator. 
   APInt& operator|=(const APInt& RHS);
 
-  /// @brief Bitwise XOR assignment operator. Performs bitwise XOR operation on
-  /// this APInt and the given APInt& RHS, assigns the result to this APInt.
+  /// Performs bitwise XOR operation on this APInt and the given APInt& RHS, 
+  /// assigns the result to this APInt.
+  /// @brief Bitwise XOR assignment operator. 
   APInt& operator^=(const APInt& RHS);
 
-  /// @brief Left-shift assignment operator. Left-shift the APInt by shiftAmt
-  /// and assigns the result to this APInt.
+  /// Left-shift the APInt by shiftAmt and assigns the result to this APInt.
+  /// @brief Left-shift assignment operator. 
   APInt& operator<<=(unsigned shiftAmt);
 
-  /// @brief Right-shift assignment operator. Right-shift the APInt by shiftAmt
-  /// and assigns the result to this APInt.
+  /// Right-shift the APInt by shiftAmt and assigns the result to this APInt.
+  /// @brief Right-shift assignment operator. 
   APInt& operator>>=(unsigned shiftAmt);
 
-  /// @brief Bitwise complement operator. Performs a bitwise complement 
-  /// operation on this APInt.
+  /// Performs a bitwise complement operation on this APInt.
+  /// @brief Bitwise complement operator. 
   APInt operator~() const;
 
-  /// @brief Multiplication assignment operator. Multiplies this APInt by the 
-  /// given APInt& RHS and assigns the result to this APInt.
+  /// Multiplies this APInt by the  given APInt& RHS and 
+  /// assigns the result to this APInt.
+  /// @brief Multiplication assignment operator. 
   APInt& operator*=(const APInt& RHS);
 
-  /// @brief Division assignment operator. Divides this APInt by the given APInt
-  /// &RHS and assigns the result to this APInt.
+  /// Divides this APInt by the given APInt &RHS and 
+  /// assigns the result to this APInt.
+  /// @brief Division assignment operator. 
   APInt& operator/=(const APInt& RHS);
 
-  /// @brief Addition assignment operator. Adds this APInt by the given APInt&
-  /// RHS and assigns the result to this APInt.
+  /// Adds this APInt by the given APInt& RHS and 
+  /// assigns the result to this APInt.
+  /// @brief Addition assignment operator. 
   APInt& operator+=(const APInt& RHS);
 
-  /// @brief Subtraction assignment operator. Subtracts this APInt by the given
-  /// APInt &RHS and assigns the result to this APInt.
+  /// Subtracts this APInt by the given APInt &RHS and 
+  /// assigns the result to this APInt.
+  /// @brief Subtraction assignment operator. 
   APInt& operator-=(const APInt& RHS);
 
-  /// @brief Remainder assignment operator. Yields the remainder from the 
-  /// division of this APInt by the given APInt& RHS and assigns the remainder 
-  /// to this APInt.
+  /// Yields the remainder from the division of this APInt by 
+  /// the given APInt& RHS and assigns the remainder to this APInt.
+  /// @brief Remainder assignment operator. 
   APInt& operator%=(const APInt& RHS);
 
-  /// @brief Bitwise AND operator. Performs bitwise AND operation on this APInt
-  /// and the given APInt& RHS.
+  /// Performs bitwise AND operation on this APInt and 
+  /// the given APInt& RHS.
+  /// @brief Bitwise AND operator. 
   APInt operator&(const APInt& RHS) const;
 
-  /// @brief Bitwise OR operator. Performs bitwise OR operation on this APInt 
-  /// and the given APInt& RHS.
+  /// Performs bitwise OR operation on this APInt and the given APInt& RHS.
+  /// @brief Bitwise OR operator. 
   APInt operator|(const APInt& RHS) const;
 
-  /// @brief Bitwise XOR operator. Performs bitwise XOR operation on this APInt
-  /// and the given APInt& RHS.
+  /// Performs bitwise XOR operation on this APInt and the given APInt& RHS.
+  /// @brief Bitwise XOR operator. 
   APInt operator^(const APInt& RHS) const;
 
-  /// @brief Logical AND operator. Performs logical AND operation on this APInt
-  /// and the given APInt& RHS.
+  /// Performs logical AND operation on this APInt and the given APInt& RHS.
+  /// @brief Logical AND operator. 
   bool operator&&(const APInt& RHS) const;
 
-  /// @brief Logical OR operator. Performs logical OR operation on this APInt 
-  /// and the given APInt& RHS.
+  /// Performs logical OR operation on this APInt and the given APInt& RHS.
+  /// @brief Logical OR operator. 
   bool operator||(const APInt& RHS) const;
 
-  /// @brief Logical negation operator. Performs logical negation operation on
-  /// this APInt.
+  /// Performs logical negation operation on this APInt.
+  /// @brief Logical negation operator. 
   bool operator !() const;
 
-  /// @brief Multiplication operator. Multiplies this APInt by the given APInt& 
-  /// RHS.
+  /// Multiplies this APInt by the given APInt& RHS.
+  /// @brief Multiplication operator. 
   APInt operator*(const APInt& RHS) const;
 
-  /// @brief Division operator. Divides this APInt by the given APInt& RHS.
+  /// Divides this APInt by the given APInt& RHS.
+  /// @brief Division operator. 
   APInt operator/(const APInt& RHS) const;
 
-  /// @brief Remainder operator. Yields the remainder from the division of this
-  /// APInt and the given APInt& RHS.
+  /// Yields the remainder from the division of 
+  /// this APInt and the given APInt& RHS.
+  /// @brief Remainder operator. 
   APInt operator%(const APInt& RHS) const;
 
-  /// @brief Addition operator. Adds this APInt by the given APInt& RHS.
+  /// Adds this APInt by the given APInt& RHS.
+  /// @brief Addition operator. 
   APInt operator+(const APInt& RHS) const;
 
-  /// @brief Subtraction operator. Subtracts this APInt by the given APInt& RHS
+  /// Subtracts this APInt by the given APInt& RHS
+  /// @brief Subtraction operator. 
   APInt operator-(const APInt& RHS) const;
 
-  /// @brief Left-shift operator. Left-shift the APInt by shiftAmt.
+  /// Left-shift the APInt by shiftAmt.
+  /// @brief Left-shift operator. 
   APInt operator<<(unsigned shiftAmt) const;
 
-  /// @brief Right-shift operator. Right-shift the APInt by shiftAmt.
+  /// Right-shift the APInt by shiftAmt.
+  /// @brief Right-shift operator. 
   APInt operator>>(unsigned shiftAmt) const;
 
   /// @brief Array-indexing support.
   bool operator[](unsigned bitPosition) const;
 
-  /// @brief Equality operator. Compare this APInt with the given APInt& RHS 
+  /// Compare this APInt with the given APInt& RHS 
   /// for the validity of the equality relationship.
+  /// @brief Equality operator. 
   bool operator==(const APInt& RHS) const;
 
-  /// @brief Inequality operator. Compare this APInt with the given APInt& RHS
+  /// Compare this APInt with the given APInt& RHS 
   /// for the validity of the inequality relationship.
+  /// @brief Inequality operator. 
   bool operator!=(const APInt& RHS) const;
 
-  /// @brief Less-than operator. Compare this APInt with the given APInt& RHS
-  /// for the validity of the less-than relationship.
+  /// Compare this APInt with the given APInt& RHS for 
+  /// the validity of the less-than relationship.
+  /// @brief Less-than operator. 
   bool operator <(const APInt& RHS) const;
 
-  /// @brief Less-than-or-equal operator. Compare this APInt with the given 
-  /// APInt& RHS for the validity of the less-than-or-equal relationship.
+  /// Compare this APInt with the given APInt& RHS for the validity 
+  /// of the less-than-or-equal relationship.
+  /// @brief Less-than-or-equal operator. 
   bool operator<=(const APInt& RHS) const;
 
-  /// @brief Greater-than operator. Compare this APInt with the given APInt& RHS
-  /// for the validity of the greater-than relationship.
+  /// Compare this APInt with the given APInt& RHS for the validity 
+  /// of the greater-than relationship.
+  /// @brief Greater-than operator. 
   bool operator> (const APInt& RHS) const;
 
-  /// @brief Greater-than-or-equal operator. Compare this APInt with the given 
-  /// APInt& RHS for the validity of the greater-than-or-equal relationship.
+  /// @brief Greater-than-or-equal operator. 
+  /// Compare this APInt with the given APInt& RHS for the validity 
+  /// of the greater-than-or-equal relationship.
   bool operator>=(const APInt& RHS) const;
 
   /// @returns a uint64_t value from this APInt. If this APInt contains a single
@@ -350,7 +318,7 @@ public:
   /// @brief Set every bit to 1.
   APInt& set();
 
-  /// Set the given bit to 1 whose poition is given as "bitPosition".
+  /// Set the given bit to 1 whose position is given as "bitPosition".
   /// @brief Set a given bit to 1.
   APInt& set(unsigned bitPosition);
 
@@ -376,15 +344,19 @@ public:
   /// @returns a character interpretation of the APInt.
   std::string to_string(uint8_t radix = 10) const;
 
+  /// Get an APInt with the same bitsnum as this APInt, just zero mask
+  /// the low bits and right shift to the least significant bit.
   /// @returns the high "numBits" bits of this APInt.
   APInt HiBits(unsigned numBits) const;
 
+  /// Get an APInt with the same bitsnum as this APInt, just zero mask
+  /// the high bits.
   /// @returns the low "numBits" bits of this APInt.
   APInt LoBits(unsigned numBits) const;
 
   /// @returns true if the argument APInt value is a power of two > 0.
   inline const bool isPowerOf2() const {
-    return *this && !(*this & (*this - 1));
+    return (!!*this) && !(*this & (*this - 1));
   }
 
   /// @returns the number of zeros from the most significant bit to the first
@@ -439,50 +411,6 @@ inline APInt LogBase2(const APInt& APIVal) {
 /// @returns the greatest common divisor of the two values 
 /// using Euclid's algorithm.
 APInt GreatestCommonDivisor(const APInt& API1, const APInt& API2);
-
-/// @returns the bit equivalent double.
-/// If the APInt numBits > 64, truncated first and then convert to double.
-inline double APIntToDouble(const APInt& APIVal) {
-  uint64_t value = APIVal.isSingleWord() ? APIVal.VAL : APIVal.pVal[0];
-  union {
-    uint64_t I;
-    double D;
-  } T;
-  T.I = value;
-  return T.D;
-}
-
-/// @returns the bit equivalent float.
-/// If the APInt numBits > 32, truncated first and then convert to double.
-inline float APIntToFloat(const APInt& APIVal) {
-  unsigned value = APIVal.isSingleWord() ? APIVal.VAL : APIVal.pVal[0];
-  union {
-    unsigned I;
-    float F;
-  } T;
-  T.I = value;
-  return T.F;
-}
-
-/// @returns the bit equivalent APInt.
-inline APInt DoubleToAPInt(double Double) {
-  union {
-    uint64_t L;
-    double D;
-  } T;
-  T.D = Double;
-  return APInt(T.L);
-}
-
-/// @returns the bit equivalent APInt.
-inline APInt FloatToAPInt(float Float) {
-  union {
-    uint32_t I;
-    float F;
-  } T;
-  T.F = Float;
-  return APInt(uint64_t(T.I));
-}
 
 } // End of llvm namespace
 
