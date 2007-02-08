6ea699e0fed363315b692d79c8af799d33b56a25
Bill Wendling <isanbard@gmail.com>
2007-02-08 01:31:38 +0000

    Declarations for functions that create different file writers.
    llvm-svn: 34028

---
+++ llvm/include/llvm/CodeGen/FileWriters.h
@@ -0,0 +1,32 @@
+//===-- FileWriters.cpp - File Writers Creation Functions -------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Bill Wendling and is distributed under the
+// University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Functions to add the various file writer passes.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CODEGEN_FILEWRITERS_H
+#define LLVM_CODEGEN_FILEWRITERS_H
+
+#include <iosfwd>
+
+namespace llvm {
+
+  class FunctionPassManager;
+  class MachineCodeEmitter;
+  class TargetMachine;
+
+  MachineCodeEmitter *AddELFWriter(FunctionPassManager &FPM, std::ostream &O,
+                                   TargetMachine &TM);
+  MachineCodeEmitter *AddMachOWriter(FunctionPassManager &FPM, std::ostream &O,
+                                     TargetMachine &TM);
+
+} // end llvm namespace
+
+#endif // LLVM_CODEGEN_FILEWRITERS_H
