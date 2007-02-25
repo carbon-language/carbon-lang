eef0a351b1897170b163783327a05330b78a106d
Chris Lattner <sabre@nondot.org>
2007-02-25 22:02:01 +0000

    new testcase
    llvm-svn: 34611

---
+++ llvm/test/CFrontend/2007-02-25-C-DotDotDot.c
@@ -0,0 +1,11 @@
+// RUN: %llvmgcc -O0 -S -o - -emit-llvm -fno-inline -fno-unit-at-a-time %s | grep 'call float @foo'
+
+// Make sure the call to foo is compiled as:
+//  call float @foo()
+// not
+//  call float (...)* bitcast (float ()* @foo to float (...)*)( )
+
+static float foo() { return 0.0; }
+float bar() { return foo()*10.0;}
+
+
