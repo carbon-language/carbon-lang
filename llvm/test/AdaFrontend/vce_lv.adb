62574103d11138c376bc00f34edec99f7121a62f
Duncan Sands <baldrick@free.fr>
2007-03-27 20:27:25 +0000

    Test that a VIEW_CONVERT_EXPR used as an lvalue has the right type.
    llvm-svn: 35387

---
+++ llvm/test/AdaFrontend/vce_lv.adb
@@ -0,0 +1,9 @@
+-- RUN: %llvmgcc -c %s -o /dev/null
+procedure VCE_LV is
+   type P is access String ;
+   type T is new P (5 .. 7);
+   subtype U is String (5 .. 7);
+   X : T := new U'(others => 'A');
+begin
+   null;
+end;
