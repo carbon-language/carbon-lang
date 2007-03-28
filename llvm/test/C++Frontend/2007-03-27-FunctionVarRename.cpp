517886963f021aa419fc5d5f4a33f6f6919fb041
Chris Lattner <sabre@nondot.org>
2007-03-28 00:03:10 +0000

    new testcase
    llvm-svn: 35391

---
+++ llvm/test/C++Frontend/2007-03-27-FunctionVarRename.cpp
@@ -0,0 +1,17 @@
+// RUN: %llvmgxx %s -emit-llvm -S -o - | not grep 'eprintf1' &&
+// RUN: %llvmgxx %s -emit-llvm -S -o - | not grep 'eprintf'
+
+// Only one eprintf should exist in the output
+
+extern "C" 
+void __eprintf();
+
+void foo() {
+
+  __eprintf();
+}
+
+void *bar() {
+  extern void *__eprintf;
+  return &__eprintf;
+}
