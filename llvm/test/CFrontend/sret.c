ecc769e4f2865b3af3d34c376471301cb052f583
Anton Korobeynikov <asl@math.spbu.ru>
2007-04-01 22:03:04 +0000

    Add testcase for correct sret handling.
    llvm-svn: 35562

---
+++ llvm/test/CFrontend/sret.c
@@ -0,0 +1,15 @@
+// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | grep 'sret' | wc -l | grep '5'
+
+struct abc {
+ int a;
+ int b;
+ int c;
+};
+ 
+struct abc foo1(void);
+struct abc foo2();
+
+void bar() {
+  struct abc dummy1 = foo1();
+  struct abc dummy2 = foo2();
+}
