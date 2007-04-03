349987ed2c2d0b7ccff36f57f98b2c4d923cfb9c
Chris Lattner <sabre@nondot.org>
2007-04-03 21:47:36 +0000

    testcase for recent bugfix
    llvm-svn: 35630

---
+++ llvm/test/FrontendObjC/2007-04-03-ObjcEH.m
@@ -0,0 +1,29 @@
+// RUN: %llvmgcc -c %s -o /dev/null
+
+@interface B 
+-(int)bar;
+@end
+
+@interface A
+-(void) Foo:(int) state;
+@end
+
+@implementation A 
+- (void) Foo:(int) state {
+
+        int wasResponded = 0;
+        @try {
+        if (state) {
+           B * b = 0;
+           @try { }
+           @finally {
+             wasResponded = ![b bar];
+           }
+        }
+        }
+        @finally {
+        }
+}
+@end
+
+
