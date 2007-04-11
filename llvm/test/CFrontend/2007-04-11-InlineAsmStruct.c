8aa5b4545f48724fded4785c684ac4a70c874609
Chris Lattner <sabre@nondot.org>
2007-04-11 23:45:16 +0000

    new testcase.
    llvm-svn: 35931

--- llvm/test/CFrontend/2007-04-11-InlineAsmStruct.c
+++ llvm/test/CFrontend/2007-04-11-InlineAsmStruct.c
@@ -1,7 +1,9 @@
 // RUN: %llvmgcc %s -S -emit-llvm -o - | grep 'call i32 asm'
 
-union U { int x; char* p; };
-void foo() {
-  union U bar;
+struct V { short X, Y; };
+int bar() {
+  struct V bar;
   __asm__ volatile("foo %0\n" : "=r"(bar));
+  return bar.X;
 }
+
