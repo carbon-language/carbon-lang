036cd14ecb62e3e07ba5f0a6f14a0034455256e7
Dale Johannesen <dalej@apple.com>
2007-04-13 22:54:11 +0000

    tests for struct/union as input operands to asm.
    llvm-svn: 35976

---
+++ llvm/test/CFrontend/2007-04-13-InlineAsmStruct2.c
@@ -0,0 +1,9 @@
+// RUN: %llvmgcc %s -S -emit-llvm -o - | grep 'call void asm'
+
+struct V { short X, Y; };
+int bar() {
+  struct V bar;
+  __asm__ volatile("foo %0\n" :: "r"(bar));
+  return bar.X;
+}
+
---
+++ llvm/test/CFrontend/2007-04-13-InlineAsmUnion2.c
@@ -0,0 +1,7 @@
+// RUN: %llvmgcc %s -S -emit-llvm -o - | grep 'call void asm'
+
+union U { int x; char* p; };
+void foo() {
+  union U bar;
+  __asm__ volatile("foo %0\n" :: "r"(bar));
+}
