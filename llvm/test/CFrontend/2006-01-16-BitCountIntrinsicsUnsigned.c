59d3ca99b6e4c3073b776ef7f4f73fc912cfe118
Reid Spencer <rspencer@reidspencer.com>
2007-04-16 01:31:11 +0000

    For PR1336: This test was failing because of signlessness and a difference in the way the AsmWriter prints arguments.
    llvm-svn: 36098

--- llvm/test/CFrontend/2006-01-16-BitCountIntrinsicsUnsigned.c
+++ llvm/test/CFrontend/2006-01-16-BitCountIntrinsicsUnsigned.c
@@ -1,5 +1,5 @@
-// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | wc -l | grep 2
-// XFAIL: *
+// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32( i32} | wc -l | grep 2
+// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | wc -l | grep 1
 
 unsigned t2(unsigned X) {
   return __builtin_clz(X);
