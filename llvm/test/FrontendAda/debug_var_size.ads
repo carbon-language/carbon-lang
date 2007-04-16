d16d149262f1a3880e58bb9bbf5174a61f2b9665
Duncan Sands <baldrick@free.fr>
2007-04-16 18:49:21 +0000

    Testcase for debug info emission for structure fields at variable offsets.
    llvm-svn: 36147

---
+++ llvm/test/FrontendAda/debug_var_size.ads
@@ -0,0 +1,8 @@
+-- RUN: %llvmgcc -c -g %s -o /dev/null
+package Debug_Var_Size is
+   subtype Length_Type is Positive range 1 .. 64;
+   type T (Length : Length_Type := 1) is record
+      Varying_Length : String (1 .. Length);
+      Fixed_Length   : Boolean;
+   end record;
+end;
