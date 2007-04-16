6f3a548ff403d0e778d39c5643be96ac5552b5d6
Jeff Cohen <jeffc@jolt-lang.org>
2007-04-16 21:59:44 +0000

    In the event that some really old non-Intel or -AMD CPU is encountered...
    llvm-svn: 36177

--- llvm/lib/Target/X86/X86Subtarget.cpp
+++ llvm/lib/Target/X86/X86Subtarget.cpp
@@ -115,8 +115,11 @@ void X86Subtarget::AutoDetectSubtargetFeatures() {
   if (ECX & 0x1)         X86SSELevel = SSE3;
   if ((ECX >> 9)  & 0x1) X86SSELevel = SSSE3;
 
-  X86::GetCpuIDAndInfo(0x80000001, &EAX, &EBX, &ECX, &EDX);
-  HasX86_64 = (EDX >> 29) & 0x1;
+  if (memcmp(text.c, "GenuineIntel", 12) == 0 ||
+      memcmp(text.c, "AuthenticAMD", 12) == 0) {
+    X86::GetCpuIDAndInfo(0x80000001, &EAX, &EBX, &ECX, &EDX);
+    HasX86_64 = (EDX >> 29) & 0x1;
+  }
 }
 
 static const char *GetCurrentX86CPU() {
