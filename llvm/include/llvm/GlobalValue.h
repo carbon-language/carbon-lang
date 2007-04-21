fa12f9d64a3fb5bf7b5027eb0e7608cad1bc74be
Jeff Cohen <jeffc@jolt-lang.org>
2007-04-21 15:29:13 +0000

    Fix breakage of bytecode reader when built with VC++.
    llvm-svn: 36303

--- llvm/include/llvm/GlobalValue.h
+++ llvm/include/llvm/GlobalValue.h
@@ -55,8 +55,10 @@ protected:
   }
 
   Module *Parent;
-  LinkageTypes Linkage : 4;   // The linkage of this global
-  VisibilityTypes Visibility : 1;  // The visibility style of this global
+  // Note: VC++ treats enums as signed, so an extra bit is required to prevent
+  // Linkage and Visibility from turning into negative values.
+  LinkageTypes Linkage : 5;   // The linkage of this global
+  VisibilityTypes Visibility : 2;  // The visibility style of this global
   unsigned Alignment : 16;    // Alignment of this symbol, must be power of two
   std::string Section;        // Section to emit this into, empty mean default
 public:
