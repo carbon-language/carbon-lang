6121802090c21c05e54676aaa7fa3ff21c013081
Chris Lattner <sabre@nondot.org>
2007-04-21 03:35:28 +0000

    make this portable to 64-bit hosts
    llvm-svn: 36297

--- llvm/test/CFrontend/2007-04-11-InlineAsmUnion.c
+++ llvm/test/CFrontend/2007-04-11-InlineAsmUnion.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc %s -S -emit-llvm -o - | grep {call i32 asm}
 
-union U { int x; char* p; };
+union U { int x; float p; };
 void foo() {
   union U bar;
   __asm__ volatile("foo %0\n" : "=r"(bar));
