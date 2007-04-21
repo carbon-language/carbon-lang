e930ee67910c016fccc536fce7312ea02f7ba3a2
Reid Spencer <rspencer@reidspencer.com>
2007-04-21 20:46:55 +0000

    Failure.sh is dead, remove it.
    llvm-svn: 36316

--- llvm/test/Failure.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Failure.sh - This little script is used by the makefile to print out errors
-# when they occur.
-
-echo
-echo "******************** TEST '$1' FAILED! ********************"
-echo
