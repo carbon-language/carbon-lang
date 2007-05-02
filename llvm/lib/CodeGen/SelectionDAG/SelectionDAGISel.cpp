11940fbba378fc6623d323274140cd0972c32164
Anton Korobeynikov <asl@math.spbu.ru>
2007-05-02 22:15:48 +0000

    Properly set arguments bitwidth of EHSELECT node
    llvm-svn: 36654

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGISel.cpp
@@ -2632,7 +2632,7 @@ SelectionDAGLowering::visitIntrinsicCall(CallInst &I, unsigned Intrinsic) {
       if (Reg) CurMBB->addLiveIn(Reg);
 
       // Insert the EHSELECTION instruction.
-      SDVTList VTs = DAG.getVTList(MVT::i32, MVT::Other);
+      SDVTList VTs = DAG.getVTList(TLI.getPointerTy(), MVT::Other);
       SDOperand Ops[2];
       Ops[0] = getValue(I.getOperand(1));
       Ops[1] = getRoot();
@@ -2640,7 +2640,7 @@ SelectionDAGLowering::visitIntrinsicCall(CallInst &I, unsigned Intrinsic) {
       setValue(&I, Op);
       DAG.setRoot(Op.getValue(1));
     } else {
-      setValue(&I, DAG.getConstant(0, MVT::i32));
+      setValue(&I, DAG.getConstant(0, TLI.getPointerTy()));
     }
     
     return 0;
