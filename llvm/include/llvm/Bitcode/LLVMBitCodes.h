7f716a937f801c41ba7e2e8b2727e79d7235a0fb
Chris Lattner <sabre@nondot.org>
2007-05-02 05:47:22 +0000

    update to reflect reality
    llvm-svn: 36643

--- llvm/include/llvm/Bitcode/LLVMBitCodes.h
+++ llvm/include/llvm/Bitcode/LLVMBitCodes.h
@@ -165,7 +165,7 @@ namespace bitc {
     FUNC_CODE_INST_CMP         =  9, // CMP:        [opty, opval, opval, pred]
     
     FUNC_CODE_INST_RET         = 10, // RET:        [opty,opval<optional>]
-    FUNC_CODE_INST_BR          = 11, // BR:         [opval, bb#, bb#] or [bb#]
+    FUNC_CODE_INST_BR          = 11, // BR:         [bb#, bb#, cond] or [bb#]
     FUNC_CODE_INST_SWITCH      = 12, // SWITCH:     [opty, opval, n, n x ops]
     FUNC_CODE_INST_INVOKE      = 13, // INVOKE:     [fnty, op0,op1,op2, ...]
     FUNC_CODE_INST_UNWIND      = 14, // UNWIND
