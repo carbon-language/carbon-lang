7aa944da8b4de0e532a5634a66f99f418d4bd0dc
Devang Patel <dpatel@apple.com>
2007-05-03 00:42:22 +0000

    New test.
    llvm-svn: 36661

---
+++ llvm/test/FrontendObjC/2007-05-02-Strong.m
@@ -0,0 +1,23 @@
+// RUN: %llvmgcc -c %s -fobjc-gc -o /dev/null
+typedef int NSInteger;
+typedef struct _NSRect {
+  int origin;
+  int size;
+} NSRect;
+
+__attribute__((objc_gc(strong))) NSRect *_cachedRectArray;
+extern const NSRect NSZeroRect;
+@interface A{
+}
+-(void)bar:(NSInteger *)rectCount;
+@end
+
+@implementation A 
+
+-(void)bar:(NSInteger *)rectCount {
+  NSRect appendRect = NSZeroRect; 
+
+  _cachedRectArray[*rectCount - 1] = NSZeroRect; 
+}
+
+@end
