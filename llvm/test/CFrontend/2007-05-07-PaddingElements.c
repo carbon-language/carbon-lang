91041718f7d25484aac9c45d91704c96e0deb07c
Devang Patel <dpatel@apple.com>
2007-05-08 06:10:12 +0000

    New test.
    llvm-svn: 36930

---
+++ llvm/test/CFrontend/2007-05-07-PaddingElements.c
@@ -0,0 +1,12 @@
+// PR 1278
+// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | not grep "4 x i8] zeroinitializer"
+// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | not grep "i32 0, i32 2"
+struct s {
+  double d1;
+  int s1;
+};
+
+struct s foo(void) {
+  struct s S = {1.1, 2};
+  return S;
+}
