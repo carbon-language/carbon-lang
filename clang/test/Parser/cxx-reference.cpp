cbf4709c039404057343491e1450dddc3064f256
Bill Wendling <isanbard@gmail.com>
2007-06-03 09:02:28 +0000

    Add const/volatile badness
    llvm-svn: 39579

--- clang/test/Parser/cxx-reference.cpp
+++ clang/test/Parser/cxx-reference.cpp
@@ -1,6 +1,19 @@
-// RUN: clang -fsyntax-only %s
+// RUN: clang -fsyntax-only %s &&
+// RUN: clang -fsyntax-only %s 2>&1 | grep "error: 'const' qualifier may not be applied to a reference" &&
+// RUN: clang -fsyntax-only %s 2>&1 | grep "error: 'volatile' qualifier may not be applied to a reference"
+
 extern char *bork;
 char *& bar = bork;
 
 void foo(int &a) {
 }
+
+typedef int & A;
+
+void g(const A aref) {
+}
+
+int & const X;
+int & volatile Y;
+int & const volatile Z;
+
