c7ebbaa10e77bb17dbdc48ba5e1710f121c59369
Lauro Ramos Venancio <lauro.venancio@gmail.com>
2007-06-08 21:06:23 +0000

    Define AsmTransCBE for ARM.
    llvm-svn: 37527

--- llvm/lib/Target/ARM/ARMTargetAsmInfo.cpp
+++ llvm/lib/Target/ARM/ARMTargetAsmInfo.cpp
@@ -18,8 +18,33 @@
 #include <cctype>
 using namespace llvm;
 
+static const char* arm_asm_table[] = {"{r0}", "r0",
+                                      "{r1}", "r1",
+                                      "{r2}", "r2",
+                                      "{r3}", "r3",
+                                      "{r4}", "r4",
+                                      "{r5}", "r5",
+                                      "{r6}", "r6",
+                                      "{r7}", "r7",
+                                      "{r8}", "r8",
+                                      "{r9}", "r9",
+                                      "{r10}", "r10",
+                                      "{r11}", "r11",
+                                      "{r12}", "r12",
+                                      "{r13}", "r13",
+                                      "{r14}", "r14",
+                                      "{lr}", "lr",
+                                      "{sp}", "sp",
+                                      "{ip}", "ip",
+                                      "{fp}", "fp",
+                                      "{sl}", "sl",
+                                      "{memory}", "memory",
+                                      "{cc}", "cc",
+                                      0,0};
+
 ARMTargetAsmInfo::ARMTargetAsmInfo(const ARMTargetMachine &TM) {
   Subtarget = &TM.getSubtarget<ARMSubtarget>();
+  AsmTransCBE = arm_asm_table;
   if (Subtarget->isTargetDarwin()) {
     GlobalPrefix = "_";
     PrivateGlobalPrefix = "L";
