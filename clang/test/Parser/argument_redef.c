87e46687d94d633f0f2de601307db443efc519cf
Bill Wendling <isanbard@gmail.com>
2007-06-27 07:31:17 +0000

    Submitted by: Bill Wendling Reviewed by: Chris Lattner
    Split up lines to have only one expected output per line. Restored some
    checks.
    
    llvm-svn: 39688

--- clang/test/Parser/argument_redef.c
+++ clang/test/Parser/argument_redef.c
@@ -1,5 +1,6 @@
 /* RUN: clang -parse-ast-check %s
 */
 
-int foo(int A) { int A; } /* expected-error {{redefinition of 'A'}} \
-                             expected-error {{previous definition is here}} */
+int foo(int A) { /* expected-error {{previous definition is here}} */
+  int A; /* expected-error {{redefinition of 'A'}} */
+}
--- clang/test/Parser/recovery-1.c
+++ clang/test/Parser/recovery-1.c
@@ -1,6 +1,7 @@
+// RUN: clang -fsyntax-only -fno-caret-diagnostics -pedantic %s 2>&1 | grep warning | wc -l | grep 1
 // RUN: clang -parse-ast-check %s
 
-char ((((*X x  ] )))); /* expected-error {{expected ')'}} \
-                          expected-error {{to match this '('}} */
+char ((((*X x  ]  /* expected-error {{expected ')'}} */
+ ))));            /* expected-error {{to match this '('}} */
 
 ;   // expected-warning {{ISO C does not allow an extra ';' outside of a function}}
