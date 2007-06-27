52b4b73442631733095ff058a9d521a91acde4d0
Bill Wendling <isanbard@gmail.com>
2007-06-27 18:18:03 +0000

    Submitted by: Bill Wendling
    Fixed checking to coincide with the correct lines.
    
    llvm-svn: 39696

--- clang/test/Parser/recovery-1.c
+++ clang/test/Parser/recovery-1.c
@@ -1,7 +1,7 @@
 // RUN: clang -fsyntax-only -fno-caret-diagnostics -pedantic %s 2>&1 | grep warning | wc -l | grep 1
 // RUN: clang -parse-ast-check %s
 
-char ((((*X x  ]  /* expected-error {{expected ')'}} */
- ))));            /* expected-error {{to match this '('}} */
+char ((((                       /* expected-error {{to match this '('}} */
+*X x ] ))));                    /* expected-error {{expected ')'}} */
 
 ;   // expected-warning {{ISO C does not allow an extra ';' outside of a function}}
