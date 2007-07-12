1e52eee1124f7bc8c96ab4a278e6c78b251530e2
Chris Lattner <sabre@nondot.org>
2007-07-12 16:48:52 +0000

    update test
    llvm-svn: 39784

--- clang/test/Parser/recovery-1.c
+++ clang/test/Parser/recovery-1.c
@@ -1,5 +1,5 @@
 // RUN: clang -fsyntax-only -fno-caret-diagnostics -pedantic %s 2>&1 | grep warning | wc -l | grep 1
-// RUN: clang -parse-ast-check %s
+// RUN: clang -parse-ast-check -pedantic %s
 
 char ((((                       /* expected-error {{to match this '('}} */
 *X x ] ))));                    /* expected-error {{expected ')'}} */
