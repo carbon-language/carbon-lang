d1c161786fdd087c109f1d62e9a79cf90d100c43
Chris Lattner <sabre@nondot.org>
2007-07-12 16:49:27 +0000

    -pedantic no longer defaults to on.
    llvm-svn: 39785

--- clang/test/Parser/attributes.c
+++ clang/test/Parser/attributes.c
@@ -1,4 +1,4 @@
-// RUN: clang -parse-ast-check %s
+// RUN: clang -parse-ast-check %s -pedantic
 
 static __inline void __attribute__((__always_inline__, __nodebug__)) // expected-warning {{extension used}}
 foo (void)
