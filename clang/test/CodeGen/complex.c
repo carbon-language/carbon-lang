03a0073b3666ad17145dbfc6ad9676e78056a9b8
Gabor Greif <ggreif@gmail.com>
2007-07-13 23:40:27 +0000

    fix type of main, use !=
    llvm-svn: 39842

--- clang/test/CodeGen/complex.c
+++ clang/test/CodeGen/complex.c
@@ -1,10 +1,10 @@
 // RUN: clang -emit-llvm %s
 // XFAIL: *
 
-void main(void)
+int main(void)
 {
   double _Complex a = 5;
   double _Complex b = 42;
 
-  return a * b == b * a;
+  return a * b != b * a;
 }
