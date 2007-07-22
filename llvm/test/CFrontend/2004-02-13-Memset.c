337e9c5b20cc2a7867bfa6a4e49eb92d07971cee
Duncan Sands <baldrick@free.fr>
2007-07-22 10:25:44 +0000

    Remove -O1 in favour of making llvm-gcc-4.2 a bit smarter.
    llvm-svn: 40391

--- llvm/test/CFrontend/2004-02-13-Memset.c
+++ llvm/test/CFrontend/2004-02-13-Memset.c
@@ -1,5 +1,4 @@
-// RUN: %llvmgcc -xc %s -c -O1 -o - | llvm-dis | grep llvm.memset | \
-// RUN:   wc -l | grep 3
+// RUN: %llvmgcc -xc %s -c -o - | llvm-dis | grep llvm.memset | wc -l | grep 3
 
 void test(int* X, char *Y) {
   memset(X, 4, 1000);
