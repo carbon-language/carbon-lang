30f060be8041a7b9c3d16a872a05712d4c502317
Dan Gohman <gohman@apple.com>
2007-07-26 16:14:06 +0000

    Fix the alias analysis query in DAGCombiner to not add in two offsets. The SrcValueOffset values are the real offsets from the SrcValue base pointers.
    llvm-svn: 40534

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -4599,8 +4599,8 @@ bool DAGCombiner::isAlias(SDOperand Ptr1, int64_t Size1,
 
   if (CombinerGlobalAA) {
     // Use alias analysis information.
-    int Overlap1 = Size1 + SrcValueOffset1 + Offset1;
-    int Overlap2 = Size2 + SrcValueOffset2 + Offset2;
+    int Overlap1 = Size1 + SrcValueOffset1;
+    int Overlap2 = Size2 + SrcValueOffset2;
     AliasAnalysis::AliasResult AAResult = 
                              AA.alias(SrcValue1, Overlap1, SrcValue2, Overlap2);
     if (AAResult == AliasAnalysis::NoAlias)
