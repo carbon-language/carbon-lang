bc1a69684ccc00a24cf65e2a19ec92da6dd40f43
Chris Lattner <sabre@nondot.org>
2007-08-01 18:37:44 +0000

    these tests aren't xfailed.
    llvm-svn: 40680

--- llvm/test/C++Frontend/2007-07-29-RestrictPtrArg.cpp
+++ llvm/test/C++Frontend/2007-07-29-RestrictPtrArg.cpp
@@ -1,5 +1,4 @@
 // RUN: %llvmgxx -c -emit-llvm %s -o - | llvm-dis | grep noalias
-// NOTE: This should be un-XFAILed when the C++ type qualifiers are fixed
 
 void foo(int * __restrict myptr1, int * myptr2) {
   myptr1[0] = 0;
