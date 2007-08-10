d79671fdf29b51285d620d64425290c6c24def0f
Chris Lattner <sabre@nondot.org>
2007-08-10 20:13:28 +0000

    New file, just a placeholder for now.
    llvm-svn: 41002

---
+++ clang/CodeGen/CGAggExpr.cpp
@@ -0,0 +1,17 @@
+//===--- CGAggExpr.cpp - Emit LLVM Code from Aggregate Expressions --------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by Chris Lattner and is distributed under
+// the University of Illinois Open Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This contains code to emit Aggregate Expr nodes as LLVM code.
+//
+//===----------------------------------------------------------------------===//
+
+#include "CodeGenFunction.h"
+
+using namespace clang;
+using namespace CodeGen;
