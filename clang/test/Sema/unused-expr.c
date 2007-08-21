91b9a4c8c3b04195f023d5d09e9780b4e2f7a90a
Chris Lattner <sabre@nondot.org>
2007-08-21 01:19:46 +0000

    we now correctly emit: unused-expr.c:8:6: warning: comparison of distinct pointer types ('int volatile *' and 'int *')   VP == P;   ~~ ^  ~
    llvm-svn: 41210

--- clang/test/Sema/unused-expr.c
+++ clang/test/Sema/unused-expr.c
@@ -5,7 +5,8 @@ int foo(int X, int Y);
 void bar(volatile int *VP, int *P, int A,
          _Complex double C, volatile _Complex double VC) {
   
-  VP == P;             // expected-warning {{expression result unused}}
+  VP == P;             // expected-warning {{expression result unused}} \
+                          expected-warning {{comparison}}
   (void)A;             // expected-warning {{expression result unused}}
   (void)foo(1,2);      // no warning.
   
