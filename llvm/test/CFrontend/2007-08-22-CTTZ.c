a0f12c5493481bf8a99d3d30dedf6d188a975755
Duncan Sands <baldrick@free.fr>
2007-08-23 17:22:50 +0000

    Fix this testcase: there are two matches for llvm.cttz.i64 because of the declaration of the intrinsic.  Also, emit-llvm is automatic and doesn't need to be specified.
    llvm-svn: 41326

--- llvm/test/CFrontend/2007-08-22-CTTZ.c
+++ llvm/test/CFrontend/2007-08-22-CTTZ.c
@@ -1,5 +1,5 @@
-// RUN: %llvmgcc -O2 -S -o - -emit-llvm %s | grep {llvm.cttz.i64} | count 1
-// RUN: %llvmgcc -O2 -S -o - -emit-llvm %s | not grep {lshr}
+// RUN: %llvmgcc -O2 -S -o - %s | grep {llvm.cttz.i64} | count 2
+// RUN: %llvmgcc -O2 -S -o - %s | not grep {lshr}
 
 int bork(unsigned long long x) {
   return __builtin_ctzll(x);
