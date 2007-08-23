afd455c0cf36d68a1d8d19450bf85fb0ff67eb47
Chris Lattner <sabre@nondot.org>
2007-08-23 23:49:47 +0000

    make this harder
    llvm-svn: 41346

--- clang/test/CodeGen/complex.c
+++ clang/test/CodeGen/complex.c
@@ -10,10 +10,20 @@ int main(void)
 
 _Complex double bar(int);
 void test(_Complex double*);
+void takecomplex(_Complex double);
 
 void test2(int c) {
   _Complex double X;
   X = bar(1);
   test(&X);
+  takecomplex(X);
 }
 
+_Complex double g1, g2;
+
+void test3() {
+  g1 = g1 + g2;
+  g1 = g1 - g2;
+  g1 = g1 * g2;
+  g1 = +-~g1;
+}
