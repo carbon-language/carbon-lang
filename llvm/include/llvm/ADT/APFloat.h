45e608ad752874f0bc74ad32715977142f2947f6
Dale Johannesen <dalej@apple.com>
2007-08-25 00:40:41 +0000

    Comment out declaration of operator== (undefined).
    llvm-svn: 41383

--- llvm/include/llvm/ADT/APFloat.h
+++ llvm/include/llvm/ADT/APFloat.h
@@ -190,7 +190,7 @@ namespace llvm {
     /* The definition of equality is not straightforward for floating point,
        so we won't use operator==.  Use one of the following, or write
        whatever it is you really mean. */
-    bool operator==(const APFloat &) const;     // DO NOT IMPLEMENT
+    // bool operator==(const APFloat &) const;     // DO NOT IMPLEMENT
     
     /* IEEE comparison with another floating point number (QNaNs
        compare unordered, 0==-0). */
