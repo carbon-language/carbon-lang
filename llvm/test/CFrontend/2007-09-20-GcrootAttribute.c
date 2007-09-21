af6912f980ef2c6878ed268098e88111e684aa69
Chris Lattner <sabre@nondot.org>
2007-09-21 17:48:39 +0000

    gcroot testcase, patch by Eric Christopher.
    llvm-svn: 42198

---
+++ llvm/test/CFrontend/2007-09-20-GcrootAttribute.c
@@ -0,0 +1,28 @@
+// RUN: %llvmgxx -S -emit-llvm %s -o - | grep llvm.gcroot
+// RUN: %llvmgxx -S -emit-llvm %s -o - | grep llvm.gcroot | count 6
+
+typedef struct foo_s
+{
+  int a;
+} foo, __attribute__ ((gcroot)) *foo_p;
+
+foo my_foo;
+
+int alpha ()
+{
+  foo my_foo2 = my_foo;
+  
+  return my_foo2.a;
+}
+
+int bar (foo a)
+{
+  foo_p b;
+  return b->a;
+}
+
+foo_p baz (foo_p a, foo_p b, foo_p *c)
+{
+  a = b = *c;
+  return a;
+}
