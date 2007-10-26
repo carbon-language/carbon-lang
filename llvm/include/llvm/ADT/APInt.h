f8555a23ecb75111911cb00094b0bbf15172dbf6
Ted Kremenek <kremenek@apple.com>
2007-10-26 20:42:45 +0000

    Fixed incorrect "path name" in preamble (comment) of header file.
    llvm-svn: 43401

--- llvm/include/llvm/ADT/APInt.h
+++ llvm/include/llvm/ADT/APInt.h
@@ -1,4 +1,4 @@
-//===-- llvm/Support/APInt.h - For Arbitrary Precision Integer -*- C++ -*--===//
+//===-- llvm/ADT/APInt.h - For Arbitrary Precision Integer -----*- C++ -*--===//
 //
 //                     The LLVM Compiler Infrastructure
 //
