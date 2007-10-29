5d18ce750a8da2e3aaa697392572e43ac1fa1c68
Ted Kremenek <kremenek@apple.com>
2007-10-29 17:02:56 +0000

    Added to test case for "self-comparison check" uses of relation operators: x < x and x > x should emit warnings.
    llvm-svn: 43451

--- clang/test/Sema/self-comparison.c
+++ clang/test/Sema/self-comparison.c
@@ -8,6 +8,14 @@ int foo2(int x) {
   return (x) != (((x))); // expected-warning {{self-comparison always results}}
 }
 
+int qux(int x) {
+   return x < x; // expected-warning {{self-comparison}}
+}
+
+int qux2(int x) {
+   return x > x; // expected-warning {{self-comparison}}
+}
+
 int bar(float x) {
   return x == x; // no-warning
 }
