eb8471bfa17e538acc5c1b03acea9ef081370132
Ted Kremenek <kremenek@apple.com>
2007-11-19 06:38:23 +0000

    Added test case for dead stores checker (live variables analysis) that tests for correct propagation/update of liveness information within subexpressions of Block-Level expressions.  Test case provided by Nuno Lopes.
    llvm-svn: 44225

--- clang/test/Sema/dead-stores.c
+++ clang/test/Sema/dead-stores.c
@@ -1,6 +1,13 @@
 // RUN: clang -warn-dead-stores -verify %s
+
 void x() {
   int k, y;
 	int abc=1;
 	long idx=abc+3*5; // expected-warning {{value stored to variable is never used}}
+}
+
+void a(void *b) {
+ char *c = (char*)b; // no-warning
+ char *d = b+1; // expected-warning {{value stored to variable is never used}}
+ printf("%s", c);
 }
