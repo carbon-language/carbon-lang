34ffaeeeed73bf6adc53ae591ef13a9392ace1d3
Zhou Sheng <zhousheng00@gmail.com>
2007-11-27 06:23:59 +0000

    Make this pass for CYGWIN.
    llvm-svn: 44354

--- llvm/test/CFrontend/extern-weak.c
+++ llvm/test/CFrontend/extern-weak.c
@@ -1,7 +1,8 @@
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep extern_weak
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | llvm-as | llc
 
-#if !defined(__linux__) && !defined(__FreeBSD__) && !defined(__OpenBSD__)
+#if !defined(__linux__) && !defined(__FreeBSD__) && \
+    !defined(__OpenBSD__) && !defined(__CYGWIN__)
 void foo() __attribute__((weak_import));
 #else
 void foo() __attribute__((weak));
