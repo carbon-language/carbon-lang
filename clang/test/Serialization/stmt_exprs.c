5d169cf2857691aedf6d756ce732e6412781e5e9
Ted Kremenek <kremenek@apple.com>
2007-11-28 21:29:54 +0000

    Inlined test case to make it independent of the stmt_exprs test case in test/Sema.
    llvm-svn: 44416

--- clang/test/Serialization/stmt_exprs.c
+++ clang/test/Serialization/stmt_exprs.c
@@ -1,2 +1,12 @@
 // RUN: clang %s --test-pickling 2>&1 | grep -q 'SUCCESS'
-#include "../Sema/stmt_exprs.c"
+
+typedef unsigned __uint32_t;
+
+#define __byte_swap_int_var(x) \
+__extension__ ({ register __uint32_t __X = (x); \
+   __asm ("bswap %0" : "+r" (__X)); \
+   __X; })
+
+int test(int _x) {
+ return (__byte_swap_int_var(_x));
+}
