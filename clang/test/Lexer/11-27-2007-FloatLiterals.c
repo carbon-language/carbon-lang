0112f62bb96694b1db11cfd71bc03469332f8c37
Christopher Lamb <christopher.lamb@gmail.com>
2007-11-30 06:35:48 +0000

    Doh! Check in this long overdue test fix.
    llvm-svn: 44450

--- clang/test/Lexer/11-27-2007-FloatLiterals.c
+++ clang/test/Lexer/11-27-2007-FloatLiterals.c
@@ -1,5 +1,5 @@
 // RUN: clang %s -emit-llvm 2>&1 | grep 0x3BFD83C940000000 | count 2
-// RUN: clang %s -emit-llvm 2>&1 | grep 0x46A3B8B5B5056E16 | count 2
+// RUN: clang %s -emit-llvm 2>&1 | grep 2.000000e+32 | count 2
 
 float  F  = 1e-19f;
 double D  = 2e32;
