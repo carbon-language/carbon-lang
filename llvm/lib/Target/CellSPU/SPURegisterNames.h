8d35728607d49f081c3bb06eec245b40904350d2
Scott Michel <scottm@aero.org>
2007-12-05 01:31:18 +0000

    Two missing files.
    llvm-svn: 44596

---
+++ llvm/lib/Target/CellSPU/SPURegisterNames.h
@@ -0,0 +1,20 @@
+//==-- SPURegisterNames.h - Wrapper header for SPU register names -*- C++ -*-=//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file was developed by a team from the Computer Systems Research
+// Department at The Aerospace Corporation.
+//
+// See README.txt for details.
+//
+//===----------------------------------------------------------------------===//
+
+#if !defined(SPU_REGISTER_NAMES_H)
+
+// Define symbolic names for Cell registers.  This defines a mapping from
+// register name to register number.
+//
+#include "SPUGenRegisterNames.inc"
+
+#define SPU_REGISTER_NAMES_H 1
+#endif
