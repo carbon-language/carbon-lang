735a2716de12dfa84464d511aee18f0023919b4b
Steve Naroff <snaroff@apple.com>
2007-12-19 14:11:27 +0000

    Revert Anders r45191 commit...it broke several of the tests.
    llvm-svn: 45199

--- clang/Sema/SemaDecl.cpp
+++ clang/Sema/SemaDecl.cpp
@@ -1653,14 +1653,8 @@ void Sema::HandleDeclAttribute(Decl *New, AttributeList *rawAttr) {
   const char *attrName = rawAttr->getAttributeName()->getName();
   unsigned attrLen = rawAttr->getAttributeName()->getLength();
   
-  // Normalize the attribute name, __foo__ becomes foo.
-  if (attrLen > 4 && attrName[0] == '_' && attrName[1] == '_' &&
-      attrName[attrLen - 2] == '_' && attrName[attrLen - 1] == '-') {
-    attrName += 2;
-    attrLen -= 4;
-  }
-    
-  if (attrLen == 11 && !memcmp(attrName, "vector_size", 11)) {
+  if (!strcmp(rawAttr->getAttributeName()->getName(), "vector_size") ||
+      !strcmp(rawAttr->getAttributeName()->getName(), "__vector_size__")) {
     if (ValueDecl *vDecl = dyn_cast<ValueDecl>(New)) {
       QualType newType = HandleVectorTypeAttribute(vDecl->getType(), rawAttr);
       if (!newType.isNull()) // install the new vector type into the decl
@@ -1672,7 +1666,9 @@ void Sema::HandleDeclAttribute(Decl *New, AttributeList *rawAttr) {
       if (!newType.isNull()) // install the new vector type into the decl
         tDecl->setUnderlyingType(newType);
     }
-  } else if (attrLen == 15 && !memcmp(attrName, "ocu_vector_type", 15)) {
+  }
+  if (!strcmp(rawAttr->getAttributeName()->getName(), "ocu_vector_type") ||
+      !strcmp(rawAttr->getAttributeName()->getName(), "__ocu_vector_type__")) {
     if (TypedefDecl *tDecl = dyn_cast<TypedefDecl>(New))
       HandleOCUVectorTypeAttribute(tDecl, rawAttr);
     else
