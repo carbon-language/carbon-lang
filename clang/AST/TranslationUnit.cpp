5b12ab8c9349f789518ebbfeec226e4cf34ad3aa
Chris Lattner <sabre@nondot.org>
2007-12-29 19:59:25 +0000

    Don't attribute in file headers anymore.  See llvmdev for the discussion of this change.
    llvm-svn: 45410

--- clang/AST/TranslationUnit.cpp
+++ clang/AST/TranslationUnit.cpp
@@ -2,8 +2,8 @@
 //
 //                     The LLVM Compiler Infrastructure
 //
-// This file was developed by Ted Kremenek and is distributed under
-// the University of Illinois Open Source License. See LICENSE.TXT for details.
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
 //
 // FIXME: This should eventually be moved out of the driver, or replaced
 //        with its eventual successor.
--- clang/Analysis/LiveVariables.cpp
+++ clang/Analysis/LiveVariables.cpp
@@ -2,8 +2,8 @@
 //
 //                     The LLVM Compiler Infrastructure
 //
-// This file was developed by Ted Kremenek and is distributed under
-// the University of Illinois Open Source License. See LICENSE.TXT for
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
 // details.
 //
 //===----------------------------------------------------------------------===//
