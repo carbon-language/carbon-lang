1d03fb4e7e5e3ecec9fad83a9f791a940eacf729
Duncan Sands <baldrick@free.fr>
2008-01-03 00:17:02 +0000

    Test handling of records for which the fields are not ordered by offset.
    llvm-svn: 45520

---
+++ llvm/test/FrontendAda/field_order.ads
@@ -0,0 +1,7 @@
+-- RUN: %llvmgcc -c %s
+package Field_Order is
+   type Tagged_Type is abstract tagged null record;
+   type With_Discriminant (L : Positive) is new Tagged_Type with record
+      S : String (1 .. L);
+   end record;
+end;
