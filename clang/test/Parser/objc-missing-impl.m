2de9a0be09c020121ba3c376805cb392a73a6b0e
Fariborz Jahanian <fjahanian@apple.com>
2008-01-10 18:10:31 +0000

    Remove non-ascii chaaracter from diagnostic
    llvm-svn: 45836

--- clang/test/Parser/objc-missing-impl.m
+++ clang/test/Parser/objc-missing-impl.m
@@ -1,2 +1,2 @@
 // RUN: clang -fsyntax-only -verify %s
-@end // expected-warning {{‘@end’ must appear in an @implementation context}}
+@end // expected-warning {{@end must appear in an @implementation context}}
