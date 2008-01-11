5a3deb99b30d611276a4b6233bb457d675f19c35
Chris Lattner <sabre@nondot.org>
2008-01-11 23:06:56 +0000

    llvm-g++ 4.0 has completely different code for this warning, just xfail it.
    llvm-svn: 45890

--- llvm/test/C++Frontend/2008-01-11-BadWarning.cpp
+++ llvm/test/C++Frontend/2008-01-11-BadWarning.cpp
@@ -1,5 +1,6 @@
 // RUN: %llvmgcc -xc++ %s -S -o /dev/null |& not grep warning
 // rdar://5683899
+// XFAIL: llvmgcc4.0.1
 void** f(void **Buckets, unsigned NumBuckets) {
   return Buckets + NumBuckets;
 }
