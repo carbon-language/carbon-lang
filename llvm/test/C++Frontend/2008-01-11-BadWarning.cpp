ca2af72c7b842a99c35088656a0cdd14907594cd
Chris Lattner <sabre@nondot.org>
2008-01-12 01:45:22 +0000

    this actually does pass with 4.0
    llvm-svn: 45899

--- llvm/test/C++Frontend/2008-01-11-BadWarning.cpp
+++ llvm/test/C++Frontend/2008-01-11-BadWarning.cpp
@@ -1,6 +1,5 @@
 // RUN: %llvmgcc -xc++ %s -S -o /dev/null |& not grep warning
 // rdar://5683899
-// XFAIL: llvmgcc4.0.1
 void** f(void **Buckets, unsigned NumBuckets) {
   return Buckets + NumBuckets;
 }
