60bd71603bd03402fa6e421de37ace67995728fc
Duncan Sands <baldrick@free.fr>
2008-01-15 19:55:41 +0000

    Testcase for gimplify_expr crash caused by an unexpected placeholder_expr.
    llvm-svn: 46006

---
+++ llvm/test/FrontendAda/placeholder.adb
@@ -0,0 +1,12 @@
+-- RUN: %llvmgcc -c %s
+procedure Placeholder is
+   subtype Bounded is Integer range 1 .. 5;
+   type Vector is array (Bounded range <>) of Integer;
+   type Interval (Length : Bounded := 1) is record
+      Points : Vector (1 .. Length);
+   end record;
+   An_Interval : Interval := (Length => 1, Points => (1 => 1));
+   generic The_Interval : Interval; package R is end;
+   package body R is end;
+   package S is new R (An_Interval);
+begin null; end;
