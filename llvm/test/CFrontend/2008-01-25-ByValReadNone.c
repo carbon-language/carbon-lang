2a80ba8c7c3a478e3af5f540b7b56d396c001122
Duncan Sands <baldrick@free.fr>
2008-01-28 19:25:47 +0000

    Pure/const functions with ByVal parameters cannot be marked readonly either.
    llvm-svn: 46456

--- llvm/test/CFrontend/2008-01-25-ByValReadNone.c
+++ llvm/test/CFrontend/2008-01-25-ByValReadNone.c
@@ -1,10 +1,15 @@
-// RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep readonly
+// RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | not grep readonly
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | not grep readnone
 
 
-// The struct being passed byval means that we need to mark the 
-// function readonly instead of readnone.  Readnone would allow
-// stores to the arg to be deleted in the caller.
+// The struct being passed byval means that we cannot mark the
+// function readnone.  Readnone would allow stores to the arg to
+// be deleted in the caller.  We also don't allow readonly since
+// the callee might write to the byval parameter.  The inliner
+// would have to assume the worse and introduce an explicit
+// temporary when inlining such a function, which is costly for
+// the common case in which the byval argument is not written.
 struct S { int A[1000]; };
 int __attribute__ ((const)) f(struct S x) { return x.A[0]; }
-
+int g(struct S x) __attribute__ ((pure));
+int h(struct S x) { return g(x); }
