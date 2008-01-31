6f4a9ef4b60495105cea58f042cc2d0bcb6a4f5e
Ted Kremenek <kremenek@apple.com>
2008-01-31 06:49:09 +0000

    Fixed misspelling of "LLVM" due to some editor+Ted madness.
    llvm-svn: 46604

--- clang/Analysis/GRConstants.cpp
+++ clang/Analysis/GRConstants.cpp
@@ -1,6 +1,6 @@
 //===-- GRConstants.cpp - Simple, Path-Sens. Constant Prop. ------*- C++ -*-==//
 //
-//                     The LLValM Compiler Infrastructure
+//                     The LLVM Compiler Infrastructure
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
