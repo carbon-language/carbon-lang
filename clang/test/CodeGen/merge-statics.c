bfc2d8acd9f507a920c64519d78d7c36ee816116
Chris Lattner <sabre@nondot.org>
2008-02-02 04:45:42 +0000

    testcase for previous patch.
    llvm-svn: 46662

---
+++ clang/test/CodeGen/merge-statics.c
@@ -0,0 +1,13 @@
+// RUN: clang %s -emit-llvm | grep internal | count 1
+
+// The two decls for 'a' should merge into one llvm GlobalVariable.
+
+struct s { int x; };
+static struct s a;
+
+struct s *ap1 = &a;
+
+static struct s a =  {
+    10
+};
+
