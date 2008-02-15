e9d0954c06ae4e521dd83c60c1c1305792bfeb4a
Eli Friedman <eli.friedman@gmail.com>
2008-02-15 06:29:53 +0000

    Nevermind, these tests work... I messed up my testing.
    llvm-svn: 47153

--- clang/test/Sema/struct-compat.c
+++ clang/test/Sema/struct-compat.c
@@ -6,14 +6,12 @@ extern struct {int a;} x; // expected-error{{redefinition of 'x'}}
 
 struct x;
 int a(struct x* b) {
-// FIXME: This test currently fails
 // Per C99 6.7.2.3, since the outer and inner "struct x"es have different
 // scopes, they don't refer to the same type, and are therefore incompatible
-struct x {int a;} *c = b;
+struct x {int a;} *c = b; // expected-warning{{incompatible pointer types}}
 }
 
 struct x {int a;} r;
 int b() {
-// FIXME: This test currently also fails
-struct x {char x;} s = r;
+struct x {char x;} s = r; // expected-error{{incompatible type initializing}}
 }
