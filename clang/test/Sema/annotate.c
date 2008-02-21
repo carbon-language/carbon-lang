b16d53ea31f0dd3e7ed26fb59613ecd334a8d301
Nate Begeman <natebegeman@mac.com>
2008-02-21 19:44:16 +0000

    add a test case
    llvm-svn: 47455

---
+++ clang/test/Sema/annotate.c
@@ -0,0 +1,7 @@
+// RUN: clang %s -fsyntax-only -verify
+
+void __attribute__((annotate("foo"))) foo(float *a) { 
+  __attribute__((annotate("bar"))) int x;
+  __attribute__((annotate(1))) int y; // expected-error {{argument to annotate attribute was not a string literal}}
+  __attribute__((annotate("bar", 1))) int z; // expected-error {{attribute requires 1 argument(s)}}
+}
