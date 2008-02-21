a2417c9d8b0bf63f3008383f7ae8938e41dceeda
Chris Lattner <sabre@nondot.org>
2008-02-21 01:42:41 +0000

    add some semantic checks for address spaces.
    llvm-svn: 47423

---
+++ clang/test/Sema/address_spaces.c
@@ -0,0 +1,15 @@
+// RUN: clang %s -fsyntax-only -verify
+
+#define _AS1 __attribute__((address_space(1)))
+#define _AS2 __attribute__((address_space(2)))
+#define _AS3 __attribute__((address_space(3)))
+
+void foo(_AS3 float *a) {
+  _AS2 *x;
+  _AS1 float * _AS2 *B;
+
+  int _AS1 _AS2 *Y;  // expected-error {{multiple address spaces specified for type}}
+  int *_AS1 _AS2 *Z;  // expected-error {{multiple address spaces specified for type}}
+
+  *a = 5.0f;
+}
