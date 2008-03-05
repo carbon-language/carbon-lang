04bd14db4de9d433ea1df5bd35f7e07660e76777
Andrew Lenharth <andrewl@lenharth.org>
2008-03-05 19:48:27 +0000

    sync ops on ptrs, was breaking libgomp
    llvm-svn: 47960

---
+++ llvm/test/CFrontend/2008-03-05-syncPtr.c
@@ -0,0 +1,25 @@
+// RUN: %llvmgcc %s -S -emit-llvm -o /dev/null 
+
+int* foo(int** a, int* b, int* c) {
+return __sync_val_compare_and_swap (a, b, c);
+}
+
+int foo2(int** a, int* b, int* c) {
+return __sync_bool_compare_and_swap (a, b, c);
+}
+
+int* foo3(int** a, int b) {
+  return __sync_fetch_and_add (a, b);
+}
+
+int* foo4(int** a, int b) {
+  return __sync_fetch_and_sub (a, b);
+}
+
+int* foo5(int** a, int* b) {
+  return __sync_lock_test_and_set (a, b);
+}
+
+int* foo6(int** a, int*** b) {
+  return __sync_lock_test_and_set (a, b);
+}
