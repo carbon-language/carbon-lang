be6bb8efacde84870c38d747bf818183ffbce0e1
Nate Begeman <natebegeman@mac.com>
2008-03-07 20:04:49 +0000

    Add new test
    llvm-svn: 48018

---
+++ clang/test/Sema/callingconv.c
@@ -0,0 +1,10 @@
+// RUN: clang %s -fsyntax-only -verify
+
+void __attribute__((fastcall)) foo(float *a) { 
+}
+
+void __attribute__((stdcall)) bar(float *a) { 
+}
+
+void __attribute__((fastcall(1))) baz(float *a) { // expected-error {{attribute requires 0 argument(s)}}
+}
