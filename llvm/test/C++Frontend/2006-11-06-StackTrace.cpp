148562f13e787d7c4d5862ee473234e4bf0a1258
Anton Korobeynikov <asl@math.spbu.ru>
2008-03-09 22:24:03 +0000

    This works on x86_64
    llvm-svn: 48121

--- llvm/test/C++Frontend/2006-11-06-StackTrace.cpp
+++ llvm/test/C++Frontend/2006-11-06-StackTrace.cpp
@@ -9,8 +9,8 @@
 // RUN: gdb -q -batch -n -x %t.in %t.exe | \
 // RUN:   grep {#7  0x.* in main.*(argc=\[12\],.*argv=.*)}
 
-// Only works on ppc and x86.  Should generalize?
-// XFAIL: alpha|ia64|arm|x86_64|amd64
+// Only works on ppc, x86 and x86_64.  Should generalize?
+// XFAIL: alpha|ia64|arm
 
 #include <stdlib.h>
 
