c6a8352820db241c178a5a8c1ac365e87dca1e1e
Ted Kremenek <kremenek@apple.com>
2008-03-19 07:31:52 +0000

    Convert tabs to spaces.
    llvm-svn: 48539

--- clang/test/Analysis/dead-stores.c
+++ clang/test/Analysis/dead-stores.c
@@ -2,8 +2,8 @@
 
 void x() {
   int k, y;
-	int abc=1;
-	long idx=abc+3*5; // expected-warning {{value stored to variable is never used}}
+  int abc=1;
+  long idx=abc+3*5; // expected-warning {{value stored to variable is never used}}
 }
 
 void a(void *b) {
@@ -13,9 +13,9 @@ void a(void *b) {
 }
 
 void z() {
-	int r;
-	if ((r = f()) != 0) { // no-warning
-		int y = r; // no-warning
-		printf("the error is: %d\n", y);
-	}
+  int r;
+  if ((r = f()) != 0) { // no-warning
+    int y = r; // no-warning
+    printf("the error is: %d\n", y);
+  }
 }
