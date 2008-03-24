ea249e3aef2fb94ffc5ca35cfc2711f0e1e1c1b1
Devang Patel <dpatel@apple.com>
2008-03-24 16:58:20 +0000

    Remove incorrect comment.
    llvm-svn: 48728

--- llvm/test/CFrontend/2008-01-24-StructAlignAndBitFields.c
+++ llvm/test/CFrontend/2008-01-24-StructAlignAndBitFields.c
@@ -1,6 +1,4 @@
 // RUN: %llvmgcc %s -S -o -
 
-// This struct is not 4 byte aligned becaues bit-field 
-// type does not influence struct alignment.
 struct U { char a; short b; int c:25; char d; } u;
 
