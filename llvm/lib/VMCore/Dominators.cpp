00cab5b9a640746ce1eb3d5c940173095be2e9e2
Chris Lattner <sabre@nondot.org>
2008-04-21 06:19:02 +0000

    don't print dominators every time it is computed with -debug.
    llvm-svn: 50032

--- llvm/lib/VMCore/Dominators.cpp
+++ llvm/lib/VMCore/Dominators.cpp
@@ -14,12 +14,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#define DEBUG_TYPE "domtree"
-
 #include "llvm/Analysis/Dominators.h"
 #include "llvm/Support/CFG.h"
 #include "llvm/Support/Compiler.h"
-#include "llvm/Support/Debug.h"
 #include "llvm/ADT/DepthFirstIterator.h"
 #include "llvm/ADT/SetOperations.h"
 #include "llvm/ADT/SmallPtrSet.h"
@@ -61,7 +58,6 @@ E("domtree", "Dominator Tree Construction", true, true);
 
 bool DominatorTree::runOnFunction(Function &F) {
   DT->recalculate(F);
-  DEBUG(DT->dump());
   return false;
 }
 
