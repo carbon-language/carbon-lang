772d9f0aae9e03f7e190ab012ec9e237353b6a8a
Ted Kremenek <kremenek@apple.com>
2008-04-30 04:40:48 +0000

    Added test case for the static analyzer.
    llvm-svn: 50467

---
+++ clang/test/Analysis/exercise-ps.c
@@ -0,0 +1,10 @@
+// RUN: clang -checker-simple -verify %s
+//
+// Just exercise the analyzer (no assertions).
+
+
+static const char * f1(const char *x, char *y) {
+  while (*x != 0) {
+    *y++ = *x++;
+  }
+}
