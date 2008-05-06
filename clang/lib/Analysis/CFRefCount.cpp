041d02201f02b407db70183f234f072fdb2eb6d7
Ted Kremenek <kremenek@apple.com>
2008-05-06 04:21:38 +0000

    Fix logic error in string processing.
    llvm-svn: 50710

--- clang/lib/Analysis/CFRefCount.cpp
+++ clang/lib/Analysis/CFRefCount.cpp
@@ -636,7 +636,7 @@ RetainSummaryManager::getInstanceMethodSummary(IdentifierInfo* ClsName,
     if (s[0] == '\0')
       break;
   
-    if (s[0]=='\0' || s[0]!='W' || s[1]!='i' || s[2]!='t' || s[3]!='h')
+    if (s[0]!='W' || s[1]!='i' || s[2]!='t' || s[3]!='h')
       break;
     
     return 0;
