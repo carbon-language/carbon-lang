f77daf5e587727241a70345484a1c6c0aab8f31a
Eli Friedman <eli.friedman@gmail.com>
2008-05-20 09:04:19 +0000

    Fix test (it was incorrectly succeeding).
    llvm-svn: 51310

--- clang/test/CodeGen/builtin-stackaddress.c
+++ clang/test/CodeGen/builtin-stackaddress.c
@@ -1,12 +1,5 @@
-// RUN: clang -emit-llvm < %s
-void* a(unsigned x) {
-return __builtin_return_address(0);
-}
-
-void* c(unsigned x) {
-return __builtin_frame_address(0);
-}
-// RUN: clang -emit-llvm < %s
+// RUN: clang -emit-llvm < %s | grep "llvm.returnaddress"
+// RUN: clang -emit-llvm < %s | grep "llvm.frameaddress"
 void* a(unsigned x) {
 return __builtin_return_address(0);
 }
