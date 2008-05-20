503731aa220c45c8f9b26be063558931c8be753d
Eli Friedman <eli.friedman@gmail.com>
2008-05-20 08:27:04 +0000

    Fix typo in test.
    llvm-svn: 51308

--- clang/test/Sema/builtin-stackaddress.c
+++ clang/test/Sema/builtin-stackaddress.c
@@ -7,10 +7,10 @@ void b(unsigned x) {
 return __builtin_return_address(x); // expected-error{{the level argument for a stack address builtin must be constant}}
 }
 
-void* a(unsigned x) {
+void* c(unsigned x) {
 return __builtin_frame_address(0);
 }
 
-void b(unsigned x) {
+void d(unsigned x) {
 return __builtin_frame_address(x); // expected-error{{the level argument for a stack address builtin must be constant}}
 }
