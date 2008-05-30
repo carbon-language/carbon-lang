048fc8db62a334c2635ed70616886867eb278a7c
Nick Lewycky <nicholas@mxc.ca>
2008-05-30 05:02:37 +0000

    Unbreak this test.
    llvm-svn: 51726

--- llvm/test/FrontendC/2006-01-13-Includes.c
+++ llvm/test/FrontendC/2006-01-13-Includes.c
@@ -1,5 +1,5 @@
 // RUN: %llvmgcc %s -g -S -o - | llvm-as | opt -std-compile-opts | \
-// RUN:   llvm-dis | grep {test/CFrontend}
+// RUN:   llvm-dis | grep {test/FrontendC}
 // PR676
 
 #include <stdio.h>
