524fbe8687bddab6decf6ad58ccd790570c8550f
Steve Naroff <snaroff@apple.com>
2008-06-04 21:38:52 +0000

    Update a test and add a FIXME.
    llvm-svn: 51967

--- clang/test/Sema/objc-types-compatible.m
+++ clang/test/Sema/objc-types-compatible.m
@@ -1,4 +1,4 @@
-// RUN: clang -fsyntax-only -verify %s
+// RUN: clang -fsyntax-only -verify -pedantic %s
 typedef signed char BOOL;
 typedef int NSInteger;
 
@@ -14,3 +14,27 @@ extern NSInteger codeAssistantCaseCompareItems(id a, id b, void *context);
 NSInteger codeAssistantCaseCompareItems(id<PBXCompletionItem> a, id<PBXCompletionItem> b, void *context)
 {
 }
+
+#if 0
+FIXME: clang needs to compare each method prototype with its definition (see below).
+
+GCC produces the following correct warnning:
+[snaroff:llvm/tools/clang] snarofflocal% cc -c test/Sema/objc-types-compatible.m 
+test/Sema/objc-types-compatible.m: In function ‘-[TedWantsToVerifyObjCDoesTheRightThing compareThis:withThat:]’:
+test/Sema/objc-types-compatible.m:26: warning: conflicting types for ‘-(id)compareThis:(id <PBXCompletionItem>)a withThat:(id <PBXCompletionItem>)b’
+test/Sema/objc-types-compatible.m:20: warning: previous declaration of ‘-(id)compareThis:(int)a withThat:(id)b’
+#endif
+
+@interface TedWantsToVerifyObjCDoesTheRightThing
+
+- compareThis:(int)a withThat:(id)b;
+
+@end
+
+@implementation TedWantsToVerifyObjCDoesTheRightThing
+
+- compareThis:(id<PBXCompletionItem>)a withThat:(id<PBXCompletionItem>)b {
+  return self;
+}
+
+@end
