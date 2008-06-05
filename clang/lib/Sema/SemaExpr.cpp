a44099f02274dcff2aee9e47e826cc2a2dba3ca6
Steve Naroff <snaroff@apple.com>
2008-06-05 18:14:25 +0000

    super fix submitted by David Chisnall.
    llvm-svn: 52014

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -99,7 +99,7 @@ Sema::ExprResult Sema::ActOnIdentifierExpr(Scope *S, SourceLocation Loc,
                                  static_cast<Expr*>(SelfExpr.Val), true, true);
       }
     }
-    if (SD == 0 && !strncmp(II.getName(), "super", 5)) {
+    if (SD == 0 && !strcmp(II.getName(), "super")) {
       QualType T = Context.getPointerType(Context.getObjCInterfaceType(
                      CurMethodDecl->getClassInterface()));
       return new ObjCSuperRefExpr(T, Loc);
