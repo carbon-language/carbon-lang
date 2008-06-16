674f866b5cd1c0efafb6a605d0a12dda27529ef0
Matthijs Kooijman <matthijs@stdin.nl>
2008-06-16 12:20:05 +0000

    Add comment.
    llvm-svn: 52313

--- llvm/include/llvm/System/Program.h
+++ llvm/include/llvm/System/Program.h
@@ -65,6 +65,9 @@ namespace sys {
           ///< should have a size of at least three. If the pointer in the array
           ///< are not null, then the inferior process's stdin(0), stdout(1),
           ///< and stderr(2) will be redirected to the corresponding Paths.
+          ///< When an empty Path is passed in, the corresponding file
+          ///< descriptor will be disconnected (ie, /dev/null'd) in a portable
+          ///< way.
         unsigned secondsToWait = 0, ///< If non-zero, this specifies the amount
           ///< of time to wait for the child process to exit. If the time
           ///< expires, the child is killed and this call returns. If zero,
