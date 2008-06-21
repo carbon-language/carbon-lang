42bbca11ccaa7e2f5ab1695ce7d311f6c48567a5
Evan Cheng <evan.cheng@apple.com>
2008-06-21 07:26:53 +0000

    Enable PRE.
    llvm-svn: 52574

--- llvm/lib/Transforms/Scalar/GVN.cpp
+++ llvm/lib/Transforms/Scalar/GVN.cpp
@@ -43,7 +43,7 @@ STATISTIC(NumGVNLoad, "Number of loads deleted");
 STATISTIC(NumGVNPRE, "Number of instructions PRE'd");
 
 static cl::opt<bool> EnablePRE("enable-pre",
-                                cl::init(false), cl::Hidden);
+                               cl::init(true), cl::Hidden);
 
 //===----------------------------------------------------------------------===//
 //                         ValueTable Class
