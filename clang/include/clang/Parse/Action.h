f159b71bdf1e4bddb8e5422ac0c043c7abd0c9b2
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2008-06-24 11:23:36 +0000

    Move the namespace action declarations at the "C++ declarations" section.
    llvm-svn: 52673

--- clang/include/clang/Parse/Action.h
+++ clang/include/clang/Parse/Action.h
@@ -133,20 +133,6 @@ public:
     return Group;
   }
 
-  /// ActOnStartNamespaceDef - This is called at the start of a namespace
-  /// definition.
-  virtual DeclTy *ActOnStartNamespaceDef(Scope *S, SourceLocation IdentLoc,
-                                        IdentifierInfo *Ident,
-                                        SourceLocation LBrace) {
-    return 0;
-  }
-
-  /// ActOnFinishNamespaceDef - This callback is called after a namespace is
-  /// exited. Decl is the DeclTy returned by ActOnStartNamespaceDef.
-  virtual void ActOnFinishNamespaceDef(DeclTy *Dcl,SourceLocation RBrace) {
-    return;
-  }
-
   /// ActOnStartOfFunctionDef - This is called at the start of a function
   /// definition, instead of calling ActOnDeclarator.  The Declarator includes
   /// information about formal arguments that are part of this function.
@@ -537,6 +523,21 @@ public:
   }
 
   //===------------------------- C++ Declarations -------------------------===//
+
+  /// ActOnStartNamespaceDef - This is called at the start of a namespace
+  /// definition.
+  virtual DeclTy *ActOnStartNamespaceDef(Scope *S, SourceLocation IdentLoc,
+                                        IdentifierInfo *Ident,
+                                        SourceLocation LBrace) {
+    return 0;
+  }
+
+  /// ActOnFinishNamespaceDef - This callback is called after a namespace is
+  /// exited. Decl is the DeclTy returned by ActOnStartNamespaceDef.
+  virtual void ActOnFinishNamespaceDef(DeclTy *Dcl,SourceLocation RBrace) {
+    return;
+  }
+
   /// ActOnParamDefaultArgument - Parse default argument for function parameter
   virtual void ActOnParamDefaultArgument(DeclTy *param,
                                          SourceLocation EqualLoc,
