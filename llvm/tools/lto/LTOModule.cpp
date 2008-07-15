7688bed4ffcdc12225d23208ed864b4f15aa83a5
Devang Patel <dpatel@apple.com>
2008-07-15 00:00:11 +0000

    LinkOnce definitions have default scope, like weak definitions. Otherwise, the linker may not be able to match LinkOnce definition from one module with an exteranl reference from other module.
    llvm-svn: 53580

--- llvm/tools/lto/LTOModule.cpp
+++ llvm/tools/lto/LTOModule.cpp
@@ -224,7 +224,8 @@ void LTOModule::addDefinedSymbol(GlobalValue* def, Mangler &mangler,
     // set scope part
     if ( def->hasHiddenVisibility() )
         attr |= LTO_SYMBOL_SCOPE_HIDDEN;
-    else if ( def->hasExternalLinkage() || def->hasWeakLinkage() )
+    else if ( def->hasExternalLinkage() || def->hasWeakLinkage() 
+              || def->hasLinkOnceLinkage() )
         attr |= LTO_SYMBOL_SCOPE_DEFAULT;
     else
         attr |= LTO_SYMBOL_SCOPE_INTERNAL;
