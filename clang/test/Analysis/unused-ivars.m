e17c037f4ff8092336bd7b47bb9c694a86accce0
Ted Kremenek <kremenek@apple.com>
2008-07-25 20:30:34 +0000

    Remove '.' in expected warning
    llvm-svn: 54051

--- clang/test/Analysis/unused-ivars.m
+++ clang/test/Analysis/unused-ivars.m
@@ -2,7 +2,7 @@
 
 @interface A
 {
-  @private int x; // expected-warning {{Instance variable 'x' in class 'A' is never used.}}
+  @private int x; // expected-warning {{Instance variable 'x' in class 'A' is never used}}
 }
 @end
 
