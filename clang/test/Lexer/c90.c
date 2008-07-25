31614aeb0a40b09ec2b708fed8dbc463c89131f2
Chris Lattner <sabre@nondot.org>
2008-07-25 18:42:46 +0000

    update this testcase.
    llvm-svn: 54037

--- clang/test/Lexer/c90.c
+++ clang/test/Lexer/c90.c
@@ -1,5 +1,6 @@
-// RUN: clang -std=c90 -fsyntax-only %s -verify
+/* RUN: clang -std=c90 -fsyntax-only %s -verify -pedantic-errors
+ */
 
 enum { cast_hex = (long) (
-      0x0p-1   /* expected-error {{invalid suffix 'p' on integer constant}} */
+      0x0p-1   /* expected-error {{hexadecimal floating constants are a C99 feature}} */
      ) };
