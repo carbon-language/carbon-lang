568bbf73b247cbebf0cb19ddf629610af3a3c86b
Steve Naroff <snaroff@apple.com>
2008-08-05 18:47:23 +0000

    Fix <rdar://problem/6108119> clang ObjC rewriter: if no newline at end of input file, rewriter inserts #define on same line as closing brace
    llvm-svn: 54363

--- clang/Driver/RewriteObjC.cpp
+++ clang/Driver/RewriteObjC.cpp
@@ -3100,7 +3100,7 @@ void RewriteObjC::RewriteImplementations(std::string &Result) {
     RewriteImplementationDecl(CategoryImplementation[i]);
   
   // This is needed for determining instance variable offsets.
-  Result += "#define __OFFSETOFIVAR__(TYPE, MEMBER) ((int) &((TYPE *)0)->MEMBER)\n";   
+  Result += "\n#define __OFFSETOFIVAR__(TYPE, MEMBER) ((int) &((TYPE *)0)->MEMBER)\n";   
   // For each implemented class, write out all its meta data.
   for (int i = 0; i < ClsDefCount; i++)
     RewriteObjCClassMetaData(ClassImplementation[i], Result);
