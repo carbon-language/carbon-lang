eca183bad2b53c5aae0d1393ec3fe796867bf87e
Daniel Dunbar <daniel@zuster.org>
2008-08-05 16:15:29 +0000

    Add test case for PR2001.
    llvm-svn: 54352

---
+++ clang/test/CodeGen/PR2001-bitfield-reload.c
@@ -0,0 +1,17 @@
+// RUN: clang --emit-llvm-bc -o - %s | opt --std-compile-opts | llvm-dis > %t &&
+// RUN: grep "ret i32" %t | count 1 &&
+// RUN: grep "ret i32 1" %t | count 1
+// PR2001
+
+/* Test that the result of the assignment properly uses the value *in
+   the bitfield* as opposed to the RHS. */
+static int foo(int i) {
+  struct {
+    int f0 : 2;
+  } x;
+  return (x.f0 = i);
+}
+
+int bar() {
+  return foo(-5) == -1;
+}
