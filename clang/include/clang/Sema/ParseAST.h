87a7d31a9feead8922960887af8f3c318e2da43c
Ted Kremenek <kremenek@apple.com>
2008-08-07 19:48:19 +0000

    Fix 80 col violation
    llvm-svn: 54487

--- clang/include/clang/Sema/ParseAST.h
+++ clang/include/clang/Sema/ParseAST.h
@@ -21,7 +21,9 @@ namespace clang {
   /// ParseAST - Parse the entire file specified, notifying the ASTConsumer as
   /// the file is parsed.  This takes ownership of the ASTConsumer and
   /// ultimately deletes it.
-  void ParseAST(Preprocessor &pp, ASTConsumer *C, bool PrintStats = false, bool DeleteConsumer = true);
+  void ParseAST(Preprocessor &pp, ASTConsumer *C, bool PrintStats = false,
+                bool DeleteConsumer = true);
+
 }  // end namespace clang
 
 #endif
