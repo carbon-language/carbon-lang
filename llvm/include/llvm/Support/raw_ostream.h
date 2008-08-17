3bed934934ad8c61050153522b983244600d774d
Chris Lattner <sabre@nondot.org>
2008-08-17 01:35:54 +0000

    remove accidental comment.
    llvm-svn: 54856

--- llvm/include/llvm/Support/raw_ostream.h
+++ llvm/include/llvm/Support/raw_ostream.h
@@ -76,7 +76,7 @@ public:
     // Handle short strings specially, memcpy isn't very good at very short
     // strings.
     switch (Size) {
-//    case 4: OutBufCur[3] = Ptr[3]; // FALL THROUGH
+    case 4: OutBufCur[3] = Ptr[3]; // FALL THROUGH
     case 3: OutBufCur[2] = Ptr[2]; // FALL THROUGH
     case 2: OutBufCur[1] = Ptr[1]; // FALL THROUGH
     case 1: OutBufCur[0] = Ptr[0]; // FALL THROUGH
