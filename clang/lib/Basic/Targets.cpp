22361bf422a3d38fddaf88e3312bb9084a21ba37
Chris Lattner <sabre@nondot.org>
2008-08-18 20:05:00 +0000

    Fix PR2627, support for Q constraint.
    llvm-svn: 54943

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -550,7 +550,8 @@ namespace X86 {
     case 'A': // edx:eax.
     case 't': // top of floating point stack.
     case 'u': // second from top of floating point stack.
-    case 'q': // a, b, c, d registers or any integer register in 64-bit.
+    case 'q': // Any register accessible as [r]l: a, b, c, and d.
+    case 'Q': // Any register accessible as [r]h: a, b, c, and d.
     case 'Z': // 32-bit integer constant for use with zero-extending x86_64
               // instructions.
     case 'N': // unsigned 8-bit integer constant for use with in and out
