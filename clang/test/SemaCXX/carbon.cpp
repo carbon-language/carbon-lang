d059d5bba0be3117f1d209086d5f0f9e727f68e2
Anders Carlsson <andersca@mac.com>
2008-08-23 22:21:00 +0000

    Add carbon.cpp and cocoa.mm tests
    llvm-svn: 55262

---
+++ clang/test/SemaCXX/carbon.cpp
@@ -0,0 +1,5 @@
+// RUN: clang %s -fsyntax-only -print-stats
+#ifdef __APPLE__
+#include <Carbon/Carbon.h>
+#endif
+
---
+++ clang/test/SemaObjCXX/cocoa.mm
@@ -0,0 +1,5 @@
+// RUN: clang %s -print-stats
+#ifdef __APPLE__
+#include <Cocoa/Cocoa.h>
+#endif
+
