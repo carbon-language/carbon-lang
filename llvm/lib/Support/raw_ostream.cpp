da42964999ce7385f3511160fd254ea3ddaa27ec
Chris Lattner <sabre@nondot.org>
2008-08-23 20:34:06 +0000

    add #include
    llvm-svn: 55252

--- llvm/lib/Support/raw_ostream.cpp
+++ llvm/lib/Support/raw_ostream.cpp
@@ -12,6 +12,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Config/config.h"
 #include <ostream>
