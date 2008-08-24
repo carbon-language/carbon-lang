d75862508342f8a759785bb523a40963c3ef084a
Chris Lattner <sabre@nondot.org>
2008-08-24 18:33:17 +0000

    add a hack to temporarily allow pseudo source  values.
    llvm-svn: 55289

--- llvm/lib/VMCore/AsmWriter.cpp
+++ llvm/lib/VMCore/AsmWriter.cpp
@@ -1778,7 +1778,8 @@ void Value::print(raw_ostream &OS, AssemblyAnnotationWriter *AAW) const {
   } else if (isa<InlineAsm>(this)) {
     WriteAsOperand(OS, this, true, 0);
   } else {
-    assert(0 && "Unknown value to print out!");
+    // FIXME: PseudoSourceValue breaks this!
+    //assert(0 && "Unknown value to print out!");
   }
 }
 
