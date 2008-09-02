95bffc74da1c9b3e75ac2ca1476740d600360ad2
Steve Naroff <snaroff@apple.com>
2008-09-02 15:20:19 +0000

    Implement block pseudo-storage class modifiers (__block, __byref).
    llvm-svn: 55635

---
+++ clang/test/Parser/block-block-storageclass.c
@@ -0,0 +1,18 @@
+// RUN: clang -fsyntax-only -verify -parse-noop %s
+
+#include <stdio.h>
+void _Block_byref_release(void*src){}
+
+int main() {
+   __block  int X = 1234;
+   __block  const char * message = "HELLO";
+
+   X = X - 1234;
+
+   X += 1;
+
+   printf ("%s(%d)\n", message, X);
+   X -= 1;
+
+   return X;
+}
