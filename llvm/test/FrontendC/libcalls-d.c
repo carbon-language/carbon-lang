462a071c388464a0e69c7a21ada0fc853a4f5806
Dale Johannesen <dalej@apple.com>
2008-09-04 21:24:28 +0000

    Adjust tests to expect new math intrinsics to be emitted only when errno is not in use.
    llvm-svn: 55797

--- llvm/test/FrontendC/libcalls-d.c
+++ llvm/test/FrontendC/libcalls-d.c
@@ -1,6 +1,8 @@
 // llvm-gcc -O1+ should run simplify libcalls, O0 shouldn't
 // and -fno-builtins shouldn't.
-// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | grep {call.*exp2\\.f64}
+// -fno-math-errno should emit an llvm intrinsic, -fmath-errno should not.
+// RUN: %llvmgcc %s -S -fno-math-errno -emit-llvm -O0 -o - | grep {call.*exp2\\.f64}
+// RUN: %llvmgcc %s -S -fmath-errno -emit-llvm -O0 -o - | grep {call.*exp2}
 // RUN: %llvmgcc %s -S -emit-llvm -O1 -o - | grep {call.*ldexp}
 // RUN: %llvmgcc %s -S -emit-llvm -O3 -fno-builtin -o - | grep {call.*exp2}
 
--- llvm/test/FrontendC/libcalls.c
+++ llvm/test/FrontendC/libcalls.c
@@ -1,6 +1,8 @@
 // llvm-gcc -O1+ should run simplify libcalls, O0 shouldn't
 // and -fno-builtins shouldn't.
-// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | grep {call.*exp2\\.f32}
+// -fno-math-errno should emit an llvm intrinsic, -fmath-errno should not.
+// RUN: %llvmgcc %s -S -emit-llvm -fno-math-errno -O0 -o - | grep {call.*exp2\\.f32}
+// RUN: %llvmgcc %s -S -emit-llvm -fmath-errno -O0 -o - | grep {call.*exp2f}
 // RUN: %llvmgcc %s -S -emit-llvm -O1 -o - | grep {call.*ldexp}
 // RUN: %llvmgcc %s -S -emit-llvm -O3 -fno-builtin -o - | grep {call.*exp2f}
 
