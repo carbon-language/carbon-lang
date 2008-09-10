b034bc71d4b75081371dd430a9976e743454eac2
Daniel Dunbar <daniel@zuster.org>
2008-09-10 23:11:23 +0000

    Add XFAIL test case for: <rdar://problem/6211479> [sema] array type invalid for Obj-C property
    llvm-svn: 56075

---
+++ clang/test/SemaObjC/rdr-6211479-array-property.m
@@ -0,0 +1,9 @@
+// RUN: clang -fsyntax-only -verify %s
+// XFAIL
+// <rdar://problem/6211479>
+
+typedef int T[2];
+
+@interface A
+@property(assign) T p2; // expected-error {{FIXME: property has invalid type}}
+@end
