07fe6c8a079217dbd394220dab8c9d7e1028ba3f
Bill Wendling <isanbard@gmail.com>
2008-09-16 06:30:47 +0000

    Remove space that was forgotten.`
    llvm-svn: 56240

--- llvm/test/FrontendC/2006-01-16-BitCountIntrinsicsUnsigned.c
+++ llvm/test/FrontendC/2006-01-16-BitCountIntrinsicsUnsigned.c
@@ -1,4 +1,4 @@
-// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32( i32} | count 2
+// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | count 2
 // RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | count 1
 
 unsigned t2(unsigned X) {
