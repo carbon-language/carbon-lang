3f41b87e8d0ffcff94c458b750d2ad78e3cd3e02
Bill Wendling <isanbard@gmail.com>
2008-09-16 06:40:04 +0000

    Fix test to account for no more whitespace and that one of the matches is the "declare" statement.
    llvm-svn: 56241

--- llvm/test/FrontendC/2006-01-16-BitCountIntrinsicsUnsigned.c
+++ llvm/test/FrontendC/2006-01-16-BitCountIntrinsicsUnsigned.c
@@ -1,5 +1,5 @@
-// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | count 2
-// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | count 1
+// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | count 3
+// RUN: %llvmgcc -S %s -o - | grep {llvm.ctlz.i32(i32} | grep declare | count 1
 
 unsigned t2(unsigned X) {
   return __builtin_clz(X);
