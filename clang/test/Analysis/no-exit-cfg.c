81ec48a1d5b65fd17aff71ee7bf9321f4adabed3
Ted Kremenek <kremenek@apple.com>
2008-09-23 18:05:01 +0000

    Added test case.
    llvm-svn: 56493

---
+++ clang/test/Analysis/no-exit-cfg.c
@@ -0,0 +1,18 @@
+// RUN: clang -checker-cfref -verify %s
+
+// This is a test case for the issue reported in PR 2819:
+//  http://llvm.org/bugs/show_bug.cgi?id=2819
+// The flow-sensitive dataflow solver should work even when no block in
+// the CFG reaches the exit block.
+
+int g(int x);
+void h(int x);
+
+int f(int x)
+{
+out_err:
+  if (g(x)) {
+    h(x);
+  }
+  goto out_err;
+}
