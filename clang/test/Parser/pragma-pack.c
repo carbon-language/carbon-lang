cc498392a2cb9ab400367703089aa4843ccd45e9
Chris Lattner <sabre@nondot.org>
2008-10-04 19:38:15 +0000

    Move the expected-warning lines to a place that clang -verify will pick them up.  Speculatularly hacktastic, but strangely beautiful? Daniel, lines 20/21 are rejected, please investigate.
    llvm-svn: 57067

--- clang/test/Parser/pragma-pack.c
+++ clang/test/Parser/pragma-pack.c
@@ -1,30 +1,33 @@
 // RUN: clang -fsyntax-only -verify %s
 // XFAIL
 
-#pragma pack 10 // expected-warning {{missing '(' after '#pragma pack'}}
+// Note that this puts the expected lines before the directives to work around
+// limitations in the -verify mode.
+
+/* expected-warning {{missing '(' after '#pragma pack'}}*/ #pragma pack 10
 #pragma pack()
 #pragma pack(8)
 
-#pragma pack(hello) // expected-warning {{unknown action for '#pragma pack'}}
+/*expected-warning {{unknown action for '#pragma pack'}}*/ #pragma pack(hello) 
 #pragma pack(push)
 #pragma pack(pop)
 
-#pragma pack(push,) // expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}
-#pragma pack(push,) // expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}
-#pragma pack(pop,) // expected-warning {{malformed '#pragma pack', expected '#pragma pack(pop}}
+/* expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}*/ #pragma pack(push,)
+/* expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}*/ #pragma pack(push,)
+/* expected-warning {{malformed '#pragma pack', expected '#pragma pack(pop}}*/  #pragma pack(pop,) 
 
 #pragma pack(push,i)
-#pragma pack(push,i, // expected-warning {{malformed '#pragma pack', expected}}
-#pragma pack(push,i,) // expected-warning {{malformed '#pragma pack', expected}}
+/* expected-warning {{malformed '#pragma pack', expected}}*/ #pragma pack(push,i, 
+/* expected-warning {{malformed '#pragma pack', expected}}*/ #pragma pack(push,i,) 
 
 #pragma pack(push,8)
-#pragma pack(push,8, // expected-warning {{malformed '#pragma pack', expected}}
-#pragma pack(push,8,help) // expected-warning {{malformed '#pragma pack', expected}}
-#pragma pack(push,8,) // expected-warning {{missing ')' after '#pragma pack'}}
-#pragma pack(push,i,8 // expected-warning {{missing ')' after '#pragma pack'}}
+/* expected-warning {{malformed '#pragma pack', expected}}*/ #pragma pack(push,8, 
+/* expected-warning {{malformed '#pragma pack', expected}}*/ #pragma pack(push,8,help) 
+/* expected-warning {{missing ')' after '#pragma pack'}}*/ #pragma pack(push,8,) 
+/* expected-warning {{missing ')' after '#pragma pack'}}*/ #pragma pack(push,i,8 
 #pragma pack(push,i,8)
 
-#pragma pack(push // expected-warning {{missing ')' after '#pragma pack'}}
+/* expected-warning {{missing ')' after '#pragma pack'}}*/ #pragma pack(push 
 
 _Pragma("pack(push)")
-_Pragma("pack(push,)") // expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}
+/* expected-warning {{malformed '#pragma pack', expected '#pragma pack(push}}*/ _Pragma("pack(push,)") 
