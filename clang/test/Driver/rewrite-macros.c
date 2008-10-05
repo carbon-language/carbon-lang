0af89ff0fba90ebd3058b80c2bb23e32e2ed67c1
Daniel Dunbar <daniel@zuster.org>
2008-10-05 01:39:04 +0000

    Add -rewrite-macros test case.
    llvm-svn: 57094

---
+++ clang/test/Driver/rewrite-macros.c
@@ -0,0 +1,18 @@
+// RUN: clang -verify --rewrite-macros -o %t %s &&
+
+#define A(a,b) a ## b
+
+// RUN: grep '12 */\*A\*/ /\*(1,2)\*/' %t &&
+A(1,2)
+
+// RUN: grep '/\*_Pragma("mark")\*/' %t &&
+_Pragma("mark")
+
+// RUN: grep "//#warning eek" %t &&
+/* expected-warning {{#warning eek}} */ #warning eek
+
+// RUN: grep "//#pragma mark mark" %t &&
+#pragma mark mark
+
+// RUN: true
+
