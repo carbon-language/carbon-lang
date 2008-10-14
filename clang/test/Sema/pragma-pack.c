eee51d125f68c5e13ed748b57063798aaf10a4df
Daniel Dunbar <daniel@zuster.org>
2008-10-14 16:46:03 +0000

    Fix typo in testcase.
    llvm-svn: 57499

--- clang/test/Sema/pragma-pack.c
+++ clang/test/Sema/pragma-pack.c
@@ -1,4 +1,4 @@
-// RUN: clang -m -triple i686-apple-darwin9 -fsyntax-only -verify %s
+// RUN: clang -triple i686-apple-darwin9 -fsyntax-only -verify %s
 
 /* expected-warning {{value of #pragma pack(show) == 8}} */ #pragma pack(show)
 /* expected-warning {{expected #pragma pack parameter to be}} */ #pragma pack(3)
