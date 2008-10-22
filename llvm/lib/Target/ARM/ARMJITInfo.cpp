1ecf1fd5c80ef44c1bc64cb32d70e879d5a3fb63
Jim Grosbach <grosbach@apple.com>
2008-10-22 22:27:51 +0000

    remove extraneous #ifdef's
    llvm-svn: 58006

--- llvm/lib/Target/ARM/ARMJITInfo.cpp
+++ llvm/lib/Target/ARM/ARMJITInfo.cpp
@@ -110,24 +110,20 @@ extern "C" void ARMCompilationCallbackC(intptr_t StubAddr) {
   // stub with:
   //   ldr pc, [pc,#-4]
   //   <addr>
-#if defined(__APPLE__)
   bool ok = sys::Memory::setRangeWritable ((void*)StubAddr, 8);
   if (!ok)
     {
       cerr << "ERROR: Unable to mark stub writable\n";
       abort();
     }
-#endif
   *(intptr_t *)StubAddr = 0xe51ff004;
   *(intptr_t *)(StubAddr+4) = NewVal;
-#if defined(__APPLE__)
   ok = sys::Memory::setRangeExecutable ((void*)StubAddr, 8);
   if (!ok)
     {
       cerr << "ERROR: Unable to mark stub executable\n";
       abort();
     }
-#endif
 }
 
 TargetJITInfo::LazyResolverFn
