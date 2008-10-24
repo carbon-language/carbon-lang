5043549666ca930ab84263130c105d9ebf0748fb
Daniel Dunbar <daniel@zuster.org>
2008-10-24 08:42:52 +0000

    Use #define trickery to de-XFAIL test/Coverage/codegen-gnu.m while still getting coverage for non-IRgen cases.
    llvm-svn: 58084

--- clang/test/Coverage/codegen-gnu.m
+++ clang/test/Coverage/codegen-gnu.m
@@ -1,5 +1,7 @@
-// RUN: clang -fgnu-runtime -emit-llvm -o %t %s &&
-// RUN: clang -g -fgnu-runtime -emit-llvm -o %t %s
-// XFAIL
+// RUN: clang -DIRGENABLE_GNU -DIRGENABLE -fgnu-runtime -emit-llvm -o %t %s && 
+// RUN: clang -DIRGENABLE_GNU -DIRGENABLE -g -fgnu-runtime -emit-llvm -o %t %s &&
+
+// FIXME: Remove once GNU can IRgen everything.
+// RUN: ! clang -fgnu-runtime -emit-llvm -o %t %s
 
 #include "objc-language-features.inc"
