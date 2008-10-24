15fadeb9cfaf77dfe31bef1bd8c19b29ada77e4c
Daniel Dunbar <daniel@zuster.org>
2008-10-24 08:39:46 +0000

    Use #define trickery to de-XFAIL test/Coverage/codegen-next.m while still getting coverage for non-IRgen cases.
    llvm-svn: 58082

--- clang/test/Coverage/codegen-next.m
+++ clang/test/Coverage/codegen-next.m
@@ -1,5 +1,7 @@
-// RUN: clang -fnext-runtime -emit-llvm -o %t %s &&
-// RUN: clang -g -fnext-runtime -emit-llvm -o %t %s
-// XFAIL
+// RUN: clang -DIRGENABLE -fnext-runtime -emit-llvm -o %t %s &&
+// RUN: clang -DIRGENABLE -g -fnext-runtime -emit-llvm -o %t %s &&
+
+// FIXME: Remove IRGENABLE when possible.
+// RUN: ! clang -fnext-runtime -emit-llvm -o %t %s
 
 #include "objc-language-features.inc"
