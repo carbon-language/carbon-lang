962c20e6f38e9717d72b832a5ab3f10d5626ab6c
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2008-10-24 22:28:18 +0000

    Add a quote from the standard about the type of 'this'.
    llvm-svn: 58124

--- clang/lib/AST/DeclCXX.cpp
+++ clang/lib/AST/DeclCXX.cpp
@@ -61,6 +61,12 @@ CXXMethodDecl::Create(ASTContext &C, CXXRecordDecl *RD,
 }
 
 QualType CXXMethodDecl::getThisType(ASTContext &C) const {
+  // C++ 9.3.2p1: The type of this in a member function of a class X is X*.
+  // If the member function is declared const, the type of this is const X*,
+  // if the member function is declared volatile, the type of this is
+  // volatile X*, and if the member function is declared const volatile,
+  // the type of this is const volatile X*.
+
   assert(isInstance() && "No 'this' for static methods!");
   QualType ClassTy = C.getTagDeclType(const_cast<CXXRecordDecl*>(
                                             cast<CXXRecordDecl>(getParent())));
