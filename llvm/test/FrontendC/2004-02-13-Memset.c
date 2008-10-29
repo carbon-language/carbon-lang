f33c3bc51fcfbd2f4663d732fcc157accf98b5ef
Duncan Sands <baldrick@free.fr>
2008-10-29 15:57:37 +0000

    Make the declaration of bzero match size_t on x86-64-linux.
    llvm-svn: 58383

--- llvm/test/FrontendC/2004-02-13-Memset.c
+++ llvm/test/FrontendC/2004-02-13-Memset.c
@@ -1,7 +1,7 @@
 // RUN: %llvmgcc -xc %s -c -o - | llvm-dis | grep llvm.memset | count 3
 
 void *memset(void*, int, long);
-void bzero(void*, int);
+void bzero(void*, long);
 
 void test(int* X, char *Y) {
   memset(X, 4, 1000);
