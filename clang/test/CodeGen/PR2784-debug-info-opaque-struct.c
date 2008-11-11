bd178d51ff135ff8d4b6d164533f0c33deb2e41d
Chris Lattner <sabre@nondot.org>
2008-11-11 06:42:53 +0000

    merge some testcases together.
    llvm-svn: 59031

--- clang/test/CodeGen/PR2784-debug-info-opaque-struct.c
+++
@@ -1,6 +0,0 @@
-// RUN: clang -g -emit-llvm -o %t %s
-// PR2784
-
-struct OPAQUE;
-typedef struct OPAQUE *PTR;
-PTR p;
--- clang/test/CodeGen/debug-info.c
+++ clang/test/CodeGen/debug-info.c
@@ -4,3 +4,19 @@
 void convert(void) {
   struct { typeof(0) f0; } v0;
 }
+
+// PR2784
+struct OPAQUE;
+typedef struct OPAQUE *PTR;
+PTR p;
+
+
+// PR2950
+struct s0;
+struct s0 { struct s0 *p; } g0;
+
+struct s0 *f0(struct s0 *a0) {
+  return a0->p;
+}
+  
+  
