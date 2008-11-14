070114fd793ee3c42868d1cbd56944b76fa16b26
Oscar Fuentes <ofv@wanadoo.es>
2008-11-14 22:21:02 +0000

    CMake: Use LLVM_MAIN_SRC_DIR instead of CMAKE_SOURCE_DIR, for consistency.
    Patch by Kevin Andre!
    
    llvm-svn: 59326

--- llvm/cmake/modules/TableGen.cmake
+++ llvm/cmake/modules/TableGen.cmake
@@ -5,7 +5,7 @@
 macro(tablegen ofn)
   file(GLOB all_tds "*.td")
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}
-    COMMAND ${LLVM_TABLEGEN} ${ARGN} -I ${CMAKE_CURRENT_SOURCE_DIR} -I ${CMAKE_SOURCE_DIR}/lib/Target -I ${LLVM_MAIN_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/${LLVM_TARGET_DEFINITIONS} -o ${ofn}
+    COMMAND ${LLVM_TABLEGEN} ${ARGN} -I ${CMAKE_CURRENT_SOURCE_DIR} -I ${LLVM_MAIN_SRC_DIR}/lib/Target -I ${LLVM_MAIN_INCLUDE_DIR} ${CMAKE_CURRENT_SOURCE_DIR}/${LLVM_TARGET_DEFINITIONS} -o ${ofn}
     DEPENDS tblgen ${all_tds}
     COMMENT "Building ${ofn}..."
     )
