06b377668dc6340e6ed654b72f81a2301981bac1
Zhongxing Xu <xuzhongxing@gmail.com>
2008-11-20 00:46:15 +0000

    Split region store specific test cases.
    llvm-svn: 59683

---
+++ clang/test/Analysis/uninit-vals-ps-region.c
@@ -0,0 +1,17 @@
+// RUN: clang -checker-simple -analyzer-store-region -verify %s
+
+struct s {
+  int data;
+};
+
+struct s global;
+
+void g(int);
+
+void f4() {
+  int a;
+  if (global.data == 0)
+    a = 3;
+  if (global.data == 0)
+    g(a); // no-warning
+}
