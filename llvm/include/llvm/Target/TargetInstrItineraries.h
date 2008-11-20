4b17f240478e75c0dc470e85ece85cc78cfdd2b8
Dan Gohman <gohman@apple.com>
2008-11-20 22:09:52 +0000

    Doxygenate comments.
    llvm-svn: 59753

--- llvm/include/llvm/Target/TargetInstrItineraries.h
+++ llvm/include/llvm/Target/TargetInstrItineraries.h
@@ -19,60 +19,56 @@
 namespace llvm {
 
 //===----------------------------------------------------------------------===//
-// Instruction stage - These values represent a step in the execution of an
-// instruction.  The latency represents the number of discrete time slots used
-// need to complete the stage.  Units represent the choice of functional units
-// that can be used to complete the stage.  Eg. IntUnit1, IntUnit2.
-//
+/// Instruction stage - These values represent a step in the execution of an
+/// instruction.  The latency represents the number of discrete time slots used
+/// need to complete the stage.  Units represent the choice of functional units
+/// that can be used to complete the stage.  Eg. IntUnit1, IntUnit2.
+///
 struct InstrStage {
-  unsigned Cycles;  // Length of stage in machine cycles
-  unsigned Units;   // Choice of functional units
+  unsigned Cycles;  ///< Length of stage in machine cycles
+  unsigned Units;   ///< Choice of functional units
 };
 
 
 //===----------------------------------------------------------------------===//
-// Instruction itinerary - An itinerary represents a sequential series of steps
-// required to complete an instruction.  Itineraries are represented as
-// sequences of instruction stages.
-//
+/// Instruction itinerary - An itinerary represents a sequential series of steps
+/// required to complete an instruction.  Itineraries are represented as
+/// sequences of instruction stages.
+///
 struct InstrItinerary {
-  unsigned First;    // Index of first stage in itinerary
-  unsigned Last;     // Index of last + 1 stage in itinerary
+  unsigned First;    ///< Index of first stage in itinerary
+  unsigned Last;     ///< Index of last + 1 stage in itinerary
 };
 
 
 
 //===----------------------------------------------------------------------===//
-// Instruction itinerary Data - Itinerary data supplied by a subtarget to be
-// used by a target.
-//
+/// Instruction itinerary Data - Itinerary data supplied by a subtarget to be
+/// used by a target.
+///
 struct InstrItineraryData {
-  const InstrStage     *Stages;         // Array of stages selected
-  const InstrItinerary *Itineratries;   // Array of itineraries selected
+  const InstrStage     *Stages;         ///< Array of stages selected
+  const InstrItinerary *Itineratries;   ///< Array of itineraries selected
 
-//
-// Ctors.
-//
+  /// Ctors.
+  ///
   InstrItineraryData() : Stages(0), Itineratries(0) {}
   InstrItineraryData(const InstrStage *S, const InstrItinerary *I)
     : Stages(S), Itineratries(I) {}
   
-  //
-  // isEmpty - Returns true if there are no itineraries.
-  //
+  /// isEmpty - Returns true if there are no itineraries.
+  ///
   inline bool isEmpty() const { return Itineratries == 0; }
   
-  //
-  // begin - Return the first stage of the itinerary.
-  // 
+  /// begin - Return the first stage of the itinerary.
+  /// 
   inline const InstrStage *begin(unsigned ItinClassIndx) const {
     unsigned StageIdx = Itineratries[ItinClassIndx].First;
     return Stages + StageIdx;
   }
 
-  //
-  // end - Return the last+1 stage of the itinerary.
-  // 
+  /// end - Return the last+1 stage of the itinerary.
+  /// 
   inline const InstrStage *end(unsigned ItinClassIndx) const {
     unsigned StageIdx = Itineratries[ItinClassIndx].Last;
     return Stages + StageIdx;
