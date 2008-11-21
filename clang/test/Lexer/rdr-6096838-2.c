1759ea400e6c3d2c369013edf29456e00882d315
Chris Lattner <sabre@nondot.org>
2008-11-21 01:04:13 +0000

    split into two tests.
    llvm-svn: 59770

---
+++ clang/test/Lexer/rdr-6096838-2.c
@@ -0,0 +1,5 @@
+/* RUN: clang -pedantic -std=gnu89 -fsyntax-only -verify %s
+ rdar://6096838
+ */
+
+long double d = 0x0.0000003ffffffff00000p-16357L; /* expected-warning {{ hexadecimal floating constants are a C99 feature }} */
