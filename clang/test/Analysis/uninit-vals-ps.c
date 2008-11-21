c7792048ff05fcf82c44d3da7c511de3d219608f
Ted Kremenek <kremenek@apple.com>
2008-11-21 00:15:15 +0000

    Enable test file for 'region store' in addition to basic store.
    llvm-svn: 59762

--- clang/test/Analysis/uninit-vals-ps.c
+++ clang/test/Analysis/uninit-vals-ps.c
@@ -1,4 +1,5 @@
-// RUN: clang -checker-simple -verify %s
+// RUN: clang -checker-simple -verify %s &&
+// RUN: clang -checker-simple -analyzer-store-region -verify %s
 
 struct FPRec {
   void (*my_func)(int * x);  
