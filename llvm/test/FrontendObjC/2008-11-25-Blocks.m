3d14916b3e81d78ae02989904e8780d9a75da05a
Bill Wendling <isanbard@gmail.com>
2008-11-26 02:21:12 +0000

    Add test for rdar://6394879.
    llvm-svn: 60079

---
+++ llvm/test/FrontendObjC/2008-11-25-Blocks.m
@@ -0,0 +1,17 @@
+// RUN: %llvmgcc -c %s -o /dev/null
+// rdar://6394879
+
+@interface bork
+- (id)B:(void (^)())blk;
+- (void)C;
+@end
+@implementation bork
+- (id)B:(void (^)())blk {
+  __attribute__((__blocks__(byref))) bork* new = ((void *)0);
+  blk();
+}
+- (void)C {
+  __attribute__((__blocks__(byref))) id var;
+  [self B:^() {}];
+}
+@end
