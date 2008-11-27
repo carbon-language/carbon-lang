2d311e835470bdaa0395564b39a18b3b1cfcee20
Zhongxing Xu <xuzhongxing@gmail.com>
2008-11-27 02:19:10 +0000

    Add license comments.
    llvm-svn: 60144

--- clang/include/clang/Driver/ManagerRegistry.h
+++ clang/include/clang/Driver/ManagerRegistry.h
@@ -1,3 +1,16 @@
+//===-- ManagerRegistry.h - Pluggable analyzer module registry --*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file defines the ManagerRegistry and Register* classes.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef LLVM_CLANG_MANAGER_REGISTRY_H
 #define LLVM_CLANG_MANAGER_REGISTRY_H
 
