0309d904f79c42bed4abf8e4820a6c12d1b55cdb
Anders Carlsson <andersca@mac.com>
2008-12-01 06:27:38 +0000

    Fix test. (0 && (a(),1)) is a valid I-C-E according to C99.
    llvm-svn: 60331

--- clang/test/Sema/i-c-e3.c
+++ clang/test/Sema/i-c-e3.c
@@ -1,3 +1,3 @@
-// RUN: clang %s -fsyntax-only -verify -pedantic-errors
+// RUN: clang %s -fsyntax-only -verify
 
-int a() {int p; *(1 ? &p : (void*)(0 && (a(),1))) = 10;} // expected-error {{null pointer expression is not an integer constant expression (but is allowed as an extension)}} // expected-note{{C does not permit evaluated commas in an integer constant expression}}
+int a() {int p; *(1 ? &p : (void*)(0 && (a(),1))) = 10;}
