4a3f1efd25fc1dfeda21d51ce31aca2e06c68e96
Ted Kremenek <kremenek@apple.com>
2008-12-04 02:07:20 +0000

    Revise bogus comment I just committed.
    llvm-svn: 60522

--- clang/test/Analysis/uninit-vals-ps-region.c
+++ clang/test/Analysis/uninit-vals-ps-region.c
@@ -12,6 +12,6 @@ void f4() {
   int a;
   if (global.data == 0)
     a = 3;
-  if (global.data == 0) // The true branch is infeasible.
+  if (global.data == 0) // When the true branch is feasible 'a = 3'.
     g(a); // no-warning
 }
