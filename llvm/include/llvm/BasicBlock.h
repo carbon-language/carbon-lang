c3fbda4d1931a4ffac96e7efe86ca48f700d3576
Chris Lattner <sabre@nondot.org>
2008-12-09 02:10:10 +0000

    remove two unneeded forward declarations, pointed out by Gabor.
    llvm-svn: 60735

--- llvm/include/llvm/BasicBlock.h
+++ llvm/include/llvm/BasicBlock.h
@@ -22,8 +22,6 @@
 namespace llvm {
 
 class TerminatorInst;
-template <class Term, class BB> class SuccIterator;  // Successor Iterator
-template <class Ptr, class USE_iterator> class PredIterator;
 
 template<> struct ilist_traits<Instruction>
   : public SymbolTableListTraits<Instruction, BasicBlock> {
