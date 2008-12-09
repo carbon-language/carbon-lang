2a46a8ef5234e867a08a7ad19b70570f23f3b4da
Zhongxing Xu <xuzhongxing@gmail.com>
2008-12-09 12:26:46 +0000

    initialize member variable.
    llvm-svn: 60759

--- clang/include/clang/Analysis/PathSensitive/MemRegion.h
+++ clang/include/clang/Analysis/PathSensitive/MemRegion.h
@@ -454,7 +454,7 @@ class MemRegionManager {
 
 public:
   MemRegionManager(llvm::BumpPtrAllocator& a)
-  : A(a), globals(0), stack(0), heap(0) {}
+    : A(a), globals(0), stack(0), heap(0), unknown(0) {}
   
   ~MemRegionManager() {}
   
