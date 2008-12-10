591b2224cb90b329fc7346fc2ab6de7b9d97191c
Anders Carlsson <andersca@mac.com>
2008-12-10 23:57:51 +0000

    Add another complex promotion test.
    llvm-svn: 60863

--- clang/test/Sema/complex-promotion.c
+++ clang/test/Sema/complex-promotion.c
@@ -9,3 +9,7 @@ double d;
 __typeof__ (d + 1i) e;
 
 int f[sizeof(e) == 2 * sizeof(double) ? 1 : -1];
+
+int g;
+int h[__builtin_classify_type(g + 1.0i) == 9 ? 1 : -1];
+int i[__builtin_classify_type(1.0i + a) == 9 ? 1 : -1];
