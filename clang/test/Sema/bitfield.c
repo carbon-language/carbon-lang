ece9ae718b24f7de48819fa428fd517c49c06831
Chris Lattner <sabre@nondot.org>
2008-12-11 23:11:52 +0000

    add annotation
    llvm-svn: 60907

--- clang/test/Sema/bitfield.c
+++ clang/test/Sema/bitfield.c
@@ -2,7 +2,10 @@
 
 struct a {
   int a : -1; // expected-error{{bit-field 'a' has negative width}}
+
+  // rdar://6081627
   int b : 33; // expected-error{{size of bit-field 'b' exceeds size of its type (32 bits)}}
+
   int c : (1 + 0.25); // expected-error{{expression is not an integer constant expression}}
   int d : (int)(1 + 0.25); 
 };
