5b3ddc8ef9b945e99c415b8cb52f8d967d212f1e
Chris Lattner <sabre@nondot.org>
2008-12-12 06:21:18 +0000

    merge recovery-1 into recovery-3.
    llvm-svn: 60930

--- clang/test/Parser/recovery-3.c
+++ clang/test/Parser/recovery-3.c
@@ -44,3 +44,18 @@ int test(int) {
     ;
 }
 
+
+
+char ((((                       /* expected-note {{to match this '('}} */
+         *X x ] ))));                    /* expected-error {{expected ')'}} */
+
+;   // expected-warning {{ISO C does not allow an extra ';' outside of a function}}
+
+
+
+
+struct S { void *X, *Y; };
+
+struct S A = {
+&BADIDENT, 0     /* expected-error {{use of undeclared identifier}} */
+};
