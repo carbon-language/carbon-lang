a7e0e61b9d454d7ea379d351bf0345306a324f07
Anders Carlsson <andersca@mac.com>
2008-12-26 22:49:10 +0000

    Eli noticed that I had forgotten some #defines. Add them.
    llvm-svn: 61445

--- clang/lib/Headers/float.h
+++ clang/lib/Headers/float.h
@@ -36,6 +36,10 @@
 
 #define DECIMAL_DIG __DECIMAL_DIG__
 
+#define FLT_DIG __FLT_DIG__
+#define DBL_DIG __DBL_DIG__
+#define LDBL_DIG __LDBL_DIG__
+
 #define FLT_MIN_EXP __FLT_MIN_EXP__
 #define DBL_MIN_EXP __DBL_MIN_EXP__
 #define LDBL_MIN_EXP __LDBL_MIN_EXP__
@@ -43,6 +47,14 @@
 #define FLT_MIN_10_EXP __FLT_MIN_10_EXP__
 #define DBL_MIN_10_EXP __DBL_MIN_10_EXP__
 #define LDBL_MIN_10_EXP __LDBL_MIN_10_EXP__
+
+#define FLT_MAX_EXP __FLT_MAX_EXP__
+#define DBL_MAX_EXP __DBL_MAX_EXP__
+#define LDBL_MAX_EXP __LDBL_MAX_EXP__
+
+#define FLT_MAX_10_EXP __FLT_MAX_10_EXP__
+#define DBL_MAX_10_EXP __DBL_MAX_10_EXP__
+#define LDBL_MAX_10_EXP __LDBL_MAX_10_EXP__
 
 #define FLT_MAX __FLT_MAX__
 #define DBL_MAX __DBL_MAX__
