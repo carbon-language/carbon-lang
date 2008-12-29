b13c83ac18f8d645bd7a2f281152dae0282cecd9
Bill Wendling <isanbard@gmail.com>
2008-12-29 21:51:42 +0000

    The FDE initial location and address range  data should be free to be 64-bit (quad) on a 64-bit platform. This fixes a problem with EH frames on Darwin.
    llvm-svn: 61483

--- llvm/lib/CodeGen/AsmPrinter/DwarfWriter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfWriter.cpp
@@ -3119,10 +3119,10 @@ private:
 
       Asm->EOL("FDE CIE offset");
 
-      EmitReference("eh_func_begin", EHFrameInfo.Number, true, true);
+      EmitReference("eh_func_begin", EHFrameInfo.Number, true);
       Asm->EOL("FDE initial location");
       EmitDifference("eh_func_end", EHFrameInfo.Number,
-                     "eh_func_begin", EHFrameInfo.Number, true);
+                     "eh_func_begin", EHFrameInfo.Number);
       Asm->EOL("FDE address range");
 
       // If there is a personality and landing pads then point to the language
