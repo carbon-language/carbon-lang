339153d3be566736ba431bda236c07b4f77f69c0
Ted Kremenek <kremenek@apple.com>
2009-01-09 00:41:48 +0000

    Re-enable PTH testing for Cocoa.h and Carbon.h (and include testing for Objective-C++).
    llvm-svn: 61965

--- clang/test/Sema/carbon.c
+++ clang/test/Sema/carbon.c
@@ -1,4 +1,5 @@
-// RUN: clang %s -fsyntax-only -print-stats
+// RUN: clang %s -fsyntax-only -print-stats &&
+// RUN: clang -x c-header -o %t %s && clang -token-cache %t %s
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
 #endif
--- clang/test/SemaObjCXX/cocoa.mm
+++ clang/test/SemaObjCXX/cocoa.mm
@@ -1,5 +1,5 @@
-// RUN: clang %s -print-stats
+// RUN: clang %s -print-stats &&
+// RUN: clang -x objective-c++-header -o %t %s && clang -token-cache %t %s
 #ifdef __APPLE__
 #include <Cocoa/Cocoa.h>
 #endif
-
