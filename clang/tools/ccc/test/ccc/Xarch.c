d064502da3857a2de3c442d29c233cba08db4256
Daniel Dunbar <daniel@zuster.org>
2009-01-13 21:11:28 +0000

    ccc: Fix a test case.
    llvm-svn: 62183

--- clang/tools/ccc/test/ccc/Xarch.c
+++ clang/tools/ccc/test/ccc/Xarch.c
@@ -1,8 +1,8 @@
 // RUN: xcc -### -fsyntax-only -Xarch_i386 -Wall -Xarch_ppc -Wunused -arch i386 -arch ppc %s &> %t &&
 // RUN: grep '"-Xarch"' %t | count 0 &&
 // RUN: grep '"-Wall"' %t | count 1 &&
-// RUN: grep 'i686-apple' %t | grep '"-m32"' | count 1 &&
-// RUN: grep '"-Wall"' %t | grep 'i686-apple' | grep '"-m32"' | count 1 &&
+// RUN: grep 'i686-apple' %t | grep -v '"-m64"' | count 1 &&
+// RUN: grep '"-Wall"' %t | grep 'i686-apple' | grep -v '"-m64"' | count 1 &&
 // RUN: grep '"-Wunused"' %t | count 1 &&
 // RUN: grep '"-arch" "ppc"' %t | count 1 &&
 // RUN: grep '"-Wunused"' %t | grep '"-arch" "ppc"' | count 1
