cba844dae66f4dbc26ea8b988df1bfb4be1ee320
Douglas Gregor <dgregor@apple.com>
2009-01-14 18:02:48 +0000

    Test explicit constructor
    llvm-svn: 62237

--- clang/test/SemaCXX/converting-constructor.cpp
+++ clang/test/SemaCXX/converting-constructor.cpp
@@ -21,3 +21,20 @@ void g(short s, Y y, Z z) {
   f(z); // expected-error{{incompatible type passing 'class Z', expected 'class X'}}
 }
 
+
+class FromShort {
+public:
+  FromShort(short s);
+};
+
+class FromShortExplicitly {
+public:
+  explicit FromShortExplicitly(short s);
+};
+
+void explicit_constructor(short s) {
+  FromShort fs1(s);
+  FromShort fs2 = s;
+  FromShortExplicitly fse1(s);
+  FromShortExplicitly fse2 = s; // expected-error{{error: cannot initialize 'fse2' with an lvalue of type 'short'}}
+}
