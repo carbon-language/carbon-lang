8973cbfb210d66aa9ec97d7f4cd5076c4e8759c4
Ted Kremenek <kremenek@apple.com>
2009-01-21 07:10:01 +0000

    This test now passes using -analyzer-store-region.
    llvm-svn: 62669

--- clang/test/Analysis/CFNumber.c
+++ clang/test/Analysis/CFNumber.c
@@ -1,4 +1,5 @@
-// RUN: clang -analyze -checker-cfref -verify -triple x86_64-apple-darwin9 %s
+// RUN: clang -analyze -checker-cfref -analyzer-store-basic -verify -triple x86_64-apple-darwin9 %s &&
+// RUN: clang -analyze -checker-cfref -analyzer-store-region -verify -triple x86_64-apple-darwin9 %s
 
 typedef signed long CFIndex;
 typedef const struct __CFAllocator * CFAllocatorRef;
