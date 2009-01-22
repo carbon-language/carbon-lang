e3a26635fbf1c826d19f96b039e8aa08f185ca73
Duncan Sands <baldrick@free.fr>
2009-01-22 15:37:29 +0000

    Remove no-longer relevant comment.  Pointed out by Gabor.
    llvm-svn: 62765

--- llvm/test/FrontendC++/2006-11-06-StackTrace.cpp
+++ llvm/test/FrontendC++/2006-11-06-StackTrace.cpp
@@ -10,7 +10,6 @@
 // RUN:   grep {#7  0x.* in main.*(argc=\[12\],.*argv=.*)}
 
 // Only works on ppc, x86 and x86_64.  Should generalize?
-// FIXME: Un-XFAIL this test for Linux when debug stuff is working again.
 // XFAIL: alpha|ia64|arm
 
 #include <stdlib.h>
