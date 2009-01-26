a7bc8473a3837fc7552452973c191721d8eb5d5a
Sebastian Redl <sebastian.redl@getdesigned.at>
2009-01-26 19:54:48 +0000

    Remove an implemented FIXME and extend test cases. Follow-up on Doug's review.
    llvm-svn: 63032

--- clang/test/SemaCXX/member-pointer.cpp
+++ clang/test/SemaCXX/member-pointer.cpp
@@ -4,6 +4,9 @@ struct A {};
 enum B { Dummy };
 namespace C {}
 struct D : A {};
+struct E : A {};
+struct F : D, E {};
+struct G : virtual D {};
 
 int A::*pdi1;
 int (::A::*pdi2);
@@ -29,4 +32,8 @@ void f() {
   // Conversion to member of derived.
   int D::*pdid = pdi1;
   pdid = pdi2;
+
+  // Fail conversion due to ambiguity and virtuality.
+  int F::*pdif = pdi1; // expected-error {{ambiguous conversion from pointer to member of base class 'struct A' to pointer to member of derived class 'struct F'}} expected-error {{incompatible type}}
+  int G::*pdig = pdi1; // expected-error {{conversion from pointer to member of class 'struct A' to pointer to member of class 'struct G' via virtual base 'struct D' is not allowed}} expected-error {{incompatible type}}
 }
