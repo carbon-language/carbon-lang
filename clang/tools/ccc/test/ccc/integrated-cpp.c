ad0d48d3722d8e0dac3d4b630bdbd48d363f3d26
Daniel Dunbar <daniel@zuster.org>
2009-01-28 19:30:43 +0000

    Fix test case (for -### printing version)
    llvm-svn: 63223

--- clang/tools/ccc/test/ccc/integrated-cpp.c
+++ clang/tools/ccc/test/ccc/integrated-cpp.c
@@ -1,3 +1,3 @@
-// RUN: xcc -fsyntax-only -### %s 2>&1 | count 1 &&
-// RUN: xcc -fsyntax-only -### %s -no-integrated-cpp 2>&1 | count 2 &&
-// RUN: xcc -fsyntax-only -### %s -save-temps 2>&1 | count 2
+// RUN: xcc -fsyntax-only -### %s 2>&1 | count 2 &&
+// RUN: xcc -fsyntax-only -### %s -no-integrated-cpp 2>&1 | count 3 &&
+// RUN: xcc -fsyntax-only -### %s -save-temps 2>&1 | count 3
