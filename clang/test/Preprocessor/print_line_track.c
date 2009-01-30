be70ced6a6965467828857fc889f1c6e7506f3b9
Chris Lattner <sabre@nondot.org>
2009-01-30 18:49:16 +0000

    add testcase for PR3437 and r63405
    llvm-svn: 63407

--- clang/test/Preprocessor/print_line_track.c
+++ clang/test/Preprocessor/print_line_track.c
@@ -1,8 +1,10 @@
 /* RUN: clang -E %s | grep 'a 3' &&
- * RUN: clang -E %s | grep 'b 14' &&
+ * RUN: clang -E %s | grep 'b 16' &&
  * RUN: clang -E -P %s | grep 'a 3' &&
- * RUN: clang -E -P %s | grep 'b 14'
+ * RUN: clang -E -P %s | grep 'b 16' &&
+ * RUN: clang -E %s | not grep '# 0 '
  * PR1848
+ * PR3437
 */
 
 #define t(x) x
