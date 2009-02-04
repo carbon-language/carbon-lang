aa64e6807d17b9624d9418fbf88cf1c661d5a5c6
Daniel Dunbar <daniel@zuster.org>
2009-02-04 00:58:28 +0000

    Force triple for this test case.
    llvm-svn: 63699

--- clang/test/Coverage/codegen-gnu.m
+++ clang/test/Coverage/codegen-gnu.m
@@ -1,7 +1,7 @@
-// RUN: clang -DIRGENABLE_GNU -DIRGENABLE -fgnu-runtime -emit-llvm -o %t %s && 
-// RUN: clang -DIRGENABLE_GNU -DIRGENABLE -g -fgnu-runtime -emit-llvm -o %t %s &&
+// RUN: clang -triple i386-unknown-unknown -DIRGENABLE_GNU -DIRGENABLE -fgnu-runtime -emit-llvm -o %t %s && 
+// RUN: clang -triple i386-unknown-unknown -DIRGENABLE_GNU -DIRGENABLE -g -fgnu-runtime -emit-llvm -o %t %s &&
 
 // FIXME: Remove once GNU can IRgen everything.
-// RUN: ! clang -fgnu-runtime -emit-llvm -o %t %s
+// RUN: ! clang -triple i386-unknown-unknown -fgnu-runtime -emit-llvm -o %t %s
 
 #include "objc-language-features.inc"
