400c040e01a5b946f50ec6e6395b14ed9a225477
Daniel Dunbar <daniel@zuster.org>
2009-02-04 23:40:00 +0000

    Chase LLVM TOT in circles (i.e., revert r63773 to match revert of r63765).
    llvm-svn: 63795

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -333,9 +333,12 @@ RValue CodeGenFunction::EmitBuiltinExpr(unsigned BuiltinID, const CallExpr *E) {
                         E->arg_end());
   
   // See if we have a target specific intrinsic.
-  const char *Name = getContext().BuiltinInfo.GetName(BuiltinID);
-  Intrinsic::ID IntrinsicID = 
-    Intrinsic::getIntrinsicForGCCBuiltin(Target.getTargetPrefix(), Name);
+  Intrinsic::ID IntrinsicID;
+  const char *TargetPrefix = Target.getTargetPrefix();
+  const char *BuiltinName = getContext().BuiltinInfo.GetName(BuiltinID);
+#define GET_LLVM_INTRINSIC_FOR_GCC_BUILTIN
+#include "llvm/Intrinsics.gen"
+#undef GET_LLVM_INTRINSIC_FOR_GCC_BUILTIN
   
   if (IntrinsicID != Intrinsic::not_intrinsic) {
     SmallVector<Value*, 16> Args;
