a936b393e424e53cc1b6cd2a8135803d067b4b8a
Chris Lattner <sabre@nondot.org>
2009-02-05 18:15:17 +0000

    testcase for rdar://6551276 and llvm-gcc r63873
    llvm-svn: 63874

---
+++ llvm/test/FrontendObjC/2009-02-05-VolatileProp.m
@@ -0,0 +1,11 @@
+/* RUN: %llvmgcc -w -x objective-c -c %s -o /dev/null -pedantic-errors
+   rdar://6551276 */
+
+void foo(const unsigned short *);
+void bar() {
+  unsigned short *s[3];
+  int i;
+  @try { } @catch (id anException) { }
+  foo(2+s[i]);
+}
+
