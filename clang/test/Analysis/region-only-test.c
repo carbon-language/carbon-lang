4bdb1240361771e528526014cab586cc0f422fdd
Zhongxing Xu <xuzhongxing@gmail.com>
2009-02-06 08:56:58 +0000

    Put the region store specific test in a separate file.
    llvm-svn: 63930

---
+++ clang/test/Analysis/region-only-test.c
@@ -0,0 +1,13 @@
+// RUN: clang -analyze -checker-simple -analyzer-store-region -verify %s
+
+// Region store must be enabled for tests in this file.
+
+// Exercise creating ElementRegion with symbolic super region.
+void foo(int* p) {
+  int *x;
+  int a;
+  if (p[0] == 1)
+    x = &a;
+  if (p[0] == 1)
+    *x; // no-warning
+}
