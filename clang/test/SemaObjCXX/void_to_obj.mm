dfa6ee53d70885f1e5195ca1ff410dec74701072
Douglas Gregor <dgregor@apple.com>
2009-02-07 17:16:20 +0000

    Add a test case for some Objective-C behavior that will not be supported in Objective-C++
    llvm-svn: 64019

---
+++ clang/test/SemaObjCXX/void_to_obj.mm
@@ -0,0 +1,11 @@
+// RUN: clang -fsyntax-only -verify %s
+
+// <rdar://problem/6463729>
+@class XX;
+
+void func() {
+  XX *obj;
+  void *vv;
+
+  obj = vv; // expected-error{{incompatible type assigning 'void *', expected 'XX *'}}
+}
