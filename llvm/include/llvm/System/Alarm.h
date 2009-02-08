8b5bfbfc7a37ea20cfc7c8c73aac14e7157e4c4e
Mikhail Glushenkov <foldr@codedgers.com>
2009-02-08 22:47:39 +0000

    Add a Sleep() function.
    llvm-svn: 64101

--- llvm/include/llvm/System/Alarm.h
+++ llvm/include/llvm/System/Alarm.h
@@ -7,8 +7,8 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file provides an operating system independent interface to alarm(2) 
-// type functionality. The Alarm class allows a one-shot alarm to be set up 
+// This file provides an operating system independent interface to alarm(2)
+// type functionality. The Alarm class allows a one-shot alarm to be set up
 // at some number of seconds in the future. When the alarm triggers, a method
 // is called to process the event
 //
@@ -20,7 +20,7 @@
 namespace llvm {
 namespace sys {
 
-  /// This function registers an alarm to trigger some number of \p seconds in 
+  /// This function registers an alarm to trigger some number of \p seconds in
   /// the future. When that time arrives, the AlarmStatus function will begin
   /// to return 1 instead of 0. The user must poll the status of the alarm by
   /// making occasional calls to AlarmStatus. If the user sends an interrupt
@@ -31,13 +31,18 @@ namespace sys {
     unsigned seconds ///< Number of seconds in future when alarm arrives
   );
 
-  /// This function terminates the alarm previously set up 
+  /// This function terminates the alarm previously set up
   /// @returns nothing
   void TerminateAlarm();
 
-  /// This function acquires the status of the alarm. 
+  /// This function acquires the status of the alarm.
   /// @returns -1=cancelled, 0=untriggered, 1=triggered
   int AlarmStatus();
+
+  /// Sleep for n seconds.
+  /// @returns nothing.
+  void Sleep(unsigned n);
+
 
 } // End sys namespace
 } // End llvm namespace
