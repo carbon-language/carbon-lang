2edf0fcb7508dcca7389a7969cb7a93dfbdfbd50
Chris Lattner <sabre@nondot.org>
2009-02-15 21:06:15 +0000

    fix PR3579: __LINE__ expands to the presumed location of the *end* of a macro instantiation, not the start of it.  This is really all about bug-for-bug compatibility with GCC, but not doing this breaks the FreeBSD kernel.
    llvm-svn: 64603

--- clang/test/Preprocessor/builtin_line.c
+++ clang/test/Preprocessor/builtin_line.c
@@ -1,4 +1,13 @@
-// RUN: clang %s -E | grep "^  4"
+// RUN: clang %s -E | grep "^  4" &&
 #define FOO __LINE__
 
   FOO
+
+// PR3579 - This should expand to the __LINE__ of the ')' not of the X.
+// RUN: clang %s -E | grep "^A 13"
+
+#define X() __LINE__
+
+A X(
+
+)
