45a2a20384f6c74d66f2b5c0b42f083d31deaefc
Daniel Dunbar <daniel@zuster.org>
2009-02-16 22:42:44 +0000

    Unbreak clang.
    Doug: please verify that it is expected that LastIdx can be less that
    NumInits. And perhaps add a comment so that Chris doesn't break your
    code. :)
    
    llvm-svn: 64688

--- clang/lib/AST/Expr.cpp
+++ clang/lib/AST/Expr.cpp
@@ -246,7 +246,7 @@ InitListExpr::InitListExpr(SourceLocation lbraceloc,
 
 void InitListExpr::resizeInits(ASTContext &Context, unsigned NumInits) {
   for (unsigned Idx = NumInits, LastIdx = InitExprs.size();
-       Idx != LastIdx; ++Idx)
+       Idx < LastIdx; ++Idx)
     delete InitExprs[Idx];
   InitExprs.resize(NumInits, 0);
 }
