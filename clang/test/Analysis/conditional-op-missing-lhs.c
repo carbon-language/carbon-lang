35b13439cc09a310432b6ee0f7a7ac2e51d058de
Ted Kremenek <kremenek@apple.com>
2009-02-17 19:53:58 +0000

    Convert tabs to spaces.
    llvm-svn: 64799

--- clang/test/Analysis/conditional-op-missing-lhs.c
+++ clang/test/Analysis/conditional-op-missing-lhs.c
@@ -2,25 +2,25 @@
 
 void f1()
 {
-	int i;
-	
-	int j = i ? : 1; // expected-warning{{use of uninitialized variable}} //expected-warning{{Value stored to 'j' during its initialization is never read}}
+  int i;
+  
+  int j = i ? : 1; // expected-warning{{use of uninitialized variable}} //expected-warning{{Value stored to 'j' during its initialization is never read}}
 }
 
 void *f2(int *i)
 {
-	return i ? : 0;
+  return i ? : 0;
 }
 
 void *f3(int *i)
 {
-	int a;
-	
-	return &a ? : i;
+  int a;
+  
+  return &a ? : i;
 }
 
 void f4()
 {
-	char c[1 ? : 2];
+  char c[1 ? : 2];
 }
 
