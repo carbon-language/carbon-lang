00211107735dddea171192f2439f8a54bf47cd2d
Chris Lattner <sabre@nondot.org>
2009-02-17 02:37:35 +0000

    asin support.  No we don't care about complex int :)
    llvm-svn: 64723

--- clang/lib/Headers/tgmath-sofar.h
+++ clang/lib/Headers/tgmath-sofar.h
@@ -69,7 +69,19 @@ __TG_ATTRS _Complex double      __tg_acos(_Complex double x)      { return cacos
 __TG_ATTRS _Complex long double __tg_acos(_Complex long double x) { return cacosl(x); }
 #define acos(x) __tg_acos(x)
 
-__TG_RC_1(x, asin, casin)
+// asin
+__TG_ATTRS float                __tg_asin(float x)                { return  asinf(x); }
+__TG_ATTRS double               __tg_asin(double x)               { return  asin(x); }
+__TG_ATTRS long double          __tg_asin(long double x)          { return  asinl(x); }
+__TG_ATTRS double               __tg_asin(long long x)            { return  asin(x); }
+__TG_ATTRS double               __tg_asin(long x)                 { return  asin(x); }
+__TG_ATTRS double               __tg_asin(int x)                  { return  asin(x); }
+__TG_ATTRS double               __tg_asin(unsigned long long x)   { return  asin(x); }
+__TG_ATTRS double               __tg_asin(unsigned long x)        { return  asin(x); }
+__TG_ATTRS double               __tg_asin(unsigned x)             { return  asin(x); }
+__TG_ATTRS _Complex float       __tg_asin(_Complex float x)       { return casinf(x); }
+__TG_ATTRS _Complex double      __tg_asin(_Complex double x)      { return casin(x); }
+__TG_ATTRS _Complex long double __tg_asin(_Complex long double x) { return casinl(x); }
 #define asin(x) __tg_asin(x)
 
 
