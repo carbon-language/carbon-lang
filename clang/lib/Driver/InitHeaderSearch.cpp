45d26bd00d1461a55fb81fa4a167ea0c30053cb9
Chris Lattner <sabre@nondot.org>
2009-02-19 04:48:57 +0000

    PR3614: "ignoring nonexistent directory" should print the -isysroot mapped path, not the requested path.
    llvm-svn: 65009

--- clang/lib/Driver/InitHeaderSearch.cpp
+++ clang/lib/Driver/InitHeaderSearch.cpp
@@ -20,7 +20,6 @@
 #include "llvm/System/Path.h"
 #include "llvm/Config/config.h"
 #include <vector>
-
 using namespace clang;
 
 void InitHeaderSearch::AddPath(const std::string &Path, IncludeDirGroup Group,
@@ -75,7 +74,8 @@ void InitHeaderSearch::AddPath(const std::string &Path, IncludeDirGroup Group,
   }
   
   if (Verbose)
-    fprintf(stderr, "ignoring nonexistent directory \"%s\"\n", Path.c_str());
+    fprintf(stderr, "ignoring nonexistent directory \"%s\"\n",
+            MappedPath.c_str());
 }
 
 
