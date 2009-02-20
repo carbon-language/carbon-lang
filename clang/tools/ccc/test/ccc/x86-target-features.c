6ef6a04dd647b60e4eafed08a154d0eb7e585760
Daniel Dunbar <daniel@zuster.org>
2009-02-20 18:53:07 +0000

    Fix test case.
    llvm-svn: 65139

--- clang/tools/ccc/test/ccc/x86-target-features.c
+++ clang/tools/ccc/test/ccc/x86-target-features.c
@@ -1,4 +1,4 @@
 // RUN: xcc -ccc-host-machine i386 -### -S %s -mno-red-zone -mno-sse -msse4a -msoft-float &> %t &&
-// RUN: grep '"--mattrs=-sse,+sse4a"' %t &&
+// RUN: grep '"--mattr=-sse,+sse4a"' %t &&
 // RUN: grep '"--disable-red-zone"' %t &&
 // RUN: grep '"--soft-float"' %t
