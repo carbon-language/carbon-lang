d0b80c8c75c6e445f0da37744ca21e7aed676528
Chris Lattner <sabre@nondot.org>
2009-02-20 17:04:14 +0000

    add support for amd64-*, patch by Brooks Davis!
    llvm-svn: 65124

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -1015,7 +1015,7 @@ TargetInfo* TargetInfo::CreateTargetInfo(const std::string &T) {
     return new SparcV8TargetInfo(T);
   }
 
-  if (T.find("x86_64-") == 0) {
+  if (T.find("x86_64-") == 0 || T.find("amd64-") == 0) {
     if (isDarwin)
       return new DarwinX86_64TargetInfo(T);
     if (isLinux)
