43e8a2b36e2496aa1ea4dc7937addf76b8002fde
Anders Carlsson <andersca@mac.com>
2009-02-26 17:38:19 +0000

    Add test for enum types
    llvm-svn: 65540

--- clang/test/CodeGen/x86_64-arguments.c
+++ clang/test/CodeGen/x86_64-arguments.c
@@ -5,7 +5,8 @@
 // RUN: grep 'define float @f3()' %t &&
 // RUN: grep 'define double @f4()' %t &&
 // RUN: grep 'define x86_fp80 @f5()' %t &&
-// RUN: grep 'define void @f6(i8 signext %a0, i16 signext %a1, i32 %a2, i64 %a3, i8\* %a4)' %t
+// RUN: grep 'define void @f6(i8 signext %a0, i16 signext %a1, i32 %a2, i64 %a3, i8\* %a4)' %t &&
+// RUN: grep 'define void @f7(i32 %a0)' %t
 
 char f0(void) {
 }
@@ -26,4 +27,9 @@ long double f5(void) {
 }
 
 void f6(char a0, short a1, int a2, long long a3, void *a4) {
+}
+
+typedef enum { A, B, C } E;
+
+void f7(E a0) {
 }
