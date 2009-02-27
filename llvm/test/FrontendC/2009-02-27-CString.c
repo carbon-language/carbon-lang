c08336adc36bef512bb561fe31b92cb82e2fd1dd
Stuart Hastings <stuart@apple.com>
2009-02-27 22:35:12 +0000

    Testcase to insure C strings go to the cstring section. Darwin-specific.
    llvm-svn: 65655

---
+++ llvm/test/FrontendC/2009-02-27-CString.c
@@ -0,0 +1,11 @@
+/* RUN: %llvmgcc %s -S -o - -emit-llvm | \
+   RUN: egrep {CSTRING SECTION.\*section.\*__TEXT,.\*__cstring}
+   XFAIL: *
+   TARGET: *-*-darwin*
+   END.
+   Insure that stings go to the cstring section.  This test is
+   intended solely for Darwin targets.
+ */
+char *foo() {
+  return "this string should go to the CSTRING SECTION";
+}
