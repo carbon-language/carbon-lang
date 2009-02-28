6311d06b2f6bb8e5dd9997633bac65f67d8ca311
Bill Wendling <isanbard@gmail.com>
2009-02-28 12:11:01 +0000

    There is a way to specify targets that should succeed. It's the "XTARGET" keyword.
    llvm-svn: 65692

--- llvm/test/FrontendC/2009-02-27-CString.c
+++ llvm/test/FrontendC/2009-02-27-CString.c
@@ -1,7 +1,7 @@
 /* RUN: %llvmgcc %s -S -o - -emit-llvm | \
    RUN: egrep {CSTRING SECTION.\*section.\*__TEXT,.\*__cstring}
-   XFAIL: linux,ia64,alpha,sparc
-   TARGET: *-*-darwin*
+   XFAIL: *
+   XTARGET: darwin
    END.
    Insure that stings go to the cstring section.  This test is
    intended solely for Darwin targets.
