e87da6656250f7b9e0062f55a1e8490ba7b649c0
Chris Lattner <sabre@nondot.org>
2009-03-01 00:59:19 +0000

    adjust to changes in the mainline llvm .ll printer.
    llvm-svn: 65745

--- clang/test/CodeGen/cast-to-union.c
+++ clang/test/CodeGen/cast-to-union.c
@@ -1,7 +1,7 @@
 // RUN: clang -emit-llvm < %s -o %t &&
 // RUN: grep "store i32 351, i32*" %t &&
-// RUN: grep "w = global <{ i32, i8, i8, i8, i8 }> <{ i32 2, i8 0, i8 0, i8 0, i8 0 }>" %t &&
-// RUN: grep "y = global <{ double }> <{ double 7.300000e+01 }>" %t
+// RUN: grep "w = global %0 <{ i32 2, i8 0, i8 0, i8 0, i8 0 }>" %t &&
+// RUN: grep "y = global %1 <{ double 7.300000e+01 }>" %t
 
 union u { int i; double d; };
 
