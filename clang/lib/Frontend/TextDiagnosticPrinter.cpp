e29509a6e10af67459f88997959fdfc46f0a870f
Chris Lattner <sabre@nondot.org>
2009-03-02 20:58:48 +0000

    fix PR2639
    llvm-svn: 65869

--- clang/lib/Frontend/TextDiagnosticPrinter.cpp
+++ clang/lib/Frontend/TextDiagnosticPrinter.cpp
@@ -218,7 +218,7 @@ void TextDiagnosticPrinter::EmitCaretDiagnostic(SourceLocation Loc,
         // We have an insertion hint. Determine whether the inserted
         // code is on the same line as the caret.
         std::pair<FileID, unsigned> HintLocInfo 
-          = SM.getDecomposedLoc(Hint->InsertionLoc);
+          = SM.getDecomposedInstantiationLoc(Hint->InsertionLoc);
         if (SM.getLineNumber(HintLocInfo.first, HintLocInfo.second) ==
               SM.getLineNumber(FID, FileOffset)) {
           // Insert the new code into the line just below the code
--- clang/test/Sema/implicit-int.c
+++ clang/test/Sema/implicit-int.c
@@ -8,3 +8,17 @@ y;  // expected-warning {{type specifier missing, defaults to 'int'}}
 // rdar://6131634
 void f((x));  // expected-warning {{type specifier missing, defaults to 'int'}}
 
+
+// PR2639
+#define PAD(ms10) { \
+	register i; \
+}
+
+#define ILPAD() PAD((NROW - tt.tt_row) * 10)	/* 1 ms per char */
+
+void
+h19_insline(n)  // expected-warning {{parameter 'n' was not declared, defaulting to type 'int'}}
+{
+	ILPAD();  // expected-warning {{type specifier missing, defaults to 'int'}}
+}
+
