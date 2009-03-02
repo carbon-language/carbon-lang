e2e704752cba58b8e5c61767fa03ac16936d8a4d
Daniel Dunbar <daniel@zuster.org>
2009-03-02 18:50:36 +0000

    Attempt to make test more robust (fails for users who put LLVM in /path/with/store/in/it").
    llvm-svn: 65852

--- clang/test/CodeGen/string-init.c
+++ clang/test/CodeGen/string-init.c
@@ -1,5 +1,7 @@
-// RUN: clang -emit-llvm %s -o - | not grep -F "[5 x i8]" &&
-// RUN: clang -emit-llvm %s -o - | not grep "store"
+// RUN: clang -emit-llvm %s -o %t &&
+// RUN: grep 'internal constant \[10 x i8\]' %t &&
+// RUN: not grep -F "[5 x i8]" %t &&
+// RUN: not grep "store " %t
 
 void test(void) {
   char a[10] = "asdf";
