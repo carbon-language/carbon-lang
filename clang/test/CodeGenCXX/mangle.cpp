4ed74dd6b72ed29bc8f37ce23f8bdc852b1abe3f
Anders Carlsson <andersca@mac.com>
2009-03-07 23:57:03 +0000

    Make mangling work with anonymous tag types. Doug, please review
    llvm-svn: 66353

--- clang/test/CodeGenCXX/mangle.cpp
+++ clang/test/CodeGenCXX/mangle.cpp
@@ -1,9 +1,21 @@
-// RUN: clang -emit-llvm %s -o - | grep _ZplRK1YRA100_P1X
+// RUN: clang -emit-llvm %s -o %t &&
 
 // FIXME: This test is intentionally trivial, because we can't yet
 // CodeGen anything real in C++.
 struct X { };
 struct Y { };
-  
+
+// RUN: grep _ZplRK1YRA100_P1X %t | count 1 &&
 bool operator+(const Y&, X* (&xs)[100]) { return false; }
 
+// RUN: grep _Z1f1s %t | count 1 &&
+typedef struct { int a; } s;
+void f(s) { }
+
+// RUN: grep _Z1f1e %t| count 1 &&
+typedef enum { foo } e;
+void f(e) { }
+
+// RUN: grep _Z1f1u %t | count 1
+typedef union { int a; } u;
+void f(u) { }
