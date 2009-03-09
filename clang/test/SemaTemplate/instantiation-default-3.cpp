d9c90a73175750384c89921a69008ee95eb8b690
Douglas Gregor <dgregor@apple.com>
2009-03-09 23:48:53 +0000

    Add a test case that goes with the last commit
    llvm-svn: 66510

---
+++ clang/test/SemaTemplate/instantiation-default-3.cpp
@@ -0,0 +1,21 @@
+// RUN: clang -fsyntax-only -verify %s
+
+template<typename T> struct A { };
+
+template<typename T, typename U = A<T*> >
+  struct B : U { };
+
+template<>
+struct A<int*> { 
+  void foo();
+};
+
+template<>
+struct A<float*> { 
+  void bar();
+};
+
+void test(B<int> *b1, B<float> *b2) {
+  b1->foo();
+  b2->bar();
+}
