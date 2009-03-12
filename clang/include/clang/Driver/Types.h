1a8738935c9d33844e493f76905a193f59c3cdad
Daniel Dunbar <daniel@zuster.org>
2009-03-12 07:40:41 +0000

    Driver: Add information on Driver input/temporary types.
    llvm-svn: 66781

---
+++ clang/include/clang/Driver/Types.h
@@ -0,0 +1,64 @@
+//===--- Types.h - Input & Temporary Driver Types ---------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef CLANG_DRIVER_TYPES_H_
+#define CLANG_DRIVER_TYPES_H_
+
+namespace clang {
+namespace driver {
+namespace types {
+  enum ID {
+    TY_INVALID,
+#define TYPE(NAME, ID, PP_TYPE, TEMP_SUFFIX, FLAGS) TY_##ID,
+#include "clang/Driver/Types.def"
+#undef TYPE
+    TY_LAST
+  };
+
+  /// getTypeName - Return the name of the type for \arg Id.
+  const char *getTypeName(ID Id);
+
+  /// getPreprocessedType - Get the ID of the type for this input when
+  /// it has been preprocessed, or INVALID if this input is not
+  /// preprocessed.
+  ID getPreprocessedType(ID Id);
+
+  /// getTypeTempSuffix - Return the suffix to use when creating a
+  /// temp file of this type, or null if unspecified.
+  const char *getTypeTempSuffix(ID Id);
+
+  /// onlyAssembleType - Should this type only be assembled.
+  bool onlyAssembleType(ID Id);
+
+  /// onlyPrecompileType - Should this type only be precompiled.
+  bool onlyPrecompileType(ID Id);
+
+  /// canTypeBeUserSpecified - Can this type be specified on the
+  /// command line (by the type name); this is used when forwarding
+  /// commands to gcc.
+  bool canTypeBeUserSpecified(ID Id);
+
+  /// appendSuffixForType - When generating outputs of this type,
+  /// should the suffix be appended (instead of replacing the existing
+  /// suffix).
+  bool appendSuffixForType(ID Id);
+
+  /// lookupTypeForExtension - Lookup the type to use for the file
+  /// extension \arg Ext.
+  ID lookupTypeForExtension(const char *Ext);
+
+  /// lookupTypeForTypSpecifier - Lookup the type to use for a user
+  /// specified type name.
+  ID lookupTypeForTypeSpecifier(const char *Name);
+
+} // end namespace types
+} // end namespace driver
+} // end namespace clang
+
+#endif
---
+++ clang/lib/Driver/Types.cpp
@@ -0,0 +1,125 @@
+//===--- Types.cpp - Driver input & temporary type information ----------*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/Driver/Types.h"
+
+#include <string.h>
+#include <cassert>
+
+using namespace clang;
+using namespace clang::driver;
+using namespace clang::driver::types;
+
+struct Info {
+  const char *Name;
+  const char *Flags;
+  const char *TempSuffix;
+  ID PreprocessedType;
+};
+
+static Info TypeInfos[] = {
+#define TYPE(NAME, ID, PP_TYPE, TEMP_SUFFIX, FLAGS) \
+  { NAME, FLAGS, TEMP_SUFFIX, TY_##PP_TYPE, },
+#include "clang/Driver/Types.def"
+#undef TYPE
+};
+static const unsigned numTypes = sizeof(TypeInfos) / sizeof(TypeInfos[0]);
+
+static Info &getInfo(unsigned id) {
+  assert(id > 0 && id - 1 < numTypes && "Invalid Type ID.");
+  return TypeInfos[id - 1];
+}
+
+const char *types::getTypeName(ID Id) { 
+  return getInfo(Id).Name; 
+}
+
+types::ID types::getPreprocessedType(ID Id) { 
+  return getInfo(Id).PreprocessedType; 
+}
+
+const char *types::getTypeTempSuffix(ID Id) { 
+  return getInfo(Id).TempSuffix; 
+}
+
+bool types::onlyAssembleType(ID Id) { 
+  return strchr(getInfo(Id).Flags, 'a'); 
+}
+
+bool types::onlyPrecompileType(ID Id) { 
+  return strchr(getInfo(Id).Flags, 'p'); 
+}
+
+bool types::canTypeBeUserSpecified(ID Id) { 
+  return strchr(getInfo(Id).Flags, 'u'); 
+}
+
+bool types::appendSuffixForType(ID Id) { 
+  return strchr(getInfo(Id).Flags, 'A'); 
+}
+
+types::ID types::lookupTypeForExtension(const char *Ext) {
+  unsigned N = strlen(Ext);
+
+  switch (N) {
+  case 1:
+    if (memcmp(Ext, "c", 1) == 0) return TY_C;
+    if (memcmp(Ext, "i", 1) == 0) return TY_PP_C;
+    if (memcmp(Ext, "m", 1) == 0) return TY_ObjC;
+    if (memcmp(Ext, "M", 1) == 0) return TY_ObjCXX;
+    if (memcmp(Ext, "h", 1) == 0) return TY_CHeader;
+    if (memcmp(Ext, "C", 1) == 0) return TY_CXX;
+    if (memcmp(Ext, "H", 1) == 0) return TY_CXXHeader;
+    if (memcmp(Ext, "f", 1) == 0) return TY_PP_Fortran;
+    if (memcmp(Ext, "F", 1) == 0) return TY_Fortran;
+    if (memcmp(Ext, "s", 1) == 0) return TY_PP_Asm;
+    if (memcmp(Ext, "S", 1) == 0) return TY_Asm;
+  case 2:
+    if (memcmp(Ext, "ii", 2) == 0) return TY_PP_CXX;
+    if (memcmp(Ext, "mi", 2) == 0) return TY_PP_ObjC;
+    if (memcmp(Ext, "mm", 2) == 0) return TY_ObjCXX;
+    if (memcmp(Ext, "cc", 2) == 0) return TY_CXX;
+    if (memcmp(Ext, "cc", 2) == 0) return TY_CXX;
+    if (memcmp(Ext, "cp", 2) == 0) return TY_CXX;
+    if (memcmp(Ext, "hh", 2) == 0) return TY_CXXHeader;
+    break;
+  case 3:
+    if (memcmp(Ext, "ads", 3) == 0) return TY_Ada;
+    if (memcmp(Ext, "adb", 3) == 0) return TY_Ada;
+    if (memcmp(Ext, "cxx", 3) == 0) return TY_CXX;
+    if (memcmp(Ext, "cpp", 3) == 0) return TY_CXX;
+    if (memcmp(Ext, "CPP", 3) == 0) return TY_CXX;
+    if (memcmp(Ext, "cXX", 3) == 0) return TY_CXX;
+    if (memcmp(Ext, "for", 3) == 0) return TY_PP_Fortran;
+    if (memcmp(Ext, "FOR", 3) == 0) return TY_PP_Fortran;
+    if (memcmp(Ext, "fpp", 3) == 0) return TY_Fortran;
+    if (memcmp(Ext, "FPP", 3) == 0) return TY_Fortran;
+    if (memcmp(Ext, "f90", 3) == 0) return TY_PP_Fortran;
+    if (memcmp(Ext, "f95", 3) == 0) return TY_PP_Fortran;
+    if (memcmp(Ext, "F90", 3) == 0) return TY_Fortran;
+    if (memcmp(Ext, "F95", 3) == 0) return TY_Fortran;
+    if (memcmp(Ext, "mii", 3) == 0) return TY_PP_ObjCXX;
+    break;
+  }
+
+  return TY_INVALID;
+}
+
+types::ID types::lookupTypeForTypeSpecifier(const char *Name) {
+  unsigned N = strlen(Name);
+
+  for (unsigned i=0; i<numTypes; ++i) {
+    types::ID Id = (types::ID) (i + 1);
+    if (canTypeBeUserSpecified(Id) && 
+        memcmp(Name, getInfo(Id).Name, N) == 0)
+      return Id;
+  }
+
+  return TY_INVALID;
+}
