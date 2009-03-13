c178c5f1265abcd52f0100fcd8b3e57a8fbab4ba
Daniel Dunbar <daniel@zuster.org>
2009-03-13 21:03:35 +0000

    Driver: Desensitize test to location of input.
    llvm-svn: 66950

--- clang/test/Driver/phases.c
+++ clang/test/Driver/phases.c
@@ -1,23 +1,23 @@
 // Basic compilation for various types of files.
 // RUN: clang-driver -ccc-host-triple i386-unknown-unknown -ccc-print-phases -x c %s -x objective-c %s -x c++ %s -x objective-c++ -x assembler %s -x assembler-with-cpp %s -x none %s &> %t &&
-// RUN: grep -F '0: input, "phases.c", c' %t &&
+// RUN: grep '0: input, ".*phases.c", c' %t &&
 // RUN: grep -F '1: preprocessor, {0}, cpp-output' %t &&
 // RUN: grep -F '2: compiler, {1}, assembler' %t &&
 // RUN: grep -F '3: assembler, {2}, object' %t &&
-// RUN: grep -F '4: input, "phases.c", objective-c' %t &&
+// RUN: grep '4: input, ".*phases.c", objective-c' %t &&
 // RUN: grep -F '5: preprocessor, {4}, objective-c-cpp-output' %t &&
 // RUN: grep -F '6: compiler, {5}, assembler' %t &&
 // RUN: grep -F '7: assembler, {6}, object' %t &&
-// RUN: grep -F '8: input, "phases.c", c++' %t &&
+// RUN: grep '8: input, ".*phases.c", c++' %t &&
 // RUN: grep -F '9: preprocessor, {8}, c++-cpp-output' %t &&
 // RUN: grep -F '10: compiler, {9}, assembler' %t &&
 // RUN: grep -F '11: assembler, {10}, object' %t &&
-// RUN: grep -F '12: input, "phases.c", assembler' %t &&
+// RUN: grep '12: input, ".*phases.c", assembler' %t &&
 // RUN: grep -F '13: assembler, {12}, object' %t &&
-// RUN: grep -F '14: input, "phases.c", assembler-with-cpp' %t &&
+// RUN: grep '14: input, ".*phases.c", assembler-with-cpp' %t &&
 // RUN: grep -F '15: preprocessor, {14}, assembler' %t &&
 // RUN: grep -F '16: assembler, {15}, object' %t &&
-// RUN: grep -F '17: input, "phases.c", c' %t &&
+// RUN: grep '17: input, ".*phases.c", c' %t &&
 // RUN: grep -F '18: preprocessor, {17}, cpp-output' %t &&
 // RUN: grep -F '19: compiler, {18}, assembler' %t &&
 // RUN: grep -F '20: assembler, {19}, object' %t &&
@@ -25,7 +25,7 @@
 
 // Universal linked image.
 // RUN: clang-driver -ccc-host-triple i386-apple-darwin9 -ccc-print-phases -x c %s -arch ppc -arch i386 &> %t &&
-// RUN: grep -F '0: input, "phases.c", c' %t &&
+// RUN: grep '0: input, ".*phases.c", c' %t &&
 // RUN: grep -F '1: preprocessor, {0}, cpp-output' %t &&
 // RUN: grep -F '2: compiler, {1}, assembler' %t &&
 // RUN: grep -F '3: assembler, {2}, object' %t &&
@@ -36,7 +36,7 @@
 
 // Universal object file.
 // RUN: clang-driver -ccc-host-triple i386-apple-darwin9 -ccc-print-phases -c -x c %s -arch ppc -arch i386 &> %t &&
-// RUN: grep -F '0: input, "phases.c", c' %t &&
+// RUN: grep '0: input, ".*phases.c", c' %t &&
 // RUN: grep -F '1: preprocessor, {0}, cpp-output' %t &&
 // RUN: grep -F '2: compiler, {1}, assembler' %t &&
 // RUN: grep -F '3: assembler, {2}, object' %t &&
