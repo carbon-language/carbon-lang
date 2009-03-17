62c72c6826bd9b724728840bed6f9ea1346dddad
Devang Patel <dpatel@apple.com>
2009-03-17 19:47:21 +0000

    test case for rev. 67095.
    llvm-svn: 67096

---
+++ llvm/test/FrontendC++/2009-03-17-dbg.cpp
@@ -0,0 +1,14 @@
+// RUN: %llvmgxx -c -emit-llvm %s -o /dev/null -g
+template <typename T1,typename T2>
+inline void f(const T1&,const T2&) { }
+
+template <typename T1,typename T2,void F(const T1&,const T2&)>
+struct A {
+    template <typename T> void g(T& i) { }
+};
+
+int main() {
+    int i;
+    A<int,int,f> a;
+    a.g(i);
+}
