2b4bda8073e55515a7d81c5e97276e7adad76fb9
Daniel Dunbar <daniel@zuster.org>
2009-03-18 06:21:12 +0000

    Driver: Claim inputs when we bind the InputAction.
    llvm-svn: 67174

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -665,6 +665,7 @@ void Driver::BuildJobsForAction(Compilation &C,
                                 InputInfo &Result) const {
   llvm::PrettyStackTraceString CrashInfo("Building compilation jobs for action");
   if (const InputAction *IA = dyn_cast<InputAction>(A)) {
+    IA->getInputArg().claim();
     const char *Name = IA->getInputArg().getValue(C.getArgs());
     Result = InputInfo(Name, A->getType(), Name);
     return;
