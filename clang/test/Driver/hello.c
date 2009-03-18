ea2be5e8eed93c3b3c55abd1085c8138a5eba898
Daniel Dunbar <daniel@zuster.org>
2009-03-18 22:49:41 +0000

    Add simple test case to make sure driver can generate executables.  - Hopefully Chris can pardon one executable test.
    llvm-svn: 67251

---
+++ clang/test/Driver/hello.c
@@ -0,0 +1,14 @@
+// RUN: clang-driver -ccc-echo -o %t %s &> %t.log &&
+
+// Make sure we used clang.
+// RUN: grep 'clang" .*hello.c' %t.log &&
+
+// RUN: %t > %t.out &&
+// RUN: grep "I'm a little driver, short and stout." %t.out
+
+#include <stdio.h>
+
+int main() {
+  printf("I'm a little driver, short and stout.");
+  return 0;
+}
