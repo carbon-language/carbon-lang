88c347443e1ccfad821698b00275681ba5062148
Chris Lattner <sabre@nondot.org>
2009-03-20 16:28:22 +0000

    add tests for the various .S bugs I fixed on wednesday.
    llvm-svn: 67381

--- clang/test/Preprocessor/assembler-with-cpp.c
+++ clang/test/Preprocessor/assembler-with-cpp.c
@@ -1,6 +1,36 @@
-// RUN: clang -x assembler-with-cpp -E %s &&
-// RUN: not clang -x c -E %s
+// RUN: clang -x assembler-with-cpp -E %s > %t &&
 
 #ifndef __ASSEMBLER__
 #error "__ASSEMBLER__ not defined"
 #endif
+
+
+// Invalid token pasting is ok. 
+// RUN: grep '1: X .' %t &&
+#define A X ## .
+1: A
+
+// Line markers are not linemarkers in .S files, they are passed through.
+// RUN: grep '# 321' %t &&
+# 321
+
+// Unknown directives are passed through.
+// RUN: grep '# B C' %t &&
+# B C
+
+// Unknown directives are expanded.
+// RUN: grep '# BAR42' %t &&
+#define D(x) BAR ## x
+# D(42)
+
+// Unmatched quotes are permitted.
+// RUN: grep "2: '" %t &&
+// RUN: grep '3: "' %t &&
+2: '
+3: "
+
+// Empty char literals are ok.
+// RUN: grep "4: ''" %t &&
+4: ''
+
+// RUN: true
