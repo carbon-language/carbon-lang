9b2fdf05c05c5939e0c7c7701c0f48d9a079fb03
Daniel Dunbar <daniel@zuster.org>
2009-03-20 19:34:03 +0000

    Update test case.
    llvm-svn: 67392

--- clang/test/Coverage/codegen-next.m
+++ clang/test/Coverage/codegen-next.m
@@ -1,7 +1,4 @@
-// RUN: clang -DIRGENABLE -fnext-runtime -emit-llvm -o %t %s &&
-// RUN: clang -DIRGENABLE -g -fnext-runtime -emit-llvm -o %t %s &&
-
-// FIXME: Remove IRGENABLE when possible.
-// RUN: ! clang -fnext-runtime -emit-llvm -o %t %s
+// RUN: clang -fnext-runtime -emit-llvm -o %t %s &&
+// RUN: clang -g -fnext-runtime -emit-llvm -o %t %s
 
 #include "objc-language-features.inc"
