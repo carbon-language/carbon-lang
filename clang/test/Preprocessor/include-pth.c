8b2c1cd5a26827d8c685c2863ed59bcd036faa9e
Ted Kremenek <kremenek@apple.com>
2009-03-20 23:45:36 +0000

    Fix include-pth.c test case to (hopefully) more resilient to where and how tests are executed.
    llvm-svn: 67419

--- clang/test/Preprocessor/include-pth.c
+++ clang/test/Preprocessor/include-pth.c
@@ -1,1 +1,2 @@
-// RUN: clang -x c-header file_to_include.h -o %t && clang -include-pth %t %s -E | grep 'file_to_include'
+// RUN: clang -x c-header %s -o %t && clang -include-pth %t %s -E | grep 'file_to_include' | count 2
+#include "file_to_include.h"
