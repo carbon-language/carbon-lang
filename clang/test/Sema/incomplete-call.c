38f849375264af27c10a9d5b98dc11ef32f64567
Eli Friedman <eli.friedman@gmail.com>
2009-03-22 22:03:03 +0000

    Add testcase for incomplete call/return types for calls.
    llvm-svn: 67486

---
+++ clang/test/Sema/incomplete-call.c
@@ -0,0 +1,13 @@
+// RUN: clang -fsyntax-only -verify %s
+
+struct foo; // expected-note 3 {{forward declaration of 'struct foo'}}
+
+struct foo a();
+void b(struct foo);
+void c();
+
+void func() {
+  a(); // expected-error{{return type of called function ('struct foo') is incomplete}}
+  b(*(struct foo*)0); // expected-error{{argument type 'struct foo' is incomplete}}
+  c(*(struct foo*)0); // expected-error{{argument type 'struct foo' is incomplete}}
+}
