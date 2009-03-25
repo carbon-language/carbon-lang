c3b2111d975a39d19f0c5d635e2961a4449c5a71
Chris Lattner <sabre@nondot.org>
2009-03-25 00:28:58 +0000

    Fix PR3874 by restoring a condition I removed, but making it more precise than it used to be.
    llvm-svn: 67662

--- llvm/lib/Transforms/Scalar/InstructionCombining.cpp
+++ llvm/lib/Transforms/Scalar/InstructionCombining.cpp
@@ -6582,7 +6582,8 @@ Instruction *InstCombiner::visitICmpInstWithInstAndIntCst(ICmpInst &ICI,
       // preferable because it allows the C<<Y expression to be hoisted out
       // of a loop if Y is invariant and X is not.
       if (Shift && Shift->hasOneUse() && RHSV == 0 &&
-          ICI.isEquality() && !Shift->isArithmeticShift()) {
+          ICI.isEquality() && !Shift->isArithmeticShift() &&
+          !isa<Constant>(Shift->getOperand(0))) {
         // Compute C << Y.
         Value *NS;
         if (Shift->getOpcode() == Instruction::LShr) {
