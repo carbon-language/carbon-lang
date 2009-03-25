4b6c049b3f7de1a180a365b6976c4dd33ef02fe7
Evan Cheng <evan.cheng@apple.com>
2009-03-25 16:49:31 +0000

    Add __builtin___memcpy_chk tests.
    llvm-svn: 67691

---
+++ llvm/test/FrontendC/memcpy_chk.c
@@ -0,0 +1,23 @@
+// RUN: %llvmgcc -S -emit-llvm -O1 %s -o - | grep call | grep memcpy_chk | count 3
+// RUN: %llvmgcc -S -emit-llvm -O1 %s -o - | grep call | grep {llvm.memcpy} | count 2
+
+void *t1(void *d, void *s) {
+  return __builtin___memcpy_chk(d, s, 16, 0);
+}
+
+void *t2(void *d, void *s) {
+  return __builtin___memcpy_chk(d, s, 16, 10);
+}
+
+void *t3(void *d, void *s) {
+  return __builtin___memcpy_chk(d, s, 16, 17);
+}
+
+void *t4(void *d, void *s, unsigned len) {
+  return __builtin___memcpy_chk(d, s, len, 17);
+}
+
+char buf[10];
+void *t5(void *s, unsigned len) {
+  return __builtin___memcpy_chk(buf, s, 5, __builtin_object_size(buf, 0));
+}
