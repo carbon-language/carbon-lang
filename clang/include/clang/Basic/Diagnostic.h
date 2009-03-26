e6224e791323759e1ea21bac6e9bad368ad2476d
Anders Carlsson <andersca@mac.com>
2009-03-26 23:48:49 +0000

    I said _fix_ the build, don't break it.
    llvm-svn: 67797

--- clang/include/clang/Basic/Diagnostic.h
+++ clang/include/clang/Basic/Diagnostic.h
@@ -144,7 +144,7 @@ public:
     ak_identifierinfo,  // IdentifierInfo
     ak_qualtype,        // QualType
     ak_declarationname, // DeclarationName
-    ak_nameddecl,       // NamedDecl *
+    ak_nameddecl        // NamedDecl *
   };
 
 private:  
