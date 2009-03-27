17941120d65ee1caa549b61a406bb9f15c704e02
Anders Carlsson <andersca@mac.com>
2009-03-27 04:54:36 +0000

    Move Sema::SetMemberAccessSpecifier to SemaAccess.cpp
    llvm-svn: 67820

--- clang/lib/Sema/SemaAccess.cpp
+++ clang/lib/Sema/SemaAccess.cpp
@@ -10,3 +10,30 @@
 // This file provides Sema routines for C++ access control semantics.
 //
 //===----------------------------------------------------------------------===//
+
+#include "Sema.h"
+using namespace clang;
+
+bool Sema::SetMemberAccessSpecifier(NamedDecl *MemberDecl, 
+                                    NamedDecl *PrevMemberDecl,
+                                    AccessSpecifier LexicalAS) {
+  if (!PrevMemberDecl) {
+    // Use the lexical access specifier.
+    MemberDecl->setAccess(LexicalAS);
+    return false;
+  }
+  
+  // C++ [class.access.spec]p3: When a member is redeclared its access
+  // specifier must be same as its initial declaration.
+  if (LexicalAS != AS_none && LexicalAS != PrevMemberDecl->getAccess()) {
+    Diag(MemberDecl->getLocation(), 
+         diag::err_class_redeclared_with_different_access) 
+      << MemberDecl << LexicalAS;
+    Diag(PrevMemberDecl->getLocation(), diag::note_previous_access_declaration)
+      << PrevMemberDecl << PrevMemberDecl->getAccess();
+    return true;
+  }
+  
+  MemberDecl->setAccess(PrevMemberDecl->getAccess());
+  return false;
+}
--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -2538,28 +2538,3 @@ void Sema::SetDeclDeleted(DeclTy *dcl, SourceLocation DelLoc) {
   }
   Fn->setDeleted();
 }
-
-bool Sema::SetMemberAccessSpecifier(NamedDecl *MemberDecl, 
-                                    NamedDecl *PrevMemberDecl,
-                                    AccessSpecifier LexicalAS) {
-  if (!PrevMemberDecl) {
-    // Use the lexical access specifier.
-    MemberDecl->setAccess(LexicalAS);
-    return false;
-  }
-  
-  // C++ [class.access.spec]p3: When a member is redeclared its access
-  // specifier must be same as its initial declaration.
-  if (LexicalAS != AS_none && LexicalAS != PrevMemberDecl->getAccess()) {
-    Diag(MemberDecl->getLocation(), 
-         diag::err_class_redeclared_with_different_access) 
-      << MemberDecl << LexicalAS;
-    Diag(PrevMemberDecl->getLocation(), diag::note_previous_access_declaration)
-      << PrevMemberDecl << PrevMemberDecl->getAccess();
-    return true;
-  }
-  
-  MemberDecl->setAccess(PrevMemberDecl->getAccess());
-  return false;
-}
-
