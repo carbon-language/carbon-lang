7ef46d9ae29f101e9e705210cfb181b3c253cbc1
Douglas Gregor <dgregor@apple.com>
2009-03-31 20:07:16 +0000

    Improve the dependent nested-name-specifier test a bit
    llvm-svn: 68136

--- clang/test/SemaTemplate/metafun-apply.cpp
+++ clang/test/SemaTemplate/metafun-apply.cpp
@@ -14,9 +14,17 @@ struct add_reference {
   };
 };
 
+struct bogus {
+  struct apply {
+    typedef int type;
+  };
+};
+
 template<typename MetaFun, typename T>
 struct apply1 {
-  typedef typename MetaFun::template apply<T>::type type; // expected-note{{in instantiation of template class 'struct add_reference::apply<void>' requested here}}
+  typedef typename MetaFun::template apply<T>::type type; // expected-note{{in instantiation of template class 'struct add_reference::apply<void>' requested here}} \
+  // expected-error{{'apply' following the 'template' keyword does not refer to a template}} \
+  // FIXME: expected-error{{type 'MetaFun::template apply<int>' cannot be used prior to '::' because it has no members}}
 };
 
 int i;
@@ -27,4 +35,9 @@ apply1<add_reference, float>::type fr = i; // expected-error{{non-const lvalue r
 void test() {
   apply1<add_reference, void>::type t; // expected-note{{in instantiation of template class 'struct apply1<struct add_reference, void>' requested here}} \
   // FIXME: expected-error{{unexpected type name 'type': expected expression}}
+
+  apply1<bogus, int>::type t2; // expected-note{{in instantiation of template class 'struct apply1<struct bogus, int>' requested here}} \
+  // FIXME: expected-error{{unexpected type name 'type': expected expression}}
 }
+
+
