0f55fd9f33027a0f931ba2ad91688c532493ee59
Steve Naroff <snaroff@apple.com>
2009-04-01 21:27:56 +0000

    Update test case and comment.
    llvm-svn: 68247

--- clang/test/SemaObjC/ucn-objc-string.m
+++ clang/test/SemaObjC/ucn-objc-string.m
@@ -5,9 +5,9 @@ extern void NSLog(NSString *format, ...) __attribute__((format(__NSString__, 1, 
 int main() {
   NSLog(@"Hi…");
   NSLog(@"Exposé");
-  // FIXME: the following 2 are still not working (will investigate).
+  NSLog(@"\U00010400\U0001D12B");
+  // FIXME: the following is still not working (will investigate).
   //NSLog(@"hello \u2192 \u2603 \u2190 world");
-  //NSLog(@"\U00010400\U0001D12B");
   return 0;
 }
 
