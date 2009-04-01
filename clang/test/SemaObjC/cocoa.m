f46945b27483074371f23285dfd177eab3ea8464
Daniel Dunbar <daniel@zuster.org>
2009-04-01 16:59:39 +0000

    Ted & I crossed paths here, these tests are in cocoa-pth.m now.
    llvm-svn: 68215

--- clang/test/SemaObjC/cocoa.m
+++ clang/test/SemaObjC/cocoa.m
@@ -1,8 +1,5 @@
 // RUN: clang-cc %s -print-stats &&
 // RUN: clang-cc %s -disable-free &&
-// RUN: clang-cc -emit-pth -o %t %s && 
-// RUN: clang-cc -token-cache %t %s &&
-// RUN: clang-cc -token-cache %t %s -E %s -o /dev/null
 #ifdef __APPLE__
 #include <Cocoa/Cocoa.h>
 #endif
