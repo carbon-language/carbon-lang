455de13de1a68ac0fb5002f749bcb61d7c5ea901
Ted Kremenek <kremenek@apple.com>
2009-04-01 15:28:20 +0000

    Re-enable PTH testing for cocoa.h and carbon.h. This tests that PTH works on real-world header files and these tests have caught serious bugs in the past.
    llvm-svn: 68204

--- clang/test/Sema/carbon.c
+++ clang/test/Sema/carbon.c
@@ -1,4 +1,8 @@
-// RUN: clang-cc %s -fsyntax-only -print-stats
+// RUN: clang-cc %s -print-stats &&
+// RUN: clang-cc %s -disable-free &&
+// RUN: clang-cc -emit-pth -o %t %s && 
+// RUN: clang-cc -token-cache %t %s &&
+// RUN: clang-cc -token-cache %t %s -E %s -o /dev/null
 #ifdef __APPLE__
 #include <Carbon/Carbon.h>
 #endif
