5230b85fc188bab279b7e78d4fe80ddb25af5f75
Mike Stump <mrs@apple.com>
2009-04-02 23:42:13 +0000

    Update wording.
    llvm-svn: 68347

--- clang/test/Lexer/fixit-errors.c
+++ clang/test/Lexer/fixit-errors.c
@@ -1,10 +1,10 @@
 // RUN: clang-cc -fsyntax-only -pedantic -fixit %s -o - | clang-cc -pedantic -Werror -x c -
 
 /* This is a test of the various code modification hints that are
-   provided as part of warning or extension diagnostics. Eventually,
-   we would like to actually try to perform the suggested
-   modifications and compile the result to test that no warnings
-   remain. */
+   provided as part of warning or extension diagnostics. All of the
+   warnings will be fixed by -fixit, and the resulting file should
+   compile cleanly with -Werror -pedantic. */
+
 // FIXME: If you put a space at the end of the line, it doesn't work yet!
 char *s = "hi\
 there";
