8554a77e8d86b0d5f28d4ff547eaf5cc3b6ef1bd
Daniel Dunbar <daniel@zuster.org>
2009-04-03 06:26:01 +0000

    Do we have fixit advice for comments yet?
    llvm-svn: 68372

--- clang/include/clang/Basic/TargetInfo.h
+++ clang/include/clang/Basic/TargetInfo.h
@@ -263,13 +263,13 @@ public:
     return ".str";
   }
 
-  /// getUnicodeStringSymbolPrefix - Get the default symbol prefix to
-  /// use for unicode string literals.
+  /// getUnicodeStringSection - Return the section to use for unicode
+  /// string literals, or 0 if no special section is used.
   virtual const char *getUnicodeStringSection() const { 
     return 0;
   }
 
-  /// getCFStringSection - Return the section to use for the CFString
+  /// getCFStringSection - Return the section to use for CFString
   /// literals, or 0 if no special section is used.
   virtual const char *getCFStringSection() const { 
     return "__DATA,__cfstring";
