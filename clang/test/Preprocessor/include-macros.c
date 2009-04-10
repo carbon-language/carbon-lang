feeb8f13164f015635d569b3722a1d13dd288581
Chris Lattner <sabre@nondot.org>
2009-04-10 16:51:32 +0000

    use doug's new %S thing to make this work when run from different locations.
    llvm-svn: 68788

--- clang/test/Preprocessor/include-macros.c
+++ clang/test/Preprocessor/include-macros.c
@@ -1,4 +1,4 @@
-// RUN: clang-cc -E -Dtest=FOO -imacros pr2086.h %s | grep 'HERE: test'
+// RUN: clang-cc -E -Dtest=FOO -imacros %S/pr2086.h %s | grep 'HERE: test'
 
 // This should not be expanded into FOO because pr2086.h undefs 'test'.
 HERE: test
