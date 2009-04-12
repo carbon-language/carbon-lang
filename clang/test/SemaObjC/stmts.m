bdccb0393c0b336b5065d2de2a3e07cfebd96cd7
Chris Lattner <sabre@nondot.org>
2009-04-12 23:29:27 +0000

    add some more coverage.
    llvm-svn: 68928

--- clang/test/SemaObjC/stmts.m
+++ clang/test/SemaObjC/stmts.m
@@ -1,9 +1,14 @@
 // RUN: clang-cc %s -verify -fsyntax-only
 
+struct some_struct;
+
 // Note: NSException is not declared.
 void f0(id x) {
   @try {
-  } @catch (NSException *x) { // expected-error{{unknown type name 'NSException'}}
+  } @catch (NSException *x) { // expected-error {{unknown type name 'NSException'}}
+  } @catch (struct some_struct x) { // expected-error {{@catch parameter is not a pointer to an interface type}}
+  } @catch (int x) { // expected-error {{@catch parameter is not a pointer to an interface type}}
+  } @catch (...) {
   }
 }
 
