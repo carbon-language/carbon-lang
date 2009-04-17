70a42144caddaf32a844acb2280274d81b122e0b
Chris Lattner <sabre@nondot.org>
2009-04-17 18:39:25 +0000

    adjust for the new -Wvector-conversions option
    llvm-svn: 69366

--- clang/test/Sema/vector-cast.c
+++ clang/test/Sema/vector-cast.c
@@ -1,4 +1,4 @@
-// RUN: clang-cc -fsyntax-only %s -verify
+// RUN: clang-cc -fsyntax-only %s -verify -Wvector-conversions
 
 typedef long long t1 __attribute__ ((vector_size (8)));
 typedef char t2 __attribute__ ((vector_size (16)));
@@ -28,3 +28,11 @@ type 't1' and integer type 'short' of different size}}
   char *r3 = (char *)v1; // -expected-error {{invalid conversion between vector\
  type 't1' and scalar type 'char *'}}
 }
+
+
+void f2(t2 X);
+
+void f3(t3 Y) {
+  f2(Y);  // expected-warning {{incompatible vector types passing 't3', expected 't2'}}
+}
+
