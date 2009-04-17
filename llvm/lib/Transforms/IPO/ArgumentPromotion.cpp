22fa407ed71eb3167879cb8fe4b0e698c4d1be6e
David Greene <greened@obbligato.org>
2009-04-17 14:56:18 +0000

    Use a safer iterator interface and get rid of std C++ library misuse. This fixes a --enable-expensive-checks problem.
    llvm-svn: 69353

--- llvm/lib/Transforms/IPO/ArgumentPromotion.cpp
+++ llvm/lib/Transforms/IPO/ArgumentPromotion.cpp
@@ -565,11 +565,10 @@ Function *ArgPromotion::DoPromotion(Function *F,
       // Add a parameter to the function for each element passed in.
       for (ScalarizeTable::iterator SI = ArgIndices.begin(),
              E = ArgIndices.end(); SI != E; ++SI) {
-        unsigned num = SI->size();
         // not allowed to dereference ->begin() if size() is 0
         Params.push_back(GetElementPtrInst::getIndexedType(I->getType(),
-                                                           num ? &*SI->begin(): 0,
-                                                           num));
+                                                           SI->begin(),
+                                                           SI->end()));
         assert(Params.back());
       }
 
