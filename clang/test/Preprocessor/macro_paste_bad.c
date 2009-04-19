ab9d51cfc40eef8133ea1d044d30cf687cfd5df2
Chris Lattner <sabre@nondot.org>
2009-04-19 20:10:12 +0000

    merge two tests
    llvm-svn: 69537

--- clang/test/Preprocessor/macro_paste_bad.c
+++ clang/test/Preprocessor/macro_paste_bad.c
@@ -1,5 +1,22 @@
-// RUN: clang-cc -Eonly -verify %s
+// RUN: clang-cc -Eonly -verify -pedantic %s
 // pasting ""x"" and ""+"" does not give a valid preprocessing token
 #define XYZ  x ## +   // expected-error {{pasting formed 'x', an invalid preprocessing token}}
 XYZ
+
+// GCC PR 20077
+// RUN: clang-cc -Eonly %s -verify
+
+#define a   a ## ## // expected-error {{'##' cannot appear at end of macro expansion}}
+#define b() b ## ## // expected-error {{'##' cannot appear at end of macro expansion}}
+#define c   c ##    // expected-error {{'##' cannot appear at end of macro expansion}}
+#define d() d ##    // expected-error {{'##' cannot appear at end of macro expansion}}
+
+
+#define e   ## ## e // expected-error {{'##' cannot appear at start of macro expansion}}
+#define f() ## ## f // expected-error {{'##' cannot appear at start of macro expansion}}
+#define g   ## g    // expected-error {{'##' cannot appear at start of macro expansion}}
+#define h() ## h    // expected-error {{'##' cannot appear at start of macro expansion}}
+#define i   ##      // expected-error {{'##' cannot appear at start of macro expansion}}
+#define j() ##      // expected-error {{'##' cannot appear at start of macro expansion}}
+
 
