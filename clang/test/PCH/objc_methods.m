01146a5fa9ba8fa2d394437504861dd4d5f5f216
Chris Lattner <sabre@nondot.org>
2009-04-22 05:31:53 +0000

    rename methods.* -> objc_methods.*
    llvm-svn: 69778

--- clang/test/PCH/objc_methods.m
+++ clang/test/PCH/objc_methods.m
@@ -1,8 +1,8 @@
 // Test this without pch.
-// FIXME: clang-cc -include %S/methods.h -fsyntax-only -verify %s &&
+// FIXME: clang-cc -include %S/objc_methods.h -fsyntax-only -verify %s &&
 
 // Test with pch.
-// FIXME: clang-cc -x=objective-c -emit-pch -o %t %S/methods.h &&
+// FIXME: clang-cc -x=objective-c -emit-pch -o %t %S/objc_methods.h &&
 // FIXME: clang-cc -include-pch %t -fsyntax-only -verify %s 
 
 void func() {
