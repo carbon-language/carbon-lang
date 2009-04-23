4645707034aa609960dcf1be20adf3ef8b99e526
Daniel Dunbar <daniel@zuster.org>
2009-04-23 07:00:09 +0000

    Add implicit definition of objc_msgSend.  - As with malloc and friends, this is important where the return type    on a 64-bit platform would otherwise end up discarding the upper    32-bits.
    llvm-svn: 69874

---
+++ clang/test/CodeGenObjC/implicit-objc_msgSend.m
@@ -0,0 +1,7 @@
+// RUN: clang-cc -triple x86_64-apple-darwin9 -emit-llvm -o %t %s &&
+// RUN: grep -F 'declare i8* @objc_msgSend(...)' %t
+
+typedef struct objc_selector *SEL;
+id f0(id x, SEL s) {
+  return objc_msgSend(x, s);
+}
