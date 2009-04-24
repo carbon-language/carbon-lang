643e0a40dbd34b86230aacf405bb471059a2ebd6
Anders Carlsson <andersca@mac.com>
2009-04-24 06:06:07 +0000

    Fix assert.
    llvm-svn: 69961

--- clang/lib/AST/DeclCXX.cpp
+++ clang/lib/AST/DeclCXX.cpp
@@ -377,8 +377,10 @@ StaticAssertDecl::~StaticAssertDecl() {
 
 CXXTempVarDecl *CXXTempVarDecl::Create(ASTContext &C, DeclContext *DC,
                                        QualType T) {
-  assert(isa<CXXRecordDecl>(T->getAsRecordType()->getDecl()) &&
-         "CXXTempVarDecl must have a C++ record type!");
+  assert(T->isDependentType() || 
+         isa<CXXRecordDecl>(T->getAsRecordType()->getDecl()) &&
+         "CXXTempVarDecl must either have a dependent type "
+         "or a C++ record type!");
   return new (C) CXXTempVarDecl(DC, T);
 }
 
