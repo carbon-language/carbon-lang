988e627f8c802af7631ad23262cc507b5ebe0dd7
Douglas Gregor <dgregor@apple.com>
2009-04-27 18:49:47 +0000

    Fix a problem with the RUN line of one of the PCH tests
    llvm-svn: 70227

--- clang/test/PCH/va_arg.c
+++ clang/test/PCH/va_arg.c
@@ -2,7 +2,7 @@
 // RUN: clang-cc -triple=x86_64-unknown-freebsd7.0 -include %S/va_arg.h %s -emit-llvm -o - &&
 
 // Test with pch.
-// RUN: clang-cc -triple=x86_64-unknown-freebsd7.0 -o %t %S/va_arg.h &&
+// RUN: clang-cc -triple=x86_64-unknown-freebsd7.0 -emit-pch -o %t %S/va_arg.h &&
 // RUN: clang-cc -triple=x86_64-unknown-freebsd7.0 -include-pch %t %s -emit-llvm -o -
 
 char *g0(char** argv, int argc) { return argv[argc]; }
