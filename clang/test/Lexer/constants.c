0cd56890efcce7781cf70ad57be481334edbe15e
Chris Lattner <sabre@nondot.org>
2009-04-28 18:43:12 +0000

    merge number.c into constants.c and start running it in -verify mode.
    llvm-svn: 70310

--- clang/test/Lexer/constants.c
+++ clang/test/Lexer/constants.c
@@ -1,8 +1,15 @@
-/* RUN: clang-cc -fsyntax-only -verify %s
- */
+// RUN: clang-cc -fsyntax-only -verify %s
 
-int x = 000000080;  /* expected-error {{invalid digit}} */
+int x = 000000080;  // expected-error {{invalid digit}}
 
 int y = 0000\
-00080;             /* expected-error {{invalid digit}} */
+00080;             // expected-error {{invalid digit}}
 
+
+
+float X = 1.17549435e-38F;
+float Y = 08.123456;
+
+// PR2252
+#if -0x8000000000000000  // should not warn.
+#endif
--- clang/test/Lexer/number.c
+++
@@ -1,9 +0,0 @@
-// RUN: clang-cc %s -fsyntax-only
-
-float X = 1.17549435e-38F;
-float Y = 08.123456;
-
-// PR2252
-#if -0x8000000000000000  // should not warn.
-#endif
-
