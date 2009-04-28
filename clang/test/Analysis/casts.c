3c3fee0fb83a195243926c648fc403c17d39899e
Zhongxing Xu <xuzhongxing@gmail.com>
2009-04-28 13:52:13 +0000

    Add test case.
    llvm-svn: 70294

---
+++ clang/test/Analysis/casts.c
@@ -0,0 +1,18 @@
+struct sockaddr_storage {
+  int d;
+};
+
+struct sockaddr {
+  int sa_family;
+};
+
+void f(int sock) {
+  struct sockaddr_storage storage;
+  struct sockaddr* sockaddr = (struct sockaddr*)&storage;
+  socklen_t addrlen = sizeof(storage);
+  getsockname(sock, sockaddr, &addrlen);
+  switch (sockaddr->sa_family) { // no-warning
+  default:
+    ;
+  }
+}
