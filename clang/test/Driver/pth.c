c1547eb338091055604500443cfc72f9b9b71af9
Douglas Gregor <dgregor@apple.com>
2009-04-28 22:43:20 +0000

    Force driver's PTH test to use PTH
    llvm-svn: 70338

--- clang/test/Driver/pth.c
+++ clang/test/Driver/pth.c
@@ -1,8 +1,8 @@
 // Test transparent PTH support.
 
-// RUN: clang -x c-header %s -o %t.h.pch -### 2> %t.log &&
-// RUN: grep '".*/clang-cc" .* "-o" ".*\.h\.pch" "-x" "c-header" ".*pth\.c"' %t.log &&
+// RUN: clang -ccc-pch-is-pth -x c-header %s -o %t.h.pth -### 2> %t.log &&
+// RUN: grep '".*/clang-cc" .* "-o" ".*\.h\.pth" "-x" "c-header" ".*pth\.c"' %t.log &&
 
 // RUN: touch %t.h.pth &&
-// RUN: clang -E -include %t.h %s -### 2> %t.log &&
+// RUN: clang -ccc-pch-is-pth -E -include %t.h %s -### 2> %t.log &&
 // RUN: grep '".*/clang-cc" .*"-include-pth" ".*\.h\.pth" .*"-x" "c" ".*pth\.c"' %t.log
