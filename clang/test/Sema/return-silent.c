a80dd1cae18763c101001f3439ff204ef50f58c4
Steve Naroff <snaroff@apple.com>
2009-04-30 17:53:16 +0000

    Name the "return-type" DiagGroup and reference it in a few places.
    llvm-svn: 70500

---
+++ clang/test/Sema/return-silent.c
@@ -0,0 +1,9 @@
+// RUN: clang-cc %s -Wno-return-type -fsyntax-only -verify
+
+int t14() {
+  return;
+}
+
+void t15() {
+  return 1;
+}
--- clang/test/Sema/return.c
+++ clang/test/Sema/return.c
@@ -7,3 +7,6 @@ int t14() {
   return; // expected-warning {{non-void function 't14' should return a value}}
 }
 
+void t15() {
+  return 1; // expected-warning {{void function 't15' should not return a value}}
+}
