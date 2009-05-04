d84881ec5325efe0b0b911ec4e6a6bd2640eb40c
Duncan Sands <baldrick@free.fr>
2009-05-04 12:54:02 +0000

    Testcase for PR3967.
    llvm-svn: 70856

---
+++ llvm/test/FrontendC/2009-05-04-EnumInreg.c
@@ -0,0 +1,17 @@
+// RUN: llvm-gcc -S -m32 -mregparm=3 %s -emit-llvm -o - | grep {inreg %action}
+// XTARGET: x86
+// PR3967
+
+enum kobject_action {
+        KOBJ_ADD,
+        KOBJ_REMOVE,
+        KOBJ_CHANGE,
+        KOBJ_MOVE,
+        KOBJ_ONLINE,
+        KOBJ_OFFLINE,
+        KOBJ_MAX
+};
+
+struct kobject;
+
+int kobject_uevent(struct kobject *kobj, enum kobject_action action) {}
