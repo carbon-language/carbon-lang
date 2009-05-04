238d0b6ecdb2d1dce889265f4a8bbdf653227953
Ted Kremenek <kremenek@apple.com>
2009-05-04 14:31:19 +0000

    Rename no-outofbounds.c to xfail-no-outofbounds.c and split off that test into a separate file to monitor the fact that BasicStoreManager passes the test.
    llvm-svn: 70859

---
+++ clang/test/Analysis/xfail-no-outofbounds.c
@@ -0,0 +1,7 @@
+// RUN: clang-cc -checker-cfref -analyze -analyzer-store=region -verify %s 
+// XFAIL
+void f() {
+  long x = 0;
+  char *y = (char*) &x;
+  char c = y[0] + y[1] + y[2]; // no-warning
+}
