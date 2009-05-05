b8420464c6bfc054a7b223ea9502e928c3d77397
Douglas Gregor <dgregor@apple.com>
2009-05-05 04:50:50 +0000

    Turns out that Sebastian already implemented the logic to compute the composite pointer type, and his is better! Updated relational- and equality-operator checking accordingly.
    llvm-svn: 70963

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -3771,7 +3771,7 @@ QualType Sema::CheckCompareOperands(Expr *&lex, Expr *&rex, SourceLocation Loc,
       //
       // C++ [expr.eq]p2 uses the same notion for (in)equality
       // comparisons of pointers.
-      QualType T = CompositePointerType(lex, rex, LHSIsNull, RHSIsNull);
+      QualType T = FindCompositePointerType(lex, rex);
       if (T.isNull()) {
         Diag(Loc, diag::err_typecheck_comparison_of_distinct_pointers)
           << lType << rType << lex->getSourceRange() << rex->getSourceRange();
