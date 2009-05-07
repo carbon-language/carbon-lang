5c633e38a8b1f0624d85eb2dcb59507727eece08
Chris Lattner <sabre@nondot.org>
2009-05-07 04:21:16 +0000

    Make this test portable to non-x86 hosts, patch by Mark Cianciosa!
    llvm-svn: 71146

--- clang/test/PCH/asm.c
+++ clang/test/PCH/asm.c
@@ -1,9 +1,9 @@
 // Test this without pch.
-// RUN: clang-cc -include %S/asm.h -fsyntax-only -verify %s &&
+// RUN: clang-cc -triple i386-unknown-unknown -include %S/asm.h -fsyntax-only -verify %s &&
 
 // Test with pch.
-// RUN: clang-cc -emit-pch -o %t %S/asm.h &&
-// RUN: clang-cc -include-pch %t -fsyntax-only -verify %s 
+// RUN: clang-cc -triple i386-unknown-unknown -emit-pch -o %t %S/asm.h &&
+// RUN: clang-cc -triple i386-unknown-unknown -include-pch %t -fsyntax-only -verify %s 
 
 
 void call_f(void) { f(); }
