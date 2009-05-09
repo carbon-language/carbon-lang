4bc5a4c3bdcba4b39a1619804dd8a1f5b2490579
Zhongxing Xu <xuzhongxing@gmail.com>
2009-05-09 13:27:17 +0000

    add comments to test case.
    llvm-svn: 71356

--- clang/test/Analysis/xfail_regionstore_wine_crash.c
+++ clang/test/Analysis/xfail_regionstore_wine_crash.c
@@ -2,7 +2,8 @@
 // XFAIL
 
 // When this test passes we should put it in the misc-ps.m test file.
-
+// This test fails now because RegionStoreManager::Retrieve() does correctly 
+// retrieve the first byte of 'x' when retrieving '*y'.
 void foo() {
   long x = 0;
   char *y = (char *) &x;
