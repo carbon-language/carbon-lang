e92449e4cc9e5dc8fa8c556d35fec21528e8149c
Daniel Dunbar <daniel@zuster.org>
2009-05-11 22:30:29 +0000

    Make cleanup-stack.c test stronger, and work in release-asserts mode.
    llvm-svn: 71484

--- clang/test/CodeGen/cleanup-stack.c
+++ clang/test/CodeGen/cleanup-stack.c
@@ -1,15 +1,25 @@
-// RUN: clang-cc -emit-llvm %s -o %t &&
-// RUN: grep "store i32 0, i32\* %cleanup" %t | count 2
-void f(int n) {
-  int a[n];
+// RUN: clang-cc -triple i386-unknown-unknown -O3 -emit-llvm %s -o %t &&
+// RUN: grep "ret i32 9" %t
+
+struct s0 {
+  int *var;
+  int addend;
+};
+
+static void f0(struct s0 *p) {
+  *p->var += p->addend;
+}
+
+int f1(void) {
+  int var = 0;
   
   {
-    int b[n];
-    
-    if (n)
-      return;
+    struct s0 x __attribute__((cleanup(f0))) = { &var, 2 };  
+    struct s0 y __attribute__((cleanup(f0))) = { &var, 3 };
+    {
+      struct s0 y __attribute__((cleanup(f0))) = { &var, 4 };
+    }
   }
-  
-  if (n)
-    return;
+
+  return var;
 }
