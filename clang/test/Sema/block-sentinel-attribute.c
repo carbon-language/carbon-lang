5e15186c9a3aecb67b8de0c139da2ff44ed14a0e
Fariborz Jahanian <fjahanian@apple.com>
2009-05-15 20:49:33 +0000

    added -fblocks to the test.
    llvm-svn: 71890

--- clang/test/Sema/block-sentinel-attribute.c
+++ clang/test/Sema/block-sentinel-attribute.c
@@ -1,4 +1,4 @@
-// RUN: clang-cc -fsyntax-only -verify %s
+// RUN: clang-cc -fblocks -fsyntax-only -verify %s
 
 void (^e) (int arg, const char * format, ...) __attribute__ ((__sentinel__ (1,1)));
 
