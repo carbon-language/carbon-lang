af7e400fdaf1bc8a62c2758571973331c10a5be7
Bill Wendling <isanbard@gmail.com>
2009-05-18 18:17:22 +0000

    Don't call RegionInlinedFnEnd if our optimization level isn't -O0.
    llvm-svn: 72024

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuild.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuild.cpp
@@ -3929,7 +3929,8 @@ SelectionDAGLowering::visitIntrinsicCall(CallInst &I, unsigned Intrinsic) {
       MachineFunction &MF = DAG.getMachineFunction();
       DISubprogram Subprogram(cast<GlobalVariable>(REI.getContext()));
 
-      if (Subprogram.isNull() || Subprogram.describes(MF.getFunction())) {
+      if (OptLevel == CodeGenOpt::None ||
+          Subprogram.isNull() || Subprogram.describes(MF.getFunction())) {
         unsigned LabelID =
           DW->RecordRegionEnd(cast<GlobalVariable>(REI.getContext()));
         DAG.setRoot(DAG.getLabel(ISD::DBG_LABEL, getCurDebugLoc(),
