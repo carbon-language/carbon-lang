c7516835c326ac7564c612d94aee5ecee5f7f90f
Chris Lattner <sabre@nondot.org>
2009-05-25 18:18:56 +0000

    reword a warning for clarity
    llvm-svn: 72391

--- clang/test/Sema/nonnull-check.c
+++ clang/test/Sema/nonnull-check.c
@@ -13,14 +13,14 @@ foo (int i1, int i2, int i3, void (^cp1)(), void (^cp2)(), void (^cp3)())
 {
   func1(cp1, cp2, i1);
 
-  func1(0, cp2, i1);  // expected-warning {{argument is null where non-null is required}}
-  func1(cp1, 0, i1);  // expected-warning {{argument is null where non-null is required}}
+  func1(0, cp2, i1);  // expected-warning {{null passed to a callee which requires a non-null argument}}
+  func1(cp1, 0, i1);  // expected-warning {{null passed to a callee which requires a non-null argument}}
   func1(cp1, cp2, 0);
 
 
-  func3(0, i2, cp3, i3); // expected-warning {{argument is null where non-null is required}}
-  func3(cp3, i2, 0, i3);  // expected-warning {{argument is null where non-null is required}}
+  func3(0, i2, cp3, i3); // expected-warning {{null passed to a callee which requires a non-null argument}}
+  func3(cp3, i2, 0, i3);  // expected-warning {{null passed to a callee which requires a non-null argument}}
 
-  func4(0, cp1); // expected-warning {{argument is null where non-null is required}}
-  func4(cp1, 0); // expected-warning {{argument is null where non-null is required}}
+  func4(0, cp1); // expected-warning {{null passed to a callee which requires a non-null argument}}
+  func4(cp1, 0); // expected-warning {{null passed to a callee which requires a non-null argument}}
 }
