af4fa989e4cb0225d07012c26e7ccb2c7b35ff8e
Dale Johannesen <dalej@apple.com>
2009-05-26 23:19:19 +0000

    Testcase for (llvm-gcc-4.2) 72442 (PR 4242).
    llvm-svn: 72443

---
+++ llvm/test/FrontendC++/x86-64-abi-sret-vs-2word-struct-param.cpp
@@ -0,0 +1,27 @@
+// RUN: %llvmgxx -S -emit-llvm %s -o - | grep byval | count 2
+// XTARGET: x86
+// PR4242
+// (PR 4242 bug is on 64-bit only, test passes on x86-32 as well)
+
+struct S {
+    void* data[3];
+};
+
+struct T {
+    void* data[2];
+};
+
+extern "C" S fail(int, int, int, int, T t, void* p) {
+    S s;
+    s.data[0] = t.data[0];
+    s.data[1] = t.data[1];
+    s.data[2] = p;
+    return s;
+}
+
+extern "C" S* succeed(S* sret, int, int, int, int, T t, void* p) {
+    sret->data[0] = t.data[0];
+    sret->data[1] = t.data[1];
+    sret->data[2] = p;
+    return sret;
+}
