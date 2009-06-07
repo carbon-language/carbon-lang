aef7175f45ee0e761e0e991db72ffe0381129e03
Mikhail Glushenkov <foldr@codedgers.com>
2009-06-07 07:08:01 +0000

    A basic PIC16 toolchain driver.
    Nice addition to the examples and also a starting point for Sanjiv to work on.
    
    llvm-svn: 73013

---
+++ llvm/tools/llvmc/example/mcc16/driver/Main.cpp
@@ -0,0 +1,14 @@
+//===--- Main.cpp - The LLVM Compiler Driver -------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open
+// Source License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  Just include CompilerDriver/Main.inc.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/CompilerDriver/Main.inc"
