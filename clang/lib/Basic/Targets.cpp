ebb9e4dc4d258758e8a08419db9ddcfd321278de
Eli Friedman <eli.friedman@gmail.com>
2009-06-08 21:16:17 +0000

    Minor tweaks to the Windows target.
    llvm-svn: 73083

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -932,6 +932,10 @@ public:
     TLSSupported = false;
     WCharType = SignedShort;
     WCharWidth = WCharAlign = 16;
+    DoubleAlign = LongLongAlign = 64;
+    DescriptionString = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
+                        "i64:64:64-f32:32:32-f64:64:64-v64:64:64-v128:128:128-"
+                        "a0:0:64-f80:32:32";
   }
   virtual void getTargetDefines(const LangOptions &Opts,
                                 std::vector<char> &Defines) const {
