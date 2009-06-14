45da0c90134e1c93a087351b7ede9d6ed69fc250
Fariborz Jahanian <fjahanian@apple.com>
2009-06-14 16:45:35 +0000

    Test modification. Patch by Jean-Daniel Dupas
    llvm-svn: 73343

--- clang/test/CodeGenObjC/encode-test-5.m
+++ clang/test/CodeGenObjC/encode-test-5.m
@@ -1,12 +1,12 @@
 // RUN: clang-cc -triple=x86_64-apple-darwin9 -fnext-runtime -emit-llvm -o %t %s &&
 
-// RUN: grep ji %t | count 1 &&
+// RUN: grep ji.00 %t | count 1 &&
 char *a = @encode(_Complex int);
 
-// RUN: grep jf %t | count 1 &&
+// RUN: grep jf.00 %t | count 1 &&
 char *b = @encode(_Complex float);
 
-// RUN: grep jd %t | count 1 &&
+// RUN: grep jd.00 %t | count 1 &&
 char *c = @encode(_Complex double);
 
 // RUN: grep "t.00" %t | count 1 &&
