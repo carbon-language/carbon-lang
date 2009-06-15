970da81e8a5f2b4f522cd45b118784db5e8e56b7
Dan Gohman <gohman@apple.com>
2009-06-15 18:05:46 +0000

    glibc has two versions of strerror_r, a standards compliant one and a GNU specific one. The GNU one is chosen when _GNU_SOURCE is defined. g++ always defines _GNU_SOURCE on linux platforms because glibc's headers won't compile in C++ mode without it. The GNU strerror_r doesn't always modify the buffer which causes empty error messages on linux.
    This patch changes MakeErrMsg to use the return value of strerror_r to get
    the string instead of assuming the buffer will be modified, on GLIBC.
    
    Patch by Benjamin Kramer!
    
    llvm-svn: 73396

--- llvm/lib/System/Unix/Unix.h
+++ llvm/lib/System/Unix/Unix.h
@@ -79,12 +79,19 @@ static inline bool MakeErrMsg(
     return true;
   char buffer[MAXPATHLEN];
   buffer[0] = 0;
+  char* str = buffer;
   if (errnum == -1)
     errnum = errno;
 #ifdef HAVE_STRERROR_R
   // strerror_r is thread-safe.
   if (errnum)
+# if defined(__GLIBC__) && defined(_GNU_SOURCE)
+    // glibc defines its own incompatible version of strerror_r
+    // which may not use the buffer supplied.
+    str = strerror_r(errnum,buffer,MAXPATHLEN-1);
+# else
     strerror_r(errnum,buffer,MAXPATHLEN-1);
+# endif
 #elif HAVE_STRERROR
   // Copy the thread un-safe result of strerror into
   // the buffer as fast as possible to minimize impact
@@ -97,7 +104,7 @@ static inline bool MakeErrMsg(
   // but, oh well, just use a generic message
   sprintf(buffer, "Error #%d", errnum);
 #endif
-  *ErrMsg = prefix + ": " + buffer;
+  *ErrMsg = prefix + ": " + str;
   return true;
 }
 
