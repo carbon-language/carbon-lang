77eb1d53332c027abec29b0bf46b0aba91de73c0
Chris Lattner <sabre@nondot.org>
2009-06-15 04:59:21 +0000

    convert this test to -verify mode.
    llvm-svn: 73374

--- clang/test/Preprocessor/_Pragma-syshdr2.c
+++ clang/test/Preprocessor/_Pragma-syshdr2.c
@@ -1,5 +1,5 @@
-// RUN: clang-cc -E %s 2>&1 | grep 'file not found'
+// RUN: clang-cc -E %s -verify
 
 #define DO_PRAGMA _Pragma 
-DO_PRAGMA ("GCC dependency \"blahblabh\"")
+DO_PRAGMA ("GCC dependency \"blahblabh\"")  // expected-error {{file not found}}
 
