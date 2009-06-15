6ed7d5964eade6c3f0308a1ef1df98641ef1e526
Chris Lattner <sabre@nondot.org>
2009-06-15 05:18:27 +0000

    Minor tweak to -fdiagnostics-print-source-range-info to make it print ranges more similar to the console output.  Consider:
    #define FOO(X, Y) X/ Y
    
    void foo(int *P, int *Q) {
      FOO(P, Q);
    }
    
    Before we emitted:
    
    t.c:4:3:{4:3-4:6}{4:3-4:6}: error: invalid operands to binary expression ('int *' and 'int *')
       FOO(P, Q);
       ^~~~~~~~~
    ...
    
    Note that while we underline the macro args that the range info just includes FOO 
    without its macros.  This change teaches the printed ranges to include macro args
    also so that we get:
    
    t.c:4:3:{4:3-4:12}{4:3-4:12}: error: invalid operands to binary expression ('int *' and 'int *')
       FOO(P, Q);
       ^~~~~~~~~
    ...
    
    This fixes rdar://6939599
    
    llvm-svn: 73378

--- clang/lib/Frontend/TextDiagnosticPrinter.cpp
+++ clang/lib/Frontend/TextDiagnosticPrinter.cpp
@@ -640,9 +640,18 @@ void TextDiagnosticPrinter::HandleDiagnostic(Diagnostic::Level Level,
 
           SourceLocation B = Info.getRange(i).getBegin();
           SourceLocation E = Info.getRange(i).getEnd();
-          std::pair<FileID, unsigned> BInfo=SM.getDecomposedInstantiationLoc(B);
-          
+          B = SM.getInstantiationLoc(B);
           E = SM.getInstantiationLoc(E);
+          
+          // If the End location and the start location are the same and are a
+          // macro location, then the range was something that came from a macro
+          // expansion or _Pragma.  If this is an object-like macro, the best we
+          // can do is to highlight the range.  If this is a function-like
+          // macro, we'd also like to highlight the arguments.
+          if (B == E && Info.getRange(i).getEnd().isMacroID())
+            E = SM.getInstantiationRange(Info.getRange(i).getEnd()).second;
+
+          std::pair<FileID, unsigned> BInfo = SM.getDecomposedLoc(B);
           std::pair<FileID, unsigned> EInfo = SM.getDecomposedLoc(E);
           
           // If the start or end of the range is in another file, just discard
