ffbc629cd79c4c6ebc953c32f83604f54b22575e
Douglas Gregor <dgregor@apple.com>
2009-06-16 23:37:56 +0000

    Make these driver tests do the right thing even when MACOSX_DEPLOYMENT_TARGET is set.
    llvm-svn: 73583

--- clang/test/Driver/analyze.c
+++ clang/test/Driver/analyze.c
@@ -1,9 +1,10 @@
 // Verify that the analyzer gets the same flags as normal compilation
 // (at least for a few key ones).
 
+// RUN: env MACOSX_DEPLOYMENT_TARGET=10.5 &&
 // RUN: clang -ccc-host-triple i386-apple-darwin9  -### --analyze -o /dev/null %s -msse 2> %t.log &&
 // RUN: grep '"-analyze"' %t.log &&
 // RUN: grep '"--fmath-errno=0"' %t.log &&
 // RUN: grep '"-target-feature" "+sse"' %t.log &&
-// RUN: grep '"-mmacosx-version-min=10.5.0"' %t.log
+// RUN: grep '"-mmacosx-version-min=10.5"' %t.log
 
