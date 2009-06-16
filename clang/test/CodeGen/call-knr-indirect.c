943658f00b28ff68df2319d8c23bf304d5316657
Chris Lattner <sabre@nondot.org>
2009-06-16 17:39:02 +0000

    testcase for r72869, it turns out that it happens in Ruby.
    llvm-svn: 73514

---
+++ clang/test/CodeGen/call-knr-indirect.c
@@ -0,0 +1,11 @@
+// RUN: clang %s -O0 -emit-llvm -S -o - | grep 'call.*rb_define_global_function'
+// This should call rb_define_global_function, not rb_f_chop.
+
+void rb_define_global_function (const char*,void(*)(),int);
+static void rb_f_chop();
+void Init_String() {
+  rb_define_global_function("chop", rb_f_chop, 0);
+}
+static void rb_f_chop() {
+}
+
