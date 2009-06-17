d04acaad93fbeb0d9a817ecdfc9ae071ad7e6ffe
Douglas Gregor <dgregor@apple.com>
2009-06-17 15:41:17 +0000

    Use env properly in test/Driver/analyze.c
    llvm-svn: 73609

--- clang/test/Driver/analyze.c
+++ clang/test/Driver/analyze.c
@@ -1,8 +1,7 @@
 // Verify that the analyzer gets the same flags as normal compilation
 // (at least for a few key ones).
 
-// RUN: env MACOSX_DEPLOYMENT_TARGET=10.5 &&
-// RUN: clang -ccc-host-triple i386-apple-darwin9  -### --analyze -o /dev/null %s -msse 2> %t.log &&
+// RUN: env MACOSX_DEPLOYMENT_TARGET=10.5 clang -ccc-host-triple i386-apple-darwin9  -### --analyze -o /dev/null %s -msse 2> %t.log &&
 // RUN: grep '"-analyze"' %t.log &&
 // RUN: grep '"--fmath-errno=0"' %t.log &&
 // RUN: grep '"-target-feature" "+sse"' %t.log &&
