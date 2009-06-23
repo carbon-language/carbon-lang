c035bf688adb22fe889cbb8b3b3933683641f2dd
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2009-06-23 00:42:21 +0000

    Remove operator '<=' overload for SourceLocation.
    llvm-svn: 73933

--- clang/include/clang/Basic/SourceLocation.h
+++ clang/include/clang/Basic/SourceLocation.h
@@ -145,10 +145,6 @@ inline bool operator<(const SourceLocation &LHS, const SourceLocation &RHS) {
   return LHS.getRawEncoding() < RHS.getRawEncoding();
 }
 
-inline bool operator<=(const SourceLocation &LHS, const SourceLocation &RHS) {
-  return LHS.getRawEncoding() <= RHS.getRawEncoding();
-}
-
 /// SourceRange - a trival tuple used to represent a source range.
 class SourceRange {
   SourceLocation B;
