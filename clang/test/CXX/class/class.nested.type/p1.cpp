829574edabe27ff43e09f38bc563d6fa87f5950b
Anders Carlsson <andersca@mac.com>
2009-06-24 00:30:37 +0000

    Add test for [class.nested.type]p1
    llvm-svn: 74033

---
+++ clang/test/CXX/class/class.nested.type/p1.cpp
@@ -0,0 +1,11 @@
+class X {
+public:
+  typedef int I;
+  class Y { };
+  I a;
+};
+
+I b; // expected-error{{unknown type name 'I'}}
+Y c; // expected-error{{unknown type name 'Y'}}
+X::Y d;
+X::I e;
