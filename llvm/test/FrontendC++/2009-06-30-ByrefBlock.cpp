e1cd1b5d2a21825e9d7f9cfb59d07fb6404b766e
Stuart Hastings <stuart@apple.com>
2009-07-06 15:36:23 +0000

    Mark this test as Darwin only.  Patch by Bill Wendling.
    llvm-svn: 74829

--- llvm/test/FrontendC++/2009-06-30-ByrefBlock.cpp
+++ llvm/test/FrontendC++/2009-06-30-ByrefBlock.cpp
@@ -1,6 +1,9 @@
-// Insure __block_holder_tmp is allocated on the stack.
+// Insure __block_holder_tmp is allocated on the stack.  Darwin only.
 // RUN: %llvmgxx %s -S -O2 -o - | egrep {__block_holder_tmp.*alloca}
+// XFAIL: *
+// XTARGET: darwin
 // <rdar://problem/5865221>
+// END.
 extern void fubar_dispatch_sync(void (^PP)(void));
 void fubar() {
   __block void *voodoo;
