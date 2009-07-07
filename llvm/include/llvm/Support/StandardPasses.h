64eafd43ffac198c07337c3ffc1326f7f5d10c73
Devang Patel <dpatel@apple.com>
2009-07-07 22:09:42 +0000

    Disable loop index split for now and let nightly tester verify its usefulness.
    llvm-svn: 74955

--- llvm/include/llvm/Support/StandardPasses.h
+++ llvm/include/llvm/Support/StandardPasses.h
@@ -129,7 +129,6 @@ namespace llvm {
       PM->add(createLoopRotatePass());            // Rotate Loop
       PM->add(createLICMPass());                  // Hoist loop invariants
       PM->add(createLoopUnswitchPass(OptimizeSize));
-      PM->add(createLoopIndexSplitPass());        // Split loop index
       PM->add(createInstructionCombiningPass());  
       PM->add(createIndVarSimplifyPass());        // Canonicalize indvars
       PM->add(createLoopDeletionPass());          // Delete dead loops
