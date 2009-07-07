7a87a78c6c4bf0a76bc42fa7ff78b20a2c083d4c
Bill Wendling <isanbard@gmail.com>
2009-07-07 21:53:07 +0000

    DWARF requires frame moves be specified at specific times. If you have a prologue like this:
    __Z3fooi:
    Leh_func_begin1:
    LBB1_0: ## entry
            pushl   %ebp
    Llabel1:
            movl    %esp, %ebp
    Llabel2:
            pushl   %esi
    Llabel3:
            subl    $20, %esp
            call    "L1$pb"
    "L1$pb":
            popl    %esi
    
    The "pushl %ebp" needs a table entry specifying the offset. The "movl %esp,
    %ebp" makes %ebp the new stack frame register, so that needs to be specified in
    DWARF. And "pushl %esi" saves the callee-saved %esi register, which also needs
    to be specified in DWARF.
    
    Before, all of this logic was in one method. This didn't work too well, because
    as you can see there are multiple FDE line entries that need to be created.
    
    This fix creates the "MachineMove" objects directly when they're needed; instead
    of waiting until the end, and losing information.
    
    llvm-svn: 74952

--- llvm/lib/Target/X86/X86RegisterInfo.h
+++ llvm/lib/Target/X86/X86RegisterInfo.h
@@ -136,11 +136,9 @@ public:
   void processFunctionBeforeCalleeSavedScan(MachineFunction &MF,
                                             RegScavenger *RS = NULL) const;
 
+  void emitCalleeSavedFrameMoves(MachineFunction &MF, unsigned LabelId) const;
   void emitPrologue(MachineFunction &MF) const;
   void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const;
-
-  void emitFrameMoves(MachineFunction &MF,
-                      unsigned FrameLabelId, unsigned ReadyLabelId) const;
 
   // Debug information queries.
   unsigned getRARegister() const;
