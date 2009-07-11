0d14291fbfa144ca58b0dbad969b767f787d4252
Anders Carlsson <andersca@mac.com>
2009-07-11 01:00:52 +0000

    Fix a test.
    llvm-svn: 75327

--- clang/test/SemaCXX/auto-cxx0x.cpp
+++ clang/test/SemaCXX/auto-cxx0x.cpp
@@ -1,5 +1,5 @@
 // RUN: clang-cc -fsyntax-only -verify %s -std=c++0x
 void f() {
-  auto int a; // expected-error{{cannot combine with previous 'auto' declaration specifier}}
+  auto int a; // expected-error{{cannot combine with previous 'auto' declaration specifier}} // expected-error{{declaration of variable 'a' with type 'auto' requires an initializer}}
   int auto b; // expected-error{{cannot combine with previous 'int' declaration specifier}}
 }
