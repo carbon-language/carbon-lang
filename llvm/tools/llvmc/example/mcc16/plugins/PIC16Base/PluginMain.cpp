63bdb562741499a2658ce917a1a87008d35ceb16
Sanjiv Gupta <sanjiv.gupta@microchip.com>
2009-07-13 10:58:55 +0000

    Added a fixme for platform specific GetDirSeparator().
    llvm-svn: 75461

--- llvm/tools/llvmc/example/mcc16/plugins/PIC16Base/PluginMain.cpp
+++ llvm/tools/llvmc/example/mcc16/plugins/PIC16Base/PluginMain.cpp
@@ -10,6 +10,8 @@ namespace llvmc {
 }
 
 // Returns the platform specific directory separator via #ifdefs.
+// FIXME: This currently work on linux and windows only. It does not 
+// work on other unices. 
 static std::string GetDirSeparator(void) {
 #ifdef __linux__
   return "/";
