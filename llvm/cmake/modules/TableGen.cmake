cb234fec500e6137d388493dd1d7990bfe344b73
Oscar Fuentes <ofv@wanadoo.es>
2009-07-13 21:35:00 +0000

    CMake: tablegen: Add .td files under the inlude/llvm directory hierarchy to the list of dependencies.
    llvm-svn: 75510

--- llvm/cmake/modules/TableGen.cmake
+++ llvm/cmake/modules/TableGen.cmake
@@ -3,14 +3,15 @@
 # Adds the name of the generated file to TABLEGEN_OUTPUT.
 
 macro(tablegen ofn)
-  file(GLOB all_tds "*.td")
+  file(GLOB local_tds "*.td")
+  file(GLOB_RECURSE global_tds "${LLVM_MAIN_SRC_DIR}/include/llvm/*.td")
 
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp
     COMMAND ${LLVM_TABLEGEN_EXE} ${ARGN} -I ${CMAKE_CURRENT_SOURCE_DIR}
     -I ${LLVM_MAIN_SRC_DIR}/lib/Target -I ${LLVM_MAIN_INCLUDE_DIR}
     ${CMAKE_CURRENT_SOURCE_DIR}/${LLVM_TARGET_DEFINITIONS} 
     -o ${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp
-    DEPENDS tblgen ${all_tds}
+    DEPENDS tblgen ${local_tds} ${global_tds}
     COMMENT "Building ${ofn}.tmp..."
     )
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}
