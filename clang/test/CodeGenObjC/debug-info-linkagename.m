450f26609593d1c2c52762be26f5670a35f166a3
Devang Patel <dpatel@apple.com>
2009-07-13 21:20:20 +0000

    New testcase for rev 75503.
    llvm-svn: 75504

---
+++ clang/test/CodeGenObjC/debug-info-linkagename.m
@@ -0,0 +1,18 @@
+// RUN: clang-cc  -g -S -o %t %s
+// RUN: not grep 001 %t
+
+
+@interface F 
+-(int) bar;
+@end
+
+@implementation F
+-(int) bar {
+	return 42;
+}
+@end
+
+extern int f(F *fn) {
+	return [fn bar];
+}
+	
