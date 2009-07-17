c3ccd180e7b5fe8c47d3c2a6150516fa64eb2123
Daniel Dunbar <daniel@zuster.org>
2009-07-17 18:10:27 +0000

    Fix indentation / trailing white space.
    llvm-svn: 76200

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -34,7 +34,7 @@ class DriverDiagnosticPrinter : public DiagnosticClient {
   llvm::raw_ostream &OS;
 
 public:
-  DriverDiagnosticPrinter(const std::string _ProgName, 
+  DriverDiagnosticPrinter(const std::string _ProgName,
                           llvm::raw_ostream &_OS)
     : ProgName(_ProgName),
       OS(_OS) {}
@@ -54,7 +54,7 @@ void DriverDiagnosticPrinter::HandleDiagnostic(Diagnostic::Level Level,
   case Diagnostic::Error:   OS << "error: "; break;
   case Diagnostic::Fatal:   OS << "fatal error: "; break;
   }
-  
+
   llvm::SmallString<100> OutStr;
   Info.FormatDiagnostic(OutStr);
   OS.write(OutStr.begin(), OutStr.size());
@@ -68,7 +68,7 @@ llvm::sys::Path GetExecutablePath(const char *Argv0) {
   return llvm::sys::Path::GetMainExecutable(Argv0, P);
 }
 
-static const char *SaveStringInSet(std::set<std::string> &SavedStrings, 
+static const char *SaveStringInSet(std::set<std::string> &SavedStrings,
                                    const std::string &S) {
   return SavedStrings.insert(S).first->c_str();
 }
@@ -101,54 +101,54 @@ static const char *SaveStringInSet(std::set<std::string> &SavedStrings,
 /// \param Edit - The override command to perform.
 /// \param SavedStrings - Set to use for storing string representations.
 void ApplyOneQAOverride(llvm::raw_ostream &OS,
-                        std::vector<const char*> &Args, 
+                        std::vector<const char*> &Args,
                         const std::string &Edit,
                         std::set<std::string> &SavedStrings) {
   // This does not need to be efficient.
 
-   if (Edit[0] == '^') {
-     const char *Str = 
-       SaveStringInSet(SavedStrings, Edit.substr(1, std::string::npos));
-     OS << "### Adding argument " << Str << " at beginning\n";
-     Args.insert(Args.begin() + 1, Str);
-   } else if (Edit[0] == '+') {
-     const char *Str = 
-       SaveStringInSet(SavedStrings, Edit.substr(1, std::string::npos));
-     OS << "### Adding argument " << Str << " at end\n";
-     Args.push_back(Str);
-   } else if (Edit[0] == 'x' || Edit[0] == 'X') {
-     std::string Option = Edit.substr(1, std::string::npos);
-     for (unsigned i = 1; i < Args.size();) {
-       if (Option == Args[i]) {
-         OS << "### Deleting argument " << Args[i] << '\n';
-         Args.erase(Args.begin() + i);
-         if (Edit[0] == 'X') {
-           if (i < Args.size()) {
-             OS << "### Deleting argument " << Args[i] << '\n';
-             Args.erase(Args.begin() + i);
-           } else
-             OS << "### Invalid X edit, end of command line!\n";
-         }
-       } else
-         ++i;
-     }
-   } else if (Edit[0] == 'O') {
-     for (unsigned i = 1; i < Args.size();) {
-       const char *A = Args[i];
-       if (A[0] == '-' && A[1] == 'O' && 
-           (A[2] == '\0' || 
-            (A[3] == '\0' && (A[2] == 's' || A[2] == 'z' ||
-                              ('0' <= A[2] && A[2] <= '9'))))) {
-         OS << "### Deleting argument " << Args[i] << '\n';
-         Args.erase(Args.begin() + i);
-       } else
-         ++i;
-     }     
-     OS << "### Adding argument " << Edit << " at end\n";
-     Args.push_back(SaveStringInSet(SavedStrings, '-' + Edit));
-   } else {
-     OS << "### Unrecognized edit: " << Edit << "\n";
-   }
+  if (Edit[0] == '^') {
+    const char *Str =
+      SaveStringInSet(SavedStrings, Edit.substr(1, std::string::npos));
+    OS << "### Adding argument " << Str << " at beginning\n";
+    Args.insert(Args.begin() + 1, Str);
+  } else if (Edit[0] == '+') {
+    const char *Str =
+      SaveStringInSet(SavedStrings, Edit.substr(1, std::string::npos));
+    OS << "### Adding argument " << Str << " at end\n";
+    Args.push_back(Str);
+  } else if (Edit[0] == 'x' || Edit[0] == 'X') {
+    std::string Option = Edit.substr(1, std::string::npos);
+    for (unsigned i = 1; i < Args.size();) {
+      if (Option == Args[i]) {
+        OS << "### Deleting argument " << Args[i] << '\n';
+        Args.erase(Args.begin() + i);
+        if (Edit[0] == 'X') {
+          if (i < Args.size()) {
+            OS << "### Deleting argument " << Args[i] << '\n';
+            Args.erase(Args.begin() + i);
+          } else
+            OS << "### Invalid X edit, end of command line!\n";
+        }
+      } else
+        ++i;
+    }
+  } else if (Edit[0] == 'O') {
+    for (unsigned i = 1; i < Args.size();) {
+      const char *A = Args[i];
+      if (A[0] == '-' && A[1] == 'O' &&
+          (A[2] == '\0' ||
+           (A[3] == '\0' && (A[2] == 's' || A[2] == 'z' ||
+                             ('0' <= A[2] && A[2] <= '9'))))) {
+        OS << "### Deleting argument " << Args[i] << '\n';
+        Args.erase(Args.begin() + i);
+      } else
+        ++i;
+    }
+    OS << "### Adding argument " << Edit << " at end\n";
+    Args.push_back(SaveStringInSet(SavedStrings, '-' + Edit));
+  } else {
+    OS << "### Unrecognized edit: " << Edit << "\n";
+  }
 }
 
 /// ApplyQAOverride - Apply a comma separate list of edits to the
@@ -156,7 +156,7 @@ void ApplyOneQAOverride(llvm::raw_ostream &OS,
 void ApplyQAOverride(std::vector<const char*> &Args, const char *OverrideStr,
                      std::set<std::string> &SavedStrings) {
   llvm::raw_ostream *OS = &llvm::errs();
-  
+
   if (OverrideStr[0] == '#') {
     ++OverrideStr;
     OS = &llvm::nulls();
@@ -203,7 +203,7 @@ int main(int argc, const char **argv) {
 
     ApplyQAOverride(StringPointers, OverrideStr, SavedStrings);
 
-    C.reset(TheDriver.BuildCompilation(StringPointers.size(), 
+    C.reset(TheDriver.BuildCompilation(StringPointers.size(),
                                        &StringPointers[0]));
   } else if (const char *Cur = ::getenv("CCC_ADD_ARGS")) {
     std::vector<const char*> StringPointers;
@@ -213,7 +213,7 @@ int main(int argc, const char **argv) {
 
     for (;;) {
       const char *Next = strchr(Cur, ',');
-      
+
       if (Next) {
         StringPointers.push_back(SaveStringInSet(SavedStrings,
                                                  std::string(Cur, Next)));
@@ -227,7 +227,7 @@ int main(int argc, const char **argv) {
 
     StringPointers.insert(StringPointers.end(), argv + 1, argv + argc);
 
-    C.reset(TheDriver.BuildCompilation(StringPointers.size(), 
+    C.reset(TheDriver.BuildCompilation(StringPointers.size(),
                                        &StringPointers[0]));
   } else
     C.reset(TheDriver.BuildCompilation(argc, argv));
