e17cf4527c99c7de2c078d4fd940b862c73a493e
Dan Gohman <gohman@apple.com>
2009-07-17 19:01:15 +0000

    Define a no-pointer-overflow flag for GetElementPtr instructions.
    llvm-svn: 76218

--- llvm/include/llvm/Instructions.h
+++ llvm/include/llvm/Instructions.h
@@ -573,6 +573,23 @@ public:
   /// a constant offset between them.
   bool hasAllConstantIndices() const;
 
+  /// hasNoPointerOverflow - Return true if this GetElementPtr is known to
+  /// never have overflow in the pointer addition portions of its effective
+  /// computation. GetElementPtr computation involves several phases;
+  /// overflow can be considered to occur in index typecasting, array index
+  /// scaling, and the addition of the base pointer with offsets. This flag
+  /// only applies to the last of these. The operands are added to the base
+  /// pointer one at a time from left to right. This function returns false
+  /// if any of these additions results in an address value which is not
+  /// known to be within the allocated address space that the base pointer
+  /// points into, or within one element (of the original allocation) past
+  /// the end.
+  bool hasNoPointerOverflow() const {
+    return SubclassOptionalData & (1 << 0);
+  }
+  void setHasNoPointerOverflow(bool B) {
+    SubclassOptionalData = (SubclassOptionalData & ~(1 << 0)) | (B << 0);
+  }
 
   // Methods for support type inquiry through isa, cast, and dyn_cast:
   static inline bool classof(const GetElementPtrInst *) { return true; }
