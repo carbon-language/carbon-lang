f4464156cf956c1f89844f8db0dbcb773f535eb1
Mike Stump <mrs@apple.com>
2009-07-21 19:03:43 +0000

    Prep for new warning.
    llvm-svn: 76628

--- clang/test/CXX/class/class.local/p3.cpp
+++ clang/test/CXX/class/class.local/p3.cpp
@@ -24,7 +24,10 @@ void f2() {
 void f3(int a) { // expected-note{{'a' declared here}}
   struct X {
     struct Y {
-      int f() { return a; } // expected-error{{reference to local variable 'a' declared in enclosed function 'f3'}}
+      int f() {
+        return a; // expected-error{{reference to local variable 'a' declared in enclosed function 'f3'}}
+        return 1;
+      }
     };
   };
 }
