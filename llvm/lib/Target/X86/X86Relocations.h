637ca93fc486fffe307ac2ec916951ec59be846b
Chris Lattner <sabre@nondot.org>
2009-07-22 16:53:56 +0000

    doxygenify
    llvm-svn: 76750

--- llvm/lib/Target/X86/X86Relocations.h
+++ llvm/lib/Target/X86/X86Relocations.h
@@ -22,17 +22,17 @@ namespace llvm {
     /// the terminology here doesn't follow x86 convention - word means
     /// 32-bit and dword means 64-bit.
     enum RelocationType {
-      // reloc_pcrel_word - PC relative relocation, add the relocated value to
-      // the value already in memory, after we adjust it for where the PC is.
+      /// reloc_pcrel_word - PC relative relocation, add the relocated value to
+      /// the value already in memory, after we adjust it for where the PC is.
       reloc_pcrel_word = 0,
 
-      // reloc_picrel_word - PIC base relative relocation, add the relocated
-      // value to the value already in memory, after we adjust it for where the
-      // PIC base is.
+      /// reloc_picrel_word - PIC base relative relocation, add the relocated
+      /// value to the value already in memory, after we adjust it for where the
+      /// PIC base is.
       reloc_picrel_word = 1,
       
-      // reloc_absolute_word, reloc_absolute_dword - Absolute relocation, just
-      // add the relocated value to the value already in memory.
+      /// reloc_absolute_word, reloc_absolute_dword - Absolute relocation, just
+      /// add the relocated value to the value already in memory.
       reloc_absolute_word = 2,
       reloc_absolute_dword = 3
     };
