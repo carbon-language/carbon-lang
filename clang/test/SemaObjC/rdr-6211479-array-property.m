53fcbb89f9d32464c687bc44eb725883e9e49b55
Steve Naroff <snaroff@apple.com>
2009-07-29 15:24:47 +0000

    Remove an ObjC-specific XFAIL (and tweak test).
    llvm-svn: 77454

--- clang/test/SemaObjC/rdr-6211479-array-property.m
+++ clang/test/SemaObjC/rdr-6211479-array-property.m
@@ -1,9 +1,8 @@
 // RUN: clang-cc -fsyntax-only -verify %s
-// XFAIL
 // <rdar://problem/6211479>
 
 typedef int T[2];
 
 @interface A
-@property(assign) T p2; // expected-error {{FIXME: property has invalid type}}
+@property(assign) T p2; // expected-error {{property cannot have array or function type 'T' (aka 'int [2]')}}
 @end
