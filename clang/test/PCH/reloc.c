b616260624910242b91298303c3f2281ca019186
Daniel Dunbar <daniel@zuster.org>
2009-08-01 05:57:38 +0000

    Use %S, not `pwd`, and enable a line that *does* work.  - Doug, please check.
    llvm-svn: 77778

--- clang/test/PCH/reloc.c
+++ clang/test/PCH/reloc.c
@@ -1,6 +1,6 @@
-// RUN: clang-cc -emit-pch -o %t --relocatable-pch -isysroot `pwd`/libroot `pwd`/libroot/usr/include/reloc.h &&
-// RUN: clang-cc -include-pch %t -isysroot `pwd`/libroot %s -verify
-// FIXME (test harness can't do this?): not clang-cc -include-pch %t %s
+// RUN: clang-cc -emit-pch -o %t --relocatable-pch -isysroot %S/libroot %S/libroot/usr/include/reloc.h &&
+// RUN: clang-cc -include-pch %t -isysroot %S/libroot %s -verify &&
+// RUN: not clang-cc -include-pch %t %s
 
 #include <reloc.h>
 
