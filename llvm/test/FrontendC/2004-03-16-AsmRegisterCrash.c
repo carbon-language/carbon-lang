fd5d735e2b8fb5bc08d0df719887d4df0fcb00ba
Bill Wendling <isanbard@gmail.com>
2009-08-04 04:48:31 +0000

    llvm-gcc checks the static asm variable is valid in ValidateRegisterVariable. Make this work for PPC.
    llvm-svn: 78051

--- llvm/test/FrontendC/2004-03-16-AsmRegisterCrash.c
+++ llvm/test/FrontendC/2004-03-16-AsmRegisterCrash.c
@@ -1,6 +1,10 @@
 // RUN: %llvmgcc -S %s -o - | llvm-as -f -o /dev/null
 
 int foo() {
+#ifdef __ppc__
+  register int X __asm__("r1");
+#else
   register int X __asm__("ebx");
+#endif
   return X;
 }
