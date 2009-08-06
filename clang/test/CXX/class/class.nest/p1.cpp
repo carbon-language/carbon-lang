9de556c8b6c3f66cd21157754029c3ea13c9d7e5
John McCall <rjmccall@apple.com>
2009-08-06 00:50:46 +0000

    AlisdairM pointed out that this will likely be relaxed in C++0x, so let's make a note of it in the test case.
    llvm-svn: 78266

--- clang/test/CXX/class/class.nest/p1.cpp
+++ clang/test/CXX/class/class.nest/p1.cpp
@@ -4,6 +4,9 @@ class Outer {
   int x;
   static int sx;
 
+  // C++0x will likely relax this rule in this specific case, but
+  // we'll still need to enforce it in C++03 mode.  See N2253 (or
+  // successor).
   class Inner {
     static char a[sizeof(x)]; // expected-error {{ invalid use of nonstatic data member 'x' }}
     static char b[sizeof(sx)]; // okay
