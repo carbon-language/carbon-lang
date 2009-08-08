4a4e4487fccdd30ddf9ffd01ba010a881fe5e4ee
Chris Lattner <sabre@nondot.org>
2009-08-08 20:55:25 +0000

    make PIC16 create its own custom MCSection.
    llvm-svn: 78501

---
+++ llvm/lib/Target/PIC16/PIC16Section.h
@@ -0,0 +1,35 @@
+//===- PIC16Section.h - PIC16-specific section representation ---*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the MCSection class.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_PIC16SECTION_H
+#define LLVM_PIC16SECTION_H
+
+#include "llvm/MC/MCSection.h"
+
+namespace llvm {
+
+  class MCSectionPIC16 : public MCSection {
+    MCSectionPIC16(const StringRef &Name, bool IsDirective, SectionKind K,
+                 MCContext &Ctx) : MCSection(Name, IsDirective, K, Ctx) {}
+  public:
+    
+    static MCSectionPIC16 *Create(const StringRef &Name, bool IsDirective, 
+                                  SectionKind K, MCContext &Ctx);
+    
+  };
+  
+  
+
+} // end namespace llvm
+
+#endif
