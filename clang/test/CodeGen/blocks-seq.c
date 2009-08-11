d362857f4c93e35d1729d9c31e73dbc9ab500096
Daniel Dunbar <daniel@zuster.org>
2009-08-11 18:37:40 +0000

    Update test case.
    llvm-svn: 78699

--- clang/test/CodeGen/blocks-seq.c
+++ clang/test/CodeGen/blocks-seq.c
@@ -1,11 +1,11 @@
 // RUN: clang-cc -fblocks -triple x86_64-apple-darwin10 -emit-llvm -o %t %s &&
 // RUN: grep '%call = call i32 (...)\* @rhs()' %t | count 1 &&
 // If this fails, see about sliding %4, %5, %6 and %7...
-// RUN: grep '%forwarding1 = getelementptr %0\* %i, i32 0, i32 1' %t | count 1 &&
+// RUN: grep '%forwarding1 = getelementptr inbounds %0\* %i, i32 0, i32 1' %t | count 1 &&
 // RUN: grep '%4 = bitcast i8\*\* %forwarding1 to %0\*\*' %t | count 1 &&
 // RUN: grep '%5 = load %0\*\* %4' %t | count 1 &&
 // RUN: grep '%call2 = call i32 (...)\* @rhs()' %t | count 1 &&
-// RUN: grep '%forwarding3 = getelementptr %0\* %i, i32 0, i32 1' %t | count 1 &&
+// RUN: grep '%forwarding3 = getelementptr inbounds %0\* %i, i32 0, i32 1' %t | count 1 &&
 // RUN: grep '%6 = bitcast i8\*\* %forwarding3 to %0\*\*' %t | count 1 &&
 // RUN: grep '%7 = load %0\*\* %6' %t | count 1
 
