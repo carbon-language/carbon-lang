f9d12f23cb822fa4997b7f7f791ef95103362821
Dan Gohman <gohman@apple.com>
2009-08-11 19:59:21 +0000

    Use wchar.h to get wchar_t, not ctype.h.
    llvm-svn: 78703

--- llvm/test/FrontendC/wchar-const.c
+++ llvm/test/FrontendC/wchar-const.c
@@ -1,6 +1,6 @@
 // RUN: %llvmgcc -S %s -o - | grep {constant \\\[18 x} | grep { 84, }
 // This should pass for any endianness combination of host and target.
-#include <ctype.h>
+#include <wchar.h>
 extern void foo(const wchar_t* p);
 int main (int argc, const char * argv[])
 {
