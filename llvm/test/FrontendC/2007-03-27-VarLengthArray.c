a3be3d7a8967a09af9f4b7456ee53ddef1317bc0
Dan Gohman <gohman@apple.com>
2009-08-12 00:35:55 +0000

    llvm-gcc now emits inbounds for this getelementptr.
    llvm-svn: 78758

--- llvm/test/FrontendC/2007-03-27-VarLengthArray.c
+++ llvm/test/FrontendC/2007-03-27-VarLengthArray.c
@@ -1,4 +1,4 @@
-// RUN: %llvmgcc -S %s -o - | grep {getelementptr \\\[0 x i32\\\]}
+// RUN: %llvmgcc -S %s -o - | grep {getelementptr inbounds \\\[0 x i32\\\]}
 extern void f(int *);
 int e(int m, int n) {
   int x[n];
