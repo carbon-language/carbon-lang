b20757bdeb97d3615e7db90dd02d5a24e0cf6489
Dan Gohman <gohman@apple.com>
2009-08-15 21:41:03 +0000

    Mingw also doesn't have st_blksize.
    llvm-svn: 79142

--- llvm/lib/Support/raw_ostream.cpp
+++ llvm/lib/Support/raw_ostream.cpp
@@ -372,7 +372,7 @@ uint64_t raw_fd_ostream::seek(uint64_t off) {
 }
 
 size_t raw_fd_ostream::preferred_buffer_size() {
-#if !defined(_MSC_VER) // Windows reportedly doesn't have st_blksize.
+#if !defined(_MSC_VER) && !defined(__MINGW32__) // Windows has no st_blksize.
   assert(FD >= 0 && "File not yet open!");
   struct stat statbuf;
   if (fstat(FD, &statbuf) == 0) {
