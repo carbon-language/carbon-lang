47a31a29c58608cf1c2940240744fb6eac560dba
Dan Gohman <gohman@apple.com>
2009-08-18 00:20:06 +0000

    Fix function alignment at -Os on x86 to be 1, not 2. getFunctionAlignment returns a log2 value.
    llvm-svn: 79293

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -1052,7 +1052,7 @@ SDValue X86TargetLowering::getPICJumpTableRelocBase(SDValue Table,
 
 /// getFunctionAlignment - Return the Log2 alignment of this function.
 unsigned X86TargetLowering::getFunctionAlignment(const Function *F) const {
-  return F->hasFnAttr(Attribute::OptimizeForSize) ? 1 : 4;
+  return F->hasFnAttr(Attribute::OptimizeForSize) ? 0 : 4;
 }
 
 //===----------------------------------------------------------------------===//
