d04405f036903adcc9e667c4f4e79309fb6f6ef7
Daniel Dunbar <daniel@zuster.org>
2009-08-18 07:07:14 +0000

    Add test for finding bfin backend.
    llvm-svn: 79326

--- clang/test/Coverage/targets.c
+++ clang/test/Coverage/targets.c
@@ -1,5 +1,6 @@
 // RUN: clang-cc -g -triple armv6-apple-darwin9 -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple armv6-unknown-unknown -emit-llvm -o %t %s &&
+// RUN: clang-cc -g -triple bfin-unknown-unknown -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple i686-apple-darwin9 -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple i686-pc-linux-gnu -emit-llvm -o %t %s &&
 // RUN: clang-cc -g -triple i686-unknown-dragonfly -emit-llvm -o %t %s &&
