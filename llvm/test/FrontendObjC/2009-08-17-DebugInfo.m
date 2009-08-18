d7f34337895312465d3e5b98fe98d6cc0ef83ddd
Daniel Dunbar <daniel@zuster.org>
2009-08-18 17:38:27 +0000

    Attempt to normalize test results.
    llvm-svn: 79350

--- llvm/test/FrontendObjC/2009-08-17-DebugInfo.m
+++ llvm/test/FrontendObjC/2009-08-17-DebugInfo.m
@@ -5,7 +5,7 @@
 // RUN: %link %t.o -o %t.exe -framework Foundation
 // RUN: echo {break randomFunc\n} > %t.in 
 // RUN: gdb -q -batch -n -x %t.in %t.exe | tee %t.out | \
-// RUN:   grep {Breakpoint 1 at 0x.*: file 2009-08-17-DebugInfo.m, line 22}
+// RUN:   grep {Breakpoint 1 at 0x.*: file 2009-08-17-DebugInfo.m, line 21}
 // XTARGETS: darwin
 @interface MyClass
 {
@@ -18,9 +18,7 @@
 @implementation MyClass
 + init {
 }
-- randomFunc { 
-  my = 42;
-}
+- randomFunc { my = 42; }
 @end
 
 int main() {
