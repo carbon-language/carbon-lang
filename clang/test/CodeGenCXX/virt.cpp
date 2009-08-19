6bc4aac8783d9fd393520fa2f772ce617a207530
Mike Stump <mrs@apple.com>
2009-08-19 13:36:31 +0000

    Update for recent improvements.
    llvm-svn: 79421

--- clang/test/CodeGenCXX/virt.cpp
+++ clang/test/CodeGenCXX/virt.cpp
@@ -241,7 +241,7 @@ struct test5_D  : virtual test5_B1, virtual test5_B21, virtual test5_B31 {
 // CHECK-LP32: .long 4
 // CHECK-LP32-NEXT: .space 4
 // CHECK-LP32-NEXT: .space 4
-// CHECK-LP32: .long 4294967288
+// CHECK-LP32-NEXT: .long 4294967288
 // CHECK-LP32-NEXT: .long __ZTI7test5_D
 // CHECK-LP32-NEXT: .long __ZN9test5_B337funcB33Ev
 // CHECK-LP32-NEXT: .long __ZN9test5_B327funcB32Ev
@@ -293,7 +293,7 @@ struct test5_D  : virtual test5_B1, virtual test5_B21, virtual test5_B31 {
 // CHECK-LP64: .quad 8
 // CHECK-LP64-NEXT: .space 8
 // CHECK-LP64-NEXT: .space 8
-// CHECK-LP64: .quad 18446744073709551600
+// CHECK-LP64-NEXT: .quad 18446744073709551600
 // CHECK-LP64-NEXT: .quad __ZTI7test5_D
 // CHECK-LP64-NEXT: .quad __ZN9test5_B337funcB33Ev
 // CHECK-LP64-NEXT: .quad __ZN9test5_B327funcB32Ev
