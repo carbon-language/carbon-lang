84d3389d7d075511ed310b74c995c9046260c897
Bill Wendling <isanbard@gmail.com>
2009-08-21 08:02:19 +0000

    Probably should emit bitcode instead of text.
    llvm-svn: 79607

--- llvm/test/FrontendC/ptr-rotate.c
+++ llvm/test/FrontendC/ptr-rotate.c
@@ -1,5 +1,5 @@
 // RUN: %llvmgcc %s -c -m32 -o /dev/null
-// RUN: %llvmgcc %s -S -O1 -m32 -emit-llvm -o - | llc -mtriple=i386-apple-darwin9.7 | FileCheck %s -check-prefix=DARWIN
+// RUN: %llvmgcc %s -c -O1 -m32 -emit-llvm -o - | llc -mtriple=i386-apple-darwin9.7 | FileCheck %s -check-prefix=DARWIN
 
 unsigned int func(void *A) {
   // DARWIN: roll $27
