2b9f0d100bd3a0f86614d747689a05028d864a21
Chris Lattner <sabre@nondot.org>
2009-08-22 20:58:17 +0000

    move the MCAsmInfo .cpp/.h files into the right directories and rename them.
    llvm-svn: 79768

--- llvm/include/llvm/MC/MCAsmInfoCOFF.h
+++ llvm/include/llvm/MC/MCAsmInfoCOFF.h
@@ -1,2 +1,24 @@
-#include "llvm/Target/COFFTargetAsmInfo.h"
+//===-- MCAsmInfoCOFF.h - COFF asm properties -------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
 
+#ifndef LLVM_COFF_TARGET_ASM_INFO_H
+#define LLVM_COFF_TARGET_ASM_INFO_H
+
+#include "llvm/MC/MCAsmInfo.h"
+
+namespace llvm {
+  class COFFMCAsmInfo : public MCAsmInfo {
+  protected:
+    explicit COFFMCAsmInfo();
+      
+  };
+}
+
+
+#endif // LLVM_COFF_TARGET_ASM_INFO_H
