b60d87c5172e402bb804dbd7bf603a9b4865663b
Ted Kremenek <kremenek@apple.com>
2009-08-26 22:36:44 +0000

    Add initial boilerplate for CIndex, a shared library that will vend high-level source symbol information (harvested by Clang) through a C API.
    llvm-svn: 80166

---
+++ clang/include/clang-c/Index.h
@@ -0,0 +1,33 @@
+/*===-- clang-c/Index.h - Indexing Public C Interface -------------*- C -*-===*\
+|*                                                                            *|
+|*                     The LLVM Compiler Infrastructure                       *|
+|*                                                                            *|
+|* This file is distributed under the University of Illinois Open Source      *|
+|* License. See LICENSE.TXT for details.                                      *|
+|*                                                                            *|
+|*===----------------------------------------------------------------------===*|
+|*                                                                            *|
+|* This header provides a public inferface to a Clang library for extracting  *|
+|* high-level symbol information from source files without exposing the full  *|
+|* Clang C++ API.                                                             *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#ifndef CLANG_C_INDEX_H
+#define CLANG_C_INDEX_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Dummy function that serves as an entrance to the library.
+ * To be replaced with specific functions.
+ */
+void clang_index_dummy(void);
+
+#ifdef __cplusplus
+}
+#endif
+#endif
+
---
+++ clang/tools/CIndex/CIndex.cpp
@@ -0,0 +1,17 @@
+//===- CIndex.cpp - Clang-C Source Indexing Library -----------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+// 
+//===----------------------------------------------------------------------===//
+//
+// This file implements the Clang-C Source Indexing library.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang-c/Index.h"
+
+void clang_index_dummy(void) {}
+
