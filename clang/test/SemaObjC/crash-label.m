18f4107eb7e66784f70d5914453444bd9bc48bc7
Fariborz Jahanian <fjahanian@apple.com>
2009-08-28 17:53:05 +0000

    test case for my last patch.
    llvm-svn: 80382

---
+++ clang/test/SemaObjC/crash-label.m
@@ -0,0 +1,9 @@
+// RUN: clang-cc  -fsyntax-only -verify %s
+
+  - (NSDictionary*) _executeScript:(NSString *)source { // expected-error 2 {{expected a type}} \
+							// expected-error {{missing context for method declaration}} 
+                          Exit:  [nilArgs release]; // expected-error {{use of undeclared identifier}}
+                          }
+                                     - (NSDictionary *) _setupKernelStandardMode:(NSString *)source { // expected-error 2 {{expected a type}} \
+expected-error {{missing context for method declaration}}
+                           Exit:   if(_ciKernel && !success ) { // expected-error {{use of undeclared identifier}} // expected-error 2 {{expected}}
