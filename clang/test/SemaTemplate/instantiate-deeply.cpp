9dc2aa0c454260a4ce22bb6673459a49211cf131
John McCall <rjmccall@apple.com>
2009-08-29 08:20:44 +0000

    Test case.
    llvm-svn: 80426

---
+++ clang/test/SemaTemplate/instantiate-deeply.cpp
@@ -0,0 +1,22 @@
+// RUN: clang-cc -fsyntax-only -Wall -verify %s
+
+template<typename a> struct A {
+  template <typename b> struct B {
+    template <typename c> struct C {
+      template <typename d> struct D {
+        template <typename e> struct E {
+          e field;
+          E() : field(0) {
+            d v1 = 4;
+            c v2 = v1 * v1;
+            b v3 = 8;
+            a v4 = v3 * v3;
+            field += v2 + v4;
+          }
+        };
+      };
+    };
+  };
+};
+
+A<int>::B<int>::C<int>::D<int>::E<int> global;
