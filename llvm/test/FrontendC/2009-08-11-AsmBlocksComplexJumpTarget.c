a1aed7bb40496ec2a727d334f5804b04e2b27fc0
Dale Johannesen <dalej@apple.com>
2009-08-31 17:49:20 +0000

    Mark test as passing on all x86, which it should, although I don't think anyone cares about this feature except Darwin.  PR 4825.
    llvm-svn: 80596

--- llvm/test/FrontendC/2009-08-11-AsmBlocksComplexJumpTarget.c
+++ llvm/test/FrontendC/2009-08-11-AsmBlocksComplexJumpTarget.c
@@ -1,7 +1,7 @@
 // RUN: %llvmgcc %s -fasm-blocks -S -o - | grep {\\\*1192}
 // Complicated expression as jump target
 // XFAIL: *
-// XTARGET: darwin
+// XTARGET: x86,i386,i686
 
 asm void Method3()
 {
