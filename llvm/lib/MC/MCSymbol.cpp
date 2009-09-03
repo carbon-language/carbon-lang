1945453843b3bec348176da6eda042f73fe35bbd
Chris Lattner <sabre@nondot.org>
2009-09-03 05:57:47 +0000

    fix MCSymbol printing to exactly match the normal mangler rules so we can diff .s files.
    llvm-svn: 80894

--- llvm/lib/MC/MCSymbol.cpp
+++ llvm/lib/MC/MCSymbol.cpp
@@ -8,41 +8,45 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/MC/MCSymbol.h"
+#include "llvm/MC/MCAsmInfo.h"
 #include "llvm/Support/raw_ostream.h"
-
 using namespace llvm;
 
 // Sentinel value for the absolute pseudo section.
 const MCSection *MCSymbol::AbsolutePseudoSection =
   reinterpret_cast<const MCSection *>(1);
 
-/// NeedsQuoting - Return true if the string \arg Str needs quoting, i.e., it
-/// does not match [a-zA-Z_.][a-zA-Z0-9_.]*.
-//
-// FIXME: This could be more permissive, do we care?
-static inline bool NeedsQuoting(const StringRef &Str) {
+/// ShouldQuoteIdentifier - Return true if the identifier \arg Str needs quotes
+/// for this assembler.
+static bool ShouldQuoteIdentifier(const StringRef &Str, const MCAsmInfo &MAI) {
+  // If the assembler doesn't support quotes, never use them.
+  if (!MAI.doesAllowQuotesInName())
+    return false;
+  
+  // If empty, we need quotes.
   if (Str.empty())
     return true;
-
-  // Check that first character is in [a-zA-Z_.].
-  if (!((Str[0] >= 'a' && Str[0] <= 'z') ||
-        (Str[0] >= 'A' && Str[0] <= 'Z') ||
-        (Str[0] == '_' || Str[0] == '.')))
+  
+  // If the first character is a number, we need quotes.
+  if (Str[0] >= '0' && Str[0] <= '9')
     return true;
 
-  // Check subsequent characters are in [a-zA-Z0-9_.].
-  for (unsigned i = 1, e = Str.size(); i != e; ++i)
-    if (!((Str[i] >= 'a' && Str[i] <= 'z') ||
-          (Str[i] >= 'A' && Str[i] <= 'Z') ||
-          (Str[i] >= '0' && Str[i] <= '9') ||
-          (Str[i] == '_' || Str[i] == '.')))
+  // If any of the characters in the string is an unacceptable character, force
+  // quotes.
+  for (unsigned i = 0, e = Str.size(); i != e; ++i) {
+    char C = Str[i];
+  
+    if ((C < 'a' || C > 'z') &&
+        (C < 'A' || C > 'Z') &&
+        (C < '0' || C > '9') &&
+        C != '_' && C != '$' && C != '.')
       return true;
-
+  }
   return false;
 }
 
 void MCSymbol::print(raw_ostream &OS, const MCAsmInfo *MAI) const {
-  if (NeedsQuoting(getName()))
+  if (!MAI || ShouldQuoteIdentifier(getName(), *MAI))
     OS << '"' << getName() << '"';
   else
     OS << getName();
