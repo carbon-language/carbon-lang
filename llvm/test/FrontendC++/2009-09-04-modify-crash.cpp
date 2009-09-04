d14b67e9319756a3ed98a30e2e31d5952615c74f
Dale Johannesen <dalej@apple.com>
2009-09-04 20:19:09 +0000

    Test for llvm-gcc commit 81037.
    llvm-svn: 81038

---
+++ llvm/test/FrontendC++/2009-09-04-modify-crash.cpp
@@ -0,0 +1,7 @@
+// RUN: %llvmgxx %s -emit-llvm -fapple-kext -S -o -
+// The extra check in 71555 caused this to crash on Darwin X86
+// in an assert build.
+class foo {
+ virtual ~foo ();
+};
+foo::~foo(){}
