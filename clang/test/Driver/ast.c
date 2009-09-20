1d9b45304da9774453b13129ca0a5083fff50cd2
Daniel Dunbar <daniel@zuster.org>
2009-09-20 23:35:52 +0000

    Work around FileCheck -NOT restriction.
    llvm-svn: 82417

--- clang/test/Driver/ast.c
+++ clang/test/Driver/ast.c
@@ -1,4 +1,5 @@
 // RUN: clang -ccc-host-triple i386-unknown-unknown -ccc-print-phases -emit-ast %s 2> %t &&
+// RUN: echo 'END' >> %t &&
 // RUN: FileCheck -check-prefix EMIT-AST-PHASES -input-file %t %s &&
 
 // EMIT-AST-PHASES: 0: input,
@@ -6,9 +7,11 @@
 // EMIT-AST-PHASES: 1: preprocessor, {0}, cpp-output
 // EMIT-AST-PHASES: 2: compiler, {1}, ast
 // EMIT-AST-PHASES-NOT: 3:
+// EMIT-AST-PHASES: END
 
 // RUN: touch %t.ast &&
 // RUN: clang -ccc-host-triple i386-unknown-unknown -ccc-print-phases -c %t.ast 2> %t &&
+// RUN: echo 'END' >> %t &&
 // RUN: FileCheck -check-prefix COMPILE-AST-PHASES -input-file %t %s
 
 // COMPILE-AST-PHASES: 0: input,
@@ -16,6 +19,7 @@
 // COMPILE-AST-PHASES: 1: compiler, {0}, assembler
 // COMPILE-AST-PHASES: 2: assembler, {1}, object
 // COMPILE-AST-PHASES-NOT: 3:
+// COMPILE-AST-PHASES: END
 
 // FIXME: There is a problem with compiling AST's in that the input language is
 // not availabe for use by other tools (for example, to automatically add
