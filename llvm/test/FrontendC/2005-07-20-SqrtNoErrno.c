ea406ed0a66729a4b63ee410a160de73d5f33521
Dale Johannesen <dalej@apple.com>
2009-09-23 21:46:36 +0000

    Disable test; what it's testing for is wrong.
    llvm-svn: 82658

--- llvm/test/FrontendC/2005-07-20-SqrtNoErrno.c
+++ llvm/test/FrontendC/2005-07-20-SqrtNoErrno.c
@@ -1,4 +1,7 @@
 // RUN: %llvmgcc %s -S -o - -fno-math-errno | grep llvm.sqrt
+// llvm.sqrt has undefined behavior on negative inputs, so it is
+// inappropriate to translate C/C++ sqrt to this.
+// XFAIL: *
 #include <math.h>
 
 float foo(float X) {
