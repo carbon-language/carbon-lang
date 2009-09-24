c594890a82bf25f8a407180caa4b30be79d15e62
Dale Johannesen <dalej@apple.com>
2009-09-24 23:37:40 +0000

    Reenable sqrt IR generation test.
    llvm-svn: 82731

--- llvm/test/FrontendC/2009-09-24-SqrtErrno.c
+++ llvm/test/FrontendC/2009-09-24-SqrtErrno.c
@@ -1,11 +1,12 @@
-// RUN: %llvmgcc %s -S -o - -fno-math-errno | FileCheck %s
+// RUN: %llvmgcc %s -S -o - -fmath-errno | FileCheck %s
 // llvm.sqrt has undefined behavior on negative inputs, so it is
 // inappropriate to translate C/C++ sqrt to this.
 #include <math.h>
 
 float foo(float X) {
 // CHECK: foo
-// CHECK: sqrtf(float %1) nounwind readonly
-  // Check that this is marked readonly when errno is ignored.
+// CHECK-NOT: readonly
+// CHECK: return
+  // Check that this is not marked readonly when errno is used.
   return sqrtf(X);
 }
