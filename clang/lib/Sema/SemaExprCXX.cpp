5aec1b54bce5f7e916acb7d758e052b04e0c2996
Fariborz Jahanian <fjahanian@apple.com>
2009-09-25 18:11:25 +0000

    Fix the AST tree so ir-gen can do the conversion via copy construction. Fixed pr5050.
    llvm-svn: 82783

--- clang/lib/Sema/SemaExprCXX.cpp
+++ clang/lib/Sema/SemaExprCXX.cpp
@@ -1113,7 +1113,9 @@ Sema::PerformImplicitConversion(Expr *&From, QualType ToType,
   if (SCS.CopyConstructor) {
     // FIXME: When can ToType be a reference type?
     assert(!ToType->isReferenceType());
-
+    if (SCS.Second == ICK_Derived_To_Base)
+      ImpCastExprToType(From, ToType, CastExpr::CK_DerivedToBase,
+                        /*isLvalue=*/true);
     OwningExprResult FromResult =
       BuildCXXConstructExpr(/*FIXME:ConstructLoc*/SourceLocation(),
                             ToType, SCS.CopyConstructor,
---
+++ clang/test/CodeGenCXX/PR5050-constructor-conversion.cpp
@@ -0,0 +1,19 @@
+// RUN: clang-cc -triple x86_64-apple-darwin -std=c++0x -S %s -o %t-64.s &&
+// RUN: FileCheck -check-prefix LP64 --input-file=%t-64.s %s &&
+// RUN: clang-cc -triple i386-apple-darwin -std=c++0x -S %s -o %t-32.s &&
+// RUN: FileCheck -check-prefix LP32 --input-file=%t-32.s %s &&
+// RUN: true
+
+struct A { A(const A&); };
+
+struct B : A { };
+
+A f(const B &b) {
+  return b;
+}
+
+// CHECK-LP64: call     __ZN1AC1ERK1A
+
+// CHECK-LP32: call     L__ZN1AC1ERK1A
+
+
