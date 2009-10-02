b5796a59c4d9708db9a3976ca669c787993364fb
Evan Cheng <evan.cheng@apple.com>
2009-10-02 06:07:47 +0000

    C++ member functions must be 2 byte aligned per ABI.
    llvm-svn: 83239

---
+++ llvm/test/FrontendC++/member-alignment.cpp
@@ -0,0 +1,19 @@
+// RUN: %llvmgxx -S -emit-llvm %s -o - | FileCheck %s
+
+// rdar://7268289
+
+class t {
+public:
+  virtual void foo(void);
+  void bar(void);
+};
+
+void
+t::bar(void) {
+// CHECK: _ZN1t3barEv{{.*}} align 2
+}
+
+void
+t::foo(void) {
+// CHECK: _ZN1t3fooEv{.*}} align 2
+}
