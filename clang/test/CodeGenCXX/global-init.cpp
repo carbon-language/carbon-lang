9776bd61bcdfe2ec6245f42d364725356e9d1d71
Anders Carlsson <andersca@mac.com>
2009-10-08 17:22:47 +0000

    Port test over to FileCheck.
    llvm-svn: 83558

--- clang/test/CodeGenCXX/global-init.cpp
+++ clang/test/CodeGenCXX/global-init.cpp
@@ -1,10 +1,10 @@
-// RUN: clang-cc -triple=x86_64-apple-darwin9 -emit-llvm %s -o %t &&
-// RUN: grep "call void @_ZN1AC1Ev" %t | count 1 &&
-// RUN: grep "call i32 @__cxa_atexit(void (i8\*)\* bitcast (void (%.truct.A\*)\* @_ZN1AD1Ev to void (i8\*)\*), i8\* getelementptr inbounds (%.truct.A\* @a, i32 0, i32 0), i8\* bitcast (i8\*\* @__dso_handle to i8\*))" %t | count 1 
+// RUN: clang-cc -triple=x86_64-apple-darwin9 -emit-llvm %s -o - |FileCheck %s
 
 struct A {
   A();
   ~A();
 };
 
+// CHECK: call void @_ZN1AC1Ev
+// CHECK: call i32 @__cxa_atexit(void (i8*)* bitcast (void (%struct.A*)* @_ZN1AD1Ev to void (i8*)*), i8* getelementptr inbounds (%struct.A* @a, i32 0, i32 0), i8* bitcast (i8** @__dso_handle to i8*))
 A a;
