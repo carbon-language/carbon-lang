39a029d8a3c7c0d79aa4ad5c68acd6599688f8f4
Devang Patel <dpatel@apple.com>
2009-10-13 20:56:38 +0000

    Optimizer may remove debug info.  This test checks debug info for include headers.
    llvm-svn: 84025

--- llvm/test/FrontendC/2006-01-13-Includes.c
+++ llvm/test/FrontendC/2006-01-13-Includes.c
@@ -1,7 +1,5 @@
-// RUN: %llvmgcc %s -g -S -o - | llvm-as | opt -std-compile-opts | \
-// RUN:   llvm-dis | grep {test/FrontendC}
+// RUN: %llvmgcc %s -g -S -o - | grep {test/FrontendC}
 // PR676
-// XFAIL: *
 
 #include <stdio.h>
 
