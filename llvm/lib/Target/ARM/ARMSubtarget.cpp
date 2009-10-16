8cd7c926135d65bc12dfb6a119cfadbbaf7bdda1
Evan Cheng <evan.cheng@apple.com>
2009-10-16 05:33:58 +0000

    Add comment.
    llvm-svn: 84246

--- llvm/lib/Target/ARM/ARMSubtarget.cpp
+++ llvm/lib/Target/ARM/ARMSubtarget.cpp
@@ -101,6 +101,8 @@ ARMSubtarget::ARMSubtarget(const std::string &TT, const std::string &FS,
   // Set CPU specific features.
   if (CPUString == "cortex-a8") {
     PostRAScheduler = true;
+    // On Cortext-a8, it's faster to perform some single-precision FP
+    // operations with NEON instructions.
     if (UseNEONFP.getPosition() == 0)
       UseNEONForSinglePrecisionFP = true;
   }
