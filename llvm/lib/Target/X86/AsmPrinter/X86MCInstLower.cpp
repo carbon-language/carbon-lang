0b4a59fc0797e9b8e67a7b369ded4563f53b43c4
Chris Lattner <sabre@nondot.org>
2009-10-19 23:35:57 +0000

    X86 should ignore implicit regs when lowering to MCInst also, no functionality change.
    llvm-svn: 84567

--- llvm/lib/Target/X86/AsmPrinter/X86MCInstLower.cpp
+++ llvm/lib/Target/X86/AsmPrinter/X86MCInstLower.cpp
@@ -306,6 +306,8 @@ void X86MCInstLower::Lower(const MachineInstr *MI, MCInst &OutMI) const {
       MI->dump();
       llvm_unreachable("unknown operand type");
     case MachineOperand::MO_Register:
+      // Ignore all implicit register operands.
+      if (MO.isImplicit()) continue;
       MCOp = MCOperand::CreateReg(MO.getReg());
       break;
     case MachineOperand::MO_Immediate:
