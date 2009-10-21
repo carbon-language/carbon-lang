53b40411516941ba1fd429cea2a63e2319ea13b2
Benjamin Kramer <benny.kra@googlemail.com>
2009-10-21 19:59:43 +0000

    Force triple; this test was failing on non-darwin platforms due to different asm comment styles (## vs #).
    llvm-svn: 84777

--- clang/test/CodeGen/2009-10-20-GlobalDebug.c
+++ clang/test/CodeGen/2009-10-20-GlobalDebug.c
@@ -1,4 +1,4 @@
-// RUN: clang -S -g -dA %s -o - | FileCheck %s
+// RUN: clang -ccc-host-triple i386-apple-darwin10 -S -g -dA %s -o - | FileCheck %s
 int global;
 // CHECK: asciz  "global"                                    ## DW_AT_MIPS_linkage_name
 int main() { return 0;}
