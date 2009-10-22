2a536740a472598f7cfc608ba14ea4685843ee12
Duncan Sands <baldrick@free.fr>
2009-10-22 12:53:25 +0000

    Check that accessing a struct field that occurs before the start of the struct (!) works correctly.
    llvm-svn: 84853

--- llvm/test/FrontendAda/negative_field_offset.adb
+++ llvm/test/FrontendAda/negative_field_offset.adb
@@ -5,6 +5,12 @@ procedure Negative_Field_Offset (N : Integer) is
    --  Force use of a thin pointer.
    for String_Pointer'Size use System.Word_Size;
    P : String_Pointer;
+
+   procedure Q (P : String_Pointer) is
+   begin
+      P (1) := 'Z';
+   end;
 begin
    P := new String (1 .. N);
+   Q (P);
 end;
