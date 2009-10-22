820782aae0f08bffe69d28448e41ffa96a3abdab
Daniel Dunbar <daniel@zuster.org>
2009-10-22 00:12:00 +0000

    Driver: Fix thinko in logic for finding gcc's tool chain directory.
    llvm-svn: 84805

--- clang/lib/Driver/ToolChains.cpp
+++ clang/lib/Driver/ToolChains.cpp
@@ -65,7 +65,8 @@ DarwinGCC::DarwinGCC(const HostInfo &Host, const llvm::Triple& Triple,
   ToolChainDir += llvm::utostr(GCCVersion[2]);
 
   // Try the next major version if that tool chain dir is invalid.
-  if (!llvm::sys::Path(ToolChainDir).exists()) {
+  std::string Tmp = "/usr/lib/gcc/" + ToolChainDir;
+  if (!llvm::sys::Path(Tmp).exists()) {
     std::string Next = "i686-apple-darwin";
     Next += llvm::utostr(DarwinVersion[0] + 1);
     Next += "/";
@@ -78,7 +79,8 @@ DarwinGCC::DarwinGCC(const HostInfo &Host, const llvm::Triple& Triple,
     // Use that if it exists, otherwise hope the user isn't linking.
     //
     // FIXME: Drop dependency on gcc's tool chain.
-    if (llvm::sys::Path(Next).exists())
+    Tmp = "/usr/lib/gcc/" + Next;
+    if (llvm::sys::Path(Tmp).exists())
       ToolChainDir = Next;
   }
 
