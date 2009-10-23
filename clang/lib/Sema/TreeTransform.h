0c78c7aa20f7e025e8b22bdefa9ae2d8803e7f36
Benjamin Kramer <benny.kra@googlemail.com>
2009-10-23 10:48:09 +0000

    Silence GCC 4.3 warning.
    TreeTransform.h:2333: warning: suggest parentheses around && within ||
    
    llvm-svn: 84949

--- clang/lib/Sema/TreeTransform.h
+++ clang/lib/Sema/TreeTransform.h
@@ -2331,8 +2331,7 @@ QualType TreeTransform<Derived>::TransformDependentSizedExtVectorType(
 
   QualType Result = TL.getType();
   if (getDerived().AlwaysRebuild() ||
-      ElementType != T->getElementType() &&
-      Size.get() != T->getSizeExpr()) {
+      (ElementType != T->getElementType() && Size.get() != T->getSizeExpr())) {
     Result = getDerived().RebuildDependentSizedExtVectorType(ElementType,
                                                          move(Size),
                                                          T->getAttributeLoc());
