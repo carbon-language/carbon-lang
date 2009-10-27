59ca7f1a1e37fad29a59bc3089252172eda264e9
Daniel Dunbar <daniel@zuster.org>
2009-10-27 19:48:00 +0000

    Convert test to FileCheck.
    llvm-svn: 85277

--- clang/test/CodeGen/function-attributes.c
+++ clang/test/CodeGen/function-attributes.c
@@ -1,12 +1,12 @@
-// RUN: clang-cc -triple i386-unknown-unknown -emit-llvm -o %t %s &&
-// RUN: grep 'define signext i8 @f0(i32 %x) nounwind' %t &&
-// RUN: grep 'define zeroext i8 @f1(i32 %x) nounwind' %t &&
-// RUN: grep 'define void @f2(i8 signext %x) nounwind' %t &&
-// RUN: grep 'define void @f3(i8 zeroext %x) nounwind' %t &&
-// RUN: grep 'define signext i16 @f4(i32 %x) nounwind' %t &&
-// RUN: grep 'define zeroext i16 @f5(i32 %x) nounwind' %t &&
-// RUN: grep 'define void @f6(i16 signext %x) nounwind' %t &&
-// RUN: grep 'define void @f7(i16 zeroext %x) nounwind' %t &&
+// RUN: clang-cc -triple i386-unknown-unknown -emit-llvm -o - %s | FileCheck %s
+// CHECK: define signext i8 @f0(i32 %x) nounwind
+// CHECK: define zeroext i8 @f1(i32 %x) nounwind
+// CHECK: define void @f2(i8 signext %x) nounwind
+// CHECK: define void @f3(i8 zeroext %x) nounwind
+// CHECK: define signext i16 @f4(i32 %x) nounwind
+// CHECK: define zeroext i16 @f5(i32 %x) nounwind
+// CHECK: define void @f6(i16 signext %x) nounwind
+// CHECK: define void @f7(i16 zeroext %x) nounwind
 
 signed char f0(int x) { return x; }
 
@@ -24,15 +24,15 @@ void f6(signed short x) { }
 
 void f7(unsigned short x) { }
 
-// RUN: grep 'define void @f8() nounwind alwaysinline' %t &&
+// CHECK: define void @f8() nounwind alwaysinline
 void __attribute__((always_inline)) f8(void) { }
 
-// RUN: grep 'call void @f9_t() noreturn' %t &&
+// CHECK: call void @f9_t() noreturn
 void __attribute__((noreturn)) f9_t(void);
 void f9(void) { f9_t(); }
 
 // FIXME: We should be setting nounwind on calls.
-// RUN: grep 'call i32 @f10_t() readnone' %t &&
+// CHECK: call i32 @f10_t() readnone
 int __attribute__((const)) f10_t(void);
 int f10(void) { return f10_t(); }
 int f11(void) {
@@ -43,13 +43,15 @@ int f12(int arg) {
   return arg ? 0 : f10_t();
 }
 
-// RUN: grep 'define void @f13() nounwind readnone' %t &&
+// CHECK: define void @f13() nounwind readnone
 void f13(void) __attribute__((pure)) __attribute__((const));
 void f13(void){}
 
 
 // Ensure that these get inlined: rdar://6853279
-// RUN: not grep '@ai_' %t &&
+// CHECK: define i32 @f14
+// CHECK-NOT: @ai_
+// CHECK: ret i32
 static __inline__ __attribute__((always_inline))
 int ai_1() {  return 4; }
 
@@ -58,12 +60,7 @@ struct {
   int a, b, c, d, e;
 } ai_2() { while (1) {} }
 
-
-int foo() {
+int f14() {
   ai_2();
   return ai_1();
 }
-
-
-
-// RUN: true
