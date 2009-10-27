995c845e5cb62a522a0addfd1ffac7d02edf5491
John Thompson <John.Thompson.JTSoftware@gmail.com>
2009-10-27 14:58:27 +0000

    Disabling some MS extensions which cause this test to fail
    llvm-svn: 85242

--- clang/test/SemaCXX/typedef-redecl.cpp
+++ clang/test/SemaCXX/typedef-redecl.cpp
@@ -1,4 +1,4 @@
-// RUN: clang-cc -fsyntax-only -verify %s 
+// RUN: clang-cc -fsyntax-only -verify -fms-extensions=0 %s 
 typedef int INT;
 typedef INT REALLY_INT; // expected-note {{previous definition is here}}
 typedef REALLY_INT REALLY_REALLY_INT;
