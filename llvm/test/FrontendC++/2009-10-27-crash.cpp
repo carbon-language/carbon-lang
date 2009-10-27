bda7fee4a8a557c031c86b0095ce17e13f9d5bf4
Dale Johannesen <dalej@apple.com>
2009-10-27 20:06:05 +0000

    Testcase for llvm-gcc patch 85284.
    llvm-svn: 85287

---
+++ llvm/test/FrontendC++/2009-10-27-crash.cpp
@@ -0,0 +1,42 @@
+// RUN: %llvmgxx -emit-llvm -S %s
+
+typedef struct
+{
+	unsigned short a : 1;
+	unsigned short b : 2;
+	unsigned short c : 1;
+	unsigned short d : 1;
+	unsigned short e : 1;
+	unsigned short f : 1;
+	unsigned short g : 2;
+	unsigned short : 7;
+	union
+	{
+		struct
+		{
+			unsigned char h : 1;
+			unsigned char i : 1;
+			unsigned char j : 1;
+			unsigned char : 5;
+		};
+		struct
+		{
+			unsigned char k : 3;
+			unsigned char : 5;
+		};
+	};
+	unsigned char : 8;
+} tt;
+
+typedef struct
+{
+ unsigned char s;
+ tt t;
+ unsigned int u;
+} ttt;
+
+ttt X = {
+    4,
+       { 0 },
+	55,
+};
