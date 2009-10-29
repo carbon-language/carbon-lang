29fb551a36dda8f60293908a63f100dd2561f126
Chris Lattner <sabre@nondot.org>
2009-10-29 01:20:34 +0000

    make this more interesting, test the static const array.
    llvm-svn: 85464

--- clang/test/CodeGen/indirect-goto.c
+++ clang/test/CodeGen/indirect-goto.c
@@ -1,9 +1,7 @@
-// RUN: clang-cc -triple i386-unknown-unknown -emit-llvm-bc -o - %s | opt -std-compile-opts | llvm-dis > %t &&
-// RUN: grep "ret i32" %t | count 1 &&
-// RUN: grep "ret i32 210" %t | count 1
+// RUN: clang-cc -triple i386-unknown-unknown -emit-llvm-bc -o - %s | opt -std-compile-opts -S | grep "ret i32 2520"
 
 static int foo(unsigned i) {
-  const void *addrs[] = { &&L1, &&L2, &&L3, &&L4, &&L5 };
+  void *addrs[] = { &&L1, &&L2, &&L3, &&L4, &&L5 };
   int res = 1;
 
   goto *addrs[i];
@@ -15,6 +13,19 @@ static int foo(unsigned i) {
   return res;
 }
 
+static int foo2(unsigned i) {
+  static const void *addrs[] = { &&L1, &&L2, &&L3, &&L4, &&L5 };
+  int res = 1;
+  
+  goto *addrs[i];
+L5: res *= 11;
+L4: res *= 7;
+L3: res *= 5;
+L2: res *= 3;
+L1: res *= 2; 
+  return res;
+}
+
 int bar() {
-  return foo(3);
+  return foo(3)+foo2(4);
 }
