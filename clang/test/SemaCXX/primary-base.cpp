5ff76e2fa2ae4509d7b3ce9e8bd2dea4dafba496
Mike Stump <mrs@apple.com>
2009-11-05 03:47:55 +0000

    Fix 80-col violation.
    llvm-svn: 86115

--- clang/test/SemaCXX/primary-base.cpp
+++ clang/test/SemaCXX/primary-base.cpp
@@ -4,8 +4,8 @@ class B : virtual A { };
 
 class C : B { };
 
-// Since A is already a primary base class, C should be the primary base class of F.
+// Since A is already a primary base class, C should be the primary base class
+// of F.
 class F : virtual A, virtual C { };
 
 int sa[sizeof(F) == sizeof(A) ? 1 : -1];
-
