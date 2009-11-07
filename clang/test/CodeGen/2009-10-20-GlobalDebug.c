22053c0f46d803884d90c664aa0df1a9149a0e2c
Devang Patel <dpatel@apple.com>
2009-11-07 00:41:37 +0000

    MIPS linkage name confuses gdb here. Generate and test DW_AT_name here.
    llvm-svn: 86318

--- clang/test/CodeGen/2009-10-20-GlobalDebug.c
+++ clang/test/CodeGen/2009-10-20-GlobalDebug.c
@@ -1,4 +1,4 @@
 // RUN: clang -ccc-host-triple i386-apple-darwin10 -S -g -dA %s -o - | FileCheck %s
 int global;
-// CHECK: asciz  "global"                                    ## DW_AT_MIPS_linkage_name
+// CHECK: asciz  "global"                                    ## DW_AT_name
 int main() { return 0;}
