91989d166a0ec952eef72be043c02414792bb34c
Ted Kremenek <kremenek@apple.com>
2009-11-11 17:17:06 +0000

    Split buffer overflow test case into two test cases, removing out logic that was commented out.
    llvm-svn: 86845

--- clang/test/Analysis/rdar-6541136-region.c
+++ clang/test/Analysis/rdar-6541136-region.c
@@ -6,17 +6,22 @@ extern kernel_tea_cheese_t _wonky_gesticulate_cheese;
 
 // This test case exercises the ElementRegion::getRValueType() logic.
 
-
-void foo( void )
-{
+void test1( void ) {
   kernel_tea_cheese_t *wonky = &_wonky_gesticulate_cheese;
   struct load_wine *cmd = (void*) &wonky[1];
   cmd = cmd;
   char *p = (void*) &wonky[1];
-  //*p = 1;  // this is also an out-of-bound access.
   kernel_tea_cheese_t *q = &wonky[1];
   // This test case tests both the RegionStore logic (doesn't crash) and
   // the out-of-bounds checking.  We don't expect the warning for now since
   // out-of-bound checking is temporarily disabled.
   kernel_tea_cheese_t r = *q; // expected-warning{{Access out-of-bound array element (buffer overflow)}}
+}
+
+void test1_b( void ) {
+  kernel_tea_cheese_t *wonky = &_wonky_gesticulate_cheese;
+  struct load_wine *cmd = (void*) &wonky[1];
+  cmd = cmd;
+  char *p = (void*) &wonky[1];
+  *p = 1;  // expected-warning{{Access out-of-bound array element (buffer overflow)}}
 }
