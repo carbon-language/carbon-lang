33935767b9eb67f20bf65b84e1a5589f0e6a8de1
David Greene <greened@obbligato.org>
2009-11-12 20:13:34 +0000

    Add comment flags so AsmPrinter can output additional information when emitting comments.  These flags carry semantic information not otherwise easily derivable from the IR text.
    llvm-svn: 87016

--- llvm/include/llvm/CodeGen/AsmPrinter.h
+++ llvm/include/llvm/CodeGen/AsmPrinter.h
@@ -86,6 +86,14 @@ namespace llvm {
     DwarfWriter *DW;
 
   public:
+    /// Flags to specify different kinds of comments to output in
+    /// assembly code.  These flags carry semantic information not
+    /// otherwise easily derivable from the IR text.
+    ///
+    enum CommentFlag {
+      ReloadReuse = 0x1
+    };
+
     /// Output stream on which we're printing assembly code.
     ///
     formatted_raw_ostream &O;
