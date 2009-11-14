6c39d457fa15fffff586b96977dbd4a626f1be8b
Daniel Dunbar <daniel@zuster.org>
2009-11-14 04:39:42 +0000

    Update FixIt tests to make it more obvious they use a separate mode.
    llvm-svn: 88758

--- clang/test/FixIt/fixit-at.c
+++ clang/test/FixIt/fixit-at.c
@@ -1,4 +1,4 @@
-// RUN: clang-cc -fsyntax-only -fixit-at=fixit-at.c:3:1 %s -o - | clang-cc -verify -x c -
+// RUN: clang-cc -fixit-at=fixit-at.c:3:1 %s -o - | clang-cc -verify -x c -
 
 _Complex cd;
 
--- clang/test/FixIt/fixit-c90.c
+++ clang/test/FixIt/fixit-c90.c
@@ -1,4 +1,4 @@
-/* RUN: clang-cc -fsyntax-only -std=c90 -pedantic -fixit %s -o - | clang-cc -pedantic -x c -std=c90 -Werror -
+/* RUN: clang-cc -std=c90 -pedantic -fixit %s -o - | clang-cc -pedantic -x c -std=c90 -Werror -
  */
 
 /* This is a test of the various code modification hints that are
