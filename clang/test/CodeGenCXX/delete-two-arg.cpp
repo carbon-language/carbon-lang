6fa768df299bcb35af42c10cd838e43cb7c51f85
Mike Stump <mrs@apple.com>
2009-11-16 18:06:39 +0000

    Fix spelling for target triplet.
    llvm-svn: 88928

--- clang/test/CodeGenCXX/delete-two-arg.cpp
+++ clang/test/CodeGenCXX/delete-two-arg.cpp
@@ -1,4 +1,4 @@
-// RUN: clang-cc -triple i686-pc-linue-gnu %s -o - -emit-llvm -verify | FileCheck %s
+// RUN: clang-cc -triple i686-pc-linux-gnu %s -o - -emit-llvm -verify | FileCheck %s
 
 struct A { void operator delete(void*,__typeof(sizeof(int))); int x; };
 void a(A* x) { delete x; }
