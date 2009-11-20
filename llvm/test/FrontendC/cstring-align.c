7dc8d2d0258e0e3afb9baf6229e1e4d818b6702d
Bill Wendling <isanbard@gmail.com>
2009-11-20 00:32:16 +0000

    Testcase for r89415.
    llvm-svn: 89417

---
+++ llvm/test/FrontendC/cstring-align.c
@@ -0,0 +1,16 @@
+// RUN: %llvmgcc %s -c -Os -m32 -emit-llvm -o - | llc -march=x86 -mtriple=i386-apple-darwin10 | FileCheck %s -check-prefix=DARWIN32
+// RUN: %llvmgcc %s -c -Os -m64 -emit-llvm -o - | llc -march=x86 -mtriple=i386-apple-darwin10 | FileCheck %s -check-prefix=DARWIN64
+
+extern void func(const char *, const char *);
+
+void long_function_name() {
+  func("%s: the function name", __func__);
+}
+
+// DARWIN64: .align 3
+// DARWIN64: ___func__.
+// DARWIN64: .asciz "long_function_name"
+
+// DARWIN32: .align 2
+// DARWIN32: ___func__.
+// DARWIN32: .asciz "long_function_name"
