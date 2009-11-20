32164f4bdb81cfd6aea0d933328346b15db8e41e
Daniel Dunbar <daniel@zuster.org>
2009-11-20 22:32:30 +0000

    Driver: Use '-target-abi' 'FOO' instead of '-target-abi=FOO'.
    llvm-svn: 89501

--- clang/test/CodeGen/arm-arguments.c
+++ clang/test/CodeGen/arm-arguments.c
@@ -1,5 +1,5 @@
-// RUN: clang-cc -triple armv7-apple-darwin9 -target-abi=apcs-gnu -emit-llvm -w -o - %s | FileCheck -check-prefix=APCS-GNU %s
-// RUN: clang-cc -triple armv7-apple-darwin9 -target-abi=aapcs -emit-llvm -w -o - %s | FileCheck -check-prefix=AAPCS %s
+// RUN: clang-cc -triple armv7-apple-darwin9 -target-abi apcs-gnu -emit-llvm -w -o - %s | FileCheck -check-prefix=APCS-GNU %s
+// RUN: clang-cc -triple armv7-apple-darwin9 -target-abi aapcs -emit-llvm -w -o - %s | FileCheck -check-prefix=AAPCS %s
 
 // APCS-GNU: define arm_apcscc signext i8 @f0()
 // AAPCS: define arm_aapcscc signext i8 @f0()
--- clang/test/Sema/align-arm-apcs.c
+++ clang/test/Sema/align-arm-apcs.c
@@ -1,4 +1,4 @@
-// RUN: clang-cc -triple arm-unknown-unknown -target-abi=apcs-gnu -fsyntax-only -verify %s
+// RUN: clang-cc -triple arm-unknown-unknown -target-abi apcs-gnu -fsyntax-only -verify %s
 
 struct s0 { double f0; int f1; };
 char chk0[__alignof__(struct s0) == 4 ? 1 : -1]; 
