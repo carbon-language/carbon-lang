0825f093060ebdb845b3d2bb397d7d93daa19046
Edward O'Callaghan <eocallaghan@auroraux.org>
2009-11-22 16:21:47 +0000

    Fix for PR5233.
    llvm-svn: 89603

--- clang/test/Sema/wchar.c
+++ clang/test/Sema/wchar.c
@@ -6,7 +6,9 @@ typedef __WCHAR_TYPE__ wchar_t;
 #if defined(_WIN32) || defined(_M_IX86) || defined(__CYGWIN__) \
  || defined(_M_X64) || defined(SHORT_WCHAR)
   #define WCHAR_T_TYPE unsigned short
-#else
+#elif defined(__sun) || defined(__AuroraUX__)
+  #define WCHAR_T_TYPE long
+#else /* Solaris or AuroraUX. */
   #define WCHAR_T_TYPE int
 #endif
  
