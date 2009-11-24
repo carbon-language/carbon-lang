dddf134a3cea47a2ab2ad7ede189d1fe29be7211
Edward O'Callaghan <eocallaghan@auroraux.org>
2009-11-24 15:19:10 +0000

    Provide Path::isSpecialFile interface for PR5568.
    llvm-svn: 89765

--- llvm/include/llvm/System/Path.h
+++ llvm/include/llvm/System/Path.h
@@ -380,6 +380,11 @@ namespace sys {
       /// in the file system.
       bool canWrite() const;
 
+      /// This function checks that what we're trying to work only on a regular file or Dir.
+      /// Check for things like /dev/null, any block special file,
+      /// or other things that aren't "regular" files.
+      bool isSpecialFile() const;
+
       /// This function determines if the path name references an executable
       /// file in the file system. This function checks for the existence and
       /// executability (by the current program) of the file.
