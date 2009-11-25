87e82ced6069568a0a3169cdd5376a000021acea
Edward O'Callaghan <eocallaghan@auroraux.org>
2009-11-25 12:00:34 +0000

    Adjust comments to new semantics.
    llvm-svn: 89862

--- llvm/include/llvm/System/Path.h
+++ llvm/include/llvm/System/Path.h
@@ -380,9 +380,9 @@ namespace sys {
       /// in the file system.
       bool canWrite() const;
 
-      /// This function checks that what we're trying to work only on a regular file
-      /// or directory. Check for things like /dev/null, any block special file,
-      /// or other things that aren't "regular" regular files or directories.
+      /// This function checks that what we're trying to work only on a regular file.
+      /// Check for things like /dev/null, any block special file,
+      /// or other things that aren't "regular" regular files.
       /// @returns true if the file is S_ISREG.
       /// @brief Determines if the file is a regular file
       bool isRegularFile() const;
