7c71b6b95b828764abcfea9a9fe94453f30393e7
Daniel Dunbar <daniel@zuster.org>
2009-11-30 08:41:04 +0000

    Use '-FOO' 'BAR' instead of '-FOO=BAR' in tests.
    llvm-svn: 90122

--- clang/test/SemaTemplate/instantiation-depth.cpp
+++ clang/test/SemaTemplate/instantiation-depth.cpp
@@ -1,4 +1,4 @@
-// RUN: clang-cc -fsyntax-only -ftemplate-depth=5 -verify %s
+// RUN: clang-cc -fsyntax-only -ftemplate-depth 5 -verify %s
 
 template<typename T> struct X : X<T*> { }; // expected-error{{recursive template instantiation exceeded maximum depth of 5}} \
 // expected-note{{use -ftemplate-depth-N to increase recursive template instantiation depth}} \
