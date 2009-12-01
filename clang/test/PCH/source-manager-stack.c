d0c9a29cb8a60428c9c2476240541577256f9ea7
Daniel Dunbar <daniel@zuster.org>
2009-12-01 21:57:39 +0000

    Add test case for PR5662.
    llvm-svn: 90264

---
+++ clang/test/PCH/source-manager-stack.c
@@ -0,0 +1,12 @@
+// Test that the source manager has the "proper" idea about the include stack
+// when using PCH.
+
+// RUN: echo 'int x;' > %t.prefix.h
+// RUN: not clang-cc -fsyntax-only -include %t.prefix.h %s 2> %t.diags.no_pch.txt
+// RUN: clang-cc -emit-pch -o %t.prefix.pch %t.prefix.h
+// RUN: not clang-cc -fsyntax-only -include-pch %t.prefix.pch %s 2> %t.diags.pch.txt
+// RUN: diff %t.diags.no_pch.txt %t.diags.pch.txt
+// XFAIL: *
+// PR5662
+
+float x;
