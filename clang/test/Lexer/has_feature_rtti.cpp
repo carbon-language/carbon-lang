7dac00b2e8f6228466fd7339c82ba248c6db97d1
Ted Kremenek <kremenek@apple.com>
2009-12-03 01:17:43 +0000

    Add FileCheck test for '__has_feature(rtti)'.
    llvm-svn: 90373

---
+++ clang/test/Lexer/has_feature_rtti.cpp
@@ -0,0 +1,11 @@
+// RUN: clang -E -frtti %s -o - | FileCheck --check-prefix=CHECK-RTTI %s
+// RUN: clang -E -fno-rtti %s -o - | FileCheck --check-prefix=CHECK-NO-RTTI %s
+
+#if __has_feature(rtti)
+int foo();
+#else
+int bar();
+#endif
+
+// CHECK-RTTI: foo
+// CHECK-NO-RTTI: bar
