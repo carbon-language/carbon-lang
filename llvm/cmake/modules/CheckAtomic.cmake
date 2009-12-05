4f6d1eb3e063d71019a1516548c13c64e024b7da
Oscar Fuentes <ofv@wanadoo.es>
2009-12-05 23:19:33 +0000

    Fix for atomic intrinsics detection when using MSVC.
    Patch by Michael Beck!
    
    llvm-svn: 90683

--- llvm/cmake/modules/CheckAtomic.cmake
+++ llvm/cmake/modules/CheckAtomic.cmake
@@ -1,14 +1,26 @@
 # atomic builtins are required for threading support.
 
 INCLUDE(CheckCXXSourceCompiles)
+
+#ifdef _MSC_VER
+#include <windows.h>
+#endif
 	
 CHECK_CXX_SOURCE_COMPILES("
 int main() {
+#ifdef _MSC_VER
+        volatile LONG val = 1;
+        MemoryBarrier();
+        InterlockedCompareExchange(&val, 0, 1);
+        InterlockedIncrement(&val);
+        InterlockedDecrement(&val);
+#else
         volatile unsigned long val = 1;
         __sync_synchronize();
         __sync_val_compare_and_swap(&val, 1, 0);
         __sync_add_and_fetch(&val, 1);
         __sync_sub_and_fetch(&val, 1);
+#endif
         return 0;
       }
 " LLVM_MULTITHREADED)
