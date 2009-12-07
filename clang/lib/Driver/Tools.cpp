7bea9a1672024457b195ccc440fb5e11f32da0e1
Ted Kremenek <kremenek@apple.com>
2009-12-07 22:26:14 +0000

    Enable '-analyzer-opt-analyze-nested-blocks' by default for testing.
    llvm-svn: 90812

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -647,6 +647,9 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     // Enable region store model by default.
     CmdArgs.push_back("-analyzer-store=region");
 
+    // Treat blocks as analysis entry points.
+    CmdArgs.push_back("-analyzer-opt-analyze-nested-blocks");
+
     // Add default argument set.
     if (!Args.hasArg(options::OPT__analyzer_no_default_checks)) {
       CmdArgs.push_back("-warn-dead-stores");
