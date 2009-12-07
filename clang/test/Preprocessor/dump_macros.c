01c73e1b1609a01af9a5fb045e7737d430895638
Chris Lattner <sabre@nondot.org>
2009-12-07 01:49:36 +0000

    filecheckize
    llvm-svn: 90733

--- clang/test/Preprocessor/dump_macros.c
+++ clang/test/Preprocessor/dump_macros.c
@@ -1,30 +1,29 @@
-// RUN: clang-cc -E -dM %s -o %t
+// RUN: clang-cc -E -dM %s -o - | FileCheck %s -strict-whitespace
 
-// Space even without expansion tokens
-// RUN: grep "#define A(x) " %t
+// Space at end even without expansion tokens
+// CHECK: {{#define A[(]x[)] $}}
 #define A(x)
 
 // Space before expansion list.
-// RUN: grep "#define B(x,y) x y" %t
+// CHECK: {{#define B[(]x,y[)] x y$}}
 #define B(x,y)x y
 
-// No space in expansion list.
-// RUN: grep "#define C(x,y) x y" %t
+// No space in argument list.
+// CHECK: #define C(x,y) x y
 #define C(x, y) x y
 
 // No paste avoidance.
-// RUN: grep "#define X() .." %t
+// CHECK: #define X() ..
 #define X() ..
 
 // Simple test.
-// RUN: grep "#define Y ." %t
-// RUN: grep "#define Z X()Y" %t
+// CHECK: #define Y .
+// CHECK: #define Z X()Y
 #define Y .
 #define Z X()Y
 
 // gcc prints macros at end of translation unit, so last one wins.
-// RUN: grep "#define foo 2" %t
-// RUN: not grep "#define foo 1" %t
+// CHECK: #define foo 2
 #define foo 1
 #undef foo
 #define foo 2
