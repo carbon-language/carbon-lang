e1996c27eb5708ca2e16df0dd377ae081721ec5e
Ted Kremenek <kremenek@apple.com>
2009-12-12 00:42:52 +0000

    Convert the remainder of this test case over to using FileCheck.
    llvm-svn: 91194

--- clang/test/Driver/clang_f_opts.c
+++ clang/test/Driver/clang_f_opts.c
@@ -1,12 +1,14 @@
-// RUN: clang -### -S -x c /dev/null -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings -fno-blocks -fno-builtin -fno-math-errno -fno-common -fno-pascal-strings -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings %s 2> %t
-// RUN: grep -F '"-fblocks"' %t
-// RUN: grep -F '"-fpascal-strings"' %t
-// RUN: clang -### -S -x c /dev/null -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings -fno-blocks -fno-builtin -fno-math-errno -fno-common -fno-pascal-strings -fno-show-source-location -fshort-wchar %s 2> %t
-// RUN: grep -F '"-fno-builtin"' %t
-// RUN: grep -F '"-fno-common"' %t
-// RUN: grep -F '"-fno-math-errno"' %t
-// RUN: grep -F '"-fno-show-source-location"' %t
-// RUN: grep -F '"-fshort-wchar"' %t
+// RUN: clang -### -S -x c /dev/null -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings -fno-blocks -fno-builtin -fno-math-errno -fno-common -fno-pascal-strings -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings %s 2>&1 | FileCheck -check-prefix=CHECK-OPTIONS1 %s
+// RUN: clang -### -S -x c /dev/null -fblocks -fbuiltin -fmath-errno -fcommon -fpascal-strings -fno-blocks -fno-builtin -fno-math-errno -fno-common -fno-pascal-strings -fno-show-source-location -fshort-wchar %s 2>&1 | FileCheck -check-prefix=CHECK-OPTIONS2 %s
 // RUN: clang -fshort-enums -x c /dev/null 2>&1 | FileCheck -check-prefix=CHECK-SHORT-ENUMS %s
+
+// CHECK-OPTIONS1: -fblocks
+// CHECK-OPTIONS1: -fpascal-strings
+
+// CHECK-OPTIONS2: -fno-builtin
+// CHECK-OPTIONS2: -fno-common
+// CHECK-OPTIONS2: -fno-math-errno
+// CHECK-OPTIONS2: -fno-show-source-location
+// CHECL-OPTIONS2: -fshort-wchar
 
 // CHECK-SHORT-ENUMS: compiler does not support '-fshort-enums'
