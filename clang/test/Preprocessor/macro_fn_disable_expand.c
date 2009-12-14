b8132253c8e3b8930e28b63227deb31d20809072
Chris Lattner <sabre@nondot.org>
2009-12-14 04:57:53 +0000

    filecheckize
    llvm-svn: 91265

--- clang/test/Preprocessor/macro_fn_disable_expand.c
+++ clang/test/Preprocessor/macro_fn_disable_expand.c
@@ -1,11 +1,10 @@
-// RUN: clang-cc %s -E | grep 'bar foo (2)'
-// RUN: clang-cc %s -E | grep 'm(ABCD)'
+// RUN: clang-cc %s -E | FileCheck %s
 
 #define foo(x) bar x
 foo(foo) (2)
-
+// CHECK: bar foo (2)
 
 #define m(a) a(w)
 #define w ABCD
-m(m)   // m(ABCD)
-
+m(m)
+// CHECK: m(ABCD)
