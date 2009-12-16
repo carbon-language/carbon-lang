3ab9e4cf87e902aecbef20c3cd2fa211f2bab387
Ted Kremenek <kremenek@apple.com>
2009-12-16 19:46:44 +0000

    Temporarily revert 91553.
    llvm-svn: 91557

--- clang/lib/Analysis/BasicStore.cpp
+++ clang/lib/Analysis/BasicStore.cpp
@@ -479,11 +479,15 @@ Store BasicStoreManager::getInitialStore(const LocationContext *InitLoc) {
       const Decl& CD = *InitLoc->getDecl();
       if (const ObjCMethodDecl* MD = dyn_cast<ObjCMethodDecl>(&CD)) {
         if (MD->getSelfDecl() == PD) {
-          // FIXME: Add type constraints (when they become available) to
-          // SelfRegion?  (i.e., it implements MD->getClassInterface()).
-          const MemRegion *SelfRegion = MRMgr.getVarRegion(PD, InitLoc);
-          St = BindInternal(St, ValMgr.makeLoc(SelfRegion),
-                            ValMgr.getRegionValueSymbolVal(SelfRegion));
+          // FIXME: Just use a symbolic region, and remove ObjCObjectRegion
+          // entirely.
+          const ObjCObjectRegion *SelfRegion =
+            MRMgr.getObjCObjectRegion(MD->getClassInterface(),
+                                      MRMgr.getHeapRegion());
+
+          St = BindInternal(St, ValMgr.makeLoc(MRMgr.getVarRegion(PD, InitLoc)),
+                            ValMgr.makeLoc(SelfRegion));
+
           // Scan the method for ivar references.  While this requires an
           // entire AST scan, the cost should not be high in practice.
           St = scanForIvars(MD->getBody(), PD, SelfRegion, St);
