726b878574a1de393a025511ee889ea258fb2cf4
Eli Friedman <eli.friedman@gmail.com>
2009-12-18 23:42:55 +0000

    Test for r91724.
    llvm-svn: 91730

---
+++ clang/test/CodeGenCXX/virtual-base-ctor.cpp
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 %s -emit-llvm -o - -O2 | FileCheck %s
+
+struct B;
+extern B x;
+char y;
+typedef __typeof(sizeof(int)) size_t;
+struct A { int a; A() { y = ((size_t)this - (size_t)&x) / sizeof(void*); } };
+struct B : virtual A { void* x; };    
+B x;
+
+// CHECK: @y = global i8 2
