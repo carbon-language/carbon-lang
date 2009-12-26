ea21448978477fb95d3326d74db15f9ea63cfedc
Daniel Dunbar <daniel@zuster.org>
2009-12-26 22:58:23 +0000

    lit: Sink code into a 'lit' package.
    llvm-svn: 92168

---
+++ llvm/utils/lit/lit/__init__.py
@@ -0,0 +1,10 @@
+"""'lit' Testing Tool"""
+
+from lit import main
+
+__author__ = 'Daniel Dunbar'
+__email__ = 'daniel@zuster.org'
+__versioninfo__ = (0, 1, 0)
+__version__ = '.'.join(map(str, __versioninfo__))
+
+__all__ = []
