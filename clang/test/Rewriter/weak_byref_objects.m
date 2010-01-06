017a6067e5831f3343b103a968183b3e00bbfa6f
Fariborz Jahanian <fjahanian@apple.com>
2010-01-06 23:31:34 +0000

    Test case for rewriting of __weak byref objects.
    llvm-svn: 92868

---
+++ clang/test/Rewriter/weak_byref_objects.m
@@ -0,0 +1,15 @@
+// RUN: %clang_cc1 -fblocks -rewrite-objc %s -o -
+
+#define nil 0
+int main() {
+        __weak __block id foo = nil;
+        __block id foo2 = nil;
+        id foo3 = nil;
+
+        void (^myblock)() = ^{
+                foo = nil;
+                foo2 = nil;
+                [foo3 bar];
+                id foo4 = foo3;
+        };
+}
