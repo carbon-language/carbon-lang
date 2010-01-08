af07fbe210b4a36cf19d213a4edd8a7e7fca0842
John McCall <rjmccall@apple.com>
2010-01-08 18:40:32 +0000

    Organize testcase into namespaces.
    llvm-svn: 93015

--- clang/test/SemaCXX/ambig-user-defined-conversions.cpp
+++ clang/test/SemaCXX/ambig-user-defined-conversions.cpp
@@ -1,52 +1,51 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
-// Test1
-struct BASE { 
-  operator int &(); // expected-note {{candidate function}}
-}; 
-struct BASE1 { 
-  operator int &(); // expected-note {{candidate function}}
-}; 
+namespace test0 {
+  struct BASE { 
+    operator int &(); // expected-note {{candidate function}}
+  }; 
+  struct BASE1 { 
+    operator int &(); // expected-note {{candidate function}}
+  }; 
 
-struct B : public BASE, BASE1 { 
+  struct B : public BASE, BASE1 {};
 
-}; 
+  extern B f(); 
+  B b1;
 
-extern B f(); 
+  void func(const int ci, const char cc); // expected-note {{candidate function}}
+  void func(const char ci, const B b); // expected-note {{candidate function}}
+  void func(const B b, const int ci); // expected-note {{candidate function}}
 
-B b1;
-void func(const int ci, const char cc); // expected-note {{candidate function}}
-void func(const char ci, const B b); // expected-note {{candidate function}}
-void func(const B b, const int ci); // expected-note {{candidate function}}
-
-const int Test1() {
-  func(b1, f()); // expected-error {{call to 'func' is ambiguous}}
-  return f(); // expected-error {{conversion from 'struct B' to 'int const' is ambiguous}}
+  const int Test1() {
+    func(b1, f()); // expected-error {{call to 'func' is ambiguous}}
+    return f(); // expected-error {{conversion from 'struct test0::B' to 'int const' is ambiguous}}
+  }
 }
 
+namespace test1 {
+  struct E;
+  struct A { 
+    A (E&); 
+  };
 
-// Test2
-struct E;
-struct A { 
-  A (E&); 
-};
+  struct E { 
+    operator A (); 
+  };
 
-struct E { 
-  operator A (); 
-};
+  struct C { 
+    C (E&);  
+  };
 
-struct C { 
-  C (E&);  
-};
+  void f1(A);	// expected-note {{candidate function}}
+  void f1(C);	// expected-note {{candidate function}}
 
-void f1(A);	// expected-note {{candidate function}}
-void f1(C);	// expected-note {{candidate function}}
-
-void Test2()
-{
-	E b;
-	f1(b);  // expected-error {{call to 'f1' is ambiguous}}	
-                // ambiguous because b -> C via constructor and
-                // b → A via constructor or conversion function.
+  void Test2()
+  {
+    E b;
+    f1(b);  // expected-error {{call to 'f1' is ambiguous}}	
+            // ambiguous because b -> C via constructor and
+            // b → A via constructor or conversion function.
+  }
 }
 
