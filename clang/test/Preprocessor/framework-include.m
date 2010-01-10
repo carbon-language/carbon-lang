e0d2e342b3db36e338d70c0223580669d818bb48
Chris Lattner <sabre@nondot.org>
2010-01-10 00:59:17 +0000

    try to make this more stable?
    llvm-svn: 93090

--- clang/test/Preprocessor/framework-include.m
+++ clang/test/Preprocessor/framework-include.m
@@ -1,4 +1,4 @@
-// RUN: %clang -E -F%S %s 2>&1 | grep "Headers/foo.h:1:10: warning: published framework headers should always #import headers within the framework with framework paths"
+// RUN: %clang -E -F%S %s 2>&1 | grep "published framework headers should always #import headers within the framework with framework paths"
 
 // rdar://7520940
 #include <foo/foo.h>
