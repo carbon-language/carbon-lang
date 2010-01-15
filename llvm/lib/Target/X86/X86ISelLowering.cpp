d2968c4c12125286d33f6eb1ab4fb24257ddad6f
Dan Gohman <gohman@apple.com>
2010-01-15 22:18:15 +0000

    Fix a typo that Anton noticed.
    llvm-svn: 93563

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -7579,7 +7579,7 @@ bool X86TargetLowering::isTruncateFree(EVT VT1, EVT VT2) const {
 
 bool X86TargetLowering::isZExtFree(const Type *Ty1, const Type *Ty2) const {
   // x86-64 implicitly zero-extends 32-bit results in 64-bit registers.
-  return Ty1->isInteger(64) && Ty2->isInteger(64) && Subtarget->is64Bit();
+  return Ty1->isInteger(32) && Ty2->isInteger(64) && Subtarget->is64Bit();
 }
 
 bool X86TargetLowering::isZExtFree(EVT VT1, EVT VT2) const {
