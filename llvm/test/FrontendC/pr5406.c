7397d6c281e6a9c543384a88f37b72613956090c
Rafael Espindola <rafael.espindola@gmail.com>
2010-01-17 04:44:55 +0000

    Add test for pr5406
    llvm-svn: 93671

---
+++ llvm/test/FrontendC/pr5406.c
@@ -0,0 +1,20 @@
+// RUN: %llvmgcc %s -S -emit-llvm -O0 -o - | FileCheck %s
+// PR 5406
+
+// ARM test.
+// XFAIL: !arm
+
+typedef struct { char x[3]; } A0;
+void foo (int i, ...);
+
+
+// CHECK: call arm_aapcscc  void (i32, ...)* @foo(i32 1, i32 {{.*}}) nounwind
+int main (void)
+{
+  A0 a3;
+  a3.x[0] = 0;
+  a3.x[0] = 0;
+  a3.x[2] = 26;
+  foo (1,  a3 );
+  return 0;
+}
