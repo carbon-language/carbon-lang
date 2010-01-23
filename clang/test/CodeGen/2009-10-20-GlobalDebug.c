b2abc13f28e539f8164e06af25864d31fcd1524b
Benjamin Kramer <benny.kra@googlemail.com>
2010-01-23 10:14:58 +0000

    Adjust testcase for recent DWARF printer changes.
    llvm-svn: 94306

--- clang/test/CodeGen/2009-10-20-GlobalDebug.c
+++ clang/test/CodeGen/2009-10-20-GlobalDebug.c
@@ -1,4 +1,4 @@
 // RUN: %clang -ccc-host-triple i386-apple-darwin10 -S -g -dA %s -o - | FileCheck %s
 int global;
-// CHECK: asciz  "global"                                    ## DW_AT_name
+// CHECK: asciz "global" ## External Name
 int main() { return 0;}
