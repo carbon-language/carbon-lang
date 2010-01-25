fe28ef686ed9e2afa644bd1e5840e3212dbf8686
Chris Lattner <sabre@nondot.org>
2010-01-25 03:37:52 +0000

    consolidate two testcases.
    llvm-svn: 94403

--- clang/test/CodeGenObjC/debug-info-crash.m
+++ clang/test/CodeGenObjC/debug-info-crash.m
@@ -7,3 +7,18 @@
 }
 @end
 
+// PR4894
+@interface I0 {
+  I0 *_iv0;
+}
+@end
+@protocol P0 @end
+
+@interface I1 @end
+@implementation I1
+- (I0<P0> *) im0 {
+  // CHECK: @"\01-[I1 im0]"
+  // CHECK: llvm.dbg.func.start
+  return 0;
+}
+@end
