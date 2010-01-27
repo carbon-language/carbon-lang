483d08a25a0ba52036677ae93b123a24c08fb553
Anders Carlsson <andersca@mac.com>
2010-01-27 02:49:52 +0000

    Fix attr.cpp by not checking the generated assembler output.
    llvm-svn: 94651

--- clang/test/CodeGenCXX/attr.cpp
+++ clang/test/CodeGenCXX/attr.cpp
@@ -1,13 +1,8 @@
-// RUN: %clang_cc1 -triple x86_64-apple-darwin -O0 -S %s -o %t.s
-// RUN: FileCheck --input-file=%t.s %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin -emit-llvm -o - %s | FileCheck %s
 
+// CHECK: define i32 @_Z3foov() nounwind align 1024
 int foo() __attribute__((aligned(1024)));
 int foo() { }
-
-// CHECK:.align 10, 0x90
-// CHECK:.globl __Z3foov
-// CHECK:__Z3foov:
-
 
 class C {
   virtual void bar1() __attribute__((aligned(1)));
@@ -15,25 +10,11 @@ class C {
   virtual void bar3() __attribute__((aligned(1024)));
 } c;
 
-// CHECK:.align 1, 0x90
-// CHECK-NEXT:.globl __ZN1CC1Ev
-
+// CHECK: define void @_ZN1C4bar1Ev(%class.C* %this) nounwind align 2
 void C::bar1() { }
 
-// CHECK:.align 1, 0x90
-// CHECK-NEXT:.globl __ZN1C4bar1Ev
-// CHECK-NEXT:__ZN1C4bar1Ev:
-
-
+// CHECK: define void @_ZN1C4bar2Ev(%class.C* %this) nounwind align 2
 void C::bar2() { }
 
-// CHECK:.align 1, 0x90
-// CHECK-NEXT:.globl __ZN1C4bar2Ev
-// CHECK-NEXT:__ZN1C4bar2Ev:
-
-
+// CHECK: define void @_ZN1C4bar3Ev(%class.C* %this) nounwind align 1024
 void C::bar3() { }
-
-// CHECK:.align 10, 0x90
-// CHECK-NEXT:.globl __ZN1C4bar3Ev
-// CHECK-NEXT:__ZN1C4bar3Ev:
