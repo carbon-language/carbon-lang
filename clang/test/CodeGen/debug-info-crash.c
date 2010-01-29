156b11368a83791f9b2dc1f676c2b6640684c205
Devang Patel <dpatel@apple.com>
2010-01-29 18:32:33 +0000

    New test case.
    llvm-svn: 94821

---
+++ clang/test/CodeGen/debug-info-crash.c
@@ -0,0 +1,21 @@
+// RUN: %clang_cc1 -triple i386-apple-darwin10 -fblocks -g -S %s -o -
+
+// rdar://7590323
+typedef struct dispatch_queue_s *dispatch_queue_t;
+__attribute__((visibility("default")))
+extern struct dispatch_queue_s _dispatch_main_q;
+typedef struct dispatch_item_s *dispatch_item_t;
+typedef void (^dispatch_legacy_block_t)(dispatch_item_t);
+dispatch_item_t LEGACY_dispatch_call(dispatch_queue_t dq,
+                                     dispatch_legacy_block_t dispatch_block,
+                                     dispatch_legacy_block_t callback_block) {
+  dispatch_queue_t lq = _dispatch_queue_get_current() ?: (&_dispatch_main_q);
+  dispatch_async(dq, ^{
+      if (callback_block) {
+        dispatch_async(lq, ^{
+          }
+          );
+      }
+    }
+    );
+}
