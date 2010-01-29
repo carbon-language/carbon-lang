aa6050b0e2dd3bad5db7ef769da3af61d84cb413
Douglas Gregor <dgregor@apple.com>
2010-01-29 19:42:41 +0000

    PR5909 had a test case for binding of const, non-volatile references to bitfields. Add it here.
    llvm-svn: 94832

---
+++ clang/test/CXX/dcl.decl/dcl.init/dcl.init.ref/p5.cpp
@@ -0,0 +1,12 @@
+// RUN: %clang_cc1 -fsyntax-only -verify %s
+
+namespace PR5909 {
+  struct Foo {
+    int x : 20;
+  };
+  
+  bool Test(const int& foo);
+  
+  const Foo f = { 0 };  // It compiles without the 'const'.
+  bool z = Test(f.x);
+}
