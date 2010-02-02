2cfade1671b8de84be3b2416d8c527587e37a10a
Anders Carlsson <andersca@mac.com>
2010-02-02 03:23:38 +0000

    Rename a test.
    llvm-svn: 95060

--- clang/test/CodeGenCXX/pointers-to-data-members.cpp
+++ clang/test/CodeGenCXX/pointers-to-data-members.cpp
@@ -1,21 +1,26 @@
-// RUN: %clang_cc1 %s -emit-llvm -o - -triple=x86_64-apple-darwin9 | FileCheck %s
+// RUN: %clang_cc1 %s -emit-llvm -o - -triple=x86_64-apple-darwin10 | FileCheck %s
 
 struct A { int a; };
 struct B { int b; };
 struct C : B, A { };
 
+// Casts.
+namespace Casts {
+
 int A::*pa;
 int C::*pc;
 
 void f() {
-  // CHECK: store i64 -1, i64* @pa
+  // CHECK: store i64 -1, i64* @_ZN5Casts2paE
   pa = 0;
 
   // CHECK: [[ADJ:%[a-zA-Z0-9\.]+]] = add i64 {{.*}}, 4
-  // CHECK: store i64 [[ADJ]], i64* @pc
+  // CHECK: store i64 [[ADJ]], i64* @_ZN5Casts2pcE
   pc = pa;
 
   // CHECK: [[ADJ:%[a-zA-Z0-9\.]+]] = sub i64 {{.*}}, 4
-  // CHECK: store i64 [[ADJ]], i64* @pa
+  // CHECK: store i64 [[ADJ]], i64* @_ZN5Casts2paE
   pa = static_cast<int A::*>(pc);
+}
+
 }
