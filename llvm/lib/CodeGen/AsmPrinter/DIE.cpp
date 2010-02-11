7c2005aa6d1caa99e06146dc61970738d437d6fc
Bill Wendling <isanbard@gmail.com>
2010-02-11 01:15:27 +0000

    Fix to get it to compile.
    llvm-svn: 95840

--- llvm/lib/CodeGen/AsmPrinter/DIE.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DIE.cpp
@@ -313,7 +313,7 @@ void DIESectionOffset::EmitValue(DwarfPrinter *D, unsigned Form) const {
   D->EmitSectionOffset(Label.getTag(), Section.getTag(),
                        Label.getNumber(), Section.getNumber(),
                        IsSmall, IsEH, UseSet);
-  EOL(""); // FIXME: Necesssary?
+  D->getAsm()->O << '\n'; // FIXME: Necesssary?
 }
 
 /// SizeOf - Determine size of delta value in bytes.
