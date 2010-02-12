0e4df63bd5544cb48f07028b3d0cc5e3155dfdee
Evan Cheng <evan.cheng@apple.com>
2010-02-12 07:48:46 +0000

    Update test to match 95961.
    llvm-svn: 95971

--- llvm/test/FrontendC/2007-02-16-WritableStrings.c
+++ llvm/test/FrontendC/2007-02-16-WritableStrings.c
@@ -1,7 +1,7 @@
 // Test the -fwritable-strings option.
 
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm -fwritable-strings %s | \
-// RUN:    grep {private global}
+// RUN:    grep {internal global}
 // RUN: %llvmgcc -O3 -S -o - -emit-llvm %s | grep {private constant}
 
 char *X = "foo";
