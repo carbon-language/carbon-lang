c9adb6a463dd06bd6f48207f077ca1c370a3516c
Anton Korobeynikov <asl@math.spbu.ru>
2010-02-15 22:38:10 +0000

    Fix a silly darwin-only typo introduced during merge.
    llvm-svn: 96289

--- llvm/lib/Target/X86/X86TargetObjectFile.cpp
+++ llvm/lib/Target/X86/X86TargetObjectFile.cpp
@@ -56,15 +56,14 @@ getSymbolForDwarfGlobalReference(const GlobalValue *GV, Mangler *Mang,
 
   // On Darwin/X86-64, we can reference dwarf symbols with foo@GOTPCREL+4, which
   // is an indirect pc-relative reference.
-  if ((Encoding & DW_EH_PE_indirect) &&
-      (Encoding & DW_EH_PE_pcrel)) {
+  if (Encoding & (DW_EH_PE_indirect | DW_EH_PE_pcrel)) {
     SmallString<128> Name;
     Mang->getNameWithPrefix(Name, GV, false);
-  const MCSymbol *Sym = getContext().CreateSymbol(Name);
-  const MCExpr *Res =
-    X86MCTargetExpr::Create(Sym, X86MCTargetExpr::GOTPCREL, getContext());
-  const MCExpr *Four = MCConstantExpr::Create(4, getContext());
-  return MCBinaryExpr::CreateAdd(Res, Four, getContext());
+    const MCSymbol *Sym = getContext().GetOrCreateSymbol(Name);
+    const MCExpr *Res =
+      X86MCTargetExpr::Create(Sym, X86MCTargetExpr::GOTPCREL, getContext());
+    const MCExpr *Four = MCConstantExpr::Create(4, getContext());
+    return MCBinaryExpr::CreateAdd(Res, Four, getContext());
   }
 
   return TargetLoweringObjectFileMachO::
