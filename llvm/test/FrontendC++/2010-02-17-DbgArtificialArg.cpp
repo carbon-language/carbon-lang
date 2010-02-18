441eb781ae20447e9d9b9ffc314ab4a9b8d9babc
Devang Patel <dpatel@apple.com>
2010-02-18 19:52:12 +0000

    Ignore target dependent value in grep search.
    llvm-svn: 96604

--- llvm/test/FrontendC++/2010-02-17-DbgArtificialArg.cpp
+++ llvm/test/FrontendC++/2010-02-17-DbgArtificialArg.cpp
@@ -1,4 +1,4 @@
-// RUN: %llvmgcc -g -S %s -o - | grep DW_TAG_pointer_type |  grep "i32 458767, metadata .., metadata ..., metadata .., i32 0, i64 64, i64 64, i64 0, i32 64, metadata ..."
+// RUN: %llvmgcc -g -S %s -o - | grep DW_TAG_pointer_type |  grep "i32 458767, metadata .., metadata ..., metadata .., i32 ., i64 .., i64 .., i64 0, i32 64, metadata ..."
 // Here, second to last argument "i32 64" indicates that artificial type is set.                                               
 // Test to artificial attribute attahed to "this" pointer type.
 // Radar 7655792 and 7655002
