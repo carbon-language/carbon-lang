1f9e9ac766bb1ff6800d802015aaed30eddadb5e
Devang Patel <dpatel@apple.com>
2010-02-19 02:58:33 +0000

    Test case for r96656.
    llvm-svn: 96657

---
+++ llvm/test/FrontendC/2010-02-18-Dbg-VectorType.c
@@ -0,0 +1,9 @@
+// RUN: %llvmgcc -S -O0 -g %s -o - | grep DW_TAG_typedef | grep float4
+typedef float float4 __attribute__((vector_size(16)));
+
+int main(){
+  volatile float4 x = (float4) { 0.0f, 1.0f, 2.0f, 3.0f };
+  x += x;
+  return 0;
+}
+
