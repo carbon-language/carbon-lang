8328341d9cad9c0d28a07eae9a4557f47cdee05d
David Greene <greened@obbligato.org>
2010-02-24 20:59:49 +0000

    Use a SmallPtrSet as suggested by Chris.
    llvm-svn: 97056

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -6344,8 +6344,8 @@ bool ShuffleVectorSDNode::isSplatMask(const int *Mask, EVT VT) {
 }
 
 static void checkForCyclesHelper(const SDNode *N,
-                                 std::set<const SDNode *> &visited,
-                                 std::set<const SDNode *> &checked) {
+                                 SmallPtrSet<const SDNode *, 32> &visited,
+                                 SmallPtrSet<const SDNode *, 32> &checked) {
   if (checked.find(N) != checked.end())
     return;
 
@@ -6371,8 +6371,8 @@ static void checkForCyclesHelper(const SDNode *N,
 void llvm::checkForCycles(const llvm::SDNode *N) {
 #ifdef XDEBUG
   assert(N && "Checking nonexistant SDNode");
-  std::set<const SDNode *> visited;
-  std::set<const SDNode *> checked;
+  SmallPtrSet<const SDNode *, 32> visited;
+  SmallPtrSet<const SDNode *, 32> checked;
   checkForCyclesHelper(N, visited, checked);
 #endif
 }
