e334bb1a6640e5b7b0c0a8ff0c43e2b7605e5603
Zhongxing Xu <xuzhongxing@gmail.com>
2010-02-27 02:44:37 +0000

    Add test case for inlining call analysis.
    llvm-svn: 97300

---
+++ clang/test/Analysis/inline.c
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -analyze -inline-call -analyzer-store region -analyze-function f2 -verify %s
+
+int f1() {
+  int y = 1;
+  y++;
+  return y;
+}
+
+void f2() {
+  int x = 1;
+  x = f1();
+  if (x == 1) {
+    int *p = 0;
+    *p = 3; // no-warning
+  }
+  if (x == 2) {
+    int *p = 0;
+    *p = 3; // expected-warning{{Dereference of null pointer loaded from variable}}
+  }
+}
