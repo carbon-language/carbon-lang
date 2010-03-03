6b11b4e050f23ef151df97045503a7c4353e0cf1
Zhongxing Xu <xuzhongxing@gmail.com>
2010-03-03 01:02:48 +0000

    Add comments to test case.
    llvm-svn: 97619

--- clang/test/Analysis/inline2.c
+++ clang/test/Analysis/inline2.c
@@ -2,6 +2,8 @@
 
 // Test parameter 'a' is registered to LiveVariables analysis data although it
 // is not referenced in the function body. 
+// Before processing 'return 1;', in RemoveDeadBindings(), we query the liveness
+// of 'a', because we have a binding for it due to parameter passing.
 int f1(int a) {
   return 1;
 }
