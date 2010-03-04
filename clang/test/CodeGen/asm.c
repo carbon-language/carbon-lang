eaa2778f278a96192718d1c88852dde77dc140b4
Chris Lattner <sabre@nondot.org>
2010-03-04 00:38:16 +0000

    fix a buildbot failure, this was passing for me because the '%t' file was left around on my disk.  doh.
    llvm-svn: 97699

--- clang/test/CodeGen/asm.c
+++ clang/test/CodeGen/asm.c
@@ -28,14 +28,16 @@ void t6(void) {
   __asm__ volatile("" : : "i" (t6));
 }
 
-// RUN: grep "T7 NAMED: \$1" %t
 void t7(int a) {
   __asm__ volatile("T7 NAMED: %[input]" : "+r"(a): [input] "i" (4));
+  // CHECK: @t7(i32
+  // CHECK: T7 NAMED: $1
 }
 
-// RUN: grep "T8 NAMED MODIFIER: \${0:c}" %t
 void t8() {
   __asm__ volatile("T8 NAMED MODIFIER: %c[input]" :: [input] "i" (4));
+  // CHECK: @t8()
+  // CHECK: T8 NAMED MODIFIER: ${0:c}
 }
 
 // PR3682
@@ -45,9 +47,11 @@ unsigned t9(unsigned int a) {
 }
 
 // PR3908
-// RUN: grep "PR3908 \$1 \$3 \$2 \$0" %t
 void t10(int r) {
   __asm__("PR3908 %[lf] %[xx] %[li] %[r]" : [r] "+r" (r) : [lf] "mx" (0), [li] "mr" (0), [xx] "x" ((double)(0)));
+  
+// CHECK: @t10(
+// CHECK:PR3908 $1 $3 $2 $0
 }         
 
 
