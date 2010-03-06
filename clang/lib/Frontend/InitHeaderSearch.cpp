8341c390aae10acf3305f63e6147316b1e7cbe44
Chris Lattner <sabre@nondot.org>
2010-03-06 19:38:10 +0000

    on both the mac and linux, /usr/local/include is treated as a "C++ Friendly" system header directory.  This fixes PR6523.
    llvm-svn: 97885

--- clang/lib/Frontend/InitHeaderSearch.cpp
+++ clang/lib/Frontend/InitHeaderSearch.cpp
@@ -436,7 +436,7 @@ void InitHeaderSearch::AddDefaultCIncludePaths(const llvm::Triple &triple) {
     break;
   }
 
-  AddPath("/usr/local/include", System, false, false, false);
+  AddPath("/usr/local/include", System, true, false, false);
   AddPath("/usr/include", System, false, false, false);
 }
 
