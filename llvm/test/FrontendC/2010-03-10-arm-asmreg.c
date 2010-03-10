dfebf1ffac7f9a5808c677b45374c701f5bfd35a
Bob Wilson <bob.wilson@apple.com>
2010-03-10 17:54:11 +0000

    Testcase for pr6552.  I changed the code to use "ip" instead of "fp" because the "fp" register name is not valid on Darwin, and the "ip" register name was broken for all ARM targets.
    llvm-svn: 98166

---
+++ llvm/test/FrontendC/2010-03-10-arm-asmreg.c
@@ -0,0 +1,15 @@
+// RUN: %llvmgcc %s -S -O0 -o - | FileCheck %s
+// pr6552
+
+// XFAIL: *
+// XTARGET: arm
+
+extern void bar(unsigned int ip);
+
+// CHECK: mov r0, r12
+void foo(void)
+{
+  register unsigned int ip __asm ("ip");
+  bar(ip);
+}
+
