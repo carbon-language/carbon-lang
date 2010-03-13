d324a7c9902a19eedaa3bd86a12d181dfd6e1d99
Daniel Dunbar <daniel@zuster.org>
2010-03-13 22:49:39 +0000

    X86: Fix ADD64i32 encoding.
    llvm-svn: 98457

--- llvm/test/MC/AsmParser/X86/x86_64-new-encoder.s
+++ llvm/test/MC/AsmParser/X86/x86_64-new-encoder.s
@@ -24,3 +24,6 @@ movq	$12, foo(%rip)
 // CHECK:  movq	$12, foo(%rip)
 // CHECK: encoding: [0x48,0xc7,0x05,A,A,A,A,0x0c,0x00,0x00,0x00]
 // CHECK:    fixup A - offset: 3, value: foo-8, kind: reloc_riprel_4byte
+
+// CHECK: addq	$-424, %rax             # encoding: [0x48,0x05,0x58,0xfe,0xff,0xff]
+addq $-424, %rax
