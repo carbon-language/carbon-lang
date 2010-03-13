3d73d1a73c36c0b765f4c526f7558cc21b7a35c0
Jeffrey Yasskin <jyasskin@google.com>
2010-03-13 01:39:20 +0000

    Remove a memory leak from MetadataTest.
    Tested: valgrind --leak-check=full unittests/VMCore/Debug/VMCoreTests
    llvm-svn: 98412

--- llvm/unittests/VMCore/MetadataTest.cpp
+++ llvm/unittests/VMCore/MetadataTest.cpp
@@ -132,9 +132,9 @@ TEST(NamedMDNodeTest, Search) {
 
   MDNode *Nodes[2] = { n, n2 };
 
-  Module *M = new Module("MyModule", Context);
+  Module M("MyModule", Context);
   const char *Name = "llvm.NMD1";
-  NamedMDNode *NMD = NamedMDNode::Create(Context, Name, &Nodes[0], 2, M);
+  NamedMDNode *NMD = NamedMDNode::Create(Context, Name, &Nodes[0], 2, &M);
   std::string Str;
   raw_string_ostream oss(Str);
   NMD->print(oss);
