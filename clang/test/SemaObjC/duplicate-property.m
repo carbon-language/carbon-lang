e01bec979144dce99e9a2588590373fc95e1a3d1
Ted Kremenek <kremenek@apple.com>
2010-03-15 18:47:29 +0000

    Add 'expected-note'
    llvm-svn: 98560

--- clang/test/SemaObjC/duplicate-property.m
+++ clang/test/SemaObjC/duplicate-property.m
@@ -3,6 +3,6 @@
 @interface Foo {
   id x;
 }
-@property (nonatomic, retain) id x;
+@property (nonatomic, retain) id x; // expected-note{{property declared here}}
 @property (nonatomic, retain) id x; // expected-error{{property has a previous declaration}}
 @end
