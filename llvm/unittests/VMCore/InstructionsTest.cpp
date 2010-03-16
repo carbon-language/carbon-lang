86ca5493f7c58d3d9ad565f65028d2ffa51e2cde
Gabor Greif <ggreif@gmail.com>
2010-03-16 11:24:53 +0000

    add single return tests
    llvm-svn: 98625

--- llvm/unittests/VMCore/InstructionsTest.cpp
+++ llvm/unittests/VMCore/InstructionsTest.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Instructions.h"
+#include "llvm/DerivedTypes.h"
 #include "llvm/LLVMContext.h"
 #include "gtest/gtest.h"
 
@@ -20,6 +21,16 @@ TEST(InstructionsTest, ReturnInst) {
   // test for PR6589
   const ReturnInst* r0 = ReturnInst::Create(C);
   EXPECT_EQ(r0->op_begin(), r0->op_end());
+
+  const IntegerType* Int1 = IntegerType::get(C, 1);
+  Constant* One = ConstantInt::get(Int1, 1, true);
+  const ReturnInst* r1 = ReturnInst::Create(C, One);
+  User::const_op_iterator b(r1->op_begin());
+  EXPECT_NE(b, r1->op_end());
+  EXPECT_EQ(*b, One);
+  EXPECT_EQ(r1->getOperand(0), One);
+  ++b;
+  EXPECT_EQ(b, r1->op_end());
 }
 
 }  // end anonymous namespace
