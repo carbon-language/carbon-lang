5444aa6d3d989cd2efc57bf0d28b8e693fe19fc9
Douglas Gregor <dgregor@apple.com>
2010-03-17 00:09:23 +0000

    Emit output of PCH consistency checking test case to a separate text file and grep that
    llvm-svn: 98695

--- clang/test/PCH/changed-files.c
+++ clang/test/PCH/changed-files.c
@@ -5,15 +5,18 @@ const char *s2 = m0;
 // RUN: echo '#define m0 ""' > %t.h
 // RUN: %clang_cc1 -emit-pch -o %t.h.pch %t.h
 // RUN: echo '' > %t.h
-// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2>&1 | grep "size of file"
+// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2> %t.stderr
+// RUN: grep "size of file" %t.stderr
 
 // RUN: echo '#define m0 000' > %t.h
 // RUN: %clang_cc1 -emit-pch -o %t.h.pch %t.h
 // RUN: echo '' > %t.h
-// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2>&1 | grep "size of file"
+// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2> %t.stderr
+// RUN: grep "size of file" %t.stderr
 
 // RUN: echo '#define m0 000' > %t.h
 // RUN: echo "#define m1 'abcd'" >> %t.h
 // RUN: %clang_cc1 -emit-pch -o %t.h.pch %t.h
 // RUN: echo '' > %t.h
-// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2>&1 | grep "size of file"
+// RUN: not %clang_cc1 -include-pch %t.h.pch %s 2> %t.stderr
+// RUN: grep "size of file" %t.stderr
