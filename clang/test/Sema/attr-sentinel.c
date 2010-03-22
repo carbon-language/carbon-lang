e783d10bc0a58d27e91e4bbdd8e6dcef6f7dc114
Chris Lattner <sabre@nondot.org>
2010-03-22 21:02:14 +0000

    merge two tests.
    llvm-svn: 99209

--- clang/test/Sema/attr-sentinel.c
+++ clang/test/Sema/attr-sentinel.c
@@ -11,9 +11,7 @@ void foo7 (int x, ...) __attribute__ ((__sentinel__(0))); // expected-note {{fun
 void foo10 (int x, ...) __attribute__ ((__sentinel__(1,1)));
 void foo12 (int x, ... ) ATTR; // expected-note {{function has been explicitly marked sentinel here}}
 
-int main ()
-{
-
+void test1() {
   foo1(1, NULL); // OK
   foo1(1, 0) ; // expected-warning {{missing sentinel in function call}}
   foo5(1, NULL, 2);  // OK
@@ -28,3 +26,23 @@ int main ()
   foo12(1); // expected-warning {{not enough variable arguments in 'foo12' declaration to fit a sentinel}}
 }
  
+
+
+void (*e) (int arg, const char * format, ...) __attribute__ ((__sentinel__ (1,1)));
+
+void test2() {
+  void (*b) (int arg, const char * format, ...) __attribute__ ((__sentinel__));  // expected-note {{function has been explicitly marked sentinel here}}
+  void (*z) (int arg, const char * format, ...) __attribute__ ((__sentinel__ (2))); // expected-note {{function has been explicitly marked sentinel here}}
+  
+  
+  void (*y) (int arg, const char * format, ...) __attribute__ ((__sentinel__ (5))); // expected-note {{function has been explicitly marked sentinel here}}
+  
+  b(1, "%s", (void*)0); // OK
+  b(1, "%s", 0);  // expected-warning {{missing sentinel in function call}}
+  z(1, "%s",4 ,1,0);  // expected-warning {{missing sentinel in function call}}
+  z(1, "%s", (void*)0, 1, 0); // OK
+  
+  y(1, "%s", 1,2,3,4,5,6,7);  // expected-warning {{missing sentinel in function call}}
+  
+  y(1, "%s", (void*)0,3,4,5,6,7); // OK
+}
