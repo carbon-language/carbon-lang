cb5a828a4584934eb40b4c2274b33964009785ba
Chris Lattner <sabre@nondot.org>
2010-03-22 21:05:15 +0000

    merge some tests.
    llvm-svn: 99212

--- clang/test/Sema/attr-format.c
+++ clang/test/Sema/attr-format.c
@@ -32,3 +32,39 @@ int rdar6623513(void *, const char*, const char*, ...)
 int rdar6623513_aux(int len, const char* s) {
   rdar6623513(0, "hello", "%.*s", len, s);
 }
+
+
+
+// same as format(printf(...))...
+void a2(const char *a, ...)    __attribute__((format(printf0, 1,2))); // no-error
+void b2(const char *a, ...)    __attribute__((format(printf0, 1,1))); // expected-error {{'format' attribute parameter 3 is out of bounds}}
+void c2(const char *a, ...)    __attribute__((format(printf0, 0,2))); // expected-error {{'format' attribute parameter 2 is out of bounds}}
+void d2(const char *a, int c)  __attribute__((format(printf0, 1,2))); // expected-error {{format attribute requires variadic function}}
+void e2(char *str, int c, ...) __attribute__((format(printf0, 2,3))); // expected-error {{format argument not a string type}}
+
+// FreeBSD usage
+#define __printf0like(fmt,va) __attribute__((__format__(__printf0__,fmt,va)))
+void null(int i, const char *a, ...) __printf0like(2,0); // no-error
+void null(int i, const char *a, ...) {
+  if (a)
+    (void)0/* vprintf(...) would go here */;
+}
+
+void callnull(void){
+  null(0,        0); // no error
+  null(0, (char*)0); // no error
+  null(0, (void*)0); // no error
+  null(0,  (int*)0); // expected-warning {{incompatible pointer types}}
+}
+
+
+
+// PR4470
+int xx_vprintf(const char *, va_list);
+
+const char *foo(const char *format) __attribute__((format_arg(1)));
+
+void __attribute__((format(printf, 1, 0)))
+foo2(const char *fmt, va_list va) {
+  xx_vprintf(foo(fmt), va);
+}
