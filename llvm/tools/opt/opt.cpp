38da2b015f36cff318d1ab2cc8d23f66b5419c65
Jeffrey Yasskin <jyasskin@google.com>
2010-03-22 15:56:04 +0000

    Avoid leaking the FunctionPassManager from opt.
    llvm-svn: 99180

--- llvm/tools/opt/opt.cpp
+++ llvm/tools/opt/opt.cpp
@@ -424,9 +424,9 @@ int main(int argc, char **argv) {
   if (TD)
     Passes.add(TD);
 
-  FunctionPassManager *FPasses = NULL;
+  OwningPtr<FunctionPassManager> FPasses;
   if (OptLevelO1 || OptLevelO2 || OptLevelO3) {
-    FPasses = new FunctionPassManager(M.get());
+    FPasses.reset(new FunctionPassManager(M.get()));
     if (TD)
       FPasses->add(new TargetData(*TD));
   }
