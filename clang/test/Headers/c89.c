445a2c7b5cf40f802137681319c30f6a66ac1848
Chris Lattner <sabre@nondot.org>
2010-03-26 17:51:13 +0000

    i386-apple-darwin10 implies sse2, this option shouldn't be needed.
    llvm-svn: 99627

--- clang/test/Headers/c89.c
+++ clang/test/Headers/c89.c
@@ -1,4 +1,4 @@
-// RUN: %clang -triple=i386-apple-darwin10 -msse2 -fsyntax-only -verify -std=c89 %s
+// RUN: %clang -triple=i386-apple-darwin10 -fsyntax-only -verify -std=c89 %s
 
 // PR6658
 #include <xmmintrin.h>
