16204101dff606552f0ecc5b372aabd0452067bb
Daniel Dunbar <daniel@zuster.org>
2010-03-26 19:02:07 +0000

    Spell -triple correctly for %clang, not %clang_cc1.
    llvm-svn: 99634

--- clang/test/Headers/c89.c
+++ clang/test/Headers/c89.c
@@ -1,4 +1,4 @@
-// RUN: %clang -triple=i386-apple-darwin10 -fsyntax-only -verify -std=c89 %s
+// RUN: %clang -ccc-host-triple i386-apple-darwin10 -fsyntax-only -verify -std=c89 %s
 
 // PR6658
 #include <xmmintrin.h>
