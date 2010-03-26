c5260901e9303640adcfe178514b71f44bb63c27
Ted Kremenek <kremenek@apple.com>
2010-03-26 01:34:53 +0000

    Update test case since c-index-test now works on invalid sources.
    llvm-svn: 99595

--- clang/test/Index/cindex-on-invalid.m
+++ clang/test/Index/cindex-on-invalid.m
@@ -1,7 +1,6 @@
-// RUN: not c-index-test -test-load-source local %s > %t 2> %t.err
-// RUN: FileCheck %s < %t.err
-// CHECK: error: expected identifier or '('
-// CHECK: Unable to load translation unit!
+// RUN: c-index-test -test-load-source local %s 2>&1 | FileCheck %s
 
 int foo;
 int
+
+// CHECK: cindex-on-invalid.m:6:70: error: expected identifier or '('
