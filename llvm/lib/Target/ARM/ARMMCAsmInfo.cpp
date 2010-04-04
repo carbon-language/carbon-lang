407f8488358c2e2ad32f2662900aa582d0328989
Chris Lattner <sabre@nondot.org>
2010-04-04 21:37:20 +0000

    fix pasto, this is the wrong setting for arm elf.
    llvm-svn: 100357

--- llvm/lib/Target/ARM/ARMMCAsmInfo.cpp
+++ llvm/lib/Target/ARM/ARMMCAsmInfo.cpp
@@ -58,7 +58,6 @@ ARMELFMCAsmInfo::ARMELFMCAsmInfo() {
   CommentString = "@";
 
   HasLEB128 = true;
-  AbsoluteDebugSectionOffsets = true;
   PrivateGlobalPrefix = ".L";
   WeakRefDirective = "\t.weak\t";
   HasLCOMMDirective = true;
