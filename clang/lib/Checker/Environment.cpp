973b5f55f85428085c2206920a96d2c5496a8615
Zhongxing Xu <xuzhongxing@gmail.com>
2010-04-05 13:00:12 +0000

    Since now we process regions in clusters when removing dead bindings, this code can be removed.
    llvm-svn: 100428

--- clang/lib/Checker/Environment.cpp
+++ clang/lib/Checker/Environment.cpp
@@ -137,14 +137,6 @@ EnvironmentManager::RemoveDeadBindings(Environment Env, const Stmt *S,
       if (isa<loc::MemRegionVal>(X)) {
         const MemRegion* R = cast<loc::MemRegionVal>(X).getRegion();
         DRoots.push_back(R);
-        // Mark the super region of the RX as live.
-        // e.g.: int x; char *y = (char*) &x; if (*y) ...
-        // 'y' => element region. 'x' is its super region.
-        // We only add one level super region for now.
-
-        // FIXME: maybe multiple level of super regions should be added.
-        if (const SubRegion *SR = dyn_cast<SubRegion>(R))
-          DRoots.push_back(SR->getSuperRegion());
       }
 
       // Mark all symbols in the block expr's value live.
