802e1088de64ab80f859eed0899dc8607e2929dc
Chris Lattner <sabre@nondot.org>
2010-04-05 22:49:48 +0000

    update unit test for api change.
    llvm-svn: 100486

--- llvm/unittests/ExecutionEngine/JIT/JITTest.cpp
+++ llvm/unittests/ExecutionEngine/JIT/JITTest.cpp
@@ -701,9 +701,7 @@ ExecutionEngine *getJITFromBitcode(
   LLVMContext &Context, const std::string &Bitcode, Module *&M) {
   // c_str() is null-terminated like MemoryBuffer::getMemBuffer requires.
   MemoryBuffer *BitcodeBuffer =
-    MemoryBuffer::getMemBuffer(Bitcode.c_str(),
-                               Bitcode.c_str() + Bitcode.size(),
-                               "Bitcode for test");
+    MemoryBuffer::getMemBuffer(Bitcode, "Bitcode for test");
   std::string errMsg;
   M = getLazyBitcodeModule(BitcodeBuffer, Context, &errMsg);
   if (M == NULL) {
