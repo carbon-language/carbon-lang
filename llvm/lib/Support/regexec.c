f93bdd1024af379cbd30bd083a62e68d97867bd4
Owen Anderson <resistor@mac.com>
2010-04-05 17:50:20 +0000

    Push const through the regex engine. Fixes some of the warnings in PR6616.
    llvm-svn: 100438

--- llvm/lib/Support/regexec.c
+++ llvm/lib/Support/regexec.c
@@ -155,7 +155,7 @@ llvm_regexec(const llvm_regex_t *preg, const char *string, size_t nmatch,
 	eflags = GOODFLAGS(eflags);
 
 	if (g->nstates <= (long)(CHAR_BIT*sizeof(states1)) && !(eflags&REG_LARGE))
-		return(smatcher(g, (char *)string, nmatch, pmatch, eflags));
+		return(smatcher(g, string, nmatch, pmatch, eflags));
 	else
-		return(lmatcher(g, (char *)string, nmatch, pmatch, eflags));
+		return(lmatcher(g, string, nmatch, pmatch, eflags));
 }
