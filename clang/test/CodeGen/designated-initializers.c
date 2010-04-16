1c5c30fdd39ac1e730cb48156fc8d95224651f92
Nuno Lopes <nunoplopes@sapo.pt>
2010-04-16 21:19:39 +0000

    add another test for the undef patch just for to have peace of mind :) this follows from C99 6.7.8p10: if it is a union, the first named member is initialized
    llvm-svn: 101539

--- clang/test/CodeGen/designated-initializers.c
+++ clang/test/CodeGen/designated-initializers.c
@@ -11,6 +11,9 @@ union { int i; float f; } u = { };
 // CHECK: @u2 = global %0 { i32 0, [4 x i8] undef }
 union { int i; double f; } u2 = { };
 
+// CHECK: @u3 = global %1 zeroinitializer
+union { double f; int i; } u3 = { };
+
 // CHECK: @b = global [2 x i32] [i32 0, i32 22]
 int b[2] = {
   [1] = 22
