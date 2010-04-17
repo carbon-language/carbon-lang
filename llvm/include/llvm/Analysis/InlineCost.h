0b685eb778712db4f06da739401e739e96c40554
Dale Johannesen <dalej@apple.com>
2010-04-17 19:56:46 +0000

    Add comment (lost when reverting and reapplying 101503).
    llvm-svn: 101664

--- llvm/include/llvm/Analysis/InlineCost.h
+++ llvm/include/llvm/Analysis/InlineCost.h
@@ -165,6 +165,8 @@ namespace llvm {
       void analyzeFunction(Function *F);
     };
 
+    // The Function* for a function can be changed (by ArgumentPromotion);
+    // the ValueMap will update itself when this happens.
     ValueMap<const Function *, FunctionInfo> CachedFunctionInfo;
 
   public:
