359675d036fb2bedf5406c165338ea5c08280968
John Thompson <John.Thompson.JTSoftware@gmail.com>
2010-04-20 05:01:46 +0000

    Altivec vector literal initializer count mismatch error removed.
    llvm-svn: 101868

--- clang/test/Sema/altivec-init.c
+++ clang/test/Sema/altivec-init.c
@@ -6,7 +6,7 @@ typedef short v8 __attribute((vector_size(16)));
 v8 foo(void) { 
   v8 a;
   v4 b;
-  a = (v8){4, 2}; // expected-error {{too few elements in vector initialization (expected 8 elements, have 2)}}
+  a = (v8){4, 2};
   b = (v4)(5, 6, 7, 8, 9); // expected-warning {{excess elements in vector initializer}}
   b = (v4)(5, 6, 8, 8.0f);
   return (v8){0, 1, 2, 3, 1, 2, 3, 4};
