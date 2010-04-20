877aaa9706e8bed893cebfc292a2ca305ec30663
Fariborz Jahanian <fjahanian@apple.com>
2010-04-20 20:28:15 +0000

    Patch to support transparent_union types on objective-c methods. Fixes radar 7875968.
    llvm-svn: 101935

--- clang/lib/Sema/SemaExprObjC.cpp
+++ clang/lib/Sema/SemaExprObjC.cpp
@@ -216,6 +216,11 @@ bool Sema::CheckMessageArgumentTypes(Expr **Args, unsigned NumArgs,
 
     AssignConvertType Result =
       CheckSingleAssignmentConstraints(lhsType, argExpr);
+    if (Result == Incompatible && !getLangOptions().CPlusPlus &&
+        CheckTransparentUnionArgumentConstraints(lhsType, argExpr)
+        == Sema::Compatible)
+      Result = Compatible;
+        
     if (Args[i] != argExpr) // The expression was converted.
       Args[i] = argExpr; // Make sure we store the converted expression.
 
