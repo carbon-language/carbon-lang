b4d7aa6fd66f4b60c8aed7ee3320e3006e1e472d
Blaine Garst <blaine@apple.com>
2010-04-21 04:34:46 +0000

    update copyright notices
    llvm-svn: 101986

--- compiler-rt/BlocksRuntime/data.c
+++ compiler-rt/BlocksRuntime/data.c
@@ -1,7 +1,7 @@
 /*
  * data.c
  *
- * Copyright 2008-2009 Apple, Inc. Permission is hereby granted, free of charge,
+ * Copyright 2008-2010 Apple, Inc. Permission is hereby granted, free of charge,
  * to any person obtaining a copy of this software and associated documentation
  * files (the "Software"), to deal in the Software without restriction,
  * including without limitation the rights to use, copy, modify, merge, publish,
