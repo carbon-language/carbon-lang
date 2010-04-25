1fc01985a3ad97ca48a3b130bb3da619e16d2634
Dale Johannesen <dalej@apple.com>
2010-04-25 21:03:54 +0000

    Add comment re byval args.  Doesn't actually work this way yet. xs
    llvm-svn: 102316

--- llvm/lib/CodeGen/SelectionDAG/FastISel.cpp
+++ llvm/lib/CodeGen/SelectionDAG/FastISel.cpp
@@ -340,6 +340,8 @@ bool FastISel::SelectCall(const User *I) {
       return true;
     const AllocaInst *AI = dyn_cast<AllocaInst>(Address);
     // Don't handle byval struct arguments or VLAs, for example.
+    // Note that if we have a byval struct argument, fast ISel is turned off;
+    // those are handled in SelectionDAGBuilder.
     if (!AI) break;
     DenseMap<const AllocaInst*, int>::iterator SI =
       StaticAllocaMap.find(AI);
@@ -347,7 +349,7 @@ bool FastISel::SelectCall(const User *I) {
     int FI = SI->second;
     if (!DI->getDebugLoc().isUnknown())
       MF.getMMI().setVariableDbgInfo(DI->getVariable(), FI, DI->getDebugLoc());
-    
+
     // Building the map above is target independent.  Generating DBG_VALUE
     // inline is target dependent; do this now.
     (void)TargetSelectInstruction(cast<Instruction>(I));
