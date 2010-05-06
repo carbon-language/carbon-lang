9bb682b7130d5211fb36c2a7cbfb3de027c5043e
mike-m <mikem.llvm@gmail.com>
2010-05-06 23:46:27 +0000

    2nd part of: Overhauled llvm/clang docs builds.
    llvm-svn: 103214

---
+++ clang/docs/api/intro.h
@@ -0,0 +1,23 @@
+/** @mainpage clang API Documentation
+<div class="intro">
+<div class="logo">
+  <img src="http://llvm.org/img/DragonMedium.png"
+    alg="medium dragon logo" width="400" height="400">
+</div>
+
+@section main_intro Introduction
+Welcome to the clang project.
+
+<br>
+This documentation describes the <b>internal</b> software that makes 
+up clang, not the <b>external</b> use of clang. There are no instructions
+here on how to use clang, only the APIs that make up the software. For 
+usage instructions, please see the programmer's guide or reference 
+manual.
+
+@section main_caveat Caveat 
+This documentation is generated directly from the source code with Doxygen. 
+Since clang is constantly under active development, what you're about to
+read is out of date!
+</div>
+*/
