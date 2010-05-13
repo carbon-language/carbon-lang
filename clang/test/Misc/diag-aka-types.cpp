5cb1abc6db5bd7699ea8f9de9cd85035e04a991a
Chandler Carruth <chandlerc@gmail.com>
2010-05-13 12:04:17 +0000

    Testcase for r103712.
    llvm-svn: 103713

---
+++ clang/test/Misc/diag-aka-types.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 %s -fsyntax-only -verify
+
+struct X {};
+typedef X foo_t;
+
+foo_t *ptr;
+char c1 = ptr; // expected-error{{'foo_t *' (aka 'X *')}}
+
+const foo_t &ref = foo_t();
+char c2 = ref; // expected-error{{'foo_t const' (aka 'X const')}}
