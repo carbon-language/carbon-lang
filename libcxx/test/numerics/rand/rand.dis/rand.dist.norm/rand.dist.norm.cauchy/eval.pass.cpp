6692b261d871ac0f2997067f3073213241f09fff
Howard Hinnant <hhinnant@apple.com>
2010-05-17 21:55:46 +0000

    [rand.dist.norm.cauchy].  I'm having trouble testing the output as all statistical properties are undefined.  They do not converge upon any one value as the number of samples increases.  Suggestions for tests welcome.
    llvm-svn: 103983

---
+++ libcxx/test/numerics/rand/rand.dis/rand.dist.norm/rand.dist.norm.cauchy/eval.pass.cpp
@@ -0,0 +1,30 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <random>
+
+// template<class RealType = double>
+// class cauchy_distribution
+
+// template<class _URNG> result_type operator()(_URNG& g);
+
+#include <random>
+
+int main()
+{
+    typedef std::cauchy_distribution<> D;
+    typedef D::param_type P;
+    typedef std::mt19937 G;
+    G g;
+    D d(0.5, 2);
+    D::result_type v = d(g);
+
+// If anyone can figure out a better test than this,
+// it would be more than welcome!
+}
