fb7df5317a7512180b9a2a6ee232be10b7f33f68
Dale Johannesen <dalej@apple.com>
2010-05-18 19:03:51 +0000

    Testcase for llvm-gcc checkin 104042.
    llvm-svn: 104043

---
+++ llvm/test/FrontendC/2010-05-18-asmsched.c
@@ -0,0 +1,19 @@
+// RUN: %llvmgcc %s -c -O3 -m64 -emit-llvm -o - | llc -march=x86-64 -mtriple=x86_64-apple-darwin | FileCheck %s
+// XFAIL: *
+// XTARGET: x86,i386,i686
+// r9 used to be clobbered before its value was moved to r10.  7993104.
+
+void foo(int x, int y) {
+// CHECK: bar
+// CHECK: movq  %r9, %r10
+// CHECK: movq  %rdi, %r9
+// CHECK: bar
+  register int lr9 asm("r9") = x;
+  register int lr10 asm("r10") = y;
+  int foo;
+  asm volatile("bar" : "=r"(lr9) : "r"(lr9), "r"(lr10));
+  foo = lr9;
+  lr9 = x;
+  lr10 = foo;
+  asm volatile("bar" : "=r"(lr9) : "r"(lr9), "r"(lr10));
+}
