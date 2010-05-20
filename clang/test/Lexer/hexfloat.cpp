fd8fb3c4cd64fb82bc7b0ae3ba6b8b40964e785a
Chris Lattner <sabre@nondot.org>
2010-05-20 23:33:51 +0000

    Don't warn about use of hex floats in c++ mode by default, matching G++'s behavior.
    Warn when -pedantic or -Wc++-hex-floats is passed, and
    don't warn if -pedantic -Wno-c++-hex-floats are both passed.
    
    llvm-svn: 104295

--- clang/test/Lexer/hexfloat.cpp
+++ clang/test/Lexer/hexfloat.cpp
@@ -1,5 +1,5 @@
-//RUN: %clang_cc1 -fsyntax-only -verify
-//RUN: %clang_cc1 -std=c++0x -fsyntax-only -verify
+// RUN: %clang_cc1 -fsyntax-only -verify -pedantic
+// RUN: %clang_cc1 -std=c++0x -fsyntax-only -verify
 
 #ifndef __GXX_EXPERIMENTAL_CXX0X__
 float f = 0x1p+1; // expected-warning {{incompatible with C++0x}}
