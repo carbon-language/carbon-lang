129b381a58330d784c3a63b1c27355d91ab0b6e6
Nick Lewycky <nicholas@mxc.ca>
2010-05-25 01:51:06 +0000

    Make this class useful as a base.
    llvm-svn: 104578

--- clang/include/clang/Frontend/FrontendActions.h
+++ clang/include/clang/Frontend/FrontendActions.h
@@ -75,11 +75,9 @@ protected:
 };
 
 class FixItAction : public ASTFrontendAction {
-private:
+protected:
   llvm::OwningPtr<FixItRewriter> Rewriter;
   llvm::OwningPtr<FixItPathRewriter> PathRewriter;
-
-protected:
 
   virtual ASTConsumer *CreateASTConsumer(CompilerInstance &CI,
                                          llvm::StringRef InFile);
