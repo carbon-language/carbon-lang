642da419a60710c57bd2a3eefb2ecdf055af6778
Anders Carlsson <andersca@mac.com>
2010-06-08 23:10:20 +0000

    Try to fix buildbot tests.
    llvm-svn: 105660

--- clang/test/CodeGenCXX/global-init-darwin.cpp
+++ clang/test/CodeGenCXX/global-init-darwin.cpp
@@ -16,8 +16,8 @@ struct B {
 
 int i = B().f();
 
-// CHECK: define internal void @__cxx_global_var_init() section "__TEXT,__StaticInit,regular,pure_instructions" {
-// CHECK: define internal void @__cxx_global_var_init1() section "__TEXT,__StaticInit,regular,pure_instructions" {
-// CHECK: define internal void @__cxx_global_array_dtor(i8*) section "__TEXT,__StaticInit,regular,pure_instructions" {
-// CHECK: define internal void @__cxx_global_var_init2() section "__TEXT,__StaticInit,regular,pure_instructions" {
-// CHECK: define internal void @_GLOBAL__I_a() section "__TEXT,__StaticInit,regular,pure_instructions" {
+// CHECK: "__TEXT,__StaticInit,regular,pure_instructions" {
+// CHECK: "__TEXT,__StaticInit,regular,pure_instructions" {
+// CHECK: "__TEXT,__StaticInit,regular,pure_instructions" {
+// CHECK: "__TEXT,__StaticInit,regular,pure_instructions" {
+// CHECK: "__TEXT,__StaticInit,regular,pure_instructions" {
