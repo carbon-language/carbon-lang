2569885963708a90c185a72e9690789819e926e8
Rafael Espindola <rafael.espindola@gmail.com>
2010-06-08 03:59:28 +0000

    Add a test to the previous commit.
    llvm-svn: 105596

--- clang/test/CodeGen/builtins-arm.c
+++ clang/test/CodeGen/builtins-arm.c
@@ -1,6 +1,12 @@
-// RUN: %clang_cc1 -triple thumbv7-eabi -target-cpu cortex-a8 -O3 -emit-llvm -o %t %s
+// RUN: %clang_cc1 -Wall -Werror -triple thumbv7-eabi -target-cpu cortex-a8 -O3 -emit-llvm -o - %s | FileCheck %s
 
 void *f0()
 {
   return __builtin_thread_pointer();
 }
+
+void f1(char *a, char *b) {
+	__clear_cache(a,b);
+}
+
+// CHECK: call void @__clear_cache
