b776ee6f17f1a3fab8a6f5e18ed31f4191cc18ee
Eli Friedman <eli.friedman@gmail.com>
2010-06-09 10:16:07 +0000

    A few more warning/error fixes.
    llvm-svn: 105723

--- lldb/source/Core/Language.cpp
+++ lldb/source/Core/Language.cpp
@@ -10,13 +10,14 @@
 #include "lldb/lldb-private.h"
 #include "lldb/Core/Language.h"
 #include "lldb/Core/Stream.h"
+#include <string.h>
 
 using namespace lldb;
 using namespace lldb_private;
 
 #define ENUM_TO_DCSTREAM(x) case x: s->PutCString(#x); return
 
-typedef struct LanguageStrings
+struct LanguageStrings
 {
     const char * names[3];
 };
@@ -24,27 +25,27 @@ typedef struct LanguageStrings
 static LanguageStrings
 g_languages[] =
 {
-    { "unknown" , NULL          , NULL                  },
-    { "c89"     , NULL          , "ISO C:1989"          },
-    { NULL      , NULL          , "K&R C"               },
-    { "ada83"   , "Ada83"       , "ISO Ada:1983"        },
-    { "c++"     , "cxx"         , "ISO C++:1998"        },
-    { "cobol74" , "Cobol74"     , "ISO Cobol:1974"      },
-    { "cobol"   , "Cobol85"     , "ISO Cobol:1985."     },
-    { "f77"     , "Fortran77"   , "ISO Fortran 77."     },
-    { "f90"     , "Fortran90"   , "ISO Fortran 90"      },
-    { "pascal"  , "Pascal83"    , "ISO Pascal:1983"     },
-    { "modula2" , "Modula2"     , "ISO Modula-2:1996"   },
-    { "java"    , NULL          , "Java"                },
-    { "c"       , "C99"         , "ISO C:1999"          },
-    { "ada"     , "Ada95"       , "ISO Ada:1995"        },
-    { "f95"     , "Fortran95"   , "ISO Fortran 95"      },
-    { "PLI"     , NULL          , "ANSI PL/I:1976"      },
-    { "objc"    , NULL          , "Objective-C"         },
-    { "objc++"  , NULL          , "Objective-C++"       },
-    { "upc"     , NULL          , "Unified Parallel C"  },
-    { "d"       , NULL          , "D"                   },
-    { "python"  , NULL          , "Python"              }
+    { { "unknown" , NULL          , NULL                  } },
+    { { "c89"     , NULL          , "ISO C:1989"          } },
+    { { NULL      , NULL          , "K&R C"               } },
+    { { "ada83"   , "Ada83"       , "ISO Ada:1983"        } },
+    { { "c++"     , "cxx"         , "ISO C++:1998"        } },
+    { { "cobol74" , "Cobol74"     , "ISO Cobol:1974"      } },
+    { { "cobol"   , "Cobol85"     , "ISO Cobol:1985."     } },
+    { { "f77"     , "Fortran77"   , "ISO Fortran 77."     } },
+    { { "f90"     , "Fortran90"   , "ISO Fortran 90"      } },
+    { { "pascal"  , "Pascal83"    , "ISO Pascal:1983"     } },
+    { { "modula2" , "Modula2"     , "ISO Modula-2:1996"   } },
+    { { "java"    , NULL          , "Java"                } },
+    { { "c"       , "C99"         , "ISO C:1999"          } },
+    { { "ada"     , "Ada95"       , "ISO Ada:1995"        } },
+    { { "f95"     , "Fortran95"   , "ISO Fortran 95"      } },
+    { { "PLI"     , NULL          , "ANSI PL/I:1976"      } },
+    { { "objc"    , NULL          , "Objective-C"         } },
+    { { "objc++"  , NULL          , "Objective-C++"       } },
+    { { "upc"     , NULL          , "Unified Parallel C"  } },
+    { { "d"       , NULL          , "D"                   } },
+    { { "python"  , NULL          , "Python"              } }
 };
 
 static const uint32_t
