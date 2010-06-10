9943850884edb39bf1d8f9ff1232640a6ed992ef
Duncan Sands <baldrick@free.fr>
2010-06-10 15:06:05 +0000

    Avoid many "variable 'Sub' set but not used [-Wunused-but-set-variable]" warnings with gcc-4.6.  The warning is wrong, since Sub *is* used (perhaps gcc is confused because the use of Sub is constant folded away?), but since it is trivial to avoid, and massively reduces the amount of warning spew, just workaround the wrong warning.
    llvm-svn: 105788

--- llvm/include/llvm/SymbolTableListTraits.h
+++ llvm/include/llvm/SymbolTableListTraits.h
@@ -47,9 +47,8 @@ public:
   /// of instructions, it returns the BasicBlock that owns them.
   ItemParentClass *getListOwner() {
     typedef iplist<ValueSubClass> ItemParentClass::*Sublist;
-    Sublist Sub(ItemParentClass::
-                getSublistAccess(static_cast<ValueSubClass*>(0)));
-    size_t Offset(size_t(&((ItemParentClass*)0->*Sub)));
+    size_t Offset(size_t(&((ItemParentClass*)0->*ItemParentClass::
+                           getSublistAccess(static_cast<ValueSubClass*>(0)))));
     iplist<ValueSubClass>* Anchor(static_cast<iplist<ValueSubClass>*>(this));
     return reinterpret_cast<ItemParentClass*>(reinterpret_cast<char*>(Anchor)-
                                               Offset);
