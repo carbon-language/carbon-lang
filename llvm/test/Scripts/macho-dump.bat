edcc628289e0e54a98535f6e65e5edf6fdcc7122
Daniel Dunbar <daniel@zuster.org>
2010-06-12 16:21:54 +0000

    tests: Make macho-dump.bat actually work.
    llvm-svn: 105891

--- llvm/test/Scripts/macho-dump.bat
+++ llvm/test/Scripts/macho-dump.bat
@@ -1,3 +1,7 @@
 @echo off
-macho-dump %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+@rem The -t here is just to avoid infinite looping if %PYTHON_EXECUTABLE% isn't set
+@rem for some reason.
+
+%PYTHON_EXECUTABLE% -t %LLVM_SRC_ROOT%\test\Scripts\macho-dump %1 %2 %3 %4 %5 %6 %7 %8 %9
 
