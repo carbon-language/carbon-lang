0684e33ba5e97ec5a7c372ee7596fb0ba4057c90
Alexis Hunt <alercah@gmail.com>
2010-06-15 02:36:48 +0000

    MinGW requires that wint_t be defined in stddef.h. In order to accomodate, we won't define it unless specifically requested via the use of __need_wint_t.
    llvm-svn: 105985

--- clang/lib/Headers/stddef.h
+++ clang/lib/Headers/stddef.h
@@ -45,6 +45,13 @@ typedef __typeof__(*L"") wchar_t;
 #define NULL ((void*)0)
 #endif
 
+// Some C libraries expect to see a wint_t here. Others (notably MinGW) will use
+// __WINT_TYPE__ directly; accomodate both by requiring __need_wint_t
+#if defined(__need_wint_t) && !defined(_WINT_T)
+#define _WINT_T
+typedef __WINT_TYPE__ wint_t;
+#endif
+
 #define offsetof(t, d) __builtin_offsetof(t, d)
 
 #endif /* __STDDEF_H */
