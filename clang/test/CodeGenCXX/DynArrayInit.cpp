d3a476b9766e9e9dd09b60bf80a87677e874e19c
Fariborz Jahanian <fjahanian@apple.com>
2010-06-25 22:32:31 +0000

    Try making BuildBot happy again.
    llvm-svn: 106898

--- clang/test/CodeGenCXX/DynArrayInit.cpp
+++ clang/test/CodeGenCXX/DynArrayInit.cpp
@@ -3,13 +3,13 @@
 
 int main() {
   // CHECK: {{for.cond:|:4}}
-  // CHECK: %isless = icmp ult i64 %0, 1133
+  // CHECK: %{{.*}} = icmp ult i64 %{{.*}}, 1133
   // CHECK: {{for.body:|:6}}
   // CHECK: store i8 0
   // CHECK: br label %{{for.inc|7}}
   // CHECK: {{for.inc:|:7}}
-  // CHECK: %inc = add i64 %2, 1
-  // CHECK: store i64 %inc
+  // CHECK: %{{.*}} = add i64 %{{.*}}, 1
+  // CHECK: store i64 %{{.*}}
   // CHECK: br label %{{for.cond|4}}
   // CHECK: {{for.end:|:12}}
   volatile char *buckets = new char[1133]();
