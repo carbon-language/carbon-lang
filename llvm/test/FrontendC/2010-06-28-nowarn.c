764b056c306121c82f92104501a45fd926b9c5fb
Dale Johannesen <dalej@apple.com>
2010-06-28 22:45:33 +0000

    Refix XTARGET.  Previous attempt matches on powerpc-apple-darwin, although I don't see why.
    llvm-svn: 107090

--- llvm/test/FrontendC/2010-06-28-nowarn.c
+++ llvm/test/FrontendC/2010-06-28-nowarn.c
@@ -1,7 +1,7 @@
 // RUN: %llvmgcc %s -c -m32 -fasm-blocks -o /dev/null
 // This should not warn about unreferenced label. 7729514.
 // XFAIL: *
-// XTARGET: i386*darwin,i686*darwin,x86*darwin
+// XTARGET: i386-apple-darwin,x86_64-apple-darwin,i686-apple-darwin
 
 void quarterAsm(int array[], int len)
 {
