0c30739cbb66ad3459fc3996cb21b188f1765814
Evan Cheng <evan.cheng@apple.com>
2010-06-29 05:37:59 +0000

    Change if-cvt options to something that actually as useable.
    llvm-svn: 107121

--- llvm/lib/Target/ARM/Thumb2InstrInfo.cpp
+++ llvm/lib/Target/ARM/Thumb2InstrInfo.cpp
@@ -29,12 +29,14 @@
 using namespace llvm;
 
 static cl::opt<unsigned>
-IfCvtLimit("thumb2-ifcvt-limit (default 3)",
-           cl::Hidden, cl::init(3));
+IfCvtLimit("thumb2-ifcvt-limit", cl::Hidden,
+           cl::desc("Thumb2 if-conversion limit (default 3)"),
+           cl::init(3));
 
 static cl::opt<unsigned>
-IfCvtDiamondLimit("thumb2-ifcvt-diamond-limit (default 3)",
-                  cl::Hidden, cl::init(3));
+IfCvtDiamondLimit("thumb2-ifcvt-diamond-limit", cl::Hidden,
+                  cl::desc("Thumb2 diamond if-conversion limit (default 3)"),
+                  cl::init(3));
 
 Thumb2InstrInfo::Thumb2InstrInfo(const ARMSubtarget &STI)
   : ARMBaseInstrInfo(STI), RI(*this, STI) {
