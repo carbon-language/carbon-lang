96278c19fb96f0e570193831510c138d260c2f91
Daniel Dunbar <daniel@zuster.org>
2010-07-01 20:20:05 +0000

    Remove stray comma.
    llvm-svn: 107425

--- llvm/include/llvm/Support/COFF.h
+++ llvm/include/llvm/Support/COFF.h
@@ -64,7 +64,7 @@ namespace COFF {
     SF_ClassMask = 0x00FF0000,
     SF_ClassShift = 16,
 
-    SF_WeakReference = 0x01000000,
+    SF_WeakReference = 0x01000000
   };
 
   enum symbol_storage_class {
