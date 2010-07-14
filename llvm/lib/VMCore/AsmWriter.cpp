1513217e27970419ae74ed1c4bf9676a7487a0f2
Dan Gohman <gohman@apple.com>
2010-07-14 21:12:44 +0000

    Just use getParent() instead of getModuleFromVal when the value is a Function.
    llvm-svn: 108358

--- llvm/lib/VMCore/AsmWriter.cpp
+++ llvm/lib/VMCore/AsmWriter.cpp
@@ -2129,7 +2129,7 @@ void Value::print(raw_ostream &ROS, AssemblyAnnotationWriter *AAW) const {
   } else if (const MDNode *N = dyn_cast<MDNode>(this)) {
     const Function *F = N->getFunction();
     SlotTracker SlotTable(F);
-    AssemblyWriter W(OS, SlotTable, F ? getModuleFromVal(F) : 0, AAW);
+    AssemblyWriter W(OS, SlotTable, F ? F->getParent() : 0, AAW);
     W.printMDNodeBody(N);
   } else if (const NamedMDNode *N = dyn_cast<NamedMDNode>(this)) {
     SlotTracker SlotTable(N->getParent());
