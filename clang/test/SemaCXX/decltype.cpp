d9d45838e028076180da0dbe63106371ef5e700e
Douglas Gregor <dgregor@apple.com>
2010-07-15 18:35:15 +0000

    Add test case for PR5290; this bug was fixed with the non-class rvalue de-cv-qualification fixes.
    llvm-svn: 108437

---
+++ clang/test/SemaCXX/decltype.cpp
@@ -0,0 +1,18 @@
+// RUN: %clang_cc1 -std=c++0x -fsyntax-only -verify %s
+
+// PR5290
+int const f0();
+void f0_test() {
+  decltype(0, f0()) i = 0; // expected-warning{{expression result unused}}
+  i = 0;
+}
+
+struct A { int a[1]; A() { } };
+typedef A const AC;
+int &f1(int*);
+float &f2(int const*);
+
+void test_f2() {
+  float &fr = f2(AC().a);
+}
+
