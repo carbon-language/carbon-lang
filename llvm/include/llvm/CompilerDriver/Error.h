204fbaf5c2105c78586e026086d8b51a2fe37efd
Mikhail Glushenkov <foldr@codedgers.com>
2010-07-23 04:19:39 +0000

    StringRef'icate.
    llvm-svn: 109217

--- llvm/include/llvm/CompilerDriver/Error.h
+++ llvm/include/llvm/CompilerDriver/Error.h
@@ -14,20 +14,16 @@
 #ifndef LLVM_INCLUDE_COMPILER_DRIVER_ERROR_H
 #define LLVM_INCLUDE_COMPILER_DRIVER_ERROR_H
 
+#include "llvm/ADT/StringRef.h"
 #include "llvm/Support/raw_ostream.h"
-
-#include <string>
 
 namespace llvmc {
 
-  inline void PrintError(const char* Err) {
+  inline void PrintError(llvm::StringRef Err) {
     extern const char* ProgramName;
     llvm::errs() << ProgramName << ": " << Err << '\n';
   }
 
-  inline void PrintError(const std::string& Err) {
-    PrintError(Err.c_str());
-  }
 }
 
 #endif // LLVM_INCLUDE_COMPILER_DRIVER_ERROR_H
