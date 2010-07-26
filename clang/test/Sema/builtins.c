e93c39045abc60f27cb7dbe30486a6ba52871af7
Benjamin Kramer <benny.kra@googlemail.com>
2010-07-26 22:04:15 +0000

    Mark __builtin_eh_return as noreturn. Patch by Dimitry Andric.
    llvm-svn: 109452

--- clang/test/Sema/builtins.c
+++ clang/test/Sema/builtins.c
@@ -80,3 +80,7 @@ void test12(void) {
 void test_unknown_builtin(int a, int b) {
   __builtin_foo(a, b); // expected-error{{use of unknown builtin}}
 }
+
+int test13() {
+  __builtin_eh_return(0, 0); // no warning, eh_return never returns.
+}
