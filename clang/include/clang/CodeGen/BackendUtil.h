4fac280618b557da36206a8082dced5ac1439eb0
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2010-07-27 22:01:17 +0000

    Merge PCHWriterDecl.cpp's isRequiredDecl and CodeGenModule::MayDeferGeneration into a new function, DeclIsRequiredFunctionOrFileScopedVar.
    This function is part of the public CodeGen interface since it's essentially a CodeGen predicate that is also
    needed by the PCH mechanism to determine whether a decl needs to be deserialized during PCH loading for codegen purposes.
    This fixes current (and avoids future) codegen-from-PCH bugs.
    
    llvm-svn: 109546

--- clang/include/clang/CodeGen/BackendUtil.h
+++ clang/include/clang/CodeGen/BackendUtil.h
@@ -19,6 +19,7 @@ namespace clang {
   class Diagnostic;
   class CodeGenOptions;
   class TargetOptions;
+  class Decl;
   
   enum BackendAction {
     Backend_EmitAssembly,  ///< Emit native assembly files
@@ -32,6 +33,14 @@ namespace clang {
   void EmitBackendOutput(Diagnostic &Diags, const CodeGenOptions &CGOpts,
                          const TargetOptions &TOpts, llvm::Module *M,
                          BackendAction Action, llvm::raw_ostream *OS);
+
+  /// \brief Determines if the decl can be CodeGen'ed or deserialized from PCH
+  /// lazily, only when used; this is only relevant for function or file scoped
+  /// var definitions.
+  ///
+  /// \returns true if the function/var must be CodeGen'ed/deserialized even if
+  /// it is not used.
+  bool DeclIsRequiredFunctionOrFileScopedVar(const Decl *D);
 }
 
 #endif
