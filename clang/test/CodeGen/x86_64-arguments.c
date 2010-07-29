44f9c3b3f1529e57f7c8a61daa6bc67e85740cec
Chris Lattner <sabre@nondot.org>
2010-07-29 17:14:05 +0000

    in release mode, irbuilder doesn't add names to instructions, this will hopefully fix the osuosl clang-i686-darwin10 builder.
    llvm-svn: 109760

--- clang/test/CodeGen/x86_64-arguments.c
+++ clang/test/CodeGen/x86_64-arguments.c
@@ -157,9 +157,9 @@ typedef float v4f32 __attribute__((__vector_size__(16)));
 v4f32 f25(v4f32 X) {
   // CHECK: define <4 x float> @f25(<4 x float> %X)
   // CHECK-NOT: alloca
-  // CHECK: %X.addr = alloca <4 x float>
+  // CHECK: alloca <4 x float>
   // CHECK-NOT: alloca
-  // CHECK: store <4 x float> %X, <4 x float>* %X.addr
+  // CHECK: store <4 x float> %X, <4 x float>*
   // CHECK-NOT: store
   // CHECK: ret <4 x float>
   return X+X;
