14eb7bd76914f57c3dd6e375b58b5d7d4e3d844f
Daniel Dunbar <daniel@zuster.org>
2010-07-30 16:40:29 +0000

    tests: Use env instead of assuming bash.
    llvm-svn: 109864

--- clang/test/Index/preamble.c
+++ clang/test/Index/preamble.c
@@ -3,6 +3,6 @@
 int wibble(int);
 
 // RUN: %clang -x c-header -o %t.pch %S/Inputs/prefix.h
-// RUN: CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 5 local -I %S/Inputs -include %t %s | FileCheck %s
+// RUN: env CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 5 local -I %S/Inputs -include %t %s | FileCheck %s
 // CHECK: preamble.c:3:5: FunctionDecl=wibble:3:5 Extent=[3:5 - 3:16]
 // CHECK: preamble.c:3:15: ParmDecl=:3:15 (Definition) Extent=[3:12 - 3:16]
