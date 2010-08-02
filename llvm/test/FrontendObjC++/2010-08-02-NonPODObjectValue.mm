0e6e8858ff9756f24b9bb7b6125c30eae0123ad8
Stuart Hastings <stuart@apple.com>
2010-08-02 22:09:53 +0000

    Testcase for r110043.  Radar 8246180.
    llvm-svn: 110070

---
+++ llvm/test/FrontendObjC++/2010-08-02-NonPODObjectValue.mm
@@ -0,0 +1,24 @@
+// RUN: not %llvmgcc %s -S -emit-llvm -o - |& FileCheck %s
+// This tests for a specific diagnostic in LLVM-GCC.
+// Clang compiles this correctly with no diagnostic,
+// ergo this test will fail with a Clang-based front-end.
+class TFENodeVector  {
+public:
+ TFENodeVector(const TFENodeVector& inNodeVector);
+ TFENodeVector();
+};
+
+@interface TWindowHistoryEntry  {}
+@property (assign, nonatomic) TFENodeVector targetPath;
+@end
+
+@implementation TWindowHistoryEntry
+@synthesize targetPath;
+- (void) initWithWindowController {
+   TWindowHistoryEntry* entry;
+   TFENodeVector newPath;
+   // CHECK: setting a C++ non-POD object value is not implemented
+   entry.targetPath = newPath;
+   [entry setTargetPath:newPath];
+}
+@end
