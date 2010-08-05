fc21c3aa324a07870f7c6bfcb7db647e0dd02805
Oscar Fuentes <ofv@wanadoo.es>
2010-08-05 02:22:51 +0000

    When building from git, add ref-id to PACKAGE_VERSION
    llvm-svn: 110301

--- llvm/cmake/modules/VersionFromVCS.cmake
+++ llvm/cmake/modules/VersionFromVCS.cmake
@@ -15,6 +15,19 @@ function(add_version_info_from_vcs VERS)
     endif()
   elseif( EXISTS ${CMAKE_CURRENT_SOURCE_DIR}/.git )
     set(result "${result}git")
+    # Try to get a ref-id
+    find_program(git_executable NAMES git git.exe git.cmd)
+    if( git_executable )
+      execute_process(COMMAND ${git_executable} show-ref HEAD
+	WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+	TIMEOUT 5
+	RESULT_VARIABLE git_result
+	OUTPUT_VARIABLE git_output)
+      if( git_result EQUAL 0 )
+	string(SUBSTRING ${git_output} 0 7 git_ref_id)
+	set(result "${result}-${git_ref_id}")
+      endif()
+    endif()
   endif()
   set(${VERS} ${result} PARENT_SCOPE)
 endfunction(add_version_info_from_vcs)
