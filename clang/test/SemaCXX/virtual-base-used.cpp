9129b00c5f9b7ea4ec5eb5a0046c31d31f388d9e
Eli Friedman <eli.friedman@gmail.com>
2010-08-08 05:07:06 +0000

    Make this test check a few more cases which didn't work correctly before r110526.
    llvm-svn: 110540

--- clang/test/SemaCXX/virtual-base-used.cpp
+++ clang/test/SemaCXX/virtual-base-used.cpp
@@ -1,10 +1,11 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 // PR7800
 
-class NoDestroy { ~NoDestroy(); }; // expected-note {{declared private here}}
+class NoDestroy { ~NoDestroy(); }; // expected-note 3 {{declared private here}}
 struct A {
   virtual ~A();
 };
+
 struct B : public virtual A {
   NoDestroy x; // expected-error {{field of type 'NoDestroy' has private destructor}}
 };
@@ -13,4 +14,29 @@ struct D : public virtual B {
   ~D();
 };
 void D::foo() { // expected-note {{implicit default destructor for 'B' first required here}}
+}
+
+struct E : public virtual A {
+  NoDestroy x; // expected-error {{field of type 'NoDestroy' has private destructor}}
+};
+struct F : public E { // expected-note {{implicit default destructor for 'E' first required here}}
+};
+struct G : public virtual F {
+  virtual void foo();
+  ~G();
+};
+void G::foo() { // expected-note {{implicit default destructor for 'F' first required here}}
+}
+
+struct H : public virtual A {
+  NoDestroy x; // expected-error {{field of type 'NoDestroy' has private destructor}}
+};
+struct I : public virtual H {
+  ~I();
+};
+struct J : public I {
+  virtual void foo();
+  ~J();
+};
+void J::foo() { // expected-note {{implicit default destructor for 'H' first required here}}
 }
