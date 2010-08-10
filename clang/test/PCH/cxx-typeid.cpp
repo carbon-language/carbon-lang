656f3bd3e2ff430703970708f19000a79cfcda4a
Daniel Dunbar <daniel@zuster.org>
2010-08-10 19:32:44 +0000

    tests: Tests which include system headers must use the driver, not -cc1.
    llvm-svn: 110708

--- clang/test/PCH/cxx-typeid.cpp
+++ clang/test/PCH/cxx-typeid.cpp
@@ -1,8 +1,8 @@
 // Test this without pch.
-// RUN: %clang_cc1 -include %S/cxx-typeid.h -fsyntax-only -verify %s
+// RUN: %clang -include %S/cxx-typeid.h -fsyntax-only -verify %s
 
-// RUN: %clang_cc1 -x c++-header -emit-pch -o %t %S/cxx-typeid.h
-// RUN: %clang_cc1 -include-pch %t -fsyntax-only -verify %s 
+// RUN: %clang -ccc-pch-is-pch -x c++-header -o %t.gch %S/cxx-typeid.h
+// RUN: %clang -ccc-pch-is-pch -include %t -fsyntax-only -Xclang -verify %s 
 
 void f() {
     (void)typeid(int);
