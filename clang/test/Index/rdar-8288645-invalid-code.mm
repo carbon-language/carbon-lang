19183acd1f180f644cecc49e4a9f6554834693b7
Ted Kremenek <kremenek@apple.com>
2010-08-10 22:30:29 +0000

    Add test case for <rdar://problem/8288645>.  While this is fixed in trunk, we previously were getting the following assertion failure not too long ago:
    Assertion failed: (getContainingDC(DC) == CurContext && "The next DeclContext should be lexically contained in the current one.")
    
    llvm-svn: 110740

---
+++ clang/test/Index/rdar-8288645-invalid-code.mm
@@ -0,0 +1,8 @@
+// RUN: c-index-test -test-load-source all %s 2>&1 | FileCheck %s
+
+// This test case previously crashed Sema.
+
+extern "C" { @implementation Foo  - (id)initWithBar:(Baz<WozBar>)pepper {
+
+// CHECK: warning: cannot find interface declaration for 'Foo'
+// CHECK: warning: '@end' is missing in implementation context
