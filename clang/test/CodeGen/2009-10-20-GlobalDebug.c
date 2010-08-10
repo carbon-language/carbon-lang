2210aa2eca031b5fa09d68ff1006df3c4adaa47b
Devang Patel <dpatel@apple.com>
2010-08-10 01:36:24 +0000

    There is no need to pubish file static variable's name. Do not rely on this code gen bug to check whether debug info is generated for such variables or not.
    llvm-svn: 110640

--- clang/test/CodeGen/2009-10-20-GlobalDebug.c
+++ clang/test/CodeGen/2009-10-20-GlobalDebug.c
@@ -1,7 +1,7 @@
 // RUN: %clang -ccc-host-triple i386-apple-darwin10 -S -g -dA %s -o - | FileCheck %s
 int global;
+// CHECK: ascii   "localstatic"          ## DW_AT_name
 // CHECK: asciz   "global" ## External Name
-// CHECK: asciz   "localstatic"          ## External Name
 int main() { 
   static int localstatic;
   return 0;
