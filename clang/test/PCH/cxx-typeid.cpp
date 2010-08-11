00012c869f519848cb2463c036c27d5a60e8275c
Daniel Dunbar <daniel@zuster.org>
2010-08-11 02:32:03 +0000

    tests: Add a missing -Xclang.
    llvm-svn: 110776

--- clang/test/PCH/cxx-typeid.cpp
+++ clang/test/PCH/cxx-typeid.cpp
@@ -1,5 +1,5 @@
 // Test this without pch.
-// RUN: %clang -include %S/cxx-typeid.h -fsyntax-only -verify %s
+// RUN: %clang -include %S/cxx-typeid.h -fsyntax-only -Xclang -verify %s
 
 // RUN: %clang -ccc-pch-is-pch -x c++-header -o %t.gch %S/cxx-typeid.h
 // RUN: %clang -ccc-pch-is-pch -include %t -fsyntax-only -Xclang -verify %s 
