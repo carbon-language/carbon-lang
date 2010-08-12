16e465bc5fbe594ddd4ee8fbfe03ad9da753d516
Devang Patel <dpatel@apple.com>
2010-08-12 17:42:42 +0000

    Make this test darwin only.
    llvm-svn: 110936

--- clang/test/CodeGenCXX/debug-info-ctor.cpp
+++ clang/test/CodeGenCXX/debug-info-ctor.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang -emit-llvm -g -S %s -o - | FileCheck %s
+// RUN: %clang -march=x86_64-apple-darwin10 -emit-llvm -g -S %s -o - | FileCheck %s
 
 struct X {
   X(int v);
@@ -7,8 +7,7 @@ struct X {
 };
 
 X::X(int v) {
-  // CHECK_TEMPORARILY_DISABLED: call void @_ZN1XC2Ei(%struct.X* %this1, i32 %tmp), !dbg
-  // TEMPORARY CHECK: X
+  // CHECK: call void @_ZN1XC2Ei(%struct.X* %this1, i32 %tmp), !dbg
   value = v;
 }
 
