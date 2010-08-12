a3025fcd457fcff0cacb75f444d7e0e92e5bb1cd
Devang Patel <dpatel@apple.com>
2010-08-12 00:00:41 +0000

    update test to reflect r110876 change.
    llvm-svn: 110884

--- clang/test/CodeGen/2010-08-10-DbgConstant.c
+++ clang/test/CodeGen/2010-08-10-DbgConstant.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -S -emit-llvm -g  %s -o - | grep DW_TAG_constant
+// RUN: %clang_cc1 -S -emit-llvm -g  %s -o - | grep DW_TAG_variable
 
 static const unsigned int ro = 201;
 void bar(int);
