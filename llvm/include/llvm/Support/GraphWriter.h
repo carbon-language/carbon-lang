b6476afeb834e9332449bbfd73f93054369bde27
Mikhail Glushenkov <foldr@codedgers.com>
2010-08-20 11:24:31 +0000

    Trailing whitespace.
    llvm-svn: 111617

--- llvm/include/llvm/Support/GraphWriter.h
+++ llvm/include/llvm/Support/GraphWriter.h
@@ -89,7 +89,7 @@ class GraphWriter {
 
 public:
   GraphWriter(raw_ostream &o, const GraphType &g, bool SN) : O(o), G(g) {
-  DTraits = DOTTraits(SN); 
+  DTraits = DOTTraits(SN);
 }
 
   void writeHeader(const std::string &Name) {
