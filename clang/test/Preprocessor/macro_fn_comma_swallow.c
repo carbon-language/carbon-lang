fee309467bea9dc53c44ddc5847060563d98f8fc
Chris Lattner <sabre@nondot.org>
2010-08-21 00:29:50 +0000

    filecheckize test
    llvm-svn: 111702

--- clang/test/Preprocessor/macro_fn_comma_swallow.c
+++ clang/test/Preprocessor/macro_fn_comma_swallow.c
@@ -1,26 +1,28 @@
 // Test the GNU comma swallowing extension.
-// RUN: %clang_cc1 %s -E | grep 'foo{A, }'
-// RUN: %clang_cc1 %s -E | grep 'fo2{A,}'
-// RUN: %clang_cc1 %s -E | grep '{foo}'
+// RUN: %clang_cc1 %s -E | FileCheck -strict-whitespace %s
 
+// CHECK: 1: foo{A, }
 #define X(Y) foo{A, Y}
-X()
+1: X()
 
+
+// CHECK: 2: fo2{A,}
 #define X2(Y) fo2{A,##Y}
-X2()
+2: X2()
 
 // should eat the comma.
+// CHECK: 3: {foo}
 #define X3(b, ...) {b, ## __VA_ARGS__}
-X3(foo)
+3: X3(foo)
 
 
 
-// RUN: %clang_cc1 %s -E | grep 'AA BB'
 // PR3880
+// CHECK: 4: AA BB
 #define X4(...)  AA , ## __VA_ARGS__ BB
-X4()
+4: X4()
 
-// RUN: %clang_cc1 %s -E | grep '5: 1'
 // PR7943
+// CHECK: 5: 1
 #define X5(x,...) x##,##__VA_ARGS__
 5: X5(1)
