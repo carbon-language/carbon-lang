846022495866f4234a7cc4e99329637888128f32
Chris Lattner <sabre@nondot.org>
2010-08-23 23:50:42 +0000

    fix PR7953 - Windows filename are case insensitive:
    #pragma once wasn't working on win32 if the header file was included
    using a different case.
    I tracked down  the problem to the fact that clang::FileManager was
    caching files using case sensitive string (UniqueFiles) on Windows.
    
    I changed FileManager to cache filename in lower case only.
    Doesn't affect UNIX because UNIX uses Inode to uniquely identify files.
    
    unix doesn't use this codepath.
    
    Analysis and patch by Francois Pichet!
    
    llvm-svn: 111866

--- clang/lib/Basic/FileManager.cpp
+++ clang/lib/Basic/FileManager.cpp
@@ -19,6 +19,7 @@
 
 #include "clang/Basic/FileManager.h"
 #include "llvm/ADT/SmallString.h"
+#include "llvm/ADT/StringExtras.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/System/Path.h"
 #include "llvm/Config/config.h"
@@ -83,6 +84,9 @@ class FileManager::UniqueFileContainer {
 public:
   FileEntry &getFile(const char *Name, struct stat &StatBuf) {
     std::string FullPath(GetFullPath(Name));
+    
+    // LowercaseString because Windows filesystem is case insensitive.
+    FullPath = llvm::LowercaseString(FullPath);
     return UniqueFiles.GetOrCreateValue(
                                FullPath.c_str(),
                                FullPath.c_str() + FullPath.size()
