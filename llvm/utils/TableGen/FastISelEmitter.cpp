98f0ea6adea5a15957037ebfd9dcfb5dd0b34661
Eric Christopher <echristo@apple.com>
2010-08-24 23:21:59 +0000

    Fix comment.
    llvm-svn: 111996

--- llvm/utils/TableGen/FastISelEmitter.cpp
+++ llvm/utils/TableGen/FastISelEmitter.cpp
@@ -108,10 +108,10 @@ struct OperandsSignature {
       else
         return false;
         
-      // For now, require the register operands' register classes to all
-      // be the same.
+      // For now, this needs to be a register class of some sort.
       if (!RC)
         return false;
+
       // For now, all the operands must have the same register class.
       if (DstRC) {
         if (DstRC != RC)
