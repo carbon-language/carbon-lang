b0958c7699d73ded943bce7b58f334f131ff4dfb
Devang Patel <dpatel@apple.com>
2010-08-26 22:54:33 +0000

    test case for r112238.
    llvm-svn: 112239

---
+++ clang/test/CodeGenCXX/debug-info-byval.cpp
@@ -0,0 +1,31 @@
+// RUN: %clang -g -S %s -o - | FileCheck %s
+// Test to check presense of debug info for byval parameter.
+// Radar 8350436.
+class DAG {
+public:
+  int i;
+  int j;
+};
+
+class EVT {
+public:
+  int a;
+  int b;
+  int c;
+};
+
+class VAL {
+public:
+  int x;
+  int y;
+};
+void foo(EVT e);
+EVT bar();
+
+void get(int *i, unsigned dl, VAL v, VAL *p, unsigned n, EVT missing_arg) {
+//CHECK: .ascii "missing_arg"
+  EVT e = bar();
+  if (dl == n)
+    foo(missing_arg);
+}
+
