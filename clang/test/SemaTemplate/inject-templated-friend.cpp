468aa3b20c4284ddd2bc45a82457352943e57609
Gabor Greif <ggreif@gmail.com>
2010-08-28 02:00:22 +0000

    check whether sema issues a redefinition error
    llvm-svn: 112347

--- clang/test/SemaTemplate/inject-templated-friend.cpp
+++ clang/test/SemaTemplate/inject-templated-friend.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang %s -S -emit-llvm -o - | grep -e "define linkonce_odr.*_ZlsR11std_ostreamRK8StreamerI3FooE"
+// RUN: %clang -cc1 %s -DREDEFINE -verify
 // PR8007: friend function not instantiated.
 
 struct std_ostream
@@ -11,7 +12,7 @@ std_ostream cout;
 template <typename STRUCT_TYPE>
 struct Streamer
 {
-    friend std_ostream& operator << (std_ostream& o, const Streamer& f)
+    friend std_ostream& operator << (std_ostream& o, const Streamer& f) // expected-error{{redefinition of 'operator<<'}}
         {
             Streamer s(f);
             s(o);
@@ -26,14 +27,17 @@ struct Streamer
 
 typedef struct Foo {} Foo;
 
-std_ostream& operator << (std_ostream& o, const Streamer<Foo>& f);
-/*std_ostream& operator << (std_ostream& o, const Streamer<Foo>& f)
+std_ostream& operator << (std_ostream&, const Streamer<Foo>&);
+#ifdef REDEFINE
+std_ostream& operator << (std_ostream& o, const Streamer<Foo>&) // expected-note{{is here}}
 {
   // Sema should flag this as a redefinition
-}*/
+  return o;
+}
+#endif
 
 template <>
-void Streamer<Foo>::operator () (std_ostream& o) const
+void Streamer<Foo>::operator () (std_ostream& o) const // expected-note{{requested here}}
 {
 }
 
