d7478d60100a81de1a1f7ba64e15d9def8b0cd60
Ted Kremenek <kremenek@apple.com>
2010-08-28 00:19:12 +0000

    Update test case, with comment to later investigate the correct behavior.  Now the behavior is at least consistent.
    llvm-svn: 112335

--- clang/test/SemaCXX/return-noreturn.cpp
+++ clang/test/SemaCXX/return-noreturn.cpp
@@ -7,9 +7,12 @@ namespace PR6884 {
     ~abort_struct() __attribute__((noreturn));
   };
 
+  // FIXME: Should either of these actually warn, since the destructor is
+  //  marked noreturn?
+
   int f() {
     abort_struct();
-  }
+  } // expected-warning{{control reaches end of non-void function}}
 
   int f2() {
     abort_struct s;
