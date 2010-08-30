cfcb94226765a37e6061cfb7144f4647c8829310
Douglas Gregor <dgregor@apple.com>
2010-08-30 14:37:53 +0000

    Add test case from PR6952, which now works (thanks to Gabor).
    llvm-svn: 112477

--- clang/test/CXX/temp/temp.decls/temp.friend/p4.cpp
+++ clang/test/CXX/temp/temp.decls/temp.friend/p4.cpp
@@ -8,3 +8,21 @@ struct X1 {
 
 X1<int> x1a; 
 X1<float> x1b; // expected-note {{in instantiation of}}
+
+template<typename T>
+struct X2 {
+  operator int();
+
+  friend void f(int x) { } // expected-error{{redefinition}} \
+                           // expected-note{{previous definition}}
+};
+
+int array0[sizeof(X2<int>)]; 
+int array1[sizeof(X2<float>)]; // expected-note{{instantiation of}}
+
+void g() {
+  X2<int> xi;
+  f(xi);
+  X2<float> xf; 
+  f(xf);
+}
