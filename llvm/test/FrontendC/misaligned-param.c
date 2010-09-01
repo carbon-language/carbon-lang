78d95e00891173f282040f0f8fe563703aa9ba7e
Dale Johannesen <dalej@apple.com>
2010-09-01 21:57:20 +0000

    Apparently only Darwin passes long double misaligned.  Compensate.
    llvm-svn: 112748

--- llvm/test/FrontendC/misaligned-param.c
+++ llvm/test/FrontendC/misaligned-param.c
@@ -1,7 +1,7 @@
 // RUN: %llvmgcc %s -m32 -S -o - | FileCheck %s
 // Misaligned parameter must be memcpy'd to correctly aligned temporary.
 // XFAIL: *
-// XTARGET: i386,i686,x86_64
+// XTARGET: i386-apple-darwin,i686-apple-darwin,x86_64-apple-darwin
 
 struct s { int x; long double y; };
 long double foo(struct s x, int i, struct s y) {
