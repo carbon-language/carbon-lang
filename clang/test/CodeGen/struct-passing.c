7f1982731e5cda60ceb910b6cddc0825e1f7caa6
Duncan Sands <baldrick@free.fr>
2010-09-02 08:52:56 +0000

    Correct this test for the fact that the number of uses is now printed in a comment.
    llvm-svn: 112813

--- clang/test/CodeGen/struct-passing.c
+++ clang/test/CodeGen/struct-passing.c
@@ -1,10 +1,10 @@
 // RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -o %t %s
-// RUN: grep 'declare i32 @f0() readnone$' %t
-// RUN: grep 'declare i32 @f1() readonly$' %t
-// RUN: grep 'declare void @f2(.* sret)$' %t
-// RUN: grep 'declare void @f3(.* sret)$' %t
-// RUN: grep 'declare void @f4(.* byval)$' %t
-// RUN: grep 'declare void @f5(.* byval)$' %t
+// RUN: grep 'declare i32 @f0() readnone ;' %t
+// RUN: grep 'declare i32 @f1() readonly ;' %t
+// RUN: grep 'declare void @f2(.* sret) ;' %t
+// RUN: grep 'declare void @f3(.* sret) ;' %t
+// RUN: grep 'declare void @f4(.* byval) ;' %t
+// RUN: grep 'declare void @f5(.* byval) ;' %t
 // PR3835
 
 typedef int T0;
