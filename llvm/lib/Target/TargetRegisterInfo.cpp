5d43a35e6d8592aa27822010d6cd44e00960d24d
Jim Grosbach <grosbach@apple.com>
2010-09-02 16:31:21 +0000

    Mask out reserved registers when constructing the set of allocatable regs.
    llvm-svn: 112828

--- llvm/lib/Target/TargetRegisterInfo.cpp
+++ llvm/lib/Target/TargetRegisterInfo.cpp
@@ -80,6 +80,11 @@ BitVector TargetRegisterInfo::getAllocatableSet(const MachineFunction &MF,
   for (TargetRegisterInfo::regclass_iterator I = regclass_begin(),
          E = regclass_end(); I != E; ++I)
     getAllocatableSetForRC(MF, *I, Allocatable);
+
+  // Mask out the reserved registers
+  BitVector Reserved = getReservedRegs(MF);
+  Allocatable ^= Reserved & Allocatable;
+
   return Allocatable;
 }
 
