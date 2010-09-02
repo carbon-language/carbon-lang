60409951285c2579d8e725542c327d29870f67e0
Jim Grosbach <grosbach@apple.com>
2010-09-02 18:44:51 +0000

    remove trailing whitespace
    llvm-svn: 112847

--- llvm/lib/Target/TargetRegisterInfo.cpp
+++ llvm/lib/Target/TargetRegisterInfo.cpp
@@ -63,7 +63,7 @@ TargetRegisterInfo::getMinimalPhysRegClass(unsigned reg, EVT VT) const {
 /// getAllocatableSetForRC - Toggle the bits that represent allocatable
 /// registers for the specific register class.
 static void getAllocatableSetForRC(const MachineFunction &MF,
-                                   const TargetRegisterClass *RC, BitVector &R){  
+                                   const TargetRegisterClass *RC, BitVector &R){
   for (TargetRegisterClass::iterator I = RC->allocation_order_begin(MF),
          E = RC->allocation_order_end(MF); I != E; ++I)
     R.set(*I);
