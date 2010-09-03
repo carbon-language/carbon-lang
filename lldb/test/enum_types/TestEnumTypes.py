84c96b84bdf5b9bd0eb023b13d091a9b333d0ea8
Johnny Chen <johnny.chen@apple.com>
2010-09-03 23:10:24 +0000

    Converted TestEnumTypes.py to Dsym/Dwarf combination. Marked test_with_dwarf() as expectedFailure where 'image lookup -t days' returns nothing.
    llvm-svn: 113028

--- lldb/test/enum_types/TestEnumTypes.py
+++ lldb/test/enum_types/TestEnumTypes.py
@@ -9,7 +9,21 @@ class EnumTypesTestCase(TestBase):
 
     mydir = "enum_types"
 
-    def test_image_lookup_for_enum_type(self):
+    @unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
+    def test_with_dsym(self):
+        """Test 'image lookup -t days' and check for correct display."""
+        self.buildDsym()
+        self.image_lookup_for_enum_type()
+
+    # rdar://problem/8394746
+    # 'image lookup -t days' returns nothing with dwarf debug format.
+    @unittest2.expectedFailure
+    def test_with_dwarf(self):
+        """Test 'image lookup -t days' and check for correct display."""
+        self.buildDwarf()
+        self.image_lookup_for_enum_type()
+
+    def image_lookup_for_enum_type(self):
         """Test 'image lookup -t days' and check for correct display."""
         exe = os.path.join(os.getcwd(), "a.out")
         self.runCmd("file " + exe, CURRENT_EXECUTABLE_SET)
