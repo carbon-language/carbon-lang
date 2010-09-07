a3188a81c0ca91d369db0022d43e304e15f4fe40
Stuart Hastings <stuart@apple.com>
2010-09-07 18:43:57 +0000

    Test case for r113248.  Raar 8361341.
    llvm-svn: 113249

--- llvm/test/FrontendC/2007-10-01-BuildArrayRef.c
+++ llvm/test/FrontendC/2007-10-01-BuildArrayRef.c
@@ -1,8 +1,20 @@
-// RUN: not %llvmgcc -S %s -o /dev/null |& grep "error: assignment of read-only location"
+// RUN: not %llvmgcc_only -c %s -o /dev/null |& FileCheck %s
 // PR 1603
-int func()
+void func()
 {
    const int *arr;
-   arr[0] = 1;
+   arr[0] = 1;  // CXHECK: error: assignment of read-only location
 }
 
+struct foo {
+  int bar;
+};
+struct foo sfoo = { 0 };
+
+int func2()
+{
+  const struct foo *fp;
+  fp = &sfoo;
+  fp[0].bar = 1;  // CHECK: error: assignment of read-only member 'bar'
+  return sfoo.bar;
+}
