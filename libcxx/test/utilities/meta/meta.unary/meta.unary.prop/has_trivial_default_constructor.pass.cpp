c3ed8c312e8a7111316224fee4ee44bc199c37ec
Howard Hinnant <hhinnant@apple.com>
2010-09-07 15:53:26 +0000

    has_trivial_default_constructor hooked up to clang.  Filed http://llvm.org/bugs/show_bug.cgi?id=8097 to take care of void and arrays of incomplete types which don't work yet.  If there is some reasons we don't want to handle these types in the compiler, I can handle them in the library.
    llvm-svn: 113205

--- libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_trivial_default_constructor.pass.cpp
+++ libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_trivial_default_constructor.pass.cpp
@@ -62,16 +62,16 @@ int main()
     test_has_not_trivial_default_constructor<void>();
     test_has_not_trivial_default_constructor<int&>();
     test_has_not_trivial_default_constructor<A>();
+    test_has_not_trivial_default_constructor<Abstract>();
+    test_has_not_trivial_default_constructor<NotEmpty>();
+    test_has_not_trivial_default_constructor<char[]>();
 
     test_has_trivial_default_constructor<Union>();
-    test_has_trivial_default_constructor<Abstract>();
     test_has_trivial_default_constructor<Empty>();
     test_has_trivial_default_constructor<int>();
     test_has_trivial_default_constructor<double>();
     test_has_trivial_default_constructor<int*>();
     test_has_trivial_default_constructor<const int*>();
     test_has_trivial_default_constructor<char[3]>();
-    test_has_trivial_default_constructor<char[3]>();
-    test_has_trivial_default_constructor<NotEmpty>();
     test_has_trivial_default_constructor<bit_zero>();
 }
