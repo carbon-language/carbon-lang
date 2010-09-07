331b3dd2ad1bb8ec9d2c6d6d676db988e7a95cdc
Howard Hinnant <hhinnant@apple.com>
2010-09-07 23:38:59 +0000

    has_trivial_copy_assign hooked up to clang (without workarounds).  Filed http://llvm.org/bugs/show_bug.cgi?id=8109 to take care of several types which don't work yet.  If there is some reason we don't want to handle these types in the compiler, I can handle most of them in the library.
    llvm-svn: 113312

--- libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_trivial_copy_assign.pass.cpp
+++ libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_trivial_copy_assign.pass.cpp
@@ -13,22 +13,10 @@
 
 #include <type_traits>
 
-template <class T>
+template <class T, bool Result>
 void test_has_trivial_assign()
 {
-    static_assert( std::has_trivial_copy_assign<T>::value, "");
-    static_assert(!std::has_trivial_copy_assign<const T>::value, "");
-    static_assert( std::has_trivial_copy_assign<volatile T>::value, "");
-    static_assert(!std::has_trivial_copy_assign<const volatile T>::value, "");
-}
-
-template <class T>
-void test_has_not_trivial_assign()
-{
-    static_assert(!std::has_trivial_copy_assign<T>::value, "");
-    static_assert(!std::has_trivial_copy_assign<const T>::value, "");
-    static_assert(!std::has_trivial_copy_assign<volatile T>::value, "");
-    static_assert(!std::has_trivial_copy_assign<const volatile T>::value, "");
+    static_assert(std::has_trivial_copy_assign<T>::value == Result, "");
 }
 
 class Empty
@@ -59,19 +47,20 @@ struct A
 
 int main()
 {
-    test_has_not_trivial_assign<void>();
-    test_has_not_trivial_assign<A>();
-    test_has_not_trivial_assign<int&>();
-    test_has_not_trivial_assign<NotEmpty>();
-    test_has_not_trivial_assign<Abstract>();
+    test_has_trivial_assign<void, false>();
+    test_has_trivial_assign<A, false>();
+    test_has_trivial_assign<int&, false>();
+    test_has_trivial_assign<NotEmpty, false>();
+    test_has_trivial_assign<Abstract, false>();
+    test_has_trivial_assign<const Empty, false>();
+    test_has_trivial_assign<char[3], false>();
+    test_has_trivial_assign<char[], false>();
 
-    test_has_trivial_assign<Union>();
-    test_has_trivial_assign<Empty>();
-    test_has_trivial_assign<int>();
-    test_has_trivial_assign<double>();
-    test_has_trivial_assign<int*>();
-    test_has_trivial_assign<const int*>();
-    test_has_trivial_assign<char[3]>();
-    test_has_trivial_assign<char[3]>();
-    test_has_trivial_assign<bit_zero>();
+    test_has_trivial_assign<Union, true>();
+    test_has_trivial_assign<Empty, true>();
+    test_has_trivial_assign<int, true>();
+    test_has_trivial_assign<double, true>();
+    test_has_trivial_assign<int*, true>();
+    test_has_trivial_assign<const int*, true>();
+    test_has_trivial_assign<bit_zero, true>();
 }
