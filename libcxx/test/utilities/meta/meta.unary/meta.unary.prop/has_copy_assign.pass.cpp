10b9b7b4bdeaa5e714542072092a61e6999af830
Howard Hinnant <hhinnant@apple.com>
2010-09-08 17:55:32 +0000

    Hooked the following up to clang:  is_class, is_enum, has_nothrow_copy_assign, has_trivial_destructor, has_virtual_destructor, is_pod.  Implemented has_copy_assign.
    llvm-svn: 113373

--- libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_copy_assign.pass.cpp
+++ libcxx/test/utilities/meta/meta.unary/meta.unary.prop/has_copy_assign.pass.cpp
@@ -13,7 +13,38 @@
 
 #include <type_traits>
 
+class Empty
+{
+};
+
+class NotEmpty
+{
+public:
+    virtual ~NotEmpty();
+};
+
+union Union {};
+
+struct bit_zero
+{
+    int :  0;
+};
+
+struct A
+{
+    A();
+};
+
 int main()
 {
-    static_assert((std::has_copy_assign<int>::value), "");
+    static_assert(( std::has_copy_assign<int>::value), "");
+    static_assert((!std::has_copy_assign<const int>::value), "");
+    static_assert((!std::has_copy_assign<int[]>::value), "");
+    static_assert((!std::has_copy_assign<int[3]>::value), "");
+    static_assert((!std::has_copy_assign<int&>::value), "");
+    static_assert(( std::has_copy_assign<A>::value), "");
+    static_assert(( std::has_copy_assign<bit_zero>::value), "");
+    static_assert(( std::has_copy_assign<Union>::value), "");
+    static_assert(( std::has_copy_assign<NotEmpty>::value), "");
+    static_assert(( std::has_copy_assign<Empty>::value), "");
 }
