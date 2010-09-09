57df3b4b12228bc103f70cbe9a5ccfb9f78f9a66
Douglas Gregor <dgregor@apple.com>
2010-09-09 16:16:39 +0000

    Use temporary files for this test, to see if we can flush out an inexplicable error on Windows
    llvm-svn: 113493

--- clang/test/Index/complete-hiding.c
+++ clang/test/Index/complete-hiding.c
@@ -16,14 +16,17 @@ void f() {
   struct StructA sa = { };
 }
 
-// RUN: c-index-test -code-completion-at=%s:16:3 %s | FileCheck -check-prefix=CHECK-CC1 %s
+// RUN: c-index-test -code-completion-at=%s:16:3 %s > %t
+// RUN: FileCheck -check-prefix=CHECK-CC1 -input-file=%t %s
 // RUN: env CINDEXTEST_EDITING=1 CINDEXTEST_COMPLETION_CACHING=1 c-index-test -code-completion-at=%s:16:3 %s | FileCheck -check-prefix=CHECK-CC1 %s
 // CHECK-CC1: VarDecl:{ResultType int}{TypedText StructA} (8)
 // CHECK-CC1: VarDecl:{ResultType int}{TypedText ValueA} (8)
 // CHECK-CC1-NOT: VarDecl:{ResultType int}{TypedText ValueA} (50)
 // CHECK-CC1: VarDecl:{ResultType int}{TypedText ValueB} (50)
-// RUN: c-index-test -code-completion-at=%s:16:10 %s | FileCheck -check-prefix=CHECK-CC2 %s
+// RUN: c-index-test -code-completion-at=%s:16:10 %s > %t
+// RUN: FileCheck -check-prefix=CHECK-CC2 -input-file=%t %s
 // CHECK-CC2: StructDecl:{TypedText StructA} (65)
 // CHECK-CC2-NOT: StructDecl:{TypedText StructB} (65)
 // CHECK-CC2: StructDecl:{TypedText StructC} (65)
-// RUN: env CINDEXTEST_EDITING=1 CINDEXTEST_COMPLETION_CACHING=1 c-index-test -code-completion-at=%s:16:10 %s | FileCheck -check-prefix=CHECK-CC2 %s
+// RUN: env CINDEXTEST_EDITING=1 CINDEXTEST_COMPLETION_CACHING=1 c-index-test -code-completion-at=%s:16:10 %s > %t
+// RUN: FileCheck -check-prefix=CHECK-CC2 -input-file=%t %s
