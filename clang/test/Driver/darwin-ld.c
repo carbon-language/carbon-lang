8f170b82392385c318e3e9bbc232a3135bbc421b
Francois Pichet <pichet2000@gmail.com>
2010-09-11 20:43:12 +0000

    Fix last test still failing on Win32. 100% pass rate now!
    llvm-svn: 113702

--- clang/test/Driver/darwin-ld.c
+++ clang/test/Driver/darwin-ld.c
@@ -28,15 +28,15 @@
 // RUN: %clang -ccc-host-triple i386-apple-darwin9 -### -arch armv6 -miphoneos-version-min=3.0 -bundle %t.o 2>> %t.log
 // RUN: FileCheck -check-prefix=LINK_IPHONE_3_0 %s < %t.log
 
-// LINK_IPHONE_3_0: ld"
+// LINK_IPHONE_3_0: {{ld(.exe)?"}}
 // LINK_IPHONE_3_0-NOT: -lcrt1.3.1.o
 // LINK_IPHONE_3_0: -lcrt1.o
 // LINK_IPHONE_3_0: -lSystem
-// LINK_IPHONE_3_0: ld"
+// LINK_IPHONE_3_0: {{ld(.exe)?"}}
 // LINK_IPHONE_3_0: -dylib
 // LINK_IPHONE_3_0: -ldylib1.o
 // LINK_IPHONE_3_0: -lSystem
-// LINK_IPHONE_3_0: ld"
+// LINK_IPHONE_3_0: {{ld(.exe)?"}}
 // LINK_IPHONE_3_0: -lbundle1.o
 // LINK_IPHONE_3_0: -lSystem
 
@@ -45,42 +45,42 @@
 // RUN: %clang -ccc-host-triple i386-apple-darwin9 -### -arch armv7 -miphoneos-version-min=3.1 -bundle %t.o 2>> %t.log
 // RUN: FileCheck -check-prefix=LINK_IPHONE_3_1 %s < %t.log
 
-// LINK_IPHONE_3_1: ld"
+// LINK_IPHONE_3_1: {{ld(.exe)?"}}
 // LINK_IPHONE_3_1-NOT: -lcrt1.o
 // LINK_IPHONE_3_1: -lcrt1.3.1.o
 // LINK_IPHONE_3_1: -lSystem
-// LINK_IPHONE_3_1: ld"
+// LINK_IPHONE_3_1: {{ld(.exe)?"}}
 // LINK_IPHONE_3_1: -dylib
 // LINK_IPHONE_3_1-NOT: -ldylib1.o
 // LINK_IPHONE_3_1: -lSystem
-// LINK_IPHONE_3_1: ld"
+// LINK_IPHONE_3_1: {{ld(.exe)?"}}
 // LINK_IPHONE_3_1-NOT: -lbundle1.o
 // LINK_IPHONE_3_1: -lSystem
 
 // RUN: %clang -ccc-host-triple i386-apple-darwin9 -### -fpie %t.o 2> %t.log
 // RUN: FileCheck -check-prefix=LINK_EXPLICIT_PIE %s < %t.log
 //
-// LINK_EXPLICIT_PIE: ld"
+// LINK_EXPLICIT_PIE: {{ld(.exe)?"}}
 // LINK_EXPLICIT_PIE: "-pie"
 
 // RUN: %clang -ccc-host-triple i386-apple-darwin9 -### -fno-pie %t.o 2> %t.log
 // RUN: FileCheck -check-prefix=LINK_EXPLICIT_NO_PIE %s < %t.log
 //
-// LINK_EXPLICIT_NO_PIE: ld"
+// LINK_EXPLICIT_NO_PIE: {{ld(.exe)?"}}
 // LINK_EXPLICIT_NO_PIE: "-no_pie"
 
 // RUN: %clang -ccc-host-triple x86_64-apple-darwin10 -### %t.o \
 // RUN:   -mlinker-version=100 2> %t.log
 // RUN: FileCheck -check-prefix=LINK_NEWER_DEMANGLE %s < %t.log
 //
-// LINK_NEWER_DEMANGLE: ld"
+// LINK_NEWER_DEMANGLE: {{ld(.exe)?"}}
 // LINK_NEWER_DEMANGLE: "-demangle"
 
 // RUN: %clang -ccc-host-triple x86_64-apple-darwin10 -### %t.o \
 // RUN:   -mlinker-version=100 -Wl,--no-demangle 2> %t.log
 // RUN: FileCheck -check-prefix=LINK_NEWER_NODEMANGLE %s < %t.log
 //
-// LINK_NEWER_NODEMANGLE: ld"
+// LINK_NEWER_NODEMANGLE: {{ld(.exe)?"}}
 // LINK_NEWER_NODEMANGLE-NOT: "-demangle"
 // LINK_NEWER_NODEMANGLE: "-lSystem"
 
@@ -88,6 +88,6 @@
 // RUN:   -mlinker-version=95 2> %t.log
 // RUN: FileCheck -check-prefix=LINK_OLDER_NODEMANGLE %s < %t.log
 //
-// LINK_OLDER_NODEMANGLE: ld"
+// LINK_OLDER_NODEMANGLE: {{ld(.exe)?"}}
 // LINK_OLDER_NODEMANGLE-NOT: "-demangle"
 // LINK_OLDER_NODEMANGLE: "-lSystem"
