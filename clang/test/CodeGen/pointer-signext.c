70004ec4567a7eda48a82962b59512837a054dc5
Cameron Esfahani <dirty@apple.com>
2010-09-15 10:52:02 +0000

    Fix pointer-signext.c test case: it was relying on value names, which don't appear in the non-assert build.  Switch to using check-next as well.
    llvm-svn: 113964

--- clang/test/CodeGen/pointer-signext.c
+++ clang/test/CodeGen/pointer-signext.c
@@ -3,9 +3,9 @@
 // Under Windows 64, int and long are 32-bits.  Make sure pointer math doesn't
 // cause any sign extensions.
 
-// CHECK: %sub.ptr.sub = add i64 %param, -8
-// CHECK: %0 = inttoptr i64 %sub.ptr.sub to %struct.anon*
-// CHECK: %tmp3 = getelementptr inbounds %struct.anon* %0, i64 0, i32 0
+// CHECK:      [[P:%.*]] = add i64 %param, -8
+// CHECK-NEXT: [[Q:%.*]] = inttoptr i64 [[P]] to [[R:%.*]]
+// CHECK-NEXT: {{%.*}} = getelementptr inbounds [[R]] [[Q]], i64 0, i32 0
 
 #define CR(Record, TYPE, Field) \
   ((TYPE *) ((unsigned char *) (Record) - (unsigned char *) &(((TYPE *) 0)->Field)))
