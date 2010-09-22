e12c8ba05b8d6247fcf9d6ad3907af0f8f562124
Jim Grosbach <grosbach@apple.com>
2010-09-22 18:37:14 +0000

    Add PrintSpecial() handling for  in ARM MC instruction printer.
    llvm-svn: 114563

--- llvm/lib/Target/ARM/AsmPrinter/ARMInstPrinter.h
+++ llvm/lib/Target/ARM/AsmPrinter/ARMInstPrinter.h
@@ -110,8 +110,7 @@ public:
   void printNEONModImmOperand(const MCInst *MI, unsigned OpNum, raw_ostream &O);
 
   void printPCLabel(const MCInst *MI, unsigned OpNum, raw_ostream &O);
-  // FIXME: Implement.
-  void PrintSpecial(const MCInst *MI, raw_ostream &O, const char *Kind) {}
+  void PrintSpecial(const MCInst *MI, raw_ostream &O, const char *Kind);
 };
 
 }
