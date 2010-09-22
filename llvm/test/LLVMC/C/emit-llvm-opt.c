922f13ce2e93f63a79433bc93c0a49370fbffe6a
Mikhail Glushenkov <foldr@codedgers.com>
2010-09-22 09:00:41 +0000

    llvmc: Support '-emit-llvm -S' with -opt.
    llvm-svn: 114541

---
+++ llvm/test/LLVMC/C/emit-llvm-opt.c
@@ -0,0 +1,7 @@
+// RUN: llvmc -c -opt -emit-llvm -o - %s | llvm-dis | grep "@f0()" | count 1
+// RUN: llvmc -c -opt -emit-llvm -S -o - %s | grep "@f0()" | count 1
+// RUN: llvmc --dry-run -c -opt -emit-llvm %s |& grep "^opt"
+// XFAIL: vg_leak
+
+int f0(void) {
+}
--- llvm/test/LLVMC/C/emit-llvm.c
+++ llvm/test/LLVMC/C/emit-llvm.c
@@ -1,4 +1,5 @@
 // RUN: llvmc -c -emit-llvm -o - %s | llvm-dis | grep "@f0()" | count 1
+// RUN: llvmc -c -emit-llvm -S -o - %s | grep "@f0()" | count 1
 // XFAIL: vg_leak
 
 int f0(void) {
