10d274d874666b4efaff535d4d48a65fe8d51bbb
Michael J. Spencer <bigcheesegs@gmail.com>
2010-09-24 09:01:13 +0000

    CMake: Build unittests.
    llvm-svn: 114725

--- llvm/utils/unittest/googletest/include/gtest/internal/gtest-port.h
+++ llvm/utils/unittest/googletest/include/gtest/internal/gtest-port.h
@@ -403,7 +403,8 @@
 // defining __GNUC__ and friends, but cannot compile GCC's tuple
 // implementation.  MSVC 2008 (9.0) provides TR1 tuple in a 323 MB
 // Feature Pack download, which we cannot assume the user has.
-#if (defined(__GNUC__) && !defined(__CUDACC__) && (GTEST_GCC_VER_ >= 40000)) \
+#if (defined(__GNUC__) && !(defined(__CUDACC__) || defined(__clang__)) \
+                       && (GTEST_GCC_VER_ >= 40000)) \
     || _MSC_VER >= 1600
 #define GTEST_USE_OWN_TR1_TUPLE 0
 #else
