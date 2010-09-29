e9da6db67fba424f20f9dcf4406f3ad93a622ff9
Duncan Sands <baldrick@free.fr>
2010-09-29 20:09:55 +0000

    Convert a bunch of uses of 'bytecode' into 'bitcode'.  This is not everything, but the remaining cases are less trivial.
    llvm-svn: 115080

--- llvm/utils/profile.pl
+++ llvm/utils/profile.pl
@@ -5,7 +5,7 @@
 # Synopsis: Insert instrumentation code into a program, run it with the JIT,
 #           then print out a profile report.
 #
-# Syntax:   profile.pl [OPTIONS] bytecodefile <arguments>
+# Syntax:   profile.pl [OPTIONS] bitcodefile <arguments>
 #
 # OPTIONS may include one or more of the following:
 #     -block    - Enable basicblock profiling
@@ -56,7 +56,7 @@ while (scalar(@ARGV) and ($_ = $ARGV[0], /^[-+]/)) {
   $LLVMProfOpts .= " " . $_;
 }
 
-die "Must specify LLVM bytecode file as first argument!" if (@ARGV == 0);
+die "Must specify LLVM bitcode file as first argument!" if (@ARGV == 0);
 
 my $BytecodeFile = $ARGV[0];
 
