1664634cbb6dc1be902c4b7352cff98098c94a64
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2010-10-01 00:00:18 +0000

    Add test case I forgot for r115159 (support implicit includes along with PCH).
    llvm-svn: 115247

---
+++ clang/test/PCH/cmdline-include.c
@@ -0,0 +1,6 @@
+// RUN: %clang_cc1 -include %S/cmdline-include1.h -x c-header %S/cmdline-include2.h -emit-pch -o %t
+// RUN: %clang_cc1 %s -include-pch %t -fsyntax-only -verify
+// RUN: %clang_cc1 -x c-header %S/cmdline-include1.h -emit-pch -o %t
+// RUN: %clang_cc1 %s -include-pch %t -include %S/cmdline-include2.h -fsyntax-only -verify
+
+int g = x1 + x2;
