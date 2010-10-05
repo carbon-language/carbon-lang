259bcdad06e52c56116dff1fd8ccd7455e7d924f
Rafael Espindola <rafael.espindola@gmail.com>
2010-10-05 15:43:32 +0000

    Tests that now pass.
    llvm-svn: 115622

--- llvm/test/MC/ELF/merge.s
+++ llvm/test/MC/ELF/merge.s
@@ -1,26 +1,57 @@
 // RUN: llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o - | elf-dump  --dump-section-data | FileCheck  %s
 
-// Test that relocations with local symbols in a mergeable section are done
+// Test that PIC relocations with local symbols in a mergeable section are done
 // with a reference to the symbol. Not sure if this is a linker limitation,
 // but this matches the behavior of gas.
 
+// Non-PIC relocations with 0 offset don't use the symbol.
+
+
+        movsd   .Lfoo(%rip), %xmm1
+        movl	$.Lfoo, %edi
+        movl	$.Lfoo+2, %edi
+
         .section        .sec1,"aM",@progbits,16
 .Lfoo:
-        .text
-        movsd   .Lfoo(%rip), %xmm1
+
+// Section 4 is "sec1"
+// CHECK: # Section 4
+// CHECK-NEXT:  (('sh_name', 18) # '.sec1'
 
 // Symbol number 1 is .Lfoo
-
 // CHECK:      # Symbol 1
 // CHECK-NEXT: (('st_name', 1) # '.Lfoo'
 
-// Relocation refers to symbol 1
+// Symbol number 5 is section 4
+// CHECK:        # Symbol 5
+// CHECK-NEXT:    (('st_name', 0) # ''
+// CHECK-NEXT:     ('st_bind', 0)
+// CHECK-NEXT:     ('st_type', 3)
+// CHECK-NEXT:     ('st_other', 0)
+// CHECK-NEXT:     ('st_shndx', 4)
 
+// Relocation 0 refers to symbol 1
 // CHECK:       ('_relocations', [
 // CHECK-NEXT:   # Relocation 0
 // CHECK-NEXT:   (('r_offset',
 // CHECK-NEXT:    ('r_sym', 1)
-// CHECK-NEXT:    ('r_type',
+// CHECK-NEXT:    ('r_type', 2
+// CHECK-NEXT:    ('r_addend',
+// CHECK-NEXT:   ),
+
+// Relocation 1 refers to symbol 5
+// CHECK-NEXT:  # Relocation 1
+// CHECK-NEXT: (('r_offset',
+// CHECK-NEXT:  ('r_sym', 5)
+// CHECK-NEXT:  ('r_type', 10)
+// CHECK-NEXT:  ('r_addend',
+// CHECK-NEXT: ),
+
+// Relocation 2 refers to symbol 1
+// CHECK-NEXT:   # Relocation 2
+// CHECK-NEXT:   (('r_offset',
+// CHECK-NEXT:    ('r_sym', 1)
+// CHECK-NEXT:    ('r_type', 10
 // CHECK-NEXT:    ('r_addend',
 // CHECK-NEXT:   ),
 // CHECK-NEXT:  ])
--- llvm/test/MC/ELF/undef.s
+++ llvm/test/MC/ELF/undef.s
@@ -1,15 +1,45 @@
 // RUN: llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o - | elf-dump  | FileCheck %s
 
-// Test that there is an undefined reference to .Lset0, but not .Lset1
+// Test which symbols should be in the symbol table
 
-        .long	.Lset0
-foo:
-bar:
-.Lset1 = foo - bar
-        .long	.Lset1
+        .long	.Lsym1
+.Lsym2:
+.Lsym3:
+.Lsym4 = .Lsym2 - .Lsym3
+        .long	.Lsym4
 
-// CHECK: ('_symbols', [
-// CHECK-NOT: Lset1
-// CHECK:  (('st_name', 9) # '.Lset0'
-// CHECK-NOT: Lset1
-// CHECK: (('sh_name', 36) # '.strtab'
+	.type	.Lsym5,@object
+        .type   sym6,@object
+
+	.section	.rodata.str1.1,"aMS",@progbits,1
+.Lsym7:
+.Lsym8:
+
+        .text
+        movsd   .Lsym8(%rip), %xmm1
+
+// CHECK:      ('_symbols', [
+// CHECK-NEXT:  # Symbol 0
+// CHECK-NEXT:  (('st_name', 0) # ''
+// CHECK:       # Symbol 1
+// CHECK-NEXT:  (('st_name', 1) # '.Lsym8'
+// CHECK:       # Symbol 2
+// CHECK-NEXT:  (('st_name', 0) # ''
+// CHECK:       # Symbol 3
+// CHECK-NEXT:  (('st_name', 0) # ''
+// CHECK:       # Symbol 4
+// CHECK-NEXT:  (('st_name', 0) # ''
+// CHECK:       # Symbol 5
+// CHECK-NEXT:  (('st_name', 0) # ''
+// CHECK:       # Symbol 6
+// CHECK-NEXT:  (('st_name', 8) # '.Lsym1'
+// CHECK:       # Symbol 7
+// CHECK-NEXT:  (('st_name', 15) # 'sym6'
+// CHECK-NEXT:   ('st_bind', 1)
+// CHECK-NEXT:   ('st_type', 1)
+// CHECK-NEXT:   ('st_other', 0)
+// CHECK-NEXT:   ('st_shndx', 0)
+// CHECK-NEXT:   ('st_value', 0)
+// CHECK-NEXT:   ('st_size', 0)
+// CHECK-NEXT:   ),
+// CHECK-NEXT:  ])
