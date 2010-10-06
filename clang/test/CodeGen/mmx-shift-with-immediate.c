6153d2c7bd4c6eff7f17421861d5ed48abea1a30
Bill Wendling <isanbard@gmail.com>
2010-10-06 03:31:26 +0000

    MMX conversion work is done. We can remove the FIXMEs.
    llvm-svn: 115736

--- clang/test/CodeGen/mmx-shift-with-immediate.c
+++ clang/test/CodeGen/mmx-shift-with-immediate.c
@@ -6,17 +6,14 @@ void shift(__m64 a, __m64 b, int c) {
   _mm_slli_pi16(a, c);
   // CHECK: x86_mmx @llvm.x86.mmx.pslli.d(x86_mmx %{{.*}}, i32 {{.*}})
   _mm_slli_pi32(a, c);
-  // FIXME: <1 x i64> @llvm.x86.mmx.pslli.q(<1 x i64> %{{.*}}, i32 {{.*}})
-  // This is currently lowered into non-intrinsic instructions. This may not be
-  // correct once the MMX reworking is finished.
+  // CHECK: x86_mmx @llvm.x86.mmx.pslli.q(x86_mmx %{{.*}}, i32 {{.*}})
   _mm_slli_si64(a, c);
 
   // CHECK: x86_mmx @llvm.x86.mmx.psrli.w(x86_mmx %{{.*}}, i32 {{.*}})
   _mm_srli_pi16(a, c);
   // CHECK: x86_mmx @llvm.x86.mmx.psrli.d(x86_mmx %{{.*}}, i32 {{.*}})
   _mm_srli_pi32(a, c);
-  // FIXME: <1 x i64> @llvm.x86.mmx.psrli.q(<1 x i64> %{{.*}}, i32 {{.*}})
-  // See above.
+  // CHECK: x86_mmx @llvm.x86.mmx.psrli.q(x86_mmx %{{.*}}, i32 {{.*}})
   _mm_srli_si64(a, c);
 
   // CHECK: x86_mmx @llvm.x86.mmx.psrai.w(x86_mmx %{{.*}}, i32 {{.*}})
