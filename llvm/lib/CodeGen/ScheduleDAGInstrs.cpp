ec0da969fb5dbdb6de13e90e0b31c12c12cf51e2
Nick Lewycky <nicholas@mxc.ca>
2010-10-06 18:11:50 +0000

    Remove unused variables.
    llvm-svn: 115802

--- llvm/lib/CodeGen/ScheduleDAGInstrs.cpp
+++ llvm/lib/CodeGen/ScheduleDAGInstrs.cpp
@@ -527,9 +527,6 @@ void ScheduleDAGInstrs::ComputeOperandLatency(SUnit *Def, SUnit *Use,
   MachineInstr *DefMI = Def->getInstr();
   int DefIdx = DefMI->findRegisterDefOperandIdx(Reg);
   if (DefIdx != -1) {
-    const TargetInstrDesc &DefTID = DefMI->getDesc();
-    unsigned DefClass = DefTID.getSchedClass();
-
     MachineInstr *UseMI = Use->getInstr();
     // For all uses of the register, calculate the maxmimum latency
     int Latency = -1;
