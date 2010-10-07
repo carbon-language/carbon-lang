7713f260457deb77908cb82977cf04b7589bd9dc
Michael J. Spencer <bigcheesegs@gmail.com>
2010-10-07 18:51:10 +0000

    Fix Whitespace.
    llvm-svn: 115959

--- llvm/tools/llvm-stub/llvm-stub.c
+++ llvm/tools/llvm-stub/llvm-stub.c
@@ -1,10 +1,10 @@
 /*===- llvm-stub.c - Stub executable to run llvm bitcode files ------------===//
-// 
+//
 //                     The LLVM Compiler Infrastructure
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
-// 
+//
 //===----------------------------------------------------------------------===//
 //
 // This tool is used by the gccld program to enable transparent execution of
