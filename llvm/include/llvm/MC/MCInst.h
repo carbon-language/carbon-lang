2a14088b656a99c7c4c8a5260f9472ed4cea94ad
Jim Grosbach <grosbach@apple.com>
2010-10-08 21:36:13 +0000

    Allow << streaming of MCOperand.
    llvm-svn: 116107

--- llvm/include/llvm/MC/MCInst.h
+++ llvm/include/llvm/MC/MCInst.h
@@ -155,6 +155,11 @@ public:
                    StringRef Separator = " ") const;
 };
 
+inline raw_ostream& operator<<(raw_ostream &OS, const MCOperand &MO) {
+  MO.print(OS, 0);
+  return OS;
+}
+
 inline raw_ostream& operator<<(raw_ostream &OS, const MCInst &MI) {
   MI.print(OS, 0);
   return OS;
