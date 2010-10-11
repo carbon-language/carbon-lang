fe2a89683a2705e3af108ae0348328146991742c
Daniel Dunbar <daniel@zuster.org>
2010-10-11 21:34:24 +0000

    Change explicit search Apple specific code to only reference __eprintf on x86.
    llvm-svn: 116239

--- llvm/lib/System/SearchForAddressOfSpecialSymbol.cpp
+++ llvm/lib/System/SearchForAddressOfSpecialSymbol.cpp
@@ -32,14 +32,6 @@ static void *DoSearch(const char* symbolName) {
     EXPLICIT_SYMBOL(__ashrdi3);
     EXPLICIT_SYMBOL(__cmpdi2);
     EXPLICIT_SYMBOL(__divdi3);
-
-    // Clang doesn't always link against libgcc.a, which is the only thing which
-    // defines ___eprintf in the modern world. Just don't attempt to export this
-    // symbol when building with Clang.
-#ifndef __clang__
-    EXPLICIT_SYMBOL(__eprintf);
-#endif
-
     EXPLICIT_SYMBOL(__fixdfdi);
     EXPLICIT_SYMBOL(__fixsfdi);
     EXPLICIT_SYMBOL(__fixunsdfdi);
@@ -50,6 +42,11 @@ static void *DoSearch(const char* symbolName) {
     EXPLICIT_SYMBOL(__moddi3);
     EXPLICIT_SYMBOL(__udivdi3);
     EXPLICIT_SYMBOL(__umoddi3);
+
+    // __eprintf is sometimes used for assert() handling on x86.
+#ifdef __i386__
+    EXPLICIT_SYMBOL(__eprintf);
+#endif
   }
 #endif
 
