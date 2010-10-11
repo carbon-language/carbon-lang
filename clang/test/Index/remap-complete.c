21d0e5a06c667534de778093b28a41a32dc65ea6
Douglas Gregor <dgregor@apple.com>
2010-10-11 23:18:55 +0000

    This test should now work on windows, since we're no longer using temporary files badly
    llvm-svn: 116261

--- clang/test/Index/remap-complete.c
+++ clang/test/Index/remap-complete.c
@@ -1,6 +1,5 @@
 // RUN: c-index-test -code-completion-at=%s:6:2 -remap-file="%s;%S/Inputs/remap-complete-to.c" %s 2> %t.err | FileCheck %s
 // RUN: FileCheck -check-prefix=CHECK-DIAGS %s < %t.err
-// XFAIL: win32
 
 // CHECK: FunctionDecl:{ResultType int}{TypedText f0}{LeftParen (}
 void f() { }
