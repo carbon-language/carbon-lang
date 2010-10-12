78c0aeb4b3bf6aa74b22e81d161660112db9dfaa
Johnny Chen <johnny.chen@apple.com>
2010-10-12 23:01:50 +0000

    Update the comment with reagard to a bug filed against possible clang-generated debug info which results in the breakpoint locations of 3, instead of 1 (as in gcc).
    llvm-svn: 116361

--- lldb/test/stl/TestSTL.py
+++ lldb/test/stl/TestSTL.py
@@ -37,6 +37,10 @@ class STLTestCase(TestBase):
 
         self.runCmd("file " + exe, CURRENT_EXECUTABLE_SET)
 
+        # rdar://problem/8543077
+        # test/stl: clang built binaries results in the breakpoint locations = 3,
+        # is this a problem with clang generated debug info?
+        #
         # Break on line 13 of main.cpp.
         self.expect("breakpoint set -f main.cpp -l %d" % self.line,
                     BREAKPOINT_CREATED,
