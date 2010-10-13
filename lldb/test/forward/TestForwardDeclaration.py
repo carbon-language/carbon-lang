1fd394170a9100a069ce235749f96f2d3c646c84
Johnny Chen <johnny.chen@apple.com>
2010-10-13 18:47:36 +0000

    Add a test case for the test/forward directory with @expectedFailure decorator for test_with_dwarf_and_run_command(self).
    llvm-svn: 116416

---
+++ lldb/test/forward/TestForwardDeclaration.py
@@ -0,0 +1,64 @@
+"""Test that forward declaration of a data structure gets resolved correctly."""
+
+import os, time
+import unittest2
+import lldb
+from lldbtest import *
+
+class ForwardDeclarationTestCase(TestBase):
+
+    mydir = "forward"
+
+    @unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
+    def test_with_dsym_and_run_command(self):
+        """Display *bar_ptr when stopped on a function with forward declaration of struct bar."""
+        self.buildDsym()
+        self.forward_declaration()
+
+    # rdar://problem/8546815
+    # './dotest.py -v -t forward' fails for test_with_dwarf_and_run_command
+    @unittest2.expectedFailure
+    def test_with_dwarf_and_run_command(self):
+        """Display *bar_ptr when stopped on a function with forward declaration of struct bar."""
+        self.buildDwarf()
+        self.forward_declaration()
+
+    def forward_declaration(self):
+        """Display *bar_ptr when stopped on a function with forward declaration of struct bar."""
+        exe = os.path.join(os.getcwd(), "a.out")
+        self.runCmd("file " + exe, CURRENT_EXECUTABLE_SET)
+
+        # Break inside the foo function which takes a bar_ptr argument.
+        self.expect("breakpoint set -n foo", BREAKPOINT_CREATED,
+            startstr = "Breakpoint created: 1: name = 'foo', locations = 1")
+
+        self.runCmd("run", RUN_SUCCEEDED)
+
+        # The stop reason of the thread should be breakpoint.
+        self.expect("thread list", STOPPED_DUE_TO_BREAKPOINT,
+            substrs = ['state is Stopped',
+                       'stop reason = breakpoint'])
+
+        # The breakpoint should have a hit count of 1.
+        self.expect("breakpoint list", BREAKPOINT_HIT_ONCE,
+            substrs = [' resolved, hit count = 1'])
+
+        # This should display correctly.
+        # Note that the member fields of a = 1 and b = 2 is by design.
+        self.expect("frame variable *bar_ptr", VARIABLES_DISPLAYED_CORRECTLY,
+            substrs = ['(struct bar *) bar_ptr = ',
+                       '(int) a = 1',
+                       '(int) b = 2'])
+
+        # And so should this.
+        self.expect("expr *bar_ptr", VARIABLES_DISPLAYED_CORRECTLY,
+            substrs = ['(struct bar)',
+                       '(int) a = 1',
+                       '(int) b = 2'])
+
+
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
