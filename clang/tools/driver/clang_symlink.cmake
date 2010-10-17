95b6f045f1f104b96d443c404755c2757b6f6cf7
Oscar Fuentes <ofv@wanadoo.es>
2010-10-17 16:10:32 +0000

    Use a script for creating the clang++ executable.
    The previous method used the DESTDIR environment variable at configure
    time, but sometimes it is only available at install time. See PR8397.
    
    llvm-svn: 116689

---
+++ clang/tools/driver/clang_symlink.cmake
@@ -0,0 +1,18 @@
+# We need to execute this script at installation time because the
+# DESTDIR environment variable may be unset at configuration time.
+# See PR8397.
+
+if(UNIX)
+  set(CLANGXX_LINK_OR_COPY create_symlink)
+  set(CLANGXX_DESTDIR $ENV{DESTDIR})
+else()
+  set(CLANGXX_LINK_OR_COPY copy)
+endif()
+
+set(bindir "${CLANGXX_DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/")
+set(clang "${bindir}clang${CMAKE_EXECUTABLE_SUFFIX}")
+set(clangxx "${bindir}clang++${CMAKE_EXECUTABLE_SUFFIX}")
+
+message("Creating clang++ executable based on ${clang}")
+
+execute_process(COMMAND "${CMAKE_COMMAND}" -E ${CLANGXX_LINK_OR_COPY} "${clang}" "${clangxx}")
