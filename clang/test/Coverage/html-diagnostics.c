19844798962ed0288a1e89bba7f32394aa03978e
NAKAMURA Takumi <geek4civic@gmail.com>
2010-10-19 03:38:22 +0000

    test/Coverage/html-diagnostics.c: Use find(1) to glob wildcards.
    MSYS cat(1) does not expand wildcards.
    
    llvm-svn: 116793

--- clang/test/Coverage/html-diagnostics.c
+++ clang/test/Coverage/html-diagnostics.c
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t
 // RUN: %clang_cc1 -analyze -analyzer-output=html -analyzer-check-objc-mem -o %t %s
-// RUN: cat %t/*.html | FileCheck %s
+// RUN: find %t -type f -name '*.html' -exec cat '{}' ';' | FileCheck %s
 
 // CHECK: <h3>Annotated Source Code</h3>
 
