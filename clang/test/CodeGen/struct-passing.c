8d3f5db7e554828c9944bbf5803613d265432b12
Michael J. Spencer <bigcheesegs@gmail.com>
2010-10-19 06:39:49 +0000

    test: FileCheck'ize and document test.
    llvm-svn: 116799

--- clang/test/CodeGen/struct-passing.c
+++ clang/test/CodeGen/struct-passing.c
@@ -1,11 +1,8 @@
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -o %t %s
-// RUN: grep 'declare i32 @f0() readnone' %t
-// RUN: grep 'declare i32 @f1() readonly' %t
-// RUN: grep 'declare void @f2(.* sret)' %t
-// RUN: grep 'declare void @f3(.* sret)' %t
-// RUN: grep 'declare void @f4(.* byval)' %t
-// RUN: grep 'declare void @f5(.* byval)' %t
-// PR3835
+// This verifies that structs returned from functions by value are passed
+// correctly according to their attributes and the ABI.
+// SEE: PR3835
+
+// RUN: %clang_cc1 -triple i386-unknown-unknown -emit-llvm %s -o - | FileCheck %s
 
 typedef int T0;
 typedef struct { int a[16]; } T1;
@@ -18,3 +15,10 @@ void __attribute__((const)) f4(T1 a);
 void __attribute__((pure)) f5(T1 a);
 
 void *ps[] = { f0, f1, f2, f3, f4, f5 };
+
+// CHECK: declare i32 @f0() readnone
+// CHECK: declare i32 @f1() readonly
+// CHECK: declare void @f2({{.*}} sret)
+// CHECK: declare void @f3({{.*}} sret)
+// CHECK: declare void @f4({{.*}} byval)
+// CHECK: declare void @f5({{.*}} byval)
