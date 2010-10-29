7eed15242d9ce5241355c7f6d96db6c71edc5ec9
Owen Anderson <resistor@mac.com>
2010-10-29 18:41:40 +0000

    Convert this file to less fragile .s form.
    llvm-svn: 117681

---
+++ llvm/test/MC/ARM/neon-bitcount-encoding.s
@@ -0,0 +1,32 @@
+// RUN: llvm-mc -triple arm-unknown-unkown -show-encoding < %s | FileCheck %s
+// XFAIL: *
+
+// CHECK: vcnt.8	d16, d16                @ encoding: [0x20,0x05,0xf0,0xf3]
+	vcnt.8	d16, d16
+// CHECK: vcnt.8	q8, q8                  @ encoding: [0x60,0x05,0xf0,0xf3]
+	vcnt.8	q8, q8
+// CHECK: vclz.i8	d16, d16                @ encoding: [0xa0,0x04,0xf0,0xf3]
+	vclz.i8	d16, d16
+// CHECK: vclz.i16	d16, d16        @ encoding: [0xa0,0x04,0xf4,0xf3]
+	vclz.i16	d16, d16
+// CHECK: vclz.i32	d16, d16        @ encoding: [0xa0,0x04,0xf8,0xf3]
+	vclz.i32	d16, d16
+// CHECK: vclz.i8	q8, q8                  @ encoding: [0xe0,0x04,0xf0,0xf3]
+	vclz.i8	q8, q8
+// CHECK: vclz.i16	q8, q8          @ encoding: [0xe0,0x04,0xf4,0xf3]
+	vclz.i16	q8, q8
+// CHECK: vclz.i32	q8, q8          @ encoding: [0xe0,0x04,0xf8,0xf3]
+	vclz.i32	q8, q8
+// CHECK: vcls.s8	d16, d16                @ encoding: [0x20,0x04,0xf0,0xf3]
+	vcls.s8	d16, d16
+// CHECK: vcls.s16	d16, d16        @ encoding: [0x20,0x04,0xf4,0xf3]
+	vcls.s16	d16, d16
+// CHECK: vcls.s32	d16, d16        @ encoding: [0x20,0x04,0xf8,0xf3]
+	vcls.s32	d16, d16
+// CHECK: vcls.s8	q8, q8                  @ encoding: [0x60,0x04,0xf0,0xf3]
+	vcls.s8	q8, q8
+// CHECK: vcls.s16	q8, q8          @ encoding: [0x60,0x04,0xf4,0xf3]
+	vcls.s16	q8, q8
+// CHECK: vcls.s32	q8, q8          @ encoding: [0x60,0x04,0xf8,0xf3]
+	vcls.s32	q8, q8
+
