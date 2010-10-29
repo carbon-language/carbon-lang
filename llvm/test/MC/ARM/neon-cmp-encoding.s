733d1b1a609aafc7d8fcf26ed5e4be4db8971ecd
Owen Anderson <resistor@mac.com>
2010-10-29 18:58:30 +0000

    Convert this test to .s form.
    llvm-svn: 117683

---
+++ llvm/test/MC/ARM/neon-cmp-encoding.s
@@ -0,0 +1,104 @@
+// RUN: llvm-mc -triple arm-unknown-unkown -show-encoding < %s | FileCheck %s
+// XFAIL: *
+
+// FIXME: We cannot currently test the following instructions, which are 
+// currently marked as for-disassembly only in the .td files:
+//  - VCEQz
+//  - VCGEz, VCLEz
+//  - VCGTz, VCLTz
+
+// CHECK: vceq.i8	d16, d16, d17           @ encoding: [0xb1,0x08,0x40,0xf3]
+	vceq.i8	d16, d16, d17
+// CHECK: vceq.i16	d16, d16, d17   @ encoding: [0xb1,0x08,0x50,0xf3]
+	vceq.i16	d16, d16, d17
+// CHECK: vceq.i32	d16, d16, d17   @ encoding: [0xb1,0x08,0x60,0xf3]
+	vceq.i32	d16, d16, d17
+// CHECK: vceq.f32	d16, d16, d17   @ encoding: [0xa1,0x0e,0x40,0xf2]
+	vceq.f32	d16, d16, d17
+// CHECK: vceq.i8	q8, q8, q9              @ encoding: [0xf2,0x08,0x40,0xf3]
+	vceq.i8	q8, q8, q9
+// CHECK: vceq.i16	q8, q8, q9      @ encoding: [0xf2,0x08,0x50,0xf3]
+	vceq.i16	q8, q8, q9
+// CHECK: vceq.i32	q8, q8, q9      @ encoding: [0xf2,0x08,0x60,0xf3]
+	vceq.i32	q8, q8, q9
+// CHECK: vceq.f32	q8, q8, q9      @ encoding: [0xe2,0x0e,0x40,0xf2]
+	vceq.f32	q8, q8, q9
+
+// CHECK: vcge.s8	d16, d16, d17           @ encoding: [0xb1,0x03,0x40,0xf2]
+	vcge.s8	d16, d16, d17
+// CHECK: vcge.s16	d16, d16, d17   @ encoding: [0xb1,0x03,0x50,0xf2]
+	vcge.s16	d16, d16, d17
+// CHECK: vcge.s32	d16, d16, d17   @ encoding: [0xb1,0x03,0x60,0xf2]
+	vcge.s32	d16, d16, d17
+// CHECK: vcge.u8	d16, d16, d17           @ encoding: [0xb1,0x03,0x40,0xf3]
+	vcge.u8	d16, d16, d17
+// CHECK: vcge.u16	d16, d16, d17   @ encoding: [0xb1,0x03,0x50,0xf3]
+	vcge.u16	d16, d16, d17
+// CHECK: vcge.u32	d16, d16, d17   @ encoding: [0xb1,0x03,0x60,0xf3]
+	vcge.u32	d16, d16, d17
+// CHECK: vcge.f32	d16, d16, d17   @ encoding: [0xa1,0x0e,0x40,0xf3]
+	vcge.f32	d16, d16, d17
+// CHECK: vcge.s8	q8, q8, q9              @ encoding: [0xf2,0x03,0x40,0xf2]
+	vcge.s8	q8, q8, q9
+// CHECK: vcge.s16	q8, q8, q9      @ encoding: [0xf2,0x03,0x50,0xf2]
+	vcge.s16	q8, q8, q9
+// CHECK: vcge.s32	q8, q8, q9      @ encoding: [0xf2,0x03,0x60,0xf2]
+	vcge.s32	q8, q8, q9
+// CHECK: vcge.u8	q8, q8, q9              @ encoding: [0xf2,0x03,0x40,0xf3]
+	vcge.u8	q8, q8, q9
+// CHECK: vcge.u16	q8, q8, q9      @ encoding: [0xf2,0x03,0x50,0xf3]
+	vcge.u16	q8, q8, q9
+// CHECK: vcge.u32	q8, q8, q9      @ encoding: [0xf2,0x03,0x60,0xf3]
+	vcge.u32	q8, q8, q9
+// CHECK: vcge.f32	q8, q8, q9      @ encoding: [0xe2,0x0e,0x40,0xf3]
+	vcge.f32	q8, q8, q9
+// CHECK: vacge.f32	d16, d16, d17   @ encoding: [0xb1,0x0e,0x40,0xf3]
+	vacge.f32	d16, d16, d17
+// CHECK: vacge.f32	q8, q8, q9      @ encoding: [0xf2,0x0e,0x40,0xf3]
+	vacge.f32	q8, q8, q9
+
+// CHECK: vcgt.s8	d16, d16, d17           @ encoding: [0xa1,0x03,0x40,0xf2]
+	vcgt.s8	d16, d16, d17
+// CHECK: vcgt.s16	d16, d16, d17   @ encoding: [0xa1,0x03,0x50,0xf2]
+	vcgt.s16	d16, d16, d17
+// CHECK: vcgt.s32	d16, d16, d17   @ encoding: [0xa1,0x03,0x60,0xf2]
+	vcgt.s32	d16, d16, d17
+// CHECK: vcgt.u8	d16, d16, d17           @ encoding: [0xa1,0x03,0x40,0xf3]
+	vcgt.u8	d16, d16, d17
+// CHECK: vcgt.u16	d16, d16, d17   @ encoding: [0xa1,0x03,0x50,0xf3]
+	vcgt.u16	d16, d16, d17
+// CHECK: vcgt.u32	d16, d16, d17   @ encoding: [0xa1,0x03,0x60,0xf3]
+	vcgt.u32	d16, d16, d17
+// CHECK: vcgt.f32	d16, d16, d17   @ encoding: [0xa1,0x0e,0x60,0xf3]
+	vcgt.f32	d16, d16, d17
+// CHECK: vcgt.s8	q8, q8, q9              @ encoding: [0xe2,0x03,0x40,0xf2]
+	vcgt.s8	q8, q8, q9
+// CHECK: vcgt.s16	q8, q8, q9      @ encoding: [0xe2,0x03,0x50,0xf2]
+	vcgt.s16	q8, q8, q9
+// CHECK: vcgt.s32	q8, q8, q9      @ encoding: [0xe2,0x03,0x60,0xf2]
+	vcgt.s32	q8, q8, q9
+// CHECK: vcgt.u8	q8, q8, q9              @ encoding: [0xe2,0x03,0x40,0xf3]
+	vcgt.u8	q8, q8, q9
+// CHECK: vcgt.u16	q8, q8, q9      @ encoding: [0xe2,0x03,0x50,0xf3]
+	vcgt.u16	q8, q8, q9
+// CHECK: vcgt.u32	q8, q8, q9      @ encoding: [0xe2,0x03,0x60,0xf3]
+	vcgt.u32	q8, q8, q9
+// CHECK: vcgt.f32	q8, q8, q9      @ encoding: [0xe2,0x0e,0x60,0xf3]
+	vcgt.f32	q8, q8, q9
+// CHECK: vacgt.f32	d16, d16, d17   @ encoding: [0xb1,0x0e,0x60,0xf3]
+	vacgt.f32	d16, d16, d17
+// CHECK: vacgt.f32	q8, q8, q9      @ encoding: [0xf2,0x0e,0x60,0xf3]
+	vacgt.f32	q8, q8, q9
+
+// CHECK: vtst.8	d16, d16, d17           @ encoding: [0xb1,0x08,0x40,0xf2]
+	vtst.8	d16, d16, d17
+// CHECK: vtst.16	d16, d16, d17           @ encoding: [0xb1,0x08,0x50,0xf2]
+	vtst.16	d16, d16, d17
+// CHECK: vtst.32	d16, d16, d17           @ encoding: [0xb1,0x08,0x60,0xf2]
+	vtst.32	d16, d16, d17
+// CHECK: vtst.8	q8, q8, q9              @ encoding: [0xf2,0x08,0x40,0xf2]
+	vtst.8	q8, q8, q9
+// CHECK: vtst.16	q8, q8, q9              @ encoding: [0xf2,0x08,0x50,0xf2]
+	vtst.16	q8, q8, q9
+// CHECK: vtst.32	q8, q8, q9              @ encoding: [0xf2,0x08,0x60,0xf2]
+	vtst.32	q8, q8, q9
