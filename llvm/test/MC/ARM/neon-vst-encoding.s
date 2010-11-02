b95618cfe0f4bd423110cf03fd078ae626ee1c50
Owen Anderson <resistor@mac.com>
2010-11-02 21:47:03 +0000

    Add correct encodings for basic variants for vst3 and vst4.
    llvm-svn: 118082

--- llvm/test/MC/ARM/neon-vst-encoding.s
+++ llvm/test/MC/ARM/neon-vst-encoding.s
@@ -30,3 +30,39 @@
   vst2.16	{d16, d17, d18, d19}, [r0, :128]
 @ CHECK: vst2.32	{d16, d17, d18, d19}, [r0, :256] @ encoding: [0xbf,0x03,0x40,0xf4]
   vst2.32	{d16, d17, d18, d19}, [r0, :256]
+
+@ CHECK: vst3.8	{d16, d17, d18}, [r0, :64] @ encoding: [0x1f,0x04,0x40,0xf4]
+  vst3.8	{d16, d17, d18}, [r0, :64]
+@ CHECK: vst3.16	{d16, d17, d18}, [r0]   @ encoding: [0x4f,0x04,0x40,0xf4]
+  vst3.16	{d16, d17, d18}, [r0]
+@ CHECK: vst3.32	{d16, d17, d18}, [r0]   @ encoding: [0x8f,0x04,0x40,0xf4]
+  vst3.32	{d16, d17, d18}, [r0]
+@ CHECK: vst3.8	{d16, d18, d20}, [r0, :64]! @ encoding: [0x1d,0x05,0x40,0xf4]
+  vst3.8	{d16, d18, d20}, [r0, :64]!
+@ CHECK: vst3.8	{d17, d19, d21}, [r0, :64]! @ encoding: [0x1d,0x15,0x40,0xf4]
+  vst3.8	{d17, d19, d21}, [r0, :64]!
+@ CHECK: vst3.16	{d16, d18, d20}, [r0]!  @ encoding: [0x4d,0x05,0x40,0xf4]
+  vst3.16	{d16, d18, d20}, [r0]!
+@ CHECK: vst3.16	{d17, d19, d21}, [r0]!  @ encoding: [0x4d,0x15,0x40,0xf4]
+  vst3.16	{d17, d19, d21}, [r0]!
+@ CHECK: vst3.32	{d16, d18, d20}, [r0]!  @ encoding: [0x8d,0x05,0x40,0xf4]
+  vst3.32	{d16, d18, d20}, [r0]!
+@ CHECK: vst3.32	{d17, d19, d21}, [r0]!  @ encoding: [0x8d,0x15,0x40,0xf4]
+  vst3.32	{d17, d19, d21}, [r0]!
+
+@ CHECK: vst4.8	{d16, d17, d18, d19}, [r0, :64] @ encoding: [0x1f,0x00,0x40,0xf4]
+  vst4.8	{d16, d17, d18, d19}, [r0, :64]
+@ CHECK: vst4.16	{d16, d17, d18, d19}, [r0, :128] @ encoding: [0x6f,0x00,0x40,0xf4]
+  vst4.16	{d16, d17, d18, d19}, [r0, :128]
+@ CHECK: vst4.8	{d16, d18, d20, d22}, [r0, :256]! @ encoding: [0x3d,0x01,0x40,0xf4]
+  vst4.8	{d16, d18, d20, d22}, [r0, :256]!
+@ CHECK: vst4.8	{d17, d19, d21, d23}, [r0, :256]! @ encoding: [0x3d,0x11,0x40,0xf4]
+  vst4.8	{d17, d19, d21, d23}, [r0, :256]!
+@ CHECK: vst4.16	{d16, d18, d20, d22}, [r0]! @ encoding: [0x4d,0x01,0x40,0xf4]
+  vst4.16	{d16, d18, d20, d22}, [r0]!
+@ CHECK: vst4.16	{d17, d19, d21, d23}, [r0]! @ encoding: [0x4d,0x11,0x40,0xf4]
+  vst4.16	{d17, d19, d21, d23}, [r0]!
+@ CHECK: vst4.32	{d16, d18, d20, d22}, [r0]! @ encoding: [0x8d,0x01,0x40,0xf4]
+  vst4.32	{d16, d18, d20, d22}, [r0]!
+@ CHECK: vst4.32	{d17, d19, d21, d23}, [r0]! @ encoding: [0x8d,0x11,0x40,0xf4]
+  vst4.32	{d17, d19, d21, d23}, [r0]!
