c06f422d73437df8061f59064bf6f4dfe8d9130f
Johnny Chen <johnny.chen@apple.com>
2010-11-08 19:03:37 +0000

    Let's also emit the informational message for where to find the session logs for failed/errored tests at the start of the test run, in case the test run crashes for any reason.  That way, it is easy to locate the session logs for accumulated failures/errors.
    llvm-svn: 118427

--- lldb/test/dotest.py
+++ lldb/test/dotest.py
@@ -573,6 +573,7 @@ if not sdir_name:
     timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H_%M_%S")
     sdir_name = timestamp
 os.environ["LLDB_SESSION_DIRNAME"] = sdir_name
+sys.stderr.write("\nSession info for test failures/errors will go into directory '%s'\n" % sdir_name)
 
 #
 # Invoke the default TextTestRunner to run the test suite, possibly iterating
