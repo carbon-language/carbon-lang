181fd8cd89ff97fe654fabbb2032e0ad6607b4d0
Michael J. Spencer <bigcheesegs@gmail.com>
2010-11-09 15:11:19 +0000

    System/Path/Windows: Make GetSystemLibraryPaths more generic.
    llvm-svn: 118505

--- llvm/lib/System/Win32/Win32.h
+++ llvm/lib/System/Win32/Win32.h
@@ -18,10 +18,12 @@
 
 // Require at least Windows 2000 API.
 #define _WIN32_WINNT 0x0500
+#define _WIN32_IE    0x0500 // MinGW at it again.
 #define WIN32_LEAN_AND_MEAN
 
 #include "llvm/Config/config.h" // Get build system configuration settings
 #include <Windows.h>
+#include <ShlObj.h>
 #include <cassert>
 #include <string>
 
