bb6e51c957e3494a7329baa6944cbe3eaaf08fc4
Michael J. Spencer <bigcheesegs@gmail.com>
2010-11-09 15:11:07 +0000

    System/Windows: Reduce dependencies.
    llvm-svn: 118504

--- llvm/lib/System/Win32/Win32.h
+++ llvm/lib/System/Win32/Win32.h
@@ -18,6 +18,7 @@
 
 // Require at least Windows 2000 API.
 #define _WIN32_WINNT 0x0500
+#define WIN32_LEAN_AND_MEAN
 
 #include "llvm/Config/config.h" // Get build system configuration settings
 #include <Windows.h>
