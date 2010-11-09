3655e596882ac6381b70f02a0d97fac2a0ae94dc
Fariborz Jahanian <fjahanian@apple.com>
2010-11-09 02:24:08 +0000

    Reverse patch for // rdar://8632525. It might has broken a build.
    llvm-svn: 118475

--- clang/test/CodeGenObjC/implicit-objc_msgSend.m
+++ clang/test/CodeGenObjC/implicit-objc_msgSend.m
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin9 -emit-llvm -o %t %s
-// RUN: grep -F 'declare i8* @objc_msgSend(i8*, i8*, ...)' %t
+// RUN: grep -F 'declare i8* @objc_msgSend(...)' %t
 
 typedef struct objc_selector *SEL;
 id f0(id x, SEL s) {
