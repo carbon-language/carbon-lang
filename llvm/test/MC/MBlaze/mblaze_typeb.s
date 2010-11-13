240162629c26a6b4ba48ef6e741b90ba5e2c136d
Wesley Peck <peckw@wesleypeck.com>
2010-11-13 00:03:46 +0000

    1. Adding missing immediate mode asm parser test cases. 2. Fixing improper immediate mode reverse subtract.
    llvm-svn: 118948

--- llvm/test/MC/MBlaze/mblaze_typeb.s
+++ llvm/test/MC/MBlaze/mblaze_typeb.s
@@ -35,3 +35,58 @@
 # BINARY:   101011 00001 00010 0000000000001111
 # CHECK:    encoding: [0xac,0x22,0x00,0x0f]
             andni   r1, r2, 0x000F
+
+# CHECK:    muli
+# BINARY:   011000 00001 00010 0000000000001111
+# CHECK:    encoding: [0x60,0x22,0x00,0x0f]
+            muli    r1, r2, 0x000F
+
+# CHECK:    ori
+# BINARY:   101000 00001 00010 0000000000001111
+# CHECK:    encoding: [0xa0,0x22,0x00,0x0f]
+            ori     r1, r2, 0x000F
+
+# CHECK:    rsubi
+# BINARY:   001001 00001 00010 0000000000001111
+# CHECK:    encoding: [0x24,0x22,0x00,0x0f]
+            rsubi   r1, r2, 0x000F
+
+# CHECK:    rsubic
+# BINARY:   001011 00001 00010 0000000000001111
+# CHECK:    encoding: [0x2c,0x22,0x00,0x0f]
+            rsubic  r1, r2, 0x000F
+
+# CHECK:    rsubik
+# BINARY:   001101 00001 00010 0000000000001111
+# CHECK:    encoding: [0x34,0x22,0x00,0x0f]
+            rsubik  r1, r2, 0x000F
+
+# CHECK:    rsubikc
+# BINARY:   001111 00001 00010 0000000000001111
+# CHECK:    encoding: [0x3c,0x22,0x00,0x0f]
+            rsubikc r1, r2, 0x000F
+
+# CHECK:    rtbd
+# BINARY:   101101 10010 01111 0000000000001111
+# CHECK:    encoding: [0xb6,0x4f,0x00,0x0f]
+            rtbd r15, 0x000F
+
+# CHECK:    rted
+# BINARY:   101101 10001 01111 0000000000001111
+# CHECK:    encoding: [0xb6,0x8f,0x00,0x0f]
+            rted r15, 0x000F
+
+# CHECK:    rtid
+# BINARY:   101101 10001 01111 0000000000001111
+# CHECK:    encoding: [0xb6,0x2f,0x00,0x0f]
+            rtid r15, 0x000F
+
+# CHECK:    rtsd
+# BINARY:   101101 10000 01111 0000000000001111
+# CHECK:    encoding: [0xb6,0x0f,0x00,0x0f]
+            rtsd r15, 0x000F
+
+# CHECK:    xori
+# BINARY:   101010 00001 00010 0000000000001111
+# CHECK:    encoding: [0xa8,0x22,0x00,0x0f]
+            xori r1, r2, 0x000F
