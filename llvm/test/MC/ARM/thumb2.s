0e7d7283274e30456c2cd5aac4ccb89b9f9509ec
Owen Anderson <resistor@mac.com>
2010-11-15 18:45:17 +0000

    Add correct Thumb2 encodings for mvn and friends.
    llvm-svn: 119170

--- llvm/test/MC/ARM/thumb2.s
+++ llvm/test/MC/ARM/thumb2.s
@@ -12,3 +12,12 @@
   adc	r1, r1, #1448498774
 @ CHECK: adc	r1, r1, #66846720       @ encoding: [0x7f,0x71,0x41,0xf1]
   adc	r1, r1, #66846720
+
+@ CHECK: mvn	r0, #187                @ encoding: [0xbb,0x00,0x6f,0xf0]
+  mvn	r0, #187
+@ CHECK: mvn	r0, #11141290           @ encoding: [0xaa,0x10,0x6f,0xf0]
+  mvn	r0, #11141290
+@ CHECK: mvn	r0, #-872363008         @ encoding: [0xcc,0x20,0x6f,0xf0]
+  mvn	r0, #-872363008
+@ CHECK: mvn	r0, #1114112            @ encoding: [0x88,0x10,0x6f,0xf4]
+  mvn	r0, #1114112
