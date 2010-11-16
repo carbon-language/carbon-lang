c4931c4746abb6d6520e6bb3222d2b74a4e49a94
Howard Hinnant <hhinnant@apple.com>
2010-11-16 21:10:23 +0000

    I have reverted all contributions made by Jesse Towner in revision 110724
    llvm-svn: 119383

--- libcxx/test/language.support/support.types/nullptr_t.pass.cpp
+++ libcxx/test/language.support/support.types/nullptr_t.pass.cpp
@@ -24,12 +24,11 @@ int main()
                   "sizeof(std::nullptr_t) == sizeof(void*)");
     A* p = 0;
     assert(p == nullptr);
-    assert(nullptr == p);
-#if !((__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ <= 5))
-    // GCC 4.2 through 4.5 can't handle this
     void (A::*pmf)() = 0;
+#ifdef __clang__
+    // GCC 4.2 can't handle this
     assert(pmf == nullptr);
-#endif  // !((__GNUC__ < 4) || (__GNUC__ == 4 && __GNUC_MINOR__ <= 5))
+#endif
     int A::*pmd = 0;
     assert(pmd == nullptr);
     A a1(nullptr);
@@ -42,20 +41,4 @@ int main()
     assert(!(nullptr != nullptr));
     assert(!(nullptr < nullptr));
     assert(!(nullptr > nullptr));
-    assert(!(&a1 == nullptr));
-    assert(!(nullptr == &a1));
-    assert(&a1 != nullptr);
-    assert(nullptr != &a1);
-    assert(nullptr < &a1);
-    assert(nullptr <= &a1);
-    assert(!(nullptr < p));
-    assert(nullptr <= p);
-    assert(!(&a1 < nullptr));
-    assert(!(&a1 <= nullptr));
-    assert(!(p < nullptr));
-    assert(p <= nullptr);
-    assert(!(nullptr > &a1));
-    assert(!(nullptr >= &a1));
-    assert(!(nullptr > p));
-    assert(nullptr >= p);
 }
