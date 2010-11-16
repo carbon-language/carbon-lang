fc4feca7d8a84307eb59ea7c9067ca609ecbb5cf
Dale Johannesen <dalej@apple.com>
2010-11-16 21:57:15 +0000

    Test for llvm-gcc patch 119392.
    llvm-svn: 119393

---
+++ llvm/test/FrontendC/2010-11-16-asmblock.c
@@ -0,0 +1,16 @@
+// RUN: %llvmgcc -S %s -fasm-blocks -o - | FileCheck %s
+// XFAIL: *
+// XTARGET: x86,i386,i686
+// 84282548
+
+void foo()
+{
+// CHECK:  %0 = call i32 asm sideeffect "", "={ecx}"() nounwind 
+// CHECK:  %asmtmp = call i32 asm sideeffect alignstack "sall $$3, $0", "={ecx},{ecx},~{dirflag},~{fpsr},~{flags},~{memory}"(i32 %0) nounwind 
+// CHECK:  store i32 %asmtmp, i32* %"%ecx"
+ __asm {
+   sal ecx, 3;
+   add esi, ecx;
+   add edi, ecx;
+ }
+}
