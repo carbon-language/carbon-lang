dca25f69ca63a3f41eed6b6753098fc6be99843d
Chris Lattner <sabre@nondot.org>
2010-11-18 02:53:02 +0000

    trivial QoI improvement.  On this invalid input:   sahf	movl	344(%rdi),%r14d we used to produce:
    t.s:2:1: error: unexpected token in argument list
    
    ^
    
    we now produce:
    t.s:1:11: error: unexpected token in argument list
    sahf	movl	344(%rdi),%r14d
        	    	^
    
    rdar://8581401
    
    llvm-svn: 119676

--- llvm/lib/Target/X86/AsmParser/X86AsmParser.cpp
+++ llvm/lib/Target/X86/AsmParser/X86AsmParser.cpp
@@ -744,8 +744,9 @@ ParseInstruction(StringRef Name, SMLoc NameLoc,
     }
 
     if (getLexer().isNot(AsmToken::EndOfStatement)) {
+      SMLoc Loc = getLexer().getLoc();
       Parser.EatToEndOfStatement();
-      return TokError("unexpected token in argument list");
+      return Error(Loc, "unexpected token in argument list");
     }
   }
 
