945b776b6eb901ae96f272594fb3151d507a1e8c
Bill Wendling <isanbard@gmail.com>
2010-11-19 01:33:10 +0000

    Add MC encodings for some Thumb instructions. Test for a few of them. The "bx lr" instruction cannot be tested just yet. It requires matching a "condition code", but adding one of those makes things go south quickly...
    llvm-svn: 119774

---
+++ llvm/test/MC/ARM/thumb.s
@@ -0,0 +1,8 @@
+@ RUN: llvm-mc -triple thumb-apple-darwin -show-encoding < %s | FileCheck %s
+        .code 16
+
+@ CHECK: cmp	r1, r2               @ encoding: [0x91,0x42]
+        cmp     r1, r2
+
+@ CHECK: pop    {r1, r2, r4}         @ encoding: [0x16,0xbc]
+        pop     {r1, r2, r4}
