1879bca4c9c38ec6278c6bd7b97f7544b5d3cafe
Nico Weber <nicolasweber@gmx.de>
2010-11-22 10:26:41 +0000

    Minor whitespace fix, no functionality change
    llvm-svn: 119965

--- clang/lib/Rewrite/RewriteObjC.cpp
+++ clang/lib/Rewrite/RewriteObjC.cpp
@@ -33,14 +33,14 @@ using llvm::utostr;
 namespace {
   class RewriteObjC : public ASTConsumer {
     enum {
-      BLOCK_FIELD_IS_OBJECT   =  3,  /* id, NSObject, __attribute__((NSObject)), 
+      BLOCK_FIELD_IS_OBJECT   =  3,  /* id, NSObject, __attribute__((NSObject)),
                                         block, ... */
       BLOCK_FIELD_IS_BLOCK    =  7,  /* a block variable */
       BLOCK_FIELD_IS_BYREF    =  8,  /* the on stack structure holding the 
                                         __block variable */
-      BLOCK_FIELD_IS_WEAK     = 16,  /* declared __weak, only used in byref copy 
+      BLOCK_FIELD_IS_WEAK     = 16,  /* declared __weak, only used in byref copy
                                         helpers */
-      BLOCK_BYREF_CALLER      = 128, /* called from __block (byref) copy/dispose 
+      BLOCK_BYREF_CALLER      = 128, /* called from __block (byref) copy/dispose
                                         support routines */
       BLOCK_BYREF_CURRENT_MAX = 256
     };
