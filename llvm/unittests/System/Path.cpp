98847783f2902b14915490c778e113b4390a6139
Michael J. Spencer <bigcheesegs@gmail.com>
2010-11-24 19:20:05 +0000

    unittests: Add SystemTests.
    llvm-svn: 120101

---
+++ llvm/unittests/System/Path.cpp
@@ -0,0 +1,18 @@
+//===- llvm/unittest/System/Path.cpp - Path tests -------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+
+namespace {
+
+TEST(System, Path) {
+  // TODO: Add tests!
+}
+
+} // anonymous namespace
---
+++ llvm/unittests/System/TimeValue.cpp
@@ -0,0 +1,23 @@
+//===- llvm/unittest/System/TimeValue.cpp - Time Vlaue tests --------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+#include "llvm/System/TimeValue.h"
+#include <time.h>
+
+using namespace llvm;
+namespace {
+
+TEST(System, TimeValue) {
+  sys::TimeValue now = sys::TimeValue::now();
+  time_t now_t = time(NULL);
+  EXPECT_TRUE(abs(static_cast<long>(now_t - now.toEpochTime())) < 2);
+}
+
+}
