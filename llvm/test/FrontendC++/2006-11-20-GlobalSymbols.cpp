0be0ae625d48d830450d8ae1054a6b02c509c259
Duncan Sands <baldrick@free.fr>
2010-11-25 21:26:21 +0000

    Judging from the comment, the system assembler is supposed to assemble the output of this test.  Since it was producing bitcode, that clearly wasn't happening!  Have it produce target assembler and assemble that instead.
    llvm-svn: 120159

--- llvm/test/FrontendC++/2006-11-20-GlobalSymbols.cpp
+++ llvm/test/FrontendC++/2006-11-20-GlobalSymbols.cpp
@@ -1,7 +1,7 @@
 // PR1013
 // Check to make sure debug symbols use the correct name for globals and
 // functions.  Will not assemble if it fails to.
-// RUN: %llvmgcc -O0 -g -c %s
+// RUN: %llvmgcc_only -O0 -g -c %s
 
 int foo __asm__("f\001oo");
 
