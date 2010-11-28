81f6f3626eaed9d3f29f1d474ba629a93dfa9388
Anders Carlsson <andersca@mac.com>
2010-11-28 17:46:52 +0000

    Add a CGCXXABI.cpp file.
    llvm-svn: 120249

---
+++ clang/lib/CodeGen/CGCXXABI.cpp
@@ -0,0 +1,18 @@
+//===----- CGCXXABI.cpp - Interface to C++ ABIs -----------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This provides an abstract class for C++ code generation. Concrete subclasses
+// of this implement code generation for specific C++ ABIs.
+//
+//===----------------------------------------------------------------------===//
+
+#include "CGCXXABI.h"
+
+using namespace clang;
+
