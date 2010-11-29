9600e97c60b2a78944016fda753d4ca4f88b49eb
Bill Wendling <isanbard@gmail.com>
2010-11-29 00:42:50 +0000

    Add Thumb encodings for REV instructions.
    llvm-svn: 120277

--- llvm/test/MC/ARM/thumb.s
+++ llvm/test/MC/ARM/thumb.s
@@ -1,4 +1,4 @@
-@ RUN: llvm-mc -triple thumb-apple-darwin -show-encoding < %s | FileCheck %s
+@ RUN: llvm-mc -triple thumbv6-apple-darwin -show-encoding < %s | FileCheck %s
         .code 16
 
 @ CHECK: cmp	r1, r2               @ encoding: [0x91,0x42]
@@ -12,3 +12,10 @@
 
 @ CHECK: blx	r9                   @ encoding: [0xc8,0x47]
 	blx	r9
+
+@ CHECK: rev	r2, r3               @ encoding: [0x1a,0xba]
+@ CHECK: rev16	r3, r4               @ encoding: [0x63,0xba]
+@ CHECK: revsh	r5, r6               @ encoding: [0xf5,0xba]
+        rev     r2, r3
+        rev16   r3, r4
+        revsh   r5, r6
