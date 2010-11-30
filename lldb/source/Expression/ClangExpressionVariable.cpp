f5a998643c4fc5647cf8313abfad2cd3bf61ae97
Sean Callanan <scallanan@apple.com>
2010-11-30 22:01:58 +0000

    Fixed a problem where m_register_info was not being copied by the copy constructor for ClangExpressionVariable. This meant that a NULL m_register_info wouldn't be copied, and instead the field was uninitialized, potentially confusing the materializer.
    llvm-svn: 120472

--- lldb/source/Expression/ClangExpressionVariable.cpp
+++ lldb/source/Expression/ClangExpressionVariable.cpp
@@ -47,7 +47,8 @@ ClangExpressionVariable::DisableDataVars()
 ClangExpressionVariable::ClangExpressionVariable(const ClangExpressionVariable &rhs) :
     m_name(rhs.m_name),
     m_user_type(rhs.m_user_type),
-    m_store(rhs.m_store),
+    m_store(rhs.m_store),  
+    m_register_info(rhs.m_register_info),
     m_index(rhs.m_index)
 {
     if (rhs.m_parser_vars.get())
