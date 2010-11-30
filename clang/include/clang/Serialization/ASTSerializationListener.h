7ac52b921b4c5c220dc863f70285ecfd5b91e0f8
Douglas Gregor <dgregor@apple.com>
2010-11-30 06:17:12 +0000

    Add missing file from last commit
    llvm-svn: 120397

---
+++ clang/include/clang/Serialization/ASTSerializationListener.h
@@ -0,0 +1,44 @@
+//===- ASTSerializationListener.h - Decl/Type PCH Write Events -*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+//  This file defines the ASTSerializationListener class, which is notified
+//  by the ASTWriter when an entity is serialized.
+//
+//===----------------------------------------------------------------------===//
+#ifndef LLVM_CLANG_FRONTEND_AST_SERIALIZATION_LISTENER_H
+#define LLVM_CLANG_FRONTEND_AST_SERIALIZATION_LISTENER_H
+
+#include "llvm/System/DataTypes.h"
+
+namespace clang {
+
+class PreprocessedEntity;
+  
+/// \brief Listener object that receives callbacks when certain kinds of 
+/// entities are serialized.
+class ASTSerializationListener {
+public:
+  virtual ~ASTSerializationListener();
+  
+  /// \brief Callback invoked whenever a preprocessed entity is serialized.
+  ///
+  /// This callback will only occur when the translation unit was created with
+  /// a detailed preprocessing record.
+  ///
+  /// \param Entity The entity that has been serialized.
+  ///
+  /// \param Offset The offset (in bits) of this entity in the resulting
+  /// AST file.
+  virtual void SerializedPreprocessedEntity(PreprocessedEntity *Entity,
+                                            uint64_t Offset) = 0;
+};
+
+}
+
+#endif
