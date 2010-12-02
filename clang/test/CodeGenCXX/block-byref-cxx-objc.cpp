3086ddffd7ab415dc7755b76a6cf477b1927d401
Fariborz Jahanian <fjahanian@apple.com>
2010-12-02 17:58:10 +0000

    Test for // rdar://8594790
    llvm-svn: 120717

---
+++ clang/test/CodeGenCXX/block-byref-cxx-objc.cpp
@@ -0,0 +1,25 @@
+// RUN: %clang_cc1 %s -emit-llvm -o - -fblocks | FileCheck %s
+// rdar://8594790
+
+struct A {
+	int x;
+	A(const A &);
+	A();
+	~A();
+};
+
+int main()
+{
+	__block A BYREF_VAR;
+        ^{ BYREF_VAR.x = 1234; };
+	return 0;
+}
+
+// CHECK: define internal void @__Block_byref_object_copy_
+// CHECK: call void @_ZN1AC1ERKS_
+// CHECK: define internal void @__Block_byref_object_dispose_
+// CHECK: call void @_ZN1AD1Ev
+// CHECK: define internal void @__copy_helper_block_
+// CHECK: call void @_Block_object_assign
+// CHECK: define internal void @__destroy_helper_block_
+// CHECK: call void @_Block_object_dispose
