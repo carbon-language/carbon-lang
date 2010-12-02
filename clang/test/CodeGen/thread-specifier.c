c629a5c7d00f2fd52784924b0108a0c59a097783
Eric Christopher <echristo@apple.com>
2010-12-02 02:30:43 +0000

    FileCheckize.
    llvm-svn: 120648

--- clang/test/CodeGen/thread-specifier.c
+++ clang/test/CodeGen/thread-specifier.c
@@ -1,11 +1,15 @@
-// RUN: %clang_cc1 -triple i686-pc-linux-gnu -emit-llvm -o - %s | grep thread_local | count 4
-// RUN: %clang_cc1 -triple i686-pc-linux-gnu -emit-llvm -o - %s | not grep common
+// RUN: %clang_cc1 -triple i686-pc-linux-gnu -emit-llvm -o - %s | FileCheck %s
 
+// CHECK: @b = external thread_local global
+// CHECK: @d.e = internal thread_local global
+// CHECK: @d.f = internal thread_local global
+// CHECK: @a = thread_local global
 __thread int a;
 extern __thread int b;
-int c() { return &b; }
+int c() { return *&b; }
 int d() {
   __thread static int e;
   __thread static union {float a; int b;} f = {.b = 1};
+  return 0;
 }
 
