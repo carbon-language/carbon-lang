5ceace4d147babba9ad867c03396611b39b3584d
Bob Wilson <bob.wilson@apple.com>
2010-12-02 00:25:18 +0000

    Add a testcase for Radar 8228022. Make sure the -Wvector-conversions does not cause unnecessary warnings when using Neon intrinsics with the correct types.
    llvm-svn: 120634

---
+++ clang/test/Headers/arm-neon-header.c
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -triple thumbv7-apple-darwin10 -target-cpu cortex-a8 -fsyntax-only -Wvector-conversions -verify %s
+// RUN: %clang_cc1 -x c++ -triple thumbv7-apple-darwin10 -target-cpu cortex-a8 -fsyntax-only -Wvector-conversions -verify %s
+
+#include <arm_neon.h>
+
+// Radar 8228022: Should not report incompatible vector types.
+int32x2_t test(int32x2_t x) {
+  return vshr_n_s32(x, 31);
+}
