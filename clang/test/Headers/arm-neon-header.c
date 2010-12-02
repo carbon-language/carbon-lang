13e3b00e7619688a9e69eec0e0a49f74f2aed365
Bob Wilson <bob.wilson@apple.com>
2010-12-02 01:18:30 +0000

    Test use of arm_neon.h with -fno-lax-vector-conversions.
    llvm-svn: 120642

--- clang/test/Headers/arm-neon-header.c
+++ clang/test/Headers/arm-neon-header.c
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -triple thumbv7-apple-darwin10 -target-cpu cortex-a8 -fsyntax-only -Wvector-conversions -verify %s
+// RUN: %clang_cc1 -triple thumbv7-apple-darwin10 -target-cpu cortex-a8 -fsyntax-only -fno-lax-vector-conversions -verify %s
 // RUN: %clang_cc1 -x c++ -triple thumbv7-apple-darwin10 -target-cpu cortex-a8 -fsyntax-only -Wvector-conversions -verify %s
 
 #include <arm_neon.h>
