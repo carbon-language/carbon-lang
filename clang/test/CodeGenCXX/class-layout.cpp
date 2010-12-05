0febb8acdfcb26f3268dd7438303efbb4f979777
Anders Carlsson <andersca@mac.com>
2010-12-05 00:08:52 +0000

    Put each test in class-layout.cpp into a separate namespace.
    llvm-svn: 120925

--- clang/test/CodeGenCXX/class-layout.cpp
+++ clang/test/CodeGenCXX/class-layout.cpp
@@ -1,13 +1,19 @@
-// RUN: %clang_cc1 %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 %s -triple x86_64-apple-darwin10 -emit-llvm -o - | FileCheck %s
 
 // An extra byte should be allocated for an empty class.
-// CHECK: %struct.A = type { i8 }
-struct A { } a;
+namespace Test1 {
+  // CHECK: %"struct.Test1::A" = type { i8 }
+  struct A { } *a;
+}
 
-// No need to add tail padding here.
-// CHECK: %struct.B = type { i8*, i32 }
-struct B { void *a; int b; } b;
+namespace Test2 {
+  // No need to add tail padding here.
+  // CHECK: %"struct.Test2::A" = type { i8*, i32 }
+  struct A { void *a; int b; } *a;
+}
 
-// C should have a vtable pointer.
-// CHECK: %struct.C = type { i32 (...)**, i32 }
-struct C { virtual void f(); int a; } *c;
+namespace Test3 {
+  // C should have a vtable pointer.
+  // CHECK: %"struct.Test3::A" = type { i32 (...)**, i32 }
+  struct A { virtual void f(); int a; } *a;
+}
