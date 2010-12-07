547cc6f0a5f52d378e4124417b0c5202e3815612
NAKAMURA Takumi <geek4civic@gmail.com>
2010-12-07 02:43:45 +0000

    lib/Target/X86/X86MCAsmInfo.cpp: [PR8741] On Win64, specify explicit PrivateGlobalPrefix as ".L".
    Or, global symbols @Lxxxx might be treated as temporal symbol by MCSymbol.
    
    llvm-svn: 121103

--- llvm/lib/Target/X86/X86MCAsmInfo.cpp
+++ llvm/lib/Target/X86/X86MCAsmInfo.cpp
@@ -103,8 +103,10 @@ getNonexecutableStackSection(MCContext &Ctx) const {
 }
 
 X86MCAsmInfoCOFF::X86MCAsmInfoCOFF(const Triple &Triple) {
-  if (Triple.getArch() == Triple::x86_64)
+  if (Triple.getArch() == Triple::x86_64) {
     GlobalPrefix = "";
+    PrivateGlobalPrefix = ".L";
+  }
 
   AsmTransCBE = x86_asm_table;
   AssemblerDialect = AsmWriterFlavor;
