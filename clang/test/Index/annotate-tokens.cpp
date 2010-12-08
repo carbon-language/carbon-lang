c247e1a11900da1acf4079fbccd528b497d101d1
Francois Pichet <pichet2000@gmail.com>
2010-12-08 09:04:37 +0000

    Fix test that didn't really test anything.
    llvm-svn: 121246

--- clang/test/Index/annotate-tokens.cpp
+++ clang/test/Index/annotate-tokens.cpp
@@ -3,7 +3,7 @@ void test(bonk X) {
     X = X;
 }
 
-// RUN: c-index-test -test-annotate-tokens=%s:1:1:5:5 %s
+// RUN: c-index-test -test-annotate-tokens=%s:1:1:5:5 %s | FileCheck %s
 // CHECK: Keyword: "struct" [1:1 - 1:7] StructDecl=bonk:1:8 (Definition)
 // CHECK: Identifier: "bonk" [1:8 - 1:12] StructDecl=bonk:1:8 (Definition)
 // CHECK: Punctuation: "{" [1:13 - 1:14] StructDecl=bonk:1:8 (Definition)
@@ -17,7 +17,7 @@ void test(bonk X) {
 // CHECK: Punctuation: ")" [2:17 - 2:18] FunctionDecl=test:2:6 (Definition)
 // CHECK: Punctuation: "{" [2:19 - 2:20] UnexposedStmt=
 // CHECK: Identifier: "X" [3:5 - 3:6] DeclRefExpr=X:2:16
-// CHECK: Punctuation: "=" [3:7 - 3:8] DeclRefExpr=operator=:1:8
+// CHECK: Punctuation: "=" [3:7 - 3:8] CallExpr=operator=:1:8
 // CHECK: Identifier: "X" [3:9 - 3:10] DeclRefExpr=X:2:16
 // CHECK: Punctuation: ";" [3:10 - 3:11] UnexposedStmt=
 // CHECK: Punctuation: "}" [4:1 - 4:2] UnexposedStmt=
