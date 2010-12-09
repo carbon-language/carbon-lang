38c3ca78e63deca89d16d2d2317570e55c5fb2a1
Bob Wilson <bob.wilson@apple.com>
2010-12-09 18:31:01 +0000

    Add a license comment to the generated arm_neon.h header. Remove the previous header.  I don't think we need to expose to end users that we use TableGen to produce our version of arm_neon.h, and that header was also using doubleslash comments which could be a problem when using it in strict C89 compilations.
    llvm-svn: 121390

--- llvm/utils/TableGen/NeonEmitter.cpp
+++ llvm/utils/TableGen/NeonEmitter.cpp
@@ -1057,9 +1057,29 @@ static std::string GenIntrinsic(const std::string &name,
 /// run - Read the records in arm_neon.td and output arm_neon.h.  arm_neon.h
 /// is comprised of type definitions and function declarations.
 void NeonEmitter::run(raw_ostream &OS) {
-  EmitSourceFileHeader("ARM NEON Header", OS);
-
-  // FIXME: emit license into file?
+  OS << 
+    "/*===---- arm_neon.h - ARM Neon intrinsics ---------------------------------===\n"
+    " *\n"
+    " * Permission is hereby granted, free of charge, to any person obtaining a copy\n"
+    " * of this software and associated documentation files (the \"Software\"), to deal\n"
+    " * in the Software without restriction, including without limitation the rights\n"
+    " * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell\n"
+    " * copies of the Software, and to permit persons to whom the Software is\n"
+    " * furnished to do so, subject to the following conditions:\n"
+    " *\n"
+    " * The above copyright notice and this permission notice shall be included in\n"
+    " * all copies or substantial portions of the Software.\n"
+    " *\n"
+    " * THE SOFTWARE IS PROVIDED \"AS IS\", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR\n"
+    " * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,\n"
+    " * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE\n"
+    " * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER\n"
+    " * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,\n"
+    " * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN\n"
+    " * THE SOFTWARE.\n"
+    " *\n"
+    " *===-----------------------------------------------------------------------===\n"
+    " */\n\n";
 
   OS << "#ifndef __ARM_NEON_H\n";
   OS << "#define __ARM_NEON_H\n\n";
