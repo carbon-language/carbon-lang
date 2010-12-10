aae7926e792986c7c8c2ce047a4f792afe30a861
Fariborz Jahanian <fjahanian@apple.com>
2010-12-10 00:39:16 +0000

    Added remaining objective-c library builtins (as in gcc) to clang. // rdar://8735023
    llvm-svn: 121441

--- clang/test/SemaObjC/builtin_objc_getClass.m
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cc1 %s -fsyntax-only -std=gnu99 -verify
-// rdar://8592641
-Class f0() { return objc_getClass("a"); } // expected-warning {{implicitly declaring C library function 'objc_getClass' with type 'id (const char *)'}} \
-					  // expected-note {{please include the header <objc/runtime.h> or explicitly provide a declaration for 'objc_getClass'}}
