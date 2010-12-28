1f22457975cc1ed32089251c0dd818998bbaa097
Duncan Sands <baldrick@free.fr>
2010-12-28 18:01:19 +0000

    Clarify that InstructionSimplify only returns values that dominate the original instruction.
    llvm-svn: 122601

--- llvm/include/llvm/Analysis/InstructionSimplify.h
+++ llvm/include/llvm/Analysis/InstructionSimplify.h
@@ -11,7 +11,8 @@
 // that do not require creating new instructions.  This does constant folding
 // ("add i32 1, 1" -> "2") but can also handle non-constant operands, either
 // returning a constant ("and i32 %x, 0" -> "0") or an already existing value
-// ("and i32 %x, %x" -> "%x").
+// ("and i32 %x, %x" -> "%x").  If the simplification is also an instruction
+// then it dominates the original instruction.
 //
 //===----------------------------------------------------------------------===//
 
