697de77339d7aa6308cfe70f35ddbbaa2f818bee
Duncan Sands <baldrick@free.fr>
2011-01-03 10:50:04 +0000

    Speed up instsimplify by about 10-15% by not bothering to retry InstructionSimplify on instructions that didn't change since the last time round the loop.
    llvm-svn: 122745

--- llvm/lib/Transforms/Utils/SimplifyInstructions.cpp
+++ llvm/lib/Transforms/Utils/SimplifyInstructions.cpp
@@ -19,6 +19,7 @@
 #include "llvm/Pass.h"
 #include "llvm/Type.h"
 #include "llvm/ADT/DepthFirstIterator.h"
+#include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/Statistic.h"
 #include "llvm/Analysis/Dominators.h"
 #include "llvm/Analysis/InstructionSimplify.h"
@@ -44,28 +45,38 @@ namespace {
     bool runOnFunction(Function &F) {
       const DominatorTree *DT = getAnalysisIfAvailable<DominatorTree>();
       const TargetData *TD = getAnalysisIfAvailable<TargetData>();
+      SmallPtrSet<const Instruction*, 8> S1, S2, *ToSimplify = &S1, *Next = &S2;
       bool Changed = false;
-      bool LocalChanged;
 
       do {
-        LocalChanged = false;
-
         for (df_iterator<BasicBlock*> DI = df_begin(&F.getEntryBlock()),
              DE = df_end(&F.getEntryBlock()); DI != DE; ++DI)
           for (BasicBlock::iterator BI = DI->begin(), BE = DI->end(); BI != BE;) {
             Instruction *I = BI++;
-            // Don't bother simplifying unused instructions.
+            // The first time through the loop ToSimplify is empty and we try to
+            // simplify all instructions.  On later iterations ToSimplify is not
+            // empty and we only bother simplifying instructions that are in it.
+            if (!ToSimplify->empty() && !ToSimplify->count(I))
+              continue;
+            // Don't waste time simplifying unused instructions.
             if (!I->use_empty())
               if (Value *V = SimplifyInstruction(I, TD, DT)) {
+                // Mark all uses for resimplification next time round the loop.
+                for (Value::use_iterator UI = I->use_begin(), UE = I->use_end();
+                     UI != UE; ++UI)
+                  Next->insert(cast<Instruction>(*UI));
                 I->replaceAllUsesWith(V);
-                LocalChanged = true;
                 ++NumSimplified;
+                Changed = true;
               }
-            LocalChanged |= RecursivelyDeleteTriviallyDeadInstructions(I);
+            Changed |= RecursivelyDeleteTriviallyDeadInstructions(I);
           }
 
-        Changed |= LocalChanged;
-      } while (LocalChanged);
+        // Place the list of instructions to simplify on the next loop iteration
+        // into ToSimplify.
+        std::swap(ToSimplify, Next);
+        Next->clear();
+      } while (!ToSimplify->empty());
 
       return Changed;
     }
