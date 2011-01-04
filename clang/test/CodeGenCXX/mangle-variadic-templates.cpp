4234eb23b3bdd14cb7d5044fefce631c9a3c8842
Douglas Gregor <dgregor@apple.com>
2011-01-04 22:41:29 +0000

    Tweak test for portability
    llvm-svn: 122850

--- clang/test/CodeGenCXX/mangle-variadic-templates.cpp
+++ clang/test/CodeGenCXX/mangle-variadic-templates.cpp
@@ -3,11 +3,11 @@
 template<unsigned I, typename ...Types>
 struct X { };
 
-// CHECK: define weak_odr void @_Z2f0IJEEv1XIXsZT_EJspRT_EE()
+// CHECK: define weak_odr void @_Z2f0IJEEv1XIXsZT_EJspRT_EE
 template<typename ...Types>
 void f0(X<sizeof...(Types), Types&...>) { }
 
 template void f0(X<0>);
 
-// CHECK: define weak_odr void @_Z2f0IJifdEEv1XIXsZT_EJspRT_EE()
+// CHECK: define weak_odr void @_Z2f0IJifdEEv1XIXsZT_EJspRT_EE
 template void f0<int, float, double>(X<3, int&, float&, double&>);
