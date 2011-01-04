ad6edc1eef555109e7f9e2a3054c9eec5e071dd8
Douglas Gregor <dgregor@apple.com>
2011-01-04 22:16:00 +0000

    Simplistic test case for mangling of variadic templates. There is more to be done in this area
    llvm-svn: 122845

---
+++ clang/test/CodeGenCXX/mangle-variadic-templates.cpp
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -std=c++0x -emit-llvm -o - %s | FileCheck %s
+
+template<unsigned I, typename ...Types>
+struct X { };
+
+// CHECK: define weak_odr void @_Z2f0IJEEv1XIXsZT_EJspRT_EE()
+template<typename ...Types>
+void f0(X<sizeof...(Types), Types&...>) { }
+
+template void f0(X<0>);
+
+// CHECK: define weak_odr void @_Z2f0IJifdEEv1XIXsZT_EJspRT_EE()
+template void f0<int, float, double>(X<3, int&, float&, double&>);
