88cf54160f64fe828c598145c77b80367864c237
Abramo Bagnara <abramo.bagnara@gmail.com>
2011-01-06 16:56:00 +0000

    Added testcase for commit r122963.
    llvm-svn: 122964

--- clang/test/Lexer/11-27-2007-FloatLiterals.c
+++ clang/test/Lexer/11-27-2007-FloatLiterals.c
@@ -4,8 +4,10 @@
 // CHECK: 2.000000e+{{[0]*}}32
 // CHECK: 0x3BFD83C940000000
 // CHECK: 2.000000e+{{[0]*}}32
+// CHECK: 0x7FF0000000000000
 
 float  F  = 1e-19f;
 double D  = 2e32;
 float  F2 = 01e-19f;
 double D2 = 02e32;
+float  F3 = 0xFp100000000000000000000F;
