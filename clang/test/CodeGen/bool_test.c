7e96295fb55b80486fa24b1c921602636f2ca722
Roman Divacky <rdivacky@freebsd.org>
2011-01-07 19:01:39 +0000

    Use -check-prefix to make it clear the test is SVR4/DARWINPPC specific.
    llvm-svn: 123005

--- clang/test/CodeGen/bool_test.c
+++ clang/test/CodeGen/bool_test.c
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -triple powerpc-apple-darwin -emit-llvm -o - %s| FileCheck %s
+// RUN: %clang_cc1 -triple powerpc-apple-darwin -emit-llvm -o - %s| FileCheck -check-prefix=DARWINPPC-CHECK %s
 
 int boolsize = sizeof(_Bool);
-//CHECK: boolsize = global i32 4, align 4
+//DARWINPPC-CHECK: boolsize = global i32 4, align 4
 
--- clang/test/CodeGen/va_list_test.c
+++ clang/test/CodeGen/va_list_test.c
@@ -1,6 +1,6 @@
-// RUN: %clang_cc1 -triple powerpc-unknown-freebsd -emit-llvm -o - %s| FileCheck %s
+// RUN: %clang_cc1 -triple powerpc-unknown-freebsd -emit-llvm -o - %s| FileCheck -check-prefix=SVR4-CHECK %s
 
 #include <stdarg.h>
 
 int va_list_size = sizeof(va_list);
-// CHECK: va_list_size = global i32 12, align 4
+// SVR4-CHECK: va_list_size = global i32 12, align 4
