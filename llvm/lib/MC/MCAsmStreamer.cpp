e2bea1c82e2c281e59912854f0957597fbf8e513
Anton Korobeynikov <asl@math.spbu.ru>
2011-01-14 21:57:39 +0000

    Use common style for .cfi directives
    llvm-svn: 123472

--- llvm/lib/MC/MCAsmStreamer.cpp
+++ llvm/lib/MC/MCAsmStreamer.cpp
@@ -712,7 +712,7 @@ bool MCAsmStreamer::EmitCFIStartProc() {
   if (this->MCStreamer::EmitCFIStartProc())
     return true;
 
-  OS << ".cfi_startproc";
+  OS << "\t.cfi_startproc";
   EmitEOL();
 
   return false;
@@ -722,7 +722,7 @@ bool MCAsmStreamer::EmitCFIEndProc() {
   if (this->MCStreamer::EmitCFIEndProc())
     return true;
 
-  OS << ".cfi_endproc";
+  OS << "\t.cfi_endproc";
   EmitEOL();
 
   return false;
@@ -732,7 +732,7 @@ bool MCAsmStreamer::EmitCFIDefCfaOffset(int64_t Offset) {
   if (this->MCStreamer::EmitCFIDefCfaOffset(Offset))
     return true;
 
-  OS << ".cfi_def_cfa_offset " << Offset;
+  OS << "\t.cfi_def_cfa_offset " << Offset;
   EmitEOL();
 
   return false;
@@ -742,7 +742,7 @@ bool MCAsmStreamer::EmitCFIDefCfaRegister(int64_t Register) {
   if (this->MCStreamer::EmitCFIDefCfaRegister(Register))
     return true;
 
-  OS << ".cfi_def_cfa_register " << Register;
+  OS << "\t.cfi_def_cfa_register " << Register;
   EmitEOL();
 
   return false;
@@ -752,7 +752,7 @@ bool MCAsmStreamer::EmitCFIOffset(int64_t Register, int64_t Offset) {
   if (this->MCStreamer::EmitCFIOffset(Register, Offset))
     return true;
 
-  OS << ".cfi_offset " << Register << ", " << Offset;
+  OS << "\t.cfi_offset " << Register << ", " << Offset;
   EmitEOL();
 
   return false;
@@ -763,7 +763,7 @@ bool MCAsmStreamer::EmitCFIPersonality(const MCSymbol *Sym,
   if (this->MCStreamer::EmitCFIPersonality(Sym, Encoding))
     return true;
 
-  OS << ".cfi_personality " << Encoding << ", " << *Sym;
+  OS << "\t.cfi_personality " << Encoding << ", " << *Sym;
   EmitEOL();
 
   return false;
@@ -773,7 +773,7 @@ bool MCAsmStreamer::EmitCFILsda(const MCSymbol *Sym, unsigned Encoding) {
   if (this->MCStreamer::EmitCFILsda(Sym, Encoding))
     return true;
 
-  OS << ".cfi_lsda " << Encoding << ", " << *Sym;
+  OS << "\t.cfi_lsda " << Encoding << ", " << *Sym;
   EmitEOL();
 
   return false;
