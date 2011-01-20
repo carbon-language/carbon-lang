d802266c6b148ef762d5c8752ea2a989f2114884
Anders Carlsson <andersca@mac.com>
2011-01-20 03:41:12 +0000

    Add more parser tests for the override control keywords.
    llvm-svn: 123875

--- clang/test/Parser/cxx0x-override-control-keywords.cpp
+++ clang/test/Parser/cxx0x-override-control-keywords.cpp
@@ -3,4 +3,11 @@ struct S {
   virtual void final() final;
   virtual void override() override;
   virtual void n() new;
+  int i : 3 new;
+  int j new;
+};
+
+struct T {
+  // virt-specifier-seq is only valid in member-declarators, and a function definition is not a member-declarator.
+  virtual void f() const override { } // expected-error {{expected ';' at end of declaration list}}
 };
