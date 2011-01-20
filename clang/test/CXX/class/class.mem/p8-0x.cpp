725987b034a6cf3a6273f35c8155ff1f8add18e4
Anders Carlsson <andersca@mac.com>
2011-01-20 04:38:09 +0000

    Change the parser error to reflect that virt-specifiers are allowed on any class member.
    llvm-svn: 123883

--- clang/test/CXX/class/class.mem/p8-0x.cpp
+++ clang/test/CXX/class/class.mem/p8-0x.cpp
@@ -1,8 +1,8 @@
 // RUN: %clang_cc1 -fsyntax-only -std=c++0x -verify %s 
 struct A {
-  virtual void f() new new; // expected-error {{member function already marked 'new'}}
-  virtual void g() override override; // expected-error {{member function already marked 'override'}}
-  virtual void h() final final; // expected-error {{member function already marked 'final'}}
+  virtual void f() new new; // expected-error {{class member already marked 'new'}}
+  virtual void g() override override; // expected-error {{class member already marked 'override'}}
+  virtual void h() final final; // expected-error {{class member already marked 'final'}}
 };
 
 struct B {
