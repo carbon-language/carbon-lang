c5efca47fc1944bae13fa54da1873548e9a4710e
Rafael Espindola <rafael.espindola@gmail.com>
2011-01-23 18:50:12 +0000

    Initialize MCNoExecStack.
    llvm-svn: 124079

--- llvm/lib/Target/TargetMachine.cpp
+++ llvm/lib/Target/TargetMachine.cpp
@@ -220,6 +220,7 @@ FunctionSections("ffunction-sections",
 TargetMachine::TargetMachine(const Target &T) 
   : TheTarget(T), AsmInfo(0),
     MCRelaxAll(false),
+    MCNoExecStack(false),
     MCUseLoc(true) {
   // Typically it will be subtargets that will adjust FloatABIType from Default
   // to Soft or Hard.
