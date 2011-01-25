389eb9b54ae0e151e393c2e5f23c40cc5baaa8a8
Howard Hinnant <hhinnant@apple.com>
2011-01-25 16:32:04 +0000

    placeholder test
    llvm-svn: 124193

---
+++ libcxx/test/utilities/meta/meta.trans/meta.trans.other/aligned_union.pass.cpp
@@ -0,0 +1,19 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// type_traits
+
+// aligned_union
+
+#include <type_traits>
+
+int main()
+{
+#error aligned_union is not implemented
+}
