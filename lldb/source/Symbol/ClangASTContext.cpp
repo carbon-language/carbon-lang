5732f242ee119d4a10bff8b4a19c711df6f559b2
Greg Clayton <gclayton@apple.com>
2011-01-27 09:15:11 +0000

    Improved support for GCC complex integers.
    llvm-svn: 124371

--- lldb/source/Symbol/ClangASTContext.cpp
+++ lldb/source/Symbol/ClangASTContext.cpp
@@ -688,7 +688,7 @@ ClangASTContext::GetBuiltinTypeForDWARFEncodingAndBitSize (const char *type_name
 
         case DW_ATE_lo_user:
             // This has been seen to mean DW_AT_complex_integer
-            if (strcmp(type_name, "complex") == 0)
+            if (::strstr(type_name, "complex"))
             {
                 clang_type_t complex_int_clang_type = GetBuiltinTypeForDWARFEncodingAndBitSize ("int", DW_ATE_signed, bit_size/2);
                 return ast->getComplexType (QualType::getFromOpaquePtr(complex_int_clang_type)).getAsOpaquePtr();
