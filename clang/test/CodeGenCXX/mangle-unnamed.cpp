9361be98d59f607df556c9be62045af103ab7e44
John McCall <rjmccall@apple.com>
2011-02-01 08:30:38 +0000

    Make that test case actually test something, and add another test.
    llvm-svn: 124654

--- clang/test/CodeGenCXX/mangle-unnamed.cpp
+++ clang/test/CodeGenCXX/mangle-unnamed.cpp
@@ -75,8 +75,18 @@ int f7() {
 enum { T8 };
 template <class T> struct Test8 {
   typedef T type;
-  // define internal void @"_ZN5Test8I3$_2EC1ES0_"(
-  Test8(type t) {}
+  Test8(type t) {} // tested later
 };
 template <class T> void make_test8(T value) { Test8<T> t(value); }
 void test8() { make_test8(T8); }
+
+// CHECK: define internal void @"_ZNV3$_35test9Ev"(
+typedef volatile struct {
+  void test9() volatile {}
+} Test9;
+void test9() {
+  Test9 a;
+  a.test9();
+}
+
+// CHECK: define internal void @"_ZN5Test8I3$_2EC1ES0_"(
