fe719128e9a1f05b456261250f91aa0b8de47aad
Nick Lewycky <nicholas@mxc.ca>
2011-02-02 07:17:02 +0000

    Fix test by fully specifying the platform.
    llvm-svn: 124719

--- clang/test/CodeGen/frame-pointer-elim.c
+++ clang/test/CodeGen/frame-pointer-elim.c
@@ -1,13 +1,22 @@
-// RUN: %clang -ccc-host-triple i386 -S -o - %s | \
-// RUN:   FileCheck --check-prefix=DEFAULT %s
-// DEFAULT: f0:
-// DEFAULT: pushl %ebp
-// DEFAULT: ret
-// DEFAULT: f1:
-// DEFAULT: pushl %ebp
-// DEFAULT: ret
+// RUN: %clang -ccc-host-triple i386-apple-darwin -S -o - %s | \
+// RUN:   FileCheck --check-prefix=DARWIN %s
+// DARWIN: f0:
+// DARWIN: pushl %ebp
+// DARWIN: ret
+// DARWIN: f1:
+// DARWIN: pushl %ebp
+// DARWIN: ret
 
-// RUN: %clang -ccc-host-triple i386 -S -o - -fomit-frame-pointer %s | \
+// RUN: %clang -ccc-host-triple i386-pc-linux-gnu -S -o - %s | \
+// RUN:   FileCheck --check-prefix=LINUX %s
+// LINUX: f0:
+// LINUX-NOT: pushl %ebp
+// LINUX: ret
+// LINUX: f1:
+// LINUX: pushl %ebp
+// LINUX: ret
+
+// RUN: %clang -ccc-host-triple i386-darwin -S -o - -fomit-frame-pointer %s | \
 // RUN:   FileCheck --check-prefix=OMIT_ALL %s
 // OMIT_ALL: f0:
 // OMIT_ALL-NOT: pushl %ebp
@@ -16,7 +25,7 @@
 // OMIT_ALL-NOT: pushl %ebp
 // OMIT_ALL: ret
 
-// RUN: %clang -ccc-host-triple i386 -S -o - -momit-leaf-frame-pointer %s | \
+// RUN: %clang -ccc-host-triple i386-darwin -S -o - -momit-leaf-frame-pointer %s | \
 // RUN:   FileCheck --check-prefix=OMIT_LEAF %s
 // OMIT_LEAF: f0:
 // OMIT_LEAF-NOT: pushl %ebp
