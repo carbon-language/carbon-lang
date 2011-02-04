768f2809f21469df9bf6620acdd85571d4c92fce
Greg Clayton <gclayton@apple.com>
2011-02-04 19:17:57 +0000

    Added the start of platform configuration designed for internal LLDB use. Internal use means for compiling the LLDB debug engine and plug-ins, but it should never make it into the public API.
    Since we don't currently have a configuration script that detects avaiable
    functionality in the LLDB build system, we are hard coding #define values
    in the host specific "Config.h" files. 
    
    #define values in these Config.h header files should set the value to zero or
    one:
    
    #define LLDB_CONFIG_TERMIOS_SUPPORTED 1
    
    #define LLDB_CONFIG_OTHER 0
    
    Then any code in the LLDB engine should check the availability using:
    
    #if LLDB_CONFIG_TERMIOS_SUPPORTED
    ....
    #endif
    
    Eventually the contents of the host specific Config.h files will be auto
    generated, but for now they will be hard coded. Any LLDB_CONFIG_XXXX items
    that are added should be added to all Config.h files and set to either zero
    or one.
    
    llvm-svn: 124892

---
+++ lldb/include/lldb/Host/Config.h
@@ -0,0 +1,35 @@
+//===-- Config.h ------------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef liblldb_Config_h_
+#define liblldb_Config_h_
+
+#if defined(__APPLE__)
+
+#include "lldb/Host/macosx/Config.h"
+
+#elif defined(__linux__)
+
+#include "lldb/Host/linux/Config.h"
+
+#elif defined(__FreeBSD__)
+
+#include "lldb/Host/freebsd/Config.h"
+
+#elif defined(__MINGW__) || defined (__MINGW32__)
+
+#include "lldb/Host/mingw/Config.h"
+
+#else
+
+#error undefined platform
+
+#endif
+
+#endif // #ifndef liblldb_Config_h_
