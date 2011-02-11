25cb63c791a9c3652bfe2079be362aaec614c5ba
Cameron Zwarich <zwarich@apple.com>
2011-02-11 06:08:25 +0000

    LoopInstSimplify preserves ScalarEvolution.
    llvm-svn: 125368

--- llvm/lib/Transforms/Scalar/LoopInstSimplify.cpp
+++ llvm/lib/Transforms/Scalar/LoopInstSimplify.cpp
@@ -42,6 +42,7 @@ namespace {
       AU.addRequiredID(LoopSimplifyID);
       AU.addPreservedID(LoopSimplifyID);
       AU.addPreservedID(LCSSAID);
+      AU.addPreserved("scalar-evolution");
     }
   };
 }
