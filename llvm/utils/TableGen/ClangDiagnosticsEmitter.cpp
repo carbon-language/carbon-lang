9f5055f037c927a0b8b3d44fbebf0c2d0adb6962
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-02-13 07:51:19 +0000

    Wrap the struct in an anonymous namespace.
    llvm-svn: 125452

--- llvm/utils/TableGen/ClangDiagnosticsEmitter.cpp
+++ llvm/utils/TableGen/ClangDiagnosticsEmitter.cpp
@@ -187,11 +187,13 @@ void ClangDiagsDefsEmitter::run(raw_ostream &OS) {
 // Warning Group Tables generation
 //===----------------------------------------------------------------------===//
 
+namespace {
 struct GroupInfo {
   std::vector<const Record*> DiagsInGroup;
   std::vector<std::string> SubGroups;
   unsigned IDNo;
 };
+} // end anonymous namespace.
 
 void ClangDiagGroupsEmitter::run(raw_ostream &OS) {
   // Compute a mapping from a DiagGroup to all of its parents.
