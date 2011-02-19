3b1b3fe03915205af76cff164c453ab4ec6359d4
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-02-19 08:03:21 +0000

    [analyzer] '-analyzer-check-objc-mem' can go through the llvm/clang codebase without crashing; enable it for C++.
    llvm-svn: 126026

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -938,14 +938,11 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
         CmdArgs.push_back("-analyzer-checker=cocoa");
       }
 
-      // Checks to perform for all languages *except* C++.
-      if (!types::isCXX(InputType)) {
-        // NOTE: Leaving -analyzer-check-objc-mem here is intentional.
-        // It also checks C code.
-        CmdArgs.push_back("-analyzer-check-objc-mem");
+      // NOTE: Leaving -analyzer-check-objc-mem here is intentional.
+      // It also checks C code.
+      CmdArgs.push_back("-analyzer-check-objc-mem");
 
-        CmdArgs.push_back("-analyzer-eagerly-assume");
-      }
+      CmdArgs.push_back("-analyzer-eagerly-assume");
     }
 
     // Set the output format. The default is plist, for (lame) historical
