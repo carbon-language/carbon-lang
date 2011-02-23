b7edc188e208ac5481e305c4910b9ff2c134bf6b
Matt Beaumont-Gay <matthewbg@google.com>
2011-02-23 19:40:41 +0000

    fix typo in *all* of the test cases
    llvm-svn: 126328

--- clang/test/SemaObjC/dist-object-modifiers.m
+++ clang/test/SemaObjC/dist-object-modifiers.m
@@ -17,5 +17,5 @@
 - (void)doStuff:(id)clientId { } // expected-warning {{conflicting distributed object modifiers on parameter type in implementation of 'doStuff:'}}
 - (bycopy id)Ok { return 0; }
 + (id) stillMore : (id)Arg  : (bycopy id)Arg1 { return Arg; } // expected-warning {{conflicting distributed object modifiers on return type in implementation of 'stillMore::'}} \
-                                                              // expected-warning 2{{conflicting distributed object modifiers on patameter type in implementation of 'stillMore::'}}
+                                                              // expected-warning 2{{conflicting distributed object modifiers on parameter type in implementation of 'stillMore::'}}
 @end
