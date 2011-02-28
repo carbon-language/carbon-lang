1401dbd211817db037a10d0f4bc02a29c737948e
Owen Anderson <resistor@mac.com>
2011-02-28 19:58:06 +0000

    Fix warning when building with clang++.
    llvm-svn: 126679

--- llvm/include/llvm/Target/TargetLowering.h
+++ llvm/include/llvm/Target/TargetLowering.h
@@ -1260,7 +1260,7 @@ public:
   }
 
   /// HandleByVal - Target-specific cleanup for formal ByVal parameters.
-  virtual void HandleByVal(CCState *) const {};
+  virtual void HandleByVal(CCState *) const {}
 
   /// CanLowerReturn - This hook should be implemented to check whether the
   /// return values described by the Outs array can fit into the return
