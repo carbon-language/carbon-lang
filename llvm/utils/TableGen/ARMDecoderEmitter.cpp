6f6d739b6ee627cc194930f528ddd1d62dde6450
Bob Wilson <bob.wilson@apple.com>
2011-03-03 07:19:52 +0000

    TableGen should not ignore BX instructions for the ARM disassembler. pr9368.
    llvm-svn: 126931

--- llvm/utils/TableGen/ARMDecoderEmitter.cpp
+++ llvm/utils/TableGen/ARMDecoderEmitter.cpp
@@ -1631,7 +1631,7 @@ ARMDEBackend::populateInstruction(const CodeGenInstruction &CGI,
     if (Name == "B") return false;
 
     // Ignore the non-Darwin BL instructions and the TPsoft (TLS) instruction.
-    if (Name == "BL" || Name == "BL_pred" || Name == "BLX" || Name == "BX" ||
+    if (Name == "BL" || Name == "BL_pred" || Name == "BLX" ||
         Name == "BLX_pred" || Name == "TPsoft")
       return false;
 
