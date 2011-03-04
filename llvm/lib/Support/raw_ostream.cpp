acf08420882298c89b1b657e698b5a93b6dcc85c
Benjamin Kramer <benny.kra@googlemail.com>
2011-03-04 18:18:16 +0000

    raw_ostream: If writing a string that is larger than the buffer, write it directly instead of doing many buffer-sized writes.
    This caps the number of write(2) calls per string to a maximum of 2.
    
    llvm-svn: 127010

--- llvm/lib/Support/raw_ostream.cpp
+++ llvm/lib/Support/raw_ostream.cpp
@@ -265,15 +265,19 @@ raw_ostream &raw_ostream::write(const char *Ptr, size_t Size) {
       return write(Ptr, Size);
     }
 
-    // Write out the data in buffer-sized blocks until the remainder
-    // fits within the buffer.
-    do {
-      size_t NumBytes = OutBufEnd - OutBufCur;
-      copy_to_buffer(Ptr, NumBytes);
-      flush_nonempty();
-      Ptr += NumBytes;
-      Size -= NumBytes;
-    } while (OutBufCur+Size > OutBufEnd);
+    // If the buffer is empty at this point we have a string that is larger
+    // than the buffer. It's better to write it unbuffered in this case.
+    if (BUILTIN_EXPECT(OutBufCur == OutBufStart, false)) {
+      write_impl(Ptr, Size);
+      return *this;
+    }
+
+    // We don't have enough space in the buffer to fit the string in. Insert as
+    // much as possible, flush and start over with the remainder.
+    size_t NumBytes = OutBufEnd - OutBufCur;
+    copy_to_buffer(Ptr, NumBytes);
+    flush_nonempty();
+    return write(Ptr + NumBytes, Size - NumBytes);
   }
 
   copy_to_buffer(Ptr, Size);
