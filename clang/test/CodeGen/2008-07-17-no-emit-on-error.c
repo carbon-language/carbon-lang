98835748e35a0dc43a57ca48213958dd13860407
NAKAMURA Takumi <geek4civic@gmail.com>
2011-03-05 11:15:55 +0000

    test/CodeGen/2008-07-17-no-emit-on-error.c: Use LLVM's opt instead of "test(1)" for Mingw MSYS.
    Mingw MSYS does not have test.exe (only shell built-in test), and is not capable to invoke test(1) from utils/not.
    
    llvm-svn: 127078

--- clang/test/CodeGen/2008-07-17-no-emit-on-error.c
+++ clang/test/CodeGen/2008-07-17-no-emit-on-error.c
@@ -1,9 +1,9 @@
 // RUN: rm -f %t1.bc
 // RUN: %clang_cc1 -DPASS %s -emit-llvm-bc -o %t1.bc
-// RUN: test -f %t1.bc
+// RUN: opt %t1.bc -disable-output
 // RUN: rm -f %t1.bc
 // RUN: not %clang_cc1 %s -emit-llvm-bc -o %t1.bc
-// RUN: not test -f %t1.bc
+// RUN: not opt %t1.bc -disable-output
 
 void f() {
 }
