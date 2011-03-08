c7b48d40d4629251858566070372aa1aeb72b957
Chris Lattner <sabre@nondot.org>
2011-03-08 16:59:03 +0000

    fix incorrect comment.
    llvm-svn: 127252

--- llvm/include/llvm/Value.h
+++ llvm/include/llvm/Value.h
@@ -51,8 +51,8 @@ class MDNode;
 /// This is a very important LLVM class. It is the base class of all values 
 /// computed by a program that may be used as operands to other values. Value is
 /// the super class of other important classes such as Instruction and Function.
-/// All Values have a Type. Type is not a subclass of Value. All types can have
-/// a name and they should belong to some Module. Setting the name on the Value
+/// All Values have a Type. Type is not a subclass of Value. Some values can
+/// have a name and they belong to some Module.  Setting the name on the Value
 /// automatically updates the module's symbol table.
 ///
 /// Every value has a "use list" that keeps track of which other Values are
