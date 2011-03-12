a2cb77f54eb4eefb761f719aa04071f672d32b9b
Sebastian Redl <sebastian.redl@getdesigned.at>
2011-03-12 13:53:47 +0000

    Disable delegating constructors for 2.9
    llvm-svn: 127543

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -1532,7 +1532,10 @@ Sema::BuildDelegatingInitializer(TypeSourceInfo *TInfo,
   if (!LangOpts.CPlusPlus0x)
     return Diag(Loc, diag::err_delegation_0x_only)
       << TInfo->getTypeLoc().getLocalSourceRange();
-
+  // Disable for 2.9.
+  return Diag(Loc, diag::err_delegation_unimplemented)
+    << TInfo->getTypeLoc().getLocalSourceRange();
+#if 0
   // Initialize the object.
   InitializedEntity DelegationEntity = InitializedEntity::InitializeDelegation(
                                      QualType(ClassDecl->getTypeForDecl(), 0));
@@ -1579,6 +1582,7 @@ Sema::BuildDelegatingInitializer(TypeSourceInfo *TInfo,
   return new (Context) CXXCtorInitializer(Context, Loc, LParenLoc, Constructor,
                                           DelegationInit.takeAs<Expr>(),
                                           RParenLoc);
+#endif
 }
 
 MemInitResult
