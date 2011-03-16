c9998aa1f4d71bc71360fdc0eaf0a2f62f2d025e
NAKAMURA Takumi <geek4civic@gmail.com>
2011-03-16 13:51:46 +0000

    test/Driver/hello.c: Tweak for cygming.
      - Driver's name may be "clang(-\d.\d).exe".
      - Emitted executable file is named as "%t.exe". It must be harmless on other OS.
    
    llvm-svn: 127729

--- clang/test/Driver/hello.c
+++ clang/test/Driver/hello.c
@@ -1,9 +1,9 @@
-// RUN: %clang -ccc-echo -o %t %s 2> %t.log
+// RUN: %clang -ccc-echo -o %t.exe %s 2> %t.log
 
 // Make sure we used clang.
-// RUN: grep 'clang\(-[0-9.]\+\)\?" -cc1 .*hello.c' %t.log
+// RUN: grep 'clang\(-[0-9.]\+\)\?\(\.[Ee][Xx][Ee]\)\?" -cc1 .*hello.c' %t.log
 
-// RUN: %t > %t.out
+// RUN: %t.exe > %t.out
 // RUN: grep "I'm a little driver, short and stout." %t.out
 
 // FIXME: We don't have a usable assembler on Windows, so we can't build real
