f5606922f8b3500b7e97125dbce3a87748c9a481
NAKAMURA Takumi <geek4civic@gmail.com>
2011-03-18 03:21:33 +0000

    test/Driver/sysroot-flags.c: Tweak for Mingw MSYS bash.
    MSYS substitutes path (that begins with "/") to de-mounted Windows path, even if it were nonexistent.
    "/foo/bar" might be substituted to "C:/msysgit/foo/bar".
    
    llvm-svn: 127861

--- clang/test/Driver/sysroot-flags.c
+++ clang/test/Driver/sysroot-flags.c
@@ -2,27 +2,27 @@
 
 // RUN: %clang -### -fsyntax-only -isysroot /foo/bar %s 2>&1 | \
 // RUN:   FileCheck %s -check-prefix=ISYSROOT
-// ISYSROOT: "-isysroot" "/foo/bar"
+// ISYSROOT: "-isysroot" "{{[^"]*}}/foo/bar"
 
 // Check that we get both isysroot for headers, and pass --sysroot on to GCC to
 // produce the final binary.
 // RUN: %clang -### -ccc-host-triple x86_64-unknown-linux-gnu \
 // RUN:   --sysroot=/foo/bar -o /dev/null %s 2>&1 | \
 // RUN:   FileCheck %s -check-prefix=SYSROOT_EQ
-// SYSROOT_EQ: "-isysroot" "/foo/bar"
-// SYSROOT_EQ: "--sysroot{{" "|=}}/foo/bar"
+// SYSROOT_EQ: "-isysroot" "{{[^"]*}}/foo/bar"
+// SYSROOT_EQ: "--sysroot{{" "|=}}{{[^"]*}}/foo/bar"
 
 // Check for overriding the header sysroot by providing both --sysroot and
 // -isysroot.
 // RUN: %clang -### -ccc-host-triple x86_64-unknown-linux-gnu -isysroot /baz \
 // RUN:   --sysroot=/foo/bar -o /dev/null %s 2>&1 | FileCheck %s \
 // RUN:   -check-prefix=ISYSROOT_AND_SYSROOT
-// ISYSROOT_AND_SYSROOT: "-isysroot" "/baz"
-// ISYSROOT_AND_SYSROOT: "--sysroot{{" "|=}}/foo/bar"
+// ISYSROOT_AND_SYSROOT: "-isysroot" "{{[^"]*}}/baz"
+// ISYSROOT_AND_SYSROOT: "--sysroot{{" "|=}}{{[^"]*}}/foo/bar"
 
 // Check that omitting the equals works as well.
 // RUN: %clang -### -ccc-host-triple x86_64-unknown-linux-gnu \
 // RUN:   --sysroot /foo/bar -o /dev/null %s 2>&1 | \
 // RUN:   FileCheck %s -check-prefix=SYSROOT_SEPARATE
-// SYSROOT_SEPARATE: "-isysroot" "/foo/bar"
-// SYSROOT_SEPARATE: "--sysroot{{" "|=}}/foo/bar"
+// SYSROOT_SEPARATE: "-isysroot" "{{[^"]*}}/foo/bar"
+// SYSROOT_SEPARATE: "--sysroot{{" "|=}}{{[^"]*}}/foo/bar"
