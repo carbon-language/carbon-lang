9cd67961043c7f490719deff5a27c3ee732c6da7
Devang Patel <dpatel@apple.com>
2011-03-19 01:40:43 +0000

    Test case for r127940.
    llvm-svn: 127941

---
+++ llvm/test/FrontendC/2011-03-18-StructReturn.c
@@ -0,0 +1,17 @@
+// RUN: %llvmgcc %s -S -O0 -o - | FileCheck %s
+// Radar 9156771
+typedef struct RGBColor {
+  unsigned short red;
+  unsigned short green;
+  unsigned short blue;
+} RGBColor;
+
+RGBColor func();
+
+RGBColor X;
+void foo() {
+//CHECK: store i48
+  X = func();
+}
+
+
