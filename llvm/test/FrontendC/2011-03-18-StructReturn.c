dddce99f028ef7e005ac69a20756f6a8c73eb664
Devang Patel <dpatel@apple.com>
2011-03-21 23:11:08 +0000

    Try again to make this test darwin only.
    llvm-svn: 128036

--- llvm/test/FrontendC/2011-03-18-StructReturn.c
+++ llvm/test/FrontendC/2011-03-18-StructReturn.c
@@ -1,5 +1,6 @@
 // RUN: %llvmgcc %s -m64 -S -O0 -o - | FileCheck %s
-// XTARGET: x86_64-apple-darwin
+// XFAIL: *
+// XTARGET: darwin
 // Radar 9156771
 typedef struct RGBColor {
   unsigned short red;
