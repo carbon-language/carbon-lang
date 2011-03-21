9746286beca2539438e0a6b783e106bc359036ca
Owen Anderson <resistor@mac.com>
2011-03-21 23:13:43 +0000

    Add support for Thumb interworking addresses for symbol offsets that get constant folded very early. This fixes SPASS with -integrated-as.  <rdar://problem/9165399>
    llvm-svn: 128037

--- llvm/lib/MC/MCExpr.cpp
+++ llvm/lib/MC/MCExpr.cpp
@@ -310,6 +310,11 @@ static void AttemptToFoldSymbolOffsetDifference(const MCAssembler *Asm,
   if (AD.getFragment() == BD.getFragment()) {
     Addend += (AD.getOffset() - BD.getOffset());
 
+    // Pointers to Thumb symbols need to have their low-bit set to allow
+    // for interworking.
+    if (Asm->isThumbFunc(&SA))
+      Addend |= 1;
+
     // Clear the symbol expr pointers to indicate we have folded these
     // operands.
     A = B = 0;
