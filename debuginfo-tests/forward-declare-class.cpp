2b54299705d296598d66da6b8539982b69c42f77
Devang Patel <dpatel@apple.com>
2011-03-23 16:30:03 +0000

    Test case for r128150.
    llvm-svn: 128151

---
+++ debuginfo-tests/forward-declare-class.cpp
@@ -0,0 +1,23 @@
+// RUN: %clangxx -O0 -g %s -c -o %t.o
+// RUN: %test_debuginfo %s %t.o
+// Radar 9168773
+
+// DEBUGGER: ptype A
+// CHECK: type = struct A {
+// CHECK-NEXT: int MyData;
+// CHECK-NEXT: }
+class A;
+class B {
+public:
+  void foo(const A *p);
+};
+
+B iEntry;
+
+class A {
+public:
+  int MyData;
+};
+
+A irp;
+
