7c2b338a7ef4c653e01f71552640446bd20af599
Duncan Sands <baldrick@free.fr>
2011-03-31 10:03:32 +0000

    Will not compile without the spec!
    llvm-svn: 128623

--- llvm/test/FrontendAda/real_cst.adb
+++ llvm/test/FrontendAda/real_cst.adb
@@ -1,4 +1,4 @@
--- RUN: %llvmgcc -S -O2 -gnatn %s
+-- RUN: %llvmgcc -S -O2 -gnatn %s -I%p/Support
 package body Real_Cst is
    Cst : constant Float := 0.0;
    procedure Write (Stream : access Ada.Streams.Root_Stream_Type'Class) is
