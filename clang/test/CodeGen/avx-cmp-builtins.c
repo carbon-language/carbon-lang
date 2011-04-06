3cacdf5dbb0d66242edef973ec2d907176b59725
John McCall <rjmccall@apple.com>
2011-04-06 04:29:52 +0000

    Anonymify this test.
    llvm-svn: 128987

--- clang/test/CodeGen/avx-cmp-builtins.c
+++ clang/test/CodeGen/avx-cmp-builtins.c
@@ -8,40 +8,40 @@ void test_cmp_ps256() {
     __m256 a, b, c;
     a = _mm256_cmp_ps(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_ps256
-    // CHECK: %0 = call <8 x float> @llvm.x86.avx.cmp.ps.256(<8 x float> %tmp, <8 x float> %tmp1, i8 13)
+    // CHECK: call <8 x float> @llvm.x86.avx.cmp.ps.256(<8 x float> {{%.*}}, <8 x float> {{%.*}}, i8 13)
 }
 
 void test_cmp_pd256() {
     __m256d a, b, c;
     a = _mm256_cmp_pd(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_pd256
-    // CHECK: %0 = call <4 x double> @llvm.x86.avx.cmp.pd.256(<4 x double> %tmp, <4 x double> %tmp1, i8 13)
+    // CHECK: call <4 x double> @llvm.x86.avx.cmp.pd.256(<4 x double> {{%.*}}, <4 x double> {{%.*}}, i8 13)
 }
 
 void test_cmp_ps() {
     __m128 a, b, c;
     a = _mm_cmp_ps(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_ps
-    // CHECK: %cmpps = call <4 x float> @llvm.x86.sse.cmp.ps(<4 x float> %tmp, <4 x float> %tmp1, i8 13)
+    // CHECK: call <4 x float> @llvm.x86.sse.cmp.ps(<4 x float> {{%.*}}, <4 x float> {{%.*}}, i8 13)
 }
 
 void test_cmp_pd() {
     __m128d a, b, c;
     a = _mm_cmp_pd(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_pd
-    // CHECK: %cmppd = call <2 x double> @llvm.x86.sse2.cmp.pd(<2 x double> %tmp, <2 x double> %tmp1, i8 13)
+    // CHECK: call <2 x double> @llvm.x86.sse2.cmp.pd(<2 x double> {{%.*}}, <2 x double> {{%.*}}, i8 13)
 }
 
 void test_cmp_sd() {
     __m128d a, b, c;
     a = _mm_cmp_sd(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_sd
-    // CHECK: %cmpsd = call <2 x double> @llvm.x86.sse2.cmp.sd(<2 x double> %tmp, <2 x double> %tmp1, i8 13)
+    // CHECK: call <2 x double> @llvm.x86.sse2.cmp.sd(<2 x double> {{%.*}}, <2 x double> {{%.*}}, i8 13)
 }
 
 void test_cmp_ss() {
     __m128 a, b, c;
     a = _mm_cmp_ss(b, c, _CMP_GE_OS);
     // CHECK: @test_cmp_ss
-    // CHECK: %cmpss = call <4 x float> @llvm.x86.sse.cmp.ss(<4 x float> %tmp, <4 x float> %tmp1, i8 13)
+    // CHECK: call <4 x float> @llvm.x86.sse.cmp.ss(<4 x float> {{%.*}}, <4 x float> {{%.*}}, i8 13)
 }
