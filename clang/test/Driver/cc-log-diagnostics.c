2f8d10e8d7950bf832ca0028927a5fd30de55d0b
Daniel Dunbar <daniel@zuster.org>
2011-04-07 19:05:06 +0000

    Fronted/CC_LOG_DIAGNOSTICS: Add test case.
    llvm-svn: 129097

---
+++ clang/test/Driver/cc-log-diagnostics.c
@@ -0,0 +1,29 @@
+// RUN: env RC_DEBUG_OPTIONS=1 \
+// RUN:     CC_LOG_DIAGNOSTICS=1 \
+// RUN:     CC_LOG_DIAGNOSTICS_FILE=%t.log \
+// RUN: %clang -ccc-host-triple x86_64-apple-darwin10 -fsyntax-only %s
+// RUN: FileCheck %s < %t.log
+
+int f0() {}
+
+// CHECK: <dict>
+// CHECK:   <key>main-file</key>
+// CHECK:   <string>{{.*}}cc-log-diagnostics.c</string>
+// CHECK:   <key>dwarf-debug-flags</key>
+// CHECK:   <string>{{.*}}clang{{.*}}-fsyntax-only{{.*}}</string>
+// CHECK:   <key>diagnostics</key>
+// CHECK:   <array>
+// CHECK:     <dict>
+// CHECK:       <key>level</key>
+// CHECK:       <string>warning</string>
+// CHECK:       <key>filename</key>
+// CHECK:       <string>{{.*}}cc-log-diagnostics.c</string>
+// CHECK:       <key>line</key>
+// CHECK:       <integer>7</integer>
+// CHECK:       <key>column</key>
+// CHECK:       <integer>11</integer>
+// CHECK:       <key>message</key>
+// CHECK:       <string>control reaches end of non-void function</string>
+// CHECK:     </dict>
+// CHECK:   </array>
+// CHECK: </dict>
