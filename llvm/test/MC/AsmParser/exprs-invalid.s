0a6895960471be0ab3afc2cc61c6219236ebda62
Eric Christopher <echristo@apple.com>
2011-04-12 00:03:38 +0000

    Test for invalid constant expr addition - bad octal constant.
    llvm-svn: 129323

--- llvm/test/MC/AsmParser/exprs-invalid.s
+++ llvm/test/MC/AsmParser/exprs-invalid.s
@@ -1,13 +1,5 @@
-// RUN: not llvm-mc -triple i386-unknown-unknown %s 2> %t
-// RUN: FileCheck -input-file %t %s
-
-// Currently XFAIL'ed, since the front-end isn't validating this. Figure out the
-// right resolution.
-//
-// XFAIL: *
-
-        .text
-a:
-        .data
-// CHECK: expected relocatable expression
-        .long -(0 + a)
+// RUN: not llvm-mc -triple x86_64-apple-darwin10 %s 2> %t.err | FileCheck %s
+// RUN: FileCheck --check-prefix=CHECK-ERRORS %s < %t.err
+// CHECK: 	.section	__TEXT,__text,regular,pure_instructions
+// CHECK-ERRORS: error: Invalid octal number
+.long 80+08
