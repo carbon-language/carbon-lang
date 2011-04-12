f31fac471254c114fb7f756be474ac7eadffd236
John McCall <rjmccall@apple.com>
2011-04-12 00:59:32 +0000

    Formatting.
    llvm-svn: 129336

--- clang/test/CodeGenCXX/anonymous-union-member-initializer.cpp
+++ clang/test/CodeGenCXX/anonymous-union-member-initializer.cpp
@@ -106,14 +106,11 @@ struct S {
 } s;
 
 
- //PR8760 
- template <typename T>
-  struct Foo {
-    Foo() : ptr(__nullptr) {}
-    union {
-      T *ptr;
-   };
+//PR8760 
+template <typename T> struct Foo {
+  Foo() : ptr(__nullptr) {}
+  union {
+    T *ptr;
   };
-  Foo<int> f;
-  
-  
+};
+Foo<int> f;
