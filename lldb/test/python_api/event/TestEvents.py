272ae4df7cac3fbfe8b850c9552e6e32a6867fd0
Johnny Chen <johnny.chen@apple.com>
2011-04-19 19:25:37 +0000

    Converted to use SBProcess.LaunchSimple().
    llvm-svn: 129789

--- lldb/test/python_api/event/TestEvents.py
+++ lldb/test/python_api/event/TestEvents.py
@@ -122,8 +122,7 @@ class EventAPITestCase(TestBase):
                         VALID_BREAKPOINT)
 
         # Now launch the process, and do not stop at the entry point.
-        error = lldb.SBError()
-        self.process = target.Launch (self.dbg.GetListener(), None, None, os.ctermid(), os.ctermid(), os.ctermid(), None, 0, False, error)
+        self.process = target.LaunchSimple(None, None, os.getcwd())
 
         self.process = target.GetProcess()
         self.assertTrue(self.process.GetState() == lldb.eStateStopped,
