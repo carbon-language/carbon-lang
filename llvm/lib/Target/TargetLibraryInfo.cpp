9483bb6bf33c34c1bdf509274c418226f5664a05
Daniel Dunbar <daniel@zuster.org>
2011-04-19 20:44:08 +0000

    Target: Eliminate a use of getDarwinMajorNumber().
    llvm-svn: 129803

--- llvm/lib/Target/TargetLibraryInfo.cpp
+++ llvm/lib/Target/TargetLibraryInfo.cpp
@@ -28,8 +28,15 @@ static void initialize(TargetLibraryInfo &TLI, const Triple &T) {
 
   
   // memset_pattern16 is only available on iOS 3.0 and Mac OS/X 10.5 and later.
-  if (T.getOS() != Triple::Darwin || T.getDarwinMajorNumber() < 9)
+  if (T.isOSX()) {
+    if (T.isOSXVersionLT(10, 5))
+      TLI.setUnavailable(LibFunc::memset_pattern16);
+  } else if (T.getOS() == Triple::IOS) {
+    if (T.isOSVersionLT(3, 0))
+      TLI.setUnavailable(LibFunc::memset_pattern16);
+  } else {
     TLI.setUnavailable(LibFunc::memset_pattern16);
+  }
 
   // iprintf and friends are only available on XCore.
   if (T.getArch() != Triple::xcore) {
