cd8cf861d7f2f608cf6c47961ed94e34d4777796
Johnny Chen <johnny.chen@apple.com>
2011-04-20 21:51:31 +0000

    Add a simple test scenario for 'command regex' invocation and subsequent use.
    llvm-svn: 129879

---
+++ lldb/test/command_regex/TestCommandRegex.py
@@ -0,0 +1,42 @@
+"""
+Test lldb 'commands regex' command which allows the user to create a regular expression command.
+"""
+
+import os
+import unittest2
+import lldb
+import pexpect
+from lldbtest import *
+
+class CommandRegexTestCase(TestBase):
+
+    mydir = "command_regex"
+
+    def test_command_regex(self):
+        """Test a simple scenario of 'commands regexp' invocation and subsequent use."""
+        prompt = "\(lldb\) "
+        regex_prompt = "Enter multiple regular expressions in the form s/find/replace/ then terminate with an empty line:\r\n"
+        regex_prompt1 = "\r\n"
+
+        child = pexpect.spawn('%s' % self.lldbExec)
+        # Turn on logging for what the child sends back.
+        if traceAlways:
+            child.logfile_read = sys.stdout
+
+        # Set the breakpoint, followed by the target stop-hook commands.
+        child.expect(prompt)
+        child.sendline('commands regex Help!')
+        child.expect(regex_prompt)
+        child.sendline('s/^$/help/')
+        child.expect(regex_prompt1)
+        child.sendline('')
+        # Help!
+        child.sendline('Help!')
+        # If we see the familiar 'help' output, the test is done.
+        child.expect('The following is a list of built-in, permanent debugger commands:')        
+
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
