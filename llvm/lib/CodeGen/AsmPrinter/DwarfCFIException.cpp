5c54ecc9afabce83c52788d410d1927fd8a700ac
Rafael Espindola <rafael.espindola@gmail.com>
2011-04-24 19:00:34 +0000

    Synchronize the conditions for producing a .cfi_startproc and a .cfi_endproc. Fixes PR9787.
    llvm-svn: 130115

--- llvm/lib/CodeGen/AsmPrinter/DwarfCFIException.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfCFIException.cpp
@@ -128,7 +128,7 @@ void DwarfCFIException::BeginFunction(const MachineFunction *MF) {
 void DwarfCFIException::EndFunction() {
   if (!shouldEmitMoves && !shouldEmitTable) return;
 
-  if (shouldEmitMoves)
+  if (shouldEmitMoves || shouldEmitTable)
     Asm->OutStreamer.EmitCFIEndProc();
 
   Asm->OutStreamer.EmitLabel(Asm->GetTempSymbol("eh_func_end",
