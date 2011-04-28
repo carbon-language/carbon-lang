7e92e57b0b9d9af7a24b4877b9959017deaf322b
Matt Beaumont-Gay <matthewbg@google.com>
2011-04-28 00:23:49 +0000

    Fix modified-header-crash.c for read-only source trees
    llvm-svn: 130359

--- clang/test/PCH/modified-header-crash.c
+++ clang/test/PCH/modified-header-crash.c
@@ -1,7 +1,8 @@
 // Don't crash.
 
-// RUN: %clang_cc1 -DCAKE -x c-header %S/modified-header-crash.h -emit-pch -o %t
-// RUN: touch %S/modified-header-crash.h
+// RUN: cp %S/modified-header-crash.h %t.h
+// RUN: %clang_cc1 -DCAKE -x c-header %t.h -emit-pch -o %t
+// RUN: echo >> %t.h
 // RUN: not %clang_cc1 %s -include-pch %t -fsyntax-only
 
 // FIXME: On Windows we don't detect that the header was modified ?
