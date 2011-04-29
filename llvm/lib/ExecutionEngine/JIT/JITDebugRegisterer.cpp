dd8061e09f1b82ee4b7ce4b885925718024b13ad
Douglas Gregor <dgregor@apple.com>
2011-04-29 16:57:12 +0000

    Fix a C++0x portability issue with std::make_pair. Explicitly providing template arguments no longer works when the call arguments are lvalues.
    llvm-svn: 130513

--- llvm/lib/ExecutionEngine/JIT/JITDebugRegisterer.cpp
+++ llvm/lib/ExecutionEngine/JIT/JITDebugRegisterer.cpp
@@ -142,7 +142,7 @@ void JITDebugRegisterer::RegisterFunction(const Function *F, DebugInfo &I) {
 
   // Add a mapping from F to the entry and buffer, so we can delete this
   // info later.
-  FnMap[F] = std::make_pair<std::string, jit_code_entry*>(Buffer, JITCodeEntry);
+  FnMap[F] = std::make_pair(Buffer, JITCodeEntry);
 
   // Acquire the lock and do the registration.
   {
