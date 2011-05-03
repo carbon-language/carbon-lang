85ac42339c2932a41d0c16cda5989d2e27931095
Devang Patel <dpatel@apple.com>
2011-05-03 16:19:38 +0000

    Update metadata id number in string compare check.
    llvm-svn: 130757

--- clang/test/Driver/darwin-debug-flags.c
+++ clang/test/Driver/darwin-debug-flags.c
@@ -1,7 +1,7 @@
 // RUN: env RC_DEBUG_OPTIONS=1 %clang -ccc-host-triple i386-apple-darwin9 -g -Os %s  -emit-llvm -S -o - | FileCheck %s
 // <rdar://problem/7256886>
 
-// CHECK: !1 = metadata !{
+// CHECK: !0 = metadata !{
 // CHECK: -g -Os
 // CHECK: -mmacosx-version-min=10.5.0
 // CHECK: [ DW_TAG_compile_unit ]
