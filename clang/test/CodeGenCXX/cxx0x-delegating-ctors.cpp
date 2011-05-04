f9b554bef944b721fff970f4a5262b51877ca3c0
Alexis Hunt <alercah@gmail.com>
2011-05-04 00:59:33 +0000

    Fix the delegating constructors test to not rely on basic block names.
    llvm-svn: 130813

--- clang/test/CodeGenCXX/cxx0x-delegating-ctors.cpp
+++ clang/test/CodeGenCXX/cxx0x-delegating-ctors.cpp
@@ -29,13 +29,13 @@ delegator::delegator(bool)
 // CHECK: define void @_ZN9delegatorC1Ec
 // CHECK: void @_ZN9delegatorC1Eb
 // CHECK: void @__cxa_throw
-// CHECK: lpad
+// CHECK: void @_ZSt9terminatev
 // CHECK: void @_ZN9delegatorD1Ev
 // CHECK: define void @_ZN9delegatorC2Ec
 // CHECK: void @_ZN9delegatorC2Eb
 // CHECK: void @__cxa_throw
-// CHECK: lpad
-// CHECK: invoke void @_ZN9delegatorD2Ev
+// CHECK: void @_ZSt9terminatev
+// CHECK: void @_ZN9delegatorD2Ev
 delegator::delegator(char)
   : delegator(true) {
   throw 0;
@@ -43,14 +43,14 @@ delegator::delegator(char)
 
 // CHECK: define void @_ZN9delegatorC1Ei
 // CHECK: void @_ZN9delegatorC1Ev
-// CHECK-NOT: lpad
+// CHECK-NOT: void @_ZSt9terminatev
 // CHECK: ret
-// CHECK-NOT: lpad
+// CHECK-NOT: void @_ZSt9terminatev
 // CHECK: define void @_ZN9delegatorC2Ei
 // CHECK: void @_ZN9delegatorC2Ev
-// CHECK-NOT: lpad
+// CHECK-NOT: void @_ZSt9terminatev
 // CHECK: ret
-// CHECK-NOT: lpad
+// CHECK-NOT: void @_ZSt9terminatev
 delegator::delegator(int)
   : delegator()
 {}
