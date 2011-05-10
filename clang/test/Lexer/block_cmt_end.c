5bd77b3b0f928eda1b9c1e457540541d69de4cda
Matt Beaumont-Gay <matthewbg@google.com>
2011-05-10 20:28:29 +0000

    Change magic string "abc" to better magic string "qux".
    Wait, what?
    
    So, we run Clang (and LLVM) tests in an environment where the md5sum of the
    input files becomes a component of the path. When testing the preprocessor,
    the path becomes part of the output (in line directives). In this test, we
    were grepping for the absence of "abc" in the output. When the stars aligned
    properly, the md5sum component of the path contained "abc" and the test
    failed. Oops.
    
    llvm-svn: 131147

--- clang/test/Lexer/block_cmt_end.c
+++ clang/test/Lexer/block_cmt_end.c
@@ -1,7 +1,7 @@
 /*
   RUN: %clang_cc1 -E -trigraphs %s | grep bar
   RUN: %clang_cc1 -E -trigraphs %s | grep foo
-  RUN: %clang_cc1 -E -trigraphs %s | not grep abc
+  RUN: %clang_cc1 -E -trigraphs %s | not grep qux
   RUN: %clang_cc1 -E -trigraphs %s | not grep xyz
   RUN: %clang_cc1 -fsyntax-only -trigraphs -verify %s  
 */
@@ -9,7 +9,7 @@
 // This is a simple comment, /*/ does not end a comment, the trailing */ does.
 int i = /*/ */ 1;
 
-/* abc
+/* qux
 
 next comment ends with normal escaped newline:
 */
@@ -32,7 +32,3 @@ foo
 // rdar://6060752 - We should not get warnings about trigraphs in comments:
 // '????'
 /* ???? */
-
-
-
-
