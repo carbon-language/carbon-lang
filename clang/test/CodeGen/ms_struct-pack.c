b0c0cdc50989edcf53d5e6816326be3223df809b
Fariborz Jahanian <fjahanian@apple.com>
2011-05-11 17:07:02 +0000

    Clean up consequences of cut and paste.
    llvm-svn: 131190

--- clang/test/CodeGen/ms_struct-pack.c
+++ clang/test/CodeGen/ms_struct-pack.c
@@ -33,29 +33,29 @@ static int a1[(sizeof(one_ms) == 38) - 1];
 
 #pragma pack(2)
 struct _two_ms {
-        short m:9;      // size is 2
-        int q:27;       // size is 6
-        short w:13;     // size is 8
-        short e:3;      // size is 8
-        char r:4;       // size is 9
-        char t:7;       // size is 10
-        short y:16;     // size is 12
-        short u:1;      // size is 14
-        char i:2;       // size is 15
-        int a;          // size is 19
-        char o:6;       // size is 20
-        char s:2;       // size is 20
-        short d:10;     // size is 22
-        short f:4;      // size is 22
-        char b;         // size is 23
-        char g:1;       // size is 24
-        short h:13;     // size is 26
-        char j:8;       // size is 27
-        char k:5;       // size is 28
-        char c;         // size is 29
-        int l:28;       // size is 33
-        char z:7;       // size is 34
-        int x:20;       // size is 38
+        short m:9;      
+        int q:27;       
+        short w:13;     
+        short e:3;      
+        char r:4;       
+        char t:7;       
+        short y:16;     
+        short u:1;      
+        char i:2;       
+        int a;          
+        char o:6;       
+        char s:2;       
+        short d:10;     
+        short f:4;      
+        char b;         
+        char g:1;       
+        short h:13;     
+        char j:8;       
+        char k:5;       
+        char c;         
+        int l:28;       
+        char z:7;       
+        int x:20;       
 } __attribute__((__ms_struct__));
 
 typedef struct _two_ms two_ms;
@@ -64,29 +64,29 @@ static int a2[(sizeof(two_ms) == 42) - 1];
 
 #pragma pack(4)
 struct _four_ms {
-        short m:9;      // size is 2
-        int q:27;       // size is 6
-        short w:13;     // size is 8
-        short e:3;      // size is 8
-        char r:4;       // size is 9
-        char t:7;       // size is 10
-        short y:16;     // size is 12
-        short u:1;      // size is 14
-        char i:2;       // size is 15
-        int a;          // size is 19
-        char o:6;       // size is 20
-        char s:2;       // size is 20
-        short d:10;     // size is 22
-        short f:4;      // size is 22
-        char b;         // size is 23
-        char g:1;       // size is 24
-        short h:13;     // size is 26
-        char j:8;       // size is 27
-        char k:5;       // size is 28
-        char c;         // size is 29
-        int l:28;       // size is 33
-        char z:7;       // size is 34
-        int x:20;       // size is 38
+        short m:9;      
+        int q:27;       
+        short w:13;     
+        short e:3;      
+        char r:4;       
+        char t:7;       
+        short y:16;     
+        short u:1;      
+        char i:2;       
+        int a;          
+        char o:6;       
+        char s:2;       
+        short d:10;     
+        short f:4;      
+        char b;         
+        char g:1;       
+        short h:13;     
+        char j:8;       
+        char k:5;       
+        char c;         
+        int l:28;       
+        char z:7;       
+        int x:20;       
 } __attribute__((__ms_struct__));
 typedef struct _four_ms four_ms;
 
@@ -94,29 +94,29 @@ static int a4[(sizeof(four_ms) == 48) - 1];
 
 #pragma pack(8)
 struct _eight_ms {
-        short m:9;      // size is 2
-        int q:27;       // size is 6
-        short w:13;     // size is 8
-        short e:3;      // size is 8
-        char r:4;       // size is 9
-        char t:7;       // size is 10
-        short y:16;     // size is 12
-        short u:1;      // size is 14
-        char i:2;       // size is 15
-        int a;          // size is 19
-        char o:6;       // size is 20
-        char s:2;       // size is 20
-        short d:10;     // size is 22
-        short f:4;      // size is 22
-        char b;         // size is 23
-        char g:1;       // size is 24
-        short h:13;     // size is 26
-        char j:8;       // size is 27
-        char k:5;       // size is 28
-        char c;         // size is 29
-        int l:28;       // size is 33
-        char z:7;       // size is 34
-        int x:20;       // size is 38
+        short m:9;      
+        int q:27;       
+        short w:13;     
+        short e:3;      
+        char r:4;       
+        char t:7;       
+        short y:16;     
+        short u:1;      
+        char i:2;       
+        int a;          
+        char o:6;       
+        char s:2;       
+        short d:10;     
+        short f:4;      
+        char b;         
+        char g:1;       
+        short h:13;     
+        char j:8;       
+        char k:5;       
+        char c;         
+        int l:28;       
+        char z:7;       
+        int x:20;       
 } __attribute__((__ms_struct__));
 
 typedef struct _eight_ms eight_ms;
