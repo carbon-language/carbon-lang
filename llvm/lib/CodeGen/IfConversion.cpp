05fc35e275529eddb6c73bed7595bd7cec93f27a
Evan Cheng <evan.cheng@apple.com>
2011-05-11 01:03:01 +0000

    Add a late optimization to BranchFolding that hoist common instruction sequences at the start of basic blocks to their common predecessor. It's actually quite common (e.g. about 50 times in JM/lencod) and has shown to be a nice code size benefit. e.g.
            pushq   %rax
            testl   %edi, %edi
            jne     LBB0_2
    ## BB#1:
            xorb    %al, %al
            popq    %rdx
            ret
    LBB0_2:
            xorb    %al, %al
            callq   _foo
            popq    %rdx
            ret
    
    =>
    
            pushq   %rax
            xorb    %al, %al
            testl   %edi, %edi
            je      LBB0_2
    ## BB#1:
            callq   _foo
    LBB0_2:
            popq    %rdx
            ret
    
    rdar://9145558
    
    llvm-svn: 131172

--- llvm/lib/CodeGen/IfConversion.cpp
+++ llvm/lib/CodeGen/IfConversion.cpp
@@ -265,7 +265,7 @@ bool IfConverter::runOnMachineFunction(MachineFunction &MF) {
   if (!TII) return false;
 
   // Tail merge tend to expose more if-conversion opportunities.
-  BranchFolder BF(true);
+  BranchFolder BF(true, false);
   bool BFChange = BF.OptimizeFunction(MF, TII,
                                    MF.getTarget().getRegisterInfo(),
                                    getAnalysisIfAvailable<MachineModuleInfo>());
@@ -399,7 +399,7 @@ bool IfConverter::runOnMachineFunction(MachineFunction &MF) {
   BBAnalysis.clear();
 
   if (MadeChange && IfCvtBranchFold) {
-    BranchFolder BF(false);
+    BranchFolder BF(false, false);
     BF.OptimizeFunction(MF, TII,
                         MF.getTarget().getRegisterInfo(),
                         getAnalysisIfAvailable<MachineModuleInfo>());
