0bf26746d9791bc27a51121d31b247867ea469ea
Benjamin Kramer <benny.kra@googlemail.com>
2011-05-20 15:11:26 +0000

    Rename the "sandybridge" subtarget to "corei7-avx", for GCC compatibility.
    llvm-svn: 131730

--- llvm/lib/Support/Host.cpp
+++ llvm/lib/Support/Host.cpp
@@ -215,7 +215,7 @@ std::string sys::getHostCPUName() {
       case 37: // Intel Core i7, laptop version.
         return "corei7";
       case 42: // SandyBridge
-        return "sandybridge";
+        return "corei7-avx";
 
       case 28: // Intel Atom processor. All processors are manufactured using
                // the 45 nm process
--- llvm/test/CodeGen/X86/vec_uint_to_fp.ll
+++ llvm/test/CodeGen/X86/vec_uint_to_fp.ll
@@ -1,4 +1,4 @@
-; RUN: llc < %s -march=x86 -mcpu=sandybridge | FileCheck %s
+; RUN: llc < %s -march=x86 -mcpu=corei7-avx | FileCheck %s
 
 ; Test that we are not lowering uinttofp to scalars
 define <4 x float> @test1(<4 x i32> %A) nounwind {
