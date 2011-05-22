2e83c4c4c3a060cf49d6111bcccf73a382484c8f
Nick Lewycky <nicholas@mxc.ca>
2011-05-22 06:50:05 +0000

    Fix fallout from r131838.
    llvm-svn: 131844

--- clang/test/CodeGen/available-externally-suppress.c
+++ clang/test/CodeGen/available-externally-suppress.c
@@ -11,17 +11,17 @@ void test() {
   f0(17);
 }
 
-inline int __attribute__((always_inline)) f1(int x) { 
+inline int __attribute__((always_inline)) f1(int x) {
   int blarg = 0;
   for (int i = 0; i < x; ++i)
     blarg = blarg + x * i;
-  return blarg; 
+  return blarg;
 }
 
 // CHECK: @test1
-int test1(int x) { 
+int test1(int x) {
   // CHECK: br i1
-  // CHECK-NOT: call
+  // CHECK-NOT: call {{.*}} @f1
   // CHECK: ret i32
-  return f1(x); 
+  return f1(x);
 }
