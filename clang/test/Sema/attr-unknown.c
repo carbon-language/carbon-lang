fb43639926bc2a52c4105f8ad6fac60ac1f1165b
Ted Kremenek <kremenek@apple.com>
2011-06-01 20:09:40 +0000

    Rename -Wunknown-attributes to -Wattributes to match GCC.
    llvm-svn: 132422

--- clang/test/Sema/attr-unknown.c
+++ clang/test/Sema/attr-unknown.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -Wunknown-attributes %s
+// RUN: %clang_cc1 -fsyntax-only -verify -Wattributes %s
 
 int x __attribute__((foobar)); // expected-warning {{unknown attribute 'foobar' ignored}}
 void z() __attribute__((bogusattr)); // expected-warning {{unknown attribute 'bogusattr' ignored}}
