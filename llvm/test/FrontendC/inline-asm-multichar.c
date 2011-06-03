714af006a222a3b69119221e7c045b6932c2dcec
Eric Christopher <echristo@apple.com>
2011-06-03 20:56:31 +0000

    Testcase for llvm-gcc commit r132591.
    Part of rdar://9037836 and rdar://9119939
    
    llvm-svn: 132592

---
+++ llvm/test/FrontendC/inline-asm-multichar.c
@@ -0,0 +1,11 @@
+// RUN: %llvmgcc -S -march=armv7a %s 
+
+// XFAIL: *
+// XTARGET: arm
+
+int t1() {
+  static float k = 1.0f;
+CHECK: call void asm sideeffect "flds s15, $0 \0A", "*^Uv,~{s15}"
+  __asm__ volatile ("flds s15, %[k] \n" :: [k] "Uv,m" (k) : "s15");
+  return 0;
+}
