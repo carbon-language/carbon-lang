b38fd263bdea8d8977d39fb823df43111d3895e1
Galina Kistanova <gkistanova@gmail.com>
2011-06-03 18:36:30 +0000

    Added registered targets for in-test dependency declarations.
    llvm-svn: 132571

--- clang/test/CodeGen/2009-10-20-GlobalDebug.c
+++ clang/test/CodeGen/2009-10-20-GlobalDebug.c
@@ -1,3 +1,4 @@
+// REQUIRES: x86-registered-target
 // RUN: %clang -ccc-host-triple i386-apple-darwin10 -S -g -dA %s -o - | FileCheck %s
 int global;
 // CHECK: ascii   "localstatic"          ## DW_AT_name
