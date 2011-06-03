f1e2a7f0f0f7a65f280f52b9d9328da86d35f537
Devang Patel <dpatel@apple.com>
2011-06-03 17:23:47 +0000

    Simplify.
    llvm-svn: 132560

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -555,10 +555,12 @@ llvm::DIType CGDebugInfo::CreateType(const TypedefType *Ty,
   // We don't set size information, but do specify where the typedef was
   // declared.
   unsigned Line = getLineNumber(Ty->getDecl()->getLocation());
-  llvm::DIType DbgTy = 
-    DBuilder.createTypedef(Src, Ty->getDecl()->getName(), Unit, Line,
-              getContextDescriptor(cast<Decl>(Ty->getDecl()->getDeclContext())));
-  return DbgTy;
+  const TypedefNameDecl *TyDecl = Ty->getDecl();
+  llvm::DIDescriptor TydefContext =
+    getContextDescriptor(cast<Decl>(Ty->getDecl()->getDeclContext()));
+
+  return  
+    DBuilder.createTypedef(Src, TyDecl->getName(), Unit, Line, TydefContext);
 }
 
 llvm::DIType CGDebugInfo::CreateType(const FunctionType *Ty,
