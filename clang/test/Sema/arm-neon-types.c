8b51f19ec1525f926e97b7f70c9afbf7ac09b7ae
Bob Wilson <bob.wilson@apple.com>
2011-06-09 17:03:27 +0000

    Add isVCVT_N flag to identify Neon VCVT_N intrinsics, which require special range checking for immediate operands.  Radar 9558930.
    llvm-svn: 132783

--- clang/test/Sema/arm-neon-types.c
+++ clang/test/Sema/arm-neon-types.c
@@ -9,5 +9,12 @@ int32x2_t test(int32x2_t x) {
 
 // ...but should warn when the types really do not match.
 float32x2_t test2(uint32x2_t x) {
-  return vcvt_n_f32_s32(x, 0); // expected-warning {{incompatible vector types}}
+  return vcvt_n_f32_s32(x, 9); // expected-warning {{incompatible vector types}}
+}
+
+// Check immediate range for vcvt_n intrinsics is 1 to 32.  Radar 9558930.
+float32x2_t test3(uint32x2_t x) {
+  // FIXME: The "incompatible result type" error is due to pr10112 and should be
+  // removed when that is fixed.
+  return vcvt_n_f32_u32(x, 0); // expected-error {{argument should be a value from 1 to 32}} expected-error {{incompatible result type}}
 }
