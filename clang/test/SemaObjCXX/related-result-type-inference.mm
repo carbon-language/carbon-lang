53cad081e31c30db31f357b03743c4e2b840bdbf
Douglas Gregor <dgregor@apple.com>
2011-06-11 04:42:58 +0000

    Extra test for related result type inference
    llvm-svn: 132874

--- clang/test/SemaObjCXX/related-result-type-inference.mm
+++ clang/test/SemaObjCXX/related-result-type-inference.mm
@@ -67,4 +67,5 @@ void test_inference() {
   NSArray *arr = [[NSMutableArray alloc] init];
   NSMutableArray *marr = [arr retain]; // expected-warning{{incompatible pointer types initializing 'NSMutableArray *' with an expression of type 'NSArray *'}}
   marr = [arr retain]; // expected-warning{{incompatible pointer types assigning to 'NSMutableArray *' from 'NSArray *'}}
+  arr = [marr retain];
 }
