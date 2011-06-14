d53eca68f0816de9b43e716dd73e0ab642bff039
Fariborz Jahanian <fjahanian@apple.com>
2011-06-14 21:54:00 +0000

    Bad table discription of fromat-y2k causes no-format-y2k turn off -Wformat altogether. // rdar://9504680
    llvm-svn: 133015

---
+++ clang/test/Sema/no-format-y2k-turnsoff-format.c
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -verify -fsyntax-only -Wformat -Wno-format-y2k
+// rdar://9504680
+
+void foo(const char *, ...) __attribute__((__format__ (__printf__, 1, 2)));
+
+void bar(unsigned int a) {
+        foo("%s", a); // expected-warning {{conversion specifies type 'char *' but the argument has type 'unsigned int'}}
+}
+
