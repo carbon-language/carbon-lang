ada065ff52e27118e92abf7ec4fc7d44b310f056
John McCall <rjmccall@apple.com>
2011-06-16 00:32:01 +0000

    Give this test a triple;  I don't think we want to #define __unsafe_unretained on all platforms in non-ARC mode.
    llvm-svn: 133114

--- clang/test/SemaObjC/arc-unsafe_unretained.m
+++ clang/test/SemaObjC/arc-unsafe_unretained.m
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -fblocks -fobjc-nonfragile-abi %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -fblocks -fobjc-nonfragile-abi %s
 // RUN: %clang_cc1 -fsyntax-only -verify -fblocks -fobjc-nonfragile-abi -fobjc-arc %s
 
 struct X {
