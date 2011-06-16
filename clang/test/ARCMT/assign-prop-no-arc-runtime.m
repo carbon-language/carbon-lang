2798ded9dece51e278bfb8b2dfabfcb8f4879981
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-06-16 17:28:31 +0000

    [arcmt] Fix the test when running in Lion.
    llvm-svn: 133167

--- clang/test/ARCMT/assign-prop-no-arc-runtime.m
+++ clang/test/ARCMT/assign-prop-no-arc-runtime.m
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fobjc-nonfragile-abi -fsyntax-only -fobjc-arc -fobjc-no-arc-runtime -x objective-c %s.result
-// RUN: arcmt-test --args -triple x86_64-apple-darwin10 -fobjc-nonfragile-abi -fsyntax-only %s > %t
+// RUN: arcmt-test --args -triple x86_64-apple-macosx10.6 -fobjc-nonfragile-abi -fsyntax-only %s > %t
 // RUN: diff %t %s.result
 
 #include "Common.h"
