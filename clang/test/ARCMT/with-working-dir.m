ec7c965c2ea95c802d579ea89beb9ff0adf0d66a
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-06-16 20:19:55 +0000

    [arcmt] Fix test for MSVC build.
    llvm-svn: 133183

--- clang/test/ARCMT/with-working-dir.m
+++ clang/test/ARCMT/with-working-dir.m
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fobjc-nonfragile-abi -fsyntax-only -fobjc-arc -x objective-c %s.result
-// RUN: arcmt-test --args -triple x86_64-apple-darwin10 -fobjc-nonfragile-abi -working-directory %S with-working-dir.m > %t
+// RUN: arcmt-test --args -triple x86_64-apple-macosx10.6 -fobjc-nonfragile-abi -working-directory %S with-working-dir.m > %t
 // RUN: diff %t %s.result
 
 typedef int BOOL;
