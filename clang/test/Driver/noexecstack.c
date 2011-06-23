1abf57d5a94124d1b8e2f8fa4f42db8eb55882f6
Nick Lewycky <nicholas@mxc.ca>
2011-06-23 05:38:06 +0000

    Fix this test on machines that don't run clang -cc1as when asked to assemble.
    llvm-svn: 133688

--- clang/test/Driver/noexecstack.c
+++ clang/test/Driver/noexecstack.c
@@ -1,1 +1,1 @@
-// RUN: %clang -### %s -c -o tmp.o -Wa,--noexecstack 2>&1 | grep "mnoexecstack"
+// RUN: %clang -### %s -c -o tmp.o -triple i686-pc-linux-gnu -integrated-as -Wa,--noexecstack 2>&1 | grep "mnoexecstack"
