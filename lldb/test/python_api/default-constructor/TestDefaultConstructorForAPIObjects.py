57e34b21db38423e437e4cc97f56dbc468b583a7
Johnny Chen <johnny.chen@apple.com>
2011-06-24 22:53:12 +0000

    Add fuzz calls for SBBroadcaster.
    llvm-svn: 133837

--- lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
+++ lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
@@ -63,6 +63,9 @@ class APIDefaultConstructorTestCase(TestBase):
         if self.TraceOn():
             print obj
         self.assertFalse(obj)
+        # Do fuzz testing on the invalid obj, it should not crash lldb.
+        import sb_broadcaster
+        sb_broadcaster.fuzz_obj(obj)
 
     @python_api_test
     def test_SBCommandReturnObject(self):
---
+++ lldb/test/python_api/default-constructor/sb_broadcaster.py
@@ -0,0 +1,19 @@
+"""
+Fuzz tests an object after the default construction to make sure it does not crash lldb.
+"""
+
+import sys
+import lldb
+
+def fuzz_obj(obj):
+    obj.BroadcastEventByType(lldb.eBreakpointEventTypeInvalidType, True)
+    obj.BroadcastEvent(lldb.SBEvent(), False)
+    listener = lldb.SBListener("fuzz_testing")
+    obj.AddInitialEventsToListener(listener, 0xffffffff)
+    obj.AddInitialEventsToListener(listener, 0)
+    obj.AddListener(listener, 0xffffffff)
+    obj.AddListener(listener, 0)
+    obj.GetName()
+    obj.EventTypeHasListeners(0)
+    obj.RemoveListener(listener, 0xffffffff)
+    obj.RemoveListener(listener, 0)
