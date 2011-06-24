6e3dfa6025c301df749adb1251a9ea592bffb391
Johnny Chen <johnny.chen@apple.com>
2011-06-24 23:16:27 +0000

    Add fuzz calls for SBCommunication.
    llvm-svn: 133839

--- lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
+++ lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
@@ -81,6 +81,9 @@ class APIDefaultConstructorTestCase(TestBase):
         if self.TraceOn():
             print obj
         self.assertFalse(obj)
+        # Do fuzz testing on the invalid obj, it should not crash lldb.
+        import sb_communication
+        sb_communication.fuzz_obj(obj)
 
     @python_api_test
     def test_SBCompileUnit(self):
