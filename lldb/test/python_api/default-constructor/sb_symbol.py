c74ed6b0deda3c8d36e1b1e7cc3fae33a05af3b9
Johnny Chen <johnny.chen@apple.com>
2011-06-28 23:38:38 +0000

    Add fuzz calls for SBSymbol and SBSymbolContext.
    llvm-svn: 134042

---
+++ lldb/test/python_api/default-constructor/sb_symbol.py
@@ -0,0 +1,16 @@
+"""
+Fuzz tests an object after the default construction to make sure it does not crash lldb.
+"""
+
+import sys
+import lldb
+
+def fuzz_obj(obj):
+    obj.GetName()
+    obj.GetMangledName()
+    obj.GetInstructions(lldb.SBTarget())
+    obj.GetStartAddress()
+    obj.GetEndAddress()
+    obj.GetPrologueByteSize()
+    obj.GetType()
+    obj.GetDescription(lldb.SBStream())
---
+++ lldb/test/python_api/default-constructor/sb_symbolcontext.py
@@ -0,0 +1,15 @@
+"""
+Fuzz tests an object after the default construction to make sure it does not crash lldb.
+"""
+
+import sys
+import lldb
+
+def fuzz_obj(obj):
+    obj.GetModule()
+    obj.GetCompileUnit()
+    obj.GetFunction()
+    obj.GetBlock()
+    obj.GetLineEntry()
+    obj.GetSymbol()
+    obj.GetDescription(lldb.SBStream())
