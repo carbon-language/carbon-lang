4e77f6f49989fc984ea0c3e0e5757c0f4147602e
Johnny Chen <johnny.chen@apple.com>
2011-06-28 20:57:22 +0000

    Add fuzz calls for SBListener.
    llvm-svn: 134029

--- lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
+++ lldb/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
@@ -201,6 +201,9 @@ class APIDefaultConstructorTestCase(TestBase):
         if self.TraceOn():
             print obj
         self.assertFalse(obj)
+        # Do fuzz testing on the invalid obj, it should not crash lldb.
+        import sb_listener
+        sb_listener.fuzz_obj(obj)
 
     @python_api_test
     def test_SBModule(self):
