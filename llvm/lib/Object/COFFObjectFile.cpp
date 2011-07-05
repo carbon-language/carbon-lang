5ebaed2404572312f2d5003b3faa8ff9a65e3819
Michael J. Spencer <bigcheesegs@gmail.com>
2011-07-05 14:48:59 +0000

    Fix warnings.
    llvm-svn: 134408

--- llvm/lib/Object/COFFObjectFile.cpp
+++ llvm/lib/Object/COFFObjectFile.cpp
@@ -117,7 +117,7 @@ error_code COFFObjectFile::getSymbolNext(DataRefImpl Symb,
 error_code COFFObjectFile::getSymbolAddress(DataRefImpl Symb,
                                             uint64_t &Result) const {
   const coff_symbol *symb = toSymb(Symb);
-  const coff_section *Section;
+  const coff_section *Section = NULL;
   if (error_code ec = getSection(symb->SectionNumber, Section))
     return ec;
   char Type;
@@ -138,7 +138,7 @@ error_code COFFObjectFile::getSymbolSize(DataRefImpl Symb,
   //        in the same section as this symbol, and looking for either the next
   //        symbol, or the end of the section.
   const coff_symbol *symb = toSymb(Symb);
-  const coff_section *Section;
+  const coff_section *Section = NULL;
   if (error_code ec = getSection(symb->SectionNumber, Section))
     return ec;
   char Type;
@@ -171,7 +171,7 @@ error_code COFFObjectFile::getSymbolNMTypeChar(DataRefImpl Symb,
 
   uint32_t Characteristics = 0;
   if (symb->SectionNumber > 0) {
-    const coff_section *Section;
+    const coff_section *Section = NULL;
     if (error_code ec = getSection(symb->SectionNumber, Section))
       return ec;
     Characteristics = Section->Characteristics;
