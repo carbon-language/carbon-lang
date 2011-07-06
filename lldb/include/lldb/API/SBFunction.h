1630367ea6896bef798f75a2e2e5ad96e14b853d
Johnny Chen <johnny.chen@apple.com>
2011-07-06 23:23:53 +0000

    Add class docstrings with example usage for SBFunction and SBAddress.
    llvm-svn: 134560

--- lldb/include/lldb/API/SBFunction.h
+++ lldb/include/lldb/API/SBFunction.h
@@ -18,7 +18,40 @@ namespace lldb {
 
 #ifdef SWIG
 %feature("docstring",
-         "Represents a generic function, which can be inlined or not."
+"Represents a generic function, which can be inlined or not.
+
+For example (in test/lldbutil.py, but slightly modified for doc purpose),
+
+        ...
+
+        frame = thread.GetFrameAtIndex(i)
+        addr = frame.GetPCAddress()
+        load_addr = addr.GetLoadAddress(target)
+        function = frame.GetFunction()
+        mod_name = frame.GetModule().GetFileSpec().GetFilename()
+
+        if not function:
+            # No debug info for 'function'.
+            symbol = frame.GetSymbol()
+            file_addr = addr.GetFileAddress()
+            start_addr = symbol.GetStartAddress().GetFileAddress()
+            symbol_name = symbol.GetName()
+            symbol_offset = file_addr - start_addr
+            print >> output, '  frame #{num}: {addr:#016x} {mod}`{symbol} + {offset}'.format(
+                num=i, addr=load_addr, mod=mod_name, symbol=symbol_name, offset=symbol_offset)
+        else:
+            # Debug info is available for 'function'.
+            func_name = frame.GetFunctionName()
+            file_name = frame.GetLineEntry().GetFileSpec().GetFilename()
+            line_num = frame.GetLineEntry().GetLine()
+            print >> output, '  frame #{num}: {addr:#016x} {mod}`{func} at {file}:{line} {args}'.format(
+                num=i, addr=load_addr, mod=mod_name,
+                func='%s [inlined]' % func_name] if frame.IsInlined() else func_name,
+                file=file_name, line=line_num, args=get_args_as_string(frame, showFuncName=False))
+
+        ...
+
+"
          ) SBFunction;
 #endif
 class SBFunction
