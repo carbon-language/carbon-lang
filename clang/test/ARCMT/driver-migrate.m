c7ec4539edde180b2494533a14bdea5ea31706c9
NAKAMURA Takumi <geek4civic@gmail.com>
2011-07-11 16:21:34 +0000

    Tweak two tests with MSYS-bash tolerant.
    llvm-svn: 134899

--- clang/test/ARCMT/driver-migrate.m
+++ clang/test/ARCMT/driver-migrate.m
@@ -1,3 +1,3 @@
 // RUN: %clang -### -ccc-arcmt-migrate /foo/bar -fsyntax-only %s 2>&1 | FileCheck %s
 
-// CHECK: "-arcmt-migrate" "-arcmt-migrate-directory" "/foo/bar"
+// CHECK: "-arcmt-migrate" "-arcmt-migrate-directory" "{{[^"]*}}/foo/bar"
