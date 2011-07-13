60654d697ae8f613ba4ea6565df199c913b1cf08
Ted Kremenek <kremenek@apple.com>
2011-07-13 20:20:58 +0000

    format string checking: long and int have the same widths on 32-bit, so we shouldn't warn about using an "int" format specifier with a "long" type in 32-bit.
    llvm-svn: 135075

---
+++ clang/test/Sema/format-strings-i386.c
@@ -0,0 +1,14 @@
+// RUN: %clang_cc1 -triple i386-apple-macosx10.7.0 -fsyntax-only -verify -Wformat-nonliteral %s
+
+int printf(const char *restrict, ...);
+
+// Test that 'long' is compatible with 'int' on 32-bit.
+typedef unsigned int UInt32;
+void test_rdar_9763999() {
+ UInt32 x = 7;
+ printf("x = %u\n", x); // no-warning
+}
+
+void test_positive() {
+  printf("%d", "hello"); // expected-warning {{conversion specifies type 'int' but the argument has type 'char *'}}
+}
