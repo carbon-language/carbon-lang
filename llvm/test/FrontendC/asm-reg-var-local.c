c56b9c75d5794914228c1835ceebe629d6c8efa7
Eric Christopher <echristo@apple.com>
2011-07-16 07:28:35 +0000

    More minor adjustments.
    llvm-svn: 135342

--- llvm/test/FrontendC/asm-reg-var-local.c
+++ llvm/test/FrontendC/asm-reg-var-local.c
@@ -17,16 +17,16 @@ int foo() {
 // CHECK:  store i32 42, i32* %a, align 4
 
   asm volatile("; %0 This asm uses rsi" : : "r"(a));
-// CHECK:  %1 = load i32* %a, align 4
-// CHECK:  call void asm sideeffect "", "{rsi}"(i32 %1) nounwind
-// CHECK:  %2 = call i32 asm sideeffect "", "={rsi}"() nounwind
-// CHECK:  call void asm sideeffect "; $0 This asm uses rsi", "{rsi},~{dirflag},~{fpsr},~{flags}"(i32 %2)
+// CHECK:  %2 = load i32* %a, align 4
+// CHECK:  call void asm sideeffect "", "{rsi}"(i32 %2) nounwind
+// CHECK:  %3 = call i32 asm sideeffect "", "={rsi}"() nounwind
+// CHECK:  call void asm sideeffect "; $0 This asm uses rsi", "{rsi},~{dirflag},~{fpsr},~{flags}"(i32 %3)
 
   return a;
-// CHECK:  %3 = load i32* %a, align 4
-// CHECK:  call void asm sideeffect "", "{rsi}"(i32 %3) nounwind
-// CHECK:  %4 = call i32 asm sideeffect "", "={rsi}"() nounwind
-// CHECK:  store i32 %4, i32* %0, align 4
-// CHECK:  %5 = load i32* %0, align 4
-// CHECK:  store i32 %5, i32* %retval, align 4
+// CHECK:  %4 = load i32* %a, align 4
+// CHECK:  call void asm sideeffect "", "{rsi}"(i32 %4) nounwind
+// CHECK:  %5 = call i32 asm sideeffect "", "={rsi}"() nounwind
+// CHECK:  store i32 %5, i32* %0, align 4
+// CHECK:  %6 = load i32* %0, align 4
+// CHECK:  store i32 %6, i32* %retval, align 4
 }
