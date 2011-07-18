397d787642024ab66a9e0c1ca5421b421fcdcdf1
Alexis Hunt <alercah@gmail.com>
2011-07-18 18:37:21 +0000

    Given that __underlying_type is now available in clang, implement std::underlying_type.
    llvm-svn: 135410

--- libcxx/test/utilities/meta/meta.trans/meta.trans.other/underlying_type.pass.cpp
+++ libcxx/test/utilities/meta/meta.trans/meta.trans.other/underlying_type.pass.cpp
@@ -12,8 +12,22 @@
 // underlying_type
 
 #include <type_traits>
+#include <climits>
 
 int main()
 {
-#error underlying_type is not implemented
+    enum E { V = INT_MIN };
+    enum F { W = UINT_MAX };
+
+    static_assert((std::is_same<std::underlying_type<E>::type, int>::value),
+                  "E has the wrong underlying type");
+    static_assert((std::is_same<std::underlying_type<F>::type, unsigned>::value),
+                  "F has the wrong underlying type");
+
+#if __has_feature(cxx_strong_enums)
+    enum G : char { };
+
+    static_assert((std::is_same<std::underlying_type<G>::type, char>::value),
+                  "G has the wrong underlying type");
+#endif // __has_feature(cxx_strong_enums)
 }
