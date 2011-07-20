60fa2d232a6b4d05df83f1f98f1886e45221143a
Marshall Clow <mclow@qualcomm.com>
2011-07-20 14:27:46 +0000

    Fixed struct/class mismatch for std::type_info and added NORETURN flags
    llvm-svn: 135584

--- libcxxabi/include/cxxabi.h
+++ libcxxabi/include/cxxabi.h
@@ -34,8 +34,8 @@ extern void * __cxa_allocate_exception(size_t thrown_size) throw();
 extern void __cxa_free_exception(void * thrown_exception) throw();
 
 // 2.4.3 Throwing the Exception Object
-extern void __cxa_throw(void * thrown_exception, struct std::type_info * tinfo, 
-                        void (*dest)(void *));
+extern LIBCXXABI_NORETURN void __cxa_throw(void * thrown_exception, 
+        std::type_info * tinfo, void (*dest)(void *));
 
 // 2.5.3 Exception Handlers
 extern void * __cxa_get_exception_ptr(void * exceptionObject) throw();
@@ -44,7 +44,7 @@ extern void __cxa_end_catch();
 extern std::type_info * __cxa_current_exception_type();
 
 // 2.5.4 Rethrowing Exceptions
-extern void __cxa_rethrow();
+extern LIBCXXABI_NORETURN void __cxa_rethrow();
 
 
 
