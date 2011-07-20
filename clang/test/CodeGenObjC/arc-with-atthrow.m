53a495bd75865173ad056d7de1a8db52f4777c11
Fariborz Jahanian <fjahanian@apple.com>
2011-07-20 23:53:57 +0000

    Removes a comment.
    llvm-svn: 135640

--- clang/test/CodeGenObjC/arc-with-atthrow.m
+++ clang/test/CodeGenObjC/arc-with-atthrow.m
@@ -1,5 +1,4 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -emit-llvm -fobjc-arc -fobjc-nonfragile-abi -fobjc-exceptions -o - %s | FileCheck %s
-// $CLANG -cc1 -fobjc-arc -fobjc-nonfragile-abi -fobjc-exceptions
 // pr10411
 
 @interface NSException
