82bb089e8715a38c6b660eeb3ccad2ffb43ed241
Alexis Hunt <alercah@gmail.com>
2011-07-21 22:01:12 +0000

    Add a missing test for the limits on wchar
    llvm-svn: 135708

---
+++ clang/test/Headers/wchar_limits.cpp
@@ -0,0 +1,15 @@
+// RUN: %clang_cc1 -fsyntax-only -verify %s
+// RUN: %clang_cc1 -fsyntax-only -verify -fshort-wchar %s
+
+#include <limits.h>
+
+const bool swchar = (wchar_t)-1 > (wchar_t)0;
+
+#ifdef __WCHAR_UNSIGNED__
+int signed_test[!swchar];
+#else
+int signed_test[swchar];
+#endif
+
+int max_test[WCHAR_MAX == (swchar ? -(WCHAR_MIN+1) : (wchar_t)-1)];
+int min_test[WCHAR_MIN == (swchar ? 0 : -WCHAR_MAX-1)];
