99c3638935e5c00f77e687509cb195fc1d3eca89
Eric Christopher <echristo@apple.com>
2011-07-26 23:44:22 +0000

    Use the correct definition for memset.
    llvm-svn: 136188

--- clang/test/CodeGen/2004-02-13-Memset.c
+++ clang/test/CodeGen/2004-02-13-Memset.c
@@ -1,11 +1,8 @@
 // RUN: %clang_cc1  %s -emit-llvm -o - | grep llvm.memset | count 3
 
-#ifndef memset
-void *memset(void*, int, unsigned long);
-#endif
-#ifndef bzero
-void bzero(void*, unsigned long);
-#endif
+typedef __SIZE_TYPE__ size_t;
+void *memset(void*, int, size_t);
+void bzero(void*, size_t);
 
 void test(int* X, char *Y) {
   // CHECK: call i8* llvm.memset
