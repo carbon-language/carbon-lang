84c7b9e586030957e02a06e4db3c65feb1b84b41
Nicolas Geoffray <nicolas.geoffray@lip6.fr>
2011-07-26 20:52:25 +0000

    Update generated code to use new API of GetElementPtrInst::Create.
    llvm-svn: 136138

--- llvm/lib/Target/CppBackend/CPPBackend.cpp
+++ llvm/lib/Target/CppBackend/CPPBackend.cpp
@@ -1240,8 +1240,7 @@ void CppWriter::printInstruction(const Instruction *I,
         nl(Out);
       }
       Out << "Instruction* " << iName << " = GetElementPtrInst::Create("
-          << opNames[0] << ", " << iName << "_indices.begin(), "
-          << iName << "_indices.end()";
+          << opNames[0] << ", " << iName << "_indices";
     }
     Out << ", \"";
     printEscapedString(gep->getName());
