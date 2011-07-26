ebc113d1d33d366e0aa2fb7385c64f5d5e24acb3
Eric Christopher <echristo@apple.com>
2011-07-26 23:18:10 +0000

    Try a little hack to fix the memset duplication on windows.
    llvm-svn: 136179

--- clang/test/CodeGen/2004-02-13-Memset.c
+++ clang/test/CodeGen/2004-02-13-Memset.c
@@ -1,7 +1,11 @@
 // RUN: %clang_cc1  %s -emit-llvm -o - | grep llvm.memset | count 3
 
+#ifndef memset
 void *memset(void*, int, unsigned long);
+#endif
+#ifndef bzero
 void bzero(void*, unsigned long);
+#endif
 
 void test(int* X, char *Y) {
   // CHECK: call i8* llvm.memset
