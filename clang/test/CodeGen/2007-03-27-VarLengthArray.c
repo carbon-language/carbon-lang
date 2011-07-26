4a8bc43083fcd9229e2349761f76b2b865cea7f0
Eric Christopher <echristo@apple.com>
2011-07-26 22:27:01 +0000

    Try to generalize the match to quiet the windows build bot.
    llvm-svn: 136162

--- clang/test/CodeGen/2007-03-27-VarLengthArray.c
+++ clang/test/CodeGen/2007-03-27-VarLengthArray.c
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck %s
 
-// CHECK: getelementptr inbounds i32* %vla
+// CHECK: getelementptr inbounds i32* %{{vla|[0-9]}}
 extern void f(int *);
 int e(int m, int n) {
   int x[n];
