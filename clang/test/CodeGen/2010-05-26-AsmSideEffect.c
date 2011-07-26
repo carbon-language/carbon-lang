19520147fc76bcc78d474c9a844bedfea39e85c0
Eric Christopher <echristo@apple.com>
2011-07-26 22:44:31 +0000

    Rewrite match line to be friendlier to misc buildbots.
    llvm-svn: 136169

--- clang/test/CodeGen/2010-05-26-AsmSideEffect.c
+++ clang/test/CodeGen/2010-05-26-AsmSideEffect.c
@@ -3,7 +3,7 @@
 
 int test (void *src) {
   register int w0 asm ("0");
-  // CHECK: call i32 asm "ldr $0, [$1]", "={ax},r,~{dirflag},~{fpsr},~{flags}"(i8* %tmp)
+  // CHECK: call i32 asm "ldr $0, [$1]", "={ax},r,~{dirflag},~{fpsr},~{flags}"(i8*
   asm ("ldr %0, [%1]": "=r" (w0): "r" (src));
   return w0;
 }
