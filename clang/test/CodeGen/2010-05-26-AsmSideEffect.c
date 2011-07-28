8afab4d19ed97c2fb200474763361bc1fde47710
Eric Christopher <echristo@apple.com>
2011-07-28 00:08:06 +0000

    This was meant to test arm anyhow, make the registers agree with the instruction and the architecture for which the instruction exists.
    llvm-svn: 136301

--- clang/test/CodeGen/2010-05-26-AsmSideEffect.c
+++ clang/test/CodeGen/2010-05-26-AsmSideEffect.c
@@ -1,9 +1,9 @@
-// RUN: %clang_cc1 %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 %s -emit-llvm -triple arm-apple-darwin -o - | FileCheck %s
 // Radar 8026855
 
 int test (void *src) {
   register int w0 asm ("0");
-  // CHECK: call i32 asm "ldr $0, [$1]", "={ax},r,~{dirflag},~{fpsr},~{flags}"(i8*
+  // CHECK: call i32 asm "ldr $0, [$1]", "={r0},r,~{dirflag},~{fpsr},~{flags}"(i8*
   asm ("ldr %0, [%1]": "=r" (w0): "r" (src));
   return w0;
 }
