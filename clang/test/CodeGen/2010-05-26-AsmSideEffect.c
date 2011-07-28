7089f25dd6d3df7ca62c671e5dc97815c6effd31
Eric Christopher <echristo@apple.com>
2011-07-28 00:30:42 +0000

    Fix thinko in last checkin.
    llvm-svn: 136311

--- clang/test/CodeGen/2010-05-26-AsmSideEffect.c
+++ clang/test/CodeGen/2010-05-26-AsmSideEffect.c
@@ -3,7 +3,7 @@
 
 int test (void *src) {
   register int w0 asm ("0");
-  // CHECK: call i32 asm "ldr $0, [$1]", "={r0},r,~{dirflag},~{fpsr},~{flags}"(i8*
+  // CHECK: call i32 asm "ldr $0, [$1]", "={r0}{{.*}}(i8*
   asm ("ldr %0, [%1]": "=r" (w0): "r" (src));
   return w0;
 }
