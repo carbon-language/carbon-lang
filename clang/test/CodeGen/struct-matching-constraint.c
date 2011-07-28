9c8dece1b64fc89b8e71e33147b5541b51d7f32c
Eric Christopher <echristo@apple.com>
2011-07-28 00:22:59 +0000

    Fix this test to work for arm and on all platforms.
    llvm-svn: 136307

--- clang/test/CodeGen/struct-matching-constraint.c
+++ clang/test/CodeGen/struct-matching-constraint.c
@@ -1,12 +1,6 @@
-// RUN: %clang_cc1 -emit-llvm -march=armv7a %s 
-
-// XFAIL: *
-// XTARGET: arm
-
-typedef struct __simd128_uint16_t
-{
-  __neon_uint16x8_t val;
-} uint16x8_t;
+// RUN: %clang -S -emit-llvm -arch arm -march=armv7a %s
+typedef unsigned short uint16_t;
+typedef __attribute__((neon_vector_type(8))) uint16_t uint16x8_t;
 
 void b(uint16x8_t sat, uint16x8_t luma)
 {
