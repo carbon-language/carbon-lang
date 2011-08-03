a8d4df429ae0bd464978cd67168abb712a84643b
Johnny Chen <johnny.chen@apple.com>
2011-08-03 20:39:26 +0000

    Add license header comment.
    llvm-svn: 136808

--- lldb/test/benchmarks/example/main.cpp
+++ lldb/test/benchmarks/example/main.cpp
@@ -1,3 +1,11 @@
+//===-- main.c --------------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
 #include <stdio.h>
 
 class Point {
