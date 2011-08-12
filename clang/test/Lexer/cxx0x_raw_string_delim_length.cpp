c0a06c5cfdc6f9a7aee0db49cc0b14facdecdb49
Craig Topper <craig.topper@gmail.com>
2011-08-12 03:20:18 +0000

    Fix a couple raw string literal tests
    llvm-svn: 137422

--- clang/test/Lexer/cxx0x_raw_string_delim_length.cpp
+++ clang/test/Lexer/cxx0x_raw_string_delim_length.cpp
@@ -1,6 +1,3 @@
 // RUN: %clang_cc1 -std=c++0x -E %s 2>&1 | grep 'error: raw string delimiter longer than 16 characters'
 
 const char *str = R"abcdefghijkmnopqrstuvwxyz(abcdef)abcdefghijkmnopqrstuvwxyz";
-// RUN: %clang_cc1 -std=c++0x -E %s 2>&1 | grep 'error: raw string delimiter longer than 16 characters'
-
-const char *str = R"abcdefghijkmnopqrstuvwxyz(abcdef)abcdefghijkmnopqrstuvwxyz";
--- clang/test/Lexer/cxx0x_raw_string_unterminated.cpp
+++ clang/test/Lexer/cxx0x_raw_string_unterminated.cpp
@@ -2,7 +2,3 @@
 
 const char *str = R"foo(abc
 def)bar";
-// RUN: %clang_cc1 -std=c++0x -E %s 2>&1 | grep 'error: raw string missing terminating delimiter )foo"'
-
-const char *str = R"foo(abc
-def)bar";
