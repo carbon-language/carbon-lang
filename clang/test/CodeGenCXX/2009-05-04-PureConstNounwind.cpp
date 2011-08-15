d9afc5c749aba22ac27b4708cc86b3737ed9d670
Eric Christopher <echristo@apple.com>
2011-08-15 22:48:14 +0000

    FileCheckize this test.
    llvm-svn: 137671

--- clang/test/CodeGenCXX/2009-05-04-PureConstNounwind.cpp
+++ clang/test/CodeGenCXX/2009-05-04-PureConstNounwind.cpp
@@ -1,8 +1,15 @@
-// RUN: %clang_cc1 -fexceptions -emit-llvm %s -o - | grep nounwind | count 4
+// RUN: %clang_cc1 -fexceptions -emit-llvm %s -o - | FileCheck %s
 int c(void) __attribute__((const));
 int p(void) __attribute__((pure));
 int t(void);
 
+// CHECK: define i32 @_Z1fv() {
 int f(void) {
+  // CHECK: call i32 @_Z1cv() nounwind readnone
+  // CHECK: call i32 @_Z1pv() nounwind readonly
   return c() + p() + t();
 }
+
+// CHECK: declare i32 @_Z1cv() nounwind readnone
+// CHECK: declare i32 @_Z1pv() nounwind readonly
+// CHECK-NOT: declare i32 @_Z1tv() nounwind
