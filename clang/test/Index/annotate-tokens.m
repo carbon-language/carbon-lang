5ef35b7ce2fa88f2886f0d0a8d9c40c1e5dee09b
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-08-15 21:45:01 +0000

    Fix the buildbot test.
    llvm-svn: 137660

--- clang/test/Index/annotate-tokens.m
+++ clang/test/Index/annotate-tokens.m
@@ -568,7 +568,7 @@ static Rdar8595462_A * Rdar8595462_staticVar;
 // CHECK-PROP-AFTER-METHOD: Punctuation: "@" [138:1 - 138:2] ObjCInterfaceDecl=Rdar8062781:134:12
 
 // RUN: c-index-test -test-annotate-tokens=%s:141:1:142:1 %s -DIBOutlet='__attribute__((iboutlet))' -DIBAction='void)__attribute__((ibaction)' | FileCheck -check-prefix=CHECK-WITH-WEAK %s
-// CHECK-WITH-WEAK: Identifier: "__weak" [141:3 - 141:9] macro expansion=__weak:137:9
+// CHECK-WITH-WEAK: Identifier: "__weak" [141:3 - 141:9] macro expansion
 // CHECK-WITH-WEAK: Identifier: "Foo" [141:10 - 141:13] ObjCClassRef=Foo:1:12
 // CHECK-WITH-WEAK: Punctuation: "*" [141:14 - 141:15] ObjCIvarDecl=foo:141:15 (Definition)
 // CHECK-WITH-WEAK: Identifier: "foo" [141:15 - 141:18] ObjCIvarDecl=foo:141:15 (Definition)
