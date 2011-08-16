f1b912b88e8014f7a6f8cd81e240d43b56e4d607
Eric Christopher <echristo@apple.com>
2011-08-16 21:41:38 +0000

    Migrate from llvm/test/FrontendC++.
    llvm-svn: 137765

---
+++ clang/test/CodeGenCXX/member-alignment.cpp
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck %s
+// XFAIL: arm,powerpc
+
+// rdar://7268289
+
+class t {
+public:
+  virtual void foo(void);
+  void bar(void);
+};
+
+void
+t::bar(void) {
+// CHECK: _ZN1t3barEv{{.*}} align 2
+}
+
+void
+t::foo(void) {
+// CHECK: _ZN1t3fooEv{{.*}} align 2
+}
