d3ad0aa41387e036b606341b689a998d1ad4cc1f
Jim Grosbach <grosbach@apple.com>
2011-08-16 20:55:41 +0000

    Tidy up formatting.
    llvm-svn: 137747

--- llvm/test/MC/ARM/mode-switch.s
+++ llvm/test/MC/ARM/mode-switch.s
@@ -1,17 +1,15 @@
 @ Test ARM / Thumb mode switching with .code
-@ RUN: llvm-mc -mcpu=cortex-a8 -triple arm-unknown-unknown -show-encoding < %s | FileCheck %s
-@ RUN: llvm-mc -mcpu=cortex-a8 -triple thumb-unknown-unknown -show-encoding < %s | FileCheck %s
+@ RUN: llvm-mc -triple armv7-unknown-unknown -show-encoding < %s | FileCheck %s
+@ RUN: llvm-mc -triple thumbv7-unknown-unknown -show-encoding <%s | FileCheck %s
 
 .code 16
-
-@ CHECK: add.w	r0, r0, r1              @ encoding: [0x00,0xeb,0x01,0x00]
 	add.w	r0, r0, r1
+@ CHECK: add.w	r0, r0, r1              @ encoding: [0x00,0xeb,0x01,0x00]
 
 .code 32
-@ CHECK: add	r0, r0, r1              @ encoding: [0x01,0x00,0x80,0xe0]
 	add	r0, r0, r1
+@ CHECK: add	r0, r0, r1              @ encoding: [0x01,0x00,0x80,0xe0]
 
 .code 16
-@ CHECK: adds	r0, r0, r1              @ encoding: [0x40,0x18]
-
         adds    r0, r0, r1
+@ CHECK: adds	r0, r0, r1              @ encoding: [0x40,0x18]
