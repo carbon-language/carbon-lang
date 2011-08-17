ae2848ed69a7000d12314005c320079ad692d83a
Devang Patel <dpatel@apple.com>
2011-08-17 18:39:13 +0000

    Remove superficial test.
    llvm-svn: 137846

--- llvm/test/FrontendObjC/2010-06-04-UnnamedCFString-dbg.m
+++
@@ -1,6 +0,0 @@
-// RUN: %llvmgcc -S -O0 -g %s -o - | grep DW_TAG_variable | count 1
-
-// Do not emit debug info for unnamed builtin CFString variable.
-@interface Foo
-@end
-Foo *FooName = @"FooBar";
