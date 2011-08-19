a0b6bc5739efb56c791b7c2bbc39d414fc01b888
Eric Christopher <echristo@apple.com>
2011-08-19 23:41:50 +0000

    Remove migrated or obsolete tests.
    llvm-svn: 138149

--- llvm/test/FrontendC++/2005-02-27-PlacementArrayNewCrash.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: %llvmgxx -S %s -o -
-
-#include <new>
-typedef double Ty[4];
-
-void foo(Ty *XX) {
-  new(XX) Ty();
-}
--- llvm/test/FrontendC++/2006-09-08-powi.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %llvmgxx -O3 -S -o - %s
-
-#include <cmath>
-
-double foo(double X, int Y) {
-  return std::pow(X, Y);
-}
