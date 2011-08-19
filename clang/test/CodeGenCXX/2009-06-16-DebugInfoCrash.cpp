b52ebc3e29c9613901b329438e53751aee45376f
Eric Christopher <echristo@apple.com>
2011-08-19 21:51:07 +0000

    Migrate 2009-06-16-DebugInfoCrash.cpp from llvm/test/FrontendC++.
    llvm-svn: 138099

---
+++ clang/test/CodeGenCXX/2009-06-16-DebugInfoCrash.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -emit-llvm %s -o /dev/null -g
+// This crashes if we try to emit debug info for TEMPLATE_DECL members.
+template <class T> class K2PtrVectorBase {};
+template <class T> class K2Vector {};
+template <class U > class K2Vector<U*> : public K2PtrVectorBase<U*> {};
+class ScriptInfoManager {
+  void PostRegister() ;
+  template <class SI> short ReplaceExistingElement(K2Vector<SI*>& v);
+};
+void ScriptInfoManager::PostRegister() {}
