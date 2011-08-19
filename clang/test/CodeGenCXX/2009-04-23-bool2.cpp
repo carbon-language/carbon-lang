20429e90d7bbe15d55f4d06134ff2a6840540f7f
Eric Christopher <echristo@apple.com>
2011-08-19 22:16:21 +0000

    Migrate 2009-04-23-bool2.cpp from llvm/test/FrontendC++.
    llvm-svn: 138110

---
+++ clang/test/CodeGenCXX/2009-04-23-bool2.cpp
@@ -0,0 +1,15 @@
+// RUN: %clang_cc1 -emit-llvm %s -o /dev/null
+// g++.old-deja/g++.jason/bool2.C from gcc testsuite.
+// Crashed before 67975 went in.
+struct F {
+  bool b1 : 1;
+  bool b2 : 7;
+};
+
+int main()
+{
+  F f = { true, true };
+
+  if (int (f.b1) != 1)
+    return 1;
+}
