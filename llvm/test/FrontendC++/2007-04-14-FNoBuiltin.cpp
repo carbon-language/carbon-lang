48f1599b0355b5a2fd94ed9ae87c08af4b94933d
Eric Christopher <echristo@apple.com>
2011-08-19 23:08:41 +0000

    Remove obsolete or migrated tests.
    llvm-svn: 138135

--- llvm/test/FrontendC++/2007-04-14-FNoBuiltin.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %llvmgcc -S %s -O2 -fno-builtin -o - | grep call.*printf
-// Check that -fno-builtin is honored.
-
-extern "C" int printf(const char*, ...);
-void foo(const char *msg) {
-	printf("%s\n",msg);
-}
