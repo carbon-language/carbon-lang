58ce352d329835e6ceedb39b5d9ca30a9c143a35
Eric Christopher <echristo@apple.com>
2011-08-19 22:17:09 +0000

    Remove tests migrated to clang or are unnecessary.
    llvm-svn: 138115

--- llvm/test/FrontendC++/2008-01-12-VecInit.cpp
+++
@@ -1,6 +0,0 @@
-// RUN: %llvmgcc -xc++ %s -S -o -
-// rdar://5685492
-
-typedef int __attribute__((vector_size(16))) v;
-v vt = {1, 2, 3, 4};
-
--- llvm/test/FrontendC++/2009-02-07-VolatileArrayRefHack.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %llvmgxx -S %s -o - | grep {load volatile}
-// PR3320
-
-void test(volatile int *a) {
-    // should be a volatile load.
-    a[0];
-}
