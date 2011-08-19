69d6c3d31a4614754cb758a3ff7e4f03eaac44f0
Eric Christopher <echristo@apple.com>
2011-08-19 21:51:03 +0000

    Migrate 2009-08-05-ZeroInitWidth.cpp from llvm/test/FrontendC++.
    llvm-svn: 138097

---
+++ clang/test/CodeGenCXX/2009-08-05-ZeroInitWidth.cpp
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -emit-llvm %s -o -
+// rdar://7114564
+struct A {
+  unsigned long long : (sizeof(unsigned long long) * 8) - 16;
+};
+struct B {
+  A a;
+};
+struct B b = {
+  {}
+};
