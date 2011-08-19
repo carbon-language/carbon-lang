029529369b2fcb5be9977b4fc4ff5e6a05c8b108
Eric Christopher <echristo@apple.com>
2011-08-19 21:51:41 +0000

    Remove this test. The feature and test have already been migrated to clang.
    llvm-svn: 138101

--- llvm/test/FrontendC++/2009-05-04-PureConstNounwind.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: %llvmgxx -S %s -o - | grep nounwind | count 4
-int c(void) __attribute__((const));
-int p(void) __attribute__((pure));
-int t(void);
-
-int f(void) {
-	return c() + p() + t();
-}
