6582dfa7c5c8a522948ba274df9572d04816e880
Eric Christopher <echristo@apple.com>
2011-08-20 01:04:56 +0000

    Remove remainder of migrated or obsolete tests from FrontendC and remove the empty directory.
    llvm-svn: 138181

--- llvm/test/FrontendObjC/2010-02-01-utf16-with-null.m
+++
@@ -1,5 +0,0 @@
-/* RUN: %llvmgcc -w -x objective-c -S %s -o - | not grep {__ustring}
-   rdar://7589850 */
-
-void *P = @"good\0bye";
-
--- llvm/test/FrontendObjC/2010-02-23-DbgInheritance.m
+++
@@ -1,9 +0,0 @@
-// RUN: %llvmgcc %s -S -g -o - | grep -v DW_TAG_member
-// Interface P should not be a member of interface I in debug info.
-@interface P 
-@end
-
-@interface I : P 
-@end
-
-void fn(I *iptr) {}
