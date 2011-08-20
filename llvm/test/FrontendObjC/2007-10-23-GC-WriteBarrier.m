1efe3d9663cabda382934070f5601ab2e5f8adc3
Eric Christopher <echristo@apple.com>
2011-08-20 00:38:20 +0000

    Remove obsolete or migrated tests.
    llvm-svn: 138173

--- llvm/test/FrontendObjC/2007-10-23-GC-WriteBarrier.m
+++
@@ -1,9 +0,0 @@
-// RUN: %llvmgcc -x objective-c -S %s -o /dev/null -fobjc-gc
-// rdar://5541393
-
-typedef unsigned int NSUInteger;
-__attribute__((objc_gc(strong))) float *_scores;
-
-void foo(int i, float f) {
-  _scores[i] = f; 
-}
