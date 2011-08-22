096423b6be2b596c1e84855092402045f05a4271
Jim Grosbach <grosbach@apple.com>
2011-08-22 23:13:54 +0000

    Tidy up. Trailing whitespace.
    llvm-svn: 138293

--- llvm/test/MC/ARM/reg-list.s
+++ llvm/test/MC/ARM/reg-list.s
@@ -1,6 +1,6 @@
 @ RUN: llvm-mc -triple thumb-apple-darwin10 -show-encoding < %s 2> %t | FileCheck %s
 @ RUN: FileCheck --check-prefix=CHECK-WARNINGS < %t %s
-        
+
         push    {r7, lr}
 @ CHECK-WARNINGS: register not in ascending order in register list
 
