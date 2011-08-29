55d6f43cab20fe84b55c641fa1e6805aa165e94a
Jim Grosbach <grosbach@apple.com>
2011-08-29 23:01:38 +0000

    Thumb2 assembly parsing and encoding support for ADC(immediate).
    llvm-svn: 138778

--- llvm/test/MC/ARM/basic-thumb2-instructions.s
+++ llvm/test/MC/ARM/basic-thumb2-instructions.s
@@ -15,6 +15,29 @@ _func:
 @ CHECK: _func
 
 @------------------------------------------------------------------------------
+@ ADC (immediate)
+@------------------------------------------------------------------------------
+        adc r0, r1, #4
+        adcs r0, r1, #0
+        adc r1, r2, #255
+        adc r3, r7, #0x00550055
+        adc r8, r12, #0xaa00aa00
+        adc r9, r7, #0xa5a5a5a5
+        adc r5, r3, #0x87000000
+        adc r4, r2, #0x7f800000
+        adc r4, r2, #0x00000680
+
+@ CHECK: adc	r0, r1, #4              @ encoding: [0x41,0xf1,0x04,0x00]
+@ CHECK: adcs	r0, r1, #0              @ encoding: [0x51,0xf1,0x00,0x00]
+@ CHECK: adc	r1, r2, #255            @ encoding: [0x42,0xf1,0xff,0x01]
+@ CHECK: adc	r3, r7, #5570645        @ encoding: [0x47,0xf1,0x55,0x13]
+@ CHECK: adc	r8, r12, #2852170240    @ encoding: [0x4c,0xf1,0xaa,0x28]
+@ CHECK: adc	r9, r7, #2779096485     @ encoding: [0x47,0xf1,0xa5,0x39]
+@ CHECK: adc	r5, r3, #2264924160     @ encoding: [0x43,0xf1,0x07,0x45]
+@ CHECK: adc	r4, r2, #2139095040     @ encoding: [0x42,0xf1,0xff,0x44]
+@ CHECK: adc	r4, r2, #1664           @ encoding: [0x42,0xf5,0xd0,0x64]
+
+@------------------------------------------------------------------------------
 @ IT
 @------------------------------------------------------------------------------
 @ Test encodings of a few full IT blocks, not just the IT instruction
