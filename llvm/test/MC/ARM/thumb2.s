4b23a65582a4ec93965707c607230bab04c5e609
Jim Grosbach <grosbach@apple.com>
2011-08-29 23:04:04 +0000

    Remove redundant tests from XFAIL'ed test file.
    llvm-svn: 138779

--- llvm/test/MC/ARM/thumb2.s
+++ llvm/test/MC/ARM/thumb2.s
@@ -2,17 +2,6 @@
 @ XFAIL: *
 .code 16
 
-@ CHECK: adc	r1, r1, #171            @ encoding: [0xab,0x01,0x41,0xf1]
-  adc	r1, r1, #171
-@ CHECK: adc	r1, r1, #1179666        @ encoding: [0x12,0x11,0x41,0xf1]
-  adc	r1, r1, #1179666
-@ CHECK: adc	r1, r1, #872428544      @ encoding: [0x34,0x21,0x41,0xf1]
-  adc	r1, r1, #872428544
-@ CHECK: adc	r1, r1, #1448498774     @ encoding: [0x56,0x31,0x41,0xf1]
-  adc	r1, r1, #1448498774
-@ CHECK: adc	r1, r1, #66846720       @ encoding: [0x7f,0x71,0x41,0xf1]
-  adc	r1, r1, #66846720
-
 @ CHECK: mvn	r0, #187                @ encoding: [0xbb,0x00,0x6f,0xf0]
   mvn	r0, #187
 @ CHECK: mvn	r0, #11141290           @ encoding: [0xaa,0x10,0x6f,0xf0]
