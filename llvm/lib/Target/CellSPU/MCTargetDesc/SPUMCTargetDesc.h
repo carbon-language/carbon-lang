87cec4d1721afabe6d7a2ee418e5bd3233d8e84d
James Molloy <james.molloy@arm.com>
2011-08-30 07:27:02 +0000

    Fix typos in SPUMCTargetDesc.h
    Patch supplied by Liu (projlc@gmail.com)
    
    llvm-svn: 138799

--- llvm/lib/Target/CellSPU/MCTargetDesc/SPUMCTargetDesc.h
+++ llvm/lib/Target/CellSPU/MCTargetDesc/SPUMCTargetDesc.h
@@ -1,4 +1,4 @@
-//===-- SPUMCTargetDesc.h - Alpha Target Descriptions ---------*- C++ -*-===//
+//===-- SPUMCTargetDesc.h - CellSPU Target Descriptions ---------*- C++ -*-===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -7,7 +7,7 @@
 //
 //===----------------------------------------------------------------------===//
 //
-// This file provides Alpha specific target descriptions.
+// This file provides CellSPU specific target descriptions.
 //
 //===----------------------------------------------------------------------===//
 
