37ad0091c3a4610c6f1701e053b12dd2955d7f41
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-09-07 21:50:18 +0000

    [arcmt] Try fixing the windows buildbot.
    llvm-svn: 139262

--- clang/test/ARCMT/migrate-space-in-path.m
+++ clang/test/ARCMT/migrate-space-in-path.m
@@ -1,7 +1,5 @@
-// RUN: mkdir -p %t/"with space"
-// RUN: cp %S/Inputs/* %t/"with space"
-// RUN: %clang_cc1 -arcmt-migrate -arcmt-migrate-directory %t.migrate %t/"with space"/test1.m.in -x objective-c -fobjc-nonfragile-abi
-// RUN: %clang_cc1 -arcmt-migrate -arcmt-migrate-directory %t.migrate %t/"with space"/test2.m.in -x objective-c -fobjc-nonfragile-abi
-// RUN: c-arcmt-test -arcmt-migrate-directory %t.migrate | arcmt-test -verify-transformed-files %t/"with space"/test1.m.in.result %t/"with space"/test2.m.in.result %t/"with space"/test.h.result
 // RUN: rm -rf %t.migrate
-// RUN: rm -rf %t
+// RUN: %clang_cc1 -arcmt-migrate -arcmt-migrate-directory %t.migrate %S/"with space"/test1.m.in -x objective-c -fobjc-nonfragile-abi
+// RUN: %clang_cc1 -arcmt-migrate -arcmt-migrate-directory %t.migrate %S/"with space"/test2.m.in -x objective-c -fobjc-nonfragile-abi
+// RUN: c-arcmt-test -arcmt-migrate-directory %t.migrate | arcmt-test -verify-transformed-files %S/"with space"/test1.m.in.result %S/"with space"/test2.m.in.result %S/"with space"/test.h.result
+// RUN: rm -rf %t.migrate
