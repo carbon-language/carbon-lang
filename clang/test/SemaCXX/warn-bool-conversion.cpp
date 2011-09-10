f1e2924b6b910e98a03ffd883a168d24e63192c5
David Blaikie <dblaikie@gmail.com>
2011-09-10 05:47:59 +0000

    Correctly referring to the null pointer as 'null' not the macro 'NULL' in the boolean conversion diagnostic message.
    llvm-svn: 139465

--- clang/test/SemaCXX/warn-bool-conversion.cpp
+++ clang/test/SemaCXX/warn-bool-conversion.cpp
@@ -1,18 +1,18 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
-int* j = false; // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
+int* j = false; // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
 
-void foo(int* i, int *j=(false)) // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
+void foo(int* i, int *j=(false)) // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
 {
-  foo(false); // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
+  foo(false); // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
   foo((int*)false); // no-warning: explicit cast
   foo(0); // no-warning: not a bool, even though its convertible to bool
 
-  foo(false == true); // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
-  foo((42 + 24) < 32); // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
+  foo(false == true); // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
+  foo((42 + 24) < 32); // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
 
   const bool kFlag = false;
-  foo(kFlag); // expected-warning{{ initialization of pointer of type 'int *' to NULL from a constant boolean expression}}
+  foo(kFlag); // expected-warning{{ initialization of pointer of type 'int *' to null from a constant boolean expression}}
 }
 
 char f(struct Undefined*);
