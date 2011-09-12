ba15240e8364f0d6b5b36a0084515889078c6162
Devang Patel <dpatel@apple.com>
2011-09-12 18:11:52 +0000

    Update test to fix windows buildbot.
    llvm-svn: 139513

--- clang/test/CodeGenCXX/debug-info-char16.cpp
+++ clang/test/CodeGenCXX/debug-info-char16.cpp
@@ -1,5 +1,8 @@
 // RUN: %clang_cc1 -S -std=c++0x -masm-verbose -g %s -o -| FileCheck %s
 
-//CHECK:	.byte	16                      ## DW_AT_encoding
+//CHECK:	.byte	16
+//CHECK-NEXT: 	.ascii	 "char16_t"
+
+// 16 is DW_ATE_UTF (0x10) encoding attribute.
 char16_t char_a = u'h';
 
