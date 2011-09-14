f0bbc1c8fd5f87078a05c013dee3df93d14844af
Akira Hatanaka <ahatanak@gmail.com>
2011-09-14 17:24:05 +0000

    Add comment.
    llvm-svn: 139700

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -2697,6 +2697,7 @@ public:
   }
   virtual void getArchDefines(const LangOptions &Opts,
                                 MacroBuilder &Builder) const {
+    // NOTE: O64 will not be supported.
     if (ABI == "o32") {
       Builder.defineMacro("__mips_o32");
       Builder.defineMacro("_ABIO32", "1");
