c2ff27446dc7f7399ada50b073ebfd423f2fb35d
Sean Callanan <scallanan@apple.com>
2011-09-15 18:41:04 +0000

    Fixed a problem where the symbol context was not being initialized properly in the absence of a process.
    llvm-svn: 139823

--- lldb/source/Expression/ClangExpressionDeclMap.cpp
+++ lldb/source/Expression/ClangExpressionDeclMap.cpp
@@ -76,6 +76,9 @@ ClangExpressionDeclMap::WillParse(ExecutionContext &exe_ctx)
         m_parser_vars->m_sym_ctx = exe_ctx.thread->GetStackFrameAtIndex(0)->GetSymbolContext(lldb::eSymbolContextEverything);
     else if (exe_ctx.process)
         m_parser_vars->m_sym_ctx = SymbolContext(exe_ctx.target->GetSP(), ModuleSP());
+    else if (exe_ctx.target)
+        m_parser_vars->m_sym_ctx = SymbolContext(exe_ctx.target->GetSP(), ModuleSP());
+    
     if (exe_ctx.target)
         m_parser_vars->m_persistent_vars = &exe_ctx.target->GetPersistentVariables();
     
