3f62af76b9fbaaa1877e98722571ae70fab49347
NAKAMURA Takumi <geek4civic@gmail.com>
2011-09-16 03:55:36 +0000

    test/CodeGen/sse-builtins.c: Make this host-independent to unbreak posix-unlike hosts.
    Without -ffreestanding, clang tries to seek /usr/include/stdlib.h in host filesystem, even on Windows hosts.
    
    llvm-svn: 139899

--- clang/test/CodeGen/sse-builtins.c
+++ clang/test/CodeGen/sse-builtins.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple i386-apple-darwin9 -target-cpu pentium4 -target-feature +sse4.1 -g -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -ffreestanding -triple i386-apple-darwin9 -target-cpu pentium4 -target-feature +sse4.1 -g -emit-llvm %s -o - | FileCheck %s
 
 #include <emmintrin.h>
 
