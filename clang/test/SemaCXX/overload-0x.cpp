a62a666273e5790e9738584b405977ea5d5238b4
John McCall <rjmccall@apple.com>
2011-09-20 06:21:28 +0000

    Fix a crash-on-invalid with bad CV-qualification on 'this' in the presence of an implicit move assignment operator.  I think the implicit copy assignment operator case was also wrong, but just in a "displaying the wrong diagnostic" way.
    llvm-svn: 140139

---
+++ clang/test/SemaCXX/overload-0x.cpp
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -fsyntax-only -std=c++0x -verify %s 
+
+namespace test0 {
+  struct A { // expected-note {{candidate function (the implicit copy assignment operator) not viable: 'this' argument has type 'const test0::A', but method is not marked const}} expected-note {{candidate function (the implicit move assignment operator) not viable: 'this' argument has type 'const test0::A', but method is not marked const}}
+    A &operator=(void*); // expected-note {{candidate function not viable: 'this' argument has type 'const test0::A', but method is not marked const}}
+  };
+
+  void test(const A &a) {
+    a = "help"; // expected-error {{no viable overloaded '='}}
+  }
+}
