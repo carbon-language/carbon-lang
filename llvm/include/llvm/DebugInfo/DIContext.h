aec423d526ec4515757ad9700f079a354e167410
Benjamin Kramer <benny.kra@googlemail.com>
2011-09-21 01:13:16 +0000

    DebugInfo: Add equality operators and default constructor to DILineInfo.
    llvm-svn: 140223

--- llvm/include/llvm/DebugInfo/DIContext.h
+++ llvm/include/llvm/DebugInfo/DIContext.h
@@ -17,6 +17,7 @@
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/DataTypes.h"
+#include <cstring>
 
 namespace llvm {
 
@@ -28,12 +29,21 @@ class DILineInfo {
   uint32_t Line;
   uint32_t Column;
 public:
+  DILineInfo() : FileName("<invalid>"), Line(0), Column(0) {}
   DILineInfo(const char *fileName, uint32_t line, uint32_t column)
     : FileName(fileName), Line(line), Column(column) {}
 
   const char *getFileName() const { return FileName; }
   uint32_t getLine() const { return Line; }
   uint32_t getColumn() const { return Column; }
+
+  bool operator==(const DILineInfo &RHS) const {
+    return Line == RHS.Line && Column == RHS.Column &&
+           std::strcmp(FileName, RHS.FileName) == 0;
+  }
+  bool operator!=(const DILineInfo &RHS) const {
+    return !(*this == RHS);
+  }
 };
 
 class DIContext {
