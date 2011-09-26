1db746afbf8c71c5f287722eb229007fc9aa189b
Richard Trieu <rtrieu@google.com>
2011-09-26 22:38:21 +0000

    Move string literal to bool conversion into its own warning flag -Wstring-conversion.
    llvm-svn: 140574

--- clang/test/SemaCXX/warn-literal-conversion.cpp
+++ clang/test/SemaCXX/warn-literal-conversion.cpp
@@ -43,19 +43,3 @@ void test0() {
   int y = (24*60*60) * 0.25;
   int pennies = 123.45 * 100;
 }
-
-// Warn on cases where a string literal is converted into a bool.
-// An exception is made for this in logical operators.
-void assert(bool condition);
-void test1() {
-  bool b0 = "hi"; // expected-warning{{implicit conversion turns string literal into bool: 'const char [3]' to 'bool'}}
-  b0 = ""; // expected-warning{{implicit conversion turns string literal into bool: 'const char [1]' to 'bool'}}
-  b0 = 0 && "";
-  assert("error"); // expected-warning{{implicit conversion turns string literal into bool: 'const char [6]' to 'bool'}}
-  assert(0 && "error");
-
-  while("hi") {} // expected-warning{{implicit conversion turns string literal into bool: 'const char [3]' to 'bool'}}
-  do {} while("hi"); // expected-warning{{implicit conversion turns string literal into bool: 'const char [3]' to 'bool'}}
-  for (;"hi";); // expected-warning{{implicit conversion turns string literal into bool: 'const char [3]' to 'bool'}}
-  if("hi") {} // expected-warning{{implicit conversion turns string literal into bool: 'const char [3]' to 'bool'}}
-}
