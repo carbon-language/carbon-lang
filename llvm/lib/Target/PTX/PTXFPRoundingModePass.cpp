957a6d5c51d34cd95b5f28ad94d084f2d283a805
Justin Holewinski <justin.holewinski@gmail.com>
2011-09-30 13:46:52 +0000

    PTX: Add programmable rounding mode specifier for int <-> fp conversion instrs. Also take this opportunity to clean up the rounding mode pass.
    llvm-svn: 140854

--- llvm/lib/Target/PTX/PTXFPRoundingModePass.cpp
+++ llvm/lib/Target/PTX/PTXFPRoundingModePass.cpp
@@ -16,6 +16,7 @@
 
 #include "PTX.h"
 #include "PTXTargetMachine.h"
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/CodeGen/MachineFunctionPass.h"
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/Support/Debug.h"
@@ -33,12 +34,18 @@ namespace llvm {
   class PTXFPRoundingModePass : public MachineFunctionPass {
     private:
       static char ID;
+
+      typedef std::pair<unsigned, unsigned> RndModeDesc;
+
       PTXTargetMachine& TargetMachine;
+      DenseMap<unsigned, RndModeDesc> Instrs;
 
     public:
       PTXFPRoundingModePass(PTXTargetMachine &TM, CodeGenOpt::Level OptLevel)
         : MachineFunctionPass(ID),
-          TargetMachine(TM) {}
+          TargetMachine(TM) {
+        initializeMap();
+      }
 
       virtual bool runOnMachineFunction(MachineFunction &MF);
 
@@ -48,6 +55,7 @@ namespace llvm {
 
     private:
 
+      void initializeMap();
       void processInstruction(MachineInstr &MI);
   }; // class PTXFPRoundingModePass
 } // namespace llvm
@@ -57,7 +65,6 @@ using namespace llvm;
 char PTXFPRoundingModePass::ID = 0;
 
 bool PTXFPRoundingModePass::runOnMachineFunction(MachineFunction &MF) {
-
   // Look at each basic block
   for (MachineFunction::iterator bbi = MF.begin(), bbe = MF.end(); bbi != bbe;
        ++bbi) {
@@ -72,79 +79,96 @@ bool PTXFPRoundingModePass::runOnMachineFunction(MachineFunction &MF) {
   return false;
 }
 
-void PTXFPRoundingModePass::processInstruction(MachineInstr &MI) {
-  // If the instruction has a rounding mode set to RndDefault, then assign an
-  // appropriate rounding mode based on the target device.
+void PTXFPRoundingModePass::initializeMap() {
+  using namespace PTXRoundingMode;
   const PTXSubtarget& ST = TargetMachine.getSubtarget<PTXSubtarget>();
-  switch (MI.getOpcode()) {
-  case PTX::FADDrr32:
-  case PTX::FADDri32:
-  case PTX::FADDrr64:
-  case PTX::FADDri64:
-  case PTX::FSUBrr32:
-  case PTX::FSUBri32:
-  case PTX::FSUBrr64:
-  case PTX::FSUBri64:
-  case PTX::FMULrr32:
-  case PTX::FMULri32:
-  case PTX::FMULrr64:
-  case PTX::FMULri64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      MI.getOperand(1).setImm(PTXRoundingMode::RndNearestEven);
+
+  // Build a map of default rounding mode for all instructions that need a
+  // rounding mode.
+  Instrs[PTX::FADDrr32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FADDri32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FADDrr64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FADDri64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSUBrr32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSUBri32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSUBrr64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSUBri64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FMULrr32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FMULri32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FMULrr64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FMULri64] = std::make_pair(1U, (unsigned)RndNearestEven);
+
+  Instrs[PTX::FNEGrr32] = std::make_pair(1U, (unsigned)RndNone);
+  Instrs[PTX::FNEGri32] = std::make_pair(1U, (unsigned)RndNone);
+  Instrs[PTX::FNEGrr64] = std::make_pair(1U, (unsigned)RndNone);
+  Instrs[PTX::FNEGri64] = std::make_pair(1U, (unsigned)RndNone);
+
+  unsigned FDivRndMode = ST.fdivNeedsRoundingMode() ? RndNearestEven : RndNone;
+  Instrs[PTX::FDIVrr32] = std::make_pair(1U, FDivRndMode);
+  Instrs[PTX::FDIVri32] = std::make_pair(1U, FDivRndMode);
+  Instrs[PTX::FDIVrr64] = std::make_pair(1U, FDivRndMode);
+  Instrs[PTX::FDIVri64] = std::make_pair(1U, FDivRndMode);
+
+  unsigned FMADRndMode = ST.fmadNeedsRoundingMode() ? RndNearestEven : RndNone;
+  Instrs[PTX::FMADrrr32] = std::make_pair(1U, FMADRndMode);
+  Instrs[PTX::FMADrri32] = std::make_pair(1U, FMADRndMode);
+  Instrs[PTX::FMADrii32] = std::make_pair(1U, FMADRndMode);
+  Instrs[PTX::FMADrrr64] = std::make_pair(1U, FMADRndMode);
+  Instrs[PTX::FMADrri64] = std::make_pair(1U, FMADRndMode);
+  Instrs[PTX::FMADrii64] = std::make_pair(1U, FMADRndMode);
+
+  Instrs[PTX::FSQRTrr32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSQRTri32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSQRTrr64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::FSQRTri64] = std::make_pair(1U, (unsigned)RndNearestEven);
+
+  Instrs[PTX::FSINrr32] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FSINri32] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FSINrr64] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FSINri64] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FCOSrr32] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FCOSri32] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FCOSrr64] = std::make_pair(1U, (unsigned)RndApprox);
+  Instrs[PTX::FCOSri64] = std::make_pair(1U, (unsigned)RndApprox);
+
+  Instrs[PTX::CVTu16f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs16f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTu16f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs16f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTu32f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs32f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTu32f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs32f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTu64f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs64f32] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTu64f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+  Instrs[PTX::CVTs64f64] = std::make_pair(1U, (unsigned)RndTowardsZeroInt);
+
+  Instrs[PTX::CVTf32u16] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32s16] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32u32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32s32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32u64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32s64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf32f64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64u16] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64s16] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64u32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64s32] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64u64] = std::make_pair(1U, (unsigned)RndNearestEven);
+  Instrs[PTX::CVTf64s64] = std::make_pair(1U, (unsigned)RndNearestEven);
+}
+
+void PTXFPRoundingModePass::processInstruction(MachineInstr &MI) {
+  // Is this an instruction that needs a rounding mode?
+  if (Instrs.count(MI.getOpcode())) {
+    const RndModeDesc &Desc = Instrs[MI.getOpcode()];
+    // Get the rounding mode operand
+    MachineOperand &Op = MI.getOperand(Desc.first);
+    // Update the rounding mode if needed
+    if (Op.getImm() == PTXRoundingMode::RndDefault) {
+      Op.setImm(Desc.second);
     }
-    break;
-  case PTX::FNEGrr32:
-  case PTX::FNEGri32:
-  case PTX::FNEGrr64:
-  case PTX::FNEGri64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      MI.getOperand(1).setImm(PTXRoundingMode::RndNone);
-    }
-    break;
-  case PTX::FDIVrr32:
-  case PTX::FDIVri32:
-  case PTX::FDIVrr64:
-  case PTX::FDIVri64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      if (ST.fdivNeedsRoundingMode())
-        MI.getOperand(1).setImm(PTXRoundingMode::RndNearestEven);
-      else
-        MI.getOperand(1).setImm(PTXRoundingMode::RndNone);
-    }
-    break;
-  case PTX::FMADrrr32:
-  case PTX::FMADrri32:
-  case PTX::FMADrii32:
-  case PTX::FMADrrr64:
-  case PTX::FMADrri64:
-  case PTX::FMADrii64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      if (ST.fmadNeedsRoundingMode())
-        MI.getOperand(1).setImm(PTXRoundingMode::RndNearestEven);
-      else
-        MI.getOperand(1).setImm(PTXRoundingMode::RndNone);
-    }
-    break;
-  case PTX::FSQRTrr32:
-  case PTX::FSQRTri32:
-  case PTX::FSQRTrr64:
-  case PTX::FSQRTri64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      MI.getOperand(1).setImm(PTXRoundingMode::RndNearestEven);
-    }
-    break;
-  case PTX::FSINrr32:
-  case PTX::FSINri32:
-  case PTX::FSINrr64:
-  case PTX::FSINri64:
-  case PTX::FCOSrr32:
-  case PTX::FCOSri32:
-  case PTX::FCOSrr64:
-  case PTX::FCOSri64:
-    if (MI.getOperand(1).getImm() == PTXRoundingMode::RndDefault) {
-      MI.getOperand(1).setImm(PTXRoundingMode::RndApprox);
-    }
-    break;
   }
 }
 
