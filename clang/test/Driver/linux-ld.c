c5ab7efea372bb2ab025e3a6168766237f9c0de7
Chandler Carruth <chandlerc@gmail.com>
2011-10-01 02:08:56 +0000

    Teach this test to cope with Windows suffixes so that msys builds can run it.
    llvm-svn: 140925

--- clang/test/Driver/linux-ld.c
+++ clang/test/Driver/linux-ld.c
@@ -3,9 +3,9 @@
 // RUN: %clang -no-canonical-prefixes -ccc-host-triple i386-unknown-linux %s -### -o %t.o 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-LD-32 %s
 // 32-bit machines may use 'lib' or 'lib32' dependending on how they setup multilib.
-// CHECK-LD-32: "{{.*}}ld" {{.*}} "-L/lib/../lib{{(32)?}}" "-L/usr/lib/../lib{{(32)?}}"
+// CHECK-LD-32: "{{.*}}ld{{(.exe)?}}" {{.*}} "-L/lib/../lib{{(32)?}}" "-L/usr/lib/../lib{{(32)?}}"
 //
 // RUN: %clang -no-canonical-prefixes -ccc-host-triple x86_64-unknown-linux %s -### -o %t.o 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-LD-64 %s
 // FIXME: Should we allow 'lib' instead of 'lib64' here?
-// CHECK-LD-64: "{{.*}}ld" {{.*}} "-L/lib/../lib64" "-L/usr/lib/../lib64"
+// CHECK-LD-64: "{{.*}}ld{{(.exe)?}}" {{.*}} "-L/lib/../lib64" "-L/usr/lib/../lib64"
