97bb84154596b1dcc38ebf47d3624df19a84e96f
Chandler Carruth <chandlerc@gmail.com>
2011-10-01 01:46:47 +0000

    Correct my failure at writing proper regex's for FileCheck. Thanks Nick.
    llvm-svn: 140920

--- clang/test/Driver/linux-ld.c
+++ clang/test/Driver/linux-ld.c
@@ -3,7 +3,7 @@
 // RUN: %clang -no-canonical-prefixes -ccc-host-triple i386-unknown-linux %s -### -o %t.o 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-LD-32 %s
 // 32-bit machines may use 'lib' or 'lib32' dependending on how they setup multilib.
-// CHECK-LD-32: "{{.*}}ld" {{.*}} "-L/lib/../lib{{32|}}" "-L/usr/lib/../lib{{32|}}"
+// CHECK-LD-32: "{{.*}}ld" {{.*}} "-L/lib/../lib{{(32)?}}" "-L/usr/lib/../lib{{(32)?}}"
 //
 // RUN: %clang -no-canonical-prefixes -ccc-host-triple x86_64-unknown-linux %s -### -o %t.o 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-LD-64 %s
