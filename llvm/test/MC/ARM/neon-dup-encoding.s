2644375abe04982b3b2fd4e325c4d37641b862c2
Jim Grosbach <grosbach@apple.com>
2011-10-04 20:34:11 +0000

    Un-XFAIL the file. Disable only the individual tests that aren't working yet.
    llvm-svn: 141108

--- llvm/test/MC/ARM/neon-dup-encoding.s
+++ llvm/test/MC/ARM/neon-dup-encoding.s
@@ -1,7 +1,5 @@
 @ RUN: llvm-mc -mcpu=cortex-a8 -triple arm-unknown-unknown -show-encoding < %s | FileCheck %s
 
-@ XFAIL: *
-
 	vdup.8	d16, r0
 	vdup.16	d16, r0
 	vdup.32	d16, r0
@@ -18,18 +16,18 @@
 @ CHECK: vdup.16	q8, r0          @ encoding: [0xb0,0x0b,0xa0,0xee]
 @ CHECK: vdup.32	q8, r0          @ encoding: [0x90,0x0b,0xa0,0xee]
 
-	vdup.8	d16, d16[1]
-	vdup.16	d16, d16[1]
-	vdup.32	d16, d16[1]
+@	vdup.8	d16, d16[1]
+@	vdup.16	d16, d16[1]
+@	vdup.32	d16, d16[1]
 
-@ CHECK: vdup.8	d16, d16[1]             @ encoding: [0x20,0x0c,0xf3,0xf3]
-@ CHECK: vdup.16	d16, d16[1]     @ encoding: [0x20,0x0c,0xf6,0xf3]
-@ CHECK: vdup.32	d16, d16[1]     @ encoding: [0x20,0x0c,0xfc,0xf3]
+@ FIXME: vdup.8	d16, d16[1]             @ encoding: [0x20,0x0c,0xf3,0xf3]
+@ FIXME: vdup.16	d16, d16[1]     @ encoding: [0x20,0x0c,0xf6,0xf3]
+@ FIXME: vdup.32	d16, d16[1]     @ encoding: [0x20,0x0c,0xfc,0xf3]
 
-	vdup.8	q8, d16[1]
-	vdup.16	q8, d16[1]
-	vdup.32	q8, d16[1]
+@	vdup.8	q8, d16[1]
+@	vdup.16	q8, d16[1]
+@	vdup.32	q8, d16[1]
 
-@ CHECK: vdup.8	q8, d16[1]              @ encoding: [0x60,0x0c,0xf3,0xf3]
-@ CHECK: vdup.16	q8, d16[1]      @ encoding: [0x60,0x0c,0xf6,0xf3]
-@ CHECK: vdup.32	q8, d16[1]      @ encoding: [0x60,0x0c,0xfc,0xf3]
+@ FIXME: vdup.8	q8, d16[1]              @ encoding: [0x60,0x0c,0xf3,0xf3]
+@ FIXME: vdup.16	q8, d16[1]      @ encoding: [0x60,0x0c,0xf6,0xf3]
+@ FIXME: vdup.32	q8, d16[1]      @ encoding: [0x60,0x0c,0xfc,0xf3]
