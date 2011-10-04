8a829e8ecb7b7856144c9ea9d71d0d07a713469f
Jim Grosbach <grosbach@apple.com>
2011-10-04 20:50:05 +0000

    Un-XFAIL file. Fix incorrect CHECK lines. General format cleanup.
    llvm-svn: 141114

--- llvm/test/MC/ARM/neont2-bitcount-encoding.s
+++ llvm/test/MC/ARM/neont2-bitcount-encoding.s
@@ -1,34 +1,38 @@
 @ RUN: llvm-mc -mcpu=cortex-a8 -triple thumb-unknown-unknown -show-encoding < %s | FileCheck %s
-@ XFAIL: *
 
 .code 16
 
-@ CHECK: vcnt.8	d16, d16                @ encoding: [0x20,0x05,0xf0,0xff]
 	vcnt.8	d16, d16
-@ CHECK: vcnt.8	q8, q8                  @ encoding: [0x60,0x05,0xf0,0xff]
 	vcnt.8	q8, q8
-@ CHECK: vclz.i8	d16, d16                @ encoding: [0xa0,0x04,0xf0,0xff]
+
+@ CHECK: vcnt.8	d16, d16                @ encoding: [0xf0,0xff,0x20,0x05]
+@ CHECK: vcnt.8	q8, q8                  @ encoding: [0xf0,0xff,0x60,0x05]
+
 	vclz.i8	d16, d16
-@ CHECK: vclz.i16	d16, d16        @ encoding: [0xa0,0x04,0xf4,0xff]
 	vclz.i16	d16, d16
-@ CHECK: vclz.i32	d16, d16        @ encoding: [0xa0,0x04,0xf8,0xff]
 	vclz.i32	d16, d16
-@ CHECK: vclz.i8	q8, q8                  @ encoding: [0xe0,0x04,0xf0,0xff]
 	vclz.i8	q8, q8
-@ CHECK: vclz.i16	q8, q8          @ encoding: [0xe0,0x04,0xf4,0xff]
 	vclz.i16	q8, q8
-@ CHECK: vclz.i32	q8, q8          @ encoding: [0xe0,0x04,0xf8,0xff]
 	vclz.i32	q8, q8
-@ CHECK: vcls.s8	d16, d16                @ encoding: [0x20,0x04,0xf0,0xff]
+
+@ CHECK: vclz.i8	d16, d16        @ encoding: [0xf0,0xff,0xa0,0x04]
+@ CHECK: vclz.i16	d16, d16        @ encoding: [0xf4,0xff,0xa0,0x04]
+@ CHECK: vclz.i32	d16, d16        @ encoding: [0xf8,0xff,0xa0,0x04]
+@ CHECK: vclz.i8	q8, q8          @ encoding: [0xf0,0xff,0xe0,0x04]
+@ CHECK: vclz.i16	q8, q8          @ encoding: [0xf4,0xff,0xe0,0x04]
+@ CHECK: vclz.i32	q8, q8          @ encoding: [0xf8,0xff,0xe0,0x04]
+
 	vcls.s8	d16, d16
-@ CHECK: vcls.s16	d16, d16        @ encoding: [0x20,0x04,0xf4,0xff]
 	vcls.s16	d16, d16
-@ CHECK: vcls.s32	d16, d16        @ encoding: [0x20,0x04,0xf8,0xff]
 	vcls.s32	d16, d16
-@ CHECK: vcls.s8	q8, q8                  @ encoding: [0x60,0x04,0xf0,0xff]
 	vcls.s8	q8, q8
-@ CHECK: vcls.s16	q8, q8          @ encoding: [0x60,0x04,0xf4,0xff]
 	vcls.s16	q8, q8
-@ CHECK: vcls.s32	q8, q8          @ encoding: [0x60,0x04,0xf8,0xff]
 	vcls.s32	q8, q8
+
+@ CHECK: vcls.s8	d16, d16        @ encoding: [0xf0,0xff,0x20,0x04]
+@ CHECK: vcls.s16	d16, d16        @ encoding: [0xf4,0xff,0x20,0x04]
+@ CHECK: vcls.s32	d16, d16        @ encoding: [0xf8,0xff,0x20,0x04]
+@ CHECK: vcls.s8	q8, q8          @ encoding: [0xf0,0xff,0x60,0x04]
+@ CHECK: vcls.s16	q8, q8          @ encoding: [0xf4,0xff,0x60,0x04]
+@ CHECK: vcls.s32	q8, q8          @ encoding: [0xf8,0xff,0x60,0x04]
 
