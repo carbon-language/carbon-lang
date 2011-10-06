0947102623f134411193fa2c7c29c569bda2432a
Jim Grosbach <grosbach@apple.com>
2011-10-06 22:04:05 +0000

    Tidy up tests. Un-XFAIL file and mark individual tests as FIXME instead.
    llvm-svn: 141321

--- llvm/test/MC/ARM/neont2-dup-encoding.s
+++ llvm/test/MC/ARM/neont2-dup-encoding.s
@@ -1,29 +1,31 @@
-@ RUN: llvm-mc -mcpu=cortex-a8 -triple thumb-unknown-unknown -show-encoding < %s | FileCheck %s
-@ XFAIL: *
+@RUN: llvm-mc -triple thumbv7-unknown-unknown -show-encoding < %s | FileCheck %s
 
 .code 16
 
-@ CHECK: vdup.8	d16, r0                 @ encoding: [0x90,0x0b,0xc0,0xee]
-	vdup.8	d16, r0
-@ CHECK: vdup.16	d16, r0                 @ encoding: [0xb0,0x0b,0x80,0xee]
-	vdup.16	d16, r0
-@ CHECK: vdup.32	d16, r0                 @ encoding: [0x90,0x0b,0x80,0xee]
-	vdup.32	d16, r0
-@ CHECK: vdup.8	q8, r0                  @ encoding: [0x90,0x0b,0xe0,0xee]
-	vdup.8	q8, r0
-@ CHECK: vdup.16	q8, r0                  @ encoding: [0xb0,0x0b,0xa0,0xee]
-	vdup.16	q8, r0
-@ CHECK: vdup.32	q8, r0                  @ encoding: [0x90,0x0b,0xa0,0xee]
-	vdup.32	q8, r0
-@ CHECK: vdup.8	d16, d16[1]             @ encoding: [0x20,0x0c,0xf3,0xff]
-	vdup.8	d16, d16[1]
-@ CHECK: vdup.16	d16, d16[1]             @ encoding: [0x20,0x0c,0xf6,0xff]
-	vdup.16	d16, d16[1]
-@ CHECK: vdup.32	d16, d16[1]             @ encoding: [0x20,0x0c,0xfc,0xff]
-	vdup.32	d16, d16[1]
-@ CHECK: vdup.8	q8, d16[1]              @ encoding: [0x60,0x0c,0xf3,0xff]
-	vdup.8	q8, d16[1]
-@ CHECK: vdup.16	q8, d16[1]              @ encoding: [0x60,0x0c,0xf6,0xff]
-	vdup.16	q8, d16[1]
-@ CHECK: vdup.32	q8, d16[1]              @ encoding: [0x60,0x0c,0xfc,0xff]
-	vdup.32	q8, d16[1]
+	vdup.8	d16, r1
+	vdup.16	d15, r2
+	vdup.32	d14, r3
+	vdup.8	q9, r4
+	vdup.16	q8, r5
+	vdup.32	q7, r6
+
+@ CHECK: vdup.8	d16, r1                 @ encoding: [0xc0,0xee,0x90,0x1b]
+@ CHECK: vdup.16	d15, r2         @ encoding: [0x8f,0xee,0x30,0x2b]
+@ CHECK: vdup.32	d14, r3         @ encoding: [0x8e,0xee,0x10,0x3b]
+@ CHECK: vdup.8	q9, r4                  @ encoding: [0xe2,0xee,0x90,0x4b]
+@ CHECK: vdup.16	q8, r5          @ encoding: [0xa0,0xee,0xb0,0x5b]
+@ CHECK: vdup.32	q7, r6          @ encoding: [0xae,0xee,0x10,0x6b]
+
+@	vdup.8	d16, d16[1]
+@	vdup.16	d16, d16[1]
+@	vdup.32	d16, d16[1]
+@	vdup.8	q8, d16[1]
+@	vdup.16	q8, d16[1]
+@	vdup.32	q8, d16[1]
+
+@ FIXME: vdup.8	d16, d16[1]             @ encoding: [0x20,0x0c,0xf3,0xff]
+@ FIXME: vdup.16 d16, d16[1]            @ encoding: [0x20,0x0c,0xf6,0xff]
+@ FIXME: vdup.32 d16, d16[1]            @ encoding: [0x20,0x0c,0xfc,0xff]
+@ FIXME: vdup.8	q8, d16[1]              @ encoding: [0x60,0x0c,0xf3,0xff]
+@ FIXME: vdup.16 q8, d16[1]             @ encoding: [0x60,0x0c,0xf6,0xff]
+@ FIXME: vdup.32 q8, d16[1]             @ encoding: [0x60,0x0c,0xfc,0xff]
