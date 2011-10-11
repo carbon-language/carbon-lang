4f4aa1a03acb3d04747b8eab9beb8d5d59f36239
Lang Hames <lhames@gmail.com>
2011-10-11 00:52:51 +0000

    Fixed natural stack alignment for Linux x86-32. Thanks Eli.
    llvm-svn: 141617

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -1974,7 +1974,7 @@ public:
     LongDoubleAlign = 32;
     DescriptionString = "e-p:32:32:32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                         "i64:32:64-f32:32:32-f64:32:64-v64:64:64-v128:128:128-"
-                        "a0:0:64-f80:32:32-n8:16:32-S32";
+                        "a0:0:64-f80:32:32-n8:16:32-S128";
     SizeType = UnsignedInt;
     PtrDiffType = SignedInt;
     IntPtrType = SignedInt;
