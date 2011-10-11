ad018f10bcfc58e33dc52773b523360441adba2e
Kaelyn Uhrain <rikka@google.com>
2011-10-11 17:17:40 +0000

    Add an explanatory comment to test/SemaCXX/typo-correction.cpp
    llvm-svn: 141680

--- clang/test/SemaCXX/typo-correction.cpp
+++ clang/test/SemaCXX/typo-correction.cpp
@@ -15,8 +15,14 @@ public:
     : _val_(_val) {}
 
   template <class E>
-  error_condition(E _e)
-    {*this = make_error_condition(_e);}
+  error_condition(E _e) {
+    // make_error_condition must not be typo corrected to error_condition
+    // even though the first declaration of make_error_condition has not
+    // yet been encountered. This was a bug in the first version of the type
+    // name typo correction patch that wasn't noticed until building LLVM with
+    // Clang failed.
+    *this = make_error_condition(_e);
+  }
 
 };
 
