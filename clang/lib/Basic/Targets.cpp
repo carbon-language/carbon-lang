bfdbb25af277f381e0bdeb9fef59d3cf5edba9e7
David Meyer <pdox@google.com>
2011-10-11 03:12:01 +0000

    Allow regparm attribute for PNaCl target
    llvm-svn: 141638

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -3418,6 +3418,7 @@ public:
     this->SizeType = TargetInfo::UnsignedInt;
     this->PtrDiffType = TargetInfo::SignedInt;
     this->IntPtrType = TargetInfo::SignedInt;
+    this->RegParmMax = 2;
     DescriptionString = "e-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-"
                         "f32:32:32-f64:64:64-p:32:32:32-v128:32:32";
   }
