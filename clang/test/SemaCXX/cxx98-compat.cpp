465f069b933e3897863e854ec7516d256dee7fe1
Richard Smith <richard-llvm@metafoo.co.uk>
2011-10-13 22:45:21 +0000

    Convert newly-added test from -std=c++0x to -std=c++11.
    llvm-svn: 141904

--- clang/test/SemaCXX/cxx98-compat.cpp
+++ clang/test/SemaCXX/cxx98-compat.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -std=c++0x -Wc++98-compat -verify %s
+// RUN: %clang_cc1 -fsyntax-only -std=c++11 -Wc++98-compat -verify %s
 
 template<typename ...T>  // expected-warning {{variadic templates are incompatible with C++98}}
 class Variadic1 {};
