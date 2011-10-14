c0b9791b63d9485499537c93a5d56be988c07314
Rafael Espindola <rafael.espindola@gmail.com>
2011-10-14 19:50:08 +0000

    Fix the CXX_INCLUDE_ROOT case that was out of date with ScanLibDirForGCCTriple.
    llvm-svn: 141980

--- clang/lib/Driver/ToolChains.cpp
+++ clang/lib/Driver/ToolChains.cpp
@@ -1575,7 +1575,7 @@ public:
       GccInstallPath.append(CXX_INCLUDE_ARCH);
       GccInstallPath.append("/");
       GccInstallPath.append(Version);
-      GccParentLibPath = GccInstallPath + "/../../../..";
+      GccParentLibPath = GccInstallPath + "/../../..";
       IsValid = true;
       return;
     }
