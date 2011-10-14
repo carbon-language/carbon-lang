4e2bfe0770fdd8154f057552df142126d1f559ae
Akira Hatanaka <ahatanaka@mips.com>
2011-10-14 02:17:30 +0000

    Add more Mips relocation types.
    Patch by Jack Carter at Mips. 
    
    llvm-svn: 141932

--- llvm/lib/Target/Mips/MipsRelocations.h
+++ llvm/lib/Target/Mips/MipsRelocations.h
@@ -33,7 +33,27 @@ namespace llvm {
       reloc_mips_lo = 3,
 
       // reloc_mips_26 - lower 28 bits of the address, shifted right by 2.
-      reloc_mips_26 = 4
+      reloc_mips_26 = 4,
+
+      // I am starting here with the rest of the relocations because
+      // I have no idea if the above enumerations are assumed somewhere
+      // else
+      reloc_mips_16         =  6, // R_MIPS_16
+      reloc_mips_32         =  7, // R_MIPS_32
+      reloc_mips_rel32      =  8, // R_MIPS_REL32
+      reloc_mips_gprel16    = 10, // R_MIPS_GPREL16
+      reloc_mips_literal    = 12, // R_MIPS_LITERAL
+      reloc_mips_got16      = 13, // R_MIPS_GOT16
+      reloc_mips_call16     = 15, // R_MIPS_CALL16
+      reloc_mips_gprel32    = 17, // R_MIPS_GPREL32
+      reloc_mips_shift5     = 18, // R_MIPS_SHIFT5
+      reloc_mips_shift6     = 19, // R_MIPS_SHIFT6
+      reloc_mips_64         = 20, // R_MIPS_64
+      reloc_mips_tlsgd      = 21, // R_MIPS_TLS_GD
+      reloc_mips_gottprel   = 22, // R_MIPS_TLS_GOTTPREL
+      reloc_mips_tprel_hi   = 23, // R_MIPS_TLS_TPREL_HI16
+      reloc_mips_tprel_lo   = 24, // R_MIPS_TLS_TPREL_LO16
+      reloc_mips_branch_pcrel = 25 // This should become R_MIPS_PC16
     };
   }
 }
