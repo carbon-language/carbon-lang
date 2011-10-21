197ca87e7e4076bcf7ff8b920a64c02370f26061
Nick Lewycky <nicholas@mxc.ca>
2011-10-21 22:21:24 +0000

    Doug reports that this test is still failing. Try applying Benjamin Kramer's suggestion!
    llvm-svn: 142692

--- clang/test/Driver/debug.c
+++ clang/test/Driver/debug.c
@@ -1,6 +1,5 @@
 // RUN: cd %S && %clang -### -g %s -c 2>&1 | grep '"-fdebug-compilation-dir" "'%S'"'
-// RUN: (sv PWD=/foo && %clang -### -g %s -c 2>&1 | grep '"-fdebug-compilation-dir" "/foo"') || \
-// RUN: PWD=/foo %clang -### -g %s -c 2>&1 | grep '"-fdebug-compilation-dir" "/foo"'
+// RUN: env PWD=/foo %clang -### -g %s -c 2>&1 | grep '"-fdebug-compilation-dir" "/foo"'
 
 // This test uses grep instead of FileCheck so that we get %S -> dirname
 // substitution.
