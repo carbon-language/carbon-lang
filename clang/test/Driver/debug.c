c3afe1f8afbfd5527450f94fd34dc96ddf01ca7f
Douglas Gregor <dgregor@apple.com>
2011-10-21 23:37:41 +0000

    More speculative test tweaking
    llvm-svn: 142701

--- clang/test/Driver/debug.c
+++ clang/test/Driver/debug.c
@@ -2,10 +2,7 @@
 // CHECK-PWD: {{"-fdebug-compilation-dir" ".*Driver.*"}}
 
 // RUN: env PWD=/foo %clang -### -g %s -c 2>&1 | FileCheck -check-prefix=CHECK-FOO %s
-// CHECK-FOO: "-fdebug-compilation-dir" "/foo"
-
-// This test uses grep instead of FileCheck so that we get %S -> dirname
-// substitution.
+// CHECK-FOO: {{"-fdebug-compilation-dir" ".*foo"}}
 
 // "PWD=/foo gcc" wouldn't necessarily work. You would need to pick a different
 // path to the same directory (try a symlink).
