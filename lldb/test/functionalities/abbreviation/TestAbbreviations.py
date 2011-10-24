9593622b08c330a1685c087ac42802ddabdb2dde
Johnny Chen <johnny.chen@apple.com>
2011-10-24 18:24:54 +0000

    Fix test regressions due to the addition of 'display' alias to the top level commands, which conflicts with the original 'dis' -> 'disassemble' unique expansion.  Change it to 'disass' now.
    llvm-svn: 142825

--- lldb/test/functionalities/abbreviation/TestAbbreviations.py
+++ lldb/test/functionalities/abbreviation/TestAbbreviations.py
@@ -131,7 +131,7 @@ class AbbreviationsTestCase(TestBase):
 
         # ARCH, if not specified, defaults to x86_64.
         if self.getArchitecture() in ["", 'x86_64', 'i386']:
-            self.expect("dis -f",
+            self.expect("disass -f",
                         startstr = "a.out`sum(int, int)",
                         substrs = [' push',
                                    ' mov',
