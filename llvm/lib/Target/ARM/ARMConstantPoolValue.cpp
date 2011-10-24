f33bfbbace7bc561203ef43f5fdcb5a8544979c2
Evan Cheng <evan.cheng@apple.com>
2011-10-24 23:01:03 +0000

    ARMConstantPoolMBB::print should print BB number.
    llvm-svn: 142867

--- llvm/lib/Target/ARM/ARMConstantPoolValue.cpp
+++ llvm/lib/Target/ARM/ARMConstantPoolValue.cpp
@@ -315,5 +315,6 @@ void ARMConstantPoolMBB::addSelectionDAGCSEId(FoldingSetNodeID &ID) {
 }
 
 void ARMConstantPoolMBB::print(raw_ostream &O) const {
+  O << "BB#" << MBB->getNumber();
   ARMConstantPoolValue::print(O);
 }
