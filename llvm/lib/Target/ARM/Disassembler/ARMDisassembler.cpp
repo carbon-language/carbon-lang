8a6ebd085a07fc57a2e47c719b9145b1f1d1568a
Owen Anderson <resistor@mac.com>
2011-10-27 22:53:10 +0000

    Add some NEON stores to the VLD decoding hook that were accidentally omitted previously.
    llvm-svn: 143162

--- llvm/lib/Target/ARM/Disassembler/ARMDisassembler.cpp
+++ llvm/lib/Target/ARM/Disassembler/ARMDisassembler.cpp
@@ -2136,6 +2136,10 @@ static DecodeStatus DecodeVLDInstruction(llvm::MCInst &Inst, unsigned Insn,
   case ARM::VLD1d16wb_fixed:
   case ARM::VLD1d32wb_fixed:
   case ARM::VLD1d64wb_fixed:
+  case ARM::VLD1d8Twb_fixed:
+  case ARM::VLD1d16Twb_fixed:
+  case ARM::VLD1d32Twb_fixed:
+  case ARM::VLD1d64Twb_fixed:
   case ARM::VLD1d8wb_register:
   case ARM::VLD1d16wb_register:
   case ARM::VLD1d32wb_register:
