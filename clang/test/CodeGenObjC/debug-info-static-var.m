c64f4a075fec9d832a79e01fa0164db9a95f082c
Nick Lewycky <nicholas@mxc.ca>
2011-10-28 05:29:57 +0000

    Fix test for LLVM change r143186.
    llvm-svn: 143187

--- clang/test/CodeGenObjC/debug-info-static-var.m
+++ clang/test/CodeGenObjC/debug-info-static-var.m
@@ -3,9 +3,8 @@
 // Radar 8801045
 // Do not emit AT_MIPS_linkage_name for static variable i
 
-// CHECK:  DW_TAG_variable
-// CHECK-NEXT:        .byte   105                     ## DW_AT_name
-// CHECK-NEXT:        .byte   0
+// CHECK: Lset6 = Lstring3-Lsection_str           ## DW_AT_name
+// CHECK-NEXT: .long   Lset6
 // CHECK-NEXT:        DW_AT_type
 // CHECK-NEXT:        DW_AT_decl_file
 // CHECK-NEXT:        DW_AT_decl_line
