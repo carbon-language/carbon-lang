61200b31db2f6ce6fb295bd891a13b7e2cac3e7f
Johnny Chen <johnny.chen@apple.com>
2011-10-29 01:58:39 +0000

    Add a simple regression test to go with r143260. CommandInterpreter::PreprocessCommand() should not infinite loop when a target has not been specified yet.
    llvm-svn: 143274

---
+++ lldb/test/functionalities/backticks/TestBackticksWithoutATarget.py
@@ -0,0 +1,23 @@
+"""
+Test that backticks without a target should work (not infinite looping).
+"""
+
+import os, time
+import unittest2
+import lldb
+from lldbtest import *
+
+class BackticksWithNoTargetTestCase(TestBase):
+
+    mydir = "functionalities/backticks"
+
+    def test_backticks_no_target(self):
+        """A simple test of backticks without a target."""
+        self.expect("print `1+2-3`",
+            substrs = [' = 0'])
+
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
