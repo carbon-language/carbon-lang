300dcb8e379c96cc4f1ad7250edc03ee34deeca4
Rafael Espindola <rafael.espindola@gmail.com>
2011-10-31 17:23:09 +0000

    Move test to the X86 directory, note the PR number and only run MC once.
    llvm-svn: 143352

--- llvm/test/MC/AsmParser/2011-09-06-NoNewline.s
+++
@@ -1,6 +0,0 @@
-// RUN: llvm-mc -triple i386-unknown-unknown %s
-movl %gs:8, %eax
-// RUN: llvm-mc -triple i386-unknown-unknown %s
-movl %gs:8, %eax
-// RUN: llvm-mc -triple i386-unknown-unknown %s
-movl %gs:8, %eax
