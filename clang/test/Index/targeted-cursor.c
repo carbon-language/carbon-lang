04cf92fe7cd90c8145c144f9373eb8f3f7a1db1f
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-11-03 19:02:30 +0000

    [libclang] Make sure pointing inside the #include of the fields will return the struct.
    llvm-svn: 143638

--- clang/test/Index/targeted-cursor.c
+++ clang/test/Index/targeted-cursor.c
@@ -25,6 +25,11 @@ int LocalVar2;
 // RUN:    -Xclang -error-on-deserialized-decl=TopVar  \
 // RUN:  | FileCheck %s -check-prefix=FIELD-CURSOR1
 
+// RUN: env CINDEXTEST_FAILONERROR=1 c-index-test -cursor-at=%S/targeted-fields.h:1:1 %s -include %t.h \
+// RUN:    -Xclang -error-on-deserialized-decl=NestedVar1  \
+// RUN:    -Xclang -error-on-deserialized-decl=TopVar  \
+// RUN:  | FileCheck %s -check-prefix=FIELD-CURSOR2
+
 // RUN: env CINDEXTEST_FAILONERROR=1 CINDEXTEST_EDITING=1 CINDEXTEST_COMPLETION_NO_CACHING=1 \
 // RUN:   c-index-test -cursor-at=%s:5:10 %s -include %t.h \
 // RUN:    -Xclang -error-on-deserialized-decl=PreambleVar  \
@@ -55,4 +60,6 @@ int LocalVar2;
 // TOP-CURSOR1: VarDecl=TopVar:11:12
 // NESTED-CURSOR1: VarDecl=NestedVar1:2:12
 // PREAMBLE-CURSOR1: VarDecl=PreambleVar:2:12
+
 // FIELD-CURSOR1: FieldDecl=z:2:7 (Definition)
+// FIELD-CURSOR2: StructDecl=:13:9 (Definition)
