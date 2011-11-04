45124500bbeb2a51a8bf782cd05a1999cf1d227d
Devang Patel <dpatel@apple.com>
2011-11-04 17:12:03 +0000

    Add new test.
    llvm-svn: 143706

---
+++ clang/test/CodeGen/debug-info-block.c
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -fblocks -g -emit-llvm -o - %s | FileCheck %s
+// APPLE LOCAL file 5939894 */
+// Verify that the desired debugging type is generated for a structure
+//  member that is a pointer to a block. 
+
+// CHECK: __block_literal_generic{{.*}}DW_TAG_structure_type
+// CHECK: __block_descriptor{{.*}}DW_TAG_structure_type
+struct inStruct {
+  void (^genericBlockPtr)();
+} is;
+
