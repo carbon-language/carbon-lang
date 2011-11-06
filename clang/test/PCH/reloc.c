996cb8ad848639f382341d41c7549750cb7b0d4e
Chandler Carruth <chandlerc@gmail.com>
2011-11-06 06:59:15 +0000

    This test was assuming that /usr/include was in the system header search path. That assumption should never have been true, but it was until I fixed it. Now that its fixed, add a triple here to get correct behavior even on Windows.
    llvm-svn: 143863

--- clang/test/PCH/reloc.c
+++ clang/test/PCH/reloc.c
@@ -1,13 +1,13 @@
-// RUN: %clang --relocatable-pch -o %t -isysroot %S/libroot %S/libroot/usr/include/reloc.h
-// RUN: %clang -fsyntax-only -include-pch %t -isysroot %S/libroot %s -Xclang -verify
-// RUN: not %clang -include-pch %t %s
+// RUN: %clang -ccc-host-triple x86_64-apple-darwin10 --relocatable-pch -o %t \
+// RUN:   -isysroot %S/libroot %S/libroot/usr/include/reloc.h
+// RUN: %clang -ccc-host-triple x86_64-apple-darwin10 -fsyntax-only \
+// RUN:   -include-pch %t -isysroot %S/libroot %s -Xclang -verify
+// RUN: not %clang -ccc-host-triple x86_64-apple-darwin10 -include-pch %t %s
 
 #include <reloc.h>
 
 int x = 2; // expected-error{{redefinition}}
 int y = 5; // expected-error{{redefinition}}
-
-
 
 
 // expected-note{{previous definition}}
