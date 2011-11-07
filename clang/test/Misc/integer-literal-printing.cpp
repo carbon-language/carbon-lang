09d3af4c2b8e1bb96d4898638b4a14a59cc6d305
Richard Trieu <rtrieu@google.com>
2011-11-07 19:30:35 +0000

    Remove test with int128 printing since it breaks on some platforms.
    llvm-svn: 143997

--- clang/test/Misc/integer-literal-printing.cpp
+++ clang/test/Misc/integer-literal-printing.cpp
@@ -7,15 +7,7 @@ template <short T> void Function1(Type1<T>& x) {} // expected-note{{candidate fu
 template <unsigned short T> class Type2 {};
 template <unsigned short T> void Function2(Type2<T>& x) {} // expected-note{{candidate function [with T = 42] not viable: no known conversion from 'Type2<42>' to 'Type2<42> &' for 1st argument;}}
 
-template <__int128_t T> class Type3 {};
-template <__int128_t T> void Function3(Type3<T>& x) {} // expected-note{{candidate function [with T = -42] not viable: no known conversion from 'Type3<-42>' to 'Type3<-42i128> &' for 1st argument;}}
-
-template <__uint128_t T> class Type4 {};
-template <__uint128_t T> void Function4(Type4<T>& x) {} // expected-note{{candidate function [with T = 42] not viable: no known conversion from 'Type4<42>' to 'Type4<42Ui128> &' for 1st argument;}}
-
 void Function() {
   Function1(Type1<-42>()); // expected-error{{no matching function for call to 'Function1'}}
   Function2(Type2<42>()); // expected-error{{no matching function for call to 'Function2'}}
-  Function3(Type3<-42>()); // expected-error{{no matching function for call to 'Function3'}}
-  Function4(Type4<42>()); // expected-error{{no matching function for call to 'Function4'}}
 }
