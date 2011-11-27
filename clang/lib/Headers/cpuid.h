fd03d0b733f76492664778ba669fb161855d0594
Rafael Espindola <rafael.espindola@gmail.com>
2011-11-27 15:13:54 +0000

    Fix file name in comments.
    llvm-svn: 145184

--- clang/lib/Headers/cpuid.h
+++ clang/lib/Headers/cpuid.h
@@ -1,4 +1,4 @@
-/*===---- stddef.h - Basic type definitions --------------------------------===
+/*===---- cpuid.h - Basic type definitions ---------------------------------===
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
