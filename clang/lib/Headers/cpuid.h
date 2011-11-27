0618d14edfd7a9e3d2f2e1db371b28601c2315c4
Rafael Espindola <rafael.espindola@gmail.com>
2011-11-27 15:21:33 +0000

    Error on non x86 architectures.
    llvm-svn: 145185

--- clang/lib/Headers/cpuid.h
+++ clang/lib/Headers/cpuid.h
@@ -21,6 +21,10 @@
  *===-----------------------------------------------------------------------===
  */
 
+#if !(__x86_64__ || __i386__)
+#error this header is for x86 only
+#endif
+
 static inline int __get_cpuid (unsigned int level, unsigned int *eax,
                                unsigned int *ebx, unsigned int *ecx,
                                unsigned int *edx) {
