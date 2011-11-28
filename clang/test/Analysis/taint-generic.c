ff029b695397697090a054bc0f58607a03212be3
Anna Zaks <ganna@apple.com>
2011-11-28 20:43:40 +0000

    [analyzer] Add more simple taint tests.
    llvm-svn: 145275

--- clang/test/Analysis/taint-generic.c
+++ clang/test/Analysis/taint-generic.c
@@ -6,7 +6,7 @@ int getchar(void);
 #define BUFSIZE 10
 
 int Buffer[BUFSIZE];
-void bufferFoo1(void)
+void bufferScanfDirect(void)
 {
   int n;
   scanf("%d", &n);
@@ -23,11 +23,26 @@ void bufferScanfArithmetic1(int x) {
 void bufferScanfArithmetic2(int x) {
   int n;
   scanf("%d", &n);
-  int m = (n + 3) * x;
+  int m = 100 / (n + 3) * x;
   Buffer[m] = 1; // expected-warning {{Out of bound memory access }}
+}
+
+void bufferScanfAssignment(int x) {
+  int n;
+  scanf("%d", &n);
+  int m;
+  if (x > 0) {
+    m = n;
+    Buffer[m] = 1; // expected-warning {{Out of bound memory access }}
+  }
 }
 
 void scanfArg() {
   int t;
   scanf("%d", t); // expected-warning {{Pointer argument is expected}}
+}
+
+void bufferGetchar(int x) {
+  int m = getchar();
+  Buffer[m] = 1;  //expected-warning {{Out of bound memory access }}
 }
