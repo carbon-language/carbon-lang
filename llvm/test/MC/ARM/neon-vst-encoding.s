2a9c43649af21c735d18d08dc12be5b98d7b1ae7
Jim Grosbach <grosbach@apple.com>
2011-11-29 22:40:32 +0000

    Enable some VST1 tests and add a few more.
    llvm-svn: 145443

--- llvm/test/MC/ARM/neon-vst-encoding.s
+++ llvm/test/MC/ARM/neon-vst-encoding.s
@@ -1,22 +1,28 @@
 @ RUN: llvm-mc -mcpu=cortex-a8 -triple armv7-apple-darwin -show-encoding < %s | FileCheck %s
 
 	vst1.8	{d16}, [r0, :64]
-@	vst1.16	{d16}, [r0]
-@	vst1.32	{d16}, [r0]
-@	vst1.64	{d16}, [r0]
-@	vst1.8	{d16, d17}, [r0, :64]
-@	vst1.16	{d16, d17}, [r0, :128]
-@	vst1.32	{d16, d17}, [r0]
-@	vst1.64	{d16, d17}, [r0]
+	vst1.16	{d16}, [r0]
+	vst1.32	{d16}, [r0]
+	vst1.64	{d16}, [r0]
+	vst1.8	{d16, d17}, [r0, :64]
+	vst1.16	{d16, d17}, [r0, :128]
+	vst1.32	{d16, d17}, [r0]
+	vst1.64	{d16, d17}, [r0]
+        vst1.8  {d16, d17, d18}, [r0, :64]
+        vst1.8  {d16, d17, d18}, [r0, :64]!
+        vst1.8  {d16, d17, d18}, [r0], r3
 
 @ CHECK: vst1.8	{d16}, [r0, :64]        @ encoding: [0x1f,0x07,0x40,0xf4]
-@ FIXME: vst1.16 {d16}, [r0]            @ encoding: [0x4f,0x07,0x40,0xf4]
-@ FIXME: vst1.32 {d16}, [r0]            @ encoding: [0x8f,0x07,0x40,0xf4]
-@ FIXME: vst1.64 {d16}, [r0]            @ encoding: [0xcf,0x07,0x40,0xf4]
-@ FIXME: vst1.8	{d16, d17}, [r0, :64]   @ encoding: [0x1f,0x0a,0x40,0xf4]
-@ FIXME: vst1.16 {d16, d17}, [r0, :128] @ encoding: [0x6f,0x0a,0x40,0xf4]
-@ FIXME: vst1.32 {d16, d17}, [r0]       @ encoding: [0x8f,0x0a,0x40,0xf4]
-@ FIXME: vst1.64 {d16, d17}, [r0]       @ encoding: [0xcf,0x0a,0x40,0xf4]
+@ CHECK: vst1.16 {d16}, [r0]            @ encoding: [0x4f,0x07,0x40,0xf4]
+@ CHECK: vst1.32 {d16}, [r0]            @ encoding: [0x8f,0x07,0x40,0xf4]
+@ CHECK: vst1.64 {d16}, [r0]            @ encoding: [0xcf,0x07,0x40,0xf4]
+@ CHECK: vst1.8	{d16, d17}, [r0, :64]   @ encoding: [0x1f,0x0a,0x40,0xf4]
+@ CHECK: vst1.16 {d16, d17}, [r0, :128] @ encoding: [0x6f,0x0a,0x40,0xf4]
+@ CHECK: vst1.32 {d16, d17}, [r0]       @ encoding: [0x8f,0x0a,0x40,0xf4]
+@ CHECK: vst1.64 {d16, d17}, [r0]       @ encoding: [0xcf,0x0a,0x40,0xf4]
+@ CHECK: vst1.8	{d16, d17, d18}, [r0, :64] @ encoding: [0x1f,0x06,0x40,0xf4]
+@ CHECK: vst1.8	{d16, d17, d18}, [r0, :64]! @ encoding: [0x1d,0x06,0x40,0xf4]
+@ CHECK: vst1.8	{d16, d17, d18}, [r0], r3 @ encoding: [0x03,0x06,0x40,0xf4]
 
 
 @	vst2.8	{d16, d17}, [r0, :64]
