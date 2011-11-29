c36d6a1696eb3654132ebc49451e98acb52cbda2
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2011-11-29 05:48:01 +0000

    [libclang] Make the test portable.
    llvm-svn: 145377

--- clang/test/Index/file-macro-refs.c
+++ clang/test/Index/file-macro-refs.c
@@ -4,7 +4,7 @@ FOO
 FOO
 
 // RUN: c-index-test -file-refs-at=%s:3:2 %s | FileCheck %s
-// RUN: CINDEXTEST_EDITING=1 c-index-test -file-refs-at=%s:3:2 %s | FileCheck %s
+// RUN: env CINDEXTEST_EDITING=1 c-index-test -file-refs-at=%s:3:2 %s | FileCheck %s
 
 // CHECK:      macro expansion=FOO:1:9
 // CHECK-NEXT: macro definition=FOO =[1:9 - 1:12]
