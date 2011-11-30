d8bcb2b808f65eca7db7ce8f9945ecfb934e41e4
Lang Hames <lhames@gmail.com>
2011-11-30 23:03:25 +0000

    Test case for fix committed in r145441.
    llvm-svn: 145534

--- clang/test/Index/complete-recovery.m
+++ clang/test/Index/complete-recovery.m
@@ -23,6 +23,9 @@
 // CHECK-CC1: VarDecl:{ResultType A *}{TypedText a}
 // CHECK-CC1: NotImplemented:{ResultType size_t}{TypedText sizeof}{LeftParen (}{Placeholder expression-or-type}{RightParen )}
 
+// Test case for fix comitted in r145441. 
+// RUN: env CINDEXTEST_CODE_COMPLETE_PATTERNS=1 c-index-test -code-completion-at=%s:9:20 %s -fms-compatibility | FileCheck -check-prefix=CHECK-CC1 %s
+
 // RUN: env CINDEXTEST_CODE_COMPLETE_PATTERNS=1 c-index-test -code-completion-at=%s:10:24 %s | FileCheck -check-prefix=CHECK-CC2 %s
 // CHECK-CC2: NotImplemented:{ResultType char[]}{TypedText @encode}{LeftParen (}{Placeholder type-name}{RightParen )}
 // CHECK-CC2: NotImplemented:{TypedText _Bool}
