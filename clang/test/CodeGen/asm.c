5037249cc317988742b14862ce4244e3600cdfb5
Eric Christopher <echristo@apple.com>
2011-12-02 02:12:12 +0000

    Clean up some oddly gratuitous vertical whitespace.
    llvm-svn: 145670

--- clang/test/CodeGen/asm.c
+++ clang/test/CodeGen/asm.c
@@ -63,7 +63,6 @@ void t10(int r) {
 // CHECK:PR3908 $1 $3 $2 $0
 }
 
-
 // PR3373
 unsigned t11(signed char input) {
   unsigned  output;
@@ -101,9 +100,6 @@ unsigned long t15(int x, struct large *P) {
   return x;
 }
 
-
-
-
 // bitfield destination of an asm.
 struct S {
   int a : 4;
@@ -112,7 +108,6 @@ struct S {
 void t14(struct S *P) {
   __asm__("abc %0" : "=r"(P->a) );
 }
-
 
 // PR4938
 int t16() {
@@ -145,7 +140,6 @@ int t18(unsigned data) {
 // CHECK-NEXT: extractvalue
 }
 
-
 // PR6780
 int t19(unsigned data) {
   int a, b;
@@ -155,7 +149,6 @@ int t19(unsigned data) {
   // CHECK: t19(i32
   // CHECK: = call {{.*}}asm "x$(abc$|def$|ghi$)z"
 }
-
 
 // PR6845 - Mismatching source/dest fp types.
 double t20(double x) {
@@ -208,7 +201,6 @@ void *t24(char c) {
   __asm__ ("foobar" : "=a" (addr) : "0" (c));
   return addr;
 }
-
 
 // PR10299 - fpsr, fpcr
 void t25(void)
