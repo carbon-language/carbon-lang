1ad92487ff566d2dee36e7911da6db506993fda7
Daniel Dunbar <daniel@zuster.org>
2011-12-06 18:25:42 +0000

    platform/clang_linux: Add profile and asan libs on x86.
    llvm-svn: 145950

--- compiler-rt/make/platform/clang_linux.mk
+++ compiler-rt/make/platform/clang_linux.mk
@@ -33,6 +33,26 @@ Configs += full-x86_64
 Arch.full-x86_64 := x86_64
 endif
 
+# Configuration for profile runtime.
+ifeq ($(CompilerTargetArch),i386)
+Configs += profile-i386
+Arch.profile-i386 := i386
+endif
+ifeq ($(CompilerTargetArch),x86_64)
+Configs += profile-x86_64
+Arch.profile-x86_64 := x86_64
+endif
+
+# Configuration for ASAN runtime.
+ifeq ($(CompilerTargetArch),i386)
+Configs += asan-i386
+Arch.asan-i386 := i386
+endif
+ifeq ($(CompilerTargetArch),x86_64)
+Configs += asan-x86_64
+Arch.asan-x86_64 := x86_64
+endif
+
 endif
 
 ###
@@ -41,9 +61,17 @@ CFLAGS := -Wall -Werror -O3 -fomit-frame-pointer
 
 CFLAGS.full-i386 := $(CFLAGS) -m32
 CFLAGS.full-x86_64 := $(CFLAGS) -m64
+CFLAGS.profile-i386 := $(CFLAGS) -m32
+CFLAGS.profile-x86_64 := $(CFLAGS) -m64
+CFLAGS.asan-i386 := $(CFLAGS) -m32
+CFLAGS.asan-x86_64 := $(CFLAGS) -m64
 
 FUNCTIONS.full-i386 := $(CommonFunctions) $(ArchFunctions.i386)
 FUNCTIONS.full-x86_64 := $(CommonFunctions) $(ArchFunctions.x86_64)
+FUNCTIONS.profile-i386 := GCDAProfiling
+FUNCTIONS.profile-x86_64 := GCDAProfiling
+FUNCTIONS.asan-i386 := $(AsanFunctions)
+FUNCTIONS.asan-x86_64 := $(AsanFunctions)
 
 # Always use optimized variants.
 OPTIMIZED := 1
