158764bbe29d76b98775d9f0057c9f02e9347a65
Douglas Gregor <dgregor@apple.com>
2011-12-06 20:51:42 +0000

    Rename Objective-C-only tests to .m
    llvm-svn: 145959

--- clang/test/Modules/auto-module-import.m
+++ clang/test/Modules/auto-module-import.m
@@ -1,6 +1,6 @@
 
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -x objective-c -Wauto-import -fmodule-cache-path %t -fauto-module-import -F %S/Inputs %s -verify
+// RUN: %clang_cc1 -Wauto-import -fmodule-cache-path %t -fauto-module-import -F %S/Inputs %s -verify
 
 #include <DependsOnModule/DependsOnModule.h> // expected-warning{{treating #include as an import of module 'DependsOnModule'}}
 
