6e09995159ff2c133edb0b663da0879a0696d1c7
Daniel Dunbar <daniel@zuster.org>
2011-12-08 21:50:03 +0000

    SDK/linux: Fix braindead pasto, caught by Matt Beaumont-Gay.
    llvm-svn: 146188

--- compiler-rt/SDKs/linux/usr/include/sys/mman.h
+++ compiler-rt/SDKs/linux/usr/include/sys/mman.h
@@ -20,7 +20,7 @@
 typedef __SIZE_TYPE__ size_t;
 
 #define PROT_READ 0x1
-#define PROT_WRITE 0x1
+#define PROT_WRITE 0x2
 #define PROT_EXEC 0x4
 
 extern int mprotect (void *__addr, size_t __len, int __prot)
