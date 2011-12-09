9b54a3e297f9035e2e23f26d0a68bc4115c90ee0
Michael J. Spencer <bigcheesegs@gmail.com>
2011-12-09 01:14:41 +0000

    unittests/SupportTests: Fix test. pop modifies the current entry, thus the dontlookhere check must be after it.
    llvm-svn: 146217

--- llvm/unittests/Support/Path.cpp
+++ llvm/unittests/Support/Path.cpp
@@ -241,13 +241,10 @@ TEST_F(FileSystemTest, DirectoryIteration) {
   for (fs::recursive_directory_iterator i(Twine(TestDirectory)
          + "/recursive", ec), e; i != e; i.increment(ec)){
     ASSERT_NO_ERROR(ec);
-    if (path::filename(i->path()) == "dontlookhere")
-      i.no_push();
-    outs() << "pre-pop: " << path::filename(i->path()) << "\n";
     if (path::filename(i->path()) == "p1")
       i.pop();
-    outs() << "post-pop: " << path::filename(i->path()) << "\n";
-    outs().flush();
+    if (path::filename(i->path()) == "dontlookhere")
+      i.no_push();
     visited.push_back(path::filename(i->path()));
   }
   v_t::const_iterator a0 = std::find(visited.begin(), visited.end(), "a0");
