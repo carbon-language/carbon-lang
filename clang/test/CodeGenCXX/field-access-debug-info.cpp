f43e681ed7170770abf9ea21a7ca3afefcd73b7b
Nick Lewycky <nicholas@mxc.ca>
2011-12-13 05:09:22 +0000

    Testcase for LLVM commit r146475.
    llvm-svn: 146476

--- clang/test/CodeGenCXX/field-access-debug-info.cpp
+++ clang/test/CodeGenCXX/field-access-debug-info.cpp
@@ -1,5 +1,8 @@
-// RUN: %clang_cc1 -g -S -masm-verbose -o %t %s
-// RUN: grep DW_AT_accessibility %t
+// RUN: %clang_cc1 -g -S -masm-verbose -o - %s | FileCheck %s
+
+// CHECK: abbrev_begin:
+// CHECK: DW_AT_accessibility
+// CHECK-NEXT: DW_FORM_data1
 
 class A {
 public:
