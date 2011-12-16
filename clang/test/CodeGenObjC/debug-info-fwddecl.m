fbfd303dc4fff9b51b02f66b8c1df5b74a187bc3
Devang Patel <dpatel@apple.com>
2011-12-16 17:56:38 +0000

    Update tests to match r146744.
    llvm-svn: 146745

--- clang/test/CodeGenObjC/debug-info-fwddecl.m
+++ clang/test/CodeGenObjC/debug-info-fwddecl.m
@@ -2,4 +2,4 @@
 @class ForwardObjcClass;
 ForwardObjcClass *ptr = 0;
 
-// CHECK: !8 = metadata !{i32 720915, metadata !6, metadata !"ForwardObjcClass", metadata !6, i32 2, i64 0, i64 0, i32 0, i32 4, i32 0, null, i32 16, i32 0} ; [ DW_TAG_structure_type ]
+// CHECK: !8 = metadata !{i32 720915, metadata !6, metadata !"ForwardObjcClass", metadata !6, i32 2, i64 0, i64 0, i32 0, i32 4, null, null, i32 16, i32 0} ; [ DW_TAG_structure_type ]
