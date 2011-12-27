c2c47f90a00026a8c518995aec28e9c744d9cfd1
Benjamin Kramer <benny.kra@googlemail.com>
2011-12-27 11:15:04 +0000

    Initialize member that ends up in PCH files.
    llvm-svn: 147288

--- clang/lib/Serialization/Module.cpp
+++ clang/lib/Serialization/Module.cpp
@@ -30,7 +30,7 @@ ModuleFile::ModuleFile(ModuleKind Kind)
     PreprocessedEntityOffsets(0), NumPreprocessedEntities(0),
     LocalNumHeaderFileInfos(0), 
     HeaderFileInfoTableData(0), HeaderFileInfoTable(0),
-    HeaderFileFrameworkStrings(0), LocalNumSubmodules(0),
+    HeaderFileFrameworkStrings(0), LocalNumSubmodules(0), BaseSubmoduleID(0),
     LocalNumSelectors(0), SelectorOffsets(0), BaseSelectorID(0),
     SelectorLookupTableData(0), SelectorLookupTable(0), LocalNumDecls(0),
     DeclOffsets(0), BaseDeclID(0),
