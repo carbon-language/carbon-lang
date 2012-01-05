8ff6bc924fbff00b63b84a8bd00cb0fd85265087
Chris Lattner <sabre@nondot.org>
2012-01-05 17:35:07 +0000

    allow clients of SmallSet to specify their own comparison function for the set. Patch by Stepan Dyatkovskiy!
    llvm-svn: 147607

--- llvm/include/llvm/ADT/SmallSet.h
+++ llvm/include/llvm/ADT/SmallSet.h
@@ -27,13 +27,13 @@ namespace llvm {
 ///
 /// Note that this set does not provide a way to iterate over members in the
 /// set.
-template <typename T, unsigned N>
+template <typename T, unsigned N,  typename C = std::less<T> >
 class SmallSet {
   /// Use a SmallVector to hold the elements here (even though it will never
   /// reach its 'large' stage) to avoid calling the default ctors of elements
   /// we will never use.
   SmallVector<T, N> Vector;
-  std::set<T> Set;
+  std::set<T, C> Set;
   typedef typename SmallVector<T, N>::const_iterator VIterator;
   typedef typename SmallVector<T, N>::iterator mutable_iterator;
 public:
