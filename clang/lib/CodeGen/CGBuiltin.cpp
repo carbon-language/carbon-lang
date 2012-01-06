df88c54f8d468f7a2880cf018639f9be8a636aea
Eli Friedman <eli.friedman@gmail.com>
2012-01-06 20:03:09 +0000

    Revert r147655; it's breaking the compiler_rt build on OSX.
    llvm-svn: 147677

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -175,8 +175,7 @@ RValue CodeGenFunction::EmitBuiltinExpr(const FunctionDecl *FD,
                                         unsigned BuiltinID, const CallExpr *E) {
   // See if we can constant fold this builtin.  If so, don't emit it at all.
   Expr::EvalResult Result;
-  if (!getContext().getLangOptions().NoBuiltin &&
-      E->EvaluateAsRValue(Result, CGM.getContext()) &&
+  if (E->EvaluateAsRValue(Result, CGM.getContext()) &&
       !Result.hasSideEffects()) {
     if (Result.Val.isInt())
       return RValue::get(llvm::ConstantInt::get(getLLVMContext(),
