60bb82a584c64ab29cec69d2f80497995a192602
Fariborz Jahanian <fjahanian@apple.com>
2012-01-16 17:52:22 +0000

    Patch for r148243 which was left behind.
    llvm-svn: 148244

---
+++ clang/test/CodeGenCXX/switch-case-folding.cpp
@@ -0,0 +1,12 @@
+// RUN: %clang_cc1 %s -emit-llvm-only
+// CHECK that we don't crash.
+
+int main(void){
+	int x = 12;
+	// Make sure we don't crash when constant folding the case 4
+	// statement due to the case 5 statement contained in the do loop
+	switch (4) {
+		case 4: do { case 5: x++;} while (x < 100);
+	}
+	return x;
+}
