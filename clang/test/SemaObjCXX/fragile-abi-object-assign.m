3c4225a8581f0b865fd0de3125d5f1a195febe72
Fariborz Jahanian <fjahanian@apple.com>
2012-01-24 18:05:45 +0000

    objc: issue error if assigning objects in fragile-abi too. // rdar://10731065
    llvm-svn: 148823

---
+++ clang/test/SemaObjCXX/fragile-abi-object-assign.m
@@ -0,0 +1,13 @@
+// RUN: %clang_cc1 -triple x86_64-apple-darwin11 -fsyntax-only -fobjc-fragile-abi -verify %s
+// rdar://10731065
+
+@interface MyView {}
+@end
+
+@implementation MyViewTemplate // expected-warning {{cannot find interface declaration for 'MyViewTemplate'}}
+- (id) createRealObject {
+  id realObj;
+  *(MyView *) realObj = *(MyView *) self; // expected-error {{cannot assign to class object - use memcpy instead}}
+}
+@end
+
