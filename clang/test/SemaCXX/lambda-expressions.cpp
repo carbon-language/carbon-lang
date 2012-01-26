f49643ddced10e607ca1c2405d230d1a6769b4a2
Eli Friedman <eli.friedman@gmail.com>
2012-01-26 03:16:41 +0000

    Add an additional testcase for a lambda with implicit void return type.
    llvm-svn: 149034

--- clang/test/SemaCXX/lambda-expressions.cpp
+++ clang/test/SemaCXX/lambda-expressions.cpp
@@ -61,6 +61,7 @@ namespace ReturnDeduction {
     [](){ return ({return 'c'; 1;}); }; // expected-error {{not supported yet}} expected-error {{must match previous return type}}
     []()->int{ return 'c'; return 1; }; // expected-error {{not supported yet}}
     [](){ return 'c'; return 1; }; // expected-error {{not supported yet}} expected-error {{must match previous return type}}
+    []() { return; return (void)0; }; // expected-error {{not supported yet}}
     // FIXME: Need to check structure of lambda body 
     [](){ return 1; return 1; }; // expected-error {{not supported yet}}
   }
