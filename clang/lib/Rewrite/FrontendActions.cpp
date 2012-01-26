e67bb67a9b8986411683269d6520b49ad339d32f
Douglas Gregor <dgregor@apple.com>
2012-01-26 03:20:14 +0000

    Add missing include of <unistd.h>
    llvm-svn: 149035

--- clang/lib/Rewrite/FrontendActions.cpp
+++ clang/lib/Rewrite/FrontendActions.cpp
@@ -23,6 +23,8 @@
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/FileSystem.h"
+#include <unistd.h>
+
 using namespace clang;
 
 //===----------------------------------------------------------------------===//
