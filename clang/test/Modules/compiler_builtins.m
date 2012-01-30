56435b49e0b137fe33445779342037c0e24c3fab
Douglas Gregor <dgregor@apple.com>
2012-01-30 22:22:39 +0000

    Remove tgmath.h from the module map for now, because it currently causes a cyclic module dependency due to its inclusion of math.h and complex.h. I'll take another shot at it later.
    llvm-svn: 149283

--- clang/test/Modules/compiler_builtins.m
+++ clang/test/Modules/compiler_builtins.m
@@ -1,9 +1,6 @@
 // RUN: rm -rf %t
 // RUN: %clang -fsyntax-only -fmodules -fmodule-cache-path %t %s -Xclang -verify
 
-// Note: disable test on MSVC, which doesn't have <complex.h>.
-// REQUIRES: shell
-
 @import __compiler_builtins.float_constants;
 
 float getFltMax() { return FLT_MAX; }
