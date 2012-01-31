232e3431e2a4b72cbf2c2863323f4110ab803e08
Douglas Gregor <dgregor@apple.com>
2012-01-31 21:57:50 +0000

    Split compiler builtin module into "stdlib" builtins and "intrinsic" builds, and bring mm_alloc.h into the fold. Start playing some tricks with these builtin modules to mirror the include_next tricks that the headers already perform.
    llvm-svn: 149434

--- clang/test/Modules/compiler_builtins.m
+++ clang/test/Modules/compiler_builtins.m
@@ -1,20 +1,24 @@
 // RUN: rm -rf %t
 // RUN: %clang -fsyntax-only -fmodules -fmodule-cache-path %t %s -Xclang -verify
 
-@import __compiler_builtins.float_constants;
+@import _Builtin_stdlib.float_constants;
 
 float getFltMax() { return FLT_MAX; }
 
-@import __compiler_builtins.limits;
+@import _Builtin_stdlib.limits;
 
 char getCharMax() { return CHAR_MAX; }
 
 size_t size; // expected-error{{unknown type name 'size_t'}}
 
+@import _Builtin_stdlib.stdint;
+
+intmax_t value;
+
 #ifdef __SSE__
-@import __compiler_builtins.intel.sse;
+@import _Builtin_intrinsics.intel.sse;
 #endif
 
 #ifdef __AVX2__
-@import __compiler_builtins.intel.avx2;
+@import _Builtin_intrinsics.intel.avx2;
 #endif
