35d19c0f1cac619ddc560fd67965bd245d67a840
NAKAMURA Takumi <geek4civic@gmail.com>
2012-02-16 08:12:24 +0000

    VMCore/AsmWriter.cpp: Tweak to check #INF and #NAN earlier.
    With MSVCRT, prior checker missed emission of #INF and #NAN.
    
    FIXME: Checking should be simpler.
    llvm-svn: 150667

--- llvm/lib/VMCore/AsmWriter.cpp
+++ llvm/lib/VMCore/AsmWriter.cpp
@@ -719,7 +719,9 @@ static void WriteConstantInternal(raw_ostream &Out, const Constant *CV,
       bool ignored;
       bool isHalf = &CFP->getValueAPF().getSemantics()==&APFloat::IEEEhalf;
       bool isDouble = &CFP->getValueAPF().getSemantics()==&APFloat::IEEEdouble;
-      if (!isHalf) {
+      bool isInf = CFP->getValueAPF().isInfinity();
+      bool isNaN = CFP->getValueAPF().isNaN();
+      if (!isHalf && !isInf && !isNaN) {
         double Val = isDouble ? CFP->getValueAPF().convertToDouble() :
                                 CFP->getValueAPF().convertToFloat();
         SmallString<128> StrVal;
