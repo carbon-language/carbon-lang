96de9933fbd86bd83ebf43be85c1f606f298f374
David Chisnall <csdavec@swan.ac.uk>
2012-02-16 16:00:47 +0000

    Tweak link order on Solaris so that global ctors work.
    llvm-svn: 150696

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -4252,7 +4252,6 @@ void solaris::Link::ConstructJob(Compilation &C, const JobAction &JA,
     } else {
       CmdArgs.push_back(Args.MakeArgString(LibPath + "crti.o"));
     }
-    CmdArgs.push_back(Args.MakeArgString(LibPath + "crtn.o"));
   }
 
   CmdArgs.push_back(Args.MakeArgString("-L" + GCCLibPath));
@@ -4277,6 +4276,7 @@ void solaris::Link::ConstructJob(Compilation &C, const JobAction &JA,
     if (!Args.hasArg(options::OPT_shared))
       CmdArgs.push_back(Args.MakeArgString(GCCLibPath + "crtend.o"));
   }
+  CmdArgs.push_back(Args.MakeArgString(LibPath + "crtn.o"));
 
   addProfileRT(getToolChain(), Args, CmdArgs, getToolChain().getTriple());
 
