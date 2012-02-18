c616e95162d8048859cb9810c793cbd3d0fa9d42
David Chisnall <csdavec@swan.ac.uk>
2012-02-18 17:00:56 +0000

    Default to not using __cxa_atexit on Solaris.
    llvm-svn: 150883

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -2079,6 +2079,7 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
     !Args.hasFlag(options::OPT_fuse_cxa_atexit, options::OPT_fno_use_cxa_atexit,
                   getToolChain().getTriple().getOS() != llvm::Triple::Cygwin &&
                   getToolChain().getTriple().getOS() != llvm::Triple::MinGW32 &&
+                  getToolChain().getTriple().getOS() != llvm::Triple::Solaris &&
                   getToolChain().getTriple().getArch() !=
                   llvm::Triple::hexagon))
     CmdArgs.push_back("-fno-use-cxa-atexit");
