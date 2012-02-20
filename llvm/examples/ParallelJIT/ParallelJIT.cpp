3bb7d41a35f2476a9aa08043d0c06a5dd10f877e
Chandler Carruth <chandlerc@gmail.com>
2012-02-20 00:02:49 +0000

    Switch to a more idiomatic way of silencing unused variable warnings in release builds. Silences clang's -Wself-assign.
    llvm-svn: 150942

--- llvm/examples/ParallelJIT/ParallelJIT.cpp
+++ llvm/examples/ParallelJIT/ParallelJIT.cpp
@@ -209,7 +209,8 @@ private:
     waitFor = 0;
 
     int result = pthread_cond_broadcast( &condition );
-    assert(result == 0); result=result;
+    (void)result;
+    assert(result == 0);
   }
 
   size_t n;
