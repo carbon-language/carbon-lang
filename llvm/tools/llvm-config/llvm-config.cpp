f320be852125ba42962dd5be2834b6d089a58cc2
Duncan Sands <baldrick@free.fr>
2012-02-23 08:25:25 +0000

    An easy case where GCC should really be able to work out that the value is only used if IsInDevelopmentTree is 'true'.  But it doesn't, so help it out.
    llvm-svn: 151244

--- llvm/tools/llvm-config/llvm-config.cpp
+++ llvm/tools/llvm-config/llvm-config.cpp
@@ -206,6 +206,7 @@ int main(int argc, char **argv) {
     ActiveObjRoot = LLVM_OBJ_ROOT;
   } else {
     IsInDevelopmentTree = false;
+    DevelopmentTreeLayout = MakefileStyle; // Initialized to avoid warnings.
   }
 
   // Compute various directory locations based on the derived location
