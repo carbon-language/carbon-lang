7e75de84bd06fe81b92c445538709f1be78b35fe
Fariborz Jahanian <fjahanian@apple.com>
2012-02-24 18:17:50 +0000

    test for writing modern ivar of struct type.
    llvm-svn: 151378

---
+++ clang/test/Rewriter/rewrite-modern-struct-ivar.mm
@@ -0,0 +1,24 @@
+// RUN: %clang_cc1 -E %s -o %t.mm
+// RUN: %clang_cc1 -fblocks -rewrite-objc -fms-extensions %t.mm -o %t-rw.cpp 
+// RUN: FileCheck --input-file=%t-rw.cpp %s
+// RUN: %clang_cc1 -fsyntax-only -Wno-address-of-temporary -D"Class=void*" -D"id=void*" -D"SEL=void*" -D"__declspec(X)=" %t-rw.cpp
+
+struct S {
+    int i1;
+    double d1;
+    void (^block1)();
+};
+
+@interface I
+{
+  struct S struct_ivar;
+
+  struct S *pstruct_ivar;
+}
+@end
+
+@implementation I
+- (struct S) dMeth{ return struct_ivar; }
+@end
+  
+// CHECK: return (*(struct S *)((char *)self + OBJC_IVAR_$_I_struct_ivar));
