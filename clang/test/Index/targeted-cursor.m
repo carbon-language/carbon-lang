b260a2ea2a4803bc871d30639c081d1d7eb8e6cd
David Blaikie <dblaikie@gmail.com>
2012-02-24 04:20:32 +0000

    Fix test for non-block-default platforms.
    llvm-svn: 151343

--- clang/test/Index/targeted-cursor.m
+++ clang/test/Index/targeted-cursor.m
@@ -1,6 +1,6 @@
 
 // rdar://10920009
-// RUN: c-index-test -write-pch %t.h.pch -x objective-c-header %S/targeted-cursor.m.h -Xclang -detailed-preprocessing-record
-// RUN: c-index-test -cursor-at=%S/targeted-cursor.m.h:5:13 %s -include %t.h | FileCheck %s
+// RUN: c-index-test -write-pch %t.h.pch -fblocks -x objective-c-header %S/targeted-cursor.m.h -Xclang -detailed-preprocessing-record
+// RUN: c-index-test -cursor-at=%S/targeted-cursor.m.h:5:13 %s -fblocks -include %t.h | FileCheck %s
 
 // CHECK: ObjCClassRef=I:2:12
