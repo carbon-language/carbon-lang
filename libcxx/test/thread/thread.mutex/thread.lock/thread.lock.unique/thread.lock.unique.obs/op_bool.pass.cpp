26bba442acb0b1ffca0b21b8815db7aa63fdf123
Howard Hinnant <hhinnant@apple.com>
2012-02-25 21:43:14 +0000

    update test for explicit bool operator.
    llvm-svn: 151461

--- libcxx/test/thread/thread.mutex/thread.lock/thread.lock.unique/thread.lock.unique.obs/op_bool.pass.cpp
+++ libcxx/test/thread/thread.mutex/thread.lock/thread.lock.unique/thread.lock.unique.obs/op_bool.pass.cpp
@@ -21,9 +21,9 @@ std::mutex m;
 int main()
 {
     std::unique_lock<std::mutex> lk0;
-    assert(lk0 == false);
+    assert(static_cast<bool>(lk0) == false);
     std::unique_lock<std::mutex> lk1(m);
-    assert(lk1 == true);
+    assert(static_cast<bool>(lk1) == true);
     lk1.unlock();
-    assert(lk1 == false);
+    assert(static_cast<bool>(lk1) == false);
 }
