2e16624291eb45807c36a1ba3203d86a9d02237b
Jim Grosbach <grosbach@apple.com>
2012-02-27 23:55:25 +0000

    Re-enable the Darwin ARM integrated assembler.
    All known nightly-test failures are fixed.
    
    llvm-svn: 151595

--- clang/lib/Driver/ToolChains.h
+++ clang/lib/Driver/ToolChains.h
@@ -329,9 +329,8 @@ public:
 #ifdef DISABLE_DEFAULT_INTEGRATED_ASSEMBLER
     return false;
 #else
-    // Default integrated assembler to on for x86.
-    return (getTriple().getArch() == llvm::Triple::x86 ||
-            getTriple().getArch() == llvm::Triple::x86_64);
+    // Default integrated assembler to on for Darwin.
+    return true;
 #endif
   }
   virtual bool IsStrictAliasingDefault() const {
