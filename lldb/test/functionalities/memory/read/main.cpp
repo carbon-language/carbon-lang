64e2e516bc506010bdeecd8f4025a13d5be77af4
Johnny Chen <johnny.chen@apple.com>
2012-02-28 18:16:44 +0000

    Forgot to check in this file with r151593.
    llvm-svn: 151640

--- lldb/test/functionalities/memory/read/main.cpp
+++ lldb/test/functionalities/memory/read/main.cpp
@@ -11,6 +11,8 @@
 int main (int argc, char const *argv[])
 {
     char my_string[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 0};
+    double my_double = 1234.5678;
     printf("my_string=%s\n", my_string); // Set break point at this line.
+    printf("my_double=%g\n", my_double);
     return 0;
 }
