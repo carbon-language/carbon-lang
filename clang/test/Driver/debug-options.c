2ad368fce35afcf0933afb2cf4074d0c810aac4a
Chad Rosier <mcrosier@apple.com>
2012-02-28 20:49:04 +0000

    [driver] Add support for -g2 and -ggdb debug flags. rdar://10947759
    llvm-svn: 151654

--- clang/test/Driver/debug-options.c
+++ clang/test/Driver/debug-options.c
@@ -2,18 +2,26 @@
 // rdar://10383444
 
 // RUN: %clang -### -c -g %s 2>&1 | FileCheck -check-prefix=G %s
+// RUN: %clang -### -c -g2 %s 2>&1 | FileCheck -check-prefix=G2 %s
 // RUN: %clang -### -c -g3 %s 2>&1 | FileCheck -check-prefix=G3 %s
 // RUN: %clang -### -c -ganything %s 2>&1 | FileCheck -check-prefix=GANY %s
+// RUN: %clang -### -c -ggdb %s 2>&1 | FileCheck -check-prefix=GGDB %s
 // RUN: %clang -### -c -gfoo %s 2>&1 | FileCheck -check-prefix=GFOO %s
 //
 // G: "-cc1"
 // G: "-g"
+//
+// G2: "-cc1"
+// G2: "-g"
 //
 // G3: "-cc1"
 // G3: "-g"
 //
 // GANY: "-cc1"
 // GANY-NOT: "-g"
+//
+// GGDB: "-cc1"
+// GGDB: "-g"
 //
 // GFOO: "-cc1"
 // GFOO-NOT: "-g"
