272a071d65be9426cba2c4cd7f50dbc4c9599d61
David Chisnall <csdavec@swan.ac.uk>
2012-02-29 15:06:12 +0000

    Add -C when linking on Solaris so that the error messages are understandable by actual humans.
    llvm-svn: 151726

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -4220,6 +4220,9 @@ void solaris::Link::ConstructJob(Compilation &C, const JobAction &JA,
 
   ArgStringList CmdArgs;
 
+  // Demangle C++ names in errors
+  CmdArgs.push_back("-C");
+
   if ((!Args.hasArg(options::OPT_nostdlib)) &&
       (!Args.hasArg(options::OPT_shared))) {
     CmdArgs.push_back("-e");
