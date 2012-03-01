5bc1f47b00421c754d3b4d825419087b0e29fde5
Eric Christopher <echristo@apple.com>
2012-03-01 22:49:10 +0000

    Update test for llvm assembly output change. Also add a fixme that this shouldn't be relying on assembly emission.
    For the record we should check the metadata output from the front
    end and then check in the backend that such a thing emits a pubtypes
    entry.
    
    llvm-svn: 151851

--- clang/test/CodeGenCXX/debug-info-pubtypes.cpp
+++ clang/test/CodeGenCXX/debug-info-pubtypes.cpp
@@ -2,6 +2,8 @@
 // RUN: %clang -cc1 -triple x86_64-apple-darwin10  -g -fno-limit-debug-info -S %s -o %t
 // RUN: FileCheck %s < %t
 
+// FIXME: This testcase shouldn't rely on assembly emission.
+//CHECK: Lpubtypes_begin1:
 //CHECK:         .asciz   "G"
 //CHECK-NEXT:    .long   0
 //CHECK-NEXT: Lpubtypes_end1:
