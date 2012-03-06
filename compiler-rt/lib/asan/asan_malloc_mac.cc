e781266905a6176c59dea7c55893871b50a8a647
Eli Friedman <eli.friedman@gmail.com>
2012-03-06 01:21:14 +0000

    Make sure to properly ifdef out an unused function on OSX < 10.6.  PR12136.  Patch from Jeremy Huddleston.
    llvm-svn: 152085

--- compiler-rt/lib/asan/asan_malloc_mac.cc
+++ compiler-rt/lib/asan/asan_malloc_mac.cc
@@ -298,10 +298,13 @@ void mi_statistics(malloc_zone_t *zone, malloc_statistics_t *stats) {
 }
 #endif
 
+#if defined(MAC_OS_X_VERSION_10_6) && \
+    MAC_OS_X_VERSION_MAX_ALLOWED >= MAC_OS_X_VERSION_10_6
 boolean_t mi_zone_locked(malloc_zone_t *zone) {
   // UNIMPLEMENTED();
   return false;
 }
+#endif
 
 }  // unnamed namespace
 
