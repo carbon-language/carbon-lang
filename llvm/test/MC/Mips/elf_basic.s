bdd125878404936f085d388b213d654889d5e8fd
Rafael Espindola <rafael.espindola@gmail.com>
2012-03-07 20:58:59 +0000

    Use llvm-mc instead of llc. Patch by Jack Carter.
    llvm-svn: 152242

--- llvm/test/MC/Mips/elf_basic.s
+++ llvm/test/MC/Mips/elf_basic.s
@@ -1,7 +1,7 @@
-; RUN: llc -filetype=obj -mtriple mips-unknown-linux %s -o - | elf-dump --dump-section-data  | FileCheck -check-prefix=CHECK-BE %s
-; RUN: llc -filetype=obj -mtriple mipsel-unknown-linux %s -o - | elf-dump --dump-section-data  | FileCheck -check-prefix=CHECK-LE %s
+// RUN: llvm-mc -filetype=obj -triple mips-unknown-linux %s -o - | elf-dump --dump-section-data  | FileCheck -check-prefix=CHECK-BE %s
+// RUN: llvm-mc -filetype=obj -triple mipsel-unknown-linux %s -o - | elf-dump --dump-section-data  | FileCheck -check-prefix=CHECK-LE %s
 
-; Check that we produce the correct endian.
+// Check that we produce the correct endian.
 
-; CHECK-BE: ('e_indent[EI_DATA]', 0x02)
-; CHECK-LE: ('e_indent[EI_DATA]', 0x01)
+// CHECK-BE: ('e_indent[EI_DATA]', 0x02)
+// CHECK-LE: ('e_indent[EI_DATA]', 0x01)
