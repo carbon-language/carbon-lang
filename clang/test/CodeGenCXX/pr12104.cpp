f5140611fcbf7722d6a5632522d266e9c1d4875d
Rafael Espindola <rafael.espindola@gmail.com>
2012-03-08 15:07:42 +0000

    Also test pr12104 with pch.
    llvm-svn: 152317

--- clang/test/CodeGenCXX/pr12104.cpp
+++ clang/test/CodeGenCXX/pr12104.cpp
@@ -1,14 +1,7 @@
-// RUN: %clang_cc1 %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -include %S/pr12104.h %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -x c++ -emit-pch -o %t %S/pr12104.h
+// RUN: %clang_cc1 -include-pch %t %s -emit-llvm -o - | FileCheck %s
 
-template <int dimm> struct Patch {
-  static const unsigned int no_neighbor = 1;
-};
-template <int dim>
-const unsigned int Patch<dim>::no_neighbor;
-void f(const unsigned int);
-void g() {
-  f(Patch<1>::no_neighbor);
-}
 template struct Patch<1>;
 
 // CHECK: _ZN5PatchILi1EE11no_neighborE
