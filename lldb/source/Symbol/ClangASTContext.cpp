1fc91ad1c5766bc80e7fd8e694b5fd4d8c08c482
Sean Callanan <scallanan@apple.com>
2012-03-10 02:00:32 +0000

    ...And finished the job.
    llvm-svn: 152472

--- lldb/source/Symbol/ClangASTContext.cpp
+++ lldb/source/Symbol/ClangASTContext.cpp
@@ -5982,7 +5982,8 @@ ClangASTContext::IsObjCObjectPointerType (lldb::clang_type_t clang_type, clang_t
                     const ObjCObjectPointerType *obj_pointer_type = dyn_cast<ObjCObjectPointerType>(qual_type);
                     if (!obj_pointer_type)
                         *class_type = NULL;
-                    *class_type = QualType(obj_pointer_type->getInterfaceType(), 0).getAsOpaquePtr();
+                    else
+                        *class_type = QualType(obj_pointer_type->getInterfaceType(), 0).getAsOpaquePtr();
                 }
             }
             return true;
