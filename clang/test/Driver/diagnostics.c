8028640704fc0621239ada4ac24850c5770ea77a
Chad Rosier <mcrosier@apple.com>
2012-03-13 20:31:40 +0000

    Update test case to use not tool.
    llvm-svn: 152664

--- clang/test/Driver/diagnostics.c
+++ clang/test/Driver/diagnostics.c
@@ -1,9 +1,9 @@
 // Parse diagnostic arguments in the driver
 // PR12181
 
-// RUN: !%clang -target x86_64-apple-darwin10 \
+// RUN: not %clang -target x86_64-apple-darwin10 \
 // RUN:   -fsyntax-only -fzyzzybalubah \ 
 // RUN:   -Werror=unused-command-line-argument %s 
 
-// RUN: !%clang -target x86_64-apple-darwin10 \
+// RUN: not %clang -target x86_64-apple-darwin10 \
 // RUN:   -fsyntax-only -fzyzzybalubah -Werror %s 
