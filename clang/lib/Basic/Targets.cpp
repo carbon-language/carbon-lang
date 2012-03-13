3ffe746ca8fff94d4b46958a2f0001bbef6cbfbf
Roman Divacky <rdivacky@freebsd.org>
2012-03-13 19:20:17 +0000

    Fix the long double to be of width/align 64. Rename va_list_test to powerpc_types and add testing for the (long) double there.
    llvm-svn: 152647

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -872,8 +872,10 @@ public:
       break;
     }
 
-    if (getTriple().getOS() == llvm::Triple::FreeBSD)
+    if (getTriple().getOS() == llvm::Triple::FreeBSD) {
+      LongDoubleWidth = LongDoubleAlign = 64;
       LongDoubleFormat = &llvm::APFloat::IEEEdouble;
+    }
   }
 
   virtual const char *getVAListDeclaration() const {
@@ -900,8 +902,10 @@ public:
     DescriptionString = "E-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                         "i64:64:64-f32:32:32-f64:64:64-v128:128:128-n32:64";
 
-    if (getTriple().getOS() == llvm::Triple::FreeBSD)
+    if (getTriple().getOS() == llvm::Triple::FreeBSD) {
+      LongDoubleWidth = LongDoubleAlign = 64;
       LongDoubleFormat = &llvm::APFloat::IEEEdouble;
+    }
   }
   virtual const char *getVAListDeclaration() const {
     return "typedef char* __builtin_va_list;";
