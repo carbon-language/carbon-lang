b8d8b4cb65de4f976524ce1cba3929b88066745a
Chad Rosier <mcrosier@apple.com>
2012-03-13 23:48:50 +0000

    Typo.
    llvm-svn: 152686

--- clang/test/Driver/altivec.cpp
+++ clang/test/Driver/altivec.cpp
@@ -1,4 +1,4 @@
-// Check that we error when -faltivec is specified on a non-ppc platforms.
+// Check that we error when -faltivec is specified on non-ppc platforms.
 
 // RUN: %clang -arch ppc -faltivec -fsyntax-only %s
 // RUN: %clang -arch ppc64 -faltivec -fsyntax-only %s
