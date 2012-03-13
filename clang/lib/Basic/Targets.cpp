816dcd17771dd258f6f3eeb6dce9583499f439c4
Roman Divacky <rdivacky@freebsd.org>
2012-03-13 16:53:54 +0000

    Long double is just double on FreeBSD/{PPC,PPC64}.
    llvm-svn: 152632

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -871,6 +871,9 @@ public:
     default:
       break;
     }
+
+    if (getTriple().getOS() == llvm::Triple::FreeBSD)
+      LongDoubleFormat = &llvm::APFloat::IEEEdouble;
   }
 
   virtual const char *getVAListDeclaration() const {
@@ -896,6 +899,9 @@ public:
     Int64Type = SignedLong;
     DescriptionString = "E-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
                         "i64:64:64-f32:32:32-f64:64:64-v128:128:128-n32:64";
+
+    if (getTriple().getOS() == llvm::Triple::FreeBSD)
+      LongDoubleFormat = &llvm::APFloat::IEEEdouble;
   }
   virtual const char *getVAListDeclaration() const {
     return "typedef char* __builtin_va_list;";
