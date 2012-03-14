1c883c58dd334360cca8ee596b153ea72a51403c
Matt Beaumont-Gay <matthewbg@google.com>
2012-03-14 01:23:41 +0000

    Remove trailing whitespace (especially after a \ which should be trailing)
    llvm-svn: 152695

--- clang/test/Driver/diagnostics.c
+++ clang/test/Driver/diagnostics.c
@@ -2,8 +2,8 @@
 // PR12181
 
 // RUN: not %clang -target x86_64-apple-darwin10 \
-// RUN:   -fsyntax-only -fzyzzybalubah \ 
-// RUN:   -Werror=unused-command-line-argument %s 
+// RUN:   -fsyntax-only -fzyzzybalubah \
+// RUN:   -Werror=unused-command-line-argument %s
 
 // RUN: not %clang -target x86_64-apple-darwin10 \
-// RUN:   -fsyntax-only -fzyzzybalubah -Werror %s 
+// RUN:   -fsyntax-only -fzyzzybalubah -Werror %s
