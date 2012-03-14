91b9ac8ae9ad30c334aca8073dfb1cab86bf8889
Benjamin Kramer <benny.kra@googlemail.com>
2012-03-14 01:17:52 +0000

    -arch is a darwin-only feature, use -target instead to make the linux buildbots happy again.
    Also check the error message.
    
    llvm-svn: 152694

--- clang/test/Driver/altivec.cpp
+++ clang/test/Driver/altivec.cpp
@@ -1,12 +1,14 @@
 // Check that we error when -faltivec is specified on non-ppc platforms.
 
-// RUN: %clang -arch ppc -faltivec -fsyntax-only %s
-// RUN: %clang -arch ppc64 -faltivec -fsyntax-only %s
+// RUN: %clang -target powerpc-apple-darwin -faltivec -fsyntax-only %s
+// RUN: %clang -target powerpc64-linux-gnu -faltivec -fsyntax-only %s
 
-// RUN: not %clang -arch i386 -faltivec -fsyntax-only %s
-// RUN: not %clang -arch x86_64 -faltivec -fsyntax-only %s
-// RUN: not %clang -arch armv6 -faltivec -fsyntax-only %s
-// RUN: not %clang -arch armv7 -faltivec -fsyntax-only %s
-// RUN: not %clang -arch mips -faltivec -fsyntax-only %s
-// RUN: not %clang -arch mips64 -faltivec -fsyntax-only %s
-// RUN: not %clang -arch sparc -faltivec -fsyntax-only %s
+// RUN: %clang -target i386-pc-win32 -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target x86_64-unknown-freebsd -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target armv6-apple-darwin -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target armv7-apple-darwin -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target mips-linux-gnu -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target mips64-linux-gnu -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang -target sparc-unknown-solaris -faltivec -fsyntax-only %s 2>&1 | FileCheck %s
+
+// CHECK: invalid argument '-faltivec' only allowed with 'ppc/ppc64'
