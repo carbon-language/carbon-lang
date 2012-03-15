0c94313d200928c7b10a714de92849c06fac525d
Greg Clayton <gclayton@apple.com>
2012-03-15 17:10:48 +0000

    <rdar://problem/11052829>
    Fixed a case where if you have a argument stirng that ends with a '\' character, it would infinite loop while consuming all of your memory.
    
    Also fixed a case where non-quote terminated strings would inefficiently be handled.
    
    llvm-svn: 152809

--- lldb/source/Interpreter/Args.cpp
+++ lldb/source/Interpreter/Args.cpp
@@ -225,6 +225,7 @@ Args::SetCommandString (const char *command)
                     {
                         case '\0':
                             arg.append (arg_piece_start);
+                            ++arg_end;
                             arg_complete = true;
                             break;
 
@@ -310,6 +311,13 @@ Args::SetCommandString (const char *command)
                                 arg_piece_start = arg_pos;
                             }
                             quote_char = '\0';
+                        }
+                        else
+                        {
+                            // Consume the rest of the string as there was no terminating quote
+                            arg.append(arg_piece_start);
+                            arg_end = arg_piece_start + strlen(arg_piece_start);
+                            arg_complete = true;
                         }
                     }
                     break;
