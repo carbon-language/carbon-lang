01da1055d30066f95a14b1f41213d5556e1f4c4f
Alexey Samsonov <samsonov@google.com>
2012-03-24 09:47:56 +0000

    [ASan] simplify ASAN_INTERCEPT_* definitions
    llvm-svn: 153378

--- compiler-rt/lib/asan/asan_interceptors.cc
+++ compiler-rt/lib/asan/asan_interceptors.cc
@@ -26,17 +26,8 @@
 
 // Use macro to describe if specific function should be
 // intercepted on a given platform.
-#if !defined(_WIN32)
-# define ASAN_INTERCEPT_STRTOLL 1
-#else
-# define ASAN_INTERCEPT_STRTOLL 0
-#endif
-
-#if !defined(__APPLE__)
-# define ASAN_INTERCEPT_STRNLEN 1
-#else
-# define ASAN_INTERCEPT_STRNLEN 0
-#endif
+#define ASAN_INTERCEPT_STRTOLL !defined(_WIN32)
+#define ASAN_INTERCEPT_STRNLEN !defined(__APPLE__)
 
 // Use extern declarations of intercepted functions on Mac and Windows
 // to avoid including system headers.
