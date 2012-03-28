f981060503b67aea27daccb19a8fe7121fbf5d7b
Enrico Granata <egranata@apple.com>
2012-03-28 02:16:50 +0000

    Adding missing file
    llvm-svn: 153559

---
+++ lldb/examples/summaries/cocoa/Class.py
@@ -0,0 +1,19 @@
+"""
+LLDB AppKit formatters
+
+part of The LLVM Compiler Infrastructure
+This file is distributed under the University of Illinois Open Source
+License. See LICENSE.TXT for details.
+"""
+import lldb
+import objc_runtime
+
+def Class_Summary(valobj,dict):
+	runtime = objc_runtime.ObjCRuntime.runtime_from_isa(valobj)
+	if runtime == None or not runtime.is_valid():
+		return '<error: unknown Class>'
+	class_data = runtime.read_class_data()
+	if class_data == None or not class_data.is_valid():
+		return '<error: unknown Class>'
+	return class_data.class_name()
+
