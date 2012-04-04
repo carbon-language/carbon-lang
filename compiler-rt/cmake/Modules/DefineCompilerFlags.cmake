6acfaaf3aaeb7e60924c6ccef65505ab38895eb3
Chandler Carruth <chandlerc@gmail.com>
2012-04-04 22:12:01 +0000

    Remove the old, and non-functional CMake build system from CompilerRT. I cannot build any part of this successfully on either Linux or Darwin, and the replacement is worlds simpler by requiring that this be built as a subproject of LLVM. If this breaks you for any reason, please let me know, and let me know what your use case is.
    llvm-svn: 154059

--- compiler-rt/cmake/Modules/DefineCompilerFlags.cmake
+++
@@ -1,6 +0,0 @@
-# Define compiler flags
-
-if( CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX )
-  #ADD_DEFINITIONS( -Wall -W -Werror -pedantic )
-  ADD_DEFINITIONS( -std=c99 -Wall -Wextra -W -pedantic -Wno-unused-parameter )
-endif( CMAKE_COMPILER_IS_GNUCC OR CMAKE_COMPILER_IS_GNUCXX )
