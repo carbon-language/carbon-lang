462217eab35dc9aa50cabcafc2371ec42a6faa1b
Daniel Dunbar <daniel@zuster.org>
2012-04-06 21:49:53 +0000

    docs: Add the standard LLVM favicon.
    llvm-svn: 154212

--- lld/docs/conf.py
+++ lld/docs/conf.py
@@ -114,12 +114,12 @@ html_theme_path = ["."]
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-#html_favicon = None
+html_favicon = 'favicon.ico'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-#html_static_path = ['_static']
+html_static_path = ['_static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
