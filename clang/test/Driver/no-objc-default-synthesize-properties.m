ba8ef296e4326dafad1fd94e4be421cc0150d5cd
Fariborz Jahanian <fjahanian@apple.com>
2012-04-09 18:20:28 +0000

    objective-c: Remove -fno-objc-default-synthesize-properties as the driver option. // rdar://11209719
    llvm-svn: 154331

---
+++ clang/test/Driver/no-objc-default-synthesize-properties.m
@@ -0,0 +1,9 @@
+// RUN: %clang  -fsyntax-only -fno-objc-default-synthesize-properties %s 2>&1 | FileCheck %s
+
+@interface I
+@property int P;
+@end
+
+@implementation I
+@end
+// CHECK: warning: argument unused during compilation: '-fno-objc-default-synthesize-properties'
