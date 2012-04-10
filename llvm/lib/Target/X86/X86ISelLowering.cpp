235a7a1746196d60f95d8df971f605da7ed73ac3
Chad Rosier <mcrosier@apple.com>
2012-04-10 19:39:18 +0000

    Revert r154396, which looks to be the real culprit behind the bot failures.
    llvm-svn: 154426

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -5396,6 +5396,7 @@ static SDValue LowerVECTOR_SHUFFLEtoBlend(SDValue Op,
   EVT InVT = V1.getValueType();
   EVT EltVT = VT.getVectorElementType();
   unsigned EltSize = EltVT.getSizeInBits();
+
   int MaskSize = VT.getVectorNumElements();
   int InSize = InVT.getVectorNumElements();
 
