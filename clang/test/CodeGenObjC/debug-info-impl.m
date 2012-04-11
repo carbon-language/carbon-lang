03c50998cb3d7229ba189e0cec0bf9cf0b01bee3
Eric Christopher <echristo@apple.com>
2012-04-11 07:08:37 +0000

    Add a triple to this test.
    llvm-svn: 154485

--- clang/test/CodeGenObjC/debug-info-impl.m
+++ clang/test/CodeGenObjC/debug-info-impl.m
@@ -1,4 +1,4 @@
-// RUN: %clang -fverbose-asm -g -S -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -g -S -emit-llvm %s -o - | FileCheck %s
 // CHECK: metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"Circle", metadata {{.*}}, i32 11, i64 64, i64 64, i32 0, i32 512, null, metadata {{.*}}, i32 16, i32 0} ; [ DW_TAG_structure_type ]
 @interface NSObject {
   struct objc_object *isa;
