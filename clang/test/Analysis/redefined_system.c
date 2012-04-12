9fcb055917c4d757678895a324d78f7eb42c1037
Anna Zaks <ganna@apple.com>
2012-04-12 22:36:43 +0000

    [analyzer] Test case for r154451 (redefining system functions).
    llvm-svn: 154624

---
+++ clang/test/Analysis/redefined_system.c
@@ -0,0 +1,17 @@
+// RUN: %clang_cc1 -analyze -analyzer-checker=unix,core,experimental.security.taint -w -verify %s
+
+// Make sure we don't crash when someone redefines a system function we reason about.
+
+char memmove ();
+char malloc();
+char system();
+char stdin();
+char memccpy();
+char free();
+char strdup();
+char atoi();
+
+int foo () {
+  return memmove() + malloc() + system + stdin() + memccpy() + free() + strdup() + atoi();
+
+}
