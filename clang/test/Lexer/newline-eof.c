b0dfdfe790c9029fb488b5c8985bbb823f795cf9
Seth Cantrell <seth.cantrell@gmail.com>
2012-04-15 04:41:49 +0000

    %clang -cc1 -> %clang_cc1
    llvm-svn: 154757

--- clang/test/Lexer/newline-eof.c
+++ clang/test/Lexer/newline-eof.c
@@ -1,4 +1,4 @@
-// RUN: %clang -cc1 -fsyntax-only -Wnewline-eof -verify %s
+// RUN: %clang_cc1 -fsyntax-only -Wnewline-eof -verify %s
 // rdar://9133072
 
 // The following line isn't terminated, don't fix it.
