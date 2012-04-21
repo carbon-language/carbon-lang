a83601d6379942a0bde9db5fc9a32761796cd728
NAKAMURA Takumi <geek4civic@gmail.com>
2012-04-21 14:50:56 +0000

    CMake: Prune redundant LLVM_COMPILER_JOBS from llvm/CMakeLists.txt. HandleLLVMOptions.cmake has it.
    llvm-svn: 155284

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -110,6 +110,8 @@ if( CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT WIN32 )
   endif( LLVM_BUILD_32_BITS )
 endif( CMAKE_SIZEOF_VOID_P EQUAL 8 AND NOT WIN32 )
 
+# On Win32 using MS tools, provide an option to set the number of parallel jobs
+# to use.
 if( MSVC_IDE AND ( MSVC90 OR MSVC10 ) )
   # Only Visual Studio 2008 and 2010 officially supports /MP.
   # Visual Studio 2005 do support it but it's experimental there.
