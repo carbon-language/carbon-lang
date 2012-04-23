04b795bc1d1381c75421647c9385043c10d13d30
Michael J. Spencer <bigcheesegs@gmail.com>
2012-04-23 19:00:27 +0000

    [Support/Unix] Unconditionally include time.h.
    When building LLVM on Linux with libc++ with CMake TIME_WITH_SYS_TIME is
    undefined, and HAVE_SYS_TIME_H is defined. This ends up including
    sys/time.h but not time.h. Unix/TimeValue.inc requires time.h for asctime_r
    and localtime. libstdc++ seems to include time.h anyway, but libc++ does
    not.
    
    Fix this by always including time.h
    
    llvm-svn: 155382

--- llvm/lib/Support/Unix/Unix.h
+++ llvm/lib/Support/Unix/Unix.h
@@ -44,16 +44,10 @@
 #include <assert.h>
 #endif
 
-#ifdef TIME_WITH_SYS_TIME
+#ifdef HAVE_SYS_TIME_H
 # include <sys/time.h>
-# include <time.h>
-#else
-# ifdef HAVE_SYS_TIME_H
-#  include <sys/time.h>
-# else
-#  include <time.h>
-# endif
 #endif
+#include <time.h>
 
 #ifdef HAVE_SYS_WAIT_H
 # include <sys/wait.h>
