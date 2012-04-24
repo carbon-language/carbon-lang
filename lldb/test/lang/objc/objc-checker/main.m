72546f01d3f53a4fa6e249d1abdc6ebdb8af647a
Sean Callanan <scallanan@apple.com>
2012-04-24 23:18:47 +0000

    Create an NSAutoreleasePool correctly in a test case so that the Objective-C runtime doesn't complain about lack of one, causing the test case to fail.
    llvm-svn: 155503

--- lldb/test/lang/objc/objc-checker/main.m
+++ lldb/test/lang/objc/objc-checker/main.m
@@ -22,9 +22,11 @@
 
 int main ()
 {
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
   Simple *my_simple = [[Simple alloc] init];
   my_simple.value = 20;
   // Set a breakpoint here.
   NSLog (@"Object has value: %d.", my_simple.value); 
+  [pool drain];
   return 0;
 }
