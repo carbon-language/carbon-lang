024e12df527deb7c4273643aae8ecba6ae9d2fa1
Chris Lattner <sabre@nondot.org>
2012-04-25 06:12:24 +0000

    OpenBSD: Remove incorrect -pthread preprocessor define _POSIX_THREADS and replace with _REENTRANT. Also remove undef _POSIX_THREADS in phread.h.
    Patch by Brad Smith!
    
    llvm-svn: 155535

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -365,7 +365,7 @@ protected:
     DefineStd(Builder, "unix", Opts);
     Builder.defineMacro("__ELF__");
     if (Opts.POSIXThreads)
-      Builder.defineMacro("_POSIX_THREADS");
+      Builder.defineMacro("_REENTRANT");
   }
 public:
   OpenBSDTargetInfo(const std::string &triple)
