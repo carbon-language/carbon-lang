3b6e07cd1a71df7b8a367adbc5635089a76ce942
Eli Bendersky <eli.bendersky@intel.com>
2012-04-29 10:26:26 +0000

    Fix comments from copy-paste to a more relevant meaning
    llvm-svn: 155790

--- llvm/include/llvm/ExecutionEngine/MCJIT.h
+++ llvm/include/llvm/ExecutionEngine/MCJIT.h
@@ -23,7 +23,7 @@ extern "C" void LLVMLinkInMCJIT();
 namespace {
   struct ForceMCJITLinking {
     ForceMCJITLinking() {
-      // We must reference the passes in such a way that compilers will not
+      // We must reference MCJIT in such a way that compilers will not
       // delete it all as dead code, even with whole program optimization,
       // yet is effectively a NO-OP. As the compiler isn't smart enough
       // to know that getenv() never returns -1, this will do the job.
