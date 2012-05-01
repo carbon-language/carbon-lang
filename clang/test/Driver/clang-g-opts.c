77b2b350f79514f63f6d26372f3b3c44cb4b24fc
Douglas Gregor <dgregor@apple.com>
2012-05-01 01:42:40 +0000

    Move a non portable test to FileCheck, from Jonathan Gray!
    llvm-svn: 155874

--- clang/test/Driver/clang-g-opts.c
+++ clang/test/Driver/clang-g-opts.c
@@ -1,5 +1,14 @@
-// RUN: %clang -S -v -o %t %s        2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g     2>&1 | grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g0    2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g -g0 2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g0 -g 2>&1 | grep -w -- -g
+// RUN: %clang -S -v -o %t %s        2>&1 | FileCheck %s
+// CHECK-NOT: -g
+
+// RUN: %clang -S -v -o %t %s -g     2>&1 | FileCheck %s
+// CHECK: -g
+
+// RUN: %clang -S -v -o %t %s -g0    2>&1 | FileCheck %s
+// CHECK-NOT: -g
+
+// RUN: %clang -S -v -o %t %s -g -g0 2>&1 | FileCheck %s
+// CHECK-NOT: -g
+
+// RUN: %clang -S -v -o %t %s -g0 -g 2>&1 | FileCheck %s
+// CHECK: -g
