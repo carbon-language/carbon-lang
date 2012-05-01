882e14c8022b504a0e142b9d64f77810196f70f1
Douglas Gregor <dgregor@apple.com>
2012-05-01 20:44:16 +0000

    We don't need a lengthy quote from the wrong standard.
    llvm-svn: 155942

--- clang/test/CXX/basic/basic.lookup/basic.lookup.classref/p1-cxx11.cpp
+++ clang/test/CXX/basic/basic.lookup/basic.lookup.classref/p1-cxx11.cpp
@@ -1,25 +1,5 @@
 // RUN: %clang_cc1 -std=c++11 -fsyntax-only -fdiagnostics-show-option -verify %s
 
-// C++98 [basic.lookup.classref]p1:
-//   In a class member access expression (5.2.5), if the . or -> token is
-//   immediately followed by an identifier followed by a <, the identifier must
-//   be looked up to determine whether the < is the beginning of a template
-//   argument list (14.2) or a less-than operator. The identifier is first
-//   looked up in the class of the object expression. If the identifier is not
-//   found, it is then looked up in the context of the entire postfix-expression
-//   and shall name a class or function template. If the lookup in the class of
-//   the object expression finds a template, the name is also looked up in the
-//   context of the entire postfix-expression and
-//    -- if the name is not found, the name found in the class of the object
-//       expression is used, otherwise
-//    -- if the name is found in the context of the entire postfix-expression
-//       and does not name a class template, the name found in the class of the
-//       object expression is used, otherwise
-//    -- if the name found is a class template, it must refer to the same
-//       entity as the one found in the class of the object expression,
-//       otherwise the program is ill-formed.
-
-// From PR 7247
 template<typename T>
 struct set{};
 struct Value {
