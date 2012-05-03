e16a8c8db8f08720f7d21bc019ecd2d381ab97b1
Simon Atanasyan <satanasyan@mips.com>
2012-05-03 13:57:00 +0000

    Test case Clang::Driver/debug-options-as.c depends on the integrated assembler. Turn it on explicitly on all platforms by the "-integrated-as" option.
    llvm-svn: 156063

--- clang/test/Driver/debug-options-as.c
+++ clang/test/Driver/debug-options-as.c
@@ -4,7 +4,8 @@
 // Check to make sure clang is somewhat picky about -g options.
 // (Delived from debug-options.c)
 // rdar://10383444
-// RUN: %clang -### -c -save-temps -g %s 2>&1 | FileCheck -check-prefix=SAVE %s
+// RUN: %clang -### -c -save-temps -integrated-as -g %s 2>&1 \
+// RUN:   | FileCheck -check-prefix=SAVE %s
 //
 // SAVE: "-cc1as"
 // SAVE-NOT: "-g"
@@ -12,7 +13,8 @@
 // Check to make sure clang with -g on a .s file gets passed.
 // rdar://9275556
 // RUN: touch %t.s
-// RUN: %clang -### -c -g %t.s 2>&1 | FileCheck -check-prefix=S %s
+// RUN: %clang -### -c -integrated-as -g %t.s 2>&1 \
+// RUN:   | FileCheck -check-prefix=S %s
 //
 // S: "-cc1as"
 // S: "-g"
