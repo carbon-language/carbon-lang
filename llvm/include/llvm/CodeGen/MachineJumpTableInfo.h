b51ffd762ca41878ab375a17383e4ef001b0c452
Jim Grosbach <grosbach@apple.com>
2012-05-07 02:25:53 +0000

    Tidy up. Whitespace.
    llvm-svn: 156276

--- llvm/include/llvm/CodeGen/MachineJumpTableInfo.h
+++ llvm/include/llvm/CodeGen/MachineJumpTableInfo.h
@@ -10,9 +10,9 @@
 // The MachineJumpTableInfo class keeps track of jump tables referenced by
 // lowered switch instructions in the MachineFunction.
 //
-// Instructions reference the address of these jump tables through the use of 
-// MO_JumpTableIndex values.  When emitting assembly or machine code, these 
-// virtual address references are converted to refer to the address of the 
+// Instructions reference the address of these jump tables through the use of
+// MO_JumpTableIndex values.  When emitting assembly or machine code, these
+// virtual address references are converted to refer to the address of the
 // function jump tables.
 //
 //===----------------------------------------------------------------------===//
@@ -34,11 +34,11 @@ class raw_ostream;
 struct MachineJumpTableEntry {
   /// MBBs - The vector of basic blocks from which to create the jump table.
   std::vector<MachineBasicBlock*> MBBs;
-  
+
   explicit MachineJumpTableEntry(const std::vector<MachineBasicBlock*> &M)
   : MBBs(M) {}
 };
-  
+
 class MachineJumpTableInfo {
 public:
   /// JTEntryKind - This enum indicates how each entry of the jump table is
@@ -57,7 +57,7 @@ public:
     /// with a relocation as gp-relative, e.g.:
     ///     .gprel32 LBB123
     EK_GPRel32BlockAddress,
-    
+
     /// EK_LabelDifference32 - Each entry is the address of the block minus
     /// the address of the jump table.  This is used for PIC jump tables where
     /// gprel32 is not supported.  e.g.:
@@ -80,18 +80,18 @@ private:
   std::vector<MachineJumpTableEntry> JumpTables;
 public:
   explicit MachineJumpTableInfo(JTEntryKind Kind): EntryKind(Kind) {}
-    
+
   JTEntryKind getEntryKind() const { return EntryKind; }
 
   /// getEntrySize - Return the size of each entry in the jump table.
   unsigned getEntrySize(const TargetData &TD) const;
   /// getEntryAlignment - Return the alignment of each entry in the jump table.
   unsigned getEntryAlignment(const TargetData &TD) const;
-  
+
   /// createJumpTableIndex - Create a new jump table.
   ///
   unsigned createJumpTableIndex(const std::vector<MachineBasicBlock*> &DestBBs);
-  
+
   /// isEmpty - Return true if there are no jump tables.
   ///
   bool isEmpty() const { return JumpTables.empty(); }
@@ -105,7 +105,7 @@ public:
   void RemoveJumpTable(unsigned Idx) {
     JumpTables[Idx].MBBs.clear();
   }
-  
+
   /// ReplaceMBBInJumpTables - If Old is the target of any jump tables, update
   /// the jump tables to branch to New instead.
   bool ReplaceMBBInJumpTables(MachineBasicBlock *Old, MachineBasicBlock *New);
