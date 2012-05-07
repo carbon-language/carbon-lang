f4c7679f35eb0775d63ecab19d0fed6b19154658
Nuno Lopes <nunoplopes@sapo.pt>
2012-05-07 20:23:03 +0000

    make this test a little more useful. I'll add more tests for bound checking once -fbounds-checking patch gets in
    llvm-svn: 156321

--- clang/test/CodeGen/catch-undef-behavior.c
+++ clang/test/CodeGen/catch-undef-behavior.c
@@ -1,7 +1,17 @@
-// RUN: %clang_cc1 -fcatch-undefined-behavior -emit-llvm-only %s
+// RUN: %clang_cc1 -fcatch-undefined-behavior -emit-llvm %s -o - | FileCheck %s
 
 // PR6805
+// CHECK: @foo
 void foo() {
   union { int i; } u;
+  // CHECK: objectsize
+  // CHECK-NEXT: icmp uge
   u.i=1;
+}
+
+// CHECK: @bar
+int bar(int *a) {
+  // CHECK: objectsize
+  // CHECK-NEXT: icmp uge
+  return *a;
 }
