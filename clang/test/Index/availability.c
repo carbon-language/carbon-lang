0ee5df8264f0e9741ece31cc1aa3826d411ce33e
Douglas Gregor <dgregor@apple.com>
2012-05-08 00:29:04 +0000

    Robustify test
    llvm-svn: 156349

--- clang/test/Index/availability.c
+++ clang/test/Index/availability.c
@@ -2,5 +2,9 @@
 
 void foo(void) __attribute__((availability(macosx,introduced=10.4,deprecated=10.5,obsoleted=10.7), availability(ios,introduced=3.2,deprecated=4.1)));
 
-// RUN: c-index-test -test-load-source all %s | FileCheck %s
-// CHECK: (ios, introduced=3.2, deprecated=4.1)  (macosx, introduced=10.4, deprecated=10.5, obsoleted=10.7)
+// RUN: c-index-test -test-load-source all %s > %t
+// RUN: FileCheck -check-prefix=CHECK-1 %s < %t
+// RUN: FileCheck -check-prefix=CHECK-2 %s < %t
+// CHECK-1: (ios, introduced=3.2, deprecated=4.1) 
+// CHECK-2: (macosx, introduced=10.4, deprecated=10.5, obsoleted=10.7)
+
