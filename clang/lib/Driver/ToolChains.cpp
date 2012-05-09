7b1fee1fe515b29b7f601bd73e3f3a0237fe0c7c
Chad Rosier <mcrosier@apple.com>
2012-05-09 18:55:57 +0000

    Roll logic into a single if statement, per David's suggestion.
    llvm-svn: 156502

--- clang/lib/Driver/ToolChains.cpp
+++ clang/lib/Driver/ToolChains.cpp
@@ -528,10 +528,9 @@ void Darwin::AddDeploymentTarget(DerivedArgList &Args) const {
 
     // If no OSX or iOS target has been specified and we're compiling for armv7,
     // go ahead as assume we're targeting iOS.
-    if (OSXTarget.empty() && iOSTarget.empty())
-      if (getDarwinArchName(Args) == "armv7") {
+    if (OSXTarget.empty() && iOSTarget.empty() &&
+        getDarwinArchName(Args) == "armv7")
         iOSTarget = iOSVersionMin;
-      }
 
     // Handle conflicting deployment targets
     //
