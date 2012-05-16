e2632210698cf280cf77650a5e2f4bc1c362f60a
Kostya Serebryany <kcc@google.com>
2012-05-16 07:48:16 +0000

    [tsan] add tiny_test.c
    llvm-svn: 156908

---
+++ compiler-rt/lib/tsan/output_tests/tiny_race.c
@@ -0,0 +1,14 @@
+#include <pthread.h>
+int Global;
+void *Thread1(void *x) {
+  Global = 42;
+  return x;
+}
+int main() {
+  pthread_t t;
+  pthread_create(&t, NULL, Thread1, NULL);
+  Global = 43;
+  pthread_join(t, NULL);
+  return Global;
+}
+// CHECK: WARNING: ThreadSanitizer: data race
