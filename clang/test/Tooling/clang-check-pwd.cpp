fe3fa7867e09959fa274d6af3863dd0dcbd36719
NAKAMURA Takumi <geek4civic@gmail.com>
2012-05-16 17:37:56 +0000

    clang/test/Tooling/clang-check-pwd.cpp: Add "REQUIRES: shell".
    This passes on MSYS bash and Cygwin.
    
    llvm-svn: 156929

--- clang/test/Tooling/clang-check-pwd.cpp
+++ clang/test/Tooling/clang-check-pwd.cpp
@@ -8,6 +8,4 @@
 // CHECK: C++ requires
 invalid;
 
-// FIXME: JSON doesn't like path separator '\', on Win32 hosts.
-// FIXME: clang-check doesn't like gcc driver on cygming.
-// XFAIL: cygwin,mingw32,win32
+// REQUIRES: shell
