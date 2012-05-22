a20890b6bc14fe37e0eecd30511a19e7304177cc
Kostya Serebryany <kcc@google.com>
2012-05-22 07:59:37 +0000

    [asan] use -fno-builtin to build asan-rt on Mac
    llvm-svn: 157239

--- compiler-rt/make/platform/clang_darwin.mk
+++ compiler-rt/make/platform/clang_darwin.mk
@@ -106,7 +106,7 @@ IOSSIM_DEPLOYMENT_ARGS += -isysroot $(ProjSrcRoot)/SDKs/darwin
 CFLAGS.eprintf		:= $(CFLAGS) $(OSX_DEPLOYMENT_ARGS)
 CFLAGS.10.4		:= $(CFLAGS) $(OSX_DEPLOYMENT_ARGS)
 # FIXME: We can't build ASAN with our stub SDK yet.
-CFLAGS.asan_osx         := $(CFLAGS) -mmacosx-version-min=10.5
+CFLAGS.asan_osx         := $(CFLAGS) -mmacosx-version-min=10.5 -fno-builtin
 
 CFLAGS.ios.i386		:= $(CFLAGS) $(IOSSIM_DEPLOYMENT_ARGS)
 CFLAGS.ios.x86_64	:= $(CFLAGS) $(IOSSIM_DEPLOYMENT_ARGS)
