777e6d01ea4fe8f7cac76f0287e4124959f733f2
Jim Ingham <jingham@apple.com>
2012-05-24 01:16:09 +0000

    Change the "Debug" build to use the current MacOSX SDK.  Fix the swig builder to have an explicit short-circuit of the Python SWIG building, rather than relying on the SDKROOT being set.
    llvm-svn: 157363

--- lldb/scripts/Python/build-swig-Python.sh
+++ lldb/scripts/Python/build-swig-Python.sh
@@ -24,7 +24,12 @@ swig_python_extensions=${SRC_ROOT}/scripts/Python/python-extensions.swig
 swig_python_wrapper=${SRC_ROOT}/scripts/Python/python-wrapper.swig
 swig_python_typemaps=${SRC_ROOT}/scripts/Python/python-typemaps.swig
 
-if [ "x$SDKROOT" = "x" ] ; then
+if [ $LLDB_DISABLE_PYTHON = "1" ] ; then
+    # SDKROOT was not empty, which currently means iOS cross build where python is disabled
+    rm -rf ${swig_output_file}
+    touch ${swig_output_file}
+
+else
 
 if [ -n "$debug_flag" -a "$debug_flag" == "-debug" ]
 then
@@ -303,8 +308,4 @@ then
     fi
 fi
 
-else
-    # SDKROOT was not empty, which currently means iOS cross build where python is disabled
-    rm -rf ${swig_output_file}
-    touch ${swig_output_file}
 fi
