ed5bc0274092336aba88f2d4cf3efe1b28f059ac
Dmitry Vyukov <dvyukov@google.com>
2012-05-28 14:14:53 +0000

    tsan: be more conservative wrt symbolizer output
    llvm-svn: 157578

--- compiler-rt/lib/tsan/output_tests/simple_stack2.cc
+++ compiler-rt/lib/tsan/output_tests/simple_stack2.cc
@@ -9,16 +9,16 @@ void __attribute__((noinline)) foo1() {
 }
 
 void __attribute__((noinline)) bar1() {
-  volatile int tmp = 42; int tmp2 = tmp; (void)tmp2;
+  volatile int tmp = 42; (void)tmp;
   foo1();
 }
 
 void __attribute__((noinline)) foo2() {
-  volatile int v = Global; int v2 = v; (void)v2;
+  volatile int v = Global; (void)v;
 }
 
 void __attribute__((noinline)) bar2() {
-  volatile int tmp = 42; int tmp2 = tmp; (void)tmp2;
+  volatile int tmp = 42; (void)tmp;
   foo2();
 }
 
@@ -37,12 +37,12 @@ int main() {
 
 // CHECK:      WARNING: ThreadSanitizer: data race
 // CHECK-NEXT:   Write of size 4 at {{.*}} by thread 1:
-// CHECK-NEXT:     #0 foo1() {{.*}}simple_stack2.cc:8 ({{.*}})
-// CHECK-NEXT:     #1 bar1() {{.*}}simple_stack2.cc:13 ({{.*}})
-// CHECK-NEXT:     #2 Thread1(void*) {{.*}}simple_stack2.cc:27 ({{.*}})
+// CHECK-NEXT:     #0 foo1{{.*}} {{.*}}simple_stack2.cc:8 ({{.*}})
+// CHECK-NEXT:     #1 bar1{{.*}} {{.*}}simple_stack2.cc:13 ({{.*}})
+// CHECK-NEXT:     #2 Thread1{{.*}} {{.*}}simple_stack2.cc:27 ({{.*}})
 // CHECK-NEXT:   Previous read of size 4 at {{.*}} by main thread:
-// CHECK-NEXT:     #0 foo2() {{.*}}simple_stack2.cc:17 ({{.*}})
-// CHECK-NEXT:     #1 bar2() {{.*}}simple_stack2.cc:22 ({{.*}})
-// CHECK-NEXT:     #2 main {{.*}}simple_stack2.cc:34 ({{.*}})
+// CHECK-NEXT:     #0 foo2{{.*}} {{.*}}simple_stack2.cc:17 ({{.*}})
+// CHECK-NEXT:     #1 bar2{{.*}} {{.*}}simple_stack2.cc:22 ({{.*}})
+// CHECK-NEXT:     #2 main{{.*}} {{.*}}simple_stack2.cc:34 ({{.*}})
 
 
