0d1c121bf77a103e6173a496c88f849f1a00d940
Gabor Greif <ggreif@gmail.com>
2012-05-30 09:56:26 +0000

    supply a simple-minded testcase for r156813, to check whether the embedded montavista gcc toolchain is recognized
    llvm-svn: 157686

---
+++ clang/test/Driver/montavista-gcc-toolchain.c
@@ -0,0 +1,9 @@
+// Test that the montavista gcc-toolchain is correctly detected
+//
+// RUN: %clang -print-libgcc-file-name 2>&1 \
+// RUN:     -target i686-montavista-linux \
+// RUN:     -gcc-toolchain %S/Inputs/montavista_i686_tree/usr \
+// RUN:   | FileCheck %s
+
+// Test for header search toolchain detection.
+// CHECK: montavista_i686_tree/usr/lib/gcc/i686-montavista-linux/4.2.0/libgcc.a
