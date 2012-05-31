f8c5a5bcbc186eaef20058ba6a2402844d7a5706
Kostya Serebryany <kcc@google.com>
2012-05-31 15:44:05 +0000

    [asan] partial fix for windows build
    llvm-svn: 157748

--- compiler-rt/lib/asan/asan_internal.h
+++ compiler-rt/lib/asan/asan_internal.h
@@ -22,20 +22,7 @@
 #endif
 
 #if defined(_WIN32)
-# if defined(__clang__)
-typedef int              sptr;
-typedef unsigned int     uptr;
-# endif
 
-// There's no <stdint.h> in Visual Studio 9, so we have to define [u]int*_t.
-typedef unsigned __int8  u8;
-typedef unsigned __int16 u16;
-typedef unsigned __int32 u32;
-typedef unsigned __int64 u64;
-typedef __int8           s8;
-typedef __int16          int16_t;
-typedef __int32          s32;
-typedef __int64          s64;
 typedef unsigned long    DWORD;  // NOLINT
 
 extern "C" void* _ReturnAddress(void);
