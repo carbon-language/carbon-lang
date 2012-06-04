96fe81069b0a8a032a5e77da5e8ac6ddb8d97486
Hans Wennborg <hans@hanshq.net>
2012-06-04 10:19:34 +0000

    Test the '__thread' before 'static' warning.
    Also fix the '__thread' before 'extern' case.
    
    llvm-svn: 157921

--- clang/test/Sema/thread-specifier.c
+++ clang/test/Sema/thread-specifier.c
@@ -1,19 +1,21 @@
-// RUN: %clang_cc1 -triple i686-pc-linux-gnu -fsyntax-only -verify %s
+// RUN: %clang_cc1 -triple i686-pc-linux-gnu -fsyntax-only -verify -pedantic %s
 
 __thread int t1;
-__thread extern int t2;
-__thread static int t3;
+__thread extern int t2; // expected-warning {{'__thread' before 'extern'}}
+__thread static int t3; // expected-warning {{'__thread' before 'static'}}
 __thread __private_extern__ int t4;
 struct t5 { __thread int x; }; // expected-error {{type name does not allow storage class to be specified}}
 __thread int t6(); // expected-error {{'__thread' is only allowed on variable declarations}}
+
 int f(__thread int t7) { // expected-error {{'__thread' is only allowed on variable declarations}}
   __thread int t8; // expected-error {{'__thread' variables must have global storage}}
-  __thread extern int t9;
-  __thread static int t10;
+  extern __thread int t9;
+  static __thread int t10;
   __thread __private_extern__ int t11;
   __thread auto int t12; // expected-error {{'__thread' variables must have global storage}}
   __thread register int t13; // expected-error {{'__thread' variables must have global storage}}
 }
+
 __thread typedef int t14; // expected-error {{'__thread' is only allowed on variable declarations}}
 __thread int t15; // expected-note {{previous definition is here}}
 int t15; // expected-error {{non-thread-local declaration of 't15' follows thread-local declaration}}
