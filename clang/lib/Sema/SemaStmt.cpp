10c92466822b498be785a24adbc163cd9b2cabdc
Richard Trieu <rtrieu@google.com>
2012-06-08 20:10:05 +0000

    Remove a commented out variable declaration.  This was originally a debugging variable which wasn't removed when the original patch was committed.
    llvm-svn: 158225

--- clang/lib/Sema/SemaStmt.cpp
+++ clang/lib/Sema/SemaStmt.cpp
@@ -1193,7 +1193,6 @@ public:
   class DeclMatcher : public EvaluatedExprVisitor<DeclMatcher> {
     llvm::SmallPtrSet<VarDecl*, 8> &Decls;
     bool FoundDecl;
-    //bool EvalDecl;
 
 public:
   typedef EvaluatedExprVisitor<DeclMatcher> Inherited;
