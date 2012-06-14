9ff952fac6f0e7a957e6b29d7f1f61ab58496dec
David Blaikie <dblaikie@gmail.com>
2012-06-14 19:55:56 +0000

    Remove absolute path form include test.
    Review feedback/bot failure from r158459 by Simon Atanasyan and Benjamin Kramer (on IRC).
    
    llvm-svn: 158464

--- clang/test/Frontend/rewrite-includes-missing.c
+++ clang/test/Frontend/rewrite-includes-missing.c
@@ -4,4 +4,4 @@
 // CHECK: {{^}}#if 0 /* expanded by -frewrite-includes */{{$}}
 // CHECK-NEXT: {{^}}#include "foobar.h"
 // CHECK-NEXT: {{^}}#endif /* expanded by -frewrite-includes */{{$}}
-// CHECK-NEXT: {{^}}# 4 "/usr/local/google/home/blaikie/Development/llvm/src/tools/clang/test/Frontend/rewrite-includes-missing.c" 2{{$}}
+// CHECK-NEXT: {{^}}# 4 "{{.*}}rewrite-includes-missing.c" 2{{$}}
