01f7a63ebc1e62dc65a9234f9a68df8c431e61f3
James Dennett <jdennett@google.com>
2012-06-15 08:49:07 +0000

    Documentation cleanup: use \p ParameterName when referring to a parameter, not \arg ParameterName (which should be used only when documenting that parameter, not when referring to it from elsewhere).
    llvm-svn: 158509

--- clang/include/clang/Driver/Types.h
+++ clang/include/clang/Driver/Types.h
@@ -23,7 +23,7 @@ namespace types {
     TY_LAST
   };
 
-  /// getTypeName - Return the name of the type for \arg Id.
+  /// getTypeName - Return the name of the type for \p Id.
   const char *getTypeName(ID Id);
 
   /// getPreprocessedType - Get the ID of the type for this input when
@@ -70,7 +70,7 @@ namespace types {
   bool isObjC(ID Id);
 
   /// lookupTypeForExtension - Lookup the type to use for the file
-  /// extension \arg Ext.
+  /// extension \p Ext.
   ID lookupTypeForExtension(const char *Ext);
 
   /// lookupTypeForTypSpecifier - Lookup the type to use for a user
@@ -81,7 +81,7 @@ namespace types {
   /// to be done for this type.
   unsigned getNumCompilationPhases(ID Id);
 
-  /// getCompilationPhase - Return the \args N th compilation phase to
+  /// getCompilationPhase - Return the \p N th compilation phase to
   /// be done for this type.
   phases::ID getCompilationPhase(ID Id, unsigned N);
   
