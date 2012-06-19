b1c3991915a01a98a8e939a4ff28131e7098549e
Alexey Samsonov <samsonov@google.com>
2012-06-19 08:57:53 +0000

    [TSan] Add a comment that tsan_flags.h may be included in the user code, and therefore shouldn't include other headers from TSan or common sanitizer runtime. User may need tsan_flags.h to provide its implementation of __tsan::OverrideFlags
    llvm-svn: 158708

--- compiler-rt/lib/tsan/rtl/tsan_flags.h
+++ compiler-rt/lib/tsan/rtl/tsan_flags.h
@@ -8,11 +8,17 @@
 //===----------------------------------------------------------------------===//
 //
 // This file is a part of ThreadSanitizer (TSan), a race detector.
-//
+// NOTE: This file may be included into user code.
 //===----------------------------------------------------------------------===//
 
 #ifndef TSAN_FLAGS_H
 #define TSAN_FLAGS_H
+
+// ----------- ATTENTION -------------
+// ThreadSanitizer user may provide its implementation of weak
+// symbol __tsan::OverrideFlags(__tsan::Flags). Therefore, this
+// header may be included in the user code, and shouldn't include
+// other headers from TSan or common sanitizer runtime.
 
 namespace __tsan {
 
