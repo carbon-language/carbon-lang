83aa94711b7e637f85e0ab59ffcd8e8e989e1727
Benjamin Kramer <benny.kra@googlemail.com>
2012-06-19 17:04:16 +0000

    Emit TableGen's header comment with C-style comments, so it can be used from C89 code.
    Should silence warnings when compiling the X86 disassembler.
    
    llvm-svn: 158723

--- llvm/lib/TableGen/TableGenBackend.cpp
+++ llvm/lib/TableGen/TableGenBackend.cpp
@@ -11,13 +11,27 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "llvm/ADT/Twine.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/TableGen/TableGenBackend.h"
 using namespace llvm;
 
+static void printLine(raw_ostream &OS, const Twine &Prefix, char Fill,
+                      StringRef Suffix) {
+  uint64_t Pos = OS.tell();
+  OS << Prefix;
+  for (unsigned i = OS.tell() - Pos, e = 80 - Suffix.size(); i != e; ++i)
+    OS << Fill;
+  OS << Suffix << '\n';
+}
+
 void llvm::emitSourceFileHeader(StringRef Desc, raw_ostream &OS) {
-  OS << "//===- TableGen'erated file -------------------------------------*-"
-       " C++ -*-===//\n//\n// " << Desc << "\n//\n// Automatically generate"
-       "d file, do not edit!\n//\n//===------------------------------------"
-       "----------------------------------===//\n\n";
+  printLine(OS, "/*===- TableGen'erated file ", '-', "*- C++ -*-===*\\");
+  printLine(OS, "|*", ' ', "*|");
+  printLine(OS, "|* " + Desc, ' ', "*|");
+  printLine(OS, "|*", ' ', "*|");
+  printLine(OS, "|* Automatically generated file, do not edit!", ' ', "*|");
+  printLine(OS, "|*", ' ', "*|");
+  printLine(OS, "\\*===", '-', "===*/");
+  OS << '\n';
 }
