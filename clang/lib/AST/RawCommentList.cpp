923074016b351405fd86dffdb66bed2646b4ba1f
Dmitri Gribenko <gribozavr@gmail.com>
2012-06-21 22:04:37 +0000

    Handle include directive with comments.  It turns out that in this case comments are not coming in source order.  Instead of trying to std::sort() comments (which can be costly), just remove comments that are not in order.
    llvm-svn: 158940

--- clang/lib/AST/RawCommentList.cpp
+++ clang/lib/AST/RawCommentList.cpp
@@ -158,13 +158,15 @@ void RawCommentList::addComment(const RawComment &RC) {
   if (RC.isInvalid())
     return;
 
-  assert((Comments.empty() ||
-          Comments.back().getSourceRange().getEnd() ==
-              RC.getSourceRange().getBegin() ||
-          SourceMgr.isBeforeInTranslationUnit(
-              Comments.back().getSourceRange().getEnd(),
-              RC.getSourceRange().getBegin())) &&
-         "comments are not coming in source order");
+  // Check if the comments are not in source order.
+  while (!Comments.empty() &&
+         !SourceMgr.isBeforeInTranslationUnit(
+              Comments.back().getSourceRange().getBegin(),
+              RC.getSourceRange().getBegin())) {
+    // If they are, just pop a few last comments that don't fit.
+    // This happens if an \#include directive contains comments.
+    Comments.pop_back();
+  }
 
   if (OnlyWhitespaceSeen) {
     if (!onlyWhitespaceBetweenComments(SourceMgr, LastComment, RC))
