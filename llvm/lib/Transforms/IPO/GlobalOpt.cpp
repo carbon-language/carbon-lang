a58862310cb510b75d77b8cf156d8bb5192b7e20
Matt Beaumont-Gay <matthewbg@google.com>
2012-06-27 17:10:33 +0000

    Revert r159136 due to PR13124.
    Original commit message:
    
    If a constant or a function has linkonce_odr linkage and unnamed_addr, mark it
    hidden. Being linkonce_odr guarantees that it is available in every dso that
    needs it. Being a constant/function with unnamed_addr guarantees that the
    copies don't have to be merged.
    
    llvm-svn: 159272

--- llvm/lib/Transforms/IPO/GlobalOpt.cpp
+++ llvm/lib/Transforms/IPO/GlobalOpt.cpp
@@ -1731,12 +1731,6 @@ bool GlobalOpt::ProcessGlobal(GlobalVariable *GV,
     return true;
   }
 
-  if (GV->hasLinkOnceODRLinkage() && GV->hasUnnamedAddr() && GV->isConstant() &&
-      GV->getVisibility() != GlobalValue::HiddenVisibility) {
-    GV->setVisibility(GlobalValue::HiddenVisibility);
-    return true;
-  }
-
   if (!GV->hasLocalLinkage())
     return false;
 
@@ -1749,7 +1743,6 @@ bool GlobalOpt::ProcessGlobal(GlobalVariable *GV,
   if (!GS.isCompared && !GV->hasUnnamedAddr()) {
     GV->setUnnamedAddr(true);
     NumUnnamed++;
-    return true;
   }
 
   if (GV->isConstant() || !GV->hasInitializer())
@@ -1924,10 +1917,6 @@ bool GlobalOpt::OptimizeFunctions(Module &M) {
       F->eraseFromParent();
       Changed = true;
       ++NumFnDeleted;
-    } else if (F->hasLinkOnceODRLinkage() && F->hasUnnamedAddr() &&
-               F->getVisibility() != GlobalValue::HiddenVisibility) {
-      F->setVisibility(GlobalValue::HiddenVisibility);
-      Changed = true;
     } else if (F->hasLocalLinkage()) {
       if (F->getCallingConv() == CallingConv::C && !F->isVarArg() &&
           !F->hasAddressTaken()) {
