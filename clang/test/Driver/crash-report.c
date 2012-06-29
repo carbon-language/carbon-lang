9c9fd7a77b3223cfa9f4b34ae7c3a07896d6a7c7
David Blaikie <dblaikie@gmail.com>
2012-06-29 23:16:06 +0000

    Unbreak the test from r159469.
    llvm-svn: 159474

--- clang/test/Driver/crash-report.c
+++ clang/test/Driver/crash-report.c
@@ -3,4 +3,4 @@
 
 #pragma clang __debug parser_crash
 // CHECK: Preprocessed source(s) and associated run script(s) are located at:
-// CHECK-NEXT: clang-3: note: diagnostic msg: {{.*}}.c
+// CHECK-NEXT: {{.*}}: note: diagnostic msg: {{.*}}.c
