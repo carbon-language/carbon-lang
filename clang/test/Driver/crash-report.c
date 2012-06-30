9d0a11d9a10e29d39e6f4801aab7c4a7aa951c0c
NAKAMURA Takumi <geek4civic@gmail.com>
2012-06-30 00:29:03 +0000

    test/Driver/crash-report.c: Mark it as XFAIL on win32 hosts, for now. Investigating.
    llvm-svn: 159476

--- clang/test/Driver/crash-report.c
+++ clang/test/Driver/crash-report.c
@@ -1,5 +1,6 @@
 // RUN: %clang -fsyntax-only %s 2>&1 | FileCheck %s
 // REQUIRES: crash-recovery
+// XFAIL: mingw32,win32
 
 #pragma clang __debug parser_crash
 // CHECK: Preprocessed source(s) and associated run script(s) are located at:
