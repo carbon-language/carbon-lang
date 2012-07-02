36e31cca7813317bafd472e6b580ac9db3fd6fe4
Bob Wilson <bob.wilson@apple.com>
2012-07-02 19:48:18 +0000

    Add a missing forward declaration of PassManagerBase.
    llvm-svn: 159566

--- llvm/include/llvm/CodeGen/Passes.h
+++ llvm/include/llvm/CodeGen/Passes.h
@@ -24,6 +24,7 @@ namespace llvm {
   class FunctionPass;
   class MachineFunctionPass;
   class PassInfo;
+  class PassManagerBase;
   class TargetLowering;
   class TargetRegisterClass;
   class raw_ostream;
