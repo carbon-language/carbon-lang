b2c42807a44a6efa554796d2dca1f3ac2d798bc6
Benjamin Kramer <benny.kra@googlemail.com>
2012-07-06 15:27:25 +0000

    Remove unreachable default case to pacify clang's -Wcovered-switch-default.
    llvm-svn: 159829

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -3636,7 +3636,6 @@ public:
   virtual void getArchDefines(const LangOptions &Opts,
                               MacroBuilder &Builder) const {
     switch (FloatABI) {
-    default:
     case HardFloat:
       Builder.defineMacro("__mips_hard_float", Twine(1));
       break;
