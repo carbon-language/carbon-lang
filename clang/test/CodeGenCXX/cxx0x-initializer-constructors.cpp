61d926859585db6acc38b79776ddb03c90a664d4
NAKAMURA Takumi <geek4civic@gmail.com>
2012-07-07 10:25:42 +0000

    clang/test/CodeGenCXX/cxx0x-initializer-constructors.cpp: Fixup for -Asserts.
    llvm-svn: 159898

--- clang/test/CodeGenCXX/cxx0x-initializer-constructors.cpp
+++ clang/test/CodeGenCXX/cxx0x-initializer-constructors.cpp
@@ -23,15 +23,15 @@ void fn3() {
   // CHECK: define void @_Z3fn3v
   S sa[] { { 1 }, { 2 }, { 3 } };
   // CHECK: alloca [3 x %struct.S], align 1
-  // CHECK: call void @_ZN1SC1Ei(%struct.S* %arrayinit.begin, i32 1)
-  // CHECK: call void @_ZN1SC1Ei(%struct.S* %arrayinit.element, i32 2)
-  // CHECK: call void @_ZN1SC1Ei(%struct.S* %arrayinit.element1, i32 3)
+  // CHECK: call void @_ZN1SC1Ei(%struct.S* %{{.+}}, i32 1)
+  // CHECK: call void @_ZN1SC1Ei(%struct.S* %{{.+}}, i32 2)
+  // CHECK: call void @_ZN1SC1Ei(%struct.S* %{{.+}}, i32 3)
 }
 
 void fn4() {
   // CHECK: define void @_Z3fn4v
   S sa[] { { 1, 2.0, 3.0 }, { 4, 5.0, 6.0 } };
   // CHECK: alloca [2 x %struct.S], align 1
-  // CHECK: call void @_ZN1SC1Eidd(%struct.S* %arrayinit.begin, i32 1, double 2.000000e+00, double 3.000000e+00)
-  // CHECK: call void @_ZN1SC1Eidd(%struct.S* %arrayinit.element, i32 4, double 5.000000e+00, double 6.000000e+00)
+  // CHECK: call void @_ZN1SC1Eidd(%struct.S* %{{.+}}, i32 1, double 2.000000e+00, double 3.000000e+00)
+  // CHECK: call void @_ZN1SC1Eidd(%struct.S* %{{.+}}, i32 4, double 5.000000e+00, double 6.000000e+00)
 }
