934a1c02312af4a45693815513c3cfb110ff67db
Eric Christopher <echristo@apple.com>
2012-07-14 19:29:12 +0000

    Capitalize comment.
    llvm-svn: 160220

--- clang/lib/CodeGen/CGBuiltin.cpp
+++ clang/lib/CodeGen/CGBuiltin.cpp
@@ -1629,7 +1629,7 @@ Value *CodeGenFunction::EmitARMBuiltinExpr(unsigned BuiltinID,
   }
   case ARM::BI__builtin_neon_vclz_v:
   case ARM::BI__builtin_neon_vclzq_v: {
-    // generate target-independent intrinsic; also need to add second argument
+    // Generate target-independent intrinsic; also need to add second argument
     // for whether or not clz of zero is undefined; on ARM it isn't.
     Function *F = CGM.getIntrinsic(Intrinsic::ctlz, Ty);
     Ops.push_back(Builder.getInt1(Target.isCLZForZeroUndef()));
