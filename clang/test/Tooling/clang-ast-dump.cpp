b7fa0d58fa31e21cb5cc38628dfc8e4178c69d87
NAKAMURA Takumi <geek4civic@gmail.com>
2012-07-17 00:33:36 +0000

    clang/test/Tooling/clang-ast-dump.cpp: Mark it as REQUIRES: asserts, for now.
    llvm-svn: 160332

--- clang/test/Tooling/clang-ast-dump.cpp
+++ clang/test/Tooling/clang-ast-dump.cpp
@@ -2,6 +2,9 @@
 // RUN: clang-ast-dump "%s" -f test_namespace::TheClass::theMethod -- -c > %t 2>&1
 // RUN: FileCheck --input-file=%t %s
 
+// FIXME: Does this run regardless of +Asserts?
+// REQUIRES: asserts
+
 // CHECK: <CXXMethod ptr="0x{{[0-9a-f]+}}" name="theMethod" prototype="true">
 // CHECK:  <ParmVar ptr="0x{{[0-9a-f]+}}" name="x" initstyle="c">
 // CHECK: (CompoundStmt
