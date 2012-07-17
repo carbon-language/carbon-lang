878f1445f99c71ead702d5fa88858217b9ce38ed
NAKAMURA Takumi <geek4civic@gmail.com>
2012-07-17 12:00:24 +0000

    test/Driver/crash-report.c: Remove "REQUIRES: shell". env(1) can be used also with gnuwin32.
    llvm-svn: 160362

--- clang/test/Driver/crash-report.c
+++ clang/test/Driver/crash-report.c
@@ -1,10 +1,9 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
-// RUN: TMPDIR=%t TEMP=%t TMP=%t %clang -fsyntax-only %s -DFOO=BAR 2>&1 | FileCheck %s
+// RUN: env TMPDIR=%t TEMP=%t TMP=%t %clang -fsyntax-only %s -DFOO=BAR 2>&1 | FileCheck %s
 // RUN: cat %t/crash-report-*.c | FileCheck --check-prefix=CHECKSRC %s
 // RUN: cat %t/crash-report-*.sh | FileCheck --check-prefix=CHECKSH %s
 // REQUIRES: crash-recovery
-// REQUIRES: shell
 
 #pragma clang __debug parser_crash
 // CHECK: Preprocessed source(s) and associated run script(s) are located at:
