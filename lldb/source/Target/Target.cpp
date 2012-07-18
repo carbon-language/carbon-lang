b83e72097f6aac394b715076316b9e1a386311a4
Jim Ingham <jingham@apple.com>
2012-07-18 18:42:57 +0000

    Add useful documentation to the "target.source-map" setting. <rdar://problem/11893881>
    llvm-svn: 160442

--- lldb/source/Target/Target.cpp
+++ lldb/source/Target/Target.cpp
@@ -2915,7 +2915,11 @@ Target::SettingsController::instance_settings_table[] =
     { TSC_PREFER_DYNAMIC    , eSetVarTypeEnum   , NULL          , g_dynamic_value_types, false, false, "Should printed values be shown as their dynamic value." },
     { TSC_ENABLE_SYNTHETIC  , eSetVarTypeBoolean, "true"        , NULL,                  false, false, "Should synthetic values be used by default whenever available." },
     { TSC_SKIP_PROLOGUE     , eSetVarTypeBoolean, "true"        , NULL,                  false, false, "Skip function prologues when setting breakpoints by name." },
-    { TSC_SOURCE_MAP        , eSetVarTypeArray  , NULL          , NULL,                  false, false, "Source path remappings to use when locating source files from debug information." },
+    { TSC_SOURCE_MAP        , eSetVarTypeArray  , NULL          , NULL,                  false, false, "Source path remappings used to track the change of location between a source file when built, and "
+                                                                                                       "where it exists on the current system.  It consists of an array of duples, the first element of each duple is "
+                                                                                                       "some part (starting at the root) of the path to the file when it was built, "
+                                                                                                       "and the second is where the remainder of the original build hierarchy is rooted on the local system.  "
+                                                                                                       "Each element of the array is checked in order and the first one that results in a match wins." },
     { TSC_EXE_SEARCH_PATHS  , eSetVarTypeArray  , NULL          , NULL,                  false, false, "Executable search paths to use when locating executable files whose paths don't match the local file system." },
     { TSC_MAX_CHILDREN      , eSetVarTypeInt    , "256"         , NULL,                  true,  false, "Maximum number of children to expand in any level of depth." },
     { TSC_MAX_STRLENSUMMARY , eSetVarTypeInt    , "1024"        , NULL,                  true,  false, "Maximum number of characters to show when using %s in summary strings." },
