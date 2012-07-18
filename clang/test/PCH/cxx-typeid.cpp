68777ac2534d5219acb20b6160b48d2d51b8571e
Aaron Ballman <aaron@aaronballman.com>
2012-07-18 21:56:43 +0000

    Fixing up a test case that was failing due to the lack of -std=c++11
    llvm-svn: 160455

--- clang/test/PCH/cxx-typeid.cpp
+++ clang/test/PCH/cxx-typeid.cpp
@@ -1,8 +1,8 @@
 // Test this without pch.
-// RUN: %clang -include %S/cxx-typeid.h -fsyntax-only -Xclang -verify %s
+// RUN: %clang -include %S/cxx-typeid.h -fsyntax-only -std=c++11 -Xclang -verify %s
 
-// RUN: %clang -ccc-pch-is-pch -x c++-header -o %t.gch %S/cxx-typeid.h
-// RUN: %clang -ccc-pch-is-pch -include %t -fsyntax-only -Xclang -verify %s 
+// RUN: %clang -ccc-pch-is-pch -std=c++11 -x c++-header -o %t.gch %S/cxx-typeid.h
+// RUN: %clang -ccc-pch-is-pch -std=c++11 -include %t -fsyntax-only -Xclang -verify %s
 
 void f() {
     (void)typeid(int);
