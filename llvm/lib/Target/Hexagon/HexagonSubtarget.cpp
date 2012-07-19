221e07e140aef4e284489dec8b8417a117540b6c
Sebastian Pop <spop@codeaurora.org>
2012-07-19 18:24:50 +0000

    default to use -mv4 when no version of Hexagon has been specified
    This fixes a bunch of make check failures of the form:
    
    Unknown Architecture Version.
    UNREACHABLE executed at ../lib/Target/Hexagon/HexagonSubtarget.cpp:60!
    
    llvm-svn: 160518

--- llvm/lib/Target/Hexagon/HexagonSubtarget.cpp
+++ llvm/lib/Target/Hexagon/HexagonSubtarget.cpp
@@ -55,7 +55,11 @@ HexagonSubtarget::HexagonSubtarget(StringRef TT, StringRef CPU, StringRef FS):
   case HexagonSubtarget::V5:
     break;
   default:
-    llvm_unreachable("Unknown Architecture Version.");
+    // If the programmer has not specified a Hexagon version, default
+    // to -mv4.
+    CPUString = "hexagonv4";
+    HexagonArchVersion = HexagonSubtarget::V4;
+    break;
   }
 
   // Initialize scheduling itinerary for the specified CPU.
