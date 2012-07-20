712de82154b6770a472ae78386273c6fd8ddf9b1
Daniel Dunbar <daniel@zuster.org>
2012-07-20 18:29:38 +0000

    Process: Add sys::Process::FileDescriptorHasColors().
    llvm-svn: 160557

--- llvm/include/llvm/Support/Process.h
+++ llvm/include/llvm/Support/Process.h
@@ -97,6 +97,10 @@ namespace sys {
       /// the user rather than being put on a pipe or stored in a file.
       static bool FileDescriptorIsDisplayed(int fd);
 
+      /// This function determines if the given file descriptor is displayd and
+      /// supports colors.
+      static bool FileDescriptorHasColors(int fd);
+
       /// This function determines the number of columns in the window
       /// if standard output is connected to a "tty" or "console"
       /// window. If standard output is not connected to a tty or
