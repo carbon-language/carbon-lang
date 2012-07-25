2538bf111eeda6e43ba4ea79449be68b5a1648f6
NAKAMURA Takumi <geek4civic@gmail.com>
2012-07-25 11:14:11 +0000

    test/PCH/objc_stmts.m: Suppress stderr to be fed to FileCheck.
    llvm-svn: 160720

--- clang/test/PCH/objc_stmts.m
+++ clang/test/PCH/objc_stmts.m
@@ -1,11 +1,11 @@
 // Test this without pch.
 // RUN: %clang_cc1 -include %S/objc_stmts.h -emit-llvm -fobjc-exceptions -o - %s
-// RUN: %clang_cc1 -include %S/objc_stmts.h -ast-dump -fobjc-exceptions -o - %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -include %S/objc_stmts.h -ast-dump -fobjc-exceptions -o - %s | FileCheck %s
 
 // Test with pch.
 // RUN: %clang_cc1 -x objective-c -emit-pch -fobjc-exceptions -o %t %S/objc_stmts.h
 // RUN: %clang_cc1 -include-pch %t -emit-llvm -fobjc-exceptions -o - %s 
-// RUN: %clang_cc1 -include-pch %t -ast-dump -fobjc-exceptions -o - %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -include-pch %t -ast-dump -fobjc-exceptions -o - %s | FileCheck %s
 
 // CHECK: catch parm = "A *a"
 // CHECK: catch parm = "B *b"
