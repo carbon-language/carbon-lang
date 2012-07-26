c2a3223da93b490966467620b9071ad33a53ec09
Jordan Rose <jordan_rose@apple.com>
2012-07-26 22:31:59 +0000

    SATestBuild should not ask for non-existent checkers.
    - "cocoa" was moved to "osx.cocoa" a long time ago.
    - "cplusplus" would be a valid package except we don't have any C++ checkers.
    
    llvm-svn: 160821

--- clang/utils/analyzer/SATestBuild.py
+++ clang/utils/analyzer/SATestBuild.py
@@ -72,7 +72,7 @@ SBOutputDirReferencePrefix = "Ref"
 
 # The list of checkers used during analyzes.
 # Currently, consists of all the non experimental checkers.
-Checkers="experimental.security.taint,core,deadcode,cplusplus,security,unix,osx,cocoa"
+Checkers="experimental.security.taint,core,deadcode,security,unix,osx"
 
 Verbose = 1
 
