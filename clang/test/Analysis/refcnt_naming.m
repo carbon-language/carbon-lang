160dcdf2d67a5cac3922d1f1e20f97aa61d842dc
Anna Zaks <ganna@apple.com>
2012-07-30 23:48:31 +0000

    [analyzer] Only run this test with no IPA
    The inlinable portion of the test relies on attributes/instead of the
    function implementation.
    
    llvm-svn: 161011

--- clang/test/Analysis/refcnt_naming.m
+++ clang/test/Analysis/refcnt_naming.m
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -analyze -analyzer-checker=core,osx.cocoa.RetainCount,experimental.core -analyzer-store=region -verify %s
+// RUN: %clang_cc1 -analyze -analyzer-checker=core,osx.cocoa.RetainCount,experimental.core -analyzer-ipa=none -analyzer-store=region -verify %s
 
 typedef const struct __CFString * CFStringRef;
 typedef const struct __CFAllocator * CFAllocatorRef;
