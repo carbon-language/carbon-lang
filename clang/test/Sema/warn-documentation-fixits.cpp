b0ddb66a8d77291f3e32483f42a8a48cac706b97
Dmitri Gribenko <gribozavr@gmail.com>
2012-08-01 23:29:52 +0000

    Comment diagnostics fixit tests: move exected-warning constructs from the comment being tested.
    llvm-svn: 161144

--- clang/test/Sema/warn-documentation-fixits.cpp
+++ clang/test/Sema/warn-documentation-fixits.cpp
@@ -1,10 +1,12 @@
 // RUN: %clang_cc1 -fsyntax-only -Wdocumentation -verify %s
 // RUN: %clang_cc1 -fsyntax-only -Wdocumentation -fdiagnostics-parseable-fixits %s 2>&1 | FileCheck %s
 
-/// \param ZZZZZZZZZZ Blah blah. expected-warning {{parameter 'ZZZZZZZZZZ' not found in the function declaration}}  expected-note {{did you mean 'a'?}}
+// expected-warning@+1 {{parameter 'ZZZZZZZZZZ' not found in the function declaration}} expected-note@+1 {{did you mean 'a'?}}
+/// \param ZZZZZZZZZZ Blah blah.
 int test1(int a);
 
-/// \param aab Blah blah. expected-warning {{parameter 'aab' not found in the function declaration}}  expected-note {{did you mean 'aaa'?}}
+// expected-warning@+1 {{parameter 'aab' not found in the function declaration}} expected-note@+1 {{did you mean 'aaa'?}}
+/// \param aab Blah blah.
 int test2(int aaa, int bbb);
 
 // expected-warning@+1 {{template parameter 'ZZZZZZZZZZ' not found in the template declaration}} expected-note@+1 {{did you mean 'T'?}}
@@ -18,8 +20,8 @@ void test3(T aaa);
 template<typename SomeTy, typename OtherTy>
 void test4(SomeTy aaa, OtherTy bbb);
 
-// CHECK: fix-it:"{{.*}}":{4:12-4:22}:"a"
-// CHECK: fix-it:"{{.*}}":{7:12-7:15}:"aaa"
-// CHECK: fix-it:"{{.*}}":{11:13-11:23}:"T"
-// CHECK: fix-it:"{{.*}}":{16:13-16:18}:"SomeTy"
+// CHECK: fix-it:"{{.*}}":{5:12-5:22}:"a"
+// CHECK: fix-it:"{{.*}}":{9:12-9:15}:"aaa"
+// CHECK: fix-it:"{{.*}}":{13:13-13:23}:"T"
+// CHECK: fix-it:"{{.*}}":{18:13-18:18}:"SomeTy"
 
