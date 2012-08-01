8c9a6bcae6114a4682ea90fedb87397fa08ea0f4
Manuel Klimek <klimek@google.com>
2012-08-01 08:48:27 +0000

    Fix typo.
    llvm-svn: 161106

--- clang/test/Tooling/pch.cpp
+++ clang/test/Tooling/pch.cpp
@@ -5,7 +5,7 @@
 // a stat cache into the FileManager:
 // RUN: %clang -x c++-header %S/Inputs/pch.h -o %t1
 
-// Use the generated pch and enforce a subsequent stat miss by by using
+// Use the generated pch and enforce a subsequent stat miss by using
 // the test file with an unrelated include as second translation unit:
 // Do not directly pipe into FileCheck, as that would hide errors from
 // valgrind due to pipefail not being set in lit.
