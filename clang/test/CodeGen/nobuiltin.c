c65c24b1786dc8209d94f7ed857a3670e4ff006b
Nuno Lopes <nunoplopes@sapo.pt>
2012-08-02 12:12:26 +0000

    add a comment to explain this test case
    llvm-svn: 161172

--- clang/test/CodeGen/nobuiltin.c
+++ clang/test/CodeGen/nobuiltin.c
@@ -1,7 +1,8 @@
 // RUN: %clang_cc1 -fno-builtin -O1 -S -o - %s | FileCheck %s
 
-void fn() {
+void PR13497() {
   char content[2];
+  // make sure we don't optimize this call to strcpy()
   // CHECK: __strcpy_chk
   __builtin___strcpy_chk(content, "", 1);
 }
