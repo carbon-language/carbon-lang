86bedb0de00508c872dbf9f1b4835efd64bae2fd
Jordan Rose <jordan_rose@apple.com>
2012-08-03 23:08:36 +0000

    [analyzer] Add a test for copy-constructor inlining.
    llvm-svn: 161274

--- clang/test/Analysis/ctor-inlining.mm
+++ clang/test/Analysis/ctor-inlining.mm
@@ -1,4 +1,6 @@
-// RUN: %clang_cc1 -analyze -analyzer-checker=core -fobjc-arc -cfg-add-implicit-dtors -Wno-null-dereference -verify %s
+// RUN: %clang_cc1 -analyze -analyzer-checker=core,debug.ExprInspection -fobjc-arc -cfg-add-implicit-dtors -Wno-null-dereference -verify %s
+
+void clang_analyzer_eval(bool);
 
 struct Wrapper {
   __strong id obj;
@@ -9,3 +11,31 @@ void test() {
   // force a diagnostic
   *(char *)0 = 1; // expected-warning{{Dereference of null pointer}}
 }
+
+
+struct IntWrapper {
+  int x;
+};
+
+void testCopyConstructor() {
+  IntWrapper a;
+  a.x = 42;
+
+  IntWrapper b(a);
+  clang_analyzer_eval(b.x == 42); // expected-warning{{TRUE}}
+}
+
+struct NonPODIntWrapper {
+  int x;
+
+  virtual int get();
+};
+
+void testNonPODCopyConstructor() {
+  NonPODIntWrapper a;
+  a.x = 42;
+
+  NonPODIntWrapper b(a);
+  clang_analyzer_eval(b.x == 42); // expected-warning{{TRUE}}
+}
+
