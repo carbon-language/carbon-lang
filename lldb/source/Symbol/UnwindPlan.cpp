4210713491e1986dc6878a1bf86ad42b41b11bb2
Jason Molenda <jmolenda@apple.com>
2012-08-10 20:52:59 +0000

    Remove a little unuseful output from the UnwindPlan::Row::Dump and UnwindPlan::Dump methods.
    llvm-svn: 161696

--- lldb/source/Symbol/UnwindPlan.cpp
+++ lldb/source/Symbol/UnwindPlan.cpp
@@ -172,7 +172,7 @@ UnwindPlan::Row::Dump (Stream& s, const UnwindPlan* unwind_plan, Thread* thread,
         s.Printf ("%s", reg_info->name);
     else
         s.Printf ("reg(%u)", GetCFARegister());
-    s.Printf ("%+3d =>", GetCFAOffset ());
+    s.Printf ("%+3d => ", GetCFAOffset ());
     for (collection::const_iterator idx = m_register_locations.begin (); idx != m_register_locations.end (); ++idx)
     {
         reg_info = unwind_plan->GetRegisterInfo (thread, idx->first);
@@ -399,21 +399,6 @@ UnwindPlan::Dump (Stream& s, Thread *thread, lldb::addr_t base_addr) const
         m_plan_valid_address_range.Dump (&s, target_sp.get(), Address::DumpStyleSectionNameOffset);
         s.EOL();
     }
-    else
-    {
-        s.PutCString ("No valid address range recorded for this UnwindPlan.\n");
-    }
-    s.Printf ("UnwindPlan register kind %d", m_register_kind);
-    switch (m_register_kind)
-    {
-        case eRegisterKindGCC:      s.PutCString (" [eRegisterKindGCC]"); break;
-        case eRegisterKindDWARF:    s.PutCString (" [eRegisterKindDWARF]"); break;
-        case eRegisterKindGeneric:  s.PutCString (" [eRegisterKindGeneric]"); break;
-        case eRegisterKindGDB:      s.PutCString (" [eRegisterKindGDB]"); break;
-        case eRegisterKindLLDB:     s.PutCString (" [eRegisterKindLLDB]"); break;
-        default: s.PutCString (" [eRegisterKind???]"); break; 
-    }
-    s.EOL();
     collection::const_iterator pos, begin = m_row_list.begin(), end = m_row_list.end();
     for (pos = begin; pos != end; ++pos)
     {
