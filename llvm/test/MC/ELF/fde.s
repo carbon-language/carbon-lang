1dcfe449ef5ec5258e3555700f06a5337bbd5c40
Nick Lewycky <nicholas@mxc.ca>
2012-08-12 08:21:27 +0000

    Give this test an explicit triple.
    llvm-svn: 161740

--- llvm/test/MC/ELF/fde.s
+++ llvm/test/MC/ELF/fde.s
@@ -1,4 +1,4 @@
-# RUN: llvm-mc -filetype=obj %s -o %t.o && llvm-objdump -s %t.o
+# RUN: llvm-mc -filetype=obj %s -o %t.o -triple x86_64-pc-linux-gnu && llvm-objdump -s %t.o
 # PR13581
 
 # CHECK: Contents of section .debug_frame:
