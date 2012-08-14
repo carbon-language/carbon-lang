bf2bfa5aa4f1c8051a5bc0bbb94ad03ff98fd028
Alexey Samsonov <samsonov@google.com>
2012-08-14 13:22:58 +0000

    [ASan] support for running OS-specific tests using lit, port clone_test as an example
    llvm-svn: 161864

---
+++ compiler-rt/lib/asan/lit_tests/Linux/clone_test.cc
@@ -0,0 +1,33 @@
+// Regression test for:
+// http://code.google.com/p/address-sanitizer/issues/detail?id=37
+
+// RUN: %clang_asan -O2 %s -o %t
+// RUN: %t | FileCheck %s
+
+#include <stdio.h>
+#include <sched.h>
+#include <sys/syscall.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int Child(void *arg) {
+  char x[32] = {0};  // Stack gets poisoned.
+  printf("Child:  %p\n", x);
+  _exit(1);  // NoReturn, stack will remain unpoisoned unless we do something.
+}
+
+int main(int argc, char **argv) {
+  const int kStackSize = 1 << 20;
+  char child_stack[kStackSize + 1];
+  char *sp = child_stack + kStackSize;  // Stack grows down.
+  printf("Parent: %p\n", sp);
+  pid_t clone_pid = clone(Child, sp, CLONE_FILES | CLONE_VM, NULL, 0, 0, 0);
+  waitpid(clone_pid, NULL, 0);
+  for (int i = 0; i < kStackSize; i++)
+    child_stack[i] = i;
+  int ret = child_stack[argc - 1];
+  printf("PASSED\n");
+  // CHECK: PASSED
+  return ret;
+}
