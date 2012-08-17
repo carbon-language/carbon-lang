0e72d5c3e52a4e83692949b58038e4e38531ef2d
Benjamin Kramer <benny.kra@googlemail.com>
2012-08-17 16:39:13 +0000

    Define __builtin_ffs[ll] with a signed argument instead of unsigned.
    GCC documents these as unsigned, but defines them as signed.
    
    llvm-svn: 162106

--- clang/test/Sema/builtins-decl.c
+++ clang/test/Sema/builtins-decl.c
@@ -6,3 +6,8 @@
 extern unsigned int __builtin_ia32_crc32qi (unsigned int, unsigned char);
 extern unsigned int __builtin_ia32_crc32hi (unsigned int, unsigned short);
 extern unsigned int __builtin_ia32_crc32si (unsigned int, unsigned int);
+
+// GCC documents these as unsigned, but they are defined with a signed argument.
+extern int __builtin_ffs(int);
+extern int __builtin_ffsl(long);
+extern int __builtin_ffsll(long long);
