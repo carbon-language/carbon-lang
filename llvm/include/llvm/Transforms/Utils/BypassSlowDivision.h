ee2b3259f125023faa2ba62c44414b9a48188304
Jakub Staszak <kubastaszak@gmail.com>
2012-09-04 19:49:17 +0000

    Remove unneeded code.
    llvm-svn: 163160

--- llvm/include/llvm/Transforms/Utils/BypassSlowDivision.h
+++ llvm/include/llvm/Transforms/Utils/BypassSlowDivision.h
@@ -27,32 +27,3 @@ bool bypassSlowDivision(llvm::Function &F,
                         const llvm::DenseMap<llvm::Type *, llvm::Type *> &BypassTypeMap);
 
 #endif
-//===- llvm/Transforms/Utils/BypassSlowDivision.h --------------*- C++ -*-===//
-//
-//                     The LLVM Compiler Infrastructure
-//
-// This file is distributed under the University of Illinois Open Source
-// License. See LICENSE.TXT for details.
-//
-//===----------------------------------------------------------------------===//
-//
-// This file contains an optimization for div and rem on architectures that
-// execute short instructions significantly faster than longer instructions.
-// For example, on Intel Atom 32-bit divides are slow enough that during
-// runtime it is profitable to check the value of the operands, and if they are
-// positive and less than 256 use an unsigned 8-bit divide.
-//
-//===----------------------------------------------------------------------===//
-
-#ifndef TRANSFORMS_UTILS_BYPASSSLOWDIVISION_H
-#define TRANSFORMS_UTILS_BYPASSSLOWDIVISION_H
-
-#include "llvm/Function.h"
-
-/// This optimization identifies DIV instructions that can be
-/// profitably bypassed and carried out with a shorter, faster divide.
-bool bypassSlowDivision(llvm::Function &F,
-                        llvm::Function::iterator &I,
-                        const llvm::DenseMap<llvm::Type *, llvm::Type *> &BypassTypeMap);
-
-#endif
