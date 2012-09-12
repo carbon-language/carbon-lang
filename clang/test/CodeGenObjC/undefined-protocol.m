cb0a08ac77094108bf3db2a848d2dacfaff3baf8
NAKAMURA Takumi <geek4civic@gmail.com>
2012-09-12 14:26:42 +0000

    clang/test: [PR13820] Suppress LLP64-incompatible tests for ObjC with +Asserts.
    llvm-svn: 163709

--- clang/test/CodeGenObjC/undefined-protocol.m
+++ clang/test/CodeGenObjC/undefined-protocol.m
@@ -1,5 +1,8 @@
 // RUN: %clang_cc1 -emit-llvm-only -fobjc-runtime=gcc %s
 
+// PR13820
+// REQUIRES: LP64
+
 @protocol MadeUpProtocol;
 
 @interface Object <MadeUpProtocol> @end
--- clang/test/Driver/objc-cpp-output.m
+++ clang/test/Driver/objc-cpp-output.m
@@ -1,5 +1,8 @@
 // RUN: %clang -x objc-cpp-output -c %s -o /dev/null
 
+// PR13820
+// REQUIRES: LP64
+
 // Should compile without errors
 @protocol P
 - (void)m;
