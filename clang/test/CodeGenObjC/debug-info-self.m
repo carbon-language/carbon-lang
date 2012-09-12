31ce693f936810de870d03964ec8c9ec04f4e4b6
Eric Christopher <echristo@apple.com>
2012-09-12 23:36:49 +0000

    Reapply:
        Make clang emit a flag for DW_AT_object_pointer for the artificial
        args where it should (implicit first arguments). FileCheck-ize a
        test as well and update tests to take into account the object
        pointer flag.
    
        rdar://9797999
    
    llvm-svn: 163755

--- clang/test/CodeGenObjC/debug-info-self.m
+++ clang/test/CodeGenObjC/debug-info-self.m
@@ -1,7 +1,11 @@
-// RUN: %clang -fverbose-asm -g -S %s -o - | grep DW_AT_artificial | count 3
+// RUN: %clang -cc1 -emit-llvm -g %s -o - | FileCheck %s
 // self and _cmd are marked as DW_AT_artificial. 
-// abbrev code emits another DT_artificial comment.
 // myarg is not marked as DW_AT_artificial.
+
+// CHECK: metadata !{i32 {{.*}}, metadata !9, metadata !"self", metadata !15, i32 16777232, metadata !30, i32 1088, i32 0} ; [ DW_TAG_arg_variable ] [self] [line 16]
+// CHECK: metadata !{i32 {{.*}}, metadata !9, metadata !"_cmd", metadata !15, i32 33554448, metadata !33, i32 64, i32 0} ; [ DW_TAG_arg_variable ] [_cmd] [line 16]
+// CHECK: metadata !{i32 {{.*}}, metadata !9, metadata !"myarg", metadata !6, i32 50331664, metadata !24, i32 0, i32 0} ; [ DW_TAG_arg_variable ] [myarg] [line 16]
+
 
 @interface MyClass {
 }
