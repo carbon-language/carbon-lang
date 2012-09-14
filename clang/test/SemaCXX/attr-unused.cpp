8beb26e4fa3493fe1772f1b7c88de99cbaf2e438
Douglas Gregor <dgregor@apple.com>
2012-09-14 16:38:59 +0000

    Add missing test from r163874.
    llvm-svn: 163910

---
+++ clang/test/SemaCXX/attr-unused.cpp
@@ -0,0 +1,9 @@
+// RUN: %clang_cc1 -verify -Wunused -Wused-but-marked-unused -fsyntax-only %s
+
+namespace ns_unused { typedef int Int_unused __attribute__((unused)); }
+namespace ns_not_unused { typedef int Int_not_unused; }
+
+void f() {
+  ns_not_unused::Int_not_unused i1; // expected-warning {{unused variable}}
+  ns_unused::Int_unused i0; // expected-warning {{'Int_unused' was marked unused but was used}}
+}
