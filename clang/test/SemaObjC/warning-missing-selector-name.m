8439c78fa5a17a0f7f94e585b65c81dc37092a8e
Fariborz Jahanian <fjahanian@apple.com>
2012-09-17 23:24:36 +0000

    objective-C: add Doug's test for my last patch.
    llvm-svn: 164079

--- clang/test/SemaObjC/warning-missing-selector-name.m
+++ clang/test/SemaObjC/warning-missing-selector-name.m
@@ -12,9 +12,14 @@
 -(void) Name1:(id)Arg1 Name2:(id) Name3:(id)Arg3; // expected-warning {{'Name3' used as the name of the previous parameter rather than as part of the selector}} \
 				   // expected-note {{introduce a parameter name to make 'Name3' part of the selector}} \
 				   // expected-note {{or insert whitespace before ':' to use 'Name3' as parameter name and have an empty entry in the selector}}
+- method:(id) second:(id)second; // expected-warning {{'second' used as the name of the previous parameter rather than as part of the selector}} \
+				   // expected-note {{introduce a parameter name to make 'second' part of the selector}} \
+				   // expected-note {{or insert whitespace before ':' to use 'second' as parameter name and have an empty entry in the selector}} \
+				   // expected-note {{method definition for 'method::' not found}}
+                                 
 @end
 
-@implementation INTF
+@implementation INTF // expected-warning {{incomplete implementation}}
 -(void) Name1:(id)Arg1 Name2:(id)Arg2{}
 -(void) Name1:(id) Name2:(id)Arg2 {} // expected-warning {{'Name2' used as the name of the previous parameter rather than as part of the selector}} \
 					// expected-note {{introduce a parameter name to make 'Name2' part of the selector}} \
@@ -23,4 +28,5 @@
 -(void) Name1:(id)Arg1 Name2:(id) Name3:(id)Arg3 {} // expected-warning {{'Name3' used as the name of the previous parameter rather than as part of the selector}} \
 					// expected-note {{introduce a parameter name to make 'Name3' part of the selector}} \
  					// expected-note {{or insert whitespace before ':' to use 'Name3' as parameter name and have an empty entry in the selector}}
+- method:(id)first second:(id)second {return 0; }
 @end
