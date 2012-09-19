dfc60de2b3ff22fa6d8151af758a2411b642f346
NAKAMURA Takumi <geek4civic@gmail.com>
2012-09-19 00:56:20 +0000

    llvm/test/MC/X86/x86_nop.s: Make sure -arch=x86 when -mcpu=geode.
    -mcpu doesn't infer -arch. Consider non-x86 host.
    
    llvm-svn: 164185

--- llvm/test/MC/X86/x86_nop.s
+++ llvm/test/MC/X86/x86_nop.s
@@ -1,4 +1,4 @@
-# RUN: llvm-mc -filetype=obj -mcpu=geode %s -o %t
+# RUN: llvm-mc -filetype=obj -arch=x86 -mcpu=geode %s -o %t
 # RUN: llvm-objdump -disassemble %t | FileCheck %s
 
 # CHECK-NOT: nopw
