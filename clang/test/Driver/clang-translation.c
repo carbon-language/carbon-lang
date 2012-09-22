ccaa3b6eb2da33458bb77cd34441cc4f6954a413
Rafael Espindola <rafael.espindola@gmail.com>
2012-09-22 22:51:00 +0000

    Use pipes.
    llvm-svn: 164469

--- clang/test/Driver/clang-translation.c
+++ clang/test/Driver/clang-translation.c
@@ -20,9 +20,8 @@
 // CORE2: "-target-cpu"
 // CORE2: "core2"
 
-// RUN: %clang -target x86_64-apple-darwin10 -### -S %s 2> %t.log \
-// RUN:   -arch armv7
-// RUN: FileCheck -check-prefix=ARMV7_DEFAULT %s < %t.log
+// RUN: %clang -target x86_64-apple-darwin10 -### -S %s -arch armv7 2>&1 | \
+// RUN: FileCheck -check-prefix=ARMV7_DEFAULT %s
 // ARMV7_DEFAULT: clang
 // ARMV7_DEFAULT: "-cc1"
 // ARMV7_DEFAULT-NOT: "-msoft-float"
@@ -30,9 +29,8 @@
 // ARMV7_DEFAULT-NOT: "-msoft-float"
 // ARMV7_DEFAULT: "-x" "c"
 
-// RUN: %clang -target x86_64-apple-darwin10 -### -S %s 2> %t.log \
-// RUN:   -arch armv7 -msoft-float
-// RUN: FileCheck -check-prefix=ARMV7_SOFTFLOAT %s < %t.log
+// RUN: %clang -target x86_64-apple-darwin10 -### -S %s -arch armv7 \
+// RUN: -msoft-float 2>&1 | FileCheck -check-prefix=ARMV7_SOFTFLOAT %s
 // ARMV7_SOFTFLOAT: clang
 // ARMV7_SOFTFLOAT: "-cc1"
 // ARMV7_SOFTFLOAT: "-msoft-float"
@@ -41,9 +39,8 @@
 // ARMV7_SOFTFLOAT: "-neon"
 // ARMV7_SOFTFLOAT: "-x" "c"
 
-// RUN: %clang -target x86_64-apple-darwin10 -### -S %s 2> %t.log \
-// RUN:   -arch armv7 -mhard-float
-// RUN: FileCheck -check-prefix=ARMV7_HARDFLOAT %s < %t.log
+// RUN: %clang -target x86_64-apple-darwin10 -### -S %s -arch armv7 \
+// RUN: -mhard-float 2>&1 | FileCheck -check-prefix=ARMV7_HARDFLOAT %s
 // ARMV7_HARDFLOAT: clang
 // ARMV7_HARDFLOAT: "-cc1"
 // ARMV7_HARDFLOAT-NOT: "-msoft-float"
@@ -51,48 +48,38 @@
 // ARMV7_HARDFLOAT-NOT: "-msoft-float"
 // ARMV7_HARDFLOAT: "-x" "c"
 
-// RUN: %clang -target arm-linux -### -S %s 2> %t.log \
-// RUN:   -march=armv5e
-// RUN: FileCheck -check-prefix=ARMV5E %s < %t.log
+// RUN: %clang -target arm-linux -### -S %s -march=armv5e 2>&1 | \
+// RUN: FileCheck -check-prefix=ARMV5E %s
 // ARMV5E: clang
 // ARMV5E: "-cc1"
 // ARMV5E: "-target-cpu" "arm1022e"
 
-// RUN: %clang -ccc-clang-archs powerpc64 \
-// RUN:   -target powerpc64-unknown-linux-gnu -### -S %s 2> %t.log \
-// RUN:   -mcpu=G5
-// RUN: FileCheck -check-prefix=PPCG5 %s < %t.log
+// RUN: %clang -ccc-clang-archs powerpc64 -target powerpc64-unknown-linux-gnu \
+// RUN: -### -S %s -mcpu=G5 2>&1 | FileCheck -check-prefix=PPCG5 %s
 // PPCG5: clang
 // PPCG5: "-cc1"
 // PPCG5: "-target-cpu" "g5"
 
-// RUN: %clang -ccc-clang-archs powerpc64 \
-// RUN:   -target powerpc64-unknown-linux-gnu -### -S %s 2> %t.log      \
-// RUN:   -mcpu=power7
-// RUN: FileCheck -check-prefix=PPCPWR7 %s < %t.log
+// RUN: %clang -ccc-clang-archs powerpc64 -target powerpc64-unknown-linux-gnu \
+// RUN: -### -S %s -mcpu=power7 2>&1 | FileCheck -check-prefix=PPCPWR7 %s
 // PPCPWR7: clang
 // PPCPWR7: "-cc1"
 // PPCPWR7: "-target-cpu" "pwr7"
 
-// RUN: %clang -ccc-clang-archs powerpc64 \
-// RUN:   -target powerpc64-unknown-linux-gnu -### -S %s 2> %t.log
-// RUN: FileCheck -check-prefix=PPC64NS %s < %t.log
+// RUN: %clang -ccc-clang-archs powerpc64 -target powerpc64-unknown-linux-gnu \
+// RUN: -### -S %s 2>&1 | FileCheck -check-prefix=PPC64NS %s
 // PPC64NS: clang
 // PPC64NS: "-cc1"
 // PPC64NS: "-target-cpu" "ppc64"
 
-// RUN: %clang -ccc-clang-archs powerpc \
-// RUN:   -target powerpc-fsl-linux -### -S %s 2> %t.log \
-// RUN:   -mcpu=e500mc
-// RUN: FileCheck -check-prefix=PPCE500MC %s < %t.log
+// RUN: %clang -ccc-clang-archs powerpc -target powerpc-fsl-linux -### -S %s \
+// RUN: -mcpu=e500mc 2>&1 | FileCheck -check-prefix=PPCE500MC %s
 // PPCE500MC: clang
 // PPCE500MC: "-cc1"
 // PPCE500MC: "-target-cpu" "e500mc"
 
-// RUN: %clang -ccc-clang-archs powerpc64 \
-// RUN:   -target powerpc64-fsl-linux -### -S %s 2> %t.log \
-// RUN:   -mcpu=e5500
-// RUN: FileCheck -check-prefix=PPCE5500 %s < %t.log
+// RUN: %clang -ccc-clang-archs powerpc64 -target powerpc64-fsl-linux -### -S \
+// RUN: %s -mcpu=e5500 2>&1 | FileCheck -check-prefix=PPCE5500 %s
 // PPCE5500: clang
 // PPCE5500: "-cc1"
 // PPCE5500: "-target-cpu" "e5500"
