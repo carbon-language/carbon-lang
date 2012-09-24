2e0e3f37be30d4be3fc7a490c0d746e8faadc7cc
Dmitri Gribenko <gribozavr@gmail.com>
2012-09-24 21:09:05 +0000

    More tests for r164545 (change extension warning about 'long long').
    llvm-svn: 164556

---
+++ clang/test/SemaCXX/warn-c++11-extensions.cpp
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 -fsyntax-only -std=c++98 -Wc++11-extensions -verify %s
+
+long long ll1 = // expected-warning {{'long long' is a C++11 extension}}
+         -42LL; // expected-warning {{'long long' is a C++11 extension}}
+unsigned long long ull1 = // expected-warning {{'long long' is a C++11 extension}}
+                   42ULL; // expected-warning {{'long long' is a C++11 extension}}
+
