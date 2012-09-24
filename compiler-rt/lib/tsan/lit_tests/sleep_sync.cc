351d486df02288a5a0b389a7683c93c664ca3d66
Alexey Samsonov <samsonov@google.com>
2012-09-24 13:35:46 +0000

    [TSan] one more attempt to fix sleep_sync test: call sleep in the spawned thread so that sleep-synchronization will be detected even if child thread is started late.
    llvm-svn: 164488

--- compiler-rt/lib/tsan/lit_tests/sleep_sync.cc
+++ compiler-rt/lib/tsan/lit_tests/sleep_sync.cc
@@ -1,23 +1,22 @@
-// FIXME: Understand why this test fails if TSan is re-execed.
 // RUN: %clangxx_tsan -O1 %s -o %t && %t 2>&1 | FileCheck %s
 #include <pthread.h>
 #include <unistd.h>
 
 int X = 0;
 
-void *Thread(void *p) {
-  X = 42;
-  return 0;
+void MySleep() {
+  usleep(50*1000);
 }
 
-void MySleep() {
-  usleep(300*1000);
+void *Thread(void *p) {
+  MySleep();  // Assume the main thread has done the write.
+  X = 42;
+  return 0;
 }
 
 int main() {
   pthread_t t;
   pthread_create(&t, 0, Thread, 0);
-  MySleep();  // Assume the thread has done the write.
   X = 43;
   pthread_join(t, 0);
   return 0;
@@ -28,4 +27,4 @@ int main() {
 // CHECK:   As if synchronized via sleep:
 // CHECK-NEXT:     #0 usleep
 // CHECK-NEXT:     #1 MySleep
-// CHECK-NEXT:     #2 main
+// CHECK-NEXT:     #2 Thread
