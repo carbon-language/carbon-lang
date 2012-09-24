34b46ca48e045976ab5f68d6f7aa5bd78abf465d
Alexander Potapenko <glider@google.com>
2012-09-24 10:25:24 +0000

    Use an explicit target to test that source fortification is off when building for Darwin with -faddress-sanitizer.
    llvm-svn: 164485

--- clang/test/Driver/darwin-asan-nofortify.c
+++ clang/test/Driver/darwin-asan-nofortify.c
@@ -1,5 +1,5 @@
 // Make sure AddressSanitizer disables _FORTIFY_SOURCE on Darwin.
 
-// REQUIRES: system-darwin
-// RUN: %clang -faddress-sanitizer %s -E -dM -o - | FileCheck %s
+// RUN: %clang -faddress-sanitizer %s -E -dM -target x86_64-darwin - | FileCheck %s
+
 // CHECK: #define _FORTIFY_SOURCE 0
