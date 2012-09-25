c980408c7174111fd3e0482e2ecabb75b99a0165
Hans Wennborg <hans@hanshq.net>
2012-09-25 10:11:17 +0000

    Fix failing test/Sema/wchar.c on ARM.
    Currently Sema/wchar.c fails because WCHAR_T_TYPE is defined as int,
    however on ARM wchar_t is unsigned int.
    
    This patch changes that, so this test passes for ARM.
    
    Patch by Joey Gouly!
    
    llvm-svn: 164598

--- clang/test/Sema/wchar.c
+++ clang/test/Sema/wchar.c
@@ -6,6 +6,8 @@ typedef __WCHAR_TYPE__ wchar_t;
 #if defined(_WIN32) || defined(_M_IX86) || defined(__CYGWIN__) \
  || defined(_M_X64) || defined(SHORT_WCHAR)
   #define WCHAR_T_TYPE unsigned short
+#elif defined(__arm)
+  #define WCHAR_T_TYPE unsigned int
 #elif defined(__sun) || defined(__AuroraUX__)
   #define WCHAR_T_TYPE long
 #else /* Solaris or AuroraUX. */
