17df40d484c650245b67a7e3fa2ae91354f5ea26
Jordan Rose <jordan_rose@apple.com>
2012-09-26 16:41:11 +0000

    Make our mini-stdint.h platform-independent by using predefined macros.
    This also adds a definition for uint64_t, which was causing build failures
    on some platforms. (I'm actually surprised this didn't happen on more
    builders, but maybe the search paths are different.)
    
    llvm-svn: 164706

--- clang/test/Headers/Inputs/include/stdint.h
+++ clang/test/Headers/Inputs/include/stdint.h
@@ -1,14 +1,18 @@
 #ifndef STDINT_H
 #define STDINT_H
 
-#if defined(__arm__) || defined(__i386__) || defined(__mips__)
-typedef unsigned int uint32_t;
-typedef unsigned int uintptr_t;
-#elif defined(__x86_64__)
-typedef unsigned int uint32_t;
-typedef unsigned long uintptr_t;
+#ifdef __INT32_TYPE__
+typedef unsigned __INT32_TYPE__ uint32_t;
+#endif
+
+#ifdef __INT64_TYPE__
+typedef unsigned __INT64_TYPE__ uint64_t;
+#endif
+
+#ifdef __INTPTR_TYPE__
+typedef unsigned __INTPTR_TYPE__ uintptr_t;
 #else
-#error "Unknown target architecture"
+#error Every target should have __INTPTR_TYPE__
 #endif
 
 #endif /* STDINT_H */
