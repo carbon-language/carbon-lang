010f114093353e6e3665cf01b741a9eaeebb41d4
Benjamin Kramer <benny.kra@googlemail.com>
2012-09-26 09:10:53 +0000

    Run test in a freestanding environment so it doesn't accidentally pick up system headers for the wrong target.
    While there add a test that verifies that the header parses in C++ mode.
    
    llvm-svn: 164679

--- clang/test/Headers/unwind.c
+++ clang/test/Headers/unwind.c
@@ -1,6 +1,5 @@
-// RUN: %clang -target arm-unknown-linux-gnueabi %s -fsyntax-only -o -
-// RUN: %clang -target i686-unknown-linux %s -fsyntax-only -o -
+// RUN: %clang -target arm-unknown-linux-gnueabi -ffreestanding -fsyntax-only %s
+// RUN: %clang -target i686-unknown-linux -ffreestanding -fsyntax-only %s
+// RUN: %clang -ffreestanding -fsyntax-only -x c++ %s
 
 #include "unwind.h"
-// CHECK-NOT: error
-// CHECK-NOT: warning
