ebca38a4c5f279caf6b3aa38adea5c2f005fc9cd
Filipe Cabecinhas <me@filcab.net>
2012-09-27 15:31:38 +0000

    Fixed a typo.
    llvm-svn: 164777

--- lldb/source/Plugins/ABI/SysV-x86_64/ABISysV_x86_64.cpp
+++ lldb/source/Plugins/ABI/SysV-x86_64/ABISysV_x86_64.cpp
@@ -589,7 +589,7 @@ ABISysV_x86_64::SetReturnValueObject(lldb::StackFrameSP &frame_sp, lldb::ValueOb
         DataExtractor data;
         size_t num_bytes = new_value_sp->GetData(data);
         uint32_t offset = 0;
-        if (num_bytes <= 64)
+        if (num_bytes <= 8)
         {
             uint64_t raw_value = data.GetMaxU64(&offset, num_bytes);
             
