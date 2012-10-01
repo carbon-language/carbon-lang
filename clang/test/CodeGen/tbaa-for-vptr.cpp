f6645c80719dc5a6bfc25ce58fb490e88abb5cca
Kostya Serebryany <kcc@google.com>
2012-10-01 08:39:00 +0000

    fix test/CodeGen/tbaa-for-vptr.cpp: don't hardcode the index of metadata
    llvm-svn: 164928

--- clang/test/CodeGen/tbaa-for-vptr.cpp
+++ clang/test/CodeGen/tbaa-for-vptr.cpp
@@ -21,7 +21,7 @@ void CallFoo(A *a) {
   a->foo();
 }
 
-// CHECK: %{{.*}} = load {{.*}} !tbaa !0
-// CHECK: store {{.*}} !tbaa !0
-// CHECK: = metadata !{metadata !"vtable pointer", metadata !{{.*}}}
+// CHECK: %{{.*}} = load {{.*}} !tbaa ![[NUM:[0-9]+]]
+// CHECK: store {{.*}} !tbaa ![[NUM]]
+// CHECK: [[NUM]] = metadata !{metadata !"vtable pointer", metadata !{{.*}}}
 // NOTBAA-NOT: = metadata !{metadata !"Simple C/C++ TBAA"}
