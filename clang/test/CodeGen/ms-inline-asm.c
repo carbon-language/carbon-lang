7768299b98ba4f5b8a4e3f627b521bb8f0e40173
Chad Rosier <mcrosier@apple.com>
2012-10-02 20:55:30 +0000

    [ms-inline asm] Fixup test case to be valid ms-style asm.  Fix whitespace.
    llvm-svn: 165045

--- clang/test/CodeGen/ms-inline-asm.c
+++ clang/test/CodeGen/ms-inline-asm.c
@@ -57,6 +57,7 @@ void t7() {
 // CHECK: call void asm sideeffect inteldialect "int $$0x2c", "~{dirflag},~{fpsr},~{flags}"() nounwind
 // CHECK: call void asm sideeffect inteldialect "", "~{dirflag},~{fpsr},~{flags}"() nounwind
 }
+
 int t8() {
   __asm int 3 ; } comments for single-line asm
   __asm {}
@@ -68,6 +69,7 @@ int t8() {
 // CHECK: call void asm sideeffect inteldialect "int $$4", "~{dirflag},~{fpsr},~{flags}"() nounwind
 // CHECK: ret i32 10
 }
+
 void t9() {
   __asm {
     push ebx
@@ -135,10 +137,10 @@ void t15(void) {
 // CHECK: call void asm sideeffect inteldialect "mov eax, DWORD PTR [eax]", "~{eax},~{dirflag},~{fpsr},~{flags}"() nounwind
 }
 
-void t16(unsigned long long V) {
+void t16(unsigned V) {
   __asm mov eax, DWORD PTR [V]
 // CHECK: t16
-// CHECK: call void asm sideeffect inteldialect "mov eax, DWORD PTR [$0]", "r,~{eax},~{dirflag},~{fpsr},~{flags}"(i64 %{{.*}}) nounwind
+// CHECK: call void asm sideeffect inteldialect "mov eax, DWORD PTR [$0]", "r,~{eax},~{dirflag},~{fpsr},~{flags}"(i32 %{{.*}}) nounwind
 }
 
 void t17(void) {
