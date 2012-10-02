5d62a7c5bb73b34e0b13e9967223608e54a5365b
NAKAMURA Takumi <geek4civic@gmail.com>
2012-10-02 00:11:53 +0000

    clang/test/Index: Fix two tests. Both %S and %t are expanded to absolute paths.
    llvm-svn: 164982

--- clang/test/Index/get-cursor-macro-args.m
+++ clang/test/Index/get-cursor-macro-args.m
@@ -6,12 +6,12 @@
 // RUN:       %s -include %S/get-cursor-macro-args.h | FileCheck %s
 
 // Test with PCH
-// RUN: c-index-test -write-pch %S/%t.pch -x objective-c-header %S/get-cursor-macro-args.h
+// RUN: c-index-test -write-pch %t.pch -x objective-c-header %S/get-cursor-macro-args.h
 // RUN: c-index-test -cursor-at=%S/get-cursor-macro-args.h:9:12 \
 // RUN:              -cursor-at=%S/get-cursor-macro-args.h:9:21 \
 // RUN:              -cursor-at=%S/get-cursor-macro-args.h:15:12 \
 // RUN:              -cursor-at=%S/get-cursor-macro-args.h:15:20 \
-// RUN:       %s -include-pch %S/%t.pch | FileCheck %s
+// RUN:       %s -include-pch %t.pch | FileCheck %s
 
 // CHECK:      ObjCClassRef=MyClass:1:12
 // CHECK-NEXT: ObjCMessageExpr=meth:2:8
