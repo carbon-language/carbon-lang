eba9c5c2596d14bf4ac61f6945814214b4eb9533
Alex Rosenberg <alexr@leftfield.org>
2012-10-05 23:12:48 +0000

    FileCheckize test
    llvm-svn: 165342

--- clang/test/CodeGen/alias.c
+++ clang/test/CodeGen/alias.c
@@ -1,28 +1,26 @@
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -o %t %s
-// RUN: grep '@g0 = common global i32 0' %t
-// RUN: grep '@f1 = alias void ()\* @f0' %t
-// RUN: grep '@g1 = alias i32\* @g0' %t
-// RUN: grep 'define void @f0() nounwind {' %t
+// RUN: %clang_cc1 -triple i386-pc-linux-gnu -emit-llvm -o - %s | FileCheck %s
+
+int g0;
+// CHECK: @g0 = common global i32 0
+static int bar1 = 42;
+// CHECK: @bar1 = internal global i32 42
+
+extern int g1;
+extern int g1 __attribute((alias("g0")));
+// CHECK: @g1 = alias i32* @g0
 
 void f0(void) { }
 extern void f1(void);
 extern void f1(void) __attribute((alias("f0")));
-
-int g0;
-extern int g1;
-extern int g1 __attribute((alias("g0")));
+// CHECK: @f1 = alias void ()* @f0
+// CHECK: define void @f0() nounwind {
 
 // Make sure that aliases cause referenced values to be emitted.
 // PR3200
-// RUN: grep 'define internal i32 @foo1()' %t
 static inline int foo1() { return 0; }
+// CHECK: define internal i32 @foo1()
 int foo() __attribute__((alias("foo1")));
-
-
-// RUN: grep '@bar1 = internal global i32 42' %t
-static int bar1 = 42;
 int bar() __attribute__((alias("bar1")));
-
 
 extern int test6();
 void test7() { test6(); }  // test6 is emitted as extern.
