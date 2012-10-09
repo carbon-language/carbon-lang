07cee6efc9f80d76df61c286485302bf0ff947de
Nadav Rotem <nrotem@apple.com>
2012-10-09 01:56:07 +0000

    whitespace
    llvm-svn: 165475

--- llvm/include/llvm/Target/Mangler.h
+++ llvm/include/llvm/Target/Mangler.h
@@ -54,13 +54,12 @@ public:
   /// symbol is the main label that is the address of the global.
   MCSymbol *getSymbol(const GlobalValue *GV);
 
-  
   /// getNameWithPrefix - Fill OutName with the name of the appropriate prefix
   /// and the specified global variable's name.  If the global variable doesn't
   /// have a name, this fills in a unique name for the global.
   void getNameWithPrefix(SmallVectorImpl<char> &OutName, const GlobalValue *GV,
                          bool isImplicitlyPrivate);
-  
+
   /// getNameWithPrefix - Fill OutName with the name of the appropriate prefix
   /// and the specified name as the global variable name.  GVName must not be
   /// empty.
