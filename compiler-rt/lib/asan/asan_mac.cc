e3e80d607f18f79d4e9f0db82af8ab3150fe2289
Alexander Potapenko <glider@google.com>
2012-10-16 16:58:10 +0000

    Do not call ReplaceCFAllocator() before __CFInitialize(), otherwise crashes are possible on 10.8. Fixes http://code.google.com/p/address-sanitizer/issues/detail?id=122
    llvm-svn: 166029

--- compiler-rt/lib/asan/asan_mac.cc
+++ compiler-rt/lib/asan/asan_mac.cc
@@ -131,7 +131,14 @@ bool AsanInterceptsSignal(int signum) {
 }
 
 void AsanPlatformThreadInit() {
-  ReplaceCFAllocator();
+  // For the first program thread, we can't replace the allocator before
+  // __CFInitialize() has been called. If it hasn't, we'll call
+  // ReplaceCFAllocator() later on this thread.
+  // For other threads __CFInitialize() has been called before their creation.
+  // See also asan_malloc_mac.cc.
+  if (((CFRuntimeBase*)kCFAllocatorSystemDefault)->_cfisa) {
+    ReplaceCFAllocator();
+  }
 }
 
 AsanLock::AsanLock(LinkerInitialized) {
