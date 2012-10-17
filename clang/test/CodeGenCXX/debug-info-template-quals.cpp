086da3411969af31c55faeecda722939ec99c4d9
Eric Christopher <echristo@gmail.com>
2012-10-17 17:37:17 +0000

    Revert r158009 since there are some uses of artificial functions in debug info.
    llvm-svn: 166109

--- clang/test/CodeGenCXX/debug-info-template-quals.cpp
+++ clang/test/CodeGenCXX/debug-info-template-quals.cpp
@@ -20,4 +20,4 @@ void foo (const char *c) {
 // CHECK: [[CH]] = metadata !{i32 {{.*}}, metadata !"char", {{.*}}} ; [ DW_TAG_base_type ] [char] [line 0, size 8, align 8, offset 0, enc DW_ATE_signed_char]
 // CHECK: metadata !{i32 {{.*}}, metadata !"_ZN12basic_stringIcE6assignEPKc", metadata !6, i32 7, metadata [[TYPE:.*]], i1 false, i1 true, i32 0, i32 0, null, i32 256, i1 false, %struct.basic_string* (%struct.basic_string*, i8*)* @_ZN12basic_stringIcE6assignEPKc, null, metadata !18, metadata !1, i32 8} ; [ DW_TAG_subprogram ] [line 7] [def] [scope 8] [assign]
 // CHECK: [[TYPE]] = metadata !{i32 {{.*}}, null, metadata [[ARGS:.*]], i32 0, i32 0}
-// CHECK: [[ARGS]] = metadata !{metadata !15, metadata !23, metadata [[P]]}
+// CHECK: [[ARGS]] = metadata !{metadata !15, metadata !24, metadata [[P]]}
