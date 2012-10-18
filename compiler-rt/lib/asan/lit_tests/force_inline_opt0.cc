580916eacb94c8f55edf416de5d60f46ed9dc863
Chandler Carruth <chandlerc@gmail.com>
2012-10-18 08:06:28 +0000

    Re-enable this test now that r166172 has hacked around the terrible limitations of the pass manager stack in the pass manager builder.
    llvm-svn: 166173

--- compiler-rt/lib/asan/lit_tests/force_inline_opt0.cc
+++ compiler-rt/lib/asan/lit_tests/force_inline_opt0.cc
@@ -1,7 +1,7 @@
 // This test checks that we are no instrumenting a memory access twice
 // (before and after inlining)
 // RUN: %clangxx_asan -m64 -O1 %s -o %t && %t
-// FIXME (enable this line): %clangxx_asan -m64 -O0 %s -o %t && %t
+// RUN: %clangxx_asan -m64 -O0 %s -o %t && %t
 __attribute__((always_inline))
 void foo(int *x) {
   *x = 0;
