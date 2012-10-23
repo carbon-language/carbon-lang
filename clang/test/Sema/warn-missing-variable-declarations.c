4e2e13edb76a1eb8322ca2043ef9653304160eea
Eli Friedman <eli.friedman@gmail.com>
2012-10-23 20:23:23 +0000

    Delete junk that snuck into r166498.
    llvm-svn: 166499

--- clang/test/Sema/warn-missing-variable-declarations.c
+++ clang/test/Sema/warn-missing-variable-declarations.c
@@ -16,21 +16,3 @@ extern int vbad4;
 extern int vgood1;
 int vgood1;
 int vgood1 = 10;
-// RUN: %clang -Wmissing-variable-declarations -fsyntax-only -Xclang -verify %s
-
-int vbad1; // expected-warning{{no previous extern declaration for non-static variable 'vbad1'}}
-
-int vbad2;
-int vbad2 = 10; // expected-warning{{no previous extern declaration for non-static variable 'vbad2'}}
-
-struct {
-  int mgood1;
-} vbad3; // expected-warning{{no previous extern declaration for non-static variable 'vbad3'}}
-
-int vbad4;
-int vbad4 = 10; // expected-warning{{no previous extern declaration for non-static variable 'vbad4'}}
-extern int vbad4;
-
-extern int vgood1;
-int vgood1;
-int vgood1 = 10;
