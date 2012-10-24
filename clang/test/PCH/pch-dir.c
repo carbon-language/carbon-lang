360d3a5b06091126ec75ae946b790d8c1197e762
Douglas Gregor <dgregor@apple.com>
2012-10-24 17:49:01 +0000

    Explicitly specify C++98 when building the C++ precompiled header for this test, so that the test will work on Windows.
    llvm-svn: 166601

--- clang/test/PCH/pch-dir.c
+++ clang/test/PCH/pch-dir.c
@@ -1,13 +1,11 @@
 // RUN: mkdir -p %t.h.gch
 // RUN: %clang -x c-header %S/pch-dir.h -o %t.h.gch/c.gch 
-// RUN: %clang -x c++-header %S/pch-dir.h -o %t.h.gch/cpp.gch 
+// RUN: %clang -x c++-header -std=c++98 %S/pch-dir.h -o %t.h.gch/cpp.gch 
 // RUN: %clang -include %t.h -fsyntax-only %s -Xclang -print-stats 2> %t.clog
 // RUN: FileCheck -check-prefix=C %s < %t.clog
-// RUN: %clang -x c++ -include %t.h -fsyntax-only %s -Xclang -print-stats 2> %t.cpplog
+// RUN: %clang -x c++ -include %t.h -std=c++98 -fsyntax-only %s -Xclang -print-stats 2> %t.cpplog
 // RUN: FileCheck -check-prefix=CPP %s < %t.cpplog
 
-// msvc(*-win32) implies -std=c++11, then it is expected to pass on msvc.
-// XFAIL: win32
 // RUN: not %clang -x c++ -std=c++11 -include %t.h -fsyntax-only %s 2> %t.cpp11log
 // RUN: FileCheck -check-prefix=CPP11 %s < %t.cpp11log
 
