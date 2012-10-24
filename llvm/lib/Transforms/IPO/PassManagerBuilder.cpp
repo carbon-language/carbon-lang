086ea5c1f5fbb2e879770146444fbcbb4a0c6c8d
Nadav Rotem <nrotem@apple.com>
2012-10-24 23:48:57 +0000

    revert accidental change
    llvm-svn: 166643

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -185,7 +185,7 @@ void PassManagerBuilder::populateModulePassManager(PassManagerBase &MPM) {
   MPM.add(createLoopIdiomPass());             // Recognize idioms like memset.
   MPM.add(createLoopDeletionPass());          // Delete dead loops
 
-  if (Vectorize || true) {
+  if (Vectorize) {
     MPM.add(createLoopVectorizePass());
     MPM.add(createLICMPass());
   }
