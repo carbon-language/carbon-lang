468f1df7533f453dca45df1adc79f4199a6a366b
Chad Rosier <mcrosier@apple.com>
2012-10-24 23:34:38 +0000

    Tell llvm-mc we're using intel syntax, so we don't have to use directives.
    llvm-svn: 166640

--- llvm/test/MC/X86/x86-32-ms-inline-asm.s
+++ llvm/test/MC/X86/x86-32-ms-inline-asm.s
@@ -1,12 +1,10 @@
-// RUN: llvm-mc -triple i386-unknown-unknown --show-encoding %s | FileCheck %s
+// RUN: llvm-mc -x86-asm-syntax=intel -triple i386-unknown-unknown --show-encoding %s | FileCheck %s
 
-.intel_syntax
 mov eax, [ebx].0
 mov [ebx].4, ecx
-.att_syntax
-        
-; CHECK: movl (%ebx), %eax
-; CHECK: encoding: [0x8b,0x03]
-; CHECK: movl %ecx, 4(%ebx)
-; CHECK: encoding: [0x89,0x4b,0x04]
+
+// CHECK: movl (%ebx), %eax
+// CHECK: encoding: [0x8b,0x03]
+// CHECK: movl %ecx, 4(%ebx)
+// CHECK: encoding: [0x89,0x4b,0x04]
         
