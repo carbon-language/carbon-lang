bd465a9cc7695b68c27cd1174a4fdde1ea8c7d6a
Chad Rosier <mcrosier@apple.com>
2012-10-31 18:59:38 +0000

    [driver] Remove the -ccc-host-triple alias.  This has been replaced by the -target option. rdar://10692880
    llvm-svn: 167158

--- clang/test/CodeGen/a15.c
+++ clang/test/CodeGen/a15.c
@@ -1,4 +1,4 @@
-// RUN: %clang -ccc-host-triple armv7-none-linux-gnueabi -mcpu=cortex-a15 -emit-llvm -S %s  -o /dev/null
+// RUN: %clang -target armv7-none-linux-gnueabi -mcpu=cortex-a15 -emit-llvm -S %s  -o /dev/null
 
 int main() {
   return 0;
