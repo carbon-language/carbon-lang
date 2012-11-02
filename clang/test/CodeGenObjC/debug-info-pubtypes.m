263f6a0ffa6ddd2318aeb1764b69eba8362d7442
David Blaikie <dblaikie@gmail.com>
2012-11-02 23:07:41 +0000

    Avoid version skew by making ObjC strict metadata tests resilient to new field.
    llvm-svn: 167332

--- clang/test/CodeGenObjC/debug-info-pubtypes.m
+++ clang/test/CodeGenObjC/debug-info-pubtypes.m
@@ -1,7 +1,7 @@
 // REQUIRES: x86-64-registered-target
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -g -emit-llvm %s -o - | FileCheck %s
 
-// CHECK: !5 = metadata !{i32 {{.*}}, metadata !6, metadata !"H", metadata !6, i32 6, i64 0, i64 8, i32 0, i32 512, null, metadata !2, i32 16, i32 0} ; [ DW_TAG_structure_type ]
+// CHECK: !5 = metadata !{i32 {{.*}}, metadata !6, metadata !"H", metadata !6, i32 6, i64 0, i64 8, i32 0, i32 512, null, metadata !2, i32 16, i32 0{{(, i32 0)?}}} ; [ DW_TAG_structure_type ]
 
 @interface H
 -(void) foo;
