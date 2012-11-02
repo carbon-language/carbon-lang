510db8378233530a3149932a6c2d232a1e44d4be
NAKAMURA Takumi <geek4civic@gmail.com>
2012-11-02 09:59:06 +0000

    clang/test/CodeGen/bitfield-promote.c: FileCheck-ize.
    llvm-svn: 167292

--- clang/test/CodeGen/bitfield-promote.c
+++ clang/test/CodeGen/bitfield-promote.c
@@ -1,18 +1,22 @@
-// RUN: %clang -O3 -emit-llvm -S -o %t %s
-// RUN: grep 'ret i64 4294967292' %t | count 2
-// RUN: grep 'ret i64 -4' %t | count 1
+// RUN: %clang -O3 -emit-llvm -S -o - %s | FileCheck %s
 
 long long f0(void) {
  struct { unsigned f0 : 32; } x = { 18 };
  return (long long) (x.f0 - (int) 22);
 }
+// CHECK: @f0()
+// CHECK: ret i64 4294967292
 
 long long f1(void) {
  struct { unsigned f0 : 31; } x = { 18 };
  return (long long) (x.f0 - (int) 22);
 }
+// CHECK: @f1()
+// CHECK: ret i64 -4
 
 long long f2(void) {
  struct { unsigned f0     ; } x = { 18 };
  return (long long) (x.f0 - (int) 22);
 }
+// CHECK: @f2()
+// CHECK: ret i64 4294967292
