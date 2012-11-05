4c69859b562871fe95c5216a029c46b79acf44b0
Douglas Gregor <dgregor@apple.com>
2012-11-05 20:11:10 +0000

    Make cpuid.h actually work with -std=c99 <rdar://problem/12552716>.
    While we're here, extend the module map to cover most of the
    newly-added instrinsic headers. Only wmmintrin.h is missing, because
    it needs to be split into AES/PCLMUL subheaders (as a separate commit).
    
    llvm-svn: 167398

--- clang/lib/Headers/cpuid.h
+++ clang/lib/Headers/cpuid.h
@@ -28,6 +28,6 @@
 static inline int __get_cpuid (unsigned int level, unsigned int *eax,
                                unsigned int *ebx, unsigned int *ecx,
                                unsigned int *edx) {
-    asm("cpuid" : "=a"(*eax), "=b" (*ebx), "=c"(*ecx), "=d"(*edx) : "0"(level));
+    __asm("cpuid" : "=a"(*eax), "=b" (*ebx), "=c"(*ecx), "=d"(*edx) : "0"(level));
     return 1;
 }
--- clang/test/Modules/compiler_builtins.m
+++ clang/test/Modules/compiler_builtins.m
@@ -1,5 +1,6 @@
 // RUN: rm -rf %t
 // RUN: %clang -fsyntax-only -fmodules -fmodule-cache-path %t -D__need_wint_t %s -Xclang -verify
+// RUN: %clang -fsyntax-only -std=c99 -fmodules -fmodule-cache-path %t -D__need_wint_t %s -Xclang -verify
 // expected-no-diagnostics
 
 #ifdef __SSE__
