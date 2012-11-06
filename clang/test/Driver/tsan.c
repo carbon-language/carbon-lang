2fa5fb5ab8a78ab9852954669a369eea79a1c757
Chad Rosier <mcrosier@apple.com>
2012-11-06 18:16:37 +0000

    Use the -fsanitize=thread flag to unbreak buildbot.
    llvm-svn: 167472

--- clang/test/Driver/tsan.c
+++ clang/test/Driver/tsan.c
@@ -1,9 +1,9 @@
-// RUN: %clang     -target i386-unknown-unknown -fthread-sanitizer %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O1 -target i386-unknown-unknown -fthread-sanitizer %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O2 -target i386-unknown-unknown -fthread-sanitizer %s -S -emit-llvm -o - | FileCheck %s
-// RUN: %clang -O3 -target i386-unknown-unknown -fthread-sanitizer %s -S -emit-llvm -o - | FileCheck %s
+// RUN: %clang     -target i386-unknown-unknown -fsanitize=thread %s -S -emit-llvm -o - | FileCheck %s
+// RUN: %clang -O1 -target i386-unknown-unknown -fsanitize=thread %s -S -emit-llvm -o - | FileCheck %s
+// RUN: %clang -O2 -target i386-unknown-unknown -fsanitize=thread %s -S -emit-llvm -o - | FileCheck %s
+// RUN: %clang -O3 -target i386-unknown-unknown -fsanitize=thread %s -S -emit-llvm -o - | FileCheck %s
 // RUN: %clang     -target i386-unknown-unknown -fsanitize=thread  %s -S -emit-llvm -o - | FileCheck %s
-// Verify that -fthread-sanitizer invokes tsan instrumentation.
+// Verify that -fsanitize=thread invokes tsan instrumentation.
 
 int foo(int *a) { return *a; }
 // CHECK: __tsan_init
