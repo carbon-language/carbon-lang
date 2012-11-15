d9cb0ba66086c0ec826bb5e06b1850e49ca0f000
Daniel Dunbar <daniel@zuster.org>
2012-11-15 20:24:58 +0000

    tests: Check that we can output to /dev/fd filesystem.
     - An LLVM unique_file() bug could cause us to infinite loop on the later test
       case.
    
    llvm-svn: 168082

--- clang/test/Misc/dev-fd-fs.c
+++ clang/test/Misc/dev-fd-fs.c
@@ -7,6 +7,24 @@
 //
 // RUN: cat %s | %clang -x c /dev/fd/0 -E > %t
 // RUN: FileCheck --check-prefix DEV-FD-INPUT < %t %s
-
+//
 // DEV-FD-INPUT: int x;
+
+
+// Check writing to /dev/fd named pipes. We use cat here as before to ensure we
+// get a named pipe.
+//
+// RUN: %clang -x c %s -E -o /dev/fd/1 | cat > %t
+// RUN: FileCheck --check-prefix DEV-FD-FIFO-OUTPUT < %t %s
+//
+// DEV-FD-FIFO-OUTPUT: int x;
+
+
+// Check writing to /dev/fd regular files.
+//
+// RUN: %clang -x c %s -E -o /dev/fd/1 > %t
+// RUN: FileCheck --check-prefix DEV-FD-REG-OUTPUT < %t %s
+//
+// DEV-FD-REG-OUTPUT: int x;
+
 int x;
