18907acb3f8a83a8264a6e7680871f56ede3128d
Benjamin Kramer <benny.kra@googlemail.com>
2012-11-25 11:52:03 +0000

    Move semantics are great, don't destroy the optimization opportunity with trivial copy ctors.
    No functionality change.
    
    llvm-svn: 168561

--- llvm/lib/VMCore/LLVMContextImpl.h
+++ llvm/lib/VMCore/LLVMContextImpl.h
@@ -46,7 +46,6 @@ struct DenseMapAPIntKeyInfo {
     APInt val;
     Type* type;
     KeyTy(const APInt& V, Type* Ty) : val(V), type(Ty) {}
-    KeyTy(const KeyTy& that) : val(that.val), type(that.type) {}
     bool operator==(const KeyTy& that) const {
       return type == that.type && this->val == that.val;
     }
@@ -71,7 +70,6 @@ struct DenseMapAPFloatKeyInfo {
   struct KeyTy {
     APFloat val;
     KeyTy(const APFloat& V) : val(V){}
-    KeyTy(const KeyTy& that) : val(that.val) {}
     bool operator==(const KeyTy& that) const {
       return this->val.bitwiseIsEqual(that.val);
     }
@@ -102,8 +100,6 @@ struct AnonStructTypeKeyInfo {
     bool isPacked;
     KeyTy(const ArrayRef<Type*>& E, bool P) :
       ETypes(E), isPacked(P) {}
-    KeyTy(const KeyTy& that) :
-      ETypes(that.ETypes), isPacked(that.isPacked) {}
     KeyTy(const StructType* ST) :
       ETypes(ArrayRef<Type*>(ST->element_begin(), ST->element_end())),
       isPacked(ST->isPacked()) {}
@@ -149,10 +145,6 @@ struct FunctionTypeKeyInfo {
     bool isVarArg;
     KeyTy(const Type* R, const ArrayRef<Type*>& P, bool V) :
       ReturnType(R), Params(P), isVarArg(V) {}
-    KeyTy(const KeyTy& that) :
-      ReturnType(that.ReturnType),
-      Params(that.Params),
-      isVarArg(that.isVarArg) {}
     KeyTy(const FunctionType* FT) :
       ReturnType(FT->getReturnType()),
       Params(ArrayRef<Type*>(FT->param_begin(), FT->param_end())),
