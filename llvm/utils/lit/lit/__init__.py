93a39dfdf32bc19af2b26a2809ebccbbf7f986c8
Daniel Dunbar <daniel@zuster.org>
2012-11-28 00:06:11 +0000

    lit: Bump the version to .3.
    llvm-svn: 168750

--- llvm/utils/lit/lit/__init__.py
+++ llvm/utils/lit/lit/__init__.py
@@ -4,7 +4,7 @@ from main import main
 
 __author__ = 'Daniel Dunbar'
 __email__ = 'daniel@zuster.org'
-__versioninfo__ = (0, 2, 0)
+__versioninfo__ = (0, 3, 0)
 __version__ = '.'.join(map(str, __versioninfo__)) + 'dev'
 
 __all__ = []
