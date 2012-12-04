384a957051663492e48b178474eab2a0d393da4a
NAKAMURA Takumi <geek4civic@gmail.com>
2012-12-04 01:03:31 +0000

    clang/test/Index/comment-objc-decls.m: Try to fix r169193, to add x86_64-darwin.
    llvm-svn: 169199

--- clang/test/Index/comment-objc-decls.m
+++ clang/test/Index/comment-objc-decls.m
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
-// RUN: c-index-test -test-load-source all -comments-xml-schema=%S/../../bindings/xml/comment-xml-schema.rng %s > %t/out
+// RUN: c-index-test -test-load-source all -comments-xml-schema=%S/../../bindings/xml/comment-xml-schema.rng -target x86_64-apple-darwin10 %s > %t/out
 // RUN: FileCheck %s < %t/out
 
 // Ensure that XML we generate is not invalid.
