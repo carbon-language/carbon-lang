6246013f7a92f1cac4c1e4635f741ec42e8a70f1
Bill Wendling <isanbard@gmail.com>
2012-12-04 07:33:40 +0000

    Don't test for ASM output but for IR output.
    llvm-svn: 169232

--- clang/test/CodeGenCXX/debug-info-zero-length-arrays.cpp
+++ clang/test/CodeGenCXX/debug-info-zero-length-arrays.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang -target x86_64-unknown-unknown -fverbose-asm -g -O0 -S %s -o - | FileCheck %s
+// RUN: %clang -target x86_64-unknown-unknown -fverbose-asm -g -O0 -S -emit-llvm %s -o - | FileCheck %s
 // <rdar://problem/12566646>
 
 class A {
@@ -6,11 +6,8 @@ class A {
 };
 A a;
 
-// CHECK:      Abbrev [3] 0x2d:0x3 DW_TAG_base_type
-// CHECK-NEXT:   DW_AT_byte_size
-// CHECK-NEXT:     DW_AT_encoding
-// CHECK-NEXT:   Abbrev [4] 0x30:0xb DW_TAG_array_type
-// CHECK-NEXT:     DW_AT_type
-// CHECK-NEXT:     Abbrev [5] 0x35:0x5 DW_TAG_subrange_type
-// CHECK-NEXT:       DW_AT_type
-// CHECK-NEXT:   End Of Children Mark
+// CHECK: !9 = metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"x", metadata {{.*}}, i32 5, i64 0, i64 0, i64 0, i32 1, metadata [[ARRAY_TYPE:.*]]} ; [ DW_TAG_member ]
+// CHECK: [[ARRAY_TYPE]] = metadata !{i32 {{.*}}, null, metadata !"", null, i32 0, i64 0, i64 32, i32 0, i32 0, metadata [[BASE_TYPE:.*]], metadata [[ELEM_TYPE:.*]], i32 0, i32 0} ; [ DW_TAG_array_type ]
+// CHECK: [[BASE_TYPE]] = metadata !{i32 {{.*}}, null, metadata !"int", null, i32 0, i64 32, i64 32, i64 0, i32 0, i32 5} ; [ DW_TAG_base_type ]
+// CHECK: [[ELEM_TYPE]] = metadata !{metadata [[SUBRANGE:.*]]}
+// CHECK: [[SUBRANGE]] = metadata !{i32 786465, i64 1, i64 0, i64 -1} ; [ DW_TAG_subrange_type ] [1, 0]
