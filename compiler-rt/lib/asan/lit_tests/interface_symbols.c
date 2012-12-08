f0e638c6df655d0d6a32402759aa733e2fa89f0c
Alexey Samsonov <samsonov@google.com>
2012-12-08 01:12:12 +0000

    ASan: fix interface-symbols test on Mac by explicitly listing all weak functions
    llvm-svn: 169660

--- compiler-rt/lib/asan/lit_tests/interface_symbols.c
+++ compiler-rt/lib/asan/lit_tests/interface_symbols.c
@@ -2,7 +2,12 @@
 
 // RUN: %clang -fsanitize=address -dead_strip -O2 %s -o %t.exe
 // RUN: nm %t.exe | grep " T " | sed "s/.* T //" \
-// RUN:    | grep "__asan_" | sed "s/___asan_/__asan_/" > %t.symbols
+// RUN:    | grep "__asan_" | sed "s/___asan_/__asan_/" \
+// RUN:    | grep -v "__asan_malloc_hook" \
+// RUN:    | grep -v "__asan_free_hook" \
+// RUN:    | grep -v "__asan_symbolize" \
+// RUN:    | grep -v "__asan_default_options" \
+// RUN:    | grep -v "__asan_on_error" > %t.symbols
 // RUN: cat %p/../../../include/sanitizer/asan_interface.h \
 // RUN:    | sed "s/\/\/.*//" | sed "s/typedef.*//" \
 // RUN:    | grep -v "OPTIONAL" \
