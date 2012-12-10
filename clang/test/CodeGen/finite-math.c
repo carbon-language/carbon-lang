43e17ad1d0bf1018d0f0e40e7472ff8e3f390637
Michael Ilseman <milseman@apple.com>
2012-12-10 18:48:08 +0000

    Remove unneeded typedef and volatile
    llvm-svn: 169743

--- clang/test/CodeGen/finite-math.c
+++ clang/test/CodeGen/finite-math.c
@@ -1,7 +1,5 @@
 // RUN: %clang_cc1 -ffinite-math-only -emit-llvm -o - %s | FileCheck %s
-typedef unsigned cond_t;
-
-volatile float f0, f1, f2;
+float f0, f1, f2;
 
 void foo(void) {
   // CHECK: define void @foo()
