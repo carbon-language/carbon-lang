16ba5ff7129a03b661b7fa02d79286026f7ddf6c
Douglas Gregor <dgregor@apple.com>
2012-12-11 22:31:46 +0000

    Try to fix Win32 failures
    llvm-svn: 169923

--- clang/test/PCH/modified-header-crash.c
+++ clang/test/PCH/modified-header-crash.c
@@ -2,7 +2,7 @@
 
 // RUN: cp %S/modified-header-crash.h %t.h
 // RUN: %clang_cc1 -DCAKE -x c-header %t.h -emit-pch -o %t
-// RUN: echo >> %t.h
+// RUN: echo 'int foobar;' >> %t.h
 // RUN: not %clang_cc1 %s -include-pch %t -fsyntax-only
 
 void f(void) {
