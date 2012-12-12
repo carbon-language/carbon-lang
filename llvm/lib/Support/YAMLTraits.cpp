cbe058412ac142be7eabbd7dddfcce0654df3bbc
Benjamin Kramer <benny.kra@googlemail.com>
2012-12-12 20:55:44 +0000

    Fix the build on case sensitive file systems.
    llvm-svn: 170021

--- llvm/lib/Support/YAMLTraits.cpp
+++ llvm/lib/Support/YAMLTraits.cpp
@@ -9,14 +9,12 @@
 
 #define BUILDING_YAMLIO
 #include "llvm/Support/YAMLTraits.h"
-
 #include "llvm/ADT/Twine.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/ErrorHandling.h"
-#include "llvm/Support/format.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/Support/Format.h"
 #include "llvm/Support/YAMLParser.h"
-
+#include "llvm/Support/raw_ostream.h"
 #include <cstring>
 
 namespace llvm {
