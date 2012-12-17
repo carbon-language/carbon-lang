d75be9b4fb290ae82903a561286f6686880c397d
Chandler Carruth <chandlerc@gmail.com>
2012-12-17 21:44:50 +0000

    Add a triple to this test -- it has to be an ELF platform...
    llvm-svn: 170374

--- llvm/test/MC/ELF/comp-dir.s
+++ llvm/test/MC/ELF/comp-dir.s
@@ -1,4 +1,4 @@
-// RUN: llvm-mc -g -fdebug-compilation-dir=/test/comp/dir %s -filetype=obj -o %t.o
+// RUN: llvm-mc -triple=x86_64-linux-unknown -g -fdebug-compilation-dir=/test/comp/dir %s -filetype=obj -o %t.o
 // RUN: llvm-dwarfdump %t.o | FileCheck %s
 
 // CHECK: DW_AT_comp_dir [DW_FORM_string] ("/test/comp/dir")
