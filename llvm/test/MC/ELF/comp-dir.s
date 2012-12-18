ad0c80b8e6908aad6b0946e36a5c3e592312bd16
NAKAMURA Takumi <geek4civic@gmail.com>
2012-12-18 05:08:12 +0000

    llvm/test/MC/ELF/comp-dir.s: Appease MSYS Bash.
    llvm-svn: 170410

--- llvm/test/MC/ELF/comp-dir.s
+++ llvm/test/MC/ELF/comp-dir.s
@@ -1,7 +1,7 @@
 // RUN: llvm-mc -triple=x86_64-linux-unknown -g -fdebug-compilation-dir=/test/comp/dir %s -filetype=obj -o %t.o
 // RUN: llvm-dwarfdump %t.o | FileCheck %s
 
-// CHECK: DW_AT_comp_dir [DW_FORM_string] ("/test/comp/dir")
+// CHECK: DW_AT_comp_dir [DW_FORM_string] ("{{([A-Za-z]:.*)?}}/test/comp/dir")
 
 f:
   nop
