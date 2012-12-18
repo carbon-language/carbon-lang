7174e90078ab49cb0f0f93037d9afa6fb3f5eef2
NAKAMURA Takumi <geek4civic@gmail.com>
2012-12-18 05:14:30 +0000

    test/Misc/dev-fd-fs.c: Mark this as XFAIL:freebsd (since r169831).
    I don't want to suppress this to be excluded with the feature "dev-fd-fs". This should work also on freebsd.
    
    llvm-svn: 170411

--- clang/test/Misc/dev-fd-fs.c
+++ clang/test/Misc/dev-fd-fs.c
@@ -1,6 +1,8 @@
 // Check that we can operate on files from /dev/fd.
 // REQUIRES: dev-fd-fs
 
+// It has not been working since r169831 on freebsd.
+// XFAIL: freebsd
 
 // Check reading from named pipes. We cat the input here instead of redirecting
 // it to ensure that /dev/fd/0 is a named pipe, not just a redirected file.
