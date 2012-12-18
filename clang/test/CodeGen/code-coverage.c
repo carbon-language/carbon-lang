8522ba845f8a824e2142e21ee71b2900c42f929e
Rafael Espindola <rafael.espindola@gmail.com>
2012-12-18 03:54:29 +0000

    Now that the uninitialized variable has been fixed in llvm, add this test back, but remove unneeded options and fix the comment.
    llvm-svn: 170405

---
+++ clang/test/CodeGen/code-coverage.c
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -emit-llvm -disable-red-zone -femit-coverage-data %s -o - | FileCheck %s
+
+// <rdar://problem/12843084>
+
+int test1(int a) {
+  switch (a % 2) {
+  case 0:
+    ++a;
+  case 1:
+    a /= 2;
+  }
+  return a;
+}
+
+// Check that the noredzone flag is set on the generated functions.
+
+// CHECK: void @__llvm_gcov_indirect_counter_increment(i32* %{{.*}}, i64** %{{.*}}) unnamed_addr noinline noredzone
+// CHECK: void @__llvm_gcov_writeout() unnamed_addr noinline noredzone
+// CHECK: void @__llvm_gcov_init() unnamed_addr noinline noredzone
+// CHECK: void @__gcov_flush() unnamed_addr noinline noredzone
