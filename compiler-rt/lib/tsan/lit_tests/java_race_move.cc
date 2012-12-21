c55ed2f538b92647ce4ca2c99c7fd288640191f0
Dmitry Vyukov <dvyukov@google.com>
2012-12-21 13:28:39 +0000

    tsan: add a test for java memory move
    llvm-svn: 170893

---
+++ compiler-rt/lib/tsan/lit_tests/java_race_move.cc
@@ -0,0 +1,31 @@
+// RUN: %clangxx_tsan -O1 %s -o %t && %t 2>&1 | FileCheck %s
+#include "java.h"
+
+jptr varaddr;
+jptr varaddr2;
+
+void *Thread(void *p) {
+  sleep(1);
+  *(int*)varaddr2 = 42;
+  return 0;
+}
+
+int main() {
+  int const kHeapSize = 1024 * 1024;
+  void *jheap = malloc(kHeapSize);
+  __tsan_java_init((jptr)jheap, kHeapSize);
+  const int kBlockSize = 64;
+  int const kMove = 1024;
+  __tsan_java_alloc((jptr)jheap, kBlockSize);
+  varaddr = (jptr)jheap + 16;
+  varaddr2 = varaddr + kMove;
+  pthread_t th;
+  pthread_create(&th, 0, Thread, 0);
+  *(int*)varaddr = 43;
+  __tsan_java_move(varaddr, varaddr2, kBlockSize);
+  pthread_join(th, 0);
+  __tsan_java_free(varaddr2, kBlockSize);
+  return __tsan_java_fini();
+}
+
+// CHECK: WARNING: ThreadSanitizer: data race
