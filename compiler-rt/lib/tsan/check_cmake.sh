47a5ad0348b93485a7a203de290fbc456df3765a
Dmitry Vyukov <dvyukov@google.com>
2012-12-21 06:30:27 +0000

    tsan: use clang in cmake build + add -Werror
    llvm-svn: 170844

--- compiler-rt/lib/tsan/check_cmake.sh
+++ compiler-rt/lib/tsan/check_cmake.sh
@@ -5,7 +5,7 @@ set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $ROOT/build
 cd $ROOT/build
-CC=gcc CXX=g++ cmake -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
+CC=clang CXX=clang++ cmake -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
 make -j64
 make check-tsan check-sanitizer -j64
 
