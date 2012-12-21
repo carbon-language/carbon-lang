b3bf5651bd92eda465e48d47461538ab814aa3a4
Dmitry Vyukov <dvyukov@google.com>
2012-12-21 13:40:27 +0000

    tsan: add java malloc stress test
    llvm-svn: 170896

---
+++ compiler-rt/lib/tsan/lit_tests/java_alloc.cc
@@ -0,0 +1,32 @@
+// RUN: %clangxx_tsan -O1 %s -o %t && %t 2>&1 | FileCheck %s
+#include "java.h"
+
+int const kHeapSize = 1024 * 1024;
+
+void stress(jptr addr) {
+  for (jptr sz = 8; sz <= 32; sz <<= 1) {
+    for (jptr i = 0; i < kHeapSize / 4 / sz; i++) {
+      __tsan_java_alloc(addr + i * sz, sz);
+    }
+    __tsan_java_move(addr, addr + kHeapSize / 2, kHeapSize / 4);
+    __tsan_java_free(addr + kHeapSize / 2, kHeapSize / 4);
+  }
+}
+
+void *Thread(void *p) {
+  stress((jptr)p);
+  return 0;
+}
+
+int main() {
+  jptr jheap = (jptr)malloc(kHeapSize);
+  __tsan_java_init(jheap, kHeapSize);
+  pthread_t th;
+  pthread_create(&th, 0, Thread, (void*)(jheap + kHeapSize / 4)
+  );
+  stress(jheap);
+  pthread_join(th, 0);
+  return __tsan_java_fini();
+}
+
+// CHECK-NOT: WARNING: ThreadSanitizer: data race
