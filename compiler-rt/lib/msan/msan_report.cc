fee82c68aee058190ca0cad07f9387de969fb773
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2012-12-26 10:16:45 +0000

    [msan] MSan, New Year Tree style.
    llvm-svn: 171106

--- compiler-rt/lib/msan/msan_report.cc
+++ compiler-rt/lib/msan/msan_report.cc
@@ -15,6 +15,7 @@
 #include "msan.h"
 #include "sanitizer_common/sanitizer_common.h"
 #include "sanitizer_common/sanitizer_mutex.h"
+#include "sanitizer_common/sanitizer_report_decorator.h"
 #include "sanitizer_common/sanitizer_stackdepot.h"
 
 using namespace __sanitizer;
@@ -23,7 +24,27 @@ static StaticSpinMutex report_mu;
 
 namespace __msan {
 
+static bool PrintsToTtyCached() {
+  static int cached = 0;
+  static bool prints_to_tty;
+  if (!cached) {  // Ok wrt threads since we are printing only from one thread.
+    prints_to_tty = PrintsToTty();
+    cached = 1;
+  }
+  return prints_to_tty;
+}
+
+class Decorator: private __sanitizer::AnsiColorDecorator {
+ public:
+  Decorator() : __sanitizer::AnsiColorDecorator(PrintsToTtyCached()) { }
+  const char *Warning()    { return Red(); }
+  const char *Origin()     { return Magenta(); }
+  const char *Name()   { return Green(); }
+  const char *End()    { return Default(); }
+};
+
 static void DescribeOrigin(u32 origin) {
+  Decorator d;
   if (flags()->verbosity)
     Printf("  raw origin id: %d\n", origin);
   if (const char *so = __msan_get_origin_descr_if_stack(origin)) {
@@ -31,13 +52,17 @@ static void DescribeOrigin(u32 origin) {
     char* sep = internal_strchr(s, '@');
     CHECK(sep);
     *sep = '\0';
-    Printf("  Uninitialised value was created by an allocation of '%s'"
-           " in the stack frame of function '%s'\n", s, sep + 1);
+    Printf("%s", d.Origin());
+    Printf("  %sUninitialised value was created by an allocation of '%s%s%s'"
+           " in the stack frame of function '%s%s%s'%s\n",
+           d.Origin(), d.Name(), s, d.Origin(), d.Name(), sep + 1,
+           d.Origin(), d.End());
     InternalFree(s);
   } else {
     uptr size = 0;
     const uptr *trace = StackDepotGet(origin, &size);
-    Printf("  Uninitialised value was created by a heap allocation\n");
+    Printf("  %sUninitialised value was created by a heap allocation%s\n",
+           d.Origin(), d.End());
     StackTrace::PrintStack(trace, size, true, "", 0);
   }
 }
@@ -47,7 +72,10 @@ void ReportUMR(StackTrace *stack, u32 origin) {
 
   GenericScopedLock<StaticSpinMutex> lock(&report_mu);
 
+  Decorator d;
+  Printf("%s", d.Warning());
   Report(" WARNING: Use of uninitialized value\n");
+  Printf("%s", d.End());
   StackTrace::PrintStack(stack->trace, stack->size, true, "", 0);
   if (origin) {
     DescribeOrigin(origin);
