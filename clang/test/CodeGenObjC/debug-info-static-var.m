bf18725d11441943deead22bae803c6e1eac214e
David Blaikie <dblaikie@gmail.com>
2012-12-28 01:16:31 +0000

    Change this to an IR test instead of an assembly test (as per comment in r171134).
    This makes the test not dependent on LLVM & won't vary/break based on LLVM
    codegen related changes. Appropriately testing at the Clang level what was
    fixed at the Clang level originally (in r124210).
    
    llvm-svn: 171175

--- clang/test/CodeGenObjC/debug-info-static-var.m
+++ clang/test/CodeGenObjC/debug-info-static-var.m
@@ -1,14 +1,8 @@
-// REQUIRES: x86-64-registered-target
-// RUN: %clang_cc1 -g -triple x86_64-apple-darwin10 -fobjc-runtime=macosx-fragile-10.5 -S -masm-verbose -o - %s | FileCheck %s
+// RUN: %clang_cc1 -g -emit-llvm -o - %s | FileCheck %s
 // Radar 8801045
 // Do not emit AT_MIPS_linkage_name for static variable i
 
-// CHECK: Lset6 = Linfo_string3-Linfo_string           ## DW_AT_name
-// CHECK-NEXT: .long   Lset6
-// CHECK-NEXT:        DW_AT_type
-// CHECK-NEXT:        DW_AT_decl_file
-// CHECK-NEXT:        DW_AT_decl_line
-// CHECK-NEXT:        DW_AT_location
+// CHECK: metadata !"i", metadata !"i", metadata !""
 
 @interface A {
 }
