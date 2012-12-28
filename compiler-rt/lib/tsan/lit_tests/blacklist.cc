8c01e76d27d57af7913a9c9a9b925b1264fab98f
Alexey Samsonov <samsonov@google.com>
2012-12-28 09:32:36 +0000

    [TSan] Add lit test for -fsanitize-blacklist= flag
    llvm-svn: 171185

---
+++ compiler-rt/lib/tsan/lit_tests/blacklist.cc
@@ -0,0 +1,31 @@
+// Test blacklist functionality for TSan.
+
+// RUN: %clangxx_tsan -O1 %s \
+// RUN:   -fsanitize-blacklist=%p/Helpers/blacklist.txt \
+// RUN:   -o %t && %t 2>&1 | FileCheck %s
+#include <pthread.h>
+#include <stdio.h>
+
+int Global;
+
+void *Thread1(void *x) {
+  Global++;
+  return NULL;
+}
+
+void *Thread2(void *x) {
+  Global--;
+  return NULL;
+}
+
+int main() {
+  pthread_t t[2];
+  pthread_create(&t[0], NULL, Thread1, NULL);
+  pthread_create(&t[1], NULL, Thread2, NULL);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+  printf("PASS\n");
+  return 0;
+}
+
+// CHECK-NOT: ThreadSanitizer: data race
