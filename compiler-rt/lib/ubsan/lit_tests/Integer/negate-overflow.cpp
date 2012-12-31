5f8061bc3697721aaa55734e291a5c6bd5b37d58
Chandler Carruth <chandlerc@gmail.com>
2012-12-31 06:16:47 +0000

    Revert r171273 which doesn't actually compile.
    Original commit message:
    [ubsan] Don't suggest casting to unsigned for unsigned unary minus overflow.
    
    llvm-svn: 171277

--- compiler-rt/lib/ubsan/lit_tests/Integer/negate-overflow.cpp
+++ compiler-rt/lib/ubsan/lit_tests/Integer/negate-overflow.cpp
@@ -1,12 +1,7 @@
-// RUN: %clang -fsanitize=signed-integer-overflow %s -o %t && %t 2>&1 | FileCheck %s --check-prefix=CHECKS
-// RUN: %clang -fsanitize=unsigned-integer-overflow %s -o %t && %t 2>&1 | FileCheck %s --check-prefix=CHECKU
+// RUN: %clang -fsanitize=signed-integer-overflow %s -o %t && %t 2>&1 | FileCheck %s
 
 int main() {
-  // CHECKS-NOT: runtime error
-  // CHECKU: negate-overflow.cpp:[[@LINE+2]]:3: runtime error: negation of 2147483648 cannot be represented in type 'unsigned int'
-  // CHECKU-NOT: cast to an unsigned
   -unsigned(-0x7fffffff - 1); // ok
-  // CHECKS: negate-overflow.cpp:[[@LINE+2]]:10: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
-  // CHECKU-NOT: runtime error
+  // CHECK: negate-overflow.cpp:6:10: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
   return -(-0x7fffffff - 1);
 }
