437db1597888674868033d567d05185c20e5f128
Sean Silva <silvas@purdue.edu>
2013-01-02 22:48:50 +0000

    Exclude docs/analyzer/ from the default Sphinx build.
    This paves the way for converting the analyzer docs to Sphinx (by
    setting up a nested Sphinx tree in this directory).
    
    llvm-svn: 171424

--- clang/docs/conf.py
+++ clang/docs/conf.py
@@ -64,7 +64,7 @@ release = '3.3'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ['_build']
+exclude_patterns = ['_build', 'analyzer']
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 #default_role = None
