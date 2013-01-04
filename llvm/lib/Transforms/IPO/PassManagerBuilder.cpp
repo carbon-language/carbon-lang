be6570d42995b6889330e5cfc67d787447fd011a
Nadav Rotem <nrotem@apple.com>
2013-01-04 17:57:44 +0000

    Move the loop vectorizer from O2 to O3. It looks like the increase in code size actually hurts the performance on many programs.
    llvm-svn: 171471

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -187,7 +187,7 @@ void PassManagerBuilder::populateModulePassManager(PassManagerBase &MPM) {
   MPM.add(createLoopIdiomPass());             // Recognize idioms like memset.
   MPM.add(createLoopDeletionPass());          // Delete dead loops
 
-  if (LoopVectorize && OptLevel > 1)
+  if (LoopVectorize && OptLevel > 2)
     MPM.add(createLoopVectorizePass());
 
   if (!DisableUnrollLoops)
