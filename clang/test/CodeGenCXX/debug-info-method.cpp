c1520bbb3422ef0efa4df4317720f5e6f7715861
David Blaikie <dblaikie@gmail.com>
2013-01-05 05:39:21 +0000

    FileCheck-ize test/CodeGenCXX/debug-info-method.cpp
    llvm-svn: 171602

--- clang/test/CodeGenCXX/debug-info-method.cpp
+++ clang/test/CodeGenCXX/debug-info-method.cpp
@@ -1,4 +1,5 @@
-// RUN: %clang -fverbose-asm -g -S %s -o - | grep DW_ACCESS_protected
+// RUN: %clang -fverbose-asm -g -S %s -o - | FileCheck %s
+// CHECK: DW_ACCESS_protected
 class A {
 protected:
   int foo();
