fe958416304929a66616513df326b74c9a831bc1
Rafael Espindola <rafael.espindola@gmail.com>
2013-01-07 23:04:04 +0000

    add a triple
    llvm-svn: 171808

--- clang/test/Misc/ast-dump-attr.cpp
+++ clang/test/Misc/ast-dump-attr.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++11 -ast-dump -ast-dump-filter Test %s | FileCheck --strict-whitespace %s
+// RUN: %clang_cc1 -triple x86_64-pc-linux -std=c++11 -ast-dump -ast-dump-filter Test %s | FileCheck --strict-whitespace %s
 
 int TestLocation
 __attribute__((unused));
