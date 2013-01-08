79fafccb5e5b1b4542bc6ebf93632cc292452a15
Michael J. Spencer <bigcheesegs@gmail.com>
2013-01-08 22:59:01 +0000

    [Driver] Fix indentation.
    llvm-svn: 171909

--- lld/lib/Driver/LinkerInvocation.cpp
+++ lld/lib/Driver/LinkerInvocation.cpp
@@ -18,58 +18,58 @@
 using namespace lld;
 
 void LinkerInvocation::operator()() {
-    // Create target.
-    std::unique_ptr<Target> target(Target::create(_options));
+  // Create target.
+  std::unique_ptr<Target> target(Target::create(_options));
 
-    if (!target) {
-      llvm::errs() << "Failed to create target for " << _options._target
-                   << "\n";
-      return;
-    }
-
-    // Read inputs
-    InputFiles inputs;
-    for (const auto &input : _options._input) {
-      auto reader = target->getReader(input);
-      if (error_code ec = reader) {
-        llvm::errs() << "Failed to get reader for: " << input.getPath() << ": "
-                     << ec.message() << "\n";
-        return;
-      }
-
-      auto buffer = input.getBuffer();
-      if (error_code ec = buffer) {
-        llvm::errs() << "Failed to read file: " << input.getPath() << ": "
-                     << ec.message() << "\n";
-        return;
-      }
-
-      std::vector<std::unique_ptr<File>> files;
-      if (llvm::error_code ec = reader->readFile(
-            buffer->getBufferIdentifier(), files)) {
-        llvm::errs() << "Failed to read file: " << input.getPath() << ": "
-                     << ec.message() << "\n";
-        return;
-      }
-      inputs.appendFiles(files);
-    }
-
-    struct Blah : ResolverOptions {
-      Blah()
-        : ResolverOptions() {
-        _undefinesAreErrors = true;
-      }
-    } ro;
-    Resolver resolver(ro, inputs);
-    resolver.resolve();
-    File &merged = resolver.resultFile();
-
-    auto writer = target->getWriter();
-    if (error_code ec = writer) {
-      llvm::errs() << "Failed to get writer: " << ec.message() << ".\n";
-      return;
-    }
-
-    if (error_code ec = writer->writeFile(merged, _options._outputPath))
-      llvm::errs() << "Failed to write file: " << ec.message() << "\n";
+  if (!target) {
+    llvm::errs() << "Failed to create target for " << _options._target
+                  << "\n";
+    return;
   }
+
+  // Read inputs
+  InputFiles inputs;
+  for (const auto &input : _options._input) {
+    auto reader = target->getReader(input);
+    if (error_code ec = reader) {
+      llvm::errs() << "Failed to get reader for: " << input.getPath() << ": "
+                    << ec.message() << "\n";
+      return;
+    }
+
+    auto buffer = input.getBuffer();
+    if (error_code ec = buffer) {
+      llvm::errs() << "Failed to read file: " << input.getPath() << ": "
+                    << ec.message() << "\n";
+      return;
+    }
+
+    std::vector<std::unique_ptr<File>> files;
+    if (llvm::error_code ec = reader->readFile(
+          buffer->getBufferIdentifier(), files)) {
+      llvm::errs() << "Failed to read file: " << input.getPath() << ": "
+                    << ec.message() << "\n";
+      return;
+    }
+    inputs.appendFiles(files);
+  }
+
+  struct Blah : ResolverOptions {
+    Blah()
+      : ResolverOptions() {
+      _undefinesAreErrors = true;
+    }
+  } ro;
+  Resolver resolver(ro, inputs);
+  resolver.resolve();
+  File &merged = resolver.resultFile();
+
+  auto writer = target->getWriter();
+  if (error_code ec = writer) {
+    llvm::errs() << "Failed to get writer: " << ec.message() << ".\n";
+    return;
+  }
+
+  if (error_code ec = writer->writeFile(merged, _options._outputPath))
+    llvm::errs() << "Failed to write file: " << ec.message() << "\n";
+}
