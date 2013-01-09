5a6a020d9b1baa9683e4a1dd1659a501238e0064
David Blaikie <dblaikie@gmail.com>
2013-01-09 17:48:41 +0000

    Suppress GCC -Wreturn warning.
    Modified from a patch by David Greene.
    
    llvm-svn: 171982

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -4870,6 +4870,7 @@ static char getObjCEncodingForPrimitiveKind(const ASTContext *C,
 #include "clang/AST/BuiltinTypes.def"
       llvm_unreachable("invalid builtin type for @encode");
     }
+    llvm_unreachable("invalid BuiltinType::Kind value");
 }
 
 static char ObjCEncodingForEnumType(const ASTContext *C, const EnumType *ET) {
