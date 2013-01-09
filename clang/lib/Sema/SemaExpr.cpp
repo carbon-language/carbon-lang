14da440018e0817c243f7d568092facba2bf4952
Fariborz Jahanian <fjahanian@apple.com>
2013-01-09 00:09:15 +0000

    put back diagnostics when flexible members are captured in lambdas.
    llvm-svn: 171921

--- clang/lib/Sema/SemaExpr.cpp
+++ clang/lib/Sema/SemaExpr.cpp
@@ -10762,10 +10762,13 @@ bool Sema::tryCaptureVariable(VarDecl *Var, SourceLocation Loc,
     // Prohibit structs with flexible array members too.
     // We cannot capture what is in the tail end of the struct.
     if (const RecordType *VTTy = Var->getType()->getAs<RecordType>()) {
-      if (VTTy->getDecl()->hasFlexibleArrayMember() && IsBlock) {
+      if (VTTy->getDecl()->hasFlexibleArrayMember()) {
         if (BuildAndDiagnose) {
           if (IsBlock)
             Diag(Loc, diag::err_ref_flexarray_type);
+          else
+            Diag(Loc, diag::err_lambda_capture_flexarray_type)
+              << Var->getDeclName();
           Diag(Var->getLocation(), diag::note_previous_decl)
             << Var->getDeclName();
         }
