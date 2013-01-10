b4e2e93c618ae0c349881380ab3c76d4bd0a5ca9
Enrico Granata <egranata@apple.com>
2013-01-10 02:36:16 +0000

    <rdar://problem/11146929>
    Enabling support for the wchar_t type.
    Without the proper language option setup, clang's ASTContexts will be configured to have wchar_t == int
    This patch enables the correct options to make sure that we report wchar_t as itself
    Added a test case to make sure we do not regress 
    
    llvm-svn: 172038

---
+++ lldb/test/lang/cpp/wchar_t/TestCxxWCharT.py
@@ -0,0 +1,76 @@
+"""
+Test that C++ supports wchar_t correctly.
+"""
+
+import os, time
+import unittest2
+import lldb
+from lldbtest import *
+import lldbutil
+
+class CxxWCharTTestCase(TestBase):
+
+    mydir = os.path.join("lang", "cpp", "wchar_t")
+
+    @unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
+    @dsym_test
+    def test_with_dsym(self):
+        """Test that C++ supports wchar_t correctly."""
+        self.buildDsym()
+        self.wchar_t()
+
+    @dwarf_test
+    def test_with_dwarf(self):
+        """Test that C++ supports wchar_t correctly."""
+        self.buildDwarf()
+        self.wchar_t()
+
+    def setUp(self):
+        # Call super's setUp().
+        TestBase.setUp(self)
+        # Find the line number to break for main.cpp.
+        self.source = 'main.cpp'
+        self.line = line_number(self.source, '// Set break point at this line.')
+
+    def wchar_t(self):
+        """Test that C++ supports wchar_t correctly."""
+        exe = os.path.join(os.getcwd(), "a.out")
+
+        # Create a target by the debugger.
+        target = self.dbg.CreateTarget(exe)
+        self.assertTrue(target, VALID_TARGET)
+
+        # Break on the struct declration statement in main.cpp.
+        lldbutil.run_break_set_by_file_and_line (self, "main.cpp", self.line)
+
+        # Now launch the process, and do not stop at entry point.
+        process = target.LaunchSimple(None, None, os.getcwd())
+
+        if not process:
+            self.fail("SBTarget.Launch() failed")
+
+        # Check that we correctly report templates on wchar_t
+        self.expect("frame variable foo_y",
+            substrs = ['(Foo<wchar_t>) foo_y = '])
+
+        # Check that we correctly report templates on int
+        self.expect("frame variable foo_x",
+            substrs = ['(Foo<int>) foo_x = '])
+
+        # Check that we correctly report wchar_t
+        self.expect("frame variable foo_y.object",
+            substrs = ['(wchar_t) foo_y.object = '])
+
+        # Check that we correctly report int
+        self.expect("frame variable foo_x.object",
+            substrs = ['(int) foo_x.object = '])
+
+        # Check that we can run expressions that return wchar_t
+        self.expect("expression L'a'",substrs = ['(wchar_t) $'])
+
+
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
---
+++ lldb/test/lang/cpp/wchar_t/main.cpp
@@ -0,0 +1,27 @@
+//===-- main.c --------------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+template <typename T>
+class Foo
+{
+public:
+    Foo () : object() {}
+    Foo (T x) : object(x) {}
+    T getObject() { return object; }
+private:
+    T object;
+};
+
+
+int main (int argc, char const *argv[])
+{
+    Foo<int> foo_x('a');
+    Foo<wchar_t> foo_y(L'a');
+    return 0; // Set break point at this line.
+}
