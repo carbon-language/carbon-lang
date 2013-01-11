9f08be02729501dba64d8da138f60652d55239bb
Dmitri Gribenko <gribozavr@gmail.com>
2013-01-11 01:15:02 +0000

    libclang logging: fixup for MSVC, which does not have __func__
    llvm-svn: 172158

--- clang/tools/libclang/CLog.h
+++ clang/tools/libclang/CLog.h
@@ -13,6 +13,7 @@
 #include "llvm/ADT/IntrusiveRefCntPtr.h"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/ADT/StringRef.h"
+#include "llvm/Support/Compiler.h"
 #include "llvm/Support/raw_ostream.h"
 #include <string>
 
@@ -91,6 +92,6 @@ public:
 ///   }
 /// \endcode
 #define LOG_SECTION(NAME) if (LogRef Log = clang::cxindex::Logger::make(NAME))
-#define LOG_FUNC_SECTION LOG_SECTION(__func__)
+#define LOG_FUNC_SECTION LOG_SECTION(LLVM_FUNCTION_NAME)
 
 #endif
