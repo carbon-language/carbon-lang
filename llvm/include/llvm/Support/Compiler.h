daa46eb7361fe3d04d377c3713ce7fe68e0f3113
Michael J. Spencer <bigcheesegs@gmail.com>
2013-01-15 05:01:39 +0000

    [Support] Add LLVM_CONSTEXPR.
    Marks a decl as constexpr if the compiler supports it.
    
    llvm-svn: 172508

--- llvm/include/llvm/Support/Compiler.h
+++ llvm/include/llvm/Support/Compiler.h
@@ -95,6 +95,12 @@
 #define LLVM_OVERRIDE
 #endif
 
+#if __has_feature(cxx_constexpr) || defined(__GXX_EXPERIMENTAL_CXX0X__)
+# define LLVM_CONSTEXPR constexpr
+#else
+# define LLVM_CONSTEXPR
+#endif
+
 /// LLVM_LIBRARY_VISIBILITY - If a class marked with this attribute is linked
 /// into a shared library, then the class should be private to the library and
 /// not accessible from outside it.  Can also be used to mark variables and
