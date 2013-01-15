f6dc082f91059dbc490830201cc2865c2309e075
Shankar Easwaran <shankare@codeaurora.org>
2013-01-15 03:02:33 +0000

    remove const from FindByName
    llvm-svn: 172507

--- lld/lib/ReaderWriter/ELF/WriterELF.cpp
+++ lld/lib/ReaderWriter/ELF/WriterELF.cpp
@@ -1543,7 +1543,7 @@ public:
   /// \brief find a absolute atom pair given a absolute atom name
   struct FindByName {
     const std::string _name;
-    FindByName(const StringRef name) : _name(name) {}
+    FindByName(StringRef name) : _name(name) {}
     bool operator()(AbsoluteAtomPair& j) { 
       return j.absoluteAtom()->name() == _name; 
     }
