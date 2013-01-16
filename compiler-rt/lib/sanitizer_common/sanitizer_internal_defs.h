7f4603200fd8f8f331610de68eba60f92ec90d4f
Dmitry Vyukov <dvyukov@google.com>
2013-01-16 14:35:13 +0000

    asan: fix ARM build
    llvm-svn: 172613

--- compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
@@ -52,7 +52,12 @@ using namespace __sanitizer;  // NOLINT
 # define UNLIKELY(x)   __builtin_expect(!!(x), 0)
 # define UNUSED __attribute__((unused))
 # define USED __attribute__((used))
-# define PREFETCH(x) __asm__("prefetchnta (%0)" : : "r" (x))
+# if defined(__i386__) || defined(__x86_64__)
+// __builtin_prefetch(x) generates prefetchnt0 on x86
+#  define PREFETCH(x) __asm__("prefetchnta (%0)" : : "r" (x))
+# else
+#  define PREFETCH(x) __builtin_prefetch(x)
+# endif
 #endif  // _MSC_VER
 
 #if defined(_WIN32)
