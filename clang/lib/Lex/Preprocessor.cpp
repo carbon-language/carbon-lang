f157e901350ae2bf2b9e16c315f0d876b46a3149
David Blaikie <dblaikie@gmail.com>
2013-01-16 23:18:16 +0000

    Remove unnecessary initialization i Added in r172668.
    echristo already fixed this in r172649, but I'll leave the reformatting in
    since I'm in the blame history for it now anyway.
    
    llvm-svn: 172672

--- clang/lib/Lex/Preprocessor.cpp
+++ clang/lib/Lex/Preprocessor.cpp
@@ -62,12 +62,11 @@ Preprocessor::Preprocessor(IntrusiveRefCntPtr<PreprocessorOptions> PPOpts,
     : PPOpts(PPOpts), Diags(&diags), LangOpts(opts), Target(target),
       FileMgr(Headers.getFileMgr()), SourceMgr(SM), HeaderInfo(Headers),
       TheModuleLoader(TheModuleLoader), ExternalSource(0),
-      ParsingIfOrElifDirective(false), Identifiers(opts, IILookup),
-      IncrementalProcessing(IncrProcessing), CodeComplete(0),
-      CodeCompletionFile(0), CodeCompletionOffset(0), CodeCompletionReached(0),
-      SkipMainFilePreamble(0, true), CurPPLexer(0), CurDirLookup(0),
-      CurLexerKind(CLK_Lexer), Callbacks(0), Listener(0), MacroArgCache(0),
-      Record(0), MIChainHead(0), MICache(0) {
+      Identifiers(opts, IILookup), IncrementalProcessing(IncrProcessing),
+      CodeComplete(0), CodeCompletionFile(0), CodeCompletionOffset(0),
+      CodeCompletionReached(0), SkipMainFilePreamble(0, true), CurPPLexer(0),
+      CurDirLookup(0), CurLexerKind(CLK_Lexer), Callbacks(0), Listener(0),
+      MacroArgCache(0), Record(0), MIChainHead(0), MICache(0) {
   OwnsHeaderSearch = OwnsHeaders;
   
   ScratchBuf = new ScratchBuffer(SourceMgr);
