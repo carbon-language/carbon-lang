8c6cbe776ba85be759a9fa72518349767e0bc889
NAKAMURA Takumi <geek4civic@gmail.com>
2013-01-17 01:07:09 +0000

    clang/test/Modules/compiler_builtins.m: Mark this as XFAIL:win32 for now. Investigating.
    llvm-svn: 172689

--- clang/test/Modules/compiler_builtins.m
+++ clang/test/Modules/compiler_builtins.m
@@ -2,6 +2,7 @@
 // RUN: %clang -fsyntax-only -fmodules -fmodule-cache-path %t -D__need_wint_t %s -Xclang -verify
 // RUN: %clang -fsyntax-only -std=c99 -fmodules -fmodule-cache-path %t -D__need_wint_t %s -Xclang -verify
 // expected-no-diagnostics
+// XFAIL: win32
 
 #ifdef __SSE__
 @import _Builtin_intrinsics.intel.sse;
