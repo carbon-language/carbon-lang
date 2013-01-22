3bb0aa762566895e620ab0eb59fd7e1313fbf444
NAKAMURA Takumi <geek4civic@gmail.com>
2013-01-22 01:51:48 +0000

    clang/test/CodeGen/blocks-seq.c: FileCheck-ize.
    llvm-svn: 173113

--- clang/test/CodeGen/blocks-seq.c
+++ clang/test/CodeGen/blocks-seq.c
@@ -1,13 +1,11 @@
-// FIXME: We forcibly strip the names so that the test doesn't vary between
-// builds with and without asserts. We need a better solution for this.
-
-// RUN: %clang_cc1 -fblocks -triple x86_64-apple-darwin10 -emit-llvm-bc -o - %s | opt -strip | llvm-dis > %t
-// RUN: grep '%6 = call i32 (...)\* @rhs()' %t | count 1
-// RUN: grep '%7 = getelementptr inbounds %0\* %1, i32 0, i32 1' %t | count 1
-// RUN: grep '%8 = load %0\*\* %7' %t | count 1
-// RUN: grep '%10 = call i32 (...)\* @rhs()' %t | count 1
-// RUN: grep '%11 = getelementptr inbounds %0\* %1, i32 0, i32 1' %t | count 1
-// RUN: grep '%12 = load %0\*\* %11' %t | count 1
+// RUN: %clang_cc1 -fblocks -triple x86_64-apple-darwin10 -emit-llvm -o - %s | FileCheck %s
+// CHECK: [[Vi:%.+]] = alloca %struct.__block_byref_i, align 8
+// CHECK: call i32 (...)* @rhs()
+// CHECK: [[V7:%.+]] = getelementptr inbounds %struct.__block_byref_i* [[Vi]], i32 0, i32 1
+// CHECK: load %struct.__block_byref_i** [[V7]]
+// CHECK: call i32 (...)* @rhs()
+// CHECK: [[V11:%.+]] = getelementptr inbounds %struct.__block_byref_i* [[Vi]], i32 0, i32 1
+// CHECK: load %struct.__block_byref_i** [[V11]]
 
 int rhs();
 
