773f74cb10a9b98d7f8718517559f261696b9507
NAKAMURA Takumi <geek4civic@gmail.com>
2013-01-23 08:30:21 +0000

    Untabify.
    llvm-svn: 173235

--- llvm/unittests/IR/WaymarkTest.cpp
+++ llvm/unittests/IR/WaymarkTest.cpp
@@ -27,13 +27,13 @@ TEST(WaymarkTest, NativeArray) {
   static uint8_t tail[22] = "s02s33s30y2y0s1x0syxS";
   Value * values[22];
   std::transform(tail, tail + 22, values, char2constant);
-  FunctionType *FT =	FunctionType::get(Type::getVoidTy(getGlobalContext()), true);
+  FunctionType *FT = FunctionType::get(Type::getVoidTy(getGlobalContext()), true);
   Function *F = Function::Create(FT, GlobalValue::ExternalLinkage);
-	const CallInst *A = CallInst::Create(F, makeArrayRef(values));
+  const CallInst *A = CallInst::Create(F, makeArrayRef(values));
   ASSERT_NE(A, (const CallInst*)NULL);
   ASSERT_EQ(1U + 22, A->getNumOperands());
-	const Use *U = &A->getOperandUse(0);
-	const Use *Ue = &A->getOperandUse(22);
+  const Use *U = &A->getOperandUse(0);
+  const Use *Ue = &A->getOperandUse(22);
   for (; U != Ue; ++U)
   {
     EXPECT_EQ(A, U->getUser());
@@ -43,7 +43,7 @@ TEST(WaymarkTest, NativeArray) {
 TEST(WaymarkTest, TwoBit) {
   Use* many = (Use*)calloc(sizeof(Use), 8212 + 1);
   ASSERT_TRUE(many);
-	Use::initTags(many, many + 8212);
+  Use::initTags(many, many + 8212);
   for (Use *U = many, *Ue = many + 8212 - 1; U != Ue; ++U)
   {
     EXPECT_EQ(reinterpret_cast<User *>(Ue + 1), U->getUser());
