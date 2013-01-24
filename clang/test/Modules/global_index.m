7a23231e6b034d75924f1986634a532a0fa9f893
NAKAMURA Takumi <geek4civic@gmail.com>
2013-01-24 07:04:14 +0000

    clang/test/Modules/global_index.m: XFAILing on win32 when investigating for now.
    llvm-svn: 173326

--- clang/test/Modules/global_index.m
+++ clang/test/Modules/global_index.m
@@ -2,6 +2,7 @@
 // RUN: %clang_cc1 -Wauto-import -fmodule-cache-path %t -fdisable-module-hash -fmodules -generate-module-index -F %S/Inputs %s -verify
 // RUN: ls %t|grep modules.idx
 // REQUIRES: shell
+// XFAIL: mingw32
 
 // expected-no-diagnostics
 @import DependsOnModule;
