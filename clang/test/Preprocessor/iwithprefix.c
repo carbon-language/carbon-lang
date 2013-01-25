b7eaa6b8ac4a79beafd7235cb8c1d269b4c03720
Daniel Dunbar <daniel@zuster.org>
2013-01-25 18:50:18 +0000

    [tests] Force a triple to ensure /usr/include is one of the entries.
    llvm-svn: 173469

--- clang/test/Preprocessor/iwithprefix.c
+++ clang/test/Preprocessor/iwithprefix.c
@@ -2,9 +2,10 @@
 //
 // RUN: rm -rf %t.tmps
 // RUN: mkdir -p %t.tmps/first %t.tmps/second
-// RUN: %clang_cc1 \
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown \
 // RUN:   -iprefix %t.tmps/ -iwithprefix second \
 // RUN:    -isystem %t.tmps/first -v 2> %t.out
+// RUN: cat %t.out
 // RUN: FileCheck < %t.out %s
 
 // CHECK: #include <...> search starts here:
