d0ed6c249dbd6bd488b6491b536a387548c00f7e
Fariborz Jahanian <fjahanian@apple.com>
2013-01-25 17:47:49 +0000

    Add space after ';'.
    llvm-svn: 173462

--- clang/test/Sema/switch-1.c
+++ clang/test/Sema/switch-1.c
@@ -4,15 +4,15 @@
 
 int f(int i) {
   switch (i) {
-    case 2147483647 + 2: // expected-warning {{overflow in expression;result is -2147483647 with type 'int'}}
+    case 2147483647 + 2: // expected-warning {{overflow in expression; result is -2147483647 with type 'int'}}
       return 1;
-    case 9223372036854775807L * 4: // expected-warning {{overflow in expression;result is -4 with type 'long'}}
+    case 9223372036854775807L * 4: // expected-warning {{overflow in expression; result is -4 with type 'long'}}
       return 2;
-    case (123456 *789012) + 1:  // expected-warning {{overflow in expression;result is -1375982336 with type 'int'}}
+    case (123456 *789012) + 1:  // expected-warning {{overflow in expression; result is -1375982336 with type 'int'}}
       return 3;
     case 2147483647:
       return 0;
   }
-  return (i, 65537) * 65537; // expected-warning {{overflow in expression;result is 131073 with type 'int'}} \
+  return (i, 65537) * 65537; // expected-warning {{overflow in expression; result is 131073 with type 'int'}} \
 			     // expected-warning {{expression result unused}}
 }
