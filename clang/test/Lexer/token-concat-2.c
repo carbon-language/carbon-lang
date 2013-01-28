75bd3a8ec1405786b2d8a9baceecf5172d65ef85
Dmitri Gribenko <gribozavr@gmail.com>
2013-01-28 20:40:50 +0000

    FileCheck'ize and merge tests
    llvm-svn: 173714

--- clang/test/Lexer/token-concat-2.c
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cc1 -E -x c -o - %s | grep '[.][*]'
-// PR4395
-#define X .*
-X
--- clang/test/Lexer/token-concat.c
+++ clang/test/Lexer/token-concat.c
@@ -1,4 +1,11 @@
-// RUN: %clang_cc1 -E -x c -o %t %s
-// RUN: grep 'IDENT.2' %t
+// RUN: %clang_cc1 -E %s | FileCheck --strict-whitespace %s
 
 IDENT.2
+// CHECK: {{^}}IDENT.2{{$}}
+
+
+// PR4395
+#define X .*
+X
+// CHECK: {{^}}.*{{$}}
+
