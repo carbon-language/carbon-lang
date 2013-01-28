9eaae74f7d13e9c5cf74bd90b78dc3d49da96c77
Dmitri Gribenko <gribozavr@gmail.com>
2013-01-28 17:30:37 +0000

    FileCheck'ize test
    llvm-svn: 173696

--- clang/test/Driver/clang-g-opts.c
+++ clang/test/Driver/clang-g-opts.c
@@ -1,5 +1,9 @@
-// RUN: %clang -S -v -o %t %s        2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g     2>&1 | grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g0    2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g -g0 2>&1 | not grep -w -- -g
-// RUN: %clang -S -v -o %t %s -g0 -g 2>&1 | grep -w -- -g
+// RUN: %clang -### -S %s        2>&1 | FileCheck --check-prefix=CHECK-WITHOUT-G %s
+// RUN: %clang -### -S %s -g     2>&1 | FileCheck --check-prefix=CHECK-WITH-G    %s
+// RUN: %clang -### -S %s -g0    2>&1 | FileCheck --check-prefix=CHECK-WITHOUT-G %s
+// RUN: %clang -### -S %s -g -g0 2>&1 | FileCheck --check-prefix=CHECK-WITHOUT-G %s
+// RUN: %clang -### -S %s -g0 -g 2>&1 | FileCheck --check-prefix=CHECK-WITH-G    %s
+
+// CHECK-WITHOUT-G-NOT: "-g"
+// CHECK-WITH-G: "-g"
+
