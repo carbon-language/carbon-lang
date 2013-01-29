ff37d94e6f74ed23aa6311af692c5c079aec3eab
Michael Han <fragmentshaders@gmail.com>
2013-01-29 04:13:20 +0000

    Add a test case for attribute print.
    llvm-svn: 173765

--- clang/test/SemaCXX/attr-print.cpp
+++ clang/test/SemaCXX/attr-print.cpp
@@ -14,3 +14,7 @@ void foo() __attribute__((const));
 
 // CHECK: void bar() __attribute__((__const));
 void bar() __attribute__((__const));
+
+// FIXME: Print this with correct format and order.
+// CHECK: void foo1() __attribute__((pure)) __attribute__((noinline));
+void foo1() __attribute__((noinline, pure));
