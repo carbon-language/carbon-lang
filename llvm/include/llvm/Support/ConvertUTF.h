3c5210b2b82b0ada8e5d5d9432bd1853533b1eda
Dmitri Gribenko <gribozavr@gmail.com>
2013-01-30 15:10:12 +0000

    LLDB uses ConvertUTF16toUTF8, remove #ifdef
    llvm-svn: 173936

--- llvm/include/llvm/Support/ConvertUTF.h
+++ llvm/include/llvm/Support/ConvertUTF.h
@@ -137,11 +137,9 @@ ConversionResult ConvertUTF8toUTF32 (
   const UTF8** sourceStart, const UTF8* sourceEnd,
   UTF32** targetStart, UTF32* targetEnd, ConversionFlags flags);
 
-#ifdef CLANG_NEEDS_THESE_ONE_DAY
 ConversionResult ConvertUTF16toUTF8 (
   const UTF16** sourceStart, const UTF16* sourceEnd,
   UTF8** targetStart, UTF8* targetEnd, ConversionFlags flags);
-#endif
 
 ConversionResult ConvertUTF32toUTF8 (
   const UTF32** sourceStart, const UTF32* sourceEnd,
