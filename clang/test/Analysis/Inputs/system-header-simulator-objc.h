b54cfa310a793b0cd9a097c7aa4883524b550630
Jordan Rose <jordan_rose@apple.com>
2013-02-01 19:50:01 +0000

    [analyzer] Explain why we have system-header-simulator*.h files.
    Suggested by Csaba. Text based on an e-mail of mine on cfe-dev.
    
    llvm-svn: 174213

--- clang/test/Analysis/Inputs/system-header-simulator-objc.h
+++ clang/test/Analysis/Inputs/system-header-simulator-objc.h
@@ -1,3 +1,8 @@
+// Like the compiler, the static analyzer treats some functions differently if
+// they come from a system header -- for example, it is assumed that system
+// functions do not arbitrarily free() their parameters, and that some bugs
+// found in system headers cannot be fixed by the user and should be
+// suppressed.
 #pragma clang system_header
 
 typedef unsigned int UInt32;
