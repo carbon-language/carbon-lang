f23fc86ca89c0db2617be724e9dcb96ec95236c0
Shankar Easwaran <shankare@codeaurora.org>
2013-02-01 15:05:11 +0000

    add proper copyright header
    llvm-svn: 174178

--- lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationFunctions.h
+++ lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationFunctions.h
@@ -1,6 +1,6 @@
 //===- HexagonRelocationFunction.h ----------------------------------------===//
 //
-//                     The MCLinker Project
+//                             The LLVM Linker
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
--- lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationHandler.h
+++ lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationHandler.h
@@ -1,6 +1,6 @@
 //===- lld/ReaderWriter/ELF/Hexagon/HexagonRelocationHandler.h -----------===//
 //
-//                     The MCLinker Project
+//                             The LLVM Linker
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
