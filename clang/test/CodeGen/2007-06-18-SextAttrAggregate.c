7df5c0df3b12b03f070b87d10d9787f51d2a667f
Tim Northover <Tim.Northover@arm.com>
2013-02-07 15:11:40 +0000

    XFAIL test that's inappropriate for AArch64 ABI
    Only some ABIs require the "signext" attribute on parameters. On most
    platforms, however, it's a useful test so it's best not to limit it to some
    random arbitrary platform.
    
    llvm-svn: 174619

--- clang/test/CodeGen/2007-06-18-SextAttrAggregate.c
+++ clang/test/CodeGen/2007-06-18-SextAttrAggregate.c
@@ -1,5 +1,13 @@
 // RUN: %clang_cc1 %s -o - -emit-llvm | FileCheck %s
+// XFAIL: aarch64
+
 // PR1513
+
+// AArch64 ABI actually requires the reverse of what this is testing: the callee
+// does any extensions and remaining bits are unspecified.
+
+// Technically this test wasn't written to test that feature, but it's a
+// valuable check nevertheless.
 
 struct s{
 long a;
