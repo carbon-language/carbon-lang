594d99588be93b93e5621803a50e302a92b778e7
Alexander Potapenko <glider@google.com>
2013-02-14 12:37:17 +0000

    [ASan] Fixed the wrong placement of #include
    llvm-svn: 175157

--- compiler-rt/lib/asan/lit_tests/Darwin/reexec-insert-libraries-env.cc
+++ compiler-rt/lib/asan/lit_tests/Darwin/reexec-insert-libraries-env.cc
@@ -1,4 +1,3 @@
-#include <stdio.h>
 // Make sure ASan doesn't hang in an exec loop if DYLD_INSERT_LIBRARIES is set.
 // This is a regression test for
 // https://code.google.com/p/address-sanitizer/issues/detail?id=159
@@ -10,6 +9,7 @@
 // FIXME: the following command line may hang in the case of a regression.
 // RUN: DYLD_INSERT_LIBRARIES=darwin-dummy-shared-lib-so.dylib \
 // RUN:     %t 2>&1 | FileCheck %s || exit 1
+#include <stdio.h>
 #include <stdlib.h>
 
 int main() {
