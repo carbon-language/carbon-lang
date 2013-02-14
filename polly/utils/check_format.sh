c38af887649c5185bb4f45fa1f54ba110d913e82
Tobias Grosser <grosser@fim.uni-passau.de>
2013-02-14 16:19:16 +0000

    cmake: add command polly-check-format
    This command runs clang-format on all files and reports formatting problems
    
    llvm-svn: 175170

---
+++ polly/utils/check_format.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+OK=0
+
+for ARG in "$@"
+  do
+    clang-format $1 | diff $1 -
+
+    if [[ $? -eq 1 ]]; then
+      OK=1
+    fi
+  done
+
+if [[ $OK -eq "1" ]]; then
+  echo "Error: clang-format reported formatting differences"
+else
+  echo "OK: clang-format reported no formatting differences"
+fi
+
