b633eab8cb27e475f6e15c4c46c50c2131edfea2
Dmitri Gribenko <gribozavr@gmail.com>
2013-02-17 03:06:16 +0000

    Fix for ARM: functions don't have extra attributes there, so {{.*}} is ""
    While there, explicitly declare functions to remove warnings.
    
    llvm-svn: 175384

--- clang/test/CodeGen/stack-protector.c
+++ clang/test/CodeGen/stack-protector.c
@@ -1,11 +1,15 @@
 // RUN: %clang_cc1 -emit-llvm -o - %s -stack-protector 0 | FileCheck -check-prefix=NOSSP %s
-// NOSSP: define void @test1(i8* %msg) nounwind {{.*}} {
+// NOSSP: define void @test1(i8* %msg) nounwind {{.*}}{
 // RUN: %clang_cc1 -emit-llvm -o - %s -stack-protector 1 | FileCheck -check-prefix=WITHSSP %s
-// WITHSSP: define void @test1(i8* %msg) nounwind ssp {{.*}} {
+// WITHSSP: define void @test1(i8* %msg) nounwind ssp {{.*}}{
 // RUN: %clang_cc1 -emit-llvm -o - %s -stack-protector 2 | FileCheck -check-prefix=SSPREQ %s
-// SSPREQ: define void @test1(i8* %msg) nounwind sspreq {{.*}} {
+// SSPREQ: define void @test1(i8* %msg) nounwind sspreq {{.*}}{
+
+typedef __SIZE_TYPE__ size_t;
 
 int printf(const char * _Format, ...);
+size_t strlen(const char *s);
+char *strcpy(char *s1, const char *s2);
 
 void test1(const char *msg) {
   char a[strlen(msg) + 1];
