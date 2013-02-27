4f4ed0acdf2b2c7bba05999a379e28b05048aeac
Shankar Easwaran <shankare@codeaurora.org>
2013-02-27 05:33:27 +0000

    adding a return to fix compilation error from buildbot, build #75
    llvm-svn: 176167

--- lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationFunctions.h
+++ lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationFunctions.h
@@ -40,6 +40,7 @@ uint32_t findBitMask(uint32_t insn, Instruction *encodings, int32_t numInsns) {
       return encodings[i].insnBitMask;
   }
   assert(0 && "found unknown instruction");
+  return 0;
 }
 
 } // elf 
