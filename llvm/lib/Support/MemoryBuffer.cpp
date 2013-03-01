db4443f7af005588d4fd0df5669a2a2ee21fac11
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2013-03-01 22:48:51 +0000

    In llvm::MemoryBuffer::getFile() remove an unnecessary stat call check.
    The sys::fs::is_directory() check is unnecessary because, if the filename is
    a directory, the function will fail anyway with the same error code returned.
    Remove the check to avoid an unnecessary stat call.
    
    Someone needs to review on windows and see if the check is necessary there or not.
    
    llvm-svn: 176386

--- llvm/lib/Support/MemoryBuffer.cpp
+++ llvm/lib/Support/MemoryBuffer.cpp
@@ -244,6 +244,8 @@ error_code MemoryBuffer::getFile(const char *Filename,
                                  OwningPtr<MemoryBuffer> &result,
                                  int64_t FileSize,
                                  bool RequiresNullTerminator) {
+  // FIXME: Review if this check is unnecessary on windows as well.
+#ifdef LLVM_ON_WIN32
   // First check that the "file" is not a directory
   bool is_dir = false;
   error_code err = sys::fs::is_directory(Filename, is_dir);
@@ -251,6 +253,7 @@ error_code MemoryBuffer::getFile(const char *Filename,
     return err;
   if (is_dir)
     return make_error_code(errc::is_a_directory);
+#endif
 
   int OpenFlags = O_RDONLY;
 #ifdef O_BINARY
