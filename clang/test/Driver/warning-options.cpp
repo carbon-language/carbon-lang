897ce8c8bb7816eab9a082df7bf391525f6588f8
Bob Wilson <bob.wilson@apple.com>
2013-03-12 20:00:34 +0000

    Revert "Revert r166370 and r166540 now that Xcode 4.6 has been available for a while."
    This reverts commit 176887.  Nico asked for more time to move to Xcode 4.6.
    
    llvm-svn: 176892

--- clang/test/Driver/warning-options.cpp
+++ clang/test/Driver/warning-options.cpp
@@ -3,6 +3,11 @@
 // RUN: %clang -### -Wlarge-by-value-copy=128 %s 2>&1 | FileCheck -check-prefix=LARGE_VALUE_COPY_JOINED %s
 // LARGE_VALUE_COPY_JOINED: -Wlarge-by-value-copy=128
 
+// FIXME: Remove this together with -Warc-abi once an Xcode is released that doesn't pass this flag.
+// RUN: %clang -### -Warc-abi -Wno-arc-abi %s 2>&1 | FileCheck -check-prefix=ARCABI %s
+// ARCABI-NOT: unknown warning option '-Warc-abi'
+// ARCABI-NOT: unknown warning option '-Wno-arc-abi'
+
 // Check that -isysroot warns on nonexistent paths.
 // RUN: %clang -### -c -target i386-apple-darwin10 -isysroot /FOO %s 2>&1 | FileCheck --check-prefix=CHECK-ISYSROOT %s
 // CHECK-ISYSROOT: warning: no such sysroot directory: '{{([A-Za-z]:.*)?}}/FOO'
