464b07086ebcb5c25a5cb253f7d71d66c2a1d15c
Ted Kremenek <kremenek@apple.com>
2013-03-12 21:49:29 +0000

    Make modules.m test portable, now that autolinking is dependent on whether or not the target uses the integrated assembler.
    llvm-svn: 176902

--- clang/test/Driver/modules.m
+++ clang/test/Driver/modules.m
@@ -4,7 +4,7 @@
 // RUN: %clang -fmodules -fno-modules -fmodules -### %s 2>&1 | FileCheck -check-prefix=CHECK-HAS-MODULES %s
 // CHECK-HAS-MODULES: -fmodules
 
-// RUN: %clang -fmodules -fno-modules -fmodules -### %s 2>&1 | FileCheck -check-prefix=CHECK-HAS-AUTOLINK %s
+// RUN: %clang -target x86_64-apple-darwin10 -fmodules -fno-modules -fmodules -### %s 2>&1 | FileCheck -check-prefix=CHECK-HAS-AUTOLINK %s
 // CHECK-HAS-AUTOLINK: -fmodules-autolink
 
 // RUN: %clang -fmodules -fno-modules -fno-modules-autolink -fmodules -### %s 2>&1 | FileCheck -check-prefix=CHECK-NO-AUTOLINK %s
