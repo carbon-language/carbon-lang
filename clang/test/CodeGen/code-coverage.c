008322f2b071412798ec74c79d1987a7cc317225
Bill Wendling <isanbard@gmail.com>
2013-03-18 23:11:54 +0000

    Update testcase for r177340.
    llvm-svn: 177344

--- clang/test/CodeGen/code-coverage.c
+++ clang/test/CodeGen/code-coverage.c
@@ -16,7 +16,7 @@ int test1(int a) {
 
 // CHECK: void @__llvm_gcov_indirect_counter_increment(i32* %{{.*}}, i64** %{{.*}}) unnamed_addr [[NRZ:#[0-9]+]]
 // CHECK: void @__llvm_gcov_writeout() unnamed_addr [[NRZ]]
+// CHECK: void @__llvm_gcov_flush() unnamed_addr [[NRZ]]
 // CHECK: void @__llvm_gcov_init() unnamed_addr [[NRZ]]
-// CHECK: void @__gcov_flush() unnamed_addr [[NRZ]]
 
 // CHECK: attributes [[NRZ]] = { {{.*}}noredzone{{.*}} }
