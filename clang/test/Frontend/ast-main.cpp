3fd576f7d66011a994088e2f5b208e9af9beb46c
Aaron Ballman <aaron@aaronballman.com>
2013-03-21 19:38:59 +0000

    Delayed template parsing is not supported by the AST serialization system yet, so turning it off.  This fixes a test breakage caused by r177336.
    llvm-svn: 177655

--- clang/test/Frontend/ast-main.cpp
+++ clang/test/Frontend/ast-main.cpp
@@ -1,5 +1,5 @@
 // RUN: %clang -emit-llvm -S -o %t1.ll -x c++ - < %s
-// RUN: %clang -emit-ast -o %t.ast %s
+// RUN: %clang -fno-delayed-template-parsing -emit-ast -o %t.ast %s
 // RUN: %clang -emit-llvm -S -o %t2.ll -x ast - < %t.ast
 // RUN: diff %t1.ll %t2.ll
 
