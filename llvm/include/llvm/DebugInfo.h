9be783da4598dac37ae033332d877d59c10400dd
David Blaikie <dblaikie@gmail.com>
2013-03-21 00:10:31 +0000

    Removing unused DISubprogram::getFile
    llvm-svn: 177614

--- llvm/include/llvm/DebugInfo.h
+++ llvm/include/llvm/DebugInfo.h
@@ -478,10 +478,6 @@ namespace llvm {
       return getFieldAs<DIFile>(6).getDirectory();
     }
 
-    DIFile getFile() const {
-      return getFieldAs<DIFile>(6);
-    }
-
     /// getScopeLineNumber - Get the beginning of the scope of the
     /// function, not necessarily where the name of the program
     /// starts.
