719f8584f21f2600e5d99bc5c3d84e99df7b66c9
NAKAMURA Takumi <geek4civic@gmail.com>
2013-03-26 18:57:40 +0000

    clang/test/CodeGenCXX/debug-info-namespace.cpp: Disable it on non-bash lit for now.
    With dosish filename, it misgenerates an improper extra entry.
    
    !1 = metadata !{metadata !"E:\5Cllvm\5Cllvm-project\5Cclang\5Ctest\5CCodeGenCXX/debug-info-namespace.cpp", metadata !"E:\5Cllvm\5Cbuild\5Cninja-win32-vs11\5Ctools\5Cclang\5Ctest\5CCodeGenCXX"}
    
    !8 = metadata !{metadata !"E:\5Cllvm\5Cllvm-project\5Cclang\5Ctest\5CCodeGenCXX\5Cdebug-info-namespace.cpp", metadata !"E:\5Cllvm\5Cbuild\5Cninja-win32-vs11\5Ctools\5Cclang\5Ctest\5CCodeGenCXX"}
    
    !8 is unexpected.
    
    llvm-svn: 178061

--- clang/test/CodeGenCXX/debug-info-namespace.cpp
+++ clang/test/CodeGenCXX/debug-info-namespace.cpp
@@ -12,3 +12,6 @@ int i;
 // CHECK: [[NS]] = {{.*}}, metadata [[FILE2:![0-9]*]], metadata [[CTXT:![0-9]*]], {{.*}} ; [ DW_TAG_namespace ] [B] [line 1]
 // CHECK: [[CTXT]] = {{.*}}, metadata [[FILE]], null, {{.*}} ; [ DW_TAG_namespace ] [A] [line 3]
 // CHECK: [[FILE2]]} ; [ DW_TAG_file_type ] [{{.*}}foo.cpp]
+
+// FIXME: It is confused on win32 to generate file entry when dosish filename is given.
+// REQUIRES: shell
