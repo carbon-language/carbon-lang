6c75232cf0922dd284180f0a876498f0ba57c15d
Eric Christopher <echristo@gmail.com>
2013-03-28 21:37:18 +0000

    These two are default in the constructor for MCAsmInfo.
    llvm-svn: 178293

--- llvm/lib/Target/R600/MCTargetDesc/AMDGPUMCAsmInfo.cpp
+++ llvm/lib/Target/R600/MCTargetDesc/AMDGPUMCAsmInfo.cpp
@@ -68,8 +68,6 @@ AMDGPUMCAsmInfo::AMDGPUMCAsmInfo(const Target &T, StringRef &TT) : MCAsmInfo() {
   //===--- Dwarf Emission Directives -----------------------------------===//
   HasLEB128 = true;
   SupportsDebugInformation = true;
-  ExceptionsType = ExceptionHandling::None;
-  DwarfUsesInlineInfoSection = false;
   DwarfSectionOffsetDirective = ".offset";
 
 }
