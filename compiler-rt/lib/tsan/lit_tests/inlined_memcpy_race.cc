3e73c8405055a38e8c4902f3d0c6f1c1319dbc6d
Kostya Serebryany <kcc@google.com>
2013-03-28 11:21:50 +0000

    [tsan] a test checking that memset/memcpy/memmove are not inlined in tsan mode
    llvm-svn: 178231

--- compiler-rt/lib/tsan/lit_tests/inlined_memcpy_race.cc
+++ compiler-rt/lib/tsan/lit_tests/inlined_memcpy_race.cc
@@ -1,27 +1,55 @@
 // RUN: %clangxx_tsan -O1 %s -o %t && %t 2>&1 | FileCheck %s
-// Currently, we don't report a race here:
-// http://code.google.com/p/thread-sanitizer/issues/detail?id=16
 #include <pthread.h>
 #include <stddef.h>
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
 
-int x[4], y[4];
+int x[4], y[4], z[4];
 
-void *Thread1(void *unused) {
-  memcpy(x, y, 16);
+void *MemCpyThread(void *a) {
+  memcpy((int*)a, z, 16);
+  return NULL;
+}
+
+void *MemMoveThread(void *a) {
+  memmove((int*)a, z, 16);
+  return NULL;
+}
+
+void *MemSetThread(void *a) {
+  sleep(1);
+  memset((int*)a, 0, 16);
   return NULL;
 }
 
 int main() {
   pthread_t t[2];
-  pthread_create(&t[0], NULL, Thread1, NULL);
-  pthread_create(&t[1], NULL, Thread1, NULL);
+  // Race on x between memcpy and memset
+  pthread_create(&t[0], NULL, MemCpyThread, x);
+  pthread_create(&t[1], NULL, MemSetThread, x);
   pthread_join(t[0], NULL);
   pthread_join(t[1], NULL);
+  // Race on y between memmove and memset
+  pthread_create(&t[0], NULL, MemMoveThread, y);
+  pthread_create(&t[1], NULL, MemSetThread, y);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+
   printf("PASS\n");
   return 0;
 }
 
-// CHECK-NOT: ThreadSanitizer
+// CHECK: WARNING: ThreadSanitizer: data race
+// CHECK:   #0 memset
+// CHECK:   #1 MemSetThread
+// CHECK:  Previous write
+// CHECK:   #0 memcpy
+// CHECK:   #1 MemCpyThread
+
+// CHECK: WARNING: ThreadSanitizer: data race
+// CHECK:   #0 memset
+// CHECK:   #1 MemSetThread
+// CHECK:  Previous write
+// CHECK:   #0 memmove
+// CHECK:   #1 MemMoveThread
