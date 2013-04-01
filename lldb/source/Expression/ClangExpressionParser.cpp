eeb4384924bf15f500bd0d516a29cfc1a78b0640
Sean Callanan <scallanan@apple.com>
2013-04-01 22:12:37 +0000

    Enabled blocks support in the expression parser. Note: although it is now possible to declare blocks and call them inside the same expression, we do not generate correct block descriptors so these blocks cannot be passed to functions like dispatch_async.
    <rdar://problem/12578656>
    
    llvm-svn: 178509

--- lldb/source/Expression/ClangExpressionParser.cpp
+++ lldb/source/Expression/ClangExpressionParser.cpp
@@ -284,6 +284,7 @@ ClangExpressionParser::ClangExpressionParser (ExecutionContextScope *exe_scope,
     
     m_compiler->getLangOpts().Bool = true;
     m_compiler->getLangOpts().WChar = true;
+    m_compiler->getLangOpts().Blocks = true;
     m_compiler->getLangOpts().DebuggerSupport = true; // Features specifically for debugger clients
     if (expr.DesiredResultType() == ClangExpression::eResultTypeId)
         m_compiler->getLangOpts().DebuggerCastResultToId = true;
