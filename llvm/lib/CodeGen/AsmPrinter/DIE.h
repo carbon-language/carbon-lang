bdcb4464e2bc50cc24fd5d81018c1f7ab4695f1b
Manman Ren <mren@apple.com>
2013-04-04 23:13:11 +0000

    Debug Info: revert 178722 for now.
    There is a difference for FORM_ref_addr between DWARF 2 and DWARF 3+.
    Since Eric is against guarding DWARF 2 ref_addr with DarwinGDBCompat, we are
    still in discussion on how to handle this.
    
    The correct solution is to update our header to say version 4 instead of version
    2 and update tool chains as well.
    
    rdar://problem/13559431
    
    llvm-svn: 178806

--- llvm/lib/CodeGen/AsmPrinter/DIE.h
+++ llvm/lib/CodeGen/AsmPrinter/DIE.h
@@ -336,7 +336,9 @@ namespace llvm {
 
     /// SizeOf - Determine size of debug information entry in bytes.
     ///
-    virtual unsigned SizeOf(AsmPrinter *AP, unsigned Form) const;
+    virtual unsigned SizeOf(AsmPrinter *AP, unsigned Form) const {
+      return sizeof(int32_t);
+    }
 
     // Implement isa/cast/dyncast.
     static bool classof(const DIEValue *E) { return E->getType() == isEntry; }
