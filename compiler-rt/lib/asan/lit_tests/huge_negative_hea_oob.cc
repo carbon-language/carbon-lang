f5407e8d8f9c7472ebcf158b64e149099cba284a
Kostya Serebryany <kcc@google.com>
2013-04-05 15:13:23 +0000

    [asan] add a test for huge left oob
    llvm-svn: 178874

---
+++ compiler-rt/lib/asan/lit_tests/huge_negative_hea_oob.cc
@@ -0,0 +1,10 @@
+// RUN: %clangxx_asan -m64  %s -o %t && %t 2>&1 | %symbolize > %t.out
+// Check that we can find huge buffer overflows to the left.
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, char **argv) {
+  char *x = (char*)malloc(1 << 20);
+  memset(x, 0, 10);
+  int res = x[-argc * 4000];  // BOOOM
+  // CHECK: is located 4000 bytes to the left of
+}
