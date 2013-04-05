228ecf46ab46f1404b53b03bc4733467513c00be
Kostya Serebryany <kcc@google.com>
2013-04-05 15:16:48 +0000

    [asan] make huge_negative_hea_oob more meaningful
    llvm-svn: 178876

--- compiler-rt/lib/asan/lit_tests/huge_negative_hea_oob.cc
+++ compiler-rt/lib/asan/lit_tests/huge_negative_hea_oob.cc
@@ -1,4 +1,5 @@
-// RUN: %clangxx_asan -m64  %s -o %t && %t 2>&1 | %symbolize > %t.out
+// RUN: %clangxx_asan -m64  %s -o %t && %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -m64 -O %s -o %t && %t 2>&1 | FileCheck %s
 // Check that we can find huge buffer overflows to the left.
 #include <stdlib.h>
 #include <string.h>
@@ -7,4 +8,6 @@ int main(int argc, char **argv) {
   memset(x, 0, 10);
   int res = x[-argc * 4000];  // BOOOM
   // CHECK: is located 4000 bytes to the left of
+  free(x);
+  return res;
 }
