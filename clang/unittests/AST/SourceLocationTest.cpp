713eef4f7ca2bb26a1a1d39713ac167a397b65b3
Tanya Lattner <tonic@nondot.org>
2013-04-05 20:14:50 +0000

    Add an error to check that all program scope variables are in the constant address space in OpenCL.
    llvm-svn: 178906

--- clang/unittests/AST/SourceLocationTest.cpp
+++ clang/unittests/AST/SourceLocationTest.cpp
@@ -132,10 +132,10 @@ TEST(CompoundLiteralExpr, CompoundVectorLiteralRange) {
 
 TEST(CompoundLiteralExpr, ParensCompoundVectorLiteralRange) {
   RangeVerifier<CompoundLiteralExpr> Verifier;
-  Verifier.expectRange(2, 11, 2, 22);
+  Verifier.expectRange(2, 20, 2, 31);
   EXPECT_TRUE(Verifier.match(
                   "typedef int int2 __attribute__((ext_vector_type(2)));\n"
-                  "int2 i2 = (int2)(1, 2);", 
+                  "constant int2 i2 = (int2)(1, 2);", 
                   compoundLiteralExpr(), Lang_OpenCL));
 }
 
@@ -149,10 +149,10 @@ TEST(InitListExpr, VectorLiteralListBraceRange) {
 
 TEST(InitListExpr, VectorLiteralInitListParens) {
   RangeVerifier<InitListExpr> Verifier;
-  Verifier.expectRange(2, 17, 2, 22);
+  Verifier.expectRange(2, 26, 2, 31);
   EXPECT_TRUE(Verifier.match(
                   "typedef int int2 __attribute__((ext_vector_type(2)));\n"
-                  "int2 i2 = (int2)(1, 2);", initListExpr(), Lang_OpenCL));
+                  "constant int2 i2 = (int2)(1, 2);", initListExpr(), Lang_OpenCL));
 }
 
 } // end namespace ast_matchers
