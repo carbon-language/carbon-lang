4f16ebfb3066138da0f501bbfbcbb5d6a57ad35f
Rafael Espindola <rafael.espindola@gmail.com>
2013-04-13 22:26:02 +0000

    Simplify test so that it is more portable.
    I have checked that the test still fails when the "|| !P.isRegularFile()" from
    the original patch is removed.
    
    llvm-svn: 179464

--- clang/test/Driver/output-file-is-dir.c
+++ clang/test/Driver/output-file-is-dir.c
@@ -1,8 +1,6 @@
-// XFAIL: hexagon
 // RUN: rm -rf %t.dir
-// RUN: mkdir -p %t.dir/a.out
-// RUN: cd %t.dir && not %clang %s
-// RUN: test -d %t.dir/a.out
-// REQUIRES: shell
+// RUN: mkdir -p %t.dir
+// RUN: not %clang %s -c -emit-llvm -o %t.dir
+// RUN: test -d %t.dir
 
 int main() { return 0; }
