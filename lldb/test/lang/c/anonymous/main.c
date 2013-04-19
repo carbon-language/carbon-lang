f6cd60ae68d4b3de043f0c0b4b7b804ad2a25ab5
Ashok Thirumurthi <ashok.thirumurthi@intel.com>
2013-04-19 20:51:34 +0000

    Improved tests for nested structs when anonymous structs are involved. - New tests can fail on OS/X and Linux, and illustrate that the compiler used to generate the DWARF can result in lldb providing clang with an external AST source that doesn't describe all required struct fields.
    - Also includes test coverage for expressions with structs that do work on Linux- Also includes a test for dereferencing a null pointer to a struct, which works on OS/X rather than complaining, and complains on Linux with an upstream error that is really a side issue.
    
    Thanks to Samuel, Andy and Daniel for their input.
    
    llvm-svn: 179884

--- lldb/test/lang/c/anonymous/main.c
+++ lldb/test/lang/c/anonymous/main.c
@@ -1,28 +1,82 @@
 #include <stdio.h>
 
-struct container {
+struct anonymous_nest {
   struct {
     struct {
       int a;
       int b;
-    };
+    }; // anonymous
     struct {
       int c;
       int d;
     } foo;
-  };
+  }; // anonymous
 };
 
-int processor (struct container *c)
+struct anonymous_child {
+  struct {
+    struct {
+      int a;
+      int b;
+    } grandchild;
+    struct {
+      int c;
+      int d;
+    } foo;
+  }; // anonymous
+};
+
+struct anonymous_grandchild {
+  struct {
+    struct {
+      int a;
+      int b;
+    }; // anonymous
+    struct {
+      int c;
+      int d;
+    } foo;
+  } child;
+};
+
+int processor_nest (struct anonymous_nest *n)
 {
-  return c->foo.d + c->b; // Set breakpoint 0 here.
+  return n->foo.d + n->b; // Set breakpoint 0 here.
 }
+
+int processor_child (struct anonymous_child *c)
+{
+  return c->foo.d + c->grandchild.b; // Set breakpoint 1 here.
+}
+
+int processor_grandchild (struct anonymous_grandchild *g)
+{
+  return g->child.foo.d + g->child.b;
+}
+
+
+
+typedef struct {
+    int dummy;
+} type_y;
+
+typedef struct {
+    type_y y;
+} type_z;
+
+
 
 int main()
 {
-  struct container c = { 0, 2, 0, 4 };
-  
-  printf("%d\n", processor(&c));
+  struct anonymous_nest n = { 0, 2, 0, 4 };
+  struct anonymous_child c = { 0, 2, 0, 4 };
+  struct anonymous_grandchild g = { 0, 2, 0, 4 };
+  type_z *pz = 0;
+  type_z z = {{2}};
+
+  printf("%d\n", processor_nest(&n));
+  printf("%d\n", processor_child(&c));
+  printf("%d\n", processor_grandchild(&g)); // Set breakpoint 2 here.
 
   return 0;
 }
