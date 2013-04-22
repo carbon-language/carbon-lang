6647fb2c60cb24a3cf624c16045dcd72f0be263a
Eric Christopher <echristo@gmail.com>
2013-04-22 13:51:44 +0000

    Remove variable store that is never read.
    llvm-svn: 180014

--- llvm/lib/Target/ARM/ARMFastISel.cpp
+++ llvm/lib/Target/ARM/ARMFastISel.cpp
@@ -2605,7 +2605,7 @@ unsigned ARMFastISel::ARMEmitIntExt(MVT SrcVT, unsigned SrcReg, MVT DestVT,
 
   unsigned Opc;
   bool isBoolZext = false;
-  const TargetRegisterClass *RC = TLI.getRegClassFor(MVT::i32);
+  const TargetRegisterClass *RC = 0;
   switch (SrcVT.SimpleTy) {
   default: return 0;
   case MVT::i16:
