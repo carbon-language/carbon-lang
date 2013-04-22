74679a93b2a4abb4f11f625340b819ced7164773
Reid Kleckner <reid@kleckner.net>
2013-04-22 19:03:55 +0000

    [Support] Fix argv string escape bug on Windows
    Summary:
    This is http://llvm.org/PR15802.  Backslashes preceding double quotes in
    arguments must be escaped.  The interesting bit is that all other
    backslashes should *not* be escaped, because the un-escaping logic is
    only triggered by the presence of a double quote character.
    
    Reviewers: Bigcheese
    
    CC: llvm-commits
    
    Differential Revision: http://llvm-reviews.chandlerc.com/D705
    
    llvm-svn: 180035

---
+++ llvm/unittests/Support/ProgramTest.cpp
@@ -0,0 +1,63 @@
+//===- unittest/Support/ProgramTest.cpp -----------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/CommandLine.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Program.h"
+#include "gtest/gtest.h"
+
+#include <stdlib.h>
+
+namespace {
+
+using namespace llvm;
+using namespace sys;
+
+static cl::opt<std::string>
+ProgramTestStringArg1("program-test-string-arg1");
+static cl::opt<std::string>
+ProgramTestStringArg2("program-test-string-arg2");
+
+TEST(ProgramTest, CreateProcessTrailingSlash) {
+  if (getenv("LLVM_PROGRAM_TEST_CHILD")) {
+    if (ProgramTestStringArg1 == "has\\\\ trailing\\" &&
+        ProgramTestStringArg2 == "has\\\\ trailing\\") {
+      exit(0);  // Success!  The arguments were passed and parsed.
+    }
+    exit(1);
+  }
+
+  // FIXME: Hardcoding argv0 here since I don't know a good cross-platform way
+  // to get it.  Maybe ParseCommandLineOptions() should save it?
+  Path my_exe = Path::GetMainExecutable("SupportTests", &ProgramTestStringArg1);
+  const char *argv[] = {
+    my_exe.c_str(),
+    "--gtest_filter=ProgramTest.CreateProcessTrailingSlashChild",
+    "-program-test-string-arg1", "has\\\\ trailing\\",
+    "-program-test-string-arg2", "has\\\\ trailing\\",
+    0
+  };
+  const char *envp[] = { "LLVM_PROGRAM_TEST_CHILD=1", 0 };
+  std::string error;
+  bool ExecutionFailed;
+  // Redirect stdout and stdin to NUL, but let stderr through.
+#ifdef LLVM_ON_WIN32
+  Path nul("NUL");
+#else
+  Path nul("/dev/null");
+#endif
+  const Path *redirects[] = { &nul, &nul, 0 };
+  int rc = Program::ExecuteAndWait(my_exe, argv, envp, redirects,
+                                   /*secondsToWait=*/10, /*memoryLimit=*/0,
+                                   &error, &ExecutionFailed);
+  EXPECT_FALSE(ExecutionFailed) << error;
+  EXPECT_EQ(0, rc);
+}
+
+} // end anonymous namespace
