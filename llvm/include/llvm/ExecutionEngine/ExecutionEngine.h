905d97c8f12d0df2073a0d371ee5be95441cab8a
Andrew Kaylor <andrew.kaylor@intel.com>
2013-04-23 21:32:32 +0000

    Fixing typo in comment.
    llvm-svn: 180147

--- llvm/include/llvm/ExecutionEngine/ExecutionEngine.h
+++ llvm/include/llvm/ExecutionEngine/ExecutionEngine.h
@@ -373,7 +373,7 @@ public:
   virtual void UnregisterJITEventListener(JITEventListener *) {}
 
   /// Sets the pre-compiled object cache.  The ownership of the ObjectCache is
-  /// not changed.  Supported by MCJIT by not JIT.
+  /// not changed.  Supported by MCJIT but not JIT.
   virtual void setObjectCache(ObjectCache *) {
     llvm_unreachable("No support for an object cache");
   }
