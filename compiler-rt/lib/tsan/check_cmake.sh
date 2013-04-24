7a8535d2f23b1dcf156e8d31b409c8cf567bebde
Dmitry Vyukov <dvyukov@google.com>
2013-04-24 11:07:25 +0000

    tsan: run tests sequentially otherwise cmake says: make[3]: make[3]: /bin/sh: ../../../../../bin/clang: Text file busy
    llvm-svn: 180183

--- compiler-rt/lib/tsan/check_cmake.sh
+++ compiler-rt/lib/tsan/check_cmake.sh
@@ -7,5 +7,6 @@ mkdir -p $ROOT/build
 cd $ROOT/build
 CC=clang CXX=clang++ cmake -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
 make -j64
-make check-sanitizer check-tsan check-asan -j64
-
+make check-sanitizer -j64
+make check-tsan -j64
+make check-asan -j64
