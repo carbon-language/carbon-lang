7e2b05c37852b16f9f18ce9b9dd2292094d85353
Adrian Prantl <aprantl@apple.com>
2013-04-30 22:17:36 +0000

    Do not generate VLAs as complex variables any more, as they are now correctly represented as breg+0 locations in the backend.
    (Paired commit with LLVM: r180815)
    
    rdar://problem/13658587
    
    llvm-svn: 180817

--- clang/test/CodeGen/debug-info-vla.c
+++ clang/test/CodeGen/debug-info-vla.c
@@ -1,9 +1,8 @@
 // RUN: %clang_cc1 -emit-llvm -g -triple x86_64-apple-darwin %s -o - | FileCheck %s
 
-// CHECK: metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"vla", metadata {{.*}}, i32 7, metadata {{.*}}, i32 0, i32 0, i64 2} ; [ DW_TAG_auto_variable ]
-
 void testVLAwithSize(int s)
 {
+// CHECK: metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"vla", metadata {{.*}}, i32 [[@LINE+1]], metadata {{.*}}, i32 0, i32 0} ; [ DW_TAG_auto_variable ] [vla] [line [[@LINE+1]]]
   int vla[s];
   int i;
   for (i = 0; i < s; i++) {
