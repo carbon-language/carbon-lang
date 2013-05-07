9589691ca777b924441c51555a76817b7d8ce6a8
Richard Sandiford <rsandifo@linux.vnet.ibm.com>
2013-05-07 12:56:31 +0000

    [SystemZ] Fix InitMCCodeGenInfo call
    createSystemZMCCodeGenInfo was not passing the optimization level to
    InitMCCodeGenInfo(), so -O0 would be ignored.  Fixes DebugInfo/namespace.ll
    after the changes in r181271.
    
    llvm-svn: 181312

--- llvm/lib/Target/SystemZ/MCTargetDesc/SystemZMCTargetDesc.cpp
+++ llvm/lib/Target/SystemZ/MCTargetDesc/SystemZMCTargetDesc.cpp
@@ -57,7 +57,7 @@ static MCSubtargetInfo *createSystemZMCSubtargetInfo(StringRef TT,
 
 static MCCodeGenInfo *createSystemZMCCodeGenInfo(StringRef TT, Reloc::Model RM,
                                                  CodeModel::Model CM,
-                                                 CodeGenOpt::Level) {
+                                                 CodeGenOpt::Level OL) {
   MCCodeGenInfo *X = new MCCodeGenInfo();
 
   // Static code is suitable for use in a dynamic executable; there is no
@@ -98,7 +98,7 @@ static MCCodeGenInfo *createSystemZMCCodeGenInfo(StringRef TT, Reloc::Model RM,
     CM = CodeModel::Small;
   else if (CM == CodeModel::JITDefault)
     CM = RM == Reloc::PIC_ ? CodeModel::Small : CodeModel::Medium;
-  X->InitMCCodeGenInfo(RM, CM);
+  X->InitMCCodeGenInfo(RM, CM, OL);
   return X;
 }
 
