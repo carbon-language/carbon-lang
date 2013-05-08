32052b62aea070ffaea723092261054d5b87ec6e
Daniel Malea <daniel.malea@intel.com>
2013-05-08 15:07:24 +0000

    Fix rdar-13338477 test-case for Linux. - Using __builtin_trap confuses the stack unwinder - __builtin_trap specific test will be added shortly
    llvm-svn: 181441

--- lldb/test/functionalities/data-formatter/rdar-13338477/main.cpp
+++ lldb/test/functionalities/data-formatter/rdar-13338477/main.cpp
@@ -7,11 +7,11 @@
 //
 //===----------------------------------------------------------------------===//
 
-void bar(int const *foo) {
-  __builtin_trap(); // Set break point at this line.
+bool bar(int const *foo) {
+  return foo != 0; // Set break point at this line.
 }
 
 int main() {
   int foo[] = {1,2,3};
-  bar(foo);
+  return bar(foo);
 }
