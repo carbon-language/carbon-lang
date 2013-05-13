c6e16af2f4cea9808ce413610648ee522e1d0a61
Richard Smith <richard-llvm@metafoo.co.uk>
2013-05-13 00:29:57 +0000

    Add missing triple to CodeGen test.
    llvm-svn: 181679

--- clang/test/CodeGenCXX/cxx1y-deduced-return-type.cpp
+++ clang/test/CodeGenCXX/cxx1y-deduced-return-type.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++1y -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -std=c++1y -triple x86_64-linux-gnu -emit-llvm %s -o - | FileCheck %s
 
 // CHECK: @x = global {{.*}} zeroinitializer
 
