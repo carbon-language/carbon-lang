c28380a094656e7a536fb4479ac8c7ac81dc6abb
Tim Northover <t.p.northover@gmail.com>
2013-05-14 08:37:13 +0000

    AArch64: add test for updated __clear_cache definition
    llvm-svn: 181765

--- clang/test/Sema/builtins-aarch64.c
+++ clang/test/Sema/builtins-aarch64.c
@@ -1,4 +1,9 @@
 // RUN: %clang_cc1 -triple aarch64-none-linux-gnu -fsyntax-only -verify %s
+// RUN: %clang_cc1 -triple aarch64-none-linux-gnu -DTEST1 -fsyntax-only -verify %s
+
+#ifdef TEST1
+void __clear_cache(char *start, char *end);
+#endif
 
 void test_clear_cache_chars(char *start, char *end) {
   __clear_cache(start, end);
