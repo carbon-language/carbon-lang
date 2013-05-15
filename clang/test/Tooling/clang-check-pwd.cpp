4fec90371753ccffcbd0f65d2918a9b0233c0049
Jordan Rose <jordan_rose@apple.com>
2013-05-15 01:45:37 +0000

    Use 'env' in tests that set environment variables.
    Patch by David Fang!
    
    llvm-svn: 181861

--- clang/test/Tooling/clang-check-pwd.cpp
+++ clang/test/Tooling/clang-check-pwd.cpp
@@ -2,7 +2,7 @@
 // RUN: mkdir %t
 // RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %t/test.cpp\",\"file\":\"%t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
 // RUN: cp "%s" "%t/test.cpp"
-// RUN: PWD="%t" clang-check -p "%t" "test.cpp" 2>&1|FileCheck %s
+// RUN: env PWD="%t" clang-check -p "%t" "test.cpp" 2>&1|FileCheck %s
 // FIXME: Make the above easier.
 
 // CHECK: C++ requires
