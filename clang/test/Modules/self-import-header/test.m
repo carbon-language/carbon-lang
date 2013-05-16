267b446324762b773e0a72f5b4a47e1c43458651
NAKAMURA Takumi <geek4civic@gmail.com>
2013-05-16 06:59:30 +0000

    clang/test/Modules/self-import-header/test.m: Add explicit -target.
    Clang has an issue between mingw/include/float.h and clang/Headers/float.h with cyclic include_next.
    For now, it should work to suppress #include_next in clang/float.h with an explicit target.
    (It may work with -U__MINGW32__, though.)
    
    llvm-svn: 181988

--- clang/test/Modules/self-import-header/test.m
+++ clang/test/Modules/self-import-header/test.m
@@ -2,6 +2,7 @@
 
 // RUN: rm -rf %t
 // RUN: %clang -fsyntax-only -isysroot %S/../Inputs/System/usr/include -fmodules -fmodules-cache-path=%t \
+// RUN:    -target x86_64-darwin \
 // RUN:    -F %S -I %S %s -D__need_wint_t -Werror=implicit-function-declaration
 
 @import af;
