709e01688ddb5f15e6eb12f8fe482d847e2997d6
Vincent Lejeune <vljn@ovi.com>
2013-05-17 16:49:49 +0000

    R600: prettier dump of clamp
    llvm-svn: 182121

--- llvm/lib/Target/R600/InstPrinter/AMDGPUInstPrinter.cpp
+++ llvm/lib/Target/R600/InstPrinter/AMDGPUInstPrinter.cpp
@@ -102,7 +102,7 @@ void AMDGPUInstPrinter::printLiteral(const MCInst *MI, unsigned OpNo,
 
 void AMDGPUInstPrinter::printLast(const MCInst *MI, unsigned OpNo,
                                   raw_ostream &O) {
-  printIfSet(MI, OpNo, O.indent(20 - O.GetNumBytesInBuffer()), "*", " ");
+  printIfSet(MI, OpNo, O.indent(25 - O.GetNumBytesInBuffer()), "*", " ");
 }
 
 void AMDGPUInstPrinter::printNeg(const MCInst *MI, unsigned OpNo,
