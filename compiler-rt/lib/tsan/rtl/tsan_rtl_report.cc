9ff10238165d339fe3b948d3f840ba5e6ec13059
Dmitry Vyukov <dvyukov@google.com>
2013-05-17 12:05:44 +0000

    tsan: remove "printing report from signal handler. Can crash or hang" output it was added to debug mysterious hangs, but it does not seem to happen anymore now it only clutter up output
    llvm-svn: 182089

--- compiler-rt/lib/tsan/rtl/tsan_rtl_report.cc
+++ compiler-rt/lib/tsan/rtl/tsan_rtl_report.cc
@@ -600,10 +600,6 @@ void ReportRace(ThreadState *thr) {
   if (!flags()->report_atomic_races && !RaceBetweenAtomicAndFree(thr))
     return;
 
-  if (thr->in_signal_handler)
-    Printf("ThreadSanitizer: printing report from signal handler."
-           " Can crash or hang.\n");
-
   bool freed = false;
   {
     Shadow s(thr->racy_state[1]);
