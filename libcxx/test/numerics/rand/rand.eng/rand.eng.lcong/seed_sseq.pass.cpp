849821cffb2fe404593450b7b067bee8704bcb4d
Howard Hinnant <hhinnant@apple.com>
2013-05-21 21:05:12 +0000

    Fix a couple of bugs in linear_congruential_engine::seed.  Regression test added.
    llvm-svn: 182421

--- libcxx/test/numerics/rand/rand.eng/rand.eng.lcong/seed_sseq.pass.cpp
+++ libcxx/test/numerics/rand/rand.eng/rand.eng.lcong/seed_sseq.pass.cpp
@@ -28,4 +28,12 @@ int main()
         e1.seed(sseq);
         assert(e1 == e2);
     }
+    {
+        unsigned a[] = {3, 5, 7, 9, 11};
+        std::seed_seq sseq(a, a+5);
+        typedef std::linear_congruential_engine<unsigned long long, 1, 1, 0x200000001ULL> E;
+        E e1(4309005589);
+        E e2(sseq);
+        assert(e1 == e2);
+    }
 }
