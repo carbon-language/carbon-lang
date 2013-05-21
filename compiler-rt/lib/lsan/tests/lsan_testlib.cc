aa0b45a0943c8a5023d042c1937d528fdb074f1f
Sergey Matveev <earthdok@google.com>
2013-05-21 12:49:33 +0000

    [lsan] LSan in LD_PRELOAD library form.
    llvm-svn: 182376

---
+++ compiler-rt/lib/lsan/tests/lsan_testlib.cc
@@ -0,0 +1,31 @@
+//===-- lsan_testlib.cc ---------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of LeakSanitizer.
+// Standalone LSan tool as a shared library, to be used with LD_PRELOAD.
+//
+//===----------------------------------------------------------------------===//
+/* Usage:
+clang++ ../sanitizer_common/sanitizer_*.cc ../interception/interception_*.cc \
+ lsan*.cc tests/lsan_testlib.cc -I. -I.. -g -ldl -lpthread -fPIC -shared -O2 \
+ -o lsan.so
+LD_PRELOAD=./lsan.so /your/app
+*/
+#include "lsan_common.h"
+#include "lsan.h"
+
+__attribute__((constructor))
+void constructor() {
+  __lsan::Init();
+}
+
+__attribute__((destructor))
+void destructor() {
+  __lsan::DoLeakCheck();
+}
