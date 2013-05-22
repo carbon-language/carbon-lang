02c9c8c9f7f75bccc8c791fc659fd090e7e3a348
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2013-05-22 13:07:23 +0000

    [sanitizer] Fix Mac build.
    llvm-svn: 182469

--- compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.h
@@ -90,7 +90,11 @@ namespace __sanitizer {
     int ai_family;
     int ai_socktype;
     int ai_protocol;
+#if SANITIZER_MAC
+    unsigned ai_addrlen;
+#else
     uptr ai_addrlen;
+#endif
     void *ai_addr;
     char *ai_canonname;
     struct __sanitizer_addrinfo *ai_next;
