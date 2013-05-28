40d7ed5d1e8ce8f9accc6e9cc90469c1276a0762
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2013-05-28 14:34:37 +0000

    [sanitizer] Improve getaddrinfo interceptor.
    llvm-svn: 182775

--- compiler-rt/lib/msan/lit_tests/getaddrinfo-positive.cc
+++ compiler-rt/lib/msan/lit_tests/getaddrinfo-positive.cc
@@ -8,10 +8,14 @@
 #include <netdb.h>
 #include <stdlib.h>
 
+volatile int z;
+
 int main(void) {
   struct addrinfo *ai;
   struct addrinfo hint;
-  int res = getaddrinfo("localhost", NULL, &hint, &ai);
+  int res = getaddrinfo("localhost", NULL, NULL, &ai);
+  if (ai) z = 1; // OK
+  res = getaddrinfo("localhost", NULL, &hint, &ai);
   // CHECK: UMR in __interceptor_getaddrinfo at offset 0 inside
   // CHECK: WARNING: MemorySanitizer: use-of-uninitialized-value
   // CHECK: #0 {{.*}} in main {{.*}}getaddrinfo-positive.cc:[[@LINE-3]]
