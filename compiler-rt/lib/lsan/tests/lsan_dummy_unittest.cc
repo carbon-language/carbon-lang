1a566ced91acd7f986b59d1bed913ade22fa18fc
Sergey Matveev <earthdok@google.com>
2013-05-31 14:15:54 +0000

    [lsan] Added a dummy unittest to suppress LIT warnings.
    llvm-svn: 183004

---
+++ compiler-rt/lib/lsan/tests/lsan_dummy_unittest.cc
@@ -0,0 +1,17 @@
+//===-- lsan_dummy_unittest.cc --------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of LeakSanitizer runtime.
+//
+//===----------------------------------------------------------------------===//
+#include "gtest/gtest.h"
+
+TEST(LeakSanitizer, EmptyTest) {
+  // Empty test to suppress LIT warnings about lack of tests.
+}
