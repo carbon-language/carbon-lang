1d1592624c73b83723f93867c459e71afc6e9aed
Daniel Malea <daniel.malea@intel.com>
2013-06-03 21:42:50 +0000

    Un-skipping test that was disabled due to llvm.org/pr16191 - adding workaround recommended by Greg (-fno-limit-debug-info clang flag) - filed bug llvm.org/pr16214 against Clang
    llvm-svn: 183156

--- lldb/test/functionalities/data-formatter/rdar-9973865/Test-rdar-9973865.py
+++ lldb/test/functionalities/data-formatter/rdar-9973865/Test-rdar-9973865.py
@@ -23,8 +23,6 @@ class Radar9973865DataFormatterTestCase(TestBase):
     @dwarf_test
     def test_with_dwarf_and_run_command(self):
         """Test data formatter commands."""
-        if "clang" in self.getCompiler() and "3.4" in self.getCompilerVersion():
-            self.skipTest("llvm.org/pr16191")
 
         self.buildDwarf()
         self.data_formatter_commands()
