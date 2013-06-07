4fdf8d66a914b9998a51e3a139f5b43b849a129e
Alexey Samsonov <samsonov@google.com>
2013-06-07 15:44:21 +0000

    [ASan] fix test case to use absolute paths
    llvm-svn: 183527

--- compiler-rt/lib/asan/lit_tests/TestCases/Darwin/unset-insert-libraries-on-exec.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/Darwin/unset-insert-libraries-on-exec.cc
@@ -3,13 +3,13 @@
 
 // RUN: %clangxx_asan %s -o %t
 // RUN: %clangxx %p/../SharedLibs/darwin-dummy-shared-lib-so.cc \
-// RUN:     -dynamiclib -o darwin-dummy-shared-lib-so.dylib
+// RUN:     -dynamiclib -o %t-darwin-dummy-shared-lib-so.dylib
 
 // Make sure DYLD_INSERT_LIBRARIES doesn't contain the runtime library before
 // execl().
 
 // RUN: %t >/dev/null 2>&1
-// RUN: DYLD_INSERT_LIBRARIES=darwin-dummy-shared-lib-so.dylib \
+// RUN: DYLD_INSERT_LIBRARIES=%t-darwin-dummy-shared-lib-so.dylib \
 // RUN:     %t 2>&1 | FileCheck %s || exit 1
 #include <unistd.h>
 int main() {
