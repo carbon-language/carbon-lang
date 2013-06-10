536a10cc2d8a64eea4761234ea696f0e7fc697db
Sean Silva <silvas@purdue.edu>
2013-06-10 23:48:38 +0000

    Fix spurious semicolons.
    Apparently these macros have semicolons inside of them already.
    
    llvm-svn: 183712

--- llvm/include/llvm/Object/ELFYAML.h
+++ llvm/include/llvm/Object/ELFYAML.h
@@ -32,10 +32,10 @@ namespace ELFYAML {
 // to the `e_machine` constants, like `EM_X86_64`.
 // In the future, these would probably be better suited by C++11 enum
 // class's with appropriate fixed underlying type.
-LLVM_YAML_STRONG_TYPEDEF(uint16_t, ELF_ET);
-LLVM_YAML_STRONG_TYPEDEF(uint32_t, ELF_EM);
-LLVM_YAML_STRONG_TYPEDEF(uint8_t, ELF_ELFCLASS);
-LLVM_YAML_STRONG_TYPEDEF(uint8_t, ELF_ELFDATA);
+LLVM_YAML_STRONG_TYPEDEF(uint16_t, ELF_ET)
+LLVM_YAML_STRONG_TYPEDEF(uint32_t, ELF_EM)
+LLVM_YAML_STRONG_TYPEDEF(uint8_t, ELF_ELFCLASS)
+LLVM_YAML_STRONG_TYPEDEF(uint8_t, ELF_ELFDATA)
 
 // For now, hardcode 64 bits everywhere that 32 or 64 would be needed
 // since 64-bit can hold 32-bit values too.
