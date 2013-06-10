152f7edf24e8a4b68212876c1a595b0d6647ee92
Alexander Potapenko <glider@google.com>
2013-06-10 14:47:43 +0000

    [ASan] Fix unset-insert-libraries-on-exec.cc on 32-bit Darwin. The test used to execute a 64-bit /bin/bash binary and preload a 32-bit dynamic library into it. Now the arch-specific version of echo-env is executed instead.
    llvm-svn: 183662

--- compiler-rt/lib/asan/lit_tests/TestCases/Darwin/unset-insert-libraries-on-exec.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/Darwin/unset-insert-libraries-on-exec.cc
@@ -2,19 +2,19 @@
 // executing other programs.
 
 // RUN: %clangxx_asan %s -o %t
+// RUN: %clangxx %p/../Helpers/echo-env.cc -o %T/echo-env
 // RUN: %clangxx %p/../SharedLibs/darwin-dummy-shared-lib-so.cc \
 // RUN:     -dynamiclib -o %t-darwin-dummy-shared-lib-so.dylib
 
 // Make sure DYLD_INSERT_LIBRARIES doesn't contain the runtime library before
 // execl().
 
-// RUN: %t >/dev/null 2>&1
+// RUN: %t %T/echo-env >/dev/null 2>&1
 // RUN: DYLD_INSERT_LIBRARIES=%t-darwin-dummy-shared-lib-so.dylib \
-// RUN:     %t 2>&1 | FileCheck %s || exit 1
+// RUN:     %t %T/echo-env 2>&1 | FileCheck %s || exit 1
 #include <unistd.h>
-int main() {
-  execl("/bin/bash", "/bin/bash", "-c",
-        "echo DYLD_INSERT_LIBRARIES=$DYLD_INSERT_LIBRARIES", NULL);
-  // CHECK:  {{DYLD_INSERT_LIBRARIES=.*darwin-dummy-shared-lib-so.dylib.*}}
+int main(int argc, char *argv[]) {
+  execl(argv[1], argv[1], "DYLD_INSERT_LIBRARIES", NULL);
+  // CHECK:  {{DYLD_INSERT_LIBRARIES = .*darwin-dummy-shared-lib-so.dylib.*}}
   return 0;
 }
