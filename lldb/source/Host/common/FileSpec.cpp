ff89ff297f2533572981e35352fc8e58abb352dc
Rafael Espindola <rafael.espindola@gmail.com>
2013-06-13 19:25:41 +0000

    Update for llvm change.
    llvm-svn: 183929

--- lldb/source/Host/common/FileSpec.cpp
+++ lldb/source/Host/common/FileSpec.cpp
@@ -552,9 +552,8 @@ FileSpec::ResolveExecutableLocation ()
         if (file_cstr)
         {
             const std::string file_str (file_cstr);
-            llvm::sys::Path path = llvm::sys::FindProgramByName (file_str);
-            const std::string &path_str = path.str();
-            llvm::StringRef dir_ref = llvm::sys::path::parent_path(path_str);
+            std::string path = llvm::sys::FindProgramByName (file_str);
+            llvm::StringRef dir_ref = llvm::sys::path::parent_path(path);
             //llvm::StringRef dir_ref = path.getDirname();
             if (! dir_ref.empty())
             {
