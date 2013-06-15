5580e5ca80d26ef11c020c3431bfb9e77e10e8fc
Andrew Trick <atrick@apple.com>
2013-06-15 04:49:42 +0000

    MI-Sched: DEBUG: print critical resource.
    llvm-svn: 184028

--- llvm/lib/CodeGen/MachineScheduler.cpp
+++ llvm/lib/CodeGen/MachineScheduler.cpp
@@ -1421,6 +1421,10 @@ init(ScheduleDAGMI *DAG, const TargetSchedModel *SchedModel) {
       CritResIdx = PIdx;
     }
   }
+  DEBUG(dbgs() << "Critical Resource: "
+        << SchedModel->getProcResource(CritResIdx)->Name
+        << ": " << RemainingCounts[CritResIdx]
+        << " / " << SchedModel->getLatencyFactor() << '\n');
 }
 
 void ConvergingScheduler::SchedBoundary::
