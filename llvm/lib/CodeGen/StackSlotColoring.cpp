302ba2314a752943c1484f359e2f7a9485ecdba2
Timur Iskhodzhanov <timurrrr@google.com>
2013-06-18 07:57:22 +0000

    Hopefully fix the MSVS build after r184105
    llvm-svn: 184178

--- llvm/lib/CodeGen/StackSlotColoring.cpp
+++ llvm/lib/CodeGen/StackSlotColoring.cpp
@@ -28,6 +28,7 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetInstrInfo.h"
 #include "llvm/Target/TargetMachine.h"
 #include <vector>
