8f99e9413f49f89d1c29e1c083068b9aedbbee4e
Stephen Lin <stephenwlin@gmail.com>
2013-06-18 20:51:51 +0000

    Partially revert r184225 test changes and make a smaller and more generic fix.
    llvm-svn: 184232

--- clang/test/CodeGenCXX/default-constructor-template-member.cpp
+++ clang/test/CodeGenCXX/default-constructor-template-member.cpp
@@ -1,5 +1,4 @@
-// RUN: %clang_cc1 %s -emit-llvm -o - -triple=i686-unknown-linux | FileCheck --check-prefix=CHECKX86 %s
-// RUN: %clang_cc1 %s -emit-llvm -o - -triple=arm-linux-gnueabihf | FileCheck --check-prefix=CHECKARM %s
+// RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck %s
 
 template <class T> struct A { A(); };
 struct B { A<int> x; };
@@ -7,10 +6,6 @@ void a() {
   B b;
 }
 
-// CHECKX86: call {{.*}} @_ZN1BC1Ev
-// CHECKX86: define linkonce_odr {{.*}} @_ZN1BC1Ev(%struct.B* %this) unnamed_addr
-// CHECKX86: call {{.*}} @_ZN1AIiEC1Ev
-
-// CHECKARM: call {{.*}} @_ZN1BC1Ev
-// CHECKARM: define linkonce_odr {{.*}} @_ZN1BC1Ev(%struct.B* returned %this) unnamed_addr
-// CHECKARM: call {{.*}} @_ZN1AIiEC1Ev
+// CHECK: call {{.*}} @_ZN1BC1Ev
+// CHECK: define linkonce_odr {{.*}} @_ZN1BC1Ev(%struct.B* {{.*}}%this) unnamed_addr
+// CHECK: call {{.*}} @_ZN1AIiEC1Ev
