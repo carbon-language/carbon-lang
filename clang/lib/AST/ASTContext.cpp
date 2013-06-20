95296d4a16424698af61f87002dae3692902611d
Nico Weber <nicolasweber@gmx.de>
2013-06-20 23:30:30 +0000

    Fix a crash with __flaot128 noticed by Eli.
    llvm-svn: 184498

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -859,12 +859,12 @@ TypedefDecl *ASTContext::getUInt128Decl() const {
 
 TypeDecl *ASTContext::getFloat128StubType() const {
   if (!Float128StubDecl) {
-    Float128StubDecl = RecordDecl::Create(const_cast<ASTContext &>(*this), 
-                                          TTK_Struct,
-                                          getTranslationUnitDecl(),
-                                          SourceLocation(),
-                                          SourceLocation(),
-                                          &Idents.get("__float128"));
+    Float128StubDecl = CXXRecordDecl::Create(const_cast<ASTContext &>(*this), 
+                                             TTK_Struct,
+                                             getTranslationUnitDecl(),
+                                             SourceLocation(),
+                                             SourceLocation(),
+                                             &Idents.get("__float128"));
   }
   
   return Float128StubDecl;
--- clang/test/Sema/128bitfloat.cc
+++ clang/test/Sema/128bitfloat.cc
@@ -2,12 +2,23 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -std=c++11 %s
 
 #if !defined(__STRICT_ANSI__)
-__float128 f;  // expected-error{{support for type '__float128' is not yet implemented}}
+__float128 f;  // expected-error {{support for type '__float128' is not yet implemented}}
 // But this should work:
 template<typename> struct __is_floating_point_helper {};
 template<> struct __is_floating_point_helper<__float128> {};
+
+// FIXME: This could have a better diag.
+void g(int x, __float128 *y) {
+  x + *y;  // expected-error {{invalid operands to binary expression ('int' and '__float128')}}
+}
+
 #else
 __float128 f;  // expected-error {{unknown type name '__float128'}}
 template<typename> struct __is_floating_point_helper {};
 template<> struct __is_floating_point_helper<__float128> {};  // expected-error {{use of undeclared identifier '__float128'}}
+
+void g(int x, __float128 *y) {  // expected-error {{unknown type name '__float128'}}
+  x + *y;
+}
+
 #endif
