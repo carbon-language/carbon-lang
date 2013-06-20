865a1efc137320c235290ce0bce57e0d87ace715
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2013-06-20 16:15:12 +0000

    [PowerPC] Support compare mnemonics with implied CR0
    Just like for branch mnemonics (where support was recently added), the
    assembler is supposed to support extended mnemonics for the compare
    instructions where no condition register is specified explicitly
    (and CR0 is assumed implicitly).
    
    This patch adds support for those extended compare mnemonics.
    
    
    Index: llvm-head/test/MC/PowerPC/ppc64-encoding-ext.s
    ===================================================================
    --- llvm-head.orig/test/MC/PowerPC/ppc64-encoding-ext.s
    +++ llvm-head/test/MC/PowerPC/ppc64-encoding-ext.s
    @@ -449,21 +449,37 @@
     
     # CHECK: cmpdi 2, 3, 128                 # encoding: [0x2d,0x23,0x00,0x80]
              cmpdi 2, 3, 128
    +# CHECK: cmpdi 0, 3, 128                 # encoding: [0x2c,0x23,0x00,0x80]
    +         cmpdi 3, 128
     # CHECK: cmpd 2, 3, 4                    # encoding: [0x7d,0x23,0x20,0x00]
              cmpd 2, 3, 4
    +# CHECK: cmpd 0, 3, 4                    # encoding: [0x7c,0x23,0x20,0x00]
    +         cmpd 3, 4
     # CHECK: cmpldi 2, 3, 128                # encoding: [0x29,0x23,0x00,0x80]
              cmpldi 2, 3, 128
    +# CHECK: cmpldi 0, 3, 128                # encoding: [0x28,0x23,0x00,0x80]
    +         cmpldi 3, 128
     # CHECK: cmpld 2, 3, 4                   # encoding: [0x7d,0x23,0x20,0x40]
              cmpld 2, 3, 4
    +# CHECK: cmpld 0, 3, 4                   # encoding: [0x7c,0x23,0x20,0x40]
    +         cmpld 3, 4
     
     # CHECK: cmpwi 2, 3, 128                 # encoding: [0x2d,0x03,0x00,0x80]
              cmpwi 2, 3, 128
    +# CHECK: cmpwi 0, 3, 128                 # encoding: [0x2c,0x03,0x00,0x80]
    +         cmpwi 3, 128
     # CHECK: cmpw 2, 3, 4                    # encoding: [0x7d,0x03,0x20,0x00]
              cmpw 2, 3, 4
    +# CHECK: cmpw 0, 3, 4                    # encoding: [0x7c,0x03,0x20,0x00]
    +         cmpw 3, 4
     # CHECK: cmplwi 2, 3, 128                # encoding: [0x29,0x03,0x00,0x80]
              cmplwi 2, 3, 128
    +# CHECK: cmplwi 0, 3, 128                # encoding: [0x28,0x03,0x00,0x80]
    +         cmplwi 3, 128
     # CHECK: cmplw 2, 3, 4                   # encoding: [0x7d,0x03,0x20,0x40]
              cmplw 2, 3, 4
    +# CHECK: cmplw 0, 3, 4                   # encoding: [0x7c,0x03,0x20,0x40]
    +         cmplw 3, 4
     
     # FIXME: Trap mnemonics
     
    Index: llvm-head/lib/Target/PowerPC/PPCInstrInfo.td
    ===================================================================
    --- llvm-head.orig/lib/Target/PowerPC/PPCInstrInfo.td
    +++ llvm-head/lib/Target/PowerPC/PPCInstrInfo.td
    @@ -2201,3 +2201,12 @@ defm : BranchExtendedMnemonic<"ne", 68>;
     defm : BranchExtendedMnemonic<"nu", 100>;
     defm : BranchExtendedMnemonic<"ns", 100>;
     
    +def : InstAlias<"cmpwi $rA, $imm", (CMPWI CR0, gprc:$rA, s16imm:$imm)>;
    +def : InstAlias<"cmpw $rA, $rB", (CMPW CR0, gprc:$rA, gprc:$rB)>;
    +def : InstAlias<"cmplwi $rA, $imm", (CMPLWI CR0, gprc:$rA, u16imm:$imm)>;
    +def : InstAlias<"cmplw $rA, $rB", (CMPLW CR0, gprc:$rA, gprc:$rB)>;
    +def : InstAlias<"cmpdi $rA, $imm", (CMPDI CR0, g8rc:$rA, s16imm:$imm)>;
    +def : InstAlias<"cmpd $rA, $rB", (CMPD CR0, g8rc:$rA, g8rc:$rB)>;
    +def : InstAlias<"cmpldi $rA, $imm", (CMPLDI CR0, g8rc:$rA, u16imm:$imm)>;
    +def : InstAlias<"cmpld $rA, $rB", (CMPLD CR0, g8rc:$rA, g8rc:$rB)>;
    +
    
    llvm-svn: 184435

--- llvm/test/MC/PowerPC/ppc64-encoding-ext.s
+++ llvm/test/MC/PowerPC/ppc64-encoding-ext.s
@@ -449,21 +449,37 @@
 
 # CHECK: cmpdi 2, 3, 128                 # encoding: [0x2d,0x23,0x00,0x80]
          cmpdi 2, 3, 128
+# CHECK: cmpdi 0, 3, 128                 # encoding: [0x2c,0x23,0x00,0x80]
+         cmpdi 3, 128
 # CHECK: cmpd 2, 3, 4                    # encoding: [0x7d,0x23,0x20,0x00]
          cmpd 2, 3, 4
+# CHECK: cmpd 0, 3, 4                    # encoding: [0x7c,0x23,0x20,0x00]
+         cmpd 3, 4
 # CHECK: cmpldi 2, 3, 128                # encoding: [0x29,0x23,0x00,0x80]
          cmpldi 2, 3, 128
+# CHECK: cmpldi 0, 3, 128                # encoding: [0x28,0x23,0x00,0x80]
+         cmpldi 3, 128
 # CHECK: cmpld 2, 3, 4                   # encoding: [0x7d,0x23,0x20,0x40]
          cmpld 2, 3, 4
+# CHECK: cmpld 0, 3, 4                   # encoding: [0x7c,0x23,0x20,0x40]
+         cmpld 3, 4
 
 # CHECK: cmpwi 2, 3, 128                 # encoding: [0x2d,0x03,0x00,0x80]
          cmpwi 2, 3, 128
+# CHECK: cmpwi 0, 3, 128                 # encoding: [0x2c,0x03,0x00,0x80]
+         cmpwi 3, 128
 # CHECK: cmpw 2, 3, 4                    # encoding: [0x7d,0x03,0x20,0x00]
          cmpw 2, 3, 4
+# CHECK: cmpw 0, 3, 4                    # encoding: [0x7c,0x03,0x20,0x00]
+         cmpw 3, 4
 # CHECK: cmplwi 2, 3, 128                # encoding: [0x29,0x03,0x00,0x80]
          cmplwi 2, 3, 128
+# CHECK: cmplwi 0, 3, 128                # encoding: [0x28,0x03,0x00,0x80]
+         cmplwi 3, 128
 # CHECK: cmplw 2, 3, 4                   # encoding: [0x7d,0x03,0x20,0x40]
          cmplw 2, 3, 4
+# CHECK: cmplw 0, 3, 4                   # encoding: [0x7c,0x03,0x20,0x40]
+         cmplw 3, 4
 
 # FIXME: Trap mnemonics
 
