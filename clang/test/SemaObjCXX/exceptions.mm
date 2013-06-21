319f41382aecc1a81b720f5aa40b16a6bd1124a6
Eli Friedman <eli.friedman@gmail.com>
2013-06-21 01:28:03 +0000

    Add missing test.
    llvm-svn: 184515

---
+++ clang/test/SemaObjCXX/exceptions.mm
@@ -0,0 +1,11 @@
+// RUN: %clang_cc1 -fcxx-exceptions -fexceptions  -triple x86_64-apple-darwin11 -fsyntax-only -verify %s 
+
+@interface NSException @end
+
+namespace test0 {
+  void test() {
+    try {
+    } catch (NSException e) { // expected-error {{can't catch an Objective-C object by value}}
+    }
+  }
+}
