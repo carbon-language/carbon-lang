5001d43dcb90af52f7fbe96c8a37237f7860679f
Dmitry Vyukov <dvyukov@google.com>
2013-06-24 16:28:02 +0000

    tsan: make the test more robust currently it episodically fails the hypothesis it is due to racy race detection algorithm the sleep should make it more robust
    llvm-svn: 184752

--- compiler-rt/lib/tsan/lit_tests/tiny_race.c
+++ compiler-rt/lib/tsan/lit_tests/tiny_race.c
@@ -1,15 +1,21 @@
 // RUN: %clang_tsan -O1 %s -o %t && %t 2>&1 | FileCheck %s
 #include <pthread.h>
+#include <unistd.h>
+
 int Global;
+
 void *Thread1(void *x) {
+  sleep(1);
   Global = 42;
   return x;
 }
+
 int main() {
   pthread_t t;
-  pthread_create(&t, NULL, Thread1, NULL);
+  pthread_create(&t, 0, Thread1, 0);
   Global = 43;
-  pthread_join(t, NULL);
+  pthread_join(t, 0);
   return Global;
 }
+
 // CHECK: WARNING: ThreadSanitizer: data race
