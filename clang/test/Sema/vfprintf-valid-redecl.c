71bdbb01010e25db6fa47c813213d0b38208f4c9
Richard Smith <richard-llvm@metafoo.co.uk>
2013-06-25 22:37:05 +0000

    Make this test properly test both things it's trying to test.
    llvm-svn: 184887

--- clang/test/Sema/vfprintf-valid-redecl.c
+++ clang/test/Sema/vfprintf-valid-redecl.c
@@ -1,11 +1,14 @@
 // RUN: %clang_cc1 %s -fsyntax-only -pedantic -verify
+// RUN: %clang_cc1 %s -fsyntax-only -pedantic -verify -DPREDECLARE
 // expected-no-diagnostics
 
+#ifdef PREDECLARE
 // PR16344
 // Clang has defined 'vfprint' in builtin list. If the following line occurs before any other
 // `vfprintf' in this file, and we getPreviousDecl()->getTypeSourceInfo() on it, then we will
 // get a null pointer since the one in builtin list doesn't has valid TypeSourceInfo.
 int vfprintf(void) { return 0; }
+#endif
 
 // PR4290
 // The following declaration is compatible with vfprintf, so we shouldn't
