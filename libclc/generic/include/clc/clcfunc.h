d84c7f5d0f65a16b714271de20b61dea582b8d82
Tom Stellard <thomas.stellard@amd.com>
2013-06-26 18:20:18 +0000

    Remove the static keyword from the _CLC_INLINE macro
    static functions are not allowed in OpenCL C
    
    llvm-svn: 184986

--- libclc/generic/include/clc/clcfunc.h
+++ libclc/generic/include/clc/clcfunc.h
@@ -1,4 +1,4 @@
 #define _CLC_OVERLOAD __attribute__((overloadable))
 #define _CLC_DECL
 #define _CLC_DEF __attribute__((always_inline))
-#define _CLC_INLINE __attribute__((always_inline)) static inline
+#define _CLC_INLINE __attribute__((always_inline)) inline
