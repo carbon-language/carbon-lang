f5dede1bebbf43ca0d9dbe0224252de33cc8a886
Andy Gibbs <andyg1001@hotmail.co.uk>
2013-06-26 07:57:53 +0000

    Support using "host" as a target in LLVM_TARGETS_TO_BUILD when using cmake.
    llvm-svn: 184923

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -384,6 +384,14 @@ else ()
   message(FATAL_ERROR "Unknown architecture ${LLVM_NATIVE_ARCH}")
 endif ()
 
+# If build targets includes "host", then replace with native architecture.
+list(FIND LLVM_TARGETS_TO_BUILD "host" idx)
+if( NOT idx LESS 0 )
+  list(REMOVE_AT LLVM_TARGETS_TO_BUILD ${idx})
+  list(APPEND LLVM_TARGETS_TO_BUILD ${LLVM_NATIVE_ARCH})
+  list(REMOVE_DUPLICATES LLVM_TARGETS_TO_BUILD)
+endif()
+
 list(FIND LLVM_TARGETS_TO_BUILD ${LLVM_NATIVE_ARCH} NATIVE_ARCH_IDX)
 if (NATIVE_ARCH_IDX EQUAL -1)
   message(STATUS
