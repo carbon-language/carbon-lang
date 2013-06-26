c8889798792d243440b618021339f0c8d27102fc
Rafael Espindola <rafael.espindola@gmail.com>
2013-06-26 12:46:06 +0000

    Remove unused header.
    llvm-svn: 184937

--- clang/tools/driver/driver.cpp
+++ clang/tools/driver/driver.cpp
@@ -34,7 +34,6 @@
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
-#include "llvm/Support/PathV1.h"
 #include "llvm/Support/PrettyStackTrace.h"
 #include "llvm/Support/Program.h"
 #include "llvm/Support/Regex.h"
