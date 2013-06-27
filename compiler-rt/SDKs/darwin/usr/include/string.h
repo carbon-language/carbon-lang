4ddc302879e680d74744274fb06ee16c95d8fb68
Bill Wendling <isanbard@gmail.com>
2013-06-27 21:17:24 +0000

    Declare 'strerror' so that we can use it with errno.
    llvm-svn: 185105

--- compiler-rt/SDKs/darwin/usr/include/string.h
+++ compiler-rt/SDKs/darwin/usr/include/string.h
@@ -28,4 +28,25 @@ char *strdup(const char *);
 size_t strlen(const char *);
 char *strncpy(char *, const char *, size_t);
 
+/* Determine the appropriate strerror() function. */
+#if defined(__ENVIRONMENT_MAC_OS_X_VERSION_MIN_REQUIRED__)
+#  if defined(__i386)
+#    define __STRERROR_NAME  "_strerror$UNIX2003"
+#  elif defined(__x86_64__) || defined(__arm)
+#    define __STRERROR_NAME  "_strerror"
+#  else
+#    error "unrecognized architecture for targetting OS X"
+#  endif
+#elif defined(__ENVIRONMENT_IPHONE_OS_VERSION_MIN_REQUIRED__)
+#  if defined(__i386) || defined (__x86_64) || defined(__arm)
+#    define __STRERROR_NAME  "_strerror"
+#  else
+#    error "unrecognized architecture for targetting iOS"
+#  endif
+#else
+#  error "unrecognized architecture for targetting Darwin"
+#endif
+
+char *strerror(int) __asm(__STRERROR_NAME);
+
 #endif /* __STRING_H__ */
