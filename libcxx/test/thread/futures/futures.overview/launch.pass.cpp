183646254547720c947b804585db8ceaa13e6c97
Howard Hinnant <hhinnant@apple.com>
2013-06-29 18:38:17 +0000

    Add operators to make launch a bitmask type.  Searched all of the standard, and libc++ to see if this error occurred elsewhere and didn't see any other place.  This fixes http://llvm.org/bugs/show_bug.cgi?id=16207
    llvm-svn: 185265

--- libcxx/test/thread/futures/futures.overview/launch.pass.cpp
+++ libcxx/test/thread/futures/futures.overview/launch.pass.cpp
@@ -17,11 +17,27 @@
 // };
 
 #include <future>
+#include <cassert>
 
 int main()
 {
+#ifdef _LIBCPP_HAS_NO_STRONG_ENUMS
     static_assert(static_cast<int>(std::launch::any) ==
                  (static_cast<int>(std::launch::async) | static_cast<int>(std::launch::deferred)), "");
+#else
+    static_assert(std::launch::any == (std::launch::async | std::launch::deferred), "");
+    static_assert(std::launch(0) == (std::launch::async & std::launch::deferred), "");
+    static_assert(std::launch::any == (std::launch::async ^ std::launch::deferred), "");
+    static_assert(std::launch(~1) == ~std::launch::async, "");
+    std::launch x = std::launch::async;
+    x &= std::launch::deferred;
+    assert(x == std::launch(0));
+    x = std::launch::async;
+    x |= std::launch::deferred;
+    assert(x == std::launch::any);
+    x ^= std::launch::deferred;
+    assert(x == std::launch::async);
+#endif
     static_assert(static_cast<int>(std::launch::async) == 1, "");
     static_assert(static_cast<int>(std::launch::deferred) == 2, "");
 }
