58ae47bf10bb603a912efdef9d336c5d8039e4d5
Bill Schmidt <wschmidt@linux.vnet.ibm.com>
2013-07-03 21:03:06 +0000

    [PowerPC] FreeBSD does not require f128 in its data layout string.
    Long double is 64 bits on FreeBSD PPC, so the f128 entry is superfluous.
    
    llvm-svn: 185582

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -1180,7 +1180,7 @@ public:
       LongDoubleWidth = LongDoubleAlign = 64;
       LongDoubleFormat = &llvm::APFloat::IEEEdouble;
       DescriptionString = "E-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
-                          "i64:64:64-f32:32:32-f64:64:64-f128:64:64-"
+                          "i64:64:64-f32:32:32-f64:64:64-"
                           "v128:128:128-n32:64";
     } else
       DescriptionString = "E-p:64:64:64-i1:8:8-i8:8:8-i16:16:16-i32:32:32-"
