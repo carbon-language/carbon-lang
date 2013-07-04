80a99478bd0e66a6b758c752ba8dfeaf0f218083
Rafael Espindola <rafael.espindola@gmail.com>
2013-07-04 15:08:20 +0000

    Convert test to FileCheck.
    llvm-svn: 185645

--- clang/test/Preprocessor/x86_target_features.c
+++ clang/test/Preprocessor/x86_target_features.c
@@ -1,32 +1,32 @@
-// RUN: %clang -target i386-unknown-unknown -march=core2 -msse4 -x c -E -dM -o %t %s
-// RUN: grep '#define __SSE2_MATH__ 1' %t
-// RUN: grep '#define __SSE2__ 1' %t
-// RUN: grep '#define __SSE3__ 1' %t
-// RUN: grep '#define __SSE4_1__ 1' %t
-// RUN: grep '#define __SSE4_2__ 1' %t
-// RUN: grep '#define __SSE_MATH__ 1' %t
-// RUN: grep '#define __SSE__ 1' %t
-// RUN: grep '#define __SSSE3__ 1' %t
+// RUN: %clang -target i386-unknown-unknown -march=core2 -msse4 -x c -E -dM -o - %s | FileCheck --check-prefix=SSE4 %s
 
-// RUN: %clang -target i386-unknown-unknown -march=core2 -msse4 -mno-sse2 -x c -E -dM -o %t %s
-// RUN: grep '#define __SSE2_MATH__ 1' %t | count 0
-// RUN: grep '#define __SSE2__ 1' %t | count 0
-// RUN: grep '#define __SSE3__ 1' %t | count 0
-// RUN: grep '#define __SSE4_1__ 1' %t | count 0
-// RUN: grep '#define __SSE4_2__ 1' %t | count 0
-// RUN: grep '#define __SSE_MATH__ 1' %t
-// RUN: grep '#define __SSE__ 1' %t
-// RUN: grep '#define __SSSE3__ 1' %t | count 0
+// SSE4: #define __SSE2_MATH__ 1
+// SSE4: #define __SSE2__ 1
+// SSE4: #define __SSE3__ 1
+// SSE4: #define __SSE4_1__ 1
+// SSE4: #define __SSE4_2__ 1
+// SSE4: #define __SSE_MATH__ 1
+// SSE4: #define __SSE__ 1
+// SSE4: #define __SSSE3__ 1
 
-// RUN: %clang -target i386-unknown-unknown -march=pentium-m -x c -E -dM -o %t %s
-// RUN: grep '#define __SSE2_MATH__ 1' %t
-// RUN: grep '#define __SSE2__ 1' %t
-// RUN: grep '#define __SSE3__ 1' %t | count 0
-// RUN: grep '#define __SSE4_1__ 1' %t | count 0
-// RUN: grep '#define __SSE4_2__ 1' %t | count 0
-// RUN: grep '#define __SSE_MATH__ 1' %t
-// RUN: grep '#define __SSE__ 1' %t
-// RUN: grep '#define __SSSE3__ 1' %t | count 0
+// RUN: %clang -target i386-unknown-unknown -march=core2 -msse4 -mno-sse2 -x c -E -dM -o - %s | FileCheck --check-prefix=SSE %s
 
+// SSE-NOT: #define __SSE2_MATH__ 1
+// SSE-NOT: #define __SSE2__ 1
+// SSE-NOT: #define __SSE3__ 1
+// SSE-NOT: #define __SSE4_1__ 1
+// SSE-NOT: #define __SSE4_2__ 1
+// SSE: #define __SSE_MATH__ 1
+// SSE: #define __SSE__ 1
+// SSE-NOT: #define __SSSE3__ 1
 
+// RUN: %clang -target i386-unknown-unknown -march=pentium-m -x c -E -dM -o - %s | FileCheck --check-prefix=SSE2 %s
 
+// SSE2: #define __SSE2_MATH__ 1
+// SSE2: #define __SSE2__ 1
+// SSE2-NOT: #define __SSE3__ 1
+// SSE2-NOT: #define __SSE4_1__ 1
+// SSE2-NOT: #define __SSE4_2__ 1
+// SSE2: #define __SSE_MATH__ 1
+// SSE2: #define __SSE__ 1
+// SSE2-NOT: #define __SSSE3__ 1
