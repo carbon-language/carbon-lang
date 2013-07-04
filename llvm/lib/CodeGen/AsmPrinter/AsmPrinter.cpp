316c37407ebff91d29a997bf9fa795cbf71e850d
Nico Rieck <nico.rieck@gmail.com>
2013-07-04 21:37:26 +0000

    Initialize object file info before output streamer
    r179494 switched to using the object file info to retrieve the default text
    section for some MC streamers. It is possible that initializing an MC
    streamer can request sections before the object file info is initialized
    when the AutoInitSections flag is set on the streamer.
    
    llvm-svn: 185670

--- llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AsmPrinter.cpp
@@ -155,14 +155,14 @@ void AsmPrinter::getAnalysisUsage(AnalysisUsage &AU) const {
 }
 
 bool AsmPrinter::doInitialization(Module &M) {
-  OutStreamer.InitStreamer();
-
   MMI = getAnalysisIfAvailable<MachineModuleInfo>();
   MMI->AnalyzeModule(M);
 
   // Initialize TargetLoweringObjectFile.
   const_cast<TargetLoweringObjectFile&>(getObjFileLowering())
     .Initialize(OutContext, TM);
+
+  OutStreamer.InitStreamer();
 
   Mang = new Mangler(OutContext, &TM);
 
