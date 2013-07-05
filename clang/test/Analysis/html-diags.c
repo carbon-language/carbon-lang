f5d46005b654ddb8a7f4de465c364df33ba76be1
Benjamin Kramer <benny.kra@googlemail.com>
2013-07-05 15:51:00 +0000

    Add a test case for r185707/PR16547.
    llvm-svn: 185708

--- clang/test/Analysis/html-diags.c
+++ clang/test/Analysis/html-diags.c
@@ -1,6 +1,14 @@
 // RUN: rm -fR %T/dir
 // RUN: mkdir %T/dir
 // RUN: %clang_cc1 -analyze -analyzer-output=html -analyzer-checker=core -o %T/dir %s
+// RUN: ls %T/dir | grep report
+
+// PR16547: Test relative paths
+// RUN: cd %T/dir
+// RUN: %clang_cc1 -analyze -analyzer-output=html -analyzer-checker=core -o testrelative %s
+// RUN: ls %T/dir/testrelative | grep report
+
+// REQUIRES: shell
 
 // Currently this test mainly checks that the HTML diagnostics doesn't crash
 // when handling macros will calls with macros.  We should actually validate
