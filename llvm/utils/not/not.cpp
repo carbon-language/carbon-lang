91487584c9bf7cdfe8560f7a8e118a6abd1fd9e8
Rafael Espindola <rafael.espindola@gmail.com>
2013-07-05 02:50:03 +0000

    Add a --crash option to not.
    Now the two possible uses of not are
    * not cmd
      Will return true if cmd doesn't crash and returns false.
    * not --crash cmd
      Will return true if cmd crashes.
    
    It will be used/tested in a followup commit for the clang crash recovery
    testing.
    
    llvm-svn: 185678

--- llvm/utils/not/not.cpp
+++ llvm/utils/not/not.cpp
@@ -13,14 +13,33 @@
 using namespace llvm;
 
 int main(int argc, const char **argv) {
-  std::string Program = sys::FindProgramByName(argv[1]);
+  bool ExpectCrash = false;
+
+  ++argv;
+  --argc;
+
+  if (argc > 0 && StringRef(argv[0]) == "--crash") {
+    ++argv;
+    --argc;
+    ExpectCrash = true;
+  }
+
+  if (argc == 0)
+    return 1;
+
+  std::string Program = sys::FindProgramByName(argv[0]);
 
   std::string ErrMsg;
-  int Result = sys::ExecuteAndWait(Program, argv + 1, 0, 0, 0, 0, &ErrMsg);
+  int Result = sys::ExecuteAndWait(Program, argv, 0, 0, 0, 0, &ErrMsg);
   if (Result < 0) {
     errs() << "Error: " << ErrMsg << "\n";
+    if (ExpectCrash)
+      return 0;
     return 1;
   }
+
+  if (ExpectCrash)
+    return 1;
 
   return Result == 0;
 }
