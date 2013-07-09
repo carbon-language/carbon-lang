7160fb65113b20a31a410e376797a5c99cff5167
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2013-07-09 12:34:25 +0000

    [sanitizer] Update glob64 interceptor to handle GLOB_ALTDIRFUNC as well.
    llvm-svn: 185935

--- compiler-rt/lib/msan/lit_tests/Linux/glob.cc
+++ compiler-rt/lib/msan/lit_tests/Linux/glob.cc
@@ -1,4 +1,5 @@
 // RUN: %clangxx_msan -m64 -O0 %s -o %t && %t %p 2>&1 | FileCheck %s
+// RUN: %clangxx_msan -m64 -O0 -D_FILE_OFFSET_BITS=64 %s -o %t && %t %p 2>&1 | FileCheck %s
 // RUN: %clangxx_msan -m64 -O3 %s -o %t && %t %p 2>&1 | FileCheck %s
 
 #include <assert.h>
