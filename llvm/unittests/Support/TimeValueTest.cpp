be2978b2e4326b60351aa6bc5a6cfbe679d9ce26
NAKAMURA Takumi <geek4civic@gmail.com>
2013-07-16 02:44:23 +0000

    unittests/Support: Add TimeValue.Win32FILETIME, corresponding to r186374.
    llvm-svn: 186375

--- llvm/unittests/Support/TimeValueTest.cpp
+++ llvm/unittests/Support/TimeValueTest.cpp
@@ -20,4 +20,20 @@ TEST(TimeValue, time_t) {
   EXPECT_TRUE(abs(static_cast<long>(now_t - now.toEpochTime())) < 2);
 }
 
+TEST(TimeValue, Win32FILETIME) {
+  uint64_t epoch_as_filetime = 0x19DB1DED53E8000ULL;
+  uint32_t ns = 765432100;
+  sys::TimeValue epoch;
+
+  // FILETIME has 100ns of intervals.
+  uint64_t ft1970 = epoch_as_filetime + ns / 100;
+  epoch.fromWin32Time(ft1970);
+
+  // The "seconds" part in Posix time may be expected as zero.
+  EXPECT_EQ(ns / 100, epoch.toPosixTime());
+
+  // Confirm it reversible.
+  EXPECT_EQ(ft1970, epoch.toWin32Time());
+}
+
 }
