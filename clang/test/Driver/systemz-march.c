875356ade3e1c91d0f26cdfa08ee69ad41ec8e52
Benjamin Kramer <benny.kra@googlemail.com>
2013-07-19 17:49:21 +0000

    Fix test to actually check things.
    llvm-svn: 186701

--- clang/test/Driver/systemz-march.c
+++ clang/test/Driver/systemz-march.c
@@ -1,9 +1,9 @@
 // Check that -march works for all supported targets.
 
 // RUN: not %clang -target s390x -S -emit-llvm -march=z9 %s -o - 2>&1 | FileCheck --check-prefix=CHECK-Z9 %s
-// RUN: %clang -target s390x -S -emit-llvm -march=z10 %s
-// RUN: %clang -target s390x -S -emit-llvm -march=z196 %s
-// RUN: %clang -target s390x -S -emit-llvm -march=zEC12 %s
+// RUN: %clang -target s390x -### -S -emit-llvm -march=z10 %s 2>&1 | FileCheck --check-prefix=CHECK-Z10 %s
+// RUN: %clang -target s390x -### -S -emit-llvm -march=z196 %s 2>&1 | FileCheck --check-prefix=CHECK-Z196 %s
+// RUN: %clang -target s390x -### -S -emit-llvm -march=zEC12 %s 2>&1 | FileCheck --check-prefix=CHECK-ZEC12 %s
 
 // CHECK-Z9: error: unknown target CPU 'z9'
 // CHECK-Z10: "-target-cpu" "z10"
