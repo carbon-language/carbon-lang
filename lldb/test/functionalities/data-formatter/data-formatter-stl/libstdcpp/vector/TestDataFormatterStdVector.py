d4b5a0f53ffbf9cf15d6b9d8a0efcb2464933d12
Ashok Thirumurthi <ashok.thirumurthi@intel.com>
2013-07-19 20:22:43 +0000

    Reworked the test decorators to match the test results on the buildbots.
    llvm-svn: 186717

--- lldb/test/functionalities/data-formatter/data-formatter-stl/libstdcpp/vector/TestDataFormatterStdVector.py
+++ lldb/test/functionalities/data-formatter/data-formatter-stl/libstdcpp/vector/TestDataFormatterStdVector.py
@@ -20,7 +20,7 @@ class StdVectorDataFormatterTestCase(TestBase):
         self.data_formatter_commands()
 
     @dwarf_test
-    @expectedFailureGcc(15301, ["==", "4.6.3"]) # llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
+    @expectedFailureClang # llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
     def test_with_dwarf_and_run_command(self):
         """Test data formatter commands."""
         self.buildDwarf()
--- lldb/test/functionalities/type_completion/TestTypeCompletion.py
+++ lldb/test/functionalities/type_completion/TestTypeCompletion.py
@@ -21,7 +21,7 @@ class TypeCompletionTestCase(TestBase):
 
     @dwarf_test
     @expectedFailureIcc # often fails with 'NameAndAddress should be valid'
-    @expectedFailureGcc(15301, [">", "4.6"]) # llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
+    # Fails with gcc 4.8.1 with llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
     def test_with_dwarf_and_run_command(self):
         """Check that types only get completed when necessary."""
         self.buildDwarf()
