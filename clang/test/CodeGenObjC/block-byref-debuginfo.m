adaeb8947abbefafc40aa200e03f7044cabcce19
Adrian Prantl <aprantl@apple.com>
2013-07-23 00:21:48 +0000

    Simplify testcase. rdar://problem/14386148
    llvm-svn: 186901

--- clang/test/CodeGenObjC/block-byref-debuginfo.m
+++ clang/test/CodeGenObjC/block-byref-debuginfo.m
@@ -4,35 +4,20 @@
 // expression (256) that locates it inside of the byref descriptor:
 // CHECK: metadata !"foo", i32 0, i64 {{[0-9]+}}, i64 64, i64 256, i32 0, metadata
 
-typedef unsigned char uint8_t;
-@protocol NSObject
-@end
-@interface NSObject <NSObject> {
+@interface NSObject {
 }
 @end
-typedef void (^dispatch_block_t)(void);
-typedef long dispatch_once_t;
-static __inline__ __attribute__((__always_inline__)) __attribute__((__nonnull__)) __attribute__((__nothrow__))
-void
-_dispatch_once(dispatch_once_t *predicate, dispatch_block_t block)
-{
-};
 typedef struct Buffer *BufferRef;
 typedef struct Foo_s {
-    uint8_t *data;
+    unsigned char *data;
 } Foo;
-@protocol DelegateProtocol <NSObject>
-@end
-@interface FileReader : NSObject <DelegateProtocol>
-{
- dispatch_once_t offset;
+@interface FileReader : NSObject {
 }
 @end
 @implementation FileReader
 - (BufferRef) bar:(int *)index
 {
   __attribute__((__blocks__(byref))) Foo foo;
-  _dispatch_once(&offset, ^{});
   return 0;
 }
 @end
