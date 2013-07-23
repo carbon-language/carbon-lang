ca0be23b39df3255ff5272b5c3d9442aee7584b2
Marshall Clow <mclow.lists@gmail.com>
2013-07-23 17:05:24 +0000

    Implement string suffixes from N3642
    llvm-svn: 186956

---
+++ libcxx/test/strings/basic.string.literals/literal1.fail.cpp
@@ -0,0 +1,22 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+#include <string>
+#include <cassert>
+
+int main()
+{
+#if _LIBCPP_STD_VER > 11 
+    using namespace std;
+
+	std::string foo  =   ""s;  // should fail w/conversion operator not found
+#else
+#error
+#endif
+}
