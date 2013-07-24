d801711efbb45201a0499d01644fab2822242390
Adrian Prantl <aprantl@apple.com>
2013-07-24 20:34:34 +0000

    add radar number to testcase.
    llvm-svn: 187073

--- clang/test/CodeGenObjC/block-byref-debuginfo.m
+++ clang/test/CodeGenObjC/block-byref-debuginfo.m
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -fblocks -fobjc-arc -fobjc-runtime-has-weak -g -triple x86_64-apple-darwin -O0 -emit-llvm %s -o - | FileCheck %s
 
+// rdar://problem/14386148
 // Test that the foo is aligned at an 8 byte boundary in the DWARF
 // expression (256) that locates it inside of the byref descriptor:
 // CHECK: metadata !"foo", i32 0, i64 {{[0-9]+}}, i64 64, i64 256, i32 0, metadata
