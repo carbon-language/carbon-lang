carbon-language/carbon-lang

commit d07c840e6a125e954bfe015062c861369aae870e

Initial architecture for clang-tidy.
This is the first version of a possible clang-tidy architecture. The
purpose of clang-tidy is to detect errors in adhering to common coding
patterns, e.g. described in the LLVM Coding Standards.

This is still heavily in flux.

Review: http://llvm-reviews.chandlerc.com/D884
llvm-svn: 187345

clang-tools-extra/clang-tidy/tool/ClangTidyMain.cpp
@@ -1,0 +1,56 @@
+//===--- tools/extra/clang-tidy/ClangTidyMain.cpp - Clang tidy tool -------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+///  \file This file implements a clang-tidy tool.
+///
+///  This tool uses the Clang Tooling infrastructure, see
+///    http://clang.llvm.org/docs/HowToSetupToolingForLLVM.html
+///  for details on setting it up with LLVM source tree.
+///
+//===----------------------------------------------------------------------===//
+
+#include "../ClangTidy.h"
+#include "clang/Driver/OptTable.h"
+#include "clang/Driver/Options.h"
+#include "llvm/Support/CommandLine.h"
+#include <vector>
+
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+cl::OptionCategory ClangTidyCategory("clang-tidy options");
+
+cl::list<std::string>
+Ranges(cl::Positional, cl::desc("<range0> [... <rangeN>]"), cl::OneOrMore);
+
+static cl::opt<std::string> Checks(
+    "checks",
+    cl::desc("Regular expression matching the names of the checks to be run."),
+    cl::init(".*"), cl::cat(ClangTidyCategory));
+static cl::opt<bool> Fix("fix", cl::desc("Fix detected errors if possible."),
+                         cl::init(false), cl::cat(ClangTidyCategory));
+
+// FIXME: Add option to list name/description of all checks.
+
+int main(int argc, const char **argv) {
+  cl::ParseCommandLineOptions(argc, argv, "TBD\n");
+  OwningPtr<clang::tooling::CompilationDatabase> Compilations(
+      FixedCompilationDatabase::loadFromCommandLine(argc, argv));
+  if (!Compilations)
+    return 0;
+  // FIXME: Load other compilation databases.
+
+  SmallVector<clang::tidy::ClangTidyError, 16> Errors;
+  clang::tidy::runClangTidy(Checks, *Compilations, Ranges, &Errors);
+  clang::tidy::handleErrors(Errors, Fix);
+
+  return 0;
+}

clang-tools-extra/CMakeLists.txt
@@ -2,6 +2,7 @@ add_subdirectory(remove-cstr-calls)
 add_subdirectory(tool-template)
 add_subdirectory(cpp11-migrate)
 add_subdirectory(modularize)
+add_subdirectory(clang-tidy)
 
 # Add the common testsuite after all the tools.
 add_subdirectory(test)

clang-tools-extra/Makefile
@@ -12,7 +12,7 @@ CLANG_LEVEL := ../..
 include $(CLANG_LEVEL)/../../Makefile.config
 
 PARALLEL_DIRS := remove-cstr-calls tool-template modularize
-DIRS := cpp11-migrate unittests
+DIRS := cpp11-migrate clang-tidy unittests
 
 include $(CLANG_LEVEL)/Makefile
 

clang-tools-extra/clang-tidy/CMakeLists.txt
@@ -1,0 +1,23 @@
+set(LLVM_LINK_COMPONENTS
+  ${LLVM_TARGETS_TO_BUILD}
+  asmparser
+  bitreader
+  support
+  mc
+  )
+
+add_clang_library(clangTidy
+  ClangTidy.cpp
+  ClangTidyModule.cpp
+  )
+target_link_libraries(clangTidy
+  clangTooling
+  clangBasic
+  clangRewriteFrontend
+  clangTidyLLVMModule
+  clangTidyGoogleModule
+  )
+
+add_subdirectory(tool)
+add_subdirectory(llvm)
+add_subdirectory(google)

clang-tools-extra/clang-tidy/ClangTidy.cpp
@@ -1,0 +1,184 @@
+//===--- tools/extra/clang-tidy/ClangTidy.cpp - Clang tidy tool -----------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+///  \file This file implements a clang-tidy tool.
+///
+///  This tool uses the Clang Tooling infrastructure, see
+///    http://clang.llvm.org/docs/HowToSetupToolingForLLVM.html
+///  for details on setting it up with LLVM source tree.
+///
+//===----------------------------------------------------------------------===//
+
+#include "ClangTidy.h"
+#include "ClangTidyDiagnosticConsumer.h"
+#include "ClangTidyModuleRegistry.h"
+#include "clang/AST/ASTConsumer.h"
+#include "clang/AST/ASTContext.h"
+#include "clang/AST/Decl.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Lex/PPCallbacks.h"
+#include "clang/Lex/Preprocessor.h"
+#include "clang/Frontend/ASTConsumers.h"
+#include "clang/Frontend/CompilerInstance.h"
+#include "clang/Frontend/FrontendActions.h"
+#include "clang/Frontend/TextDiagnosticPrinter.h"
+#include "clang/Rewrite/Frontend/FixItRewriter.h"
+#include "clang/Rewrite/Frontend/FrontendActions.h"
+#include "clang/Tooling/Tooling.h"
+#include "clang/Tooling/Refactoring.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Signals.h"
+#include <vector>
+
+using namespace clang::ast_matchers;
+using namespace clang::driver;
+using namespace clang::tooling;
+using namespace llvm;
+
+namespace clang {
+namespace tidy {
+namespace {
+
+class ClangTidyPPAction : public PreprocessOnlyAction {
+public:
+  ClangTidyPPAction(SmallVectorImpl<ClangTidyCheck *> &Checks,
+                    ClangTidyContext &Context)
+      : Checks(Checks), Context(Context) {}
+
+private:
+  virtual bool BeginSourceFileAction(CompilerInstance &Compiler,
+                                     llvm::StringRef file_name) {
+    Context.setSourceManager(&Compiler.getSourceManager());
+    for (SmallVectorImpl<ClangTidyCheck *>::iterator I = Checks.begin(),
+                                                     E = Checks.end();
+         I != E; ++I)
+      (*I)->registerPPCallbacks(Compiler);
+    return true;
+  }
+
+  SmallVectorImpl<ClangTidyCheck *> &Checks;
+  ClangTidyContext &Context;
+};
+
+class ClangTidyPPActionFactory : public FrontendActionFactory {
+public:
+  ClangTidyPPActionFactory(SmallVectorImpl<ClangTidyCheck *> &Checks,
+                           ClangTidyContext &Context)
+      : Checks(Checks), Context(Context) {}
+
+  virtual FrontendAction *create() {
+    return new ClangTidyPPAction(Checks, Context);
+  }
+
+private:
+  SmallVectorImpl<ClangTidyCheck *> &Checks;
+  ClangTidyContext &Context;
+};
+
+} // namespace
+
+ClangTidyError::ClangTidyError(const SourceManager &Sources, SourceLocation Loc,
+                               StringRef Message,
+                               const tooling::Replacements &Fix)
+    : Message(Message), Fix(Fix) {
+  FilePath = Sources.getFilename(Loc);
+  FileOffset = Sources.getFileOffset(Loc);
+}
+
+DiagnosticBuilder ClangTidyContext::Diag(SourceLocation Loc,
+                                         StringRef Message) {
+  return DiagEngine->Report(
+      Loc, DiagEngine->getCustomDiagID(DiagnosticsEngine::Warning, Message));
+}
+
+void ClangTidyContext::setDiagnosticsEngine(DiagnosticsEngine *Engine) {
+  DiagEngine = Engine;
+}
+
+void ClangTidyContext::setSourceManager(SourceManager *SourceMgr) {
+  DiagEngine->setSourceManager(SourceMgr);
+}
+
+/// \brief Store a \c ClangTidyError.
+void ClangTidyContext::storeError(const ClangTidyError &Error) {
+  Errors->push_back(Error);
+}
+
+void ClangTidyCheck::run(const ast_matchers::MatchFinder::MatchResult &Result) {
+  Context->setSourceManager(Result.SourceManager);
+  check(Result);
+}
+
+void runClangTidy(StringRef CheckRegexString,
+                  const tooling::CompilationDatabase &Compilations,
+                  ArrayRef<std::string> Ranges,
+                  SmallVectorImpl<ClangTidyError> *Errors) {
+  // FIXME: Ranges are currently full files. Support selecting specific
+  // (line-)ranges.
+  ClangTool Tool(Compilations, Ranges);
+  clang::tidy::ClangTidyContext Context(Errors);
+  ClangTidyDiagnosticConsumer DiagConsumer(Context);
+  ClangTidyCheckFactories CheckFactories;
+  for (ClangTidyModuleRegistry::iterator I = ClangTidyModuleRegistry::begin(),
+                                         E = ClangTidyModuleRegistry::end();
+       I != E; ++I) {
+    OwningPtr<ClangTidyModule> Module(I->instantiate());
+    Module->addCheckFactories(CheckFactories);
+  }
+
+  SmallVector<ClangTidyCheck *, 16> Checks;
+  CheckFactories.createChecks(CheckRegexString, Checks);
+
+  MatchFinder Finder;
+  for (SmallVectorImpl<ClangTidyCheck *>::iterator I = Checks.begin(),
+                                                   E = Checks.end();
+       I != E; ++I) {
+    (*I)->setContext(&Context);
+    (*I)->registerMatchers(&Finder);
+  }
+
+  Tool.run(new ClangTidyPPActionFactory(Checks, Context));
+  Tool.run(newFrontendActionFactory(&Finder));
+}
+
+void handleErrors(SmallVectorImpl<ClangTidyError> &Errors, bool Fix) {
+  FileManager Files((FileSystemOptions()));
+  IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts = new DiagnosticOptions();
+  DiagnosticConsumer *DiagPrinter =
+      new TextDiagnosticPrinter(llvm::outs(), &*DiagOpts);
+  DiagnosticsEngine Diags(IntrusiveRefCntPtr<DiagnosticIDs>(new DiagnosticIDs),
+                          &*DiagOpts, DiagPrinter);
+  DiagPrinter->BeginSourceFile(LangOptions());
+  SourceManager SourceMgr(Diags, Files);
+  Rewriter Rewrite(SourceMgr, LangOptions());
+  for (SmallVectorImpl<ClangTidyError>::iterator I = Errors.begin(),
+                                                 E = Errors.end();
+       I != E; ++I) {
+    const FileEntry *File = Files.getFile(I->FilePath);
+    FileID ID = SourceMgr.createFileID(File, SourceLocation(), SrcMgr::C_User);
+    SourceLocation Loc = SourceMgr.getLocForStartOfFile(ID);
+    Diags.Report(Loc.getLocWithOffset(I->FileOffset),
+                 Diags.getCustomDiagID(DiagnosticsEngine::Warning, I->Message));
+    tooling::applyAllReplacements(I->Fix, Rewrite);
+  }
+  // FIXME: Run clang-format on changes.
+  if (Fix)
+    Rewrite.overwriteChangedFiles();
+}
+
+// This anchor is used to force the linker to link the LLVMModule.
+extern volatile int LLVMModuleAnchorSource;
+static int LLVMModuleAnchorDestination = LLVMModuleAnchorSource;
+
+// This anchor is used to force the linker to link the GoogleModule.
+extern volatile int GoogleModuleAnchorSource;
+static int GoogleModuleAnchorDestination = GoogleModuleAnchorSource;
+
+} // namespace tidy
+} // namespace clang

clang-tools-extra/clang-tidy/ClangTidy.h
@@ -1,0 +1,161 @@
+//===--- ClangTidy.h - clang-tidy -------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_H
+
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Tooling/Refactoring.h"
+
+namespace clang {
+
+class CompilerInstance;
+namespace ast_matchers {
+class MatchFinder;
+}
+namespace tooling {
+class CompilationDatabase;
+}
+
+namespace tidy {
+
+/// \brief A detected error complete with information to display diagnostic and
+/// automatic fix.
+///
+/// This is used as an intermediate format to transport Diagnostics without a
+/// dependency on a SourceManager.
+///
+/// FIXME: Make Diagnostics flexible enough to support this directly.
+struct ClangTidyError {
+  ClangTidyError(const SourceManager &Sources, SourceLocation Loc,
+                 StringRef Message, const tooling::Replacements &Fix);
+
+  std::string Message;
+  unsigned FileOffset;
+  std::string FilePath;
+  tooling::Replacements Fix;
+};
+
+/// \brief Every \c ClangTidyCheck reports errors through a \c DiagnosticEngine
+/// provided by this context.
+///
+/// A \c ClangTidyCheck always has access to the active context to report
+/// warnings like:
+/// \code
+/// Context->Diag(Loc, "Single-argument constructors must be explicit")
+///     << FixItHint::CreateInsertion(Loc, "explicit ");
+/// \endcode
+class ClangTidyContext {
+public:
+  ClangTidyContext(SmallVectorImpl<ClangTidyError> *Errors) : Errors(Errors) {}
+
+  /// \brief Report any errors detected using this method.
+  ///
+  /// This is still under heavy development and will likely change towards using
+  /// tablegen'd diagnostic IDs.
+  /// FIXME: Figure out a way to manage ID spaces.
+  DiagnosticBuilder Diag(SourceLocation Loc, StringRef Message);
+
+  /// \brief Sets the \c DiagnosticsEngine so that Diagnostics can be generated
+  /// correctly.
+  ///
+  /// This is called from the \c ClangTidyCheck base class.
+  void setDiagnosticsEngine(DiagnosticsEngine *Engine);
+
+  /// \brief Sets the \c SourceManager of the used \c DiagnosticsEngine.
+  ///
+  /// This is called from the \c ClangTidyCheck base class.
+  void setSourceManager(SourceManager *SourceMgr);
+
+private:
+  friend class ClangTidyDiagnosticConsumer; // Calls storeError().
+
+  /// \brief Store a \c ClangTidyError.
+  void storeError(const ClangTidyError &Error);
+
+  SmallVectorImpl<ClangTidyError> *Errors;
+  DiagnosticsEngine *DiagEngine;
+};
+
+/// \brief Base class for all clang-tidy checks.
+///
+/// To implement a \c ClangTidyCheck, write a subclass and overwrite some of the
+/// base class's methods. E.g. to implement a check that validates namespace
+/// declarations, overwrite \c registerMatchers:
+///
+/// \code
+/// registerMatchers(ast_matchers::MatchFinder *Finder) {
+///   Finder->addMatcher(namespaceDecl().bind("namespace"), this);
+/// }
+/// \endcode
+///
+/// and then overwrite \c check(const MatchResult &Result) to do the actual
+/// check for each match.
+///
+/// A new \c ClangTidyCheck instance is created per translation unit.
+///
+/// FIXME: Figure out whether carrying information from one TU to another is
+/// useful/necessary.
+class ClangTidyCheck : public ast_matchers::MatchFinder::MatchCallback {
+public:
+  virtual ~ClangTidyCheck() {}
+
+  /// \brief Overwrite this to register \c PPCallbacks with \c Compiler.
+  ///
+  /// This should be used for clang-tidy checks that analyze preprocessor-
+  /// dependent properties, e.g. the order of include directives.
+  virtual void registerPPCallbacks(CompilerInstance &Compiler) {}
+
+  /// \brief Overwrite this to register ASTMatchers with \p Finder.
+  ///
+  /// This should be used by clang-tidy checks that analyze code properties that
+  /// dependent on AST knowledge.
+  ///
+  /// You can register as many matchers as necessary with \p Finder. Usually,
+  /// "this" will be used as callback, but you can also specify other callback
+  /// classes. Thereby, different matchers can trigger different callbacks.
+  ///
+  /// If you need to merge information between the different matchers, you can
+  /// store these as members of the derived class. However, note that all
+  /// matches occur in the order of the AST traversal.
+  virtual void registerMatchers(ast_matchers::MatchFinder *Finder) {}
+
+  /// \brief \c ClangTidyChecks that register ASTMatchers should do the actual
+  /// work in here.
+  virtual void check(const ast_matchers::MatchFinder::MatchResult &Result) {}
+
+  /// \brief The infrastructure sets the context to \p Ctx with this function.
+  void setContext(ClangTidyContext *Ctx) { Context = Ctx; }
+
+protected:
+  ClangTidyContext *Context;
+
+private:
+  virtual void run(const ast_matchers::MatchFinder::MatchResult &Result);
+};
+
+/// \brief Run a set of clang-tidy checks on a set of files.
+void runClangTidy(StringRef CheckRegexString,
+                  const tooling::CompilationDatabase &Compilations,
+                  ArrayRef<std::string> Ranges,
+                  SmallVectorImpl<ClangTidyError> *Errors);
+
+// FIXME: This interface will need to be significantly extended to be useful.
+// FIXME: Implement confidence levels for displaying/fixing errors.
+//
+/// \brief Displays the found \p Errors to the users. If \p Fix is true, \p
+/// Errors containing fixes are automatically applied.
+void handleErrors(SmallVectorImpl<ClangTidyError> &Errors, bool Fix);
+
+} // end namespace tidy
+} // end namespace clang
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_H

clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.h
@@ -1,0 +1,71 @@
+//===--- ClangTidyDiagnosticConsumer.h - clang-tidy -------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_DIAGNOSTIC_CONSUMER_H
+#define LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_DIAGNOSTIC_CONSUMER_H
+
+#include "clang/Basic/Diagnostic.h"
+#include "clang/Basic/SourceManager.h"
+#include "clang/Tooling/Refactoring.h"
+#include "llvm/ADT/SmallString.h"
+
+namespace clang {
+
+class CompilerInstance;
+namespace ast_matchers {
+class MatchFinder;
+}
+namespace tooling {
+class CompilationDatabase;
+}
+
+namespace tidy {
+
+/// \brief A diagnostic consumer that turns each \c Diagnostic into a
+/// \c SourceManager-independent \c ClangTidyError.
+//
+// FIXME: If we move away from unit-tests, this can be moved to a private
+// implementation file.
+class ClangTidyDiagnosticConsumer : public DiagnosticConsumer {
+public:
+  ClangTidyDiagnosticConsumer(ClangTidyContext &Ctx) : Context(Ctx) {
+    IntrusiveRefCntPtr<DiagnosticOptions> DiagOpts = new DiagnosticOptions();
+    Diags.reset(new DiagnosticsEngine(
+        IntrusiveRefCntPtr<DiagnosticIDs>(new DiagnosticIDs), &*DiagOpts, this,
+        /*ShouldOwnClient=*/false));
+    Context.setDiagnosticsEngine(Diags.get());
+  }
+
+  // FIXME: The concept of converting between FixItHints and Replacements is
+  // more generic and should be pulled out into a more useful Diagnostics
+  // library.
+  virtual void HandleDiagnostic(DiagnosticsEngine::Level DiagLevel,
+                                const Diagnostic &Info) {
+    tooling::Replacements Replacements;
+    SourceManager &SourceMgr = Info.getSourceManager();
+    for (unsigned i = 0, e = Info.getNumFixItHints(); i != e; ++i) {
+      Replacements.insert(tooling::Replacement(
+          SourceMgr, Info.getFixItHint(i).RemoveRange.getBegin(), 0,
+          Info.getFixItHint(i).CodeToInsert));
+    }
+    SmallString<100> Buf;
+    Info.FormatDiagnostic(Buf);
+    Context.storeError(
+        ClangTidyError(SourceMgr, Info.getLocation(), Buf.str(), Replacements));
+  }
+
+private:
+  ClangTidyContext &Context;
+  OwningPtr<DiagnosticsEngine> Diags;
+};
+
+} // end namespace tidy
+} // end namespace clang
+
+#endif // LLVM_CLANG_TOOLS_EXTRA_CLANG_TIDY_CLANG_TIDY_DIAGNOSTIC_CONSUMER_H

clang-tools-extra/clang-tidy/ClangTidyModule.cpp
@@ -1,0 +1,49 @@
+//===--- tools/extra/clang-tidy/ClangTidyModule.cpp - Clang tidy tool -----===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+///  \file Implements classes required to build clang-tidy modules.
+///
+//===----------------------------------------------------------------------===//
+
+#include "ClangTidyModule.h"
+#include "llvm/Support/Regex.h"
+
+namespace clang {
+namespace tidy {
+
+CheckFactoryBase::~CheckFactoryBase() {}
+
+ClangTidyCheckFactories::~ClangTidyCheckFactories() {
+  for (std::map<std::string, CheckFactoryBase *>::iterator
+           I = Factories.begin(),
+           E = Factories.end();
+       I != E; ++I) {
+    delete I->second;
+  }
+}
+void ClangTidyCheckFactories::addCheckFactory(StringRef Name,
+                                              CheckFactoryBase *Factory) {
+
+  Factories[Name] = Factory;
+}
+
+void ClangTidyCheckFactories::createChecks(
+    StringRef CheckRegexString, SmallVectorImpl<ClangTidyCheck *> &Checks) {
+  llvm::Regex CheckRegex(CheckRegexString);
+  for (std::map<std::string, CheckFactoryBase *>::iterator
+           I = Factories.begin(),
+           E = Factories.end();
+       I != E; ++I) {
+    if (CheckRegex.match(I->first))
+      Checks.push_back(I->second->createCheck());
+  }
+}
+
+} // namespace tidy
+} // namespace clang

commit 03524b774ab28c37192443b4b8e0a6ec50e6ac94

Fix build error caused by r187345.
llvm-svn: 187346

clang-tools-extra/clang-tidy/tool/ClangTidyMain.cpp
@@ -16,8 +16,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "../ClangTidy.h"
-#include "clang/Driver/OptTable.h"
-#include "clang/Driver/Options.h"
+#include "llvm/Option/OptTable.h"
+#include "llvm/Option/Option.h"
 #include "llvm/Support/CommandLine.h"
 #include <vector>
 