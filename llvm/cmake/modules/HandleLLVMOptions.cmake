83afc1e43b78ceea236f166c1ceaa7b3183499a6
Eric Christopher <echristo@gmail.com>
2013-07-30 22:34:30 +0000

    Make sure that -gsplit-dwarf isn't passed to the linker.
    llvm-svn: 187456

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -263,7 +263,7 @@ endif()
 
 # Turn on -gsplit-dwarf if requested
 if(LLVM_USE_SPLIT_DWARF)
-  add_flag_if_supported("-gsplit-dwarf")
+  add_llvm_definitions("-gsplit-dwarf")
 endif()
 
 add_llvm_definitions( -D__STDC_CONSTANT_MACROS )
