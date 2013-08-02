9c99030dea72db4784f351476f7ec8242f6fccb8
Matt Kopec <Matt.Kopec@intel.com>
2013-08-02 17:53:28 +0000

    Mark additional icc test failures as expected fail.
    llvm-svn: 187660

--- lldb/test/expression_command/call-function/TestCallStdStringFunction.py
+++ lldb/test/expression_command/call-function/TestCallStdStringFunction.py
@@ -28,6 +28,7 @@ class ExprCommandCallFunctionTestCase(TestBase):
     @dwarf_test
     @expectedFailureFreeBSD # llvm.org/pr16697
     @expectedFailureGcc # llvm.org/pr14437, fails with GCC 4.6.3 and 4.7.2
+    @expectedFailureIcc # llvm.org/pr14437, fails with ICC 13.1
     def test_with_dwarf(self):
         """Test calling std::String member function."""
         self.buildDwarf()
--- lldb/test/lang/cpp/this/TestCPPThis.py
+++ lldb/test/lang/cpp/this/TestCPPThis.py
@@ -21,6 +21,7 @@ class CPPThisTestCase(TestBase):
     #rdar://problem/9962849
     @expectedFailureFreeBSD('llvm.org/pr16697') # Expression fails with 'there is no JIT compiled function'
     @expectedFailureGcc # llvm.org/pr15439 The 'this' pointer isn't available during expression evaluation when stopped in an inlined member function.
+    @expectedFailureIcc # ICC doesn't emit correct DWARF inline debug info for inlined member functions
     @dwarf_test
     def test_with_dwarf_and_run_command(self):
         """Test that the appropriate member variables are available when stopped in C++ static, inline, and const methods"""
