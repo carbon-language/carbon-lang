71044cbe16b10601d258a4257151514210f22543
Serge Pavlov <sepavloff@gmail.com>
2013-08-06 08:44:18 +0000

    Unbreak Debug build on Windows
    llvm-svn: 187786

--- llvm/lib/Transforms/Utils/FlattenCFG.cpp
+++ llvm/lib/Transforms/Utils/FlattenCFG.cpp
@@ -18,6 +18,7 @@
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 using namespace llvm;
 
