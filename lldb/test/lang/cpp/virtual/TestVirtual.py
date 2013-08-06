3449bb4484c7ad9859f759a77d9b48cb86a583cf
Matt Kopec <Matt.Kopec@intel.com>
2013-08-06 20:15:03 +0000

    Mark remaining failing test on icc as expected fail..
    llvm-svn: 187814

--- lldb/test/lang/cpp/virtual/TestVirtual.py
+++ lldb/test/lang/cpp/virtual/TestVirtual.py
@@ -29,6 +29,7 @@ class CppVirtualMadness(TestBase):
         self.virtual_madness_test()
 
     @expectedFailureFreeBSD('llvm.org/pr16697') # Expression fails with 'there is no JIT compiled function'
+    @expectedFailureIcc('llvm.org/pr16808') # lldb does not call the correct virtual function with icc
     def test_virtual_madness_dwarf(self):
         """Test that expression works correctly with virtual inheritance as well as virtual function."""
         self.buildDwarf()
