cad7e5e0b40dc67f47a34098d03c3385eeec715f
Chandler Carruth <chandlerc@gmail.com>
2013-08-07 08:47:36 +0000

    Add support for linking against a curses library when available and using it to detect whether or not a terminal supports colors. This replaces a particularly egregious hack that merely compared the TERM environment variable to "dumb". That doesn't really translate to a reasonable experience for users that have actually ensured their terminal's capabilities are accurately reflected.
    This makes testing a terminal for color support somewhat more expensive,
    but it is called very rarely anyways. The important fast path when the
    output is being piped somewhere is already in place.
    
    The global lock may seem excessive, but the spec for calling into curses
    is *terrible*. The whole library is terrible, and I spent quite a bit of
    time looking for a better way of doing this before convincing myself
    that this was the fundamentally correct way to behave. The damage of the
    curses library is very narrowly confined, and we continue to use raw
    escape codes for actually manipulating the colors which is a much sane
    system than directly using curses here (IMO).
    
    If this causes trouble for folks, please let me know. I've tested it on
    Linux and will watch the bots carefully. I've also worked to account for
    the variances of curses interfaces that I could finde documentation for,
    but that may not have been sufficient.
    
    llvm-svn: 187874

--- llvm/include/llvm/Config/config.h.cmake
+++ llvm/include/llvm/Config/config.h.cmake
@@ -48,6 +48,12 @@
 /* Define to 1 if you have the `closedir' function. */
 #cmakedefine HAVE_CLOSEDIR ${HAVE_CLOSEDIR}
 
+/* Define if curses provides the has_color() function on this platform. */
+#cmakedefine HAVE_CURSES
+
+/* Define to 1 if you have the <curses.h> header file. */
+#cmakedefine HAVE_CURSES_H
+
 /* Define to 1 if you have the <cxxabi.h> header file. */
 #cmakedefine HAVE_CXXABI_H ${HAVE_CXXABI_H}
 
@@ -255,6 +261,18 @@
 
 /* Define if mmap() can map files into memory */
 #undef HAVE_MMAP_FILE
+
+/* Define to 1 if you have the <ncursesw/curses.h> header file. */
+#cmakedefine HAVE_NCURSESW_CURSES_H
+
+/* Define to 1 if you have the <ncursesw.h> header file. */
+#cmakedefine HAVE_NCURSESW_H
+
+/* Define to 1 if you have the <ncurses/curses.h> header file. */
+#cmakedefine HAVE_NCURSES_CURSES_H
+
+/* Define to 1 if you have the <ncurses.h> header file. */
+#cmakedefine HAVE_NCURSES_H
 
 /* Define to 1 if you have the <ndir.h> header file, and it defines `DIR'. */
 #cmakedefine HAVE_NDIR_H ${HAVE_NDIR_H}
