668c61f68719ed9db739333ccdc9e681ee516024
Daniel Dunbar <daniel@zuster.org>
2013-08-07 03:12:51 +0000

    [lit] tests: Change discovery test to use --show-tests.
    llvm-svn: 187851

--- llvm/utils/lit/tests/discovery.py
+++ llvm/utils/lit/tests/discovery.py
@@ -1,7 +1,8 @@
 # Check the basic discovery process, including a sub-suite.
 #
 # RUN: %{lit} %{inputs}/discovery \
-# RUN:   -j 1 --debug --no-execute --show-suites -v > %t.out 2> %t.err
+# RUN:   -j 1 --debug --show-tests --show-suites --max-tests 0 \
+# RUN:   -v > %t.out 2> %t.err
 # RUN: FileCheck --check-prefix=CHECK-BASIC-OUT < %t.out %s
 # RUN: FileCheck --check-prefix=CHECK-BASIC-ERR < %t.err %s
 #
@@ -17,12 +18,13 @@
 # CHECK-BASIC-OUT:     Source Root: {{.*/discovery$}}
 # CHECK-BASIC-OUT:     Exec Root  : {{.*/discovery$}}
 #
-# CHECK-BASIC-OUT: -- Testing: 5 tests, 1 threads --
-# CHECK-BASIC-OUT: PASS: sub-suite :: test-one
-# CHECK-BASIC-OUT: PASS: sub-suite :: test-two
-# CHECK-BASIC-OUT: PASS: top-level-suite :: subdir/test-three
-# CHECK-BASIC-OUT: PASS: top-level-suite :: test-one
-# CHECK-BASIC-OUT: PASS: top-level-suite :: test-two
+# CHECK-BASIC-OUT: -- Available Tests --
+# CHECK-BASIC-OUT: sub-suite :: test-one
+# CHECK-BASIC-OUT: sub-suite :: test-two
+# CHECK-BASIC-OUT: top-level-suite :: subdir/test-three
+# CHECK-BASIC-OUT: top-level-suite :: test-one
+# CHECK-BASIC-OUT: top-level-suite :: test-two
+# CHECK-BASIC-OUT: -- Testing: 0
 
 
 # Check discovery when exact test names are given.
@@ -30,18 +32,20 @@
 # RUN: %{lit} \
 # RUN:     %{inputs}/discovery/subdir/test-three.py \
 # RUN:     %{inputs}/discovery/subsuite/test-one.txt \
-# RUN:   -j 1 --no-execute --show-suites -v > %t.out
+# RUN:   -j 1 --show-tests --show-suites --max-tests 0 -v > %t.out
 # RUN: FileCheck --check-prefix=CHECK-EXACT-TEST < %t.out %s
 #
-# CHECK-EXACT-TEST: -- Testing: 2 tests, 1 threads --
-# CHECK-EXACT-TEST: PASS: sub-suite :: test-one
-# CHECK-EXACT-TEST: PASS: top-level-suite :: subdir/test-three
+# CHECK-EXACT-TEST: -- Available Tests --
+# CHECK-EXACT-TEST: sub-suite :: test-one
+# CHECK-EXACT-TEST: top-level-suite :: subdir/test-three
+# CHECK-EXACT-TEST: -- Testing: 0
 
 
 # Check discovery when using an exec path.
 #
 # RUN: %{lit} %{inputs}/exec-discovery \
-# RUN:   -j 1 --debug --no-execute --show-suites -v > %t.out 2> %t.err
+# RUN:   -j 1 --debug --show-tests --show-suites --max-tests 0 \
+# RUN:   -v > %t.out 2> %t.err
 # RUN: FileCheck --check-prefix=CHECK-ASEXEC-OUT < %t.out %s
 # RUN: FileCheck --check-prefix=CHECK-ASEXEC-ERR < %t.err %s
 #
@@ -60,13 +64,13 @@
 # CHECK-ASEXEC-OUT:     Source Root: {{.*/discovery$}}
 # CHECK-ASEXEC-OUT:     Exec Root  : {{.*/exec-discovery$}}
 #
-# CHECK-ASEXEC-OUT: -- Testing: 5 tests, 1 threads --
-# CHECK-ASEXEC-OUT: PASS: sub-suite :: test-one
-# CHECK-ASEXEC-OUT: PASS: sub-suite :: test-two
-# CHECK-ASEXEC-OUT: PASS: top-level-suite :: subdir/test-three
-# CHECK-ASEXEC-OUT: PASS: top-level-suite :: test-one
-# CHECK-ASEXEC-OUT: PASS: top-level-suite :: test-two
-
+# CHECK-ASEXEC-OUT: -- Available Tests --
+# CHECK-ASEXEC-OUT: sub-suite :: test-one
+# CHECK-ASEXEC-OUT: sub-suite :: test-two
+# CHECK-ASEXEC-OUT: top-level-suite :: subdir/test-three
+# CHECK-ASEXEC-OUT: top-level-suite :: test-one
+# CHECK-ASEXEC-OUT: top-level-suite :: test-two
+# CHECK-ASEXEC-OUT: -- Testing: 0
 
 # Check discovery when exact test names are given.
 #
@@ -74,11 +78,12 @@
 #
 # RUN: %{lit} \
 # RUN:     %{inputs}/exec-discovery/subdir/test-three.py \
-# RUN:   -j 1 --no-execute --show-suites -v > %t.out
+# RUN:   -j 1 --show-tests --show-suites --max-tests 0 -v > %t.out
 # RUN: FileCheck --check-prefix=CHECK-ASEXEC-EXACT-TEST < %t.out %s
 #
-# CHECK-ASEXEC-EXACT-TEST: -- Testing: 1 tests, 1 threads --
-# CHECK-ASEXEC-EXACT-TEST: PASS: top-level-suite :: subdir/test-three
+# CHECK-ASEXEC-EXACT-TEST: -- Available Tests --
+# CHECK-ASEXEC-EXACT-TEST: top-level-suite :: subdir/test-three
+# CHECK-ASEXEC-EXACT-TEST: -- Testing: 0
 
 
 # Check that we don't recurse infinitely when loading an site specific test
@@ -86,11 +91,12 @@
 #
 # RUN: %{lit} \
 # RUN:     %{inputs}/exec-discovery-in-tree/obj/ \
-# RUN:   -j 1 --no-execute --show-suites -v > %t.out
+# RUN:   -j 1 --show-tests --show-suites --max-tests 0 -v > %t.out
 # RUN: FileCheck --check-prefix=CHECK-ASEXEC-INTREE < %t.out %s
 #
 #      CHECK-ASEXEC-INTREE:   exec-discovery-in-tree-suite - 1 tests
 # CHECK-ASEXEC-INTREE-NEXT:     Source Root: {{.*/exec-discovery-in-tree$}}
 # CHECK-ASEXEC-INTREE-NEXT:     Exec Root  : {{.*/exec-discovery-in-tree/obj$}}
-# CHECK-ASEXEC-INTREE-NEXT: -- Testing: 1 tests, 1 threads --
-# CHECK-ASEXEC-INTREE-NEXT: PASS: exec-discovery-in-tree-suite :: test-one
+# CHECK-ASEXEC-INTREE-NEXT: -- Available Tests --
+# CHECK-ASEXEC-INTREE-NEXT: exec-discovery-in-tree-suite :: test-one
+# CHECK-ASEXEC-INTREE: -- Testing: 0
