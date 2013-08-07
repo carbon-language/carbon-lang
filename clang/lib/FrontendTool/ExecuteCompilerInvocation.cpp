958297dabf9f31f390c763b6667e0b7010302d8b
Rafael Espindola <rafael.espindola@gmail.com>
2013-08-07 12:54:47 +0000

    Indicate success for simple options when clang called with -cc1.
    Patch by David Wiberg.
    
    llvm-svn: 187888

--- clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
+++ clang/lib/FrontendTool/ExecuteCompilerInvocation.cpp
@@ -183,7 +183,7 @@ bool clang::ExecuteCompilerInvocation(CompilerInstance *Clang) {
     Opts->PrintHelp(llvm::outs(), "clang -cc1",
                     "LLVM 'Clang' Compiler: http://clang.llvm.org",
                     /*Include=*/ driver::options::CC1Option, /*Exclude=*/ 0);
-    return 0;
+    return true;
   }
 
   // Honor -version.
@@ -191,7 +191,7 @@ bool clang::ExecuteCompilerInvocation(CompilerInstance *Clang) {
   // FIXME: Use a better -version message?
   if (Clang->getFrontendOpts().ShowVersion) {
     llvm::cl::PrintVersionMessage();
-    return 0;
+    return true;
   }
 
   // Load any requested plugins.
@@ -223,7 +223,7 @@ bool clang::ExecuteCompilerInvocation(CompilerInstance *Clang) {
   // This should happen AFTER plugins have been loaded!
   if (Clang->getAnalyzerOpts()->ShowCheckerHelp) {
     ento::printCheckerHelp(llvm::outs(), Clang->getFrontendOpts().Plugins);
-    return 0;
+    return true;
   }
 #endif
 
