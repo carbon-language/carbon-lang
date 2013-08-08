ba6c662669b0fb394bbabe35937c9c3ff4b45307
John Thompson <John.Thompson.JTSoftware@gmail.com>
2013-08-08 00:01:09 +0000

    Expanded modularize tests.
    llvm-svn: 187943

--- clang-tools-extra/test/modularize/Inputs/SomeDecls.h
+++ clang-tools-extra/test/modularize/Inputs/SomeDecls.h
@@ -3,3 +3,14 @@
 void FuncOne();
 
 int FuncTwo(int arg);
+
+void FuncOverload(int arg) {}
+void FuncOverload(char *arg) {}
+
+namespace Namespace1 {
+  void FuncNameSpace() {}
+}
+
+namespace Namespace2 {
+  void FuncNameSpace() {}
+}
--- clang-tools-extra/test/modularize/Inputs/SomeTypes.h
+++ clang-tools-extra/test/modularize/Inputs/SomeTypes.h
@@ -14,3 +14,25 @@ public:
 private:
   int Member;
 };
+
+/*
+This currently doesn't work.  Can't handle same name in different namespaces.
+namespace Namespace1 {
+  class NamespaceClass {
+  public:
+    NamespaceClass() : Member(0) {}
+  private:
+    int Member;
+  };
+}
+
+namespace Namespace2 {
+  class NamespaceClass {
+  public:
+    NamespaceClass() : Member(0) {}
+  private:
+    int Member;
+  };
+}
+*/
+
