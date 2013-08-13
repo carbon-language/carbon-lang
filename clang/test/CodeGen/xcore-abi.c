68443b0a1487c6f7e583de387fb76a778f4ececf
Arnold Schwaighofer <aschwaighofer@apple.com>
2013-08-13 14:51:38 +0000

    Fix xcore tests to use regex.
    llvm-svn: 188271

--- clang/test/CodeGen/xcore-abi.c
+++ clang/test/CodeGen/xcore-abi.c
@@ -7,26 +7,26 @@
 struct x { int a; };
 void testva (int n, ...) {
   va_list ap;
-  // CHECK: %ap = alloca i8*, align 4
+  // CHECK: [[AP:%[a-z0-9]+]] = alloca i8*, align 4
 
   char* v1 = va_arg (ap, char*);
-  // CHECK: %0 = va_arg i8** %ap, i8*
+  // CHECK: va_arg i8** [[AP]], i8*
 
   int v2 = va_arg (ap, int);
-  // CHECK: %1 = va_arg i8** %ap, i32
+  // CHECK: va_arg i8** [[AP]], i32
 
   long long int v3 = va_arg (ap, long long int);
-  // CHECK: %2 = va_arg i8** %ap, i64
+  // CHECK: va_arg i8** [[AP]], i64
 
   //struct x t = va_arg (ap, struct x);
   //cannot compile aggregate va_arg expressions yet
 }
 
 void testbuiltin (void) {
-// CHECK: %0 = tail call i32 @llvm.xcore.getid()
-// CHECK: %1 = tail call i32 @llvm.xcore.getps(i32 %0)
-// CHECK: %2 = tail call i32 @llvm.xcore.bitrev(i32 %1)
-// CHECK: tail call void @llvm.xcore.setps(i32 %0, i32 %2)
+// CHECK: [[I:%[a-z0-9]+]] = tail call i32 @llvm.xcore.getid()
+// CHECK: [[UI:%[a-z0-9]+]] = tail call i32 @llvm.xcore.getps(i32 [[I]])
+// CHECK: [[UI2:%[a-z0-9]+]] = tail call i32 @llvm.xcore.bitrev(i32 [[UI]])
+// CHECK: tail call void @llvm.xcore.setps(i32 [[I]], i32 [[UI2]])
   int i = __builtin_getid();
   unsigned int ui = __builtin_getps(i);
   ui = __builtin_bitrev(ui);
