83930ac0b7a8dd79059f86fefe08f42c5ebc7b98
Shuxin Yang <shuxin.llvm@gmail.com>
2013-08-14 19:45:49 +0000

    Do no use -emit-llvm for -flto.
    Tested on multiple OSes.
    
    llvm-svn: 188406

--- clang/test/Driver/lto.c
+++ clang/test/Driver/lto.c
@@ -1,11 +1,10 @@
-// -emit-llvm, -flto, and -O4 all cause a switch to llvm-bc object files.
+// -flto, and -O4 all cause a switch to llvm-bc object files.
 // RUN: %clang -ccc-print-phases -c %s -flto 2> %t.log
 // RUN: grep '2: compiler, {1}, lto-bc' %t.log
 // RUN: %clang -ccc-print-phases -c %s -O4 2> %t.log
 // RUN: grep '2: compiler, {1}, lto-bc' %t.log
 
-// and -emit-llvm doesn't alter pipeline (unfortunately?).
-// RUN: %clang -ccc-print-phases %s -emit-llvm 2> %t.log
+// RUN: %clang -ccc-print-phases %s -flto 2> %t.log
 // RUN: grep '0: input, ".*lto.c", c' %t.log
 // RUN: grep '1: preprocessor, {0}, cpp-output' %t.log
 // RUN: grep '2: compiler, {1}, lto-bc' %t.log
@@ -13,11 +12,11 @@
 
 // llvm-bc and llvm-ll outputs need to match regular suffixes
 // (unfortunately).
-// RUN: %clang %s -emit-llvm -save-temps -### 2> %t.log
+// RUN: %clang %s -flto -save-temps -### 2> %t.log
 // RUN: grep '"-o" ".*lto\.i" "-x" "c" ".*lto\.c"' %t.log
 // RUN: grep '"-o" ".*lto\.o" .*".*lto\.i"' %t.log
 // RUN: grep '".*a.out" .*".*lto\.o"' %t.log
 
-// RUN: %clang %s -emit-llvm -S -### 2> %t.log
+// RUN: %clang %s -flto -S -### 2> %t.log
 // RUN: grep '"-o" ".*lto\.s" "-x" "c" ".*lto\.c"' %t.log
 
