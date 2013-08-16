414a0cdd38e0ae2f397d9936abcfe73586be0dd2
Aaron Ballman <aaron@aaronballman.com>
2013-08-16 01:43:31 +0000

    Fixing a warning about control reaching the end of a non-void function.
    llvm-svn: 188524

--- llvm/utils/TableGen/CodeGenInstruction.cpp
+++ llvm/utils/TableGen/CodeGenInstruction.cpp
@@ -192,6 +192,7 @@ CGIOperandList::ParseOperandName(const std::string &Op, bool AllowWholeOp) {
 
   // Otherwise, didn't find it!
   PrintFatalError(TheDef->getName() + ": unknown suboperand name in '" + Op + "'");
+  return std::make_pair(0U, 0U);
 }
 
 static void ParseConstraint(const std::string &CStr, CGIOperandList &Ops) {
