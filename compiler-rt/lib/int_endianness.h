9dce26947e3bd5da661f96a77d32c01b0413fd5f
Joerg Sonnenberger <joerg@bec.de>
2013-08-17 22:32:10 +0000

    Fix endian conditions on Solaris. From Alexander Esilevich.
    llvm-svn: 188613

--- compiler-rt/lib/int_endianness.h
+++ compiler-rt/lib/int_endianness.h
@@ -19,13 +19,15 @@
 #if defined(__SVR4) && defined(__sun)
 #include <sys/byteorder.h>
 
-#if _BYTE_ORDER == _BIG_ENDIAN
+#if defined(_BIG_ENDIAN)
 #define _YUGA_LITTLE_ENDIAN 0
 #define _YUGA_BIG_ENDIAN    1
-#elif _BYTE_ORDER == _LITTLE_ENDIAN 
+#elif defined(_LITTLE_ENDIAN)
 #define _YUGA_LITTLE_ENDIAN 1
 #define _YUGA_BIG_ENDIAN    0
-#endif /* _BYTE_ORDER */
+#else /* !_LITTLE_ENDIAN */
+#error "unknown endianness"
+#endif /* !_LITTLE_ENDIAN */
 
 #endif /* Solaris and AuroraUX. */
 
