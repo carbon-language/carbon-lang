22735c8a5746a57e7fead19a548dca01c7b75185
Alexey Samsonov <samsonov@google.com>
2013-08-19 09:45:19 +0000

    Explicitly specify triple in driver test from r188660
    llvm-svn: 188662

--- clang/test/Driver/integrated-as.s
+++ clang/test/Driver/integrated-as.s
@@ -8,5 +8,5 @@
 // RUN: not %clang -c -integrated-as -Wa,--compress-debug-sections %s 2>&1 | FileCheck --check-prefix=INVALID %s
 // INVALID: error: unsupported argument '--compress-debug-sections' to option 'Wa,'
 
-// RUN: %clang -### -c -integrated-as %s -fsanitize=address 2>&1 %s | FileCheck --check-prefix=SANITIZE %s
+// RUN: %clang -### -target x86_64-linux-gnu -c -integrated-as %s -fsanitize=address 2>&1 %s | FileCheck --check-prefix=SANITIZE %s
 // SANITIZE: argument unused during compilation: '-fsanitize=address'
