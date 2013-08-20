422a3b4570e621c0674176123d152548cc6dd7fa
NAKAMURA Takumi <geek4civic@gmail.com>
2013-08-20 12:16:35 +0000

    clang/test/Driver: Remove the feature "clang-driver" in three tests since they might work as expected since their first commit.
    llvm-svn: 188788

--- clang/test/Driver/objc_default_synth.m
+++ clang/test/Driver/objc_default_synth.m
@@ -2,5 +2,4 @@
 // RUN: %clang -### -target armv7-unknown-freebsd %s 2>&1 | FileCheck %s
 // RUN: %clang -### -target armv7-apple-ios %s 2>&1 | FileCheck %s
 // RUN: %clang -### -target i686-apple-macosx %s 2>&1 | FileCheck %s
-// REQUIRES: clang-driver
 // CHECK: -fobjc-default-synthesize
--- clang/test/Driver/target-as.s
+++ clang/test/Driver/target-as.s
@@ -1,5 +1,3 @@
-// REQUIRES: clang-driver
-
 // Make sure the -march is passed down to cc1as.
 // RUN: %clang -target i386-unknown-freebsd -### -c -integrated-as %s \
 // RUN: -march=geode 2>&1 | FileCheck -check-prefix=TARGET %s
