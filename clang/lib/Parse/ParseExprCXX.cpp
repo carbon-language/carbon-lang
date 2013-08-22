dbca225589b111837eca921dfa493627b341f271
Faisal Vali <faisalv@yahoo.com>
2013-08-22 02:13:38 +0000

    Remove some unused variables identified by Juergen Ributzka *I need to turn on this warning in Visual C++ - sorry!*
    llvm-svn: 188979

--- clang/lib/Parse/ParseExprCXX.cpp
+++ clang/lib/Parse/ParseExprCXX.cpp
@@ -938,7 +938,6 @@ ExprResult Parser::ParseLambdaExpressionAfterIntroducer(
 
     
     if (Tok.isNot(tok::r_paren)) {
-      sema::LambdaScopeInfo *LSI = Actions.getCurLambda(); 
       if (getLangOpts().CPlusPlus1y)
         Actions.RecordParsingTemplateParameterDepth(TemplateParameterDepth);
       ParseParameterDeclarationClause(D, Attr, ParamInfo, EllipsisLoc);
