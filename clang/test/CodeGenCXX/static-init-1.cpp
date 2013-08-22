90e119a0fcee3581d8d7dbe77b4906002525c714
Reid Kleckner <reid@kleckner.net>
2013-08-22 18:45:40 +0000

    FileCheckify a grep test.
    llvm-svn: 189042

--- clang/test/CodeGenCXX/static-init-1.cpp
+++ clang/test/CodeGenCXX/static-init-1.cpp
@@ -1,5 +1,4 @@
-// RUN: %clang_cc1 -triple=x86_64-apple-darwin9 -emit-llvm %s -o %t
-// RUN: grep "call i32 @_Z5func1i" %t | count 3
+// RUN: %clang_cc1 -triple=x86_64-apple-darwin9 -emit-llvm %s -o - | FileCheck %s
 
 extern "C" int printf(...);
 
@@ -9,15 +8,18 @@ int func2(int c) { return printf("loading the func2(%d)\n", c); };
 int func1(int c) { return printf("loading the func1(%d)\n", c); }
 
 static int loader_1 = func1(++count);
+// CHECK: call i32 @_Z5func1i
 
 int loader_2 = func2(++count);
 
 static int loader_3 = func1(++count);
-
+// CHECK: call i32 @_Z5func1i
 
 int main() {}
 
 int loader_4 = func2(++count);
 static int loader_5 = func1(++count);
 int loader_6 = func2(++count);
+// CHECK: call i32 @_Z5func1i
 
+// CHECK-NOT: call i32 @_Z5func1i
