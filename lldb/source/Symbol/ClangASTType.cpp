093bf45f9c889552ee46a727e3600c28c772ac57
Michael Sartain <mikesart@valvesoftware.com>
2013-08-22 20:50:18 +0000

    A clang::Type::Decayed type to kill compile warning
    llvm-svn: 189061

--- lldb/source/Symbol/ClangASTType.cpp
+++ lldb/source/Symbol/ClangASTType.cpp
@@ -1428,6 +1428,9 @@ ClangASTType::GetTypeClass () const
         case clang::Type::Decltype:                 break;
         case clang::Type::TemplateSpecialization:   break;
         case clang::Type::Atomic:                   break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:                  break;
     }
     // We don't know hot to display this type...
     return lldb::eTypeClassOther;
@@ -1865,7 +1868,10 @@ ClangASTType::GetEncoding (uint64_t &count) const
         case clang::Type::TemplateSpecialization:
         case clang::Type::Atomic:
             break;
-            
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:
+            break;
     }
     count = 0;
     return lldb::eEncodingInvalid;
@@ -1993,6 +1999,10 @@ ClangASTType::GetFormat () const
         case clang::Type::Decltype:
         case clang::Type::TemplateSpecialization:
         case clang::Type::Atomic:
+            break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:
             break;
     }
     // We don't know hot to display this type...
@@ -5165,6 +5175,9 @@ ClangASTType::GetDeclContextForType () const
         case clang::Type::InjectedClassName:        break;
         case clang::Type::DependentName:            break;
         case clang::Type::Atomic:                   break;
+
+        // pointer type decayed from an array or function type.
+        case clang::Type::Decayed:                  break;
     }
     // No DeclContext in this type...
     return NULL;
