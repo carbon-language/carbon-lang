469c056299c2efd046c85fee5864d098facbd627
Peter Collingbourne <peter@pcc.me.uk>
2013-08-23 18:45:22 +0000

    [dfsan] Integration test for the equal-labels case
    llvm-svn: 189134

--- compiler-rt/lib/dfsan/lit_tests/propagate.c
+++ compiler-rt/lib/dfsan/lit_tests/propagate.c
@@ -22,6 +22,9 @@ int main(void) {
   dfsan_label k_label = dfsan_create_label("k", 0);
   dfsan_set_label(k_label, &k, sizeof(k));
 
+  int k2 = 4;
+  dfsan_set_label(k_label, &k2, sizeof(k2));
+
   dfsan_label ij_label = dfsan_get_label(i + j);
   assert(dfsan_has_label(ij_label, i_label));
   assert(dfsan_has_label(ij_label, j_label));
@@ -34,6 +37,8 @@ int main(void) {
   assert(dfsan_has_label(ijk_label, i_label));
   assert(dfsan_has_label(ijk_label, j_label));
   assert(dfsan_has_label(ijk_label, k_label));
+
+  assert(dfsan_get_label(k + k2) == k_label);
 
   struct { int i, j; } s = { i, j };
   assert(dfsan_read_label(&s, sizeof(s)) == ij_label);
