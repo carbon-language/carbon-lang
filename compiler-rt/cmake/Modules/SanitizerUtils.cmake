acab30e927d0bcfb17e18b41c55d0d6f105ab659
Alexey Samsonov <samsonov@google.com>
2013-08-27 15:08:02 +0000

    Properly generate lists of exported symbols for sanitizer runtimes
    This change adds a Python script that is invoked for
    the just-built sanitizer runtime to generate the list of exported symbols
    passed to the linker. By default, it contains interceptors and sanitizer
    interface functions, but can be extended with tool-specific lists.
    
    llvm-svn: 189356

---
+++ compiler-rt/cmake/Modules/SanitizerUtils.cmake
@@ -0,0 +1,22 @@
+include(LLVMParseArguments)
+
+set(SANITIZER_GEN_DYNAMIC_LIST
+  ${COMPILER_RT_SOURCE_DIR}/lib/sanitizer_common/scripts/gen_dynamic_list.py)
+
+# Create a target "<name>-symbols" that would generate the list of symbols
+# that need to be exported from sanitizer runtime "<name>". Function
+# interceptors are exported automatically, user can also provide files with
+# symbol names that should be exported as well.
+#   add_sanitizer_rt_symbols(<name> <files with extra symbols to export>)
+macro(add_sanitizer_rt_symbols name)
+  get_target_property(libfile ${name} LOCATION)
+  set(symsfile "${libfile}.syms")
+  add_custom_target(${name}-symbols ALL
+    COMMAND ${SANITIZER_GEN_DYNAMIC_LIST} ${libfile} ${ARGN}
+      > ${symsfile}
+    DEPENDS ${name} ${SANITIZER_GEN_DYNAMIC_LIST} ${ARGN}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
+    VERBATIM
+    SOURCES ${SANITIZER_GEN_DYNAMIC_LIST} ${ARGN})
+  install(FILES ${symsfile} DESTINATION ${COMPILER_RT_LIBRARY_INSTALL_DIR})
+endmacro()
---
+++ compiler-rt/lib/sanitizer_common/scripts/gen_dynamic_list.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python
+#===- lib/sanitizer_common/scripts/gen_dynamic_list.py ---------------------===#
+#
+#                     The LLVM Compiler Infrastructure
+#
+# This file is distributed under the University of Illinois Open Source
+# License. See LICENSE.TXT for details.
+#
+#===------------------------------------------------------------------------===#
+#
+# Generates the list of functions that should be exported from sanitizer
+# runtimes. The output format is recognized by --dynamic-list linker option.
+# Usage:
+#   gen_dynamic_list.py libclang_rt.*san*.a [ files ... ]
+#
+#===------------------------------------------------------------------------===#
+import re
+import os
+import subprocess
+import sys
+
+def get_global_functions(library):
+  functions = []
+  nm_proc = subprocess.Popen(['nm', library], stdout=subprocess.PIPE)
+  nm_out = nm_proc.communicate()[0].split('\n')
+  if nm_proc.returncode != 0:
+    raise subprocess.CalledProcessError(nm_proc.returncode, 'nm')
+  for line in nm_out:
+    cols = line.split(' ')
+    if (len(cols) == 3 and cols[1] in ('T', 'W')) :
+      functions.append(cols[2])
+  return functions
+
+result = []
+
+library = sys.argv[1]
+all_functions = get_global_functions(library)
+function_set = set(all_functions)
+new_delete = set(['_ZdaPv', '_ZdaPvRKSt9nothrow_t',
+                  '_ZdlPv', '_ZdlPvRKSt9nothrow_t',
+                  '_Znam', '_ZnamRKSt9nothrow_t',
+                  '_Znwm', '_ZnwmRKSt9nothrow_t'])
+versioned_functions = set(['memcpy', 'pthread_cond_broadcast',
+                           'pthread_cond_destroy', 'pthread_cond_signal',
+                           'pthread_cond_timedwait', 'pthread_cond_wait',
+                           'realpath', 'sched_getaffinity'])
+for func in all_functions:
+  # Export new/delete operators.
+  if func in new_delete:
+    result.append(func)
+    continue
+  # Export interceptors.
+  match = re.match('__interceptor_(.*)', func)
+  if match:
+    result.append(func)
+    # We have to avoid exporting the interceptors for versioned library
+    # functions due to gold internal error.
+    orig_name = match.group(1)
+    if orig_name in function_set and orig_name not in versioned_functions:
+      result.append(orig_name)
+    continue
+  # Export sanitizer interface functions.
+  if re.match('__sanitizer_(.*)', func):
+    result.append(func)
+
+# Additional exported functions from files.
+for fname in sys.argv[2:]:
+  f = open(fname, 'r')
+  for line in f:
+    result.append(line.rstrip())
+
+print '{'
+result.sort()
+for f in result:
+  print '  ' + f + ';'
+print '};'
