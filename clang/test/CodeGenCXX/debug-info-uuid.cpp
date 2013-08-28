27c22edde44d1aa87458a88cb7f3e8a6ebc3739d
Manman Ren <manman.ren@gmail.com>
2013-08-28 21:19:31 +0000

    Debug Info testing case: update uuid testing case to check against Itanium.
    Right now, the output for Itanium vs. Microsoft is the same. Once we start
    calling mangler to get the unique identifier, this testing case will require
    support for uuid mangling.
    
    llvm-svn: 189518

--- clang/test/CodeGenCXX/debug-info-uuid.cpp
+++ clang/test/CodeGenCXX/debug-info-uuid.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -emit-llvm -fms-extensions -triple=x86_64-pc-win32 -cxx-abi microsoft -g %s -o - -std=c++11 | FileCheck %s
+// RUN: %clang_cc1 -emit-llvm -fms-extensions -triple=x86_64-unknown-unknown -g %s -o - -std=c++11 | FileCheck %s
 
 // CHECK: metadata [[TGIARGS:![0-9]*]], null} ; [ DW_TAG_structure_type ] [tmpl_guid<&__uuidof(uuid)>]
 // CHECK: [[TGIARGS]] = metadata !{metadata [[TGIARG1:![0-9]*]]}
