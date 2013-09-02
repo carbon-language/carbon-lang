47aeab5cbe887f4dd0b87eae9674ac5a32c4402a
Renato Golin <renato.golin@linaro.org>
2013-09-02 14:11:33 +0000

    Fix gnueeabi typo in tests
    llvm-svn: 189759

--- clang/test/Driver/arm-alignment.c
+++ clang/test/Driver/arm-alignment.c
@@ -1,19 +1,19 @@
-// RUN: %clang -target arm-none-gnueeabi -munaligned-access -### %s 2> %t
+// RUN: %clang -target arm-none-gnueabi -munaligned-access -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-UNALIGNED < %t %s
 
-// RUN: %clang -target arm-none-gnueeabi -mstrict-align -munaligned-access -### %s 2> %t
+// RUN: %clang -target arm-none-gnueabi -mstrict-align -munaligned-access -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-UNALIGNED < %t %s
 
-// RUN: %clang -target arm-none-gnueeabi -mno-unaligned-access -munaligned-access -### %s 2> %t
+// RUN: %clang -target arm-none-gnueabi -mno-unaligned-access -munaligned-access -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-UNALIGNED < %t %s
 
 // CHECK-UNALIGNED: "-backend-option" "-arm-no-strict-align"
 
 
-// RUN: %clang -target arm-none-gnueeabi -mno-unaligned-access -### %s 2> %t
+// RUN: %clang -target arm-none-gnueabi -mno-unaligned-access -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-ALIGNED < %t %s
 
-// RUN: %clang -target arm-none-gnueeabi -mstrict-align -### %s 2> %t
+// RUN: %clang -target arm-none-gnueabi -mstrict-align -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-ALIGNED < %t %s
 
 // RUN: %clang -target arm-none-gnueabi -munaligned-access -mno-unaligned-access -### %s 2> %t
