0b7fc15e7035276e2f8a778ebe15f4cad9007c99
John Thompson <John.Thompson.JTSoftware@gmail.com>
2013-09-03 18:38:32 +0000

    Undo r189834 - broke test.
    llvm-svn: 189835

--- clang-tools-extra/test/modularize/Inputs/TemplateClasses.h
+++ clang-tools-extra/test/modularize/Inputs/TemplateClasses.h
@@ -1,16 +1,15 @@
 // Exercise some template issues.  Should not produce errors.
 
 // Forward declaration.
-template <class T> class TemplateClass;
+template<class T> class TemplateClass;
 
 // Full declaration.
-template <class T> class TemplateClass {
+template<class T>class TemplateClass {
 public:
   TemplateClass() {}
-
 private:
   T Member;
 };
 
 // Template alias.
-template <class T> using TemplateClassAlias = TemplateClass<T>;
+template<class T> using TemplateClassAlias = TemplateClass<T>;
