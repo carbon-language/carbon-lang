4313451ba4a9e70537728e864256ec26b8a39779
Rui Ueyama <ruiu@google.com>
2013-09-03 00:50:22 +0000

    Resolver: Do not use assert() to notify user input error.
    llvm-svn: 189775

--- lld/lib/Core/Resolver.cpp
+++ lld/lib/Core/Resolver.cpp
@@ -293,7 +293,11 @@ void Resolver::deadStripOptimize() {
   // Or, use list of names that are dead stip roots.
   for (const StringRef &name : _context.deadStripRoots()) {
     const Atom *symAtom = _symbolTable.findByName(name);
-    assert(symAtom->definition() != Atom::definitionUndefined);
+    if (symAtom->definition() == Atom::definitionUndefined) {
+      llvm::errs() << "Dead strip root '" << symAtom->name()
+                   << "' is not defined\n";
+      return;
+    }
     _deadStripRoots.insert(symAtom);
   }
 
