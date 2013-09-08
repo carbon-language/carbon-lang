fb8297668f4b5a6aea19b8b79da47a3ba090d6e3
Marshall Clow <mclow.lists@gmail.com>
2013-09-08 19:11:51 +0000

    LWG Issue 2210 (Part #2 & #3): list and forward_list
    llvm-svn: 190279

--- libcxx/test/containers/sequences/forwardlist/forwardlist.cons/size.pass.cpp
+++ libcxx/test/containers/sequences/forwardlist/forwardlist.cons/size.pass.cpp
@@ -10,12 +10,24 @@
 // <forward_list>
 
 // explicit forward_list(size_type n);
+// explicit forward_list(size_type n, const Alloc& a);
 
 #include <forward_list>
 #include <cassert>
 
 #include "../../../DefaultOnly.h"
 #include "../../../min_allocator.h"
+
+template <class T, class Allocator>
+void check_allocator(unsigned n, Allocator const &alloc = Allocator())
+{
+#if _LIBCPP_STD_VER > 11
+    typedef std::forward_list<T, Allocator> C;
+    C d(n, alloc);
+    assert(d.get_allocator() == alloc);
+    assert(std::distance(l.begin(), l.end()) == 3);
+#endif
+}
 
 int main()
 {
@@ -47,6 +59,8 @@ int main()
             ;
 #endif
         assert(n == N);
+        check_allocator<T, min_allocator<T>> ( 0 );
+        check_allocator<T, min_allocator<T>> ( 3 );
     }
 #endif
 }
--- libcxx/test/containers/sequences/list/list.cons/size_type.pass.cpp
+++ libcxx/test/containers/sequences/list/list.cons/size_type.pass.cpp
@@ -17,6 +17,23 @@
 #include "../../../stack_allocator.h"
 #include "../../../min_allocator.h"
 
+template <class T, class Allocator>
+void
+test3(unsigned n, Allocator const &alloc = Allocator())
+{
+#if _LIBCPP_STD_VER > 11
+    typedef std::list<T, Allocator> C;
+    typedef typename C::const_iterator const_iterator;
+    {
+    C d(n, alloc);
+    assert(d.size() == n);
+    assert(std::distance(d.begin(), d.end()) == 3);
+    assert(d.get_allocator() == alloc);
+    }
+#endif
+}
+
+
 int main()
 {
     {
@@ -41,6 +58,21 @@ int main()
         ++i;
         assert(*i == 0);
     }
+#if _LIBCPP_STD_VER > 11
+    {
+    	typedef std::list<int, min_allocator<int> > C;
+        C l(3, min_allocator<int> ());
+        assert(l.size() == 3);
+        assert(std::distance(l.begin(), l.end()) == 3);
+        C::const_iterator i = l.begin();
+        assert(*i == 0);
+        ++i;
+        assert(*i == 0);
+        ++i;
+        assert(*i == 0);
+        test3<int, min_allocator<int>> (3);
+    }
+#endif
 #ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     {
         std::list<DefaultOnly> l(3);
