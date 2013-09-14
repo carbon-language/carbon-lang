a369cb6280710f25b57ae63d106d46c6d89c9297
Marshall Clow <mclow.lists@gmail.com>
2013-09-14 00:47:59 +0000

    LWG Issue 2210 (Part #7): vector and vector<bool>
    llvm-svn: 190736

--- libcxx/test/containers/sequences/vector.bool/construct_size.pass.cpp
+++ libcxx/test/containers/sequences/vector.bool/construct_size.pass.cpp
@@ -16,10 +16,27 @@
 #include <cassert>
 
 #include "../../min_allocator.h"
+#include "../../test_allocator.h"
 
 template <class C>
 void
-test(typename C::size_type n)
+test2(typename C::size_type n, typename C::allocator_type const& a = typename C::allocator_type ())
+{
+#if _LIBCPP_STD_VER > 11
+    C c(n, a);
+    assert(c.__invariants());
+    assert(c.size() == n);
+    assert(c.get_allocator() == a);
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
+    for (typename C::const_iterator i = c.cbegin(), e = c.cend(); i != e; ++i)
+        assert(*i == typename C::value_type());
+#endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
+#endif
+}
+
+template <class C>
+void
+test1(typename C::size_type n)
 {
     C c(n);
     assert(c.__invariants());
@@ -29,10 +46,19 @@ test(typename C::size_type n)
         assert(*i == typename C::value_type());
 }
 
+template <class C>
+void
+test(typename C::size_type n)
+{
+    test1<C> ( n );
+    test2<C> ( n );
+}
+
 int main()
 {
     test<std::vector<bool> >(50);
 #if __cplusplus >= 201103L
     test<std::vector<bool, min_allocator<bool>> >(50);
+    test2<std::vector<bool, test_allocator<bool>> >( 100, test_allocator<bool>(23));
 #endif
 }
--- libcxx/test/containers/sequences/vector/vector.cons/construct_size.pass.cpp
+++ libcxx/test/containers/sequences/vector/vector.cons/construct_size.pass.cpp
@@ -16,10 +16,27 @@
 
 #include "../../../DefaultOnly.h"
 #include "../../../min_allocator.h"
+#include "../../../test_allocator.h"
 
 template <class C>
 void
-test(typename C::size_type n)
+test2(typename C::size_type n, typename C::allocator_type const& a = typename C::allocator_type ())
+{
+#if _LIBCPP_STD_VER > 11
+    C c(n, a);
+    assert(c.__invariants());
+    assert(c.size() == n);
+    assert(c.get_allocator() == a);
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
+    for (typename C::const_iterator i = c.cbegin(), e = c.cend(); i != e; ++i)
+        assert(*i == typename C::value_type());
+#endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
+#endif
+}
+
+template <class C>
+void
+test1(typename C::size_type n)
 {
     C c(n);
     assert(c.__invariants());
@@ -31,6 +48,14 @@ test(typename C::size_type n)
 #endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
 }
 
+template <class C>
+void
+test(typename C::size_type n)
+{
+    test1<C> ( n );
+    test2<C> ( n );
+}
+
 int main()
 {
     test<std::vector<int> >(50);
@@ -39,6 +64,7 @@ int main()
 #if __cplusplus >= 201103L
     test<std::vector<int, min_allocator<int>> >(50);
     test<std::vector<DefaultOnly, min_allocator<DefaultOnly>> >(500);
+    test2<std::vector<DefaultOnly, test_allocator<DefaultOnly>> >( 100, test_allocator<DefaultOnly>(23));
     assert(DefaultOnly::count == 0);
 #endif
 }
