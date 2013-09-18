0315f38a92fe55dcb5a6070d6b80a414f1791724
Adrian Prantl <aprantl@apple.com>
2013-09-18 22:08:57 +0000

    Debug info: Explicitly set the DIVariable::isIndirect() flag for VLAs. This allows us to get rid of an ugly hack in the backend. Paired commit with LLVM.
    llvm-svn: 190960

--- clang/test/CodeGen/debug-info-vla.c
+++ clang/test/CodeGen/debug-info-vla.c
@@ -2,7 +2,7 @@
 
 void testVLAwithSize(int s)
 {
-// CHECK: metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"vla", metadata {{.*}}, i32 [[@LINE+1]], metadata {{.*}}, i32 0, i32 0} ; [ DW_TAG_auto_variable ] [vla] [line [[@LINE+1]]]
+// CHECK: metadata !{i32 {{.*}}, metadata {{.*}}, metadata !"vla", metadata {{.*}}, i32 [[@LINE+1]], metadata {{.*}}, i32 8192, i32 0} ; [ DW_TAG_auto_variable ] [vla] [line [[@LINE+1]]]
   int vla[s];
   int i;
   for (i = 0; i < s; i++) {
