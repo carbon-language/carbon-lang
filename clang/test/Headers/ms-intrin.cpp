5cc15addbec0797e8c8e2b8dc099415762b4b8de
Reid Kleckner <reid@kleckner.net>
2013-09-19 00:32:11 +0000

    Avoid including <stdlib.h> in the intrin.h test
    llvm-svn: 190979

--- clang/test/Headers/ms-intrin.cpp
+++ clang/test/Headers/ms-intrin.cpp
@@ -1,5 +1,10 @@
 // RUN: %clang -target i386-pc-win32 -fms-extensions -fsyntax-only %s
 
+// Get size_t, but avoid including mm_malloc.h which includes stdlib.h which may
+// not exist.
+#include <stdint.h>
+#undef __STDC_HOSTED__
+
 #include <Intrin.h>
 
 template <typename T>
