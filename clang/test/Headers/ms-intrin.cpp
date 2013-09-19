2f7fe8acb32a800e0329f0baf1240419bbb29e06
Reid Kleckner <reid@kleckner.net>
2013-09-19 01:54:40 +0000

    Use -ffreestanding in ms-intrin.cpp and define size_t manually.
    llvm-svn: 190985

--- clang/test/Headers/ms-intrin.cpp
+++ clang/test/Headers/ms-intrin.cpp
@@ -1,11 +1,11 @@
-// RUN: %clang -target i386-pc-win32 -fms-extensions -fsyntax-only %s
+// RUN: %clang -target i386-pc-win32 -fms-extensions -ffreestanding -fsyntax-only %s
 
-// Get size_t, but avoid including mm_malloc.h which includes stdlib.h which may
-// not exist.
-#include <stdint.h>
-#undef __STDC_HOSTED__
+// Intrin.h needs size_t, but -ffreestanding prevents us from getting it from
+// stddef.h.  Work around it with this typedef.
+typedef __SIZE_TYPE__ size_t;
 
 #include <Intrin.h>
 
+// Use some C++ to make sure we closed the extern "C" brackets.
 template <typename T>
 void foo(T V) {}
