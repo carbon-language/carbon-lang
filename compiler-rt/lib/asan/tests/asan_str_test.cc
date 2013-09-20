bff7e64a3fadc08532ee2b5e00a37fe145f59896
Alexander Potapenko <glider@google.com>
2013-09-20 11:03:27 +0000

    [ASan] Fix compilation of asan_str_test.cc on non-Darwin.
    llvm-svn: 191083

--- compiler-rt/lib/asan/tests/asan_str_test.cc
+++ compiler-rt/lib/asan/tests/asan_str_test.cc
@@ -12,7 +12,9 @@
 //===----------------------------------------------------------------------===//
 #include "asan_test_utils.h"
 
-#include <AvailabilityMacros.h>
+#if defined(__APPLE__)
+#include <AvailabilityMacros.h>  // For MAC_OS_X_VERSION_*
+#endif
 
 // Used for string functions tests
 static char global_string[] = "global";
