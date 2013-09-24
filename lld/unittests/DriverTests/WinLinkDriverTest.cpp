0e08d7757b96e19908c774c08750cfc8994c970f
Rui Ueyama <ruiu@google.com>
2013-09-24 04:04:00 +0000

    [PECOFF] Ignore /pdbaltpath option.
    /PDBALTPATH:<path> is an option to embed a different path for the PDB file to
    the binary than the actual PDB file location. Because we don't support PDB
    file, we'll just ignore the option for now.
    
    llvm-svn: 191273

--- lld/unittests/DriverTests/WinLinkDriverTest.cpp
+++ lld/unittests/DriverTests/WinLinkDriverTest.cpp
@@ -364,7 +364,8 @@ TEST_F(WinLinkParserTest, Ignore) {
   // compatibility with link.exe.
   EXPECT_FALSE(parse("link.exe", "/nologo", "/errorreport:prompt",
                      "/incremental", "/incremental:no", "/delay:unload",
-                     "/delayload:user32", "/pdb:foo", "a.obj", nullptr));
+                     "/delayload:user32", "/pdb:foo", "/pdbaltpath:bar",
+                     "a.obj", nullptr));
   EXPECT_EQ("", errorMessage());
   EXPECT_EQ(1, inputFileCount());
   EXPECT_EQ("a.obj", inputFile(0));
