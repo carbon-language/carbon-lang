96a051367be3482097b5cf862b003bb7da03ca7b
Rafael Espindola <rafael.espindola@gmail.com>
2013-09-24 13:37:21 +0000

    Use a valid option (-msse) for testing QA_OVERRIDE_GCC3_OPTIONS.
    llvm-svn: 191300

--- clang/test/Driver/qa_override.c
+++ clang/test/Driver/qa_override.c
@@ -1,13 +1,12 @@
 // RUN: env QA_OVERRIDE_GCC3_OPTIONS="#+-Os +-Oz +-O +-O3 +-Oignore +a +b +c xb Xa Omagic ^-###  " %clang -target x86_64-apple-darwin %s -O2 b -O3 2>&1 | FileCheck %s
-// RUN: env QA_OVERRIDE_GCC3_OPTIONS="x-Werror +-mfoo" %clang -target x86_64-apple-darwin -Werror %s -c -### 2>&1 | FileCheck %s -check-prefix=RM-WERROR
+// RUN: env QA_OVERRIDE_GCC3_OPTIONS="x-Werror +-msse" %clang -target x86_64-apple-darwin -Werror %s -c -### 2>&1 | FileCheck %s -check-prefix=RM-WERROR
 
 // CHECK: "-cc1"
 // CHECK-NOT: "-Oignore"
 // CHECK: "-Omagic"
 // CHECK-NOT: "-Oignore"
 
-// RM-WERROR: ### QA_OVERRIDE_GCC3_OPTIONS: x-Werror +-mfoo
+// RM-WERROR: ### QA_OVERRIDE_GCC3_OPTIONS: x-Werror +-msse
 // RM-WERROR-NEXT: ### Deleting argument -Werror
-// RM-WERROR-NEXT: ### Adding argument -mfoo at end
-// RM-WERROR: warning: argument unused during compilation: '-mfoo'
+// RM-WERROR-NEXT: ### Adding argument -msse at end
 // RM-WERROR-NOT: "-Werror"
