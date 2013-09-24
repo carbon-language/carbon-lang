4f3f5ade5467cd9d8b2ba0f37489f1b8ea8fcee3
Rafael Espindola <rafael.espindola@gmail.com>
2013-09-24 19:35:01 +0000

    Produce an error for unknown -m options.
    llvm-svn: 191328

--- clang/test/Driver/unknown-arg.c
+++ clang/test/Driver/unknown-arg.c
@@ -1,14 +1,14 @@
-// RUN: not %clang_cc1 %s -cake-is-lie -%0 -%d -HHHH 2>&1 | \
+// RUN: not %clang_cc1 %s -cake-is-lie -%0 -%d -HHHH -munknown-to-clang-option 2>&1 | \
 // RUN: FileCheck %s
 
 // CHECK: unknown argument: '-cake-is-lie'
 // CHECK: unknown argument: '-%0'
 // CHECK: unknown argument: '-%d'
 // CHECK: unknown argument: '-HHHH'
+// CHECK: unknown argument: '-munknown-to-clang-option'
 
 
-// RUN: %clang -S %s -o %t.s -funknown-to-clang-option -Wunknown-to-clang-option -munknown-to-clang-option 2>&1 | FileCheck --check-prefix=IGNORED %s
+// RUN: %clang -S %s -o %t.s -funknown-to-clang-option -Wunknown-to-clang-option 2>&1 | FileCheck --check-prefix=IGNORED %s
 
 // IGNORED: warning: argument unused during compilation: '-funknown-to-clang-option'
-// IGNORED: warning: argument unused during compilation: '-munknown-to-clang-option'
 // IGNORED: warning: unknown warning option '-Wunknown-to-clang-option'
