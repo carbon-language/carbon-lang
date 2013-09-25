e5545ce577b5012a4a9f46505cb9f125f45ce022
Joerg Sonnenberger <joerg@bec.de>
2013-09-25 17:57:20 +0000

    editline headers are under readline/ for NetBSD.
    llvm-svn: 191389

--- lldb/tools/driver/IOChannel.h
+++ lldb/tools/driver/IOChannel.h
@@ -13,7 +13,7 @@
 #include <string>
 #include <queue>
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__)
 #include <readline/readline.h>
 #else
 #include <editline/readline.h>
