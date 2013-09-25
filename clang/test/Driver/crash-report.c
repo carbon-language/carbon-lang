cbbe1d44546db52c71c9a2b18f85b87ae82df9e7
Rafael Espindola <rafael.espindola@gmail.com>
2013-09-25 15:47:05 +0000

    Use -Xclang to pass -internal-isystem and -internal-externc-isystem to -cc1.
    They are cc1 options only, so the driver was ignoring them.
    
    llvm-svn: 191379

--- clang/test/Driver/crash-report.c
+++ clang/test/Driver/crash-report.c
@@ -3,7 +3,8 @@
 // RUN: not env TMPDIR=%t TEMP=%t TMP=%t RC_DEBUG_OPTIONS=1 %clang -fsyntax-only %s \
 // RUN:  -F/tmp/ -I /tmp/ -idirafter /tmp/ -iquote /tmp/ -isystem /tmp/ \
 // RUN:  -iprefix /the/prefix -iwithprefix /tmp -iwithprefixbefore /tmp/ \
-// RUN:  -internal-isystem /tmp/ -internal-externc-isystem /tmp/ \
+// RUN:  -Xclang -internal-isystem -Xclang /tmp/                         \
+// RUN:  -Xclang -internal-externc-isystem -Xclang /tmp/                 \
 // RUN:  -DFOO=BAR 2>&1 | FileCheck %s
 // RUN: cat %t/crash-report-*.c | FileCheck --check-prefix=CHECKSRC %s
 // RUN: cat %t/crash-report-*.sh | FileCheck --check-prefix=CHECKSH %s
