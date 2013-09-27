61f71c39033d6694937b675878220429f8b3968d
Craig Topper <craig.topper@gmail.com>
2013-09-27 03:57:18 +0000

    Remove some stray underscores from copyright block. Fix first line length to match length of the one after the copyright block.
    llvm-svn: 191483

--- clang/lib/Headers/f16cintrin.h
+++ clang/lib/Headers/f16cintrin.h
@@ -1,6 +1,6 @@
-/*===---- f16cintrin.h - F16C intrinsics ---------------------------------===
+/*===---- f16cintrin.h - F16C intrinsics -----------------------------------===
  *
- * Permission is hereby granted, free of charge, to any person obtaining __a copy
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
