3fddccfa43e8c2904da810794bd2dd0d8085e30a
NAKAMURA Takumi <geek4civic@gmail.com>
2013-09-28 01:35:07 +0000

    MipsMachineFunction.cpp: Add missing #include <raw_ostream.h>
    llvm-svn: 191597

--- llvm/lib/Target/Mips/MipsMachineFunction.cpp
+++ llvm/lib/Target/Mips/MipsMachineFunction.cpp
@@ -15,6 +15,7 @@
 #include "llvm/CodeGen/MachineRegisterInfo.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
