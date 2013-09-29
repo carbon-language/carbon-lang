98b8e1837901a433485fa1a62c8db99d08716a3b
Faisal Vali <faisalv@yahoo.com>
2013-09-29 20:27:06 +0000

    Revert the linkage fix. I got a bunch of buildbot failures that i don't understand - sorry.
    llvm-svn: 191647

--- clang/test/SemaCXX/lambda-expressions.cpp
+++ clang/test/SemaCXX/lambda-expressions.cpp
@@ -265,21 +265,3 @@ namespace TypeDeduction {
 #endif
   }
 }
-
-
-namespace lambdas_in_NSDMIs {
-  template<class T>
-  struct L {
-      T t{};
-      T t2 = ([](int a) { return [](int b) { return b; };})(t)(t);    
-  };
-  L<int> l; 
-  
-  namespace non_template {
-    struct L {
-      int t = 0;
-      int t2 = ([](int a) { return [](int b) { return b; };})(t)(t);    
-    };
-    L l; 
-  }
-}
