0f01d4e3090cc8ddeee1083c227c460e591af1f2
Benjamin Kramer <benny.kra@googlemail.com>
2013-09-29 11:29:20 +0000

    Plug a memory leak in a unit test. Stack allocation is sufficient here.
    llvm-svn: 191638

--- llvm/unittests/CodeGen/DIEHashTest.cpp
+++ llvm/unittests/CodeGen/DIEHashTest.cpp
@@ -19,11 +19,10 @@ namespace {
 using namespace llvm;
 TEST(DIEHashData1Test, DIEHash) {
   DIEHash Hash;
-  DIE *Die = new DIE(dwarf::DW_TAG_base_type);
-  DIEValue *Size = new DIEInteger(4);
-  Die->addValue(dwarf::DW_AT_byte_size, dwarf::DW_FORM_data1, Size);
-  uint64_t MD5Res = Hash.computeTypeSignature(Die);
+  DIE Die(dwarf::DW_TAG_base_type);
+  DIEInteger Size(4);
+  Die.addValue(dwarf::DW_AT_byte_size, dwarf::DW_FORM_data1, &Size);
+  uint64_t MD5Res = Hash.computeTypeSignature(&Die);
   ASSERT_TRUE(MD5Res == 0x540e9ff30ade3e4aULL);
-  delete Die;
 }
 }
