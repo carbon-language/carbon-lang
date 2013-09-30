0e1dd8c176e5d7d8f0c4aaeb585802850251939c
Tilmann Scheller <tilmann.scheller@googlemail.com>
2013-09-30 17:31:26 +0000

    [ARM] Use FileCheck instead of grep for ARM LDRD negative tests.
    llvm-svn: 191683

--- llvm/test/MC/ARM/arm-ldrd.s
+++ llvm/test/MC/ARM/arm-ldrd.s
@@ -1,34 +1,57 @@
-// RUN: not llvm-mc -arch arm -mattr=+v5te \
-// RUN: < %s >/dev/null 2> %t
-// RUN: grep "error: Rt must be even-numbered" %t | count 7
-// RUN: grep "error: Rt can't be R14" %t | count 7
-// RUN: grep "error: destination operands must be sequential" %t | count 7
-// RUN: grep "error: base register needs to be different from destination registers" %t | count 4
+// RUN: not llvm-mc -arch arm -mattr=+v5te %s 2>&1 | FileCheck %s
+//
 // rdar://14479793
 
 ldrd r1, r2, [pc, #0]
-ldrd lr, pc, [pc, #0]
-ldrd r0, r3, [pc, #0]
 ldrd r1, r2, [r3, #4]
-ldrd lr, pc, [r3, #4]
-ldrd r0, r3, [r4, #4]
 ldrd r1, r2, [r3], #4
-ldrd lr, pc, [r3], #4
-ldrd r0, r3, [r4], #4
 ldrd r1, r2, [r3, #4]!
-ldrd lr, pc, [r3, #4]!
-ldrd r0, r3, [r4, #4]!
 ldrd r1, r2, [r3, -r4]!
-ldrd lr, pc, [r3, -r4]!
-ldrd r0, r3, [r4, -r5]!
 ldrd r1, r2, [r3, r4]
-ldrd lr, pc, [r3, r4]
-ldrd r0, r3, [r4, r5]
 ldrd r1, r2, [r3], r4
-ldrd lr, pc, [r3], r4
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+// CHECK: error: Rt must be even-numbered
+
+ldrd r0, r3, [pc, #0]
+ldrd r0, r3, [r4, #4]
+ldrd r0, r3, [r4], #4
+ldrd r0, r3, [r4, #4]!
+ldrd r0, r3, [r4, -r5]!
+ldrd r0, r3, [r4, r5]
 ldrd r0, r3, [r4], r5
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+// CHECK: error: destination operands must be sequential
+
+ldrd lr, pc, [pc, #0]
+ldrd lr, pc, [r3, #4]
+ldrd lr, pc, [r3], #4
+ldrd lr, pc, [r3, #4]!
+ldrd lr, pc, [r3, -r4]!
+ldrd lr, pc, [r3, r4]
+ldrd lr, pc, [r3], r4
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
+// CHECK: error: Rt can't be R14
 
 ldrd r0, r1, [r0], #4
 ldrd r0, r1, [r1], #4
 ldrd r0, r1, [r0, #4]!
 ldrd r0, r1, [r1, #4]!
+// CHECK: error: base register needs to be different from destination registers
+// CHECK: error: base register needs to be different from destination registers
+// CHECK: error: base register needs to be different from destination registers
+// CHECK: error: base register needs to be different from destination registers
