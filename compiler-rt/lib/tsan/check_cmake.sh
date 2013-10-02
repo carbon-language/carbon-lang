8358c414777075972af342cf7f9f5f8ae222a36d
Dmitry Vyukov <dvyukov@google.com>
2013-10-02 12:57:43 +0000

    tsan: use ninja instead of make (faster and less output)
    llvm-svn: 191819

--- compiler-rt/lib/tsan/check_cmake.sh
+++ compiler-rt/lib/tsan/check_cmake.sh
@@ -5,9 +5,9 @@ set -e
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 mkdir -p $ROOT/build
 cd $ROOT/build
-CC=clang CXX=clang++ cmake -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
-make -j64
-make check-sanitizer -j64
-make check-tsan -j64
-make check-asan -j64
-make check-msan -j64
+CC=clang CXX=clang++ cmake -G Ninja -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
+ninja
+ninja check-sanitizer
+ninja check-tsan
+ninja check-asan
+ninja check-msan
