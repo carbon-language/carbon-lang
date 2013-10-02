f60d8ae15544d1fd59b2c9241f6387d66489780d
Nick Lewycky <nicholas@mxc.ca>
2013-10-02 05:05:28 +0000

    This fixme is resolved, fix the test.
    llvm-svn: 191810

--- compiler-rt/lib/ubsan/lit_tests/Misc/bool.cpp
+++ compiler-rt/lib/ubsan/lit_tests/Misc/bool.cpp
@@ -5,7 +5,6 @@ unsigned char NotABool = 123;
 int main(int argc, char **argv) {
   bool *p = (bool*)&NotABool;
 
-  // FIXME: Provide a better source location here.
-  // CHECK: bool.exe:0x{{[0-9a-f]*}}: runtime error: load of value 123, which is not a valid value for type 'bool'
+  // CHECK: bool.cpp:9:10: runtime error: load of value 123, which is not a valid value for type 'bool'
   return *p;
 }
