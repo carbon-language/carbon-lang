6b186a610b9c7ab8d17d2773354de59f57a46dda
Rafael Espindola <rafael.espindola@gmail.com>
2013-10-03 19:18:22 +0000

    Fix test.
    With -### clang return 0, so we have to use FileCheck to see if there was an
    error.
    
    llvm-svn: 191923

--- clang/test/Driver/x86_features.c
+++ clang/test/Driver/x86_features.c
@@ -3,4 +3,5 @@
 // Note that we filter out all but the last -m(no)sse.
 
 // Test that we don't produce an error with -mieee-fp.
-// RUN: %clang -### %s -mieee-fp -S
+// RUN: %clang -### %s -mieee-fp -S 2>&1 | FileCheck --check-prefix=IEEE %s
+// IEEE-NOT: error: unknown argument
