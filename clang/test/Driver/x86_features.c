72f55d46c8873a76d9070ca200eeede6aca64d7c
Rafael Espindola <rafael.espindola@gmail.com>
2013-10-03 17:06:43 +0000

    Ignore -mieee-fp.
    It looks like clang always produce code with the ieee comparisons, so it is
    safe to ignore this flag (we still error on -mno-ieee-fp).
    
    llvm-svn: 191912

--- clang/test/Driver/x86_features.c
+++ clang/test/Driver/x86_features.c
@@ -1,3 +1,6 @@
 // RUN: %clang -target i386-unknown-unknown -### -S %s -msse -msse4 -mno-sse -mno-mmx -msse  2>&1 | FileCheck %s
 // CHECK: "pentium4" "-target-feature" "+sse4" "-target-feature" "-mmx" "-target-feature" "+sse"
 // Note that we filter out all but the last -m(no)sse.
+
+// Test that we don't produce an error with -mieee-fp.
+// RUN: %clang -S %s -mieee-fp
