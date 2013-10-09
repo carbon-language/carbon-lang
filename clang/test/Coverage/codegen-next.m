f16acbbad38bac3136a0bc31990df7874449d6ee
NAKAMURA Takumi <geek4civic@gmail.com>
2013-10-09 09:45:27 +0000

    clang/test/Coverage/codegen-next.m: Exclude this for targeting x86_64-win32.
    llvm-svn: 192286

--- clang/test/Coverage/codegen-next.m
+++ clang/test/Coverage/codegen-next.m
@@ -1,4 +1,12 @@
 // RUN: %clang_cc1 -emit-llvm -fobjc-exceptions -o %t %s
 // RUN: %clang_cc1 -g -emit-llvm -fobjc-exceptions -o %t %s
 
+// An error could be seen for targeting x86_64-win32;
+//
+//     Bitcast requires types of same width
+//       %7 = bitcast i32 %6 to i64
+//
+// Then, This test should be excluded on x86_64-win32.
+// REQUIRES: LP64
+
 #include "objc-language-features.inc"
