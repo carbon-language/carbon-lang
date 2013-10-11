81034928c68332bef68a1735aa35258599c76e06
Craig Topper <craig.topper@gmail.com>
2013-10-11 06:59:57 +0000

    Remove another unnecessary filter from the disassembler.
    llvm-svn: 192425

--- llvm/utils/TableGen/X86RecognizableInstr.cpp
+++ llvm/utils/TableGen/X86RecognizableInstr.cpp
@@ -531,9 +531,6 @@ RecognizableInstr::filter_ret RecognizableInstr::filter() const {
 
   if (Name.find("MOV") != Name.npos && Name.find("r0") != Name.npos)
     return FILTER_WEAK;
-  if (Name.find("MOVZ") != Name.npos && Name.find("MOVZX") == Name.npos &&
-      Name != "MOVZPQILo2PQIrr")
-    return FILTER_WEAK;
   if (Name.find("Fs") != Name.npos)
     return FILTER_WEAK;
   if (Name == "PUSH64i16"         ||
