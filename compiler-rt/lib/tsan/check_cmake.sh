d534fff6af256af414c19974982a42bab0ccd2c6
Dmitry Vyukov <dvyukov@google.com>
2013-10-15 12:54:50 +0000

    tsan: do not build llvm from scratch on each presubmit
    llvm-svn: 192694

--- compiler-rt/lib/tsan/check_cmake.sh
+++ compiler-rt/lib/tsan/check_cmake.sh
@@ -3,9 +3,13 @@ set -u
 set -e
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-mkdir -p $ROOT/build
-cd $ROOT/build
-CC=clang CXX=clang++ cmake -G Ninja -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
+if [ -d "$ROOT/build" ]; then
+  cd $ROOT/build
+else
+  mkdir -p $ROOT/build
+  cd $ROOT/build
+  CC=clang CXX=clang++ cmake -G Ninja -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
+fi
 ninja
 ninja check-sanitizer
 ninja check-tsan
