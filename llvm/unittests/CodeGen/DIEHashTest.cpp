5980419379315158ddca45c302dd7574c8fa0f32
David Blaikie <dblaikie@gmail.com>
2013-10-15 23:00:17 +0000

    Use ASSERT_EQ rather than ASSERT_TRUE for better unit test failures.
    Also minor using namespace move so it's not hard-up against the function
    definition and outside the namespace as is usual.
    
    llvm-svn: 192744

--- llvm/unittests/CodeGen/DIEHashTest.cpp
+++ llvm/unittests/CodeGen/DIEHashTest.cpp
@@ -14,15 +14,15 @@
 #include "llvm/Support/Format.h"
 #include "gtest/gtest.h"
 
-namespace {
-
 using namespace llvm;
+
+namespace {
 TEST(DIEHashData1Test, DIEHash) {
   DIEHash Hash;
   DIE Die(dwarf::DW_TAG_base_type);
   DIEInteger Size(4);
   Die.addValue(dwarf::DW_AT_byte_size, dwarf::DW_FORM_data1, &Size);
   uint64_t MD5Res = Hash.computeTypeSignature(&Die);
-  ASSERT_TRUE(MD5Res == 0x540e9ff30ade3e4aULL);
+  ASSERT_EQ(MD5Res, 0x540e9ff30ade3e4aULL);
 }
 }
