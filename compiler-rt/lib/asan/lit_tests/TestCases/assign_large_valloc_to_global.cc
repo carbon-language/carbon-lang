1f5573cca2e79dee892c184a9824a5b9123f1502
Alexander Potapenko <glider@google.com>
2013-10-17 15:48:24 +0000

    [ASan] fix assign_large_valloc_to_global.cc to use the correct header for valloc() on OSX.
    llvm-svn: 192901

--- compiler-rt/lib/asan/lit_tests/TestCases/assign_large_valloc_to_global.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/assign_large_valloc_to_global.cc
@@ -1,5 +1,9 @@
 // Make sure we don't report a leak nor hang.
 // RUN: %clangxx_asan -O3 %s -o %t && %t
+#if defined(__APPLE__)
+#include <stdlib.h>
+#else
 #include <malloc.h>
+#endif
 int *p = (int*)valloc(1 << 20);
 int main() { }
