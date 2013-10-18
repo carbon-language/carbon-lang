e2007b706e8535e3b2402b4b233872f233d0cc47
Kostya Serebryany <kcc@google.com>
2013-10-18 09:42:24 +0000

    [asan] fix a test on Mac
    llvm-svn: 192960

--- compiler-rt/lib/asan/lit_tests/TestCases/assign_large_valloc_to_global.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/assign_large_valloc_to_global.cc
@@ -1,6 +1,8 @@
 // Make sure we don't report a leak nor hang.
 // RUN: %clangxx_asan -O3 %s -o %t && %t
 #include <stdlib.h>
+#ifndef __APPLE__
 #include <malloc.h>
+#endif  // __APPLE__
 int *p = (int*)valloc(1 << 20);
 int main() { }
