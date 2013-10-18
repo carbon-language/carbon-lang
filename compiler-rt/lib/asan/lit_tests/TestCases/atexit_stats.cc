2fe16d720cc8392f457a9ebdd16b915c34a2d8cb
Alexander Potapenko <glider@google.com>
2013-10-18 14:12:15 +0000

    [ASan] fix compilation of atexit_stats.cc on OSX
    llvm-svn: 192973

--- compiler-rt/lib/asan/lit_tests/TestCases/atexit_stats.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/atexit_stats.cc
@@ -2,7 +2,9 @@
 // RUN: %clangxx_asan -O3 %s -o %t
 // RUN: ASAN_OPTIONS=atexit=1:print_stats=1 %t 2>&1 | FileCheck %s
 #include <stdlib.h>
+#if !defined(__APPLE__)
 #include <malloc.h>
+#endif
 int *p1 = (int*)malloc(900);
 int *p2 = (int*)malloc(90000);
 int *p3 = (int*)malloc(9000000);
