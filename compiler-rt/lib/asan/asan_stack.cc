8bb5d602356521e1b76a76d618456dfab150724b
Kostya Serebryany <kcc@google.com>
2013-10-18 17:46:43 +0000

    [asan] fix stack trace printing on Mac
    llvm-svn: 192990

--- compiler-rt/lib/asan/asan_stack.cc
+++ compiler-rt/lib/asan/asan_stack.cc
@@ -25,6 +25,10 @@ static bool MaybeCallAsanSymbolize(const void *pc, char *out_buffer,
 }
 
 void PrintStack(const uptr *trace, uptr size) {
+  if (!trace) {
+    Printf("<empty stack>\n");
+    return;
+  }
   StackTrace::PrintStack(trace, size, common_flags()->symbolize,
                          MaybeCallAsanSymbolize);
 }
