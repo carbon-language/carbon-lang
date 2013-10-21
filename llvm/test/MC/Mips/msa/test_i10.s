8ddad151770aa759dc9a27386c561c90dac872ab
Matheus Almeida <matheus.almeida@imgtec.com>
2013-10-21 12:56:20 +0000

    [mips][msa] Direct Object Emission support for LDI instructions.
    llvm-svn: 193081

---
+++ llvm/test/MC/Mips/msa/test_i10.s
@@ -0,0 +1,19 @@
+# RUN: llvm-mc %s -triple=mipsel-unknown-linux -show-encoding -mcpu=mips32r2 -mattr=+msa -arch=mips | FileCheck %s
+#
+# RUN: llvm-mc %s -triple=mipsel-unknown-linux -mcpu=mips32r2 -mattr=+msa -arch=mips -filetype=obj -o - | llvm-objdump -d -triple=mipsel-unknown-linux -mattr=+msa -arch=mips - | FileCheck %s -check-prefix=CHECKOBJDUMP
+#
+
+# CHECK:        ldi.b   $w8, 198                # encoding: [0x79,0x06,0x32,0x0c]
+# CHECK:        ldi.h   $w20, 313               # encoding: [0x79,0x29,0xcd,0x0c]
+# CHECK:        ldi.w   $w24, 492               # encoding: [0x79,0x4f,0x66,0x0c]
+# CHECK:        ldi.d   $w27, -180              # encoding: [0x79,0x7a,0x66,0xcc]
+
+# CHECKOBJDUMP:        ldi.b   $w8, 198
+# CHECKOBJDUMP:        ldi.h   $w20, 313
+# CHECKOBJDUMP:        ldi.w   $w24, 492
+# CHECKOBJDUMP:        ldi.d   $w27, 844
+
+                ldi.b   $w8, 198
+                ldi.h   $w20, 313
+                ldi.w   $w24, 492
+                ldi.d   $w27, -180
