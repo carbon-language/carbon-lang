89d4681b81ef23611b2ebe18f56035728111de5a
Rui Ueyama <ruiu@google.com>
2013-10-25 20:58:12 +0000

    Put missing license header.
    llvm-svn: 193435

--- lld/lib/ReaderWriter/ELF/PPC/PPCLinkingContext.cpp
+++ lld/lib/ReaderWriter/ELF/PPC/PPCLinkingContext.cpp
@@ -1,3 +1,12 @@
+//===- lib/ReaderWriter/ELF/PPC/PPCLinkingContext.cpp ---------------------===//
+//
+//                             The LLVM Linker
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "PPCLinkingContext.h"
 
 #include "lld/Core/LLVM.h"
--- lld/lib/ReaderWriter/ELF/X86/X86LinkingContext.cpp
+++ lld/lib/ReaderWriter/ELF/X86/X86LinkingContext.cpp
@@ -1,3 +1,12 @@
+//===- lib/ReaderWriter/ELF/X86/X86LinkingContext.cpp ---------------------===//
+//
+//                             The LLVM Linker
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "X86LinkingContext.h"
 
 #include "lld/Core/LLVM.h"
