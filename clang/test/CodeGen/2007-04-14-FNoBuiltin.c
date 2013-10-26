a933f94c92862db06ec6c564683012180b20762c
Alp Toker <alp@nuanti.com>
2013-10-26 15:43:55 +0000

    FileCheckize
    llvm-svn: 193474

--- clang/test/CodeGen/2007-04-14-FNoBuiltin.c
+++ clang/test/CodeGen/2007-04-14-FNoBuiltin.c
@@ -1,7 +1,10 @@
-// RUN: %clang_cc1 -emit-llvm %s -O2 -fno-builtin -o - | grep 'call.*printf'
+// RUN: %clang_cc1 -emit-llvm %s -O2 -fno-builtin -o - | FileCheck %s
 // Check that -fno-builtin is honored.
 
 extern int printf(const char*, ...);
+
+// CHECK: define void {{.*}}foo(
 void foo(const char *msg) {
-	printf("%s\n",msg);
+  // CHECK: call {{.*}}printf
+  printf("%s\n",msg);
 }
