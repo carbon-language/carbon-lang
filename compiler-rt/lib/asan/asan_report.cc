a1d048acb7b9d7ccbb825743d3ae953ccec02c74
Timur Iskhodzhanov <timurrrr@google.com>
2013-10-27 13:37:23 +0000

    Work around ASan/Win breakage by r193448
    llvm-svn: 193501

--- compiler-rt/lib/asan/asan_report.cc
+++ compiler-rt/lib/asan/asan_report.cc
@@ -551,7 +551,9 @@ class ScopedInErrorReport {
 
 static void ReportSummary(const char *error_type, StackTrace *stack) {
   AddressInfo ai;
-  if (Symbolizer::Get()->IsAvailable()) {
+  // FIXME: The symbolizer interface should be changed in order to support
+  // Windows where we don't have a symbolizer.
+  if (!SANITIZER_WINDOWS && Symbolizer::Get()->IsAvailable()) {
     // Currently, we include the first stack frame into the report summary.
     // Maybe sometimes we need to choose another frame (e.g. skip memcpy/etc).
     uptr pc = StackTrace::GetPreviousInstructionPc(stack->trace[0]);
