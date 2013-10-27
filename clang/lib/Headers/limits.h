1334d0aedf9837341eace73661007f38550e1f68
JF Bastien <jfb@google.com>
2013-10-27 19:00:49 +0000

    Define [U]LLONG_{MIN,MAX} for C++11, add tests.
    Add tests for limits.h, not just [U]LLONG_{MIN,MAX}.
    
    llvm-svn: 193506

--- clang/lib/Headers/limits.h
+++ clang/lib/Headers/limits.h
@@ -87,8 +87,10 @@
 #define CHAR_MAX __SCHAR_MAX__
 #endif
 
-/* C99 5.2.4.2.1: Added long long. */
-#if __STDC_VERSION__ >= 199901
+/* C99 5.2.4.2.1: Added long long.
+   C++11 18.3.3.2: same contents as the Standard C Library header <limits.h>.
+ */
+#if __STDC_VERSION__ >= 199901 || __cplusplus >= 201103L
 
 #undef  LLONG_MIN
 #undef  LLONG_MAX
