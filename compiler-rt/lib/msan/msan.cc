d8282087136cce39f629ba131a67ccb162b094b0
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2013-10-28 18:53:37 +0000

    [msan] Fix a typo and enable poison_in_free flag.
    llvm-svn: 193529

--- compiler-rt/lib/msan/msan.cc
+++ compiler-rt/lib/msan/msan.cc
@@ -122,7 +122,7 @@ static void ParseFlagsFromString(Flags *f, const char *str) {
   ParseFlag(str, &f->poison_heap_with_zeroes, "poison_heap_with_zeroes");
   ParseFlag(str, &f->poison_stack_with_zeroes, "poison_stack_with_zeroes");
   ParseFlag(str, &f->poison_in_malloc, "poison_in_malloc");
-  ParseFlag(str, &f->poison_in_malloc, "poison_in_free");
+  ParseFlag(str, &f->poison_in_free, "poison_in_free");
   ParseFlag(str, &f->exit_code, "exit_code");
   if (f->exit_code < 0 || f->exit_code > 127) {
     Printf("Exit code not in [0, 128) range: %d\n", f->exit_code);
