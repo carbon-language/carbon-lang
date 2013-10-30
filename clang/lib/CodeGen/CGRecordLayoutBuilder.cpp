7c55e7ef0efe02f6e4a9df5e26a8319d18fd1646
Warren Hunt <whunt@google.com>
2013-10-30 00:14:55 +0000

    Minor efficiency refactor related to 193661.  No functional change.
    llvm-svn: 193665

--- clang/lib/CodeGen/CGRecordLayoutBuilder.cpp
+++ clang/lib/CodeGen/CGRecordLayoutBuilder.cpp
@@ -201,11 +201,9 @@ public:
 }
 
 void CGRecordLayoutBuilder::Layout(const RecordDecl *D) {
-  Alignment = Types.getContext().getASTRecordLayout(D).getAlignment();
-  Packed = D->hasAttr<PackedAttr>();
   const ASTRecordLayout &Layout = Types.getContext().getASTRecordLayout(D);
-  if (Layout.getSize() % Layout.getAlignment() != 0)
-    Packed = true;
+  Alignment = Layout.getAlignment();
+  Packed = D->hasAttr<PackedAttr>() || Layout.getSize() % Alignment != 0;
 
   if (D->isUnion()) {
     LayoutUnion(D);
