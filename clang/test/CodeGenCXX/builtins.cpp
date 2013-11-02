9f6383c741321af1f5296ad6fd43494f9a37386d
NAKAMURA Takumi <geek4civic@gmail.com>
2013-11-02 02:58:42 +0000

    clang/test/CodeGenCXX/builtins.cpp: Fix for -Asserts.
      - Don't seek "entry:" It dosen't appear with -Asserts.
      - Take care that Codegen'ed Instruction(s) wouldn't have name(s) in -Asserts.
    
    llvm-svn: 193908

--- clang/test/CodeGenCXX/builtins.cpp
+++ clang/test/CodeGenCXX/builtins.cpp
@@ -21,9 +21,8 @@ long __builtin_abs(long);          // #2
 extern "C" int __builtin_abs(int); // #3
 
 int x = __builtin_abs(-2);
-// CHECK: entry:
-// CHECK-NEXT:  store i32 2, i32* @x, align 4
+// CHECK:  store i32 2, i32* @x, align 4
 
 long y = __builtin_abs(-2l);
-// CHECK: entry:
-// CHECK-NEXT:  %call = call i64 @_Z13__builtin_absl(i64 -2)
+// CHECK:  [[Y:%.+]] = call i64 @_Z13__builtin_absl(i64 -2)
+// CHECK:  store i64 [[Y]], i64* @y, align 8
