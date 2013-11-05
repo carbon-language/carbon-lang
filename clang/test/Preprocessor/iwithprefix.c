f80de76ca9af2af24cea44c0a6ad36caaa8370c6
Alp Toker <alp@nuanti.com>
2013-11-05 12:45:43 +0000

    Make test run reliably
    llc waits for input on stdin, which was not provided in this test. It
    was running only thanks to a quirk in the way lit concatenates
    commands.
    
    llvm-svn: 194071

--- clang/test/Preprocessor/iwithprefix.c
+++ clang/test/Preprocessor/iwithprefix.c
@@ -4,9 +4,8 @@
 // RUN: mkdir -p %t.tmps/first %t.tmps/second
 // RUN: %clang_cc1 -triple x86_64-unknown-unknown \
 // RUN:   -iprefix %t.tmps/ -iwithprefix second \
-// RUN:    -isystem %t.tmps/first -v 2> %t.out
-// RUN: cat %t.out
-// RUN: FileCheck < %t.out %s
+// RUN:    -isystem %t.tmps/first -v %s 2> %t.out
+// RUN: FileCheck %s < %t.out
 
 // CHECK: #include <...> search starts here:
 // CHECK: {{.*}}.tmps/first
