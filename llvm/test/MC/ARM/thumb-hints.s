f02287db274d955bf4198c6ee10b67e6ebf3985a
Tim Northover <tnorthover@apple.com>
2013-11-05 21:36:02 +0000

    ARM: permit bare dmb/dsb/isb aliases on Cortex-M0
    Cortex-M0 supports these 32-bit instructions despite being Thumb1 only
    (mostly). We knew about that but not that the aliases without the default "sy"
    operand were also permitted.
    
    llvm-svn: 194094

--- llvm/test/MC/ARM/thumb-hints.s
+++ llvm/test/MC/ARM/thumb-hints.s
@@ -17,8 +17,20 @@
 @ CHECK: wfi                            @ encoding: [0x30,0xbf]
 @ CHECK: sev                            @ encoding: [0x40,0xbf]
 
-
 @ CHECK-EVIL-PRE-UAL: mov r8, r8                     @ encoding: [0xc0,0x46]
+
+        dmb sy
+        dmb
+        dsb sy
+        dsb
+        isb sy
+        isb
+@ CHECK: dmb	sy                      @ encoding: [0xbf,0xf3,0x5f,0x8f]
+@ CHECK: dmb	sy                      @ encoding: [0xbf,0xf3,0x5f,0x8f]
+@ CHECK: dsb	sy                      @ encoding: [0xbf,0xf3,0x4f,0x8f]
+@ CHECK: dsb	sy                      @ encoding: [0xbf,0xf3,0x4f,0x8f]
+@ CHECK: isb	sy                      @ encoding: [0xbf,0xf3,0x6f,0x8f]
+@ CHECK: isb	sy                      @ encoding: [0xbf,0xf3,0x6f,0x8f]
 
 
 @ CHECK-ERROR: error: instruction requires: armv6m or armv6t2
@@ -32,3 +44,21 @@
 
 @ CHECK-ERROR: error: instruction requires: armv6m or armv6t2
 @ CHECK-ERROR-NEXT: sev
+
+@ CHECK-ERROR: error:
+@ CHECK-ERROR-NEXT: dmb sy
+
+@ CHECK-ERROR: error: instruction requires: data-barriers
+@ CHECK-ERROR-NEXT: dmb
+
+@ CHECK-ERROR: error:
+@ CHECK-ERROR-NEXT: dsb sy
+
+@ CHECK-ERROR: error: instruction requires: data-barriers
+@ CHECK-ERROR-NEXT: dsb
+
+@ CHECK-ERROR: error:
+@ CHECK-ERROR-NEXT: isb sy
+
+@ CHECK-ERROR: error: instruction requires: data-barriers
+@ CHECK-ERROR-NEXT: isb
