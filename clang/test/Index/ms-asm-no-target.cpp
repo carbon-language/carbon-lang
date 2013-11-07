72c5d911714e990b1125a2c16a2585092bfe254e
Richard Barton <richard.barton@arm.com>
2013-11-07 18:05:57 +0000

    XFAIL the test for non-x86 targets for which this message cannot be generated.
    llvm-svn: 194211

--- clang/test/Index/ms-asm-no-target.cpp
+++ clang/test/Index/ms-asm-no-target.cpp
@@ -1,3 +1,4 @@
+// XFAIL: aarch64, arm, mips, hexagon, powerpc, sparc
 // REQUIRES: x86-registered-target
 // RUN: c-index-test -test-load-source all -fasm-blocks -Wno-microsoft %s 2>&1 | FileCheck %s
 
