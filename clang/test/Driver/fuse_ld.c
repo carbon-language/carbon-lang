10737e80b03eec18fae3bd2d3ae901fa529ea0d6
David Chisnall <csdavec@swan.ac.uk>
2013-11-09 14:43:08 +0000

    Correctly set the target in fuse_ld.c correctly...
    llvm-svn: 194330

--- clang/test/Driver/fuse_ld.c
+++ clang/test/Driver/fuse_ld.c
@@ -1,17 +1,17 @@
-// RUN: %clang %s -target=x86_64-unknown-freebsd -### 2>&1 | FileCheck %s
+// RUN: %clang %s -target x86_64-unknown-freebsd -### 2>&1 | FileCheck %s
 // CHECK: ld
 // RUN: %clang -fuse-ld=bfd --sysroot=%S/Inputs/basic_freebsd_tree \
-// RUN:   -target=x86_64-unknown-freebsd \
+// RUN:   -target x86_64-unknown-freebsd \
 // RUN:   -B%S/Inputs/basic_freebsd_tree %s  -### 2>&1 | \
 // RUN:    FileCheck -check-prefix=CHECK-BFD %s
 // CHECK-BFD: ld.bfd
 // RUN: %clang -fuse-ld=gold --sysroot=%S/Inputs/basic_freebsd_tree \
-// RUN:   -target=x86_64-unknown-freebsd \
+// RUN:   -target x86_64-unknown-freebsd \
 // RUN:   -B%S/Inputs/basic_freebsd_tree %s -### 2>&1 | \
 // RUN:    FileCheck -check-prefix=CHECK-GOLD %s
 // CHECK-GOLD: ld.gold
 // RUN: %clang -fuse-ld=plib --sysroot=%S/Inputs/basic_freebsd_tree \
-// RUN:   -target=x86_64-unknown-freebsd \
+// RUN:   -target x86_64-unknown-freebsd \
 // RUN:   -B%S/Inputs/basic_freebsd_tree %s -### 2>&1 | \
 // RUN:    FileCheck -check-prefix=CHECK-PLIB %s
 // CHECK-PLIB: error: invalid linker name
