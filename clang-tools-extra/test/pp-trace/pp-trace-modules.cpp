da0b753035f28f78dc6a30b5c554b07ca50c9506
John Thompson <John.Thompson.JTSoftware@gmail.com>
2013-11-11 21:20:02 +0000

    [extra] pp-trace - Test for moduleImport.
    llvm-svn: 194422

---
+++ clang-tools-extra/test/pp-trace/pp-trace-modules.cpp
@@ -0,0 +1,11 @@
+// RUN: pp-trace -ignore FileChanged,MacroDefined %s -x objective-c++ -undef -target x86_64 -std=c++11 -fmodules -fcxx-modules -fmodules-cache-path=%t -I%S -I%S/Input | FileCheck --strict-whitespace %s
+
+@import Level1A;
+
+// CHECK: ---
+// CHECK-NEXT: - Callback: moduleImport
+// CHECK-NEXT:   ImportLoc: "{{.*}}{{[/\\]}}pp-trace-modules.cpp:3:2"
+// CHECK-NEXT:   Path: [{Name: Level1A, Loc:"{{.*}}{{[/\\]}}pp-trace-modules.cpp:3:9"}]
+// CHECK-NEXT:   Imported: Level1A
+// CHECK-NEXT: - Callback: EndOfMainFile
+// CHECK-NEXT: ...
