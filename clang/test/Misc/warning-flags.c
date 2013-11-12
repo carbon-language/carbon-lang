f8ffda089efa212f6e4542a2dcd9db325eea1e4b
Sylvestre Ledru <sylvestre@debian.org>
2013-11-12 18:07:35 +0000

    Remove trailing spaces
    llvm-svn: 194498

--- clang/test/Misc/warning-flags.c
+++ clang/test/Misc/warning-flags.c
@@ -5,16 +5,16 @@ This test serves two purposes:
 
 (1) It documents all existing warnings that currently have no associated -W flag,
     and ensures that the list never grows.
-    
+
     If take an existing warning and add a flag, this test will fail.  To
     fix this test, simply remove that warning from the list below.
-    
+
 (2) It prevents us adding new warnings to Clang that have no -W flag.  All
     new warnings should have -W flags.
-    
+
     If you add a new warning without a flag, this test will fail.  To fix
     this test, simply add a warning group to that warning.
-    
+
 
 The list of warnings below should NEVER grow.  It should gradually shrink to 0.
 
