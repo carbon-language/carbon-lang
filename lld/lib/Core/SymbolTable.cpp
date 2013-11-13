0acd8243e351d9f4c36347bd3967052871190b59
Rui Ueyama <ruiu@google.com>
2013-11-13 23:33:49 +0000

    Remove default label from fully covered switch.
    llvm-svn: 194624

--- lld/lib/Core/SymbolTable.cpp
+++ lld/lib/Core/SymbolTable.cpp
@@ -234,9 +234,6 @@ void SymbolTable::addByName(const Atom & newAtom) {
     break;
   case NCR_Error:
     llvm::errs() << "SymbolTable: error while merging " << name << "\n";
-    // FALLTHRU
-  default:
-    llvm::report_fatal_error("SymbolTable::addByName(): unhandled switch clause");
   }
 
   if (useNew) {
