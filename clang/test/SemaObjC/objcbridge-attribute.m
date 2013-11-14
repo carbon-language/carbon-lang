a0f0395b9f7dadf6ef0cffc94bd7c8c14a316e3f
Fariborz Jahanian <fjahanian@apple.com>
2013-11-14 01:00:26 +0000

    Use a more realistic NS class names in objc_bridge attributes in my previous test. Per Jordan's comment.
    llvm-svn: 194645

--- clang/test/SemaObjC/objcbridge-attribute.m
+++ clang/test/SemaObjC/objcbridge-attribute.m
@@ -1,25 +1,25 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -Wno-objc-root-class %s
 // rdar://15454846
 
-typedef struct __CFColor * __attribute__ ((objc_bridge(NSError))) CFColorRef;
+typedef struct __CFErrorRef * __attribute__ ((objc_bridge(NSError))) CFErrorRef;
 
 typedef struct __CFMyColor  * __attribute__((objc_bridge(12))) CFMyColorRef; // expected-error {{parameter of 'objc_bridge' attribute must be a single name of an Objective-C class}}
 
 typedef struct __CFArray *  __attribute__ ((objc_bridge)) CFArrayRef; // expected-error {{parameter of 'objc_bridge' attribute must be a single name of an Objective-C class}}
 
-typedef void *  __attribute__ ((objc_bridge(NSString))) CFRef; 
+typedef void *  __attribute__ ((objc_bridge(NSURL))) CFURLRef; 
 
-typedef void * CFTypeRef __attribute__ ((objc_bridge(NSError)));
+typedef void * CFStringRef __attribute__ ((objc_bridge(NSString)));
 
-typedef struct __CFLocale * __attribute__((objc_bridge(NSString, NSError))) CFLocaleRef;// expected-error {{use of undeclared identifier 'NSError'}}
+typedef struct __CFLocale * __attribute__((objc_bridge(NSLocale, NSError))) CFLocaleRef;// expected-error {{use of undeclared identifier 'NSError'}}
 
-typedef struct __CFData __attribute__((objc_bridge(NSError))) CFDataRef; // expected-error {{'objc_bridge' attribute must be applied to a pointer type}}
+typedef struct __CFData __attribute__((objc_bridge(NSData))) CFDataRef; // expected-error {{'objc_bridge' attribute must be applied to a pointer type}}
 
-typedef struct __attribute__((objc_bridge(NSError))) __CFDictionary * CFDictionaryRef; // expected-error {{'objc_bridge' attribute must be put on a typedef only}}
+typedef struct __attribute__((objc_bridge(NSDictionary))) __CFDictionary * CFDictionaryRef; // expected-error {{'objc_bridge' attribute must be put on a typedef only}}
 
-typedef struct __CFObject * CFObjectRef __attribute__((objc_bridge(NSError)));
+typedef struct __CFSetRef * CFSetRef __attribute__((objc_bridge(NSSet)));
 
-typedef union __CFUColor * __attribute__((objc_bridge(NSError))) CFUColorRef; // expected-error {{'objc_bridge' attribute only applies to structs}}
+typedef union __CFUColor * __attribute__((objc_bridge(NSUColor))) CFUColorRef; // expected-error {{'objc_bridge' attribute only applies to structs}}
 
 @interface I
 {
