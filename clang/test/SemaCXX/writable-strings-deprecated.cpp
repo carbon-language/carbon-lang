d3d5c3300e71be3a66042b3a2b5e872d911e92c5
Richard Smith <richard-llvm@metafoo.co.uk>
2013-11-14 22:22:31 +0000

    Add -Wdeprecated-writable-string in C++ to -Wc++11-compat, since it's ill-formed in C++11.
    llvm-svn: 194736

--- clang/test/SemaCXX/writable-strings-deprecated.cpp
+++ clang/test/SemaCXX/writable-strings-deprecated.cpp
@@ -1,11 +1,15 @@
 // RUN: %clang_cc1 -fsyntax-only -Wno-deprecated-writable-strings -verify %s
 // RUN: %clang_cc1 -fsyntax-only -fwritable-strings -verify %s
 // RUN: %clang_cc1 -fsyntax-only -Wno-write-strings -verify %s
+// RUN: %clang_cc1 -fsyntax-only -Werror=c++11-compat -verify %s -DERROR
 // rdar://8827606
 
 char *fun(void)
 {
    return "foo";
+#ifdef ERROR
+   // expected-error@-2 {{deprecated}}
+#endif
 }
 
 void test(bool b)
