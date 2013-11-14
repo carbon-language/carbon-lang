9410f77b9f54a4dde8c028ba2cd5a5e5aa3b7d45
Peter Zotov <whitequark@whitequark.org>
2013-11-14 06:34:21 +0000

    [OCaml] Build stub OCaml libraries for all configured targets
    This allows to only link in the needed targets, reducing binary
    size and more importantly link time.
    
    Note that this is an incomplete implementation: currently,
    LLVM does not have the plumbing which would allow to conditionally
    link in AsmPrinter, AsmParser and Disassembler for the targets
    which support them. This should be improved in the future.
    
    llvm-svn: 194670

---
+++ llvm/bindings/ocaml/backends/backend_ocaml.c
@@ -0,0 +1,37 @@
+/*===-- backend_ocaml.c - LLVM OCaml Glue -----------------------*- C++ -*-===*\
+|*                                                                            *|
+|*                     The LLVM Compiler Infrastructure                       *|
+|*                                                                            *|
+|* This file is distributed under the University of Illinois Open Source      *|
+|* License. See LICENSE.TXT for details.                                      *|
+|*                                                                            *|
+|*===----------------------------------------------------------------------===*|
+|*                                                                            *|
+|* This file glues LLVM's OCaml interface to its C interface. These functions *|
+|* are by and large transparent wrappers to the corresponding C functions.    *|
+|*                                                                            *|
+|* Note that these functions intentionally take liberties with the CAMLparamX *|
+|* macros, since most of the parameters are not GC heap objects.              *|
+|*                                                                            *|
+\*===----------------------------------------------------------------------===*/
+
+#include "llvm-c/Target.h"
+#include "caml/alloc.h"
+#include "caml/memory.h"
+
+#define INITIALIZER1(target) \
+  CAMLprim value llvm_initialize_ ## target(value Unit) {  \
+    LLVMInitialize ## target ## TargetInfo();              \
+    LLVMInitialize ## target ## Target();                  \
+    LLVMInitialize ## target ## TargetMC();                \
+    // TODO: Figure out how to call these only for targets \
+    // which support them.                                 \
+    // LLVMInitialize ## target ## AsmPrinter();           \
+    // LLVMInitialize ## target ## AsmParser();            \
+    // LLVMInitialize ## target ## Disassembler();         \
+    return Val_unit;                                       \
+  }
+
+#define INITIALIZER(target) INITIALIZER1(target)
+
+INITIALIZER(TARGET)
