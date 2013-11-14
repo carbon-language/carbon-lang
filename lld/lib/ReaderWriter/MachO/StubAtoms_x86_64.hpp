fef5d42ef3093ed5e1384c65bec80992c4ba5eab
Rui Ueyama <ruiu@google.com>
2013-11-14 19:18:52 +0000

    Fix trailing whitespace and indentation.
    llvm-svn: 194722

--- lld/lib/ReaderWriter/MachO/StubAtoms_x86_64.hpp
+++ lld/lib/ReaderWriter/MachO/StubAtoms_x86_64.hpp
@@ -29,12 +29,12 @@ namespace mach_o {
 //
 class X86_64StubAtom : public SimpleDefinedAtom {
 public:
-        X86_64StubAtom(const File &file, const Atom &lazyPointer)
-                       : SimpleDefinedAtom(file) {
-          this->addReference(KindHandler_x86_64::ripRel32, 2, &lazyPointer, 0);
-        }
+  X86_64StubAtom(const File &file, const Atom &lazyPointer)
+      : SimpleDefinedAtom(file) {
+    this->addReference(KindHandler_x86_64::ripRel32, 2, &lazyPointer, 0);
+  }
 
-  virtual ContentType contentType() const  {
+  virtual ContentType contentType() const {
     return DefinedAtom::typeStub;
   }
 
@@ -42,19 +42,17 @@ public:
     return 6;
   }
 
-  virtual ContentPermissions permissions() const  {
+  virtual ContentPermissions permissions() const {
     return DefinedAtom::permR_X;
   }
 
   virtual ArrayRef<uint8_t> rawContent() const {
     static const uint8_t instructions[] =
-              { 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 }; // jmp *lazyPointer
+        { 0xFF, 0x25, 0x00, 0x00, 0x00, 0x00 }; // jmp *lazyPointer
     assert(sizeof(instructions) == this->size());
     return makeArrayRef(instructions);
   }
-
 };
-
 
 //
 // X86_64 Stub Helper Common Atom created by the stubs pass.
@@ -62,13 +60,13 @@ public:
 class X86_64StubHelperCommonAtom : public SimpleDefinedAtom {
 public:
   X86_64StubHelperCommonAtom(const File &file, const Atom &cache,
-                                               const Atom &binder)
-  : SimpleDefinedAtom(file) {
+                             const Atom &binder)
+      : SimpleDefinedAtom(file) {
     this->addReference(KindHandler_x86_64::ripRel32, 3,  &cache, 0);
     this->addReference(KindHandler_x86_64::ripRel32, 11, &binder, 0);
   }
 
-  virtual ContentType contentType() const  {
+  virtual ContentType contentType() const {
     return DefinedAtom::typeStubHelper;
   }
 
@@ -76,23 +74,20 @@ public:
     return 16;
   }
 
-  virtual ContentPermissions permissions() const  {
+  virtual ContentPermissions permissions() const {
     return DefinedAtom::permR_X;
   }
 
   virtual ArrayRef<uint8_t> rawContent() const {
     static const uint8_t instructions[] =
-    { 0x4C, 0x8D, 0x1D, 0x00, 0x00, 0x00, 0x00,   // leaq cache(%rip),%r11
-      0x41, 0x53,                                 // push %r11
-      0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,         // jmp *binder(%rip)
-      0x90 };                                     // nop
+        { 0x4C, 0x8D, 0x1D, 0x00, 0x00, 0x00, 0x00,   // leaq cache(%rip),%r11
+          0x41, 0x53,                                 // push %r11
+          0xFF, 0x25, 0x00, 0x00, 0x00, 0x00,         // jmp *binder(%rip)
+          0x90 };                                     // nop
     assert(sizeof(instructions) == this->size());
     return makeArrayRef(instructions);
   }
-
 };
-
-
 
 //
 // X86_64 Stub Helper Atom created by the stubs pass.
@@ -100,12 +95,12 @@ public:
 class X86_64StubHelperAtom : public SimpleDefinedAtom {
 public:
   X86_64StubHelperAtom(const File &file, const Atom &helperCommon)
-  : SimpleDefinedAtom(file) {
+      : SimpleDefinedAtom(file) {
     this->addReference(KindHandler_x86_64::lazyImmediate, 1, this, 0);
     this->addReference(KindHandler_x86_64::ripRel32, 6, &helperCommon, 0);
   }
 
-  virtual ContentType contentType() const  {
+  virtual ContentType contentType() const {
     return DefinedAtom::typeStubHelper;
   }
 
@@ -113,56 +108,53 @@ public:
     return 10;
   }
 
-  virtual ContentPermissions permissions() const  {
+  virtual ContentPermissions permissions() const {
     return DefinedAtom::permR_X;
   }
 
   virtual ArrayRef<uint8_t> rawContent() const {
     static const uint8_t instructions[] =
-              { 0x68, 0x00, 0x00, 0x00, 0x00,   // pushq $lazy-info-offset
-                0xE9, 0x00, 0x00, 0x00, 0x00 }; // jmp helperhelper
+        { 0x68, 0x00, 0x00, 0x00, 0x00,   // pushq $lazy-info-offset
+          0xE9, 0x00, 0x00, 0x00, 0x00 }; // jmp helperhelper
     assert(sizeof(instructions) == this->size());
     return makeArrayRef(instructions);
   }
-
 };
-
 
 //
 // X86_64 Lazy Pointer Atom created by the stubs pass.
 //
 class X86_64LazyPointerAtom : public SimpleDefinedAtom {
 public:
-        X86_64LazyPointerAtom(const File &file, const Atom &helper,
-                                                const Atom &shlib)
-              : SimpleDefinedAtom(file) {
-                this->addReference(KindHandler_x86_64::pointer64, 0, &helper, 0);
-                this->addReference(KindHandler_x86_64::lazyTarget, 0, &shlib, 0);
-        }
+  X86_64LazyPointerAtom(const File &file, const Atom &helper,
+                        const Atom &shlib)
+      : SimpleDefinedAtom(file) {
+    this->addReference(KindHandler_x86_64::pointer64, 0, &helper, 0);
+    this->addReference(KindHandler_x86_64::lazyTarget, 0, &shlib, 0);
+  }
 
-  virtual ContentType contentType() const  {
+  virtual ContentType contentType() const {
     return DefinedAtom::typeLazyPointer;
   }
 
-  virtual Alignment alignment() const { 
-    return Alignment(3); 
+  virtual Alignment alignment() const {
+    return Alignment(3);
   }
-  
+
   virtual uint64_t size() const {
     return 8;
   }
 
-  virtual ContentPermissions permissions() const  {
+  virtual ContentPermissions permissions() const {
     return DefinedAtom::permRW_;
   }
 
   virtual ArrayRef<uint8_t> rawContent() const {
     static const uint8_t bytes[] =
-                            { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     return makeArrayRef(bytes);
   }
 };
-
 
 //
 // X86_64 NonLazy (GOT) Pointer Atom created by the stubs pass.
@@ -170,39 +162,35 @@ public:
 class X86_64NonLazyPointerAtom : public SimpleDefinedAtom {
 public:
   X86_64NonLazyPointerAtom(const File &file)
-  : SimpleDefinedAtom(file) {
-  }
+      : SimpleDefinedAtom(file) {}
 
   X86_64NonLazyPointerAtom(const File &file, const Atom &shlib)
-  : SimpleDefinedAtom(file) {
+      : SimpleDefinedAtom(file) {
     this->addReference(KindHandler_x86_64::pointer64, 0, &shlib, 0);
   }
 
-  virtual ContentType contentType() const  {
+  virtual ContentType contentType() const {
     return DefinedAtom::typeGOT;
   }
 
-  virtual Alignment alignment() const { 
-    return Alignment(3); 
+  virtual Alignment alignment() const {
+    return Alignment(3);
   }
-  
+
   virtual uint64_t size() const {
     return 8;
   }
 
-  virtual ContentPermissions permissions() const  {
+  virtual ContentPermissions permissions() const {
     return DefinedAtom::permRW_;
   }
 
   virtual ArrayRef<uint8_t> rawContent() const {
     static const uint8_t bytes[] =
-    { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
+        { 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
     return makeArrayRef(bytes);
   }
-
 };
-
-
 
 } // namespace mach_o
 } // namespace lld
