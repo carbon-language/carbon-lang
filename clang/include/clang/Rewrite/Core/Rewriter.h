02413d88f15df9766ea66829804273f64b883d28
Alp Toker <alp@nuanti.com>
2013-11-17 18:13:43 +0000

    RewriteBuffer::write(): Add a doc comment about non-portable use
    The function isn't strictly at fault but there are callers using it
    incorrectly, causing crashes with in-place edits of 64KB or larger files on
    Windows.
    
    See PR17960 for details.
    
    llvm-svn: 194972

--- clang/include/clang/Rewrite/Core/Rewriter.h
+++ clang/include/clang/Rewrite/Core/Rewriter.h
@@ -49,6 +49,9 @@ public:
 
   /// \brief Write to \p Stream the result of applying all changes to the
   /// original buffer.
+  /// Note that it isn't safe to use this function to overwrite memory mapped
+  /// files in-place (PR17960). Consider using a higher-level utility such as
+  /// Rewriter::overwriteChangedFiles() instead.
   ///
   /// The original buffer is not actually changed.
   raw_ostream &write(raw_ostream &Stream) const;
