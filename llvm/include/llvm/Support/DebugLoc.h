9c131c1f36661d9f82fd5680029fac15a1433f98
Yaron Keren <yaron.keren@gmail.com>
2013-11-17 09:47:39 +0000

    DebugLoc defines LineCol as 32 bit in comment but unsigned in code. This patch modifies LineCol to be a uint32_t.
    See http://llvm.org/bugs/show_bug.cgi?id=17957
    
    llvm-svn: 194957

--- llvm/include/llvm/Support/DebugLoc.h
+++ llvm/include/llvm/Support/DebugLoc.h
@@ -15,6 +15,8 @@
 #ifndef LLVM_SUPPORT_DEBUGLOC_H
 #define LLVM_SUPPORT_DEBUGLOC_H
 
+#include "llvm/Support/DataTypes.h"
+
 namespace llvm {
   template <typename T> struct DenseMapInfo;
   class MDNode;
@@ -45,7 +47,7 @@ namespace llvm {
     /// LineCol - This 32-bit value encodes the line and column number for the
     /// location, encoded as 24-bits for line and 8 bits for col.  A value of 0
     /// for either means unknown.
-    unsigned LineCol;
+    uint32_t LineCol;
 
     /// ScopeIdx - This is an opaque ID# for Scope/InlinedAt information,
     /// decoded by LLVMContext.  0 is unknown.
