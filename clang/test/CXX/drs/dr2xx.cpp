7101fd0e562a21ccb5223ed62b9bc406c2c27517
Richard Smith <richard-llvm@metafoo.co.uk>
2013-11-17 02:50:30 +0000

    Tests for core issues 224-240.
    llvm-svn: 194946

--- clang/test/CXX/drs/dr2xx.cpp
+++ clang/test/CXX/drs/dr2xx.cpp
@@ -217,3 +217,191 @@ namespace dr221 { // dr221: yes
 // apparently both voted into the WP and acted upon by the editor.
 
 // dr223: na
+
+namespace dr224 { // dr224: no
+  namespace example1 {
+    template <class T> class A {
+      typedef int type;
+      A::type a;
+      A<T>::type b;
+      A<T*>::type c; // expected-error {{missing 'typename'}}
+      ::dr224::example1::A<T>::type d;
+
+      class B {
+        typedef int type;
+
+        A::type a;
+        A<T>::type b;
+        A<T*>::type c; // expected-error {{missing 'typename'}}
+        ::dr224::example1::A<T>::type d;
+
+        B::type e;
+        A<T>::B::type f;
+        A<T*>::B::type g; // expected-error {{missing 'typename'}}
+        typename A<T*>::B::type h;
+      };
+    };
+
+    template <class T> class A<T*> {
+      typedef int type;
+      A<T*>::type a;
+      A<T>::type b; // expected-error {{missing 'typename'}}
+    };
+
+    template <class T1, class T2, int I> struct B {
+      typedef int type;
+      B<T1, T2, I>::type b1;
+      B<T2, T1, I>::type b2; // expected-error {{missing 'typename'}}
+
+      typedef T1 my_T1;
+      static const int my_I = I;
+      static const int my_I2 = I+0;
+      static const int my_I3 = my_I;
+      B<my_T1, T2, my_I>::type b3; // FIXME: expected-error {{missing 'typename'}}
+      B<my_T1, T2, my_I2>::type b4; // expected-error {{missing 'typename'}}
+      B<my_T1, T2, my_I3>::type b5; // FIXME: expected-error {{missing 'typename'}}
+    };
+  }
+
+  namespace example2 {
+    template <int, typename T> struct X { typedef T type; };
+    template <class T> class A {
+      static const int i = 5;
+      X<i, int>::type w; // FIXME: expected-error {{missing 'typename'}}
+      X<A::i, char>::type x; // FIXME: expected-error {{missing 'typename'}}
+      X<A<T>::i, double>::type y; // FIXME: expected-error {{missing 'typename'}}
+      X<A<T*>::i, long>::type z; // expected-error {{missing 'typename'}}
+      int f();
+    };
+    template <class T> int A<T>::f() {
+      return i;
+    }
+  }
+}
+
+// dr225: yes
+template<typename T> void dr225_f(T t) { dr225_g(t); } // expected-error {{call to function 'dr225_g' that is neither visible in the template definition nor found by argument-dependent lookup}}
+void dr225_g(int); // expected-note {{should be declared prior to the call site}}
+template void dr225_f(int); // expected-note {{in instantiation of}}
+
+namespace dr226 { // dr226: no
+  template<typename T = void> void f() {}
+#if __cplusplus < 201103L
+  // expected-error@-2 {{extension}}
+  // FIXME: This appears to be wrong: default arguments for function templates
+  // are listed as a defect (in c++98) not an extension. EDG accepts them in
+  // strict c++98 mode.
+#endif
+  template<typename T> struct S {
+    template<typename U = void> void g();
+#if __cplusplus < 201103L
+  // expected-error@-2 {{extension}}
+#endif
+    template<typename U> struct X;
+    template<typename U> void h();
+  };
+  template<typename T> template<typename U> void S<T>::g() {}
+  template<typename T> template<typename U = void> struct S<T>::X {}; // expected-error {{cannot add a default template arg}}
+  template<typename T> template<typename U = void> void S<T>::h() {} // expected-error {{cannot add a default template arg}}
+
+  template<typename> void friend_h();
+  struct A {
+    // FIXME: This is ill-formed.
+    template<typename=void> struct friend_B;
+    // FIXME: f, h, and i are ill-formed.
+    //  f is ill-formed because it is not a definition.
+    //  h and i are ill-formed because they are not the only declarations of the
+    //  function in the translation unit.
+    template<typename=void> void friend_f();
+    template<typename=void> void friend_g() {}
+    template<typename=void> void friend_h() {}
+    template<typename=void> void friend_i() {}
+#if __cplusplus < 201103L
+  // expected-error@-5 {{extension}} expected-error@-4 {{extension}}
+  // expected-error@-4 {{extension}} expected-error@-3 {{extension}}
+#endif
+  };
+  template<typename> void friend_i();
+
+  template<typename=void, typename X> void foo(X) {}
+  template<typename=void, typename X> struct Foo {}; // expected-error {{missing a default argument}} expected-note {{here}}
+#if __cplusplus < 201103L
+  // expected-error@-3 {{extension}}
+#endif
+
+  template<typename=void, typename X, typename, typename Y> int foo(X, Y);
+  template<typename, typename X, typename=void, typename Y> int foo(X, Y);
+  int x = foo(0, 0);
+#if __cplusplus < 201103L
+  // expected-error@-4 {{extension}}
+  // expected-error@-4 {{extension}}
+#endif
+}
+
+void dr227(bool b) { // dr227: yes
+  if (b)
+    int n;
+  else
+    int n;
+}
+
+namespace dr228 { // dr228: yes
+  template <class T> struct X {
+    void f();
+  };
+  template <class T> struct Y {
+    void g(X<T> x) { x.template X<T>::f(); }
+  };
+}
+
+namespace dr229 { // dr229: yes
+  template<typename T> void f();
+  template<typename T> void f<T*>() {} // expected-error {{function template partial specialization}}
+  template<> void f<int>() {}
+}
+
+namespace dr231 { // dr231: yes
+  namespace outer {
+    namespace inner {
+      int i; // expected-note {{here}}
+    }
+    void f() { using namespace inner; }
+    int j = i; // expected-error {{undeclared identifier 'i'; did you mean 'inner::i'?}}
+  }
+}
+
+// dr234: na
+// dr235: na
+
+namespace dr236 { // dr236: yes
+  void *p = int();
+#if __cplusplus < 201103L
+  // expected-warning@-2 {{null pointer}}
+#else
+  // expected-error@-4 {{cannot initialize}}
+#endif
+}
+
+namespace dr237 { // dr237: dup 470
+  template<typename T> struct A { void f() { T::error; } };
+  template<typename T> struct B : A<T> {};
+  template struct B<int>; // ok
+}
+
+namespace dr239 { // dr239: yes
+  namespace NS {
+    class T {};
+    void f(T);
+    float &g(T, int);
+  }
+  NS::T parm;
+  int &g(NS::T, float);
+  int main() {
+    f(parm);
+    float &r = g(parm, 1);
+    extern int &g(NS::T, float);
+    int &s = g(parm, 1);
+  }
+}
+
+// dr240: dup 616
