59243333ded982f8cd99d2827dbcdb1d52e9e5dc
Alexey Samsonov <samsonov@google.com>
2013-11-18 14:54:50 +0000

    [ASan] Run one of UAS tests in UAR mode as well
    llvm-svn: 195015

--- compiler-rt/lib/asan/lit_tests/TestCases/use-after-scope.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/use-after-scope.cc
@@ -1,5 +1,6 @@
 // RUN: %clangxx_asan -O0 -fsanitize=use-after-scope %s -o %t && \
-// RUN:     not %t 2>&1 | FileCheck %s
+// RUN: not %t 2>&1 | FileCheck %s
+// RUN: ASAN_OPTIONS="detect_stack_use_after_return=1" not %t 2>&1 | FileCheck %s
 
 int main() {
   int *p = 0;
