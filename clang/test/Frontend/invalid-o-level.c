be5df46f26ce05b36e0b1091ac961f553b9ea967
Hans Wennborg <hans@hanshq.net>
2013-11-18 21:58:33 +0000

    Make test/Driver/clang_f_opts.c not write to the test dir
    After r195009, the test would write a .o file to the test dir. Send that to
    /dev/null instead. Also fix the typo in test/Frontend/invalid-o-level.c.
    
    llvm-svn: 195047

--- clang/test/Frontend/invalid-o-level.c
+++ clang/test/Frontend/invalid-o-level.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -O900 -o /dev/nul 2> %t.log
+// RUN: %clang_cc1 %s -O900 -o /dev/null 2> %t.log
 // RUN: FileCheck %s -input-file=%t.log
 
 // CHECK: warning: optimization level '-O900' is unsupported; using '-O3' instead.
