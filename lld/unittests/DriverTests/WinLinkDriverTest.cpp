f8b41867c8adef1f2352477d84859fbeccdffdb5
Rui Ueyama <ruiu@google.com>
2013-11-20 21:57:30 +0000

    [PECOFF] Recognize but ignore /implib and /safeseh for now.
    So that the LLD won't print error message saying that it couldn't find
    /implib or /safeseh files.
    
    llvm-svn: 195276

--- lld/unittests/DriverTests/WinLinkDriverTest.cpp
+++ lld/unittests/DriverTests/WinLinkDriverTest.cpp
@@ -515,7 +515,8 @@ TEST_F(WinLinkParserTest, Ignore) {
                     "/disallowlib:foo", "/delayload:user32", "/pdb:foo",
                     "/pdbaltpath:bar", "/verbose", "/verbose:icf", "/wx",
                     "/wx:no", "/tlbid:1", "/tlbout:foo", "/idlout:foo",
-                    "/ignoreidl", "a.obj", nullptr));
+                    "/ignoreidl", "/implib:foo", "/safeseh", "/safeseh:no",
+                    "a.obj", nullptr));
   EXPECT_EQ("", errorMessage());
   EXPECT_EQ(1, inputFileCount());
   EXPECT_EQ("a.obj", inputFile(0));
