53c51a167573551f200de11ad682d5dd1c3dd914
Kostya Serebryany <kcc@google.com>
2013-11-21 09:09:47 +0000

    [tsan] use FileCheck in race_on_speculative_load.cc
    llvm-svn: 195331

--- compiler-rt/lib/tsan/lit_tests/race_on_speculative_load.cc
+++ compiler-rt/lib/tsan/lit_tests/race_on_speculative_load.cc
@@ -1,8 +1,9 @@
-// RUN: %clangxx_tsan -O1 %s -o %t && %t
+// RUN: %clangxx_tsan -O1 %s -o %t && %t | FileCheck %s
 // Regtest for https://code.google.com/p/thread-sanitizer/issues/detail?id=40
 // This is a correct program and tsan should not report a race.
 #include <pthread.h>
 #include <unistd.h>
+#include <stdio.h>
 int g;
 __attribute__((noinline))
 int foo(int cond) {
@@ -21,4 +22,6 @@ int main() {
   pthread_create(&t, 0, Thread1, 0);
   g = 1;
   pthread_join(t, 0);
+  printf("PASS\n");
+  // CHECK: PASS
 }
