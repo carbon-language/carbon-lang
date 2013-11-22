ae215951a924210c7cbcf2b81967ec4e16bedc21
Dmitri Gribenko <gribozavr@gmail.com>
2013-11-22 01:47:15 +0000

    Comment parsing tests: XML schema: actually run the valid-function-10.xml test
    It was there all the time, but I missed adding a RUN line for it.
    
    llvm-svn: 195405

--- clang/test/Index/comment-xml-schema.c
+++ clang/test/Index/comment-xml-schema.c
@@ -11,6 +11,7 @@
 // RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-function-07.xml
 // RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-function-08.xml
 // RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-function-09.xml
+// RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-function-10.xml
 //
 // RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-availability-attr-01.xml
 // RUN: xmllint --noout --relaxng %S/../../bindings/xml/comment-xml-schema.rng %S/Inputs/CommentXML/valid-availability-attr-02.xml
