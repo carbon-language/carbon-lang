556f03ef908f13f4a5c54ba2b1fbdaa276acee36
Richard Smith <richard-llvm@metafoo.co.uk>
2013-11-23 05:21:32 +0000

    Fix module name collision in tests.
    llvm-svn: 195545

--- clang/test/Modules/malformed.cpp
+++ clang/test/Modules/malformed.cpp
@@ -6,18 +6,18 @@
 #define STR(x) STR2(x)
 #include STR(HEADER)
 
-// CHECK-A: While building module 'a'
+// CHECK-A: While building module 'malformed_a'
 // CHECK-A: a1.h:1:{{.*}} error: expected '}'
 // CHECK-A: a1.h:1:{{.*}} note: to match this '{'
 //
-// CHECK-A: While building module 'a'
+// CHECK-A: While building module 'malformed_a'
 // CHECK-A: a2.h:1:{{.*}} error: extraneous closing brace
 
-// CHECK-B: While building module 'b'
+// CHECK-B: While building module 'malformed_b'
 // CHECK-B: b1.h:2:{{.*}} error: expected '}'
 // CHECK-B: b1.h:1:{{.*}} note: to match this '{'
 // CHECK-B: b1.h:3:{{.*}} error: extraneous closing brace ('}')
 //
-// CHECK-B: While building module 'b'
+// CHECK-B: While building module 'malformed_b'
 // CHECK-B: b2.h:1:{{.*}} error: redefinition of 'g'
 // CHECK-B: b2.h:1:{{.*}} note: previous definition is here
