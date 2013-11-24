4f1bb9c384ba33e1f9f17901bcb746b190dcc1c7
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2013-11-24 17:05:58 +0000

    [test] Add test case I forgot to add for r195564.
    llvm-svn: 195572

---
+++ clang/test/CodeGenCXX/duplicate-mangled-name.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -emit-llvm-only %s -verify
+
+// rdar://15522601
+class MyClass {
+ static void meth();
+};
+void MyClass::meth() { }
+extern "C" {
+  void _ZN7MyClass4methEv() { } // expected-error {{definition with same mangled name as another definition}}
+}
