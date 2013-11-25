f59125f5bb39ba6b2844b6318d5db5cda5d6d303
Amara Emerson <amara.emerson@arm.com>
2013-11-25 11:24:18 +0000

    Revert r195599 as it broke the builds.
    llvm-svn: 195636

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -8692,11 +8692,6 @@ bool DAGCombiner::MergeConsecutiveStores(StoreSDNode* St) {
         Index = STn;
         break;
       } else if (LoadSDNode *Ldn = dyn_cast<LoadSDNode>(NextInChain)) {
-        if (Ldn->isVolatile()) {
-          Index = NULL;
-          break;
-        }
-
         // Save the load node for later. Continue the scan.
         AliasLoadNodes.push_back(Ldn);
         NextInChain = Ldn->getChain().getNode();
