6de7cd1672ad59cdb5c73edb7ec3398bfeb616dc
Alp Toker <alp@nuanti.com>
2013-11-27 23:29:37 +0000

    One last portability fix attempt for r195886
    llvm-svn: 195891

--- clang/test/Frontend/rewrite-includes-bom.c
+++ clang/test/Frontend/rewrite-includes-bom.c
@@ -1,5 +1,6 @@
-// RUN: grep -E "^\\xEF\\xBB\\xBF" %S/Inputs/rewrite-includes-bom.h
-// RUN: %clang_cc1 -E -frewrite-includes -I %S/Inputs %s -o - | %clang_cc1 -fsyntax-only -verify -x c - | not grep -E "\\xEF\\xBB\\xBF"
+// RUN: grep $'^\xEF\xBB\xBF' %S/Inputs/rewrite-includes-bom.h
+// RUN: %clang_cc1 -E -frewrite-includes -I %S/Inputs %s -o - | %clang_cc1 -fsyntax-only -verify -x c - | not grep $'\xEF\xBB\xBF'
 // expected-no-diagnostics
+// REQUIRES: shell
 
 #include "rewrite-includes-bom.h"
