d57e83644750b9b32a3d70092d7a6e04714cb82b
Alp Toker <alp@nuanti.com>
2013-11-27 23:16:06 +0000

    Attempt to make test from r195886 portable with different shells
    Windows and Hexagon complained. It's not clear which command is failing, so
    will back out the changes if this doesn't do the trick.
    
    llvm-svn: 195889

--- clang/test/Frontend/rewrite-includes-bom.c
+++ clang/test/Frontend/rewrite-includes-bom.c
@@ -1,5 +1,5 @@
-// RUN: grep '^\xEF\xBB\xBF' %S/Inputs/rewrite-includes-bom.h
-// RUN: %clang_cc1 -E -frewrite-includes -I %S/Inputs %s -o - | %clang_cc1 -fsyntax-only -verify -x c - | not grep '\xEF\xBB\xBF'
+// RUN: grep -E "^\\xEF\\xBB\\xBF" %S/Inputs/rewrite-includes-bom.h
+// RUN: %clang_cc1 -E -frewrite-includes -I %S/Inputs %s -o - | %clang_cc1 -fsyntax-only -verify -x c - | not grep -E "\\xEF\\xBB\\xBF"
 // expected-no-diagnostics
 
 #include "rewrite-includes-bom.h"
