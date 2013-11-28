6a7f41f0ec84b6a75a5c133ace45252ad3fb22fd
Alp Toker <alp@nuanti.com>
2013-11-28 05:55:40 +0000

    Fix test on Windows
    'not' on Windows tries and fails to convert the argument to UTF-16 and back for
    some reason:
    
      Error: Unable to convert command-line to UTF-16
    
    Let's try replacing it with !.
    
    (There was no obvious way to do these with FileCheck)
    
    llvm-svn: 195907

--- clang/test/Frontend/rewrite-includes-bom.c
+++ clang/test/Frontend/rewrite-includes-bom.c
@@ -1,6 +1,6 @@
-// RUN: grep $'^\xEF\xBB\xBF' %S/Inputs/rewrite-includes-bom.h
+// RUN: grep -q $'^\xEF\xBB\xBF' %S/Inputs/rewrite-includes-bom.h
 // RUN: %clang_cc1 -E -frewrite-includes -I %S/Inputs %s -o %t.c
-// RUN: not grep $'\xEF\xBB\xBF' %t.c
+// RUN: ! grep -q $'\xEF\xBB\xBF' %t.c
 // RUN: %clang_cc1 -fsyntax-only -verify %t.c
 // expected-no-diagnostics
 // REQUIRES: shell
