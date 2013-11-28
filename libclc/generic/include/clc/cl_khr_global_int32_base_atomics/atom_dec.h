8bb6cb800969ec87224b896cb9f548ff51719aca
Tom Stellard <thomas.stellard@amd.com>
2013-11-28 00:17:29 +0000

    Fix a C&P error in r195021 (65a950abab3cb8435ccb2646ac4773986c995c81)
    Patch by: Kai Wasserbäch
    
    Signed-off-by: Kai Wasserbäch <kai@dev.carbon-project.org>
    llvm-svn: 195898

--- libclc/generic/include/clc/cl_khr_global_int32_base_atomics/atom_dec.h
+++ libclc/generic/include/clc/cl_khr_global_int32_base_atomics/atom_dec.h
@@ -1,2 +1,2 @@
-_CLC_OVERLOAD _CLC_DECL int atom_add(global int *p);
-_CLC_OVERLOAD _CLC_DECL unsigned int atom_add(global unsigned int *p);
+_CLC_OVERLOAD _CLC_DECL int atom_dec(global int *p);
+_CLC_OVERLOAD _CLC_DECL unsigned int atom_dec(global unsigned int *p);
