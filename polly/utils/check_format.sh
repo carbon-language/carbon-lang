2cbc7bf64a9fca50e1cad67e157ec8ddf5f31ad8
Tobias Grosser <tobias@grosser.es>
2013-11-28 23:35:08 +0000

    (re)enable formatting checks
    llvm-svn: 195935

--- polly/utils/check_format.sh
+++ polly/utils/check_format.sh
@@ -6,8 +6,7 @@ if [ "${CLANG_FORMAT}x" = "x" ]; then
   CLANG_FORMAT=`which clang-format`
   if [ "${CLANG_FORMAT}x" = "x" ]; then
      echo "Error: cannot find clang-format in your path"
-     # Disable error reporting to silence the buildbots
-     exit 0
+     exit 1
   fi
 fi
 
@@ -24,7 +23,7 @@ for ARG in "$@"
 
 if [[ $OK -eq "1" ]]; then
   echo "Error: clang-format reported formatting differences"
-  exit 0
+  exit 1
 else
   echo "OK: clang-format reported no formatting differences"
   exit 0
