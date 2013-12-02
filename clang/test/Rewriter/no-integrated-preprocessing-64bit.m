99416d3ef2b58f8d67d13e43358009fa5559c547
Alp Toker <alp@nuanti.com>
2013-12-02 03:50:16 +0000

    Correct test to target 'unknown'
    llvm-svn: 196060

--- clang/test/Rewriter/no-integrated-preprocessing-64bit.m
+++ clang/test/Rewriter/no-integrated-preprocessing-64bit.m
@@ -1,4 +1,4 @@
-// RUN: %clang -target x86_64-unkown-unknown -fms-extensions -rewrite-objc %s -o - | FileCheck %s
+// RUN: %clang -target x86_64-unknown-unknown -fms-extensions -rewrite-objc %s -o - | FileCheck %s
 // rdar://12189793
 
 #ifdef __cplusplus
