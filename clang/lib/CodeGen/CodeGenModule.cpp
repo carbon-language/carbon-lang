ba2fa8b68fc4c0b623fd0512f7d6c30614b27330
Manman Ren <manman.ren@gmail.com>
2013-12-02 19:37:35 +0000

    Expand comments for Debug Info Version.
    llvm-svn: 196143

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -260,7 +260,10 @@ void CodeGenModule::Release() {
                               CodeGenOpts.DwarfVersion);
   if (DebugInfo)
     // We support a single version in the linked module: error out when
-    // modules do not have the same version.
+    // modules do not have the same version. We are going to implement dropping
+    // debug info when the version number is not up-to-date. Once that is
+    // done, the bitcode linker is not going to see modules with different
+    // version numbers.
     getModule().addModuleFlag(llvm::Module::Error, "Debug Info Version",
                               llvm::dwarf::DEBUG_INFO_VERSION);
 
