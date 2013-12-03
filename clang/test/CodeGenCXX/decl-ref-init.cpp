6ffd88d6645e27565bc9e4a91cffd31c0348092a
Rafael Espindola <rafael.espindola@gmail.com>
2013-12-03 14:35:42 +0000

    Check IR on this test.
    llvm-svn: 196284

--- clang/test/CodeGenCXX/decl-ref-init.cpp
+++ clang/test/CodeGenCXX/decl-ref-init.cpp
@@ -1,8 +1,7 @@
-// REQUIRES: x86-registered-target,x86-64-registered-target
-// RUN: %clang_cc1 -triple x86_64-apple-darwin -S %s -o %t-64.s
-// RUN: FileCheck -check-prefix CHECK-LP64 --input-file=%t-64.s %s
-// RUN: %clang_cc1 -triple i386-apple-darwin -S %s -o %t-32.s
-// RUN: FileCheck -check-prefix CHECK-LP32 --input-file=%t-32.s %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin -emit-llvm %s -o - | \
+// RUN: FileCheck %s
+// RUN: %clang_cc1 -triple i386-apple-darwin -emit-llvm %s -o - | \
+// RUN: FileCheck %s
 
 struct A {};
 
@@ -24,8 +23,5 @@ int main() {
 	const A& rca2 = d();
 }
 
-// CHECK-LP64: callq    __ZN1BcvR1AEv
-// CHECK-LP64: callq    __ZN1BcvR1AEv
-
-// CHECK-LP32: calll     L__ZN1BcvR1AEv
-// CHECK-LP32: calll     L__ZN1BcvR1AEv
+// CHECK: call %struct.A* @_ZN1BcvR1AEv
+// CHECK: call %struct.A* @_ZN1BcvR1AEv
