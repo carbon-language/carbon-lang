a1226fc722e7b231ad3b22c54e58dd723e5bedf0
Rafael Espindola <rafael.espindola@gmail.com>
2013-12-03 14:09:19 +0000

    Check IR in this test.
    llvm-svn: 196277

--- clang/test/CodeGenCXX/array-operator-delete-call.cpp
+++ clang/test/CodeGenCXX/array-operator-delete-call.cpp
@@ -1,8 +1,7 @@
-// REQUIRES: x86-registered-target,x86-64-registered-target
-// RUN: %clang_cc1 -triple x86_64-apple-darwin -std=c++11 -S %s -o %t-64.s
-// RUN: FileCheck -check-prefix CHECK-LP64 --input-file=%t-64.s %s
-// RUN: %clang_cc1 -triple i386-apple-darwin -std=c++11 -S %s -o %t-32.s
-// RUN: FileCheck -check-prefix CHECK-LP32 --input-file=%t-32.s %s
+// RUN: %clang_cc1 -triple x86_64-apple-darwin -std=c++11 -emit-llvm %s -o - | \
+// RUN: FileCheck %s
+// RUN: %clang_cc1 -triple i386-apple-darwin -std=c++11 -emit-llvm %s -o - | \
+// RUN: FileCheck %s
 
 extern "C" int printf(...);
 
@@ -58,7 +57,5 @@ int main()
 }
 COST c2;
 
-// CHECK-LP64: callq    __ZdaPv
-
-// CHECK-LP32: calll     L__ZdaPv
+// CHECK: call void @_ZdaPv
 
