a31d1dd179e8a40982b723b7070f1724c51e6224
Alp Toker <alp@nuanti.com>
2013-12-05 18:04:42 +0000

    AttributeList: tweak the conditional order to avoid two strcmps
    llvm-svn: 196518

--- clang/lib/Sema/AttributeList.cpp
+++ clang/lib/Sema/AttributeList.cpp
@@ -122,8 +122,8 @@ AttributeList::Kind AttributeList::getKind(const IdentifierInfo *Name,
   StringRef AttrName = Name->getName();
 
   // Normalize the attribute name, __foo__ becomes foo.
-  if (AttrName.startswith("__") && AttrName.endswith("__") &&
-      AttrName.size() >= 4)
+  if (AttrName.size() >= 4 && AttrName.startswith("__") &&
+      AttrName.endswith("__"))
     AttrName = AttrName.substr(2, AttrName.size() - 4);
 
   SmallString<64> Buf;
