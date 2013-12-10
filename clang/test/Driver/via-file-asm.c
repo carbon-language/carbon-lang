1a74b6e11869fe221b7451b2db857472c35f14a6
David Peixotto <dpeixott@codeaurora.org>
2013-12-10 00:54:30 +0000

    Fix via-file-asm test failure on windows
    The windows target does not support using an external assembler so
    the test case was failing with this error:
    
    error: there is no external assembler that can be used on this platform
    
    The test was updated to always explicitly pass a target that has
    both an interal and external assembler.
    
    llvm-svn: 196854

--- clang/test/Driver/via-file-asm.c
+++ clang/test/Driver/via-file-asm.c
@@ -1,14 +1,10 @@
 // Should save and read back the assembly from a file
-// RUN: %clang -integrated-as -via-file-asm %s -### 2>&1 | FileCheck %s
+// RUN: %clang -target arm-none-linux-gnueabi -integrated-as -via-file-asm %s -### 2>&1 | FileCheck %s
 // CHECK: "-cc1"
 // CHECK: "-o" "[[TMP:[^"]*]]"
 // CHECK: -cc1as
 // CHECK: [[TMP]]
 
 // Should not force using the integrated assembler
-// RUN: %clang -no-integrated-as -via-file-asm %s -### 2>&1 | FileCheck --check-prefix=NO_IAS %s
+// RUN: %clang -target arm-none-linux-gnueabi -no-integrated-as -via-file-asm %s -### 2>&1 | FileCheck --check-prefix=NO_IAS %s
 // NO_IAS-NOT: "-cc1as"
-
-// Test arm target specifically for the same behavior
-// RUN: %clang -target arm -integrated-as -via-file-asm %s -### 2>&1 | FileCheck %s
-// RUN: %clang -target arm -no-integrated-as -via-file-asm %s -### 2>&1 | FileCheck --check-prefix=NO_IAS %s
