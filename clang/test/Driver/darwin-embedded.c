a5877505d7f4bd0157a3cbbe12907157c3383c7d
Tim Northover <tnorthover@apple.com>
2013-12-13 16:05:12 +0000

    ARM MachO embedded: fix test without FileCheck line
    Well, that's one way to pass a test, I suppose. Unfortunately actually doing
    the testing means I didn't pass all I thought (embedded v7a is not supported,
    apparently). I'll deal with that with the move to -none-macho rather than
    putting heinous hacks in right now.
    
    llvm-svn: 197240

--- clang/test/Driver/darwin-embedded.c
+++ clang/test/Driver/darwin-embedded.c
@@ -3,8 +3,10 @@
 // RUN: %clang -target x86_64-apple-darwin -arch armv7em -mhard-float -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
 
 // RUN: %clang -target x86_64-apple-darwin -arch armv7m -fPIC -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
-// RUN: %clang -target x86_64-apple-darwin -arch armv7 -fPIC -mfloat-abi=hard -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
-// RUN: %clang -target x86_64-apple-darwin-eabi -arch armv7em -fPIC -mfloat-abi=softfp -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
+// RUN: %clang -target x86_64-apple-darwin -arch armv7em -fPIC -mfloat-abi=hard -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
+// RUN: %clang -target x86_64-apple-darwin -arch armv7em -fPIC -mfloat-abi=softfp -resource-dir=%S/Inputs/resource_dir %s -### 2>> %t
+
+// RUN: FileCheck %s < %t
 
 // ARMv6m has no float
 // CHECK: libclang_rt.soft_static.a
@@ -18,9 +20,11 @@
 // ARMv7m has no float either
 // CHECK: libclang_rt.soft_pic.a
 
-// But it can be enabled on ARMv7
+// But it can be enabled on ARMv7em
 // CHECK: libclang_rt.hard_pic.a
 
 // "softfp" must link against a soft-float library since that's what the
 // callers we're compiling will expect.
 // CHECK: libclang_rt.soft_pic.a
+
+// FIXME: test ARMv7a when we switch to -none-macho as the triple
