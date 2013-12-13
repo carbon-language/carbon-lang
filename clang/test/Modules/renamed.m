6b5460c75a7ede8a9b37d8b6d5e427b84d39be15
Manuel Klimek <klimek@google.com>
2013-12-13 10:08:24 +0000

    Move the input files for the unnecessary-module-map-parsing test to Inputs.
    Needed to change rename.m to set the right include path so we don't
    import a broken module due to recursive module checking.
    
    llvm-svn: 197222

--- clang/test/Modules/renamed.m
+++ clang/test/Modules/renamed.m
@@ -3,6 +3,6 @@
 int f() { return same_api; }
 
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -fmodules -I %S/Inputs -fmodules-cache-path=%t %s -verify
+// RUN: %clang_cc1 -fmodules -I %S/Inputs/oldname -fmodules-cache-path=%t %s -verify
 
 // expected-no-diagnostics
--- clang/test/Modules/unnecessary-module-map-parsing.c
+++ clang/test/Modules/unnecessary-module-map-parsing.c
@@ -1,7 +1,7 @@
 // This checks that we are not parsing module maps if modules are not enabled.
 
-// RUN: not %clang_cc1 -fmodules -I %S/unnecessary-module-map-parsing -fsyntax-only %s 2>&1 | FileCheck %s
-// RUN: %clang_cc1 -I %S/unnecessary-module-map-parsing -fsyntax-only %s
+// RUN: not %clang_cc1 -fmodules -I %S/Inputs/unnecessary-module-map-parsing -fsyntax-only %s 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -I %S/Inputs/unnecessary-module-map-parsing -fsyntax-only %s
 
 // CHECK: error: header 'unknown.h' not found
 
