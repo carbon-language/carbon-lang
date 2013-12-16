f057093fdcfb6cb4f80ce8342bbaa902dd884cae
Rafael Espindola <rafael.espindola@gmail.com>
2013-12-16 17:41:15 +0000

    Don't duplicate the DataLayout defaults for integer, floats and vectors.
    llvm-svn: 197398

--- llvm/lib/Target/R600/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/R600/AMDGPUTargetMachine.cpp
@@ -51,15 +51,10 @@ SchedCustomRegistry("r600", "Run R600's custom scheduler",
 
 static std::string computeDataLayout(const AMDGPUSubtarget &ST) {
   std::string DataLayout = std::string(
-   "e"
-   "-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:64:64-f32:32:32"
-   "-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v64:64:64-v96:128:128-v128:128:128"
+   "e-i64:64:64-v16:16:16-v24:32:32-v32:32:32-v48:64:64-v96:128:128"
    "-v192:256:256-v256:256:256-v512:512:512-v1024:1024:1024-v2048:2048:2048"
    "-n32:64"
   );
-
-  if (ST.hasHWFP64())
-    DataLayout.append("-f64:64:64");
 
   if (!ST.is64bit())
     DataLayout.append("-p:32:32:32");
