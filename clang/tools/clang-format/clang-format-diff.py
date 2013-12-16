25c683891c5a1ff465b46e32110552e8f1690073
Alexander Kornienko <alexfh@google.com>
2013-12-16 10:57:30 +0000

    Added -iregex for case-insensitive regex to filter file names.
    Summary:
    -regex and -iregex both mimic options of the find utility.
    Made the default list of extensions case-insensitive, so that it's not only C
    and CPP extensions are accepted in upper case.
    
    Reviewers: djasper
    
    Reviewed By: djasper
    
    CC: cfe-commits
    
    Differential Revision: http://llvm-reviews.chandlerc.com/D2415
    
    llvm-svn: 197378

--- clang/tools/clang-format/clang-format-diff.py
+++ clang/tools/clang-format/clang-format-diff.py
@@ -43,9 +43,13 @@ def main():
                       help='apply edits to files instead of displaying a diff')
   parser.add_argument('-p', metavar='NUM', default=0,
                       help='strip the smallest prefix containing P slashes')
-  parser.add_argument('-regex', metavar='PATTERN', default=
-                      r'.*\.(cpp|cc|CPP|C|c\+\+|cxx|c|h|hpp|m|mm|inc|js)',
-                      help='custom pattern selecting file paths to reformat')
+  parser.add_argument('-regex', metavar='PATTERN', default='',
+                      help='custom pattern selecting file paths to reformat '
+                      '(case sensitive, override -iregex)')
+  parser.add_argument('-iregex', metavar='PATTERN', default=
+                      r'.*\.(cpp|cc|c\+\+|cxx|c|h|hpp|m|mm|inc|js)',
+                      help='custom pattern selecting file paths to reformat '
+                      '(case insensitive, override -regex)')
   parser.add_argument(
       '-style',
       help=
@@ -62,8 +66,12 @@ def main():
     if filename == None:
       continue
 
-    if not re.match(args.regex, filename):
-      continue
+    if args.regex != '':
+      if not re.match(args.regex, filename):
+        continue
+    else:
+      if not re.match(args.iregex, filename, re.IGNORECASE):
+        continue
 
     match = re.search('^@@.*\+(\d+)(,(\d+))?', line)
     if match:
