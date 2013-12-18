67e6d506d88bb68d5f5e26e7a8318cf1090de390
Artyom Skrobov <Artyom.Skrobov@arm.com>
2013-12-18 18:55:36 +0000

    Adding a win32-targeted test into Tooling/multi-jobs.cpp, to make sure it doesn't get broken again (prompted by NAKAMURA Takumi)
    llvm-svn: 197596

--- clang/test/Tooling/multi-jobs.cpp
+++ clang/test/Tooling/multi-jobs.cpp
@@ -1,4 +1,5 @@
 // RUN: not clang-check "%s" -- -no-integrated-as -c 2>&1 | FileCheck %s
+// RUN: not clang-check "%s" -- -target x86_64-win32 -no-integrated-as -c 2>&1 | FileCheck %s
 
 // CHECK: C++ requires
 invalid;
