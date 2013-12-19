08d32ee7571fd1dcb499a7f4ad745d033480874b
Rafael Espindola <rafael.espindola@gmail.com>
2013-12-19 23:38:09 +0000

    Update the ML test to expect the new string format of getStringRepresentation.
    llvm-svn: 197750

--- llvm/test/Bindings/Ocaml/target.ml
+++ llvm/test/Bindings/Ocaml/target.ml
@@ -43,9 +43,7 @@ let machine = TargetMachine.create (Target.default_triple ()) target
 
 let test_target_data () =
   let module DL = DataLayout in
-  let layout = "e-p:32:32:32-S32-i1:8:8-i8:8:8-i16:16:16-i32:32:32-i64:32:64-" ^
-               "f16:16:16-f32:32:32-f64:32:64-f128:128:128-v64:32:64-v128:32:128-" ^
-               "a0:0:64-n32" in
+  let layout = "e-p:32:32-f64:32:64-v64:32:64-v128:32:128-n32-S32" in
   let dl     = DL.of_string layout in
   let sty    = struct_type context [| i32_type; i64_type |] in
   
