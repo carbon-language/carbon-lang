86409e1cde6a329427d146a21e93104b7c278986
Joey Gouly <joey.gouly@gmail.com>
2013-12-20 00:01:00 +0000

    Fix the build by adding back a comma...
    llvm-svn: 197754

--- lld/include/lld/ReaderWriter/Reader.h
+++ lld/include/lld/ReaderWriter/Reader.h
@@ -43,7 +43,7 @@ public:
   /// 1) the file_magic enumeration returned by identify_magic()
   /// 2) the file extension (e.g. ".obj")
   /// 3) the whole file content buffer if the above is not enough. 
-  virtual bool canParse(file_magic magic, StringRef fileExtension
+  virtual bool canParse(file_magic magic, StringRef fileExtension,
                         const MemoryBuffer &mb) const = 0;
 
   /// \brief Parse a supplied buffer (already filled with the contents of a
