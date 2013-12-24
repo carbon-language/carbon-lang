428a93b8aba6b9f94aab185c194ce1891d98053d
Nico Weber <nicolasweber@gmx.de>
2013-12-24 20:48:13 +0000

    Add a FIXME to a failing test.
    (See discussion in the bug for why this isn't XFAILed.)
    
    llvm-svn: 197991

--- clang/test/Headers/cxx11.cpp
+++ clang/test/Headers/cxx11.cpp
@@ -1,6 +1,8 @@
 // RUN: %clang_cc1 -ffreestanding -fsyntax-only -std=c++11 %s
 // RUN: %clang_cc1 -ffreestanding -fsyntax-only -std=c++11 -fmodules %s
 
+// FIXME: This test fails on OS X 10.9 with headers in /usr/include, see PR18322
+
 #include <stdalign.h>
 
 #if defined alignas
