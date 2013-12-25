2f392d237ca586fb510c51bf3511a83ad8fe3306
Alexey Samsonov <samsonov@google.com>
2013-12-25 08:01:16 +0000

    [Sanitizer] Move OS-specific code for MemoryMappingLayout into separate source files.
    llvm-svn: 198003

--- compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
@@ -71,33 +71,10 @@ class MemoryMappingLayout {
   // lookup.
   bool IterateForObjectNameAndOffset(uptr addr, uptr *offset,
                                      char filename[], uptr filename_size,
-                                     uptr *protection) {
-    Reset();
-    uptr start, end, file_offset;
-    for (int i = 0; Next(&start, &end, &file_offset, filename, filename_size,
-                         protection);
-         i++) {
-      if (addr >= start && addr < end) {
-        // Don't subtract 'start' for the first entry:
-        // * If a binary is compiled w/o -pie, then the first entry in
-        //   process maps is likely the binary itself (all dynamic libs
-        //   are mapped higher in address space). For such a binary,
-        //   instruction offset in binary coincides with the actual
-        //   instruction address in virtual memory (as code section
-        //   is mapped to a fixed memory range).
-        // * If a binary is compiled with -pie, all the modules are
-        //   mapped high at address space (in particular, higher than
-        //   shadow memory of the tool), so the module can't be the
-        //   first entry.
-        *offset = (addr - (i ? start : 0)) + file_offset;
-        return true;
-      }
-    }
-    if (filename_size)
-      filename[0] = '\0';
-    return false;
-  }
+                                     uptr *protection);
 
+  // FIXME: Hide implementation details for different platforms in
+  // platform-specific files.
 # if SANITIZER_LINUX
   ProcSelfMapsBuff proc_self_maps_;
   char *current_;
