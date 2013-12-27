52b10afb08d7d85263209929f24a804e6ad75b08
Reid Kleckner <reid@kleckner.net>
2013-12-27 20:41:49 +0000

    Move MS header search test inputs to Inputs/
    llvm-svn: 198086

--- clang/test/Preprocessor/microsoft-header-search.c
+++ clang/test/Preprocessor/microsoft-header-search.c
@@ -1,6 +1,6 @@
-// RUN: %clang_cc1 -I%S/microsoft-header-search %s -fms-compatibility -verify
+// RUN: %clang_cc1 -I%S/Inputs/microsoft-header-search %s -fms-compatibility -verify
 
-// expected-warning@microsoft-header-search/a/findme.h:3 {{findme.h successfully included using MS search rules}}
-// expected-warning@microsoft-header-search/a/b/include3.h:3 {{#include resolved using non-portable MSVC search rules as}}
+// expected-warning@Inputs/microsoft-header-search/a/findme.h:3 {{findme.h successfully included using MS search rules}}
+// expected-warning@Inputs/microsoft-header-search/a/b/include3.h:3 {{#include resolved using non-portable MSVC search rules as}}
 
-#include "microsoft-header-search/include1.h"
+#include "Inputs/microsoft-header-search/include1.h"
