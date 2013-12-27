9e26f5f397b4e5c899c6c2c1849d0230a9828117
Rui Ueyama <ruiu@google.com>
2013-12-27 02:51:53 +0000

    Fix comment.
    llvm-svn: 198066

--- lld/lib/Driver/WinLinkDriver.cpp
+++ lld/lib/Driver/WinLinkDriver.cpp
@@ -895,10 +895,10 @@ WinLinkDriver::parse(int argc, const char *argv[], PECOFFLinkingContext &ctx,
 
       // Mangle the symbol name only if it is reading user-supplied command line
       // arguments. Because the symbol name in the .drectve section is already
-      // mangled by the compiler, we shouldn't add a leading undescore here.
-      // It's odd that the command line option has different semantics in the
-      // .drectve section, but this behavior is needed for compatibility with
-      // MSVC's link.exe.
+      // mangled by the compiler, we shouldn't add a leading underscore in that
+      // case. It's odd that the command line option has different semantics in
+      // the .drectve section, but this behavior is needed for compatibility
+      // with MSVC's link.exe.
       if (!isReadingDirectiveSection)
         desc.name = ctx.decorateSymbol(desc.name);
       ctx.addDllExport(desc);
