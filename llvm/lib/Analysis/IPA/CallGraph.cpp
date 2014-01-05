c4ddab6ff23cefb07b06f2ab4e06773b82aa84e2
Chandler Carruth <chandlerc@gmail.com>
2014-01-05 10:38:52 +0000

    [PM] Add a definition for the static PassID in the CallGraphAnalysis. Missed this when adding the skeleton analysis. Caught by a build break in the next patch I'm working on when trying to use the analysis.
    llvm-svn: 198556

--- llvm/lib/Analysis/IPA/CallGraph.cpp
+++ llvm/lib/Analysis/IPA/CallGraph.cpp
@@ -237,6 +237,12 @@ void CallGraphNode::replaceCallEdge(CallSite CS,
 }
 
 //===----------------------------------------------------------------------===//
+// Out-of-line definitions of CallGraphAnalysis class members.
+//
+
+char CallGraphAnalysis::PassID;
+
+//===----------------------------------------------------------------------===//
 // Implementations of the CallGraphWrapperPass class methods.
 //
 
