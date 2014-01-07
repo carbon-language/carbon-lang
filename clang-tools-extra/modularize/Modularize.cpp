f7e45c0ed4a66b59d3180b2b394f2b7a5fce2081
Chandler Carruth <chandlerc@gmail.com>
2014-01-07 22:15:39 +0000

    Fix the top header that corresponds to this source file. Thanks for spotting this Cody Maloney!
    llvm-svn: 198716

--- clang-tools-extra/modularize/Modularize.cpp
+++ clang-tools-extra/modularize/Modularize.cpp
@@ -142,9 +142,9 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "clang/AST/ASTConsumer.h"
 #include "Modularize.h"
 #include "PreprocessorTracker.h"
+#include "clang/AST/ASTConsumer.h"
 #include "clang/AST/ASTContext.h"
 #include "clang/AST/RecursiveASTVisitor.h"
 #include "clang/Basic/SourceManager.h"
