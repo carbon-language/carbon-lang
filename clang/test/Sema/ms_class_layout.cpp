b700566a3a28deaf39df4a3c9084709522d4b8b0
Warren Hunt <whunt@google.com>
2014-01-09 00:48:32 +0000

    [ms-abi] Fixed failing lit test.
    This test adjustment was missing from the previous patch.
    
    llvm-svn: 198822

--- clang/test/Sema/ms_class_layout.cpp
+++ clang/test/Sema/ms_class_layout.cpp
@@ -289,7 +289,7 @@ int main() {
 // CHECK-NEXT:  8 |     (D vftable pointer)
 // CHECK-NEXT: 16 |     double a
 // CHECK-NEXT: sizeof=24, align=8
-// CHECK-NEXT: nvsize=8, nvalign=4
+// CHECK-NEXT: nvsize=8, nvalign=8
 
 // CHECK: %struct.H = type { %struct.G, i32*, %class.D }
 
