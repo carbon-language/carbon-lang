3a47244c94d3fb74a6b77fac8050b15898bc7780
Sergey Matveev <earthdok@google.com>
2014-01-13 17:26:57 +0000

    [lsan] Add __lsan_default_suppressions() to the public interface header.
    llvm-svn: 199121

--- compiler-rt/include/sanitizer/lsan_interface.h
+++ compiler-rt/include/sanitizer/lsan_interface.h
@@ -41,6 +41,14 @@ extern "C" {
   void __lsan_register_root_region(const void *p, size_t size);
   void __lsan_unregister_root_region(const void *p, size_t size);
 
+  // Calling this function makes LSan enter the leak checking phase immediately.
+  // Use this if normal end-of-process leak checking happens too late (e.g. if
+  // you have intentional memory leaks in your shutdown code). Calling this
+  // function overrides end-of-process leak checking; it must be called at
+  // most once per process. This function will terminate the process if there
+  // are memory leaks and the exit_code flag is non-zero.
+  void __lsan_do_leak_check();
+
   // The user may optionally provide this function to disallow leak checking
   // for the program it is linked into (if the return value is non-zero). This
   // function must be defined as returning a constant value; any behavior beyond
@@ -52,13 +60,9 @@ extern "C" {
   // LeakSanitizerIsTurnedOffForTheCurrentProcess is used.
   int __lsan_is_turned_off();
 
-  // Calling this function makes LSan enter the leak checking phase immediately.
-  // Use this if normal end-of-process leak checking happens too late (e.g. if
-  // you have intentional memory leaks in your shutdown code). Calling this
-  // function overrides end-of-process leak checking; it must be called at
-  // most once per process. This function will terminate the process if there
-  // are memory leaks and the exit_code flag is non-zero.
-  void __lsan_do_leak_check();
+  // This function may be optionally provided by the user and should return
+  // a string containing LSan suppressions.
+  const char *__lsan_default_suppressions();
 #ifdef __cplusplus
 }  // extern "C"
 
