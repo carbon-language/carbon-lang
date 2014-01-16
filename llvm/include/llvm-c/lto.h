40fdf5fee65f17f6bf836553860a8a977966e5c1
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2014-01-16 21:37:17 +0000

    LTO: document LTO_API_VERSION for each API
    Adding a doxygen comment for each bit of API to indicate at which
    LTO_API_VERSION each was available, manually gleaned from successive
    git-blames.  A few notes:
    
    - LTO_API_VERSION was set to 3 at its introduction.
    - I've indicated all the API introduced before LTO_API_VERSION was
      around as available "prior to LTO_API_VERSION=3".
    - A number of API changes neglected to bump LTO_API_VERSION.  These I've
      indicated as available at the *next* bump of LTO_API_VERSION.
    
    llvm-svn: 199429

--- llvm/include/llvm-c/lto.h
+++ llvm/include/llvm-c/lto.h
@@ -42,6 +42,9 @@ typedef bool lto_bool_t;
 
 #define LTO_API_VERSION 7
 
+/**
+ * \since prior to LTO_API_VERSION=3
+ */
 typedef enum {
     LTO_SYMBOL_ALIGNMENT_MASK              = 0x0000001F, /* log2 of alignment */
     LTO_SYMBOL_PERMISSIONS_MASK            = 0x000000E0,
@@ -62,17 +65,26 @@ typedef enum {
     LTO_SYMBOL_SCOPE_DEFAULT_CAN_BE_HIDDEN = 0x00002800
 } lto_symbol_attributes;
 
+/**
+ * \since prior to LTO_API_VERSION=3
+ */
 typedef enum {
     LTO_DEBUG_MODEL_NONE         = 0,
     LTO_DEBUG_MODEL_DWARF        = 1
 } lto_debug_model;
 
+/**
+ * \since prior to LTO_API_VERSION=3
+ */
 typedef enum {
     LTO_CODEGEN_PIC_MODEL_STATIC         = 0,
     LTO_CODEGEN_PIC_MODEL_DYNAMIC        = 1,
     LTO_CODEGEN_PIC_MODEL_DYNAMIC_NO_PIC = 2
 } lto_codegen_model;
 
+/**
+ * \since LTO_API_VERSION=6
+ */
 typedef enum {
     LTO_INTERNALIZE_FULL   = 0,
     LTO_INTERNALIZE_NONE   = 1,
@@ -91,6 +103,8 @@ extern "C" {
 
 /**
  * Returns a printable string.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern const char*
 lto_get_version(void);
@@ -98,12 +112,16 @@ lto_get_version(void);
 
 /**
  * Returns the last error string or NULL if last operation was successful.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern const char*
 lto_get_error_message(void);
 
 /**
  * Checks if a file is a loadable object file.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_module_is_object_file(const char* path);
@@ -111,6 +129,8 @@ lto_module_is_object_file(const char* path);
 
 /**
  * Checks if a file is a loadable object compiled for requested target.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_module_is_object_file_for_target(const char* path,
@@ -119,6 +139,8 @@ lto_module_is_object_file_for_target(const char* path,
 
 /**
  * Checks if a buffer is a loadable object file.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_module_is_object_file_in_memory(const void* mem, size_t length);
@@ -126,6 +148,8 @@ lto_module_is_object_file_in_memory(const void* mem, size_t length);
 
 /**
  * Checks if a buffer is a loadable object compiled for requested target.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_module_is_object_file_in_memory_for_target(const void* mem, size_t length,
@@ -135,6 +159,8 @@ lto_module_is_object_file_in_memory_for_target(const void* mem, size_t length,
 /**
  * Loads an object file from disk.
  * Returns NULL on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_module_t
 lto_module_create(const char* path);
@@ -143,6 +169,8 @@ lto_module_create(const char* path);
 /**
  * Loads an object file from memory.
  * Returns NULL on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_module_t
 lto_module_create_from_memory(const void* mem, size_t length);
@@ -150,6 +178,8 @@ lto_module_create_from_memory(const void* mem, size_t length);
 /**
  * Loads an object file from disk. The seek point of fd is not preserved.
  * Returns NULL on error (check lto_get_error_message() for details).
+ *
+ * \since LTO_API_VERSION=5
  */
 extern lto_module_t
 lto_module_create_from_fd(int fd, const char *path, size_t file_size);
@@ -157,28 +187,34 @@ lto_module_create_from_fd(int fd, const char *path, size_t file_size);
 /**
  * Loads an object file from disk. The seek point of fd is not preserved.
  * Returns NULL on error (check lto_get_error_message() for details).
+ *
+ * \since LTO_API_VERSION=5
  */
 extern lto_module_t
 lto_module_create_from_fd_at_offset(int fd, const char *path, size_t file_size,
                                     size_t map_size, off_t offset);
 
-
 /**
  * Frees all memory internally allocated by the module.
  * Upon return the lto_module_t is no longer valid.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern void
 lto_module_dispose(lto_module_t mod);
 
-
 /**
  * Returns triple string which the object module was compiled under.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern const char*
 lto_module_get_target_triple(lto_module_t mod);
 
 /**
  * Sets triple string with which the object will be codegened.
+ *
+ * \since LTO_API_VERSION=4
  */
 extern void
 lto_module_set_target_triple(lto_module_t mod, const char *triple);
@@ -186,6 +222,8 @@ lto_module_set_target_triple(lto_module_t mod, const char *triple);
 
 /**
  * Returns the number of symbols in the object module.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern unsigned int
 lto_module_get_num_symbols(lto_module_t mod);
@@ -193,6 +231,8 @@ lto_module_get_num_symbols(lto_module_t mod);
 
 /**
  * Returns the name of the ith symbol in the object module.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern const char*
 lto_module_get_symbol_name(lto_module_t mod, unsigned int index);
@@ -200,12 +240,16 @@ lto_module_get_symbol_name(lto_module_t mod, unsigned int index);
 
 /**
  * Returns the attributes of the ith symbol in the object module.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_symbol_attributes
 lto_module_get_symbol_attribute(lto_module_t mod, unsigned int index);
 
 /**
  * Diagnostic severity.
+ *
+ * \since LTO_API_VERSION=7
  */
 typedef enum {
   LTO_DS_ERROR,
@@ -219,6 +263,8 @@ typedef enum {
  * \p diag is the actual diagnostic.
  * The diagnostic is not prefixed by any of severity keyword, e.g., 'error: '.
  * \p ctxt is used to pass the context set with the diagnostic handler.
+ *
+ * \since LTO_API_VERSION=7
  */
 typedef void (*lto_diagnostic_handler_t)(
     lto_codegen_diagnostic_severity_t severity, const char *diag, void *ctxt);
@@ -227,6 +273,8 @@ typedef void (*lto_diagnostic_handler_t)(
  * Set a diagnostic handler and the related context (void *).
  * This is more general than lto_get_error_message, as the diagnostic handler
  * can be called at anytime within lto.
+ *
+ * \since LTO_API_VERSION=7
  */
 extern void lto_codegen_set_diagnostic_handler(lto_code_gen_t,
                                                lto_diagnostic_handler_t,
@@ -235,32 +283,35 @@ extern void lto_codegen_set_diagnostic_handler(lto_code_gen_t,
 /**
  * Instantiates a code generator.
  * Returns NULL on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_code_gen_t
 lto_codegen_create(void);
 
-
 /**
  * Frees all code generator and all memory it internally allocated.
  * Upon return the lto_code_gen_t is no longer valid.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern void
 lto_codegen_dispose(lto_code_gen_t);
 
-
-
 /**
  * Add an object module to the set of modules for which code will be generated.
  * Returns true on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_codegen_add_module(lto_code_gen_t cg, lto_module_t mod);
 
-
-
 /**
  * Sets if debug info should be generated.
  * Returns true on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_codegen_set_debug_model(lto_code_gen_t cg, lto_debug_model);
@@ -269,6 +320,8 @@ lto_codegen_set_debug_model(lto_code_gen_t cg, lto_debug_model);
 /**
  * Sets which PIC code model to generated.
  * Returns true on error (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern lto_bool_t
 lto_codegen_set_pic_model(lto_code_gen_t cg, lto_codegen_model);
@@ -276,6 +329,8 @@ lto_codegen_set_pic_model(lto_code_gen_t cg, lto_codegen_model);
 
 /**
  * Sets the cpu to generate code for.
+ *
+ * \since LTO_API_VERSION=4
  */
 extern void
 lto_codegen_set_cpu(lto_code_gen_t cg, const char *cpu);
@@ -284,12 +339,16 @@ lto_codegen_set_cpu(lto_code_gen_t cg, const char *cpu);
 /**
  * Sets the location of the assembler tool to run. If not set, libLTO
  * will use gcc to invoke the assembler.
+ *
+ * \since LTO_API_VERSION=3
  */
 extern void
 lto_codegen_set_assembler_path(lto_code_gen_t cg, const char* path);
 
 /**
  * Sets extra arguments that libLTO should pass to the assembler.
+ *
+ * \since LTO_API_VERSION=4
  */
 extern void
 lto_codegen_set_assembler_args(lto_code_gen_t cg, const char **args,
@@ -298,6 +357,8 @@ lto_codegen_set_assembler_args(lto_code_gen_t cg, const char **args,
 /**
  * Sets the strategy to use during internalize.  Default strategy is
  * LTO_INTERNALIZE_FULL.
+ *
+ * \since LTO_API_VERSION=6
  */
 extern void
 lto_codegen_set_internalize_strategy(lto_code_gen_t cg,
@@ -306,6 +367,8 @@ lto_codegen_set_internalize_strategy(lto_code_gen_t cg,
 /**
  * Tells LTO optimization passes that this symbol must be preserved
  * because it is referenced by native code or a command line option.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern void
 lto_codegen_add_must_preserve_symbol(lto_code_gen_t cg, const char* symbol);
@@ -314,6 +377,8 @@ lto_codegen_add_must_preserve_symbol(lto_code_gen_t cg, const char* symbol);
  * Writes a new object file at the specified path that contains the
  * merged contents of all modules added so far.
  * Returns true on error (check lto_get_error_message() for details).
+ *
+ * \since LTO_API_VERSION=5
  */
 extern lto_bool_t
 lto_codegen_write_merged_modules(lto_code_gen_t cg, const char* path);
@@ -325,6 +390,8 @@ lto_codegen_write_merged_modules(lto_code_gen_t cg, const char* path);
  * lto_code_gen_t and will be freed when lto_codegen_dispose()
  * is called, or lto_codegen_compile() is called again.
  * On failure, returns NULL (check lto_get_error_message() for details).
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern const void*
 lto_codegen_compile(lto_code_gen_t cg, size_t* length);
@@ -332,6 +399,8 @@ lto_codegen_compile(lto_code_gen_t cg, size_t* length);
 /**
  * Generates code for all added modules into one native object file.
  * The name of the file is written to name. Returns true on error.
+ *
+ * \since LTO_API_VERSION=5
  */
 extern lto_bool_t
 lto_codegen_compile_to_file(lto_code_gen_t cg, const char** name);
@@ -339,6 +408,8 @@ lto_codegen_compile_to_file(lto_code_gen_t cg, const char** name);
 
 /**
  * Sets options to help debug codegen bugs.
+ *
+ * \since prior to LTO_API_VERSION=3
  */
 extern void
 lto_codegen_debug_options(lto_code_gen_t cg, const char *);
@@ -346,6 +417,8 @@ lto_codegen_debug_options(lto_code_gen_t cg, const char *);
 /**
  * Initializes LLVM disassemblers.
  * FIXME: This doesn't really belong here.
+ *
+ * \since LTO_API_VERSION=5
  */
 extern void
 lto_initialize_disassembler(void);
