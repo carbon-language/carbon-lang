a9b3aee516211c4c92e2d5f8e7c7600d68cc5021
Joerg Sonnenberger <joerg@bec.de>
2014-01-17 14:06:22 +0000

    Support NetBSD/ARM.
    llvm-svn: 199492

--- compiler-rt/lib/clear_cache.c
+++ compiler-rt/lib/clear_cache.c
@@ -13,6 +13,9 @@
 #if __APPLE__
   #include <libkern/OSCacheControl.h>
 #endif
+#if defined(__NetBSD__) && defined(__arm__)
+  #include <machine/sysarch.h>
+#endif
 
 /*
  * The compiler generates calls to __clear_cache() when creating 
@@ -28,6 +31,13 @@ void __clear_cache(void* start, void* end)
  * Intel processors have a unified instruction and data cache
  * so there is nothing to do
  */
+#elif defined(__NetBSD__) && defined(__arm__)
+  struct arm_sync_icache_args arg;
+
+  arg.addr = (uintptr_t)start;
+  arg.len = (uintptr_t)end - (uintptr_t)start;
+
+  sysarch(ARM_SYNC_ICACHE, &arg);
 #else
     #if __APPLE__
         /* On Darwin, sys_icache_invalidate() provides this functionality */
