eafb76ee9bfcf9da9c276a9762e80c0966767a65
Simon Atanasyan <simon@atanasyan.com>
2014-01-18 21:13:05 +0000

    [Mips] Specify the type of a symbol table entries in the test assembler file.
    llvm-svn: 199561

--- lld/test/elf/Mips/Inputs/ext.s
+++ lld/test/elf/Mips/Inputs/ext.s
@@ -10,36 +10,42 @@
 #   lld -flavor gnu -target mipsel -e ext1 -o %t2 %t1
 
     .global ext1
+    .type   ext1,@function
     .ent    ext1
 ext1:
     nop
     .end    ext1
 
     .global ext2
+    .type   ext2,@function
     .ent    ext2
 ext2:
     nop
     .end    ext2
 
     .global ext3
+    .type   ext3,@function
     .ent    ext3
 ext3:
     nop
     .end    ext3
 
     .global ext4
+    .type   ext4,@function
     .ent    ext4
 ext4:
     nop
     .end    ext4
 
     .global ext5
+    .type   ext5,@function
     .ent    ext5
 ext5:
     nop
     .end    ext5
 
     .global ext6
+    .type   ext6,@function
     .ent    ext6
 ext6:
     nop
