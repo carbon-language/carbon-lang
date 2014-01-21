cb6e1257ffb92a8ebad80bb62e3b331a9460f8e8
Adrian Prantl <aprantl@apple.com>
2014-01-21 18:23:43 +0000

    revert 199757 for buildbot breakage.
    llvm-svn: 199758

--- clang/test/CodeGen/debug-info-typedef.c
+++
@@ -1,8 +0,0 @@
-// RUN: %clang_cc1 -emit-llvm -g -I%p %s -o - | FileCheck %s
-// Test that the location of the typedef points to the header file.
-#include "typedef.h"
-
-MyType a;
-
-// CHECK: metadata ![[HEADER:[0-9]+]]} ; [ DW_TAG_typedef ] [MyType] [line 1, size 0, align 0, offset 0] [from int]
-// CHECK: ![[HEADER]] = {{.*}}debug-info-typedef.h",
--- clang/test/CodeGen/debug-info-typedef.h
+++
@@ -1,2 +0,0 @@
-typedef int MyType;
-
