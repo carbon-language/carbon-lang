1fc9e613564f7dd8eae28f05152185de4d3003ce
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-02-03 15:12:09 +0000

    [asan] asan_device_setup.sh: allow overriding adb path.
    llvm-svn: 200698

--- compiler-rt/lib/asan/scripts/asan_device_setup.sh
+++ compiler-rt/lib/asan/scripts/asan_device_setup.sh
@@ -65,9 +65,9 @@ while [[ $# > 0 ]]; do
   shift
 done
 
-ADB="adb"
+ADB=${ADB:-adb}
 if [[ x$device != x ]]; then
-    ADB="adb -s $device"
+    ADB="$ADB -s $device"
 fi
 
 ASAN_RT="libclang_rt.asan-arm-android.so"
