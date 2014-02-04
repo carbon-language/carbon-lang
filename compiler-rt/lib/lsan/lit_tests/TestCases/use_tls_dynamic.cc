b965a2fa32874c92eb91be18ff8432f6c73a4449
Kostya Serebryany <kcc@google.com>
2014-02-04 09:07:16 +0000

    [lsan] simplify use_tls_dynamic.cc test
    llvm-svn: 200748

--- compiler-rt/lib/lsan/lit_tests/TestCases/use_tls_dynamic.cc
+++ compiler-rt/lib/lsan/lit_tests/TestCases/use_tls_dynamic.cc
@@ -1,11 +1,12 @@
 // Test that dynamically allocated TLS space is included in the root set.
 // RUN: LSAN_BASE="report_objects=1:use_stacks=0:use_registers=0"
-// RUN: %clangxx %p/SharedLibs/huge_tls_lib_so.cc -fPIC -shared -o %t-so.so
+// RUN: %clangxx %s -DBUILD_DSO -fPIC -shared -o %t-so.so
 // RUN: %clangxx_lsan %s -o %t
 // RUN: LSAN_OPTIONS=$LSAN_BASE:"use_tls=0" not %t 2>&1 | FileCheck %s
 // RUN: LSAN_OPTIONS=$LSAN_BASE:"use_tls=1" %t 2>&1
 // RUN: LSAN_OPTIONS="" %t 2>&1
 
+#ifndef BUILD_DSO
 #include <assert.h>
 #include <dlfcn.h>
 #include <stdio.h>
@@ -22,6 +23,7 @@ int main(int argc, char *argv[]) {
   assert(dlerror() == 0);
 
   void *p = malloc(1337);
+  // If we don't  know about dynamic TLS, we will return a false leak above.
   void **p_in_tls = StoreToTLS(p);
   assert(*p_in_tls == p);
   fprintf(stderr, "Test alloc: %p.\n", p);
@@ -31,3 +33,18 @@ int main(int argc, char *argv[]) {
 // CHECK: LeakSanitizer: detected memory leaks
 // CHECK: [[ADDR]] (1337 bytes)
 // CHECK: SUMMARY: {{(Leak|Address)}}Sanitizer:
+
+#else  // BUILD_DSO
+// A loadable module with a large thread local section, which would require
+// allocation of a new TLS storage chunk when loaded with dlopen(). We use it
+// to test the reachability of such chunks in LSan tests.
+
+// This must be large enough that it doesn't fit into preallocated static TLS
+// space (see STATIC_TLS_SURPLUS in glibc).
+__thread void *huge_thread_local_array[(1 << 20) / sizeof(void *)]; // NOLINT
+
+extern "C" void **StoreToTLS(void *p) {
+  huge_thread_local_array[0] = p;
+  return &huge_thread_local_array[0];
+}
+#endif  // BUILD_DSO
