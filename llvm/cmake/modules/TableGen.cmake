e6bf06bbdcf80d6480584318675fedd46cc4d86e
NAKAMURA Takumi <geek4civic@gmail.com>
2014-02-04 01:17:12 +0000

    Revert r200150, "[CMake] tablegen(): Use -I <dir> according to the list by include_directories()."
    It missed include/llvm/Target. Could I avoid GLOB_RECURSE anyways? :(
    FYI, I intended to prune ${LLVM_MAIN_INCLUDE_DIR} in TableGen.cmake in r200150.
    
    llvm-svn: 200730

--- llvm/cmake/modules/TableGen.cmake
+++ llvm/cmake/modules/TableGen.cmake
@@ -3,16 +3,8 @@
 # Adds the name of the generated file to TABLEGEN_OUTPUT.
 
 function(tablegen project ofn)
-  # Use the list by include_directories().
-  get_property(include_dirs DIRECTORY PROPERTY INCLUDE_DIRECTORIES)
-
-  # Collect possible dependent *.td(s).
-  # FIXME: It is far from optimal.
-  file(GLOB dependent_tds "*.td")
-  foreach(inc ${include_dirs})
-    file(GLOB tds "${inc}/*.td")
-    list(APPEND dependent_tds ${tds})
-  endforeach()
+  file(GLOB local_tds "*.td")
+  file(GLOB_RECURSE global_tds "${LLVM_MAIN_INCLUDE_DIR}/llvm/*.td")
 
   if (IS_ABSOLUTE ${LLVM_TARGET_DEFINITIONS})
     set(LLVM_TARGET_DEFINITIONS_ABSOLUTE ${LLVM_TARGET_DEFINITIONS})
@@ -20,20 +12,16 @@ function(tablegen project ofn)
     set(LLVM_TARGET_DEFINITIONS_ABSOLUTE 
       ${CMAKE_CURRENT_SOURCE_DIR}/${LLVM_TARGET_DEFINITIONS})
   endif()
-  foreach(inc ${include_dirs})
-    list(APPEND TABLEGEN_INCLUDE_DIRECTORIES -I ${inc})
-  endforeach()
   add_custom_command(OUTPUT ${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp
     # Generate tablegen output in a temporary file.
-    COMMAND ${${project}_TABLEGEN_EXE} ${ARGN}
-    -I ${CMAKE_CURRENT_SOURCE_DIR}
-    ${TABLEGEN_INCLUDE_DIRECTORIES}
+    COMMAND ${${project}_TABLEGEN_EXE} ${ARGN} -I ${CMAKE_CURRENT_SOURCE_DIR}
+    -I ${LLVM_MAIN_SRC_DIR}/lib/Target -I ${LLVM_MAIN_INCLUDE_DIR}
     ${LLVM_TARGET_DEFINITIONS_ABSOLUTE} 
     -o ${CMAKE_CURRENT_BINARY_DIR}/${ofn}.tmp
     # The file in LLVM_TARGET_DEFINITIONS may be not in the current
     # directory and local_tds may not contain it, so we must
     # explicitly list it here:
-    DEPENDS ${${project}_TABLEGEN_EXE} ${dependent_tds}
+    DEPENDS ${${project}_TABLEGEN_EXE} ${local_tds} ${global_tds}
     ${LLVM_TARGET_DEFINITIONS_ABSOLUTE}
     COMMENT "Building ${ofn}..."
     )
