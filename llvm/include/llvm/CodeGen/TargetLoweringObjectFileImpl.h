3d8a106f6f3afae80e02023721cbf65edc9c0c43
Rafael Espindola <rafael.espindola@gmail.com>
2014-02-07 22:45:13 +0000

    Comment cleanup. Don't repeat the function name in the comment.
    llvm-svn: 201001

--- llvm/include/llvm/CodeGen/TargetLoweringObjectFileImpl.h
+++ llvm/include/llvm/CodeGen/TargetLoweringObjectFileImpl.h
@@ -42,8 +42,8 @@ public:
                                     const TargetMachine &TM,
                                     const MCSymbol *Sym) const;
 
-  /// getSectionForConstant - Given a constant with the SectionKind, return a
-  /// section that it should be placed in.
+  /// Given a constant with the SectionKind, return a section that it should be
+  /// placed in.
   virtual const MCSection *getSectionForConstant(SectionKind Kind) const;
 
 
@@ -55,14 +55,14 @@ public:
   SelectSectionForGlobal(const GlobalValue *GV, SectionKind Kind,
                          Mangler *Mang, const TargetMachine &TM) const;
 
-  /// getTTypeGlobalReference - Return an MCExpr to use for a reference to the
-  /// specified type info global variable from exception handling information.
+  /// Return an MCExpr to use for a reference to the specified type info global
+  /// variable from exception handling information.
   virtual const MCExpr *
   getTTypeGlobalReference(const GlobalValue *GV, Mangler *Mang,
                           MachineModuleInfo *MMI, unsigned Encoding,
                           MCStreamer &Streamer) const;
 
-  // getCFIPersonalitySymbol - The symbol that gets passed to .cfi_personality.
+  // The symbol that gets passed to .cfi_personality.
   virtual MCSymbol *
   getCFIPersonalitySymbol(const GlobalValue *GV, Mangler *Mang,
                           MachineModuleInfo *MMI) const;
@@ -80,12 +80,11 @@ class TargetLoweringObjectFileMachO : public TargetLoweringObjectFile {
 public:
   virtual ~TargetLoweringObjectFileMachO() {}
 
-  /// getDepLibFromLinkerOpt - Extract the dependent library name from a linker
-  /// option string. Returns StringRef() if the option does not specify a library.
+  /// Extract the dependent library name from a linker option string. Returns
+  /// StringRef() if the option does not specify a library.
   virtual StringRef getDepLibFromLinkerOpt(StringRef LinkerOption) const;
 
-  /// emitModuleFlags - Emit the module flags that specify the garbage
-  /// collection information.
+  /// Emit the module flags that specify the garbage collection information.
   virtual void emitModuleFlags(MCStreamer &Streamer,
                                ArrayRef<Module::ModuleFlagEntry> ModuleFlags,
                                Mangler *Mang, const TargetMachine &TM) const;
@@ -100,20 +99,19 @@ public:
 
   virtual const MCSection *getSectionForConstant(SectionKind Kind) const;
 
-  /// shouldEmitUsedDirectiveFor - This hook allows targets to selectively
-  /// decide not to emit the UsedDirective for some symbols in llvm.used.
+  /// This hook allows targets to selectively decide not to emit the
+  /// UsedDirective for some symbols in llvm.used.
   /// FIXME: REMOVE this (rdar://7071300)
   virtual bool shouldEmitUsedDirectiveFor(const GlobalValue *GV,
                                           Mangler *) const;
 
-  /// getTTypeGlobalReference - The mach-o version of this method
-  /// defaults to returning a stub reference.
+  /// The mach-o version of this method defaults to returning a stub reference.
   virtual const MCExpr *
   getTTypeGlobalReference(const GlobalValue *GV, Mangler *Mang,
                           MachineModuleInfo *MMI, unsigned Encoding,
                           MCStreamer &Streamer) const;
 
-  // getCFIPersonalitySymbol - The symbol that gets passed to .cfi_personality.
+  // The symbol that gets passed to .cfi_personality.
   virtual MCSymbol *
   getCFIPersonalitySymbol(const GlobalValue *GV, Mangler *Mang,
                           MachineModuleInfo *MMI) const;
@@ -133,12 +131,12 @@ public:
   SelectSectionForGlobal(const GlobalValue *GV, SectionKind Kind,
                          Mangler *Mang, const TargetMachine &TM) const;
 
-  /// getDepLibFromLinkerOpt - Extract the dependent library name from a linker
-  /// option string. Returns StringRef() if the option does not specify a library.
+  /// Extract the dependent library name from a linker option string. Returns
+  /// StringRef() if the option does not specify a library.
   virtual StringRef getDepLibFromLinkerOpt(StringRef LinkerOption) const;
 
-  /// emitModuleFlags - Emit Obj-C garbage collection and linker options.  Only
-  /// linker option emission is implemented for COFF.
+  /// Emit Obj-C garbage collection and linker options. Only linker option
+  /// emission is implemented for COFF.
   virtual void emitModuleFlags(MCStreamer &Streamer,
                                ArrayRef<Module::ModuleFlagEntry> ModuleFlags,
                                Mangler *Mang, const TargetMachine &TM) const;
