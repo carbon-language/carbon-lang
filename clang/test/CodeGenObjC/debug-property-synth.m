ab340334dce9c99304382c48840f11019ba7d2b2
Adrian Prantl <aprantl@apple.com>
2014-02-12 18:24:04 +0000

    Revert "Remove bad debug info test."
    This reverts commit r201183.
    The test, albeit undocumented and badly written is still testing something
    useful. It will be updated in a subsequent commit.
    
    llvm-svn: 201247

---
+++ clang/test/CodeGenObjC/debug-property-synth.m
@@ -0,0 +1,20 @@
+// RUN: %clang_cc1 -triple %itanium_abi_triple -masm-verbose -S -g %s -o - | FileCheck %s
+// Radar 9468526
+@interface I {
+  int _p1;
+}
+@property int p1;
+@end
+
+@implementation I
+@synthesize p1 = _p1;
+@end
+
+int main() {
+  I *myi;
+  myi.p1 = 2;
+  return 0;
+}
+
+// FIXME: Make this test ir files.
+// CHECK:       .loc    2 6 0
