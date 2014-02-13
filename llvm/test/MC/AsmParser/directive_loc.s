8d9be9a4cfa99592ac36e82d08f8e3e581d04602
Diego Novillo <dnovillo@google.com>
2014-02-13 20:16:42 +0000

    Simplify checks in MC/AsmParser/directive_loc.s
    llvm-svn: 201361

--- llvm/test/MC/AsmParser/directive_loc.s
+++ llvm/test/MC/AsmParser/directive_loc.s
@@ -1,18 +1,14 @@
-# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s -check-prefix=FILE
-# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s -check-prefix=BASIC-LOC-1
-# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s -check-prefix=BASIC-LOC-2
-# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s -check-prefix=DISCRIMINATOR
-# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s -check-prefix=ISA
+# RUN: llvm-mc -triple i386-unknown-unknown %s | FileCheck %s
 
         .file 1 "hello"
-# FILE: .file 1 "hello"
+# CHECK: .file 1 "hello"
         .loc 1
         .loc 1 2
-# BASIC-LOC-1: .loc 1 2 0
+# CHECK: .loc 1 2 0
         .loc 1 2 3
-# BASIC-LOC-2: .loc 1 2 3
+# CHECK: .loc 1 2 3
         .loc 1 2 discriminator 1
-# DISCRIMINATOR: 1 2 0 discriminator 1
+# CHECK: 1 2 0 discriminator 1
         .loc 1 2 0 isa 3
-# ISA: 1 2 0 isa 3
+# CHECK: 1 2 0 isa 3
         .loc 1 0
