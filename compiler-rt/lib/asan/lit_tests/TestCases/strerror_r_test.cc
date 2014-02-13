788527d9cdf69dd031f294029106e6811e93378f
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-02-13 12:24:10 +0000

    Remove a check from strerror_r test.
    It's not always true: on Android, strerror_r with invalid errno
    prints "Unknown error ..." to the buffer and returns 0.
    
    This test now only checks that strerror_r does not crash.
    
    llvm-svn: 201321

--- compiler-rt/lib/asan/lit_tests/TestCases/strerror_r_test.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/strerror_r_test.cc
@@ -4,10 +4,11 @@
 
 #include <assert.h>
 #include <string.h>
+#include <stdio.h>
 
 int main() {
   char buf[1024];
   char *res = (char *)strerror_r(300, buf, sizeof(buf));
-  assert(res != 0);
+  printf("%p\n", res);
   return 0;
 }
