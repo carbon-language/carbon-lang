4e6096b859aa9e37dafd952de7e4cef40561c50c
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-02-13 14:37:04 +0000

    [asan] Remove extra clone() arguments in test.
    Android headers define clone() as a 4-argument function without ellipsis.
    
    llvm-svn: 201331

--- compiler-rt/lib/asan/lit_tests/TestCases/Linux/clone_test.cc
+++ compiler-rt/lib/asan/lit_tests/TestCases/Linux/clone_test.cc
@@ -24,7 +24,7 @@ int main(int argc, char **argv) {
   char child_stack[kStackSize + 1];
   char *sp = child_stack + kStackSize;  // Stack grows down.
   printf("Parent: %p\n", sp);
-  pid_t clone_pid = clone(Child, sp, CLONE_FILES | CLONE_VM, NULL, 0, 0, 0);
+  pid_t clone_pid = clone(Child, sp, CLONE_FILES | CLONE_VM, NULL);
   int status;
   pid_t wait_result = waitpid(clone_pid, &status, __WCLONE);
   if (wait_result < 0) {
