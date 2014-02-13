f79669a32365144e60cb85f13674973acc982bff
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-02-13 14:33:24 +0000

    [asan] Enable signal and sigaction interceptors on Android.
    Fixes AddressSanitizer.SignalTest breakage.
    
    llvm-svn: 201330

--- compiler-rt/lib/asan/asan_interceptors.cc
+++ compiler-rt/lib/asan/asan_interceptors.cc
@@ -186,6 +186,16 @@ INTERCEPTOR(int, pthread_create, void *thread,
 #endif  // ASAN_INTERCEPT_PTHREAD_CREATE
 
 #if ASAN_INTERCEPT_SIGNAL_AND_SIGACTION
+
+#if SANITIZER_ANDROID
+INTERCEPTOR(void*, bsd_signal, int signum, void *handler) {
+  if (!AsanInterceptsSignal(signum) ||
+      common_flags()->allow_user_segv_handler) {
+    return REAL(bsd_signal)(signum, handler);
+  }
+  return 0;
+}
+#else
 INTERCEPTOR(void*, signal, int signum, void *handler) {
   if (!AsanInterceptsSignal(signum) ||
       common_flags()->allow_user_segv_handler) {
@@ -193,6 +203,7 @@ INTERCEPTOR(void*, signal, int signum, void *handler) {
   }
   return 0;
 }
+#endif
 
 INTERCEPTOR(int, sigaction, int signum, const struct sigaction *act,
                             struct sigaction *oldact) {
@@ -754,7 +765,11 @@ void InitializeAsanInterceptors() {
   ASAN_INTERCEPT_FUNC(longjmp);
 #if ASAN_INTERCEPT_SIGNAL_AND_SIGACTION
   ASAN_INTERCEPT_FUNC(sigaction);
+#if SANITIZER_ANDROID
+  ASAN_INTERCEPT_FUNC(bsd_signal);
+#else
   ASAN_INTERCEPT_FUNC(signal);
+#endif
 #endif
 #if ASAN_INTERCEPT_SWAPCONTEXT
   ASAN_INTERCEPT_FUNC(swapcontext);
