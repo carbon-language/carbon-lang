9098f1d3d96e36e3892c63d295de591791cc8849
Jason Molenda <jmolenda@apple.com>
2014-02-13 04:19:32 +0000

    Change the ABI CallFrameAddressIsValid methods for i386 and x86_64. They were enforcing 16-byte alignment on stack frames for Darwin x86 programs. But we've found that trap handlers typically don't have the stack pointer aligned correctly when a trap happens and lldb wasn't backtracing all the way through.  This method is only used as a safety guard to prevent lldb's unwinder from using a bogus address as a stack frame - we'll still enforce word-size alignment on stack frames so that should be fine.
    Also rolled back akaylor's changes from August 2013 in r188952 which changed
    the i386 ABI plugin to relax the CallFrameAddressIsValid offsets for non-Darwin
    targets where only 4-byte alignment is enforced.  Now Darwin is the same as
    those environments.
    
    <rdar://problem/15982682> 
    
    llvm-svn: 201292

--- lldb/source/Plugins/ABI/SysV-x86_64/ABISysV_x86_64.h
+++ lldb/source/Plugins/ABI/SysV-x86_64/ABISysV_x86_64.h
@@ -68,12 +68,22 @@ public:
         return true;
     }
     
+    // The SysV x86_64 ABI requires that stack frames be 16 byte aligned.
+    // When there is a trap handler on the stack, e.g. _sigtramp in userland
+    // code, we've seen that the stack pointer is often not aligned properly
+    // before the handler is invoked.  This means that lldb will stop the unwind
+    // early -- before the function which caused the trap.
+    //
+    // To work around this, we relax that alignment to be just word-size (8-bytes).
+    // Whitelisting the trap handlers for user space would be easy (_sigtramp) but
+    // in other environments there can be a large number of different functions
+    // involved in async traps.
     virtual bool
     CallFrameAddressIsValid (lldb::addr_t cfa)
     {
-        // Make sure the stack call frame addresses are 16 byte aligned
-        if (cfa & (16ull - 1ull))
-            return false;   // Not 16 byte aligned
+        // Make sure the stack call frame addresses are 8 byte aligned
+        if (cfa & (8ull - 1ull))
+            return false;   // Not 8 byte aligned
         if (cfa == 0)
             return false;   // Zero is not a valid stack address
         return true;
