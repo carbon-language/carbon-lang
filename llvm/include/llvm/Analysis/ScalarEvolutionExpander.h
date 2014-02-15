847d96142c60245bd58007b0499816a089332392
Arnold Schwaighofer <aschwaighofer@apple.com>
2014-02-15 18:16:56 +0000

    Revert "SCEVExpander: Try hard not to create derived induction variables in other loops"
    This reverts commit r201465. It broke an arm bot.
    
    llvm-svn: 201466

--- llvm/include/llvm/Analysis/ScalarEvolutionExpander.h
+++ llvm/include/llvm/Analysis/ScalarEvolutionExpander.h
@@ -260,9 +260,7 @@ namespace llvm {
     PHINode *getAddRecExprPHILiterally(const SCEVAddRecExpr *Normalized,
                                        const Loop *L,
                                        Type *ExpandTy,
-                                       Type *IntTy,
-                                       Type *&TruncTy,
-                                       bool &InvertStep);
+                                       Type *IntTy);
     Value *expandIVInc(PHINode *PN, Value *StepV, const Loop *L,
                        Type *ExpandTy, Type *IntTy, bool useSubtract);
   };
