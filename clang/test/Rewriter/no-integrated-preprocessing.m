4f6428030dd98cdbc10632ed67ddc39055380ece
Nico Rieck <nico.rieck@gmail.com>
2014-02-16 08:18:13 +0000

    Use -target instead of ignored -arch to fix x86_64 builders
    llvm-svn: 201480

--- clang/test/Rewriter/no-integrated-preprocessing.m
+++ clang/test/Rewriter/no-integrated-preprocessing.m
@@ -1,4 +1,4 @@
-// RUN: %clang -arch i386 -fms-extensions -rewrite-objc %s -o %t-rw.cpp
+// RUN: %clang -target i386-unknown-unknown -fms-extensions -rewrite-objc %s -o %t-rw.cpp
 // RUN: FileCheck %s < %t-rw.cpp
 // rdar://12189793
 
