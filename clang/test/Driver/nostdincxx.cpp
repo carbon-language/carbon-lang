088488777552577008d93448e78ef3f0b9a671fd
NAKAMURA Takumi <geek4civic@gmail.com>
2014-02-16 10:15:57 +0000

    check-clang: Introduce the feature "non-ms-sdk" to suppress Driver/nostdincxx.cpp on msvc.
    llvm-svn: 201484

--- clang/test/Driver/nostdincxx.cpp
+++ clang/test/Driver/nostdincxx.cpp
@@ -1,4 +1,6 @@
 // RUN: not %clangxx -nostdinc++ %s 2>&1 | FileCheck %s
-// XFAIL: win32
 // CHECK: file not found
 #include <vector> 
+
+// MSVC has C++ headers in same directory as C headers.
+// REQUIRES: non-ms-sdk
