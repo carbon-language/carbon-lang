b549019c3c3a0cd417fcc9dc4ec17340de4c6046
Peter Collingbourne <peter@pcc.me.uk>
2014-02-18 19:46:01 +0000

    Add a makefile for clang-query. Patch by Alex Horn!
    While at it, have cmake build and test the tool if libedit is not installed,
    as this dependency is now optional.
    
    llvm-svn: 201599

--- clang-tools-extra/test/clang-query/errors.c
+++ clang-tools-extra/test/clang-query/errors.c
@@ -2,7 +2,6 @@
 // RUN: not clang-query -f %S/Inputs/foo.script %s -- | FileCheck %s
 // RUN: not clang-query -f %S/Inputs/nonexistent.script %s -- 2>&1 | FileCheck --check-prefix=CHECK-NONEXISTENT %s
 // RUN: not clang-query -c foo -f foo %s -- 2>&1 | FileCheck --check-prefix=CHECK-BOTH %s
-// REQUIRES: libedit
 
 // CHECK: unknown command: foo
 // CHECK-NOT: unknown command: bar
--- clang-tools-extra/test/clang-query/function-decl.c
+++ clang-tools-extra/test/clang-query/function-decl.c
@@ -1,5 +1,4 @@
 // RUN: clang-query -c "match functionDecl()" %s -- | FileCheck %s
-// REQUIRES: libedit
 
-// CHECK: function-decl.c:5:1: note: "root" binds here
+// CHECK: function-decl.c:4:1: note: "root" binds here
 void foo(void) {}
