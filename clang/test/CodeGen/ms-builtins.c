7281928be6f2e163ad5b0190485ffe6d6fedfa94
Warren Hunt <whunt@google.com>
2014-02-19 23:57:54 +0000

    Updated to r201734.  Removed unused declaration from lit test.
    Also updating lit test to be more roboust (changing fixed offsets to 
    flexible offsets)
    
    llvm-svn: 201742

--- clang/test/CodeGen/ms-builtins.c
+++ clang/test/CodeGen/ms-builtins.c
@@ -1,6 +1,5 @@
 // RUN: %clang_cc1 -triple=i686-unknown-unknown -fms-extensions -emit-llvm -o - %s | FileCheck %s
 
-extern void printf(const char*, ...);
 void f(char *a, volatile long* b) {
   _mm_prefetch(a, 0);
   _mm_prefetch(a, 1);
@@ -12,10 +11,10 @@ void f(char *a, volatile long* b) {
   _InterlockedExchangeAdd(b, 2);
 };
 
-// CHECK: call void @llvm.prefetch(i8* %1, i32 0, i32 0, i32 1)
-// CHECK: call void @llvm.prefetch(i8* %3, i32 0, i32 1, i32 1)
-// CHECK: call void @llvm.prefetch(i8* %5, i32 0, i32 2, i32 1)
-// CHECK: call void @llvm.prefetch(i8* %7, i32 0, i32 3, i32 1)
+// CHECK: call void @llvm.prefetch(i8* %{{.*}}, i32 0, i32 0, i32 1)
+// CHECK: call void @llvm.prefetch(i8* %{{.*}}, i32 0, i32 1, i32 1)
+// CHECK: call void @llvm.prefetch(i8* %{{.*}}, i32 0, i32 2, i32 1)
+// CHECK: call void @llvm.prefetch(i8* %{{.*}}, i32 0, i32 3, i32 1)
 // CHECK: cmpxchg
 // CHECK: atomicrmw volatile add
 // CHECK: atomicrmw volatile sub
