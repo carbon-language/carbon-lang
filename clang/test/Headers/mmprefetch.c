2b05fd96d5e3302389625749ef5e8d75b0c23dfb
Daniel Jasper <djasper@google.com>
2014-02-20 08:45:49 +0000

    Don't actually emit output in test/Headers/mmprefetch.c.
    The test directory cannot assumed to be writable.
    
    llvm-svn: 201768

--- clang/test/Headers/mmprefetch.c
+++ clang/test/Headers/mmprefetch.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple=x86_64-unknown-unknown -fms-extensions -emit-llvm -verify %s
+// RUN: %clang_cc1 -triple=x86_64-unknown-unknown -fms-extensions -emit-llvm-only -verify %s
 
 #include <mmintrin.h>
 
