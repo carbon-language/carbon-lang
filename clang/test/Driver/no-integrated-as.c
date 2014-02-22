42e4b5997a2cad5f505dfb997b83f14f91ef0546
Saleem Abdulrasool <compnerd@compnerd.org>
2014-02-22 23:37:58 +0000

    clang: forward -no-integrated-as from the driver
    Forward the -no-integrated-as option to -cc1 rather than simply invoking the
    appropriate tool.  This is useful since this option has been overloaded to
    permit disabling of parsing inline assembly at the MC layer.
    
    This re-applies the previous version of the patch with a renaming of the driver
    option to the public name rather than the internal name (-target vs -triple).
    The actual failure is fixed separately of an overly aggressive negative pattern
    match in the MIPS driver tests.  It also fixes the incorrect test for targets
    that have the integrated assembler disabled by default.
    
    llvm-svn: 201960

---
+++ clang/test/Driver/no-integrated-as.c
@@ -0,0 +1,19 @@
+// RUN: %clang -target i386 -### -no-integrated-as -c %s 2>&1 \
+// RUN:     | FileCheck %s -check-prefix NOIAS
+
+// NOIAS: -no-integrated-as
+
+// RUN: %clang -target i386 -### -integrated-as -c %s 2>&1 \
+// RUN:     | FileCheck %s -check-prefix IAS
+
+// IAS-NOT: -no-integrated-as
+
+// RUN: %clang -target i386 -### -c %s 2>&1 | FileCheck %s -check-prefix DEFAULT
+
+// DEFAULT-NOT: -no-integrated-as
+
+// RUN: %clang -target msp430 -### -c %s 2>&1 \
+// RUN:     | FileCheck %s -check-prefix NO-IAS-DEFAULT
+
+// NO-IAS-DEFAULT: -no-integrated-as
+
