8587f0ca99a0365c05b52cb89e32820e7ec3874a
Alp Toker <alp@nuanti.com>
2014-02-24 08:17:02 +0000

    Add driver test for multiple preprocessor inputs
    Fix an unintentional stdin read in the darwin-asan-nofortify.c test and replace
    it with an explicit test for multiple -E inputs passed to the driver.
    
    Noticed while working on the in-process driver patch.
    
    llvm-svn: 202007

--- clang/test/Driver/darwin-asan-nofortify.c
+++ clang/test/Driver/darwin-asan-nofortify.c
@@ -1,6 +1,6 @@
 // Make sure AddressSanitizer disables _FORTIFY_SOURCE on Darwin.
 
-// RUN: %clang -faddress-sanitizer %s -E -dM -target x86_64-darwin - | FileCheck %s
-// RUN: %clang -fsanitize=address  %s -E -dM -target x86_64-darwin - | FileCheck %s
+// RUN: %clang -faddress-sanitizer %s -E -dM -target x86_64-darwin | FileCheck %s
+// RUN: %clang -fsanitize=address  %s -E -dM -target x86_64-darwin | FileCheck %s
 
 // CHECK: #define _FORTIFY_SOURCE 0
