5090d9a7320bfe6058ea867f045bf834d544c9a5
David Majnemer <david.majnemer@gmail.com>
2014-02-25 01:47:33 +0000

    AST: Simplify CharUnits::alignmentAtOffset
    CharUnits::alignmentAtOffset is equivalent to llvm::MinAlign but
    slightly less efficient.  Use it's implementation instead.
    
    llvm-svn: 202099

--- clang/include/clang/AST/CharUnits.h
+++ clang/include/clang/AST/CharUnits.h
@@ -173,12 +173,7 @@ namespace clang {
       /// Given that this is a non-zero alignment value, what is the
       /// alignment at the given offset?
       CharUnits alignmentAtOffset(CharUnits offset) {
-        // alignment: 0010000
-        // offset:    1011100
-        // lowBits:   0001011
-        // result:    0000100
-        QuantityType lowBits = (Quantity-1) & (offset.Quantity-1);
-        return CharUnits((lowBits + 1) & ~lowBits);
+        return CharUnits(llvm::MinAlign(Quantity, offset.Quantity));
       }
 
 
