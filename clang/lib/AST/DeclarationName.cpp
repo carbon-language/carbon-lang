48f52e926d6e8273587f5befc997fc784b04855b
Benjamin Kramer <benny.kra@googlemail.com>
2014-02-25 17:26:26 +0000

    Pretty Printer: Fix printing of conversion operator decls and calls.
    - Don't emit anything when we encounter a call to a conversion operator.
        "bar(a & b)" instead of "bar(a & b.operator int())"
      This preserves the semantics and is still idempotent if we print the AST multiple times.
    
    - Properly print declarations of conversion operators.
        "explicit operator bool();" instead of "bool operator _Bool();"
    
    PR18776.
    
    llvm-svn: 202167

--- clang/lib/AST/DeclarationName.cpp
+++ clang/lib/AST/DeclarationName.cpp
@@ -191,6 +191,7 @@ raw_ostream &operator<<(raw_ostream &OS, DeclarationName N) {
       return OS << *Rec->getDecl();
     LangOptions LO;
     LO.CPlusPlus = true;
+    LO.Bool = true;
     return OS << Type.getAsString(PrintingPolicy(LO));
   }
   case DeclarationName::CXXUsingDirective:
@@ -546,6 +547,7 @@ void DeclarationNameInfo::printName(raw_ostream &OS) const {
         OS << "operator ";
       LangOptions LO;
       LO.CPlusPlus = true;
+      LO.Bool = true;
       OS << TInfo->getType().getAsString(PrintingPolicy(LO));
     } else
       OS << Name;
