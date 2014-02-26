16da3240517366168e9d0618eff2d8ce957024df
Marshall Clow <mclow.lists@gmail.com>
2014-02-26 01:56:31 +0000

    Implement LWG issue 2306: match_results::reference should be value_type&, not const value_type&. This is a general move by the LWG to have the reference type of read-only containers be a non-const reference; however, there are no methods that return a non-const reference to a match_result entry, so there's no worries about getting a non-const reference to a constant object.
    llvm-svn: 202214

--- libcxx/test/re/re.results/types.pass.cpp
+++ libcxx/test/re/re.results/types.pass.cpp
@@ -35,7 +35,7 @@ test()
     typedef std::match_results<CharT*> MR;
     static_assert((std::is_same<typename MR::value_type, std::sub_match<CharT*> >::value), "");
     static_assert((std::is_same<typename MR::const_reference, const std::sub_match<CharT*>& >::value), "");
-    static_assert((std::is_same<typename MR::reference, const std::sub_match<CharT*>& >::value), "");
+    static_assert((std::is_same<typename MR::reference, std::sub_match<CharT*>& >::value), "");
     static_assert((!std::is_same<typename MR::const_iterator, void>::value), "");
     static_assert((std::is_same<typename MR::difference_type, std::ptrdiff_t>::value), "");
     static_assert((std::is_same<typename MR::size_type, std::size_t>::value), "");
