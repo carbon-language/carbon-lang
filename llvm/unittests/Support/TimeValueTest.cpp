51a184058dce16539cab86c4db94e259bc9aca99
David Blaikie <dblaikie@gmail.com>
2014-02-26 19:12:28 +0000

    Use the overloaded std::abs rather than C's abs(int) to address Clang's -Wabsolute-value
    llvm-svn: 202286

--- llvm/unittests/Support/TimeValueTest.cpp
+++ llvm/unittests/Support/TimeValueTest.cpp
@@ -17,7 +17,7 @@ namespace {
 TEST(TimeValue, time_t) {
   sys::TimeValue now = sys::TimeValue::now();
   time_t now_t = time(NULL);
-  EXPECT_TRUE(abs(static_cast<long>(now_t - now.toEpochTime())) < 2);
+  EXPECT_TRUE(std::abs(static_cast<long>(now_t - now.toEpochTime())) < 2);
 }
 
 TEST(TimeValue, Win32FILETIME) {
