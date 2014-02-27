c204c130fabdffa131d487ec1c701fc95dba0ffe
Marshall Clow <mclow.lists@gmail.com>
2014-02-27 02:11:50 +0000

    LWG issue #2188: Reverse iterator does not fully support targets that overload operator&. Also mark #2272 and #2299  as complete; libc++ already implements them.
    llvm-svn: 202339

--- libcxx/test/iterators/predef.iterators/reverse.iterators/reverse.iter.ops/reverse.iter.opref/op_arrow.pass.cpp
+++ libcxx/test/iterators/predef.iterators/reverse.iterators/reverse.iter.ops/reverse.iter.opref/op_arrow.pass.cpp
@@ -17,6 +17,7 @@
 //    http://www.open-std.org/jtc1/sc22/wg21/docs/lwg-defects.html#198
 
 #include <iterator>
+#include <list>
 #include <cassert>
 
 class A
@@ -61,9 +62,47 @@ test(It i, typename std::iterator_traits<It>::value_type x)
     assert(r->get() == x.get());
 }
 
+class B
+{
+    int data_;
+public:
+    B(int d=1) : data_(d) {}
+    ~B() {data_ = -1;}
+
+    int get() const {return data_;}
+
+    friend bool operator==(const B& x, const B& y)
+        {return x.data_ == y.data_;}
+    const B *operator&() const { return nullptr; }
+    B       *operator&()       { return nullptr; }
+};
+
 int main()
 {
     test(weird_iterator<A>(A()), A());
     A a;
     test(&a+1, A());
+    
+    {
+    std::list<B> l;
+    l.push_back(B(0));
+    l.push_back(B(1));
+    l.push_back(B(2));
+    
+    {
+    std::list<B>::const_iterator i = l.begin();
+    assert ( i->get() == 0 );  ++i;
+    assert ( i->get() == 1 );  ++i;
+    assert ( i->get() == 2 );  ++i;
+    assert ( i == l.end ());
+    }
+    
+    {
+    std::list<B>::const_reverse_iterator ri = l.rbegin();
+    assert ( ri->get() == 2 );  ++ri;
+    assert ( ri->get() == 1 );  ++ri;
+    assert ( ri->get() == 0 );  ++ri;
+    assert ( ri == l.rend ());
+    }
+    }
 }
