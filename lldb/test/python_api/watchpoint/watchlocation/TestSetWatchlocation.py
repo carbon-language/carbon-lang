163bc1da1bae20a6fc5df5cd10a181f0ea8cca3a
Todd Fiala <tfiala@google.com>
2014-02-28 17:28:44 +0000

    Enabled TestSetWatchlocation.py on Linux.
    This worked 25 out of 25 times for me on Ubuntu 12.04 LTS x86_64 built
    with gcc 4.8.2 and the July 2013 libedit.
    
    llvm-svn: 202516

--- lldb/test/python_api/watchpoint/watchlocation/TestSetWatchlocation.py
+++ lldb/test/python_api/watchpoint/watchlocation/TestSetWatchlocation.py
@@ -30,7 +30,6 @@ class SetWatchlocationAPITestCase(TestBase):
         self.buildDsym()
         self.do_set_watchlocation()
 
-    @skipIfLinux # Sometimes passes, sometimes not.
     @python_api_test
     @dwarf_test
     def test_watch_location_with_dwarf(self):
