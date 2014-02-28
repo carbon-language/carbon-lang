bae1d15491ea283ad30e623f2ae91a779013669a
Todd Fiala <tfiala@google.com>
2014-02-28 17:49:20 +0000

    Adjust TestExprDoesntBlock.py to be expected failure on Linux.
    I suspect I may have misrun the test previously for check-in
    r202456 re: pr15258.  This test fails consistently on my end.
    I changed the test to expected failure on Linux.
    
    llvm-svn: 202517

--- lldb/test/functionalities/expr-doesnt-deadlock/TestExprDoesntBlock.py
+++ lldb/test/functionalities/expr-doesnt-deadlock/TestExprDoesntBlock.py
@@ -24,7 +24,7 @@ class ExprDoesntDeadlockTestCase(TestBase):
 
     @dwarf_test
     @expectedFailureFreeBSD('llvm.org/pr17946')
-    # @skipIfLinux # llvm.org/pr15258: disabled due to assertion failure in ProcessMonitor::GetCrashReasonForSIGSEGV:
+    @expectedFailureLinux('llvm.org/pr15258') # disabled due to assertion failure in ProcessMonitor::GetCrashReasonForSIGSEGV:
     def test_with_dwarf_and_run_command(self):
         """Test that expr will time out and allow other threads to run if it blocks."""
         self.buildDwarf()
