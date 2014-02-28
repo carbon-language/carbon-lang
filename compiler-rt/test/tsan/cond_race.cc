371639ea1f0b4e9c3c16a2d5562c993eac2749cf
Dmitry Vyukov <dvyukov@google.com>
2014-02-28 10:47:07 +0000

    tsan: deflake test
    llvm-svn: 202484

--- compiler-rt/test/tsan/cond_race.cc
+++ compiler-rt/test/tsan/cond_race.cc
@@ -5,6 +5,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
+#include <unistd.h>
 
 struct Ctx {
   pthread_mutex_t m;
@@ -31,6 +32,8 @@ int main() {
   while (!c->done)
     pthread_cond_wait(&c->c, &c->m);
   pthread_mutex_unlock(&c->m);
+  // w/o this sleep, it can be reported as use-after-free
+  sleep(1);
   delete c;
   pthread_join(th, 0);
 }
