eaaf5908d92f838da20e594b5cba9b0e4df55508
Rui Ueyama <ruiu@google.com>
2014-02-28 00:06:20 +0000

    Remove unnecessary temporary variable.
    llvm-svn: 202445

--- llvm/tools/llvm-objdump/COFFDump.cpp
+++ llvm/tools/llvm-objdump/COFFDump.cpp
@@ -224,10 +224,8 @@ static void printCOFFSymbolAddress(llvm::raw_ostream &Out,
                                    const std::vector<RelocationRef> &Rels,
                                    uint64_t Offset, uint32_t Disp) {
   StringRef Sym;
-  if (error_code EC = resolveSymbolName(Rels, Offset, Sym)) {
-    error(EC);
-    return ;
-  }
+  if (error(resolveSymbolName(Rels, Offset, Sym)))
+    return;
   Out << Sym;
   if (Disp > 0)
     Out << format(" + 0x%04x", Disp);
