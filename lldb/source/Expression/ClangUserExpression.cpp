8225d59b3f55d2f4071e0545a0b59a50ada8ef12
Jim Ingham <jingham@apple.com>
2014-03-03 19:16:45 +0000

    Only require thread scope when we're about to run the function on a thread.
    llvm-svn: 202740

--- lldb/source/Expression/ClangUserExpression.cpp
+++ lldb/source/Expression/ClangUserExpression.cpp
@@ -763,12 +763,6 @@ ClangUserExpression::Execute (Stream &error_stream,
     // expression, it's quite convenient to have these logs come out with the STEP log as well.
     Log *log(lldb_private::GetLogIfAnyCategoriesSet (LIBLLDB_LOG_EXPRESSIONS | LIBLLDB_LOG_STEP));
 
-    if (!exe_ctx.HasThreadScope() && !m_can_interpret)
-    {
-        error_stream.Printf("ClangUserExpression::Execute called with no thread selected.");
-        return eExecutionSetupError;
-    }
-        
     if (m_jit_start_addr != LLDB_INVALID_ADDRESS || m_can_interpret)
     {
         lldb::addr_t struct_address = LLDB_INVALID_ADDRESS;
@@ -829,6 +823,12 @@ ClangUserExpression::Execute (Stream &error_stream,
         }
         else
         {
+            if (!exe_ctx.HasThreadScope())
+            {
+                error_stream.Printf("ClangUserExpression::Execute called with no thread selected.");
+                return eExecutionSetupError;
+            }
+                
             Address wrapper_address (m_jit_start_addr);
             
             llvm::SmallVector <lldb::addr_t, 3> args;
