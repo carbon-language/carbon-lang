6419e98ee1bb278c70de883d7762cfcb12eb0bdf
Deepak Panickal <deepak@codeplay.com>
2014-03-03 15:53:37 +0000

    Check if the terminal is interactive and set the flags accordingly
    llvm-svn: 202726

--- lldb/source/Host/common/File.cpp
+++ lldb/source/Host/common/File.cpp
@@ -887,7 +887,13 @@ File::CalculateInteractiveAndTerminal ()
     {
         m_is_interactive = eLazyBoolNo;
         m_is_real_terminal = eLazyBoolNo;
-#ifndef _MSC_VER
+#ifdef _MSC_VER
+        if (_isatty(fd))
+        {
+            m_is_interactive = eLazyBoolYes;
+            m_is_real_terminal = eLazyBoolYes;
+        }
+#else
         if (isatty(fd))
         {
             m_is_interactive = eLazyBoolYes;
