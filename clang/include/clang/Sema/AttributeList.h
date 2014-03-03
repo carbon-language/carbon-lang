f08a5c7608bb7f02931ebc183f0b851d11c8dc20
Aaron Ballman <aaron@aaronballman.com>
2014-03-03 18:41:48 +0000

    I guess we're still using LLVM_DELETED_FUNCTION instead of = delete. This should fix a complaining built bot.
    llvm-svn: 202734

--- clang/include/clang/Sema/AttributeList.h
+++ clang/include/clang/Sema/AttributeList.h
@@ -705,7 +705,7 @@ public:
     : pool(factory), list(0) {
   }
 
-  ParsedAttributes(const ParsedAttributes &) = delete;
+  ParsedAttributes(const ParsedAttributes &) LLVM_DELETED_FUNCTION;
 
   AttributePool &getPool() const { return pool; }
 
