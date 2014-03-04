7288503bd598131a961e29faf740dec43d25c59a
Chandler Carruth <chandlerc@gmail.com>
2014-03-04 12:11:55 +0000

    [Modules] Stop including CFG.h from GenericDomTree.h. This used to be necessary, but is no longer.
    llvm-svn: 202836

--- llvm/include/llvm/Support/GenericDomTree.h
+++ llvm/include/llvm/Support/GenericDomTree.h
@@ -23,7 +23,6 @@
 #include "llvm/ADT/GraphTraits.h"
 #include "llvm/ADT/SmallPtrSet.h"
 #include "llvm/ADT/SmallVector.h"
-#include "llvm/IR/CFG.h"
 #include "llvm/Support/Compiler.h"
 #include "llvm/Support/raw_ostream.h"
 #include <algorithm>
