c8c38dbb187746d1cb40fd274bca467c7ba93cb3
Alexey Samsonov <samsonov@google.com>
2014-03-05 08:29:56 +0000

    Increase threshold in StackTrace::LocatePcInTrace
    llvm-svn: 202950

--- compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.cc
@@ -72,7 +72,7 @@ void StackTrace::PopStackFrames(uptr count) {
 uptr StackTrace::LocatePcInTrace(uptr pc) {
   // Use threshold to find PC in stack trace, as PC we want to unwind from may
   // slightly differ from return address in the actual unwinded stack trace.
-  const int kPcThreshold = 256;
+  const int kPcThreshold = 288;
   for (uptr i = 0; i < size; ++i) {
     if (MatchPc(pc, trace[i], kPcThreshold))
       return i;
