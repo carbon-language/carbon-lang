5145f4c1c81c6477b64087d40132810068e0f96c
NAKAMURA Takumi <geek4civic@gmail.com>
2014-03-06 00:12:04 +0000

    clang/test/Modules/recursive.c: Tweak expressions for win32.
    llvm-svn: 203034

--- clang/test/Modules/recursive.c
+++ clang/test/Modules/recursive.c
@@ -5,7 +5,7 @@
 // RUN: rm -rf %t
 // RUN: not %clang_cc1 -fmodules -x objective-c -fmodules-cache-path=%t -emit-module -fmodule-name=recursive1 %S/Inputs/module.map 2>&1 | FileCheck %s
 
-// CHECK:      While building module 'recursive1'{{( imported from .*/recursive.c:3)?}}:
-// CHECK-NEXT: While building module 'recursive2' imported from {{.*}}Inputs/recursive1.h:1:
+// CHECK:      While building module 'recursive1'{{( imported from .*[/\]recursive.c:3)?}}:
+// CHECK-NEXT: While building module 'recursive2' imported from {{.*Inputs[/\]}}recursive1.h:1:
 // CHECK-NEXT: In file included from <module-includes>:1:
 // CHECK-NEXT: recursive2.h:1:10: fatal error: cyclic dependency in module 'recursive1': recursive1 -> recursive2 -> recursive1
