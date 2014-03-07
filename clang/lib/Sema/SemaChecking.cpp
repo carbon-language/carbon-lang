91e45e056b3335ebdfbf15863cc53b7eef2de13d
Ted Kremenek <kremenek@apple.com>
2014-03-07 20:51:10 +0000

    Zap another dead 'break' statement.
    llvm-svn: 203282

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -669,7 +669,6 @@ bool Sema::CheckX86BuiltinFunctionCall(unsigned BuiltinID, CallExpr *TheCall) {
   switch (BuiltinID) {
   case X86::BI_mm_prefetch:
     return SemaBuiltinMMPrefetch(TheCall);
-    break;
   }
   return false;
 }
