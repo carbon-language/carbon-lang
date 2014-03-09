67101a52defbfb6542eabd35c1a99d8b522866a3
NAKAMURA Takumi <geek4civic@gmail.com>
2014-03-09 04:10:17 +0000

    clang-tools-extra/test/pp-trace/pp-trace-modules.cpp: Flush module cache to let robust around r203317.
    llvm-svn: 203369

--- clang-tools-extra/test/pp-trace/pp-trace-modules.cpp
+++ clang-tools-extra/test/pp-trace/pp-trace-modules.cpp
@@ -1,3 +1,4 @@
+// RUN: rm -rf %t
 // RUN: pp-trace -ignore FileChanged,MacroDefined %s -x objective-c++ -undef -target x86_64 -std=c++11 -fmodules -fcxx-modules -fmodules-cache-path=%t -I%S -I%S/Input | FileCheck --strict-whitespace %s
 
 // CHECK: ---
