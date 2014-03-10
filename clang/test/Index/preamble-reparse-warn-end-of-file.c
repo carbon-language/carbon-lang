5e9c8214209fdcc1fc42d55d14d1de4135e26ac5
NAKAMURA Takumi <geek4civic@gmail.com>
2014-03-10 06:02:03 +0000

    clang/test/Index/preamble-reparse-warn-*.c: Use @LINE in FileCheck.
    llvm-svn: 203445

--- clang/test/Index/preamble-reparse-warn-end-of-file.c
+++ clang/test/Index/preamble-reparse-warn-end-of-file.c
@@ -3,9 +3,9 @@
 // RUN: env CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 2 local %s -I %t 2> %t.err.txt > %t.out.txt
 // RUN: cat %t.err.txt >> %t.out.txt
 // RUN: FileCheck -input-file=%t.out.txt %s
-// CHECK: preamble-reparse-warn-end-of-file.c:[[FnLine:.*]]:6: FunctionDecl=test:[[FnLine]]:6
-// CHECK: preamble-reparse-warn-end-of-file.c:[[FnLine]]:14: error: expected '}'
-// CHECK: preamble-reparse-warn-end-of-file.c:[[FnLine]]:14: error: expected '}'
+// CHECK: preamble-reparse-warn-end-of-file.c:[[@LINE+6]]:6: FunctionDecl=test:[[@LINE+6]]:6
+// CHECK: preamble-reparse-warn-end-of-file.c:[[@LINE+5]]:14: error: expected '}'
+// CHECK: preamble-reparse-warn-end-of-file.c:[[@LINE+4]]:14: error: expected '}'
 
 #include "header.h"
 
--- clang/test/Index/preamble-reparse-warn-macro.c
+++ clang/test/Index/preamble-reparse-warn-macro.c
@@ -3,9 +3,9 @@
 // RUN: env CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 2 local %s -I %t 2> %t.err.txt > %t.out.txt
 // RUN: cat %t.err.txt >> %t.out.txt
 // RUN: FileCheck -input-file=%t.out.txt %s
-// CHECK: preamble-reparse-warn-macro.c:14:9: warning: 'MAC' macro redefined
+// CHECK: preamble-reparse-warn-macro.c:[[@LINE+8]]:9: warning: 'MAC' macro redefined
 // CHECK-NEXT: Number FIX-ITs = 0
-// CHECK-NEXT: preamble-reparse-warn-macro.c:10:9: note: previous definition is here
+// CHECK-NEXT: preamble-reparse-warn-macro.c:[[@LINE+2]]:9: note: previous definition is here
 
 #define MAC 1
 #include "header.h"
