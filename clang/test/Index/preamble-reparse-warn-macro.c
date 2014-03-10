2b85e546f263c71b48a7a8e9a5ca41db25bd7c20
NAKAMURA Takumi <geek4civic@gmail.com>
2014-03-10 06:02:13 +0000

    clang/test/Index/preamble-reparse-warn-*.c: Stabilize on win32.
    llvm-svn: 203446

--- clang/test/Index/preamble-reparse-warn-macro.c
+++ clang/test/Index/preamble-reparse-warn-macro.c
@@ -1,8 +1,6 @@
 // RUN: mkdir -p %t
 // RUN: touch %t/header.h
-// RUN: env CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 2 local %s -I %t 2> %t.err.txt > %t.out.txt
-// RUN: cat %t.err.txt >> %t.out.txt
-// RUN: FileCheck -input-file=%t.out.txt %s
+// RUN: env CINDEXTEST_EDITING=1 c-index-test -test-load-source-reparse 2 local %s -I %t 2>&1 > %t.out.txt | FileCheck %s
 // CHECK: preamble-reparse-warn-macro.c:[[@LINE+8]]:9: warning: 'MAC' macro redefined
 // CHECK-NEXT: Number FIX-ITs = 0
 // CHECK-NEXT: preamble-reparse-warn-macro.c:[[@LINE+2]]:9: note: previous definition is here
