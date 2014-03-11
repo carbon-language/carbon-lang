faa297e89e82e36aee9702623fa6e99e1cc55b6b
Matt Arsenault <Matthew.Arsenault@amd.com>
2014-03-11 00:01:37 +0000

    Remove incomplete comment
    llvm-svn: 203518

--- llvm/lib/Target/R600/SIInstrInfo.h
+++ llvm/lib/Target/R600/SIInstrInfo.h
@@ -31,8 +31,6 @@ private:
                                              unsigned MovRelOp,
                                              unsigned Dst,
                                              unsigned Src0) const;
-  // If you add or remove instructions from this function, you will
-
 public:
   explicit SIInstrInfo(AMDGPUTargetMachine &tm);
 
