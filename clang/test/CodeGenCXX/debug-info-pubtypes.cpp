f9b84720446cece2938c71ecb4dc95b3e91c6c71
David Blaikie <dblaikie@gmail.com>
2014-03-11 23:38:05 +0000

    DebugInfo: Fix test for LLVM change r203619
    I'll fix this to not test assembly in a moment.
    
    llvm-svn: 203623

--- clang/test/CodeGenCXX/debug-info-pubtypes.cpp
+++ clang/test/CodeGenCXX/debug-info-pubtypes.cpp
@@ -2,10 +2,10 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10  -g -fstandalone-debug -S -mllvm -generate-dwarf-pub-sections=Enable %s -o - | FileCheck %s
 
 // FIXME: This testcase shouldn't rely on assembly emission.
-//CHECK: Lpubtypes_begin[[SECNUM:[0-9]:]]
+//CHECK: LpubTypes_begin[[SECNUM:[0-9]:]]
 //CHECK:         .asciz   "G"
 //CHECK-NEXT:    .long   0
-//CHECK-NEXT: Lpubtypes_end[[SECNUM]]
+//CHECK-NEXT: LpubTypes_end[[SECNUM]]
 
 class G {
 public:
