7868c329788b2461e7bb47e6600f2dee3c47be6c
Rafael Espindola <rafael.espindola@gmail.com>
2014-03-11 21:10:44 +0000

    Remove dead code.
    llvm-svn: 203604

--- llvm/include/llvm/MC/MCMachObjectWriter.h
+++ llvm/include/llvm/MC/MCMachObjectWriter.h
@@ -153,10 +153,6 @@ public:
   /// @{
 
   bool is64Bit() const { return TargetObjectWriter->is64Bit(); }
-  bool isARM() const {
-    uint32_t CPUType = TargetObjectWriter->getCPUType() & ~MachO::CPU_ARCH_MASK;
-    return CPUType == MachO::CPU_TYPE_ARM;
-  }
 
   /// @}
 
