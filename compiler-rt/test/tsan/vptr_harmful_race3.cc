47fd6179b103f2de0968a450e9666bf1ab5ec5c6
Dmitry Vyukov <dvyukov@google.com>
2014-03-12 08:24:49 +0000

    tsan: add disabled test for http://llvm.org/bugs/show_bug.cgi?id=19113
    llvm-svn: 203646

---
+++ compiler-rt/test/tsan/vptr_harmful_race3.cc
@@ -0,0 +1,57 @@
+// RUN: %clangxx_tsan -O1 %s -o %t && not %t 2>&1 | FileCheck %s
+#include <pthread.h>
+#include <semaphore.h>
+#include <stdio.h>
+#include <unistd.h>
+
+struct A {
+  A() {
+    sem_init(&sem_, 0, 0);
+  }
+  virtual void F() {
+  }
+  void Done() {
+    sem_post(&sem_);
+  }
+  virtual ~A() {
+    sem_wait(&sem_);
+    sem_destroy(&sem_);
+  }
+  sem_t sem_;
+};
+
+struct B : A {
+  virtual void F() {
+  }
+  virtual ~B() { }
+};
+
+static A *obj = new B;
+static void (A::*fn)() = &A::F;
+
+void *Thread1(void *x) {
+  sleep(1);
+  (obj->*fn)();
+  obj->Done();
+  return NULL;
+}
+
+void *Thread2(void *x) {
+  delete obj;
+  return NULL;
+}
+
+int main() {
+  pthread_t t[2];
+  pthread_create(&t[0], NULL, Thread1, NULL);
+  pthread_create(&t[1], NULL, Thread2, NULL);
+  pthread_join(t[0], NULL);
+  pthread_join(t[1], NULL);
+}
+
+// CHECK: WARNING: ThreadSanitizer: data race
+
+// but should be:
+// WARNING: ThreadSanitizer: data race on vptr
+// see http://llvm.org/bugs/show_bug.cgi?id=19113
+
