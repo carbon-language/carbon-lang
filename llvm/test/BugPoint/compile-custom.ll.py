477a2f39cbaa7774743dd2847f54f730fa875fb5
NAKAMURA Takumi <geek4civic@gmail.com>
2014-03-13 00:10:37 +0000

    llvm/test/BugPoint/compile-custom.ll.py: Make it py3-compatible. [PR19112]
    FIXME: Get rid of invoking this.
    I guess it wouldn't run on win32 due to lacking of shell support.
    
    llvm-svn: 203740

--- llvm/test/BugPoint/compile-custom.ll.py
+++ llvm/test/BugPoint/compile-custom.ll.py
@@ -5,6 +5,6 @@ import sys
 # Currently any print-out from the custom tool is interpreted as a crash
 # (i.e. test is still interesting)
 
-print "Error: " + ' '.join(sys.argv[1:])
+print("Error: " + ' '.join(sys.argv[1:]))
 
 sys.exit(1)
