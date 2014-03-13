07e7486128a40887f5b55d0f4c9b29fad3685954
Mark Seaborn <mseaborn@chromium.org>
2014-03-13 00:04:17 +0000

    Fix typo in comment: "inwoke" -> "invoke"
    llvm-svn: 203739

--- llvm/lib/Transforms/Scalar/GlobalMerge.cpp
+++ llvm/lib/Transforms/Scalar/GlobalMerge.cpp
@@ -212,7 +212,7 @@ void GlobalMerge::setMustKeepGlobalVariables(Module &M) {
        ++IFn) {
     for (Function::iterator IBB = IFn->begin(), IEndBB = IFn->end();
          IBB != IEndBB; ++IBB) {
-      // Follow the inwoke link to find the landing pad instruction
+      // Follow the invoke link to find the landing pad instruction
       const InvokeInst *II = dyn_cast<InvokeInst>(IBB->getTerminator());
       if (!II) continue;
 
