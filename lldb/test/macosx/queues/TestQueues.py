b00cc1f92f2f01a396348b42ff1840543ffc0a7e
Jason Molenda <jmolenda@apple.com>
2014-03-13 23:04:22 +0000

    Refactor the Queues test case a little based on feedback from Jim. Add queues tests for when libBacktraceRecording is absent.
    llvm-svn: 203864

--- lldb/test/macosx/queues/TestQueues.py
+++ lldb/test/macosx/queues/TestQueues.py
@@ -17,6 +17,7 @@ class TestQueues(TestBase):
         """Test queues inspection SB APIs."""
         self.buildDsym()
         self.queues()
+        self.queues_with_libBacktraceRecording()
 
     @unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
     @python_api_test
@@ -25,6 +26,7 @@ class TestQueues(TestBase):
         """Test queues inspection SB APIs."""
         self.buildDwarf()
         self.queues()
+        self.queues_with_libBacktraceRecording()
 
     def setUp(self):
         # Call super's setUp().
@@ -32,17 +34,97 @@ class TestQueues(TestBase):
         # Find the line numbers that we will step to in main:
         self.main_source = "main.c"
 
+    def check_queue_for_valid_queue_id(self, queue):
+        self.assertTrue(queue.GetQueueID() != 0, "Check queue %s for valid QueueID (got 0x%x)" % (queue.GetName(), queue.GetQueueID()))
+
+    def check_running_and_pending_items_on_queue(self, queue, expected_running, expected_pending):
+        self.assertTrue(queue.GetNumPendingItems() == expected_pending, "queue %s should have %d pending items, instead has %d pending items" % (queue.GetName(), expected_pending, (queue.GetNumPendingItems())))
+        self.assertTrue(queue.GetNumRunningItems() == expected_running, "queue %s should have %d running items, instead has %d running items" % (queue.GetName(), expected_running, (queue.GetNumRunningItems())))
+
+    def check_number_of_threads_owned_by_queue(self, queue, number_threads):
+        self.assertTrue(queue.GetNumThreads() == number_threads, "queue %s should have %d thread executing, but has %d" % (queue.GetName(), number_threads, queue.GetNumThreads()))
+
+    def check_queue_kind (self, queue, kind):
+        expected_kind_string = "Unknown"
+        if kind == lldb.eQueueKindSerial:
+            expected_kind_string = "Serial queue"
+        if kind == lldb.eQueueKindConcurrent:
+            expected_kind_string = "Concurrent queue"
+        actual_kind_string = "Unknown"
+        if queue.GetKind() == lldb.eQueueKindSerial:
+            actual_kind_string = "Serial queue"
+        if queue.GetKind() == lldb.eQueueKindConcurrent:
+            actual_kind_string = "Concurrent queue"
+        self.assertTrue(queue.GetKind() == kind, "queue %s is expected to be a %s but it is actually a %s" % (queue.GetName(), expected_kind_string, actual_kind_string))
+
+    def check_queues_threads_match_queue(self, queue):
+        for idx in range(0, queue.GetNumThreads()):
+            t = queue.GetThreadAtIndex(idx)
+            self.assertTrue(t.IsValid(), "Queue %s's thread #%d must be valid" % (queue.GetName(), idx))
+            self.assertTrue(t.GetQueueID() == queue.GetQueueID(), "Queue %s has a QueueID of %d but its thread #%d has a QueueID of %d" % (queue.GetName(), queue.GetQueueID(), idx, t.GetQueueID()))
+            self.assertTrue(t.GetQueueName() == queue.GetName(), "Queue %s has a QueueName of %s but its thread #%d has a QueueName of %s" % (queue.GetName(), queue.GetName(), idx, t.GetQueueName()))
+
     def queues(self):
-        """Test queues inspection SB APIs."""
+        """Test queues inspection SB APIs without libBacktraceRecording."""
+        exe = os.path.join(os.getcwd(), "a.out")
+
+        target = self.dbg.CreateTarget(exe)
+        self.assertTrue(target, VALID_TARGET)
+        self.main_source_spec = lldb.SBFileSpec (self.main_source)
+        break1 = target.BreakpointCreateByName ("stopper", 'a.out')
+        self.assertTrue(break1, VALID_BREAKPOINT)
+        process = target.LaunchSimple (None, None, self.get_process_working_directory())
+        self.assertTrue(process, PROCESS_IS_VALID)
+        threads = lldbutil.get_threads_stopped_at_breakpoint (process, break1)
+        if len(threads) != 1:
+            self.fail ("Failed to stop at breakpoint 1.")
+
+        queue_submittor_1 = lldb.SBQueue()
+        queue_performer_1 = lldb.SBQueue()
+        queue_performer_2 = lldb.SBQueue()
+        queue_performer_3 = lldb.SBQueue()
+        for idx in range (0, process.GetNumQueues()):
+          q = process.GetQueueAtIndex(idx)
+          if q.GetName() == "com.apple.work_submittor_1":
+            queue_submittor_1 = q
+          if q.GetName() == "com.apple.work_performer_1":
+            queue_performer_1 = q
+          if q.GetName() == "com.apple.work_performer_2":
+            queue_performer_2 = q
+          if q.GetName() == "com.apple.work_performer_3":
+            queue_performer_3 = q
+
+        self.assertTrue(queue_submittor_1.IsValid() and queue_performer_1.IsValid() and queue_performer_2.IsValid() and queue_performer_3.IsValid(), "Got all four expected queues: %s %s %s %s" % (queue_submittor_1.IsValid(), queue_performer_1.IsValid(), queue_performer_2.IsValid(), queue_performer_3.IsValid()))
+
+        self.check_queue_for_valid_queue_id (queue_submittor_1)
+        self.check_queue_for_valid_queue_id (queue_performer_1)
+        self.check_queue_for_valid_queue_id (queue_performer_2)
+        self.check_queue_for_valid_queue_id (queue_performer_3)
+
+        self.check_number_of_threads_owned_by_queue (queue_submittor_1, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_1, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_2, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_3, 4)
+
+        self.check_queue_kind (queue_submittor_1, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_1, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_2, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_3, lldb.eQueueKindConcurrent)
+        
+        self.check_queues_threads_match_queue (queue_submittor_1)
+        self.check_queues_threads_match_queue (queue_performer_1)
+        self.check_queues_threads_match_queue (queue_performer_2)
+        self.check_queues_threads_match_queue (queue_performer_3)
+
+    def queues_with_libBacktraceRecording(self):
+        """Test queues inspection SB APIs with libBacktraceRecording present."""
         exe = os.path.join(os.getcwd(), "a.out")
 
         if not os.path.isfile('/Applications/Xcode.app/Contents/Developer/usr/lib/libBacktraceRecording.dylib'):
           self.skipTest ("Skipped because libBacktraceRecording.dylib was present on the system.")
-          self.buildDefault()
           
         if not os.path.isfile('/usr/lib/system/introspection/libdispatch.dylib'):
           self.skipTest ("Skipped because introspection libdispatch dylib is not present.")
-          self.buildDefault()
           
         target = self.dbg.CreateTarget(exe)
         self.assertTrue(target, VALID_TARGET)
@@ -62,68 +144,62 @@ class TestQueues(TestBase):
         if len(threads) != 1:
             self.fail ("Failed to stop at breakpoint 1.")
 
-        libbtr_module_filespec = lldb.SBFileSpec()
-        libbtr_module_filespec.SetFilename ("libBacktraceRecording.dylib")
+        libbtr_module_filespec = lldb.SBFileSpec("libBacktraceRecording.dylib")
         libbtr_module = target.FindModule (libbtr_module_filespec)
         if not libbtr_module.IsValid():
           self.skipTest ("Skipped because libBacktraceRecording.dylib was not loaded into the process.")
-          self.buildDefault()
 
-        self.assertTrue(process.GetNumQueues() == 4, "Found the correct number of queues.")
+        self.assertTrue(process.GetNumQueues() >= 4, "Found the correct number of queues.")
 
-        q0 = process.GetQueueAtIndex(0)
-        q1 = process.GetQueueAtIndex(1)
-        q2 = process.GetQueueAtIndex(2)
-        q3 = process.GetQueueAtIndex(3)
+        queue_submittor_1 = lldb.SBQueue()
+        queue_performer_1 = lldb.SBQueue()
+        queue_performer_2 = lldb.SBQueue()
+        queue_performer_3 = lldb.SBQueue()
+        for idx in range (0, process.GetNumQueues()):
+          q = process.GetQueueAtIndex(idx)
+          if q.GetName() == "com.apple.work_submittor_1":
+            queue_submittor_1 = q
+          if q.GetName() == "com.apple.work_performer_1":
+            queue_performer_1 = q
+          if q.GetName() == "com.apple.work_performer_2":
+            queue_performer_2 = q
+          if q.GetName() == "com.apple.work_performer_3":
+            queue_performer_3 = q
 
-        self.assertTrue(q0.IsValid(), "queue 0 is valid")
-        self.assertTrue(q1.IsValid(), "queue 1 is valid")
-        self.assertTrue(q2.IsValid(), "queue 2 is valid")
-        self.assertTrue(q3.IsValid(), "queue 3 is valid")
+        self.assertTrue(queue_submittor_1.IsValid() and queue_performer_1.IsValid() and queue_performer_2.IsValid() and queue_performer_3.IsValid(), "Got all four expected queues: %s %s %s %s" % (queue_submittor_1.IsValid(), queue_performer_1.IsValid(), queue_performer_2.IsValid(), queue_performer_3.IsValid()))
 
-        self.assertTrue(q0.GetName() == "com.apple.work_submittor_1", "Get name of first queue")
-        self.assertTrue(q1.GetName() == "com.apple.work_performer_1", "Get name of second queue")
-        self.assertTrue(q2.GetName() == "com.apple.work_performer_2", "Get name of third queue")
-        self.assertTrue(q3.GetName() == "com.apple.work_performer_3", "Get name of fourth queue")
+        self.check_queue_for_valid_queue_id (queue_submittor_1)
+        self.check_queue_for_valid_queue_id (queue_performer_1)
+        self.check_queue_for_valid_queue_id (queue_performer_2)
+        self.check_queue_for_valid_queue_id (queue_performer_3)
 
-        self.assertTrue(q0.GetQueueID() != 0, "Check queue 0 for valid QueueID")
-        self.assertTrue(q1.GetQueueID() != 0, "Check queue 1 for valid QueueID")
-        self.assertTrue(q2.GetQueueID() != 0, "Check queue 2 for valid QueueID")
-        self.assertTrue(q3.GetQueueID() != 0, "Check queue 3 for valid QueueID")
+        self.check_running_and_pending_items_on_queue (queue_submittor_1, 1, 0)
+        self.check_running_and_pending_items_on_queue (queue_performer_1, 1, 3)
+        self.check_running_and_pending_items_on_queue (queue_performer_2, 1, 9999)
+        self.check_running_and_pending_items_on_queue (queue_performer_3, 4, 0)
+       
+        self.check_number_of_threads_owned_by_queue (queue_submittor_1, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_1, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_2, 1)
+        self.check_number_of_threads_owned_by_queue (queue_performer_3, 4)
 
-        self.assertTrue(q0.GetNumPendingItems() == 0, "queue 0 should have no pending items")
-        self.assertTrue(q0.GetNumRunningItems() == 1, "queue 0 should have one running item")
-
-        self.assertTrue(q1.GetNumPendingItems() == 3, "queue 1 should have 3 pending items")
-        self.assertTrue(q1.GetNumRunningItems() == 1, "queue 1 should have 1 running item")
-    
-        self.assertTrue(q2.GetNumPendingItems() == 9999, "queue 2 should have 9999 pending items")
-        self.assertTrue(q2.GetNumRunningItems() == 1, "queue 2 should have 1 running item")
-
-        self.assertTrue(q3.GetNumPendingItems() == 0, "queue 3 should have 0 pending items")
-        self.assertTrue(q3.GetNumRunningItems() == 4, "queue 3 should have 4 running item")
-
-        self.assertTrue(q0.GetNumThreads() == 1, "queue 0 should have 1 thread executing")
-        self.assertTrue(q3.GetNumThreads() == 4, "queue 3 should have 4 threads executing")
-
-        self.assertTrue(q0.GetKind() == lldb.eQueueKindSerial, "queue 0 is a serial queue")
-        self.assertTrue(q1.GetKind() == lldb.eQueueKindSerial, "queue 1 is a serial queue")
-        self.assertTrue(q2.GetKind() == lldb.eQueueKindSerial, "queue 2 is a serial queue")
-        self.assertTrue(q3.GetKind() == lldb.eQueueKindConcurrent, "queue 3 is a concurrent queue")
+        self.check_queue_kind (queue_submittor_1, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_1, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_2, lldb.eQueueKindSerial)
+        self.check_queue_kind (queue_performer_3, lldb.eQueueKindConcurrent)
         
 
-        self.assertTrue(q1.GetThreadAtIndex(0).GetQueueID() == q1.GetQueueID(), "queue 1's thread should be owned by the same QueueID")
-        self.assertTrue(q1.GetThreadAtIndex(0).GetQueueName() == q1.GetName(), "queue 1's thread should have the same queue name")
+        self.check_queues_threads_match_queue (queue_submittor_1)
+        self.check_queues_threads_match_queue (queue_performer_1)
+        self.check_queues_threads_match_queue (queue_performer_2)
+        self.check_queues_threads_match_queue (queue_performer_3)
 
-        self.assertTrue(q3.GetThreadAtIndex(0).GetQueueID() == q3.GetQueueID(), "queue 3's threads should be owned by the same QueueID")
-        self.assertTrue(q3.GetThreadAtIndex(0).GetQueueName() == q3.GetName(), "queue 3's threads should have thes ame queue name")
-
-        self.assertTrue(q2.GetPendingItemAtIndex(0).IsValid(), "queue 2's pending item #0 is valid")
-        self.assertTrue(q2.GetPendingItemAtIndex(0).GetAddress().GetSymbol().GetName() == "doing_the_work_2", "queue 2's pending item #0 should be doing_the_work_2")
-        self.assertTrue(q2.GetNumPendingItems() == 9999, "verify that queue 2 still has 9999 pending items")
-        self.assertTrue(q2.GetPendingItemAtIndex(9998).IsValid(), "queue 2's pending item #9998 is valid")
-        self.assertTrue(q2.GetPendingItemAtIndex(9998).GetAddress().GetSymbol().GetName() == "doing_the_work_2", "queue 2's pending item #0 should be doing_the_work_2")
-        self.assertTrue(q2.GetPendingItemAtIndex(9999).IsValid() == False, "queue 2's pending item #9999 is invalid")
+        self.assertTrue(queue_performer_2.GetPendingItemAtIndex(0).IsValid(), "queue 2's pending item #0 is valid")
+        self.assertTrue(queue_performer_2.GetPendingItemAtIndex(0).GetAddress().GetSymbol().GetName() == "doing_the_work_2", "queue 2's pending item #0 should be doing_the_work_2")
+        self.assertTrue(queue_performer_2.GetNumPendingItems() == 9999, "verify that queue 2 still has 9999 pending items")
+        self.assertTrue(queue_performer_2.GetPendingItemAtIndex(9998).IsValid(), "queue 2's pending item #9998 is valid")
+        self.assertTrue(queue_performer_2.GetPendingItemAtIndex(9998).GetAddress().GetSymbol().GetName() == "doing_the_work_2", "queue 2's pending item #0 should be doing_the_work_2")
+        self.assertTrue(queue_performer_2.GetPendingItemAtIndex(9999).IsValid() == False, "queue 2's pending item #9999 is invalid")
 
         
 if __name__ == '__main__':
