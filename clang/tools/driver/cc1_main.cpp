17fac04433640dd9d3e7b63e4664ec531b04e50e
Sebastian Pop <spop@codeaurora.org>
2014-03-14 04:04:27 +0000

    static link polly
    llvm-svn: 203887

--- clang/tools/driver/cc1_main.cpp
+++ clang/tools/driver/cc1_main.cpp
@@ -57,6 +57,12 @@ static void LLVMErrorHandler(void *UserData, const std::string &Message,
   exit(GenCrashDiag ? 70 : 1);
 }
 
+#ifdef LINK_POLLY_INTO_TOOLS
+namespace polly {
+void initializePollyPasses(llvm::PassRegistry &Registry);
+}
+#endif
+
 int cc1_main(const char **ArgBegin, const char **ArgEnd,
              const char *Argv0, void *MainAddr) {
   std::unique_ptr<CompilerInstance> Clang(new CompilerInstance());
@@ -67,6 +73,11 @@ int cc1_main(const char **ArgBegin, const char **ArgEnd,
   llvm::InitializeAllTargetMCs();
   llvm::InitializeAllAsmPrinters();
   llvm::InitializeAllAsmParsers();
+
+#ifdef LINK_POLLY_INTO_TOOLS
+  llvm::PassRegistry &Registry = *llvm::PassRegistry::getPassRegistry();
+  polly::initializePollyPasses(Registry);
+#endif
 
   // Buffer diagnostics from argument parsing so that we can output them using a
   // well formed diagnostic object.
