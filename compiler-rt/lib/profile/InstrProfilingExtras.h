c962cda1db568582dada8380e4ab5efd06c1d8a8
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2014-03-20 19:39:01 +0000

    PGO: Add missing file...
    llvm-svn: 204384

---
+++ compiler-rt/lib/profile/InstrProfilingExtras.h
@@ -0,0 +1,28 @@
+/*===- InstrProfilingExtras.h - Support library for PGO instrumentation ---===*\
+|*
+|*                     The LLVM Compiler Infrastructure
+|*
+|* This file is distributed under the University of Illinois Open Source
+|* License. See LICENSE.TXT for details.
+|*
+\*===----------------------------------------------------------------------===*/
+
+/*!
+ * \brief Write instrumentation data to the current file.
+ *
+ * Writes to the file with the last name given to \a __llvm_pgo_set_filename(),
+ * or if it hasn't been called, the \c LLVM_PROFILE_FILE environment variable,
+ * or if that's not set, \c "default.profdata".
+ */
+void __llvm_pgo_write_file();
+
+/*!
+ * \brief Set the filename for writing instrumentation data.
+ *
+ * Sets the filename to be used for subsequent calls to
+ * \a __llvm_pgo_write_file().
+ */
+void __llvm_pgo_set_filename(const char *Name);
+
+/*! \brief Register to write instrumentation data to file at exit. */
+void __llvm_pgo_register_write_file_atexit();
