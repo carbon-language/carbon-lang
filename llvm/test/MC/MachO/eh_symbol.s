df1be1f4c5fd16cf97a2e73de2e1de676292989e
Rafael Espindola <rafael.espindola@gmail.com>
2014-03-20 22:05:59 +0000

    Convert CodeGen test into a more specific MC test.
    llvm-svn: 204406

---
+++ llvm/test/MC/MachO/eh_symbol.s
@@ -0,0 +1,14 @@
+// RUN: llvm-mc -triple i386-apple-darwin9 %s -filetype=obj -o - | llvm-nm | FileCheck %s
+
+// test that we don't produce foo.eh symbols in a debug_frame section.
+// CHECK-NOT: _f.eh
+// CHECK: T _f
+// CHECK-NOT: _f.eh
+
+	.globl	_f
+_f:
+	.cfi_startproc
+	retl
+	.cfi_endproc
+
+	.cfi_sections .debug_frame
