f1b10242c071a049fb4fdf9cf332946ed8dee367
Rafael Espindola <rafael.espindola@gmail.com>
2014-03-21 00:55:42 +0000

    Convert a CodeGen test into a MC test.
    llvm-svn: 204421

---
+++ llvm/test/MC/X86/no-elf-compact-unwind.s
@@ -0,0 +1,15 @@
+// RUN: llvm-mc < %s -filetype=obj -triple x86_64-apple-macosx10.8.0 | llvm-readobj -s | FileCheck -check-prefix=MACHO %s
+// RUN: llvm-mc < %s -filetype=obj -triple x86_64-unknown-linux | llvm-readobj -s | FileCheck -check-prefix=ELF %s
+
+	.globl	__Z3barv
+	.align	4, 0x90
+__Z3barv:
+	.cfi_startproc
+	pushq	%rax
+	.cfi_def_cfa_offset 16
+	popq	%rax
+	retq
+	.cfi_endproc
+
+// MACHO: Name: __compact_unwind
+// ELF-NOT: __compact_unwind
