8d44206f530ec1bbe3d05f647086e71d8dddf94d
Hafiz Abid Qadeer <hafiz_abid@mentor.com>
2014-03-21 10:15:34 +0000

    More cleanup of win32.h. PATH_MAX is defined if it is not already defined. A duplicate definition is removed. The declaration of struct timespec is moved outside #ifdef _MSC_VER to make it available for mingw.
    llvm-svn: 204449

--- lldb/include/lldb/Host/windows/win32.h
+++ lldb/include/lldb/Host/windows/win32.h
@@ -18,7 +18,9 @@ int vasprintf(char **ret, const char *fmt, va_list ap);
 char * strcasestr(const char *s, const char* find);
 char* realpath(const char * name, char * resolved);
 
+#ifndef PATH_MAX
 #define PATH_MAX MAX_PATH
+#endif
 
 #define O_NOCTTY    0
 #define O_NONBLOCK  0
@@ -57,8 +59,6 @@ char *dirname(char *path);
 int strcasecmp(const char* s1, const char* s2);
 int strncasecmp(const char* s1, const char* s2, size_t n);
 
-
-#define PATH_MAX MAX_PATH
 #define STDIN_FILENO  0
 #define STDOUT_FILENO 1
 #define STDERR_FILENO 2
@@ -69,6 +69,7 @@ int strncasecmp(const char* s1, const char* s2, size_t n);
 #define S_ISDIR(mode)  (((mode) & S_IFMT) == S_IFDIR)
 
 #define snprintf _snprintf
+#endif
 
 // timespec
 struct timespec
@@ -77,6 +78,5 @@ struct timespec
     long   tv_nsec;
 };
 
-#endif
 
 #endif  // LLDB_lldb_win32_h_
