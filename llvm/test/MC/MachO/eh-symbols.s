734f105379be747087f3ab96db0e8ccd23dd103d
Rafael Espindola <rafael.espindola@gmail.com>
2014-03-21 17:55:34 +0000

    Move codegen test over to MC.
    llvm-svn: 204490

--- llvm/test/MC/MachO/eh-symbols.s
+++ llvm/test/MC/MachO/eh-symbols.s
@@ -10,8 +10,16 @@ _main:
 	retl
 	.cfi_endproc
 
+"_-[NSString(local) isNullOrNil]":
+	.cfi_startproc
+	retl
+	.cfi_endproc
+
+// CHECK: Name: _-[NSString(local) isNullOrNil].eh
+
 // CHECK:       Name: _main
 // CHECK-NEXT:  PrivateExtern
 
 // CHECK:       Name: _main.eh
 // CHECK-NEXT:  PrivateExtern
+
