b46a017248356c4bf568623ea1ec2e90fa474e18
David Majnemer <david.majnemer@gmail.com>
2014-03-23 18:22:10 +0000

    CodeGen: Make string tests exercise the MS ABI
    r204562 unwittingly failed tests for some bots.  Make those tests work
    with both the Itanium and MS ABIs.
    
    llvm-svn: 204566

--- clang/test/CodeGen/string-literal-short-wstring.c
+++ clang/test/CodeGen/string-literal-short-wstring.c
@@ -1,32 +1,40 @@
-// RUN: %clang_cc1 -x c++ -emit-llvm -fshort-wchar %s -o - | FileCheck %s
+// RUN: %clang_cc1 -x c++ -triple %itanium_abi_triple -emit-llvm -fshort-wchar %s -o - | FileCheck %s
+// RUN: %clang_cc1 -x c++ -triple %ms_abi_triple -emit-llvm -fshort-wchar %s -o - | FileCheck %s --check-prefix=WIN32
 // Runs in c++ mode so that wchar_t is available.
 
 int main() {
   // This should convert to utf8.
   // CHECK: private unnamed_addr constant [10 x i8] c"\E1\84\A0\C8\A0\F4\82\80\B0\00", align 1
+  // WIN32: private unnamed_addr constant [10 x i8] c"\E1\84\A0\C8\A0\F4\82\80\B0\00", align 1
   char b[10] = "\u1120\u0220\U00102030";
 
   // CHECK: private unnamed_addr constant [3 x i16] [i16 65, i16 66, i16 0]
+  // WIN32: linkonce_odr unnamed_addr constant [3 x i16] [i16 65, i16 66, i16 0]
   const wchar_t *foo = L"AB";
 
   // This should convert to utf16.
   // CHECK: private unnamed_addr constant [5 x i16] [i16 4384, i16 544, i16 -9272, i16 -9168, i16 0]
+  // WIN32: linkonce_odr unnamed_addr constant [5 x i16] [i16 4384, i16 544, i16 -9272, i16 -9168, i16 0]
   const wchar_t *bar = L"\u1120\u0220\U00102030";
 
 
 
   // Should pick second character.
   // CHECK: store i8 98
+  // WIN32: store i8 98
   char c = 'ab';
 
   // CHECK: store i16 97
+  // WIN32: store i16 97
   wchar_t wa = L'a';
 
   // Should pick second character.
   // CHECK: store i16 98
+  // WIN32: store i16 98
   wchar_t wb = L'ab';
 
   // -4085 == 0xf00b
   // CHECK: store i16 -4085
+  // WIN32: store i16 -4085
   wchar_t wc = L'\uF00B';
 }
