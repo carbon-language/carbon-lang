3b3edfb16d67d4d8abd6f6ca0a46e7c1a9e89d3f
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2014-03-28 18:22:26 +0000

    InstrProf: Fix MSVC after r205023
    llvm-svn: 205025

--- clang/lib/CodeGen/CodeGenPGO.cpp
+++ clang/lib/CodeGen/CodeGenPGO.cpp
@@ -815,8 +815,8 @@ namespace {
 }
 
 static void emitRuntimeHook(CodeGenModule &CGM) {
-  constexpr const char *RuntimeVarName = "__llvm_profile_runtime";
-  constexpr const char *RuntimeUserName = "__llvm_profile_runtime_user";
+  const char *RuntimeVarName = "__llvm_profile_runtime";
+  const char *RuntimeUserName = "__llvm_profile_runtime_user";
   if (CGM.getModule().getGlobalVariable(RuntimeVarName))
     return;
 
