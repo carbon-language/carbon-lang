92bee36b3efa2e7fd23f661412fb75c8fff0151e
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-03-28 11:21:45 +0000

    [sanitizer] Intercept rand_r.
    llvm-svn: 205001

---
+++ compiler-rt/test/msan/rand_r.cc
@@ -0,0 +1,18 @@
+// RUN: %clangxx_msan -m64 -O0 -g %s -o %t && %t
+// RUN: %clangxx_msan -m64 -O0 -g -DUNINIT %s -o %t && not %t 2>&1 | FileCheck %s
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  unsigned seed;
+#ifndef UNINIT
+  seed = 42;
+#endif
+  int v = rand_r(&seed);
+  // CHECK: MemorySanitizer: use-of-uninitialized-value
+  // CHECK: in main{{.*}}rand_r.cc:[[@LINE-2]]
+  if (v) printf(".\n");
+  return 0;
+}
