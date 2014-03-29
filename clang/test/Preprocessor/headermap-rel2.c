ca1aa4626d5c9fd3e849410faf1410948bc08647
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2014-03-29 03:37:04 +0000

    [test] Add a RUN line to get a hint on why the test is failing at the buildbots.
    llvm-svn: 205072

--- clang/test/Preprocessor/headermap-rel2.c
+++ clang/test/Preprocessor/headermap-rel2.c
@@ -1,6 +1,7 @@
 // This uses a headermap with this entry:
 //   someheader.h -> Product/someheader.h
 
+// RUN: %clang_cc1 -v -fsyntax-only %s -iquote %S/Inputs/headermap-rel2/project-headers.hmap -isysroot %S/Inputs/headermap-rel2/system -I %S/Inputs/headermap-rel2 -H
 // RUN: %clang_cc1 -fsyntax-only %s -iquote %S/Inputs/headermap-rel2/project-headers.hmap -isysroot %S/Inputs/headermap-rel2/system -I %S/Inputs/headermap-rel2 -H 2> %t.out
 // RUN: FileCheck %s -input-file %t.out
 
