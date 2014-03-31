ae2f0bbcf1b8ac81295a59ac9ad6858a054a811a
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2014-03-31 22:45:37 +0000

    InstrProf: Add simple compiler-rt test
    Add the test infrastructure for testing lib/profile and a single test.
    This initial commit only enables the tests on Darwin, but they'll be
    enabled on Linux soon after.
    
    <rdar://problem/16458307>
    
    llvm-svn: 205256

---
+++ compiler-rt/test/profile/instrprof-basic.c
@@ -0,0 +1,12 @@
+// RUN: %clang_profgen -o %t -O3 -flto  %s
+// RUN: env LLVM_PROFILE_FILE=%t.profraw %t
+// RUN: llvm-profdata merge -o %t.profdata %t.profraw
+// RUN: %clang_profuse=%t.profdata -o - -S -emit-llvm %s | FileCheck %s
+
+int main(int argc, const char *argv[]) {
+  // CHECK: br i1 %{{.*}}, label %{{.*}}, label %{{.*}}, !prof !1
+  if (argc)
+    return 0;
+  return 1;
+}
+// CHECK: !1 = metadata !{metadata !"branch_weights", i32 2, i32 1}
