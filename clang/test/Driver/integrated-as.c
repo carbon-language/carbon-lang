9e135ddd1daadd152f738ecaf3ad89e4fc629604
Benjamin Kramer <benny.kra@googlemail.com>
2014-03-31 16:55:01 +0000

    Remove unused diagnostic + dead test for it.
    llvm-svn: 205220

--- clang/test/Driver/integrated-as.c
+++ clang/test/Driver/integrated-as.c
@@ -13,8 +13,3 @@
 // NOFIAS-NOT: cc1as
 // NOFIAS: -cc1
 // NOFIAS: -no-integrated-as
-
-// RUN: %clang -### -c -integrated-as -Wa,-compress-debug-sections -Wno-missing-debug-compression %s 2>&1 | FileCheck --check-prefix=COMPRESS_DEBUG_QUIET %s
-// COMPRESS_DEBUG_QUIET-NOT: warning: DWARF compression is not implemented
-// COMPRESS_DEBUG_QUIET-NOT: warning: argument unused during compilation
-// COMPRESS_DEBUG_QUIET: -cc1
