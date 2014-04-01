a5f806e59964ef9b4992a60bdadb076025c0bec3
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2014-04-01 15:29:27 +0000

    InstrProf: Remove LTO dependency from test
    The -flto flag relies on linker features that are not available on all
    platforms.
    
    <rdar://problem/16458307>
    
    llvm-svn: 205318

--- compiler-rt/test/profile/instrprof-basic.c
+++ compiler-rt/test/profile/instrprof-basic.c
@@ -1,4 +1,4 @@
-// RUN: %clang_profgen -o %t -O3 -flto  %s
+// RUN: %clang_profgen -o %t -O3 %s
 // RUN: env LLVM_PROFILE_FILE=%t.profraw %t
 // RUN: llvm-profdata merge -o %t.profdata %t.profraw
 // RUN: %clang_profuse=%t.profdata -o - -S -emit-llvm %s | FileCheck %s
