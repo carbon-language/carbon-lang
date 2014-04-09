6bd32296160d8e2fcecff8918669d57742beaa0f
Viktor Kutuzov <vkutuzov@accesssoftek.com>
2014-04-09 11:43:34 +0000

    Add support for building LLVM on FreeBSD 9.2
    llvm-svn: 205847

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -17,6 +17,11 @@ if( UNIX AND NOT BEOS )
   # Used by check_symbol_exists:
   set(CMAKE_REQUIRED_LIBRARIES m)
 endif()
+# x86_64 FreeBSD 9.2 requires libcxxrt to be specified explicitly.
+if( CMAKE_SYSTEM MATCHES "FreeBSD-9.2-RELEASE" AND
+    CMAKE_SIZEOF_VOID_P EQUAL 8 )
+  list(APPEND CMAKE_REQUIRED_LIBRARIES "cxxrt")
+endif()
 
 # Helper macros and functions
 macro(add_cxx_include result files)
