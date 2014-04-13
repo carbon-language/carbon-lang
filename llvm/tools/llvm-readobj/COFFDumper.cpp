381037066ed7a6b88c4a90e01eef4e5275782475
Saleem Abdulrasool <compnerd@compnerd.org>
2014-04-13 22:54:15 +0000

    tools: remove duplication of coff_aux_file
    Now that COFF::coff_aux_file is defined, use that rather than redefining the
    type locally.
    
    llvm-svn: 206140

--- llvm/tools/llvm-readobj/COFFDumper.cpp
+++ llvm/tools/llvm-readobj/COFFDumper.cpp
@@ -428,13 +428,6 @@ static const EnumEntry<unsigned> UnwindOpInfo[] = {
   { "R15", 15 }
 };
 
-// Some additional COFF structures not defined by llvm::object.
-namespace {
-  struct coff_aux_file_record {
-    char FileName[18];
-  };
-} // namespace
-
 static uint64_t getOffsetOfLSDA(const Win64EH::UnwindInfo& UI) {
   return static_cast<const char*>(UI.getLanguageSpecificData())
          - reinterpret_cast<const char*>(&UI);
@@ -979,13 +972,12 @@ void COFFDumper::printSymbol(const SymbolRef &Sym) {
       W.printBinary("Unused", makeArrayRef(Aux->Unused));
 
     } else if (Symbol->isFileRecord()) {
-      const coff_aux_file_record *Aux;
+      const coff_aux_file *Aux;
       if (error(getSymbolAuxData(Obj, Symbol + I, Aux)))
         break;
 
       DictScope AS(W, "AuxFileRecord");
       W.printString("FileName", StringRef(Aux->FileName));
-
     } else if (Symbol->isSectionDefinition()) {
       const coff_aux_section_definition *Aux;
       if (error(getSymbolAuxData(Obj, Symbol + I, Aux)))
