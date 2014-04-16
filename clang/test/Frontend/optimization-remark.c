17b2a1b4b0722d9f4588bb4d862e0f756653ecee
Diego Novillo <dnovillo@google.com>
2014-04-16 19:02:35 +0000

    Fix check failure on hexagon targets.
    This test was failing because there is no assembler for hexagon-elf on
    this buildbot:
    
    http://lab.llvm.org:8011/builders/clang-hexagon-elf/builds/10470
    
    Fixed by adding -S to the driver invocation.
    
    llvm-svn: 206412

--- clang/test/Frontend/optimization-remark.c
+++ clang/test/Frontend/optimization-remark.c
@@ -2,10 +2,10 @@
 // designed to always trigger the inliner, so it should be independent
 // of the optimization level.
 
-// RUN: %clang -c %s -Rpass=inline -O0 -gline-tables-only -o /dev/null 2> %t.err
+// RUN: %clang -c %s -Rpass=inline -O0 -gline-tables-only -S -o /dev/null 2> %t.err
 // RUN: FileCheck < %t.err %s --check-prefix=INLINE
 
-// RUN: %clang -c %s -Rpass=inline -O0 -o /dev/null 2> %t.err
+// RUN: %clang -c %s -Rpass=inline -O0 -S -o /dev/null 2> %t.err
 // RUN: FileCheck < %t.err %s --check-prefix=INLINE-NO-LOC
 
 int foo(int x, int y) __attribute__((always_inline));
