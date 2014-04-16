6f6e76dfe010f8e4e49f26317f9f4d8d689a91f4
Reid Kleckner <reid@kleckner.net>
2014-04-16 20:10:16 +0000

    Revert "Move -fms-extensions predefined macros into InitPreprocessor"
    This reverts commit r206413.
    
    This was proposed before, but it's not clear if this is really a good
    idea:
    http://reviews.llvm.org/D3034
    
    llvm-svn: 206415

--- clang/test/Preprocessor/microsoft-ext.c
+++ clang/test/Preprocessor/microsoft-ext.c
@@ -1,9 +1,5 @@
-// RUN: %clang_cc1 -triple i686-pc-win32 -E -fms-extensions -fms-compatibility %s -o - | FileCheck %s
-// RUN: %clang_cc1 -triple i686-linux-gnu -E -fms-extensions -fms-compatibility %s -o - | FileCheck %s
-
-#ifndef _MSC_EXTENSIONS
-# error "_MSC_EXTENSIONS not defined with -fms-extensions"
-#endif
+// RUN: %clang_cc1 -E -fms-compatibility %s -o %t
+// RUN: FileCheck %s < %t
 
 # define M2(x, y) x + y
 # define P(x, y) {x, y}
