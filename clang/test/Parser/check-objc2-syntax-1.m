ec328b8e6f1b0fee6c6ebe40ee97ed2e7acfaa14
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2014-04-17 02:32:52 +0000

    [Parser] Remove "add -fmodules" from the "use of '@import' when modules are disabled, add -fmodules" error.
    We disable modules for C++, at which point this is confusing.
    rdar://16641918
    
    llvm-svn: 206440

--- clang/test/Parser/check-objc2-syntax-1.m
+++ clang/test/Parser/check-objc2-syntax-1.m
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -fsyntax-only -verify %s
 
 // rdar://15505492
-@import Foundation; // expected-error {{use of '@import' when modules are disabled, add -fmodules}}
+@import Foundation; // expected-error {{use of '@import' when modules are disabled}}
 
 @interface Subclass 
 + (int)magicNumber;
