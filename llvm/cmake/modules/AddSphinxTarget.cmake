9f5eb637cb1758a5efa38845f4ce9c5f600dd99b
Reid Kleckner <reid@kleckner.net>
2014-04-18 21:45:25 +0000

    Added Sphinx documentation generation to CMake build system.
    The option LLVM_ENABLE_SPHINX option enables the "docs-llvm-html",
    "docs-llvm-man" targets but does not build them by default. The
    following CMake options have been added that control what targets are
    made available
    
    SPHINX_OUTPUT_HTML
    SPHINX_OUTPUT_MAN
    
    If LLVM_BUILD_DOCS is enabled then the enabled docs-llvm-* targets will
    be built by default and if ``make install`` is run then docs-llvm-html
    and docs-llvm-man will be installed (tested on Linux only).
    
    The add_sphinx_target function is in its own file so it can be included
    by other projects that use Sphinx for their documentation.
    
    Patch by Daniel Liew <daniel.liew@imperial.ac.uk>!
    
    llvm-svn: 206655

---
+++ llvm/cmake/modules/AddSphinxTarget.cmake
@@ -0,0 +1,54 @@
+# Handy function for creating the different Sphinx targets.
+#
+# ``builder`` should be one of the supported builders used by
+# the sphinx-build command.
+#
+# ``project`` should be the project name
+function (add_sphinx_target builder project)
+  set(SPHINX_BUILD_DIR "${CMAKE_CURRENT_BINARY_DIR}/${builder}")
+  set(SPHINX_DOC_TREE_DIR "${CMAKE_CURRENT_BINARY_DIR}/_doctrees")
+  set(SPHINX_TARGET_NAME docs-${project}-${builder})
+  add_custom_target(${SPHINX_TARGET_NAME}
+                    COMMAND ${SPHINX_EXECUTABLE}
+                            -b ${builder}
+                            -d "${SPHINX_DOC_TREE_DIR}"
+                            -q # Quiet: no output other than errors and warnings.
+                            -W # Warnings are errors.
+                            "${CMAKE_CURRENT_SOURCE_DIR}" # Source
+                            "${SPHINX_BUILD_DIR}" # Output
+                    COMMENT
+                    "Generating ${builder} Sphinx documentation for ${project}")
+
+  # When "clean" target is run, remove the Sphinx build directory
+  set_property(DIRECTORY APPEND PROPERTY
+               ADDITIONAL_MAKE_CLEAN_FILES
+               "${SPHINX_BUILD_DIR}")
+
+  # We need to remove ${SPHINX_DOC_TREE_DIR} when make clean is run
+  # but we should only add this path once
+  get_property(_CURRENT_MAKE_CLEAN_FILES
+               DIRECTORY PROPERTY ADDITIONAL_MAKE_CLEAN_FILES)
+  list(FIND _CURRENT_MAKE_CLEAN_FILES "${SPHINX_DOC_TREE_DIR}" _INDEX)
+  if (_INDEX EQUAL -1)
+    set_property(DIRECTORY APPEND PROPERTY
+                 ADDITIONAL_MAKE_CLEAN_FILES
+                 "${SPHINX_DOC_TREE_DIR}")
+  endif()
+
+  if (LLVM_BUILD_DOCS)
+    add_dependencies(sphinx ${SPHINX_TARGET_NAME})
+
+    # Handle installation
+    if (builder STREQUAL man)
+      # FIXME: We might not ship all the tools that these man pages describe
+      install(DIRECTORY "${SPHINX_BUILD_DIR}/" # Slash indicates contents of
+              DESTINATION share/man/man1)
+
+    elseif (builder STREQUAL html)
+      install(DIRECTORY "${SPHINX_BUILD_DIR}"
+              DESTINATION "share/doc/${project}")
+    else()
+      message(WARNING Installation of ${builder} not supported)
+    endif()
+  endif()
+endfunction()
