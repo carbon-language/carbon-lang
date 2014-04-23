e3e047e768e013c70f34d221c57012e5efe150c4
Rafael Espindola <rafael.espindola@gmail.com>
2014-04-23 12:38:13 +0000

    This reverts commit r206934 and r206935.
    The original messages were:
      "Driver: Honor %INCLUDE% when built with MinGW"
      "Add missing test triples"
    
    The test was still failing on OS X.
    
    llvm-svn: 206973

--- clang/lib/Driver/WindowsToolChain.cpp
+++ clang/lib/Driver/WindowsToolChain.cpp
@@ -293,6 +293,7 @@ void Windows::AddClangSystemIncludeArgs(const ArgList &DriverArgs,
   if (DriverArgs.hasArg(options::OPT_nostdlibinc))
     return;
 
+#ifdef _MSC_VER
   // Honor %INCLUDE%. It should know essential search paths with vcvarsall.bat.
   if (const char *cl_include_dir = getenv("INCLUDE")) {
     SmallVector<StringRef, 8> Dirs;
@@ -304,7 +305,6 @@ void Windows::AddClangSystemIncludeArgs(const ArgList &DriverArgs,
       return;
   }
 
-#ifdef _MSC_VER
   std::string VSDir;
   std::string WindowsSDKDir;
 
