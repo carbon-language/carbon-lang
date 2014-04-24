02fefcfc3bc205abd9e4848d0e285c41fdbfc014
Reid Kleckner <reid@kleckner.net>
2014-04-24 23:06:17 +0000

    Reland r206934 with a hopefully fixed test
    llvm-svn: 207155

---
+++ clang/test/Driver/windows-include-env.c
@@ -0,0 +1,4 @@
+// RUN: env INCLUDE=my_system_headers %clang --target=i686-windows -c %s -### 2>&1 | FileCheck %s
+// RUN: env INCLUDE=my_system_headers %clang_cl -c %s -### 2>&1 | FileCheck %s
+// CHECK: "-cc1"
+// CHECK: "-internal-isystem" "my_system_headers"
