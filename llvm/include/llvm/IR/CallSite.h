3981faecbd62349fb3180ad9bf7b93a3f4899e2b
Reid Kleckner <reid@kleckner.net>
2014-04-24 20:19:22 +0000

    Remove dead inline function that doesn't compile
    MSVC doesn't diagnose this, interestingly.
    
    llvm-svn: 207144

--- llvm/include/llvm/IR/CallSite.h
+++ llvm/include/llvm/IR/CallSite.h
@@ -160,9 +160,6 @@ public:
   ///
   FunTy *getCaller() const { return (*this)->getParent()->getParent(); }
 
-  /// \brief Tests if this is a tail call.  Only a CallInst can be a tail call.
-  bool isTailCall() const { return isCall() && cast<CallInst>->isTailCall(); }
-
   /// \brief Tests if this call site must be tail call optimized.  Only a
   /// CallInst can be tail call optimized.
   bool isMustTailCall() const {
