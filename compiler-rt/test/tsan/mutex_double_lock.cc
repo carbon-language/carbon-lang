ea014b787a59e53f40b48d8f47b9a5f9bb0bfcfe
Dmitry Vyukov <dvyukov@google.com>
2014-04-25 07:49:36 +0000

    tsan: add new test for commit 207204 (forget to add new file)
    llvm-svn: 207205

---
+++ compiler-rt/test/tsan/mutex_double_lock.cc
@@ -0,0 +1,29 @@
+// RUN: %clangxx_tsan -O1 %s -o %t && not %t 2>&1 | FileCheck %s
+#include <pthread.h>
+#include <unistd.h>
+
+extern "C" void AnnotateRWLockAcquired(const char *f, int l, void *m, long rw);
+
+void *ThreadFunc(void *m) {
+  AnnotateRWLockAcquired(__FILE__, __LINE__, m, 1);
+  return 0;
+}
+
+int main() {
+  int m = 0;
+  AnnotateRWLockAcquired(__FILE__, __LINE__, &m, 1);
+  pthread_t th;
+  pthread_create(&th, 0, ThreadFunc, &m);
+  pthread_join(th, 0);
+  return 0;
+}
+
+// CHECK: WARNING: ThreadSanitizer: double lock of a mutex
+// CHECK:     #0 AnnotateRWLockAcquired
+// CHECK:     #1 ThreadFunc
+// CHECK: Location is stack of main thread.
+// CHECK:   Mutex M1 ({{.*}}) created at:
+// CHECK:     #0 AnnotateRWLockAcquired
+// CHECK:     #1 main
+// CHECK: SUMMARY: ThreadSanitizer: double lock of a mutex {{.*}}mutex_double_lock.cc{{.*}}ThreadFunc
+
