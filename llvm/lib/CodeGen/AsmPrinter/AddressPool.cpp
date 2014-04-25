69d0cf06bc3ed24cd1059487d6aa77b7cc2421b9
David Blaikie <dblaikie@gmail.com>
2014-04-25 06:22:32 +0000

    Add missing cpp file header
    Code review feedback from Paul Robinson on r207022
    
    llvm-svn: 207198

--- llvm/lib/CodeGen/AsmPrinter/AddressPool.cpp
+++ llvm/lib/CodeGen/AsmPrinter/AddressPool.cpp
@@ -1,3 +1,11 @@
+//===-- llvm/CodeGen/AddressPool.cpp - Dwarf Debug Framework ---*- C++ -*--===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
 
 #include "AddressPool.h"
 #include "llvm/CodeGen/AsmPrinter.h"
