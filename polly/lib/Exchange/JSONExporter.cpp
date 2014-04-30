ff69b3c26076c49002d312c4dc7a33991628dd58
Benjamin Kramer <benny.kra@googlemail.com>
2014-04-30 07:26:28 +0000

    Add missing include.
    llvm-svn: 207618

--- polly/lib/Exchange/JSONExporter.cpp
+++ polly/lib/Exchange/JSONExporter.cpp
@@ -18,6 +18,7 @@
 #include "polly/ScopPass.h"
 #include "llvm/ADT/OwningPtr.h"
 #include "llvm/ADT/Statistic.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/ToolOutputFile.h"
 #include "llvm/Support/system_error.h"
