98dccb109bc910597683a179281f73f5b9c65c78
Tom Stellard <thomas.stellard@amd.com>
2014-04-30 18:35:20 +0000

    Fix build broken by LLVM commit r207593
    llvm-svn: 207685

--- libclc/utils/prepare-builtins.cpp
+++ libclc/utils/prepare-builtins.cpp
@@ -7,6 +7,7 @@
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/system_error.h"
 #include "llvm/Support/ErrorOr.h"
