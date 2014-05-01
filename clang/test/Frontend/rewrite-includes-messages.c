1f03d08a8c103a3bc5de93e4f5e394c6f5ffbf1d
Lubos Lunak <l.lunak@centrum.cz>
2014-05-01 22:40:00 +0000

    do not hide clang stderr output during a test
    I don't know why this fails on the buildbot.
    
    llvm-svn: 207808

--- clang/test/Frontend/rewrite-includes-messages.c
+++ clang/test/Frontend/rewrite-includes-messages.c
@@ -1,5 +1,5 @@
-// RUN: %clang -E -frewrite-includes %s -I%S/Inputs/ | %clang -Wall -Wunused-macros -x c -c - 2> %t.1
-// RUN: %clang -I%S/Inputs/ -Wall -Wunused-macros -c %s 2> %t.2
+// RUN: %clang -E -frewrite-includes %s -I%S/Inputs/ | %clang -Wall -Wunused-macros -x c -c - 2>&1 | tee %t.1
+// RUN: %clang -I%S/Inputs/ -Wall -Wunused-macros -c %s 2>&1 | tee %t.2
 // RUN: diff %t.1 %t.2
 // expected-no-diagnostics
 // REQUIRES: shell
