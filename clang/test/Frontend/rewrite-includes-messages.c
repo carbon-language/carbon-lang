adc2fde0166d7dd9f335b8f1fe59fdbd9caca761
Lubos Lunak <l.lunak@centrum.cz>
2014-05-01 21:36:08 +0000

    use 'diff' rather than 'cmp -s' in a test
    That's what all tests use, no idea where I got the latter from.
    
    llvm-svn: 207797

--- clang/test/Frontend/rewrite-includes-messages.c
+++ clang/test/Frontend/rewrite-includes-messages.c
@@ -1,6 +1,6 @@
 // RUN: %clang -E -frewrite-includes %s -I%S/Inputs/ | %clang -Wall -Wunused-macros -x c -c - 2> %t.1
 // RUN: %clang -I%S/Inputs/ -Wall -Wunused-macros -c %s 2> %t.2
-// RUN: cmp -s %t.1 %t.2
+// RUN: diff %t.1 %t.2
 // expected-no-diagnostics
 // REQUIRES: shell
 
