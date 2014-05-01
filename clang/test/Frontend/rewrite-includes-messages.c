d9070c5ee77c5429b7f84df965df4206045c3504
Lubos Lunak <l.lunak@centrum.cz>
2014-05-01 23:58:20 +0000

    -fsyntax-only for a test
    llvm-svn: 207814

--- clang/test/Frontend/rewrite-includes-messages.c
+++ clang/test/Frontend/rewrite-includes-messages.c
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -E -frewrite-includes %s -I%S/Inputs/ | %clang_cc1 -Wall -fsyntax-only -Wunused-macros -x c - 2>&1 > %t.1
-// RUN: %clang_cc1 -I%S/Inputs/ -Wall -Wunused-macros %s 2>&1 > %t.2
+// RUN: %clang_cc1 -I%S/Inputs/ -Wall -Wunused-macros -fsyntax-only %s 2>&1 > %t.2
 // RUN: diff %t.1 %t.2 -u
 // expected-no-diagnostics
 
