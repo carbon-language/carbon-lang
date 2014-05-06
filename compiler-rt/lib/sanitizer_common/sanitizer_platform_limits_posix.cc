98e55ef41a83cb5190f3cf9af0b7c86f3f6039cd
Kostya Serebryany <kcc@google.com>
2014-05-06 07:36:51 +0000

    [sanitizer] fix build with glibc 2.4, patch by Sandra Loosemore
    llvm-svn: 208066

--- compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_posix.cc
@@ -288,15 +288,20 @@ namespace __sanitizer {
   int ptrace_setfpregs = PTRACE_SETFPREGS;
   int ptrace_getfpxregs = PTRACE_GETFPXREGS;
   int ptrace_setfpxregs = PTRACE_SETFPXREGS;
+#if defined(PTRACE_GETSIGINFO) && defined(PTRACE_SETSIGINFO)
   int ptrace_getsiginfo = PTRACE_GETSIGINFO;
   int ptrace_setsiginfo = PTRACE_SETSIGINFO;
+#else
+  int ptrace_getsiginfo = -1;
+  int ptrace_setsiginfo = -1;
+#endif  // PTRACE_GETSIGINFO/PTRACE_SETSIGINFO
 #if defined(PTRACE_GETREGSET) && defined(PTRACE_SETREGSET)
   int ptrace_getregset = PTRACE_GETREGSET;
   int ptrace_setregset = PTRACE_SETREGSET;
 #else
   int ptrace_getregset = -1;
   int ptrace_setregset = -1;
-#endif
+#endif  // PTRACE_GETREGSET/PTRACE_SETREGSET
 #endif
 
   unsigned path_max = PATH_MAX;
