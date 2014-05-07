e185d2886208794eb1d792d35a8011fa9baa564c
Yunzhong Gao <Yunzhong_Gao@playstation.sony.com>
2014-05-07 01:58:02 +0000

    Clean up some existing keyword tests in the test/Lexer directory by using the pre-defined __is_identifier() macro.
    Differential Revision: http://reviews.llvm.org/D3460
    
    llvm-svn: 208147

--- clang/test/Lexer/cxx0x_keyword.cpp
+++
@@ -1,2 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -std=c++11 %s 2>&1
-int static_assert; /* expected-error {{expected unqualified-id}} */
---
+++ clang/test/Lexer/keywords_test.cpp
@@ -0,0 +1,34 @@
+// RUN: %clang_cc1 -std=c++03 -fsyntax-only %s
+// RUN: %clang_cc1 -std=c++11 -DCXX11 -fsyntax-only %s
+
+#define IS_KEYWORD(NAME) _Static_assert(!__is_identifier(NAME), #NAME)
+#define NOT_KEYWORD(NAME) _Static_assert(__is_identifier(NAME), #NAME)
+#define IS_TYPE(NAME) void is_##NAME##_type() { int f(NAME); }
+
+#ifdef CXX11
+#define CXX11_KEYWORD(NAME)  IS_KEYWORD(NAME)
+#define CXX11_TYPE(NAME)     IS_TYPE(NAME)
+#else
+#define CXX11_KEYWORD(NAME)  NOT_KEYWORD(NAME)
+#define CXX11_TYPE(NAME)
+#endif
+
+// C++11 keywords
+CXX11_KEYWORD(nullptr);
+CXX11_KEYWORD(decltype);
+CXX11_KEYWORD(alignof);
+CXX11_KEYWORD(alignas);
+CXX11_KEYWORD(char16_t);
+CXX11_TYPE(char16_t);
+CXX11_KEYWORD(char32_t);
+CXX11_TYPE(char32_t);
+CXX11_KEYWORD(constexpr);
+CXX11_KEYWORD(noexcept);
+CXX11_KEYWORD(static_assert);
+CXX11_KEYWORD(thread_local);
+
+// Clang extension
+IS_KEYWORD(__char16_t);
+IS_TYPE(__char16_t);
+IS_KEYWORD(__char32_t);
+IS_TYPE(__char32_t);
