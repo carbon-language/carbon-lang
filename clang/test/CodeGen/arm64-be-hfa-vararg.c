847501f92e985699c6487bb998113fec949c49a1
James Molloy <james.molloy@arm.com>
2014-05-07 14:51:57 +0000

    Re-satisfy the bots. Due to a de-sync between my clang and LLVM trees, I didn't notice that the generated BE alignment register is now unnamed and is not called '%align_be'.
    llvm-svn: 208217

--- clang/test/CodeGen/arm64-be-hfa-vararg.c
+++ clang/test/CodeGen/arm64-be-hfa-vararg.c
@@ -4,7 +4,7 @@
 
 // A single member HFA must be aligned just like a non-HFA register argument.
 double callee(int a, ...) {
-// CHECK: %align_be = add i64 %{{.*}}, 8
+// CHECK: = add i64 %{{.*}}, 8
   va_list vl;
   va_start(vl, a);
   double result = va_arg(vl, struct { double a; }).a;
