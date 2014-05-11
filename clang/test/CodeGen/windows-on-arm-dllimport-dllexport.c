8ab3e83efa434b5a3301802a0c4e9f30b99f999e
Saleem Abdulrasool <compnerd@compnerd.org>
2014-05-11 01:31:57 +0000

    CodeGen: support dll{ex,im}port on WoA
    Add ARM support for dllexport and dllimport attributes.  This is a relatively
    conservative change.  The alternative is to entirely drop the architecture
    requirement.  The dllimport and dllexport attributes are not restricted to any
    architecture, simply to platforms that support this attribute (currently
    Windows).
    
    llvm-svn: 208486

---
+++ clang/test/CodeGen/windows-on-arm-dllimport-dllexport.c
@@ -0,0 +1,25 @@
+// RUN: %clang_cc1 -Werror -triple thumbv7-windows-itanium -mfloat-abi hard -emit-llvm %s -o - | FileCheck %s
+
+__declspec(dllexport) int export_int;
+
+__declspec(dllimport) int import_int;
+
+__declspec(dllexport) void export_declared_function();
+
+__declspec(dllexport) void export_implemented_function() {
+}
+
+__declspec(dllimport) void import_function(int);
+
+void call_imported_function() {
+  export_declared_function();
+  return import_function(import_int);
+}
+
+// CHECK: @import_int = external dllimport global i32
+// CHECK: @export_int = common dllexport global i32 0, align 4
+
+// CHECK: define dllexport arm_aapcs_vfpcc void @export_implemented_function()
+
+// CHECK: declare dllimport arm_aapcs_vfpcc void @import_function(i32)
+
