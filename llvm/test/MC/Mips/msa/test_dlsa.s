04092f5bc52ff1ad4a87db91f8992bae84598c82
Matheus Almeida <matheus.almeida@imgtec.com>
2014-05-12 16:20:46 +0000

    [mips] Move disassembler tests (Mips MSA test_lsa, test_dlsa) into correct folder.
    llvm-svn: 208588

--- llvm/test/MC/Mips/msa/test_dlsa.s
+++ llvm/test/MC/Mips/msa/test_dlsa.s
@@ -1,19 +1,10 @@
 # RUN: llvm-mc %s -arch=mips64 -mcpu=mips64r2 -mattr=+msa -show-encoding | \
 # RUN:   FileCheck %s
 #
-# RUN: llvm-mc %s -arch=mips -mcpu=mips64r2 -mattr=+msa -filetype=obj -o - | \
-# RUN:   llvm-objdump -d -arch=mips64 -mattr=+msa - | \
-# RUN:     FileCheck %s -check-prefix=CHECKOBJDUMP
-#
 # CHECK:        dlsa        $8, $9, $10, 1              # encoding: [0x01,0x2a,0x40,0x15]
 # CHECK:        dlsa        $8, $9, $10, 2              # encoding: [0x01,0x2a,0x40,0x55]
 # CHECK:        dlsa        $8, $9, $10, 3              # encoding: [0x01,0x2a,0x40,0x95]
 # CHECK:        dlsa        $8, $9, $10, 4              # encoding: [0x01,0x2a,0x40,0xd5]
-
-# CHECKOBJDUMP: dlsa        $8, $9, $10, 1
-# CHECKOBJDUMP: dlsa        $8, $9, $10, 2
-# CHECKOBJDUMP: dlsa        $8, $9, $10, 3
-# CHECKOBJDUMP: dlsa        $8, $9, $10, 4
 
                 dlsa        $8, $9, $10, 1
                 dlsa        $8, $9, $10, 2
--- llvm/test/MC/Mips/msa/test_lsa.s
+++ llvm/test/MC/Mips/msa/test_lsa.s
@@ -1,18 +1,9 @@
 # RUN: llvm-mc %s -arch=mips -mcpu=mips32r2 -mattr=+msa -show-encoding | FileCheck %s
 #
-# RUN: llvm-mc %s -arch=mips -mcpu=mips32r2 -mattr=+msa -filetype=obj -o - | \
-# RUN: llvm-objdump -d -arch=mips -mattr=+msa - | \
-# RUN: FileCheck %s -check-prefix=CHECKOBJDUMP
-#
 # CHECK:        lsa        $8, $9, $10, 1              # encoding: [0x01,0x2a,0x40,0x05]
 # CHECK:        lsa        $8, $9, $10, 2              # encoding: [0x01,0x2a,0x40,0x45]
 # CHECK:        lsa        $8, $9, $10, 3              # encoding: [0x01,0x2a,0x40,0x85]
 # CHECK:        lsa        $8, $9, $10, 4              # encoding: [0x01,0x2a,0x40,0xc5]
-
-# CHECKOBJDUMP: lsa        $8, $9, $10, 1
-# CHECKOBJDUMP: lsa        $8, $9, $10, 2
-# CHECKOBJDUMP: lsa        $8, $9, $10, 3
-# CHECKOBJDUMP: lsa        $8, $9, $10, 4
 
                 lsa        $8, $9, $10, 1
                 lsa        $8, $9, $10, 2
