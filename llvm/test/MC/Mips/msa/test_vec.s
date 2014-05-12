440000d6ec41845ca5fc306d070f289b41c28dac
Matheus Almeida <matheus.almeida@imgtec.com>
2014-05-12 16:31:45 +0000

    [mips] Move disassembler test (Mips MSA test_vec) into correct folder.
    llvm-svn: 208592

--- llvm/test/MC/Mips/msa/test_vec.s
+++ llvm/test/MC/Mips/msa/test_vec.s
@@ -1,9 +1,5 @@
 # RUN: llvm-mc %s -arch=mips -mcpu=mips32r2 -mattr=+msa -show-encoding | FileCheck %s
 #
-# RUN: llvm-mc %s -arch=mips -mcpu=mips32r2 -mattr=+msa -filetype=obj -o - | \
-# RUN: llvm-objdump -d -arch=mips -mattr=+msa - | \
-# RUN: FileCheck %s -check-prefix=CHECKOBJDUMP
-#
 # CHECK:        and.v   $w25, $w20, $w27        # encoding: [0x78,0x1b,0xa6,0x5e]
 # CHECK:        bmnz.v  $w17, $w6, $w7          # encoding: [0x78,0x87,0x34,0x5e]
 # CHECK:        bmz.v   $w3, $w17, $w9          # encoding: [0x78,0xa9,0x88,0xde]
@@ -11,14 +7,6 @@
 # CHECK:        nor.v   $w7, $w31, $w0          # encoding: [0x78,0x40,0xf9,0xde]
 # CHECK:        or.v    $w24, $w26, $w30        # encoding: [0x78,0x3e,0xd6,0x1e]
 # CHECK:        xor.v   $w7, $w27, $w15         # encoding: [0x78,0x6f,0xd9,0xde]
-
-# CHECKOBJDUMP:        and.v   $w25, $w20, $w27
-# CHECKOBJDUMP:        bmnz.v  $w17, $w6, $w7
-# CHECKOBJDUMP:        bmz.v   $w3, $w17, $w9
-# CHECKOBJDUMP:        bsel.v  $w8, $w0, $w14
-# CHECKOBJDUMP:        nor.v   $w7, $w31, $w0
-# CHECKOBJDUMP:        or.v    $w24, $w26, $w30
-# CHECKOBJDUMP:        xor.v   $w7, $w27, $w15
 
                 and.v   $w25, $w20, $w27
                 bmnz.v  $w17, $w6, $w7
