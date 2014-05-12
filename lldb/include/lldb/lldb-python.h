f69b64d6d2ccfef57c5ed6896180af05fb6ee017
Saleem Abdulrasool <compnerd@compnerd.org>
2014-05-12 06:44:01 +0000

    python: silence macro redefinition warnings
    Python defines _XOPEN_SOURCE and _POSIX_C_SOURCE unconditionally.  On Linux,
    this is problematic as glibc's features.h defines these values if _GNU_SOURCE is
    defined to the value that is currently implemented.  Python defines it to the
    versions that it requires, which may be different.  Undefine the macros on Linux
    (technically, this should be safe to do globally) before including the python
    headers.
    
    llvm-svn: 208520

--- lldb/include/lldb/lldb-python.h
+++ lldb/include/lldb/lldb-python.h
@@ -18,6 +18,15 @@
 
 #else
 
+#if defined(__linux__)
+// features.h will define _POSIX_C_SOURCE if _GNU_SOURCE is defined.  This value
+// may be different from the value that Python defines it to be which results
+// in a warning.  Undefine _POSIX_C_SOURCE before including Python.h  The same
+// holds for _XOPEN_SOURCE.
+#undef _POSIX_C_SOURCE
+#undef _XOPEN_SOURCE
+#endif
+
 #include <Python.h>
 
 #endif // LLDB_DISABLE_PYTHON
