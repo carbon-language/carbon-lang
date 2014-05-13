a27b0441662a7adcfd2ab7e06077098fefb101a1
Timur Iskhodzhanov <timurrrr@google.com>
2014-05-13 13:59:05 +0000

    Define the InterlockedCompareExchange64 intrinsic on 32-bits too
    llvm-svn: 208699

--- clang/lib/Headers/Intrin.h
+++ clang/lib/Headers/Intrin.h
@@ -825,14 +825,12 @@ _InterlockedCompareExchangePointer(void *volatile *_Destination,
   return _Comparand;
 }
 #endif
-#ifdef __x86_64__
 static __inline__ __int64 __attribute__((__always_inline__, __nodebug__))
 _InterlockedCompareExchange64(__int64 volatile *_Destination,
                               __int64 _Exchange, __int64 _Comparand) {
   __atomic_compare_exchange(_Destination, &_Comparand, &_Exchange, 0, 0, 0);
   return _Comparand;
 }
-#endif
 /*----------------------------------------------------------------------------*\
 |* Barriers
 \*----------------------------------------------------------------------------*/
