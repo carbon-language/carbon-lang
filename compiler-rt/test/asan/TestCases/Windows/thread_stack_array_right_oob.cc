8bf36cae283ddb7c61461c616268454e688f8f0e
Timur Iskhodzhanov <timurrrr@google.com>
2014-05-15 09:12:55 +0000

    [ASan tests] Add ASan Windows threading tests
    llvm-svn: 208854

--- compiler-rt/test/asan/TestCases/Windows/thread_stack_array_right_oob.cc
+++ compiler-rt/test/asan/TestCases/Windows/thread_stack_array_right_oob.cc
@@ -5,21 +5,21 @@
 #include <windows.h>
 
 DWORD WINAPI thread_proc(void *) {
-  int subscript = -1;
+  int subscript = 42;
   volatile char stack_buffer[42];
   stack_buffer[subscript] = 42;
 // CHECK: AddressSanitizer: stack-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
 // CHECK: WRITE of size 1 at [[ADDR]] thread T1
-// CHECK:   {{#0 .* thread_proc .*thread_stack_array_left_oob.cc}}:[[@LINE-3]]
+// CHECK:   {{#0 .* thread_proc .*thread_stack_array_right_oob.cc}}:[[@LINE-3]]
 // CHECK: Address [[ADDR]] is located in stack of thread T1 at offset {{.*}} in frame
 // CHECK:   thread_proc
   return 0;
 }
 
-int main() {
+int main(void) {
   HANDLE thr = CreateThread(NULL, 0, thread_proc, NULL, 0, NULL);
 // CHECK: Thread T1 created by T0 here:
-// CHECK:   {{#[01] .* main .*thread_stack_array_left_oob.cc}}:[[@LINE-2]]
+// CHECK:   {{#[01] .* main .*thread_stack_array_right_oob.cc}}:[[@LINE-2]]
 
   // A failure to create a thread should fail the test!
   if (thr == 0) return 0;
---
+++ compiler-rt/test/asan/TestCases/Windows/windows_h.cc
@@ -0,0 +1,7 @@
+// RUN: %clangxx_asan -O0 %s -Fe%t
+// RUN: %run %t
+
+// Just make sure we can parse <windows.h>
+#include <windows.h>
+
+int main() {}
