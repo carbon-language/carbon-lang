f32b690f690b151d4ed3f338630659768a7c8269
Timur Iskhodzhanov <timurrrr@google.com>
2014-05-15 10:58:35 +0000

    [ASan/Win tests] Add tests for aligned mallocs, bitfields and global strings
    llvm-svn: 208859

---
+++ compiler-rt/test/asan/TestCases/Windows/global_const_string.cc
@@ -0,0 +1,12 @@
+// RUN: %clangxx_asan -O0 %s -Fe%t
+// RUN: %run %t | FileCheck %s
+
+#include <windows.h>
+#include <stdio.h>
+
+int main(void) {
+  static const char *foo = "foobarspam";
+  printf("Global string is `%s`\n", foo);
+// CHECK: Global string is `foobarspam`
+  return 0;
+}
---
+++ compiler-rt/test/asan/TestCases/Windows/global_const_string_oob.cc
@@ -0,0 +1,21 @@
+// RUN: %clangxx_asan -O0 %s -Fe%t
+// FIXME: 'cat' is needed due to PR19744.
+// RUN: not %run %t 2>&1 | cat | FileCheck %s
+
+#include <windows.h>
+#include <stdio.h>
+
+extern "C" const char *foo = "foobarspam";
+
+int main(void) {
+  if (foo[16])
+    printf("Boo\n");
+// CHECK-NOT: Boo
+// CHECK: AddressSanitizer: global-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
+// CHECK: READ of size 1 at [[ADDR]] thread T0
+// CHECK-NEXT:   {{#0 .* main .*global_const_string_oob.cc:}}[[@LINE-5]]
+// CHECK: [[ADDR]] is located 5 bytes to the right of global variable [[STR:.*]] from {{'.*global_const_string_oob.cc' .*}} of size 11
+// CHECK:   [[STR]] is ascii string 'foobarspam'
+  return 0;
+}
+
