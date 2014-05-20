fcbed5af67002c45cf1e02149fa466ef58a5cded
Kevin Enderby <enderby@apple.com>
2014-05-20 21:10:15 +0000

    Revert r209235 as it broke two tests: Failing Tests (2): 	    LLVM :: ExecutionEngine/MCJIT/stubs-sm-pic.ll 	    LLVM :: ExecutionEngine/MCJIT/stubs.ll
    llvm-svn: 209236

--- llvm/lib/Object/MachOObjectFile.cpp
+++ llvm/lib/Object/MachOObjectFile.cpp
@@ -472,18 +472,10 @@ error_code MachOObjectFile::getSymbolAddress(DataRefImpl Symb,
                                              uint64_t &Res) const {
   if (is64Bit()) {
     MachO::nlist_64 Entry = getSymbol64TableEntry(Symb);
-    if ((Entry.n_type & MachO::N_TYPE) == MachO::N_UNDF &&
-        Entry.n_value == 0)
-      Res = UnknownAddressOrSize;
-    else
-      Res = Entry.n_value;
+    Res = Entry.n_value;
   } else {
     MachO::nlist Entry = getSymbolTableEntry(Symb);
-    if ((Entry.n_type & MachO::N_TYPE) == MachO::N_UNDF &&
-        Entry.n_value == 0)
-      Res = UnknownAddressOrSize;
-    else
-      Res = Entry.n_value;
+    Res = Entry.n_value;
   }
   return object_error::success;
 }
