e8103abdbc6729e471988d0dbd13b5c46c0fb41b
Sylvestre Ledru <sylvestre@debian.org>
2014-05-23 16:47:42 +0000

    revert "r209526 List the function/method name in the index page of scan-build " Depends on http://reviews.llvm.org/D3762
    llvm-svn: 209527

--- clang/lib/StaticAnalyzer/Core/HTMLDiagnostics.cpp
+++ clang/lib/StaticAnalyzer/Core/HTMLDiagnostics.cpp
@@ -215,10 +215,6 @@ void HTMLDiagnostics::ReportDiag(const PathDiagnostic& D,
 
     os << "\n<!-- BUGFILE " << DirName << Entry->getName() << " -->\n";
 
-    os << "\n<!-- FILENAME " << llvm::sys::path::filename(Entry->getName()) << " -->\n";
-
-    os  << "\n<!-- FUNCTIONNAME " <<  declName << " -->\n";
-
     os << "\n<!-- BUGLINE "
        << path.back()->getLocation().asLocation().getExpansionLineNumber()
        << " -->\n";
