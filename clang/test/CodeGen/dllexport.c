e9277401b7db23096ff49931d7d2694220ebd9ce
Hans Wennborg <hans@hanshq.net>
2014-05-23 23:29:44 +0000

    This test doesn't need -O2 -disable-llvm-optzns
    I forgot to fix this one in r209145. We use these flags on dllimport tests
    to make sure we emit code for available_externaly functions and don't inline
    the IR.
    
    llvm-svn: 209564

--- clang/test/CodeGen/dllexport.c
+++ clang/test/CodeGen/dllexport.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple i686-pc-win32 -std=c99 -O2 -disable-llvm-optzns -emit-llvm < %s | FileCheck %s
+// RUN: %clang_cc1 -triple i686-pc-win32 -std=c99 -emit-llvm < %s | FileCheck %s
 
 #define DLLEXPORT __declspec(dllexport)
 
