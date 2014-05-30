3e44a7fa5266d82c1bbb1a4285761484bd8c2c2b
Aaron Ballman <aaron@aaronballman.com>
2014-05-30 13:09:38 +0000

    This test should no longer be XFAILed; the standard has stabilized, and the test contents are acceptable. No diagnostics expected from this test.
    llvm-svn: 209891

--- clang/test/CXX/basic/basic.lookup/basic.lookup.qual/p6-0x.cpp
+++ clang/test/CXX/basic/basic.lookup/basic.lookup.qual/p6-0x.cpp
@@ -1,26 +1,25 @@
 // RUN: %clang_cc1 -std=c++11 -fsyntax-only -verify %s
-// XFAIL: *
-// Our C++0x doesn't currently have specialized destructor name handling,
-// since the specification is still in flux.
-struct C { 
-  typedef int I;
-}; 
+// expected-no-diagnostics
 
-typedef int I1, I2; 
-extern int* p; 
-extern int* q; 
+struct C {
+  typedef int I;
+};
+
+typedef int I1, I2;
+extern int* p;
+extern int* q;
 
 void f() {
-  p->C::I::~I(); 
+  p->C::I::~I();
   q->I1::~I2();
 }
 
-struct A { 
+struct A {
   ~A();
-}; 
+};
 
-typedef A AB; 
+typedef A AB;
 int main() {
-  AB *p; 
+  AB *p;
   p->AB::~AB();
 }
