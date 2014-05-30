f00500bc8e0e0d6146d53072c430059d8f9eb70f
Aaron Ballman <aaron@aaronballman.com>
2014-05-30 13:17:17 +0000

    This test is no longer relevant. It is covered already by [basic.scope.pdecl]p10, and our current test for that is more thorough (though our test is named p9.cpp).
    llvm-svn: 209892

--- clang/test/CXX/temp/temp.param/p14.cpp
+++
@@ -1,5 +0,0 @@
-// RUN: %clang_cc1 -fsyntax-only -verify %s 
-// XFAIL: *
-
-// A template-parameter shall not be used in its own default argument.
-template<typename T = typename T::type> struct X; // expected-error{{default}}
