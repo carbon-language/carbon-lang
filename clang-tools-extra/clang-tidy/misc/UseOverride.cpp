5070c18928e4b4855202ee327f537c7a1969051e
Daniel Jasper <djasper@google.com>
2014-05-30 09:21:17 +0000

    Remove redundant check discovered in post-commit review of r209505.
    llvm-svn: 209882

--- clang-tools-extra/clang-tidy/misc/UseOverride.cpp
+++ clang-tools-extra/clang-tidy/misc/UseOverride.cpp
@@ -96,10 +96,8 @@ void UseOverride::check(const MatchFinder::MatchResult &Result) {
       }
     }
 
-    if (InsertLoc.isInvalid() && Method->doesThisDeclarationHaveABody() &&
-        Method->getBody()) {
+    if (InsertLoc.isInvalid() && Method->doesThisDeclarationHaveABody())
       InsertLoc = Method->getBody()->getLocStart();
-    }
 
     if (!InsertLoc.isValid()) {
       if (Tokens.size() > 2 && GetText(Tokens.back(), Sources) == "0" &&
