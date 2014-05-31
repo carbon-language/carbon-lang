ca14e2b132201dbb0e2ecedd67a1b758fcc089d8
Justin Bogner <mail@justinbogner.com>
2014-05-31 07:59:03 +0000

    Fix XFAIL condition from r208840
    The XFAILs in r208840 were too general. They were meant to only apply
    to testing when use_system_lib was set.
    
    llvm-svn: 209972

--- libcxx/test/thread/futures/futures.future_error/what.pass.cpp
+++ libcxx/test/thread/futures/futures.future_error/what.pass.cpp
@@ -10,9 +10,9 @@
 // LWG 2056 changed the values of future_errc, so if we're using new headers
 // with an old library we'll get incorrect messages.
 //
-// XFAIL: x86_64-apple-darwin11
-// XFAIL: x86_64-apple-darwin12
-// XFAIL: x86_64-apple-darwin13
+// XFAIL: with_system_lib=x86_64-apple-darwin11
+// XFAIL: with_system_lib=x86_64-apple-darwin12
+// XFAIL: with_system_lib=x86_64-apple-darwin13
 
 // <future>
 
