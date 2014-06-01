d6f11a70abb1f674f00993429c81094a2f4014db
Rafael Espindola <rafael.espindola@gmail.com>
2014-06-01 13:45:09 +0000

    Test fixes and improvements.
    Remove redundant -fno-stack-protector run with openbsd.
    Add -target to the -fstack-protector tests so they pass on openbsd.
    
    Patch by Brad Smith.
    
    llvm-svn: 209991

--- clang/test/Driver/stack-protector.c
+++ clang/test/Driver/stack-protector.c
@@ -1,12 +1,12 @@
 // RUN: %clang -fno-stack-protector -### %s 2>&1 | FileCheck %s -check-prefix=NOSSP
-// NOSSP-NOT: "-stack-protector" "1"
+// NOSSP-NOT: "-stack-protector"
 // NOSSP-NOT: "-stack-protector-buffer-size" 
 
-// RUN: %clang -fstack-protector -### %s 2>&1 | FileCheck %s -check-prefix=SSP
+// RUN: %clang -target i386-unknown-linux -fstack-protector -### %s 2>&1 | FileCheck %s -check-prefix=SSP
 // SSP: "-stack-protector" "1"
 // SSP-NOT: "-stack-protector-buffer-size" 
 
-// RUN: %clang -fstack-protector --param ssp-buffer-size=16 -### %s 2>&1 | FileCheck %s -check-prefix=SSP-BUF
+// RUN: %clang -target i386-unknown-linux -fstack-protector --param ssp-buffer-size=16 -### %s 2>&1 | FileCheck %s -check-prefix=SSP-BUF
 // SSP-BUF: "-stack-protector" "1"
 // SSP-BUF: "-stack-protector-buffer-size" "16" 
 
@@ -15,9 +15,6 @@
 
 // RUN: %clang -target i386-pc-openbsd -fstack-protector -### %s 2>&1 | FileCheck %s -check-prefix=OPENBSD_SPS
 // OPENBSD_SPS: "-stack-protector" "2"
-
-// RUN: %clang -target i386-pc-openbsd -fno-stack-protector -### %s 2>&1 | FileCheck %s -check-prefix=OPENBSD_OFF
-// OPENBSD_OFF-NOT: "-stack-protector"
 
 // RUN: %clang -fstack-protector-strong -### %s 2>&1 | FileCheck %s -check-prefix=SSP-STRONG
 // SSP-STRONG: "-stack-protector" "2"
