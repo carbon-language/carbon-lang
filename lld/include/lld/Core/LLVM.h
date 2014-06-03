e3986f3e04bb9842bc0ade9b15fc1eb760fd0cb6
Rafael Espindola <rafael.espindola@gmail.com>
2014-06-03 15:36:31 +0000

    Remove dead code.
    New code should probably use shared_ptr anyway.
    
    llvm-svn: 210110

--- lld/include/lld/Core/LLVM.h
+++ lld/include/lld/Core/LLVM.h
@@ -38,12 +38,6 @@ namespace llvm {
   template<typename T>
   class ErrorOr;
 
-  // Reference counting.
-  template <typename T> class IntrusiveRefCntPtr;
-  template <typename T> struct IntrusiveRefCntPtrInfo;
-  template <class Derived> class RefCountedBase;
-  class RefCountedBaseVPTR;
-
   class error_code;
   class raw_ostream;
   // TODO: DenseMap, ...
@@ -67,12 +61,6 @@ namespace lld {
   using llvm::SmallVectorImpl;
   using llvm::SaveAndRestore;
   using llvm::ErrorOr;
-
-  // Reference counting.
-  using llvm::IntrusiveRefCntPtr;
-  using llvm::IntrusiveRefCntPtrInfo;
-  using llvm::RefCountedBase;
-  using llvm::RefCountedBaseVPTR;
 
   using llvm::error_code;
   using llvm::raw_ostream;
