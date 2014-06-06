731b501981fac1e5d78f97b9b67bf86abeb2f02b
Richard Trieu <rtrieu@google.com>
2014-06-06 23:56:22 +0000

    Fix my poor grammar from r210372
    llvm-svn: 210387

--- clang/test/SemaCXX/warn-undefined-bool-conversion.cpp
+++ clang/test/SemaCXX/warn-undefined-bool-conversion.cpp
@@ -6,10 +6,10 @@
 void test1(int &x) {
   if (x == 1) { }
   if (&x) { }
-  // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed always converted to true}}
+  // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed to always convert to true}}
 
   if (!&x) { }
-  // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed always converted to true}}
+  // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed to always convert to true}}
 }
 
 class test2 {
@@ -17,19 +17,19 @@ class test2 {
 
   void foo() {
     if (this) { }
-    // expected-warning@-1{{'this' pointer cannot be null in well-defined C++ code; pointer may be assumed always converted to true}}
+    // expected-warning@-1{{'this' pointer cannot be null in well-defined C++ code; pointer may be assumed to always convert to true}}
 
     if (!this) { }
-    // expected-warning@-1{{'this' pointer cannot be null in well-defined C++ code; pointer may be assumed always converted to true}}
+    // expected-warning@-1{{'this' pointer cannot be null in well-defined C++ code; pointer may be assumed to always convert to true}}
   }
 
   void bar() {
     if (x == 1) { }
     if (&x) { }
-    // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed always converted to true}}
+    // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed to always convert to true}}
 
     if (!&x) { }
-    // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed always converted to true}}
+    // expected-warning@-1{{reference cannot be bound to dereferenced null pointer in well-defined C++ code; pointer may be assumed to always convert to true}}
   }
 
   int &x;
