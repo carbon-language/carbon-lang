1e58f3aa3ab4178652546d1783903714bfe8619d
Alp Toker <alp@nuanti.com>
2014-06-10 07:03:25 +0000

    Don't suppress backend diagnostics in system headers
    Doing so would be inconsistent with the common fallback case where backend
    diagnostics without source locations are emitted unconditionally.
    
    llvm-svn: 210515

--- clang/test/Misc/backend-stack-frame-diagnostics.cpp
+++ clang/test/Misc/backend-stack-frame-diagnostics.cpp
@@ -1,11 +1,20 @@
 // REQUIRES: x86-registered-target
 // RUN: %clang -target i386-apple-darwin -std=c++11 -fblocks -Wframe-larger-than=70 -Xclang -verify -o /dev/null -c %s
+// RUN: %clang -target i386-apple-darwin -std=c++11 -fblocks -Wframe-larger-than=70 -Xclang -verify -o /dev/null -c %s -DIS_SYSHEADER
 
 // Test that:
 //  * The driver passes the option through to the backend.
 //  * The frontend diagnostic handler 'demangles' and resolves the correct function definition.
 
 // TODO: Support rich backend diagnostics for Objective-C methods.
+
+// Backend diagnostics aren't suppressed in system headers because such results
+// are significant and actionable.
+#ifdef IS_HEADER
+
+#ifdef IS_SYSHEADER
+#pragma clang system_header
+#endif
 
 extern void doIt(char *);
 
@@ -24,12 +33,17 @@ void frameSizeWarning(int) {}
 
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wframe-larger-than="
-// expected-warning@+1 {{unknown warning group '-Wframe-larger-than'}}
-#pragma GCC diagnostic ignored "-Wframe-larger-than"
 void frameSizeWarningIgnored() {
   char buffer[80];
   doIt(buffer);
 }
+#pragma GCC diagnostic pop
+
+#pragma GCC diagnostic push
+#ifndef IS_SYSHEADER
+// expected-warning@+2 {{unknown warning group '-Wframe-larger-than'}}
+#endif
+#pragma GCC diagnostic ignored "-Wframe-larger-than"
 #pragma GCC diagnostic pop
 
 void frameSizeLocalClassWarning() {
@@ -59,3 +73,9 @@ void frameSizeBlocksWarning() {
   };
   fn();
 }
+
+#else
+
+#define IS_HEADER
+#include __FILE__
+#endif
