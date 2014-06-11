bde66d812dd5677d049b0c3713f6ce27347ea068
Bob Wilson <bob.wilson@apple.com>
2014-06-11 20:19:07 +0000

    Remove dev/null redirect and x86 backend requirement from new test.
    llvm-svn: 210699

--- clang/test/Misc/win32-macho.c
+++ clang/test/Misc/win32-macho.c
@@ -1,3 +1,2 @@
 // Check that basic use of win32-macho targets works.
-// REQUIRES: x86-registered-target
-// RUN: %clang -fsyntax-only -target x86_64-pc-win32-macho %s -o /dev/null
+// RUN: %clang -fsyntax-only -target x86_64-pc-win32-macho %s
