8730ee737517d11554eae525a8a78925ea94e7f4
Rafael Espindola <rafael.espindola@gmail.com>
2014-06-12 19:22:17 +0000

    Try to fix the polly build.
    llvm-svn: 210811

--- polly/lib/Exchange/JSONExporter.cpp
+++ polly/lib/Exchange/JSONExporter.cpp
@@ -20,7 +20,6 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/ToolOutputFile.h"
-#include "llvm/Support/system_error.h"
 
 #include "json/reader.h"
 #include "json/writer.h"
@@ -32,6 +31,7 @@
 
 #include <memory>
 #include <string>
+#include <system_error>
 
 using namespace llvm;
 using namespace polly;
