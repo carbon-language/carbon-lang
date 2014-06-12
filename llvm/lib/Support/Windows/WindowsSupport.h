885719f02731dda9c53eb0a89784bc06918cdad7
Rafael Espindola <rafael.espindola@gmail.com>
2014-06-12 17:49:35 +0000

    Trying to fix the windows build.
    llvm-svn: 210805

--- llvm/lib/Support/Windows/WindowsSupport.h
+++ llvm/lib/Support/Windows/WindowsSupport.h
@@ -163,10 +163,9 @@ c_str(SmallVectorImpl<T> &str) {
 
 namespace sys {
 namespace windows {
-error_code UTF8ToUTF16(StringRef utf8,
-                       SmallVectorImpl<wchar_t> &utf16);
-error_code UTF16ToUTF8(const wchar_t *utf16, size_t utf16_len,
-                       SmallVectorImpl<char> &utf8);
+std::error_code UTF8ToUTF16(StringRef utf8, SmallVectorImpl<wchar_t> &utf16);
+std::error_code UTF16ToUTF8(const wchar_t *utf16, size_t utf16_len,
+                            SmallVectorImpl<char> &utf8);
 } // end namespace windows
 } // end namespace sys
 } // end namespace llvm.
