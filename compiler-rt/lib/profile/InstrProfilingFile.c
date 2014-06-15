9101af0612f473306c7b90625cdaef14bd073bc8
Viktor Kutuzov <vkutuzov@accesssoftek.com>
2014-06-15 14:01:18 +0000

    Fix building InstrProfilingFile.c on FreeBSD
    llvm-svn: 210989

--- compiler-rt/lib/profile/InstrProfilingFile.c
+++ compiler-rt/lib/profile/InstrProfilingFile.c
@@ -8,7 +8,6 @@
 \*===----------------------------------------------------------------------===*/
 
 #include "InstrProfiling.h"
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
