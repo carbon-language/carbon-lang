f3bd08ae63266b6ef47bf0ce1a79037f198719ef
Jeroen Ketema <j.ketema@imperial.ac.uk>
2014-06-16 22:15:50 +0000

    Add remaining float constants
    Reviewed-by: Aaron Watry <awatry@gmail.com>
    llvm-svn: 211062

--- libclc/generic/include/clc/float/definitions.h
+++ libclc/generic/include/clc/float/definitions.h
@@ -9,11 +9,19 @@
 #define FLT_MIN         0x1.0p-126f
 #define FLT_EPSILON     0x1.0p-23f
 
+#define M_E_F           0x1.5bf0a8p+1f
 #define M_LOG2E_F       0x1.715476p+0f
 #define M_LOG10E_F      0x1.bcb7b2p-2f
 #define M_LN2_F         0x1.62e430p-1f
 #define M_LN10_F        0x1.26bb1cp+1f
 #define M_PI_F          0x1.921fb6p+1f
+#define M_PI_2_F        0x1.921fb6p+0f
+#define M_PI_4_F        0x1.921fb6p-1f
+#define M_1_PI_F        0x1.45f306p-2f
+#define M_2_PI_F        0x1.45f306p-1f
+#define M_2_SQRTPI_F    0x1.20dd76p+0f
+#define M_SQRT2_F       0x1.6a09e6p+0f
+#define M_SQRT1_2_F     0x1.6a09e6p-1f
 
 #ifdef cl_khr_fp64
 
