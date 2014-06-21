355597e99e3c8f4c3caafce64d3e316d0a3170c7
David Majnemer <david.majnemer@gmail.com>
2014-06-21 22:49:50 +0000

    The i128 suffix isn't always available.
    This Lexer test unconditionally used the i128 integer literal suffix.
    This suffix is only available to targets that have 128-bit arithmetic
    support.
    
    llvm-svn: 211446

--- clang/test/Lexer/ms-extensions.c
+++ clang/test/Lexer/ms-extensions.c
@@ -6,7 +6,9 @@ __int16 x2 = 4i16;
 __int32 x3 = 5i32;
 __int64 x5 = 0x42i64;
 __int64 x6 = 0x42I64;
+#ifdef __SIZEOF_INT128__
 __int64 x4 = 70000000i128;
+#endif
 
 __int64 y = 0x42i64u;  // expected-error {{invalid suffix}}
 __int64 w = 0x43ui64; 
