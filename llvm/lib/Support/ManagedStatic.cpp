d119fa028ac28ed4ec0bf11d1178bf58dedf72a7
Zachary Turner <zturner@google.com>
2014-06-21 00:24:51 +0000

    Fix the MinGW builder.  Apparently std::call_once and std::recursive_mutex are not available on MinGW and breaks the builder.  Revert to using a function local static and sys::Mutex just to get the tree green until we figure out a better solution.
    llvm-svn: 211424

--- llvm/lib/Support/ManagedStatic.cpp
+++ llvm/lib/Support/ManagedStatic.cpp
@@ -14,33 +14,26 @@
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Config/config.h"
 #include "llvm/Support/Atomic.h"
+#include "llvm/Support/Mutex.h"
+#include "llvm/Support/MutexGuard.h"
 #include <cassert>
-#include <mutex>
 using namespace llvm;
 
 static const ManagedStaticBase *StaticList = nullptr;
 
-// ManagedStatics can get created during execution of static constructors.  As a
-// result, we cannot use a global static std::mutex object for the lock since it
-// may not have been constructed.  Instead, we do a call-once initialization of
-// a pointer to a mutex.
-static std::once_flag MutexInitializationFlag;
-static std::recursive_mutex* ManagedStaticMutex = nullptr;
-
-// Not all supported platforms (in particular VS2012) have thread-safe function
-// static initialization, so roll our own.
-static std::recursive_mutex& GetManagedStaticMutex() {
-  std::call_once(MutexInitializationFlag,
-      []() { ManagedStaticMutex = new std::recursive_mutex(); } );
-
-  return *ManagedStaticMutex;
+static sys::Mutex& getManagedStaticMutex() {
+  // We need to use a function local static here, since this can get called
+  // during a static constructor and we need to guarantee that it's initialized
+  // correctly.
+  static sys::Mutex ManagedStaticMutex;
+  return ManagedStaticMutex;
 }
 
 void ManagedStaticBase::RegisterManagedStatic(void *(*Creator)(),
                                               void (*Deleter)(void*)) const {
   assert(Creator);
   if (llvm_is_multithreaded()) {
-    std::lock_guard<std::recursive_mutex> Lock(GetManagedStaticMutex());
+    MutexGuard Lock(getManagedStaticMutex());
 
     if (!Ptr) {
       void* tmp = Creator();
@@ -90,7 +83,7 @@ void ManagedStaticBase::destroy() const {
 
 /// llvm_shutdown - Deallocate and destroy all ManagedStatic variables.
 void llvm::llvm_shutdown() {
-  std::lock_guard<std::recursive_mutex> Lock(GetManagedStaticMutex());
+  MutexGuard Lock(getManagedStaticMutex());
 
   while (StaticList)
     StaticList->destroy();
