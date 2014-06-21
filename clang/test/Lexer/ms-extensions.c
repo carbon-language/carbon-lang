e492fae27d840c7a3b65b221408d19690d16cc05
Alp Toker <alp@nuanti.com>
2014-06-21 23:32:05 +0000

    Make MS i128 suffix test from r211446 more robust
    We want to catch both negative and positive failure conditions.
    
    llvm-svn: 211449

--- clang/test/Lexer/ms-extensions.c
+++ clang/test/Lexer/ms-extensions.c
@@ -1,14 +1,16 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -fms-extensions %s
 // RUN: %clang_cc1 -fsyntax-only -verify -fms-compatibility %s
+// RUN: %clang_cc1 -fsyntax-only -verify -triple i386-pc-win32 -fms-compatibility %s
 
 __int8 x1  = 3i8;
 __int16 x2 = 4i16;
 __int32 x3 = 5i32;
 __int64 x5 = 0x42i64;
 __int64 x6 = 0x42I64;
-#ifdef __SIZEOF_INT128__
-__int64 x4 = 70000000i128;
+#ifndef __SIZEOF_INT128__
+// expected-error@+2 {{__int128 is not supported on this target}}
 #endif
+__int64 x4 = 70000000i128;
 
 __int64 y = 0x42i64u;  // expected-error {{invalid suffix}}
 __int64 w = 0x43ui64; 
