0f427f5943d78e38efc461e3d3b1bccad1660f97
Rafael Espindola <rafael.espindola@gmail.com>
2014-06-23 15:17:34 +0000

    clang-format initializer to reduce noise in another patch.
    llvm-svn: 211501

--- llvm/include/llvm/MC/MCDwarf.h
+++ llvm/include/llvm/MC/MCDwarf.h
@@ -465,9 +465,10 @@ public:
 
 struct MCDwarfFrameInfo {
   MCDwarfFrameInfo()
-    : Begin(nullptr), End(nullptr), Personality(nullptr), Lsda(nullptr),
-      Function(nullptr), Instructions(), PersonalityEncoding(), LsdaEncoding(0),
-      CompactUnwindEncoding(0), IsSignalFrame(false), IsSimple(false) {}
+      : Begin(nullptr), End(nullptr), Personality(nullptr), Lsda(nullptr),
+        Function(nullptr), Instructions(), PersonalityEncoding(),
+        LsdaEncoding(0), CompactUnwindEncoding(0), IsSignalFrame(false),
+        IsSimple(false) {}
   MCSymbol *Begin;
   MCSymbol *End;
   const MCSymbol *Personality;
