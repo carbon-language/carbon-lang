5e275bc83a76a2c10e836320a5b464778c29784b
Johannes Doerfert <jdoerfert@codeaurora.org>
2014-06-24 17:02:53 +0000

    [Refactor] Create nicer test cases from C/C++
    Insert a header into the new testcase containing a sample RUN line a FIXME and
    an XFAIL. Then insert the formated C code and finally the LLVM-IR without
    attributes, the module ID or the target triple.
    
    llvm-svn: 211612

--- polly/test/create_ll.sh
+++ polly/test/create_ll.sh
@@ -1,8 +1,33 @@
 #!/bin/sh
 
 LLFILE=`echo $1 | sed -e 's/\.c/.ll/g'`
+LLFILE_TMP=${LLFILE}.tmp
+
+# The number of lines to cut of the LLVM-IR file clang produces.
+CUT_N_LINES=9
 
 clang -c -S -emit-llvm -O0 $1 -o ${LLFILE}
 
 opt -correlated-propagation -mem2reg -instcombine -loop-simplify -indvars \
--instnamer ${LLFILE} -S -o ${LLFILE}
+-instnamer ${LLFILE} -S -o ${LLFILE_TMP}
+
+# Insert a header into the new testcase containing a sample RUN line a FIXME and
+# an XFAIL. Then insert the formated C code and finally the LLVM-IR without
+# attributes, the module ID or the target triple.
+echo '; RUN: opt %loadPolly -analyze < %s | FileCheck %s' > ${LLFILE}
+echo ';' >> ${LLFILE}
+echo '; FIXME: Edit the run line and add checks!' >> ${LLFILE}
+echo ';' >> ${LLFILE}
+echo '; XFAIL: *' >> ${LLFILE}
+echo ';' >> ${LLFILE}
+clang-format $1 | sed -e 's/^/;    /' >> ${LLFILE}
+echo ';' >> ${LLFILE}
+
+cat ${LLFILE_TMP} | sed -e 's/ \#0//' >> ${LLFILE}
+sed -i".tmp" '/; Function Attrs:/d' ${LLFILE}
+sed -i".tmp" '/; ModuleID =/d' ${LLFILE}
+sed -i".tmp" '/target triple/d' ${LLFILE}
+
+head --lines=-${CUT_N_LINES} ${LLFILE} > ${LLFILE_TMP}
+
+mv ${LLFILE_TMP} ${LLFILE}
