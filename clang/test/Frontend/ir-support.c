830c34e269a6823719040e9db11b8f8d665f7a84
Alp Toker <alp@nuanti.com>
2014-06-27 02:59:39 +0000

    Restore test from r211844 skipping '.file' directives
    At least this answers the question of whether .bc/.ll input processed by the
    frontend produces identical output to the original compilation.
    
    llvm-svn: 211853

--- clang/test/Frontend/ir-support.c
+++ clang/test/Frontend/ir-support.c
@@ -1,19 +1,19 @@
 // Test that we can consume LLVM IR/bitcode in the frontend and produce
-// identical output to a standard compilation.
+// equivalent output to a standard compilation.
 
-// FIXME: line directives don't match the reference output on ELF so we can't compare.
+// We strip differing '.file' directives before comparing.
 
 // Reference output:
-// RUN: %clang_cc1 -S -o %t.s %s
+// RUN: %clang_cc1 -S -o - %s | grep -v '\t\.file' > %t.s
 
 // LLVM bitcode:
 // RUN: %clang_cc1 -emit-llvm-bc -o %t.bc %s
-// RUN: %clang_cc1 -S -o - %t.bc > %t.bc.s
-// RUN-FIXME: diff %t.s %t.bc.s
+// RUN: %clang_cc1 -S -o - %t.bc | grep -v '\t\.file' > %t.bc.s
+// RUN: diff %t.s %t.bc.s
 
 // LLVM IR source code:
-// RUN: %clang_cc1 -emit-llvm-bc -o %t.ll %s
-// RUN: %clang_cc1 -S -o - %t.ll > %t.ll.s
-// RUN-FIXME: diff %t.s %t.ll.s
+// RUN: %clang_cc1 -emit-llvm -o %t.ll %s
+// RUN: %clang_cc1 -S -o - %t.ll | grep -v '\t\.file' > %t.ll.s
+// RUN: diff %t.s %t.ll.s
 
 int f() { return 0; }
