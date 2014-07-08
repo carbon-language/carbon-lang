a054343415ab516a98ae9fa4fc4fe4fb4fdb14ff
Benjamin Kramer <benny.kra@googlemail.com>
2014-07-08 16:42:54 +0000

    As funny as it might look, this wasn't what I intended to test.
    llvm-svn: 212549

--- clang-tools-extra/test/clang-tidy/llvm-twine-local.cpp
+++ clang-tools-extra/test/clang-tidy/llvm-twine-local.cpp
@@ -15,19 +15,19 @@ using namespace llvm;
 void foo(const Twine &x);
 
 static Twine Moo = Twine("bark") + "bah";
-// CHECK-MASSAGES: twine variables are prone to use-after-free bugs
+// CHECK-MESSAGES: twine variables are prone to use-after-free bugs
 // CHECK-MESSAGES: note: FIX-IT applied suggested code changes
 // CHECK: static std::string Moo = (Twine("bark") + "bah").str();
 
 int main() {
   const Twine t = Twine("a") + "b" + Twine(42);
-// CHECK-MASSAGES: twine variables are prone to use-after-free bugs
+// CHECK-MESSAGES: twine variables are prone to use-after-free bugs
 // CHECK-MESSAGES: note: FIX-IT applied suggested code changes
 // CHECK: std::string t = (Twine("a") + "b" + Twine(42)).str();
   foo(Twine("a") + "b");
 
   Twine Prefix = false ? "__INT_FAST" : "__UINT_FAST";
-// CHECK-MASSAGES: twine variables are prone to use-after-free bugs
+// CHECK-MESSAGES: twine variables are prone to use-after-free bugs
 // CHECK-MESSAGES: note: FIX-IT applied suggested code changes
 // CHECK: const char * Prefix = false ? "__INT_FAST" : "__UINT_FAST";
 }
