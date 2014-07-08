926731b1e5a5e9240f818d371cc4259d2bed419a
Marshall Clow <mclow.lists@gmail.com>
2014-07-08 22:38:11 +0000

    string_view enhancements. Move to the correct namespace. Better constexpr support (thanks to Richard for the suggestions). Update the tests to match this. Add <experimental/__config for experimental macros/etc to live.
    llvm-svn: 212569

--- libcxx/test/experimental/string.view/string.view.access/at.pass.cpp
+++ libcxx/test/experimental/string.view/string.view.access/at.pass.cpp
@@ -43,7 +43,7 @@ int main () {
     test ( U"a", 1 );
 #endif
 
-#if _LIBCPP_STD_VER > 11
+#if __cplusplus >= 201103L
     {
     constexpr std::experimental::basic_string_view<char> sv ( "ABC", 2 );
     static_assert ( sv.length() ==  2,  "" );
