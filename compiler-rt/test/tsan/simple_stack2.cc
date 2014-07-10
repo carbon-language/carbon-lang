89360191c8caa0fe5524e0db1d6de29f53221c81
Diego Novillo <dnovillo@google.com>
2014-07-10 23:29:10 +0000

    Fix expected column numbers in two tests.
    This is a minor fix to two tsan tests that were expecting the wrong
    column information. Now that clang emits column information by default
    in its debugging output, the tests had started failing.
    
    llvm-svn: 212779

--- compiler-rt/test/tsan/simple_stack2.cc
+++ compiler-rt/test/tsan/simple_stack2.cc
@@ -48,6 +48,6 @@ int main() {
 // CHECK-NEXT:     #1 bar1{{.*}} {{.*}}simple_stack2.cc:16{{(:3)?}} ({{.*}})
 // CHECK-NEXT:     #2 Thread1{{.*}} {{.*}}simple_stack2.cc:34{{(:3)?}} ({{.*}})
 // CHECK:        Previous read of size 4 at {{.*}} by main thread:
-// CHECK-NEXT:     #0 foo2{{.*}} {{.*}}simple_stack2.cc:20{{(:28)?}} ({{.*}})
+// CHECK-NEXT:     #0 foo2{{.*}} {{.*}}simple_stack2.cc:20{{(:3)?}} ({{.*}})
 // CHECK-NEXT:     #1 bar2{{.*}} {{.*}}simple_stack2.cc:29{{(:3)?}} ({{.*}})
 // CHECK-NEXT:     #2 main{{.*}} {{.*}}simple_stack2.cc:41{{(:3)?}} ({{.*}})
