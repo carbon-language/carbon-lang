fb9756131e84cdab56f411d25700a01888925bad
Greg Clayton <gclayton@apple.com>
2014-07-11 00:51:53 +0000

    Remove code that was merged incorrectly.
    llvm-svn: 212791

--- lldb/source/Expression/IRExecutionUnit.cpp
+++ lldb/source/Expression/IRExecutionUnit.cpp
@@ -318,8 +318,6 @@ IRExecutionUnit::GetRunnableInfo(Error &error,
                                                                mCPU,
                                                                mAttrs);
 
-    m_failed_lookups.clear();
-
     m_execution_engine_ap.reset(builder.create(target_machine));
 
     if (!m_execution_engine_ap.get())
