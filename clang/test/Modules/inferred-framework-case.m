2474e816cc0242d44dae2e7f8f7836f7b3c9adc9
Ben Langmuir <blangmuir@apple.com>
2014-07-14 20:36:40 +0000

    XFAIL test that doesn't work on Windows yet
    llvm-svn: 212980

--- clang/test/Modules/inferred-framework-case.m
+++ clang/test/Modules/inferred-framework-case.m
@@ -1,5 +1,7 @@
 // RUN: rm -rf %t
 // RUN: %clang_cc1 -fmodules-cache-path=%t -fmodules -F %S/Inputs %s -verify -DA
+// FIXME: getCanonicalName() is not implemented on Windows.
+// XFAIL: win32
 
 @import MOdule; // expected-error{{module 'MOdule' not found}}
 @import Module;
