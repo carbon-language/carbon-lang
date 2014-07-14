ba5bd0bed68a79dbc40851a72473c8054d3e6278
Ben Langmuir <blangmuir@apple.com>
2014-07-14 21:14:28 +0000

    Try harder to supress the test from r212975 on Windows
    The attempt in r212980 was broken because we might not fail if
    LLVM_ON_UNIX is enabled for cross compiling to Windows, and it didn't
    consider mingw either.
    
    llvm-svn: 212989

--- clang/test/Modules/inferred-framework-case.m
+++ clang/test/Modules/inferred-framework-case.m
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t
 // RUN: %clang_cc1 -fmodules-cache-path=%t -fmodules -F %S/Inputs %s -verify -DA
-// FIXME: getCanonicalName() is not implemented on Windows.
-// XFAIL: win32
+// FIXME: PR20299 - getCanonicalName() is not implemented on Windows.
+// REQUIRES: shell
 
 @import MOdule; // expected-error{{module 'MOdule' not found}}
 @import Module;
