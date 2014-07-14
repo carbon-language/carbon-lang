d73e7cc8793c8501592161daa9ff307abc432155
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-07-14 13:07:51 +0000

    [sanitizer] Intercept getpass.
    llvm-svn: 212937

---
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/getpass.cc
@@ -0,0 +1,33 @@
+// RUN: %clangxx -O0 -g %s -lutil -o %t && %run %t | FileCheck %s
+#include <assert.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <pty.h>
+
+int
+main (int argc, char** argv)
+{
+    int master;
+    int pid = forkpty(&master, NULL, NULL, NULL);
+
+    if(pid == -1) {
+      fprintf(stderr, "forkpty failed\n");
+      return 1;
+    } else if (pid > 0) {
+      char buf[1024];
+      int res = read(master, buf, sizeof(buf));
+      write(1, buf, res);
+      write(master, "password\n", 9);
+      res = read(master, buf, sizeof(buf));
+      write(1, buf, res);
+    } else {
+      char *s = getpass("prompt");
+      assert(strcmp(s, "password") == 0);
+      write(1, "done\n", 5);
+    }
+    return 0;
+}
+
+// CHECK: prompt
+// CHECK: done
