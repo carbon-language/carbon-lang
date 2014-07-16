4a9638324ab920ae46091b1317ebf0fe2d3a4747
Viktor Kutuzov <vkutuzov@accesssoftek.com>
2014-07-16 10:14:01 +0000

    Add FreeBSD support to the address sanitizer's assign_large_valloc_to_global.cc test case Differential Revision: http://reviews.llvm.org/D4525
    llvm-svn: 213135

--- compiler-rt/test/asan/TestCases/Posix/assign_large_valloc_to_global.cc
+++ compiler-rt/test/asan/TestCases/Posix/assign_large_valloc_to_global.cc
@@ -1,8 +1,9 @@
 // Make sure we don't report a leak nor hang.
 // RUN: %clangxx_asan -O3 %s -o %t && %run %t
 #include <stdlib.h>
-#ifndef __APPLE__
+#include <unistd.h>
+#if !defined(__APPLE__) && !defined(__FreeBSD__)
 # include <malloc.h>
-#endif  // __APPLE__
+#endif  // !__APPLE__ && !__FreeBSD__
 int *p = (int*)valloc(1 << 20);
 int main() { }
