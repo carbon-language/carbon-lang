7433281b5e4f907d0ade86a8e0a1a42591cd61ff
Hans Wennborg <hans@hanshq.net>
2014-07-16 00:55:31 +0000

    Add a test for wildcard expansion on Windows
    This depends on LLVM r213114
    
    llvm-svn: 213115

---
+++ clang/test/Driver/windows-wildcard-expansion.c
@@ -0,0 +1,6 @@
+// Clang does wildcard expansion on Windows. On other OSs, it's done by the shell.
+// REQUIRES: system-windows
+
+// RUN: %clang -c -### %S/Inputs/wildcard*.c 2>&1 | FileCheck %s
+// CHECK: wildcard1.c
+// CHECK: wildcard2.c
