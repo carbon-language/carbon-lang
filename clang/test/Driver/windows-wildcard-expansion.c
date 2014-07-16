a6c062b283ee937b0dc04c358e0ba01fc0d358fc
Hans Wennborg <hans@hanshq.net>
2014-07-16 16:14:09 +0000

    Expand the wildcard expansion test to also cover '?'
    llvm-svn: 213165

--- clang/test/Driver/windows-wildcard-expansion.c
+++ clang/test/Driver/windows-wildcard-expansion.c
@@ -2,5 +2,6 @@
 // REQUIRES: system-windows
 
 // RUN: %clang -c -### %S/Inputs/wildcard*.c 2>&1 | FileCheck %s
+// RUN: %clang -c -### %S/Inputs/wildcard?.c 2>&1 | FileCheck %s
 // CHECK: wildcard1.c
 // CHECK: wildcard2.c
