30102536c0712ef8e4220d665516bf9c1313a197
Aaron Watry <awatry@gmail.com>
2014-07-17 22:07:27 +0000

    relational: Implement isgreaterequal
    v2: Use relational macros instead of hand-rolled macros
    
    Signed-off-by: Aaron Watry <awatry@gmail.com>
    Reviewed-by: Tom Stellard <thomas.stellard@amd.com>
    llvm-svn: 213319

--- libclc/generic/include/clc/clc.h
+++ libclc/generic/include/clc/clc.h
@@ -111,6 +111,7 @@
 #include <clc/relational/bitselect.h>
 #include <clc/relational/isequal.h>
 #include <clc/relational/isgreater.h>
+#include <clc/relational/isgreaterequal.h>
 #include <clc/relational/isnan.h>
 #include <clc/relational/select.h>
 #include <clc/relational/signbit.h>
