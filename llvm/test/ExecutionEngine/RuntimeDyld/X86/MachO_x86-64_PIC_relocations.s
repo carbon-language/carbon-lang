0a6af4391bbb8d52cb4393c2ad29ae0a4694c5e1
NAKAMURA Takumi <geek4civic@gmail.com>
2014-07-23 04:32:21 +0000

    Rework to let RuntimeDyld/X86/MachO_x86-64_PIC_relocations.s pass on win32.
    FIXME: "llvm-rtdyld -verify -check" is still sensitive to path separator.
    Fix searching StubMap to be tolerant of both '/' and '\\' on Win32.
    
    llvm-svn: 213723

--- llvm/test/ExecutionEngine/RuntimeDyld/X86/MachO_x86-64_PIC_relocations.s
+++ llvm/test/ExecutionEngine/RuntimeDyld/X86/MachO_x86-64_PIC_relocations.s
@@ -1,9 +1,6 @@
 # RUN: llvm-mc -triple=x86_64-apple-macosx10.9 -relocation-model=pic -filetype=obj -o %T/foo.o %s
 # RUN: sed "s,<filename>,%/T/foo.o,g" %s > %T/foo.s
-# RUN: llvm-rtdyld -triple=x86_64-apple-macosx10.9 -verify -check=%T/foo.s %T/foo.o
-
-# FIXME: Fix searching StubMap with '/' and '\\' on Win32.
-# REQUIRES: shell
+# RUN: llvm-rtdyld -triple=x86_64-apple-macosx10.9 -verify -check=%T/foo.s %/T/foo.o
 
         .section	__TEXT,__text,regular,pure_instructions
 	.globl	foo
