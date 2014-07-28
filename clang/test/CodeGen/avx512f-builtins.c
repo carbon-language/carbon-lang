062ba618f5fc9414c2740517485d264372e12c05
Adam Nemet <anemet@apple.com>
2014-07-28 17:14:36 +0000

    [AVX512] Add CHECK-LABELs to test/CodeGen/avx512f-builtins.c
    llvm-svn: 214095

--- clang/test/CodeGen/avx512f-builtins.c
+++ clang/test/CodeGen/avx512f-builtins.c
@@ -7,24 +7,28 @@
 
 __m512d test_mm512_sqrt_pd(__m512d a)
 {
+  // CHECK-LABEL: @test_mm512_sqrt_pd
   // CHECK: @llvm.x86.avx512.sqrt.pd.512
   return _mm512_sqrt_pd(a);
 }
 
 __m512 test_mm512_sqrt_ps(__m512 a)
 {
+  // CHECK-LABEL: @test_mm512_sqrt_ps
   // CHECK: @llvm.x86.avx512.sqrt.ps.512
   return _mm512_sqrt_ps(a);
 }
 
 __m512d test_mm512_rsqrt14_pd(__m512d a)
 {
+  // CHECK-LABEL: @test_mm512_rsqrt14_pd
   // CHECK: @llvm.x86.avx512.rsqrt14.pd.512
   return _mm512_rsqrt14_pd(a);
 }
 
 __m512 test_mm512_rsqrt14_ps(__m512 a)
 {
+  // CHECK-LABEL: @test_mm512_rsqrt14_ps
   // CHECK: @llvm.x86.avx512.rsqrt14.ps.512
   return _mm512_rsqrt14_ps(a);
 }
