99ef10f9156e818ca85da2421f8b3cb776afba87
Joerg Sonnenberger <joerg@bec.de>
2014-07-29 23:16:31 +0000

    Recognize BookE's mbar instruction.
    llvm-svn: 214244

--- llvm/test/MC/PowerPC/ppc64-encoding-bookII.s
+++ llvm/test/MC/PowerPC/ppc64-encoding-bookII.s
@@ -53,6 +53,9 @@
 # CHECK-BE: wait 2                          # encoding: [0x7c,0x40,0x00,0x7c]
 # CHECK-LE: wait 2                          # encoding: [0x7c,0x00,0x40,0x7c]
             wait 2
+# CHECK-BE: mbar 1                          # encoding: [0x7c,0x20,0x06,0xac]
+# CHECK-LE: mbar 1                          # encoding: [0xac,0x06,0x20,0x7c]
+            mbar 1
 
 # Extended mnemonics
 
