e098b3b993892e2385ec7aa0ae293b598ead413d
NAKAMURA Takumi <geek4civic@gmail.com>
2014-07-30 10:44:35 +0000

    Suppress clang/test/Sema/struct-packed-align.c for targeting LLP64.
    llvm-svn: 214298

--- clang/test/Sema/struct-packed-align.c
+++ clang/test/Sema/struct-packed-align.c
@@ -120,6 +120,7 @@ extern int m1[sizeof(struct packed_fas2) == 1 ? 1 : -1];
 extern int m2[__alignof(struct packed_fas2) == 1 ? 1 : -1];
 
 // Attribute aligned can round down typedefs.  PR9253
+// REQUIRES: LP64
 typedef long long  __attribute__((aligned(1))) nt;
 
 struct nS {
