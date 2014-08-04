89872e99a51761419440e16c839f4e8b6b4e6fce
Marshall Clow <mclow.lists@gmail.com>
2014-08-04 19:20:17 +0000

    Fix a problem with reference_wrapper in C++03 that was causing counting predicates to fail. Add a test to make sure it works. However, most of the reference_wrapper tests still fail in C++03 mode, due to a lack of decltype. No change there.
    llvm-svn: 214760

--- libcxx/test/utilities/function.objects/refwrap/refwrap.helpers/ref_2.pass.cpp
+++ libcxx/test/utilities/function.objects/refwrap/refwrap.helpers/ref_2.pass.cpp
@@ -16,10 +16,28 @@
 #include <functional>
 #include <cassert>
 
+#include "counting_predicates.hpp"
+
+bool is5 ( int i ) { return i == 5; }
+
+template <typename T>
+bool call_pred ( T pred ) { return pred(5); }
+
 int main()
 {
+    {
     int i = 0;
     std::reference_wrapper<int> r1 = std::ref(i);
     std::reference_wrapper<int> r2 = std::ref(r1);
     assert(&r2.get() == &i);
+    }
+    {
+    unary_counting_predicate<bool(*)(int), int> cp(is5);
+    assert(!cp(6));
+    assert(cp.count() == 1);
+	assert(call_pred(cp));
+    assert(cp.count() == 1);
+	assert(call_pred(std::ref(cp)));
+    assert(cp.count() == 2);
+	}
 }
