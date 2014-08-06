6fd227c9ad44582dee3c096ea6a1e1c429e8d253
Benjamin Kramer <benny.kra@googlemail.com>
2014-08-06 08:19:30 +0000

    [clang-tidy] forward arguments of the testing script to clang-tidy.
    llvm-svn: 214952

--- clang-tools-extra/test/clang-tidy/check_clang_tidy_fix.sh
+++ clang-tools-extra/test/clang-tidy/check_clang_tidy_fix.sh
@@ -5,6 +5,7 @@
 INPUT_FILE=$1
 CHECK_TO_RUN=$2
 TEMPORARY_FILE=$3.cpp
+shift 3
 
 # Remove the contents of the CHECK lines to avoid CHECKs matching on themselves.
 # We need to keep the comments to preserve line numbers while avoiding empty
@@ -12,7 +13,7 @@ TEMPORARY_FILE=$3.cpp
 sed 's#// *[A-Z-]\+:.*#//#' ${INPUT_FILE} > ${TEMPORARY_FILE}
 
 clang-tidy ${TEMPORARY_FILE} -fix --checks="-*,${CHECK_TO_RUN}" -- --std=c++11 \
-  > ${TEMPORARY_FILE}.msg 2>&1 || exit $?
+  $* > ${TEMPORARY_FILE}.msg 2>&1 || exit $?
 
 FileCheck -input-file=${TEMPORARY_FILE} ${INPUT_FILE} \
   -check-prefix=CHECK-FIXES -strict-whitespace || exit $?
