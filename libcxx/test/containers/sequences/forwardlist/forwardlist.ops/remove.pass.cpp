99d2df956d4e7a643f2884b057178139e3a5da3c
Marshall Clow <mclow.lists@gmail.com>
2014-08-08 15:58:00 +0000

    Apply a similar fix to <forward_list> as I did for <list> in r215210. Again, thanks to Ion Gaztañaga for noticing this problem w.r.t LWG#526
    llvm-svn: 215213

--- libcxx/test/containers/sequences/forwardlist/forwardlist.ops/remove.pass.cpp
+++ libcxx/test/containers/sequences/forwardlist/forwardlist.ops/remove.pass.cpp
@@ -17,6 +17,17 @@
 
 #include "min_allocator.h"
 
+struct S {
+	S(int i) : i_(new int(i)) {}
+	S(const S &rhs) : i_(new int(*rhs.i_)) {}
+	S& operator = (const S &rhs) { *i_ = *rhs.i_; return *this; }
+	~S () { delete i_; i_ = NULL; }
+	bool operator == (const S &rhs) const { return *i_ == *rhs.i_; }
+	int get () const { return *i_; }
+	int *i_;
+	};
+
+
 int main()
 {
     {
@@ -65,6 +76,32 @@ int main()
         C c2(std::begin(t2), std::end(t2));
         c1.remove(0);
         assert(c1 == c2);
+    }
+    {  // LWG issue #526
+    typedef int T;
+    typedef std::forward_list<T> C;
+    int t1[] = {1, 2, 1, 3, 5, 8, 11};
+    int t2[] = {   2,    3, 5, 8, 11};
+    C c1(std::begin(t1), std::end(t1));
+    C c2(std::begin(t2), std::end(t2));
+    c1.remove(c1.front());
+    assert(c1 == c2);
+    }
+    {
+    typedef S T;
+    typedef std::forward_list<T> C;
+    int t1[] = {1, 2, 1, 3, 5, 8, 11, 1};
+    int t2[] = {   2,    3, 5, 8, 11   };
+    C c;
+    for(int *ip = std::end(t1); ip != std::begin(t1);)
+        c.push_front(S(*--ip));
+    c.remove(c.front());
+    C::const_iterator it = c.begin();
+    for(int *ip = std::begin(t2); ip != std::end(t2); ++ip, ++it) {
+	    assert ( it != c.end());
+        assert ( *ip == it->get());
+	    }
+	assert ( it == c.end ());
     }
 #if __cplusplus >= 201103L
     {
