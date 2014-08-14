e8d2a9d755c22ffd3c50e2520963bc8dbe049ba2
DeLesley Hutchins <delesley@google.com>
2014-08-14 21:54:34 +0000

    Thread Safety Analysis:  Move -Wthread-safety-negative out of the -Wthread-safety umbrella flag, pending updates to documentation.  The flag works, but is likely to be confusing to existing users of -Wthread-safety.
    llvm-svn: 215679

--- clang/test/SemaCXX/warn-thread-safety-negative.cpp
+++ clang/test/SemaCXX/warn-thread-safety-negative.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -fsyntax-only -verify -std=c++11 -Wthread-safety -Wthread-safety-beta -fcxx-exceptions %s
+// RUN: %clang_cc1 -fsyntax-only -verify -std=c++11 -Wthread-safety -Wthread-safety-beta -Wthread-safety-negative -fcxx-exceptions %s
 
 // FIXME: should also run  %clang_cc1 -fsyntax-only -verify -Wthread-safety -std=c++11 -Wc++98-compat %s
 // FIXME: should also run  %clang_cc1 -fsyntax-only -verify -Wthread-safety %s
