69d2ad031b5d4244f0fe35493f82e0c51296dc32
Rafael Espindola <rafael.espindola@gmail.com>
2014-08-17 16:22:16 +0000

    Add a test for -no-canonical-prefixes.
    llvm-svn: 215836

---
+++ clang/test/Driver/no-canonical-prefixes.c
@@ -0,0 +1,10 @@
+// REQUIRES: shell
+// RUN: mkdir -p %t
+// RUN: cd %t
+// RUN: ln -sf %clang test-clang
+// RUN: ./test-clang -v -S %s 2>&1 | FileCheck %s
+// RUN: ./test-clang -v -S %s -no-canonical-prefixes 2>&1 | FileCheck --check-prefix=NCP %s
+
+
+// CHECK: /bin/clang{{.*}}" -cc1
+// NCP: test-clang" -cc1
