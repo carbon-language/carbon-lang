63f9b571476b7afbb794296fc70c7fb726809dd0
Dylan Noblesmith <nobled@dreamwidth.org>
2014-08-23 21:10:58 +0000

    cmake: actually test -Wcomment
    This test was testing nothing, as only -Werror was ever
    being added to the compiler flags.
    
    You can see the final nitty-gritty compiler invocation in
    CMakeFiles/CMakeOutput.log (for successful tests) and
    CMakeFiles/CMakeError.log (for failed tests).
    
    Before:
    Building C object CMakeFiles/cmTryCompileExec3385359576.dir/src.c.o
    /usr/bin/clang   -fPIC -Wall -W -Wno-unused-parameter -Wwrite-strings -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default  -DC_WCOMMENT_ALLOWS_LINE_WRAP  -Werror   -o CMakeFiles/cmTryCompileExec3385359576.dir/src.c.o   -c /home/nobled/code/llvm-b9/CMakeFiles/CMakeTmp/src.c
    
    After:
    Building C object CMakeFiles/cmTryCompileExec3385359576.dir/src.c.o
    /usr/bin/clang   -fPIC -Wall -W -Wno-unused-parameter -Wwrite-strings -Wmissing-field-initializers -pedantic -Wno-long-long -Wcovered-switch-default  -DC_WCOMMENT_ALLOWS_LINE_WRAP  -Werror -Wcomment   -o CMakeFiles/cmTryCompileExec3385359576.dir/src.c.o   -c /home/nobled/code/llvm-b9/CMakeFiles/CMakeTmp/src.c
    
    llvm-svn: 216328

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -306,7 +306,7 @@ elseif( LLVM_COMPILER_IS_GCC_COMPATIBLE )
     # Check if -Wcomment is OK with an // comment ending with '\' if the next
     # line is also a // comment.
     set(OLD_CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS})
-    set(CMAKE_REQUIRED_FLAGS ${CMAKE_REQUIRED_FLAGS} -Werror -Wcomment)
+    set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -Werror -Wcomment")
     CHECK_C_SOURCE_COMPILES("// \\\\\\n//\\nint main() {return 0;}"
                             C_WCOMMENT_ALLOWS_LINE_WRAP)
     set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
