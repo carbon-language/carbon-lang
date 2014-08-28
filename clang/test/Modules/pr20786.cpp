6a5ca27af553af6659dbbce9f56e9f5d40fa9fc6
Richard Smith <richard-llvm@metafoo.co.uk>
2014-08-28 21:09:23 +0000

    Oops, use correct RUN: line for test.
    llvm-svn: 216685

--- clang/test/Modules/pr20786.cpp
+++ clang/test/Modules/pr20786.cpp
@@ -1,2 +1,2 @@
-// RUN: rm -rf %T
-// RUN: %clang_cc1 -fmodules -fmodules-cache-path=%T -fmodule-name=TBranchProxy -emit-module -x c++ %S/Inputs/PR20786/module.modulemap
+// RUN: rm -rf %t
+// RUN: %clang_cc1 -fmodules -fmodules-cache-path=%t -fmodule-name=TBranchProxy -emit-module -x c++ %S/Inputs/PR20786/module.modulemap
