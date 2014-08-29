ae08e431ddc1d981afa9a16a3e2041a6213846d8
Anna Zaks <ganna@apple.com>
2014-08-29 21:51:22 +0000

    Fixup for r216763. Add a driver test.
    llvm-svn: 216783

--- clang/test/Analysis/disable-all-checks.c
+++ clang/test/Analysis/disable-all-checks.c
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -analyze -analyzer-checker=core -analyzer-store=region -analyzer-disable-all-checks -verify %s
 // RUN: %clang_cc1 -analyze -analyzer-disable-all-checks -analyzer-checker=core -analyzer-store=region -verify %s
+// RUN: %clang --analyze -Xanalyzer -analyzer-disable-all-checks -Xclang -verify %s
 // RUN: not %clang_cc1 -analyze -analyzer-checker=core -analyzer-store=region -analyzer-disable-checker -verify %s 2>&1 | FileCheck %s
 // expected-no-diagnostics
 
