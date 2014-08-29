cdbe51b543509777cf457ab0e33e59ec1383c872
David Blaikie <dblaikie@gmail.com>
2014-08-29 22:05:31 +0000

    Add a trivial functor for use with unique_ptrs managing memory that needs to be freed rather than deleted.
    llvm-svn: 216790

--- llvm/include/llvm/ADT/STLExtras.h
+++ llvm/include/llvm/ADT/STLExtras.h
@@ -530,6 +530,12 @@ make_unique(size_t n) {
 
 #endif
 
+struct FreeDeleter {
+  void operator()(void* v) {
+    ::free(v);
+  }
+};
+
 template<typename First, typename Second>
 struct pair_hash {
   size_t operator()(const std::pair<First, Second> &P) const {
