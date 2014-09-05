0cc371c166df8d0f1afb3fa24e934e049796dbd1
Todd Fiala <todd.fiala@gmail.com>
2014-09-05 14:56:13 +0000

    Set the process vendor in GDBRemoteCommunicationClient::GetCurrentProcessInfo.
    Change by Stephane Sezer.
    
    llvm-svn: 217252

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
@@ -2501,6 +2501,7 @@ GDBRemoteCommunicationClient::GetCurrentProcessInfo ()
                 {
                     assert (pointer_byte_size == m_process_arch.GetAddressByteSize());
                 }
+                m_process_arch.GetTriple().setVendorName (llvm::StringRef (vendor_name));
                 m_process_arch.GetTriple().setOSName(llvm::StringRef (os_name));
                 m_host_arch.GetTriple().setVendorName (llvm::StringRef (vendor_name));
                 m_host_arch.GetTriple().setOSName (llvm::StringRef (os_name));
