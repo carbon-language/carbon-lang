869342c1cb5d30b117467b1504aac6c45f14c991
Alexander Potapenko <glider@google.com>
2014-09-05 06:26:08 +0000

    [ASan] disable zero_page_pc.cc on Android. Bug: https://code.google.com/p/address-sanitizer/issues/detail?id=336
    llvm-svn: 217228

--- compiler-rt/test/asan/TestCases/zero_page_pc.cc
+++ compiler-rt/test/asan/TestCases/zero_page_pc.cc
@@ -1,6 +1,9 @@
 // Check that ASan correctly detects SEGV on the zero page.
 // RUN: %clangxx_asan %s -o %t && not %run %t 2>&1 | FileCheck %s
 
+// https://code.google.com/p/address-sanitizer/issues/detail?id=336
+// XFAIL: android
+
 typedef void void_f();
 int main() {
   void_f *func = (void_f *)0x7;
