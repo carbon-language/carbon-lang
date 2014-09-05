b582372e87a7a9821c0ddfd472ab3d651e4ae04e
Rafael Espindola <rafael.espindola@gmail.com>
2014-09-05 18:03:38 +0000

    Revert "Disable the fix for pr20793 because of a gnu ld bug."
    This reverts commit r217211.
    
    Both the bfd ld and gold outputs were valid. They were using a Rela relocation,
    so the value present in the relocated location was not used, which caused me
    to misread the output.
    
    llvm-svn: 217264

--- llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
+++ llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
@@ -31,7 +31,6 @@
 #include "llvm/MC/MCSectionMachO.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/MC/MCSymbol.h"
-#include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Dwarf.h"
 #include "llvm/Support/ELF.h"
 #include "llvm/Support/ErrorHandling.h"
@@ -41,13 +40,6 @@
 #include "llvm/Target/TargetSubtargetInfo.h"
 using namespace llvm;
 using namespace dwarf;
-
-// Disabled by default because it hits bug 17350 in GNU ld (gold is fine)
-static cl::opt<bool>
-    EnableStructorCOMDAT("enable-structor-comdat", cl::Hidden,
-                         cl::desc("Use comdats to keep only one copy of a "
-                                  "constructor/destructor invocation"),
-                         cl::init(false));
 
 //===----------------------------------------------------------------------===//
 //                                  ELF
@@ -371,9 +363,6 @@ static const MCSectionELF *getStaticStructorSection(MCContext &Ctx,
                                                     bool IsCtor,
                                                     unsigned Priority,
                                                     const MCSymbol *KeySym) {
-  if (!EnableStructorCOMDAT)
-    KeySym = nullptr;
-
   std::string Name;
   unsigned Type;
   unsigned Flags = ELF::SHF_ALLOC | ELF::SHF_WRITE;
