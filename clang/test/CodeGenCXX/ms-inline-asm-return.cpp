3fe67c9fc1c99f56df98c3e3a53dddd4223ed4d2
Reid Kleckner <reid@kleckner.net>
2014-09-05 20:06:06 +0000

    Require an x86 target for ms-inline-asm-return.cpp instead of XFAIL
    llvm-svn: 217272

--- clang/test/CodeGenCXX/ms-inline-asm-return.cpp
+++ clang/test/CodeGenCXX/ms-inline-asm-return.cpp
@@ -1,5 +1,5 @@
+// REQUIRES: x86-registered-target
 // RUN: %clang_cc1 %s -triple i686-pc-windows-msvc -emit-llvm -o - -fasm-blocks | FileCheck %s
-// XFAIL: arm
 
 // Check that we take EAX or EAX:EDX and return it from these functions for MSVC
 // compatibility.
