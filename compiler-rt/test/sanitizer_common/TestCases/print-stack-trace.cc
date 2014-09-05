62063b46fcbe811726432dd897e9b7f46f84eb44
Alexey Samsonov <vonosmas@gmail.com>
2014-09-05 22:05:32 +0000

    Use "-gline-tables-only" instead of "-g" in sanitizer test suites.
    We are interested in verifying that -gline-tables-only provides enough
    debug information for verbose error reports and symbolized stack traces.
    
    llvm-svn: 217284

--- compiler-rt/test/sanitizer_common/TestCases/print-stack-trace.cc
+++ compiler-rt/test/sanitizer_common/TestCases/print-stack-trace.cc
@@ -16,5 +16,5 @@ int main() {
   return 0;
 }
 // CHECK: {{    #0 0x.* in __sanitizer_print_stack_trace}}
-// CHECK: {{    #1 0x.* in FooBarBaz\(\) .*print-stack-trace.cc:11}}
+// CHECK: {{    #1 0x.* in FooBarBaz(\(\))? .*print-stack-trace.cc:11}}
 // CHECK: {{    #2 0x.* in main.*print-stack-trace.cc:15}}
