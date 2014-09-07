34cd2fa339b8ddca105b2062bb44589902573c7b
David Blaikie <dblaikie@gmail.com>
2014-09-07 16:59:13 +0000

    Fix test to not write output to the test directory, as it may not be writable.
    llvm-svn: 217337

--- clang/test/Modules/warn-unused-local-typedef.cpp
+++ clang/test/Modules/warn-unused-local-typedef.cpp
@@ -1,6 +1,6 @@
 // RUN: rm -rf %t
-// RUN: %clang -Wunused-local-typedef -c -x objective-c++ -fcxx-modules -fmodules -fmodules-cache-path=%t -I %S/Inputs %s 2>&1 | FileCheck %s -check-prefix=CHECK_1
-// RUN: %clang -Wunused-local-typedef -c -x objective-c++ -fcxx-modules -fmodules -fmodules-cache-path=%t -I %S/Inputs %s 2>&1 | FileCheck %s -check-prefix=CHECK_2 -allow-empty
+// RUN: %clang -Wunused-local-typedef -c -x objective-c++ -fcxx-modules -fmodules -fmodules-cache-path=%t -I %S/Inputs %s -o /dev/null 2>&1 | FileCheck %s -check-prefix=CHECK_1
+// RUN: %clang -Wunused-local-typedef -c -x objective-c++ -fcxx-modules -fmodules -fmodules-cache-path=%t -I %S/Inputs %s -o /dev/null 2>&1 | FileCheck %s -check-prefix=CHECK_2 -allow-empty
 
 // For modules, the warning should only fire the first time, when the module is
 // built.
