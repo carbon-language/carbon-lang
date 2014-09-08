bdbca15ccde2f5573acce8400ba6bebe479d0bc5
Chad Rosier <mcrosier@codeaurora.org>
2014-09-08 15:34:16 +0000

    [AArch64] Enabled AA support for Cortex-A57.
    llvm-svn: 217381

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -111,7 +111,7 @@ public:
   bool isCortexA57() const { return CPUString == "cortex-a57"; }
   bool isCortexA53() const { return CPUString == "cortex-a53"; }
 
-  bool useAA() const override { return isCortexA53(); }
+  bool useAA() const override { return isCortexA53() || isCortexA57(); }
 
   /// getMaxInlineSizeThreshold - Returns the maximum memset / memcpy size
   /// that still makes it profitable to inline the call.
