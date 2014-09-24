c4067328cf10a4f487b2624a174e8584aa2370fc
Kaelyn Takata <rikka@google.com>
2014-09-24 18:00:20 +0000

    Revert "Add support for ARM and AArch64 BE object files"
    This reverts commit r218389 as it depends on r218388.
    
    llvm-svn: 218398

--- llvm/include/llvm/Object/RelocVisitor.h
+++ llvm/include/llvm/Object/RelocVisitor.h
@@ -288,8 +288,7 @@ private:
 
   // AArch64 ELF
   RelocToApply visitELF_AARCH64_ABS32(RelocationRef R, uint64_t Value) {
-    int64_t Addend;
-    getELFRelocationAddend(R, Addend);
+    int64_t Addend = getAddend64LE(R);
     int64_t Res =  Value + Addend;
 
     // Overflow check allows for both signed and unsigned interpretation.
@@ -300,8 +299,7 @@ private:
   }
 
   RelocToApply visitELF_AARCH64_ABS64(RelocationRef R, uint64_t Value) {
-    int64_t Addend;
-    getELFRelocationAddend(R, Addend);
+    int64_t Addend = getAddend64LE(R);
     return RelocToApply(Value + Addend, 8);
   }
 
@@ -338,15 +336,8 @@ private:
   }
 
   RelocToApply visitELF_ARM_ABS32(RelocationRef R, uint64_t Value) {
-    int64_t Addend;
-    getELFRelocationAddend(R, Addend);
-    int64_t Res = Value + Addend;
-
-    // Overflow check allows for both signed and unsigned interpretation.
-    if (Res < INT32_MIN || Res > UINT32_MAX)
-      HasError = true;
-
-    return RelocToApply(static_cast<uint32_t>(Res), 4);
+    int64_t Addend = getAddend32LE(R);
+    return RelocToApply(Value + Addend, 4);
   }
 
 };
