8a86fa37af7c37ef0ea10a66e74763acf18777a8
Kostya Serebryany <kcc@google.com>
2014-09-25 23:30:37 +0000

    [asan] disable a test that uses -coverage on Android
    llvm-svn: 218475

--- compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
+++ compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
@@ -1,5 +1,6 @@
 // RUN: %clangxx_asan -coverage -O0 %s -o %t
 // RUN: env ASAN_OPTIONS=check_initialization_order=1 %run %t 2>&1 | FileCheck %s
+// XFAIL: android
 #include <stdio.h>
 int foo() { return 1; }
 int XXX = foo();
