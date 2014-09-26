6ed6ef7ac244b430f70c1e9f0ff6f73381a5add4
NAKAMURA Takumi <geek4civic@gmail.com>
2014-09-26 09:37:15 +0000

    clang/test/CodeGen/builtin-assume-aligned.c: Fix for -Asserts.
    llvm-svn: 218507

--- clang/test/CodeGen/builtin-assume-aligned.c
+++ clang/test/CodeGen/builtin-assume-aligned.c
@@ -47,10 +47,10 @@ int *m1() __attribute__((assume_aligned(64)));
 // CHECK-LABEL: @test5
 int test5() {
   return *m1();
-// CHECK: %ptrint = ptrtoint
-// CHECK: %maskedptr = and i64 %ptrint, 63
-// CHECK: %maskcond = icmp eq i64 %maskedptr, 0
-// CHECK: call void @llvm.assume(i1 %maskcond)
+// CHECK: [[PTRINT5:%.+]] = ptrtoint
+// CHECK: [[MASKEDPTR5:%.+]] = and i64 [[PTRINT5]], 63
+// CHECK: [[MASKCOND5:%.+]] = icmp eq i64 [[MASKEDPTR5]], 0
+// CHECK: call void @llvm.assume(i1 [[MASKCOND5]])
 }
 
 int *m2() __attribute__((assume_aligned(64, 12)));
@@ -58,10 +58,10 @@ int *m2() __attribute__((assume_aligned(64, 12)));
 // CHECK-LABEL: @test6
 int test6() {
   return *m2();
-// CHECK: %ptrint = ptrtoint
-// CHECK: %offsetptr = sub i64 %ptrint, 12
-// CHECK: %maskedptr = and i64 %offsetptr, 63
-// CHECK: %maskcond = icmp eq i64 %maskedptr, 0
-// CHECK: call void @llvm.assume(i1 %maskcond)
+// CHECK: [[PTRINT6:%.+]] = ptrtoint
+// CHECK: [[OFFSETPTR6:%.+]] = sub i64 [[PTRINT6]], 12
+// CHECK: [[MASKEDPTR6:%.+]] = and i64 [[OFFSETPTR6]], 63
+// CHECK: [[MASKCOND6:%.+]] = icmp eq i64 [[MASKEDPTR6]], 0
+// CHECK: call void @llvm.assume(i1 [[MASKCOND6]])
 }
 
