89fd66813b0115faa49b3490df1dce84e9cc20ec
Jim Ingham <jingham@apple.com>
2014-09-30 20:33:25 +0000

    Not all processes have a Dynamic Loader.  Be sure to check that it exists before using it.
    <rdar://problem/18491391>
    
    llvm-svn: 218699

--- lldb/source/Target/ThreadPlanStepThrough.cpp
+++ lldb/source/Target/ThreadPlanStepThrough.cpp
@@ -87,7 +87,10 @@ ThreadPlanStepThrough::DidPush ()
 void
 ThreadPlanStepThrough::LookForPlanToStepThroughFromCurrentPC()
 {
-    m_sub_plan_sp = m_thread.GetProcess()->GetDynamicLoader()->GetStepThroughTrampolinePlan (m_thread, m_stop_others);
+    DynamicLoader *loader = m_thread.GetProcess()->GetDynamicLoader();
+    if (loader)
+        m_sub_plan_sp = loader->GetStepThroughTrampolinePlan (m_thread, m_stop_others);
+        
     // If that didn't come up with anything, try the ObjC runtime plugin:
     if (!m_sub_plan_sp.get())
     {
