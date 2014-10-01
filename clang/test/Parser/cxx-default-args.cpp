56b11ca8a547e590ec6c50679b57f25cae1f018f
Fariborz Jahanian <fjahanian@apple.com>
2014-10-01 21:33:22 +0000

    Test case for my r218780 patch. Suggested by Richard Smith. rdar://18508589.
    llvm-svn: 218830

--- clang/test/Parser/cxx-default-args.cpp
+++ clang/test/Parser/cxx-default-args.cpp
@@ -31,3 +31,8 @@ struct T {
   void f7(bool a = T1<int, bool>::V < 3);
   void f8(int = func<0,1<2>(0), int = 1<0, T1<int,int>(int) = 0);
 };
+
+// rdar://18508589
+struct S { 
+  void f(int &r = error);  // expected-error {{use of undeclared identifier 'error'}}
+};
