815f2869adfe7e9dfec14f4f30639a6c129ee5cf
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-10-01 10:07:28 +0000

    Revert r218721, r218735.
    Failing bootstrap on Linux (arm, x86).
    
    http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux/builds/13139/steps/bootstrap%20clang/logs/stdio
    http://lab.llvm.org:8011/builders/clang-cmake-armv7-a15-selfhost/builds/470
    http://lab.llvm.org:8011/builders/clang-native-arm-lnt/builds/8518
    
    llvm-svn: 218752

--- llvm/lib/Transforms/InstCombine/InstCombine.h
+++ llvm/lib/Transforms/InstCombine/InstCombine.h
@@ -14,7 +14,6 @@
 #include "llvm/Analysis/AssumptionTracker.h"
 #include "llvm/Analysis/TargetFolder.h"
 #include "llvm/Analysis/ValueTracking.h"
-#include "llvm/IR/Dominators.h"
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/IR/InstVisitor.h"
 #include "llvm/IR/IntrinsicInst.h"
@@ -99,7 +98,7 @@ class LLVM_LIBRARY_VISIBILITY InstCombiner
   AssumptionTracker *AT;
   const DataLayout *DL;
   TargetLibraryInfo *TLI;
-  DominatorTree *DT;
+  DominatorTree *DT; // not required
   bool MadeIRChange;
   LibCallSimplifier *Simplifier;
   bool MinimizeSize;
@@ -114,8 +113,7 @@ public:
   BuilderTy *Builder;
 
   static char ID; // Pass identification, replacement for typeid
-  InstCombiner()
-      : FunctionPass(ID), DL(nullptr), DT(nullptr), Builder(nullptr) {
+  InstCombiner() : FunctionPass(ID), DL(nullptr), Builder(nullptr) {
     MinimizeSize = false;
     initializeInstCombinerPass(*PassRegistry::getPassRegistry());
   }
@@ -244,11 +242,6 @@ public:
 
   // visitInstruction - Specify what to return for unhandled instructions...
   Instruction *visitInstruction(Instruction &I) { return nullptr; }
-  bool dominatesAllUses(const Instruction *DI, const Instruction *UI,
-                        const BasicBlock *DB) const;
-  bool replacedSelectWithOperand(SelectInst *SI, const ICmpInst *Icmp,
-                                 const ConstantInt *CI1,
-                                 const ConstantInt *CI2);
 
 private:
   bool ShouldChangeType(Type *From, Type *To) const;
