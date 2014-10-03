16c54d7186e21631543ff31af570d81adfd5c4e1
Hal Finkel <hfinkel@anl.gov>
2014-10-03 04:46:48 +0000

    Make test/Sema/atomic-ops.c free-standing
    This test includes stdint.h, which might include system headers (and that might
    not work, depending on the system configuration). Attempting to fix
    llvm-clang-lld-x86_64-debian-fast.
    
    llvm-svn: 218959

--- clang/test/Sema/atomic-ops.c
+++ clang/test/Sema/atomic-ops.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -verify -fsyntax-only -triple=i686-linux-gnu -std=c11
+// RUN: %clang_cc1 %s -verify -ffreestanding -fsyntax-only -triple=i686-linux-gnu -std=c11
 
 // Basic parsing/Sema tests for __c11_atomic_*
 
