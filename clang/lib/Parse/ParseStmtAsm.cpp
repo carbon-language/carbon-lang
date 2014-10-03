de952d118003838ad4fd724bfabc5c3335ec9c27
Benjamin Kramer <benny.kra@googlemail.com>
2014-10-03 21:48:23 +0000

    Initialize MCObjectFileInfo when parsing ms-style asm.
    Otherwise we're left with an half-initialized bag of variables that may or may
    not explode later on. Should bring the MSVC buildbot back to life.
    
    llvm-svn: 219023

--- clang/lib/Parse/ParseStmtAsm.cpp
+++ clang/lib/Parse/ParseStmtAsm.cpp
@@ -512,6 +512,8 @@ StmtResult Parser::ParseMicrosoftAsmStatement(SourceLocation AsmLoc) {
 
   llvm::SourceMgr TempSrcMgr;
   llvm::MCContext Ctx(MAI.get(), MRI.get(), MOFI.get(), &TempSrcMgr);
+  MOFI->InitMCObjectFileInfo(TT, llvm::Reloc::Default, llvm::CodeModel::Default,
+                             Ctx);
   std::unique_ptr<llvm::MemoryBuffer> Buffer =
       llvm::MemoryBuffer::getMemBuffer(AsmString, "<MS inline asm>");
 
