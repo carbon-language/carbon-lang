d6c04770c19c742709603d9d7c6d34a92f77c711
Yuri Gorshenin <ygorshenin@google.com>
2014-10-07 11:12:55 +0000

    Fixed asan-asm-stacktrace-test.cc
    Summary: Fixed asan-asm-stacktrace-test.cc. Now it's supported on x86_64 and added test run when no debug info is generated.
    
    Differential Revision: http://reviews.llvm.org/D5547
    
    llvm-svn: 219200

--- compiler-rt/test/asan/TestCases/Linux/asan-asm-stacktrace-test.cc
+++ compiler-rt/test/asan/TestCases/Linux/asan-asm-stacktrace-test.cc
@@ -1,15 +1,19 @@
 // Check that a stack unwinding algorithm works corretly even with the assembly
 // instrumentation.
 
-// REQUIRES: x86_64-supported-target,i386-supported-target
+// REQUIRES: x86_64-supported-target
 // RUN: %clangxx_asan -g -O1 %s -fno-inline-functions -fno-omit-frame-pointer -mno-omit-leaf-frame-pointer -mllvm -asan-instrument-assembly -o %t && not %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -g -O1 %s -fno-inline-functions -fomit-frame-pointer -momit-leaf-frame-pointer -mllvm -asan-instrument-assembly -o %t && not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx_asan -g0 -O1 %s -fno-unwind-tables -fno-asynchronous-unwind-tables -fno-exceptions -fno-inline-functions -fomit-frame-pointer -momit-leaf-frame-pointer -mllvm -asan-instrument-assembly -o %t && not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-nounwind
 
 #include <cstddef>
 
 // CHECK: READ of size 4
-// CHECK: {{#0 0x[0-9a-fA-F]+ in foo}}
-// CHECK: {{#1 0x[0-9a-fA-F]+ in main}}
+// CHECK-NEXT: {{#0 0x[0-9a-fA-F]+ in foo}}
+// CHECK-NEXT: {{#1 0x[0-9a-fA-F]+ in main}}
+
+// CHECK-nounwind: READ of size 4
+// CHECK-nounwind-NEXT: {{#0 0x[0-9a-fA-F]+ in foo}}
 
 __attribute__((noinline)) int foo(size_t n, int *buffer) {
   int r;
