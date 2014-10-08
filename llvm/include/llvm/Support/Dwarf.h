eea4f88575b482bb68fc712284812d774984e517
Frederic Riss <friss@apple.com>
2014-10-08 14:59:44 +0000

    Update dwarf::ApplePropertyAttributes enum to meaningful values.
    Summary:
    We currently emit an DW_AT_APPLE_property_attribute with a value that is a
    bitfield describing the various attributes applied to an ObjectiveC property.
    While trying to add testing to one of my dwarfdump patches that would pretty
    print that, I realized this information looks totally broken and has maybe
    never been correct.
    
    As with every DWARF info, we have some enum in Dwarf.h that describes this
    attribute (enum ApplePropertyAttributes). It seems however that the attribute
    value is set from another definition of these flags in Sema/DeclSpec.h (enum
    ObjCPropertyAttributeKind). And these 2 enums aren't in sync.
    
    This patch updates the Dwarf.h values to the ones we are (and have been for
    a very long time) emitting. We change some publicly (and even documented
    in SourceLevelDebugging.rst) values, but I doubt this could be an issue as
    the information has been wrong for so long...
    
    Reviewers: echristo, dblaikie, aprantl
    
    Subscribers: llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D5653
    
    llvm-svn: 219311

--- llvm/include/llvm/Support/Dwarf.h
+++ llvm/include/llvm/Support/Dwarf.h
@@ -780,14 +780,22 @@ enum LocationListEntry : unsigned char {
   DW_LLE_offset_pair_entry
 };
 
+/// Contstants for the DW_APPLE_PROPERTY_attributes attribute.
+/// Keep this list in sync with clang's DeclSpec.h ObjCPropertyAttributeKind.
 enum ApplePropertyAttributes {
   // Apple Objective-C Property Attributes
   DW_APPLE_PROPERTY_readonly = 0x01,
-  DW_APPLE_PROPERTY_readwrite = 0x02,
+  DW_APPLE_PROPERTY_getter = 0x02,
   DW_APPLE_PROPERTY_assign = 0x04,
-  DW_APPLE_PROPERTY_retain = 0x08,
-  DW_APPLE_PROPERTY_copy = 0x10,
-  DW_APPLE_PROPERTY_nonatomic = 0x20
+  DW_APPLE_PROPERTY_readwrite = 0x08,
+  DW_APPLE_PROPERTY_retain = 0x10,
+  DW_APPLE_PROPERTY_copy = 0x20,
+  DW_APPLE_PROPERTY_nonatomic = 0x40,
+  DW_APPLE_PROPERTY_setter = 0x80,
+  DW_APPLE_PROPERTY_atomic = 0x100,
+  DW_APPLE_PROPERTY_weak =   0x200,
+  DW_APPLE_PROPERTY_strong = 0x400,
+  DW_APPLE_PROPERTY_unsafe_unretained = 0x800
 };
 
 /// TagString - Return the string for the specified tag.
