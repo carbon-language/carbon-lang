804a3e655f8dada68ba48fd3009afe858c77498f
Ulrich Weigand <ulrich.weigand@de.ibm.com>
2014-10-14 16:46:45 +0000

    [SystemZ] Address review comments for r219679
    llvm-svn: 219691

--- clang/test/Driver/systemz-as.s
+++ clang/test/Driver/systemz-as.s
@@ -1,8 +1,7 @@
 // Make sure SystemZ defaults to using the integrated assembler
 
 // RUN: %clang -target s390x-ibm-linux -### -c %s 2>&1 \
-// RUN: | FileCheck -check-prefix=DEFAULT %s
-// DEFAULT: "-cc1as"{{.*}} "-target-cpu" "z10"
+// RUN: | FileCheck -check-prefix=IAS %s
 
 // RUN: %clang -target s390x-ibm-linux -integrated-as -### -c %s 2>&1 \
 // RUN: | FileCheck -check-prefix=IAS %s
@@ -10,5 +9,6 @@
 
 // RUN: %clang -target s390x-ibm-linux -no-integrated-as -### -c %s 2>&1 \
 // RUN: | FileCheck -check-prefix=NO-IAS %s
+// NO-IAS-NOT: -cc1as
 // NO-IAS: "-march=z10"
 
