a08a8dc01d208635b58dd41b804967802f743c79
Dmitry Vyukov <dvyukov@google.com>
2014-10-15 08:56:15 +0000

    tsan: use a different dir for llvm build build is used as gtest temp build dir, and is cleared during every build
    llvm-svn: 219783

--- compiler-rt/lib/tsan/check_cmake.sh
+++ compiler-rt/lib/tsan/check_cmake.sh
@@ -3,11 +3,11 @@ set -u
 set -e
 
 ROOT="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-if [ -d "$ROOT/build" ]; then
-  cd $ROOT/build
+if [ -d "$ROOT/llvm-build" ]; then
+  cd $ROOT/llvm-build
 else
-  mkdir -p $ROOT/build
-  cd $ROOT/build
+  mkdir -p $ROOT/llvm-build
+  cd $ROOT/llvm-build
   CC=clang CXX=clang++ cmake -G Ninja -DLLVM_ENABLE_WERROR=ON -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON $ROOT/../../../..
 fi
 ninja
