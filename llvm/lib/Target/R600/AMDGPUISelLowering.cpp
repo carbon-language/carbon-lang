7acfddf17c01dbf953a67793427b375dd8c9d02a
Matt Arsenault <Matthew.Arsenault@amd.com>
2014-10-15 05:06:57 +0000

    R600: Remove outdated comment
    llvm-svn: 219777

--- llvm/lib/Target/R600/AMDGPUISelLowering.cpp
+++ llvm/lib/Target/R600/AMDGPUISelLowering.cpp
@@ -2148,9 +2148,6 @@ SDValue AMDGPUTargetLowering::PerformDAGCombine(SDNode *N,
 
     if (ConstantSDNode *CVal = dyn_cast<ConstantSDNode>(N->getOperand(0))) {
       if (Signed) {
-        // Avoid undefined left shift of a negative in the constant fold.
-        // TODO: I'm not sure what the behavior of the hardware is, this should
-        // probably follow that instead.
         return constantFoldBFE<int32_t>(DAG,
                                         CVal->getSExtValue(),
                                         OffsetVal,
