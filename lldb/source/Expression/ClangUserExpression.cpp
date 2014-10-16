e5d8eaf0422a75a7ee2b8a343f686590ed7d6ba8
Jason Molenda <jmolenda@apple.com>
2014-10-16 21:25:43 +0000

    Only call RemovePersistentVariable on expr_result if that shared pointer contains something.
    llvm-svn: 219966

--- lldb/source/Expression/ClangUserExpression.cpp
+++ lldb/source/Expression/ClangUserExpression.cpp
@@ -1070,7 +1070,7 @@ ClangUserExpression::Evaluate (ExecutionContext &exe_ctx,
                                                              user_expression_sp,
                                                              expr_result);
 
-            if (options.GetResultIsInternal() && process)
+            if (options.GetResultIsInternal() && expr_result && process)
             {
                 process->GetTarget().GetPersistentVariables().RemovePersistentVariable (expr_result);
             }
