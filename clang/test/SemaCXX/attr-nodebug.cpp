953e985ed02617ee01b32c77ce4ac8b3bc21fb81
Aaron Ballman <aaron@aaronballman.com>
2014-10-21 15:46:57 +0000

    The nodebug attribute has a C++11-style spelling supported by GCC as well. This modifies it so we support that spelling as well.
    llvm-svn: 220297

--- clang/test/SemaCXX/attr-nodebug.cpp
+++ clang/test/SemaCXX/attr-nodebug.cpp
@@ -1,7 +1,9 @@
-// RUN: %clang_cc1 %s -verify -fsyntax-only
+// RUN: %clang_cc1 %s -std=c++11 -verify -fsyntax-only
 // Note: most of the 'nodebug' tests are in attr-nodebug.c.
 
 // expected-no-diagnostics
 class c {
   void t3() __attribute__((nodebug));
 };
+
+[[gnu::nodebug]] void f() {}
