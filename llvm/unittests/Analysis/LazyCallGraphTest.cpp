395456655414cebaed8e70ddd9a123cd19a89d53
Filipe Cabecinhas <me@filcab.net>
2014-10-22 02:16:06 +0000

    Silence gcc's -Wcomment
    gcc's (4.7, I think) -Wcomment warning is not "as smart" as clang's and
    warns even if the line right after the backslash-newline sequence only has
    a line comment that starts at the beginning of the line.
    
    llvm-svn: 220360

--- llvm/unittests/Analysis/LazyCallGraphTest.cpp
+++ llvm/unittests/Analysis/LazyCallGraphTest.cpp
@@ -37,21 +37,23 @@ std::unique_ptr<Module> parseAssembly(const char *Assembly) {
   return M;
 }
 
-// IR forming a call graph with a diamond of triangle-shaped SCCs:
-//
-//         d1
-//        /  \
-//       d3--d2
-//      /     \
-//     b1     c1
-//   /  \    /  \
-//  b3--b2  c3--c2
-//       \  /
-//        a1
-//       /  \
-//      a3--a2
-//
-// All call edges go up between SCCs, and clockwise around the SCC.
+/*
+   IR forming a call graph with a diamond of triangle-shaped SCCs:
+
+           d1
+          /  \
+         d3--d2
+        /     \
+       b1     c1
+     /  \    /  \
+    b3--b2  c3--c2
+         \  /
+          a1
+         /  \
+        a3--a2
+
+   All call edges go up between SCCs, and clockwise around the SCC.
+ */
 static const char DiamondOfTriangles[] =
      "define void @a1() {\n"
      "entry:\n"
