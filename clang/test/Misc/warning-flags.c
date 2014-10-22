51c8b3e9dbcdac883ea20d559a4714d2336227ab
David Majnemer <david.majnemer@gmail.com>
2014-10-22 23:03:58 +0000

    Basic: Add ext_delete_void_ptr_operand to -Wdelete-incomplete
    This fixes PR21340.
    
    llvm-svn: 220442

--- clang/test/Misc/warning-flags.c
+++ clang/test/Misc/warning-flags.c
@@ -18,8 +18,7 @@ This test serves two purposes:
 
 The list of warnings below should NEVER grow.  It should gradually shrink to 0.
 
-CHECK: Warnings without flags (99):
-CHECK-NEXT:   ext_delete_void_ptr_operand
+CHECK: Warnings without flags (98):
 CHECK-NEXT:   ext_excess_initializers
 CHECK-NEXT:   ext_excess_initializers_in_char_array_initializer
 CHECK-NEXT:   ext_expected_semi_decl_list
