789d29df34169e21d40bd8581c303e1b3a29f6d2
Todd Fiala <todd.fiala@gmail.com>
2014-10-26 22:25:33 +0000

    Fix up TestRegisters for Linux ptracer lock-down.
    All of these test fixups are prep work for when llgs is
    running with llgs for local process debugging, where these
    tests fail without the ptracer lock-down suppression.
    
    llvm-svn: 220656

--- lldb/test/functionalities/register/main.cpp
+++ lldb/test/functionalities/register/main.cpp
@@ -9,8 +9,25 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#if defined(__linux__)
+#include <sys/prctl.h>
+#endif
+
 int main (int argc, char const *argv[])
 {
+#if defined(__linux__)
+    // Immediately enable any ptracer so that we can allow the stub attach
+    // operation to succeed.  Some Linux kernels are locked down so that
+    // only an ancestor process can be a ptracer of a process.  This disables that
+    // restriction.  Without it, attach-related stub tests will fail.
+#if defined(PR_SET_PTRACER) && defined(PR_SET_PTRACER_ANY)
+    // For now we execute on best effort basis.  If this fails for
+    // some reason, so be it.
+    const int prctl_result = prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+    static_cast<void> (prctl_result);
+#endif
+#endif
+
     char my_string[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 0};
     double my_double = 1234.5678;
 
