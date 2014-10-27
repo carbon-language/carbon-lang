949fb6d276d5539ecbd8b95a00fd93cc6959b396
NAKAMURA Takumi <geek4civic@gmail.com>
2014-10-27 23:29:27 +0000

    AArch64InstrInfo.h: Fix a warning introduced in clang r220703. [-Winconsistent-missing-override]
    llvm-svn: 220739

--- llvm/lib/Target/AArch64/AArch64InstrInfo.h
+++ llvm/lib/Target/AArch64/AArch64InstrInfo.h
@@ -165,7 +165,7 @@ public:
   bool optimizeCompareInstr(MachineInstr *CmpInstr, unsigned SrcReg,
                             unsigned SrcReg2, int CmpMask, int CmpValue,
                             const MachineRegisterInfo *MRI) const override;
-  bool optimizeCondBranch(MachineInstr *MI) const;
+  bool optimizeCondBranch(MachineInstr *MI) const override;
   /// hasPattern - return true when there is potentially a faster code sequence
   /// for an instruction chain ending in <Root>. All potential patterns are
   /// listed
