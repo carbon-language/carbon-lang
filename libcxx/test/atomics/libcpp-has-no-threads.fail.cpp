b2a6048b18304e7a86d59b6220611e30b8e06ec5
Eric Fiselier <eric@efcs.ca>
2014-10-27 21:38:23 +0000

    Add test to ensure including <atomic> fails when _LIBCPP_HAS_NO_THREADS is defined.
    llvm-svn: 220722

---
+++ libcxx/test/atomics/libcpp-has-no-threads.fail.cpp
@@ -0,0 +1,23 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <atomic>
+
+// Test that including <atomic> fails to compile when _LIBCPP_HAS_NO_THREADS
+// is defined.
+
+#ifndef _LIBCPP_HAS_NO_THREADS
+#define _LIBCPP_HAS_NO_THREADS
+#endif
+
+#include <atomic>
+
+int main()
+{
+}
