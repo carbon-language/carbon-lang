0135e236b6cc28bd6180864bb28a704218a55b84
Todd Fiala <todd.fiala@gmail.com>
2014-10-27 00:31:05 +0000

    Fix TestProcessAttach for Linux ptracer lock-down and llgs-local.
    llvm-svn: 220660

--- lldb/test/functionalities/process_attach/main.c
+++ lldb/test/functionalities/process_attach/main.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#if defined(__linux__)
+#include <sys/prctl.h>
+#endif
+
 int main(int argc, char const *argv[]) {
+    int temp;
+#if defined(__linux__)
+    // Immediately enable any ptracer so that we can allow the stub attach
+    // operation to succeed.  Some Linux kernels are locked down so that
+    // only an ancestor process can be a ptracer of a process.  This disables that
+    // restriction.  Without it, attach-related stub tests will fail.
+#if defined(PR_SET_PTRACER) && defined(PR_SET_PTRACER_ANY)
+    int prctl_result;
+
+    // For now we execute on best effort basis.  If this fails for
+    // some reason, so be it.
+    prctl_result = prctl(PR_SET_PTRACER, PR_SET_PTRACER_ANY, 0, 0, 0);
+    (void) prctl_result;
+#endif
+#endif
+
     // Waiting to be attached by the debugger.
-    int temp = 0;
+    temp = 0;
+
     while (temp < 30) // Waiting to be attached...
     {
         sleep(1);
