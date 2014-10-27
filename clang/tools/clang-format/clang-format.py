1f060a85526ac08fd23b46968fd97f75a2930030
Saleem Abdulrasool <compnerd@compnerd.org>
2014-10-27 17:13:33 +0000

    clang-format: improve vim integration docs
    Improve the documentation for vim integration of clang-format.  Prefer the use
    of <c-o> to do the normal mode command execution to avoid side-effects of the
    escape and re-insertion (cursor movement).  Tweak the macros to use a double
    return to avoid having to manually return control to the editor from the
    subprocess.
    
    llvm-svn: 220685

--- clang/tools/clang-format/clang-format.py
+++ clang/tools/clang-format/clang-format.py
@@ -2,8 +2,8 @@
 # - Change 'binary' if clang-format is not on the path (see below).
 # - Add to your .vimrc:
 #
-#   map <C-I> :pyf <path-to-this-file>/clang-format.py<CR>
-#   imap <C-I> <ESC>:pyf <path-to-this-file>/clang-format.py<CR>i
+#   map <C-I> :pyf <path-to-this-file>/clang-format.py<cr>
+#   imap <C-I> <c-o>:pyf <path-to-this-file>/clang-format.py<cr><cr>
 #
 # The first line enables clang-format for NORMAL and VISUAL mode, the second
 # line adds support for INSERT mode. Change "C-I" to another binding if you
