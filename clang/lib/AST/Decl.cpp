9491a5cd4d2d315a98d113b373b0fc11cbd350a9
David Blaikie <dblaikie@gmail.com>
2014-10-31 17:08:11 +0000

    Fix unused-function warning differently from r220853
    Rather than executing this code only needed for an assertion even in a
    non-asserts build, just roll the function into the assert. The assertion
    text literally describes the two cases so it doesn't seem like this
    benefits much from having a separate function (& have to hassle about
    ifndef NDEBUG it out, etc)
    
    llvm-svn: 220970

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -3283,11 +3283,6 @@ bool FieldDecl::isAnonymousStructOrUnion() const {
   return false;
 }
 
-static bool isVLATypeCapturingAllowed(const RecordDecl *RD) {
-  // Allow variable-length array capturing in Lambdas and CapturedStmts.
-  return RD->isLambda() || RD->isCapturedRecord();
-}
-
 unsigned FieldDecl::getBitWidthValue(const ASTContext &Ctx) const {
   assert(isBitField() && "not a bitfield");
   Expr *BitWidth = static_cast<Expr *>(InitStorage.getPointer());
@@ -3330,10 +3325,8 @@ SourceRange FieldDecl::getSourceRange() const {
 }
 
 void FieldDecl::setCapturedVLAType(const VariableArrayType *VLAType) {
-  bool fVLATypeCapturingAllowed = isVLATypeCapturingAllowed(getParent());
-  assert(fVLATypeCapturingAllowed &&
+  assert((RD->isLambda() || RD->isCapturedRecord()) &&
          "capturing type in non-lambda or captured record.");
-  (void)fVLATypeCapturingAllowed;
   assert(InitStorage.getInt() == ISK_BitWidthOrNothing &&
          InitStorage.getPointer() == nullptr &&
          "bit width, initializer or captured type already set");
