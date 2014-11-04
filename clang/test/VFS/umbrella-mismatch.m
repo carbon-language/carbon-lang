cf15c170d5f795b3fcaad81b30dac714e3cc9c1c
NAKAMURA Takumi <geek4civic@gmail.com>
2014-11-04 13:05:23 +0000

    clang/test/VFS/umbrella-mismatch.m: Let it work on win32 to avoid backslashes in yaml.
    FIXME: Is it intentional? s;OUT_DIR;%/S/Inputs;
    llvm-svn: 221264

--- clang/test/VFS/umbrella-mismatch.m
+++ clang/test/VFS/umbrella-mismatch.m
@@ -1,6 +1,5 @@
 // RUN: rm -rf %t
-// RUN: sed -e "s:INPUT_DIR:%S/Inputs:g" -e "s:OUT_DIR:%S/Inputs:g" %S/Inputs/vfsoverlay.yaml > %t.yaml
-// REQUIRES: shell
+// RUN: sed -e "s;INPUT_DIR;%/S/Inputs;g" -e "s;OUT_DIR;%/S/Inputs;g" %S/Inputs/vfsoverlay.yaml > %t.yaml
 
 // RUN: %clang_cc1 -Werror -fmodules -fmodules-cache-path=%t -ivfsoverlay %t.yaml -F %S/Inputs -fsyntax-only %s -verify
 // RUN: %clang_cc1 -Werror -fmodules -fmodules-cache-path=%t -F %S/Inputs -fsyntax-only %s -verify
