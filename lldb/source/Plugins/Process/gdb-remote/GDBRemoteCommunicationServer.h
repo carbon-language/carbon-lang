0010b202ba2545d8bd0fa329083b62bec22d3d79
Eric Christopher <echristo@gmail.com>
2014-11-04 03:14:57 +0000

    Fix one more [-Werror,-Winconsistent-missing-override] error.
    llvm-svn: 221232

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationServer.h
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationServer.h
@@ -58,7 +58,7 @@ public:
                               bool &quit);
 
     virtual bool
-    GetThreadSuffixSupported ()
+    GetThreadSuffixSupported () override
     {
         return true;
     }
