83910ae5b68562f9693d5c179bac407e9a8312dd
NAKAMURA Takumi <geek4civic@gmail.com>
2014-11-04 13:32:17 +0000

    clang/test/Tooling: [PR15590] Avoid backslashes in JSON. Should work on win32.
    llvm-svn: 221266

--- clang/test/Tooling/auto-detect-from-source.cpp
+++ clang/test/Tooling/auto-detect-from-source.cpp
@@ -1,12 +1,8 @@
 // RUN: rm -rf %t
 // RUN: mkdir %t
-// RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %t/test.cpp\",\"file\":\"%t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
+// RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %/t/test.cpp\",\"file\":\"%/t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
 // RUN: cp "%s" "%t/test.cpp"
 // RUN: not clang-check "%t/test.cpp" 2>&1 | FileCheck %s
 
 // CHECK: C++ requires
 invalid;
-
-// REQUIRES: shell
-// PR15590
-// XFAIL: win64
--- clang/test/Tooling/clang-check-autodetect-dir.cpp
+++ clang/test/Tooling/clang-check-autodetect-dir.cpp
@@ -1,13 +1,9 @@
 // RUN: rm -rf %t
 // RUN: mkdir -p %t/abc/def
-// RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %t/test.cpp\",\"file\":\"%t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
+// RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %/t/test.cpp\",\"file\":\"%/t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
 // RUN: cp "%s" "%t/test.cpp"
 // RUN: not clang-check -p "%t/abc/def" "%t/test.cpp" 2>&1|FileCheck %s
 // FIXME: Make the above easier.
 
 // CHECK: C++ requires
 invalid;
-
-// REQUIRES: shell
-// PR15590
-// XFAIL: win64
