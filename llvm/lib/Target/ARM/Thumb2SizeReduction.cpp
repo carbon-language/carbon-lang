219ad280761db479bd75d32b2fc17405d2e11d48
Tilmann Scheller <t.scheller@samsung.com>
2014-11-05 17:28:19 +0000

    [ARM] Remove redundant assignment.
    Found by the Clang static analyzer.
    
    llvm-svn: 221366

--- llvm/lib/Target/ARM/Thumb2SizeReduction.cpp
+++ llvm/lib/Target/ARM/Thumb2SizeReduction.cpp
@@ -384,7 +384,6 @@ Thumb2SizeReduce::ReduceLoadStore(MachineBasicBlock &MBB, MachineInstr *MI,
     if (MI->getOperand(1).getReg() == ARM::SP) {
       Opc = Entry.NarrowOpc2;
       ImmLimit = Entry.Imm2Limit;
-      HasOffReg = false;
     }
 
     Scale = 4;
