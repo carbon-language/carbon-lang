ba7308c07e6d57b020eeb6915220fdc9636edd46
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2014-11-14 13:02:28 +0000

    [asan] Revert r221882.
    This code is not part of ASan runtime (as it may be linked into a different
    DSO), and thus can not call non-exported functions.
    
    llvm-svn: 221986

--- compiler-rt/lib/asan/asan_preinit.cc
+++ compiler-rt/lib/asan/asan_preinit.cc
@@ -18,6 +18,8 @@ using namespace __asan;
 #if SANITIZER_CAN_USE_PREINIT_ARRAY
   // The symbol is called __local_asan_preinit, because it's not intended to be
   // exported.
+  // This code linked into the main executable when -fsanitize=address is in
+  // the link flags. It can only use exported interface functions.
   __attribute__((section(".preinit_array"), used))
-  void (*__local_asan_preinit)(void) = AsanInitFromRtl;
+  void (*__local_asan_preinit)(void) = __asan_init;
 #endif
