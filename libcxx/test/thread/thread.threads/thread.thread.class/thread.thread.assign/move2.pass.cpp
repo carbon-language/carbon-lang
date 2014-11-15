3fe611e508b5e570837e2d82dc4dc26165d51578
Eric Fiselier <eric@efcs.ca>
2014-11-15 01:58:45 +0000

    Split thread test into two parts. Mark one as XFAIL with ASAN.
    The second part of the test checks that std::terminate is called when a running
    thread is move assigned to. Calling std::terminate prevents some of the destructors
    to be called and ASAN fires on this.
    
    llvm-svn: 222076

--- libcxx/test/thread/thread.threads/thread.thread.class/thread.thread.assign/move2.pass.cpp
+++ libcxx/test/thread/thread.threads/thread.thread.class/thread.thread.assign/move2.pass.cpp
@@ -9,6 +9,10 @@
 //
 // UNSUPPORTED: libcpp-has-no-threads
 
+// NOTE: std::terminate is called so the destructors are not invoked and the
+// memory is not freed. This will cause ASAN to fail.
+// XFAIL: asan
+
 // <thread>
 
 // class thread
@@ -16,6 +20,8 @@
 // thread& operator=(thread&& t);
 
 #include <thread>
+#include <exception>
+#include <cstdlib>
 #include <cassert>
 
 class G
@@ -28,6 +34,13 @@ public:
     G() : alive_(1) {++n_alive;}
     G(const G& g) : alive_(g.alive_) {++n_alive;}
     ~G() {alive_ = 0; --n_alive;}
+
+    void operator()()
+    {
+        assert(alive_ == 1);
+        assert(n_alive >= 1);
+        op_run = true;
+    }
 
     void operator()(int i, double j)
     {
@@ -42,21 +55,21 @@ public:
 int G::n_alive = 0;
 bool G::op_run = false;
 
+void f1()
+{
+    std::exit(0);
+}
+
 int main()
 {
 #ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
+    std::set_terminate(f1);
     {
-        assert(G::n_alive == 0);
-        assert(!G::op_run);
         std::thread t0(G(), 5, 5.5);
         std::thread::id id = t0.get_id();
         std::thread t1;
-        t1 = std::move(t0);
-        assert(t1.get_id() == id);
-        assert(t0.get_id() == std::thread::id());
-        t1.join();
-        assert(G::n_alive == 0);
-        assert(G::op_run);
+        t0 = std::move(t1);
+        assert(false);
     }
 #endif  // _LIBCPP_HAS_NO_RVALUE_REFERENCES
 }
