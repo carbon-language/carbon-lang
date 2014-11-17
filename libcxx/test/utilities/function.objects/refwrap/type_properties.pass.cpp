5a8c46653f15350abf326fbf46d7b91cc976671d
Marshall Clow <mclow.lists@gmail.com>
2014-11-17 15:04:46 +0000

    Add tests to ensure that reference_wrapper<T> is trivially copyable. This was added to C++1z with the adoption of N4277, but libc++ already implemented it as a conforming extension. No code changes were needed, just more tests.
    llvm-svn: 222132

--- libcxx/test/utilities/function.objects/refwrap/type_properties.pass.cpp
+++ libcxx/test/utilities/function.objects/refwrap/type_properties.pass.cpp
@@ -16,12 +16,43 @@
 
 #include <functional>
 #include <type_traits>
+#include <string>
+
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
+class MoveOnly
+{
+    MoveOnly(const MoveOnly&);
+    MoveOnly& operator=(const MoveOnly&);
+
+    int data_;
+public:
+    MoveOnly(int data = 1) : data_(data) {}
+    MoveOnly(MoveOnly&& x)
+        : data_(x.data_) {x.data_ = 0;}
+    MoveOnly& operator=(MoveOnly&& x)
+        {data_ = x.data_; x.data_ = 0; return *this;}
+
+    int get() const {return data_;}
+};
+#endif
+
+
+template <class T>
+void test()
+{
+    typedef std::reference_wrapper<T> Wrap;
+    static_assert(std::is_copy_constructible<Wrap>::value, "");
+    static_assert(std::is_copy_assignable<Wrap>::value, "");
+    // Extension up for standardization: See N4151.
+    static_assert(std::is_trivially_copyable<Wrap>::value, "");
+}
 
 int main()
 {
-    typedef std::reference_wrapper<int> T;
-    static_assert(std::is_copy_constructible<T>::value, "");
-    static_assert(std::is_copy_assignable<T>::value, "");
-    // Extension up for standardization: See N4151.
-    static_assert(std::is_trivially_copyable<T>::value, "");
+    test<int>();
+    test<double>();
+    test<std::string>(); 
+#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
+    test<MoveOnly>(); 
+#endif
 }
