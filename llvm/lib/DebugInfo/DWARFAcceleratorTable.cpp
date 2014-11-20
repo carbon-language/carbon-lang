7c41c64db76d4c21b0ff5a22817f40ec8a15317e
Frederic Riss <friss@apple.com>
2014-11-20 16:21:06 +0000

    Add missing copyright headers.
    llvm-svn: 222435

--- llvm/lib/DebugInfo/DWARFAcceleratorTable.cpp
+++ llvm/lib/DebugInfo/DWARFAcceleratorTable.cpp
@@ -1,3 +1,12 @@
+//===--- DWARFAcceleratorTable.cpp ----------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "DWARFAcceleratorTable.h"
 
 #include "llvm/Support/Dwarf.h"
