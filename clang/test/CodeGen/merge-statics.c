bdcc1ed66ddeaba195a072e6def14bfb34917321
Tim Northover <tnorthover@apple.com>
2014-11-24 21:03:34 +0000

    testing: make test use FileCheck
    The "grep internal | count" was fragile when your source or remote paths could
    contain the word "internal".
    
    llvm-svn: 222685

--- clang/test/CodeGen/merge-statics.c
+++ clang/test/CodeGen/merge-statics.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 < %s -emit-llvm | grep internal | count 1
+// RUN: %clang_cc1 < %s -emit-llvm | FileCheck %s
 
 // The two decls for 'a' should merge into one llvm GlobalVariable.
 
@@ -11,3 +11,6 @@ static struct s a =  {
     10
 };
 
+// CHECK-NOT: internal global
+// CHECK: @a = internal global %struct.s { i32 10 }
+// CHECK-NOT: internal-global
