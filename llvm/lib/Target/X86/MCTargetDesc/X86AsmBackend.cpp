27de9b0f7001d3da9423e3e9b7b17565a38c1b51
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2014-12-08 18:18:32 +0000

    [CompactUnwind] Fix register encoding logic
    Fix a compact unwind encoding logic bug which would try to encode
    more callee saved registers than it should, leading to early bail out
    in the encoding logic and abusive use of DWARF frame mode unnecessarily.
    
    Also remove no-compact-unwind.ll which was testing the wrong thing
    based on this bug and move it to valid 'compact unwind' tests. Added
    other few more tests too.
    
    llvm-svn: 223676

--- llvm/lib/Target/X86/MCTargetDesc/X86AsmBackend.cpp
+++ llvm/lib/Target/X86/MCTargetDesc/X86AsmBackend.cpp
@@ -682,7 +682,7 @@ private:
     //     4       3
     //     5       3
     //
-    for (unsigned i = 0; i != CU_NUM_SAVED_REGS; ++i) {
+    for (unsigned i = 0; i < RegCount; ++i) {
       int CUReg = getCompactUnwindRegNum(SavedRegs[i]);
       if (CUReg == -1) return ~0U;
       SavedRegs[i] = CUReg;
