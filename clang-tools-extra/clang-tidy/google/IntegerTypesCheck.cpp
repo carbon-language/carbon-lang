980385095ef8666ebccd72d46bc8f9c6ac95821a
Alexander Kornienko <alexfh@google.com>
2014-12-08 11:17:25 +0000

    [clang-tidy] google-runtime-int: Don't check C code.
    llvm-svn: 223651

--- clang-tools-extra/clang-tidy/google/IntegerTypesCheck.cpp
+++ clang-tools-extra/clang-tidy/google/IntegerTypesCheck.cpp
@@ -26,6 +26,10 @@ void IntegerTypesCheck::registerMatchers(MatchFinder *Finder) {
 }
 
 void IntegerTypesCheck::check(const MatchFinder::MatchResult &Result) {
+  // The relevant Style Guide rule only applies to C++.
+  if (!Result.Context->getLangOpts().CPlusPlus)
+    return;
+
   auto TL = *Result.Nodes.getNodeAs<TypeLoc>("tl");
   SourceLocation Loc = TL.getLocStart();
 
