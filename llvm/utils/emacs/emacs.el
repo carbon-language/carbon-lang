3b7d03b3327e44f4c63e2eca2713aa2b9f7293c8
Will Newton <will.newton@linaro.org>
2014-12-09 08:58:31 +0000

    Improve emacs coding style
    Remove setting of default style, this way is not recommended and
    means that all the settings have to be duplicated to demonstrate the
    c-add-style method which is a much better way of doing it.
    
    Remove the modified date as it is better stored in SVN.
    
    Tweak a few style parameters to make them conform to the actual LLVM
    style.
    
    llvm-svn: 223765

--- llvm/utils/emacs/emacs.el
+++ llvm/utils/emacs/emacs.el
@@ -1,25 +1,20 @@
 ;; LLVM coding style guidelines in emacs
 ;; Maintainer: LLVM Team, http://llvm.org/
-;; Modified:   2009-07-28
 
-;; Max 80 cols per line, indent by two spaces, no tabs.
-;; Apparently, this does not affect tabs in Makefiles.
-(custom-set-variables
-  '(fill-column 80)
-  '(c++-indent-level 2)
-  '(c-basic-offset 2)
-  '(indent-tabs-mode nil))
-
-
-;; Alternative to setting the global style.  Only files with "llvm" in
-;; their names will automatically set to the llvm.org coding style.
+;; Add a cc-mode style for editing LLVM C++ code
 (c-add-style "llvm.org"
              '((fill-column . 80)
 	       (c++-indent-level . 2)
 	       (c-basic-offset . 2)
 	       (indent-tabs-mode . nil)
-               (c-offsets-alist . ((innamespace 0)))))
+	       (c-offsets-alist . ((arglist-intro . ++)
+				   (innamespace . 0)
+				   (member-init-intro . ++)
+				   ))
+               ))
 
+;; Files with "llvm" in their names will automatically be set to the
+;; llvm.org coding style.
 (add-hook 'c-mode-hook
 	  (function
 	   (lambda nil 
