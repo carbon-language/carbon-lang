7c2f55c3b91aeb9c7b3843f9a4b57ef45a1aef4c
Jonathan Roelofs <jonathan@codesourcery.com>
2014-12-11 22:05:18 +0000

    Add 'REQUIERS: long_tests' to a few more long tests
    llvm-svn: 224064

--- libcxx/test/containers/unord/next_prime.pass.cpp
+++ libcxx/test/containers/unord/next_prime.pass.cpp
@@ -6,6 +6,8 @@
 // Source Licenses. See LICENSE.TXT for details.
 //
 //===----------------------------------------------------------------------===//
+//
+// REQUIRES: long_tests
 
 // Not a portable test
 
