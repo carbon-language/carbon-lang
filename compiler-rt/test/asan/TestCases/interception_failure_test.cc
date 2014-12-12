7163d0035996b90e7a131376c500e76ee0e0e415
Viktor Kutuzov <vkutuzov@accesssoftek.com>
2014-12-12 12:13:40 +0000

    [Asan] XFAIL the interception_failure_test.cc test on FreeBSD Differential Revision: http://reviews.llvm.org/D6591
    llvm-svn: 224117

--- compiler-rt/test/asan/TestCases/interception_failure_test.cc
+++ compiler-rt/test/asan/TestCases/interception_failure_test.cc
@@ -5,6 +5,7 @@
 // RUN: %clangxx_asan -O1 %s -o %t && %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -O2 %s -o %t && %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -O3 %s -o %t && %run %t 2>&1 | FileCheck %s
+// XFAIL: freebsd
 #include <stdlib.h>
 #include <stdio.h>
 
