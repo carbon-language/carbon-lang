a47ae907e83a36b0c79a017fb3a9eeaee11b0dee
Alexey Bataev <a.bataev@hotmail.com>
2014-12-15 06:12:42 +0000

    Fixed test/CodeGen/atomic_ops.c for compatibility with hexagon target
    llvm-svn: 224231

--- clang/test/CodeGen/atomic_ops.c
+++ clang/test/CodeGen/atomic_ops.c
@@ -7,12 +7,12 @@ void foo(int x)
   // Check that multiply / divides on atomics produce a cmpxchg loop
   i *= 2;
   // CHECK: mul nsw i32
-  // CHECK: cmpxchg i32*
+  // CHECK: {{(cmpxchg i32*|i1 @__atomic_compare_exchange\(i32 4,)}}
   i /= 2;
   // CHECK: sdiv i32
-  // CHECK: cmpxchg i32*
+  // CHECK: {{(cmpxchg i32*|i1 @__atomic_compare_exchange\(i32 4, )}}
   j /= x;
   // CHECK: sdiv i32
-  // CHECK: cmpxchg i16*
+  // CHECK: {{(cmpxchg i16*|i1 @__atomic_compare_exchange\(i32 2, )}}
 
 }
