393d8013f32abac70e87de76e7acbe730ca23798
David Majnemer <david.majnemer@gmail.com>
2014-12-15 01:04:49 +0000

    StringPool: Cleanup typos in unittest comments
    No functional change intended.
    
    llvm-svn: 224226

--- llvm/unittests/Support/StringPool.cpp
+++ llvm/unittests/Support/StringPool.cpp
@@ -1,6 +1,6 @@
-//===- llvm/unittest/Support/ThreadLocalTest.cpp - Therad Local tests   ---===//
+//===- llvm/unittest/Support/StringPoiil.cpp - StringPool tests -----------===//
 //
-//		       The LLVM Compiler Infrastructure
+//                     The LLVM Compiler Infrastructure
 //
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
