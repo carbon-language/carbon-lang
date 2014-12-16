470a231b7c314542788ad1fbb03f0b3268f23647
Marshall Clow <mclow.lists@gmail.com>
2014-12-16 00:30:07 +0000

    Add test to ensure that iterator_traits<NotAnIterator> doesn't have a value type
    llvm-svn: 224295

--- libcxx/test/iterators/iterator.primitives/iterator.traits/empty.pass.cpp
+++ libcxx/test/iterators/iterator.primitives/iterator.traits/empty.pass.cpp
@@ -20,7 +20,19 @@ struct not_an_iterator
 {
 };
 
+template <class _Tp>
+struct has_value_type
+{
+private:
+    struct two {char lx; char lxx;};
+    template <class _Up> static two test(...);
+    template <class _Up> static char test(typename _Up::value_type* = 0);
+public:
+    static const bool value = sizeof(test<_Tp>(0)) == 1;
+};
+
 int main()
 {
     typedef std::iterator_traits<not_an_iterator> It;
+    static_assert(!(has_value_type<It>::value), "");
 }
