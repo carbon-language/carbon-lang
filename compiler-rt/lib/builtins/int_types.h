7a6f5e47cf3144ea1fdf4105279f0a2bc23aa59f
Petar Jovanovic <petar.jovanovic@imgtec.com>
2014-12-18 01:21:33 +0000

    Do not define CRT_HAS_128BIT for MIPS64
    Do not define it for MIPS64 until its backend supports it.
    See the bug report [1] for more information.
    
    [1] http://llvm.org/bugs/show_bug.cgi?id=20098
    
    Differential Revision: http://reviews.llvm.org/D6703
    
    llvm-svn: 224488

--- compiler-rt/lib/builtins/int_types.h
+++ compiler-rt/lib/builtins/int_types.h
@@ -56,7 +56,8 @@ typedef union
     }s;
 } udwords;
 
-#if __LP64__
+/* MIPS64 issue: PR 20098 */
+#if defined(__LP64__) && !(defined(__mips__) && defined(__clang__))
 #define CRT_HAS_128BIT
 #endif
 
