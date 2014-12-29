60617eac4713376500f25e09afbe04a9eb1a281a
Chandler Carruth <chandlerc@gmail.com>
2014-12-29 19:23:31 +0000

    [py3] Make this test compatible with Python 3 where bytes and strings don't mix so easily. This fixes the last remaining failure I have in 'check-all' on a system with both Python3 and and Python2 installed.
    llvm-svn: 224947

--- lld/test/mach-o/Inputs/DependencyDump.py
+++ lld/test/mach-o/Inputs/DependencyDump.py
@@ -9,19 +9,19 @@ import sys
 
 f = open(sys.argv[1], "rb")
 byte = f.read(1)
-while byte != '':
-    if byte == '\000':
+while byte != b'':
+    if byte == b'\000':
         sys.stdout.write("linker-vers: ")
-    elif byte == '\020':
+    elif byte == b'\020':
         sys.stdout.write("input-file:  ")
-    elif byte == '\021':
+    elif byte == b'\021':
         sys.stdout.write("not-found:   ")
-    elif byte == '\100':
+    elif byte == b'\100':
         sys.stdout.write("output-file: ")
     byte = f.read(1)
-    while byte != '\000':
-        if byte != '\012':
-            sys.stdout.write(byte)
+    while byte != b'\000':
+        if byte != b'\012':
+            sys.stdout.write(byte.decode("ascii"))
         byte = f.read(1)
     sys.stdout.write("\n")
     byte = f.read(1)
