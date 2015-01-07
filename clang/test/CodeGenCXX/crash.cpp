eac50037fb01dff75fda73c32ceebc7b34afd11d
Nico Weber <nicolasweber@gmx.de>
2015-01-07 18:23:08 +0000

    Revert r225085, it caused PR22096.
    PR22096 has several test cases that assert that look fairly different. I'm
    adding one of those as an automated test, but when relanding the other cases
    should probably be checked as well.
    
    llvm-svn: 225361

--- clang/test/CodeGenCXX/crash.cpp
+++ clang/test/CodeGenCXX/crash.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 %s -std=c++11 -emit-llvm-only
+// RUN: %clang_cc1 -emit-obj -o %t -gline-tables-only -O2 -std=c++11 %s
 // CHECK that we don't crash.
 
 // PR11676's example is ill-formed:
@@ -32,4 +33,29 @@ template <class ELFT> void finalizeDefaultAtomValues() {
 }
 
 void f() { finalizeDefaultAtomValues<int>(); }
+}
+
+namespace PR22096 {
+class _String_val {
+  union _Bxty { int i; } _Bx;
+};
+struct string : public _String_val {
+  string(const char *_Ptr) : _String_val() {}
+};
+
+
+int ConvertIPv4NumberToIPv6Number(int);
+struct IPEndPoint {
+  IPEndPoint();
+  IPEndPoint(const int &address, int port);
+  const int &address() const {}
+};
+
+struct SourceAddressTokenTest {
+  SourceAddressTokenTest()
+      : ip4_dual_(ConvertIPv4NumberToIPv6Number(ip4_.address()), 1) {}
+  const string kPrimary = "<primary>";
+  IPEndPoint ip4_;
+  IPEndPoint ip4_dual_;
+} s;
 }
