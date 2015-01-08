c51d3ecb98bc3559c8ad4c785f241e84f5fc3877
Marshall Clow <mclow.lists@gmail.com>
2015-01-08 06:18:59 +0000

    Add checks to make sure the hash functor has the right typedefs
    llvm-svn: 225429

--- libcxx/test/std/utilities/type.index/type.index.hash/hash.pass.cpp
+++ libcxx/test/std/utilities/type.index/type.index.hash/hash.pass.cpp
@@ -23,6 +23,10 @@
 
 int main()
 {
+    typedef std::hash<std::type_index> H;
+    static_assert((std::is_same<typename H::argument_type, std::type_index>::value), "" );
+    static_assert((std::is_same<typename H::result_type, std::size_t>::value), "" );
+
     std::type_index t1 = typeid(int);
     assert(std::hash<std::type_index>()(t1) == t1.hash_code());
 }
