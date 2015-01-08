8ac1419dfd98bada8bae719f54bfd6eabd29126e
Richard Trieu <rtrieu@google.com>
2015-01-08 02:40:08 +0000

    Attempt to fix test from r225423 to get build bots green.
    llvm-svn: 225426

--- clang/test/Misc/diag-special-chars.c
+++ clang/test/Misc/diag-special-chars.c
@@ -1,11 +1,11 @@
 // RUN: %clang_cc1 %s -verify
 // RUN: not %clang_cc1 %s 2>&1 | FileCheck %s
 
-// There are two special characters on the following line, one which is used
-// as a marker character for diagnostic printing.  Ensure diagnostics involving
-// these characters do not cause problems with the diagnostic printer.
-#error Hi   Bye
-//expected-error@-1 {{Hi   Bye}}
+// There is a special characters on the following line, which is used as a
+// marker character for diagnostic printing.  Ensure diagnostics involving
+// this character does not cause problems with the diagnostic printer.
+#error Hi  Bye
+//expected-error@-1 {{Hi  Bye}}
 
-// CHECK: error: Hi   Bye
-// CHECK: #error Hi <U+007F> <U+0080> Bye
+// CHECK: error: Hi  Bye
+// CHECK: #error Hi <U+007F> Bye
