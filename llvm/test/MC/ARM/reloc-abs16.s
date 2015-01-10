ff2da70fdd1767e2f405c86a544a80397e7a451b
Saleem Abdulrasool <compnerd@compnerd.org>
2015-01-10 02:48:25 +0000

    tests: convert a couple of ARM relocation tests to readobj
    These tests are checking the relocation generation.  Use the readobj output as
    it is much easier to follow when glancing over the tests.
    
    llvm-svn: 225575

--- llvm/test/MC/ARM/reloc-abs16.s
+++ llvm/test/MC/ARM/reloc-abs16.s
@@ -1,6 +1,6 @@
-@ RUN: llvm-mc -triple armv7-eabi -filetype obj -o - %s | llvm-objdump -r - \
+@ RUN: llvm-mc -triple armv7-eabi -filetype obj -o - %s | llvm-readobj -r - \
 @ RUN:     | FileCheck %s
-@ RUN: llvm-mc -triple thumbv7-eabi -filetype obj -o - %s | llvm-objdump -r - \
+@ RUN: llvm-mc -triple thumbv7-eabi -filetype obj -o - %s | llvm-readobj -r - \
 @ RUN:     | FileCheck %s
 
 	.syntax unified
@@ -8,6 +8,10 @@
 	.short abs16_0 -32768
 	.short abs16_1 +65535
 
-@ CHECK: 0 R_ARM_ABS16 abs16_0
-@ CHECK: 2 R_ARM_ABS16 abs16_1
+@ CHECK: Relocations {
+@ CHECK:   Section (2) .rel.text {
+@ CHECK:     0x0 R_ARM_ABS16 abs16_0 0x0
+@ CHECK:     0x2 R_ARM_ABS16 abs16_1 0x0
+@ CHECK:   }
+@ CHECK: }
 
--- llvm/test/MC/ARM/reloc-abs8.s
+++ llvm/test/MC/ARM/reloc-abs8.s
@@ -1,6 +1,6 @@
-@ RUN: llvm-mc -triple armv7-eabi -filetype obj -o - %s | llvm-objdump -r - \
+@ RUN: llvm-mc -triple armv7-eabi -filetype obj -o - %s | llvm-readobj -r - \
 @ RUN:     | FileCheck %s
-@ RUN: llvm-mc -triple thumbv7-eabi -filetype obj -o - %s | llvm-objdump -r - \
+@ RUN: llvm-mc -triple thumbv7-eabi -filetype obj -o - %s | llvm-readobj -r - \
 @ RUN:     | FileCheck %s
 
 	.syntax unified
@@ -8,5 +8,9 @@
 	.byte abs8_0 -128
 	.byte abs8_1 +255
 
-@ CHECK: 0 R_ARM_ABS8 abs8_0
-@ CHECK: 1 R_ARM_ABS8 abs8_1
+@ CHECK: Relocations {
+@ CHECK:   Section (2) .rel.text {
+@ CHECK:     0x0 R_ARM_ABS8 abs8_0 0x0
+@ CHECK:     0x1 R_ARM_ABS8 abs8_1 0x0
+@ CHECK:   }
+@ CHECK: }
