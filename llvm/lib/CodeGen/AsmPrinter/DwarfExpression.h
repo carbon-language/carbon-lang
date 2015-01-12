9cffbd8daa9504579d1acc814e30c852745d5559
Adrian Prantl <aprantl@apple.com>
2015-01-12 23:36:50 +0000

    remove extra semicolon
    llvm-svn: 225730

--- llvm/lib/CodeGen/AsmPrinter/DwarfExpression.h
+++ llvm/lib/CodeGen/AsmPrinter/DwarfExpression.h
@@ -28,7 +28,7 @@ protected:
   TargetMachine &TM;
 public:
   DwarfExpression(TargetMachine &TM) : TM(TM) {}
-  virtual ~DwarfExpression() {};
+  virtual ~DwarfExpression() {}
 
   virtual void EmitOp(uint8_t Op, const char* Comment = nullptr) = 0;
   virtual void EmitSigned(int Value) = 0;
