ffbadedcf9124d6937562880cfafa3668db93e43
Eric Fiselier <eric@efcs.ca>
2015-01-12 20:43:34 +0000

    [LIT] Remove string decoding in gtest discovery code. lit.util.capture now does decoding.
    llvm-svn: 225693

--- llvm/utils/lit/lit/formats/googletest.py
+++ llvm/utils/lit/lit/formats/googletest.py
@@ -31,7 +31,6 @@ class GoogleTest(TestFormat):
         try:
             lines = lit.util.capture([path, '--gtest_list_tests'],
                                      env=localConfig.environment)
-            lines = lines.decode('utf-8')
             if kIsWindows:
               lines = lines.replace('\r', '')
             lines = lines.split('\n')
