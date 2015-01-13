9db9069cf31eb9eddc1ff19b0d5667e33a8c3f75
Marshall Clow <mclow.lists@gmail.com>
2015-01-13 16:49:52 +0000

    Make regex::assign not clobber the regex in case of failure. Fixes PR#22213
    llvm-svn: 225799

--- libcxx/test/std/re/re.regex/re.regex.assign/assign.pass.cpp
+++ libcxx/test/std/re/re.regex/re.regex.assign/assign.pass.cpp
@@ -23,4 +23,13 @@ int main()
     r2.assign(r1);
     assert(r2.flags() == std::regex::ECMAScript);
     assert(r2.mark_count() == 2);
+    assert(std::regex_search("ab", r2));
+    
+    bool caught = false;
+    try { r2.assign("(def", std::regex::extended); }
+    catch(std::regex_error &) { caught = true; }
+    assert(caught);
+    assert(r2.flags() == std::regex::ECMAScript);
+    assert(r2.mark_count() == 2);
+    assert(std::regex_search("ab", r2));
 }
