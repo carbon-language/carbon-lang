f8637360e2ea0e1c697bcfc24dafa8f12d31171c
David Majnemer <david.majnemer@gmail.com>
2015-01-15 08:41:25 +0000

    AST: Ensure implicit records have default visibility
    Types composed with certain implicit record types would have their RTTI
    marked as hidden because the implicit record type didn't have any
    visibility.
    
    This manifests itself as triggering false positives from tools like
    clang's -fsantize=function feature.  The RTTI for a function type's
    return type wouldn't match if the return type was an implicit record
    type.
    
    Patch by Stephan Bergmann!
    
    llvm-svn: 226148

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -876,6 +876,8 @@ RecordDecl *ASTContext::buildImplicitRecord(StringRef Name,
     NewDecl = RecordDecl::Create(*this, TK, getTranslationUnitDecl(), Loc, Loc,
                                  &Idents.get(Name));
   NewDecl->setImplicit();
+  NewDecl->addAttr(TypeVisibilityAttr::CreateImplicit(
+      const_cast<ASTContext &>(*this), TypeVisibilityAttr::Default));
   return NewDecl;
 }
 
