d47dec469056f488818dc2ab0ea124e81dcc2a61
Adam Nemet <anemet@apple.com>
2015-01-16 18:51:46 +0000

    Fix typo in r225922.
    llvm-svn: 226297

--- clang/test/CodeGen/avx512f-builtins.c
+++ clang/test/CodeGen/avx512f-builtins.c
@@ -197,14 +197,14 @@ __m512d test_mm512_unpacklo_pd(__m512d a, __m512d b)
   return _mm512_unpacklo_pd(a, b);
 }
 
-__m512d test_mm512_unpackhi_ps(__m512d a, __m512d b)
+__m512 test_mm512_unpackhi_ps(__m512 a, __m512 b)
 {
   // CHECK-LABEL: @test_mm512_unpackhi_ps
   // CHECK: shufflevector <16 x float> {{.*}} <i32 2, i32 18, i32 3, i32 19, i32 6, i32 22, i32 7, i32 23, i32 10, i32 26, i32 11, i32 27, i32 14, i32 30, i32 15, i32 31>
   return _mm512_unpackhi_ps(a, b);
 }
 
-__m512d test_mm512_unpacklo_ps(__m512d a, __m512d b)
+__m512 test_mm512_unpacklo_ps(__m512 a, __m512 b)
 {
   // CHECK-LABEL: @test_mm512_unpacklo_ps
   // CHECK: shufflevector <16 x float> {{.*}} <i32 0, i32 16, i32 1, i32 17, i32 4, i32 20, i32 5, i32 21, i32 8, i32 24, i32 9, i32 25, i32 12, i32 28, i32 13, i32 29>
