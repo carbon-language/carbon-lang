82d3ef1617c8bc17999f16a292b89e80ee8153d3
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-01-16 10:30:53 +0000

    [asan] Fix asan_options-include test.
    Wrong include order.
    
    llvm-svn: 226268

--- compiler-rt/test/asan/TestCases/asan_options-include.cc
+++ compiler-rt/test/asan/TestCases/asan_options-include.cc
@@ -1,8 +1,8 @@
 // RUN: %clangxx_asan -O0 %s -o %t
-// RUN: echo "symbolize=1\ninclude='%t.options2.txt'" > %t.options1.txt
-// RUN: echo "verbosity=1" > %t.options2.txt
-// RUN: ASAN_OPTIONS="verbosity=0:include='%t.options2.txt'" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY1
-// RUN: ASAN_OPTIONS="include='%t.options2.txt',verbosity=0" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY0
+// RUN: echo -e "symbolize=1\ninclude='%t.options2.txt'" >%t.options1.txt
+// RUN: echo -e "verbosity=1\n" >%t.options2.txt
+// RUN: ASAN_OPTIONS="verbosity=0:include='%t.options1.txt'" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY1
+// RUN: ASAN_OPTIONS="include='%t.options1.txt',verbosity=0" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY0
 // RUN: ASAN_OPTIONS="include='%t.options-not-found.txt',verbosity=0" not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-NOT-FOUND
 
 #include <stdio.h>
