44480078857a2f211c2b4c49308186180b041c8f
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-01-16 12:08:32 +0000

    [asan] More verbose output from one of the tests.
    Trying to debug a buildbot-only failure.
    
    llvm-svn: 226270

--- compiler-rt/test/asan/TestCases/asan_options-include.cc
+++ compiler-rt/test/asan/TestCases/asan_options-include.cc
@@ -1,9 +1,14 @@
 // RUN: %clangxx_asan -O0 %s -o %t
 // RUN: echo -e "symbolize=1\ninclude='%t.options2.txt'" >%t.options1.txt
 // RUN: echo -e "verbosity=1\n" >%t.options2.txt
-// RUN: ASAN_OPTIONS="verbosity=0:include='%t.options1.txt'" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY1
-// RUN: ASAN_OPTIONS="include='%t.options1.txt',verbosity=0" %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-VERBOSITY0
-// RUN: ASAN_OPTIONS="include='%t.options-not-found.txt',verbosity=0" not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-NOT-FOUND
+// RUN: cat %t.options1.txt
+// RUN: cat %t.options2.txt
+// RUN: ASAN_OPTIONS="verbosity=0:include='%t.options1.txt'" %run %t 2>&1 | tee %t.out
+// RUN: FileCheck %s --check-prefix=CHECK-VERBOSITY1 <%t.out
+// RUN: ASAN_OPTIONS="include='%t.options1.txt',verbosity=0" %run %t 2>&1 | tee %t.out
+// RUN: FileCheck %s --check-prefix=CHECK-VERBOSITY0 <%t.out
+// RUN: ASAN_OPTIONS="include='%t.options-not-found.txt',verbosity=0" not %run %t 2>&1 | tee %t.out
+// RUN: FileCheck %s --check-prefix=CHECK-NOT-FOUND < %t.out
 
 #include <stdio.h>
 
