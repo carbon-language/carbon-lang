e35a6e69f22cb4086f3050d3473869304b87199d
Rui Ueyama <ruiu@google.com>
2015-01-17 23:27:37 +0000

    Remove dead code.
    llvm-svn: 226381

--- lld/include/lld/ReaderWriter/ELFLinkingContext.h
+++ lld/include/lld/ReaderWriter/ELFLinkingContext.h
@@ -290,17 +290,6 @@ public:
   bool alignSegments() const { return _alignSegments; }
   void setAlignSegments(bool align) { _alignSegments = align; }
 
-  /// \brief The attributes class provides a way for a input file to look into
-  /// all the positional attributes that were specified in the command line.
-  /// There are few positional operators and the number of arguments to the
-  /// ELFFileNode class keeps growing. This achieves code to be clean as well.
-  class Attributes {
-  public:
-    Attributes() : _isSysRooted(false) {}
-    void setSysRooted(bool isSysRooted) { _isSysRooted = isSysRooted; }
-    bool _isSysRooted;
-  };
-
 private:
   ELFLinkingContext() LLVM_DELETED_FUNCTION;
 
