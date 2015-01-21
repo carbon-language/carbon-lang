3f02c14cc742180a27f5c99a998e3c5d1ac2f6c8
Yaron Keren <yaron.keren@gmail.com>
2015-01-21 16:20:38 +0000

    Add missing include guards to WindowsSupport.h.
    llvm-svn: 226669

--- llvm/lib/Support/Windows/WindowsSupport.h
+++ llvm/lib/Support/Windows/WindowsSupport.h
@@ -19,6 +19,9 @@
 //===          is guaranteed to work on *all* Win32 variants.
 //===----------------------------------------------------------------------===//
 
+#ifndef LLVM_SUPPORT_WINDOWSSUPPORT_H
+#define LLVM_SUPPORT_WINDOWSSUPPORT_H
+
 // mingw-w64 tends to define it as 0x0502 in its headers.
 #undef _WIN32_WINNT
 #undef _WIN32_IE
@@ -178,3 +181,5 @@ std::error_code UTF16ToCurCP(const wchar_t *utf16, size_t utf16_len,
 } // end namespace windows
 } // end namespace sys
 } // end namespace llvm.
+
+#endif
