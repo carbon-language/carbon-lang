312beb174f2c10c2003a5ca89bf566063af76d61
Ramkumar Ramachandra <artagnon@gmail.com>
2015-01-22 20:56:25 +0000

    [emacs] Use c-mode-common-hook, derive from "gnu"
    Make it clear that the "llvm.org" style is deriving from "gnu" style,
    and use the c-mode-common-hook instead of c-mode-hook and c++-mode-hook.
    
    Differential Revision: http://reviews.llvm.org/D7035
    
    llvm-svn: 226861

--- llvm/utils/emacs/emacs.el
+++ llvm/utils/emacs/emacs.el
@@ -1,34 +1,22 @@
 ;; LLVM coding style guidelines in emacs
 ;; Maintainer: LLVM Team, http://llvm.org/
 
-;; Add a cc-mode style for editing LLVM C++ code
+;; Add a cc-mode style for editing LLVM C and C++ code
 (c-add-style "llvm.org"
-             '((fill-column . 80)
+             '("gnu"
+	       (fill-column . 80)
 	       (c++-indent-level . 2)
 	       (c-basic-offset . 2)
 	       (indent-tabs-mode . nil)
 	       (c-offsets-alist . ((arglist-intro . ++)
 				   (innamespace . 0)
-				   (member-init-intro . ++)
-				   ))
-               ))
+				   (member-init-intro . ++)))))
 
 ;; Files with "llvm" in their names will automatically be set to the
 ;; llvm.org coding style.
-(add-hook 'c-mode-hook
+(add-hook 'c-mode-common-hook
 	  (function
 	   (lambda nil 
 	     (if (string-match "llvm" buffer-file-name)
 		 (progn
-		   (c-set-style "llvm.org")
-		   )
-	       ))))
-
-(add-hook 'c++-mode-hook
-	  (function
-	   (lambda nil 
-	     (if (string-match "llvm" buffer-file-name)
-		 (progn
-		   (c-set-style "llvm.org")
-		   )
-	       ))))
+		   (c-set-style "llvm.org"))))))
