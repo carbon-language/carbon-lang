d2a7eda2b7415a0936023a0e742e84e6a42a76b2
Richard Smith <richard-llvm@metafoo.co.uk>
2015-01-23 00:30:44 +0000

    Fix reference to sysroot in this test (broken in r226875).
    llvm-svn: 226885

--- clang/test/Modules/self-import-header.m
+++ clang/test/Modules/self-import-header.m
@@ -1,7 +1,7 @@
 // rdar://13840148
 
 // RUN: rm -rf %t
-// RUN: %clang -fsyntax-only -isysroot %S/../Inputs/System/usr/include -fmodules -fmodules-cache-path=%t \
+// RUN: %clang -fsyntax-only -isysroot %S/Inputs/System/usr/include -fmodules -fmodules-cache-path=%t \
 // RUN:    -target x86_64-darwin \
 // RUN:    -F %S -I %S/Inputs/self-import-header %s -D__need_wint_t -Werror=implicit-function-declaration
 
