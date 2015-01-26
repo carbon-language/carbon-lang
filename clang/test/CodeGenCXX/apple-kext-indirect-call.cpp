99e05d02cb731de923a6ac8d6408a05e69ccc3be
Nico Weber <nicolasweber@gmx.de>
2015-01-26 02:59:07 +0000

    Test that qualified virtual calls mark vtables referenced in apple kext mode.
    I broke this locally while working on PR20337 and no test caught that.  Now
    there's coverage for this, and a comment explaining why this is needed.
    
    llvm-svn: 227068

--- clang/test/CodeGenCXX/apple-kext-indirect-call.cpp
+++ clang/test/CodeGenCXX/apple-kext-indirect-call.cpp
@@ -1,5 +1,7 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fapple-kext -emit-llvm -o - %s | FileCheck %s
 
+// CHECK: @_ZTV5TemplIiE = internal unnamed_addr constant [5 x i8*] [i8* null, i8* bitcast ({ i8*, i8* }* @_ZTI5TemplIiE to i8*), i8* bitcast (void (%struct.Templ*)* @_ZN5TemplIiE1fEv to i8*), i8* bitcast (void (%struct.Templ*)* @_ZN5TemplIiE1gEv to i8*), i8* null]
+
 struct Base { 
   virtual void abc(void) const; 
 };
@@ -12,3 +14,30 @@ void FUNC(Base* p) {
 
 // CHECK: getelementptr inbounds (void (%struct.Base*)** bitcast ([4 x i8*]* @_ZTV4Base to void (%struct.Base*)**), i64 2)
 // CHECK-NOT: call void @_ZNK4Base3abcEv
+
+template<class T>
+struct Templ {
+  virtual void f() {}
+  virtual void g() {}
+};
+template<class T>
+struct SubTempl : public Templ<T> {
+  virtual void f() {} // override
+  virtual void g() {} // override
+};
+
+void f(SubTempl<int>* t) {
+  // Qualified calls go through the (qualified) vtable in apple-kext mode.
+  // Since t's this pointer points to SubTempl's vtable, this call needs
+  // to load Templ<int>'s vtable, so that needs to be defined in this TU,
+  // which in turn means that Templ<int>::g needs to be instantiated in this TU,
+  // for it's referenced by the vtable.
+  // (This happens only in apple-kext mode; elsewhere virtual calls can always
+  // use the vtable pointer off this instead of having to load the vtable
+  // symbol.)
+  t->Templ::f();
+}
+
+// CHECK: getelementptr inbounds (void (%struct.Templ*)** bitcast ([5 x i8*]* @_ZTV5TemplIiE to void (%struct.Templ*)**), i64 2)
+// CHECK: define internal void @_ZN5TemplIiE1fEv(%struct.Templ* %this)
+// CHECK: define internal void @_ZN5TemplIiE1gEv(%struct.Templ* %this)
