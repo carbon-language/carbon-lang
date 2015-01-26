2081cefdbe9ffc5ff703fbc529485a7161495aec
NAKAMURA Takumi <geek4civic@gmail.com>
2015-01-26 09:30:29 +0000

    Revert llvm/test/MC/ELF/noexec.s in r227074, "Fix a problem where the AArch64 ELF assembler was failing with"
    It should be split into target-specific location.
    
    llvm-svn: 227080

--- llvm/test/MC/ELF/noexec.s
+++ llvm/test/MC/ELF/noexec.s
@@ -1,5 +1,4 @@
 // RUN: llvm-mc -no-exec-stack -filetype=obj -triple x86_64-pc-linux-gnu %s -o - | llvm-readobj -s -t | FileCheck  %s
-// RUN: llvm-mc -no-exec-stack -filetype=obj -triple aarch64-linux-gnu %s -o - | llvm-readobj -s -t | FileCheck %s
 
 // CHECK:        Section {
 // CHECK:          Index: 4
