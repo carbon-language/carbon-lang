56f455c9fcd5f51a4888f452884043c632d026d3
Greg Clayton <gclayton@apple.com>
2015-01-27 23:45:56 +0000

    Added test case to make sure we don't regress on settings breakpoints by selector and with the various partially specified ObjC specified breakpoints.
    This test tests the equivalent of:
        breakpoint set --name count
        breakpoint set --selector count
        breakpoint set --name isEqual:
        breakpoint set --selector isEqual:
        breakpoint set --name "-[MyClass(MyCategory) myCategoryFunction]"
        breakpoint set --name "-[MyClass myCategoryFunction]"
        breakpoint set --name "[MyClass myCategoryFunction]"
    
    llvm-svn: 227271

---
+++ lldb/test/functionalities/breakpoint/objc/TestObjCBreakpoints.py
@@ -0,0 +1,105 @@
+"""
+Test that objective-c constant strings are generated correctly by the expression
+parser.
+"""
+
+import os, time
+import unittest2
+import lldb
+from lldbtest import *
+import lldbutil
+import shutil
+import subprocess
+
+@unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
+class TestObjCBreakpoints(TestBase):
+
+    mydir = TestBase.compute_mydir(__file__)
+
+    @dsym_test
+    def test_break_with_dsym(self):
+        """Test setting Objective C specific breakpoints (dSYM)."""
+        self.buildDsym()
+        self.setTearDownCleanup()
+        self.check_objc_breakpoints(True)
+
+    @dwarf_test
+    def test_break_with_dwarf(self):
+        """Test setting Objective C specific breakpoints (DWARF in .o files)."""
+        self.buildDwarf()
+        self.setTearDownCleanup()
+        self.check_objc_breakpoints(False)
+
+    def setUp(self):
+        # Call super's setUp().
+        TestBase.setUp(self)
+        # Find the line number to break inside main().
+        self.main_source = "main.m"
+        self.line = line_number(self.main_source, '// Set breakpoint here')
+
+    def check_category_breakpoints(self):
+        name_bp = self.target.BreakpointCreateByName ("myCategoryFunction")
+        selector_bp = self.target.BreakpointCreateByName ("myCategoryFunction", lldb.eFunctionNameTypeSelector, lldb.SBFileSpecList(), lldb.SBFileSpecList())
+        self.assertTrue(name_bp.GetNumLocations() == selector_bp.GetNumLocations(), 'Make sure setting a breakpoint by name "myCategoryFunction" sets a breakpoint even though it is in a category')
+        for bp_loc in selector_bp:
+            function_name = bp_loc.GetAddress().GetSymbol().GetName()
+            self.assertTrue(" myCategoryFunction]" in function_name, 'Make sure all function names have " myCategoryFunction]" in their names')
+            
+        category_bp = self.target.BreakpointCreateByName ("-[MyClass(MyCategory) myCategoryFunction]")
+        stripped_bp = self.target.BreakpointCreateByName ("-[MyClass myCategoryFunction]")
+        stripped2_bp = self.target.BreakpointCreateByName ("[MyClass myCategoryFunction]")
+        self.assertTrue(category_bp.GetNumLocations() == 1, "Make sure we can set a breakpoint using a full objective C function name with the category included (-[MyClass(MyCategory) myCategoryFunction])")
+        self.assertTrue(stripped_bp.GetNumLocations() == 1, "Make sure we can set a breakpoint using a full objective C function name without the category included (-[MyClass myCategoryFunction])")
+        self.assertTrue(stripped2_bp.GetNumLocations() == 1, "Make sure we can set a breakpoint using a full objective C function name without the category included ([MyClass myCategoryFunction])")
+        
+    def check_objc_breakpoints(self, have_dsym):
+        """Test constant string generation amd comparison by the expression parser."""
+
+        # Set debugger into synchronous mode
+        self.dbg.SetAsync(False)
+
+        # Create a target by the debugger.
+        exe = os.path.join(os.getcwd(), "a.out")
+        self.target = self.dbg.CreateTarget(exe)
+        self.assertTrue(self.target, VALID_TARGET)
+
+        #----------------------------------------------------------------------
+        # Set breakpoints on all selectors whose name is "count". This should 
+        # catch breakpoints that are both C functions _and_ anything whose 
+        # selector is "count" because just looking at "count" we can't tell
+        # definitively if the name is a selector or a C function
+        #----------------------------------------------------------------------
+        name_bp = self.target.BreakpointCreateByName ("count")
+        selector_bp = self.target.BreakpointCreateByName ("count", lldb.eFunctionNameTypeSelector, lldb.SBFileSpecList(), lldb.SBFileSpecList())
+        self.assertTrue(name_bp.GetNumLocations() >= selector_bp.GetNumLocations(), 'Make sure we get at least the same amount of breakpoints if not more when setting by name "count"')
+        self.assertTrue(selector_bp.GetNumLocations() > 50, 'Make sure we find a lot of "count" selectors') # There are 93 on the latest MacOSX
+        for bp_loc in selector_bp:
+            function_name = bp_loc.GetAddress().GetSymbol().GetName()
+            self.assertTrue(" count]" in function_name, 'Make sure all function names have " count]" in their names')
+
+        #----------------------------------------------------------------------
+        # Set breakpoints on all selectors whose name is "isEqual:". This should
+        # catch breakpoints that are only ObjC selectors because no C function
+        # can end with a :
+        #----------------------------------------------------------------------
+        name_bp = self.target.BreakpointCreateByName ("isEqual:")
+        selector_bp = self.target.BreakpointCreateByName ("isEqual:", lldb.eFunctionNameTypeSelector, lldb.SBFileSpecList(), lldb.SBFileSpecList())
+        self.assertTrue(name_bp.GetNumLocations() == selector_bp.GetNumLocations(), 'Make sure setting a breakpoint by name "isEqual:" only sets selector breakpoints')
+        for bp_loc in selector_bp:
+            function_name = bp_loc.GetAddress().GetSymbol().GetName()
+            self.assertTrue(" isEqual:]" in function_name, 'Make sure all function names have " isEqual:]" in their names')
+    
+        self.check_category_breakpoints()
+        
+        if have_dsym:
+            shutil.rmtree(exe + ".dSYM")
+        self.assertTrue(subprocess.call(['/usr/bin/strip', '-Sx', exe]) == 0, 'stripping dylib succeeded')
+        
+        # Check breakpoints again, this time using the symbol table only
+        self.check_category_breakpoints()
+        
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
