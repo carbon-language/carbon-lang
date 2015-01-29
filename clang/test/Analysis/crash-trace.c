4b643c23fd1a84aa125b4b6bd9c1a78fc4bceb28
NAKAMURA Takumi <geek4civic@gmail.com>
2015-01-29 13:23:23 +0000

    Disable a couple of crash-* tests for now. It seems they might be incompatible to win32.
    llvm-svn: 227434

--- clang/test/Analysis/crash-trace.c
+++ clang/test/Analysis/crash-trace.c
@@ -1,6 +1,9 @@
 // RUN: not --crash %clang_cc1 -analyze -analyzer-checker=debug.ExprInspection %s 2>&1 | FileCheck %s
 // REQUIRES: crash-recovery
 
+// FIXME: CHECKs might be incompatible to win32.
+// REQUIRES: shell
+
 void clang_analyzer_crash(void);
 
 void inlined() {
--- clang/test/Parser/crash-report.c
+++ clang/test/Parser/crash-report.c
@@ -1,6 +1,9 @@
 // RUN: not --crash %clang_cc1 %s 2>&1 | FileCheck %s
 // REQUIRES: crash-recovery
 
+// FIXME: CHECKs might be incompatible to win32.
+// REQUIRES: shell
+
 #prag\
 ma clang __debug crash
 
