fb9a7c8b6753e8e27be55beb4d10c172edade02c
Eric Christopher <echristo@gmail.com>
2015-01-29 23:46:39 +0000

    Remove unused header.
    llvm-svn: 227516

--- llvm/lib/Target/MSP430/MSP430MCInstLower.cpp
+++ llvm/lib/Target/MSP430/MSP430MCInstLower.cpp
@@ -26,7 +26,6 @@
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Target/TargetMachine.h"
-#include "llvm/Target/TargetSubtargetInfo.h"
 using namespace llvm;
 
 MCSymbol *MSP430MCInstLower::
