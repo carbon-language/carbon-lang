18cf8e018ad83bfd6cd626456e472728fdba092f
Enrico Granata <egranata@apple.com>
2015-02-02 23:03:37 +0000

    Some cleanup in lldbpexpect
    llvm-svn: 227889

--- lldb/test/lldbpexpect.py
+++ lldb/test/lldbpexpect.py
@@ -11,35 +11,29 @@ class PExpectTest(TestBase):
     mydir = TestBase.compute_mydir(__file__)
 
     def setUp(self):
-        # Call super's setUp().
         TestBase.setUp(self)
 
-    def doTest(self):
-        # put your commands here
-        pass
-
     def launchArgs(self):
-        return ""
+        pass
 
     def launch(self):
         self.timeout = 5
         self.child = pexpect.spawn('%s %s' % (self.lldbHere, self.launchArgs()))
 
     def expect(self, patterns=None, timeout=None):
-        if patterns is None: patterns = '.*'
+        if patterns is None: return None
+        if timeout is None: timeout = self.timeout
         return self.child.expect(patterns, timeout=timeout)
 
     def sendimpl(self, sender, command, patterns=None, timeout=None):
-        if timeout is None: timeout = self.timeout
         sender(command)
-        if patterns is not None: return self.expect(patterns=patterns, timeout=timeout)
-        return None
+        return self.expect(patterns=patterns, timeout=timeout)
 
     def send(self, command, patterns=None, timeout=None):
-        self.sendimpl(self.child.send, command, patterns, timeout)
+        return self.sendimpl(self.child.send, command, patterns, timeout)
 
     def sendline(self, command, patterns=None, timeout=None):
-        self.sendimpl(self.child.sendline, command, patterns, timeout)
+        return self.sendimpl(self.child.sendline, command, patterns, timeout)
 
     def quit(self, gracefully=None):
         if gracefully is None: gracefully = True
