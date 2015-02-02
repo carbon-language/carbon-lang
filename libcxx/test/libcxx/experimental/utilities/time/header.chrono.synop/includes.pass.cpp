2f746d4c943a59626925ac625583ddc081b34388
Eric Fiselier <eric@efcs.ca>
2015-02-02 21:05:47 +0000

    [libcxx] Add <experimental/chrono>
    Summary:
    This patch adds <experimental/chrono> which only contains a single variable template.
    
    See: https://rawgit.com/cplusplus/fundamentals-ts/v1/fundamentals-ts.html#time
    
    Reviewers: jroelofs, danalbert, K-ballo, mclow.lists
    
    Reviewed By: mclow.lists
    
    Subscribers: cfe-commits
    
    Differential Revision: http://reviews.llvm.org/D7352
    
    llvm-svn: 227860

---
+++ libcxx/test/libcxx/experimental/utilities/time/header.chrono.synop/includes.pass.cpp
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <experimental/chrono>
+
+#include <experimental/chrono>
+
+#if _LIBCPP_STD_VER > 11
+# ifndef _LIBCPP_CHRONO
+#   error "<experimental/chrono> must include <chrono>"
+# endif
+#endif
+
+int main()
+{
+}
