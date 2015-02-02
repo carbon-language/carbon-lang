b9595b79f22b54fc2cd85a2a1d4040d84f08692a
Marshall Clow <mclow.lists@gmail.com>
2015-02-02 18:16:35 +0000

    Fix PR#22433. The algorithm is_partitioned was testing an item in the middle of the sequence twice.
    llvm-svn: 227824

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.partitions/is_partitioned.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.partitions/is_partitioned.pass.cpp
@@ -17,6 +17,7 @@
 #include <cassert>
 
 #include "test_iterators.h"
+#include "counting_predicates.hpp"
 
 struct is_odd
 {
@@ -27,32 +28,50 @@ int main()
 {
     {
         const int ia[] = {1, 2, 3, 4, 5, 6};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
         assert(!std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
                                     input_iterator<const int*>(std::end(ia)),
-                                    is_odd()));
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::end(ia)));
     }
     {
         const int ia[] = {1, 3, 5, 2, 4, 6};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
         assert( std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
                                     input_iterator<const int*>(std::end(ia)),
-                                    is_odd()));
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::end(ia)));
     }
     {
         const int ia[] = {2, 4, 6, 1, 3, 5};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
         assert(!std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
                                     input_iterator<const int*>(std::end(ia)),
-                                    is_odd()));
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::end(ia)));
     }
     {
         const int ia[] = {1, 3, 5, 2, 4, 6, 7};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
         assert(!std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
                                     input_iterator<const int*>(std::end(ia)),
-                                    is_odd()));
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::end(ia)));
     }
     {
         const int ia[] = {1, 3, 5, 2, 4, 6, 7};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
         assert( std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
                                     input_iterator<const int*>(std::begin(ia)),
-                                    is_odd()));
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::begin(ia)));
+    }
+    {
+        const int ia[] = {1, 3, 5, 7, 9, 11, 2};
+        unary_counting_predicate<is_odd, int> pred((is_odd()));
+        assert( std::is_partitioned(input_iterator<const int*>(std::begin(ia)),
+                                    input_iterator<const int*>(std::end(ia)),
+                                    std::ref(pred)));
+        assert(pred.count() <= std::distance(std::begin(ia), std::end(ia)));
     }
 }
