7bffc89cb9af197c2cbfafd093ca8eee708fba34
Eric Fiselier <eric@efcs.ca>
2015-02-03 16:03:24 +0000

    [libcxx] Add <experimental/system_error>
    Summary:
    This patch just adds the variable templates in <experimental/system_error>.
    
    see: https://rawgit.com/cplusplus/fundamentals-ts/v1/fundamentals-ts.html#syserror
    
    
    Reviewers: jroelofs, danalbert, K-ballo, mclow.lists
    
    Reviewed By: mclow.lists
    
    Subscribers: chandlerc, cfe-commits
    
    Differential Revision: http://reviews.llvm.org/D7353
    
    llvm-svn: 227973

---
+++ libcxx/test/libcxx/experimental/utilities/syserror/header.system_error.synop/includes.pass.cpp
@@ -0,0 +1,22 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <experimental/system_error>
+
+#include <experimental/system_error>
+
+#ifndef _LIBCPP_STD_VER > 11
+# ifndef _LIBCPP_SYSTEM_ERROR
+#   error "<experimental/system_error> must include <system_error>"
+# endif
+#endif
+
+int main()
+{
+}
---
+++ libcxx/test/std/experimental/utilities/syserror/header.system_error.synop/is_error_condition_enum.pass.cpp
@@ -0,0 +1,38 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11
+
+// <experimental/system_error>
+
+// template <class T> constexpr bool is_error_condition_enum_v;
+
+#include <experimental/system_error>
+#include <type_traits>
+namespace ex = std::experimental;
+
+int main() {
+  {
+    static_assert(ex::is_error_condition_enum_v<std::errc>, "");
+
+    static_assert(ex::is_error_condition_enum_v<std::errc> ==
+                  std::is_error_condition_enum<std::errc>::value, "");
+
+    static_assert(
+        std::is_same<decltype(ex::is_error_condition_enum_v<std::errc>),
+                     const bool>::value,
+        "");
+  }
+  {
+    static_assert(!ex::is_error_condition_enum_v<int>, "");
+
+    static_assert(ex::is_error_condition_enum_v<int> ==
+                  std::is_error_condition_enum<int>::value, "");
+  }
+}
