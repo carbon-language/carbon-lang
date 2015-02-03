836a63576433da671135fbfaaf1b3108c1e2383c
Davide Italiano <davide@freebsd.org>
2015-02-03 23:44:33 +0000

    Avoid two function calls of file() when not needed.
    Reported by:	ruiu
    
    llvm-svn: 228069

--- lld/lib/Core/DefinedAtom.cpp
+++ lld/lib/Core/DefinedAtom.cpp
@@ -83,11 +83,14 @@ DefinedAtom::ContentPermissions DefinedAtom::permissions(ContentType type) {
 
 bool DefinedAtom::compareByPosition(const DefinedAtom *lhs,
                                     const DefinedAtom *rhs) {
-  const File *lhsFile = &lhs->file();
-  const File *rhsFile = &rhs->file();
+  const File *lhsFile;
+  const File *rhsFile;
 
   if (lhs == rhs)
     return false;
+
+  lhsFile = &lhs->file();
+  rhsFile = &rhs->file();
 
   if (lhsFile->ordinal() != rhsFile->ordinal())
     return lhsFile->ordinal() < rhsFile->ordinal();
