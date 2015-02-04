1d9b36227a0534e471bb3c90af80246a8d813a0a
Ben Langmuir <blangmuir@apple.com>
2015-02-04 18:34:26 +0000

    Port test/Driver/mg.c to use FileCheck instead of fgrep
    Patch by İsmail Dönmez
    
    llvm-svn: 228164

--- clang/test/Driver/mg.c
+++ clang/test/Driver/mg.c
@@ -1,5 +1,5 @@
-// RUN: %clang -M -MG -include nonexistent-preinclude.h %s > %t
-// RUN: grep -F nonexistent-preinclude.h %t
-// RUN: grep -F nonexistent-ppinclude.h %t
+// RUN: %clang -M -MG -include nonexistent-preinclude.h %s | FileCheck %s
+// CHECK: nonexistent-preinclude.h
+// CHECK: nonexistent-ppinclude.h
 
 #include "nonexistent-ppinclude.h"
