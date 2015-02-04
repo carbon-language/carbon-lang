2c1990778dcd748cc86392aad21197dd8e9b2808
Reid Kleckner <reid@kleckner.net>
2015-02-04 23:17:19 +0000

    Remove useless call to isOSCygMing()
    This used to do something when we modeled the Cygwin and MinGW
    environments as distinct OSs, but now it is not needed.
    
    llvm-svn: 228229

--- llvm/include/llvm/ADT/Triple.h
+++ llvm/include/llvm/ADT/Triple.h
@@ -428,7 +428,7 @@ public:
 
   /// \brief Tests whether the OS is Windows.
   bool isOSWindows() const {
-    return getOS() == Triple::Win32 || isOSCygMing();
+    return getOS() == Triple::Win32;
   }
 
   /// \brief Tests whether the OS is NaCl (Native Client)
