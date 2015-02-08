92545a03dfb8d4360738828ec7dcfcbed5d372fc
Zachary Turner <zturner@google.com>
2015-02-08 00:41:31 +0000

    Removed unused function mistakenly left in, triggering -Werror.
    llvm-svn: 228517

--- llvm/lib/DebugInfo/PDB/PDBSymbolExe.cpp
+++ llvm/lib/DebugInfo/PDB/PDBSymbolExe.cpp
@@ -15,28 +15,7 @@
 #include "llvm/Support/FileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 
-#if defined(_WIN32)
-#include <windows.h>
-#endif
-
 using namespace llvm;
-
-namespace {
-std::string GuidToString(PDB_UniqueId *Id) {
-#if defined(_WIN32)
-  GUID *Guid = reinterpret_cast<GUID *>(Id);
-  OLECHAR GuidBuf[40];
-  int Result = StringFromGUID2(*Guid, GuidBuf, 39);
-  const char *InputBytes = reinterpret_cast<const char *>(GuidBuf);
-  std::string ResultString;
-  convertUTF16ToUTF8String(ArrayRef<char>(InputBytes, Result * 2),
-                           ResultString);
-  return ResultString;
-#else
-  return std::string();
-#endif
-}
-}
 
 PDBSymbolExe::PDBSymbolExe(std::unique_ptr<IPDBRawSymbol> Symbol)
     : PDBSymbol(std::move(Symbol)) {}
