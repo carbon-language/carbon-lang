421f669215f87567301d101221b8d900a7d2e38a
Greg Bedwell <greg_bedwell@sn.scee.net>
2015-02-12 12:41:28 +0000

    Fix dependency generation crash test to run clang and clean up after itself.
    Previously the test did not have a RUN: prefix for the clang command.
    In addition it was leaving behind a tmp file with no permissions causing issues when
    deleting the build directory on Windows.
    
    Differential Revision: http://reviews.llvm.org/D7534
    
    llvm-svn: 228919

--- clang/test/Frontend/dependency-generation-crash.c
+++ clang/test/Frontend/dependency-generation-crash.c
@@ -1,4 +1,8 @@
 // RUN: touch %t
 // RUN: chmod 0 %t
-// %clang -E -dependency-file bla -MT %t -MP -o %t -x c /dev/null
+// RUN: not %clang_cc1 -E -dependency-file bla -MT %t -MP -o %t -x c /dev/null 2>&1 | FileCheck %s
+// RUN: rm -f %t
+
+// CHECK: error: unable to open output file
+
 // rdar://9286457
