f220f88fc9545f85947384658019eaf28e19d185
Timur Iskhodzhanov <timurrrr@google.com>
2015-02-16 15:24:20 +0000

    [ASan/Win] Add an OOM test
    llvm-svn: 229399

---
+++ compiler-rt/test/asan/TestCases/Windows/oom.cc
@@ -0,0 +1,12 @@
+// RUN: %clang_cl_asan -O0 %s -Fe%t
+// RUN: not %run %t 2>&1 | FileCheck %s
+
+#include <malloc.h>
+
+int main() {
+  while (true) {
+    void *ptr = malloc(200 * 1024 * 1024);  // 200MB
+    free(ptr);
+  }
+// CHECK: failed to allocate
+}
