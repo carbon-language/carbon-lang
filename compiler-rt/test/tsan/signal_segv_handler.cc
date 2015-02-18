8870ee77558ddc25c82eaff89b22359957a7f386
Dmitry Vyukov <dvyukov@google.com>
2015-02-18 15:13:29 +0000

    tsan: fix signal handling during stop-the-world
    Long story short: stop-the-world briefly resets SIGSEGV handler to SIG_DFL.
    This breaks programs that handle and continue after SIGSEGV (namely JVM).
    See the test and comments for details.
    
    http://reviews.llvm.org/D7722
    
    llvm-svn: 229678

---
+++ compiler-rt/test/tsan/signal_segv_handler.cc
@@ -0,0 +1,43 @@
+// RUN: %clang_tsan -O1 %s -o %t && TSAN_OPTIONS="flush_memory_ms=1 memory_limit_mb=1" %run %t 2>&1 | FileCheck %s
+
+// JVM uses SEGV to preempt threads. All threads do a load from a known address
+// periodically. When runtime needs to preempt threads, it unmaps the page.
+// Threads start triggering SEGV one by one. The signal handler blocks
+// threads while runtime does its thing. Then runtime maps the page again
+// and resumes the threads.
+// Previously this pattern conflicted with stop-the-world machinery,
+// because it briefly reset SEGV handler to SIG_DFL.
+// As the consequence JVM just silently died.
+
+// This test sets memory flushing rate to maximum, then does series of
+// "benign" SEGVs that are handled by signal handler, and ensures that
+// the process survive.
+
+#include "test.h"
+#include <signal.h>
+#include <sys/mman.h>
+
+void *guard;
+
+void handler(int signo, siginfo_t *info, void *uctx) {
+  mprotect(guard, 4096, PROT_READ | PROT_WRITE);
+}
+
+int main() {
+  // Disabled for now, because there is another issue in ptrace.
+  // It will be addressed in a subsequent change.
+  goto done;
+  struct sigaction a;
+  a.sa_sigaction = handler;
+  a.sa_flags = SA_SIGINFO;
+  sigaction(SIGSEGV, &a, 0);
+  guard = mmap(0, 4096, PROT_NONE, MAP_ANON | MAP_PRIVATE, -1, 0);
+  for (int i = 0; i < 1000000; i++) {
+    mprotect(guard, 4096, PROT_NONE);
+    *(int*)guard = 1;
+  }
+ done:
+  fprintf(stderr, "DONE\n");
+}
+
+// CHECK: DONE
