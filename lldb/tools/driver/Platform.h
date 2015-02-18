019ea8a7c044ab0d5773c1ec2f6de7f8499a28af
Hafiz Abid Qadeer <hafiz_abid@mentor.com>
2015-02-18 11:12:11 +0000

    Fix some warnings in MSVC build. Platform.h was causing soem warning due to multiple defined maros. It already has a define that excludes the singal.h but that was not being used at correct place. It has been fixed now.
    Tested by building on Windows(MSVC) and Linux(gcc).
    
    llvm-svn: 229664

--- lldb/tools/driver/Platform.h
+++ lldb/tools/driver/Platform.h
@@ -10,12 +10,11 @@
 #ifndef lldb_Platform_h_
 #define lldb_Platform_h_
 
-#include "lldb/Host/HostGetOpt.h"
-
 #if defined( _WIN32 )
 
     // this will stop signal.h being included
     #define _INC_SIGNAL
+    #include "lldb/Host/HostGetOpt.h"
     #include <io.h>
 #if defined( _MSC_VER )
     #include <eh.h>
@@ -93,7 +92,7 @@
     extern int  tcgetattr( int fildes, struct termios *termios_p );
 
 #else
-
+    #include "lldb/Host/HostGetOpt.h"
     #include <inttypes.h>
 
     #include <libgen.h>
