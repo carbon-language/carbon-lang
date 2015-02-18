67bf4bd3161fe335da6d7918d00b66828d6f9b92
Renato Golin <renato.golin@linaro.org>
2015-02-18 20:30:32 +0000

    Revert "Enable ASAN build and test on AArch64"
    This reverts commit r229665. It seems that the AArch64 ASAN tests, that
    pass on all our internal machines, doesn't like the public buildbot.
    
    Turning this off until we can investigate the public bot for a better
    understanding.
    
    llvm-svn: 229739

--- compiler-rt/cmake/config-ix.cmake
+++ compiler-rt/cmake/config-ix.cmake
@@ -197,7 +197,7 @@ endfunction()
 filter_available_targets(SANITIZER_COMMON_SUPPORTED_ARCH
   x86_64 i386 i686 powerpc64 powerpc64le arm aarch64 mips mips64 mipsel mips64el)
 filter_available_targets(ASAN_SUPPORTED_ARCH
-  x86_64 i386 i686 powerpc64 powerpc64le arm aarch64 mips mipsel mips64 mips64el)
+  x86_64 i386 i686 powerpc64 powerpc64le arm mips mipsel mips64 mips64el)
 filter_available_targets(DFSAN_SUPPORTED_ARCH x86_64 mips64 mips64el)
 filter_available_targets(LSAN_SUPPORTED_ARCH x86_64)
 # LSan common files should be available on all architectures supported
