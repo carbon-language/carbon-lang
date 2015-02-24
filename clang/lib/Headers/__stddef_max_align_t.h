a586ea13a41a0c4ce0a3f773bb975a05a22d07ea
Dmitri Gribenko <gribozavr@gmail.com>
2015-02-24 01:06:22 +0000

    Restore the libc++ definition of max_align_t on Apple platforms
    Clang has introduced ::max_align_t in stddef.h in r201729, but libc++ was
    already defining std::max_align_t on Darwin because there was none in the
    global namespace.  After that Clang commit though, libc++ started defining
    std::max_align_t to be a typedef for ::max_align_t, which has a different
    definition.  This changed the ABI.  This commit restores the previous
    definition.
    
    rdar://19919394 rdar://18557982
    
    llvm-svn: 230292

--- clang/lib/Headers/__stddef_max_align_t.h
+++ clang/lib/Headers/__stddef_max_align_t.h
@@ -26,15 +26,18 @@
 #ifndef __CLANG_MAX_ALIGN_T_DEFINED
 #define __CLANG_MAX_ALIGN_T_DEFINED
 
-#ifndef _MSC_VER
+#if defined(_MSC_VER)
+typedef double max_align_t;
+#elif defined(__APPLE__)
+typedef long double max_align_t;
+#else
+// Define 'max_align_t' to match the GCC definition.
 typedef struct {
   long long __clang_max_align_nonce1
       __attribute__((__aligned__(__alignof__(long long))));
   long double __clang_max_align_nonce2
       __attribute__((__aligned__(__alignof__(long double))));
 } max_align_t;
-#else
-typedef double max_align_t;
 #endif
 
 #endif
