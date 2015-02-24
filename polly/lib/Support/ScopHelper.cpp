1ddb91d6f443cd66a80471e99f150977a4bc408c
Johannes Doerfert <doerfert@cs.uni-saarland.de>
2015-02-24 16:00:41 +0000

    [FIX] Silence warning
    llvm-svn: 230336

--- polly/lib/Support/ScopHelper.cpp
+++ polly/lib/Support/ScopHelper.cpp
@@ -170,6 +170,7 @@ BasicBlock *polly::simplifyRegion(Scop *S, Pass *P) {
   // Create single exit edge if the region has multiple exit edges.
   if (!R->getExitingBlock()) {
     BasicBlock *NewExiting = createSingleExitEdge(R, P);
+    (void) NewExiting;
     assert(NewExiting == R->getExitingBlock() &&
            "Did not create a single exiting block");
   }
