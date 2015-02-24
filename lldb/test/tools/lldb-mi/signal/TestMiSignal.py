ff115f0ea4a5e19b055018356297591912cd11af
Ilia K <ki.stfu@gmail.com>
2015-02-24 17:54:34 +0000

    Add comment in MiSignalTestCase.test_lldbmi_stopped_when_stopatentry_local back and revert expecting string.
    llvm-svn: 230351

--- lldb/test/tools/lldb-mi/signal/TestMiSignal.py
+++ lldb/test/tools/lldb-mi/signal/TestMiSignal.py
@@ -64,7 +64,10 @@ class MiSignalTestCase(lldbmi_testcase.MiTestCaseBase):
         self.expect("\^done")
 
         # Test that *stopped is printed
-        self.expect("\*stopped,reason=\"(signal-received\",signal-name=\"SIGINT\",signal-meaning=\"Interrupt\",.*)|(end-stepping-range\"),.*thread-id=\"1\",stopped-threads=\"all\"")
+        # Note that message is different in Darwin and Linux:
+        # Darwin: "*stopped,reason=\"signal-received\",signal-name=\"SIGINT\",signal-meaning=\"Interrupt\",frame={level=\"0\",addr=\"0x[0-9a-f]+\",func=\"_dyld_start\",file=\"??\",fullname=\"??\",line=\"-1\"},thread-id=\"1\",stopped-threads=\"all\"
+        # Linux:  "*stopped,reason=\"end-stepping-range\",frame={addr=\"0x[0-9a-f]+\",func=\"??\",args=\[\],file=\"??\",fullname=\"??\",line=\"-1\"},thread-id=\"1\",stopped-threads=\"all\"
+        self.expect("\*stopped,reason=\"(signal-received|end-stepping-range)\",.+,thread-id=\"1\",stopped-threads=\"all\"")
 
         # Run to main to make sure we have not exited the application
         self.runCmd("-break-insert -f main")
