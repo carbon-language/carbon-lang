f32d651df6f1688350bbaa3df19fc7f61be87f31
Johannes Doerfert <doerfert@cs.uni-saarland.de>
2015-03-01 18:45:58 +0000

    [Refactor] Include explicitly what is used
    llvm-svn: 230901

--- polly/lib/CodeGen/BlockGenerators.cpp
+++ polly/lib/CodeGen/BlockGenerators.cpp
@@ -14,10 +14,6 @@
 //===----------------------------------------------------------------------===//
 
 #include "polly/ScopInfo.h"
-#include "isl/aff.h"
-#include "isl/ast.h"
-#include "isl/ast_build.h"
-#include "isl/set.h"
 #include "polly/CodeGen/BlockGenerators.h"
 #include "polly/CodeGen/CodeGeneration.h"
 #include "polly/CodeGen/IslExprBuilder.h"
@@ -25,11 +21,21 @@
 #include "polly/Support/GICHelper.h"
 #include "polly/Support/SCEVValidator.h"
 #include "polly/Support/ScopHelper.h"
+
 #include "llvm/Analysis/LoopInfo.h"
+#include "llvm/Analysis/RegionInfo.h"
 #include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Analysis/ScalarEvolutionExpander.h"
+
 #include "llvm/IR/IntrinsicInst.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+
+#include "isl/aff.h"
+#include "isl/ast.h"
+#include "isl/set.h"
+#include "isl/ast_build.h"
+
+#include <deque>
 
 using namespace llvm;
 using namespace polly;
