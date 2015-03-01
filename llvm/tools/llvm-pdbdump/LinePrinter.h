98162469f50b7b21f315c71f23b28d85b9ba32f0
Zachary Turner <zturner@google.com>
2015-03-01 06:59:57 +0000

    [llvm-pdbdump] Fix GCC build.
    llvm-svn: 230890

--- llvm/tools/llvm-pdbdump/LinePrinter.h
+++ llvm/tools/llvm-pdbdump/LinePrinter.h
@@ -10,6 +10,7 @@
 #ifndef LLVM_TOOLS_LLVMPDBDUMP_LINEPRINTER_H
 #define LLVM_TOOLS_LLVMPDBDUMP_LINEPRINTER_H
 
+#include "llvm/ADT/StringRef.h"
 #include "llvm/ADT/Twine.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Support/Regex.h"
@@ -25,13 +26,19 @@ public:
   LinePrinter(int Indent, raw_ostream &Stream);
 
   template <typename Iter> void SetTypeFilters(Iter Begin, Iter End) {
-    TypeFilters.assign(Begin, End);
+    TypeFilters.clear();
+    for (; Begin != End; ++Begin)
+      TypeFilters.push_back(StringRef(*Begin));
   }
   template <typename Iter> void SetSymbolFilters(Iter Begin, Iter End) {
-    SymbolFilters.assign(Begin, End);
+    SymbolFilters.clear();
+    for (; Begin != End; ++Begin)
+      SymbolFilters.push_back(StringRef(*Begin));
   }
   template <typename Iter> void SetCompilandFilters(Iter Begin, Iter End) {
-    CompilandFilters.assign(Begin, End);
+    CompilandFilters.clear();
+    for (; Begin != End; ++Begin)
+      CompilandFilters.push_back(StringRef(*Begin));
   }
 
   void Indent();
