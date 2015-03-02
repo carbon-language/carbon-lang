1f72a0006ca727eab0d279fa9e08855706e4efc3
Filipe Cabecinhas <me@filcab.net>
2015-03-02 19:11:53 +0000

    Add -fexceptions for targets where it isn't the default.
    It still tests that objc++-cpp-output turns on -fcxx-exceptions and
    -fobjc-exceptions by being a c++ and objc(++) file.
    
    llvm-svn: 230992

--- clang/test/Driver/objc++-cpp-output.mm
+++ clang/test/Driver/objc++-cpp-output.mm
@@ -1,5 +1,5 @@
-// RUN: %clang -emit-llvm -x objc++-cpp-output -S %s -o /dev/null
-// RUN: %clang -emit-llvm -x objc++-cpp-output -S %s -o /dev/null -### 2>&1 | FileCheck %s
+// RUN: %clang -fexceptions -emit-llvm -x objc++-cpp-output -S %s -o /dev/null
+// RUN: %clang -fexceptions -emit-llvm -x objc++-cpp-output -S %s -o /dev/null -### 2>&1 | FileCheck %s
 
 // PR13820
 // REQUIRES: LP64
