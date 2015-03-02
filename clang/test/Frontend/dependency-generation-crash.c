e0fc8cabda04c31623c836f46db8be85ab65b8c3
Matthias Braun <matze@braunis.de>
2015-03-02 20:44:09 +0000

    Improve robustness of dependency-generation-crash.c test.
    The test wants to provoke a failure when opening the output file.
    Using chmod 0 on the output file does not work reliably on all
    filesystems or when running the test as root.
    Change the test to use a nonexistant directory instead.
    
    Differential Revision: http://reviews.llvm.org/D7620
    
    llvm-svn: 231009

--- clang/test/Frontend/dependency-generation-crash.c
+++ clang/test/Frontend/dependency-generation-crash.c
@@ -1,7 +1,4 @@
-// RUN: touch %t
-// RUN: chmod 0 %t
-// RUN: not %clang_cc1 -E -dependency-file bla -MT %t -MP -o %t -x c /dev/null 2>&1 | FileCheck %s
-// RUN: rm -f %t
+// RUN: not %clang_cc1 -E -dependency-file bla -MT %t/doesnotexist/bla.o -MP -o $t/doesnotexist/bla.o -x c /dev/null 2>&1 | FileCheck %s
 
 // CHECK: error: unable to open output file
 
