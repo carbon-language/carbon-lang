3cb611b59efcc4a7186a7b2a7279589618bbb67e
Zachary Turner <zturner@google.com>
2015-03-02 18:33:50 +0000

    [Windows] Fix getcwd() on Windows.
    Windows doesn't define getcwd(), so this test executable failed
    to compile.
    
    Patch by Adrian McCarthy
    Differential Revision: http://reviews.llvm.org/D7962
    
    llvm-svn: 230983

--- lldb/test/functionalities/process_launch/print_cwd.cpp
+++ lldb/test/functionalities/process_launch/print_cwd.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
+
+#ifdef _MSC_VER
+#define _CRT_NONSTDC_NO_WARNINGS
+#include <direct.h>
+#undef getcwd
+#define getcwd(buffer, length) _getcwd(buffer, length)
+#else
 #include <unistd.h>
+#endif
 
 int
 main (int argc, char **argv)
