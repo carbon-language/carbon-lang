1859025df8c836c55fd5beb3bd705256c4ce4bad
Reid Kleckner <reid@kleckner.net>
2015-03-03 17:51:26 +0000

    Disable a Clang test until the begincatch change lands
    llvm-svn: 231087

--- clang/test/CodeGenCXX/microsoft-abi-try-throw.cpp
+++ clang/test/CodeGenCXX/microsoft-abi-try-throw.cpp
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -emit-llvm %s -o - -triple=i386-pc-win32 -mconstructor-aliases -fcxx-exceptions -fexceptions -fno-rtti -DTRY   | FileCheck %s -check-prefix=TRY
-// RUN: %clang_cc1 -emit-llvm %s -o - -triple=i386-pc-win32 -mconstructor-aliases -fcxx-exceptions -fexceptions -fno-rtti -DTHROW | FileCheck %s -check-prefix=THROW
+// FIXME: Disabled until catch IRgen change lands.
+// RUNX: %clang_cc1 -emit-llvm %s -o - -triple=i386-pc-win32 -mconstructor-aliases -fcxx-exceptions -fexceptions -fno-rtti -DTHROW | FileCheck %s -check-prefix=THROW
 
 void external();
 
