cbdf3b79d4f155f75018575d2487ac2ff350ef92
Daniel Jasper <djasper@google.com>
2015-03-04 15:02:22 +0000

    Revert "[IAS] Teach -cc1as about the 'target-abi' option."
    This reverts commit 0e41c8faeff75614cf4627533331d780ba3db030.
    
    This is breaking buildbots:
    http://lab.llvm.org:8080/green/job/clang-stage1-configure-RA_check/4668/
    
    llvm-svn: 231248

--- clang/test/Driver/target-abi-cc1as.s
+++
@@ -1,5 +0,0 @@
-// Check if -cc1as knows about the 'target-abi' argument.
-
-// RUN: %clang -cc1as -triple mips--linux-gnu -filetype obj -target-cpu mips32 -target-abi o32 %s 2>&1 | \
-// RUN:   FileCheck --check-prefix=ABI-O32 %s
-// ABI-O32-NOT: clang -cc1as: error: unknown argument: '-target-abi'
