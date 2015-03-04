8dc4a2a3be60e0c6d01160624296ce35aadfc5e0
Daniel Jasper <djasper@google.com>
2015-03-04 15:02:17 +0000

    Prevent test from writing files.
    llvm-svn: 231247

--- clang/test/CodeGen/builtin-longjmp.c
+++ clang/test/CodeGen/builtin-longjmp.c
@@ -3,10 +3,10 @@
 // RUN: %clang_cc1 -triple powerpc-unknown-unknown -emit-llvm < %s| FileCheck %s
 // RUN: %clang_cc1 -triple powerpc64-unknown-unknown -emit-llvm < %s| FileCheck %s
 
-// RUN: %clang_cc1 -triple arm-unknown-unknown -emit-llvm -verify %s
-// RUN: %clang_cc1 -triple aarch64-unknown-unknown -emit-llvm -verify %s
-// RUN: %clang_cc1 -triple mips-unknown-unknown -emit-llvm -verify %s
-// RUN: %clang_cc1 -triple mips64-unknown-unknown -emit-llvm -verify %s
+// RUN: %clang_cc1 -triple arm-unknown-unknown -emit-llvm-only -verify %s
+// RUN: %clang_cc1 -triple aarch64-unknown-unknown -emit-llvm-only -verify %s
+// RUN: %clang_cc1 -triple mips-unknown-unknown -emit-llvm-only -verify %s
+// RUN: %clang_cc1 -triple mips64-unknown-unknown -emit-llvm-only -verify %s
 
 // Check that __builtin_longjmp and __builtin_setjmp are lowerd into
 // IR intrinsics on those architectures that can handle them.
