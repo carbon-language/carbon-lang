1b625ddc0d9bade0c6a0f798a9ea81c918e7ce3f
Andrey Churbanov <Andrey.Churbanov@intel.com>
2015-03-05 17:50:48 +0000

    moved Windows-specific flags under the WINDOWS guard in CMake
    llvm-svn: 231384

--- openmp/runtime/cmake/Intel/AsmFlags.cmake
+++ openmp/runtime/cmake/Intel/AsmFlags.cmake
@@ -18,18 +18,18 @@
 function(append_assembler_specific_asm_flags input_asm_flags)
     set(local_asm_flags)
     append_asm_flags("-x assembler-with-cpp") # Assembly file that needs to be preprocessed
-    if(${IA32})
-        append_asm_flags("-safeseh") # Registers exception handlers for safe exception handling.
-        append_asm_flags("-coff") # Generates common object file format (COFF) type of object module. 
-                                  # Generally required for Win32 assembly language development.
-        append_asm_flags("-D _M_IA32")
-    elseif(${INTEL64})
-        append_asm_flags("-D _M_AMD64")
-    endif()
     if(${MIC})
         append_asm_flags("-mmic") # Build Intel(R) MIC Architecture native code
     endif()
     if(${WINDOWS})
+        if(${IA32})
+            append_asm_flags("-safeseh") # Registers exception handlers for safe exception handling.
+            append_asm_flags("-coff") # Generates common object file format (COFF) type of object module. 
+                                      # Generally required for Win32 assembly language development.
+            append_asm_flags("-D _M_IA32")
+        elseif(${INTEL64})
+            append_asm_flags("-D _M_AMD64")
+        endif()
         # CMake prefers the /MD flags when compiling Windows sources, but libiomp5 needs to use /MT instead
         # So we replace these /MD instances with /MT within the CMAKE_*_FLAGS variables and put that out to the CACHE.
         # replace_md_with_mt() is in HelperFunctions.cmake
