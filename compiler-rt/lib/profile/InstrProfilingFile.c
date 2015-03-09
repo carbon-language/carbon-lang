ef24b4171d8a2ce8eb626014301912746fc7cd16
Joerg Sonnenberger <joerg@bec.de>
2015-03-09 11:23:29 +0000

    At least on NetBSD, sys/errno.h and errno.h are different, so use the correct header to get errno.
    llvm-svn: 231647

--- compiler-rt/lib/profile/InstrProfilingFile.c
+++ compiler-rt/lib/profile/InstrProfilingFile.c
@@ -8,10 +8,10 @@
 \*===----------------------------------------------------------------------===*/
 
 #include "InstrProfiling.h"
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/errno.h>
 
 #define UNCONST(ptr) ((void *)(uintptr_t)(ptr))
 
