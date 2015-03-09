1d2c052e9f09144cc1a837e135cbf1982d6b4b9f
Ed Schouten <ed@80386.nl>
2015-03-09 12:04:16 +0000

    Add CloudABI locale names to platform_support.h.
    On CloudABI we should append the timezone name to the end of the locale
    (e.g., nl_NL.UTF-8@Europe/Amsterdam). By fixing the locale names we can
    already let a lot of locale related tests pass.
    
    llvm-svn: 231649

--- libcxx/test/support/platform_support.h
+++ libcxx/test/support/platform_support.h
@@ -25,6 +25,16 @@
 #define LOCALE_fr_CA_ISO8859_1 "French_Canada.1252"
 #define LOCALE_ru_RU_UTF_8     "Russian_Russia.1251"
 #define LOCALE_zh_CN_UTF_8     "Chinese_China.936"
+#elif defined(__CloudABI__)
+// Timezones are integrated into locales through LC_TIMEZONE_MASK on
+// CloudABI. LC_ALL_MASK can only be used if a timezone has also been
+// provided. UTC should be all right.
+#define LOCALE_en_US_UTF_8     "en_US.UTF-8@UTC"
+#define LOCALE_fr_FR_UTF_8     "fr_FR.UTF-8@UTC"
+#define LOCALE_fr_CA_ISO8859_1 "fr_CA.ISO-8859-1@UTC"
+#define LOCALE_cs_CZ_ISO8859_2 "cs_CZ.ISO-8859-2@UTC"
+#define LOCALE_ru_RU_UTF_8     "ru_RU.UTF-8@UTC"
+#define LOCALE_zh_CN_UTF_8     "zh_CN.UTF-8@UTC"
 #else
 #define LOCALE_en_US_UTF_8     "en_US.UTF-8"
 #define LOCALE_fr_FR_UTF_8     "fr_FR.UTF-8"
