cda27e8197fc15da162a445b3aaedff5a006a462
Ed Schouten <ed@80386.nl>
2015-03-10 08:01:10 +0000

    Don't include <sys/sysctl.h> on CloudABI.
    As CloudABI does not provide sysctl(), this header is not present. Make
    thread.cpp build correctly (and pass all tests) by not including the header.
    
    llvm-svn: 231768

--- libcxx/src/thread.cpp
+++ libcxx/src/thread.cpp
@@ -17,9 +17,9 @@
 #include "limits"
 #include <sys/types.h>
 #if !defined(_WIN32)
-# if !defined(__sun__) && !defined(__linux__) && !defined(_AIX) && !defined(__native_client__)
+# if !defined(__sun__) && !defined(__linux__) && !defined(_AIX) && !defined(__native_client__) && !defined(__CloudABI__)
 #   include <sys/sysctl.h>
-# endif // !defined(__sun__) && !defined(__linux__) && !defined(_AIX) && !defined(__native_client__)
+# endif // !defined(__sun__) && !defined(__linux__) && !defined(_AIX) && !defined(__native_client__) && !defined(__CloudABI__)
 # include <unistd.h>
 #endif // !_WIN32
 
