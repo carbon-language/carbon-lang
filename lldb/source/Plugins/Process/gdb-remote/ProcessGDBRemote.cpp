a55a953a29ebfc3f960531c2bcea3400d4cce4fe
Pavel Labath <labath@google.com>
2015-03-11 09:53:42 +0000

    Fix bug in ProcessGDBRemote
    Summary:
    ProcessGDBRemote::AsyncThread nuked its own thread handle upon exiting. This prevented the main
    thread from joining it correctly in StopAsyncThread. I address this by moving the Reset() call to
    StopAsyncThread, after the join.
    
    Reviewers: clayborg, zturner
    
    Subscribers: lldb-commits
    
    Differential Revision: http://reviews.llvm.org/D8218
    
    llvm-svn: 231915

--- lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.cpp
+++ lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.cpp
@@ -3025,6 +3025,7 @@ ProcessGDBRemote::StopAsyncThread ()
 
         // Stop the stdio thread
         m_async_thread.Join(nullptr);
+        m_async_thread.Reset();
     }
     else if (log)
         log->Printf("ProcessGDBRemote::%s () - Called when Async thread was not running.", __FUNCTION__);
@@ -3169,7 +3170,6 @@ ProcessGDBRemote::AsyncThread (void *arg)
     if (log)
         log->Printf ("ProcessGDBRemote::%s (arg = %p, pid = %" PRIu64 ") thread exiting...", __FUNCTION__, arg, process->GetID());
 
-    process->m_async_thread.Reset();
     return NULL;
 }
 
