e849103f5afb660089984d0931a8eafb1369dae8
Vince Harron <vince@nethacker.com>
2015-03-12 20:07:13 +0000

    Test that software breakpoints aren't visible in disassembly
    Linux lldb-server Handle_m doesn't properly replace software breakpoints
    with the original instructions. This test is added with expectedFailureLinux
    
    Differential Revision: http://reviews.llvm.org/D8191
    
    llvm-svn: 232091

---
+++ lldb/test/functionalities/disassembly/TestDisassembleBreakpoint.py
@@ -0,0 +1,60 @@
+"""
+Test some lldb command abbreviations.
+"""
+
+import os, time
+import unittest2
+import lldb
+from lldbtest import *
+import lldbutil
+
+class DisassemblyTestCase(TestBase):
+
+    mydir = TestBase.compute_mydir(__file__)
+
+    @unittest2.skipUnless(sys.platform.startswith("darwin"), "requires Darwin")
+    @dsym_test
+    def test_with_dsym (self):
+        self.buildDsym ()
+        self.disassemble_breakpoint ()
+
+    @dwarf_test
+    @expectedFailureLinux # llgs Handle_m returns target memory with breakpoints
+    def test_with_dwarf (self):
+        self.buildDwarf ()
+        self.disassemble_breakpoint ()
+
+    def disassemble_breakpoint (self):
+        exe = os.path.join (os.getcwd(), "a.out")
+        self.expect("file " + exe,
+                    patterns = [ "Current executable set to .*a.out.*" ])
+
+        match_object = lldbutil.run_break_set_command (self, "br s -n sum")
+        lldbutil.check_breakpoint_result (self, match_object, symbol_name='sum', symbol_match_exact=False, num_locations=1)
+
+        self.expect("run",
+                    patterns = [ "Process .* launched: "])
+
+        self.runCmd("dis -f")
+        disassembly = self.res.GetOutput()
+
+        # ARCH, if not specified, defaults to x86_64.
+        if self.getArchitecture() in ["", 'x86_64', 'i386']:
+            # make sure that the software breakpoint has been removed
+            self.assertFalse("int3" in disassembly)
+            # make sure a few reasonable assembly instructions are here
+            self.expect(disassembly, exe=False,
+                        startstr = "a.out`sum(int, int)",
+                        substrs = [' mov',
+                                   ' addl ',
+                                   'ret'])
+        else:
+            # TODO please add your arch here
+            self.fail('unimplemented for arch = "{arch}"'.format(arch=self.getArchitecture()))
+
+if __name__ == '__main__':
+    import atexit
+    lldb.SBDebugger.Initialize()
+    atexit.register(lambda: lldb.SBDebugger.Terminate())
+    unittest2.main()
+
---
+++ lldb/test/functionalities/disassembly/main.cpp
@@ -0,0 +1,28 @@
+//===-- main.cpp ------------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+int
+sum (int a, int b)
+{
+    int result = a + b;
+    return result;
+}
+
+int
+main(int argc, char const *argv[])
+{
+
+    int array[3];
+
+    array[0] = sum (1238, 78392);
+    array[1] = sum (379265, 23674);
+    array[2] = sum (872934, 234);
+
+    return 0;
+}
