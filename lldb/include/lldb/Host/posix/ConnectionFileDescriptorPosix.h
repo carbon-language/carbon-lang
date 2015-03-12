2aa191c8781671d76cfc2dfde99f79a413bad5d9
Enrico Granata <egranata@apple.com>
2015-03-12 22:33:52 +0000

    Add override markers
    llvm-svn: 232116

--- lldb/include/lldb/Host/posix/ConnectionFileDescriptorPosix.h
+++ lldb/include/lldb/Host/posix/ConnectionFileDescriptorPosix.h
@@ -40,17 +40,17 @@ class ConnectionFileDescriptor : public Connection
 
     virtual ~ConnectionFileDescriptor();
 
-    virtual bool IsConnected() const;
+    bool IsConnected() const override;
 
-    virtual lldb::ConnectionStatus Connect(const char *s, Error *error_ptr);
+    lldb::ConnectionStatus Connect(const char *s, Error *error_ptr) override;
 
-    virtual lldb::ConnectionStatus Disconnect(Error *error_ptr);
+    lldb::ConnectionStatus Disconnect(Error *error_ptr) override;
 
-    virtual size_t Read(void *dst, size_t dst_len, uint32_t timeout_usec, lldb::ConnectionStatus &status, Error *error_ptr);
+    size_t Read(void *dst, size_t dst_len, uint32_t timeout_usec, lldb::ConnectionStatus &status, Error *error_ptr) override;
 
-    virtual size_t Write(const void *src, size_t src_len, lldb::ConnectionStatus &status, Error *error_ptr);
+    size_t Write(const void *src, size_t src_len, lldb::ConnectionStatus &status, Error *error_ptr) override;
 
-    virtual std::string GetURI();
+    std::string GetURI() override;
 
     lldb::ConnectionStatus BytesAvailable(uint32_t timeout_usec, Error *error_ptr);
 
