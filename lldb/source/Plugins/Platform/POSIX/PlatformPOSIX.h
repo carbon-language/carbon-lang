2463d982b46b78ba77632f35b311ce2b4dfd2e02
Greg Clayton <gclayton@apple.com>
2015-03-12 00:17:08 +0000

    Remove "virtual" and add "override" to all virtual functions.
    llvm-svn: 231991

--- lldb/source/Plugins/Platform/POSIX/PlatformPOSIX.h
+++ lldb/source/Plugins/Platform/POSIX/PlatformPOSIX.h
@@ -32,12 +32,12 @@ public:
     // lldb_private::Platform functions
     //------------------------------------------------------------
 
-    virtual bool
+    bool
     GetModuleSpec (const lldb_private::FileSpec& module_file_spec,
                    const lldb_private::ArchSpec& arch,
-                   lldb_private::ModuleSpec &module_spec);
+                   lldb_private::ModuleSpec &module_spec) override;
 
-    virtual lldb_private::OptionGroupOptions
+    lldb_private::OptionGroupOptions
     *GetConnectionOptions(
         lldb_private::CommandInterpreter &interpreter) override;
 
@@ -50,50 +50,50 @@ public:
     const char *
     GetGroupName (uint32_t gid) override;
 
-    virtual lldb_private::Error
+    lldb_private::Error
     PutFile (const lldb_private::FileSpec& source,
              const lldb_private::FileSpec& destination,
              uint32_t uid = UINT32_MAX,
              uint32_t gid = UINT32_MAX) override;
     
-    virtual lldb::user_id_t
+    lldb::user_id_t
     OpenFile (const lldb_private::FileSpec& file_spec,
               uint32_t flags,
               uint32_t mode,
               lldb_private::Error &error) override;
     
-    virtual bool
+    bool
     CloseFile (lldb::user_id_t fd,
                lldb_private::Error &error) override;
     
-    virtual uint64_t
+    uint64_t
     ReadFile (lldb::user_id_t fd,
               uint64_t offset,
               void *dst,
               uint64_t dst_len,
               lldb_private::Error &error) override;
     
-    virtual uint64_t
+    uint64_t
     WriteFile (lldb::user_id_t fd,
                uint64_t offset,
                const void* src,
                uint64_t src_len,
                lldb_private::Error &error) override;
     
-    virtual lldb::user_id_t
+    lldb::user_id_t
     GetFileSize (const lldb_private::FileSpec& file_spec) override;
 
-    virtual lldb_private::Error
+    lldb_private::Error
     CreateSymlink(const char *src, const char *dst) override;
 
-    virtual lldb_private::Error
+    lldb_private::Error
     GetFile (const lldb_private::FileSpec& source,
              const lldb_private::FileSpec& destination) override;
     
-    virtual lldb_private::ConstString
+    lldb_private::ConstString
     GetRemoteWorkingDirectory() override;
     
-    virtual bool
+    bool
     SetRemoteWorkingDirectory(const lldb_private::ConstString &path) override;
 
     bool
@@ -114,7 +114,7 @@ public:
     bool
     IsConnected () const override;
 
-    virtual lldb_private::Error
+    lldb_private::Error
     RunShellCommand (const char *command,           // Shouldn't be NULL
                      const char *working_dir,       // Pass NULL to use the current working directory
                      int *status_ptr,               // Pass NULL if you don't want the process exit status
@@ -122,19 +122,19 @@ public:
                      std::string *command_output,   // Pass NULL if you don't want the command output
                      uint32_t timeout_sec) override;// Timeout in seconds to wait for shell program to finish
 
-    virtual lldb_private::Error
+    lldb_private::Error
     MakeDirectory (const char *path, uint32_t mode) override;
     
-    virtual lldb_private::Error
+    lldb_private::Error
     GetFilePermissions (const char *path, uint32_t &file_permissions) override;
 
-    virtual lldb_private::Error
+    lldb_private::Error
     SetFilePermissions (const char *path, uint32_t file_permissions) override;
 
-    virtual bool
+    bool
     GetFileExists (const lldb_private::FileSpec& file_spec) override;
     
-    virtual lldb_private::Error
+    lldb_private::Error
     Unlink (const char *path) override;
 
     lldb_private::Error
@@ -155,15 +155,15 @@ public:
                   lldb_private::Target *target,       // Can be NULL, if NULL create a new target, else use existing one
                   lldb_private::Error &error) override;
 
-    virtual std::string
+    std::string
     GetPlatformSpecificConnectionInformation() override;
     
-    virtual bool
+    bool
     CalculateMD5 (const lldb_private::FileSpec& file_spec,
                   uint64_t &low,
                   uint64_t &high) override;
 
-    virtual void
+    void
     CalculateTrapHandlerSymbolNames () override;
 
     lldb_private::Error
