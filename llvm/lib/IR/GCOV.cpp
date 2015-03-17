d0a6243529c23e79c3d9a033cf977c0f7df43ead
Justin Bogner <mail@justinbogner.com>
2015-03-17 00:18:51 +0000

    llvm-cov: Warn instead of error if a .gcda has arcs from an exit block
    Patch by Vanderson M. Rosario. Thanks!
    
    llvm-svn: 232443

--- llvm/lib/IR/GCOV.cpp
+++ llvm/lib/IR/GCOV.cpp
@@ -302,10 +302,12 @@ bool GCOVFunction::readGCDA(GCOVBuffer &Buff, GCOV::GCOVVersion Version) {
   // required to combine the edge counts that are contained in the GCDA file.
   for (uint32_t BlockNo = 0; Count > 0; ++BlockNo) {
     // The last block is always reserved for exit block
-    if (BlockNo >= Blocks.size() - 1) {
+    if (BlockNo >= Blocks.size()) {
       errs() << "Unexpected number of edges (in " << Name << ").\n";
       return false;
     }
+    if (BlockNo == Blocks.size() - 1)
+      errs() << "(" << Name << ") has arcs from exit block.\n";
     GCOVBlock &Block = *Blocks[BlockNo];
     for (size_t EdgeNo = 0, End = Block.getNumDstEdges(); EdgeNo < End;
          ++EdgeNo) {
