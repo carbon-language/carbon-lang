7523ec4f47e2d61532d0147a979947d243d9c65a
Siva Chandra <sivachandra@google.com>
2015-03-17 21:43:01 +0000

    Enable TestDataFormatterStdIterator with GCC.
    Summary:
    This test now passes for Clang and GCC. I do not know why it was
    disabled for GCC with link to a bug which should not have had an
    effect on this test.
    
    Test Plan: dotest.py -C gcc -p TestDataFormatterStdIterator
    
    Reviewers: vharron
    
    Subscribers: lldb-commits
    
    Differential Revision: http://reviews.llvm.org/D8396
    
    llvm-svn: 232552

--- lldb/test/functionalities/data-formatter/data-formatter-stl/libstdcpp/iterator/TestDataFormatterStdIterator.py
+++ lldb/test/functionalities/data-formatter/data-formatter-stl/libstdcpp/iterator/TestDataFormatterStdIterator.py
@@ -21,7 +21,6 @@ class StdIteratorDataFormatterTestCase(TestBase):
 
     @dwarf_test
     @expectedFailureFreeBSD("llvm.org/pr20548") # fails to build on lab.llvm.org buildbot
-    @expectedFailureGcc # llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
     @expectedFailureIcc # llvm.org/pr15301 LLDB prints incorrect sizes of STL containers
     def test_with_dwarf_and_run_command(self):
         """Test data formatter commands."""
