28aae9c29b6dd740c4d8da4b7019f2b83bc446b2
David Majnemer <david.majnemer@gmail.com>
2015-03-18 04:15:23 +0000

    MSVC Compat: Permit char16_t, char32_t and _Atomic when targeting > 2013
    We disabled support for _Atomic because the STL had name conflicts,
    they've been resolved in 2015.  Similarly, reenable char16_t and
    char32_t.
    
    llvm-svn: 232611

--- clang/lib/Basic/IdentifierTable.cpp
+++ clang/lib/Basic/IdentifierTable.cpp
@@ -105,10 +105,10 @@ namespace {
     KEYOPENCL = 0x200,
     KEYC11 = 0x400,
     KEYARC = 0x800,
-    KEYNOMS = 0x01000,
+    KEYNOMS18 = 0x01000,
     WCHARSUPPORT = 0x02000,
     HALFSUPPORT = 0x04000,
-    KEYALL = (0xffff & ~KEYNOMS) // Because KEYNOMS is used to exclude.
+    KEYALL = (0xffff & ~KEYNOMS18) // Because KEYNOMS18 is used to exclude.
   };
 
   /// \brief How a keyword is treated in the selected standard.
@@ -154,7 +154,8 @@ static void AddKeyword(StringRef Keyword,
   KeywordStatus AddResult = getKeywordStatus(LangOpts, Flags);
 
   // Don't add this keyword under MSVCCompat.
-  if (LangOpts.MSVCCompat && (Flags & KEYNOMS))
+  if (LangOpts.MSVCCompat && (Flags & KEYNOMS18) &&
+      !LangOpts.isCompatibleWithMSVC(19))
      return;
   // Don't add this keyword if disabled in this language.
   if (AddResult == KS_Disabled) return;
--- clang/test/SemaCXX/MicrosoftCompatibility.cpp
+++ clang/test/SemaCXX/MicrosoftCompatibility.cpp
@@ -1,9 +1,15 @@
-// RUN: %clang_cc1 %s -triple i686-pc-win32 -fsyntax-only -std=c++11 -Wmicrosoft -verify -fms-compatibility -fexceptions -fcxx-exceptions
+// RUN: %clang_cc1 %s -triple i686-pc-win32 -fsyntax-only -std=c++11 -Wmicrosoft -verify -fms-compatibility -fexceptions -fcxx-exceptions -fms-compatibility-version=19.00
+// RUN: %clang_cc1 %s -triple i686-pc-win32 -fsyntax-only -std=c++11 -Wmicrosoft -verify -fms-compatibility -fexceptions -fcxx-exceptions -fms-compatibility-version=18.00
 
-
+#if _MSC_VER >= 1900
+char16_t x;
+char32_t y;
+_Atomic(int) z;
+#else
 typedef unsigned short char16_t;
 typedef unsigned int char32_t;
 struct _Atomic {};
+#endif
 
 typename decltype(3) a; // expected-warning {{expected a qualified name after 'typename'}}
 
