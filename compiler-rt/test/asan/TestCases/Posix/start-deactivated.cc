d8a128aafb1a78ed592d0d93988159c128fa13c4
Renato Golin <renato.golin@linaro.org>
2015-03-18 15:42:15 +0000

    [ARM] Remove XFAIL from passing RT test
    This test started passing without warning, so in theory, we shouldn't mark
    is as XPASS, but the buildbots are red and there's been an uncaught regression
    meanwhile. Since it's passing on {ARM,Thumb2} x {NEON,VFPv3}, I'll risk future
    failures for the benefit of getting the bots green again.
    
    llvm-svn: 232642

--- compiler-rt/test/asan/TestCases/Posix/start-deactivated.cc
+++ compiler-rt/test/asan/TestCases/Posix/start-deactivated.cc
@@ -19,7 +19,6 @@
 // RUN:   ASAN_ACTIVATION_OPTIONS=help=1,handle_segv=0,verbosity=1 not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-UNSUPPORTED
 
 // XFAIL: arm-linux-gnueabi
-// XFAIL: armv7l-unknown-linux-gnueabihf
 
 #if !defined(SHARED_LIB)
 #include <assert.h>
