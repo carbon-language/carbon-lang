2f69d0fe1d9ddf7e62c634376f293786bd979af3
Peter Collingbourne <peter@pcc.me.uk>
2015-03-24 21:47:00 +0000

    MC: Add more stringent symbol checking to test.
    llvm-svn: 233118

--- llvm/test/MC/ELF/alias.s
+++ llvm/test/MC/ELF/alias.s
@@ -99,12 +99,15 @@ bar6:
 // CHECK-NEXT:   }
 // CHECK-NEXT:   Symbol {
 // CHECK-NEXT:     Name: .text (0)
+// CHECK-NOT: Symbol {
 // CHECK:        }
 // CHECK-NEXT:   Symbol {
 // CHECK-NEXT:     Name: .data (0)
+// CHECK-NOT: Symbol {
 // CHECK:        }
 // CHECK-NEXT:   Symbol {
 // CHECK-NEXT:     Name: .bss (0)
+// CHECK-NOT: Symbol {
 // CHECK:        }
 // CHECK-NEXT:   Symbol {
 // CHECK-NEXT:     Name: bar3
