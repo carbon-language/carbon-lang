2974e27682e4c27e2337ea3165f5a9f2a8c47cc1
Sean Callanan <scallanan@apple.com>
2015-04-03 00:24:32 +0000

    Added a C language test case that verifies that DWARF types and types from modules can coexist even if they have the same name and refer to two different things.
    <rdar://problem/18805055>
    
    llvm-svn: 233988

---
+++ lldb/test/lang/c/modules/main.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+
+int printf(const char * restrict format, ...);
+
+typedef struct {
+    int a;
+    int b;
+} FILE;
+
+int main()
+{
+    FILE *myFile = malloc(sizeof(FILE));
+
+    myFile->a = 5;
+    myFile->b = 9;
+
+    printf("%d\n", myFile->a + myFile->b); // Set breakpoint 0 here.
+
+    free(myFile);
+}
