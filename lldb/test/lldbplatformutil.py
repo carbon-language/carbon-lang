af15b6dad95da8c70ec8be4ba5e93ad80ed1bd55
Ed Maste <emaste@freebsd.org>
2015-04-06 15:23:21 +0000

    test: Add amd64 architecture alias for x86_64
    64-bit x86 is called amd64 on FreeBSD.
    
    llvm-svn: 234164

--- lldb/test/lldbplatformutil.py
+++ lldb/test/lldbplatformutil.py
@@ -2,7 +2,7 @@
 architecture and/or the platform dependent nature of the tests. """
 
 def check_first_register_readable(test_case):
-    if test_case.getArchitecture() in ['x86_64', 'i386']:
+    if test_case.getArchitecture() in ['amd64', 'x86_64', 'i386']:
         test_case.expect("register read eax", substrs = ['eax = 0x'])
     elif test_case.getArchitecture() in ['aarch64']:
         test_case.expect("register read x0", substrs = ['x0 = 0x'])
