a01846de2ac88e42fd32d07b92864f9ac8989a25
NAKAMURA Takumi <geek4civic@gmail.com>
2015-04-06 08:16:36 +0000

    clang/test/Profile/profile-does-not-exist.c: Avoid checking a message line in the message catalog.
    llvm-svn: 234146

--- clang/test/Profile/profile-does-not-exist.c
+++ clang/test/Profile/profile-does-not-exist.c
@@ -1,4 +1,4 @@
 // RUN: not %clang_cc1 -emit-llvm %s -fprofile-instr-use=%t.nonexistent.profdata 2>&1 | FileCheck %s
 
-// CHECK: error: Could not read profile: No such file or directory
+// CHECK: error: Could not read profile:
 // CHECK-NOT: Assertion failed
