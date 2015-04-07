3d5ce49ce5c968aa4b321843f37cdcb4eecdf834
Toma Tabacu <toma.tabacu@imgtec.com>
2015-04-07 12:10:11 +0000

    [TableGen] Prevent invalid code generation when emitting AssemblerPredicate conditions.
    Summary:
    The loop which emits AssemblerPredicate conditions also links them together by emitting a '&&'.
    If the 1st predicate is not an AssemblerPredicate, while the 2nd one is, nothing gets emitted for the 1st one, but we still emit the '&&' because of the 2nd predicate.
    This generated code looks like "( && Cond2)" and is invalid.
    
    Reviewers: dsanders
    
    Reviewed By: dsanders
    
    Subscribers: dsanders, llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D8294
    
    llvm-svn: 234312

--- llvm/utils/TableGen/FixedLenDecoderEmitter.cpp
+++ llvm/utils/TableGen/FixedLenDecoderEmitter.cpp
@@ -1112,6 +1112,7 @@ bool FilterChooser::emitPredicateMatch(raw_ostream &o, unsigned &Indentation,
                                        unsigned Opc) const {
   ListInit *Predicates =
     AllInstructions[Opc]->TheDef->getValueAsListInit("Predicates");
+  bool IsFirstEmission = true;
   for (unsigned i = 0; i < Predicates->getSize(); ++i) {
     Record *Pred = Predicates->getElementAsRecord(i);
     if (!Pred->getValue("AssemblerMatcherPredicate"))
@@ -1122,7 +1123,7 @@ bool FilterChooser::emitPredicateMatch(raw_ostream &o, unsigned &Indentation,
     if (!P.length())
       continue;
 
-    if (i != 0)
+    if (!IsFirstEmission)
       o << " && ";
 
     StringRef SR(P);
@@ -1133,6 +1134,7 @@ bool FilterChooser::emitPredicateMatch(raw_ostream &o, unsigned &Indentation,
       pairs = pairs.second.split(',');
     }
     emitSinglePredicateMatch(o, pairs.first, Emitter->PredicateNamespace);
+    IsFirstEmission = false;
   }
   return Predicates->getSize() > 0;
 }
