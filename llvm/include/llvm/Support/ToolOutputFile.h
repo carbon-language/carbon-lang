3a10e8012b6269fad68dacb6904322328e33c3cb
Rafael Espindola <rafael.espindola@gmail.com>
2015-04-08 13:52:09 +0000

    Don't repeat names in comments. NFC.
    llvm-svn: 234405

--- llvm/include/llvm/Support/ToolOutputFile.h
+++ llvm/include/llvm/Support/ToolOutputFile.h
@@ -18,16 +18,16 @@
 
 namespace llvm {
 
-/// tool_output_file - This class contains a raw_fd_ostream and adds a
-/// few extra features commonly needed for compiler-like tool output files:
+/// This class contains a raw_fd_ostream and adds a few extra features commonly
+/// needed for compiler-like tool output files:
 ///   - The file is automatically deleted if the process is killed.
 ///   - The file is automatically deleted when the tool_output_file
 ///     object is destroyed unless the client calls keep().
 class tool_output_file {
-  /// Installer - This class is declared before the raw_fd_ostream so that
-  /// it is constructed before the raw_fd_ostream is constructed and
-  /// destructed after the raw_fd_ostream is destructed. It installs
-  /// cleanups in its constructor and uninstalls them in its destructor.
+  /// This class is declared before the raw_fd_ostream so that it is constructed
+  /// before the raw_fd_ostream is constructed and destructed after the
+  /// raw_fd_ostream is destructed. It installs cleanups in its constructor and
+  /// uninstalls them in its destructor.
   class CleanupInstaller {
     /// The name of the file.
     std::string Filename;
@@ -39,8 +39,7 @@ class tool_output_file {
     ~CleanupInstaller();
   } Installer;
 
-  /// OS - The contained stream. This is intentionally declared after
-  /// Installer.
+  /// The contained stream. This is intentionally declared after Installer.
   raw_fd_ostream OS;
 
 public:
@@ -51,11 +50,11 @@ public:
 
   tool_output_file(StringRef Filename, int FD);
 
-  /// os - Return the contained raw_fd_ostream.
+  /// Return the contained raw_fd_ostream.
   raw_fd_ostream &os() { return OS; }
 
-  /// keep - Indicate that the tool's job wrt this output file has been
-  /// successful and the file should not be deleted.
+  /// Indicate that the tool's job wrt this output file has been successful and
+  /// the file should not be deleted.
   void keep() { Installer.Keep = true; }
 };
 
