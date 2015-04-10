038a330ede31623d14398773e78368352666feaf
Toma Tabacu <toma.tabacu@imgtec.com>
2015-04-10 10:46:59 +0000

    [mips] [IAS] Make the mips-expansions-bad.s test more readable. NFC.
    Move the check lines below the code lines and change the indentation from 8
    spaces to 2 spaces.
    
    llvm-svn: 234584

--- llvm/test/MC/Mips/mips-expansions-bad.s
+++ llvm/test/MC/Mips/mips-expansions-bad.s
@@ -1,6 +1,8 @@
 # RUN: not llvm-mc %s -arch=mips -mcpu=mips32r2 2>%t1
 # RUN: FileCheck %s < %t1
 
-        .text
-        li $5, 0x100000000 # CHECK: :[[@LINE]]:9: error: instruction requires a 64-bit architecture
-        dli $5, 1 # CHECK: :[[@LINE]]:9: error: instruction requires a 64-bit architecture
+  .text
+  li $5, 0x100000000
+  # CHECK: :[[@LINE-1]]:3: error: instruction requires a 64-bit architecture
+  dli $5, 1
+  # CHECK: :[[@LINE-1]]:3: error: instruction requires a 64-bit architecture
