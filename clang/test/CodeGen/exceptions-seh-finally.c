865c2ca882015dedac711e72206bccc309c48c98
Reid Kleckner <reid@kleckner.net>
2015-04-10 17:45:23 +0000

    Try to fix exceptions-seh-finally.c
    llvm-svn: 234614

--- clang/test/CodeGen/exceptions-seh-finally.c
+++ clang/test/CodeGen/exceptions-seh-finally.c
@@ -152,8 +152,6 @@ int finally_with_return() {
   }
 }
 // CHECK-LABEL: define i32 @finally_with_return()
-// CHECK: alloca i32
-// CHECK-NEXT: store i32
 // CHECK-NEXT: %[[fp:[^ ]*]] = call i8* @llvm.frameaddress(i32 0)
 // CHECK-NEXT: call void @"\01?fin$0@0@finally_with_return@@"(i1 zeroext false, i8* %[[fp]])
 // CHECK-NEXT: ret i32 42
