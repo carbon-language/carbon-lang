54700b05c7e312e41155b52a367313898904ef8a
Adrian McCarthy <amccarth@google.com>
2015-04-13 20:33:05 +0000

    Skip libc++ data formmatting tests on Windows.  libc++ hasn't been ported to windows yet.
    llvm-svn: 234794

--- lldb/test/functionalities/data-formatter/data-formatter-stl/libcxx/map/TestDataFormatterLibccMap.py
+++ lldb/test/functionalities/data-formatter/data-formatter-stl/libcxx/map/TestDataFormatterLibccMap.py
@@ -20,6 +20,7 @@ class LibcxxMapDataFormatterTestCase(TestBase):
         self.data_formatter_commands()
 
     @skipIfLinux # No standard locations for libc++ on Linux, so skip for now 
+    @skipIfWindows # libc++ not ported to Windows yet
     @dwarf_test
     def test_with_dwarf_and_run_command(self):
         """Test data formatter commands."""
