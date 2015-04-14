bf138134b0addd5da3aca678935aa7dcfd18ea69
Rui Ueyama <ruiu@google.com>
2015-04-14 07:09:03 +0000

    ELF: Remove a header which is used only by one .cpp file.
    The data and the inline function defined in the header is used only
    by HexagonRelocationHandler.cpp.
    
    llvm-svn: 234867

--- lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationHandler.cpp
+++ lld/lib/ReaderWriter/ELF/Hexagon/HexagonRelocationHandler.cpp
@@ -8,15 +8,42 @@
 //===----------------------------------------------------------------------===//
 
 #include "HexagonLinkingContext.h"
-#include "HexagonRelocationFunctions.h"
 #include "HexagonRelocationHandler.h"
 #include "HexagonTargetHandler.h"
+#include "llvm/ADT/STLExtras.h"
 #include "llvm/Support/Endian.h"
 
 using namespace lld;
 using namespace lld::elf;
 using namespace llvm::ELF;
 using namespace llvm::support::endian;
+
+/// \brief Applying fixup on Hexagon requires the relocator to fetch the fixup
+/// mask from the instruction. To fetch the fixup encoding, the linker uses a
+/// static array that contains the instruction mask, the compare mask and the
+/// relocation mask.
+typedef struct {
+  uint32_t insnMask;    // Instruction mask.
+  uint32_t insnCmpMask; // Compare mask.
+  uint32_t insnBitMask; // Relocation mask.
+  bool isDuplex;        // Indicates if the instruction is a duplex instruction.
+} Instruction;
+
+#include "HexagonEncodings.h"
+
+/// \brief finds the scatter Bits that need to be used to apply relocations
+inline uint32_t findv4bitmask(uint8_t *location) {
+  uint32_t insn = llvm::support::endian::read32le(location);
+  for (int32_t i = 0, e = llvm::array_lengthof(insn_encodings); i < e; i++) {
+    if (((insn & 0xc000) == 0) && !(insn_encodings[i].isDuplex))
+      continue;
+    if (((insn & 0xc000) != 0) && (insn_encodings[i].isDuplex))
+      continue;
+    if (((insn_encodings[i].insnMask) & insn) == insn_encodings[i].insnCmpMask)
+      return insn_encodings[i].insnBitMask;
+  }
+  llvm_unreachable("found unknown Hexagon instruction");
+}
 
 // Scatter val's bits as specified by the mask. Example:
 //
