029e52bb570d2213b4a77a648683f69f4190a223
Fariborz Jahanian <fjahanian@apple.com>
2015-04-16 21:52:34 +0000

    [Objective-C Sema]. In my last patch change the attribute name to objc_independent_class. rdar://20255473
    llvm-svn: 235135

--- clang/include/clang/AST/Type.h
+++ clang/include/clang/AST/Type.h
@@ -1575,7 +1575,7 @@ public:
   bool isObjCLifetimeType() const;              // (array of)* retainable type
   bool isObjCIndirectLifetimeType() const;      // (pointer to)* lifetime type
   bool isObjCNSObjectType() const;              // __attribute__((NSObject))
-  bool isObjCIndependentClassType() const;      // __attribute__((IndependentClass))
+  bool isObjCIndependentClassType() const;      // __attribute__((objc_independent_class))
   // FIXME: change this to 'raw' interface type, so we can used 'interface' type
   // for the common case.
   bool isObjCObjectType() const;                // NSString or typeof(*(id)0)
