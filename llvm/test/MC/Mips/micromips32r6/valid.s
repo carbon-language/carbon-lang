207d248eba51c25dfca8dc3b1a38703e07fb7b2b
Jozef Kolek <jozef.kolek@imgtec.com>
2015-04-20 18:14:59 +0000

    [mips][microMIPSr6] Implement BITSWAP instruction
    Implement BITSWAP instruction using mapping.
    
    Differential Revision: http://reviews.llvm.org/D8857
    
    llvm-svn: 235321

--- llvm/test/MC/Mips/micromips32r6/valid.s
+++ llvm/test/MC/Mips/micromips32r6/valid.s
@@ -3,3 +3,4 @@
   .set noat
   balc 14572256            # CHECK: balc 14572256       # encoding: [0xb4,0x37,0x96,0xb8]
   bc 14572256              # CHECK: bc 14572256         # encoding: [0x94,0x37,0x96,0xb8]
+  bitswap $4, $2           # CHECK: bitswap $4, $2      # encoding: [0x00,0x44,0x0b,0x3c]
