f06f583f1f1bd39d4a1537183b4d7b64dc07b09a
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2015-04-20 20:28:43 +0000

    Fix polly build after LLVM r235327
    llvm-svn: 235343

--- polly/lib/Analysis/ScopDetectionDiagnostic.cpp
+++ polly/lib/Analysis/ScopDetectionDiagnostic.cpp
@@ -72,7 +72,7 @@ void getDebugLocation(const Region *R, unsigned &LineBegin, unsigned &LineEnd,
       if (!DL)
         continue;
 
-      DIScope Scope = cast<MDScope>(DL.getScope());
+      auto *Scope = cast<MDScope>(DL.getScope());
 
       if (FileName.empty())
         FileName = Scope->getFilename();
