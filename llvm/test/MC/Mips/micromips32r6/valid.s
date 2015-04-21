8e086cedfa36cd3badb6cfcb1b19811dcdc4c3b3
Jozef Kolek <jozef.kolek@imgtec.com>
2015-04-21 11:17:25 +0000

    [mips][microMIPSr6] Implement CACHE and PREF instructions
    Implement CACHE and PREF instructions using mapping.
    
    Differential Revision: http://reviews.llvm.org/D8893
    
    llvm-svn: 235379

--- llvm/test/MC/Mips/micromips32r6/valid.s
+++ llvm/test/MC/Mips/micromips32r6/valid.s
@@ -4,3 +4,5 @@
   balc 14572256            # CHECK: balc 14572256       # encoding: [0xb4,0x37,0x96,0xb8]
   bc 14572256              # CHECK: bc 14572256         # encoding: [0x94,0x37,0x96,0xb8]
   bitswap $4, $2           # CHECK: bitswap $4, $2      # encoding: [0x00,0x44,0x0b,0x3c]
+  cache 1, 8($5)           # CHECK: cache 1, 8($5)      # encoding: [0x20,0x25,0x60,0x08]
+  pref 1, 8($5)            # CHECK: pref 1, 8($5)       # encoding: [0x60,0x25,0x20,0x08]
