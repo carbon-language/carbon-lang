bbaba9686060290e5f58902c15965ccd439b42b0
Pavel Labath <labath@google.com>
2015-04-28 12:28:08 +0000

    Skip TestProcessAttach.py on Linux
    The test was already XFAILed. Changing this to skip, because the test timeouts and gets flagged
    as an error anyway.
    
    llvm-svn: 235979

--- lldb/test/functionalities/process_attach/TestProcessAttach.py
+++ lldb/test/functionalities/process_attach/TestProcessAttach.py
@@ -22,7 +22,7 @@ class ProcessAttachTestCase(TestBase):
         self.process_attach_by_id()
 
     @dwarf_test
-    @expectedFailureLinux # llvm.org/pr23360
+    @skipIfLinux # llvm.org/pr23360
     def test_attach_to_process_by_id_with_dwarf(self):
         """Test attach by process id"""
         self.buildDwarf()
@@ -36,7 +36,7 @@ class ProcessAttachTestCase(TestBase):
         self.process_attach_by_name()
 
     @dwarf_test
-    @expectedFailureLinux # llvm.org/pr23360
+    @skipIfLinux # llvm.org/pr23360
     def test_attach_to_process_by_name_with_dwarf(self):
         """Test attach by process name"""
         self.buildDwarf()
