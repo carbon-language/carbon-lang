374934cb8a34c8ea64a439e628b7937cda45c188
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2015-04-28 01:09:20 +0000

    DebugInfo: Add a clang test for LLVM fix for PR23332
    Add a clang test for LLVM r235955, which added support for up to 2^16
    arguments.
    
    llvm-svn: 235956

---
+++ clang/test/CodeGen/debug-info-257-args.c
@@ -0,0 +1,25 @@
+// RUN: %clang_cc1 -x c++ -g -emit-llvm -triple x86_64-linux-gnu -o - %s | FileCheck %s
+// PR23332
+
+// CHECK: MDLocalVariable(tag: DW_TAG_arg_variable, arg: 255
+// CHECK: MDLocalVariable(tag: DW_TAG_arg_variable, arg: 256
+// CHECK: MDLocalVariable(tag: DW_TAG_arg_variable, arg: 257
+void fn1(int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int, int, int, int, int, int, int, int, int, int,
+         int, int, int, int, int) {}
