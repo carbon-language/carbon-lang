f5ec2acca9456f9c6c5f4060aa53ce72a9bb07f5
Richard Smith <richard-llvm@metafoo.co.uk>
2015-04-29 23:40:48 +0000

    Add an assert to get information on buildbot failure.
    llvm-svn: 236181

--- clang/lib/Lex/Preprocessor.cpp
+++ clang/lib/Lex/Preprocessor.cpp
@@ -627,6 +627,7 @@ bool Preprocessor::HandleIdentifier(Token &Identifier) {
   // If this is a macro to be expanded, do it.
   if (MacroDefinition MD = getMacroDefinition(&II)) {
     auto *MI = MD.getMacroInfo();
+    assert(MI && "macro definition with no macro info?");
     if (!DisableMacroExpansion) {
       if (!Identifier.isExpandDisabled() && MI->isEnabled()) {
         // C99 6.10.3p10: If the preprocessing token immediately after the
