b81f4aa95297e1bb11d6805ea6d66b6ec960bd8f
Tom Stellard <thomas.stellard@amd.com>
2015-05-04 16:45:08 +0000

    R600/SI: Code cleanup
    This is a follow-up to r236004
    
    llvm-svn: 236427

--- llvm/lib/Target/R600/AMDGPUMCInstLower.cpp
+++ llvm/lib/Target/R600/AMDGPUMCInstLower.cpp
@@ -132,9 +132,8 @@ void AMDGPUAsmPrinter::EmitInstruction(const MachineInstr *MI) {
       SmallVector<char, 16> CodeBytes;
       raw_svector_ostream CodeStream(CodeBytes);
 
-      MCObjectStreamer *ObjStreamer =
-          static_cast<MCObjectStreamer *>(OutStreamer.get());
-      MCCodeEmitter &InstEmitter = ObjStreamer->getAssembler().getEmitter();
+      auto &ObjStreamer = static_cast<MCObjectStreamer&>(*OutStreamer);
+      MCCodeEmitter &InstEmitter = ObjStreamer.getAssembler().getEmitter();
       InstEmitter.EncodeInstruction(TmpInst, CodeStream, Fixups,
                                     MF->getSubtarget<MCSubtargetInfo>());
       CodeStream.flush();
