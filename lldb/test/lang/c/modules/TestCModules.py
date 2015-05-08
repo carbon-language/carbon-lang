a6ad6d3cc9187e487dacb319fd082336dfd6be22
Vince Harron <vince@nethacker.com>
2015-05-08 23:17:46 +0000

    TestCModules was effectively skipped on Linux
    Summary: I've removed the secret skip and added expectedFailureLinux
        
    Test Plan: run tests
        
    Reviewers: flackr
        
    Subscribers: lldb-commits
        
    Differential Revision: http://reviews.llvm.org/D9615
    
    llvm-svn: 236914

--- lldb/test/lang/c/modules/TestCModules.py
+++ lldb/test/lang/c/modules/TestCModules.py
@@ -22,6 +22,7 @@ class CModulesTestCase(TestBase):
 
     @dwarf_test
     @skipIfFreeBSD
+    @expectedFailureLinux('http://llvm.org/pr23456') # 'fopen' has unknown return type
     def test_expr_with_dwarf(self):
         self.buildDwarf()
         self.expr()
@@ -33,9 +34,7 @@ class CModulesTestCase(TestBase):
         self.line = line_number('main.c', '// Set breakpoint 0 here.')
 
     def applies(self):
-        if platform.system() != "Darwin":
-            return False
-        if StrictVersion('12.0.0') > platform.release():
+        if platform.system() == "Darwin" and platform.release() < StrictVersion('12.0.0'):
             return False
 
         return True
