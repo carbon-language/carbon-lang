ec1fc97e5da00a4bfbb16ac74449b2e042dc37cb
Douglas Katzman <dougk@google.com>
2015-05-08 15:34:12 +0000

    Prevent further errors of omission when adding backend names.
    Differential Revision: http://reviews.llvm.org/D9441
    
    llvm-svn: 236865

---
+++ llvm/unittests/Support/TargetRegistry.cpp
@@ -0,0 +1,43 @@
+//===- unittests/Support/TargetRegistry.cpp - -----------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Support/TargetRegistry.h"
+#include "llvm/Support/TargetSelect.h"
+#include "gtest/gtest.h"
+
+using namespace llvm;
+
+namespace {
+
+TEST(TargetRegistry, TargetHasArchType) {
+  // Presence of at least one target will be asserted when done with the loop,
+  // else this would pass by accident if InitializeAllTargetInfos were omitted.
+  int Count = 0;
+
+  llvm::InitializeAllTargetInfos();
+
+  llvm::TargetRegistry RegistryRoot;
+  for (const auto &Target : RegistryRoot) {
+    StringRef Name = Target.getName();
+    // There is really no way (at present) to ask a Target whether it targets
+    // a specific architecture, because the logic for that is buried in a
+    // predicate.
+    // We can't ask the predicate "Are you a function that always returns
+    // false?"
+    // So given that the cpp backend truly has no target arch, it is skipped.
+    if (Name != "cpp") {
+      Triple::ArchType Arch = Triple::getArchTypeForLLVMName(Name);
+      EXPECT_NE(Arch, Triple::UnknownArch);
+      ++Count;
+    }
+  }
+  ASSERT_NE(Count, 0);
+}
+
+} // end namespace
