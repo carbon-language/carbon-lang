cf385dc8879f05581e94944d8ae7f00138c9ed72
Nikola Smiljanic <popizdeh@gmail.com>
2015-05-08 06:02:37 +0000

    Revert "Fix path separator issue on Windows."
    This reverts commit 9242ff16b0460b488691fd70b42a2bf81a531e3a.
    
    llvm-svn: 236806

--- clang/lib/Lex/HeaderSearch.cpp
+++ clang/lib/Lex/HeaderSearch.cpp
@@ -614,8 +614,10 @@ const FileEntry *HeaderSearch::LookupFile(
       const FileEntry *Includer = IncluderAndDir.first;
 
       // Concatenate the requested file onto the directory.
+      // FIXME: Portability.  Filename concatenation should be in sys::Path.
       TmpDir = IncluderAndDir.second->getName();
-      llvm::sys::path::append(TmpDir, Filename);
+      TmpDir.push_back('/');
+      TmpDir.append(Filename.begin(), Filename.end());
 
       // FIXME: We don't cache the result of getFileInfo across the call to
       // getFileAndSuggestModule, because it's a reference to an element of
