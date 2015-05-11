ec05fc7b16473e5bee82bc8f18bcd97fe68df101
Daniel Jasper <djasper@google.com>
2015-05-11 09:14:50 +0000

    clang-format: [JS] Clean up export declaration parsing.
    NFC intended.
    
    llvm-svn: 236982

--- clang/lib/Format/UnwrappedLineParser.cpp
+++ clang/lib/Format/UnwrappedLineParser.cpp
@@ -1666,10 +1666,12 @@ void UnwrappedLineParser::parseJavaScriptEs6ImportExport() {
   assert(FormatTok->isOneOf(Keywords.kw_import, tok::kw_export));
   nextToken();
 
-  // Consume "function" and "default function", so that these get parsed as
-  // free-standing JS functions, i.e. do not require a trailing semicolon.
+  // Consume the "default" in "export default class/function".
   if (FormatTok->is(tok::kw_default))
     nextToken();
+
+  // Consume "function" and "default function", so that these get parsed as
+  // free-standing JS functions, i.e. do not require a trailing semicolon.
   if (FormatTok->is(Keywords.kw_function)) {
     nextToken();
     return;
@@ -1677,11 +1679,6 @@ void UnwrappedLineParser::parseJavaScriptEs6ImportExport() {
 
   if (FormatTok->isOneOf(tok::kw_const, tok::kw_class, Keywords.kw_var))
     return; // Fall through to parsing the corresponding structure.
-
-  if (FormatTok->is(tok::kw_default)) {
-    nextToken(); // export default ..., fall through after eating 'default'.
-    return;
-  }
 
   if (FormatTok->is(tok::l_brace)) {
     FormatTok->BlockKind = BK_Block;
