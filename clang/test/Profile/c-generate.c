3dda83f03a4df43ecb63c54048ee600ae1b2b70a
Justin Bogner <mail@justinbogner.com>
2015-05-12 21:23:16 +0000

    InstrProf: Update name of compiler-rt routine for setting filename
    Patch by Teresa Johnson.
    
    llvm-svn: 237187

--- clang/test/Profile/c-generate.c
+++ clang/test/Profile/c-generate.c
@@ -2,8 +2,8 @@
 // RUN: %clang_cc1 -main-file-name c-generate.c %s -o - -emit-llvm -fprofile-instr-generate=c-generate-test.profraw | FileCheck %s
 
 // CHECK: private constant [24 x i8] c"c-generate-test.profraw\00"
-// CHECK: call void @__llvm_profile_set_filename_env_override(i8* getelementptr inbounds ([24 x i8], [24 x i8]* @0, i32 0, i32 0))
-// CHECK: declare void @__llvm_profile_set_filename_env_override(i8*)
+// CHECK: call void @__llvm_profile_override_default_filename(i8* getelementptr inbounds ([24 x i8], [24 x i8]* @0, i32 0, i32 0))
+// CHECK: declare void @__llvm_profile_override_default_filename(i8*)
 
 int main(void) {
   return 0;
