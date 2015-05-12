2680fe5f077b039a760c6b45d0e4c720460f3506
Sergey Matveev <earthdok@google.com>
2015-05-12 16:32:41 +0000

    [sanitizer] Override pipefail in coverage-missing test.
    This should finally fix it.
    
    llvm-svn: 237147

--- compiler-rt/test/asan/TestCases/Linux/coverage-missing.cc
+++ compiler-rt/test/asan/TestCases/Linux/coverage-missing.cc
@@ -21,13 +21,10 @@
 // RUN: [ $(cat bar.txt | wc -l) == 4 ]
 // RUN: %sancov missing %t < foo.txt > foo-missing.txt
 // RUN: sort main.txt foo-missing.txt -o foo-missing-with-main.txt
-// Next two lines are debug output.
-// RUN: tail -n +1 foo.txt bar.txt main.txt foo-missing.txt foo-missing-with-main.txt
-// RUN: echo; objdump -d %t | grep '__sanitizer_cov'
 // The "missing from foo" set may contain a few bogus PCs from the sanitizer
 // runtime, but it must include the entire "bar" code path as a subset. Sorted
 // lists can be tested for set inclusion with diff + grep.
-// RUN: diff bar.txt foo-missing-with-main.txt | not grep "^<"
+// RUN: ( diff bar.txt foo-missing-with-main.txt || true ) | not grep "^<"
 
 // Second case: coverage from DSO.
 // cd %T
@@ -46,10 +43,7 @@
 // RUN: rm *.sancov
 // RUN: [ $(cat bar.txt | wc -l) == 3 ]
 // RUN: %sancov missing %dynamiclib < foo.txt > foo-missing.txt
-// Next two lines are debug output.
-// RUN: tail -n +1 foo.txt bar.txt foo-missing.txt
-// RUN: echo; objdump -d %t | grep '__sanitizer_cov'
-// RUN: diff bar.txt foo-missing.txt | not grep "^<"
+// RUN: ( diff bar.txt foo-missing.txt || true ) | not grep "^<"
 
 // REQUIRES: x86_64-supported-target, i386-supported-target
 // XFAIL: android
