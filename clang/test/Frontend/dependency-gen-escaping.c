01477e5d6a929cd3b3ee2cd937a07660b24cf73a
NAKAMURA Takumi <geek4civic@gmail.com>
2015-05-14 04:25:54 +0000

    clang/test/Frontend/dependency-gen-escaping.c: Tweak r237296, to let '/' and '\\' distinguishd, to unbreak "--host=linux --target=msvc".
    llvm-svn: 237333

--- clang/test/Frontend/dependency-gen-escaping.c
+++ clang/test/Frontend/dependency-gen-escaping.c
@@ -1,5 +1,6 @@
 // PR15642
-// RUN: %clang -M -MG %s | FileCheck -strict-whitespace %s
+// RUN: %clang -M -MG -fno-ms-compatibility %s | FileCheck -strict-whitespace %s --check-prefix=CHECK --check-prefix=SEP2F
+// RUN: %clang -M -MG -fms-compatibility %s | FileCheck -strict-whitespace %s --check-prefix=CHECK --check-prefix=SEP5C
 // RUN: %clang -M -MG -MV %s | FileCheck -strict-whitespace %s --check-prefix=NMAKE
 
 // CHECK: \ \ \ \ .h
@@ -20,8 +21,11 @@
 // Backslash followed by # or space should escape both characters, because
 // that's what GNU Make wants.  GCC does the right thing with space, but not
 // #, so Clang does too. (There should be 3 backslashes before the #.)
-// CHECK: a\b\\#c\\\ d.h
+// SEP2F: a\b\\#c\\\ d.h
+// With -fms-compatibility, Backslashes in #include are treated as path separators.
+// Backslashes are given in the emission for special characters, like 0x20 or 0x23.
+// SEP5C: a{{[/\\]}}b{{[/\\]}}\#c{{[/\\]}}\ d.h
 // These combinations are just another case for NMAKE.
-// NMAKE: "a\b\#c\ d.h"
+// NMAKE: "a{{[/\\]}}b{{[/\\]}}#c{{[/\\]}} d.h"
 
 #include "a\b\#c\ d.h"
