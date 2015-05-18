0e166e5a9a98ab9112d9d07e9e5c9af300730211
Marshall Clow <mclow.lists@gmail.com>
2015-05-18 17:48:45 +0000

    Fix for LWG Issue 2458: N3778 and new library deallocation signatures.
    llvm-svn: 237592

--- libcxx/src/new.cpp
+++ libcxx/src/new.cpp
@@ -140,13 +140,6 @@ operator delete(void* ptr, size_t) _NOEXCEPT
 
 _LIBCPP_WEAK _LIBCPP_NEW_DELETE_VIS
 void
-operator delete(void* ptr, size_t, const std::nothrow_t& nt) _NOEXCEPT
-{
-    ::operator delete(ptr, nt);
-}
-
-_LIBCPP_WEAK _LIBCPP_NEW_DELETE_VIS
-void
 operator delete[] (void* ptr) _NOEXCEPT
 {
     ::operator delete(ptr);
@@ -164,13 +157,6 @@ void
 operator delete[] (void* ptr, size_t) _NOEXCEPT
 {
     ::operator delete[](ptr);
-}
-
-_LIBCPP_WEAK _LIBCPP_NEW_DELETE_VIS
-void
-operator delete[] (void* ptr, size_t, const std::nothrow_t& nt) _NOEXCEPT
-{
-    ::operator delete[](ptr, nt);
 }
 
 #endif // !__GLIBCXX__
