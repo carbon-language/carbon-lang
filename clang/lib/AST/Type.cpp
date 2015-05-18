5fb3a4277aaeeb83b3c501026c6b9cd09adaaac8
Nico Weber <nicolasweber@gmx.de>
2015-05-18 00:00:29 +0000

    Fix confusing indent. No behavior change.
    llvm-svn: 237546

--- clang/lib/AST/Type.cpp
+++ clang/lib/AST/Type.cpp
@@ -729,7 +729,7 @@ bool Type::isSignedIntegerType() const {
 bool Type::isSignedIntegerOrEnumerationType() const {
   if (const BuiltinType *BT = dyn_cast<BuiltinType>(CanonicalType)) {
     return BT->getKind() >= BuiltinType::Char_S &&
-    BT->getKind() <= BuiltinType::Int128;
+           BT->getKind() <= BuiltinType::Int128;
   }
   
   if (const EnumType *ET = dyn_cast<EnumType>(CanonicalType)) {
