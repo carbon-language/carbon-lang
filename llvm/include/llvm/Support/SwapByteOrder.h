fa7c2da1000dc079b75c082b2705c96de310ef04
Pawel Bylica <chfast@gmail.com>
2015-05-19 08:44:15 +0000

    Fix getSwappedBytes for double.
    llvm-svn: 237673

--- llvm/include/llvm/Support/SwapByteOrder.h
+++ llvm/include/llvm/Support/SwapByteOrder.h
@@ -104,7 +104,7 @@ inline float getSwappedBytes(float C) {
   return out.f;
 }
 
-inline float getSwappedBytes(double C) {
+inline double getSwappedBytes(double C) {
   union {
     uint64_t i;
     double d;
