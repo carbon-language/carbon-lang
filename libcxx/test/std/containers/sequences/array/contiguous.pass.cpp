c5c29006dc665b90ab5cab3222704369f3f3b5a2
Marshall Clow <mclow.lists@gmail.com>
2015-05-26 18:57:27 +0000

    Add tests to ensure that string/vector/array have contiguous iterators - which they did. Mark N4284 as complete
    llvm-svn: 238233

--- libcxx/test/std/containers/sequences/array/contiguous.pass.cpp
+++ libcxx/test/std/containers/sequences/array/contiguous.pass.cpp
@@ -7,6 +7,25 @@
 //
 //===----------------------------------------------------------------------===//
 
+// <array>
+
+// An array is a contiguous container
+
+#include <array>
+#include <cassert>
+
+template <class C>
+void test_contiguous ( const C &c )
+{
+    for ( size_t i = 0; i < c.size(); ++i )
+        assert ( *(c.begin() + i) == *(std::addressof(*c.begin()) + i));
+}
+
 int main()
 {
+    {
+        typedef double T;
+        typedef std::array<T, 3> C;
+        test_contiguous (C());
+    }
 }
