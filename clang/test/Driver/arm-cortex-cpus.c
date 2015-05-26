5b30b0aaaeeb2a5bf67f2883eb3cd3787be44756
Ranjeet Singh <Ranjeet.Singh@arm.com>
2015-05-26 17:30:35 +0000

    Test commit.
    llvm-svn: 238220

--- clang/test/Driver/arm-cortex-cpus.c
+++ clang/test/Driver/arm-cortex-cpus.c
@@ -207,7 +207,7 @@
 
 // ================== Check that a bogus CPU gives an error
 // RUN: %clang -target arm -mcpu=bogus -### -c %s 2>&1 | FileCheck -check-prefix=CHECK-BOGUS-CPU %s
-// CHECK-BOGUS-CPU: error: the clang compiler does not support '-mcpu=bogus' 
+// CHECK-BOGUS-CPU: error: the clang compiler does not support '-mcpu=bogus'
 
 // ================== Check default Architecture on each ARM11 CPU
 // RUN: %clang -target arm-linux-gnueabi -mcpu=arm1136j-s -### -c %s 2>&1 | FileCheck -check-prefix=CHECK-CPUV6 %s
