b41354c9d0c7f9517c112be7ff4691e8de9ce5ec
Chaoren Lin <chaorenl@google.com>
2015-05-28 19:15:15 +0000

    Move inlined cxa_demangle.cpp to a separate file.
    Summary: In preparation for some changes to make this compatible with MSVC.
    
    Reviewers: emaste, zturner, clayborg
    
    Reviewed By: clayborg
    
    Subscribers: lldb-commits
    
    Differential Revision: http://reviews.llvm.org/D9949
    
    llvm-svn: 238459

---
+++ lldb/include/lldb/Core/FastDemangle.h
@@ -0,0 +1,24 @@
+//===-- FastDemangle.h ------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef liblldb_FastDemangle_h_
+#define liblldb_FastDemangle_h_
+
+namespace lldb_private
+{
+
+    char *
+    FastDemangle(const char *mangled_name);
+
+    char *
+    FastDemangle(const char *mangled_name, long mangled_name_length);
+
+}
+
+#endif
