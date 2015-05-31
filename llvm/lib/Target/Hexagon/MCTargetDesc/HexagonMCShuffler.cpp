a97365b8e0c88296b8926d4097986d28306f1bf4
Colin LeMahieu <colinl@codeaurora.org>
2015-05-31 22:29:33 +0000

    [Hexagon] Including raw_ostream for debug builds.
    llvm-svn: 238695

--- llvm/lib/Target/Hexagon/MCTargetDesc/HexagonMCShuffler.cpp
+++ llvm/lib/Target/Hexagon/MCTargetDesc/HexagonMCShuffler.cpp
@@ -20,6 +20,7 @@
 #include "MCTargetDesc/HexagonMCTargetDesc.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
 
