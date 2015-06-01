c44223ed962ebf56ec8a8146e1592465fff7bfee
Denis Protivensky <dprotivensky@accesssoftek.com>
2015-06-01 08:12:44 +0000

    COFF: Add noexcept to std::error_category::name
    This fixes build error with gcc.
    
    llvm-svn: 238732

--- lld/COFF/Error.h
+++ lld/COFF/Error.h
@@ -26,7 +26,7 @@ enum class LLDError {
 
 class LLDErrorCategory : public std::error_category {
 public:
-  const char *name() const override { return "lld"; }
+  const char *name() const noexcept override { return "lld"; }
 
   std::string message(int EV) const override {
     switch (static_cast<LLDError>(EV)) {
