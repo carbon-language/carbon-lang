f87702112d7a2f6d5897af1d8f9a749da105125a
Daniel Sanders <daniel.sanders@imgtec.com>
2015-06-02 10:09:08 +0000

    [mips] Test both %dtprel_hi and %dtprel_lo instead of testing %dtprel_hi twice.
    The second %dtprel_hi is used on an addiu so it looks like a copy/paste error.
    
    llvm-svn: 238815

--- llvm/test/MC/Mips/mips-relocations.s
+++ llvm/test/MC/Mips/mips-relocations.s
@@ -11,8 +11,8 @@
 # CHECK:                                  #   fixup A - offset: 0, value: loop_1@GOT, kind: fixup_Mips_GOT_Local
 # CHECK:  lui     $2, %dtprel_hi(_gp_disp) # encoding: [A,A,0x02,0x3c]
 # CHECK:                                        #   fixup A - offset: 0, value: _gp_disp@DTPREL_HI, kind: fixup_Mips_DTPREL_HI
-# CHECK:  addiu   $2, $2, %dtprel_hi(_gp_disp) # encoding: [A,A,0x42,0x24]
-# CHECK:                                  #   fixup A - offset: 0, value: _gp_disp@DTPREL_HI, kind: fixup_Mips_DTPREL_HI
+# CHECK:  addiu   $2, $2, %dtprel_lo(_gp_disp) # encoding: [A,A,0x42,0x24]
+# CHECK:                                  #   fixup A - offset: 0, value: _gp_disp@DTPREL_LO, kind: fixup_Mips_DTPREL_LO
 # CHECK:  lw      $3, %got(loop_1)($2)      # encoding: [A,A,0x43,0x8c]
 # CHECK:                                    #   fixup A - offset: 0, value: loop_1@GOT, kind: fixup_Mips_GOT_Local
 # CHECK:  lw      $4, %got_disp(loop_2)($3) # encoding: [A,A,0x64,0x8c]
@@ -31,7 +31,7 @@
     lw $25, %call16(strchr)($gp)
     lw      $3, %got(loop_1)($2)
     lui $2, %dtprel_hi(_gp_disp)
-    addiu $2, $2, %dtprel_hi(_gp_disp)
+    addiu $2, $2, %dtprel_lo(_gp_disp)
     lw $3, %got(loop_1)($2)
     lw $4, %got_disp(loop_2)($3)
     lw $5, %got_page(loop_3)($4)
