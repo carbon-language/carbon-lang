af5f51f6a8e8fa5427b0c464931b307dcaaa4520
Rafael Espindola <rafael.espindola@gmail.com>
2015-06-04 05:51:13 +0000

    Add testcase that would crash before the previous revert.
    llvm-svn: 239011

---
+++ llvm/test/MC/PowerPC/st-other-crash.s
@@ -0,0 +1,21 @@
+// RUN: llvm-mc < %s -filetype=obj -triple powerpc64le-pc-linux | \
+// RUN:   llvm-readobj -t | FileCheck %s
+
+// This used to crash. Make sure it produce the correct symbol.
+
+// CHECK:       Symbol {
+// CHECK:         Name: _ZN4llvm11SmallVectorIcLj0EEC2Ev (12)
+// CHECK-NEXT:    Value: 0x0
+// CHECK-NEXT:    Size: 0
+// CHECK-NEXT:    Binding: Local (0x0)
+// CHECK-NEXT:    Type: None (0x0)
+// CHECK-NEXT:    Other: 64
+// CHECK-NEXT:    Section: .group (0x5)
+// CHECK-NEXT:  }
+
+
+	.section	.text._ZN4llvm11SmallVectorIcLj0EEC2Ev,"axG",@progbits,_ZN4llvm11SmallVectorIcLj0EEC2Ev,comdat
+.Ltmp2:
+	addis 2, 12, .TOC.-.Ltmp2@ha
+.Ltmp3:
+	.localentry	_ZN4llvm11SmallVectorIcLj0EEC2Ev, .Ltmp3-.Ltmp2
