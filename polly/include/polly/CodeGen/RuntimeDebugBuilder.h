039955a44c58489b20ec489843a238a74770b297
Tobias Grosser <tobias@grosser.es>
2015-06-08 16:24:49 +0000

    Revert "Add NVIDIA vprintf printing to RuntimeDebugBuilder"
    This reverts commit 239219 which requires some LLVM changes I forgot to commit.
    
    Reported-by: Marshall Clow
    llvm-svn: 239306

--- polly/include/polly/CodeGen/RuntimeDebugBuilder.h
+++ polly/include/polly/CodeGen/RuntimeDebugBuilder.h
@@ -13,8 +13,7 @@
 #define RUNTIME_DEBUG_BUILDER_H
 
 #include "polly/CodeGen/IRBuilder.h"
-#include "llvm/ADT/ArrayRef.h"
-#include "llvm/ADT/StringRef.h"
+#include <string>
 
 namespace llvm {
 class Value;
@@ -53,70 +52,6 @@ struct RuntimeDebugBuilder {
   /// If the current module does not yet contain a reference to printf, we
   /// insert a reference to it. Otherwise the existing reference is returned.
   static llvm::Function *getPrintF(PollyIRBuilder &Builder);
-
-  /// @brief Print a set of LLVM-IR Values or StringRefs on an NVIDIA GPU.
-  ///
-  ///  This function emits a call to vprintf that will print the given
-  ///  arguments from within a kernel thread. It is useful for debugging
-  ///  CUDA program kernels. All arguments given in this list will be
-  ///  automatically concatenated and the resulting string will be printed
-  ///  atomically. We also support ArrayRef arguments, which can be used to
-  ///  provide for example a list of thread-id values.
-  ///
-  ///  @param Builder The builder used to emit the printer calls.
-  ///  @param Args    The list of values to print.
-  template <typename... Args>
-  static void createGPUPrinter(PollyIRBuilder &Builder, Args... args) {
-    std::vector<llvm::Value *> Vector;
-    createGPUVAPrinter(Builder, Vector, args...);
-  }
-
-private:
-  /// @brief GPU printing - Print a list of LLVM Values.
-  ///
-  static void createGPUVAPrinter(PollyIRBuilder &Builder,
-                                 llvm::ArrayRef<llvm::Value *> Values);
-
-  /// @brief GPU printing - Handle Values.
-  template <typename... Args>
-  static void createGPUVAPrinter(PollyIRBuilder &Builder,
-                                 std::vector<llvm::Value *> &Values,
-                                 llvm::Value *Value, Args... args) {
-    Values.push_back(Value);
-    createGPUVAPrinter(Builder, Values, args...);
-  }
-
-  /// @brief GPU printing - Handle StringRefs.
-  template <typename... Args>
-  static void createGPUVAPrinter(PollyIRBuilder &Builder,
-                                 std::vector<llvm::Value *> &Values,
-                                 llvm::StringRef String, Args... args) {
-    Values.push_back(Builder.CreateGlobalStringPtr(String, "", 4));
-    createGPUVAPrinter(Builder, Values, args...);
-  }
-
-  /// @brief GPU printing - Handle ArrayRefs.
-  template <typename... Args>
-  static void createGPUVAPrinter(PollyIRBuilder &Builder,
-                                 std::vector<llvm::Value *> &Values,
-                                 llvm::ArrayRef<llvm::Value *> Array,
-                                 Args... args) {
-    if (Array.size() >= 2)
-      createGPUVAPrinter(
-          Builder, Values, Array[0], " ",
-          llvm::ArrayRef<llvm::Value *>(&Array[1], Array.size() - 1), args...);
-    else
-      createGPUVAPrinter(Builder, Values, Array[0], args...);
-  }
-
-  /// @brief Get (and possibly insert) a vprintf declaration into the module.
-  static llvm::Function *getVPrintF(PollyIRBuilder &Builder);
-
-  /// @brief Get (and possibly insert) a NVIDIA address space cast call.
-  static llvm::Function *getAddressSpaceCast(PollyIRBuilder &Builder,
-                                             unsigned Src, unsigned Dst,
-                                             unsigned SrcBits = 8,
-                                             unsigned DstBits = 8);
 };
 }
 
