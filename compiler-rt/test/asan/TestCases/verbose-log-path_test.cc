dd9e6578f03c08bc07b19daf6b0f5d31b418b52e
Anna Zaks <ganna@apple.com>
2015-06-08 23:24:55 +0000

    [asan] Fixup to r239134. This test does pass on darwin.
    This should restore the darwin buildbot.
    
    llvm-svn: 239364

--- compiler-rt/test/asan/TestCases/verbose-log-path_test.cc
+++ compiler-rt/test/asan/TestCases/verbose-log-path_test.cc
@@ -6,7 +6,7 @@
 // RUN: FileCheck %s --check-prefix=CHECK-ERROR < %T/asan.log.verbose-log-path_test-binary.*
 
 // FIXME: only FreeBSD and Linux have verbose log paths now.
-// XFAIL: win32,android,darwin
+// XFAIL: win32,android
 
 #include <stdlib.h>
 #include <string.h>
