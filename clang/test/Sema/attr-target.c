0ee1c8cd0bb01406fd505c7723d4be040b3cf932
Eric Christopher <echristo@gmail.com>
2015-06-12 20:13:50 +0000

    Quote the user provided string in the warning message and update tests accordingly.
    llvm-svn: 239635

--- clang/test/Sema/attr-target.c
+++ clang/test/Sema/attr-target.c
@@ -2,7 +2,7 @@
 
 int __attribute__((target("avx,sse4.2,arch=ivybridge"))) foo() { return 4; }
 int __attribute__((target())) bar() { return 4; } //expected-error {{'target' attribute takes one argument}}
-int __attribute__((target("tune=sandybridge"))) baz() { return 4; } //expected-warning {{Ignoring unsupported tune= in the target attribute string}}
-int __attribute__((target("fpmath=387"))) walrus() { return 4; } //expected-warning {{Ignoring unsupported fpmath= in the target attribute string}}
+int __attribute__((target("tune=sandybridge"))) baz() { return 4; } //expected-warning {{Ignoring unsupported 'tune=' in the target attribute string}}
+int __attribute__((target("fpmath=387"))) walrus() { return 4; } //expected-warning {{Ignoring unsupported 'fpmath=' in the target attribute string}}
 
 
