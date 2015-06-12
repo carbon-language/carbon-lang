1f48192d7ce23d5e36631398b087e8822cb289c1
Matt Wala <wala@google.com>
2015-06-12 22:49:11 +0000

    [Scalarizer] Fix potential for stale data in Scattered across invocations
    Summary:
    Scalarizer has two data structures that hold information about changes
    to the function, Gathered and Scattered. These are cleared in finish()
    at the end of runOnFunction() if finish() detects any changes to the
    function. 
    
    However, finish() was checking for changes by only checking if
    Gathered was non-empty. The function visitStore() only modifies
    Scattered without touching Gathered. As a result, Scattered could have
    ended up having stale data if Scalarizer only scalarized store
    instructions. Since the data in Scattered is used during the execution
    of the pass, this introduced dangling pointer errors. 
    
    The fix is to check whether both Scattered and Gathered are empty
    before deciding what to do in finish().
    
    Reviewers: srhines
    
    Reviewed By: srhines
    
    Subscribers: llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D10422
    
    llvm-svn: 239644

--- llvm/lib/Transforms/Scalar/Scalarizer.cpp
+++ llvm/lib/Transforms/Scalar/Scalarizer.cpp
@@ -636,7 +636,9 @@ bool Scalarizer::visitStoreInst(StoreInst &SI) {
 // Delete the instructions that we scalarized.  If a full vector result
 // is still needed, recreate it using InsertElements.
 bool Scalarizer::finish() {
-  if (Gathered.empty())
+  // The presence of data in Gathered or Scattered indicates changes
+  // made to the Function.
+  if (Gathered.empty() && Scattered.empty())
     return false;
   for (GatherList::iterator GMI = Gathered.begin(), GME = Gathered.end();
        GMI != GME; ++GMI) {
