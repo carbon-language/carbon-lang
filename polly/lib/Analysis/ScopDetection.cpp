afa4ea71217a3564e4cb1a2439c6dd5bb3bfa863
Chandler Carruth <chandlerc@gmail.com>
2015-06-17 08:29:32 +0000

    [PM/AA] Update Polly for r239886 which removed UnknownSize from AliasAnalysis by getting it from its newly canonical home of MemoryLocation::UnknownSize.
    llvm-svn: 239890

--- polly/lib/Analysis/ScopDetection.cpp
+++ polly/lib/Analysis/ScopDetection.cpp
@@ -634,7 +634,7 @@ bool ScopDetection::isValidMemoryAccess(Instruction &Inst,
   AAMDNodes AATags;
   Inst.getAAMetadata(AATags);
   AliasSet &AS = Context.AST.getAliasSetForPointer(
-      BaseValue, AliasAnalysis::UnknownSize, AATags);
+      BaseValue, MemoryLocation::UnknownSize, AATags);
 
   // INVALID triggers an assertion in verifying mode, if it detects that a
   // SCoP was detected by SCoP detection and that this SCoP was invalidated by
