a941129d006e41d9f59e81d7f2fef631766a4464
Jingyue Wu <jingyue@google.com>
2015-06-18 03:35:57 +0000

    [NFC] more comments in SLSR
    llvm-svn: 239984

--- llvm/lib/Transforms/Scalar/StraightLineStrengthReduce.cpp
+++ llvm/lib/Transforms/Scalar/StraightLineStrengthReduce.cpp
@@ -632,6 +632,15 @@ void StraightLineStrengthReduce::rewriteCandidateWithBasis(
       // trivially dead.
       RecursivelyDeleteTriviallyDeadInstructions(Bump);
     } else {
+      // It's tempting to preserve nsw on Bump and/or Reduced. However, it's
+      // usually unsound, e.g.,
+      //
+      // X = (-2 +nsw 1) *nsw INT_MAX
+      // Y = (-2 +nsw 3) *nsw INT_MAX
+      //   =>
+      // Y = X + 2 * INT_MAX
+      //
+      // Neither + and * in the resultant expression are nsw.
       Reduced = Builder.CreateAdd(Basis.Ins, Bump);
     }
     break;
