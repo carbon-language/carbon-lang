c32ed570bf5754ce5ddc46ae6e9a5e7cd2a26dc3
Rafael Espindola <rafael.espindola@gmail.com>
2015-06-22 15:06:17 +0000

    Support Solaris unused sections' gc link syntax.
    It is not clear if this would work or not with LLVM_NO_DEAD_STRIP
    binaries, so be conservative for now.
    
    Patch by Xan López.
    
    llvm-svn: 240287

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -186,6 +186,9 @@ function(add_link_opts target_name)
         # ld64's implementation of -dead_strip breaks tools that use plugins.
         set_property(TARGET ${target_name} APPEND_STRING PROPERTY
                      LINK_FLAGS " -Wl,-dead_strip")
+      elseif(${CMAKE_SYSTEM_NAME} MATCHES "SunOS")
+        set_property(TARGET ${target_name} APPEND_STRING PROPERTY
+                     LINK_FLAGS " -Wl,-z -Wl,discard-unused=sections")
       elseif(NOT WIN32 AND NOT LLVM_LINKER_IS_GOLD)
         # Object files are compiled with -ffunction-data-sections.
         # Versions of bfd ld < 2.23.1 have a bug in --gc-sections that breaks
