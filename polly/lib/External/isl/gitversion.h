0c1c55345d73185e62ee23a37537ea9a3daac7b2
Michael Kruse <llvm@meinersbur.de>
2015-06-22 18:07:42 +0000

    Replace repository version of ISL by 'make dist' output
    The 'make dist' archive is not dependent on ./configure output and
    contains a GIT_HEAD_ID file that identifies the version of ISL used.
    
    None of the files added or removed are used part of Polly's build
    process (except of GIT_HEAD_ID since the previous revision r240301). No
    functional change intended.
    
    llvm-svn: 240306

--- polly/lib/External/isl/gitversion.h
+++
@@ -1,1 +0,0 @@
-#define GIT_HEAD_ID "isl-0.15-3-g532568a"
--- polly/lib/External/isl/interface/python.h
+++
@@ -1,7 +0,0 @@
-#include <set>
-#include <clang/AST/Decl.h>
-
-using namespace std;
-using namespace clang;
-
-void generate_python(set<RecordDecl *> &types, set<FunctionDecl *> functions);
