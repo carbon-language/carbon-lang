ecc728bde9a75d1e67e0693bc21afbeb564fcf27
Pavel Labath <labath@google.com>
2015-06-25 15:25:30 +0000

    Update TestPrintStackTraces XFAIL once more
    it seems the failure happens also with clang. The main thing which triggers the failure is
    architecture.
    
    llvm-svn: 240652

--- lldb/test/python_api/lldbutil/process/TestPrintStackTraces.py
+++ lldb/test/python_api/lldbutil/process/TestPrintStackTraces.py
@@ -18,7 +18,7 @@ class ThreadsStackTracesTestCase(TestBase):
         # Find the line number to break inside main().
         self.line = line_number('main.cpp', '// Set break point at this line.')
 
-    @expectedFailureAll("llvm.org/pr23043", ["linux"], "gcc", archs=["i386"]) # We are unable to produce a backtrace of the main thread when the thread is blocked in fgets
+    @expectedFailureAll("llvm.org/pr23043", ["linux"], archs=["i386"]) # We are unable to produce a backtrace of the main thread when the thread is blocked in fgets
     @python_api_test
     def test_stack_traces(self):
         """Test SBprocess and SBThread APIs with printing of the stack traces."""
