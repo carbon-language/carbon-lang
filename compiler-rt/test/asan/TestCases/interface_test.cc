8dbebeb53fc5094bbbee2d328c964b01d81cd789
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-06-26 23:43:03 +0000

    [asan] Add -pie to uninstrumented executables on interface_test.cc
    This helps Android (which only support PIE) and does not hurt anything else.
    
    llvm-svn: 240857

--- compiler-rt/test/asan/TestCases/interface_test.cc
+++ compiler-rt/test/asan/TestCases/interface_test.cc
@@ -1,8 +1,8 @@
 // Check that user may include ASan interface header.
 // RUN: %clang_asan %s -o %t && %run %t
 // RUN: %clang_asan -x c %s -o %t && %run %t
-// RUN: %clang %s -o %t && %run %t
-// RUN: %clang -x c %s -o %t && %run %t
+// RUN: %clang %s -pie -o %t && %run %t
+// RUN: %clang -x c %s -pie -o %t && %run %t
 #include <sanitizer/asan_interface.h>
 
 int main() {
