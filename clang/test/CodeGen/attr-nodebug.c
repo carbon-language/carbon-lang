889d722e876ccaf684ef07be65c1d6673640b904
Paul Robinson <paul_robinson@playstation.sony.com>
2015-06-26 00:36:50 +0000

    FileCheck-ize test and make sure more things don't happen.
    Attribute 'nodebug' means no llvm.dbg.* intrinsics, no !dbg
    annotations, and no DISubprogram for the function.
    
    Differential Revision: http://reviews.llvm.org/D10747
    
    llvm-svn: 240747

--- clang/test/CodeGen/attr-nodebug.c
+++ clang/test/CodeGen/attr-nodebug.c
@@ -1,12 +1,32 @@
-// RUN: %clang_cc1 -g -emit-llvm -o %t %s
-// RUN: not grep 'call void @llvm.dbg.func.start' %t
+// RUN: %clang_cc1 -g -emit-llvm -o - %s | FileCheck %s
 
 void t1() __attribute__((nodebug));
 
 void t1()
 {
   int a = 10;
-  
   a++;
 }
+
+void t2()
+{
+  int b = 10;
+  b++;
+}
+
+// With nodebug, IR should have no llvm.dbg.* calls, or !dbg annotations.
+// CHECK-LABEL: @t1
+// CHECK-NOT:   dbg
+// CHECK:       }
+
+// For sanity, check those things do occur normally.
+// CHECK-LABEL: @t2
+// CHECK:       call{{.*}}llvm.dbg
+// CHECK:       !dbg
+// CHECK:       }
+
+// We should see a function description for t2 but not t1.
+// CHECK-NOT: DISubprogram(name: "t1"
+// CHECK:     DISubprogram(name: "t2"
+// CHECK-NOT: DISubprogram(name: "t1"
 
