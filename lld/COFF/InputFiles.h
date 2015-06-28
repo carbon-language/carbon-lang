b0a360bf153d7c7dcbd572cf939a2cdbed621949
Rui Ueyama <ruiu@google.com>
2015-06-28 02:00:33 +0000

    COFF: Remove useless "explicit".
    llvm-svn: 240899

--- lld/COFF/InputFiles.h
+++ lld/COFF/InputFiles.h
@@ -58,7 +58,7 @@ public:
   StringRef getDirectives() { return StringRef(Directives).trim(); }
 
 protected:
-  explicit InputFile(Kind K, MemoryBufferRef M) : MB(M), FileKind(K) {}
+  InputFile(Kind K, MemoryBufferRef M) : MB(M), FileKind(K) {}
   MemoryBufferRef MB;
   std::string Directives;
 
