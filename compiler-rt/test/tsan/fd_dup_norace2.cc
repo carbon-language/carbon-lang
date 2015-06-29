29093488d8971e147b7c413099be931ee842ddd2
Dmitry Vyukov <dvyukov@google.com>
2015-06-29 13:56:31 +0000

    tsan: fix flaky test
    See the comment for explanation.
    
    llvm-svn: 240943

--- compiler-rt/test/tsan/fd_dup_norace2.cc
+++ compiler-rt/test/tsan/fd_dup_norace2.cc
@@ -3,6 +3,7 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
+#include <errno.h>
 
 // dup2(oldfd, newfd) races with read(newfd).
 // This is not reported as race because:
@@ -14,14 +15,33 @@ int fd;
 
 void *Thread(void *x) {
   char buf;
-  if (read(fd, &buf, 1) != 1)
-    exit(printf("read failed\n"));
+  int n = read(fd, &buf, 1);
+  if (n != 1) {
+    // This read can "legitimately" fail regadless of the fact that glibc claims
+    // that "there is no instant in the middle of calling dup2 at which new is
+    // closed and not yet a duplicate of old". Strace of the failing runs
+    // looks as follows:
+    //
+    //    [pid 122196] open("/dev/urandom", O_RDONLY) = 3
+    //    [pid 122196] open("/dev/urandom", O_RDONLY) = 4
+    //    Process 122382 attached
+    //    [pid 122382] read(3,  <unfinished ...>
+    //    [pid 122196] dup2(4, 3 <unfinished ...>
+    //    [pid 122382] <... read resumed> 0x7fcd139960b7, 1) = -1 EBADF (Bad file descriptor)
+    //    [pid 122196] <... dup2 resumed> )       = 3
+    //    read failed: n=-1 errno=9
+    //
+    // The failing read does not interfere with what this test tests,
+    // so we just ignore the failure.
+    //
+    // exit(printf("read failed: n=%d errno=%d\n", n, errno));
+  }
   return 0;
 }
 
 int main() {
-  fd = open("/dev/random", O_RDONLY);
-  int fd2 = open("/dev/random", O_RDONLY);
+  fd = open("/dev/urandom", O_RDONLY);
+  int fd2 = open("/dev/urandom", O_RDONLY);
   if (fd == -1 || fd2 == -1)
     exit(printf("open failed\n"));
   pthread_t th;
