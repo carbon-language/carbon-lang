942eca7e48eeaf005d65572c01bab67d3e360f38
NAKAMURA Takumi <geek4civic@gmail.com>
2015-07-01 10:28:09 +0000

    Revert part of r241149, "Fix PR23872: Integrated assembler error message when using .type directive with @ in AArch32 assembly."
    The test should be split among targets. llvm/test/MC/ELF/ is assumed as X86.
    
    llvm-svn: 241151

--- llvm/test/MC/ELF/gnu-type-diagnostics.s
+++ llvm/test/MC/ELF/gnu-type-diagnostics.s
@@ -1,29 +1,4 @@
 // RUN: not llvm-mc -triple i686-elf -filetype asm -o /dev/null %s 2>&1 | FileCheck %s
-// RUN: not llvm-mc -triple aarch64-elf -filetype asm -o /dev/null %s 2>&1 | FileCheck %s
-// RUN: not llvm-mc -triple arm-elf -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple armeb-elf -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumb-elf -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumbeb-elf -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple arm-coff -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple armeb-coff -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumb-coff -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumbeb-coff -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple arm-apple -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple armeb-apple -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumb-apple -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
-// RUN: not llvm-mc -triple thumbeb-apple -filetype asm -o /dev/null %s 2>&1 \
-// RUN:     | FileCheck -check-prefix=CHECK-INVALID-AT-IN-TYPE-DIRECTIVE %s
 
 	.type TYPE FUNC
 // CHECK: error: unsupported attribute in '.type' directive
@@ -40,7 +15,4 @@
 // CHECK: .type symbol 32
 // CHECK:              ^
 
-// CHECK-INVALID-AT-IN-TYPE-DIRECTIVE: error: expected STT_<TYPE_IN_UPPER_CASE>, '#<type>', '%<type>' or "<type>"
-// CHECK-INVALID-AT-IN-TYPE-DIRECTIVE: .type symbol 32
-// CHECK-INVALID-AT-IN-TYPE-DIRECTIVE:              ^
 
