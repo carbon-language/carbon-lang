73b076159bf679e5584c52a487f18dd8cef43114
Alexandros Lamprineas <alexandros.lamprineas@arm.com>
2015-07-02 13:21:34 +0000

    test commit
    Change-Id: Ibaa2e1115544ddfa491335b81226819fd9cc04b4
    llvm-svn: 241260

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -4224,7 +4224,9 @@ public:
     // zero length bitfield.
     UseZeroLengthBitfieldAlignment = true;
   }
+
   StringRef getABI() const override { return ABI; }
+
   bool setABI(const std::string &Name) override {
     ABI = Name;
 
