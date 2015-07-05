b120712b2949170bb15396dfbf7a349461feb2bb
Jay Foad <jay.foad@gmail.com>
2015-07-05 08:11:36 +0000

    Enable this test for PPC64.
    llvm-svn: 241400

--- compiler-rt/test/msan/vector_select.cc
+++ compiler-rt/test/msan/vector_select.cc
@@ -11,7 +11,7 @@ __m128d select(bool b, __m128d c, __m128d d)
 {
   return b ? c : d;
 }
-#elif defined (__mips64)
+#elif defined (__mips64) || defined (__powerpc64__)
 typedef double __w64d __attribute__ ((vector_size(16)));
 
 __w64d select(bool b, __w64d c, __w64d d)
