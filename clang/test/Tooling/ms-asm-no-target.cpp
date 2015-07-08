8c73ba65bbc091e4809786b5c716193805350a61
Adrian Prantl <aprantl@apple.com>
2015-07-08 01:39:38 +0000

    Change the expectation for test/Tooling/ms-asm-no-target.cpp since clang-check now initializes the available targets to support clang module containers.
    llvm-svn: 241656

--- clang/test/Tooling/ms-asm-no-target.cpp
+++ clang/test/Tooling/ms-asm-no-target.cpp
@@ -1,13 +1,13 @@
-// RUN: not clang-check "%s" -- -fasm-blocks -target x86_64-apple-darwin10 2>&1 | FileCheck -check-prefix=CHECK-X86 %s
+// RUN: clang-check "%s" -- -fasm-blocks -target x86_64-apple-darwin10 2>&1 | FileCheck -check-prefix=CHECK-X86 %s -allow-empty
 // RUN: not clang-check "%s" -- -fasm-blocks -target powerpc-apple-darwin10 2>&1 | FileCheck -check-prefix=CHECK-PPC %s
-
-// Test that we diagnose instead of crashing when the application hasn't
-// initialized LLVM targets supporting the MS-style inline asm parser.
-// Also test that the ordinary error is emitted on unsupported architectures.
+// REQUIRES: x86-registered-target
 
 void Break() {
   __asm { int 3 }
 }
 
-// CHECK-X86: error: MS-style inline assembly is not available
+// clang-check should initialize the x86 target, so x86 should work.
+// CHECK-X86-NOT: error: MS-style inline assembly is not available
+
+// Test that the ordinary error is emitted on unsupported architectures.
 // CHECK-PPC: error: Unsupported architecture 'powerpc' for MS-style inline assembly
