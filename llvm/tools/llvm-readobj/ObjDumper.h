20546ffd4aa654066d7af1eed475598c1f1ac7b9
Michael J. Spencer <bigcheesegs@gmail.com>
2015-07-09 22:32:24 +0000

    [Object][ELF] Support dumping hash-tables from files with no section table.
    This time without breaking the bots.
    
    llvm-svn: 241869

--- llvm/tools/llvm-readobj/ObjDumper.h
+++ llvm/tools/llvm-readobj/ObjDumper.h
@@ -37,6 +37,7 @@ public:
   virtual void printDynamicTable() { }
   virtual void printNeededLibraries() { }
   virtual void printProgramHeaders() { }
+  virtual void printHashTable() { }
 
   // Only implemented for ARM ELF at this time.
   virtual void printAttributes() { }
