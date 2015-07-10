2ef5ec6b2bc0c0bcc5b7b09004de4038fe1f901e
Tom Stellard <thomas.stellard@amd.com>
2015-07-10 13:37:07 +0000

    Fix implementation of sqrt v2
    Passing values less than 0 to the llvm.sqrt() intrinsic results in
    undefined behavior, so we need to check the input and return NaN if
    is is less than 0.
    
    v2:
      - Fix build failures.
    
    llvm-svn: 241906

--- libclc/generic/include/clc/math/sqrt.h
+++ libclc/generic/include/clc/math/sqrt.h
@@ -1,6 +1,3 @@
-#undef sqrt
-#define sqrt __clc_sqrt
-
-#define __CLC_FUNCTION __clc_sqrt
-#define __CLC_INTRINSIC "llvm.sqrt"
-#include <clc/math/unary_intrin.inc>
+#define __CLC_BODY <clc/math/sqrt.inc>
+#include <clc/math/gentype.inc>
+#undef __CLC_BODY
