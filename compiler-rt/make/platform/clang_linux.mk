f8f780ccc5ad5f26e6473ffbbca71b4d7f98d255
Justin Bogner <mail@justinbogner.com>
2015-07-13 06:39:53 +0000

    profile: Autoconf build system support for r241824
    r241824 didn't update the autoconf build system to handle the new file
    in the profile library.
    
    llvm-svn: 242013

--- compiler-rt/make/platform/clang_linux.mk
+++ compiler-rt/make/platform/clang_linux.mk
@@ -78,7 +78,7 @@ FUNCTIONS.builtins-i386 := $(CommonFunctions) $(ArchFunctions.i386)
 FUNCTIONS.builtins-x86_64 := $(CommonFunctions) $(ArchFunctions.x86_64)
 FUNCTIONS.profile-i386 := GCDAProfiling InstrProfiling InstrProfilingBuffer \
                           InstrProfilingFile InstrProfilingPlatformOther \
-                          InstrProfilingRuntime
+                          InstrProfilingRuntime InstrProfilingUtil
 FUNCTIONS.profile-x86_64 := $(FUNCTIONS.profile-i386)
 
 # Always use optimized variants.
