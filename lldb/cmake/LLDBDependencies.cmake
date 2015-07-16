109ef8cf6b3aa56e4abf1e292900b5ff6fd4eddf
Chaoren Lin <chaorenl@google.com>
2015-07-16 23:45:08 +0000

    Re-landing r242318 after buildbot update.
    llvm-svn: 242488

--- lldb/cmake/LLDBDependencies.cmake
+++ lldb/cmake/LLDBDependencies.cmake
@@ -155,7 +155,12 @@ endif()
 list(APPEND LLDB_SYSTEM_LIBS ${system_libs})
 
 if (LLVM_BUILD_STATIC)
-  list(APPEND LLDB_SYSTEM_LIBS python2.7 z util termcap gpm ssl crypto bsd)
+  if (NOT LLDB_DISABLE_PYTHON)
+    list(APPEND LLDB_SYSTEM_LIBS python2.7 util)
+  endif()
+  if (NOT LLDB_DISABLE_CURSES)
+    list(APPEND LLDB_SYSTEM_LIBS gpm)
+  endif()
 endif()
 
 set( LLVM_LINK_COMPONENTS
