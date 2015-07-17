b873e84bd92f88966eca5834d5c5bcbca88608e3
Enrico Granata <egranata@apple.com>
2015-07-17 21:11:46 +0000

    Make this test case be somewhat less verbose when not asked to
    llvm-svn: 242573

--- lldb/test/expression_command/formatters/TestFormatters.py
+++ lldb/test/expression_command/formatters/TestFormatters.py
@@ -50,12 +50,13 @@ class ExprFormattersTestCase(TestBase):
         lldbutil.run_break_set_by_file_and_line (self, "main.cpp", self.line, loc_exact=True)
 
         self.runCmd("run", RUN_SUCCEEDED)
-        self.runCmd("script import formatters")
-        self.runCmd("script import foosynth")
+        self.runCmd("command script import formatters.py")
+        self.runCmd("command script import foosynth.py")
         
-        self.runCmd("frame variable foo1 --show-types")
-        self.runCmd("frame variable foo1.b --show-types")
-        self.runCmd("frame variable foo1.b.b_ref --show-types")
+        if self.TraceOn():
+            self.runCmd("frame variable foo1 --show-types")
+            self.runCmd("frame variable foo1.b --show-types")
+            self.runCmd("frame variable foo1.b.b_ref --show-types")
 
         self.expect("expression --show-types -- *(new foo(47))",
             substrs = ['(int) a = 47', '(bar) b = {', '(int) i = 94', '(baz) b = {', '(int) k = 99'])
