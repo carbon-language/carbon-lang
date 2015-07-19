838838f06a4e7e9c67039233af9f2c9687cee70f
Davide Italiano <davide@freebsd.org>
2015-07-19 22:07:14 +0000

    [Sema] Make an assertion stricter.
    We now check for the exact range of IdealIndex.
    
    llvm-svn: 242652

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -4138,7 +4138,7 @@ static void DiagnoseBaseOrMemInitializerOrder(
         if (InitKey == IdealInitKeys[IdealIndex])
           break;
 
-      assert(IdealIndex != NumIdealInits &&
+      assert(IdealIndex >= 0 && IdealIndex < NumIdealInits &&
              "initializer not found in initializer list");
     }
 
