6b7fff9ce57f42dbc0f20f8884a210007b201aa6
Akira Hatanaka <ahatanaka@apple.com>
2015-07-20 23:20:53 +0000

    [ARM] Pass subtarget feature "+reserve-r9" instead of passing backend option "-arm-reserve-r9".
    This change is needed since backend options do not make it to the backend
    when doing LTO and are not capable of changing the behavior of code-gen
    passes on a per-function basis.
    
    rdar://problem/21529937
    
    Differential Revision: http://reviews.llvm.org/D11319
    
    llvm-svn: 242736

--- clang/test/Driver/arm-fixed-r9.c
+++ clang/test/Driver/arm-fixed-r9.c
@@ -1,4 +1,4 @@
 // RUN: %clang -target arm-none-gnueabi -ffixed-r9 -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-FIXED-R9 < %t %s
 
-// CHECK-FIXED-R9: "-backend-option" "-arm-reserve-r9"
+// CHECK-FIXED-R9: "-target-feature" "+reserve-r9"
