bf5a711e9632055cfb0a2fce92d836d72078d4ac
Michael Kruse <llvm@meinersbur.de>
2015-07-21 12:01:14 +0000

    Add configure-time test for latest ISL
    Query the isl_config.h macros recently added to ISL. One of it looks for
    the ffs (find first set), whose functionality is available in Visual
    Studio with _BitScanForward. Also add isl_ffs.c to the source files
    which contains the implementation of ffs using _BitScanForward.
    
    Reviewers: grosser
    llvm-svn: 242770

--- polly/lib/External/isl_config.h.cmake
+++ polly/lib/External/isl_config.h.cmake
@@ -1,7 +1,9 @@
+/* define if your compiler has __attribute__ */
+#cmakedefine HAVE___ATTRIBUTE__ /**/
 
-/* most gcc compilers know a function __attribute__((__warn_unused_result__))
-   */
+/* most gcc compilers know a function __attribute__((__warn_unused_result__)) */
 #define GCC_WARN_UNUSED_RESULT @GCC_WARN_UNUSED_RESULT@
+
 
 /* Define to 1 if you have the declaration of `ffs', and to 0 if you don't. */
 #define HAVE_DECL_FFS @HAVE_DECL_FFS@
@@ -10,8 +12,37 @@
    don't. */
 #define HAVE_DECL___BUILTIN_FFS @HAVE_DECL___BUILTIN_FFS@
 
-/* define if your compiler has __attribute__ */
-#cmakedefine HAVE___ATTRIBUTE__ /**/
+/* Define to 1 if you have the declaration of `_BitScanForward', and to 0 if
+   you don't. */
+#define HAVE_DECL__BITSCANFORWARD @HAVE_DECL__BITSCANFORWARD@
+
+
+/* Define to 1 if you have the declaration of `strcasecmp', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRCASECMP @HAVE_DECL_STRCASECMP@
+
+/* Define to 1 if you have the declaration of `_stricmp', and to 0 if you
+   don't. */
+#define HAVE_DECL__STRICMP @HAVE_DECL__STRICMP@
+
+
+/* Define to 1 if you have the declaration of `strncasecmp', and to 0 if you
+   don't. */
+#define HAVE_DECL_STRNCASECMP @HAVE_DECL_STRNCASECMP@
+
+/* Define to 1 if you have the declaration of `_strnicmp', and to 0 if you
+   don't. */
+#define HAVE_DECL__STRNICMP @HAVE_DECL__STRNICMP@
+
+
+/* Define to 1 if you have the declaration of `snprintf', and to 0 if you
+   don't. */
+#define HAVE_DECL_SNPRINTF @HAVE_DECL_SNPRINTF@
+
+/* Define to 1 if you have the declaration of `_snprintf', and to 0 if you
+   don't. */
+#define HAVE_DECL__SNPRINTF @HAVE_DECL__SNPRINTF@
+
 
 /* use gmp to implement isl_int */
 #cmakedefine USE_GMP_FOR_MP
