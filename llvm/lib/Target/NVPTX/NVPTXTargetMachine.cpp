20d73c6cc0e0010edd5e3239b78225388380f0d6
Jingyue Wu <jingyue@google.com>
2015-07-22 04:16:52 +0000

    [BranchFolding] do not iterate the aliases of virtual registers
    Summary:
    MCRegAliasIterator only works for physical registers. So, do not run it
    on virtual registers.
    
    With this issue fixed, we can resurrect the BranchFolding pass in NVPTX
    backend.
    
    Reviewers: jholewinski, bkramer
    
    Subscribers: henryhu, meheff, llvm-commits, jholewinski
    
    Differential Revision: http://reviews.llvm.org/D11174
    
    llvm-svn: 242871

--- llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
+++ llvm/lib/Target/NVPTX/NVPTXTargetMachine.cpp
@@ -163,7 +163,6 @@ void NVPTXPassConfig::addIRPasses() {
   // NVPTXPrologEpilog pass (see NVPTXPrologEpilogPass.cpp).
   disablePass(&PrologEpilogCodeInserterID);
   disablePass(&MachineCopyPropagationID);
-  disablePass(&BranchFolderPassID);
   disablePass(&TailDuplicateID);
 
   addPass(createNVPTXImageOptimizerPass());
