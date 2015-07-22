f8e102da6abf797f834ce9792807dc7568c18ceb
Chaoren Lin <chaorenl@google.com>
2015-07-22 01:17:51 +0000

    Xfail test_dyld_library_path for Android.
    Originally, the source for the hidden lib_d and the regular lib_d were the same
    file, so we always got the "correct" source for each. Splitting them up in
    D11367 exposed a bug of showing the incorrect source file for the hidden lib_d.
    
    llvm-svn: 242862

--- lldb/test/functionalities/load_unload/TestLoadUnload.py
+++ lldb/test/functionalities/load_unload/TestLoadUnload.py
@@ -121,6 +121,7 @@ class LoadUnloadTestCase(TestBase):
 
     @skipIfFreeBSD # llvm.org/pr14424 - missing FreeBSD Makefiles/testcase support
     @skipUnlessListedRemote(['android'])
+    @expectedFailureAndroid # wrong source file shows up for hidden library
     def test_dyld_library_path(self):
         """Test (DY)LD_LIBRARY_PATH after moving libd.dylib, which defines d_function, somewhere else."""
 
