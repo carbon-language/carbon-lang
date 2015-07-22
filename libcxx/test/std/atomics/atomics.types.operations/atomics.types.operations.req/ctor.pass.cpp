2ea8daaee1bcd639ea1663eb94954e348c535dea
Justin Bogner <mail@justinbogner.com>
2015-07-22 23:32:57 +0000

    Mark this test as XFAIL with older compilers, since they hit PR18097
    llvm-svn: 242967

--- libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.req/ctor.pass.cpp
+++ libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.req/ctor.pass.cpp
@@ -10,6 +10,10 @@
 // UNSUPPORTED: libcpp-has-no-threads
 // UNSUPPORTED: c++98, c++03
 
+// NOTE: atomic<> of a TriviallyCopyable class is wrongly rejected by older
+// clang versions. It was fixed right before the llvm 3.5 release. See PR18097.
+// XFAIL: apple-clang-6.0, clang-3.4, clang-3.3
+
 // <atomic>
 
 // constexpr atomic<T>::atomic(T value)
