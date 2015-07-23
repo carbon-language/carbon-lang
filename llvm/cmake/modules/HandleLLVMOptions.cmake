5519ad3368ddc443543a11a49ec3ffebc008eac5
Yaron Keren <yaron.keren@gmail.com>
2015-07-23 08:06:12 +0000

    Avoid using -Wl,-z,defs on Cygwin.
    Prior to CMAKE 2.8.4 that was covered by the WIN32 conditional but
    from 2.8.4 CMAKE no longer defined WIN32 when running under Cygwin
    and it needs its own test.
    
    Patch by Martell Malone!
    
    http://reviews.llvm.org/D11347
    
    llvm-svn: 242993

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -131,7 +131,7 @@ endif()
 
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
-if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR
+if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
         ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD") AND
    NOT LLVM_USE_SANITIZER)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
