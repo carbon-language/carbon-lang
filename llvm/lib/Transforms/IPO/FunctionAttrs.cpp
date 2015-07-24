b9bf447d9018209304d22b5b485fd315cc468236
Diego Novillo <dnovillo@google.com>
2015-07-24 19:18:32 +0000

    Remove unused variable. NFC.
    llvm-svn: 243145

--- llvm/lib/Transforms/IPO/FunctionAttrs.cpp
+++ llvm/lib/Transforms/IPO/FunctionAttrs.cpp
@@ -415,7 +415,6 @@ determinePointerReadAttrs(Argument *A,
                                                        
   SmallVector<Use*, 32> Worklist;
   SmallSet<Use*, 32> Visited;
-  int Count = 0;
 
   // inalloca arguments are always clobbered by the call.
   if (A->hasInAllocaAttr())
