2da6ec2ee6da456e13c61e0c7a913bcd9b6b9224
Akira Hatanaka <ahatanaka@apple.com>
2015-07-25 00:33:57 +0000

    Fix test case.
    This is a follow-up to r243185, which made changes to pass subtarget
    feature "+reserve-x18" instead of backend option "-aarch64-reserve-x18".
    
    llvm-svn: 243189

--- clang/test/Driver/aarch64-fixed-x18.c
+++ clang/test/Driver/aarch64-fixed-x18.c
@@ -1,4 +1,4 @@
 // RUN: %clang -target aarch64-none-gnu -ffixed-x18 -### %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-FIXED-X18 < %t %s
 
-// CHECK-FIXED-X18: "-backend-option" "-aarch64-reserve-x18"
+// CHECK-FIXED-X18: "-target-feature" "+reserve-x18"
