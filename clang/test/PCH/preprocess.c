00bd0a403419952b46ae2e679dc71f8d044f115c
Sean Silva <chisophugis@gmail.com>
2015-07-30 01:21:56 +0000

    Remove bad test.
    We currently don't canonicalize paths in the preprocessed files.
    But we do when writing to PCH.
    This causes a discrepancy on Windows with the test below.
    This test fails even on unix if you change the test to use
    `%S//preprocess.h`.
    
    I am led to conclude that the invariant that this test was intending to
    test has not been upheld for a while (and may never have been).
    
    llvm-svn: 243602

--- clang/test/PCH/preprocess.c
+++
@@ -1,8 +0,0 @@
-// Check that -E mode is invariant when using an implicit PCH.
-
-// RUN: %clang_cc1 -include %S/preprocess.h -E -o %t.orig %s
-// RUN: %clang_cc1 -emit-pch -o %t %S/preprocess.h
-// RUN: %clang_cc1 -include-pch %t -E -o %t.from_pch %s
-// RUN: diff %t.orig %t.from_pch
-
-a_typedef a_value;
--- clang/test/PCH/preprocess.h
+++
@@ -1,7 +0,0 @@
-// Helper header for preprocess.c PCH test
-#ifndef PREPROCESS_H
-#define PREPROCESS_H
-
-typedef int a_typedef;
-
-#endif // PREPROCESS_H
