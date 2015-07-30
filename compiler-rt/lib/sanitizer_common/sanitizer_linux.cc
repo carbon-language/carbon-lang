043717d5724b88c313d6bb6a82e28d92088636b9
Daniel Sanders <daniel.sanders@imgtec.com>
2015-07-30 21:00:48 +0000

    [Sanitizers] [Bug 24151] Generalize type of offset in internal_mmap
    Summary:
    Using u64 as type for offset changes its value, changing starting address for map in file.
    This patch solves Bug 24151, which raises issue while mapping file in mips32.
    
    Patch by Mohit Bhakkad
    
    Reviewers: dsanders, kcc
    
    Subscribers: hans, llvm-commits, samsonov, nitesh.jain, sagar, bhushan, jaydeep
    
    Differential Revision: http://reviews.llvm.org/D11588
    
    llvm-svn: 243686

--- compiler-rt/lib/sanitizer_common/sanitizer_linux.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_linux.cc
@@ -104,7 +104,7 @@ namespace __sanitizer {
 
 // --------------- sanitizer_libc.h
 uptr internal_mmap(void *addr, uptr length, int prot, int flags, int fd,
-                   u64 offset) {
+                   OFF_T offset) {
 #if SANITIZER_FREEBSD || SANITIZER_LINUX_USES_64BIT_SYSCALLS
   return internal_syscall(SYSCALL(mmap), (uptr)addr, length, prot, flags, fd,
                           offset);
