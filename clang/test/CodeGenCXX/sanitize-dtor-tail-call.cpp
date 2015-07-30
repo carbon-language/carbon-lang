690132b344214d3105c8ea7a7e7715927942528b
Naomi Musgrave <nmusgrave@google.com>
2015-07-30 17:59:52 +0000

    Testing to verify function attribute disable-tail-calls applied to destructor
    llvm-svn: 243671

--- clang/test/CodeGenCXX/sanitize-dtor-tail-call.cpp
+++ clang/test/CodeGenCXX/sanitize-dtor-tail-call.cpp
@@ -1,4 +1,5 @@
 // Test -fsanitize-memory-use-after-dtor
+// RUN: %clang_cc1 -fsanitize=memory -fsanitize-memory-use-after-dtor -disable-llvm-optzns -std=c++11 -triple=x86_64-pc-linux -emit-llvm -o - %s | FileCheck %s
 // RUN: %clang_cc1 -O1 -fsanitize=memory -fsanitize-memory-use-after-dtor -disable-llvm-optzns -std=c++11 -triple=x86_64-pc-linux -emit-llvm -o - %s | FileCheck %s
 
 struct Simple {
@@ -17,3 +18,7 @@ Simple s;
 // CHECK: {{^ *}}call void @__sanitizer_dtor_callback
 // CHECK-NOT: tail call void @__sanitizer_dtor_callback
 // CHECK: ret void
+
+// Destructor does not emit any tail calls
+// CHECK: attributes #1 = {{.*("disable-tail-calls"="true").*}}
+// CHECK: }{{$}}
