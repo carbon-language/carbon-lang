fbe7a2e8411d124aa2ce4a8f5e821aa9ab76913a
Tobias Grosser <tobias@grosser.es>
2015-08-04 11:49:39 +0000

    Run ScopInfo after the ScopViewers
    This change is required to see the detected scops even in cases where there is
    no other ScopInfo user after the ScopViewers. Before this change, when
    running with -polly-optimizer=none -polly-code-generator=none detected scops
    have not been shown.
    
    llvm-svn: 243971

--- polly/lib/Support/RegisterPasses.cpp
+++ polly/lib/Support/RegisterPasses.cpp
@@ -190,8 +190,6 @@ void registerPollyPasses(llvm::legacy::PassManagerBase &PM) {
   if (PollyDetectOnly)
     return;
 
-  PM.add(polly::createScopInfoPass());
-
   if (PollyViewer)
     PM.add(polly::createDOTViewerPass());
   if (PollyOnlyViewer)
@@ -200,6 +198,8 @@ void registerPollyPasses(llvm::legacy::PassManagerBase &PM) {
     PM.add(polly::createDOTPrinterPass());
   if (PollyOnlyPrinter)
     PM.add(polly::createDOTOnlyPrinterPass());
+
+  PM.add(polly::createScopInfoPass());
 
   if (ImportJScop)
     PM.add(polly::createJSONImporterPass());
