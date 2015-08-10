6ef95d6a9d088270ad634e194e9f68abf23bc6e0
NAKAMURA Takumi <geek4civic@gmail.com>
2015-08-10 04:21:19 +0000

    llvm/include/llvm/Support/Memory.h: Fix comment header.
    llvm-svn: 244429

--- llvm/include/llvm/Support/Memory.h
+++ llvm/include/llvm/Support/Memory.h
@@ -1,4 +1,4 @@
-//===- llvm/Support/Memory.h - Memory Support --------------------*- C++ -*-===//
+//===- llvm/Support/Memory.h - Memory Support -------------------*- C++ -*-===//
 //
 //                     The LLVM Compiler Infrastructure
 //
