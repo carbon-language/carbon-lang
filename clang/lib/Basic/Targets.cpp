2e0551f8d2c6414c11780716c66192c832e62365
Petar Jovanovic <petar.jovanovic@imgtec.com>
2015-08-11 21:27:39 +0000

    [MIPS] Use arch values for lock-free atomic operations
    Let NaClMips32ELTargetInfo inherit arch values for maximum width lock-free
    atomic operations.
    
    Differential Revision: http://reviews.llvm.org/D11949
    
    llvm-svn: 244675

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -6842,8 +6842,7 @@ void PNaClTargetInfo::getGCCRegAliases(const GCCRegAlias *&Aliases,
 class NaClMips32ELTargetInfo : public Mips32ELTargetInfo {
 public:
   NaClMips32ELTargetInfo(const llvm::Triple &Triple) :
-    Mips32ELTargetInfo(Triple)  {
-      MaxAtomicPromoteWidth = MaxAtomicInlineWidth = 0;
+    Mips32ELTargetInfo(Triple) {
   }
 
   BuiltinVaListKind getBuiltinVaListKind() const override {
