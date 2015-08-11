7c388187c942583ad99a6ae151ab52a1ab6f0963
Rafael Espindola <rafael.espindola@gmail.com>
2015-08-11 16:30:34 +0000

    Don't report a conflict between two local symbols.
    llvm-svn: 244624

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -59,8 +59,11 @@ template <class ELFT> void elf2::ObjectFile<ELFT>::initializeSymbols() {
   StringRef StringTable = *StringTableOrErr;
 
   Elf_Sym_Range Syms = ELFObj->symbols(Symtab);
-  Syms = Elf_Sym_Range(Syms.begin() + 1, Syms.end());
   auto NumSymbols = std::distance(Syms.begin(), Syms.end());
+  uint32_t FirstNonLocal = Symtab->sh_info;
+  if (FirstNonLocal > NumSymbols)
+    error("Invalid sh_info in symbol table");
+  Syms = llvm::make_range(Syms.begin() + FirstNonLocal, Syms.end());
   SymbolBodies.reserve(NumSymbols);
   for (const Elf_Sym &Sym : Syms)
     SymbolBodies.push_back(createSymbolBody(StringTable, &Sym));
---
+++ lld/test/elf2/local.s
@@ -0,0 +1,10 @@
+// RUN: llvm-mc -filetype=obj -triple=i686-unknown-linux %s -o %t
+// RUN: llvm-mc -filetype=obj -triple=i686-unknown-linux %p/Inputs/local.s -o %t2
+// RUN: lld -flavor gnu2 %t %t2 -o %t3
+// REQUIRES: x86
+
+.globl _start
+_start:
+        nop
+
+local:
