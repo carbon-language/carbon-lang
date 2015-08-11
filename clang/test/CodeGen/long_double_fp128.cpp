9cfd488443dd2b304e239cb89e4c4662758f6ce4
Derek Schuff <dschuff@google.com>
2015-08-11 00:19:53 +0000

    Add NaCl to long double/fp128 mangling test
    Summary:
    NaCl is a platform where long double is the same as double.
    Its mangling is spelled with "long double" but its ABI lowering is the same
    as double.
    
    Reviewers: rnk, chh
    
    Subscribers: jfb, cfe-commits, dschuff
    
    Differential Revision: http://reviews.llvm.org/D11922
    
    llvm-svn: 244541

--- clang/test/CodeGen/long_double_fp128.cpp
+++ clang/test/CodeGen/long_double_fp128.cpp
@@ -10,6 +10,8 @@
 // RUN:    | FileCheck %s --check-prefix=G32
 // RUN: %clang_cc1 -triple powerpc-linux-gnu -emit-llvm -o - %s \
 // RUN:    | FileCheck %s --check-prefix=P32
+// RUN: %clang_cc1 -triple x86_64-nacl -emit-llvm -o - %s \
+// RUN:    | FileCheck %s --check-prefix=N64
 
 // Check mangled name of long double.
 // Android's gcc and llvm use fp128 for long double.
@@ -20,3 +22,4 @@ void test(long, float, double, long double, long double _Complex) { }
 // A32:  define void @_Z4testlfdeCe(i32, float, double, double, { double, double }*
 // G32:  define void @_Z4testlfdeCe(i32, float, double, x86_fp80, { x86_fp80, x86_fp80 }*
 // P32:  define void @_Z4testlfdgCg(i32, float, double, ppc_fp128, { ppc_fp128, ppc_fp128 }*
+// N64: define void @_Z4testlfdeCe(i32, float, double, double, double {{.*}}, double
