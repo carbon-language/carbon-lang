c6f046893779316739e356180708781a412c5ce0
Greg Clayton <gclayton@apple.com>
2015-08-11 21:49:32 +0000

    Added missing files from checking regarding:
    http://reviews.llvm.org/D8712
    
    llvm-svn: 244681

---
+++ lldb/source/Symbol/TypeSystem.cpp
@@ -0,0 +1,20 @@
+//
+//  TypeSystem.cpp
+//  lldb
+//
+//  Created by Ryan Brown on 3/29/15.
+//
+//
+
+#include "lldb/Symbol/TypeSystem.h"
+
+using namespace lldb_private;
+
+TypeSystem::TypeSystem()
+{
+}
+
+TypeSystem::~TypeSystem()
+{
+    
+}
