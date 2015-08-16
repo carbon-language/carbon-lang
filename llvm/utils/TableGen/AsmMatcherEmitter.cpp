c4de7ee73d2e8a985e975a89de09feb84d5752a9
Craig Topper <craig.topper@gmail.com>
2015-08-16 21:27:08 +0000

    [TableGen] Move the ConversionRow vector into the ConversionTable instead of copying.
    llvm-svn: 245190

--- llvm/utils/TableGen/AsmMatcherEmitter.cpp
+++ llvm/utils/TableGen/AsmMatcherEmitter.cpp
@@ -1974,7 +1974,7 @@ static void emitConvertFuncs(CodeGenTarget &Target, StringRef ClassName,
       continue;
 
     // Add the row to the table.
-    ConversionTable.push_back(ConversionRow);
+    ConversionTable.push_back(std::move(ConversionRow));
   }
 
   // Finish up the converter driver function.
