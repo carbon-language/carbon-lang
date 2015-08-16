e0e6e5e11e8162c5ddf538766678457b9e44ec0b
Yaron Keren <yaron.keren@gmail.com>
2015-08-16 19:02:49 +0000

    Enable passing test on Windows + MSYS.
    llvm-svn: 245184

--- clang/test/Preprocessor/macro-multiline.c
+++ clang/test/Preprocessor/macro-multiline.c
@@ -1,5 +1,4 @@
 // RUN: printf -- "-DX=A\nTHIS_SHOULD_NOT_EXIST_IN_THE_OUTPUT" | xargs -0 %clang -E %s | FileCheck -strict-whitespace %s
-// REQUIRES: shell
 
 // Per GCC -D semantics, \n and anything that follows is ignored.
 
