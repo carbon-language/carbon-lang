2362b69dd9e1ad3b5be54780b051bb0f3860853f
Douglas Katzman <dougk@google.com>
2015-08-19 19:30:57 +0000

    [Sparc]: asm-only support for the ldstub instruction.
    llvm-svn: 245485

--- llvm/test/MC/Sparc/sparc-atomic-instructions.s
+++ llvm/test/MC/Sparc/sparc-atomic-instructions.s
@@ -12,3 +12,12 @@
 
         ! CHECK: swapa [%i0+%l6] 131, %o2   ! encoding: [0xd4,0xfe,0x10,0x76]
         swapa [%i0+%l6] 131, %o2
+
+        ! CHECK: ldstub [%i0+40], %g1 ! encoding: [0xc2,0x6e,0x20,0x28]
+        ldstub [%i0+40], %g1
+
+        ! CHECK: ldstub [%i0+%i2], %g1 ! encoding: [0xc2,0x6e,0x00,0x1a]
+        ldstub [%i0+%i2], %g1
+
+        ! CHECK: ldstuba [%i0+%i2] 131, %g1 ! encoding: [0xc2,0xee,0x10,0x7a]
+        ldstuba [%i0+%i2] 131, %g1
