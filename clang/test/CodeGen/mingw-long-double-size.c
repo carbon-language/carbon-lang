24b64a940579cbdb33554be5eeff8b9f6a0a331a
Yaron Keren <yaron.keren@gmail.com>
2015-08-19 17:18:32 +0000

    Add REQUIRES: x86-registered-target to test since it's required.
    llvm-svn: 245462

--- clang/test/CodeGen/mingw-long-double-size.c
+++ clang/test/CodeGen/mingw-long-double-size.c
@@ -1,3 +1,4 @@
+// REQUIRES: x86-registered-target
 // RUN: %clang_cc1 -triple i686-pc-windows-gnu -S %s  -o - | FileCheck %s -check-prefix=CHECK_I686
 // CHECK_I686: lda,12
 // RUN: %clang_cc1 -triple x86_64-pc-windows-gnu -S %s  -o - | FileCheck %s -check-prefix=CHECK_X86_64
