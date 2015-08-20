7defcae5f753e08eb9fc4a6ebb7a3b34a279177d
Eric Fiselier <eric@efcs.ca>
2015-08-20 00:10:22 +0000

    Fix more uses of uninitialized values in dynarray
    llvm-svn: 245525

--- libcxx/test/libcxx/experimental/containers/sequences/dynarray/dynarray.data/default.pass.cpp
+++ libcxx/test/libcxx/experimental/containers/sequences/dynarray/dynarray.data/default.pass.cpp
@@ -7,15 +7,13 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: c++98, c++03, c++11
+
 // dynarray.data
 
 // T* data() noexcept;
 // const T* data() const noexcept;
 
-  
-#include <__config>
-
-#if _LIBCPP_STD_VER > 11
 
 #include <experimental/dynarray>
 #include <cassert>
@@ -27,41 +25,44 @@
 using std::experimental::dynarray;
 
 template <class T>
-void dyn_test_const ( const dynarray<T> &dyn ) {
+void dyn_test_const(const dynarray<T> &dyn, bool CheckEquals = true) {
     const T *data = dyn.data ();
     assert ( data != NULL );
-    assert ( std::equal ( dyn.begin(), dyn.end(), data ));
+    if (CheckEquals) {
+        assert ( std::equal ( dyn.begin(), dyn.end(), data ));
     }
+}
 
 template <class T>
-void dyn_test ( dynarray<T> &dyn ) {
+void dyn_test( dynarray<T> &dyn, bool CheckEquals = true) {
     T *data = dyn.data ();
     assert ( data != NULL );
-    assert ( std::equal ( dyn.begin(), dyn.end(), data ));
+    if (CheckEquals) {
+        assert ( std::equal ( dyn.begin(), dyn.end(), data ));
     }
+}
 
     
 
 template <class T>
-void test ( const T &val ) {
+void test(const T &val, bool DefaultValueIsIndeterminate = false) {
     typedef dynarray<T> dynA;
+
+    const bool CheckDefaultValues = !DefaultValueIsIndeterminate;
+
+    dynA d1(4);
+    dyn_test(d1, CheckDefaultValues);
+    dyn_test_const(d1, CheckDefaultValues);
     
-    dynA d1 ( 4 );
-    dyn_test ( d1 );
-    dyn_test_const ( d1 );
-    
-    dynA d2 ( 7, val );
+    dynA d2 (7, val);
     dyn_test ( d2 );
     dyn_test_const ( d2 );
-    }
+}
 
 int main()
 {
-    test<int> ( 14 );
-    test<double> ( 14.0 );
+    test<int>(14, /* DefaultValueIsIndeterminate */ true);
+    test<double>(14.0, true);
     test<std::complex<double>> ( std::complex<double> ( 14, 0 ));
     test<std::string> ( "fourteen" );
 }
-#else
-int main() {}
-#endif
