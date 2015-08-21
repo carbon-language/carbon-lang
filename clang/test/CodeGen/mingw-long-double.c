cc84a1bea134e6e6d5aba3f6611947dafa8bb519
Yaron Keren <yaron.keren@gmail.com>
2015-08-21 08:26:31 +0000

    Expand mingw-long-double.c to test for long double alignment.
    llvm-svn: 245679

--- clang/test/CodeGen/mingw-long-double.c
+++ clang/test/CodeGen/mingw-long-double.c
@@ -1,6 +1,12 @@
 // REQUIRES: x86-registered-target
 // RUN: %clang_cc1 -triple i686-pc-windows-gnu -S %s  -o - | FileCheck %s -check-prefix=CHECK_I686
-// CHECK_I686: lda,12
+// CHECK_I686: _lda,12
+// CHECK_I686: _lds,16
 // RUN: %clang_cc1 -triple x86_64-pc-windows-gnu -S %s  -o - | FileCheck %s -check-prefix=CHECK_X86_64
 // CHECK_X86_64: lda,16
+// CHECK_X86_64: lds,32
 long double lda;
+struct {
+  char c;
+  long double ldb;
+} lds;
