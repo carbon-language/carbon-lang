68961bba063c3cfb867bf23f74364a42c4c535ea
Davide Italiano <davide@freebsd.org>
2015-08-25 18:27:59 +0000

    [MachO] Move trivial accessors to header.
    Requested by: Jim Grosbach.
    
    llvm-svn: 245963

--- llvm/include/llvm/Object/MachO.h
+++ llvm/include/llvm/Object/MachO.h
@@ -344,12 +344,6 @@ public:
   getLinkerOptionLoadCommand(const LoadCommandInfo &L) const;
   MachO::version_min_command
   getVersionMinLoadCommand(const LoadCommandInfo &L) const;
-  static uint32_t
-  getVersionMinMajor(MachO::version_min_command &C, bool SDK);
-  static uint32_t
-  getVersionMinMinor(MachO::version_min_command &C, bool SDK);
-  static uint32_t
-  getVersionMinUpdate(MachO::version_min_command &C, bool SDK);
   MachO::dylib_command
   getDylibIDLoadCommand(const LoadCommandInfo &L) const;
   MachO::dyld_info_command
@@ -426,6 +420,24 @@ public:
 
   static bool classof(const Binary *v) {
     return v->isMachO();
+  }
+
+  static uint32_t
+  getVersionMinMajor(MachO::version_min_command &C, bool SDK) {
+    uint32_t VersionOrSDK = (SDK) ? C.sdk : C.version;
+    return (VersionOrSDK >> 16) & 0xffff;
+  }
+
+  static uint32_t
+  getVersionMinMinor(MachO::version_min_command &C, bool SDK) {
+    uint32_t VersionOrSDK = (SDK) ? C.sdk : C.version;
+    return (VersionOrSDK >> 8) & 0xff;
+  }
+
+  static uint32_t
+  getVersionMinUpdate(MachO::version_min_command &C, bool SDK) {
+    uint32_t VersionOrSDK = (SDK) ? C.sdk : C.version;
+    return VersionOrSDK & 0xff;
   }
 
 private:
