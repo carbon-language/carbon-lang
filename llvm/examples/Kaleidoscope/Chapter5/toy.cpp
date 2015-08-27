f624d1ab8dbcf2a10e4d02eba004bb1e877c159e
Lang Hames <lhames@gmail.com>
2015-08-27 18:13:34 +0000

    Add Kaleidoscope regression tests.
    These will be run if LLVM_BUILD_EXAMPLES is enabled.
    
    llvm-svn: 246175

--- llvm/examples/Kaleidoscope/Chapter5/toy.cpp
+++ llvm/examples/Kaleidoscope/Chapter5/toy.cpp
@@ -906,13 +906,13 @@ static void MainLoop() {
 
 /// putchard - putchar that takes a double and returns 0.
 extern "C" double putchard(double X) {
-  putchar((char)X);
+  fputc((char)X, stderr);
   return 0;
 }
 
 /// printd - printf that takes a double prints it as "%f\n", returning 0.
 extern "C" double printd(double X) {
-  printf("%f\n", X);
+  fprintf(stderr, "%f\n", X);
   return 0;
 }
 
