e7708a84b9e7e74058f642486946d97c12e61c5c
Simon Pilgrim <llvm-dev@redking.me.uk>
2015-08-29 17:13:40 +0000

    [X86] Reapplied r246204, r246206, r246211, r246223
    (Re)added debug codegen test for F16C, FMA4, XOP + 3DNow! intrinsics
    
    Part of PR24590
    
    llvm-svn: 246363

--- clang/test/CodeGen/f16c-builtins.c
+++ clang/test/CodeGen/f16c-builtins.c
@@ -1,4 +1,6 @@
-// RUN: %clang_cc1 %s -O3 -triple=x86_64-apple-darwin -target-feature +f16c -emit-llvm -o - | FileCheck %s
+// REQUIRES: x86-registered-target
+// RUN: %clang_cc1 %s -O0 -triple=x86_64-apple-darwin -target-feature +f16c -emit-llvm -o - -Werror | FileCheck %s
+// RUN: %clang_cc1 %s -O0 -triple=x86_64-apple-darwin -target-feature +f16c -S -o - -Werror | FileCheck %s --check-prefix=CHECK-ASM
 
 // Don't include mm_malloc.h, it's system specific.
 #define __MM_MALLOC_H
@@ -7,20 +9,24 @@
 
 __m128 test_mm_cvtph_ps(__m128i a) {
   // CHECK: @llvm.x86.vcvtph2ps.128
+  // CHECK-ASM: vcvtph2ps %xmm{{.*}}, %xmm{{.*}}
   return _mm_cvtph_ps(a);
 }
 
 __m256 test_mm256_cvtph_ps(__m128i a) {
   // CHECK: @llvm.x86.vcvtph2ps.256
+  // CHECK-ASM: vcvtph2ps %xmm{{.*}}, %ymm{{.*}}
   return _mm256_cvtph_ps(a);
 }
 
 __m128i test_mm_cvtps_ph(__m128 a) {
   // CHECK: @llvm.x86.vcvtps2ph.128
+  // CHECK-ASM: vcvtps2ph $0, %xmm{{.*}}, %xmm{{.*}}
   return _mm_cvtps_ph(a, 0);
 }
 
 __m128i test_mm256_cvtps_ph(__m256 a) {
   // CHECK: @llvm.x86.vcvtps2ph.256
+  // CHECK-ASM: vcvtps2ph $0, %ymm{{.*}}, %xmm{{.*}}
   return _mm256_cvtps_ph(a, 0);
 }
