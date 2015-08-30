de1495b9b6585d3a8b3e48e511aa96c3ed208dca
Eric Fiselier <eric@efcs.ca>
2015-08-30 17:58:50 +0000

    Remove task to get C++03 tests passing from TODO.txt
    llvm-svn: 246392

--- libcxx/test/std/re/re.alg/re.alg.search/grep.pass.cpp
+++ libcxx/test/std/re/re.alg/re.alg.search/grep.pass.cpp
@@ -41,7 +41,7 @@ extern "C" void LLVMFuzzerTestOneInput(const char *data)
 
 void fuzz_tests()  // patterns that the fuzzer has found
 {
-// Raw string literals are a C++11
+// Raw string literals are a C++11 feature.
 #if TEST_STD_VER >= 11
     LLVMFuzzerTestOneInput(R"XX(Õ)_%()()((\8'_%()_%()_%()_%(()_%()_%()_%(.t;)()¥f()_%()(.)_%;)()!¥f(((()()XX");
 #endif
