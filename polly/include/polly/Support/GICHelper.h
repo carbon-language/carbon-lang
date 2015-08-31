4e8907f4952bc7fd749126428b44bb08d142cfc5
Johannes Doerfert <doerfert@cs.uni-saarland.de>
2015-08-31 13:54:36 +0000

    [NFC] Add isl_set output stream operator
    llvm-svn: 246442

--- polly/include/polly/Support/GICHelper.h
+++ polly/include/polly/Support/GICHelper.h
@@ -65,6 +65,12 @@ inline llvm::raw_ostream &operator<<(llvm::raw_ostream &OS,
 }
 
 inline llvm::raw_ostream &operator<<(llvm::raw_ostream &OS,
+                                     __isl_keep isl_set *Set) {
+  OS << polly::stringFromIslObj(Set);
+  return OS;
+}
+
+inline llvm::raw_ostream &operator<<(llvm::raw_ostream &OS,
                                      __isl_keep isl_pw_aff *Map) {
   OS << polly::stringFromIslObj(Map);
   return OS;
