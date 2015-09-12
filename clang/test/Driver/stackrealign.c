78ee1e5be400dd7804d8faded1d813ba9a5786c9
NAKAMURA Takumi <geek4civic@gmail.com>
2015-09-12 01:29:35 +0000

    clang/test/Driver/stackrealign.c REQUIRES clang-driver.
    GCC driver, for example cygwin, both "-mstackrealign" "-mno-stackrealign" are passed.
    
    llvm-svn: 247496

--- clang/test/Driver/stackrealign.c
+++ clang/test/Driver/stackrealign.c
@@ -1,6 +1,7 @@
 // RUN: %clang -### %s 2>&1 | FileCheck %s -check-prefix=NO-REALIGN
 // RUN: %clang -### -mno-stackrealign -mstackrealign %s 2>&1 | FileCheck %s -check-prefix=REALIGN
 // RUN: %clang -### -mstackrealign -mno-stackrealign %s 2>&1 | FileCheck %s -check-prefix=NO-REALIGN
+// REQUIRES: clang-driver
 
 // REALIGN: -mstackrealign
 // NO-REALIGN-NOT: -mstackrealign
