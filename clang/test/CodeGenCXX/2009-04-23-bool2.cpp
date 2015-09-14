500c1e1adf638c7788b1d138ca82d71231d5f712
NAKAMURA Takumi <geek4civic@gmail.com>
2015-09-14 23:50:08 +0000

    clang/test/CodeGenCXX/2009-04-23-bool2.cpp: Avoid -mms-bitfields to add explicit triple.
    FIXME: Better way to cancel -mms-bitfields?
    llvm-svn: 247640

--- clang/test/CodeGenCXX/2009-04-23-bool2.cpp
+++ clang/test/CodeGenCXX/2009-04-23-bool2.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -emit-llvm %s -o /dev/null
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -emit-llvm %s -o /dev/null
 // g++.old-deja/g++.jason/bool2.C from gcc testsuite.
 // Crashed before 67975 went in.
 struct F {
