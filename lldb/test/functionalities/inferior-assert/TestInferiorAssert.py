906d7be922cd148d3b0027ab1c06eb2e56fe7e49
Ed Maste <emaste@freebsd.org>
2015-09-14 14:40:22 +0000

    Remove expectedFailureFreeBSD from passing test_inferior_asserting_disassemble
    llvm.org/pr18533
    
    llvm-svn: 247564

--- lldb/test/functionalities/inferior-assert/TestInferiorAssert.py
+++ lldb/test/functionalities/inferior-assert/TestInferiorAssert.py
@@ -38,7 +38,6 @@ class AssertingInferiorTestCase(TestBase):
         self.inferior_asserting_registers()
 
     @expectedFailurei386("llvm.org/pr17384: lldb needs to be aware of linux-vdso.so to unwind stacks properly")
-    @expectedFailureFreeBSD('llvm.org/pr18533 - PC in __assert frame is outside of function')
     @expectedFailureWindows("llvm.org/pr21793: need to implement support for detecting assertion / abort on Windows")
     def test_inferior_asserting_disassemble(self):
         """Test that lldb reliably disassembles frames after asserting (command)."""
