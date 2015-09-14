32868feeee0301114fb19de4797fb1596c6c07f8
Ed Maste <emaste@freebsd.org>
2015-09-14 14:41:58 +0000

    Enable StopHookForMultipleThreadsTestCase::test_stop_hook_multiple_threads_with_dwarf on FreeBSD
    This test passes locally but was disabled due to pexpect issues on the
    FreeBSD buildbot. That buildbot has been retired as it was overloaded,
    and we will investigate again if this fails once a new buildbot is in
    place. Noted by John Wolfe.
    
    llvm.org/pr22784
    
    This re-applies r247013.
    
    llvm-svn: 247565

--- lldb/test/functionalities/stop-hook/multiple_threads/TestStopHookMultipleThreads.py
+++ lldb/test/functionalities/stop-hook/multiple_threads/TestStopHookMultipleThreads.py
@@ -23,7 +23,6 @@ class StopHookForMultipleThreadsTestCase(TestBase):
     @expectedFlakeyFreeBSD("llvm.org/pr15037")
     @expectedFlakeyLinux("llvm.org/pr15037") # stop hooks sometimes fail to fire on Linux
     @expectedFailureHostWindows("llvm.org/pr22274: need a pexpect replacement for windows")
-    @skipIfFreeBSD # Can cause an indefinite hang in dotest.py on FreeBSD
     def test_stop_hook_multiple_threads_with_dwarf(self):
         """Test that lldb stop-hook works for multiple threads."""
         self.buildDwarf(dictionary=self.d)
