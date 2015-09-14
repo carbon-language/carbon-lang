e1daedfbb4816188ddf189857a2aa41852e6924f
Ed Maste <emaste@freebsd.org>
2015-09-14 14:31:46 +0000

    Remove expectedFailureFreeBSD from passing test_with_dwarf_formatters_api
    llvm.org/pr24282
    
    llvm-svn: 247563

--- lldb/test/python_api/formatters/TestFormattersSBAPI.py
+++ lldb/test/python_api/formatters/TestFormattersSBAPI.py
@@ -19,7 +19,6 @@ class SBFormattersAPITestCase(TestBase):
         self.setTearDownCleanup()
         self.formatters()
 
-    @expectedFailureFreeBSD("llvm.org/pr24282 Empty2 fails")
     @python_api_test
     @dwarf_test
     def test_with_dwarf_formatters_api(self):
