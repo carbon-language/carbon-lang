cfb6f45168d1510783eb4b8ba1bc6e3d5440adb4
Nico Weber <nicolasweber@gmx.de>
2015-09-15 18:49:51 +0000

    Rebase tests after LLVM r247707.
    llvm-svn: 247712

--- clang/test/CodeGenObjC/exceptions.m
+++ clang/test/CodeGenObjC/exceptions.m
@@ -124,7 +124,7 @@ void f3() {
   }
 
   // CHECK:      call void @f3_helper(i32 4, i32* nonnull [[X]])
-  // CHECK-NEXT: call void @llvm.lifetime.end(i64 4, i8* [[XPTR]])
+  // CHECK-NEXT: call void @llvm.lifetime.end(i64 4, i8* nonnull [[XPTR]])
   // CHECK-NEXT: ret void
   f3_helper(4, &x);
 }
