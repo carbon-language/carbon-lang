8d8fc3f1ca0cf059d54821a05ec0b870a80713e4
Nico Weber <nicolasweber@gmx.de>
2015-09-15 23:51:08 +0000

    Remove accidental superfluous newline added in r247750.
    llvm-svn: 247752

--- clang/unittests/Format/FormatTestJava.cpp
+++ clang/unittests/Format/FormatTestJava.cpp
@@ -397,7 +397,6 @@ TEST_F(FormatTestJava, AssertKeyword) {
   verifyFormat("assert a && b;");
 }
 
-
 TEST_F(FormatTestJava, PackageDeclarations) {
   verifyFormat("package some.really.loooooooooooooooooooooong.package;",
                getStyleWithColumns(50));
