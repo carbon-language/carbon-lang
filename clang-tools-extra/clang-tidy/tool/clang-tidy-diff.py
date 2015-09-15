1395253bf3964f48ed50ea23aa5cb9ef9b0575ff
Alexander Kornienko <alexfh@google.com>
2015-09-15 13:13:48 +0000

    [clang-tidy] Update check name in the comment. NFC.
    llvm-svn: 247682

--- clang-tools-extra/clang-tidy/tool/clang-tidy-diff.py
+++ clang-tools-extra/clang-tidy/tool/clang-tidy-diff.py
@@ -20,7 +20,7 @@ Example usage for git/svn users:
 
   git diff -U0 HEAD^ | clang-tidy-diff.py -p1
   svn diff --diff-cmd=diff -x-U0 | \
-      clang-tidy-diff.py -fix -checks=-*,misc-use-override
+      clang-tidy-diff.py -fix -checks=-*,modernize-use-override
 
 """
 
