778ac14dfb97730518bf5b566cdf119864613742
David Majnemer <david.majnemer@gmail.com>
2015-09-15 23:34:59 +0000

    Try to appease the build bots
    llvm-svn: 247743

--- clang/test/CodeGenCXX/exceptions-cxx-new.cpp
+++ clang/test/CodeGenCXX/exceptions-cxx-new.cpp
@@ -16,41 +16,41 @@ void test_catch() {
 // CHECK:   invoke i32 @"\01?f@@YAHH@Z"(i32 1)
 // CHECK:         to label %[[NORMAL:.*]] unwind label %[[CATCH_INT:.*]]
 
-// CHECK: [[CATCH_INT]]:
+// CHECK: [[CATCH_INT]]
 // CHECK:   %[[CATCHPAD_INT:.*]] = catchpad [{{.*}} @llvm.eh.handlertype.H.0, i8* null]
 // CHECK:         to label %[[CATCH_INT_HANDLER:.*]] unwind label %[[CATCH_DOUBLE:.*]]
 
-// CHECK: [[CATCH_INT_HANDLER]]:
+// CHECK: [[CATCH_INT_HANDLER]]
 // CHECK:   invoke i32 @"\01?f@@YAHH@Z"(i32 2)
 // CHECK:           to label %[[CATCH_INT_DONE:.*]] unwind label %[[CATCHENDPAD:.*]]
 
-// CHECK: [[CATCH_INT_DONE]]:
+// CHECK: [[CATCH_INT_DONE]]
 // CHECK:   catchret %[[CATCHPAD_INT]] to label %[[LEAVE_INT_CATCH:.*]]
 
-// CHECK: [[LEAVE_INT_CATCH]]:
+// CHECK: [[LEAVE_INT_CATCH]]
 // CHECK:   br label %[[LEAVE_FUNC:.*]]
 
-// CHECK: [[LEAVE_FUNC]]:
+// CHECK: [[LEAVE_FUNC]]
 // CHECK:   ret void
 
-// CHECK: [[CATCH_DOUBLE]]:
+// CHECK: [[CATCH_DOUBLE]]
 // CHECK:   %[[CATCHPAD_DOUBLE:.*]] = catchpad [{{.*}} @llvm.eh.handlertype.N.0, i8* null]
 // CHECK:           to label %[[CATCH_DOUBLE_HANDLER:.*]] unwind label %[[CATCHENDPAD]]
 
-// CHECK: [[CATCH_DOUBLE_HANDLER]]:
+// CHECK: [[CATCH_DOUBLE_HANDLER]]
 // CHECK:   invoke i32 @"\01?f@@YAHH@Z"(i32 3)
 // CHECK:           to label %[[CATCH_DOUBLE_DONE:.*]] unwind label %[[CATCHENDPAD]]
 
-// CHECK: [[CATCH_DOUBLE_DONE]]:
+// CHECK: [[CATCH_DOUBLE_DONE]]
 // CHECK:   catchret %[[CATCHPAD_DOUBLE]] to label %[[LEAVE_DOUBLE_CATCH:.*]]
 
-// CHECK: [[LEAVE_DOUBLE_CATCH]]:
+// CHECK: [[LEAVE_DOUBLE_CATCH]]
 // CHECK:   br label %[[LEAVE_FUNC]]
 
-// CHECK: [[CATCHENDPAD]]:
+// CHECK: [[CATCHENDPAD]]
 // CHECK:   catchendpad unwind to caller
 
-// CHECK: [[NORMAL]]:
+// CHECK: [[NORMAL]]
 // CHECK:   br label %[[LEAVE_FUNC]]
 
 struct Cleanup {
@@ -66,11 +66,11 @@ void test_cleanup() {
 // CHECK:   invoke i32 @"\01?f@@YAHH@Z"(i32 1)
 // CHECK:           to label %[[LEAVE_FUNC:.*]] unwind label %[[CLEANUP:.*]]
 
-// CHECK: [[LEAVE_FUNC]]:
+// CHECK: [[LEAVE_FUNC]]
 // CHECK:   call x86_thiscallcc void @"\01??_DCleanup@@QAE@XZ"(
 // CHECK:   ret void
 
-// CHECK: [[CLEANUP]]:
+// CHECK: [[CLEANUP]]
 // CHECK:   %[[CLEANUPPAD:.*]] = cleanuppad []
 // CHECK:   call x86_thiscallcc void @"\01??_DCleanup@@QAE@XZ"(
 // CHECK:   cleanupret %[[CLEANUPPAD]] unwind to caller
@@ -81,9 +81,9 @@ void test_cleanup() {
 // CHECK:   invoke i32 @"\01?f@@YAHH@Z"(i32 -1)
 // CHECK:           to label %[[LEAVE_FUNC:.*]] unwind label %[[TERMINATE:.*]]
 
-// CHECK: [[LEAVE_FUNC]]:
+// CHECK: [[LEAVE_FUNC]]
 // CHECK:   ret void
 
-// CHECK: [[TERMINATE]]:
+// CHECK: [[TERMINATE]]
 // CHECK:   terminatepad [void ()* @"\01?terminate@@YAXXZ"] unwind to caller
 
