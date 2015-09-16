f3695c87cd17d4e108c668c6759523aa80abcbc4
Devin Coughlin <dcoughlin@apple.com>
2015-09-16 01:52:32 +0000

    [analyzer] SATestBuild.py: Move additional checkers logic so SATestAdd.py can use it as well.
    Move the logic looking for additional checkers in the SA_ADDITIONAL_CHECKERS
    environmental variable from SATestBuild's main() to runScanBuild(). This allows
    SATestAdd.py to use the variable as well. Without it, we won't include
    additional checkers when building reference results for the build bot.
    
    Differential Revision: http://reviews.llvm.org/D12891
    
    llvm-svn: 247767

--- clang/utils/analyzer/SATestBuild.py
+++ clang/utils/analyzer/SATestBuild.py
@@ -210,9 +210,14 @@ def runScanBuild(Dir, SBOutputDir, PBuildLogFile):
     if not os.path.exists(BuildScriptPath):
         print "Error: build script is not defined: %s" % BuildScriptPath
         sys.exit(-1)
+
+    AllCheckers = Checkers
+    if os.environ.has_key('SA_ADDITIONAL_CHECKERS'):
+        AllCheckers = AllCheckers + ',' + os.environ['SA_ADDITIONAL_CHECKERS']
+
     SBOptions = "--use-analyzer " + Clang + " "
     SBOptions += "-plist-html -o " + SBOutputDir + " "
-    SBOptions += "-enable-checker " + Checkers + " "
+    SBOptions += "-enable-checker " + AllCheckers + " "
     SBOptions += "--keep-empty "
     # Always use ccc-analyze to ensure that we can locate the failures
     # directory.
@@ -613,8 +618,5 @@ if __name__ == '__main__':
     elif Args.update_reference:
         IsReference = True
         UpdateSVN = True
-
-    if os.environ.has_key('SA_ADDITIONAL_CHECKERS'):
-        Checkers = Checkers + ',' + os.environ['SA_ADDITIONAL_CHECKERS']
 
     testAll(IsReference, UpdateSVN, Strictness)
