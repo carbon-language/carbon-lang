c7a6b40d106324f39945c07e3a29c4d7b4e9a7cb
Alexander Kornienko <alexfh@google.com>
2015-09-16 16:16:53 +0000

    [clang-tidy] Ignore spaces in -checks=
    llvm-svn: 247812

--- clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp
+++ clang-tools-extra/clang-tidy/ClangTidyDiagnosticConsumer.cpp
@@ -130,7 +130,7 @@ static bool ConsumeNegativeIndicator(StringRef &GlobList) {
 // Converts first glob from the comma-separated list of globs to Regex and
 // removes it and the trailing comma from the GlobList.
 static llvm::Regex ConsumeGlob(StringRef &GlobList) {
-  StringRef Glob = GlobList.substr(0, GlobList.find(','));
+  StringRef Glob = GlobList.substr(0, GlobList.find(',')).trim();
   GlobList = GlobList.substr(Glob.size() + 1);
   llvm::SmallString<128> RegexText("^");
   StringRef MetaChars("()^$|*+?.[]\\{}");
