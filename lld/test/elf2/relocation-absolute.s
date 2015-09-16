72373fb5b707884526c7d392c1faabf554e3e8b4
Rafael Espindola <rafael.espindola@gmail.com>
2015-09-16 12:50:32 +0000

    Fix test to actually test something.
    llvm-svn: 247790

--- lld/test/elf2/relocation-absolute.s
+++ lld/test/elf2/relocation-absolute.s
@@ -1,12 +1,12 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %S/Inputs/abs.s -o %tabs
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
 // RUN: lld -flavor gnu2 %tabs %t -o %tout
-// RUN: llvm-objdump -s %tout
+// RUN: llvm-objdump -d %tout | FileCheck %s
 // REQUIRES: x86
 
 .global _start
 _start:
   movl $abs, %edx
 
-#CHECK:      Contents of section .text:
-#CHECK-NEXT:  {{[0-1a-f]+}} ba420000 00
+//CHECK:      start:
+//CHECK-NEXT: movl	$66, %edx
