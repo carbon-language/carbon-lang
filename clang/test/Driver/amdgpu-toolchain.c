9b3ee4bf2cffee6e262b34596f7731ff70649eda
Rafael Espindola <rafael.espindola@gmail.com>
2015-09-18 20:22:36 +0000

    Fix test to pass when the directory name has lld in it.
    CHECK-LABEL assumes that there is only one occurrence of the match.
    
    The output looks like:
    
    clang version 3.8.0 (trunk 247999)
    ....
    /path/to/build/dir/bin/clang-3.8 ....
    
    If the path contains lld, the second CHECK-LABEL matches it and we fail since
    there is no -cc1as between clang and lld.
    
    llvm-svn: 248029

--- clang/test/Driver/amdgpu-toolchain.c
+++ clang/test/Driver/amdgpu-toolchain.c
@@ -1,6 +1,5 @@
 // RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
-// AS_LINK-LABEL: clang
-// AS_LINK: "-cc1as"
-// AS_LINK-LABEL: lld
-// AS_LINK: "-flavor" "gnu" "-target" "amdgcn--amdhsa"
-// REQUIRES: clang-driver
+// AS_LINK: /clang
+// AS_LINK-SAME: "-cc1as"
+// AS_LINK: /lld
+// AS_LINK-SAME: "-flavor" "gnu" "-target" "amdgcn--amdhsa"
