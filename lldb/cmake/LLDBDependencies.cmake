0a484819ca92113e4f22acb34085d2fdaa6df2e9
Stephane Sezer <sas@cd80.net>
2015-09-18 17:14:49 +0000

    Link NetBSD with execinfo (CMAKE build)
    Summary: FreeBSD and NetBSD share the same library execinfo.
    
    Reviewers: joerg, sas, brucem
    
    Subscribers: brucem, sas, lldb-commits
    
    Differential Revision: http://reviews.llvm.org/D12750
    
    Change by Kamil Rytarowski <n54@gmx.com>
    
    llvm-svn: 248007

--- lldb/cmake/LLDBDependencies.cmake
+++ lldb/cmake/LLDBDependencies.cmake
@@ -147,8 +147,8 @@ if (NOT CMAKE_SYSTEM_NAME MATCHES "Windows" AND NOT __ANDROID_NDK__)
     endif()
   endif()
 endif()
-# On FreeBSD backtrace() is provided by libexecinfo, not libc.
-if (CMAKE_SYSTEM_NAME MATCHES "FreeBSD")
+# On FreeBSD/NetBSD backtrace() is provided by libexecinfo, not libc.
+if (CMAKE_SYSTEM_NAME MATCHES "FreeBSD" OR CMAKE_SYSTEM_NAME MATCHES "NetBSD")
   list(APPEND LLDB_SYSTEM_LIBS execinfo)
 endif()
 
