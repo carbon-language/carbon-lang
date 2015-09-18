a528f34b430dcd0af73c740c536eb521d9e1bc41
NAKAMURA Takumi <geek4civic@gmail.com>
2015-09-18 15:56:16 +0000

    amdgpu-toolchain.c: Add a probe line temporally. Will revert later.
    llvm-svn: 247997

--- clang/test/Driver/amdgpu-toolchain.c
+++ clang/test/Driver/amdgpu-toolchain.c
@@ -1,3 +1,4 @@
+// RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s
 // RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
 // AS_LINK-LABEL: clang
 // AS_LINK: "-cc1as"
