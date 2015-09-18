4859773a9538b28622d9cd92d8bf2dc5ac69a833
Rafael Espindola <rafael.espindola@gmail.com>
2015-09-18 20:44:09 +0000

    Yet another try at fixing this at every bot.
    This time handle " and - after clang and lld.
    
    llvm-svn: 248033

--- clang/test/Driver/amdgpu-toolchain.c
+++ clang/test/Driver/amdgpu-toolchain.c
@@ -1,3 +1,3 @@
 // RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
-// AS_LINK: bin/clang {{.*}} "-cc1as"
-// AS_LINK: bin/lld {{.*}} "-flavor" "gnu" "-target" "amdgcn--amdhsa"
+// AS_LINK: bin/clang{{.*}} "-cc1as"
+// AS_LINK: bin/lld{{.*}} "-flavor" "gnu" "-target" "amdgcn--amdhsa"
