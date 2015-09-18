e7229f4921bef04dc441b02be4505aab761bc9f5
NAKAMURA Takumi <geek4civic@gmail.com>
2015-09-18 16:21:27 +0000

    Suppress amdgpu-toolchain.c for targeting ps4, for now. Investigating.
    llvm-svn: 248000

--- clang/test/Driver/amdgpu-toolchain.c
+++ clang/test/Driver/amdgpu-toolchain.c
@@ -1,6 +1,6 @@
-// RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s
 // RUN: %clang -### -target amdgcn--amdhsa -x assembler -mcpu=kaveri %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
 // AS_LINK-LABEL: clang
 // AS_LINK: "-cc1as"
 // AS_LINK-LABEL: lld
 // AS_LINK: "-flavor" "gnu" "-target" "amdgcn--amdhsa"
+// REQUIRES: clang-driver
