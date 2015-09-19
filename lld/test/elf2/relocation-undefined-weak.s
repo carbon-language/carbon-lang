acf5bdfd88ca2763b3fa6a2eb466ea87e5826051
Michael J. Spencer <bigcheesegs@gmail.com>
2015-09-19 00:15:38 +0000

    [elf2] Improve relocation-undefined-weak.s test.
    llvm-svn: 248071

--- lld/test/elf2/relocation-undefined-weak.s
+++ lld/test/elf2/relocation-undefined-weak.s
@@ -1,7 +1,10 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
 // RUN: lld -flavor gnu2 %t -o %tout
-// RUN: llvm-objdump -d %tout | FileCheck %s
+// RUN: llvm-readobj -sections %tout | FileCheck %s
+// RUN: llvm-objdump -d %tout | FileCheck %s --check-prefix DISASM
 // REQUIRES: x86
+
+// Check that undefined weak symbols are treated as having a VA of 0.
 
 .global _start
 _start:
@@ -9,4 +12,16 @@ _start:
 
 .weak sym1
 
-// CHECK: movl    $1, -69642(%rip)
+// CHECK:      Name: .text
+// CHECK-NEXT: Type: SHT_PROGBITS
+// CHECK-NEXT: Flags [
+// CHECK-NEXT:   SHF_ALLOC
+// CHECK-NEXT:   SHF_EXECINSTR
+// CHECK-NEXT: ]
+// CHECK-NEXT: Address: 0x11000
+
+// Unfortunately FileCheck can't do math, so we have to check for explicit
+// values:
+// R_86_64_PC32 = 0 + (-8 - (0x11000 + 2)) = -69642
+
+// DISASM: movl    $1, -69642(%rip)
