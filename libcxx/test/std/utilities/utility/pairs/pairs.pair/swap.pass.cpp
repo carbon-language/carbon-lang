835e77e29e441a1839b9f1e8162515aa1c6813ca
Marshall Clow <mclow.lists@gmail.com>
2015-09-22 17:57:41 +0000

    Check in the test for PR#24890 that I forgot in previous commit
    llvm-svn: 248305

--- libcxx/test/std/utilities/utility/pairs/pairs.pair/swap.pass.cpp
+++ libcxx/test/std/utilities/utility/pairs/pairs.pair/swap.pass.cpp
@@ -16,12 +16,30 @@
 #include <utility>
 #include <cassert>
 
+struct S {
+    int i; 
+    S() : i(0) {}
+    S(int j) : i(j) {}
+    S const * operator& () const { assert(false); return this; }
+    bool operator==(int x) const { return i == x; }
+    };
+
 int main()
 {
     {
         typedef std::pair<int, short> P1;
         P1 p1(3, 4);
         P1 p2(5, 6);
+        p1.swap(p2);
+        assert(p1.first == 5);
+        assert(p1.second == 6);
+        assert(p2.first == 3);
+        assert(p2.second == 4);
+    }
+    {
+        typedef std::pair<int, S> P1;
+        P1 p1(3, S(4));
+        P1 p2(5, S(6));
         p1.swap(p2);
         assert(p1.first == 5);
         assert(p1.second == 6);
