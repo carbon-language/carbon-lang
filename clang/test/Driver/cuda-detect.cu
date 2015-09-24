7d6266ead3f1dd1c753e3643471284abb121d7af
Artem Belevich <tra@google.com>
2015-09-24 00:15:47 +0000

    Disabled one of the test runs in cuda-detect.cu
    --sysroot apparently can't handle long paths which triggers test
    failure on some platforms.
    
    llvm-svn: 248448

--- clang/test/Driver/cuda-detect.cu
+++ clang/test/Driver/cuda-detect.cu
@@ -4,8 +4,6 @@
 // RUN: %clang -v --target=i386-unknown-linux \
 // RUN:   --sysroot=/tmp/no-cuda-there 2>&1 | FileCheck %s -check-prefix NOCUDA
 // RUN: %clang -v --target=i386-unknown-linux \
-// RUN:   --sysroot=%S/Inputs/CUDA 2>&1 | FileCheck %s
-// RUN: %clang -v --target=i386-unknown-linux \
 // RUN:   --cuda-path=%S/Inputs/CUDA/usr/local/cuda 2>&1 | FileCheck %s
 
 // CHECK: Found CUDA installation: {{.*}}/Inputs/CUDA/usr/local/cuda
