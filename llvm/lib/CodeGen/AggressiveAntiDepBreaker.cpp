d9f102b464d5074e7aac7eef49c49e606c892d59
Chad Rosier <mcrosier@codeaurora.org>
2015-09-25 20:20:22 +0000

    Simplify code.  NFC.
    llvm-svn: 248610

--- llvm/lib/CodeGen/AggressiveAntiDepBreaker.cpp
+++ llvm/lib/CodeGen/AggressiveAntiDepBreaker.cpp
@@ -224,12 +224,7 @@ bool AggressiveAntiDepBreaker::IsImplicitDefUse(MachineInstr *MI,
   if (Reg == 0)
     return false;
 
-  MachineOperand *Op = nullptr;
-  if (MO.isDef())
-    Op = MI->findRegisterUseOperand(Reg, true);
-  else
-    Op = MI->findRegisterDefOperand(Reg);
-
+  MachineOperand *Op = MI->findRegisterUseOperand(Reg, /*isKill=*/MO.isDef());
   return(Op && Op->isImplicit());
 }
 
