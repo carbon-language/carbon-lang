57737529feea5229c3d259ed0fe666f61ec42fac
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2015-09-30 19:55:07 +0000

    [DarwinDriver] Use -lto_library to specify the path for libLTO.dylib
    Usually, when using LTO with a clang installation newer than the
    system's one, there's a libLTO.dylib version mismatch and LTO fails. One
    solution to this is to make ld point to the right libLTO.dylib by
    changing DYLD_LIBRARY_PATH.
    
    However, ld64 supports specifying the complete path to the desired
    libLTO.dylib through the -lto_library option. This commit adds support
    for the clang driver to use this option whenever it's capable of finding
    a libLTO.dylib in clang's installed library directory. This way, we
    don't need to rely on DYLD_LIBRARY_PATH nor get caught by version
    mismatches.
    
    Differential Revision: http://reviews.llvm.org/D13117
    
    rdar://problem/7363476
    
    llvm-svn: 248932

--- clang/test/Driver/darwin-ld.c
+++ clang/test/Driver/darwin-ld.c
@@ -110,6 +110,21 @@
 // LINK_OBJECT_LTO_PATH: {{ld(.exe)?"}}
 // LINK_OBJECT_LTO_PATH: "-object_path_lto"
 
+// RUN: %clang -target x86_64-apple-darwin10 -### %s \
+// RUN:   -mlinker-version=133 -flto 2> %t.log
+// RUN: cat %t.log
+// RUN: FileCheck -check-prefix=LINK_LTOLIB_PATH %s < %t.log
+//
+// LINK_LTOLIB_PATH: {{ld(.exe)?"}}
+// LINK_LTOLIB_PATH: "-lto_library"
+
+// RUN: %clang -target x86_64-apple-darwin10 -### %s \
+// RUN:   -ccc-install-dir %S/dummytestdir -mlinker-version=133 -flto 2> %t.log
+// RUN: cat %t.log
+// RUN: FileCheck -check-prefix=LINK_LTOLIB_PATH_WRN %s < %t.log
+//
+// LINK_LTOLIB_PATH_WRN: warning: libLTO.dylib relative to clang installed dir not found; using 'ld' default search path instead
+
 // RUN: %clang -target x86_64-apple-darwin10 -### %t.o \
 // RUN:   -force_load a -force_load b 2> %t.log
 // RUN: cat %t.log
