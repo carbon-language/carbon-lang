c02fcafef6138aec87b27c30c07b3d9481b1d183
Davide Italiano <davide@freebsd.org>
2015-09-30 19:44:30 +0000

    [ELF2] Expand test comment a little bit, use reloc name as section name.
    llvm-svn: 248930

--- lld/test/elf2/aarch64-relocs.s
+++ lld/test/elf2/aarch64-relocs.s
@@ -3,12 +3,16 @@
 # RUN: llvm-objdump -d %t2 | FileCheck %s
 # REQUIRES: aarch64
 
+.section .R_AARCH64_ADR_PREL_LO21,"ax",@progbits
 .globl _start
 _start:
   adr x1,msg
 msg:  .asciz  "Hello, world\n"
 msgend:
 
-// R_AARCH64_ADR_PREL_LO21
+# CHECK: Disassembly of section .R_AARCH64_ADR_PREL_LO21:
 # CHECK: _start:
-# CHECK:   11000:       21 00 00 10     adr     x1, #4
+# CHECK:        0:       21 00 00 10     adr     x1, #4
+# CHECK: msg:
+# CHECK:        4:
+# #4 is the adr immediate value.
