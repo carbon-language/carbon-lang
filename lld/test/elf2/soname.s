b9287b1fba572370eeb83f81e1d5aef8a3c5ba3b
Rui Ueyama <ruiu@google.com>
2015-10-01 19:39:02 +0000

    ELF2: Use -soname to create a test .so file. Remove the .so binary from repository.
    llvm-svn: 249059

--- lld/test/elf2/soname.s
+++ lld/test/elf2/soname.s
@@ -1,5 +1,6 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
-// RUN: lld -flavor gnu2 %t.o %p/Inputs/soname.so -o %t
+// RUN: lld -flavor gnu2 %t.o -shared -soname=bar -o %t.so
+// RUN: lld -flavor gnu2 %t.o %t.so -o %t
 // RUN: llvm-readobj --dynamic-table %t | FileCheck %s
 
 // CHECK:  0x0000000000000001 NEEDED               SharedLibrary (bar)
