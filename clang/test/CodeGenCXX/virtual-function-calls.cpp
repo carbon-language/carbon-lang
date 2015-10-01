41106188a4fea286c094fc201e8066b416455463
Renato Golin <renato.golin@linaro.org>
2015-10-01 12:58:41 +0000

    Revert "Decorating virtual functions load with invariant.load" and fix
    This reverts commit r248982 as it was breaking the ARM buildbots and the fix didn't work.
    This reverts commit r248984, the fix that didn't work.
    
    llvm-svn: 249005

--- clang/test/CodeGenCXX/virtual-function-calls.cpp
+++ clang/test/CodeGenCXX/virtual-function-calls.cpp
@@ -1,5 +1,4 @@
 // RUN: %clang_cc1 %s -triple %itanium_abi_triple -std=c++11 -emit-llvm -o - | FileCheck %s
-// RUN: %clang_cc1 %s -triple %itanium_abi_triple -std=c++11 -emit-llvm -o - -fstrict-vtable-pointers -O1 | FileCheck --check-prefix=CHECK-INVARIANT %s
 
 // PR5021
 namespace PR5021 {
@@ -43,14 +42,10 @@ namespace VirtualNoreturn {
     [[noreturn]] virtual void f();
   };
 
-  // CHECK-LABEL: @_ZN15VirtualNoreturn1f
-  // CHECK-INVARIANT-LABEL: define void @_ZN15VirtualNoreturn1f
+  // CHECK: @_ZN15VirtualNoreturn1f
   void f(A *p) {
     p->f();
     // CHECK: call {{.*}}void %{{[^#]*$}}
     // CHECK-NOT: unreachable
-    // CHECK-INVARIANT: load {{.*}} !invariant.load ![[EMPTY_NODE:[0-9]]]
   }
 }
-
-// CHECK-INVARIANT: ![[EMPTY_NODE]] = !{}
