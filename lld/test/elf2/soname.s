ff1dcc59d99a9341cff7295c407dad93101a9b3c
Rafael Espindola <rafael.espindola@gmail.com>
2015-10-01 20:04:31 +0000

    Make the test a bit stronger.
    This shows that it is really DT_SONAME that is used to detect dups.
    
    llvm-svn: 249062

--- lld/test/elf2/soname.s
+++ lld/test/elf2/soname.s
@@ -1,6 +1,7 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
 // RUN: lld -flavor gnu2 %t.o -shared -soname=bar -o %t.so
-// RUN: lld -flavor gnu2 %t.o %t.so %t.so -o %t
+// RUN: lld -flavor gnu2 %t.o -shared -soname=bar -o %t2.so
+// RUN: lld -flavor gnu2 %t.o %t.so %t2.so -o %t
 // RUN: llvm-readobj --dynamic-table %t | FileCheck %s
 
 // CHECK:  0x0000000000000001 NEEDED               SharedLibrary (bar)
