25b9ced0ed6a342863b084b558f1a238f2f93283
Zachary Turner <zturner@google.com>
2015-10-02 22:47:37 +0000

    Re-disable TestBuiltinTrap on Windows.
    This was inadvertently re-enabled as a in r248883.
    
    llvm-svn: 249209

--- lldb/test/linux/builtin_trap/TestBuiltinTrap.py
+++ lldb/test/linux/builtin_trap/TestBuiltinTrap.py
@@ -21,6 +21,7 @@ class BuiltinTrapTestCase(TestBase):
 
     @expectedFailureAll("llvm.org/pr15936", compiler="gcc", compiler_version=["<=","4.6"])
     @expectedFailureAll(archs="arm", compiler="gcc", triple=".*-android") # gcc generates incorrect linetable
+    @skipIfWindows
     def test_with_run_command(self):
         """Test that LLDB handles a function with __builtin_trap correctly."""
         self.build()
