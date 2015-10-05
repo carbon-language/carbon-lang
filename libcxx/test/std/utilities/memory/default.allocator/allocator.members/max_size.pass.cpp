e652e4dae1cc7e277babb3185795809a9d9fcc8c
Marshall Clow <mclow.lists@gmail.com>
2015-10-05 20:50:25 +0000

    Fixed a possible overflow in a test of allocator::max_size().
    llvm-svn: 249349

--- libcxx/test/std/utilities/memory/default.allocator/allocator.members/max_size.pass.cpp
+++ libcxx/test/std/utilities/memory/default.allocator/allocator.members/max_size.pass.cpp
@@ -22,6 +22,6 @@ int new_called = 0;
 int main()
 {
     const std::allocator<int> a;
-    std::size_t M = a.max_size() * sizeof(int);
-    assert(M > 0xFFFF && M <= std::numeric_limits<std::size_t>::max());
+    std::size_t M = a.max_size();
+    assert(M > 0xFFFF && M <= (std::numeric_limits<std::size_t>::max() / sizeof(int)));
 }
