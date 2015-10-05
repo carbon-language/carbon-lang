ee4e08ba94e2ee04ad7ad0e0e396147be417c79f
Rafael Espindola <rafael.espindola@gmail.com>
2015-10-05 11:49:35 +0000

    Use llvm::errc instead of std::errc.
    llvm-svn: 249302

--- clang/lib/Frontend/CompilerInstance.cpp
+++ clang/lib/Frontend/CompilerInstance.cpp
@@ -641,7 +641,7 @@ std::unique_ptr<llvm::raw_pwrite_stream> CompilerInstance::createOutputFile(
       if (llvm::sys::fs::exists(Status)) {
         // Fail early if we can't write to the final destination.
         if (!llvm::sys::fs::can_write(OutputPath)) {
-          Error = std::make_error_code(std::errc::operation_not_permitted);
+          Error = make_error_code(llvm::errc::operation_not_permitted);
           return nullptr;
         }
 
