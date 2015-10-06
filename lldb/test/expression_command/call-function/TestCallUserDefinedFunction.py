0f247a479ead42e4637c9002b6ede030c8e1a175
Todd Fiala <todd.fiala@gmail.com>
2015-10-06 15:23:52 +0000

    Switching TestCallUserDefinedFunction from xfail to flaky on OS X.
    This is passing 50/50 times for me.  Moving to flaky as precautionary
    measure.
    
    llvm-svn: 249419

--- lldb/test/expression_command/call-function/TestCallUserDefinedFunction.py
+++ lldb/test/expression_command/call-function/TestCallUserDefinedFunction.py
@@ -22,7 +22,7 @@ class ExprCommandCallUserDefinedFunction(TestBase):
         # Find the line number to break for main.c.
         self.line = line_number('main.cpp',
                                 '// Please test these expressions while stopped at this line:')
-    @expectedFailureDarwin("llvm.org/pr20274", debug_info=["dsym"]) # intermittent failure on MacOSX
+    @expectedFlakeyDarwin("llvm.org/pr20274") # intermittent failure on MacOSX, dsym only
     @expectedFailureWindows("llvm.org/pr24489: Name lookup not working correctly on Windows")
     def test(self):
         """Test return values of user defined function calls."""
