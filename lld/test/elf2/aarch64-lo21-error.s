8d9ace60ca714c00872f1391aee690f88ede57c0
Davide Italiano <davide@freebsd.org>
2015-10-07 23:15:22 +0000

    [ELF/AArch64] Add tests to verify we handle overflow correctly for relocs.
    llvm-svn: 249634

---
+++ lld/test/elf2/aarch64-lo21-error.s
@@ -0,0 +1,8 @@
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-freebsd %S/Inputs/abs.s -o %tabs
+// RUN: llvm-mc -filetype=obj -triple=aarch64-pc-freebsd %s -o %t
+// RUN: not ld.lld2 %tabs -shared %t -o %t2 2>&1 | FileCheck %s
+// REQUIRES: aarch64
+
+adr x0, big
+
+#CHECK: R_AARCH64_ADR_PREL_LO21 out of range
