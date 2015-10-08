32bd3c8cff982c21dcf30a0316c3a0827f3df0eb
Benjamin Kramer <benny.kra@googlemail.com>
2015-10-08 10:31:17 +0000

    [Driver] Use Twine instead of itostr. NFC.
    No need to construct temporary std::strings here.
    
    llvm-svn: 249676

--- clang/lib/Driver/Tools.cpp
+++ clang/lib/Driver/Tools.cpp
@@ -2353,7 +2353,7 @@ static void RenderDebugEnablingArgs(const ArgList &Args, ArgStringList &CmdArgs,
   }
   if (DwarfVersion > 0)
     CmdArgs.push_back(
-        Args.MakeArgString("-dwarf-version=" + llvm::itostr(DwarfVersion)));
+        Args.MakeArgString("-dwarf-version=" + Twine(DwarfVersion)));
 }
 
 static void CollectArgsForIntegratedAssembler(Compilation &C,
