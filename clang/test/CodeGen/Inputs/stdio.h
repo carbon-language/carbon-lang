9e593499b91261d85f35eb6187369e11529aa12d
Saleem Abdulrasool <compnerd@compnerd.org>
2015-10-12 21:19:30 +0000

    test: change argument
    This failed on AArch64 due to the type mismatch using int instead of
    __builtin_va_list.
    
    llvm-svn: 250112

--- clang/test/CodeGen/Inputs/stdio.h
+++ clang/test/CodeGen/Inputs/stdio.h
@@ -5,5 +5,5 @@ extern int vprintf(const char *format, __builtin_va_list arg);
 extern __inline __attribute__((gnu_inline,always_inline)) int
 vprintf(const char *x, __builtin_va_list y)
 {
-  return vfprintf (0, 0, 0);
+  return vfprintf (0, 0, y);
 }
