035ddc20461abfd3a296022b53096ed8ef34d89a
Eric Fiselier <eric@efcs.ca>
2015-10-14 19:01:47 +0000

    Configure for config site header
    llvm-svn: 250313

--- libcxxabi/test/libcxxabi/test/config.py
+++ libcxxabi/test/libcxxabi/test/config.py
@@ -31,6 +31,7 @@ class Configuration(LibcxxConfiguration):
         super(Configuration, self).configure_compile_flags()
 
     def configure_compile_flags_header_includes(self):
+        self.configure_config_site_header()
         cxx_headers = self.get_lit_conf(
             'cxx_headers',
             os.path.join(self.libcxx_src_root, '/include'))
