7dccca543a5f29cee53e2fea8ed8abb56be90c3c
Michael J. Spencer <bigcheesegs@gmail.com>
2015-10-16 22:05:52 +0000

    [elf2] Simplify and rename x86-64 relocation error tests.
    llvm-svn: 250556

--- lld/test/elf2/x86-64-reloc-32-error.s
+++ lld/test/elf2/x86-64-reloc-32-error.s
@@ -1,10 +1,8 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %S/Inputs/abs.s -o %tabs
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
-// RUN: not ld.lld2 %tabs %t -o %t2 2>&1 | FileCheck %s
+// RUN: not ld.lld2 -shared %tabs %t -o %t2 2>&1 | FileCheck %s
 // REQUIRES: x86
 
-.global _start
-_start:
   movl $big, %edx
 
 #CHECK: R_X86_64_32 out of range
--- lld/test/elf2/x86-64-reloc-32S-error.s
+++ lld/test/elf2/x86-64-reloc-32S-error.s
@@ -1,9 +1,7 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
-// RUN: not ld.lld2 %t -o %t2 2>&1 | FileCheck %s
+// RUN: not ld.lld2 -shared %t -o %t2 2>&1 | FileCheck %s
 // REQUIRES: x86
 
-.global _start
-_start:
   movq _start - 0x1000000000000, %rdx
 
 #CHECK: R_X86_64_32S out of range
