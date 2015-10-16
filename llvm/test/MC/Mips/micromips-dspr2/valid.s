4c4f21b971b2a9c154c3d932235202c40e43053e
Zlatko Buljan <Zlatko.Buljan@imgtec.com>
2015-10-16 13:03:10 +0000

    Commited two test files which are forgotten during commit of patch for http://reviews.llvm.org/D13376
    llvm-svn: 250512

---
+++ llvm/test/MC/Mips/micromips-dspr2/valid.s
@@ -0,0 +1,7 @@
+# RUN: llvm-mc %s -triple=mips-unknown-linux -show-encoding -mcpu=mips32r6 -mattr=micromips -mattr=+dspr2 | FileCheck %s
+
+  .set noat
+  dpa.w.ph $ac0, $3, $2        # CHECK: dpa.w.ph $ac0, $3, $2      # encoding: [0x00,0x43,0x00,0xbc]
+  dpaqx_s.w.ph $ac3, $12, $7   # CHECK: dpaqx_s.w.ph $ac3, $12, $7 # encoding: [0x00,0xec,0xe2,0xbc]
+  dpaqx_sa.w.ph $ac0, $5, $6   # CHECK: dpaqx_sa.w.ph $ac0, $5, $6 # encoding: [0x00,0xc5,0x32,0xbc]
+  dpax.w.ph $ac3, $2, $1       # CHECK: dpax.w.ph $ac3, $2, $1     # encoding: [0x00,0x22,0xd0,0xbc]
