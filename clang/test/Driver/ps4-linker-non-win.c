c4d8ed0e532349c5c597ed9679ff281ca3799445
NAKAMURA Takumi <geek4civic@gmail.com>
2015-10-17 23:15:16 +0000

    clang/test/Driver/ps4-linker-non-win.c: Make sure that %T/ps4-ld would be used but *fails*.
    llvm-svn: 250630

--- clang/test/Driver/ps4-linker-non-win.c
+++ clang/test/Driver/ps4-linker-non-win.c
@@ -1,7 +1,9 @@
 // UNSUPPORTED: system-windows
 // REQUIRES: x86-registered-target
 
+// RUN: rm -f %T/ps4-ld
 // RUN: touch %T/ps4-ld
+// XFAIL: *
 
 // RUN: env "PATH=%T" %clang -### -target x86_64-scei-ps4  %s -fuse-ld=gold 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-PS4-LINKER %s
@@ -15,4 +17,5 @@
 // RUN: env "PATH=%T" %clang -### -target x86_64-scei-ps4  %s -shared \
 // RUN:     -fuse-ld=ps4 2>&1 | FileCheck --check-prefix=CHECK-PS4-LINKER %s
 
-// CHECK-PS4-LINKER: ps4-ld
+// FIXME: This depends that %T ends with "Output".
+// CHECK-PS4-LINKER: Output/ps4-ld
