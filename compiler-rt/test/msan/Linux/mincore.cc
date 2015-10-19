0b98b27677b0325e5133344407a13410b3cf1423
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-10-19 23:39:11 +0000

    [msan] Intercept mincore.
    llvm-svn: 250761

---
+++ compiler-rt/test/msan/Linux/mincore.cc
@@ -0,0 +1,36 @@
+// RUN: %clangxx_msan -std=c++11 -O0 %s -o %t && %run %t
+
+#include <assert.h>
+#include <unistd.h>
+#include <sys/mman.h>
+#include <sanitizer/msan_interface.h>
+
+int main(void) {
+  unsigned char vec[20];
+  int res;
+  size_t PS = sysconf(_SC_PAGESIZE);
+  void *addr = mmap(nullptr, 20 * PS, PROT_READ | PROT_WRITE,
+                    MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
+
+  __msan_poison(&vec, sizeof(vec));
+  res = mincore(addr, 10 * PS, vec);
+  assert(res == 0);
+  assert(__msan_test_shadow(vec, sizeof(vec)) == 10);
+
+  __msan_poison(&vec, sizeof(vec));
+  res = mincore(addr, 10 * PS + 42, vec);
+  assert(res == 0);
+  assert(__msan_test_shadow(vec, sizeof(vec)) == 11);
+
+  __msan_poison(&vec, sizeof(vec));
+  res = mincore(addr, 10 * PS - 1, vec);
+  assert(res == 0);
+  assert(__msan_test_shadow(vec, sizeof(vec)) == 10);
+
+  __msan_poison(&vec, sizeof(vec));
+  res = mincore(addr, 1, vec);
+  assert(res == 0);
+  assert(__msan_test_shadow(vec, sizeof(vec)) == 1);
+
+  return 0;
+}
