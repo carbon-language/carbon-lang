74ca428df7fc1e3ec19029afa4b189d19e9d43e1
Manuel Klimek <klimek@google.com>
2015-10-19 08:27:51 +0000

    Make test not rely on %T ending on /Output.
    llvm-svn: 250690

--- clang/test/Driver/ps4-linker-non-win.c
+++ clang/test/Driver/ps4-linker-non-win.c
@@ -1,21 +1,21 @@
 // UNSUPPORTED: system-windows
 // REQUIRES: x86-registered-target
 
-// RUN: rm -f %T/ps4-ld
-// RUN: touch %T/ps4-ld
-// RUN: chmod +x %T/ps4-ld
+// RUN: mkdir -p %T/Output
+// RUN: rm -f %T/Output/ps4-ld
+// RUN: touch %T/Output/ps4-ld
+// RUN: chmod +x %T/Output/ps4-ld
 
-// RUN: env "PATH=%T:%PATH%" %clang -### -target x86_64-scei-ps4  %s -fuse-ld=gold 2>&1 \
+// RUN: env "PATH=%T/Output:%PATH%" %clang -### -target x86_64-scei-ps4  %s -fuse-ld=gold 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-PS4-LINKER %s
-// RUN: env "PATH=%T:%PATH%" %clang -### -target x86_64-scei-ps4  %s -shared 2>&1 \
+// RUN: env "PATH=%T/Output:%PATH%" %clang -### -target x86_64-scei-ps4  %s -shared 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-PS4-LINKER %s
 
-// RUN: env "PATH=%T:%PATH%" %clang -### -target x86_64-scei-ps4  %s 2>&1 \
+// RUN: env "PATH=%T/Output:%PATH%" %clang -### -target x86_64-scei-ps4  %s 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-PS4-LINKER %s
-// RUN: env "PATH=%T:%PATH%" %clang -### -target x86_64-scei-ps4  %s -fuse-ld=ps4 2>&1 \
+// RUN: env "PATH=%T/Output:%PATH%" %clang -### -target x86_64-scei-ps4  %s -fuse-ld=ps4 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-PS4-LINKER %s
-// RUN: env "PATH=%T:%PATH%" %clang -### -target x86_64-scei-ps4  %s -shared \
+// RUN: env "PATH=%T/Output:%PATH%" %clang -### -target x86_64-scei-ps4  %s -shared \
 // RUN:     -fuse-ld=ps4 2>&1 | FileCheck --check-prefix=CHECK-PS4-LINKER %s
 
-// FIXME: This depends that %T ends with "Output".
 // CHECK-PS4-LINKER: Output/ps4-ld
