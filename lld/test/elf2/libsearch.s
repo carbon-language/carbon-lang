505f792cbb67ec38083c8d7c7c1d78775a3c5ce8
George Rimar <grimar@accesssoftek.com>
2015-10-20 11:07:03 +0000

    Fixed buildbot. Sorry.
    llvm-svn: 250815

--- lld/test/elf2/libsearch.s
+++ lld/test/elf2/libsearch.s
@@ -13,7 +13,7 @@
 // Should fail if no library specified
 // RUN: not ld.lld2 -l 2>&1 \
 // RUN:   | FileCheck --check-prefix=NOLIBRARY %s
-// NOLIBRARY: No library specified for -l
+// NOLIBRARY: missing arg value for "-l", expected 1 argument.
 
 // Should not link because of undefined symbol _bar
 // RUN: not ld.lld2 -o %t3 %t.o 2>&1 \
