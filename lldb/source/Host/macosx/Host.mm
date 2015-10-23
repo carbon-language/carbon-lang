a96a8c4c08a3c48db1790d262bae1948e79baa35
Jason Molenda <jmolenda@apple.com>
2015-10-23 00:59:31 +0000

    Fix one more place where we were using the old name of the xpc service. <rdar://problem/23223624>
    llvm-svn: 251086

--- lldb/source/Host/macosx/Host.mm
+++ lldb/source/Host/macosx/Host.mm
@@ -1117,11 +1117,7 @@ LaunchProcessXPC(const char *exe_path, ProcessLaunchInfo &launch_info, lldb::pid
     const char *xpc_service  = nil;
     bool send_auth = false;
     AuthorizationExternalForm extForm;
-    if ((requested_uid == UINT32_MAX) || (requested_uid == HostInfo::GetEffectiveUserID()))
-    {
-        xpc_service = "com.apple.lldb.launcherXPCService";
-    }
-    else if (requested_uid == 0)
+    if (requested_uid == 0)
     {
         if (AuthorizationMakeExternalForm(authorizationRef, &extForm) == errAuthorizationSuccess)
         {
@@ -1137,12 +1133,12 @@ LaunchProcessXPC(const char *exe_path, ProcessLaunchInfo &launch_info, lldb::pid
             }
             return error;
         }
-        xpc_service = "com.apple.lldb.launcherRootXPCService";
+        xpc_service = LaunchUsingXPCRightName;
     }
     else
     {
         error.SetError(4, eErrorTypeGeneric);
-        error.SetErrorStringWithFormat("Launching via XPC is only currently available for either the login user or root.");
+        error.SetErrorStringWithFormat("Launching via XPC is only currently available for root.");
         if (log)
         {
             error.PutToLog(log, "%s", error.AsCString());
