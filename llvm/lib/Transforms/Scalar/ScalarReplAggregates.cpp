26c3872666358e25b9b0bc914d86a2bcb2ad2389
NAKAMURA Takumi <geek4civic@gmail.com>
2015-10-24 06:42:42 +0000

    ScalarReplAggregates.cpp: Try to appease clash of anonymous::SROA in modules build.
    llvm-svn: 251181

--- llvm/lib/Transforms/Scalar/ScalarReplAggregates.cpp
+++ llvm/lib/Transforms/Scalar/ScalarReplAggregates.cpp
@@ -60,6 +60,7 @@ STATISTIC(NumAdjusted,  "Number of scalar allocas adjusted to allow promotion");
 STATISTIC(NumConverted, "Number of aggregates converted to scalar");
 
 namespace {
+#define SROA SROA_
   struct SROA : public FunctionPass {
     SROA(int T, bool hasDT, char &ID, int ST, int AT, int SLT)
       : FunctionPass(ID), HasDomTree(hasDT) {
