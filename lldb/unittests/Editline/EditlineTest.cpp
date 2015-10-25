528a30d62c092e41aff5d02793a9af115abf3f14
Todd Fiala <todd.fiala@gmail.com>
2015-10-25 21:42:35 +0000

    Xcode: added lldb-gtest target to build and run the gtests in the unittests directory.
    Also added a placeholder Editline gtest for some code that I'll add as soon
    as I make sure this addition doesn't break any of the build bots.
    
    This change also introduces some Xcode user-defined variables that I've used
    to attempt to isolate the way Python is integrated into the build.  I don't have
    the rest of LLDB using it yet, I'm using the gtests as my guinea pig on that.
    Currently these are:
    PYTHON_FRAMEWORK_PATH
    PYTHON_VERSION_MAJOR
    PYTHON_VERSION_MINOR
    
    I will convert the rest over to it after this gets a little time to bake
    and any kinks are worked out of it.
    
    llvm-svn: 251261

---
+++ lldb/unittests/Editline/EditlineTest.cpp
@@ -0,0 +1,15 @@
+//===-- EditlineTest.cpp -----------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include "gtest/gtest.h"
+
+TEST(EditlineTest, BasicTest)
+{
+    EXPECT_TRUE(true);
+}
