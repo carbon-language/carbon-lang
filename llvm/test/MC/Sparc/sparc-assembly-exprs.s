7bb30968e278d404e73f02fdf88b771628fd3141
David Majnemer <david.majnemer@gmail.com>
2015-10-26 03:34:29 +0000

    Update test to take into account for r251271.
    llvm-svn: 251272

--- llvm/test/MC/Sparc/sparc-assembly-exprs.s
+++ llvm/test/MC/Sparc/sparc-assembly-exprs.s
@@ -3,7 +3,7 @@
         ! CHECK: mov 1033, %o1  ! encoding: [0x92,0x10,0x24,0x09]
         mov      (0x400|9), %o1
         ! CHECK: mov 60, %o2    ! encoding: [0x94,0x10,0x20,0x3c]
-        mov      (12+3<<2), %o2
+        mov      ((12+3)<<2), %o2
 
 ! "." is exactly like a temporary symbol equated to the current line.
 ! RUN: llvm-mc %s -arch=sparc | FileCheck %s --check-prefix=DOTEXPR
