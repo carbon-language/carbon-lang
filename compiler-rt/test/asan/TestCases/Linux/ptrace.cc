6c583b8b25e2bd9aad5e0420c297190608d3dc41
Daniel Sanders <daniel.sanders@imgtec.com>
2015-10-27 23:13:26 +0000

    [mips][asan] XFAIL ptrace.cc
    It was recently enabled for non-x86 targets and doesn't seem to work for MIPS.
    The reason is currently unclear so XFAILing while I investigate.
    
    llvm-svn: 251466

--- compiler-rt/test/asan/TestCases/Linux/ptrace.cc
+++ compiler-rt/test/asan/TestCases/Linux/ptrace.cc
@@ -1,5 +1,6 @@
 // FIXME: https://code.google.com/p/address-sanitizer/issues/detail?id=316
 // XFAIL: android
+// XFAIL: mips
 //
 // RUN: %clangxx_asan -O0 %s -o %t && %run %t
 // RUN: %clangxx_asan -DPOSITIVE -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
