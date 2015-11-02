06fb04f1fa22b2d2398e3c426a204484fd524919
Tim Northover <tnorthover@apple.com>
2015-11-02 18:33:35 +0000

    MachO: improve load command tests slightly
    llvm-svn: 251815

--- llvm/test/MC/MachO/darwin-version-min-load-command.s
+++ llvm/test/MC/MachO/darwin-version-min-load-command.s
@@ -16,9 +16,14 @@
 // CHECK-DARWIN-NOT: LC_VERSION_MIN
 
 
-// FIXME: llvm-objdump doesn't know about WATCHOS LC yet
+// FIXME: llvm-objdump doesn't know about WATCHOS and TVOS VERSION_MIN LC yet
 // FIXME: llvm-mc -triple x86_64-apple-watchos1.0.0 %s -filetype=obj -o - | llvm-objdump -macho -private-headers - | FileCheck %s --check-prefix=CHECK-WATCHOS
+// FIXME: llvm-mc -triple x86_64-apple-tvos8.0.0 %s -filetype=obj -o - | llvm-objdump -macho -private-headers - | FileCheck %s --check-prefix=CHECK-TVOS
 // CHECK-WATCHOS: Load command
-// CHECK-WATCHOS:       cmd LC_VERSION_MIN_WATCHOS
-// CHECK-WATCHOS:   cmdsize 16
-// CHECK-WATCHOS:   version 1.0
+// CHECK-WATCHOS:           cmd LC_VERSION_MIN_WATCHOS
+// CHECK-WATCHOS-NEXT:   cmdsize 16
+// CHECK-WATCHOS-NEXT:   version 1.0
+
+// CHECK-TVOS:            cmd LC_VERSION_MIN_TVOS
+// CHECK-TVOS-NEXT:   cmdsize 16
+// CHECK-TVOS-NEXT:   version 8.0
