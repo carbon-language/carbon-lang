25aa9f091e9437dfe587ba8c68df0f35801dbd4b
Akira Hatanaka <ahatanaka@apple.com>
2015-11-11 02:13:36 +0000

    Reorder the check strings in test case following r252692.
    rdar://problem/19836465
    
    llvm-svn: 252693

--- clang/test/CodeGen/ms-declspecs.c
+++ clang/test/CodeGen/ms-declspecs.c
@@ -41,4 +41,4 @@ void noalias_caller(int *x) { noalias_callee(x); }
 // CHECK: attributes [[NUW]] = { nounwind{{.*}} }
 // CHECK: attributes [[NI]] = { noinline nounwind{{.*}} }
 // CHECK: attributes [[NR]] = { noreturn }
-// CHECK: attributes [[NA]] = { nounwind argmemonly{{.*}} }
+// CHECK: attributes [[NA]] = { argmemonly nounwind{{.*}} }
