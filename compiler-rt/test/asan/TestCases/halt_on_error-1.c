b8e3bff98ada11413489efa77e92cb9c4d36c161
Reid Kleckner <rnk@google.com>
2015-11-12 21:58:28 +0000

    [Windows] Fix halt_on_error-1.c test on Windows with %env_asan_opts
    llvm-svn: 252958

--- compiler-rt/test/asan/TestCases/halt_on_error-1.c
+++ compiler-rt/test/asan/TestCases/halt_on_error-1.c
@@ -3,8 +3,8 @@
 // RUN: %clang_asan -fsanitize-recover=address %s -o %t
 //
 // RUN: env not %run %t 2>&1 | FileCheck %s
-// RUN: env ASAN_OPTIONS=$ASAN_OPTIONS:halt_on_error=true not %run %t 2>&1 | FileCheck %s
-// RUN: env ASAN_OPTIONS=$ASAN_OPTIONS:halt_on_error=false %run %t 2>&1 | FileCheck %s --check-prefix CHECK-RECOVER
+// RUN: %env_asan_opts=halt_on_error=true not %run %t 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=halt_on_error=false %run %t 2>&1 | FileCheck %s --check-prefix CHECK-RECOVER
 
 #include <string.h>
 
