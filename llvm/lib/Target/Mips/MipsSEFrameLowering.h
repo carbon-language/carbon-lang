48e0256ed65be9188232ff5fe54de51ba1eefe1a
Vasileios Kalintiris <Vasileios.Kalintiris@imgtec.com>
2015-11-12 14:11:43 +0000

    Re-apply "[mips] Use correct frame register for DWARF info when dynamically realigning the stack.""
    r252219 reversed the direction of subprogram -> function edge. Fixed the
    IR to account for this.
    
    llvm-svn: 252895

--- llvm/lib/Target/Mips/MipsSEFrameLowering.h
+++ llvm/lib/Target/Mips/MipsSEFrameLowering.h
@@ -27,6 +27,9 @@ public:
   void emitPrologue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
   void emitEpilogue(MachineFunction &MF, MachineBasicBlock &MBB) const override;
 
+  int getFrameIndexReference(const MachineFunction &MF, int FI,
+                             unsigned &FrameReg) const override;
+
   bool spillCalleeSavedRegisters(MachineBasicBlock &MBB,
                                  MachineBasicBlock::iterator MI,
                                  const std::vector<CalleeSavedInfo> &CSI,
