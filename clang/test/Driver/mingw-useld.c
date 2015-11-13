273f9560c971744475e3ab22c38dc20fbe2460fb
Yaron Keren <yaron.keren@gmail.com>
2015-11-13 20:02:08 +0000

    Relax mingw-useld test to fix bot failures.
    llvm-svn: 253069

--- clang/test/Driver/mingw-useld.c
+++ clang/test/Driver/mingw-useld.c
@@ -4,7 +4,7 @@
 // CHECK_LD_32_NOT: "-flavor" "gnu"
 
 // RUN: %clang -### -target i686-pc-windows-gnu --sysroot=%S/Inputs/mingw_clang_tree/mingw32 %s -fuse-ld=lld 2>&1 | FileCheck -check-prefix=CHECK_LLD_32 %s
-// CHECK_LLD_32: "lld" "-flavor" "gnu"
+// CHECK_LLD_32: lld" "-flavor" "gnu"
 // CHECK_LLD_32: "i386pe"
 
 // RUN: %clang -### -target i686-pc-windows-gnu --sysroot=%S/Inputs/mingw_clang_tree/mingw32 %s -fuse-ld=link.exe 2>&1 | FileCheck -check-prefix=CHECK_LINK_32 %s
@@ -12,9 +12,9 @@
 // CHECK_LINK_32: "i386pe"
 
 // RUN: %clang -### -target x86_64-pc-windows-gnu --sysroot=%S/Inputs/mingw_clang_tree/mingw32 %s -fuse-ld=lld 2>&1 | FileCheck -check-prefix=CHECK_LLD_64 %s
-// CHECK_LLD_64: "lld" "-flavor" "gnu"
+// CHECK_LLD_64: lld" "-flavor" "gnu"
 // CHECK_LLD_64: "i386pep"
 
 // RUN: %clang -### -target arm-pc-windows-gnu --sysroot=%S/Inputs/mingw_clang_tree/mingw32 %s -fuse-ld=lld 2>&1 | FileCheck -check-prefix=CHECK_LLD_ARM %s
-// CHECK_LLD_ARM: "lld" "-flavor" "gnu"
+// CHECK_LLD_ARM: lld" "-flavor" "gnu"
 // CHECK_LLD_ARM: "thumb2pe"
