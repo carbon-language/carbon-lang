fa5558307b31f9cb996a8e906b273800e68363f4
Colin LeMahieu <colinl@codeaurora.org>
2015-11-13 01:46:06 +0000

    [Hexagon] NFC. Adding a number of packet correctness tests.
    llvm-svn: 253000

---
+++ llvm/test/MC/Hexagon/endloop.s
@@ -0,0 +1,19 @@
+# RUN: llvm-mc -triple=hexagon -filetype=asm %s 2>%t; FileCheck %s <%t
+
+# Check that a branch in an end-loop packet is caught.
+
+1:
+{
+	r0 = #1
+	p0 = cmp.eq (r1, r2)
+	if (p0) jump 1b
+}:endloop0
+
+2:
+{
+        r0 = #1
+        p0 = cmp.eq (r1, r2)
+        if (p0) jump 2b
+}:endloop1
+
+# CHECK: rror: packet marked with `:endloop{{.}}' cannot contain instructions that modify register
