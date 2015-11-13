3c5ada813632d2a26f6f5342d31306355d44b6fa
Pavel Labath <labath@google.com>
2015-11-13 11:56:09 +0000

    Re-add XFAILs to two tests
    these decorators were two of many removed in r253026. Unlike others, these tests still seem to be
    failing...
    
    llvm-svn: 253034

--- lldb/packages/Python/lldbsuite/test/api/multithreaded/TestMultithreaded.py
+++ lldb/packages/Python/lldbsuite/test/api/multithreaded/TestMultithreaded.py
@@ -17,6 +17,7 @@ class SBBreakpointCallbackCase(TestBase):
     @skipIfRemote
     @skipIfNoSBHeaders
     @skipIfWindows # clang-cl does not support throw or catch (llvm.org/pr24538)
+    @expectedFailureAll("llvm.org/pr23139", oslist=["linux"], compiler="gcc", compiler_version=[">=","4.9"], archs=["x86_64"])
     def test_breakpoint_callback(self):
         """Test the that SBBreakpoint callback is invoked when a breakpoint is hit. """
         self.build_and_test('driver.cpp test_breakpoint_callback.cpp',
