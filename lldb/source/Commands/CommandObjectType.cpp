54c22c2245a40a0b18e64905b422cead19d9d527
Enrico Granata <egranata@apple.com>
2015-11-14 01:09:07 +0000

    Fixes for uniformity in type clear and delete commands
    llvm-svn: 253113

--- lldb/source/Commands/CommandObjectType.cpp
+++ lldb/source/Commands/CommandObjectType.cpp
@@ -1160,10 +1160,8 @@ private:
     PerCategoryCallback(void* param,
                         const lldb::TypeCategoryImplSP& cate)
     {
-        cate->GetTypeFormatsContainer()->Clear();
-        cate->GetRegexTypeFormatsContainer()->Clear();
+        cate->Clear(eFormatCategoryItemValue | eFormatCategoryItemRegexValue);
         return true;
-        
     }
     
 public:
@@ -3581,10 +3579,11 @@ private:
     
     static bool
     PerCategoryCallback(void* param,
-                        const lldb::TypeCategoryImplSP& cate)
+                        const lldb::TypeCategoryImplSP& category_sp)
     {
         ConstString *name = (ConstString*)param;
-        return cate->Delete(*name, eFormatCategoryItemFilter | eFormatCategoryItemRegexFilter);
+        category_sp->Delete(*name, eFormatCategoryItemFilter | eFormatCategoryItemRegexFilter);
+        return true;
     }
     
 public:
@@ -3761,10 +3760,11 @@ private:
     
     static bool
     PerCategoryCallback(void* param,
-                        const lldb::TypeCategoryImplSP& cate)
+                        const lldb::TypeCategoryImplSP& category_sp)
     {
-        ConstString* name = (ConstString*)param;
-        return cate->Delete(*name, eFormatCategoryItemSynth | eFormatCategoryItemRegexSynth);
+        ConstString *name = (ConstString*)param;
+        category_sp->Delete(*name, eFormatCategoryItemSynth | eFormatCategoryItemRegexSynth);
+        return true;
     }
     
 public:
