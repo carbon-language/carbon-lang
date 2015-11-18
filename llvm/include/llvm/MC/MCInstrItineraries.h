bba11df1850e4ad220fb2f53627239cfb503f2a2
Fraser Cormack <fraser@codeplay.com>
2015-11-18 15:02:59 +0000

    Fix typo in comment. NFC.
    llvm-svn: 253462

--- llvm/include/llvm/MC/MCInstrItineraries.h
+++ llvm/include/llvm/MC/MCInstrItineraries.h
@@ -38,7 +38,7 @@ namespace llvm {
 ///
 ///   { 2, x|y, 1 }
 ///      indicates that the stage occupies either FU x or FU y for 2
-///      consecuative cycles and that the next stage starts one cycle
+///      consecutive cycles and that the next stage starts one cycle
 ///      after this stage starts. That is, the stage requirements
 ///      overlap in time.
 ///
