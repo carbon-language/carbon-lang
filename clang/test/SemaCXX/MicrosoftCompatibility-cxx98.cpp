bee2bc3932db8ef01da92afc81486543e9a09e22
David Majnemer <david.majnemer@gmail.com>
2015-11-18 21:42:38 +0000

    [MSVC Compat] Make -Wmicrosoft-cast not an error by default
    Too much code is sloppy about this to error by default.
    
    llvm-svn: 253506

--- clang/test/SemaCXX/MicrosoftCompatibility-cxx98.cpp
+++ clang/test/SemaCXX/MicrosoftCompatibility-cxx98.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 %s -triple i686-pc-win32 -fsyntax-only -std=c++98 -Wmicrosoft -verify -fms-compatibility -fexceptions -fcxx-exceptions -Wno-error=microsoft-cast
+// RUN: %clang_cc1 %s -triple i686-pc-win32 -fsyntax-only -std=c++98 -Wmicrosoft -verify -fms-compatibility -fexceptions -fcxx-exceptions
 
 
 //MSVC allows forward enum declaration
