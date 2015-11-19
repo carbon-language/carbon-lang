9bf1528573de62c150c9a80c5102c1184b704169
Sagar Thakur <sagar.thakur@imgtec.com>
2015-11-19 11:01:21 +0000

    [LLDB][MIPS] Fix lldbplatformutil.py Failure
    Patch by Nitesh Jain
    
    Summary: This patch check whether first register is readable.
    
    Subscribers: lldb-commits, mohit.bhakkad, sagar, bhushan
    Reviewers: clayborg, ovyalov, jaydeep
    Differential: http://reviews.llvm.org/D14635
    llvm-svn: 253555

--- lldb/packages/Python/lldbsuite/test/lldbplatformutil.py
+++ lldb/packages/Python/lldbsuite/test/lldbplatformutil.py
@@ -9,13 +9,19 @@ from __future__ import absolute_import
 
 # LLDB modules
 
+import re
+
 def check_first_register_readable(test_case):
-    if test_case.getArchitecture() in ['x86_64', 'i386']:
+    arch = test_case.getArchitecture()
+
+    if arch in ['x86_64', 'i386']:
         test_case.expect("register read eax", substrs = ['eax = 0x'])
-    elif test_case.getArchitecture() in ['arm']:
+    elif arch in ['arm']:
     	test_case.expect("register read r0", substrs = ['r0 = 0x'])
-    elif test_case.getArchitecture() in ['aarch64']:
+    elif arch in ['aarch64']:
         test_case.expect("register read x0", substrs = ['x0 = 0x'])
+    elif re.match("mips",arch):
+        test_case.expect("register read zero", substrs = ['zero = 0x'])
     else:
         # TODO: Add check for other architectures
         test_case.fail("Unsupported architecture for test case (arch: %s)" % test_case.getArchitecture())
