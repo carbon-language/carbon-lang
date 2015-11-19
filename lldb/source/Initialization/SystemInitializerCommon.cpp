3190d17b12bba944d86b3c59c5b86878241f9691
Ed Maste <emaste@freebsd.org>
2015-11-19 21:30:22 +0000

    Plug-in PlatformNetBSD initializer and terminator
    Patch by Kamil Rytarowski
    
    Differential Revision:	http://reviews.llvm.org/D14530
    
    llvm-svn: 253601

--- lldb/source/Initialization/SystemInitializerCommon.cpp
+++ lldb/source/Initialization/SystemInitializerCommon.cpp
@@ -115,6 +115,7 @@ SystemInitializerCommon::Initialize()
     DynamicLoaderWindowsDYLD::Initialize();
     platform_freebsd::PlatformFreeBSD::Initialize();
     platform_linux::PlatformLinux::Initialize();
+    platform_netbsd::PlatformNetBSD::Initialize();
     PlatformWindows::Initialize();
     PlatformKalimba::Initialize();
     platform_android::PlatformAndroid::Initialize();
@@ -162,6 +163,7 @@ SystemInitializerCommon::Terminate()
     DynamicLoaderWindowsDYLD::Terminate();
     platform_freebsd::PlatformFreeBSD::Terminate();
     platform_linux::PlatformLinux::Terminate();
+    platform_netbsd::PlatformNetBSD::Terminate();
     PlatformWindows::Terminate();
     PlatformKalimba::Terminate();
     platform_android::PlatformAndroid::Terminate();
