4cd1d51a4d0961ece8c8fd3d8927eec71a4e5ca3
Tilmann Scheller <t.scheller@samsung.com>
2015-11-20 13:27:30 +0000

    [Hexagon] Remove redundant assignment.
    Identified by the Clang static analyzer.
    
    llvm-svn: 253664

--- llvm/lib/Target/Hexagon/HexagonGenInsert.cpp
+++ llvm/lib/Target/Hexagon/HexagonGenInsert.cpp
@@ -1495,7 +1495,7 @@ bool HexagonGenInsert::runOnMachineFunction(MachineFunction &MF) {
   // version of DCE that preserves lifetime markers. Without it, merging
   // of stack objects can fail to recognize and merge disjoint objects
   // leading to unnecessary stack growth.
-  Changed |= removeDeadCode(MDT->getRootNode());
+  removeDeadCode(MDT->getRootNode());
 
   const HexagonEvaluator HE(*HRI, *MRI, *HII, MF);
   BitTracker BTLoc(HE, MF);
