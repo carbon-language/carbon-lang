5961fef32efb1f3c9e30a3c813795a3889d4dac3
Matthias Braun <matze@braunis.de>
2015-11-21 02:28:42 +0000

    Let's try to fix this test again with a fixed target triple
    llvm-svn: 253760

--- clang/test/CodeGen/c-unicode.c
+++ clang/test/CodeGen/c-unicode.c
@@ -1,7 +1,8 @@
-// RUN: %clang -S %s -o - | FileCheck %s -check-prefix=ALLOWED
-// RUN: not %clang -std=c89 -S %s -o - 2>&1 | FileCheck %s -check-prefix=DENIED
+// REQUIRES: x86-registered-target
+// RUN: %clang --target=x86_64--linug-gnu -S %s -o - | FileCheck %s -check-prefix=ALLOWED
+// RUN: not %clang --target=x86_64--linux-gnu -std=c89 -S %s -o - 2>&1 | FileCheck %s -check-prefix=DENIED
 int \uaccess = 0;
-// ALLOWED: "{{_?}}곎ss":
+// ALLOWED: "곎ss":
 // ALLOWED-NOT: "\uaccess":
 // DENIED: warning: universal character names are only valid in C99 or C++; treating as '\' followed by identifier [-Wunicode]
 // DENIED: error: expected identifier or '('
