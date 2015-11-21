cf24bbb12d56f556fb33ddf2e1ef5a02c3b2ab27
Yaron Keren <yaron.keren@gmail.com>
2015-11-21 06:33:54 +0000

    Unbreak build on OpenBSD by not adding -Wl,-z,defs to linker flags. This is similar to the fix for FreeBSD in r226862. Without this patch, the build aborts when linkling libLTO.so, complaining about undefined references to assert2, cxa_atexit, etc.
    Patch by Stefan Kempf!
    
    http://reviews.llvm.org/D14236
    
    llvm-svn: 253769

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -132,7 +132,8 @@ endif()
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
 if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
-        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD") AND
+        ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR
+        ${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD") AND
    NOT LLVM_USE_SANITIZER)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
 endif()
