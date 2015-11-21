84055ecb7dabc7172c395b902106b328d4f08ae1
Matthias Braun <matze@braunis.de>
2015-11-21 02:13:29 +0000

    Fix c-unicode.c testcase again.
    Specifying a fixed triple is not possible because that target may not
    even be compiler. Go for a simpler fix by using a _? regex for the
    prefix.
    
    llvm-svn: 253758

--- clang/test/CodeGen/c-unicode.c
+++ clang/test/CodeGen/c-unicode.c
@@ -1,7 +1,7 @@
-// RUN: %clang --target=x86_64--linux-gnu -S %s -o - | FileCheck %s -check-prefix=ALLOWED
-// RUN: not %clang --target=x86_64--linux-gnu -std=c89 -S %s -o - 2>&1 | FileCheck %s -check-prefix=DENIED
+// RUN: %clang -S %s -o - | FileCheck %s -check-prefix=ALLOWED
+// RUN: not %clang -std=c89 -S %s -o - 2>&1 | FileCheck %s -check-prefix=DENIED
 int \uaccess = 0;
-// ALLOWED: "곎ss":
+// ALLOWED: "{{_?}}곎ss":
 // ALLOWED-NOT: "\uaccess":
 // DENIED: warning: universal character names are only valid in C99 or C++; treating as '\' followed by identifier [-Wunicode]
 // DENIED: error: expected identifier or '('
