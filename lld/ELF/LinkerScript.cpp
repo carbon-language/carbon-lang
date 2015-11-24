44136b675df0844c0a6022456607ca931f961b85
Simon Atanasyan <simon@atanasyan.com>
2015-11-24 22:16:25 +0000

    ELF2: LinkerScript: lookup absolute paths under sysroot
    In case a sysroot prefix is configured, and the filename starts with the
    '/' character, and the script being processed was located inside the
    sysroot prefix, the file's name will be looked for in the sysroot
    prefix. Otherwise, the linker falls to the common lookup scheme.
    
    https://www.sourceware.org/binutils/docs-2.24/ld/File-Commands.html
    
    Differential Revision: http://reviews.llvm.org/D14916
    
    llvm-svn: 254031

--- lld/ELF/LinkerScript.cpp
+++ lld/ELF/LinkerScript.cpp
@@ -28,8 +28,8 @@ using namespace lld::elf2;
 namespace {
 class LinkerScript {
 public:
-  LinkerScript(BumpPtrAllocator *A, StringRef S)
-      : Saver(*A), Tokens(tokenize(S)) {}
+  LinkerScript(BumpPtrAllocator *A, StringRef S, bool B)
+      : Saver(*A), Tokens(tokenize(S)), IsUnderSysroot(B) {}
   void run();
 
 private:
@@ -58,6 +58,7 @@ private:
   StringSaver Saver;
   std::vector<StringRef> Tokens;
   size_t Pos = 0;
+  bool IsUnderSysroot;
 };
 }
 
@@ -161,6 +162,12 @@ void LinkerScript::expect(StringRef Expect) {
 
 void LinkerScript::addFile(StringRef S) {
   if (sys::path::is_absolute(S)) {
+    if (IsUnderSysroot) {
+      SmallString<128> Path;
+      (Config->Sysroot + S).toStringRef(Path);
+      if (sys::fs::exists(Path))
+        S = Saver.save(Path.str());
+    }
     Driver->addFile(S);
   } else if (S.startswith("=")) {
     if (Config->Sysroot.empty())
@@ -292,5 +299,12 @@ void LinkerScript::readOutputSectionDescription() {
 
 // Entry point. The other functions or classes are private to this file.
 void lld::elf2::readLinkerScript(BumpPtrAllocator *A, MemoryBufferRef MB) {
-  LinkerScript(A, MB.getBuffer()).run();
+  StringRef Path = MB.getBufferIdentifier();
+  if (!Config->Sysroot.empty())
+    for (; !Path.empty(); Path = sys::path::parent_path(Path))
+      if (sys::fs::equivalent(Config->Sysroot, Path)) {
+        LinkerScript(A, MB.getBuffer(), true).run();
+        return;
+      }
+  LinkerScript(A, MB.getBuffer(), false).run();
 }
