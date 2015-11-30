c8f3816e634958c73e1d9f8ea7c1089b3ec7666e
Marshall Clow <mclow.lists@gmail.com>
2015-11-30 05:10:10 +0000

    Fix bad macros
    llvm-svn: 254287

--- libcxx/test/std/utilities/meta/meta.unary.prop.query/alignment_of.pass.cpp
+++ libcxx/test/std/utilities/meta/meta.unary.prop.query/alignment_of.pass.cpp
@@ -23,7 +23,7 @@ void test_alignment_of()
     static_assert( std::alignment_of<const T>::value == A, "");
     static_assert( std::alignment_of<volatile T>::value == A, "");
     static_assert( std::alignment_of<const volatile T>::value == A, "");
-#if TEST_STD_VERS > 14
+#if TEST_STD_VER > 14
     static_assert( std::alignment_of_v<T> == A, "");
     static_assert( std::alignment_of_v<const T> == A, "");
     static_assert( std::alignment_of_v<volatile T> == A, "");
