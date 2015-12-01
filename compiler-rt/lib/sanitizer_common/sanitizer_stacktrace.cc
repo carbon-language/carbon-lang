b6f28ef2c9ca0c910890d15ba17acebf4a9abf7f
Alexey Samsonov <vonosmas@gmail.com>
2015-12-01 20:58:44 +0000

    [Sanitizer] Increase kPcThreshold in slow unwinder after r254395.
    llvm-svn: 254460

--- compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_stacktrace.cc
@@ -118,7 +118,7 @@ void BufferedStackTrace::PopStackFrames(uptr count) {
 uptr BufferedStackTrace::LocatePcInTrace(uptr pc) {
   // Use threshold to find PC in stack trace, as PC we want to unwind from may
   // slightly differ from return address in the actual unwinded stack trace.
-  const int kPcThreshold = 304;
+  const int kPcThreshold = 320;
   for (uptr i = 0; i < size; ++i) {
     if (MatchPc(pc, trace[i], kPcThreshold))
       return i;
