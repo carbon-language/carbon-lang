4b971e44636dc5396b8cf4d7c2c24fa4ce66c009
Dan Gohman <dan433584@gmail.com>
2015-12-05 01:36:31 +0000

    [WebAssembly] Remove an obsolete TODO comment.
    llvm-svn: 254817

--- clang/lib/Basic/Targets.cpp
+++ clang/lib/Basic/Targets.cpp
@@ -7192,7 +7192,6 @@ private:
                    clang::WebAssembly::LastTSBuiltin - Builtin::FirstTSBuiltin);
   }
   BuiltinVaListKind getBuiltinVaListKind() const final {
-    // TODO: Implement va_list properly.
     return VoidPtrBuiltinVaList;
   }
   ArrayRef<const char *> getGCCRegNames() const final {
