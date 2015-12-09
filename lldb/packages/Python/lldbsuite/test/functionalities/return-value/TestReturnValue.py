35b952ac3e9b5a0929539e959cd167a88b2cb8b2
Pavel Labath <labath@google.com>
2015-12-09 15:49:40 +0000

    Correctly XFAIL TestReturnValue
    android is not an "os", use the target triple to match it.
    
    llvm-svn: 255118

--- lldb/packages/Python/lldbsuite/test/functionalities/return-value/TestReturnValue.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/return-value/TestReturnValue.py
@@ -18,7 +18,7 @@ class ReturnValueTestCase(TestBase):
 
     @expectedFailureAll(oslist=["macosx","freebsd"], archs=["i386"])
     @expectedFailureAll(oslist=["linux"], compiler="clang", compiler_version=["<=", "3.6"], archs=["i386"])
-    @expectedFailureAll(bugnumber="llvm.org/pr25785", oslist=["android"], hostoslist=["windows"], compiler="gcc", archs=["i386"])
+    @expectedFailureAll(bugnumber="llvm.org/pr25785", hostoslist=["windows"], compiler="gcc", archs=["i386"], triple='.*-android')
     @expectedFailureWindows("llvm.org/pr24778")
     @add_test_categories(['pyapi'])
     def test_with_python(self):
