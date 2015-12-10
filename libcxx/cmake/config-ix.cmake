2abfe726b2a1896dbe3966e26689752b54ab0617
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2015-12-10 00:45:18 +0000

    Replace cmake check for printf with a check for fopen.
    Printf is a builtin, and the check fails with -Werror because of a clang
    warning about an incompatible redeclaration.
    
    llvm-svn: 255187

--- libcxx/cmake/config-ix.cmake
+++ libcxx/cmake/config-ix.cmake
@@ -13,7 +13,7 @@ check_cxx_compiler_flag(/GR-                    LIBCXX_HAS_NO_GR_FLAG)
 
 # Check libraries
 check_library_exists(pthread pthread_create "" LIBCXX_HAS_PTHREAD_LIB)
-check_library_exists(c printf "" LIBCXX_HAS_C_LIB)
+check_library_exists(c fopen "" LIBCXX_HAS_C_LIB)
 check_library_exists(m ccos "" LIBCXX_HAS_M_LIB)
 check_library_exists(rt clock_gettime "" LIBCXX_HAS_RT_LIB)
 check_library_exists(gcc_s __gcc_personality_v0 "" LIBCXX_HAS_GCC_S_LIB)
