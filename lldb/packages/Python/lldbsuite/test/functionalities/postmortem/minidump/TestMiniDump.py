cf6177131ab6ad50df208053ab54c8ca302f521e
Adrian McCarthy <amccarth@google.com>
2015-12-11 22:27:57 +0000

    Add some tests for stack and local variable inspection for mini dumps.
    Differential Revision: http://reviews.llvm.org/D15435
    
    llvm-svn: 255379

--- lldb/packages/Python/lldbsuite/test/functionalities/postmortem/minidump/TestMiniDump.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/postmortem/minidump/TestMiniDump.py
@@ -10,21 +10,30 @@ import lldb
 from lldbsuite.test.lldbtest import *
 import lldbsuite.test.lldbutil as lldbutil
 
-@skipUnlessWindows  # for now mini-dump debugging is limited to Windows hosts
 class MiniDumpTestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
+    @skipUnlessWindows  # for now mini-dump debugging is limited to Windows hosts
     @no_debug_info_test
     def test_process_info_in_mini_dump(self):
         """Test that lldb can read the process information from the minidump."""
+        # target create -c fizzbuzz_no_heap.dmp
+        self.dbg.CreateTarget("")
+        self.target = self.dbg.GetSelectedTarget()
+        self.process = self.target.LoadCore("fizzbuzz_no_heap.dmp")
         self.assertTrue(self.process, PROCESS_IS_VALID)
         self.assertEqual(self.process.GetNumThreads(), 1)
         self.assertEqual(self.process.GetProcessID(), 4440)
 
+    @skipUnlessWindows  # for now mini-dump debugging is limited to Windows hosts
     @no_debug_info_test
     def test_thread_info_in_mini_dump(self):
         """Test that lldb can read the thread information from the minidump."""
+        # target create -c fizzbuzz_no_heap.dmp
+        self.dbg.CreateTarget("")
+        self.target = self.dbg.GetSelectedTarget()
+        self.process = self.target.LoadCore("fizzbuzz_no_heap.dmp")
         # This process crashed due to an access violation (0xc0000005) in its one and only thread.
         self.assertEqual(self.process.GetNumThreads(), 1)
         thread = self.process.GetThreadAtIndex(0)
@@ -32,9 +41,14 @@ class MiniDumpTestCase(TestBase):
         stop_description = thread.GetStopDescription(256);
         self.assertTrue("0xc0000005" in stop_description);
 
+    @skipUnlessWindows  # for now mini-dump debugging is limited to Windows hosts
     @no_debug_info_test
     def test_stack_info_in_mini_dump(self):
-        """Test that we can see the stack."""
+        """Test that we can see a trivial stack in a VS-generate mini dump."""
+        # target create -c fizzbuzz_no_heap.dmp
+        self.dbg.CreateTarget("")
+        self.target = self.dbg.GetSelectedTarget()
+        self.process = self.target.LoadCore("fizzbuzz_no_heap.dmp")
         self.assertEqual(self.process.GetNumThreads(), 1)
         thread = self.process.GetThreadAtIndex(0)
         # The crash is in main, so there should be one frame on the stack.
@@ -46,10 +60,69 @@ class MiniDumpTestCase(TestBase):
         self.assertTrue(eip.IsValid())
         self.assertEqual(pc, eip.GetValueAsUnsigned())
 
-    def setUp(self):
-        # Call super's setUp().
-        TestBase.setUp(self)
-        # target create -c fizzbuzz_no_heap.dmp
-        self.dbg.CreateTarget("")
-        self.target = self.dbg.GetSelectedTarget()
-        self.process = self.target.LoadCore("fizzbuzz_no_heap.dmp")
+    @skipUnlessWindows
+    @not_remote_testsuite_ready
+    def test_deeper_stack_in_mini_dump(self):
+        """Test that we can examine a more interesting stack in a mini dump."""
+        self.build()
+        exe = os.path.join(os.getcwd(), "a.out")
+        core = os.path.join(os.getcwd(), "core.dmp")
+        try:
+            # Set a breakpoint and capture a mini dump.
+            target = self.dbg.CreateTarget(exe)
+            breakpoint = target.BreakpointCreateByName("bar")
+            process = target.LaunchSimple(None, None, self.get_process_working_directory())
+            self.assertEqual(process.GetState(), lldb.eStateStopped)
+            self.assertTrue(process.SaveCore(core))
+            self.assertTrue(os.path.isfile(core))
+            self.assertTrue(process.Kill().Success())
+
+            # Launch with the mini dump, and inspect the stack.
+            target = self.dbg.CreateTarget(None)
+            process = target.LoadCore(core)
+            thread = process.GetThreadAtIndex(0)
+
+            expected_stack = { 0: 'bar', 1: 'foo', 2: 'main' }
+            self.assertEqual(thread.GetNumFrames(), len(expected_stack))
+            for index, name in expected_stack.iteritems():
+                frame = thread.GetFrameAtIndex(index)
+                self.assertTrue(frame.IsValid())
+                function_name = frame.GetFunctionName()
+                self.assertTrue(name in function_name)
+
+        finally:
+            # Clean up the mini dump file.
+            self.assertTrue(self.dbg.DeleteTarget(target))
+            if (os.path.isfile(core)):
+                os.unlink(core)
+
+    @skipUnlessWindows
+    @not_remote_testsuite_ready
+    def test_local_variables_in_mini_dump(self):
+        """Test that we can examine local variables in a mini dump."""
+        self.build()
+        exe = os.path.join(os.getcwd(), "a.out")
+        core = os.path.join(os.getcwd(), "core.dmp")
+        try:
+            # Set a breakpoint and capture a mini dump.
+            target = self.dbg.CreateTarget(exe)
+            breakpoint = target.BreakpointCreateByName("bar")
+            process = target.LaunchSimple(None, None, self.get_process_working_directory())
+            self.assertEqual(process.GetState(), lldb.eStateStopped)
+            self.assertTrue(process.SaveCore(core))
+            self.assertTrue(os.path.isfile(core))
+            self.assertTrue(process.Kill().Success())
+
+            # Launch with the mini dump, and inspect a local variable.
+            target = self.dbg.CreateTarget(None)
+            process = target.LoadCore(core)
+            thread = process.GetThreadAtIndex(0)
+            frame = thread.GetFrameAtIndex(0)
+            value = frame.EvaluateExpression('x')
+            self.assertEqual(value.GetValueAsSigned(), 3)
+
+        finally:
+            # Clean up the mini dump file.
+            self.assertTrue(self.dbg.DeleteTarget(target))
+            if (os.path.isfile(core)):
+                os.unlink(core)
