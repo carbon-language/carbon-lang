8f3118f4499cb7a6d086a1d55dac3fe1485ab7ce
Richard Trieu <rtrieu@google.com>
2015-12-15 23:47:17 +0000

    Remove one of the void casts used to suppress unused variable warning.
    llvm-svn: 255709

--- llvm/lib/Transforms/IPO/CrossDSOCFI.cpp
+++ llvm/lib/Transforms/IPO/CrossDSOCFI.cpp
@@ -89,7 +89,6 @@ ConstantInt *CrossDSOCFI::extractBitSetTypeId(MDNode *MD) {
   // Can be null if a function was removed by an optimization.
   if (FM) {
     auto F = dyn_cast<Function>(FM->getValue());
-    (void)F;
     // But can never be a function declaration.
     assert(!F || !F->isDeclaration());
     (void)F; // Suppress unused variable warning in the no-asserts build.
