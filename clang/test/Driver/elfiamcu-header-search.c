1f5d84545208f0453ecfe856d74f11c25b9a4aaf
Diego Novillo <dnovillo@google.com>
2015-12-16 19:52:05 +0000

    Add -fsyntax-only to fix failure in read-only directories.
    Internally, this test is executed in a read-only directory, which causes
    it to fail because the driver tries to generate a file unnecessarily.
    Adding -fsyntax-only fixes the issue (thanks to Artem Belevich for
    figuring out the root cause).
    
    llvm-svn: 255809

--- clang/test/Driver/elfiamcu-header-search.c
+++ clang/test/Driver/elfiamcu-header-search.c
@@ -1,6 +1,6 @@
 // REQUIRES: x86-registered-target
 
-// RUN: %clang -target i386-pc-elfiamcu -c -v %s 2>&1 | FileCheck %s
+// RUN: %clang -target i386-pc-elfiamcu -c -v -fsyntax-only %s 2>&1 | FileCheck %s
 // CHECK-NOT: /usr/include
 // CHECK-NOT: /usr/local/include
 
