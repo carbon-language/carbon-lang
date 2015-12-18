545b8861fc40bb194d6e8daa382f9d6d4659a13b
Eric Fiselier <eric@efcs.ca>
2015-12-18 00:36:55 +0000

    [libcxx] LWG2485: get() should be overloaded for const tuple&&. Patch from K-Ballo.
    Review: http://reviews.llvm.org/D14839
    llvm-svn: 255941

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.elem/tuple.by.type.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.elem/tuple.by.type.pass.cpp
@@ -10,30 +10,31 @@
 // UNSUPPORTED: c++98, c++03, c++11
 
 #include <tuple>
+#include <utility>
 #include <string>
 #include <complex>
+#include <type_traits>
 
 #include <cassert>
 
 int main()
 {
-#if _LIBCPP_STD_VER > 11
     typedef std::complex<float> cf;
     {
     auto t1 = std::tuple<int, std::string, cf> { 42, "Hi", { 1,2 }};
-    assert ( std::get<int>(t1) == 42 ); // find at the beginning 
+    assert ( std::get<int>(t1) == 42 ); // find at the beginning
     assert ( std::get<std::string>(t1) == "Hi" ); // find in the middle
     assert ( std::get<cf>(t1).real() == 1 ); // find at the end
     assert ( std::get<cf>(t1).imag() == 2 );
     }
-    
+
     {
     auto t2 = std::tuple<int, std::string, int, cf> { 42, "Hi", 23, { 1,2 }};
 //  get<int> would fail!
     assert ( std::get<std::string>(t2) == "Hi" );
     assert (( std::get<cf>(t2) == cf{ 1,2 } ));
     }
-    
+
     {
     constexpr std::tuple<int, const int, double, double> p5 { 1, 2, 3.4, 5.6 };
     static_assert ( std::get<int>(p5) == 1, "" );
@@ -53,8 +54,40 @@ int main()
     std::tuple<upint> t(upint(new int(4)));
     upint p = std::get<upint>(std::move(t)); // get rvalue
     assert(*p == 4);
-    assert(std::get<0>(t) == nullptr); // has been moved from
+    assert(std::get<upint>(t) == nullptr); // has been moved from
     }
 
-#endif
+    {
+    typedef std::unique_ptr<int> upint;
+    const std::tuple<upint> t(upint(new int(4)));
+    const upint&& p = std::get<upint>(std::move(t)); // get const rvalue
+    assert(*p == 4);
+    assert(std::get<upint>(t) != nullptr);
+    }
+
+    {
+    int x = 42;
+    int tuple y = 43;
+    std::tuple<int&, int const&> const t(x, y);
+    static_assert(std::is_same<int&, decltype(std::get<int&>(std::move(t)))>::value, "");
+    static_assert(noexcept(std::get<int&>(std::move(t))), "");
+    static_assert(std::is_same<int const&, decltype(std::get<int const&>(std::move(t)))>::value, "");
+    static_assert(noexcept(std::get<int const&>(std::move(t))), "");
+    }
+
+    {
+    int x = 42;
+    int tuple y = 43;
+    std::tuple<int&&, int const&&> const t(std::move(x), std::move(y));
+    static_assert(std::is_same<int&&, decltype(std::get<int&&>(std::move(t)))>::value, "");
+    static_assert(noexcept(std::get<int&&>(std::move(t))), "");
+    static_assert(std::is_same<int const&&, decltype(std::get<int const&&>(std::move(t)))>::value, "");
+    static_assert(noexcept(std::get<int const&&>(std::move(t))), "");
+    }
+
+    {
+    constexpr const std::tuple<int, const int, double, double> t { 1, 2, 3.4, 5.6 };
+    static_assert(std::get<int>(std::move(t)) == 1, "");
+    static_assert(std::get<const int>(std::move(t)) == 2, "");
+    }
 }
