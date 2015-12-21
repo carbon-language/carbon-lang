751a449e2fe199fd983f32de659af5057af5c72c
Dylan McKay <dylanmckay34@gmail.com>
2015-12-21 23:13:15 +0000

    [AVR] Added configuration file and machine function information class
    This commit adds the 'AVRMachineFunctionInfo' class, which simply stores
    basic properties about generated machine functions.
    
    llvm-svn: 256213

---
+++ llvm/lib/Target/AVR/AVRConfig.h
@@ -0,0 +1,15 @@
+//===-- AVRConfig.h - AVR Backend Configuration Header ----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_AVR_CONFIG_H
+#define LLVM_AVR_CONFIG_H
+
+#define LLVM_AVR_GCC_COMPAT
+
+#endif // LLVM_AVR_CONFIG_H
