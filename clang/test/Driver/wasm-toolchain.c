0746adeb8648ef581d549b2d5cae0ec622a6a874
Eric Christopher <echristo@gmail.com>
2015-12-22 01:45:45 +0000

    Use -no-canonical-prefixes to make sure binaries names are easier to match.
    llvm-svn: 256228

--- clang/test/Driver/wasm-toolchain.c
+++ clang/test/Driver/wasm-toolchain.c
@@ -1,3 +1,3 @@
-// RUN: %clang -### -target wasm32-unknown-unknown -x assembler %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
+// RUN: %clang -### -no-canonical-prefixes -target wasm32-unknown-unknown -x assembler %s 2>&1 | FileCheck -check-prefix=AS_LINK %s
 // AS_LINK: clang{{.*}}" "-cc1as" {{.*}} "-o" "[[temp:[^"]*]]"
 // AS_LINK: lld" "-flavor" "ld" "[[temp]]" "-o" "a.out"
