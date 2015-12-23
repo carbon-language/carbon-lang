7a608aa232c18cefebde9eb5035ccdcc17f58c9d
Eric Fiselier <eric@efcs.ca>
2015-12-23 08:20:26 +0000

    [libcxx] Fix LWG Issue #2367 - Fixing std::tuple and std::pair's default constructors.
    Summary: This patch implements the solution for LWG Issue #2367. See http://cplusplus.github.io/LWG/lwg-active.html#2367
    
    Reviewers: mclow.lists, EricWF
    
    Subscribers: cfe-commits
    
    Differential Revision: http://reviews.llvm.org/D13750
    
    llvm-svn: 256325

--- libcxx/test/std/utilities/utility/pairs/pairs.pair/default.pass.cpp
+++ libcxx/test/std/utilities/utility/pairs/pairs.pair/default.pass.cpp
@@ -13,8 +13,14 @@
 
 // constexpr pair();
 
+// NOTE: The SFINAE on the default constructor is tested in
+//       default-sfinae.pass.cpp
+
 #include <utility>
+#include <type_traits>
 #include <cassert>
+
+#include "test_macros.h"
 
 int main()
 {
@@ -24,13 +30,12 @@ int main()
     assert(p.first == 0.0f);
     assert(p.second == nullptr);
     }
-    
-#if _LIBCPP_STD_VER > 11
+#if TEST_STD_VER >= 11
     {
-    typedef std::pair<float, short*> P;
-    constexpr P p;
-    static_assert(p.first == 0.0f, "");
-    static_assert(p.second == nullptr, "");
+        typedef std::pair<float, short*> P;
+        constexpr P p;
+        static_assert(p.first == 0.0f, "");
+        static_assert(p.second == nullptr, "");
     }
 #endif
 }
