e238372258e0d6950923a8b69fd89feab34f07ee
Ben Craig <ben.craig@codeaurora.org>
2015-12-29 22:43:17 +0000

    [libcxx] Fixing the Linux sanitizer builds
    llvm-svn: 256591

--- libcxx/test/libcxx/test/target_info.py
+++ libcxx/test/libcxx/test/target_info.py
@@ -142,7 +142,8 @@ class LinuxLocalTI(DefaultTargetInfo):
             flags += ['-lunwind', '-ldl']
         else:
             flags += ['-lgcc_s', '-lgcc']
-        if self.full_config.get_lit_bool('use_sanitizer', False):
+        san = self.get_lit_conf('use_sanitizer', '').strip()
+        if san:
             # The libraries and their order are taken from the
             # linkSanitizerRuntimeDeps function in
             # clang/lib/Driver/Tools.cpp
