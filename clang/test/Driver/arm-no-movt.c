18799ff8376f8e00840d41feebb7f8c2b55adada
Davide Italiano <davide@freebsd.org>
2015-12-30 13:53:25 +0000

    Disable generating movt on FreeBSD.
    It's sort of an hack, but we have no choice.
    The linker in the base system doesn't handle that correctly (yet).
    Once FreeBSD will import lld, this can be backed out.
    
    Patch by: Andrew Turner!
    
    llvm-svn: 256641

--- clang/test/Driver/arm-no-movt.c
+++ clang/test/Driver/arm-no-movt.c
@@ -4,6 +4,11 @@
 // RUN: %clang -target armv7-apple-darwin -mkernel -### %s 2>&1 \
 // RUN:    | FileCheck %s -check-prefix CHECK-KERNEL
 
+// RUN: %clang -target armv7-gnueabi-freebsd11 -### %s 2>&1 \
+// RUN:    | FileCheck %s -check-prefix CHECK-FREEBSD
+
 // CHECK-DEFAULT-NOT: "-target-feature" "+no-movt"
 
 // CHECK-KERNEL: "-target-feature" "+no-movt"
+
+// CHECK-FREEBSD: "-target-feature" "+no-movt"
