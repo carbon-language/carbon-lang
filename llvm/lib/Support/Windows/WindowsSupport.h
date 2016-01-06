34c0199378625cded2b0eee86179d7d7702d575b
Yunzhong Gao <Yunzhong_Gao@playstation.sony.com>
2016-01-06 03:01:10 +0000

    Do not define NOGDI. Mingw defines LOGFONTW type in wingdi.h and the mingw version of shlobj.h includes shobjidl.h and the latter uses the LOGFONTW type.
    llvm-svn: 256904

--- llvm/lib/Support/Windows/WindowsSupport.h
+++ llvm/lib/Support/Windows/WindowsSupport.h
@@ -30,7 +30,6 @@
 #define _WIN32_WINNT 0x0601
 #define _WIN32_IE    0x0800 // MinGW at it again. FIXME: verify if still needed.
 #define WIN32_LEAN_AND_MEAN
-#define NOGDI
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
