b0244454449bd5518a2e964d5ad01828c0fc7e8b
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-01-11 15:47:41 +0000

    [Hexagon] Mark D14 and GP as reserved registers
    llvm-svn: 257336

--- llvm/lib/Target/Hexagon/HexagonRegisterInfo.cpp
+++ llvm/lib/Target/Hexagon/HexagonRegisterInfo.cpp
@@ -103,6 +103,8 @@ BitVector HexagonRegisterInfo::getReservedRegs(const MachineFunction &MF)
   Reserved.set(Hexagon::R30);
   Reserved.set(Hexagon::R31);
   Reserved.set(Hexagon::PC);
+  Reserved.set(Hexagon::GP);
+  Reserved.set(Hexagon::D14);
   Reserved.set(Hexagon::D15);
   Reserved.set(Hexagon::LC0);
   Reserved.set(Hexagon::LC1);
