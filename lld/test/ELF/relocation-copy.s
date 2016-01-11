5644d391658f1ba82512f751b5cb1f4576351bf0
George Rimar <grimar@accesssoftek.com>
2016-01-11 13:25:21 +0000

    [ELF] - improved relocation-copy.s test (added R_X86_64_32 relocations)
    * Added instructions to generate R_X86_64_32 relocations. Without that next part of code was uncovered by test, code worked without it:
    bool X86_64TargetInfo::relocNeedsPlt(uint32_t Type, const SymbolBody &S) const {
      if (needsCopyRel(Type, S))
        return false;
    * Removed -e main, added _start
    
    Differential revision: http://reviews.llvm.org/D15714
    
    llvm-svn: 257331

--- lld/test/ELF/relocation-copy.s
+++ lld/test/ELF/relocation-copy.s
@@ -2,18 +2,19 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %p/Inputs/relocation-copy.s -o %t2.o
 // RUN: ld.lld -shared %t2.o -o %t.so
-// RUN: ld.lld -e main %t.o %t.so -o %t3
+// RUN: ld.lld %t.o %t.so -o %t3
 // RUN: llvm-readobj -s -r --expand-relocs %t3 | FileCheck %s
 // RUN: llvm-objdump -d %t3 | FileCheck -check-prefix=CODE %s
 
 .text
-.globl main
-.align 16, 0x90
-.type main,@function
-main:
+.global _start
+_start:
 movl $5, x
 movl $7, y
 movl $9, z
+movl $x, %edx
+movl $y, %edx
+movl $z, %edx
 
 // CHECK:      Name: .bss
 // CHECK-NEXT:  Type: SHT_NOBITS (0x8)
@@ -57,7 +58,10 @@ movl $9, z
 // 77840 = 0x13000 + 16
 // 77844 = 0x13000 + 16 + 4
 // CODE: Disassembly of section .text:
-// CODE-NEXT: main:
+// CODE-NEXT: _start:
 // CODE-NEXT: 11000: c7 04 25 00 30 01 00 05 00 00 00 movl $5, 77824
 // CODE-NEXT: 1100b: c7 04 25 10 30 01 00 07 00 00 00 movl $7, 77840
 // CODE-NEXT: 11016: c7 04 25 14 30 01 00 09 00 00 00 movl $9, 77844
+// CODE-NEXT: 11021: ba 00 30 01 00                   movl $77824, %edx
+// CODE-NEXT: 11026: ba 10 30 01 00                   movl $77840, %edx
+// CODE-NEXT: 1102b: ba 14 30 01 00                   movl $77844, %edx
