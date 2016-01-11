156622ad9d95727af893ee8f7924fbf4b962db11
Craig Topper <craig.topper@gmail.com>
2016-01-11 00:44:56 +0000

    [AVX-512] Remove unused Round and Itinerary from the maskable_cmp multiclasses. They weren't used and there were extra spaces in the asm string to prepare for the concatenations of the round string that wasn't ever used.
    llvm-svn: 257300

--- llvm/test/MC/X86/intel-syntax-avx512.s
+++ llvm/test/MC/X86/intel-syntax-avx512.s
@@ -32,7 +32,7 @@ vaddpd zmm1,zmm1,zmm2,{rd-sae}
 // CHECK:  encoding: [0x62,0xf1,0xf5,0x78,0x58,0xca]
 vaddpd zmm1,zmm1,zmm2,{rz-sae}
 
-// CHECK: vcmppd k2 , zmm12, zmm26, 171
+// CHECK: vcmppd k2, zmm12, zmm26, 171
 // CHECK:  encoding: [0x62,0x91,0x9d,0x48,0xc2,0xd2,0xab]
           vcmppd k2,zmm12,zmm26,0xab
 
@@ -40,63 +40,63 @@ vaddpd zmm1,zmm1,zmm2,{rz-sae}
 // CHECK:  encoding: [0x62,0x91,0x9d,0x4b,0xc2,0xd2,0xab]
           vcmppd k2{k3},zmm12,zmm26,0xab
 
-// CHECK: vcmppd k2 , zmm12, zmm26, {sae}, 171
+// CHECK: vcmppd k2, zmm12, zmm26, {sae}, 171
 // CHECK:  encoding: [0x62,0x91,0x9d,0x18,0xc2,0xd2,0xab]
           vcmppd k2,zmm12,zmm26,{sae},0xab
 
-// CHECK: vcmppd k2 , zmm12, zmm26, 123
+// CHECK: vcmppd k2, zmm12, zmm26, 123
 // CHECK:  encoding: [0x62,0x91,0x9d,0x48,0xc2,0xd2,0x7b]
           vcmppd k2 ,zmm12,zmm26,0x7b
 
-// CHECK: vcmppd k2 , zmm12, zmm26, {sae}, 123
+// CHECK: vcmppd k2, zmm12, zmm26, {sae}, 123
 // CHECK:  encoding: [0x62,0x91,0x9d,0x18,0xc2,0xd2,0x7b]
           vcmppd k2,zmm12,zmm26,{sae},0x7b
 
-// CHECK: vcmppd k2 , zmm12, zmmword ptr [rcx], 123
+// CHECK: vcmppd k2, zmm12, zmmword ptr [rcx], 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x48,0xc2,0x11,0x7b]
           vcmppd k2,zmm12,zmmword PTR [rcx],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, zmmword ptr [rax + 8*r14 + 291], 123
+// CHECK: vcmppd  k2, zmm12, zmmword ptr [rax + 8*r14 + 291], 123
 // CHECK:  encoding: [0x62,0xb1,0x9d,0x48,0xc2,0x94,0xf0,0x23,0x01,0x00,0x00,0x7b]
           vcmppd k2 ,zmm12,zmmword PTR [rax+r14*8+0x123],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, qword ptr [rcx]{1to8}, 123
+// CHECK: vcmppd  k2, zmm12, qword ptr [rcx]{1to8}, 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x58,0xc2,0x11,0x7b]
           vcmppd k2,zmm12,QWORD PTR [rcx]{1to8},0x7b
 
-// CHECK: vcmppd  k2 , zmm12, zmmword ptr [rdx + 8128], 123
+// CHECK: vcmppd  k2, zmm12, zmmword ptr [rdx + 8128], 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x48,0xc2,0x52,0x7f,0x7b]
           vcmppd k2,zmm12,zmmword PTR [rdx+0x1fc0],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, zmmword ptr [rdx + 8192], 123
+// CHECK: vcmppd  k2, zmm12, zmmword ptr [rdx + 8192], 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x48,0xc2,0x92,0x00,0x20,0x00,0x00,0x7b]
           vcmppd k2,zmm12,zmmword PTR [rdx+0x2000],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, zmmword ptr [rdx - 8192], 123
+// CHECK: vcmppd  k2, zmm12, zmmword ptr [rdx - 8192], 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x48,0xc2,0x52,0x80,0x7b]
           vcmppd k2,zmm12,zmmword PTR [rdx-0x2000],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, zmmword ptr [rdx - 8256], 123
+// CHECK: vcmppd  k2, zmm12, zmmword ptr [rdx - 8256], 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x48,0xc2,0x92,0xc0,0xdf,0xff,0xff,0x7b]
           vcmppd k2,zmm12,zmmword PTR [rdx-0x2040],0x7b
 
-// CHECK: vcmppd  k2 , zmm12, qword ptr [rdx + 1016]{1to8}, 123
+// CHECK: vcmppd  k2, zmm12, qword ptr [rdx + 1016]{1to8}, 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x58,0xc2,0x52,0x7f,0x7b]
           vcmppd k2,zmm12,QWORD PTR [rdx+0x3f8]{1to8},0x7b
 
-// CHECK: vcmppd  k2 , zmm12, qword ptr [rdx + 1024]{1to8}, 123
+// CHECK: vcmppd  k2, zmm12, qword ptr [rdx + 1024]{1to8}, 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x58,0xc2,0x92,0x00,0x04,0x00,0x00,0x7b]
           vcmppd k2,zmm12,QWORD PTR [rdx+0x400]{1to8},0x7b
 
-// CHECK: vcmppd  k2 , zmm12, qword ptr [rdx - 1024]{1to8}, 123
+// CHECK: vcmppd  k2, zmm12, qword ptr [rdx - 1024]{1to8}, 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x58,0xc2,0x52,0x80,0x7b]
           vcmppd k2,zmm12,QWORD PTR [rdx-0x400]{1to8},0x7b
 
-// CHECK: vcmppd  k2 , zmm12, qword ptr [rdx - 1032]{1to8}, 123
+// CHECK: vcmppd  k2, zmm12, qword ptr [rdx - 1032]{1to8}, 123
 // CHECK:  encoding: [0x62,0xf1,0x9d,0x58,0xc2,0x92,0xf8,0xfb,0xff,0xff,0x7b]
           vcmppd k2,zmm12,QWORD PTR [rdx-0x408]{1to8},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmm22, 171
+// CHECK: vcmpps  k2, zmm17, zmm22, 171
 // CHECK:  encoding: [0x62,0xb1,0x74,0x40,0xc2,0xd6,0xab]
           vcmpps k2,zmm17,zmm22,0xab
 
@@ -104,59 +104,59 @@ vaddpd zmm1,zmm1,zmm2,{rz-sae}
 // CHECK:  encoding: [0x62,0xb1,0x74,0x43,0xc2,0xd6,0xab]
           vcmpps k2{k3},zmm17,zmm22,0xab
 
-// CHECK: vcmpps  k2 , zmm17, zmm22, {sae}, 171
+// CHECK: vcmpps  k2, zmm17, zmm22, {sae}, 171
 // CHECK:  encoding: [0x62,0xb1,0x74,0x10,0xc2,0xd6,0xab]
           vcmpps k2,zmm17,zmm22,{sae},0xab
 
-// CHECK: vcmpps  k2 , zmm17, zmm22, 123
+// CHECK: vcmpps  k2, zmm17, zmm22, 123
 // CHECK:  encoding: [0x62,0xb1,0x74,0x40,0xc2,0xd6,0x7b]
           vcmpps k2,zmm17,zmm22,0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmm22, {sae}, 123
+// CHECK: vcmpps  k2, zmm17, zmm22, {sae}, 123
 // CHECK:  encoding: [0x62,0xb1,0x74,0x10,0xc2,0xd6,0x7b]
           vcmpps k2,zmm17,zmm22,{sae},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rcx], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rcx], 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x40,0xc2,0x11,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rcx],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rax + 8*r14 + 291], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rax + 8*r14 + 291], 123
 // CHECK:  encoding: [0x62,0xb1,0x74,0x40,0xc2,0x94,0xf0,0x23,0x01,0x00,0x00,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rax+r14*8+0x123],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, dword ptr [rcx]{1to16}, 123
+// CHECK: vcmpps  k2, zmm17, dword ptr [rcx]{1to16}, 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x50,0xc2,0x11,0x7b]
           vcmpps k2,zmm17,DWORD PTR [rcx]{1to16},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rdx + 8128], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rdx + 8128], 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x40,0xc2,0x52,0x7f,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rdx+0x1fc0],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rdx + 8192], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rdx + 8192], 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x40,0xc2,0x92,0x00,0x20,0x00,0x00,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rdx+0x2000],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rdx - 8192], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rdx - 8192], 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x40,0xc2,0x52,0x80,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rdx-0x2000],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, zmmword ptr [rdx - 8256], 123
+// CHECK: vcmpps  k2, zmm17, zmmword ptr [rdx - 8256], 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x40,0xc2,0x92,0xc0,0xdf,0xff,0xff,0x7b]
           vcmpps k2,zmm17,zmmword PTR [rdx-0x2040],0x7b
 
-// CHECK: vcmpps  k2 , zmm17, dword ptr [rdx + 508]{1to16}, 123
+// CHECK: vcmpps  k2, zmm17, dword ptr [rdx + 508]{1to16}, 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x50,0xc2,0x52,0x7f,0x7b]
           vcmpps k2,zmm17,DWORD PTR [rdx+0x1fc]{1to16},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, dword ptr [rdx + 512]{1to16}, 123
+// CHECK: vcmpps  k2, zmm17, dword ptr [rdx + 512]{1to16}, 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x50,0xc2,0x92,0x00,0x02,0x00,0x00,0x7b]
           vcmpps k2,zmm17,DWORD PTR [rdx+0x200]{1to16},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, dword ptr [rdx - 512]{1to16}, 123
+// CHECK: vcmpps  k2, zmm17, dword ptr [rdx - 512]{1to16}, 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x50,0xc2,0x52,0x80,0x7b]
           vcmpps k2,zmm17,DWORD PTR [rdx-0x200]{1to16},0x7b
 
-// CHECK: vcmpps  k2 , zmm17, dword ptr [rdx - 516]{1to16}, 123
+// CHECK: vcmpps  k2, zmm17, dword ptr [rdx - 516]{1to16}, 123
 // CHECK:  encoding: [0x62,0xf1,0x74,0x50,0xc2,0x92,0xfc,0xfd,0xff,0xff,0x7b]
           vcmpps k2,zmm17,DWORD PTR [rdx-0x204]{1to16},0x7b
 
