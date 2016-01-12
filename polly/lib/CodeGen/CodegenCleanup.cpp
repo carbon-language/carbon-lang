b0c4e49a371603bd38b2476675bc957dc5cdb360
Roman Gareev <gareevroman@gmail.com>
2016-01-12 20:47:48 +0000

    Fix of r257495.
    Remove redundant "FPM->add(createDemoteRegisterToMemoryPass());"
    
    llvm-svn: 257514

--- polly/lib/CodeGen/CodegenCleanup.cpp
+++ polly/lib/CodeGen/CodegenCleanup.cpp
@@ -68,7 +68,6 @@ public:
     FPM->add(createIndVarSimplifyPass());
     FPM->add(createLoopIdiomPass());
     FPM->add(createLoopDeletionPass());
-    FPM->add(createDemoteRegisterToMemoryPass());
     FPM->add(createCFGSimplificationPass());
     FPM->add(createSimpleLoopUnrollPass());
     FPM->add(createMergedLoadStoreMotionPass());
