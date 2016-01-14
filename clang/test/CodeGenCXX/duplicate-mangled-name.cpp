834ea6826e7afb8016f79efbda392bdf12e90742
Andrey Bokhanko <andreybokhanko@gmail.com>
2016-01-14 11:53:50 +0000

    Fix for armv7-a15 and thumbv7-a15 buildbot fails.
    llvm-svn: 257757

--- clang/test/CodeGenCXX/duplicate-mangled-name.cpp
+++ clang/test/CodeGenCXX/duplicate-mangled-name.cpp
@@ -38,9 +38,9 @@ namespace nm {
 
 float foo() {
   _ZN1TD1Ev();
-// CHECK: call void bitcast (void (%struct.T*)* @_ZN1TD1Ev to void ()*)()
+// CHECK: call void bitcast ({{.*}} (%struct.T*)* @_ZN1TD1Ev to void ()*)()
   T t;
-// CHECK: call void @_ZN1TD1Ev(%struct.T* %t)
+// CHECK: call {{.*}} @_ZN1TD1Ev(%struct.T* %t)
   return _ZN2nm3abcE + nm::abc;
 }
 
