06a9b6f5d183ef197cd387a840b577fd53803f8a
George Rimar <grimar@accesssoftek.com>
2016-01-15 18:21:34 +0000

    Recommit r257912 Added forgotten ELFDumper.cpp to commit.
    Initial commit message:
    [llvm-readobj] Add support for TLSDESC_PLT and TLSDESC_GOT dynamic section tags to the llvm-readobj.
    
    If module uses uses lazy TLSDESC relocations it should define DT_TLSDESC_PLT and DT_TLSDESC_GOT entries.
    They were unknown for llvm-readobj before this patch.
    
    Differential revision: http://reviews.llvm.org/D16224
    
    llvm-svn: 257914

--- llvm/test/tools/llvm-readobj/Inputs/dynamic-table.c
+++ llvm/test/tools/llvm-readobj/Inputs/dynamic-table.c
@@ -2,8 +2,13 @@
 //       -o dynamic-table-so.mips
 // clang -target mipsel-linux-gnu -lc dynamic-table.c \
 //       -o dynamic-table-exe.mips
+// clang -target aarch64-linux-gnu -fPIC -shared dynamic-table.c \ 
+//       -o dynamic-table-so.aarch64
 int puts(const char *);
+
+__thread int foo;
 
 int main(void) {
   puts("Hello, World");
+  foo = 0;
 }
