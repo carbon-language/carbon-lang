79db917139bb0314b5e53a18996d84c57eeeb2fb
Rafael Espindola <rafael.espindola@gmail.com>
2016-01-15 18:23:46 +0000

    Don't try to check all uses if lazy loading.
    This means that LTO_SYMBOL_SCOPE_DEFAULT_CAN_BE_HIDDEN will not be set
    in a few cases.
    
    This should have no impact in ld64 since it doesn't use lazy loading
    when merging modules and that is when it checks
    LTO_SYMBOL_SCOPE_DEFAULT_CAN_BE_HIDDEN.
    
    llvm-svn: 257915

--- llvm/lib/CodeGen/Analysis.cpp
+++ llvm/lib/CodeGen/Analysis.cpp
@@ -639,6 +639,11 @@ bool llvm::canBeOmittedFromSymbolTable(const GlobalValue *GV) {
   if (isa<GlobalAlias>(GV))
     return false;
 
+  // If we don't see every use, we have to be conservative and assume the value
+  // address is significant.
+  if (GV->getParent()->getMaterializer())
+    return false;
+
   GlobalStatus GS;
   if (GlobalStatus::analyzeGlobal(GV, GS))
     return false;
