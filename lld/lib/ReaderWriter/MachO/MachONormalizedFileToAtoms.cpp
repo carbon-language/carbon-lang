4b6bed98e20467b965e6f5e7260e8ff4ae877ccd
Pete Cooper <peter_cooper@apple.com>
2016-01-16 00:57:07 +0000

    Give error on binaries containing GC objc image infos.
    The image info struct contains flags for what kind of GC/retain/release is required.
    
    Give an error if we parse GC flags as these are unsupported.
    
    llvm-svn: 257974

--- lld/lib/ReaderWriter/MachO/MachONormalizedFileToAtoms.cpp
+++ lld/lib/ReaderWriter/MachO/MachONormalizedFileToAtoms.cpp
@@ -892,10 +892,12 @@ std::error_code parseObjCImageInfo(const NormalizedFile &normalizedFile,
   //		uint32_t	version;	// initially 0
   //		uint32_t	flags;
   //	};
-  // #define OBJC_IMAGE_SUPPORTS_GC   2
-  // #define OBJC_IMAGE_GC_ONLY       4
-  // #define OBJC_IMAGE_IS_SIMULATED  32
-  //
+  enum {
+    OBJC_IMAGE_SUPPORTS_GC=2,
+    OBJC_IMAGE_GC_ONLY=4,
+    OBJC_IMAGE_IS_SIMULATED=32,
+  };
+
   ArrayRef<uint8_t> content = imageInfoSection->content;
   if (content.size() != 8)
     return make_dynamic_error_code(imageInfoSection->segmentName + "/" +
@@ -912,6 +914,12 @@ std::error_code parseObjCImageInfo(const NormalizedFile &normalizedFile,
                                    " should have version=0");
 
   uint32_t flags = read32(content.data() + 4, isBig);
+  if (flags & (OBJC_IMAGE_SUPPORTS_GC|OBJC_IMAGE_GC_ONLY))
+    return make_dynamic_error_code(imageInfoSection->segmentName + "/" +
+                                   imageInfoSection->sectionName +
+                                   " in file " + file.path() +
+                                   " uses GC.  This is not supported");
+
   file.setSwiftVersion((flags >> 8) & 0xFF);
 
   return std::error_code();
