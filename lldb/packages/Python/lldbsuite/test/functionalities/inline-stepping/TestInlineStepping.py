db1958d1361247e9a7ef3a9fa74b3c2dbbebb44a
Pavel Labath <labath@google.com>
2016-01-20 12:17:26 +0000

    Enable TestInlineStepping on linux i386
    Test has been passing at least the last 200 buildbot runs.
    
    llvm-svn: 258305

--- lldb/packages/Python/lldbsuite/test/functionalities/inline-stepping/TestInlineStepping.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/inline-stepping/TestInlineStepping.py
@@ -16,8 +16,6 @@ class TestInlineStepping(TestBase):
     @add_test_categories(['pyapi'])
     @expectedFailureFreeBSD('llvm.org/pr17214')
     @expectedFailureIcc # Not really a bug.  ICC combines two inlined functions.
-    # failed 1/365 dosep runs, (i386-clang), TestInlineStepping.py:237 failed to stop at first breakpoint in main
-    @expectedFailureAll(oslist=["linux"], archs=["i386"])
     def test_with_python_api(self):
         """Test stepping over and into inlined functions."""
         self.build()
