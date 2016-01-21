61035fa3cb77245e181f6d198561f3454cfc0ca3
Vedant Kumar <vsk@apple.com>
2016-01-21 17:04:42 +0000

    [GCOV] Avoid emitting profile arcs for module and skeleton CUs
    Do not emit profile arc files and note files for module and skeleton
    CU's.
    
    Our users report seeing unexpected *.gcda and *.gcno files in their
    projects when using gcov-style profiling with modules or frameworks.
    The unwanted files come from these modules. This is not very helpful
    for end-users. Further, we've seen reports of instrumented programs
    crashing while writing these files out (due to I/O failures).
    
    rdar://problem/22838296
    
    Reviewed-by: aprantl
    
    Differential Revision: http://reviews.llvm.org/D15997
    
    llvm-svn: 258406

--- llvm/lib/Transforms/Instrumentation/GCOVProfiling.cpp
+++ llvm/lib/Transforms/Instrumentation/GCOVProfiling.cpp
@@ -494,6 +494,11 @@ void GCOVProfiler::emitProfileNotes() {
     // LTO, we'll generate the same .gcno files.
 
     auto *CU = cast<DICompileUnit>(CU_Nodes->getOperand(i));
+
+    // Skip module skeleton (and module) CUs.
+    if (CU->getDWOId())
+      continue;
+
     std::error_code EC;
     raw_fd_ostream out(mangleName(CU, "gcno"), EC, sys::fs::F_None);
     std::string EdgeDestinations;
@@ -853,6 +858,11 @@ Function *GCOVProfiler::insertCounterWriteout(
   if (CU_Nodes) {
     for (unsigned i = 0, e = CU_Nodes->getNumOperands(); i != e; ++i) {
       auto *CU = cast<DICompileUnit>(CU_Nodes->getOperand(i));
+
+      // Skip module skeleton (and module) CUs.
+      if (CU->getDWOId())
+        continue;
+
       std::string FilenameGcda = mangleName(CU, "gcda");
       uint32_t CfgChecksum = FileChecksums.empty() ? 0 : FileChecksums[i];
       Builder.CreateCall(StartFile,
