524afae807388f00a71220f3d62e3d732191a965
Adrian McCarthy <amccarth@google.com>
2016-01-21 23:07:48 +0000

    Fix TestImport.py to work with Python 3.5.
    Differential Revision: http://reviews.llvm.org/D16431
    
    llvm-svn: 258448

--- lldb/packages/Python/lldbsuite/test/functionalities/command_script/import/thepackage/__init__.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/command_script/import/thepackage/__init__.py
@@ -1,5 +1,7 @@
-import TPunitA
-import TPunitB
+from __future__ import absolute_import
+
+from . import TPunitA
+from . import TPunitB
 
 def __lldb_init_module(debugger,*args):
 	debugger.HandleCommand("command script add -f thepackage.TPunitA.command TPcommandA")
