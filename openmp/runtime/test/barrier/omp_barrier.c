24d4eba0f9027f787ae6fdff8d29132e7ed823d9
Andrey Churbanov <Andrey.Churbanov@intel.com>
2016-01-25 16:52:10 +0000

    omp_barrier.c test fixed in order to reliably and faster run on any number of processors
    llvm-svn: 258695

--- openmp/runtime/test/barrier/omp_barrier.c
+++ openmp/runtime/test/barrier/omp_barrier.c
@@ -15,7 +15,7 @@ int test_omp_barrier()
     int rank;
     rank = omp_get_thread_num ();
     if (rank ==1) {
-      my_sleep(SLEEPTIME);
+      my_sleep(((double)SLEEPTIME)/REPETITIONS); // give 1 sec to whole test
       result2 = 3;
     }
     #pragma omp barrier
@@ -31,10 +31,14 @@ int main()
   int i;
   int num_failed=0;
 
+#ifdef _OPENMP
+  omp_set_dynamic(0); // prevent runtime to change number of threads
+  omp_set_num_threads(4); // the test expects at least 3 threads
   for(i = 0; i < REPETITIONS; i++) {
     if(!test_omp_barrier()) {
       num_failed++;
     }
   }
+#endif
   return num_failed;
 }
