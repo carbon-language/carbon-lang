bbdf697a51e14d6a0f000569cb6c1a87bc172f07
Justin Lebar <jlebar@google.com>
2016-01-25 22:52:31 +0000

    [CUDA] Add -target to cuda-constructor-alias.cu test so it doesn't fail on Mac.
    The test was checking that we passed -mconstructor-alias to host
    compilation, but that explicitly shouldn't happen on Mac.
    
    llvm-svn: 258737

--- clang/test/Driver/cuda-constructor-alias.cu
+++ clang/test/Driver/cuda-constructor-alias.cu
@@ -5,7 +5,7 @@
 // Check that we don't pass -mconstructor-aliases to CUDA device-side
 // compilation, but we do pass it to host-side compilation.
 
-// RUN: %clang -### %s 2>&1 | FileCheck %s
+// RUN: %clang -### -target x86_64-linux-gnu %s 2>&1 | FileCheck %s
 // CHECK: "-cc1"
 // CHECK-NOT: "-fcuda-is-device" {{.*}}"-mconstructor-aliases"
 // CHECK-NOT: "-mconstructor-aliases" {{.*}}"-fcuda-is-device"
