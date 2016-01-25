cd2f6bbd5c12972328f006a07353995b269dad44
Justin Lebar <jlebar@google.com>
2016-01-25 22:36:37 +0000

    [CUDA] Don't generate aliases for static extern "C" functions.
    Summary:
    These aliases are done to support inline asm, but there's nothing we can
    do: NVPTX doesn't support aliases.
    
    Reviewers: tra
    
    Subscribers: cfe-commits, jhen, echristo
    
    Differential Revision: http://reviews.llvm.org/D16501
    
    llvm-svn: 258734

--- clang/lib/CodeGen/CodeGenModule.cpp
+++ clang/lib/CodeGen/CodeGenModule.cpp
@@ -3836,6 +3836,10 @@ static void EmitGlobalDeclMetadata(CodeGenModule &CGM,
 /// to such functions with an unmangled name from inline assembly within the
 /// same translation unit.
 void CodeGenModule::EmitStaticExternCAliases() {
+  // Don't do anything if we're generating CUDA device code -- the NVPTX
+  // assembly target doesn't support aliases.
+  if (Context.getTargetInfo().getTriple().isNVPTX())
+    return;
   for (auto &I : StaticExternCValues) {
     IdentifierInfo *Name = I.first;
     llvm::GlobalValue *Val = I.second;
