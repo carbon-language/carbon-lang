6fdfa3dc32cb883c268f75c789ae23e48a11bf64
Colin LeMahieu <colinl@codeaurora.org>
2016-02-01 18:15:39 +0000

    [NFC] Referencing manual for reason why subregbit is checked
    llvm-svn: 259380

--- llvm/lib/Target/Hexagon/Disassembler/HexagonDisassembler.cpp
+++ llvm/lib/Target/Hexagon/Disassembler/HexagonDisassembler.cpp
@@ -382,7 +382,8 @@ DecodeStatus HexagonDisassembler::getSingleInstruction(
       if (Producer >= Hexagon::W0 && Producer <= Hexagon::W15)
         Producer = ((Producer - Hexagon::W0) << 1) + SubregBit + Hexagon::V0;
       else if (SubregBit)
-        // Subreg bit should not be set for non-doublevector newvalue producers
+		// Hexagon PRM 10.11 New-value operands
+		// Nt[0] is reserved and should always be encoded as zero.
         return MCDisassembler::Fail;
       assert(Producer != Hexagon::NoRegister);
       MCO.setReg(Producer);
