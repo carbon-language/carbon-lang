fdcb3ceb75480872eed9d83b08f3c30166fc598f
Rui Ueyama <ruiu@google.com>
2016-02-02 20:24:33 +0000

    Do not use filename in a lit test.
    So that the file is move-safe.
    
    llvm-svn: 259555

--- lld/test/ELF/conflict.s
+++ lld/test/ELF/conflict.s
@@ -1,22 +1,22 @@
 # REQUIRES: x86
 
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
-# RUN: not ld.lld %t %t -o %t2 2>&1 | FileCheck -check-prefix=DEMANGLE %s
+# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t1.o
+# RUN: not ld.lld %t1.o %t1.o -o %t2 2>&1 | FileCheck -check-prefix=DEMANGLE %s
 
 # DEMANGLE:    duplicate symbol: {{mul\(double, double\)|_Z3muldd}} in
 # DEMANGLE:    duplicate symbol: foo in
 
-# RUN: not ld.lld %t %t -o %t2 --no-demangle 2>&1 | \
+# RUN: not ld.lld %t1.o %t1.o -o %t2 --no-demangle 2>&1 | \
 # RUN:   FileCheck -check-prefix=NO_DEMANGLE %s
 
 # NO_DEMANGLE: duplicate symbol: _Z3muldd in
 # NO_DEMANGLE: duplicate symbol: foo in
 
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %S/Inputs/conflict.s -o %t2
-# RUN: llvm-ar rcs %t.a %t2
-# RUN: not ld.lld %t %t.a -u baz -o %t2 2>&1 | FileCheck -check-prefix=ARCHIVE %s
+# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %S/Inputs/conflict.s -o %t2.o
+# RUN: llvm-ar rcs %t3.a %t2.o
+# RUN: not ld.lld %t1.o %t3.a -u baz -o %t2 2>&1 | FileCheck -check-prefix=ARCHIVE %s
 
-# ARCHIVE: duplicate symbol: foo in {{.*}}conflict.s.tmp and {{.*}}.a(conflict.s.tmp2)
+# ARCHIVE: duplicate symbol: foo in {{.*}}1.o and {{.*}}3.a({{.*}}2.o)
 
 .globl _Z3muldd, foo
 _Z3muldd:
