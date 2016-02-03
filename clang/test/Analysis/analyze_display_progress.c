0826bf2211c1064d2aac48c401539c1e719dda81
Yury Gribov <y.gribov@samsung.com>
2016-02-03 13:36:31 +0000

    Forgot to remove file in previous commit.
    llvm-svn: 259647

--- clang/test/Analysis/analyze_display_progress.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -analyze -analyzer-display-progress %s 2>&1 | FileCheck %s
-
-void f() {};
-void g() {};
-void h() {}
-
-// CHECK: analyze_display_progress.c f
-// CHECK: analyze_display_progress.c g
-// CHECK: analyze_display_progress.c h
