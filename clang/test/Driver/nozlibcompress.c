fe7c13343cff4b9e3f574c3edc9b02c50f3fa62d
Nico Weber <nicolasweber@gmx.de>
2016-02-07 21:32:17 +0000

    Make nozlibcompress.c pass and reenable it.
    llvm-svn: 260058

--- clang/test/Driver/nozlibcompress.c
+++ clang/test/Driver/nozlibcompress.c
@@ -1,10 +1,6 @@
 // RUN: %clang -c %s -Wa,--compress-debug-sections 2>&1 | FileCheck %s
-// RUN: %clang -c %s -Wa,--compress-debug-sections -Wa,--nocompress-debug-sections 2>&1 | FileCheck --check-prefix=NOWARN %s
+// RUN: %clang -c %s -Wa,--compress-debug-sections -Wa,--nocompress-debug-sections 2>&1 | FileCheck --allow-empty --check-prefix=NOWARN %s
 // REQUIRES: nozlib
-
-// FIXME: This test hasn't run until r259976 made REQUIRES: zlib work -- and
-// the test has been failing since. Figure out what's up and enable this.
-// XFAIL: *
 
 // CHECK: warning: cannot compress debug sections (zlib not installed)
 // NOWARN-NOT: warning: cannot compress debug sections (zlib not installed)
