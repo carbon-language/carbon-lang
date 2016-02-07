8a611a858b77d3be422f83c5cb04c754dffbfec5
Simon Atanasyan <simon@atanasyan.com>
2016-02-07 20:36:40 +0000

    [ELF][MIPS] Perform two checks using the single llvm-objdump / FileCheck run
    NFC
    
    llvm-svn: 260054

--- lld/test/ELF/mips-relocs.s
+++ lld/test/ELF/mips-relocs.s
@@ -2,14 +2,14 @@
 
 # RUN: llvm-mc -filetype=obj -triple=mips-unknown-linux %s -o %t-be.o
 # RUN: ld.lld -shared %t-be.o -o %t-be.so
-# RUN: llvm-objdump -t %t-be.so | FileCheck %s
-# RUN: llvm-objdump -s %t-be.so | FileCheck -check-prefix=BE %s
+# RUN: llvm-objdump -t -s %t-be.so \
+# RUN:   | FileCheck -check-prefix=SYM -check-prefix=BE %s
 # RUN: llvm-readobj -r -dynamic-table %t-be.so | FileCheck -check-prefix=REL %s
 
 # RUN: llvm-mc -filetype=obj -triple=mipsel-unknown-linux %s -o %t-el.o
 # RUN: ld.lld -shared %t-el.o -o %t-el.so
-# RUN: llvm-objdump -t %t-el.so | FileCheck %s
-# RUN: llvm-objdump -s %t-el.so | FileCheck -check-prefix=EL %s
+# RUN: llvm-objdump -t -s %t-el.so \
+# RUN:   | FileCheck -check-prefix=SYM -check-prefix=EL %s
 # RUN: llvm-readobj -r -dynamic-table %t-el.so | FileCheck -check-prefix=REL %s
 
 # REQUIRES: mips
@@ -31,10 +31,6 @@ v2:
   .word v2+4 # R_MIPS_32 target v2 addend 4
   .word v1   # R_MIPS_32 target v1 addend 0
 
-# CHECK: SYMBOL TABLE:
-# CHECK: 00030000 l       .data           00000004 v1
-# CHECK: 00030004 g       .data           00000008 v2
-
 # BE: Contents of section .data:
 # BE-NEXT: 30000 00000000 00030008 00030000
 #                         ^-- v2+4 ^-- v1
@@ -42,6 +38,10 @@ v2:
 # EL: Contents of section .data:
 # EL-NEXT: 30000 00000000 08000300 00000300
 #                         ^-- v2+4 ^-- v1
+
+# SYM: SYMBOL TABLE:
+# SYM: 00030000 l       .data           00000004 v1
+# SYM: 00030004 g       .data           00000008 v2
 
 # REL:      Relocations [
 # REL-NEXT:   Section (7) .rel.dyn {
