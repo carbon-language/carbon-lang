48e8d8bcdd4a31da5d2491196938c9121aef02e0
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2016-02-09 19:43:13 +0000

    clang-cl: Enable plugins on Windows
    llvm-svn: 260266

--- clang/test/Frontend/plugins.c
+++ clang/test/Frontend/plugins.c
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/PrintFunctionNames%pluginext -plugin print-fns %s 2>&1 | FileCheck %s
+// RUN: %clang_cl -Xclang -load -Xclang %llvmshlibdir/PrintFunctionNames%pluginext -Xclang -plugin -Xclang print-fns %s 2>&1 | FileCheck %s
 // REQUIRES: plugins, examples
 
 // CHECK: top-level-decl: "x"
