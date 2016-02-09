a951e8ece71b6393b602fd3d5f3538a264508673
Xinliang David Li <davidxl@google.com>
2016-02-09 20:02:59 +0000

    [PGO] Fix issue: explicitly defaulted assignop is not profiled
    Differential Revision: http://reviews.llvm.org/D16947
     
    
    llvm-svn: 260270

--- clang/lib/CodeGen/CGClass.cpp
+++ clang/lib/CodeGen/CGClass.cpp
@@ -1608,6 +1608,7 @@ void CodeGenFunction::emitImplicitAssignmentOperatorBody(FunctionArgList &Args) 
 
   LexicalScope Scope(*this, RootCS->getSourceRange());
 
+  incrementProfileCounter(RootCS);
   AssignmentMemcpyizer AM(*this, AssignOp, Args);
   for (auto *I : RootCS->body())
     AM.emitAssignment(I);
