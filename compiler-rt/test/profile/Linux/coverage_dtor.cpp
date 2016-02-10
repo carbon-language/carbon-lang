00ec96b5a00d277df56f011a93139ebf4d571df4
Xinliang David Li <davidxl@google.com>
2016-02-10 18:38:27 +0000

    Rollback 260394 -- 32bit test needs more config support
    llvm-svn: 260402

--- compiler-rt/test/profile/Linux/coverage_dtor.cpp
+++ compiler-rt/test/profile/Linux/coverage_dtor.cpp
@@ -2,11 +2,6 @@
 // RUN: env LLVM_PROFILE_FILE=%t.profraw %run %t
 // RUN: llvm-profdata merge -o %t.profdata %t.profraw
 // RUN: llvm-cov show %t -instr-profile %t.profdata -filename-equivalence 2>&1 | FileCheck %s
-// 32 bit testing
-// RUN: %clang -x c++ -fno-exceptions  -std=c++11 -fuse-ld=gold -fprofile-instr-generate -fcoverage-mapping -m32 -o %t.32 %s
-// RUN: env LLVM_PROFILE_FILE=%t.32.profraw %run %t.32
-// RUN: llvm-profdata merge -o %t.32.profdata %t.32.profraw
-// RUN: llvm-cov show %t.32 -instr-profile %t.32.profdata -filename-equivalence 2>&1 | FileCheck %s
 
 int g = 100;
 struct Base {
--- compiler-rt/test/profile/Linux/coverage_test.cpp
+++ compiler-rt/test/profile/Linux/coverage_test.cpp
@@ -2,16 +2,11 @@
 // RUN: env LLVM_PROFILE_FILE=%t.profraw %run %t
 // RUN: llvm-profdata merge -o %t.profdata %t.profraw
 // RUN: llvm-cov show %t -instr-profile %t.profdata -filename-equivalence 2>&1 | FileCheck %s
-// Testing PIE
+//
 // RUN: %clang_profgen -fuse-ld=gold -O2 -fdata-sections -ffunction-sections -fPIE -pie -fprofile-instr-generate -fcoverage-mapping -Wl,--gc-sections -o %t.pie %s
 // RUN: env LLVM_PROFILE_FILE=%t.pie.profraw %run %t.pie
 // RUN: llvm-profdata merge -o %t.pie.profdata %t.pie.profraw
 // RUN: llvm-cov show %t.pie -instr-profile %t.pie.profdata -filename-equivalence 2>&1 | FileCheck %s
-// testing m32
-// RUN: %clang_profgen -fuse-ld=gold -O2 -fdata-sections -ffunction-sections -fPIE -pie -fprofile-instr-generate -fcoverage-mapping -m32 -Wl,--gc-sections -o %t.32.pie %s
-// RUN: env LLVM_PROFILE_FILE=%t.32.pie.profraw %run %t.32.pie
-// RUN: llvm-profdata merge -o %t.32.pie.profdata %t.32.pie.profraw
-// RUN: llvm-cov show %t.32.pie -instr-profile %t.32.pie.profdata -filename-equivalence 2>&1 | FileCheck %s
 
 void foo(bool cond) { // CHECK:  1| [[@LINE]]|void foo(
   if (cond) {         // CHECK:  1| [[@LINE]]|  if (cond) {
