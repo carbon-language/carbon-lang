031bbd062ba165b22902f0d996605c5b4abcb92e
Reid Kleckner <rnk@google.com>
2016-02-11 02:02:56 +0000

    Un-XFAIL a passing test on Windows
    llvm-svn: 260481

--- compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
+++ compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
@@ -1,6 +1,6 @@
 // RUN: %clangxx_asan -coverage -O0 %s -o %t
 // RUN: %env_asan_opts=check_initialization_order=1 %run %t 2>&1 | FileCheck %s
-// XFAIL: android,win32
+// XFAIL: android
 #include <stdio.h>
 int foo() { return 1; }
 int XXX = foo();
