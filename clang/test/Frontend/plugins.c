ba7d0fe280691b2eb0a0e7183a588136a53de462
NAKAMURA Takumi <geek4civic@gmail.com>
2016-02-11 16:43:08 +0000

    Revert r260266 (and r260276), "clang-cl: Enable plugins on Windows"
    It doesn't work, at least, i686-win32.
    
    llvm-svn: 260537

--- clang/test/Frontend/plugins.c
+++ clang/test/Frontend/plugins.c
@@ -1,5 +1,4 @@
 // RUN: %clang_cc1 -load %llvmshlibdir/PrintFunctionNames%pluginext -plugin print-fns %s 2>&1 | FileCheck %s
-// RUN: %clang_cl -c -Xclang -load -Xclang %llvmshlibdir/PrintFunctionNames%pluginext -Xclang -plugin -Xclang print-fns -Tc %s 2>&1 | FileCheck %s
 // REQUIRES: plugins, examples
 
 // CHECK: top-level-decl: "x"
