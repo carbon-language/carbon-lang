6615654f565c3a19f1f8a6e74ccdc723fd0c71d3
Craig Topper <craig.topper@gmail.com>
2016-02-16 04:24:58 +0000

    [TableGen] Fix inconsistent spacing. NFC
    llvm-svn: 260935

--- llvm/utils/TableGen/X86RecognizableInstr.cpp
+++ llvm/utils/TableGen/X86RecognizableInstr.cpp
@@ -799,8 +799,8 @@ void RecognizableInstr::emitInstructionSpecifier() {
   case X86Local::MRM_F1: case X86Local::MRM_F2: case X86Local::MRM_F3:
   case X86Local::MRM_F4: case X86Local::MRM_F5: case X86Local::MRM_F6:
   case X86Local::MRM_F7: case X86Local::MRM_F9: case X86Local::MRM_FA:
-  case X86Local::MRM_FB:  case X86Local::MRM_FC: case X86Local::MRM_FD:
-  case X86Local::MRM_FE:  case X86Local::MRM_FF:
+  case X86Local::MRM_FB: case X86Local::MRM_FC: case X86Local::MRM_FD:
+  case X86Local::MRM_FE: case X86Local::MRM_FF:
     // Ignored.
     break;
   }
