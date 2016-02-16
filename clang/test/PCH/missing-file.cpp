b75dd0c9a936112d898523291834482585921b03
Reid Kleckner <rnk@google.com>
2016-02-16 19:16:28 +0000

    Exclude PCH/missing-file.cpp on Windows, it does not pass reliably
    Tag the test with "REQUIRES: can-remove-opened-file", which is what we
    use for the similar test Modules/explicit-build-missing-file.cpp.
    
    llvm-svn: 260994

--- clang/test/PCH/missing-file.cpp
+++ clang/test/PCH/missing-file.cpp
@@ -4,15 +4,14 @@
 // RUN: echo 'struct S{char c; int i; }; void foo() {}' > %t.h
 // RUN: echo 'template <typename T> void tf() { T::foo(); }' >> %t.h
 // RUN: %clang_cc1 -x c++ -emit-pch -o %t.h.pch %t.h
-
-// %t.h might be touched by scanners as a hot file on Windows,
-// to fail to remove %.h with single run.
-// FIXME: Do we really want to work around bugs in virus checkers here?
-// RUN: rm %t.h || rm %t.h || rm %t.h
+// RUN: rm %t.h
 
 // Check diagnostic with location in original source:
 // RUN: not %clang_cc1 -include-pch %t.h.pch -emit-obj -o %t.o %s 2> %t.stderr
 // RUN: grep 'could not find file' %t.stderr
+
+// Oftentimes on Windows there are open handles, and deletion will fail.
+// REQUIRES: can-remove-opened-file
 
 void qq(S*) {}
 
