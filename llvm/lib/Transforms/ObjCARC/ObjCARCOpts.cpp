009d60650dd52219d579a4ec35db892cb7692217
Frederic Riss <friss@apple.com>
2016-02-17 18:51:27 +0000

    [ObjCARC] Handle ARCInstKind::ClaimRV in OptimizeIndividualCalls.
    When support for objc_unsafeClaimAutoreleasedReturnValue has been added to the
    ARC optimizer in r258970, one case was missed which would lead the optimizer
    to execute an llvm_unreachable. In this case, just handle ClaimRV in the same
    way we handle RetainRV.
    
    llvm-svn: 261134

--- llvm/lib/Transforms/ObjCARC/ObjCARCOpts.cpp
+++ llvm/lib/Transforms/ObjCARC/ObjCARCOpts.cpp
@@ -889,6 +889,7 @@ void ObjCARCOpt::OptimizeIndividualCalls(Function &F) {
                            Inst->getParent(), Inst,
                            DependingInstructions, Visited, PA);
           break;
+        case ARCInstKind::ClaimRV:
         case ARCInstKind::RetainRV:
         case ARCInstKind::AutoreleaseRV:
           // Don't move these; the RV optimization depends on the autoreleaseRV
