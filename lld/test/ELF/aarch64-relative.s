c36e97ae3977f4f1cc595daac98ff3f845979b13
Rafael Espindola <rafael.espindola@gmail.com>
2016-02-24 19:36:30 +0000

    R_AARCH64_ADR_PREL_PG_HI21 is relative.
    llvm-svn: 261764

--- lld/test/ELF/aarch64-relative.s
+++ lld/test/ELF/aarch64-relative.s
@@ -2,8 +2,11 @@
 // RUN: llvm-mc -filetype=obj -triple=aarch64-unknown-freebsd %s -o %t.o
 // RUN: ld.lld %t.o -o %t.so -shared
 // RUN: llvm-readobj -r %t.so | FileCheck %s
+        adrp    x8, .Lfoo
 
+        .data
         .Lfoo:
+
         .rodata
         .long .Lfoo - .
 
