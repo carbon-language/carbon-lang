161a158e5c497a54ea0ba588d242fa98e7c0a3e8
Nikolay Haustov <Nikolay.Haustov@amd.com>
2016-02-25 16:09:14 +0000

    [AMDGPU] Disassembler: Support for all VOP1 instructions.
    Support all instructions with VOP1 encoding with 32 or 64-bit operands for VI subtarget:
    
    VGPR_32 and VReg_64 operand register classes
    VS_32 and VS_64 operand register classes with inline and literal constants
    Tests for VOP1 instructions.
    
    Patch by: skolton
    
    Reviewers: arsenm, tstellarAMD
    
    Review: http://reviews.llvm.org/D17194
    llvm-svn: 261878

--- llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.cpp
+++ llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.cpp
@@ -39,66 +39,67 @@ typedef llvm::MCDisassembler::DecodeStatus DecodeStatus;
 
 static DecodeStatus DecodeVGPR_32RegisterClass(MCInst &Inst, unsigned Imm,
                                                uint64_t Addr, const void *Decoder) {
-  const AMDGPUDisassembler *Dis = 
+  const AMDGPUDisassembler *Dis =
     static_cast<const AMDGPUDisassembler *>(Decoder);
   return Dis->DecodeVGPR_32RegisterClass(Inst, Imm, Addr);
 }
 
 static DecodeStatus DecodeVS_32RegisterClass(MCInst &Inst, unsigned Imm,
                                              uint64_t Addr, const void *Decoder) {
-  const AMDGPUDisassembler *Dis = 
+  const AMDGPUDisassembler *Dis =
     static_cast<const AMDGPUDisassembler *>(Decoder);
   return Dis->DecodeVS_32RegisterClass(Inst, Imm, Addr);
 }
 
-static DecodeStatus DecodeVS_64RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeVS_64RegisterClass(MCInst &Inst, unsigned Imm,
                                              uint64_t Addr, const void *Decoder) {
-  // ToDo
-  return MCDisassembler::Fail;
+  const AMDGPUDisassembler *Dis =
+    static_cast<const AMDGPUDisassembler *>(Decoder);
+  return Dis->DecodeVS_64RegisterClass(Inst, Imm, Addr);
 }
 
-static DecodeStatus DecodeVReg_64RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeVReg_64RegisterClass(MCInst &Inst, unsigned Imm,
+                                               uint64_t Addr, const void *Decoder) {
+  const AMDGPUDisassembler *Dis =
+    static_cast<const AMDGPUDisassembler *>(Decoder);
+  return Dis->DecodeVReg_64RegisterClass(Inst, Imm, Addr);
+}
+
+static DecodeStatus DecodeVReg_96RegisterClass(MCInst &Inst, unsigned Imm,
                                                uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
 
-static DecodeStatus DecodeVReg_96RegisterClass(MCInst &Inst, unsigned Imm, 
-                                               uint64_t Addr, const void *Decoder) {
-  // ToDo
-  return MCDisassembler::Fail;
-}
-
-static DecodeStatus DecodeVReg_128RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeVReg_128RegisterClass(MCInst &Inst, unsigned Imm,
                                                 uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
 
-static DecodeStatus DecodeSReg_32RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeSReg_32RegisterClass(MCInst &Inst, unsigned Imm,
                                                uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
 
-static DecodeStatus DecodeSReg_64RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeSReg_64RegisterClass(MCInst &Inst, unsigned Imm,
                                                uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
 
-static DecodeStatus DecodeSReg_128RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeSReg_128RegisterClass(MCInst &Inst, unsigned Imm,
                                                 uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
 
-static DecodeStatus DecodeSReg_256RegisterClass(MCInst &Inst, unsigned Imm, 
+static DecodeStatus DecodeSReg_256RegisterClass(MCInst &Inst, unsigned Imm,
                                                 uint64_t Addr, const void *Decoder) {
   // ToDo
   return MCDisassembler::Fail;
 }
-
 
 #define GET_SUBTARGETINFO_ENUM
 #include "AMDGPUGenSubtargetInfo.inc"
@@ -112,13 +113,16 @@ static DecodeStatus DecodeSReg_256RegisterClass(MCInst &Inst, unsigned Imm,
 
 DecodeStatus AMDGPUDisassembler::getInstruction(MCInst &MI, uint64_t &Size,
                                                 ArrayRef<uint8_t> Bytes,
-                                                uint64_t Address, 
+                                                uint64_t Address,
                                                 raw_ostream &WS,
                                                 raw_ostream &CS) const {
   CommentStream = &CS;
 
   // ToDo: AMDGPUDisassembler supports only VI ISA.
   assert(AMDGPU::isVI(STI) && "Can disassemble only VI ISA.");
+
+  HasLiteral = false;
+  this->Bytes = Bytes;
 
   // Try decode 32-bit instruction
   if (Bytes.size() < 4) {
@@ -135,14 +139,19 @@ DecodeStatus AMDGPUDisassembler::getInstruction(MCInst &MI, uint64_t &Size,
       Size = 0;
       return MCDisassembler::Fail;
   }
-  Size = 4;
+  if (HasLiteral == true) {
+    Size = 8;
+    HasLiteral = false;
+  } else {
+    Size = 4;
+  }
 
   return MCDisassembler::Success;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeLitFloat(unsigned Imm, uint32_t& F) const {
+DecodeStatus AMDGPUDisassembler::DecodeImmedFloat(unsigned Imm, uint32_t &F) const {
   // ToDo: case 248: 1/(2*PI) - is allowed only on VI
-  // ToDo: AMDGPUInstPrinter does not support 1/(2*PI). It consider 1/(2*PI) as 
+  // ToDo: AMDGPUInstPrinter does not support 1/(2*PI). It consider 1/(2*PI) as
   // literal constant.
   switch(Imm) {
   case 240: F = FloatToBits(0.5f); return MCDisassembler::Success;
@@ -158,8 +167,23 @@ DecodeStatus AMDGPUDisassembler::DecodeLitFloat(unsigned Imm, uint32_t& F) const
   }
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeLitInteger(unsigned Imm, 
-                                                  int64_t& I) const {
+DecodeStatus AMDGPUDisassembler::DecodeImmedDouble(unsigned Imm, uint64_t &D) const {
+  switch(Imm) {
+  case 240: D = DoubleToBits(0.5); return MCDisassembler::Success;
+  case 241: D = DoubleToBits(-0.5); return MCDisassembler::Success;
+  case 242: D = DoubleToBits(1.0); return MCDisassembler::Success;
+  case 243: D = DoubleToBits(-1.0); return MCDisassembler::Success;
+  case 244: D = DoubleToBits(2.0); return MCDisassembler::Success;
+  case 245: D = DoubleToBits(-2.0); return MCDisassembler::Success;
+  case 246: D = DoubleToBits(4.0); return MCDisassembler::Success;
+  case 247: D = DoubleToBits(-4.0); return MCDisassembler::Success;
+  case 248: D = 0x3fc45f306dc9c882; return MCDisassembler::Success; // 1/(2*PI)
+  default: return MCDisassembler::Fail;
+  }
+}
+
+DecodeStatus AMDGPUDisassembler::DecodeImmedInteger(unsigned Imm,
+                                                    int64_t &I) const {
   if ((Imm >= 128) && (Imm <= 192)) {
     I = Imm - 128;
     return MCDisassembler::Success;
@@ -170,33 +194,91 @@ DecodeStatus AMDGPUDisassembler::DecodeLitInteger(unsigned Imm,
   return MCDisassembler::Fail;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeVgprRegister(unsigned Val, 
-                                                    unsigned& RegID) const {
-  if (Val > 255) {
+DecodeStatus AMDGPUDisassembler::DecodeVgprRegister(unsigned Val,
+                                                    unsigned &RegID,
+                                                    unsigned Size) const {
+  if (Val > (256 - Size / 32)) {
     return MCDisassembler::Fail;
   }
-  RegID = AMDGPUMCRegisterClasses[AMDGPU::VGPR_32RegClassID].getRegister(Val);
+  unsigned RegClassID;
+  switch (Size) {
+  case 32: RegClassID = AMDGPU::VGPR_32RegClassID; break;
+  case 64: RegClassID = AMDGPU::VReg_64RegClassID; break;
+  case 96: RegClassID = AMDGPU::VReg_96RegClassID; break;
+  case 128: RegClassID = AMDGPU::VReg_128RegClassID; break;
+  case 256: RegClassID = AMDGPU::VReg_256RegClassID; break;
+  case 512: RegClassID = AMDGPU::VReg_512RegClassID; break;
+  default:
+    return MCDisassembler::Fail;
+  }
+
+  RegID = AMDGPUMCRegisterClasses[RegClassID].getRegister(Val);
   return MCDisassembler::Success;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeSgprRegister(unsigned Val, 
-                                                    unsigned& RegID) const {
+DecodeStatus AMDGPUDisassembler::DecodeSgprRegister(unsigned Val,
+                                                    unsigned &RegID,
+                                                    unsigned Size) const {
   // ToDo: SI/CI have 104 SGPRs, VI - 102
-  if (Val > 101) {
+  unsigned RegClassID;
+
+  switch (Size) {
+  case 32:
+    if (Val > 101) {
+      return MCDisassembler::Fail;
+    }
+    RegClassID = AMDGPU::SGPR_32RegClassID;
+    break;
+  case 64:
+    if ((Val % 2 != 0) || (Val > 100)) {
+      return MCDisassembler::Fail;
+    }
+    Val /= 2;
+    RegClassID = AMDGPU::SGPR_64RegClassID;
+    break;
+  case 128:
+    // ToDo: unclear if s[100:104] is available on VI. Can we use VCC as SGPR in
+    // this bundle?
+    if ((Val % 4 != 0) || (Val > 96)) {
+      return MCDisassembler::Fail;
+    }
+    Val /= 4;
+    RegClassID = AMDGPU::SReg_128RegClassID;
+    break;
+  case 256:
+    // ToDo: unclear if s[96:104] is available on VI. Can we use VCC as SGPR in
+    // this bundle?
+    if ((Val % 4 != 0) || (Val > 92)) {
+      return MCDisassembler::Fail;
+    }
+    Val /= 4;
+    RegClassID = AMDGPU::SReg_256RegClassID;
+    break;
+  case 512:
+    // ToDo: unclear if s[88:104] is available on VI. Can we use VCC as SGPR in
+    // this bundle?
+    if ((Val % 4 != 0) || (Val > 84)) {
+      return MCDisassembler::Fail;
+    }
+    Val /= 4;
+    RegClassID = AMDGPU::SReg_512RegClassID;
+    break;
+  default:
     return MCDisassembler::Fail;
   }
-  RegID = AMDGPUMCRegisterClasses[AMDGPU::SGPR_32RegClassID].getRegister(Val);
+
+  RegID = AMDGPUMCRegisterClasses[RegClassID].getRegister(Val);
   return MCDisassembler::Success;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeSrcRegister(unsigned Val, 
-                                                   unsigned& RegID) const {
-  // ToDo: deal with out-of range registers  
+DecodeStatus AMDGPUDisassembler::DecodeSrc32Register(unsigned Val,
+                                                     unsigned &RegID) const {
+  // ToDo: deal with out-of range registers
   using namespace AMDGPU;
   if (Val <= 101) {
-    return DecodeSgprRegister(Val, RegID);
+    return DecodeSgprRegister(Val, RegID, 32);
   } else if ((Val >= 256) && (Val <= 511)) {
-    return DecodeVgprRegister(Val - 256, RegID);
+    return DecodeVgprRegister(Val - 256, RegID, 32);
   } else {
     switch(Val) {
     case 102: RegID = getMCReg(FLAT_SCR_LO, STI); return MCDisassembler::Success;
@@ -239,8 +321,40 @@ DecodeStatus AMDGPUDisassembler::DecodeSrcRegister(unsigned Val,
   return MCDisassembler::Fail;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeVGPR_32RegisterClass(llvm::MCInst &Inst, 
-                                                            unsigned Imm, 
+DecodeStatus AMDGPUDisassembler::DecodeSrc64Register(unsigned Val,
+                                                     unsigned &RegID) const {
+  // ToDo: deal with out-of range registers
+  using namespace AMDGPU;
+  if (Val <= 101) {
+    return DecodeSgprRegister(Val, RegID, 64);
+  } else if ((Val >= 256) && (Val <= 511)) {
+    return DecodeVgprRegister(Val - 256, RegID, 64);
+  } else {
+    switch(Val) {
+    case 102: RegID = getMCReg(FLAT_SCR, STI); return MCDisassembler::Success;
+    case 106: RegID = getMCReg(VCC, STI); return MCDisassembler::Success;
+    case 126: RegID = getMCReg(EXEC, STI); return MCDisassembler::Success;
+    default: return MCDisassembler::Fail;
+    }
+  }
+  return MCDisassembler::Fail;
+}
+
+DecodeStatus AMDGPUDisassembler::DecodeLiteralConstant(MCInst &Inst,
+                                                       uint64_t &Literal) const {
+  // For now all literal constants are supposed to be unsigned integer
+  // ToDo: deal with signed/unsigned 64-bit integer constants
+  // ToDo: deal with float/double constants
+  if (Bytes.size() < 8) {
+    return MCDisassembler::Fail;
+  }
+  Literal =
+    0 | (Bytes[7] << 24) | (Bytes[6] << 16) | (Bytes[5] << 8) | (Bytes[4] << 0);
+  return MCDisassembler::Success;
+}
+
+DecodeStatus AMDGPUDisassembler::DecodeVGPR_32RegisterClass(llvm::MCInst &Inst,
+                                                            unsigned Imm,
                                                             uint64_t Addr) const {
   unsigned RegID;
   if (DecodeVgprRegister(Imm, RegID) == MCDisassembler::Success) {
@@ -250,21 +364,30 @@ DecodeStatus AMDGPUDisassembler::DecodeVGPR_32RegisterClass(llvm::MCInst &Inst,
   return MCDisassembler::Fail;
 }
 
-DecodeStatus AMDGPUDisassembler::DecodeVS_32RegisterClass(MCInst &Inst, 
-                                                          unsigned Imm, 
-                                                          uint64_t Addr) const {
-  // ToDo: different opcodes allow different formats og this operands
+DecodeStatus AMDGPUDisassembler::DecodeVSRegisterClass(MCInst &Inst,
+                                                       unsigned Imm,
+                                                       uint64_t Addr,
+                                                       bool Is32) const {
+  // ToDo: different opcodes allow different formats of this operands
   if ((Imm >= 128) && (Imm <= 208)) {
     // immediate integer
     int64_t Val;
-    if (DecodeLitInteger(Imm, Val) == MCDisassembler::Success) {
+    if (DecodeImmedInteger(Imm, Val) == MCDisassembler::Success) {
       Inst.addOperand(MCOperand::createImm(Val));
       return MCDisassembler::Success;
     }
   } else if ((Imm >= 240) && (Imm <= 248)) {
-    // immediate float
-    uint32_t Val;
-    if (DecodeLitFloat(Imm, Val) == MCDisassembler::Success) {
+    // immediate float/double
+    uint64_t Val;
+    DecodeStatus status;
+    if (Is32) {
+      uint32_t Val32;
+      status = DecodeImmedFloat(Imm, Val32);
+      Val = static_cast<uint64_t>(Val32);
+    } else {
+      status = DecodeImmedDouble(Imm, Val);
+    }
+    if (status == MCDisassembler::Success) {
       Inst.addOperand(MCOperand::createImm(Val));
       return MCDisassembler::Success;
     }
@@ -273,23 +396,57 @@ DecodeStatus AMDGPUDisassembler::DecodeVS_32RegisterClass(MCInst &Inst,
     // ToDo: implement LDS direct read
   } else if (Imm == 255) {
     // literal constant
-  } else if ((Imm == 125) || 
-             ((Imm >= 209) && (Imm <= 239)) || 
-             (Imm == 249) || 
-             (Imm == 250) || 
+    HasLiteral = true;
+    uint64_t Literal;
+    if (DecodeLiteralConstant(Inst, Literal) == MCDisassembler::Success) {
+      Inst.addOperand(MCOperand::createImm(Literal));
+      return MCDisassembler::Success;
+    }
+    return MCDisassembler::Fail;
+  } else if ((Imm == 125) ||
+             ((Imm >= 209) && (Imm <= 239)) ||
+             (Imm == 249) ||
+             (Imm == 250) ||
              (Imm >= 512)) {
     // reserved
     return MCDisassembler::Fail;
   } else {
     // register
     unsigned RegID;
-    if (DecodeSrcRegister(Imm, RegID) == MCDisassembler::Success) {
+    DecodeStatus status = Is32 ? DecodeSrc32Register(Imm, RegID)
+                               : DecodeSrc64Register(Imm, RegID);
+    if (status == MCDisassembler::Success) {
       Inst.addOperand(MCOperand::createReg(RegID));
       return MCDisassembler::Success;
     }
   }
   return MCDisassembler::Fail;
 }
+
+DecodeStatus AMDGPUDisassembler::DecodeVS_32RegisterClass(MCInst &Inst,
+                                                          unsigned Imm,
+                                                          uint64_t Addr) const {
+  return DecodeVSRegisterClass(Inst, Imm, Addr, true);
+}
+
+DecodeStatus AMDGPUDisassembler::DecodeVS_64RegisterClass(MCInst &Inst,
+                                                          unsigned Imm,
+                                                          uint64_t Addr) const {
+  return DecodeVSRegisterClass(Inst, Imm, Addr, false);
+}
+
+DecodeStatus AMDGPUDisassembler::DecodeVReg_64RegisterClass(llvm::MCInst &Inst,
+                                                            unsigned Imm,
+                                                            uint64_t Addr) const {
+  unsigned RegID;
+  if (DecodeVgprRegister(Imm, RegID, 64) == MCDisassembler::Success) {
+    Inst.addOperand(MCOperand::createReg(RegID));
+    return MCDisassembler::Success;
+  }
+  return MCDisassembler::Fail;
+}
+
+
 
 static MCDisassembler *createAMDGPUDisassembler(const Target &T,
                                                 const MCSubtargetInfo &STI,
--- llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.h
+++ llvm/lib/Target/AMDGPU/Disassembler/AMDGPUDisassembler.h
@@ -25,9 +25,14 @@ namespace llvm {
   class MCSubtargetInfo;
 
   class AMDGPUDisassembler : public MCDisassembler {
+  private:
+    /// true if 32-bit literal constant is placed after instruction
+    mutable bool HasLiteral;
+    mutable ArrayRef<uint8_t> Bytes;
+
   public:
     AMDGPUDisassembler(const MCSubtargetInfo &STI, MCContext &Ctx) :
-      MCDisassembler(STI, Ctx) {}
+      MCDisassembler(STI, Ctx), HasLiteral(false) {}
 
     ~AMDGPUDisassembler() {}
 
@@ -35,23 +40,41 @@ namespace llvm {
                                 ArrayRef<uint8_t> Bytes, uint64_t Address,
                                 raw_ostream &WS, raw_ostream &CS) const override;
 
-    /// Decode inline float value in VSrc field
-    DecodeStatus DecodeLitFloat(unsigned Imm, uint32_t& F) const;
-    /// Decode inline integer value in VSrc field
-    DecodeStatus DecodeLitInteger(unsigned Imm, int64_t& I) const;
+    /// Decode inline float value in SRC field
+    DecodeStatus DecodeImmedFloat(unsigned Imm, uint32_t &F) const;
+    /// Decode inline double value in SRC field
+    DecodeStatus DecodeImmedDouble(unsigned Imm, uint64_t &D) const;
+    /// Decode inline integer value in SRC field
+    DecodeStatus DecodeImmedInteger(unsigned Imm, int64_t &I) const;
     /// Decode VGPR register
-    DecodeStatus DecodeVgprRegister(unsigned Val, unsigned& RegID) const;
+    DecodeStatus DecodeVgprRegister(unsigned Val, unsigned &RegID,
+                                    unsigned Size = 32) const;
     /// Decode SGPR register
-    DecodeStatus DecodeSgprRegister(unsigned Val, unsigned& RegID) const;
-    /// Decode register in VSrc field
-    DecodeStatus DecodeSrcRegister(unsigned Val, unsigned& RegID) const;
+    DecodeStatus DecodeSgprRegister(unsigned Val, unsigned &RegID,
+                                    unsigned Size = 32) const;
+    /// Decode 32-bit register in SRC field
+    DecodeStatus DecodeSrc32Register(unsigned Val, unsigned &RegID) const;
+    /// Decode 64-bit register in SRC field
+    DecodeStatus DecodeSrc64Register(unsigned Val, unsigned &RegID) const;
 
-    DecodeStatus DecodeVS_32RegisterClass(MCInst &Inst, unsigned Imm, 
+    /// Decode literal constant after instruction
+    DecodeStatus DecodeLiteralConstant(MCInst &Inst, uint64_t &Literal) const;
+
+    DecodeStatus DecodeVGPR_32RegisterClass(MCInst &Inst, unsigned Imm,
+                                            uint64_t Addr) const;
+
+    DecodeStatus DecodeVSRegisterClass(MCInst &Inst, unsigned Imm,
+                                       uint64_t Addr, bool Is32) const;
+
+    DecodeStatus DecodeVS_32RegisterClass(MCInst &Inst, unsigned Imm,
                                           uint64_t Addr) const;
 
-    DecodeStatus DecodeVGPR_32RegisterClass(MCInst &Inst, unsigned Imm, 
+    DecodeStatus DecodeVS_64RegisterClass(MCInst &Inst, unsigned Imm,
+                                          uint64_t Addr) const;
+
+    DecodeStatus DecodeVReg_64RegisterClass(MCInst &Inst, unsigned Imm,
                                             uint64_t Addr) const;
   };
 } // namespace llvm
 
-#endif //LLVM_LIB_TARGET_AMDGPU_DISASSEMBLER_AMDGPUDISASSEMBLER_H
+#endif // LLVM_LIB_TARGET_AMDGPU_DISASSEMBLER_AMDGPUDISASSEMBLER_H
