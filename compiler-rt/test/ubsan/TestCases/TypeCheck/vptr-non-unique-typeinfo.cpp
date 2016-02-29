1456e2581a9330f18acc1783369bee0dc650cfbd
Reid Kleckner <rnk@google.com>
2016-02-29 20:38:35 +0000

    Avoid use of -fsanitize=vptr in MSVC env, it is not yet supported
    llvm-svn: 262251

--- compiler-rt/test/ubsan/TestCases/TypeCheck/vptr-non-unique-typeinfo.cpp
+++ compiler-rt/test/ubsan/TestCases/TypeCheck/vptr-non-unique-typeinfo.cpp
@@ -1,6 +1,8 @@
 // RUN: %clangxx -frtti -fsanitize=vptr -fno-sanitize-recover=vptr -I%p/Helpers %p/Helpers/vptr-non-unique-typeinfo-lib.cpp -fPIC -shared -o %t-lib.so
 // RUN: %clangxx -frtti -fsanitize=vptr -fno-sanitize-recover=vptr -I%p/Helpers -g %s -O3 -o %t %t-lib.so
 // RUN: %run %t
+//
+// REQUIRES: cxxabi
 
 #include "vptr-non-unique-typeinfo-lib.h"
 
