1bf6bafb2d47867d19f5b9e666e3195b776c0bf7
Michael Kruse <llvm@meinersbur.de>
2016-03-02 14:59:16 +0000

    Fix: Add pass manager barrier.
    The LNT test suite with -polly-process-unprofitable
    -polly-position=before-vectorizer currenty fails 59 tests. With this
    barrier added, only 16 keep failing. This is probably because Polly's
    code generation currently does not correctly preserve all analyses it
    promised to preserve. Temporarily add this barrier until further
    investigation.
    
    llvm-svn: 262488

--- polly/lib/Support/RegisterPasses.cpp
+++ polly/lib/Support/RegisterPasses.cpp
@@ -30,6 +30,7 @@
 #include "polly/ScopInfo.h"
 #include "llvm/Analysis/CFGPrinter.h"
 #include "llvm/IR/LegacyPassManager.h"
+#include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Vectorize.h"
@@ -225,6 +226,11 @@ void registerPollyPasses(llvm::legacy::PassManagerBase &PM) {
   case CODEGEN_NONE:
     break;
   }
+
+  // FIXME: This dummy ModulePass keeps some programs from miscompiling,
+  // probably some not correctly preserved analyses. It acts as a barrier to
+  // force all analysis results to be recomputed.
+  PM.add(createBarrierNoopPass());
 
   if (CFGPrinter)
     PM.add(llvm::createCFGPrinterPass());
