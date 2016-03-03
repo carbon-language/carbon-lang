56de012b41fbc8ed743b7de89a0e0b8df3c373db
Michael Zuckerman <Michael.zuckerman@intel.com>
2016-03-03 12:17:50 +0000

    Fixing a checkfile error in avx512vlbw-builtins.c
    Differential Revision: http://reviews.llvm.org/D17814
    
    llvm-svn: 262611

--- clang/test/CodeGen/avx512vlbw-builtins.c
+++ clang/test/CodeGen/avx512vlbw-builtins.c
@@ -1959,49 +1959,49 @@ __m256i test_mm256_maskz_slli_epi16(__mmask16 __U, __m256i __A) {
 
 __m128i test_mm_mask_mov_epi16(__m128i __W, __mmask8 __U, __m128i __A) {
   // CHECK-LABEL: @test_mm_mask_mov_epi16
-  // CHECK: @llvm.x86.avx512.mask.mov.128
+  // CHECK: @llvm.x86.avx512.mask.movu.w.128
   return _mm_mask_mov_epi16(__W, __U, __A); 
 }
 
 __m128i test_mm_maskz_mov_epi16(__mmask8 __U, __m128i __A) {
   // CHECK-LABEL: @test_mm_maskz_mov_epi16
-  // CHECK: @llvm.x86.avx512.mask.mov.128
+  // CHECK: @llvm.x86.avx512.mask.movu.w.128
   return _mm_maskz_mov_epi16(__U, __A); 
 }
 
 __m256i test_mm256_mask_mov_epi16(__m256i __W, __mmask16 __U, __m256i __A) {
   // CHECK-LABEL: @test_mm256_mask_mov_epi16
-  // CHECK: @llvm.x86.avx512.mask.mov.256
+  // CHECK: @llvm.x86.avx512.mask.movu.w.256
   return _mm256_mask_mov_epi16(__W, __U, __A); 
 }
 
 __m256i test_mm256_maskz_mov_epi16(__mmask16 __U, __m256i __A) {
   // CHECK-LABEL: @test_mm256_maskz_mov_epi16
-  // CHECK: @llvm.x86.avx512.mask.mov.256
+  // CHECK: @llvm.x86.avx512.mask.movu.w.256
   return _mm256_maskz_mov_epi16(__U, __A); 
 }
 
 __m128i test_mm_mask_mov_epi8(__m128i __W, __mmask16 __U, __m128i __A) {
   // CHECK-LABEL: @test_mm_mask_mov_epi8
-  // CHECK: @llvm.x86.avx512.mask.mov.128
+  // CHECK: @llvm.x86.avx512.mask.movu.b.128
   return _mm_mask_mov_epi8(__W, __U, __A); 
 }
 
 __m128i test_mm_maskz_mov_epi8(__mmask16 __U, __m128i __A) {
   // CHECK-LABEL: @test_mm_maskz_mov_epi8
-  // CHECK: @llvm.x86.avx512.mask.mov.128
+  // CHECK: @llvm.x86.avx512.mask.movu.b.128
   return _mm_maskz_mov_epi8(__U, __A); 
 }
 
 __m256i test_mm256_mask_mov_epi8(__m256i __W, __mmask32 __U, __m256i __A) {
   // CHECK-LABEL: @test_mm256_mask_mov_epi8
-  // CHECK: @llvm.x86.avx512.mask.mov.256
+  // CHECK: @llvm.x86.avx512.mask.movu.b.256
   return _mm256_mask_mov_epi8(__W, __U, __A); 
 }
 
 __m256i test_mm256_maskz_mov_epi8(__mmask32 __U, __m256i __A) {
   // CHECK-LABEL: @test_mm256_maskz_mov_epi8
-  // CHECK: @llvm.x86.avx512.mask.mov.256
+  // CHECK: @llvm.x86.avx512.mask.movu.b.256
   return _mm256_maskz_mov_epi8(__U, __A); 
 }
 
