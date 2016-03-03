65c02ec83785e6e4ddb656d7bc33d7242ef0737d
Gabor Horvath <xazax.hun@gmail.com>
2016-03-03 13:43:23 +0000

    [clang-tidy] Improve the robustness of a test.
    llvm-svn: 262618

--- clang-tools-extra/test/clang-tidy/misc-suspicious-semicolon-fail.cpp
+++ clang-tools-extra/test/clang-tidy/misc-suspicious-semicolon-fail.cpp
@@ -1,5 +1,4 @@
-// RUN: clang-tidy %s -checks="-*,misc-suspicious-semicolon" -- 2>&1 > %t
-// RUN: FileCheck --input-file=%t %s
+// RUN: clang-tidy %s -checks="-*,misc-suspicious-semicolon" -- 2>&1 | FileCheck %s
 
 // Note: This test verifies that, the checker does not emit any warning for
 //       files that do not compile.
@@ -8,6 +7,6 @@ bool g();
 
 void f() {
   if (g());
-  // CHECK-NOT: :[[@LINE-1]]:11: warning: potentially unintended semicolon [misc-suspicious-semicolon]
+  // CHECK-NOT: [misc-suspicious-semicolon]
   int a
 }
