499a5c632366b12968b9b19400117a8fcb91a38c
Valery Pykhtin <Valery.Pykhtin@amd.com>
2016-03-06 13:27:13 +0000

    [AMDGPU] table-driven parser/printer for amd_kernel_code_t structure fields
    Differential Revision: http://reviews.llvm.org/D17150
    
    llvm-svn: 262804

---
+++ llvm/lib/Target/AMDGPU/Utils/AMDKernelCodeTUtils.h
@@ -0,0 +1,38 @@
+//===- AMDGPUKernelCodeTUtils.h - helpers for amd_kernel_code_t  *- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+/// \file AMDKernelCodeTUtils.h
+//===----------------------------------------------------------------------===//
+
+#ifndef AMDKERNELCODETUTILS_H
+#define AMDKERNELCODETUTILS_H
+
+#include "AMDKernelCodeT.h"
+
+namespace llvm {
+
+class MCAsmLexer;
+class raw_ostream;
+class StringRef;
+
+void printAmdKernelCodeField(const amd_kernel_code_t& C,
+  int FldIndex,
+  raw_ostream& OS);
+
+void dumpAmdKernelCode(const amd_kernel_code_t* C,
+  raw_ostream& OS,
+  const char* tab);
+
+bool parseAmdKernelCodeField(StringRef ID,
+  MCAsmLexer& Lexer,
+  amd_kernel_code_t& C,
+  raw_ostream& Err);
+
+}
+
+#endif // AMDKERNELCODETUTILS_H
