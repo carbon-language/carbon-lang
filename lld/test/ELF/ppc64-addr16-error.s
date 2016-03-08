715b04eda622ee836716423d94ee6fd1d6115a2e
Rafael Espindola <rafael.espindola@gmail.com>
2016-03-08 19:51:58 +0000

    Use defined hidden symbols for out of range tests.
    When the symbol can be preempted the error is not entirely accurate.
    
    This just makes upcoming patches more readable.
    
    llvm-svn: 262954

--- lld/test/ELF/ppc64-addr16-error.s
+++ lld/test/ELF/ppc64-addr16-error.s
@@ -1,5 +1,6 @@
 // RUN: llvm-mc -filetype=obj -triple=powerpc64-unknown-linux %s -o %t
-// RUN: not ld.lld -shared %t -o %t2 2>&1 | FileCheck %s
+// RUN: llvm-mc -filetype=obj -triple=powerpc64-unknown-linux %p/Inputs/ppc64-addr16-error.s -o %t2
+// RUN: not ld.lld -shared %t %t2 -o %t3 2>&1 | FileCheck %s
 // REQUIRES: ppc
 
 .short sym+65539
--- lld/test/ELF/x86-64-reloc-error.s
+++ lld/test/ELF/x86-64-reloc-error.s
@@ -1,10 +1,10 @@
-// RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %S/Inputs/abs.s -o %tabs
+// RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %S/Inputs/x86-64-reloc-error.s -o %tabs
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
 // RUN: not ld.lld -shared %tabs %t -o %t2 2>&1 | FileCheck %s
 // REQUIRES: x86
 
   movl $big, %edx
-  movq _start - 0x1000000000000, %rdx
+  movq $foo - 0x1000000000000, %rdx
 
 # CHECK: R_X86_64_32 out of range
 # CHECK: R_X86_64_32S out of range
