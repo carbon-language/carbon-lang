416ad130eab2047e64fa1be7ab5f60a6caad7cb9
Nico Weber <nicolasweber@gmx.de>
2016-03-10 22:40:02 +0000

    Reenable asm-errors.c
    r134811 made the test pass and reenabled it, but r134831
    accidentally disabled it again due to a bad merge.
    
    llvm-svn: 263168

--- clang/test/CodeGen/asm-errors.c
+++ clang/test/CodeGen/asm-errors.c
@@ -1,8 +1,7 @@
 // REQUIRES: x86-registered-target
 
-// RUN: true
-// UN: not %clang_cc1 -triple i386-apple-darwin10 -emit-obj %s -o /dev/null > %t 2>&1
-// UN: FileCheck %s < %t
+// RUN: not %clang_cc1 -triple i386-apple-darwin10 -emit-obj %s -o /dev/null > %t 2>&1
+// RUN: FileCheck %s < %t
 // RUN: %clang_cc1 -triple i386-apple-darwin10 -emit-llvm-bc %s -o %t.bc
 // RUN: %clang_cc1 -triple i386-apple-darwin10 -emit-obj %t.bc -o /dev/null 2>&1 | \
 // RUN:   FileCheck --check-prefix=CRASH-REPORT %s
