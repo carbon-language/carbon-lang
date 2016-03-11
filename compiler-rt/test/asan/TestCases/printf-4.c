7a2113882ffc8d588fca232e13878c855b3c3ece
Reid Kleckner <rnk@google.com>
2016-03-11 19:11:15 +0000

    Make printf-4 more robust to strlen interception after r263177
    The CHECK line was matching stack-buffer-overflow from puts calling
    strlen, which is not the bug the test is trying to catch.
    
    llvm-svn: 263282

--- compiler-rt/test/asan/TestCases/printf-4.c
+++ compiler-rt/test/asan/TestCases/printf-4.c
@@ -1,10 +1,8 @@
 // RUN: %clang_asan -O2 %s -o %t
-// We need replace_str=0 and replace_intrin=0 to avoid reporting errors in
-// strlen() and memcpy() called by puts().
-// RUN: %env_asan_opts=replace_str=0:replace_intrin=0:check_printf=1 not %run %t 2>&1 | FileCheck --check-prefix=CHECK-ON %s
-// RUN: %env_asan_opts=replace_str=0:replace_intrin=0 not %run %t 2>&1 | FileCheck --check-prefix=CHECK-ON %s
+// RUN: %env_asan_opts=check_printf=1 not %run %t 2>&1 | FileCheck --check-prefix=CHECK-ON %s
+// RUN: not %run %t 2>&1 | FileCheck --check-prefix=CHECK-ON %s
 
-// FIXME: printf is not intercepted on Windows yet.
+// FIXME: sprintf is not intercepted on Windows yet.
 // XFAIL: win32
 
 #include <stdio.h>
@@ -14,10 +12,14 @@ int main() {
   volatile float f = 1.239;
   volatile char s[] = "34";
   volatile char buf[2];
+  puts("before sprintf");
   sprintf((char *)buf, "%c %d %.3f %s\n", c, x, f, s);
+  puts("after sprintf");
   puts((const char *)buf);
   return 0;
   // Check that size of output buffer is sanitized.
+  // CHECK-ON: before sprintf
+  // CHECK-ON-NOT: after sprintf
   // CHECK-ON: stack-buffer-overflow
   // CHECK-ON-NOT: 0 12 1.239 34
 }
