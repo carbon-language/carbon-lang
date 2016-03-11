91b4bd4b1261d22c3948caed959eccb03dc558ec
Todd Fiala <todd.fiala@gmail.com>
2016-03-11 08:12:36 +0000

    skip newly segfaulting test on OS X public CI
    llvm-svn: 263205

--- lldb/packages/Python/lldbsuite/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
+++ lldb/packages/Python/lldbsuite/test/python_api/default-constructor/TestDefaultConstructorForAPIObjects.py
@@ -114,6 +114,7 @@ class APIDefaultConstructorTestCase(TestBase):
 
     @add_test_categories(['pyapi'])
     @no_debug_info_test
+    @skipIfDarwin  # seg faulting, bugnumber="<rdar://problem/25105824>"
     def test_SBDebugger(self):
         obj = lldb.SBDebugger()
         if self.TraceOn():
