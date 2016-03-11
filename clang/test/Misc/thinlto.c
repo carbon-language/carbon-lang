f39a70cf32b2be7fa9fc080af7ea137e16200a85
Teresa Johnson <tejohnson@google.com>
2016-03-11 18:52:42 +0000

    Update test case for llvm summary format changes in D17592.
    llvm-svn: 263276

--- clang/test/Misc/thinlto.c
+++ clang/test/Misc/thinlto.c
@@ -1,8 +1,8 @@
 // RUN: %clang_cc1 -flto=thin -emit-llvm-bc < %s | llvm-bcanalyzer -dump | FileCheck %s
-// CHECK: <FUNCTION_SUMMARY_BLOCK
-// CHECK-NEXT: <PERMODULE_ENTRY
-// CHECK-NEXT: <PERMODULE_ENTRY
-// CHECK-NEXT: </FUNCTION_SUMMARY_BLOCK
+// CHECK: <GLOBALVAL_SUMMARY_BLOCK
+// CHECK-NEXT: <PERMODULE
+// CHECK-NEXT: <PERMODULE
+// CHECK-NEXT: </GLOBALVAL_SUMMARY_BLOCK
 
 __attribute__((noinline)) void foo() {}
 
