efe190a6eb8a841b8dde8fd81e0ead6e5144337a
Nico Weber <nicolasweber@gmx.de>
2016-03-13 02:48:51 +0000

    Make test a bit stricter to check not just the file basename is printed.
    llvm-svn: 263353

--- clang/test/Driver/cl-pch-showincludes.cpp
+++ clang/test/Driver/cl-pch-showincludes.cpp
@@ -9,13 +9,13 @@
 // input itself) and header3.h (included directly, above) should be printed.
 // RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Ycheader2.h /FIheader2.h /Fp%t.pch /c -- %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-YC %s
-// CHECK-YC: Note: including file: {{.*header2.h}}
-// CHECK-YC: Note: including file: {{.*header1.h}}
-// CHECK-YC: Note: including file: {{.*header3.h}}
+// CHECK-YC: Note: including file: {{.+header2.h}}
+// CHECK-YC: Note: including file: {{.+header1.h}}
+// CHECK-YC: Note: including file: {{.+header3.h}}
 
 // When using the pch, only the direct include is printed.
 // RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Yuheader2.h /FIheader2.h /Fp%t.pch /c -- %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-YU %s
 // CHECK-YU-NOT: Note: including file: {{.*header1.h}}
 // CHECK-YU-NOT: Note: including file: {{.*header2.h}}
-// CHECK-YU: Note: including file: {{.*header3.h}}
+// CHECK-YU: Note: including file: {{.+header3.h}}
