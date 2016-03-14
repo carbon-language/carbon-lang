ae6d694ae7a1685b81c3924a3756b246f79cfb24
Eric Christopher <echristo@gmail.com>
2016-03-14 06:21:07 +0000

    Give the test a temporary output so it can be cleaned up.
    llvm-svn: 263410

--- clang/test/Driver/cl-pch-showincludes.cpp
+++ clang/test/Driver/cl-pch-showincludes.cpp
@@ -8,14 +8,14 @@
 
 // When building the pch, header1.h (included by header2.h), header2.h (the pch
 // input itself) and header3.h (included directly, above) should be printed.
-// RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Ycheader2.h /FIheader2.h /Fp%t.pch /c -- %s 2>&1 \
+// RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Ycheader2.h /FIheader2.h /Fp%t.pch /c /Fo%t -- %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-YC %s
 // CHECK-YC: Note: including file: {{.+header2.h}}
 // CHECK-YC: Note: including file: {{.+header1.h}}
 // CHECK-YC: Note: including file: {{.+header3.h}}
 
 // When using the pch, only the direct include is printed.
-// RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Yuheader2.h /FIheader2.h /Fp%t.pch /c -- %s 2>&1 \
+// RUN: %clang_cl -Werror /showIncludes /I%S/Inputs /Yuheader2.h /FIheader2.h /Fp%t.pch /c /Fo%t -- %s 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-YU %s
 // CHECK-YU-NOT: Note: including file: {{.*pch}}
 // CHECK-YU-NOT: Note: including file: {{.*header1.h}}
