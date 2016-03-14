423ec18cc2c626bcbdc22d9417c1c337abe7e035
NAKAMURA Takumi <geek4civic@gmail.com>
2016-03-14 23:00:44 +0000

    clang/test/CodeGenCXX/virtual-function-attrs.cpp: Appease i686-mingw32 to expect x86_thiscallcc.
    llvm-svn: 263505

--- clang/test/CodeGenCXX/virtual-function-attrs.cpp
+++ clang/test/CodeGenCXX/virtual-function-attrs.cpp
@@ -8,7 +8,7 @@ class A {
 
 void A::f() {}
 
-// CHECK: define void @_ZN1A1fEv({{.*}}) unnamed_addr
-// CHECK: declare void @_ZN1A1gEv({{.*}}) unnamed_addr
+// CHECK: define [[CC:(x86_thiscallcc )?]]void @_ZN1A1fEv({{.*}}) unnamed_addr
+// CHECK: declare [[CC]]void @_ZN1A1gEv({{.*}}) unnamed_addr
 // CHECK: declare {{.*}} @_ZN1AD1Ev({{.*}}) unnamed_addr
-// CHECK: declare void @_ZN1AD0Ev({{.*}}) unnamed_addr
+// CHECK: declare [[CC]]void @_ZN1AD0Ev({{.*}}) unnamed_addr
