5e1493b560da95a64d9fb445f5e54664122fa6b9
Arpith Chacko Jacob <acjacob@us.ibm.com>
2016-03-15 21:04:57 +0000

    [OpenMP] Base support for target directive codegen on NVPTX device.
    Summary:
    This patch adds base support for codegen of the target directive on the NVPTX device.
    
    Reviewers: ABataev
    
    Differential Revision: http://reviews.llvm.org/D17877
    
    llvm-svn: 263587

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -4145,6 +4145,14 @@ void CGOpenMPRuntime::emitTargetOutlinedFunction(
     CGF.EmitStmt(CS.getCapturedStmt());
   };
 
+  emitTargetOutlinedFunctionHelper(D, ParentName, OutlinedFn, OutlinedFnID,
+                                   IsOffloadEntry, CodeGen);
+}
+
+void CGOpenMPRuntime::emitTargetOutlinedFunctionHelper(
+    const OMPExecutableDirective &D, StringRef ParentName,
+    llvm::Function *&OutlinedFn, llvm::Constant *&OutlinedFnID,
+    bool IsOffloadEntry, const RegionCodeGenTy &CodeGen) {
   // Create a unique name for the entry function using the source location
   // information of the current target region. The name will be something like:
   //
@@ -4165,6 +4173,8 @@ void CGOpenMPRuntime::emitTargetOutlinedFunction(
     OS << "__omp_offloading" << llvm::format("_%x", DeviceID)
        << llvm::format("_%x_", FileID) << ParentName << "_l" << Line;
   }
+
+  const CapturedStmt &CS = *cast<CapturedStmt>(D.getAssociatedStmt());
 
   CodeGenFunction CGF(CGM, true);
   CGOpenMPTargetRegionInfo CGInfo(CS, CodeGen, EntryFnName);
