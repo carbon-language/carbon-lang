00a7cf9ffeeff953e88d8acf611a2d89f4063f06
Davide Italiano <davide@freebsd.org>
2016-03-15 22:24:58 +0000

    [ELF] Ignore _GLOBAL_OFFSET_TABLE_ symbol in shared libs.
    This fixes PR26705.
    
    Differential Revision:  http://reviews.llvm.org/D18166
    
    llvm-svn: 263594

---
+++ lld/test/ELF/global_offset_table_shared.s
@@ -0,0 +1,6 @@
+// RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
+// RUN: ld.lld -shared %t -o %t2
+// RUN: llvm-readobj -t %t2 | Filecheck %s
+.long _GLOBAL_OFFSET_TABLE_
+
+// CHECK-NOT: Name: _GLOBAL_OFFSET_TABLE_
