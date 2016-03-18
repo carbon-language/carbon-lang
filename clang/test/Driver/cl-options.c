72a9d15aba28a07c93abf18222b51f0afcd365ed
Reid Kleckner <rnk@google.com>
2016-03-18 18:42:56 +0000

    [clang-cl] Allow use of -gline-tables-only
    llvm-svn: 263816

--- clang/test/Driver/cl-options.c
+++ clang/test/Driver/cl-options.c
@@ -394,6 +394,10 @@
 // Z7: "-gcodeview"
 // Z7: "-debug-info-kind=limited"
 
+// RUN: %clang_cl -gline-tables-only /Z7 /c -### -- %s 2>&1 | FileCheck -check-prefix=Z7GMLT %s
+// Z7GMLT: "-gcodeview"
+// Z7GMLT: "-debug-info-kind=line-tables-only"
+
 // RUN: %clang_cl /c -### -- %s 2>&1 | FileCheck -check-prefix=BreproDefault %s
 // BreproDefault: "-mincremental-linker-compatible"
 
