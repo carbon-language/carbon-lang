7aba60c853634f42988544c8150b3510cc7468fd
Adam Nemet <anemet@apple.com>
2016-03-24 17:59:26 +0000

    [LLE] Check for mismatching types between the store and the load earlier
    isDependenceDistanceOfOne asserts that the store and the load access
    through the same type.  This function is also used by
    removeDependencesFromMultipleStores so we need to make sure we filter
    out mismatching types before reaching this point.
    
    Now we do this when the initial candidates are gathered.
    
    This is a refinement of the fix made in r262267.
    
    Fixes PR27048.
    
    llvm-svn: 264313

--- llvm/lib/Transforms/Scalar/LoopLoadElimination.cpp
+++ llvm/lib/Transforms/Scalar/LoopLoadElimination.cpp
@@ -171,6 +171,12 @@ public:
       auto *Load = dyn_cast<LoadInst>(Destination);
       if (!Load)
         continue;
+
+      // Only progagate the value if they are of the same type.
+      if (Store->getPointerOperand()->getType() !=
+          Load->getPointerOperand()->getType())
+        continue;
+
       Candidates.emplace_front(Load, Store);
     }
 
@@ -438,10 +444,6 @@ public:
     unsigned NumForwarding = 0;
     for (const StoreToLoadForwardingCandidate Cand : StoreToLoadDependences) {
       DEBUG(dbgs() << "Candidate " << Cand);
-      // Only progagate value if they are of the same type.
-      if (Cand.Store->getPointerOperand()->getType() !=
-          Cand.Load->getPointerOperand()->getType())
-        continue;
 
       // Make sure that the stored values is available everywhere in the loop in
       // the next iteration.
