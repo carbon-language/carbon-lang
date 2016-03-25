f72d5b608dacc1d4184d848cde3d8ca480df43cc
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2016-03-25 00:38:14 +0000

    CGLoopInfo: Use the MD_loop metadata kind from r264371, NFC
    Besides a small compile-time speedup, there should be no real
    functionality change here.
    
    llvm-svn: 264372

--- clang/lib/CodeGen/CGLoopInfo.cpp
+++ clang/lib/CodeGen/CGLoopInfo.cpp
@@ -258,7 +258,7 @@ void LoopInfoStack::InsertHelper(Instruction *I) const {
   if (TerminatorInst *TI = dyn_cast<TerminatorInst>(I)) {
     for (unsigned i = 0, ie = TI->getNumSuccessors(); i < ie; ++i)
       if (TI->getSuccessor(i) == L.getHeader()) {
-        TI->setMetadata("llvm.loop", L.getLoopID());
+        TI->setMetadata(llvm::LLVMContext::MD_loop, L.getLoopID());
         break;
       }
     return;
