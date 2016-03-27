6648a0817ef664d75374b303c1f4abb350576f31
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2016-03-27 22:55:19 +0000

    Support: Move StreamingMemoryObject{,Test}.cpp, NFC
    Change the filename to indicate this is a test, rename the tests, move
    them into an anonymous namespace, and rename some variables.  All to
    match our usual style before making further changes.
    
    llvm-svn: 264548

--- llvm/unittests/Support/StreamingMemoryObjectTest.cpp
+++ llvm/unittests/Support/StreamingMemoryObjectTest.cpp
@@ -1,4 +1,4 @@
-//===- llvm/unittest/Support/StreamingMemoryObject.cpp - unit tests -------===//
+//===- unittests/Support/StreamingMemoryObjectTest.cpp --------------------===//
 //
 //                     The LLVM Compiler Infrastructure
 //
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/ADT/STLExtras.h"
+#include "llvm/ADT/StringRef.h"
 #include "llvm/Support/StreamingMemoryObject.h"
 #include "gtest/gtest.h"
 #include <string.h>
@@ -15,25 +16,27 @@
 using namespace llvm;
 
 namespace {
+
 class NullDataStreamer : public DataStreamer {
-  size_t GetBytes(unsigned char *buf, size_t len) override {
-    memset(buf, 0, len);
-    return len;
+  size_t GetBytes(unsigned char *Buffer, size_t Length) override {
+    memset(Buffer, 0, Length);
+    return Length;
   }
 };
-}
 
-TEST(StreamingMemoryObject, Test) {
+TEST(StreamingMemoryObjectTest, isValidAddress) {
   auto DS = make_unique<NullDataStreamer>();
   StreamingMemoryObject O(std::move(DS));
   EXPECT_TRUE(O.isValidAddress(32 * 1024));
 }
 
-TEST(StreamingMemoryObject, TestSetKnownObjectSize) {
+TEST(StreamingMemoryObjectTest, setKnownObjectSize) {
   auto DS = make_unique<NullDataStreamer>();
   StreamingMemoryObject O(std::move(DS));
   uint8_t Buf[32];
-  EXPECT_EQ((uint64_t) 16, O.readBytes(Buf, 16, 0));
+  EXPECT_EQ(16u, O.readBytes(Buf, 16, 0));
   O.setKnownObjectSize(24);
-  EXPECT_EQ((uint64_t) 8, O.readBytes(Buf, 16, 16));
+  EXPECT_EQ(8u, O.readBytes(Buf, 16, 16));
 }
+
+} // end namespace
