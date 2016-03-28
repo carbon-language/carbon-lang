f20b6cec1c50d624fb5fb6a9003407be6e9f1bad
Vedant Kumar <vsk@apple.com>
2016-03-28 20:12:07 +0000

    Reapply "[PGO] Fix name encoding for ObjC-like functions"
    Function names in ObjC can have spaces in them. This interacts poorly
    with name compression, which uses spaces to separate PGO names. Fix the
    issue by using a different separator and update a test.
    
    I chose "\01" as the separator because 1) it's non-printable, 2) we
    strip it from PGO names, and 3) it's the next natural choice once "\00"
    is discarded (that one's overloaded).
    
    This reverts the revert commit beaf3d18. What's changed?
    
    - I fixed up the covmap-V2 binary format tests using a linux VM.
    - I updated the expected counts in instrprof-comdat.h to account for
      the fact that there have been bugfixes to clang coverage.
    - I added an assert to make sure we don't get bitten by this again.
    
    Differential Revision: http://reviews.llvm.org/D18516
    
    llvm-svn: 264641

--- llvm/test/tools/llvm-cov/Inputs/instrprof-comdat.h
+++ llvm/test/tools/llvm-cov/Inputs/instrprof-comdat.h
@@ -13,7 +13,12 @@ template <class T> T FOO<T>::DoIt(T ti) { // HEADER:  2| [[@LINE]]|template
     t += I;                               // HEADER: 20| [[@LINE]]|    t += I;
     if (I > ti / 2)                       // HEADER: 20| [[@LINE]]|    if (I > ti 
       t -= 1;                             // HEADER:  8| [[@LINE]]|      t -= 1;
-  }                                       // HEADER: 10| [[@LINE]]|  }
-                                          // HEADER:  1| [[@LINE]]|
-  return t;                               // HEADER:  1| [[@LINE]]|  return t;
+  }                                       // HEADER: 20| [[@LINE]]|  }
+                                          // HEADER:  2| [[@LINE]]|
+  return t;                               // HEADER:  2| [[@LINE]]|  return t;
 }
+
+// To generate the binaries which correspond to this file, you must first
+// compile a program with two calls to Foo<int>::DoIt(10) for each desired
+// architecture. Collect a raw profile from any one of these binaries, index
+// it, and check it in along with the executables.
