dcafeddfb73342658bee3ecdcedf144d0ce3db78
Vedant Kumar <vsk@apple.com>
2016-03-29 19:24:58 +0000

    [profile] Make a test work if run by the super-user
    llvm-svn: 264773

--- compiler-rt/test/profile/instrprof-error.c
+++ compiler-rt/test/profile/instrprof-error.c
@@ -1,8 +1,5 @@
 // RUN: %clang_profgen -o %t -O3 %s
-// RUN: touch %t.profraw
-// RUN: chmod -w %t.profraw
-// RUN: env LLVM_PROFILE_FILE=%t.profraw LLVM_PROFILE_VERBOSE_ERRORS=1 %run %t 1 2>&1 | FileCheck %s
-// RUN: chmod +w %t.profraw
+// RUN: env LLVM_PROFILE_FILE="/" LLVM_PROFILE_VERBOSE_ERRORS=1 %run %t 1 2>&1 | FileCheck %s
 
 int main(int argc, const char *argv[]) {
   if (argc < 2)
