333e69d70fff7f061836eddbff53b612dcb1abc6
Yunzhong Gao <Yunzhong_Gao@playstation.sony.com>
2016-03-29 22:59:20 +0000

    Fixing PR26558: remove the adx target attribute requirement from adc builtins.
    The addcarry and subborrow variants of the builtins do not require the adx
    target attribute; only the addcarryx variants require them.
    
    Differential Revision: http://reviews.llvm.org/D18533
    
    llvm-svn: 264801

--- clang/test/CodeGen/adc-builtins.c
+++ clang/test/CodeGen/adc-builtins.c
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-unknown-unknown -target-feature +adx -emit-llvm -o - %s | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -emit-llvm -o - %s | FileCheck %s
 
 #define __MM_MALLOC_H
 
