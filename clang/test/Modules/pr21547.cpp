a3cbf2a738042819bb2d7d4fae048e633d2588d2
Vassil Vassilev <v.g.vassilev@gmail.com>
2016-03-30 20:10:07 +0000

    [modules] Add a regression test for PR21547.
    llvm-svn: 264908

---
+++ clang/test/Modules/pr21547.cpp
@@ -0,0 +1,7 @@
+// RUN: rm -rf %t
+// RUN: %clang_cc1 -I%S/Inputs/PR21547 -verify %s
+// RUN: %clang_cc1 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -I%S/Inputs/PR21547 -verify %s
+
+#include "Inputs/PR21547/FirstHeader.h"
+
+//expected-no-diagnostics
