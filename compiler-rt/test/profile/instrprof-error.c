62f0576c5c9f691ae008710b42c9dc372618385c
Vedant Kumar <vsk@apple.com>
2016-03-30 04:36:10 +0000

    [profile] Remove quotes around env variable, NFC
    llvm-svn: 264824

--- compiler-rt/test/profile/instrprof-error.c
+++ compiler-rt/test/profile/instrprof-error.c
@@ -1,5 +1,5 @@
 // RUN: %clang_profgen -o %t -O3 %s
-// RUN: env LLVM_PROFILE_FILE="%t/" LLVM_PROFILE_VERBOSE_ERRORS=1 %run %t 1 2>&1 | FileCheck %s
+// RUN: env LLVM_PROFILE_FILE=%t/ LLVM_PROFILE_VERBOSE_ERRORS=1 %run %t 1 2>&1 | FileCheck %s
 
 int main(int argc, const char *argv[]) {
   if (argc < 2)
