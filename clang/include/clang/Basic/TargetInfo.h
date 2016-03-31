19cb9476a2beea3ed33bf001080e73de1d3b7220
Akira Hatanaka <ahatanaka@apple.com>
2016-03-31 18:32:38 +0000

    Fix the comment for the function added in r264998.
    llvm-svn: 265035

--- clang/include/clang/Basic/TargetInfo.h
+++ clang/include/clang/Basic/TargetInfo.h
@@ -414,16 +414,18 @@ public:
   /// types for the given target.
   unsigned getSimdDefaultAlign() const { return SimdDefaultAlign; }
 
-  /// Return the alignment (in bits) of the thrown exception object.
+  /// Return the alignment (in bits) of the thrown exception object. This is
+  /// only meaningful for targets that allocate C++ exceptions in a system
+  /// runtime, such as those using the Itanium C++ ABI.
   virtual unsigned getExnObjectAlignment() const {
-    /// Itanium says that an _Unwind_Exception has to be "double-word"
-    /// aligned (and thus the end of it is also so-aligned), meaning 16
-    /// bytes.  Of course, that was written for the actual Itanium,
-    /// which is a 64-bit platform.  Classically, the ABI doesn't really
-    /// specify the alignment on other platforms, but in practice
-    /// libUnwind declares the struct with __attribute__((aligned)), so
-    /// we assume that alignment here.  (It's generally 16 bytes, but
-    /// some targets overwrite it.)
+    // Itanium says that an _Unwind_Exception has to be "double-word"
+    // aligned (and thus the end of it is also so-aligned), meaning 16
+    // bytes.  Of course, that was written for the actual Itanium,
+    // which is a 64-bit platform.  Classically, the ABI doesn't really
+    // specify the alignment on other platforms, but in practice
+    // libUnwind declares the struct with __attribute__((aligned)), so
+    // we assume that alignment here.  (It's generally 16 bytes, but
+    // some targets overwrite it.)
     return getDefaultAlignForAttributeAligned();
   }
 
