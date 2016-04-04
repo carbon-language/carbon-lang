2ed6beac719c211c43ae85b44d209af97d50282d
Reid Kleckner <rnk@google.com>
2016-04-04 23:14:14 +0000

    Fix test failure from r265361
    llvm-svn: 265362

--- clang/test/CodeGen/align_value.cpp
+++ clang/test/CodeGen/align_value.cpp
@@ -4,7 +4,7 @@ typedef double * __attribute__((align_value(64))) aligned_double;
 
 void foo(aligned_double x, double * y __attribute__((align_value(32))),
          double & z __attribute__((align_value(128)))) { };
-// CHECK: define void @_Z3fooPdS_Rd(double* align 64 %x, double* align 32 %y, double* dereferenceable(8) align 128 %z)
+// CHECK: define void @_Z3fooPdS_Rd(double* align 64 %x, double* align 32 %y, double* align 128 dereferenceable(8) %z)
 
 struct ad_struct {
   aligned_double a;
