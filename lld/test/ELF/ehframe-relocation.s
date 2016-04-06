1b6188d2f865366ef94d65b65596f47c9196c20c
Rafael Espindola <rafael.espindola@gmail.com>
2016-04-06 19:38:26 +0000

    Make this test a bit stronger.
    Show that in addition to handling empty .eh_frame, it is merged with any
    regular .eh_frame.
    
    llvm-svn: 265588

--- lld/test/ELF/ehframe-relocation.s
+++ lld/test/ELF/ehframe-relocation.s
@@ -1,6 +1,7 @@
 // REQUIRES: x86
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
-// RUN: ld.lld %t.o -o %t
+// RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %p/Inputs/ehframe-relocation.s  -o %t2.o
+// RUN: ld.lld %t.o %t2.o -o %t
 // RUN: llvm-readobj -s %t | FileCheck %s
 // RUN: llvm-objdump -d %t | FileCheck --check-prefix=DISASM %s
 
@@ -11,7 +12,8 @@
 // CHECK-NEXT: ]
 // CHECK-NEXT: Address: 0x10120
 // CHECK-NEXT: Offset:
-// CHECK-NEXT: Size: 0
+// CHECK-NEXT: Size: 48
+// CHECK-NOT: .eh_frame
 
 // 0x10120 = 65824
 // 0x10120 + 5 = 65829
