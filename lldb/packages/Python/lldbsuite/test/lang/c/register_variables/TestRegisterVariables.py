b05a37f347a20e3d9f72fd8c65d54e115bdb1677
Tamas Berghammer <tberghammer@google.com>
2016-04-06 10:34:29 +0000

    Fix and xfail TestRegisterVariables after rL265498
    llvm-svn: 265527

--- lldb/packages/Python/lldbsuite/test/lang/c/register_variables/TestRegisterVariables.py
+++ lldb/packages/Python/lldbsuite/test/lang/c/register_variables/TestRegisterVariables.py
@@ -92,7 +92,8 @@ class RegisterVariableTestCase(TestBase):
 
 
     @expectedFailureAll(compiler="clang", compiler_version=['<', '3.5'])
-    @expectedFailureAll(compiler="gcc", compiler_version=['=', '4.8.2'])
+    @expectedFailureAll(compiler="gcc", compiler_version=['>=', '4.8.2'])
+    @expectedFailureAll(oslist="linux", archs="i386")
     def test_and_run_command(self):
         """Test expressions on register values."""
 
