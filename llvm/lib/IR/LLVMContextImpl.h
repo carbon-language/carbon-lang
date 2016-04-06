3e0430e0a81980e179f4efe79ea2e00d5f929317
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2016-04-06 06:41:54 +0000

    IR: Move MDStrings to a BumpPtrAllocator
    We never delete any MDString until the context is destroyed.  Might as
    well throw them onto a BumpPtrAllocator.
    
    llvm-svn: 265520

--- llvm/lib/IR/LLVMContextImpl.h
+++ llvm/lib/IR/LLVMContextImpl.h
@@ -931,7 +931,7 @@ public:
   FoldingSet<AttributeSetImpl> AttrsLists;
   FoldingSet<AttributeSetNode> AttrsSetNodes;
 
-  StringMap<MDString> MDStringCache;
+  StringMap<MDString, BumpPtrAllocator> MDStringCache;
   DenseMap<Value *, ValueAsMetadata *> ValuesAsMetadata;
   DenseMap<Metadata *, MetadataAsValue *> MetadataAsValues;
 
