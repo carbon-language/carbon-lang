8f42e69b18cdefdbb6672ae940b20e02b094aadd
Manman Ren <manman.ren@gmail.com>
2016-04-06 23:28:26 +0000

    Keep -fmodule-implementation-of as an alias of -fmodule-name.
    This helps us transitioning to -fmodule-name. Once the transitioning is done,
    we can remove this alias.
    
    rdar://24800983
    
    llvm-svn: 265616

--- clang/test/Modules/implementation-of-module.m
+++ clang/test/Modules/implementation-of-module.m
@@ -1,18 +1,18 @@
 // RUN: rm -rf %t
 // RUN: %clang_cc1 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -w -Werror=auto-import %s -I %S/Inputs \
-// RUN:     -fmodule-name=category_right -fsyntax-only
+// RUN:     -fmodule-implementation-of category_right -fsyntax-only
 
 // RUN: %clang_cc1 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -w -Werror=auto-import %s -I %S/Inputs \
-// RUN:     -fmodule-name=category_right -dM -E -o - 2>&1 | FileCheck %s
+// RUN:     -fmodule-implementation-of category_right -dM -E -o - 2>&1 | FileCheck %s
 // CHECK-NOT: __building_module
 
 // RUN: %clang_cc1 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -w -Werror=auto-import %s -I %S/Inputs \
-// RUN:     -fmodule-name=category_left -verify
+// RUN:     -fmodule-implementation-of category_left -verify
 
 // RUN: %clang_cc1 -x objective-c-header -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -w -Werror=auto-import %s -I %S/Inputs \
-// RUN:     -fmodule-name=category_right -emit-pch -o %t.pch
+// RUN:     -fmodule-implementation-of category_right -emit-pch -o %t.pch
 // RUN: %clang_cc1 -x objective-c-header -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -w -Werror=auto-import %s -I %S/Inputs \
-// RUN:     -DWITH_PREFIX -fmodules-ignore-macro=WITH_PREFIX -include-pch %t.pch -fmodule-name=category_right
+// RUN:     -DWITH_PREFIX -fmodules-ignore-macro=WITH_PREFIX -include-pch %t.pch -fmodule-implementation-of category_right
 
 #ifndef WITH_PREFIX
 
