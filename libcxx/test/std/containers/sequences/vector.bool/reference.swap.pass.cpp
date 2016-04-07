f90ec0e82e098d0a64070f4f16c6388821132430
Marshall Clow <mclow.lists@gmail.com>
2016-04-07 14:24:16 +0000

    Added a noexcept test
    llvm-svn: 265674

--- libcxx/test/std/containers/sequences/vector.bool/reference.swap.pass.cpp
+++ libcxx/test/std/containers/sequences/vector.bool/reference.swap.pass.cpp
@@ -17,12 +17,18 @@
 
 int main()
 {
+
     bool a[] = {false, true, false, true};
     bool* an = a + sizeof(a)/sizeof(a[0]);
 
 	std::vector<bool> v(a, an);
 	std::vector<bool>::reference r1 = v[0];
 	std::vector<bool>::reference r2 = v[3];
+
+#if __has_feature(cxx_noexcept)
+    static_assert((noexcept(v.swap(r1,r2))), "");
+#endif
+
 	assert(!r1);
 	assert( r2);
 	v.swap(r1, r2);
