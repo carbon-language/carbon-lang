541dff5acdefec44eec022dbedee5e406352eba1
Pavel Labath <labath@google.com>
2016-04-12 13:55:54 +0000

    Fixup TestFdLeak
    this test was unintentionally XFAILed due to a change in the behavior of the expectedFailure
    decorator. Fix that. Also, mark the test as debug-info independent while I'm in there.
    
    llvm-svn: 266072

--- lldb/packages/Python/lldbsuite/test/functionalities/avoids-fd-leak/TestFdLeak.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/avoids-fd-leak/TestFdLeak.py
@@ -16,11 +16,14 @@ from lldbsuite.test.decorators import *
 def python_leaky_fd_version(test):
     import sys
     # Python random module leaks file descriptors on some versions.
-    return (sys.version_info >= (2, 7, 8) and sys.version_info < (2, 7, 10),
-            "Python random module leaks file descriptors in this python version")
+    if sys.version_info >= (2, 7, 8) and sys.version_info < (2, 7, 10):
+        return "Python random module leaks file descriptors in this python version"
+    return None
 
 
 class AvoidsFdLeakTestCase(TestBase):
+
+    NO_DEBUG_INFO_TESTCASE = True
 
     mydir = TestBase.compute_mydir(__file__)
 
@@ -56,7 +59,6 @@ class AvoidsFdLeakTestCase(TestBase):
 
     @expectedFailure(python_leaky_fd_version, "bugs.freebsd.org/197376")
     @expectedFailureAll(oslist=['freebsd'], bugnumber="llvm.org/pr25624 still failing with Python 2.7.10")
-    @expectedFlakeyLinux
     @skipIfWindows # The check for descriptor leakage needs to be implemented differently here.
     @skipIfTargetAndroid() # Android have some other file descriptors open by the shell
     def test_fd_leak_multitarget (self):
