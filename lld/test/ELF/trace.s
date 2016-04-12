f2638d42560998a77d9e973496e57e3b55676f85
George Rimar <grimar@accesssoftek.com>
2016-04-12 12:01:31 +0000

    [ELF] Removed redundant llvm-mc call. NFC.
    llvm-svn: 266059

--- lld/test/ELF/trace.s
+++ lld/test/ELF/trace.s
@@ -1,6 +1,5 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.foo.o
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t1.bar.o
 
 ## Check -t
 # RUN: ld.lld -shared %t.foo.o -o %t.so -t 2>&1 | FileCheck %s
