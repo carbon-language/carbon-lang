7950b12957775d9e71fad3edb40d9389138c705b
Davide Italiano <davide@freebsd.org>
2016-04-15 17:34:32 +0000

    [ParallelCG] Add a new splitCodeGen() API which takes a TargetMachineFactory.
    This is a recommit of r266390 with a fix that will allow tests to pass
    (hopefully). Before we got a StringRef to M->getTargetTriple() and right
    after we moved the Module so we were referencing a dangling object.
    
    llvm-svn: 266456

--- llvm/include/llvm/CodeGen/ParallelCG.h
+++ llvm/include/llvm/CodeGen/ParallelCG.h
@@ -18,6 +18,8 @@
 #include "llvm/Support/CodeGen.h"
 #include "llvm/Target/TargetMachine.h"
 
+#include <functional>
+
 namespace llvm {
 
 class Module;
@@ -40,6 +42,18 @@ splitCodeGen(std::unique_ptr<Module> M, ArrayRef<raw_pwrite_stream *> OSs,
              Reloc::Model RM = Reloc::Default,
              CodeModel::Model CM = CodeModel::Default,
              CodeGenOpt::Level OL = CodeGenOpt::Default,
+             TargetMachine::CodeGenFileType FT = TargetMachine::CGFT_ObjectFile,
+             bool PreserveLocals = false);
+
+/// Split M into OSs.size() partitions, and generate code for each.
+/// It is a variant that takes a factory function for the TargetMachine
+/// TMFactory. See the other splitCodeGen() for a more detailed description.
+///
+/// \returns M if OSs.size() == 1, otherwise returns std::unique_ptr<Module>().
+std::unique_ptr<Module>
+splitCodeGen(std::unique_ptr<Module> M, ArrayRef<raw_pwrite_stream *> OSs,
+             ArrayRef<llvm::raw_pwrite_stream *> BCOSs,
+             const std::function<std::unique_ptr<TargetMachine>()> &TMFactory,
              TargetMachine::CodeGenFileType FT = TargetMachine::CGFT_ObjectFile,
              bool PreserveLocals = false);
 
