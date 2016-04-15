812e5595890da74e531c47b90db8ddd24b57fe8c
Sean Callanan <scallanan@apple.com>
2016-04-15 00:26:32 +0000

    Added a testcase for defining and using lambdas in the expression parser.
    <rdar://problem/25739133>
    
    llvm-svn: 266397

---
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/lambdas/main.cpp
@@ -0,0 +1,17 @@
+//===-- main.cpp ------------------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+#include <stdio.h>
+
+int main (int argc, char const *argv[])
+{
+    printf("Stop here\n");     //% self.runCmd("expression auto $add = [](int a, int b) { return a + b };")
+                               //% self.expect("expression $add(2,3)", substrs = ['= 5'])
+    return 0; 
+}
