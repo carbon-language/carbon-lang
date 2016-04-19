d66dc19251d0304dd159d8f013760e852db50bbd
Akira Hatanaka <ahatanaka@apple.com>
2016-04-19 02:21:47 +0000

    Add a test case to check a member's default constructor is also run.
    This is a follow-up to r266645.
    
    llvm-svn: 266706

--- clang/test/Parser/objc-default-ctor-init.mm
+++ clang/test/Parser/objc-default-ctor-init.mm
@@ -1,5 +1,6 @@
 // RUN: %clang_cc1 -triple x86_64-apple-macosx10.10 -std=c++11 -ast-dump %s | FileCheck %s
 // CHECK: CXXCtorInitializer Field {{.*}} 'ptr' 'void *'
+// CHECK: CXXCtorInitializer Field {{.*}} 'q' 'struct Q'
 
 @interface NSObject
 @end
@@ -7,9 +8,12 @@
 @interface I : NSObject
 @end
 
+struct Q { Q(); };
+
 struct S {
   S();
   void *ptr = nullptr;
+  Q q;
 };
 
 @implementation I
