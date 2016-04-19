8743f8ca245d94e91889f376c236aed81b61ffa3
Ben Craig <ben.craig@codeaurora.org>
2016-04-19 20:13:55 +0000

    Include initializer_list from utility
    The C++11 and C++14 standards both say in the header <utility> synopsis that
    <utility> shall include <initializer_list>.
    
    llvm-svn: 266808

---
+++ libcxx/test/std/utilities/utility/synopsis.pass.cpp
@@ -0,0 +1,20 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <utility>
+
+// #include <initializer_list>
+
+#include <utility>
+
+int main()
+{
+    std::initializer_list<int> x;
+}
+
