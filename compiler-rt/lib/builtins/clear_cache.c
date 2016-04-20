de120799a02806f7b88f39e9a934a5c491462158
Saleem Abdulrasool <compnerd@compnerd.org>
2016-04-20 17:43:43 +0000

    builtins: fix -Werror,-Wunused-variable warning
    This removes the unused variable `flags`.  NFC
    
    llvm-svn: 266892

--- compiler-rt/lib/builtins/clear_cache.c
+++ compiler-rt/lib/builtins/clear_cache.c
@@ -110,7 +110,6 @@ void __clear_cache(void *start, void *end) {
     #elif defined(__linux__)
          register int start_reg __asm("r0") = (int) (intptr_t) start;
          const register int end_reg __asm("r1") = (int) (intptr_t) end;
-         const register int flags __asm("r2") = 0;
          const register int syscall_nr __asm("r7") = __ARM_NR_cacheflush;
          __asm __volatile("svc 0x0"
                           : "=r"(start_reg)
