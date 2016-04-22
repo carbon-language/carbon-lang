e2c64057089bb597fccd178a2baff353504d4150
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-04-22 16:25:00 +0000

    [Hexagon] Add definitions for trap/pause instructions
    Also add tests for other instructions from HexagonSystemInst.td.
    
    llvm-svn: 267162

--- llvm/test/MC/Hexagon/instructions/system_user.s
+++ llvm/test/MC/Hexagon/instructions/system_user.s
@@ -24,3 +24,39 @@ dcfetch(r17 + #168)
 # Send value to ETM trace
 # CHECK: 00 c0 51 62
 trace(r17)
+
+# CHECK: 00 c0 00 a0
+dccleana(r0)
+
+# CHECK: 00 c0 41 a0
+dccleaninva(r1)
+
+# CHECK: 00 c0 22 a0
+dcinva(r2)
+
+# CHECK: 00 c0 c3 a0
+dczeroa(r3)
+
+# CHECK: 00 c0 c4 56
+icinva(r4)
+
+# CHECK: 02 c0 c0 57
+isync
+
+# CHECK: 00 c6 05 a6
+l2fetch(r5, r6)
+
+# CHECK: 00 c8 87 a6
+l2fetch(r7, r9:8)
+
+# CHECK: 1c df 40 54
+pause(#255)
+
+# CHECK: 00 c0 40 a8
+syncht
+
+# CHECK: 18 df 00 54
+trap0(#254)
+
+# CHECK: 14 df 80 54
+trap1(#253)
