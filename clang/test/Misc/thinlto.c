5655795bf4acb155b37bd94ee991980526179a61
Mehdi Amini <mehdi.amini@apple.com>
2016-04-24 03:44:55 +0000

    Make thinlto clang test more robust against LLVM changes.
    We should just test the effect of the clang level option here, i.e.
    that a summary is correctly emitted with -flto=thin
    
    From: Mehdi Amini <mehdi.amini@apple.com>
    llvm-svn: 267321

--- clang/test/Misc/thinlto.c
+++ clang/test/Misc/thinlto.c
@@ -1,9 +1,4 @@
 // RUN: %clang_cc1 -flto=thin -emit-llvm-bc < %s | llvm-bcanalyzer -dump | FileCheck %s
+// ; Check that the -flto=thin option emits a summary
 // CHECK: <GLOBALVAL_SUMMARY_BLOCK
-// CHECK-NEXT: <PERMODULE
-// CHECK-NEXT: <PERMODULE
-// CHECK-NEXT: </GLOBALVAL_SUMMARY_BLOCK
-
-__attribute__((noinline)) void foo() {}
-
-int main() { foo(); }
+int main() {}
