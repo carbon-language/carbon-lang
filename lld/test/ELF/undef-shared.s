7fa5970e690bfc0a40bd32a919a4f5d9eaa7bd5a
Peter Collingbourne <peter@pcc.me.uk>
2016-04-24 02:42:34 +0000

    Test what I intended to test.
    llvm-svn: 267316

--- lld/test/ELF/undef-shared.s
+++ lld/test/ELF/undef-shared.s
@@ -1,5 +1,5 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-# RUN: not ld.lld %t.o -o %t 2>&1 | FileCheck %s
+# RUN: not ld.lld %t.o -o %t.so -shared 2>&1 | FileCheck %s
 
 # CHECK: undefined symbol: hidden in {{.*}}
 .global hidden
