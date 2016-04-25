6d547205f026bba193ad13a5358269510acea196
Todd Fiala <todd.fiala@gmail.com>
2016-04-25 15:48:34 +0000

    skip TestBitfields.py on OS X
    tracked by:
    https://llvm.org/bugs/show_bug.cgi?id=27515
    
    llvm-svn: 267421

--- lldb/packages/Python/lldbsuite/test/lang/c/bitfields/TestBitfields.py
+++ lldb/packages/Python/lldbsuite/test/lang/c/bitfields/TestBitfields.py
@@ -22,6 +22,7 @@ class BitfieldsTestCase(TestBase):
 
     @skipIfWindows # BitFields exhibit crashes in record layout on Windows (http://llvm.org/pr21800)
     @skipIf("llvm.org/pr27510", oslist=["linux"], compiler="clang", compiler_version=[">=", "3.9"]) # expectedFailure, skip to avoid crash
+    @skipIf("llvm.org/pr27515", oslist=["macosx"]) # Assertion failed: (Offset >= Size), function insertPadding CGRecordLayoutBuilder.cpp
     def test_and_run_command(self):
         """Test 'frame variable ...' on a variable with bitfields."""
         self.build()
