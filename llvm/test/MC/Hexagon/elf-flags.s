a4602b5be19faec0f09658365d6348163cb53ed8
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-04-25 18:09:36 +0000

    [Hexagon] Use llvm-mc instead of llc in an MC testcase
    Remember to svn add the new file.
    
    llvm-svn: 267435

---
+++ llvm/test/MC/Hexagon/elf-flags.s
@@ -0,0 +1,9 @@
+# RUN: llvm-mc -arch=hexagon -mcpu=hexagonv4 --filetype=obj %s -o - | llvm-readobj -file-headers -elf-output-style=GNU | FileCheck --check-prefix=CHECK-V4 %s
+# RUN: llvm-mc -arch=hexagon -mcpu=hexagonv5 --filetype=obj %s -o - | llvm-readobj -file-headers -elf-output-style=GNU | FileCheck --check-prefix=CHECK-V5 %s
+# RUN: llvm-mc -arch=hexagon -mcpu=hexagonv55 --filetype=obj %s -o - | llvm-readobj -file-headers -elf-output-style=GNU | FileCheck --check-prefix=CHECK-V55 %s
+# RUN: llvm-mc -arch=hexagon -mcpu=hexagonv60 --filetype=obj %s -o - | llvm-readobj -file-headers -elf-output-style=GNU | FileCheck --check-prefix=CHECK-V60 %s
+
+# CHECK-V4: Flags: 0x3
+# CHECK-V5: Flags: 0x4
+# CHECK-V55: Flags: 0x5
+# CHECK-V60: Flags: 0x60
