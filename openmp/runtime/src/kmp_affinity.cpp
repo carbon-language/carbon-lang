8407f5b3bdc129a795df6b58a03e917a69eb4094
Jonathan Peyton <jonathan.l.peyton@intel.com>
2016-04-25 21:11:26 +0000

    Remove architecture dependent Hwloc DEBUG section
    This debug sections's functionality can be replicated using the environment
    variable KMP_TOPOLOGY_METHOD with different values and KMP_AFFINITY=verbose
    
    llvm-svn: 267472

--- openmp/runtime/src/kmp_affinity.cpp
+++ openmp/runtime/src/kmp_affinity.cpp
@@ -3777,36 +3777,6 @@ __kmp_aux_affinity_initialize(void)
             KMP_ASSERT(address2os == NULL);
             return;
         }
-#  if KMP_DEBUG
-        AddrUnsPair *otheraddress2os = NULL;
-        int otherdepth = -1;
-#   if KMP_MIC
-        otherdepth = __kmp_affinity_create_apicid_map(&otheraddress2os, &msg_id);
-#   else
-        otherdepth = __kmp_affinity_create_x2apicid_map(&otheraddress2os, &msg_id);
-#   endif
-        if(otheraddress2os != NULL && address2os != NULL) {
-            int i;
-            unsigned arent_equal_flag = 0;
-            for(i=0;i<__kmp_avail_proc;i++) {
-                if(otheraddress2os[i] != address2os[i]) arent_equal_flag = 1;
-            }
-            if(arent_equal_flag) {
-                KA_TRACE(10, ("__kmp_aux_affinity_initialize: Hwloc affinity places are different from APICID\n"));
-                KA_TRACE(10, ("__kmp_aux_affinity_initialize: APICID Table:\n"));
-                for(i=0;i<__kmp_avail_proc;i++) {
-                    otheraddress2os[i].print(); __kmp_printf("\n");
-                }
-                KA_TRACE(10, ("__kmp_aux_affinity_initialize: Hwloc Table:\n"));
-                for(i=0;i<__kmp_avail_proc;i++) {
-                    address2os[i].print(); __kmp_printf("\n");
-                }
-            }
-            else {
-                KA_TRACE(10, ("__kmp_aux_affinity_initialize: Hwloc affinity places are same as APICID\n"));
-            }
-        }
-#  endif // KMP_DEBUG
     }
 # endif // KMP_USE_HWLOC
 
