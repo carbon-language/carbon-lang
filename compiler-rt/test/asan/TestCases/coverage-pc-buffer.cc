a3060c1159ffb3a86daa613024ae47fd2fafcb05
Mike Aizatsky <aizatsky@chromium.org>
2016-04-26 21:56:14 +0000

    [sanitizers] rewriting test assuming assert() changes coverage.
    Summary:
    On windows platform assert() call creates two distinct CFG edges
    which are coverage-instrumented. Simply calling assert would
    change coverage numbers on the platform.
    
    Subscribers: kubabrecka
    
    Differential Revision: http://reviews.llvm.org/D19514
    
    llvm-svn: 267610

--- compiler-rt/test/asan/TestCases/coverage-pc-buffer.cc
+++ compiler-rt/test/asan/TestCases/coverage-pc-buffer.cc
@@ -19,23 +19,47 @@ void assertNotZeroPcs(uintptr_t *buf, uintptr_t size) {
 }
 
 int main() {
-  uintptr_t *buf = NULL;
-  uintptr_t sz = __sanitizer_get_coverage_pc_buffer(&buf);
-  assertNotZeroPcs(buf, sz);
-  assert(sz);
+  {
+    uintptr_t *buf = NULL;
+    uintptr_t sz = __sanitizer_get_coverage_pc_buffer(&buf);
+    assertNotZeroPcs(buf, sz);
+    assert(sz);
+  }
 
-  foo();
-  bar();
-  uintptr_t *buf1 = NULL;
-  uintptr_t sz1 = __sanitizer_get_coverage_pc_buffer(&buf1);
-  assertNotZeroPcs(buf1, sz1);
-  assert(buf1 == buf);
-  assert(sz1 > sz);
+  {
+    uintptr_t *buf = NULL;
+    uintptr_t sz = __sanitizer_get_coverage_pc_buffer(&buf);
+    // call functions for the first time.
+    foo();
+    bar();
+    uintptr_t *buf1 = NULL;
+    uintptr_t sz1 = __sanitizer_get_coverage_pc_buffer(&buf1);
+    assertNotZeroPcs(buf1, sz1);
+    assert(buf1 == buf);
+    assert(sz1 > sz);
+  }
 
-  __sanitizer_reset_coverage();
-  uintptr_t *buf3 = NULL;
-  uintptr_t sz3 = __sanitizer_get_coverage_pc_buffer(&buf3);
-  assertNotZeroPcs(buf3, sz3);
-  assert(buf3 == buf);
-  assert(sz3 < sz1);
+  {
+    uintptr_t *buf = NULL;
+    uintptr_t sz = __sanitizer_get_coverage_pc_buffer(&buf);
+    // second call shouldn't increase coverage.
+    bar();
+    uintptr_t *buf1 = NULL;
+    uintptr_t sz1 = __sanitizer_get_coverage_pc_buffer(&buf1);
+    assertNotZeroPcs(buf1, sz1);
+    assert(buf1 == buf);
+    assert(sz1 == sz);
+  }
+
+  {
+    uintptr_t *buf = NULL;
+    uintptr_t sz = __sanitizer_get_coverage_pc_buffer(&buf);
+    // reset coverage to 0.
+    __sanitizer_reset_coverage();
+    uintptr_t *buf1 = NULL;
+    uintptr_t sz1 = __sanitizer_get_coverage_pc_buffer(&buf1);
+    assertNotZeroPcs(buf1, sz1);
+    assert(buf1 == buf);
+    assert(sz1 < sz);
+  }
 }
