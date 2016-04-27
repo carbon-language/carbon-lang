68d2b881d28ac77933d2580a50e23ec2e89a6a13
Haojian Wu <hokein@google.com>
2016-04-27 13:23:39 +0000

    Remove explain-config testcase that detect hard-coded check.
    Remove it for now, we need to better way to figure out how to test it.
    
    llvm-svn: 267711

--- clang-tools-extra/test/clang-tidy/explain-checks.cpp
+++ clang-tools-extra/test/clang-tidy/explain-checks.cpp
@@ -3,7 +3,6 @@
 // RUN: clang-tidy -checks=modernize-use-nullptr -config="{Checks: '-*,modernize-use-nullptr'}" -explain-config | FileCheck --check-prefix=CHECK-MESSAGE3 %s
 // RUN: clang-tidy -checks=modernize-use-nullptr -config="{Checks: '-*,-modernize-use-nullptr'}" %S/Inputs/explain-config/a.cc -explain-config | FileCheck --check-prefix=CHECK-MESSAGE4 %s
 // RUN: clang-tidy -checks=modernize-use-nullptr -config="{Checks: '-*,modernize-*'}" -explain-config | FileCheck --check-prefix=CHECK-MESSAGE5 %s
-// RUN: clang-tidy -config="{Checks: 'modernize-use-nullptr'}" -explain-config | FileCheck --check-prefix=CHECK-MESSAGE6 %s
 // RUN: clang-tidy -explain-config %S/Inputs/explain-config/a.cc -- | grep "'modernize-use-nullptr' is enabled in the %S/Inputs/explain-config/.clang-tidy."
 
 // CHECK-MESSAGE1: 'modernize-use-nullptr' is enabled in the command-line option '-checks'.
@@ -11,4 +10,3 @@
 // CHECK-MESSAGE3: 'modernize-use-nullptr' is enabled in the command-line option '-checks'.
 // CHECK-MESSAGE4: 'modernize-use-nullptr' is enabled in the command-line option '-checks'.
 // CHECK-MESSAGE5: 'modernize-use-nullptr' is enabled in the command-line option '-checks'.
-// CHECK-MESSAGE6: 'clang-analyzer-unix.API' is enabled in the clang-tidy binary.
