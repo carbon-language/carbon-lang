514f05543f2493450e8265a850292c576d8be3ad
Michael Zuckerman <Michael.zuckerman@intel.com>
2016-04-27 15:35:13 +0000

    [Clang][Builtin][AVX512] Adding intrisnics for the vpconflict{q|d} instruction set
    Differential Revision: http://reviews.llvm.org/D19525
    
    llvm-svn: 267728

--- clang/lib/Headers/avx512vlcdintrin.h
+++ clang/lib/Headers/avx512vlcdintrin.h
@@ -56,6 +56,104 @@ _mm256_broadcastmw_epi32 (__mmask16 __A)
 }
 
 
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_conflict_epi64 (__m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictdi_128_mask ((__v2di) __A,
+               (__v2di) _mm_undefined_si128 (),
+               (__mmask8) -1);
+}
+
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_mask_conflict_epi64 (__m128i __W, __mmask8 __U, __m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictdi_128_mask ((__v2di) __A,
+               (__v2di) __W,
+               (__mmask8) __U);
+}
+
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_maskz_conflict_epi64 (__mmask8 __U, __m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictdi_128_mask ((__v2di) __A,
+               (__v2di)
+               _mm_setzero_di (),
+               (__mmask8) __U);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_conflict_epi64 (__m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictdi_256_mask ((__v4di) __A,
+               (__v4di)  _mm256_undefined_si256 (),
+               (__mmask8) -1);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_mask_conflict_epi64 (__m256i __W, __mmask8 __U, __m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictdi_256_mask ((__v4di) __A,
+               (__v4di) __W,
+               (__mmask8) __U);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_maskz_conflict_epi64 (__mmask8 __U, __m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictdi_256_mask ((__v4di) __A,
+               (__v4di) _mm256_setzero_si256 (),
+               (__mmask8) __U);
+}
+
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_conflict_epi32 (__m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictsi_128_mask ((__v4si) __A,
+               (__v4si) _mm_undefined_si128 (),
+               (__mmask8) -1);
+}
+
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_mask_conflict_epi32 (__m128i __W, __mmask8 __U, __m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictsi_128_mask ((__v4si) __A,
+               (__v4si) __W,
+               (__mmask8) __U);
+}
+
+static __inline__ __m128i __DEFAULT_FN_ATTRS
+_mm_maskz_conflict_epi32 (__mmask8 __U, __m128i __A)
+{
+  return (__m128i) __builtin_ia32_vpconflictsi_128_mask ((__v4si) __A,
+               (__v4si) _mm_setzero_si128 (),
+               (__mmask8) __U);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_conflict_epi32 (__m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictsi_256_mask ((__v8si) __A,
+               (__v8si) _mm256_undefined_si256 (),
+               (__mmask8) -1);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_mask_conflict_epi32 (__m256i __W, __mmask8 __U, __m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictsi_256_mask ((__v8si) __A,
+               (__v8si) __W,
+               (__mmask8) __U);
+}
+
+static __inline__ __m256i __DEFAULT_FN_ATTRS
+_mm256_maskz_conflict_epi32 (__mmask8 __U, __m256i __A)
+{
+  return (__m256i) __builtin_ia32_vpconflictsi_256_mask ((__v8si) __A,
+               (__v8si)
+               _mm256_setzero_si256 (),
+               (__mmask8) __U);
+}
+
 #undef __DEFAULT_FN_ATTRS
 
 #endif /* __AVX512VLCDINTRIN_H */
--- clang/test/CodeGen/avx512vlcd-builtins.c
+++ clang/test/CodeGen/avx512vlcd-builtins.c
@@ -28,3 +28,75 @@ __m256i test_mm256_broadcastmw_epi32(__mmask16 __A) {
   // CHECK: @llvm.x86.avx512.broadcastmw.256
   return _mm256_broadcastmw_epi32(__A); 
 }
+
+__m128i test_mm_conflict_epi64(__m128i __A) {
+  // CHECK-LABEL: @test_mm_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.128
+  return _mm_conflict_epi64(__A); 
+}
+
+__m128i test_mm_mask_conflict_epi64(__m128i __W, __mmask8 __U, __m128i __A) {
+  // CHECK-LABEL: @test_mm_mask_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.128
+  return _mm_mask_conflict_epi64(__W, __U, __A); 
+}
+
+__m128i test_mm_maskz_conflict_epi64(__mmask8 __U, __m128i __A) {
+  // CHECK-LABEL: @test_mm_maskz_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.128
+  return _mm_maskz_conflict_epi64(__U, __A); 
+}
+
+__m256i test_mm256_conflict_epi64(__m256i __A) {
+  // CHECK-LABEL: @test_mm256_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.256
+  return _mm256_conflict_epi64(__A); 
+}
+
+__m256i test_mm256_mask_conflict_epi64(__m256i __W, __mmask8 __U, __m256i __A) {
+  // CHECK-LABEL: @test_mm256_mask_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.256
+  return _mm256_mask_conflict_epi64(__W, __U, __A); 
+}
+
+__m256i test_mm256_maskz_conflict_epi64(__mmask8 __U, __m256i __A) {
+  // CHECK-LABEL: @test_mm256_maskz_conflict_epi64
+  // CHECK: @llvm.x86.avx512.mask.conflict.q.256
+  return _mm256_maskz_conflict_epi64(__U, __A); 
+}
+
+__m128i test_mm_conflict_epi32(__m128i __A) {
+  // CHECK-LABEL: @test_mm_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.128
+  return _mm_conflict_epi32(__A); 
+}
+
+__m128i test_mm_mask_conflict_epi32(__m128i __W, __mmask8 __U, __m128i __A) {
+  // CHECK-LABEL: @test_mm_mask_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.128
+  return _mm_mask_conflict_epi32(__W, __U, __A); 
+}
+
+__m128i test_mm_maskz_conflict_epi32(__mmask8 __U, __m128i __A) {
+  // CHECK-LABEL: @test_mm_maskz_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.128
+  return _mm_maskz_conflict_epi32(__U, __A); 
+}
+
+__m256i test_mm256_conflict_epi32(__m256i __A) {
+  // CHECK-LABEL: @test_mm256_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.256
+  return _mm256_conflict_epi32(__A); 
+}
+
+__m256i test_mm256_mask_conflict_epi32(__m256i __W, __mmask8 __U, __m256i __A) {
+  // CHECK-LABEL: @test_mm256_mask_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.256
+  return _mm256_mask_conflict_epi32(__W, __U, __A); 
+}
+
+__m256i test_mm256_maskz_conflict_epi32(__mmask8 __U, __m256i __A) {
+  // CHECK-LABEL: @test_mm256_maskz_conflict_epi32
+  // CHECK: @llvm.x86.avx512.mask.conflict.d.256
+  return _mm256_maskz_conflict_epi32(__U, __A); 
+}
