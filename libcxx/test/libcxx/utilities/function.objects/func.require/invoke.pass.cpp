bc32b5cf8f4c9f8243fe7b914500bee3bb6473c0
Eric Fiselier <eric@efcs.ca>
2016-04-29 01:52:57 +0000

    Move INVOKE tests into test/libcxx sub-tree.
    Testing the concrete implementation of INVOKE means calling the implementation
    specific names `__invoke` and `__invoke_constexpr`. For this reason the test
    are non-standard. For this reason it's best if the tests live outside of the
    `test/std` directory.
    
    llvm-svn: 267973

--- libcxx/test/libcxx/utilities/function.objects/func.require/invoke.pass.cpp
+++ libcxx/test/libcxx/utilities/function.objects/func.require/invoke.pass.cpp
@@ -9,12 +9,10 @@
 
 // [func.require]
 
-// INVOKE
-#if __cplusplus < 201103L
-int main () {}      // no __invoke in C++03
-#else
-
 #include <type_traits>
+#include <functional>
+
+#include "test_macros.h"
 
 template <typename T, int N>
 struct Array
@@ -26,10 +24,9 @@ struct Type
 {
     Array<char, 1>::type& f1();
     Array<char, 2>::type& f2() const;
-    
+#if TEST_STD_VER >= 11
     Array<char, 1>::type& g1()        &;
     Array<char, 2>::type& g2() const  &;
-#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     Array<char, 3>::type& g3()       &&;
     Array<char, 4>::type& g4() const &&;
 #endif
@@ -39,12 +36,10 @@ int main()
 {
     static_assert(sizeof(std::__invoke(&Type::f1, std::declval<Type        >())) == 1, "");
     static_assert(sizeof(std::__invoke(&Type::f2, std::declval<Type const  >())) == 2, "");
-    
+#if TEST_STD_VER >= 11
     static_assert(sizeof(std::__invoke(&Type::g1, std::declval<Type       &>())) == 1, "");
     static_assert(sizeof(std::__invoke(&Type::g2, std::declval<Type const &>())) == 2, "");
-#ifndef _LIBCPP_HAS_NO_RVALUE_REFERENCES
     static_assert(sizeof(std::__invoke(&Type::g3, std::declval<Type      &&>())) == 3, "");
     static_assert(sizeof(std::__invoke(&Type::g4, std::declval<Type const&&>())) == 4, "");
 #endif
 }
-#endif
