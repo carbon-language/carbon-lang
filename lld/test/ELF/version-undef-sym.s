f1e880eb8d267dbccdf14783fd1b53bc502c9295
Rafael Espindola <rafael.espindola@gmail.com>
2016-04-29 23:35:49 +0000

    Document how to recreate a binary.
    llvm-svn: 268136

--- lld/test/ELF/version-undef-sym.s
+++ lld/test/ELF/version-undef-sym.s
@@ -3,6 +3,27 @@
 // RUN: llvm-readobj --dyn-symbols %p/Inputs/version-undef-sym.so | FileCheck %s
 
 
+// Inputs/version-undef-sym.so consists of the assembly file
+//
+//         .global bar
+// bar:
+//        .weak abc1
+//        .weak abc2
+//        .weak abc3
+//        .weak abc4
+//        .weak abc5
+//
+// linked into a shared library with the version script
+//
+// VER_1 {
+// global:
+//   bar;
+// };
+//
+// Assuming we can reproduce the desired property (a few undefined symbols
+// before bar) we should create it with lld itself once it supports that.
+
+
 // Show that the input .so has undefined symbols before bar. That is what would
 // get our version parsing out of sync.
 
