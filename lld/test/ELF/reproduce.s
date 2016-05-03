d8dfdfd26868cc351c41c8608502fd533168a862
Rafael Espindola <rafael.espindola@gmail.com>
2016-05-03 14:29:42 +0000

    Don't depend on checking the response file when lld fails.
    llvm-svn: 268388

--- lld/test/ELF/reproduce.s
+++ lld/test/ELF/reproduce.s
@@ -26,18 +26,21 @@
 # RUN: ld.lld ./../../../foo.o -o bar -shared --as-needed --reproduce repro
 # RUN: diff %t.dir/build2/foo.o repro/%:t.dir/build2/foo.o
 
-# RUN: touch file
-# RUN: not ld.lld --reproduce repro2 'foo bar' -L"foo bar" -Lfile \
-# RUN:   --dynamic-list file -rpath file --script file --version-script file \
+# RUN: echo "{ local: *; };" >  ver
+# RUN: echo > dyn
+# RUN: echo > file
+# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o 'foo bar'
+# RUN: ld.lld --reproduce repro2 'foo bar' -L"foo bar" -Lfile \
+# RUN:   --dynamic-list dyn -rpath file --script file --version-script ver \
 # RUN:   --dynamic-linker "some unusual/path"
 # RUN: FileCheck %s --check-prefix=RSP2 < repro2/response.txt
-# RSP2:      "foo bar"
-# RSP2-NEXT: -L "foo bar"
+# RSP2:      "{{.*}}foo bar"
+# RSP2-NEXT: -L "{{.*}}foo bar"
 # RSP2-NEXT: -L {{.+}}file
-# RSP2-NEXT: --dynamic-list {{.+}}file
+# RSP2-NEXT: --dynamic-list {{.+}}dyn
 # RSP2-NEXT: -rpath {{.+}}file
 # RSP2-NEXT: --script {{.+}}file
-# RSP2-NEXT: --version-script {{.+}}file
+# RSP2-NEXT: --version-script {{.+}}ver
 # RSP2-NEXT: --dynamic-linker "some unusual/path"
 
 # RUN: not ld.lld build1/foo.o -o bar -shared --as-needed --reproduce . 2>&1 \
