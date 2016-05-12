eab80e585696e5c156048ead3dbb105c086a4414
Kamil Rytarowski <n54@gmx.com>
2016-05-12 19:46:08 +0000

    Turn on LLDB_EDITLINE_USE_WCHAR on NetBSD
    Summary: The editline(3) library in NetBSD is by default internally wide.
    
    Reviewers: clayborg, emaste
    
    Subscribers: joerg, lldb-commits
    
    Differential Revision: http://reviews.llvm.org/D20188
    
    llvm-svn: 269330

--- lldb/include/lldb/Host/Editline.h
+++ lldb/include/lldb/Host/Editline.h
@@ -27,11 +27,12 @@
 
 #include <sstream>
 #include <vector>
+#include <locale>
 
 // components needed to handle wide characters ( <codecvt>, codecvt_utf8, libedit built with '--enable-widec' )
-// are not consistenly available on non-OSX platforms.  The wchar_t versions of libedit functions will only be
+// are available on some platforms. The wchar_t versions of libedit functions will only be
 // used in cases where this is true.  This is a compile time dependecy, for now selected per target Platform
-#if defined (__APPLE__)
+#if defined (__APPLE__) || defined(__NetBSD__)
 #define LLDB_EDITLINE_USE_WCHAR 1
 #include <codecvt>
 #else
