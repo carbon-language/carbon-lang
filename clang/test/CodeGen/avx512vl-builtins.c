0f7ea93541a51d3a74ca42d46da6a478774fe26f
Craig Topper <craig.topper@gmail.com>
2016-05-15 22:18:00 +0000

    [X86] Add immediate range checks for many of the builtins.
    llvm-svn: 269619

--- clang/test/CodeGen/avx512vl-builtins.c
+++ clang/test/CodeGen/avx512vl-builtins.c
@@ -5090,97 +5090,97 @@ __m256i test_mm256_maskz_ternarylogic_epi64(__mmask8 __U, __m256i __A, __m256i _
 __m256 test_mm256_shuffle_f32x4(__m256 __A, __m256 __B) {
   // CHECK-LABEL: @test_mm256_shuffle_f32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.f32x4
-  return _mm256_shuffle_f32x4(__A, __B, 4); 
+  return _mm256_shuffle_f32x4(__A, __B, 3); 
 }
 
 __m256 test_mm256_mask_shuffle_f32x4(__m256 __W, __mmask8 __U, __m256 __A, __m256 __B) {
   // CHECK-LABEL: @test_mm256_mask_shuffle_f32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.f32x4
-  return _mm256_mask_shuffle_f32x4(__W, __U, __A, __B, 4); 
+  return _mm256_mask_shuffle_f32x4(__W, __U, __A, __B, 3); 
 }
 
 __m256 test_mm256_maskz_shuffle_f32x4(__mmask8 __U, __m256 __A, __m256 __B) {
   // CHECK-LABEL: @test_mm256_maskz_shuffle_f32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.f32x4
-  return _mm256_maskz_shuffle_f32x4(__U, __A, __B, 4); 
+  return _mm256_maskz_shuffle_f32x4(__U, __A, __B, 3); 
 }
 
 __m256d test_mm256_shuffle_f64x2(__m256d __A, __m256d __B) {
   // CHECK-LABEL: @test_mm256_shuffle_f64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.f64x2
-  return _mm256_shuffle_f64x2(__A, __B, 4); 
+  return _mm256_shuffle_f64x2(__A, __B, 3); 
 }
 
 __m256d test_mm256_mask_shuffle_f64x2(__m256d __W, __mmask8 __U, __m256d __A, __m256d __B) {
   // CHECK-LABEL: @test_mm256_mask_shuffle_f64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.f64x2
-  return _mm256_mask_shuffle_f64x2(__W, __U, __A, __B, 4); 
+  return _mm256_mask_shuffle_f64x2(__W, __U, __A, __B, 3); 
 }
 
 __m256d test_mm256_maskz_shuffle_f64x2(__mmask8 __U, __m256d __A, __m256d __B) {
   // CHECK-LABEL: @test_mm256_maskz_shuffle_f64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.f64x2
-  return _mm256_maskz_shuffle_f64x2(__U, __A, __B, 4); 
+  return _mm256_maskz_shuffle_f64x2(__U, __A, __B, 3); 
 }
 
 __m256i test_mm256_shuffle_i32x4(__m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_shuffle_i32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.i32x4
-  return _mm256_shuffle_i32x4(__A, __B, 4); 
+  return _mm256_shuffle_i32x4(__A, __B, 3); 
 }
 
 __m256i test_mm256_mask_shuffle_i32x4(__m256i __W, __mmask8 __U, __m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_mask_shuffle_i32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.i32x4
-  return _mm256_mask_shuffle_i32x4(__W, __U, __A, __B, 4); 
+  return _mm256_mask_shuffle_i32x4(__W, __U, __A, __B, 3); 
 }
 
 __m256i test_mm256_maskz_shuffle_i32x4(__mmask8 __U, __m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_maskz_shuffle_i32x4
   // CHECK: @llvm.x86.avx512.mask.shuf.i32x4
-  return _mm256_maskz_shuffle_i32x4(__U, __A, __B, 4); 
+  return _mm256_maskz_shuffle_i32x4(__U, __A, __B, 3); 
 }
 
 __m256i test_mm256_shuffle_i64x2(__m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_shuffle_i64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.i64x2
-  return _mm256_shuffle_i64x2(__A, __B, 4); 
+  return _mm256_shuffle_i64x2(__A, __B, 3); 
 }
 
 __m256i test_mm256_mask_shuffle_i64x2(__m256i __W, __mmask8 __U, __m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_mask_shuffle_i64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.i64x2
-  return _mm256_mask_shuffle_i64x2(__W, __U, __A, __B, 4); 
+  return _mm256_mask_shuffle_i64x2(__W, __U, __A, __B, 3); 
 }
 
 __m256i test_mm256_maskz_shuffle_i64x2(__mmask8 __U, __m256i __A, __m256i __B) {
   // CHECK-LABEL: @test_mm256_maskz_shuffle_i64x2
   // CHECK: @llvm.x86.avx512.mask.shuf.i64x2
-  return _mm256_maskz_shuffle_i64x2(__U, __A, __B, 4); 
+  return _mm256_maskz_shuffle_i64x2(__U, __A, __B, 3); 
 }
 
 __m128d test_mm_mask_shuffle_pd(__m128d __W, __mmask8 __U, __m128d __A, __m128d __B) {
   // CHECK-LABEL: @test_mm_mask_shuffle_pd
   // CHECK: @llvm.x86.avx512.mask.shuf.pd.128
-  return _mm_mask_shuffle_pd(__W, __U, __A, __B, 4); 
+  return _mm_mask_shuffle_pd(__W, __U, __A, __B, 3); 
 }
 
 __m128d test_mm_maskz_shuffle_pd(__mmask8 __U, __m128d __A, __m128d __B) {
   // CHECK-LABEL: @test_mm_maskz_shuffle_pd
   // CHECK: @llvm.x86.avx512.mask.shuf.pd.128
-  return _mm_maskz_shuffle_pd(__U, __A, __B, 4); 
+  return _mm_maskz_shuffle_pd(__U, __A, __B, 3); 
 }
 
 __m256d test_mm256_mask_shuffle_pd(__m256d __W, __mmask8 __U, __m256d __A, __m256d __B) {
   // CHECK-LABEL: @test_mm256_mask_shuffle_pd
   // CHECK: @llvm.x86.avx512.mask.shuf.pd.256
-  return _mm256_mask_shuffle_pd(__W, __U, __A, __B, 4); 
+  return _mm256_mask_shuffle_pd(__W, __U, __A, __B, 3); 
 }
 
 __m256d test_mm256_maskz_shuffle_pd(__mmask8 __U, __m256d __A, __m256d __B) {
   // CHECK-LABEL: @test_mm256_maskz_shuffle_pd
   // CHECK: @llvm.x86.avx512.mask.shuf.pd.256
-  return _mm256_maskz_shuffle_pd(__U, __A, __B, 4); 
+  return _mm256_maskz_shuffle_pd(__U, __A, __B, 3); 
 }
 
 __m128 test_mm_mask_shuffle_ps(__m128 __W, __mmask8 __U, __m128 __A, __m128 __B) {
