8ca5373c72564d81af98f9879d5cf4cffb2f08c4
Craig Topper <craig.topper@gmail.com>
2016-05-17 03:42:37 +0000

    [X86] Fix a few intrinsic tests to use the return type that matches the intrinsic they're testing.
    llvm-svn: 269735

--- clang/test/CodeGen/sse-builtins.c
+++ clang/test/CodeGen/sse-builtins.c
@@ -454,25 +454,25 @@ __m128d test_mm_cmpnge_pd(__m128d __a, __m128d __b) {
   return _mm_cmpnge_pd(__a, __b);
 }
 
-__m128 test_mm_slli_si128(__m128 a) {
+__m128i test_mm_slli_si128(__m128 a) {
   // CHECK-LABEL: @test_mm_slli_si128
   // CHECK: shufflevector <16 x i8> {{.*}}, <16 x i8> {{.*}}, <16 x i32> <i32 11, i32 12, i32 13, i32 14, i32 15, i32 16, i32 17, i32 18, i32 19, i32 20, i32 21, i32 22, i32 23, i32 24, i32 25, i32 26>
   return _mm_slli_si128(a, 5);
 }
 
-__m128 test_mm_bslli_si128(__m128 a) {
+__m128i test_mm_bslli_si128(__m128 a) {
   // CHECK-LABEL: @test_mm_bslli_si128
   // CHECK: shufflevector <16 x i8> {{.*}}, <16 x i8> {{.*}}, <16 x i32> <i32 11, i32 12, i32 13, i32 14, i32 15, i32 16, i32 17, i32 18, i32 19, i32 20, i32 21, i32 22, i32 23, i32 24, i32 25, i32 26>
   return _mm_bslli_si128(a, 5);
 }
 
-__m128 test_mm_srli_si128(__m128 a) {
+__m128i test_mm_srli_si128(__m128 a) {
   // CHECK-LABEL: @test_mm_srli_si128
   // CHECK: shufflevector <16 x i8> {{.*}}, <16 x i8> {{.*}}, <16 x i32> <i32 5, i32 6, i32 7, i32 8, i32 9, i32 10, i32 11, i32 12, i32 13, i32 14, i32 15, i32 16, i32 17, i32 18, i32 19, i32 20>
   return _mm_srli_si128(a, 5);
 }
 
-__m128 test_mm_bsrli_si128(__m128 a) {
+__m128i test_mm_bsrli_si128(__m128 a) {
   // CHECK-LABEL: @test_mm_bsrli_si128
   // CHECK: shufflevector <16 x i8> {{.*}}, <16 x i8> {{.*}}, <16 x i32> <i32 5, i32 6, i32 7, i32 8, i32 9, i32 10, i32 11, i32 12, i32 13, i32 14, i32 15, i32 16, i32 17, i32 18, i32 19, i32 20>
   return _mm_bsrli_si128(a, 5);
