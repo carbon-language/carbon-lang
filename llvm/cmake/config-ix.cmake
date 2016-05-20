6b58c4723a94c8e89cb91e8e83d9fb8047fee329
Chris Bieneman <beanz@apple.com>
2016-05-20 20:15:17 +0000

    Revert "Work around a glibc bug: backtrace() spuriously fails if..."
    This commit has been breaking the FreeBSD bots:
    http://lab.llvm.org:8011/builders/lld-x86_64-freebsd
    
    This reverts commit r269992.
    
    llvm-svn: 270267

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -144,7 +144,6 @@ endif()
 # function checks
 check_symbol_exists(arc4random "stdlib.h" HAVE_DECL_ARC4RANDOM)
 check_symbol_exists(backtrace "execinfo.h" HAVE_BACKTRACE)
-check_symbol_exists(_Unwind_Backtrace "unwind.h" HAVE_UNWIND_BACKTRACE)
 check_symbol_exists(getpagesize unistd.h HAVE_GETPAGESIZE)
 check_symbol_exists(getrusage sys/resource.h HAVE_GETRUSAGE)
 check_symbol_exists(setrlimit sys/resource.h HAVE_SETRLIMIT)
