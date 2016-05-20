f57a5905d37488111fef9c048e538a00df6ad1e9
Rui Ueyama <ruiu@google.com>
2016-05-20 21:39:07 +0000

    Simplify. NFC.
    llvm-svn: 270281

--- lld/ELF/OutputSections.cpp
+++ lld/ELF/OutputSections.cpp
@@ -269,11 +269,11 @@ PltSection<ELFT>::PltSection()
 }
 
 template <class ELFT> void PltSection<ELFT>::writeTo(uint8_t *Buf) {
-  size_t Off = 0;
   // At beginning of PLT, we have code to call the dynamic linker
   // to resolve dynsyms at runtime. Write such code.
   Target->writePltZero(Buf);
-  Off += Target->PltZeroSize;
+  size_t Off = Target->PltZeroSize;
+
   for (auto &I : Entries) {
     const SymbolBody *B = I.first;
     unsigned RelOff = I.second;
