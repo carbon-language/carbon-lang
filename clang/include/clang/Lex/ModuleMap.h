25c9125f11b6e0bbb944b377646b62c4a9b45c9c
Bob Wilson <bob.wilson@apple.com>
2016-05-23 01:52:50 +0000

    Fix typo in documentation comment.
    llvm-svn: 270391

--- clang/include/clang/Lex/ModuleMap.h
+++ clang/include/clang/Lex/ModuleMap.h
@@ -59,7 +59,7 @@ public:
   /// \brief Called when an umbrella header is added during module map parsing.
   ///
   /// \param FileMgr FileManager instance
-  /// \param Header The umbreall header to collect.
+  /// \param Header The umbrella header to collect.
   virtual void moduleMapAddUmbrellaHeader(FileManager *FileMgr,
                                           const FileEntry *Header) {}
 };
