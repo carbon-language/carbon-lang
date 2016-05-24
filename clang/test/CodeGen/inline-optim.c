9464491aa710a7dab1939420ea137effe4876592
Hans Wennborg <hans@hanshq.net>
2016-05-24 23:37:56 +0000

    Rename test/CodeGen/inline-optim.cc to .c and provide a triple
    llvm-svn: 270633

--- clang/test/CodeGen/inline-optim.c
+++ clang/test/CodeGen/inline-optim.c
@@ -1,8 +1,8 @@
 // Make sure -finline-functions family flags are behaving correctly.
 
-// RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck -check-prefix=NOINLINE %s
-// RUN: %clang_cc1 -O3 -fno-inline-functions -emit-llvm %s -o - | FileCheck -check-prefix=NOINLINE %s
-// RUN: %clang_cc1 -finline-functions -emit-llvm %s -o - | FileCheck -check-prefix=INLINE %s
+// RUN: %clang_cc1 -triple i686-pc-win32 -emit-llvm %s -o - | FileCheck -check-prefix=NOINLINE %s
+// RUN: %clang_cc1 -triple i686-pc-win32 -O3 -fno-inline-functions -emit-llvm %s -o - | FileCheck -check-prefix=NOINLINE %s
+// RUN: %clang_cc1 -triple i686-pc-win32 -finline-functions -emit-llvm %s -o - | FileCheck -check-prefix=INLINE %s
 
 inline int inline_hint(int a, int b) { return(a+b); }
 
