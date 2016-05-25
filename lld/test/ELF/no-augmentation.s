2e04361a21977061cadbb8441a2f8ee900883c48
Ed Maste <emaste@freebsd.org>
2016-05-25 15:14:08 +0000

    ELF: improve CIE no-augmentation test
    Add another possible error that may be reported for the same case. The
    original reproduction case that prompted r270706 produced the error
    "corrupted CIE" instead of "corrupted or unsupported CIE information".
    The specific error depends on arbitrary data later in the file so
    check that neither is emitted in case the input is ever changed.
    
    Document the process used to create the input .o and rename the test
    file to .s, as requested by Rafael.
    
    llvm-svn: 270709

--- lld/test/ELF/no-augmentation.s
+++ lld/test/ELF/no-augmentation.s
@@ -1,3 +1,9 @@
+// Input file generated on a mips64 device running FreeBSD 11-CURRENT
+// using the system compiler GCC 4.2.1, from no-augmentation.c containing:
+// int fn(int a) { return a + 1; }
+// with command:
+// cc -funwind-tables -g -O0 -c no-augmentation.c
+
 // RUN: llvm-mc -filetype=obj -triple=mips64-unknown-freebsd %s -o %t.o
 // RUN: ld.lld --eh-frame-hdr %t.o %p/Inputs/no-augmentation.o -o %t \
 // RUN:   | FileCheck -allow-empty %s
@@ -5,6 +11,7 @@
 // REQUIRES: mips
 
 // CHECK-NOT: corrupted or unsupported CIE information
+// CHECK-NOT: corrupted CIE
 
 .global __start
 __start:
