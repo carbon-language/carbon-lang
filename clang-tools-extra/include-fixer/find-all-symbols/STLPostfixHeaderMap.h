0e871f0b66438abaec77f22c05e3ec41dd34234c
Benjamin Kramer <benny.kra@googlemail.com>
2016-05-25 14:14:52 +0000

    Include local header with quotes instead of angle brackets.
    This works by accident because we pass '-I.'
    
    llvm-svn: 270701

--- clang-tools-extra/include-fixer/find-all-symbols/STLPostfixHeaderMap.h
+++ clang-tools-extra/include-fixer/find-all-symbols/STLPostfixHeaderMap.h
@@ -10,7 +10,7 @@
 #ifndef LLVM_CLANG_TOOLS_EXTRA_FIND_ALL_SYMBOLS_TOOL_STL_POSTFIX_HEADER_MAP_H
 #define LLVM_CLANG_TOOLS_EXTRA_FIND_ALL_SYMBOLS_TOOL_STL_POSTFIX_HEADER_MAP_H
 
-#include <HeaderMapCollector.h>
+#include "HeaderMapCollector.h"
 
 namespace clang {
 namespace find_all_symbols {
