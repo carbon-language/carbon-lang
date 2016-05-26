d1af08c3d6608a87d8b99a60ef297368367cd4b9
Akira Hatanaka <ahatanaka@apple.com>
2016-05-26 02:32:10 +0000

    Don't feed standard error to FileCheck.
    This is an attempt to fix the buildbot that started failing after
    r270808.
    
    http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux-fast/builds/13141 
    
    llvm-svn: 270817

--- clang/test/SemaObjC/property-atomic-bool.m
+++ clang/test/SemaObjC/property-atomic-bool.m
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple x86_64-apple-macosx10.10 -ast-dump "%s" 2>&1 | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-apple-macosx10.10 -ast-dump "%s" | FileCheck %s
 
 // CHECK: TypedefDecl {{.*}} referenced AtomicBool '_Atomic(_Bool)'
 // CHECK:  AtomicType {{.*}} '_Atomic(_Bool)'
