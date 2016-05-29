ab3e10a7a0ed91f251a3dbc4df3ecfa58cb8bef5
Rafael Espindola <rafael.espindola@gmail.com>
2016-05-29 02:36:16 +0000

    Mark test as requiring x86-registered-target.
    llvm-svn: 271163

--- clang/test/CodeGen/relax.c
+++ clang/test/CodeGen/relax.c
@@ -1,3 +1,4 @@
+// REQUIRES: x86-registered-target
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -emit-obj --mrelax-relocations %s -mrelocation-model pic -o %t
 // RUN: llvm-readobj -r %t | FileCheck  %s
 
