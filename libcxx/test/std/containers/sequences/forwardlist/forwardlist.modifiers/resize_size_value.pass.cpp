0ddc14c256058aeab504846f901f22a51a1bd1f8
Eric Fiselier <eric@efcs.ca>
2016-05-31 01:03:07 +0000

    Mark LWG issue #2585 as complete
    llvm-svn: 271240

--- libcxx/test/std/containers/sequences/forwardlist/forwardlist.modifiers/resize_size_value.pass.cpp
+++ libcxx/test/std/containers/sequences/forwardlist/forwardlist.modifiers/resize_size_value.pass.cpp
@@ -14,8 +14,13 @@
 #include <forward_list>
 #include <cassert>
 
+#include "test_macros.h"
 #include "DefaultOnly.h"
 #include "min_allocator.h"
+
+#if TEST_STD_VER >= 11
+#include "container_test_types.h"
+#endif
 
 int main()
 {
@@ -49,7 +54,7 @@ int main()
         assert(*next(c.begin(), 4) == 10);
         assert(*next(c.begin(), 5) == 10);
     }
-#if __cplusplus >= 201103L
+#if TEST_STD_VER >= 11
     {
         typedef int T;
         typedef std::forward_list<T, min_allocator<T>> C;
@@ -79,6 +84,20 @@ int main()
         assert(*next(c.begin(), 3) == 10);
         assert(*next(c.begin(), 4) == 10);
         assert(*next(c.begin(), 5) == 10);
+    }
+    {
+        // Test that the allocator's construct method is being used to
+        // construct the new elements and that it's called exactly N times.
+        typedef int T;
+        typedef std::forward_list<int, ContainerTestAllocator<int, int>> Container;
+        ConstructController* cc = getConstructController();
+        cc->reset();
+        {
+            Container c;
+            cc->expect<int const&>(6);
+            c.resize(6, 42);
+            assert(!cc->unchecked());
+        }
     }
 #endif
 }
