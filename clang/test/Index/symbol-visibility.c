3a562e6f703a0eaeee7db3674baf7876ed954f91
Ehsan Akhgari <ehsan.akhgari@gmail.com>
2016-05-31 15:39:10 +0000

    Revert r253909 because it was committed with an incorrect message
    llvm-svn: 271291

--- clang/test/Index/symbol-visibility.c
+++
@@ -1,7 +0,0 @@
-// RUN: c-index-test -test-print-visibility %s | FileCheck %s
-
-__attribute__ ((visibility ("default"))) void foo1();
-__attribute__ ((visibility ("hidden"))) void foo2();
-
-// CHECK: FunctionDecl=foo1:3:47visibility=Default
-// CHECK: FunctionDecl=foo2:4:46visibility=Hidden
