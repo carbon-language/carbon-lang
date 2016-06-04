79abce39bb851b7a865a4cb37a1e4b9fa467ebc6
Chandler Carruth <chandlerc@gmail.com>
2016-06-04 06:16:49 +0000

    Temporary hack to this test to try and get the sanitizer-windows bot to show enough information for me to fix the patterns used here.
    llvm-svn: 271779

--- compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cc
+++ compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cc
@@ -20,7 +20,15 @@ int main() {
 // CHECK:   {{#0 .* main .*}}intercept_strdup.cc:[[@LINE-3]]
 // CHECK: [[ADDR]] is located 1 bytes to the left of 6-byte region
 // CHECK: allocated by thread T0 here:
-// CHECK:   {{#[0-9] .*strdup}}
-// CHECK:   {{#[0-9] .* main .*}}intercept_strdup.cc:[[@LINE-15]]
+// CHECK:   #0 {{.*strdup}}
+// CHECK:   #1 {{.* main .*}}intercept_strdup.cc:[[@LINE-15]]
+// CHECK:   #2 {{.*foobar}}
+// CHECK:   #3 {{.*foobar}}
+// CHECK:   #4 {{.*foobar}}
+// CHECK:   #5 {{.*foobar}}
+// CHECK:   #6 {{.*foobar}}
+// CHECK:   #7 {{.*foobar}}
+// CHECK:   #8 {{.*foobar}}
+// CHECK:   #9 {{.*foobar}}
   free(ptr);
 }
