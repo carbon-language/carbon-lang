4f785dc1f675a341f7b65d10e9c83fe3bcb526ce
Chandler Carruth <chandlerc@gmail.com>
2016-06-04 08:09:24 +0000

    I didn't actually get this right, but now I have frame #0 and frame #2. This should fail on frame #1 and show what that should actually be.
    llvm-svn: 271783

--- compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cc
+++ compiler-rt/test/asan/TestCases/Windows/intercept_strdup.cc
@@ -20,7 +20,8 @@ int main() {
 // CHECK:   {{#0 .* main .*}}intercept_strdup.cc:[[@LINE-3]]
 // CHECK: [[ADDR]] is located 1 bytes to the left of 6-byte region
 // CHECK: allocated by thread T0 here:
-// CHECK:   #0 {{.*strdup}}
-// CHECK:   #1 {{.* main .*}}intercept_strdup.cc:[[@LINE-15]]
+// CHECK:   #0 {{.*}} in malloc
+// CHECK:   #1 {{.*FIXME}}
+// CHECK:   #2 {{.* main .*}}intercept_strdup.cc:[[@LINE-15]]
   free(ptr);
 }
