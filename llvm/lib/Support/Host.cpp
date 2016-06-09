d665b410c6e0d0c8960d23bcb730332057f5e0e8
Alina Sbirlea <asbirlea@google.com>
2016-06-09 23:04:15 +0000

    Reapply 272328 and 272329 as a single patch.
    [cpu-detection] [amdfam10] Return barcelona, and amdfam10 for all other
    subtypes. Address Bug 28067.
    
    Along with the refactoring of Host.cpp, getHostCPUName() was modified to
    return more precise types for CPUs in amdfam10.
    However, callers of getHostCPUName() do string matching on type, so this
    cannot be modified.
    Currently there is support in the x86 backend for barcelona.
    For all other subtypes the assumed return value is amdfam10.
    
    Fix: getHostCPUName() returns barcelona subtype and amdfam10 for all
    others. This can be extended further when support for the other subtypes
    is added.
    
    Differential revision: http://reviews.llvm.org/D21193
    
    llvm-svn: 272333

--- llvm/lib/Support/Host.cpp
+++ llvm/lib/Support/Host.cpp
@@ -869,16 +869,9 @@ StringRef sys::getHostCPUName() {
         return "athlon";
       }
     case AMDFAM10H:
-      switch (Subtype) {
-      case AMDFAM10H_BARCELONA:
-        return "amdfam10-barcelona";
-      case AMDFAM10H_SHANGHAI:
-        return "amdfam10-shanghai";
-      case AMDFAM10H_ISTANBUL:
-        return "amdfam10-istanbul";
-      default:
-        return "amdfam10";
-      }
+      if(Subtype == AMDFAM10H_BARCELONA)
+        return "barcelona";
+      return "amdfam10";
     case AMDFAM14H:
       return "btver1";
     case AMDFAM15H:
