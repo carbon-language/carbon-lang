fbe79c9d250cfa98fd3285009317b48150d1f1cf
Eric Fiselier <eric@efcs.ca>
2016-06-14 01:36:15 +0000

    Remove _LIBCPP_TRIVIAL_PAIR_COPY_CTOR option.
    llvm-svn: 272613

--- libcxx/test/std/utilities/utility/pairs/pairs.pair/move_ctor.pass.cpp
+++ libcxx/test/std/utilities/utility/pairs/pairs.pair/move_ctor.pass.cpp
@@ -7,33 +7,38 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: c++98, c++03
+
 // <utility>
 
 // template <class T1, class T2> struct pair
 
-// pair(const pair&) = default;
+// pair(pair&&) = default;
 
 #include <utility>
+#include <memory>
 #include <cassert>
 
 #include "test_macros.h"
+
+struct Dummy {
+  Dummy(Dummy const&) = delete;
+  Dummy(Dummy &&) = default;
+};
 
 int main()
 {
     {
         typedef std::pair<int, short> P1;
+        static_assert(std::is_move_constructible<P1>::value, "");
         P1 p1(3, 4);
-        P1 p2 = p1;
+        P1 p2 = std::move(p1);
         assert(p2.first == 3);
         assert(p2.second == 4);
     }
-#if TEST_STD_VER > 11
     {
-        typedef std::pair<int, short> P1;
-        constexpr P1 p1(3, 4);
-        constexpr P1 p2 = p1;
-        static_assert(p2.first == 3, "");
-        static_assert(p2.second == 4, "");
+        using P = std::pair<Dummy, int>;
+        static_assert(!std::is_copy_constructible<P>::value, "");
+        static_assert(std::is_move_constructible<P>::value, "");
     }
-#endif
 }
