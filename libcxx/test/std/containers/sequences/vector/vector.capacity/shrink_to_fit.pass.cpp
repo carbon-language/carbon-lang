5d3e09ecc102224bd346c8437735f9c98a8b29c2
Eric Fiselier <eric@efcs.ca>
2016-06-15 01:44:22 +0000

    Improve portability of vector tests. Patch from STL@microsoft.com
    llvm-svn: 272745

--- libcxx/test/std/containers/sequences/vector/vector.capacity/shrink_to_fit.pass.cpp
+++ libcxx/test/std/containers/sequences/vector/vector.capacity/shrink_to_fit.pass.cpp
@@ -43,7 +43,7 @@ int main()
         v.push_back(1);
         assert(is_contiguous_container_asan_correct(v));
         v.shrink_to_fit();
-        assert(v.capacity() == 200);
+        LIBCPP_ASSERT(v.capacity() == 200); // assumes libc++'s 2x growth factor
         assert(v.size() == 101);
         assert(is_contiguous_container_asan_correct(v));
     }
