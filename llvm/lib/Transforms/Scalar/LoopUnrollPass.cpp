4a697c312f901a3a1ec92e24c91cefd64f6b4346
David Majnemer <david.majnemer@gmail.com>
2016-06-15 00:19:56 +0000

    [LoopUnroll] Don't crash trying to unroll loop with EH pad exit
    We do not support splitting cleanuppad or catchswitches.  This is
    problematic for passes which assume that a loop is in loop simplify
    form (the loop would have a dedicated exit block instead of sharing it).
    
    While it isn't great that we don't support this for cleanups, we still
    cannot make loop-simplify form an assertable precondition because
    indirectbr will also disable these sorts of CFG cleanups.
    
    This fixes PR28132.
    
    llvm-svn: 272739

--- llvm/lib/Transforms/Scalar/LoopUnrollPass.cpp
+++ llvm/lib/Transforms/Scalar/LoopUnrollPass.cpp
@@ -922,6 +922,11 @@ static bool tryToUnrollLoop(Loop *L, DominatorTree &DT, LoopInfo *LI,
     DEBUG(dbgs() << "  Not unrolling loop with inlinable calls.\n");
     return false;
   }
+  if (!L->isLoopSimplifyForm()) {
+    DEBUG(
+        dbgs() << "  Not unrolling loop which is not in loop-simplify form.\n");
+    return false;
+  }
 
   // Find trip count and trip multiple if count is not available
   unsigned TripCount = 0;
