00cfe279c11b83ca956ac661121db499237d2da8
Haojian Wu <hokein@google.com>
2016-06-16 13:27:02 +0000

    A follow-up fixing on cuda-march.cu: Don't match clang to other place.
    llvm-svn: 272890

--- clang/test/Driver/cuda-march.cu
+++ clang/test/Driver/cuda-march.cu
@@ -6,19 +6,17 @@
 // REQUIRES: x86-registered-target
 // REQUIRES: nvptx-registered-target
 
-// RUN: %clang -### -target x86_64-linux-gnu -c -march=haswell %s 2>&1 | FileCheck %s
+// RUN: %clang -no-canonical-prefixes -### -target x86_64-linux-gnu -c \
+// RUN: -march=haswell %s 2>&1 | FileCheck %s
+// RUN: %clang -no-canonical-prefixes -### -target x86_64-linux-gnu -c \
+// RUN: -march=haswell --cuda-gpu-arch=sm_20 %s 2>&1 | FileCheck %s
 
-// RUN: %clang -### -target x86_64-linux-gnu -c -march=haswell --cuda-gpu-arch=sm_20 %s 2>&1 | \
-// RUN: FileCheck %s
-
-// CHECK: bin{{/|\\+}}clang
-// CHECK: "-cc1"
+// CHECK: {{.*}}clang{{.*}}" "-cc1"
 // CHECK-SAME: "-triple" "nvptx
 // CHECK-SAME: "-target-cpu" "sm_20"
 
 // CHECK: ptxas
 // CHECK-SAME: "--gpu-name" "sm_20"
 
-// CHECK: bin{{/|\\+}}clang
-// CHECK-SAME: "-cc1"
+// CHECK: {{.*}}clang{{.*}}" "-cc1"
 // CHECK-SAME: "-target-cpu" "haswell"
