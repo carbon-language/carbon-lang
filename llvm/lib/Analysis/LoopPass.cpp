f3b71581dd9b56cca44e80a7b595d2b8a641d8f6
Eli Friedman <eli.friedman@gmail.com>
2016-06-20 02:48:11 +0000

    Fix dynamically linked debug builds.
    On the surface, this might not look like it does anything... but
    actually it brings in the declaration "extern template class
    AnalysisManager<Loop>;", which suppresses the instantiation of the
    constructor, which avoids the funny interaction between "extern
    template" and -fvisibility-inlines-hidden.
    
    llvm-svn: 273133

--- llvm/lib/Analysis/LoopPass.cpp
+++ llvm/lib/Analysis/LoopPass.cpp
@@ -14,6 +14,7 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Analysis/LoopPass.h"
+#include "llvm/Analysis/LoopPassManager.h"
 #include "llvm/IR/IRPrintingPasses.h"
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/OptBisect.h"
