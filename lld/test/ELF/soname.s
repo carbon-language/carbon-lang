a074fe41bfc5729249e3805816327a8fbaecb0e9
Davide Italiano <davide@freebsd.org>
2016-06-20 17:58:09 +0000

    [ELF] Accept --soname= (two dashes).
    Update an existing test to exercise the new functionality, while
    I'm here.
    
    llvm-svn: 273175

--- lld/test/ELF/soname.s
+++ lld/test/ELF/soname.s
@@ -1,6 +1,6 @@
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
 // RUN: ld.lld %t.o -shared -soname=bar -o %t.so
-// RUN: ld.lld %t.o -shared -soname=bar -o %t2.so
+// RUN: ld.lld %t.o -shared --soname=bar -o %t2.so
 // RUN: ld.lld %t.o %t.so %t2.so -o %t
 // RUN: llvm-readobj --dynamic-table %t | FileCheck %s
 
