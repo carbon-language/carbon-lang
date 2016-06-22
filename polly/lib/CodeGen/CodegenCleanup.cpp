616449df6d5491fc8559e109c45bc9aeca7f083a
Tobias Grosser <tobias@grosser.es>
2016-06-22 16:29:28 +0000

    Add missing copyright header
    This cleanup was suggested by Eugene Zelenko <eugene.zelenko@gmail.com> in
    http://reviews.llvm.org/D21488 and was split out to increase readability.
    
    llvm-svn: 273436

--- polly/lib/CodeGen/CodegenCleanup.cpp
+++ polly/lib/CodeGen/CodegenCleanup.cpp
@@ -1,3 +1,12 @@
+//===- CodegenCleanup.cpp -------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #include "polly/CodeGen/CodegenCleanup.h"
 
 #include "llvm/Analysis/CFLAliasAnalysis.h"
