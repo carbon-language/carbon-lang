94713d1671cddcb1e0c94209e9d783babc73c0e1
Eric Fiselier <eric@efcs.ca>
2016-06-22 05:33:52 +0000

    Support old GCC exception and rtti detection macros
    llvm-svn: 273383

--- libcxx/test/support/test_macros.h
+++ libcxx/test/support/test_macros.h
@@ -71,11 +71,13 @@
 #define TEST_NOEXCEPT
 #endif
 
-#if !TEST_HAS_FEATURE(cpp_rtti) && !defined(__cpp_rtti)
+#if !TEST_HAS_FEATURE(cpp_rtti) && !defined(__cpp_rtti) \
+    && !defined(__GXX_RTTI)
 #define TEST_HAS_NO_RTTI
 #endif
 
-#if !TEST_HAS_FEATURE(cpp_exceptions) && !defined(__cpp_exceptions)
+#if !TEST_HAS_FEATURE(cpp_exceptions) && !defined(__cpp_exceptions) \
+     && !defined(__EXCEPTIONS)
 #define TEST_HAS_NO_EXCEPTIONS
 #endif
 
