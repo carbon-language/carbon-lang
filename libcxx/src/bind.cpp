555330719a31dd4294741493781bebe41cd6afbb
Eric Fiselier <eric@efcs.ca>
2016-06-26 21:01:34 +0000

    Implement LWG 2488 - Make the placeholders constexpr.
    This patch makes the bind placeholders in std::placeholders both (1) const and
    (2) constexpr (See below).
    
    This is technically a breaking change for any code using the placeholders
    outside of std::bind and depending on them being non-const. However I don't
    think this will break any real world code.
    
    (1) Previously the placeholders were non-const extern globals in all
    dialects. This patch changes these extern globals to be const in all dialects.
    Since the cv-qualifiers don't participate in name mangling for globals this
    is an ABI compatible change.
    
    (2) Make the placeholders constexpr in C++11 and beyond. Although LWG 2488 only
    applies to C++17 I don't see any reason not to backport this change.
    
    llvm-svn: 273824

--- libcxx/src/bind.cpp
+++ libcxx/src/bind.cpp
@@ -7,6 +7,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#define _LIBCPP_BUILDING_BIND
 #include "functional"
 
 _LIBCPP_BEGIN_NAMESPACE_STD
@@ -14,16 +15,16 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 namespace placeholders
 {
 
-__ph<1>   _1;
-__ph<2>   _2;
-__ph<3>   _3;
-__ph<4>   _4;
-__ph<5>   _5;
-__ph<6>   _6;
-__ph<7>   _7;
-__ph<8>   _8;
-__ph<9>   _9;
-__ph<10> _10;
+const __ph<1>   _1{};
+const __ph<2>   _2{};
+const __ph<3>   _3{};
+const __ph<4>   _4{};
+const __ph<5>   _5{};
+const __ph<6>   _6{};
+const __ph<7>   _7{};
+const __ph<8>   _8{};
+const __ph<9>   _9{};
+const __ph<10> _10{};
 
 }  // placeholders
 
--- libcxx/test/std/utilities/function.objects/bind/func.bind/func.bind.place/placeholders.pass.cpp
+++ libcxx/test/std/utilities/function.objects/bind/func.bind/func.bind.place/placeholders.pass.cpp
@@ -10,9 +10,13 @@
 // <functional>
 
 // placeholders
+// The placeholders are constexpr in C++17 and beyond.
+// libc++ provides constexpr placeholders in C++11 and beyond.
 
 #include <functional>
 #include <type_traits>
+
+#include "test_macros.h"
 
 template <class T>
 void
@@ -27,6 +31,30 @@ test(const T& t)
     static_assert(std::is_nothrow_copy_constructible<T>::value, "");
     static_assert(std::is_nothrow_move_constructible<T>::value, "");
 }
+
+#if TEST_STD_VER >= 11
+constexpr decltype(std::placeholders::_1)  default1{};
+constexpr decltype(std::placeholders::_2)  default2{};
+constexpr decltype(std::placeholders::_3)  default3{};
+constexpr decltype(std::placeholders::_4)  default4{};
+constexpr decltype(std::placeholders::_5)  default5{};
+constexpr decltype(std::placeholders::_6)  default6{};
+constexpr decltype(std::placeholders::_7)  default7{};
+constexpr decltype(std::placeholders::_8)  default8{};
+constexpr decltype(std::placeholders::_9)  default9{};
+constexpr decltype(std::placeholders::_10) default10{};
+
+constexpr decltype(std::placeholders::_1)  cp1 = std::placeholders::_1;
+constexpr decltype(std::placeholders::_2)  cp2 = std::placeholders::_2;
+constexpr decltype(std::placeholders::_3)  cp3 = std::placeholders::_3;
+constexpr decltype(std::placeholders::_4)  cp4 = std::placeholders::_4;
+constexpr decltype(std::placeholders::_5)  cp5 = std::placeholders::_5;
+constexpr decltype(std::placeholders::_6)  cp6 = std::placeholders::_6;
+constexpr decltype(std::placeholders::_7)  cp7 = std::placeholders::_7;
+constexpr decltype(std::placeholders::_8)  cp8 = std::placeholders::_8;
+constexpr decltype(std::placeholders::_9)  cp9 = std::placeholders::_9;
+constexpr decltype(std::placeholders::_10) cp10 = std::placeholders::_10;
+#endif // TEST_STD_VER >= 11
 
 int main()
 {
