bb94dca13a096a187c66f594d7c802a29bf5fb69
Eric Fiselier <eric@efcs.ca>
2016-06-26 22:56:26 +0000

    Fix PR28079 - std::wstring_convert move constructor broken.
    The move constructor for wstring_convert accidentally copied the state member
    into the converted count member in the move constructor. This patch fixes
    the typo.
    
    While working on this I discovered that wstring_convert doesn't actually
    provide a move constructor according to the standard and therefore this
    constructor is a libc++ extension. I'll look further into whether libc++ should
    provide this constructor at all. Neither libstdc++ or MSVC's STL provide it.
    
    llvm-svn: 273831

---
+++ libcxx/test/std/localization/locales/locale.convenience/conversions/conversions.string/ctor_copy.pass.cpp
@@ -0,0 +1,27 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// <locale>
+
+// wstring_convert<Codecvt, Elem, Wide_alloc, Byte_alloc>
+
+// wstring_convert(wstring_convert const&) = delete;
+// wstring_convert& operator=(wstring_convert const&) = delete;
+
+#include <locale>
+#include <codecvt>
+#include <cassert>
+
+int main()
+{
+    typedef std::codecvt_utf8<wchar_t> Codecvt;
+    typedef std::wstring_convert<Codecvt> Myconv;
+    static_assert(!std::is_copy_constructible<Myconv>::value, "");
+    static_assert(!std::is_copy_assignable<Myconv>::value, "");
+}
