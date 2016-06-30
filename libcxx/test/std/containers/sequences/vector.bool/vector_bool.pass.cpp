3b0184321fd2369497d4d3269000175f9985cbbb
Eric Fiselier <eric@efcs.ca>
2016-06-30 04:28:12 +0000

    Fix unary_function inheritance assumption. Patch from STL@microsoft.com
    llvm-svn: 274205

--- libcxx/test/std/containers/sequences/vector.bool/vector_bool.pass.cpp
+++ libcxx/test/std/containers/sequences/vector.bool/vector_bool.pass.cpp
@@ -41,8 +41,8 @@ int main()
     {
     typedef std::vector<bool, min_allocator<bool>> T;
     typedef std::hash<T> H;
-    static_assert((std::is_base_of<std::unary_function<T, std::size_t>,
-                                   H>::value), "");
+    static_assert((std::is_same<H::argument_type, T>::value), "" );
+    static_assert((std::is_same<H::result_type, std::size_t>::value), "" );
     bool ba[] = {true, false, true, true, false};
     T vb(std::begin(ba), std::end(ba));
     H h;
