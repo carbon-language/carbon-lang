4357f021e3dcc2c5eb951f0eb82c1045990c32f8
Kuba Brecka <kuba.brecka@gmail.com>
2016-07-01 12:55:36 +0000

    [tsan] Relax the "ignored-interceptors.mm" testcase.  The test has been flaky because it's detecting a false positive race (coming from a system library) and sometimes that race is detected after we're printing "Done".
    llvm-svn: 274346

--- compiler-rt/test/tsan/Darwin/ignored-interceptors.mm
+++ compiler-rt/test/tsan/Darwin/ignored-interceptors.mm
@@ -6,7 +6,7 @@
 // RUN: %clang_tsan %s -o %t -framework Foundation
 
 // Check that without the flag, there are false positives.
-// RUN: %deflake %run %t 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=CHECK-RACE
+// RUN: %deflake %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-RACE
 
 // With ignore_interceptors_accesses=1, no races are reported.
 // RUN: %env_tsan_opts=ignore_interceptors_accesses=1 %run %t 2>&1 | FileCheck %s
