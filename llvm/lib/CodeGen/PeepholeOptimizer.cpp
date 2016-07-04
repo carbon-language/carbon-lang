e617adea12927a578fa551f3c2c74c9a2494a62d
Eric Liu <ioeric@google.com>
2016-07-04 12:10:08 +0000

    Fixed warning caused by r274402.
    llvm-svn: 274497

--- llvm/lib/CodeGen/PeepholeOptimizer.cpp
+++ llvm/lib/CodeGen/PeepholeOptimizer.cpp
@@ -1886,11 +1886,11 @@ ValueTrackerResult ValueTracker::getNextSourceFromPHI() {
 ValueTrackerResult ValueTracker::getNextSourceImpl() {
   assert(Def && "This method needs a valid definition");
 
-  assert(
-      (Def->getOperand(DefIdx).isDef() &&
-       (DefIdx < Def->getDesc().getNumDefs() || Def->getDesc().isVariadic()) ||
-       Def->getOperand(DefIdx).isImplicit()) &&
-       "Invalid DefIdx");
+  assert(((Def->getOperand(DefIdx).isDef() &&
+           (DefIdx < Def->getDesc().getNumDefs() ||
+            Def->getDesc().isVariadic())) ||
+          Def->getOperand(DefIdx).isImplicit()) &&
+         "Invalid DefIdx");
   if (Def->isCopy())
     return getNextSourceFromCopy();
   if (Def->isBitcast())
