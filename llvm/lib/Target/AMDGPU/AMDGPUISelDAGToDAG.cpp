17a0ec540083a11bd739984093ae04291ca1a989
Tom Stellard <thomas.stellard@amd.com>
2016-07-04 20:41:48 +0000

    AMDGPU/SI: Remove hack for selecting < 32-bit loads to MUBUF instructions
    Summary:
    The isGlobalLoad() query was returning true for constant address space loads
    with memory types less than 32-bits, which is wrong.  This logic has been
    replaced with PatFrag in the TableGen files, to provide the same functionality.
    
    Reviewers: arsenm
    
    Subscribers: arsenm, kzhuravl, llvm-commits
    
    Differential Revision: http://reviews.llvm.org/D21696
    
    llvm-svn: 274521

--- llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.cpp
@@ -533,10 +533,6 @@ bool AMDGPUDAGToDAGISel::isGlobalLoad(const MemSDNode *N) const {
     if (Subtarget->getGeneration() < AMDGPUSubtarget::SOUTHERN_ISLANDS)
       return !isa<GlobalValue>(GetUnderlyingObject(
           N->getMemOperand()->getValue(), CurDAG->getDataLayout()));
-
-    //TODO: Why do we need this?
-    if (N->getMemoryVT().bitsLT(MVT::i32))
-      return true;
   }
 
   return checkType(N->getMemOperand()->getValue(), AMDGPUAS::GLOBAL_ADDRESS);
