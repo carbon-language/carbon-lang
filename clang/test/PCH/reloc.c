2976e014fcf88e14010c691304452a046dc504b2
Justin Bogner <mail@justinbogner.com>
2016-07-05 16:06:12 +0000

    test: Use add_lit_testsuites so that subsets of tests can be specified
    This creates make/ninja targets like check-clang-codegen and
    check-clang-unit, much like LLVM already has. I had to move some input
    files into Input directories so they weren't picked up as test
    directories.
    
    llvm-svn: 274560

--- clang/test/PCH/reloc.c
+++ clang/test/PCH/reloc.c
@@ -1,7 +1,7 @@
 // RUN: %clang -target x86_64-apple-darwin10 --relocatable-pch -o %t \
-// RUN:   -isysroot %S/libroot %S/libroot/usr/include/reloc.h
+// RUN:   -isysroot %S/Inputs/libroot %S/Inputs/libroot/usr/include/reloc.h
 // RUN: %clang -target x86_64-apple-darwin10 -fsyntax-only \
-// RUN:   -include-pch %t -isysroot %S/libroot %s -Xclang -verify
+// RUN:   -include-pch %t -isysroot %S/Inputs/libroot %s -Xclang -verify
 // RUN: not %clang -target x86_64-apple-darwin10 -include-pch %t %s
 // REQUIRES: x86-registered-target
 
@@ -11,5 +11,5 @@ int x = 2; // expected-error{{redefinition}}
 int y = 5; // expected-error{{redefinition}}
 
 
-// expected-note@libroot/usr/include/reloc.h:13{{previous definition}}
-// expected-note@libroot/usr/include/reloc2.h:14{{previous definition}}
+// expected-note@Inputs/libroot/usr/include/reloc.h:13{{previous definition}}
+// expected-note@Inputs/libroot/usr/include/reloc2.h:14{{previous definition}}
