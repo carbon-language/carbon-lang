d35f706cc2849c82e6095ce93c400809771d5001
Justin Lebar <jlebar@google.com>
2016-07-12 23:23:01 +0000

    [CUDA] Don't assume that destructors can't be overloaded.
    Summary:
    You can overload a destructor in CUDA, and SemaOverload needs to be
    tweaked not to crash when it sees an explicit call to an overloaded
    destructor.
    
    Reviewers: rsmith
    
    Subscribers: cfe-commits, tra
    
    Differential Revision: http://reviews.llvm.org/D21912
    
    llvm-svn: 275231

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -12407,8 +12407,7 @@ Sema::BuildCallToMemberFunction(Scope *S, Expr *MemExprE,
   if (CXXDestructorDecl *DD =
           dyn_cast<CXXDestructorDecl>(TheCall->getMethodDecl())) {
     // a->A::f() doesn't go through the vtable, except in AppleKext mode.
-    bool CallCanBeVirtual = !cast<MemberExpr>(NakedMemExpr)->hasQualifier() ||
-                            getLangOpts().AppleKext;
+    bool CallCanBeVirtual = !MemExpr->hasQualifier() || getLangOpts().AppleKext;
     CheckVirtualDtorCall(DD, MemExpr->getLocStart(), /*IsDelete=*/false,
                          CallCanBeVirtual, /*WarnOnNonAbstractTypes=*/true,
                          MemExpr->getMemberLoc());
