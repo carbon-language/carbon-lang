00dc68dff6ccf18295964af3daf10354ceb7cc76
Tim Northover <tnorthover@apple.com>
2016-07-12 17:38:50 +0000

    AArch64: fix return type of vqmovun_high_*.
    These should be returning an unsigned quantity.
    
    llvm-svn: 275195

---
+++ clang/test/Sema/arm64-neon-header.c
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 -triple arm64-apple-darwin -target-feature +neon -Wvector-conversion -fsyntax-only -ffreestanding -verify %s
+
+#include <arm_neon.h>
+
+int16x8_t foo(int8x8_t p0, int16x8_t p1) {
+  return vqmovun_high_s16(p0, p1); // expected-warning {{incompatible vector types returning 'uint8x16_t'}}
+}
