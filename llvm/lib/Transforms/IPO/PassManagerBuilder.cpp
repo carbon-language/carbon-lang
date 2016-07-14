755cd760cdf8a6e3e1b7d15adce8fd811a3404b6
Nico Weber <nicolasweber@gmx.de>
2016-07-14 14:41:25 +0000

    Revert r275401, it caused PR28551.
    llvm-svn: 275420

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -223,7 +223,6 @@ void PassManagerBuilder::populateFunctionPassManager(
   FPM.add(createCFGSimplificationPass());
   FPM.add(createSROAPass());
   FPM.add(createEarlyCSEPass());
-  FPM.add(createGVNHoistPass());
   FPM.add(createLowerExpectIntrinsicPass());
 }
 
