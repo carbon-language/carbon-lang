9377a7b6a811cd0d55c100cc693ea6f46ceeac75
Pavel Labath <labath@google.com>
2016-07-15 12:19:28 +0000

    Fix TestDarwinNSLogOutput for windows
    pexpect python package does not exist on windows
    
    llvm-svn: 275555

--- lldb/packages/Python/lldbsuite/test/macosx/nslog/TestDarwinNSLogOutput.py
+++ lldb/packages/Python/lldbsuite/test/macosx/nslog/TestDarwinNSLogOutput.py
@@ -10,7 +10,6 @@ from __future__ import print_function
 
 import lldb
 import os
-import pexpect
 import platform
 import re
 import sys
@@ -58,6 +57,7 @@ class DarwinNSLogOutputTestCase(lldbtest.TestBase):
         prompt = self.child_prompt
 
         # So that the child gets torn down after the test.
+        import pexpect
         self.child = pexpect.spawn('%s %s %s' % (lldbtest_config.lldbExec,
                                                  self.lldbOption, exe))
         child = self.child
