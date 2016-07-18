eea0737a34674c3e19ed88447fdd0e5a310ac17b
Devin Coughlin <dcoughlin@apple.com>
2016-07-18 17:23:30 +0000

    [analyzer] Add checker modeling potential C++ self-assignment
    This checker checks copy and move assignment operators whether they are
    protected against self-assignment. Since C++ core guidelines discourages
    explicit checking for `&rhs==this` in general we take a different approach: in
    top-frame analysis we branch the exploded graph for two cases, where &rhs==this
    and &rhs!=this and let existing checkers (e.g. unix.Malloc) do the rest of the
    work. It is important that we check all copy and move assignment operator in top
    frame even if we checked them already since self-assignments may happen
    undetected even in the same translation unit (e.g. using random indices for an
    array what may or may not be the same).
    
    A patch by Ádám Balogh!
    
    Differential Revision: https://reviews.llvm.org/D19311
    
    llvm-svn: 275820

--- clang/lib/StaticAnalyzer/Frontend/AnalysisConsumer.cpp
+++ clang/lib/StaticAnalyzer/Frontend/AnalysisConsumer.cpp
@@ -431,6 +431,13 @@ static bool shouldSkipFunction(const Decl *D,
   //   Count naming convention errors more aggressively.
   if (isa<ObjCMethodDecl>(D))
     return false;
+  // We also want to reanalyze all C++ copy and move assignment operators to
+  // separately check the two cases where 'this' aliases with the parameter and
+  // where it may not. (cplusplus.SelfAssignmentChecker)
+  if (const auto *MD = dyn_cast<CXXMethodDecl>(D)) {
+    if (MD->isCopyAssignmentOperator() || MD->isMoveAssignmentOperator())
+      return false;
+  }
 
   // Otherwise, if we visited the function before, do not reanalyze it.
   return Visited.count(D);
@@ -442,9 +449,7 @@ AnalysisConsumer::getInliningModeForFunction(const Decl *D,
   // We want to reanalyze all ObjC methods as top level to report Retain
   // Count naming convention errors more aggressively. But we should tune down
   // inlining when reanalyzing an already inlined function.
-  if (Visited.count(D)) {
-    assert(isa<ObjCMethodDecl>(D) &&
-           "We are only reanalyzing ObjCMethods.");
+  if (Visited.count(D) && isa<ObjCMethodDecl>(D)) {
     const ObjCMethodDecl *ObjCM = cast<ObjCMethodDecl>(D);
     if (ObjCM->getMethodFamily() != OMF_init)
       return ExprEngine::Inline_Minimal;
