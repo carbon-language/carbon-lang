01c078966d384e41653a0c3cb390ba259c0dd333
Rafael Espindola <rafael.espindola@gmail.com>
2016-07-18 18:24:41 +0000

    Delete dead code.
    And make it easier to spot code going dead by using elf::.
    
    llvm-svn: 275858

--- lld/ELF/Error.cpp
+++ lld/ELF/Error.cpp
@@ -17,49 +17,38 @@
 using namespace llvm;
 
 namespace lld {
-namespace elf {
 
-bool HasError;
-raw_ostream *ErrorOS;
+bool elf::HasError;
+raw_ostream *elf::ErrorOS;
 
-void log(const Twine &Msg) {
+void elf::log(const Twine &Msg) {
   if (Config->Verbose)
     outs() << Msg << "\n";
 }
 
-void warning(const Twine &Msg) {
+void elf::warning(const Twine &Msg) {
   if (Config->FatalWarnings)
     error(Msg);
   else
     *ErrorOS << Msg << "\n";
 }
 
-void error(const Twine &Msg) {
+void elf::error(const Twine &Msg) {
   *ErrorOS << Msg << "\n";
   HasError = true;
 }
 
-void error(std::error_code EC, const Twine &Prefix) {
+void elf::error(std::error_code EC, const Twine &Prefix) {
   error(Prefix + ": " + EC.message());
 }
 
-void fatal(const Twine &Msg) {
+void elf::fatal(const Twine &Msg) {
   *ErrorOS << Msg << "\n";
   exit(1);
 }
 
-void fatal(const Twine &Msg, const Twine &Prefix) {
+void elf::fatal(const Twine &Msg, const Twine &Prefix) {
   fatal(Prefix + ": " + Msg);
 }
 
-void check(std::error_code EC) {
-  if (EC)
-    fatal(EC.message());
-}
-
-void check(Error Err) {
-  check(errorToErrorCode(std::move(Err)));
-}
-
-} // namespace elf
 } // namespace lld
