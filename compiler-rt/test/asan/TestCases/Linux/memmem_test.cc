1abc356e3e5f4698a383c4998609c608a0f60420
Kostya Serebryany <kcc@google.com>
2016-07-19 21:35:01 +0000

    [asan] trying to fix the android bot
    llvm-svn: 276031

--- compiler-rt/test/asan/TestCases/Linux/memmem_test.cc
+++ compiler-rt/test/asan/TestCases/Linux/memmem_test.cc
@@ -1,6 +1,6 @@
 // RUN: %clangxx_asan  %s -o %t
-// RUN: not %run %t   2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=A1
-// RUN: not %run %t 1 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=A2
+// RUN: not %run %t   2>&1 | FileCheck %s --check-prefix=A1
+// RUN: not %run %t 1 2>&1 | FileCheck %s --check-prefix=A2
 // RUN: %env_asan_opts=intercept_memmem=0 %run %t
 
 #include <string.h>
@@ -12,10 +12,14 @@ int main(int argc, char **argv) {
     res = memmem(a1, sizeof(a1) + 1, a2, sizeof(a2));  // BOOM
   else
     res = memmem(a1, sizeof(a1), a2, sizeof(a2) + 1);  // BOOM
-  // CHECK: AddressSanitizer: stack-buffer-overflow
-  // CHECK: {{#0.*memmem}}
-  // CHECK: {{#1.*main}}
+  // A1: AddressSanitizer: stack-buffer-overflow
+  // A1: {{#0.*memmem}}
+  // A1-NEXT: {{#1.*main}}
   // A1: 'a1' <== Memory access at offset
+  //
+  // A2: AddressSanitizer: stack-buffer-overflow
+  // A2: {{#0.*memmem}}
+  // A2-NEXT: {{#1.*main}}
   // A2: 'a2' <== Memory access at offset
   return res == NULL;
 }
