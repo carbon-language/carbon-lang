8765dbcb7899b4687fbe5af2880ec2d6308f1366
Daniel Dunbar <daniel@zuster.org>
2016-07-21 23:17:44 +0000

    [lit] Bump version number.
    llvm-svn: 276353

--- llvm/utils/lit/lit/__init__.py
+++ llvm/utils/lit/lit/__init__.py
@@ -4,8 +4,8 @@ from __future__ import absolute_import
 from .main import main
 
 __author__ = 'Daniel Dunbar'
-__email__ = 'daniel@zuster.org'
-__versioninfo__ = (0, 5, 0)
+__email__ = 'daniel@minormatter.com'
+__versioninfo__ = (0, 6, 0)
 __version__ = '.'.join(str(v) for v in __versioninfo__) + 'dev'
 
 __all__ = []
