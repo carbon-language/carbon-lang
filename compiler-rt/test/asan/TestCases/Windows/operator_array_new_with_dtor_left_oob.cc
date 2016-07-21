1558a2f3ee5a8e7e5cf0c9206c743148cafe2a9a
Reid Kleckner <rnk@google.com>
2016-07-21 21:04:34 +0000

    [asan] Fix Win64 test portability issues
    The OOM test should really only run on 32-bits, since it's hard to OOM
    on x64.
    
    The operator_array_new_with_dtor_left_oob tests need to account for the
    larger array cookie on x64 (8 bytes instead of 4).
    
    Use -std=c++14 in use-after-scope-capture.cc to avoid errors in the MSVC
    2015 STL on Windows. The default there is C++14 anyway.
    
    llvm-svn: 276332

--- compiler-rt/test/asan/TestCases/Windows/operator_array_new_with_dtor_left_oob.cc
+++ compiler-rt/test/asan/TestCases/Windows/operator_array_new_with_dtor_left_oob.cc
@@ -8,7 +8,7 @@ struct C {
 
 int main() {
   C *buffer = new C[42];
-  buffer[-2].x = 42;
+  buffer[-(1 + sizeof(void*) / 4)].x = 42;
 // CHECK: AddressSanitizer: heap-buffer-overflow on address [[ADDR:0x[0-9a-f]+]]
 // CHECK: WRITE of size 4 at [[ADDR]] thread T0
 // CHECK-NEXT: {{#0 .* main .*operator_array_new_with_dtor_left_oob.cc}}:[[@LINE-3]]
@@ -16,7 +16,7 @@ int main() {
 // FIXME: Currently it says "4 bytes ... left of 172-byte region",
 //        should be "8 bytes ... left of 168-byte region", see
 //        https://code.google.com/p/address-sanitizer/issues/detail?id=314
-// CHECK: [[ADDR]] is located {{.*}} bytes to the left of 172-byte region
+// CHECK: [[ADDR]] is located {{.*}} bytes to the left of {{(172|176)}}-byte region
 // CHECK-LABEL: allocated by thread T0 here:
 // FIXME: The 'operator new' frame should have [].
 // CHECK-NEXT: {{#0 .* operator new}}
