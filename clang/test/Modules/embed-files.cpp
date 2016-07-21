07915f6cb050500195021d05b70817db08ecdd59
Reid Kleckner <rnk@google.com>
2016-07-21 21:07:47 +0000

    Disable a flaky test on Windows that uses "echo >>"
    llvm-svn: 276335

--- clang/test/Modules/embed-files.cpp
+++ clang/test/Modules/embed-files.cpp
@@ -10,6 +10,10 @@
 //
 // RUN: %clang_cc1 -fmodules -I%t -fmodules-embed-all-files %t/modulemap -fmodule-name=a -x c++ -emit-module -o %t/a.pcm
 // RUN: %clang_cc1 -fmodules -I%t -fmodules-embed-all-files %t/modulemap -fmodule-name=b -x c++ -emit-module -o %t/b.pcm
+// FIXME: This test is flaky on Windows because attempting to delete a file
+// after writing it just doesn't seem to work well, at least not in the lit
+// shell.
+// REQUIRES: shell
 // RUN: rm %t/x.h
 // RUN: %clang_cc1 -fmodules -I%t -fmodule-map-file=%t/modulemap -fmodule-file=%t/a.pcm -fmodule-file=%t/b.pcm %s -verify
 #include "a.h"
@@ -19,3 +23,4 @@ char t; // expected-error {{different type}}
 #include "b.h"
 char t; // expected-error {{different type}}
 // expected-note@t.h:1 {{here}}
+
