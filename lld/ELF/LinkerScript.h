01151e9c248041be983715f9dc198243408b9b90
Rui Ueyama <ruiu@google.com>
2016-07-24 01:06:18 +0000

    Split LinkerScript::createSections.
    createSections is getting longer, so it is probably time to split.
    
    Differential Revision: https://reviews.llvm.org/D22730
    
    llvm-svn: 276538

--- lld/ELF/LinkerScript.h
+++ lld/ELF/LinkerScript.h
@@ -130,6 +130,9 @@ private:
   // "ScriptConfig" is a bit too long, so define a short name for it.
   ScriptConfiguration &Opt = *ScriptConfig;
 
+  std::vector<OutputSectionBase<ELFT> *>
+  filter(std::vector<OutputSectionBase<ELFT> *> &Sections);
+
   int getSectionIndex(StringRef Name);
   std::vector<size_t> getPhdrIndicesForSection(StringRef Name);
   void dispatchAssignment(SymbolAssignment *Cmd);
