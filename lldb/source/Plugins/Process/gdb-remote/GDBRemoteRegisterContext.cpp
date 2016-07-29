e768c4b858bd32b814baf142dd2acfeae6022638
Pavel Labath <labath@google.com>
2016-07-29 13:10:02 +0000

    Rewrite gdb-remote's SendContinuePacketAndWaitForResponse
    SendContinuePacketAndWaitForResponse was huge function with very complex interactions with
    several other functions (SendAsyncSignal, SendInterrupt, SendPacket). This meant that making any
    changes to how packet sending functions and threads interact was very difficult and error-prone.
    
    This change does not add any functionality yet, it merely paves the way for future changes. In a
    follow-up, I plan to add the ability to have multiple query packets in flight (i.e.,
    request,request,response,response instead of the usual request,response sequences) and use that
    to speed up qModuleInfo packet processing.
    
    Here, I introduce two special kinds of locks: ContinueLock, which is used by the continue thread,
    and Lock, which is used by everyone else. ContinueLock (atomically) sends a continue packet, and
    blocks any other async threads from accessing the connection. Other threads create an instance of
    the Lock object when they want to access the connection. This object, while in scope prevents the
    continue from being send. Optionally, it can also interrupt the process to gain access to the
    connection for async processing.
    
    Most of the syncrhonization logic is encapsulated within these two classes. Some of it still
    had to bleed over into the SendContinuePacketAndWaitForResponse, but the function is still much
    more manageable than before -- partly because of most of the work is done in the ContinueLock
    class, and partly because I have factored out a lot of the packet processing code separate
    functions (this also makes the functionality more easily testable). Most importantly, there is
    none of syncrhonization code in the async thread users -- as far as they are concerned, they just
    need to declare a Lock object, and they are good to go (SendPacketAndWaitForResponse is now a
    very thin wrapper around the NoLock version of the function, whereas previously it had over 100
    lines of synchronization code).  This will make my follow up changes there easy.
    
    I have written a number of unit tests for the new code and I have ran the test suite on linux and
    osx with no regressions.
    
    Subscribers: tberghammer
    
    Differential Revision: https://reviews.llvm.org/D22629
    
    llvm-svn: 277139

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteRegisterContext.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteRegisterContext.cpp
@@ -372,12 +372,6 @@ GDBRemoteRegisterContext::WriteRegisterBytes (const RegisterInfo *reg_info, Data
         return false;
 
     GDBRemoteCommunicationClient &gdb_comm (((ProcessGDBRemote *)process)->GetGDBRemote());
-// FIXME: This check isn't right because IsRunning checks the Public state, but this
-// is work you need to do - for instance in ShouldStop & friends - before the public
-// state has been changed.
-//    if (gdb_comm.IsRunning())
-//        return false;
-
 
 #if defined (LLDB_CONFIGURATION_DEBUG)
     assert (m_reg_data.GetByteSize() >= reg_info->byte_offset + reg_info->byte_size);
@@ -401,8 +395,8 @@ GDBRemoteRegisterContext::WriteRegisterBytes (const RegisterInfo *reg_info, Data
                                   reg_info->byte_size,          // dst length
                                   m_reg_data.GetByteOrder()))   // dst byte order
     {
-        std::unique_lock<std::recursive_mutex> lock;
-        if (gdb_comm.GetSequenceMutex(lock, "Didn't get sequence mutex for write register."))
+        GDBRemoteClientBase::Lock lock(gdb_comm, false);
+        if (lock)
         {
             const bool thread_suffix_supported = gdb_comm.GetThreadSuffixSupported();
             ProcessSP process_sp (m_thread.GetProcess());
@@ -570,8 +564,8 @@ GDBRemoteRegisterContext::ReadAllRegisterValues (lldb::DataBufferSP &data_sp)
 
     const bool use_g_packet = gdb_comm.AvoidGPackets ((ProcessGDBRemote *)process) == false;
 
-    std::unique_lock<std::recursive_mutex> lock;
-    if (gdb_comm.GetSequenceMutex(lock, "Didn't get sequence mutex for read all registers."))
+    GDBRemoteClientBase::Lock lock(gdb_comm, false);
+    if (lock)
     {
         SyncThreadState(process);
         
@@ -679,8 +673,8 @@ GDBRemoteRegisterContext::WriteAllRegisterValues (const lldb::DataBufferSP &data
     const bool use_g_packet = gdb_comm.AvoidGPackets ((ProcessGDBRemote *)process) == false;
 
     StringExtractorGDBRemote response;
-    std::unique_lock<std::recursive_mutex> lock;
-    if (gdb_comm.GetSequenceMutex(lock, "Didn't get sequence mutex for write all registers."))
+    GDBRemoteClientBase::Lock lock(gdb_comm, false);
+    if (lock)
     {
         const bool thread_suffix_supported = gdb_comm.GetThreadSuffixSupported();
         ProcessSP process_sp (m_thread.GetProcess());
