78fb132af094873fb6e042f1f77f6d25c6da2c0c
Paul Robinson <paul.robinson@sony.com>
2016-08-01 22:12:46 +0000

    Add FIXMEs for MSVC 2013 hacks in r277211. NFC.
    llvm-svn: 277396

--- clang/lib/CodeGen/CGOpenMPRuntime.cpp
+++ clang/lib/CodeGen/CGOpenMPRuntime.cpp
@@ -5489,6 +5489,7 @@ public:
       Info[VD].push_back({L, MapType, MapModifier, ReturnDevicePointer});
     };
 
+    // FIXME: MSVC 2013 seems to require this-> to find member CurDir.
     for (auto *C : this->CurDir.getClausesOfKind<OMPMapClause>())
       for (auto L : C->component_lists())
         InfoGen(L.first, L.second, C->getMapType(), C->getMapTypeModifier(),
@@ -5506,6 +5507,7 @@ public:
     // entries as such. If there is no map information for an entry in the
     // use_device_ptr list, we create one with map type 'alloc' and zero size
     // section. It is the user fault if that was not mapped before.
+    // FIXME: MSVC 2013 seems to require this-> to find member CurDir.
     for (auto *C : this->CurDir.getClausesOfKind<OMPUseDevicePtrClause>())
       for (auto L : C->component_lists()) {
         assert(!L.second.empty() && "Not expecting empty list of components!");
@@ -5538,6 +5540,7 @@ public:
 
         // We didn't find any match in our map information - generate a zero
         // size array section.
+        // FIXME: MSVC 2013 seems to require this-> to find member CGF.
         llvm::Value *Ptr =
             this->CGF
                 .EmitLoadOfLValue(this->CGF.EmitLValue(IE), SourceLocation())
@@ -5558,6 +5561,7 @@ public:
 
         // Remember the current base pointer index.
         unsigned CurrentBasePointersIdx = BasePointers.size();
+        // FIXME: MSVC 2013 seems to require this-> to find the member method.
         this->generateInfoForComponentList(L.MapType, L.MapTypeModifier,
                                            L.Components, BasePointers, Pointers,
                                            Sizes, Types, IsFirstComponentList);
@@ -5637,6 +5641,7 @@ public:
       return;
     }
 
+    // FIXME: MSVC 2013 seems to require this-> to find member CurDir.
     for (auto *C : this->CurDir.getClausesOfKind<OMPMapClause>())
       for (auto L : C->decl_component_lists(VD)) {
         assert(L.first == VD &&
