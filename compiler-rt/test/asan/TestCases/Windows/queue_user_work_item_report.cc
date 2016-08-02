dabcee8b2c3dac44aac7fc8a1029c7282ae05c6f
Reid Kleckner <rnk@google.com>
2016-08-02 20:26:59 +0000

    Remove stale CHECK lines that should have been included in r277478
    We no longer assign ids to unregistered threads. We don't have any stack
    trace for thread creation for these worker threads, so this shouldn't
    affect report quality much.
    
    llvm-svn: 277514

--- compiler-rt/test/asan/TestCases/Windows/queue_user_work_item_report.cc
+++ compiler-rt/test/asan/TestCases/Windows/queue_user_work_item_report.cc
@@ -12,8 +12,6 @@ DWORD CALLBACK work_item(LPVOID) {
 // CHECK: AddressSanitizer: stack-buffer-underflow on address [[ADDR:0x[0-9a-f]+]]
 // CHECK: WRITE of size 1 at [[ADDR]] thread T1
 // CHECK:   {{#0 .* work_item.*queue_user_work_item_report.cc}}:[[@LINE-3]]
-// CHECK: Address [[ADDR]] is located in stack of thread T1 at offset {{.*}} in frame
-// CHECK:   work_item
   SetEvent(done);
   return 0;
 }
