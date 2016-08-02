d0e8d251eb5516ac3c27cb607295278587088e86
Nirav Dave <niravd@google.com>
2016-08-02 01:27:09 +0000

    Fix test from rL277407.
    llvm-svn: 277408

--- llvm/test/MC/ARM/preserve-comments-arm.s
+++ llvm/test/MC/ARM/preserve-comments-arm.s
@@ -1,6 +1,6 @@
-	#RUN: llvm-mc -preserve-comments -n -triple arm-eabi < %s > %t
-	#RUN: sed 's/[\t]#/	@/g' %s > %t2
-	#RUN: diff %t %t2
+	@RUN: llvm-mc -preserve-comments -n -triple arm-eabi < %s > %t
+	@RUN: sed 's/[\t]#/	@/g' %s > %t2
+	@RUN: diff %t %t2
 	.text
 
 	mov	r0, r0
