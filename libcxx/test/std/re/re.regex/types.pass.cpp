ac98d5980279c9faf5a2d581c32dfa57832d1639
Hubert Tong <hubert.reinterpretcast@gmail.com>
2016-08-02 21:34:48 +0000

    [libcxx] basic_regex: add traits_type, string_type
    Summary:
    In the synopsis in C++11 subclause 28.8 [re.regex], `basic_regex` is
    specified to have member typedefs `traits_type` and `string_type`. This
    change adds them to libc++.
    
    Reviewers: mclow.lists, rsmith, hubert.reinterpretcast
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D22698
    
    Patch by Jason Liu!
    
    llvm-svn: 277526

--- libcxx/test/std/re/re.regex/types.pass.cpp
+++ libcxx/test/std/re/re.regex/types.pass.cpp
@@ -15,6 +15,8 @@
 // public:
 //     // types:
 //     typedef charT                               value_type;
+//     typedef traits                              traits_type;
+//     typedef typename traits::string_type        string_type;
 //     typedef regex_constants::syntax_option_type flag_type;
 //     typedef typename traits::locale_type        locale_type;
 
@@ -25,11 +27,15 @@
 int main()
 {
     static_assert((std::is_same<std::basic_regex<char>::value_type, char>::value), "");
+    static_assert((std::is_same<std::basic_regex<char>::traits_type, std::regex_traits<char> >::value), "");
+    static_assert((std::is_same<std::basic_regex<char>::string_type, std::basic_string<char> >::value), "");
     static_assert((std::is_same<std::basic_regex<char>::flag_type,
                                 std::regex_constants::syntax_option_type>::value), "");
     static_assert((std::is_same<std::basic_regex<char>::locale_type, std::locale>::value), "");
 
     static_assert((std::is_same<std::basic_regex<wchar_t>::value_type, wchar_t>::value), "");
+    static_assert((std::is_same<std::basic_regex<wchar_t>::traits_type, std::regex_traits<wchar_t> >::value), "");
+    static_assert((std::is_same<std::basic_regex<wchar_t>::string_type, std::basic_string<wchar_t> >::value), "");
     static_assert((std::is_same<std::basic_regex<wchar_t>::flag_type,
                                 std::regex_constants::syntax_option_type>::value), "");
     static_assert((std::is_same<std::basic_regex<wchar_t>::locale_type, std::locale>::value), "");
