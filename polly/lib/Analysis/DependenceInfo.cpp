d82222fc1b9175ed0b3c5077e53d559c138c545b
Michael Kruse <llvm@meinersbur.de>
2016-08-05 11:31:02 +0000

    [DependenceInfo] Reset operations counter when setting limit.
    When entering the dependence computation and the max_operations is set, the
    operations counter may have already exceeded the counter, thus aborting any ISL
    computation from the start. The counter is reset at the end of the dependence
    calculation such that a follow-up recomputation might succeed, ie. the success
    of the first dependence calculation depends on unrelated ISL operations that
    happened before, giving it a disadvantage to the following calculations.
    
    This patch resets the operations counter at the beginning of the dependence
    recalculation to not depend on previous actions. Otherwise additional
    preprocessing of the Scop that aims to improve its schedulability (eg. DeLICM)
    do have the effect that DependenceInfo and hence the scheduling fail more
    likely, contraproductive to the goal of said preprocessing.
    
    llvm-svn: 277810

--- polly/lib/Analysis/DependenceInfo.cpp
+++ polly/lib/Analysis/DependenceInfo.cpp
@@ -366,8 +366,10 @@ void Dependences::calculateDependences(Scop &S) {
   }
 
   long MaxOpsOld = isl_ctx_get_max_operations(IslCtx.get());
-  if (OptComputeOut)
+  if (OptComputeOut) {
+    isl_ctx_reset_operations(IslCtx.get());
     isl_ctx_set_max_operations(IslCtx.get(), OptComputeOut);
+  }
 
   auto OnErrorStatus = isl_options_get_on_error(IslCtx.get());
   isl_options_set_on_error(IslCtx.get(), ISL_ON_ERROR_CONTINUE);
