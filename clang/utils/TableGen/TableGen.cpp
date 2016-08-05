957d7a00bc6ff64a268c82a66a3b318108e8596a
Vedant Kumar <vsk@apple.com>
2016-08-05 22:48:53 +0000

    [clang-tblgen] Remove unused #include (NFC)
    llvm-svn: 277885

--- clang/utils/TableGen/TableGen.cpp
+++ clang/utils/TableGen/TableGen.cpp
@@ -13,7 +13,6 @@
 
 #include "TableGenBackends.h" // Declares all backends.
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/PrettyStackTrace.h"
 #include "llvm/Support/Signals.h"
 #include "llvm/TableGen/Error.h"
