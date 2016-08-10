abd6186216461d2572c13a2e8959f908a6f2e020
Todd Fiala <todd.fiala@gmail.com>
2016-08-10 00:53:21 +0000

    Undo usage of LLVM macros in debugserver
    We don't take a dependency on LLVM in debugserver.
    This was failing to compile before.
    
    llvm-svn: 278190

--- lldb/tools/debugserver/source/RNBRemote.cpp
+++ lldb/tools/debugserver/source/RNBRemote.cpp
@@ -183,14 +183,14 @@ RNBRemote::RNBRemote () :
     m_enable_compression_next_send_packet (false),
     m_compression_mode (compression_types::none)
 {
-    DNBLogThreadedIf (LOG_RNB_REMOTE, "%s", LLVM_PRETTY_FUNCTION);
+    DNBLogThreadedIf (LOG_RNB_REMOTE, "%s", __PRETTY_FUNCTION__);
     CreatePacketTable ();
 }
 
 
 RNBRemote::~RNBRemote()
 {
-    DNBLogThreadedIf (LOG_RNB_REMOTE, "%s", LLVM_PRETTY_FUNCTION);
+    DNBLogThreadedIf (LOG_RNB_REMOTE, "%s", __PRETTY_FUNCTION__);
     StopReadRemoteDataThread();
 }
 
