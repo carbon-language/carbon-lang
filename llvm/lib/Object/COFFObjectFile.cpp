8950a53821d1b76ced7cb8cc0baf6e6292c77406
Rui Ueyama <ruiu@google.com>
2016-08-11 22:02:44 +0000

    Re-commit r278066: Do not ignore SizeOfOptionalHeader in COFF header even if PE header is not present.
    llvm-svn: 278429

--- llvm/lib/Object/COFFObjectFile.cpp
+++ llvm/lib/Object/COFFObjectFile.cpp
@@ -718,8 +718,10 @@ COFFObjectFile::COFFObjectFile(MemoryBufferRef Object, std::error_code &EC)
     }
     if ((EC = getObject(DataDirectory, Data, DataDirAddr, DataDirSize)))
       return;
-    CurPtr += COFFHeader->SizeOfOptionalHeader;
   }
+
+  if (COFFHeader)
+    CurPtr += COFFHeader->SizeOfOptionalHeader;
 
   if ((EC = getObject(SectionTable, Data, base() + CurPtr,
                       (uint64_t)getNumberOfSections() * sizeof(coff_section))))
