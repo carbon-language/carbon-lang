edfed3deeb006b1f599498b0aed3be1dec1640f2
Daniel Sanders <daniel.sanders@imgtec.com>
2016-08-15 18:56:54 +0000

    [msan] Correct @LINE expression in obstack.cc
    Summary:
    
    [[@LINE-30]] only worked because the resulting 3 matches the first character of
    30. With the additional blank lines the resulting 5 no longer matches 30.
    
    Reviewers: eugenis
    
    Subscribers: eugenis, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D23515
    
    llvm-svn: 278715

--- compiler-rt/test/msan/Linux/obstack.cc
+++ compiler-rt/test/msan/Linux/obstack.cc
@@ -1,6 +1,8 @@
 // RUN: %clangxx_msan -O0 -g %s -o %t && %run %t
 // RUN: %clangxx_msan -O0 -g -DPOSITIVE %s -o %t && not %run %t |& FileCheck %s
+
 // XFAIL: target-is-mips64el
+
 #include <obstack.h>
 #include <sanitizer/msan_interface.h>
 #include <stdlib.h>
@@ -30,7 +32,7 @@ int main(void) {
       __msan_check_mem_is_initialized(p, sizeof(data) + 1);
     }
     // CHECK: WARNING: MemorySanitizer: use-of-uninitialized-value
-    // CHECK: #0 0x{{.*}} in main{{.*}}obstack.cc:[[@LINE-30]]
+    // CHECK: #0 0x{{.*}} in main{{.*}}obstack.cc:[[@LINE-3]]
 #endif
   }
   obstack_free(&obs, 0);
