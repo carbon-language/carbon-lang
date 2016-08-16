fda072de9714c557fdd7a2ba608980a25d2afe61
Francis Ricci <francisjricci@gmail.com>
2016-08-16 02:15:51 +0000

    [compiler-rt] Allow c++ abi to be explictly disabled in cmake configuration
        Summary: This will allow for the sanitizers to be used when c++ abi is unavailable.
    
        Reviewers: samsonov, beanz, pcc, rnk
    
        Subscribers: llvm-commits, kubabrecka, compnerd, dberris
    
        Differential Revision: https://reviews.llvm.org/D23376
    
    llvm-svn: 278772

--- compiler-rt/lib/ubsan/ubsan_type_hash_itanium.cc
+++ compiler-rt/lib/ubsan/ubsan_type_hash_itanium.cc
@@ -13,7 +13,7 @@
 
 #include "sanitizer_common/sanitizer_platform.h"
 #include "ubsan_platform.h"
-#if CAN_SANITIZE_UB && !SANITIZER_WINDOWS
+#if CAN_SANITIZE_UB && UBSAN_CAN_USE_CXXABI
 #include "ubsan_type_hash.h"
 
 #include "sanitizer_common/sanitizer_common.h"
