a2f96619cb046ab0dd8e49c086c25cc375c74898
George Rimar <grimar@accesssoftek.com>
2016-08-19 12:57:45 +0000

    [ELF] - Removed excessive llvm-readobj call from linkerscript-ouputformat.s. NFC.
    llvm-svn: 279236

--- lld/test/ELF/linkerscript/linkerscript-ouputformat.s
+++ lld/test/ELF/linkerscript/linkerscript-ouputformat.s
@@ -7,4 +7,3 @@
 # RUN: echo "OUTPUT_FORMAT(x, y)" > %t.script
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-freebsd %s -o %t1
 # RUN: not ld.lld -shared -o %t2 %t1 %t.script
-# RUN: llvm-readobj %t2 > /dev/null
