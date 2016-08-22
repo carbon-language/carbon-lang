c41fcaf8aa1e0fce242fcf0af55d1d8113e67bcb
David Blaikie <dblaikie@gmail.com>
2016-08-22 17:49:50 +0000

    Remove redundant test
    test/CodeGenCXX/debug-info-zero-length-arrays.cpp tests this
    functionality more comprehensively
    
    llvm-svn: 279444

--- clang/test/CodeGenCXX/debug-info-flex-member.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -emit-llvm -debug-info-kind=limited -triple x86_64-apple-darwin %s -o - | FileCheck %s
-
-// CHECK: !DISubrange(count: -1)
-
-struct StructName {
-  int member[];
-};
-
-struct StructName SN;
