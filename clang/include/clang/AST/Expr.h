81899af5501ac02d1c42ea43e1370ad5a6a92bd0
Vedant Kumar <vsk@apple.com>
2016-08-24 06:44:57 +0000

    [AST] Make InitListExpr::isExplicit const (NFC)
    Patch by Alexander Shaposhnikov!
    
    Differential Revision: https://reviews.llvm.org/D23828
    
    llvm-svn: 279613

--- clang/include/clang/AST/Expr.h
+++ clang/include/clang/AST/Expr.h
@@ -3862,7 +3862,7 @@ public:
 
   // Explicit InitListExpr's originate from source code (and have valid source
   // locations). Implicit InitListExpr's are created by the semantic analyzer.
-  bool isExplicit() {
+  bool isExplicit() const {
     return LBraceLoc.isValid() && RBraceLoc.isValid();
   }
 
