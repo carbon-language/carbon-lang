571a6478538ba4802ae19b488c3b3f1e0ae710c6
Richard Smith <richard-llvm@metafoo.co.uk>
2016-08-24 21:30:00 +0000

    Disable test under asan: it uses a lot of stack, and asan increases the per-frame stack usage enough to cause it to hit our stack limit. This is not ideal; we should find a better way of dealing with this, such as increasing our stack allocation when built with ASan.
    llvm-svn: 279668

--- clang/test/SemaTemplate/instantiation-depth-default.cpp
+++ clang/test/SemaTemplate/instantiation-depth-default.cpp
@@ -1,9 +1,14 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -ftemplate-backtrace-limit 2 %s
+//
+// FIXME: Disable this test when Clang was built with ASan, because ASan
+// increases our per-frame stack usage enough that this test no longer fits
+// within our normal stack space allocation.
+// REQUIRES: not_asan
 
 template<int N, typename T> struct X : X<N+1, T*> {};
-// expected-error-re@3 {{recursive template instantiation exceeded maximum depth of 1024{{$}}}}
-// expected-note@3 {{instantiation of template class}}
-// expected-note@3 {{skipping 1023 contexts in backtrace}}
-// expected-note@3 {{use -ftemplate-depth=N to increase recursive template instantiation depth}}
+// expected-error-re@8 {{recursive template instantiation exceeded maximum depth of 1024{{$}}}}
+// expected-note@8 {{instantiation of template class}}
+// expected-note@8 {{skipping 1023 contexts in backtrace}}
+// expected-note@8 {{use -ftemplate-depth=N to increase recursive template instantiation depth}}
 
 X<0, int> x; // expected-note {{in instantiation of}}
