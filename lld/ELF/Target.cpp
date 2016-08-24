d08aa5c3915c063c87ab58dc7987a7653f2f0572
Nico Weber <nicolasweber@gmx.de>
2016-08-24 16:36:41 +0000

    fix typo "varaibles"
    llvm-svn: 279638

--- lld/ELF/Target.cpp
+++ lld/ELF/Target.cpp
@@ -10,7 +10,7 @@
 // Machine-specific things, such as applying relocations, creation of
 // GOT or PLT entries, etc., are handled in this file.
 //
-// Refer the ELF spec for the single letter varaibles, S, A or P, used
+// Refer the ELF spec for the single letter variables, S, A or P, used
 // in this file.
 //
 // Some functions defined in this file has "relaxTls" as part of their names.
