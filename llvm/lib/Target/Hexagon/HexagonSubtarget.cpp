b5ec48755d594c83c790e69156e6ac66b38cf51f
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-08-24 17:17:39 +0000

    [Hexagon] Enable subregister liveness tracking
    llvm-svn: 279642

--- llvm/lib/Target/Hexagon/HexagonSubtarget.cpp
+++ llvm/lib/Target/Hexagon/HexagonSubtarget.cpp
@@ -66,7 +66,7 @@ static cl::opt<bool> DisableHexagonMISched("disable-hexagon-misched",
   cl::desc("Disable Hexagon MI Scheduling"));
 
 static cl::opt<bool> EnableSubregLiveness("hexagon-subreg-liveness",
-  cl::Hidden, cl::ZeroOrMore, cl::init(false),
+  cl::Hidden, cl::ZeroOrMore, cl::init(true),
   cl::desc("Enable subregister liveness tracking for Hexagon"));
 
 static cl::opt<bool> OverrideLongCalls("hexagon-long-calls",
