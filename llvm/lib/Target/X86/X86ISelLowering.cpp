0ad9f3e93bbec3926ce61e280a458b3d72e93c2c
Simon Pilgrim <llvm-dev@redking.me.uk>
2016-08-25 12:45:16 +0000

    [X86][AVX] Provide SubVectorBroadcast fallback if load fold fails (PR29133)
    Fix for PR29133, matching the approach that was taken for AVX1 scalar broadcasts.
    
    llvm-svn: 279735

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -12987,8 +12987,7 @@ static SDValue LowerINSERT_SUBVECTOR(SDValue Op, const X86Subtarget &Subtarget,
       // lower to a VBROADCASTF128/VBROADCASTI128/etc.
       if (auto *Ld = dyn_cast<LoadSDNode>(peekThroughOneUseBitcasts(SubVec2))) {
         if (SubVec2 == SubVec && ISD::isNormalLoad(Ld) &&
-            areOnlyUsersOf(SubVec2.getNode(), {Op, Vec}) &&
-            !Ld->hasAnyUseOfValue(1)) {
+            areOnlyUsersOf(SubVec2.getNode(), {Op, Vec})) {
           return DAG.getNode(X86ISD::SUBV_BROADCAST, dl, OpVT, SubVec);
         }
       }
