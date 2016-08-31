b2e620a23b950ca8e21ab6da0e14321cd566425f
Logan Chien <tzuhsiang.chien@gmail.com>
2016-08-31 15:16:40 +0000

    Wrap LIBCXXABI_USE_LLVM_UNWINDER with defined().
    This commit fixes -Wundef by replacing:
    
        #if !LIBCXXABI_USE_LLVM_UNWINDER
    
    with:
    
        #if !defined(LIBCXXABI_USE_LLVM_UNWINDER)
    
    llvm-svn: 280251

--- libcxxabi/src/cxa_personality.cpp
+++ libcxxabi/src/cxa_personality.cpp
@@ -1035,7 +1035,7 @@ static _Unwind_Reason_Code continue_unwind(_Unwind_Exception* unwind_exception,
 }
 
 // ARM register names
-#if !LIBCXXABI_USE_LLVM_UNWINDER
+#if !defined(LIBCXXABI_USE_LLVM_UNWINDER)
 static const uint32_t REG_UCB = 12;  // Register to save _Unwind_Control_Block
 #endif
 static const uint32_t REG_SP = 13;
@@ -1071,7 +1071,7 @@ __gxx_personality_v0(_Unwind_State state,
     bool native_exception = (unwind_exception->exception_class & get_vendor_and_language) ==
                             (kOurExceptionClass & get_vendor_and_language);
 
-#if !LIBCXXABI_USE_LLVM_UNWINDER
+#if !defined(LIBCXXABI_USE_LLVM_UNWINDER)
     // Copy the address of _Unwind_Control_Block to r12 so that
     // _Unwind_GetLanguageSpecificData() and _Unwind_GetRegionStart() can
     // return correct address.
