576225daf5f0c85183369a86ab1be01455c383bc
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-08-31 18:02:19 +0000

    Add an optional parameter with a list of undefs to extendToIndices
    llvm-svn: 280268

--- llvm/lib/CodeGen/LiveIntervalAnalysis.cpp
+++ llvm/lib/CodeGen/LiveIntervalAnalysis.cpp
@@ -568,11 +568,12 @@ void LiveIntervals::shrinkToUses(LiveInterval::SubRange &SR, unsigned Reg) {
 }
 
 void LiveIntervals::extendToIndices(LiveRange &LR,
-                                    ArrayRef<SlotIndex> Indices) {
+                                    ArrayRef<SlotIndex> Indices,
+                                    ArrayRef<SlotIndex> Undefs) {
   assert(LRCalc && "LRCalc not initialized.");
   LRCalc->reset(MF, getSlotIndexes(), DomTree, &getVNInfoAllocator());
   for (unsigned i = 0, e = Indices.size(); i != e; ++i)
-    LRCalc->extend(LR, Indices[i], /*PhysReg=*/0, /*Undefs=*/{});
+    LRCalc->extend(LR, Indices[i], /*PhysReg=*/0, Undefs);
 }
 
 void LiveIntervals::pruneValue(LiveRange &LR, SlotIndex Kill,
