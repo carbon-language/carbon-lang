3ddbb81ea034179623a4ffa8c00779e1e06cd550
James Molloy <james.molloy@arm.com>
2016-09-05 12:28:49 +0000

    Attempt to fix buildbots not targetting x86
    r280613 introduced failures for all builds that don't target x86 by default. Add an explicit target to avoid a missing feature diagnostic.
    
    llvm-svn: 280658

--- clang/test/Modules/compiler_builtins_x86.c
+++ clang/test/Modules/compiler_builtins_x86.c
@@ -1,5 +1,5 @@
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -fsyntax-only -fmodules -fimplicit-module-maps -fmodules-cache-path=%t %s -verify -ffreestanding
+// RUN: %clang_cc1 -triple x86 -fsyntax-only -fmodules -fimplicit-module-maps -fmodules-cache-path=%t %s -verify -ffreestanding
 // expected-no-diagnostics
 
 #include<x86intrin.h>
