490ccc9cc71e714f85e239913cbbcc1797c85fb8
Reid Kleckner <rnk@google.com>
2016-09-08 16:46:06 +0000

    Fix linker warning about sanitizer_common_nolibc functions on Windows
    llvm-svn: 280962

--- compiler-rt/lib/sanitizer_common/sanitizer_common_nolibc.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_common_nolibc.cc
@@ -17,6 +17,9 @@
 
 namespace __sanitizer {
 
+// The Windows implementations of these functions use the win32 API directly,
+// bypassing libc.
+#if !SANITIZER_WINDOWS
 #if SANITIZER_LINUX
 bool ShouldLogAfterPrintf() { return false; }
 void LogMessageOnPrintf(const char *str) {}
@@ -24,5 +27,6 @@ void LogMessageOnPrintf(const char *str) {}
 void WriteToSyslog(const char *buffer) {}
 void Abort() { internal__exit(1); }
 void SleepForSeconds(int seconds) { internal_sleep(seconds); }
+#endif // !SANITIZER_WINDOWS
 
 }  // namespace __sanitizer
