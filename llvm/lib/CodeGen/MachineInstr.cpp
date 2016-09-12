925961b20cfa7efb184cb579c6f239a309901506
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2016-09-12 16:21:49 +0000

    [GlobalISel] Fix mismatched "<..)" in intrinsic MO printing. NFC.
    llvm-svn: 281229

--- llvm/lib/CodeGen/MachineInstr.cpp
+++ llvm/lib/CodeGen/MachineInstr.cpp
@@ -470,9 +470,9 @@ void MachineOperand::print(raw_ostream &OS, ModuleSlotTracker &MST,
   case MachineOperand::MO_IntrinsicID: {
     Intrinsic::ID ID = getIntrinsicID();
     if (ID < Intrinsic::num_intrinsics)
-      OS << "<intrinsic:@" << Intrinsic::getName(ID, None) << ')';
+      OS << "<intrinsic:@" << Intrinsic::getName(ID, None) << '>';
     else if (IntrinsicInfo)
-      OS << "<intrinsic:@" << IntrinsicInfo->getName(ID) << ')';
+      OS << "<intrinsic:@" << IntrinsicInfo->getName(ID) << '>';
     else
       OS << "<intrinsic:" << ID << '>';
     break;
