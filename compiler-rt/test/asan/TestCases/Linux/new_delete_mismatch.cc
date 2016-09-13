610816f2688168cadadddb729598de716d89f0e1
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2016-09-13 22:25:44 +0000

    [asan] alloc_dealloc_mismatch=0 by default on Android.
    All known (to me) Android deployments are disabling this flag anyway.
    The in-tree script (asan_device_setup) does that, too.
    
    llvm-svn: 281410

--- compiler-rt/test/asan/TestCases/Linux/new_delete_mismatch.cc
+++ compiler-rt/test/asan/TestCases/Linux/new_delete_mismatch.cc
@@ -1,8 +1,8 @@
 // Check that we report new[] vs delete as alloc-dealloc-mismatch and not as
 // new-delete-type-mismatch when -fsized-deallocation is enabled.
 
-// RUN: %clangxx_asan -g %s -o %t && not %run %t |& FileCheck %s
-// RUN: %clangxx_asan -fsized-deallocation -g %s -o %t && not %run %t |& FileCheck %s
+// RUN: %clangxx_asan -g %s -o %t && %env_asan_opts=alloc_dealloc_mismatch=1 not %run %t |& FileCheck %s
+// RUN: %clangxx_asan -fsized-deallocation -g %s -o %t && %env_asan_opts=alloc_dealloc_mismatch=1 not %run %t |& FileCheck %s
 
 #include <stdlib.h>
 
