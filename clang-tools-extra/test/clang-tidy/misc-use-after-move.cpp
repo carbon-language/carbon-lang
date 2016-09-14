2ca31969989db90f85a2fa3f43e7864b90cdf008
Martin Bohme <mboehme@google.com>
2016-09-14 12:22:35 +0000

    [clang-tidy] Make test for misc-use-after-move pass under Windows
    Summary: Adds -fno-delayed-template-parsing
    
    Reviewers: alexfh
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D24550
    
    llvm-svn: 281455

--- clang-tools-extra/test/clang-tidy/misc-use-after-move.cpp
+++ clang-tools-extra/test/clang-tidy/misc-use-after-move.cpp
@@ -1,4 +1,4 @@
-// RUN: %check_clang_tidy %s misc-use-after-move %t
+// RUN: %check_clang_tidy %s misc-use-after-move %t -- -- -std=c++11 -fno-delayed-template-parsing
 
 typedef decltype(nullptr) nullptr_t;
 
