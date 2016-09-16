b53b62eb69cd5768423786c5ced700bc2d1d2e20
Mehdi Amini <mehdi.amini@apple.com>
2016-09-16 00:38:18 +0000

    Fix autoupgrade logic for Objective-C class properties module flag
    Previous we were issuing an error when linking a module containing
    the new Objective-C metadata structure for class properties with an
    "old" one.
    Now instead we downgrade the module flag so that the Objective-C
    runtime does not expect the new metadata structure.
    
    This is consistent with what ld64 is doing on binary files.
    
    Differential Revision: https://reviews.llvm.org/D24620
    
    llvm-svn: 281685

--- llvm/lib/IR/AutoUpgrade.cpp
+++ llvm/lib/IR/AutoUpgrade.cpp
@@ -1587,11 +1587,11 @@ bool llvm::UpgradeModuleFlags(Module &M) {
   }
   // "Objective-C Class Properties" is recently added for Objective-C. We
   // upgrade ObjC bitcodes to contain a "Objective-C Class Properties" module
-  // flag of value 0, so we can correclty report error when trying to link
-  // an ObjC bitcode without this module flag with an ObjC bitcode with this
-  // module flag.
+  // flag of value 0, so we can correclty downgrade this flag when trying to
+  // link an ObjC bitcode without this module flag with an ObjC bitcode with
+  // this module flag.
   if (HasObjCFlag && !HasClassProperties) {
-    M.addModuleFlag(llvm::Module::Error, "Objective-C Class Properties",
+    M.addModuleFlag(llvm::Module::Override, "Objective-C Class Properties",
                     (uint32_t)0);
     return true;
   }
--- llvm/test/Bitcode/upgrade-module-flag.ll
+++ llvm/test/Bitcode/upgrade-module-flag.ll
@@ -6,4 +6,4 @@
 !0 = !{i32 1, !"Objective-C Image Info Version", i32 0}
 
 ; CHECK: !0 = !{i32 1, !"Objective-C Image Info Version", i32 0}
-; CHECK: !1 = !{i32 1, !"Objective-C Class Properties", i32 0}
+; CHECK: !1 = !{i32 4, !"Objective-C Class Properties", i32 0}
