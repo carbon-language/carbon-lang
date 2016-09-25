221f689f3bc10cd51e5fa41bc63f722c88e9a3a9
Eric Fiselier <eric@efcs.ca>
2016-09-25 08:30:05 +0000

    Update -verify test to use new static assert message
    llvm-svn: 282352

--- libcxx/test/std/containers/sequences/array/array.tuple/get.fail.cpp
+++ libcxx/test/std/containers/sequences/array/array.tuple/get.fail.cpp
@@ -19,7 +19,6 @@
 #include <array>
 #include <cassert>
 
-#include "test_macros.h"
 
 // std::array is explicitly allowed to be initialized with A a = { init-list };.
 // Disable the missing braces warning for this reason.
@@ -32,10 +31,6 @@ int main()
         typedef std::array<T, 3> C;
         C c = {1, 2, 3.5};
         std::get<3>(c) = 5.5; // expected-note {{requested here}}
-#if TEST_STD_VER >= 11
         // expected-error@array:* {{static_assert failed "Index out of bounds in std::get<> (std::array)"}}
-#else
-        // expected-error@array:* {{implicit instantiation of undefined template '__static_assert_test<false>'}}
-#endif
     }
 }
