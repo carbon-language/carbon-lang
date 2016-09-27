7d6935c184d708d297b78c6503e43087cd952d1c
Kostya Serebryany <kcc@google.com>
2016-09-27 03:33:57 +0000

    [libFuzzer] run re2 test in 8 threads by default
    llvm-svn: 282469

--- llvm/lib/Fuzzer/fuzzer-test-suite/re2-2014-12-09/test.sh
+++ llvm/lib/Fuzzer/fuzzer-test-suite/re2-2014-12-09/test.sh
@@ -6,5 +6,5 @@ CORPUS=CORPUS-$EXECUTABLE_NAME_BASE
 JOBS=8
 rm -rf $CORPUS
 mkdir $CORPUS
-[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -exit_on_src_pos=re2/dfa.cc:474 -exit_on_src_pos=re2/dfa.cc:474  -runs=1000000 -jobs=$JOBS $CORPUS
+[ -e $EXECUTABLE_NAME_BASE ] && ./$EXECUTABLE_NAME_BASE -exit_on_src_pos=re2/dfa.cc:474 -exit_on_src_pos=re2/dfa.cc:474  -runs=1000000 -jobs=$JOBS -workers=$JOBS $CORPUS
 grep "INFO: found line matching 're2/dfa.cc:474', exiting." fuzz-0.log
