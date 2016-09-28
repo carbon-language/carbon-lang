9e447e9726b12a07ed147a093339f127d5f94474
Aleksei Sidorin <a.sidorin@samsung.com>
2016-09-28 11:04:42 +0000

    ASTMerge: explicitly specify arch for GCCAsmStmt test to calm non-x86 buildbots
    This should fix r282572.
    
    llvm-svn: 282578

---
+++ clang/test/ASTMerge/asm.cpp
@@ -0,0 +1,7 @@
+// RUN: %clang_cc1 -triple i386-unknown-unknown -fcxx-exceptions -emit-pch -o %t.1.ast %S/Inputs/asm-function.cpp
+// RUN: %clang_cc1 -triple i386-unknown-unknown -fcxx-exceptions -ast-merge %t.1.ast -fsyntax-only -verify %s
+// expected-no-diagnostics
+
+void testAsmImport() {
+  asmFunc(12, 42);
+}
