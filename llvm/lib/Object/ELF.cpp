35047ed7416d53b32a71ea98e802e3e29adfe350
Dylan McKay <dylanmckay34@gmail.com>
2016-09-28 13:23:42 +0000

    [AVR] Handle AVR relocations when handling ELF files
    llvm-svn: 282586

--- llvm/lib/Object/ELF.cpp
+++ llvm/lib/Object/ELF.cpp
@@ -54,6 +54,13 @@ StringRef getELFRelocationTypeName(uint32_t Machine, uint32_t Type) {
       break;
     }
     break;
+  case ELF::EM_AVR:
+    switch (Type) {
+#include "llvm/Support/ELFRelocs/AVR.def"
+    default:
+      break;
+    }
+    break;
   case ELF::EM_HEXAGON:
     switch (Type) {
 #include "llvm/Support/ELFRelocs/Hexagon.def"
