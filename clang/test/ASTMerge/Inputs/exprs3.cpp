2234e0da4c33cd77f82fd4b7c99a54f711e3057c
Aleksei Sidorin <a.sidorin@samsung.com>
2016-09-28 10:57:36 +0000

    ASTMerge: specify arch for GCCAsmStmt test explicitly to calm non-x86 buildbots
    llvm-svn: 282576

--- clang/test/ASTMerge/Inputs/exprs3.cpp
+++ clang/test/ASTMerge/Inputs/exprs3.cpp
@@ -104,17 +104,6 @@ void testOffsetOf() {
 }
 
 
-unsigned char asmFunc(unsigned char a, unsigned char b) {
-  unsigned int la = a;
-  unsigned int lb = b;
-  unsigned int bigres;
-  unsigned char res;
-  __asm__ ("0:\n1:\n" : [bigres] "=la"(bigres) : [la] "0"(la), [lb] "c"(lb) :
-                        "edx", "cc");
-  res = bigres;
-  return res;
-}
-
 int testDefaultArg(int a = 2*2) {
   return a;
 }
