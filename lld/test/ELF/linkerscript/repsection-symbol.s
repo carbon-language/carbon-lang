2d68f8ade40d6a948092561d90142c7cc40c54b6
Rafael Espindola <rafael.espindola@gmail.com>
2016-09-29 23:02:57 +0000

    Break long line. NFC.
    llvm-svn: 282806

--- lld/test/ELF/linkerscript/repsection-symbol.s
+++ lld/test/ELF/linkerscript/repsection-symbol.s
@@ -1,7 +1,9 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
 
-# RUN: echo "SECTIONS {.foo : {foo1 = .; *(.foo.*) foo2 = .; *(.bar) foo3 = .;} }" > %t.script
+# RUN: echo "SECTIONS { \
+# RUN:   .foo : {foo1 = .;  *(.foo.*) foo2 = .;  *(.bar) foo3 = .;} \
+# RUN: }" > %t.script
 # RUN: ld.lld -o %t1 --script %t.script %t -shared
 # RUN: llvm-readobj -t %t1 | FileCheck %s
 
