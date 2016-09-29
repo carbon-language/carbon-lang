5c50539503f9dbef71c1df3eeebde7d12ce5bad9
Joerg Sonnenberger <joerg@bec.de>
2016-09-29 21:07:57 +0000

    HAVE_UNWIND_BACKTRACE -> HAVE__UNWIND_BACKTRACE Check for existance and not truth value.
    llvm-svn: 282767

--- llvm/include/llvm/Config/config.h.cmake
+++ llvm/include/llvm/Config/config.h.cmake
@@ -340,7 +340,7 @@
 #cmakedefine HAVE_UNISTD_H ${HAVE_UNISTD_H}
 
 /* Define to 1 if you have the `_Unwind_Backtrace' function. */
-#cmakedefine HAVE_UNWIND_BACKTRACE ${HAVE_UNWIND_BACKTRACE}
+#cmakedefine HAVE__UNWIND_BACKTRACE ${HAVE__UNWIND_BACKTRACE}
 
 /* Define to 1 if you have the <utime.h> header file. */
 #cmakedefine HAVE_UTIME_H ${HAVE_UTIME_H}
