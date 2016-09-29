a0a1d2dbeeee1c6a41a227e6b3518aa6a9778c3a
Sean Callanan <scallanan@apple.com>
2016-09-29 00:16:37 +0000

    Introduced a null check to avoid a crash in a test on i386.
    llvm-svn: 282653

--- lldb/source/Target/StackFrame.cpp
+++ lldb/source/Target/StackFrame.cpp
@@ -1419,6 +1419,10 @@ ValueObjectSP GetValueForDereferincingOffset(StackFrame &frame,
 
   Error error;
   ValueObjectSP pointee = base->Dereference(error);
+    
+  if (!pointee) {
+    return ValueObjectSP();
+  }
 
   if (offset >= 0 && uint64_t(offset) >= pointee->GetByteSize()) {
     int64_t index = offset / pointee->GetByteSize();
