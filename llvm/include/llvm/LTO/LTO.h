0aff59f32d104df3c6fa620a2efa58349e7df553
Davide Italiano <davide@freebsd.org>
2016-09-29 00:31:45 +0000

    [LTO] Add a FIXME, we shouldn't expose getComdat().
    Thanks to Peter for the suggestion.
    
    llvm-svn: 282655

--- llvm/include/llvm/LTO/LTO.h
+++ llvm/include/llvm/LTO/LTO.h
@@ -175,6 +175,8 @@ public:
       // FIXME: Expose a thread-local flag for module asm symbols.
       return GV && GV->isThreadLocal();
     }
+
+    //FIXME: We shouldn't expose this information.
     Expected<const Comdat *> getComdat() const {
       if (!GV)
         return nullptr;
@@ -191,6 +193,7 @@ public:
         return GO->getComdat();
       return nullptr;
     }
+
     uint64_t getCommonSize() const {
       assert(Flags & object::BasicSymbolRef::SF_Common);
       if (!GV)
