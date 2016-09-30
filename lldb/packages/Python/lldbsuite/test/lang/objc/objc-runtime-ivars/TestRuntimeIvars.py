64bf0509f6c9cbf61f1a167d75130f463f999c4c
Sean Callanan <scallanan@apple.com>
2016-09-30 16:02:28 +0000

    Skip TestRuntimeIvars on i386; the Objective-C V1 runtime doesn't list ivars.
    llvm-svn: 282869

--- lldb/packages/Python/lldbsuite/test/lang/objc/objc-runtime-ivars/TestRuntimeIvars.py
+++ lldb/packages/Python/lldbsuite/test/lang/objc/objc-runtime-ivars/TestRuntimeIvars.py
@@ -3,4 +3,4 @@ from lldbsuite.test import decorators
 
 lldbinline.MakeInlineTest(
     __file__, globals(), [
-        decorators.skipIfFreeBSD, decorators.skipIfLinux, decorators.skipIfWindows])
+        decorators.skipIfFreeBSD, decorators.skipIfLinux, decorators.skipIfWindows, decorators.skipIf(archs=["i386", "i686"])])
