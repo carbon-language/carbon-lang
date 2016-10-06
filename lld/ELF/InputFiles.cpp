24adce95b27e97b972fddcbcd93cf90421f8689d
George Rimar <grimar@accesssoftek.com>
2016-10-06 09:17:55 +0000

    [ELF] - Make checks in ObjectFile<ELFT>::getSection() stricter.
    This patch makes the check for null section stricter, 
    so it is only allowed for STT_SECTION symbols now.
    
    Differential revision: https://reviews.llvm.org/D25231
    
    llvm-svn: 283426

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -377,17 +377,22 @@ template <class ELFT>
 InputSectionBase<ELFT> *
 elf::ObjectFile<ELFT>::getSection(const Elf_Sym &Sym) const {
   uint32_t Index = this->getSectionIndex(Sym);
-  if (Index == 0)
-    return nullptr;
   if (Index >= Sections.size())
     fatal(getFilename(this) + ": invalid section index: " + Twine(Index));
   InputSectionBase<ELFT> *S = Sections[Index];
+
   // We found that GNU assembler 2.17.50 [FreeBSD] 2007-07-03
   // could generate broken objects. STT_SECTION symbols can be
   // associated with SHT_REL[A]/SHT_SYMTAB/SHT_STRTAB sections.
   // In this case it is fine for section to be null here as we
   // do not allocate sections of these types.
-  if (!S || S == &InputSectionBase<ELFT>::Discarded)
+  if (!S) {
+    if (Index == 0 || Sym.getType() == STT_SECTION)
+      return nullptr;
+    fatal(getFilename(this) + ": invalid section index: " + Twine(Index));
+  }
+
+  if (S == &InputSectionBase<ELFT>::Discarded)
     return S;
   return S->Repl;
 }
