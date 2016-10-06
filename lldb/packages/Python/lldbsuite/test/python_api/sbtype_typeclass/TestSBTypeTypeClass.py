83380c2a00e680b5c12404a12fa5bbb11df101ee
Todd Fiala <todd.fiala@gmail.com>
2016-10-06 19:23:29 +0000

    xfail TestSBTypeTypeClass.py on macOS i386
    Tracked by:
    rdar://28656677
    
    llvm-svn: 283484

--- lldb/packages/Python/lldbsuite/test/python_api/sbtype_typeclass/TestSBTypeTypeClass.py
+++ lldb/packages/Python/lldbsuite/test/python_api/sbtype_typeclass/TestSBTypeTypeClass.py
@@ -3,4 +3,8 @@ from lldbsuite.test import lldbinline
 
 lldbinline.MakeInlineTest(
     __file__, globals(), [
-        decorators.skipIfFreeBSD, decorators.skipIfLinux, decorators.skipIfWindows])
+        decorators.skipIfFreeBSD, decorators.skipIfLinux,
+        decorators.skipIfWindows,
+        decorators.expectedFailureAll(
+            oslist=['macosx'], archs=['i386'],
+            bugnumber='rdar://28656677')])
