f1ccf72d386c11fbcf7e73cad9932f33ac580c18
Vedant Kumar <vsk@apple.com>
2016-10-11 21:56:05 +0000

    [Profile] Update testcase for r283948 (NFC)
    Old: "__DATA,__llvm_prf_data"
    New: "__DATA,__llvm_prf_data,regular,live_support"
    
    This should fix the following bot failure:
    
      http://bb.pgr.jp/builders/cmake-clang-x86_64-linux/builds/55158
    
    llvm-svn: 283949

--- clang/test/Profile/c-linkage-available_externally.c
+++ clang/test/Profile/c-linkage-available_externally.c
@@ -3,7 +3,7 @@
 // RUN: %clang_cc1 -O2 -triple x86_64-apple-macosx10.9 -main-file-name c-linkage-available_externally.c %s -o - -emit-llvm -fprofile-instrument=clang | FileCheck %s
 
 // CHECK: @__profc_foo = linkonce_odr hidden global [1 x i64] zeroinitializer, section "__DATA,__llvm_prf_cnts", align 8
-// CHECK: @__profd_foo = linkonce_odr hidden global {{.*}} i64* getelementptr inbounds ([1 x i64], [1 x i64]* @__profc_foo, i32 0, i32 0){{.*}}, section "__DATA,__llvm_prf_data", align 8
+// CHECK: @__profd_foo = linkonce_odr hidden global {{.*}} i64* getelementptr inbounds ([1 x i64], [1 x i64]* @__profc_foo, i32 0, i32 0){{.*}}, section "__DATA,__llvm_prf_data,regular,live_support", align 8
 inline int foo(void) { return 1; }
 
 int main(void) {
