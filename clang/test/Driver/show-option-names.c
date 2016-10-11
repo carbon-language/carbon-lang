b81cf735c3e5fb98f5ea760dde0daff38da5cc5c
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2016-10-11 18:31:45 +0000

    [Driver] Fix test from r283913 to unbreak bots
    Followup from r283913 & r283827
    
    http://bb.pgr.jp/builders/cmake-clang-x86_64-linux/builds/55135
    
    llvm-svn: 283915

--- clang/test/Driver/show-option-names.c
+++ clang/test/Driver/show-option-names.c
@@ -1,5 +1,7 @@
-// RUN: %clang -c -isysroot /FOO %s 2>&1 | FileCheck --check-prefix=CHECK-SHOW-OPTION-NAMES %s
+// REQUIRES: x86-registered-target
+
+// RUN: %clang -target x86_64-apple-darwin -c -isysroot /FOO %s 2>&1 | FileCheck --check-prefix=CHECK-SHOW-OPTION-NAMES %s
 // CHECK-SHOW-OPTION-NAMES: warning: no such sysroot directory: '{{([A-Za-z]:.*)?}}/FOO' [-Wmissing-sysroot]
 
-// RUN: %clang -c -fno-diagnostics-show-option -isysroot /FOO %s 2>&1 | FileCheck --check-prefix=CHECK-NO-SHOW-OPTION-NAMES %s
+// RUN: %clang -target x86_64-apple-darwin -c -fno-diagnostics-show-option -isysroot /FOO %s 2>&1 | FileCheck --check-prefix=CHECK-NO-SHOW-OPTION-NAMES %s
 // CHECK-NO-SHOW-OPTION-NAMES: warning: no such sysroot directory: '{{([A-Za-z]:.*)?}}/FOO'{{$}}
