e9587bd771724fbb2a35c828500c6e5b8bfe009c
NAKAMURA Takumi <geek4civic@gmail.com>
2016-10-11 17:38:30 +0000

    ARMMachineFunctionInfo.cpp: Add an initializer of ARMFunctionInfo::ReturnRegsCount in the explicit ctor.
    It caused crash since r283867.
    
    llvm-svn: 283909

--- llvm/lib/Target/ARM/ARMMachineFunctionInfo.cpp
+++ llvm/lib/Target/ARM/ARMMachineFunctionInfo.cpp
@@ -16,8 +16,8 @@ void ARMFunctionInfo::anchor() {}
 ARMFunctionInfo::ARMFunctionInfo(MachineFunction &MF)
     : isThumb(MF.getSubtarget<ARMSubtarget>().isThumb()),
       hasThumb2(MF.getSubtarget<ARMSubtarget>().hasThumb2()),
-      StByValParamsPadding(0), ArgRegsSaveSize(0), HasStackFrame(false),
-      RestoreSPFromFP(false), LRSpilledForFarJump(false),
+      StByValParamsPadding(0), ArgRegsSaveSize(0), ReturnRegsCount(0),
+      HasStackFrame(false), RestoreSPFromFP(false), LRSpilledForFarJump(false),
       FramePtrSpillOffset(0), GPRCS1Offset(0), GPRCS2Offset(0), DPRCSOffset(0),
       GPRCS1Size(0), GPRCS2Size(0), DPRCSSize(0), PICLabelUId(0),
       VarArgsFrameIndex(0), HasITBlocks(false), ArgumentStackSize(0),
