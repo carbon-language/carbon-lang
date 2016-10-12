e68f1add5019aab9df250b20ff7c84629ca49c56
George Rimar <grimar@accesssoftek.com>
2016-10-12 09:20:28 +0000

    [Support/ELF] - Sort PT_OPENBSD_* added previously. NFC.
    llvm-svn: 283992

--- llvm/include/llvm/Support/ELF.h
+++ llvm/include/llvm/Support/ELF.h
@@ -1036,11 +1036,11 @@ enum {
   PT_SUNW_EH_FRAME = 0x6474e550,
   PT_SUNW_UNWIND   = 0x6464e550,
 
+  PT_GNU_STACK = 0x6474e551, // Indicates stack executability.
+  PT_GNU_RELRO = 0x6474e552, // Read-only after relocation.
+
   PT_OPENBSD_RANDOMIZE = 0x65a3dbe6, // Fill with random data.
   PT_OPENBSD_WXNEEDED  = 0x65a3dbe7, // Program does W^X violations.
-
-  PT_GNU_STACK  = 0x6474e551, // Indicates stack executability.
-  PT_GNU_RELRO  = 0x6474e552, // Read-only after relocation.
 
   // ARM program header types.
   PT_ARM_ARCHEXT = 0x70000000, // Platform architecture compatibility info
