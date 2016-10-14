ccc6502abcbff4489261cc87e099f148248c22c0
Eric Fiselier <eric@efcs.ca>
2016-10-14 09:12:53 +0000

    Mark test as unsupported without threads
    llvm-svn: 284217

--- libcxxabi/test/thread_local_destruction_order.pass.cpp
+++ libcxxabi/test/thread_local_destruction_order.pass.cpp
@@ -8,6 +8,7 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++98, c++03
+// UNSUPPORTED: libcxxabi-no-threads
 
 #include <cassert>
 #include <thread>
