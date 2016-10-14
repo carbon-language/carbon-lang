c37a49896a414b5dcea12a637283f07dd80f0ec6
David Sheinkman <davidsheinkman@outlook.com>
2016-10-14 20:43:37 +0000

    __builtin_fpclassify missing one int parameter
    Patch by Tania Albarghouthi.
    
    Differential Revision: https://reviews.llvm.org/D25480
    
    llvm-svn: 284277

--- clang/test/Sema/builtin-unary-fp.c
+++ clang/test/Sema/builtin-unary-fp.c
@@ -11,6 +11,7 @@ void a() {
   check(__builtin_isnan(1,2)); // expected-error{{too many arguments}}
   check(__builtin_fpclassify(0, 0, 0, 0, 0, 1.0));
   check(__builtin_fpclassify(0, 0, 0, 0, 0, 1)); // expected-error{{requires argument of floating point type}}
+  check(__builtin_fpclassify(0, 1, 2, 3, 4.5, 5.0)); // expected-warning{{implicit conversion from 'double' to 'int' changes value from 4.5 to 4}}
   check(__builtin_fpclassify(0, 0, 0, 0, 1)); // expected-error{{too few arguments}}
   check(__builtin_fpclassify(0, 0, 0, 0, 0, 1, 0)); // expected-error{{too many arguments}}
 }
