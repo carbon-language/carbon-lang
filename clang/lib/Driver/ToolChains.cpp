cdcd403bc2223e6ed71323fe2b36030e9a7bc674
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2016-10-17 13:23:41 +0000

    Return correct path from HexagonToolChain::getHexagonTargetDir
    This problem was exposed by r284129, causing clang-hexagon-elf to fail
    clang tests.
    
    llvm-svn: 284383

--- clang/lib/Driver/ToolChains.cpp
+++ clang/lib/Driver/ToolChains.cpp
@@ -2970,7 +2970,7 @@ std::string HexagonToolChain::getHexagonTargetDir(
   if (getVFS().exists(InstallRelDir = InstalledDir + "/../target"))
     return InstallRelDir;
 
-  return InstallRelDir;
+  return InstalledDir;
 }
 
 Optional<unsigned> HexagonToolChain::getSmallDataThreshold(
