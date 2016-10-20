b663f1b4071abdf2df7efe7468ccb1602428cb49
Peter Smith <peter.smith@linaro.org>
2016-10-20 10:16:13 +0000

    [ELF] Add REQUIRES: arm to test to fix buildbot without ARM backend.
    llvm-svn: 284711

--- lld/test/ELF/arm-exidx-shared.s
+++ lld/test/ELF/arm-exidx-shared.s
@@ -2,6 +2,7 @@
 // RUN: ld.lld %t --shared -o %t2 2>&1
 // RUN: llvm-readobj --relocations %t2 | FileCheck %s
 // RUN: llvm-objdump -s -triple=armv7a-none-linux-gnueabi %t2 | FileCheck -check-prefix=CHECK-EXTAB %s
+// REQUIRES: arm
 
 // Check that the relative R_ARM_PREL31 relocation can access a PLT entry
 // for when the personality routine is referenced from a shared library.
