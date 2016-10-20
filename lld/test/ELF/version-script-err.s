613c00db88e0a206e1223e5be7dfeb9b4a868c7d
Reid Kleckner <rnk@google.com>
2016-10-20 20:02:24 +0000

    Add a space to work around a Windows CPython / MSys quoting bug
    Summary:
    The rules for quoting the command line that a subprocess receives are
    user space conventions implemented by the C runtime. Python's quoting
    rules are implemented here:
    https://github.com/python/cpython/blob/c30098c8c6014f3340a369a31df9c74bdbacc269/Lib/subprocess.py#L725
    
    The result is that the final command line C string computed by Python is
    'echo \"'. Mingw doesn't appear to interpret that backslash as escaping
    the quote because it is not already inside a quoted region. As a result,
    our echo command prints a single backslash instead of a quote.
    
    The whole issue can be sidestepped by adding a space a forcing Python to
    put the argument to echo in double quotes.
    
    Reviewers: inglorion, ruiu
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D25841
    
    llvm-svn: 284768

--- lld/test/ELF/version-script-err.s
+++ lld/test/ELF/version-script-err.s
@@ -4,7 +4,7 @@
 // RUN: not ld.lld -shared %t.o -o %t.so --version-script %p/Inputs/version-script-err.script 2>&1 | FileCheck %s
 // CHECK: ; expected, but got }
 
-// RUN: echo    "\"" > %terr1.script
+// RUN: echo    " \"" > %terr1.script
 // RUN: not ld.lld --version-script %terr1.script -shared %t.o -o %t.so 2>&1 | \
 // RUN:   FileCheck -check-prefix=ERR1 %s
 // ERR1: unclosed quote
