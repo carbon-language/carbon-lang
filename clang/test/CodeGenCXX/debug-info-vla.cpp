11eabd4cde8e03155b3bd78204c1db1746bf0bf1
Victor Leschuk <vleschuk@accesssoftek.com>
2016-10-20 00:37:07 +0000

    Upgrade debug-info-vla test: we do not pass default alignment now
    llvm-svn: 284683

--- clang/test/CodeGenCXX/debug-info-vla.cpp
+++ clang/test/CodeGenCXX/debug-info-vla.cpp
@@ -15,7 +15,6 @@ int (*fp)(int[][*]) = nullptr;
 //
 // CHECK: !DICompositeType(tag: DW_TAG_array_type,
 // CHECK-NOT:                               size:
-// CHECK-SAME:                              align: 32
 // CHECK-SAME:                              elements: [[ELEM_TYPE:![0-9]+]]
 // CHECK: [[ELEM_TYPE]] = !{[[THREE:.*]], [[NOCOUNT]]}
 // CHECK: [[THREE]] = !DISubrange(count: 3)
