d11fdad33ef1ba9a8329339490f1d1f6b971cbbc
Eric Fiselier <eric@efcs.ca>
2016-10-23 22:24:11 +0000

    Backout enabling -Wshadow until I have time to fix the breakage
    llvm-svn: 284952

--- libcxx/test/libcxx/test/config.py
+++ libcxx/test/libcxx/test/config.py
@@ -627,7 +627,8 @@ class Configuration(object):
                 '-D_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER',
                 '-Wall', '-Wextra', '-Werror'
             ]
-            self.cxx.addWarningFlagIfSupported('-Wshadow')
+            # FIXME turn this back on after fixing potential breakage.
+            #self.cxx.addWarningFlagIfSupported('-Wshadow')
             self.cxx.addWarningFlagIfSupported('-Wno-unused-command-line-argument')
             self.cxx.addWarningFlagIfSupported('-Wno-attributes')
             self.cxx.addWarningFlagIfSupported('-Wno-pessimizing-move')
