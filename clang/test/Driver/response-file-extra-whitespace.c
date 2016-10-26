e38a8d2f14c514d3a088283cab2e5d4fb7ec3ad8
Rui Ueyama <ruiu@google.com>
2016-10-26 03:38:48 +0000

    Use printf instead of "echo -ne".
    Not all echo commands support "-e".
    
    llvm-svn: 285162

--- clang/test/Driver/response-file-extra-whitespace.c
+++ clang/test/Driver/response-file-extra-whitespace.c
@@ -3,7 +3,7 @@
 // some joined arguments (like "-x c") across lines to ensure that regular
 // clang (not clang-cl) can process it correctly.
 //
-// RUN: echo -en "-x\r\nc\r\n-DTEST\r\n" > %t.0.txt
+// RUN: printf " -x\r\nc\r\n-DTEST\r\n" > %t.0.txt
 // RUN: %clang -E @%t.0.txt %s -v 2>&1 | FileCheck %s -check-prefix=SHORT
 // SHORT: extern int it_works;
 
