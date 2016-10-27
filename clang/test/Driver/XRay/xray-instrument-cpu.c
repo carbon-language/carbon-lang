9d61e14771e52134091441f147590d6094a8c65e
Benjamin Kramer <benny.kra@googlemail.com>
2016-10-27 08:49:18 +0000

    [Xray] Don't generate output for xray tests
    Patch by Sam McCall!
    
    Differential Revision: http://reviews.llvm.org/D26024
    
    llvm-svn: 285276

--- clang/test/Driver/XRay/xray-instrument-cpu.c
+++ clang/test/Driver/XRay/xray-instrument-cpu.c
@@ -1,4 +1,4 @@
-// RUN: not %clang -v -fxray-instrument -c %s
+// RUN: not %clang -o /dev/null -v -fxray-instrument -c %s
 // XFAIL: amd64-, x86_64-, x86_64h-, arm
 // REQUIRES: linux
 typedef int a;
--- clang/test/Driver/XRay/xray-instrument-os.c
+++ clang/test/Driver/XRay/xray-instrument-os.c
@@ -1,4 +1,4 @@
-// RUN: not %clang -v -fxray-instrument -c %s
+// RUN: not %clang -o /dev/null -v -fxray-instrument -c %s
 // XFAIL: -linux-
 // REQUIRES-ANY: amd64, x86_64, x86_64h, arm
 typedef int a;
