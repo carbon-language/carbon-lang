a0d9acafe11969665939a572538eb1bcc53d8e1b
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2016-10-28 02:37:04 +0000

    [Modules] Make test from r285377 darwin specific
    Appease buildbots:
    http://bb.pgr.jp/builders/cmake-clang-x86_64-linux/builds/55876
    
    llvm-svn: 285378

--- clang/test/Modules/umbrella-header-include-builtin.mm
+++ clang/test/Modules/umbrella-header-include-builtin.mm
@@ -1,3 +1,5 @@
+// REQUIRES: system-darwin
+
 // RUN: rm -rf %t
 // RUN: %clang -cc1 -fsyntax-only -nostdinc++ -isysroot %S/Inputs/libc-libcxx/sysroot -isystem %S/Inputs/libc-libcxx/sysroot/usr/include/c++/v1 -F%S/Inputs/libc-libcxx/sysroot/Frameworks -std=c++11 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -x objective-c++ %s
 
