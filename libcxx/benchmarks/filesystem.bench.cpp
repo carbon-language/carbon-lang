a55333003d33ceb5857c315093bdd7e58af4ee92
Eric Fiselier <eric@efcs.ca>
2016-10-31 02:46:25 +0000

    Optimize filesystem::path by providing weaker exception guarantees.
    path uses string::append to construct, append, and concatenate paths. Unfortunatly
    string::append has a strong exception safety guaranteed and if it can't prove
    that the iterator operations don't throw then it will allocate a temporary
    string copy to append to. However this extra allocation and copy is very
    undesirable for path which doesn't have the same exception guarantees.
    
    To work around this this patch adds string::__append_forward_unsafe which exposes
    the std::string::append interface for forward iterators without enforcing
    that the iterator is noexcept.
    
    llvm-svn: 285532

--- libcxx/benchmarks/filesystem.bench.cpp
+++ libcxx/benchmarks/filesystem.bench.cpp
@@ -2,6 +2,7 @@
 
 #include "benchmark/benchmark_api.h"
 #include "GenerateInput.hpp"
+#include "test_iterators.h"
 
 namespace fs = std::experimental::filesystem;
 
@@ -40,6 +41,39 @@ void BM_PathConstructCStr(benchmark::State &st, GenInputs gen) {
 }
 BENCHMARK_CAPTURE(BM_PathConstructCStr, large_string,
   getRandomStringInputs)->Arg(TestNumInputs);
+
+
+template <template <class...> class ItType, class GenInputs>
+void BM_PathConstructIter(benchmark::State &st, GenInputs gen) {
+  using namespace fs;
+  using Iter = ItType<std::string::const_iterator>;
+  const auto in = gen(st.range(0));
+  path PP;
+  for (auto& Part : in)
+    PP /= Part;
+  auto Start = Iter(PP.native().begin());
+  auto End = Iter(PP.native().end());
+  benchmark::DoNotOptimize(PP.native().data());
+  benchmark::DoNotOptimize(Start);
+  benchmark::DoNotOptimize(End);
+  while (st.KeepRunning()) {
+    const path P(Start, End);
+    benchmark::DoNotOptimize(P.native().data());
+  }
+}
+template <class GenInputs>
+void BM_PathConstructInputIter(benchmark::State &st, GenInputs gen) {
+  BM_PathConstructIter<input_iterator>(st, gen);
+}
+template <class GenInputs>
+void BM_PathConstructForwardIter(benchmark::State &st, GenInputs gen) {
+  BM_PathConstructIter<forward_iterator>(st, gen);
+}
+BENCHMARK_CAPTURE(BM_PathConstructInputIter, large_string,
+  getRandomStringInputs)->Arg(TestNumInputs);
+BENCHMARK_CAPTURE(BM_PathConstructForwardIter, large_string,
+  getRandomStringInputs)->Arg(TestNumInputs);
+
 
 template <class GenInputs>
 void BM_PathIterateMultipleTimes(benchmark::State &st, GenInputs gen) {
