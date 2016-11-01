d03c2546621eec79b248e7df4f0eaec939fb7738
Malcolm Parsons <malcolm.parsons@gmail.com>
2016-11-01 20:07:05 +0000

    [clang-query] Fix Clang-tidy readability-redundant-string-cstr warnings
    Reviewers: pcc, dblaikie
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D26205
    
    llvm-svn: 285731

--- clang-tools-extra/clang-query/tool/ClangQuery.cpp
+++ clang-tools-extra/clang-query/tool/ClangQuery.cpp
@@ -80,7 +80,7 @@ int main(int argc, const char **argv) {
     for (cl::list<std::string>::iterator I = Commands.begin(),
                                          E = Commands.end();
          I != E; ++I) {
-      QueryRef Q = QueryParser::parse(I->c_str(), QS);
+      QueryRef Q = QueryParser::parse(*I, QS);
       if (!Q->run(llvm::outs(), QS))
         return 1;
     }
@@ -97,7 +97,7 @@ int main(int argc, const char **argv) {
         std::string Line;
         std::getline(Input, Line);
 
-        QueryRef Q = QueryParser::parse(Line.c_str(), QS);
+        QueryRef Q = QueryParser::parse(Line, QS);
         if (!Q->run(llvm::outs(), QS))
           return 1;
       }
