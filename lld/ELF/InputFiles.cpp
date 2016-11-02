c17e0b6f68dc9ee7bf3b165062fb9f878a1e9576
Rafael Espindola <rafael.espindola@gmail.com>
2016-11-02 13:36:31 +0000

    Simplify SHF_LINK_ORDER handling. NFC.
    While ARM is the only currently user we support this is a general
    feature.
    
    This avoids a second walk over the sections.
    
    llvm-svn: 285805

--- lld/ELF/InputFiles.cpp
+++ lld/ELF/InputFiles.cpp
@@ -205,8 +205,6 @@ void elf::ObjectFile<ELFT>::parse(DenseSet<CachedHashStringRef> &ComdatGroups) {
   // Read section and symbol tables.
   initializeSections(ComdatGroups);
   initializeSymbols();
-  if (Config->GcSections && Config->EMachine == EM_ARM)
-    initializeReverseDependencies();
 }
 
 // Sections with SHT_GROUP and comdat bits define comdat section groups.
@@ -331,24 +329,16 @@ void elf::ObjectFile<ELFT>::initializeSections(
     default:
       Sections[I] = createInputSection(Sec, SectionStringTable);
     }
-  }
-}
 
-// .ARM.exidx sections have a reverse dependency on the InputSection they
-// have a SHF_LINK_ORDER dependency, this is identified by the sh_link.
-template <class ELFT>
-void elf::ObjectFile<ELFT>::initializeReverseDependencies() {
-  unsigned I = -1;
-  for (const Elf_Shdr &Sec : this->ELFObj.sections()) {
-    ++I;
-    if ((Sections[I] == &InputSection<ELFT>::Discarded) ||
-        !(Sec.sh_flags & SHF_LINK_ORDER))
-      continue;
-    if (Sec.sh_link >= Sections.size())
-      fatal(getFilename(this) + ": invalid sh_link index: " +
-            Twine(Sec.sh_link));
-    auto *IS = cast<InputSection<ELFT>>(Sections[Sec.sh_link]);
-    IS->DependentSection = Sections[I];
+    // .ARM.exidx sections have a reverse dependency on the InputSection they
+    // have a SHF_LINK_ORDER dependency, this is identified by the sh_link.
+    if (Sec.sh_flags & SHF_LINK_ORDER) {
+      if (Sec.sh_link >= Sections.size())
+        fatal(getFilename(this) + ": invalid sh_link index: " +
+              Twine(Sec.sh_link));
+      auto *IS = cast<InputSection<ELFT>>(Sections[Sec.sh_link]);
+      IS->DependentSection = Sections[I];
+    }
   }
 }
 
