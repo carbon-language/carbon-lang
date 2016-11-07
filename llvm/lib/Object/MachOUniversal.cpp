2b5ba7bae667faccb096d15bb7fdb35d986a6a90
Davide Italiano <davide@freebsd.org>
2016-11-07 21:01:42 +0000

    [lib/Object] Modernize. NFCI.
    llvm-svn: 286146

--- llvm/lib/Object/MachOUniversal.cpp
+++ llvm/lib/Object/MachOUniversal.cpp
@@ -157,10 +157,9 @@ MachOUniversalBinary::getObjectForArch(StringRef ArchName) const {
                                               ArchName,
                                           object_error::arch_not_found);
 
-  for (object_iterator I = begin_objects(), E = end_objects(); I != E; ++I) {
-    if (I->getArchTypeName() == ArchName)
-      return I->getAsObjectFile();
-  }
+  for (auto &Obj : objects())
+    if (Obj.getArchTypeName() == ArchName)
+      return Obj.getAsObjectFile();
   return make_error<GenericBinaryError>("fat file does not "
                                         "contain " +
                                             ArchName,
