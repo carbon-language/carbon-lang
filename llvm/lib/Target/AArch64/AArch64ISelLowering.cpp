614b44bbe982dfd5930f228b3b0a5d084fd541e4
Amara Emerson <amara.emerson@arm.com>
2016-11-07 15:42:12 +0000

    This patch adds support for 16 bit floating point registers to the inline asm register selection on AArch64.
    Without this patch, register allocation for the example below fails.
    
    define half @test(half %a1, half %a2) #0 {
    entry:
      %0 = tail call half asm "sqrshl ${0:h}, ${1:h}, ${2:h}", "=w,w,w" (half %a1, half %a2) #1
      ret half %0
    }
    
    Patch by Florian Hahn.
    
    Differential Revision: https://reviews.llvm.org/D25080
    
    llvm-svn: 286111

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -4780,6 +4780,8 @@ AArch64TargetLowering::getRegForInlineAsmConstraint(
         return std::make_pair(0U, &AArch64::GPR64commonRegClass);
       return std::make_pair(0U, &AArch64::GPR32commonRegClass);
     case 'w':
+      if (VT.getSizeInBits() == 16)
+        return std::make_pair(0U, &AArch64::FPR16RegClass);
       if (VT.getSizeInBits() == 32)
         return std::make_pair(0U, &AArch64::FPR32RegClass);
       if (VT.getSizeInBits() == 64)
