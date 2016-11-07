4a3f94c442e497e5fc5a128413de14eff4cbfb98
Felix Berger <flx@google.com>
2016-11-07 21:45:58 +0000

    [clang-tidy] Move incomplete type test into separate test file
    Summary: Move in complete type test which does not compile into its own test file.
    
    Reviewers: alexfh, sbenza, aaron.ballman
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D26369
    
    llvm-svn: 286155

---
+++ clang-tools-extra/test/clang-tidy/performance-unnecessary-value-param-incomplete-type.cpp
@@ -0,0 +1,9 @@
+// RUN: %check_clang_tidy %s performance-unnecessary-value-param %t -- -fix-errors -- --std=c++11
+
+// Ensure that incomplete types result in an error from the frontend and not a
+// clang-tidy diagnostic about IncompleteType being expensive to copy.
+struct IncompleteType;
+void NegativeForIncompleteType(IncompleteType I) {
+  // CHECK-MESSAGES: [[@LINE-1]]:47: error: variable has incomplete type 'IncompleteType' [clang-diagnostic-error]
+}
+
--- clang-tools-extra/test/clang-tidy/performance-unnecessary-value-param.cpp
+++ clang-tools-extra/test/clang-tidy/performance-unnecessary-value-param.cpp
@@ -1,4 +1,4 @@
-// RUN: %check_clang_tidy %s performance-unnecessary-value-param %t -- -fix-errors -- --std=c++11
+// RUN: %check_clang_tidy %s performance-unnecessary-value-param %t
 
 // CHECK-FIXES: #include <utility>
 
@@ -236,13 +236,6 @@ void PositiveConstRefNotMoveAssignable(ExpensiveToCopyType A) {
   // CHECK-FIXES: void PositiveConstRefNotMoveAssignable(const ExpensiveToCopyType& A) {
   ExpensiveToCopyType B;
   B = A;
-}
-
-// Ensure that incomplete types result in an error from the frontend and not a
-// clang-tidy diagnostic about IncompleteType being expensive to copy.
-struct IncompleteType;
-void NegativeForIncompleteType(IncompleteType I) {
-  // CHECK-MESSAGES: [[@LINE-1]]:47: error: variable has incomplete type 'IncompleteType' [clang-diagnostic-error]
 }
 
 // Case where parameter in declaration is already const-qualified but not in
