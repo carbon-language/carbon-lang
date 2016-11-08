09d52e1cd45232337c0a5ef68fd986f74b91f053
Dominic Chen <d.c.ddcc@gmail.com>
2016-11-08 22:39:14 +0000

    [analyzer] Provide Contains() on ImmutableMap program state partial trait.
    Reviewers: zaks.anna, dcoughlin
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D26373
    
    llvm-svn: 286306

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/ProgramStateTrait.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/ProgramStateTrait.h
@@ -8,7 +8,7 @@
 //===----------------------------------------------------------------------===//
 //
 //  This file defines partial implementations of template specializations of
-//  the class ProgramStateTrait<>.  ProgramStateTrait<> is used by ProgramState 
+//  the class ProgramStateTrait<>.  ProgramStateTrait<> is used by ProgramState
 //  to implement set/get methods for manipulating a ProgramState's
 //  generic data map.
 //
@@ -79,6 +79,10 @@ namespace ento {
 
     static data_type Remove(data_type B, key_type K, context_type F) {
       return F.remove(B, K);
+    }
+
+    static bool Contains(data_type B, key_type K) {
+      return B.contains(K);
     }
 
     static inline context_type MakeContext(void *p) {
@@ -185,7 +189,7 @@ namespace ento {
     }
   };
 
-  
+
   // Partial specialization for bool.
   template <> struct ProgramStatePartialTrait<bool> {
     typedef bool data_type;
@@ -198,7 +202,7 @@ namespace ento {
       return (void*) (uintptr_t) d;
     }
   };
-  
+
   // Partial specialization for unsigned.
   template <> struct ProgramStatePartialTrait<unsigned> {
     typedef unsigned data_type;
