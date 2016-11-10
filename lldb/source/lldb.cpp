b92cfe68044ecbde49562b99d4e11b92fb386a91
Chris Bieneman <beanz@apple.com>
2016-11-10 17:33:19 +0000

    Unify Darwin and Non-Darwin printing of version output
    Summary:
    This change unifies and simplifies the code paths between the Darwin and non-Darwin code to print the LLDB version information.
    
    It also introduces a new variable in CMake LLDB_VERSION_STRING which can be used to specify custom version information. On Darwin this value is implicitly set based on the resource/LLDB-Info.plist file.
    
    With the LLDB_VERSION_STRING variable set to lldb-360.99.0, the -version output is:
    
    > ./bin/lldb -version
    lldb version 4.0.0 (lldb-360.99.0)
      clang revision 286264
      llvm revision 286265
    
    This behavior is unified across all target platforms.
    
    Reviewers: lldb-commits
    
    Subscribers: mgorny, tfiala
    
    Differential Revision: https://reviews.llvm.org/D26478
    
    llvm-svn: 286479

--- lldb/source/lldb.cpp
+++ lldb/source/lldb.cpp
@@ -12,14 +12,14 @@
 using namespace lldb;
 using namespace lldb_private;
 
-#if defined(__APPLE__)
-extern "C" const unsigned char liblldb_coreVersionString[];
-#else
-
 #include "clang/Basic/Version.h"
 
 #ifdef HAVE_SVN_VERSION_INC
 #  include "SVNVersion.inc"
+#endif
+
+#ifdef HAVE_APPLE_VERSION_INC
+#  include "AppleVersion.inc"
 #endif
 
 static const char *GetLLDBRevision() {
@@ -38,33 +38,8 @@ static const char *GetLLDBRepository() {
 #endif
 }
 
-#endif
 
 const char *lldb_private::GetVersion() {
-#if defined(__APPLE__)
-  static char g_version_string[32];
-  if (g_version_string[0] == '\0') {
-    const char *version_string =
-        ::strstr((const char *)liblldb_coreVersionString, "PROJECT:");
-
-    if (version_string)
-      version_string += sizeof("PROJECT:") - 1;
-    else
-      version_string = "unknown";
-
-    const char *newline_loc = strchr(version_string, '\n');
-
-    size_t version_len = sizeof(g_version_string) - 1;
-
-    if (newline_loc &&
-        (newline_loc - version_string < static_cast<ptrdiff_t>(version_len)))
-      version_len = newline_loc - version_string;
-
-    ::snprintf(g_version_string, version_len + 1, "%s", version_string);
-  }
-
-  return g_version_string;
-#else
   // On platforms other than Darwin, report a version number in the same style
   // as the clang tool.
   static std::string g_version_str;
@@ -81,21 +56,24 @@ const char *lldb_private::GetVersion() {
     if (lldb_rev) {
       g_version_str += " revision ";
       g_version_str += lldb_rev;
+      g_version_str += ")";
     }
+#ifdef LLDB_VERSION_STRING
+    g_version_str += " (";
+    g_version_str += LLDB_VERSION_STRING;
+    g_version_str += ")";
+#endif
     std::string clang_rev(clang::getClangRevision());
     if (clang_rev.length() > 0) {
-      g_version_str += " clang revision ";
+      g_version_str += "\n  clang revision ";
       g_version_str += clang_rev;
     }
     std::string llvm_rev(clang::getLLVMRevision());
     if (llvm_rev.length() > 0) {
-      g_version_str += " llvm revision ";
+      g_version_str += "\n  llvm revision ";
       g_version_str += llvm_rev;
     }
-
-    if (lldb_repo)
-      g_version_str += ")";
+      
   }
   return g_version_str.c_str();
-#endif
 }
