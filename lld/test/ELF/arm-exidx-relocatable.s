eb1c2a0dace5786086f54ad2303d604e07f14b6e
Rafael Espindola <rafael.espindola@gmail.com>
2016-11-11 22:07:15 +0000

    Make test more strict. NFC.
    It was non-obvious that a CHECK was not matching the following section.
    
    llvm-svn: 286643

--- lld/test/ELF/arm-exidx-relocatable.s
+++ lld/test/ELF/arm-exidx-relocatable.s
@@ -47,52 +47,81 @@ f3:
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:         Size: 24
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 24
 // CHECK-NEXT:    Link: 7
+
 // CHECK:         Name: .ARM.exidx.text.f1
 // CHECK-NEXT:    Type: SHT_ARM_EXIDX (0x70000001)
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:    Size: 8
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 8
 // CHECK-NEXT:    Link: 8
+
 // CHECK:         Name: .ARM.exidx.text.f2
 // CHECK-NEXT:    Type: SHT_ARM_EXIDX (0x70000001)
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:         Size: 16
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 16
 // CHECK-NEXT:    Link: 9
+
 // CHECK:         Name: .ARM.exidx.func1
 // CHECK-NEXT:    Type: SHT_ARM_EXIDX (0x70000001)
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:         Size: 8
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 8
 // CHECK-NEXT:    Link: 10
+
 // CHECK:         Name: .ARM.exidx.func2
 // CHECK-NEXT:    Type: SHT_ARM_EXIDX (0x70000001)
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:         Size: 8
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 8
 // CHECK-NEXT:    Link: 11
+
 // CHECK:         Name: .ARM.exidx.func3
 // CHECK-NEXT:    Type: SHT_ARM_EXIDX (0x70000001)
 // CHECK-NEXT:    Flags [ (0x82)
 // CHECK-NEXT:      SHF_ALLOC (0x2)
 // CHECK-NEXT:      SHF_LINK_ORDER (0x80)
-// CHECK:         Size: 8
+// CHECK-NEXT:    ]
+// CHECK-NEXT:    Address
+// CHECK-NEXT:    Offset:
+// CHECK-NEXT:    Size: 8
 // CHECK-NEXT:    Link: 12
+
 // CHECK:         Index: 7
 // CHECK-NEXT:    Name: .text
+
 // CHECK:         Index: 8
 // CHECK-NEXT:    Name: .text.f1
+
 // CHECK:         Index: 9
 // CHECK-NEXT:    Name: .text.f2
+
 // CHECK:         Index: 10
 // CHECK-NEXT:    Name: .func1
+
 // CHECK:         Index: 11
 // CHECK-NEXT:    Name: .func2
+
 // CHECK:         Index: 12
 // CHECK-NEXT:    Name: .func3
