c557191b210650b1726d64d67a8b824b4d47cd5f
Vitaly Buka <vitalybuka@google.com>
2016-11-12 04:32:31 +0000

    [asan] Fix strncmp and strncasecmp interceptors
    Summary:
    In non-strict mode we will check memory access for both strings from beginning
    to either:
      1. 0-char
      2. size
      3. different chars
    
    In strict mode we will check from beginning to either:
      1. 0-char
      2. size
    
    Previously in strict mode we always checked up to the 0-char.
    
    Reviewers: kcc, eugenis
    
    Subscribers: llvm-commits, kubabrecka
    
    Differential Revision: https://reviews.llvm.org/D26574
    
    llvm-svn: 286708

--- compiler-rt/test/asan/TestCases/strncmp_strict.c
+++ compiler-rt/test/asan/TestCases/strncmp_strict.c
@@ -1,26 +1,81 @@
 // Test strict_string_checks option in strncmp function
-// RUN: %clang_asan %s -o %t && %run %t 2>&1
-// RUN: %env_asan_opts=strict_string_checks=false %run %t 2>&1
-// RUN: %env_asan_opts=strict_string_checks=true not %run %t 2>&1 | FileCheck %s
+// RUN: %clang_asan %s -o %t
+
+// RUN: %env_asan_opts=strict_string_checks=false %run %t a 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true %run %t a 2>&1
+// RUN: not %run %t b 2>&1 | FileCheck %s
+// RUN: not %run %t c 2>&1 | FileCheck %s
+// RUN: not %run %t d 2>&1 | FileCheck %s
+// RUN: not %run %t e 2>&1 | FileCheck %s
+// RUN: not %run %t f 2>&1 | FileCheck %s
+// RUN: not %run %t g 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=strict_string_checks=false %run %t h 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true not %run %t h 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=strict_string_checks=false %run %t i 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true not %run %t i 2>&1 | FileCheck %s
+
+// RUN: %env_asan_opts=strict_string_checks=false %run %t A 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true %run %t A 2>&1
+// RUN: not %run %t B 2>&1 | FileCheck %s
+// RUN: not %run %t C 2>&1 | FileCheck %s
+// RUN: not %run %t D 2>&1 | FileCheck %s
+// RUN: not %run %t E 2>&1 | FileCheck %s
+// RUN: not %run %t F 2>&1 | FileCheck %s
+// RUN: not %run %t G 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=strict_string_checks=false %run %t H 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true not %run %t H 2>&1 | FileCheck %s
+// RUN: %env_asan_opts=strict_string_checks=false %run %t I 2>&1
+// RUN: %env_asan_opts=strict_string_checks=true not %run %t I 2>&1 | FileCheck %s
 
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
+#include <ctype.h>
 
 int main(int argc, char **argv) {
-  size_t size = 100;
+  assert(argc >= 2);
+  const size_t size = 100;
   char fill = 'o';
-  char *s1 = (char*)malloc(size);
+  char s1[size];
+  char s2[size];
   memset(s1, fill, size);
-  char *s2 = (char*)malloc(size);
   memset(s2, fill, size);
-  s1[size - 1] = 'z';
-  s2[size - 1] = 'x';
-  int r = strncmp(s1, s2, size + 1);
-  // CHECK: {{.*ERROR: AddressSanitizer: heap-buffer-overflow on address}}
-  // CHECK: READ of size 101
-  assert(r == 1);
-  free(s1);
-  free(s2);
+
+  int (*cmp_fn)(const char *, const char *, size_t);
+  cmp_fn = islower(argv[1][0]) ? &strncmp : &strncasecmp;
+
+  switch(tolower(argv[1][0])) {
+    case 'a':
+      s1[size - 1] = 'z';
+      s2[size - 1] = 'x';
+      for (int i = 0; i <= size; ++i)
+        assert((cmp_fn(s1, s2, i) == 0) == (i < size));
+      s1[size - 1] = '\0';
+      s2[size - 1] = '\0';
+      assert(cmp_fn(s1, s2, 2*size) == 0);
+      break;
+    case 'b':
+      return cmp_fn(s1-1, s2, 1);
+    case 'c':
+      return cmp_fn(s1, s2-1, 1);
+    case 'd':
+      return cmp_fn(s1+size, s2, 1);
+    case 'e':
+      return cmp_fn(s1, s2+size, 1);
+    case 'f':
+      return cmp_fn(s1+1, s2, size);
+    case 'g':
+      return cmp_fn(s1, s2+1, size);
+    case 'h':
+      s1[size - 1] = '\0';
+      assert(cmp_fn(s1, s2, 2*size) != 0);
+      break;
+    case 'i':
+      s2[size - 1] = '\0';
+      assert(cmp_fn(s1, s2, 2*size) != 0);
+      break;
+    // CHECK: {{.*}}ERROR: AddressSanitizer: stack-buffer-{{ov|und}}erflow on address
+  }
   return 0;
 }
