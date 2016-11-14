e95fc44ce594c80af76883a0398630d1865dea12
Eric Fiselier <eric@efcs.ca>
2016-11-14 07:03:50 +0000

    Add explicit (void) cast to result of unique_ptr::release()
    llvm-svn: 286796

--- clang/tools/libclang/CIndex.cpp
+++ clang/tools/libclang/CIndex.cpp
@@ -7761,7 +7761,7 @@ CXTUResourceUsage clang_getCXTUResourceUsage(CXTranslationUnit TU) {
   CXTUResourceUsage usage = { (void*) entries.get(),
                             (unsigned) entries->size(),
                             !entries->empty() ? &(*entries)[0] : nullptr };
-  entries.release();
+  (void)entries.release();
   return usage;
 }
 
