87f2f1687ef380a96226a10fb189ab65f80ca63f
Marshall Clow <mclow.lists@gmail.com>
2016-11-14 18:56:24 +0000

    Implement P0510 'Make future_error Constructible' adopted in Issaquah
    llvm-svn: 286864

--- libcxx/test/std/thread/futures/futures.future_error/code.pass.cpp
+++ libcxx/test/std/thread/futures/futures.future_error/code.pass.cpp
@@ -12,11 +12,15 @@
 // <future>
 
 // class future_error
+//     future_error(error_code __ec);  // exposition only
+//     explicit future_error(future_errc _Ev) : __ec_(make_error_code(_Ev)) {} // C++17
 
 // const error_code& code() const throw();
 
 #include <future>
 #include <cassert>
+
+#include "test_macros.h"
 
 int main()
 {
@@ -40,4 +44,14 @@ int main()
         std::future_error f(ec);
         assert(f.code() == ec);
     }
+#if TEST_STD_VER > 14
+    {
+        std::future_error f(std::future_errc::broken_promise);
+        assert(f.code() == std::make_error_code(std::future_errc::broken_promise));
+    }
+    {
+        std::future_error f(std::future_errc::no_state);
+        assert(f.code() == std::make_error_code(std::future_errc::no_state));
+    }
+#endif
 }
