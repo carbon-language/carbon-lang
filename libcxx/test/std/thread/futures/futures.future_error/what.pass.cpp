64bac8b5fe24e5b2de556c24551b474e68264c46
Stephan T. Lavavej <stl@exchange.microsoft.com>
2016-11-15 17:00:32 +0000

    [libcxx] [test] D26625: future_error::what() is implementation-defined.
    llvm-svn: 286983

--- libcxx/test/std/thread/futures/futures.future_error/what.pass.cpp
+++ libcxx/test/std/thread/futures/futures.future_error/what.pass.cpp
@@ -26,25 +26,27 @@
 #include <cstring>
 #include <cassert>
 
+#include "test_macros.h"
+
 int main()
 {
     {
         std::future_error f(std::make_error_code(std::future_errc::broken_promise));
-        assert(std::strcmp(f.what(), "The associated promise has been destructed prior "
+        LIBCPP_ASSERT(std::strcmp(f.what(), "The associated promise has been destructed prior "
                       "to the associated state becoming ready.") == 0);
     }
     {
         std::future_error f(std::make_error_code(std::future_errc::future_already_retrieved));
-        assert(std::strcmp(f.what(), "The future has already been retrieved from "
+        LIBCPP_ASSERT(std::strcmp(f.what(), "The future has already been retrieved from "
                       "the promise or packaged_task.") == 0);
     }
     {
         std::future_error f(std::make_error_code(std::future_errc::promise_already_satisfied));
-        assert(std::strcmp(f.what(), "The state of the promise has already been set.") == 0);
+        LIBCPP_ASSERT(std::strcmp(f.what(), "The state of the promise has already been set.") == 0);
     }
     {
         std::future_error f(std::make_error_code(std::future_errc::no_state));
-        assert(std::strcmp(f.what(), "Operation not permitted on an object without "
+        LIBCPP_ASSERT(std::strcmp(f.what(), "Operation not permitted on an object without "
                       "an associated state.") == 0);
     }
 }
