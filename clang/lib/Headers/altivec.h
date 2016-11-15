56fa12c5a30cf5aa006cba4d05e0b3b31f9ca616
Zaara Syeda <syzaara@ca.ibm.com>
2016-11-15 15:57:33 +0000

    test commmit
    llvm-svn: 286977

--- clang/lib/Headers/altivec.h
+++ clang/lib/Headers/altivec.h
@@ -17,7 +17,6 @@
  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
  * THE SOFTWARE.
- *
 \*===----------------------------------------------------------------------===*/
 
 #ifndef __ALTIVEC_H
