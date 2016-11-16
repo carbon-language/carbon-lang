f4c5a0e630b035c3445364597111c682ea7609e3
Adrian Prantl <aprantl@apple.com>
2016-11-16 19:26:11 +0000

    Add the missing FileCheck invocation to this testcase.
    llvm-svn: 287139

--- clang/test/CodeGenCXX/debug-info-thunk-msabi.cpp
+++ clang/test/CodeGenCXX/debug-info-thunk-msabi.cpp
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 %s -triple i386-pc-windows-msvc19.0.0 -emit-obj \
-// RUN:     -debug-info-kind=line-tables-only -fms-extensions
+// RUN: %clang_cc1 %s -triple i386-pc-windows-msvc19.0.0 -emit-llvm \
+// RUN:     -debug-info-kind=line-tables-only -fms-extensions -o - | FileCheck %s
 class __declspec(dllexport) A {
   A(int * = new int) {}
 };
@@ -8,12 +8,13 @@ class __declspec(dllexport) A {
 // CHECK-NOT: {{ret }}
 // CHECK: call x86_thiscallcc %class.A* @"\01??0A@@AAE@PAH@Z"(%class.A* %this1, i32* %0)
 // CHECK-SAME: !dbg ![[DBG:[0-9]+]]
-// CHECK: ret void, !dbg ![[DBG]]
+// CHECK: ret void, !dbg ![[DBG1:[0-9]+]]
 //
 // CHECK: ![[SP]] = distinct !DISubprogram(
-// CHECK-SAME:          line: 3
+// CHECK-SAME:          line: 4
 // CHECK-SAME:          isDefinition: true
 // CHECK-SAME:          DIFlagArtificial
 // CHECK-SAME:          ){{$}}
 //
+// CHECK: ![[DBG1]] = !DILocation(line: 0
 // CHECK: ![[DBG]] = !DILocation(line: 0
