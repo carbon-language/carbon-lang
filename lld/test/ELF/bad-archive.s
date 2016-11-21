91972d7f9d9ab4c4db34bc003df1064ae5805736
Eugene Leviant <eleviant@accesssoftek.com>
2016-11-21 13:57:50 +0000

    [ELF] Attempt to fix Windows buidbot
    llvm-svn: 287538

--- lld/test/ELF/bad-archive.s
+++ lld/test/ELF/bad-archive.s
@@ -3,11 +3,9 @@
 // Check bad archive error reporting with --whole-archive
 // and without it.
 // RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-// RUN: echo "!<arch>" > %t.bad.a
-// RUN: echo "bad archive" >> %t.bad.a
-// RUN: not ld.lld %t.o %t.bad.a -o %t 2>&1 | FileCheck %s
-// RUN: not ld.lld %t.o --whole-archive %t.bad.a -o %t 2>&1 | FileCheck %s
-// CHECK: {{.*}}.bad.a: failed to parse archive
+// RUN: not ld.lld %t.o %p/Inputs/bad-archive.a -o %t 2>&1 | FileCheck %s
+// RUN: not ld.lld %t.o --whole-archive %p/Inputs/bad-archive.a -o %t 2>&1 | FileCheck %s
+// CHECK: bad-archive.a: failed to parse archive
 
 .globl _start
 _start:
