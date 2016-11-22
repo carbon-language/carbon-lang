4e1a698be05697549072c8328918375a1609f3bd
Rafael Espindola <rafael.espindola@gmail.com>
2016-11-22 21:37:38 +0000

    Fix test to not depend on the path size.
    llvm-svn: 287704

--- lld/test/ELF/empty-ver.s
+++ lld/test/ELF/empty-ver.s
@@ -1,7 +1,9 @@
 // REQUIRES: x86
+// RUN: mkdir -p %t.dir
+// RUN: cd %t.dir
 // RUN: llvm-mc %s -o %t.o -filetype=obj -triple=x86_64-pc-linux
-// RUN: ld.lld %t.o -o %t.so -shared -version-script %p/Inputs/empty-ver.ver
-// RUN: llvm-readobj -s -version-info %t.so | FileCheck %s
+// RUN: ld.lld %t.o -o t.so -shared -version-script %p/Inputs/empty-ver.ver
+// RUN: llvm-readobj -s -section-data -version-info t.so | FileCheck %s
 
 // CHECK:      Name: .dynstr
 // CHECK-NEXT: Type: SHT_STRTAB
@@ -10,7 +12,14 @@
 // CHECK-NEXT: ]
 // CHECK-NEXT: Address:
 // CHECK-NEXT: Offset:
-// CHECK-NEXT: Size: 81
+// CHECK-NEXT: Size: 14
+// CHECK-NEXT: Link: 0
+// CHECK-NEXT: Info: 0
+// CHECK-NEXT: AddressAlignment: 1
+// CHECK-NEXT: EntrySize: 0
+// CHECK-NEXT: SectionData (
+// CHECK-NEXT:   0000: 00666F6F 00742E73 6F007665 7200      |.foo.t.so.ver.|
+// CHECK-NEXT: )
 
 // CHECK:      Version symbols {
 // CHECK-NEXT:   Section Name:
