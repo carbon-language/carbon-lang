e99e8d345c0bde318befd55cb5ce14d787f960f6
Kuba Mracek <mracek@apple.com>
2016-11-22 20:24:26 +0000

    [sanitizer] Fix the dedup_token_length_test.cc testcase to not fail when user's home directory contains "bar"
    Differential Revision: https://reviews.llvm.org/D24605
    
    llvm-svn: 287696

--- compiler-rt/test/sanitizer_common/TestCases/Posix/dedup_token_length_test.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/dedup_token_length_test.cc
@@ -1,10 +1,10 @@
 // Test dedup_token_length
 // RUN: %clangxx -O0 %s -o %t
-// RUN: env %tool_options='abort_on_error=0'                    not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK0
-// RUN: env %tool_options='abort_on_error=0, dedup_token_length=0' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK0
-// RUN: env %tool_options='abort_on_error=0, dedup_token_length=1' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK1
-// RUN: env %tool_options='abort_on_error=0, dedup_token_length=2' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK2
-// RUN: env %tool_options='abort_on_error=0, dedup_token_length=3' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK3
+// RUN: env %tool_options='abort_on_error=0'                       not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK0 --match-full-lines
+// RUN: env %tool_options='abort_on_error=0, dedup_token_length=0' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK0 --match-full-lines
+// RUN: env %tool_options='abort_on_error=0, dedup_token_length=1' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK1 --match-full-lines
+// RUN: env %tool_options='abort_on_error=0, dedup_token_length=2' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK2 --match-full-lines
+// RUN: env %tool_options='abort_on_error=0, dedup_token_length=3' not %run %t 2>&1   | FileCheck %s --check-prefix=CHECK3 --match-full-lines
 
 // REQUIRES: stable-runtime
 // FIXME: implement SEGV handler in other sanitizers, not just asan.
@@ -34,7 +34,5 @@ int main(int argc, char **argv) {
 
 // CHECK0-NOT: DEDUP_TOKEN:
 // CHECK1: DEDUP_TOKEN: void Xyz::Abc<int, int>()
-// CHECK1-NOT: bar
 // CHECK2: DEDUP_TOKEN: void Xyz::Abc<int, int>()--bar
-// CHECK2-NOT: FOO
 // CHECK3: DEDUP_TOKEN: void Xyz::Abc<int, int>()--bar--FOO()
