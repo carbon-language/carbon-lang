15a5a21a08d4cf60a3bbb274d1b8c5f23a2d3b41
Pavel Labath <labath@google.com>
2016-11-24 14:54:53 +0000

    Enable TestRegisterVariables for some configurations
    It consistently passes for linux-clang-i386, and linux-gcc-x86_64.
    
    llvm-svn: 287883

--- lldb/packages/Python/lldbsuite/test/lang/c/register_variables/TestRegisterVariables.py
+++ lldb/packages/Python/lldbsuite/test/lang/c/register_variables/TestRegisterVariables.py
@@ -99,11 +99,8 @@ class RegisterVariableTestCase(TestBase):
     mydir = TestBase.compute_mydir(__file__)
 
     @expectedFailureAll(compiler="clang", compiler_version=['<', '3.5'])
-    @expectedFailureAll(compiler="clang", archs=["i386"])
-    @expectedFailureAll(
-        compiler="gcc", compiler_version=[
-            '>=', '4.8.2'], archs=[
-            "i386", "x86_64"])
+    @expectedFailureAll(compiler="gcc", compiler_version=[
+            '>=', '4.8.2'], archs=["i386"])
     def test_and_run_command(self):
         """Test expressions on register values."""
 
