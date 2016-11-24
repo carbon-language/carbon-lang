d399b10e011539301b2b6a57b08c4d76ff17b9d6
Pavel Labath <labath@google.com>
2016-11-24 15:10:15 +0000

    Enable TestMultithreaded-sb_api_listener_event_process_state on linux/gcc
    Passing consistently now.
    
    llvm-svn: 287885

--- lldb/packages/Python/lldbsuite/test/api/multithreaded/TestMultithreaded.py
+++ lldb/packages/Python/lldbsuite/test/api/multithreaded/TestMultithreaded.py
@@ -52,14 +52,6 @@ class SBBreakpointCallbackCase(TestBase):
     # clang-cl does not support throw or catch (llvm.org/pr24538)
     @skipIfWindows
     @expectedFlakeyFreeBSD
-    @expectedFailureAll(
-        "llvm.org/pr23139",
-        oslist=["linux"],
-        compiler="gcc",
-        compiler_version=[
-            ">=",
-            "4.9"],
-        archs=["x86_64"])
     def test_sb_api_listener_event_process_state(self):
         """ Test that a registered SBListener receives events when a process
             changes state.
