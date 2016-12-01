5fb9b5e1c939fd716af22c89af14bf9679492d32
Artem Belevich <tra@google.com>
2016-12-01 19:34:35 +0000

    Send compiler output to /dev/null in defsym.s test.
    Fixes test failures if tests are run in a read-only source tree.
    
    llvm-svn: 288406

--- clang/test/Driver/defsym.s
+++ clang/test/Driver/defsym.s
@@ -11,12 +11,12 @@
 // CHECK-DEFSYM1: "-defsym"
 // CHECK-DEFSYM1: "xyz=0xa"
 
-// RUN: not %clang -c -integrated-as %s \
+// RUN: not %clang -c -integrated-as -o /dev/null %s \
 // RUN: -Wa,-defsym,abc= \
 // RUN: 2>&1 | FileCheck %s --check-prefix=CHECK-DEFSYM-ERR1
 // CHECK-DEFSYM-ERR1: error: defsym must be of the form: sym=value: abc=
 
-// RUN: not %clang -c -integrated-as %s \
+// RUN: not %clang -c -integrated-as -o /dev/null %s \
 // RUN: -Wa,-defsym,abc=1a2b3c \
 // RUN: 2>&1 | FileCheck %s --check-prefix=CHECK-DEFSYM-ERR2
 // CHECK-DEFSYM-ERR2: error: Value is not an integer: 1a2b3c
