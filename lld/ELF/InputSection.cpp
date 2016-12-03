27004d336f1faa5e1433e08c6efb6b35833424a0
Rafael Espindola <rafael.espindola@gmail.com>
2016-12-03 15:26:18 +0000

    Ignone SHF_INFO_LINK.
    Some elf producers (dtrace) put this flag in relocation sections and
    some (MC) don't. If we don't ignore the flag we end up with multiple
    relocation sections poiting to the same section, which we don't
    support.
    
    llvm-svn: 288585

--- lld/ELF/InputSection.cpp
+++ lld/ELF/InputSection.cpp
@@ -92,9 +92,10 @@ template <class ELFT>
 InputSectionBase<ELFT>::InputSectionBase(elf::ObjectFile<ELFT> *File,
                                          const Elf_Shdr *Hdr, StringRef Name,
                                          Kind SectionKind)
-    : InputSectionBase(File, Hdr->sh_flags, Hdr->sh_type, Hdr->sh_entsize,
-                       Hdr->sh_link, Hdr->sh_info, Hdr->sh_addralign,
-                       getSectionContents(File, Hdr), Name, SectionKind) {
+    : InputSectionBase(File, Hdr->sh_flags & ~SHF_INFO_LINK, Hdr->sh_type,
+                       Hdr->sh_entsize, Hdr->sh_link, Hdr->sh_info,
+                       Hdr->sh_addralign, getSectionContents(File, Hdr), Name,
+                       SectionKind) {
   this->Offset = Hdr->sh_offset;
 }
 
