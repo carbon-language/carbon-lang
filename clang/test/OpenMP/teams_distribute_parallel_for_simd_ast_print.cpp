9965d2233483d9b01ddffefa80ba6991a586dae3
Kelvin Li <kkwli0@gmail.com>
2016-12-06 23:18:06 +0000

    [OpenMP] Fix typo in the run command in the test case. NFC.
    llvm-svn: 288862

--- clang/test/OpenMP/teams_distribute_parallel_for_simd_ast_print.cpp
+++ clang/test/OpenMP/teams_distribute_parallel_for_simd_ast_print.cpp
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -verify -fopenmp -ast-print %s | FileCheck %s
-// rUN: %clang_cc1 -fopenmp -x c++ -std=c++11 -emit-pch -o %t %s
-// rUN: %clang_cc1 -fopenmp -std=c++11 -include-pch %t -fsyntax-only -verify %s -ast-print | FileCheck %s
+// RUN: %clang_cc1 -fopenmp -x c++ -std=c++11 -emit-pch -o %t %s
+// RUN: %clang_cc1 -fopenmp -std=c++11 -include-pch %t -fsyntax-only -verify %s -ast-print | FileCheck %s
 // expected-no-diagnostics
 
 #ifndef HEADER
