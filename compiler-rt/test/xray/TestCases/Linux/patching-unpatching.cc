3c265b55fc18547be667cbbb84667e00bcf376e7
Serge Rogatch <srogatch@accesssoftek.com>
2016-12-07 22:00:02 +0000

    [XRay][AArch64] Disable the unstable test XRay-aarch64-linux::patching-unpatching.cc
    Summary:
    The test `XRay-aarch64-linux::patching-unpatching.cc` sometimes passes, sometimes fails on buildbots.
    This patch disables test `patching-unpatching.cc` for AArch64 targets.
    
    Reviewers: rengolin, dberris
    
    Subscribers: llvm-commits, iid_iunknown, aemerson
    
    Differential Revision: https://reviews.llvm.org/D27528
    
    llvm-svn: 288988

--- compiler-rt/test/xray/TestCases/Linux/patching-unpatching.cc
+++ compiler-rt/test/xray/TestCases/Linux/patching-unpatching.cc
@@ -3,6 +3,7 @@
 //
 // RUN: %clangxx_xray -fxray-instrument -std=c++11 %s -o %t
 // RUN: XRAY_OPTIONS="patch_premain=false" %run %t 2>&1 | FileCheck %s
+// REQUIRES: stable-runtime
 
 #include "xray/xray_interface.h"
 
