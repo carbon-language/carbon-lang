a4b43bf8e85cea87fa2b0a6aec31781c5be520c7
Kostya Serebryany <kcc@google.com>
2016-12-12 22:39:33 +0000

    [libFuzzer] make  SimpleCmpTest a bit simpler to crack and more verbose
    llvm-svn: 289477

--- llvm/lib/Fuzzer/test/SimpleCmpTest.cpp
+++ llvm/lib/Fuzzer/test/SimpleCmpTest.cpp
@@ -7,25 +7,34 @@
 #include <cstring>
 #include <cstdio>
 
+extern int AllLines[];
+
+bool PrintOnce(int Line) {
+  if (!AllLines[Line])
+    fprintf(stderr, "Seen line %d\n", Line);
+  AllLines[Line] = 1;
+  return true;
+}
+
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  if (Size < 14) return 0;
+  if (Size != 22) return 0;
   uint64_t x = 0;
   int64_t  y = 0;
-  int z = 0;
-  unsigned short a = 0;
-  memcpy(&x, Data, 8);
-  memcpy(&y, Data + Size - 8, 8);
-  memcpy(&z, Data + Size / 2, sizeof(z));
-  memcpy(&a, Data + Size / 2 + 4, sizeof(a));
+  int32_t z = 0;
+  uint16_t a = 0;
+  memcpy(&x, Data, 8);  // 8
+  memcpy(&y, Data + 8, 8);  // 16
+  memcpy(&z, Data + 16, sizeof(z));  // 20
+  memcpy(&a, Data + 20, sizeof(a));  // 22
 
-  if (x > 1234567890 &&
-      x < 1234567895 &&
-      y >= 987654321 &&
-      y <= 987654325 &&
-      z < -10000 &&
-      z >= -10005 &&
-      z != -10003 &&
-      a == 4242 &&
+  if (x > 1234567890 && PrintOnce(__LINE__) &&
+      x < 1234567895 && PrintOnce(__LINE__) &&
+      a == 0x4242 && PrintOnce(__LINE__) &&
+      y >= 987654321 && PrintOnce(__LINE__) &&
+      y <= 987654325 && PrintOnce(__LINE__) &&
+      z < -10000 && PrintOnce(__LINE__) &&
+      z >= -10005 && PrintOnce(__LINE__) &&
+      z != -10003 && PrintOnce(__LINE__) &&
       true) {
     fprintf(stderr, "BINGO; Found the target: size %zd (%zd, %zd, %d, %d), exiting.\n",
             Size, x, y, z, a);
@@ -33,3 +42,5 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   }
   return 0;
 }
+
+int AllLines[__LINE__ + 1];  // Must be the last line.
