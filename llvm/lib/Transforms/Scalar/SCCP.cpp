463bebc319b7ef73e45d447e0903481528fb44df
Davide Italiano <davide@freebsd.org>
2016-12-13 05:56:04 +0000

    [SCCP] Debug diagnostic goes under DEBUG(). NFCI.
    llvm-svn: 289519

--- llvm/lib/Transforms/Scalar/SCCP.cpp
+++ llvm/lib/Transforms/Scalar/SCCP.cpp
@@ -549,7 +549,7 @@ private:
 
   void visitInstruction(Instruction &I) {
     // If a new instruction is added to LLVM that we don't handle.
-    dbgs() << "SCCP: Don't know how to handle: " << I << '\n';
+    DEBUG(dbgs() << "SCCP: Don't know how to handle: " << I << '\n');
     markAnythingOverdefined(&I);   // Just in case
   }
 };
