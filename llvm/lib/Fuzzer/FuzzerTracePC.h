f6f82c2cc8a3dc8584cf19e521da93a8d17ba9eb
Kostya Serebryany <kcc@google.com>
2016-12-13 22:49:14 +0000

    [libFuzzer] fix an UB (invalid shift) spotted by ubsan. The code worked fine by luck, because the way shifts actually work on clang+x86
    llvm-svn: 289607

--- llvm/lib/Fuzzer/FuzzerTracePC.h
+++ llvm/lib/Fuzzer/FuzzerTracePC.h
@@ -126,7 +126,7 @@ size_t TracePC::CollectFeatures(Callback CB) {
     uint64_t Bundle = *reinterpret_cast<uint64_t*>(&Counters[Idx]);
     if (!Bundle) continue;
     for (size_t i = Idx; i < Idx + Step; i++) {
-      uint8_t Counter = (Bundle >> (i * 8)) & 0xff;
+      uint8_t Counter = (Bundle >> ((i - Idx) * 8)) & 0xff;
       if (!Counter) continue;
       Counters[i] = 0;
       unsigned Bit = 0;
