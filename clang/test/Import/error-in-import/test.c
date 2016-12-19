ca2f40dd6803e133f38c18254a85625a1a1619bf
Sean Callanan <scallanan@apple.com>
2016-12-19 19:15:43 +0000

    Reverting r290004, r290006, r290010 pending review.
    llvm-svn: 290130

--- clang/test/Import/error-in-import/test.c
+++
@@ -1,6 +0,0 @@
-// RUN: not clang-import-test -import %S/Inputs/S.c -expression %s 2>&1 | FileCheck %s
-// CHECK: {{.*}}expected unqualified-id{{.*}}
-void expr() {
-  struct S MyS;
-  void *MyPtr = &MyS;
-}
