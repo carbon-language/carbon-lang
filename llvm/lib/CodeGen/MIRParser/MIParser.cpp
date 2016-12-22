9751e61fe1ce38ad3b6fcb550c28af41f7cad44c
Quentin Colombet <qcolombet@apple.com>
2016-12-22 21:56:29 +0000

    [MIRParser] Non-generic virtual register may have a type.
    When generic virtual registers get constrained, because of a use on a
    target specific operation for instance, we end up with regular virtual
    registers with a type and that's perfectly fine.
    
    llvm-svn: 290376

--- llvm/lib/CodeGen/MIRParser/MIParser.cpp
+++ llvm/lib/CodeGen/MIRParser/MIParser.cpp
@@ -1029,9 +1029,6 @@ bool MIParser::parseRegisterOperand(MachineOperand &Dest,
     // Virtual registers may have a size with GlobalISel.
     if (!TargetRegisterInfo::isVirtualRegister(Reg))
       return error("unexpected size on physical register");
-    if (RegInfo->Kind != VRegInfo::GENERIC &&
-        RegInfo->Kind != VRegInfo::REGBANK)
-      return error("unexpected size on non-generic virtual register");
 
     LLT Ty;
     if (parseLowLevelType(Token.location(), Ty))
