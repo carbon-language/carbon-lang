03226c5e0632268ffc778414ecb421fccead4367
Eric Fiselier <eric@efcs.ca>
2016-12-30 13:11:17 +0000

    Revert r290750 - Fix PR19460 - std::ios is convertible to int.
    llvm-svn: 290752

--- libcxx/test/std/input.output/iostreams.base/ios/iostate.flags/bool.pass.cpp
+++ libcxx/test/std/input.output/iostreams.base/ios/iostate.flags/bool.pass.cpp
@@ -14,10 +14,7 @@
 // operator unspecified-bool-type() const;
 
 #include <ios>
-#include <type_traits>
 #include <cassert>
-
-#include "test_macros.h"
 
 int main()
 {
@@ -25,10 +22,4 @@ int main()
     assert(static_cast<bool>(ios) == !ios.fail());
     ios.setstate(std::ios::failbit);
     assert(static_cast<bool>(ios) == !ios.fail());
-    static_assert((!std::is_convertible<std::ios, void*>::value), "");
-    static_assert((!std::is_convertible<std::ios, int>::value), "");
-    static_assert((!std::is_convertible<std::ios const&, int>::value), "");
-#if TEST_STD_VER >= 11
-    static_assert((!std::is_convertible<std::ios, bool>::value), "");
-#endif
 }
