11c0c53367efbcc8d54ff26c562e8b7891ebc1b1
Saleem Abdulrasool <compnerd@compnerd.org>
2017-01-02 21:09:16 +0000

    win32: temporarily disable setting locale on 14+
    The locale structures have been made opaque in CRT 14+.  This currently
    prevents building libc++ for Windows.  We can re-enable this in the
    future when we have replicated the structure to access the private field
    for the name (unless there exists a better supported mechanism to query
    the name of a locale given the locale_t).
    
    llvm-svn: 290835

--- libcxx/src/support/win32/locale_win32.cpp
+++ libcxx/src/support/win32/locale_win32.cpp
@@ -13,6 +13,8 @@
 #include <memory>
 #include <type_traits>
 
+#include <crtversion.h>
+
 typedef _VSTD::remove_pointer<locale_t>::type __locale_struct;
 typedef _VSTD::unique_ptr<__locale_struct, decltype(&uselocale)> __locale_raii;
 
@@ -29,7 +31,9 @@ locale_t uselocale( locale_t newloc )
     // uselocale sets the thread's locale by definition, so unconditionally use thread-local locale
     _configthreadlocale( _ENABLE_PER_THREAD_LOCALE );
     // uselocale sets all categories
+#if _VC_CRT_MAJOR_VERSION < 14
     setlocale( LC_ALL, newloc->locinfo->lc_category[LC_ALL].locale );
+#endif
     // uselocale returns the old locale_t
     return old_locale;
 }
