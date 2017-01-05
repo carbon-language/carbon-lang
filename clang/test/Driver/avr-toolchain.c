9f4a935b2aba21cd9df4ed824fa9316cc750a6da
Haojian Wu <hokein@google.com>
2017-01-05 10:06:58 +0000

    No canonical-prefixes match in avr-toolchain.c.
    llvm-svn: 291096

--- clang/test/Driver/avr-toolchain.c
+++ clang/test/Driver/avr-toolchain.c
@@ -1,4 +1,4 @@
 // A basic clang -cc1 command-line.
 
-// RUN: %clang %s -### -target avr 2>&1 | FileCheck -check-prefix=CC1 %s
+// RUN: %clang %s -### -no-canonical-prefixes -target avr 2>&1 | FileCheck -check-prefix=CC1 %s
 // CC1: clang{{.*}} "-cc1" "-triple" "avr"
