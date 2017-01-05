945d79ad8b866cc52fae686bda9401358b59f13e
Eric Fiselier <eric@efcs.ca>
2017-01-05 09:06:30 +0000

    Add gcc-[56] clang-3.[678] to list of XFAILS for variant tests. Patch from Michael Park
    llvm-svn: 291094

--- libcxx/test/std/utilities/variant/variant.variant/variant.status/valueless_by_exception.pass.cpp
+++ libcxx/test/std/utilities/variant/variant.variant/variant.status/valueless_by_exception.pass.cpp
@@ -10,8 +10,10 @@
 
 // UNSUPPORTED: c++98, c++03, c++11, c++14
 
-// Clang 3.8 doesn't allow constexpr variables of non-literal type
-// XFAIL: clang-3.8, apple-clang-7, apple-clang-8
+// The following compilers don't allow constexpr variables of non-literal type.
+// XFAIL: gcc-5, gcc-6
+// XFAIL: clang-3.5, clang-3.6, clang-3.7, clang-3.8
+// XFAIL: apple-clang-6, apple-clang-7, apple-clang-8
 
 // <variant>
 
