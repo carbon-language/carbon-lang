6f8e7e0853b2d5976bc53ca8ff7ac9456ac5740f
Marshall Clow <mclow.lists@gmail.com>
2017-01-09 20:29:28 +0000

    Added XFAIL for the apple versions of clang as well
    llvm-svn: 291475

--- libcxx/test/std/strings/string.view/string_view.literals/literal1.fail.cpp
+++ libcxx/test/std/strings/string.view/string_view.literals/literal1.fail.cpp
@@ -10,6 +10,7 @@
 
 // UNSUPPORTED: c++98, c++03, c++11
 // UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9
+// UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8
 
 #include <string_view>
 #include <cassert>
