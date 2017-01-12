6d9e2c4a9c7c677a1c1ce250d14834b8f5841f16
Hans Wennborg <hans@hanshq.net>
2017-01-12 21:22:36 +0000

    Bump trunk version to 5.0.0svn
    llvm-svn: 291815

--- llvm/utils/release/build_llvm_package.bat
+++ llvm/utils/release/build_llvm_package.bat
@@ -25,8 +25,8 @@ set PATH=%PATH%;c:\gnuwin32\bin
 
 set revision=%1
 set branch=trunk
-set package_version=4.0.0-r%revision%
-set clang_format_vs_version=4.0.0.%revision%
+set package_version=5.0.0-r%revision%
+set clang_format_vs_version=5.0.0.%revision%
 set build_dir=llvm_package_%revision%
 
 echo Branch: %branch%
