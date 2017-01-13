d30e6f7421f4b319f1203d660bbc89f09d8944f7
Chandler Carruth <chandlerc@gmail.com>
2017-01-13 02:47:34 +0000

    Another fix for r291850 because there are apparently targets which add "-mllvm" flags to the CC1 invocation (notably, Hexagon seems to hit this).
    llvm-svn: 291881

--- clang/test/Driver/disable-llvm.c
+++ clang/test/Driver/disable-llvm.c
@@ -2,8 +2,8 @@
 // RUN: %clang -O2 -Xclang -disable-llvm-passes -### %s 2>&1 \
 // RUN:     | FileCheck --check-prefix=DISABLED %s
 // DISABLED: -cc1
-// DISABLED-NOT: -mllvm
-// DISABLED: -disable-llvm-passes
+// DISABLED-NOT: "-mllvm" "-disable-llvm-passes"
+// DISABLED: "-disable-llvm-passes"
 //
 // We also support two alternative spellings for historical reasons.
 // RUN: %clang -O2 -Xclang -disable-llvm-optzns -### %s 2>&1 \
@@ -11,8 +11,8 @@
 // RUN: %clang -O2 -mllvm -disable-llvm-optzns -### %s 2>&1 \
 // RUN:     | FileCheck --check-prefix=DISABLED-LEGACY %s
 // DISABLED-LEGACY: -cc1
-// DISABLED-LEGACY-NOT: -mllvm
-// DISABLED-LEGACY: -disable-llvm-optzns
+// DISABLED-LEGACY-NOT: "-mllvm" "-disable-llvm-optzns"
+// DISABLED-LEGACY: "-disable-llvm-optzns"
 //
 // The main flag shouldn't be specially handled when used with '-mllvm'.
 // RUN: %clang -O2 -mllvm -disable-llvm-passes -### %s 2>&1 | FileCheck --check-prefix=MLLVM %s
