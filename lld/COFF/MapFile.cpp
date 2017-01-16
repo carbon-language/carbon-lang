548adcdec4be1ff2a606502db15f71d64cc2c9e7
George Rimar <grimar@accesssoftek.com>
2017-01-16 11:46:55 +0000

    [COFF] - Fixed format in writeOutSecLine()
    The same as https://reviews.llvm.org/rL292102,
    fixes next testcases under msvs2015/win32:
    
    25>  Failing Tests (3):
    25>      lld :: COFF/lldmap.test
    25>      lld :: COFF/weak-external.test
    25>      lld :: COFF/weak-external3.test
    
    llvm-svn: 292104

--- lld/COFF/MapFile.cpp
+++ lld/COFF/MapFile.cpp
@@ -36,7 +36,8 @@ using namespace lld::coff;
 
 static void writeOutSecLine(raw_fd_ostream &OS, uint64_t Address, uint64_t Size,
                             uint64_t Align, StringRef Name) {
-  OS << format("%08x %08x %5x ", Address, Size, Align) << left_justify(Name, 7);
+  OS << format("%08llx %08llx %5llx ", Address, Size, Align)
+     << left_justify(Name, 7);
 }
 
 static void writeInSecLine(raw_fd_ostream &OS, uint64_t Address, uint64_t Size,
