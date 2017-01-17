608319101ae2dd7e25c4dbc98a9c6264d2a4a5d9
Michal Gorny <mgorny@gentoo.org>
2017-01-17 21:04:19 +0000

    [cmake] Update SOVERSION for the new versioning scheme
    Update SOVERSION to use just the major version number rather than
    major+minor, to match the new versioning scheme where only major is used
    to indicate API/ABI version.
    
    Since two-digit SOVERSIONs were introduced post 3.9 branching, this
    change does not risk any SOVERSION collisions. In the past,
    two-component X.Y SOVERSIONs were shortly used but those will not
    interfere with the new ones since the new versions start at 4.
    
    Differential Revision: https://reviews.llvm.org/D28730
    
    llvm-svn: 292255

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -462,11 +462,9 @@ function(llvm_add_library name)
     if(UNIX AND NOT APPLE AND NOT ARG_SONAME)
       set_target_properties(${name}
         PROPERTIES
-		# Concatenate the version numbers since ldconfig expects exactly
-		# one component indicating the ABI version, while LLVM uses
-		# major+minor for that.
-        SOVERSION ${LLVM_VERSION_MAJOR}${LLVM_VERSION_MINOR}
-        VERSION ${LLVM_VERSION_MAJOR}${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}${LLVM_VERSION_SUFFIX})
+        # Since 4.0.0, the ABI version is indicated by the major version
+        SOVERSION ${LLVM_VERSION_MAJOR}
+        VERSION ${LLVM_VERSION_MAJOR}.${LLVM_VERSION_MINOR}.${LLVM_VERSION_PATCH}${LLVM_VERSION_SUFFIX})
     endif()
   endif()
 
