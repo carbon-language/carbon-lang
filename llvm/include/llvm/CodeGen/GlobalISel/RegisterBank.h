517b61cb69d6cf19841c07242dc1c7dc901736c4
Daniel Sanders <daniel_l_sanders@apple.com>
2017-01-18 14:17:50 +0000

    Re-commit: [globalisel] Tablegen-erate current Register Bank Information
    Summary:
    Adds a RegisterBank tablegen class that can be used to declare the register
    banks and an associated tablegen pass to generate the necessary code.
    
    Changes since last commit:
    The new tablegen pass is now correctly guarded by LLVM_BUILD_GLOBAL_ISEL and
    this should fix the buildbots however it may not be the whole fix. The previous
    buildbot failures suggest there may be a memory bug lurking that I'm unable to
    reproduce (including when using asan) or spot in the source. If they re-occur
    on this commit then I'll need assistance from the bot owners to track it down.
    
    Reviewers: t.p.northover, ab, rovka, qcolombet
    
    Reviewed By: qcolombet
    
    Subscribers: aditya_nandakumar, rengolin, kristof.beyls, vkalintiris, mgorny, dberris, llvm-commits, rovka
    
    Differential Revision: https://reviews.llvm.org/D27338
    
    llvm-svn: 292367

--- llvm/include/llvm/CodeGen/GlobalISel/RegisterBank.h
+++ llvm/include/llvm/CodeGen/GlobalISel/RegisterBank.h
@@ -42,7 +42,7 @@ private:
 
 public:
   RegisterBank(unsigned ID, const char *Name, unsigned Size,
-               const uint32_t *ContainedRegClasses);
+               const uint32_t *ContainedRegClasses, unsigned NumRegClasses);
 
   /// Get the identifier of this register bank.
   unsigned getID() const { return ID; }
