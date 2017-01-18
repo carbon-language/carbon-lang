8f03fbe678e560a34edbc0d9cc9330a6a670da2b
Renato Golin <renato.golin@linaro.org>
2017-01-18 09:07:35 +0000

    Revert "[XRay][Arm] Enable back XRay testing on Arm32 and fix the failing tests"
    This reverts commit r292211, as it broke the Thumb buldbot with:
    
    clang-5.0: error: the clang compiler does not support '-fxray-instrument
    on thumbv7-unknown-linux-gnueabihf'
    
    llvm-svn: 292356

--- compiler-rt/lib/xray/xray_arm.cc
+++ compiler-rt/lib/xray/xray_arm.cc
@@ -19,8 +19,6 @@
 #include <atomic>
 #include <cassert>
 
-extern "C" void __clear_cache(void* start, void* end);
-
 namespace __xray {
 
 uint64_t cycleFrequency() XRAY_NEVER_INSTRUMENT {
@@ -118,8 +116,8 @@ inline static bool patchSled(const bool Enable, const uint32_t FuncId,
   //   B #20
 
   uint32_t *FirstAddress = reinterpret_cast<uint32_t *>(Sled.Address);
-  uint32_t *CurAddress = FirstAddress + 1;
   if (Enable) {
+    uint32_t *CurAddress = FirstAddress + 1;
     CurAddress =
         Write32bitLoadR0(CurAddress, reinterpret_cast<uint32_t>(FuncId));
     CurAddress =
@@ -127,7 +125,6 @@ inline static bool patchSled(const bool Enable, const uint32_t FuncId,
     *CurAddress = uint32_t(PatchOpcodes::PO_BlxIp);
     CurAddress++;
     *CurAddress = uint32_t(PatchOpcodes::PO_PopR0Lr);
-    CurAddress++;
     std::atomic_store_explicit(
         reinterpret_cast<std::atomic<uint32_t> *>(FirstAddress),
         uint32_t(PatchOpcodes::PO_PushR0Lr), std::memory_order_release);
@@ -136,8 +133,6 @@ inline static bool patchSled(const bool Enable, const uint32_t FuncId,
         reinterpret_cast<std::atomic<uint32_t> *>(FirstAddress),
         uint32_t(PatchOpcodes::PO_B20), std::memory_order_release);
   }
-  __clear_cache(reinterpret_cast<char*>(FirstAddress),
-      reinterpret_cast<char*>(CurAddress));
   return true;
 }
 
