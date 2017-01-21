6bfbcd099ecaa39f4b4342f5f5f7c0f8c4af4dee
Vitaly Buka <vitalybuka@google.com>
2017-01-21 01:21:56 +0000

    [libFuzzer] Use CXX to set compiler to use
    Reviewers: kcc
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D28976
    
    llvm-svn: 292697

--- llvm/lib/Fuzzer/build.sh
+++ llvm/lib/Fuzzer/build.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 LIBFUZZER_SRC_DIR=$(dirname $0)
+CXX="${CXX:-clang}"
 for f in $LIBFUZZER_SRC_DIR/*.cpp; do
-  clang -g -O2 -fno-omit-frame-pointer -std=c++11 $f -c &
+  $CXX -g -O2 -fno-omit-frame-pointer -std=c++11 $f -c &
 done
 wait
 rm -f libFuzzer.a
