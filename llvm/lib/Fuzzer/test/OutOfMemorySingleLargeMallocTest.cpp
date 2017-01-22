bbfc8c357c0356db8d3bd4e6d2277248025619ea
Marcos Pividori <mpividori@google.com>
2017-01-22 01:58:50 +0000

    [libFuzzer] Fix OutOfMemory tests to work on 32 bits.
    I add 2 changes to make the tests work on 32 bits and on 64 bits.
    I change the size allocated to 0x20000000 and add the flag: -rss_limit_mb=300.
    Otherwise the output for 32 bits and 64 bits is different.
    For 64 bits the value 0xff000000 doesn't exceed kMaxAllowedMallocSize.
    For 32 bits, kMaxAllowedMallocSize is set to 0xc0000000, so the call to
    Allocate() will fail earlier printing "WARNING: AddressSanitizer failed to
    allocate ..." , and wont't call malloc hooks.
    So, we need to consider a size smaller than 2GB (so malloc doesn't fail on
    32bits) and greater that the value provided by -rss_limit_mb.
    Because of that I use: 0x20000000.
    
    Differential Revision: https://reviews.llvm.org/D28706
    
    llvm-svn: 292744

--- llvm/lib/Fuzzer/test/OutOfMemorySingleLargeMallocTest.cpp
+++ llvm/lib/Fuzzer/test/OutOfMemorySingleLargeMallocTest.cpp
@@ -15,7 +15,7 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size > 0 && Data[0] == 'H') {
     if (Size > 1 && Data[1] == 'i') {
       if (Size > 2 && Data[2] == '!') {
-          size_t kSize = 0xff000000U;
+          size_t kSize = 0x20000000U;
           char *p = new char[kSize];
           SinkPtr = p;
           delete [] p;
