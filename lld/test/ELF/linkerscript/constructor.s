23be5d94eb3a19518b8c5f4fbfacefa17388d671
George Rimar <grimar@accesssoftek.com>
2017-01-23 16:55:13 +0000

    [ELF] - Committed missing ld.ldd invocation to constructor.s
    Thanks to Meador Ingle for noticing.
    
    llvm-svn: 292799

--- lld/test/ELF/linkerscript/constructor.s
+++ lld/test/ELF/linkerscript/constructor.s
@@ -1,6 +1,7 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 # RUN: echo "SECTIONS { foo : { *(.foo) CONSTRUCTORS } }" > %t.script
+# RUN: ld.lld -o %t1 --script %t.script %t.o
 
 # RUN: llvm-objdump -section-headers %t1 | FileCheck %s
 # CHECK:      Sections:
