ae834047a442ecb6f096c54df1aa59d8dd263663
Antonio Maiorano <amaiorano@gmail.com>
2017-01-23 13:20:23 +0000

    clang-format: remove tests that assume no config file will be found as this is not always the case
    These tests fail for developers who place their build directories under the
    llvm root directory because llvm's own .clang-format file will be found.
    Anyway these cases are covered by FormatStyle.GetStyleOfFile tests
    (FormatTest.cpp).
    
    Differential Revision: https://reviews.llvm.org/D28983
    
    llvm-svn: 292787

--- clang/test/Format/style-on-command-line.cpp
+++ clang/test/Format/style-on-command-line.cpp
@@ -13,16 +13,11 @@
 // RUN: clang-format -style="{BasedOnStyle: WebKit, PointerBindsToType: false}" %s | FileCheck -strict-whitespace -check-prefix=CHECK9 %s
 
 // Fallback style tests
-// RUN: rm %T/_clang-format
-// Test no config file found, WebKit fallback style is applied
-// RUN: clang-format -style=file -fallback-style=WebKit -assume-filename=%T/foo.cpp < %s 2>&1 | FileCheck -strict-whitespace -check-prefix=CHECK10 %s
-// Test no config file and fallback style "none", no formatting is applied
-// RUN: clang-format -style=file -fallback-style=none -assume-filename=%T/foo.cpp < %s 2>&1 | FileCheck -strict-whitespace -check-prefix=CHECK11 %s
 // Test config file with no based style, and fallback style "none", formatting is applied
 // RUN: printf "IndentWidth: 6\n" > %T/_clang-format
-// RUN: clang-format -style=file -fallback-style=none -assume-filename=%T/foo.cpp < %s 2>&1 | FileCheck -strict-whitespace -check-prefix=CHECK12 %s
+// RUN: clang-format -style=file -fallback-style=none -assume-filename=%T/foo.cpp < %s 2>&1 | FileCheck -strict-whitespace -check-prefix=CHECK10 %s
 // Test yaml with no based style, and fallback style "none", LLVM formatting applied
-// RUN: clang-format -style="{IndentWidth: 7}" -fallback-style=none %s | FileCheck -strict-whitespace -check-prefix=CHECK13 %s
+// RUN: clang-format -style="{IndentWidth: 7}" -fallback-style=none %s | FileCheck -strict-whitespace -check-prefix=CHECK11 %s
 
 void f() {
 // CHECK1: {{^        int\* i;$}}
@@ -35,10 +30,8 @@ void f() {
 // CHECK7: {{^      int\* i;$}}
 // CHECK8: {{^  int\* i;$}}
 // CHECK9: {{^    int \*i;$}}
-// CHECK10: {{^    int\* i;$}}
-// CHECK11: {{^int\*i;$}}
-// CHECK12: {{^      int \*i;$}}
-// CHECK13: {{^       int \*i;$}}
+// CHECK10: {{^      int \*i;$}}
+// CHECK11: {{^       int \*i;$}}
 int*i;
 int j;
 }
