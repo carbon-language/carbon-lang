c38cd326fcd7a62cf188a1c0beb5af47ced8b2ac
Saleem Abdulrasool <compnerd@compnerd.org>
2017-01-24 18:52:19 +0000

    Demangle: avoid butchering parameter type
    When demangling a CV-qualified function type with a final parameter with
    a reference type, we would insert the CV qualification on the parameter
    rather than the function, and in the process adjust the insertion point
    by one extra, splitting the type name.  This avoids doing so, even
    though the attribution is still incorrect.
    
    llvm-svn: 292965

--- llvm/lib/Demangle/ItaniumDemangle.cpp
+++ llvm/lib/Demangle/ItaniumDemangle.cpp
@@ -1665,9 +1665,9 @@ static const char *parse_type(const char *first, const char *last, C &db) {
             if (is_function) {
               size_t p = db.names[k].second.size();
               if (db.names[k].second[p - 2] == '&')
-                p -= 3;
-              else if (db.names[k].second.back() == '&')
                 p -= 2;
+              else if (db.names[k].second.back() == '&')
+                p -= 1;
               if (cv & 1) {
                 db.names[k].second.insert(p, " const");
                 p += 6;
