c7b9dfaf0b1a5c9ebde0d24abbd2bee6d20673bb
Rafael Espindola <rafael.espindola@gmail.com>
2017-01-25 20:53:36 +0000

    Make this test more strict.
    While at it, also remove unused command line options.
    
    llvm-svn: 293084

--- lld/test/ELF/retain-symbols-file.s
+++ lld/test/ELF/retain-symbols-file.s
@@ -3,20 +3,41 @@
 # RUN: echo "bar" > %t_retain.txt
 # RUN: echo "foo" >> %t_retain.txt
 # RUN: ld.lld -shared --retain-symbols-file=%t_retain.txt %t -o %t2
-# RUN: llvm-readobj -s -sd -t %t2 | FileCheck %s
+# RUN: llvm-readobj -t %t2 | FileCheck %s
 
 ## Check separate form.
 # RUN: ld.lld -shared --retain-symbols-file %t_retain.txt %t -o %t2
-# RUN: llvm-readobj -s -sd -t %t2 | FileCheck %s
+# RUN: llvm-readobj -t %t2 | FileCheck %s
 
-# CHECK:       Symbols [
+# CHECK:      Symbols [
 # CHECK-NEXT:   Symbol {
 # CHECK-NEXT:     Name:  (0)
-# CHECK:        Symbol {
+# CHECK-NEXT:     Value:
+# CHECK-NEXT:     Size:
+# CHECK-NEXT:     Binding:
+# CHECK-NEXT:     Type:
+# CHECK-NEXT:     Other:
+# CHECK-NEXT:     Section:
+# CHECK-NEXT:   }
+# CHECK-NEXT:   Symbol {
 # CHECK-NEXT:     Name: bar
-# CHECK:        Symbol {
+# CHECK-NEXT:     Value:
+# CHECK-NEXT:     Size:
+# CHECK-NEXT:     Binding: Global
+# CHECK-NEXT:     Type:
+# CHECK-NEXT:     Other:
+# CHECK-NEXT:     Section: .text
+# CHECK-NEXT:   }
+# CHECK-NEXT:   Symbol {
 # CHECK-NEXT:     Name: foo
-# CHECK-NOT:    Symbol
+# CHECK-NEXT:     Value:
+# CHECK-NEXT:     Size:
+# CHECK-NEXT:     Binding: Global
+# CHECK-NEXT:     Type:
+# CHECK-NEXT:     Other:
+# CHECK-NEXT:     Section: .text
+# CHECK-NEXT:   }
+# CHECK-NEXT: ]
 
 .text
 .globl _start
