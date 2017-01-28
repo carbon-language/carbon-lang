3caaaa94c6a2aea5d0cda2550a9d83a3145ed5fd
Kamil Rytarowski <n54@gmx.com>
2017-01-28 20:04:53 +0000

    Switch HostInfoNetBSD::GetProgramFileSpec to sysctl(7)
    Summary:
    Remove dependency on the proc (/proc) filesystem, which is optional.
    
    KERN_PROC_PATHNAME is available in NetBSD-current and will land NetBSD 8.0.
    Older stable versions of NetBSD will not be supported.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: emaste, joerg, labath, clayborg
    
    Reviewed By: clayborg
    
    Subscribers: #lldb
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D29089
    
    llvm-svn: 293392

--- lldb/source/Host/netbsd/HostInfoNetBSD.cpp
+++ lldb/source/Host/netbsd/HostInfoNetBSD.cpp
@@ -85,15 +85,15 @@ FileSpec HostInfoNetBSD::GetProgramFileSpec() {
   static FileSpec g_program_filespec;
 
   if (!g_program_filespec) {
-    ssize_t len;
-    static char buf[PATH_MAX];
-    char name[PATH_MAX];
+    static const int name[] = {
+        CTL_KERN, KERN_PROC_ARGS, -1, KERN_PROC_PATHNAME,
+    };
+    char path[MAXPATHLEN];
+    size_t len;
 
-    ::snprintf(name, PATH_MAX, "/proc/%d/exe", ::getpid());
-    len = ::readlink(name, buf, PATH_MAX - 1);
-    if (len != -1) {
-      buf[len] = '\0';
-      g_program_filespec.SetFile(buf, false);
+    len = sizeof(path);
+    if (sysctl(name, __arraycount(name), path, &len, NULL, 0) != -1) {
+        g_program_filespec.SetFile(path, false);
     }
   }
   return g_program_filespec;
