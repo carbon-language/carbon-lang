87bf5ada39ecbb167c5d15fa6cea8ad15fedd1d8
George Rimar <grimar@accesssoftek.com>
2017-01-30 13:56:57 +0000

    [ELF] - Change i386 i386-pc8.s/i386-pc16.test to work with 8/16 bits values accordingly.
    ld.bfd showed error on previous inputs, result values were larger than 8/16 bits,
    though ld.gold accepted them.
    
    ABI says "The R_386_16, and R_386_8 relocations truncate the computed 
    value to 16-bits and 8-bits respectively".
    
    Patch changes inputs to have result calculated values of relocations to fit 8 and 16 bits.
    That can be used for implementation of more strict checks, like bfd do.
    
    Differential revision: https://reviews.llvm.org/D29270
    
    llvm-svn: 293479

--- lld/test/ELF/i386-pc8.s
+++ lld/test/ELF/i386-pc8.s
@@ -1,11 +1,11 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=i386-pc-linux-gnu %s -o %t1.o
 # RUN: llvm-mc -filetype=obj -triple=i386-pc-linux-gnu %S/Inputs/i386-pc8.s -o %t2.o
-# RUN: ld.lld %t1.o %t2.o -o %t.out
+# RUN: ld.lld -Ttext 0x0 %t1.o %t2.o -o %t.out
 # RUN: llvm-objdump -s -section=.text %t.out | FileCheck %s
 
 # CHECK:      Contents of section .text:
-# CHECK-NEXT:  11000 15253748
+# CHECK-NEXT:  0000 15253748
 
 .byte und-.+0x11
 .byte und-.+0x22
