fbc229dc8ff04b6fde275e6001099c1146487728
Bob Haarman <llvm@inglorion.net>
2017-02-02 23:53:10 +0000

    added missing files for r293965
    llvm-svn: 293966

---
+++ lld/include/lld/Core/TargetOptionsCommandFlags.h
@@ -0,0 +1,18 @@
+//===-- TargetOptionsCommandFlags.h ----------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Helper to create TargetOptions from command line flags.
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Target/TargetOptions.h"
+
+namespace lld {
+llvm::TargetOptions InitTargetOptionsFromCodeGenFlags();
+}
