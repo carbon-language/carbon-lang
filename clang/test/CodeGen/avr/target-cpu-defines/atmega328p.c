37a2d6d6991c23972cc1f1cded3b73d386d6d177
Diana Picus <diana.picus@linaro.org>
2017-02-06 11:35:42 +0000

    Revert "[AVR] Allow specifying the CPU on the command line"
    This reverts commit r294177. It seems to have broken some buildbots.
    
    llvm-svn: 294180

--- clang/test/CodeGen/avr/target-cpu-defines/atmega328p.c
+++
@@ -1,6 +0,0 @@
-// RUN: %clang_cc1 -E -dM -triple avr-unknown-unknown -target-cpu atmega328p /dev/null | FileCheck -match-full-lines %s
-
-// CHECK: #define AVR 1
-// CHECK: #define __AVR 1
-// CHECK: #define __AVR_ATmega328P 1
-// CHECK: #define __AVR__ 1
