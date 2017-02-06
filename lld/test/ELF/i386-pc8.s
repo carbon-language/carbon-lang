60ca31dff8b9ea750156d41213f54ddf35be1f43
George Rimar <grimar@accesssoftek.com>
2017-02-06 09:25:56 +0000

    [ELF] - Simplify i386-ps8.s testcase.
    Removes dependency on second input file.
    
    llvm-svn: 294178

--- lld/test/ELF/i386-pc8.s
+++ lld/test/ELF/i386-pc8.s
@@ -1,7 +1,6 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=i386-pc-linux-gnu %s -o %t1.o
-# RUN: llvm-mc -filetype=obj -triple=i386-pc-linux-gnu %S/Inputs/i386-pc8.s -o %t2.o
-# RUN: ld.lld -Ttext 0x0 %t1.o %t2.o -o %t.out
+# RUN: ld.lld -Ttext 0x0 %t1.o -o %t.out
 # RUN: llvm-objdump -s -section=.text %t.out | FileCheck %s
 
 # CHECK:      Contents of section .text:
@@ -11,3 +10,6 @@
 .byte und-.+0x22
 .byte und+0x33
 .byte und+0x44
+
+.section .und, "ax"
+und:
