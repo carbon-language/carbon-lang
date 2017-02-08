c12d82ba2dfba2290ba6ffdf2a76a82ddb4e4d79
Nitesh Jain <nitesh.jain@imgtec.com>
2017-02-08 07:29:24 +0000

    [LLDB][MIPS] Fix TestMiniDumpNew
    Reviewers: labath, clayborg
    
    Subscribers: jaydeep, bhushan, lldb-commits, slthakur
    
    Differential Revision: https://reviews.llvm.org/D29215
    
    llvm-svn: 294415

--- lldb/packages/Python/lldbsuite/test/functionalities/postmortem/minidump-new/TestMiniDumpNew.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/postmortem/minidump-new/TestMiniDumpNew.py
@@ -23,6 +23,14 @@ class MiniDumpNewTestCase(TestBase):
     _linux_x86_64_not_crashed_pid = 29939
     _linux_x86_64_not_crashed_pid_offset = 0xD967
 
+    def setUp(self):
+        super(MiniDumpNewTestCase, self).setUp()
+        self._initial_platform = lldb.DBG.GetSelectedPlatform()
+
+    def tearDown(self):
+        lldb.DBG.SetSelectedPlatform(self._initial_platform)
+        super(MiniDumpNewTestCase, self).tearDown()
+
     def test_process_info_in_minidump(self):
         """Test that lldb can read the process information from the Minidump."""
         # target create -c linux-x86_64.dmp
