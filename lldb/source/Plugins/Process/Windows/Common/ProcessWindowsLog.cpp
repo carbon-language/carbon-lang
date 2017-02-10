38e2d5859418f360e6fb345c12041c08e4ca4ea3
Pavel Labath <labath@google.com>
2017-02-10 13:04:32 +0000

    Really fix windows build
    this time I have actually tried that it compiles on windows.
    
    llvm-svn: 294744

--- lldb/source/Plugins/Process/Windows/Common/ProcessWindowsLog.cpp
+++ lldb/source/Plugins/Process/Windows/Common/ProcessWindowsLog.cpp
@@ -102,7 +102,7 @@ void ProcessWindowsLog::DisableLog(const char **args, Stream *feedback_strm) {
     log->GetMask().Reset(flag_bits);
     if (flag_bits == 0) {
       g_log_enabled = false;
-      log->SetStream(lldb::StreamSP());
+      log->SetStream(nullptr);
     }
   }
 
@@ -110,7 +110,7 @@ void ProcessWindowsLog::DisableLog(const char **args, Stream *feedback_strm) {
 }
 
 Log *ProcessWindowsLog::EnableLog(
-    std::shared_ptr<llvm::raw_ostream> &log_stream_sp, uint32_t log_options,
+    const std::shared_ptr<llvm::raw_ostream> &log_stream_sp, uint32_t log_options,
     const char **args, Stream *feedback_strm) {
   // Try see if there already is a log - that way we can reuse its settings.
   // We could reuse the log in toto, but we don't know that the stream is the
