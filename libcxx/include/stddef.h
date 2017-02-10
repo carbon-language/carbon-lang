4b2273622298d4dfd70a736de39d52581aee1b1b
David L. Jones <dlj@google.com>
2017-02-10 01:27:42 +0000

    Check for musl-libc's max_align_t in addition to other variants.
    Summary:
    Libcxx will define its own max_align_t when it is not available. However, the
    availability checks today only check for Clang's definition and GCC's
    definition. In particular, it does not check for musl's definition, which is the
    same as GCC's but guarded with a different macro.
    
    Reviewers: mclow.lists, EricWF
    
    Reviewed By: EricWF
    
    Subscribers: chandlerc, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D28478
    
    llvm-svn: 294683

--- libcxx/include/stddef.h
+++ libcxx/include/stddef.h
@@ -53,7 +53,8 @@ using std::nullptr_t;
 }
 
 // Re-use the compiler's <stddef.h> max_align_t where possible.
-#if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T)
+#if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && \
+    !defined(__DEFINED_max_align_t)
 typedef long double max_align_t;
 #endif
 
