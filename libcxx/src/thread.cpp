de5669e46c4f391fb3d939e80fc8613f92190181
Ed Schouten <ed@nuxi.nl>
2017-02-11 08:30:18 +0000

    Fix the build of thread.cpp on CloudABI.
    CloudABI does provide unistd.h, but doesn't define __unix__. We need to
    include this header file to make hardware_concurrency work.
    
    llvm-svn: 294832

--- libcxx/src/thread.cpp
+++ libcxx/src/thread.cpp
@@ -24,9 +24,9 @@
 # endif // defined(BSD)
 #endif // defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
 
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__CloudABI__)
 # include <unistd.h>
-#endif // defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
+#endif // defined(__unix__) || (defined(__APPLE__) && defined(__MACH__)) || defined(__CloudABI__)
 
 #if defined(__NetBSD__)
 #pragma weak pthread_create // Do not create libpthread dependency
