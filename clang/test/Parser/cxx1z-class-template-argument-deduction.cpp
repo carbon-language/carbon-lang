eb7702e07d1d924752bd41e707a66e9a755297fc
NAKAMURA Takumi <geek4civic@gmail.com>
2017-02-14 03:18:24 +0000

    clang/test/Parser/cxx1z-class-template-argument-deduction.cpp: Tweak to ignore thiscall.
      Line 38: multiple overloads of 'f' instantiate to the same signature 'void (int) __attribute__((thiscall))'
    
    llvm-svn: 295020

--- clang/test/Parser/cxx1z-class-template-argument-deduction.cpp
+++ clang/test/Parser/cxx1z-class-template-argument-deduction.cpp
@@ -35,7 +35,7 @@ namespace injected_class_name {
       A a = 1;
       injected_class_name::A b = 1; // expected-note {{in instantiation of template class 'injected_class_name::A<int>'}}
     }
-    void f(T); // expected-error {{multiple overloads of 'f' instantiate to the same signature 'void (int)'}}
+    void f(T); // expected-error {{multiple overloads of 'f' instantiate to the same signature 'void (int)}}
   };
   A<short> ai = 1;
   A<double>::A b(1); // expected-error {{constructor name}}
