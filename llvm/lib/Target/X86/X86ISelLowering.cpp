2f2d8dc6304b18862f62fa33e7324a9a8099657f
Simon Pilgrim <llvm-dev@redking.me.uk>
2017-02-18 22:56:17 +0000

    Fix signed/unsigned comparison warning.
    llvm-svn: 295580

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -5738,8 +5738,8 @@ static bool getFauxShuffleMask(SDValue N, SmallVectorImpl<int> &Mask,
     // Attempt to recognise a PINSR*(VEC, 0, Idx) shuffle pattern.
     if (X86::isZeroNode(InScl)) {
       Ops.push_back(InVec);
-      for (int i = 0; i != (int)NumElts; ++i)
-        Mask.push_back(i == InIdx ? SM_SentinelZero : i);
+      for (unsigned i = 0; i != NumElts; ++i)
+        Mask.push_back(i == InIdx ? SM_SentinelZero : (int)i);
       return true;
     }
 
