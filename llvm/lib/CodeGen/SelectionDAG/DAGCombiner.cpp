f0a4823b919a07a53bf8e5b4e5f5f4a75660f29e
Matt Arsenault <Matthew.Arsenault@amd.com>
2017-02-21 22:47:27 +0000

    DAG: Check if extract_vector_elt is legal or custom
    Avoids test regressions in future AMDGPU commits when
    more vector types are custom lowered.
    
    llvm-svn: 295782

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -7882,7 +7882,7 @@ SDValue DAGCombiner::visitTRUNCATE(SDNode *N) {
     EVT SrcVT = VecSrc.getValueType();
     if (SrcVT.isVector() && SrcVT.getScalarType() == VT &&
         (!LegalOperations ||
-         TLI.isOperationLegal(ISD::EXTRACT_VECTOR_ELT, SrcVT))) {
+         TLI.isOperationLegalOrCustom(ISD::EXTRACT_VECTOR_ELT, SrcVT))) {
       SDLoc SL(N);
 
       EVT IdxVT = TLI.getVectorIdxTy(DAG.getDataLayout());
