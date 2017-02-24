9a2bba7251ab552b941d3964d6cb6f2b54fd14c0
Petr Hosek <phosek@chromium.org>
2017-02-24 03:18:59 +0000

    Revert "[ELF] Make __ehdr_start point to ELF file headers"
    This reverts commit r296079.
    
    llvm-svn: 296083

--- lld/test/ELF/ehdr_start.s
+++ lld/test/ELF/ehdr_start.s
@@ -2,16 +2,8 @@
 
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 # RUN: ld.lld %t.o -o %t
-# RUN: llvm-readobj -symbols %t | FileCheck %s
-# CHECK:    Name: __ehdr_start (1)
-# CHECK-NEXT:    Value: 0x200000
-# CHECK-NEXT:    Size: 0
-# CHECK-NEXT:    Binding: Local (0x0)
-# CHECK-NEXT:    Type: None (0x0)
-# CHECK-NEXT:    Other [ (0x2)
-# CHECK-NEXT:      STV_HIDDEN (0x2)
-# CHECK-NEXT:    ]
-# CHECK-NEXT:    Section: Undefined (0x0)
+# RUN: llvm-objdump -t %t | FileCheck %s
+# CHECK: 0000000000200040 .text 00000000 .hidden __ehdr_start
 
 .text
 .global _start, __ehdr_start
@@ -19,12 +11,6 @@ _start:
   .quad __ehdr_start
 
 # RUN: ld.lld -r %t.o -o %t.r
-# RUN: llvm-readobj -symbols %t.r | FileCheck %s --check-prefix=RELOCATABLE
+# RUN: llvm-objdump -t %t.r | FileCheck %s --check-prefix=RELOCATABLE
 
-# RELOCATABLE:    Name: __ehdr_start (1)
-# RELOCATABLE-NEXT:    Value: 0x0
-# RELOCATABLE-NEXT:    Size: 0
-# RELOCATABLE-NEXT:    Binding: Global (0x1)
-# RELOCATABLE-NEXT:    Type: None (0x0)
-# RELOCATABLE-NEXT:    Other: 0
-# RELOCATABLE-NEXT:    Section: Undefined (0x0)
+# RELOCATABLE: 0000000000000000 *UND* 00000000 __ehdr_start
