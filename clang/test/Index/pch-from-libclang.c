df3e14a1683def39a3b56ee3ddf359b9f340cd92
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2017-02-25 18:35:53 +0000

    [test] Add target to test/Index/pch-from-libclang.c.
    Attempt to fix the failing bots.
    
    llvm-svn: 296263

--- clang/test/Index/pch-from-libclang.c
+++ clang/test/Index/pch-from-libclang.c
@@ -1,6 +1,6 @@
 // Check that clang can use a PCH created from libclang.
-// RUN: c-index-test -write-pch %t.h.pch %s -fmodules -fmodules-cache-path=%t.mcp
-// RUN: %clang -fsyntax-only -include %t.h %s -Xclang -verify -fmodules -fmodules-cache-path=%t.mcp -Xclang -detailed-preprocessing-record
+// RUN: c-index-test -write-pch %t.h.pch %s -fmodules -fmodules-cache-path=%t.mcp -target x86_64-apple-darwin
+// RUN: %clang -fsyntax-only -include %t.h %s -Xclang -verify -fmodules -fmodules-cache-path=%t.mcp -Xclang -detailed-preprocessing-record -target x86_64-apple-darwin
 
 // expected-no-diagnostics
 
