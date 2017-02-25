30844025e9922296104397cc81b584c5ec75aeb2
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2017-02-25 18:55:32 +0000

    [test] Use cc1 -triple for test/Index/pch-from-libclang.c.
    Note quite sure why driver -target has no effect.
    
    llvm-svn: 296265

--- clang/test/Index/pch-from-libclang.c
+++ clang/test/Index/pch-from-libclang.c
@@ -1,6 +1,6 @@
 // Check that clang can use a PCH created from libclang.
-// RUN: c-index-test -write-pch %t.h.pch %s -fmodules -fmodules-cache-path=%t.mcp -target x86_64-apple-darwin
-// RUN: %clang -fsyntax-only -include %t.h %s -Xclang -verify -fmodules -fmodules-cache-path=%t.mcp -Xclang -detailed-preprocessing-record -target x86_64-apple-darwin
+// RUN: c-index-test -write-pch %t.h.pch %s -fmodules -fmodules-cache-path=%t.mcp -Xclang -triple -Xclang x86_64-apple-darwin
+// RUN: %clang -fsyntax-only -include %t.h %s -Xclang -verify -fmodules -fmodules-cache-path=%t.mcp -Xclang -detailed-preprocessing-record -Xclang -triple -Xclang x86_64-apple-darwin
 
 // expected-no-diagnostics
 
