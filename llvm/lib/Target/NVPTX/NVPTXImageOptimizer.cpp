1d1cf7ba5d8c749cf78b7a9318e33d8080389fbe
Justin Lebar <jlebar@google.com>
2017-03-08 01:14:15 +0000

    [NVPTX] Remove unnecessary isImageReadoOnly(), isImageWriteOnly(), & isImageReadWrite calls
    This is repetition of isImage() function in NVPTXUtilities.cpp.
    
    Patch by Briana Grace!
    
    Differential Revision: https://reviews.llvm.org/D30706
    
    llvm-svn: 297252

--- llvm/lib/Target/NVPTX/NVPTXImageOptimizer.cpp
+++ llvm/lib/Target/NVPTX/NVPTXImageOptimizer.cpp
@@ -96,9 +96,7 @@ bool NVPTXImageOptimizer::replaceIsTypePSampler(Instruction &I) {
     // This is an OpenCL sampler, so it must be a samplerref
     replaceWith(&I, ConstantInt::getTrue(I.getContext()));
     return true;
-  } else if (isImageWriteOnly(*TexHandle) ||
-             isImageReadWrite(*TexHandle) ||
-             isImageReadOnly(*TexHandle)) {
+  } else if (isImage(*TexHandle)) {
     // This is an OpenCL image, so it cannot be a samplerref
     replaceWith(&I, ConstantInt::getFalse(I.getContext()));
     return true;
