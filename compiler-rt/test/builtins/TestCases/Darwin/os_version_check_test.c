6fa97926e910b2ebaa3b8c2ce70f9a27320d0be0
Erik Pilkington <erik.pilkington@gmail.com>
2017-03-09 14:17:36 +0000

    [compiler-rt][builtins] Add __isOSVersionAtLeast()
    This predicate compares the host's marketing OS version to one passed as
    argument. Currently, only darwin targets are supported. This is done by parsing
    the SystemVersion.plist file.
    
    Also added in this patch is some lit testing infrastructure for builtins, which
    previously had none. This part of the patch was written by Alex Lorenz (with
    some minor modifications).
    
    This patch is part of a feature I proposed here:
    http://lists.llvm.org/pipermail/cfe-dev/2016-July/049851.html
    
    Differential revision: https://reviews.llvm.org/D30136
    
    llvm-svn: 297382

---
+++ compiler-rt/test/builtins/TestCases/Darwin/os_version_check_test.c
@@ -0,0 +1,19 @@
+// RUN: %clang %s -o %t -mmacosx-version-min=10.5 -framework CoreFoundation -DMAJOR=%macos_version_major -DMINOR=%macos_version_minor -DSUBMINOR=%macos_version_subminor
+// RUN: %run %t
+
+int __isOSVersionAtLeast(int Major, int Minor, int Subminor);
+
+int main() {
+  if (!__isOSVersionAtLeast(MAJOR, MINOR, SUBMINOR))
+    return 1;
+  if (__isOSVersionAtLeast(MAJOR, MINOR, SUBMINOR + 1))
+    return 1;
+  if (SUBMINOR && __isOSVersionAtLeast(MAJOR + 1, MINOR, SUBMINOR - 1))
+    return 1;
+  if (SUBMINOR && !__isOSVersionAtLeast(MAJOR, MINOR, SUBMINOR - 1))
+    return 1;
+  if (MAJOR && !__isOSVersionAtLeast(MAJOR - 1, MINOR + 1, SUBMINOR))
+    return 1;
+
+  return 0;
+}
