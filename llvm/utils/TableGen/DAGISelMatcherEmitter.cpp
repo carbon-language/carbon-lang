ab0d1e4a2f54699df9169fb3912dd524feeb9b31
Joey Gouly <joey.gouly@gmail.com>
2017-03-09 13:38:06 +0000

    [SelectionDAG] Make SelectCode return void
    SelectCode has been returning nullptr since 182dac0 ("SDAG: Make
    SelectCodeCommon return void", 2016-05-10). Make SelectCode also
    return void instead, as all callers have been updated.
    
    Patch by Sven van Haastregt.
    
    Review: https://reviews.llvm.org/D30497
    llvm-svn: 297377

--- llvm/utils/TableGen/DAGISelMatcherEmitter.cpp
+++ llvm/utils/TableGen/DAGISelMatcherEmitter.cpp
@@ -955,7 +955,7 @@ void llvm::EmitMatcherTable(const Matcher *TheMatcher,
   formatted_raw_ostream OS(O);
 
   OS << "// The main instruction selector code.\n";
-  OS << "SDNode *SelectCode(SDNode *N) {\n";
+  OS << "void SelectCode(SDNode *N) {\n";
 
   MatcherTableEmitter MatcherEmitter(CGP);
 
@@ -970,7 +970,6 @@ void llvm::EmitMatcherTable(const Matcher *TheMatcher,
 
   OS << "  #undef TARGET_VAL\n";
   OS << "  SelectCodeCommon(N, MatcherTable,sizeof(MatcherTable));\n";
-  OS << "  return nullptr;\n";
   OS << "}\n";
 
   // Next up, emit the function for node and pattern predicates:
