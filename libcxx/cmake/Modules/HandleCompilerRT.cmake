704c8a929b1f93ff808b3c8d1ecfd914c72f7c67
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2017-03-14 04:12:29 +0000

    Fix cmake to find the compiler-rt libs on darwin
    Followup for r297553, which left darwin in a broken state
    http://green.lab.llvm.org/green/job/clang-stage2-cmake-RgSan_build/3812
    
    rdar://problem/31011980
    
    llvm-svn: 297703

--- libcxx/cmake/Modules/HandleCompilerRT.cmake
+++ libcxx/cmake/Modules/HandleCompilerRT.cmake
@@ -25,15 +25,27 @@ function(find_compiler_rt_dir dest)
     message(FATAL_ERROR "LIBCXX_COMPILE_FLAGS must be defined when using this function")
   endif()
   set(dest "" PARENT_SCOPE)
-  set(CLANG_COMMAND ${CMAKE_CXX_COMPILER} ${LIBCXX_COMPILE_FLAGS}
-      "--rtlib=compiler-rt" "--print-libgcc-file-name")
-  execute_process(
-      COMMAND ${CLANG_COMMAND}
-      RESULT_VARIABLE HAD_ERROR
-      OUTPUT_VARIABLE LIBRARY_FILE
-  )
-  string(STRIP "${LIBRARY_FILE}" LIBRARY_FILE)
-  get_filename_component(LIBRARY_DIR "${LIBRARY_FILE}" DIRECTORY)
+  if (APPLE)
+    set(CLANG_COMMAND ${CMAKE_CXX_COMPILER} ${LIBCXX_COMPILE_FLAGS}
+        "-print-file-name=lib")
+    execute_process(
+        COMMAND ${CLANG_COMMAND}
+        RESULT_VARIABLE HAD_ERROR
+        OUTPUT_VARIABLE LIBRARY_DIR
+    )
+    string(STRIP "${LIBRARY_DIR}" LIBRARY_DIR)
+    set(LIBRARY_DIR "${LIBRARY_DIR}/darwin")
+  else()
+    set(CLANG_COMMAND ${CMAKE_CXX_COMPILER} ${LIBCXX_COMPILE_FLAGS}
+        "--rtlib=compiler-rt" "--print-libgcc-file-name")
+    execute_process(
+        COMMAND ${CLANG_COMMAND}
+        RESULT_VARIABLE HAD_ERROR
+        OUTPUT_VARIABLE LIBRARY_FILE
+    )
+    string(STRIP "${LIBRARY_FILE}" LIBRARY_FILE)
+    get_filename_component(LIBRARY_DIR "${LIBRARY_FILE}" DIRECTORY)
+  endif()
   if (NOT HAD_ERROR AND EXISTS "${LIBRARY_DIR}")
     message(STATUS "Found compiler-rt directory: ${LIBRARY_DIR}")
     set(${dest} "${LIBRARY_DIR}" PARENT_SCOPE)
