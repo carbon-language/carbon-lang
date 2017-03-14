dd913de968fbd4df48a8525715f6ca68c403b13d
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2017-03-14 19:33:44 +0000

    [cfi] Add test for type metadata split issue.
    https://reviews.llvm.org/D30716
    
    llvm-svn: 297771

---
+++ compiler-rt/test/cfi/bad-split.cpp
@@ -0,0 +1,19 @@
+// GlobalSplit used to lose type metadata for classes with virtual bases but no virtual methods.
+// RUN: %clangxx_cfi -o %t1 %s && %t1
+
+struct Z {
+};
+
+struct ZZ : public virtual Z {
+};
+
+struct A : public ZZ {
+};
+
+struct B : public A {
+};
+
+int main() {
+  A* a = new B();
+  B *b = (B*)a;
+}
