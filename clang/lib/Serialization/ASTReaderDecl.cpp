c6c4ec8099cfece8eb362c381796ba0905260720
Argyrios Kyrtzidis <akyrtzi@gmail.com>
2017-03-17 00:49:42 +0000

    [Serialization] Follow-up to r297972, deserialize name/loc in separate statements to make sure they deserialize in defined order.
    This should fix the windows bots.
    
    llvm-svn: 298027

--- clang/lib/Serialization/ASTReaderDecl.cpp
+++ clang/lib/Serialization/ASTReaderDecl.cpp
@@ -1124,10 +1124,12 @@ void ASTDeclReader::VisitObjCPropertyDecl(ObjCPropertyDecl *D) {
       (ObjCPropertyDecl::PropertyAttributeKind)Record.readInt());
   D->setPropertyImplementation(
       (ObjCPropertyDecl::PropertyControl)Record.readInt());
-  D->setGetterName(Record.readDeclarationName().getObjCSelector(),
-                   ReadSourceLocation());
-  D->setSetterName(Record.readDeclarationName().getObjCSelector(),
-                   ReadSourceLocation());
+  DeclarationName GetterName = Record.readDeclarationName();
+  SourceLocation GetterLoc = ReadSourceLocation();
+  D->setGetterName(GetterName.getObjCSelector(), GetterLoc);
+  DeclarationName SetterName = Record.readDeclarationName();
+  SourceLocation SetterLoc = ReadSourceLocation();
+  D->setSetterName(SetterName.getObjCSelector(), SetterLoc);
   D->setGetterMethodDecl(ReadDeclAs<ObjCMethodDecl>());
   D->setSetterMethodDecl(ReadDeclAs<ObjCMethodDecl>());
   D->setPropertyIvarDecl(ReadDeclAs<ObjCIvarDecl>());
