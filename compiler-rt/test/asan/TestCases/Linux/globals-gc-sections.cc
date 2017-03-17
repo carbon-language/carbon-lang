79ee0c1813780eb66bf53eb857d06a3226ddda07
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2017-03-17 22:17:38 +0000

    [asan] Fix dead stripping of globals on Linux (compiler-rt)
    Runtime support for the new instrumentation of globals based on !associated, and a bunch of tests.
    
    Differential Revision: https://reviews.llvm.org/D30120
    
    llvm-svn: 298159

--- compiler-rt/test/asan/TestCases/Linux/globals-gc-sections.cc
+++ compiler-rt/test/asan/TestCases/Linux/globals-gc-sections.cc
@@ -1,12 +1,18 @@
-// RUN: %clangxx_asan %s -o %t -Wl,--gc-sections -ffunction-sections -mllvm -asan-globals=0
-// RUN: %clangxx_asan %s -o %t -Wl,--gc-sections -ffunction-sections -mllvm -asan-globals=1
+// RUN: %clangxx_asan %s -o %t -Wl,--gc-sections -fuse-ld=bfd -ffunction-sections -fdata-sections -mllvm -asan-globals=0
+// RUN: %clangxx_asan %s -o %t -Wl,--gc-sections -fuse-ld=bfd -ffunction-sections -fdata-sections -mllvm -asan-globals=1
 
 // https://code.google.com/p/address-sanitizer/issues/detail?id=260
-// XFAIL: *
 
 int undefined();
 
-int (*unused)() = undefined;
+// bug in ld.bfd: with multiple "asan_globals" sections, __start_asan_globals is
+// treated as a strong GC reference to the first such section. As a result, the
+// first (for some definition of the word) global is never gc-ed.
+int first_unused = 42;
+
+// On i386 clang adds --export-dynamic when linking with ASan, which adds all
+// non-hidden globals to GC roots.
+__attribute__((visibility("hidden"))) int (*unused)() = undefined;
 
 int main() {
         return 0;
