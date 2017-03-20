8a67eb144fcd91d14431434bd9fc26c59305e02f
Konstantin Zhuravlyov <kzhuravl_dev@outlook.com>
2017-03-20 09:26:08 +0000

    Revert "[AMDGPU] Run always inliner early in opt"
    This reverts commit r297958, it breaks device-libs build.
    
    llvm-svn: 298239

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -253,7 +253,6 @@ void AMDGPUTargetMachine::adjustPassManager(PassManagerBuilder &Builder) {
         }));
         PM.add(createGlobalDCEPass());
       }
-      PM.add(createAMDGPUAlwaysInlinePass());
   });
 }
 
