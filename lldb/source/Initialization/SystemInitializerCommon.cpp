a87101d6a777c7d3ed9c516509a324a9b77903ad
Kamil Rytarowski <n54@gmx.com>
2017-03-21 17:26:55 +0000

    Enable ProcessPOSIXLog on NetBSD
    Summary:
    NetBSD can share the same logging functionality with Linux and FreeBSD.
    
    Sponsored by <The NetBSD Foundation>
    
    
    Reviewers: labath, emaste, joerg, kettenis
    
    Reviewed By: labath, emaste
    
    Subscribers: #lldb
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D31191
    
    llvm-svn: 298406

--- lldb/source/Initialization/SystemInitializerCommon.cpp
+++ lldb/source/Initialization/SystemInitializerCommon.cpp
@@ -26,7 +26,7 @@
 #include "Plugins/ObjectFile/Mach-O/ObjectFileMachO.h"
 #endif
 
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
 #include "Plugins/Process/POSIX/ProcessPOSIXLog.h"
 #endif
 
@@ -93,7 +93,7 @@ void SystemInitializerCommon::Initialize() {
 #if defined(__APPLE__)
   ObjectFileMachO::Initialize();
 #endif
-#if defined(__linux__) || defined(__FreeBSD__)
+#if defined(__linux__) || defined(__FreeBSD__) || defined(__NetBSD__)
   ProcessPOSIXLog::Initialize();
 #endif
 #if defined(_MSC_VER)
