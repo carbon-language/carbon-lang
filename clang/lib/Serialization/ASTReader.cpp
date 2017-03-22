573b13fb9c6f80a9d00cd5c660fc57850e5bf425
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2017-03-22 00:11:21 +0000

    [Modules] Rebuild modules on umbrella header mismatch
    This restores behavior pre-r230064 since after PCMCache work (r298278)
    we don't reload PCMs from disk within the same compiler invocation.
    
    Testcases from r230064 are still left around since they still guarantee
    the correct behavior we're expecting.
    
    rdar://problem/19889777
    
    llvm-svn: 298464

--- clang/lib/Serialization/ASTReader.cpp
+++ clang/lib/Serialization/ASTReader.cpp
@@ -4890,13 +4890,9 @@ ASTReader::ReadSubmoduleBlock(ModuleFile &F, unsigned ClientLoadCapabilities) {
         if (!CurrentModule->getUmbrellaHeader())
           ModMap.setUmbrellaHeader(CurrentModule, Umbrella, Blob);
         else if (CurrentModule->getUmbrellaHeader().Entry != Umbrella) {
-          // This can be a spurious difference caused by changing the VFS to
-          // point to a different copy of the file, and it is too late to
-          // to rebuild safely.
-          // FIXME: If we wrote the virtual paths instead of the 'real' paths,
-          // after input file validation only real problems would remain and we
-          // could just error. For now, assume it's okay.
-          break;
+          if ((ClientLoadCapabilities & ARR_OutOfDate) == 0)
+            Error("mismatched umbrella headers in submodule");
+          return OutOfDate;
         }
       }
       break;
