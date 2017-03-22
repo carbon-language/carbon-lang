3087c96348e26da71fcfe75fe0f3320e1a82bdcc
Adam Nemet <anemet@apple.com>
2017-03-22 00:58:15 +0000

    Change -ffp-contract=fast test to run on Aarch64
    (I don't have powerpc enabled in my build and I am changing
    how -ffp-contract=fast works.)
    
    llvm-svn: 298468

--- clang/test/CodeGen/ffp-contract-option.c
+++ clang/test/CodeGen/ffp-contract-option.c
@@ -1,8 +1,8 @@
-// RUN: %clang_cc1 -O3 -ffp-contract=fast -triple=powerpc-apple-darwin10 -S -o - %s | FileCheck %s
-// REQUIRES: powerpc-registered-target
+// RUN: %clang_cc1 -O3 -ffp-contract=fast -triple=aarch64-apple-darwin -S -o - %s | FileCheck %s
+// REQUIRES: aarch64-registered-target
 
 float fma_test1(float a, float b, float c) {
-// CHECK: fmadds
+// CHECK: fmadd
   float x = a * b;
   float y = x + c;
   return y;
