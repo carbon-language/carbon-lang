26b09b27e5c7bc9e35875df4fc349529429c185e
Kamil Rytarowski <n54@gmx.com>
2017-03-22 17:24:37 +0000

    Reuse appropriate Launch and Attach on NetBSD
    Summary:
    NetBSD ships with NativeProcessNetBSD  inherited from NativeProcessProtocol.
    
    Link Plugins/Process/gdb-remote with lldbPluginProcessNetBSD in order to resolve
    correctly the linking to Launch and Attach from the NetBSD plugin.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: kettenis, labath, emaste, joerg
    
    Reviewed By: labath, emaste
    
    Subscribers: mgorny, #lldb
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D31231
    
    llvm-svn: 298524

--- lldb/source/Host/common/NativeProcessProtocol.cpp
+++ lldb/source/Host/common/NativeProcessProtocol.cpp
@@ -504,7 +504,7 @@ Error NativeProcessProtocol::ResolveProcessArchitecture(lldb::pid_t pid,
     return Error("failed to retrieve a valid architecture from the exe module");
 }
 
-#ifndef __linux__
+#if !defined(__linux__) && !defined(__NetBSD__)
 // These need to be implemented to support lldb-gdb-server on a given platform.
 // Stubs are
 // provided to make the rest of the code link on non-supported platforms.
