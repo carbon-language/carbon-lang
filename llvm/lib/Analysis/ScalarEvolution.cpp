f47c27513b20a41ce23f46b6857e2b8886eb9d74
Zhaoshi Zheng <zhaoshiz@coduaurora.org>
2017-03-23 17:52:20 +0000

    revert test commit r298629
    llvm-svn: 298630

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -95,8 +95,6 @@
 #include <algorithm>
 using namespace llvm;
 
-
-
 #define DEBUG_TYPE "scalar-evolution"
 
 STATISTIC(NumArrayLenItCounts,
@@ -114,8 +112,6 @@ MaxBruteForceIterations("scalar-evolution-max-iterations", cl::ReallyHidden,
                                  "symbolically execute a constant "
                                  "derived loop"),
                         cl::init(100));
-
-
 
 // FIXME: Enable this with EXPENSIVE_CHECKS when the test suite is clean.
 static cl::opt<bool>
