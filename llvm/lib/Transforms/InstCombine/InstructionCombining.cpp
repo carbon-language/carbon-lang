36f2e0eee834fca853e6e0df6b4fa76b06c0f329
Craig Topper <craig.topper@gmail.com>
2017-03-24 02:58:02 +0000

    [InstCombine] Use range-based for loop. NFC
    llvm-svn: 298680

--- llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
+++ llvm/lib/Transforms/InstCombine/InstructionCombining.cpp
@@ -3019,12 +3019,11 @@ static bool AddReachableCodeToWorklist(BasicBlock *BB, const DataLayout &DL,
         }
 
       // See if we can constant fold its operands.
-      for (User::op_iterator i = Inst->op_begin(), e = Inst->op_end(); i != e;
-           ++i) {
-        if (!isa<ConstantVector>(i) && !isa<ConstantExpr>(i))
+      for (Use &U : Inst->operands()) {
+        if (!isa<ConstantVector>(U) && !isa<ConstantExpr>(U))
           continue;
 
-        auto *C = cast<Constant>(i);
+        auto *C = cast<Constant>(U);
         Constant *&FoldRes = FoldedConstants[C];
         if (!FoldRes)
           FoldRes = ConstantFoldConstant(C, DL, TLI);
@@ -3035,7 +3034,7 @@ static bool AddReachableCodeToWorklist(BasicBlock *BB, const DataLayout &DL,
           DEBUG(dbgs() << "IC: ConstFold operand of: " << *Inst
                        << "\n    Old = " << *C
                        << "\n    New = " << *FoldRes << '\n');
-          *i = FoldRes;
+          U = FoldRes;
           MadeIRChange = true;
         }
       }
