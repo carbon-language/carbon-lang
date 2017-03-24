cbc69712da58c6e9a4d72fe37e829bb27409d3b8
Peter Collingbourne <peter@pcc.me.uk>
2017-03-24 19:32:20 +0000

    Fix a test so that it actually checks the output.
    llvm-svn: 298728

--- clang/test/Modules/module-impl-with-link.c
+++ clang/test/Modules/module-impl-with-link.c
@@ -1,5 +1,5 @@
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -fmodules-cache-path=%t -fmodules -fimplicit-module-maps -fmodule-name=Clib %s -I %S/Inputs/module-impl-with-link -emit-llvm -o -
+// RUN: %clang_cc1 -fmodules-cache-path=%t -fmodules -fimplicit-module-maps -fmodule-name=Clib %s -I %S/Inputs/module-impl-with-link -emit-llvm -o - | FileCheck %s
 #include "foo.h"
 // CHECK: !{{[0-9]+}} = !{i32 6, !"Linker Options", ![[LINK_OPTIONS:[0-9]+]]}
 // Make sure we don't generate linker option for module Clib since this TU is
