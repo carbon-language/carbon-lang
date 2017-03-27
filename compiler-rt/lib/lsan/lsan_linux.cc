3e564e985f7f26a7eb1401ec87754df4f1124bbb
Francis Ricci <francisjricci@gmail.com>
2017-03-27 19:42:37 +0000

    Move lsan allocator cache from lsan_common_linux to lsan_linux
    Having this function in common seems to trigger a lot of unrelated
    test failures. Given that this isn't really common code anyway,
    move this to a new linux-specific lsan file.
    
    llvm-svn: 298878

---
+++ compiler-rt/lib/lsan/lsan_linux.cc
@@ -0,0 +1,26 @@
+//=-- lsan_linux.cc -------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is a part of LeakSanitizer. Linux-specific code.
+//
+//===----------------------------------------------------------------------===//
+
+#if SANITIZER_LINUX
+
+#include "lsan_allocator.h"
+
+namespace __lsan {
+
+static THREADLOCAL AllocatorCache allocator_cache;
+AllocatorCache *GetAllocatorCache() { return &allocator_cache; }
+
+} // namespace __lsan
+
+#endif // CAN_SANITIZE_LEAKS && SANITIZER_LINUX
+
