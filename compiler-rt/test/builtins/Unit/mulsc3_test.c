ce957f9070982912a817188fc616158f9268f85d
Weiming Zhao <weimingz@codeaurora.org>
2017-03-29 14:55:51 +0000

    [Compiler-RT][Builtins] Remove XFAIL for mulsc3; NFC
    It was XFAILed in r298974. However, the problem was not exposed on
    the buildbot because hardfp flag was not passed during the test.
    
    We can fix the CMAKE to pass the same flag as building the lib to the
    RUN line to see if the problem is still there. For now, we remove the
    XFAIL.
    
    llvm-svn: 298997

--- compiler-rt/test/builtins/Unit/mulsc3_test.c
+++ compiler-rt/test/builtins/Unit/mulsc3_test.c
@@ -1,6 +1,4 @@
 // RUN: %clang_builtins %s %librt -lm -o %t && %run %t
-// XFAIL: armhf-target-arch
-// See pr32261
 //===-- mulsc3_test.c - Test __mulsc3 -------------------------------------===//
 //
 //                     The LLVM Compiler Infrastructure
