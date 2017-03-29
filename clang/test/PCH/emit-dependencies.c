317b931823b2d25958c57c463a051a1f6fb399ea
Graydon Hoare <ghoare@apple.com>
2017-03-29 17:58:41 +0000

    Unbreak windows bot.
    llvm-svn: 299012

--- clang/test/PCH/emit-dependencies.c
+++ clang/test/PCH/emit-dependencies.c
@@ -1,7 +1,7 @@
 // RUN: rm -f %t.pch
 // RUN: %clang_cc1 -emit-pch -o %t.pch %S/Inputs/chain-decls1.h
 // RUN: %clang_cc1 -include-pch %t.pch -fsyntax-only -MT %s.o -dependency-file - %s | FileCheck %s
-// CHECK: Inputs/chain-decls1.h
+// CHECK: chain-decls1.h
 
 int main() {
   f();
