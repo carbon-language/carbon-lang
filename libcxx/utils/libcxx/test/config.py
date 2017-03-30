566279fad79261294957b48ab9161f741ca24fc6
Mehdi Amini <mehdi.amini@apple.com>
2017-03-30 05:07:18 +0000

    libc++ testing: fix assertion around `use_system_cxx_lib`
    Actually fix (hopefully) the assertions about `use_system_cxx_lib`,
    the previous attempt failed because I misread the error.
    
    llvm-svn: 299056

--- libcxx/utils/libcxx/test/config.py
+++ libcxx/utils/libcxx/test/config.py
@@ -279,8 +279,8 @@ class Configuration(object):
             self.use_system_cxx_lib = True
         elif self.use_system_cxx_lib == 'false':
             self.use_system_cxx_lib = False
-        else:
-            assert os.path.isdir(str(self.use_system_cxx_lib))
+        else if self.use_system_cxx_lib:
+            assert os.path.isdir(self.use_system_cxx_lib)
         self.lit_config.note(
             "inferred use_system_cxx_lib as: %r" % self.use_system_cxx_lib)
 
