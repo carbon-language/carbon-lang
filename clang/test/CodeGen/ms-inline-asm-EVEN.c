b76fb84032fb25f26041101ec78096bd82a518a0
Coby Tayree <coby.tayree@intel.com>
2017-04-04 19:20:21 +0000

    [fixup][X86][inline-asm] Add support for MS 'EVEN' directive
    refining tested targets resolution, to amend failures caused by rL299454
    
    llvm-svn: 299459

--- clang/test/CodeGen/ms-inline-asm-EVEN.c
+++ clang/test/CodeGen/ms-inline-asm-EVEN.c
@@ -1,4 +1,5 @@
-// RUN: %clang_cc1 %s -triple i386-unknown-unknown -fasm-blocks -emit-llvm -o - | FileCheck %s
+// REQUIRES: x86-registered-target
+// RUN: %clang_cc1 %s -triple i386-apple-darwin10 -fasm-blocks -emit-llvm -o - | FileCheck %s
 
 // CHECK: .byte 64
 // CHECK: .byte 64
