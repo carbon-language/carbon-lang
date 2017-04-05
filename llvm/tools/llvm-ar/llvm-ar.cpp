de87d145cca526f365ea46ae76d4a7fbf59ef072
Davide Italiano <davide@freebsd.org>
2017-04-05 15:05:05 +0000

    [llvm-ar] Remove unneeded std::, NFCI.
    This makes it more consistent with other exit() calls in llvm-ar
    (and the tools in general).
    
    llvm-svn: 299549

--- llvm/tools/llvm-ar/llvm-ar.cpp
+++ llvm/tools/llvm-ar/llvm-ar.cpp
@@ -168,7 +168,7 @@ LLVM_ATTRIBUTE_NORETURN static void
 show_help(const std::string &msg) {
   errs() << ToolName << ": " << msg << "\n\n";
   cl::PrintHelpMessage();
-  std::exit(1);
+  exit(1);
 }
 
 // Extract the member filename from the command line for the [relpos] argument
@@ -465,7 +465,7 @@ static void performReadOperation(ArchiveOperation Operation,
     return;
   for (StringRef Name : Members)
     errs() << Name << " was not found\n";
-  std::exit(1);
+  exit(1);
 }
 
 static void addMember(std::vector<NewArchiveMember> &Members,
