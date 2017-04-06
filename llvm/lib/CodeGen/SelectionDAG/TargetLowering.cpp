6b15606051fbda19d31f43927d6863469f089e47
Craig Topper <craig.topper@gmail.com>
2017-04-06 04:03:34 +0000

    Revert accidental commit of r299618
    llvm-svn: 299621

--- llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
@@ -775,9 +775,6 @@ bool TargetLowering::SimplifyDemandedBits(SDValue Op,
       // TODO: Should we check for other forms of sign-bit comparisons?
       // Examples: X <= -1, X >= 0
     }
-    if (getBooleanContents(Op.getOperand(0).getValueType()) ==
-            BooleanContent::ZeroOrOneBooleanContent && BitWidth > 1)
-      KnownZero.setBitsFrom(1);
     break;
   }
   case ISD::SHL:
