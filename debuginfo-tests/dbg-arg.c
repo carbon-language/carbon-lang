469e119a6fd10e2fed2bde40a2b2b536b8347ffa
Adrian Prantl <aprantl@apple.com>
2017-04-06 17:59:50 +0000

    Add DEBUGGER and CHECKs back to dbg-arg.c
    When this testcase was migrated from IR to source the DEBUGGER
    commands were not migrated together with the rest of the testcase. It
    was also compiling without debug info.
    
    Make the testcase slightly less useless by adding them back in :-)
    
    llvm-svn: 299673

--- debuginfo-tests/dbg-arg.c
+++ debuginfo-tests/dbg-arg.c
@@ -1,7 +1,12 @@
 // This test case checks debug info during register moves for an argument.
-// RUN: %clang %target_itanium_abi_host_triple -arch x86_64 -mllvm -fast-isel=false  %s -c -o %t.o
+// RUN: %clang %target_itanium_abi_host_triple -arch x86_64 -mllvm -fast-isel=false  %s -c -o %t.o -g
 // RUN: %clang %target_itanium_abi_host_triple -arch x86_64 %t.o -o %t.out
 // RUN: %test_debuginfo %s %t.out
+//
+// DEBUGGER: break 26
+// DEBUGGER: r
+// DEBUGGER: print mutex
+// CHECK:  ={{.*}} 0x0
 //
 // Radar 8412415
 
