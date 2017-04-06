e228f68811ffe816faaa892b27dafb788393aba8
Gabor Horvath <xazax.hun@gmail.com>
2017-04-06 12:49:35 +0000

    Attempt to fix build bots after r299638.
    llvm-svn: 299645

--- clang-tools-extra/test/clang-tidy/misc-forwarding-reference-overload.cpp
+++ clang-tools-extra/test/clang-tidy/misc-forwarding-reference-overload.cpp
@@ -1,8 +1,8 @@
-// RUN: %check_clang_tidy %s misc-forwarding-reference-overload %t
+// RUN: %check_clang_tidy %s misc-forwarding-reference-overload %t -- -- -std=c++14
 
 namespace std {
 template <bool B, class T = void>
-struct enable_if {};
+struct enable_if { typedef T type; };
 
 template <class T>
 struct enable_if<true, T> { typedef T type; };
