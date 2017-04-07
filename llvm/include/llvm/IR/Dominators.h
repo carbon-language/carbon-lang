40bf3a4bfd3c06fd97a5b764b967b8aee0ed5db2
Davide Italiano <davide@freebsd.org>
2017-04-07 17:34:37 +0000

    [Dominators] Simplify a member function. NFCI.
    llvm-svn: 299789

--- llvm/include/llvm/IR/Dominators.h
+++ llvm/include/llvm/IR/Dominators.h
@@ -129,14 +129,8 @@ public:
   inline bool compare(const DominatorTree &Other) const {
     const DomTreeNode *R = getRootNode();
     const DomTreeNode *OtherR = Other.getRootNode();
-
-    if (!R || !OtherR || R->getBlock() != OtherR->getBlock())
-      return true;
-
-    if (Base::compare(Other))
-      return true;
-
-    return false;
+    return !R || !OtherR || R->getBlock() != OtherR->getBlock() ||
+           Base::compare(Other);
   }
 
   // Ensure base-class overloads are visible.
