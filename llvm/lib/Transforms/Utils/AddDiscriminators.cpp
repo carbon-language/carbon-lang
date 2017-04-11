8e26936bfd722a74e5b1fcc8506c0ef9f93b2f4f
Andrea Di Biagio <Andrea_DiBiagio@sn.scee.net>
2017-04-11 19:07:30 +0000

    [AddDiscriminators] Assign discriminators to MemIntrinsic calls.
    Before this patch, pass AddDiscriminators always avoided to assign
    discriminators to intrinsic calls. This was done mainly for two reasons:
     1) We wanted to minimize the number of based discriminators used.
     2) We wanted to avoid non-deterministic discriminator assignment for
        different debug levels.
    
    Unfortunately, that approach was problematic for MemIntrinsic calls.
    MemIntrinsic calls can be split by SROA into loads and stores, and each new
    load/store instruction would obtain the debug location from the original
    intrinsic call.
    If we don't assign a discriminator to MemIntrinsic calls, then we cannot
    correctly set the discriminator for the newly created loads and stores.
    This may have a negative impact on the basic block weight computation
    performed by the SampleLoader.
    
    This patch fixes the issue by letting MemIntrinsic calls have a discriminator.
    
    Differential Revision: https://reviews.llvm.org/D31900
    
    llvm-svn: 299972

--- llvm/lib/Transforms/Utils/AddDiscriminators.cpp
+++ llvm/lib/Transforms/Utils/AddDiscriminators.cpp
@@ -102,6 +102,10 @@ FunctionPass *llvm::createAddDiscriminatorsPass() {
   return new AddDiscriminatorsLegacyPass();
 }
 
+static bool shouldHaveDiscriminator(const Instruction *I) {
+  return !isa<IntrinsicInst>(I) || isa<MemIntrinsic>(I);
+}
+
 /// \brief Assign DWARF discriminators.
 ///
 /// To assign discriminators, we examine the boundaries of every
@@ -176,7 +180,13 @@ static bool addDiscriminators(Function &F) {
   // discriminator for this instruction.
   for (BasicBlock &B : F) {
     for (auto &I : B.getInstList()) {
-      if (isa<IntrinsicInst>(&I))
+      // Not all intrinsic calls should have a discriminator.
+      // We want to avoid a non-deterministic assignment of discriminators at
+      // different debug levels. We still allow discriminators on memory
+      // intrinsic calls because those can be early expanded by SROA into
+      // pairs of loads and stores, and the expanded load/store instructions
+      // should have a valid discriminator.
+      if (!shouldHaveDiscriminator(&I))
         continue;
       const DILocation *DIL = I.getDebugLoc();
       if (!DIL)
@@ -207,6 +217,10 @@ static bool addDiscriminators(Function &F) {
     LocationSet CallLocations;
     for (auto &I : B.getInstList()) {
       CallInst *Current = dyn_cast<CallInst>(&I);
+      // We bypass intrinsic calls for the following two reasons:
+      //  1) We want to avoid a non-deterministic assigment of
+      //     discriminators.
+      //  2) We want to minimize the number of base discriminators used.
       if (!Current || isa<IntrinsicInst>(&I))
         continue;
 
