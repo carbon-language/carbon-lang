e544379b30cc103d5cb8f406516f848a0004ca31
Ed Maste <emaste@freebsd.org>
2017-04-12 13:51:00 +0000

    Fix detection of backtrace() availability on FreeBSD
    On FreeBSD backtrace is not part of libc and depends on libexecinfo
    being available. Instead of using manual checks we can use the builtin
    CMake module FindBacktrace.cmake to detect availability of backtrace()
    in a portable way.
    
    Patch By:	Alex Richardson
    Differential Revision:	https://reviews.llvm.org/D27143
    
    llvm-svn: 300062

--- llvm/include/llvm/Config/config.h.cmake
+++ llvm/include/llvm/Config/config.h.cmake
@@ -16,6 +16,8 @@
 /* Define to 1 if you have the `backtrace' function. */
 #cmakedefine HAVE_BACKTRACE ${HAVE_BACKTRACE}
 
+#define BACKTRACE_HEADER <${BACKTRACE_HEADER}>
+
 /* Define to 1 if you have the <CrashReporterClient.h> header file. */
 #cmakedefine HAVE_CRASHREPORTERCLIENT_H
 
