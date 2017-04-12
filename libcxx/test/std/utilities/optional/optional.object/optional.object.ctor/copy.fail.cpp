308624127fd6cc36558b6eee4d4ffa4e215a074e
Marshall Clow <mclow.lists@gmail.com>
2017-04-12 00:07:29 +0000

    Add some FAIL constexpr tests for optional's copy/move ctors.
    llvm-svn: 300009

---
+++ libcxx/test/std/utilities/optional/optional.object/optional.object.ctor/copy.fail.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+// <optional>
+
+// constexpr optional(const optional<T>& rhs);
+//   If is_trivially_copy_constructible_v<T> is true, 
+//    this constructor shall be a constexpr constructor.
+
+#include <optional>
+#include <type_traits>
+#include <cassert>
+
+#include "test_macros.h"
+
+struct S {
+	constexpr S()   : v_(0) {}
+	S(int v)        : v_(v) {}
+	S(const S &rhs) : v_(rhs.v_) {}  // make it not trivially copyable
+	int v_;
+	};
+	
+
+int main()
+{
+    static_assert (!std::is_trivially_copy_constructible_v<S>, "" );
+    constexpr std::optional<S> o1;
+    constexpr std::optional<S> o2 = o1;  // not constexpr
+}
