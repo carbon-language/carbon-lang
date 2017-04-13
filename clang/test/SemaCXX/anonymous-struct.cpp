78fe17131b7c926c7d347c5bbcb3bc7ca48a0b44
Richard Smith <richard-llvm@metafoo.co.uk>
2017-04-13 21:51:04 +0000

    Add test for anonymous struct containing an implicitly private data member.
    Patch by Jacob Young!
    
    llvm-svn: 300266

--- clang/test/SemaCXX/anonymous-struct.cpp
+++ clang/test/SemaCXX/anonymous-struct.cpp
@@ -21,6 +21,9 @@ struct E {
   };
   static struct {
   };
+  class {
+    int anon_priv_field; // expected-error {{anonymous struct cannot contain a private data member}}
+  };
 };
 
 template <class T> void foo(T);
