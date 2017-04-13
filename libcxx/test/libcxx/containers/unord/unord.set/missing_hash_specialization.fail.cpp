6e47b7ea913ef1b150a6575891b5632df30cf682
Eric Fiselier <eric@efcs.ca>
2017-04-13 01:03:41 +0000

    Update tests -verify error messages after r300140.
    llvm-svn: 300156

--- libcxx/test/libcxx/containers/unord/unord.set/missing_hash_specialization.fail.cpp
+++ libcxx/test/libcxx/containers/unord/unord.set/missing_hash_specialization.fail.cpp
@@ -53,7 +53,7 @@ int main() {
   // FIXME: It would be great to suppress the below diagnostic all together.
   //        but for now it's sufficient that it appears last. However there is
   //        currently no way to test the order diagnostics are issued.
-  // expected-error@memory:* {{call to implicitly-deleted default constructor of 'std::__1::hash<std::__1::pair<int, int> >'}}
+  // expected-error@memory:* {{call to implicitly-deleted default constructor of '__compressed_pair_elem}}
   }
   {
     using Set = std::unordered_set<int, BadHashNoCopy>;
