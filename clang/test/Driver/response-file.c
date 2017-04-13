29a0f0db9ebb58f1b6fdb2614654be4ea72134cd
Richard Smith <richard-llvm@metafoo.co.uk>
2017-04-13 00:46:50 +0000

    Fix broken test. We can't assume that 2MB of args is enough to require a response file.
    This test has apparently been broken for years, but we never noticed before
    because it's a long test and long tests approximately never get run.
    
    llvm-svn: 300151

--- clang/test/Driver/response-file.c
+++ clang/test/Driver/response-file.c
@@ -9,11 +9,12 @@
 
 // Check that clang is able to process long response files, routing a long
 // sequence of arguments to other tools by using response files as well.
-// We generate a 2MB response file to be big enough to surpass any system
-// limit.
+// We generate a 2MB response file to attempt to surpass any system limit.
+// But there's no guarantee that we actually will (the system limit could be
+// *huge*), so just check that invoking cc1 succeeds under these conditions.
+//
 // RUN: %clang -E %S/Inputs/gen-response.c | grep DTEST > %t.1.txt
 // RUN: %clang -E @%t.1.txt %s -v 2>&1 | FileCheck %s -check-prefix=LONG
-// LONG: Arguments passed via response file
 // LONG: extern int it_works;
 
 #ifdef TEST
