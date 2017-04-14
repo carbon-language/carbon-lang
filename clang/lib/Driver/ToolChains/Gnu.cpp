750fe2220e53bc6d9c8fd4d5e0ab7f59d693b14d
Erich Keane <erich.keane@intel.com>
2017-04-14 15:21:18 +0000

    Make Gentoo GNU GCC Config override whitespace tolerant
    The config-*triple* file handling isn't tolerant of 
    leading/trailing whitespace, making it not terribly 
    obvious when a single extraneous tab/space/etc will 
    cause the override to be ignored. This patch simply 
    trims the lines to ensure that it is tolerant of 
    whitespace.
    
    llvm-svn: 300328

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -2173,6 +2173,7 @@ bool Generic_GCC::GCCInstallationDetector::ScanGentooGccConfig(
     SmallVector<StringRef, 2> Lines;
     File.get()->getBuffer().split(Lines, "\n");
     for (StringRef Line : Lines) {
+      Line = Line.trim();
       // CURRENT=triple-version
       if (Line.consume_front("CURRENT=")) {
         const std::pair<StringRef, StringRef> ActiveVersion =
