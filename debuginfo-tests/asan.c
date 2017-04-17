c26059f831d315dab3b9d87ba86cb3532302aa0b
Adrian Prantl <aprantl@apple.com>
2017-04-17 17:57:01 +0000

    Revert "Revert "Add a test for __block variables + asan.""
    This reapplies commit r300228.
    
    llvm-svn: 300467

--- debuginfo-tests/asan.c
+++ debuginfo-tests/asan.c
@@ -3,24 +3,38 @@
 // REQUIRES: not_asan
 //           Zorg configures the ASAN stage2 bots to not build the asan
 //           compiler-rt. Only run this test on non-asanified configurations.
-//
-// DEBUGGER: break 24
-// DEBUGGER: r
-// DEBUGGER: p s
-//
-// CHECK: a = ([0] = 0, [1] = 1, [2] = 2, [3] = 3, [4] = 4, [5] = 5, [6] = 6, [7] = 7)
-
+void b();
 struct S {
   int a[8];
 };
 
 int f(struct S s, unsigned i) {
+  // DEBUGGER: break 16
+  // DEBUGGER: r
+  // DEBUGGER: p s
+  // CHECK: a = ([0] = 0, [1] = 1, [2] = 2, [3] = 3, [4] = 4, [5] = 5, [6] = 6, [7] = 7)
   return s.a[i];
 }
 
 int main(int argc, const char **argv) {
   struct S s = {{0, 1, 2, 3, 4, 5, 6, 7}};
-  if (f(s, 4) == 4)
-    return f(s, 0);
+  if (f(s, 4) == 4) {
+    // DEBUGGER: break 26
+    // DEBUGGER: c
+    // DEBUGGER: p s
+    // CHECK: a = ([0] = 0, [1] = 1, [2] = 2, [3] = 3, [4] = 4, [5] = 5, [6] = 6, [7] = 7)
+    b();
+  }
   return 0;
+}
+
+void c() {}
+
+void b() {
+  // DEBUGGER: break 39
+  // DEBUGGER: c
+  // DEBUGGER: p x
+  // CHECK: 42
+  __block int x = 42;
+  c();
 }
