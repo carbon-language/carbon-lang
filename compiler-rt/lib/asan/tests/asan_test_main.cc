87276d68cd85368e8b5ec61e7b62bec72ec0c7b3
Alex Shlyapnikov <alekseys@google.com>
2017-04-19 20:39:09 +0000

    Turn symbolization on for ASan unit test.
    Summary:
    On PowerPC and ARM (possibly, need to verify), couple tests involving
    pthread_exit fail due to leaks detected by LSan. pthread_exit tries
    to perform unwinding that leads to dlopen'ing libgcc_s.so. dlopen
    mallocs "libgcc_s.so" string which confuses LSan, it fails to
    realize that this allocation happens in dynamic linker and should
    be ignored.
    Symbolized leak report is required to define a suppression for this
    known problem.
    
    Reviewers: eugenis
    
    Subscribers: aemerson, rengolin, kubamracek, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D32194
    
    Turn symbolization on for PPC and Thumb only to do not slow down other platforms.
    
    llvm-svn: 300748

--- compiler-rt/lib/asan/tests/asan_test_main.cc
+++ compiler-rt/lib/asan/tests/asan_test_main.cc
@@ -13,15 +13,26 @@
 #include "asan_test_utils.h"
 #include "sanitizer_common/sanitizer_platform.h"
 
-// Default ASAN_OPTIONS for the unit tests. Let's turn symbolication off to
-// speed up testing (unit tests don't use it anyway).
+// Default ASAN_OPTIONS for the unit tests.
 extern "C" const char* __asan_default_options() {
 #if SANITIZER_MAC
   // On Darwin, we default to `abort_on_error=1`, which would make tests run
-  // much slower. Let's override this and run lit tests with 'abort_on_error=0'.
-  // Also, make sure we do not overwhelm the syslog while testing.
+  // much slower. Let's override this and run lit tests with 'abort_on_error=0'
+  // and make sure we do not overwhelm the syslog while testing. Also, let's
+  // turn symbolization off to speed up testing, especially when not running
+  // with llvm-symbolizer but with atos.
   return "symbolize=false:abort_on_error=0:log_to_syslog=0";
+#elif SANITIZER_PPC || defined(__thumb__)
+  // On PowerPC and ARM Thumb, a couple tests involving pthread_exit fail due to
+  // leaks detected by LSan. pthread_exit tries to perform unwinding that leads
+  // to dlopen'ing libgcc_s.so. dlopen mallocs "libgcc_s.so" string which
+  // confuses LSan, it fails to realize that this allocation happens in dynamic
+  // linker and should be ignored.  Symbolized leak report is required to define
+  // a suppression for this known problem.
+  return "";
 #else
+  // Let's turn symbolization off to speed up testing (more than 3 times speedup
+  // observed).
   return "symbolize=false";
 #endif
 }
