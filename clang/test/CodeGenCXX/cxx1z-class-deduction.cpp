2b0d6134c768e36d9c1d3c2a8485e8e245b3b6f4
Richard Smith <richard-llvm@metafoo.co.uk>
2017-04-20 01:02:29 +0000

    Add a triple to codegen test.
    llvm-svn: 300803

--- clang/test/CodeGenCXX/cxx1z-class-deduction.cpp
+++ clang/test/CodeGenCXX/cxx1z-class-deduction.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++1z %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -std=c++1z %s -triple %itanium_abi_triple -emit-llvm -o - | FileCheck %s
 
 template<typename T> struct A {
   A(T = 0);
