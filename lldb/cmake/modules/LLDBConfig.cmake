37283039cda37ef5cedfe66d4ea6fa9f9f6051c3
Kamil Rytarowski <n54@gmx.com>
2017-04-20 16:35:36 +0000

    Resurrect LLDB Standalone build on NetBSD
    Include CheckIncludeFile for check_include_file() in CMake.
    
    Detect <sys/event.h> in CMake.
    This header is available on NetBSD.
    
    llvm-svn: 300853

--- lldb/cmake/modules/LLDBConfig.cmake
+++ lldb/cmake/modules/LLDBConfig.cmake
@@ -433,7 +433,9 @@ endif()
 
 find_package(Backtrace)
 
+include(CheckIncludeFile)
 check_include_file(termios.h HAVE_TERMIOS_H)
+check_include_file(sys/event.h HAVE_SYS_EVENT_H)
 
 # These checks exist in LLVM's configuration, so I want to match the LLVM names
 # so that the check isn't duplicated, but we translate them into the LLDB names
