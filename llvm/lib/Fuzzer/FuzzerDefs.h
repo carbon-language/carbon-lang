9eb170fede34b4cf3ee43998e4e0f04240b72b3b
Kuba Mracek <mracek@apple.com>
2017-04-21 16:57:37 +0000

    [libFuzzer] Check for target(popcnt) capability before usage
    Older compilers (e.g. LLVM 3.4) do not support the attribute target("popcnt").
    In order to support those, this diff check the attribute support using the preprocessor.
    
    Patch by George Karpenkov.
    
    Differential Revision: https://reviews.llvm.org/D32311
    
    llvm-svn: 300999

--- llvm/lib/Fuzzer/FuzzerDefs.h
+++ llvm/lib/Fuzzer/FuzzerDefs.h
@@ -36,12 +36,20 @@
 #error "Support for your platform has not been implemented"
 #endif
 
+#ifndef __has_attribute
+#  define __has_attribute(x) 0
+#endif
+
 #define LIBFUZZER_POSIX LIBFUZZER_APPLE || LIBFUZZER_LINUX
 
 #ifdef __x86_64
-#define ATTRIBUTE_TARGET_POPCNT __attribute__((target("popcnt")))
+#  if __has_attribute(target)
+#    define ATTRIBUTE_TARGET_POPCNT __attribute__((target("popcnt")))
+#  else
+#    define ATTRIBUTE_TARGET_POPCNT
+#  endif
 #else
-#define ATTRIBUTE_TARGET_POPCNT
+#  define ATTRIBUTE_TARGET_POPCNT
 #endif
 
 
