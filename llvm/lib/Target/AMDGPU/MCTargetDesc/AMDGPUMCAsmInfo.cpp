df6539f44b8b6a9bf93e5305e0bfbab9a707a443
Matt Arsenault <Matthew.Arsenault@amd.com>
2017-04-24 19:40:51 +0000

    AMDGPU: Set StackGrowsUp in MCAsmInfo
    Not sure what this does though.
    
    llvm-svn: 301229

--- llvm/lib/Target/AMDGPU/MCTargetDesc/AMDGPUMCAsmInfo.cpp
+++ llvm/lib/Target/AMDGPU/MCTargetDesc/AMDGPUMCAsmInfo.cpp
@@ -15,6 +15,7 @@ using namespace llvm;
 
 AMDGPUMCAsmInfo::AMDGPUMCAsmInfo(const Triple &TT) : MCAsmInfoELF() {
   CodePointerSize = (TT.getArch() == Triple::amdgcn) ? 8 : 4;
+  StackGrowsUp = true;
   HasSingleParameterDotFile = false;
   //===------------------------------------------------------------------===//
   MinInstAlignment = 4;
