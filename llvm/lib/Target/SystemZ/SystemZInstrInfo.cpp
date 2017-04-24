1e8648577c6a47fbd136801ec686e30c3819a8d3
Jonas Paulsson <paulsson@linux.vnet.ibm.com>
2017-04-24 12:40:28 +0000

    [SystemZ]  Update kill-flag in splitMove().
    EarlierMI needs to clear the kill flag on the first operand in case of a store.
    
    Review: Ulrich Weigand
    llvm-svn: 301177

--- llvm/lib/Target/SystemZ/SystemZInstrInfo.cpp
+++ llvm/lib/Target/SystemZ/SystemZInstrInfo.cpp
@@ -104,8 +104,9 @@ void SystemZInstrInfo::splitMove(MachineBasicBlock::iterator MI,
   MachineOperand &LowOffsetOp = MI->getOperand(2);
   LowOffsetOp.setImm(LowOffsetOp.getImm() + 8);
 
-  // Clear the kill flags for the base and index registers in the first
-  // instruction.
+  // Clear the kill flags on the registers in the first instruction.
+  if (EarlierMI->getOperand(0).isReg() && EarlierMI->getOperand(0).isUse())
+    EarlierMI->getOperand(0).setIsKill(false);
   EarlierMI->getOperand(1).setIsKill(false);
   EarlierMI->getOperand(3).setIsKill(false);
 
