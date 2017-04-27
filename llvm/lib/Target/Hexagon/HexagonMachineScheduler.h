14f10e03e05b1498bf4cb24b90c858687a9177e4
Krzysztof Parzyszek <kparzysz@codeaurora.org>
2017-04-27 14:38:21 +0000

    Fix typo and place comment close to its target
    Patch by Wei-Ren Chen.
    
    Differential Revision: https://reviews.llvm.org/D32594
    
    llvm-svn: 301546

--- llvm/lib/Target/Hexagon/HexagonMachineScheduler.h
+++ llvm/lib/Target/Hexagon/HexagonMachineScheduler.h
@@ -32,14 +32,10 @@
 using namespace llvm;
 
 namespace llvm {
-//===----------------------------------------------------------------------===//
-// ConvergingVLIWScheduler - Implementation of the standard
-// MachineSchedStrategy.
-//===----------------------------------------------------------------------===//
 
 class VLIWResourceModel {
   /// ResourcesModel - Represents VLIW state.
-  /// Not limited to VLIW targets per say, but assumes
+  /// Not limited to VLIW targets per se, but assumes
   /// definition of DFA by a target.
   DFAPacketizer *ResourcesModel;
 
@@ -109,6 +105,11 @@ public:
   /// time to do some work.
   void schedule() override;
 };
+
+//===----------------------------------------------------------------------===//
+// ConvergingVLIWScheduler - Implementation of the standard
+// MachineSchedStrategy.
+//===----------------------------------------------------------------------===//
 
 /// ConvergingVLIWScheduler shrinks the unscheduled zone using heuristics
 /// to balance the schedule.
