499de32ad03af6877ca2ee0780530ee4721b4062
Alex Lorenz <arphaman@gmail.com>
2017-04-28 14:45:25 +0000

    Use the -Wunknown-warning-option group for the "unknown warning group" diagnostic in #pragma diagnostic
    This matches the warning group that's specified for the unknown warning options
    that are passed-in as command line arguments.
    
    rdar://29526025
    
    llvm-svn: 301647

--- clang/test/Preprocessor/pragma_diagnostic.c
+++ clang/test/Preprocessor/pragma_diagnostic.c
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -Wno-undef %s
+// RUN: %clang_cc1 -fsyntax-only -verify -Wno-undef -Wno-unknown-warning-option -DAVOID_UNKNOWN_WARNING %s
 // rdar://2362963
 
 #if FOO    // ok.
@@ -28,8 +29,10 @@
 #pragma GCC diagnostic error "-Wundef" 42  // expected-warning {{unexpected token in pragma diagnostic}}
 #pragma GCC diagnostic error "invalid-name"  // expected-warning {{pragma diagnostic expected option name (e.g. "-Wundef")}}
 
-#pragma GCC diagnostic error "-Winvalid-name"  // expected-warning {{unknown warning group '-Winvalid-name', ignored}}
-
+#pragma GCC diagnostic error "-Winvalid-name"
+#ifndef AVOID_UNKNOWN_WARNING
+// expected-warning@-2 {{unknown warning group '-Winvalid-name', ignored}}
+#endif
 
 // Testing pragma clang diagnostic with -Weverything
 void ppo(){} // First test that we do not diagnose on this.
