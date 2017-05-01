e3dd6443268fb6493972fc6fff6d4a890bc57edb
Craig Topper <craig.topper@gmail.com>
2017-05-01 21:16:44 +0000

    [APInt] Fix copy/paste mistake in comment for isNullValue. NFC
    llvm-svn: 301838

--- llvm/include/llvm/ADT/APInt.h
+++ llvm/include/llvm/ADT/APInt.h
@@ -377,9 +377,10 @@ public:
     return countPopulationSlowCase() == BitWidth;
   }
 
-  /// \brief Determine if all bits are set
+  /// \brief Determine if all bits are clear
   ///
-  /// This checks to see if the value has all bits of the APInt are set or not.
+  /// This checks to see if the value has all bits of the APInt are clear or
+  /// not.
   bool isNullValue() const { return !*this; }
 
   /// \brief Determine if this is the largest unsigned value.
