9decbfee32cd1ff8538cbb97e1ec4d7ee7cb577e
Rui Ueyama <ruiu@google.com>
2017-05-02 22:42:24 +0000

    Remove a dead function declaration.
    llvm-svn: 301982

--- lld/ELF/Driver.h
+++ lld/ELF/Driver.h
@@ -64,8 +64,6 @@ enum {
 };
 
 void printHelp(const char *Argv0);
-std::vector<uint8_t> parseHexstring(StringRef S);
-
 std::string createResponseFile(const llvm::opt::InputArgList &Args);
 
 llvm::Optional<std::string> findFromSearchPaths(StringRef Path);
