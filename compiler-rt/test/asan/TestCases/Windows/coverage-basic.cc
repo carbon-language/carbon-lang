eceba0d2e30446ab270f905f33cdea75d0f32356
Reid Kleckner <rnk@google.com>
2017-05-03 16:11:01 +0000

    Revert my bad winasan coverage test fix and apply one that actually works
    trace-pc doesn't work, but trace-pc-guard does. *shrug*
    
    llvm-svn: 302045

--- compiler-rt/test/asan/TestCases/Windows/coverage-basic.cc
+++ compiler-rt/test/asan/TestCases/Windows/coverage-basic.cc
@@ -1,6 +1,6 @@
 // RUN: rm -rf %T/coverage-basic
 // RUN: mkdir %T/coverage-basic && cd %T/coverage-basic
-// RUN: %clangxx_asan -fsanitize-coverage=func,trace-pc %s -o test.exe
+// RUN: %clangxx_asan -fsanitize-coverage=func %s -o test.exe
 // RUN: %env_asan_opts=coverage=1 %run ./test.exe
 //
 // RUN: %sancov print *.sancov | FileCheck %s
