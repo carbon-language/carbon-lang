5191e80b8731a7472423876628373a36b9003909
Alex Lorenz <arphaman@gmail.com>
2017-05-03 16:22:16 +0000

    Revert r302037
    The commit caused the following two buildbot failures:
    
        Clang :: Misc/error-limit-multiple-notes.cpp
        Clang :: Misc/error-limit.c
    
    llvm-svn: 302046

--- clang/lib/Basic/Diagnostic.cpp
+++ clang/lib/Basic/Diagnostic.cpp
@@ -146,9 +146,8 @@ void DiagnosticsEngine::SetDelayedDiagnostic(unsigned DiagID, StringRef Arg1,
 }
 
 void DiagnosticsEngine::ReportDelayed() {
-  unsigned ID = DelayedDiagID;
+  Report(DelayedDiagID) << DelayedDiagArg1 << DelayedDiagArg2;
   DelayedDiagID = 0;
-  Report(ID) << DelayedDiagArg1 << DelayedDiagArg2;
   DelayedDiagArg1.clear();
   DelayedDiagArg2.clear();
 }
