90a8fc8cb837083fe379a4188bd9775c90af93b8
Dean Michael Berris <dberris@google.com>
2017-05-08 00:38:13 +0000

    [XRay][compiler-rt] XFAIL on ppc
    Follow-up on D32846.
    
    llvm-svn: 302392

--- compiler-rt/test/xray/TestCases/Linux/func-id-utils.cc
+++ compiler-rt/test/xray/TestCases/Linux/func-id-utils.cc
@@ -3,6 +3,8 @@
 //
 // RUN: %clangxx_xray -std=c++11 %s -o %t
 // RUN: XRAY_OPTIONS="patch_premain=false xray_naive_log=false" %run %t
+// FIXME: When we know why this fails in ppc, un-xfail it.
+// XFAIL: powerpc64le
 
 #include "xray/xray_interface.h"
 #include <algorithm>
