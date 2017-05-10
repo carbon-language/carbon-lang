6957d35a9b6f3a15383acf836007344ac0a961ba
Billy Robert O'Neal III <bion@microsoft.com>
2017-05-10 19:43:31 +0000

    Update is_trivially_copyable tests with CWG 2094
    Clang 5.0 implements these changes here: https://github.com/llvm-mirror/clang/commit/87cd035326a39523eeb1b295ad36cff337141ef9
    MSVC++ will implement these changes in the first toolset update to 2017.
    
    Differential Revision: https://reviews.llvm.org/D33021
    
    llvm-svn: 302710

--- libcxx/test/std/utilities/meta/meta.unary/meta.unary.prop/is_trivially_copyable.pass.cpp
+++ libcxx/test/std/utilities/meta/meta.unary/meta.unary.prop/is_trivially_copyable.pass.cpp
@@ -22,13 +22,13 @@ void test_is_trivially_copyable()
 {
     static_assert( std::is_trivially_copyable<T>::value, "");
     static_assert( std::is_trivially_copyable<const T>::value, "");
-    static_assert(!std::is_trivially_copyable<volatile T>::value, "");
-    static_assert(!std::is_trivially_copyable<const volatile T>::value, "");
+    static_assert( std::is_trivially_copyable<volatile T>::value, "");
+    static_assert( std::is_trivially_copyable<const volatile T>::value, "");
 #if TEST_STD_VER > 14
     static_assert( std::is_trivially_copyable_v<T>, "");
     static_assert( std::is_trivially_copyable_v<const T>, "");
-    static_assert(!std::is_trivially_copyable_v<volatile T>, "");
-    static_assert(!std::is_trivially_copyable_v<const volatile T>, "");
+    static_assert( std::is_trivially_copyable_v<volatile T>, "");
+    static_assert( std::is_trivially_copyable_v<const volatile T>, "");
 #endif
 }
 
