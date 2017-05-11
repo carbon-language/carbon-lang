33a97ec4ed4e5a2e49823ce1a690e26ace7b72da
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2017-05-11 17:16:55 +0000

    [AMDGPU] Fix incorrect register pressure calculation
    Earlier fix D32572 introduced a bug where live-ins were calculated
    for basic block instead of scheduling region. This change fixes it.
    
    Differential Revision: https://reviews.llvm.org/D33086
    
    llvm-svn: 302812

--- llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
+++ llvm/lib/Target/AMDGPU/GCNSchedStrategy.cpp
@@ -422,9 +422,10 @@ void GCNScheduleDAGMILive::discoverLiveIns() {
   unsigned SGPRs = 0;
   unsigned VGPRs = 0;
 
-  auto &MI = *begin()->getParent()->getFirstNonDebugInstr();
+  auto I = begin();
+  I = skipDebugInstructionsForward(I, I->getParent()->end());
   const SIRegisterInfo *SRI = static_cast<const SIRegisterInfo*>(TRI);
-  SlotIndex SI = LIS->getInstructionIndex(MI).getBaseIndex();
+  SlotIndex SI = LIS->getInstructionIndex(*I).getBaseIndex();
   assert (SI.isValid());
 
   DEBUG(dbgs() << "Region live-ins:");
