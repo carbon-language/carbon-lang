0f01fb39e3fe3d8e99df1dd185e75ad584b777b3
Sean Callanan <scallanan@apple.com>
2017-05-11 23:38:21 +0000

    xfail TestClassTemplateParameterPack on gcc to mollify lldb-x86_64-ubuntu-14.04-cmake.
    llvm-svn: 302850

--- lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
@@ -4,4 +4,6 @@ from lldbsuite.test import decorators
 lldbinline.MakeInlineTest(
     __file__, globals(), [
         decorators.expectedFailureAll(
-            oslist=["windows"], bugnumber="llvm.org/pr24764")])
+            oslist=["windows"], bugnumber="llvm.org/pr24764"),
+        decorators.expectedFailureAll(
+            compiler="gcc")])
