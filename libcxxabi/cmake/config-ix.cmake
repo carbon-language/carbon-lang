f57d5d08ba77b4386394573989bb79fb56b18a31
Eric Fiselier <eric@efcs.ca>
2017-05-11 02:00:25 +0000

    Apply libc++ MinGW CMake fix to libc++abi
    llvm-svn: 302754

--- libcxxabi/cmake/config-ix.cmake
+++ libcxxabi/cmake/config-ix.cmake
@@ -27,6 +27,11 @@ if (LIBCXXABI_HAS_NODEFAULTLIBS_FLAG)
   elseif (LIBCXXABI_HAS_GCC_S_LIB)
     list(APPEND CMAKE_REQUIRED_LIBRARIES gcc_s)
   endif ()
+  if (MINGW)
+    # Mingw64 requires quite a few "C" runtime libraries in order for basic
+    # programs to link successfully with -nodefaultlibs.
+    list(APPEND CMAKE_REQUIRED_LIBRARIES mingw32 gcc gcc_eh mingwex msvcrt gcc)
+  endif()
   if (CMAKE_C_FLAGS MATCHES -fsanitize OR CMAKE_CXX_FLAGS MATCHES -fsanitize)
     set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} -fno-sanitize=all")
   endif ()
