9ce59db426e92cb65cbf97e56f22bb2bfb8a0a76
Pierre Gousseau <pierregousseau14@gmail.com>
2017-05-11 16:26:50 +0000

    [asan] Test 'strndup_oob_test.cc' added in r302781 fails on the clang-cmake-thumbv7-a15-full-sh bot. Marking as unsupported on armv7l-unknown-linux-gnueabihf, same as strdup_oob_test.cc
    llvm-svn: 302807

--- compiler-rt/test/asan/TestCases/Posix/strndup_oob_test.cc
+++ compiler-rt/test/asan/TestCases/Posix/strndup_oob_test.cc
@@ -6,7 +6,8 @@
 // When built as C on Linux, strndup is transformed to __strndup.
 // RUN: %clangxx_asan -O3 -xc %s -o %t && not %run %t 2>&1 | FileCheck %s
 
-// UNSUPPORTED: win32,s390
+// Unwind problem on arm: "main" is missing from the allocation stack trace.
+// UNSUPPORTED: win32,s390,armv7l-unknown-linux-gnueabihf
 
 #include <string.h>
 
