627ad0fec3d0050331e99fa7a67f0dfeb0ade108
Max Kazantsev <max.kazantsev@azul.com>
2017-05-18 08:26:41 +0000

    [SCEV][NFC] Remove duplication of isLoopInvariant code
    Replace two places that duplicate the code of isLoopInvariant method with
    the invocation of this method.
    
    Differential Revision: https://reviews.llvm.org/D33313
    
    llvm-svn: 303336

--- llvm/lib/Analysis/ScalarEvolution.cpp
+++ llvm/lib/Analysis/ScalarEvolution.cpp
@@ -3885,7 +3885,7 @@ public:
       : SCEVRewriteVisitor(SE), L(L), Valid(true) {}
 
   const SCEV *visitUnknown(const SCEVUnknown *Expr) {
-    if (!(SE.getLoopDisposition(Expr, L) == ScalarEvolution::LoopInvariant))
+    if (!SE.isLoopInvariant(Expr, L))
       Valid = false;
     return Expr;
   }
@@ -3919,7 +3919,7 @@ public:
 
   const SCEV *visitUnknown(const SCEVUnknown *Expr) {
     // Only allow AddRecExprs for this loop.
-    if (!(SE.getLoopDisposition(Expr, L) == ScalarEvolution::LoopInvariant))
+    if (!SE.isLoopInvariant(Expr, L))
       Valid = false;
     return Expr;
   }
