1e39e5e96417808bb6ce51d2237e4f62c0f8525b
Martell Malone <martellmalone@gmail.com>
2017-05-20 19:56:44 +0000

    COFF: migrate def parser from LLD to LLVM [2/2]
    This is split up into two commits.
    This commit removes the DEF parser from LLD
    See the previous commit for the creation in LLVM.
    
    Reviewers: ruiu
    
    Differential Revision: https://reviews.llvm.org/D32689
    
    llvm-svn: 303491

--- lld/COFF/Driver.h
+++ lld/COFF/Driver.h
@@ -124,9 +124,6 @@ private:
   std::vector<MemoryBufferRef> Resources;
 };
 
-void parseModuleDefs(MemoryBufferRef MB);
-void writeImportLibrary();
-
 // Functions below this line are defined in DriverUtils.cpp.
 
 void printHelp(const char *Argv0);
