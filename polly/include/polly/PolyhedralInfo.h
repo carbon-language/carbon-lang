7bccbe780e8cff390b2a8303ced884117ba20865
Siddharth Bhat <siddu.druid@gmail.com>
2017-05-23 11:41:52 +0000

    [NFC] [PolyhedralInfo] fix typo
    "an" -> "and"
    
    llvm-svn: 303631

--- polly/include/polly/PolyhedralInfo.h
+++ polly/include/polly/PolyhedralInfo.h
@@ -10,7 +10,8 @@
 /// This file contains the declaration of the PolyhedralInfo class, which will
 /// provide an interface to expose polyhedral analysis information of Polly.
 ///
-/// This is work in progress. We will add more API's as an when deemed required.
+/// This is work in progress. We will add more API's as and when deemed
+/// required.
 //===----------------------------------------------------------------------===///
 
 #ifndef POLLY_POLYHEDRAL_INFO_H
