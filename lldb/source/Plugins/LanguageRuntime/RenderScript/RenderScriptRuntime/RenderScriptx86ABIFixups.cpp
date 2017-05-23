e3bb52bb2ef02f6aa8fc44cdde11e8fd260cc701
Stephane Sezer <sas@cd80.net>
2017-05-23 19:22:31 +0000

    Fix bad change in RenderScriptx86ABIFixups.cpp, forgot to change everything necessary
    Summary: I didn't change all instances of i to I in this loop. I am a bad person and should feel bad. :(
    
    Reviewers: sas
    
    Differential Revision: https://reviews.llvm.org/D33456
    
    Change by Alex Langford <apl@fb.com>
    
    llvm-svn: 303677

--- lldb/source/Plugins/LanguageRuntime/RenderScript/RenderScriptRuntime/RenderScriptx86ABIFixups.cpp
+++ lldb/source/Plugins/LanguageRuntime/RenderScript/RenderScriptRuntime/RenderScriptx86ABIFixups.cpp
@@ -257,9 +257,9 @@ bool fixupRSAllocationStructByValCalls(llvm::Module &module) {
     for (unsigned I = call_attribs.index_begin(); I != call_attribs.index_end();
          I++) {
       // if this argument is passed by val
-      if (call_attribs.hasAttribute(i, llvm::Attribute::ByVal)) {
+      if (call_attribs.hasAttribute(I, llvm::Attribute::ByVal)) {
         // strip away the byval attribute
-        call_inst->removeAttribute(i, llvm::Attribute::ByVal);
+        call_inst->removeAttribute(I, llvm::Attribute::ByVal);
         changed = true;
       }
     }
