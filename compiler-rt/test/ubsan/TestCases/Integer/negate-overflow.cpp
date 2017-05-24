0eec1f0b960fd8b6a671ae05847ff07ee799e289
Hans Wennborg <hans@hanshq.net>
2017-05-24 21:52:40 +0000

    Fix negate-overflow.cpp test on Windows after r303440
    lit would interpret the exit code as failuire.
    
    llvm-svn: 303809

--- compiler-rt/test/ubsan/TestCases/Integer/negate-overflow.cpp
+++ compiler-rt/test/ubsan/TestCases/Integer/negate-overflow.cpp
@@ -6,7 +6,9 @@ int main() {
   // CHECKU: negate-overflow.cpp:[[@LINE+2]]:3: runtime error: negation of 2147483648 cannot be represented in type 'unsigned int'
   // CHECKU-NOT: cast to an unsigned
   -unsigned(-0x7fffffff - 1); // ok
-  // CHECKS: negate-overflow.cpp:[[@LINE+2]]:10: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
+  // CHECKS: negate-overflow.cpp:[[@LINE+2]]:3: runtime error: negation of -2147483648 cannot be represented in type 'int'; cast to an unsigned type to negate this value to itself
   // CHECKU-NOT: runtime error
-  return -(-0x7fffffff - 1);
+  -(-0x7fffffff - 1);
+
+  return 0;
 }
