4aaf69749baa844336a3660d71dbfb96c7bf9b5c
Eric Fiselier <eric@efcs.ca>
2017-05-28 20:25:41 +0000

    Remove XFAIL's for recently fixed bug
    llvm-svn: 304102

--- libcxx/test/libcxx/experimental/language.support/support.coroutines/dialect_support.sh.cpp
+++ libcxx/test/libcxx/experimental/language.support/support.coroutines/dialect_support.sh.cpp
@@ -10,10 +10,6 @@
 
 // REQUIRES: fcoroutines-ts
 
-// These configurations run the tests with '-g', which hits a bug in Clangs
-// coroutine implementation.
-// XFAIL: asan, msan, ubsan, tsan
-
 // RUN: %build -fcoroutines-ts
 // RUN: %run
 
