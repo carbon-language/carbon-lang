c34a497b407ebb65e6d8c582ba5fe1c45cc88958
Eric Fiselier <eric@efcs.ca>
2017-05-28 19:38:21 +0000

    [coroutines] Add end-to-end tests within libc++
    This patch adds end-to-end/breathing tests for coroutines
    into libc++. The tests aren't specifically to test libc++ requirements
    but instead are intented to ensure coroutines are working fine in general.
    
    Although libc++ isn't exactly the most correct place for these tests
    to live, there is one major advantage. The libc++ test suite is also
    used by MSVC and by adding the tests here it ensures they will be
    run against all currently available coroutine implementations.
    
    llvm-svn: 304101

---
+++ libcxx/test/std/experimental/language.support/support.coroutines/end.to.end/bool_await_suspend.sh.cpp
@@ -0,0 +1,67 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11
+// REQUIRES: fcoroutines-ts
+
+// RUN: %build -fcoroutines-ts
+// RUN: %run
+
+#include <experimental/coroutine>
+#include <cassert>
+
+using namespace std::experimental;
+
+struct coro_t {
+  struct promise_type {
+    coro_t get_return_object() {
+      coroutine_handle<promise_type>{};
+      return {};
+    }
+    suspend_never initial_suspend() { return {}; }
+    suspend_never final_suspend() { return {}; }
+    void return_void(){}
+    void unhandled_exception() {}
+  };
+};
+
+struct NoSuspend {
+  bool await_ready() { return false; }
+  void await_resume() {}
+  template <typename F> bool await_suspend(F) { return false; }
+};
+
+struct DoSuspend {
+  bool await_ready() { return false; }
+  void await_resume() {}
+  template <typename F> bool await_suspend(F) { return true; }
+};
+
+bool f_started, f_resumed = false;
+coro_t f() {
+  f_started = true;
+  co_await DoSuspend{};
+  f_resumed = true;
+}
+
+bool g_started, g_resumed = false;
+coro_t g() {
+  g_started = true;
+  co_await NoSuspend{};
+  g_resumed = true;
+}
+
+int main() {
+  assert(!f_started && !f_resumed && !g_started && !g_resumed);
+  f();
+  assert(f_started && !f_resumed);
+  g();
+  assert(g_started && g_resumed);
+}
