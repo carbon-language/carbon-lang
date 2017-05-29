df1832cf866f133a4b74818425de2bb531f0d725
Zachary Turner <zturner@google.com>
2017-05-29 02:19:37 +0000

    Resubmit "[X86] Adding new LLVM TableGen backend that generates the X86 backend memory folding tables."
    This was reverted due to buildbot breakages and I was not familiar
    with this code to investigate it.  But while trying to get a
    useful backtrace for the author, it turns out the fix was very
    obvious.  Resubmitting this patch as is, and will submit the
    fix in a followup so that the fix is not hidden in the larger
    CL.
    
    llvm-svn: 304122

--- llvm/utils/TableGen/TableGen.cpp
+++ llvm/utils/TableGen/TableGen.cpp
@@ -46,6 +46,7 @@ enum ActionType {
   GenAttributes,
   GenSearchableTables,
   GenGlobalISel,
+  GenX86FoldTables,
   GenX86EVEX2VEXTables,
   GenRegisterBank,
 };
@@ -97,6 +98,8 @@ namespace {
                                "Generate generic binary-searchable table"),
                     clEnumValN(GenGlobalISel, "gen-global-isel",
                                "Generate GlobalISel selector"),
+                    clEnumValN(GenX86FoldTables, "gen-x86-fold-tables",
+                               "Generate X86 fold tables"),
                     clEnumValN(GenX86EVEX2VEXTables, "gen-x86-EVEX2VEX-tables",
                                "Generate X86 EVEX to VEX compress tables"),
                     clEnumValN(GenRegisterBank, "gen-register-bank",
@@ -189,6 +192,9 @@ bool LLVMTableGenMain(raw_ostream &OS, RecordKeeper &Records) {
     break;
   case GenGlobalISel:
     EmitGlobalISel(Records, OS);
+    break;
+  case GenX86FoldTables:
+    EmitX86FoldTables(Records, OS);
     break;
   case GenRegisterBank:
     EmitRegisterBank(Records, OS);
