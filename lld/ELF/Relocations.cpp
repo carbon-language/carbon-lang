d602987271add15aac3d6d3c751a219f7b30e067
George Rimar <grimar@accesssoftek.com>
2017-06-01 07:53:58 +0000

    [ELF] - Properly handle R_386_GOTPC relocation.
    This is PR33243. R_GOTONLY_PC_FROM_END was not in a list of link time constant
    expressions and that was a result of confusiing messages like PR shows:
    
    /usr/bin/ld.lld: error: /usr/lib/go/src/runtime/alg.go:47: 
    can't create dynamic relocation R_386_GOTPC against local symbol in readonly segment defined in /tmp/nice/go-link-597453838/go.o
    
    Though in reality we just should not have try to create a dynamic relocation for this case at all.
    
    Patch fixes the issue.
    
    Differential revision: https://reviews.llvm.org/D33717
    
    llvm-svn: 304393

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -360,9 +360,9 @@ static bool isStaticLinkTimeConstant(RelExpr E, uint32_t Type,
   // These expressions always compute a constant
   if (isRelExprOneOf<R_SIZE, R_GOT_FROM_END, R_GOT_OFF, R_MIPS_GOT_LOCAL_PAGE,
                      R_MIPS_GOT_OFF, R_MIPS_GOT_OFF32, R_MIPS_GOT_GP_PC,
-                     R_MIPS_TLSGD, R_GOT_PAGE_PC, R_GOT_PC, R_PLT_PC,
-                     R_TLSGD_PC, R_TLSGD, R_PPC_PLT_OPD, R_TLSDESC_CALL,
-                     R_TLSDESC_PAGE, R_HINT>(E))
+                     R_MIPS_TLSGD, R_GOT_PAGE_PC, R_GOT_PC,
+                     R_GOTONLY_PC_FROM_END, R_PLT_PC, R_TLSGD_PC, R_TLSGD,
+                     R_PPC_PLT_OPD, R_TLSDESC_CALL, R_TLSDESC_PAGE, R_HINT>(E))
     return true;
 
   // These never do, except if the entire file is position dependent or if
