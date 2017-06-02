a70ed1d69390bb66b4f4aeb9fb9016c1841ff123
Tim Shen <timshen91@gmail.com>
2017-06-02 00:08:58 +0000

    [ThinLTO] Add x86 requires to thin_link_bitcode. NFC.
    It already specifies the triples, so the intention was to test x86 for
    now (or then).
    
    Differential Revision: https://reviews.llvm.org/D33692
    
    llvm-svn: 304501

--- clang/test/CodeGen/thin_link_bitcode.c
+++ clang/test/CodeGen/thin_link_bitcode.c
@@ -1,3 +1,5 @@
+// REQUIRES: x86-registered-target
+//
 // RUN: %clang_cc1 -o %t -flto=thin -fthin-link-bitcode=%t.nodebug -triple x86_64-unknown-linux-gnu -emit-llvm-bc -debug-info-kind=limited %s
 // RUN: llvm-bcanalyzer -dump %t | FileCheck %s
 // RUN: llvm-bcanalyzer -dump %t.nodebug | FileCheck %s --check-prefix=NO_DEBUG
