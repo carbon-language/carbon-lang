e6035a51f772b41ab0925956d0f6417b40277c08
Krasimir Georgiev <krasimir@google.com>
2017-06-08 15:11:51 +0000

    [clangd] Add parameter and return type information to completion results
    Summary:
    This patch adds information about the parameters and return types of completion
    candidates.
    Previously, for the following code:
    ```
    struct S {
      int func(int a, double b) const;
    };
    ```
    the completer would only return the label of the candidate `func`.
    Now it will also return the return type `int` and will format the label for the
    candidate as `func(int a, double b) const`.
    
    Reviewers: bkramer, ilya-biryukov
    
    Reviewed By: ilya-biryukov
    
    Subscribers: cfe-commits
    
    Tags: #clang-tools-extra
    
    Differential Revision: https://reviews.llvm.org/D34033
    
    llvm-svn: 304980

--- clang-tools-extra/clangd/ClangdUnit.cpp
+++ clang-tools-extra/clangd/ClangdUnit.cpp
@@ -138,9 +138,21 @@ public:
           CodeCompleteOpts.IncludeBriefComments);
       if (CCS) {
         CompletionItem Item;
+        for (CodeCompletionString::Chunk C : *CCS) {
+          switch (C.Kind) {
+          case CodeCompletionString::CK_ResultType:
+            Item.detail = C.Text;
+            break;
+          case CodeCompletionString::CK_Optional:
+            break;
+          default:
+            Item.label += C.Text;
+            break;
+          }
+        }
         assert(CCS->getTypedText());
-        Item.label = CCS->getTypedText();
         Item.kind = getKind(Result.CursorKind);
+        Item.insertText = Item.sortText = Item.filterText = CCS->getTypedText();
         if (CCS->getBriefComment())
           Item.documentation = CCS->getBriefComment();
         Items->push_back(std::move(Item));
