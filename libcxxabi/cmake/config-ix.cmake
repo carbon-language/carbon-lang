cf0e48ca01e648cff27170f0574fcff965f55d60
Saleem Abdulrasool <compnerd@compnerd.org>
2017-06-11 23:59:26 +0000

    build: use cmake to pass -std=c++11
    Rather than manually checking for support for the spelling of the C++
    standard, indicate to CMake that we require that the compiler support
    C++11 and that we compile without the GNU extensions.  This simplifies
    the flags handling in libc++abi itself by relying on CMake to translate
    the flag and add it as appropriate.
    
    llvm-svn: 305175

--- libcxxabi/cmake/config-ix.cmake
+++ libcxxabi/cmake/config-ix.cmake
@@ -81,11 +81,6 @@ check_cxx_compiler_flag(/EHsc                 LIBCXXABI_HAS_EHSC_FLAG)
 check_cxx_compiler_flag(/EHs-                 LIBCXXABI_HAS_NO_EHS_FLAG)
 check_cxx_compiler_flag(/EHa-                 LIBCXXABI_HAS_NO_EHA_FLAG)
 check_cxx_compiler_flag(/GR-                  LIBCXXABI_HAS_NO_GR_FLAG)
-check_cxx_compiler_flag(-std=c++11            LIBCXXABI_HAS_STD_CXX11)
-
-if(LIBCXXABI_HAS_STD_CXX11)
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
-endif()
 
 # Check libraries
 check_library_exists(dl dladdr "" LIBCXXABI_HAS_DL_LIB)
