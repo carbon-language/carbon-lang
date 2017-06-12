b5fe855dfbb1c62e9d82b34cd4f000c122e26256
George Burgess IV <george.burgess.iv@gmail.com>
2017-06-12 17:44:30 +0000

    [Sema] Use the right FoldingSet.
    We were doing FindNodeOrInsertPos on SubstTemplateTypeParmPackTypes, so
    we should presumably be inserting into SubstTemplateTypeParmPackTypes.
    
    Looks like the FoldingSet API can be tweaked a bit so that we can catch
    things like this at compile-time. I'll look into that shortly.
    
    I'm unsure of how to test this; suggestions welcome.
    
    Thanks to Vladimir Voskresensky for bringing this up!
    
    llvm-svn: 305207

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -3565,7 +3565,7 @@ QualType ASTContext::getSubstTemplateTypeParmPackType(
     = new (*this, TypeAlignment) SubstTemplateTypeParmPackType(Parm, Canon,
                                                                ArgPack);
   Types.push_back(SubstParm);
-  SubstTemplateTypeParmTypes.InsertNode(SubstParm, InsertPos);
+  SubstTemplateTypeParmPackTypes.InsertNode(SubstParm, InsertPos);
   return QualType(SubstParm, 0);  
 }
 
