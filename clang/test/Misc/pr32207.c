374b41a6aab43830578215cf1337ebb501e7dea7
Yaron Keren <yaron.keren@gmail.com>
2017-06-12 18:29:37 +0000

    Address David Blaikie comment by replacing grep with FileCheck.
    llvm-svn: 305215

--- clang/test/Misc/pr32207.c
+++ clang/test/Misc/pr32207.c
@@ -1,3 +1,4 @@
 // test for r305179
-// RUN: %clang_cc1 -emit-llvm -O -mllvm -print-after-all %s -o %t 2>&1 | grep '*** IR Dump After Function Integration/Inlining ***'
+// RUN: %clang_cc1 -emit-llvm -O -mllvm -print-after-all %s -o %t 2>&1 | FileCheck %s
+// CHECK: *** IR Dump After Function Integration/Inlining ***
 void foo() {}
