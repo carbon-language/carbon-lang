9cac9ad9d4b835f7f1da76dde865e358363e1e75
Michael Park <mcypark@gmail.com>
2017-06-14 05:51:18 +0000

    Add an `__is_inplace_index` metafunction.
    Summary: This is used to constrain `variant`'s converting constructor correctly.
    
    Reviewers: EricWF, mclow.lists
    
    Reviewed By: EricWF, mclow.lists
    
    Differential Revision: https://reviews.llvm.org/D34111
    
    llvm-svn: 305370

---
+++ libcxx/test/libcxx/utilities/utility/__is_inplace_index.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// template <class _Tp> using __is_inplace_index
+
+#include <utility>
+
+struct S {};
+
+int main() {
+  using I = std::in_place_index_t<0>;
+  static_assert( std::__is_inplace_index<I>::value, "");
+  static_assert( std::__is_inplace_index<const I>::value, "");
+  static_assert( std::__is_inplace_index<const volatile I>::value, "");
+  static_assert( std::__is_inplace_index<I&>::value, "");
+  static_assert( std::__is_inplace_index<const I&>::value, "");
+  static_assert( std::__is_inplace_index<const volatile I&>::value, "");
+  static_assert( std::__is_inplace_index<I&&>::value, "");
+  static_assert( std::__is_inplace_index<const I&&>::value, "");
+  static_assert( std::__is_inplace_index<const volatile I&&>::value, "");
+  static_assert(!std::__is_inplace_index<std::in_place_type_t<int>>::value, "");
+  static_assert(!std::__is_inplace_index<std::in_place_t>::value, "");
+  static_assert(!std::__is_inplace_index<void>::value, "");
+  static_assert(!std::__is_inplace_index<int>::value, "");
+  static_assert(!std::__is_inplace_index<S>::value, "");
+}
---
+++ libcxx/test/libcxx/utilities/utility/__is_inplace_type.pass.cpp
@@ -0,0 +1,32 @@
+//===----------------------------------------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is dual licensed under the MIT and the University of Illinois Open
+// Source Licenses. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
+// template <class _Tp> using __is_inplace_type
+
+#include <utility>
+
+struct S {};
+
+int main() {
+  using T = std::in_place_type_t<int>;
+  static_assert( std::__is_inplace_type<T>::value, "");
+  static_assert( std::__is_inplace_type<const T>::value, "");
+  static_assert( std::__is_inplace_type<const volatile T>::value, "");
+  static_assert( std::__is_inplace_type<T&>::value, "");
+  static_assert( std::__is_inplace_type<const T&>::value, "");
+  static_assert( std::__is_inplace_type<const volatile T&>::value, "");
+  static_assert( std::__is_inplace_type<T&&>::value, "");
+  static_assert( std::__is_inplace_type<const T&&>::value, "");
+  static_assert( std::__is_inplace_type<const volatile T&&>::value, "");
+  static_assert(!std::__is_inplace_type<std::in_place_index_t<0>>::value, "");
+  static_assert(!std::__is_inplace_type<std::in_place_t>::value, "");
+  static_assert(!std::__is_inplace_type<void>::value, "");
+  static_assert(!std::__is_inplace_type<int>::value, "");
+  static_assert(!std::__is_inplace_type<S>::value, "");
+}
