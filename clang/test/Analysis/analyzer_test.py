f94c108395a3eb0ad785a2433877dc21d79cf6bd
Dominic Chen <d.c.ddcc@gmail.com>
2017-06-15 17:05:07 +0000

    [analyzer]: Improve test handling with multiple constraint managers
    Summary: Modify the test infrastructure to properly handle tests that require z3, and merge together the output of all tests on success. This is required for D28954.
    
    Reviewers: dcoughlin, zaks.anna, NoQ, xazax.hun
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D33308
    
    llvm-svn: 305480

--- clang/test/Analysis/analyzer_test.py
+++ clang/test/Analysis/analyzer_test.py
@@ -5,24 +5,39 @@ import lit.TestRunner
 class AnalyzerTest(lit.formats.ShTest):
 
     def execute(self, test, litConfig):
-        result = self.executeWithAnalyzeSubstitution(
-            test, litConfig, '-analyzer-constraints=range')
+        results = []
 
-        if result.code == lit.Test.FAIL:
-            return result
+        # Parse any test requirements ('REQUIRES: ')
+        saved_test = test
+        lit.TestRunner.parseIntegratedTestScript(test)
+
+        if 'z3' not in test.requires:
+            results.append(self.executeWithAnalyzeSubstitution(
+                saved_test, litConfig, '-analyzer-constraints=range'))
+
+            if results[-1].code == lit.Test.FAIL:
+                return results[-1]
 
         # If z3 backend available, add an additional run line for it
         if test.config.clang_staticanalyzer_z3 == '1':
-            result = self.executeWithAnalyzeSubstitution(
-                test, litConfig, '-analyzer-constraints=z3 -DANALYZER_CM_Z3')
+            results.append(self.executeWithAnalyzeSubstitution(
+                saved_test, litConfig, '-analyzer-constraints=z3 -DANALYZER_CM_Z3'))
 
-        return result
+        # Combine all result outputs into the last element
+        for x in results:
+            if x != results[-1]:
+                results[-1].output = x.output + results[-1].output
+
+        if results:
+            return results[-1]
+        return lit.Test.Result(lit.Test.UNSUPPORTED,
+            "Test requires the following unavailable features: z3")
 
     def executeWithAnalyzeSubstitution(self, test, litConfig, substitution):
         saved_substitutions = list(test.config.substitutions)
         test.config.substitutions.append(('%analyze', substitution))
         result = lit.TestRunner.executeShTest(test, litConfig,
-                                              self.execute_external)
+            self.execute_external)
         test.config.substitutions = saved_substitutions
 
         return result
