16809493c5dca220304ef5f5cf1b247cf6c1f094
Daniel Jasper <djasper@google.com>
2017-06-16 19:29:20 +0000

    Ignore return value in test.
    llvm-svn: 305574

--- clang/test/Driver/m_and_mm.c
+++ clang/test/Driver/m_and_mm.c
@@ -5,7 +5,7 @@
 // RUN: %clang -M -MM %s 2> %t
 // RUN: not grep "warning" %t
 
-// RUN: %clang -MMD -MD %s 2> %t
+// RUN: %clang -MMD -MD %s 2> %t || true
 // RUN: grep "warning" %t
 
 #warning "This warning shouldn't show up with -M and -MM"
