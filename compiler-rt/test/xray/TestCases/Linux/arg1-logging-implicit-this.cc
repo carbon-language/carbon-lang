10bbc4f99eda44d96df29bba74b6a0d6cc226113
Dean Michael Berris <dberris@google.com>
2017-06-16 03:24:07 +0000

    Add test for logging the implicit "this" argument for C++ member functions.
    Summary:
    This allows us to do more interesting things with the data available to
    C++ methods, to log the `this` pointer.
    
    Depends on D34050.
    
    Reviewers: pelikan
    
    Subscribers: llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D34051
    
    llvm-svn: 305545

---
+++ compiler-rt/test/xray/TestCases/Linux/arg1-logging-implicit-this.cc
@@ -0,0 +1,31 @@
+// Intercept the implicit 'this' argument of class member functions.
+//
+// RUN: %clangxx_xray -g -std=c++11 %s -o %t
+// RUN: rm log-args-this-* || true
+// RUN: XRAY_OPTIONS="patch_premain=true verbosity=1 xray_logfile_base=log-args-this-" %run %t
+//
+// XFAIL: arm || aarch64 || mips
+// UNSUPPORTED: powerpc64le
+#include "xray/xray_interface.h"
+#include <cassert>
+
+class A {
+ public:
+  [[clang::xray_always_instrument, clang::xray_log_args(1)]] void f() {
+    // does nothing.
+  }
+};
+
+volatile uint64_t captured = 0;
+
+void handler(int32_t, XRayEntryType, uint64_t arg1) {
+  captured = arg1;
+}
+
+int main() {
+  __xray_set_handler_arg1(handler);
+  A instance;
+  instance.f();
+  __xray_remove_handler_arg1();
+  assert(captured == (uint64_t)&instance);
+}
