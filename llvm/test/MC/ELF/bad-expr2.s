5f4a10bf23b2d1c0cbe2b11d4980772e84b5b0f0
Rafael Espindola <rafael.espindola@gmail.com>
2017-06-20 21:11:58 +0000

    Make this test a bit more strict. NFC.
    llvm-svn: 305855

--- llvm/test/MC/ELF/bad-expr2.s
+++ llvm/test/MC/ELF/bad-expr2.s
@@ -1,11 +1,10 @@
 // RUN: not llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o /dev/null \
 // RUN: 2>&1 | FileCheck %s
 
-// CHECK: No relocation available to represent this relative expression
-// CHECK: call foo - bar
-
-
+// CHECK: [[@LINE+2]]:{{[0-9]+}}: error: No relocation available to represent this relative expression
+// CHECK-NEXT: call foo - bar
         call foo - bar
+
         .section .foo
 foo:
         .section .bar
