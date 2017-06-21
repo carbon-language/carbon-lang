bac3570d534bd600de05f9d3847abf96cac220c2
Peter Collingbourne <peter@pcc.me.uk>
2017-06-21 21:29:51 +0000

    ELF: Don't dereference Repl in MarkLive. NFCI.
    This is unnecessary because --gc-sections runs before ICF.
    
    Differential Revision: https://reviews.llvm.org/D34465
    
    llvm-svn: 305954

--- lld/ELF/MarkLive.cpp
+++ lld/ELF/MarkLive.cpp
@@ -78,7 +78,7 @@ static void resolveReloc(InputSectionBase &Sec, RelT &Rel,
     typename ELFT::uint Offset = D->Value;
     if (D->isSection())
       Offset += getAddend<ELFT>(Sec, Rel);
-    Fn({cast<InputSectionBase>(D->Section)->Repl, Offset});
+    Fn({cast<InputSectionBase>(D->Section), Offset});
   } else if (auto *U = dyn_cast<Undefined>(&B)) {
     for (InputSectionBase *Sec : CNamedSections.lookup(U->getName()))
       Fn({Sec, 0});
