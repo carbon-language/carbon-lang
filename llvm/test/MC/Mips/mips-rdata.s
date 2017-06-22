1c73fcc131de7b62e55c3f0e8da95e47779d2826
Simon Dardis <simon.dardis@imgtec.com>
2017-06-22 10:41:51 +0000

    [mips] Implement the ".rdata" MIPS assembly directive.
    Rather than creating a separate ".rdata" section distinct from the
    customary ".rodata" in ELF, ".rdata" switches to the ".rodata" section.
    
    This patch relands r305949 and r305950 with the correct commit message
    and addresses nit raised during review.
    
    Patch By: John Baldwin!
    
    Differential Revision: https://reviews.llvm.org/D34452
    
    llvm-svn: 305995

---
+++ llvm/test/MC/Mips/mips-rdata.s
@@ -0,0 +1,13 @@
+# Check that .rdata sections have proper name, flags, and section types.
+
+# RUN: llvm-mc -filetype=obj -triple=mips-unknown-linux %s -o - \
+# RUN:   | llvm-readobj -s | FileCheck %s
+
+  .rdata
+  .word 0
+
+# CHECK:      Name: .rodata
+# CHECK-NEXT: Type: SHT_PROGBITS
+# CHECK-NEXT: Flags [ (0x2)
+# CHECK-NEXT:   SHF_ALLOC
+# CHECK-NEXT: ]
