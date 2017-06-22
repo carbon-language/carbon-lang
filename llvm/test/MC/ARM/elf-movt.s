f9df4290689c34e1cf5db948c1a6cdbfd5cb13a0
Rafael Espindola <rafael.espindola@gmail.com>
2017-06-22 00:44:05 +0000

    Also test thumb.
    llvm-svn: 305976

--- llvm/test/MC/ARM/elf-movt.s
+++ llvm/test/MC/ARM/elf-movt.s
@@ -1,6 +1,9 @@
 @ RUN: llvm-mc %s -triple=armv7-linux-gnueabi | FileCheck -check-prefix=ASM %s
 @ RUN: llvm-mc %s -triple=armv7-linux-gnueabi -filetype=obj -o %t.o
-@ RUN:    llvm-objdump -d -r %t.o -triple=armv7-linux-gnueabi | FileCheck -check-prefix=OBJ %s
+@ RUN: llvm-objdump -d -r %t.o -triple=armv7-linux-gnueabi | FileCheck -check-prefix=OBJ %s
+@ RUN: llvm-mc %s -triple=thumbv7-linux-gnueabi -filetype=obj -o %t.o
+@ RUN: llvm-objdump -d -r %t.o -triple=thumbv7-linux-gnueabi | FileCheck -check-prefix=THUMB %s
+
 	.syntax unified
 	.text
 	.globl	barf
@@ -20,3 +23,10 @@ barf:                                   @ @barf
 @OBJ-NEXT: 00000000:         R_ARM_MOVW_PREL_NC   GOT
 @OBJ-NEXT: 4:             f4 0f 4f e3     movt    r0, #65524
 @OBJ-NEXT: 00000004:         R_ARM_MOVT_PREL      GOT
+
+@THUMB:      Disassembly of section .text:
+@THUMB-NEXT: barf:
+@THUMB-NEXT: 0:             4f f6 f0 70     movw    r0, #65520
+@THUMB-NEXT: 00000000:         R_ARM_THM_MOVW_PREL_NC GOT
+@THUMB-NEXT: 4:             cf f6 f4 70     movt    r0, #65524
+@THUMB-NEXT: 00000004:         R_ARM_THM_MOVT_PREL    GOT
