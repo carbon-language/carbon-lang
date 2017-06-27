6a92381c50dbc25bd8b4c7cbe31c9deaeb4f8806
Saleem Abdulrasool <compnerd@compnerd.org>
2017-06-27 18:57:50 +0000

    test: fix test for release builds
    Use a regex capture to avoid hardcoding the name.  This should repair
    the failing buildbot.
    
    llvm-svn: 306447

--- clang/test/CodeGenObjCXX/arc-indirect.mm
+++ clang/test/CodeGenObjCXX/arc-indirect.mm
@@ -9,6 +9,7 @@ struct S {
 
 @interface C
 @end
+
 @implementation C
 - (void)object:(id)obj struct:(S)s {
 }
@@ -17,5 +18,5 @@ struct S {
 // CHECK-GNUSTEP: define internal void @_i_C__object_struct_(<{ %0*, i8*, i8*, %struct.S, [3 x i8] }>* inalloca)
 // CHECK-DARWIN: define internal void @"\01-[C object:struct:]"(<{ %0*, i8*, i8*, %struct.S, [3 x i8] }>* inalloca)
 // CHECK: %obj = getelementptr inbounds <{ %0*, i8*, i8*, %struct.S, [3 x i8] }>, <{ %0*, i8*, i8*, %struct.S, [3 x i8] }>* %0, i32 0, i32 2
-// CHECK: %1 = load i8*, i8** %obj, align 4
-// CHECK: call void @objc_storeStrong(i8** %obj, i8* %1)
+// CHECK: %[[INSTANCE:[0-9]+]] = load i8*, i8** %obj, align 4
+// CHECK: call void @objc_storeStrong(i8** %obj, i8* %[[INSTANCE]])
