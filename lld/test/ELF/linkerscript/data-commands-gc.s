36f2edb6fdaedb5a620678795057b331ad2956c1
Rafael Espindola <rafael.espindola@gmail.com>
2017-06-28 01:46:31 +0000

    Check the produced file instead of stderr.
    It is somewhat pointless to check that a specific error is not
    produced. That is already checked by the ld.lld exit value.
    
    Instead make the test a bit stronger by checking that the output file
    has the expected symbol and section.
    
    llvm-svn: 306496

--- lld/test/ELF/linkerscript/data-commands-gc.s
+++ lld/test/ELF/linkerscript/data-commands-gc.s
@@ -1,9 +1,10 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 # RUN: echo "SECTIONS { .text : { *(.text*) QUAD(bar) } }" > %t.script
-# RUN: ld.lld --gc-sections -o %t %t.o --script %t.script | FileCheck -allow-empty %s
+# RUN: ld.lld --gc-sections -o %t %t.o --script %t.script
+# RUN: llvm-objdump -t %t | FileCheck %s
 
-# CHECK-NOT: unable to evaluate expression: input section .rodata.bar has no output section assigned
+# CHECK: 0000000000000011         .rodata                 00000000 bar
 
 .section .rodata.bar
 .quad 0x1122334455667788
