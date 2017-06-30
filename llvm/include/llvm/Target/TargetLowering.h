d1698af8c8127b0e9b2e72a83764dfed0e50e4a4
Simon Pilgrim <llvm-dev@redking.me.uk>
2017-06-30 13:26:17 +0000

    Remove unnecessary commented out argument. NFCI.
    llvm-svn: 306824

--- llvm/include/llvm/Target/TargetLowering.h
+++ llvm/include/llvm/Target/TargetLowering.h
@@ -2722,7 +2722,7 @@ public:
   //  This transformation may not be desirable if it disrupts a particularly
   //  auspicious target-specific tree (e.g. bitfield extraction in AArch64).
   //  By default, it returns true.
-  virtual bool isDesirableToCommuteWithShift(const SDNode *N /*Op*/) const {
+  virtual bool isDesirableToCommuteWithShift(const SDNode *N) const {
     return true;
   }
 
