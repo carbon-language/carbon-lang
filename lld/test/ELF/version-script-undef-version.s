aec6287f5db4565ca5c214482ebc7c70b44daaca
Rafael Espindola <rafael.espindola@gmail.com>
2017-07-04 14:59:28 +0000

    Make the test more strict.
    We now don't produce "has undefined version" errors when producing
    binaries even when there is a linker script.
    
    llvm-svn: 307082

--- lld/test/ELF/version-script-undef-version.s
+++ lld/test/ELF/version-script-undef-version.s
@@ -3,6 +3,8 @@
 # Test that we don't error on undefined versions when static linking.
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 # RUN: ld.lld %t.o -o %t
+# RUN: echo "DEFINED { global: *; };" > %t.map
+# RUN: ld.lld %t.o --version-script %t.map -o %t
 
 .global _start
 .global bar
