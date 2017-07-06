8c1fc04f3f5c1e13528bf42ff1da29e123d42bfc
Rafael Espindola <rafael.espindola@gmail.com>
2017-07-06 21:33:23 +0000

    Use @LINE in two more tests.
    llvm-svn: 307318

--- llvm/test/MC/ELF/bad-expr3.s
+++ llvm/test/MC/ELF/bad-expr3.s
@@ -1,8 +1,7 @@
 // RUN: not llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o /dev/null \
 // RUN: 2>&1 | FileCheck %s
 
-// CHECK: Cannot represent a difference across sections
-
+// CHECK: [[@LINE+1]]:{{[0-9]+}}: error: Cannot represent a difference across sections
         .long foo - bar
         .section .zed
 foo:
