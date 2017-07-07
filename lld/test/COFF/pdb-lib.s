cea99709c463da16558f93b8c4374907226957f6
Zachary Turner <zturner@google.com>
2017-07-07 05:41:25 +0000

    Fix lld tests after r307356.
    llvm-svn: 307360

--- lld/test/COFF/pdb-lib.s
+++ lld/test/COFF/pdb-lib.s
@@ -13,12 +13,15 @@
 # CHECK-NEXT:   Mod 0000 | Name: `{{.*pdb-lib.s.tmp[/\\]foo.obj}}`:
 # CHECK-NEXT:              Obj: `{{.*pdb-lib.s.tmp[/\\]foo.obj}}`:
 # CHECK-NEXT:              debug stream: 9, # files: 0, has ec info: false
+# CHECK-NEXT:              pdb file ni: 0 ``, src file ni: 0 ``
 # CHECK-NEXT:   Mod 0001 | Name: `bar.obj`:
 # CHECK-NEXT:              Obj: `{{.*pdb-lib.s.tmp[/\\]bar.lib}}`:
 # CHECK-NEXT:              debug stream: 10, # files: 0, has ec info: false
+# CHECK-NEXT:              pdb file ni: 0 ``, src file ni: 0 ``
 # CHECK-NEXT:   Mod 0002 | Name: `* Linker *`:
 # CHECK-NEXT:              Obj: ``:
 # CHECK-NEXT:              debug stream: 11, # files: 0, has ec info: false
+# CHECK-NEXT:              pdb file ni: 1 `{{.*foo.pdb}}`, src file ni: 0 ``
 
         .def     _main;
         .scl    2;
