2d06d7e512b6f1d28145154afeabc074e36d24f2
Dan Liew <dan@su-root.co.uk>
2017-07-11 18:27:48 +0000

    [LibFuzzer] Fix `-Wpedantic` warning reported by Eric Christopher.
    The warning is reproducible with GCC 4.8. Thanks to David Blaikie for
    the suggested fix.
    
    The reported warning was
    
    ```
    /usr/local/google/home/echristo/sources/llvm/lib/Fuzzer/FuzzerExtFunctions.def:29:10: warning: ISO C++ forbids casting between pointer-to-function and pointer-to-object [-Wpedantic]
     EXT_FUNC(__lsan_enable, void, (), false);
              ^
    /usr/local/google/home/echristo/sources/llvm/lib/Fuzzer/FuzzerExtFunctionsWeak.cpp:44:24: note: in definition of macro ‘EXT_FUNC’
       CheckFnPtr((void *)::NAME, #NAME, WARN);
                            ^
    ```
    
    Differential Revision: https://reviews.llvm.org/D35243
    
    llvm-svn: 307686

--- llvm/lib/Fuzzer/FuzzerExtFunctionsWeak.cpp
+++ llvm/lib/Fuzzer/FuzzerExtFunctionsWeak.cpp
@@ -41,7 +41,8 @@ namespace fuzzer {
 ExternalFunctions::ExternalFunctions() {
 #define EXT_FUNC(NAME, RETURN_TYPE, FUNC_SIG, WARN)                            \
   this->NAME = ::NAME;                                                         \
-  CheckFnPtr((void *)::NAME, #NAME, WARN);
+  CheckFnPtr(reinterpret_cast<void *>(reinterpret_cast<uintptr_t>(::NAME)),    \
+             #NAME, WARN);
 
 #include "FuzzerExtFunctions.def"
 
