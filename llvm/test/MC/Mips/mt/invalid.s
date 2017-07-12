2de1ddbd9c592c5297c6f02e52a472af2ed2ea3c
Simon Dardis <simon.dardis@imgtec.com>
2017-07-12 14:48:27 +0000

    [mips][mt][4/7] Add IAS support for dvpe, evpe instructions.
    Reviewers: slthakur, atanasyan
    
    Differential Revision: https://reviews.llvm.org/D35251
    
    llvm-svn: 307793

--- llvm/test/MC/Mips/mt/invalid.s
+++ llvm/test/MC/Mips/mt/invalid.s
@@ -5,3 +5,9 @@
   emt 4           # CHECK: error: invalid operand for instruction
   emt $4, $5      # CHECK: error: invalid operand for instruction
   emt $5, 0($5)   # CHECK: error: invalid operand for instruction
+  dvpe 4          # CHECK: error: invalid operand for instruction
+  dvpe $4, $5     # CHECK: error: invalid operand for instruction
+  dvpe $5, 0($4)  # CHECK: error: invalid operand for instruction
+  evpe 4          # CHECK: error: invalid operand for instruction
+  evpe $4, $5     # CHECK: error: invalid operand for instruction
+  evpe $5, 0($5)  # CHECK: error: invalid operand for instruction
