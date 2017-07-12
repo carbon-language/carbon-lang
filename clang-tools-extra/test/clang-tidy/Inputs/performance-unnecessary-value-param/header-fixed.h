98a6b3ea87152728f2e4e513fe05d629b722e4e0
Chih-Hung Hsieh <chh@google.com>
2017-07-12 16:27:00 +0000

    [clang-tidy] add regression test to performance-unnecessary-value-param
    This test shows the problem in https://bugs.llvm.org/show_bug.cgi?id=33734
    
    Differential Revision: https://reviews.llvm.org/D35225
    
    llvm-svn: 307810

---
+++ clang-tools-extra/test/clang-tidy/Inputs/performance-unnecessary-value-param/header-fixed.h
@@ -0,0 +1,15 @@
+// struct ABC is expensive to copy and should be
+// passed as a const referece.
+struct ABC {
+  ABC(const ABC&);
+  int get(int) const;
+};
+
+
+int f1(int n,              const ABC& v1,   const ABC& v2); // line 9
+
+int f1(int n, ABC v1); // line 11
+
+
+
+int f2(        int n,       const ABC& v2); // line 15
