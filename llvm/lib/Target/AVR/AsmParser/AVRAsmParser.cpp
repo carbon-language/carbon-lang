fb22c187ee8e8cc612f7e3abadb49224b2993c07
Dylan McKay <me@dylanmckay.io>
2017-07-13 08:39:46 +0000

    [AVR] Add a 'LLVM_FALLTHROUGH' statement to the AsmParser
    Should fix warnings in the build.
    
    llvm-svn: 307890

--- llvm/lib/Target/AVR/AsmParser/AVRAsmParser.cpp
+++ llvm/lib/Target/AVR/AsmParser/AVRAsmParser.cpp
@@ -466,6 +466,7 @@ bool AVRAsmParser::parseOperand(OperandVector &Operands) {
     if (!tryParseRegisterOperand(Operands)) {
       return false;
     }
+  LLVM_FALLTHROUGH;
   case AsmToken::LParen:
   case AsmToken::Integer:
   case AsmToken::Dot:
