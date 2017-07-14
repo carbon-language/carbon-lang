6fdfede10d0643209f43e3fbd99fbdbe5f85b4d2
Davide Italiano <davide@freebsd.org>
2017-07-14 21:20:29 +0000

    [AMDGPU] Throw away more dead code. NFCI.
    llvm-svn: 308055

--- llvm/lib/Target/AMDGPU/AMDGPUAnnotateKernelFeatures.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUAnnotateKernelFeatures.cpp
@@ -35,8 +35,6 @@ private:
 
   bool addFeatureAttributes(Function &F);
 
-  void addAttrToCallers(Function &Intrin, StringRef AttrName);
-
 public:
   static char ID;
 
@@ -253,20 +251,6 @@ bool AMDGPUAnnotateKernelFeatures::addFeatureAttributes(Function &F) {
   }
 
   return Changed;
-}
-
-void AMDGPUAnnotateKernelFeatures::addAttrToCallers(Function &Intrin,
-                                                    StringRef AttrName) {
-  SmallPtrSet<Function *, 4> SeenFuncs;
-
-  for (User *U : Intrin.users()) {
-    // CallInst is the only valid user for an intrinsic.
-    CallInst *CI = cast<CallInst>(U);
-
-    Function *CallingFunction = CI->getParent()->getParent();
-    if (SeenFuncs.insert(CallingFunction).second)
-      CallingFunction->addFnAttr(AttrName);
-  }
 }
 
 bool AMDGPUAnnotateKernelFeatures::runOnSCC(CallGraphSCC &SCC) {
