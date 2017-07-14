202a9f6817cd3fc6de20c88813986a1b8bfd4cfa
Igor Kudrin <ikudrin@accesssoftek.com>
2017-07-14 08:10:45 +0000

    [ELF] Fix writing the content of the .got section in a wrong place.
    In filling the .got sections, InputSection::OutSecOff was added twice
    when finding the position to apply a relocation: first time in
    InputSection::writeTo() and then in SectionBase::getOffset().
    
    Differential revision: https://reviews.llvm.org/D34232
    
    llvm-svn: 308003

--- lld/ELF/SyntheticSections.cpp
+++ lld/ELF/SyntheticSections.cpp
@@ -662,7 +662,12 @@ bool GotSection::empty() const {
   return NumEntries == 0 && !HasGotOffRel;
 }
 
-void GotSection::writeTo(uint8_t *Buf) { relocateAlloc(Buf, Buf + Size); }
+void GotSection::writeTo(uint8_t *Buf) {
+  // Buf points to the start of this section's buffer,
+  // whereas InputSectionBase::relocateAlloc() expects its argument
+  // to point to the start of the output section.
+  relocateAlloc(Buf - OutSecOff, Buf - OutSecOff + Size);
+}
 
 MipsGotSection::MipsGotSection()
     : SyntheticSection(SHF_ALLOC | SHF_WRITE | SHF_MIPS_GPREL, SHT_PROGBITS, 16,
