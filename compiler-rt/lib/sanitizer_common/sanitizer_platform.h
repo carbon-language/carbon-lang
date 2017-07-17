27169a7709b50bdf1ed6bc96a91191af7fa1acd0
Kamil Rytarowski <n54@gmx.com>
2017-07-17 20:47:53 +0000

    Introduce SANITIZER_NETBSD in sanitizer_platform.h
    Summary:
    Add defines for new NetBSD: SANITIZER_NETBSD,
    it will be used across the codebase for sanitizers.
    
    NetBSD is a POSIX-like platform, add it to SANITIZER_POSIX.
    
    Part of the code inspired by the original work on libsanitizer in GCC 5.4 by Christos Zoulas.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, kcc, dim, alekseyshl, filcab, eugenis, vitalybuka
    
    Reviewed By: kcc
    
    Subscribers: kubamracek, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D35467
    
    llvm-svn: 308216

--- compiler-rt/lib/sanitizer_common/sanitizer_platform.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_platform.h
@@ -13,7 +13,7 @@
 #ifndef SANITIZER_PLATFORM_H
 #define SANITIZER_PLATFORM_H
 
-#if !defined(__linux__) && !defined(__FreeBSD__) && \
+#if !defined(__linux__) && !defined(__FreeBSD__) && !defined(__NetBSD__) \
   !defined(__APPLE__) && !defined(_WIN32)
 # error "This operating system is not supported"
 #endif
@@ -28,6 +28,12 @@
 # define SANITIZER_FREEBSD 1
 #else
 # define SANITIZER_FREEBSD 0
+#endif
+
+#if defined(__NetBSD__)
+# define SANITIZER_NETBSD 1
+#else
+# define SANITIZER_NETBSD 0
 #endif
 
 #if defined(__APPLE__)
@@ -79,7 +85,8 @@
 # define SANITIZER_ANDROID 0
 #endif
 
-#define SANITIZER_POSIX (SANITIZER_FREEBSD || SANITIZER_LINUX || SANITIZER_MAC)
+#define SANITIZER_POSIX \
+  (SANITIZER_FREEBSD || SANITIZER_LINUX || SANITIZER_MAC || SANITIZER_NETBSD)
 
 #if __LP64__ || defined(_WIN64)
 #  define SANITIZER_WORDSIZE 64
