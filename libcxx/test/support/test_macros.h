33116350ad15d082de8442c30099da3d027f22c9
Marshall Clow <mclow.lists@gmail.com>
2017-07-17 03:02:27 +0000

    Add case for c++2a to libc++ and test macros
    llvm-svn: 308159

--- libcxx/test/support/test_macros.h
+++ libcxx/test/support/test_macros.h
@@ -81,8 +81,11 @@
 # define TEST_STD_VER 11
 #elif __cplusplus <= 201402L
 # define TEST_STD_VER 14
+#elif __cplusplus <= 201703L
+# define TEST_STD_VER 17
 #else
-# define TEST_STD_VER 16    // current year; greater than current standard
+# define TEST_STD_VER 99    // greater than current standard
+// This is deliberately different than _LIBCPP_STD_VER to discourage matching them up.
 #endif
 #endif
 
