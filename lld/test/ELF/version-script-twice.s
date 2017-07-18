2b84fa418bb26b8091a628a2d92c590e1c4b8833
Rafael Espindola <rafael.espindola@gmail.com>
2017-07-18 23:26:16 +0000

    Make the test more precise.
    The real issue was the missing explicit versions.
    
    llvm-svn: 308391

--- lld/test/ELF/version-script-twice.s
+++ lld/test/ELF/version-script-twice.s
@@ -1,13 +1,14 @@
 # REQUIRES: x86
 
-# RUN: echo "FBSD_1.2 {};" > %t.ver
+# RUN: echo "FBSD_1.1 {}; FBSD_1.2 {};" > %t.ver
 # RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
 # RUN: ld.lld -shared %t.o -o %t.so --version-script=%t.ver
-# RUN: llvm-nm --dynamic %t.so | FileCheck %s
+# RUN: llvm-readobj --dyn-symbols --elf-output-style=GNU %t.so | FileCheck %s
 
         .weak	openat
 openat:
+openat@FBSD_1.1 = openat
 openat@@FBSD_1.2 = openat
 
-# CHECK: 0000000000001000 W openat
-# CHECK-NEXT: 0000000000001000 W openat
+# CHECK-DAG: openat@FBSD_1.1
+# CHECK-DAG: openat@@FBSD_1.2
