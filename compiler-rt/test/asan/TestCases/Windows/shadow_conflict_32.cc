1eb9595c532d3e1b401e68c738f9b72a8de4ca24
Vitaly Buka <vitalybuka@google.com>
2017-07-20 18:20:12 +0000

    [asan] Update unittest for changes in logging r308637
    llvm-svn: 308647

--- compiler-rt/test/asan/TestCases/Windows/shadow_conflict_32.cc
+++ compiler-rt/test/asan/TestCases/Windows/shadow_conflict_32.cc
@@ -20,9 +20,9 @@ int main() {
 extern "C" __declspec(dllexport) int test_function() { return 0; }
 #endif
 
-// CHECK: =={{[0-9]+}}==Shadow memory range interleaves with an existing memory mapping. ASan cannot proceed correctly. ABORTING.
-// CHECK: =={{[0-9]+}}==ASan shadow was supposed to be located in the [0x2fff0000-0x3fffffff] range.
-// CHECK: =={{[0-9]+}}==Dumping process modules
+// CHECK: =={{[0-9:]+}}==Shadow memory range interleaves with an existing memory mapping. ASan cannot proceed correctly. ABORTING.
+// CHECK: =={{[0-9:]+}}==ASan shadow was supposed to be located in the [0x2fff0000-0x3fffffff] range.
+// CHECK: =={{[0-9:]+}}==Dumping process modules
 
 // CHECK-DAG: {{0x30000000-0x300.....}} {{.*}}\shadow_conflict_32.cc.tmp_dll.dll
 // CHECK-DAG: {{0x........-0x........}} {{.*}}\shadow_conflict_32.cc.tmp.exe
