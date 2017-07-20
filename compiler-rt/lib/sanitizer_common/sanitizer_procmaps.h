734acfda0f02e035f2839d188b0a57fce3159f70
Kamil Rytarowski <n54@gmx.com>
2017-07-20 13:31:06 +0000

    Add NetBSD support in sanitizer_procmaps.h
    Summary:
    Reuse Linux and FreeBSD - no NetBSD specific changes.
    
    Part of the code inspired by the original work on libsanitizer in GCC 5.4 by Christos Zoulas.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, filcab, kcc, vitalybuka
    
    Reviewed By: filcab
    
    Subscribers: llvm-commits, emaste, kubamracek, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D35629
    
    llvm-svn: 308615

--- compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_procmaps.h
@@ -20,7 +20,7 @@
 
 namespace __sanitizer {
 
-#if SANITIZER_FREEBSD || SANITIZER_LINUX
+#if SANITIZER_FREEBSD || SANITIZER_LINUX || SANITIZER_NETBSD
 struct ProcSelfMapsBuff {
   char *data;
   uptr mmaped_size;
@@ -29,7 +29,7 @@ struct ProcSelfMapsBuff {
 
 // Reads process memory map in an OS-specific way.
 void ReadProcMaps(ProcSelfMapsBuff *proc_maps);
-#endif  // SANITIZER_FREEBSD || SANITIZER_LINUX
+#endif  // SANITIZER_FREEBSD || SANITIZER_LINUX || SANITIZER_NETBSD
 
 // Memory protection masks.
 static const uptr kProtectionRead = 1;
@@ -76,7 +76,7 @@ class MemoryMappingLayout {
 
   // FIXME: Hide implementation details for different platforms in
   // platform-specific files.
-# if SANITIZER_FREEBSD || SANITIZER_LINUX
+# if SANITIZER_FREEBSD || SANITIZER_LINUX || SANITIZER_NETBSD
   ProcSelfMapsBuff proc_self_maps_;
   const char *current_;
 
