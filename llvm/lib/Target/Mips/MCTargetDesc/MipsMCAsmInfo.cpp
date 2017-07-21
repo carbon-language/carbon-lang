94942582233a992af614f13dfeb206137d098703
Petar Jovanovic <petar.jovanovic@imgtec.com>
2017-07-21 14:25:42 +0000

    [mips] Enable IAS by default for Android MIPS64
    Follow up to r306280 in Clang.
    Enable IAS by default for Android MIPS64 (uses N64 ABI).
    
    Differential Revision: https://reviews.llvm.org/D35482
    
    llvm-svn: 308742

--- llvm/lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
+++ llvm/lib/Target/Mips/MCTargetDesc/MipsMCAsmInfo.cpp
@@ -61,4 +61,8 @@ MipsMCAsmInfo::MipsMCAsmInfo(const Triple &TheTriple) {
   // Enable IAS by default for Debian mips64/mips64el.
   if (TheTriple.getEnvironment() == Triple::GNUABI64)
     UseIntegratedAssembler = true;
+
+  // Enable IAS by default for Android mips64el that uses N64 ABI.
+  if (TheTriple.getArch() == Triple::mips64el && TheTriple.isAndroid())
+    UseIntegratedAssembler = true;
 }
