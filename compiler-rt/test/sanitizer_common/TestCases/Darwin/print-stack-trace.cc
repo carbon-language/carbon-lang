680d102023b75135b8330f090f4eeb4d7721e943
George Karpenkov <ekarpenkov@apple.com>
2017-07-24 20:35:20 +0000

    Splitting out test for Darwin for print-stack-trace:
    New default symbolizer can not symbolize inlined function which appear
    under -O3.
    
    llvm-svn: 308925

--- compiler-rt/test/sanitizer_common/TestCases/Darwin/print-stack-trace.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Darwin/print-stack-trace.cc
@@ -1,9 +1,5 @@
 // RUN: %clangxx -O0 %s -o %t && %env_tool_opts=stack_trace_format=DEFAULT %run %t 2>&1 | FileCheck %s
-// RUN: %clangxx -O3 %s -o %t && %env_tool_opts=stack_trace_format=DEFAULT %run %t 2>&1 | FileCheck %s
 // RUN: %env_tool_opts=stack_trace_format='"frame:%n lineno:%l"' %run %t 2>&1 | FileCheck %s --check-prefix=CUSTOM
-// RUN: %env_tool_opts=symbolize_inline_frames=false:stack_trace_format=DEFAULT %run %t 2>&1 | FileCheck %s --check-prefix=NOINLINE
-
-// UNSUPPORTED: darwin
 
 #include <sanitizer/common_interface_defs.h>
 
@@ -21,6 +17,3 @@ int main() {
 
 // CUSTOM: frame:1 lineno:[[@LINE-11]]
 // CUSTOM: frame:2 lineno:[[@LINE-8]]
-
-// NOINLINE: #0 0x{{.*}} in __sanitizer_print_stack_trace
-// NOINLINE: #1 0x{{.*}} in main{{.*}}print-stack-trace.cc:[[@LINE-15]]
