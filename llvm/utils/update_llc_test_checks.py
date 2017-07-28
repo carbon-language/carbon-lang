b6fc8dc5e59294dffa627f261d05e83c58e4dc6f
Eli Friedman <efriedma@codeaurora.org>
2017-07-28 23:58:24 +0000

    Fix update_llc_test_checks.py ARM parsing
    When I tried running the script, the ARM regex parser could not parse
    my code. It failed because the .Lfunc_end line has a comment at the
    end of it, so this commit removes the newline at the end of the regex.
    
    Patch by Joel Galenson!
    
    Differential Revision: https://reviews.llvm.org/D35641
    
    llvm-svn: 309457

--- llvm/utils/update_llc_test_checks.py
+++ llvm/utils/update_llc_test_checks.py
@@ -49,7 +49,7 @@ ASM_FUNCTION_ARM_RE = re.compile(
         r'^(?P<func>[0-9a-zA-Z_]+):\n' # f: (name of function)
         r'\s+\.fnstart\n' # .fnstart
         r'(?P<body>.*?)\n' # (body of the function)
-        r'.Lfunc_end[0-9]+:\n', # .Lfunc_end0:
+        r'.Lfunc_end[0-9]+:', # .Lfunc_end0:
         flags=(re.M | re.S))
 
 RUN_LINE_RE = re.compile('^\s*;\s*RUN:\s*(.*)$')
