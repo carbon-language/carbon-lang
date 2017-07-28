a64ba70183c36c3be8bfcdadc8c4900a74bc4550
Francois Ferrand <thetypz@gmail.com>
2017-07-28 07:56:18 +0000

    clang-format: merge short case labels with trailing comments
    Summary:
    Allow merging short case labels when they actually end with a comment
    (like a comment after the ``break``) and when followed by switch-level
    comments (e.g. aligned with next case):
    
      switch(a) {
      case 0: break; // comment at end of case
      case 1: return value;
      // comment related to next case
      // comment related to next case
      case 2:
      }
    
    Reviewers: krasimir, djasper
    
    Reviewed By: krasimir
    
    Subscribers: klimek, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D35557
    
    llvm-svn: 309370

--- clang/lib/Format/UnwrappedLineFormatter.cpp
+++ clang/lib/Format/UnwrappedLineFormatter.cpp
@@ -382,7 +382,9 @@ private:
       return 0;
     unsigned NumStmts = 0;
     unsigned Length = 0;
+    bool EndsWithComment = false;
     bool InPPDirective = I[0]->InPPDirective;
+    const unsigned Level = I[0]->Level;
     for (; NumStmts < 3; ++NumStmts) {
       if (I + 1 + NumStmts == E)
         break;
@@ -392,9 +394,26 @@ private:
       if (Line->First->isOneOf(tok::kw_case, tok::kw_default, tok::r_brace))
         break;
       if (Line->First->isOneOf(tok::kw_if, tok::kw_for, tok::kw_switch,
-                               tok::kw_while, tok::comment) ||
-          Line->Last->is(tok::comment))
+                               tok::kw_while) ||
+          EndsWithComment)
         return 0;
+      if (Line->First->is(tok::comment)) {
+        if (Level != Line->Level)
+          return 0;
+        SmallVectorImpl<AnnotatedLine *>::const_iterator J = I + 2 + NumStmts;
+        for (; J != E; ++J) {
+          Line = *J;
+          if (Line->InPPDirective != InPPDirective)
+            break;
+          if (Line->First->isOneOf(tok::kw_case, tok::kw_default, tok::r_brace))
+            break;
+          if (Line->First->isNot(tok::comment) || Level != Line->Level)
+            return 0;
+        }
+        break;
+      }
+      if (Line->Last->is(tok::comment))
+        EndsWithComment = true;
       Length += I[1 + NumStmts]->Last->TotalLength + 1; // 1 for the space.
     }
     if (NumStmts == 0 || NumStmts == 3 || Length > Limit)
