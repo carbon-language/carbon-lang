ce5e65ee93ea171e98d7d5a05a69b4d94b225c7a
Kostya Serebryany <kcc@google.com>
2017-07-28 01:38:43 +0000

    [sanitizer-coverage] add a run-time test for  -fsanitize-coverage=inline-8bit-counters,pc-table
    llvm-svn: 309351

--- compiler-rt/test/sanitizer_common/TestCases/sanitizer_coverage_inline8bit_counter.cc
+++ compiler-rt/test/sanitizer_common/TestCases/sanitizer_coverage_inline8bit_counter.cc
@@ -1,11 +1,12 @@
-// Tests -fsanitize-coverage=inline-8bit-counters
+// Tests -fsanitize-coverage=inline-8bit-counters,pc-table
 //
 // REQUIRES: has_sancovcc,stable-runtime
 // UNSUPPORTED: i386-darwin
 //
-// RUN: %clangxx -O0 %s -fsanitize-coverage=inline-8bit-counters 2>&1
+// RUN: %clangxx -O0 %s -fsanitize-coverage=inline-8bit-counters,pc-table 2>&1
 
 #include <stdio.h>
+#include <stdint.h>
 #include <assert.h>
 
 const char *first_counter;
@@ -17,7 +18,19 @@ void __sanitizer_cov_8bit_counters_init(const char *start, const char *end) {
   first_counter = start;
 }
 
+uintptr_t FirstPC;
+
+extern "C" void __sanitizer_cov_pcs_init(const uint8_t *pcs_beg,
+                                         const uint8_t *pcs_end) {
+  const uintptr_t *B = (const uintptr_t *)pcs_beg;
+  const uintptr_t *E = (const uintptr_t *)pcs_end;
+  assert(B < E);
+  FirstPC = *B;
+}
+
+
 int main() {
   assert(first_counter);
   assert(*first_counter == 1);
+  assert(FirstPC == (uintptr_t)&main);
 }
