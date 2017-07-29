6dc0f5b9471ba20f27d0703ab729bbf6aafacc38
Vedant Kumar <vsk@apple.com>
2017-07-29 00:19:52 +0000

    [test] FileCheck-ify a test to avoid a spurious failure, NFC
    The path to one of my source trees contains 'builtin' as a substring, so
    this test failed. Fix it with FileCheck.
    
    llvm-svn: 309460

--- clang/test/CodeGen/2004-02-20-Builtins.c
+++ clang/test/CodeGen/2004-02-20-Builtins.c
@@ -1,5 +1,8 @@
-// RUN: %clang_cc1  %s -emit-llvm -o - | not grep builtin
+// RUN: %clang_cc1  %s -emit-llvm -o - | FileCheck %s
 double sqrt(double x);
+
+// CHECK-LABEL: @zsqrtxxx
+// CHECK-NOT: builtin
 void zsqrtxxx(float num) {
    num = sqrt(num);
 }
