4b4c47c406eb3ef3500240b6347c32ebb1c45e36
David Blaikie <dblaikie@gmail.com>
2017-07-30 08:12:05 +0000

    DebugInfo: Group member variable along with the rest
    Committed in r309498 I didn't spot where the rest of the private members
    were in DWARFContext at the time - group them up again.
    
    llvm-svn: 309506

--- llvm/include/llvm/DebugInfo/DWARF/DWARFContext.h
+++ llvm/include/llvm/DebugInfo/DWARF/DWARFContext.h
@@ -83,6 +83,7 @@ class DWARFContext : public DIContext {
   StringMap<std::weak_ptr<DWOFile>> DWOFiles;
   std::weak_ptr<DWOFile> DWP;
   bool CheckedForDWP = false;
+  std::string DWPName;
 
   /// Read compile units from the debug_info section (if necessary)
   /// and store them in CUs.
@@ -102,13 +103,12 @@ class DWARFContext : public DIContext {
 
 protected:
   std::unique_ptr<const DWARFObject> DObj;
-  std::string DWPName;
 
 public:
   DWARFContext(std::unique_ptr<const DWARFObject> DObj,
                std::string DWPName = "")
-      : DIContext(CK_DWARF), DObj(std::move(DObj)),
-        DWPName(std::move(DWPName)) {}
+      : DIContext(CK_DWARF), DWPName(std::move(DWPName)),
+        DObj(std::move(DObj)) {}
   DWARFContext(DWARFContext &) = delete;
   DWARFContext &operator=(DWARFContext &) = delete;
 
