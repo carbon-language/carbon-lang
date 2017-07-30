880e8788b10e63460dec286c9c3dd2afddbc50d3
Lama Saba <lama.saba@intel.com>
2017-07-30 20:12:17 +0000

    NFC: spell correction.
    On behalf of jbhateja
    
    Differential Revision: https://reviews.llvm.org/D35885
    
    llvm-svn: 309521

--- llvm/utils/TableGen/DAGISelMatcherGen.cpp
+++ llvm/utils/TableGen/DAGISelMatcherGen.cpp
@@ -305,7 +305,7 @@ void MatcherGen::EmitOperatorMatchCode(const TreePatternNode *N,
   const SDNodeInfo &CInfo = CGP.getSDNodeInfo(N->getOperator());
 
   // If this is an 'and R, 1234' where the operation is AND/OR and the RHS is
-  // a constant without a predicate fn that has more that one bit set, handle
+  // a constant without a predicate fn that has more than one bit set, handle
   // this as a special case.  This is usually for targets that have special
   // handling of certain large constants (e.g. alpha with it's 8/16/32-bit
   // handling stuff).  Using these instructions is often far more efficient
