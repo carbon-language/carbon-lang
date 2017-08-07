dc213718dbff594f7f7b0f3f052ad94066976389
Kamil Rytarowski <n54@gmx.com>
2017-08-07 10:59:44 +0000

    Add NetBSD support in sanitizer_test_utils.h
    Summary:
    NetBSD ships with printf_l(3) like FreeBSD.
    
    NetBSD does not ship with memalign, pvalloc, malloc with "usable size"
    and is the same here as Darwin, Android, FreeBSD and Windows.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, vitalybuka, kcc, fjricci, filcab
    
    Reviewed By: vitalybuka
    
    Subscribers: srhines, llvm-commits, emaste, kubamracek, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D36373
    
    llvm-svn: 310248

--- compiler-rt/lib/sanitizer_common/tests/sanitizer_test_utils.h
+++ compiler-rt/lib/sanitizer_common/tests/sanitizer_test_utils.h
@@ -101,8 +101,8 @@ static inline uint32_t my_rand() {
 # define SANITIZER_TEST_HAS_POSIX_MEMALIGN 0
 #endif
 
-#if !defined(__APPLE__) && !defined(__FreeBSD__) && \
-    !defined(__ANDROID__) && !defined(_WIN32)
+#if !defined(__APPLE__) && !defined(__FreeBSD__) && !defined(__ANDROID__) && \
+    !defined(__NetBSD__) && !defined(_WIN32)
 # define SANITIZER_TEST_HAS_MEMALIGN 1
 # define SANITIZER_TEST_HAS_PVALLOC 1
 # define SANITIZER_TEST_HAS_MALLOC_USABLE_SIZE 1
@@ -118,7 +118,7 @@ static inline uint32_t my_rand() {
 # define SANITIZER_TEST_HAS_STRNLEN 0
 #endif
 
-#if defined(__FreeBSD__)
+#if defined(__FreeBSD__) || defined(__NetBSD__)
 # define SANITIZER_TEST_HAS_PRINTF_L 1
 #else
 # define SANITIZER_TEST_HAS_PRINTF_L 0
