123f62d5155aa1ee59217ec4cc2612fa6a025572
Kamil Rytarowski <n54@gmx.com>
2017-08-07 23:34:45 +0000

    Add NetBSD support in asan_stack.h
    Summary:
    Part of the code inspired by the original work on libsanitizer in GCC 5.4 by Christos Zoulas.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, kcc, vitalybuka, filcab, fjricci
    
    Reviewed By: vitalybuka
    
    Subscribers: davide, kubamracek, llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D36377
    
    llvm-svn: 310322

--- compiler-rt/lib/asan/asan_stack.h
+++ compiler-rt/lib/asan/asan_stack.h
@@ -44,7 +44,8 @@ void GetStackTraceWithPcBpAndContext(BufferedStackTrace *stack, uptr max_depth,
       // On FreeBSD the slow unwinding that leverages _Unwind_Backtrace()
       // yields the call stack of the signal's handler and not of the code
       // that raised the signal (as it does on Linux).
-      if (SANITIZER_FREEBSD && t->isInDeadlySignal()) fast = true;
+      if ((SANITIZER_FREEBSD || SANITIZER_NETBSD) && t->isInDeadlySignal())
+        fast = true;
       uptr stack_top = t->stack_top();
       uptr stack_bottom = t->stack_bottom();
       ScopedUnwinding unwind_scope(t);
