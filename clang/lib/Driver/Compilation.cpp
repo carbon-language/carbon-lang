bcfdd7861e4f390e478751c06d678066049cd061
Gheorghe-Teodor Bercea <gheorghe-teod.bercea@ibm.com>
2017-08-07 18:43:37 +0000

    Non-functional change. Fix previous patch D34784.
    llvm-svn: 310282

--- clang/lib/Driver/Compilation.cpp
+++ clang/lib/Driver/Compilation.cpp
@@ -60,11 +60,15 @@ Compilation::getArgsForToolChain(const ToolChain *TC, StringRef BoundArch,
   DerivedArgList *&Entry = TCArgs[{TC, BoundArch, DeviceOffloadKind}];
   if (!Entry) {
     // Translate OpenMP toolchain arguments provided via the -Xopenmp-target flags.
-    Entry = TC->TranslateOpenMPTargetArgs(*TranslatedArgs, DeviceOffloadKind);
-    if (!Entry)
-      Entry = TranslatedArgs;
+    DerivedArgList *OpenMPArgs = TC->TranslateOpenMPTargetArgs(*TranslatedArgs,
+        DeviceOffloadKind);
+    if (!OpenMPArgs) {
+      Entry = TC->TranslateArgs(*TranslatedArgs, BoundArch, DeviceOffloadKind);
+    } else {
+      Entry = TC->TranslateArgs(*OpenMPArgs, BoundArch, DeviceOffloadKind);
+      delete OpenMPArgs;
+    }
 
-    Entry = TC->TranslateArgs(*Entry, BoundArch, DeviceOffloadKind);
     if (!Entry)
       Entry = TranslatedArgs;
   }
