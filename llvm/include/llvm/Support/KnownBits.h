0ff0d74187dae2a105499f7d8cecbc584ff40934
Craig Topper <craig.topper@intel.com>
2017-08-07 22:35:55 +0000

    [KnownBits] Fix copy pasto in comment. NFC
    llvm-svn: 310320

--- llvm/include/llvm/Support/KnownBits.h
+++ llvm/include/llvm/Support/KnownBits.h
@@ -25,7 +25,7 @@ struct KnownBits {
   APInt One;
 
 private:
-  // Internal constructor for creating a ConstantRange from two APInts.
+  // Internal constructor for creating a KnownBits from two APInts.
   KnownBits(APInt Zero, APInt One)
       : Zero(std::move(Zero)), One(std::move(One)) {}
 
