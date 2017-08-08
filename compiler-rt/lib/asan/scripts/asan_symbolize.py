c5c9ba1ca965091b85e3cbe6710adbf17aed3402
Kamil Rytarowski <n54@gmx.com>
2017-08-08 20:50:07 +0000

    Add NetBSD support in asan_symbolize.py
    Summary:
    Part of the code inspired by the original work on libsanitizer in GCC 5.4 by Christos Zoulas.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, filcab, vitalybuka, kcc, fjricci
    
    Reviewed By: fjricci
    
    Subscribers: kubamracek, llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D36484
    
    llvm-svn: 310413

--- compiler-rt/lib/asan/scripts/asan_symbolize.py
+++ compiler-rt/lib/asan/scripts/asan_symbolize.py
@@ -280,7 +280,7 @@ def BreakpadSymbolizerFactory(binary):
 def SystemSymbolizerFactory(system, addr, binary, arch):
   if system == 'Darwin':
     return DarwinSymbolizer(addr, binary, arch)
-  elif system == 'Linux' or system == 'FreeBSD':
+  elif system == 'Linux' or system == 'FreeBSD' or system == 'NetBSD':
     return Addr2LineSymbolizer(binary)
 
 
@@ -370,7 +370,7 @@ class SymbolizationLoop(object):
       self.binary_name_filter = binary_name_filter
       self.dsym_hint_producer = dsym_hint_producer
       self.system = os.uname()[0]
-      if self.system not in ['Linux', 'Darwin', 'FreeBSD']:
+      if self.system not in ['Linux', 'Darwin', 'FreeBSD', 'NetBSD']:
         raise Exception('Unknown system')
       self.llvm_symbolizers = {}
       self.last_llvm_symbolizer = None
