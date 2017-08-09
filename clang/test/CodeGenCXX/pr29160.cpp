8b928feeb4d7b945302c7fc906d6722f512391cc
George Burgess IV <george.burgess.iv@gmail.com>
2017-08-09 05:20:05 +0000

    Attempt to appease msc buildbot
    It was timing out on this test, but for reasons unrelated to the
    specific bug it was testing for. Randomly breaking in gdb with `clang
    -target i686-windows -fmsc-version=1700` reveals *many* frames from
    MicrosoftCXXNameMangler. So, it would seem that some caching is needed
    there, as well...
    
    Fingers crossed that specifying a triple is sufficient to work around
    this.
    
    llvm-svn: 310444

--- clang/test/CodeGenCXX/pr29160.cpp
+++ clang/test/CodeGenCXX/pr29160.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++11 %s -o /dev/null -S
+// RUN: %clang_cc1 -std=c++11 -triple i686-linux %s -o /dev/null -S
 //
 // This test's failure mode is running ~forever. (For some value of "forever"
 // that's greater than 25 minutes on my machine)
