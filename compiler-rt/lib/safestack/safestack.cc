fbd51b954da5626d8aec17cfaa38bfa66600628a
Kamil Rytarowski <n54@gmx.com>
2017-08-10 18:40:09 +0000

    Enable SafeStack on NetBSD
    Summary:
    make check-safestack:
    
    -- Testing: 8 tests, 8 threads --
    Testing: 0 .. 10.. 20.. 30.. 40.. 50.. 60.. 70.. 80.. 90..
    Testing Time: 0.44s
      Expected Passes    : 7
      Unsupported Tests  : 1
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, vitalybuka, kcc, fjricci, filcab
    
    Reviewed By: vitalybuka
    
    Subscribers: mgorny, llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D36542
    
    llvm-svn: 310646

--- compiler-rt/lib/safestack/safestack.cc
+++ compiler-rt/lib/safestack/safestack.cc
@@ -21,7 +21,9 @@
 #include <unistd.h>
 #include <sys/resource.h>
 #include <sys/types.h>
+#if !defined(__NetBSD__)
 #include <sys/user.h>
+#endif
 
 #include "interception/interception.h"
 #include "sanitizer_common/sanitizer_common.h"
