7b5eaa6f62752bad52d041427437bb38dcfdf137
Philip Pfaffe <philip.pfaffe@gmail.com>
2017-08-10 15:40:36 +0000

    Add missing license text to two headers. NFC.
    llvm-svn: 310612

--- polly/include/polly/CodePreparation.h
+++ polly/include/polly/CodePreparation.h
@@ -1,3 +1,16 @@
+//===- polly/ScopPreparation.h - Code preparation pass ----------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// Prepare the Function for polyhedral codegeneration.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef POLLY_CODEPREPARATION_H
 #define POLLY_CODEPREPARATION_H
 
--- polly/include/polly/JSONExporter.h
+++ polly/include/polly/JSONExporter.h
@@ -1,3 +1,12 @@
+//===- polly/JSONExporter.h - Import/Export to/from jscop files.-*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+
 #ifndef POLLY_JSONEXPORTER_H
 #define POLLY_JSONEXPORTER_H
 
