a24e4cda006772f27a3b484959207ef1d34e2056
Ahmed Bougacha <ahmed.bougacha@gmail.com>
2017-08-10 21:23:00 +0000

    Revert "[AsmParser] Hash is not a comment on some targets"
    This reverts commit r310457.
    
    It causes clang-produced IR to fail llvm codegen.
    
    llvm-svn: 310662

--- llvm/lib/MC/MCParser/MCAsmParser.cpp
+++ llvm/lib/MC/MCParser/MCAsmParser.cpp
@@ -40,6 +40,11 @@ bool MCAsmParser::parseTokenLoc(SMLoc &Loc) {
 }
 
 bool MCAsmParser::parseEOL(const Twine &Msg) {
+  if (getTok().getKind() == AsmToken::Hash) {
+    StringRef CommentStr = parseStringToEndOfStatement();
+    getLexer().Lex();
+    getLexer().UnLex(AsmToken(AsmToken::EndOfStatement, CommentStr));
+  }
   if (getTok().getKind() != AsmToken::EndOfStatement)
     return Error(getTok().getLoc(), Msg);
   Lex();
@@ -65,6 +70,9 @@ bool MCAsmParser::parseIntToken(int64_t &V, const Twine &Msg) {
 
 bool MCAsmParser::parseOptionalToken(AsmToken::TokenKind T) {
   bool Present = (getTok().getKind() == T);
+  // if token is EOL and current token is # this is an EOL comment.
+  if (getTok().getKind() == AsmToken::Hash && T == AsmToken::EndOfStatement)
+    Present = true;
   if (Present)
     parseToken(T);
   return Present;
