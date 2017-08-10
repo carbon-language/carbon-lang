c717041e847db8bd7dfee61ee2a00c3653704651
Simon Atanasyan <simon@atanasyan.com>
2017-08-10 15:42:31 +0000

    [mips] Group all `-mabicalls` related checks in the single place. NFC
    llvm-svn: 310615

--- clang/test/Driver/mips-abicalls-warning.c
+++ clang/test/Driver/mips-abicalls-warning.c
@@ -1,3 +1,9 @@
 // REQUIRES: mips-registered-target
 // RUN: %clang -### -c -target mips64-mti-elf -fno-PIC -mabicalls %s 2>&1 | FileCheck %s
 // CHECK: warning: ignoring '-mabicalls' option as it cannot be used with non position-independent code and the N64 ABI
+
+// RUN: %clang -### -c -target mips-mti-elf -mlong-calls %s 2>&1 | FileCheck -check-prefix=LONGCALL-IMP %s
+// LONGCALL-IMP: warning: ignoring '-mlong-calls' option as it is not currently supported with the implicit usage of -mabicalls
+
+// RUN: %clang -### -c -target mips-mti-elf -mlong-calls -mabicalls %s 2>&1 | FileCheck -check-prefix=LONGCALL-EXP %s
+// LONGCALL-EXP: warning: ignoring '-mlong-calls' option as it is not currently supported with -mabicalls
--- clang/test/Driver/mips-longcalls-warning.c
+++
@@ -1,6 +0,0 @@
-// REQUIRES: mips-registered-target
-// RUN: %clang -### -c -target mips-mti-elf -mlong-calls %s 2>&1 | FileCheck -check-prefix=IMPLICIT %s
-// IMPLICIT: warning: ignoring '-mlong-calls' option as it is not currently supported with the implicit usage of -mabicalls
-
-// RUN: %clang -### -c -target mips-mti-elf -mlong-calls -mabicalls %s 2>&1 | FileCheck -check-prefix=EXPLICIT %s
-// EXPLICIT: warning: ignoring '-mlong-calls' option as it is not currently supported with -mabicalls
