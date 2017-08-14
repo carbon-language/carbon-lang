c3f6b2806f2a73e3b98d6c2e3aa36b0e8795eef7
Simon Dardis <simon.dardis@imgtec.com>
2017-08-14 16:20:33 +0000

    Revert "Reland "[mips][mt][6/7] Add support for mftr, mttr instructions.""
    This reverts r310834. It didn't pacify the buildbot, FileCheck is still
    crashing.
    
    llvm-svn: 310854

--- llvm/test/MC/Mips/mt/invalid.s
+++ llvm/test/MC/Mips/mt/invalid.s
@@ -12,17 +12,3 @@
   evpe 4                  # CHECK: error: invalid operand for instruction
   evpe $4, $5             # CHECK: error: invalid operand for instruction
   evpe $5, 0($5)          # CHECK: error: invalid operand for instruction
-  mftr $4, 0($5), 0, 0, 0 # CHECK: error:
-  mftr $4, $5, 2, 0, 0    # CHECK: error:
-  mftr $4, $5, -1, 0, 0   # CHECK: error:
-  mftr $4, $5, 0, 8, 0    # CHECK: error:
-  mftr $4, $5, 0, -1, 0   # CHECK: error:
-  mftr $4, $4, 0, 0, 2    # CHECK: error:
-  mftr $4, $5, 0, 0, -1   # CHECK: error:
-  mttr $4, 0($5), 0, 0, 0 # CHECK: error:
-  mttr $4, $5, 2, 0, 0    # CHECK: error:
-  mttr $4, $5, -1, 0, 0   # CHECK: error:
-  mttr $4, $5, 0, 8, 0    # CHECK: error:
-  mttr $4, $5, 0, -1, 0   # CHECK: error:
-  mttr $4, $4, 0, 0, 2    # CHECK: error:
-  mttr $4, $5, 0, 0, -1   # CHECK: error:
--- llvm/test/MC/Mips/mt/mftr-mttr-reserved-valid.s
+++
@@ -1,8 +0,0 @@
-# RUN: llvm-mc -arch=mips -mcpu=mips32r2 -mattr=+mt -show-encoding < %s | FileCheck %s
-
-# The selector value and register values here are marked as reserved in the
-# documentation, but GAS accepts them without warning.
-  mftr  $31, $31, 1, 1, 0       # CHECK: mftr  $ra, $ra, 1, 1, 0   # encoding: [0x41,0x1f,0xf8,0x21]
-  mttr  $31, $31, 1, 1, 0       # CHECK: mttr  $ra, $ra, 1, 1, 0   # encoding: [0x41,0x9f,0xf8,0x21]
-  mftr  $31, $13, 1, 6, 0       # CHECK: mftr  $ra, $13, 1, 6, 0   # encoding: [0x41,0x0d,0xf8,0x26]
-  mttr  $31, $13, 1, 6, 0       # CHECK: mttr  $ra, $13, 1, 6, 0   # encoding: [0x41,0x9f,0x68,0x26]
