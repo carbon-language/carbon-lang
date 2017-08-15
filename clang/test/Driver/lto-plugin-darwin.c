8afdd2f81e575e030e9478e08417753f3bf92b4e
Dan Albert <danalbert@google.com>
2017-08-15 23:57:32 +0000

    Revert "Add a target for new LTO plugin name tests."
    llvm-svn: 310976

--- clang/test/Driver/lto-plugin-darwin.c
+++ clang/test/Driver/lto-plugin-darwin.c
@@ -1,6 +1,6 @@
 // Check that Darwin uses LLVMgold.dylib.
 // REQUIRES: system-darwin
-// RUN: %clang -target x86_64-unknown-linux -### %s -flto 2>&1 \
+// RUN: %clang -### %s -flto 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-LTO-PLUGIN %s
 //
 // CHECK-LTO-PLUGIN: "-plugin" "{{.*}}/LLVMgold.dylib"
--- clang/test/Driver/lto-plugin-linux.c
+++ clang/test/Driver/lto-plugin-linux.c
@@ -1,6 +1,6 @@
 // Check that non-Windows, non-Darwin OSs use LLVMgold.so.
 // REQUIRES: !system-darwin && !system-windows
-// RUN: %clang -target x86_64-unknown-linux -### %s -flto 2>&1 \
+// RUN: %clang -### %s -flto 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-LTO-PLUGIN %s
 //
 // CHECK-LTO-PLUGIN: "-plugin" "{{.*}}/LLVMgold.so"
