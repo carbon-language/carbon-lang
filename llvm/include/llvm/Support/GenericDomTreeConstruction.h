4552e9de9f3cd4cf4ffa9746ff865c88e2b1db59
Jakub Kuderski <kubakuderski@gmail.com>
2017-08-16 20:50:23 +0000

    [ADCE][Dominators] Teach ADCE to preserve dominators
    Summary:
    This patch teaches ADCE to preserve both DominatorTrees and PostDominatorTrees.
    
    I didn't notice any performance impact when bootstrapping clang with this patch.
    
    Reviewers: dberlin, chandlerc, sanjoy, davide, grosser, brzycki
    
    Reviewed By: davide
    
    Subscribers: grandinj, zhendongsu, llvm-commits, david2050
    
    Differential Revision: https://reviews.llvm.org/D35869
    
    llvm-svn: 311039

--- llvm/include/llvm/Support/GenericDomTreeConstruction.h
+++ llvm/include/llvm/Support/GenericDomTreeConstruction.h
@@ -914,7 +914,12 @@ struct SemiNCAInfo {
     if (!FromTN) return;
 
     const TreeNodePtr ToTN = DT.getNode(To);
-    assert(ToTN && "To already unreachable -- there is no edge to delete");
+    if (!ToTN) {
+      DEBUG(dbgs() << "\tTo (" << BlockNamePrinter(To)
+                   << ") already unreachable -- there is no edge to delete\n");
+      return;
+    }
+
     const NodePtr NCDBlock = DT.findNearestCommonDominator(From, To);
     const TreeNodePtr NCD = DT.getNode(NCDBlock);
 
