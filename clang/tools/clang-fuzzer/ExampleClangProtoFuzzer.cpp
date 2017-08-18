6178cfaf7bd4144371b75279e903f6fcc405b6f3
Matt Morehouse <mascasa@google.com>
2017-08-18 18:34:39 +0000

    [clang-proto-fuzzer] Allow user-specified compiler arguments.
    Summary:
    Arguments can be specified after -ignore_remaining_args=1 to modify
    the compiler invocation.  For example, the following command-line
    will fuzz LLVM with a custom optimization level and target triple:
      clang-proto-fuzzer CORPUS/ -ignore_remaining_args -O3 \
          -triple arm64-apple-ios9
    
    Reviewers: vitalybuka, kcc
    
    Reviewed By: vitalybuka
    
    Subscribers: aemerson, cfe-commits, kristof.beyls
    
    Differential Revision: https://reviews.llvm.org/D36882
    
    llvm-svn: 311185

--- clang/tools/clang-fuzzer/ExampleClangProtoFuzzer.cpp
+++ clang/tools/clang-fuzzer/ExampleClangProtoFuzzer.cpp
@@ -20,9 +20,25 @@
 
 #include "src/libfuzzer/libfuzzer_macro.h"
 
+#include <cstring>
+
 using namespace clang_fuzzer;
+
+static std::vector<const char *> CLArgs;
+
+extern "C" int LLVMFuzzerInitialize(int *argc, char ***argv) {
+  CLArgs.push_back("-O2");
+  for (int I = 1; I < *argc; I++) {
+    if (strcmp((*argv)[I], "-ignore_remaining_args=1") == 0) {
+      for (I++; I < *argc; I++)
+        CLArgs.push_back((*argv)[I]);
+      break;
+    }
+  }
+  return 0;
+}
 
 DEFINE_BINARY_PROTO_FUZZER(const Function& input) {
   auto S = FunctionToString(input);
-  HandleCXX(S, {"-O2"});
+  HandleCXX(S, CLArgs);
 }
