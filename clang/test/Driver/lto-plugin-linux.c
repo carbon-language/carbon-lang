79865d98d0abd002f93678824c19fd952954ae6d
Dan Albert <danalbert@google.com>
2017-08-22 21:16:22 +0000

    Degeneralize more tests.
    As before, not every platform supports LTO. Make sure the platform
    we're targeting is one that supports it (regardless of the *host*
    platform).
    
    llvm-svn: 311488

--- clang/test/Driver/lto-plugin-linux.c
+++ clang/test/Driver/lto-plugin-linux.c
@@ -1,6 +1,6 @@
 // Check that non-Windows, non-Darwin OSs use LLVMgold.so.
 // REQUIRES: !system-darwin && !system-windows
-// RUN: %clang -### %s -flto 2>&1 \
+// RUN: %clang -### %s -target x86_64-unknown-linux -flto 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-LTO-PLUGIN %s
 //
 // CHECK-LTO-PLUGIN: "-plugin" "{{.*}}/LLVMgold.so"
--- clang/test/Driver/lto-plugin-windows.c
+++ clang/test/Driver/lto-plugin-windows.c
@@ -1,6 +1,6 @@
 // Check that Windows uses LLVMgold.dll.
 // REQUIRES: system-windows
-// RUN: %clang -### %s -flto 2>&1 \
+// RUN: %clang -### %s -target x86_64-unknown-linux -flto 2>&1 \
 // RUN:   | FileCheck -check-prefix=CHECK-LTO-PLUGIN %s
 //
 // CHECK-LTO-PLUGIN: "-plugin" "{{.*}}\\LLVMgold.dll"
