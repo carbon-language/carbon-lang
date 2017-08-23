f8774f1d735253f54531b585915a7529837d70ce
Saleem Abdulrasool <compnerd@compnerd.org>
2017-08-23 16:50:27 +0000

    ARM: explicitly specify the 8-byte alignment
    It seems that GCC interprets `__attribute__((__aligned__))` as 8-byte
    alignment on ARM, but clang does not.  Explicitly specify the
    double-word alignment value to ensure that the structure is properly
    aligned.
    
    llvm-svn: 311574

--- libunwind/test/alignment.pass.cpp
+++ libunwind/test/alignment.pass.cpp
@@ -13,8 +13,16 @@
 
 #include <unwind.h>
 
-struct MaxAligned {} __attribute__((aligned));
-static_assert(alignof(_Unwind_Exception) == alignof(MaxAligned), "");
+// EHABI  : 8-byte aligned
+// itanium: largest supported alignment for the system
+#if defined(_LIBUNWIND_ARM_EHABI)
+static_assert(alignof(_Unwind_Control_Block) == 8,
+              "_Unwind_Control_Block must be double-word aligned");
+#else
+struct MaxAligned {} __attribute__((__aligned__));
+static_assert(alignof(_Unwind_Exception) == alignof(MaxAligned),
+              "_Unwind_Exception must be maximally aligned");
+#endif
 
 int main()
 {
