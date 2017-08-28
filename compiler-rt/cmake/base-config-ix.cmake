0e52de05db389f7efd26c8e5ad27064df9cb0c64
Michal Gorny <mgorny@gentoo.org>
2017-08-28 20:30:12 +0000

    Reland r311842 - [cmake] Remove i686 target that is duplicate to i386
    Remove the explicit i686 target that is completely duplicate to
    the i386 target, with the latter being used more commonly.
    
    1. The runtime built for i686 will be identical to the one built for
    i386.
    
    2. Supporting both -i386 and -i686 suffixes causes unnecessary confusion
    on the clang end which has to expect either of them.
    
    3. The checks are based on wrong assumption that __i686__ is defined for
    all newer x86 CPUs. In fact, it is only declared when -march=i686 is
    explicitly used. It is not available when a more specific (or newer)
    -march is used.
    
    Curious enough, if CFLAGS contain -march=i686, the runtime will be built
    both for i386 and i686. For any other value, only i386 variant will be
    built.
    
    Differential Revision: https://reviews.llvm.org/D26764
    
    llvm-svn: 311924

--- compiler-rt/cmake/base-config-ix.cmake
+++ compiler-rt/cmake/base-config-ix.cmake
@@ -139,10 +139,6 @@ macro(test_targets)
     elseif("${COMPILER_RT_DEFAULT_TARGET_ARCH}" MATCHES "i[2-6]86|x86|amd64")
       if(NOT MSVC)
         test_target_arch(x86_64 "" "-m64")
-        # FIXME: We build runtimes for both i686 and i386, as "clang -m32" may
-        # target different variant than "$CMAKE_C_COMPILER -m32". This part should
-        # be gone after we resolve PR14109.
-        test_target_arch(i686 __i686__ "-m32")
         test_target_arch(i386 __i386__ "-m32")
       else()
         if (CMAKE_SIZEOF_VOID_P EQUAL 4)
