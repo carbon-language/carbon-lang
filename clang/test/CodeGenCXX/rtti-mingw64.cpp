c6c5af75f2c84d1b6204c20f1f31f23ee78b446e
Martin Storsjo <martin@martin.st>
2017-09-01 06:41:55 +0000

    Reland r312224 - [ItaniumCXXABI] Always use linkonce_odr linkage for RTTI data on MinGW
    This fixes cases where dynamic classes produced RTTI data with
    external linkage, producing linker errors about duplicate symbols.
    
    This touches code close to what was changed in SVN r244266, but
    this change doesn't break the tests added in that revision.
    
    The previous version had missed to update CodeGenCXX/virt-dtor-key.cpp,
    which had a behaviour change only when running the testsuite on windows.
    
    Differential revision: https://reviews.llvm.org/D37327
    
    llvm-svn: 312306

--- clang/test/CodeGenCXX/rtti-mingw64.cpp
+++ clang/test/CodeGenCXX/rtti-mingw64.cpp
@@ -2,7 +2,12 @@
 struct A { int a; };
 struct B : virtual A { int b; };
 B b;
+class C {
+  virtual ~C();
+};
+C::~C() {}
 
+// CHECK: @_ZTI1C = linkonce_odr
 // CHECK: @_ZTI1B = linkonce_odr constant { i8*, i8*, i32, i32, i8*, i64 }
 // CHECK-SAME:  i8* bitcast (i8** getelementptr inbounds (i8*, i8** @_ZTVN10__cxxabiv121__vmi_class_type_infoE, i64 2) to i8*),
 // CHECK-SAME:  i8* getelementptr inbounds ([3 x i8], [3 x i8]* @_ZTS1B, i32 0, i32 0),
