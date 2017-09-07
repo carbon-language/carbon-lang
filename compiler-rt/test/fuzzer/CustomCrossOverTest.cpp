d0386fac269b2583a774b4dc8d88ede75fb96946
Kostya Serebryany <kcc@google.com>
2017-09-07 02:04:06 +0000

    [libFuzzer] simplify and re-enable CustomCrossOverTest
    llvm-svn: 312689

--- compiler-rt/test/fuzzer/CustomCrossOverTest.cpp
+++ compiler-rt/test/fuzzer/CustomCrossOverTest.cpp
@@ -1,7 +1,7 @@
 // This file is distributed under the University of Illinois Open Source
 // License. See LICENSE.TXT for details.
 
-// Simple test for a cutom mutator.
+// Simple test for a cutom crossover.
 #include <assert.h>
 #include <cstddef>
 #include <cstdint>
@@ -10,23 +10,18 @@
 #include <ostream>
 #include <random>
 #include <string.h>
+#include <functional>
 
-#include "FuzzerInterface.h"
-
-static const char *Separator = "-_^_-";
-static const char *Target = "012-_^_-abc";
-
-static volatile int sink;
+static const char *Separator = "-########-";
+static const char *Target = "A-########-B";
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   assert(Data);
   std::string Str(reinterpret_cast<const char *>(Data), Size);
+  static const size_t TargetHash = std::hash<std::string>{}(std::string(Target));
+  size_t StrHash = std::hash<std::string>{}(Str);
 
-  // Ensure that two different elements exist in the corpus.
-  if (Size && Data[0] == '0') sink++;
-  if (Size && Data[0] == 'a') sink--;
-
-  if (Str.find(Target) != std::string::npos) {
+  if (TargetHash == StrHash) {
     std::cout << "BINGO; Found the target, exiting\n" << std::flush;
     exit(1);
   }
@@ -37,28 +32,20 @@ extern "C" size_t LLVMFuzzerCustomCrossOver(const uint8_t *Data1, size_t Size1,
                                             const uint8_t *Data2, size_t Size2,
                                             uint8_t *Out, size_t MaxOutSize,
                                             unsigned int Seed) {
-  static bool Printed;
+  static size_t Printed;
   static size_t SeparatorLen = strlen(Separator);
 
-  if (!Printed) {
-    std::cerr << "In LLVMFuzzerCustomCrossover\n";
-    Printed = true;
-  }
+  if (Printed++ < 32)
+    std::cerr << "In LLVMFuzzerCustomCrossover " << Size1 << " " << Size2 << "\n";
 
-  std::mt19937 R(Seed);
-
-  size_t Offset1 = 0;
-  size_t Len1 = R() % (Size1 - Offset1);
-  size_t Offset2 = 0;
-  size_t Len2 = R() % (Size2 - Offset2);
-  size_t Size = Len1 + Len2 + SeparatorLen;
+  size_t Size = Size1 + Size2 + SeparatorLen;
 
   if (Size > MaxOutSize)
     return 0;
 
-  memcpy(Out, Data1 + Offset1, Len1);
-  memcpy(Out + Len1, Separator, SeparatorLen);
-  memcpy(Out + Len1 + SeparatorLen, Data2 + Offset2, Len2);
+  memcpy(Out, Data1, Size1);
+  memcpy(Out + Size1, Separator, SeparatorLen);
+  memcpy(Out + Size1 + SeparatorLen, Data2, Size2);
 
-  return Len1 + Len2 + SeparatorLen;
+  return Size;
 }
