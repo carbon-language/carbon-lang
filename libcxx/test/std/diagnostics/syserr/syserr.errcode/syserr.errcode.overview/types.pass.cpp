bc455478e10907c28270b4eb241d26c79595cfe8
Marshall Clow <mclow.lists@gmail.com>
2017-09-11 16:05:42 +0000

    Add include of <string> to <system_error>, since things in there return strings. Fixes PR#34529.
    llvm-svn: 312923

--- libcxx/test/std/diagnostics/syserr/syserr.errcode/syserr.errcode.overview/types.pass.cpp
+++ libcxx/test/std/diagnostics/syserr/syserr.errcode/syserr.errcode.overview/types.pass.cpp
@@ -7,6 +7,17 @@
 //
 //===----------------------------------------------------------------------===//
 
+// <system_error>
+// class error_code
+
+// Make sure that the error_code bits of <system_error> are self-contained.
+
+#include <system_error>
+
 int main()
 {
+    std::error_code x;
+    (void) x.category();   // returns a std::error_category &
+    (void) x.default_error_condition(); // std::error_condition
+    (void) x.message();    // returns a std::string
 }
--- libcxx/test/std/diagnostics/syserr/syserr.errcondition/syserr.errcondition.overview/types.pass.cpp
+++ libcxx/test/std/diagnostics/syserr/syserr.errcondition/syserr.errcondition.overview/types.pass.cpp
@@ -7,6 +7,16 @@
 //
 //===----------------------------------------------------------------------===//
 
+// <system_error>
+// class error_condition
+
+// Make sure that the error_condition bits of <system_error> are self-contained.
+
+#include <system_error>
+
 int main()
 {
+    std::error_condition x = std::errc(0);
+    (void) x.category();   // returns a std::error_condition &
+    (void) x.message();    // returns a std::string
 }
