f922501d98153df01ee12a0b8b7b12ecdef5bfe7
Martell Malone <martellmalone@gmail.com>
2017-09-12 22:32:02 +0000

    libcxx: fix merge_archives error variable name
    Differential Revision: https://reviews.llvm.org/D37728
    
    llvm-svn: 313084

--- libcxx/utils/merge_archives.py
+++ libcxx/utils/merge_archives.py
@@ -120,7 +120,7 @@ def main():
 
     files = glob.glob(os.path.join(temp_directory_root, '*.o*'))
     if not files:
-        print_and_exit('Failed to glob for %s' % glob_path)
+        print_and_exit('Failed to glob for %s' % temp_directory_root)
     cmd = [ar_exe, '-qc', args.output] + files
     execute_command_verbose(cmd, cwd=temp_directory_root, verbose=args.verbose)
 
