51529eb0c25ba48a83ea5f99b95708325d70c2c2
Robert Lougher <rob.lougher@gmail.com>
2017-09-12 18:23:15 +0000

    Revert "[DWARF] Incorrect prologue end line record."
    This reverts commit r313047 as it is causing buildbot failure (lldb inline
    stepping tests).
    
    llvm-svn: 313057

--- llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -1158,9 +1158,7 @@ void DwarfDebug::beginInstruction(const MachineInstr *MI) {
     return;
 
   // Check if source location changes, but ignore DBG_VALUE and CFI locations.
-  // If the instruction is part of the function frame setup code, do not emit
-  // any line record, as there is no correspondence with any user code.
-  if (MI->isMetaInstruction() || MI->getFlag(MachineInstr::FrameSetup))
+  if (MI->isMetaInstruction())
     return;
   const DebugLoc &DL = MI->getDebugLoc();
   // When we emit a line-0 record, we don't update PrevInstLoc; so look at
