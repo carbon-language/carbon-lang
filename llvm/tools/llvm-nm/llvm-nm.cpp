44550600f295b4bd3486da95507d37c600d1539e
Kevin Enderby <enderby@apple.com>
2017-09-13 21:01:49 +0000

    Fix a crash in llvm-nm for a bad Mach-O file that has an N_SECT type symbol and a zero n_sect value.
    The code in llvm-nm for Mach-O files to determine the section type for an
    N_SECT type symbol it will call getSymbolSection() and check for the error,
    but in the case the n_sect value is zero it will return section_end() (aka nullptr).
    And the code was using that and crashing instead of just returning a ’s’ for a
    section or printing (?,?) as it would if getSymbolSection() returned an error.
    
    rdar://33136604
    
    llvm-svn: 313193

--- llvm/tools/llvm-nm/llvm-nm.cpp
+++ llvm/tools/llvm-nm/llvm-nm.cpp
@@ -486,6 +486,10 @@ static void darwinPrintSymbol(SymbolicFile &Obj, SymbolListT::iterator I,
         break;
       }
       Sec = *SecOrErr;
+      if (Sec == MachO->section_end()) {
+        outs() << "(?,?) ";
+        break;
+      }
     } else {
       Sec = I->Section;
     }
@@ -997,6 +1001,8 @@ static char getSymbolNMTypeChar(MachOObjectFile &Obj, basic_symbol_iterator I) {
       return 's';
     }
     section_iterator Sec = *SecOrErr;
+    if (Sec == Obj.section_end())
+      return 's';
     DataRefImpl Ref = Sec->getRawDataRefImpl();
     StringRef SectionName;
     Obj.getSectionName(Ref, SectionName);
