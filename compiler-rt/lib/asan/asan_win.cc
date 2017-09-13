72e8b55bab3b45b74ae6aa0684fd64ec9aeeec5b
Vitaly Buka <vitalybuka@google.com>
2017-09-13 19:39:06 +0000

    [asan] Fix Windows build
    llvm-svn: 313177

--- compiler-rt/lib/asan/asan_win.cc
+++ compiler-rt/lib/asan/asan_win.cc
@@ -58,7 +58,7 @@ long __asan_unhandled_exception_filter(EXCEPTION_POINTERS *info) {
   // FIXME: Handle EXCEPTION_STACK_OVERFLOW here.
 
   SignalContext sig = SignalContext::Create(exception_record, context);
-  ReportDeadlySignal(exception_record->ExceptionCode, sig);
+  ReportDeadlySignal(sig);
   UNREACHABLE("returned from reporting deadly signal");
 }
 
