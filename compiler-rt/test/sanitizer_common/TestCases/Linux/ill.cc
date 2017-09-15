f716931bb27aebb67a0a1b79e84bf4c11d951694
Vitaly Buka <vitalybuka@google.com>
2017-09-15 00:02:30 +0000

    [lsan] Disable clang-format on few RUN: statements
    llvm-svn: 313321

--- compiler-rt/test/sanitizer_common/TestCases/Linux/ill.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/ill.cc
@@ -1,8 +1,12 @@
 // Test the handle_sigill option.
+
+// clang-format off
 // RUN: %clang %s -o %t -O1
 // RUN:                                not --crash %run %t 2>&1 | FileCheck --check-prefix=CHECK0 %s
 // RUN: %env_tool_opts=handle_sigill=0 not --crash %run %t 2>&1 | FileCheck --check-prefix=CHECK0 %s
 // RUN: %env_tool_opts=handle_sigill=1 not         %run %t 2>&1 | FileCheck --check-prefix=CHECK1 %s
+// clang-format on
+
 // FIXME: implement in other sanitizers, not just asan.
 // XFAIL: msan
 // XFAIL: lsan
--- compiler-rt/test/sanitizer_common/TestCases/Posix/dump_instruction_bytes.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/dump_instruction_bytes.cc
@@ -1,9 +1,12 @@
 // Check that sanitizer prints the faulting instruction bytes on
 // dump_instruction_bytes=1
+
+// clang-format off
 // RUN: %clangxx  %s -o %t
 // RUN: %env_tool_opts=dump_instruction_bytes=1 not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-DUMP
 // RUN: not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK-NODUMP
-//
+// clang-format on
+
 // REQUIRES: x86-target-arch
 // XFAIL: lsan, msan, tsan, ubsan
 
