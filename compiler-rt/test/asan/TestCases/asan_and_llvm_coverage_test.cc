581fbc61b2beb45c0c58140cc49641847792c083
Vitaly Buka <vitalybuka@google.com>
2017-09-16 03:26:03 +0000

    [asan] Enable asan_and_llvm_coverage_test.cc on Android
    Test just needs profile.
    
    llvm-svn: 313438

--- compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
+++ compiler-rt/test/asan/TestCases/asan_and_llvm_coverage_test.cc
@@ -1,6 +1,6 @@
 // RUN: %clangxx_asan -coverage -O0 %s -o %t
 // RUN: %env_asan_opts=check_initialization_order=1 %run %t 2>&1 | FileCheck %s
-// XFAIL: android
+
 // We don't really support running tests using profile runtime on Windows.
 // UNSUPPORTED: win32
 #include <stdio.h>
