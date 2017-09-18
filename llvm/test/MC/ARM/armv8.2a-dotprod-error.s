4e6df159621fd578e7380732b3965dc9eab4bbf4
Sjoerd Meijer <sjoerd.meijer@arm.com>
2017-09-18 14:17:57 +0000

    [ARM] Fix for indexed dot product instruction descriptions
    The indexed dot product instructions only accept the lower 16 D-registers as
    the indexed register, but we were e.g. incorrectly accepting:
    
    vudot.u8 d16,d16,d18[0]
    
    Differential Revision: https://reviews.llvm.org/D37968
    
    llvm-svn: 313531

--- llvm/test/MC/ARM/armv8.2a-dotprod-error.s
+++ llvm/test/MC/ARM/armv8.2a-dotprod-error.s
@@ -3,12 +3,34 @@
 // RUN: not llvm-mc -triple thumb -mattr=+dotprod -show-encoding < %s 2> %t
 // RUN: FileCheck --check-prefix=CHECK-ERROR < %t %s
 
+// Only indices 0 an 1 should be accepted:
+
 vudot.u8 d0, d1, d2[2]
 vsdot.s8 d0, d1, d2[2]
 vudot.u8 q0, q1, d4[2]
 vsdot.s8 q0, q1, d4[2]
 
 // CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vudot.u8 d0, d1, d2[2]
+// CHECK-ERROR:                    ^
 // CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vsdot.s8 d0, d1, d2[2]
+// CHECK-ERROR:                    ^
 // CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vudot.u8 q0, q1, d4[2]
+// CHECK-ERROR:                    ^
 // CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vsdot.s8 q0, q1, d4[2]
+// CHECK-ERROR:                    ^
+
+// Only the lower 16 D-registers should be accepted:
+
+vudot.u8 q0, q1, d16[0]
+vsdot.s8 q0, q1, d16[0]
+
+// CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vudot.u8 q0, q1, d16[0]
+// CHECK-ERROR:                     ^
+// CHECK-ERROR: error: invalid operand for instruction
+// CHECK-ERROR: vsdot.s8 q0, q1, d16[0]
+// CHECK-ERROR:                     ^
