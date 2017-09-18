1492cb89cabd1c984c9f0940b406acf59f56d705
Tamas Berghammer <tberghammer@google.com>
2017-09-18 10:24:48 +0000

    Fix Linux remote debugging after r313442
    On Linux lldb-server sends an OK response to qfThreadInfo if no process
    is started yet. I don't know why would LLDB issue a qfThreadInfo packet
    before starting a process but creating a fake thread ID in case of an
    OK or Error respoinse sounds bad anyway so lets not do it.
    
    llvm-svn: 313525

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunicationClient.cpp
@@ -2624,7 +2624,8 @@ size_t GDBRemoteCommunicationClient::GetCurrentThreadIDs(
      * tid.
      * Assume pid=tid=1 in such cases.
     */
-    if (thread_ids.size() == 0 && IsConnected()) {
+    if ((response.IsUnsupportedResponse() || response.IsNormalResponse()) &&
+        thread_ids.size() == 0 && IsConnected()) {
       thread_ids.push_back(1);
     }
   } else {
