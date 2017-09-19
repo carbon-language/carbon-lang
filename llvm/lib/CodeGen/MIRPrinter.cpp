d652aeb1443619f237dba9c7c31459325ed75ab3
Quentin Colombet <qcolombet@apple.com>
2017-09-19 23:34:12 +0000

    [MIRPrinter] Print empty successor lists when they cannot be guessed
    This re-applies commit r313685, this time with the proper updates to
    the test cases.
    
    Original commit message:
    Unreachable blocks in the machine instr representation are these
    weird empty blocks with no successors.
    The MIR printer used to not print empty lists of successors. However,
    the MIR parser now treats non-printed list of successors as "please
    guess it for me". As a result, the parser tries to guess the list of
    successors and given the block is empty, just assumes it falls through
    the next block (if any).
    
    For instance, the following test case used to fail the verifier.
    The MIR printer would print
    
             entry
            /      \
       true (def)   false (no list of successors)
           |
     split.true (use)
    
    The MIR parser would understand this:
    
             entry
            /      \
       true (def)   false
           |        /  <-- invalid edge
     split.true (use)
    
    Because of the invalid edge, we get the "def does not
    dominate all uses" error.
    
    The fix consists in printing empty successor lists, so that the parser
    knows what to do for unreachable blocks.
    
    rdar://problem/34022159
    
    llvm-svn: 313696

--- llvm/lib/CodeGen/MIRPrinter.cpp
+++ llvm/lib/CodeGen/MIRPrinter.cpp
@@ -598,8 +598,14 @@ void MIPrinter::print(const MachineBasicBlock &MBB) {
   bool HasLineAttributes = false;
   // Print the successors
   bool canPredictProbs = canPredictBranchProbabilities(MBB);
-  if (!MBB.succ_empty() && (!SimplifyMIR || !canPredictProbs ||
-                            !canPredictSuccessors(MBB))) {
+  // Even if the list of successors is empty, if we cannot guess it,
+  // we need to print it to tell the parser that the list is empty.
+  // This is needed, because MI model unreachable as empty blocks
+  // with an empty successor list. If the parser would see that
+  // without the successor list, it would guess the code would
+  // fallthrough.
+  if ((!MBB.succ_empty() && !SimplifyMIR) || !canPredictProbs ||
+      !canPredictSuccessors(MBB)) {
     OS.indent(2) << "successors: ";
     for (auto I = MBB.succ_begin(), E = MBB.succ_end(); I != E; ++I) {
       if (I != MBB.succ_begin())
