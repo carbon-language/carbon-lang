6888dbcda7890b28ae31056cf72addbff5a381a2
Quentin Colombet <qcolombet@apple.com>
2017-09-19 22:03:50 +0000

    Revert "[MIRPrinter] Print empty successor lists when they cannot be guessed"
    This reverts commit r313685.
    
    I thought I had ran ninja check, but apparently I didn't...
    Need to update a bunch of mir tests.
    
    llvm-svn: 313686

--- llvm/lib/CodeGen/MIRPrinter.cpp
+++ llvm/lib/CodeGen/MIRPrinter.cpp
@@ -598,7 +598,8 @@ void MIPrinter::print(const MachineBasicBlock &MBB) {
   bool HasLineAttributes = false;
   // Print the successors
   bool canPredictProbs = canPredictBranchProbabilities(MBB);
-  if (!SimplifyMIR || !canPredictProbs || !canPredictSuccessors(MBB)) {
+  if (!MBB.succ_empty() && (!SimplifyMIR || !canPredictProbs ||
+                            !canPredictSuccessors(MBB))) {
     OS.indent(2) << "successors: ";
     for (auto I = MBB.succ_begin(), E = MBB.succ_end(); I != E; ++I) {
       if (I != MBB.succ_begin())
