01a409520b39946bd4c7e86f83bc2d01d8c839d4
Rafael Espindola <rafael.espindola@gmail.com>
2017-09-20 18:56:08 +0000

    Consider only A.Sec in moveAbsRight.
    The idea of this function is to simplify the implementation of binary
    operators like add.
    
    A value might be absolute because of an ABSOLUTE expression, but it
    still depends on the value of a section and we might not be able to
    evaluate it early. We should keep such values on the LHS, so that we
    can delay the evaluation.
    
    We can now handle both "1 + ABSOLUTE(foo)" and "ABSOLUTE(foo) + 1".
    
    llvm-svn: 313794

--- lld/test/ELF/linkerscript/early-assign-symbol.s
+++ lld/test/ELF/linkerscript/early-assign-symbol.s
@@ -1,15 +1,16 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
 
-# RUN: echo "SECTIONS { aaa = ABSOLUTE(foo - 1) + 1; .text  : { *(.text*) } }" > %t2.script
-# RUN: not ld.lld -o %t --script %t2.script %t.o 2>&1 | FileCheck %s
-
 # RUN: echo "SECTIONS { aaa = foo | 1; .text  : { *(.text*) } }" > %t3.script
 # RUN: not ld.lld -o %t --script %t3.script %t.o 2>&1 | FileCheck %s
 
 # CHECK: error: {{.*}}.script:1: unable to evaluate expression: input section .text has no output section assigned
 
 # Simple cases that we can handle.
+
+# RUN: echo "SECTIONS { aaa = ABSOLUTE(foo - 1) + 1; .text  : { *(.text*) } }" > %t.script
+# RUN: ld.lld -o %t --script %t.script %t.o
+# RUN: llvm-objdump -t %t | FileCheck --check-prefix=VAL %s
 
 # RUN: echo "SECTIONS { aaa = 1 + ABSOLUTE(foo - 1); .text  : { *(.text*) } }" > %t.script
 # RUN: ld.lld -o %t --script %t.script %t.o
