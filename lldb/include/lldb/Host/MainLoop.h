4ce010e7753073622964a339365306bd44962bfb
Eugene Zemtsov <ezemtsov@google.com>
2017-09-20 00:39:04 +0000

    Signal polling is supported with pselect
    Older Android API levels don't have ppoll, but LLDB works just fine,
    since on Android it always uses pselect anyway.
    
    llvm-svn: 313704

--- lldb/include/lldb/Host/MainLoop.h
+++ lldb/include/lldb/Host/MainLoop.h
@@ -15,7 +15,12 @@
 #include "llvm/ADT/DenseMap.h"
 #include <csignal>
 
-#if !HAVE_PPOLL && !HAVE_SYS_EVENT_H
+#ifdef __ANDROID__
+#define FORCE_PSELECT
+#endif
+
+#if !defined(HAVE_PPOLL) && !defined(HAVE_SYS_EVENT_H) && \
+    !defined(FORCE_PSELECT)
 #define SIGNAL_POLLING_UNSUPPORTED 1
 #endif
 
