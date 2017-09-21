7a256839d9e391bdc56fd3e3cb33c6ad6504df66
George Karpenkov <ekarpenkov@apple.com>
2017-09-21 21:47:13 +0000

    [Analyzer] Remove dead code from CmpRuns.py.
    Differential Revision: https://reviews.llvm.org/D38003
    
    llvm-svn: 313923

--- clang/utils/analyzer/CmpRuns.py
+++ clang/utils/analyzer/CmpRuns.py
@@ -89,30 +89,6 @@ class AnalysisDiagnostic:
     def getRawData(self):
         return self._data
 
-class multidict:
-    def __init__(self, elts=()):
-        self.data = {}
-        for key,value in elts:
-            self[key] = value
-
-    def __getitem__(self, item):
-        return self.data[item]
-    def __setitem__(self, key, value):
-        if key in self.data:
-            self.data[key].append(value)
-        else:
-            self.data[key] = [value]
-    def items(self):
-        return self.data.items()
-    def values(self):
-        return self.data.values()
-    def keys(self):
-        return self.data.keys()
-    def __len__(self):
-        return len(self.data)
-    def get(self, key, default=None):
-        return self.data.get(key, default)
-
 class CmpOptions:
     def __init__(self, verboseLog=None, rootA="", rootB=""):
         self.rootA = rootA
