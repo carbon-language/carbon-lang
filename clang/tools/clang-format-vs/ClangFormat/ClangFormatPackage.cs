978419bf37546da788a7042d8de4044196c1d515
Hans Wennborg <hans@hanshq.net>
2017-09-22 21:47:39 +0000

    clang-format plugin: Add missing NL (new line) at EOF (end of file)
    clang-format.exe removes trailing new lines at end of file.
    
    However, if no NL is found at EOF one should be added.
    
    Patch by Teodor MICU!
    
    Differential Revision: https://reviews.llvm.org/D37732
    
    llvm-svn: 314033

--- clang/tools/clang-format-vs/ClangFormat/ClangFormatPackage.cs
+++ clang/tools/clang-format-vs/ClangFormat/ClangFormatPackage.cs
@@ -326,7 +326,13 @@ namespace LLVM.ClangFormat
 
             string filePath = Vsix.GetDocumentPath(view);
             var path = Path.GetDirectoryName(filePath);
+
             string text = view.TextBuffer.CurrentSnapshot.GetText();
+            if (!text.EndsWith(Environment.NewLine))
+            {
+                view.TextBuffer.Insert(view.TextBuffer.CurrentSnapshot.Length, Environment.NewLine);
+                text += Environment.NewLine;
+            }
 
             RunClangFormatAndApplyReplacements(text, 0, text.Length, path, filePath, options, view);
         }
