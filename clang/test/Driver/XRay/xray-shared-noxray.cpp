e935cbe86fb7d3981ddd109c7d12285b7297f7a4
Dean Michael Berris <dberris@google.com>
2017-09-26 03:45:37 +0000

    [XRay] Stop running tests for 'amd64', and remove -fPIE from tests.
    Follow-up to D38226.
    
    llvm-svn: 314189

--- clang/test/Driver/XRay/xray-shared-noxray.cpp
+++ clang/test/Driver/XRay/xray-shared-noxray.cpp
@@ -2,8 +2,6 @@
 // RUN:     FileCheck %s --check-prefix=SHARED
 // RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s 2>&1 -DMAIN | \
 // RUN:     FileCheck %s --check-prefix=STATIC
-// RUN: %clangxx -static -fPIE -o /dev/null -v -fxray-instrument %s 2>&1 \
-// RUN:     -DMAIN | FileCheck %s --check-prefix=STATIC
 //
 // SHARED-NOT: {{clang_rt\.xray-}}
 // STATIC: {{clang_rt\.xray-}}
