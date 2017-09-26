c67811ce1fc334f271a1ebbea36742a1756ec711
Dean Michael Berris <dberris@google.com>
2017-09-26 03:18:11 +0000

    [XRay][Driver] Do not link in XRay runtime in shared libs
    Summary:
    This change ensures that we don't link in the XRay runtime when building
    shared libraries with clang. This doesn't prevent us from building
    shared libraris tht have XRay instrumentation sleds, but it does prevent
    us from linking in the static XRay runtime into a shared library.
    
    The XRay runtime currently doesn't support dynamic registration of
    instrumentation sleds in shared objects, which we'll start enabling in
    the future. That work has to happen in the back-end and in the runtime.
    
    Reviewers: rnk, pelikan
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D38226
    
    llvm-svn: 314188

--- clang/lib/Driver/ToolChains/Gnu.cpp
+++ clang/lib/Driver/ToolChains/Gnu.cpp
@@ -206,6 +206,10 @@ void tools::gcc::Linker::RenderExtraToolArgs(const JobAction &JA,
 
 static bool addXRayRuntime(const ToolChain &TC, const ArgList &Args,
                            ArgStringList &CmdArgs) {
+  // Do not add the XRay runtime to shared libraries.
+  if (Args.hasArg(options::OPT_shared))
+    return false;
+
   if (Args.hasFlag(options::OPT_fxray_instrument,
                    options::OPT_fnoxray_instrument, false)) {
     CmdArgs.push_back("-whole-archive");
@@ -213,6 +217,7 @@ static bool addXRayRuntime(const ToolChain &TC, const ArgList &Args,
     CmdArgs.push_back("-no-whole-archive");
     return true;
   }
+
   return false;
 }
 
---
+++ clang/test/Driver/XRay/xray-shared-noxray.cpp
@@ -0,0 +1,16 @@
+// RUN: %clangxx -shared -fPIC -o /dev/null -v -fxray-instrument %s 2>&1 | \
+// RUN:     FileCheck %s --check-prefix=SHARED
+// RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s 2>&1 -DMAIN | \
+// RUN:     FileCheck %s --check-prefix=STATIC
+// RUN: %clangxx -static -fPIE -o /dev/null -v -fxray-instrument %s 2>&1 \
+// RUN:     -DMAIN | FileCheck %s --check-prefix=STATIC
+//
+// SHARED-NOT: {{clang_rt\.xray-}}
+// STATIC: {{clang_rt\.xray-}}
+//
+// REQUIRES: linux
+int foo() { return 42; }
+
+#ifdef MAIN
+int main() { return foo(); }
+#endif
