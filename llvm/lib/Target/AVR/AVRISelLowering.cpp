dada014781e8277ddc829e47aa337ab9792d8485
Dylan McKay <me@dylanmckay.io>
2017-09-26 00:51:03 +0000

    [AVR] When lowering shifts into loops, put newly generated MBBs in the same spot as the original MBB
    Discovered in avr-rust/rust#62
    https://github.com/avr-rust/rust/issues/62
    
    Patch by Gergo Erdi.
    
    llvm-svn: 314180

--- llvm/lib/Target/AVR/AVRISelLowering.cpp
+++ llvm/lib/Target/AVR/AVRISelLowering.cpp
@@ -1469,8 +1469,10 @@ MachineBasicBlock *AVRTargetLowering::insertShift(MachineInstr &MI,
   }
 
   const BasicBlock *LLVM_BB = BB->getBasicBlock();
-  MachineFunction::iterator I = BB->getParent()->begin();
-  ++I;
+
+  MachineFunction::iterator I;
+  for (I = F->begin(); I != F->end() && &(*I) != BB; ++I);
+  if (I != F->end()) ++I;
 
   // Create loop block.
   MachineBasicBlock *LoopBB = F->CreateMachineBasicBlock(LLVM_BB);
