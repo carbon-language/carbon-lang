7fb0545986cb29f9a7d0dc3e7fc18dfffdd0237b
Dean Michael Berris <dberris@google.com>
2017-09-26 04:29:25 +0000

    [XRay] Avoid actual linking when testing the driver
    Use -### in the command to see just look for the output of -v.
    
    Follow-up to D38226.
    
    llvm-svn: 314194

--- clang/test/Driver/XRay/xray-shared-noxray.cpp
+++ clang/test/Driver/XRay/xray-shared-noxray.cpp
@@ -1,9 +1,9 @@
-// RUN: %clangxx -shared -o /dev/null -v -fxray-instrument %s
-// RUN: %clangxx -shared -o /dev/null -v -fxray-instrument %s 2>&1 | \
+// RUN: %clangxx -shared -o /dev/null -v -fxray-instrument %s -###
+// RUN: %clangxx -shared -o /dev/null -v -fxray-instrument %s -### 2>&1 | \
 // RUN:     FileCheck %s --check-prefix=SHARED
-// RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s -DMAIN
-// RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s 2>&1 -DMAIN | \
-// RUN:     FileCheck %s --check-prefix=STATIC
+// RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s -### -DMAIN
+// RUN: %clangxx -static -o /dev/null -v -fxray-instrument %s -### 2>&1 -DMAIN \
+// RUN:     | FileCheck %s --check-prefix=STATIC
 //
 // SHARED-NOT: {{clang_rt\.xray-}}
 // STATIC: {{clang_rt\.xray-}}
