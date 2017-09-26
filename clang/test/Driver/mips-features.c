ef41f6a57328100471c169332d03fc35918a65d1
Simon Dardis <simon.dardis@imgtec.com>
2017-09-26 15:01:21 +0000

    [mips] Accept but ignore -m(no-)branch-likely
    -mbranch-likely and -mno-branch-likely are used in some build systems for
    some MIPS targets. Accept these options but ignore them as they are an
    (de)optimiztion hint, and that branch likely instructions were deprecated
    but not removed from MIPS32 and MIPS64 ISAs.
    
    Reviewers: atanasyan, nitesh.jain
    
    Differential Revision: https://reviews.llvm.org/D38168
    
    llvm-svn: 314213

--- clang/test/Driver/mips-features.c
+++ clang/test/Driver/mips-features.c
@@ -392,3 +392,13 @@
 // LONG-CALLS-ON: "-target-feature" "+long-calls"
 // LONG-CALLS-OFF: "-target-feature" "-long-calls"
 // LONG-CALLS-DEF-NOT: "long-calls"
+//
+// -mbranch-likely
+// RUN: %clang -target -mips-mti-linux-gnu -### -c %s -mbranch-likely 2>&1 \
+// RUN:   | FileCheck --check-prefix=BRANCH-LIKELY %s
+// BRANCH-LIKELY: argument unused during compilation: '-mbranch-likely'
+//
+// -mno-branch-likely
+// RUN: %clang -target -mips-mti-linux-gnu -### -c %s -mno-branch-likely 2>&1 \
+// RUN:   | FileCheck --check-prefix=NO-BRANCH-LIKELY %s
+// NO-BRANCH-LIKELY: argument unused during compilation: '-mno-branch-likely'
