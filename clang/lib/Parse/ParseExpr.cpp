e8c0e558a40a68aa6db8b6436f391c42329b0481
Aaron Ballman <aaron@aaronballman.com>
2017-09-28 21:29:18 +0000

    Properly parse a postfix expression following a Boolean literal. Fixes PR34273.
    Patch by Nicolas Lesser.
    
    llvm-svn: 314463

--- clang/lib/Parse/ParseExpr.cpp
+++ clang/lib/Parse/ParseExpr.cpp
@@ -798,7 +798,8 @@ ExprResult Parser::ParseCastExpression(bool isUnaryExpression,
 
   case tok::kw_true:
   case tok::kw_false:
-    return ParseCXXBoolLiteral();
+    Res = ParseCXXBoolLiteral();
+    break;
   
   case tok::kw___objc_yes:
   case tok::kw___objc_no:
