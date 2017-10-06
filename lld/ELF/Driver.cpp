5a8928c605a579ddb26bb46b933b451e7161a1b9
Peter Collingbourne <peter@pcc.me.uk>
2017-10-06 21:33:59 +0000

    ELF: Export preempted symbols even if there is a dynamic list.
    Dynamic lists in an executable are additive, not restrictive, so we
    must continue to export preempted symbols even with a dynamic list.
    
    This fixes sanitizer interception of libc symbols (and should also fix
    symbol preemption by users of sanitizers).
    
    Differential Revision: https://reviews.llvm.org/D38647
    
    llvm-svn: 315114

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -790,18 +790,8 @@ void LinkerDriver::readConfigs(opt::InputArgList &Args) {
         readDynamicList(*Buffer);
 
     for (auto *Arg : Args.filtered(OPT_export_dynamic_symbol))
-      Config->VersionScriptGlobals.push_back(
+      Config->DynamicList.push_back(
           {Arg->getValue(), /*IsExternCpp*/ false, /*HasWildcard*/ false});
-
-    // Dynamic lists are a simplified linker script that doesn't need the
-    // "global:" and implicitly ends with a "local:*". Set the variables
-    // needed to simulate that.
-    if (Args.hasArg(OPT_dynamic_list) ||
-        Args.hasArg(OPT_export_dynamic_symbol)) {
-      Config->ExportDynamic = true;
-      if (!Config->Shared)
-        Config->DefaultSymbolVersion = VER_NDX_LOCAL;
-    }
   }
 
   if (auto *Arg = Args.getLastArg(OPT_version_script))
