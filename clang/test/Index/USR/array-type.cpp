10141da380b298a02e7beda27d1935e83c4e79bd
Jan Korous <jkorous@apple.com>
2017-10-09 20:17:28 +0000

    PR13575: Fix test
    Ignore OS-specific mangled name.
    
    llvm-svn: 315241

--- clang/test/Index/USR/array-type.cpp
+++ clang/test/Index/USR/array-type.cpp
@@ -3,9 +3,9 @@
 // Function template specializations differing in array type parameter should have unique USRs.
 
 template<class buffer> void foo(buffer);
-// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n16C>#*C# | __Z3fooIA16_cEvT_ | Decl,RelSpecialization | rel: 1
+// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n16C>#*C#
 template<> void foo<char[16]>(char[16]);
-// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n32C>#*C# | __Z3fooIA32_cEvT_ | Decl,RelSpecialization | rel: 1
+// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n32C>#*C#
 template<> void foo<char[32]>(char[32]);
-// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n64C>#*C# | __Z3fooIA64_cEvT_ | Decl,RelSpecialization | rel: 1
+// CHECK: {{[0-9]+}}:17 | function(Gen,TS)/C++ | foo | c:@F@foo<#{n64C>#*C#
 template<> void foo<char[64]>(char[64]);
