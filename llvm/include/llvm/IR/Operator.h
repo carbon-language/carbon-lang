bdaa2f9d0591e75062ce84409d75c0f95e12a108
Sanjay Patel <spatel@rotateright.com>
2017-10-13 18:25:23 +0000

    [LLVMCore] fix description for OverflowingBinaryOperator; NFC
    llvm-svn: 315726

--- llvm/include/llvm/IR/Operator.h
+++ llvm/include/llvm/IR/Operator.h
@@ -61,9 +61,9 @@ public:
   }
 };
 
-/// Utility class for integer arithmetic operators which may exhibit overflow -
-/// Add, Sub, and Mul. It does not include SDiv, despite that operator having
-/// the potential for overflow.
+/// Utility class for integer operators which may exhibit overflow - Add, Sub,
+/// Mul, and Shl. It does not include SDiv, despite that operator having the
+/// potential for overflow.
 class OverflowingBinaryOperator : public Operator {
 public:
   enum {
