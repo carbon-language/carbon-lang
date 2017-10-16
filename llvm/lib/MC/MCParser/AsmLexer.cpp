b0c9e0d647672f7b6c4f7e01f1b93dedfca7ed1c
Reid Kleckner <rnk@google.com>
2017-10-16 22:20:03 +0000

    [MC] Lex CRLF as one token
    This will prevent doubling of line endings when parsing assembly and
    emitting assembly.
    
    Otherwise we'd parse the directive, consume the end of statement, hit
    the next end of statement, and emit a fresh newline.
    
    llvm-svn: 315943

--- llvm/lib/MC/MCParser/AsmLexer.cpp
+++ llvm/lib/MC/MCParser/AsmLexer.cpp
@@ -606,8 +606,16 @@ AsmToken AsmLexer::LexToken() {
       return LexToken(); // Ignore whitespace.
     else
       return AsmToken(AsmToken::Space, StringRef(TokStart, CurPtr - TokStart));
+  case '\r': {
+    IsAtStartOfLine = true;
+    IsAtStartOfStatement = true;
+    // If this is a CR followed by LF, treat that as one token.
+    if (CurPtr != CurBuf.end() && *CurPtr == '\n')
+      ++CurPtr;
+    return AsmToken(AsmToken::EndOfStatement,
+                    StringRef(TokStart, CurPtr - TokStart));
+  }
   case '\n':
-  case '\r':
     IsAtStartOfLine = true;
     IsAtStartOfStatement = true;
     return AsmToken(AsmToken::EndOfStatement, StringRef(TokStart, 1));
