f7596ece76570794f4fc89a65b49857b8c8570f1
Saleem Abdulrasool <compnerd@compnerd.org>
2017-10-20 18:47:35 +0000

    GNU: do not read the FDE count if omitted
    If there is no binary search table computed, the FDECount encoding is
    DW_EH_PE_omit.  Do not attempt to read the FDECount in such a situation
    as we will read an incorrect value.  binutils only writes out the
    FDECount if the encoding is not DW_EH_PE_omit.
    
    llvm-svn: 316224

--- libunwind/src/EHHeaderParser.hpp
+++ libunwind/src/EHHeaderParser.hpp
@@ -67,7 +67,9 @@ void EHHeaderParser<A>::decodeEHHdr(A &addressSpace, pint_t ehHdrStart,
   ehHdrInfo.eh_frame_ptr =
       addressSpace.getEncodedP(p, ehHdrEnd, eh_frame_ptr_enc, ehHdrStart);
   ehHdrInfo.fde_count =
-      addressSpace.getEncodedP(p, ehHdrEnd, fde_count_enc, ehHdrStart);
+      fde_count_enc == DW_EH_PE_omit
+          ? 0
+          : addressSpace.getEncodedP(p, ehHdrEnd, fde_count_enc, ehHdrStart);
   ehHdrInfo.table = p;
 }
 
