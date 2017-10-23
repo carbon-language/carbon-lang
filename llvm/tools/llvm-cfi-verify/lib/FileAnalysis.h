f91a31b7a36d86d5f7f0d99a982a9f36024660ba
Mitch Phillips <mitchphillips@outlook.com>
2017-10-23 18:11:31 +0000

    Patch in
    llvm-svn: 316358

--- llvm/tools/llvm-cfi-verify/lib/FileAnalysis.h
+++ llvm/tools/llvm-cfi-verify/lib/FileAnalysis.h
@@ -10,6 +10,7 @@
 #ifndef LLVM_CFI_VERIFY_FILE_ANALYSIS_H
 #define LLVM_CFI_VERIFY_FILE_ANALYSIS_H
 
+#include "llvm/ADT/DenseMap.h"
 #include "llvm/BinaryFormat/ELF.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCContext.h"
@@ -161,7 +162,7 @@ private:
 
   // Contains a mapping between a specific address, and a list of instructions
   // that use this address as a branch target (including call instructions).
-  std::unordered_map<uint64_t, std::vector<uint64_t>> StaticBranchTargetings;
+  DenseMap<uint64_t, std::vector<uint64_t>> StaticBranchTargetings;
 
   // A list of addresses of indirect control flow instructions.
   std::set<uint64_t> IndirectInstructions;
