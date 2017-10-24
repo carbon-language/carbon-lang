75bda3006b887d7238889485a2e80a0ee7db8a97
David Blaikie <dblaikie@gmail.com>
2017-10-24 21:29:15 +0000

    WebassemblyAsmPrinter.h: Include WebAssemblyMachineFunctionInfo for use with MachineFunction::getInfo
    llvm-svn: 316507

--- llvm/lib/Target/WebAssembly/WebAssemblyAsmPrinter.h
+++ llvm/lib/Target/WebAssembly/WebAssemblyAsmPrinter.h
@@ -10,6 +10,7 @@
 #ifndef LLVM_LIB_TARGET_WEBASSEMBLY_WEBASSEMBLYASMPRINTER_H
 #define LLVM_LIB_TARGET_WEBASSEMBLY_WEBASSEMBLYASMPRINTER_H
 
+#include "WebAssemblyMachineFunctionInfo.h"
 #include "WebAssemblySubtarget.h"
 #include "llvm/CodeGen/AsmPrinter.h"
 #include "llvm/MC/MCStreamer.h"
@@ -17,7 +18,6 @@
 
 namespace llvm {
 class MCSymbol;
-class WebAssemblyFunctionInfo;
 class WebAssemblyTargetStreamer;
 class WebAssemblyMCInstLower;
 
