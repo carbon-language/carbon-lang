c507b370a1a6949edd5b608c2a6cb65000cf90d2
Oliver Stannard <oliver.stannard@arm.com>
2017-10-24 08:55:36 +0000

    [ARM] Remove tCPS alias which just crashed
    This alias caused a crash when trying to print the "cps #0" instruction in a
    diagnostic for thumbv6 (which doesn't have that instruction).
    	    
    The comment was incorrect, this instruction is UNPREDICTABLE if no flag bits
    are set, so I don't think it's worth keeping.
    
    Differential Revision: https://reviews.llvm.org/D39191
    
    llvm-svn: 316420

--- llvm/test/MC/ARM/cps.s
+++ llvm/test/MC/ARM/cps.s
@@ -3,6 +3,8 @@
 @ RUN: llvm-mc -triple=thumbv7r--none-eabi -show-encoding < %s | FileCheck %s
 @ RUN: llvm-mc -triple=thumbv8a--none-eabi -show-encoding < %s | FileCheck %s
 @ RUN: not llvm-mc -triple=thumbv7m--none-eabi -show-encoding < %s 2>&1 | FileCheck %s --check-prefix=UNDEF
+@ RUN: not llvm-mc -triple=thumbv6--none-eabi -show-encoding < %s 2>%t | FileCheck %s --check-prefix=V6
+@ RUN: FileCheck %s < %t --check-prefix=V6-ERRORS
 
   cpsie f
   cpsie i, #3
@@ -15,3 +17,15 @@
 @ UNDEF-DAG: cpsie f                         @ encoding: [0x61,0xb6]
 @ UNDEF-DAG: instruction requires:
 @ UNDEF-DAG: error: instruction 'cps' requires effect for M-class
+
+@ V6: cpsie f                         @ encoding: [0x61,0xb6]
+@ V6-ERRORS: error: invalid instruction, any one of the following would fix this:
+@ V6-ERRORS-NEXT: cpsie i, #3
+@ V6-ERRORS: note: instruction requires: thumb2
+@ V6-ERRORS: note: instruction requires: arm-mode
+@ V6-ERRORS: note: too many operands for instruction
+@ V6-ERRORS: error: invalid instruction, any one of the following would fix this:
+@ V6-ERRORS: cps #0
+@ V6-ERRORS: note: too few operands for instruction
+@ V6-ERRORS: note: instruction requires: arm-mode
+@ V6-ERRORS: note: instruction requires: thumb2
