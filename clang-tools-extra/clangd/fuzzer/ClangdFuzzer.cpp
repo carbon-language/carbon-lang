09113ae2c5671afea23c3f9d69ae5228055c3d1a
Benjamin Kramer <benny.kra@googlemail.com>
2017-10-26 10:03:11 +0000

    [clangd] Add a simple fuzzer. It crashes a lot :)
    llvm-svn: 316649

---
+++ clang-tools-extra/clangd/fuzzer/ClangdFuzzer.cpp
@@ -0,0 +1,34 @@
+//===-- ClangdFuzzer.cpp - Fuzz clangd ------------------------------------===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// \brief This file implements a function that runs clangd on a single input.
+/// This function is then linked into the Fuzzer library.
+///
+//===----------------------------------------------------------------------===//
+
+#include "ClangdLSPServer.h"
+#include "llvm/Support/Program.h"
+#include <sstream>
+
+extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
+  /// Change stdin to binary to not lose \r\n on windows.
+  llvm::sys::ChangeStdinToBinary();
+
+  clang::clangd::JSONOutput Out(llvm::nulls(), llvm::nulls(), nullptr);
+
+  /// Initialize and run ClangdLSPServer.
+  clang::clangd::ClangdLSPServer LSPServer(
+      Out, clang::clangd::getDefaultAsyncThreadsCount(),
+      /*EnableSnippets=*/false, llvm::None, llvm::None);
+
+  std::istringstream In(std::string(reinterpret_cast<char *>(data), size));
+  LSPServer.run(In);
+  return 0;
+}
