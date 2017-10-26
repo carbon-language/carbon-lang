4ffbeeba9238846a04d080f2d5358e91556fc4ce
Stephane Sezer <sas@cd80.net>
2017-10-26 17:04:20 +0000

    Allow SysV-i386 ABI on everything other than Apple targets
    Summary:
    This matches other SysV ABIs that are different on Apple and non-Apple targets,
    like `ABISysV_arm.cpp` for instance.
    
    Reviewers: clayborg, emaste
    
    Subscribers: aemerson, kristof.beyls, lldb-commits
    
    Differential Revision: https://reviews.llvm.org/D39335
    
    llvm-svn: 316673

--- lldb/source/Plugins/ABI/SysV-i386/ABISysV_i386.cpp
+++ lldb/source/Plugins/ABI/SysV-i386/ABISysV_i386.cpp
@@ -205,11 +205,12 @@ ABISysV_i386::GetRegisterInfoArray(uint32_t &count) {
 ABISP
 ABISysV_i386::CreateInstance(lldb::ProcessSP process_sp, const ArchSpec &arch) {
   static ABISP g_abi_sp;
-  if ((arch.GetTriple().getArch() == llvm::Triple::x86) &&
-      (arch.GetTriple().isOSLinux() || arch.GetTriple().isOSFreeBSD())) {
-    if (!g_abi_sp)
-      g_abi_sp.reset(new ABISysV_i386(process_sp));
-    return g_abi_sp;
+  if (arch.GetTriple().getVendor() != llvm::Triple::Apple) {
+    if (arch.GetTriple().getArch() == llvm::Triple::x86) {
+      if (!g_abi_sp)
+        g_abi_sp.reset(new ABISysV_i386(process_sp));
+      return g_abi_sp;
+    }
   }
   return ABISP();
 }
