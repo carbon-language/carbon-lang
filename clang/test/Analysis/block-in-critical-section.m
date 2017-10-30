bde3be43d7c7bae4af655f2a29a2b7d262b9e50b
Gabor Horvath <xazax.hun@gmail.com>
2017-10-30 09:01:48 +0000

    Add missing expected-no-diagnostics comment to test.
    llvm-svn: 316886

--- clang/test/Analysis/block-in-critical-section.m
+++ clang/test/Analysis/block-in-critical-section.m
@@ -1,4 +1,5 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core,alpha.unix.BlockInCriticalSection -verify -Wno-objc-root-class %s
+// expected-no-diagnostics
 
 @interface SomeClass
 -(void)someMethod;
