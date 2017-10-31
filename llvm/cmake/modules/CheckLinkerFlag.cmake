72404b801ee41b0aecd3ee4f741287dda94d4a05
Shoaib Meenai <smeenai@fb.com>
2017-10-31 01:30:46 +0000

    [cmake] Make check_linker_flags operate via linker flags
    `check_linker_flags` currently sets the *compiler* flags (via
    `CMAKE_REQUIRED_FLAGS`), and thus implicitly relies on cmake's default
    behavior of passing the compiler flags to the linker. This breaks when
    cmake's build rules have been altered to not pollute the link line with
    compiler flags (which can be desirable for build cleanliness). Instead,
    set `CMAKE_EXE_LINKER_FLAGS` explicitly and use `CMP0056` to ensure the
    linker flags are passed along. Additionally, since we're inside a
    function, we can just alter the variable directly (as the alteration
    will be limited to the scope of the function) rather than saving and
    restoring the old value.
    
    Differential Revision: https://reviews.llvm.org/D39431
    
    llvm-svn: 316972

--- llvm/cmake/modules/CheckLinkerFlag.cmake
+++ llvm/cmake/modules/CheckLinkerFlag.cmake
@@ -1,8 +1,6 @@
 include(CheckCXXCompilerFlag)
 
 function(check_linker_flag flag out_var)
-  set(OLD_CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS}")
-  set(CMAKE_REQUIRED_FLAGS "${CMAKE_REQUIRED_FLAGS} ${flag}")
+  set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${flag}")
   check_cxx_compiler_flag("" ${out_var})
-  set(CMAKE_REQUIRED_FLAGS ${OLD_CMAKE_REQUIRED_FLAGS})
 endfunction()
