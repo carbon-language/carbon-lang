ba4b98c6916d6dbcf80f130b4a57a9958b24e497
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:15 +0000

    asin: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317235

--- libclc/generic/include/clc/math/asin.h
+++ libclc/generic/include/clc/math/asin.h
@@ -1,2 +1,7 @@
-#define __CLC_BODY <clc/math/asin.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION asin
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
