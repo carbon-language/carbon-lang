d44fdc2b66bb8abce0fad999a107fc9c0f4b957e
Alex Lorenz <arphaman@gmail.com>
2017-11-02 19:42:40 +0000

    revert r317228: remove unused function from ObjCRuntime.h, NFC
    This function is actually used in LLDB
    
    llvm-svn: 317231

--- clang/include/clang/Basic/ObjCRuntime.h
+++ clang/include/clang/Basic/ObjCRuntime.h
@@ -67,6 +67,11 @@ public:
   ObjCRuntime(Kind kind, const VersionTuple &version)
     : TheKind(kind), Version(version) {}
 
+  void set(Kind kind, VersionTuple version) {
+    TheKind = kind;
+    Version = version;
+  }
+
   Kind getKind() const { return TheKind; }
   const VersionTuple &getVersion() const { return Version; }
 
