e4d5d10076229e6f6dbfa106f7881b89de8b2048
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:57 +0000

    tan: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317258

--- libclc/generic/include/clc/math/tan.h
+++ libclc/generic/include/clc/math/tan.h
@@ -1,2 +1,7 @@
-#define __CLC_BODY <clc/math/tan.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION tan
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
