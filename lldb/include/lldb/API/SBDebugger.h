edc2def4a65764991ffb50e9c9af1c740ced534c
Jason Molenda <jmolenda@apple.com>
2017-11-02 02:43:27 +0000

    Commit Lawrence D'Anna's patch to change SetOututFileHandle to work with IOBase.
    I did make one change after checking with Larry --
    I renamed SBDebugger::Flush to FlushDebuggerOutputHandles
    and added a short docstring to the .i file to make it
    a little clearer under which context programs may need
    to use this API.
    
    Differential Revision: https://reviews.llvm.org/D39128
    <rdar://problem/34870417> 
    
    llvm-svn: 317182

--- lldb/include/lldb/API/SBDebugger.h
+++ lldb/include/lldb/API/SBDebugger.h
@@ -78,6 +78,8 @@ public:
   void SetOutputFileHandle(FILE *f, bool transfer_ownership);
 
   void SetErrorFileHandle(FILE *f, bool transfer_ownership);
+  
+  void FlushDebuggerOutputHandles();
 
   FILE *GetInputFileHandle();
 
