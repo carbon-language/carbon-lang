7fc23fbdcb4c5f161d62a735ce01eacf594fda31
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:39 +0000

    log2: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317248

--- libclc/generic/include/clc/math/log2.h
+++ libclc/generic/include/clc/math/log2.h
@@ -20,5 +20,10 @@
  * THE SOFTWARE.
  */
 
-#define __CLC_BODY <clc/math/log2.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION log2
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
