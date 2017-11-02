fd13434d839fef30dcae06101c8ec7563571597a
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:46 +0000

    native_log2: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317252

--- libclc/generic/include/clc/math/native_log2.h
+++ libclc/generic/include/clc/math/native_log2.h
@@ -20,8 +20,12 @@
  * THE SOFTWARE.
  */
 
-#define __CLC_BODY <clc/math/native_log2.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION native_log2
 #define __FLOAT_ONLY
+
 #include <clc/math/gentype.inc>
-#undef __CLC_BODY
+
 #undef __FLOAT_ONLY
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
