4708b10878f0c0db2d1448bd9227cb996760f497
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:51 +0000

    sinh: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317255

--- libclc/generic/include/clc/math/sinh.h
+++ libclc/generic/include/clc/math/sinh.h
@@ -20,5 +20,10 @@
  * THE SOFTWARE.
  */
 
-#define __CLC_BODY <clc/math/sinh.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION sinh
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
