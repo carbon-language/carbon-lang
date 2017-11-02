1bd2ac257ab8ccbb4417e9b210fba9bd5c1fee63
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-02 19:48:22 +0000

    atanh: Use unary_decl instead of custom inc file
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    Reviewed-By: Aaron Watry <awatry@gmail.com>
    llvm-svn: 317239

--- libclc/generic/include/clc/math/atanh.h
+++ libclc/generic/include/clc/math/atanh.h
@@ -20,5 +20,10 @@
  * THE SOFTWARE.
  */
 
-#define __CLC_BODY <clc/math/atanh.inc>
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION atanh
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
