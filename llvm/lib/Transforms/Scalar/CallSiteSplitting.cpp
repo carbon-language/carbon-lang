c7c05ae4be905f592947579f025aadc4f8b96c7b
Davide Italiano <davide@freebsd.org>
2017-11-04 00:44:01 +0000

    [CallSiteSplitting] clang-format my last commit. NFCI.
    Thanks to Rui for pointing out.
    
    llvm-svn: 317393

--- llvm/lib/Transforms/Scalar/CallSiteSplitting.cpp
+++ llvm/lib/Transforms/Scalar/CallSiteSplitting.cpp
@@ -126,9 +126,8 @@ static bool createCallSitesOnOrPredicatedArgument(
       Instruction *&CallUntakenFromHeader =
           IsCSInTakenPath ? NewCSTakenFromNextCond : NewCSTakenFromHeader;
 
-      assert((Pred == ICmpInst::ICMP_EQ ||
-             Pred == ICmpInst::ICMP_NE) &&
-                 "Unexpected predicate in an OR condition");
+      assert((Pred == ICmpInst::ICMP_EQ || Pred == ICmpInst::ICMP_NE) &&
+             "Unexpected predicate in an OR condition");
 
       // Set the constant value for agruments in the call predicated based on
       // the OR condition.
