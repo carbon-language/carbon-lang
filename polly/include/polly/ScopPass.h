6720a089fd566be17e761a693a4f8bbba79d6d05
Florian Hahn <florian.hahn@arm.com>
2017-11-06 14:26:04 +0000

    [Polly] Fix using order, as this caused a test failure (NFC)
    Summary:
    Without this patch, clang-format in check-polly fails for me, with current master:
    
    ```
    FAILED: cd build/tools/polly && build/bin/clang-format -sort-includes -style=llvm llvm/tools/polly/include/polly/ScopPass.h | diff -u llvm/tools/polly/include/polly/ScopPass.h -
    --- llvm/tools/polly/include/polly/ScopPass.h	2017-11-06 14:05:49.885345000 +0000
    +++ -	2017-11-06 14:07:24.956241758 +0000
    @@ -40,12 +40,12 @@
     } // namespace polly
    
     namespace llvm {
    +using polly::SPMUpdater;
     using polly::Scop;
     using polly::ScopAnalysisManager;
     using polly::ScopAnalysisManagerFunctionProxy;
     using polly::ScopInfo;
     using polly::ScopStandardAnalysisResults;
    -using polly::SPMUpdater;
    
     template <>
     class InnerAnalysisManagerProxy<ScopAnalysisManager, Function>::Result {
    ```
    
    Reviewers: grosser, Meinersbur, bollu
    
    Reviewed By: Meinersbur
    
    Subscribers: llvm-commits, pollydev
    
    Differential Revision: https://reviews.llvm.org/D39683
    
    llvm-svn: 317478

--- polly/include/polly/ScopPass.h
+++ polly/include/polly/ScopPass.h
@@ -40,12 +40,12 @@ using FunctionAnalysisManagerScopProxy =
 } // namespace polly
 
 namespace llvm {
+using polly::SPMUpdater;
 using polly::Scop;
 using polly::ScopAnalysisManager;
 using polly::ScopAnalysisManagerFunctionProxy;
 using polly::ScopInfo;
 using polly::ScopStandardAnalysisResults;
-using polly::SPMUpdater;
 
 template <>
 class InnerAnalysisManagerProxy<ScopAnalysisManager, Function>::Result {
