fe34d6d8cdf6460f9eb8886d9d1ccd18572595f7
Sriraman Tallam <tmsriram@google.com>
2017-11-07 22:34:55 +0000

    Change noplt.c to work for non-x86 targets.
    Differential Revision: https://reviews.llvm.org/D39759
    
    llvm-svn: 317627

--- clang/test/CodeGen/noplt.c
+++ clang/test/CodeGen/noplt.c
@@ -1,7 +1,7 @@
 // RUN: %clang_cc1 -emit-llvm -fno-plt %s -o - | FileCheck %s -check-prefix=CHECK-NOPLT
 
 // CHECK-NOPLT: Function Attrs: nonlazybind
-// CHECK-NOPLT-NEXT: declare i32 @foo
+// CHECK-NOPLT-NEXT: declare {{.*}}i32 @foo
 int foo();
 
 int bar() {
