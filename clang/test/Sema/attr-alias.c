4e30b96834cea5682a8e9e024dda06319825000a
Alex Lorenz <arphaman@gmail.com>
2017-11-07 00:31:19 +0000

    Clarify the error message for unsupported aliases on Darwin
    rdar://35109556
    
    llvm-svn: 317532

--- clang/test/Sema/attr-alias.c
+++ clang/test/Sema/attr-alias.c
@@ -2,7 +2,4 @@
 
 void g() {}
 
-// It is important that the following string be in the error message. The gcc
-// testsuite looks for it to decide if a target supports aliases.
-
-void f() __attribute__((alias("g"))); //expected-error {{only weak aliases are supported}}
+void f() __attribute__((alias("g"))); //expected-error {{aliases are not supported on darwin}}
