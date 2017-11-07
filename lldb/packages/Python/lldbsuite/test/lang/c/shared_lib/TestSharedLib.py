d20106b5ecd36edecf1a1109d3c50682dea849a7
Stephane Sezer <sas@cd80.net>
2017-11-07 00:14:40 +0000

    Disable tests in lang/c/shared_lib on Windows
    Summary: These fail because `-fPIC` is not supported on Windows.
    
    Reviewers: zturner, jingham, clayborg
    
    Reviewed By: clayborg
    
    Subscribers: lldb-commits
    
    Differential Revision: https://reviews.llvm.org/D39692
    
    llvm-svn: 317529

--- lldb/packages/Python/lldbsuite/test/lang/c/shared_lib/TestSharedLib.py
+++ lldb/packages/Python/lldbsuite/test/lang/c/shared_lib/TestSharedLib.py
@@ -5,6 +5,7 @@ from __future__ import print_function
 
 import unittest2
 import lldb
+from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 import lldbsuite.test.lldbutil as lldbutil
 
@@ -34,10 +35,12 @@ class SharedLibTestCase(TestBase):
             "expression GetMeASubFoo(my_foo_ptr)",
             startstr="(sub_foo *) $")
 
+    @expectedFailureAll(oslist=["windows"])
     def test_expr(self):
         """Test that types work when defined in a shared library and forward-declared in the main executable"""
         self.common_test_expr(True)
 
+    @expectedFailureAll(oslist=["windows"])
     def test_expr_no_preload(self):
         """Test that types work when defined in a shared library and forward-declared in the main executable, but with preloading disabled"""
         self.common_test_expr(False)
