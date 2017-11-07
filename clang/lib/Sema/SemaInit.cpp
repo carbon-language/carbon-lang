bbe51d86eca2e9a663813fdcc2c6893885c4e452
Alex Lorenz <arphaman@gmail.com>
2017-11-07 21:40:11 +0000

    [ObjC++] Don't warn about pessimizing move for __block variables
    rdar://33316951
    
    llvm-svn: 317620

--- clang/lib/Sema/SemaInit.cpp
+++ clang/lib/Sema/SemaInit.cpp
@@ -6422,6 +6422,10 @@ static void CheckMoveOnConstruction(Sema &S, const Expr *InitExpr,
     if (!VD || !VD->hasLocalStorage())
       return;
 
+    // __block variables are not moved implicitly.
+    if (VD->hasAttr<BlocksAttr>())
+      return;
+
     QualType SourceType = VD->getType();
     if (!SourceType->isRecordType())
       return;
