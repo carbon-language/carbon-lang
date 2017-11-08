bc7f6318ee26b9980b59b3ad6a47d33b485798a6
Volodymyr Sapsai <vsapsai@apple.com>
2017-11-08 22:30:29 +0000

    [libcxx] Mark test cxa_deleted_virtual.pass.cpp as failing for previous libcxx versions.
    r313500 added a fix for undefined "___cxa_deleted_virtual" symbol.
    Previous libcxx versions don't have the fix and corresponding test
    should be failing.
    
    rdar://problem/34521053
    
    Reviewers: EricWF, mclow.lists, ahatanak
    
    Reviewed By: ahatanak
    
    Subscribers: mehdi_amini, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D39776
    
    llvm-svn: 317734

--- libcxx/test/libcxx/language.support/cxa_deleted_virtual.pass.cpp
+++ libcxx/test/libcxx/language.support/cxa_deleted_virtual.pass.cpp
@@ -10,6 +10,15 @@
 // UNSUPPORTED: c++98, c++03
 
 // Test exporting the symbol: "__cxa_deleted_virtual" in macosx
+// But don't expect the symbol to be exported in previous versions.
+//
+// XFAIL: with_system_cxx_lib=macosx10.13
+// XFAIL: with_system_cxx_lib=macosx10.12
+// XFAIL: with_system_cxx_lib=macosx10.11
+// XFAIL: with_system_cxx_lib=macosx10.10
+// XFAIL: with_system_cxx_lib=macosx10.9
+// XFAIL: with_system_cxx_lib=macosx10.8
+// XFAIL: with_system_cxx_lib=macosx10.7
 
 struct S { virtual void f() = delete; virtual ~S() {} };
 int main() {
