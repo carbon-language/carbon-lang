1e8813c6b4a2f4e70ca622ba0f633bc0c18cc164
Rui Ueyama <ruiu@google.com>
2017-11-08 04:22:40 +0000

    Remove a given file even if openFileForRead fails.
    I think the constract of this function is to remove a file in some way,
    whether in background or in foreground. This patch makes sure that it
    removes a given file.
    
    Differential Revision: https://reviews.llvm.org/D39778
    
    llvm-svn: 317663

--- lld/ELF/Filesystem.cpp
+++ lld/ELF/Filesystem.cpp
@@ -56,13 +56,12 @@ void elf::unlinkAsync(StringRef Path) {
   // Instead we open the file and unlink it on this thread. The unlink is fast
   // since the open fd guarantees that it is not removing the last reference.
   int FD;
-  if (sys::fs::openFileForRead(Path, FD))
-    return;
-
+  std::error_code EC = sys::fs::openFileForRead(Path, FD);
   sys::fs::remove(Path);
 
   // close and therefore remove TempPath in background.
-  std::thread([=] { ::close(FD); }).detach();
+  if (!EC)
+    std::thread([=] { ::close(FD); }).detach();
 #endif
 }
 
