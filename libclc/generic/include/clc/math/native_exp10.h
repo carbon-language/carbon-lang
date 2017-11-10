6b4a625438983ab8dc84b087a90c9d9bb94ffdde
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-10 22:16:41 +0000

    native_exp10: Switch implementation to llvm intrinsic
    v2: Use native_log2 instead of wrong constant
    
    Reviewer: Jeroen Ketema
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 317941

--- libclc/generic/include/clc/math/native_exp10.h
+++ libclc/generic/include/clc/math/native_exp10.h
@@ -1,1 +1,9 @@
-#define native_exp10 exp10
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION native_exp10
+#define __FLOAT_ONLY
+
+#include <clc/math/gentype.inc>
+
+#undef __FLOAT_ONLY
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
