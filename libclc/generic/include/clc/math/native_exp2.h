edbde58de06efa3d5a8258dec7acbb26d9a99d86
Jan Vesely <jan.vesely@rutgers.edu>
2017-11-10 22:16:31 +0000

    native_exp2: Switch implementation to llvm intrinsic
    Reviewer: Jeroen Ketema
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 317937

--- libclc/generic/include/clc/math/native_exp2.h
+++ libclc/generic/include/clc/math/native_exp2.h
@@ -1,1 +1,9 @@
-#define native_exp2 exp2
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION native_exp2
+#define __FLOAT_ONLY
+
+#include <clc/math/gentype.inc>
+
+#undef __FLOAT_ONLY
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
