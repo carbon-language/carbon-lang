3e29890a7f5b4a9efaecb5884fb29ee87c8571fb
Sanjay Patel <spatel@rotateright.com>
2017-11-15 16:33:11 +0000

    [(new) Pass Manager] instantiate SimplifyCFG with the same options as the old PM
    This is a recommit of r316869 which was speculatively reverted with r317444 and 
    subsequently shown to not be the cause of PR35210. That crash should be fixed
    after r318237.
    
    Original commit message:
    
    The old PM sets the options of what used to be known as "latesimplifycfg" on the
    instantiation after the vectorizers have run, so that's what we'redoing here.
    
    FWIW, there's a later SimplifyCFGPass instantiation in both PMs where we do not
    set the "late" options. I'm not sure if that's intentional or not.
    
    Differential Revision: https://reviews.llvm.org/D39407
    
    llvm-svn: 318299

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -757,8 +757,13 @@ PassBuilder::buildModuleOptimizationPipeline(OptimizationLevel Level,
   // Optimize parallel scalar instruction chains into SIMD instructions.
   OptimizePM.addPass(SLPVectorizerPass());
 
-  // Cleanup after all of the vectorizers.
-  OptimizePM.addPass(SimplifyCFGPass());
+  // Cleanup after all of the vectorizers. Simplification passes like CVP and
+  // GVN, loop transforms, and others have already run, so it's now better to
+  // convert to more optimized IR using more aggressive simplify CFG options.
+  OptimizePM.addPass(SimplifyCFGPass(SimplifyCFGOptions().
+                                         forwardSwitchCondToPhi(true).
+                                         convertSwitchToLookupTable(true).
+                                         needCanonicalLoops(false)));
   OptimizePM.addPass(InstCombinePass());
 
   // Unroll small loops to hide loop backedge latency and saturate any parallel
