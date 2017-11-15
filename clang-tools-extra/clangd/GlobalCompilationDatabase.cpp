e90fd06723d368cc95debac74ecb4ab54659f24a
Sam McCall <sam.mccall@gmail.com>
2017-11-15 21:33:56 +0000

    [clangd] Revert broken r318329 and disable tests on PS4
    (Clearly this is not a permanent solution)
    
    llvm-svn: 318337

--- clang-tools-extra/clangd/GlobalCompilationDatabase.cpp
+++ clang-tools-extra/clangd/GlobalCompilationDatabase.cpp
@@ -31,11 +31,7 @@ static void addExtraFlags(tooling::CompileCommand &Command,
 }
 
 tooling::CompileCommand getDefaultCompileCommand(PathRef File) {
-  // We don't specify --std because we want to infer it from the filename.
-  // We force PC because PS4 will change --std from under us.
-  // FIXME: there must be a more principled way to do this!
-  std::vector<std::string> CommandLine{
-      "clang", "-fsyntax-only", "-target=unknown-pc-unknown", File.str()};
+  std::vector<std::string> CommandLine{"clang", "-fsyntax-only", File.str()};
   return tooling::CompileCommand(llvm::sys::path::parent_path(File),
                                  llvm::sys::path::filename(File), CommandLine,
                                  /*Output=*/"");
