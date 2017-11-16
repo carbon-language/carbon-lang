089082378f28820fc441c9ae812c1423de32d692
Craig Topper <craig.topper@intel.com>
2017-11-16 23:09:06 +0000

    [X86] Add DAG combine to remove sext i32->i64 from gather/scatter instructions.
    Only do this pre-legalize in case we're using the sign extend to legalize for KNL.
    
    This recovers all of the tests that changed when I stopped SelectionDAGBuilder from deleting sign extends.
    
    There's more work that could be done here particularly to fix the i8->i64 test case that experienced split.
    
    llvm-svn: 318468

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -35836,13 +35836,34 @@ static SDValue combineGatherScatter(SDNode *N, SelectionDAG &DAG,
   // Pre-shrink oversized index elements to avoid triggering scalarization.
   if (DCI.isBeforeLegalize()) {
     SDValue Index = N->getOperand(4);
-    if (Index.getValueType().getScalarSizeInBits() > 64) {
+    if (Index.getScalarValueSizeInBits() > 64) {
       EVT IndexVT = EVT::getVectorVT(*DAG.getContext(), MVT::i64,
                                    Index.getValueType().getVectorNumElements());
       SDValue Trunc = DAG.getNode(ISD::TRUNCATE, DL, IndexVT, Index);
       SmallVector<SDValue, 5> NewOps(N->op_begin(), N->op_end());
       NewOps[4] = Trunc;
       DAG.UpdateNodeOperands(N, NewOps);
+      DCI.AddToWorklist(N);
+      return SDValue(N, 0);
+    }
+  }
+
+  // Try to remove sign extends from i32 to i64 on the index.
+  // Only do this before legalize in case we are relying on it for
+  // legalization.
+  // TODO: We should maybe remove any sign extend once we learn how to sign
+  // extend narrow index during lowering.
+  if (DCI.isBeforeLegalizeOps()) {
+    SDValue Index = N->getOperand(4);
+    if (Index.getScalarValueSizeInBits() == 64 &&
+        Index.getOpcode() == ISD::SIGN_EXTEND &&
+        Index.getOperand(0).getScalarValueSizeInBits() == 32) {
+      SmallVector<SDValue, 5> NewOps(N->op_begin(), N->op_end());
+      NewOps[4] = Index.getOperand(0);
+      DAG.UpdateNodeOperands(N, NewOps);
+      // The original sign extend has less users, add back to worklist in case
+      // it needs to be removed.
+      DCI.AddToWorklist(Index.getNode());
       DCI.AddToWorklist(N);
       return SDValue(N, 0);
     }
