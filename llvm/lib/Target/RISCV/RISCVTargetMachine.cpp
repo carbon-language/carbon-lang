47fbc5911dca29ca64eb8d3500d382dde7921975
Mandeep Singh Grang <mgrang@codeaurora.org>
2017-11-16 20:30:49 +0000

    [RISCV] Fix 64-bit data layout mismatch between backend and target description
    Reviewers: asb
    
    Reviewed By: asb
    
    Subscribers: rbar, johnrusso, simoncook, jordy.potman.lists, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D40145
    
    llvm-svn: 318454

--- llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
+++ llvm/lib/Target/RISCV/RISCVTargetMachine.cpp
@@ -30,7 +30,7 @@ extern "C" void LLVMInitializeRISCVTarget() {
 
 static std::string computeDataLayout(const Triple &TT) {
   if (TT.isArch64Bit()) {
-    return "e-m:e-i64:64-n32:64-S128";
+    return "e-m:e-p:64:64-i64:64-i128:128-n64-S128";
   } else {
     assert(TT.isArch32Bit() && "only RV32 and RV64 are currently supported");
     return "e-m:e-p:32:32-i64:64-n32-S128";
