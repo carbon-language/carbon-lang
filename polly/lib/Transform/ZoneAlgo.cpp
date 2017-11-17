ceec175dff8e38b7bade415323e81c86ee2925e4
Zhaoshi Zheng <zhaoshiz@coduaurora.org>
2017-11-17 22:05:19 +0000

    [NFC] Make r318597 compatible with clang-format
    llvm-svn: 318561

--- polly/lib/Transform/ZoneAlgo.cpp
+++ polly/lib/Transform/ZoneAlgo.cpp
@@ -154,8 +154,8 @@
 #include "polly/Support/GICHelper.h"
 #include "polly/Support/ISLTools.h"
 #include "polly/Support/VirtualInstruction.h"
-#include "llvm/Support/raw_ostream.h"
 #include "llvm/ADT/Statistic.h"
+#include "llvm/Support/raw_ostream.h"
 
 #define DEBUG_TYPE "polly-zone"
 
