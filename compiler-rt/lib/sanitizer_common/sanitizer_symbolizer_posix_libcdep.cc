e36f5cb48072e390207d18939aa6470c4147b4be
Kamil Rytarowski <n54@gmx.com>
2017-11-25 16:47:20 +0000

    Plug dlerror() leak for swift_demangle
    Summary:
    InitializeSwiftDemangler() attempts to resolve the
    swift_demangle symbol. If this is not available, we
    observe dlerror message leak.
    
    Caught on NetBSD/amd64 in TSan.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, kubamracek, vitalybuka, dvyukov, eugenis
    
    Reviewed By: dvyukov
    
    Subscribers: llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D40382
    
    llvm-svn: 318980

--- compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_symbolizer_posix_libcdep.cc
@@ -77,6 +77,7 @@ static swift_demangle_ft swift_demangle_f;
 // symbolication.
 static void InitializeSwiftDemangler() {
   swift_demangle_f = (swift_demangle_ft)dlsym(RTLD_DEFAULT, "swift_demangle");
+  (void)dlerror(); // Cleanup error message in case of failure
 }
 
 // Attempts to demangle a Swift name. The demangler will return nullptr if a
