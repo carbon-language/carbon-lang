1ea2f5e374e4b0801e9b576eced32b3f2d08d4c6
Marshall Clow <mclow.lists@gmail.com>
2017-11-27 16:17:19 +0000

    Fix failure on C++03 bots
    llvm-svn: 319042

--- libcxx/test/std/utilities/memory/unique.ptr/unique.ptr.special/io.fail.cpp
+++ libcxx/test/std/utilities/memory/unique.ptr/unique.ptr.special/io.fail.cpp
@@ -7,6 +7,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+// UNSUPPORTED: c++98, c++03
+//  Because we don't have a functioning decltype in C++03
+
 // <memory>
 
 // unique_ptr
