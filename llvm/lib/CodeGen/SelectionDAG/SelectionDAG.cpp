820ce043771378796551d94b8dfe3cd266c3354a
Craig Topper <craig.topper@intel.com>
2017-11-27 19:54:57 +0000

    [SelectionDAG] Add a debug message when vector_shuffle nodes are created.
    We print a debug message when most nodes are created, but getVectorShuffle was missing.
    
    llvm-svn: 319085

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAG.cpp
@@ -1638,7 +1638,9 @@ SDValue SelectionDAG::getVectorShuffle(EVT VT, const SDLoc &dl, SDValue N1,
 
   CSEMap.InsertNode(N, IP);
   InsertNode(N);
-  return SDValue(N, 0);
+  SDValue V = SDValue(N, 0);
+  NewSDValueDbgMsg(V, "Creating new node: ", this);
+  return V;
 }
 
 SDValue SelectionDAG::getCommutedVectorShuffle(const ShuffleVectorSDNode &SV) {
