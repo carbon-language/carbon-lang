bba7f862d8ab3f97372969856e376a0e4b636227
Rafael Espindola <rafael.espindola@gmail.com>
2017-11-28 18:50:08 +0000

    Fix non assert build warnings.
    llvm-svn: 319200

--- llvm/lib/DebugInfo/CodeView/ContinuationRecordBuilder.cpp
+++ llvm/lib/DebugInfo/CodeView/ContinuationRecordBuilder.cpp
@@ -108,6 +108,7 @@ void ContinuationRecordBuilder::writeMemberType(RecordType &Record) {
     // the previous member.  Save off the length of the member we just wrote so
     // that we can do some sanity checking on it.
     uint32_t MemberLength = SegmentWriter.getOffset() - OriginalOffset;
+    (void) MemberLength;
     insertSegmentEnd(OriginalOffset);
     // Since this member now becomes a new top-level record, it should have
     // gotten a RecordPrefix injected, and that RecordPrefix + the member we
@@ -126,6 +127,7 @@ uint32_t ContinuationRecordBuilder::getCurrentSegmentLength() const {
 
 void ContinuationRecordBuilder::insertSegmentEnd(uint32_t Offset) {
   uint32_t SegmentBegin = SegmentOffsets.back();
+  (void)SegmentBegin;
   assert(Offset > SegmentBegin);
   assert(Offset - SegmentBegin <= MaxSegmentLength);
 
@@ -136,6 +138,7 @@ void ContinuationRecordBuilder::insertSegmentEnd(uint32_t Offset) {
 
   uint32_t NewSegmentBegin = Offset + ContinuationLength;
   uint32_t SegmentLength = NewSegmentBegin - SegmentOffsets.back();
+  (void) SegmentLength;
 
   assert(SegmentLength % 4 == 0);
   assert(SegmentLength <= MaxRecordLength);
