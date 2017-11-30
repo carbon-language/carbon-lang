78c19d60a97fa6c7d3854827ec647f88e48904b4
Dan Gohman <dan433584@gmail.com>
2017-11-30 18:16:49 +0000

    [WebAssembly] Revert r319186 "Support bitcasted function addresses with varargs."
    The patch broke Emscripten's EM_ASM macros, which utiltize unprototyped
    functions.
    
    See https://bugs.llvm.org/show_bug.cgi?id=35385 for details.
    
    llvm-svn: 319452

--- llvm/lib/Target/WebAssembly/WebAssemblyFixFunctionBitcasts.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyFixFunctionBitcasts.cpp
@@ -162,6 +162,11 @@ bool FixFunctionBitcasts::runOnModule(Module &M) {
     if (!Ty)
       continue;
 
+    // Bitcasted vararg functions occur in Emscripten's implementation of
+    // EM_ASM, so suppress wrappers for them for now.
+    if (Ty->isVarArg() || F->isVarArg())
+      continue;
+
     auto Pair = Wrappers.insert(std::make_pair(std::make_pair(F, Ty), nullptr));
     if (Pair.second)
       Pair.first->second = CreateWrapper(F, Ty);
