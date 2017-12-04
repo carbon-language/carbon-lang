feb8343281cf723081c189d3e661209490f92ac8
Marshall Clow <mclow.lists@gmail.com>
2017-12-04 18:59:14 +0000

    Fix PR#35948: generate_n does not accept floating point Size arguments.
    llvm-svn: 319675

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.generate/generate_n.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.generate/generate_n.pass.cpp
@@ -18,27 +18,40 @@
 #include <algorithm>
 #include <cassert>
 
+#include "test_macros.h"
 #include "test_iterators.h"
 #include "user_defined_integral.hpp"
-
-typedef UserDefinedIntegral<unsigned> UDI;
 
 struct gen_test
 {
     int operator()() const {return 2;}
 };
 
-template <class Iter>
+template <class Iter, class Size>
 void
-test()
+test2()
 {
     const unsigned n = 4;
     int ia[n] = {0};
-    assert(std::generate_n(Iter(ia), UDI(n), gen_test()) == Iter(ia+n));
+    assert(std::generate_n(Iter(ia), Size(n), gen_test()) == Iter(ia+n));
     assert(ia[0] == 2);
     assert(ia[1] == 2);
     assert(ia[2] == 2);
     assert(ia[3] == 2);
+}
+
+template <class Iter>
+void
+test()
+{
+    test2<Iter, int>();
+    test2<Iter, unsigned int>();
+    test2<Iter, long>();
+    test2<Iter, unsigned long>();
+    test2<Iter, UserDefinedIntegral<unsigned> >();
+    test2<Iter, float>();
+    test2<Iter, double>();  // this is PR#35498
+    test2<Iter, long double>();
 }
 
 int main()
