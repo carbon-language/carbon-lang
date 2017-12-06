8122ca92c88f4c011c63455970244a9a2cf531fb
Derek Schuff <dschuff@google.com>
2017-12-06 01:38:29 +0000

    [WebAssembly] Only emit stack pointer delcaration in BinFormatWasm assembly
    llvm-svn: 319870

--- llvm/lib/Target/WebAssembly/WebAssemblyAsmPrinter.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyAsmPrinter.cpp
@@ -79,8 +79,10 @@ WebAssemblyTargetStreamer *WebAssemblyAsmPrinter::getTargetStreamer() {
 
 void WebAssemblyAsmPrinter::EmitEndOfAsmFile(Module &M) {
   // Declare the stack pointer.
-  getTargetStreamer()->emitStackPointer(
-      GetExternalSymbolSymbol("__stack_pointer"));
+  if (TM.getTargetTriple().isOSBinFormatWasm()) {
+    getTargetStreamer()->emitStackPointer(
+        GetExternalSymbolSymbol("__stack_pointer"));
+  }
 
   for (const auto &F : M) {
     // Emit function type info for all undefined functions
