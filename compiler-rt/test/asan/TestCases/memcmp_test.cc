ad850f83eeaf74d3f7f9aa85ed92878599c8f959
Tim Northover <tnorthover@apple.com>
2017-12-09 13:35:52 +0000

    Update sanitizer tests for C++14 default in Clang
    llvm-svn: 320251

--- compiler-rt/test/asan/TestCases/memcmp_test.cc
+++ compiler-rt/test/asan/TestCases/memcmp_test.cc
@@ -7,8 +7,8 @@
 
 #include <string.h>
 int main(int argc, char **argv) {
-  char a1[] = {argc, 2, 3, 4};
-  char a2[] = {1, 2*argc, 3, 4};
+  char a1[] = {static_cast<char>(argc), 2, 3, 4};
+  char a2[] = {1, static_cast<char>(2*argc), 3, 4};
   int res = memcmp(a1, a2, 4 + argc);  // BOOM
   // CHECK: AddressSanitizer: stack-buffer-overflow
   // CHECK: {{#0.*memcmp}}
--- compiler-rt/test/msan/msan_print_shadow3.cc
+++ compiler-rt/test/msan/msan_print_shadow3.cc
@@ -6,11 +6,11 @@
 
 int main(void) {
   unsigned long long x = 0; // For 8-byte alignment.
-  char x_s[4] = {0x87, 0x65, 0x43, 0x21};
+  char x_s[4] = {0x77, 0x65, 0x43, 0x21};
   __msan_partial_poison(&x, &x_s, sizeof(x_s));
   __msan_print_shadow(&x, sizeof(x_s));
   return 0;
 }
 
 // CHECK: Shadow map of [{{.*}}), 4 bytes:
-// CHECK: 0x{{.*}}: 87654321 ........ ........ ........
+// CHECK: 0x{{.*}}: 77654321 ........ ........ ........
