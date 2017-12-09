008f63c3c9f1123831a7e57ae2c152470583b365
Kamil Rytarowski <n54@gmx.com>
2017-12-09 00:36:17 +0000

    Disable test/msan/ftime.cc on NetBSD
    Summary:
    ftime(3) has been removed from libc/NetBSD.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, eugenis, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D41018
    
    llvm-svn: 320222

--- compiler-rt/test/msan/ftime.cc
+++ compiler-rt/test/msan/ftime.cc
@@ -1,7 +1,7 @@
 // RUN: %clangxx_msan -O0 -g %s -o %t && %run %t
 
-// ftime() is deprecated on FreeBSD.
-// XFAIL: freebsd
+// ftime() is deprecated on FreeBSD and NetBSD.
+// UNSUPPORTED: freebsd, netbsd
 
 #include <assert.h>
 #include <sys/timeb.h>
