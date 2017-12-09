a85d3481117860f60270e58a65b72e750274e6dc
Kamil Rytarowski <n54@gmx.com>
2017-12-09 00:37:20 +0000

    Disable test/msan/pvalloc.cc on NetBSD
    Summary:
    The pvalloc(3) function is a non-standard extension missing on NetBSD.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, eugenis, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D41014
    
    llvm-svn: 320224

--- compiler-rt/test/msan/pvalloc.cc
+++ compiler-rt/test/msan/pvalloc.cc
@@ -4,7 +4,7 @@
 // RUN: MSAN_OPTIONS=allocator_may_return_null=0 not %run %t psm1 2>&1 | FileCheck %s
 // RUN: MSAN_OPTIONS=allocator_may_return_null=1     %run %t psm1 2>&1
 
-// UNSUPPORTED: win32, freebsd
+// UNSUPPORTED: win32, freebsd, netbsd
 
 // Checks that pvalloc overflows are caught. If the allocator is allowed to
 // return null, the errno should be set to ENOMEM.
