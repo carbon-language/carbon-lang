b43da540cde92a73bf0cc8cfa04b17fe706112d4
Rafael Espindola <rafael.espindola@gmail.com>
2017-12-11 01:06:39 +0000

    Fix test to test what it intended to test.
    The test was added to test that we don't merge read only data
    sections, but the sections in the test were rw.
    
    llvm-svn: 320339

--- lld/test/ELF/icf9.s
+++ lld/test/ELF/icf9.s
@@ -3,18 +3,21 @@
 ### Make sure that we do not merge data.
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
 # RUN: ld.lld %t -o %t2 --icf=all --verbose | FileCheck %s
+# RUN: llvm-readelf -S -W %t2 | FileCheck --check-prefix=SEC %s
 
-# CHECK-NOT: selected .data.d1
-# CHECK-NOT: selected .data.d2
+# SEC:  .rodata      PROGBITS  0000000000200120 000120 000002 00 A 0 0 1
+
+# CHECK-NOT: selected .rodata.d1
+# CHECK-NOT: selected .rodata.d2
 
 .globl _start, d1, d2
 _start:
   ret
 
-.section .data.f1, "a"
+.section .rodata.f1, "a"
 d1:
   .byte 1
 
-.section .data.f2, "a"
+.section .rodata.f2, "a"
 d2:
   .byte 1
