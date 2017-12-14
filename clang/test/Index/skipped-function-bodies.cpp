33eaf177024e6b3126fa05043e229abf1579fd20
Ilya Biryukov <ibiryukov@google.com>
2017-12-14 14:51:17 +0000

    Renamed test file to use proper naming convention
    Also changed the order of CHECK statements.
    CHEKC-NOT must come before CHECK in skipped-function-bodies.cpp
    
    llvm-svn: 320702

--- clang/test/Index/skipped-function-bodies.cpp
+++ clang/test/Index/skipped-function-bodies.cpp
@@ -5,5 +5,5 @@ inline int with_body() { return 10; }
 inline int without_body();
 
 int x = with_body() + without_body();
-// CHECK: warning: inline function 'without_body' is not defined
 // CHECK-NOT: warning: inline function 'with_body' is not defined
+// CHECK: warning: inline function 'without_body' is not defined
