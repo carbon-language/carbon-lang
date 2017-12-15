476a73924e7a789c5a0844280ba0a9deac155d7b
Rui Ueyama <ruiu@google.com>
2017-12-15 00:17:04 +0000

    Do not create an unnecessary object file in a test.
    llvm-svn: 320773

--- lld/test/ELF/invalid-local-symbol-in-dso.s
+++ lld/test/ELF/invalid-local-symbol-in-dso.s
@@ -1,13 +1,12 @@
 # REQUIRES: x86
 
 # We used to crash on this
-# RUN: echo | llvm-mc - -o %t1.o -filetype=obj -triple x86_64-pc-linux
-# RUN: ld.lld %t1.o %p/Inputs/local-symbol-in-dso.so -o %t 2>&1 | \
+# RUN: ld.lld %p/Inputs/local-symbol-in-dso.so -o %t 2>&1 | \
 # RUN:   FileCheck -check-prefix=WARN %s
 # WARN: found local symbol 'foo' in global part of symbol table in file {{.*}}local-symbol-in-dso.so
 
-# RUN: llvm-mc %s -o %t2.o -filetype=obj -triple x86_64-pc-linux
-# RUN: not ld.lld %t2.o %p/Inputs/local-symbol-in-dso.so -o %t
+# RUN: llvm-mc %s -o %t.o -filetype=obj -triple x86_64-pc-linux
+# RUN: not ld.lld %t.o %p/Inputs/local-symbol-in-dso.so -o %t
 
 .globl main
 main:
