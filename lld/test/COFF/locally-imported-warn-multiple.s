ed46ccbb7ab55dcb97127f6a186fc1fb3a4afd37
Shoaib Meenai <smeenai@fb.com>
2017-12-15 08:08:26 +0000

    Repair Windows buildbots after r320792
    Windows paths have colons in them, so the regex will fail there. Just
    match for any character; the rest of the message will restrict the match
    to the path anyway.
    
    llvm-svn: 320793

--- lld/test/COFF/locally-imported-warn-multiple.s
+++ lld/test/COFF/locally-imported-warn-multiple.s
@@ -6,8 +6,8 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-windows-msvc -o %t.obj %s
 # RUN: lld-link /entry:main %T/locally-imported-def.obj %T/locally-imported-imp1.obj %T/locally-imported-imp2.obj %t.obj 2>&1 | FileCheck %s
 
-# CHECK: warning: [[TESTDIR:[^:]+]]locally-imported-imp1.obj: locally defined symbol imported: f (defined in [[TESTDIR]]locally-imported-def.obj)
-# CHECK-NEXT: warning: [[TESTDIR:[^:]+]]locally-imported-imp2.obj: locally defined symbol imported: f (defined in [[TESTDIR]]locally-imported-def.obj)
+# CHECK: warning: [[TESTDIR:.+]]locally-imported-imp1.obj: locally defined symbol imported: f (defined in [[TESTDIR]]locally-imported-def.obj)
+# CHECK-NEXT: warning: [[TESTDIR:.+]]locally-imported-imp2.obj: locally defined symbol imported: f (defined in [[TESTDIR]]locally-imported-def.obj)
 
 .globl main
 main:
