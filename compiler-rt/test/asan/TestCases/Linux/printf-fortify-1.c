de74bdb3d2cf84f7a586dd23a7e0fc993359c791
Maxim Ostapenko <chefmax7@gmail.com>
2017-12-18 15:31:26 +0000

    [asan] Add interceptor for printf_chk
    There could be a situation when a specific DSO was built with FORTIFY_SOURCE option. In case asan-ed binary link against that DSO,
    libasan can't handle the possible memory error because it does not have interceptors for spinrtf_chk, snprintf_chk, vprintf_chk,
    vsnprintf_chk, __fprintf_chk functions. Let's interceptors for them.
    
    Patch by Denis Khalikov.
    
    Differential Revision: https://reviews.llvm.org/D40951
    
    llvm-svn: 320990

---
+++ compiler-rt/test/asan/TestCases/Linux/printf-fortify-1.c
@@ -0,0 +1,18 @@
+// RUN: %clang -fPIC -shared -O2 -D_FORTIFY_SOURCE=2 -D_DSO %s -o %t.so
+// RUN: %clang_asan -o %t %t.so %s
+// RUN: not %run %t 2>&1 | FileCheck %s
+// UNSUPPORTED: android
+#ifdef _DSO
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+__attribute__((noinline)) int foo() {
+  char *write_buffer = (char *)malloc(1);
+  // CHECK: AddressSanitizer: heap-buffer-overflow
+  sprintf(write_buffer, "%s_%s", "one", "two");
+  return write_buffer[0];
+}
+#else
+extern int foo();
+int main() { return foo(); }
+#endif
