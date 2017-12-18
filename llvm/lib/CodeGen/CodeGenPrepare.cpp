b0b67a8d38643f74c4d748cdbfdc8d78cd857319
Serguei Katkov <serguei.katkov@azul.com>
2017-12-18 04:25:07 +0000

    [CGP] Fix the handling select inst in complex addressing mode
    When we put the value in select placeholder we must pass
    the value through simplification tracker due to the value might
    be already simplified and erased.
    
    This is a fix for PR35658.
    
    Reviewers: john.brawn, uabelho
    Reviewed By: john.brawn
    Subscribers: llvm-commits
    Differential Revision: https://reviews.llvm.org/D41251
    
    llvm-svn: 320956

--- llvm/lib/CodeGen/CodeGenPrepare.cpp
+++ llvm/lib/CodeGen/CodeGenPrepare.cpp
@@ -2988,13 +2988,13 @@ private:
                                               ? CurrentBlock
                                               : nullptr };
         assert(Map.find(TrueItem) != Map.end() && "No True Value!");
-        Select->setTrueValue(Map[TrueItem]);
+        Select->setTrueValue(ST.Get(Map[TrueItem]));
         auto *FalseValue = CurrentSelect->getFalseValue();
         ValueInBB FalseItem = { FalseValue, isa<Instruction>(FalseValue)
                                                 ? CurrentBlock
                                                 : nullptr };
         assert(Map.find(FalseItem) != Map.end() && "No False Value!");
-        Select->setFalseValue(Map[FalseItem]);
+        Select->setFalseValue(ST.Get(Map[FalseItem]));
       } else {
         // Must be a Phi node then.
         PHINode *PHI = cast<PHINode>(V);
