5486d2472cf35afb4794a1244f25d6bd2d899f8e
Francis Visoiu Mistrih <francisvm@yahoo.com>
2017-12-18 17:49:27 +0000

    [YAML] Fix mustQuote return type changed in r320996
    llvm-svn: 320997

--- lld/lib/ReaderWriter/MachO/MachONormalizedFileYAML.cpp
+++ lld/lib/ReaderWriter/MachO/MachONormalizedFileYAML.cpp
@@ -257,7 +257,7 @@ template <> struct ScalarTraits<SectionAlignment> {
     return StringRef(); // returning empty string means success
   }
 
-  static bool mustQuote(StringRef) { return false; }
+  static QuotingType mustQuote(StringRef) { return QuotingType::None; }
 };
 
 template <>
@@ -522,7 +522,7 @@ struct ScalarTraits<VMProtect> {
     // Return the empty string on success,
     return StringRef();
   }
-  static bool mustQuote(StringRef) { return false; }
+  static QuotingType mustQuote(StringRef) { return QuotingType::None; }
 };
 
 
@@ -706,7 +706,7 @@ struct ScalarTraits<PackedVersion> {
     // Return the empty string on success,
     return StringRef();
   }
-  static bool mustQuote(StringRef) { return false; }
+  static QuotingType mustQuote(StringRef) { return QuotingType::None; }
 };
 
 template <>
