614f3702d93a45aa421a547c365a66fe5aebde21
Volodymyr Sapsai <vsapsai@apple.com>
2017-12-21 19:42:37 +0000

    [CodeGen] Fix crash when a function taking transparent union is redeclared.
    When a function taking transparent union is declared as taking one of
    union members earlier in the translation unit, clang would hit an
    "Invalid cast" assertion during EmitFunctionProlog. This case
    corresponds to function f1 in test/CodeGen/transparent-union-redecl.c.
    We decided to cast i32 to union because after merging function
    declarations function parameter type becomes int,
    CGFunctionInfo::ArgInfo type matches with ABIArgInfo type, so we decide
    it is a trivial case. But these types should also be castable to
    parameter declaration type which is not the case here.
    
    The fix is in checking for the trivial case if ABIArgInfo type matches with
    parameter declaration type. It exposed inconsistency that we check
    hasScalarEvaluationKind for different types in EmitParmDecl and
    EmitFunctionProlog, and comment says they should match.
    
    Additional tests in Sema/transparent-union.c capture current behavior and make
    sure there are no regressions.
    
    rdar://problem/34949329
    
    Reviewers: rjmccall, rafael
    
    Reviewed By: rjmccall
    
    Subscribers: aemerson, cfe-commits, kristof.beyls
    
    Differential Revision: https://reviews.llvm.org/D41311
    
    llvm-svn: 321296

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2317,7 +2317,7 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
 
       // If we have the trivial case, handle it with no muss and fuss.
       if (!isa<llvm::StructType>(ArgI.getCoerceToType()) &&
-          ArgI.getCoerceToType() == ConvertType(Ty) &&
+          ArgI.getCoerceToType() == ConvertType(Arg->getType()) &&
           ArgI.getDirectOffset() == 0) {
         assert(NumIRArgs == 1);
         llvm::Value *V = FnArgs[FirstIRArg];
@@ -2414,8 +2414,8 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
         break;
       }
 
-      Address Alloca = CreateMemTemp(Ty, getContext().getDeclAlign(Arg),
-                                     Arg->getName());
+      Address Alloca = CreateMemTemp(
+          Arg->getType(), getContext().getDeclAlign(Arg), Arg->getName());
 
       // Pointer to store into.
       Address Ptr = emitAddressAtOffset(*this, Alloca, ArgI);
@@ -2461,9 +2461,9 @@ void CodeGenFunction::EmitFunctionProlog(const CGFunctionInfo &FI,
       }
 
       // Match to what EmitParmDecl is expecting for this type.
-      if (CodeGenFunction::hasScalarEvaluationKind(Ty)) {
+      if (CodeGenFunction::hasScalarEvaluationKind(Arg->getType())) {
         llvm::Value *V =
-          EmitLoadOfScalar(Alloca, false, Ty, Arg->getLocStart());
+            EmitLoadOfScalar(Alloca, false, Arg->getType(), Arg->getLocStart());
         if (isPromoted)
           V = emitArgumentDemotion(*this, Arg, V);
         ArgVals.push_back(ParamValue::forDirect(V));
--- clang/test/CodeGen/kr-func-promote.c
+++ clang/test/CodeGen/kr-func-promote.c
@@ -1,5 +1,5 @@
 // RUN: %clang_cc1 -triple i386-unknown-unknown %s -emit-llvm -o - | FileCheck %s
-// CHECK: i32 @a(i32)
+// CHECK: i32 @a(i32
 
 int a();
 int a(x) short x; {return x;}
