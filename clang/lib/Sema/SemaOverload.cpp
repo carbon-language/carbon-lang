1dbc09363a03be45163db9526895ba16b14ba7c6
Jacob Bandes-Storch <jacob@bandes-stor.ch>
2017-12-31 05:13:03 +0000

    Reverted 321592: [Sema] Improve diagnostics for const- and ref-qualified member functions
    A few tests need to be fixed
    
    llvm-svn: 321593

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -5145,8 +5145,7 @@ Sema::PerformObjectArgumentInitialization(Expr *From,
       *this, From->getLocStart(), From->getType(), FromClassification, Method,
       Method->getParent());
   if (ICS.isBad()) {
-    switch (ICS.Bad.Kind) {
-    case BadConversionSequence::bad_qualifiers: {
+    if (ICS.Bad.Kind == BadConversionSequence::bad_qualifiers) {
       Qualifiers FromQs = FromRecordType.getQualifiers();
       Qualifiers ToQs = DestType.getQualifiers();
       unsigned CVR = FromQs.getCVRQualifiers() & ~ToQs.getCVRQualifiers();
@@ -5159,28 +5158,10 @@ Sema::PerformObjectArgumentInitialization(Expr *From,
           << Method->getDeclName();
         return ExprError();
       }
-      break;
-    }
-
-    case BadConversionSequence::lvalue_ref_to_rvalue:
-    case BadConversionSequence::rvalue_ref_to_lvalue: {
-      bool IsRValueQualified =
-        Method->getRefQualifier() == RefQualifierKind::RQ_RValue;
-      Diag(From->getLocStart(), diag::err_member_function_call_bad_ref)
-        << Method->getDeclName() << FromClassification.isRValue()
-        << IsRValueQualified;
-      Diag(Method->getLocation(), diag::note_previous_decl)
-        << Method->getDeclName();
-      return ExprError();
-    }
-
-    case BadConversionSequence::no_conversion:
-    case BadConversionSequence::unrelated_class:
-      break;
     }
 
     return Diag(From->getLocStart(),
-                diag::err_member_function_call_bad_type)
+                diag::err_implicit_object_parameter_init)
        << ImplicitParamRecordType << FromRecordType << From->getSourceRange();
   }
 
--- clang/test/CXX/over/over.match/over.match.funcs/p4-0x.cpp
+++ clang/test/CXX/over/over.match/over.match.funcs/p4-0x.cpp
@@ -1,4 +1,5 @@
 // RUN: %clang_cc1 -std=c++11 -fsyntax-only -verify %s
+// expected-no-diagnostics
 
 template<typename T> T &lvalue();
 template<typename T> T &&xvalue();
@@ -19,18 +20,6 @@ struct X0 {
 
   void g();
 
-  void c() const; // expected-note {{'c' declared here}}
-  void v() volatile; // expected-note {{'v' declared here}}
-  void r() __restrict__; // expected-note {{'r' declared here}}
-  void cr() const __restrict__; // expected-note {{'cr' declared here}}
-  void cv() const volatile;
-  void vr() volatile __restrict__; // expected-note {{'vr' declared here}}
-  void cvr() const volatile __restrict__;
-
-  void lvalue() &; // expected-note 2 {{'lvalue' declared here}}
-  void const_lvalue() const&;
-  void rvalue() &&; // expected-note {{'rvalue' declared here}}
-
   int &operator+(const X0&) &;
   float &operator+(const X0&) &&;
 
@@ -43,7 +32,7 @@ struct X0 {
   float &h2() const&&;
 };
 
-void X0::g() { // expected-note {{'g' declared here}}
+void X0::g() {
   int &ir1 = f();
   int &ir2 = X0::f();
 }
@@ -79,27 +68,4 @@ void test_ref_qualifier_overloading() {
   int &ir2 = lvalue<X0>().h2();
   float &fr3 = xvalue<X0>().h2();
   float &fr4 = prvalue<X0>().h2();
-}
-
-void test_diagnostics(const volatile X0 &__restrict__ cvr) {
-  cvr.g(); // expected-error {{'this' argument to member function 'g' has type 'const volatile X0', but function is not marked const or volatile}}
-  cvr.c(); // expected-error {{not marked volatile}}
-  cvr.v(); // expected-error {{not marked const}}
-  cvr.r(); // expected-error {{not marked const or volatile}}
-  cvr.cr(); // expected-error {{not marked volatile}}
-  cvr.cv();
-  cvr.vr(); // expected-error {{not marked const}}
-  cvr.cvr();
-
-  lvalue<X0>().lvalue();
-  lvalue<X0>().const_lvalue();
-  lvalue<X0>().rvalue(); // expected-error {{'this' argument to member function 'rvalue' is an lvalue, but function has rvalue ref-qualifier}}
-
-  xvalue<X0>().lvalue(); // expected-error {{'this' argument to member function 'lvalue' is an rvalue, but function has non-const lvalue ref-qualifier}}
-  xvalue<X0>().const_lvalue();
-  xvalue<X0>().rvalue();
-
-  prvalue<X0>().lvalue(); // expected-error {{'this' argument to member function 'lvalue' is an rvalue, but function has non-const lvalue ref-qualifier}}
-  prvalue<X0>().const_lvalue();
-  prvalue<X0>().rvalue();
 }
