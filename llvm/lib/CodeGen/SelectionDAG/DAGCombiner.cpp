8a100dd2a60c714a7d8b6a3620f800c1056fb86d
Amara Emerson <aemerson@apple.com>
2018-01-04 18:38:45 +0000

    [DAGCombine] Ensure SDNode use iterator is incremented properly.
    Fixes an ASAN bug found by oss-fuzz.
    
    llvm-svn: 321813

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -2816,8 +2816,8 @@ SDValue DAGCombiner::useDivRem(SDNode *Node) {
   SDValue Op1 = Node->getOperand(1);
   SDValue combined;
   for (SDNode::use_iterator UI = Op0.getNode()->use_begin(),
-         UE = Op0.getNode()->use_end(); UI != UE;) {
-    SDNode *User = *UI++;
+         UE = Op0.getNode()->use_end(); UI != UE; ++UI) {
+    SDNode *User = *UI;
     if (User == Node || User->use_empty())
       continue;
     // Convert the other matching node(s), too;
