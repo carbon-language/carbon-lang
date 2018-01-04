f0197626d6b065139b7bacfd15c19625a6a3935b
Hubert Tong <hubert.reinterpretcast@gmail.com>
2018-01-04 23:03:48 +0000

    Use POSIX argument syntax in test rewrite-includes-messages.c
    Invoke diff such that options precede operands. Not doing so leads to
    unspecified behaviour under the LSB.
    
    llvm-svn: 321830

--- clang/test/Frontend/rewrite-includes-messages.c
+++ clang/test/Frontend/rewrite-includes-messages.c
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -E -frewrite-includes %s -I%S/Inputs/ | %clang_cc1 -Wall -fsyntax-only -Wunused-macros -x c - 2>&1 > %t.1
 // RUN: %clang_cc1 -I%S/Inputs/ -Wall -Wunused-macros -fsyntax-only %s 2>&1 > %t.2
-// RUN: diff %t.1 %t.2 -u
+// RUN: diff -u %t.1 %t.2
 // expected-no-diagnostics
 
 #include "rewrite-includes-messages.h"
