31db71361549c0428b683462a83beb94dbaae4ac
Sean Eveson <eveson.sean@gmail.com>
2018-01-08 14:43:28 +0000

    Fix test added in r321992 failing on some buildbots.
    llvm-svn: 321995

--- clang/test/CodeGen/stack-size-section.c
+++ clang/test/CodeGen/stack-size-section.c
@@ -1,7 +1,7 @@
-// RUN: %clang_cc1 -triple x86_64-unknown %s -S -o - | FileCheck %s --check-prefix=CHECK-ABSENT
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown %s -S -o - | FileCheck %s --check-prefix=CHECK-ABSENT
 // CHECK-ABSENT-NOT: section .stack_sizes
 
-// RUN: %clang_cc1 -triple x86_64-unknown -fstack-size-section %s -S -o - | FileCheck %s --check-prefix=CHECK-PRESENT
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -fstack-size-section %s -S -o - | FileCheck %s --check-prefix=CHECK-PRESENT
 // CHECK-PRESENT: section .stack_sizes
 
 int foo() { return 42; }
