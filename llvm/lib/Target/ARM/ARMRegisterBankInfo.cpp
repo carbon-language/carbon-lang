0ed7513c83a545e3787c7b1fcf19b96affa604a3
Diana Picus <diana.picus@linaro.org>
2018-01-10 11:13:31 +0000

    [ARM GlobalISel] Map G_FNEG to the FPR bank
    llvm-svn: 322169

--- llvm/lib/Target/ARM/ARMRegisterBankInfo.cpp
+++ llvm/lib/Target/ARM/ARMRegisterBankInfo.cpp
@@ -263,7 +263,8 @@ ARMRegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
   case G_FADD:
   case G_FSUB:
   case G_FMUL:
-  case G_FDIV: {
+  case G_FDIV:
+  case G_FNEG: {
     LLT Ty = MRI.getType(MI.getOperand(0).getReg());
     OperandsMapping =Ty.getSizeInBits() == 64
                           ? &ARM::ValueMappings[ARM::DPR3OpsIdx]
