d657eb65515d6eb6eb437fdf9e56e7a9d4ea784b
Jim Ingham <jingham@apple.com>
2018-01-10 22:52:42 +0000

    Add a test for finding a binary in an app package.
    llvm-svn: 322232

---
+++ lldb/packages/Python/lldbsuite/test/macosx/find-app-in-bundle/TestFindAppInBundle.py
@@ -0,0 +1,62 @@
+"""
+Make sure we can find the binary inside an app bundle.
+"""
+
+from __future__ import print_function
+
+
+import os
+import time
+import re
+import lldb
+from lldbsuite.test.decorators import *
+import lldbsuite.test.lldbutil as lldbutil
+from lldbsuite.test.lldbtest import *
+
+@decorators.skipUnlessDarwin
+class FindAppInMacOSAppBundle(TestBase):
+
+    mydir = TestBase.compute_mydir(__file__)
+
+    NO_DEBUG_INFO_TESTCASE = True
+
+    def test_find_app_in_bundle(self):
+        """There can be many tests in a test case - describe this test here."""
+        self.build()
+        self.main_source_file = lldb.SBFileSpec("main.c")
+        self.find_app_in_bundle_test()
+
+    def setUp(self):
+        # Call super's setUp().
+        TestBase.setUp(self)
+
+    def find_app_in_bundle_test(self):
+        """This reads in the .app, makes sure we get the right binary and can run it."""
+
+        # This function starts a process, "a.out" by default, sets a source
+        # breakpoint, runs to it, and returns the thread, process & target.
+        # It optionally takes an SBLaunchOption argument if you want to pass
+        # arguments or environment variables.
+        exe = os.path.join(os.getcwd(), "TestApp.app")
+        target = self.dbg.CreateTarget(exe)
+        self.assertTrue(target, "Target: TestApp.app is not valid.")
+        exe_module_spec = target.GetExecutable()
+        self.assertTrue(exe_module_spec.GetFilename(), "TestApp")
+
+        bkpt = target.BreakpointCreateBySourceRegex("Set a breakpoint here", self.main_source_file)
+        self.assertTrue(bkpt.GetNumLocations() == 1, "Couldn't set a breakpoint in the main app")
+
+        launch_info = lldb.SBLaunchInfo(None)
+        launch_info.SetWorkingDirectory(self.get_process_working_directory())
+
+        error = lldb.SBError()
+        process = target.Launch(launch_info, error)
+
+        self.assertTrue(process, "Could not create a valid process for TestApp: %s"%(error.GetCString()))
+
+        # Frame #0 should be at our breakpoint.
+        threads = lldbutil.get_threads_stopped_at_breakpoint(process, bkpt)
+
+        self.assertTrue(len(threads) == 1, "Expected 1 thread to stop at breakpoint, %d did."%(len(threads)))
+
+
