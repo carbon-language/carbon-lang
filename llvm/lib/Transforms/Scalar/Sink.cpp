efe6a84e5b5015326ee0825e275bc929f8afee5f
Fiona Glaser <escha@apple.com>
2018-01-11 21:28:57 +0000

    [Sink] Really really fix predicate in legality check
    LoadInst isn't enough; we need to include intrinsics that perform loads too.
    
    All side-effecting intrinsics and such are already covered by the isSafe
    check, so we just need to care about things that read from memory.
    
    D41960, originally from D33179.
    
    llvm-svn: 322311

--- llvm/lib/Transforms/Scalar/Sink.cpp
+++ llvm/lib/Transforms/Scalar/Sink.cpp
@@ -114,7 +114,7 @@ static bool IsAcceptableTarget(Instruction *Inst, BasicBlock *SuccToSinkTo,
   if (SuccToSinkTo->getUniquePredecessor() != Inst->getParent()) {
     // We cannot sink a load across a critical edge - there may be stores in
     // other code paths.
-    if (isa<LoadInst>(Inst))
+    if (Inst->mayReadFromMemory())
       return false;
 
     // We don't want to sink across a critical edge if we don't dominate the
