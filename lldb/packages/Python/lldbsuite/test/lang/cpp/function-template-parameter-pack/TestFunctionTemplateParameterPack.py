3ffbbeabbcd84f68bb1b7765226d0665eb8492a6
Vedant Kumar <vsk@apple.com>
2018-01-12 01:23:52 +0000

    Skip TestFunctionTemplateParameterPack.py, which unexpectedly asserts (PR35920)
    This test stresses expression evaluation support for template functions.
    Currently the support is rudimentary, and running this test causes assertion
    failures in clang. This test cannot be XFAIL'ed because the test harness
    treats assertion failures as unexpected events. For now, the test must be
    skipped.
    
    llvm-svn: 322340

--- lldb/packages/Python/lldbsuite/test/lang/cpp/function-template-parameter-pack/TestFunctionTemplateParameterPack.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/function-template-parameter-pack/TestFunctionTemplateParameterPack.py
@@ -1,6 +1,12 @@
 from lldbsuite.test import lldbinline
 from lldbsuite.test import decorators
 
+# https://bugs.llvm.org/show_bug.cgi?id=35920
+# This test stresses expression evaluation support for template functions.
+# Currently the support is rudimentary, and running this test causes assertion
+# failures in clang. This test cannot be XFAIL'ed because the test harness
+# treats assertion failures as unexpected events. For now, the test must be
+# skipped.
 lldbinline.MakeInlineTest(
     __file__, globals(), [
-        decorators.expectedFailureAll(bugnumber="rdar://problem/32096064")])
+        decorators.skipIf])
