8ed89551e2eedaec1d2b049b4a985fe2df9be4db
Alexey Bataev <a.bataev@hotmail.com>
2018-01-16 17:41:04 +0000

    [OPENMP] Add support for `depend` clauses on `target parallel for` directives.
    Added codegen for `depend` clause on `#pragma omp target parallel for`
    directives.
    
    llvm-svn: 322577

--- clang/lib/Sema/SemaOpenMP.cpp
+++ clang/lib/Sema/SemaOpenMP.cpp
@@ -8156,12 +8156,12 @@ static OpenMPDirectiveKind getOpenMPCaptureRegionForClause(
     case OMPD_target_parallel:
     case OMPD_target_teams_distribute:
     case OMPD_target_teams_distribute_simd:
+    case OMPD_target_parallel_for:
       CaptureRegion = OMPD_task;
       break;
     case OMPD_target_teams_distribute_parallel_for:
     case OMPD_target_teams_distribute_parallel_for_simd:
     case OMPD_target_data:
-    case OMPD_target_parallel_for:
     case OMPD_target_parallel_for_simd:
       // Do not capture device-clause expressions.
       break;
