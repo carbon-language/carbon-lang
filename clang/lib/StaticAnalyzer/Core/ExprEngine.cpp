c8032c623300dac2c47cf3c747529046b3ba1f44
Artem Dergachev <artem.dergachev@gmail.com>
2018-01-17 23:03:25 +0000

    [analyzer] operator new: Fix ambigious type name.
    Hopefully fixes an MSVC buildbot failure.
    
    llvm-svn: 322781

--- clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngine.cpp
@@ -65,7 +65,7 @@ REGISTER_TRAIT_WITH_PROGRAMSTATE(InitializedTemporariesSet,
 
 typedef llvm::ImmutableMap<std::pair<const CXXNewExpr *,
                            const LocationContext *>, SVal>
-    CXXNewAllocatorValuesTy;
+    CXXNewAllocatorValuesMap;
 
 // Keeps track of return values of various operator new() calls between
 // evaluation of the inlined operator new(), through the constructor call,
@@ -75,7 +75,8 @@ typedef llvm::ImmutableMap<std::pair<const CXXNewExpr *,
 // is evaluated, and removed from the stack when the whole CXXNewExpr
 // is fully evaluated.
 // Probably do something similar to the previous trait as well.
-REGISTER_TRAIT_WITH_PROGRAMSTATE(CXXNewAllocatorValues, CXXNewAllocatorValuesTy)
+REGISTER_TRAIT_WITH_PROGRAMSTATE(CXXNewAllocatorValues,
+                                 CXXNewAllocatorValuesMap)
 
 //===----------------------------------------------------------------------===//
 // Engine construction and deletion.
