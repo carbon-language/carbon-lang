a1aba44ffa742e02b0b0cc555d8359d456402fc2
Jan Vesely <jan.vesely@rutgers.edu>
2018-01-18 21:11:56 +0000

    half_log2: Implement using log2
    Passes CTS on carrizo
    v2: Use full precision implementation
    
    Reviewer: Jeroen Ketema <j.ketema@xs4all.nl>
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 322895

--- libclc/generic/include/clc/clc.h
+++ libclc/generic/include/clc/clc.h
@@ -76,6 +76,7 @@
 #include <clc/math/half_exp2.h>
 #include <clc/math/half_log.h>
 #include <clc/math/half_log10.h>
+#include <clc/math/half_log2.h>
 #include <clc/math/half_rsqrt.h>
 #include <clc/math/half_sqrt.h>
 #include <clc/math/hypot.h>
