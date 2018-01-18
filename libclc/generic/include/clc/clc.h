398108b91e0a7fa01d8216406bc6a0c6f1e2ff43
Jan Vesely <jan.vesely@rutgers.edu>
2018-01-18 21:11:58 +0000

    half_recip: Implement using 1/x
    Passes CTS on carrizo
    v2: Use full precision implementation
    
    Reviewer: Jeroen Ketema <j.ketema@xs4all.nl>
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 322896

--- libclc/generic/include/clc/clc.h
+++ libclc/generic/include/clc/clc.h
@@ -77,6 +77,7 @@
 #include <clc/math/half_log.h>
 #include <clc/math/half_log10.h>
 #include <clc/math/half_log2.h>
+#include <clc/math/half_recip.h>
 #include <clc/math/half_rsqrt.h>
 #include <clc/math/half_sqrt.h>
 #include <clc/math/hypot.h>
