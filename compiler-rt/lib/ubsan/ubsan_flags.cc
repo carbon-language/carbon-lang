564f845b74833e28b82e14e075d8e93ef3562bac
Francis Ricci <francisjricci@gmail.com>
2018-01-23 19:54:02 +0000

    [ubsan] Add preinit initializer for ubsan
    Summary:
    Now that ubsan does function interception (for signals), we
    need to ensure that ubsan is initialized before any library
    constructors are called. Otherwise, if a constructor calls
    sigaction, ubsan will intercept in an unitialized state, which
    will cause a crash.
    
    This patch is a partial revert of r317757, which removed
    preinit arrays for ubsan.
    
    Reviewers: vitalybuka, eugenis, pcc
    
    Subscribers: kubamracek, mgorny, llvm-commits, #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D42389
    
    llvm-svn: 323249

--- compiler-rt/lib/ubsan/ubsan_flags.cc
+++ compiler-rt/lib/ubsan/ubsan_flags.cc
@@ -26,6 +26,15 @@ const char *MaybeCallUbsanDefaultOptions() {
   return (&__ubsan_default_options) ? __ubsan_default_options() : "";
 }
 
+static const char *GetFlag(const char *flag) {
+  // We cannot call getenv() from inside a preinit array initializer
+  if (SANITIZER_CAN_USE_PREINIT_ARRAY) {
+    return GetEnv(flag);
+  } else {
+    return getenv(flag);
+  }
+}
+
 Flags ubsan_flags;
 
 void Flags::SetDefaults() {
@@ -47,7 +56,7 @@ void InitializeFlags() {
     CommonFlags cf;
     cf.CopyFrom(*common_flags());
     cf.print_summary = false;
-    cf.external_symbolizer_path = getenv("UBSAN_SYMBOLIZER_PATH");
+    cf.external_symbolizer_path = GetFlag("UBSAN_SYMBOLIZER_PATH");
     OverrideCommonFlags(cf);
   }
 
@@ -61,7 +70,7 @@ void InitializeFlags() {
   // Override from user-specified string.
   parser.ParseString(MaybeCallUbsanDefaultOptions());
   // Override from environment variable.
-  parser.ParseString(getenv("UBSAN_OPTIONS"));
+  parser.ParseString(GetFlag("UBSAN_OPTIONS"));
   InitializeCommonFlags();
   if (Verbosity()) ReportUnrecognizedFlags();
 
