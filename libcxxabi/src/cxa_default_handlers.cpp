a674a8fd1e1a9db00bb0d43b656b17cf3615601c
Peter Collingbourne <peter@pcc.me.uk>
2018-01-25 01:38:43 +0000

    [libc++abi] Fix unused function when building with LIBCXXABI_SILENT_TERMINATE
    This fixes:
    src/cxa_default_handlers.cpp:25:13: error: unused function 'demangling_terminate_handler' [-Werror,-Wunused-function]
    
    Patch by Thomas Anderson!
    
    Differential Revision: https://reviews.llvm.org/D42399
    
    llvm-svn: 323397

--- libcxxabi/src/cxa_default_handlers.cpp
+++ libcxxabi/src/cxa_default_handlers.cpp
@@ -19,6 +19,7 @@
 #include "cxa_exception.hpp"
 #include "private_typeinfo.h"
 
+#if !defined(LIBCXXABI_SILENT_TERMINATE)
 static const char* cause = "uncaught";
 
 __attribute__((noreturn))
@@ -84,7 +85,6 @@ static void demangling_unexpected_handler()
     std::terminate();
 }
 
-#if !defined(LIBCXXABI_SILENT_TERMINATE)
 static std::terminate_handler default_terminate_handler = demangling_terminate_handler;
 static std::terminate_handler default_unexpected_handler = demangling_unexpected_handler;
 #else
