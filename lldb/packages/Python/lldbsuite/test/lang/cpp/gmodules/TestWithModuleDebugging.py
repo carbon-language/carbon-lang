af379085ef38ba3c6c7315f7064c1faeeb4a73e6
Pavel Labath <labath@google.com>
2018-01-30 09:39:22 +0000

    Skip TestWithModuleDebugging on i386 linux (pr36146)
    It fails due to an assertion (if these are enabled).
    
    llvm-svn: 323736

--- lldb/packages/Python/lldbsuite/test/lang/cpp/gmodules/TestWithModuleDebugging.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/gmodules/TestWithModuleDebugging.py
@@ -9,6 +9,7 @@ class TestWithGmodulesDebugInfo(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
+    @skipIf(bugnumber="llvm.org/pr36146", oslist=["linux"], archs=["i386"])
     @add_test_categories(["gmodules"])
     def test_specialized_typedef_from_pch(self):
         self.build()
