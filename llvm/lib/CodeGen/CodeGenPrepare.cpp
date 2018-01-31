be58a220e96d5e7feabeed7211e75da1af0bc387
Daniel Neilson <dneilson@azul.com>
2018-01-31 17:24:53 +0000

    [CodeGenPrepare] Improve source and dest alignments of memory intrinsics independently
    Summary:
      This change is part of step five in the series of changes to remove alignment argument from
    memcpy/memmove/memset in favour of alignment attributes. In particular, this changes the
    CodeGenPrepare pass to be more aggressive in improving the source and destination alignments
    of memcpy/memmove/memset by exploiting our new ability to record independent alignments
    for each argument.
    
    Steps:
    Step 1) Remove alignment parameter and create alignment parameter attributes for
    memcpy/memmove/memset. ( rL322965, rC322964, rL322963 )
    Step 2) Expand the IRBuilder API to allow creation of memcpy/memmove with differing
    source and dest alignments. ( rL323597 )
    Step 3) Update Clang to use the new IRBuilder API. ( rC323617 )
    Step 4) Update Polly to use the new IRBuilder API. ( rL323618 )
    Step 5) Update LLVM passes that create memcpy/memmove calls to use the new IRBuilder API,
    and those that use use MemIntrinsicInst::[get|set]Alignment() to use [get|set]DestAlignment()
    and [get|set]SourceAlignment() instead. ( rL323886 )
    Step 6) Remove the single-alignment IRBuilder API for memcpy/memmove, and the
    MemIntrinsicInst::[get|set]Alignment() methods.
    
    Reference
       http://lists.llvm.org/pipermail/llvm-dev/2015-August/089384.html
       http://lists.llvm.org/pipermail/llvm-commits/Week-of-Mon-20151109/312083.html
    
    llvm-svn: 323891

--- llvm/lib/CodeGen/CodeGenPrepare.cpp
+++ llvm/lib/CodeGen/CodeGenPrepare.cpp
@@ -1606,11 +1606,14 @@ bool CodeGenPrepare::optimizeCallInst(CallInst *CI, bool &ModifiedDT) {
     // If this is a memcpy (or similar) then we may be able to improve the
     // alignment
     if (MemIntrinsic *MI = dyn_cast<MemIntrinsic>(CI)) {
-      unsigned Align = getKnownAlignment(MI->getDest(), *DL);
-      if (MemTransferInst *MTI = dyn_cast<MemTransferInst>(MI))
-        Align = std::min(Align, getKnownAlignment(MTI->getSource(), *DL));
-      if (Align > MI->getAlignment())
-        MI->setAlignment(Align);
+      unsigned DestAlign = getKnownAlignment(MI->getDest(), *DL);
+      if (DestAlign > MI->getDestAlignment())
+        MI->setDestAlignment(DestAlign);
+      if (MemTransferInst *MTI = dyn_cast<MemTransferInst>(MI)) {
+        unsigned SrcAlign = getKnownAlignment(MTI->getSource(), *DL);
+        if (SrcAlign > MTI->getSourceAlignment())
+          MTI->setSourceAlignment(SrcAlign);
+      }
     }
   }
 
