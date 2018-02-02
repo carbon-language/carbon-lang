e6bba9bc6f8b3f6f835ea99e26130f48d0665a04
Kamil Rytarowski <n54@gmx.com>
2018-02-02 18:45:33 +0000

    Add new NetBSD interceptors: devname(3), devname_r(3)
    Summary:
    devname, devname_r - get device name
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: joerg, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: kubamracek, llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D42053
    
    llvm-svn: 324120

---
+++ compiler-rt/test/sanitizer_common/TestCases/NetBSD/devname.cc
@@ -0,0 +1,22 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+
+int main(void) {
+  struct stat st;
+  char *name;
+
+  if (stat("/dev/null", &st))
+    exit(1);
+
+  if (!(name = devname(st.st_rdev, S_ISCHR(st.st_mode) ? S_IFCHR : S_IFBLK)))
+    exit(1);
+
+  printf("%s\n", name);
+
+  // CHECK: null
+
+  return 0;
+}
---
+++ compiler-rt/test/sanitizer_common/TestCases/NetBSD/devname_r.cc
@@ -0,0 +1,27 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck
+
+#include <sys/cdefs.h>
+#include <sys/stat.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  struct stat st;
+  char name[10];
+  mode_t type;
+
+  if (stat("/dev/null", &st))
+    exit(1);
+
+  type = S_ISCHR(st.st_mode) ? S_IFCHR : S_IFBLK;
+
+  if (devname_r(st.st_rdev, type, name, __arraycount(name)))
+    exit(1);
+
+  printf("%s\n", name);
+
+  // CHECK: null
+
+  return 0;
+}
