9b5ff7a6bcacbe7896e395f155f8be80b4f47433
Eric Fiselier <eric@efcs.ca>
2018-02-03 01:45:35 +0000

    Work around Clang bug introduced in r324062
    When Clang encounters an already invalid class declaration, it can
    emit incorrect diagnostics about the exception specification on
    some of its members. This patch temporarily works around that
    incorrect diagnostic.
    
    The clang bug was introduced in r324062.
    
    llvm-svn: 324164

--- libcxx/test/libcxx/input.output/file.streams/fstreams/traits_mismatch.fail.cpp
+++ libcxx/test/libcxx/input.output/file.streams/fstreams/traits_mismatch.fail.cpp
@@ -21,5 +21,10 @@ int main()
 	std::basic_fstream<char, std::char_traits<wchar_t> > f;
 //  expected-error-re@ios:* {{static_assert failed{{.*}} "traits_type::char_type must be the same type as CharT"}}
 //  expected-error-re@streambuf:* {{static_assert failed{{.*}} "traits_type::char_type must be the same type as CharT"}}
+
+// FIXME: As of commit r324062 Clang incorrectly generates a diagnostic about mismatching
+// exception specifications for types which are already invalid for one reason or another.
+// For now we tolerate this diagnostic.
+// expected-error@ostream:* 0-1 {{exception specification of overriding function is more lax than base version}}
 }
 
