3c23f6668b31c6645d5f4b1c99cc7fa04d8d9c7a
Daniel Neilson <dneilson@azul.com>
2018-02-05 22:23:58 +0000

    [LowerMemIntrinsics] Update uses of deprecated MemIntrinsic::getAlignment API (NFC)
    Summary:
    This change is part of step five in the series of changes to remove alignment argument from
    memcpy/memmove/memset in favour of alignment attributes. In particular, this changes the
    LowerMemIntrinsics pass to cease using the old getAlignment() API of MemoryIntrinsic in
    favour of getting source & dest specific alignments through the new API.
    
    Steps:
    Step 1) Remove alignment parameter and create alignment parameter attributes for
    memcpy/memmove/memset. ( rL322965, rC322964, rL322963 )
    Step 2) Expand the IRBuilder API to allow creation of memcpy/memmove with differing
    source and dest alignments. ( rL323597 )
    Step 3) Update Clang to use the new IRBuilder API. ( rC323617 )
    Step 4) Update Polly to use the new IRBuilder API. ( rL323618 )
    Step 5) Update LLVM passes that create memcpy/memmove calls to use the new IRBuilder API,
    and those that use use MemIntrinsicInst::[get|set]Alignment() to use [get|set]DestAlignment()
    and [get|set]SourceAlignment() instead. ( rL323886, rL323891, rL324148, rL324273 )
    Step 6) Remove the single-alignment IRBuilder API for memcpy/memmove, and the
    MemIntrinsicInst::[get|set]Alignment() methods.
    
    Reference
       http://lists.llvm.org/pipermail/llvm-dev/2015-August/089384.html
       http://lists.llvm.org/pipermail/llvm-commits/Week-of-Mon-20151109/312083.html
    
    llvm-svn: 324278

--- llvm/lib/Transforms/Utils/LowerMemIntrinsics.cpp
+++ llvm/lib/Transforms/Utils/LowerMemIntrinsics.cpp
@@ -409,8 +409,8 @@ void llvm::expandMemCpyAsLoop(MemCpyInst *Memcpy,
                               /* SrcAddr */ Memcpy->getRawSource(),
                               /* DstAddr */ Memcpy->getRawDest(),
                               /* CopyLen */ CI,
-                              /* SrcAlign */ Memcpy->getAlignment(),
-                              /* DestAlign */ Memcpy->getAlignment(),
+                              /* SrcAlign */ Memcpy->getSourceAlignment(),
+                              /* DestAlign */ Memcpy->getDestAlignment(),
                               /* SrcIsVolatile */ Memcpy->isVolatile(),
                               /* DstIsVolatile */ Memcpy->isVolatile(),
                               /* TargetTransformInfo */ TTI);
@@ -419,8 +419,8 @@ void llvm::expandMemCpyAsLoop(MemCpyInst *Memcpy,
                                 /* SrcAddr */ Memcpy->getRawSource(),
                                 /* DstAddr */ Memcpy->getRawDest(),
                                 /* CopyLen */ Memcpy->getLength(),
-                                /* SrcAlign */ Memcpy->getAlignment(),
-                                /* DestAlign */ Memcpy->getAlignment(),
+                                /* SrcAlign */ Memcpy->getSourceAlignment(),
+                                /* DestAlign */ Memcpy->getDestAlignment(),
                                 /* SrcIsVolatile */ Memcpy->isVolatile(),
                                 /* DstIsVolatile */ Memcpy->isVolatile(),
                                 /* TargetTransfomrInfo */ TTI);
@@ -432,8 +432,8 @@ void llvm::expandMemMoveAsLoop(MemMoveInst *Memmove) {
                     /* SrcAddr */ Memmove->getRawSource(),
                     /* DstAddr */ Memmove->getRawDest(),
                     /* CopyLen */ Memmove->getLength(),
-                    /* SrcAlign */ Memmove->getAlignment(),
-                    /* DestAlign */ Memmove->getAlignment(),
+                    /* SrcAlign */ Memmove->getSourceAlignment(),
+                    /* DestAlign */ Memmove->getDestAlignment(),
                     /* SrcIsVolatile */ Memmove->isVolatile(),
                     /* DstIsVolatile */ Memmove->isVolatile());
 }
@@ -443,6 +443,6 @@ void llvm::expandMemSetAsLoop(MemSetInst *Memset) {
                    /* DstAddr */ Memset->getRawDest(),
                    /* CopyLen */ Memset->getLength(),
                    /* SetValue */ Memset->getValue(),
-                   /* Alignment */ Memset->getAlignment(),
+                   /* Alignment */ Memset->getDestAlignment(),
                    Memset->isVolatile());
 }
