9dc740d0a9ae2b14fbf897d021217b6b5ce54dcd
George Rimar <grimar@accesssoftek.com>
2018-02-06 12:20:05 +0000

    [ELF] - Re-commit r324322 "Use InitTargetOptionsFromCodeGenFlags/ParseCommandLineOptions for parsing LTO options.".
    With fix:
    Keep logic that ignores -plugin-opt=mcpu=x86-64 -plugin-opt=thinlto,
    add checks for those to testcases.
    
    Original commit message:
    
    [ELF] - Use InitTargetOptionsFromCodeGenFlags/ParseCommandLineOptions for parsing LTO options.
    
    gold plugin uses InitTargetOptionsFromCodeGenFlags + 
    ParseCommandLineOptions for parsing LTO options.
    Patch do the same change for LLD.
    
    Such change helps to avoid parsing/whitelisting LTO
    plugin options again on linker side, what can help LLD
    to automatically support new -plugin-opt=xxx options
    passed.
    
    Differential revision: https://reviews.llvm.org/D42733
    
    llvm-svn: 324340

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -42,6 +42,7 @@
 #include "lld/Common/Driver.h"
 #include "lld/Common/ErrorHandler.h"
 #include "lld/Common/Memory.h"
+#include "lld/Common/TargetOptionsCommandFlags.h"
 #include "lld/Common/Threads.h"
 #include "lld/Common/Version.h"
 #include "llvm/ADT/StringExtras.h"
@@ -252,18 +253,11 @@ void LinkerDriver::addLibrary(StringRef Name) {
 // LTO calls LLVM functions to compile bitcode files to native code.
 // Technically this can be delayed until we read bitcode files, but
 // we don't bother to do lazily because the initialization is fast.
-static void initLLVM(opt::InputArgList &Args) {
+static void initLLVM() {
   InitializeAllTargets();
   InitializeAllTargetMCs();
   InitializeAllAsmPrinters();
   InitializeAllAsmParsers();
-
-  // Parse and evaluate -mllvm options.
-  std::vector<const char *> V;
-  V.push_back("lld (LLVM option parsing)");
-  for (auto *Arg : Args.filtered(OPT_mllvm))
-    V.push_back(Arg->getValue());
-  cl::ParseCommandLineOptions(V.size(), V.data());
 }
 
 // Some command line options or some combinations of them are not allowed.
@@ -372,7 +366,7 @@ void LinkerDriver::main(ArrayRef<const char *> ArgsArr, bool CanExitEarly) {
   }
 
   readConfigs(Args);
-  initLLVM(Args);
+  initLLVM();
   createFiles(Args);
   inferMachineType();
   setConfigs(Args);
@@ -696,6 +690,7 @@ void LinkerDriver::readConfigs(opt::InputArgList &Args) {
   Config->ZWxneeded = hasZOption(Args, "wxneeded");
 
   // Parse LTO plugin-related options for compatibility with gold.
+  std::vector<const char *> LTOOptions({Config->Argv[0].data()});
   for (auto *Arg : Args.filtered(OPT_plugin_opt)) {
     StringRef S = Arg->getValue();
     if (S == "disable-verify")
@@ -710,10 +705,13 @@ void LinkerDriver::readConfigs(opt::InputArgList &Args) {
       Config->ThinLTOJobs = parseInt(S.substr(5), Arg);
     else if (!S.startswith("/") && !S.startswith("-fresolution=") &&
              !S.startswith("-pass-through=") && !S.startswith("mcpu=") &&
-             !S.startswith("thinlto") && S != "-function-sections" &&
-             S != "-data-sections")
-      error(Arg->getSpelling() + ": unknown option: " + S);
+             !S.startswith("thinlto"))
+      LTOOptions.push_back(S.data());
   }
+  // Parse and evaluate -mllvm options.
+  for (auto *Arg : Args.filtered(OPT_mllvm))
+    LTOOptions.push_back(Arg->getValue());
+  cl::ParseCommandLineOptions(LTOOptions.size(), LTOOptions.data());
 
   if (Config->LTOO > 3)
     error("invalid optimization level for LTO: " + Twine(Config->LTOO));
