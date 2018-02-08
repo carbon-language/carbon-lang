2a81d30471a8a39accf969d6277aeb1d5f37032a
Marshall Clow <mclow.lists@gmail.com>
2018-02-08 16:25:57 +0000

    Clean up string's deduction guides tests. Mark old versions of clang as unsupported, b/c they don't have deduction guides, even in C++17 mode
    llvm-svn: 324619

--- libcxx/test/std/strings/basic.string/string.cons/iter_alloc.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.cons/iter_alloc.pass.cpp
@@ -13,17 +13,6 @@
 //   basic_string(InputIterator begin, InputIterator end,
 //   const Allocator& a = Allocator());
 
-// template<class InputIterator,
-//      class Allocator = allocator<typename iterator_traits<InputIterator>::value_type>>
-//  basic_string(InputIterator, InputIterator, Allocator = Allocator())
-//    -> basic_string<typename iterator_traits<InputIterator>::value_type,
-//                 char_traits<typename iterator_traits<InputIterator>::value_type>,
-//                 Allocator>;
-//
-//	The deduction guide shall not participate in overload resolution if InputIterator
-//  is a type that does not qualify as an input iterator, or if Allocator is a type
-//  that does not qualify as an allocator.
-
 
 #include <string>
 #include <iterator>
@@ -127,51 +116,5 @@ int main()
     test(input_iterator<const char*>(s), input_iterator<const char*>(s+50));
     test(input_iterator<const char*>(s), input_iterator<const char*>(s+50), A());
     }
-#endif
-
-#if 0
-//  Test deduction guides
-#if TEST_STD_VER > 14
-    {
-    const char* s = "12345678901234";
-    std::basic_string s1{s, s+10, std::allocator<char>{}};
-    using S = decltype(s1); // what type did we get?
-    static_assert(std::is_same_v<S::value_type,                      char>,  "");
-    static_assert(std::is_same_v<S::traits_type,    std::char_traits<char>>, "");
-    static_assert(std::is_same_v<S::allocator_type,   std::allocator<char>>, "");
-    assert(s1.size() == 10);
-    assert(s1.compare(0, s1.size(), s, s1.size()) == 0);
-    }
-    {
-    const wchar_t* s = L"12345678901234";
-    std::basic_string s1{s, s+10, test_allocator<wchar_t>{}};
-    using S = decltype(s1); // what type did we get?
-    static_assert(std::is_same_v<S::value_type,                      wchar_t>,  "");
-    static_assert(std::is_same_v<S::traits_type,    std::char_traits<wchar_t>>, "");
-    static_assert(std::is_same_v<S::allocator_type,   test_allocator<wchar_t>>, "");
-    assert(s1.size() == 10);
-    assert(s1.compare(0, s1.size(), s, s1.size()) == 0);
-    }
-    {
-    const char16_t* s = u"12345678901234";
-    std::basic_string s1{s, s+10, min_allocator<char16_t>{}};
-    using S = decltype(s1); // what type did we get?
-    static_assert(std::is_same_v<S::value_type,                      char16_t>,  "");
-    static_assert(std::is_same_v<S::traits_type,    std::char_traits<char16_t>>, "");
-    static_assert(std::is_same_v<S::allocator_type,    min_allocator<char16_t>>, "");
-    assert(s1.size() == 10);
-    assert(s1.compare(0, s1.size(), s, s1.size()) == 0);
-    }
-    {
-    const char32_t* s = U"12345678901234";
-    std::basic_string s1{s, s+10, explicit_allocator<char32_t>{}};
-    using S = decltype(s1); // what type did we get?
-    static_assert(std::is_same_v<S::value_type,                        char32_t>,  "");
-    static_assert(std::is_same_v<S::traits_type,      std::char_traits<char32_t>>, "");
-    static_assert(std::is_same_v<S::allocator_type, explicit_allocator<char32_t>>, "");
-    assert(s1.size() == 10);
-    assert(s1.compare(0, s1.size(), s, s1.size()) == 0);
-    }
-#endif
 #endif
 }
--- libcxx/test/std/strings/basic.string/string.cons/iter_alloc_deduction.fail.cpp
+++ libcxx/test/std/strings/basic.string/string.cons/iter_alloc_deduction.fail.cpp
@@ -9,7 +9,7 @@
 
 // <string>
 // UNSUPPORTED: c++98, c++03, c++11, c++14
-// XFAIL: c++17
+// UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9, clang.4-0
 
 // template<class InputIterator,
 //      class Allocator = allocator<typename iterator_traits<InputIterator>::value_type>>
