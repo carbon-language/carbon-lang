6aafc8b30816e234deb5c395b7f8476e615a3ad1
Marshall Clow <mclow.lists@gmail.com>
2018-02-08 17:06:08 +0000

    Once more, with feeling. Spell 'clang-4.0' correctly this time
    llvm-svn: 324624

--- libcxx/test/std/strings/basic.string/string.cons/iter_alloc_deduction.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.cons/iter_alloc_deduction.pass.cpp
@@ -9,7 +9,7 @@
 
 // <string>
 // UNSUPPORTED: c++98, c++03, c++11, c++14
-// UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9, clang.4-0
+// UNSUPPORTED: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9, clang-4.0
 
 // template<class InputIterator>
 //   basic_string(InputIterator begin, InputIterator end,
