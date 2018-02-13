413495fbe7b0408803436c9695aa087d3b174b9f
Aditya Nandakumar <aditya_nandakumar@apple.com>
2018-02-13 20:09:11 +0000

    [GISel]: Make Pattern matcher for FADD commutative
    llvm-svn: 325043

--- llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
+++ llvm/include/llvm/CodeGen/GlobalISel/MIPatternMatch.h
@@ -197,9 +197,9 @@ m_GMul(const LHS &L, const RHS &R) {
 }
 
 template <typename LHS, typename RHS>
-inline BinaryOp_match<LHS, RHS, TargetOpcode::G_FADD> m_GFAdd(const LHS &L,
-                                                              const RHS &R) {
-  return BinaryOp_match<LHS, RHS, TargetOpcode::G_FADD>(L, R);
+inline BinaryOp_match<LHS, RHS, TargetOpcode::G_FADD, true>
+m_GFAdd(const LHS &L, const RHS &R) {
+  return BinaryOp_match<LHS, RHS, TargetOpcode::G_FADD, true>(L, R);
 }
 
 // Helper for unary instructions (G_[ZSA]EXT/G_TRUNC) etc
