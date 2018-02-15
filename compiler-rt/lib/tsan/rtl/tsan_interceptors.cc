e7c26e7929fe4af83c0642e0e6ec526ecb29143a
Kamil Rytarowski <n54@gmx.com>
2018-02-15 14:58:15 +0000

    Enable syscall-specific functions in TSan/NetBSD
    NetBSD ships now with netbsd_syscall_hooks.h and requires support
    for TSan specific features to be enabled.
    
    This is follow up of:
    
      D42048: Add NetBSD syscall hooks skeleton in sanitizers
    
    Sponsored by <The NetBSD Foundation>
    
    llvm-svn: 325245

--- compiler-rt/lib/tsan/rtl/tsan_interceptors.cc
+++ compiler-rt/lib/tsan/rtl/tsan_interceptors.cc
@@ -2424,7 +2424,7 @@ struct ScopedSyscall {
   }
 };
 
-#if !SANITIZER_FREEBSD && !SANITIZER_MAC && !SANITIZER_NETBSD
+#if !SANITIZER_FREEBSD && !SANITIZER_MAC
 static void syscall_access_range(uptr pc, uptr p, uptr s, bool write) {
   TSAN_SYSCALL();
   MemoryAccessRange(thr, pc, p, s, write);
