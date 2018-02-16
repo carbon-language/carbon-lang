bab5252789438ffb1f01d5ece0809254762183e5
Kamil Rytarowski <n54@gmx.com>
2018-02-16 14:45:20 +0000

    Add initial XRay support for NetBSD
    Summary:
    Reuse the existing FreeBSD code as it is.
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: dberris, rnk, vitalybuka
    
    Reviewed By: dberris
    
    Subscribers: mclow.lists, emaste, mgorny, llvm-commits, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D43370
    
    llvm-svn: 325345

--- compiler-rt/lib/xray/xray_x86_64.cc
+++ compiler-rt/lib/xray/xray_x86_64.cc
@@ -3,7 +3,8 @@
 #include "xray_defs.h"
 #include "xray_interface_internal.h"
 
-#if SANITIZER_FREEBSD
+#if SANITIZER_FREEBSD || SANITIZER_NETBSD
+#include <sys/types.h>
 #include <sys/sysctl.h>
 #endif
 
@@ -76,7 +77,7 @@ uint64_t getTSCFrequency() XRAY_NEVER_INSTRUMENT {
   }
   return TSCFrequency == -1 ? 0 : static_cast<uint64_t>(TSCFrequency);
 }
-#elif SANITIZER_FREEBSD
+#elif SANITIZER_FREEBSD || SANITIZER_NETBSD
 uint64_t getTSCFrequency() XRAY_NEVER_INSTRUMENT {
     long long TSCFrequency = -1;
     size_t tscfreqsz = sizeof(TSCFrequency);
