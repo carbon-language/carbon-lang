8ad69f2d1f7f420b776bbc6acb4fc34c59cff8e8
Dylan McKay <me@dylanmckay.io>
2018-02-19 10:46:16 +0000

    [AVR] Set the program address space in the data layout
    This is accompanied by r325481 in LLVM.
    
    llvm-svn: 325483

--- clang/lib/Basic/Targets/AVR.h
+++ clang/lib/Basic/Targets/AVR.h
@@ -55,7 +55,7 @@ public:
     WIntType = SignedInt;
     Char32Type = UnsignedLong;
     SigAtomicType = SignedChar;
-    resetDataLayout("e-p:16:8-i8:8-i16:8-i32:8-i64:8-f32:8-f64:8-n8-a:8");
+    resetDataLayout("e-P1-p:16:8-i8:8-i16:8-i32:8-i64:8-f32:8-f64:8-n8-a:8");
   }
 
   void getTargetDefines(const LangOptions &Opts,
