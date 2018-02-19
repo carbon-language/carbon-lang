9a2a996c1c086942d597f5d94f30b8b13760a720
Dylan McKay <me@dylanmckay.io>
2018-02-19 10:40:59 +0000

    [AVR] Set the program address space in the data layout
    This adds the program memory address space setting to the AVR data
    layout.
    
    This setting was very recently added under r325479.
    
    At the moment, there are no uses of this setting. In the future, things
    such as switch lookup tables should reside there.
    
    llvm-svn: 325481

--- llvm/lib/Target/AVR/AVRTargetMachine.cpp
+++ llvm/lib/Target/AVR/AVRTargetMachine.cpp
@@ -25,7 +25,7 @@
 
 namespace llvm {
 
-static const char *AVRDataLayout = "e-p:16:8-i8:8-i16:8-i32:8-i64:8-f32:8-f64:8-n8-a:8";
+static const char *AVRDataLayout = "e-P1-p:16:8-i8:8-i16:8-i32:8-i64:8-f32:8-f64:8-n8-a:8";
 
 /// Processes a CPU name.
 static StringRef getCPU(StringRef CPU) {
