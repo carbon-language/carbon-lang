4c4a9835a2854590f38b88e9e7c4daf780dee0f9
Aaron Ballman <aaron@aaronballman.com>
2018-02-19 17:32:07 +0000

    Add several more attributes to be parsed in C with [[]] when -fdouble-square-bracket-attributes is specified.
    Also flags a few attributes that should not be available with the C spelling as they only matter in C++.
    
    llvm-svn: 325520

---
+++ clang/test/Sema/attr-cx2.c
@@ -0,0 +1,26 @@
+// RUN: %clang_cc1 -triple x86_64-apple-darwin10 -fsyntax-only -verify -fdouble-square-bracket-attributes %s
+
+struct S {};
+struct S * [[clang::address_space(1)]] Foo;
+
+enum [[clang::enum_extensibility(open)]] EnumOpen {
+  C0 = 1, C1 = 10
+};
+
+enum [[clang::flag_enum]] EnumFlag {
+  D0 = 1, D1 = 8
+};
+
+void foo(void *c) [[clang::overloadable]];
+void foo(char *c) [[clang::overloadable]];
+
+void context_okay(void *context [[clang::swift_context]]) [[clang::swiftcall]];
+void context_okay2(void *context [[clang::swift_context]], void *selfType, char **selfWitnessTable) [[clang::swiftcall]];
+
+void *f1(void) [[clang::ownership_returns(foo)]];
+void *f2() [[clang::ownership_returns(foo)]]; // expected-warning {{'ownership_returns' attribute only applies to non-K&R-style functions}}
+
+void foo2(void) [[clang::unavailable("not available - replaced")]]; // expected-note {{'foo2' has been explicitly marked unavailable here}}
+void bar(void) {
+  foo2(); // expected-error {{'foo2' is unavailable: not available - replaced}}
+}
