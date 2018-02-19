78f3c745eafb4e3069f985e4031748a6141d410b
Stephane Chauveau <stephane@chauveau-central.net>
2018-02-19 14:44:54 +0100

    [flang] Add missing include config file skeleton
    Original-commit: flang-compiler/f18@beb71e4e4bb75e04a60813cc2318b842c05e5613
    Reviewed-on: https://github.com/flang-compiler/f18/pull/13

---
+++ flang/include/flang/Config/config.h.cmake
@@ -0,0 +1,13 @@
+/* This generated file is for internal use. Do not include it from headers. */
+
+#ifdef FLANG_CONFIG_H
+#error config.h can only be included once
+#else
+#define FLANG_CONFIG_H
+
+
+#define FLANG_VERSION            "${FLANG_VERSION}"
+
+#endif
+
+
