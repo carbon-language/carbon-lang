8fad26e5f3641abbe3c335131ccb51cfd98649e7
Eric Christopher <echristo@gmail.com>
2018-02-19 05:26:49 +0000

    Add LanaiMCTargetDesc.h to LanaiInstrInfo.h to make it self contained with instruction enum definitions.
    llvm-svn: 325473

--- llvm/lib/Target/Lanai/LanaiInstrInfo.h
+++ llvm/lib/Target/Lanai/LanaiInstrInfo.h
@@ -15,6 +15,7 @@
 #define LLVM_LIB_TARGET_LANAI_LANAIINSTRINFO_H
 
 #include "LanaiRegisterInfo.h"
+#include "MCTargetDesc/LanaiMCTargetDesc.h"
 #include "llvm/CodeGen/TargetInstrInfo.h"
 
 #define GET_INSTRINFO_HEADER
