8317565532f7e9dfaa041ed64577e74fa1c68eaa
Kamil Rytarowski <n54@gmx.com>
2018-02-20 15:52:08 +0000

    Add new interceptor: fgetln(3)
    Summary:
    fgetln - get a line from a stream
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: vitalybuka, joerg
    
    Reviewed By: vitalybuka
    
    Subscribers: llvm-commits, kubamracek, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D43482
    
    llvm-svn: 325587

---
+++ compiler-rt/test/sanitizer_common/TestCases/NetBSD/fgetln.cc
@@ -0,0 +1,23 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  FILE *fp;
+  size_t len;
+  char *s;
+
+  fp = fopen("/etc/hosts", "r");
+  if (!fp)
+    exit(1);
+
+  s = fgetln(fp, &len);
+
+  printf("%.*s\n", (int)len, s);
+
+  if (fclose(fp) == EOF)
+    exit(1);
+
+  return 0;
+}
