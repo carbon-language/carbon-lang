6b287234ea0a8d5a5533a1b687c1cbaddffa6404
Maxim Kuvyrkov <maxim.kuvyrkov@gmail.com>
2018-02-21 18:03:50 +0000

    Fix racy msan test
    Summary:
    Child thread may finish before pthread_setname_np() and/or
    pthread_getname_np() had a chance to run, which causes these functions
    to fail with ENOENT (meaning /proc/self/task/[tid]/comm doesn't exist).
    
    Fix by stalling child thread on a mutex.
    
    Reviewers: eugenis
    
    Reviewed By: eugenis
    
    Subscribers: llvm-commits, #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D43548
    
    llvm-svn: 325703

--- compiler-rt/test/msan/pthread_getname_np.cc
+++ compiler-rt/test/msan/pthread_getname_np.cc
@@ -10,12 +10,22 @@
 
 #include <stdio.h>
 
+// Stall child thread on this lock to make sure it doesn't finish
+// before the end of the pthread_getname_np() / pthread_setname_np() tests.
+static pthread_mutex_t lock;
+
 void *ThreadFn(void *) {
+  pthread_mutex_lock (&lock);
+  pthread_mutex_unlock (&lock);
   return nullptr;
 }
 
 int main(void) {
   pthread_t t;
+
+  pthread_mutex_init (&lock, NULL);
+  pthread_mutex_lock (&lock);
+
   int res = pthread_create(&t, 0, ThreadFn, 0);
   assert(!res);
 
@@ -27,6 +37,8 @@ int main(void) {
   res = pthread_getname_np(t, buf, sizeof(buf));
   assert(!res);
   assert(strcmp(buf, kMyThreadName) == 0);
+
+  pthread_mutex_unlock (&lock);
 
   res = pthread_join(t, 0);
   assert(!res);
