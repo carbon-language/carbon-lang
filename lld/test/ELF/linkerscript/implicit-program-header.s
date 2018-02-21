db7c630b01e7c867a8d945e70f2d9425b25b52b0
George Rimar <grimar@accesssoftek.com>
2018-02-21 11:56:55 +0000

    [ELF] - Simplify testcase. NFC.
    This removes script input file and inlines script into
    testcase body. That is consistent with othet LS tests
    and makes testcase easier to read.
    
    llvm-svn: 325673

--- lld/test/ELF/linkerscript/implicit-program-header.s
+++ lld/test/ELF/linkerscript/implicit-program-header.s
@@ -1,6 +1,15 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t.o
-# RUN: ld.lld --hash-style=sysv -o %t1 --script %S/Inputs/implicit-program-header.script  \
+# RUN: echo "PHDRS {                    \
+# RUN:   ph_write PT_LOAD FLAGS(2);     \
+# RUN:   ph_exec  PT_LOAD FLAGS(1);     \
+# RUN: }                                \
+# RUN: SECTIONS {                       \
+# RUN:  .bar : { *(.bar) } : ph_exec    \
+# RUN:  .foo : { *(.foo) }              \
+# RUN:  .text : { *(.text) } : ph_write \
+# RUN: }" > %t.script
+# RUN: ld.lld --hash-style=sysv -o %t1 --script %t.script  \
 # RUN:   %t.o -shared
 # RUN: llvm-readobj -elf-output-style=GNU -l %t1 | FileCheck %s
 
