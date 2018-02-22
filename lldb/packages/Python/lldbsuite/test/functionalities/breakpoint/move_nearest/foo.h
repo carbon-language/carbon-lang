7de450033b62cec4c2eb6037bfc6655db8a3c144
Adrian McCarthy <amccarth@google.com>
2018-02-22 22:47:47 +0000

    Fix TestMoveNearest on Windows
    The header file for the DLL tried to declare inline functions and a local
    function as dllexport which broke the compile and link.  Removing the bad
    declarations solves the problem, and the test passes on Windows now.
    
    Differential Revision: https://reviews.llvm.org/D43600
    
    llvm-svn: 325836

--- lldb/packages/Python/lldbsuite/test/functionalities/breakpoint/move_nearest/foo.h
+++ lldb/packages/Python/lldbsuite/test/functionalities/breakpoint/move_nearest/foo.h
@@ -1,6 +1,5 @@
-LLDB_TEST_API inline int foo1() { return 1; } // !BR1
+inline int foo1() { return 1; } // !BR1
 
-LLDB_TEST_API inline int foo2() { return 2; } // !BR2
+inline int foo2() { return 2; } // !BR2
 
 LLDB_TEST_API extern int call_foo1();
-LLDB_TEST_API extern int call_foo2();
