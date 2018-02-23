a2fb097c804838cefc77f1187c2c918c50aa602d
George Burgess IV <george.burgess.iv@gmail.com>
2018-02-23 18:50:39 +0000

    [MemorySSA] Reduce padding in MemoryDefs. NFC
    llvm-svn: 325934

--- llvm/include/llvm/Analysis/MemorySSA.h
+++ llvm/include/llvm/Analysis/MemorySSA.h
@@ -388,8 +388,8 @@ private:
   static void deleteMe(DerivedUser *Self);
 
   const unsigned ID;
+  unsigned OptimizedID = INVALID_MEMORYACCESS_ID;
   MemoryAccess *Optimized = nullptr;
-  unsigned int OptimizedID = INVALID_MEMORYACCESS_ID;
 };
 
 template <>
