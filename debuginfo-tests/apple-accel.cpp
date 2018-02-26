58c707d1ad44bad8a7b8aa5e5b39391200378c99
Davide Italiano <davide@freebsd.org>
2018-02-26 20:56:45 +0000

    [Darwin] Specify DWARF 2/4 when running apple accelerator tests.
    These sections will be retired. Also, explicitly list llvm-objdump
    as a dependency. This should've been done in the previous commit,
    but I failed to squash the two changes together.
    
    Thanks to Adrian for pointing the first problem out in a comment.
    
    llvm-svn: 326121

--- debuginfo-tests/apple-accel.cpp
+++ debuginfo-tests/apple-accel.cpp
@@ -1,7 +1,11 @@
 // REQUIRES: system-darwin
 // Test that clang produces the __apple accelerator tables,
 // e.g., __apple_types, correctly.
-// RUN: %clang %s %target_itanium_abi_host_triple -O0 -c -g -o %t-ex
+// These sections are going to be retired in DWARF 5, so we hardcode
+// the DWARF version in the tests.
+// RUN: %clang %s %target_itanium_abi_host_triple -gdwarf-2 -O0 -c -g -o %t-ex
+// RUN: llvm-objdump -section-headers %t-ex | FileCheck %s
+// RUN: %clang %s %target_itanium_abi_host_triple -gdwarf-4 -O0 -c -g -o %t-ex
 // RUN: llvm-objdump -section-headers %t-ex | FileCheck %s
 
 int main (int argc, char const *argv[]) { return argc; }
