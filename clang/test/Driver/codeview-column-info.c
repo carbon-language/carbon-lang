a54daa6d97343ed404b363a09787938b228539ab
Reid Kleckner <rnk@google.com>
2018-02-26 23:06:40 +0000

    Fix codeview-column-info.c test with a triple
    llvm-svn: 326144

--- clang/test/Driver/codeview-column-info.c
+++ clang/test/Driver/codeview-column-info.c
@@ -2,11 +2,11 @@
 // 1) When -gcodeview is present via the clang or clang++ driver
 // 2) When /Z7 is present via the cl driver.
 
-// RUN: %clang -### -c -g -gcodeview %s 2> %t1
+// RUN: %clang -### --target=x86_64-windows-msvc -c -g -gcodeview %s 2> %t1
 // RUN: FileCheck < %t1 %s
-// RUN: %clangxx -### -c -g -gcodeview %s 2> %t2
+// RUN: %clangxx -### --target=x86_64-windows-msvc -c -g -gcodeview %s 2> %t2
 // RUN: FileCheck < %t2 %s
-// RUN: %clang_cl -### /c /Z7 %s 2> %t2
+// RUN: %clang_cl -### --target=x86_64-windows-msvc /c /Z7 %s 2> %t2
 // RUN: FileCheck < %t2 %s
 
 // CHECK: "-cc1"
