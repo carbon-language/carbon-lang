31254d4366359717fc56350a3043309ac1db55dd
George Karpenkov <ekarpenkov@apple.com>
2018-02-27 01:31:06 +0000

    [analyzer] Logging test typo quickfix.
    llvm-svn: 326156

--- clang/test/Analysis/region_store_overflow.c
+++ clang/test/Analysis/region_store_overflow.c
@@ -1,4 +1,4 @@
-// RUN: %clang_analyze_cc1 -analyze -analyzer-checker=core -mllvm -debug-only MemRegion %s 2>&1 | FileCheck %s
+// RUN: %clang_analyze_cc1 -analyze -analyzer-checker=core -mllvm -debug-only=MemRegion %s 2>&1 | FileCheck %s
 // REQUIRES: asserts
 
 int **h;
