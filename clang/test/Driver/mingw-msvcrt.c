f9b3c67956ef81342ca748398ca6c6aacd88d7ca
Martin Storsjo <martin@martin.st>
2018-02-27 08:35:35 +0000

    [test] Extend the Driver/mingw-msvcrt.c test with a -SAME check. NFC.
    llvm-svn: 326180

--- clang/test/Driver/mingw-msvcrt.c
+++ clang/test/Driver/mingw-msvcrt.c
@@ -2,4 +2,5 @@
 // RUN: %clang -v -target i686-pc-windows-gnu -lmsvcr120 -### %s 2>&1 | FileCheck -check-prefix=CHECK_MSVCR120 %s
 
 // CHECK_DEFAULT: "-lmingwex" "-lmsvcrt" "-ladvapi32"
-// CHECK_MSVCR120: "-lmingwex" "-ladvapi32"
+// CHECK_MSVCR120: "-lmsvcr120"
+// CHECK_MSVCR120-SAME: "-lmingwex" "-ladvapi32"
