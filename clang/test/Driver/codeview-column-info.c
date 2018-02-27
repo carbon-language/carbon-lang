80396248cf4dcb9987c82fed1566180b124444dc
Shoaib Meenai <smeenai@fb.com>
2018-02-27 06:04:23 +0000

    [Driver] Fix codeview-column-info on macOS
    macOS home directory paths begin with /Users, and clang-cl interprets
    the /U portion as a macro undefine rather than a path, causing test
    failures on macOS. Use a -- to explicitly treat the input file as a path
    and fix the test.
    
    This effectively reverts r326168 and adds an alternative fix.
    
    llvm-svn: 326171

--- clang/test/Driver/codeview-column-info.c
+++ clang/test/Driver/codeview-column-info.c
@@ -1,4 +1,3 @@
-// REQUIRES: system-windows
 // Check that -dwarf-column-info does not get added to the cc1 line:
 // 1) When -gcodeview is present via the clang or clang++ driver
 // 2) When /Z7 is present via the cl driver.
@@ -7,7 +6,7 @@
 // RUN: FileCheck < %t1 %s
 // RUN: %clangxx -### --target=x86_64-windows-msvc -c -g -gcodeview %s 2> %t2
 // RUN: FileCheck < %t2 %s
-// RUN: %clang_cl -### --target=x86_64-windows-msvc /c /Z7 %s 2> %t2
+// RUN: %clang_cl -### --target=x86_64-windows-msvc /c /Z7 -- %s 2> %t2
 // RUN: FileCheck < %t2 %s
 
 // CHECK: "-cc1"
