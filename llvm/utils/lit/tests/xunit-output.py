9db7bd5cabbb50e9f14f963e62045b09706b0b4e
Lei Huang <lei@ca.ibm.com>
2018-02-28 20:05:24 +0000

    Losen time contraint to accommodate system loads
    llvm-svn: 326359

--- llvm/utils/lit/tests/xunit-output.py
+++ llvm/utils/lit/tests/xunit-output.py
@@ -5,6 +5,6 @@
 # CHECK: <?xml version="1.0" encoding="UTF-8" ?>
 # CHECK: <testsuites>
 # CHECK: <testsuite name='test-data' tests='1' failures='0'>
-# CHECK: <testcase classname='test-data.test-data' name='metrics.ini' time='0.{{[0-9]+}}'/>
+# CHECK: <testcase classname='test-data.test-data' name='metrics.ini' time='{{[0-1]}}.{{[0-9]+}}'/>
 # CHECK: </testsuite>
 # CHECK: </testsuites>
