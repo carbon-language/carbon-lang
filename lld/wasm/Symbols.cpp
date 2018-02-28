5420dd098b91a85feff07e3e879b4bc5adfd7564
Rui Ueyama <ruiu@google.com>
2018-02-28 01:10:50 +0000

    [WebAssembly] Simplify Symbol::getWasmType. NFC.
    Differential Revision: https://reviews.llvm.org/D43717
    
    llvm-svn: 326293

--- lld/wasm/Symbols.cpp
+++ lld/wasm/Symbols.cpp
@@ -30,19 +30,13 @@ DefinedData *WasmSym::HeapBase;
 DefinedGlobal *WasmSym::StackPointer;
 
 WasmSymbolType Symbol::getWasmType() const {
-  switch (SymbolKind) {
-  case Symbol::DefinedFunctionKind:
-  case Symbol::UndefinedFunctionKind:
+  if (isa<FunctionSymbol>(this))
     return llvm::wasm::WASM_SYMBOL_TYPE_FUNCTION;
-  case Symbol::DefinedDataKind:
-  case Symbol::UndefinedDataKind:
+  if (isa<DataSymbol>(this))
     return llvm::wasm::WASM_SYMBOL_TYPE_DATA;
-  case Symbol::DefinedGlobalKind:
-  case Symbol::UndefinedGlobalKind:
+  if (isa<GlobalSymbol>(this))
     return llvm::wasm::WASM_SYMBOL_TYPE_GLOBAL;
-  default:
-    llvm_unreachable("invalid symbol kind");
-  }
+  llvm_unreachable("invalid symbol kind");
 }
 
 bool Symbol::hasOutputIndex() const {
