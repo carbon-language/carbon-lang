05660dacedc14fa911d90317312cb613c3b776af
Rui Ueyama <ruiu@google.com>
2018-03-01 04:21:42 +0000

    Convert more tests as linker scripts instead of assembly.
    llvm-svn: 326415

--- lld/test/ELF/linkerscript/eh-frame-hdr.s
+++ lld/test/ELF/linkerscript/eh-frame-hdr.s
@@ -1,9 +1,6 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
-# RUN: echo "SECTIONS { \
-# RUN:          .eh_frame_hdr : {} \
-# RUN:          .eh_frame : {} \
-# RUN:       }" > %t.script
+# RUN: echo "SECTIONS { .eh_frame_hdr : {} .eh_frame : {} }" > %t.script
 # RUN: ld.lld -o %t1 --eh-frame-hdr --script %t.script %t
 # RUN: llvm-objdump -s -section=".eh_frame_hdr" %t1 | FileCheck %s
 
