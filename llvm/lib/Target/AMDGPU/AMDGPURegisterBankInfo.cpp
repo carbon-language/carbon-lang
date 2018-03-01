8931bbf8dffe4ce46f23915480cfd07fc579396c
Matt Arsenault <Matthew.Arsenault@amd.com>
2018-03-01 20:24:37 +0000

    AMDGPU/GlobalISel: Define instruction mapping for @llvm.amdgcn.exp
    Patch by Tom Stellard
    
    llvm-svn: 326477

--- llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -285,7 +285,27 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
     OpdsMapping[3] = AMDGPU::getValueMapping(Op3Bank, Size);
     break;
   }
-
+  case AMDGPU::G_INTRINSIC_W_SIDE_EFFECTS: {
+    unsigned IntrinsicID = MI.getOperand(0).getIntrinsicID();
+    switch (IntrinsicID) {
+    case Intrinsic::amdgcn_exp: {
+      OpdsMapping[0] = nullptr; // IntrinsicID
+      // FIXME: These are immediate values which can't be read from registers.
+      OpdsMapping[1] = AMDGPU::getValueMapping(AMDGPU::SGPRRegBankID, 32);
+      OpdsMapping[2] = AMDGPU::getValueMapping(AMDGPU::SGPRRegBankID, 32);
+      // FIXME: Could we support packed types here?
+      OpdsMapping[3] = AMDGPU::getValueMapping(AMDGPU::VGPRRegBankID, 32);
+      OpdsMapping[4] = AMDGPU::getValueMapping(AMDGPU::VGPRRegBankID, 32);
+      OpdsMapping[5] = AMDGPU::getValueMapping(AMDGPU::VGPRRegBankID, 32);
+      OpdsMapping[6] = AMDGPU::getValueMapping(AMDGPU::VGPRRegBankID, 32);
+      // FIXME: These are immediate values which can't be read from registers.
+      OpdsMapping[7] = AMDGPU::getValueMapping(AMDGPU::SGPRRegBankID, 32);
+      OpdsMapping[8] = AMDGPU::getValueMapping(AMDGPU::SGPRRegBankID, 32);
+      break;
+    }
+    }
+    break;
+  }
   case AMDGPU::G_LOAD:
     return getInstrMappingForLoad(MI);
   }
