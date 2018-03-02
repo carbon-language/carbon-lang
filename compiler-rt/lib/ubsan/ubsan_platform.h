4410bbc9ed569b53fc59dd6b5ee6d06c2aec0938
Kamil Rytarowski <n54@gmx.com>
2018-03-02 07:30:22 +0000

    OpenBSD UBsan support / ubsan part
    Summary: UBsan, enable OpenBSD platform
    
    Patch by: David CARLIER
    
    Reviewers: krytarowski, kettenis, visa, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: kubamracek, fedor.sergeev, llvm-commits, #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D43894
    
    llvm-svn: 326543

--- compiler-rt/lib/ubsan/ubsan_platform.h
+++ compiler-rt/lib/ubsan/ubsan_platform.h
@@ -15,7 +15,8 @@
 
 // Other platforms should be easy to add, and probably work as-is.
 #if defined(__linux__) || defined(__FreeBSD__) || defined(__APPLE__) ||        \
-    defined(__NetBSD__) || (defined(__sun__) && defined(__svr4__)) ||          \
+    defined(__NetBSD__) || defined(__OpenBSD__) || \
+    (defined(__sun__) && defined(__svr4__)) || \
     defined(_WIN32) || defined(__Fuchsia__)
 # define CAN_SANITIZE_UB 1
 #else
