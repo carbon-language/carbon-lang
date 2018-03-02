c0f34c9e3665020b32c561c6555eea44d2b6a08f
Matt Arsenault <Matthew.Arsenault@amd.com>
2018-03-02 01:22:01 +0000

    AMDGPU/GlobalISel: Define instruction mapping for G_AND
    Patch by Tom Stellard
    
    llvm-svn: 326523

--- llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPURegisterBankInfo.cpp
@@ -284,6 +284,7 @@ AMDGPURegisterBankInfo::getInstrMapping(const MachineInstr &MI) const {
   switch (MI.getOpcode()) {
   default:
     return getInvalidInstructionMapping();
+  case AMDGPU::G_AND:
   case AMDGPU::G_OR:
     if (isSALUMapping(MI))
       return getDefaultMappingSOP(MI);
