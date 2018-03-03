fc44eb07a74d50ea72a97f45f148fd8573b70eaf
Kamil Rytarowski <n54@gmx.com>
2018-03-03 18:38:20 +0000

    Fix NetBSD 32-bit build
    Replace 'defined(SANITIZER_OPENBSD)' with appropriate 'SANITIZER_OPENBSD'.
    
    This is a fallout from adding OpenBSD partial support.
    
    llvm-svn: 326662

--- compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
@@ -172,7 +172,7 @@ typedef u64  OFF64_T;
 #if (SANITIZER_WORDSIZE == 64) || SANITIZER_MAC
 typedef uptr operator_new_size_type;
 #else
-# if defined(SANITIZER_OPENBSD) || defined(__s390__) && !defined(__s390x__)
+# if SANITIZER_OPENBSD || defined(__s390__) && !defined(__s390x__)
 // Special case: 31-bit s390 has unsigned long as size_t.
 typedef unsigned long operator_new_size_type;
 # else
