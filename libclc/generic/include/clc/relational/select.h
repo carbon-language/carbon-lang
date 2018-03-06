ae156b66f86dc07dc7c7e6b108d02c6ddaf14ddb
Jan Vesely <jan.vesely@rutgers.edu>
2018-03-06 17:48:43 +0000

    select: Add vector implementation
    Passes CTS on Carrizo
    
    Reviewed-by: Aaron Watry <awatry@gmail.com>
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 326819

--- libclc/generic/include/clc/relational/select.h
+++ libclc/generic/include/clc/relational/select.h
@@ -1,1 +1,11 @@
-#define select(a, b, c) ((c) ? (b) : (a))
+/* Duplciate these so we don't have to distribute utils.h */
+#define __CLC_CONCAT(x, y) x ## y
+#define __CLC_XCONCAT(x, y) __CLC_CONCAT(x, y)
+
+#define __CLC_BODY <clc/relational/select.inc>
+#include <clc/math/gentype.inc>
+#define __CLC_BODY <clc/relational/select.inc>
+#include <clc/integer/gentype.inc>
+
+#undef __CLC_CONCAT
+#undef __CLC_XCONCAT
