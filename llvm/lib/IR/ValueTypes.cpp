c332ae8be81f3ae7c57d068e806d720d017d69ec
Heejin Ahn <aheejin@gmail.com>
2018-03-09 23:29:06 +0000

    [WebAssembly] Add EVT::getEVTString() for except_ref type
    Summary: This was missing in D43706 (rL326985).
    
    Reviewers:
    
    Subscribers: jfb, sbc100, jgravelle-google, sunfish, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D44325
    
    llvm-svn: 327187

--- llvm/lib/IR/ValueTypes.cpp
+++ llvm/lib/IR/ValueTypes.cpp
@@ -196,6 +196,7 @@ std::string EVT::getEVTString() const {
   case MVT::v8f64:   return "v8f64";
   case MVT::Metadata:return "Metadata";
   case MVT::Untyped: return "Untyped";
+  case MVT::ExceptRef: return "ExceptRef";
   }
 }
 
