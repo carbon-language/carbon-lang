81ccb97024bb5a05f1d27d352ec20d0e83d8e3e7
Jonas Devlieghere <jonas@devlieghere.com>
2018-03-14 14:16:23 +0000

    [test] Disable TestMachCore everywhere except on Darwin
    Apparently the parser is wrapped inside ifdef's so the logic isn't
    available on non-Darwin platforms.
    
    Should fix build bot failure:
      http://lab.llvm.org:8011/builders/lldb-x86_64-ubuntu-14.04-cmake/builds/20463
    
    llvm-svn: 327512

--- lldb/packages/Python/lldbsuite/test/functionalities/postmortem/mach-core/TestMachCore.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/postmortem/mach-core/TestMachCore.py
@@ -26,6 +26,7 @@ class MachCoreTestCase(TestBase):
         lldb.DBG.SetSelectedPlatform(self._initial_platform)
         super(MachCoreTestCase, self).tearDown()
 
+    @skipUnlessDarwin
     def test_selected_thread(self):
         """Test that the right thread is selected after a core is loaded."""
         # Create core form YAML.
