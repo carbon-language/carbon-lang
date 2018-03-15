b84d653e9b49e33f3b109d082cfa527b45c22674
Davide Italiano <davide@freebsd.org>
2018-03-15 00:07:05 +0000

    [Dictionary] Rewrite the test added in r327587 as an inline test.
    Until we have a better story for putting commands and check lines
    in the same file (they're currently ignored), it seems that inline
    tests are actually more concise and easier to understand.
    Too bad we have still some python boilerplate, but that's not
    really substantial so we can live with it.
    
    Thanks to Fred for pointing out and Jim for explaining me how
    to use the inline test format.
    
    <rdar://problem/34806516>
    
    llvm-svn: 327592

--- lldb/lit/DataFormatters/Inputs/NSDict.m
+++
@@ -1,9 +0,0 @@
-#include <Foundation/Foundation.h>
-
-int main(void)
-{
-  NSDictionary *emptyDictionary = [[NSDictionary alloc] init];
-  NSMutableDictionary *mutableDictionary = [NSMutableDictionary dictionary];
-  NSDictionary *dictionary = @{ @"key": @{} };
-  return 0;
-}
