96cf00d01d18feb5541e1efdd53830bad1d098bf
Adrian Prantl <aprantl@apple.com>
2018-03-15 17:07:05 +0000

    Split skipIf decorator, the condition is supposed to be OR
    llvm-svn: 327644

--- lldb/packages/Python/lldbsuite/test/lang/objc/modules-auto-import/TestModulesAutoImport.py
+++ lldb/packages/Python/lldbsuite/test/lang/objc/modules-auto-import/TestModulesAutoImport.py
@@ -26,7 +26,8 @@ class ObjCModulesAutoImportTestCase(TestBase):
         self.line = line_number('main.m', '// Set breakpoint 0 here.')
 
     @skipUnlessDarwin
-    @skipIf(macos_version=["<", "10.12"], debug_info=no_match(["gmodules"]))
+    @skipIf(macos_version=["<", "10.12"])
+    @skipIf(debug_info=no_match(["gmodules"]))
     def test_expr(self):
         self.build()
         exe = self.getBuildArtifact("a.out")
