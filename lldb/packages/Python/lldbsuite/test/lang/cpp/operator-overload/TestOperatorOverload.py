95f7572c6f57facd227e882a9d775c348d8e03ee
Pavel Labath <labath@google.com>
2018-03-21 09:43:50 +0000

    Fix TestOperatorOverload for 32-bit builds
    - use more goodies from Makefile.rules to correctly build a 32-bit
    binary.
    - avoid hardcoding typeof(nil) in the test.
    
    This should partially fix the linux bot. There is still one assertion
    failure remaining, which I'll have to investigate separately, as I am
    not experiencing it locally.
    
    llvm-svn: 328083

--- lldb/packages/Python/lldbsuite/test/lang/cpp/operator-overload/TestOperatorOverload.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/operator-overload/TestOperatorOverload.py
@@ -10,13 +10,13 @@ class TestOperatorOverload(TestBase):
     self.build()
     (target, process, thread,
       main_breakpoint) = lldbutil.run_to_source_breakpoint(self,
-        "break here", lldb.SBFileSpec("b.cpp"), exe_name = "main")
+        "break here", lldb.SBFileSpec("b.cpp"))
     frame = thread.GetSelectedFrame()
     value = frame.EvaluateExpression("x == nil")
     self.assertTrue(str(value.GetError())
       .find("comparison between NULL and non-pointer ('Tinky' and NULL)")
         != -1)
     self.assertTrue(str(value.GetError())
-      .find("invalid operands to binary expression ('Tinky' and 'long')")
+      .find("invalid operands to binary expression ('Tinky' and")
         != -1)
     self.assertFalse(value.GetError().Success())
