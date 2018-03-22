2d91a8a3baddfcd817b07cc06be63cdbaf49a676
Andrey Churbanov <Andrey.Churbanov@intel.com>
2018-03-22 18:51:51 +0000

    Fixed __kmpc_get_target_offload() to call library initialization.
    Differential Revision: https://reviews.llvm.org/D44793
    
    llvm-svn: 328228

--- openmp/runtime/src/kmp_csupport.cpp
+++ openmp/runtime/src/kmp_csupport.cpp
@@ -4066,7 +4066,12 @@ void __kmpc_doacross_fini(ident_t *loc, int gtid) {
 #endif
 
 #if OMP_50_ENABLED
-int __kmpc_get_target_offload(void) { return __kmp_target_offload; }
+int __kmpc_get_target_offload(void) {
+  if (!__kmp_init_serial) {
+    __kmp_serial_initialize();
+  }
+  return __kmp_target_offload;
+}
 #endif // OMP_50_ENABLED
 
 // end of file //
