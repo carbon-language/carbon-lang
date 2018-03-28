757c8cf615cd0aefff636b725ab48a48c57086c0
Reid Kleckner <rnk@google.com>
2018-03-28 19:56:26 +0000

    Fix polly build after r328717
    llvm-svn: 328728

--- polly/lib/CodeGen/CodegenCleanup.cpp
+++ polly/lib/CodeGen/CodegenCleanup.cpp
@@ -19,6 +19,7 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Utils.h"
 
 #define DEBUG_TYPE "polly-cleanup"
 
--- polly/lib/Transform/Canonicalization.cpp
+++ polly/lib/Transform/Canonicalization.cpp
@@ -18,6 +18,7 @@
 #include "polly/Options.h"
 #include "llvm/Transforms/IPO.h"
 #include "llvm/Transforms/Scalar.h"
+#include "llvm/Transforms/Utils.h"
 
 using namespace llvm;
 using namespace polly;
