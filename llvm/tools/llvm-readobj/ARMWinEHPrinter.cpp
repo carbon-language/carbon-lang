dd4baff48d3318b5bcd71e720050cb5696908083
Eric Christopher <echristo@gmail.com>
2018-03-29 21:59:04 +0000

    Typo fix: epilouge->epilogue. NFC.
    llvm-svn: 328833

--- llvm/tools/llvm-readobj/ARMWinEHPrinter.cpp
+++ llvm/tools/llvm-readobj/ARMWinEHPrinter.cpp
@@ -33,7 +33,7 @@
 // (.pdata) entry.
 //
 // The exception data contains information about the frame setup, all of the
-// epilouge scopes (for functions for which there are multiple exit points) and
+// epilogue scopes (for functions for which there are multiple exit points) and
 // the associated exception handler.  Additionally, the entry contains byte-code
 // describing how to unwind the function (c.f. Decoder::decodeOpcodes).
 //
