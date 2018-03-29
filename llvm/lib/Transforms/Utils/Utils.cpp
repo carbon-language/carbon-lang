6b995a4a7ed67241a672eb631c0a3b87dceffed1
Benjamin Kramer <benny.kra@googlemail.com>
2018-03-29 07:56:53 +0000

    [Transforms] Make sure to include the c binding header when defining c binding functions
    Otherwise the definitions can't see the extern C declarations and get
    name mangled, making it impossible for users to call them. This breaks
    the Go bindings.
    
    llvm-svn: 328765

--- llvm/lib/Transforms/Utils/Utils.cpp
+++ llvm/lib/Transforms/Utils/Utils.cpp
@@ -14,6 +14,7 @@
 
 #include "llvm/Transforms/Utils.h"
 #include "llvm-c/Initialization.h"
+#include "llvm-c/Transforms/Scalar.h"
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/InitializePasses.h"
 #include "llvm/PassRegistry.h"
