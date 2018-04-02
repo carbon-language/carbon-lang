30eebb0e494e923333457e0161e3fa38efcdde74
Nico Weber <nicolasweber@gmx.de>
2018-04-02 18:13:05 +0000

    Remove unused PACKAGE_TARNAME; looks like a remnant of the old autoconf build.
    llvm-svn: 328998

--- llvm/include/llvm/Config/config.h.cmake
+++ llvm/include/llvm/Config/config.h.cmake
@@ -350,9 +350,6 @@
 /* Define to the full name and version of this package. */
 #cmakedefine PACKAGE_STRING "${PACKAGE_STRING}"
 
-/* Define to the one symbol short name of this package. */
-#undef PACKAGE_TARNAME
-
 /* Define to the version of this package. */
 #cmakedefine PACKAGE_VERSION "${PACKAGE_VERSION}"
 
