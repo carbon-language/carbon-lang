5b31b6f12dd673f16b68a968768132a0a7302420
George Rimar <grimar@accesssoftek.com>
2018-04-02 11:28:44 +0000

    [ELF] - cref.s: check that we print symbols from archives. NFCI.
    This is consistent with bfd and we already supported it,
    though test did not contain the explicit check.
    
    llvm-svn: 328967

--- lld/test/ELF/cref.s
+++ lld/test/ELF/cref.s
@@ -2,9 +2,12 @@
 
 // RUN: echo '.global foo; foo:' | llvm-mc -filetype=obj -triple=x86_64-pc-linux - -o %t1.o
 // RUN: echo '.global foo, bar; bar:' | llvm-mc -filetype=obj -triple=x86_64-pc-linux - -o %t2.o
+// RUN: echo '.global zed; zed:' | llvm-mc -filetype=obj -triple=x86_64-pc-linux - -o %ta.o
+// RUN: rm -f %t.a
+// RUN: llvm-ar rcs %t.a %ta.o
 // RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t3.o
 // RUN: ld.lld -shared -o %t1.so %t1.o -gc-sections
-// RUN: ld.lld -o /dev/null %t1.so %t2.o %t3.o -cref | FileCheck -strict-whitespace %s
+// RUN: ld.lld -o /dev/null %t1.so %t2.o %t3.o %t.a -cref | FileCheck -strict-whitespace %s
 
 //      CHECK: Symbol                                            File
 // CHECK-NEXT: bar                                               {{.*}}2.o
@@ -14,11 +17,14 @@
 // CHECK-NEXT:                                                   {{.*}}3.o
 // CHECK-NEXT: _start                                            {{.*}}3.o
 // CHECK-NEXT: baz                                               {{.*}}3.o
+// CHECK-NEXT: zed                                               {{.*}}.a({{.*}}a.o)
+// CHECK-NEXT:                                                   {{.*}}3.o
 
 .global _start, foo, bar, baz
 _start:
   call foo
   call bar
+  call zed
 localsym:
 baz:
 
