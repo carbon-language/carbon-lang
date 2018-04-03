7f2b79e4426ed950fddd83b255176045b0dfdf9b
David Blaikie <dblaikie@gmail.com>
2018-04-03 18:22:14 +0000

    Restrict a test using named file descriptors to using the system shell
    llvm-svn: 329097

--- clang/test/Misc/dev-fd-fs.c
+++ clang/test/Misc/dev-fd-fs.c
@@ -1,5 +1,6 @@
 // Check that we can operate on files from /dev/fd.
 // REQUIRES: dev-fd-fs
+// REQUIRES: shell
 
 // Check reading from named pipes. We cat the input here instead of redirecting
 // it to ensure that /dev/fd/0 is a named pipe, not just a redirected file.
