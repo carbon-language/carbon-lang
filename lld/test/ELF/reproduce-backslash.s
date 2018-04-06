d726d04f4523bbf4d439b436757ad91108484904
Benjamin Kramer <benny.kra@googlemail.com>
2018-04-06 10:05:47 +0000

    [ELF] Don't write to the source directory in test.
    llvm-svn: 329384

--- lld/test/ELF/reproduce-backslash.s
+++ lld/test/ELF/reproduce-backslash.s
@@ -3,7 +3,7 @@
 # Test that we don't erroneously replace \ with / on UNIX, as it's
 # legal for a filename to contain backslashes.
 # RUN: llvm-mc %s -o %T/foo\\.o -filetype=obj -triple=x86_64-pc-linux
-# RUN: ld.lld %T/foo\\.o --reproduce repro.tar
-# RUN: tar tf repro.tar | FileCheck %s
+# RUN: ld.lld %T/foo\\.o --reproduce %T/repro.tar -o /dev/null
+# RUN: tar tf %T/repro.tar | FileCheck %s
 
 # CHECK: repro/{{.*}}/foo\\.o
