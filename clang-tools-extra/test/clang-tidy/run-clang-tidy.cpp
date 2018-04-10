9f742bc2e95075867b239c5c8bde907532efa744
Roman Lebedev <lebedev.ri@gmail.com>
2018-04-10 20:59:27 +0000

    [clang-tidy] Unbreak run-clang-tidy.cpp test (modernize-use-auto.MinTypeNameLength)
    Again, refs. D45405, rL329730.
    
    llvm-svn: 329756

--- clang-tools-extra/test/clang-tidy/run-clang-tidy.cpp
+++ clang-tools-extra/test/clang-tidy/run-clang-tidy.cpp
@@ -3,6 +3,9 @@
 // RUN: echo "[{\"directory\":\".\",\"command\":\"clang++ -c %/t/test.cpp\",\"file\":\"%/t/test.cpp\"}]" | sed -e 's/\\/\\\\/g' > %t/compile_commands.json
 // RUN: echo "Checks: '-*,modernize-use-auto'" > %t/.clang-tidy
 // RUN: echo "WarningsAsErrors: '*'" >> %t/.clang-tidy
+// RUN: echo "CheckOptions:" >> %t/.clang-tidy
+// RUN: echo "  - key:             modernize-use-auto.MinTypeNameLength" >> %t/.clang-tidy
+// RUN: echo "    value:           '0'" >> %t/.clang-tidy
 // RUN: cp "%s" "%t/test.cpp"
 // RUN: cd "%t"
 // RUN: not %run_clang_tidy "%t/test.cpp"
