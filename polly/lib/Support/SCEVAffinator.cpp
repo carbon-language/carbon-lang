7bbacbf4ca4a9ea1ca1c818e8005daa9b353c41f
Tobias Grosser <tobias@grosser.es>
2018-04-11 16:58:08 +0000

    Revert r327216 'Add isl operator overloads for isl::pw_aff'
    This commit requires further discussions.
    
    llvm-svn: 329825

--- polly/lib/Support/SCEVAffinator.cpp
+++ polly/lib/Support/SCEVAffinator.cpp
@@ -15,7 +15,6 @@
 #include "polly/Options.h"
 #include "polly/ScopInfo.h"
 #include "polly/Support/GICHelper.h"
-#include "polly/Support/ISLOperators.h"
 #include "polly/Support/SCEVValidator.h"
 #include "polly/Support/ScopHelper.h"
 #include "isl/aff.h"
