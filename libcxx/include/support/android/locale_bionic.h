a4e874c516517a5e479599755734a734faefe128
Saleem Abdulrasool <compnerd@compnerd.org>
2018-04-13 18:14:57 +0000

    support: add missing locale stubs for android L, M
    The strto* family was introduced in android O (API Level 26).  However,
    the support headers were adjusted to indicate that all locale aware
    functions were added in L.  Provide stubs for the locale aware strto*
    family until O.
    
    llvm-svn: 330045

--- libcxx/include/support/android/locale_bionic.h
+++ libcxx/include/support/android/locale_bionic.h
@@ -24,7 +24,44 @@ extern "C" {
 }
 #endif
 
+#if defined(__ANDROID__)
+
+#include <android/api-level.h>
+
+// Android gained most locale aware functions in L (API level 21)
+#if __ANDROID_API__ < 21
 #include <support/xlocale/__posix_l_fallback.h>
+#endif
+
+// The strto* family was added in O (API Level 26)
+#if __ANDROID_API__ < 26
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+inline _LIBCPP_ALWAYS_INLINE float strtof_l(const char* __nptr, char** __endptr,
+                                            locale_t) {
+  return ::strtof(__nptr, __endptr);
+}
+
+inline _LIBCPP_ALWAYS_INLINE double strtod_l(const char* __nptr,
+                                             char** __endptr, locale_t) {
+  return ::strtod(__nptr, __endptr);
+}
+
+inline _LIBCPP_ALWAYS_INLINE long strtol_l(const char* __nptr, char** __endptr,
+                                           int __base, locale_t) {
+  return ::strtol(__nptr, __endptr, __base);
+}
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif // __ANDROID_API__ < 26
+
+#endif // defined(__ANDROID__)
 
 #endif // defined(__BIONIC__)
 #endif // _LIBCPP_SUPPORT_ANDROID_LOCALE_BIONIC_H
