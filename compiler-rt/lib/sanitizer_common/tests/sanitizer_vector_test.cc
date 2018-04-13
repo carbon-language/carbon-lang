63ee534ab10b7ece326bfb6c0a5011bd40895e83
Vitaly Buka <vitalybuka@google.com>
2018-04-13 18:41:41 +0000

    [sanitizer] Replace cast with unsigned literals in tests
    llvm-svn: 330048

--- compiler-rt/lib/sanitizer_common/tests/sanitizer_vector_test.cc
+++ compiler-rt/lib/sanitizer_common/tests/sanitizer_vector_test.cc
@@ -17,12 +17,12 @@ namespace __sanitizer {
 
 TEST(Vector, Basic) {
   Vector<int> v;
-  EXPECT_EQ(v.Size(), (uptr)0);
+  EXPECT_EQ(v.Size(), 0u);
   v.PushBack(42);
-  EXPECT_EQ(v.Size(), (uptr)1);
+  EXPECT_EQ(v.Size(), 1u);
   EXPECT_EQ(v[0], 42);
   v.PushBack(43);
-  EXPECT_EQ(v.Size(), (uptr)2);
+  EXPECT_EQ(v.Size(), 2u);
   EXPECT_EQ(v[0], 42);
   EXPECT_EQ(v[1], 43);
 }
@@ -31,7 +31,7 @@ TEST(Vector, Stride) {
   Vector<int> v;
   for (int i = 0; i < 1000; i++) {
     v.PushBack(i);
-    EXPECT_EQ(v.Size(), (uptr)(i + 1));
+    EXPECT_EQ(v.Size(), i + 1u);
     EXPECT_EQ(v[i], i);
   }
   for (int i = 0; i < 1000; i++) {
