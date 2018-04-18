50c7db17f796fefecf1d9de1243091da7b3144ea
Douglas Yung <douglas.yung@sony.com>
2018-04-18 23:58:05 +0000

    Fix test from r330245 on Windows.
    llvm-svn: 330305

--- clang-tools-extra/test/clang-tidy/read_file_config.cpp
+++ clang-tools-extra/test/clang-tidy/read_file_config.cpp
@@ -1,7 +1,7 @@
 // RUN: mkdir -p %T/read-file-config/
 // RUN: cp %s %T/read-file-config/test.cpp
 // RUN: echo 'Checks: "-*,modernize-use-nullptr"' > %T/read-file-config/.clang-tidy
-// RUN: echo '[{"command": "cc -c -o test.o test.cpp", "directory": "%T/read-file-config", "file": "%T/read-file-config/test.cpp"}]' > %T/read-file-config/compile_commands.json
+// RUN: echo '[{"command": "cc -c -o test.o test.cpp", "directory": "%/T/read-file-config", "file": "%/T/read-file-config/test.cpp"}]' > %T/read-file-config/compile_commands.json
 // RUN: clang-tidy %T/read-file-config/test.cpp | not grep "warning: .*\[clang-analyzer-deadcode.DeadStores\]$"
 // RUN: clang-tidy -checks="-*,clang-analyzer-*" %T/read-file-config/test.cpp | grep "warning: .*\[clang-analyzer-deadcode.DeadStores\]$"
 
