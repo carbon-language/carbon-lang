f2876e338585b149b59106be8e542d92faae9b73
Yan Zhang <ynzhang@google.com>
2018-04-23 01:05:02 +0000

    update readability-identifier-naming-objc test to use interface ivar. Implementation ivars are not supported in 32-bits OS.
    Reviewers: alexfh, chandlerc
    
    Subscribers: klimek, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D45936
    
    llvm-svn: 330562

--- clang-tools-extra/test/clang-tidy/readability-identifier-naming-objc.m
+++ clang-tools-extra/test/clang-tidy/readability-identifier-naming-objc.m
@@ -3,13 +3,10 @@
 // RUN:  [{key: readability-identifier-naming.ObjcIvarPrefix, value: '_'}]}' \
 // RUN: --
 
-@interface Foo
-@end 
-
-@implementation Foo {
+@interface Foo {
     int _bar;
     int barWithoutPrefix;
     // CHECK-MESSAGES: :[[@LINE-1]]:9: warning: invalid case style for objc ivar 'barWithoutPrefix' [readability-identifier-naming]
     // CHECK-FIXES: int _barWithoutPrefix;
 }
-@end
+@end 
