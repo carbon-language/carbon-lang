bec2a7c4ebd89b3f99523fb0e1987bb185f8c4af
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2018-04-25 09:23:56 +0000

    [DebugInfo] Invalidate debug info in ReassociatePass::RewriteExprTree
    Summary:
    When Reassociate is rewriting an expression tree it may
    reuse old binary expression nodes, for new expressions.
    Whenever an expression node is reused, but with a non-trivial
    change in the result, we need to invalidate any debug info
    that is associated with the node.
    
    If for example rewriting
      x = mul a, b
      y = mul c, x
    into
      x = mul c, b
      y = mul a, x
    we still get the same result for 'y', but 'x' is a new expression.
    All debug info referring to 'x' must be invalidated (marked as
    optimized out) since we no longer calculate the expected value.
    
    As a side-effect this patch avoid (at least some) problems where
    reassociate could end up creating IR with debug-use before def.
    Earlier the dbg.value nodes where left untouched in the IR, while
    the reused binary nodes where sinked to just before the root node
    of the rewritten expression tree. See PR27273 for more info about
    such problems.
    
    Reviewers: dblaikie, aprantl, dexonsmith
    
    Reviewed By: aprantl
    
    Subscribers: JDevlieghere, llvm-commits
    
    Tags: #debug-info
    
    Differential Revision: https://reviews.llvm.org/D45975
    
    llvm-svn: 330804

--- llvm/lib/Transforms/Scalar/Reassociate.cpp
+++ llvm/lib/Transforms/Scalar/Reassociate.cpp
@@ -43,6 +43,7 @@
 #include "llvm/IR/InstrTypes.h"
 #include "llvm/IR/Instruction.h"
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
 #include "llvm/IR/Operator.h"
 #include "llvm/IR/PassManager.h"
 #include "llvm/IR/PatternMatch.h"
@@ -780,6 +781,18 @@ void ReassociatePass::RewriteExprTree(BinaryOperator *I,
 
       if (ExpressionChanged == I)
         break;
+
+      // Discard any debug info related to the expressions that has changed (we
+      // can leave debug infor related to the root, since the result of the
+      // expression tree should be the same even after reassociation).
+      SmallVector<DbgInfoIntrinsic *, 1> DbgUsers;
+      findDbgUsers(DbgUsers, ExpressionChanged);
+      for (auto *DII : DbgUsers) {
+        Value *Undef = UndefValue::get(ExpressionChanged->getType());
+        DII->setOperand(0, MetadataAsValue::get(DII->getContext(),
+                                                ValueAsMetadata::get(Undef)));
+      }
+
       ExpressionChanged->moveBefore(I);
       ExpressionChanged = cast<BinaryOperator>(*ExpressionChanged->user_begin());
     } while (true);
