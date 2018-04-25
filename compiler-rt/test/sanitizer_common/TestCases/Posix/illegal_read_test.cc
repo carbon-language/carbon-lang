152060275fd43196bc34648d1a264e6478555860
Petar Jovanovic <petar.jovanovic@mips.com>
2018-04-25 19:34:48 +0000

    Add s390x to XFAIL for illegal_read/write_test.cc
    Follow up to r330840 and r330849.
    It seems that s390 is also not distinguishing illegal WRITE and READ memory
    access.
    Add s390x to XFAIL for the tests.
    
    llvm-svn: 330863

--- compiler-rt/test/sanitizer_common/TestCases/Posix/illegal_read_test.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/illegal_read_test.cc
@@ -2,7 +2,7 @@
 // RUN: %clangxx -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
 
 // REQUIRES: stable-runtime
-// XFAIL: powerpc64
+// XFAIL: powerpc64, s390x
 
 volatile int *null = 0;
 volatile int a;
