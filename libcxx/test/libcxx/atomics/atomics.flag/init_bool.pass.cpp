0badc676cf0d398ebee0b4310d4ebf1b4d532e6a
Marshall Clow <mclow.lists@gmail.com>
2018-04-25 16:09:47 +0000

    Disable the test I just added when testing C++03.
    llvm-svn: 330838

--- libcxx/test/libcxx/atomics/atomics.flag/init_bool.pass.cpp
+++ libcxx/test/libcxx/atomics/atomics.flag/init_bool.pass.cpp
@@ -18,15 +18,21 @@
 #include <atomic>
 #include <cassert>
 
- // Ensure that static initialization happens; this is PR#37226
+#include "test_macros.h"
+
+#if TEST_STD_VER >= 11
+// Ensure that static initialization happens; this is PR#37226
 extern std::atomic_flag global;
 struct X { X() { global.test_and_set(); }};
 X x;
 std::atomic_flag global = ATOMIC_FLAG_INIT;
+#endif
 
 int main()
 {
+#if TEST_STD_VER >= 11
     assert(global.test_and_set() == 1);
+#endif
     {
         std::atomic_flag f(false);
         assert(f.test_and_set() == 0);
