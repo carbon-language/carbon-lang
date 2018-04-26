b768d235a94570425698531a3ffad4d44b2a4db4
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2018-04-26 17:54:53 +0000

    Revert "Enable EliminateAvailableExternally pass for -O1"
    This reverts commit r330961 because it breaks a handful of clang tests.
    
    llvm-svn: 330964

--- llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
+++ llvm/lib/Transforms/IPO/PassManagerBuilder.cpp
@@ -523,7 +523,7 @@ void PassManagerBuilder::populateModulePassManager(
   if (RunPartialInlining)
     MPM.add(createPartialInliningPass());
 
-  if (!PrepareForLTO && !PrepareForThinLTO)
+  if (OptLevel > 1 && !PrepareForLTO && !PrepareForThinLTO)
     // Remove avail extern fns and globals definitions if we aren't
     // compiling an object file for later LTO. For LTO we want to preserve
     // these so they are eligible for inlining at link-time. Note if they
