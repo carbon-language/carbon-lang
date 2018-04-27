21dc68fe7b4cd06b2ac194a66467352ddc2c4ecb
Dmitry Vyukov <dvyukov@google.com>
2018-04-27 08:59:35 +0000

    tsan: improve "destroy of a locked mutex" reports
    1. Allow to suppress by current stack.
    We generally allow to suppress by all main stacks.
    Current is probably the stack one wants to use to
    suppress such reports.
    
    2. Fix last lock stack restoration.
    We trimmed shadow value by storing it in u32.
    This magically worked for the test that provoked
    the report on the main thread. But this breaks
    for locks in any other threads.
    
    llvm-svn: 331023

--- compiler-rt/lib/tsan/rtl/tsan_rtl_mutex.cc
+++ compiler-rt/lib/tsan/rtl/tsan_rtl_mutex.cc
@@ -104,7 +104,7 @@ void MutexDestroy(ThreadState *thr, uptr pc, uptr addr, u32 flagz) {
     unlock_locked = true;
   }
   u64 mid = s->GetId();
-  u32 last_lock = s->last_lock;
+  u64 last_lock = s->last_lock;
   if (!unlock_locked)
     s->Reset(thr->proc());  // must not reset it before the report is printed
   s->mtx.Unlock();
@@ -114,7 +114,7 @@ void MutexDestroy(ThreadState *thr, uptr pc, uptr addr, u32 flagz) {
     rep.AddMutex(mid);
     VarSizeStackTrace trace;
     ObtainCurrentStack(thr, pc, &trace);
-    rep.AddStack(trace);
+    rep.AddStack(trace, true);
     FastState last(last_lock);
     RestoreStack(last.tid(), last.epoch(), &trace, 0);
     rep.AddStack(trace, true);
