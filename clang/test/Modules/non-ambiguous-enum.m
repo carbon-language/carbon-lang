763cd2a98a61083593cd8f167bf6d27aa7eb6d96
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2018-04-30 22:57:02 +0000

    [Modules] Fix testcases from r331232
    llvm-svn: 331233

--- clang/test/Modules/non-ambiguous-enum.m
+++ clang/test/Modules/non-ambiguous-enum.m
@@ -1,7 +1,7 @@
 // RUN: rm -rf %t
 // RUN: %clang_cc1 -fmodules -fimplicit-module-maps -fmodules-cache-path=%t -F%S/Inputs/non-ambiguous-enum -fsyntax-only %s -verify
-#import <B/B.h>
-#import <A/A.h>
+#import <B/b.h>
+#import <A/a.h>
 
 // expected-no-diagnostics
 
