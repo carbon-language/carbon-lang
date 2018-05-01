ddbd2b530c42d46eff1ad1360eb6cc5664f52bb3
Douglas Yung <douglas.yung@sony.com>
2018-05-01 23:32:09 +0000

    This test fails if there is no integrated assembler, so change the -c option to -S as it is not important to the test and allows it to pass when there is no integrated assembler.
    llvm-svn: 331318

--- clang/test/Driver/config-file4.c
+++ clang/test/Driver/config-file4.c
@@ -1,2 +1,2 @@
-// RUN: %clang --config %S/Inputs/empty.cfg -Wall -Wextra -Wformat -Wstrict-aliasing -Wshadow -Wpacked -Winline -Wimplicit-function-declaration -c %s -O2 -o /dev/null -v 2>&1 | FileCheck %s -check-prefix PR37196
+// RUN: %clang --config %S/Inputs/empty.cfg -Wall -Wextra -Wformat -Wstrict-aliasing -Wshadow -Wpacked -Winline -Wimplicit-function-declaration -S %s -O2 -o /dev/null -v 2>&1 | FileCheck %s -check-prefix PR37196
 // PR37196: Configuration file: {{.*}}empty.cfg
