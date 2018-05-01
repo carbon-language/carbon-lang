abb22405e6ccf8263033d05a107be9980b345718
Chandler Carruth <chandlerc@gmail.com>
2018-05-01 06:48:30 +0000

    Fix up r331244 - the emitted definition is weak_odr linkage. Should get the build bots to healthy again without a full revert. As the functionality added has nothing to do with linkage this seems unlikely to represent a deep or interesting bug in the patch.
    llvm-svn: 331245

--- clang/test/CodeGenCXX/char8_t.cpp
+++ clang/test/CodeGenCXX/char8_t.cpp
@@ -3,6 +3,6 @@
 // CHECK: define void @_Z1fDu(
 void f(char8_t c) {}
 
-// CHECK: define void @_Z1gIiEvDTplplcvT__ELA4_KDuELDu114EE
+// CHECK: define weak_odr void @_Z1gIiEvDTplplcvT__ELA4_KDuELDu114EE
 template<typename T> void g(decltype(T() + u8"foo" + u8'r')) {}
 template void g<int>(const char8_t*);
