f4cf59ef9957ed788821565a00e8b9ca4a9ff1f4
Karl-Johan Karlsson <karl-johan.karlsson@ericsson.com>
2018-05-03 05:53:29 +0000

    Fix -Wunused-variable warning in Clang.cpp
    llvm-svn: 331438

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1485,7 +1485,7 @@ void Clang::AddAArch64TargetArgs(const ArgList &Args,
       CmdArgs.push_back("-aarch64-enable-global-merge=true");
   }
 
-  if (Arg *A = Args.getLastArg(options::OPT_foutline)) {
+  if (Args.getLastArg(options::OPT_foutline)) {
     CmdArgs.push_back("-mllvm");
     CmdArgs.push_back("-enable-machine-outliner");
   }
