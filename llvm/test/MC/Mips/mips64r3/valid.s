65b0492f0daa23a655c591d6f23fab9a911f6af2
Simon Dardis <simon.dardis@mips.com>
2018-05-04 15:00:54 +0000

    [mips] Correct the predicates of sign extension instructions
    And eliminatw the duplication of those instructions for microMIPS32r6.
    
    Reviewers: smaksimovic, abeserminji, atanasyan
    
    Differential Revision: https://reviews.llvm.org/D46117
    
    llvm-svn: 331526

--- llvm/test/MC/Mips/mips64r3/valid.s
+++ llvm/test/MC/Mips/mips64r3/valid.s
@@ -299,9 +299,13 @@ a:
         sdr       $11,-20423($12)
         sdxc1     $f11,$10($14)
         seb       $25, $15             # CHECK: seb $25, $15           # encoding: [0x7c,0x0f,0xcc,0x20]
+                                       # CHECK-NOT:                    # <MCInst #{{[0-9]+}} SEB_MM
         seb       $25                  # CHECK: seb $25, $25           # encoding: [0x7c,0x19,0xcc,0x20]
+                                       # CHECK-NOT:                    # <MCInst #{{[0-9]+}} SEB_MM
         seh       $3, $12              # CHECK: seh $3, $12            # encoding: [0x7c,0x0c,0x1e,0x20]
+                                       # CHECK-NOT:                    # <MCInst #{{[0-9]+}} SEH_MM
         seh       $3                   # CHECK: seh $3, $3             # encoding: [0x7c,0x03,0x1e,0x20]
+                                       # CHECK-NOT:                    # <MCInst #{{[0-9]+}} SEH_MM
         sh        $14,-6704($15)
         sll       $4, $5               # CHECK: sllv $4, $4, $5        # encoding: [0x00,0xa4,0x20,0x04]
                                        # CHECK-NEXT:                   # <MCInst #{{[0-9]+}} SLL
