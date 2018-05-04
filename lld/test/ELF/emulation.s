2689d9f4f0ec66245cf4eafb8c3cacc3c01936ff
Peter Smith <peter.smith@linaro.org>
2018-05-04 14:28:29 +0000

    [ELF][AArch64] Add aarch64_elf64_le_vec emulation
    Android AOSP has started specifying -m aarch64_elf64_le_vec as supported
    by gold and BFD. This is a simple change to add the emulation so that LLD
    doesn't immediately error when used as a linker in an AOSP build.
    
    Differential Revision: https://reviews.llvm.org/D46429
    
    llvm-svn: 331521

--- lld/test/ELF/emulation.s
+++ lld/test/ELF/emulation.s
@@ -366,8 +366,12 @@
 # RUN: llvm-mc -filetype=obj -triple=aarch64-unknown-linux %s -o %taarch64
 # RUN: ld.lld -m aarch64linux %taarch64 -o %t2aarch64
 # RUN: llvm-readobj -file-headers %t2aarch64 | FileCheck --check-prefix=AARCH64 %s
-# RUN: ld.lld %taarch64 -o %t3aarch64
+# RUN: ld.lld -m aarch64elf %taarch64 -o %t3aarch64
 # RUN: llvm-readobj -file-headers %t3aarch64 | FileCheck --check-prefix=AARCH64 %s
+# RUN: ld.lld -m aarch64_elf64_le_vec %taarch64 -o %t4aarch64
+# RUN: llvm-readobj -file-headers %t4aarch64 | FileCheck --check-prefix=AARCH64 %s
+# RUN: ld.lld %taarch64 -o %t5aarch64
+# RUN: llvm-readobj -file-headers %t5aarch64 | FileCheck --check-prefix=AARCH64 %s
 # AARCH64:      ElfHeader {
 # AARCH64-NEXT:   Ident {
 # AARCH64-NEXT:     Magic: (7F 45 4C 46)
