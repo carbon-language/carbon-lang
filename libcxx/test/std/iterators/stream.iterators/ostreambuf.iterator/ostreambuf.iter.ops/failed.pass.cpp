00d83601b47dab3fb2893db2647964b568a045e3
Stephan T. Lavavej <stl@exchange.microsoft.com>
2018-05-05 01:40:27 +0000

    [libcxx] [test] Fix whitespace, NFC.
    Strip trailing whitespace and untabify.
    
    llvm-svn: 331576

--- libcxx/test/std/iterators/stream.iterators/ostreambuf.iterator/ostreambuf.iter.ops/failed.pass.cpp
+++ libcxx/test/std/iterators/stream.iterators/ostreambuf.iterator/ostreambuf.iter.ops/failed.pass.cpp
@@ -19,23 +19,23 @@
 
 template <typename Char, typename Traits = std::char_traits<Char> >
 struct my_streambuf : public std::basic_streambuf<Char,Traits> {
-	typedef typename std::basic_streambuf<Char,Traits>::int_type  int_type;
-	typedef typename std::basic_streambuf<Char,Traits>::char_type char_type;
-	
-	my_streambuf() {}
-	int_type sputc(char_type) { return Traits::eof(); }
-	};
+    typedef typename std::basic_streambuf<Char,Traits>::int_type  int_type;
+    typedef typename std::basic_streambuf<Char,Traits>::char_type char_type;
+
+    my_streambuf() {}
+    int_type sputc(char_type) { return Traits::eof(); }
+    };
 
 int main()
 {
     {
-    	my_streambuf<char> buf;
+        my_streambuf<char> buf;
         std::ostreambuf_iterator<char> i(&buf);
         i = 'a';
         assert(i.failed());
     }
     {
-    	my_streambuf<wchar_t> buf;
+        my_streambuf<wchar_t> buf;
         std::ostreambuf_iterator<wchar_t> i(&buf);
         i = L'a';
         assert(i.failed());
