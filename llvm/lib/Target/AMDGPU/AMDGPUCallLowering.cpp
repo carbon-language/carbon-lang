37444285f1ea3cfe708d51d86a296dcac7f1c118
Tom Stellard <tstellar@redhat.com>
2018-05-07 22:17:54 +0000

    AMDGPU/GlobalISel: Don't try to lower hull shaders
    Summary: The AMDGPU_HS calling convention is not supported yet.
    
    Reviewers: arsenm, nhaehnle
    
    Subscribers: kzhuravl, wdng, yaxunl, rovka, kristof.beyls, dstuttard, tpr, t-tye, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D46149
    
    llvm-svn: 331691

--- llvm/lib/Target/AMDGPU/AMDGPUCallLowering.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUCallLowering.cpp
@@ -88,8 +88,9 @@ void AMDGPUCallLowering::lowerParameter(MachineIRBuilder &MIRBuilder,
 bool AMDGPUCallLowering::lowerFormalArguments(MachineIRBuilder &MIRBuilder,
                                               const Function &F,
                                               ArrayRef<unsigned> VRegs) const {
-  // AMDGPU_GS is not supported yet.
-  if (F.getCallingConv() == CallingConv::AMDGPU_GS)
+  // AMDGPU_GS and AMDGP_HS are not supported yet.
+  if (F.getCallingConv() == CallingConv::AMDGPU_GS ||
+      F.getCallingConv() == CallingConv::AMDGPU_HS)
     return false;
 
   MachineFunction &MF = MIRBuilder.getMF();
