4ef47eb12120d6472933bdd1df2358d7cd63b573
Erich Keane <erich.keane@intel.com>
2018-05-07 22:01:06 +0000

    [NFC] Move 2 variable initialization from Ctor to member initializers.
    In response to dblaikie's suggestion on r331536, replace the two enum
    typed variable initializers in the constructor with member initializers.
    
    llvm-svn: 331688

--- clang/include/clang/Frontend/DependencyOutputOptions.h
+++ clang/include/clang/Frontend/DependencyOutputOptions.h
@@ -34,10 +34,10 @@ public:
   unsigned IncludeModuleFiles : 1; ///< Include module file dependencies.
 
   /// Destination of cl.exe style /showIncludes info.
-  ShowIncludesDestination ShowIncludesDest;
+  ShowIncludesDestination ShowIncludesDest = ShowIncludesDestination::None;
 
   /// The format for the dependency file.
-  DependencyOutputFormat OutputFormat;
+  DependencyOutputFormat OutputFormat = DependencyOutputFormat::Make;
 
   /// The file to write dependency output to.
   std::string OutputFile;
@@ -67,9 +67,7 @@ public:
 public:
   DependencyOutputOptions()
       : IncludeSystemHeaders(0), ShowHeaderIncludes(0), UsePhonyTargets(0),
-        AddMissingHeaderDeps(0), IncludeModuleFiles(0),
-        ShowIncludesDest(ShowIncludesDestination::None),
-        OutputFormat(DependencyOutputFormat::Make) {}
+        AddMissingHeaderDeps(0), IncludeModuleFiles(0) {}
 };
 
 }  // end namespace clang
