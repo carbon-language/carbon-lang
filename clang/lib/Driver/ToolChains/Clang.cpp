635af6223b7e8ab034696c49c1bf3bb722ca96a1
Jessica Paquette <jpaquette@apple.com>
2018-05-08 20:53:19 +0000

    Change -foutline to -moutline
    Nitpicky, but the MachineOutliner is a machine-level pass, and so we should
    reflect that by using "m" instead of "n".
    
    Figured we should get this in before people get used to the letter f. :)
    
    llvm-svn: 331806

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -1485,7 +1485,7 @@ void Clang::AddAArch64TargetArgs(const ArgList &Args,
       CmdArgs.push_back("-aarch64-enable-global-merge=true");
   }
 
-  if (Args.getLastArg(options::OPT_foutline)) {
+  if (Args.getLastArg(options::OPT_moutline)) {
     CmdArgs.push_back("-mllvm");
     CmdArgs.push_back("-enable-machine-outliner");
   }
--- clang/test/Driver/aarch64-outliner.c
+++ clang/test/Driver/aarch64-outliner.c
@@ -1,4 +1,4 @@
 // REQUIRES: aarch64-registered-target
 
-// RUN: %clang -target aarch64 -foutline -S %s -### 2>&1 | FileCheck %s
+// RUN: %clang -target aarch64 -moutline -S %s -### 2>&1 | FileCheck %s
 // CHECK: "-mllvm" "-enable-machine-outliner"
