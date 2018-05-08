a7a8506ce65d392fa455390da62c61bed460c316
peter klausler <pklausler@nvidia.com>
2018-05-08 13:15:13 -0700

    [flang] reformat for merge
    Original-commit: flang-compiler/f18@ca8f9611f52ad24d969343a00e31b5e7e1675d38
    Reviewed-on: https://github.com/flang-compiler/f18/pull/84

--- flang/lib/parser/message.cc
+++ flang/lib/parser/message.cc
@@ -116,9 +116,10 @@ bool Message::SortBefore(const Message &that) const {
 }
 
 bool Message::IsFatal() const {
-  return std::visit(visitors{[](const MessageExpectedText &) { return true; },
-                        [](const MessageFixedText &x) { return x.isFatal(); },
-                        [](const MessageFormattedText &x) { return x.isFatal(); }},
+  return std::visit(
+      visitors{[](const MessageExpectedText &) { return true; },
+          [](const MessageFixedText &x) { return x.isFatal(); },
+          [](const MessageFormattedText &x) { return x.isFatal(); }},
       text_);
 }
 
