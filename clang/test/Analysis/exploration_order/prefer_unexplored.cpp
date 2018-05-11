c41c5b2747434f41a15ecbc3f9efaa11f930e65f
George Karpenkov <ekarpenkov@apple.com>
2018-05-11 20:38:39 +0000

    [analyzer] Fix the filename for the exploration_order test.
    Ensures that the test is being run.
    
    llvm-svn: 332135

--- clang/test/Analysis/exploration_order/prefer_unexplored.cpp
+++ clang/test/Analysis/exploration_order/prefer_unexplored.cpp
@@ -1,5 +1,5 @@
-// RUN: %clang_analyze_cc1 -w -analyzer-checker=core -analyzer-config exploration_strategy=unexplored_first -analyzer-output=text -verify %s | FileCheck %s
-// RUN: %clang_analyze_cc1 -w -analyzer-checker=core -analyzer-config exploration_strategy=unexplored_first_queue -analyzer-output=text -verify %s | FileCheck %s
+// RUN: %clang_analyze_cc1 -w -analyzer-checker=core -analyzer-config exploration_strategy=unexplored_first -analyzer-output=text -verify %s
+// RUN: %clang_analyze_cc1 -w -analyzer-checker=core -analyzer-config exploration_strategy=unexplored_first_queue -analyzer-output=text -verify %s
 
 extern int coin();
 
@@ -7,6 +7,7 @@ int foo() {
     int *x = 0; // expected-note {{'x' initialized to a null pointer value}}
     while (coin()) { // expected-note{{Loop condition is true}}
         if (coin())  // expected-note {{Taking true branch}}
+                     // expected-note@-1 {{Assuming the condition is true}}
             return *x; // expected-warning{{Dereference of null pointer (loaded from variable 'x')}}
                        // expected-note@-1{{Dereference of null pointer (loaded from variable 'x')}}
     }
@@ -14,15 +15,16 @@ int foo() {
 }
 
 void bar() {
-    while(coin()) // expected-note{{Loop condition is true}}
-        if (coin()) // expected-note {{Assuming the condition is true}}
-            foo(); // expected-note{{Calling 'foo'}}
+    while(coin())
+        if (coin())
+            foo();
 }
 
 int foo2() {
     int *x = 0; // expected-note {{'x' initialized to a null pointer value}}
     while (coin()) { // expected-note{{Loop condition is true}}
         if (coin())  // expected-note {{Taking false branch}}
+                     // expected-note@-1 {{Assuming the condition is false}}
             return false;
         else
             return *x; // expected-warning{{Dereference of null pointer (loaded from variable 'x')}}
@@ -34,7 +36,8 @@ int foo2() {
 void bar2() {
     while(coin()) // expected-note{{Loop condition is true}}
         if (coin()) // expected-note {{Assuming the condition is false}}
-          return false;
+                    // expected-note@-1 {{Taking false branch}}
+          return;
         else
             foo(); // expected-note{{Calling 'foo'}}
 }
