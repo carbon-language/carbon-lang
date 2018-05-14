0b0ad22aa538121a01756df34873892b85c650a5
Eric Liu <ioeric@google.com>
2018-05-14 12:07:56 +0000

    Redirect output to /dev/null in the tests added in r332160.
    llvm-svn: 332236

--- clang/test/Driver/clang-abi-compat.cpp
+++ clang/test/Driver/clang-abi-compat.cpp
@@ -1,6 +1,6 @@
 // PS4 target requires clang ABI version 6, check that a warning is emitted when a version other than 6 is requested.
-// RUN: %clang -S --target=x86_64-scei-ps4 -fclang-abi-compat=4 %s 2>&1 | FileCheck %s -check-prefix=CHECK-WARNING
-// RUN: %clang -S --target=x86_64-scei-ps4 -fclang-abi-compat=latest %s 2>&1 | FileCheck %s -check-prefix=CHECK-WARNING
+// RUN: %clang -S --target=x86_64-scei-ps4 -fclang-abi-compat=4 %s -o /dev/null 2>&1 | FileCheck %s -check-prefix=CHECK-WARNING
+// RUN: %clang -S --target=x86_64-scei-ps4 -fclang-abi-compat=latest %s -o /dev/null 2>&1 | FileCheck %s -check-prefix=CHECK-WARNING
 
 // REQUIRES: x86-registered-target
 
