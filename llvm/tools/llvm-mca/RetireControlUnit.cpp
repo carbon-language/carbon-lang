e2492c860a3ffba8660caba58f9462f2cd3c73f1
Andrea Di Biagio <Andrea_DiBiagio@sn.scee.net>
2018-05-15 09:31:32 +0000

    [llvm-mca] Add file header to RetireControlUnit.cpp.
    Strictly speaking, this is not necessary for .cpp files. However, other .cpp
    files from this same tool have it. This also matches what we do in other tools.
    
    llvm-svn: 332334

--- llvm/tools/llvm-mca/RetireControlUnit.cpp
+++ llvm/tools/llvm-mca/RetireControlUnit.cpp
@@ -1,3 +1,18 @@
+//===---------------------- RetireControlUnit.cpp ---------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+/// \file
+///
+/// This file implements methods declared by the RetireControlUnit interface.
+///
+//===----------------------------------------------------------------------===//
+
+
 #include "RetireControlUnit.h"
 #include "Dispatch.h"
 #include "llvm/MC/MCSchedule.h"
