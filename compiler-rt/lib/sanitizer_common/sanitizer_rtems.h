e35f57f023fc76df8618a77f205caf8d71e21cd7
Walter Lee <waltl@google.com>
2018-05-18 04:10:12 +0000

    [asan] Add target-specific files for Myriad RTEMS port
    Differential Revision: https://reviews.llvm.org/D46468
    
    llvm-svn: 332691

---
+++ compiler-rt/lib/sanitizer_common/sanitizer_rtems.h
@@ -0,0 +1,28 @@
+//===-- sanitizer_rtems.h ---------------------------------------*- C++ -*-===//
+//
+//                     The LLVM Compiler Infrastructure
+//
+// This file is distributed under the University of Illinois Open Source
+// License. See LICENSE.TXT for details.
+//
+//===----------------------------------------------------------------------===//
+//
+// This file is shared between various sanitizers' runtime libraries and
+// provides definitions for RTEMS-specific functions.
+//===----------------------------------------------------------------------===//
+#ifndef SANITIZER_RTEMS_H
+#define SANITIZER_RTEMS_H
+
+#include "sanitizer_platform.h"
+#if SANITIZER_RTEMS
+#include "sanitizer_common.h"
+
+namespace __sanitizer {
+
+extern uptr MainThreadStackBase, MainThreadStackSize;
+extern uptr MainThreadTlsBase, MainThreadTlsSize;
+
+}  // namespace __sanitizer
+
+#endif  // SANITIZER_RTEMS
+#endif  // SANITIZER_RTEMS_H
