0a0a4d77858a804f49c089b8fd06a31aca7abced
Brian Gesiak <modocache@gmail.com>
2018-05-19 12:44:02 +0000

    [Driver] Temporarily remove test for LLVM findNearest
    Summary:
    This fixes a failure caused by the revert of https://reviews.llvm.org/rL332805.
    
    llvm-svn: 332806

--- clang/test/Driver/unknown-arg.c
+++ clang/test/Driver/unknown-arg.c
@@ -51,7 +51,6 @@
 // SILENT-NOT: warning:
 // CC1AS-DID-YOU-MEAN: error: unknown argument '-hell', did you mean '-help'?
 // CC1AS-DID-YOU-MEAN: error: unknown argument '--version', did you mean '-version'?
-// CC1AS-DID-YOU-MEAN: error: unknown argument '-debug-info-macros'
 // UNKNOWN-INTEGRATED: error: unknown integrated tool 'asphalt'. Valid tools include '-cc1' and '-cc1as'.
 
 // RUN: %clang -S %s -o %t.s  -Wunknown-to-clang-option 2>&1 | FileCheck --check-prefix=IGNORED %s
