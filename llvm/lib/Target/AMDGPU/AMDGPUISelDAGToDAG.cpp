9badad2051c41049354e2650c1b321e7e0d0d3e1
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2018-05-21 18:18:52 +0000

    [AMDGPU] Add divergence analysis as a dependency for ISel
    AMDGPUDAGToDAGISel adds DivergenceAnalysis in getAnalysisUsage
    but does not list it in pass dependencies which may lead to
    crash.
    
    Differential Revision: https://reviews.llvm.org/D47151
    
    llvm-svn: 332862

--- llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUISelDAGToDAG.cpp
@@ -241,6 +241,7 @@ public:
 INITIALIZE_PASS_BEGIN(AMDGPUDAGToDAGISel, "isel",
                       "AMDGPU DAG->DAG Pattern Instruction Selection", false, false)
 INITIALIZE_PASS_DEPENDENCY(AMDGPUArgumentUsageInfo)
+INITIALIZE_PASS_DEPENDENCY(DivergenceAnalysis)
 INITIALIZE_PASS_END(AMDGPUDAGToDAGISel, "isel",
                     "AMDGPU DAG->DAG Pattern Instruction Selection", false, false)
 
