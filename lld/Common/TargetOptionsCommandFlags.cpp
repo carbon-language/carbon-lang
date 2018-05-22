8d3fcb45e685607aef56f9e1af81538834dc1ecb
Fangrui Song <maskray@google.com>
2018-05-22 06:12:23 +0000

    CommandFlags.def -> CommandFlags.inc, leftover after r329840
    llvm-svn: 332951

--- lld/Common/TargetOptionsCommandFlags.cpp
+++ lld/Common/TargetOptionsCommandFlags.cpp
@@ -8,7 +8,7 @@
 //===----------------------------------------------------------------------===//
 //
 // This file exists as a place for global variables defined in LLVM's
-// CodeGen/CommandFlags.def. By putting the resulting object file in
+// CodeGen/CommandFlags.inc. By putting the resulting object file in
 // an archive and linking with it, the definitions will automatically be
 // included when needed and skipped when already present.
 //
@@ -21,7 +21,7 @@
 
 // Define an externally visible version of
 // InitTargetOptionsFromCodeGenFlags, so that its functionality can be
-// used without having to include llvm/CodeGen/CommandFlags.def, which
+// used without having to include llvm/CodeGen/CommandFlags.inc, which
 // would lead to multiple definitions of the command line flags.
 llvm::TargetOptions lld::InitTargetOptionsFromCodeGenFlags() {
   return ::InitTargetOptionsFromCodeGenFlags();
