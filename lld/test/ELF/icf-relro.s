52d0abd7819382ac33ba0226d830638cfcc5a44f
Peter Collingbourne <peter@pcc.me.uk>
2018-05-22 23:30:44 +0000

    Add missing REQUIRES: x86.
    llvm-svn: 333043

--- lld/test/ELF/icf-relro.s
+++ lld/test/ELF/icf-relro.s
@@ -1,3 +1,4 @@
+# REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
 # RUN: ld.lld %t -o %t2 --icf=all --ignore-data-address-equality --print-icf-sections | FileCheck %s
 
