9f7786855cad66b5fa2280fc5dc4e8833a6b0747
Dan Liew <dan@su-root.co.uk>
2018-05-22 15:06:24 +0000

    [lit] Don't run `slow.py` in `shtest-timeout.py` test.
    The program used to be used in `quick_then_slow.py` but that was
    removed in r328702. The tests always run `slow.py` on its own but
    this doesn't really test additional code so we'll just drop running
    `slow.py` so the tests run faster.
    
    rdar://problem/40221572
    
    llvm-svn: 332986

--- llvm/utils/lit/tests/Inputs/shtest-timeout/slow.py
+++
@@ -1,9 +0,0 @@
-# RUN: %{python} %s
-from __future__ import print_function
-
-import time
-import sys
-
-print("Running slow program")
-sys.stdout.flush() # Make sure the print gets flushed so it appears in lit output.
-time.sleep(6)
--- llvm/utils/lit/tests/shtest-timeout.py
+++ llvm/utils/lit/tests/shtest-timeout.py
@@ -7,7 +7,6 @@
 # RUN: not %{lit} \
 # RUN: %{inputs}/shtest-timeout/infinite_loop.py \
 # RUN: %{inputs}/shtest-timeout/short.py \
-# RUN: %{inputs}/shtest-timeout/slow.py \
 # RUN: -j 1 -v --debug --timeout 1 --param external=1 > %t.extsh.out 2> %t.extsh.err
 # RUN: FileCheck --check-prefix=CHECK-OUT-COMMON < %t.extsh.out %s
 # RUN: FileCheck --check-prefix=CHECK-EXTSH-ERR < %t.extsh.err %s
@@ -18,7 +17,6 @@
 # RUN: not %{lit} \
 # RUN: %{inputs}/shtest-timeout/infinite_loop.py \
 # RUN: %{inputs}/shtest-timeout/short.py \
-# RUN: %{inputs}/shtest-timeout/slow.py \
 # RUN: -j 1 -v --debug --timeout 1 --param external=0 > %t.intsh.out 2> %t.intsh.err
 # RUN: FileCheck  --check-prefix=CHECK-OUT-COMMON < %t.intsh.out %s
 # RUN: FileCheck --check-prefix=CHECK-INTSH-OUT < %t.intsh.out %s
@@ -28,17 +26,12 @@
 # CHECK-INTSH-OUT: command output:
 # CHECK-INTSH-OUT: command reached timeout: True
 
-# CHECK-INTSH-OUT: TIMEOUT: per_test_timeout :: slow.py
-# CHECK-INTSH-OUT: command output:
-# CHECK-INTSH-OUT: command reached timeout: True
-
 # CHECK-INTSH-ERR: Using internal shell
 
 # Test per test timeout set via a config file rather than on the command line
 # RUN: not %{lit} \
 # RUN: %{inputs}/shtest-timeout/infinite_loop.py \
 # RUN: %{inputs}/shtest-timeout/short.py \
-# RUN: %{inputs}/shtest-timeout/slow.py \
 # RUN: -j 1 -v --debug --param external=0 \
 # RUN: --param set_timeout=1 > %t.cfgset.out 2> %t.cfgset.err
 # RUN: FileCheck --check-prefix=CHECK-OUT-COMMON  < %t.cfgset.out %s
@@ -52,19 +45,14 @@
 
 # CHECK-OUT-COMMON: PASS: per_test_timeout :: short.py
 
-# CHECK-OUT-COMMON: TIMEOUT: per_test_timeout :: slow.py
-# CHECK-OUT-COMMON: Timeout: Reached timeout of 1 seconds
-# CHECK-OUT-COMMON: Command {{([0-9]+ )?}}Output
-
 # CHECK-OUT-COMMON: Expected Passes{{ *}}: 1
-# CHECK-OUT-COMMON: Individual Timeouts{{ *}}: 2
+# CHECK-OUT-COMMON: Individual Timeouts{{ *}}: 1
 
 # Test per test timeout via a config file and on the command line.
 # The value set on the command line should override the config file.
 # RUN: not %{lit} \
 # RUN: %{inputs}/shtest-timeout/infinite_loop.py \
 # RUN: %{inputs}/shtest-timeout/short.py \
-# RUN: %{inputs}/shtest-timeout/slow.py \
 # RUN: -j 1 -v --debug --param external=0 \
 # RUN: --param set_timeout=1 --timeout=2 > %t.cmdover.out 2> %t.cmdover.err
 # RUN: FileCheck --check-prefix=CHECK-CMDLINE-OVERRIDE-OUT  < %t.cmdover.out %s
@@ -78,9 +66,5 @@
 
 # CHECK-CMDLINE-OVERRIDE-OUT: PASS: per_test_timeout :: short.py
 
-# CHECK-CMDLINE-OVERRIDE-OUT: TIMEOUT: per_test_timeout :: slow.py
-# CHECK-CMDLINE-OVERRIDE-OUT: Timeout: Reached timeout of 2 seconds
-# CHECK-CMDLINE-OVERRIDE-OUT: Command {{([0-9]+ )?}}Output
-
 # CHECK-CMDLINE-OVERRIDE-OUT: Expected Passes{{ *}}: 1
-# CHECK-CMDLINE-OVERRIDE-OUT: Individual Timeouts{{ *}}: 2
+# CHECK-CMDLINE-OVERRIDE-OUT: Individual Timeouts{{ *}}: 1
