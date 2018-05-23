6356571ec03d8ba816f9ac1d506b9fd34cfc9f3f
Daniel Cederman <cederman@gaisler.com>
2018-05-23 08:26:49 +0000

    [Sparc] Add mnemonic aliases for flush, stb, stba, sth, and stha
    Reviewers: jyknight
    
    Reviewed By: jyknight
    
    Subscribers: fedor.sergeev, jrtc27, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D47140
    
    llvm-svn: 333068

--- llvm/test/MC/Sparc/sparc-mem-instructions.s
+++ llvm/test/MC/Sparc/sparc-mem-instructions.s
@@ -61,8 +61,16 @@
         stb %o2, [%i0 + 32]
         ! CHECK: stb %o2, [%g1]       ! encoding: [0xd4,0x28,0x40,0x00]
         stb %o2, [%g1]
+        ! CHECK: stb %o2, [%g1]       ! encoding: [0xd4,0x28,0x40,0x00]
+        stub %o2, [%g1]
+        ! CHECK: stb %o2, [%g1]       ! encoding: [0xd4,0x28,0x40,0x00]
+        stsb %o2, [%g1]
         ! CHECK: stba %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xae,0x10,0x76]
         stba %o2, [%i0 + %l6] 131
+        ! CHECK: stba %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xae,0x10,0x76]
+        stuba %o2, [%i0 + %l6] 131
+        ! CHECK: stba %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xae,0x10,0x76]
+        stsba %o2, [%i0 + %l6] 131
 
         ! CHECK: sth %o2, [%i0+%l6]   ! encoding: [0xd4,0x36,0x00,0x16]
         sth %o2, [%i0 + %l6]
@@ -70,8 +78,16 @@
         sth %o2, [%i0 + 32]
         ! CHECK: sth %o2, [%g1]       ! encoding: [0xd4,0x30,0x40,0x00]
         sth %o2, [%g1]
+        ! CHECK: sth %o2, [%g1]       ! encoding: [0xd4,0x30,0x40,0x00]
+        stuh %o2, [%g1]
+        ! CHECK: sth %o2, [%g1]       ! encoding: [0xd4,0x30,0x40,0x00]
+        stsh %o2, [%g1]
         ! CHECK: stha %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xb6,0x10,0x76]
         stha %o2, [%i0 + %l6] 131
+        ! CHECK: stha %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xb6,0x10,0x76]
+        stuha %o2, [%i0 + %l6] 131
+        ! CHECK: stha %o2, [%i0+%l6] 131 ! encoding: [0xd4,0xb6,0x10,0x76]
+        stsha %o2, [%i0 + %l6] 131
 
         ! CHECK: st %o2, [%i0+%l6]    ! encoding: [0xd4,0x26,0x00,0x16]
         st %o2, [%i0 + %l6]
@@ -100,3 +116,5 @@
         ! Not specified in manual, but accepted by gas.
         ! CHECK:  flush %g0             ! encoding: [0x81,0xd8,0x00,0x00]
         flush
+        ! CHECK:  flush %g0             ! encoding: [0x81,0xd8,0x00,0x00]
+        iflush
