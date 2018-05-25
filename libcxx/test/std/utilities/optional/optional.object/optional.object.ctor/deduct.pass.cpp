3854964531716ac271adc4aa85abb3533fa46142
Marshall Clow <mclow.lists@gmail.com>
2018-05-25 02:22:54 +0000

    Add one more test for optional
    llvm-svn: 333252

--- libcxx/test/std/utilities/optional/optional.object/optional.object.ctor/deduct.pass.cpp
+++ libcxx/test/std/utilities/optional/optional.object/optional.object.ctor/deduct.pass.cpp
@@ -25,20 +25,29 @@ int main()
 {  
 //  Test the explicit deduction guides
     {
-//	optional(T)
+//  optional(T)
     std::optional opt(5);
     static_assert(std::is_same_v<decltype(opt), std::optional<int>>, "");
-	assert(static_cast<bool>(opt));
-	assert(*opt == 5);
+    assert(static_cast<bool>(opt));
+    assert(*opt == 5);
     }
 
     {
-//	optional(T)
+//  optional(T)
     std::optional opt(A{});
     static_assert(std::is_same_v<decltype(opt), std::optional<A>>, "");
-	assert(static_cast<bool>(opt));
+    assert(static_cast<bool>(opt));
     }
 
 //  Test the implicit deduction guides
+
+    {
+//  optional(const optional &);
+    std::optional<char> source('A');
+    std::optional opt(source);
+    static_assert(std::is_same_v<decltype(opt), std::optional<char>>, "");
+    assert(static_cast<bool>(opt) == static_cast<bool>(source));
+    assert(*opt == *source);
+    }
 
 }
