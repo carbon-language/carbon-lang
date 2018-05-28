6f3df02fdc7faf125208f2c03a99ba3b4e9f8b1d
Zaara Syeda <syzaara@ca.ibm.com>
2018-05-28 15:27:58 +0000

    [PowerPC] Set isAsmParserOnly=1 for X-form TLS loads/stores
    The X-form TLS load/store instructions added for optimizing the initial-exec
    sequence in https://reviews.llvm.org/rL327635 fail to assemble. llvm-mc fails
    with the error: invalid operand for instruction. This patch adds these
    instructions into a block with isAsmParserOnly, similar to how ADD8TLS_ is
    currently handled.
    
    Differential Revision: https://reviews.llvm.org/D47382
    
    llvm-svn: 333374

--- llvm/test/MC/PowerPC/ppc64-tls-relocs-01.s
+++ llvm/test/MC/PowerPC/ppc64-tls-relocs-01.s
@@ -4,7 +4,16 @@
         .text
         addis 3, 13, t@tprel@ha
         addi 3, 3, t@tprel@l
-
+        addis 3, 2, t@got@tprel@ha
+        ld 3, t@got@tprel@l(3)
+        lwzx 4, 3, t@tls
+        lhzx 4, 3, t@tls
+        lbzx 4, 3, t@tls
+        ldx 4, 3, t@tls
+        stbx 4, 3, t@tls
+        sthx 4, 3, t@tls
+        stwx 4, 3, t@tls
+        stdx 4, 3, t@tls
         .type t,@object
         .section .tbss,"awT",@nobits
         .globl t
@@ -19,4 +28,14 @@ t:
 # CHECK:        Section ({{[0-9]+}}) .rela.text {
 # CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TPREL16_HA t
 # CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TPREL16_LO t
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_GOT_TPREL16_HA t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_GOT_TPREL16_LO_DS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
+# CHECK-NEXT:     0x{{[0-9,A-F]+}} R_PPC64_TLS t 0x0
 # CHECK-NEXT:   }
