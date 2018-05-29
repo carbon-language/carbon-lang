cc09d7829701b76503b49e1a533b2a5aee6e8ff9
Luke Geeson <luke.geeson@arm.com>
2018-05-29 10:12:27 +0000

    Test Commit Access - Removed Whitespace
    llvm-svn: 333406

--- llvm/examples/Kaleidoscope/MCJIT/lazy/toy-jit.cpp
+++ llvm/examples/Kaleidoscope/MCJIT/lazy/toy-jit.cpp
@@ -33,14 +33,14 @@ enum Token {
 
   // primary
   tok_identifier = -4, tok_number = -5,
-  
+
   // control
   tok_if = -6, tok_then = -7, tok_else = -8,
   tok_for = -9, tok_in = -10,
-  
+
   // operators
   tok_binary = -11, tok_unary = -12,
-  
+
   // var definition
   tok_var = -13
 };
