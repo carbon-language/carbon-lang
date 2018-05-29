6572425462375a3289b3870f5ed5359f7557a9bd
Alexander Ivchenko <alexander.ivchenko@intel.com>
2018-05-29 14:49:51 +0000

    [llvm-readobj] Support GNU_PROPERTY_X86_FEATURE_1_AND notes in .note.gnu.property
    This patch allows parsing GNU_PROPERTY_X86_FEATURE_1_AND
    notes in .note.gnu.property sections. These notes
    indicate that the object file is built to support Intel CET.
    
    patch by mike.dvoretsky
    
    Differential Revision: https://reviews.llvm.org/D47473
    
    llvm-svn: 333424

--- llvm/test/tools/llvm-readobj/note-gnu-property.s
+++ llvm/test/tools/llvm-readobj/note-gnu-property.s
@@ -2,16 +2,21 @@
 // RUN: llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o %t
 // RUN: llvm-readobj -elf-output-style GNU --notes %t | FileCheck %s
 
-// CHECK:      Displaying notes found at file offset 0x00000040 with length 0x00000070:
+// CHECK:      Displaying notes found at file offset 0x00000040 with length 0x000000b8:
 // CHECK-NEXT:   Owner                 Data size       Description
-// CHECK-NEXT:   GNU                   0x00000060      NT_GNU_PROPERTY_TYPE_0 (property note)
+// CHECK-NEXT:   GNU                   0x000000a8      NT_GNU_PROPERTY_TYPE_0 (property note)
 // CHECK-NEXT:     Properties:  stack size: 0x100
 // CHECK-NEXT:     stack size: 0x100
 // CHECK-NEXT:     no copy on protected
+// CHECK-NEXT:     X86 features: SHSTK
+// CHECK-NEXT:     X86 features: IBT, SHSTK
+// CHECK-NEXT:     X86 features: none
 // CHECK-NEXT:     <application-specific type 0xfefefefe>
 // CHECK-NEXT:     stack size: <corrupt length: 0x0>
 // CHECK-NEXT:     stack size: <corrupt length: 0x4> 
 // CHECK-NEXT:     no copy on protected <corrupt length: 0x1>
+// CHECK-NEXT:     X86 features: <corrupt length: 0x0>
+// CHECK-NEXT:     X86 features: IBT, <unknown flags: 0xf000f000f000f000>
 // CHECK-NEXT:     <corrupt type (0x2) datasz: 0x1>
 
 .section ".note.gnu.property", "a"
@@ -26,7 +31,7 @@ begin:
   .long 8           /* Data size */
   .quad 0x100       /* Data (stack size) */
   .p2align 3        /* Align to 8 byte for 64 bit */
-  
+
   /* Test we handle alignment properly */
   .long 1           /* Type: GNU_PROPERTY_STACK_SIZE */
   .long 8           /* Data size */
@@ -35,6 +40,23 @@ begin:
   
   .long 2           /* Type: GNU_PROPERTY_NO_COPY_ON_PROTECTED */
   .long 0           /* Data size */
+  .p2align 3        /* Align to 8 byte for 64 bit */
+
+  /* CET property note */
+  .long 0xc0000002  /* Type: GNU_PROPERTY_X86_FEATURE_1_AND */
+  .long 8           /* Data size */
+  .quad 2           /* GNU_PROPERTY_X86_FEATURE_1_SHSTK */
+  .p2align 3        /* Align to 8 byte for 64 bit */
+
+  /* CET property note with padding */
+  .long 0xc0000002  /* Type: GNU_PROPERTY_X86_FEATURE_1_AND */
+  .long 4           /* Data size */
+  .long 3           /* Full CET support */
+  .p2align 3        /* Align to 8 byte for 64 bit */
+
+  .long 0xc0000002  /* Type: GNU_PROPERTY_X86_FEATURE_1_AND */
+  .long 8           /* Data size */
+  .quad 0           /* Empty flags, not an error */
   .p2align 3        /* Align to 8 byte for 64 bit */
   
   /* All notes below are broken. Test we are able to report them. */
@@ -60,6 +82,17 @@ begin:
   .long 1           /* Data size (corrupted) */
   .byte 1           /* Data */
   .p2align 3        /* Align to 8 byte for 64 bit */
+
+  /* CET note with size zero */
+  .long 0xc0000002  /* Type: GNU_PROPERTY_X86_FEATURE_1_AND */
+  .long 0           /* Data size */
+  .p2align 3        /* Align to 8 byte for 64 bit */
+
+  /* CET note with bad flags */
+  .long 0xc0000002         /* Type: GNU_PROPERTY_X86_FEATURE_1_AND */
+  .long 8                  /* Data size */
+  .quad 0xf000f000f000f001 /* GNU_PROPERTY_X86_FEATURE_1_IBT and bad bits */
+  .p2align 3               /* Align to 8 byte for 64 bit */
   
   /* GNU_PROPERTY_NO_COPY_ON_PROTECTED with pr_datasz and without data */
   .long 2           /* Type: GNU_PROPERTY_NO_COPY_ON_PROTECTED */
