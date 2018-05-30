8cc53aecad897c8f50ad41715a16b94da2462c39
Daniel Cederman <cederman@gaisler.com>
2018-05-30 06:02:18 +0000

    [Sparc] Add floating-point register names
    Reviewers: jyknight
    
    Reviewed By: jyknight
    
    Subscribers: eraman, fedor.sergeev, jrtc27, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D47137
    
    llvm-svn: 333510

--- clang/lib/Basic/Targets/Sparc.cpp
+++ clang/lib/Basic/Targets/Sparc.cpp
@@ -20,9 +20,17 @@ using namespace clang;
 using namespace clang::targets;
 
 const char *const SparcTargetInfo::GCCRegNames[] = {
+    // Integer registers
     "r0",  "r1",  "r2",  "r3",  "r4",  "r5",  "r6",  "r7",  "r8",  "r9",  "r10",
     "r11", "r12", "r13", "r14", "r15", "r16", "r17", "r18", "r19", "r20", "r21",
-    "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31"
+    "r22", "r23", "r24", "r25", "r26", "r27", "r28", "r29", "r30", "r31",
+
+    // Floating-point registers
+    "f0",  "f1",  "f2",  "f3",  "f4",  "f5",  "f6",  "f7",  "f8",  "f9",  "f10",
+    "f11", "f12", "f13", "f14", "f15", "f16", "f17", "f18", "f19", "f20", "f21",
+    "f22", "f23", "f24", "f25", "f26", "f27", "f28", "f29", "f30", "f31", "f32",
+    "f34", "f36", "f38", "f40", "f42", "f44", "f46", "f48", "f50", "f52", "f54",
+    "f56", "f58", "f60", "f62",
 };
 
 ArrayRef<const char *> SparcTargetInfo::getGCCRegNames() const {
