367a53b05994fdfda2d67cb4549289792acc37d0
Sander de Smalen <sander.desmalen@arm.com>
2018-06-04 05:58:06 +0000

    [AArch64][SVE] Asm: Support for FCPY immediate instructions.
    Predicated copy of floating-point immediate value to SVE vector,
    along with MOV-aliases.
    
    Reviewers: rengolin, fhahn, samparker, SjoerdMeijer, javed.absar
    
    Reviewed By: javed.absar
    
    Differential Revision: https://reviews.llvm.org/D47518
    
    llvm-svn: 333869

--- llvm/test/MC/AArch64/SVE/fmov-diagnostics.s
+++ llvm/test/MC/AArch64/SVE/fmov-diagnostics.s
@@ -1,6 +1,24 @@
 // RUN: not llvm-mc -triple=aarch64 -show-encoding -mattr=+sve  2>&1 < %s| FileCheck %s
 
 // --------------------------------------------------------------------------//
+// Invalid predicate suffix
+fmov z0.h, p0/z, #0.0
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
+// CHECK-NEXT: fmov z0.h, p0/z, #0.0
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.s, p0/z, #0.0
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
+// CHECK-NEXT: fmov z0.s, p0/z, #0.0
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.d, p0/z, #0.0
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
+// CHECK-NEXT: fmov z0.d, p0/z, #0.0
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+
+// --------------------------------------------------------------------------//
 // Invalid immediates
 
 fmov z0.h, #-0.05859375   // r = -4, n = 15
@@ -61,4 +79,64 @@ fmov z0.s, #64.00000000   // r = 5, n = 32
 fmov z0.d, #64.00000000   // r = 5, n = 32
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant
 // CHECK-NEXT: fmov z0.d, #64.00000000   // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.h, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.h, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.s, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.s, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.d, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.d, p0/m, #-0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.h, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.h, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.s, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.s, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.d, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.d, p0/m, #-64.00000000    // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.h, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.h, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.s, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.s, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.d, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.d, p0/m, #0.05859375    // r = -4, n = 15
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.h, p0/m, #64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.h, p0/m, #64.00000000    // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.s, p0/m, #64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.s, p0/m, #64.00000000    // r = 5, n = 32
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+fmov z0.d, p0/m, #64.00000000    // r = 5, n = 32
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: expected compatible register or floating-point constant 
+// CHECK-NEXT: fmov z0.d, p0/m, #64.00000000    // r = 5, n = 32
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
