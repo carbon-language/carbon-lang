80a764bab17cf984db298078ba9010a553a15335
Vlad Tsyrklevich <vlad@tsyrklevich.net>
2018-06-06 06:25:37 +0000

    [Analyzer] Fix the Z3 lit test config
    Summary:
    The '%analyze' extra_args config argument seems to have been erroneously
    deleted in r315627 disabling Z3 tests for the clang analyzer. Add the
    flag back.
    
    Reviewers: george.karpenkov, NoQ, ddcc
    
    Reviewed By: george.karpenkov
    
    Subscribers: xazax.hun, szepet, delcypher, a.sidorin, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D47722
    
    llvm-svn: 334066

--- llvm/utils/lit/lit/llvm/config.py
+++ llvm/utils/lit/lit/llvm/config.py
@@ -388,7 +388,7 @@ class LLVMConfig(object):
         builtin_include_dir = self.get_clang_builtin_include_dir(self.config.clang)
         tool_substitutions = [
             ToolSubst('%clang', command=self.config.clang),
-            ToolSubst('%clang_analyze_cc1', command='%clang_cc1', extra_args=['-analyze']),
+            ToolSubst('%clang_analyze_cc1', command='%clang_cc1', extra_args=['-analyze', '%analyze']),
             ToolSubst('%clang_cc1', command=self.config.clang, extra_args=['-cc1', '-internal-isystem', builtin_include_dir, '-nostdsysteminc']),
             ToolSubst('%clang_cpp', command=self.config.clang, extra_args=['--driver-mode=cpp']),
             ToolSubst('%clang_cl', command=self.config.clang, extra_args=['--driver-mode=cl']),
