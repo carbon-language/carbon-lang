6e85e6309d38393c2a4360f8232d5b11540a6aa4
Jan Vesely <jan.vesely@rutgers.edu>
2018-06-07 20:27:43 +0000

    math/fma: Add fp32 software implementation
    Passes CTS on carrizo (when forced to use sw fma) and turks.
    Reviewer: Tom Stellard <tstellar@redhat.com>
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    
    llvm-svn: 334226

--- libclc/generic/include/clc/math/fma.h
+++ libclc/generic/include/clc/math/fma.h
@@ -1,6 +1,7 @@
-#undef fma
-#define fma __clc_fma
+#define __CLC_BODY <clc/math/ternary_decl.inc>
+#define __CLC_FUNCTION fma
 
-#define __CLC_FUNCTION __clc_fma
-#define __CLC_INTRINSIC "llvm.fma"
-#include <clc/math/ternary_intrin.inc>
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
---
+++ libclc/generic/include/math/clc_fma.h
@@ -0,0 +1,11 @@
+#define __CLC_FUNCTION __clc_fma
+#define __CLC_INTRINSIC "llvm.fma"
+#include <clc/math/ternary_intrin.inc>
+
+#define __FLOAT_ONLY
+#define __CLC_FUNCTION __clc_sw_fma
+#define __CLC_BODY <clc/math/ternary_decl.inc>
+#include <clc/math/gentype.inc>
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
+#undef __FLOAT_ONLY
