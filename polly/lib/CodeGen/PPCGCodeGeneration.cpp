e6e182800498b5e3402109938d0a4f8d576c3d0f
Philip Pfaffe <philip.pfaffe@gmail.com>
2018-06-07 08:32:13 +0000

    Run clang-format
    llvm-svn: 334172

--- polly/lib/CodeGen/PPCGCodeGeneration.cpp
+++ polly/lib/CodeGen/PPCGCodeGeneration.cpp
@@ -1408,7 +1408,7 @@ const std::map<std::string, std::string> IntrinsicToLibdeviceFunc = {
 ///
 /// Return "" if we are not compiling for CUDA.
 std::string getCUDALibDeviceFuntion(Function *F) {
-  auto FnName = [&] () -> const std::string {
+  auto FnName = [&]() -> const std::string {
     auto It = IntrinsicToLibdeviceFunc.find(F->getName());
     if (It != IntrinsicToLibdeviceFunc.end())
       return It->second;
