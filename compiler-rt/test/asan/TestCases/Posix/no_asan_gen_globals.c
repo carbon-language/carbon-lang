683e2d636d482ecd71dc7e6bd3cb28df03fd75d2
Vitaly Buka <vitalybuka@google.com>
2018-06-08 21:04:44 +0000

    [asan] Re-enable no_asan_gen_globals for 32-bit
    PR22682. Bug in gold was fixed 3 years ago.
    
    llvm-svn: 334319

--- compiler-rt/test/asan/TestCases/Posix/no_asan_gen_globals.c
+++ compiler-rt/test/asan/TestCases/Posix/no_asan_gen_globals.c
@@ -1,7 +1,5 @@
 // FIXME: https://code.google.com/p/address-sanitizer/issues/detail?id=316
 // XFAIL: android
-// FIXME: http://llvm.org/bugs/show_bug.cgi?id=22682
-// REQUIRES: asan-64-bits
 // Make sure __asan_gen_* strings do not end up in the symbol table.
 
 // RUN: %clang_asan %s -o %t.exe
