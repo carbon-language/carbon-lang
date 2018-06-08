982241097ce0c559b1815a6593f7ace979515930
Pavel Labath <labath@google.com>
2018-06-08 10:39:55 +0000

    Fix TestMiExec.py
    r334215 changed the error message the tool prints for invalid thread
    arguments to -exec-next command. This adjust the test to match that.
    
    llvm-svn: 334279

--- lldb/packages/Python/lldbsuite/test/tools/lldb-mi/control/TestMiExec.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-mi/control/TestMiExec.py
@@ -187,9 +187,9 @@ class MiExecTestCase(lldbmi_testcase.MiTestCaseBase):
 
         # Test that an invalid --thread is handled
         self.runCmd("-exec-next --thread 0")
-        self.expect("\^error,message=\"error: Thread index 0 is out of range")
+        self.expect("\^error,msg=\"Command 'exec-next'. Thread ID invalid")
         self.runCmd("-exec-next --thread 10")
-        self.expect("\^error,message=\"error: Thread index 10 is out of range")
+        self.expect("\^error,msg=\"Command 'exec-next'. Thread ID invalid")
 
         # Test that an invalid --frame is handled
         # FIXME: no error is returned
