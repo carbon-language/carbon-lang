ecba8846aab3c7ab0c6d223456834222b8bec488
Frederic Riss <friss@apple.com>
2018-06-15 20:36:03 +0000

    Fix TestExec after r334783
    The second makefile that was added has implicit rules which meant
    that secondprog.cpp would be built once into a secondprog binary,
    but it would also be compiled as a.out overwriting the main binary.
    This lead to spurious failures.
    
    This commit simplifies the Makefile to build only once with the correct
    executable name.
    
    llvm-svn: 334861

--- lldb/packages/Python/lldbsuite/test/functionalities/exec/secondprog.mk
+++ lldb/packages/Python/lldbsuite/test/functionalities/exec/secondprog.mk
@@ -1,13 +1,6 @@
 LEVEL = ../../make
 
 CXX_SOURCES := secondprog.cpp
-
-all: secondprog
-
-secondprog:
-	$(CXX) $(CXXFLAGS) -o secondprog $(SRCDIR)/secondprog.cpp
-
-clean::
-	rm -rf secondprog secondprog.dSYM
+EXE = secondprog
 
 include $(LEVEL)/Makefile.rules
