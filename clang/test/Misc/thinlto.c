9a8be606f3f0801df0e128f09e37057da10a0b64
Tobias Edler von Koch <tobias@codeaurora.org>
2018-06-21 20:20:41 +0000

    [LTO] Enable module summary emission by default for regular LTO
    Summary:
    With D33921, we gained the ability to have module summaries in regular
    LTO modules without triggering ThinLTO compilation. Module summaries in
    regular LTO allow garbage collection (dead stripping) before LTO
    compilation and thus open up additional optimization opportunities.
    
    This patch enables summary emission in regular LTO for all targets
    except ld64-based ones (which use the legacy LTO API).
    
    Reviewers: pcc, tejohnson, mehdi_amini
    
    Subscribers: inglorion, eraman, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D34156
    
    llvm-svn: 335284

--- clang/test/Misc/thinlto.c
+++
@@ -1,4 +0,0 @@
-// RUN: %clang_cc1 -flto=thin -emit-llvm-bc < %s | llvm-bcanalyzer -dump | FileCheck %s
-// ; Check that the -flto=thin option emits a summary
-// CHECK: <GLOBALVAL_SUMMARY_BLOCK
-int main() {}
