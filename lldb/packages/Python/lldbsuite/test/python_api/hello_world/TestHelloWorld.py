56a196c0209d4700511525844542bdd4ab2aeaf9
Adrian Prantl <aprantl@apple.com>
2018-06-22 20:26:53 +0000

    Mark this test as no debuginfo
    llvm-svn: 335386

--- lldb/packages/Python/lldbsuite/test/python_api/hello_world/TestHelloWorld.py
+++ lldb/packages/Python/lldbsuite/test/python_api/hello_world/TestHelloWorld.py
@@ -14,7 +14,7 @@ from lldbsuite.test import lldbutil
 
 
 class HelloWorldTestCase(TestBase):
-
+    NO_DEBUG_INFO_TESTCASE = True
     mydir = TestBase.compute_mydir(__file__)
 
     def setUp(self):
