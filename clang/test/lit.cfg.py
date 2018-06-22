9508af351a1648995a28199e972df724350727ca
Benjamin Kramer <benny.kra@googlemail.com>
2018-06-22 20:03:32 +0000

    Reinstate quotes around the path to python that I accidentaly removed in r335330
    This broke users with spaces in the path, like C:\Program Files\Python
    
    llvm-svn: 335382

--- clang/test/lit.cfg.py
+++ clang/test/lit.cfg.py
@@ -70,7 +70,7 @@ if config.clang_examples:
 llvm_config.add_tool_substitutions(tools, tool_dirs)
 
 config.substitutions.append(
-    ('%hmaptool', '%s %s' % (config.python_executable,
+    ('%hmaptool', "'%s' %s" % (config.python_executable,
                              os.path.join(config.llvm_tools_dir, 'hmaptool'))))
 
 # Plugins (loadable modules)
