6c434880301e4913aa93002d0ea73e0b41f60ff0
Paul Robinson <paul.robinson@sony.com>
2018-06-22 15:07:26 +0000

    Fix test, nop is not always 1 byte
    llvm-svn: 335353

--- llvm/test/MC/ELF/dwarf-loc0.s
+++ llvm/test/MC/ELF/dwarf-loc0.s
@@ -17,9 +17,9 @@
 # CHECK-NEXT: name: "header.h"
 # CHECK:      Address Line Column File
 # CHECK-NEXT: -------
-# CHECK-NEXT: 0x{{0+}}0 10 0 0
-# CHECK-NEXT: 0x{{0+}}1 20 0 1
-# CHECK-NEXT: 0x{{0+}}2 20 0 1 {{.*}} end_sequence
+# CHECK-NEXT: 0x{{[0-9a-f]+}} 10 0 0
+# CHECK-NEXT: 0x{{[0-9a-f]+}} 20 0 1
+# CHECK-NEXT: 0x{{[0-9a-f]+}} 20 0 1 {{.*}} end_sequence
 
 # ERR:      file number less than one in '.loc' directive
 # ERR-NEXT: .loc 0 10 0
