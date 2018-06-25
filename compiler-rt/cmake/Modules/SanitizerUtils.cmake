7a0bf90dcf0b1df5af201a280680b240131f6955
David Carlier <devnexen@gmail.com>
2018-06-25 21:10:25 +0000

    [UBsan] Enable ubsan minimal unit tests on OpenBSD
    OpenBSD needs lld linker for sanitisers.
    Disabling lint checking as some symbols cannot be defined and block the proper unit tests launch.
    
    Reviewers: lebedev.ri, vitalybuka
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D48528
    
    llvm-svn: 335524

--- compiler-rt/cmake/Modules/SanitizerUtils.cmake
+++ compiler-rt/cmake/Modules/SanitizerUtils.cmake
@@ -81,7 +81,7 @@ macro(add_sanitizer_rt_version_list name)
 endmacro()
 
 # Add target to check code style for sanitizer runtimes.
-if(CMAKE_HOST_UNIX)
+if(CMAKE_HOST_UNIX AND NOT OS_NAME MATCHES "OpenBSD")
   add_custom_target(SanitizerLintCheck
     COMMAND env LLVM_CHECKOUT=${LLVM_MAIN_SRC_DIR} SILENT=1 TMPDIR=
       PYTHON_EXECUTABLE=${PYTHON_EXECUTABLE}
@@ -90,5 +90,8 @@ if(CMAKE_HOST_UNIX)
     DEPENDS ${SANITIZER_LINT_SCRIPT}
     COMMENT "Running lint check for sanitizer sources..."
     VERBATIM)
+else()
+  add_custom_target(SanitizerLintCheck
+    COMMAND echo "No lint check")
 endif()
 
