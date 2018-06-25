871b0591294ba68f694ec7105e0479499354f99e
George Rimar <grimar@accesssoftek.com>
2018-06-25 14:35:47 +0000

    [ELF] - Rewrote comment. NFC.
    llvm-svn: 335479

--- lld/test/ELF/icf13.s
+++ lld/test/ELF/icf13.s
@@ -1,8 +1,8 @@
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t1
 # RUN: ld.lld -shared -z notext %t1 -o %t --icf=all --print-icf-sections 2>&1 | FileCheck -allow-empty %s
 
-## Check ICF does not collect sections which relocations point to symbols
-## of the different types. Like to defined and undefined symbols in this test case.
+## Check that ICF does not merge sections which relocations point to symbols
+## that are not of the regular defined kind. 
 
 # CHECK-NOT: selected
 
