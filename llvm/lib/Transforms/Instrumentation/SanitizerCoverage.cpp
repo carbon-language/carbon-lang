1649774816f791ee725349f2de4f73f2d0ff402b
Benjamin Kramer <benny.kra@googlemail.com>
2018-06-25 21:43:09 +0000

    [Instrumentation] Remove unused include
    It's also a layering violation.
    
    llvm-svn: 335528

--- llvm/lib/Transforms/Instrumentation/SanitizerCoverage.cpp
+++ llvm/lib/Transforms/Instrumentation/SanitizerCoverage.cpp
@@ -35,7 +35,6 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 #include "llvm/Transforms/Instrumentation.h"
-#include "llvm/Transforms/Scalar.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
 #include "llvm/Transforms/Utils/ModuleUtils.h"
 
