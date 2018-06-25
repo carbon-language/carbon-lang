a8448ad09823e3ea38dc4a50e815ebd46cdc2cd9
Kamil Rytarowski <n54@gmx.com>
2018-06-25 18:49:13 +0000

    Handle NetBSD specific path in findDebugBinary()
    Summary:
    The NetBSD Operating System installs debuginfo
    files into /usr/libdata/debug, rather than other path
    like in some other popular distribution.
    
    This change makes llvm-symbolizer functional with
    the basesystem executables.
    
    Reviewers: joerg, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: JDevlieghere, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D48525
    
    llvm-svn: 335511

--- llvm/lib/DebugInfo/Symbolize/Symbolize.cpp
+++ llvm/lib/DebugInfo/Symbolize/Symbolize.cpp
@@ -192,8 +192,13 @@ bool findDebugBinary(const std::string &OrigPath,
     Result = DebugPath.str();
     return true;
   }
+#if defined(__NetBSD__)
+  // Try /usr/libdata/debug/path/to/original_binary/debuglink_name
+  DebugPath = "/usr/libdata/debug";
+#else
   // Try /usr/lib/debug/path/to/original_binary/debuglink_name
   DebugPath = "/usr/lib/debug";
+#endif
   llvm::sys::path::append(DebugPath, llvm::sys::path::relative_path(OrigDir),
                           DebuglinkName);
   if (checkFileCRC(DebugPath, CRCHash)) {
