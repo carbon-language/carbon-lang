a3be535e6ec26865ed4aff7b8356fbf5c4283fb0
George Rimar <grimar@accesssoftek.com>
2018-06-28 10:10:02 +0000

    [ELF] - Add test case to cover "corrupted CIE (failed to read string)"
    It was uncovered by our test cases.
    
    llvm-svn: 335824

---
+++ lld/test/ELF/invalid-eh-frame2.s
@@ -0,0 +1,23 @@
+# REQUIRES: x86
+
+# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux %s -o %t
+# RUN: not ld.lld --eh-frame-hdr %t -o %t2 2>&1 | FileCheck %s
+
+# CHECK:      error: corrupted .eh_frame: corrupted CIE (failed to read string)
+# CHECK-NEXT: >>> defined in {{.*}}:(.eh_frame+0x9)
+
+.section .eh_frame
+.align 1
+  .byte 0x08
+  .byte 0x00
+  .byte 0x00
+  .byte 0x00
+  .byte 0x00
+  .byte 0x00
+  .byte 0x00
+  .byte 0x00
+  .byte 0x01
+  .byte 0x01
+  .byte 0x01
+  .byte 0x01
+ 
