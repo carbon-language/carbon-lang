d93a1e52715f74ad4fe4bf758af0f6b607d3cdf8
Haojian Wu <hokein@google.com>
2018-06-28 15:24:34 +0000

    Correct the test modified in rL335777.
    llvm-svn: 335865

--- compiler-rt/test/cfi/cross-dso-diagnostic.cpp
+++ compiler-rt/test/cfi/cross-dso-diagnostic.cpp
@@ -2,7 +2,7 @@
 
 // RUN: %clangxx_cfi_diag -g -DSHARED_LIB -fPIC -shared -o %dynamiclib %s %ld_flags_rpath_so
 // RUN: %clangxx_cfi_diag -g -o %t_exe_suffix %s %ld_flags_rpath_exe
-// RUN: %t_exe_suffix 2>&1 | FileCheck -DDSONAME=%xdynamiclib_filename %s
+// RUN: %t_exe_suffix 2>&1 | FileCheck -DDSONAME=%xdynamiclib_namespec %s
 
 // UNSUPPORTED: win32
 // REQUIRES: cxxabi
