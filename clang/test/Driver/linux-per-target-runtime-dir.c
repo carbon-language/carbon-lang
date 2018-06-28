1a330a1acfacee3baef50104d3f1a32aa028aac0
Petr Hosek <phosek@chromium.org>
2018-06-28 03:54:08 +0000

    Handle both Linux and Windows path separator for the resource dir
    The resource dir path used for the multiarch runtimes support is
    constructed in a platform independent way and therefore will use
    native path separators on each platform. We need to make sure that
    the per target runtime directory test handles both to not fail
    when the test is being executed on Windows.
    
    llvm-svn: 335810

--- clang/test/Driver/linux-per-target-runtime-dir.c
+++ clang/test/Driver/linux-per-target-runtime-dir.c
@@ -12,10 +12,10 @@
 // CHECK-PER-TARGET-RUNTIME: "-internal-isystem" "[[RESDIR]]/include/c++/v1"
 // CHECK-PER-TARGET-RUNTIME: "-internal-isystem" "[[SYSROOT]]/usr/local/include"
 // CHECK-PER-TARGET-RUNTIME: "--sysroot=[[SYSROOT]]"
-// CHECK-PER-TARGET-RUNTIME: "-L[[RESDIR]]/x86_64-linux-gnu/lib"
+// CHECK-PER-TARGET-RUNTIME: "-L[[RESDIR]]{{/|\\\\}}x86_64-linux-gnu{{/|\\\\}}lib"
 
 // RUN: %clang -rtlib=compiler-rt -print-libgcc-file-name 2>&1 \
 // RUN:     --target=x86_64-linux-gnu \
 // RUN:     -resource-dir=%S/Inputs/resource_dir_with_per_target_subdir \
 // RUN:   | FileCheck --check-prefix=CHECK-CLANGRT-X8664 %s
-// CHECK-CLANGRT-X8664: x86_64-linux-gnu/lib/libclang_rt.builtins.a
+// CHECK-CLANGRT-X8664: x86_64-linux-gnu{{/|\\\\}}lib{{/|\\\\}}libclang_rt.builtins.a
