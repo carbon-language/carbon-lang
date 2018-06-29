ac9b6adf77d72b1b72e55f97893bdfc47c0f5851
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2018-06-29 15:16:45 +0000

    [scs] Disable negative test in shadowcallstack.
    The test checks that scs does NOT work correctly w/o runtime support.
    That's a strange thing to test, and it is also flaky, because things
    may just work if x18 happens to point to a writable page.
    
    llvm-svn: 335982

--- compiler-rt/test/shadowcallstack/init.c
+++ compiler-rt/test/shadowcallstack/init.c
@@ -1,18 +1,10 @@
-// RUN: %clang_scs -D INCLUDE_RUNTIME %s -o %t
-// RUN: %run %t
-
 // RUN: %clang_scs %s -o %t
-// RUN: not --crash %run %t
+// RUN: %run %t
 
 // Basic smoke test for the runtime
 
 #include "libc_support.h"
-
-#ifdef INCLUDE_RUNTIME
 #include "minimal_runtime.h"
-#else
-#define scs_main main
-#endif
 
 int scs_main(void) {
   scs_fputs_stdout("In main.\n");
