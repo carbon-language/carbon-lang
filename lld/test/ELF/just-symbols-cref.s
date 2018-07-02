36e021490d3fcc5a3fce95d877ba0ed10aa31f83
Fangrui Song <maskray@google.com>
2018-07-02 19:12:10 +0000

    [ELF] Fix just-symbols*.s after r336129
    llvm-svn: 336135

--- lld/test/ELF/just-symbols-cref.s
+++ lld/test/ELF/just-symbols-cref.s
@@ -1,9 +1,9 @@
 # REQUIRES: x86
 
 # RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t.o
-# RUN: ld.lld %t.o -o /dev/null -Ttext=0x10000
+# RUN: ld.lld %t.o -o %t1.exe -Ttext=0x10000
 
-# RUN: ld.lld -just-symbols=%t1.exe -o /dev/null -cref | FileCheck %s
+# RUN: ld.lld -just-symbols=%t1.exe -o %t2.exe -cref | FileCheck %s
 
 # CHECK:      Symbol      File
 # CHECK-NEXT: bar         {{.*exe}}
