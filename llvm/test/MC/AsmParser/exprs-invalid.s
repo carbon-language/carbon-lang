07ef10ccb6e74586f1a29e8afccad49c1e7913d5
Alex Bradbury <asb@lowrisc.org>
2018-07-02 14:13:27 +0000

    [X86] Fix test/MC/AsmParser/exprs-invalid.s after rL336104
    This was my mistake for only running test/MC/X86 and test/CodeGen/X86. 
    Arguably .word should be removed from this test, as it is not supported 
    universally.
    
    llvm-svn: 336107

--- llvm/test/MC/AsmParser/exprs-invalid.s
+++ llvm/test/MC/AsmParser/exprs-invalid.s
@@ -13,5 +13,5 @@
 // CHECK-ERRORS: [[@LINE+1]]:7: error: out of range literal value in '.long' directive
 .long 4e71cf69 // double floating point constant due to missing "0x"
 
-// CHECK-ERRORS: [[@LINE+1]]:7: error: literal value out of range for directive
+// CHECK-ERRORS: [[@LINE+1]]:7:  error: out of range literal value in '.word' directive
 .word 0xfffffffff
