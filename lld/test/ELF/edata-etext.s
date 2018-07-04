0fc47bea90c0627e4c07d0c4306855e5c984a6fe
George Rimar <grimar@accesssoftek.com>
2018-07-04 14:47:19 +0000

    [ELF] - Extend edata-etext.s test case.
    We did not have a test that would test that
    _etext address is equal to etext, _end == end and _edata == edata.
    
    Because of that, the following line was never
    executed when running our tests:
    https://github.com/llvm-mirror/lld/blob/master/ELF/Writer.cpp#L928
    
    Patch fixes that.
    
    llvm-svn: 336280

--- lld/test/ELF/edata-etext.s
+++ lld/test/ELF/edata-etext.s
@@ -10,6 +10,7 @@
 ##    greater than the address of _etext, the address of _end is same as the address
 ##    of _edata." (https://docs.oracle.com/cd/E53394_01/html/E54766/u-etext-3c.html).
 ## 3) Address of _end is different from _edata because of 2.
+## 4) Addresses of _edata == edata, _end == end and _etext == etext.
 # CHECK:      Sections:
 # CHECK-NEXT:  Idx Name          Size      Address          Type
 # CHECK-NEXT:    0               00000000 0000000000000000
@@ -22,6 +23,9 @@
 # CHECK-NEXT:  000000000020200a         .bss  00000000 _end
 # CHECK-NEXT:  0000000000201001         .text 00000000 _etext
 # CHECK-NEXT:  0000000000201000         .text 00000000 _start
+# CHECK-NEXT:  0000000000202002         .data 00000000 edata
+# CHECK-NEXT:  000000000020200a         .bss  00000000 end
+# CHECK-NEXT:  0000000000201001         .text 00000000 etext
 
 # RUN: ld.lld -r %t.o -o %t2
 # RUN: llvm-objdump -t %t2 | FileCheck %s --check-prefix=RELOCATABLE
@@ -29,7 +33,7 @@
 # RELOCATABLE-NEXT:  0000000000000000 *UND* 00000000 _end
 # RELOCATABLE-NEXT:  0000000000000000 *UND* 00000000 _etext
 
-.global _start,_end,_etext,_edata
+.global _start,_end,_etext,_edata,end,etext,edata
 .text
 _start:
   nop
