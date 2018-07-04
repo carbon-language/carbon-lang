1e4dc2e97d6cf6a8c13e90cf5cc9eec56a62e4f8
Sander de Smalen <sander.desmalen@arm.com>
2018-07-04 14:05:33 +0000

    [AArch64][SVE] Asm: Support for reversed subtract (SUBR) instruction.
    This patch adds both a vector and an immediate form, e.g.                  
                                                                               
    - Vector form:                                                             
                                                                               
        subr z0.h, p0/m, z0.h, z1.h                                            
                                                                               
      subtract active elements of z0 from z1, and store the result in z0.      
                                                                               
    - Immediate form:                                                          
                                                                               
        subr z0.h, z0.h, #255                                                  
                                                                               
      subtract elements of z0, and store the result in z0.
    
    llvm-svn: 336274

---
+++ llvm/test/MC/AArch64/SVE/subr.s
@@ -0,0 +1,117 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+
+subr    z0.b, p0/m, z0.b, z0.b
+// CHECK-INST: subr z0.b, p0/m, z0.b, z0.b
+// CHECK-ENCODING: [0x00,0x00,0x03,0x04]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 00 03 04 <unknown>
+
+subr    z0.h, p0/m, z0.h, z0.h
+// CHECK-INST: subr z0.h, p0/m, z0.h, z0.h
+// CHECK-ENCODING: [0x00,0x00,0x43,0x04]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 00 43 04 <unknown>
+
+subr    z0.s, p0/m, z0.s, z0.s
+// CHECK-INST: subr z0.s, p0/m, z0.s, z0.s
+// CHECK-ENCODING: [0x00,0x00,0x83,0x04]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 00 83 04 <unknown>
+
+subr    z0.d, p0/m, z0.d, z0.d
+// CHECK-INST: subr z0.d, p0/m, z0.d, z0.d
+// CHECK-ENCODING: [0x00,0x00,0xc3,0x04]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 00 c3 04 <unknown>
+
+subr    z0.b, z0.b, #0
+// CHECK-INST: subr z0.b, z0.b, #0
+// CHECK-ENCODING: [0x00,0xc0,0x23,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 c0 23 25 <unknown>
+
+subr    z31.b, z31.b, #255
+// CHECK-INST: subr z31.b, z31.b, #255
+// CHECK-ENCODING: [0xff,0xdf,0x23,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff df 23 25 <unknown>
+
+subr    z0.h, z0.h, #0
+// CHECK-INST: subr z0.h, z0.h, #0
+// CHECK-ENCODING: [0x00,0xc0,0x63,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 c0 63 25 <unknown>
+
+subr    z0.h, z0.h, #0, lsl #8
+// CHECK-INST: subr z0.h, z0.h, #0, lsl #8
+// CHECK-ENCODING: [0x00,0xe0,0x63,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 e0 63 25 <unknown>
+
+subr    z31.h, z31.h, #255, lsl #8
+// CHECK-INST: subr z31.h, z31.h, #65280
+// CHECK-ENCODING: [0xff,0xff,0x63,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff 63 25 <unknown>
+
+subr    z31.h, z31.h, #65280
+// CHECK-INST: subr z31.h, z31.h, #65280
+// CHECK-ENCODING: [0xff,0xff,0x63,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff 63 25 <unknown>
+
+subr    z0.s, z0.s, #0
+// CHECK-INST: subr z0.s, z0.s, #0
+// CHECK-ENCODING: [0x00,0xc0,0xa3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 c0 a3 25 <unknown>
+
+subr    z0.s, z0.s, #0, lsl #8
+// CHECK-INST: subr z0.s, z0.s, #0, lsl #8
+// CHECK-ENCODING: [0x00,0xe0,0xa3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 e0 a3 25 <unknown>
+
+subr    z31.s, z31.s, #255, lsl #8
+// CHECK-INST: subr z31.s, z31.s, #65280
+// CHECK-ENCODING: [0xff,0xff,0xa3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff a3 25 <unknown>
+
+subr    z31.s, z31.s, #65280
+// CHECK-INST: subr z31.s, z31.s, #65280
+// CHECK-ENCODING: [0xff,0xff,0xa3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff a3 25 <unknown>
+
+subr    z0.d, z0.d, #0
+// CHECK-INST: subr z0.d, z0.d, #0
+// CHECK-ENCODING: [0x00,0xc0,0xe3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 c0 e3 25 <unknown>
+
+subr    z0.d, z0.d, #0, lsl #8
+// CHECK-INST: subr z0.d, z0.d, #0, lsl #8
+// CHECK-ENCODING: [0x00,0xe0,0xe3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 00 e0 e3 25 <unknown>
+
+subr    z31.d, z31.d, #255, lsl #8
+// CHECK-INST: subr z31.d, z31.d, #65280
+// CHECK-ENCODING: [0xff,0xff,0xe3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff e3 25 <unknown>
+
+subr    z31.d, z31.d, #65280
+// CHECK-INST: subr z31.d, z31.d, #65280
+// CHECK-ENCODING: [0xff,0xff,0xe3,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff ff e3 25 <unknown>
