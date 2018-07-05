ccf0db9974d80a5d7eca466ba8a1e7d7ae868890
George Rimar <grimar@accesssoftek.com>
2018-07-05 14:09:47 +0000

    [ELF] - Convert excessive dyn_cast -> cast. NFC.
    Currently, there are only OutputSection and SymbolAssignment
    commands possible at the first level under SECTIONS tag.
    
    Hence, dyn_cast was excessive.
    
    llvm-svn: 336354

--- lld/ELF/LinkerScript.cpp
+++ lld/ELF/LinkerScript.cpp
@@ -248,13 +248,12 @@ void LinkerScript::declareSymbols() {
       declareSymbol(Cmd);
       continue;
     }
-    auto *Sec = dyn_cast<OutputSection>(Base);
-    if (!Sec)
-      continue;
+
     // If the output section directive has constraints,
     // we can't say for sure if it is going to be included or not.
     // Skip such sections for now. Improve the checks if we ever
     // need symbols from that sections to be declared early.
+    auto *Sec = cast<OutputSection>(Base);
     if (Sec->Constraint != ConstraintKind::NoConstraint)
       continue;
     for (BaseCommand *Base2 : Sec->SectionCommands)
