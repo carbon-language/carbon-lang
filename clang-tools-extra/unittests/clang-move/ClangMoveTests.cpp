83953233920a51047263fa2735be21864516687b
Simon Marchi <simon.marchi@ericsson.com>
2018-07-05 14:53:17 +0000

    [clang-move] ClangMoveTests: Remove dots in output paths
    Summary:
    Following D48903 ([VirtualFileSystem] InMemoryFileSystem::status: Return
    a Status with the requested name), the paths output by clang-move in the
    FileToReplacements map may contain leading "./".  For example, where we
    would get "foo.h", we'll now get "./foo.h".  This breaks the tests,
    because we are doing exact string lookups in the FileToFileID and
    Results maps (they contain "foo.h", but we search for "./foo.h").
    
    To mitigate this, try to normalize a little bit the paths output by
    clang-move to remove that leading "./".
    
    This patch should be safe to merge before D48903, remove_dots will just
    be a no-op.
    
    Reviewers: ilya-biryukov, hokein
    
    Reviewed By: hokein
    
    Subscribers: ioeric, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D48951
    
    llvm-svn: 336358

--- clang-tools-extra/unittests/clang-move/ClangMoveTests.cpp
+++ clang-tools-extra/unittests/clang-move/ClangMoveTests.cpp
@@ -239,8 +239,10 @@ runClangMoveOnCode(const move::MoveDefinitionSpec &Spec,
   // The Key is file name, value is the new code after moving the class.
   std::map<std::string, std::string> Results;
   for (const auto &It : FileToReplacements) {
-    StringRef FilePath = It.first;
-    Results[FilePath] = Context.getRewrittenText(FileToFileID[FilePath]);
+    // The path may come out as "./foo.h", normalize to "foo.h".
+    SmallString<32> FilePath (It.first);
+    llvm::sys::path::remove_dots(FilePath);
+    Results[FilePath.str().str()] = Context.getRewrittenText(FileToFileID[FilePath]);
   }
   return Results;
 }
