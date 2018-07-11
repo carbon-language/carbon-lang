ecfbe665a703e25e32fd7438a1def15675fa1fa7
George Karpenkov <ekarpenkov@apple.com>
2018-07-11 02:01:18 +0000

    [analyzer] Fix bots by changing the analyzer-config tests.
    To be investigated.
    
    llvm-svn: 336756

--- clang/test/Analysis/analyzer-config.cpp
+++ clang/test/Analysis/analyzer-config.cpp
@@ -40,7 +40,6 @@ public:
 // CHECK-NEXT: inline-lambdas = true
 // CHECK-NEXT: ipa = dynamic-bifurcate
 // CHECK-NEXT: ipa-always-inline-size = 3
-// CHECK-NEXT: leak-diagnostics-reference-allocation = false
 // CHECK-NEXT: max-inlinable-size = 100
 // CHECK-NEXT: max-nodes = 225000
 // CHECK-NEXT: max-times-inline-large = 32
@@ -51,4 +50,4 @@ public:
 // CHECK-NEXT: unroll-loops = false
 // CHECK-NEXT: widen-loops = false
 // CHECK-NEXT: [stats]
-// CHECK-NEXT: num-entries = 31
+// CHECK-NEXT: num-entries = 30
