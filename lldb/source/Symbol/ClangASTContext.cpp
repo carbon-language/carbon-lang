94c4512a8c6a60e544394a9d504625eb26addd0c
Sterling Augustine <saugustine@google.com>
2018-07-13 23:03:15 +0000

    Rollback r337070.
    Someone simultaneously fixed the breakage it was designed to fix.
    
    llvm-svn: 337071

--- lldb/source/Symbol/ClangASTContext.cpp
+++ lldb/source/Symbol/ClangASTContext.cpp
@@ -5105,7 +5105,6 @@ lldb::Encoding ClangASTContext::GetEncoding(lldb::opaque_compiler_type_t type,
         .GetEncoding(count);
   case clang::Type::DependentSizedArray:
   case clang::Type::DependentSizedExtVector:
-  case clang::Type::DependentVector:
   case clang::Type::UnresolvedUsing:
   case clang::Type::Attributed:
   case clang::Type::TemplateTypeParm:
@@ -5273,7 +5272,6 @@ lldb::Format ClangASTContext::GetFormat(lldb::opaque_compiler_type_t type) {
         .GetFormat();
   case clang::Type::DependentSizedArray:
   case clang::Type::DependentSizedExtVector:
-  case clang::Type::DependentVector:
   case clang::Type::UnresolvedUsing:
   case clang::Type::Attributed:
   case clang::Type::TemplateTypeParm:
