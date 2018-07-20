c03b04d53305de01ee28ffe80be17cfaeca24cec
Duncan P. N. Exon Smith <dexonsmith@apple.com>
2018-07-20 00:44:58 +0000

    Reapply "ADT: Shrink size of SmallVector by 8B on 64-bit platforms"
    I'm optimistically reverting commit r337511, effectively reapplying
    r337504 *without* changes.
    
    The failing bots that had `SmallVector` in the backtrace recovered after
    the unrelated commit r337508.  The backtraces looked bogus anyway, with
    `SmallVector::size()` calling (e.g.) `ConstantArray::get()`.
    
    Here's the original commit message:
    
        ADT: Shrink size of SmallVector by 8B on 64-bit platforms
    
        Represent size and capacity directly as unsigned and calculate
        `end()` using `begin() + size()`.
    
        This limits the maximum size/capacity of a vector to UINT32_MAX.
    
        https://reviews.llvm.org/D48518
    
    llvm-svn: 337514

--- llvm/lib/Support/SmallVector.cpp
+++ llvm/lib/Support/SmallVector.cpp
@@ -15,30 +15,33 @@
 using namespace llvm;
 
 // Check that no bytes are wasted.
-static_assert(sizeof(SmallVector<void *, 1>) == sizeof(void *) * 4,
+static_assert(sizeof(SmallVector<void *, 1>) ==
+                  sizeof(unsigned) * 2 + sizeof(void *) * 2,
               "wasted space in SmallVector size 1; missing EBO?");
 
 /// grow_pod - This is an implementation of the grow() method which only works
 /// on POD-like datatypes and is out of line to reduce code duplication.
-void SmallVectorBase::grow_pod(void *FirstEl, size_t MinSizeInBytes,
+void SmallVectorBase::grow_pod(void *FirstEl, size_t MinCapacity,
                                size_t TSize) {
-  size_t CurSizeBytes = size_in_bytes();
-  size_t NewCapacityInBytes = 2 * capacity_in_bytes() + TSize; // Always grow.
-  if (NewCapacityInBytes < MinSizeInBytes)
-    NewCapacityInBytes = MinSizeInBytes;
+  // Ensure we can fit the new capacity in 32 bits.
+  if (MinCapacity > UINT32_MAX)
+    report_bad_alloc_error("SmallVector capacity overflow during allocation");
+
+  size_t NewCapacity = 2 * capacity() + 1; // Always grow.
+  NewCapacity =
+      std::min(std::max(NewCapacity, MinCapacity), size_t(UINT32_MAX));
 
   void *NewElts;
   if (BeginX == FirstEl) {
-    NewElts = safe_malloc(NewCapacityInBytes);
+    NewElts = safe_malloc(NewCapacity * TSize);
 
     // Copy the elements over.  No need to run dtors on PODs.
-    memcpy(NewElts, this->BeginX, CurSizeBytes);
+    memcpy(NewElts, this->BeginX, size() * TSize);
   } else {
     // If this wasn't grown from the inline copy, grow the allocated space.
-    NewElts = safe_realloc(this->BeginX, NewCapacityInBytes);
+    NewElts = safe_realloc(this->BeginX, NewCapacity * TSize);
   }
 
-  this->EndX = (char*)NewElts+CurSizeBytes;
   this->BeginX = NewElts;
-  this->CapacityX = (char*)this->BeginX + NewCapacityInBytes;
+  this->Capacity = NewCapacity;
 }
