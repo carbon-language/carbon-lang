bf443b91815a0235cdee9cc20113d2598c9783ca
Zachary Turner <zturner@google.com>
2018-07-20 16:39:32 +0000

    Add llvm::Any.
    This is analogous to std::any which is only available in C++17.
    
    Differential Revision: https://reviews.llvm.org/D48807
    
    llvm-svn: 337573

--- llvm/include/llvm/ADT/STLExtras.h
+++ llvm/include/llvm/ADT/STLExtras.h
@@ -58,6 +58,18 @@ using ValueOfRange = typename std::remove_reference<decltype(
 } // end namespace detail
 
 //===----------------------------------------------------------------------===//
+//     Extra additions to <type_traits>
+//===----------------------------------------------------------------------===//
+
+template <typename T> struct negation : std::bool_constant<!bool(T::value)> {};
+
+template <typename...> struct conjunction : std::true_type {};
+template <typename B1> struct conjunction<B1> : B1 {};
+template <typename B1, typename... Bn>
+struct conjunction<B1, Bn...>
+    : std::conditional<bool(B1::value), conjunction<Bn...>, B1>::type {};
+
+//===----------------------------------------------------------------------===//
 //     Extra additions to <functional>
 //===----------------------------------------------------------------------===//
 
