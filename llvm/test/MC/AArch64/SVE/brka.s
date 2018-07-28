3878bf83ddf7d83b446ab79dc30158fc50465c54
Sander de Smalen <sander.desmalen@arm.com>
2018-07-28 14:04:52 +0000

    [AArch64][SVE] Asm: Data-dependent loop predicate partitioning instructions.
    This patch adds support for instructions that partition a predicate
    based on data-dependent termination conditions in a loop.
    
      BRKA      Break after the first true condition
      BRKAS     Break after the first true condition, setting condition flags
      BRKB      Break before the first true condition
      BRKBS     Break before the first true condition, setting condition flags
    
      BRKPA     Break after the first true condition, propagating from the 
                previous partition
      BRKPAS    Break after the first true condition, propagating from the 
                previous partition, setting condition flags
      BRKPB     Break before the first true condition, propagating from the 
                previous partition
      BRKPBS    Break before the first true condition, propagating from the 
                previous partition, setting condition flags
    
      BRKN      Propagate break to next partition
      BKRNS     Propagate break to next partition, setting condition flags
    
    llvm-svn: 338196

---
+++ llvm/test/MC/AArch64/SVE/brka.s
@@ -0,0 +1,20 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+brka  p0.b, p15/m, p15.b
+// CHECK-INST: brka	p0.b, p15/m, p15.b
+// CHECK-ENCODING: [0xf0,0x7d,0x10,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: f0 7d 10 25 <unknown>
+
+brka  p0.b, p15/z, p15.b
+// CHECK-INST: brka	p0.b, p15/z, p15.b
+// CHECK-ENCODING: [0xe0,0x7d,0x10,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: e0 7d 10 25 <unknown>
---
+++ llvm/test/MC/AArch64/SVE/brkn.s
@@ -0,0 +1,20 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+brkn  p0.b, p15/z, p1.b, p0.b
+// CHECK-INST: brkn	p0.b, p15/z, p1.b, p0.b
+// CHECK-ENCODING: [0x20,0x7c,0x18,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: 20 7c 18 25 <unknown>
+
+brkn  p15.b, p15/z, p15.b, p15.b
+// CHECK-INST: brkn	p15.b, p15/z, p15.b, p15.b
+// CHECK-ENCODING: [0xef,0x7d,0x18,0x25]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ef 7d 18 25 <unknown>
