959fe03933e5f841f77219d526f88055c081d9a0
Wolfgang Pieb <Wolfgang.Pieb@sony.com>
2018-07-31 19:37:29 +0000

    [DWARF] Change a test to ensure the creation of a __debug_ranges section.
    Reviewer: aprantl
    llvm-svn: 338424

--- debuginfo-tests/apple-accel.cpp
+++ debuginfo-tests/apple-accel.cpp
@@ -8,6 +8,9 @@
 // RUN: %clang %s %target_itanium_abi_host_triple -gdwarf-4 -O0 -c -g -o %t-ex
 // RUN: llvm-objdump -section-headers %t-ex | FileCheck %s
 
+// A function in a different section forces the compiler to create the 
+// __debug_ranges section.
+__attribute__((section("1,__text_foo"))) void foo() {}
 int main (int argc, char const *argv[]) { return argc; }
 
 // CHECK: __debug_str
