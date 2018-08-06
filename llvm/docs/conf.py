1230d22c5996043ea28092f130a8e2af33e5252b
Chandler Carruth <chandlerc@gmail.com>
2018-08-06 00:38:31 +0000

    [docs] Reinstate r337730 - Add support for Markdown documentation in Sphinx.
    We think the bot is updated now, so trying this again. I'm landing it
    (with permission) as Michael is at a con at the moment.
    
    Actual patch largely by Michael Spencer.
    
    Differential Revision: https://reviews.llvm.org/D44910
    
    llvm-svn: 338977

--- llvm/docs/conf.py
+++ llvm/docs/conf.py
@@ -31,7 +31,9 @@ extensions = ['sphinx.ext.intersphinx', 'sphinx.ext.todo']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+source_suffix = ['.rst', '.md']
+
+source_parsers = {'.md': 'recommonmark.parser.CommonMarkParser'}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
