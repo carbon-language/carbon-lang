85be391cb904da02d64b481b0608a3d7c32a1a01
Stella Stamenova <stilis@microsoft.com>
2018-08-06 22:37:45 +0000

    [lit, python] Always add quotes around the python path in lit
    Summary:
    The issue with the python path is that the path to python on Windows can contain spaces. To make the tests always work, the path to python needs to be surrounded by quotes.
    
    This is a companion change to: https://reviews.llvm.org/D50206
    
    Reviewers: asmith, zturner
    
    Subscribers: cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D50281
    
    llvm-svn: 339074

--- clang/test/Tooling/clang-diff-json.cpp
+++ clang/test/Tooling/clang-diff-json.cpp
@@ -1,10 +1,10 @@
 // RUN: clang-diff -ast-dump-json %s -- \
-// RUN: | '%python' -c 'import json, sys; json.dump(json.loads(sys.stdin.read()), sys.stdout, sort_keys=True, indent=2)' \
+// RUN: | %python -c 'import json, sys; json.dump(json.loads(sys.stdin.read()), sys.stdout, sort_keys=True, indent=2)' \
 // RUN: | FileCheck %s
 
-// CHECK: "begin": 301,
+// CHECK: "begin": 311,
 // CHECK: "type": "FieldDecl",
-// CHECK: "end": 321,
+// CHECK: "end": 319,
 // CHECK: "type": "CXXRecordDecl",
 class A {
   int x;
