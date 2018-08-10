abb71f90d5837e1ce4459332740181321f62f4f4
Hans Wennborg <hans@hanshq.net>
2018-08-10 11:40:50 +0000

    clang-cl: accept -fcrash-diagnostics-dir=
    llvm-svn: 339424

--- clang/test/Driver/cl-options.c
+++ clang/test/Driver/cl-options.c
@@ -577,6 +577,7 @@
 // RUN: %clang_cl \
 // RUN:     --driver-mode=cl \
 // RUN:     -fblocks \
+// RUN:     -fcrash-diagnostics-dir=/foo \
 // RUN:     -fno-blocks \
 // RUN:     -fbuiltin \
 // RUN:     -fno-builtin \
