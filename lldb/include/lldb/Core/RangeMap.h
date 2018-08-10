4b2e0b6c324f8b738313c50bf6f49c05fe6e84f8
Raphael Isemann <teemperor@gmail.com>
2018-08-10 21:31:44 +0000

    Remove copy-pasted and unrelated comment [NFC]
    That comment was copied from the
    CombineConsecutiveEntriesWithEqualData() implementation below,
    and doesn't actually describe what's happening in the current
    function.
    
    llvm-svn: 339473

--- lldb/include/lldb/Core/RangeMap.h
+++ lldb/include/lldb/Core/RangeMap.h
@@ -169,8 +169,6 @@ public:
 #ifdef ASSERT_RANGEMAP_ARE_SORTED
   bool IsSorted() const {
     typename Collection::const_iterator pos, end, prev;
-    // First we determine if we can combine any of the Entry objects so we
-    // don't end up allocating and making a new collection for no reason
     for (pos = m_entries.begin(), end = m_entries.end(), prev = end; pos != end;
          prev = pos++) {
       if (prev != end && *pos < *prev)
