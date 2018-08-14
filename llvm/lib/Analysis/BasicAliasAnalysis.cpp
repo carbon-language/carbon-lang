40e7663b1fca58b099c1cb1d5278f368494ea46d
Reid Kleckner <rnk@google.com>
2018-08-14 01:24:35 +0000

    [BasicAA] Don't assume tail calls with byval don't alias allocas
    Summary:
    Calls marked 'tail' cannot read or write allocas from the current frame
    because the current frame might be destroyed by the time they run.
    However, a tail call may use an alloca with byval. Calling with byval
    copies the contents of the alloca into argument registers or stack
    slots, so there is no lifetime issue. Tail calls never modify allocas,
    so we can return just ModRefInfo::Ref.
    
    Fixes PR38466, a longstanding bug.
    
    Reviewers: hfinkel, nlewycky, gbiv, george.burgess.iv
    
    Subscribers: hiraditya, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D50679
    
    llvm-svn: 339636

--- llvm/lib/Analysis/BasicAliasAnalysis.cpp
+++ llvm/lib/Analysis/BasicAliasAnalysis.cpp
@@ -801,14 +801,15 @@ ModRefInfo BasicAAResult::getModRefInfo(ImmutableCallSite CS,
 
   const Value *Object = GetUnderlyingObject(Loc.Ptr, DL);
 
-  // If this is a tail call and Loc.Ptr points to a stack location, we know that
-  // the tail call cannot access or modify the local stack.
-  // We cannot exclude byval arguments here; these belong to the caller of
-  // the current function not to the current function, and a tail callee
-  // may reference them.
+  // Calls marked 'tail' cannot read or write allocas from the current frame
+  // because the current frame might be destroyed by the time they run. However,
+  // a tail call may use an alloca with byval. Calling with byval copies the
+  // contents of the alloca into argument registers or stack slots, so there is
+  // no lifetime issue.
   if (isa<AllocaInst>(Object))
     if (const CallInst *CI = dyn_cast<CallInst>(CS.getInstruction()))
-      if (CI->isTailCall())
+      if (CI->isTailCall() &&
+          !CI->getAttributes().hasAttrSomewhere(Attribute::ByVal))
         return ModRefInfo::NoModRef;
 
   // If the pointer is to a locally allocated object that does not escape,
