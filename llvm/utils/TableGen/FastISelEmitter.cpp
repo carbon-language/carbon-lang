77af5fdb185e12c1e301a5fd28f99def0679f710
Simon Pilgrim <llvm-dev@redking.me.uk>
2018-08-14 11:17:38 +0000

    [TableGen] Pass string/vector types by const reference (PR37666). NFCI
    llvm-svn: 339670

--- llvm/utils/TableGen/FastISelEmitter.cpp
+++ llvm/utils/TableGen/FastISelEmitter.cpp
@@ -39,11 +39,12 @@ struct InstructionMemo {
   std::vector<std::string> PhysRegs;
   std::string PredicateCheck;
 
-  InstructionMemo(std::string Name, const CodeGenRegisterClass *RC,
-                  std::string SubRegNo, std::vector<std::string> PhysRegs,
-                  std::string PredicateCheck)
-    : Name(Name), RC(RC), SubRegNo(SubRegNo), PhysRegs(PhysRegs),
-      PredicateCheck(PredicateCheck) {}
+  InstructionMemo(const std::string &Name, const CodeGenRegisterClass *RC,
+                  const std::string &SubRegNo,
+                  const std::vector<std::string> &PhysRegs,
+                  const std::string &PredicateCheck)
+      : Name(Name), RC(RC), SubRegNo(SubRegNo), PhysRegs(PhysRegs),
+        PredicateCheck(PredicateCheck) {}
 
   // Make sure we do not copy InstructionMemo.
   InstructionMemo(const InstructionMemo &Other) = delete;
