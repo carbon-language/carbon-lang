feb0427c91ad9ea077d59648b67dcd549609d642
Louis Dionne <ldionne@apple.com>
2018-08-15 00:18:01 +0000

    [libc++] Disable failing C11 feature tests for <cfloat> and <float.h>
    Summary:
    Those tests are breaking the test bots. A Bugzilla has been filed to
    make sure those tests are re-enabled: https://bugs.llvm.org/show_bug.cgi?id=38572
    
    Reviewers: mclow.lists, EricWF
    
    Subscribers: krytarowski, christof, dexonsmith, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D50748
    
    llvm-svn: 339742

--- libcxx/test/std/language.support/support.limits/c.limits/cfloat.pass.cpp
+++ libcxx/test/std/language.support/support.limits/c.limits/cfloat.pass.cpp
@@ -25,7 +25,7 @@
 #error FLT_RADIX not defined
 #endif
 
-#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES)
+#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES) && 0
 #ifndef FLT_HAS_SUBNORM
 #error FLT_HAS_SUBNORM not defined
 #endif
@@ -55,7 +55,7 @@
 #error DECIMAL_DIG not defined
 #endif
 
-#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES)
+#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES) && 0
 #ifndef FLT_DECIMAL_DIG
 #error FLT_DECIMAL_DIG not defined
 #endif
@@ -165,7 +165,7 @@
 #error LDBL_MIN not defined
 #endif
 
-#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES)
+#if TEST_STD_VER > 14 && defined(TEST_HAS_C11_FEATURES) && 0
 #ifndef FLT_TRUE_MIN
 #error FLT_TRUE_MIN not defined
 #endif
