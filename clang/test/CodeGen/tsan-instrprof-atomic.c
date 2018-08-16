61cdea81e766ba4c1b221d0254b2132a7653a224
Vedant Kumar <vsk@apple.com>
2018-08-16 23:19:50 +0000

    Relax a CHECK line to allow for dso_local
    Fixes a bot failure:
    http://lab.llvm.org:8011/builders/llvm-clang-x86_64-expensive-checks-win/builds/11806
    
    llvm-svn: 339964

--- clang/test/CodeGen/tsan-instrprof-atomic.c
+++ clang/test/CodeGen/tsan-instrprof-atomic.c
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 %s -emit-llvm -fprofile-instrument=clang -fsanitize=thread -o - | FileCheck %s
 
-// CHECK: define void @foo
+// CHECK: define {{.*}}@foo
 // CHECK-NOT: load {{.*}}foo
 // CHECK: ret void
 void foo() {}
