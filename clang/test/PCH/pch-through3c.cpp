3820f582eb9c822ad26c90ec9450e4f70d93c1d3
Mike Rice <michael.p.rice@intel.com>
2018-08-17 21:16:21 +0000

    test commit: add a comment
    llvm-svn: 340082

--- clang/test/PCH/pch-through3c.cpp
+++ clang/test/PCH/pch-through3c.cpp
@@ -2,6 +2,7 @@
 // RUN:   -include Inputs/pch-through3c.h \
 // RUN:   -pch-through-header=Inputs/pch-through3c.h -o %t.3c %s
 
+// Checks that no warnings appear for this successful use.
 // RUN: %clang_cc1 -verify -I %S -include-pch %t.3c \
 // RUN:   -include Inputs/pch-through3c.h \
 // RUN:   -pch-through-header=Inputs/pch-through3c.h \
