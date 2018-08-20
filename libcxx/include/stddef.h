22c9256a49c6a15a44c06895985d51e87d24130d
Kamil Rytarowski <n54@gmx.com>
2018-08-20 22:29:20 +0000

    Teach libc++ to use native NetBSD's max_align_t
    Summary:
    The NetBSD headers ship with max_align_t, that is not
    compatible with the fallback version in libc++.
    
    There is no defined a compiler specific symbol in the headers like:
     - __CLANG_MAX_ALIGN_T_DEFINED
     - _GCC_MAX_ALIGN_T
     - __DEFINED_max_align_t
    
    Sponsored by <The NetBSD Foundation>
    
    Reviewers: chandlerc, dlj, EricWF, joerg
    
    Reviewed By: joerg
    
    Subscribers: bsdjhb, llvm-commits, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D47814
    
    llvm-svn: 340224

--- libcxx/include/stddef.h
+++ libcxx/include/stddef.h
@@ -54,7 +54,7 @@ using std::nullptr_t;
 
 // Re-use the compiler's <stddef.h> max_align_t where possible.
 #if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && \
-    !defined(__DEFINED_max_align_t)
+    !defined(__DEFINED_max_align_t) && !defined(__NetBSD__)
 typedef long double max_align_t;
 #endif
 
