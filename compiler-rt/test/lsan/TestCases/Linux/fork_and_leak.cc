dd4c14e4dd7b5fd6faba00d75f4cf735d37ab9e2
Vitaly Buka <vitalybuka@google.com>
2018-08-27 19:15:05 +0000

    [lsan] Check that leak sanitizer works in the forked process
    Regression test for PR38698
    
    llvm-svn: 340769

---
+++ compiler-rt/test/lsan/TestCases/Linux/fork_and_leak.cc
@@ -0,0 +1,23 @@
+// Test that leaks detected after forking without exec().
+// RUN: %clangxx_lsan %s -o %t && not %run %t 2>&1 | FileCheck %s
+
+#include <assert.h>
+#include <stdlib.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+int main() {
+  pid_t pid = fork();
+  assert(pid >= 0);
+  if (pid > 0) {
+    int status = 0;
+    waitpid(pid, &status, 0);
+    assert(WIFEXITED(status));
+    return WEXITSTATUS(status);
+  } else {
+    malloc(1337);
+    // CHECK: LeakSanitizer: detected memory leaks
+  }
+  return 0;
+}
+
