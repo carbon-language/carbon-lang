d0ab67f7d02023482c5dc06fcc14ea8488929684
Marshall Clow <mclow.lists@gmail.com>
2018-08-29 23:02:15 +0000

    Last week, someone noted that a couple of the time_point member functions were not constexpr. I looked, and they were right. They were made constexpr in p0505, so I looked at all the other bits in that paper to make sure that I didn't miss anything else. There were a couple methods in the synopsis that should have been marked constexpr, but the code was correct.
    llvm-svn: 340992

--- libcxx/test/std/utilities/time/time.point/time.point.arithmetic/op_+=.pass.cpp
+++ libcxx/test/std/utilities/time/time.point/time.point.arithmetic/op_+=.pass.cpp
@@ -12,15 +12,35 @@
 // time_point
 
 // time_point& operator+=(const duration& d);
+// constexpr in c++17
 
 #include <chrono>
 #include <cassert>
 
+#include "test_macros.h"
+
+#if TEST_STD_VER > 14
+constexpr bool constexpr_test()
+{
+    typedef std::chrono::system_clock Clock;
+    typedef std::chrono::milliseconds Duration;
+    std::chrono::time_point<Clock, Duration> t(Duration(5));
+    t += Duration(4);
+    return t.time_since_epoch() == Duration(9);
+}
+#endif
+
 int main()
 {
+    {
     typedef std::chrono::system_clock Clock;
     typedef std::chrono::milliseconds Duration;
     std::chrono::time_point<Clock, Duration> t(Duration(3));
     t += Duration(2);
     assert(t.time_since_epoch() == Duration(5));
+    }
+    
+#if TEST_STD_VER > 14
+    static_assert(constexpr_test(), "");
+#endif
 }
