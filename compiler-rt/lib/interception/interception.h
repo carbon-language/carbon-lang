d1c1e036ae0489e0e321e52425f0ed222ad42c99
Kamil Rytarowski <n54@gmx.com>
2018-08-29 09:11:17 +0000

    Introduce new type for inteceptors UINTMAX_T
    Summary:
    The UINTMAX_T type will be used in new interceptors.
    
    While there, correct the type of strtoumax(3) from INTMAX_T to UINTMAX_T.
    
    Original patch from Yang Zheng.
    
    Reviewers: vitalybuka, kcc, joerg
    
    Reviewed By: vitalybuka
    
    Subscribers: kubamracek, llvm-commits, tomsun.0.7, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D51106
    
    llvm-svn: 340907

--- compiler-rt/lib/interception/interception.h
+++ compiler-rt/lib/interception/interception.h
@@ -29,6 +29,7 @@ typedef __sanitizer::uptr    SIZE_T;
 typedef __sanitizer::sptr    SSIZE_T;
 typedef __sanitizer::sptr    PTRDIFF_T;
 typedef __sanitizer::s64     INTMAX_T;
+typedef __sanitizer::u64     UINTMAX_T;
 typedef __sanitizer::OFF_T   OFF_T;
 typedef __sanitizer::OFF64_T OFF64_T;
 
