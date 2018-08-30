d859668c762b8744a075dd086cb45aa4befb81b0
Dean Michael Berris <dberris@google.com>
2018-08-30 08:15:42 +0000

    [XRay] Move out template and use perfect forwarding
    Follow up to D51210.
    
    llvm-svn: 341032

--- llvm/lib/XRay/FDRTraceWriter.cpp
+++ llvm/lib/XRay/FDRTraceWriter.cpp
@@ -65,11 +65,12 @@ template <size_t Index> struct IndexedMemcpy {
 };
 
 template <uint8_t Kind, class... Data>
-Error writeMetadata(raw_ostream &OS, Data... Ds) {
+Error writeMetadata(raw_ostream &OS, Data&&... Ds) {
   MetadataBlob B;
   B.Type = 1;
   B.RecordKind = Kind;
-  IndexedMemcpy<0>::Copy(B.Data, std::make_tuple(Ds...));
+  auto T = std::make_tuple(std::forward<Data>(std::move(Ds))...);
+  IndexedMemcpy<0>::Copy(B.Data, T);
   OS.write(reinterpret_cast<const char *>(&B), sizeof(MetadataBlob));
   return Error::success();
 }
