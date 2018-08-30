d9b6b81d08fd8588566499d475f4d4499cd74511
Andrew Kaylor <andrew.kaylor@intel.com>
2018-08-30 18:37:18 +0000

    Reverting r340807.
    This patch restores the old behavior of getAllocationDataForFunction in MemoryBuiltins.cpp.
    
    llvm-svn: 341091

--- llvm/lib/Analysis/MemoryBuiltins.cpp
+++ llvm/lib/Analysis/MemoryBuiltins.cpp
@@ -150,7 +150,7 @@ getAllocationDataForFunction(const Function *Callee, AllocType AllocTy,
     return None;
 
   const AllocFnsTy *FnData = &Iter->second;
-  if ((FnData->AllocTy & AllocTy) == 0)
+  if ((FnData->AllocTy & AllocTy) != FnData->AllocTy)
     return None;
 
   // Check function prototype.
