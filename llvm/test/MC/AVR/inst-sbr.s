97daa142f441b806cd1a0aaaee0d404ae54298c3
Dylan McKay <me@dylanmckay.io>
2018-09-01 12:22:54 +0000

    [AVR] Redefine the 'SBR' instruction as an alias
    This fixes a TableGen warning about duplicate bit patterns.
    
    SBR
    ===
    
    This is an alias of 'ORI Rd, K'.
    
    llvm-svn: 341277

--- llvm/test/MC/AVR/inst-sbr.s
+++ llvm/test/MC/AVR/inst-sbr.s
@@ -9,10 +9,10 @@ foo:
 
   sbr r19, _start
 
-; CHECK: sbr r17, 208                  ; encoding: [0x10,0x6d]
-; CHECK: sbr r24, 190                  ; encoding: [0x8e,0x6b]
-; CHECK: sbr r20, 173                  ; encoding: [0x4d,0x6a]
-; CHECK: sbr r31, 0                    ; encoding: [0xf0,0x60]
+; CHECK: ori r17, 208                  ; encoding: [0x10,0x6d]
+; CHECK: ori r24, 190                  ; encoding: [0x8e,0x6b]
+; CHECK: ori r20, 173                  ; encoding: [0x4d,0x6a]
+; CHECK: ori r31, 0                    ; encoding: [0xf0,0x60]
 
-; CHECK: sbr     r19, _start           ; encoding: [0x30'A',0x60]
+; CHECK: ori     r19, _start           ; encoding: [0x30'A',0x60]
 ; CHECK:                               ;   fixup A - offset: 0, value: _start, kind: fixup_ldi
