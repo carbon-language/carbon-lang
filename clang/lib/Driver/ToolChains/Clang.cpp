6d924ad2bccfc463ff20e2e255f515883b7674ed
David Chisnall <csdavec@swan.ac.uk>
2018-09-04 09:23:18 +0000

    Disable the GNUstep v2 ABI on Windows.
    The code remains so that we can potentially reenable it in a point
    release, but the driver will reject it.  Several issues were raised
    during testing that made it clear that this was not quite ready for
    general consumption.
    
    Approved by: Hans Wennborg
    
    llvm-svn: 341350

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -4949,8 +4949,7 @@ ObjCRuntime Clang::AddObjCRuntimeArgs(const ArgList &args,
     }
     if ((runtime.getKind() == ObjCRuntime::GNUstep) &&
         (runtime.getVersion() >= VersionTuple(2, 0)))
-      if (!getToolChain().getTriple().isOSBinFormatELF() &&
-          !getToolChain().getTriple().isOSBinFormatCOFF()) {
+      if (!getToolChain().getTriple().isOSBinFormatELF()) {
         getToolChain().getDriver().Diag(
             diag::err_drv_gnustep_objc_runtime_incompatible_binary)
           << runtime.getVersion().getMajor();
