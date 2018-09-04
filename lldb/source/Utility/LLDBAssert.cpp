4d46fde6790815c63d5eb1419e3f1fd7b2c83078
David Bolvansky <david.bolvansky@gmail.com>
2018-09-04 17:19:15 +0000

    Terminate debugger if an assert was hit
    Reviewers: JDevlieghere, teemperor, #lldb
    
    Reviewed By: JDevlieghere
    
    Subscribers: clayborg, lemo, lldb-commits
    
    Differential Revision: https://reviews.llvm.org/D51604
    
    llvm-svn: 341387

--- lldb/source/Utility/LLDBAssert.cpp
+++ lldb/source/Utility/LLDBAssert.cpp
@@ -19,14 +19,14 @@ using namespace lldb_private;
 void lldb_private::lldb_assert(bool expression, const char *expr_text,
                                const char *func, const char *file,
                                unsigned int line) {
-  if (expression)
-    ;
-  else {
-    errs() << format("Assertion failed: (%s), function %s, file %s, line %u\n",
-                     expr_text, func, file, line);
-    errs() << "backtrace leading to the failure:\n";
-    llvm::sys::PrintStackTrace(errs());
-    errs() << "please file a bug report against lldb reporting this failure "
-              "log, and as many details as possible\n";
-  }
+  if (LLVM_LIKELY(expression))
+    return;
+
+  errs() << format("Assertion failed: (%s), function %s, file %s, line %u\n",
+                   expr_text, func, file, line);
+  errs() << "backtrace leading to the failure:\n";
+  llvm::sys::PrintStackTrace(errs());
+  errs() << "please file a bug report against lldb reporting this failure "
+            "log, and as many details as possible\n";
+  abort();
 }
