f14e3030e8cd99efef5f520884f289f2dc5de109
Tim Keith <tkeith@nvidia.com>
2018-09-06 13:44:21 -0700

    [flang] Code cleanup
    Rename stmtFuncsToConvert to stmtFuncsToConvert_.
    Make member functions out-of-line.
    
    Original-commit: flang-compiler/f18@3ada89d22f7fb79d7730a7498d89a96dbc9bffab
    Reviewed-on: https://github.com/flang-compiler/f18/pull/177
    Tree-same-pre-rewrite: false

--- flang/lib/semantics/rewrite-parse-tree.cc
+++ flang/lib/semantics/rewrite-parse-tree.cc
@@ -39,52 +39,10 @@ public:
   template<typename T> bool Pre(T &) { return true; }
   template<typename T> void Post(T &) {}
 
-  // Fill in name.symbol if there is a corresponding symbol
-  void Post(parser::Name &name) {
-    const auto it{symbols_.find(name.source.begin())};
-    if (it != symbols_.end()) {
-      name.symbol = it->second;
-    } else if (errorOnUnresolvedName_) {
-      messages_.Say(name.source, "Internal: no symbol found for '%s'"_err_en_US,
-          name.ToString().c_str());
-    }
-  }
-
-  using stmtFuncType =
-      parser::Statement<common::Indirection<parser::StmtFunctionStmt>>;
-
-  // Find mis-parsed statement functions and move to stmtFuncsToConvert list.
-  void Post(parser::SpecificationPart &x) {
-    auto &list{std::get<std::list<parser::DeclarationConstruct>>(x.t)};
-    for (auto it{list.begin()}; it != list.end();) {
-      if (auto stmt{std::get_if<stmtFuncType>(&it->u)}) {
-        Symbol *symbol{std::get<parser::Name>(stmt->statement->t).symbol};
-        if (symbol && symbol->has<ObjectEntityDetails>()) {
-          // not a stmt func: remove it here and add to ones to convert
-          stmtFuncsToConvert.push_back(std::move(*stmt));
-          it = list.erase(it);
-          continue;
-        }
-      }
-      ++it;
-    }
-  }
-
-  // Insert converted assignments at start of ExecutionPart.
-  bool Pre(parser::ExecutionPart &x) {
-    auto origFirst{x.v.begin()};  // insert each elem before origFirst
-    for (stmtFuncType &sf : stmtFuncsToConvert) {
-      auto &&stmt = sf.statement->ConvertToAssignment();
-      stmt.source = sf.source;
-      x.v.insert(origFirst,
-          parser::ExecutionPartConstruct{parser::ExecutableConstruct{stmt}});
-    }
-    stmtFuncsToConvert.clear();
-    return true;
-  }
-
+  void Post(parser::Name &);
+  void Post(parser::SpecificationPart &);
+  bool Pre(parser::ExecutionPart &);
   void Post(parser::Variable &x) { ConvertFunctionRef(x); }
-
   void Post(parser::Expr &x) { ConvertFunctionRef(x); }
 
   // Don't bother resolving names in end statements.
@@ -106,9 +64,11 @@ public:
   bool Pre(parser::EndWhereStmt &) { return false; }
 
 private:
+  using stmtFuncType =
+      parser::Statement<common::Indirection<parser::StmtFunctionStmt>>;
   bool errorOnUnresolvedName_{true};
   const symbolMap &symbols_;
-  std::list<stmtFuncType> stmtFuncsToConvert;
+  std::list<stmtFuncType> stmtFuncsToConvert_;
   parser::Messages messages_;
 
   // For T = Variable or Expr, if x has a function reference that really
@@ -120,14 +80,55 @@ private:
     if (!funcRef) {
       return;
     }
-    parser::Name *name = std::get_if<parser::Name>(
-        &std::get<parser::ProcedureDesignator>((*funcRef)->v.t).u);
+    parser::Name *name{std::get_if<parser::Name>(
+        &std::get<parser::ProcedureDesignator>((*funcRef)->v.t).u)};
     if (!name || !name->symbol || !name->symbol->has<ObjectEntityDetails>()) {
       return;
     }
     x.u = common::Indirection{(*funcRef)->ConvertToArrayElementRef()};
   }
 };
+
+// Fill in name.symbol if there is a corresponding symbol
+void RewriteMutator::Post(parser::Name &name) {
+  const auto it{symbols_.find(name.source.begin())};
+  if (it != symbols_.end()) {
+    name.symbol = it->second;
+  } else if (errorOnUnresolvedName_) {
+    messages_.Say(name.source, "Internal: no symbol found for '%s'"_err_en_US,
+        name.ToString().c_str());
+  }
+}
+
+// Find mis-parsed statement functions and move to stmtFuncsToConvert_ list.
+void RewriteMutator::Post(parser::SpecificationPart &x) {
+  auto &list{std::get<std::list<parser::DeclarationConstruct>>(x.t)};
+  for (auto it{list.begin()}; it != list.end();) {
+    if (auto stmt{std::get_if<stmtFuncType>(&it->u)}) {
+      Symbol *symbol{std::get<parser::Name>(stmt->statement->t).symbol};
+      if (symbol && symbol->has<ObjectEntityDetails>()) {
+        // not a stmt func: remove it here and add to ones to convert
+        stmtFuncsToConvert_.push_back(std::move(*stmt));
+        it = list.erase(it);
+        continue;
+      }
+    }
+    ++it;
+  }
+}
+
+// Insert converted assignments at start of ExecutionPart.
+bool RewriteMutator::Pre(parser::ExecutionPart &x) {
+  auto origFirst{x.v.begin()};  // insert each elem before origFirst
+  for (stmtFuncType &sf : stmtFuncsToConvert_) {
+    auto &&stmt = sf.statement->ConvertToAssignment();
+    stmt.source = sf.source;
+    x.v.insert(origFirst,
+        parser::ExecutionPartConstruct{parser::ExecutableConstruct{stmt}});
+  }
+  stmtFuncsToConvert_.clear();
+  return true;
+}
 
 static void CollectSymbol(Symbol &symbol, symbolMap &symbols) {
   for (const auto &name : symbol.occurrences()) {
