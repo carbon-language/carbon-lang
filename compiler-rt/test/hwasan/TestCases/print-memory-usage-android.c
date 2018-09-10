7e6c32aa4519b21098807ae4f0255ec0ac4ee0ef
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2018-09-10 22:22:02 +0000

    [hwasan] Re-enable print-memory-usage-android test.
    The problem was not in a non-rooted device, but in tagged local
    variable address passed to a system call, see comments in the code.
    
    llvm-svn: 341875

--- compiler-rt/test/hwasan/TestCases/print-memory-usage-android.c
+++ compiler-rt/test/hwasan/TestCases/print-memory-usage-android.c
@@ -2,14 +2,17 @@
 // RUN: %clang_hwasan %s -o %t && %env_hwasan_opts=export_memory_stats=1 %run %t 2>&1 | FileCheck %s
 // REQUIRES: android
 
-// This test requires a rooted device.
-// UNSUPPORTED: android
-
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 
+// The function needs to be unsanitized in order for &cmd to be untagged. This
+// address is passed to system() and then to execve() syscall. The tests need to
+// run on unpatched linux kernel, which at this time does not accept tagged
+// pointers in system call arguments (but there is hope: see
+// https://lore.kernel.org/patchwork/cover/979328).
+__attribute__((no_sanitize("hwaddress")))
 int main() {
   char cmd[1024];
   snprintf(cmd, sizeof(cmd), "cat /proc/%d/maps", getpid());
