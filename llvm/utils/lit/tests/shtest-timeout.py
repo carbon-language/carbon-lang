4f780ea1266c61a736bcdad2489c3769cf882591
Stella Stamenova <stilis@microsoft.com>
2018-09-10 20:24:05 +0000

    [lit] Disable shtest-timeout on Windows
    Summary: This is the only test that is still failing on Windows - or rather, it is expected to fail on the bots, but passes on the new bot that we're preparing causing a failure, so I'm going to disable it. Since the test has rarely, if ever, passed on the bots, this should have the same effect and it will unblock the creation of the new bot.
    
    Reviewers: asmith, delcypher, zturner
    
    Subscribers: stella.stamenova, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D51871
    
    llvm-svn: 341856

--- llvm/utils/lit/tests/shtest-timeout.py
+++ llvm/utils/lit/tests/shtest-timeout.py
@@ -1,7 +1,7 @@
 # REQUIRES: python-psutil
 
-# PR33944
-# XFAIL: windows
+# llvm.org/PR33944
+# REQUIRES: nowindows
 
 # FIXME: This test is fragile because it relies on time which can
 # be affected by system performance. In particular we are currently
