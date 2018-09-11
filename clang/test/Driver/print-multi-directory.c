28ea6775c7d52721ba03b4ab0ce757e6a1110595
Douglas Yung <douglas.yung@sony.com>
2018-09-11 06:48:45 +0000

    Make test more robust in case the expected output appears in clang version string.
    llvm-svn: 341907

--- clang/test/Driver/print-multi-directory.c
+++ clang/test/Driver/print-multi-directory.c
@@ -1,4 +1,4 @@
-// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>&1 \
+// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>/dev/null \
 // RUN:     -target i386-none-linux \
 // RUN:     -B%S/Inputs/multilib_64bit_linux_tree/usr \
 // RUN:     -print-multi-directory \
@@ -8,7 +8,7 @@
 // CHECK-X86-MULTILIBS-NOT:  x32
 // CHECK-X86-MULTILIBS-NOT:  .
 
-// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>&1 \
+// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>/dev/null \
 // RUN:     -target i386-none-linux -m64 \
 // RUN:     -B%S/Inputs/multilib_64bit_linux_tree/usr \
 // RUN:     -print-multi-directory \
@@ -18,7 +18,7 @@
 // CHECK-X86_64-MULTILIBS-NOT:  x32
 // CHECK-X86_64-MULTILIBS-NOT:  32
 
-// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>&1 \
+// RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>/dev/null \
 // RUN:     -target arm-linux-androideabi21 \
 // RUN:     -mthumb \
 // RUN:     -B%S/Inputs/basic_android_ndk_tree \
