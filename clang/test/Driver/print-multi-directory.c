eaf4fd749bbcea2187b23bd4742702f707907858
Christian Bruel <christian.bruel@st.com>
2018-09-12 08:59:17 +0000

    Fix Check test to avoid output string mismatch
    Differential Revision: http://reviews.llvm.org/D51354
    
    llvm-svn: 342031

--- clang/test/Driver/print-multi-directory.c
+++ clang/test/Driver/print-multi-directory.c
@@ -2,21 +2,19 @@
 // RUN:     -target i386-none-linux \
 // RUN:     -B%S/Inputs/multilib_64bit_linux_tree/usr \
 // RUN:     -print-multi-directory \
-// RUN:   | FileCheck --check-prefix=CHECK-X86-MULTILIBS %s
+// RUN:   | FileCheck --match-full-lines --check-prefix=CHECK-X86-MULTILIBS %s
 
 // CHECK-X86-MULTILIBS:      32
-// CHECK-X86-MULTILIBS-NOT:  x32
-// CHECK-X86-MULTILIBS-NOT:  .
+// CHECK-X86-MULTILIBS-NOT:  {{^.+$}}
 
 // RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>/dev/null \
 // RUN:     -target i386-none-linux -m64 \
 // RUN:     -B%S/Inputs/multilib_64bit_linux_tree/usr \
 // RUN:     -print-multi-directory \
-// RUN:   | FileCheck --check-prefix=CHECK-X86_64-MULTILIBS %s
+// RUN:   | FileCheck --match-full-lines --check-prefix=CHECK-X86_64-MULTILIBS %s
 
 // CHECK-X86_64-MULTILIBS:      .
-// CHECK-X86_64-MULTILIBS-NOT:  x32
-// CHECK-X86_64-MULTILIBS-NOT:  32
+// CHECK-X86_64-MULTILIBS-NOT:  {{^.+$}}
 
 // RUN: %clang -no-canonical-prefixes %s -### -o %t.o 2>/dev/null \
 // RUN:     -target arm-linux-androideabi21 \
@@ -24,7 +22,7 @@
 // RUN:     -B%S/Inputs/basic_android_ndk_tree \
 // RUN:     --sysroot=%S/Inputs/basic_android_ndk_tree/sysroot \
 // RUN:     -print-multi-directory \
-// RUN:   | FileCheck  --check-prefix=CHECK-ARM-MULTILIBS %s
+// RUN:   | FileCheck --match-full-lines --check-prefix=CHECK-ARM-MULTILIBS %s
 
 // CHECK-ARM-MULTILIBS:      thumb
-// CHECK-ARM-MULTILIBS-NOT:  .
+// CHECK-ARM-MULTILIBS-NOT:  {{^.+$}}
