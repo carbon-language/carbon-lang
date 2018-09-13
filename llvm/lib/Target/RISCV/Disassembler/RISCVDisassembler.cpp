b0799dda77c6b75526e1415331e2a9656abd6f95
Ana Pazos <apazos@codeaurora.org>
2018-09-13 18:21:19 +0000

    [RISCV] Fix decoding of invalid instruction with C extension enabled.
    Summary:
    The illegal instruction 0x00 0x00 is being wrongly decoded as
    c.addi4spn with 0 immediate.
    
    The invalid instruction 0x01 0x61 is being wrongly decoded as
    c.addi16sp with 0 immediate.
    
    This bug was uncovered by a LLVM MC Disassembler Protocol Buffer Fuzzer
    for the RISC-V assembly language.
    
    Reviewers: asb
    
    Reviewed By: asb
    
    Subscribers: rbar, johnrusso, simoncook, sabuasal, niosHD, kito-cheng, shiva0217, zzheng, edward-jones, mgrang, rogfer01, MartinMosbeck, brucehoult, the_o, rkruppe, PkmX, jocewei, asb
    
    Differential Revision: https://reviews.llvm.org/D51815
    
    llvm-svn: 342159

--- llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
+++ llvm/lib/Target/RISCV/Disassembler/RISCVDisassembler.cpp
@@ -212,6 +212,15 @@ static DecodeStatus decodeUImmOperand(MCInst &Inst, uint64_t Imm,
 }
 
 template <unsigned N>
+static DecodeStatus decodeUImmNonZeroOperand(MCInst &Inst, uint64_t Imm,
+                                             int64_t Address,
+                                             const void *Decoder) {
+  if (Imm == 0)
+    return MCDisassembler::Fail;
+  return decodeUImmOperand<N>(Inst, Imm, Address, Decoder);
+}
+
+template <unsigned N>
 static DecodeStatus decodeSImmOperand(MCInst &Inst, uint64_t Imm,
                                       int64_t Address, const void *Decoder) {
   assert(isUInt<N>(Imm) && "Invalid immediate");
@@ -219,6 +228,15 @@ static DecodeStatus decodeSImmOperand(MCInst &Inst, uint64_t Imm,
   // Sign-extend the number in the bottom N bits of Imm
   Inst.addOperand(MCOperand::createImm(SignExtend64<N>(Imm)));
   return MCDisassembler::Success;
+}
+
+template <unsigned N>
+static DecodeStatus decodeSImmNonZeroOperand(MCInst &Inst, uint64_t Imm,
+                                             int64_t Address,
+                                             const void *Decoder) {
+  if (Imm == 0)
+    return MCDisassembler::Fail;
+  return decodeSImmOperand<N>(Inst, Imm, Address, Decoder);
 }
 
 template <unsigned N>
