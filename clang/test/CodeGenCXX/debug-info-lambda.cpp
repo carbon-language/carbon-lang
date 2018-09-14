29bb7641ad829f7699d8c9ddd540565ee8af4736
Reid Kleckner <rnk@google.com>
2018-09-14 20:50:39 +0000

    Relax assumption about default method calling convention in new test
    llvm-svn: 342281

--- clang/test/CodeGenCXX/debug-info-lambda.cpp
+++ clang/test/CodeGenCXX/debug-info-lambda.cpp
@@ -6,7 +6,7 @@ void lambda_in_func(int &ref) {
   // CHECK: [[ref_slot:%.*]] = getelementptr inbounds %class.anon, %class.anon* {{.*}}, i32 0, i32 0, !dbg [[lambda_decl_loc:![0-9]+]]
   // CHECK-NEXT: %1 = load i32*, i32** %ref.addr, align {{.*}}, !dbg [[capture_init_loc:![0-9]+]]
   // CHECK-NEXT: store i32* %1, i32** %0, align {{.*}}, !dbg [[lambda_decl_loc]]
-  // CHECK-NEXT: call void {{.*}}, !dbg [[lambda_call_loc:![0-9]+]]
+  // CHECK-NEXT: call {{.*}}void {{.*}}, !dbg [[lambda_call_loc:![0-9]+]]
 
   auto helper = [       // CHECK: [[lambda_decl_loc]] = !DILocation(line: [[@LINE]], column: 17
                  &]() { // CHECK: [[capture_init_loc]] = !DILocation(line: [[@LINE]], column: 18
