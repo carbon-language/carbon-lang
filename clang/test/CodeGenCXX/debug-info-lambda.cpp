ce9e2965ecd0c6d9623113486c15dfc45ef4303e
Vedant Kumar <vsk@apple.com>
2018-09-14 00:16:37 +0000

    Relax alignment assumptions in a test after r342194
    Don't hardcode align 8.
    
    Fixes a bot failure:
    http://lab.llvm.org:8011/builders/clang-cmake-armv8-full/builds/6373
    
    llvm-svn: 342196

--- clang/test/CodeGenCXX/debug-info-lambda.cpp
+++ clang/test/CodeGenCXX/debug-info-lambda.cpp
@@ -4,8 +4,8 @@
 // CHECK-LABEL: define{{.*}}lambda_in_func
 void lambda_in_func(int &ref) {
   // CHECK: [[ref_slot:%.*]] = getelementptr inbounds %class.anon, %class.anon* {{.*}}, i32 0, i32 0, !dbg [[lambda_decl_loc:![0-9]+]]
-  // CHECK-NEXT: %1 = load i32*, i32** %ref.addr, align 8, !dbg [[capture_init_loc:![0-9]+]]
-  // CHECK-NEXT: store i32* %1, i32** %0, align 8, !dbg [[lambda_decl_loc]]
+  // CHECK-NEXT: %1 = load i32*, i32** %ref.addr, align {{.*}}, !dbg [[capture_init_loc:![0-9]+]]
+  // CHECK-NEXT: store i32* %1, i32** %0, align {{.*}}, !dbg [[lambda_decl_loc]]
   // CHECK-NEXT: call void {{.*}}, !dbg [[lambda_call_loc:![0-9]+]]
 
   auto helper = [       // CHECK: [[lambda_decl_loc]] = !DILocation(line: [[@LINE]], column: 17
