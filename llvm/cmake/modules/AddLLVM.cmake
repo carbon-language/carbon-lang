7bff00e8419350996a1acdc95c289998c6db6357
Lion Yang <lion@aosc.io>
2018-09-14 19:43:11 +0000

    Test commit access
    Remove trailing spaces
    
    llvm-svn: 342268

--- llvm/cmake/modules/AddLLVM.cmake
+++ llvm/cmake/modules/AddLLVM.cmake
@@ -1112,7 +1112,7 @@ function(add_unittest test_suite test_name)
   # executable must be linked with it in order to provide consistent
   # API for all shared libaries loaded by this executable.
   target_link_libraries(${test_name} PRIVATE gtest_main gtest ${LLVM_PTHREAD_LIB})
-  
+
   add_dependencies(${test_suite} ${test_name})
   get_target_property(test_suite_folder ${test_suite} FOLDER)
   if (NOT ${test_suite_folder} STREQUAL "NOTFOUND")
