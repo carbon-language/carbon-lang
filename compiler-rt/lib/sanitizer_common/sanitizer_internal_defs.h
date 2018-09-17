040ab65c533b868acc2a3bc1b46d5cab96a3930c
Petr Hosek <phosek@chromium.org>
2018-09-17 05:22:26 +0000

    [sanitizer_common] Fuchsia now supports .preinit_array
    Support for .preinit_array has been implemented in Fuchsia's libc,
    add Fuchsia to the list of platforms that support this feature.
    
    Differential Revision: https://reviews.llvm.org/D52155
    
    llvm-svn: 342357

--- compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_internal_defs.h
@@ -105,8 +105,8 @@
 //
 // FIXME: do we have anything like this on Mac?
 #ifndef SANITIZER_CAN_USE_PREINIT_ARRAY
-#if ((SANITIZER_LINUX && !SANITIZER_ANDROID) || SANITIZER_OPENBSD) && \
-    !defined(PIC)
+#if ((SANITIZER_LINUX && !SANITIZER_ANDROID) || SANITIZER_OPENBSD || \
+     SANITIZER_FUCHSIA) && !defined(PIC)
 #define SANITIZER_CAN_USE_PREINIT_ARRAY 1
 // Before Solaris 11.4, .preinit_array is fully supported only with GNU ld.
 // FIXME: Check for those conditions.
