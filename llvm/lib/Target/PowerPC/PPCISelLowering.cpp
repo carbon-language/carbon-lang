488fd4e6255d6216f2a678d9ac6a184f4e449695
Strahinja Petrovic <strahinja.petrovic@rt-rk.com>
2018-09-17 11:03:40 +0000

    [PowerPC] Fix label address calculation for ppc64
    This patch fixes calculating address of label for non-pic ppc64.
    
    Differential Revision: https://reviews.llvm.org/D50965
    
    llvm-svn: 342368

--- llvm/lib/Target/PowerPC/PPCISelLowering.cpp
+++ llvm/lib/Target/PowerPC/PPCISelLowering.cpp
@@ -2705,7 +2705,8 @@ SDValue PPCTargetLowering::LowerBlockAddress(SDValue Op,
 
   // 64-bit SVR4 ABI code is always position-independent.
   // The actual BlockAddress is stored in the TOC.
-  if (Subtarget.isSVR4ABI() && isPositionIndependent()) {
+  if (Subtarget.isSVR4ABI() &&
+      (Subtarget.isPPC64() || isPositionIndependent())) {
     if (Subtarget.isPPC64())
       setUsesTOCBasePtr(DAG);
     SDValue GA = DAG.getTargetBlockAddress(BA, PtrVT, BASDN->getOffset());
