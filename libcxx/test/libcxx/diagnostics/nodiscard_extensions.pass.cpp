2f02863bf2f29a6287a0be86e01a60bef27ddc97
Roman Lebedev <lebedev.ri@gmail.com>
2018-09-22 20:07:02 +0000

    [libcxx] Readjust nodiscard_extensions.pass.cpp test - just disable for ASAN
    In rL342814, i have committed a blind fix to unbreak the asan buildbot,
    but as it was later discussed, the leak is intentional,
    so we can not fix the failure that way.
    
    So this reverts the leak 'fix',
    and simply disables the test in the presence of ASAN.
    
    llvm-svn: 342819

--- libcxx/test/libcxx/diagnostics/nodiscard_extensions.pass.cpp
+++ libcxx/test/libcxx/diagnostics/nodiscard_extensions.pass.cpp
@@ -11,6 +11,9 @@
 // Test that entities declared [[nodiscard]] as at extension by libc++, are
 // only actually declared such when _LIBCPP_ENABLE_NODISCARD is specified.
 
+// This test intentionally leaks memory, so it is unsupported under ASAN.
+// UNSUPPORTED: asan
+
 // All entities to which libc++ applies [[nodiscard]] as an extension should
 // be tested here and in nodiscard_extensions.fail.cpp. They should also
 // be listed in `UsingLibcxx.rst` in the documentation for the extension.
@@ -21,7 +24,6 @@
 
 int main() {
   {
-    const auto p = std::get_temporary_buffer<int>(1);
-    std::return_temporary_buffer(p.first);
+    std::get_temporary_buffer<int>(1); // intentional memory leak.
   }
 }
