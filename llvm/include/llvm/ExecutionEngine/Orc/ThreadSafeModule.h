533863a350d0220daa2ea078823e83898182dfbb
Lang Hames <lhames@gmail.com>
2018-09-26 23:32:53 +0000

    Re-revert r343129.
    Apparently the fixes in r343149 did not cover all the issues. Re-reverting
    while I investigate.
    
    llvm-svn: 343151

--- llvm/include/llvm/ExecutionEngine/Orc/ThreadSafeModule.h
+++ llvm/include/llvm/ExecutionEngine/Orc/ThreadSafeModule.h
@@ -16,7 +16,6 @@
 
 #include "llvm/IR/LLVMContext.h"
 #include "llvm/IR/Module.h"
-#include "llvm/Support/Compiler.h"
 
 #include <functional>
 #include <memory>
@@ -41,7 +40,7 @@ private:
 public:
 
   // RAII based lock for ThreadSafeContext.
-  class LLVM_NODISCARD Lock {
+  class Lock {
   private:
     using UnderlyingLock = std::lock_guard<std::recursive_mutex>;
   public:
@@ -86,26 +85,16 @@ public:
   /// null context.
   ThreadSafeModule() = default;
 
-  ThreadSafeModule(ThreadSafeModule &&Other) = default;
-
-  ThreadSafeModule& operator=(ThreadSafeModule &&Other) {
-    // We have to explicitly define this move operator to copy the fields in
-    // reverse order (i.e. module first) to ensure the dependencies are
-    // protected: The old module that is being overwritten must be destroyed
-    // *before* the context that it depends on.
-    M = std::move(Other.M);
-    TSCtx = std::move(Other.TSCtx);
-    return *this;
-  }
-
   /// Construct a ThreadSafeModule from a unique_ptr<Module> and a
   /// unique_ptr<LLVMContext>. This creates a new ThreadSafeContext from the
   /// given context.
-  ThreadSafeModule(std::unique_ptr<Module> M, std::unique_ptr<LLVMContext> Ctx)
-      : TSCtx(std::move(Ctx)), M(std::move(M)) {}
+  ThreadSafeModule(std::unique_ptr<Module> M,
+                   std::unique_ptr<LLVMContext> Ctx)
+    : M(std::move(M)), TSCtx(std::move(Ctx)) {}
 
-  ThreadSafeModule(std::unique_ptr<Module> M, ThreadSafeContext TSCtx)
-      : TSCtx(std::move(TSCtx)), M(std::move(M)) {}
+  ThreadSafeModule(std::unique_ptr<Module> M,
+                   ThreadSafeContext TSCtx)
+    : M(std::move(M)), TSCtx(std::move(TSCtx)) {}
 
   Module* getModule() { return M.get(); }
 
@@ -120,8 +109,8 @@ public:
   }
 
 private:
-  ThreadSafeContext TSCtx;
   std::unique_ptr<Module> M;
+  ThreadSafeContext TSCtx;
 };
 
 using GVPredicate = std::function<bool(const GlobalValue&)>;
