55321d82bd329bb88799e5a853a256b4a70e4106
Hsiangkai Wang <hsiangkai@gmail.com>
2018-09-26 04:19:23 +0000

    [DebugInfo] Do not generate address info for removed debug labels.
    In some senario, LLVM will remove llvm.dbg.labels in IR. For example,
    when the labels are in unreachable blocks, these labels will not
    be generated in LLVM IR. In the case, these debug labels will have
    address zero as their address. It is not legal address for debugger to
    set breakpoints or query sources. So, the patch inhibits the address info
    (DW_AT_low_pc) of removed labels.
    
    Fix build failed in BuildBot, clang-stage1-cmake-RA-incremental, on macOS.
    
    Differential Revision: https://reviews.llvm.org/D51908
    
    llvm-svn: 343062

--- llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfCompileUnit.cpp
@@ -865,10 +865,9 @@ void DwarfCompileUnit::finishEntityDefinition(const DbgEntity *Entity) {
       llvm_unreachable("DbgEntity must be DbgVariable or DbgLabel.");
   }
 
-  if (Label) {
-    const MCSymbol *Sym = Label->getSymbol();
-    addLabelAddress(*Die, dwarf::DW_AT_low_pc, Sym);
-  }
+  if (Label)
+    if (const auto *Sym = Label->getSymbol())
+      addLabelAddress(*Die, dwarf::DW_AT_low_pc, Sym);
 }
 
 DbgEntity *DwarfCompileUnit::getExistingAbstractEntity(const DINode *Node) {
