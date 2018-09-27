1a7018f6f5bff55c75d925556314c0f3d46ab6c0
Luke Cheeseman <luke.cheeseman@arm.com>
2018-09-27 10:42:14 +0000

    Update CallFrameString API to account for r343114
    - CallFrameString now takes an Arch parameter to account for multiplexing
      overlapping CFI directives
    
    llvm-svn: 343193

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFDefines.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFDefines.cpp
@@ -504,9 +504,9 @@ const char *DW_MACINFO_value_to_name(uint32_t val) {
   return llvmstr.data();
 }
 
-const char *DW_CFA_value_to_name(uint32_t val) {
+const char *DW_CFA_value_to_name(uint32_t val, llvm::Triple::ArchType Arch) {
   static char invalid[100];
-  llvm::StringRef llvmstr = llvm::dwarf::CallFrameString(val);
+  llvm::StringRef llvmstr = llvm::dwarf::CallFrameString(val, Arch);
   if (llvmstr.empty()) {
     snprintf(invalid, sizeof(invalid), "Unknown DW_CFA constant: 0x%x", val);
     return invalid;
