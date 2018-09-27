f6844b307a5369ea934550ccb85b063d73a268ee
Luke Cheeseman <luke.cheeseman@arm.com>
2018-09-27 10:39:20 +0000

    Reapply changes reverted in r343114, lldb patch to follow shortly
    llvm-svn: 343192

--- llvm/tools/llvm-dwarfdump/llvm-dwarfdump.cpp
+++ llvm/tools/llvm-dwarfdump/llvm-dwarfdump.cpp
@@ -393,7 +393,7 @@ bool collectStatsForObjectFile(ObjectFile &Obj, DWARFContext &DICtx,
 
 static bool dumpObjectFile(ObjectFile &Obj, DWARFContext &DICtx, Twine Filename,
                            raw_ostream &OS) {
-  logAllUnhandledErrors(DICtx.loadRegisterInfo(Obj), errs(),
+  logAllUnhandledErrors(DICtx.loadArchitectureInfo(Obj), errs(),
                         Filename.str() + ": ");
   // The UUID dump already contains all the same information.
   if (!(DumpType & DIDT_UUID) || DumpType == DIDT_All)
