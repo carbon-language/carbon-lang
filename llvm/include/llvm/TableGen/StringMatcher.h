66f4e45b35c809ff81b81044260a520e041a7d49
Sven van Haastregt <sven.vanhaastregt@arm.com>
2018-09-28 13:31:55 +0000

    Fix and modernize StringMatcher comment; NFC
    llvm-svn: 343316

--- llvm/include/llvm/TableGen/StringMatcher.h
+++ llvm/include/llvm/TableGen/StringMatcher.h
@@ -23,12 +23,11 @@ namespace llvm {
 
 class raw_ostream;
 
-/// StringMatcher - Given a list of strings and code to execute when they match,
-/// output a simple switch tree to classify the input string.
+/// Given a list of strings and code to execute when they match, output a
+/// simple switch tree to classify the input string.
 ///
-/// If a match is found, the code in Vals[i].second is executed; control must
+/// If a match is found, the code in Matches[i].second is executed; control must
 /// not exit this code fragment.  If nothing matches, execution falls through.
-///
 class StringMatcher {
 public:
   using StringPair = std::pair<std::string, std::string>;
