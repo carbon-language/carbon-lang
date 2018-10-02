eb9ca5ab1864a2a7c86da1b0e84151c5ed277faf
Diogo N. Sampaio <diogo.sampaio@arm.com>
2018-10-02 14:55:48 +0000

    [ARM] Emmit data symbol for constant pool data
    The ARM elf emitter would omit printing data
    symbol when constant data. This patch
    overrides the emitFill method as to enforce that
    the symbol is correctly printed.
    
    Differential revision: https://reviews.llvm.org/D52737
    
    llvm-svn: 343594

--- llvm/lib/Target/ARM/MCTargetDesc/ARMELFStreamer.cpp
+++ llvm/lib/Target/ARM/MCTargetDesc/ARMELFStreamer.cpp
@@ -465,6 +465,11 @@ public:
   void emitPad(int64_t Offset);
   void emitRegSave(const SmallVectorImpl<unsigned> &RegList, bool isVector);
   void emitUnwindRaw(int64_t Offset, const SmallVectorImpl<uint8_t> &Opcodes);
+  void emitFill(const MCExpr &NumBytes, uint64_t FillValue,
+                SMLoc Loc) override {
+    EmitDataMappingSymbol();
+    MCObjectStreamer::emitFill(NumBytes, FillValue, Loc);
+  }
 
   void ChangeSection(MCSection *Section, const MCExpr *Subsection) override {
     LastMappingSymbols[getCurrentSection().first] = std::move(LastEMSInfo);
