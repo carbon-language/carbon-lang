9f6ac4c264f8f9cc9c6f5f0935a01f423b291428
Zachary Turner <zturner@google.com>
2018-10-08 04:34:41 +0000

    Fix a compilation failure on non-MSVC compilers.
    llvm-svn: 343952

--- llvm/lib/DebugInfo/CodeView/SymbolDumper.cpp
+++ llvm/lib/DebugInfo/CodeView/SymbolDumper.cpp
@@ -261,7 +261,7 @@ Error CVSymbolDumperImpl::visitKnownRecord(CVSymbol &CVR,
 
 Error CVSymbolDumperImpl::visitKnownRecord(CVSymbol &CVR,
                                            Compile3Sym &Compile3) {
-  W.printEnum("Language", Compile3.getLanguage(), getSourceLanguageNames());
+  W.printEnum("Language", uint8_t(Compile3.getLanguage()), getSourceLanguageNames());
   W.printFlags("Flags", uint32_t(Compile3.getFlags()),
                getCompileSym3FlagNames());
   W.printEnum("Machine", unsigned(Compile3.Machine), getCPUTypeNames());
