254b01790596bd420143bbdda097b3902d2e7eb2
Eric Liu <ioeric@google.com>
2018-10-09 15:17:16 +0000

    [clang-move] Fix broken json output.
    llvm-svn: 344055

--- clang-tools-extra/clang-move/tool/ClangMoveMain.cpp
+++ clang-tools-extra/clang-move/tool/ClangMoveMain.cpp
@@ -128,7 +128,7 @@ int main(int argc, const char **argv) {
                                  InitialDirectory.str(), Style, DumpDecls};
   move::DeclarationReporter Reporter;
   move::ClangMoveActionFactory Factory(&Context, &Reporter);
-  
+
   int CodeStatus = Tool.run(&Factory);
   if (CodeStatus)
     return CodeStatus;
@@ -138,8 +138,9 @@ int main(int argc, const char **argv) {
     const auto &Declarations = Reporter.getDeclarationList();
     for (auto I = Declarations.begin(), E = Declarations.end(); I != E; ++I) {
       llvm::outs() << "  {\n";
-      llvm::outs() << "    \"DeclarationName\": \"" << I->QualifiedName << "\",\n";
-      llvm::outs() << "    \"DeclarationType\": \"" << I->Kind << "\"\n";
+      llvm::outs() << "    \"DeclarationName\": \"" << I->QualifiedName
+                   << "\",\n";
+      llvm::outs() << "    \"DeclarationType\": \"" << I->Kind << "\",\n";
       llvm::outs() << "    \"Templated\": " << (I->Templated ? "true" : "false")
                    << "\n";
       llvm::outs() << "  }";
