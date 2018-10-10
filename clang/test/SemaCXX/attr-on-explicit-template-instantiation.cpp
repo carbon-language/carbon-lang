8c98eebea15f9657f3ae19c493a60589263996f9
Louis Dionne <ldionne@apple.com>
2018-10-10 17:37:37 +0000

    [clang] Fix failing attribute test on Windows
    The test added in r344146 was failing because the ABI on Windows is
    different, and that test includes ABI-specific details. The test now
    harcodes which ABI to use so we can rely on those details.
    
    llvm-svn: 344159

--- clang/test/SemaCXX/attr-on-explicit-template-instantiation.cpp
+++ clang/test/SemaCXX/attr-on-explicit-template-instantiation.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -emit-llvm %s -o - | FileCheck %s
 
 // PR39118
 // Make sure that attributes are properly applied to explicit template
