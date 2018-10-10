7b2b0185ba6c4b21906310183e92e8b32f43a0d7
Kostya Serebryany <kcc@google.com>
2018-10-10 23:57:38 +0000

    [hwasan] simplify a test
    llvm-svn: 344203

--- compiler-rt/test/hwasan/TestCases/stack-history-length.c
+++ compiler-rt/test/hwasan/TestCases/stack-history-length.c
@@ -1,7 +1,6 @@
-// RUN: %clang_hwasan -O1 -DX=2046 %s -o %t.2046
-// RUN: %clang_hwasan -O1 -DX=2047 %s -o %t.2047
-// RUN: %env_hwasan_opts=stack_history_size=2048 not %run %t.2046 2>&1 | FileCheck %s --check-prefix=YES
-// RUN: %env_hwasan_opts=stack_history_size=2048 not %run %t.2047 2>&1 | FileCheck %s --check-prefix=NO
+// RUN: %clang_hwasan -O1 %s -o %t
+// RUN: %env_hwasan_opts=stack_history_size=2048 not %run %t 2046 2>&1 | FileCheck %s --check-prefix=YES
+// RUN: %env_hwasan_opts=stack_history_size=2048 not %run %t 2047 2>&1 | FileCheck %s --check-prefix=NO
 
 // REQUIRES: stable-runtime
 
@@ -16,7 +15,8 @@ __attribute__((noinline)) void FUNC0() { int x[4]; USE(&x[0]); }
 __attribute__((noinline)) void FUNC() { int x[4]; USE(&x[0]); }
 __attribute__((noinline)) void OOB() { int x[4]; x[four] = 0; USE(&x[0]); }
 
-int main() {
+int main(int argc, char **argv) {
+  int X = argc == 2 ? atoi(argv[1]) : 10;
   // FUNC0 is X+2's element of the ring buffer.
   // If runtime buffer size is less than it, FUNC0 record will be lost.
   FUNC0();
