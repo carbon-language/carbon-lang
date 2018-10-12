730985b1b37a300bb100ed2ae7eaec83efe202ba
Shafik Yaghmour <syaghmour@apple.com>
2018-10-12 19:46:17 +0000

    Changing test names in TestDataFormatterLibcxxVariant.py and TestStdFunctionStepIntoCallable.py to be unique, NFC
    llvm-svn: 344407

--- lldb/packages/Python/lldbsuite/test/functionalities/data-formatter/data-formatter-stl/libcxx/variant/TestDataFormatterLibcxxVariant.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/data-formatter/data-formatter-stl/libcxx/variant/TestDataFormatterLibcxxVariant.py
@@ -12,7 +12,7 @@ from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
-class LibcxxOptionalDataFormatterTestCase(TestBase):
+class LibcxxVariantDataFormatterTestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
--- lldb/packages/Python/lldbsuite/test/lang/cpp/std-function-step-into-callable/TestStdFunctionStepIntoCallable.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/std-function-step-into-callable/TestStdFunctionStepIntoCallable.py
@@ -12,7 +12,7 @@ from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
 
-class LibCxxFunctionTestCase(TestBase):
+class LibCxxFunctionSteppingIntoCallableTestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
