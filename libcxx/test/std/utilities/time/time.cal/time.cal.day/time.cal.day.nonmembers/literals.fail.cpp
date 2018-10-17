1b22150f76875b6ea0ea42243391b0fd952e183c
Louis Dionne <ldionne@apple.com>
2018-10-17 00:34:13 +0000

    [libcxx] Mark chrono literal unit tests as being unsupported on AppleClang 10
    llvm-svn: 344661

--- libcxx/test/std/utilities/time/time.cal/time.cal.day/time.cal.day.nonmembers/literals.fail.cpp
+++ libcxx/test/std/utilities/time/time.cal/time.cal.day/time.cal.day.nonmembers/literals.fail.cpp
@@ -8,7 +8,7 @@
 //===----------------------------------------------------------------------===//
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // UNSUPPORTED: clang-5, clang-6
-// UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8, apple-clang-9
+// UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8, apple-clang-9, apple-clang-10
 
 // <chrono>
 // class day;
