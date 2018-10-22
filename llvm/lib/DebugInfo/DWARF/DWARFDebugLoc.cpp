209232091cdcb6b8e8e61d91170c32166b2ec2c1
George Rimar <grimar@accesssoftek.com>
2018-10-22 12:18:30 +0000

    [llvm-dwarfdump] - Fix win10 build bot failture.
    Bot failed: 
    http://lab.llvm.org:8011/builders/llvm-clang-lld-x86_64-scei-ps4-windows10pro-fast/builds/20877/steps/test/logs/stdio
    
    This was broken after the 
    r344895 "[llvm-dwarfdump] - Add the support of parsing .debug_loclists."
    because of wrong formatting specifiers used.
    
    llvm-svn: 344896

--- llvm/lib/DebugInfo/DWARF/DWARFDebugLoc.cpp
+++ llvm/lib/DebugInfo/DWARF/DWARFDebugLoc.cpp
@@ -227,13 +227,16 @@ void DWARFDebugLoclists::LocationList::dump(raw_ostream &OS, uint64_t BaseAddr,
     case dwarf::DW_LLE_start_length:
       OS << '\n';
       OS.indent(Indent);
-      OS << format("[0x%8.8x, 0x%8.8x): ", E.Value0, E.Value0 + E.Value1);
+      OS << format("[0x%*.*" PRIx64 ", 0x%*.*x): ", AddressSize * 2,
+                   AddressSize * 2, E.Value0, AddressSize * 2, AddressSize * 2,
+                   E.Value0 + E.Value1);
       break;
     case dwarf::DW_LLE_offset_pair:
       OS << '\n';
       OS.indent(Indent);
-      OS << format("[0x%8.8x, 0x%8.8x): ", BaseAddr + E.Value0,
-                   BaseAddr + E.Value1);
+      OS << format("[0x%*.*" PRIx64 ", 0x%*.*x): ", AddressSize * 2,
+                   AddressSize * 2, BaseAddr + E.Value0, AddressSize * 2,
+                   AddressSize * 2, BaseAddr + E.Value1);
       break;
     case dwarf::DW_LLE_base_address:
       BaseAddr = E.Value0;
