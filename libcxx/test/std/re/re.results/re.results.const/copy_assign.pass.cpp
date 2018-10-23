9b435818062dfb0a40a51d8752a5c73623fc7034
Marshall Clow <mclow.lists@gmail.com>
2018-10-23 02:51:26 +0000

    Add tests for match_results copy/move assignment operators. NFC
    llvm-svn: 344998

--- libcxx/test/std/re/re.results/re.results.const/copy_assign.pass.cpp
+++ libcxx/test/std/re/re.results/re.results.const/copy_assign.pass.cpp
@@ -11,9 +11,7 @@
 
 // class match_results<BidirectionalIterator, Allocator>
 
-// match_results(match_results&& m) noexcept;
-//
-//  Additionally, the stored Allocator value is move constructed from m.get_allocator().
+// match_results& operator=(const match_results& m);
 
 #include <regex>
 #include <cassert>
@@ -25,15 +23,16 @@ void
 test(const Allocator& a)
 {
     typedef std::match_results<const CharT*, Allocator> SM;
-    ASSERT_NOEXCEPT(SM(std::declval<SM&&>()));
-
     SM m0(a);
-    assert(m0.get_allocator() == a);
-
-    SM m1(std::move(m0));
-    assert(m1.size() == 0);
-    assert(m1.str() == std::basic_string<CharT>());
-    assert(m1.get_allocator() == a);
+    SM m1;
+    
+    m1 = m0;
+    assert(m1.size()          == m0.size());
+    assert(m1.str()           == m0.str());
+    if (std::allocator_traits<Allocator>::propagate_on_container_copy_assignment::value)
+        assert(m1.get_allocator() == m0.get_allocator());
+    else
+        assert(m1.get_allocator() == Allocator());
 }
 
 int main()
@@ -41,8 +40,11 @@ int main()
     test<char>   (std::allocator<std::sub_match<const char *> >());
     test<wchar_t>(std::allocator<std::sub_match<const wchar_t *> >());
 
+//  test_allocator has POCCA -> false
     test<char>   (test_allocator<std::sub_match<const char*> >(3));
-    assert(test_alloc_base::moved == 1);
     test<wchar_t>(test_allocator<std::sub_match<const wchar_t*> >(3));
-    assert(test_alloc_base::moved == 2);
+
+//  other_allocator has POCCA -> true
+    test<char>   (other_allocator<std::sub_match<const char*> >(3));
+    test<wchar_t>(other_allocator<std::sub_match<const wchar_t*> >(3));
 }
--- libcxx/test/std/re/re.results/re.results.const/move_assign.pass.cpp
+++ libcxx/test/std/re/re.results/re.results.const/move_assign.pass.cpp
@@ -11,9 +11,7 @@
 
 // class match_results<BidirectionalIterator, Allocator>
 
-// match_results(match_results&& m) noexcept;
-//
-//  Additionally, the stored Allocator value is move constructed from m.get_allocator().
+// match_results& operator=(match_results&& m);
 
 #include <regex>
 #include <cassert>
@@ -25,15 +23,16 @@ void
 test(const Allocator& a)
 {
     typedef std::match_results<const CharT*, Allocator> SM;
-    ASSERT_NOEXCEPT(SM(std::declval<SM&&>()));
-
     SM m0(a);
-    assert(m0.get_allocator() == a);
-
-    SM m1(std::move(m0));
-    assert(m1.size() == 0);
-    assert(m1.str() == std::basic_string<CharT>());
-    assert(m1.get_allocator() == a);
+    SM m1;
+    
+    m1 = std::move(m0);
+    assert(m1.size()          == 0);
+    assert(m1.str()           == std::basic_string<CharT>());
+    if (std::allocator_traits<Allocator>::propagate_on_container_move_assignment::value)
+        assert(m1.get_allocator() == a);
+    else
+        assert(m1.get_allocator() == Allocator());
 }
 
 int main()
@@ -41,8 +40,11 @@ int main()
     test<char>   (std::allocator<std::sub_match<const char *> >());
     test<wchar_t>(std::allocator<std::sub_match<const wchar_t *> >());
 
+//  test_allocator has POCMA -> false
     test<char>   (test_allocator<std::sub_match<const char*> >(3));
-    assert(test_alloc_base::moved == 1);
     test<wchar_t>(test_allocator<std::sub_match<const wchar_t*> >(3));
-    assert(test_alloc_base::moved == 2);
+
+//  other_allocator has POCMA -> true
+    test<char>   (other_allocator<std::sub_match<const char*> >(3));
+    test<wchar_t>(other_allocator<std::sub_match<const wchar_t*> >(3));
 }
