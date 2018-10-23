551a9585ce5c8e1e2afa5cbd8dc7a7c4fe5fdb85
Adrian Prantl <aprantl@apple.com>
2018-10-23 19:39:02 +0000

    Fix doxygen comment.
    llvm-svn: 345071

--- clang/include/clang/AST/DeclTemplate.h
+++ clang/include/clang/AST/DeclTemplate.h
@@ -1093,7 +1093,7 @@ public:
   /// template.
   ArrayRef<TemplateArgument> getInjectedTemplateArgs();
 
-  /// Merge \param Prev with our RedeclarableTemplateDecl::Common.
+  /// Merge \p Prev with our RedeclarableTemplateDecl::Common.
   void mergePrevDecl(FunctionTemplateDecl *Prev);
 
   /// Create a function template node.
