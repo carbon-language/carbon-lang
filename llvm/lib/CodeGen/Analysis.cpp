18bfb3a5ec410d4675eb1a7c913dfeb60017df3f
Robert Lougher <rob.lougher@gmail.com>
2018-10-24 17:03:19 +0000

    [CodeGen] skip lifetime end marker in isInTailCallPosition
    A lifetime end intrinsic between a tail call and the return should not
    prevent the call from being tail call optimized.
    
    Differential Revision: https://reviews.llvm.org/D53519
    
    llvm-svn: 345163

--- llvm/lib/CodeGen/Analysis.cpp
+++ llvm/lib/CodeGen/Analysis.cpp
@@ -496,6 +496,10 @@ bool llvm::isInTailCallPosition(ImmutableCallSite CS, const TargetMachine &TM) {
       // Debug info intrinsics do not get in the way of tail call optimization.
       if (isa<DbgInfoIntrinsic>(BBI))
         continue;
+      // A lifetime end intrinsic should not stop tail call optimization.
+      if (const IntrinsicInst *II = dyn_cast<IntrinsicInst>(BBI))
+        if (II->getIntrinsicID() == Intrinsic::lifetime_end)
+          continue;
       if (BBI->mayHaveSideEffects() || BBI->mayReadFromMemory() ||
           !isSafeToSpeculativelyExecute(&*BBI))
         return false;
