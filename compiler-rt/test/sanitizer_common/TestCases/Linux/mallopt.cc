f4a6f6424daf2b7876fa83b190feed1f5b225ac4
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2018-10-26 15:13:20 +0000

    [sanitizer] Fix mallopt test on Android.
    There is not a single common mallopt option between gnu/linux and
    android, so simply use a random number there.
    
    llvm-svn: 345394

--- compiler-rt/test/sanitizer_common/TestCases/Linux/mallopt.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/mallopt.cc
@@ -4,6 +4,7 @@
 #include <malloc.h>
 
 int main() {
-  int res = mallopt(M_ARENA_MAX, 0);
+  // Try a random mallopt option, possibly invalid.
+  int res = mallopt(-42, 0);
   assert(res == 0 || res == 1);
 }
