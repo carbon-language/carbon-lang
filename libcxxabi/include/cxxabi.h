f503f7fd420cfd095407cea00e531497c13c6b9b
Petr Hosek <phosek@chromium.org>
2018-10-29 20:20:26 +0000

    [libc++abi] Provide __cxa_thread_atexit on Fuchsia
    Fuchsia already supports this interface.
    
    Differential Revision: https://reviews.llvm.org/D53801
    
    llvm-svn: 345534

--- libcxxabi/include/cxxabi.h
+++ libcxxabi/include/cxxabi.h
@@ -160,8 +160,8 @@ __cxa_decrement_exception_refcount(void *primary_exception) throw();
 extern _LIBCXXABI_FUNC_VIS bool __cxa_uncaught_exception() throw();
 extern _LIBCXXABI_FUNC_VIS unsigned int __cxa_uncaught_exceptions() throw();
 
-#ifdef __linux__
-// Linux TLS support. Not yet an official part of the Itanium ABI.
+#if defined(__linux__) || defined(__Fuchsia__)
+// Linux and Fuchsia TLS support. Not yet an official part of the Itanium ABI.
 // https://sourceware.org/glibc/wiki/Destructor%20support%20for%20thread_local%20variables
 extern _LIBCXXABI_FUNC_VIS int __cxa_thread_atexit(void (*)(void *), void *,
                                                    void *) throw();
