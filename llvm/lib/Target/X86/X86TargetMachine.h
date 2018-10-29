090a444cb7e00a5c4a0d525909ae6a7014c305ad
Simon Pilgrim <llvm-dev@redking.me.uk>
2018-10-29 19:51:52 +0000

    [X86] Set isMachineVerifierClean() back to false (PR27481)
    Put back the isMachineVerifierClean() override removed at rL345513 to fix Windows ThinLTO tests
    
    llvm-svn: 345528

--- llvm/lib/Target/X86/X86TargetMachine.h
+++ llvm/lib/Target/X86/X86TargetMachine.h
@@ -53,6 +53,10 @@ public:
   TargetLoweringObjectFile *getObjFileLowering() const override {
     return TLOF.get();
   }
+
+  bool isMachineVerifierClean() const override {
+    return false;
+  }
 };
 
 } // end namespace llvm
