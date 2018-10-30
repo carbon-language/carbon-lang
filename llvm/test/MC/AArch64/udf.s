1cc49d3ca85c24bfd700c633c00221eba415e46b
Diogo N. Sampaio <diogo.sampaio@arm.com>
2018-10-30 11:39:33 +0000

    [FIX][AArch64] Add support for UDF instruction
    Fix wrong test files submited
    in rL345581
    
    llvm-svn: 345587

--- llvm/test/MC/AArch64/udf.s
+++ llvm/test/MC/AArch64/udf.s
@@ -1,15 +1,9 @@
 # RUN: llvm-mc -assemble -show-encoding -triple=aarch64- %s | FileCheck %s
 # CHECK:  .text
 # CHECK-NEXT: udf #0      // encoding: [0x00,0x00,0x00,0x00]
-# CHECK-NEXT: udf #1      // encoding: [0x01,0x00,0x00,0x00]
-# CHECK-NEXT: udf #16     // encoding: [0x10,0x00,0x00,0x00]
-# CHECK-NEXT: udf #32     // encoding: [0x20,0x00,0x00,0x00]
-# CHECK-NEXT: udf #48     // encoding: [0x30,0x00,0x00,0x00]
-# CHECK-NEXT: udf #65535      // encoding: [0xff,0xff,0x00,0x00]
+# CHECK-NEXT: udf #513    // encoding: [0x01,0x02,0x00,0x00]
+# CHECK-NEXT: udf #65535  // encoding: [0xff,0xff,0x00,0x00]
 .text
 udf 0
-udf 1
-udf 16
-udf 32
-udf 48
+udf 513
 udf 65535
