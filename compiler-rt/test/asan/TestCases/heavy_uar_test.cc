02f838d778f032064fa98d42de009789a3397469
Kamil Rytarowski <n54@gmx.com>
2018-10-30 13:49:15 +0000

    Adapt ASan test heavy_uar_test for NetBSD
    The stack size is tight for the main thread in multithread
    environment and follow the FreeBSD approach of reducing stack
    usage.
    
    llvm-svn: 345599

--- compiler-rt/test/asan/TestCases/heavy_uar_test.cc
+++ compiler-rt/test/asan/TestCases/heavy_uar_test.cc
@@ -53,8 +53,8 @@ int main(int argc, char **argv) {
     RecursiveFunctionWithStackFrame<1024>(depth);
     RecursiveFunctionWithStackFrame<2000>(depth);
     // The stack size is tight for the main thread in multithread
-    // environment on FreeBSD.
-#if !defined(__FreeBSD__)
+    // environment on FreeBSD and NetBSD.
+#if !defined(__FreeBSD__) && !defined(__NetBSD__)
     RecursiveFunctionWithStackFrame<5000>(depth);
     RecursiveFunctionWithStackFrame<10000>(depth);
 #endif
