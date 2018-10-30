d54d17b6e2fca660f7d1bb5f6753022060e06ff4
Kamil Rytarowski <n54@gmx.com>
2018-10-30 18:21:58 +0000

    Mark interception_failure_test with XFAIL for NetBSD
    This test breaks also on FreeBSD.
    
    llvm-svn: 345619

--- compiler-rt/test/asan/TestCases/interception_failure_test.cc
+++ compiler-rt/test/asan/TestCases/interception_failure_test.cc
@@ -5,7 +5,7 @@
 // RUN: %clangxx_asan -O1 %s -o %t && %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -O2 %s -o %t && %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -O3 %s -o %t && %run %t 2>&1 | FileCheck %s
-// XFAIL: freebsd
+// XFAIL: freebsd, netbsd
 
 // On Windows, defining strtoll in a static build results in linker errors, but
 // it works with the dynamic runtime.
