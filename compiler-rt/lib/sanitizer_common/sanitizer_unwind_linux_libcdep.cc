98f1dd0bd593fc147bddff45c706542c4c08cadd
Kamil Rytarowski <n54@gmx.com>
2018-10-30 18:25:12 +0000

    Cast the return value of _Unwind_GetIP() to uptr
    This is needed for NetBSD to match the expected type in
    Unwind_GetIP().
    
    llvm-svn: 345620

--- compiler-rt/lib/sanitizer_common/sanitizer_unwind_linux_libcdep.cc
+++ compiler-rt/lib/sanitizer_common/sanitizer_unwind_linux_libcdep.cc
@@ -97,7 +97,7 @@ uptr Unwind_GetIP(struct _Unwind_Context *ctx) {
   // Clear the Thumb bit.
   return val & ~(uptr)1;
 #else
-  return _Unwind_GetIP(ctx);
+  return (uptr)_Unwind_GetIP(ctx);
 #endif
 }
 
