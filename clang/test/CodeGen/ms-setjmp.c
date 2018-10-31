e7c7934a11b18df4a26b55408c8f0ee481d177ca
Mandeep Singh Grang <mgrang@codeaurora.org>
2018-10-31 23:17:36 +0000

    [COFF, ARM64] Change setjmp for AArch64 Windows to use Intrinsic.sponentry
    Summary: ARM64 setjmp expects sp on entry instead of framepointer.
    
    Reviewers: mgrang, rnk, TomTan, compnerd, mstorsjo, efriedma
    
    Reviewed By: mstorsjo
    
    Subscribers: javed.absar, kristof.beyls, chrib, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D53684
    
    llvm-svn: 345792

--- clang/test/CodeGen/ms-setjmp.c
+++ clang/test/CodeGen/ms-setjmp.c
@@ -25,7 +25,7 @@ int test_setjmp() {
   // X64-NEXT:  ret i32 %[[call]]
 
   // AARCH64-LABEL: define dso_local i32 @test_setjmp
-  // AARCH64:       %[[addr:.*]] = call i8* @llvm.frameaddress(i32 0)
+  // AARCH64:       %[[addr:.*]] = call i8* @llvm.sponentry()
   // AARCH64:       %[[call:.*]] = call i32 @_setjmpex(i8* getelementptr inbounds ([1 x i8], [1 x i8]* @jb, i32 0, i32 0), i8* %[[addr]])
   // AARCH64-NEXT:  ret i32 %[[call]]
 }
@@ -38,7 +38,7 @@ int test_setjmpex() {
   // X64-NEXT:  ret i32 %[[call]]
 
   // AARCH64-LABEL: define dso_local i32 @test_setjmpex
-  // AARCH64:       %[[addr:.*]] = call i8* @llvm.frameaddress(i32 0)
+  // AARCH64:       %[[addr:.*]] = call i8* @llvm.sponentry()
   // AARCH64:       %[[call:.*]] = call i32 @_setjmpex(i8* getelementptr inbounds ([1 x i8], [1 x i8]* @jb, i32 0, i32 0), i8* %[[addr]])
   // AARCH64-NEXT:  ret i32 %[[call]]
 }
