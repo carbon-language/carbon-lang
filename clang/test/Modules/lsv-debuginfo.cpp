d1932dcdd3e3212ad6ad21f586cfa8a44940b8f9
Adrian Prantl <aprantl@apple.com>
2018-11-02 22:19:02 +0000

    Add an explicit -std=c++14 to this test.
    rdar://problem/45642490
    
    llvm-svn: 346048

--- clang/test/Modules/lsv-debuginfo.cpp
+++ clang/test/Modules/lsv-debuginfo.cpp
@@ -1,7 +1,7 @@
 // Test C++ -gmodules debug info in the PCMs with local submodule visibility.
 // REQUIRES: asserts
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -triple %itanium_abi_triple    \
+// RUN: %clang_cc1 -triple %itanium_abi_triple -std=c++14 \
 // RUN:   -fmodules-local-submodule-visibility %s \
 // RUN:   -dwarf-ext-refs -fmodule-format=obj -debug-info-kind=standalone \
 // RUN:   -dwarf-version=4 -fmodules -fimplicit-module-maps \
@@ -10,13 +10,13 @@
 // RUN: cat %t-mod.ll | FileCheck %s
 
 // RUN: rm -rf %t
-// RUN: %clang_cc1 -triple %itanium_abi_triple    \
+// RUN: %clang_cc1 -triple %itanium_abi_triple -std=c++14 \
 // RUN:   -fmodules-local-submodule-visibility %s \
 // RUN:   -dwarf-ext-refs -fmodule-format=obj -debug-info-kind=standalone \
 // RUN:   -dwarf-version=4 -fmodules -fimplicit-module-maps \
 // RUN:   -fmodules-cache-path="%t" -o %t.ll -I%S/Inputs/lsv-debuginfo \
-// RUN:   -mllvm -debug-only=pchcontainer &>%t-mod.ll \
-// RUN:   -DWITH_NAMESPACE
+// RUN:   -DWITH_NAMESPACE \
+// RUN:   -mllvm -debug-only=pchcontainer &>%t-mod.ll
 // RUN: cat %t-mod.ll | FileCheck %s
 
 // ADT
