dfc56b43fadf5c4f84cdb8706f29ccf1ba68e5f1
Ilya Biryukov <ibiryukov@google.com>
2018-11-02 10:50:26 +0000

    Revert "Fix regression in behavior of clang -x c++-header -fmodule-name=XXX"
    This reverts commit r345803 and r345915 (a follow-up fix to r345803).
    
    Reason: r345803 blocks our internal integrate because of the new
    warnings showing up in too many places. The fix is actually correct,
    we will reland it after figuring out how to integrate properly.
    
    llvm-svn: 345963

--- clang/lib/Frontend/CompilerInstance.cpp
+++ clang/lib/Frontend/CompilerInstance.cpp
@@ -372,9 +372,6 @@ static void InitializeFileRemapping(DiagnosticsEngine &Diags,
 void CompilerInstance::createPreprocessor(TranslationUnitKind TUKind) {
   const PreprocessorOptions &PPOpts = getPreprocessorOpts();
 
-  // The module manager holds a reference to the old preprocessor (if any).
-  ModuleManager.reset();
-
   // Create a PTH manager if we are using some form of a token cache.
   PTHManager *PTHMgr = nullptr;
   if (!PPOpts.TokenCache.empty())
