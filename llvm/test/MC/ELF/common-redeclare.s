c0d5d5c05db37bf0cef8ae7e45b66c4987df6b8d
Kristina Brooks <kristina@nym.hush.com>
2018-11-11 18:40:33 +0000

    [llvm][test] Update tests using objdump
    Update tests using llvm-objdump since check strings don't
    match anymore due to the extra `O` in place. This is a 
    followup for rL346610. 
     
    
    llvm-svn: 346611

--- llvm/test/MC/ELF/common-redeclare.s
+++ llvm/test/MC/ELF/common-redeclare.s
@@ -1,5 +1,5 @@
 # RUN: llvm-mc -filetype=obj -triple x86_64-pc-linux %s | llvm-objdump -t - | FileCheck %s
 
-# CHECK: 0000000000000004 g       *COM*  00000004 C
+# CHECK: 0000000000000004 g     O *COM*  00000004 C
         .comm   C,4,4
         .comm   C,4,4
