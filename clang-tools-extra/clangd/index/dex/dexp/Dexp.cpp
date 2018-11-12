172c045590dee0962d86f84ffbeba0db1d0fc2aa
Haojian Wu <hokein@google.com>
2018-11-12 16:41:15 +0000

    [clangd] Don't show all refs results if -name is ambiguous in dexp.
    Reviewers: ioeric
    
    Subscribers: ilya-biryukov, MaskRay, jkorous, arphaman, kadircet, cfe-commits
    
    Differential Revision: https://reviews.llvm.org/D54430
    
    llvm-svn: 346671

--- clang-tools-extra/clangd/index/dex/dexp/Dexp.cpp
+++ clang-tools-extra/clangd/index/dex/dexp/Dexp.cpp
@@ -218,6 +218,12 @@ class Refs : public Command {
       IDs.push_back(*SID);
     } else {
       IDs = getSymbolIDsFromIndex(Name, Index);
+      if (IDs.size() > 1) {
+        outs() << formatv("The name {0} is ambiguous, found {1} different "
+                          "symbols. Please use id flag to disambiguate.\n",
+                          Name, IDs.size());
+        return;
+      }
     }
     RefsRequest RefRequest;
     RefRequest.IDs.insert(IDs.begin(), IDs.end());
