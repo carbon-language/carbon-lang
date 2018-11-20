289f1ce53e52e27454a02670673ee1d53ff06f86
Marshall Clow <mclow.lists@gmail.com>
2018-11-20 22:55:40 +0000

    A couple of tests were broken when clang implemented the compiler parts of P0482 (support for char8_t). Comment out those bits until we implement the corresponding bits in libc++
    llvm-svn: 347360

--- libcxx/test/std/strings/basic.string.literals/literal.pass.cpp
+++ libcxx/test/std/strings/basic.string.literals/literal.pass.cpp
@@ -13,12 +13,17 @@
 #include <string>
 #include <cassert>
 
+#include "test_macros.h"
+
 int main()
 {
     using namespace std::literals::string_literals;
 
     static_assert ( std::is_same<decltype(   "Hi"s), std::string>::value, "" );
+//	This is changed by P0482 to return a std::u8string
+#if TEST_STD_VER <= 17
     static_assert ( std::is_same<decltype( u8"Hi"s), std::string>::value, "" );
+#endif
     static_assert ( std::is_same<decltype(  L"Hi"s), std::wstring>::value, "" );
     static_assert ( std::is_same<decltype(  u"Hi"s), std::u16string>::value, "" );
     static_assert ( std::is_same<decltype(  U"Hi"s), std::u32string>::value, "" );
@@ -29,19 +34,28 @@ int main()
     std::u32string Ufoo;
 
     foo  =   ""s;     assert( foo.size() == 0);
+//	This is changed by P0482 to return a std::u8string
+#if TEST_STD_VER <= 17
     foo  = u8""s;     assert( foo.size() == 0);
+#endif
     Lfoo =  L""s;     assert(Lfoo.size() == 0);
     ufoo =  u""s;     assert(ufoo.size() == 0);
     Ufoo =  U""s;     assert(Ufoo.size() == 0);
 
     foo  =   " "s;     assert( foo.size() == 1);
+//	This is changed by P0482 to return a std::u8string
+#if TEST_STD_VER <= 17
     foo  = u8" "s;     assert( foo.size() == 1);
+#endif
     Lfoo =  L" "s;     assert(Lfoo.size() == 1);
     ufoo =  u" "s;     assert(ufoo.size() == 1);
     Ufoo =  U" "s;     assert(Ufoo.size() == 1);
 
     foo  =   "ABC"s;     assert( foo ==   "ABC");   assert( foo == std::string   (  "ABC"));
+//	This is changed by P0482 to return a std::u8string
+#if TEST_STD_VER <= 17
     foo  = u8"ABC"s;     assert( foo == u8"ABC");   assert( foo == std::string   (u8"ABC"));
+#endif
     Lfoo =  L"ABC"s;     assert(Lfoo ==  L"ABC");   assert(Lfoo == std::wstring  ( L"ABC"));
     ufoo =  u"ABC"s;     assert(ufoo ==  u"ABC");   assert(ufoo == std::u16string( u"ABC"));
     Ufoo =  U"ABC"s;     assert(Ufoo ==  U"ABC");   assert(Ufoo == std::u32string( U"ABC"));
--- libcxx/test/std/strings/string.view/string_view.literals/literal.pass.cpp
+++ libcxx/test/std/strings/string.view/string_view.literals/literal.pass.cpp
@@ -16,12 +16,17 @@
 #include <string_view>
 #include <cassert>
 
+#include "test_macros.h"
+
 int main()
 {
     using namespace std::literals::string_view_literals;
 
     static_assert ( std::is_same<decltype(   "Hi"sv), std::string_view>::value, "" );
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     static_assert ( std::is_same<decltype( u8"Hi"sv), std::string_view>::value, "" );
+#endif
     static_assert ( std::is_same<decltype(  L"Hi"sv), std::wstring_view>::value, "" );
     static_assert ( std::is_same<decltype(  u"Hi"sv), std::u16string_view>::value, "" );
     static_assert ( std::is_same<decltype(  U"Hi"sv), std::u32string_view>::value, "" );
@@ -32,31 +37,46 @@ int main()
     std::u32string_view Ufoo;
 
     foo  =   ""sv;     assert( foo.size() == 0);
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     foo  = u8""sv;     assert( foo.size() == 0);
+#endif
     Lfoo =  L""sv;     assert(Lfoo.size() == 0);
     ufoo =  u""sv;     assert(ufoo.size() == 0);
     Ufoo =  U""sv;     assert(Ufoo.size() == 0);
 
     foo  =   " "sv;     assert( foo.size() == 1);
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     foo  = u8" "sv;     assert( foo.size() == 1);
+#endif
     Lfoo =  L" "sv;     assert(Lfoo.size() == 1);
     ufoo =  u" "sv;     assert(ufoo.size() == 1);
     Ufoo =  U" "sv;     assert(Ufoo.size() == 1);
 
     foo  =   "ABC"sv;     assert( foo ==   "ABC");   assert( foo == std::string_view   (  "ABC"));
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     foo  = u8"ABC"sv;     assert( foo == u8"ABC");   assert( foo == std::string_view   (u8"ABC"));
+#endif
     Lfoo =  L"ABC"sv;     assert(Lfoo ==  L"ABC");   assert(Lfoo == std::wstring_view  ( L"ABC"));
     ufoo =  u"ABC"sv;     assert(ufoo ==  u"ABC");   assert(ufoo == std::u16string_view( u"ABC"));
     Ufoo =  U"ABC"sv;     assert(Ufoo ==  U"ABC");   assert(Ufoo == std::u32string_view( U"ABC"));
 
     static_assert(  "ABC"sv.size() == 3, "");
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     static_assert(u8"ABC"sv.size() == 3, "");
+#endif
     static_assert( L"ABC"sv.size() == 3, "");
     static_assert( u"ABC"sv.size() == 3, "");
     static_assert( U"ABC"sv.size() == 3, "");
 
     static_assert(noexcept(  "ABC"sv), "");
+//	This is changed by P0482 to return a std::u8string - re-enable when we implement that.
+#if TEST_STD_VER <= 17
     static_assert(noexcept(u8"ABC"sv), "");
+#endif
     static_assert(noexcept( L"ABC"sv), "");
     static_assert(noexcept( u"ABC"sv), "");
     static_assert(noexcept( U"ABC"sv), "");
