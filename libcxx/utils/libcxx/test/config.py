bb4f2dfc36f1648d089467e548692153dc7ca320
Louis Dionne <ldionne@apple.com>
2018-11-21 23:00:45 +0000

    [libcxx] Improve error message when an invalid directory is provided as use_system_cxx_lib
    llvm-svn: 347435

--- libcxx/utils/libcxx/test/config.py
+++ libcxx/utils/libcxx/test/config.py
@@ -307,7 +307,7 @@ class Configuration(object):
         elif self.use_system_cxx_lib == 'false':
             self.use_system_cxx_lib = False
         elif self.use_system_cxx_lib:
-            assert os.path.isdir(self.use_system_cxx_lib)
+            assert os.path.isdir(self.use_system_cxx_lib), "the specified use_system_cxx_lib parameter (%s) is not a valid directory" % self.use_system_cxx_lib
         self.lit_config.note(
             "inferred use_system_cxx_lib as: %r" % self.use_system_cxx_lib)
 
