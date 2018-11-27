1a1fbdc6efede45777d8a3b94ba915a9fa2e3294
Henry Wong <movietravelcode@outlook.com>
2018-11-27 04:27:00 +0000

    [clangd] NFC: Prefer `isa<>` to `dyn_cast<>` to do the checking.
    Summary: Prefer `isa<>` to `dyn_cast<>` when there only need a checking.
    
    Reviewers: ilya-biryukov, MaskRay
    
    Reviewed By: ilya-biryukov, MaskRay
    
    Subscribers: ioeric, MaskRay, jkorous, arphaman, kadircet, cfe-commits, MTC
    
    Differential Revision: https://reviews.llvm.org/D54878
    
    llvm-svn: 347635

--- clang-tools-extra/clangd/AST.cpp
+++ clang-tools-extra/clangd/AST.cpp
@@ -95,11 +95,11 @@ std::string printName(const ASTContext &Ctx, const NamedDecl &ND) {
     return Out.str();
   }
   // The name was empty, so present an anonymous entity.
-  if (llvm::dyn_cast<NamespaceDecl>(&ND))
+  if (isa<NamespaceDecl>(ND))
     return "(anonymous namespace)";
   if (auto *Cls = llvm::dyn_cast<RecordDecl>(&ND))
     return ("(anonymous " + Cls->getKindName() + ")").str();
-  if (llvm::dyn_cast<EnumDecl>(&ND))
+  if (isa<EnumDecl>(ND))
     return "(anonymous enum)";
   return "(anonymous)";
 }
