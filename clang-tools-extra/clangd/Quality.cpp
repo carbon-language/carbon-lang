1208240ac9d5eb00f7d6fe26d134081d5283e52c
Eric Liu <ioeric@google.com>
2018-11-28 14:00:09 +0000

    [clangd] Fix test broken in r347754.
    llvm-svn: 347755

--- clang-tools-extra/clangd/Quality.cpp
+++ clang-tools-extra/clangd/Quality.cpp
@@ -325,7 +325,7 @@ static float scopeBoost(ScopeDistance &Distance,
   auto D = Distance.distance(*SymbolScope);
   if (D == FileDistance::Unreachable)
     return 0.6f;
-  return std::max(0.6, 2.0 * std::pow(0.6, D / 2.0));
+  return std::max(0.65, 2.0 * std::pow(0.6, D / 2.0));
 }
 
 float SymbolRelevanceSignals::evaluate() const {
