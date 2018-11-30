27b1e3bd4f7a064a2672e30cc6be31433ebd300b
Joseph Tremoulet <jotrem@microsoft.com>
2018-11-30 19:20:02 +0000

    [Mem2Reg] Fix nondeterministic corner case
    Summary:
    When mem2reg inserts phi nodes in blocks with unreachable predecessors,
    it adds undef operands for those incoming edges.  When there are
    multiple such predecessors, the order is currently based on the address
    of the BasicBlocks.  This change fixes that by using the BBNumbers in
    the sort/search predicates, as is done elsewhere in mem2reg to ensure
    determinism.
    
    Also adds a testcase with a bunch of unreachable preds, which
    (nodeterministically) fails without the fix.
    
    
    Reviewers: majnemer
    
    Reviewed By: majnemer
    
    Subscribers: mgrang, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D55077
    
    llvm-svn: 348024

--- llvm/lib/Transforms/Utils/PromoteMemoryToRegister.cpp
+++ llvm/lib/Transforms/Utils/PromoteMemoryToRegister.cpp
@@ -751,14 +751,18 @@ void PromoteMem2Reg::run() {
     // Ok, now we know that all of the PHI nodes are missing entries for some
     // basic blocks.  Start by sorting the incoming predecessors for efficient
     // access.
-    llvm::sort(Preds);
+    auto CompareBBNumbers = [this](BasicBlock *A, BasicBlock *B) {
+      return BBNumbers.lookup(A) < BBNumbers.lookup(B);
+    };
+    llvm::sort(Preds, CompareBBNumbers);
 
     // Now we loop through all BB's which have entries in SomePHI and remove
     // them from the Preds list.
     for (unsigned i = 0, e = SomePHI->getNumIncomingValues(); i != e; ++i) {
       // Do a log(n) search of the Preds list for the entry we want.
       SmallVectorImpl<BasicBlock *>::iterator EntIt = std::lower_bound(
-          Preds.begin(), Preds.end(), SomePHI->getIncomingBlock(i));
+          Preds.begin(), Preds.end(), SomePHI->getIncomingBlock(i),
+          CompareBBNumbers);
       assert(EntIt != Preds.end() && *EntIt == SomePHI->getIncomingBlock(i) &&
              "PHI node has entry for a block which is not a predecessor!");
 
