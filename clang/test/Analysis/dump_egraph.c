58c3837ee38c27d2370d3d7b047255c650106bb0
George Karpenkov <ekarpenkov@apple.com>
2018-11-30 02:17:18 +0000

    [analyzer] [NFC] Test dumping trimmed graph
    Differential Revision: https://reviews.llvm.org/D54972
    
    llvm-svn: 347941

--- clang/test/Analysis/dump_egraph.c
+++ clang/test/Analysis/dump_egraph.c
@@ -1,5 +1,6 @@
 // RUN: %clang_analyze_cc1 -analyzer-checker=core -analyzer-dump-egraph=%t.dot %s
 // RUN: cat %t.dot | FileCheck %s
+// RUN: %clang_analyze_cc1 -analyzer-checker=core -analyzer-dump-egraph=%t.dot -trim-egraph %s
 // REQUIRES: asserts
 
 int getJ();
