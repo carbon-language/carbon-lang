765e1a44728634cc02078b91828ee336b43ea71c
Stephen Kelly <steveire@gmail.com>
2018-12-02 16:36:23 +0000

    Add dump tests for ArrayInitLoopExpr and ArrayInitIndexExpr
    llvm-svn: 348093

---
+++ clang/test/AST/ast-dump-array.cpp
@@ -0,0 +1,10 @@
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -ast-dump %s | FileCheck -strict-whitespace %s
+
+void testArrayInitExpr()
+{
+    int a[10];
+    auto l = [a]{
+    };
+    // CHECK: |-ArrayInitLoopExpr 0x{{[^ ]*}} <col:15> 'int [10]'
+    // CHECK: | `-ArrayInitIndexExpr 0x{{[^ ]*}} <<invalid sloc>> 'unsigned long'
+}
