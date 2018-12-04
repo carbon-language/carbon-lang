0fa7bf099dc5572c757639ea8667310927daa374
Stephen Kelly <steveire@gmail.com>
2018-12-04 09:53:36 +0000

    Extend test for DependentSizedArrayType
    Use a using declaration to force the type to appear in the -ast-dump
    output.
    
    llvm-svn: 348241

--- clang/test/AST/ast-dump-array.cpp
+++ clang/test/AST/ast-dump-array.cpp
@@ -8,3 +8,12 @@ void testArrayInitExpr()
     // CHECK: |-ArrayInitLoopExpr 0x{{[^ ]*}} <col:15> 'int [10]'
     // CHECK: |     `-ArrayInitIndexExpr 0x{{[^ ]*}} <<invalid sloc>> 'unsigned long'
 }
+
+template<typename T, int Size>
+class array {
+  T data[Size];
+
+  using array_T_size = T[Size];
+  // CHECK: `-DependentSizedArrayType 0x{{[^ ]*}} 'T [Size]' dependent   <col:25, col:30>
+};
+
