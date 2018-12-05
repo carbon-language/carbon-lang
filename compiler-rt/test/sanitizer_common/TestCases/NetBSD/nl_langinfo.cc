fab764359f8df01a3aa8f1e76de5dd87a0ecf7d5
Kamil Rytarowski <n54@gmx.com>
2018-12-05 15:06:53 +0000

    Add a new interceptor for nl_langinfo(3) from NetBSD
    Summary:
    nl_langinfo - gets locale information.
    
    Add a dedicated test.
    
    Reviewers: vitalybuka, joerg
    
    Reviewed By: vitalybuka
    
    Subscribers: kubamracek, llvm-commits, mgorny, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D55015
    
    llvm-svn: 348369

---
+++ compiler-rt/test/sanitizer_common/TestCases/NetBSD/nl_langinfo.cc
@@ -0,0 +1,18 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck %s
+
+#include <langinfo.h>
+
+#include <stdio.h>
+
+int main(void) {
+  printf("nl_langinfo\n");
+
+  char *info = nl_langinfo(DAY_1);
+
+  printf("DAY_1='%s'\n", info);
+
+  // CHECK: nl_langinfo
+  // CHECK: DAY_1='{{.*}}'
+
+  return 0;
+}
