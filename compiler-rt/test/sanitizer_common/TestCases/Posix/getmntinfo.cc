51e820d0d8bbd5586cb8c627e243d75b45fdf32f
David Carlier <devnexen@gmail.com>
2018-12-06 17:04:18 +0000

    [Sanitizer] getmntinfo support in FreeBSD
    Reviewers: krytarowski
    
    Reviewed By: krytarowski
    
    Differential Revision: https://reviews.llvm.org/D55354
    
    llvm-svn: 348500

--- compiler-rt/test/sanitizer_common/TestCases/Posix/getmntinfo.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/getmntinfo.cc
@@ -1,8 +1,14 @@
 // RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck %s
+//
+// UNSUPPORTED: linux, darwin, solaris
 
 #include <sys/types.h>
 
+#if defined(__NetBSD__)
 #include <sys/statvfs.h>
+#else
+#include <sys/mount.h>
+#endif
 
 #include <err.h>
 #include <stdio.h>
@@ -11,7 +17,11 @@
 int main(void) {
   printf("getmntinfo\n");
 
+#if defined(__NetBSD__)
   struct statvfs *fss;
+#else
+  struct statfs *fss;
+#endif
   int nfss = getmntinfo(&fss, MNT_NOWAIT);
   if (nfss <= 0)
     errx(1, "getmntinfo");
