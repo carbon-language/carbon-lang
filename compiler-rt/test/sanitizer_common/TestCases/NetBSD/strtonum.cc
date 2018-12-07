0fed92a933380a4680d6b0242e076ae883249f49
Kamil Rytarowski <n54@gmx.com>
2018-12-07 21:47:36 +0000

    Add new interceptor for strtonum(3)
    Summary:
    strtonum(3) reliably convertss string value to an integer.
    This function is used in OpenBSD compat namespace
    and is located inside NetBSD's libc.
    
    Add a dedicated test for this interface.
    
    It's a reworked version of the original code by Yang Zheng.
    
    Reviewers: joerg, vitalybuka
    
    Reviewed By: vitalybuka
    
    Subscribers: tomsun.0.7, kubamracek, llvm-commits, mgorny, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D54527
    
    llvm-svn: 348651

---
+++ compiler-rt/test/sanitizer_common/TestCases/NetBSD/strtonum.cc
@@ -0,0 +1,52 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck %s
+
+#define _OPENBSD_SOURCE
+
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+  const char *errstr;
+
+  printf("strtonum\n");
+
+  long long l = strtonum("100", 1, 100, &errstr);
+  assert(!errstr);
+  printf("%lld\n", l);
+
+  l = strtonum("200", 1, 100, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  l = strtonum("300", 1000, 1001, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  l = strtonum("abc", 1000, 1001, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  l = strtonum("1000", 1001, 1000, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  l = strtonum("1000abc", 1000, 1001, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  l = strtonum("1000.0", 1000, 1001, &errstr);
+  assert(errstr);
+  printf("%s\n", errstr);
+
+  // CHECK: strtonum
+  // CHECK: 100
+  // CHECK: too large
+  // CHECK: too small
+  // CHECK: invalid
+  // CHECK: invalid
+  // CHECK: invalid
+  // CHECK: invalid
+
+  return 0;
+}
