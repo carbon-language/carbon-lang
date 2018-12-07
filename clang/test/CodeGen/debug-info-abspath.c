677b72f70158ffbc2313ecad0886b24ffe89d294
Adrian Prantl <aprantl@apple.com>
2018-12-07 17:04:26 +0000

    Make testcase more robust for completely-out-of-tree builds.
    Thats to Dave Zarzycki for reprorting this!
    
    llvm-svn: 348612

--- clang/test/CodeGen/debug-info-abspath.c
+++ clang/test/CodeGen/debug-info-abspath.c
@@ -1,15 +1,19 @@
+// RUN: mkdir -p %t/UNIQUEISH_SENTINEL
+// RUN: cp %s %t/UNIQUEISH_SENTINEL/debug-info-abspath.c
+
 // RUN: %clang_cc1 -debug-info-kind=limited -triple %itanium_abi_triple \
-// RUN:   %s -emit-llvm -o - | FileCheck %s
+// RUN:   %t/UNIQUEISH_SENTINEL/debug-info-abspath.c -emit-llvm -o - \
+// RUN:   | FileCheck %s
 
 // RUN: cp %s %t.c
 // RUN: %clang_cc1 -debug-info-kind=limited -triple %itanium_abi_triple \
 // RUN:   %t.c -emit-llvm -o - | FileCheck %s --check-prefix=INTREE
 void foo() {}
 
-// Since %s is an absolute path, directory should be a nonempty
-// prefix, but the CodeGen part should be part of the filename.
+// Since %s is an absolute path, directory should be the common
+// prefix, but the directory part should be part of the filename.
 
-// CHECK: DIFile(filename: "{{.*}}CodeGen{{.*}}debug-info-abspath.c"
-// CHECK-SAME:   directory: "{{.+}}")
+// CHECK: DIFile(filename: "{{.*}}UNIQUEISH_SENTINEL{{.*}}debug-info-abspath.c"
+// CHECK-NOT:    directory: "{{.*}}UNIQUEISH_SENTINEL
 
 // INTREE: DIFile({{.*}}directory: "{{.+}}CodeGen{{.*}}")
