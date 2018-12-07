31975f601fa58cc1e9fda574243b7aa104c02dc1
Heejin Ahn <aheejin@gmail.com>
2018-12-07 21:48:38 +0000

    clang-format LLVM.h (NFC)
    Summary:
    - LLVM style does not indent inside namespaces
    - Alphabetize
    
    Reviewers: ruiu
    
    Subscribers: sbc100, llvm-commits
    
    Differential Revision: https://reviews.llvm.org/D55406
    
    llvm-svn: 348652

--- lld/include/lld/Common/LLVM.h
+++ lld/include/lld/Common/LLVM.h
@@ -22,65 +22,65 @@
 #include <utility>
 
 namespace llvm {
-  // ADT's.
-  class raw_ostream;
-  class Error;
-  class StringRef;
-  class Twine;
-  class MemoryBuffer;
-  class MemoryBufferRef;
-  template<typename T> class ArrayRef;
-  template<unsigned InternalLen> class SmallString;
-  template<typename T, unsigned N> class SmallVector;
-  template<typename T> class ErrorOr;
-  template<typename T> class Expected;
+// ADT's.
+class raw_ostream;
+class Error;
+class StringRef;
+class Twine;
+class MemoryBuffer;
+class MemoryBufferRef;
+template <typename T> class ArrayRef;
+template <unsigned InternalLen> class SmallString;
+template <typename T, unsigned N> class SmallVector;
+template <typename T> class ErrorOr;
+template <typename T> class Expected;
 
-  namespace object {
-    class WasmObjectFile;
-    struct WasmSection;
-    struct WasmSegment;
-    class WasmSymbol;
-  }
+namespace object {
+class WasmObjectFile;
+struct WasmSection;
+struct WasmSegment;
+class WasmSymbol;
+} // namespace object
 
-  namespace wasm {
-    struct WasmFunction;
-    struct WasmGlobal;
-    struct WasmGlobalType;
-    struct WasmRelocation;
-    struct WasmSignature;
-  }
-}
+namespace wasm {
+struct WasmFunction;
+struct WasmGlobal;
+struct WasmGlobalType;
+struct WasmRelocation;
+struct WasmSignature;
+} // namespace wasm
+} // namespace llvm
 
 namespace lld {
-  // Casting operators.
-  using llvm::isa;
-  using llvm::cast;
-  using llvm::dyn_cast;
-  using llvm::dyn_cast_or_null;
-  using llvm::cast_or_null;
+// Casting operators.
+using llvm::cast;
+using llvm::cast_or_null;
+using llvm::dyn_cast;
+using llvm::dyn_cast_or_null;
+using llvm::isa;
 
-  // ADT's.
-  using llvm::raw_ostream;
-  using llvm::Error;
-  using llvm::StringRef;
-  using llvm::Twine;
-  using llvm::MemoryBuffer;
-  using llvm::MemoryBufferRef;
-  using llvm::ArrayRef;
-  using llvm::SmallString;
-  using llvm::SmallVector;
-  using llvm::ErrorOr;
-  using llvm::Expected;
+// ADT's.
+using llvm::ArrayRef;
+using llvm::Error;
+using llvm::ErrorOr;
+using llvm::Expected;
+using llvm::MemoryBuffer;
+using llvm::MemoryBufferRef;
+using llvm::raw_ostream;
+using llvm::SmallString;
+using llvm::SmallVector;
+using llvm::StringRef;
+using llvm::Twine;
 
-  using llvm::object::WasmObjectFile;
-  using llvm::object::WasmSection;
-  using llvm::object::WasmSegment;
-  using llvm::object::WasmSymbol;
-  using llvm::wasm::WasmFunction;
-  using llvm::wasm::WasmGlobal;
-  using llvm::wasm::WasmGlobalType;
-  using llvm::wasm::WasmRelocation;
-  using llvm::wasm::WasmSignature;
+using llvm::object::WasmObjectFile;
+using llvm::object::WasmSection;
+using llvm::object::WasmSegment;
+using llvm::object::WasmSymbol;
+using llvm::wasm::WasmFunction;
+using llvm::wasm::WasmGlobal;
+using llvm::wasm::WasmGlobalType;
+using llvm::wasm::WasmRelocation;
+using llvm::wasm::WasmSignature;
 } // end namespace lld.
 
 namespace std {
@@ -90,6 +90,6 @@ public:
     return llvm::hash_value(s);
   }
 };
-}
+} // namespace std
 
 #endif
