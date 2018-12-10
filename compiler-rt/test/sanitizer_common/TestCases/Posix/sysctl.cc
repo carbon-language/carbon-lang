2b26a98a0d1c03e2196f37f0853141d2e4a5de6b
David Carlier <devnexen@gmail.com>
2018-12-10 16:29:30 +0000

    [Sanitizer] expand sysctl/getmntinfo/nl_langinfo to Darwin
    Reviewers: vitalybuka, krytarowski, kubamracek
    
    Reviewed By: vitalybuka, krytarowski
    
    Differential Revision: https://reviews.llvm.org/D55473
    
    llvm-svn: 348770

--- compiler-rt/test/sanitizer_common/TestCases/Posix/sysctl.cc
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/sysctl.cc
@@ -1,6 +1,6 @@
 // RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck %s
 //
-// UNSUPPORTED: linux, darwin, solaris
+// UNSUPPORTED: linux, solaris
 
 #include <sys/param.h>
 #include <sys/types.h>
@@ -48,65 +48,17 @@ void test_sysctlnametomib() {
   printf("sysctlnametomib: '%s' size: '%zu'\n", buf, len);
 }
 
-#if defined(__NetBSD__)
-void test_asysctl() {
-  int mib[] = {CTL_KERN, KERN_OSTYPE};
-  size_t len;
-  char *buf = (char *)asysctl(mib, __arraycount(mib), &len);
-  assert(buf);
-
-  printf("asysctl: '%s' size: '%zu'\n", buf, len);
-
-  free(buf);
-}
-
-void test_asysctlbyname() {
-  size_t len;
-  char *buf = (char *)asysctlbyname("kern.ostype", &len);
-  assert(buf);
-
-  printf("asysctlbyname: '%s' size: '%zu'\n", buf, len);
-
-  free(buf);
-}
-
-void test_sysctlgetmibinfo() {
-  int mib[CTL_MAXNAME];
-  unsigned int mib_len = __arraycount(mib);
-  int rv = sysctlgetmibinfo("kern.ostype", &mib[0], &mib_len, NULL, NULL, NULL,
-                       SYSCTL_VERSION);
-  assert(!rv);
-
-  char buf[100];
-  size_t len = sizeof(buf);
-  rv = sysctl(mib, mib_len, buf, &len, NULL, 0);
-  assert(!rv);
-
-  printf("sysctlgetmibinfo: '%s' size: '%zu'\n", buf, len);
-}
-#endif
-
 int main(void) {
   printf("sysctl\n");
 
   test_sysctl();
   test_sysctlbyname();
   test_sysctlnametomib();
-#if defined(__NetBSD__)
-  test_asysctl();
-  test_asysctlbyname();
-  test_sysctlgetmibinfo();
-#endif
 
   // CHECK: sysctl
   // CHECK: sysctl: '{{.*}}' size: '{{.*}}'
   // CHECK: sysctlbyname: '{{.*}}' size: '{{.*}}'
   // CHECK: sysctlnametomib: '{{.*}}' size: '{{.*}}'
-#if defined(__NetBSD__)
-  // CHECK: asysctl: '{{.*}}' size: '{{.*}}'
-  // CHECK: asysctlbyname: '{{.*}}' size: '{{.*}}'
-  // CHECK: sysctlgetmibinfo: '{{.*}}' size: '{{.*}}'
-#endif
 
   return 0;
 }
