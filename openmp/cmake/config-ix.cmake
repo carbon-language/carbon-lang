e9d4267277068ad46a20cf6001a7077d89244401
Michal Gorny <mgorny@gentoo.org>
2018-12-11 15:39:22 +0000

    [cmake] Use -std=gnu++11 to fix alloca() on NetBSD
    Prefer using '-std=gnu++11' over '-std=c++11' when available, as NetBSD
    exposes the correct alloca() implementation only with gnu* C/C++
    standards.
    
    Differential Revision: https://reviews.llvm.org/D55477
    
    llvm-svn: 348854

--- openmp/cmake/config-ix.cmake
+++ openmp/cmake/config-ix.cmake
@@ -3,4 +3,5 @@ include(CheckCXXCompilerFlag)
 
 check_c_compiler_flag(-Werror OPENMP_HAVE_WERROR_FLAG)
 
+check_cxx_compiler_flag(-std=gnu++11 OPENMP_HAVE_STD_GNUPP11_FLAG)
 check_cxx_compiler_flag(-std=c++11 OPENMP_HAVE_STD_CPP11_FLAG)
