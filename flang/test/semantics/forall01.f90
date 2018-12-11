c6396e0af9df5cddea77cdfe424717ec4ab1c4f6
peter klausler <pklausler@nvidia.com>
2018-12-11 14:40:01 -0800

    [flang] improve test
    Original-commit: flang-compiler/f18@cfcf9dae4d9268c84baaf87ab1bb85832f100f07
    Reviewed-on: https://github.com/flang-compiler/f18/pull/243

--- flang/test/semantics/forall01.f90
+++ flang/test/semantics/forall01.f90
@@ -13,15 +13,15 @@
 ! limitations under the License.
 
 ! RUN: ${F18} -funparse-with-symbols %s 2>&1 | ${FileCheck} %s
-! CHECK: error: 'i' is already declared in this scoping unit
 ! CHECK: Previous declaration of 'i'
-! CHECK: error: 'j' is already declared in this scoping unit
 ! CHECK: Previous declaration of 'j'
 
 subroutine forall
   real :: a(9)
+! ERROR: 'i' is already declared in this scoping unit
   forall (i=1:8, i=1:9)  a(i) = i
   forall (j=1:8)
+! ERROR: 'j' is already declared in this scoping unit
     forall (j=1:9)
     end forall
   end forall
