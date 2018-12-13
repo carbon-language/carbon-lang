a4ee8542599611ba2491bdd8cdc1f56120b90465
David Carlier <devnexen@gmail.com>
2018-12-13 15:05:24 +0000

    [Sanitizer] capsicum further support of the API
    Reviewers: vitalybuka, krytarowski, emaste
    
    Reviewed By: emaste
    
    Differential Revision: https://reviews.llvm.org/D55622
    
    llvm-svn: 349042

--- compiler-rt/test/sanitizer_common/TestCases/FreeBSD/capsicum.cc
+++ compiler-rt/test/sanitizer_common/TestCases/FreeBSD/capsicum.cc
@@ -2,47 +2,61 @@
 
 #include <sys/capsicum.h>
 #include <sys/ioctl.h>
-#include <stdio.h>
-#include <errno.h>
-#include <unistd.h>
-#include <termios.h>
-#include <string.h>
+
 #include <assert.h>
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+#include <termios.h>
+#include <unistd.h>
 
 void test_cap_ioctls() {
-	cap_rights_t rights;
-	unsigned long ncmds[] = {TIOCGETA, TIOCGWINSZ, FIODTYPE};
-	unsigned long rcmds = 0;
-	cap_rights_t *rptr = cap_rights_init(&rights, CAP_IOCTL, CAP_READ);
-	assert(rptr);
+  cap_rights_t rights;
+  unsigned long ncmds[] = {TIOCGETA, TIOCGWINSZ, FIODTYPE};
+  unsigned long rcmds = 0;
+  cap_rights_t *rptr = cap_rights_init(&rights, CAP_IOCTL, CAP_READ);
+  assert(rptr);
 
-	int rv = cap_rights_limit(STDIN_FILENO, &rights);
-	assert(rv == 0);
-	rv = cap_ioctls_limit(STDIN_FILENO, ncmds, 3);
-	assert(rv == 0);
-	ssize_t rz = cap_ioctls_get(STDIN_FILENO, &rcmds, 3);
-	assert(rz == 3);
-	printf("ioctls test: %ld commands authorized\n", rz);
+  int rv = cap_rights_limit(STDIN_FILENO, &rights);
+  assert(rv == 0);
+  rv = cap_ioctls_limit(STDIN_FILENO, ncmds, 3);
+  assert(rv == 0);
+  ssize_t rz = cap_ioctls_get(STDIN_FILENO, &rcmds, 3);
+  assert(rz == 3);
+  printf("ioctls test: %ld commands authorized\n", rz);
 }
 
 void test_cap_rights() {
-	cap_rights_t rights, grights;
-	cap_rights_t *rptr = cap_rights_init(&rights, CAP_IOCTL, CAP_READ);
-	assert(rptr);
+  cap_rights_t rights, little, remove, grights;
+  cap_rights_t *rptr = cap_rights_init(&rights, CAP_IOCTL, CAP_READ);
+  assert(rptr);
+  cap_rights_t *gptr = cap_rights_init(&remove, CAP_IOCTL);
+  assert(gptr);
+  cap_rights_t *sptr = cap_rights_init(&little, CAP_READ);
+  assert(sptr);
+  bool hasit = cap_rights_contains(rptr, sptr);
+  assert(hasit == true);
+  cap_rights_t *pptr = cap_rights_remove(&rights, gptr);
+  hasit = cap_rights_contains(pptr, sptr);
+  assert(hasit == true);
+  cap_rights_t *aptr = cap_rights_merge(&rights, gptr);
+  assert(aptr);
+  bool correct = cap_rights_is_valid(&rights);
+  assert(correct == true);
 
-	int rv = cap_rights_limit(STDIN_FILENO, &rights);
-	assert(rv == 0);
-	rv = cap_rights_get(STDIN_FILENO, &grights);
-	assert(rv == 0);
-	assert(memcmp(&grights, &rights, sizeof(grights)) == 0);
-	printf("rights test: %d\n", rv);
+  int rv = cap_rights_limit(STDIN_FILENO, &rights);
+  assert(rv == 0);
+  rv = cap_rights_get(STDIN_FILENO, &grights);
+  assert(rv == 0);
+  assert(memcmp(&grights, &rights, sizeof(grights)) == 0);
+  printf("rights test: %d\n", rv);
 }
 
 int main(void) {
-	test_cap_ioctls();
-	
-	test_cap_rights();
+  test_cap_ioctls();
 
-	// CHECK: ioctls test: {{.*}} commands authorized
-	// CHECK: rights test: {{.*}}
+  test_cap_rights();
+
+  // CHECK: ioctls test: {{.*}} commands authorized
+  // CHECK: rights test: {{.*}}
 }
