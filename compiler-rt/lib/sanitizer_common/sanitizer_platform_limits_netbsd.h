0bc12879fc7190553a1794a63a2725b86d715b8f
Kamil Rytarowski <n54@gmx.com>
2018-12-13 10:19:00 +0000

    Add a new interceptors for cdbr(3) and cdbw(3) API from NetBSD
    Summary:
    cdb - formats of the constant database.
    
    cdbr, cdbr_open, cdbr_open_mem, cdbr_entries, cdbr_get, cdbr_find,
    cdbr_close - constant database access methods.
    
    cdbw_open, cdbw_put, cdbw_put_data, cdbw_put_key, cdbw_stable_seeder,
    cdbw_output, cdbw_close - creates constant databases.
    
    Add a dedicated test for this API.
    
    Reviewers: vitalybuka, joerg
    
    Reviewed By: vitalybuka
    
    Subscribers: kubamracek, llvm-commits, mgorny, #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D55167
    
    llvm-svn: 349021

--- compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_netbsd.h
+++ compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_netbsd.h
@@ -2256,6 +2256,41 @@ SHA2_EXTERN(512);
 
 extern const int unvis_valid;
 extern const int unvis_validpush;
+
+struct __sanitizer_cdbr {
+  void (*unmap)(void *, void *, uptr);
+  void *cookie;
+  u8 *mmap_base;
+  uptr mmap_size;
+
+  u8 *hash_base;
+  u8 *offset_base;
+  u8 *data_base;
+
+  u32 data_size;
+  u32 entries;
+  u32 entries_index;
+  u32 seed;
+
+  u8 offset_size;
+  u8 index_size;
+
+  u32 entries_m;
+  u32 entries_index_m;
+  u8 entries_s1, entries_s2;
+  u8 entries_index_s1, entries_index_s2;
+};
+
+struct __sanitizer_cdbw {
+  uptr data_counter;
+  uptr data_allocated;
+  uptr data_size;
+  uptr *data_len;
+  void **data_ptr;
+  uptr hash_size;
+  void *hash;
+  uptr key_counter;
+};
 }  // namespace __sanitizer
 
 #define CHECK_TYPE_SIZE(TYPE) \
