077a0aff164a651ac439a035571dfe6af85f0221
Eric Fiselier <eric@efcs.ca>
2018-12-17 18:37:59 +0000

    Unbreak green dragon bots w/o __builtin_launder
    llvm-svn: 349373

--- libcxx/test/std/language.support/support.dynamic/ptr.launder/launder.types.fail.cpp
+++ libcxx/test/std/language.support/support.dynamic/ptr.launder/launder.types.fail.cpp
@@ -29,8 +29,8 @@ int main ()
     (void) std::launder((const          void *) nullptr);
     (void) std::launder((      volatile void *) nullptr);
     (void) std::launder((const volatile void *) nullptr);  // expected-error-re@new:* 4 {{static_assert failed{{.*}} "can't launder cv-void"}}
-    // expected-error@new:* 4 {{void pointer argument to '__builtin_launder' is not allowed}}
+    // expected-error@new:* 0-4 {{void pointer argument to '__builtin_launder' is not allowed}}
 
     (void) std::launder(foo);                              // expected-error-re@new:* 1 {{static_assert failed{{.*}} "can't launder functions"}}
-    // expected-error@new:* {{function pointer argument to '__builtin_launder' is not allowed}}
+    // expected-error@new:* 0-1 {{function pointer argument to '__builtin_launder' is not allowed}}
 }
