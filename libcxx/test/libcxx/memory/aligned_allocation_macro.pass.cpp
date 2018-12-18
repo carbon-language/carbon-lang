afb1d72e7b5952919ee54bd6be264f2d208a091b
Louis Dionne <ldionne@apple.com>
2018-12-18 00:30:15 +0000

    [libcxx] Properly mark aligned allocation macro test as XFAIL on OS X
    This test was initially marked as XFAIL using `XFAIL: macosx10.YY`, and
    was then moved to `UNSUPPORTED: macosx10.YY`. The intent is to mark the
    test as XFAILing when a deployment target older than macosx10.14 is used,
    and the right way to do this is `XFAIL: availability=macosx10.YY`.
    
    llvm-svn: 349426

--- libcxx/test/libcxx/memory/aligned_allocation_macro.pass.cpp
+++ libcxx/test/libcxx/memory/aligned_allocation_macro.pass.cpp
@@ -10,12 +10,12 @@
 // UNSUPPORTED: c++98, c++03, c++11, c++14
 
 // aligned allocation functions are not provided prior to macosx10.13
-// UNSUPPORTED: macosx10.12
-// UNSUPPORTED: macosx10.11
-// UNSUPPORTED: macosx10.10
-// UNSUPPORTED: macosx10.9
-// UNSUPPORTED: macosx10.8
-// UNSUPPORTED: macosx10.7
+// XFAIL: availability=macosx10.12
+// XFAIL: availability=macosx10.11
+// XFAIL: availability=macosx10.10
+// XFAIL: availability=macosx10.9
+// XFAIL: availability=macosx10.8
+// XFAIL: availability=macosx10.7
 
 #include <new>
 
