cdecca032464d7b69104a6a4410b2951c09288bc
Vitaly Buka <vitalybuka@google.com>
2018-12-20 23:25:26 +0000

    Revert "[asan] Disable test on powerpc64be"
    Now the test is passing on that bot. Some incremental build issues?
    
    This reverts commit e00b5a5229ae02088d9f32a4e328eaa08abaf354.
    
    llvm-svn: 349852

--- compiler-rt/test/asan/TestCases/Linux/odr-vtable.cc
+++ compiler-rt/test/asan/TestCases/Linux/odr-vtable.cc
@@ -3,9 +3,6 @@
 // RUN: %clangxx_asan -fno-rtti %s %ld_flags_rpath_exe1 %ld_flags_rpath_exe2 -o %t
 // RUN: %env_asan_opts=fast_unwind_on_malloc=0:detect_odr_violation=2 not %run %t 2>&1 | FileCheck %s
 
-// FIXME: Somehow does not detect ODR on powerpc64be
-// XFAIL: powerpc64
-
 struct XYZ {
   virtual void foo();
 };
