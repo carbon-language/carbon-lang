a643e6449ba284e345ddd35479b8c4f897eed386
Reid Kleckner <rnk@google.com>
2018-12-26 21:04:08 +0000

    [clang-cl] Treat inputs as C++ with /E, like MSVC
    midl invokes the compiler on .idl files with /E. Before this change, we
    would treat unrecognized inputs as object files. Now we pre-process to
    stdout as expected. I checked that MSVC defines __cplusplus when invoked
    this way, so treating the input as C++ seems like the right thing to do.
    
    After this change, I was able to run midl like this with clang-cl:
    $ midl -cpp_cmd clang-cl.exe foo.idl
    
    Things worked for the example IDL file in the Microsoft documentation,
    but beyond that, I don't know if this will work well.
    
    Fixes PR40140
    
    llvm-svn: 350072

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -2057,7 +2057,8 @@ void Driver::BuildInputs(const ToolChain &TC, DerivedArgList &Args,
           Ty = types::TY_C;
         } else {
           // Otherwise lookup by extension.
-          // Fallback is C if invoked as C preprocessor or Object otherwise.
+          // Fallback is C if invoked as C preprocessor, C++ if invoked with
+          // clang-cl /E, or Object otherwise.
           // We use a host hook here because Darwin at least has its own
           // idea of what .s is.
           if (const char *Ext = strrchr(Value, '.'))
@@ -2066,6 +2067,8 @@ void Driver::BuildInputs(const ToolChain &TC, DerivedArgList &Args,
           if (Ty == types::TY_INVALID) {
             if (CCCIsCPP())
               Ty = types::TY_C;
+            else if (IsCLMode() && Args.hasArgNoClaim(options::OPT_E))
+              Ty = types::TY_CXX;
             else
               Ty = types::TY_Object;
           }
