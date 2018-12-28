f11fcc046ecf9891eae5f6c3d1274ef84e6cb034
David Carlier <devnexen@gmail.com>
2018-12-28 16:04:28 +0000

    [Sanitizer] Intercept arc4random_buf / arc4random_addrandom on FreeBSD/NetBSD
    - Disabled on purpose on Android and Darwin platform (for now).
    - Darwin supports it, would need interception in its specific code before enabling it.
    - Linux does not support it but only via third party library.
    - Android supports it via bionic however it is known to have issue with older versions of the implementations. Can be enabled by an Android committer later on if necessary once there is more 'certainity'/been more tested.
    
    Reviewers: krytarowski, vitalybuka
    
    Reviewed By: krytarowski
    
    Differential Revision: https://reviews.llvm.org/D56125
    
    llvm-svn: 350123

---
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/arc4random.cc
@@ -0,0 +1,62 @@
+// RUN: %clangxx -O0 -g %s -o %t && %run %t 2>&1 | FileCheck %s
+//
+// UNSUPPORTED: linux, darwin, solaris
+
+#include <cstdlib>
+#include <ctime>
+#include <cstdio>
+#include <inttypes.h>
+
+void print_buf(unsigned char *buf, size_t buflen) {
+  printf("buf '");
+  for (auto i = 0; i < buflen; i ++)
+    printf("%" PRIx8, buf[i]);
+  printf("'\n");
+}
+
+void test_seed() {
+  time_t now = ::time(nullptr);
+  arc4random_addrandom((unsigned char *)&now, sizeof(now));
+}
+
+void test_arc4random() {
+  printf("test_arc4random\n");
+  auto i = arc4random();
+  print_buf((unsigned char *)&i, sizeof(i));
+}
+
+void test_arc4random_uniform() {
+  printf("test_arc4random_uniform\n");
+  auto i = arc4random_uniform(1024);
+  print_buf((unsigned char *)&i, sizeof(i));
+}
+
+void test_arc4random_buf10() {
+  printf("test_arc4random_buf10\n");
+  char buf[10];
+  arc4random_stir();
+  arc4random_buf(buf, sizeof(buf));
+  print_buf((unsigned char *)buf, sizeof(buf));
+}
+
+void test_arc4random_buf256() {
+  printf("test_arc4random_buf256\n");
+  char buf[256];
+  arc4random_stir();
+  arc4random_buf(buf, sizeof(buf));
+  print_buf((unsigned char *)buf, sizeof(buf));
+}
+
+int main(void) {
+  test_seed();
+  test_arc4random();
+  test_arc4random_buf10();
+  test_arc4random_buf256();
+  return 0;
+  // CHECK: test_arc4random
+  // CHECK: buf '{{.*}}'
+  // CHECK: test_arc4random_buf10
+  // CHECK: buf '{{.*}}'
+  // CHECK: test_arc4random_buf256
+  // CHECK: buf '{{.*}}'
+}
