90004149ccf4dba77af4db8ee41c15ed60e3a062
David Bolvansky <david.bolvansky@gmail.com>
2018-12-30 13:18:17 +0000

    [NFC] Fixed extra semicolon warning -This line, and those below, will be ignored--
    M    lib/Support/Error.cpp
    
    llvm-svn: 350162

--- llvm/lib/Support/Error.cpp
+++ llvm/lib/Support/Error.cpp
@@ -54,7 +54,7 @@ namespace llvm {
 void ErrorInfoBase::anchor() {}
 char ErrorInfoBase::ID = 0;
 char ErrorList::ID = 0;
-void ECError::anchor() {};
+void ECError::anchor() {}
 char ECError::ID = 0;
 char StringError::ID = 0;
 char FileError::ID = 0;
