7343e24f78c82cd7923d1e595745195846cdce5b
Michal Gorny <mgorny@gentoo.org>
2018-12-31 13:48:12 +0000

    [test] Fix propagating HOME envvar to unittests
    Propagate HOME environment variable to unittests.  This is necessary
    to fix test failures resulting from pw_home pointing to a non-existing
    directory while being overriden with HOME.  Apparently Gentoo users
    hit this sometimes when they override build directory for Portage.
    
    Original bug report: https://bugs.gentoo.org/674088
    
    Differential Revision: https://reviews.llvm.org/D56162
    
    llvm-svn: 350175

--- llvm/test/Unit/lit.cfg.py
+++ llvm/test/Unit/lit.cfg.py
@@ -31,6 +31,11 @@ if 'TMP' in os.environ:
 if 'TEMP' in os.environ:
     config.environment['TEMP'] = os.environ['TEMP']
 
+# Propagate HOME as it can be used to override incorrect homedir in passwd
+# that causes the tests to fail.
+if 'HOME' in os.environ:
+    config.environment['HOME'] = os.environ['HOME']
+
 # Propagate path to symbolizer for ASan/MSan.
 for symbolizer in ['ASAN_SYMBOLIZER_PATH', 'MSAN_SYMBOLIZER_PATH']:
     if symbolizer in os.environ:
