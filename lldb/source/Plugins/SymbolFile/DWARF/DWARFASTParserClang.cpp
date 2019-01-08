6ea33bc19bc21040e5b5bab14cb57cfe8e196e64
Adrian Prantl <aprantl@apple.com>
2019-01-08 20:48:40 +0000

    Convert to LLDB coding style (NFC)
    llvm-svn: 350651

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFASTParserClang.cpp
@@ -125,7 +125,7 @@ ClangASTImporter &DWARFASTParserClang::GetClangASTImporter() {
 }
 
 /// Detect a forward declaration that is nested in a DW_TAG_module.
-static bool isClangModuleFwdDecl(const DWARFDIE &Die) {
+static bool IsClangModuleFwdDecl(const DWARFDIE &Die) {
   if (!Die.GetAttributeValueAsUnsigned(DW_AT_declaration, 0))
     return false;
   auto Parent = Die.GetParent();
@@ -151,17 +151,17 @@ TypeSP DWARFASTParserClang::ParseTypeFromDWO(const DWARFDIE &die, Log *log) {
 
   if (!dwo_module_sp->GetSymbolVendor()->FindTypes(decl_context, true,
                                                    dwo_types)) {
-    if (!isClangModuleFwdDecl(die))
+    if (!IsClangModuleFwdDecl(die))
       return TypeSP();
 
     // Since this this type is defined in one of the Clang modules imported by
     // this symbol file, search all of them.
-    auto *SymFile = die.GetCU()->GetSymbolFileDWARF();
-    for (const auto &NameModule : SymFile->getExternalTypeModules()) {
-      if (!NameModule.second)
+    auto *sym_file = die.GetCU()->GetSymbolFileDWARF();
+    for (const auto &name_module : sym_file->getExternalTypeModules()) {
+      if (!name_module.second)
         continue;
-      SymbolVendor *SymVendor = NameModule.second->GetSymbolVendor();
-      if (SymVendor->FindTypes(decl_context, true, dwo_types))
+      SymbolVendor *sym_vendor = name_module.second->GetSymbolVendor();
+      if (sym_vendor->FindTypes(decl_context, true, dwo_types))
         break;
     }
   }
