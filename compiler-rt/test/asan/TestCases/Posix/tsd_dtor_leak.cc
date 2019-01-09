5e53818be5110d7d14497d9b2e15a9cbfc2290be
David Carlier <devnexen@gmail.com>
2019-01-09 17:14:57 +0000

    [asan] Disable TSD dtor leak unit tests on FreeBSD x86 64
    - Assertion fails in the third iteration.
    
    Reviewers: krytarowski
    
    Reviewed By: krytarowski
    
    Differential Revision: https://reviews.llvm.org/D56497
    
    llvm-svn: 350744

--- compiler-rt/test/asan/TestCases/Posix/tsd_dtor_leak.cc
+++ compiler-rt/test/asan/TestCases/Posix/tsd_dtor_leak.cc
@@ -3,6 +3,8 @@
 // RUN: %clangxx_asan -O1 %s -pthread -o %t
 // RUN: %env_asan_opts=quarantine_size_mb=0 %run %t
 // XFAIL: x86_64-netbsd
+// Assertion fails
+// XFAIL: x86_64-freebsd
 #include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
