6552b298673017a5ad9297369d59b9fac94cdbfc
George Rimar <grimar@accesssoftek.com>
2019-01-10 16:26:05 +0000

    [LLD][ELF] - Fix tests after r350840.
    D56076 (r350840) changed the llvm-objdump output.
    
    This is a follow up commit to fix LLD test cases.
    
    llvm-svn: 350842

--- lld/test/ELF/wrap-no-real.s
+++ lld/test/ELF/wrap-no-real.s
@@ -18,8 +18,7 @@
 // RUN: llvm-objdump -t %t | FileCheck -check-prefix=SYM %s
 
 
-// SYM:      0000000000000000  *UND*     00000000
-// SYM-NEXT: 0000000000202000  .dynamic  00000000 .hidden _DYNAMIC
+// SYM:      0000000000202000  .dynamic  00000000 .hidden _DYNAMIC
 // SYM-NEXT: 0000000000011000  *ABS*     00000000 __real_foo
 // SYM-NEXT: 0000000000011010  *ABS*     00000000 __wrap_foo
 // SYM-NEXT: 0000000000201000  .text     00000000 _start
