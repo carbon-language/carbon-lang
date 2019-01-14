add3080cb95d1ccd3ec41c1a1d1c5f21fe32060a
Roman Lebedev <lebedev.ri@gmail.com>
2019-01-14 21:02:25 +0000

    [compiler-rt] UBSan: Disable 3 of the new alignment assumption tests on android.
    Once again, just like with r338296, these tests seem to only have
    failed sanitizer-x86_64-linux-android, so let's just disable them,
    since that seems like the pre-established practice here..
    
    To be noted, they failed on some configs there, but not all,
    so it is not XFAIL.
    
    llvm-svn: 351119

--- compiler-rt/test/ubsan/TestCases/Pointer/alignment-assumption-attribute-alloc_align-on-function.cpp
+++ compiler-rt/test/ubsan/TestCases/Pointer/alignment-assumption-attribute-alloc_align-on-function.cpp
@@ -1,3 +1,8 @@
+// FIXME: https://code.google.com/p/address-sanitizer/issues/detail?id=316
+// I'm not sure this is actually *that* issue, but this seems oddly similar to the other XFAIL'ed cases.
+// UNSUPPORTED: android
+// UNSUPPORTED: ios
+
 // RUN: %clang   -x c   -fsanitize=alignment -O0 %s -o %t && %run %t 2>&1 | FileCheck %s --implicit-check-not=" assumption " --implicit-check-not="note:" --implicit-check-not="error:"
 // RUN: %clang   -x c   -fsanitize=alignment -O1 %s -o %t && %run %t 2>&1 | FileCheck %s --implicit-check-not=" assumption " --implicit-check-not="note:" --implicit-check-not="error:"
 // RUN: %clang   -x c   -fsanitize=alignment -O2 %s -o %t && %run %t 2>&1 | FileCheck %s --implicit-check-not=" assumption " --implicit-check-not="note:" --implicit-check-not="error:"
