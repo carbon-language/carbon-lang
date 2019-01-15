1c268554bacc004f4a0741e143f177a85f36a822
Jonathan Peyton <jonathan.l.peyton@intel.com>
2019-01-15 19:08:26 +0000

    [OpenMP][Cmake] Allowed OpenMP testing detect test compiler with same generator
    Fix ninja build detect test compiler failed under windows.
    
    Patch by Peiyuan Song
    
    Differential Revision: https://reviews.llvm.org/D53479
    
    llvm-svn: 351223

--- openmp/cmake/OpenMPTesting.cmake
+++ openmp/cmake/OpenMPTesting.cmake
@@ -99,7 +99,7 @@ if (${OPENMP_STANDALONE_BUILD})
   # project is built which is too late for detecting the compiler...
   file(MAKE_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/DetectTestCompiler)
   execute_process(
-    COMMAND ${CMAKE_COMMAND} ${CMAKE_CURRENT_LIST_DIR}/DetectTestCompiler
+    COMMAND ${CMAKE_COMMAND} -G${CMAKE_GENERATOR} ${CMAKE_CURRENT_LIST_DIR}/DetectTestCompiler
       -DCMAKE_C_COMPILER=${OPENMP_TEST_C_COMPILER}
       -DCMAKE_CXX_COMPILER=${OPENMP_TEST_CXX_COMPILER}
     WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}/DetectTestCompiler
