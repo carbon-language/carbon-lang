e8374614c97b62229daf2750d095e279fd848b15
Peter Smith <peter.smith@linaro.org>
2019-01-16 13:24:02 +0000

    [ELF][AArch64] Add R_AARCH64_PLT_PAGE_PC to isRelExpr
    As a follow on to D56666 (r351186) there is a case when taking the address
    of an ifunc when linking -pie that can generate a spurious can't create
    dynamic relocation R_AARCH64_ADR_PREL_PG_HI21 against symbol in readonly
    segment. Specifically the case is where the ifunc is in the same
    translation unit as the address taker, so given -fpie the compiler knows
    the ifunc is defined in the executable so it can use a non-got-generating
    relocation.
    
    The error message is due to R_AARCH64_PLT_PAGE_PC not being added to
    isRelExpr, its non PLT equivalent R_AARCH64_PAGE_PC is already in
    isRelExpr.
    
    Differential Revision: https://reviews.llvm.org/D56724
    
    llvm-svn: 351335

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -356,7 +356,7 @@ static bool needsGot(RelExpr Expr) {
 static bool isRelExpr(RelExpr Expr) {
   return isRelExprOneOf<R_PC, R_GOTREL, R_GOTREL_FROM_END, R_MIPS_GOTREL,
                         R_PPC_CALL, R_PPC_CALL_PLT, R_AARCH64_PAGE_PC,
-                        R_RELAX_GOT_PC>(Expr);
+                        R_AARCH64_PLT_PAGE_PC, R_RELAX_GOT_PC>(Expr);
 }
 
 // Returns true if a given relocation can be computed at link-time.
