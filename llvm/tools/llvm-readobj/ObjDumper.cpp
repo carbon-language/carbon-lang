2623870441c6f57c960536c9f8192f2a4234fe40
Sid Manning <sidneym@codeaurora.org>
2019-01-16 16:17:19 +0000

    [llvm-readobj] Set correct offset when dumping hex section output.
    Differential Revision: https://reviews.llvm.org/D56369
    
    llvm-svn: 351356

--- llvm/tools/llvm-readobj/ObjDumper.cpp
+++ llvm/tools/llvm-readobj/ObjDumper.cpp
@@ -117,7 +117,8 @@ void ObjDumper::printSectionAsHex(const object::ObjectFile *Obj,
     uint8_t i;
     uint8_t k;
 
-    W.startLine() << format_hex(SecPtr - SecContent, 10);
+    W.startLine() << format_hex(Section.getAddress() + (SecPtr - SecContent),
+                                10);
     W.startLine() << ' ';
     for (i = 0; TmpSecPtr < SecEnd && i < 4; ++i) {
       for (k = 0; TmpSecPtr < SecEnd && k < 4; k++, TmpSecPtr++) {
