d108bf85b09e3f1355fea866d90e3dd2266d67b5
Eric Fiselier <eric@efcs.ca>
2019-01-16 01:51:12 +0000

    Move internal usages of `alignof`/`__alignof` to use `_LIBCPP_ALIGNOF`.
    Summary:
    Starting in Clang 8.0 and GCC 8.0, `alignof` and `__alignof` return different values in same cases. Specifically `alignof` and `_Alignof` return the minimum alignment for a type, where as `__alignof` returns the preferred alignment. libc++ currently uses `__alignof` but means to use `alignof`. See  llvm.org/PR39713
    
    This patch introduces the macro `_LIBCPP_ALIGNOF` so we can control which spelling gets used.
    
    This patch does not introduce any ABI guard to provide the old behavior with newer compilers. However, if we decide that is needed, this patch makes it trivial to implement.
    
    I think we should commit this change immediately, and decide what we want to do about the ABI afterwards. 
    
    Reviewers: ldionne, EricWF
    
    Reviewed By: ldionne, EricWF
    
    Subscribers: jyknight, christof, libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D54814
    
    llvm-svn: 351289

--- libcxx/test/std/utilities/meta/meta.unary.prop.query/alignment_of.pass.cpp
+++ libcxx/test/std/utilities/meta/meta.unary.prop.query/alignment_of.pass.cpp
@@ -19,6 +19,9 @@
 template <class T, unsigned A>
 void test_alignment_of()
 {
+    const unsigned AlignofResult = TEST_ALIGNOF(T);
+    static_assert( AlignofResult == A, "Golden value does not match result of alignof keyword");
+    static_assert( std::alignment_of<T>::value == AlignofResult, "");
     static_assert( std::alignment_of<T>::value == A, "");
     static_assert( std::alignment_of<const T>::value == A, "");
     static_assert( std::alignment_of<volatile T>::value == A, "");
@@ -45,7 +48,10 @@ int main()
     test_alignment_of<const int*, sizeof(intptr_t)>();
     test_alignment_of<char[3], 1>();
     test_alignment_of<int, 4>();
-    test_alignment_of<double, 8>();
+    // The test case below is a hack. It's hard to detect what golden value
+    // we should expect. In most cases it should be 8. But in i386 builds
+    // with Clang >= 8 or GCC >= 8 the value is '4'.
+    test_alignment_of<double, TEST_ALIGNOF(double)>();
 #if (defined(__ppc__) && !defined(__ppc64__))
     test_alignment_of<bool, 4>();   // 32-bit PPC has four byte bool
 #else
