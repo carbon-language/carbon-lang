f2e25e708c49fa79a2dbe8b68f3ce4873a489739
Sam McCall <sam.mccall@gmail.com>
2019-01-16 09:41:26 +0000

    Reapply [Tooling] Make clang-tool find libc++ dir on mac when running on a file without compilation database.
    This reverts commit r351282, and re-lands r351222 and r351229 with the
    use-after-free fixed.
    
    llvm-svn: 351316

---
+++ clang/test/Tooling/clang-check-mac-libcxx-fixed-compilation-db.cpp
@@ -0,0 +1,16 @@
+// Clang on MacOS can find libc++ living beside the installed compiler.
+// This test makes sure our libTooling-based tools emulate this properly with
+// fixed compilation database.
+//
+// RUN: rm -rf %t
+// RUN: mkdir %t
+//
+// Install the mock libc++ (simulates the libc++ directory structure).
+// RUN: cp -r %S/Inputs/mock-libcxx %t/
+//
+// RUN: cp $(which clang-check) %t/mock-libcxx/bin/
+// RUN: cp "%s" "%t/test.cpp"
+// RUN: %t/mock-libcxx/bin/clang-check -p "%t" "%t/test.cpp" -- -stdlib=libc++
+
+#include <mock_vector>
+vector v;
