ea512d629ab641d76e760afcfb6bd007e81fd179
Brad Smith <brad@comstyle.com>
2019-01-18 01:36:58 +0000

    Use llvm::VersionTuple instead of manual version marshalling
    llvm-svn: 351504

--- lldb/source/Host/openbsd/HostInfoOpenBSD.cpp
+++ lldb/source/Host/openbsd/HostInfoOpenBSD.cpp
@@ -17,16 +17,17 @@
 
 using namespace lldb_private;
 
-bool HostInfoOpenBSD::GetOSVersion(uint32_t &major, uint32_t &minor,
-                                   uint32_t &update) {
+llvm::VersionTuple HostInfoOpenBSD::GetOSVersion() {
   struct utsname un;
 
   ::memset(&un, 0, sizeof(utsname));
   if (uname(&un) < 0)
-    return false;
+    return llvm::VersionTuple();
 
-  int status = sscanf(un.release, "%u.%u", &major, &minor);
-  return status == 2;
+  unsigned major, minor;
+  if (2 == sscanf(un.release, "%u.%u", &major, &minor))
+    return llvm::VersionTuple(major, minor);
+  return llvm::VersionTuple();
 }
 
 bool HostInfoOpenBSD::GetOSBuildString(std::string &s) {
