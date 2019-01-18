fbca7094c934eb5c5f2746fb4f83e328ffabdc50
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-01-18 15:57:59 +0000

    [LTO] Change test/tools/lto/no-bitcode.s requirement from arm to aarch64
    Set the test to properly require aarch64 instead of arm. Otherwise, this test fails with LLVM_TARGETS_TO_BUILD='ARM;X86'
    
    bin/llvm-mc: : error: unable to get target for 'arm64-apple-ios7.0.0'
    
    Committed on behalf of @easyaspi314 (Devin)
    
    Differential Revision: https://reviews.llvm.org/D56472
    
    llvm-svn: 351560

--- llvm/test/tools/lto/no-bitcode.s
+++ llvm/test/tools/lto/no-bitcode.s
@@ -1,4 +1,4 @@
-; REQUIRES: arm-registered-target
+; REQUIRES: aarch64-registered-target
 ; libLTO.dylib shouldn't assert on invalid inputs.
 ; RUN: llvm-mc -triple=arm64-apple-ios7.0.0 -filetype=obj -o %t.o
 ; RUN: llvm-ar r %t.a %t.o
