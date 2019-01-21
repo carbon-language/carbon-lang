4d54038279cd0f667d6acf6a85de19a2bb9b72ce
Michal Gorny <mgorny@gentoo.org>
2019-01-21 17:05:43 +0000

    [test] Pass -ccc-install-dir in mac compilation db test
    Pass -ccc-install-dir explicitly as the compilation database code does
    not pass argv[0] to getMainExecutable(), while some systems require it
    to return the correct path.  Since the relevant code is apparently only
    applicable to Darwin, just pass correct -ccc-install-dir to make
    the tests pass on *BSD systems.
    
    Differential Revision: https://reviews.llvm.org/D56976
    
    llvm-svn: 351752

--- clang/test/Tooling/clang-check-mac-libcxx-fixed-compilation-db.cpp
+++ clang/test/Tooling/clang-check-mac-libcxx-fixed-compilation-db.cpp
@@ -10,6 +10,11 @@
 //
 // RUN: cp clang-check %t/mock-libcxx/bin/
 // RUN: cp %s %t/test.cpp
-// RUN: "%t/mock-libcxx/bin/clang-check" -p %t %t/test.cpp -- -stdlib=libc++ -target x86_64-apple-darwin
+// RUN: "%t/mock-libcxx/bin/clang-check" -p %t %t/test.cpp -- \
+// RUN:     -stdlib=libc++ -target x86_64-apple-darwin \
+// RUN:     -ccc-install-dir %t/mock-libcxx/bin
+//
+// ^ -ccc-install-dir passed to unbreak tests on *BSD where
+//   getMainExecutable() relies on real argv[0] being passed
 #include <mock_vector>
 vector v;
