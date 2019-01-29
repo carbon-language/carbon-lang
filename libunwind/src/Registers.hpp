d27bec48542e9638ff41033ad714e90207b53c68
Martin Storsjo <martin@martin.st>
2019-01-29 09:00:32 +0000

    Don't define unw_fpreg_t to uint64_t for __ARM_DWARF_EH__
    The existing typedef of unw_fpreg_t to uint64_t might work and be
    correct for the ARM_EHABI case, but for dwarf, some cases in e.g.
    DwarfInstructions.hpp convert between double and unw_fpreg_t.
    
    When converting implicitly between double and unw_fpreg_t (uint64_t),
    the values get interpreted as integers and converted to float and vice
    versa, while the correct thing would be to keep the same bit pattern.
    
    Avoid the whole issue by using the same definition of unw_fpreg_t
    as all other architectures, when using dwarf unwinding on ARM.
    
    Change assembler functions to take a void pointer instead of
    unw_fpreg_t pointer, to avoid having a different mangled symbol name
    depending on the actual value of this typedef.
    
    Differential Revision: https://reviews.llvm.org/D57001
    
    llvm-svn: 352461

--- libunwind/src/Registers.hpp
+++ libunwind/src/Registers.hpp
@@ -2103,16 +2103,16 @@ private:
     uint32_t __pc;    // Program counter r15
   };
 
-  static void saveVFPWithFSTMD(unw_fpreg_t*);
-  static void saveVFPWithFSTMX(unw_fpreg_t*);
-  static void saveVFPv3(unw_fpreg_t*);
-  static void restoreVFPWithFLDMD(unw_fpreg_t*);
-  static void restoreVFPWithFLDMX(unw_fpreg_t*);
-  static void restoreVFPv3(unw_fpreg_t*);
+  static void saveVFPWithFSTMD(void*);
+  static void saveVFPWithFSTMX(void*);
+  static void saveVFPv3(void*);
+  static void restoreVFPWithFLDMD(void*);
+  static void restoreVFPWithFLDMX(void*);
+  static void restoreVFPv3(void*);
 #if defined(__ARM_WMMX)
-  static void saveiWMMX(unw_fpreg_t*);
+  static void saveiWMMX(void*);
   static void saveiWMMXControl(uint32_t*);
-  static void restoreiWMMX(unw_fpreg_t*);
+  static void restoreiWMMX(void*);
   static void restoreiWMMXControl(uint32_t*);
 #endif
   void restoreCoreAndJumpTo();
