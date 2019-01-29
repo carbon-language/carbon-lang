33c9d9a9fc6338c3fefd3f0b36eeaf31843b6412
Paul Robinson <paul.robinson@sony.com>
2019-01-29 20:57:50 +0000

    [cc1as] Test that -g of empty .s file does something sensible.
    Depends on LLVM r352541.
    
    llvm-svn: 352542

---
+++ clang/test/Misc/cc1as-asm-debug.s
@@ -0,0 +1,11 @@
+// Run cc1as with debug on empty file. Needs a known name so we can check it.
+// REQUIRES: x86-registered-target
+// RUN: rm -rf %t && mkdir -p %t
+// RUN: cp %s %t/comment.s
+// RUN: %clang -cc1as -triple x86_64-linux-gnu -filetype asm -debug-info-kind=limited -dwarf-version=4 %t/comment.s | FileCheck %s
+// RUN: %clang -cc1as -triple x86_64-linux-gnu -filetype asm -debug-info-kind=limited -dwarf-version=5 %t/comment.s | FileCheck %s
+// Asm output actually emits the .section directives twice.
+// CHECK: {{\.}}section .debug_info
+// CHECK: {{\.}}section .debug_info
+// CHECK-NOT: {{\.}}section
+// CHECK: .ascii "comment.s"
