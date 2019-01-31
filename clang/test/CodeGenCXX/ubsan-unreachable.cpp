a22c72ca8ff55a70e38ea0d40929a48324e5b8a4
Eric Liu <ioeric@google.com>
2019-01-31 14:20:02 +0000

    Revert "[Sanitizers] UBSan unreachable incompatible with ASan in the presence of `noreturn` calls"
    This reverts commit r352690. This causes clang to crash. Sent reproducer to the
    author in the orginal commit.
    
    llvm-svn: 352755

--- clang/test/CodeGenCXX/ubsan-unreachable.cpp
+++ clang/test/CodeGenCXX/ubsan-unreachable.cpp
@@ -1,37 +1,39 @@
 // RUN: %clang_cc1 -triple x86_64-apple-darwin10 -emit-llvm -o - %s -fsanitize=unreachable | FileCheck %s
 
-void abort() __attribute__((noreturn));
+extern void __attribute__((noreturn)) abort();
 
-// CHECK-LABEL: define void @_Z14calls_noreturnv()
+// CHECK-LABEL: define void @_Z14calls_noreturnv
 void calls_noreturn() {
-  // Check absence ([^#]*) of call site attributes (including noreturn)
-  // CHECK: call void @_Z5abortv(){{[^#]*}}
   abort();
+
+  // Check that there are no attributes on the call site.
+  // CHECK-NOT: call void @_Z5abortv{{.*}}#
 
   // CHECK: __ubsan_handle_builtin_unreachable
   // CHECK: unreachable
 }
 
 struct A {
-  // CHECK: declare void @_Z5abortv() [[EXTERN_FN_ATTR:#[0-9]+]]
+  // CHECK: declare void @_Z5abortv{{.*}} [[ABORT_ATTR:#[0-9]+]]
 
   // CHECK-LABEL: define linkonce_odr void @_ZN1A5call1Ev
   void call1() {
-    // CHECK: call void @_ZN1A16does_not_return2Ev({{.*}}){{[^#]*}}
+    // CHECK-NOT: call void @_ZN1A16does_not_return2Ev{{.*}}#
     does_not_return2();
 
     // CHECK: __ubsan_handle_builtin_unreachable
     // CHECK: unreachable
   }
 
-  // Test static members. Checks are below after `struct A` scope ends.
-  static void does_not_return1() __attribute__((noreturn)) {
+  // Test static members.
+  static void __attribute__((noreturn)) does_not_return1() {
+    // CHECK-NOT: call void @_Z5abortv{{.*}}#
     abort();
   }
 
   // CHECK-LABEL: define linkonce_odr void @_ZN1A5call2Ev
   void call2() {
-    // CHECK: call void @_ZN1A16does_not_return1Ev(){{[^#]*}}
+    // CHECK-NOT: call void @_ZN1A16does_not_return1Ev{{.*}}#
     does_not_return1();
 
     // CHECK: __ubsan_handle_builtin_unreachable
@@ -39,23 +41,23 @@ struct A {
   }
 
   // Test calls through pointers to non-static member functions.
-  typedef void (A::*MemFn)() __attribute__((noreturn));
+  typedef void __attribute__((noreturn)) (A::*MemFn)();
 
   // CHECK-LABEL: define linkonce_odr void @_ZN1A5call3Ev
   void call3() {
     MemFn MF = &A::does_not_return2;
-    // CHECK: call void %{{[0-9]+\(.*}}){{[^#]*}}
     (this->*MF)();
 
+    // CHECK-NOT: call void %{{.*}}#
     // CHECK: __ubsan_handle_builtin_unreachable
     // CHECK: unreachable
   }
 
   // Test regular members.
   // CHECK-LABEL: define linkonce_odr void @_ZN1A16does_not_return2Ev({{.*}})
-  // CHECK-SAME: [[USER_FN_ATTR:#[0-9]+]]
-  void does_not_return2() __attribute__((noreturn)) {
-    // CHECK: call void @_Z5abortv(){{[^#]*}}
+  // CHECK-SAME: [[DOES_NOT_RETURN_ATTR:#[0-9]+]]
+  void __attribute__((noreturn)) does_not_return2() {
+    // CHECK-NOT: call void @_Z5abortv(){{.*}}#
     abort();
 
     // CHECK: call void @__ubsan_handle_builtin_unreachable
@@ -66,9 +68,7 @@ struct A {
   }
 };
 
-// CHECK-LABEL: define linkonce_odr void @_ZN1A16does_not_return1Ev()
-// CHECK-SAME: [[USER_FN_ATTR]]
-// CHECK: call void @_Z5abortv(){{[^#]*}}
+// CHECK: define linkonce_odr void @_ZN1A16does_not_return1Ev() [[DOES_NOT_RETURN_ATTR]]
 
 void force_irgen() {
   A a;
@@ -77,7 +77,5 @@ void force_irgen() {
   a.call3();
 }
 
-// `noreturn` should be removed from functions and call sites
-// CHECK-LABEL: attributes
-// CHECK-NOT: [[USER_FN_ATTR]] = { {{.*noreturn.*}} }
-// CHECK-NOT: [[EXTERN_FN_ATTR]] = { {{.*noreturn.*}} }
+// CHECK-NOT: [[ABORT_ATTR]] = {{[^}]+}}noreturn
+// CHECK-NOT: [[DOES_NOT_RETURN_ATTR]] = {{[^}]+}}noreturn
