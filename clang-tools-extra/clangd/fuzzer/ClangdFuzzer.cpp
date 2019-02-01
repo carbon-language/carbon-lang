554644e5128a539f465444f837072d361feb82c7
Sam McCall <sam.mccall@gmail.com>
2019-02-01 11:09:06 +0000

    [clangd] Unbreak fuzzer target
    llvm-svn: 352857

--- clang-tools-extra/clangd/fuzzer/ClangdFuzzer.cpp
+++ clang-tools-extra/clangd/fuzzer/ClangdFuzzer.cpp
@@ -15,6 +15,7 @@
 #include "ClangdLSPServer.h"
 #include "ClangdServer.h"
 #include "CodeComplete.h"
+#include "FSProvider.h"
 #include <cstdio>
 #include <sstream>
 
@@ -29,12 +30,13 @@ extern "C" int LLVMFuzzerTestOneInput(uint8_t *data, size_t size) {
   auto Transport = newJSONTransport(In, llvm::nulls(),
                                     /*InMirror=*/nullptr, /*Pretty=*/false,
                                     /*Style=*/JSONStreamStyle::Standard);
+  RealFileSystemProvider FS;
   CodeCompleteOptions CCOpts;
   CCOpts.EnableSnippets = false;
   ClangdServer::Options Opts;
 
   // Initialize and run ClangdLSPServer.
-  ClangdLSPServer LSPServer(*Transport, CCOpts, llvm::None, false, Opts);
+  ClangdLSPServer LSPServer(*Transport, FS, CCOpts, llvm::None, false, Opts);
   LSPServer.run();
   return 0;
 }
