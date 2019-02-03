4a0a64ac1d7420889053e81b67da512765a2e2c8
David Zarzycki <dave@znu.io>
2019-02-03 15:49:11 +0000

    Hot fix two test regressions (%T vs %t)
    Different Unix "errno" values are returned for the following scenarios:
    
    $ echo test > /tmp/existingFile/impossibleDir/impossibleFile
    "Not a directory"
    $ echo test > /tmp/nonexistentDir/impossibleFile
    "No such file or directory"
    
    This fixes the regression introduced by r352971 / D57592.
    
    llvm-svn: 352996

--- clang/test/Frontend/output-failures.c
+++ clang/test/Frontend/output-failures.c
@@ -1,4 +1,4 @@
-// RUN: not %clang_cc1 -emit-llvm -o %t/doesnotexist/somename %s 2> %t
+// RUN: not %clang_cc1 -emit-llvm -o %t.doesnotexist/somename %s 2> %t
 // RUN: FileCheck -check-prefix=OUTPUTFAIL -input-file=%t %s
 
 // OUTPUTFAIL: error: unable to open output file '{{.*}}doesnotexist{{.}}somename': '{{[nN]}}o such file or directory'
