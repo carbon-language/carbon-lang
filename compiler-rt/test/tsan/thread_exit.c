17132b62e0379af4840ef9ca9f672ef37b4b866e
Dmitry Vyukov <dvyukov@google.com>
2019-02-07 10:46:02 +0000

    tsan: Implement pthread_exit() interceptor for Thread sanitizer
    This change is preparation for fiber support.
    
    Author: yuri (Yuri Per)
    Reviewed in: https://reviews.llvm.org/D57876
    Context: https://reviews.llvm.org/D54889
    
    llvm-svn: 353385

---
+++ compiler-rt/test/tsan/thread_exit.c
@@ -0,0 +1,25 @@
+// RUN: %clang_tsan -O1 %s -o %t && %run %t 2>&1 | FileCheck %s
+#include "test.h"
+
+int var;
+
+void *Thread(void *x) {
+  pthread_exit(&var);
+  return 0;
+}
+
+int main() {
+  pthread_t t;
+  pthread_create(&t, 0, Thread, 0);
+  void *retval = 0;
+  pthread_join(t, &retval);
+  if (retval != &var) {
+    fprintf(stderr, "Unexpected return value\n");
+    exit(1);
+  }
+  fprintf(stderr, "PASS\n");
+  return 0;
+}
+
+// CHECK-NOT: WARNING: ThreadSanitizer:
+// CHECK: PASS
