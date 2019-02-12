a39eb26bd3f1e9fb5eacc755a8a31f4081682f0e
Anton Korobeynikov <anton@korobeynikov.info>
2019-02-12 20:46:00 +0000

    Disable PIC/PIE for MSP430 target
    Relocatable code generation is meaningless on MSP430, as the platform is too small to use shared libraries.
    
    Patch by Dmitry Mikushev!
    
    Differential Revision: https://reviews.llvm.org/D56927
    
    llvm-svn: 353877

--- clang/lib/Driver/ToolChains/MSP430.h
+++ clang/lib/Driver/ToolChains/MSP430.h
@@ -36,6 +36,10 @@ public:
                              llvm::opt::ArgStringList &CC1Args,
                              Action::OffloadKind) const override;
 
+  bool isPICDefault() const override { return false; }
+  bool isPIEDefault() const override { return false; }
+  bool isPICDefaultForced() const override { return true; }
+
 protected:
   Tool *buildLinker() const override;
 
