9ebc9dbd3c7d8ce5fb35d3d166279b39808dac0b
Eric Fiselier <eric@efcs.ca>
2019-02-12 00:05:14 +0000

    Don't declare fenv.h functions when they're a macro.
    libc still provides function declarations, and these declarations
    conflict with libc++'s
    
    llvm-svn: 353774

--- libcxx/include/fenv.h
+++ libcxx/include/fenv.h
@@ -62,140 +62,51 @@ int feupdateenv(const fenv_t* envp);
 extern "C++" {
 
 #ifdef feclearexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_feclearexcept(int __excepts) {
-  return feclearexcept(__excepts);
-}
 #undef feclearexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int feclearexcept(int __excepts) {
-  return ::__libcpp_feclearexcept(__excepts);
-}
-#endif // defined(feclearexcept)
+#endif
 
 #ifdef fegetexceptflag
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fegetexceptflag(fexcept_t* __out_ptr, int __excepts) {
-  return fegetexceptflag(__out_ptr, __excepts);
-}
 #undef fegetexceptflag
-_LIBCPP_INLINE_VISIBILITY
-inline int fegetexceptflag(fexcept_t *__out_ptr, int __excepts) {
-  return ::__libcpp_fegetexceptflag(__out_ptr, __excepts);
-}
-#endif // defined(fegetexceptflag)
+#endif
 
 
 #ifdef feraiseexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_feraiseexcept(int __excepts) {
-  return feraiseexcept(__excepts);
-}
 #undef feraiseexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int feraiseexcept(int __excepts) {
-  return ::__libcpp_feraiseexcept(__excepts);
-}
-#endif // defined(feraiseexcept)
-
+#endif
 
 #ifdef fesetexceptflag
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fesetexceptflag(const fexcept_t* __out_ptr, int __excepts) {
-  return fesetexceptflag(__out_ptr, __excepts);
-}
 #undef fesetexceptflag
-_LIBCPP_INLINE_VISIBILITY
-inline int fesetexceptflag(const fexcept_t *__out_ptr, int __excepts) {
-  return ::__libcpp_fesetexceptflag(__out_ptr, __excepts);
-}
-#endif // defined(fesetexceptflag)
+#endif
 
 
 #ifdef fetestexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fetestexcept(int __excepts) {
-  return fetestexcept(__excepts);
-}
 #undef fetestexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int fetestexcept(int __excepts) {
-  return ::__libcpp_fetestexcept(__excepts);
-}
-#endif // defined(fetestexcept)
+#endif
 
 #ifdef fegetround
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fegetround() {
-  return fegetround();
-}
 #undef fegetround
-_LIBCPP_INLINE_VISIBILITY
-inline int fegetround() {
-  return ::__libcpp_fegetround();
-}
-#endif // defined(fegetround)
+#endif
 
 #ifdef fesetround
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fesetround(int __round) {
-  return fesetround(__round);
-}
 #undef fesetround
-_LIBCPP_INLINE_VISIBILITY
-inline int fesetround(int __round) {
-  return ::__libcpp_fesetround(__round);
-}
-#endif // defined(fesetround)
+#endif
 
 #ifdef fegetenv
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fegetenv(fenv_t* __envp) {
-  return fegetenv(__envp);
-}
 #undef fegetenv
-_LIBCPP_INLINE_VISIBILITY
-inline int fegetenv(fenv_t* __envp) {
-  return ::__libcpp_fegetenv(__envp);
-}
-#endif // defined(fegetenv)
+#endif
 
 #ifdef feholdexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_feholdexcept(fenv_t* __envp) {
-  return feholdexcept(__envp);
-}
 #undef feholdexcept
-_LIBCPP_INLINE_VISIBILITY
-inline int feholdexcept(fenv_t* __envp) {
-  return ::__libcpp_feholdexcept(__envp);
-}
-#endif // defined(feholdexcept)
+#endif
 
 
 #ifdef fesetenv
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_fesetenv(const fenv_t* __envp) {
-  return fesetenv(__envp);
-}
 #undef fesetenv
-_LIBCPP_INLINE_VISIBILITY
-inline int fesetenv(const fenv_t* __envp) {
-  return ::__libcpp_fesetenv(__envp);
-}
-#endif // defined(fesetenv)
+#endif
 
 #ifdef feupdateenv
-_LIBCPP_INLINE_VISIBILITY
-inline int __libcpp_feupdateenv(const fenv_t* __envp) {
-  return feupdateenv(__envp);
-}
 #undef feupdateenv
-_LIBCPP_INLINE_VISIBILITY
-inline int feupdateenv(const fenv_t* __envp) {
-  return ::__libcpp_feupdateenv(__envp);
-}
-#endif // defined(feupdateenv)
+#endif
 
 } // extern "C++"
 
