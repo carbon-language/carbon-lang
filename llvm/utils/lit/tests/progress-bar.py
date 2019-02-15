bd40ecf7d61d3b88c2115a5854dab89a8996fabf
Julian Lettner <jlettner@apple.com>
2019-02-15 02:44:52 +0000

    [lit][NFC] Cleanup copy&paste naming mistake
    llvm-svn: 354095

--- llvm/utils/lit/tests/progress-bar.py
+++ llvm/utils/lit/tests/progress-bar.py
@@ -4,10 +4,10 @@
 # RUN: FileCheck < %t.out %s
 #
 # CHECK: Testing: 0 .. 10.. 20
-# CHECK: FAIL: shtest-shell :: test-1.txt (1 of 4)
+# CHECK: FAIL: progress-bar :: test-1.txt (1 of 4)
 # CHECK: Testing: 0 .. 10.. 20.. 30.. 40.. 
-# CHECK: FAIL: shtest-shell :: test-2.txt (2 of 4)
+# CHECK: FAIL: progress-bar :: test-2.txt (2 of 4)
 # CHECK: Testing: 0 .. 10.. 20.. 30.. 40.. 50.. 60.. 70
-# CHECK: FAIL: shtest-shell :: test-3.txt (3 of 4)
+# CHECK: FAIL: progress-bar :: test-3.txt (3 of 4)
 # CHECK: Testing: 0 .. 10.. 20.. 30.. 40.. 50.. 60.. 70.. 80.. 90.. 
-# CHECK: FAIL: shtest-shell :: test-4.txt (4 of 4)
+# CHECK: FAIL: progress-bar :: test-4.txt (4 of 4)
