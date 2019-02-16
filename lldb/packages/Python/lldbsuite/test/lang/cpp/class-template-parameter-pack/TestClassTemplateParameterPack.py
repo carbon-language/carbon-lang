81086e2a6531dc0ffb2126e62c267fdee8f1ff29
Richard Smith <richard-llvm@metafoo.co.uk>
2019-02-16 00:13:26 +0000

    Temporarily disable test:
    test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
    
    It fails on Mac OS; apparently a VarDecl 'void *&C' is implicitly
    declared there, making the class template name C ambiguous.
    
    llvm-svn: 354185

--- lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/class-template-parameter-pack/TestClassTemplateParameterPack.py
@@ -4,4 +4,7 @@ from lldbsuite.test import decorators
 lldbinline.MakeInlineTest(
     __file__, globals(), [
         decorators.expectedFailureAll(
-            compiler="gcc")])
+            compiler="gcc"),
+        decorators.expectedFailureAll(
+            oslist=['ios', 'watchos', 'tvos', 'bridgeos'],
+            bugnumber="rdar://problem/48128064: class template declaration unexpectedly shadowed by VarDecl on MacOS")])
