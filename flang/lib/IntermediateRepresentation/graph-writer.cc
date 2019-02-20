96f7b6c808bd3a0ef6c9027600e278e478a1064d
Eric Schweitz <eschweitz@nvidia.com>
2019-02-20 13:07:49 -0800

    [flang] Fortran IR: part 4. Dotty graph visualization and other miscellaneous headers.
    Original-commit: flang-compiler/f18@d7d9515f20557608ad7df45ef90647a318441e68
    Reviewed-on: https://github.com/flang-compiler/f18/pull/296
    Tree-same-pre-rewrite: false

---
+++ flang/lib/IntermediateRepresentation/graph-writer.cc
@@ -0,0 +1,122 @@
+// Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "graph-writer.h"
+
+namespace Fortran::IntermediateRepresentation {
+
+std::optional<llvm::raw_ostream *> GraphWriter::defaultOutput_{std::nullopt};
+
+void GraphWriter::dumpHeader() { output_ << "digraph G {\n"; }
+void GraphWriter::dumpFooter() { output_ << "}\n"; }
+
+void GraphWriter::dump(Program &program) {
+  dumpHeader();
+  for (auto iter{program.procedureMap_.begin()},
+       iend{program.procedureMap_.end()};
+       iter != iend; ++iter) {
+    dump(*iter->getValue(), true);
+  }
+  dumpFooter();
+}
+
+void GraphWriter::dump(Procedure &procedure, bool box) {
+  if (box) {
+    output_ << "subgraph cluster" << counter()
+            << " {\n  node[style=filled];\n  color=red;\n";
+  }
+  for (auto iter{procedure.regionList_.begin()},
+       iend{procedure.regionList_.end()};
+       iter != iend; ++iter) {
+    dump(*iter, true);
+  }
+  if (box) {
+    output_ << "  label = \"procedure";
+    if (procedure.getName().empty()) {
+      output_ << '#' << counter();
+    } else {
+      output_ << ": " << procedure.getName().str();
+    }
+    output_ << "\"\n}\n";
+  }
+}
+
+void GraphWriter::dump(Region &region, bool box) {
+  if (box) {
+    output_ << "  subgraph cluster" << counter()
+            << " {\n    node[style=filled];\n";
+  }
+  for (auto iter{region.begin()}, iend{region.end()}; iter != iend; ++iter) {
+    dump(*iter, true);
+  }
+  std::set<BasicBlock *> myNodes;
+  auto blocks{region.getBlocks()};
+  auto iend{blocks.end()};
+  auto iexit{iend};
+  --iexit;
+  auto ientry{blocks.begin()};
+  for (auto iter{ientry}; iter != iend; ++iter) {
+    isEntry_ = iter == ientry && region.IsOutermost();
+    isExit_ = iter == iexit && region.IsOutermost();
+    dump(**iter);
+    myNodes.insert(*iter);
+  }
+  std::list<std::pair<BasicBlock *, BasicBlock *>> emitAfter;
+  for (auto iter{blocks.begin()}, iend{blocks.end()}; iter != iend; ++iter) {
+    dumpInternalEdges(**iter, myNodes, emitAfter);
+  }
+  if (box) {
+    output_ << "    style=dashed;\n    color=blue;\n    label = \"region#"
+            << counter() << "\\nvariables: {...}\\n\"\n  }\n";
+  }
+  for (auto pair : emitAfter) {
+    output_ << "  " << block_id(*pair.first) << " -> " << block_id(*pair.second)
+            << ";\n";
+  }
+}
+
+void GraphWriter::dump(BasicBlock &block, std::optional<const char *> color) {
+  output_ << "    " << block_id(block) << " [label = \"";
+  if (isEntry_) {
+    output_ << "<<ENTRY>>\\n";
+  }
+  output_ << block_id(block) << '(' << reinterpret_cast<std::intptr_t>(&block)
+          << ")\\n";
+  for (auto &action : block.getSublist(static_cast<Statement *>(nullptr))) {
+    output_ << action.dump() << "\\n";
+  }
+  if (isExit_) {
+    output_ << "<<EXIT>>";
+  }
+  output_ << "\",shape=rectangle";
+  if (color) {
+    output_ << ",color=" << *color;
+  }
+  output_ << "];\n";
+}
+
+void GraphWriter::dumpInternalEdges(BasicBlock &block,
+    std::set<BasicBlock *> &nodeSet,
+    std::list<std::pair<BasicBlock *, BasicBlock *>> &emitAfter) {
+  for (auto succ : succ_list(block)) {
+    if (nodeSet.count(succ)) {
+      output_ << "    " << block_id(block) << " -> " << block_id(*succ)
+              << ";\n";
+    } else {
+      emitAfter.push_back({&block, succ});
+    }
+  }
+}
+
+}
---
+++ flang/lib/IntermediateRepresentation/mixin.h
@@ -0,0 +1,114 @@
+// Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef FORTRAN_INTERMEDIATEREPRESENTATION_MIXIN_H_
+#define FORTRAN_INTERMEDIATEREPRESENTATION_MIXIN_H_
+
+#include "llvm/ADT/ilist.h"
+#include <optional>
+#include <tuple>
+#include <type_traits>
+#include <variant>
+
+namespace Fortran::IntermediateRepresentation {
+
+template<typename T, typename E = void> struct SumTypeMixin {};
+template<typename T>  // T must be std::optional<...>
+struct SumTypeMixin<T, std::enable_if_t<std::variant_size_v<T>>> {
+  template<typename A> SumTypeMixin(A &&x) : u{std::move(x)} {}
+  using SumTypeTrait = std::true_type;
+  SumTypeMixin(SumTypeMixin &&) = default;
+  SumTypeMixin &operator=(SumTypeMixin &&) = default;
+  SumTypeMixin(const SumTypeMixin &) = delete;
+  SumTypeMixin &operator=(const SumTypeMixin &) = delete;
+  SumTypeMixin() = delete;
+  T u;
+};
+
+template<typename T, typename E = void> struct SumTypeCopyMixin {};
+template<typename T>  // T must be std::optional<...>
+struct SumTypeCopyMixin<T, std::enable_if_t<std::variant_size_v<T>>> {
+  using CopyableSumTypeTrait = std::true_type;
+  SumTypeCopyMixin(SumTypeCopyMixin &&) = default;
+  SumTypeCopyMixin &operator=(SumTypeCopyMixin &&) = default;
+  SumTypeCopyMixin(const SumTypeCopyMixin &) = default;
+  SumTypeCopyMixin &operator=(const SumTypeCopyMixin &) = default;
+  SumTypeCopyMixin() = delete;
+  T u;
+};
+#define SUM_TYPE_COPY_MIXIN(Derived) \
+  Derived(const Derived &derived) : SumTypeCopyMixin(derived) {} \
+  Derived &operator=(const Derived &derived) { \
+    SumTypeCopyMixin::operator=(derived); \
+    return *this; \
+  }
+
+template<typename T, typename E = void> struct ProductTypeMixin {};
+template<typename T>  // T must be std::tuple<...>
+struct ProductTypeMixin<T, std::enable_if_t<std::tuple_size_v<T>>> {
+  template<typename A> ProductTypeMixin(A &&x) : t{std::move(x)} {}
+  using ProductTypeTrait = std::true_type;
+  ProductTypeMixin(ProductTypeMixin &&) = default;
+  ProductTypeMixin &operator=(ProductTypeMixin &&) = default;
+  ProductTypeMixin(const ProductTypeMixin &) = delete;
+  ProductTypeMixin &operator=(const ProductTypeMixin &) = delete;
+  ProductTypeMixin() = delete;
+  T t;
+};
+
+template<typename T, typename E = void> struct MaybeMixin {};
+template<typename T>  // T must be std::optional<...>
+struct MaybeMixin<T,
+    std::enable_if_t<
+        std::is_same_v<std::optional<typename T::value_type>, T>>> {
+  template<typename A> MaybeMixin(A &&x) : o{std::move(x)} {}
+  using MaybeTrait = std::true_type;
+  MaybeMixin(MaybeMixin &&) = default;
+  MaybeMixin &operator=(MaybeMixin &&) = default;
+  MaybeMixin(const MaybeMixin &) = delete;
+  MaybeMixin &operator=(const MaybeMixin &) = delete;
+  MaybeMixin() = delete;
+  T o;
+};
+
+template<typename T, typename P> struct ChildMixin {
+protected:
+  P *parent;
+
+public:
+  ChildMixin(P *p) : parent{p} {}
+  inline const P *getParent() const { return parent; }
+  inline P *getParent() { return parent; }
+  llvm::iplist<T> &getList() { return parent->getSublist(this); }
+};
+
+template<typename A, typename B, typename C>
+C Zip(C out, A first, A last, B other) {
+  std::transform(first, last, other, out,
+      [](auto &&a, auto &&b) -> std::pair<decltype(a), decltype(b)> {
+        return {a, b};
+      });
+  return out;
+}
+template<typename A, typename B> B &Unzip(B &out, A first, A last) {
+  std::transform(first, last, std::back_inserter(out.first),
+      [](auto &&a) -> decltype(a.first) { return a.first; });
+  std::transform(first, last, std::back_inserter(out.second),
+      [](auto &&a) -> decltype(a.second) { return a.second; });
+  return out;
+}
+
+}
+
+#endif
