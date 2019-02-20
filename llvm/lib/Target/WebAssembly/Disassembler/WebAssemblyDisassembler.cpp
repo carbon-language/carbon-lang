8a28ce1a123a79b6de86a0870c130f6db7e19e7e
Wouter van Oortmerssen <aardappel@gmail.com>
2019-02-20 00:55:59 +0000

    [WebAssembly] Fixed disassembler not knowing about OPERAND_EVENT
    Reviewers: aheejin
    
    Subscribers: dschuff, sbc100, jgravelle-google, sunfish, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D58414
    
    llvm-svn: 354416

--- llvm/lib/Target/WebAssembly/Disassembler/WebAssemblyDisassembler.cpp
+++ llvm/lib/Target/WebAssembly/Disassembler/WebAssemblyDisassembler.cpp
@@ -199,6 +199,7 @@ MCDisassembler::DecodeStatus WebAssemblyDisassembler::getInstruction(
     case WebAssembly::OPERAND_OFFSET32:
     case WebAssembly::OPERAND_P2ALIGN:
     case WebAssembly::OPERAND_TYPEINDEX:
+    case WebAssembly::OPERAND_EVENT:
     case MCOI::OPERAND_IMMEDIATE: {
       if (!parseLEBImmediate(MI, Size, Bytes, false))
         return MCDisassembler::Fail;
