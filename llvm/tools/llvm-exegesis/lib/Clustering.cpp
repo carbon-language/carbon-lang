14e15ec18dd23e75fe54a72dde203dc266390561
Hans Wennborg <hans@hanshq.net>
2019-02-20 14:50:08 +0000

    Fix the build with gcc/libstdc++ 4.8.2 after r354441
    llvm-svn: 354469

--- llvm/tools/llvm-exegesis/lib/Clustering.cpp
+++ llvm/tools/llvm-exegesis/lib/Clustering.cpp
@@ -226,10 +226,10 @@ void InstructionBenchmarkClustering::stabilize(unsigned NumOpcodes) {
                       ClusterIdForPoint_[P] = UnstableCluster.Id;
                     });
       // Actually append to-be-moved points to the new cluster.
-      UnstableCluster.PointIndices.insert(UnstableCluster.PointIndices.cend(),
+      UnstableCluster.PointIndices.insert(UnstableCluster.PointIndices.end(),
                                           it, OldCluster.PointIndices.end());
       // And finally, remove "to-be-moved" points form the old cluster.
-      OldCluster.PointIndices.erase(it, OldCluster.PointIndices.cend());
+      OldCluster.PointIndices.erase(it, OldCluster.PointIndices.end());
       // Now, the old cluster may end up being empty, but let's just keep it
       // in whatever state it ended up. Purging empty clusters isn't worth it.
     };
