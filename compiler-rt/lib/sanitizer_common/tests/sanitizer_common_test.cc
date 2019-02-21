9b900dc7deb248437e4a25f221b00d5621560feb
Matt Morehouse <mascasa@google.com>
2019-02-21 17:06:23 +0000

    Revert "[sanitizers] Restore internal_readlink for x32"
    This reverts r354451 since it broke the Windows sanitizer bot.
    
    llvm-svn: 354601

--- compiler-rt/lib/sanitizer_common/tests/sanitizer_common_test.cc
+++ compiler-rt/lib/sanitizer_common/tests/sanitizer_common_test.cc
@@ -438,9 +438,4 @@ TEST(SanitizerCommon, ReservedAddressRangeUnmap) {
   EXPECT_DEATH(address_range.Unmap(base_addr + (PageSize * 2), PageSize), ".*");
 }
 
-TEST(SanitizerCommon, ReadBinaryNameCached) {
-  char buf[256];
-  EXPECT_NE((uptr)0, ReadBinaryNameCached(buf, sizeof(buf)));
-}
-
 }  // namespace __sanitizer
