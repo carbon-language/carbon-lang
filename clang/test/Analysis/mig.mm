95a3537cde9c2bde016274c1ace1984087eed8b4
Artem Dergachev <artem.dergachev@gmail.com>
2019-02-22 00:02:47 +0000

    [analyzer] MIGChecker: Take advantage of the mig_server_routine annotation.
    r354530 has added a new function/block/message attribute "mig_server_routine"
    that attracts compiler's attention to functions that need to follow the MIG
    server routine convention with respect to deallocating out-of-line data that
    was passed to them as an argument.
    
    Teach the checker to identify MIG routines by looking at this attribute,
    rather than by making heuristic-based guesses.
    
    rdar://problem/35380337
    
    Differential Revision: https://reviews.llvm.org/58366
    
    llvm-svn: 354638

--- clang/test/Analysis/mig.mm
+++ clang/test/Analysis/mig.mm
@@ -1,4 +1,5 @@
-// RUN: %clang_analyze_cc1 -w -analyzer-checker=core,alpha.osx.MIG -verify %s
+// RUN: %clang_analyze_cc1 -w -analyzer-checker=core,alpha.osx.MIG\
+// RUN:                    -fblocks -verify %s
 
 // XNU APIs.
 
@@ -12,8 +13,12 @@ typedef unsigned vm_size_t;
 
 kern_return_t vm_deallocate(mach_port_name_t, vm_address_t, vm_size_t);
 
+#define MIG_SERVER_ROUTINE __attribute__((mig_server_routine))
+
+
 // Tests.
 
+MIG_SERVER_ROUTINE
 kern_return_t basic_test(mach_port_name_t port, vm_address_t address, vm_size_t size) {
   vm_deallocate(port, address, size);
   if (size > 10) {
@@ -22,6 +27,7 @@ kern_return_t basic_test(mach_port_name_t port, vm_address_t address, vm_size_t 
   return KERN_SUCCESS;
 }
 
+MIG_SERVER_ROUTINE
 kern_return_t test_unknown_return_value(mach_port_name_t port, vm_address_t address, vm_size_t size) {
   extern kern_return_t foo();
 
@@ -31,6 +37,48 @@ kern_return_t test_unknown_return_value(mach_port_name_t port, vm_address_t addr
 }
 
 // Make sure we don't crash when they forgot to write the return statement.
+MIG_SERVER_ROUTINE
 kern_return_t no_crash(mach_port_name_t port, vm_address_t address, vm_size_t size) {
   vm_deallocate(port, address, size);
+}
+
+// Check that we work on Objective-C messages and blocks.
+@interface I
+- (kern_return_t)fooAtPort:(mach_port_name_t)port withAddress:(vm_address_t)address ofSize:(vm_size_t)size;
+@end
+
+@implementation I
+- (kern_return_t)fooAtPort:(mach_port_name_t)port
+               withAddress:(vm_address_t)address
+                    ofSize:(vm_size_t)size MIG_SERVER_ROUTINE {
+  vm_deallocate(port, address, size);
+  return KERN_ERROR; // expected-warning{{MIG callback fails with error after deallocating argument value. This is a use-after-free vulnerability because the caller will try to deallocate it again}}
+}
+@end
+
+void test_block() {
+  kern_return_t (^block)(mach_port_name_t, vm_address_t, vm_size_t) =
+      ^MIG_SERVER_ROUTINE (mach_port_name_t port,
+                           vm_address_t address, vm_size_t size) {
+        vm_deallocate(port, address, size);
+        return KERN_ERROR; // expected-warning{{MIG callback fails with error after deallocating argument value. This is a use-after-free vulnerability because the caller will try to deallocate it again}}
+      };
+}
+
+void test_block_with_weird_return_type() {
+  struct Empty {};
+
+  // The block is written within a function so that it was actually analyzed as
+  // a top-level function during analysis. If we were to write it as a global
+  // variable of block type instead, it would not have been analyzed, because
+  // ASTConsumer won't find the block's code body within the VarDecl.
+  // At the same time, we shouldn't call it from the function, because otherwise
+  // it will be analyzed as an inlined function rather than as a top-level
+  // function.
+  Empty (^block)(mach_port_name_t, vm_address_t, vm_size_t) =
+      ^MIG_SERVER_ROUTINE(mach_port_name_t port,
+                          vm_address_t address, vm_size_t size) {
+        vm_deallocate(port, address, size);
+        return Empty{}; // no-crash
+      };
 }
