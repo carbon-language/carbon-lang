76004da1c91205df751e4d46e8b7a0224a00be8c
Emilio Cobos Alvarez <emilio@crisal.io>
2019-02-25 21:15:34 +0000

    [libclang] Fix a trivial error introduced in D57946.
    The value for CXCursor_ConvergentAttr is not 420. I'm not really sure how easy
    it is to test this, and I'm not familiar with the python bindings, just noticed
    the error while looking at D57946 to write D58570.
    
    Differential Revision: https://reviews.llvm.org/D58571
    
    llvm-svn: 354823

--- clang/bindings/python/clang/cindex.py
+++ clang/bindings/python/clang/cindex.py
@@ -1342,7 +1342,7 @@ CursorKind.VISIBILITY_ATTR = CursorKind(417)
 
 CursorKind.DLLEXPORT_ATTR = CursorKind(418)
 CursorKind.DLLIMPORT_ATTR = CursorKind(419)
-CursorKind.CONVERGENT_ATTR = CursorKind(420)
+CursorKind.CONVERGENT_ATTR = CursorKind(438)
 
 ###
 # Preprocessing
