27931e47d19f79941d069dc18eab9f677a3867d3
peter klausler <pklausler@nvidia.com>
2019-02-28 11:47:33 -0800

    [flang] Missed a fortran.h instance
    Original-commit: flang-compiler/f18@3a9a185383fb4bf967624258f59e2f22cd143ed5
    Reviewed-on: https://github.com/flang-compiler/f18/pull/308

--- flang/runtime/type-code.h
+++ flang/runtime/type-code.h
@@ -1,4 +1,4 @@
-// Copyright (c) 2018, NVIDIA CORPORATION.  All rights reserved.
+// Copyright (c) 2018-2019, NVIDIA CORPORATION.  All rights reserved.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -16,7 +16,7 @@
 #define FORTRAN_RUNTIME_TYPE_CODE_H_
 
 #include "../include/flang/ISO_Fortran_binding.h"
-#include "../lib/common/fortran.h"
+#include "../lib/common/Fortran.h"
 
 namespace Fortran::runtime {
 
