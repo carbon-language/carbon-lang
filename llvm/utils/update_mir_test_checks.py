f436f70fb8667e221c11a838ed804181eb450c04
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-03-02 11:14:01 +0000

    Fix update_mir_test_checks.py to run on python3
    Split off from D58817
    
    Differential Revision: https://reviews.llvm.org/D58820
    
    llvm-svn: 355268

--- llvm/utils/update_mir_test_checks.py
+++ llvm/utils/update_mir_test_checks.py
@@ -62,6 +62,8 @@ class LLC:
         with open(ir) as ir_file:
             stdout = subprocess.check_output('{} {}'.format(self.bin, args),
                                              shell=True, stdin=ir_file)
+            if sys.version_info[0] > 2:
+              stdout = stdout.decode()
             # Fix line endings to unix CR style.
             stdout = stdout.replace('\r\n', '\n')
         return stdout
@@ -408,7 +410,7 @@ def update_test_file(llc, test, remove_common_prefixes=False, verbose=False):
     log('Writing {} lines to {}...'.format(len(output_lines), test), verbose)
 
     with open(test, 'wb') as fd:
-        fd.writelines([l + '\n' for l in output_lines])
+        fd.writelines(['{}\n'.format(l).encode('utf-8') for l in output_lines])
 
 
 def main():
