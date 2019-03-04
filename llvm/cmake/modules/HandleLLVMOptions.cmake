0c5b6ede2ff19731cb047803d6193bcd4c072f90
Kamil Rytarowski <n54@gmx.com>
2019-03-04 14:36:43 +0000

    Unbreak shared library linkage on DragonFlyBSD.
    Patch submitted by rimvydas.
    
    Reviewers: llvm-commits, krytarowski, mgorny
    
    Reviewed By: krytarowski, mgorny
    
    Subscribers: mgorny
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D35125
    
    llvm-svn: 355315

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -138,6 +138,7 @@ endif()
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
 if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin" OR WIN32 OR CYGWIN OR
+        ${CMAKE_SYSTEM_NAME} MATCHES "DragonFly" OR
         ${CMAKE_SYSTEM_NAME} MATCHES "FreeBSD" OR
 	${CMAKE_SYSTEM_NAME} MATCHES "OpenBSD" OR
 	${CMAKE_SYSTEM_NAME} MATCHES "DragonFly") AND
