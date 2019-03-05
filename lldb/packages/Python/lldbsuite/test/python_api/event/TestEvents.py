d15f3b188bf728e0e7bff64163e29dfbee030965
Stella Stamenova <stilis@microsoft.com>
2019-03-05 21:03:36 +0000

    [lldbsuite, windows] Skip the TestEvents tests on Windows
    These tests are flakey on Windows and recently they have started failing AND also hanging the whole suite when they fail.
    
    llvm-svn: 355443

--- lldb/packages/Python/lldbsuite/test/python_api/event/TestEvents.py
+++ lldb/packages/Python/lldbsuite/test/python_api/event/TestEvents.py
@@ -31,6 +31,7 @@ class EventAPITestCase(TestBase):
     @expectedFailureAll(
         oslist=["linux"],
         bugnumber="llvm.org/pr23730 Flaky, fails ~1/10 cases")
+    @skipIfWindows # This is flakey on Windows AND when it fails, it hangs: llvm.org/pr38373
     def test_listen_for_and_print_event(self):
         """Exercise SBEvent API."""
         self.build()
@@ -119,6 +120,7 @@ class EventAPITestCase(TestBase):
 
     @add_test_categories(['pyapi'])
     @expectedFlakeyLinux("llvm.org/pr23730")  # Flaky, fails ~1/100 cases
+    @skipIfWindows # This is flakey on Windows AND when it fails, it hangs: llvm.org/pr38373
     def test_wait_for_event(self):
         """Exercise SBListener.WaitForEvent() API."""
         self.build()
@@ -197,6 +199,7 @@ class EventAPITestCase(TestBase):
     @expectedFailureAll(
         oslist=["linux"],
         bugnumber="llvm.org/pr23617 Flaky, fails ~1/10 cases")
+    @skipIfWindows # This is flakey on Windows AND when it fails, it hangs: llvm.org/pr38373
     def test_add_listener_to_broadcaster(self):
         """Exercise some SBBroadcaster APIs."""
         self.build()
