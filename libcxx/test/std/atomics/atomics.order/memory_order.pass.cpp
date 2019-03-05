1061cb6a93c2661905b5ee08718660f2bed243a3
Davide Italiano <davide@freebsd.org>
2019-03-05 17:38:33 +0000

    [libcxx] Revert set of atomic patches that broke lldb.
    Revert "[libc++] Fix <atomic> failures on GCC"
    Revert "[libc++] Change memory_order to an enum class"
    Revert "[libc++] decoupling Freestanding atomic<T> from libatomic.a"
    
    The lldb formatter nededs to be updated. Shafik and Louis will
    coordinate to do so.
    
    llvm-svn: 355417

--- libcxx/test/std/atomics/atomics.order/memory_order.pass.cpp
+++ libcxx/test/std/atomics/atomics.order/memory_order.pass.cpp
@@ -21,15 +21,14 @@
 
 int main(int, char**)
 {
-    assert(static_cast<int>(std::memory_order_relaxed) == 0);
-    assert(static_cast<int>(std::memory_order_consume) == 1);
-    assert(static_cast<int>(std::memory_order_acquire) == 2);
-    assert(static_cast<int>(std::memory_order_release) == 3);
-    assert(static_cast<int>(std::memory_order_acq_rel) == 4);
-    assert(static_cast<int>(std::memory_order_seq_cst) == 5);
-
+    assert(std::memory_order_relaxed == 0);
+    assert(std::memory_order_consume == 1);
+    assert(std::memory_order_acquire == 2);
+    assert(std::memory_order_release == 3);
+    assert(std::memory_order_acq_rel == 4);
+    assert(std::memory_order_seq_cst == 5);
     std::memory_order o = std::memory_order_seq_cst;
-    assert(static_cast<int>(o) == 5);
+    assert(o == 5);
 
-    return 0;
+  return 0;
 }
