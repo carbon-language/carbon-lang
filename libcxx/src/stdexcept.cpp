2710d8e105a683656cdaa825b6c0af46ae25f8ae
Eric Fiselier <eric@efcs.ca>
2019-03-06 20:31:57 +0000

    Fix ABI compatibility of `<stdexcept>` with VCRuntime.
    Summary:
    Currently, libc++'s `<stdexcept>` doesn't play nice with `vcruntime`. Specifically:
    
    * `logic_error` and `runtime_error` have a different layout.
    * libc++'s `logic_error` and `runtime_error` override `what()` but `vcruntime` does not.
    *  `vcruntime` uses weak vtables for `<stdexcept>` types.
    * libc++'s `<stdexcept>` constructors and assignment operators may have different manglings than `vcruntimes`.
    
    This patch makes libc++'s declarations in `<stdexcept>` match those provided by MSVC's STL as closely as possible.
    If MSVC doesn't declare a special member, then neither do we. This ensures that the implicit definitions have the same linkage, visibility, triviality, and noexcept-ness.
    
    
    
    
    
    
    
    Reviewers: thomasanderson, ldionne, smeenai
    
    Reviewed By: thomasanderson
    
    Subscribers: jdoerfert, libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D58945
    
    llvm-svn: 355546

--- libcxx/src/stdexcept.cpp
+++ libcxx/src/stdexcept.cpp
@@ -10,87 +10,10 @@
 #include "new"
 #include "string"
 #include "system_error"
-#include "include/refstring.h"
 
-/* For _LIBCPPABI_VERSION */
-#if !defined(_LIBCPP_BUILDING_HAS_NO_ABI_LIBRARY) && \
-    (defined(LIBCXX_BUILDING_LIBCXXABI) || defined(__APPLE__) || defined(LIBCXXRT))
-#include <cxxabi.h>
+
+#ifdef _LIBCPP_ABI_VCRUNTIME
+#include "support/runtime/stdexcept_vcruntime.ipp"
+#else
+#include "support/runtime/stdexcept_default.ipp"
 #endif
-
-static_assert(sizeof(std::__libcpp_refstring) == sizeof(const char *), "");
-
-
-namespace std  // purposefully not using versioning namespace
-{
-
-logic_error::logic_error(const string& msg) : __imp_(msg.c_str())
-{
-}
-
-logic_error::logic_error(const char* msg) : __imp_(msg)
-{
-}
-
-logic_error::logic_error(const logic_error& le) _NOEXCEPT : __imp_(le.__imp_)
-{
-}
-
-logic_error&
-logic_error::operator=(const logic_error& le) _NOEXCEPT
-{
-    __imp_ = le.__imp_;
-    return *this;
-}
-
-runtime_error::runtime_error(const string& msg) : __imp_(msg.c_str())
-{
-}
-
-runtime_error::runtime_error(const char* msg) : __imp_(msg)
-{
-}
-
-runtime_error::runtime_error(const runtime_error& le) _NOEXCEPT
-  : __imp_(le.__imp_)
-{
-}
-
-runtime_error&
-runtime_error::operator=(const runtime_error& le) _NOEXCEPT
-{
-    __imp_ = le.__imp_;
-    return *this;
-}
-
-#if !defined(_LIBCPPABI_VERSION) && !defined(LIBSTDCXX)
-
-const char*
-logic_error::what() const _NOEXCEPT
-{
-    return __imp_.c_str();
-}
-
-const char*
-runtime_error::what() const _NOEXCEPT
-{
-    return __imp_.c_str();
-}
-
-#if !defined(_LIBCPP_ABI_VCRUNTIME)
-
-logic_error::~logic_error() _NOEXCEPT {}
-domain_error::~domain_error() _NOEXCEPT {}
-invalid_argument::~invalid_argument() _NOEXCEPT {}
-length_error::~length_error() _NOEXCEPT {}
-out_of_range::~out_of_range() _NOEXCEPT {}
-
-runtime_error::~runtime_error() _NOEXCEPT {}
-range_error::~range_error() _NOEXCEPT {}
-overflow_error::~overflow_error() _NOEXCEPT {}
-underflow_error::~underflow_error() _NOEXCEPT {}
-
-#endif
-#endif
-
-}  // std
