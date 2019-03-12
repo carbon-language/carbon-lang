5b1e1c0537d0e896ae1c0c5c2ff80ed68b397a80
Kristina Brooks <notstina@gmail.com>
2019-03-12 07:08:19 +0000

    Very minor typo. NFC
    Typo `we we're` => `we were` in the pass EarlyCSE
    
    Patch by liangdzou (Liang ZOU)
    
    Differential Revision: https://reviews.llvm.org/D59241
    
    llvm-svn: 355895

--- llvm/lib/Transforms/Scalar/EarlyCSE.cpp
+++ llvm/lib/Transforms/Scalar/EarlyCSE.cpp
@@ -1102,7 +1102,7 @@ bool EarlyCSE::processNode(DomTreeNode *Node) {
         // At the moment, we don't remove ordered stores, but do remove
         // unordered atomic stores.  There's no special requirement (for
         // unordered atomics) about removing atomic stores only in favor of
-        // other atomic stores since we we're going to execute the non-atomic
+        // other atomic stores since we were going to execute the non-atomic
         // one anyway and the atomic one might never have become visible.
         if (LastStore) {
           ParseMemoryInst LastStoreMemInst(LastStore, TTI);
