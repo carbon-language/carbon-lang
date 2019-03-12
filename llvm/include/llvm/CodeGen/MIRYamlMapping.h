b5f14326b447e5a97b3d7654448c36d7745a6882
Matt Arsenault <Matthew.Arsenault@amd.com>
2019-03-12 20:42:14 +0000

    Fix missing C++ mode comments
    llvm-svn: 355971

--- llvm/include/llvm/CodeGen/MIRYamlMapping.h
+++ llvm/include/llvm/CodeGen/MIRYamlMapping.h
@@ -1,4 +1,4 @@
-//===- MIRYamlMapping.h - Describes the mapping between MIR and YAML ------===//
+//===- MIRYamlMapping.h - Describe mapping between MIR and YAML--*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
