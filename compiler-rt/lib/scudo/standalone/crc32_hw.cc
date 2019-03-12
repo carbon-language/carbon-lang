1f066a717cb1db55e6b219f1308a80b6c3ed116d
Kostya Kortchinsky <kostyak@google.com>
2019-03-12 14:46:31 +0000

    [scudo][standalone] Implement checksumming functions
    Summary:
    This CL implements the checksumming functions. This departs from the
    current Scudo code in one aspect: the software version is no longer
    CRC32 but a BSD checksum. This is because the software CRC32 was too
    impactful in terms of performances, the BSD checksum has no array
    lookup which is better (and saves 1KB of data).
    
    As with the current version, we only flip the CRC compiler flag for
    a single compilation unit by default, to allow for a library compiled
    with HW CRC32 to work on a system without HW CRC32.
    
    There is some platform & hardware specific code in those files, but
    since departs from a mere platform split, it felt right to me to have
    it that way.
    
    Reviewers: morehouse, eugenis, vitalybuka, hctim, mcgrathr
    
    Reviewed By: morehouse
    
    Subscribers: mgorny, delcypher, jfb, jdoerfert, #sanitizers, llvm-commits
    
    Tags: #llvm, #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D59116
    
    llvm-svn: 355923

---
+++ compiler-rt/lib/scudo/standalone/crc32_hw.cc
@@ -0,0 +1,19 @@
+//===-- crc32_hw.h ----------------------------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "checksum.h"
+
+namespace scudo {
+
+#if defined(__SSE4_2__) || defined(__ARM_FEATURE_CRC32)
+u32 computeHardwareCRC32(u32 Crc, uptr Data) {
+  return static_cast<u32>(CRC32_INTRINSIC(Crc, Data));
+}
+#endif // defined(__SSE4_2__) || defined(__ARM_FEATURE_CRC32)
+
+} // namespace scudo
