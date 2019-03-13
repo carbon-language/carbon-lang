4b0b9a727ebb6cdc67e9435ec0cae128dd58998f
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:05:53 +0000

    mad: Convert to standard ternary header
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356011

--- libclc/generic/include/clc/math/mad.h
+++ libclc/generic/include/clc/math/mad.h
@@ -1,2 +1,7 @@
-#define __CLC_BODY <clc/math/mad.inc>
+#define __CLC_BODY <clc/math/ternary_decl.inc>
+#define __CLC_FUNCTION mad
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
