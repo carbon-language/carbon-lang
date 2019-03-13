ee555aa9929976a6b178f549ec0bf68069bafa52
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:06:10 +0000

    trunc: Remove llvm intrinsic from the header.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356018

--- libclc/generic/include/clc/math/trunc.h
+++ libclc/generic/include/clc/math/trunc.h
@@ -1,9 +1,7 @@
-#undef trunc
-#define trunc __clc_trunc
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION trunc
 
-#define __CLC_FUNCTION __clc_trunc
-#define __CLC_INTRINSIC "llvm.trunc"
-#include <clc/math/unary_intrin.inc>
+#include <clc/math/gentype.inc>
 
+#undef __CLC_BODY
 #undef __CLC_FUNCTION
-#undef __CLC_INTRINSIC
