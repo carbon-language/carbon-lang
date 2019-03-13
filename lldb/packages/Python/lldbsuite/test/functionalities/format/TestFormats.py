796aa0e49af1895b251af49eb93c5bda2fbca6d6
Davide Italiano <davide@freebsd.org>
2019-03-13 01:26:01 +0000

    [testsuite] Remove dead code in TestFormats.
    llvm-svn: 356000

--- lldb/packages/Python/lldbsuite/test/functionalities/format/TestFormats.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/format/TestFormats.py
@@ -28,9 +28,6 @@ class TestFormats(TestBase):
         child = pexpect.spawn(
             '%s %s -x -o "b main" -o r %s' %
             (lldbtest_config.lldbExec, self.lldbOption, exe))
-        # Turn on logging for what the child sends back.
-        if self.TraceOn():
-            child.logfile_read = sys.stdout
         # So that the spawned lldb session gets shutdown durng teardown.
         self.child = child
 
@@ -40,27 +37,3 @@ class TestFormats(TestBase):
         child.expect_exact(prompt + 'r')
         child.expect_exact(prompt)
         child.sendline()
-        # child.expect_exact(prompt + "target create")
-        #
-        # child.sendline("command regex 'Help__'")
-        # child.expect_exact(regex_prompt)
-        # child.sendline('s/^$/help/')
-        # child.expect_exact(regex_prompt1)
-        # child.sendline('')
-        # child.expect_exact(prompt)
-        # # Help!
-        # child.sendline('Help__')
-        # # If we see the familiar 'help' output, the test is done.
-        # child.expect('Debugger commands:')
-        # # Try and incorrectly remove "Help__" using "command unalias" and verify we fail
-        # child.sendline('command unalias Help__')
-        # child.expect_exact("error: 'Help__' is not an alias, it is a debugger command which can be removed using the 'command delete' command")
-        # child.expect_exact(prompt)
-        #
-        # # Delete the regex command using "command delete"
-        # child.sendline('command delete Help__')
-        # child.expect_exact(prompt)
-        # # Verify the command was removed
-        # child.sendline('Help__')
-        # child.expect_exact("error: 'Help__' is not a valid command")
-        # child.expect_exact(prompt)
