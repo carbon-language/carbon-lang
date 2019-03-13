e7c0c37a315a89f49468efc03a98ed50d262ac74
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:13:32 +0000

    fdim: Use binary_decl_tt.inc instead of custom inc file.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356027

--- libclc/generic/include/clc/math/fdim.h
+++ libclc/generic/include/clc/math/fdim.h
@@ -1,2 +1,7 @@
-#define __CLC_BODY <clc/math/fdim.inc>
+#define __CLC_FUNCTION fdim
+#define __CLC_BODY <clc/math/binary_decl_tt.inc>
+
 #include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
