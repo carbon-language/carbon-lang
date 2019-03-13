b3d64e4a83e0de4f2ac1d498637432cf29c1c6a5
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:06:06 +0000

    rint: Remove llvm intrinsic from the header.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356016

--- libclc/generic/include/clc/math/rint.h
+++ libclc/generic/include/clc/math/rint.h
@@ -1,6 +1,7 @@
-#undef rint
-#define rint __clc_rint
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION rint
 
-#define __CLC_FUNCTION __clc_rint
-#define __CLC_INTRINSIC "llvm.rint"
-#include <clc/math/unary_intrin.inc>
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
