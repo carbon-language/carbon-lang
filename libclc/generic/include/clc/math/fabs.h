fda15e56a637ba9c3743bee41f21646c42025036
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:06:00 +0000

    fabs: Remove llvm intrinsic from the header.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356014

--- libclc/generic/include/clc/math/fabs.h
+++ libclc/generic/include/clc/math/fabs.h
@@ -1,6 +1,7 @@
-#undef fabs
-#define fabs __clc_fabs
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION fabs
 
-#define __CLC_FUNCTION __clc_fabs
-#define __CLC_INTRINSIC "llvm.fabs"
-#include <clc/math/unary_intrin.inc>
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
