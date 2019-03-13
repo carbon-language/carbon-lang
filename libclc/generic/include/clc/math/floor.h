fd199f013948d6d9304caefed4cfd35d6f1e18ca
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:06:03 +0000

    floor: Remove llvm isntrinsic from the header.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356015

--- libclc/generic/include/clc/math/floor.h
+++ libclc/generic/include/clc/math/floor.h
@@ -1,6 +1,7 @@
-#undef floor
-#define floor __clc_floor
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION floor
 
-#define __CLC_FUNCTION __clc_floor
-#define __CLC_INTRINSIC "llvm.floor"
-#include <clc/math/unary_intrin.inc>
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
