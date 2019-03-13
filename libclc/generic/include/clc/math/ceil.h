54eb4d3a6d863fbe1ca92e4e741ed1c842a2cfa0
Jan Vesely <jan.vesely@rutgers.edu>
2019-03-13 07:05:58 +0000

    ceil: Remove llvm intrinsic from the header.
    Reviewer: Aaron Watry
    Signed-off-by: Jan Vesely <jan.vesely@rutgers.edu>
    llvm-svn: 356013

--- libclc/generic/include/clc/math/ceil.h
+++ libclc/generic/include/clc/math/ceil.h
@@ -1,6 +1,7 @@
-#undef ceil
-#define ceil __clc_ceil
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION ceil
 
-#define __CLC_FUNCTION __clc_ceil
-#define __CLC_INTRINSIC "llvm.ceil"
-#include <clc/math/unary_intrin.inc>
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
