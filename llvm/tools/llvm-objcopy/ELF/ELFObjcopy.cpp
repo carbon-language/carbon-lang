c03a95d46576a5de3b3c15fc5d818cf0fe66668f
James Henderson <jh7370@my.bristol.ac.uk>
2019-03-14 10:20:27 +0000

    [llvm-objcopy]Don't implicitly strip sections in segments
    This patch changes llvm-objcopy's behaviour to not strip sections that
    are in segments, if they otherwise would be due to a stripping operation
    (--strip-all, --strip-sections, --strip-non-alloc). This preserves the
    segment contents. It does not change the behaviour of --strip-all-gnu
    (although we could choose to do so), because GNU objcopy's behaviour in
    this case seems to be to strip the section, nor does it prevent removing
    of sections in segments with --remove-section (if a user REALLY wants to
    remove a section, we should probably let them, although I could be
    persuaded that warning might be appropriate). Tests have been added to
    show this latter behaviour.
    
    This fixes https://bugs.llvm.org/show_bug.cgi?id=41006.
    
    Reviewed by: grimar, rupprecht, jakehehrlich
    
    Differential Revision: https://reviews.llvm.org/D59293
    
    llvm-svn: 356129

--- llvm/tools/llvm-objcopy/ELF/ELFObjcopy.cpp
+++ llvm/tools/llvm-objcopy/ELF/ELFObjcopy.cpp
@@ -403,7 +403,7 @@ static Error handleArgs(const CopyConfig &Config, Object &Obj,
 
   if (Config.StripSections) {
     RemovePred = [RemovePred](const SectionBase &Sec) {
-      return RemovePred(Sec) || (Sec.Flags & SHF_ALLOC) == 0;
+      return RemovePred(Sec) || Sec.ParentSegment == nullptr;
     };
   }
 
@@ -419,7 +419,7 @@ static Error handleArgs(const CopyConfig &Config, Object &Obj,
         return true;
       if (&Sec == Obj.SectionNames)
         return false;
-      return (Sec.Flags & SHF_ALLOC) == 0;
+      return (Sec.Flags & SHF_ALLOC) == 0 && Sec.ParentSegment == nullptr;
     };
 
   if (Config.StripAll)
@@ -429,6 +429,8 @@ static Error handleArgs(const CopyConfig &Config, Object &Obj,
       if (&Sec == Obj.SectionNames)
         return false;
       if (StringRef(Sec.Name).startswith(".gnu.warning"))
+        return false;
+      if (Sec.ParentSegment != nullptr)
         return false;
       return (Sec.Flags & SHF_ALLOC) == 0;
     };
