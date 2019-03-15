5bcca9ffd167a50dafa2c3958b36e498ee71ad68
Marshall Clow <mclow.lists@gmail.com>
2019-03-15 00:29:35 +0000

    Mark vector::operator[] and front/back as noexcept. We already do this for string and string_view. This should give better codegen inside of noexcept functions. Add tests for op[]/front/back/at, because apparently we had none.
    llvm-svn: 356224

--- libcxx/test/libcxx/debug/containers/db_sequence_container_iterators.pass.cpp
+++ libcxx/test/libcxx/debug/containers/db_sequence_container_iterators.pass.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+// XFAIL: *
 // UNSUPPORTED: c++98, c++03, c++11, c++14
 // UNSUPPORTED: libcpp-no-exceptions, libcpp-no-if-constexpr
 // MODULES_DEFINES: _LIBCPP_DEBUG=1
