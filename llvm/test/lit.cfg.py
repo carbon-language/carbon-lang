67646d0570b4e25215416cb5cf73756733e130e0
Evgeniy Stepanov <eugeni.stepanov@gmail.com>
2019-03-27 20:15:08 +0000

    Fix llvm-rc tests.
    Summary:
    Follow-up for D56743.
    * Add more "--" in llvm-rc invocations.
    * Add llvm-rc to the tools list. This uses full path to llvm-rc in test
      RUN lines (llvm-lit -v), making them copy-pasteable.
    
    Reviewers: mstorsjo, zturner
    
    Subscribers: llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D59858
    
    llvm-svn: 357118

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -145,7 +145,7 @@ tools.extend([
     'llvm-isel-fuzzer', 'llvm-opt-fuzzer', 'llvm-lib', 'llvm-link', 'llvm-lto',
     'llvm-lto2', 'llvm-mc', 'llvm-mca', 'llvm-modextract', 'llvm-nm',
     'llvm-objcopy', 'llvm-objdump', 'llvm-pdbutil', 'llvm-profdata',
-    'llvm-ranlib', 'llvm-readelf', 'llvm-readobj', 'llvm-rtdyld', 'llvm-size',
+    'llvm-ranlib', 'llvm-rc', 'llvm-readelf', 'llvm-readobj', 'llvm-rtdyld', 'llvm-size',
     'llvm-split', 'llvm-strings', 'llvm-strip', 'llvm-tblgen', 'llvm-undname',
     'llvm-c-test', 'llvm-cxxfilt', 'llvm-xray', 'yaml2obj', 'obj2yaml',
     'yaml-bench', 'verify-uselistorder', 'bugpoint', 'llc', 'llvm-symbolizer',
