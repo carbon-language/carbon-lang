beb6efb42eca21ef5b9d1cf616558dfb991186c7
Louis Dionne <ldionne@apple.com>
2019-04-01 19:53:44 +0000

    [libcxx] Make sure reference_wrapper works with incomplete types
    Summary: Completes P0357R3, which was merged into the C++20 Working Draft in San Diego.
    
    Reviewers: EricWF, mclow.lists
    
    Subscribers: christof, jkorous, dexonsmith, libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D54722
    
    llvm-svn: 357423

---
+++ libcxx/test/std/utilities/function.objects/refwrap/refwrap.invoke/invoke.incomplete.fail.cpp
@@ -0,0 +1,37 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11, c++14, c++17
+
+// <functional>
+//
+// reference_wrapper
+//
+// template <class... ArgTypes>
+//  std::invoke_result_t<T&, ArgTypes...>
+//      operator()(ArgTypes&&... args) const;
+//
+// Requires T to be a complete type (since C++20).
+
+#include <functional>
+
+
+struct Foo;
+Foo& get_foo();
+
+void test() {
+    std::reference_wrapper<Foo> ref = get_foo();
+    ref(0); // incomplete at the point of call
+}
+
+struct Foo { void operator()(int) const { } };
+Foo& get_foo() { static Foo foo; return foo; }
+
+int main() {
+    test();
+}
