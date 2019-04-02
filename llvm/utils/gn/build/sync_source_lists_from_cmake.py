7d3e97d6fa78fc52fc3f55b074cdbcff547dbc1e
Nico Weber <nicolasweber@gmx.de>
2019-04-02 14:39:34 +0000

    gn build: Add build files for non-framework xpc clangd bits
    Differential Revision: https://reviews.llvm.org/D60124
    
    llvm-svn: 357476

--- llvm/utils/gn/build/sync_source_lists_from_cmake.py
+++ llvm/utils/gn/build/sync_source_lists_from_cmake.py
@@ -61,8 +61,7 @@ def sync_unittests():
     # Matches e.g. |add_llvm_unittest_with_input_files|.
     unittest_re = re.compile(r'^add_\S+_unittest', re.MULTILINE)
 
-    # FIXME: Add 'clang-tools-extra'.
-    checked = [ 'clang', 'lld', 'llvm' ]
+    checked = [ 'clang', 'clang-tools-extra', 'lld', 'llvm' ]
     for c in checked:
         for root, _, _ in os.walk(os.path.join(c, 'unittests')):
             cmake_file = os.path.join(root, 'CMakeLists.txt')
