98e3954fe9d6ab74888c8757284136134330e475
Don Hinton <hintonda@gmail.com>
2019-04-04 17:35:41 +0000

    [llvm-objcopy] [llvm-symbolizer] Fix failing tests
    Summary: Fix failing tests that matched substrings in path.
    
    Reviewers: evgeny777, mattd, espindola, alexshap, rupprecht, jhenderson
    
    Reviewed By: jhenderson
    
    Subscribers: Bulletmagnet, emaste, arichardson, jakehehrlich, MaskRay, rupprecht, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D60170
    
    llvm-svn: 357709

--- llvm/test/tools/llvm-symbolizer/ignore-undefined-symbols.s
+++ llvm/test/tools/llvm-symbolizer/ignore-undefined-symbols.s
@@ -1,6 +1,6 @@
 # REQUIRES: x86-registered-target
 # RUN: llvm-mc --filetype=obj --triple=x86_64-pc-linux %s -o %t.o -g
-# RUN: llvm-symbolizer --obj=%t.o 0 | FileCheck %s --implicit-check-not=bar
+# RUN: llvm-symbolizer --obj=%t.o 0 | FileCheck %s --implicit-check-not={{^}}bar{{$}}
 
 # CHECK:      foo
 # CHECK-NEXT: ignore-undefined-symbols.s:12:0
