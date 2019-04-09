563f35ab2dad6ac9ec0b9ff1c1280b9447160680
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-04-09 10:15:10 +0000

    [llvm-rtdyld] Fix missing include on MSVC builds.
    llvm-svn: 357990

--- llvm/tools/llvm-rtdyld/llvm-rtdyld.cpp
+++ llvm/tools/llvm-rtdyld/llvm-rtdyld.cpp
@@ -29,6 +29,7 @@
 #include "llvm/Support/InitLLVM.h"
 #include "llvm/Support/Memory.h"
 #include "llvm/Support/MemoryBuffer.h"
+#include "llvm/Support/MSVCErrorWorkarounds.h"
 #include "llvm/Support/Path.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/TargetSelect.h"
