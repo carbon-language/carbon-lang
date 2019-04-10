103556279fc3bbea5c0a5eba911d5afbb2875029
John McCall <rjmccall@apple.com>
2019-04-10 19:11:32 +0000

    Fix for different build configurations.
    llvm-svn: 358125

--- clang/test/CodeGen/unreachable-ret.c
+++ clang/test/CodeGen/unreachable-ret.c
@@ -5,7 +5,7 @@ extern void abort() __attribute__((noreturn));
 void f1() {
   abort();
 }
-// CHECK-LABEL: define void @f1()
+// CHECK-LABEL: define {{.*}}void @f1()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   call void @abort()
 // CHECK-NEXT:   unreachable
@@ -15,7 +15,7 @@ void *f2() {
   abort();
   return 0;
 }
-// CHECK-LABEL: define i8* @f2()
+// CHECK-LABEL: define {{.*}}i8* @f2()
 // CHECK-NEXT: entry:
 // CHECK-NEXT:   call void @abort()
 // CHECK-NEXT:   unreachable
