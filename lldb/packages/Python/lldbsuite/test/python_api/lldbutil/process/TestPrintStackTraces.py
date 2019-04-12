539b7e65b450f899a1550e8333e817028a3f093f
Pavel Labath <pavel@labath.sk>
2019-04-12 08:02:28 +0000

    Make TestPrintStackTraces deterministic
    This test contained an incredibly complicated inferior, but in reality,
    all it was testing was that we can backtrace up to main and see main's
    arguments.
    
    However, the way this was implemented (setting a breakpoint on a
    separate thread) meant that each time the test would run, it would stop
    in a different location on the main thread. Most of the time this
    location would be deep in some libc function, which meant that the
    success of this test depended on our ability to backtrace out of a
    random function of the c library that the user happens to have
    installed.
    
    This makes the test unpredictable. Backtracing out of a libc function is
    an important functionality, but this is not the way to test it. Often it
    is not even our fault that we cannot backtrace out because the C library
    contains a lot of assembly routines that may not have correct unwind
    info associated with them.
    
    For this reason the test has accumulated numerous @expectedFail/Flaky
    decorators. In this patch, I replace the inferior with one that does not
    depend on libc functions. Instead I create a couple of stack frames of
    user code, and have the test verify that. I also simplify the test by
    using lldbutil.run_to_source_breakpoint.
    
    llvm-svn: 358266

--- lldb/packages/Python/lldbsuite/test/python_api/lldbutil/process/TestPrintStackTraces.py
+++ lldb/packages/Python/lldbsuite/test/python_api/lldbutil/process/TestPrintStackTraces.py
@@ -18,47 +18,12 @@ class ThreadsStackTracesTestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
-    def setUp(self):
-        # Call super's setUp().
-        TestBase.setUp(self)
-        # Find the line number to break inside main().
-        self.line = line_number('main.cpp', '// Set break point at this line.')
-
-    # We are unable to produce a backtrace of the main thread when the thread
-    # is blocked in fgets
-    @expectedFailureAll("llvm.org/pr23043", ["linux"], archs=["i386"])
-    # The __thread_start function in libc doesn't contain any epilogue and prologue instructions
-    # hence unwinding fail when we are stopped in __thread_start
-    @expectedFailureAll(triple='mips*')
-    @expectedFailureAll(oslist=["windows"], bugnumber="llvm.org/pr24778")
-    @expectedFlakeyAndroid("llvm.org/26492", archs=["arm"])
-    @expectedFlakeyLinux("llvm.org/pr27687")
-    @expectedFailureNetBSD
     @add_test_categories(['pyapi'])
     def test_stack_traces(self):
         """Test SBprocess and SBThread APIs with printing of the stack traces."""
         self.build()
-        exe = self.getBuildArtifact("a.out")
-
-        target = self.dbg.CreateTarget(exe)
-        self.assertTrue(target, VALID_TARGET)
-
-        breakpoint = target.BreakpointCreateByLocation("main.cpp", self.line)
-        self.assertTrue(breakpoint, VALID_BREAKPOINT)
-
-        # Now launch the process, and do not stop at entry point.
-        process = target.LaunchSimple(
-            ["abc", "xyz"], None, self.get_process_working_directory())
-
-        if not process:
-            self.fail("SBTarget.LaunchProcess() failed")
-
-        import lldbsuite.test.lldbutil as lldbutil
-        if process.GetState() != lldb.eStateStopped:
-            self.fail("Process should be in the 'stopped' state, "
-                      "instead the actual state is: '%s'" %
-                      lldbutil.state_type_to_str(process.GetState()))
-
+        (_, process, _, _) = lldbutil.run_to_source_breakpoint(self,
+                "// BREAK HERE", lldb.SBFileSpec("main.cpp"))
         stacktraces = lldbutil.print_stacktraces(process, string_buffer=True)
         self.expect(stacktraces, exe=False,
-                    substrs=['(int)argc=3'])
+                    substrs=['(int)x=4', '(int)y=6', '(int)x=3', '(int)argc=1'])
