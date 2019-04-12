7243929103717eab604de1cf51d661a9efd74b3a
peter klausler <pklausler@nvidia.com>
2019-04-11 17:25:43 -0700

    [flang] Split doconcurrent04.f90 test into one with expression checks and one for later semantics pass.
    Original-commit: flang-compiler/f18@6b9c25177b606eca0cb9d8d47dd1d5ac1ee7635b
    Reviewed-on: https://github.com/flang-compiler/f18/pull/406
    Tree-same-pre-rewrite: false

---
+++ flang/test/semantics/doconcurrent08.f90
@@ -0,0 +1,25 @@
+! Copyright (c) 2019, NVIDIA CORPORATION.  All rights reserved.
+!
+! Licensed under the Apache License, Version 2.0 (the "License");
+! you may not use this file except in compliance with the License.
+! You may obtain a copy of the License at
+!
+!     http://www.apache.org/licenses/LICENSE-2.0
+!
+! Unless required by applicable law or agreed to in writing, software
+! distributed under the License is distributed on an "AS IS" BASIS,
+! WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+! See the License for the specific language governing permissions and
+! limitations under the License.
+
+! RUN: ${F18} -funparse-with-symbols %s 2>&1 | ${FileCheck} %s
+! CHECK: image control statement not allowed in DO CONCURRENT
+! CHECK: SYNC ALL
+
+subroutine do_concurrent_test1(i,n)
+  implicit none
+  integer :: i, n
+  do 10 concurrent (i = 1:n)
+     SYNC ALL
+10 continue
+end subroutine do_concurrent_test1
