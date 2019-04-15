3568e4585648c69b4ed36de3d8f3f60935e4a208
Steve Scalpone <sscalpone@nvidia.com>
2019-04-15 10:09:40 -0700

    [flang] FindContainingSubprogram returns a pointer, not a reference.
    Original-commit: flang-compiler/f18@7d221b09bd9b77800d85e9bc2a0084a95237be65

--- flang/lib/semantics/check-return.cc
+++ flang/lib/semantics/check-return.cc
@@ -39,7 +39,7 @@ void ReturnStmtChecker::Leave(const parser::ReturnStmt &returnStmt) {
   // C1575 The scalar-int-expr is allowed only in the inclusive scope of a
   // subroutine subprogram.
   const auto &scope{context_.FindScope(*context_.location())};
-  const auto &subprogramScope{FindContainingSubprogram(scope)};
+  const auto *subprogramScope{FindContainingSubprogram(scope)};
   if (subprogramScope == nullptr) {
     context_.Say(
         "RETURN must in the inclusive scope of a SUBPROGRAM"_err_en_US);
