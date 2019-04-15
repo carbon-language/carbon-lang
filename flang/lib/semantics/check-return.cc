e2d6bbc01630869b57787ebddb6545cab729d58c
Steve Scalpone <sscalpone@nvidia.com>
2019-04-14 22:16:09 -0700

    [flang] Scope is only ever set to the address of a Scope, so it can never be null. The condition should be scope->kind() != Scope::Kind::Global. And clang-format.
    Original-commit: flang-compiler/f18@b686ab548a727561e1d65976432fed48e85b28d6
    Tree-same-pre-rewrite: false

--- flang/lib/semantics/check-return.cc
+++ flang/lib/semantics/check-return.cc
@@ -22,32 +22,34 @@ namespace Fortran::semantics {
 
 const Scope *FindContainingSubprogram(const Scope &start) {
   const Scope *scope{&start};
-  while (scope != nullptr) {
+  while (scope->kind() != Scope::Kind::Global) {
     switch (scope->kind()) {
     case Scope::Kind::MainProgram:
-    case Scope::Kind::Subprogram:
-      return scope;
-    default:
-      scope = &scope->parent();
-      break;
+    case Scope::Kind::Subprogram: return scope;
+    default: scope = &scope->parent(); break;
     }
   }
   return nullptr;
 }
 
 void ReturnStmtChecker::Leave(const parser::ReturnStmt &returnStmt) {
-  // R1542 -- expression analysis validates the scalar-int-expr
-  // C1574 The return-stmt shall be in the inclusive scope of a function or subroutine subprogram.
-  // C1575 The scalar-int-expr is allowed only in the inclusive scope of a subroutine subprogram.
+  // R1542 Expression analysis validates the scalar-int-expr
+  // C1574 The return-stmt shall be in the inclusive scope of a function or
+  // subroutine subprogram.
+  // C1575 The scalar-int-expr is allowed only in the inclusive scope of a
+  // subroutine subprogram.
   auto scope{context_.FindScope(*context_.location())};
   auto subprogramScope{FindContainingSubprogram(scope)};
-  if(subprogramScope == nullptr) {
-    context_.Say("RETURN must in the inclusive scope of a SUBPROGRAM"_err_en_US);
+  if (subprogramScope == nullptr) {
+    context_.Say(
+        "RETURN must in the inclusive scope of a SUBPROGRAM"_err_en_US);
     return;
   }
-  if(returnStmt.v.has_value() && subprogramScope->kind() == Scope::Kind::Subprogram) {
-    if(IsFunction(*subprogramScope->GetSymbol())) {
-      context_.Say("RETURN with expression is only allowed in SUBROUTINE subprogram"_err_en_US);
+  if (returnStmt.v.has_value() &&
+      subprogramScope->kind() == Scope::Kind::Subprogram) {
+    if (IsFunction(*subprogramScope->GetSymbol())) {
+      context_.Say(
+          "RETURN with expression is only allowed in SUBROUTINE subprogram"_err_en_US);
     }
   }
 }
