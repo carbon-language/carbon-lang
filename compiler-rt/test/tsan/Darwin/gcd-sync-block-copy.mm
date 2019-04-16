263264345485263e318f3acc90b2ad90a8847b39
Julian Lettner <jlettner@apple.com>
2019-04-16 01:34:38 +0000

    [TSan][libdispatch] Port gcd-sync-block-copy.mm to C++
    Summary:
    Apparently, it makes a difference on where a block lives depending on if
    it's passed "inline" versus assigned and then passed via a variable.
    Both tests in this commit now give a signal, if `Block_copy` is used in
    `dispatch_sync`.
    
    Since these tests use different mechanisms (Objective-C retain versus
    C++ copy constructor) as proxies to observe if the block was copied, we
    should keep both of them.
    
    Commit, that first avoided the unnecessary copy:
    faef7d034a9ec6cb757137adce8e8670ec6c2d7b
    
    Subscribers: kubamracek, #sanitizers, llvm-commits
    
    Tags: #sanitizers, #llvm
    
    Differential Revision: https://reviews.llvm.org/D60639
    
    llvm-svn: 358469

--- compiler-rt/test/tsan/Darwin/gcd-sync-block-copy.mm
+++ compiler-rt/test/tsan/Darwin/gcd-sync-block-copy.mm
@@ -1,9 +1,9 @@
 // This test verifies that dispatch_sync() doesn't actually copy the block under TSan (without TSan, it doesn't).
 
-// RUN: %clang_tsan -fno-sanitize=thread %s -o %t_no_tsan -framework Foundation
-// RUN: %run %t_no_tsan 2>&1 | FileCheck %s
-
+// RUN: %clang_tsan %s -o %t_no_tsan   -framework Foundation -fno-sanitize=thread
 // RUN: %clang_tsan %s -o %t_with_tsan -framework Foundation
+
+// RUN: %run %t_no_tsan   2>&1 | FileCheck %s
 // RUN: %run %t_with_tsan 2>&1 | FileCheck %s
 
 #import <Foundation/Foundation.h>
@@ -22,9 +22,13 @@
 int main(int argc, const char* argv[]) {
   dispatch_queue_t q = dispatch_queue_create("my.queue", NULL);
   id object = [[MyClass alloc] init];
+  void (^block)(void) = ^ {
+    NSLog(@"%@", object);
+  };
   dispatch_sync(q, ^{
     NSLog(@"%@", object);
   });
+  dispatch_sync(q, block);
   [object release];
   NSLog(@"Done.");
   return 0;
