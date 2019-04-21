18cfd9fdaf0b01e7a9b2cc67fddfeed877c8fc8f
Nico Weber <nicolasweber@gmx.de>
2019-04-21 19:18:41 +0000

    Attempt to fix LLVM_ENABLE_THREADS=OFF build after r358665
    llvm-svn: 358858

--- clang/tools/libclang/CIndex.cpp
+++ clang/tools/libclang/CIndex.cpp
@@ -8723,7 +8723,9 @@ void clang::setThreadBackgroundPriority() {
   if (getenv("LIBCLANG_BGPRIO_DISABLE"))
     return;
 
+#if LLVM_ENABLE_THREADS
   llvm::set_thread_priority(llvm::ThreadPriority::Background);
+#endif
 }
 
 void cxindex::printDiagsToStderr(ASTUnit *Unit) {
