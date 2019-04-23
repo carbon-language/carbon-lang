27924c3a3c6744ec731fb49bda1bfc3a79442f7c
Josh Stone <jistone@redhat.com>
2019-04-23 23:43:47 +0000

    [Lint] Permit aliasing noalias readonly arguments
    Summary:
    If two arguments are both readonly, then they have no memory dependency
    that would violate noalias, even if they do actually overlap.
    
    Reviewers: hfinkel, efriedma
    
    Reviewed By: efriedma
    
    Subscribers: efriedma, hiraditya, llvm-commits, tstellar
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D60239
    
    llvm-svn: 359047

--- llvm/lib/Analysis/Lint.cpp
+++ llvm/lib/Analysis/Lint.cpp
@@ -267,10 +267,14 @@ void Lint::visitCallSite(CallSite CS) {
         if (Formal->hasNoAliasAttr() && Actual->getType()->isPointerTy()) {
           AttributeList PAL = CS.getAttributes();
           unsigned ArgNo = 0;
-          for (CallSite::arg_iterator BI = CS.arg_begin(); BI != AE; ++BI) {
+          for (CallSite::arg_iterator BI = CS.arg_begin(); BI != AE;
+               ++BI, ++ArgNo) {
             // Skip ByVal arguments since they will be memcpy'd to the callee's
             // stack so we're not really passing the pointer anyway.
-            if (PAL.hasParamAttribute(ArgNo++, Attribute::ByVal))
+            if (PAL.hasParamAttribute(ArgNo, Attribute::ByVal))
+              continue;
+            // If both arguments are readonly, they have no dependence.
+            if (Formal->onlyReadsMemory() && CS.onlyReadsMemory(ArgNo))
               continue;
             if (AI != BI && (*BI)->getType()->isPointerTy()) {
               AliasResult Result = AA->alias(*AI, *BI);
