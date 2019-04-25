9e8d0d981b0cb1e3cbd0260af4e3b1f44ddf7be2
Fangrui Song <maskray@google.com>
2019-04-25 10:25:52 +0000

    [llvm-objdump] Prep for adding newlines before and after "Disassembly of section ...:"
    llvm-svn: 359181

--- llvm/test/tools/llvm-objdump/X86/demangle.s
+++ llvm/test/tools/llvm-objdump/X86/demangle.s
@@ -1,5 +1,5 @@
 # RUN: llvm-mc %s -filetype=obj -triple=x86_64-pc-linux -o %t
-# RUN: llvm-objdump -syms -reloc -demangle %t | FileCheck %s
+# RUN: llvm-objdump -t -r --demangle %t | FileCheck %s
 
 ## Check we demangle symbols when printing relocations.
 # CHECK:      000000000000001 R_X86_64_PLT32 foo()-4
@@ -9,9 +9,8 @@
 # CHECK-NEXT: 0000000000000000 g     F .text           00000000 foo()
 
 ## Check the case when relocations are inlined into disassembly.
-# RUN: llvm-objdump -d -r -demangle %t | FileCheck %s --check-prefix=INLINE
-# INLINE:      Disassembly of section .text:
-# INLINE-NEXT: foo():
+# RUN: llvm-objdump -d -r --demangle %t | FileCheck %s --check-prefix=INLINE
+# INLINE:      foo():
 # INLINE-NEXT:  0: {{.*}}  callq   0 <_Z3foov+0x5>
 # INLINE-NEXT:  0000000000000001:  R_X86_64_PLT32 foo()-4
 
--- llvm/test/tools/llvm-objdump/X86/print-symbol-addr.s
+++ llvm/test/tools/llvm-objdump/X86/print-symbol-addr.s
@@ -2,17 +2,15 @@
 
 // Check we print the address of `foo` and `bar`.
 // RUN: llvm-objdump -d %t.o | FileCheck %s
-// CHECK:      Disassembly of section .text:
-// CHECK-NEXT: 0000000000000000 foo:
+// CHECK:      0000000000000000 foo:
 // CHECK-NEXT:   0: {{.*}}  nop
 // CHECK-NEXT:   1: {{.*}}  nop
 // CHECK:      0000000000000002 bar:
 // CHECK-NEXT:   2: {{.*}}  nop
 
 // Check we do not print the addresses with -no-leading-addr.
-// RUN: llvm-objdump -d -no-leading-addr %t.o | FileCheck %s --check-prefix=NOADDR
-// NOADDR:      Disassembly of section .text:
-// NOADDR-NEXT: {{^}}foo:
+// RUN: llvm-objdump -d --no-leading-addr %t.o | FileCheck %s --check-prefix=NOADDR
+// NOADDR:      {{^}}foo:
 // NOADDR-NEXT:   {{.*}} nop
 // NOADDR-NEXT:   {{.*}} nop
 // NOADDR:      {{^}}bar:
