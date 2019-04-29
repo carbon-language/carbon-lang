8a02f8d92801e040efecd595be9d2f2dcfaca164
Marshall Clow <mclow.lists@gmail.com>
2019-04-29 15:38:07 +0000

    Add tests specifically for LWG2164. We already did this; but now we have tests. NFC
    llvm-svn: 359458

--- libcxx/test/std/containers/sequences/vector/vector.modifiers/pop_back.pass.cpp
+++ libcxx/test/std/containers/sequences/vector/vector.modifiers/pop_back.pass.cpp
@@ -38,5 +38,17 @@ int main(int, char**)
     }
 #endif
 
+    { // LWG 526
+        int arr[] = {0, 1, 2, 3, 4};
+        int sz = 5;
+        std::vector<int> c(arr, arr+sz);
+        while (c.size() < c.capacity())
+            c.push_back(sz++);
+        c.push_back(c.front());
+        assert(c.back() == 0);
+        for (int i = 0; i < sz; ++i)
+            assert(c[i] == i);
+    }
+
   return 0;
 }
