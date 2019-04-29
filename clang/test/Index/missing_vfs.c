2ac6aca8af70ee4496667587df58f467f5240252
Douglas Yung <douglas.yung@sony.com>
2019-04-29 22:57:15 +0000

    Make test more robust by writing stdout/stderr to different files.
    Our internal build bots were failing this test randomly as the stderr
    output was emitted to the file in the middle of the stdout output
    line that the test was checking.
    
    llvm-svn: 359512

--- clang/test/Index/missing_vfs.c
+++ clang/test/Index/missing_vfs.c
@@ -1,6 +1,6 @@
-// RUN: c-index-test -test-load-source local %s -ivfsoverlay %t/does-not-exist.yaml &> %t.out
-// RUN: FileCheck -check-prefix=STDERR %s < %t.out
+// RUN: c-index-test -test-load-source local %s -ivfsoverlay %t/does-not-exist.yaml > %t.stdout 2> %t.stderr
+// RUN: FileCheck -check-prefix=STDERR %s < %t.stderr
 // STDERR: fatal error: virtual filesystem overlay file '{{.*}}' not found
-// RUN: FileCheck %s < %t.out
+// RUN: FileCheck %s < %t.stdout
 // CHECK: missing_vfs.c:[[@LINE+1]]:6: FunctionDecl=foo:[[@LINE+1]]:6
 void foo(void);
