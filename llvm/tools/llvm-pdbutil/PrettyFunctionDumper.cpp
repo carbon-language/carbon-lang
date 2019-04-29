1a52eaf7733867f1e92921a057a4cace203050b1
David Bolvansky <david.bolvansky@gmail.com>
2019-04-29 23:57:20 +0000

    [PDB] Fixed null pointer dereference
    Reviewers: zturner, rnk
    
    Reviewed By: rnk
    
    Subscribers: llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D61279
    
    llvm-svn: 359520

--- llvm/tools/llvm-pdbutil/PrettyFunctionDumper.cpp
+++ llvm/tools/llvm-pdbutil/PrettyFunctionDumper.cpp
@@ -227,7 +227,7 @@ void FunctionDumper::dump(const PDBSymbolTypeFunctionArg &Symbol) {
   // through to the real thing and dump it.
   uint32_t TypeId = Symbol.getTypeId();
   auto Type = Symbol.getSession().getSymbolById(TypeId);
-  if (Type)
+  if (!Type)
     Type->dump(*this);
   else
     Printer << "<unknown-type>";
