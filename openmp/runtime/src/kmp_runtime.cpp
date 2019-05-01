147ce2334c96b4e2dc0bbcd9ff15c4754a5248e0
Dimitry Andric <dimitry@andric.com>
2019-05-01 19:32:58 +0000

    Enable OpenMP build for 32-bit FreeBSD
    Summary:
    To be able to successfully build OpenMP on 32-bit FreeBSD, such as
    FreeBSD/i386, I first had to provide a few wrappers (see D60916), and
    then add `KMP_OS_FREEBSD` to the list of defines checked for 32-bit
    architectures in `kmp_runtime.cpp`.
    
    I have successfully built libomp.so and ran a bunch of test programs on
    FreeBSD/i386 with this.
    
    See also https://svnweb.freebsd.org/changeset/base/345283
    
    Reviewers: emaste, jlpeyton, Hahnfeld
    
    Reviewed By: jlpeyton
    
    Subscribers: krytarowski, guansong, jdoerfert, openmp-commits, llvm-commits
    
    Tags: #openmp
    
    Differential Revision: https://reviews.llvm.org/D60917
    
    llvm-svn: 359716

--- openmp/runtime/src/kmp_runtime.cpp
+++ openmp/runtime/src/kmp_runtime.cpp
@@ -8256,7 +8256,7 @@ __kmp_determine_reduction_method(
 
 #elif KMP_ARCH_X86 || KMP_ARCH_ARM || KMP_ARCH_AARCH || KMP_ARCH_MIPS
 
-#if KMP_OS_LINUX || KMP_OS_WINDOWS || KMP_OS_HURD
+#if KMP_OS_LINUX || KMP_OS_FREEBSD || KMP_OS_WINDOWS || KMP_OS_HURD
 
     // basic tuning
 
