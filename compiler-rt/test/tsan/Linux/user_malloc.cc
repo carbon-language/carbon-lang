fbcec6cad03969405e3712a73531c4703dd030a3
Vitaly Buka <vitalybuka@google.com>
2019-05-01 21:53:39 +0000

    [tsan] Fix and re-enable user_malloc.cc test
    Summary: no_sanitize_thread is not enough as it still puts some tsan instrumentation
    
    Reviewers: eugenis
    
    Subscribers: kubamracek, #sanitizers, llvm-commits
    
    Tags: #sanitizers, #llvm
    
    Differential Revision: https://reviews.llvm.org/D61393
    
    llvm-svn: 359731

--- compiler-rt/test/tsan/Linux/user_malloc.cc
+++ compiler-rt/test/tsan/Linux/user_malloc.cc
@@ -1,18 +1,14 @@
-// RUN: %clangxx_tsan -O1 %s -o %t && %run %t 2>&1 | FileCheck %s
-
-// UNSUPPORTED: powerpc64le
-
-// FIXME: Remove the test or find how to fix this.
-// On some distributions, probably with newer glibc, tsan initialization calls
-// dlsym which then calls malloc and crashes because of tsan is not initialized.
-// UNSUPPORTED: linux
+// RUN: %clangxx_tsan -c -O1 -fno-sanitize=thread %s -o %t.o
+// RUN: %clangxx_tsan -O1 %s %t.o -o %t && %run %t 2>&1 | FileCheck %s
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#if !__has_feature(thread_sanitizer)
 
 // Defined by tsan.
 extern "C" void *__interceptor_malloc(unsigned long size);
 extern "C" void __interceptor_free(void *p);
-
 extern "C" void *malloc(unsigned long size) {
   static int first = 0;
   if (__sync_lock_test_and_set(&first, 1) == 0)
@@ -24,11 +20,15 @@ extern "C" void free(void *p) {
   __interceptor_free(p);
 }
 
+#else
+
 int main() {
   volatile char *p = (char*)malloc(10);
   p[0] = 0;
   free((void*)p);
 }
+
+#endif
 
 // CHECK: user malloc
 // CHECK-NOT: ThreadSanitizer
