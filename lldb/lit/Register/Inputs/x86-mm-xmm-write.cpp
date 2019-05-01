b268a2a4af4259ce446bf85a0c136520d2aea58f
Michal Gorny <mgorny@gentoo.org>
2019-05-01 13:55:13 +0000

    [lldb] [lit] Add write tests for MM/XMM registers
    Differential Revision: https://reviews.llvm.org/D61303
    
    llvm-svn: 359681

---
+++ lldb/lit/Register/Inputs/x86-mm-xmm-write.cpp
@@ -0,0 +1,67 @@
+#include <cinttypes>
+#include <cstdint>
+#include <cstdio>
+
+struct alignas(16) xmm_t {
+  uint64_t a, b;
+};
+
+int main() {
+  constexpr xmm_t xmm_fill = { 0x0F0F0F0F0F0F0F0F, 0x0F0F0F0F0F0F0F0F };
+
+  uint64_t mm[8];
+  xmm_t xmm[8];
+
+  asm volatile(
+    "movq    %2, %%mm0\n\t"
+    "movq    %2, %%mm1\n\t"
+    "movq    %2, %%mm2\n\t"
+    "movq    %2, %%mm3\n\t"
+    "movq    %2, %%mm4\n\t"
+    "movq    %2, %%mm5\n\t"
+    "movq    %2, %%mm6\n\t"
+    "movq    %2, %%mm7\n\t"
+    "\n\t"
+    "movaps  %2, %%xmm0\n\t"
+    "movaps  %2, %%xmm1\n\t"
+    "movaps  %2, %%xmm2\n\t"
+    "movaps  %2, %%xmm3\n\t"
+    "movaps  %2, %%xmm4\n\t"
+    "movaps  %2, %%xmm5\n\t"
+    "movaps  %2, %%xmm6\n\t"
+    "movaps  %2, %%xmm7\n\t"
+    "\n\t"
+    "int3\n\t"
+    "\n\t"
+    "lea     %0, %%rbx\n\t"
+    "movq    %%mm0, 0x00(%%rbx)\n\t"
+    "movq    %%mm1, 0x08(%%rbx)\n\t"
+    "movq    %%mm2, 0x10(%%rbx)\n\t"
+    "movq    %%mm3, 0x18(%%rbx)\n\t"
+    "movq    %%mm4, 0x20(%%rbx)\n\t"
+    "movq    %%mm5, 0x28(%%rbx)\n\t"
+    "movq    %%mm6, 0x30(%%rbx)\n\t"
+    "movq    %%mm7, 0x38(%%rbx)\n\t"
+    "\n\t"
+    "lea     %1, %%rbx\n\t"
+    "movaps  %%xmm0, 0x00(%%rbx)\n\t"
+    "movaps  %%xmm1, 0x10(%%rbx)\n\t"
+    "movaps  %%xmm2, 0x20(%%rbx)\n\t"
+    "movaps  %%xmm3, 0x30(%%rbx)\n\t"
+    "movaps  %%xmm4, 0x40(%%rbx)\n\t"
+    "movaps  %%xmm5, 0x50(%%rbx)\n\t"
+    "movaps  %%xmm6, 0x60(%%rbx)\n\t"
+    "movaps  %%xmm7, 0x70(%%rbx)\n\t"
+    : "=m"(mm), "=m"(xmm)
+    : "m"(xmm_fill)
+    : "%rbx", "%mm0", "%mm1", "%mm2", "%mm3", "%mm4", "%mm5", "%mm6", "%mm7",
+      "%xmm0", "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7"
+  );
+
+  for (int i = 0; i < 8; ++i)
+    printf("mm%d = 0x%016" PRIx64 "\n", i, mm[i]);
+  for (int i = 0; i < 8; ++i)
+    printf("xmm%d = 0x%016" PRIx64 "%016" PRIx64 "\n", i, xmm[i].b, xmm[i].a);
+
+  return 0;
+}
