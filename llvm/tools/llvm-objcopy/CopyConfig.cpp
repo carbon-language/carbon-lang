e4a89a1beefe667691a906c8f1b4b3c343c4e9d3
James Henderson <jh7370@my.bristol.ac.uk>
2019-05-02 11:53:02 +0000

    [llvm-strip]Add --no-strip-all to disable --strip-all behaviour (including default stripping)
    If certain switches are not specified, llvm-strip behaves as if
    --strip-all were specified. This means that for testing, when we don't
    want the stripping behaviour, we have to specify one of these switches,
    which can be confusing. This change adds --no-strip-all to allow an
    alternative way of suppressing the default stripping, in a less
    confusing manner.
    
    Reviewed by: jakehehrlich, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D61377
    
    llvm-svn: 359781

--- llvm/tools/llvm-objcopy/CopyConfig.cpp
+++ llvm/tools/llvm-objcopy/CopyConfig.cpp
@@ -730,7 +730,8 @@ Expected<DriverConfig> parseStripOptions(ArrayRef<const char *> ArgsArr) {
             ? DiscardType::All
             : DiscardType::Locals;
   Config.StripUnneeded = InputArgs.hasArg(STRIP_strip_unneeded);
-  Config.StripAll = InputArgs.hasArg(STRIP_strip_all);
+  if (auto Arg = InputArgs.getLastArg(STRIP_strip_all, STRIP_no_strip_all))
+    Config.StripAll = Arg->getOption().getID() == STRIP_strip_all;
   Config.StripAllGNU = InputArgs.hasArg(STRIP_strip_all_gnu);
   Config.OnlyKeepDebug = InputArgs.hasArg(STRIP_only_keep_debug);
   Config.KeepFileSymbols = InputArgs.hasArg(STRIP_keep_file_symbols);
@@ -747,8 +748,9 @@ Expected<DriverConfig> parseStripOptions(ArrayRef<const char *> ArgsArr) {
   for (auto Arg : InputArgs.filtered(STRIP_keep_symbol))
     Config.SymbolsToKeep.emplace_back(Arg->getValue(), UseRegexp);
 
-  if (!Config.StripDebug && !Config.StripUnneeded &&
-      Config.DiscardMode == DiscardType::None && !Config.StripAllGNU && Config.SymbolsToRemove.empty())
+  if (!InputArgs.hasArg(STRIP_no_strip_all) && !Config.StripDebug &&
+      !Config.StripUnneeded && Config.DiscardMode == DiscardType::None &&
+      !Config.StripAllGNU && Config.SymbolsToRemove.empty())
     Config.StripAll = true;
 
   Config.DeterministicArchives =
