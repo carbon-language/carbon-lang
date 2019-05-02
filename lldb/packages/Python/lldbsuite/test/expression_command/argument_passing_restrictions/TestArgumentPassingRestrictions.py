0682fc5e5fb4174b4dc5ede3be11680b4017518d
Jonas Devlieghere <jonas@devlieghere.com>
2019-05-02 18:26:46 +0000

    Disable TestArgumentPassingRestrictions for clang < 7
    http://green.lab.llvm.org/green/view/LLDB/job/lldb-cmake-matrix/
    
    llvm-svn: 359812

--- lldb/packages/Python/lldbsuite/test/expression_command/argument_passing_restrictions/TestArgumentPassingRestrictions.py
+++ lldb/packages/Python/lldbsuite/test/expression_command/argument_passing_restrictions/TestArgumentPassingRestrictions.py
@@ -9,16 +9,17 @@ and to also make sure that the ASTImporter is copying over this
 setting when importing the CXXRecordDecl via setArgPassingRestrictions.
 """
 
-
 import lldb
 from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
+
 class TestArgumentPassingRestrictions(TestBase):
 
   mydir = TestBase.compute_mydir(__file__)
 
+  @skipIf(compiler="clang", compiler_version=['<', '7.0'])
   def test_argument_passing_restrictions(self):
     self.build()
 
