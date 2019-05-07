f496ee1f7e637145f0595103c42be5abf382522f
Jonas Devlieghere <jonas@devlieghere.com>
2019-05-07 23:14:06 +0000

    [Docs] Add timestamp
    llvm-svn: 360209

--- lldb/docs/conf.py
+++ lldb/docs/conf.py
@@ -133,7 +133,7 @@ html_extra_path = ['.htaccess']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
-#html_last_updated_fmt = '%b %d, %Y'
+html_last_updated_fmt = '%b %d, %Y'
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
