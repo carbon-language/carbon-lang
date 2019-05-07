d6d3808fa4324585e6949db6e4e8cd0b323c4406
Diana Picus <diana.picus@linaro.org>
2019-05-07 10:48:01 +0000

    [ARM GlobalISel] Widen G_INTTOPTR/G_PTRTOINT
    We actually have a couple of G_PTRTOINT to s8 when building clang, so
    we should do something about them.
    
    llvm-svn: 360130

--- llvm/lib/Target/ARM/ARMLegalizerInfo.cpp
+++ llvm/lib/Target/ARM/ARMLegalizerInfo.cpp
@@ -114,8 +114,12 @@ ARMLegalizerInfo::ARMLegalizerInfo(const ARMSubtarget &ST) {
       setAction({Op, s32}, Libcall);
   }
 
-  getActionDefinitionsBuilder(G_INTTOPTR).legalFor({{p0, s32}});
-  getActionDefinitionsBuilder(G_PTRTOINT).legalFor({{s32, p0}});
+  getActionDefinitionsBuilder(G_INTTOPTR)
+      .legalFor({{p0, s32}})
+      .minScalar(1, s32);
+  getActionDefinitionsBuilder(G_PTRTOINT)
+      .legalFor({{s32, p0}})
+      .minScalar(0, s32);
 
   getActionDefinitionsBuilder(G_CONSTANT)
       .legalFor({s32, p0})
