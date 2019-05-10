8273fdc2a4847fa4631780812820c33b422def9e
Paul Robinson <paul.robinson@sony.com>
2019-05-10 18:47:39 +0000

    Replace 'REQUIRES: nozlib' with '!zlib' because we don't need two ways to say the same thing.
    llvm-svn: 360455

--- llvm/test/MC/ELF/nocompression.s
+++ llvm/test/MC/ELF/nocompression.s
@@ -1,4 +1,4 @@
-// REQUIRES: nozlib
+// REQUIRES: !zlib
 // RUN: not llvm-mc -filetype=obj -compress-debug-sections=zlib -triple x86_64-pc-linux-gnu %s -o - 2>&1 | FileCheck %s
 // RUN: not llvm-mc -filetype=obj -compress-debug-sections=zlib-gnu -triple x86_64-pc-linux-gnu %s -o - 2>&1 | FileCheck %s
 
