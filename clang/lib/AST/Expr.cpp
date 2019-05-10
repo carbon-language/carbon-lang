c5f2544cc4b8f8957ec5730e18abe9a6c858f2cf
Dmitri Gribenko <gribozavr@gmail.com>
2019-05-10 06:39:20 +0000

    Added an assert in `isConstantInitializer`: initializer lists must be in semantic form
    Subscribers: cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D61485
    
    llvm-svn: 360406

--- clang/lib/AST/Expr.cpp
+++ clang/lib/AST/Expr.cpp
@@ -2947,6 +2947,7 @@ bool Expr::isConstantInitializer(ASTContext &Ctx, bool IsForRef,
   }
   case InitListExprClass: {
     const InitListExpr *ILE = cast<InitListExpr>(this);
+    assert(ILE->isSemanticForm() && "InitListExpr must be in semantic form");
     if (ILE->getType()->isArrayType()) {
       unsigned numInits = ILE->getNumInits();
       for (unsigned i = 0; i < numInits; i++) {
