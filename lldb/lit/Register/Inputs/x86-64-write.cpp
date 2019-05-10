8d15f9913b9e2cbae3b6f6a4ba947b20b526ebe4
Michal Gorny <mgorny@gentoo.org>
2019-05-10 13:12:36 +0000

    [lldb] [lit] Fix clobbers in x86_64 register test
    llvm-svn: 360423

--- lldb/lit/Register/Inputs/x86-64-write.cpp
+++ lldb/lit/Register/Inputs/x86-64-write.cpp
@@ -55,8 +55,8 @@ int main() {
     "movaps  %%xmm15, 0x70(%1)\n\t"
     :
     : "a"(r64), "b"(xmm), "m"(xmm_fill)
-    : "%mm0", "%mm1", "%mm2", "%mm3", "%mm4", "%mm5", "%mm6", "%mm7", "%xmm0",
-      "%xmm1", "%xmm2", "%xmm3", "%xmm4", "%xmm5", "%xmm6", "%xmm7"
+    : "%r8", "%r9", "%r10", "%r11", "%r12", "%r13", "%r14", "%r15", "%xmm8",
+      "%xmm9", "%xmm10", "%xmm11", "%xmm12", "%xmm13", "%xmm14", "%xmm15"
   );
 
   for (int i = 0; i < 8; ++i)
