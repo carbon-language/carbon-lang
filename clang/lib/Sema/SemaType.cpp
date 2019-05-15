ad08ea284ff0935acd069a1b9587bfd196e3b42b
Kevin Petit <kevin.petit@arm.com>
2019-05-15 16:39:28 +0000

    Test commit
    Remove stray space.
    
    Signed-off-by: Kevin Petit <kevin.petit@arm.com>
    llvm-svn: 360783

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -7561,7 +7561,7 @@ static void processTypeAttrs(TypeProcessingState &state, QualType &type,
             state.getDeclarator().isPrototypeContext() &&
             !hasOuterPointerLikeChunk(state.getDeclarator(), endIndex);
         if (checkNullabilityTypeSpecifier(
-              state, 
+              state,
               type,
               attr,
               allowOnArrayType)) {
