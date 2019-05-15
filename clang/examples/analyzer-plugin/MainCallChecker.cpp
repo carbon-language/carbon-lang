750a45fe2551b18a12e58f61a5defbdd23a95411
Kristof Umann <kristof.umann@ericsson.com>
2019-05-15 20:19:51 +0000

    Revert "[analyzer] Add a test for plugins using checker dependencies"
    Buildbots don't seem to find the new plugin.
    
    llvm-svn: 360805

--- clang/examples/analyzer-plugin/MainCallChecker.cpp
+++ clang/examples/analyzer-plugin/MainCallChecker.cpp
@@ -1,13 +1,13 @@
-#include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
 #include "clang/StaticAnalyzer/Core/Checker.h"
-#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
+#include "clang/StaticAnalyzer/Core/BugReporter/BugType.h"
 #include "clang/StaticAnalyzer/Frontend/CheckerRegistry.h"
+#include "clang/StaticAnalyzer/Core/PathSensitive/CheckerContext.h"
 
 using namespace clang;
 using namespace ento;
 
 namespace {
-class MainCallChecker : public Checker<check::PreStmt<CallExpr>> {
+class MainCallChecker : public Checker < check::PreStmt<CallExpr> > {
   mutable std::unique_ptr<BugType> BT;
 
 public:
@@ -15,8 +15,7 @@ public:
 };
 } // end anonymous namespace
 
-void MainCallChecker::checkPreStmt(const CallExpr *CE,
-                                   CheckerContext &C) const {
+void MainCallChecker::checkPreStmt(const CallExpr *CE, CheckerContext &C) const {
   const Expr *Callee = CE->getCallee();
   const FunctionDecl *FD = C.getSVal(Callee).getAsFunctionDecl();
 
@@ -25,7 +24,7 @@ void MainCallChecker::checkPreStmt(const CallExpr *CE,
 
   // Get the name of the callee.
   IdentifierInfo *II = FD->getIdentifier();
-  if (!II) // if no identifier, not a simple C function
+  if (!II)   // if no identifier, not a simple C function
     return;
 
   if (II->isStr("main")) {
@@ -44,11 +43,12 @@ void MainCallChecker::checkPreStmt(const CallExpr *CE,
 }
 
 // Register plugin!
-extern "C" void clang_registerCheckers(CheckerRegistry &registry) {
+extern "C"
+void clang_registerCheckers (CheckerRegistry &registry) {
   registry.addChecker<MainCallChecker>(
       "example.MainCallChecker", "Disallows calls to functions called main",
       "");
 }
 
-extern "C" const char clang_analyzerAPIVersionString[] =
-    CLANG_ANALYZER_API_VERSION_STRING;
+extern "C"
+const char clang_analyzerAPIVersionString[] = CLANG_ANALYZER_API_VERSION_STRING;
--- clang/test/Analysis/checker-plugins.c
+++ clang/test/Analysis/checker-plugins.c
@@ -1,8 +1,5 @@
-// RUN: %clang_analyze_cc1 -verify %s \
-// RUN:   -load %llvmshlibdir/SampleAnalyzerPlugin%pluginext \
-// RUN:   -analyzer-checker='example.MainCallChecker'
-
-// REQUIRES: plugins
+// RUN: %clang_analyze_cc1 -load %llvmshlibdir/SampleAnalyzerPlugin%pluginext -analyzer-checker='example.MainCallChecker' -verify %s
+// REQUIRES: plugins, examples
 
 // Test that the MainCallChecker example analyzer plugin loads and runs.
 
@@ -11,22 +8,3 @@ int main();
 void caller() {
   main(); // expected-warning {{call to main}}
 }
-
-// RUN: %clang_analyze_cc1 %s \
-// RUN:   -load %llvmshlibdir/CheckerDependencyHandlingAnalyzerPlugin%pluginext\
-// RUN:   -analyzer-checker=example.DependendentChecker \
-// RUN:   -analyzer-list-enabled-checkers \
-// RUN:   2>&1 | FileCheck %s -check-prefix=CHECK-IMPLICITLY-ENABLED
-
-// CHECK-IMPLICITLY-ENABLED: example.Dependency
-// CHECK-IMPLICITLY-ENABLED: example.DependendentChecker
-
-// RUN: %clang_analyze_cc1 %s \
-// RUN:   -load %llvmshlibdir/CheckerDependencyHandlingAnalyzerPlugin%pluginext\
-// RUN:   -analyzer-checker=example.DependendentChecker \
-// RUN:   -analyzer-disable-checker=example.Dependency \
-// RUN:   -analyzer-list-enabled-checkers \
-// RUN:   2>&1 | FileCheck %s -check-prefix=CHECK-IMPLICITLY-DISABLED
-
-// CHECK-IMPLICITLY-DISABLED-NOT: example.Dependency
-// CHECK-IMPLICITLY-DISABLED-NOT: example.DependendentChecker
