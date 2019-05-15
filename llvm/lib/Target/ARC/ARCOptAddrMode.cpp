1ca049959f579c1d5f51ffc0ed8a4d74e8a1c6c8
Pete Couperus <petecoup@synopsys.com>
2019-05-15 19:46:17 +0000

    Uncomment LLVM_FALLTHROUGH.
    llvm-svn: 360798

--- llvm/lib/Target/ARC/ARCOptAddrMode.cpp
+++ llvm/lib/Target/ARC/ARCOptAddrMode.cpp
@@ -124,7 +124,7 @@ static bool isAddConstantOp(const MachineInstr &MI, int64_t &Amount) {
   switch (MI.getOpcode()) {
   case ARC::SUB_rru6:
     Sign = -1;
-    // LLVM_FALLTHROUGH
+    LLVM_FALLTHROUGH;
   case ARC::ADD_rru6:
     assert(MI.getOperand(2).isImm() && "Expected immediate operand");
     Amount = Sign * MI.getOperand(2).getImm();
