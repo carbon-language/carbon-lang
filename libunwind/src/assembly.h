905af40cc6ce0d246e42bf7db456d8ca944caaf6
Martin Storsjo <martin@martin.st>
2019-05-16 06:49:20 +0000

    [PPC] Fix 32-bit build of libunwind
    Clang integrated assembler was unable to build libunwind PPC32 assembly code,
    present in functions used to save/restore register context.
    
    This change consists in replacing the assembly style used in libunwind source,
    to one that is compatible with both Clang integrated assembler as well as
    GNU assembler.
    
    Patch by Leandro Lupori!
    
    Differential Revision: https://reviews.llvm.org/D61792
    
    llvm-svn: 360862

--- libunwind/src/assembly.h
+++ libunwind/src/assembly.h
@@ -28,8 +28,6 @@
 #ifdef _ARCH_PWR8
 #define PPC64_HAS_VMX
 #endif
-#elif defined(__POWERPC__) || defined(__powerpc__) || defined(__ppc__)
-#define SEPARATOR @
 #elif defined(__arm64__)
 #define SEPARATOR %%
 #else
