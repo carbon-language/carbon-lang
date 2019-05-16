064f6ab556499c8b513d1dc423fc0740ad9a1408
Cullen Rhodes <cullen.rhodes@arm.com>
2019-05-16 09:07:26 +0000

    [AArch64][SVE2] Asm: add unpredicated integer multiply instructions
    Summary:
    Add support for the following instructions:
    
      * MUL (indexed and unpredicated vectors forms)
      * SQDMULH (indexed and unpredicated vectors forms)
      * SQRDMULH (indexed and unpredicated vectors forms)
      * SMULH (unpredicated, predicated form added in SVE)
      * UMULH (unpredicated, predicated form added in SVE)
      * PMUL (unpredicated)
    
    The specification can be found here:
    https://developer.arm.com/docs/ddi0602/latest
    
    Reviewed By: SjoerdMeijer, rovka
    
    Differential Revision: https://reviews.llvm.org/D61902
    
    llvm-svn: 360867

---
+++ llvm/test/MC/AArch64/SVE2/sqdmulh.s
@@ -0,0 +1,50 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2 < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve2 - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+sqdmulh z0.b, z1.b, z2.b
+// CHECK-INST: sqdmulh	z0.b, z1.b, z2.b
+// CHECK-ENCODING: [0x20,0x70,0x22,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 20 70 22 04 <unknown>
+
+sqdmulh z0.h, z1.h, z2.h
+// CHECK-INST: sqdmulh	z0.h, z1.h, z2.h
+// CHECK-ENCODING: [0x20,0x70,0x62,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 20 70 62 04 <unknown>
+
+sqdmulh z29.s, z30.s, z31.s
+// CHECK-INST: sqdmulh z29.s, z30.s, z31.s
+// CHECK-ENCODING: [0xdd,0x73,0xbf,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: dd 73 bf 04 <unknown>
+
+sqdmulh z31.d, z31.d, z31.d
+// CHECK-INST: sqdmulh z31.d, z31.d, z31.d
+// CHECK-ENCODING: [0xff,0x73,0xff,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: ff 73 ff 04 <unknown>
+
+sqdmulh z0.h, z1.h, z7.h[7]
+// CHECK-INST: sqdmulh	z0.h, z1.h, z7.h[7]
+// CHECK-ENCODING: [0x20,0xf0,0x7f,0x44]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 20 f0 7f 44 <unknown>
+
+sqdmulh z0.s, z1.s, z7.s[3]
+// CHECK-INST: sqdmulh	z0.s, z1.s, z7.s[3]
+// CHECK-ENCODING: [0x20,0xf0,0xbf,0x44]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 20 f0 bf 44 <unknown>
+
+sqdmulh z0.d, z1.d, z15.d[1]
+// CHECK-INST: sqdmulh	z0.d, z1.d, z15.d[1]
+// CHECK-ENCODING: [0x20,0xf0,0xff,0x44]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 20 f0 ff 44 <unknown>
