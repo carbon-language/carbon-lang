f12c9730ef0d147dc575c55a91ec08cad735c1c9
Jonas Toth <jonas.toth@gmail.com>
2019-05-16 12:39:22 +0000

    [clang-tidy] Removed superfluous and slightly annoying newlines in run-clang-tidy's output.
    Summary: The output of clang-tidy itself already has enough newlines, so the resulting output is more in line with the usual compiler output.
    
    Patch by svenpanne.
    
    Reviewers: alexfh, JonasToth
    
    Reviewed By: JonasToth
    
    Subscribers: JonasToth, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D61850
    
    llvm-svn: 360883

--- clang-tools-extra/clang-tidy/tool/run-clang-tidy.py
+++ clang-tools-extra/clang-tidy/tool/run-clang-tidy.py
@@ -170,10 +170,10 @@ def run_tidy(args, tmpdir, build_path, queue, lock, failed_files):
     if proc.returncode != 0:
       failed_files.append(name)
     with lock:
-      sys.stdout.write(' '.join(invocation) + '\n' + output.decode('utf-8') + '\n')
+      sys.stdout.write(' '.join(invocation) + '\n' + output.decode('utf-8'))
       if len(err) > 0:
         sys.stdout.flush()
-        sys.stderr.write(err.decode('utf-8') + '\n')
+        sys.stderr.write(err.decode('utf-8'))
     queue.task_done()
 
 
