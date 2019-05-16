fe331a67a54a9a4b2ef0276431e6e7f736f1290b
Eric Fiselier <eric@efcs.ca>
2019-05-16 22:21:42 +0000

    Fix failing source location test on Windows
    llvm-svn: 360947

--- clang/test/CodeGenCXX/builtin_FUNCTION.cpp
+++ clang/test/CodeGenCXX/builtin_FUNCTION.cpp
@@ -8,10 +8,10 @@ constexpr const char *test_default_arg(const char *f = __builtin_FUNCTION()) {
 }
 // CHECK: @[[EMPTY_STR:.+]] = private unnamed_addr constant [1 x i8] zeroinitializer, align 1
 
-// CHECK: @_ZN9test_func6globalE = global i8* getelementptr inbounds ([1 x i8], [1 x i8]* @[[EMPTY_STR]], i32 0, i32 0), align 8
+// CHECK: @_ZN9test_func6globalE = {{(dso_local )?}}global i8* getelementptr inbounds ([1 x i8], [1 x i8]* @[[EMPTY_STR]], i32 0, i32 0), align 8
 const char *global = test_default_arg();
 
-// CHECK: @_ZN9test_func10global_twoE = global i8* getelementptr inbounds ([1 x i8], [1 x i8]* @[[EMPTY_STR]], i32 0, i32 0), align 8
+// CHECK: @_ZN9test_func10global_twoE = {{(dso_local )?}}global i8* getelementptr inbounds ([1 x i8], [1 x i8]* @[[EMPTY_STR]], i32 0, i32 0), align 8
 const char *global_two = __builtin_FUNCTION();
 
 const char * const global_three = test_default_arg();
@@ -20,19 +20,19 @@ const char * const global_three = test_default_arg();
 // CHECK: @[[STR_TWO:.+]] = private unnamed_addr constant [14 x i8] c"test_func_two\00", align 1
 // CHECK: @[[STR_THREE:.+]] = private unnamed_addr constant [20 x i8] c"do_default_arg_test\00", align 1
 
-// CHECK: define i8* @_ZN9test_func13test_func_oneEv()
+// CHECK: define {{(dso_local )?}}i8* @_ZN9test_func13test_func_oneEv()
 // CHECK: ret i8* getelementptr inbounds ([14 x i8], [14 x i8]* @[[STR_ONE]], i32 0, i32 0)
 const char *test_func_one() {
   return __builtin_FUNCTION();
 }
 
-// CHECK: define i8* @_ZN9test_func13test_func_twoEv()
+// CHECK: define {{(dso_local )?}}i8* @_ZN9test_func13test_func_twoEv()
 // CHECK: ret i8* getelementptr inbounds ([14 x i8], [14 x i8]* @[[STR_TWO]], i32 0, i32 0)
 const char *test_func_two() {
   return __builtin_FUNCTION();
 }
 
-// CHECK: define void @_ZN9test_func19do_default_arg_testEv()
+// CHECK: define {{(dso_local )?}}void @_ZN9test_func19do_default_arg_testEv()
 // CHECK: %call = call i8* @_ZN9test_func16test_default_argEPKc(i8* getelementptr inbounds ([20 x i8], [20 x i8]* @[[STR_THREE]], i32 0, i32 0))
 void do_default_arg_test() {
   test_default_arg();
