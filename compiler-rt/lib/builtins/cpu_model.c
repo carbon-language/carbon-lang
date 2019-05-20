b93f8ae7a78bd260970d4d28bcd6407b778c0a28
Craig Topper <craig.topper@intel.com>
2019-05-20 16:58:38 +0000

    [X86] Add icelake-client and tremont model numbers to compiler-rt's implementation of __builtin_cpu_is.
    llvm-svn: 361175

--- compiler-rt/lib/builtins/cpu_model.c
+++ compiler-rt/lib/builtins/cpu_model.c
@@ -362,6 +362,12 @@ static void getIntelProcessorTypeAndSubtype(unsigned Family, unsigned Model,
       *Subtype = INTEL_COREI7_CANNONLAKE; // "cannonlake"
       break;
 
+    // Icelake:
+    case 0x7e:
+      *Type = INTEL_COREI7;
+      *Subtype = INTEL_COREI7_ICELAKE_CLIENT; // "icelake-client"
+      break;
+
     case 0x1c: // Most 45 nm Intel Atom processors
     case 0x26: // 45 nm Atom Lincroft
     case 0x27: // 32 nm Atom Medfield
@@ -386,6 +392,9 @@ static void getIntelProcessorTypeAndSubtype(unsigned Family, unsigned Model,
       break; // "goldmont"
     case 0x7a:
       *Type = INTEL_GOLDMONT_PLUS;
+      break;
+    case 0x86:
+      *Type = INTEL_TREMONT;
       break;
 
     case 0x57:
