1e593f342f00433c491c1f6db9dfe600a15ab2e8
Dmitri Gribenko <gribozavr@gmail.com>
2019-05-20 15:12:12 +0000

    Disable the modernize-use-trailing-return-type.cpp test in C++2a mode
    It is performing a use-of-uninitialized-value, as detected by MSan.
    
    llvm-svn: 361163

--- clang-tools-extra/test/clang-tidy/modernize-use-trailing-return-type.cpp
+++ clang-tools-extra/test/clang-tidy/modernize-use-trailing-return-type.cpp
@@ -1,4 +1,6 @@
-// RUN: %check_clang_tidy -std=c++14-or-later %s modernize-use-trailing-return-type %t -- -- -fdeclspec -fexceptions
+// RUN: %check_clang_tidy -std=c++14,c++17 %s modernize-use-trailing-return-type %t -- -- -fdeclspec -fexceptions
+// FIXME: Fix the checker to work in C++2a mode, it is performing a
+// use-of-uninitialized-value.
 
 namespace std {
     template <typename T>
