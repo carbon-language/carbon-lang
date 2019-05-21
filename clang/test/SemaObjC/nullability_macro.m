3af7cf58bb09a3c50effc795caa4abca3410efc3
Leonard Chan <leonardchan@google.com>
2019-05-21 20:12:00 +0000

    Remove unicode character from test
    llvm-svn: 361302

--- clang/test/SemaObjC/nullability_macro.m
+++ clang/test/SemaObjC/nullability_macro.m
@@ -1,12 +1,13 @@
 // Test that nullability attributes still get merged even though they are
 // wrapped with a MacroQualifiedType. This should just compile with no errors.
 // RUN: %clang_cc1 %s -Wno-objc-root-class -fsyntax-only -verify
+// expected-no-diagnostics
 #define UI_APPEARANCE_SELECTOR __attribute__((annotate("ui_appearance_selector")))
 
 @class UIColor;
 
 @interface Test
-@property(null_resettable, nonatomic, strong) UIColor *onTintColor UI_APPEARANCE_SELECTOR; // expected-warning{{treating Unicode character as whitespace}}
+@property(null_resettable, nonatomic, strong) UIColor *onTintColor UI_APPEARANCE_SELECTOR;
 @end
 
 @implementation Test
