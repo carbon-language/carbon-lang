81f433b48c1892034e7061452ffb158e5244f62d
Petr Hosek <phosek@chromium.org>
2019-05-22 21:08:33 +0000

    [runtimes] Move libunwind, libc++abi and libc++ to lib/$target/c++ and include/c++
    This change is a consequence of the discussion in "RFC: Place libs in
    Clang-dedicated directories", specifically the suggestion that
    libunwind, libc++abi and libc++ shouldn't be using Clang resource
    directory. Tools like clangd make this assumption, but this is
    currently not true for the LLVM_ENABLE_PER_TARGET_RUNTIME_DIR build.
    This change addresses that by moving the output of these libraries to
    lib/$target/c++ and include/c++ directories, leaving resource directory
    only for compiler-rt runtimes and Clang builtin headers.
    
    Differential Revision: https://reviews.llvm.org/D59168
    
    llvm-svn: 361432

--- clang/test/Driver/linux-per-target-runtime-dir.c
+++ clang/test/Driver/linux-per-target-runtime-dir.c
@@ -1,4 +1,4 @@
-// RUN: %clang -no-canonical-prefixes -x c++ %s -### -o %t.o 2>&1 \
+// RUN: %clangxx -no-canonical-prefixes -x c++ %s -### -o %t.o 2>&1 \
 // RUN:     --target=x86_64-linux-gnu \
 // RUN:     -stdlib=libc++ \
 // RUN:     -resource-dir=%S/Inputs/resource_dir_with_per_target_subdir \
@@ -9,9 +9,10 @@
 // CHECK-PER-TARGET-RUNTIME: "{{[^"]*}}clang{{[^"]*}}" "-cc1"
 // CHECK-PER-TARGET-RUNTIME: "-resource-dir" "[[RESDIR:[^"]*]]"
 // CHECK-PER-TARGET-RUNTIME: "-isysroot" "[[SYSROOT:[^"]+]]"
-// CHECK-PER-TARGET-RUNTIME: "-internal-isystem" "[[RESDIR]]/include/c++/v1"
+// CHECK-PER-TARGET-RUNTIME: "-internal-isystem" "{{.*}}/../include/c++/v1"
 // CHECK-PER-TARGET-RUNTIME: "-internal-isystem" "[[SYSROOT]]/usr/local/include"
 // CHECK-PER-TARGET-RUNTIME: "--sysroot=[[SYSROOT]]"
+// CHECK-PER-TARGET-RUNTIME: "-L{{.*}}{{/|\\\\}}..{{/|\\\\}}lib{{/|\\\\}}x86_64-linux-gnu{{/|\\\\}}c++"
 // CHECK-PER-TARGET-RUNTIME: "-L[[RESDIR]]{{/|\\\\}}x86_64-linux-gnu{{/|\\\\}}lib"
 
 // RUN: %clang -rtlib=compiler-rt -print-libgcc-file-name 2>&1 \
