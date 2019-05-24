21efe2afed7b743f37780f39b090af6145b4d527
Alina Sbirlea <asbirlea@google.com>
2019-05-24 17:40:52 +0000

    [NewPassManager] Add tuning option: LoopUnrolling [clang-change]
    Summary:
    Use CodeGenOpts's setting for loop unrolling.
    [to be coupled with D61618]
    
    Reviewers: chandlerc
    
    Subscribers: jlebar, dmgreen, cfe-commits, llvm-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D61620
    
    llvm-svn: 361653

--- clang/lib/CodeGen/BackendUtil.cpp
+++ clang/lib/CodeGen/BackendUtil.cpp
@@ -1051,6 +1051,7 @@ void EmitAssemblyHelper::EmitAssemblyWithNewPassManager(
   }
 
   PipelineTuningOptions PTO;
+  PTO.LoopUnrolling = CodeGenOpts.UnrollLoops;
   // For historical reasons, loop interleaving is set to mirror setting for loop
   // unrolling.
   PTO.LoopInterleaving = CodeGenOpts.UnrollLoops;
