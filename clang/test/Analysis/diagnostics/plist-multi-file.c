425e565783ddb7c440953ae74daf6d54f4ee9d74
Artem Dergachev <artem.dergachev@gmail.com>
2019-05-24 02:29:18 +0000

    [analyzer] NFC: Prevent multi-file plist test from spamming up the build folder.
    It was producing an HTML report with a random name on every tests run
    and never cleaned those up.
    
    llvm-svn: 361592

--- clang/test/Analysis/diagnostics/plist-multi-file.c
+++ clang/test/Analysis/diagnostics/plist-multi-file.c
@@ -1,5 +1,5 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=core -analyzer-output=plist-html -o %t.plist -verify %s
-// RUN: tail -n +11 %t.plist | %diff_plist --ignore-matching-lines=report %S/Inputs/expected-plists/plist-multi-file.c.plist -
+// RUN: %clang_analyze_cc1 -analyzer-checker=core -analyzer-output=plist-multi-file -o %t.plist -verify %s
+// RUN: tail -n +11 %t.plist | %diff_plist %S/Inputs/expected-plists/plist-multi-file.c.plist -
 
 #include "plist-multi-file.h"
 
