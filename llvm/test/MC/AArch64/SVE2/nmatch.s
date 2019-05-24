b3e58df80c592b3ce3de77455b9b8cdfd66bb7c4
Cullen Rhodes <cullen.rhodes@arm.com>
2019-05-24 10:32:01 +0000

    [AArch64][SVE2] Asm: support SVE2 String Processing Group
    Summary:
    Patch adds support for the SVE2 character match instructions MATCH and NMATCH.
    
    The specification can be found here:
    https://developer.arm.com/docs/ddi0602/latest
    
    Reviewed By: SjoerdMeijer
    
    Differential Revision: https://reviews.llvm.org/D62206
    
    llvm-svn: 361627

---
+++ llvm/test/MC/AArch64/SVE2/nmatch.s
@@ -0,0 +1,32 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2 < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve2 - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+nmatch p0.b, p0/z, z0.b, z0.b
+// CHECK-INST: nmatch p0.b, p0/z, z0.b, z0.b
+// CHECK-ENCODING: [0x10,0x80,0x20,0x45]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 10 80 20 45 <unknown>
+
+nmatch p0.h, p0/z, z0.h, z0.h
+// CHECK-INST: nmatch p0.h, p0/z, z0.h, z0.h
+// CHECK-ENCODING: [0x10,0x80,0x60,0x45]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: 10 80 60 45 <unknown>
+
+nmatch p15.b, p7/z, z30.b, z31.b
+// CHECK-INST: nmatch p15.b, p7/z, z30.b, z31.b
+// CHECK-ENCODING: [0xdf,0x9f,0x3f,0x45]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: df 9f 3f 45 <unknown>
+
+nmatch p15.h, p7/z, z30.h, z31.h
+// CHECK-INST: nmatch p15.h, p7/z, z30.h, z31.h
+// CHECK-ENCODING: [0xdf,0x9f,0x7f,0x45]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: df 9f 7f 45 <unknown>
