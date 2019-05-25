a846427ad0ac94506661a65f9949c627011a73a4
Akira Hatanaka <ahatanaka@apple.com>
2019-05-25 00:50:03 +0000

    Revert "[Analysis] Link library dependencies to Analysis plugins"
    This reverts commit r361340. The following builder has been broken for
    the past few days because of this commit:
    
    http://green.lab.llvm.org/green/job/clang-stage2-cmake-RgSan/
    
    Also revert r361399, which was committed to fix r361340.
    
    llvm-svn: 361685

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -914,14 +914,10 @@ endif()
 
 # Plugin support
 # FIXME: Make this configurable.
-if(WIN32 OR CYGWIN)
-  if(BUILD_SHARED_LIBS OR LLVM_BUILD_LLVM_DYLIB)
-    set(LLVM_ENABLE_PLUGINS ON)
-  else()
-    set(LLVM_ENABLE_PLUGINS OFF)
-  endif()
+if(BUILD_SHARED_LIBS OR LLVM_BUILD_LLVM_DYLIB)
+  set(LLVM_ENABLE_PLUGINS ON)
 else()
-  set(LLVM_ENABLE_PLUGINS ${LLVM_ENABLE_PIC})
+  set(LLVM_ENABLE_PLUGINS OFF)
 endif()
 
 # By default we should enable LLVM_ENABLE_IDE only for multi-configuration
