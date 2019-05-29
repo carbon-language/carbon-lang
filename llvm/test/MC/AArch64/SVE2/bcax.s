6c04ef3d48ab771b63d478aa9c338ad63c6f7836
Cullen Rhodes <cullen.rhodes@arm.com>
2019-05-29 09:03:27 +0000

    [AArch64][SVE2] Asm: support SVE Bitwise Logical - Unpredicated Group
    Summary:
    Patch adds support for the following instructions:
        * EOR3, BSL, BCAX, BSL1N, BSL2N, NBSL, XAR
    
    Aliases for types .B/.H/.S for EOR3 and BCAX have been added, the
    preferred disassembly is .D.
    
    The specification can be found here:
    https://developer.arm.com/docs/ddi0602/latest
    
    Reviewed By: SjoerdMeijer
    
    Differential Revision: https://reviews.llvm.org/D62387
    
    llvm-svn: 361936

---
+++ llvm/test/MC/AArch64/SVE2/bcax.s
@@ -0,0 +1,52 @@
+// RUN: llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2 < %s \
+// RUN:        | FileCheck %s --check-prefixes=CHECK-ENCODING,CHECK-INST
+// RUN: not llvm-mc -triple=aarch64 -show-encoding < %s 2>&1 \
+// RUN:        | FileCheck %s --check-prefix=CHECK-ERROR
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d -mattr=+sve2 - | FileCheck %s --check-prefix=CHECK-INST
+// RUN: llvm-mc -triple=aarch64 -filetype=obj -mattr=+sve2 < %s \
+// RUN:        | llvm-objdump -d - | FileCheck %s --check-prefix=CHECK-UNKNOWN
+
+bcax z29.d, z29.d, z30.d, z31.d
+// CHECK-INST: bcax z29.d, z29.d, z30.d, z31.d
+// CHECK-ENCODING: [0xfd,0x3b,0x7e,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: fd 3b 7e 04 <unknown>
+
+
+// --------------------------------------------------------------------------//
+// Test aliases.
+
+bcax z29.b, z29.b, z30.b, z31.b
+// CHECK-INST: bcax z29.d, z29.d, z30.d, z31.d
+// CHECK-ENCODING: [0xfd,0x3b,0x7e,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: fd 3b 7e 04 <unknown>
+
+bcax z29.h, z29.h, z30.h, z31.h
+// CHECK-INST: bcax z29.d, z29.d, z30.d, z31.d
+// CHECK-ENCODING: [0xfd,0x3b,0x7e,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: fd 3b 7e 04 <unknown>
+
+bcax z29.s, z29.s, z30.s, z31.s
+// CHECK-INST: bcax z29.d, z29.d, z30.d, z31.d
+// CHECK-ENCODING: [0xfd,0x3b,0x7e,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: fd 3b 7e 04 <unknown>
+
+
+// --------------------------------------------------------------------------//
+// Test compatibility with MOVPRFX instruction.
+
+movprfx z31, z7
+// CHECK-INST: movprfx z31, z7
+// CHECK-ENCODING: [0xff,0xbc,0x20,0x04]
+// CHECK-ERROR: instruction requires: sve
+// CHECK-UNKNOWN: ff bc 20 04 <unknown>
+
+bcax z31.d, z31.d, z30.d, z29.d
+// CHECK-INST: bcax z31.d, z31.d, z30.d, z29.d
+// CHECK-ENCODING: [0xbf,0x3b,0x7e,0x04]
+// CHECK-ERROR: instruction requires: sve2
+// CHECK-UNKNOWN: bf 3b 7e 04 <unknown>
---
+++ llvm/test/MC/AArch64/SVE2/bsl-diagnostics.s
@@ -0,0 +1,39 @@
+// RUN: not llvm-mc -triple=aarch64 -show-encoding -mattr=+sve2  2>&1 < %s| FileCheck %s
+
+
+// ------------------------------------------------------------------------- //
+// Invalid element width
+
+bsl z0.b, z0.b, z1.b, z2.b
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid element width
+// CHECK-NEXT: bsl z0.b, z0.b, z1.b, z2.b
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+bsl z0.h, z0.h, z1.h, z2.h
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid element width
+// CHECK-NEXT: bsl z0.h, z0.h, z1.h, z2.h
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+bsl z0.s, z0.s, z1.s, z2.s
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid element width
+// CHECK-NEXT: bsl z0.s, z0.s, z1.s, z2.s
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+
+// --------------------------------------------------------------------------//
+// Source and Destination Registers must match
+
+bsl z0.d, z1.d, z2.d, z3.d
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: operand must match destination register
+// CHECK-NEXT: bsl z0.d, z1.d, z2.d, z3.d
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+
+// --------------------------------------------------------------------------//
+// Negative tests for instructions that are incompatible with movprfx
+
+movprfx z0.d, p0/z, z7.d
+bsl z0.d, z0.d, z1.d, z2.d
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: instruction is unpredictable when following a predicated movprfx, suggest using unpredicated movprfx
+// CHECK-NEXT: bsl z0.d, z0.d, z1.d, z2.d
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
