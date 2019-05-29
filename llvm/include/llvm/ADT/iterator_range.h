64345f280b64ed8d51c4af88fd0cdf851186d072
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-05-29 14:39:37 +0000

    Revert rL361944 from llvm/trunk : [ADT] add iterator_range::empty() ........ Breaks windows buildbots
    llvm-svn: 361963

--- llvm/include/llvm/ADT/iterator_range.h
+++ llvm/include/llvm/ADT/iterator_range.h
@@ -44,7 +44,6 @@ public:
 
   IteratorT begin() const { return begin_iterator; }
   IteratorT end() const { return end_iterator; }
-  bool empty() const { return begin_iterator == end_iterator; }
 };
 
 /// Convenience function for iterating over sub-ranges.
