83c28abdb252670ccf557e02042195c012b45431
Stella Stamenova <stilis@microsoft.com>
2019-05-29 18:07:39 +0000

    lit: modernize the lit configuration for the lit tests
    Summary: This also normalizes the config feature that represents the windows platform to "system-windows" as opposed to having both "windows" and "system-windows"
    
    Reviewers: asmith, probinson
    
    Subscribers: delcypher, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D61798
    
    llvm-svn: 361998

--- llvm/utils/lit/tests/shtest-timeout.py
+++ llvm/utils/lit/tests/shtest-timeout.py
@@ -1,9 +1,7 @@
 # REQUIRES: python-psutil
 
 # llvm.org/PR33944
-# This should be system-windows as windows does not cover all cases, but
-# apparently neither does system-windows
-# UNSUPPORTED: windows
+# UNSUPPORTED: system-windows
 
 # FIXME: This test is fragile because it relies on time which can
 # be affected by system performance. In particular we are currently
