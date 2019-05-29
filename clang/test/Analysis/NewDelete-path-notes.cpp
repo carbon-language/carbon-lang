8193ea60f6a01319c1bae26ec186c2676f2ba5f8
Csaba Dabis <dabis.csaba98@gmail.com>
2019-05-29 20:13:29 +0000

    [analyzer] ConditionBRVisitor: Test 'add-pop-up-notes=false'
    Summary: -
    
    Reviewers: NoQ, alexfh
    
    Reviewed By: alexfh
    
    Subscribers: cfe-commits, xazax.hun, baloghadamsoftware, szepet, a.sidorin,
                 mikhail.ramalho, Szelethus, donat.nagy, dkrupp
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D61060
    
    llvm-svn: 362023

--- clang/test/Analysis/NewDelete-path-notes.cpp
+++ clang/test/Analysis/NewDelete-path-notes.cpp
@@ -1,13 +1,23 @@
-// RUN: %clang_analyze_cc1 -analyzer-checker=cplusplus.NewDelete,unix.Malloc -analyzer-output=text -verify %s
-// RUN: %clang_analyze_cc1 -analyzer-checker=cplusplus.NewDelete,unix.Malloc -analyzer-output=text -analyzer-config c++-allocator-inlining=true -verify %s
-// RUN: %clang_analyze_cc1 -analyzer-checker=cplusplus.NewDelete,unix.Malloc -analyzer-output=plist %s -o %t.plist
-// RUN: cat %t.plist | %diff_plist %S/Inputs/expected-plists/NewDelete-path-notes.cpp.plist -
+// RUN: %clang_analyze_cc1 \
+// RUN:   -analyzer-checker=cplusplus.NewDelete,unix.Malloc \
+// RUN:   -analyzer-config add-pop-up-notes=false \
+// RUN:   -analyzer-output=text -verify %s
+// RUN: %clang_analyze_cc1 \
+// RUN:   -analyzer-checker=cplusplus.NewDelete,unix.Malloc \
+// RUN:   -analyzer-config c++-allocator-inlining=true \
+// RUN:   -analyzer-config add-pop-up-notes=false \
+// RUN:   -analyzer-output=text -verify %s
+// RUN: %clang_analyze_cc1 \
+// RUN:   -analyzer-checker=cplusplus.NewDelete,unix.Malloc \
+// RUN:   -analyzer-config add-pop-up-notes=false \
+// RUN:   -analyzer-output=plist %s -o %t.plist
+// RUN: cat %t.plist | %diff_plist \
+// RUN:   %S/Inputs/expected-plists/NewDelete-path-notes.cpp.plist -
 
 void test() {
   int *p = new int;
   // expected-note@-1 {{Memory is allocated}}
-  if (p) // expected-note {{'p' is non-null}}
-         // expected-note@-1 {{Taking true branch}}
+  if (p) // expected-note {{Taking true branch}}
     delete p;
     // expected-note@-1 {{Memory is released}}
 
