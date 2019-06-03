2f66316c9688830056df8b10b1ed0651154182c7
Dmitri Gribenko <gribozavr@gmail.com>
2019-06-03 12:42:48 +0000

    Include what you use in LanaiMCCodeEmitter.cpp
    LanaiMCCodeEmitter.cpp was not using any APIs from Lanai.h, and was only
    including it for transitive dependencies.  Doing so is problematic from
    include-what-you-use perspective, but it is also a layering issue (it
    creates a dependency cycle between the primary Lanai target library and
    the MCTargetDesc library).
    
    llvm-svn: 362394

--- llvm/lib/Target/Lanai/MCTargetDesc/LanaiMCCodeEmitter.cpp
+++ llvm/lib/Target/Lanai/MCTargetDesc/LanaiMCCodeEmitter.cpp
@@ -10,7 +10,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "Lanai.h"
+#include "LanaiAluCode.h"
 #include "MCTargetDesc/LanaiBaseInfo.h"
 #include "MCTargetDesc/LanaiFixupKinds.h"
 #include "MCTargetDesc/LanaiMCExpr.h"
