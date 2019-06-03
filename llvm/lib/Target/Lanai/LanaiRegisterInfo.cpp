8e317e29da4a7391087889ef499b1d0f39db600c
Dmitri Gribenko <gribozavr@gmail.com>
2019-06-03 16:31:37 +0000

    Include what you use in LanaiRegisterInfo.cpp
    llvm-svn: 362416

--- llvm/lib/Target/Lanai/LanaiRegisterInfo.cpp
+++ llvm/lib/Target/Lanai/LanaiRegisterInfo.cpp
@@ -11,8 +11,10 @@
 //===----------------------------------------------------------------------===//
 
 #include "LanaiRegisterInfo.h"
-#include "Lanai.h"
-#include "LanaiSubtarget.h"
+#include "LanaiAluCode.h"
+#include "LanaiCondCode.h"
+#include "LanaiFrameLowering.h"
+#include "LanaiInstrInfo.h"
 #include "llvm/ADT/BitVector.h"
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/CodeGen/MachineFrameInfo.h"
