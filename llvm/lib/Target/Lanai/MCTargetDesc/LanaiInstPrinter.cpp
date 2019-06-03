7a3e4ab286cad51b706166ba5a1137b3811ff5c2
Dmitri Gribenko <gribozavr@gmail.com>
2019-06-03 12:53:05 +0000

    Include what you use in LanaiInstPrinter.cpp
    llvm-svn: 362395

--- llvm/lib/Target/Lanai/MCTargetDesc/LanaiInstPrinter.cpp
+++ llvm/lib/Target/Lanai/MCTargetDesc/LanaiInstPrinter.cpp
@@ -11,11 +11,14 @@
 //===----------------------------------------------------------------------===//
 
 #include "LanaiInstPrinter.h"
-#include "Lanai.h"
 #include "LanaiMCExpr.h"
+#include "LanaiAluCode.h"
+#include "LanaiCondCode.h"
+#include "MCTargetDesc/LanaiMCTargetDesc.h"
 #include "llvm/MC/MCAsmInfo.h"
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCInst.h"
+#include "llvm/MC/MCRegisterInfo.h"
 #include "llvm/MC/MCSymbol.h"
 #include "llvm/Support/ErrorHandling.h"
 #include "llvm/Support/FormattedStream.h"
