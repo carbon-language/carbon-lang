6d04fd15b5a2b46a844132fd724cda87d764cc9c
Eric Christopher <echristo@gmail.com>
2019-06-03 23:16:06 +0000

    Remove test/CodeGen/builtin-stackaddress.c as it duplicates test/CodeGen/2004-02-13-BuiltinFrameReturnAddress.c.
    Differential Revision: https://reviews.llvm.org/D62133
    
    llvm-svn: 362462

--- clang/test/CodeGen/builtin-stackaddress.c
+++
@@ -1,9 +0,0 @@
-// RUN: %clang_cc1 -emit-llvm < %s | grep "llvm.returnaddress"
-// RUN: %clang_cc1 -emit-llvm < %s | grep "llvm.frameaddress"
-void* a(unsigned x) {
-return __builtin_return_address(0);
-}
-
-void* c(unsigned x) {
-return __builtin_frame_address(0);
-}
