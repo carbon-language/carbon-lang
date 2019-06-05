44fb55bf96158ac3c9a90eae1818fb6b1ddefef6
Artem Dergachev <artem.dergachev@gmail.com>
2019-06-05 02:09:49 +0000

    [analyzer] exploded-graph-rewriter: Add the missing license header!
    llvm-svn: 362575

--- clang/utils/analyzer/exploded-graph-rewriter.py
+++ clang/utils/analyzer/exploded-graph-rewriter.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+#
+#===- exploded-graph-rewriter.py - ExplodedGraph dump tool -----*- python -*--#
+#
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+#
+#===-----------------------------------------------------------------------===#
+
 
 from __future__ import print_function
 
