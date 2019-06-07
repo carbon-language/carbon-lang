5f55544caef4e7d66caf584476eb7899b7ef7bcf
Peter Collingbourne <peter@pcc.me.uk>
2019-06-07 20:26:47 +0000

    Relax test so that the clang binary doesn't need to be named "clang".
    llvm-svn: 362836

--- clang/test/Driver/symbol-partition.c
+++ clang/test/Driver/symbol-partition.c
@@ -2,4 +2,4 @@
 // RUN: %clang -### -target x86_64-pc-win32 -c -fsymbol-partition=foo %s 2>&1 | FileCheck --check-prefix=ERROR %s
 
 // CHECK: "-fsymbol-partition=foo"
-// ERROR: clang: error: unsupported option '-fsymbol-partition=foo' for target 'x86_64-pc-windows-msvc'
+// ERROR: error: unsupported option '-fsymbol-partition=foo' for target 'x86_64-pc-windows-msvc'
