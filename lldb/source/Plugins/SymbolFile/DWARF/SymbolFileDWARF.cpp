62c905a2e6e350cbf7f72741540513d16f206e37
Pavel Labath <pavel@labath.sk>
2019-06-07 09:43:47 +0000

    DWARF: Simplify SymbolFileDWARF::GetDWARFCompileUnit
    Summary:
    The DWARFCompileUnit is set as the "user data" of the lldb compile unit
    directly in the constructor (see ParseCompileUnit).
    
    This means that instead of going through unit indexes, we can just fetch
    the DWARF unit directly from there.
    
    Reviewers: clayborg, JDevlieghere
    
    Subscribers: aprantl, jdoerfert, lldb-commits
    
    Differential Revision: https://reviews.llvm.org/D62943
    
    llvm-svn: 362783

--- lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/SymbolFileDWARF.cpp
@@ -608,15 +608,7 @@ SymbolFileDWARF::GetDWARFCompileUnit(lldb_private::CompileUnit *comp_unit) {
   if (!comp_unit)
     return nullptr;
 
-  DWARFDebugInfo *info = DebugInfo();
-  if (info) {
-    // The compile unit ID is the index of the DWARF unit.
-    DWARFUnit *dwarf_cu = info->GetUnitAtIndex(comp_unit->GetID());
-    if (dwarf_cu && dwarf_cu->GetUserData() == nullptr)
-      dwarf_cu->SetUserData(comp_unit);
-    return dwarf_cu;
-  }
-  return nullptr;
+  return static_cast<DWARFUnit *>(comp_unit->GetUserData());
 }
 
 DWARFDebugRangesBase *SymbolFileDWARF::GetDebugRanges() {
