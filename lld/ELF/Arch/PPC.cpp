27de3d395035f94e94c759b84679bc923294dcbe
Fangrui Song <maskray@google.com>
2019-06-08 05:19:05 +0000

    [ELF][PPC] Simplify {read,write}FromHalf16
    I've change the variable names used in PPC64.cpp from "Instr" to "Insn"
    because "Insn" is a more common abbreviation for "instruction".
    
    While changing PPC64.cpp relocateOne(), make R_PPC64_ADDR16_LO{_DS}
    slightly more efficient by saving a read and a write for the TocOptimize
    case.
    
    Reviewed By: ruiu
    
    Differential Revision: https://reviews.llvm.org/D63043
    
    llvm-svn: 362867

--- lld/ELF/Arch/PPC.cpp
+++ lld/ELF/Arch/PPC.cpp
@@ -53,11 +53,11 @@ static uint16_t lo(uint32_t V) { return V; }
 static uint16_t ha(uint32_t V) { return (V + 0x8000) >> 16; }
 
 static uint32_t readFromHalf16(const uint8_t *Loc) {
-  return read32(Loc - (Config->EKind == ELF32BEKind ? 2 : 0));
+  return read32(Config->IsLE ? Loc : Loc - 2);
 }
 
 static void writeFromHalf16(uint8_t *Loc, uint32_t Insn) {
-  write32(Loc - (Config->EKind == ELF32BEKind ? 2 : 0), Insn);
+  write32(Config->IsLE ? Loc : Loc - 2, Insn);
 }
 
 void elf::writePPC32GlinkSection(uint8_t *Buf, size_t NumEntries) {
