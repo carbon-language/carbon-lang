93f96b2be93681e5cc97187e06ad20c089210591
Dmitri Gribenko <gribozavr@gmail.com>
2019-06-13 14:27:54 +0000

    [clang-tidy] Made abseil-upgrade-duration-conversions tests pass on c++17
    Summary: Made abseil-upgrade-duration-conversions tests pass on c++17
    
    Reviewers: hokein, gribozavr
    
    Reviewed By: gribozavr
    
    Subscribers: xazax.hun, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D63262
    
    Patch by Johan Vikström.
    
    llvm-svn: 363270

--- clang-tools-extra/clang-tidy/abseil/UpgradeDurationConversionsCheck.cpp
+++ clang-tools-extra/clang-tidy/abseil/UpgradeDurationConversionsCheck.cpp
@@ -40,7 +40,8 @@ void UpgradeDurationConversionsCheck::registerMatchers(MatchFinder *Finder) {
           callee(functionDecl(
               hasParent(functionTemplateDecl()),
               unless(hasTemplateArgument(0, refersToType(builtinType()))),
-              hasAnyName("operator*=", "operator/=")))),
+              hasAnyName("operator*=", "operator/="))))
+          .bind("OuterExpr"),
       this);
 
   // Match expressions like `a.operator*=(b)` and `a.operator/=(b)` where `a`
@@ -52,7 +53,8 @@ void UpgradeDurationConversionsCheck::registerMatchers(MatchFinder *Finder) {
               hasParent(functionTemplateDecl()),
               unless(hasTemplateArgument(0, refersToType(builtinType()))),
               hasAnyName("operator*=", "operator/="))),
-          argumentCountIs(1), hasArgument(0, expr().bind("arg"))),
+          argumentCountIs(1), hasArgument(0, expr().bind("arg")))
+          .bind("OuterExpr"),
       this);
 
   // Match expressions like `a * b`, `a / b`, `operator*(a, b)`, and
@@ -66,7 +68,8 @@ void UpgradeDurationConversionsCheck::registerMatchers(MatchFinder *Finder) {
                argumentCountIs(2),
                hasArgument(0, expr(hasType(
                                   cxxRecordDecl(hasName("::absl::Duration"))))),
-               hasArgument(1, expr().bind("arg"))),
+               hasArgument(1, expr().bind("arg")))
+          .bind("OuterExpr"),
       this);
 
   // Match expressions like `a * b` and `operator*(a, b)` where `a` is not of a
@@ -77,8 +80,9 @@ void UpgradeDurationConversionsCheck::registerMatchers(MatchFinder *Finder) {
                    unless(hasTemplateArgument(0, refersToType(builtinType()))),
                    hasName("::absl::operator*"))),
                argumentCountIs(2), hasArgument(0, expr().bind("arg")),
-               hasArgument(1, expr(hasType(cxxRecordDecl(
-                                  hasName("::absl::Duration")))))),
+               hasArgument(1, expr(hasType(
+                                  cxxRecordDecl(hasName("::absl::Duration"))))))
+          .bind("OuterExpr"),
       this);
 
   // For the factory functions, we match only the non-templated overloads that
@@ -103,8 +107,9 @@ void UpgradeDurationConversionsCheck::registerMatchers(MatchFinder *Finder) {
                 has(implicitCastExpr(hasCastKind(CK_UserDefinedConversion)))),
           hasParent(callExpr(
               callee(functionDecl(DurationFactoryFunction(),
-                  unless(hasParent(functionTemplateDecl())))),
-              hasArgument(0, expr().bind("arg"))))),
+                                  unless(hasParent(functionTemplateDecl())))),
+              hasArgument(0, expr().bind("arg")))))
+          .bind("OuterExpr"),
       this);
 }
 
@@ -117,7 +122,10 @@ void UpgradeDurationConversionsCheck::check(
   const auto *ArgExpr = Result.Nodes.getNodeAs<Expr>("arg");
   SourceLocation Loc = ArgExpr->getBeginLoc();
 
-  if (!match(isInTemplateInstantiation(), *ArgExpr, *Result.Context).empty()) {
+  const auto *OuterExpr = Result.Nodes.getNodeAs<Expr>("OuterExpr");
+
+  if (!match(isInTemplateInstantiation(), *OuterExpr, *Result.Context)
+           .empty()) {
     if (MatchedTemplateLocations.count(Loc.getRawEncoding()) == 0) {
       // For each location matched in a template instantiation, we check if the
       // location can also be found in `MatchedTemplateLocations`. If it is not
