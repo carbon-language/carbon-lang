14059d2a13667409ccf2860ff7d1c56f6c8c70a6
Richard Smith <richard-llvm@metafoo.co.uk>
2019-06-14 17:46:39 +0000

    Remove unused SK_LValueToRValue initialization step.
    In addition to being unused and duplicating code, this was also wrong
    (it didn't properly mark the operand as being potentially not odr-used).
    
    This reinstates r363340, reverted in r363352.
    
    llvm-svn: 363430

--- clang/lib/Sema/SemaInit.cpp
+++ clang/lib/Sema/SemaInit.cpp
@@ -3282,7 +3282,6 @@ void InitializationSequence::Step::Destroy() {
   case SK_QualificationConversionXValue:
   case SK_QualificationConversionLValue:
   case SK_AtomicConversion:
-  case SK_LValueToRValue:
   case SK_ListInitialization:
   case SK_UnwrapInitList:
   case SK_RewrapInitList:
@@ -3462,15 +3461,6 @@ void InitializationSequence::AddQualificationConversionStep(QualType Ty,
 void InitializationSequence::AddAtomicConversionStep(QualType Ty) {
   Step S;
   S.Kind = SK_AtomicConversion;
-  S.Type = Ty;
-  Steps.push_back(S);
-}
-
-void InitializationSequence::AddLValueToRValueStep(QualType Ty) {
-  assert(!Ty.hasQualifiers() && "rvalues may not have qualifiers");
-
-  Step S;
-  S.Kind = SK_LValueToRValue;
   S.Type = Ty;
   Steps.push_back(S);
 }
@@ -7505,7 +7495,6 @@ ExprResult InitializationSequence::Perform(Sema &S,
   case SK_QualificationConversionXValue:
   case SK_QualificationConversionRValue:
   case SK_AtomicConversion:
-  case SK_LValueToRValue:
   case SK_ConversionSequence:
   case SK_ConversionSequenceNoNarrowing:
   case SK_ListInitialization:
@@ -7774,18 +7763,6 @@ ExprResult InitializationSequence::Perform(Sema &S,
       assert(CurInit.get()->isRValue() && "cannot convert glvalue to atomic");
       CurInit = S.ImpCastExprToType(CurInit.get(), Step->Type,
                                     CK_NonAtomicToAtomic, VK_RValue);
-      break;
-    }
-
-    case SK_LValueToRValue: {
-      assert(CurInit.get()->isGLValue() && "cannot load from a prvalue");
-      // C++ [conv.lval]p3:
-      //   If T is cv std::nullptr_t, the result is a null pointer constant.
-      CastKind CK =
-          Step->Type->isNullPtrType() ? CK_NullToPointer : CK_LValueToRValue;
-      CurInit =
-          ImplicitCastExpr::Create(S.Context, Step->Type, CK, CurInit.get(),
-                                   /*BasePath=*/nullptr, VK_RValue);
       break;
     }
 
@@ -9008,10 +8985,6 @@ void InitializationSequence::dump(raw_ostream &OS) const {
 
     case SK_AtomicConversion:
       OS << "non-atomic-to-atomic conversion";
-      break;
-
-    case SK_LValueToRValue:
-      OS << "load (lvalue to rvalue)";
       break;
 
     case SK_ConversionSequence:
