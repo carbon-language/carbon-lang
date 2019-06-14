2dd0053d3adc36c10b5839cb9897d18dfa13ccf8
Pavel Labath <pavel@labath.sk>
2019-06-14 14:33:04 +0000

    EditlineTest: Improve error message
    This test seems to occasionally fail because editline returns a
    different number of lines. Rewrite the message in such a way that we
    also see the actual lines when that happens (and not just their count).
    
    Also, clean up the dependencies of the test while I'm in there.
    
    llvm-svn: 363404

--- lldb/unittests/Editline/EditlineTest.cpp
+++ lldb/unittests/Editline/EditlineTest.cpp
@@ -13,10 +13,10 @@
 #include <stdio.h>
 #include <unistd.h>
 
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
 #include <memory>
 #include <thread>
-
-#include "gtest/gtest.h"
 
 #include "lldb/Host/Editline.h"
 #include "lldb/Host/FileSystem.h"
@@ -311,11 +311,11 @@ TEST_F(EditlineTestFixture, EditlineReceivesMultiLineText) {
 
   // Without any auto indentation support, our output should directly match our
   // input.
-  EXPECT_EQ(input_lines.size(), el_reported_lines.GetSize());
-  if (input_lines.size() == el_reported_lines.GetSize()) {
-    for (size_t i = 0; i < input_lines.size(); ++i)
-      EXPECT_EQ(input_lines[i], el_reported_lines[i]);
-  }
+  std::vector<std::string> reported_lines;
+  for (size_t i = 0; i < el_reported_lines.GetSize(); ++i)
+    reported_lines.push_back(el_reported_lines[i]);
+
+  EXPECT_THAT(reported_lines, testing::ContainerEq(input_lines));
 }
 
 #endif
