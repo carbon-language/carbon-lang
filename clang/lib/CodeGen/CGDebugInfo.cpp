4f3b7364a4595a547b246d00f07e38630bf2cf97
David Blaikie <dblaikie@gmail.com>
2019-06-17 19:40:52 +0000

    PR42205: DebugInfio: Do not attempt to emit debug info metadata for static member variable template partial specializations
    Would cause a crash in an attempt to create the type for the still
    unresolved 'auto' in the partial specialization (& even without the use
    of 'auto', the expression would be value dependent &
    crash/assertion-fail there).
    
    llvm-svn: 363606

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -1410,6 +1410,9 @@ void CGDebugInfo::CollectRecordFields(
             isa<VarTemplateSpecializationDecl>(V))
           continue;
 
+        if (isa<VarTemplatePartialSpecializationDecl>(V))
+          continue;
+
         // Reuse the existing static member declaration if one exists
         auto MI = StaticDataMemberCache.find(V->getCanonicalDecl());
         if (MI != StaticDataMemberCache.end()) {
