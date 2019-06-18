91185b69d12eeaa53d733d428589bd9c17b83781
Louis Dionne <ldionne@apple.com>
2019-06-18 15:01:59 +0000

    [libc++] Re-apply XFAIL to is_base_of test that was inadvertently reverted
    llvm-svn: 363689

--- libcxx/test/std/utilities/meta/meta.rel/is_base_of_union.pass.cpp
+++ libcxx/test/std/utilities/meta/meta.rel/is_base_of_union.pass.cpp
@@ -14,10 +14,11 @@
 
 #include "test_macros.h"
 
-//	Clang before v9 and apple-clang up to and including v10 do not
+//  Clang before v9 and apple-clang up to and including v10 do not
 //  report that unions are never base classes - nor can they have bases.
 //  See https://reviews.llvm.org/D61858
-// XFAIL: apple-clang-6.0, apple-clang-7.0, apple-clang-8.0, apple-clang-9.0, apple-clang-10.0
+// XFAIL: apple-clang-6.0, apple-clang-7.0, apple-clang-8.0
+// XFAIL: apple-clang-9.0, apple-clang-9.1, apple-clang-10.0
 // XFAIL: clang-3.3, clang-3.4, clang-3.5, clang-3.6, clang-3.7, clang-3.8, clang-3.9
 // XFAIL: clang-4.0, clang-5.0, clang-6.0, clang-7.0, clang-8.0
 
@@ -54,7 +55,7 @@ struct I1 {};
 
 int main(int, char**)
 {
-//	A union is never the base class of anything (including incomplete types)
+    // A union is never the base class of anything (including incomplete types)
     test_is_not_base_of<U0, B>();
     test_is_not_base_of<U0, B1>();
     test_is_not_base_of<U0, B2>();
@@ -71,7 +72,7 @@ int main(int, char**)
     test_is_not_base_of<I0, int>();
     test_is_not_base_of<I1, int>();
 
-//	A union never has base classes (including incomplete types)
+    // A union never has base classes (including incomplete types)
     test_is_not_base_of<B,  U0>();
     test_is_not_base_of<B1, U0>();
     test_is_not_base_of<B2, U0>();
