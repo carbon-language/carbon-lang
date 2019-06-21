b9b1aaf07dff947fab46b3ac6348778a8f0e2d98
Kadir Cetinkaya <kadircet@google.com>
2019-06-21 07:54:27 +0000

    [clang-tidy] Move test files of rL363975 into Inputs directory
    llvm-svn: 364008

--- clang-tools-extra/test/clang-tidy/empty-database.cpp
+++ clang-tools-extra/test/clang-tidy/empty-database.cpp
@@ -1,3 +1,3 @@
-// RUN: not clang-tidy -p %S/empty-database %s 2>&1 | FileCheck %s
+// RUN: not clang-tidy -p %S/Inputs/empty-database %s 2>&1 | FileCheck %s
 
 // CHECK: LLVM ERROR: Cannot chdir into ""!
