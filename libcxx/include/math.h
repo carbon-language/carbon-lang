3359a17b3aef1effa494da3abe7f438f5bb184a7
Eric Fiselier <eric@efcs.ca>
2019-06-23 20:28:29 +0000

    Apply new meta-programming traits throughout the library.
    The new meta-programming primitives are lower cost than the old versions. This patch removes those old versions and switches libc++ to use the new ones.
    
    llvm-svn: 364160

--- libcxx/include/math.h
+++ libcxx/include/math.h
@@ -857,7 +857,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double atan2(long double __lcpp_y, long do
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -866,8 +866,8 @@ typename std::__lazy_enable_if
 atan2(_A1 __lcpp_y, _A2 __lcpp_x) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::atan2((__result_type)__lcpp_y, (__result_type)__lcpp_x);
 }
 
@@ -952,7 +952,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double fmod(long double __lcpp_x, long dou
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -961,8 +961,8 @@ typename std::__lazy_enable_if
 fmod(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::fmod((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1030,7 +1030,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double pow(long double __lcpp_x, long doub
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1039,8 +1039,8 @@ typename std::__lazy_enable_if
 pow(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::pow((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1157,7 +1157,7 @@ copysign(long double __lcpp_x, long double __lcpp_y) _NOEXCEPT {
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1166,8 +1166,8 @@ typename std::__lazy_enable_if
 copysign(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::copysign((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1218,7 +1218,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double fdim(long double __lcpp_x, long dou
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1227,8 +1227,8 @@ typename std::__lazy_enable_if
 fdim(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::fdim((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1239,7 +1239,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double fma(long double __lcpp_x, long doub
 
 template <class _A1, class _A2, class _A3>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value &&
@@ -1249,9 +1249,9 @@ typename std::__lazy_enable_if
 fma(_A1 __lcpp_x, _A2 __lcpp_y, _A3 __lcpp_z) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2, _A3>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value &&
-                     std::is_same<_A3, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value &&
+                     std::_IsSame<_A3, __result_type>::value)), "");
     return ::fma((__result_type)__lcpp_x, (__result_type)__lcpp_y, (__result_type)__lcpp_z);
 }
 
@@ -1262,7 +1262,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double fmax(long double __lcpp_x, long dou
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1271,8 +1271,8 @@ typename std::__lazy_enable_if
 fmax(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::fmax((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1283,7 +1283,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double fmin(long double __lcpp_x, long dou
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1292,8 +1292,8 @@ typename std::__lazy_enable_if
 fmin(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::fmin((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1304,7 +1304,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double hypot(long double __lcpp_x, long do
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1313,8 +1313,8 @@ typename std::__lazy_enable_if
 hypot(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::hypot((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1427,7 +1427,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double nextafter(long double __lcpp_x, lon
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1436,8 +1436,8 @@ typename std::__lazy_enable_if
 nextafter(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::nextafter((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1458,7 +1458,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double remainder(long double __lcpp_x, lon
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1467,8 +1467,8 @@ typename std::__lazy_enable_if
 remainder(_A1 __lcpp_x, _A2 __lcpp_y) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::remainder((__result_type)__lcpp_x, (__result_type)__lcpp_y);
 }
 
@@ -1479,7 +1479,7 @@ inline _LIBCPP_INLINE_VISIBILITY long double remquo(long double __lcpp_x, long d
 
 template <class _A1, class _A2>
 inline _LIBCPP_INLINE_VISIBILITY
-typename std::__lazy_enable_if
+typename std::_EnableIf
 <
     std::is_arithmetic<_A1>::value &&
     std::is_arithmetic<_A2>::value,
@@ -1488,8 +1488,8 @@ typename std::__lazy_enable_if
 remquo(_A1 __lcpp_x, _A2 __lcpp_y, int* __lcpp_z) _NOEXCEPT
 {
     typedef typename std::__promote<_A1, _A2>::type __result_type;
-    static_assert((!(std::is_same<_A1, __result_type>::value &&
-                     std::is_same<_A2, __result_type>::value)), "");
+    static_assert((!(std::_IsSame<_A1, __result_type>::value &&
+                     std::_IsSame<_A2, __result_type>::value)), "");
     return ::remquo((__result_type)__lcpp_x, (__result_type)__lcpp_y, __lcpp_z);
 }
 
--- libcxx/test/libcxx/type_traits/lazy_metafunctions.pass.cpp
+++ libcxx/test/libcxx/type_traits/lazy_metafunctions.pass.cpp
@@ -10,7 +10,7 @@
 
 // <type_traits>
 
-// __lazy_enable_if, __lazy_not, __lazy_and and __lazy_or
+// __lazy_enable_if, __lazy_not, _And and _Or
 
 // Test the libc++ lazy meta-programming helpers in <type_traits>
 
@@ -19,8 +19,8 @@
 #include "test_macros.h"
 
 template <class Type>
-struct Identity {
-    typedef Type type;
+struct Identity : Type {
+
 };
 
 typedef std::true_type TrueT;
@@ -32,7 +32,6 @@ typedef Identity<FalseT> LazyFalseT;
 // A type that cannot be instantiated
 template <class T>
 struct CannotInst {
-    typedef T type;
     static_assert(std::is_same<T, T>::value == false, "");
 };
 
@@ -62,32 +61,21 @@ struct HasTypeImp {
 template <class Type>
 struct HasType : HasTypeImp<Type>::type {};
 
-void LazyEnableIfTest() {
-    {
-        typedef std::__lazy_enable_if<true, NextInt<0> > Result;
-        static_assert(HasType<Result>::value, "");
-        static_assert(Result::type::value == 1, "");
-    }
-    {
-        typedef std::__lazy_enable_if<false, CannotInst<int> > Result;
-        static_assert(!HasType<Result>::value, "");
-    }
-}
 
 void LazyNotTest() {
     {
-        typedef std::__lazy_not<LazyTrueT> NotT;
+        typedef std::_Not<LazyTrueT> NotT;
         static_assert(std::is_same<typename NotT::type, FalseT>::value, "");
         static_assert(NotT::value == false, "");
     }
     {
-        typedef std::__lazy_not<LazyFalseT> NotT;
+        typedef std::_Not<LazyFalseT> NotT;
         static_assert(std::is_same<typename NotT::type, TrueT>::value, "");
         static_assert(NotT::value == true, "");
     }
     {
          // Check that CannotInst<int> is not instantiated.
-        typedef std::__lazy_not<CannotInst<int> > NotT;
+        typedef std::_Not<CannotInst<int> > NotT;
 
         static_assert(std::is_same<NotT, NotT>::value, "");
 
@@ -96,42 +84,42 @@ void LazyNotTest() {
 
 void LazyAndTest() {
     { // Test that it acts as the identity function for a single value
-        static_assert(std::__lazy_and<LazyFalseT>::value == false, "");
-        static_assert(std::__lazy_and<LazyTrueT>::value == true, "");
+        static_assert(std::_And<LazyFalseT>::value == false, "");
+        static_assert(std::_And<LazyTrueT>::value == true, "");
     }
     {
-        static_assert(std::__lazy_and<LazyTrueT, LazyTrueT>::value == true, "");
-        static_assert(std::__lazy_and<LazyTrueT, LazyFalseT>::value == false, "");
-        static_assert(std::__lazy_and<LazyFalseT, LazyTrueT>::value == false, "");
-        static_assert(std::__lazy_and<LazyFalseT, LazyFalseT>::value == false, "");
+        static_assert(std::_And<LazyTrueT, LazyTrueT>::value == true, "");
+        static_assert(std::_And<LazyTrueT, LazyFalseT>::value == false, "");
+        static_assert(std::_And<LazyFalseT, LazyTrueT>::value == false, "");
+        static_assert(std::_And<LazyFalseT, LazyFalseT>::value == false, "");
     }
     { // Test short circuiting - CannotInst<T> should never be instantiated.
-        static_assert(std::__lazy_and<LazyFalseT, CannotInst<int>>::value == false, "");
-        static_assert(std::__lazy_and<LazyTrueT, LazyFalseT, CannotInst<int>>::value == false, "");
+        static_assert(std::_And<LazyFalseT, CannotInst<int>>::value == false, "");
+        static_assert(std::_And<LazyTrueT, LazyFalseT, CannotInst<int>>::value == false, "");
     }
 }
 
 
 void LazyOrTest() {
     { // Test that it acts as the identity function for a single value
-        static_assert(std::__lazy_or<LazyFalseT>::value == false, "");
-        static_assert(std::__lazy_or<LazyTrueT>::value == true, "");
+        static_assert(std::_Or<LazyFalseT>::value == false, "");
+        static_assert(std::_Or<LazyTrueT>::value == true, "");
     }
     {
-        static_assert(std::__lazy_or<LazyTrueT, LazyTrueT>::value == true, "");
-        static_assert(std::__lazy_or<LazyTrueT, LazyFalseT>::value == true, "");
-        static_assert(std::__lazy_or<LazyFalseT, LazyTrueT>::value == true, "");
-        static_assert(std::__lazy_or<LazyFalseT, LazyFalseT>::value == false, "");
+        static_assert(std::_Or<LazyTrueT, LazyTrueT>::value == true, "");
+        static_assert(std::_Or<LazyTrueT, LazyFalseT>::value == true, "");
+        static_assert(std::_Or<LazyFalseT, LazyTrueT>::value == true, "");
+        static_assert(std::_Or<LazyFalseT, LazyFalseT>::value == false, "");
     }
     { // Test short circuiting - CannotInst<T> should never be instantiated.
-        static_assert(std::__lazy_or<LazyTrueT, CannotInst<int>>::value == true, "");
-        static_assert(std::__lazy_or<LazyFalseT, LazyTrueT, CannotInst<int>>::value == true, "");
+        static_assert(std::_Or<LazyTrueT, CannotInst<int>>::value == true, "");
+        static_assert(std::_Or<LazyFalseT, LazyTrueT, CannotInst<int>>::value == true, "");
     }
 }
 
 
 int main(int, char**) {
-    LazyEnableIfTest();
+
     LazyNotTest();
     LazyAndTest();
     LazyOrTest();
