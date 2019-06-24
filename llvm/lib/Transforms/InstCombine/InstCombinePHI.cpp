b617b0808de4106096c205b9f284a19f00b0ffa4
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-06-24 13:13:36 +0000

    [InstCombine] SliceUpIllegalIntegerPHI - bail on out of range shifts
    trunc(lshr) handling - if the shift is out of range (undefined) then bail like we do for non-constant shifts.
    
    Fixes OSS Fuzz #15217
    
    llvm-svn: 364181

--- llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
@@ -1004,6 +1004,11 @@ Instruction *InstCombiner::SliceUpIllegalIntegerPHI(PHINode &FirstPhi) {
           !isa<ConstantInt>(UserI->getOperand(1)))
         return nullptr;
 
+      // Bail on out of range shifts.
+      unsigned SizeInBits = UserI->getType()->getScalarSizeInBits();
+      if (cast<ConstantInt>(UserI->getOperand(1))->getValue().uge(SizeInBits))
+        return nullptr;
+
       unsigned Shift = cast<ConstantInt>(UserI->getOperand(1))->getZExtValue();
       PHIUsers.push_back(PHIUsageRecord(PHIId, Shift, UserI->user_back()));
     }
