49885b1245c2678299e1e22c18add2511377b37e
Csaba Dabis <dabis.csaba98@gmail.com>
2019-06-25 00:44:33 +0000

    [analyzer] ExprEngine: Escape pointers in bitwise operations
    Summary:
    After evaluation it would be an Unknown value and tracking would be lost.
    
    Reviewers: NoQ, xazax.hun, ravikandhadai, baloghadamsoftware, Szelethus
    
    Reviewed By: NoQ
    
    Subscribers: szepet, rnkovacs, a.sidorin, mikhail.ramalho, donat.nagy,
                 dkrupp, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D63720
    
    llvm-svn: 364259

--- clang/lib/StaticAnalyzer/Core/ExprEngineC.cpp
+++ clang/lib/StaticAnalyzer/Core/ExprEngineC.cpp
@@ -100,6 +100,10 @@ void ExprEngine::VisitBinaryOperator(const BinaryOperator* B,
       SVal Result = evalBinOp(state, Op, LeftV, RightV, B->getType());
       if (!Result.isUnknown()) {
         state = state->BindExpr(B, LCtx, Result);
+      } else {
+        // If we cannot evaluate the operation escape the operands.
+        state = escapeValue(state, LeftV, PSK_EscapeOther);
+        state = escapeValue(state, RightV, PSK_EscapeOther);
       }
 
       Bldr.generateNode(B, *it, state);
