a5bbe8c8b93b45ecab868ea0c3b2f4ba588344c9
Djordje Todorovic <djordje.todorovic@rt-rk.com>
2019-06-26 08:35:43 +0000

    [TargetOption] Add option to ebanble the debug entry values
    The option enables debug info about parameter's entry values.
    
    ([2/13] Introduce the debug entry values.)
    
    Co-authored-by: Ananth Sowda <asowda@cisco.com>
    Co-authored-by: Nikola Prica <nikola.prica@rt-rk.com>
    Co-authored-by: Ivan Baev <ibaev@cisco.com>
    
    Differential Revision: https://reviews.llvm.org/D60961
    
    llvm-svn: 364395

--- llvm/include/llvm/Target/TargetOptions.h
+++ llvm/include/llvm/Target/TargetOptions.h
@@ -118,7 +118,8 @@ namespace llvm {
           NoTrapAfterNoreturn(false), EmulatedTLS(false),
           ExplicitEmulatedTLS(false), EnableIPRA(false),
           EmitStackSizeSection(false), EnableMachineOutliner(false),
-          SupportsDefaultOutlining(false), EmitAddrsig(false) {}
+          SupportsDefaultOutlining(false), EmitAddrsig(false),
+          EnableDebugEntryValues(false) {}
 
     /// PrintMachineCode - This flag is enabled when the -print-machineinstrs
     /// option is specified on the command line, and should enable debugging
@@ -251,6 +252,9 @@ namespace llvm {
 
     /// Emit address-significance table.
     unsigned EmitAddrsig : 1;
+
+    /// Emit debug info about parameter's entry values.
+    unsigned EnableDebugEntryValues : 1;
 
     /// FloatABIType - This setting is set by -float-abi=xxx option is specfied
     /// on the command line. This setting may either be Default, Soft, or Hard.
