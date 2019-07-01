92e78b7bedb987b1fcb7f25207df8a7381d9d7a6
Fangrui Song <maskray@google.com>
2019-07-01 11:41:07 +0000

    [RISCV] Add break; to the last switch case
    As suggested by jrtc27 in the post-commit review of D60528.
    
    llvm-svn: 364746

--- llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
+++ llvm/lib/Target/RISCV/AsmParser/RISCVAsmParser.cpp
@@ -1761,6 +1761,7 @@ bool RISCVAsmParser::processInstruction(MCInst &Inst, SMLoc IDLoc,
   case RISCV::PseudoAddTPRel:
     if (checkPseudoAddTPRel(Inst, Operands))
       return true;
+    break;
   }
 
   emitToStreamer(Out, Inst);
