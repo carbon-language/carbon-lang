d1523f7a8c2247adfee559b127206a40ce4b2591
Eric Fiselier <eric@efcs.ca>
2019-07-01 19:59:34 +0000

    Ensure bitset's string constructor doesn't poison the overload set.
    llvm-svn: 364842

--- libcxx/test/std/utilities/template.bitset/bitset.cons/string_ctor.pass.cpp
+++ libcxx/test/std/utilities/template.bitset/bitset.cons/string_ctor.pass.cpp
@@ -74,6 +74,18 @@ void test_string_ctor()
     }
 }
 
+struct Nonsense {
+    virtual ~Nonsense() {}
+};
+
+void test_for_non_eager_instantiation() {
+    // Ensure we don't accidentally instantiate `std::basic_string<Nonsense>`
+    // since it may not be well formed and can cause an error in the
+    // non-immediate context.
+    static_assert(!std::is_constructible<std::bitset<3>, Nonsense*>::value, "");
+    static_assert(!std::is_constructible<std::bitset<3>, Nonsense*, size_t, Nonsense&, Nonsense&>::value, "");
+}
+
 int main(int, char**)
 {
     test_string_ctor<0>();
@@ -85,6 +97,7 @@ int main(int, char**)
     test_string_ctor<64>();
     test_string_ctor<65>();
     test_string_ctor<1000>();
+    test_for_non_eager_instantiation();
 
   return 0;
 }
