5613874947462fd4ceb4f66926c879269609463f
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-07-02 18:20:38 +0000

    [X86] getTargetConstantBitsFromNode - remove unnecessary getZExtValue() (PR42486)
    Don't use APInt::getZExtValue() if you can avoid it - eventually someone will call it with i128 or something that doesn't fit into 64-bits.
    
    In this case it was completely superfluous as we'd moved the rest of the code to always use APInt.
    
    Fixes the <1 x i128> addition bug in PR42486
    
    llvm-svn: 364953

--- llvm/lib/Target/X86/X86ISelLowering.cpp
+++ llvm/lib/Target/X86/X86ISelLowering.cpp
@@ -5873,8 +5873,7 @@ static bool getTargetConstantBitsFromNode(SDValue Op, unsigned EltSizeInBits,
       if (UndefEltBits.getBoolValue() && !AllowPartialUndefs)
         return false;
 
-      APInt Bits = MaskBits.extractBits(EltSizeInBits, BitOffset);
-      EltBits[i] = Bits.getZExtValue();
+      EltBits[i] = MaskBits.extractBits(EltSizeInBits, BitOffset);
     }
     return true;
   };
