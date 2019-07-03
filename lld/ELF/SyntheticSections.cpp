347692e2dedb516b9a61524b1679eb21505ef0df
Fangrui Song <maskray@google.com>
2019-07-03 10:03:49 +0000

    [ELF] Allow placing SHF_MERGE sections with different alignments into the same MergeSyntheticSection
    This should fix PR42289: the Linux kernel has a use case that input
    files have .rodata.cst32 sections with different alignments. The
    expectation (and what ld.bfd and gold do) is that in the -r link, there
    is only one .rodata.cst32 (SHF_MERGE sections with different alignments
    can be combined), but lld currently creates one for each different
    alignment.
    
    The current merging strategy:
    
    1) Group SHF_MERGE sections by (name, sh_flags, sh_entsize and
       sh_addralign). String merging is performed among a group, even if -O0 is specified.
    2) Create one output section for each group. This is a special case in
       addInputSec().
    
    This patch changes 1) to:
    
    1) Group SHF_MERGE sections by (name, sh_flags, sh_entsize).
       String merging is performed among a group, even if -O0 is specified.
    
    We will thus create just one .rodata.cst32 . This also improves merging
    efficiency when sections with the same name but different alignments are
    combined.
    
    Reviewed By: ruiu
    
    Differential Revision: https://reviews.llvm.org/D63432
    
    llvm-svn: 365015

--- lld/ELF/SyntheticSections.cpp
+++ lld/ELF/SyntheticSections.cpp
@@ -2919,6 +2919,7 @@ template <class ELFT> bool VersionNeedSection<ELFT>::isNeeded() const {
 void MergeSyntheticSection::addSection(MergeInputSection *MS) {
   MS->Parent = this;
   Sections.push_back(MS);
+  Alignment = std::max(Alignment, MS->Alignment);
 }
 
 MergeTailSection::MergeTailSection(StringRef Name, uint32_t Type,
@@ -3063,7 +3064,7 @@ void elf::mergeSections() {
       // Using Entsize in here also allows us to propagate it to the synthetic
       // section.
       return Sec->Name == OutsecName && Sec->Flags == MS->Flags &&
-             Sec->Entsize == MS->Entsize && Sec->Alignment == MS->Alignment;
+             Sec->Entsize == MS->Entsize;
     });
     if (I == MergeSections.end()) {
       MergeSyntheticSection *Syn =
