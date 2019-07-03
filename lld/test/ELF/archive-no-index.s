556305fc4426a3fd8cc567b1b407143f7b5bf146
Rui Ueyama <ruiu@google.com>
2019-07-03 05:52:35 +0000

    Revert r364999: [lld] Use -o /dev/null in test when output is not needed.
    This reverts commit r364999 as it broke a build. Looks like `%t.archive.o`
    is used two lines below where it was created.
    
    llvm-svn: 365001

--- lld/test/ELF/archive-no-index.s
+++ lld/test/ELF/archive-no-index.s
@@ -1,7 +1,7 @@
 # REQUIRES: x86
 # Tests error on archive file without a symbol table
 # RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux -o %t.o %s
-# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux -o /dev/null %S/Inputs/archive.s
+# RUN: llvm-mc -filetype=obj -triple=x86_64-pc-linux -o %t.archive.o %S/Inputs/archive.s
 # RUN: rm -f %t.a
 # RUN: llvm-ar crS %t.a %t.archive.o
 
