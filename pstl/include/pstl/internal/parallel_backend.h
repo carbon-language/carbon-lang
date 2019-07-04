d9b7b2d5f7a1d6225eb9756f050d4fc22a5e3d0b
Louis Dionne <ldionne@apple.com>
2019-07-04 19:39:29 +0000

    [NFC][pstl] Do not name each header file in the leading comment
    With the renaming that will happen, it's just a pain to maintain the
    right names.
    
    llvm-svn: 365156

--- pstl/include/pstl/internal/parallel_backend.h
+++ pstl/include/pstl/internal/parallel_backend.h
@@ -1,5 +1,5 @@
 // -*- C++ -*-
-//===-- parallel_backend.h ------------------------------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
