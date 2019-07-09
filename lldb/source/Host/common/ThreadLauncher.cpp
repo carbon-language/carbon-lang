53d5f3a08d2c93b4a719151dbf7b414589ad7c90
Jonas Devlieghere <jonas@devlieghere.com>
2019-07-09 01:35:34 +0000

    Fix ASCII art header
    llvm-svn: 365421

--- lldb/source/Host/common/ThreadLauncher.cpp
+++ lldb/source/Host/common/ThreadLauncher.cpp
@@ -1,5 +1,4 @@
-//===-- ThreadLauncher.cpp ---------------------------------------*- C++
-//-*-===//
+//===-- ThreadLauncher.cpp --------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
