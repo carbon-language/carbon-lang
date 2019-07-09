848a19e4eb66b626f42ee5140dd10876591ec4c0
Matthew G McGovern <matthew.mcgovern@microsoft.com>
2019-07-09 01:47:08 +0000

    [sanitizers][windows] Rtl-Heap Interception and tests    - Adds interceptors for Rtl[Allocate|Free|Size|ReAllocate]Heap    - Adds unit tests for the new interceptors and expands HeapAlloc      tests to demonstrate new functionality.    Reviewed as D62927
    llvm-svn: 365422

--- compiler-rt/test/asan/TestCases/Windows/heapalloc_zero_size.cc
+++ compiler-rt/test/asan/TestCases/Windows/heapalloc_zero_size.cc
@@ -6,7 +6,7 @@
 // XFAIL: asan-64-bits
 #include <cassert>
 #include <stdio.h>
-#include<windows.h>
+#include <windows.h>
 
 int main() {
   HANDLE heap = HeapCreate(0, 0, 0);
---
+++ compiler-rt/test/asan/TestCases/Windows/rtlallocateheap_flags_fallback.cc
@@ -0,0 +1,45 @@
+// RUN: %clang_cl_asan -O0 %s -Fe%t /MD
+// RUN: %env_asan_opts=windows_hook_rtl_allocators=true %run %t 2>&1 | FileCheck %s
+// XFAIL: asan-64-bits
+// REQUIRES: asan-rtl-heap-interception
+
+#include <assert.h>
+#include <stdio.h>
+#include <windows.h>
+
+extern "C" int __sanitizer_get_ownership(const volatile void *p);
+using AllocateFunctionPtr = PVOID(__stdcall *)(PVOID, ULONG, SIZE_T);
+using FreeFunctionPtr = PVOID(__stdcall *)(PVOID, ULONG, PVOID);
+
+int main() {
+  HMODULE NtDllHandle = GetModuleHandle("ntdll.dll");
+  if (!NtDllHandle) {
+    puts("Couldn't load ntdll??");
+    return -1;
+  }
+
+  auto RtlAllocateHeap_ptr = (AllocateFunctionPtr)GetProcAddress(NtDllHandle, "RtlAllocateHeap");
+  if (RtlAllocateHeap_ptr == 0) {
+    puts("Couldn't RtlAllocateHeap");
+    return -1;
+  }
+
+  auto RtlFreeHeap_ptr = (FreeFunctionPtr)GetProcAddress(NtDllHandle, "RtlFreeHeap");
+  if (RtlFreeHeap_ptr == 0) {
+    puts("Couldn't RtlFreeHeap");
+    return -1;
+  }
+
+  char *winbuf;
+  char *asanbuf;
+  winbuf = (char *)RtlAllocateHeap_ptr(GetProcessHeap(), HEAP_GENERATE_EXCEPTIONS, 32),
+  asanbuf = (char *)RtlAllocateHeap_ptr(GetProcessHeap(), 0, 32),
+  winbuf[0] = 'a';
+  assert(!__sanitizer_get_ownership(winbuf));
+  assert(__sanitizer_get_ownership(asanbuf));
+
+  RtlFreeHeap_ptr(GetProcessHeap(), 0, winbuf);
+  RtlFreeHeap_ptr(GetProcessHeap(), 0, asanbuf);
+  puts("Okay");
+  // CHECK: Okay
+}
