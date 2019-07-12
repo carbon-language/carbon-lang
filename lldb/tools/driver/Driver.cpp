320d6795b29efee3f5d47e2c88dcd74d621e482f
Nathan Lanza <nathan@lanza.io>
2019-07-12 04:43:46 +0000

    [windows] re-call signal in sigint_handler for Windows
    Summary:
    Windows requires re-setting the signal handler each time it is used
    and thus ctrl-c was not behaving properly on Windows
    
    Reviewers: jfb
    
    Differential Revision: https://reviews.llvm.org/D64046
    
    llvm-svn: 365868

--- lldb/tools/driver/Driver.cpp
+++ lldb/tools/driver/Driver.cpp
@@ -706,6 +706,9 @@ void sigwinch_handler(int signo) {
 }
 
 void sigint_handler(int signo) {
+#ifdef _WIN32 // Restore handler as it is not persistent on Windows
+  signal(SIGINT, sigint_handler);
+#endif
   static std::atomic_flag g_interrupt_sent = ATOMIC_FLAG_INIT;
   if (g_driver != nullptr) {
     if (!g_interrupt_sent.test_and_set()) {
