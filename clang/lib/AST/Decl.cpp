118ee5f2e06a9972bd9fd171444cc080c03b0b99
Akira Hatanaka <ahatanaka@apple.com>
2019-07-13 03:59:55 +0000

    Initialize the non-trivial C union bits I added to RecordDeclBitfields in r365985
    These bits weren't being initialized in the RecordDecl's constructor,
    which probably caused test/Modules/stress1.cpp to fail on a couple of
    bots.
    
    llvm-svn: 365989

--- clang/lib/AST/Decl.cpp
+++ clang/lib/AST/Decl.cpp
@@ -4252,6 +4252,9 @@ RecordDecl::RecordDecl(Kind DK, TagKind TK, const ASTContext &C,
   setNonTrivialToPrimitiveDefaultInitialize(false);
   setNonTrivialToPrimitiveCopy(false);
   setNonTrivialToPrimitiveDestroy(false);
+  setHasNonTrivialToPrimitiveDefaultInitializeCUnion(false);
+  setHasNonTrivialToPrimitiveDestructCUnion(false);
+  setHasNonTrivialToPrimitiveCopyCUnion(false);
   setParamDestroyedInCallee(false);
   setArgPassingRestrictions(APK_CanPassInRegs);
 }
