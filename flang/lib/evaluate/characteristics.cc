8c0aa901852d1b7d38d6376c8f2d6147959979d3
peter klausler <pklausler@nvidia.com>
2019-07-15 13:04:32 -0700

    [flang] Handle USE and host associations in characteristics analysis
    Original-commit: flang-compiler/f18@1625f72e35a4ec5f8626b2dd07776f49f1a84f5a
    Reviewed-on: https://github.com/flang-compiler/f18/pull/565
    Tree-same-pre-rewrite: false

--- flang/lib/evaluate/characteristics.cc
+++ flang/lib/evaluate/characteristics.cc
@@ -57,6 +57,12 @@ std::optional<TypeAndShape> TypeAndShape::Characterize(
               return Characterize(*interface.symbol());
             }
           },
+          [&](const semantics::UseDetails &use) {
+            return Characterize(use.symbol());
+          },
+          [&](const semantics::HostAssocDetails &assoc) {
+            return Characterize(assoc.symbol());
+          },
           [](const auto &) -> std::optional<TypeAndShape> {
             return std::nullopt;
           },
@@ -420,12 +426,13 @@ std::optional<Procedure> Procedure::Characterize(
             }
             return result;
           },
-          [](const semantics::GenericDetails &) -> std::optional<Procedure> {
-            return std::nullopt;
+          [&](const semantics::UseDetails &use) {
+            return Characterize(use.symbol(), intrinsics);
           },
-          [](const semantics::GenericBindingDetails &)
-              -> std::optional<Procedure> { return std::nullopt; },
-          [](const auto &) -> std::optional<Procedure> { CRASH_NO_CASE; },
+          [&](const semantics::HostAssocDetails &assoc) {
+            return Characterize(assoc.symbol(), intrinsics);
+          },
+          [](const auto &) -> std::optional<Procedure> { return std::nullopt; },
       },
       symbol.details());
 }
