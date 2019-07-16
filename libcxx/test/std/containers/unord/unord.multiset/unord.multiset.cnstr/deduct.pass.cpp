12154ee8f1ee170bac97ef238dece50d504de08a
Louis Dionne <ldionne@apple.com>
2019-07-16 20:41:33 +0000

    [libc++] Add missing UNSUPPORTED for CTAD tests
    The tests for unordered_set and unordered_multiset were missing UNSUPPORTED
    markup for Apple Clang 9.1, which is still being used on some CI bots.
    
    llvm-svn: 366259

--- libcxx/test/std/containers/unord/unord.multiset/unord.multiset.cnstr/deduct.pass.cpp
+++ libcxx/test/std/containers/unord/unord.multiset/unord.multiset.cnstr/deduct.pass.cpp
@@ -9,6 +9,7 @@
 // <unordered_set>
 // UNSUPPORTED: c++98, c++03, c++11, c++14
 // UNSUPPORTED: libcpp-no-deduction-guides
+// UNSUPPORTED: apple-clang-9.1
 
 // template<class InputIterator,
 //        class Hash = hash<iter-value-type<InputIterator>>,
