b8d352a08bc6530a9de442af8f55649199481f1b
Alex Bradbury <asb@lowrisc.org>
2019-07-18 07:52:41 +0000

    [RISCV] Reset NoPHIS MachineFunctionProperty in emitSelectPseudo
    We insered PHIS were there were none before, so the property must be
    reset. This error was found on an EXPENSIVE_CHECKS build.
    
    llvm-svn: 366412

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -1308,6 +1308,7 @@ static MachineBasicBlock *emitSelectPseudo(MachineInstr &MI,
     SelectMBBI = Next;
   }
 
+  F->getProperties().reset(MachineFunctionProperties::Property::NoPHIs);
   return TailMBB;
 }
 
