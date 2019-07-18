cf41271448bdace4b14f4ec7b714086a9bdf6cae
Ilya Biryukov <ibiryukov@google.com>
2019-07-18 16:24:09 +0000

    [ASTUnit] Attempt to unbreak Windows buildbots after r366448
    llvm-svn: 366457

--- clang/test/Index/complete-qualified-cached.cpp
+++ clang/test/Index/complete-qualified-cached.cpp
@@ -5,7 +5,7 @@ Class::
 // START-OF-LINE: a_namespace
 // START-OF-LINE: Class
 // -- Using cached completions.
-// RUN: CINDEXTEST_EDITING=1 c-index-test -code-completion-at=%s:3:1 %s \
+// RUN: env CINDEXTEST_EDITING=1 c-index-test -code-completion-at=%s:3:1 %s \
 // RUN: | FileCheck --check-prefix=START-OF-LINE %s
 // -- Without cached completions.
 // RUN: c-index-test -code-completion-at=%s:3:1 %s \
@@ -15,7 +15,7 @@ Class::
 // ... and should not be available after 'Class::^'
 // AFTER-QUALIFIER: Class
 // -- Using cached completions.
-// RUN: CINDEXTEST_EDITING=1 c-index-test -code-completion-at=%s:3:8 %s \
+// RUN: env CINDEXTEST_EDITING=1 c-index-test -code-completion-at=%s:3:8 %s \
 // RUN: | FileCheck --implicit-check-not=a_namespace --check-prefix=AFTER-QUALIFIER %s
 // -- Without cached completions.
 // RUN: c-index-test -code-completion-at=%s:3:8 %s \
