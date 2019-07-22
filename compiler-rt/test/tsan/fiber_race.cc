2ef9ec40504b280cf99f65e8a0105c5b980a8e7d
Julian Lettner <jlettner@apple.com>
2019-07-22 21:13:19 +0000

    [TSan] Enable fiber tests on iOS simulator
    These tests *do not* work on device, but they *do* work in the
    simulator.
    
    rdar://53403778
    
    llvm-svn: 366738

--- compiler-rt/test/tsan/fiber_race.cc
+++ compiler-rt/test/tsan/fiber_race.cc
@@ -1,6 +1,6 @@
 // RUN: %clang_tsan -O1 %s -o %t && %deflake %run %t 2>&1 | FileCheck %s
 // UNSUPPORTED: tvos, watchos
-// XFAIL: ios
+// XFAIL: ios && !iossim
 #include "sanitizer_common/sanitizer_ucontext.h"
 #include "test.h"
 
