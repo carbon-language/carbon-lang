d2b45f299ebac457d088a52aa1dd4f57ec254433
Guillaume Chatelet <gchatelet@google.com>
2019-07-22 15:34:27 +0000

    [Polly] Clang complains about missing virtual destructor
    Summary: Feel free to reassign if needed.
    
    Reviewers: mhalk, bollu, jdoerfert
    
    Reviewed By: jdoerfert
    
    Subscribers: jdoerfert, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D64786
    
    llvm-svn: 366697

--- polly/include/polly/CodeGen/LoopGenerators.h
+++ polly/include/polly/CodeGen/LoopGenerators.h
@@ -118,6 +118,8 @@ public:
             Type::getIntNTy(Builder.getContext(), DL.getPointerSizeInBits())),
         M(Builder.GetInsertBlock()->getParent()->getParent()) {}
 
+  virtual ~ParallelLoopGenerator() {}
+
   /// Create a parallel loop.
   ///
   /// This function is the main function to automatically generate a parallel
