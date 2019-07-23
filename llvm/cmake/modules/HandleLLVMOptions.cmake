2b7f3a2b33524de62011a507a0c9521698c57f8d
Rainer Orth <ro@gcc.gnu.org>
2019-07-23 16:27:34 +0000

    [CMake] Add -z defs on Solaris
    Like other ELF targets, shared objects should be linked with -z defs on Solaris.
    
    Tested on x86_64-pc-solaris2.11 and sparcv9-sun-solaris2.11.
    
    Differential Revision: https://reviews.llvm.org/D64484
    
    llvm-svn: 366821

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -169,7 +169,7 @@ endif()
 
 # Pass -Wl,-z,defs. This makes sure all symbols are defined. Otherwise a DSO
 # build might work on ELF but fail on MachO/COFF.
-if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX" OR
+if(NOT (${CMAKE_SYSTEM_NAME} MATCHES "Darwin|FreeBSD|OpenBSD|DragonFly|AIX|SunOS" OR
         WIN32 OR CYGWIN) AND
    NOT LLVM_USE_SANITIZER)
   set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -Wl,-z,defs")
