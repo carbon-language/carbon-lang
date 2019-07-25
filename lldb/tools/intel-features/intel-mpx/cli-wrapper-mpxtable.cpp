5b406d7058bfc50db291ef435d28843bb9d5c5f3
Fangrui Song <maskray@google.com>
2019-07-25 05:15:50 +0000

    cli-wrapper-mpxtable.cpp: fix file header
    llvm-svn: 366983

--- lldb/tools/intel-features/intel-mpx/cli-wrapper-mpxtable.cpp
+++ lldb/tools/intel-features/intel-mpx/cli-wrapper-mpxtable.cpp
@@ -1,5 +1,4 @@
-//===-- cli-wrapper-mpxtable.cpp----------------------------------*- C++
-//-*-===//
+//===-- cli-wrapper-mpxtable.cpp --------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
