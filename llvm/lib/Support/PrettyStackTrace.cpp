463e9bdfa9dd15d4aa4369e6a7b24a200618c198
JF Bastien <jfbastien@apple.com>
2019-07-25 16:50:10 +0000

    Fix unused function from r367031
    llvm-svn: 367035

--- llvm/lib/Support/PrettyStackTrace.cpp
+++ llvm/lib/Support/PrettyStackTrace.cpp
@@ -121,7 +121,7 @@ extern "C" const char *__crashreporter_info__
 asm(".desc ___crashreporter_info__, 0x10");
 #endif
 
-static void setCrashLogMessage(const char *msg) {
+static void setCrashLogMessage(const char *msg) LLVM_ATTRIBUTE_UNUSED {
 #ifdef HAVE_CRASHREPORTERCLIENT_H
   (void)CRSetCrashLogMessage(msg);
 #elif HAVE_CRASHREPORTER_INFO
