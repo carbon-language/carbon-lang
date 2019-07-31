ebbce04c14bbbed3c73e3ac32b49f49fd5743aa1
Nico Weber <nicolasweber@gmx.de>
2019-07-31 18:01:55 +0000

    compiler-rt: Rename .cc files in lib/interception to .cpp.
    See https://reviews.llvm.org/D58620 for discussion, and for the commands
    I ran. In addition I also ran
    
      for f in $(svn diff | diffstat | grep .cc | cut -f 2 -d ' '); do rg $f . ; done
    
    and manually updated references to renamed files found by that.
    
    llvm-svn: 367456

--- compiler-rt/lib/interception/interception_mac.cpp
+++ compiler-rt/lib/interception/interception_mac.cpp
@@ -1,4 +1,4 @@
-//===-- interception_mac.cc -------------------------------------*- C++ -*-===//
+//===-- interception_mac.cpp ------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
