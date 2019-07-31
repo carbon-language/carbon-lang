0e86e7fda32e3ff2a241e75ef9f2864ce2e2e642
Nico Weber <nicolasweber@gmx.de>
2019-07-31 20:12:19 +0000

    Attempt to heal bots after r367467
    llvm-svn: 367473

--- compiler-rt/lib/sanitizer_common/scripts/check_lint.sh
+++ compiler-rt/lib/sanitizer_common/scripts/check_lint.sh
@@ -72,7 +72,7 @@ run_lint ${SANITIZER_INCLUDES_LINT_FILTER} ${SANITIZER_INCLUDES}/*.h &
 COMMON_RTL=${COMPILER_RT}/lib/sanitizer_common
 run_lint ${COMMON_RTL_INC_LINT_FILTER} ${COMMON_RTL}/*.cpp \
                                        ${COMMON_RTL}/*.h \
-                                       ${COMMON_RTL}/tests/*.cc &
+                                       ${COMMON_RTL}/tests/*.cpp &
 
 # Interception
 INTERCEPTION=${COMPILER_RT}/lib/interception
