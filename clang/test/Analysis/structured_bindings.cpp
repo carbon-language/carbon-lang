5c2d5f066fba2c080438a8643550b544ac466f5d
Nico Weber <nicolasweber@gmx.de>
2019-08-01 15:06:57 +0000

    Rename two clang tests from .cc to .cpp.
    clang/test/lit.cfg.py doesn't list .cc as test extension, so these
    tests never ran.
    
    Tweak one of the two tests to actually pass, now that it runs.
    (The other one was already passing.)
    
    llvm-svn: 367574

--- clang/test/Analysis/structured_bindings.cpp
+++ clang/test/Analysis/structured_bindings.cpp
@@ -1,10 +1,9 @@
 // RUN: %clang_analyze_cc1 -std=c++17 -analyzer-checker=core -verify %s
 
-// expected-no-diagnostics
 struct s { int a; };
 int foo() {
     auto[a] = s{1}; // FIXME: proper modelling
     if (a) {
     }
-}
+} // expected-warning{{control reaches end of non-void function}}
 
