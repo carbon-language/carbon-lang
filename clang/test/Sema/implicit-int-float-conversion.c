7f62ff91140692810b38d8274466ca68ede98931
Ziang Wan <ziangw2@illinois.edu>
2019-08-01 01:39:21 +0000

    [Sema] Enable -Wimplicit-float-conversion for integral to floating point precision loss
    Fix one test case for it to be system-independent.
    
    llvm-svn: 367502

--- clang/test/Sema/implicit-int-float-conversion.c
+++ clang/test/Sema/implicit-int-float-conversion.c
@@ -8,13 +8,8 @@ void testAssignment() {
   float f = 222222;
   double b = 222222222222L;
 
-#ifndef __ILP32__
-  float ff = 222222222222L;    // expected-warning {{implicit conversion from 'long' to 'float' changes value from 222222222222 to 222222221312}}
-  float ffff = 222222222222UL; // expected-warning {{implicit conversion from 'unsigned long' to 'float' changes value from 222222222222 to 222222221312}}
-#else
-  float ff = 222222222222L;    // expected-warning {{implicit conversion from 'long long' to 'float' changes value from 222222222222 to 222222221312}}
-  float ffff = 222222222222UL; // expected-warning {{implicit conversion from 'unsigned long long' to 'float' changes value from 222222222222 to 222222221312}}
-#endif
+  float ff = 222222222222L;    // expected-warning {{changes value from 222222222222 to 222222221312}}
+  float ffff = 222222222222UL; // expected-warning {{changes value from 222222222222 to 222222221312}}
 
   long l = 222222222222L;
   float fff = l; // expected-warning {{implicit conversion from 'long' to 'float' may lose precision}}
@@ -23,11 +18,7 @@ void testAssignment() {
 void testExpression() {
   float a = 0.0f;
 
-#ifndef __ILP32__
-  float b = 222222222222L + a; // expected-warning {{implicit conversion from 'long' to 'float' changes value from 222222222222 to 222222221312}}
-#else
-  float b = 222222222222L + a; // expected-warning {{implicit conversion from 'long long' to 'float' changes value from 222222222222 to 222222221312}}
-#endif
+  float b = 222222222222L + a; // expected-warning {{changes value from 222222222222 to 222222221312}}
 
   float g = 22222222 + 22222222;
   float c = 22222222 + 22222223; // expected-warning {{implicit conversion from 'int' to 'float' changes value from 44444445 to 44444444}}
@@ -42,11 +33,7 @@ void testExpression() {
 void testCNarrowing() {
   // Since this is a C file. C++11 narrowing is not in effect.
   // In this case, we should issue warnings.
-#ifndef __ILP32__
-  float a = {222222222222L}; // expected-warning {{implicit conversion from 'long' to 'float' changes value from 222222222222 to 222222221312}}
-#else
-  float a = {222222222222L};   // expected-warning {{implicit conversion from 'long long' to 'float' changes value from 222222222222 to 222222221312}}
-#endif
+  float a = {222222222222L}; // expected-warning {{changes value from 222222222222 to 222222221312}}
 
   long b = 222222222222L;
   float c = {b}; // expected-warning {{implicit conversion from 'long' to 'float' may lose precision}}
