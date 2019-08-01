1265d0f44c35b8a62fdc2283ccc649a7bca2f68f
Nico Weber <nicolasweber@gmx.de>
2019-08-01 14:09:37 +0000

    compiler-rt: Rename .cc file in lib/msan/tests to .cpp
    Like r367463, but for msan/tests.
    
    llvm-svn: 367563

--- compiler-rt/lib/msan/tests/msan_test_main.cpp
+++ compiler-rt/lib/msan/tests/msan_test_main.cpp
@@ -1,4 +1,4 @@
-//===-- msan_test_main.cc -------------------------------------------------===//
+//===-- msan_test_main.cpp ------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
