f6d42dccb53469a089af8fe988a307bc29ee1de9
Yuanfang Chen <yuanfang.chen@sony.com>
2019-08-01 18:49:59 +0000

    Test linux only for absolute paths in the -fuse-ld option
    Some target do not use this option and may emit a error message for
    using it.
    
    llvm-svn: 367602

--- clang/test/Driver/fuse-ld.c
+++ clang/test/Driver/fuse-ld.c
@@ -1,5 +1,6 @@
 // RUN: %clang %s -### \
 // RUN:     -fuse-ld=/usr/local/bin/or1k-linux-ld 2>&1 \
+// RUN:     -target x86_64-unknown-linux \
 // RUN:   | FileCheck %s --check-prefix=CHECK-ABSOLUTE-LD
 // CHECK-ABSOLUTE-LD: /usr/local/bin/or1k-linux-ld
 
