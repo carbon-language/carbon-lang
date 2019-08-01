9642e337ebe672c3810c80cf9920f3678806ea20
Nico Weber <nicolasweber@gmx.de>
2019-08-01 13:56:52 +0000

    compiler-rt: Rename .cc file in lib/{interception/tests,safestack} to .cpp
    Like r367463, but for interception/tests and safestack.
    
    llvm-svn: 367560

--- compiler-rt/lib/interception/tests/interception_test_main.cpp
+++ compiler-rt/lib/interception/tests/interception_test_main.cpp
@@ -1,4 +1,4 @@
-//===-- interception_test_main.cc------------------------------------------===//
+//===-- interception_test_main.cpp ----------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
