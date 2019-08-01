bc0d697db9e3552cebe21c3f1832c14762bd6ebd
Nico Weber <nicolasweber@gmx.de>
2019-08-01 12:35:27 +0000

    compiler-rt: Rename .cc file in lib/xray/tests/unit to .cpp
    Like r367463, but for xray/texts/unit.
    
    llvm-svn: 367550

--- compiler-rt/lib/xray/tests/unit/buffer_queue_test.cpp
+++ compiler-rt/lib/xray/tests/unit/buffer_queue_test.cpp
@@ -1,4 +1,4 @@
-//===-- buffer_queue_test.cc ----------------------------------------------===//
+//===-- buffer_queue_test.cpp ---------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- compiler-rt/lib/xray/tests/unit/profile_collector_test.cpp
+++ compiler-rt/lib/xray/tests/unit/profile_collector_test.cpp
@@ -1,4 +1,4 @@
-//===-- profile_collector_test.cc -----------------------------------------===//
+//===-- profile_collector_test.cpp ----------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
