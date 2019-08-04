6d2e8eb9cac046d9243c776cb871a6842c16312d
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-08-04 16:28:37 +0000

    [UpdateTestChecks] Add end_function directive to regex matcher for wasm32 function body
    llvm-svn: 367786

--- llvm/utils/UpdateTestChecks/asm.py
+++ llvm/utils/UpdateTestChecks/asm.py
@@ -130,7 +130,7 @@ ASM_FUNCTION_ARM_IOS_RE = re.compile(
 ASM_FUNCTION_WASM32_RE = re.compile(
     r'^_?(?P<func>[^:]+):[ \t]*#+[ \t]*@(?P=func)\n'
     r'(?P<body>.*?)\n'
-    r'.Lfunc_end[0-9]+:\n',
+    r'^\s*(\.Lfunc_end[0-9]+:\n|end_function)',
     flags=(re.M | re.S))
 
 
