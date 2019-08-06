c10180ed8ed9c5d8071eb575c0eaa119d517cc85
Alexey Bataev <a.bataev@hotmail.com>
2019-08-06 18:13:39 +0000

    [OPENMP][OFFLOADING]Fix the test, NFC.
    llvm-svn: 368068

--- openmp/libomptarget/test/mapping/declare_mapper_api.cpp
+++ openmp/libomptarget/test/mapping/declare_mapper_api.cpp
@@ -1,10 +1,11 @@
-// RUN: %libomptarget-compile-run-and-check-aarch64-unknown-linux-gnu
-// RUN: %libomptarget-compile-run-and-check-powerpc64-ibm-linux-gnu
-// RUN: %libomptarget-compile-run-and-check-powerpc64le-ibm-linux-gnu
-// RUN: %libomptarget-compile-run-and-check-x86_64-pc-linux-gnu
+// RUN: %libomptarget-compilexx-run-and-check-aarch64-unknown-linux-gnu
+// RUN: %libomptarget-compilexx-run-and-check-powerpc64-ibm-linux-gnu
+// RUN: %libomptarget-compilexx-run-and-check-powerpc64le-ibm-linux-gnu
+// RUN: %libomptarget-compilexx-run-and-check-x86_64-pc-linux-gnu
 
 #include <cstdio>
 #include <cstdlib>
+#include <vector>
 
 // Data structure definitions copied from OpenMP RTL.
 struct MapComponentInfoTy {
