7aaf74c1bcc34f912409ce9694ca645b80ccfb9b
Caroline Concatto <caroline.concatto@arm.com>
2019-08-06 09:26:40 +0100

    [flang] Removing two sentence error
    Original-commit: flang-compiler/f18@fd5aafc5af14e41cfbae9454394d4eb6b39e2e91
    Reviewed-on: https://github.com/flang-compiler/f18/pull/584
    Tree-same-pre-rewrite: false

--- flang/test/semantics/blockconstruct02.f90
+++ flang/test/semantics/blockconstruct02.f90
@@ -22,7 +22,7 @@ program  main
   !ERROR: 'argmnt1' appears as a COMMON block in a SAVE statement but not in a COMMON statement
   save /argmnt1/
   block
-    !ERROR: 'argmnt2' specifier is not allowed in a BLOCK Construct. It is a Common Block Name
+    !ERROR: SAVE statement in BLOCK construct may not contain a common block name 'argmnt2'
     save /argmnt2/
   end block
 end program
