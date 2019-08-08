fde236b1f719b3a366af4cd5810e847cdb18e480
Marshall Clow <mclow.lists@gmail.com>
2019-08-08 14:36:07 +0000

    Implement hh_mm_ss from P1466R3. Reviewed as https://reviews.llvm.org/D65365.
    llvm-svn: 368299

---
+++ libcxx/test/std/utilities/time/time.hms/time.hms.nonmembers/nothing.to.do.pass.cpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <chrono>
+
+// template <class Duration> class hh_mm_ss;
+
+#include <chrono>
+#include <cassert>
+
+#include "test_macros.h"
+
+int main(int, char**)
+{
+    return 0;
+}
