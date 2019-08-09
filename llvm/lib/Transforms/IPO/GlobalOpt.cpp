991834a51616799456a5e65cefde2f41c350aca2
Sanjay Patel <spatel@rotateright.com>
2019-08-09 12:43:25 +0000

    [GlobalOpt] prevent crashing on large integer types (PR42932)
    This is a minimal fix (copy the predicate for the assert) to
    prevent the crashing seen in:
    https://bugs.llvm.org/show_bug.cgi?id=42932
    ...when converting a constant integer of arbitrary width to uint64_t.
    
    Differential Revision: https://reviews.llvm.org/D65970
    
    llvm-svn: 368437

--- llvm/lib/Transforms/IPO/GlobalOpt.cpp
+++ llvm/lib/Transforms/IPO/GlobalOpt.cpp
@@ -1643,10 +1643,12 @@ static bool TryToShrinkGlobalToBoolean(GlobalVariable *GV, Constant *OtherVal) {
   // instead of a select to synthesize the desired value.
   bool IsOneZero = false;
   bool EmitOneOrZero = true;
-  if (ConstantInt *CI = dyn_cast<ConstantInt>(OtherVal)){
+  auto *CI = dyn_cast<ConstantInt>(OtherVal);
+  if (CI && CI->getValue().getActiveBits() <= 64) {
     IsOneZero = InitVal->isNullValue() && CI->isOne();
 
-    if (ConstantInt *CIInit = dyn_cast<ConstantInt>(GV->getInitializer())){
+    auto *CIInit = dyn_cast<ConstantInt>(GV->getInitializer());
+    if (CIInit && CIInit->getValue().getActiveBits() <= 64) {
       uint64_t ValInit = CIInit->getZExtValue();
       uint64_t ValOther = CI->getZExtValue();
       uint64_t ValMinus = ValOther - ValInit;
