50fcf7285eeb001d751eadac5d001a0e216fd701
Reid Kleckner <rnk@google.com>
2019-08-09 19:49:14 +0000

    Don't diagnose errors when a file matches an include component
    This regressed in r368322, and was reported as PR42948 and on the
    mailing list. The fix is to ignore the specific error code for this
    case. The problem doesn't seem to reproduce on Windows, where a
    different error code is used instead.
    
    llvm-svn: 368475

--- clang/lib/Lex/HeaderSearch.cpp
+++ clang/lib/Lex/HeaderSearch.cpp
@@ -316,7 +316,8 @@ const FileEntry *HeaderSearch::getFileAndSuggestModule(
     // message.
     std::error_code EC = File.getError();
     if (EC != std::errc::no_such_file_or_directory &&
-        EC != std::errc::invalid_argument && EC != std::errc::is_a_directory) {
+        EC != std::errc::invalid_argument && EC != std::errc::is_a_directory &&
+        EC != std::errc::not_a_directory) {
       Diags.Report(IncludeLoc, diag::err_cannot_open_file)
           << FileName << EC.message();
     }
