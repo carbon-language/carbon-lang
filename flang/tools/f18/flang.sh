af794f959be600e5373983d87982061ce8b237ae
Steve Scalpone <sscalpone@nvidia.com>
2019-08-12 21:40:03 -0700

    [flang] Create a throwaway wrapper for the throwaway driver. Add a script called flang to the bin directory that calls f18 with the -I option pointing to the module files for the predefined modules. The wrapper also sets the module suffix to .fmf to avoid naming conflicts with module files generated by other compilers. Now, the build creates and installs both .mod and .fmf files for the predefined module files to accommodate the flang script.
    Original-commit: flang-compiler/f18@b6c30284e7876f6ccd4bb024bd5f349128e99b7c
    Reviewed-on: https://github.com/flang-compiler/f18/pull/653
    Tree-same-pre-rewrite: false

---
+++ flang/tools/f18/flang.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+function abspath() {
+  pushd . > /dev/null;
+  if [ -d "$1" ]; then
+    cd "$1";
+    dirs -l +0;
+  else
+    cd "`dirname \"$1\"`";
+    cur_dir=`dirs -l +0`;
+    if [ "$cur_dir" == "/" ]; then
+      echo "$cur_dir`basename \"$1\"`";
+    else
+      echo "$cur_dir/`basename \"$1\"`";
+    fi;
+  fi;
+  popd > /dev/null;
+}
+
+wd=`abspath $(dirname "$0")/..`
+
+$wd/bin/f18 -fdebug-semantics -module-suffix .fmf -I $wd/include $*
