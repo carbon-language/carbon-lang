b4aebff4f1a6891c31f4f3bb54cdd42c00d5112a
Louis Dionne <ldionne@apple.com>
2019-08-14 20:17:18 +0000

    [libc++] Mark std::tuple CTAD test as failing on AppleClang 9
    Like CTAD for std::unordered_set, AppleClang 9's support for CTAD is
    insufficient. I suspect the corresponding LLVM Clang is broken too,
    but we don't seem to have testers using that Clang.
    
    llvm-svn: 368911

--- libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/deduct.pass.cpp
+++ libcxx/test/std/utilities/tuple/tuple.tuple/tuple.cnstr/deduct.pass.cpp
@@ -8,6 +8,7 @@
 
 // UNSUPPORTED: c++98, c++03, c++11, c++14
 // UNSUPPORTED: libcpp-no-deduction-guides
+// UNSUPPORTED: apple-clang-9
 
 // GCC's implementation of class template deduction is still immature and runs
 // into issues with libc++. However GCC accepts this code when compiling
