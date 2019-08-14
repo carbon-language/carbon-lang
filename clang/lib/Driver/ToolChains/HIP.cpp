15e26d1fe945eda5de84ab9b90bc63f2945ba9cc
Sven van Haastregt <sven.vanhaastregt@arm.com>
2019-08-14 10:30:18 +0000

    Fix _WIN32 / _WIN64 Wundef warnings
    For these macros it is the definedness that matters rather than
    the value.  Make new uses of these macros consistent with existing
    uses.
    
    llvm-svn: 368822

--- clang/lib/Driver/ToolChains/HIP.cpp
+++ clang/lib/Driver/ToolChains/HIP.cpp
@@ -23,7 +23,7 @@ using namespace clang::driver::tools;
 using namespace clang;
 using namespace llvm::opt;
 
-#if _WIN32 || _WIN64
+#if defined(_WIN32) || defined(_WIN64)
 #define NULL_FILE "nul"
 #else
 #define NULL_FILE "/dev/null"
