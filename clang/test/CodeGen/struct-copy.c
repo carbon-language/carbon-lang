bffa4a2b17b12f090059754011ec0bae08f810f4
David Bolvansky <david.bolvansky@gmail.com>
2019-08-14 15:35:40 +0000

    [NFC] Fix testcase for ARMs
    llvm-svn: 368863

--- clang/test/CodeGen/struct-copy.c
+++ clang/test/CodeGen/struct-copy.c
@@ -4,14 +4,14 @@ struct x { int a[100]; };
 
 void foo(struct x *P, struct x *Q) {
 // CHECK-LABEL: @foo(
-// CHECK:    call void @llvm.memcpy.p0i8.p0i8.i64
+// CHECK:    call void @llvm.memcpy.p0i8.p0i8
   *P = *Q;
 }
 
-// CHECK: declare void @llvm.memcpy.p0i8.p0i8.i64(i8* noalias nocapture writeonly, i8* noalias nocapture readonly, i64, i1 immarg)
+// CHECK: declare void @llvm.memcpy.p0i8.p0i8{{.*}}(i8* noalias nocapture writeonly, i8* noalias nocapture readonly
 
 void bar(struct x *P, struct x *Q) {
 // CHECK-LABEL: @bar(
-// CHECK:    call void @llvm.memcpy.p0i8.p0i8.i64
+// CHECK:    call void @llvm.memcpy.p0i8.p0i8
   __builtin_memcpy(P, Q, sizeof(struct x));
 }
