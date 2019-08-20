cb93f650b6e5e1cfc65bd5e53e9bf0e5470ad346
Louis Dionne <ldionne@apple.com>
2019-08-20 16:45:27 +0000

    [libc++] Disable <chrono> ""d and ""y literal tests on AppleClang 10.0.0
    In r368882, I enabled those tests for all AppleClang's above version 9.
    However, it turns out that the feature is only supported starting with
    AppleClang 10.0.1, not AppleClang 10.0.0. This commit fixes that hole.
    
    llvm-svn: 369409

--- libcxx/test/std/utilities/time/time.cal/time.cal.day/time.cal.day.nonmembers/literals.pass.cpp
+++ libcxx/test/std/utilities/time/time.cal/time.cal.day/time.cal.day.nonmembers/literals.pass.cpp
@@ -8,6 +8,7 @@
 // UNSUPPORTED: c++98, c++03, c++11, c++14, c++17
 // UNSUPPORTED: clang-5, clang-6, clang-7
 // UNSUPPORTED: apple-clang-6, apple-clang-7, apple-clang-8, apple-clang-9
+// UNSUPPORTED: apple-clang-10.0.0
 
 // <chrono>
 // class day;
