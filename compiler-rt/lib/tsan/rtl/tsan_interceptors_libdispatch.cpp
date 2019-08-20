def061e6f0d58a5f9741ce507ab24390a657f72e
Julian Lettner <jlettner@apple.com>
2019-08-20 00:04:19 +0000

    [TSan] Rename file with libdispatch interceptors
    llvm-svn: 369314

--- compiler-rt/lib/tsan/rtl/tsan_interceptors_libdispatch.cpp
+++ compiler-rt/lib/tsan/rtl/tsan_interceptors_libdispatch.cpp
@@ -1,4 +1,4 @@
-//===-- tsan_libdispatch_interceptors.cpp ---------------------------------===//
+//===-- tsan_interceptors_libdispatch.cpp ---------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
