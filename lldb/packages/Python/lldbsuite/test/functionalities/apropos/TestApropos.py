717717b1ff8b6ee48331a86feaf2f68b28b6c9d8
Raphael Isemann <teemperor@gmail.com>
2019-08-21 14:22:59 +0000

    [lldb][NFC] Merge multiple TestApropos.py
    That's cleaner and makes lldb-dotest no longer fail due to conflicting names.
    
    llvm-svn: 369530

--- lldb/packages/Python/lldbsuite/test/functionalities/apropos/TestApropos.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/apropos/TestApropos.py
@@ -17,3 +17,13 @@ class AproposTestCase(TestBase):
                     substrs=[' must be called with exactly one argument'])
         self.expect("apropos ''", error=True,
                     substrs=['\'\' is not a valid search word'])
+
+    @no_debug_info_test
+    def test_apropos_variable(self):
+        """Test that 'apropos variable' prints the fully qualified command name"""
+        self.expect(
+            'apropos variable',
+            substrs=[
+                'frame variable',
+                'target variable',
+                'watchpoint set variable'])
