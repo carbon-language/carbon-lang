246750c2a91951fc4b77ebe43d568f47843e6399
Fangrui Song <maskray@google.com>
2019-08-22 01:48:34 +0000

    [COFF] Fix section name for constants larger than 64 bits on Windows
    APIntToHexString returns wrong value ("0000000000000000ffffffffffffffff")
    for integer larger than 64 bits, and thus
    TargetLoweringObjectFileCOFF::getSectionForConstant returns same section name
    for all numbers larger than 64 bits. This patch tries to fix it.
    
    Differential Revision: https://reviews.llvm.org/D66458
    Patch by Senran Zhang
    
    llvm-svn: 369610

--- llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
+++ llvm/lib/CodeGen/TargetLoweringObjectFileImpl.cpp
@@ -1597,7 +1597,8 @@ const MCExpr *TargetLoweringObjectFileCOFF::lowerRelativeReference(
 
 static std::string APIntToHexString(const APInt &AI) {
   unsigned Width = (AI.getBitWidth() / 8) * 2;
-  std::string HexString = utohexstr(AI.getLimitedValue(), /*LowerCase=*/true);
+  std::string HexString = AI.toString(16, /*Signed=*/false);
+  transform(HexString.begin(), HexString.end(), HexString.begin(), tolower);
   unsigned Size = HexString.size();
   assert(Width >= Size && "hex string is too large!");
   HexString.insert(HexString.begin(), Width - Size, '0');
