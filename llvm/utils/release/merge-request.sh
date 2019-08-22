dd323c350751fad4f0ba626de2cc7d005d781d70
Jonas Devlieghere <jonas@devlieghere.com>
2019-08-22 23:42:31 +0000

    [utils] Update shebang to use the environment.
    This changes the shebang to launch bash through /usr/bin/env.
    
    llvm-svn: 369717

--- llvm/utils/release/merge-request.sh
+++ llvm/utils/release/merge-request.sh
@@ -1,4 +1,4 @@
-# !/bin/bash
+#!/usr/bin/env bash
 #===-- merge-request.sh  ---------------------------------------------------===#
 #
 # Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
