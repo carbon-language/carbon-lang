028b5499fffa536236cb5714e6d14b786883a4b4
Petr Hosek <phosek@chromium.org>
2019-08-22 07:03:38 +0000

    Revert "[GWP-ASan] Remove c++ standard lib dependency."
    This reverts commit r369606: this doesn't addressed the underlying
    problem and it's not the correct solution.
    
    llvm-svn: 369623

--- compiler-rt/lib/gwp_asan/stack_trace_compressor_fuzzer.cpp
+++ compiler-rt/lib/gwp_asan/stack_trace_compressor_fuzzer.cpp
@@ -1,7 +1,8 @@
-#include <stddef.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <cstddef>
+#include <cstdint>
+#include <cstdio>
+#include <cstdlib>
+#include <vector>
 
 #include "gwp_asan/stack_trace_compressor.h"
 
@@ -9,22 +10,23 @@ constexpr size_t kBytesForLargestVarInt = (sizeof(uintptr_t) * 8) / 7 + 1;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   size_t BufferSize = kBytesForLargestVarInt * Size / sizeof(uintptr_t);
-  uint8_t *Buffer = reinterpret_cast<uint8_t *>(malloc(BufferSize));
-  uint8_t *Buffer2 = reinterpret_cast<uint8_t *>(malloc(BufferSize));
+  std::vector<uint8_t> Buffer(BufferSize);
+  std::vector<uint8_t> Buffer2(BufferSize);
 
   // Unpack the fuzz bytes.
   gwp_asan::compression::unpack(Data, Size,
-                                reinterpret_cast<uintptr_t *>(Buffer2),
+                                reinterpret_cast<uintptr_t *>(Buffer2.data()),
                                 BufferSize / sizeof(uintptr_t));
 
   // Pack the fuzz bytes.
-  size_t BytesWritten =
-      gwp_asan::compression::pack(reinterpret_cast<const uintptr_t *>(Data),
-                                  Size / sizeof(uintptr_t), Buffer, BufferSize);
+  size_t BytesWritten = gwp_asan::compression::pack(
+      reinterpret_cast<const uintptr_t *>(Data), Size / sizeof(uintptr_t),
+      Buffer.data(), BufferSize);
 
   // Unpack the compressed buffer.
   size_t DecodedElements = gwp_asan::compression::unpack(
-      Buffer, BytesWritten, reinterpret_cast<uintptr_t *>(Buffer2),
+      Buffer.data(), BytesWritten,
+      reinterpret_cast<uintptr_t *>(Buffer2.data()),
       BufferSize / sizeof(uintptr_t));
 
   // Ensure that every element was encoded and decoded properly.
@@ -33,7 +35,8 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
 
   // Ensure that the compression and uncompression resulted in the same trace.
   const uintptr_t *FuzzPtrs = reinterpret_cast<const uintptr_t *>(Data);
-  const uintptr_t *DecodedPtrs = reinterpret_cast<const uintptr_t *>(Buffer2);
+  const uintptr_t *DecodedPtrs =
+      reinterpret_cast<const uintptr_t *>(Buffer2.data());
   for (size_t i = 0; i < Size / sizeof(uintptr_t); ++i) {
     if (FuzzPtrs[i] != DecodedPtrs[i]) {
       fprintf(stderr, "FuzzPtrs[%zu] != DecodedPtrs[%zu] (0x%zx vs. 0x%zx)", i,
