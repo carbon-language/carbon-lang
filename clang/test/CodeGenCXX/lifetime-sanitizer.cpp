98bcf690ae03db85608b2ea22eac7a91c84df4dc
David Zarzycki <dave@znu.io>
2019-08-24 08:12:51 +0000

    [Testing] Unbreak r369830
    llvm-svn: 369843

--- clang/test/CodeGenCXX/lifetime-sanitizer.cpp
+++ clang/test/CodeGenCXX/lifetime-sanitizer.cpp
@@ -3,6 +3,7 @@
 // RUN: %clang -w -target x86_64-linux-gnu -S -emit-llvm -o - -fno-exceptions -O0 \
 // RUN:     -fsanitize=address -fsanitize-address-use-after-scope %s | \
 // RUN:     FileCheck %s -check-prefixes=CHECK,LIFETIME
+// REQUIRES: assertions
 
 extern int bar(char *A, int n);
 
