ac1d5986c836924896aeb934ff71432f80c70063
Richard Smith <richard-llvm@metafoo.co.uk>
2019-08-26 18:21:13 +0000

    Fix file header.
    llvm-svn: 369941

--- clang/lib/Basic/Stack.cpp
+++ clang/lib/Basic/Stack.cpp
@@ -1,4 +1,4 @@
-//===--- Stack.h - Utilities for dealing with stack space -------*- C++ -*-===//
+//===--- Stack.cpp - Utilities for dealing with stack space ---------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
