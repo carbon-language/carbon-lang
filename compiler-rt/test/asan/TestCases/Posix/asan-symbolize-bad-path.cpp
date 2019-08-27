79f3459deb706228ab7ceb6ea9ca9d80393855f1
Alexander Richardson <arichardson.kde@gmail.com>
2019-08-27 21:10:47 +0000

    Fix asan-symbolize-bad-path.cpp on Darwin
    I accidentally made the CHECK line stricter when committing D65322.
    While it happens to work for Linux and FreeBSD, it broke on Darwin.
    This commit restores the previous behaviour.
    
    llvm-svn: 370110

--- compiler-rt/test/asan/TestCases/Posix/asan-symbolize-bad-path.cpp
+++ compiler-rt/test/asan/TestCases/Posix/asan-symbolize-bad-path.cpp
@@ -1,7 +1,8 @@
 // Test that asan_symbolize does not hang when provided with an non-existing
 // path.
 // RUN: echo '#0 0xabcdabcd (%t/bad/path+0x1234)' | %asan_symbolize | FileCheck %s -check-prefix CHECK-BAD-FILE
-// CHECK-BAD-FILE: #0 0xabcdabcd in ?? ??:0
+// Note: can't check for "0xabcdabcd in ?? ??:0" since DarwinSymbolizer will print the file even if it doesn't exist.
+// CHECK-BAD-FILE: #0 0xabcdabcd in
 // CHECK-BAD-FILE-EMPTY:
 
 // Also test that asan_symbolize doesn't assert on an invalid address with a valid file:
