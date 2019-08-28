3ae9b9d5e40d1d9bdea1fd8e6fca322df920754a
Julian Lettner <jlettner@apple.com>
2019-08-28 20:40:55 +0000

    [ASan] Make insertion of version mismatch guard configurable
    By default ASan calls a versioned function
    `__asan_version_mismatch_check_vXXX` from the ASan module constructor to
    check that the compiler ABI version and runtime ABI version are
    compatible. This ensures that we get a predictable linker error instead
    of hard-to-debug runtime errors.
    
    Sometimes, however, we want to skip this safety guard. This new command
    line option allows us to do just that.
    
    rdar://47891956
    
    Reviewed By: kubamracek
    
    Differential Revision: https://reviews.llvm.org/D66826
    
    llvm-svn: 370258

--- llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
+++ llvm/lib/Transforms/Instrumentation/AddressSanitizer.cpp
@@ -193,6 +193,11 @@ static cl::opt<bool> ClRecover(
     cl::desc("Enable recovery mode (continue-after-error)."),
     cl::Hidden, cl::init(false));
 
+static cl::opt<bool> ClInsertVersionCheck(
+    "asan-guard-against-version-mismatch",
+    cl::desc("Guard against compiler/runtime version mismatch."),
+    cl::Hidden, cl::init(true));
+
 // This flag may need to be replaced with -f[no-]asan-reads.
 static cl::opt<bool> ClInstrumentReads("asan-instrument-reads",
                                        cl::desc("instrument read instructions"),
@@ -2426,8 +2431,9 @@ bool ModuleAddressSanitizer::instrumentModule(Module &M) {
 
   // Create a module constructor. A destructor is created lazily because not all
   // platforms, and not all modules need it.
+  std::string AsanVersion = std::to_string(GetAsanVersion(M));
   std::string VersionCheckName =
-      kAsanVersionCheckNamePrefix + std::to_string(GetAsanVersion(M));
+      ClInsertVersionCheck ? (kAsanVersionCheckNamePrefix + AsanVersion) : "";
   std::tie(AsanCtorFunction, std::ignore) = createSanitizerCtorAndInitFunctions(
       M, kAsanModuleCtorName, kAsanInitName, /*InitArgTypes=*/{},
       /*InitArgs=*/{}, VersionCheckName);
