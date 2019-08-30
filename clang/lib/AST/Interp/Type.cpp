a5590950549719d0d9ea69ed164b0c8c0f4e02e6
Nandor Licker <n@ndor.email>
2019-08-30 15:02:09 +0000

    [Clang Interpreter] Initial patch for the constexpr interpreter
    Summary:
    This patch introduces the skeleton of the constexpr interpreter,
    capable of evaluating a simple constexpr functions consisting of
    if statements. The interpreter is described in more detail in the
    RFC. Further patches will add more features.
    
    Reviewers: Bigcheese, jfb, rsmith
    
    Subscribers: bruno, uenoku, ldionne, Tyker, thegameg, tschuett, dexonsmith, mgorny, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D64146
    
    llvm-svn: 370476

---
+++ clang/lib/AST/Interp/Type.cpp
@@ -0,0 +1,22 @@
+//===--- Type.cpp - Types for the constexpr VM ------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Type.h"
+
+using namespace clang;
+using namespace clang::interp;
+
+namespace clang {
+namespace interp {
+
+size_t primSize(PrimType Type) {
+  TYPE_SWITCH(Type, return sizeof(T));
+}
+
+} // namespace interp
+} // namespace clang
