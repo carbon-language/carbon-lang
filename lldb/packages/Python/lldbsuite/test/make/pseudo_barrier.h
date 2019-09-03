e44f2eb3153d57604cf7204b84892901663ad7ce
Jonas Devlieghere <jonas@devlieghere.com>
2019-09-03 17:04:04 +0000

    [test] Addres  TestConcurrentMany*.py flakiness on macOS
    On "fast" macOS machines, the TestConcurrentMany*.py tests would fail
    randomly with different numbers of breakpoints, watchpoints, etc. This
    seems to be avoidable by giving the threads a little time to breath
    after the passing the synchronization barrier. This is far from a
    structural fix but it reduces the flakiness.
    
    llvm-svn: 370785

--- lldb/packages/Python/lldbsuite/test/make/pseudo_barrier.h
+++ lldb/packages/Python/lldbsuite/test/make/pseudo_barrier.h
@@ -1,20 +1,15 @@
 #include <atomic>
-
-// Note that although hogging the CPU while waiting for a variable to change
-// would be terrible in production code, it's great for testing since it
-// avoids a lot of messy context switching to get multiple threads synchronized.
+#include <thread>
 
 typedef std::atomic<int> pseudo_barrier_t;
-#define pseudo_barrier_wait(barrier)        \
-    do                                      \
-    {                                       \
-        --(barrier);                        \
-        while ((barrier).load() > 0)        \
-            ;                               \
-    } while (0)
 
-#define pseudo_barrier_init(barrier, count) \
-    do                                      \
-    {                                       \
-        (barrier) = (count);                \
-    } while (0)
+static inline void pseudo_barrier_wait(pseudo_barrier_t &barrier) {
+  --barrier;
+  while (barrier > 0)
+    std::this_thread::yield();
+  std::this_thread::sleep_for(std::chrono::milliseconds(100));
+}
+
+static inline void pseudo_barrier_init(pseudo_barrier_t &barrier, int count) {
+  barrier = count;
+}
