9508738cd1d4ad179021314bfe54f2a02cb029cd
Max Moroz <mmoroz@chromium.org>
2019-09-09 19:30:48 +0000

    [UBSan] Do not overwrite the default print_summary sanitizer option.
    Summary:
    This option is true by default in sanitizer common. The default
    false value was added a while ago without any reasoning in
    https://github.com/llvm-mirror/compiler-rt/commit/524e934112a593ac081bf2b05aa0d60a67987f05
    
    so, presumably it's safe to remove for consistency.
    
    Reviewers: hctim, samsonov, morehouse, kcc, vitalybuka
    
    Reviewed By: hctim, samsonov, vitalybuka
    
    Subscribers: delcypher, #sanitizers, llvm-commits, kcc
    
    Tags: #llvm, #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D67193
    
    llvm-svn: 371442

--- compiler-rt/lib/ubsan/ubsan_flags.cpp
+++ compiler-rt/lib/ubsan/ubsan_flags.cpp
@@ -54,7 +54,6 @@ void InitializeFlags() {
   {
     CommonFlags cf;
     cf.CopyFrom(*common_flags());
-    cf.print_summary = false;
     cf.external_symbolizer_path = GetFlag("UBSAN_SYMBOLIZER_PATH");
     OverrideCommonFlags(cf);
   }
