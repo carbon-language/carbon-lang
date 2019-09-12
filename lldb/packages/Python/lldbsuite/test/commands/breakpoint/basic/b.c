03a111dc46456ca6c397a801da0c8f0c22ffc39e
Raphael Isemann <teemperor@gmail.com>
2019-09-12 14:07:16 +0000

    [lldb] Remove duplicated breakpoint tests
    After reverting the deletion of the functionalities/breakpoint tests,
    we now have some tests twice in the test/ folder which breaks dotest:
    
    * commands/breakpoint/basic
    * functionalities/breakpoint/breakpoint_command
    
    After looking over these tests, I think it makes sense to only
    keep the original functionalities/ folder. The commands/breakpoint/basic
    test are not exclusively testing the breakpoint command itself, so
    they shouldn't be in commands/ in the first place. Note that these
    folders have identical contents (beside small adjustments regarding
    the Makefile which landed after the restructuring).
    
    llvm-svn: 371734

--- lldb/packages/Python/lldbsuite/test/commands/breakpoint/basic/b.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int
-b_MyFunction ()
-{
-  // Set a breakpoint here.
-  printf ("b is about to return 20.\n");
-  return 20;
-}
--- lldb/packages/Python/lldbsuite/test/commands/breakpoint/basic/bktptcmd.py
+++
@@ -1,5 +0,0 @@
-from __future__ import print_function
-import side_effect
-
-def function(frame, bp_loc, dict):
-    side_effect.bktptcmd = "function was here"
