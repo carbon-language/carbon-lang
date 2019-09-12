a3cd67c0078607fb928bd5b5b0187e450fc7bd23
Vitaly Buka <vitalybuka@google.com>
2019-09-12 01:35:09 +0000

    [compiler-rt] Better lint output for .inc files
    llvm-svn: 371702

--- compiler-rt/lib/sanitizer_common/scripts/check_lint.sh
+++ compiler-rt/lib/sanitizer_common/scripts/check_lint.sh
@@ -124,20 +124,12 @@ run_lint ${SCUDO_RTL_LINT_FILTER} ${SCUDO_RTL}/*.cpp \
                                   ${SCUDO_RTL}/*.h &
 
 # Misc files
-FILES=${COMMON_RTL}/*.inc
-TMPFILES=""
-for FILE in $FILES; do
-  TMPFILE="$(${MKTEMP}).$(basename ${FILE}).cpp"
-  cp -f $FILE $TMPFILE
-  run_lint ${COMMON_RTL_INC_LINT_FILTER} $TMPFILE &
-  TMPFILES="$TMPFILES $TMPFILE"
-done
+rsync -a --prune-empty-dirs --exclude='*/profile/*' --exclude='*/builtins/*' --exclude='*/xray/*' --include='*/' --include='*.inc' --exclude='*' "${COMPILER_RT}/" "${MKTEMP_DIR}/"
+find ${MKTEMP_DIR} -type f -name '*.inc' -exec mv {} {}.cpp \;
+( ERROR_LOG=${ERROR_LOG}.inc run_lint ${COMMON_RTL_INC_LINT_FILTER} $(find ${MKTEMP_DIR} -type f -name '*.inc.cpp') )
+sed "s|${MKTEMP_DIR}|${COMPILER_RT}|g" ${ERROR_LOG}.inc | sed "s|.inc.cpp|.inc|g" >> ${ERROR_LOG}
 
 wait
-
-for temp in $TMPFILES; do
-  rm -f $temp
-done
 
 if [ -s $ERROR_LOG ]; then
   cat $ERROR_LOG
