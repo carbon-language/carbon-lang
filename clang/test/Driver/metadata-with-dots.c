fc637d03b18c54ca9cc16ccb06f33e3c8b8715d6
Tim Shen <timshen91@gmail.com>
2019-09-13 21:06:47 +0000

    Fix test to use %t for newly created files.
    This is both for consistency with other `mkdir`s in tests, and
    fixing permission issues with the non-temporary cwd during testing (they
    are not always writable).
    
    llvm-svn: 371897

--- clang/test/Driver/metadata-with-dots.c
+++ clang/test/Driver/metadata-with-dots.c
@@ -1,10 +1,10 @@
 // REQUIRES: shell
-// RUN: mkdir -p out.dir
-// RUN: cat %s > out.dir/test.c
-// RUN: %clang -E -MMD %s -o out.dir/test
+// RUN: mkdir -p %t/out.dir
+// RUN: cat %s > %t/out.dir/test.c
+// RUN: %clang -E -MMD %s -o %t/out.dir/test
 // RUN: test ! -f %out.d
-// RUN: test -f out.dir/test.d
-// RUN: rm -rf out.dir/test.d out.dir/ out.d
+// RUN: test -f %t/out.dir/test.d
+// RUN: rm -rf %t/out.dir/test.d %t/out.dir/ out.d
 int main (void)
 {
     return 0;
