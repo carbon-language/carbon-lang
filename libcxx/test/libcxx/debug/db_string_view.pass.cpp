6bc1236d395187334051b99e2fcd715a54753f33
Eric Fiselier <eric@efcs.ca>
2019-09-14 19:55:28 +0000

    Add debug check for null pointers passed to <string_view>
    llvm-svn: 371925

---
+++ libcxx/test/libcxx/debug/db_string_view.pass.cpp
@@ -0,0 +1,36 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+// UNSUPPORTED: windows
+// UNSUPPORTED: libcpp-no-if-constexpr
+// MODULES_DEFINES: _LIBCPP_DEBUG=1
+
+// Can't test the system lib because this test enables debug mode
+// UNSUPPORTED: with_system_cxx_lib
+
+// test container debugging
+
+#define _LIBCPP_DEBUG 1
+#include <string_view>
+
+#include "test_macros.h"
+#include "debug_mode_helper.h"
+
+void test_null_argument() {
+  EXPECT_DEATH(std::string_view(nullptr));
+  EXPECT_DEATH(std::string_view(NULL));
+  EXPECT_DEATH(std::string_view(static_cast<const char*>(0)));
+}
+
+int main(int, char**)
+{
+  test_null_argument();
+
+  return 0;
+}
