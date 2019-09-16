6d03890384517919a3ba7fe4c35535425f278f89
Amy Huang <akhuang@google.com>
2019-09-16 15:46:49 -0700

    [CodeView] Add option to disable inline line tables.
    Summary:
    This adds a clang option to disable inline line tables. When it is used,
    the inliner uses the call site as the location of the inlined function instead of
    marking it as an inline location with the function location.
    
    See https://bugs.llvm.org/show_bug.cgi?id=42344
    
    Reviewers: rnk
    
    Subscribers: hiraditya, cfe-commits, llvm-commits
    
    Tags: #clang, #llvm
    
    Differential Revision: https://reviews.llvm.org/D67723

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -3296,6 +3296,12 @@ static void RenderDebugOptions(const ToolChain &TC, const Driver &D,
     }
   }
 
+  // Omit inline line tables if requested.
+  if (!Args.hasFlag(options::OPT_ginline_line_tables,
+                    options::OPT_gno_inline_line_tables, false)) {
+    CmdArgs.push_back("-gno-inline-line-tables");
+  }
+
   // Adjust the debug info kind for the given toolchain.
   TC.adjustDebugInfoKind(DebugInfoKind, Args);
 
