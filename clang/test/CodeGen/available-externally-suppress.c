b9909ffed82f7ed8a211b21146225badc0f22428
Roman Lebedev <lebedev.ri@gmail.com>
2019-09-16 17:46:01 +0000

    [Clang][Codegen] Relax available-externally-suppress.c test
    That test is broken by design.
    It depends on llvm middle-end behavior.
    No clang codegen test should be doing that.
    This one is salvageable by relaxing check lines.
    
    llvm-svn: 372014

--- clang/test/CodeGen/available-externally-suppress.c
+++ clang/test/CodeGen/available-externally-suppress.c
@@ -30,10 +30,8 @@ inline int __attribute__((always_inline)) f1(int x) {
 // CHECK: @test1
 // LTO: @test1
 int test1(int x) {
-  // CHECK: br i1
   // CHECK-NOT: call {{.*}} @f1
   // CHECK: ret i32
-  // LTO: br i1
   // LTO-NOT: call {{.*}} @f1
   // LTO: ret i32
   return f1(x);
