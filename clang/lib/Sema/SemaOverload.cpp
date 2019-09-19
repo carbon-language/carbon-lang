08f938bd1ae356544923042423ea3f30cd730bf8
Mitch Phillips <mitchphillips@outlook.com>
2019-09-19 21:11:28 +0000

    Revert "[CUDA][HIP] Fix typo in `BestViableFunction`"
    Broke the msan buildbots (see comments on rL372318 for more details).
    
    This reverts commit eb231d15825ac345b546f4c99372d1cac8f14f02.
    
    llvm-svn: 372353

--- clang/lib/Sema/SemaOverload.cpp
+++ clang/lib/Sema/SemaOverload.cpp
@@ -9422,19 +9422,17 @@ OverloadCandidateSet::BestViableFunction(Sema &S, SourceLocation Loc,
     const FunctionDecl *Caller = dyn_cast<FunctionDecl>(S.CurContext);
     bool ContainsSameSideCandidate =
         llvm::any_of(Candidates, [&](OverloadCandidate *Cand) {
-          // Consider viable function only.
-          return Cand->Viable && Cand->Function &&
+          return Cand->Function &&
                  S.IdentifyCUDAPreference(Caller, Cand->Function) ==
                      Sema::CFP_SameSide;
         });
     if (ContainsSameSideCandidate) {
-      // Clear viable flag for WrongSide varible candidates.
-      llvm::for_each(Candidates, [&](OverloadCandidate *Cand) {
-        if (Cand->Viable && Cand->Function &&
-            S.IdentifyCUDAPreference(Caller, Cand->Function) ==
-                Sema::CFP_WrongSide)
-          Cand->Viable = false;
-      });
+      auto IsWrongSideCandidate = [&](OverloadCandidate *Cand) {
+        return Cand->Function &&
+               S.IdentifyCUDAPreference(Caller, Cand->Function) ==
+                   Sema::CFP_WrongSide;
+      };
+      llvm::erase_if(Candidates, IsWrongSideCandidate);
     }
   }
 
