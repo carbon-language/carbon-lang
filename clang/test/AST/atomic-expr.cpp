a957eaad0091dd623b448c4468e51219da124bd3
Erich Keane <erich.keane@intel.com>
2019-09-23 14:12:13 +0000

    Fix test atomic-expr.cpp after R372422
    The test tried to match a path in a printout by doing '^:' which failed
    on windows, since C:\... is a path.
    
    llvm-svn: 372611

--- clang/test/AST/atomic-expr.cpp
+++ clang/test/AST/atomic-expr.cpp
@@ -9,7 +9,7 @@ void useage(){
   pr43370();
 }
 
-// CHECK:FunctionTemplateDecl 0x{{[0-9a-f]+}} <{{[^:]+}}:3:1, line:7:1> line:4:6 pr43370
+// CHECK:FunctionTemplateDecl 0x{{[0-9a-f]+}} <{{[^,]+}}, line:7:1> line:4:6 pr43370
 // CHECK: AtomicExpr
 // CHECK-NEXT: ImplicitCastExpr
 // CHECK-SAME: <ArrayToPointerDecay>
