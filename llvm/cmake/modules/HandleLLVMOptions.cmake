a80048f3df292abb5f290c924ef6606982b47262
Justin Bogner <mail@justinbogner.com>
2019-09-24 23:56:22 +0000

    Use double quotes for LDFLAGS in -DLLVM_BUILD_INSTRUMENTED_COVERAGE
    Mimics the changes in r372209 to handle the change of quotes in
    r372226. Probably isn't sufficient for windows, but unbreaks the cmake
    flag at least.
    
    llvm-svn: 372791

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -867,7 +867,7 @@ endif()
 
 option(LLVM_BUILD_INSTRUMENTED_COVERAGE "Build LLVM and tools with Code Coverage instrumentation" Off)
 mark_as_advanced(LLVM_BUILD_INSTRUMENTED_COVERAGE)
-append_if(LLVM_BUILD_INSTRUMENTED_COVERAGE "-fprofile-instr-generate='${LLVM_PROFILE_FILE_PATTERN}' -fcoverage-mapping"
+append_if(LLVM_BUILD_INSTRUMENTED_COVERAGE "-fprofile-instr-generate=\"${LLVM_PROFILE_FILE_PATTERN}\" -fcoverage-mapping"
   CMAKE_CXX_FLAGS
   CMAKE_C_FLAGS
   CMAKE_EXE_LINKER_FLAGS
