b73e3ca39aab58d3231cac353b919c63987cc65f
Michal Gorny <mgorny@gentoo.org>
2019-09-25 10:18:38 +0000

    [lldb] [test] Add NetBSD to XFAIL list for thread_local test
    llvm-svn: 372840

--- lldb/packages/Python/lldbsuite/test/lang/cpp/thread_local/TestThreadLocal.py
+++ lldb/packages/Python/lldbsuite/test/lang/cpp/thread_local/TestThreadLocal.py
@@ -2,4 +2,5 @@ from lldbsuite.test import lldbinline
 from lldbsuite.test import decorators
 
 lldbinline.MakeInlineTest(__file__, globals(),
-                          lldbinline.expectedFailureAll(oslist=["windows", "linux"]))
+                          lldbinline.expectedFailureAll(oslist=[
+                              "windows", "linux", "netbsd"]))
