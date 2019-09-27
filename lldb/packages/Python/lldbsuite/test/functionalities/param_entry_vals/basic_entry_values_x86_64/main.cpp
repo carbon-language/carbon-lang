06bc9d579cacae1f4209635bb2770b61231befbe
Djordje Todorovic <djordje.todorovic@rt-rk.com>
2019-09-27 12:16:29 +0000

    [lldb] Disable testing entry values as memory location
    The D67717 excludes such locations for now.
    
    llvm-svn: 373074

--- lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/main.cpp
+++ lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/main.cpp
@@ -152,7 +152,10 @@ int main() {
   func2(sink, 123);
 
   // Test evaluation of "DW_OP_fbreg -24, DW_OP_deref" in the parent frame.
+  // Disabled for now, see: llvm.org/PR43343
+#if 0
   func3(sink, s1.field2);
+#endif
 
   // The sequences `main -> func4 -> func{5,6}_amb -> sink` are both plausible.
   // Test that lldb doesn't attempt to guess which one occurred: entry value
