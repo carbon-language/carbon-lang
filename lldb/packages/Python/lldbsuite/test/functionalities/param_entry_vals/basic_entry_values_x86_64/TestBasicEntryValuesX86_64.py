b5a1cf9bf88db4ff531f27746f84f7545d0a8583
Vedant Kumar <vsk@apple.com>
2019-09-30 17:11:46 +0000

    [test] Make TestBasicEntryValuesX86_64 run on Linux as well as Darwin
    I think a reasonable plan here is to add support for OSes following the
    SysV ABI one by one, watching the bots as we go.
    
    llvm-svn: 373233

--- lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/TestBasicEntryValuesX86_64.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/TestBasicEntryValuesX86_64.py
@@ -1,8 +1,12 @@
 from lldbsuite.test import lldbinline
 from lldbsuite.test import decorators
+from lldbsuite.test import lldbplatformutil
+
+supported_platforms = ["linux"]
+supported_platforms.extend(lldbplatformutil.getDarwinOSTriples())
 
 lldbinline.MakeInlineTest(__file__, globals(),
-        [decorators.skipUnlessDarwin,
+        [decorators.skipUnlessPlatform(supported_platforms),
          decorators.skipUnlessArch('x86_64'),
          decorators.skipUnlessHasCallSiteInfo,
          decorators.skipIf(dwarf_version=['<', '4'])])
