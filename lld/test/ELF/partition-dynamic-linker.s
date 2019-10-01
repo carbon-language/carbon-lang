42ec0b777f1ee60b162588eef16a43c527be10d6
Peter Collingbourne <peter@pcc.me.uk>
2019-10-01 17:35:56 +0000

    Add missing REQUIRES: arm.
    llvm-svn: 373357

--- lld/test/ELF/partition-dynamic-linker.s
+++ lld/test/ELF/partition-dynamic-linker.s
@@ -1,6 +1,7 @@
 ## Test that we don't create a .ARM.exidx for the main partition.
 ## Previously we were doing so, which is unnecessary and led to a crash.
 
+# REQUIRES: arm
 # RUN: llvm-mc -filetype=obj -triple=armv7a-none-linux-gnueabi %p/Inputs/shared.s -o %t1.o
 # RUN: ld.lld -shared %t1.o -o %t.so
 # RUN: llvm-mc -filetype=obj -triple=armv7a-none-linux-gnueabi %s -o %t.o
