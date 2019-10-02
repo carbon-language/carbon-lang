47dbcbd8ec6bf6c0b9cbe5811e81a37cc55e73ef
Florian Hahn <flo@fhahn.com>
2019-10-02 08:32:25 +0000

    Revert [GlobalOpt] Pass DTU to removeUnreachableBlocks instead of recomputing.
    This breaks http://lab.llvm.org:8011/builders/sanitizer-windows/builds/52310
    
    This reverts r373430 (git commit 70f70035484ba199a329f9f8d9bd67e37bc2b408)
    
    llvm-svn: 373432

--- llvm/lib/Transforms/IPO/GlobalOpt.cpp
+++ llvm/lib/Transforms/IPO/GlobalOpt.cpp
@@ -2285,10 +2285,14 @@ OptimizeFunctions(Module &M,
     // So, remove unreachable blocks from the function, because a) there's
     // no point in analyzing them and b) GlobalOpt should otherwise grow
     // some more complicated logic to break these cycles.
+    // Removing unreachable blocks might invalidate the dominator so we
+    // recalculate it.
     if (!F->isDeclaration()) {
-      auto &DT = LookupDomTree(*F);
-      DomTreeUpdater DTU(DT, DomTreeUpdater::UpdateStrategy::Lazy);
-      Changed |= removeUnreachableBlocks(*F, nullptr, &DTU);
+      if (removeUnreachableBlocks(*F)) {
+        auto &DT = LookupDomTree(*F);
+        DT.recalculate(*F);
+        Changed = true;
+      }
     }
 
     Changed |= processGlobal(*F, GetTLI, LookupDomTree);
