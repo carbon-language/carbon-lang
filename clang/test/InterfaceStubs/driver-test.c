30c8df02ba22a2f9b2af44714f382e2e4a4aafdb
Puyan Lotfi <puyan@puyan.org>
2019-10-03 01:19:51 +0000

    Fixing broken builds due to r373538, issues with filepath and hexagon toolchain.
    It appears there are some issues with the hexagon toolchain, and also the file
    path for the library file. If this doesn't fix the remaining breakages I will
    attempt a revert.
    
    llvm-svn: 373552

--- clang/test/InterfaceStubs/driver-test.c
+++ clang/test/InterfaceStubs/driver-test.c
@@ -1,12 +1,19 @@
-// RUN: %clang -x c -o libfoo.so -emit-interface-stubs %s %S/object.c %S/weak.cpp && \
-// RUN: llvm-nm libfoo.so 2>&1 | FileCheck %s
+// REQUIRES: x86-registered-target
 
-// RUN: %clang -x c -o libfoo.so -shared %s %S/object.c %S/weak.cpp && \
-// RUN: llvm-nm libfoo.so 2>&1 | FileCheck %s
+// RUN: %clang -target x86_64-unknown-linux-gnu -x c -o %t1.so -emit-interface-stubs %s %S/object.c %S/weak.cpp && \
+// RUN: llvm-nm %t1.so 2>&1 | FileCheck --check-prefix=CHECK-IFS %s
 
-// CHECK-DAG: data
-// CHECK-DAG: foo
-// CHECK-DAG: strongFunc
-// CHECK-DAG: weakFunc
+// RUN: %clang -target x86_64-unknown-linux-gnu -x c -o %t2.so -shared %s %S/object.c %S/weak.cpp && \
+// RUN: llvm-nm %t2.so 2>&1 | FileCheck --check-prefix=CHECK-SO %s
+
+// CHECK-IFS-DAG: data
+// CHECK-IFS-DAG: foo
+// CHECK-IFS-DAG: strongFunc
+// CHECK-IFS-DAG: weakFunc
+
+// CHECK-SO-DAG: data
+// CHECK-SO-DAG: foo
+// CHECK-SO-DAG: strongFunc
+// CHECK-SO-DAG: weakFunc
 
 int foo(int bar) { return 42 + 1844; }
