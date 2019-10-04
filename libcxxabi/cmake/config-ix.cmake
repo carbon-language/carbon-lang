ce452b1ca9fdb12604db3dc2059246e189e55d7b
Louis Dionne <ldionne@apple.com>
2019-10-04 18:32:46 +0000

    [libc++abi] Link against libSystem on Apple platforms
    On Apple platforms, libSystem is an umbrella for all other system
    libraries, and libpthread (and friends) are actually just symlinks
    to libSystem.
    
    llvm-svn: 373770

--- libcxxabi/cmake/config-ix.cmake
+++ libcxxabi/cmake/config-ix.cmake
@@ -75,3 +75,4 @@ check_library_exists(dl dladdr "" LIBCXXABI_HAS_DL_LIB)
 check_library_exists(pthread pthread_once "" LIBCXXABI_HAS_PTHREAD_LIB)
 check_library_exists(c __cxa_thread_atexit_impl ""
   LIBCXXABI_HAS_CXA_THREAD_ATEXIT_IMPL)
+check_library_exists(System write "" LIBCXXABI_HAS_SYSTEM_LIB)
