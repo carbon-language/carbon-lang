35b63a435c95a75e21929966c05fdc0c4a66b6aa
Adrian Prantl <aprantl@apple.com>
2019-10-08 21:34:22 +0000

    Revert Trust the arange accelerator tables in dSYMs
    This reverts r374117 (git commit 6399db2f6fd64fa250093368be40eb5ae3af513b)
    while inspecting bot breakage.
    
    llvm-svn: 374121

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFDebugInfo.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFDebugInfo.cpp
@@ -54,19 +54,13 @@ llvm::Expected<DWARFDebugAranges &> DWARFDebugInfo::GetCompileUnitAranges() {
 
   // Manually build arange data for everything that wasn't in the
   // .debug_aranges table.
-  //
-  // This step is skipped for dSYMs and other debug-info-only
-  // objects, which are always trusted to have a complete table.
-  auto *obj = m_dwarf.GetObjectFile();
-  if (!obj || obj->GetType() != ObjectFile::eTypeDebugInfo) {
-    const size_t num_units = GetNumUnits();
-    for (size_t idx = 0; idx < num_units; ++idx) {
-      DWARFUnit *cu = GetUnitAtIndex(idx);
+  const size_t num_units = GetNumUnits();
+  for (size_t idx = 0; idx < num_units; ++idx) {
+    DWARFUnit *cu = GetUnitAtIndex(idx);
 
-      dw_offset_t offset = cu->GetOffset();
-      if (cus_with_data.find(offset) == cus_with_data.end())
-        cu->BuildAddressRangeTable(m_cu_aranges_up.get());
-    }
+    dw_offset_t offset = cu->GetOffset();
+    if (cus_with_data.find(offset) == cus_with_data.end())
+      cu->BuildAddressRangeTable(m_cu_aranges_up.get());
   }
 
   const bool minimize = true;
