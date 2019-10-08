ca2973bb2024a72efd9bd583d6587bfff0a5545f
Andrey Churbanov <Andrey.Churbanov@intel.com>
2019-10-08 12:39:04 +0000

    Don't assume Type from `readelf -d` has parentheses
    Patch by jbeich (Jan Beich)
    
    Differential Revision: https://reviews.llvm.org/D68053
    
    llvm-svn: 374038

--- openmp/runtime/tools/check-depends.pl
+++ openmp/runtime/tools/check-depends.pl
@@ -131,7 +131,7 @@ sub get_deps_readelf($) {
     # Parse body.
     while ( $i < @bulk ) {
         my $line = $bulk[ $i ];
-        if ( $line !~ m{^\s*0x[0-9a-f]+\s+\(([_A-Z0-9]+)\)\s+(.*)\s*$}i ) {
+        if ( $line !~ m{^\s*0x[0-9a-f]+\s+\(?([_A-Z0-9]+)\)?\s+(.*)\s*$}i ) {
             parse_error( $tool, @bulk, $i );
         }; # if
         my ( $type, $value ) = ( $1, $2 );
