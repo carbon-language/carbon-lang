c7cfa7c34e3ea9aa20288d2475ccb2ca85730605
Vedant Kumar <vsk@apple.com>
2019-10-09 20:21:33 +0000

    [test] Skip entry value test when clang < 10.0.0
    clang-9 emitted the wrong opcode for entry values on Darwin.
    
    rdar://56119661
    
    llvm-svn: 374212

--- lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/TestBasicEntryValuesX86_64.py
+++ lldb/packages/Python/lldbsuite/test/functionalities/param_entry_vals/basic_entry_values_x86_64/TestBasicEntryValuesX86_64.py
@@ -7,6 +7,7 @@ supported_platforms.extend(lldbplatformutil.getDarwinOSTriples())
 
 lldbinline.MakeInlineTest(__file__, globals(),
         [decorators.skipUnlessPlatform(supported_platforms),
+         decorators.skipIf(compiler="clang", compiler_version=['<', '10.0']),
          decorators.skipUnlessArch('x86_64'),
          decorators.skipUnlessHasCallSiteInfo,
          decorators.skipIf(dwarf_version=['<', '4'])])
