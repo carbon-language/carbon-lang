07775b207a95da2abd07b286310e224865bde203
Rui Ueyama <ruiu@google.com>
2019-10-09 07:04:38 +0000

    Use lld-link instead of llvm-dlltool to create an implib
    Suggested by Martin Storsjö.
    
    llvm-svn: 374142

--- lld/test/COFF/imports-ordinal-only.s
+++ lld/test/COFF/imports-ordinal-only.s
@@ -1,6 +1,6 @@
 # REQUIRES: x86
 #
-# RUN: llvm-dlltool -k -m i386 --input-def %p/Inputs/ordinal-only-implib.def --output-lib %t-implib.a
+# RUN: lld-link -machine:x86 -def:%p/Inputs/ordinal-only-implib.def -implib:%t-implib.a
 # RUN: llvm-mc -triple=i386-pc-win32 %s -filetype=obj -o %t.obj
 # RUN: lld-link -out:%t.exe -entry:main -subsystem:console -safeseh:no -debug %t.obj %t-implib.a
 # RUN: llvm-objdump -private-headers %t.exe | FileCheck --match-full-lines %s
