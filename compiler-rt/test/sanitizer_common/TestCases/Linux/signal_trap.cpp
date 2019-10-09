c3317658cf26471d78008c6f401d98e36f4fbdd7
Vitaly Buka <vitalybuka@google.com>
2019-10-09 20:18:27 +0000

    [sanitizer] Use raise() in test and cover more signals
    llvm-svn: 374211

--- compiler-rt/test/sanitizer_common/TestCases/Linux/signal_trap.cpp
+++
@@ -1,8 +0,0 @@
-// RUN: %clangxx -O1 %s -o %t && %env_tool_opts=handle_sigtrap=2 not %run %t 2>&1 | FileCheck %s
-
-int main() {
-  __builtin_debugtrap();
-}
-
-// CHECK: Sanitizer:DEADLYSIGNAL
-// CHECK: Sanitizer: TRAP on unknown address
