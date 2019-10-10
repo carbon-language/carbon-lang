7cc1fac8c9a1a4c9f9b146321177b8a4cb29b558
Jonas Devlieghere <jonas@devlieghere.com>
2019-10-10 19:51:47 +0000

    [test] Cleanup top-level lit.cfg.py
    llvm-svn: 374434

--- lldb/test/lit.cfg.py
+++ lldb/test/lit.cfg.py
@@ -1,41 +1,15 @@
 # -*- Python -*-
 
 import os
-import platform
-import re
-import shutil
-import site
-import sys
 
 import lit.formats
 from lit.llvm import llvm_config
-from lit.llvm.subst import FindTool
-from lit.llvm.subst import ToolSubst
-from distutils.spawn import find_executable
 
 # This is the top level configuration. Most of these configuration options will
 # be overriden by individual lit configuration files in the test
-# subdirectories.
+# subdirectories. Anything configured here will *not* be loaded when pointing
+# lit at on of the subdirectories.
 
-# name: The name of this test suite.
 config.name = 'lldb'
-
-# testFormat: The test format to use to interpret tests.
-config.test_format = lit.formats.ShTest(not llvm_config.use_lit_shell)
-
-# suffixes: A list of file extensions to treat as test files. This is overriden
-# by individual lit.local.cfg files in the test subdirectories.
-config.suffixes = ['.test', '.cpp', '.s']
-
-# excludes: A list of directories to exclude from the testsuite. The 'Inputs'
-# subdirectories contain auxiliary inputs for various tests in their parent
-# directories.
-config.excludes = ['Inputs', 'CMakeLists.txt', 'README.txt', 'LICENSE.txt']
-
-# test_source_root: The root path where tests are located.
 config.test_source_root = os.path.dirname(__file__)
-
-# test_exec_root: The root path where tests should be run.
 config.test_exec_root = os.path.join(config.lldb_obj_root, 'test')
-
-llvm_config.use_default_substitutions()
