9d4806a387892972fd544c0dcaefb0926126220c
Reid Kleckner <rnk@google.com>
2019-10-10 18:01:20 +0000

    Re-land "Use -fdebug-compilation-dir to form absolute paths in coverage mappings"
    This reverts r374324 (git commit 62808631acceaa8b78f8ab9b407eb6b943ff5f77)
    
    I changed the test to not rely on finding the sequence "clang, test,
    CoverageMapping" in the CWD used to run the test. Instead it makes its
    own internal directory hierarchy of foo/bar/baz and looks for that.
    
    llvm-svn: 374403

--- clang/lib/CodeGen/CoverageMappingGen.h
+++ clang/lib/CodeGen/CoverageMappingGen.h
@@ -54,10 +54,14 @@ class CoverageMappingModuleGen {
   std::vector<llvm::Constant *> FunctionNames;
   llvm::StructType *FunctionRecordTy;
   std::vector<std::string> CoverageMappings;
+  SmallString<256> CWD;
+
+  /// Make the filename absolute, remove dots, and normalize slashes to local
+  /// path style.
+  std::string normalizeFilename(StringRef Filename);
 
 public:
-  CoverageMappingModuleGen(CodeGenModule &CGM, CoverageSourceInfo &SourceInfo)
-      : CGM(CGM), SourceInfo(SourceInfo), FunctionRecordTy(nullptr) {}
+  CoverageMappingModuleGen(CodeGenModule &CGM, CoverageSourceInfo &SourceInfo);
 
   CoverageSourceInfo &getSourceInfo() const {
     return SourceInfo;
