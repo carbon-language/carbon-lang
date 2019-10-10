90c8b59cfc6ea5c1b1f6e024c3a31b990b1b0e66
David Carlier <devnexen@gmail.com>
2019-10-10 11:31:37 +0000

    [Sanitizers] Porting getrandom/getentropy interceptors to FreeBSD
    - Available from 12.x branch, by the time it lands next year in FreeBSD tree, the 11.x's might be EOL.
    - Intentionally changed the getrandom test to C code as with 12.0 (might be fixed in CURRENT since), there is a linkage issue in C++ context.
    
    Reviewers: emaste, dim, vitalybuka
    
    Reviewed-By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D68451
    
    llvm-svn: 374315

--- compiler-rt/test/sanitizer_common/TestCases/Posix/getrandom.c
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/getrandom.c
@@ -1,5 +1,5 @@
-// RUN: %clangxx -O2 %s -o %t && %run %t
-// UNSUPPORTED: android
+// RUN: %clang -O2 %s -o %t && %run %t
+// UNSUPPORTED: android netbsd darwin solaris
 //
 
 #include <sys/types.h>
@@ -8,14 +8,18 @@
 #define __GLIBC_PREREQ(a, b) 0
 #endif
 
-#if __GLIBC_PREREQ(2, 25)
+#if (defined(__linux__) && __GLIBC_PREREQ(2, 25)) || defined(__FreeBSD__)
+#define HAS_GETRANDOM
+#endif
+
+#if defined(HAS_GETRANDOM)
 #include <sys/random.h>
 #endif
 
 int main() {
   char buf[16];
   ssize_t n = 1;
-#if __GLIBC_PREREQ(2, 25)
+#if defined(HAS_GETRANDOM)
   n = getrandom(buf, sizeof(buf), 0);
 #endif
   return (int)(n <= 0);
