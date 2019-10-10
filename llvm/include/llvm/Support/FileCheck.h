dfd2b6f07fc40a190335f580d8a965bbebfe94df
Kai Nacke <kai.nacke@redstar.de>
2019-10-10 13:15:41 +0000

    [FileCheck] Implement --ignore-case option.
    The FileCheck utility is enhanced to support a `--ignore-case`
    option. This is useful in cases where the output of Unix tools
    differs in case (e.g. case not specified by Posix).
    
    Reviewers: Bigcheese, jakehehrlich, rupprecht, espindola, alexshap, jhenderson, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D68146
    
    llvm-svn: 374339

--- llvm/include/llvm/Support/FileCheck.h
+++ llvm/include/llvm/Support/FileCheck.h
@@ -30,6 +30,7 @@ struct FileCheckRequest {
   std::vector<std::string> GlobalDefines;
   bool AllowEmptyInput = false;
   bool MatchFullLines = false;
+  bool IgnoreCase = false;
   bool EnableVarScope = false;
   bool AllowDeprecatedDagOverlap = false;
   bool Verbose = false;
