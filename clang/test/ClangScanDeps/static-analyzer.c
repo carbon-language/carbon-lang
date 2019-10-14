9d0a84f5f3d30c1399b8a983d4ace91106c47853
Jan Korous <jkorous@apple.com>
2019-10-14 21:06:11 +0000

    [NFC] Fix ClangScanDeps/static-analyzer.c test on Windows
    Follow-up to c5d14b5c6fa
    
    llvm-svn: 374821

--- clang/test/ClangScanDeps/static-analyzer.c
+++ clang/test/ClangScanDeps/static-analyzer.c
@@ -1,12 +1,12 @@
 // RUN: rm -rf %t.dir
-// RUN: rm -rf %t.dir/cdb.json
+// RUN: rm -rf %t-cdb.json
 // RUN: mkdir -p %t.dir
 // RUN: cp %s %t.dir/static-analyzer.c
 // RUN: mkdir %t.dir/Inputs
 // RUN: cp %S/Inputs/header.h %t.dir/Inputs/analyze_header_input.h
-// RUN: sed -e "s|DIR|%t.dir|g" %S/Inputs/static-analyzer-cdb.json > %t.dir/cdb.json
+// RUN: sed -e "s|DIR|%/t.dir|g" %S/Inputs/static-analyzer-cdb.json > %t-cdb.json
 //
-// RUN: clang-scan-deps -compilation-database %t.dir/cdb.json -j 1 | FileCheck %s
+// RUN: clang-scan-deps -compilation-database %t-cdb.json -j 1 | FileCheck %s
 
 #ifdef __clang_analyzer__
 #include "Inputs/analyze_header_input.h"
