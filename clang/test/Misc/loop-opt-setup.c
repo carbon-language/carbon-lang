d93d001cbad37b19c7e70c8ed8ec2ac6a7e05940
Eric Christopher <echristo@gmail.com>
2019-10-14 23:01:48 +0000

    Add -fno-experimental-pass-manager to make clear which pass manager we're running and to make flipping the default not regress testing.
    llvm-svn: 374840

--- clang/test/Misc/loop-opt-setup.c
+++ clang/test/Misc/loop-opt-setup.c
@@ -1,5 +1,5 @@
 // RUN: %clang -O1 -fexperimental-new-pass-manager -fno-unroll-loops -S -o - %s -emit-llvm | FileCheck %s
-// RUN: %clang -O1 -fno-unroll-loops -S -o - %s -emit-llvm | FileCheck %s
+// RUN: %clang -O1 -fno-experimental-new-pass-manager -fno-unroll-loops -S -o - %s -emit-llvm | FileCheck %s
 extern int a[16];
 int b = 0;
 int foo(void) {
