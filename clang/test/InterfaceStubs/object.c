4e4b4f4099a6fdff9caba93535c8f5e23f10a40f
Puyan Lotfi <puyan@puyan.org>
2019-10-14 18:57:29 +0000

    [clang][IFS][test] Fixing lit test breakages on macOS due to r374798
    Adding the quotes breaks tests because on Darwin the name mangling is prefixed
    with an underscore.
    
    llvm-svn: 374805

--- clang/test/InterfaceStubs/object.c
+++ clang/test/InterfaceStubs/object.c
@@ -2,6 +2,6 @@
 // RUN: %clang -fvisibility=default -c -o - -emit-interface-stubs %s | FileCheck -check-prefix=CHECK-SYMBOLS %s
 // RUN: %clang -fvisibility=default -c -o - %s | llvm-nm - 2>&1 | FileCheck -check-prefix=CHECK-SYMBOLS %s
 
-// CHECK-TAPI: "data" : { Type: Object, Size: 4 }
+// CHECK-TAPI: data" : { Type: Object, Size: 4 }
 // CHECK-SYMBOLS: data
 int data = 42;
