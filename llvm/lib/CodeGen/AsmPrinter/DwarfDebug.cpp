8535bed79504ffe3ed7f2de8c158a17e79e54736
David Stenberg <david.stenberg@ericsson.com>
2019-10-14 12:49:58 +0000

    [DebugInfo] Fix truncation of call site immediates
    Summary:
    This addresses a bug in collectCallSiteParameters() where call site
    immediates would be truncated from int64_t to unsigned.
    
    This fixes PR43525.
    
    Reviewers: djtodoro, NikolaPrica, aprantl, vsk
    
    Reviewed By: aprantl
    
    Subscribers: hiraditya, llvm-commits
    
    Tags: #debug-info, #llvm
    
    Differential Revision: https://reviews.llvm.org/D68869
    
    llvm-svn: 374770

--- llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DwarfDebug.cpp
@@ -620,7 +620,7 @@ static void collectCallSiteParameters(const MachineInstr *CallMI,
 
     if (auto ParamValue = TII->describeLoadedValue(*I)) {
       if (ParamValue->first.isImm()) {
-        unsigned Val = ParamValue->first.getImm();
+        int64_t Val = ParamValue->first.getImm();
         DbgValueLoc DbgLocVal(ParamValue->second, Val);
         finishCallSiteParam(DbgLocVal, Reg);
       } else if (ParamValue->first.isReg()) {
