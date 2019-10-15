0650355c09ab8e6605ae37b818270a7a7c8ce2c7
Jian Cai <caij2003@gmail.com>
2019-10-15 18:17:08 +0000

    [clang] refactor -Wa,-W test cases.
    Remove REQUIRES and only keep the clang driver tests, since the
    assembler are already tested with -Wa,--no-warn. This way we could run
    the test on non-linux platforms and catch breaks on them.
    
    llvm-svn: 374932

--- clang/test/Driver/as-w-option.c
+++ clang/test/Driver/as-w-option.c
@@ -1,17 +1,7 @@
 // RUN: %clang -### %s -c -o tmp.o -fno-integrated-as -Wa,-W 2>&1 | FileCheck -check-prefix=CHECK-NOIAS %s
 // RUN: %clang -### %s -c -o tmp.o -integrated-as -Wa,-W 2>&1 | FileCheck -check-prefix=CHECK-IAS %s
-// RUN: %clang %s -c -o %t.o -integrated-as -Wa,-W 2>&1 | FileCheck -allow-empty --check-prefix=CHECK-AS-NOWARN %s
-// RUN: %clang %s -c -o %t.o -fno-integrated-as -Wa,-W 2>&1 | FileCheck -allow-empty --check-prefix=CHECK-AS-NOWARN %s
-// RUN: not %clang %s -c -o %t.o -integrated-as -Wa,--fatal-warnings 2>&1 | FileCheck --check-prefix=CHECK-AS-FATAL %s
-// RUN: not %clang %s -c -o %t.o -fno-integrated-as -Wa,--fatal-warnings 2>&1 | FileCheck --check-prefix=CHECK-AS-FATAL %s
-
-// REQUIRES: clang-driver
-// REQUIRES: linux
 
 // CHECK-IAS: "-cc1" {{.*}} "-massembler-no-warn"
 // CHECK-NOIAS: "-W"
-// CHECK-AS-NOWARN-NOT: warning:
-// CHECK-AS-FATAL-NOT: warning:
-// CHECK-AS-FATAL: error
 
 __asm(".warning");
