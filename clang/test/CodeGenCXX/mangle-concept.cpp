1e3e77e0b689fc3d2eab48d3a5179d6fed1941eb
Saar Raz <saar@raz.email>
2019-10-16 02:56:40 +0000

    Fix failing mangle-concept.cpp test.
    llvm-svn: 374971

--- clang/test/CodeGenCXX/mangle-concept.cpp
+++ clang/test/CodeGenCXX/mangle-concept.cpp
@@ -6,11 +6,11 @@ template <bool> struct S {};
 template <typename> concept C = true;
 template <typename T = int> S<C<T>> f0() { return S<C<T>>{}; }
 template S<C<int>> f0<>();
-// CHECK: void @_ZN5test12f0IiEENS_1SIXL_ZNS_1CIT_EEEEEEv()
+// CHECK: void @_ZN5test12f0IiEENS_1SIXL_ZNS_1CIT_EEEEEEv(
 }
 
 template <bool> struct S {};
 template <typename> concept C = true;
 template <typename T = int> S<C<T>> f0() { return S<C<T>>{}; }
 template S<C<int>> f0<>();
-// CHECK: void @_Z2f0IiE1SIXL_Z1CIT_EEEEv()
+// CHECK: void @_Z2f0IiE1SIXL_Z1CIT_EEEEv(
