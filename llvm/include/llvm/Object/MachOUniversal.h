68db51d5c677239fe5ca286f068909479deb75c7
Alexander Shaposhnikov <shal1t712@gmail.com>
2019-10-17 18:48:07 +0000

    [Object] Fix the return type of getOffset/getSize
    Header64.offset/Header64.size are uint64_t, thus we should not 
    truncate them to unit32_t. Moreover, there are a number of places 
    where we sum the offset and the size (e.g. in various checks in MachOUniversal.cpp),
    the truncation causes issues since the offset/size can perfectly fit into uint32_t, 
    while the sum overflows.
    
    Differential revision: https://reviews.llvm.org/D69126
    
    Test plan: make check-all
    
    llvm-svn: 375154

--- llvm/include/llvm/Object/MachOUniversal.h
+++ llvm/include/llvm/Object/MachOUniversal.h
@@ -66,13 +66,13 @@ public:
       else // Parent->getMagic() == MachO::FAT_MAGIC_64
         return Header64.cpusubtype;
     }
-    uint32_t getOffset() const {
+    uint64_t getOffset() const {
       if (Parent->getMagic() == MachO::FAT_MAGIC)
         return Header.offset;
       else // Parent->getMagic() == MachO::FAT_MAGIC_64
         return Header64.offset;
     }
-    uint32_t getSize() const {
+    uint64_t getSize() const {
       if (Parent->getMagic() == MachO::FAT_MAGIC)
         return Header.size;
       else // Parent->getMagic() == MachO::FAT_MAGIC_64
