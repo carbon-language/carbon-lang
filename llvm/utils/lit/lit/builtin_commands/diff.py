5e684e8d88ba60c867b12dbde274db0730d7b517
Joel E. Denny <jdenny.ornl@gmail.com>
2019-10-17 14:43:26 +0000

    Revert r375116: "[lit] Extend internal diff to support `-` argument"
    This series of patches still breaks a Windows bot.
    
    llvm-svn: 375120

--- llvm/utils/lit/lit/builtin_commands/diff.py
+++ llvm/utils/lit/lit/builtin_commands/diff.py
@@ -31,13 +31,8 @@ def getDirTree(path, basedir=""):
 def compareTwoFiles(flags, filepaths):
     filelines = []
     for file in filepaths:
-        if file == "-":
-            stdin_fileno = sys.stdin.fileno()
-            with os.fdopen(os.dup(stdin_fileno), 'rb') as stdin_bin:
-                filelines.append(stdin_bin.readlines())
-        else:
-            with open(file, 'rb') as file_bin:
-                filelines.append(file_bin.readlines())
+        with open(file, 'rb') as file_bin:
+            filelines.append(file_bin.readlines())
 
     try:
         return compareTwoTextFiles(flags, filepaths, filelines,
@@ -218,13 +213,10 @@ def main(argv):
     exitCode = 0
     try:
         for file in args:
-            if file != "-" and not os.path.isabs(file):
+            if not os.path.isabs(file):
                 file = os.path.realpath(os.path.join(os.getcwd(), file))
 
             if flags.recursive_diff:
-                if file == "-":
-                    sys.stderr.write("Error: cannot recursively compare '-'\n")
-                    sys.exit(1)
                 dir_trees.append(getDirTree(file))
             else:
                 filepaths.append(file)
