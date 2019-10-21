0cfd9e5b580f06d63be7054ba4efa0524011f096
Fangrui Song <maskray@google.com>
2019-10-21 18:04:52 +0000

    [test] Merge Driver/as-w-warnings.c into as-no-warnings.c
    For -integrated-as RUN lines we can remove -target.
    
    llvm-svn: 375439

--- clang/test/Driver/as-w-option.c
+++
@@ -1,7 +0,0 @@
-// RUN: %clang -### %s -c -o tmp.o -fno-integrated-as --target=x86_64-linux-gnu -Wa,-W 2>&1 | FileCheck -check-prefix=CHECK-NOIAS %s
-// RUN: %clang -### %s -c -o tmp.o -integrated-as -Wa,-W 2>&1 | FileCheck -check-prefix=CHECK-IAS %s
-
-// CHECK-IAS: "-cc1" {{.*}} "-massembler-no-warn"
-// CHECK-NOIAS: "-W"
-
-__asm(".warning");
