b82fa80e807df81a712fb55a30b91ee96e1eca07
Sanjay Patel <spatel@rotateright.com>
2019-10-24 09:55:07 -0400

    [SLP] adjust code comment; NFC
    (check commit access)

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -3397,7 +3397,7 @@ int BoUpSLP::getSpillCost() const {
         continue;
       }
 
-      // Debug informations don't impact spill cost.
+      // Debug information does not impact spill cost.
       if ((isa<CallInst>(&*PrevInstIt) &&
            !isa<DbgInfoIntrinsic>(&*PrevInstIt)) &&
           &*PrevInstIt != PrevInst)
