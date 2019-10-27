418d1ea555cd18bef83b35c9ee9bbc18a3da9abe
Saleem Abdulrasool <compnerd@compnerd.org>
2019-10-27 18:29:58 -0400

    PM: silence `-Wpessimizing-move` from GCC 9.2.1 (NFC)
    Remove the explicit move enabling NVRO.

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -1445,7 +1445,7 @@ auto parsePassParameters(ParametersParseCallableT &&Parser, StringRef Name,
   Expected<ParametersT> Result = Parser(Params);
   assert((Result || Result.template errorIsA<StringError>()) &&
          "Pass parameter parser can only return StringErrors.");
-  return std::move(Result);
+  return Result;
 }
 
 /// Parser of parameters for LoopUnroll pass.
