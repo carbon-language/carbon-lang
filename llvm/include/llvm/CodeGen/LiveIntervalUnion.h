e4d6f1300fa2dd12d9d68fab2ded72c0e69b19ee
Marcello Maggioni <mmaggioni@apple.com>
2019-10-28 09:15:52 -0700

    [LiveIntervalUnion] Expose extraction of last index in map for external users
    Differential Revision: https://reviews.llvm.org/D69515

--- llvm/include/llvm/CodeGen/LiveIntervalUnion.h
+++ llvm/include/llvm/CodeGen/LiveIntervalUnion.h
@@ -75,6 +75,7 @@ public:
 
   bool empty() const { return Segments.empty(); }
   SlotIndex startIndex() const { return Segments.start(); }
+  SlotIndex endIndex() const { return Segments.stop(); }
 
   // Provide public access to the underlying map to allow overlap iteration.
   using Map = LiveSegments;
