d46c65592e3ac6a78c54514e4919d505c1f0c74a
Davide Italiano <ditaliano@apple.com>
2019-10-29 13:05:56 -0700

    [ValueObjectDisplay] Generalize the description of an option.

--- lldb/source/Interpreter/OptionGroupValueObjectDisplay.cpp
+++ lldb/source/Interpreter/OptionGroupValueObjectDisplay.cpp
@@ -41,7 +41,7 @@ static const OptionDefinition g_option_table[] = {
      {}, 0, eArgTypeNone, "Show variable location information."},
     {LLDB_OPT_SET_1, false, "object-description", 'O',
      OptionParser::eNoArgument, nullptr, {}, 0, eArgTypeNone,
-     "Print as an Objective-C object."},
+     "Display using a language-specific description API, if possible."},
     {LLDB_OPT_SET_1, false, "ptr-depth", 'P', OptionParser::eRequiredArgument,
      nullptr, {}, 0, eArgTypeCount, "The number of pointers to be traversed "
                                     "when dumping values (default is zero)."},
