e9f9dfab82bbaadc6b6d8d0eafba017ef5d94fd0
Jon Chesterfield <jonathanchesterfield@gmail.com>
2019-10-31 01:58:29 +0000

    [libomptarget] Change nvcc compilation to use a unity build
    Summary:
    [libomptarget] Change nvcc compilation to use a unity build
    
    This allows nvcc to inline functions between what would otherwise be distinct
    translation units, which in turn removes any runtime cost from implementing
    functions in source files (as opposed to inline in headers).
    
    This will then allow the circular dependencies in deviceRTL to be readily
    broken and individual components more easily shared between architectures.
    
    Reviewers: ABataev, jdoerfert, grokos, RaviNarayanaswamy, hfinkel, ronlieb, gregrodgers
    
    Reviewed By: jdoerfert
    
    Subscribers: mgorny, openmp-commits
    
    Tags: #openmp
    
    Differential Revision: https://reviews.llvm.org/D69489

---
+++ openmp/libomptarget/deviceRTLs/nvptx/unity.cu
@@ -0,0 +1,25 @@
+//===------ unity.cu - Unity build of NVPTX deviceRTL ------------ CUDA -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Support compilers, specifically NVCC, which have not implemented link time
+// optimisation. This removes the runtime cost of moving inline functions into
+// source files in exchange for preventing efficient incremental builds.
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/cancel.cu"
+#include "src/critical.cu"
+#include "src/data_sharing.cu"
+#include "src/libcall.cu"
+#include "src/loop.cu"
+#include "src/omp_data.cu"
+#include "src/omptarget-nvptx.cu"
+#include "src/parallel.cu"
+#include "src/reduction.cu"
+#include "src/sync.cu"
+#include "src/task.cu"
