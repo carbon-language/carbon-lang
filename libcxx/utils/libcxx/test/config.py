dba83965722b540f6baf43163210943c41e1378a
Dávid Bolvanský <david.bolvansky@gmail.com>
2019-11-01 13:46:05 +0100

    [libcxx] Disable -Wconstant-evaluated for testsuite
    Reviewers: EricWF
    
    Subscribers: christof, ldionne, libcxx-commits
    
    Tags: #libc
    
    Differential Revision: https://reviews.llvm.org/D69708

--- libcxx/utils/libcxx/test/config.py
+++ libcxx/utils/libcxx/test/config.py
@@ -915,6 +915,7 @@ class Configuration(object):
         self.cxx.addWarningFlagIfSupported('-Wshadow')
         self.cxx.addWarningFlagIfSupported('-Wno-unused-command-line-argument')
         self.cxx.addWarningFlagIfSupported('-Wno-attributes')
+        self.cxx.addWarningFlagIfSupported('-Wno-constant-evaluated')
         self.cxx.addWarningFlagIfSupported('-Wno-pessimizing-move')
         self.cxx.addWarningFlagIfSupported('-Wno-c++11-extensions')
         self.cxx.addWarningFlagIfSupported('-Wno-user-defined-literals')
