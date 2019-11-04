1abb2c1a39fb20e12210472fda42a0c942f83be8
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-11-04 15:30:18 +0000

    AliasSetTracker - fix uninitialized variable warnings. NFCI.

--- llvm/include/llvm/Analysis/AliasSetTracker.h
+++ llvm/include/llvm/Analysis/AliasSetTracker.h
@@ -344,8 +344,8 @@ class AliasSetTracker {
   struct ASTCallbackVHDenseMapInfo : public DenseMapInfo<Value *> {};
 
   AliasAnalysis &AA;
-  MemorySSA *MSSA;
-  Loop *L;
+  MemorySSA *MSSA = nullptr;
+  Loop *L = nullptr;
   ilist<AliasSet> AliasSets;
 
   using PointerMapType = DenseMap<ASTCallbackVH, AliasSet::PointerRec *,
