ee10d934dddd49eccfd3b6e6ae8f02551b1a03a4
Bill Wendling <morbo@google.com>
2019-11-05 13:09:42 -0800

    Fix typo so that '-O0' is correctly specified

--- clang/test/CodeGen/asm-goto.c
+++ clang/test/CodeGen/asm-goto.c
@@ -1,16 +1,16 @@
 // REQUIRES: x86-registered-target
 // RUN: %clang_cc1 -triple x86_64-pc-linux-gnu -O0 -emit-llvm %s -o - | FileCheck %s
-// RUN: %clang_cc1 -triple i386-pc-linux-gnu-O0 -emit-llvm %s -o - | FileCheck %s
+// RUN: %clang_cc1 -triple i386-pc-linux-gnu -O0 -emit-llvm %s -o - | FileCheck %s
 
 int foo(int cond)
 {
   // CHECK: callbr void asm sideeffect
-  // CHECK: to label %asm.fallthrough [label %label_true, label %loop], !srcloc
+  // CHECK: to label %asm.fallthrough [label %label_true, label %loop]
   // CHECK: asm.fallthrough:
   asm volatile goto("testl %0, %0; jne %l1;" :: "r"(cond)::label_true, loop);
   asm volatile goto("testl %0, %0; jne %l1;" :: "r"(cond)::label_true, loop);
   // CHECK: callbr void asm sideeffect
-  // CHECK: to label %asm.fallthrough1 [label %label_true, label %loop], !srcloc
+  // CHECK: to label %asm.fallthrough1 [label %label_true, label %loop]
   // CHECK: asm.fallthrough1:
   return 0;
 loop:
