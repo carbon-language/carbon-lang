bdeb2724f0aa9c518f94d998d24d8620a1e88727
Jan Korous <jkorous@apple.com>
2019-11-07 14:40:22 -0800

    [compiler-rt] Fix tests after 03b84e4f6d0
    Fallout from:
    [clang] Report sanitizer blacklist as a dependency in cc1
    
    Default blacklists are now passed via -fsanitize-system-blacklist from driver to cc1.

--- compiler-rt/test/msan/default_blacklist.cpp
+++ compiler-rt/test/msan/default_blacklist.cpp
@@ -1,3 +1,3 @@
 // Test that MSan uses the default blacklist from resource directory.
 // RUN: %clangxx_msan -### %s 2>&1 | FileCheck %s
-// CHECK: fsanitize-blacklist={{.*}}msan_blacklist.txt
+// CHECK: fsanitize-system-blacklist={{.*}}msan_blacklist.txt
