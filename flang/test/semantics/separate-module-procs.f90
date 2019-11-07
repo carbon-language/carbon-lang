17332e283e257b4259a315ce21cf53784f3b6060
kiranktp <kirankumart.tp@amd.com>
2019-11-07 15:17:57 +0530

    [flang] Fix for flang-compiler/f18#694 - Unexpected error when compiling submodule Incorporated all review comments and updated the test case.
    Change-Id: I03939bfc705cc5319a0b7da3305026b8403b8edc
    
    Original-commit: flang-compiler/f18@5c602bfbc74a3cad1de6f35204bbfd64ac29bfa3
    Reviewed-on: https://github.com/flang-compiler/f18/pull/817

--- flang/test/semantics/separate-module-procs.f90
+++ flang/test/semantics/separate-module-procs.f90
@@ -1,10 +1,10 @@
-!===-- separate-module-procs-2.f90 - Test separate module procedure ---------===
+!===--- separate-module-procs.f90 - Test separate module procedure ---------===
 !
 ! Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 ! See https://llvm.org/LICENSE.txt for license information.
 ! SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 !
-!===-------------------------------------------------------------------------===
+!===------------------------------------------------------------------------===
 
 ! case 1: ma_create_new_fun' was not declared a separate module procedure
 module m1
