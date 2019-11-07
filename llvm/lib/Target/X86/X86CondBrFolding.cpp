f08324067939d299047cf742f7d5224060e2e29c
Simon Pilgrim <llvm-dev@redking.me.uk>
2019-11-07 16:41:48 +0000

    X86CondBrFolding - remove non-existent fixBranchProb function. NFC.

--- llvm/lib/Target/X86/X86CondBrFolding.cpp
+++ llvm/lib/Target/X86/X86CondBrFolding.cpp
@@ -115,8 +115,6 @@ private:
 
   void optimizeCondBr(MachineBasicBlock &MBB,
                       SmallVectorImpl<MachineBasicBlock *> &BranchPath);
-  void fixBranchProb(MachineBasicBlock *NextMBB, MachineBasicBlock *RootMBB,
-                     SmallVectorImpl<MachineBasicBlock *> &BranchPath);
   void replaceBrDest(MachineBasicBlock *MBB, MachineBasicBlock *OrigDest,
                      MachineBasicBlock *NewDest);
   void fixupModifiedCond(MachineBasicBlock *MBB);
