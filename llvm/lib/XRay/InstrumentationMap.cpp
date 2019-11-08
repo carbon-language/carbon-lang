1d321434a202b12b3afe6f305262b930bbf05665
Aditya Kumar <hiraditya@msn.com>
2019-11-07 21:20:34 -0800

    [llvm-xray] Add AArch64 to llvm-xray extract
    This required adding support for resolving R_AARCH64_ABS64 relocations to
    get accurate addresses for function names to resolve.
    
    Authored by: ianlevesque (Ian Levesque)
    Reviewers: dberris, phosek, smeenai, tetsuo-cpp
    Differential Revision: https://reviews.llvm.org/D69967

--- llvm/lib/XRay/InstrumentationMap.cpp
+++ llvm/lib/XRay/InstrumentationMap.cpp
@@ -20,6 +20,7 @@
 #include "llvm/Object/Binary.h"
 #include "llvm/Object/ELFObjectFile.h"
 #include "llvm/Object/ObjectFile.h"
+#include "llvm/Object/RelocationResolver.h"
 #include "llvm/Support/DataExtractor.h"
 #include "llvm/Support/Error.h"
 #include "llvm/Support/FileSystem.h"
@@ -59,7 +60,8 @@ loadObj(StringRef Filename, object::OwningBinary<object::ObjectFile> &ObjFile,
   // Find the section named "xray_instr_map".
   if ((!ObjFile.getBinary()->isELF() && !ObjFile.getBinary()->isMachO()) ||
       !(ObjFile.getBinary()->getArch() == Triple::x86_64 ||
-        ObjFile.getBinary()->getArch() == Triple::ppc64le))
+        ObjFile.getBinary()->getArch() == Triple::ppc64le ||
+        ObjFile.getBinary()->getArch() == Triple::aarch64))
     return make_error<StringError>(
         "File format not supported (only does ELF and Mach-O little endian 64-bit).",
         std::make_error_code(std::errc::not_supported));
@@ -99,12 +101,22 @@ loadObj(StringRef Filename, object::OwningBinary<object::ObjectFile> &ObjFile,
         return static_cast<uint32_t>(0);
     }(ObjFile.getBinary());
 
+    bool (*SupportsRelocation)(uint64_t);
+    object::RelocationResolver Resolver;
+    std::tie(SupportsRelocation, Resolver) =
+        object::getRelocationResolver(*ObjFile.getBinary());
+
     for (const object::SectionRef &Section : Sections) {
       for (const object::RelocationRef &Reloc : Section.relocations()) {
-        if (Reloc.getType() != RelativeRelocation)
-          continue;
-        if (auto AddendOrErr = object::ELFRelocationRef(Reloc).getAddend())
-          Relocs.insert({Reloc.getOffset(), *AddendOrErr});
+        if (SupportsRelocation && SupportsRelocation(Reloc.getType())) {
+          auto AddendOrErr = object::ELFRelocationRef(Reloc).getAddend();
+          auto A = AddendOrErr ? *AddendOrErr : 0;
+          uint64_t resolved = Resolver(Reloc, Reloc.getSymbol()->getValue(), A);
+          Relocs.insert({Reloc.getOffset(), resolved});
+        } else if (Reloc.getType() == RelativeRelocation) {
+          if (auto AddendOrErr = object::ELFRelocationRef(Reloc).getAddend())
+            Relocs.insert({Reloc.getOffset(), *AddendOrErr});
+        }
       }
     }
   }
