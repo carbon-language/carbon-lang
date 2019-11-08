6b45e1bc11e91ea7b57a6ab1c19461a86dba33f8
Jeremy Morse <jeremy.morse@sony.com>
2019-11-08 11:33:40 +0000

    Revert "[clang] Report sanitizer blacklist as a dependency in cc1"
    This reverts commit 03b84e4f6d0e1c04f22d69cc445f36e1f713beb4.
    
    This breaks dfsan tests with a linking failure, in for example this build:
    
      http://lab.llvm.org:8011/builders/sanitizer-x86_64-linux/builds/24312
    
    Reverting this patch locally makes those tests succeed.

--- clang/include/clang/Driver/SanitizerArgs.h
+++ clang/include/clang/Driver/SanitizerArgs.h
@@ -25,8 +25,8 @@ class SanitizerArgs {
   SanitizerSet RecoverableSanitizers;
   SanitizerSet TrapSanitizers;
 
-  std::vector<std::string> UserBlacklistFiles;
-  std::vector<std::string> SystemBlacklistFiles;
+  std::vector<std::string> BlacklistFiles;
+  std::vector<std::string> ExtraDeps;
   int CoverageFeatures = 0;
   int MsanTrackOrigins = 0;
   bool MsanUseAfterDtor = true;
