99e2cba219aea80b3f11de2aa4e0192b28852de4
Jan Korous <jkorous@apple.com>
2019-11-08 14:17:09 -0800

    Reland "[compiler-rt] Fix tests after 03b84e4f6d0"
    This reverts commit d6be9273c6035c07b25dd1494f76cd61d523b878.

--- compiler-rt/test/asan/TestCases/default_blacklist.cpp
+++ compiler-rt/test/asan/TestCases/default_blacklist.cpp
@@ -4,4 +4,4 @@
 //
 // Test that ASan uses the default blacklist from resource directory.
 // RUN: %clangxx_asan -### %s 2>&1 | FileCheck %s
-// CHECK: fsanitize-blacklist={{.*}}asan_blacklist.txt
+// CHECK: fsanitize-system-blacklist={{.*}}asan_blacklist.txt
