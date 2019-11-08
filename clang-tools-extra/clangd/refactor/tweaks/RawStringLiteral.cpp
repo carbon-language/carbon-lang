e1b07aac3d1c92f58e05cb5c37c0707842da5839
Alex Lorenz <arphaman@gmail.com>
2019-11-08 14:50:26 -0800

    [clangd] NFC, reuse the source manager variable in the RawStringLiteral apply method
    Differential Revision: https://reviews.llvm.org/D69544

--- clang-tools-extra/clangd/refactor/tweaks/RawStringLiteral.cpp
+++ clang-tools-extra/clangd/refactor/tweaks/RawStringLiteral.cpp
@@ -90,8 +90,7 @@ bool RawStringLiteral::prepare(const Selection &Inputs) {
 Expected<Tweak::Effect> RawStringLiteral::apply(const Selection &Inputs) {
   auto &SM = Inputs.AST.getSourceManager();
   auto Reps = tooling::Replacements(
-      tooling::Replacement(Inputs.AST.getSourceManager(), Str,
-                           ("R\"(" + Str->getBytes() + ")\"").str(),
+      tooling::Replacement(SM, Str, ("R\"(" + Str->getBytes() + ")\"").str(),
                            Inputs.AST.getASTContext().getLangOpts()));
   return Effect::mainFileEdit(SM, std::move(Reps));
 }
