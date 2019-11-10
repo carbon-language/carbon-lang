c2751737e58c86a9c90bc8d6246893abcb06c3ca
Craig Topper <craig.topper@gmail.com>
2019-11-09 17:58:56 -0800

    [X86] Handle MO_ConstantPoolIndex in X86AsmPrinter::PrintOperand
    Fixes PR43952

--- llvm/lib/Target/X86/X86AsmPrinter.cpp
+++ llvm/lib/Target/X86/X86AsmPrinter.cpp
@@ -218,6 +218,7 @@ void X86AsmPrinter::PrintOperand(const MachineInstr *MI, unsigned OpNo,
     O << MO.getImm();
     return;
 
+  case MachineOperand::MO_ConstantPoolIndex:
   case MachineOperand::MO_GlobalAddress: {
     if (IsATT)
       O << '$';
