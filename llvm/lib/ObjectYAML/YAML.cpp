6b15c5dface2c70c9a17f80d3aa95af2c7658a7d
Georgii Rymar <grimar@accesssoftek.com>
2019-11-11 12:11:54 +0300

    [FixBB] - Fix one more std::min -> std::min<uint64_t> to make BB happy.
    BB: http://lab.llvm.org:8011/builders/clang-armv7-linux-build-cache/builds/22133/steps/build%20stage%201/logs/stdio

--- llvm/lib/ObjectYAML/YAML.cpp
+++ llvm/lib/ObjectYAML/YAML.cpp
@@ -39,7 +39,7 @@ StringRef yaml::ScalarTraits<yaml::BinaryRef>::input(StringRef Scalar, void *,
 
 void yaml::BinaryRef::writeAsBinary(raw_ostream &OS, uint64_t N) const {
   if (!DataIsHexString) {
-    OS.write((const char *)Data.data(), std::min(N, Data.size()));
+    OS.write((const char *)Data.data(), std::min<uint64_t>(N, Data.size()));
     return;
   }
 
