1d55c9e59ebf3f3ff572d42da433b2f72f1ce900
David Zarzycki <dave@znu.io>
2019-11-13 10:55:19 +0200

    [libcxx testing] Fix -Wtautological-overlap-compare bug

--- libcxx/test/support/container_debug_tests.h
+++ libcxx/test/support/container_debug_tests.h
@@ -63,15 +63,15 @@ enum ContainerType {
 };
 
 constexpr bool isSequential(ContainerType CT) {
-  return CT_Vector >= CT && CT_ForwardList <= CT;
+  return CT >= CT_Vector && CT <= CT_ForwardList;
 }
 
 constexpr bool isAssociative(ContainerType CT) {
-  return CT_Map >= CT && CT_MultiSet <= CT;
+  return CT >= CT_Map && CT <= CT_MultiSet;
 }
 
 constexpr bool isUnordered(ContainerType CT) {
-  return CT_UnorderedMap >= CT && CT_UnorderedMultiSet <= CT;
+  return CT >= CT_UnorderedMap && CT <= CT_UnorderedMultiSet;
 }
 
 constexpr bool isSet(ContainerType CT) {
