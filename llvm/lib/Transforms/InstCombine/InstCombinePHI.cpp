a4c76be5068991780342cc5aaac12f8c73050ea1
Francis Visoiu Mistrih <francisvm@yahoo.com>
2019-11-14 17:45:27 -0800

    [InstCombine] Don't use getFirstNonPHI in FoldIntegerTypedPHI
    getFirstNonPHI iterates over all the instructions in a block until it
    finds a non-PHI.
    
    Then, the loop starts from the beginning of the block and goes through
    all the instructions until it reaches the instruction found by
    getFirstNonPHI.
    
    Instead of doing that, just stop when a non-PHI is found.
    
    This reduces the compile-time of a test case discussed in
    https://reviews.llvm.org/D47023 by 13x.
    
    Not entirely sure how to come up with a test case for this since it's a
    compile time issue that would significantly slow down running the tests.
    
    Differential Revision: https://reviews.llvm.org/D70016

--- llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombinePHI.cpp
@@ -181,13 +181,14 @@ Instruction *InstCombiner::FoldIntegerTypedPHI(PHINode &PN) {
          "Not enough available ptr typed incoming values");
   PHINode *MatchingPtrPHI = nullptr;
   unsigned NumPhis = 0;
-  for (auto II = BB->begin(), EI = BasicBlock::iterator(BB->getFirstNonPHI());
-       II != EI; II++, NumPhis++) {
+  for (auto II = BB->begin(); II != BB->end(); II++, NumPhis++) {
     // FIXME: consider handling this in AggressiveInstCombine
+    PHINode *PtrPHI = dyn_cast<PHINode>(II);
+    if (!PtrPHI)
+      break;
     if (NumPhis > MaxNumPhis)
       return nullptr;
-    PHINode *PtrPHI = dyn_cast<PHINode>(II);
-    if (!PtrPHI || PtrPHI == &PN || PtrPHI->getType() != IntToPtr->getType())
+    if (PtrPHI == &PN || PtrPHI->getType() != IntToPtr->getType())
       continue;
     MatchingPtrPHI = PtrPHI;
     for (unsigned i = 0; i != PtrPHI->getNumIncomingValues(); ++i) {
