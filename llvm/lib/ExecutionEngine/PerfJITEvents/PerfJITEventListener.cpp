114f3e5b08afe9a9034d1a09aa9a880afeed9091
Sylvestre Ledru <sylvestre@debian.org>
2019-11-16 14:39:59 +0100

    Fix a build failure with perf: Add a missing include to llvm/Support/ManagedStatic.h
    It was failing with
    PerfJITEventListener.cpp:489:7: error: 'ManagedStatic' in namespace 'llvm' does not name a template type
     llvm::ManagedStatic<PerfJITEventListener> PerfListener;

--- llvm/lib/ExecutionEngine/PerfJITEvents/PerfJITEventListener.cpp
+++ llvm/lib/ExecutionEngine/PerfJITEvents/PerfJITEventListener.cpp
@@ -24,6 +24,7 @@
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/Errno.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Mutex.h"
 #include "llvm/Support/Path.h"
