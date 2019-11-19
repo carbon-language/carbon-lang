d044dcc5e492181b1517347013a780d9eda6c3c3
Puyan Lotfi <puyan@puyan.org>
2019-11-19 02:08:22 -0500

    Revert "[clang][IFS] Driver pipeline: generate interface stubs after standard pipeline."
    This reverts commit 58ea00b51fe9b011301484957556872fced7dd08.
    
    Test for .o + .ifs sidecar files is brittle and failing on bots.
    Reverting to unblock.

--- clang/test/InterfaceStubs/windows.cpp
+++ clang/test/InterfaceStubs/windows.cpp
@@ -1,7 +1,6 @@
 // REQUIRES: x86-registered-target
 // RUN: %clang_cc1 -triple x86_64-windows-msvc -o - %s -emit-interface-stubs | FileCheck -check-prefix=CHECK-CC1 %s
-// RUN: %clang -target x86_64-windows-msvc -o - %s -emit-interface-stubs -emit-merged-ifs -S | FileCheck -check-prefix=CHECK-IFS %s
-// note: -S is added here to prevent clang from invoking link.exe
+// RUN: %clang -target x86_64-windows-msvc -o - %s -emit-interface-stubs -emit-merged-ifs | FileCheck -check-prefix=CHECK-IFS %s
 
 // CHECK-CC1: Symbols:
 // CHECK-CC1-NEXT: ?helloWindowsMsvc@@YAHXZ
