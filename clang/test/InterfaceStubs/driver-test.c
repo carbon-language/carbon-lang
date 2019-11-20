27b229dc17b2ea1d06fe566df8631bb2fff7b1c8
Puyan Lotfi <puyan@puyan.org>
2019-11-19 21:59:10 -0500

    Revert "[clang][IFS][test] Removing driver-test.c. Test is still too brittle."
    This reverts commit f37356d6f60ae5db978611621d3a375ed87ec0f0.

---
+++ clang/test/InterfaceStubs/driver-test.c
@@ -0,0 +1,15 @@
+// REQUIRES: x86-registered-target
+// REQUIRES: !powerpc-registered-target
+// REQUIRES: !system-darwin && !system-windows
+
+// RUN: %clang -o %t1 -target x86_64-unknown-linux-gnu \
+// RUN:   -emit-interface-stubs -emit-merged-ifs %s %S/object.c %S/weak.cpp
+// RUN: cat %t1.ifs | FileCheck %s
+
+// CHECK-DAG: data
+// CHECK-DAG: foo
+// CHECK-DAG: strongFunc
+// CHECK-DAG: weakFunc
+
+int foo(int bar) { return 42 + 1844; }
+int main() { return foo(23); }
