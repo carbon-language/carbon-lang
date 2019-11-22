b6ae524cd2d5139748e207d4b60b4a900775d6a7
Jonas Devlieghere <jonas@devlieghere.com>
2019-11-22 15:43:39 -0800

    [Examples] Move structured-data unpacking out of the loop. (NFC)
    There's no need to repeat this work in the loop.

--- lldb/examples/python/in_call_stack.py
+++ lldb/examples/python/in_call_stack.py
@@ -9,10 +9,10 @@ def __lldb_init_module(debugger, internal_dict):
 
 def in_call_stack(frame, bp_loc, arg_dict, _):
   """Only break if the given name is in the current call stack."""
+  name = arg_dict.GetValueForKey('name').GetStringValue(1000)
   thread = frame.GetThread()
   found = False
   for frame in thread.frames:
-    name = arg_dict.GetValueForKey('name').GetStringValue(1000)
     # Check the symbol.
     symbol = frame.GetSymbol()
     if symbol and name in frame.GetSymbol().GetName():
