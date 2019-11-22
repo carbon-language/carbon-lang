8ca7871addc53a8ddb356ae8b99fa6f7e0e8dd15
Evgenii Stepanov <eugenis@google.com>
2019-11-22 10:58:26 -0800

    Reinstate MSan suppression of PR24578.
    Summary:
    Revert "Rollback of commit "Repress sanitization on User dtor.""
    
    There is no point in keeping an active MSan error in the codebase.
    PR24578 tracks the actual UB in LLVM code; this change enables testing
    of LLVM with MSAN + -fsanitize-memory-use-after-dtor.
    
    This reverts commit 21c1bc46aee2b69c2c48db8e961f0ce8394f21e1.
    
    Reviewers: vitalybuka
    
    Subscribers: hiraditya, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D70611

--- llvm/lib/IR/Metadata.cpp
+++ llvm/lib/IR/Metadata.cpp
@@ -489,7 +489,9 @@ void *MDNode::operator new(size_t Size, unsigned NumOps) {
   return Ptr;
 }
 
-void MDNode::operator delete(void *Mem) {
+// Repress memory sanitization, due to use-after-destroy by operator
+// delete. Bug report 24578 identifies this issue.
+LLVM_NO_SANITIZE_MEMORY_ATTRIBUTE void MDNode::operator delete(void *Mem) {
   MDNode *N = static_cast<MDNode *>(Mem);
   size_t OpSize = N->NumOperands * sizeof(MDOperand);
   OpSize = alignTo(OpSize, alignof(uint64_t));
