717140a0dcc651ca2fec23248d1675fb2d388b9c
Thomas Preud'homme <thomasp@graphcore.ai>
2019-11-26 22:40:18 +0000

    [clang test] Do not assume default target
    Summary:
    clang test Driver/darwin-opt-record.c assumes the default target is
    x86_64 by its uses of the -arch x86_64 and -arch x86_64h and thus fail
    on systems where it is not the case. Adding a target
    x86_64-apple-darwin10 reveals another problem: the driver refuses 2
    -arch for an assembly output so this commit also changes the output to
    be an object file.
    
    Reviewers: thegameg
    
    Reviewed By: thegameg
    
    Subscribers: cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D70748

--- clang/test/Driver/darwin-opt-record.c
+++ clang/test/Driver/darwin-opt-record.c
@@ -1,6 +1,6 @@
 // REQUIRES: system-darwin
 
-// RUN: %clang -### -S -o FOO -fsave-optimization-record -arch x86_64 -arch x86_64h %s 2>&1 | FileCheck %s --check-prefix=CHECK-MULTIPLE-ARCH
+// RUN: %clang -target x86_64-apple-darwin10 -### -c -o FOO -fsave-optimization-record -arch x86_64 -arch x86_64h %s 2>&1 | FileCheck %s --check-prefix=CHECK-MULTIPLE-ARCH
 //
 // CHECK-MULTIPLE-ARCH: "-cc1"
 // CHECK-MULTIPLE-ARCH: "-opt-record-file" "FOO-x86_64.opt.yaml"
