f1b117394d7f9ae6decf9730ed9d443ca1b54769
Raphael Isemann <teemperor@gmail.com>
2019-11-27 09:46:56 +0100

    [lldb][NFC] Remove unused CompilerType memory functions
    Summary:
    All these functions are unused from what I can see. Unless I'm missing something here, this code
    can go the way of the Dodo.
    
    Reviewers: labath
    
    Reviewed By: labath
    
    Subscribers: abidh, JDevlieghere, lldb-commits
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D70770

--- lldb/include/lldb/Symbol/CompilerType.h
+++ lldb/include/lldb/Symbol/CompilerType.h
@@ -357,14 +357,6 @@ public:
   bool GetValueAsScalar(const DataExtractor &data, lldb::offset_t data_offset,
                         size_t data_byte_size, Scalar &value) const;
 
-  bool SetValueFromScalar(const Scalar &value, Stream &strm);
-
-  bool ReadFromMemory(ExecutionContext *exe_ctx, lldb::addr_t addr,
-                      AddressType address_type, DataExtractor &data);
-
-  bool WriteToMemory(ExecutionContext *exe_ctx, lldb::addr_t addr,
-                     AddressType address_type, StreamString &new_value);
-
   void Clear() {
     m_type = nullptr;
     m_type_system = nullptr;
