536cedaecbe586ec9cf86d5102872adc27e6ea23
Reid Kleckner <rnk@google.com>
2019-12-02 15:03:31 -0800

    FileCheck IR output for blockaddress in new test
    Minor improvement to a test added in 1ac700cdef787383ad49a

--- clang/test/CodeGen/label-array-aggregate-init.c
+++ clang/test/CodeGen/label-array-aggregate-init.c
@@ -1,6 +1,10 @@
-// RUN: %clang -cc1 -emit-llvm %s -o /dev/null
+// RUN: %clang -cc1 -triple x86_64-windows-msvc -emit-llvm %s -o - | FileCheck %s
+
+// CHECK: @constinit = private global [3 x i8*] [i8* blockaddress(@main, %L), i8* null, i8* null]
+
+void receivePtrs(void **);
 
 int main() {
 L:
-  (void)(void *[]){ &&L, 0, 0 };
+  receivePtrs((void *[]){ &&L, 0, 0 });
 }
