8b0780f795eb58fca0a2456e308adaaa1a0b5013
Tozer <stephen.tozer@sony.com>
2019-12-04 18:47:08 +0000

    Revert "[DebugInfo] Recover debug intrinsics when killing duplicated/empty basic blocks"
    This reverts commit 72ce759928e6dfee6a9efa310b966c19722352ba.
    
    Reverted due to build failure.

--- llvm/include/llvm/Transforms/Utils/Local.h
+++ llvm/include/llvm/Transforms/Utils/Local.h
@@ -178,8 +178,6 @@ bool SimplifyInstructionsInBlock(BasicBlock *BB,
 /// Returns true if the dbg values have been changed.
 bool replaceDbgUsesWithUndef(Instruction *I);
 
-void setDbgVariableUndef(DbgVariableIntrinsic *DVI);
-
 //===----------------------------------------------------------------------===//
 //  Control Flow Graph Restructuring.
 //
