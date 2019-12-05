8ddcd1dc26bad9d97bbf34cf4fc91dbf3c153431
Russell Gallop <russell.gallop@sony.com>
2019-12-05 09:43:49 +0000

    [Support] Extend TimeProfiler to support multiple threads
    This makes TimeTraceProfilerInstance thread local. Added
    timeTraceProfilerFinishThread() which moves the thread local instance to
    a global vector of instances. timeTraceProfilerWrite() then writes
    recorded data from all instances.
    
    Threads are identified based on their thread ids. Totals are reported
    with artificial thread ids higher than the real ones.
    
    Replaced raw pointer for TimeTraceProfilerInstance with unique_ptr.
    
    Differential Revision: https://reviews.llvm.org/D71059

--- llvm/include/llvm/Support/TimeProfiler.h
+++ llvm/include/llvm/Support/TimeProfiler.h
@@ -14,7 +14,8 @@
 namespace llvm {
 
 struct TimeTraceProfiler;
-extern TimeTraceProfiler *TimeTraceProfilerInstance;
+extern thread_local std::unique_ptr<TimeTraceProfiler>
+    TimeTraceProfilerInstance;
 
 /// Initialize the time trace profiler.
 /// This sets up the global \p TimeTraceProfilerInstance
@@ -24,6 +25,9 @@ void timeTraceProfilerInitialize(unsigned TimeTraceGranularity,
 
 /// Cleanup the time trace profiler, if it was initialized.
 void timeTraceProfilerCleanup();
+
+/// Finish a time trace profiler running on a worker thread.
+void timeTraceProfilerFinishThread();
 
 /// Is the time trace profiler enabled, i.e. initialized?
 inline bool timeTraceProfilerEnabled() {
