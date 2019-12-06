757bc55f8314c8c789f81b7f0b3397a8c6148b68
Douglas Yung <douglas.yung@sony.com>
2019-12-05 18:35:08 -0800

    Tighten up CHECK lines added in a9f10ebffa to work on ARM.
    On ARM platforms, the compiler generates an additional line containing `-CXXRecordDecl which is not the intended line, but preceeds the intended match causing the test to fail.

--- clang/test/Import/struct-and-var/test.cpp
+++ clang/test/Import/struct-and-var/test.cpp
@@ -1,6 +1,6 @@
 // RUN: clang-import-test -dump-ast --import %S/Inputs/S1.cpp --import %S/Inputs/S2.cpp -expression %s | FileCheck %s
 
-// CHECK: `-CXXRecordDecl
+// CHECK: {{^`-CXXRecordDecl}}
 // CHECK-SAME: Inputs/S2.cpp:1:1, line:3:1> line:1:8 struct F
 
 void expr() {
