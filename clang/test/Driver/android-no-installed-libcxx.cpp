b32490270b786d2c5ba154e613ee2d5e36ed4197
David Zarzycki <dave@znu.io>
2019-12-08 16:41:46 +0200

    Revert "Driver: Don't look for libc++ headers in the install directory on Android."
    This reverts commit 198fbcb817492ff45946e3f7517de15e8cdf0607.
    
    This breaks Fedora 31.

--- clang/test/Driver/android-no-installed-libcxx.cpp
+++
@@ -1,8 +0,0 @@
-// Check that we don't find the libc++ in the installation directory when
-// targeting Android.
-
-// RUN: mkdir -p %t/bin
-// RUN: mkdir -p %t/include/c++/v1
-// RUN: %clang -target aarch64-linux-android -ccc-install-dir %t/bin \
-// RUN:   -stdlib=libc++ -fsyntax-only %s -### 2>&1 | FileCheck %s
-// CHECK-NOT: "-internal-isystem" "{{.*}}v1"
