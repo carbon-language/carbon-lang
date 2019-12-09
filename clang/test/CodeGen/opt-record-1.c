27f5d35137cb45e60d9988a9b55875883c55023c
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2019-12-09 00:50:30 -0500

    Fix for build bot failure. For more details see: https://reviews.llvm.org/D70691 Upated LIT test.

--- clang/test/CodeGen/opt-record-1.c
+++ clang/test/CodeGen/opt-record-1.c
@@ -1,12 +1,12 @@
-// RUN: %clang_cc1 %s -opt-record-file=t1.opt -fopenmp -emit-llvm-bc -o %t.bc
-// RUN: %clang_cc1 -x ir %t.bc -opt-record-file %t.opt -fopenmp -emit-obj
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -target-cpu x86-64  %s -O3 -opt-record-file=t1.opt -fopenmp -emit-llvm-bc -o %t.bc
+// RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -target-cpu x86-64 -O3 -x ir %t.bc -opt-record-file %t.opt -fopenmp -emit-obj
 // RUN: cat %t.opt |  FileCheck -check-prefix=CHECK  %s
 
 void foo(int *a, int *b, int *c) {
 #pragma omp parallel for
- for (int i = 0; i < 100; i++) {
- a[i] = b[i] + c[i];
- }
+  for (int i = 0; i < 100; i++) {
+    a[i] = b[i] + c[i];
+  }
 }
 
 // CHECK: --- !Missed
