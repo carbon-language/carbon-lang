32c802e0f539511f8535aaa1309cd06be86c2398
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2019-12-09 09:24:47 -0500

    Fix build bot fails due to the patch here: https://reviews.llvm.org/D70691 Fixed the LIT test case. Added the REQUIRES instruction.

--- clang/test/CodeGen/opt-record-1.c
+++ clang/test/CodeGen/opt-record-1.c
@@ -1,6 +1,7 @@
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -target-cpu x86-64  %s -O3 -opt-record-file=t1.opt -fopenmp -emit-llvm-bc -o %t.bc
 // RUN: %clang_cc1 -triple x86_64-unknown-linux-gnu -target-cpu x86-64 -O3 -x ir %t.bc -opt-record-file %t.opt -fopenmp -emit-obj
 // RUN: cat %t.opt |  FileCheck -check-prefix=CHECK  %s
+// REQUIRES: x86-registered-target
 
 void foo(int *a, int *b, int *c) {
 #pragma omp parallel for
