46d970cc436068af826e5f6a59033dd0f10c570d
Vedant Kumar <vsk@apple.com>
2019-12-12 11:08:56 -0800

    TypeCategory: Initialize m_enabled to false
    The initialization was accidentally lost in https://reviews.llvm.org/D71310, causing a ubsan failure:
    
    /Users/buildslave/jenkins/workspace/lldb-cmake-sanitized/llvm-project/lldb/include/lldb/DataFormatters/TypeCategory.h:278:35: runtime error: load of value 190, which is not a valid value for type 'bool'
    SUMMARY: UndefinedBehaviorSanitizer: undefined-behavior /Users/buildslave/jenkins/workspace/lldb-cmake-sanitized/llvm-project/lldb/include/lldb/DataFormatters/TypeCategory.h:278:35 in
    
    http://lab.llvm.org:8080/green/view/LLDB/job/lldb-cmake-sanitized/621/consoleText

--- lldb/source/DataFormatters/TypeCategory.cpp
+++ lldb/source/DataFormatters/TypeCategory.cpp
@@ -18,7 +18,7 @@ TypeCategoryImpl::TypeCategoryImpl(IFormatChangeListener *clist,
     : m_format_cont("format", "regex-format", clist),
       m_summary_cont("summary", "regex-summary", clist),
       m_filter_cont("filter", "regex-filter", clist),
-      m_synth_cont("synth", "regex-synth", clist),
+      m_synth_cont("synth", "regex-synth", clist), m_enabled(false),
       m_change_listener(clist), m_mutex(), m_name(name), m_languages() {}
 
 static bool IsApplicable(lldb::LanguageType category_lang,
