2bbd32f5e8f0f62d895966e2623d9bdb9778b50b
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2019-12-13 12:19:51 +0000

    [ELF] Allow getErrPlace() to work before Out::bufferStart is set
    Summary:
    So far it seems like the only test affected by this change is the one I
    recently added for R_MIPS_JALR relocations since the other test cases that
    use this function early (unknown-relocation-*) do not have a valid input
    section for the relocation offset.
    
    Reviewers: ruiu, grimar, MaskRay, espindola
    
    Reviewed By: ruiu, MaskRay
    
    Subscribers: emaste, sdardis, jrtc27, atanasyan, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D70659

--- lld/ELF/Target.cpp
+++ lld/ELF/Target.cpp
@@ -91,15 +91,15 @@ TargetInfo *getTarget() {
 }
 
 template <class ELFT> static ErrorPlace getErrPlace(const uint8_t *loc) {
-  if (!Out::bufferStart)
-    return {};
-
   for (InputSectionBase *d : inputSections) {
     auto *isec = cast<InputSection>(d);
     if (!isec->getParent())
       continue;
 
-    uint8_t *isecLoc = Out::bufferStart + isec->getParent()->offset + isec->outSecOff;
+    const uint8_t *isecLoc =
+        Out::bufferStart
+            ? (Out::bufferStart + isec->getParent()->offset + isec->outSecOff)
+            : isec->data().data();
     if (isecLoc <= loc && loc < isecLoc + isec->getSize())
       return {isec, isec->template getLocation<ELFT>(loc - isecLoc) + ": "};
   }
