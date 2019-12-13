cdb45605571202a1bbc3fe6de300b93da79cea83
Julian Lettner <julian.lettner@apple.com>
2019-12-12 17:28:54 -0800

    [iOS sim] Simplify iossim_run.py script

--- compiler-rt/test/sanitizer_common/ios_commands/iossim_run.py
+++ compiler-rt/test/sanitizer_common/ios_commands/iossim_run.py
@@ -3,10 +3,9 @@
 import glob, os, pipes, sys, subprocess
 
 
-if not "SANITIZER_IOSSIM_TEST_DEVICE_IDENTIFIER" in os.environ:
+device_id = os.environ.get('SANITIZER_IOSSIM_TEST_DEVICE_IDENTIFIER')
+if not device_id:
   raise EnvironmentError("Specify SANITIZER_IOSSIM_TEST_DEVICE_IDENTIFIER to select which simulator to use.")
-
-device_id = os.environ["SANITIZER_IOSSIM_TEST_DEVICE_IDENTIFIER"]
 
 for e in ["ASAN_OPTIONS", "TSAN_OPTIONS", "UBSAN_OPTIONS", "APPLE_ASAN_INIT_FOR_DLOPEN", "ASAN_ACTIVATION_OPTIONS"]:
   if e in os.environ:
