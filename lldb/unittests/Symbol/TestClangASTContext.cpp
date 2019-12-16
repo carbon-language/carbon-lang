959ed0e2944c454a3df3aa3bc8ab551c8b587e9b
Raphael Isemann <teemperor@gmail.com>
2019-12-16 09:34:16 +0100

    [lldb][NFC] Fix file header of TestClangASTContext.cpp

--- lldb/unittests/Symbol/TestClangASTContext.cpp
+++ lldb/unittests/Symbol/TestClangASTContext.cpp
@@ -1,6 +1,4 @@
-//===-- TestClangASTContext.cpp ---------------------------------------*- C++
-//-*-===//
-
+//===-- TestClangASTContext.cpp ---------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
