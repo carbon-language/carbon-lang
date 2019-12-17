18188a7f44506892a2f4be4100d66971f18000ec
Georgii Rymar <grimar@accesssoftek.com>
2019-12-17 13:54:29 +0300

    [llvm-readobj][test] - Improve dyn-symbols.test.
    This removes the precompiled binary used, simplifies
    the first test case, adds comments and llvm-readelf tool
    invocations.
    It also adds a test case for checking versioning symbols.
    
    Differential revision: https://reviews.llvm.org/D71595

--- llvm/test/tools/llvm-readobj/ELF/Inputs/dynamic-table.c
+++ llvm/test/tools/llvm-readobj/ELF/Inputs/dynamic-table.c
@@ -1,5 +1,3 @@
-// clang -target x86_64-linux-gnu -shared -fPIC -lc dynamic-table.c \
-//       -o dynamic-table-so.x86 -Wl,-f,aux.so -Wl,-F,filter.so
 // clang -target mipsel-linux-gnu -shared -fPIC -lc dynamic-table.c \
 //       -o dynamic-table-so.mips
 // clang -target mipsel-linux-gnu -lc dynamic-table.c \
