be88a20c900463c4919433109e4c17cd001da580
Alex Lorenz <arphaman@gmail.com>
2019-12-17 14:24:23 -0800

    [driver][darwin] Use explicit -mlinker-version in the -platform_version tests
    The driver actually adds a default -mlinker-version, based on HOST_LINK_VERSION
    cmake variable. The tests should be explicit about which version they're using to
    trigger the right behavior.

--- clang/test/Driver/darwin-ld-platform-version-tvos.c
+++ clang/test/Driver/darwin-ld-platform-version-tvos.c
@@ -1,8 +1,8 @@
 // RUN: touch %t.o
 
-// RUN: %clang -target arm64-apple-tvos12.3 -isysroot %S/Inputs/iPhoneOS13.0.sdk -### %t.o 2>&1 \
+// RUN: %clang -target arm64-apple-tvos12.3 -isysroot %S/Inputs/iPhoneOS13.0.sdk -mlinker-version=520 -### %t.o 2>&1 \
 // RUN:   | FileCheck %s
-// RUN: %clang -target x86_64-apple-tvos13-simulator -isysroot %S/Inputs/iPhoneOS13.0.sdk -### %t.o 2>&1 \
+// RUN: %clang -target x86_64-apple-tvos13-simulator -isysroot %S/Inputs/iPhoneOS13.0.sdk -mlinker-version=520 -### %t.o 2>&1 \
 // RUN:   | FileCheck --check-prefix=SIMUL %s
 
 // CHECK: "-platform_version" "tvos" "12.3.0" "13.0"
