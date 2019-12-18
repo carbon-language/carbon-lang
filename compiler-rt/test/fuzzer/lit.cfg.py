d372576d8a3baeff180e3a2fada3220215204ed1
Michał Górny <mgorny@gentoo.org>
2019-12-18 16:51:31 +0100

    [compiler-rt] [test] Disable ASLR for fuzzer tests on NetBSD

--- compiler-rt/test/fuzzer/lit.cfg.py
+++ compiler-rt/test/fuzzer/lit.cfg.py
@@ -116,3 +116,6 @@ config.substitutions.append(('%env_asan_opts=',
 
 if not config.parallelism_group:
   config.parallelism_group = 'shadow-memory'
+
+if config.host_os == 'NetBSD':
+  config.substitutions.insert(0, ('%run', config.netbsd_noaslr_prefix))
