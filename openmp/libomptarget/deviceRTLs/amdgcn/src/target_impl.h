9aefe5f65ec6e8143f08b8e6f5b7d138d3283562
JonChesterfield <jonathanchesterfield@gmail.com>
2019-12-19 00:11:21 +0000

    [libomptarget][amdgcn] Correct return type of extern __clock64 to unsigned

--- openmp/libomptarget/deviceRTLs/amdgcn/src/target_impl.h
+++ openmp/libomptarget/deviceRTLs/amdgcn/src/target_impl.h
@@ -109,7 +109,7 @@ INLINE uint32_t __kmpc_impl_smid() {
 
 INLINE double __target_impl_get_wtick() { return ((double)1E-9); }
 
-EXTERN int64_t __clock64();
+EXTERN uint64_t __clock64();
 INLINE double __target_impl_get_wtime() {
   return ((double)1.0 / 745000000.0) * __clock64();
 }
