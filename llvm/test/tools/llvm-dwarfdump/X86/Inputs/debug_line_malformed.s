f6f0cb4fd1a1bddbee82c2c02f9aae5a0e8b821a
James Henderson <jh7370@my.bristol.ac.uk>
2019-12-20 12:30:23 +0000

    [test][llvm-dwarfdump] Normalise contents and checks for line tables
    The line tables in debug_line_malformed.s had contents that varied more
    than was necessary for the testing, making it harder to follow what was
    important. This patch normalises them so that they all share
    more-or-less the same body. Additionally, it makes the testing for what
    was printed more consistent, to show that the right parts of the line
    table prologue and body are/are not parsed and printed.
    
    Reviewed By: JDevlieghere
    
    Differential Revision: https://reviews.llvm.org/D71755

--- llvm/test/tools/llvm-dwarfdump/X86/Inputs/debug_line_malformed.s
+++ llvm/test/tools/llvm-dwarfdump/X86/Inputs/debug_line_malformed.s
@@ -55,6 +55,9 @@
 .byte   0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1 # Standard Opcode Lengths
 .byte   0               # directory table (invalid as no path component)
 .Lprologue_v5_end:
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0x8877665544332211
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Lunit_v5_end:
 
 # Short prologue.
@@ -76,9 +79,12 @@
 .asciz "file1"          # File table
 .byte   0, 0, 0
 .asciz "file2"
-.byte   1, 0, 0
+.byte   1, 2, 3
 .byte   0
 .Lprologue_short_prologue_end:
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0x1122334455667788
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Lunit_short_prologue_end:
 
 # Over-long prologue.
@@ -100,9 +106,12 @@
 .asciz "file1"          # File table
 .byte   0, 0, 0
 .asciz "file2"
-.byte   1, 0, 0
+.byte   1, 2, 3
 .byte   0
 .Lprologue_long_prologue_end:
+.byte   0, 9, 2        # DW_LNE_set_address
+.quad   0x1111222233334444
+.byte   0, 1, 1        # DW_LNE_end_sequence
 .Lunit_long_prologue_end:
 
 # Over-long extended opcode.
@@ -193,12 +202,11 @@
 # File table entries
 .byte   1               # 1 file
 .asciz  "a.c"
-.byte   0
+.byte   1
 .Linvalid_description_header_end0:
-.byte   0,2,4,1         # DW_LNE_set_discriminator 1
-.byte   1               # DW_LNS_copy
-.byte   33              # address += 1, line += 1
-.byte   0,1,1           # DW_LNE_end_sequence
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0xbabb1ebabb1e
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Linvalid_description_end0:
 
 # V5 prologue ends during file table.
@@ -232,12 +240,11 @@
 # File table entries
 .byte   1               # 1 file
 .asciz  "a.c"
-.byte   0
+.byte   1
 .Linvalid_file_header_end0:
-.byte   0,2,4,1         # DW_LNE_set_discriminator 1
-.byte   1               # DW_LNS_copy
-.byte   33              # address += 1, line += 1
-.byte   0,1,1           # DW_LNE_end_sequence
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0xab4acadab4a
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Linvalid_file_end0:
 
 # V5 prologue ends during directory table.
@@ -271,12 +278,11 @@
 # File table entries
 .byte   1               # 1 file
 .asciz  "a.c"
-.byte   0
+.byte   1
 .Linvalid_dir_header_end0:
-.byte   0,2,4,1         # DW_LNE_set_discriminator 1
-.byte   1               # DW_LNS_copy
-.byte   33              # address += 1, line += 1
-.byte   0,1,1           # DW_LNE_end_sequence
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0x4444333322221111
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Linvalid_dir_end0:
 
 # Invalid MD5 hash.
@@ -314,10 +320,9 @@
 .asciz  "a.c"
 .byte   0
 .Linvalid_md5_header_end0:
-.byte   0,2,4,1         # DW_LNE_set_discriminator 1
-.byte   1               # DW_LNS_copy
-.byte   33              # address += 1, line += 1
-.byte   0,1,1           # DW_LNE_end_sequence
+.byte   0, 9, 2         # DW_LNE_set_address
+.quad   0x1234123412341234
+.byte   0, 1, 1         # DW_LNE_end_sequence
 .Linvalid_md5_end0:
 
 # Trailing good section.
