810c3cfa664b38b27bc30afaadab3d775cb17922
Jim Ingham <jingham@apple.com>
2019-12-20 10:56:39 -0800

    ThreadPlanTracer::TracingStarted can't call virtual methods on Thread.
        TracingStarted gets called in the Thread constructor, which means you can't
        call a virtual method of the class.  So delay setting up the m_register_values
        till you need them.  NFC as lldb just crashes if you don't do this.
    
        The thread tracing is an only occasionally useful feature, and it only sort
        of works.  I'm not adding tests etc. at this point, I'm just poking at it a
        bit.  If I get it working better I'll write tests and so forth.

--- lldb/source/Target/ThreadPlanTracer.cpp
+++ lldb/source/Target/ThreadPlanTracer.cpp
@@ -115,10 +115,6 @@ TypeFromUser ThreadPlanAssemblyTracer::GetIntPointerType() {
 ThreadPlanAssemblyTracer::~ThreadPlanAssemblyTracer() = default;
 
 void ThreadPlanAssemblyTracer::TracingStarted() {
-  RegisterContext *reg_ctx = m_thread.GetRegisterContext().get();
-
-  if (m_register_values.empty())
-    m_register_values.resize(reg_ctx->GetRegisterCount());
 }
 
 void ThreadPlanAssemblyTracer::TracingEnded() { m_register_values.clear(); }
@@ -206,6 +202,11 @@ void ThreadPlanAssemblyTracer::Log() {
           stream->PutCString(", ");
       }
     }
+  }
+
+  if (m_register_values.empty()) {
+    RegisterContext *reg_ctx = m_thread.GetRegisterContext().get();
+    m_register_values.resize(reg_ctx->GetRegisterCount());
   }
 
   RegisterValue reg_value;
