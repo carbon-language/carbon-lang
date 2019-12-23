83d574bf079bff222763204356d20d86226c8887
Tim Keith <tkeith@nvidia.com>
2019-12-23 10:40:20 -0800

    [flang] Move forall01 to ERROR_TESTS
    That seems to be what was intended and it provides for better checking
    of errors.
    
    Original-commit: flang-compiler/f18@99376d77a07aa04173bd20dcc45d08b76cae4a73
    Reviewed-on: https://github.com/flang-compiler/f18/pull/904
    Tree-same-pre-rewrite: false

--- flang/test/semantics/forall01.f90
+++ flang/test/semantics/forall01.f90
@@ -1,13 +1,9 @@
-! RUN: ${F18} -funparse-with-symbols %s 2>&1 | ${FileCheck} %s
-! CHECK: Previous declaration of 'i'
-! CHECK: Previous declaration of 'j'
-
 subroutine forall
   real :: a(9)
-! ERROR: 'i' is already declared in this scoping unit
+  !ERROR: 'i' is already declared in this scoping unit
   forall (i=1:8, i=1:9)  a(i) = i
   forall (j=1:8)
-! ERROR: 'j' is already declared in this scoping unit
+  !ERROR: 'j' is already declared in this scoping unit
     forall (j=1:9)
     end forall
   end forall
