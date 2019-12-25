583ef13f0be7b0045f9508dfceee17ab4ef4b1ea
Fangrui Song <maskray@google.com>
2019-12-24 17:10:32 -0800

    [MCJIT] Migrate function attribute "no-frame-pointer-elim" to "frame-pointer"

--- llvm/lib/ExecutionEngine/ExecutionEngineBindings.cpp
+++ llvm/lib/ExecutionEngine/ExecutionEngineBindings.cpp
@@ -183,13 +183,13 @@ LLVMBool LLVMCreateMCJITCompilerForModule(
   std::unique_ptr<Module> Mod(unwrap(M));
 
   if (Mod)
-    // Set function attribute "no-frame-pointer-elim" based on
+    // Set function attribute "frame-pointer" based on
     // NoFramePointerElim.
     for (auto &F : *Mod) {
       auto Attrs = F.getAttributes();
-      StringRef Value(options.NoFramePointerElim ? "true" : "false");
+      StringRef Value = options.NoFramePointerElim ? "all" : "none";
       Attrs = Attrs.addAttribute(F.getContext(), AttributeList::FunctionIndex,
-                                 "no-frame-pointer-elim", Value);
+                                 "frame-pointer", Value);
       F.setAttributes(Attrs);
     }
 
