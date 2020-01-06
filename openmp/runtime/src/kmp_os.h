19433b199d1ccc8798475dbe9084fc66360ec5d5
Kelvin Li <kkwli0@gmail.com>
2020-01-06 15:00:10 -0500

    [OpenMP] Fix incorrect property of __has_attribute() macro
    __has_attribute(fallthough) -> __has_attribute(fallthrough)
    
    Submitted by: kiszk (Kazuaki Ishizaki <ishizaki@jp.ibm.com>)
    
    Differential Revision: https://reviews.llvm.org/D72287

--- openmp/runtime/src/kmp_os.h
+++ openmp/runtime/src/kmp_os.h
@@ -313,7 +313,7 @@ extern "C" {
 #  define KMP_FALLTHROUGH() [[fallthrough]]
 #elif __has_cpp_attribute(clang::fallthrough)
 #  define KMP_FALLTHROUGH() [[clang::fallthrough]]
-#elif __has_attribute(fallthough) || __GNUC__ >= 7
+#elif __has_attribute(fallthrough) || __GNUC__ >= 7
 #  define KMP_FALLTHROUGH() __attribute__((__fallthrough__))
 #else
 #  define KMP_FALLTHROUGH() ((void)0)
