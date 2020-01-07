21f7b362095f7497aa87ccc334fe18bbc072854c
Heejin Ahn <aheejin@gmail.com>
2020-01-06 16:15:53 -0800

    [WebAssembly] Fix landingpad-only case in Emscripten EH
    Summary:
    Previously we didn't set `Changed` to true when there are only landing
    pads but not invokes. This fixes it and we set `Changed` to true
    whenever we have landing pads. (There can't be invokes without landing
    pads, so that case is covered too)
    
    The test case for this has to be a separate file because this pass is a
    `ModulePass` and `Changed` is computed based on the whole module.
    
    Reviewers: tlively
    
    Subscribers: dschuff, sbc100, jgravelle-google, hiraditya, sunfish, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D72308

--- llvm/lib/Target/WebAssembly/WebAssemblyLowerEmscriptenEHSjLj.cpp
+++ llvm/lib/Target/WebAssembly/WebAssemblyLowerEmscriptenEHSjLj.cpp
@@ -751,7 +751,6 @@ bool WebAssemblyLowerEmscriptenEHSjLj::runEHOnFunction(Function &F) {
     auto *II = dyn_cast<InvokeInst>(BB.getTerminator());
     if (!II)
       continue;
-    Changed = true;
     LandingPads.insert(II->getLandingPadInst());
     IRB.SetInsertPoint(II);
 
@@ -831,6 +830,7 @@ bool WebAssemblyLowerEmscriptenEHSjLj::runEHOnFunction(Function &F) {
     if (auto *LPI = dyn_cast<LandingPadInst>(I))
       LandingPads.insert(LPI);
   }
+  Changed = !LandingPads.empty();
 
   // Handle all the landingpad for this function together, as multiple invokes
   // may share a single lp
