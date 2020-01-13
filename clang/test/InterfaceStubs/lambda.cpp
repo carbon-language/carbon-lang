a506f7f9105eec4baac296d21c922457d6f4b52a
Puyan Lotfi <puyan@puyan.org>
2020-01-13 16:48:59 -0500

    [clang][IFS][test] Fixing mangled name of a test for Darwin.
    Darwin adds an extra '_' before every C/global function mangled name and
    because of this, this test was breaking on Darwin.
    
    This is a fix for commit: https://reviews.llvm.org/D71301

--- clang/test/InterfaceStubs/lambda.cpp
+++ clang/test/InterfaceStubs/lambda.cpp
@@ -5,6 +5,6 @@
 // CHECK-NEXT: Triple:
 // CHECK-NEXT: ObjectFileFormat: ELF
 // CHECK-NEXT: Symbols:
-// CHECK-NEXT:   "f" : { Type: Object, Size: 1 }
+// CHECK-NEXT:   f" : { Type: Object, Size: 1 }
 // CHECK-NEXT: ...
 auto f = [](void* data) { int i; };
