b5b2cf7af47f1ca04635dae7b787c8a81d5af4c9
Luboš Luňák <l.lunak@centrum.cz>
2020-01-14 23:46:35 +0100

    fix recent -fmodules-codegen fix test

--- clang/test/Modules/codegen-extern-template.cpp
+++ clang/test/Modules/codegen-extern-template.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -triple=x86_64-linux-gnu -fmodules -fmodules-codegen -emit-module -fmodule-name=foo %S/codegen-extern-template.modulemap -x c++ -o %t.pcm
+// RUN: %clang_cc1 -triple=x86_64-linux-gnu -fmodules -fmodules-codegen -emit-module -fmodule-name=foo %S/Inputs/codegen-extern-template.modulemap -x c++ -o %t.pcm
 // RUN: %clang_cc1 -triple x86_64-linux-gnu -fmodules -fmodule-file=%t.pcm %s -emit-llvm -o - | FileCheck %s
 // expected-no-diagnostics
 
