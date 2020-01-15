c6e69880ae4d9ce4d27bf046292a0a20c3ab3540
Douglas Yung <douglas.yung@sony.com>
2020-01-14 18:58:18 -0800

    Modify test to use -S instead of -c so that it works when an external assembler is used that is not present.

--- clang/test/Driver/cc1-spawnprocess.c
+++ clang/test/Driver/cc1-spawnprocess.c
@@ -1,4 +1,4 @@
-// RUN: env CLANG_SPAWN_CC1= %clang -c %s -o /dev/null
-// RUN: env CLANG_SPAWN_CC1=0 %clang -c %s -o /dev/null
-// RUN: env CLANG_SPAWN_CC1=1 %clang -c %s -o /dev/null
-// RUN: env CLANG_SPAWN_CC1=test not %clang -c %s -o /dev/null
+// RUN: env CLANG_SPAWN_CC1= %clang -S %s -o /dev/null
+// RUN: env CLANG_SPAWN_CC1=0 %clang -S %s -o /dev/null
+// RUN: env CLANG_SPAWN_CC1=1 %clang -S %s -o /dev/null
+// RUN: env CLANG_SPAWN_CC1=test not %clang -S %s -o /dev/null
