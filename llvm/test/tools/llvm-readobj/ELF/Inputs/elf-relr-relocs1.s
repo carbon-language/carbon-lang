66a35d330b3d6d3009b332a41406414c5cbd17d7
Georgii Rymar <grimar@accesssoftek.com>
2020-01-15 16:17:09 +0300

    [llvm-readobj][test] - Cleanup SHT_RELR sections testing.
    After recent changes (D71872) in yaml2obj, it is possible so cleanup
    testing of the SHT_RELR sections.
    
    Differential revision: https://reviews.llvm.org/D71874

--- llvm/test/tools/llvm-readobj/ELF/Inputs/elf-relr-relocs1.s
+++
@@ -1,5 +0,0 @@
-.quad 0x0000000000010d60  // Initial offset
-.quad 0x0000000000000103  // Continuation bitmap
-.quad 0x0000000000020000  // New offset
-.quad 0x00000000000f0501  // Continuation bitmap
-.quad 0x000a700550400009  // Continuation bitmap
--- llvm/test/tools/llvm-readobj/ELF/Inputs/elf-relr-relocs2.s
+++
@@ -1,5 +0,0 @@
-.long 0x00010d60  // Initial offset
-.long 0x00000103  // Continuation bitmap
-.long 0x00020000  // New offset
-.long 0x000f0501  // Continuation bitmap
-.long 0x50400009  // Continuation bitmap
