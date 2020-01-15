afb22d7c33a246967d5f21d5ab3c83c148178564
Igor Kudrin <ikudrin@accesssoftek.com>
2020-01-15 18:17:07 +0700

    [DebugInfo] Simplify the constructor of DWARFDebugAranges::Range. NFC.
    This removes the default values of the arguments. The only caller,
    DWARFDebugAranges::construct(), provides all three parameters.
    
    Differential Revision: https://reviews.llvm.org/D72757

--- llvm/include/llvm/DebugInfo/DWARF/DWARFDebugAranges.h
+++ llvm/include/llvm/DebugInfo/DWARF/DWARFDebugAranges.h
@@ -32,8 +32,7 @@ private:
   void construct();
 
   struct Range {
-    explicit Range(uint64_t LowPC = -1ULL, uint64_t HighPC = -1ULL,
-                   uint64_t CUOffset = -1ULL)
+    explicit Range(uint64_t LowPC, uint64_t HighPC, uint64_t CUOffset)
       : LowPC(LowPC), Length(HighPC - LowPC), CUOffset(CUOffset) {}
 
     void setHighPC(uint64_t HighPC) {
