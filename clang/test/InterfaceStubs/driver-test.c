c38e42527b21acee8d01a016d5bfa2fb83202e29
Puyan Lotfi <puyan@puyan.org>
2020-01-21 22:45:23 -0500

    [clang][IFS][test] Temporary work around for in-process cc1 ASAN issues.
    When using in-process cc1, the Clang Interface Stubs pipeline setup
    exposes an ASAN bug. I am still investigating this issue but want to
    green the bots for now. I don't think this is a huge issue since the
    Clang Interface Stubs Driver setup code is the only code path that sets
    up such a pipeline (ie N cc1's for N c files followed by another N cc1's
    for to generate stub files for the same N c files).
    
    This issue is being discussed in https://reviews.llvm.org/D69825.
    If a resolution is not found soon, a bugzilla filling will be in order.

--- clang/test/InterfaceStubs/driver-test.c
+++ clang/test/InterfaceStubs/driver-test.c
@@ -1,8 +1,17 @@
 // REQUIRES: x86-registered-target
 // REQUIRES: shell
 
+// NOTE: -fno-integrated-cc1 has been added to work around an ASAN failure
+//       caused by in-process cc1 invocation. Clang InterfaceStubs is not the
+//       culprit, but Clang Interface Stubs' Driver pipeline setup uncovers an
+//       existing ASAN issue when invoking multiple normal cc1 jobs along with
+//       multiple Clang Interface Stubs cc1 jobs together.
+//       There is currently a discussion of this going on at:
+//         https://reviews.llvm.org/D69825
 // RUN: mkdir -p %t; cd %t
-// RUN: %clang -target x86_64-unknown-linux-gnu -x c -S -emit-interface-stubs %s %S/object.c %S/weak.cpp && \
+// RUN: %clang -target x86_64-unknown-linux-gnu -x c -S \
+// RUN:   -fno-integrated-cc1 \
+// RUN: -emit-interface-stubs %s %S/object.c %S/weak.cpp && \
 // RUN: llvm-nm %t/a.out.ifso 2>&1 | FileCheck --check-prefix=CHECK-IFS %s
 
 // CHECK-IFS-DAG: data
