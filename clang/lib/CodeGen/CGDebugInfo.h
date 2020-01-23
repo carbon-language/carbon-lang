c83602fdf51b2692e3bacb06bf861f20f74e987f
Awanish Pandey <Awanish.Pandey@amd.com>
2020-01-23 16:06:52 +0530

    Recommit "[DWARF5][clang]: Added support for DebugInfo generation for auto return type for C++ member functions."
    Summary:
    This was reverted in e45fcfc3aa57bb237fd4fd694d0c257be66d5482 due to
    libcxx build failure. This revision addresses that case.
    
    Original commit message:
        This patch will provide support for auto return type for the C++ member
        functions.
    
        This patch includes clang side implementation of this feature.
    
        Patch by: Awanish Pandey <Awanish.Pandey@amd.com>
    
        Reviewers: dblaikie, aprantl, shafik, alok, SouraVX, jini.susan.george
        Reviewed by: dblaikie
    
        Differential Revision: https://reviews.llvm.org/D70524

--- clang/lib/CodeGen/CGDebugInfo.h
+++ clang/lib/CodeGen/CGDebugInfo.h
@@ -165,6 +165,7 @@ class CGDebugInfo {
   /// ivars and property accessors.
   llvm::DIType *CreateType(const BuiltinType *Ty);
   llvm::DIType *CreateType(const ComplexType *Ty);
+  llvm::DIType *CreateType(const AutoType *Ty);
   llvm::DIType *CreateQualifiedType(QualType Ty, llvm::DIFile *Fg);
   llvm::DIType *CreateType(const TypedefType *Ty, llvm::DIFile *Fg);
   llvm::DIType *CreateType(const TemplateSpecializationType *Ty,
@@ -214,10 +215,10 @@ class CGDebugInfo {
   /// not updated to include implicit \c this pointer. Use this routine
   /// to get a method type which includes \c this pointer.
   llvm::DISubroutineType *getOrCreateMethodType(const CXXMethodDecl *Method,
-                                                llvm::DIFile *F);
+                                                llvm::DIFile *F, bool decl);
   llvm::DISubroutineType *
   getOrCreateInstanceMethodType(QualType ThisPtr, const FunctionProtoType *Func,
-                                llvm::DIFile *Unit);
+                                llvm::DIFile *Unit, bool decl);
   llvm::DISubroutineType *
   getOrCreateFunctionType(const Decl *D, QualType FnType, llvm::DIFile *F);
   /// \return debug info descriptor for vtable.
