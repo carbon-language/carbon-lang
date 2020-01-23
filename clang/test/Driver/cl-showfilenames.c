e174da447c180b586719cb28f7bd556e30625762
Mitch Phillips <31459023+hctim@users.noreply.github.com>
2020-01-23 14:23:38 -0800

    [Clang][IFS][Test] Work around in-process cc1 ASAN issues #2.
    Using the same strategy as c38e42527b21.
    
    D69825 revealed (introduced?) a problem when building with ASan, and
    some memory leaks somewhere. More details are available in the original
    patch.
    
    Looks like we missed one failing tests, this patch adds the workaround
    to this test as well.

--- clang/test/Driver/cl-showfilenames.c
+++ clang/test/Driver/cl-showfilenames.c
@@ -2,11 +2,19 @@
 // target Windows.
 // REQUIRES: x86-registered-target
 
-// RUN: %clang_cl --target=i686-pc-win32 /c /Fo%T/ /showFilenames -- %s 2>&1 | FileCheck -check-prefix=show %s
-// RUN: %clang_cl --target=i686-pc-win32 /c /Fo%T/ /showFilenames -- %s %S/Inputs/wildcard*.c 2>&1 | FileCheck -check-prefix=multiple %s
+// NOTE: -fno-integrated-cc1 has been added to work around an ASAN failure
+//       caused by in-process cc1 invocation. Clang InterfaceStubs is not the
+//       culprit, but Clang Interface Stubs' Driver pipeline setup uncovers an
+//       existing ASAN issue when invoking multiple normal cc1 jobs along with
+//       multiple Clang Interface Stubs cc1 jobs together.
+//       There is currently a discussion of this going on at:
+//         https://reviews.llvm.org/D69825
 
-// RUN: %clang_cl --target=i686-pc-win32 /c /Fo%T/ -- %s 2>&1 | FileCheck -check-prefix=noshow %s
-// RUN: %clang_cl --target=i686-pc-win32 /c /Fo%T/ /showFilenames /showFilenames- -- %s 2>&1 | FileCheck -check-prefix=noshow %s
+// RUN: %clang_cl -fno-integrated-cc1 --target=i686-pc-win32 /c /Fo%T/ /showFilenames -- %s 2>&1 | FileCheck -check-prefix=show %s
+// RUN: %clang_cl -fno-integrated-cc1 --target=i686-pc-win32 /c /Fo%T/ /showFilenames -- %s %S/Inputs/wildcard*.c 2>&1 | FileCheck -check-prefix=multiple %s
+
+// RUN: %clang_cl -fno-integrated-cc1 --target=i686-pc-win32 /c /Fo%T/ -- %s 2>&1 | FileCheck -check-prefix=noshow %s
+// RUN: %clang_cl -fno-integrated-cc1 --target=i686-pc-win32 /c /Fo%T/ /showFilenames /showFilenames- -- %s 2>&1 | FileCheck -check-prefix=noshow %s
 
 
 #pragma message "Hello"
