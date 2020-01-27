ddd20ed1586c55947e84620d674a60c118ec6905
Ilya Biryukov <ibiryukov@google.com>
2020-01-27 14:54:11 +0100

    [AST] Move dependence computations into a separate file
    To group the code in one place, simplify it and make it easier to add
    the containsErrors bit and find existing bugs.

--- clang/include/clang/AST/TemplateBase.h
+++ clang/include/clang/AST/TemplateBase.h
@@ -669,6 +669,7 @@ struct alignas(void *) ASTTemplateKWAndArgsInfo {
   void initializeFrom(SourceLocation TemplateKWLoc,
                       const TemplateArgumentListInfo &List,
                       TemplateArgumentLoc *OutArgArray);
+  // FIXME: cleanup the unused Deps parameter, the caller doesn't need it now.
   void initializeFrom(SourceLocation TemplateKWLoc,
                       const TemplateArgumentListInfo &List,
                       TemplateArgumentLoc *OutArgArray,
