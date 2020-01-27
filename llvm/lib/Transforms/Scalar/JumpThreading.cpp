fe6f6cd6b8e647c5b4ac82f4fcd56c057c2ef8ce
stozer <stephen.tozer@sony.com>
2020-01-27 16:05:34 +0000

    [DebugInfo] Prevent explosion of debug intrinsics during jump threading
    This patch is a fix following the revert of 72ce759
    (https://reviews.llvm.org/rG72ce759928e6dfee6a9efa310b966c19722352ba)
    and fixes the failure that it caused.
    
    The above patch failed on the Thread Sanitizer buildbot with an out of
    memory error. After an investigation, the cause was identified as an
    explosion in debug intrinsics while running the Jump Threading pass on
    ModuleMap.ll. The above patched prevented debug intrinsics from being
    dropped when their Basic Block was deleted due to being "empty". In this
    case, one of the functions in ModuleMap.ll had (after many optimization
    passes) a very large number of debug intrinsics representing a set of
    repeatedly inlined variables. Previously the vast majority of these were
    silently dropped during Jump Threading when their blocks were deleted,
    but as of the above patch they survived for longer, causing a large
    increase in the number of debug intrinsics. These intrinsics were then
    repeatedly cloned by the Jump Threading pass as edges were threaded,
    multiplying the intrinsic count further. The memory consumed by this
    process spiralled out of control, crashing the buildbot that uses TSan
    (which has an estimated 5-10x memory overhead compared to non-sanitized
    builds).
    
    This patch adds RemoveRedundantDbgInstrs to the Jump Threading pass, in
    order to reduce the number of debug intrinsics down to a manageable
    amount in cases where many intrinsics for the same variable end up
    bunched together contiguously, as in this case.
    
    Differential Revision: https://reviews.llvm.org/D73054

--- llvm/lib/Transforms/Scalar/JumpThreading.cpp
+++ llvm/lib/Transforms/Scalar/JumpThreading.cpp
@@ -405,6 +405,12 @@ bool JumpThreadingPass::runImpl(Function &F, TargetLibraryInfo *TLI_,
         continue;
       while (ProcessBlock(&BB)) // Thread all of the branches we can over BB.
         Changed = true;
+
+      // Jump threading may have introduced redundant debug values into BB
+      // which should be removed.
+      if (Changed)
+        RemoveRedundantDbgInstrs(&BB);
+
       // Stop processing BB if it's the entry or is now deleted. The following
       // routines attempt to eliminate BB and locating a suitable replacement
       // for the entry is non-trivial.
@@ -427,17 +433,21 @@ bool JumpThreadingPass::runImpl(Function &F, TargetLibraryInfo *TLI_,
       // ProcessBlock doesn't thread BBs with unconditional TIs. However, if BB
       // is "almost empty", we attempt to merge BB with its sole successor.
       auto *BI = dyn_cast<BranchInst>(BB.getTerminator());
-      if (BI && BI->isUnconditional() &&
-          // The terminator must be the only non-phi instruction in BB.
-          BB.getFirstNonPHIOrDbg()->isTerminator() &&
-          // Don't alter Loop headers and latches to ensure another pass can
-          // detect and transform nested loops later.
-          !LoopHeaders.count(&BB) && !LoopHeaders.count(BI->getSuccessor(0)) &&
-          TryToSimplifyUncondBranchFromEmptyBlock(&BB, DTU)) {
-        // BB is valid for cleanup here because we passed in DTU. F remains
-        // BB's parent until a DTU->getDomTree() event.
-        LVI->eraseBlock(&BB);
-        Changed = true;
+      if (BI && BI->isUnconditional()) {
+        BasicBlock *Succ = BI->getSuccessor(0);
+        if (
+            // The terminator must be the only non-phi instruction in BB.
+            BB.getFirstNonPHIOrDbg()->isTerminator() &&
+            // Don't alter Loop headers and latches to ensure another pass can
+            // detect and transform nested loops later.
+            !LoopHeaders.count(&BB) && !LoopHeaders.count(Succ) &&
+            TryToSimplifyUncondBranchFromEmptyBlock(&BB, DTU)) {
+          RemoveRedundantDbgInstrs(Succ);
+          // BB is valid for cleanup here because we passed in DTU. F remains
+          // BB's parent until a DTU->getDomTree() event.
+          LVI->eraseBlock(&BB);
+          Changed = true;
+        }
       }
     }
     EverChanged |= Changed;
