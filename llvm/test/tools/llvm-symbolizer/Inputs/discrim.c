b96e6859c993097ccf97ce56db5b509234f932d5
David Blaikie <dblaikie@gmail.com>
2020-01-28 11:02:16 -0800

    llvm-symbolizer test: Add a bit of extra detail on how to compile/reproduce this
    The details are also in the .test file, but doesn't hurt to make it a
    bit clearer.

--- llvm/test/tools/llvm-symbolizer/Inputs/discrim.c
+++ llvm/test/tools/llvm-symbolizer/Inputs/discrim.c
@@ -1,3 +1,7 @@
+// Compile with:  clang -gmlt -fdebug-info-for-profiling -O2 discrim.c -o discrim
+// to get an input file with DWARF line table discriminators in it.
+// Tested in test/tools/llvm-symbolizer/sym-verbose.test
+
 static volatile int do_mul;
 static volatile int x, v;
 
