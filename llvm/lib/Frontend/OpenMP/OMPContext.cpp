ae5d3e8c5115695fe2b1f7f652a696b0b0069551
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-02-03 11:08:48 +0000

    Fix MSVC "result of 32-bit shift implicitly converted to 64 bits" warning. NFCI.

--- llvm/lib/Frontend/OpenMP/OMPContext.cpp
+++ llvm/lib/Frontend/OpenMP/OMPContext.cpp
@@ -214,13 +214,13 @@ static APInt getVariantMatchScore(const VariantMatchInfo &VMI,
 
     switch (getOpenMPContextTraitSelectorForProperty(Property)) {
     case TraitSelector::device_kind:
-      Score += (1 << (NoConstructTraits + 0));
+      Score += (1ULL << (NoConstructTraits + 0));
       continue;
     case TraitSelector::device_arch:
-      Score += (1 << (NoConstructTraits + 1));
+      Score += (1ULL << (NoConstructTraits + 1));
       continue;
     case TraitSelector::device_isa:
-      Score += (1 << (NoConstructTraits + 2));
+      Score += (1ULL << (NoConstructTraits + 2));
       continue;
     default:
       continue;
@@ -236,7 +236,7 @@ static APInt getVariantMatchScore(const VariantMatchInfo &VMI,
            "Ill-formed variant match info!");
     (void)Property;
     // ConstructMatches is the position p - 1 and we need 2^(p-1).
-    Score += (1 << ConstructMatches[ConstructIdx++]);
+    Score += (1ULL << ConstructMatches[ConstructIdx++]);
   }
 
   LLVM_DEBUG(dbgs() << "[" << DEBUG_TYPE << "] Variant has a score of " << Score
