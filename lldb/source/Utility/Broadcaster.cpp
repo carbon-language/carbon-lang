50d2d33b8ef530fbee369cbf8e7ea4479597742d
Reid Kleckner <rnk@google.com>
2020-02-04 15:56:51 -0800

    Fix BroadcasterManager::RemoveListener to really remove the listener
    This appears to be a real bug caught by -Wunused-value. std::find_if
    doesn't modify the underlying collection, it just returns an iterator
    pointing to the matching element.
    
    Reviewed By: JDevlieghere
    
    Differential Revision: https://reviews.llvm.org/D74010

--- lldb/source/Utility/Broadcaster.cpp
+++ lldb/source/Utility/Broadcaster.cpp
@@ -406,7 +406,7 @@ void BroadcasterManager::RemoveListener(Listener *listener) {
   listener_collection::iterator iter = m_listeners.begin(),
                                 end_iter = m_listeners.end();
 
-  std::find_if(iter, end_iter, predicate);
+  iter = std::find_if(iter, end_iter, predicate);
   if (iter != end_iter)
     m_listeners.erase(iter);
 
