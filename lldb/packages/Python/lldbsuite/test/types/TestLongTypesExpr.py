c1912c7542d6f49dfd6cce03193727981529db8a
Jonas Devlieghere <jonas@devlieghere.com>
2020-02-03 20:32:36 -0800

    [lldb/Test] Use arrays instead of sets for build_and_run_expr

--- lldb/packages/Python/lldbsuite/test/types/TestLongTypesExpr.py
+++ lldb/packages/Python/lldbsuite/test/types/TestLongTypesExpr.py
@@ -13,39 +13,39 @@ class LongTypesExprTestCase(AbstractBase.GenericTester):
 
     def test_long_type(self):
         """Test that long-type variable expressions are evaluated correctly."""
-        self.build_and_run_expr('long.cpp', set(['long']))
+        self.build_and_run_expr('long.cpp', ['long'])
 
     @skipUnlessDarwin
     def test_long_type_from_block(self):
         """Test that long-type variables are displayed correctly from a block."""
-        self.build_and_run_expr('long.cpp', set(['long']), bc=True)
+        self.build_and_run_expr('long.cpp', ['long'], bc=True)
 
     def test_unsigned_long_type(self):
         """Test that 'unsigned long'-type variable expressions are evaluated correctly."""
-        self.build_and_run_expr('unsigned_long.cpp', set(['unsigned', 'long']))
+        self.build_and_run_expr('unsigned_long.cpp', ['unsigned', 'long'])
 
     @skipUnlessDarwin
     def test_unsigned_long_type_from_block(self):
         """Test that 'unsigned_long'-type variables are displayed correctly from a block."""
         self.build_and_run_expr(
-            'unsigned_long.cpp', set(['unsigned', 'long']), bc=True)
+            'unsigned_long.cpp', ['unsigned', 'long'], bc=True)
 
     def test_long_long_type(self):
         """Test that 'long long'-type variable expressions are evaluated correctly."""
-        self.build_and_run_expr('long_long.cpp', set(['long long']))
+        self.build_and_run_expr('long_long.cpp', ['long long'])
 
     @skipUnlessDarwin
     def test_long_long_type_from_block(self):
         """Test that 'long_long'-type variables are displayed correctly from a block."""
-        self.build_and_run_expr('long_long.cpp', set(['long long']), bc=True)
+        self.build_and_run_expr('long_long.cpp', ['long long'], bc=True)
 
     def test_unsigned_long_long_type(self):
         """Test that 'unsigned long long'-type variable expressions are evaluated correctly."""
         self.build_and_run_expr('unsigned_long_long.cpp',
-                                set(['unsigned', 'long long']))
+                                ['unsigned', 'long long'])
 
     @skipUnlessDarwin
     def test_unsigned_long_long_type_from_block(self):
         """Test that 'unsigned_long_long'-type variables are displayed correctly from a block."""
         self.build_and_run_expr(
-            'unsigned_long_long.cpp', set(['unsigned', 'long long']), bc=True)
+            'unsigned_long_long.cpp', ['unsigned', 'long long'], bc=True)
