77519b60d99233ccea2622ea3e40b30018d2f228
Fangrui Song <maskray@google.com>
2020-02-05 10:24:51 -0800

    [test] yaml2obj -docnum => --docnum=

--- lld/test/ELF/invalid/symtab-sh-info.s
+++ lld/test/ELF/invalid/symtab-sh-info.s
@@ -1,7 +1,7 @@
 ## .symtab's sh_info contains zero value. First entry in a .symtab is a
 ## zero entry that must exist in a valid object, so sh_info can't be null.
 ## Check we report a proper error for that case.
-# RUN: yaml2obj -docnum=1 %s -o %t.o
+# RUN: yaml2obj --docnum=1 %s -o %t.o
 # RUN: not ld.lld %t.o -o %t2 2>&1 | FileCheck %s --check-prefix=ERR1
 # ERR1: invalid sh_info in symbol table
 
@@ -21,7 +21,7 @@ Symbols:
 
 ## sh_info has value 2 what says that non-local symbol `foo` is local.
 ## Check we report this case.
-# RUN: yaml2obj -docnum=2 %s -o %t.o
+# RUN: yaml2obj --docnum=2 %s -o %t.o
 # RUN: not ld.lld %t.o -o %t2 2>&1 | FileCheck --check-prefix=ERR2 %s
 # ERR2: broken object: getLocalSymbols returns a non-local symbol
 
@@ -41,7 +41,7 @@ Symbols:
 
 ## sh_info has value 0xff what is larger than number of symbols in a .symtab.
 ## Check we report this case.
-# RUN: yaml2obj -docnum=3 %s -o %t.o
+# RUN: yaml2obj --docnum=3 %s -o %t.o
 # RUN: not ld.lld %t.o -o %t2 2>&1 | FileCheck --check-prefix=ERR1 %s
 
 --- !ELF
