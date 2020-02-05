4b23a2e8e971876d075d3ae322754dbc0495413d
Jan Vesely <jan.vesely@rutgers.edu>
2020-02-04 20:09:12 -0500

    libclc: Move rsqrt implementation to a .cl file
    Reviewer: awatry
    Differential Revision: https://reviews.llvm.org/D74013

--- libclc/generic/include/clc/math/rsqrt.h
+++ libclc/generic/include/clc/math/rsqrt.h
@@ -1,1 +1,7 @@
-#define rsqrt(x) (1.f/sqrt(x))
+#define __CLC_BODY <clc/math/unary_decl.inc>
+#define __CLC_FUNCTION rsqrt
+
+#include <clc/math/gentype.inc>
+
+#undef __CLC_BODY
+#undef __CLC_FUNCTION
