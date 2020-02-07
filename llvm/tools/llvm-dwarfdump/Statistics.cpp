d173cb1db73add8e563663838e630e72723c3f92
Djordje Todorovic <djordje.todorovic@rt-rk.com>
2020-02-07 12:37:29 +0100

    [llvm-dwarfdump][Stats] Add the license header
    Add the License header into the Statistics.cpp.
    
    Differential Revision: https://reviews.llvm.org/D74207

--- llvm/tools/llvm-dwarfdump/Statistics.cpp
+++ llvm/tools/llvm-dwarfdump/Statistics.cpp
@@ -1,3 +1,11 @@
+//===- Statistics.cpp ------------------------------------------- *- C++ *-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/StringExtras.h"
 #include "llvm/ADT/StringSet.h"
