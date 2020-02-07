d180899cb7107fdee4078f890cc18ea635925c09
Djordje Todorovic <djordje.todorovic@rt-rk.com>
2020-02-07 18:15:04 +0100

    [llvm-dwarfdump][Stats] Fix the License header
    Fix the added License.
    
    Differential Revision: https://reviews.llvm.org/D74207

--- llvm/tools/llvm-dwarfdump/Statistics.cpp
+++ llvm/tools/llvm-dwarfdump/Statistics.cpp
@@ -1,4 +1,4 @@
-//===- Statistics.cpp ------------------------------------------- *- C++ *-===//
+//===-- Statistics.cpp - Debug Info quality metrics -----------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
