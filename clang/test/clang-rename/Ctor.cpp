2733ad2c244ab33553bbd1aaca4c76907fdd0760
Haojian Wu <hokein.wu@gmail.com>
2020-02-11 10:05:52 +0100

    [clang-rename] Fix the failure rename test.
    We cannot run two different tests in a single lit test, split into two.

--- clang/test/clang-rename/Ctor.cpp
+++ clang/test/clang-rename/Ctor.cpp
@@ -5,23 +5,10 @@ public:
 
 Foo::Foo()  /* Test 2 */ {}   // CHECK: Bar::Bar()  /* Test 2 */ {}
 
-
-class Foo2 { /* Test 3 */     // CHECK: class Bar2 {
-public:
-  template <typename T>
-  Foo2();                     // CHECK: Bar2();
-
-  template <typename T>
-  Foo2(Foo2 &);               // CHECK: Bar2(Bar2 &);
-};
-
-
 // Test 1.
 // RUN: clang-rename -offset=62 -new-name=Bar %s -- | sed 's,//.*,,' | FileCheck %s
 // Test 2.
 // RUN: clang-rename -offset=116 -new-name=Bar %s -- | sed 's,//.*,,' | FileCheck %s
-// Test 3.
-// RUN: clang-rename -offset=187 -new-name=Bar2 %s -- | sed 's,//.*,,' | FileCheck %s
 
 // To find offsets after modifying the file, use:
 //   grep -Ubo 'Foo.*' <file>
