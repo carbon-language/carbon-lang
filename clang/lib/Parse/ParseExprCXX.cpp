271e495399170d69627c1acd591c9298cb0b5b4b
Saar Raz <saar@raz.email>
2020-02-12 16:23:55 +0200

    [Concepts] Add missing TPA commit to requires expression parsing
    If an error had occurred when annotating a scope spec during the tentative parse
    for a type-requirement, we would not revert nor commit the tentative parse, triggerring
    an assertion failure.
    
    Commit the TPA in this case and then do error recovery.

--- clang/lib/Parse/ParseExprCXX.cpp
+++ clang/lib/Parse/ParseExprCXX.cpp
@@ -3486,6 +3486,7 @@ ExprResult Parser::ParseRequiresExpression() {
           // We need to consume the typename to allow 'requires { typename a; }'
           SourceLocation TypenameKWLoc = ConsumeToken();
           if (TryAnnotateCXXScopeToken()) {
+            TPA.Commit();
             SkipUntil(tok::semi, tok::r_brace, SkipUntilFlags::StopBeforeMatch);
             break;
           }
