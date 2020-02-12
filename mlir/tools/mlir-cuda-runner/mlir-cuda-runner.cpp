56aba9699d8d1d10f7e93f60dd11f7e4cce3eb93
Valentin Clement <clementval@gmail.com>
2020-02-12 22:35:02 +0100

    [MLIR] Fix wrong header for mlir-cuda-runner
    Just updated the wrong header probably copied from the mlir-cpu-runner
    
    Differential Revision: https://reviews.llvm.org/D74497

--- mlir/tools/mlir-cuda-runner/mlir-cuda-runner.cpp
+++ mlir/tools/mlir-cuda-runner/mlir-cuda-runner.cpp
@@ -1,4 +1,4 @@
-//===- mlir-cpu-runner.cpp - MLIR CPU Execution Driver---------------------===//
+//===- mlir-cuda-runner.cpp - MLIR CUDA Execution Driver-------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
