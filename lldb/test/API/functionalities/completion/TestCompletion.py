346cb7b50f7f892b33b46313742eef86bfde7943
Raphael Isemann <teemperor@gmail.com>
2020-02-12 07:13:48 +0100

    [lldb] Remove skipIfFreeBSD from TestCompletion
    This was skipped because of an pexpect issue (http://llvm.org/pr22784),
    but this test is no longer using pexpect and is running fine on FreeBSD.

--- lldb/test/API/functionalities/completion/TestCompletion.py
+++ lldb/test/API/functionalities/completion/TestCompletion.py
@@ -27,17 +27,14 @@ class CommandLineCompletionTestCase(TestBase):
         except:
             pass
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_at(self):
         """Test that 'at' completes to 'attach '."""
         self.complete_from_to('at', 'attach ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_de(self):
         """Test that 'de' completes to 'detach '."""
         self.complete_from_to('de', 'detach ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_frame_variable(self):
         self.build()
         self.main_source = "main.cpp"
@@ -76,95 +73,79 @@ class CommandLineCompletionTestCase(TestBase):
         self.complete_from_to('frame variable ptr_container->Mem',
                               'frame variable ptr_container->MemberVar')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_process_attach_dash_dash_con(self):
         """Test that 'process attach --con' completes to 'process attach --continue '."""
         self.complete_from_to(
             'process attach --con',
             'process attach --continue ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_process_launch_arch(self):
         self.complete_from_to('process launch --arch ',
                               ['mips',
                                'arm64'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_ambiguous_long_opt(self):
         self.completions_match('breakpoint modify --th',
                                ['--thread-id',
                                 '--thread-index',
                                 '--thread-name'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_plugin_load(self):
         self.complete_from_to('plugin load ', [])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_log_enable(self):
         self.complete_from_to('log enable ll', ['lldb'])
         self.complete_from_to('log enable dw', ['dwarf'])
         self.complete_from_to('log enable lldb al', ['all'])
         self.complete_from_to('log enable lldb sym', ['symbol'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_log_enable(self):
         self.complete_from_to('log disable ll', ['lldb'])
         self.complete_from_to('log disable dw', ['dwarf'])
         self.complete_from_to('log disable lldb al', ['all'])
         self.complete_from_to('log disable lldb sym', ['symbol'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_log_list(self):
         self.complete_from_to('log list ll', ['lldb'])
         self.complete_from_to('log list dw', ['dwarf'])
         self.complete_from_to('log list ll', ['lldb'])
         self.complete_from_to('log list lldb dwa', ['dwarf'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_quoted_command(self):
         self.complete_from_to('"set',
                               ['"settings" '])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_quoted_arg_with_quoted_command(self):
         self.complete_from_to('"settings" "repl',
                               ['"replace" '])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_quoted_arg_without_quoted_command(self):
         self.complete_from_to('settings "repl',
                               ['"replace" '])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_single_quote_command(self):
         self.complete_from_to("'set",
                               ["'settings' "])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_terminated_quote_command(self):
         # This should not crash, but we don't get any
         # reasonable completions from this.
         self.complete_from_to("'settings'", [])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_process_launch_arch_arm(self):
         self.complete_from_to('process launch --arch arm',
                               ['arm64'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_symbols_add_shlib(self):
         # Doesn't seem to work, but at least it shouldn't crash.
         self.complete_from_to('target symbols add --shlib ', [])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_log_file(self):
         # Complete in our source directory which contains a 'main.cpp' file.
         src_dir =  os.path.dirname(os.path.realpath(__file__)) + '/'
         self.complete_from_to('log enable lldb expr -f ' + src_dir,
                               ['main.cpp'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_log_dir(self):
         # Complete our source directory.
         src_dir =  os.path.dirname(os.path.realpath(__file__))
@@ -172,140 +153,118 @@ class CommandLineCompletionTestCase(TestBase):
                               [src_dir + os.sep], turn_off_re_match=True)
 
     # <rdar://problem/11052829>
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_infinite_loop_while_completing(self):
         """Test that 'process print hello\' completes to itself and does not infinite loop."""
         self.complete_from_to('process print hello\\', 'process print hello\\',
                               turn_off_re_match=True)
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_watchpoint_co(self):
         """Test that 'watchpoint co' completes to 'watchpoint command '."""
         self.complete_from_to('watchpoint co', 'watchpoint command ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_watchpoint_command_space(self):
         """Test that 'watchpoint command ' completes to ['add', 'delete', 'list']."""
         self.complete_from_to(
             'watchpoint command ', [
                 'add', 'delete', 'list'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_watchpoint_command_a(self):
         """Test that 'watchpoint command a' completes to 'watchpoint command add '."""
         self.complete_from_to(
             'watchpoint command a',
             'watchpoint command add ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_watchpoint_set_ex(self):
         """Test that 'watchpoint set ex' completes to 'watchpoint set expression '."""
         self.complete_from_to(
             'watchpoint set ex',
             'watchpoint set expression ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_watchpoint_set_var(self):
         """Test that 'watchpoint set var' completes to 'watchpoint set variable '."""
         self.complete_from_to('watchpoint set var', 'watchpoint set variable ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_help_fi(self):
         """Test that 'help fi' completes to ['file', 'finish']."""
         self.complete_from_to(
             'help fi', [
                 'file', 'finish'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_help_watchpoint_s(self):
         """Test that 'help watchpoint s' completes to 'help watchpoint set '."""
         self.complete_from_to('help watchpoint s', 'help watchpoint set ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_append_target_er(self):
         """Test that 'settings append target.er' completes to 'settings append target.error-path'."""
         self.complete_from_to(
             'settings append target.er',
             'settings append target.error-path')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_insert_after_target_en(self):
         """Test that 'settings insert-after target.env' completes to 'settings insert-after target.env-vars'."""
         self.complete_from_to(
             'settings insert-after target.env',
             'settings insert-after target.env-vars')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_insert_before_target_en(self):
         """Test that 'settings insert-before target.env' completes to 'settings insert-before target.env-vars'."""
         self.complete_from_to(
             'settings insert-before target.env',
             'settings insert-before target.env-vars')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_replace_target_ru(self):
         """Test that 'settings replace target.ru' completes to 'settings replace target.run-args'."""
         self.complete_from_to(
             'settings replace target.ru',
             'settings replace target.run-args')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_show_term(self):
         self.complete_from_to(
             'settings show term-',
             'settings show term-width')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_list_term(self):
         self.complete_from_to(
             'settings list term-',
             'settings list term-width')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_remove_term(self):
         self.complete_from_to(
             'settings remove term-',
             'settings remove term-width')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_s(self):
         """Test that 'settings s' completes to ['set', 'show']."""
         self.complete_from_to(
             'settings s', [
                 'set', 'show'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_th(self):
         """Test that 'settings set thread-f' completes to 'settings set thread-format'."""
         self.complete_from_to('settings set thread-f', 'settings set thread-format')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_s_dash(self):
         """Test that 'settings set --g' completes to 'settings set --global'."""
         self.complete_from_to('settings set --g', 'settings set --global')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_clear_th(self):
         """Test that 'settings clear thread-f' completes to 'settings clear thread-format'."""
         self.complete_from_to(
             'settings clear thread-f',
             'settings clear thread-format')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_ta(self):
         """Test that 'settings set ta' completes to 'settings set target.'."""
         self.complete_from_to(
             'settings set target.ma',
             'settings set target.max-')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_target_exec(self):
         """Test that 'settings set target.exec' completes to 'settings set target.exec-search-paths '."""
         self.complete_from_to(
             'settings set target.exec',
             'settings set target.exec-search-paths')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_target_pr(self):
         """Test that 'settings set target.pr' completes to [
         'target.prefer-dynamic-value', 'target.process.']."""
@@ -313,21 +272,18 @@ class CommandLineCompletionTestCase(TestBase):
                               ['target.prefer-dynamic-value',
                                'target.process.'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_target_process(self):
         """Test that 'settings set target.process' completes to 'settings set target.process.'."""
         self.complete_from_to(
             'settings set target.process',
             'settings set target.process.')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_target_process_dot(self):
         """Test that 'settings set target.process.t' completes to 'settings set target.process.thread.'."""
         self.complete_from_to(
             'settings set target.process.t',
             'settings set target.process.thread.')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_settings_set_target_process_thread_dot(self):
         """Test that 'settings set target.process.thread.' completes to [
         'target.process.thread.step-avoid-regexp', 'target.process.thread.trace-thread']."""
@@ -335,7 +291,6 @@ class CommandLineCompletionTestCase(TestBase):
                               ['target.process.thread.step-avoid-regexp',
                                'target.process.thread.trace-thread'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_space(self):
         """Test that 'target ' completes to ['create', 'delete', 'list',
         'modules', 'select', 'stop-hook', 'variable']."""
@@ -348,7 +303,6 @@ class CommandLineCompletionTestCase(TestBase):
                                'stop-hook',
                                'variable'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_modules_dump_line_table(self):
         """Tests source file completion by completing the line-table argument."""
         self.build()
@@ -356,7 +310,6 @@ class CommandLineCompletionTestCase(TestBase):
         self.complete_from_to('target modules dump line-table main.cp',
                               ['main.cpp'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_modules_load_aout(self):
         """Tests modules completion by completing the target modules load argument."""
         self.build()
@@ -364,12 +317,10 @@ class CommandLineCompletionTestCase(TestBase):
         self.complete_from_to('target modules load a.ou',
                               ['a.out'])
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_create_dash_co(self):
         """Test that 'target create --co' completes to 'target variable --core '."""
         self.complete_from_to('target create --co', 'target create --core ')
 
-    @skipIfFreeBSD  # timing out on the FreeBSD buildbot
     def test_target_va(self):
         """Test that 'target va' completes to 'target variable '."""
         self.complete_from_to('target va', 'target variable ')
