f8b8a1ca8b6a82f28c96cbb1e3c2c136dc5b836b
serge-sans-paille <sguelton@redhat.com>
2020-02-13 21:48:56 +0100

    Fix lit version test
    Looks like on some system, version is printed on stderr, on some it's on stdout...

--- llvm/utils/lit/tests/version.py
+++ llvm/utils/lit/tests/version.py
@@ -1,5 +1,5 @@
 # Basic sanity check that --version works.
 #
-# RUN: %{lit} --version | FileCheck %s
+# RUN: %{lit} --version 2>&1 | FileCheck %s
 #
 # CHECK: lit {{[0-9]+\.[0-9]+\.[0-9]+[a-zA-Z0-9]*}}
