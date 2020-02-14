a7018e8a2edf05370f3b4e3cdc018e1f5bc932de
Dan Liew <dan@su-root.co.uk>
2020-02-14 11:43:35 -0800

    [compiler-rt] Disable building LibFuzzer for WatchOS and the corresponding simulator.
    rdar://problem/59466685

--- compiler-rt/cmake/config-ix.cmake
+++ compiler-rt/cmake/config-ix.cmake
@@ -500,6 +500,10 @@ if(APPLE)
     endforeach()
   endif()
 
+  # Explictly disable unsupported Sanitizer configurations.
+  list(REMOVE_ITEM FUZZER_SUPPORTED_OS "watchos")
+  list(REMOVE_ITEM FUZZER_SUPPORTED_OS "watchossim")
+
   # for list_intersect
   include(CompilerRTUtils)
 
