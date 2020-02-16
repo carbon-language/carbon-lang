857bf5da35af8e1f9425e1865dab5f5fce5e38f2
Johannes Doerfert <johannes@jdoerfert.de>
2020-02-16 00:51:11 -0600

    [FIX] Do not copy an llvm::function_ref if it has to be reused
    Some buildbots signaled a problem in this method when the
    llvm::function_ref was copied and reused after 1228d42ddab8. To
    eliminate the problem we avoid copying the llvm::function_ref and
    instead we pass it as a const reference.

--- clang/include/clang/AST/OpenMPClause.h
+++ clang/include/clang/AST/OpenMPClause.h
@@ -6682,10 +6682,10 @@ struct OMPTraitInfo {
   llvm::SmallVector<OMPTraitSet, 4> Sets;
 
   bool anyScoreOrCondition(
-      llvm::function_ref<bool(Expr *&, bool /* IsScore */)> Cond) {
-    return llvm::any_of(Sets, [Cond](OMPTraitInfo::OMPTraitSet &Set) {
+      const llvm::function_ref<bool(Expr *&, bool /* IsScore */)> &Cond) {
+    return llvm::any_of(Sets, [&Cond](OMPTraitInfo::OMPTraitSet &Set) {
       return llvm::any_of(
-          Set.Selectors, [Cond](OMPTraitInfo::OMPTraitSelector &Selector) {
+          Set.Selectors, [&Cond](OMPTraitInfo::OMPTraitSelector &Selector) {
             return Cond(Selector.ScoreOrCondition,
                         /* IsScore */ Selector.Kind !=
                             llvm::omp::TraitSelector::user_condition);
