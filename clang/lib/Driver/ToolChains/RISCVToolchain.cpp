dd230142d8a00f5f30c3930a2407000e845dcfbf
serge-sans-paille <sguelton@redhat.com>
2020-02-17 13:42:00 +0100

    Support -fuse-ld=lld for riscv
    Differential Revision: https://reviews.llvm.org/D74704

--- clang/lib/Driver/ToolChains/RISCVToolchain.cpp
+++ clang/lib/Driver/ToolChains/RISCVToolchain.cpp
@@ -142,7 +142,7 @@ void RISCV::Linker::ConstructJob(Compilation &C, const JobAction &JA,
     CmdArgs.push_back("elf32lriscv");
   }
 
-  std::string Linker = getToolChain().GetProgramPath(getShortName());
+  std::string Linker = getToolChain().GetLinkerPath();
 
   bool WantCRTs =
       !Args.hasArg(options::OPT_nostdlib, options::OPT_nostartfiles);
