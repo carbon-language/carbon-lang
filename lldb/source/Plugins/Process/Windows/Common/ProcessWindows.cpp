3431dc32a4162c0c3e0e0cd3e9e550c5f3c57047
Jonas Devlieghere <jonas@devlieghere.com>
2020-02-17 10:23:43 -0800

    [lldb/Plugin] Fix plugin definition for ProcessWindows
    This should fix the unresolved external symbol error.

--- lldb/source/Plugins/Process/Windows/Common/ProcessWindows.cpp
+++ lldb/source/Plugins/Process/Windows/Common/ProcessWindows.cpp
@@ -44,7 +44,7 @@
 using namespace lldb;
 using namespace lldb_private;
 
-LLDB_PLUGIN_DEFINE(ProcessWindows)
+LLDB_PLUGIN_DEFINE_ADV(ProcessWindows, ProcessWindowsCommon)
 
 namespace {
 std::string GetProcessExecutableName(HANDLE process_handle) {
