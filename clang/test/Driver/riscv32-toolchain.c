cc7a197f9f1039e36625249199aa652f84506802
serge-sans-paille <sguelton@redhat.com>
2020-02-18 12:31:11 +0100

    Fix riscv/lld test interaction
    Fix for dd230142d8a00f5f30c3930a2407000e845dcfbf, in case ld.lld is not
    available.

--- clang/test/Driver/riscv32-toolchain.c
+++ clang/test/Driver/riscv32-toolchain.c
@@ -3,9 +3,9 @@
 // RUN: %clang %s -### -no-canonical-prefixes -target riscv32 2>&1 | FileCheck -check-prefix=CC1 %s
 // CC1: clang{{.*}} "-cc1" "-triple" "riscv32"
 
-// Test interaction with -fuse-ld=lld
+// Test interaction with -fuse-ld=lld, if ld.lld is available.
 // RUN: %clang %s -### -no-canonical-prefixes -target riscv32 -fuse-ld=lld 2>&1 | FileCheck -check-prefix=LLD %s
-// LLD: ld.lld
+// LLD: {{(error: invalid linker name in argument '-fuse-ld=lld')|(ld.lld)}}
 
 // In the below tests, --rtlib=platform is used so that the driver ignores
 // the configure-time CLANG_DEFAULT_RTLIB option when choosing the runtime lib
