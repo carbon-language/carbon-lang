7d91633a2b9b1f563dc14c632cc0c461c3651f76
David Goldman <davg@google.com>
2020-02-19 16:58:22 -0500

    Fix broken test on Windows caused by D74790

--- clang/test/CodeCompletion/included-symlinks.cpp
+++ clang/test/CodeCompletion/included-symlinks.cpp
@@ -1,3 +1,4 @@
+// REQUIRES: shell
 // RUN: rm -rf %t && mkdir -p %t/real/myproj && mkdir -p %t/links
 // RUN: touch %t/real/foo.h && ln -s %t/real/foo.h %t/links/foo.h
 // RUN: touch %t/real/foobar.h && ln -s %t/real/foobar.h %t/links/foobar.h
