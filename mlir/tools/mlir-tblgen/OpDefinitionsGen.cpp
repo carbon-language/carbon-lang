476ca094c846a0b6d4d9f37710aba21a6b0b265a
Alexandre Eichenberger <alexe@us.ibm.com>
2020-02-19 11:39:28 -0500

    [mlir][ods] Adding attribute setters generation
    In some dialects, attributes may have default values that may be
    determined only after shape inference. For example, attributes that
    are dependent on the rank of the input cannot be assigned a default
    value until the rank of the tensor is inferred.
    
    While we can set attributes without explicit setters, referring to
    the attributes via accessors instead of having to use the string
    interface is better for compile time verification.
    
    The proposed patch add one method per operation attribute that let us
    set its value. The code is a very small modification of the existing
    getter methods.
    
    Differential Revision: https://reviews.llvm.org/D74143

--- mlir/tools/mlir-tblgen/OpDefinitionsGen.cpp
+++ mlir/tools/mlir-tblgen/OpDefinitionsGen.cpp
@@ -171,6 +171,9 @@ private:
   // Generates getters for the attributes.
   void genAttrGetters();
 
+  // Generates setter for the attributes.
+  void genAttrSetters();
+
   // Generates getters for named operands.
   void genNamedOperandGetters();
 
@@ -300,6 +303,7 @@ OpEmitter::OpEmitter(const Operator &op)
   genNamedResultGetters();
   genNamedRegionGetters();
   genAttrGetters();
+  genAttrSetters();
   genBuilder();
   genParser();
   genPrinter();
@@ -378,6 +382,25 @@ void OpEmitter::genAttrGetters() {
       emitAttrWithStorageType(name, attr);
       emitAttrWithReturnType(name, attr);
     }
+  }
+}
+
+void OpEmitter::genAttrSetters() {
+  // Generate raw named setter type. This is a wrapper class that allows setting
+  // to the attributes via setters instead of having to use the string interface
+  // for better compile time verification.
+  auto emitAttrWithStorageType = [&](StringRef name, Attribute attr) {
+    auto &method = opClass.newMethod("void", (name + "Attr").str(),
+                                     (attr.getStorageType() + " attr").str());
+    auto &body = method.body();
+    body << "  this->setAttr(\"" << name << "\", attr);";
+  };
+
+  for (auto &namedAttr : op.getAttributes()) {
+    const auto &name = namedAttr.name;
+    const auto &attr = namedAttr.attr;
+    if (!attr.isDerivedAttr())
+      emitAttrWithStorageType(name, attr);
   }
 }
 
