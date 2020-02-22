1b1a97e9b55754302c4b41f6bddc8fbc3870f68a
Alex Brachet <alexbrachetmialot@gmail.com>
2020-02-21 20:48:07 -0500

    Remove unused variable

--- libc/src/signal/linux/raise.cpp
+++ libc/src/signal/linux/raise.cpp
@@ -15,7 +15,7 @@ namespace __llvm_libc {
 
 int LLVM_LIBC_ENTRYPOINT(raise)(int sig) {
   __llvm_libc::Sigset sigset;
-  int got = __llvm_libc::block_all_signals(sigset);
+  __llvm_libc::block_all_signals(sigset);
   long pid = __llvm_libc::syscall(SYS_getpid);
   long tid = __llvm_libc::syscall(SYS_gettid);
   int ret = __llvm_libc::syscall(SYS_tgkill, pid, tid, sig);
