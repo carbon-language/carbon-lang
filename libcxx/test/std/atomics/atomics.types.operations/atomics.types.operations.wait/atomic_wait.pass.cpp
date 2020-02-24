80e73f22956c532e581c280a7388cfa87cec98e8
Louis Dionne <ldionne@apple.com>
2020-02-24 10:08:41 -0500

    [libc++] Adapt a few things around the implementation of P1135R6
    - Add the new symbols to the ABI list on Darwin
    - Add XFAIL markup to the tests that require dylib support on older platforms
    - Add availability markup for back-deployment

--- libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.wait/atomic_wait.pass.cpp
+++ libcxx/test/std/atomics/atomics.types.operations/atomics.types.operations.wait/atomic_wait.pass.cpp
@@ -9,6 +9,15 @@
 // UNSUPPORTED: libcpp-has-no-threads
 // XFAIL: c++98, c++03
 
+// This test requires the dylib support introduced in D68480
+// XFAIL: with_system_cxx_lib=macosx10.15
+// XFAIL: with_system_cxx_lib=macosx10.14
+// XFAIL: with_system_cxx_lib=macosx10.13
+// XFAIL: with_system_cxx_lib=macosx10.12
+// XFAIL: with_system_cxx_lib=macosx10.11
+// XFAIL: with_system_cxx_lib=macosx10.10
+// XFAIL: with_system_cxx_lib=macosx10.9
+
 // <atomic>
 
 #include <atomic>
--- libcxx/test/std/thread/thread.semaphore/try_acquire.pass.cpp
+++ libcxx/test/std/thread/thread.semaphore/try_acquire.pass.cpp
@@ -9,6 +9,15 @@
 // UNSUPPORTED: libcpp-has-no-threads
 // UNSUPPORTED: c++98, c++03, c++11
 
+// This test requires the dylib support introduced in D68480
+// XFAIL: with_system_cxx_lib=macosx10.15
+// XFAIL: with_system_cxx_lib=macosx10.14
+// XFAIL: with_system_cxx_lib=macosx10.13
+// XFAIL: with_system_cxx_lib=macosx10.12
+// XFAIL: with_system_cxx_lib=macosx10.11
+// XFAIL: with_system_cxx_lib=macosx10.10
+// XFAIL: with_system_cxx_lib=macosx10.9
+
 // <semaphore>
 
 #include <semaphore>
