fc466f87804f97b322394ef3b9db43ea3febcc15
Benjamin Kramer <benny.kra@googlemail.com>
2020-02-25 16:03:06 +0100

    Make test not write to the source directory

--- clang/test/CodeGen/static-init.cpp
+++ clang/test/CodeGen/static-init.cpp
@@ -1,8 +1,8 @@
 // RUN: not %clang_cc1 -triple powerpc-ibm-aix-xcoff -S -emit-llvm -x c++ %s \
-// RUN: 2>&1 | FileCheck %s
+// RUN: -o /dev/null 2>&1 | FileCheck %s
 
 // RUN: not %clang_cc1 -triple powerpc64-ibm-aix-xcoff -S -emit-llvm -x c++ %s \
-// RUN: 2>&1 | FileCheck %s
+// RUN: -o /dev/null 2>&1 | FileCheck %s
 
 struct test {
   test();
