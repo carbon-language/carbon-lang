7c2f4a8370829aae5a96b7353fb0727d4e47e8fd
Louis Dionne <ldionne@apple.com>
2020-02-25 11:42:08 -0500

    [libc++] Revert 03dd205c151 "Adjust max_align_t handling"
    That commit was made without approval from a libc++ reviewer, and it
    also broke the build in C++03 mode.

--- libcxx/include/stddef.h
+++ libcxx/include/stddef.h
@@ -51,6 +51,12 @@ extern "C++" {
 using std::nullptr_t;
 }
 
+// Re-use the compiler's <stddef.h> max_align_t where possible.
+#if !defined(__CLANG_MAX_ALIGN_T_DEFINED) && !defined(_GCC_MAX_ALIGN_T) && \
+    !defined(__DEFINED_max_align_t) && !defined(__NetBSD__)
+typedef long double max_align_t;
+#endif
+
 #endif
 
 #endif  // _LIBCPP_STDDEF_H
