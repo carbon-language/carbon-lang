082f1a3b15999c803265fabcb555ad253a00d477
Jason Molenda <jmolenda@apple.com>
2020-02-24 17:18:08 -0800

    My prevous commit to RegisterContextLLDB is causing a test fail on aarch64-ubuntu - collect a little information about what is happening on this target before I revert my change; I don't have access to this target.

--- lldb/test/API/functionalities/signal/handle-abrt/TestHandleAbort.py
+++ lldb/test/API/functionalities/signal/handle-abrt/TestHandleAbort.py
@@ -55,6 +55,9 @@ class HandleAbortTestCase(TestBase):
         # Expect breakpoint in 'handler'
         frame = thread.GetFrameAtIndex(0)
         self.assertEqual(frame.GetDisplayFunctionName(), "handler", "Unexpected break?")
+        
+        self.runCmd("log enable lldb unwind")
+        self.runCmd("bt")
 
         # Expect that unwinding should find 'abort_caller'
         foundFoo = False
@@ -62,6 +65,7 @@ class HandleAbortTestCase(TestBase):
             if frame.GetDisplayFunctionName() == "abort_caller":
                 foundFoo = True
 
+        self.runCmd("log disable lldb unwind")
         self.assertTrue(foundFoo, "Unwinding did not find func that called abort")
 
         # Continue until we exit.
