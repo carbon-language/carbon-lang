7d91fd23dfa8ad33b3d270b6a2b8f6cf6a589a38
Alex Zinenko <zinenko@google.com>
2020-02-25 15:14:59 +0100

    [mlir] NFC: update documentation in ConvertLinalgToLLVM
    The documentation was describing an obsolete version of the
    transformation.

--- mlir/lib/Conversion/LinalgToLLVM/LinalgToLLVM.cpp
+++ mlir/lib/Conversion/LinalgToLLVM/LinalgToLLVM.cpp
@@ -194,12 +194,10 @@ public:
 };
 
 /// Conversion pattern that transforms a linalg.slice op into:
-///   1. A function entry `alloca` operation to allocate a ViewDescriptor.
-///   2. A load of the ViewDescriptor from the pointer allocated in 1.
-///   3. Updates to the ViewDescriptor to introduce the data ptr, offset, size
+///   1. An "undef" value for the ViewDescriptor.
+///   2. Updates to the ViewDescriptor to introduce the data ptr, offset, size
 ///      and stride corresponding to the region of memory within the bounds of
 ///      the parent view.
-///   4. A store of the resulting ViewDescriptor to the alloca'ed pointer.
 /// The linalg.slice op is replaced by the alloca'ed pointer.
 class SliceOpConversion : public ConvertToLLVMPattern {
 public:
