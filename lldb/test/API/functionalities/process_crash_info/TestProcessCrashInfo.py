7822c8c03e9fe8c857da21c4ccbe28396b43130d
Vedant Kumar <vsk@apple.com>
2020-02-26 10:11:39 -0800

    [lldb/test] Skip running a test under ASan, it intentionally double-frees

--- lldb/test/API/functionalities/process_crash_info/TestProcessCrashInfo.py
+++ lldb/test/API/functionalities/process_crash_info/TestProcessCrashInfo.py
@@ -25,6 +25,7 @@ class PlatformProcessCrashInfoTestCase(TestBase):
         self.runCmd("settings clear auto-confirm")
         TestBase.tearDown(self)
 
+    @skipIfAsan # The test process intentionally double-frees.
     @skipUnlessDarwin
     def test_cli(self):
         """Test that `process status --verbose` fetches the extended crash
@@ -41,6 +42,7 @@ class PlatformProcessCrashInfoTestCase(TestBase):
                     patterns=["\"message\".*pointer being freed was not allocated"])
 
 
+    @skipIfAsan # The test process intentionally hits a memory bug.
     @skipUnlessDarwin
     def test_api(self):
         """Test that lldb can fetch a crashed process' extended crash information
