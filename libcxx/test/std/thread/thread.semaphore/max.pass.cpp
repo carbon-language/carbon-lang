621388468b5125f05d649262681676bed2faefe6
ogiroux <ogiroux@gmail.com>
2020-02-26 16:49:37 -0800

    Some fixes for open breaks on MacOS and UBSan

--- libcxx/test/std/thread/thread.semaphore/max.pass.cpp
+++ libcxx/test/std/thread/thread.semaphore/max.pass.cpp
@@ -21,7 +21,6 @@ int main(int, char**)
   static_assert(std::counting_semaphore<>::max() > 0, "");
   static_assert(std::counting_semaphore<1>::max() >= 1, "");
   static_assert(std::counting_semaphore<std::numeric_limits<int>::max()>::max() >= 1, "");
-  static_assert(std::counting_semaphore<std::numeric_limits<unsigned>::max()>::max() >= 1, "");
   static_assert(std::counting_semaphore<std::numeric_limits<ptrdiff_t>::max()>::max() >= 1, "");
   static_assert(std::counting_semaphore<1>::max() == std::binary_semaphore::max(), "");
   return 0;
