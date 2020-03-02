c61401b89742f230b7e6a2cc0548fbf7442e906d
Luboš Luňák <l.lunak@centrum.cz>
2020-03-02 22:31:37 +0100

    Revert "[clang] detect switch fallthrough marked by a comment (PR43465)"
    This reverts commit 398b4ed87d488b42032c8d0304324dce76ba9b66.
    As requested in https://bugs.llvm.org/show_bug.cgi?id=43465#c37 .

--- clang/lib/Sema/AnalysisBasedWarnings.cpp
+++ clang/lib/Sema/AnalysisBasedWarnings.cpp
@@ -1148,11 +1148,6 @@ namespace {
           continue;
         }
 
-        if (isFollowedByFallThroughComment(LastStmt)) {
-          ++AnnotatedCnt;
-          continue; // Fallthrough comment, good.
-        }
-
         ++UnannotatedCnt;
       }
       return !!UnannotatedCnt;
@@ -1213,41 +1208,10 @@ namespace {
       return nullptr;
     }
 
-    bool isFollowedByFallThroughComment(const Stmt *Statement) {
-      // Try to detect whether the fallthough is marked by a comment like
-      // /*FALLTHOUGH*/.
-      bool Invalid;
-      const char *SourceData = S.getSourceManager().getCharacterData(
-          Statement->getEndLoc(), &Invalid);
-      if (Invalid)
-        return false;
-      const char *LineStart = SourceData;
-      for (;;) {
-        LineStart = strchr(LineStart, '\n');
-        if (LineStart == nullptr)
-          return false;
-        ++LineStart; // Start of next line.
-        const char *LineEnd = strchr(LineStart, '\n');
-        StringRef Line(LineStart,
-                       LineEnd ? LineEnd - LineStart : strlen(LineStart));
-        if (LineStart == LineEnd ||
-            Line.find_first_not_of(" \t\r") == StringRef::npos)
-          continue; // Whitespace-only line.
-        if (!FallthroughRegex.isValid())
-          FallthroughRegex =
-              llvm::Regex("(/\\*[ \\t]*fall(s | |-)?thr(ough|u)\\.?[ \\t]*\\*/)"
-                          "|(//[ \\t]*fall(s | |-)?thr(ough|u)\\.?[ \\t]*)",
-                          llvm::Regex::IgnoreCase);
-        assert(FallthroughRegex.isValid());
-        return FallthroughRegex.match(Line);
-      }
-    }
-
     bool FoundSwitchStatements;
     AttrStmts FallthroughStmts;
     Sema &S;
     llvm::SmallPtrSet<const CFGBlock *, 16> ReachableBlocks;
-    llvm::Regex FallthroughRegex;
   };
 } // anonymous namespace
 
