1bedb2340774099fd3faa6610a78119a4f802955
Jacques Pienaar <jpienaar@google.com>
2020-03-03 12:01:54 -0800

    [mlir][ods] Add query for derived attribute
    For ODS generated operations enable querying whether there is a derived
    attribute with a given name.
    
    Rollforward of commit 5aa57c2 without using llvm::is_contained.

--- mlir/tools/mlir-tblgen/OpDefinitionsGen.cpp
+++ mlir/tools/mlir-tblgen/OpDefinitionsGen.cpp
@@ -391,6 +391,20 @@ void OpEmitter::genAttrGetters() {
       emitAttrWithReturnType(name, attr);
     }
   }
+
+  // Generate helper method to query whether a named attribute is a derived
+  // attribute. This enables, for example, avoiding adding an attribute that
+  // overlaps with a derived attribute.
+  auto &method =
+      opClass.newMethod("bool", "isDerivedAttribute", "StringRef name");
+  auto &body = method.body();
+  auto derivedAttr = make_filter_range(op.getAttributes(),
+                                       [](const NamedAttribute &namedAttr) {
+                                         return namedAttr.attr.isDerivedAttr();
+                                       });
+  for (auto namedAttr : derivedAttr)
+    body << "    if (name == \"" << namedAttr.name << "\") return true;\n";
+  body << " return false;";
 }
 
 void OpEmitter::genAttrSetters() {
