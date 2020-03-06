16af23fae8ad2949048e0fc34cf9114dfbe4742a
Michael Spencer <bigcheesegs@gmail.com>
2020-03-05 17:56:13 -0800

    [clang][Headers] Use __has_builtin instead of _MSC_VER.
    arm_acle.h relied on `_MSC_VER` to determine if a given function was
    already defined as a builtin. This was incorrect because
    `-fms-extensions` enables these builtins, but is not responsible for
    defining `_MSC_VER` on any target. The next closest thing is
    `_MSC_EXTENSIONS`, which is only defined on Windows targets, but even
    this is suboptimal. What this conditional is actually trying to
    determine is if the given functions are defined as builtins, so just
    check that directly.
    
    I also attempted to do this for `__nop`, but in that case intrin.h,
    which is only includable if `_MSC_VER` is defined, has its own
    definition. So in that case `_MSC_VER` is correct.
    
    Differential Revision: https://reviews.llvm.org/D75719
    rdar://60102353

--- clang/test/Headers/arm-acle-header.c
+++ clang/test/Headers/arm-acle-header.c
@@ -6,6 +6,7 @@
 // RUN: %clang_cc1 -x c++ -triple thumbv7-windows -target-cpu cortex-a15 -fsyntax-only -ffreestanding %s
 // RUN: %clang_cc1 -x c++ -triple thumbv7-windows -target-cpu cortex-a15 -fsyntax-only -ffreestanding -fms-extensions -fms-compatibility -fms-compatibility-version=19.11 %s
 // RUN: %clang_cc1 -x c++ -triple aarch64-windows -target-cpu cortex-a53 -fsyntax-only -ffreestanding -fms-extensions -fms-compatibility -fms-compatibility-version=19.11 %s
+// RUN: %clang_cc1 -x c++ -triple arm64-apple-ios -target-cpu apple-a7 -fsyntax-only -ffreestanding -fms-extensions %s
 // expected-no-diagnostics
 
 #include <arm_acle.h>
