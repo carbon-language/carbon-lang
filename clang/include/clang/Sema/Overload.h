a5704f92b835d1810d83a223f70dfe6c92a34c03
Rafael Ávila de Espíndola <rafael@espindo.la>
2020-03-07 10:52:51 -0800

    [Sema] Initialize IsSurrogate
    This fixes https://bugs.llvm.org/show_bug.cgi?id=45096
    
    Differential Revision: https://reviews.llvm.org/D75701

--- clang/include/clang/Sema/Overload.h
+++ clang/include/clang/Sema/Overload.h
@@ -908,7 +908,7 @@ class Sema;
   private:
     friend class OverloadCandidateSet;
     OverloadCandidate()
-        : IsADLCandidate(CallExpr::NotADL), RewriteKind(CRK_None) {}
+        : IsSurrogate(false), IsADLCandidate(CallExpr::NotADL), RewriteKind(CRK_None) {}
   };
 
   /// OverloadCandidateSet - A set of overload candidates, used in C++
