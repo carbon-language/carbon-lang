a8f76e99ba5966adc82afd98cea5d6239c1501ac
Marcello Maggioni <hayarms@gmail.com>
2020-03-08 10:55:21 -0700

    Fix indentation using clang-format for Spiller.h. NFC Spiller.h had indentation that doesn't match LLVM guidelines. Fixing it
    Reviewers: qcolombet, arsenm
    
    Subscribers: MatzeB, wdng, hiraditya, llvm-commits, qcolombet
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D75830

--- llvm/include/llvm/CodeGen/Spiller.h
+++ llvm/include/llvm/CodeGen/Spiller.h
@@ -16,27 +16,26 @@ class MachineFunction;
 class MachineFunctionPass;
 class VirtRegMap;
 
-  /// Spiller interface.
-  ///
-  /// Implementations are utility classes which insert spill or remat code on
-  /// demand.
-  class Spiller {
-    virtual void anchor();
+/// Spiller interface.
+///
+/// Implementations are utility classes which insert spill or remat code on
+/// demand.
+class Spiller {
+  virtual void anchor();
 
-  public:
-    virtual ~Spiller() = 0;
+public:
+  virtual ~Spiller() = 0;
 
-    /// spill - Spill the LRE.getParent() live interval.
-    virtual void spill(LiveRangeEdit &LRE) = 0;
+  /// spill - Spill the LRE.getParent() live interval.
+  virtual void spill(LiveRangeEdit &LRE) = 0;
 
-    virtual void postOptimization() {}
-  };
+  virtual void postOptimization() {}
+};
 
-  /// Create and return a spiller that will insert spill code directly instead
-  /// of deferring though VirtRegMap.
-  Spiller *createInlineSpiller(MachineFunctionPass &pass,
-                               MachineFunction &mf,
-                               VirtRegMap &vrm);
+/// Create and return a spiller that will insert spill code directly instead
+/// of deferring though VirtRegMap.
+Spiller *createInlineSpiller(MachineFunctionPass &pass, MachineFunction &mf,
+                             VirtRegMap &vrm);
 
 } // end namespace llvm
 
