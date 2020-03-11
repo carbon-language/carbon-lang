36e018b94111b90797489050aa8edc004781a1b5
Jacques Pienaar <jpienaar@google.com>
2020-03-11 16:14:54 -0700

    [mlir] Add derived attribute op interface
    Interface provides uniform access to the the derived attribute query method.

---
+++ mlir/include/mlir/Interfaces/DerivedAttributeOpInterface.h
@@ -0,0 +1,22 @@
+//===- DerivedAttributeOpInterface.h ----------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file contains a set of interfaces for derived attribute op interface.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_INTERFACES_DERIVEDATTRIBUTEOPINTERFACE_H_
+#define MLIR_INTERFACES_DERIVEDATTRIBUTEOPINTERFACE_H_
+
+#include "mlir/IR/OpDefinition.h"
+
+namespace mlir {
+#include "mlir/Interfaces/DerivedAttributeOpInterface.h.inc"
+} // namespace mlir
+
+#endif // MLIR_INTERFACES_DERIVEDATTRIBUTEOPINTERFACE_H_
