90dbec26326cfd6a81a013139aa22cfe480322b1
Mehdi Amini <joker-eph@gmail.com>
2020-03-11 23:37:46 +0000

    Fix MLIR build after header change in LLVM (NFC)

--- mlir/lib/ExecutionEngine/ExecutionEngine.cpp
+++ mlir/lib/ExecutionEngine/ExecutionEngine.cpp
@@ -30,6 +30,7 @@
 #include "llvm/IR/IRBuilder.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/Error.h"
+#include "llvm/Support/Host.h"
 #include "llvm/Support/TargetRegistry.h"
 #include "llvm/Support/ToolOutputFile.h"
 
