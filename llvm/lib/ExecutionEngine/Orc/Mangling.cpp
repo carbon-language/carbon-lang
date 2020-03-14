a7d187d9c05a011de100d2bfa1d0e537e43c2324
Lang Hames <lhames@gmail.com>
2020-03-14 16:52:25 -0700

    Revert "[JITLink][MachO] Treat linker private symbols as hidden rather than private."
    This reverts commit b64afadf306f284a684ee656c6eefbd43c192c8d.
    
    Reverting while I investigate bot failures.

--- llvm/lib/ExecutionEngine/Orc/Mangling.cpp
+++ llvm/lib/ExecutionEngine/Orc/Mangling.cpp
@@ -89,25 +89,14 @@ getObjectSymbolInfo(ExecutionSession &ES, MemoryBufferRef ObjBuffer) {
   if (!Obj)
     return Obj.takeError();
 
-  bool IsMachO = isa<object::MachOObjectFile>(Obj->get());
-
   SymbolFlagsMap SymbolFlags;
   for (auto &Sym : (*Obj)->symbols()) {
     // Skip symbols not defined in this object file.
     if (Sym.getFlags() & object::BasicSymbolRef::SF_Undefined)
       continue;
 
-    // Get the symbol name.
-    auto Name = Sym.getName();
-    if (!Name)
-      return Name.takeError();
-
-    bool IsLinkerPrivate = IsMachO && Name->startswith("l");
-
-    // Skip symbols that are not global. Treat linker private symbols as global
-    // hidden.
-    if (!(Sym.getFlags() & object::BasicSymbolRef::SF_Global) &&
-        !IsLinkerPrivate)
+    // Skip symbols that are not global.
+    if (!(Sym.getFlags() & object::BasicSymbolRef::SF_Global))
       continue;
 
     // Skip symbols that have type SF_File.
@@ -117,6 +106,9 @@ getObjectSymbolInfo(ExecutionSession &ES, MemoryBufferRef ObjBuffer) {
     } else
       return SymType.takeError();
 
+    auto Name = Sym.getName();
+    if (!Name)
+      return Name.takeError();
     auto InternedName = ES.intern(*Name);
     auto SymFlags = JITSymbolFlags::fromObjectSymbol(Sym);
     if (!SymFlags)
@@ -126,10 +118,9 @@ getObjectSymbolInfo(ExecutionSession &ES, MemoryBufferRef ObjBuffer) {
 
   SymbolStringPtr InitSymbol;
 
-  if (IsMachO) {
-    auto &MachOObj = cast<object::MachOObjectFile>(*Obj->get());
-    for (auto &Sec : MachOObj.sections()) {
-      auto SecType = MachOObj.getSectionType(Sec);
+  if (auto *MachOObj = dyn_cast<object::MachOObjectFile>(Obj->get())) {
+    for (auto &Sec : MachOObj->sections()) {
+      auto SecType = MachOObj->getSectionType(Sec);
       if ((SecType & MachO::SECTION_TYPE) == MachO::S_MOD_INIT_FUNC_POINTERS) {
         std::string InitSymString;
         raw_string_ostream(InitSymString)
