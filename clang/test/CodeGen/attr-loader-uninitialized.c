1d19b153955a87bd0f83c8a6a072d69239f76d63
Jon Chesterfield <jonathanchesterfield@gmail.com>
2020-03-17 22:14:40 +0000

    Fix arm build broken by D74361 by dropping align from filecheck pattern

--- clang/test/CodeGen/attr-loader-uninitialized.c
+++ clang/test/CodeGen/attr-loader-uninitialized.c
@@ -1,14 +1,14 @@
 // RUN: %clang_cc1 -emit-llvm %s -o - | FileCheck %s
 
-// CHECK: @tentative_attr_first = global i32 undef, align 4
+// CHECK: @tentative_attr_first = global i32 undef
 int tentative_attr_first __attribute__((loader_uninitialized));
 int tentative_attr_first;
 
-// CHECK: @tentative_attr_second = global i32 undef, align 4
+// CHECK: @tentative_attr_second = global i32 undef
 int tentative_attr_second;
 int tentative_attr_second __attribute__((loader_uninitialized));
 
-// CHECK: @array = global [16 x float] undef, align 16
+// CHECK: @array = global [16 x float] undef
 float array[16] __attribute__((loader_uninitialized));
 
 typedef struct
@@ -17,8 +17,8 @@ typedef struct
   float y;
 } s;
 
-// CHECK: @i = global %struct.s undef, align 4
+// CHECK: @i = global %struct.s undef
 s i __attribute__((loader_uninitialized));
 
-// CHECK: @private_extern_ok = hidden global i32 undef, align 4
+// CHECK: @private_extern_ok = hidden global i32 undef
 __private_extern__ int private_extern_ok __attribute__((loader_uninitialized));
--- clang/test/CodeGenCXX/attr-loader-uninitialized.cpp
+++ clang/test/CodeGenCXX/attr-loader-uninitialized.cpp
@@ -21,9 +21,9 @@ class trivial
 // CHECK: @ut = global %class.trivial undef
 trivial ut [[clang::loader_uninitialized]];
 
-// CHECK: @arr = global [32 x double] undef, align 16
+// CHECK: @arr = global [32 x double] undef
 double arr[32] __attribute__((loader_uninitialized));
 
 // Defining as arr2[] [[clang..]] raises the error: attribute cannot be applied to types
-// CHECK: @arr2 = global [4 x double] undef, align 16
+// CHECK: @arr2 = global [4 x double] undef
 double arr2 [[clang::loader_uninitialized]] [4];
