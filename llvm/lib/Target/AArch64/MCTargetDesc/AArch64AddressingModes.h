8b3b04eb41647bf3166a09df2e1eb7964d525592
Eric Christopher <echristo@gmail.com>
2020-03-16 17:39:01 -0700

    Make isValidImmForSVEVecImmAddrMode inline static rather than just static.
    Fixes -Werror builds.

--- llvm/lib/Target/AArch64/MCTargetDesc/AArch64AddressingModes.h
+++ llvm/lib/Target/AArch64/MCTargetDesc/AArch64AddressingModes.h
@@ -839,7 +839,6 @@ inline static bool isAnyMOVWMovAlias(uint64_t Value, int RegWidth) {
 
   return isAnyMOVZMovAlias(Value, RegWidth);
 }
-
 /// Check if the value of \p OffsetInBytes can be used as an immediate for
 /// the gather load/prefetch and scatter store instructions with vector base and
 /// immediate offset addressing mode:
@@ -847,8 +846,8 @@ inline static bool isAnyMOVWMovAlias(uint64_t Value, int RegWidth) {
 ///      [<Zn>.[S|D]{, #<imm>}]
 ///
 /// where <imm> = sizeof(<T>) * k, for k = 0, 1, ..., 31.
-static bool isValidImmForSVEVecImmAddrMode(unsigned OffsetInBytes,
-                                           unsigned ScalarSizeInBytes) {
+inline static bool isValidImmForSVEVecImmAddrMode(unsigned OffsetInBytes,
+                                                  unsigned ScalarSizeInBytes) {
   // The immediate is not a multiple of the scalar size.
   if (OffsetInBytes % ScalarSizeInBytes)
     return false;
