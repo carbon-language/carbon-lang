a678724401088997f1ed594f30935c6660bf0ac0
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-03-21 11:23:52 +0000

    Fix Wdocumentation warning. NFCI.

--- clang/lib/Sema/SemaDeclCXX.cpp
+++ clang/lib/Sema/SemaDeclCXX.cpp
@@ -6322,7 +6322,6 @@ static bool canPassInRegisters(Sema &S, CXXRecordDecl *D,
 ///
 /// \param DiagID the primary error to report.
 /// \param MD the overriding method.
-/// \param OEK which overrides to include as notes.
 static bool
 ReportOverrides(Sema &S, unsigned DiagID, const CXXMethodDecl *MD,
                 llvm::function_ref<bool(const CXXMethodDecl *)> Report) {
