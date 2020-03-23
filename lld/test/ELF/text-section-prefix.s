0a076f2660be13d2b5d0683f2dda2963317aecd3
Fangrui Song <maskray@google.com>
2020-03-23 09:27:49 -0700

    [ELF][test] Clean text-section-prefix.s

--- lld/test/ELF/text-section-prefix.s
+++ lld/test/ELF/text-section-prefix.s
@@ -1,39 +1,41 @@
 # REQUIRES: x86
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
-# RUN: ld.lld -z keep-text-section-prefix %t -o %t2
-# RUN: llvm-readelf -l %t2 | FileCheck %s
-# RUN: ld.lld %t -o %t3
-# RUN: llvm-readelf -l %t3 | FileCheck --check-prefix=CHECKNO %s
-# RUN: ld.lld -z nokeep-text-section-prefix %t -o %t4
-# RUN: llvm-readelf -l %t4 | FileCheck --check-prefix=CHECKNO %s
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t.o
+# RUN: ld.lld %t.o -o %t1
+# RUN: llvm-readelf -S %t1 | FileCheck --check-prefix=NOKEEP %s
+# RUN: ld.lld -z nokeep-text-section-prefix %t.o -o %t2
+# RUN: cmp %t1 %t2
 
-# CHECK: .text
-# CHECK: .text.hot
-# CHECK: .text.startup
-# CHECK: .text.exit
-# CHECK: .text.unlikely
-# CHECKNO: .text
-# CHECKNO-NOT: .text.hot
+# RUN: ld.lld -z keep-text-section-prefix %t.o -o %t.keep
+# RUN: llvm-readelf -S %t.keep | FileCheck --check-prefix=KEEP %s
 
+# KEEP:      [ 1] .text
+# KEEP-NEXT: [ 2] .text.hot
+# KEEP-NEXT: [ 3] .text.startup
+# KEEP-NEXT: [ 4] .text.exit
+# KEEP-NEXT: [ 5] .text.unlikely
+
+# NOKEEP:    [ 1] .text
+# NOKEEP-NOT:     .text
+
+# RUN: echo 'SECTIONS {}' > %t.lds
+# RUN: ld.lld -T %t.lds -z keep-text-section-prefix %t.o -o %t.script
+# RUN: llvm-readelf -S %t.script | FileCheck --check-prefix=KEEP %s
+
+.globl _start
 _start:
   ret
 
 .section .text.f,"ax"
-f:
   nop
 
 .section .text.hot.f_hot,"ax"
-f_hot:
   nop
 
 .section .text.startup.f_startup,"ax"
-f_startup:
   nop
 
 .section .text.exit.f_exit,"ax"
-f_exit:
   nop
 
 .section .text.unlikely.f_unlikely,"ax"
-f_unlikely:
   nop
