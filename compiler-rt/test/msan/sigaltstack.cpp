987f153929e813db1ea7e3354fd7a0b6b4adf4c0
Evgenii Stepanov <eugenis@google.com>
2020-03-23 16:15:58 -0700

    [msan] Fix sigaltstack false positive.
    struct stack_t on Linux x86_64 has internal padding which may be left
    uninitialized. The check should be replaced with multiple checks for
    individual fields of the struct. For now, remove the check altogether.

--- compiler-rt/test/msan/sigaltstack.cpp
+++ compiler-rt/test/msan/sigaltstack.cpp
@@ -1,4 +1,4 @@
-// RUN: %clangxx_msan -O0 -g %s -o %t && not %run %t
+// RUN: %clangxx_msan -O0 -g %s -o %t && %run %t
 //
 #include <signal.h>
 #include <assert.h>
@@ -10,11 +10,6 @@ int main(void) {
 
   assert(sigaltstack(nullptr, &old_ss) == 0);
   __msan_check_mem_is_initialized(&old_ss, sizeof(stack_t));
-
-  stack_t ss;
-  sigaltstack(&ss, nullptr);
-// CHECK: WARNING: MemorySanitizer: use-of-uninitialized-value
-// CHECK:    in main {{.*}}sigaltstack.cpp:15
 
   return 0;
 }
