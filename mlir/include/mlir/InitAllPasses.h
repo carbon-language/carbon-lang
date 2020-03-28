9dd92a569721a3a37e32387b6677a2c023c32eea
Mehdi Amini <joker.eph@gmail.com>
2020-03-28 03:49:53 +0000

    Remove spurious duplicated header include (NFC)

--- mlir/include/mlir/InitAllPasses.h
+++ mlir/include/mlir/InitAllPasses.h
@@ -26,7 +26,6 @@
 #include "mlir/Conversion/StandardToSPIRV/ConvertStandardToSPIRVPass.h"
 #include "mlir/Dialect/Affine/Passes.h"
 #include "mlir/Dialect/FxpMathOps/Passes.h"
-#include "mlir/Dialect/FxpMathOps/Passes.h"
 #include "mlir/Dialect/GPU/Passes.h"
 #include "mlir/Dialect/LLVMIR/Transforms/LegalizeForExport.h"
 #include "mlir/Dialect/Linalg/Passes.h"
