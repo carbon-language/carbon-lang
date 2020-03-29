0175999805cf05d91c8a127ebd8c9d54a640abe9
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-03-29 10:32:03 -0400

    [AMDGPU] Add options -mamdgpu-ieee -mno-amdgpu-ieee
    AMDGPU backend need to know whether floating point opcodes that support exception
    flag gathering quiet and propagate signaling NaN inputs per IEEE754-2008, which is
    conveyed by a function attribute "amdgpu-ieee". "amdgpu-ieee"="false" turns this off.
    Without this function attribute backend assumes it is on for compute functions.
    
    -mamdgpu-ieee and -mno-amdgpu-ieee are added to Clang to control this function attribute.
    By default it is on. -mno-amdgpu-ieee requires -fno-honor-nans or equivalent.
    
    Reviewed by: Matt Arsenault
    
    Differential Revision: https://reviews.llvm.org/D77013

--- clang/lib/CodeGen/TargetInfo.cpp
+++ clang/lib/CodeGen/TargetInfo.cpp
@@ -9166,6 +9166,9 @@ void AMDGPUTargetCodeGenInfo::setTargetAttributes(
 
   if (M.getContext().getTargetInfo().allowAMDGPUUnsafeFPAtomics())
     F->addFnAttr("amdgpu-unsafe-fp-atomics", "true");
+
+  if (!getABIInfo().getCodeGenOpts().EmitIEEENaNCompliantInsts)
+    F->addFnAttr("amdgpu-ieee", "false");
 }
 
 unsigned AMDGPUTargetCodeGenInfo::getOpenCLKernelCallingConv() const {
