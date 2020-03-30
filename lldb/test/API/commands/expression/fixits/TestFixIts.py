064ab22fb29be673fa2ac4ab3b28cc30f9371e9c
Raphael Isemann <teemperor@gmail.com>
2020-03-30 09:41:06 +0200

    [lldb] Run TestFixIts on non-Darwin platforms
    This test also passes on my Linux machine, so this seems too strict.

--- lldb/test/API/commands/expression/fixits/TestFixIts.py
+++ lldb/test/API/commands/expression/fixits/TestFixIts.py
@@ -23,7 +23,6 @@ class ExprCommandWithFixits(TestBase):
         self.assertEqual(result, lldb.eReturnStatusSuccessFinishResult, "The expression was successful.")
         self.assertTrue("Fix-it applied" in ret_val.GetError(), "Found the applied FixIt.")
 
-    @skipUnlessDarwin
     def test_with_target(self):
         """Test calling expressions with errors that can be fixed by the FixIts."""
         self.build()
