f6b2c003f360c3d0dcfe13d751805643f229f79b
Chris Jackson <chris.jackson@sony.com>
2020-03-30 15:08:26 +0100

    [DebugInfo]  Ensure that a demanded bits optimisation in InstCombine does not result in an incorrect debuginfo variable value
    - Add an additional salvage and a test.
    
    Reviewers: aprantl, djtodoro
    
    Differential Revision: https://reviews.llvm.org/D76854
    
    Bugzilla:  https://bugs.llvm.org/show_bug.cgi?id=44371

--- llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
+++ llvm/lib/Transforms/InstCombine/InstCombineSimplifyDemanded.cpp
@@ -87,6 +87,9 @@ bool InstCombiner::SimplifyDemandedBits(Instruction *I, unsigned OpNo,
   Value *NewVal = SimplifyDemandedUseBits(U.get(), DemandedMask, Known,
                                           Depth, I);
   if (!NewVal) return false;
+  if (Instruction* OpInst = dyn_cast<Instruction>(U))
+    salvageDebugInfoOrMarkUndef(*OpInst);
+    
   replaceUse(U, NewVal);
   return true;
 }
