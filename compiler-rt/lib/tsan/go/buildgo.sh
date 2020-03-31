41c135d6d2882ee6592a1c51a0221ef6def79ee4
Douglas Yung <douglas.yung@sony.com>
2020-03-31 09:26:43 -0700

    Fix buildgo.sh script to fix linux build failure due to changes in ad1466f8cbc5.
    The above change used a binary literal that is not supported in c++11 mode when
    using gcc. It was formalized into the c++14 standard and works when using that
    mode to compile, so change the script to use c++14 instead.
    
    Reviewed by: dvyukov
    
    Differential Revision: https://reviews.llvm.org/D77111

--- compiler-rt/lib/tsan/go/buildgo.sh
+++ compiler-rt/lib/tsan/go/buildgo.sh
@@ -149,7 +149,7 @@ for F in $SRCS; do
 	cat $F >> $DIR/gotsan.cpp
 done
 
-FLAGS=" -I../rtl -I../.. -I../../sanitizer_common -I../../../include -std=c++11 -Wall -fno-exceptions -fno-rtti -DSANITIZER_GO=1 -DSANITIZER_DEADLOCK_DETECTOR_VERSION=2 $OSCFLAGS $ARCHCFLAGS"
+FLAGS=" -I../rtl -I../.. -I../../sanitizer_common -I../../../include -std=c++14 -Wall -fno-exceptions -fno-rtti -DSANITIZER_GO=1 -DSANITIZER_DEADLOCK_DETECTOR_VERSION=2 $OSCFLAGS $ARCHCFLAGS"
 DEBUG_FLAGS="$FLAGS -DSANITIZER_DEBUG=1 -g"
 FLAGS="$FLAGS -DSANITIZER_DEBUG=0 -O3 -fomit-frame-pointer"
 if [ "$SUFFIX" = "linux_ppc64le" ]; then
