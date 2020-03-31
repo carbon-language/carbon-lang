e26eece1e2e7d7e6fbadb1207afef9b6353b5330
Med Ismail Bennani <medismail.bennani@gmail.com>
2020-03-31 02:33:42 +0200

    [lldb/DWARF] Fix evaluator crash when accessing empty stack.
    This patch fixes a crash that happens on the DWARF expression evaluator
    when trying to access the top of the stack while it's empty.
    
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- lldb/source/Expression/DWARFExpression.cpp
+++ lldb/source/Expression/DWARFExpression.cpp
@@ -2318,6 +2318,12 @@ bool DWARFExpression::Evaluate(
     // rather is a constant value.  The value from the top of the stack is the
     // value to be used.  This is the actual object value and not the location.
     case DW_OP_stack_value:
+      if (stack.empty()) {
+        if (error_ptr)
+          error_ptr->SetErrorString(
+              "Expression stack needs at least 1 item for DW_OP_stack_value.");
+        return false;
+      }
       stack.back().SetValueType(Value::eValueTypeScalar);
       break;
 
