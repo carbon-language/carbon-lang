ec11c5615a7c153a68d098903edebbb92719f5f9
Adrian Prantl <aprantl@apple.com>
2020-03-31 14:58:25 -0700

    Replace uint32_t with typedef (NFC)

--- lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.h
+++ lldb/source/Plugins/TypeSystem/Clang/TypeSystemClang.h
@@ -61,7 +61,7 @@ public:
   TypePayloadClang() = default;
   explicit TypePayloadClang(bool is_complete_objc_class);
   explicit TypePayloadClang(uint32_t opaque_payload) : m_payload(opaque_payload) {}
-  operator uint32_t() { return m_payload; }
+  operator Type::Payload() { return m_payload; }
 
   static constexpr unsigned ObjCClassBit = 1 << 31;
   bool IsCompleteObjCClass() { return Flags(m_payload).Test(ObjCClassBit); }
