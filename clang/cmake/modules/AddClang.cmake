ae044c5b0caa095602b6ef4cca40d57efc26a8f6
Stephen Neuendorffer <stephen.neuendorffer@xilinx.com>
2020-03-31 11:45:59 -0700

    [CMAKE] Plumb include_directories() into tablegen()
    Previously, the tablegen() cmake command, which defines custom
    commands for running tablegen, included several hardcoded paths.  This
    becomes unwieldy as there are more users for which these paths are
    insufficient.  For most targets, cmake uses include_directories() and
    the INCLUDE_DIRECTORIES directory property to specify include paths.
    This change picks up the INCLUDE_DIRECTORIES property and adds it
    to the include path used when running tablegen.  As a side effect, this
    allows us to remove several hard coded paths to tablegen that are redundant
    with specified include_directories().
    
    I haven't removed the hardcoded path to CMAKE_CURRENT_SOURCE_DIR, which
    seems generically useful.  There are several users in clang which apparently
    don't have the current directory as an include_directories().  This could
    be considered separately.
    
    Differential Revision: https://reviews.llvm.org/D77156

--- clang/cmake/modules/AddClang.cmake
+++ clang/cmake/modules/AddClang.cmake
@@ -17,7 +17,7 @@ function(clang_tablegen)
     message(FATAL_ERROR "SOURCE source-file required by clang_tablegen")
   endif()
 
-  set( CLANG_TABLEGEN_ARGUMENTS -I ${CLANG_SOURCE_DIR}/include )
+  set( CLANG_TABLEGEN_ARGUMENTS "" )
   set( LLVM_TARGET_DEFINITIONS ${CTG_SOURCE} )
   tablegen(CLANG ${CTG_UNPARSED_ARGUMENTS} ${CLANG_TABLEGEN_ARGUMENTS})
 
