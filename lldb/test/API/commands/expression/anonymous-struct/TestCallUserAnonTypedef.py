fa1b602ee6c3bd3dc245cbafe7534be5381e71e7
Raphael Isemann <teemperor@gmail.com>
2020-04-01 09:45:20 +0200

    [lldb][NFC] Modernize TestCallUserAnonTypedef

--- lldb/test/API/commands/expression/anonymous-struct/TestCallUserAnonTypedef.py
+++ lldb/test/API/commands/expression/anonymous-struct/TestCallUserAnonTypedef.py
@@ -6,21 +6,14 @@ untagged structures.
 Ticket: https://llvm.org/bugs/show_bug.cgi?id=26790
 """
 
-
 import lldb
 
 from lldbsuite.test.decorators import *
 from lldbsuite.test.lldbtest import *
 from lldbsuite.test import lldbutil
 
-
 class TestExprLookupAnonStructTypedef(TestBase):
     mydir = TestBase.compute_mydir(__file__)
-
-    def setUp(self):
-        TestBase.setUp(self)
-        # Find the breakpoint
-        self.line = line_number('main.cpp', '// lldb testsuite break')
 
     @expectedFailureAll(
         oslist=['linux'],
@@ -29,16 +22,5 @@ class TestExprLookupAnonStructTypedef(TestBase):
     def test(self):
         """Test typedeffed untagged struct arguments for function call expressions"""
         self.build()
-
-        self.runCmd("file "+self.getBuildArtifact("a.out"),
-                    CURRENT_EXECUTABLE_SET)
-        lldbutil.run_break_set_by_file_and_line(
-            self,
-            "main.cpp",
-            self.line,
-            num_expected_locations=-1,
-            loc_exact=True
-        )
-
-        self.runCmd("run", RUN_SUCCEEDED)
+        lldbutil.run_to_source_breakpoint(self, "// break here", lldb.SBFileSpec("main.cpp"))
         self.expect_expr("multiply(&s)", result_type="double", result_value="1")
