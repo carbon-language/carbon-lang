59d10dc703357631e9a13000ef15930b86a93335
Diogo Sampaio <diogo.sampaio@arm.com>
2020-04-05 12:39:59 +0100

    [ARM] add ARMv8.6-A Activity monitors virtualization extension
    Summary:
    This patch upstreams v8.6A activity monitors virtualization
    assembler support, which consists of 32 new system
    registers (two groups, each with 16 numbered registers).
    
    See ARMv8.6-AMU in the Arm Architecture Reference Manual Armv8 for more
    information.
    
    Reviewers: t.p.northover, rengolin, SjoerdMeijer, ab, john.brawn, ostannard
    
    Reviewed By: ostannard
    
    Subscribers: LukeGeeson, dnsampaio, ostannard, kristof.beyls, hiraditya, danielkiss, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D76998

--- llvm/lib/Target/AArch64/AArch64Subtarget.h
+++ llvm/lib/Target/AArch64/AArch64Subtarget.h
@@ -147,6 +147,7 @@ protected:
 
   // Armv8.6-A Extensions
   bool HasBF16 = false;
+  bool HasAMVS = false;
 
   // Arm SVE2 extensions
   bool HasSVE2AES = false;
@@ -451,6 +452,7 @@ public:
   bool hasDIT() const { return HasDIT; }
   bool hasTRACEV8_4() const { return HasTRACEV8_4; }
   bool hasAM() const { return HasAM; }
+  bool hasAMVS() const { return HasAMVS; }
   bool hasSEL2() const { return HasSEL2; }
   bool hasPMU() const { return HasPMU; }
   bool hasTLB_RMI() const { return HasTLB_RMI; }
