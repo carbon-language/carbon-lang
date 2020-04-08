401fa5b493fc21fe3f56f502befe7b5532b06df6
Maksim Panchenko <maks@fb.com>
2020-04-07 22:10:12 -0700

    [BOLT] Further speedup ICF
    Summary:
    Further speedup ICF by applying stricter rules for congruent functions.
    While checking symbolic operands in congruent functions, consider
    operands congruent only if they are equal or reference functions
    with identical hashes, i.e. potentially foldable functions.
    Note that jump table operands are handled as a special case.
    
    (cherry picked from FBD20912054)

--- bolt/src/Passes/IdenticalCodeFolding.cpp
+++ bolt/src/Passes/IdenticalCodeFolding.cpp
@@ -147,14 +147,11 @@ bool isInstrEquivalentWith(const MCInst &InstA, const BinaryBasicBlock &BBA,
 /// Returns true if this function has identical code and CFG with
 /// the given function \p BF.
 ///
-/// If \p IgnoreSymbols is set to true, then symbolic operands
-/// that reference functions are ignored during the comparison.
-///
-/// If \p UseDFS is set to true, then compute DFS of each function and use
-/// is for CFG equivalency. Potentially it will help to catch more cases,
-/// but is slower.
+/// If \p CongruentSymbols is set to true, then symbolic operands that reference
+/// potentially identical but different functions are ignored during the
+/// comparison.
 bool isIdenticalWith(const BinaryFunction &A, const BinaryFunction &B,
-                     bool IgnoreSymbols, bool UseDFS) {
+                     bool CongruentSymbols) {
   assert(A.hasCFG() && B.hasCFG() && "both functions should have CFG");
 
   // Compare the two functions, one basic block at a time.
@@ -170,8 +167,8 @@ bool isIdenticalWith(const BinaryFunction &A, const BinaryFunction &B,
     return false;
 
   // Process both functions in either DFS or existing order.
-  const auto &OrderA = UseDFS ? A.dfs() : A.getLayout();
-  const auto &OrderB = UseDFS ? B.dfs() : B.getLayout();
+  const auto &OrderA = opts::UseDFS ? A.dfs() : A.getLayout();
+  const auto &OrderB = opts::UseDFS ? B.dfs() : B.getLayout();
 
   const auto &BC = A.getBinaryContext();
 
@@ -199,78 +196,77 @@ bool isIdenticalWith(const BinaryFunction &A, const BinaryFunction &B,
     auto I = BB->begin(), E = BB->end();
     auto OtherI = OtherBB->begin(), OtherE = OtherBB->end();
     while (I != E && OtherI != OtherE) {
+      // Compare symbols.
+      auto AreSymbolsIdentical = [&] (const MCSymbol *SymbolA,
+                                      const MCSymbol *SymbolB) {
+        if (SymbolA == SymbolB)
+          return true;
 
-      bool Identical;
-      if (IgnoreSymbols) {
-        Identical =
-          isInstrEquivalentWith(*I, *BB, *OtherI, *OtherBB,
-                                [&](const MCSymbol *A, const MCSymbol *B) {
-                                  return true;
-                                });
-      } else {
-        // Compare symbols.
-        auto AreSymbolsIdentical = [&] (const MCSymbol *SymbolA,
-                                        const MCSymbol *SymbolB) {
-          if (SymbolA == SymbolB)
+        // All local symbols are considered identical since they affect a
+        // control flow and we check the control flow separately.
+        // If a local symbol is escaped, then the function (potentially) has
+        // multiple entry points and we exclude such functions from
+        // comparison.
+        if (SymbolA->isTemporary() && SymbolB->isTemporary())
+          return true;
+
+        // Compare symbols as functions.
+        uint64_t EntryIDA{0};
+        uint64_t EntryIDB{0};
+        const auto *FunctionA = BC.getFunctionForSymbol(SymbolA, &EntryIDA);
+        const auto *FunctionB = BC.getFunctionForSymbol(SymbolB, &EntryIDB);
+        if (FunctionA && EntryIDA)
+          FunctionA = nullptr;
+        if (FunctionB && EntryIDB)
+          FunctionB = nullptr;
+        if (FunctionA && FunctionB) {
+          // Self-referencing functions and recursive calls.
+          if (FunctionA == &A && FunctionB == &B)
             return true;
 
-          // All local symbols are considered identical since they affect a
-          // control flow and we check the control flow separately.
-          // If a local symbol is escaped, then the function (potentially) has
-          // multiple entry points and we exclude such functions from
-          // comparison.
-          if (SymbolA->isTemporary() && SymbolB->isTemporary())
-            return true;
+          // Functions with different hash values can never become identical,
+          // hence A and B are different.
+          if (CongruentSymbols)
+            return FunctionA->getHash() == FunctionB->getHash();
 
-          // Compare symbols as functions.
-          uint64_t EntryIDA{0};
-          uint64_t EntryIDB{0};
-          const auto *FunctionA = BC.getFunctionForSymbol(SymbolA, &EntryIDA);
-          const auto *FunctionB = BC.getFunctionForSymbol(SymbolB, &EntryIDB);
-          if (FunctionA && EntryIDA)
-            FunctionA = nullptr;
-          if (FunctionB && EntryIDB)
-            FunctionB = nullptr;
-          if (FunctionA && FunctionB) {
-            // Self-referencing functions and recursive calls.
-            if (FunctionA == &A && FunctionB == &B)
-              return true;
-            return FunctionA == FunctionB;
-          }
+          return FunctionA == FunctionB;
+        }
 
-          // Check if symbols are jump tables.
-          auto *SIA = BC.getBinaryDataByName(SymbolA->getName());
-          if (!SIA)
-            return false;
-          auto *SIB = BC.getBinaryDataByName(SymbolB->getName());
-          if (!SIB)
-            return false;
+        // One of the symbols represents a function, the other one does not.
+        if (FunctionA != FunctionB) {
+          return false;
+        }
 
-          assert((SIA->getAddress() != SIB->getAddress()) &&
-                 "different symbols should not have the same value");
+        // Check if symbols are jump tables.
+        auto *SIA = BC.getBinaryDataByName(SymbolA->getName());
+        if (!SIA)
+          return false;
+        auto *SIB = BC.getBinaryDataByName(SymbolB->getName());
+        if (!SIB)
+          return false;
 
-          const auto *JumpTableA =
-             A.getJumpTableContainingAddress(SIA->getAddress());
-          if (!JumpTableA)
-            return false;
+        assert((SIA->getAddress() != SIB->getAddress()) &&
+               "different symbols should not have the same value");
 
-          const auto *JumpTableB =
-             B.getJumpTableContainingAddress(SIB->getAddress());
-          if (!JumpTableB)
-            return false;
+        const auto *JumpTableA =
+           A.getJumpTableContainingAddress(SIA->getAddress());
+        if (!JumpTableA)
+          return false;
 
-          if ((SIA->getAddress() - JumpTableA->getAddress()) !=
-              (SIB->getAddress() - JumpTableB->getAddress()))
-            return false;
+        const auto *JumpTableB =
+           B.getJumpTableContainingAddress(SIB->getAddress());
+        if (!JumpTableB)
+          return false;
 
-          return equalJumpTables(*JumpTableA, *JumpTableB, A, B);
-        };
+        if ((SIA->getAddress() - JumpTableA->getAddress()) !=
+            (SIB->getAddress() - JumpTableB->getAddress()))
+          return false;
 
-        Identical = isInstrEquivalentWith(*I, *BB, *OtherI, *OtherBB,
-                                          AreSymbolsIdentical);
-      }
+        return equalJumpTables(*JumpTableA, *JumpTableB, A, B);
+      };
 
-      if (!Identical) {
+      if(!isInstrEquivalentWith(*I, *BB, *OtherI, *OtherBB,
+                                AreSymbolsIdentical)) {
         return false;
       }
 
@@ -306,11 +302,18 @@ struct KeyHash {
   }
 };
 
+/// Identify two congruent functions. Two functions are considered congruent,
+/// if they are identical/equal except for some of their instruction operands
+/// that reference potentially identical functions, i.e. functions that could
+/// be folded later. Congruent functions are candidates for folding in our
+/// iterative ICF algorithm.
+///
+/// Congruent functions are required to have identical hash.
 struct KeyCongruent {
   bool operator()(const BinaryFunction *A, const BinaryFunction *B) const {
     if (A == B)
       return true;
-    return isIdenticalWith(*A, *B, /*IgnoreSymbols=*/true, opts::UseDFS);
+    return isIdenticalWith(*A, *B, /*CongruentSymbols=*/true);
   }
 };
 
@@ -318,7 +321,7 @@ struct KeyEqual {
   bool operator()(const BinaryFunction *A, const BinaryFunction *B) const {
     if (A == B)
       return true;
-    return isIdenticalWith(*A, *B, /*IgnoreSymbols=*/false, opts::UseDFS);
+    return isIdenticalWith(*A, *B, /*CongruentSymbols=*/false);
   }
 };
 
@@ -355,7 +358,7 @@ std::string hashSymbol(BinaryContext &BC, const MCSymbol &Symbol) {
   if (!ErrorOrValue)
     return HashString;
 
-  // Ignore jump tables references.
+  // Ignore jump table references.
   if (BC.getJumpTableContainingAddress(*ErrorOrValue))
     return HashString;
 
