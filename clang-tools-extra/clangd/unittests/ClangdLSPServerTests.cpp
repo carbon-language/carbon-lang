1229245df7c7323fa916054bf20edc01b3606dd2
Sam McCall <sam.mccall@gmail.com>
2020-04-09 02:02:29 +0200

    [clangd] Set up machinery for gtests of ClangdLSPServer.
    Summary:
    This is going to be needed to test e.g. diagnostics regeneration on
    didSave where files changed on disk. Coordinating such changes is too
    hard in lit tests.
    
    Reviewers: kadircet
    
    Subscribers: mgorny, ilya-biryukov, MaskRay, jkorous, arphaman, usaxena95, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D77766

---
+++ clang-tools-extra/clangd/unittests/ClangdLSPServerTests.cpp
@@ -0,0 +1,131 @@
+//===-- ClangdLSPServerTests.cpp ------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "Annotations.h"
+#include "ClangdLSPServer.h"
+#include "CodeComplete.h"
+#include "LSPClient.h"
+#include "Logger.h"
+#include "Protocol.h"
+#include "TestFS.h"
+#include "refactor/Rename.h"
+#include "llvm/Support/JSON.h"
+#include "llvm/Testing/Support/SupportHelpers.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace clang {
+namespace clangd {
+namespace {
+
+MATCHER_P(DiagMessage, M, "") {
+  if (const auto *O = arg.getAsObject()) {
+    if (const auto Msg = O->getString("message"))
+      return *Msg == M;
+  }
+  return false;
+}
+
+class LSPTest : public ::testing::Test, private clangd::Logger {
+protected:
+  LSPTest() : LogSession(*this) {}
+
+  LSPClient &start() {
+    EXPECT_FALSE(Server.hasValue()) << "Already initialized";
+    Server.emplace(Client.transport(), FS, CCOpts, RenameOpts,
+                   /*CompileCommandsDir=*/llvm::None, /*UseDirBasedCDB=*/false,
+                   /*ForcedOffsetEncoding=*/llvm::None, Opts);
+    ServerThread.emplace([&] { EXPECT_TRUE(Server->run()); });
+    Client.call("initialize", llvm::json::Object{});
+    return Client;
+  }
+
+  void stop() {
+    assert(Server);
+    Client.call("shutdown", nullptr);
+    Client.notify("exit", nullptr);
+    Client.stop();
+    ServerThread->join();
+    Server.reset();
+    ServerThread.reset();
+  }
+
+  ~LSPTest() {
+    if (Server)
+      stop();
+  }
+
+  MockFSProvider FS;
+  CodeCompleteOptions CCOpts;
+  RenameOptions RenameOpts;
+  ClangdServer::Options Opts = ClangdServer::optsForTest();
+
+private:
+  // Color logs so we can distinguish them from test output.
+  void log(Level L, const llvm::formatv_object_base &Message) override {
+    raw_ostream::Colors Color;
+    switch (L) {
+    case Level::Verbose:
+      Color = raw_ostream::BLUE;
+      break;
+    case Level::Error:
+      Color = raw_ostream::RED;
+      break;
+    default:
+      Color = raw_ostream::YELLOW;
+      break;
+    }
+    std::lock_guard<std::mutex> Lock(LogMu);
+    (llvm::outs().changeColor(Color) << Message << "\n").resetColor();
+  }
+  std::mutex LogMu;
+
+  LoggingSession LogSession;
+  llvm::Optional<ClangdLSPServer> Server;
+  llvm::Optional<std::thread> ServerThread;
+  LSPClient Client;
+};
+
+TEST_F(LSPTest, GoToDefinition) {
+  Annotations Code(R"cpp(
+    int [[fib]](int n) {
+      return n >= 2 ? ^fib(n - 1) + fib(n - 2) : 1;
+    }
+  )cpp");
+  auto &Client = start();
+  Client.didOpen("foo.cpp", Code.code());
+  auto &Def = Client.call("textDocument/definition",
+                          llvm::json::Object{
+                              {"textDocument", Client.documentID("foo.cpp")},
+                              {"position", Code.point()},
+                          });
+  llvm::json::Value Want = llvm::json::Array{llvm::json::Object{
+      {"uri", Client.uri("foo.cpp")}, {"range", Code.range()}}};
+  EXPECT_EQ(Def.takeValue(), Want);
+}
+
+TEST_F(LSPTest, Diagnostics) {
+  auto &Client = start();
+  Client.didOpen("foo.cpp", "void main(int, char**);");
+  EXPECT_THAT(Client.diagnostics("foo.cpp"),
+              llvm::ValueIs(testing::ElementsAre(
+                  DiagMessage("'main' must return 'int' (fix available)"))));
+
+  Client.didChange("foo.cpp", "int x = \"42\";");
+  EXPECT_THAT(Client.diagnostics("foo.cpp"),
+              llvm::ValueIs(testing::ElementsAre(
+                  DiagMessage("Cannot initialize a variable of type 'int' with "
+                              "an lvalue of type 'const char [3]'"))));
+
+  Client.didClose("foo.cpp");
+  EXPECT_THAT(Client.diagnostics("foo.cpp"), llvm::ValueIs(testing::IsEmpty()));
+}
+
+} // namespace
+} // namespace clangd
+} // namespace clang
---
+++ clang-tools-extra/clangd/unittests/LSPClient.h
@@ -0,0 +1,82 @@
+//===-- LSPClient.h - Helper for ClangdLSPServer tests ----------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include <condition_variable>
+#include <deque>
+#include <llvm/ADT/Optional.h>
+#include <llvm/Support/Error.h>
+#include <llvm/Support/JSON.h>
+#include <mutex>
+
+namespace clang {
+namespace clangd {
+class Transport;
+
+// A client library for talking to ClangdLSPServer in tests.
+// Manages serialization of messages, pairing requests/repsonses, and implements
+// the Transport abstraction.
+class LSPClient {
+  class TransportImpl;
+  std::unique_ptr<TransportImpl> T;
+
+public:
+  // Represents the result of an LSP call: a promise for a result or error.
+  class CallResult {
+  public:
+    ~CallResult();
+    // Blocks up to 10 seconds for the result to be ready.
+    // Records a test failure if there was no reply.
+    llvm::Expected<llvm::json::Value> take();
+    // Like take(), but records a test failure if the result was an error.
+    llvm::json::Value takeValue();
+
+  private:
+    // Should be called once to provide the value.
+    void set(llvm::Expected<llvm::json::Value> V);
+
+    llvm::Optional<llvm::Expected<llvm::json::Value>> Value;
+    std::mutex Mu;
+    std::condition_variable CV;
+
+    friend TransportImpl; // Calls set().
+  };
+
+  LSPClient();
+  ~LSPClient();
+  LSPClient(LSPClient &&) = delete;
+  LSPClient &operator=(LSPClient &&) = delete;
+
+  // Enqueue an LSP method call, returns a promise for the reply. Threadsafe.
+  CallResult &call(llvm::StringRef Method, llvm::json::Value Params);
+  // Enqueue an LSP notification. Threadsafe.
+  void notify(llvm::StringRef Method, llvm::json::Value Params);
+  // Returns matching notifications since the last call to takeNotifications.
+  std::vector<llvm::json::Value> takeNotifications(llvm::StringRef Method);
+  // The transport is shut down after all pending messages are sent.
+  void stop();
+
+  // Shorthand for common LSP methods. Relative paths are passed to testPath().
+  static llvm::json::Value uri(llvm::StringRef Path);
+  static llvm::json::Value documentID(llvm::StringRef Path);
+  void didOpen(llvm::StringRef Path, llvm::StringRef Content);
+  void didChange(llvm::StringRef Path, llvm::StringRef Content);
+  void didClose(llvm::StringRef Path);
+  // Blocks until the server is idle (using the 'sync' protocol extension).
+  void sync();
+  // sync()s to ensure pending diagnostics arrive, and returns the newest set.
+  llvm::Optional<std::vector<llvm::json::Value>>
+  diagnostics(llvm::StringRef Path);
+
+  // Get the transport used to connect this client to a ClangdLSPServer.
+  Transport &transport();
+
+private:
+};
+
+} // namespace clangd
+} // namespace clang
