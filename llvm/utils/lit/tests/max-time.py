5925c4a0ff720fa85a83a44f0358da4076297651
Julian Lettner <julian.lettner@apple.com>
2020-04-10 16:22:00 -0700

    [lit] Increase sleep time in timeout test
    Fixup for cbe42a9d5fa.  Increase values for testing the overall lit
    timeout (--max-time) which wasn't enough for the test to complete on
    very slow build bots.

--- llvm/utils/lit/tests/max-time.py
+++ llvm/utils/lit/tests/max-time.py
@@ -1,6 +1,6 @@
 # Test overall lit timeout (--max-time).
 #
-# RUN: %{lit} %{inputs}/max-time --max-time=1 2>&1  |  FileCheck %s
+# RUN: %{lit} %{inputs}/max-time --max-time=5 2>&1  |  FileCheck %s
 
 # CHECK: reached timeout, skipping remaining tests
 # CHECK: Skipped Tests  : 1
