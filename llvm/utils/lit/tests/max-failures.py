15000650a6d2e501eb521e0d94e6ea6fa6b16609
Julian Lettner <julian.lettner@apple.com>
2020-04-10 17:12:54 -0700

    [lit] Fix tests on Windows
    max-time.py:
      Windows does not have a native `sleep` command, use `time.sleep()` in
      Python instead.
    
    max-failures.py:
      The max-failure test reused the shtest-shell test inputs instead of
      defining its own "test domain".  However, the output of this
      shtest-shell "test domain" is slightly different on Windows, which now
      bites us since we made the max-failures test stricter.  Let's define
      our own "max failures" test domain.

--- llvm/utils/lit/tests/max-failures.py
+++ llvm/utils/lit/tests/max-failures.py
@@ -6,18 +6,17 @@
 # RUN: not %{lit} --max-failures=0 -j 1 %{inputs}/max-failures 2>> %t.out
 # RUN: FileCheck < %t.out %s
 #
-# END.
 
 # CHECK-NOT: reached maximum number of test failures
 # CHECK-NOT: Skipped Tests
-# CHECK: Unexpected Failures: 35
+# CHECK: Unexpected Failures: 3
 
 # CHECK: reached maximum number of test failures, skipping remaining tests
-# CHECK: Skipped Tests      : 41
-# CHECK: Unexpected Failures:  1
+# CHECK: Skipped Tests      : 2
+# CHECK: Unexpected Failures: 1
 
 # CHECK: reached maximum number of test failures, skipping remaining tests
-# CHECK: Skipped Tests      : 40
-# CHECK: Unexpected Failures:  2
+# CHECK: Skipped Tests      : 1
+# CHECK: Unexpected Failures: 2
 
 # CHECK: error: argument --max-failures: requires positive integer, but found '0'
