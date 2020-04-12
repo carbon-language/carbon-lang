38c271f6f921e30855c77666dbfab7e9c456e3d1
Fangrui Song <maskray@google.com>
2020-04-12 08:14:37 -0700

    [MC][test] Reorganize macro tests
    * Add locations
    * Use FileCheck --match-full-lines --strict-whitespace
    * Use CHECK-NEXT:
    * Merge related tests
    * Delete redundant tests
    * Improve test coverage
    
    Differential Revision: https://reviews.llvm.org/D77981

--- llvm/test/MC/AsmParser/macro-def-in-instantiation.s
+++ llvm/test/MC/AsmParser/macro-def-in-instantiation.s
@@ -1,33 +1,30 @@
-// RUN: llvm-mc -triple x86_64-apple-darwin10 %s | FileCheck %s
+# RUN: llvm-mc -triple=x86_64 %s | FileCheck %s
+# RUN: llvm-mc -triple=x86_64-apple-darwin10 %s | FileCheck %s
 
-.macro .make_macro
-$0 $1
-$2 $3
-$4
-.endmacro
+.macro make_macro a, b, c ,d ,e, f
+\a \b \c
+\d \e
+\f
+.endm
+make_macro .macro,mybyte,a,.byte,\a,.endm
+# CHECK: .byte 42
+mybyte 42
 
-.make_macro .macro,.mybyte,.byte,$0,.endmacro
-
-.data
-// CHECK: .byte 10
-.mybyte 10
-
-// PR18599
+# PR18599
 .macro macro_a
+ .macro macro_b
+  .byte 10
+  .macro macro_c
+  .endm
 
-.macro macro_b
-.byte 10
-.macro macro_c
+  macro_c
+  .purgem macro_c
+ .endm
+
+ macro_b
 .endm
 
-macro_c
-.purgem macro_c
-.endm
-
-macro_b
-.endm
-
+# CHECK: .byte 10
+# CHECK: .byte 10
 macro_a
 macro_b
-// CHECK: .byte 10
-// CHECK: .byte 10
--- llvm/test/MC/AsmParser/macro-duplicate-params-names-err.s
+++
@@ -1,8 +0,0 @@
-// REQUIRES: default_triple
-// RUN: not llvm-mc %s 2> %t
-// RUN: FileCheck < %t %s
-
-.macro M a a
-.endm
-
-// CHECK: macro 'M' has multiple parameters named 'a'
