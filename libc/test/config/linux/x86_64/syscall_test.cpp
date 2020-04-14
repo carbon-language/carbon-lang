54d13b5b2d927652429b0538e27dcd28b783a302
Alex Brachet <alexbrachetmialot@gmail.com>
2020-04-14 03:27:10 -0400

    [libc] Remove <functional> dependency in syscall_test.cpp
    Summary: Create self contained functional header which has a type similar to `std::function`
    
    Reviewers: sivachandra, PaulkaToast
    
    Reviewed By: sivachandra
    
    Subscribers: mgorny, tschuett, libc-commits
    
    Differential Revision: https://reviews.llvm.org/D77948

--- libc/test/config/linux/x86_64/syscall_test.cpp
+++ libc/test/config/linux/x86_64/syscall_test.cpp
@@ -9,36 +9,36 @@
 #include "config/linux/syscall.h"
 #include "utils/UnitTest/Test.h"
 
-#include <functional>
+#include "utils/CPP/Functional.h"
 
 TEST(X86_64_SyscallTest, APITest) {
   // We only do a signature test here. Actual functionality tests are
   // done by the unit tests of the syscall wrappers like mmap.
 
-  std::function<long(long)> f([](long n) { return __llvm_libc::syscall(n); });
-  std::function<long(long, long)> f1(
+  using __llvm_libc::cpp::function;
+
+  function<long(long)> f([](long n) { return __llvm_libc::syscall(n); });
+  function<long(long, long)> f1(
       [](long n, long a1) { return __llvm_libc::syscall(n, a1); });
-  std::function<long(long, long, long)> f2(
+  function<long(long, long, long)> f2(
       [](long n, long a1, long a2) { return __llvm_libc::syscall(n, a1, a2); });
-  std::function<long(long, long, long, long)> f3(
+  function<long(long, long, long, long)> f3(
       [](long n, long a1, long a2, long a3) {
         return __llvm_libc::syscall(n, a1, a2, a3);
       });
-  std::function<long(long, long, long, long, long)> f4(
+  function<long(long, long, long, long, long)> f4(
       [](long n, long a1, long a2, long a3, long a4) {
         return __llvm_libc::syscall(n, a1, a2, a3, a4);
       });
-  std::function<long(long, long, long, long, long, long)> f5(
+  function<long(long, long, long, long, long, long)> f5(
       [](long n, long a1, long a2, long a3, long a4, long a5) {
         return __llvm_libc::syscall(n, a1, a2, a3, a4, a5);
       });
-  std::function<long(long, long, long, long, long, long, long)> f6(
+  function<long(long, long, long, long, long, long, long)> f6(
       [](long n, long a1, long a2, long a3, long a4, long a5, long a6) {
         return __llvm_libc::syscall(n, a1, a2, a3, a4, a5, a6);
       });
 
-  std::function<long(long, void *)> notLongType(
-      [](long n, void *a1) {
-        return __llvm_libc::syscall(n, a1);
-      });
+  function<long(long, void *)> notLongType(
+      [](long n, void *a1) { return __llvm_libc::syscall(n, a1); });
 }
