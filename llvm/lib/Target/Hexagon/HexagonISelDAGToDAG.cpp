2819e5de42e03f100c9a90c1328465e99d28ee5d
Ikhlas Ajbar <iajbar@quicinc.com>
2020-04-16 15:56:56 -0500

    [Hexagon] Handle instruction selection for select(I1,Q,Q)
    Lower select(I1,Q,Q) by converting vector predicate Q to vector register V,
    doing select(I1,V,V), and then converting the resulting V back to Q. Also,
    try to avoid creating such situations in the first place.

--- llvm/lib/Target/Hexagon/HexagonISelDAGToDAG.cpp
+++ llvm/lib/Target/Hexagon/HexagonISelDAGToDAG.cpp
@@ -1176,6 +1176,9 @@ void HexagonDAGToDAGISel::ppHoistZextI1(std::vector<SDNode*> &&Nodes) {
       EVT UVT = U->getValueType(0);
       if (!UVT.isSimple() || !UVT.isInteger() || UVT.getSimpleVT() == MVT::i1)
         continue;
+      // Do not generate select for all i1 vector type.
+      if (UVT.isVector() && UVT.getVectorElementType() == MVT::i1)
+        continue;
       if (isMemOPCandidate(N, U))
         continue;
 
