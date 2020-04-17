9eaf0abebff9c61fa01c6ca69cbc74b1464efe14
Jonas Devlieghere <jonas@devlieghere.com>
2020-04-16 17:50:56 -0700

    Revert "[lldb/Utility] Provide a stringify_append overload for function pointers."
    Temporarily reverts commit d10386e1779599d217b5b849a079f29dfbe17024
    because it breaks the Windows build. MSVC complains about an ambiguous
    call to an overloaded function.

--- lldb/include/lldb/Utility/ReproducerInstrumentation.h
+++ lldb/include/lldb/Utility/ReproducerInstrumentation.h
@@ -34,17 +34,12 @@ inline void stringify_append(llvm::raw_string_ostream &ss, const T &t) {
 
 template <typename T>
 inline void stringify_append(llvm::raw_string_ostream &ss, T *t) {
-  ss << static_cast<void *>(t);
+  ss << reinterpret_cast<void *>(t);
 }
 
 template <typename T>
 inline void stringify_append(llvm::raw_string_ostream &ss, const T *t) {
-  ss << static_cast<const void *>(t);
-}
-
-template <typename T, typename... Args>
-inline void stringify_append(llvm::raw_string_ostream &ss, T (*t)(Args...)) {
-  ss << "function pointer";
+  ss << reinterpret_cast<const void *>(t);
 }
 
 template <>
