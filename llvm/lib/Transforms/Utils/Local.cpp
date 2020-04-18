e00cfe254d99629ec344031adfe1878a84f3b0b3
Craig Topper <craig.topper@gmail.com>
2020-04-18 11:57:50 -0700

    [Local] Simplify the alignment limits in getOrEnforceKnownAlignment. NFCI
    We previously clamped the trailing zero count to 31 bits. And
    then clamped the final alignment to MaximumAlignment which is
    1 << 29.
    
    This patch simplifies this to just clamp the trailing zero to
    29 using MaxAlignmentExponent.
    
    I was looking into changing this function to use Align/MaybeAlign
    and noticed this.
    
    Differential Revision: https://reviews.llvm.org/D78418

--- llvm/lib/Transforms/Utils/Local.cpp
+++ llvm/lib/Transforms/Utils/Local.cpp
@@ -1215,12 +1215,10 @@ unsigned llvm::getOrEnforceKnownAlignment(Value *V, unsigned PrefAlign,
 
   // Avoid trouble with ridiculously large TrailZ values, such as
   // those computed from a null pointer.
-  TrailZ = std::min(TrailZ, unsigned(sizeof(unsigned) * CHAR_BIT - 1));
+  // LLVM doesn't support alignments larger than (1 << MaxAlignmentExponent).
+  TrailZ = std::min(TrailZ, Value::MaxAlignmentExponent);
 
   unsigned Align = 1u << std::min(Known.getBitWidth() - 1, TrailZ);
-
-  // LLVM doesn't support alignments larger than this currently.
-  Align = std::min(Align, +Value::MaximumAlignment);
 
   if (PrefAlign > Align)
     Align = enforceKnownAlignment(V, Align, PrefAlign, DL);
