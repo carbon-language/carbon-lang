84aab8b77259e6bc37199d2004127d2720e96b2f
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-04-19 14:20:53 +0100

    X86SelectionDAGInfo.h - remove unnecessary includes and forward declarations. NFC.

--- llvm/lib/Target/X86/X86SelectionDAGInfo.h
+++ llvm/lib/Target/X86/X86SelectionDAGInfo.h
@@ -14,13 +14,8 @@
 #define LLVM_LIB_TARGET_X86_X86SELECTIONDAGINFO_H
 
 #include "llvm/CodeGen/SelectionDAGTargetInfo.h"
-#include "llvm/MC/MCRegisterInfo.h"
 
 namespace llvm {
-
-class X86TargetLowering;
-class X86TargetMachine;
-class X86Subtarget;
 
 class X86SelectionDAGInfo : public SelectionDAGTargetInfo {
   /// Returns true if it is possible for the base register to conflict with the
