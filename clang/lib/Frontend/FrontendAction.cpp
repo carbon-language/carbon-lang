d892eec710caae84099f38fdb89d32ca15a23c1a
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2020-04-21 15:30:54 -0700

    Reapply: Make header inclusion order from umbrella dirs deterministic
    Sort the headers by name before adding the includes in
    collectModuleHeaderIncludes. This makes the include order for building
    umbrellas deterministic across different filesystems and also guarantees
    that the ASTWriter always dump top headers in the same order.
    
    There's currently no good way to test for this behavior.
    
    This was first introduced in r289478 and reverted few times because of
    ASANifed test failures on open source bots (both from LLVM and Swift).
    
    Finally reproduced the problem in a Linux machine and use std::sort as a
    fix, since we are not dealing with POD-like types.
    
    rdar://problem/28116411

--- clang/lib/Frontend/FrontendAction.cpp
+++ clang/lib/Frontend/FrontendAction.cpp
@@ -364,6 +364,7 @@ static std::error_code collectModuleHeaderIncludes(
     llvm::sys::path::native(UmbrellaDir.Entry->getName(), DirNative);
 
     llvm::vfs::FileSystem &FS = FileMgr.getVirtualFileSystem();
+    SmallVector<std::pair<std::string, const FileEntry *>, 8> Headers;
     for (llvm::vfs::recursive_directory_iterator Dir(FS, DirNative, EC), End;
          Dir != End && !EC; Dir.increment(EC)) {
       // Check whether this entry has an extension typically associated with
@@ -394,13 +395,25 @@ static std::error_code collectModuleHeaderIncludes(
            ++It)
         llvm::sys::path::append(RelativeHeader, *It);
 
-      // Include this header as part of the umbrella directory.
-      Module->addTopHeader(*Header);
-      addHeaderInclude(RelativeHeader, Includes, LangOpts, Module->IsExternC);
+      std::string RelName = RelativeHeader.c_str();
+      Headers.push_back(std::make_pair(RelName, *Header));
     }
 
     if (EC)
       return EC;
+
+    // Sort header paths and make the header inclusion order deterministic
+    // across different OSs and filesystems.
+    llvm::sort(Headers.begin(), Headers.end(), [](
+      const std::pair<std::string, const FileEntry *> &LHS,
+      const std::pair<std::string, const FileEntry *> &RHS) {
+        return LHS.first < RHS.first;
+    });
+    for (auto &H : Headers) {
+      // Include this header as part of the umbrella directory.
+      Module->addTopHeader(H.second);
+      addHeaderInclude(H.first, Includes, LangOpts, Module->IsExternC);
+    }
   }
 
   // Recurse into submodules.
