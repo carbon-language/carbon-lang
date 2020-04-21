b783f70a42575a5d9147bea1ac97e872370fe55b
Vedant Kumar <vsk@apple.com>
2020-04-21 10:00:13 -0700

    [lldb/DataFormatter] Check for overflow when finding NSDate epoch
    Summary:
    Fixes UBSan-reported issues where the date value inside of an
    uninitialized NSDate overflows the 64-bit epoch.
    
    rdar://61774575
    
    Reviewers: JDevlieghere, mib, teemperor
    
    Subscribers: mgorny, lldb-commits
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D80150

---
+++ lldb/unittests/DataFormatter/MockTests.cpp
@@ -0,0 +1,40 @@
+//===-- MockTests.cpp -----------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "lldb/DataFormatters/Mock.h"
+#include "lldb/Utility/StreamString.h"
+#include "llvm/ADT/Optional.h"
+#include "gtest/gtest.h"
+#include <string>
+
+using namespace lldb_private;
+
+// Try to format the date_value field of a NSDate.
+static llvm::Optional<std::string> formatDateValue(double date_value) {
+  StreamString s;
+  bool succcess = formatters::NSDate::FormatDateValue(date_value, s);
+  if (succcess)
+    return std::string(s.GetData());
+  return llvm::None;
+}
+
+TEST(DataFormatterMockTest, NSDate) {
+  EXPECT_EQ(*formatDateValue(-63114076800), "0001-12-30 00:00:00 +0000");
+
+  // Can't convert the date_value to a time_t.
+  EXPECT_EQ(formatDateValue(std::numeric_limits<time_t>::max() + 1),
+            llvm::None);
+  EXPECT_EQ(formatDateValue(std::numeric_limits<time_t>::min() - 1),
+            llvm::None);
+
+  // Can't add the macOS epoch to the converted date_value (the add overflows).
+  EXPECT_EQ(formatDateValue(std::numeric_limits<time_t>::max()), llvm::None);
+  EXPECT_EQ(formatDateValue(std::numeric_limits<time_t>::min()), llvm::None);
+
+  EXPECT_EQ(*formatDateValue(0), "2001-01-01 00:00:00 UTC");
+}
