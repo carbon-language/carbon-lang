ef9a19cb5c49ce1a7eb6db8ad19230eab17f6feb
Alexander Shaposhnikov <alexshap@fb.com>
2020-04-22 16:05:07 -0700

    [llvm-objcopy][MachO] Add missing license header
    Add missing license header to Object.cpp.
    NFC.

--- llvm/tools/llvm-objcopy/MachO/Object.cpp
+++ llvm/tools/llvm-objcopy/MachO/Object.cpp
@@ -1,3 +1,11 @@
+//===- Object.cpp - Mach-O object file model --------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
 #include "Object.h"
 #include "../llvm-objcopy.h"
 #include "llvm/ADT/SmallPtrSet.h"
