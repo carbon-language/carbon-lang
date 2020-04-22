164845cd925adbaf70cc82d83621e5596bdc031b
Nikita Popov <nikita.ppv@gmail.com>
2020-04-22 21:43:07 +0200

    [GVN] Reduce expression size (NFC)
    Reduce size of GVN::Expression by reordering fields to reduce padding.

--- llvm/lib/Transforms/Scalar/GVN.cpp
+++ llvm/lib/Transforms/Scalar/GVN.cpp
@@ -115,8 +115,8 @@ static cl::opt<uint32_t> MaxNumDeps(
 
 struct llvm::GVN::Expression {
   uint32_t opcode;
-  Type *type = nullptr;
   bool commutative = false;
+  Type *type = nullptr;
   SmallVector<uint32_t, 4> varargs;
 
   Expression(uint32_t o = ~2U) : opcode(o) {}
