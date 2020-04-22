673612a0358b6ec953749d7c57ff33da73954e11
Peter Smith <peter.smith@arm.com>
2020-04-22 11:57:13 +0100

    [LLD][ELF][ARM] Replace adr, ldr with .inst .reloc in test [NFC]
    After D78301 MC no longer emits a relocation for this case. Change to use
    .inst and .reloc to synthesize the same instruction and relocation. One
    more test case I missed.

--- lld/test/ELF/arm-thumb-pc8-weak.s
+++ lld/test/ELF/arm-thumb-pc8-weak.s
@@ -1,7 +1,7 @@
 // REQUIRES: arm
-// RUN: llvm-mc --arm-add-build-attributes -filetype=obj -triple=thumbv6a-none-linux-gnueabi %s -o %t
+// RUN: llvm-mc --arm-add-build-attributes -filetype=obj -triple=thumbv5-none-linux-gnueabi %s -o %t
 // RUN: ld.lld %t -o %t2
-// RUN: llvm-objdump --no-show-raw-insn --triple=thumbv6a-none-linux-gnueabi -d %t2
+// RUN: llvm-objdump --no-show-raw-insn --triple=thumbv6a-none-linux-gnueabi -d %t2 | FileCheck %s
 
 /// Check that the ARM ABI rules for undefined weak symbols are applied.
 /// Relative relocations are resolved to the place. Although we can't encode
@@ -16,9 +16,12 @@
  .global _start
 _start:
  /// R_ARM_THM_PC8
- adr r0, target
- ldr r0, target
-
-// CHECK: 000110b4 _start:
-// CHECK-NEXT: 110b4: adr     r0, #0
+ /// adr r0, target
+ .inst.n 0xa0ff
+ .reloc 0, R_ARM_THM_PC8, target
+ /// ldr r0, target
+ .inst.n 0x48ff
+ .reloc 2, R_ARM_THM_PC8, target
+// CHECK:      000200b4 <_start>:
+// CHECK-NEXT: 200b4: adr     r0, #0
 // CHECK-NEXT:        ldr     r0, [pc, #0]
