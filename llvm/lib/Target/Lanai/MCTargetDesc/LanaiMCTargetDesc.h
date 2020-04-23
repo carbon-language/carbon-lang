82583b17ce7a43c3fd2b325122a20353387ceaa7
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-04-23 14:12:01 +0100

    LanaiMCTargetDesc.h - remove unused forward declarations. NFC.

--- llvm/lib/Target/Lanai/MCTargetDesc/LanaiMCTargetDesc.h
+++ llvm/lib/Target/Lanai/MCTargetDesc/LanaiMCTargetDesc.h
@@ -22,14 +22,9 @@ class MCAsmBackend;
 class MCCodeEmitter;
 class MCContext;
 class MCInstrInfo;
-class MCInstrAnalysis;
 class MCObjectTargetWriter;
-class MCRelocationInfo;
 class MCSubtargetInfo;
 class Target;
-class Triple;
-class StringRef;
-class raw_pwrite_stream;
 
 MCCodeEmitter *createLanaiMCCodeEmitter(const MCInstrInfo &MCII,
                                         const MCRegisterInfo &MRI,
