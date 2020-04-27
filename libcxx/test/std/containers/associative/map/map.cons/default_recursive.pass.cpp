b4437992ecceb3a30429b4c4901e2eeecf8e683a
Casey Carter <cacarter@microsoft.com>
2020-04-27 09:55:00 -0700

    [libc++][test] Disable test for extension that's unsupportable in C++20
    Defining the nested types `reference` and `iterator_concept` of `reverse_iterator<I>` necessarily requires `I` to be complete in C++20. These tests that verify that `std::map<int, X>::reverse_iterator` can be instantiated when `X` is incomplete are going to have a bad time.
    
    Differential Revision: https://reviews.llvm.org/D78944

--- libcxx/test/std/containers/associative/map/map.cons/default_recursive.pass.cpp
+++ libcxx/test/std/containers/associative/map/map.cons/default_recursive.pass.cpp
@@ -21,12 +21,11 @@ struct X
     std::map<int, X> m;
     std::map<int, X>::iterator i;
     std::map<int, X>::const_iterator ci;
+#if TEST_STD_VER <= 17
+    // These reverse_iterator specializations require X to be complete in C++20.
     std::map<int, X>::reverse_iterator ri;
     std::map<int, X>::const_reverse_iterator cri;
+#endif // TEST_STD_VER <= 17
 };
 
-int main(int, char**)
-{
-
-  return 0;
-}
+int main(int, char**) { return 0; }
--- libcxx/test/std/containers/associative/multimap/multimap.cons/default_recursive.pass.cpp
+++ libcxx/test/std/containers/associative/multimap/multimap.cons/default_recursive.pass.cpp
@@ -21,12 +21,11 @@ struct X
     std::multimap<int, X> m;
     std::multimap<int, X>::iterator i;
     std::multimap<int, X>::const_iterator ci;
+#if TEST_STD_VER <= 17
+    // These reverse_iterator specializations require X to be complete in C++20.
     std::multimap<int, X>::reverse_iterator ri;
     std::multimap<int, X>::const_reverse_iterator cri;
+#endif // TEST_STD_VER <= 17
 };
 
-int main(int, char**)
-{
-
-  return 0;
-}
+int main(int, char**) { return 0; }
