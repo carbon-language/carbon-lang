ad38f4b371bdca214e3a3cda9a76ec2213215c68
Vojtěch Štěpančík <vojtechstepancik@outlook.com>
2020-04-28 22:03:30 +0200

    Add a facility to get system cache directory and use it in clangd
    Summary:
    This patch adds a function that is similar to `llvm::sys::path::home_directory`, but provides access to the system cache directory.
    
    For Windows, that is %LOCALAPPDATA%, and applications should put their files under %LOCALAPPDATA%\Organization\Product\.
    
    For *nixes, it adheres to the XDG Base Directory Specification, so it first looks at the XDG_CACHE_HOME environment variable and falls back to ~/.cache/.
    
    Subsequently, the Clangd Index storage leverages this new API to put index files somewhere else than the users home directory.
    
    Fixes https://github.com/clangd/clangd/issues/341
    
    Reviewers: sammccall, chandlerc, Bigcheese
    
    Reviewed By: sammccall
    
    Subscribers: hiraditya, ilya-biryukov, MaskRay, jkorous, dexonsmith, arphaman, kadircet, ormris, usaxena95, cfe-commits, llvm-commits
    
    Tags: #clang-tools-extra, #clang, #llvm
    
    Differential Revision: https://reviews.llvm.org/D78501

--- llvm/include/llvm/Support/Path.h
+++ llvm/include/llvm/Support/Path.h
@@ -368,6 +368,13 @@ void system_temp_directory(bool erasedOnReboot, SmallVectorImpl<char> &result);
 /// @result True if a home directory is set, false otherwise.
 bool home_directory(SmallVectorImpl<char> &result);
 
+/// Get the directory where installed packages should put their
+/// machine-local cache, e.g. $XDG_CACHE_HOME.
+///
+/// @param result Holds the resulting path name.
+/// @result True if the appropriate path was determined, it need not exist.
+bool cache_directory(SmallVectorImpl<char> &result);
+
 /// Has root name?
 ///
 /// root_name != ""
