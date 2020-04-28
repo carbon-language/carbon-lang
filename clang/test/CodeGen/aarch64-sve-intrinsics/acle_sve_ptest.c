43d1d52ad2282235d1ca52facaa2a49198f3d8bd
Sander de Smalen <sander.desmalen@arm.com>
2020-04-28 16:06:26 +0100

    [SveEmitter] Add builtins for logical and predicate operations.
    This patch adds builtins for logical ops:
    - svand, svbic, sveor, svorr, svcnot, svnot
    
    and builtins for predicate operations:
    - svand_b_z, svbic_b_z, sveor_b_z, svnand_b_z, svnor_b_z, svorn_b_z, svorr_b_z
    - svbrka_b_z, svbrkb_b_z, svbrkpa_b_z, svbrkpb_b_z, svbrkn_b_z
    - svpfirst_b
    - svpnext
    - svptest_any
    - svptest_first
    - svptest_last

---
+++ clang/test/CodeGen/aarch64-sve-intrinsics/acle_sve_ptest.c
@@ -0,0 +1,27 @@
+// RUN: %clang_cc1 -D__ARM_FEATURE_SVE -triple aarch64-none-linux-gnu -target-feature +sve -fallow-half-arguments-and-returns -S -O1 -Werror -Wall -emit-llvm -o - %s | FileCheck %s
+
+#include <arm_sve.h>
+
+bool test_svptest_any(svbool_t pg, svbool_t op)
+{
+  // CHECK-LABEL: test_svptest_any
+  // CHECK: %[[INTRINSIC:.*]] = call i1 @llvm.aarch64.sve.ptest.any{{(.nxv16i1)?}}(<vscale x 16 x i1> %pg, <vscale x 16 x i1> %op)
+  // CHECK: ret i1 %[[INTRINSIC]]
+  return svptest_any(pg, op);
+}
+
+bool test_svptest_first(svbool_t pg, svbool_t op)
+{
+  // CHECK-LABEL: test_svptest_first
+  // CHECK: %[[INTRINSIC:.*]] = call i1 @llvm.aarch64.sve.ptest.first{{(.nxv16i1)?}}(<vscale x 16 x i1> %pg, <vscale x 16 x i1> %op)
+  // CHECK: ret i1 %[[INTRINSIC]]
+  return svptest_first(pg, op);
+}
+
+bool test_svptest_last(svbool_t pg, svbool_t op)
+{
+  // CHECK-LABEL: test_svptest_last
+  // CHECK: %[[INTRINSIC:.*]] = call i1 @llvm.aarch64.sve.ptest.last{{(.nxv16i1)?}}(<vscale x 16 x i1> %pg, <vscale x 16 x i1> %op)
+  // CHECK: ret i1 %[[INTRINSIC]]
+  return svptest_last(pg, op);
+}
