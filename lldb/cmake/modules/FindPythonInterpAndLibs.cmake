ae6d2ff633a07a04aad62a0870afe28950472938
Jonas Devlieghere <jonas@devlieghere.com>
2020-04-30 10:34:03 -0700

    [lldb] fix RPATH when linking against Python3.framework
    The install name for the Python 3 framework in Xcode is relative to
    the framework's location and not the dylib itself.
    
      @rpath/Python3.framework/Versions/3.x/Python3
    
    This means that we need to compute the path to the Python3.framework
    and use that as the RPATH instead of the usual dylib's directory.

--- lldb/cmake/modules/FindPythonInterpAndLibs.cmake
+++ lldb/cmake/modules/FindPythonInterpAndLibs.cmake
@@ -17,10 +17,27 @@ else()
         set(PYTHON_LIBRARIES ${Python3_LIBRARIES})
         set(PYTHON_INCLUDE_DIRS ${Python3_INCLUDE_DIRS})
         set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE})
+
+        # The install name for the Python 3 framework in Xcode is relative to
+        # the framework's location and not the dylib itself.
+        #
+        #   @rpath/Python3.framework/Versions/3.x/Python3
+        #
+        # This means that we need to compute the path to the Python3.framework
+        # and use that as the RPATH instead of the usual dylib's directory.
+        #
+        # The check below shouldn't match Homebrew's Python framework as it is
+        # called Python.framework instead of Python3.framework.
+        if (APPLE AND Python3_LIBRARIES MATCHES "Python3.framework")
+          string(FIND "${Python3_LIBRARIES}" "Python3.framework" python_framework_pos)
+          string(SUBSTRING "${Python3_LIBRARIES}" "0" ${python_framework_pos} PYTHON_RPATH)
+        endif()
+
         mark_as_advanced(
           PYTHON_LIBRARIES
           PYTHON_INCLUDE_DIRS
           PYTHON_EXECUTABLE
+          PYTHON_RPATH
           SWIG_EXECUTABLE)
       elseif(NOT CMAKE_SYSTEM_NAME STREQUAL Windows)
         # Use PYTHON_HOME as a hint to find Python 2.
@@ -34,6 +51,7 @@ else()
             PYTHON_LIBRARIES
             PYTHON_INCLUDE_DIRS
             PYTHON_EXECUTABLE
+            PYTHON_RPATH
             SWIG_EXECUTABLE)
         endif()
       endif()
@@ -54,6 +72,7 @@ else()
               PYTHON_LIBRARIES
               PYTHON_INCLUDE_DIRS
               PYTHON_EXECUTABLE
+              PYTHON_RPATH
               SWIG_EXECUTABLE)
           endif()
         endif()
@@ -63,6 +82,7 @@ else()
     message(STATUS "SWIG 2 or later is required for Python support in LLDB but could not be found")
   endif()
 
+
   include(FindPackageHandleStandardArgs)
   find_package_handle_standard_args(PythonInterpAndLibs
                                     FOUND_VAR
@@ -71,5 +91,6 @@ else()
                                       PYTHON_LIBRARIES
                                       PYTHON_INCLUDE_DIRS
                                       PYTHON_EXECUTABLE
+                                      PYTHON_RPATH
                                       SWIG_EXECUTABLE)
 endif()
