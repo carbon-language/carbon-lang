1adc494bce44f6004994deed61b30d4b71fe1d05
Petr Hosek <phosek@google.com>
2020-04-30 13:07:13 -0700

    [CMake] Simplify CMake handling for zlib
    Rather than handling zlib handling manually, use find_package from CMake
    to find zlib properly. Use this to normalize the LLVM_ENABLE_ZLIB,
    HAVE_ZLIB, HAVE_ZLIB_H. Furthermore, require zlib if LLVM_ENABLE_ZLIB is
    set to YES, which requires the distributor to explicitly select whether
    zlib is enabled or not. This simplifies the CMake handling and usage in
    the rest of the tooling.
    
    This is a reland of abb0075 with all followup changes and fixes that
    should address issues that were reported in PR44780.
    
    Differential Revision: https://reviews.llvm.org/D79219

--- lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunication.cpp
+++ lldb/source/Plugins/Process/gdb-remote/GDBRemoteCommunication.cpp
@@ -50,7 +50,7 @@
 #include <compression.h>
 #endif
 
-#if defined(HAVE_LIBZ)
+#if LLVM_ENABLE_ZLIB
 #include <zlib.h>
 #endif
 
@@ -582,7 +582,7 @@ bool GDBRemoteCommunication::DecompressPacket() {
   }
 #endif
 
-#if defined(HAVE_LIBZ)
+#if LLVM_ENABLE_ZLIB
   if (decompressed_bytes == 0 && decompressed_bufsize != ULONG_MAX &&
       decompressed_buffer != nullptr &&
       m_compression_type == CompressionType::ZlibDeflate) {
