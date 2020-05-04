d75a6e93ae99bfcd67219454307da56ebd155d45
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2020-05-04 17:27:41 -0400

    [CUDA][HIP] Fix empty ctor/dtor check for union
    union ctor does not call ctors of its data members. union dtor does not call dtors of its data members.
    Also union does not have base class.
    
    Currently when clang checks whether union has an empty ctor/dtor, it checks the ctors/dtors of its
    data members. This causes incorrectly diagnose device side global variables and shared variables as
    having non-empty ctors/dtors.
    
    This patch fixes that.
    
    Differential Revision: https://reviews.llvm.org/D79367

--- clang/lib/Sema/SemaCUDA.cpp
+++ clang/lib/Sema/SemaCUDA.cpp
@@ -426,6 +426,10 @@ bool Sema::isEmptyCudaConstructor(SourceLocation Loc, CXXConstructorDecl *CD) {
   if (CD->getParent()->isDynamicClass())
     return false;
 
+  // Union ctor does not call ctors of its data members.
+  if (CD->getParent()->isUnion())
+    return true;
+
   // The only form of initializer allowed is an empty constructor.
   // This will recursively check all base classes and member initializers
   if (!llvm::all_of(CD->inits(), [&](const CXXCtorInitializer *CI) {
@@ -464,6 +468,11 @@ bool Sema::isEmptyCudaDestructor(SourceLocation Loc, CXXDestructorDecl *DD) {
   // Its class has no virtual functions and no virtual base classes.
   if (ClassDecl->isDynamicClass())
     return false;
+
+  // Union does not have base class and union dtor does not call dtors of its
+  // data members.
+  if (DD->getParent()->isUnion())
+    return true;
 
   // Only empty destructors are allowed. This will recursively check
   // destructors for all base classes...
