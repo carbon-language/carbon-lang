5fa0eeec6eb1d1f6946d3e6ee2455e95bb79d870
Sander de Smalen <sander.desmalen@arm.com>
2020-05-05 14:20:26 +0100

    [SveEmitter] Add more SVE2 builtins for shift operations
    This patch adds builtins for:
    - svqshlu
    - svrshr
    - svrsra
    - svsli
    - svsra
    - svsri

--- clang/test/CodeGen/aarch64-sve2-intrinsics/negative/acle_sve2_qshlu.c
+++ clang/test/CodeGen/aarch64-sve2-intrinsics/negative/acle_sve2_qshlu.c
@@ -10,6 +10,30 @@
 
 #include <arm_sve.h>
 
+svuint8_t test_svqshlu_n_s8_z(svbool_t pg, svint8_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 7]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s8,_z,)(pg, op1, -1);
+}
+
+svuint16_t test_svqshlu_n_s16_z(svbool_t pg, svint16_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 15]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s16,_z,)(pg, op1, -1);
+}
+
+svuint32_t test_svqshlu_n_s32_z(svbool_t pg, svint32_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 31]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s32,_z,)(pg, op1, -1);
+}
+
+svuint64_t test_svqshlu_n_s64_z(svbool_t pg, svint64_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 63]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s64,_z,)(pg, op1, -1);
+}
+
 svuint8_t test_svqshlu_n_s8_m(svbool_t pg, svint8_t op1)
 {
   // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 7]}}
@@ -32,4 +56,28 @@ svuint64_t test_svqshlu_n_s64_m(svbool_t pg, svint64_t op1)
 {
   // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 63]}}
   return SVE_ACLE_FUNC(svqshlu,_n_s64,_m,)(pg, op1, -1);
+}
+
+svuint8_t test_svqshlu_n_s8_x(svbool_t pg, svint8_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 7]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s8,_x,)(pg, op1, -1);
+}
+
+svuint16_t test_svqshlu_n_s16_x(svbool_t pg, svint16_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 15]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s16,_x,)(pg, op1, -1);
+}
+
+svuint32_t test_svqshlu_n_s32_x(svbool_t pg, svint32_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 31]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s32,_x,)(pg, op1, -1);
+}
+
+svuint64_t test_svqshlu_n_s64_x(svbool_t pg, svint64_t op1)
+{
+  // expected-error-re@+1 {{argument value {{[0-9]+}} is outside the valid range [0, 63]}}
+  return SVE_ACLE_FUNC(svqshlu,_n_s64,_x,)(pg, op1, -1);
 }
