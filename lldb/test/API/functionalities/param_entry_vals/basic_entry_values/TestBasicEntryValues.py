a5c23179826a6dc4f26a4f3fcda66290bc04a201
Pavel Labath <pavel@labath.sk>
2020-05-06 16:06:42 +0200

    [lldb] Revive TestBasicEntryValuesX86_64
    Summary:
    This function rewrites the test to be (hopefully) less susceptible to
    codegen changes and re-enables it.
    
    The most interesting changes are:
    - use an __attribute__((optnone)) function instead of a volatile asm to
      "use" a value. This isn't strictly necessary, but it makes the
      function simpler while achieving the same effect.
    - use a call to a function with the exact same signature instead of a
      volatile asm to "destroy" arguments. This makes the independent of the
      ABI, and (together with avoiding the usage of the arguments after the
      call) ensures that the compiler has no reason to move the argument
      from its initial register (previously we needed to guess where will
      the compiler store the arguments).
    
    Reviewers: vsk, djtodoro, dblaikie
    
    Subscribers: lldb-commits
    
    Tags: #lldb
    
    Differential Revision: https://reviews.llvm.org/D79491

---
+++ lldb/test/API/functionalities/param_entry_vals/basic_entry_values/TestBasicEntryValues.py
@@ -0,0 +1,11 @@
+from lldbsuite.test import lldbinline
+from lldbsuite.test.decorators import *
+from lldbsuite.test import lldbplatformutil
+
+supported_archs = ["x86_64", "arm", "aarch64"]
+
+lldbinline.MakeInlineTest(__file__, globals(),
+        [skipIf(archs=no_match(supported_archs)),
+         skipIf(compiler="clang", compiler_version=['<', '10.0']),
+         skipUnlessHasCallSiteInfo,
+         skipIf(dwarf_version=['<', '4'])])
