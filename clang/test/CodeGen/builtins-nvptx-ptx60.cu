314f99e7d42ded663386190a54b5831dc4a6f3c1
Artem Belevich <tra@google.com>
2020-05-06 12:26:55 -0700

    [CUDA] Enable existing builtins for PTX7.0 as well.
    Differential Revision: https://reviews.llvm.org/D79515

--- clang/test/CodeGen/builtins-nvptx-ptx60.cu
+++ clang/test/CodeGen/builtins-nvptx-ptx60.cu
@@ -6,6 +6,10 @@
 // RUN:            -fcuda-is-device -target-feature +ptx65 \
 // RUN:            -S -emit-llvm -o - -x cuda %s \
 // RUN:   | FileCheck -check-prefix=CHECK %s
+// RUN: %clang_cc1 -triple nvptx64-unknown-unknown -target-cpu sm_80 \
+// RUN:            -fcuda-is-device -target-feature +ptx70 \
+// RUN:            -S -emit-llvm -o - -x cuda %s \
+// RUN:   | FileCheck -check-prefix=CHECK %s
 // RUN: %clang_cc1 -triple nvptx-unknown-unknown -target-cpu sm_60 \
 // RUN:   -fcuda-is-device -S -o /dev/null -x cuda -verify %s
 
