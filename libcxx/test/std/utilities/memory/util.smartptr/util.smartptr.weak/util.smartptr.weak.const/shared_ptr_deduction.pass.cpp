83564056d4b186c9fcf016cdbb388755009f7b5a
Logan Smith <logan.r.smith0@gmail.com>
2020-05-07 12:07:01 -0400

    [libcxx] Add deduction guides for shared_ptr and weak_ptr
    This patch adds deduction guides to <memory> to allow deducing
    construction of shared_ptrs from unique_ptrs, and from weak_ptrs
    and vice versa, as specified by C++17.
    
    Differential Revision: https://reviews.llvm.org/D69603

---
+++ libcxx/test/std/utilities/memory/util.smartptr/util.smartptr.weak/util.smartptr.weak.const/shared_ptr_deduction.pass.cpp
@@ -0,0 +1,34 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <memory>
+// UNSUPPORTED: c++98, c++03, c++11, c++14
+// UNSUPPORTED: libcpp-no-deduction-guides
+
+// template<class T> class weak_ptr
+
+// weak_ptr(shared_ptr<T>) -> weak_ptr<T>
+
+#include <memory>
+#include <cassert>
+
+#include "test_macros.h"
+
+struct A {};
+
+int main(int, char**)
+{
+  std::shared_ptr<A> s(new A);
+  auto w = std::weak_ptr(s);
+  ASSERT_SAME_TYPE(decltype(w), std::weak_ptr<A>);
+  assert(!w.expired());
+  assert(w.use_count() == 1);
+  assert(w.lock().get() == s.get());
+
+  return 0;
+}
