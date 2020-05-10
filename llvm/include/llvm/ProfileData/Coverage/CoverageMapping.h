d7f1eafd03bce220af80c15254c92c0c9304b880
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-05-10 14:19:54 +0100

    CoverageMapping.h - remove unused StringSet.h include. NFC.

--- llvm/include/llvm/ProfileData/Coverage/CoverageMapping.h
+++ llvm/include/llvm/ProfileData/Coverage/CoverageMapping.h
@@ -20,7 +20,6 @@
 #include "llvm/ADT/Hashing.h"
 #include "llvm/ADT/None.h"
 #include "llvm/ADT/StringRef.h"
-#include "llvm/ADT/StringSet.h"
 #include "llvm/ADT/iterator.h"
 #include "llvm/ADT/iterator_range.h"
 #include "llvm/ProfileData/InstrProf.h"
