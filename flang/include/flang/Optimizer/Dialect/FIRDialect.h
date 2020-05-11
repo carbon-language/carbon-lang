63a4fdda8c33174dc9fc80fed385df759abbf14f
Valentin Clement <clementval@gmail.com>
2020-05-11 13:13:58 -0400

    [flang] Fix compilation after rename of Loop dialect to scf
    Summary: Flang was not compiling correctly after the renaming of Loop dialect to SCF. This patch fixes the problem.
    
    Reviewers: ftynse, DavidTruby
    
    Reviewed By: ftynse
    
    Subscribers: aartbik, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D79723

--- flang/include/flang/Optimizer/Dialect/FIRDialect.h
+++ flang/include/flang/Optimizer/Dialect/FIRDialect.h
@@ -38,7 +38,7 @@ inline void registerFIR() {
   [[maybe_unused]] static bool init_once = [] {
     mlir::registerDialect<mlir::AffineDialect>();
     mlir::registerDialect<mlir::LLVM::LLVMDialect>();
-    mlir::registerDialect<mlir::loop::LoopOpsDialect>();
+    mlir::registerDialect<mlir::scf::SCFDialect>();
     mlir::registerDialect<mlir::StandardOpsDialect>();
     mlir::registerDialect<mlir::vector::VectorDialect>();
     mlir::registerDialect<FIROpsDialect>();
