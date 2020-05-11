288c9e81781e5468cf6db5c4f6e579f39cfe72ba
Davide Italiano <ditaliano@apple.com>
2020-05-11 16:21:26 -0700

    [GlobalISel] Remove debug locations when emitting G_FCONSTANT.
    <rdar://problem/62991543>

--- llvm/lib/CodeGen/GlobalISel/MachineIRBuilder.cpp
+++ llvm/lib/CodeGen/GlobalISel/MachineIRBuilder.cpp
@@ -324,6 +324,7 @@ MachineInstrBuilder MachineIRBuilder::buildFConstant(const DstOp &Res,
   }
 
   auto Const = buildInstr(TargetOpcode::G_FCONSTANT);
+  Const->setDebugLoc(DebugLoc());
   Res.addDefToMIB(*getMRI(), Const);
   Const.addFPImm(&Val);
   return Const;
