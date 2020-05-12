ab22f71dd75895fbde1cd1a515b2164227d5dfd2
Jonas Devlieghere <jonas@devlieghere.com>
2020-05-12 13:18:35 -0700

    [lldb/Reproducers] Also record directories FileSystem::Collect.
    Now that the FileCollector knows how to deal with directories we no
    longer have to ignore them in the FileSystem class.

--- lldb/source/Host/common/FileSystem.cpp
+++ lldb/source/Host/common/FileSystem.cpp
@@ -487,7 +487,11 @@ void FileSystem::Collect(const FileSpec &file_spec) {
 }
 
 void FileSystem::Collect(const llvm::Twine &file) {
-  if (m_collector && !llvm::sys::fs::is_directory(file)) {
+  if (!m_collector)
+    return;
+
+  if (llvm::sys::fs::is_directory(file))
+    m_collector->addDirectory(file);
+  else
     m_collector->addFile(file);
-  }
 }
