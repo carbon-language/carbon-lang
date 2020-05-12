carbon-language/carbon-lang

commit ec2d93c7d7b00374535cb3d6658abec4b150f6f6

[analyzer] SATestBuild.py: Allow comments in run_static_analyzer.cmd.
Because those can get really weird sometimes.

clang/utils/analyzer/SATestBuild.py
@@ -298,7 +298,7 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
         SBPrefix = "scan-build " + SBOptions + " "
         for Command in SBCommandFile:
             Command = Command.strip()
-            if len(Command) == 0:
+            if len(Command) == 0 or Command.startswith("#"):
                 continue
 
             # Custom analyzer invocation specified by project.

commit 11c8c2a551c53db5ecf8c1f253b1a49d5e98324f

[analyzer] SATestBuild.py: Fix support for #NOPREFIX.
Regressed in ec2d93c.

clang/utils/analyzer/SATestBuild.py
@@ -298,7 +298,7 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
         SBPrefix = "scan-build " + SBOptions + " "
         for Command in SBCommandFile:
             Command = Command.strip()
-            if len(Command) == 0 or Command.startswith("#"):
+            if len(Command) == 0:
                 continue
 
             # Custom analyzer invocation specified by project.
@@ -309,6 +309,9 @@ def runScanBuild(Args, Dir, SBOutputDir, PBuildLogFile):
                 ExtraEnv['OUTPUT'] = SBOutputDir
                 ExtraEnv['CC'] = Clang
                 ExtraEnv['ANALYZER_CONFIG'] = generateAnalyzerConfig(Args)
+                continue
+
+            if Command.startswith("#"):
                 continue
 
             # If using 'make', auto imply a -jX argument