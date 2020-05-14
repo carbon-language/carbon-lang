7c3768495e8c1599dc30986f7bd47d5e91f303f2
Gabor Marton <gabor.marton@ericsson.com>
2020-05-14 15:49:07 +0200

    [analyzer] Improve PlacementNewChecker
    Summary:
    1. Added insufficient storage check for arrays
    2. Added align support check
    
    Based on https://reviews.llvm.org/D76229
    
    Reviewers: aaron.ballman, lebedev.ri, NoQ, martong
    
    Reviewed By: martong
    
    Subscribers: xazax.hun, baloghadamsoftware, szepet, rnkovacs, a.sidorin, mikhail.ramalho, Szelethus, donat.nagy, dkrupp, Charusso, ASDenysPetrov, cfe-commits
    
    Tags: #clang
    
    Differential Revision: https://reviews.llvm.org/D76996
    
    Patch by Karasev Nikita!

--- clang/test/Analysis/placement-new.cpp
+++ clang/test/Analysis/placement-new.cpp
@@ -155,3 +155,309 @@ void f() {
   (void)dp;
 }
 } // namespace testHierarchy
+
+namespace testArrayTypesAllocation {
+void f1() {
+  struct S {
+    short a;
+  };
+
+  // bad (not enough space).
+  const unsigned N = 32;
+  alignas(S) unsigned char buffer1[sizeof(S) * N]; // expected-note {{'buffer1' initialized here}}
+  ::new (buffer1) S[N];                            // expected-warning{{Storage provided to placement new is only 64 bytes, whereas the allocated array type requires more space for internal needs}} expected-note 1 {{}}
+}
+
+void f2() {
+  struct S {
+    short a;
+  };
+
+  // maybe ok but we need to warn.
+  const unsigned N = 32;
+  alignas(S) unsigned char buffer2[sizeof(S) * N + sizeof(int)]; // expected-note {{'buffer2' initialized here}}
+  ::new (buffer2) S[N];                                          // expected-warning{{68 bytes is possibly not enough for array allocation which requires 64 bytes. Current overhead requires the size of 4 bytes}} expected-note 1 {{}}
+}
+} // namespace testArrayTypesAllocation
+
+namespace testStructAlign {
+void f1() {
+  struct X {
+    char a[9];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad (struct X is aligned to char).
+  ::new (&Xi.a) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f2() {
+  struct X {
+    char a;
+    char b;
+    long c;
+  } Xi;
+
+  // ok (struct X is aligned to long).
+  ::new (&Xi.a) long;
+}
+
+void f3() {
+  struct X {
+    char a;
+    char b;
+    long c;
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad (struct X is aligned to long but field 'b' is aligned to 1 because of its offset)
+  ::new (&Xi.b) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f4() {
+  struct X {
+    char a;
+    struct alignas(alignof(short)) Y {
+      char b;
+      char c;
+    } y;
+    long d;
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad. 'b' is aligned to short
+  ::new (&Xi.y.b) long; // expected-warning{{Storage type is aligned to 2 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f5() {
+  short b[10]; // expected-note {{'b' initialized here}}
+
+  ::new (&b) long; // expected-warning{{Storage type is aligned to 2 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f6() {
+  short b[10]; // expected-note {{'b' initialized here}}
+
+  // bad (same as previous but checks ElementRegion case)
+  ::new (&b[0]) long; // expected-warning{{Storage type is aligned to 2 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f7() {
+  alignas(alignof(long)) short b[10];
+
+  // ok. aligned to long(ok). offset 4*2(ok)
+  ::new (&b[4]) long;
+}
+
+void f8() {
+  alignas(alignof(long)) short b[10]; // expected-note {{'b' initialized here}}
+
+  // ok. aligned to long(ok). offset 3*2(ok)
+  ::new (&b[3]) long; // expected-warning{{Storage type is aligned to 6 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f9() {
+  struct X {
+    char a;
+    alignas(alignof(long)) char b[20];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // ok. aligned to long(ok). offset 8*1(ok)
+  ::new (&Xi.b[8]) long;
+
+  // bad. aligned to long(ok). offset 1*1(ok)
+  ::new (&Xi.b[1]) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f10() {
+  struct X {
+    char a[8];
+    alignas(2) char b;
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad (struct X is aligned to 2).
+  ::new (&Xi.a) long; // expected-warning{{Storage type is aligned to 2 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void f11() {
+  struct X {
+    char a;
+    char b;
+    struct Y {
+      long c;
+    } d;
+  } Xi;
+
+  // ok (struct X is aligned to long).
+  ::new (&Xi.a) long;
+}
+
+void f12() {
+  struct alignas(alignof(long)) X {
+    char a;
+    char b;
+  } Xi;
+
+  // ok (struct X is aligned to long).
+  ::new (&Xi.a) long;
+}
+
+void test13() {
+  struct Y {
+    char a[10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad. X,A are aligned to 'char'
+  ::new (&Xi.b[0].a) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void test14() {
+  struct Y {
+    char a[10];
+  };
+
+  struct alignas(alignof(long)) X {
+    Y b[10];
+  } Xi;
+
+  // ok. X is aligned to 'long' and field 'a' goes with zero offset
+  ::new (&Xi.b[0].a) long;
+}
+
+void test15() {
+  struct alignas(alignof(long)) Y {
+    char a[10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi;
+
+  // ok. X is aligned to 'long' because it contains struct 'Y' which is aligned to 'long'
+  ::new (&Xi.b[0].a) long;
+}
+
+void test16() {
+  struct alignas(alignof(long)) Y {
+    char p;
+    char a[10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad. aligned to long(ok). offset 1(bad)
+  ::new (&Xi.b[0].a) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void test17() {
+  struct alignas(alignof(long)) Y {
+    char p;
+    char a[10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi;
+
+  // ok. aligned to long(ok). offset 1+7*1(ok)
+  ::new (&Xi.b[0].a[7]) long;
+}
+
+void test18() {
+  struct Y {
+    char p;
+    alignas(alignof(long)) char a[10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // ok. aligned to long(ok). offset 8*1(ok)
+  ::new (&Xi.b[0].a[8]) long;
+
+  // bad. aligned to long(ok). offset 1(bad)
+  ::new (&Xi.b[0].a[1]) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void test19() {
+  struct Z {
+    char p;
+    char c[10];
+  };
+
+  struct Y {
+    char p;
+    Z b[10];
+  };
+
+  struct X {
+    Y a[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // bad. all structures X,Y,Z are aligned to char
+  ::new (&Xi.a[1].b[1].c) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void test20() {
+  struct Z {
+    char p;
+    alignas(alignof(long)) char c[10];
+  };
+
+  struct Y {
+    char p;
+    Z b[10];
+  };
+
+  struct X {
+    Y a[10];
+  } Xi;
+
+  // ok. field 'c' is aligned to 'long'
+  ::new (&Xi.a[1].b[1].c) long;
+}
+
+void test21() {
+  struct Z {
+    char p;
+    char c[10];
+  };
+
+  struct Y {
+    char p;
+    Z b[10];
+  };
+
+  struct alignas(alignof(long)) X {
+    Y a[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // ok. aligned to long(ok). offset 1+7*1(ok)
+  ::new (&Xi.a[0].b[0].c[7]) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+void test22() {
+  struct alignas(alignof(long)) Y {
+    char p;
+    char a[10][10];
+  };
+
+  struct X {
+    Y b[10];
+  } Xi; // expected-note {{'Xi' initialized here}}
+
+  // ok. aligned to long(ok). offset ok. 1(field 'a' offset) + 0*10(index '0' * first dimension size '10') + 7*1(index '7')
+  ::new (&Xi.b[0].a[0][7]) long;
+
+  // ok. aligned to long(ok). offset ok. 1(field 'a' offset) + 1*10(index '1' * first dimension size '10') + 5*1(index '5')
+  ::new (&Xi.b[0].a[1][5]) long;
+
+  // bad. aligned to long(ok). offset ok. 1(field 'a' offset) + 1*10(index '1' * first dimension size '10') + 6*1(index '5')
+  ::new (&Xi.b[0].a[1][6]) long; // expected-warning{{Storage type is aligned to 1 bytes but allocated type is aligned to 8 bytes}} expected-note 1 {{}}
+}
+
+} // namespace testStructAlign
