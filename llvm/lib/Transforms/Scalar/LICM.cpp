da52aa2c3340d91025c0188175012c476967dc74
Davide Italiano <ditaliano@apple.com>
2020-05-14 16:48:53 -0700

    [LICM] When promoting loads to the preheader, drop the location.
    It's really almost going to be misleading, see the example in
    https://bugs.llvm.org/show_bug.cgi?id=45820
    
    Maybe at some point we can do something fancier, but at least
    this will fix a bug where we step on dead code while debugging.

--- llvm/lib/Transforms/Scalar/LICM.cpp
+++ llvm/lib/Transforms/Scalar/LICM.cpp
@@ -2089,7 +2089,7 @@ bool llvm::promoteLoopAccessesToScalars(
   if (SawUnorderedAtomic)
     PreheaderLoad->setOrdering(AtomicOrdering::Unordered);
   PreheaderLoad->setAlignment(Align(Alignment));
-  PreheaderLoad->setDebugLoc(DL);
+  PreheaderLoad->setDebugLoc(DebugLoc());
   if (AATags)
     PreheaderLoad->setAAMetadata(AATags);
   SSA.AddAvailableValue(Preheader, PreheaderLoad);
