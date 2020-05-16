228913780b61d7399de5150b1c5d85875d9198f0
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-05-16 14:40:00 +0100

    DIEHash.cpp - remove headers explicitly included in DIEHash.h. NFC.
    Don't duplicate module header includes.

--- llvm/lib/CodeGen/AsmPrinter/DIEHash.cpp
+++ llvm/lib/CodeGen/AsmPrinter/DIEHash.cpp
@@ -17,10 +17,8 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/BinaryFormat/Dwarf.h"
 #include "llvm/CodeGen/AsmPrinter.h"
-#include "llvm/CodeGen/DIE.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/Endian.h"
-#include "llvm/Support/MD5.h"
 #include "llvm/Support/raw_ostream.h"
 
 using namespace llvm;
