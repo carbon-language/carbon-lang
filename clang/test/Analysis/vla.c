56079e1de1129837aa7569d8b3bb5e50afc0f1ea
Balázs Kéri <1.int32@gmail.com>
2020-05-19 12:12:28 +0200

    [Analyzer][VLASizeChecker] Try to fix vla.c test problems.

--- clang/test/Analysis/vla.c
+++ clang/test/Analysis/vla.c
@@ -18,11 +18,11 @@ void check_uninit_sized_VLA() {
 }
 
 // Negative VLAs.
-static void vla_allocate_signed(int x) {
+static void vla_allocate_signed(short x) {
   int vla[x]; // expected-warning{{Declared variable-length array (VLA) has negative size}}
 }
 
-static void vla_allocate_unsigned(unsigned int x) {
+static void vla_allocate_unsigned(unsigned short x) {
   int vla[x]; // no-warning
 }
 
@@ -35,12 +35,12 @@ void check_negative_sized_VLA_2() {
 }
 
 void check_negative_sized_VLA_3() {
-  int x = -1;
+  short x = -1;
   int vla[x]; // expected-warning{{Declared variable-length array (VLA) has negative size}}
 }
 
 void check_negative_sized_VLA_4() {
-  unsigned int x = -1;
+  unsigned short x = -1;
   int vla[x]; // no-warning
 }
 
@@ -79,12 +79,12 @@ void check_negative_sized_VLA_10(int x) {
     check_negative_sized_VLA_10_sub(x);
 }
 
-static void check_negative_sized_VLA_11_sub(int x)
+static void check_negative_sized_VLA_11_sub(short x)
 {
   int vla[x]; // no-warning
 }
 
-void check_negative_sized_VLA_11(int x) {
+void check_negative_sized_VLA_11(short x) {
   if (x > 0)
     check_negative_sized_VLA_11_sub(x);
 }
