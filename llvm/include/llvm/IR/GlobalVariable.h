8e64c096946a8b697f675b9b3df4b016f8c435a8
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-05-19 12:08:29 +0100

    GlobalVariable.h - remove unused PointerUnion.h include. NFC

--- llvm/include/llvm/IR/GlobalVariable.h
+++ llvm/include/llvm/IR/GlobalVariable.h
@@ -19,7 +19,6 @@
 #ifndef LLVM_IR_GLOBALVARIABLE_H
 #define LLVM_IR_GLOBALVARIABLE_H
 
-#include "llvm/ADT/PointerUnion.h"
 #include "llvm/ADT/Twine.h"
 #include "llvm/ADT/ilist_node.h"
 #include "llvm/IR/Attributes.h"
