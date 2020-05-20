1219221f9ca165306e13d375e05682241d857c1c
Benjamin Kramer <benny.kra@googlemail.com>
2020-05-20 11:55:37 +0200

    [gold-plugin] Unbreak the build after d9b9ce6c04764275a23cd0cf1856a35aae921af7

--- llvm/tools/gold/gold-plugin.cpp
+++ llvm/tools/gold/gold-plugin.cpp
@@ -24,6 +24,7 @@
 #include "llvm/Support/CachePruning.h"
 #include "llvm/Support/CommandLine.h"
 #include "llvm/Support/FileSystem.h"
+#include "llvm/Support/Host.h"
 #include "llvm/Support/ManagedStatic.h"
 #include "llvm/Support/MemoryBuffer.h"
 #include "llvm/Support/Path.h"
