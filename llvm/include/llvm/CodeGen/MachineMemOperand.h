dd60ad0b79e0f35a9c670bfa14846de577c1230c
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-05-20 13:52:22 +0100

    PseudoSourceValue.h - reduce GlobalValue.h include to forward declaration. NFC.
    Exposes an implicit dependency on Type.h in MachineMemOperand.h

--- llvm/include/llvm/CodeGen/MachineMemOperand.h
+++ llvm/include/llvm/CodeGen/MachineMemOperand.h
@@ -18,6 +18,7 @@
 #include "llvm/ADT/BitmaskEnum.h"
 #include "llvm/ADT/PointerUnion.h"
 #include "llvm/CodeGen/PseudoSourceValue.h"
+#include "llvm/IR/Type.h"
 #include "llvm/IR/Value.h" // PointerLikeTypeTraits<Value*>
 #include "llvm/Support/AtomicOrdering.h"
 #include "llvm/Support/DataTypes.h"
