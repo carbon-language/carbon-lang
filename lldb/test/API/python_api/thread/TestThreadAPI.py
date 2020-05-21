60dff35fd435e5c5b434091019a78183260552be
Adrian Prantl <aprantl@apple.com>
2020-05-21 10:42:26 -0700

    Move decorator to the correct function.

--- lldb/test/API/python_api/thread/TestThreadAPI.py
+++ lldb/test/API/python_api/thread/TestThreadAPI.py
@@ -37,6 +37,7 @@ class ThreadAPITestCase(TestBase):
         self.setTearDownCleanup(dictionary=d)
         self.run_to_address(self.exe_name)
 
+    @skipIfAsan # The output looks different under ASAN.
     @add_test_categories(['pyapi'])
     @expectedFailureAll(oslist=["linux"], archs=['arm'], bugnumber="llvm.org/pr45892")
     @expectedFailureAll(oslist=['freebsd'], bugnumber='llvm.org/pr20476')
@@ -138,7 +139,6 @@ class ThreadAPITestCase(TestBase):
         # Test that we can pass in a much larger size and still get the right output.
         self.assertEqual("breakpoint 1.1", thread.GetStopDescription(len('breakpoint 1.1') + 100))
 
-    @skipIfAsan # The output looks different under ASAN.
     def step_out_of_malloc_into_function_b(self, exe_name):
         """Test Python SBThread.StepOut() API to step out of a malloc call where the call site is at function b()."""
         exe = self.getBuildArtifact(exe_name)
