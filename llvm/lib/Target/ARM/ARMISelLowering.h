872ee78f65f47fae19d4f8db7396eb56d902aef1
Victor Campos <victor.campos@arm.com>
2020-05-22 10:03:18 +0100

    Revert "[ARM] Improve codegen of volatile load/store of i64"
    This reverts commit 8a12553223180246eeafaa0fa7bfa11e834d34b6.
    
    A bug has been found when generating code for Thumb2. In some very
    specific cases, the prologue/epilogue emitter generates erroneous stack
    offsets for the new LDRD instructions that access the stack.
    
    This bug does not seem to be caused by the reverted patch though. Likely
    the latter has made an undiscovered issue emerge in the
    prologue/epilogue emission pass. Nevertheless, this reversion is
    necessary since it is blocking users of the ARM backend.

--- llvm/lib/Target/ARM/ARMISelLowering.h
+++ llvm/lib/Target/ARM/ARMISelLowering.h
@@ -305,11 +305,7 @@ class VectorType;
       VST4_UPD,
       VST2LN_UPD,
       VST3LN_UPD,
-      VST4LN_UPD,
-
-      // Load/Store of dual registers
-      LDRD,
-      STRD
+      VST4LN_UPD
     };
 
   } // end namespace ARMISD
@@ -775,8 +771,6 @@ class VectorType;
     SDValue LowerFSETCC(SDValue Op, SelectionDAG &DAG) const;
     void lowerABS(SDNode *N, SmallVectorImpl<SDValue> &Results,
                   SelectionDAG &DAG) const;
-    void LowerLOAD(SDNode *N, SmallVectorImpl<SDValue> &Results,
-                   SelectionDAG &DAG) const;
 
     Register getRegisterByName(const char* RegName, LLT VT,
                                const MachineFunction &MF) const override;
