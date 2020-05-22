d89c98a020c9903f31b659e3f5cc513ea878c900
Jonas Devlieghere <jonas@devlieghere.com>
2020-05-22 09:22:53 -0700

    [lldb/Test] Remove issue_verification subdirectory
    These files haven't been touched since 2015. According to Pavel these
    were intended to be test for the test framework which never really took
    of and are mostly irrelevant by now.
    
    Differential revision: https://reviews.llvm.org/D80408

--- lldb/test/API/issue_verification/inline_rerun_inferior.cpp
+++
@@ -1,6 +0,0 @@
-typedef int Foo;
-
-int main() {
-    Foo array[3] = {1,2,3};
-    return 0; //% self.expect("frame variable array --show-types --", substrs = ['(Foo [3]) wrong_type_here = {','(Foo) [0] = 1','(Foo) [1] = 2','(Foo) [2] = 3'])
-}
