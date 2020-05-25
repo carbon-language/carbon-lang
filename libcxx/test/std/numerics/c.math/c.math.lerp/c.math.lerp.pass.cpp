d1dbda10cefeaa124e28eb289cdc92c049c3d973
Marek Kurdej <marek@quasardb.net>
2020-05-25 22:26:50 +0200

    [libc++] [LWG3201] Update status page: lerp should be marked noexcept.
    Summary: Update status page and test synopsis. Add synopsis in <cmath>.
    
    Reviewed By: ldionne, #libc
    
    Differential Revision: https://reviews.llvm.org/D80456

--- libcxx/test/std/numerics/c.math/c.math.lerp/c.math.lerp.pass.cpp
+++ libcxx/test/std/numerics/c.math/c.math.lerp/c.math.lerp.pass.cpp
@@ -8,10 +8,9 @@
 // UNSUPPORTED: c++98, c++03, c++11, c++14, c++17
 // <cmath>
 
-// constexpr float lerp(float a, float b, float t);
-// constexpr double lerp(double a, double b, double t);
-// constexpr long double lerp(long double a, long double b, long double t);
-
+// constexpr float lerp(float a, float b, float t) noexcept;
+// constexpr double lerp(double a, double b, double t) noexcept;
+// constexpr long double lerp(long double a, long double b, long double t) noexcept;
 
 #include <cmath>
 #include <limits>
