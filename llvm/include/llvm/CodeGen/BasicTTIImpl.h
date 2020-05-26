1f72d5880e332dfbd36c22388d2b72bd2bd70411
Sam Parker <sam.parker@arm.com>
2020-05-26 08:31:38 +0100

    [CostModel] Check for free intrinsics in BasicTTI
    Recommitting part of "[CostModel] Unify Intrinsic Costs."
    de71def3f59dc9f12f67141b5040d8e15c84d08a
    
    Now that the 'free' intrinsic information has been sunk to the lowest
    level, query the base implementation in BasicTTI before doing
    anything else. I suspect this is the change that was causing the main
    changes, particularly the large effects on debug builds.
    
    Differential Revision: https://reviews.llvm.org/D80012

--- llvm/include/llvm/CodeGen/BasicTTIImpl.h
+++ llvm/include/llvm/CodeGen/BasicTTIImpl.h
@@ -1091,6 +1091,9 @@ public:
   unsigned getIntrinsicInstrCost(const IntrinsicCostAttributes &ICA,
                                  TTI::TargetCostKind CostKind) {
 
+    if (BaseT::getIntrinsicInstrCost(ICA, CostKind) == 0)
+      return 0;
+
     // TODO: Combine these two logic paths.
     if (ICA.isTypeBasedOnly())
       return getTypeBasedIntrinsicInstrCost(ICA, CostKind);
