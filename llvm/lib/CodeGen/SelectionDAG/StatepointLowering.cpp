4d6cda9bdaca01f581ef23904f54443ef5c2acac
Philip Reames <listmail@philipreames.com>
2020-05-28 12:31:49 -0700

    [Statepoint] Use iterate_range.empty [NFC]

--- llvm/lib/CodeGen/SelectionDAG/StatepointLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/StatepointLowering.cpp
@@ -869,7 +869,7 @@ SelectionDAGBuilder::LowerStatepoint(const GCStatepointInst &I,
   SI.ID = I.getID();
 
   if (auto Opt = I.getOperandBundle(LLVMContext::OB_deopt)) {
-    assert(ISP.deopt_begin() == ISP.deopt_end() &&
+    assert(ISP.deopt_operands().empty() &&
            "can't list both deopt operands and deopt bundle");
     auto &Inputs = Opt->Inputs;
     SI.DeoptState = ArrayRef<const Use>(Inputs.begin(), Inputs.end());
@@ -877,7 +877,7 @@ SelectionDAGBuilder::LowerStatepoint(const GCStatepointInst &I,
     SI.DeoptState = ArrayRef<const Use>(ISP.deopt_begin(), ISP.deopt_end());
   }
   if (auto Opt = I.getOperandBundle(LLVMContext::OB_gc_transition)) {
-    assert(ISP.gc_transition_args_begin() == ISP.gc_transition_args_end() &&
+    assert(ISP.gc_transition_args().empty() &&
            "can't list both gc_transition operands and bundle");
     auto &Inputs = Opt->Inputs;
     SI.GCTransitionArgs = ArrayRef<const Use>(Inputs.begin(), Inputs.end());
