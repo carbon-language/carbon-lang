1a945757ac7debd9ad14497fa404e3900869cad5
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-05-28 13:58:35 +0100

    WithColor.h - reduce unnecessary includes to forward declarations. NFC.

--- llvm/include/llvm/Support/WithColor.h
+++ llvm/include/llvm/Support/WithColor.h
@@ -9,15 +9,15 @@
 #ifndef LLVM_SUPPORT_WITHCOLOR_H
 #define LLVM_SUPPORT_WITHCOLOR_H
 
-#include "llvm/ADT/StringRef.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Error.h"
 
 namespace llvm {
 
-extern cl::OptionCategory ColorCategory;
-
+class Error;
 class raw_ostream;
+class StringRef;
+
+extern cl::OptionCategory ColorCategory;
 
 // Symbolic names for various syntax elements.
 enum class HighlightColor {
