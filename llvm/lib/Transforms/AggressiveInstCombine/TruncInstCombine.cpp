e6cf402e8364a7a7e483a60aa1fca1213f4fb475
Christopher Tetreault <ctetreau@quicinc.com>
2020-05-29 15:41:06 -0700

    [SVE] Eliminate calls to default-false VectorType::get() from AggressiveInstCombine
    Reviewers: efriedma, aymanmus, c-rhodes, david-arm
    
    Reviewed By: david-arm
    
    Subscribers: tschuett, hiraditya, rkruppe, psnobl, llvm-commits
    
    Tags: #llvm
    
    Differential Revision: https://reviews.llvm.org/D80332

--- llvm/lib/Transforms/AggressiveInstCombine/TruncInstCombine.cpp
+++ llvm/lib/Transforms/AggressiveInstCombine/TruncInstCombine.cpp
@@ -281,8 +281,10 @@ Type *TruncInstCombine::getBestTruncatedType() {
 /// version of \p Ty, otherwise return \p Ty.
 static Type *getReducedType(Value *V, Type *Ty) {
   assert(Ty && !Ty->isVectorTy() && "Expect Scalar Type");
-  if (auto *VTy = dyn_cast<VectorType>(V->getType()))
-    return VectorType::get(Ty, VTy->getNumElements());
+  if (auto *VTy = dyn_cast<VectorType>(V->getType())) {
+    // FIXME: should this handle scalable vectors?
+    return FixedVectorType::get(Ty, VTy->getNumElements());
+  }
   return Ty;
 }
 
