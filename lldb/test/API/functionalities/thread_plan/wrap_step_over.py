2388a096e7865c043e83ece4e26654bd3d1a20d5
Jonas Devlieghere <jonas@devlieghere.com>
2020-05-29 23:01:09 -0700

    [lldb/Test] use GetLoadAddress from scripted thread plan
    Commit 0800529fe605 adds a runtime error which triggers when using
    SBAddress properties that use the current process/target from a
    non-interactive session. TestThreadPlanCommands.py was doing exactly
    this and this patch fixes that by use GetLoadAddress instead.

--- lldb/test/API/functionalities/thread_plan/wrap_step_over.py
+++ lldb/test/API/functionalities/thread_plan/wrap_step_over.py
@@ -3,14 +3,16 @@ import lldb
 class WrapStepOver():
     def __init__(self, thread_plan, args_data, dict):
         self.plan = thread_plan
-        frame_0 = thread_plan.GetThread().frames[0]
+        thread = thread_plan.GetThread()
+        target = thread.GetProcess().GetTarget()
+        frame_0 = thread.frames[0]
         line_entry = frame_0.line_entry
         start_addr = line_entry.addr
         end_addr = line_entry.end_addr
-        range_size = int(end_addr) - int(start_addr)
+        range_size = end_addr.GetLoadAddress(target) - start_addr.GetLoadAddress(target)
         error = lldb.SBError()
         self.sub_plan = thread_plan.QueueThreadPlanForStepOverRange(start_addr, range_size)
-        
+
     def should_step(self):
         return False
 
