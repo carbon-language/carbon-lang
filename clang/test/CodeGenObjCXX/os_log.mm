a66e1d2aa943959e158821be8956109cb5ef3b3b
Vedant Kumar <vsk@apple.com>
2020-06-01 16:54:16 -0700

    [os_log][test] Remove -O1 from a test, NFC

--- clang/test/CodeGenObjCXX/os_log.mm
+++ clang/test/CodeGenObjCXX/os_log.mm
@@ -1,7 +1,5 @@
 // RUN: %clang_cc1 %s -emit-llvm -o - -triple x86_64-darwin-apple -fobjc-arc \
-// RUN:   -fexceptions -fcxx-exceptions -O1 -fno-experimental-new-pass-manager | FileCheck %s
-// RUN: %clang_cc1 %s -emit-llvm -o - -triple x86_64-darwin-apple -fobjc-arc \
-// RUN:   -fexceptions -fcxx-exceptions -O1 -fexperimental-new-pass-manager -fno-inline | FileCheck %s
+// RUN:   -fexceptions -fcxx-exceptions | FileCheck %s
 
 // Check that no EH cleanup is emitted around the call to __os_log_helper.
 namespace no_eh_cleanup {
@@ -10,11 +8,11 @@ namespace no_eh_cleanup {
   // CHECK-LABEL: define {{.*}} @_ZN13no_eh_cleanup3logERiPcS1_(
   void log(int &i, char *data, char *buf) {
       int lock __attribute__((cleanup(release)));
-      // CHECK: call void @__os_log_helper_1_2_2_4_0_8_34(
-      // CHECK-NEXT: call void @_ZN13no_eh_cleanup7releaseEPi
       __builtin_os_log_format(buf, "%d %{public}s", i, data);
   }
 
+  // An `invoke` of a `nounwind` callee is simplified to a direct
+  // call by an optimization in llvm. Just check that we emit `nounwind`.
   // CHECK: define {{.*}} @__os_log_helper_1_2_2_4_0_8_34({{.*}} [[NUW:#[0-9]+]]
 }
 
