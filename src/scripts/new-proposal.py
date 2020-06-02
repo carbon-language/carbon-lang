f1ae78efca88ea7304df5505f1e00ebe105303b6
jonmeow <46229924+jonmeow@users.noreply.github.com>
2020-06-02 10:06:36 -0700

    Progress

--- src/scripts/new-proposal.py
+++ src/scripts/new-proposal.py
@@ -10,8 +10,29 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 import os
 import shutil
+import sys
+
+_USAGE = """Usage:
+  ./new-proposal.py <title>
+
+Generates a branch and PR for a new proposal with the specified title.
+"""
+
+_PROMPT = """This will:
+  - Create and switch to a new branch named '%s'.
+  - Create a new proposal titled '%s'.
+  - Create a PR for the proposal.
+
+Continue? (Y/n) """
 
 if __name__ == "__main__":
+  # Require an argument.
+  if len(sys.argv) != 2:
+    sys.exit(_USAGE)
+  title = sys.argv[1]
+  # Only use the first 20 chars of the title for branch names.
+  branch = 'proposal-%s' % (title.lower().replace(' ', '-')[0:20])
+
   # Verify git and gh are available.
   git_bin = shutil.which('git')
   if not git_bin:
@@ -19,6 +40,13 @@ if __name__ == "__main__":
   gh_bin = shutil.which('gh')
   if not gh_bin:
     sys.exit('Missing `gh` CLI. https://github.com/cli/cli#installation')
+
+  # Prompt before proceeding.
+  response = "?"
+  while response not in ("y", "n", ""):
+    response = input(_PROMPT % (branch, title)).lower()
+  if response == "n":
+    sys.exit('Cancelled')
 
   # Create a proposal branch.
   # Copy template.md to new.md.
