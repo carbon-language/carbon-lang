f59311e31dec9d439819d38353ac9fb098f9547b
Arthur Eubanks <aeubanks@google.com>
2020-06-04 13:08:35 -0700

    Mark lambda in use-after-scope-capture.cpp as noinline
    Summary:
    As explained in https://bugs.llvm.org/show_bug.cgi?id=46208,
    symbolization on Windows after inlining and around
    lambdas/std::functions doesn't work very well. Under the new pass
    manager, there is inlining at -O1.
    use-after-scope-capture.cpp checks that the symbolization points to the
    line containing "return x;", but the combination of
    Windows/inlining/lambdas makes the symbolization point to the line
    "f = [&x]() {".
    
    Mark the lambda as noinline since this test is not a test for
    symbolization.
    
    Reviewers: hans, dblaikie, vitalybuka
    
    Subscribers: #sanitizers
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D81193

--- compiler-rt/test/asan/TestCases/use-after-scope-capture.cpp
+++ compiler-rt/test/asan/TestCases/use-after-scope-capture.cpp
@@ -7,7 +7,7 @@ int main() {
   std::function<int()> f;
   {
     int x = 0;
-    f = [&x]() {
+    f = [&x]() __attribute__((noinline)) {
       return x;  // BOOM
       // CHECK: ERROR: AddressSanitizer: stack-use-after-scope
       // CHECK: #0 0x{{.*}} in {{.*}}use-after-scope-capture.cpp:[[@LINE-2]]
