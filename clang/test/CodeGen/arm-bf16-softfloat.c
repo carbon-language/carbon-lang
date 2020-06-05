c31d2134639bb87d6705fe8fb1a46de61bc4cebf
Kadir Cetinkaya <kadircet@google.com>
2020-06-05 13:01:08 +0200

    [clang][test] Put output into temp directory
    To unbreak builds that happen on a read-only directory

--- clang/test/CodeGen/arm-bf16-softfloat.c
+++ clang/test/CodeGen/arm-bf16-softfloat.c
@@ -1,4 +1,4 @@
-// RUN: not %clang -target arm-arm-eabi -march=armv8-a+bf16 -mfloat-abi=soft -c %s 2>&1 | FileCheck %s
+// RUN: not %clang -o %t.out -target arm-arm-eabi -march=armv8-a+bf16 -mfloat-abi=soft -c %s 2>&1 | FileCheck %s
 
 // CHECK: error: __bf16 is not supported on this target
 extern __bf16 var;
