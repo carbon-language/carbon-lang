d1a6e4d2db493c0ce27ccba7f287acfeac1776a6
Evgenii Stepanov <eugenis@google.com>
2020-06-05 10:57:44 -0700

    [hwasan] Disable malloc-fill by default.
    Summary: Non-zero malloc fill is causing way too many hard to debug issues.
    
    Reviewers: kcc, pcc, hctim
    
    Subscribers: #sanitizers, llvm-commits
    
    Tags: #sanitizers
    
    Differential Revision: https://reviews.llvm.org/D81284

--- compiler-rt/test/hwasan/TestCases/malloc_fill.cpp
+++ compiler-rt/test/hwasan/TestCases/malloc_fill.cpp
@@ -1,6 +1,8 @@
 // Check that we fill malloc-ed memory correctly.
 // RUN: %clangxx_hwasan %s -o %t
-// RUN: %run %t | FileCheck %s
+// RUN: %run %t | FileCheck %s --check-prefix=CHECK-0
+// RUN: %env_hwasan_opts=max_malloc_fill_size=20 %run %t | FileCheck %s --check-prefix=CHECK-20-be
+// RUN: %env_hwasan_opts=max_malloc_fill_size=0:malloc_fill_byte=8 %run %t | FileCheck %s --check-prefix=CHECK-0
 // RUN: %env_hwasan_opts=max_malloc_fill_size=10:malloc_fill_byte=8 %run %t | FileCheck %s --check-prefix=CHECK-10-8
 // RUN: %env_hwasan_opts=max_malloc_fill_size=20:malloc_fill_byte=171 %run %t | FileCheck %s --check-prefix=CHECK-20-ab
 
@@ -20,6 +22,7 @@ int main(int argc, char **argv) {
   delete [] x;
 }
 
-// CHECK: -bebebebebebebebebebebebebebebebebebebebebebebebebebebebebebebebebe-
+// CHECK-0: -000000000000000000000000000000000000000000000000000000000000000000-
+// CHECK-20-be: -bebebebebebebebebebebebebebebebebebebebe00000000000000000000000000-
 // CHECK-10-8: -080808080808080808080000000000000000000000000000000000000000000000-
 // CHECK-20-ab: -abababababababababababababababababababab00000000000000000000000000-
