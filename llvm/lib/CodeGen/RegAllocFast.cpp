0671a4c5087d40450603d9d26cf239f1a8b1367e
Matt Arsenault <Matthew.Arsenault@amd.com>
2020-06-10 15:11:19 -0400

    RegAllocFast: Avoid unused method warning in release builds

--- llvm/lib/CodeGen/RegAllocFast.cpp
+++ llvm/lib/CodeGen/RegAllocFast.cpp
@@ -184,7 +184,10 @@ namespace {
     bool isLastUseOfLocalReg(const MachineOperand &MO) const;
 
     void addKillFlag(const LiveReg &LRI);
+#ifndef NDEBUG
     bool verifyRegStateMapping(const LiveReg &LR) const;
+#endif
+
     void killVirtReg(LiveReg &LR);
     void killVirtReg(Register VirtReg);
     void spillVirtReg(MachineBasicBlock::iterator MI, LiveReg &LR);
@@ -381,6 +384,7 @@ void RegAllocFast::addKillFlag(const LiveReg &LR) {
   }
 }
 
+#ifndef NDEBUG
 bool RegAllocFast::verifyRegStateMapping(const LiveReg &LR) const {
   for (MCRegUnitIterator UI(LR.PhysReg, TRI); UI.isValid(); ++UI) {
     if (RegUnitStates[*UI] != LR.VirtReg)
@@ -389,6 +393,7 @@ bool RegAllocFast::verifyRegStateMapping(const LiveReg &LR) const {
 
   return true;
 }
+#endif
 
 /// Mark virtreg as no longer available.
 void RegAllocFast::killVirtReg(LiveReg &LR) {
