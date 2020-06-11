03480e0533765e1e4dd5308b579657547aff7e09
jonmeow <46229924+jonmeow@users.noreply.github.com>
2020-06-11 13:22:17 -0700

    TOC work

--- src/jekyll/theme/js/customscripts.js
+++ src/jekyll/theme/js/customscripts.js
@@ -1,55 +1,68 @@
-$('#mysidebar').height($(".nav").height());
+$('#mysidebar').height($('.nav').height());
 
+$(document).ready(function () {
+  // This handles the automatic toc. Use ## for subheads to auto-generate the
+  // on-page minitoc. If you use html tags, you must supply an ID for the
+  // heading element in order for it to appear in the minitoc.
+  $('#toc').toc({
+    minimumHeaders: 0,
+    listType: 'ul',
+    showSpeed: 0,
+    headers: 'h2,h3,h4'
+  });
 
 $( document ).ready(function() {
 
-    //this script says, if the height of the viewport is greater than 800px, then insert affix class, which makes the nav bar float in a fixed
-    // position as your scroll. if you have a lot of nav items, this height may not work for you.
-    var h = $(window).height();
-    //console.log (h);
-    if (h > 800) {
-        $( "#mysidebar" ).attr("class", "nav affix");
+      // Starting with the TOC header, elements until the next header.
+      child.style.display = 'none';
+      for (++i; i < content.children.length; ++i) {
+        child = content.children[i];
+        if (child.tagName.match(/H[0-9]/)) break;
+        child.style.display = 'none';
+      }
+      break;
     }
-    // activate tooltips. although this is a bootstrap js function, it must be activated this way in your theme.
-    $('[data-toggle="tooltip"]').tooltip({
-        placement : 'top'
-    });
+  }
+
+  // AnchorJS
+  // Note that this needs to be run early, not on document.ready(), or jumps
+  // to the invented IDs will not work on page load.
+  anchors.add('h2,h3,h4,h5');
 });
 
-document.addEventListener("DOMContentLoaded", function(event) {
-    // AnchorJS
-    // Note that this needs to be run early, not on document.ready(), or jumps
-    // to the invented IDs will not work on page load.
-    anchors.add('h2,h3,h4,h5');
-});
+// Needed for nav tabs on pages. See Formatting > Nav tabs for more details.
+// Script from http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
+$(function () {
+  var json, tabsState;
+  $('a[data-toggle="pill"], a[data-toggle="tab"]').on('shown.bs.tab', function (
+    e
+  ) {
+    var href, json, parentId, tabsState;
 
-// needed for nav tabs on pages. See Formatting > Nav tabs for more details.
-// script from http://stackoverflow.com/questions/10523433/how-do-i-keep-the-current-tab-active-with-twitter-bootstrap-after-a-page-reload
-$(function() {
-    var json, tabsState;
-    $('a[data-toggle="pill"], a[data-toggle="tab"]').on('shown.bs.tab', function(e) {
-        var href, json, parentId, tabsState;
+    tabsState = localStorage.getItem('tabs-state');
+    json = JSON.parse(tabsState || '{}');
+    parentId = $(e.target)
+      .parents('ul.nav.nav-pills, ul.nav.nav-tabs')
+      .attr('id');
+    href = $(e.target).attr('href');
+    json[parentId] = href;
 
-        tabsState = localStorage.getItem("tabs-state");
-        json = JSON.parse(tabsState || "{}");
-        parentId = $(e.target).parents("ul.nav.nav-pills, ul.nav.nav-tabs").attr("id");
-        href = $(e.target).attr('href');
-        json[parentId] = href;
+    return localStorage.setItem('tabs-state', JSON.stringify(json));
+  });
 
-        return localStorage.setItem("tabs-state", JSON.stringify(json));
-    });
+  tabsState = localStorage.getItem('tabs-state');
+  json = JSON.parse(tabsState || '{}');
 
-    tabsState = localStorage.getItem("tabs-state");
-    json = JSON.parse(tabsState || "{}");
+  $.each(json, function (containerId, href) {
+    return $('#' + containerId + ' a[href=' + href + ']').tab('show');
+  });
 
-    $.each(json, function(containerId, href) {
-        return $("#" + containerId + " a[href=" + href + "]").tab('show');
-    });
-
-    $("ul.nav.nav-pills, ul.nav.nav-tabs").each(function() {
-        var $this = $(this);
-        if (!json[$this.attr("id")]) {
-            return $this.find("a[data-toggle=tab]:first, a[data-toggle=pill]:first").tab("show");
-        }
-    });
+  $('ul.nav.nav-pills, ul.nav.nav-tabs').each(function () {
+    var $this = $(this);
+    if (!json[$this.attr('id')]) {
+      return $this
+        .find('a[data-toggle=tab]:first, a[data-toggle=pill]:first')
+        .tab('show');
+    }
+  });
 });
