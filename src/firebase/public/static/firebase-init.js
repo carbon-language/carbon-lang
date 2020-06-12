75a407d4ca8a2ef2a580db5a8c6412c35bbfd7f2
jonmeow <46229924+jonmeow@users.noreply.github.com>
2020-06-12 13:55:04 -0700

    Set up pre-commit
    Added here:
    
    - .pre-commit-config.yaml
    - docs/project/contribution_tools.md
    - CONTRIBUTING.md
    
    The rest consists of auto-generated fixes.

--- src/firebase/public/static/firebase-init.js
+++ src/firebase/public/static/firebase-init.js
@@ -1,10 +1,10 @@
 // See https://carbon-lang.dev/__/firebase/init.js.
 // We customize authDomain.
 firebase.initializeApp({
-  "apiKey": "AIzaSyDyAAcQeReDNXVUd_ZdkSzYgqIDJe-kPK8",
-  "authDomain": "carbon-lang.dev",
-  "databaseURL": "https://carbon-language.firebaseio.com",
-  "messagingSenderId": "985662022432",
-  "projectId": "carbon-language",
-  "storageBucket": "carbon-language.appspot.com"
+  apiKey: 'AIzaSyDyAAcQeReDNXVUd_ZdkSzYgqIDJe-kPK8',
+  authDomain: 'carbon-lang.dev',
+  databaseURL: 'https://carbon-language.firebaseio.com',
+  messagingSenderId: '985662022432',
+  projectId: 'carbon-language',
+  storageBucket: 'carbon-language.appspot.com',
 });
