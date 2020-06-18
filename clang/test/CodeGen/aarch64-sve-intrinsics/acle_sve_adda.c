108560e58c41fc70341d763b941a0d6c24c4d3a9
Hans Wennborg <hans@chromium.org>
2020-06-18 11:59:42 +0200

    Try to remove the acle_sve_adda.s file that's breaking tests

--- clang/test/CodeGen/aarch64-sve-intrinsics/acle_sve_adda.c
+++ clang/test/CodeGen/aarch64-sve-intrinsics/acle_sve_adda.c
@@ -1,3 +1,5 @@
+// REQUIRES: shell
+// RUN: rm -f -- "%S/acle_sve_adda.s"
 // RUN: %clang_cc1 -D__ARM_FEATURE_SVE -triple aarch64-none-linux-gnu -target-feature +sve -fallow-half-arguments-and-returns -S -O1 -Werror -Wall -emit-llvm -o - %s | FileCheck %s
 // RUN: %clang_cc1 -D__ARM_FEATURE_SVE -DSVE_OVERLOADED_FORMS -triple aarch64-none-linux-gnu -target-feature +sve -fallow-half-arguments-and-returns -S -O1 -Werror -Wall -emit-llvm -o - %s | FileCheck %s
 
