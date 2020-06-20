ab888262b32bcdbf9c117e29b90b88c27859bb35
Raphael Isemann <teemperor@gmail.com>
2020-06-20 11:22:44 +0200

    [lldb] Skip TestBuiltinFormats.py on arm for now

--- lldb/test/API/functionalities/data-formatter/builtin-formats/TestBuiltinFormats.py
+++ lldb/test/API/functionalities/data-formatter/builtin-formats/TestBuiltinFormats.py
@@ -23,6 +23,8 @@ class TestCase(TestBase):
 
     @no_debug_info_test
     @skipIfWindows
+    # uint128_t not available on arm.
+    @skipIf(archs=['arm', 'aarch64'])
     def test(self):
         self.build()
         lldbutil.run_to_source_breakpoint(self, "// break here", lldb.SBFileSpec("main.cpp"))
