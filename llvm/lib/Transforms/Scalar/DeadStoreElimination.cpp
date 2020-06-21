40569db7b3d71b5f4f71108015165c6224b0b242
Florian Hahn <flo@fhahn.com>
2020-06-21 16:34:54 +0100

    [DSE,MSSA] Move reachability check to main loop.
    As we traverse the CFG backwards, we could end up reaching unreachable
    blocks. For unreachable blocks, we won't have computed post order
    numbers and because DomAccess is reachable, unreachable blocks cannot be
    on any path from it.
    
    This fixes a crash with unreachable blocks.

--- llvm/lib/Transforms/Scalar/DeadStoreElimination.cpp
+++ llvm/lib/Transforms/Scalar/DeadStoreElimination.cpp
@@ -1802,11 +1802,8 @@ struct DSEState {
         if (CommonPred)
           WorkList.insert(CommonPred);
         else
-          for (BasicBlock *R : PDT.getRoots()) {
-            if (!DT.isReachableFromEntry(R))
-              continue;
+          for (BasicBlock *R : PDT.getRoots())
             WorkList.insert(R);
-          }
 
         NumCFGTries++;
         // Check if all paths starting from an exit node go through one of the
@@ -1818,6 +1815,12 @@ struct DSEState {
             continue;
           if (Current == DomAccess->getBlock())
             return None;
+
+          // DomAccess is reachable from the entry, so we don't have to explore
+          // unreachable blocks further.
+          if (!DT.isReachableFromEntry(Current))
+            continue;
+
           unsigned CPO = PostOrderNumbers.find(Current)->second;
           // Current block is not on a path starting at DomAccess.
           if (CPO > UpperBound)
