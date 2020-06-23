985cfadfc930a2f117d0bda6804e37fd9f8d11d7
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-06-23 12:04:05 +0100

    DWARFLinkerDeclContext.h - fix implicit FileSystem.h dependency. NFC.
    Was relying on other headers to define sys::fs::real_path for use in CachedPathResolver::resolve inlines

--- llvm/include/llvm/DWARFLinker/DWARFLinkerDeclContext.h
+++ llvm/include/llvm/DWARFLinker/DWARFLinkerDeclContext.h
@@ -16,6 +16,7 @@
 #include "llvm/CodeGen/NonRelocatableStringpool.h"
 #include "llvm/DWARFLinker/DWARFLinkerCompileUnit.h"
 #include "llvm/DebugInfo/DWARF/DWARFDie.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/Path.h"
 
 namespace llvm {
