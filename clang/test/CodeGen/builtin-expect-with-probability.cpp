47fb21d2ea903fc4cce38f8da8160cf0eacc16d0
Zhi Zhuang <Zhi.Zhuang@mediatek.com>
2020-06-23 13:30:23 -0700

    fix test failure for clang/test/CodeGen/builtin-expect-with-probability.cpp
    Fix test case added by D79830
    Rewrite the test case, which did similar thing as builtin-expect.c
    does(test generated llvm intrinsic instead of test branch weights).
    Currently pass by "-disable-llvm-passes" option.
    
    Differential Revision: https://reviews.llvm.org/D82403

--- clang/test/CodeGen/builtin-expect-with-probability.cpp
+++ clang/test/CodeGen/builtin-expect-with-probability.cpp
@@ -1,14 +1,38 @@
-// RUN: %clang_cc1 -emit-llvm -o - %s -O1 | FileCheck %s
-// RUN: %clang_cc1 -emit-llvm -o - -fexperimental-new-pass-manager %s -O1 | FileCheck %s
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -emit-llvm -o - %s -O1 -disable-llvm-passes | FileCheck %s --check-prefix=ALL --check-prefix=O1
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -emit-llvm -o - %s -O0 | FileCheck %s --check-prefix=ALL --check-prefix=O0
 extern int global;
+
+int expect_taken(int x) {
+// ALL-LABEL: expect_taken
+// O1:        call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 1, double 9.000000e-01)
+// O0-NOT:    @llvm.expect.with.probability
+
+  if (__builtin_expect_with_probability(x == 100, 1, 0.9)) {
+    return 0;
+  }
+  return x;
+}
+
+int expect_not_taken(int x) {
+// ALL-LABEL: expect_not_taken
+// O1:        call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 0, double 9.000000e-01)
+// O0-NOT:    @llvm.expect.with.probability
+
+  if (__builtin_expect_with_probability(x == 100, 0, 0.9)) {
+    return 0;
+  }
+  return x;
+}
 
 struct S {
   static constexpr int prob = 1;
 };
 
 template<typename T>
-int expect_taken(int x) {
-// CHECK: !{{[0-9]+}} = !{!"branch_weights", i32 2147483647, i32 1}
+int expect_taken_template(int x) {
+// ALL-LABEL: expect_taken_template
+// O1:        call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 1, double 1.000000e+00)
+// O0-NOT:    @llvm.expect.with.probability
 
 	if (__builtin_expect_with_probability (x == 100, 1, T::prob)) {
 		return 0;
@@ -17,20 +41,31 @@ int expect_taken(int x) {
 }
 
 int f() {
-  return expect_taken<S>(global);
+  return expect_taken_template<S>(global);
 }
 
-int expect_taken2(int x) {
-  // CHECK: !{{[0-9]+}} = !{!"branch_weights", i32 1932735283, i32 214748366}
+int x;
+extern "C" {
+  int y(void);
+}
+void foo();
 
-  if (__builtin_expect_with_probability(x == 100, 1, 0.9)) {
-    return 0;
-  }
-  return x;
+void expect_value_side_effects() {
+// ALL-LABEL: expect_value_side_effects
+// ALL: [[CALL:%.*]] = call i32 @y
+// O1:  [[SEXT:%.*]] = sext i32 [[CALL]] to i64
+// O1:  call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 [[SEXT]], double 6.000000e-01)
+// O0-NOT: @llvm.expect.with.probability
+
+  if (__builtin_expect_with_probability(x, y(), 0.6))
+    foo();
 }
 
-int expect_taken3(int x) {
-  // CHECK: !{{[0-9]+}} = !{!"branch_weights", i32 107374184, i32 107374184, i32 1717986918, i32 107374184, i32 107374184}
+int switch_cond(int x) {
+// ALL-LABEL: switch_cond
+// O1:        call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 1, double 8.000000e-01)
+// O0-NOT:    @llvm.expect.with.probability
+
   switch (__builtin_expect_with_probability(x, 1, 0.8)) {
   case 0:
     x = x + 0;
@@ -44,4 +79,23 @@ int expect_taken3(int x) {
     x = x + 6;
   }
   return x;
+}
+
+constexpr double prob = 0.8;
+
+int variable_expected(int stuff) {
+// ALL-LABEL: variable_expected
+// O1: call i64 @llvm.expect.with.probability.i64(i64 {{%.*}}, i64 {{%.*}}, double 8.000000e-01)
+// O0-NOT: @llvm.expect.with.probability
+
+  int res = 0;
+
+  switch(__builtin_expect_with_probability(stuff, stuff, prob)) {
+    case 0:
+      res = 1;
+      break;
+    default:
+      break;
+  }
+  return res;
 }
