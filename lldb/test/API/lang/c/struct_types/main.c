cad79f73b6b67c9306ad147381e8a8be5db6d5cf
Raphael Isemann <teemperor@gmail.com>
2020-06-24 20:28:00 +0200

    [lldb][NFC] Use expect_expr in TestStructTypes.py

--- lldb/test/API/lang/c/struct_types/main.c
+++ lldb/test/API/lang/c/struct_types/main.c
@@ -17,7 +17,7 @@ int main (int argc, char const *argv[])
         char padding[0];
     }; //% self.expect("frame variable pt.padding[0]", DATA_TYPES_DISPLAYED_CORRECTLY, substrs = ["pt.padding[0] = "])
        //% self.expect("frame variable pt.padding[1]", DATA_TYPES_DISPLAYED_CORRECTLY, substrs = ["pt.padding[1] = "])
-       //% self.expect("expression -- (pt.padding[0])", DATA_TYPES_DISPLAYED_CORRECTLY, substrs = ["(char)", " = "])
+       //% self.expect_expr("pt.padding[0]", result_type="char")
        //% self.expect("image lookup -t point_tag", DATA_TYPES_DISPLAYED_CORRECTLY, substrs = ['padding[]'])
 
     struct {} empty;
@@ -32,7 +32,7 @@ int main (int argc, char const *argv[])
     struct rect_tag rect = {{1, 2, {}}, {3, 4, {}}};
     struct things_to_sum tts = { 2, 3, 4 };
 
-    int sum = sum_things(tts); //% self.expect("expression -- &pt == (struct point_tag*)0", substrs = ['false'])
-                               //% self.expect("expression -- sum_things(tts)", substrs = ['9'])
+    int sum = sum_things(tts); //% self.expect_expr("&pt == (struct point_tag*)0", result_value="false")
+                               //% self.expect_expr("sum_things(tts)", result_value="9")
     return 0;
 }
