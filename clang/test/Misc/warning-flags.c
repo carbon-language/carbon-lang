16501782c8d849bc1812d527dc8466574700663d
Elvina Yakubova <elvina.yakubova@huawei.com>
2020-06-25 16:53:23 +0300

    [Clang] Add support for -Wno-inline-namespace-reopened-noninline
    This patch adds the option for disabling warn_inline_namespace_reopened_noninline
    warning described here: https://bugs.llvm.org/show_bug.cgi?id=46106
    
    Patch by Elvina Yakubova
    
    Differential Revision: https://reviews.llvm.org/D81825

--- clang/test/Misc/warning-flags.c
+++ clang/test/Misc/warning-flags.c
@@ -18,7 +18,7 @@ This test serves two purposes:
 
 The list of warnings below should NEVER grow.  It should gradually shrink to 0.
 
-CHECK: Warnings without flags (69):
+CHECK: Warnings without flags (68):
 
 CHECK-NEXT:   ext_expected_semi_decl_list
 CHECK-NEXT:   ext_explicit_specialization_storage_class
@@ -58,7 +58,6 @@ CHECK-NEXT:   warn_fe_macro_contains_embedded_newline
 CHECK-NEXT:   warn_ignoring_ftabstop_value
 CHECK-NEXT:   warn_implements_nscopying
 CHECK-NEXT:   warn_incompatible_qualified_id
-CHECK-NEXT:   warn_inline_namespace_reopened_noninline
 CHECK-NEXT:   warn_invalid_asm_cast_lvalue
 CHECK-NEXT:   warn_maynot_respond
 CHECK-NEXT:   warn_method_param_redefinition
