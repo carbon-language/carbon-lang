c061e56e880a20488e0f7e6cf9975aa24b83067c
David Sherwood <david.sherwood@arm.com>
2020-06-29 09:39:22 +0100

    [CodeGen] Fix warnings in sve-vector-splat.ll and sve-trunc.ll
    This patch fixes all remaining warnings in:
    
      llvm/test/CodeGen/AArch64/sve-trunc.ll
      llvm/test/CodeGen/AArch64/sve-vector-splat.ll
    
    I hit some warnings related to getCopyPartsToVector. I fixed two
    issues:
    
    1. In widenVectorToPartType() we assumed that we'd always be
    using BUILD_VECTOR nodes to expand from one vector type to another,
    which is incorrect for scalable vector types. I've fixed this for now
    by simply bailing out immediately for scalable vectors.
    2. In getCopyToPartsVector() I've changed the code to compare
    the element counts of different types.
    
    Differential Revision: https://reviews.llvm.org/D83028

--- llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
+++ llvm/lib/CodeGen/SelectionDAG/SelectionDAGBuilder.cpp
@@ -656,7 +656,7 @@ static void getCopyToParts(SelectionDAG &DAG, const SDLoc &DL, SDValue Val,
 
 static SDValue widenVectorToPartType(SelectionDAG &DAG,
                                      SDValue Val, const SDLoc &DL, EVT PartVT) {
-  if (!PartVT.isVector())
+  if (!PartVT.isFixedLengthVector())
     return SDValue();
 
   EVT ValueVT = Val.getValueType();
@@ -702,8 +702,9 @@ static void getCopyToPartsVector(SelectionDAG &DAG, const SDLoc &DL,
       Val = Widened;
     } else if (PartVT.isVector() &&
                PartEVT.getVectorElementType().bitsGE(
-                 ValueVT.getVectorElementType()) &&
-               PartEVT.getVectorNumElements() == ValueVT.getVectorNumElements()) {
+                   ValueVT.getVectorElementType()) &&
+               PartEVT.getVectorElementCount() ==
+                   ValueVT.getVectorElementCount()) {
 
       // Promoted vector extract
       Val = DAG.getAnyExtOrTrunc(Val, DL, PartVT);
