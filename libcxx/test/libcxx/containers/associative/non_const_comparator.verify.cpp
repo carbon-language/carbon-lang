73370b2c0ce80810ff945f763dd61a98778a8e8f
Louis Dionne <ldionne@apple.com>
2020-06-30 14:16:00 -0400

    [libc++] Add missing include in test

--- libcxx/test/libcxx/containers/associative/non_const_comparator.verify.cpp
+++ libcxx/test/libcxx/containers/associative/non_const_comparator.verify.cpp
@@ -12,8 +12,9 @@
 // Test that libc++ generates a warning diagnostic when the container is
 // provided a non-const callable comparator.
 
-#include <set>
 #include <map>
+#include <set>
+#include <type_traits> // for __invokable
 
 struct BadCompare {
   template <class T, class U>
