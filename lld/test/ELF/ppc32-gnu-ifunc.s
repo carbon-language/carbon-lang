f5e0ae240f2daeee214097f222e12cd23706b1db
Fangrui Song <maskray@google.com>
2020-06-30 22:05:01 -0700

    [ELF][test] Fix ppc32-gnu-ifunc.s and remove XFAIL: *

--- lld/test/ELF/ppc32-gnu-ifunc.s
+++ lld/test/ELF/ppc32-gnu-ifunc.s
@@ -1,5 +1,4 @@
 # REQUIRES: ppc
-# XFAIL: *
 # RUN: llvm-mc -filetype=obj -triple=powerpc %s -o %t.o
 # RUN: ld.lld %t.o -o %t
 # RUN: llvm-readobj -r %t | FileCheck --check-prefix=RELOC %s
@@ -7,22 +6,22 @@
 # RUN: llvm-objdump -d --no-show-raw-insn %t | FileCheck %s
 
 # RELOC:      .rela.dyn {
-# RELOC-NEXT:   0x10020114 R_PPC_IRELATIVE - 0x100100E0
+# RELOC-NEXT:   0x10020118 R_PPC_IRELATIVE - 0x100100E0
 # RELOC-NEXT: }
 
 # SEC: .rela.dyn RELA 100000d4 0000d4 00000c
 # SYM: 100100e0 0 FUNC GLOBAL DEFAULT {{.*}} func
 
-# CHECK:      func_resolver:
-# CHECK-NEXT:   100100e0:
-# CHECK:      _start:
-# CHECK-NEXT:   bl .+20
+# CHECK-LABEL: <func_resolver>:
+# CHECK-NEXT:    100100e0:
+# CHECK-LABEL: <_start>:
+# CHECK-NEXT:    bl 0x100100f8
 ## .rela.plt = 0x100000d4 = 65536*4096+212
 ## end(.rela.plt) = 0x100000d4+0xc = 65536*4096+224
-# CHECK-NEXT:   lis 9, 4096
-# CHECK-NEXT:   lis 8, 4096
-# CHECK-NEXT:   addi 9, 9, 212
-# CHECK-NEXT:   addi 8, 8, 224
+# CHECK-NEXT:    lis 9, 4096
+# CHECK-NEXT:    lis 8, 4096
+# CHECK-NEXT:    addi 9, 9, 212
+# CHECK-NEXT:    addi 8, 8, 224
 
 .globl func
 .type func, @gnu_indirect_function
