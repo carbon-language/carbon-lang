1276855f2b4485ec312b379c1b8eaf5510d9b157
Pavel Labath <pavel@labath.sk>
2020-07-01 16:55:34 +0200

    [lldb] Attempt to fix TestLimitDebugInfo on windows
    The test fails due to link errors. I believe this change should fix
    that.

--- lldb/test/API/functionalities/limit-debug-info/onetwo.h
+++ lldb/test/API/functionalities/limit-debug-info/onetwo.h
@@ -1,10 +1,22 @@
-struct One {
+#ifdef IN_ONE
+#define ONE_API LLDB_DYLIB_EXPORT
+#else
+#define ONE_API LLDB_DYLIB_IMPORT
+#endif
+
+#ifdef IN_TWO
+#define TWO_API LLDB_DYLIB_EXPORT
+#else
+#define TWO_API LLDB_DYLIB_IMPORT
+#endif
+
+struct ONE_API One {
   int one = 142;
   constexpr One() = default;
   virtual ~One();
 };
 
-struct Two : One {
+struct TWO_API Two : One {
   int two = 242;
   constexpr Two() = default;
   ~Two() override;
