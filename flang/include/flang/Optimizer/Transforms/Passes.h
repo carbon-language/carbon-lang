5c02a2421b951a14d1294598d0082b32659c7082
Eric Schweitz <eschweitz@nvidia.com>
2020-07-01 17:00:02 -0700

    [flang] Add inliner pass.
    This adds a minimalist inliner implementation. Along with the inliner, a
    minimum number of support files are also included. These will pave the
    way for future diffs to add more transformation passes to flang. A
    future diff will add the inline test, which cannot be run successfully
    quite yet as some components have not yet been upstreamed.
    
    Differential revision:

---
+++ flang/include/flang/Optimizer/Transforms/Passes.h
@@ -0,0 +1,58 @@
+//===-- Optimizer/Transforms/Passes.h ---------------------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef OPTIMIZER_TRANSFORMS_PASSES_H
+#define OPTIMIZER_TRANSFORMS_PASSES_H
+
+#include "mlir/Pass/Pass.h"
+#include "mlir/Pass/PassRegistry.h"
+#include <memory>
+
+namespace mlir {
+class BlockAndValueMapping;
+class Operation;
+class Pass;
+class Region;
+} // namespace mlir
+
+namespace fir {
+
+/// Convert fir.select_type to the standard dialect
+std::unique_ptr<mlir::Pass> createControlFlowLoweringPass();
+
+/// Effects aware CSE pass
+std::unique_ptr<mlir::Pass> createCSEPass();
+
+/// Convert FIR loop constructs to the Affine dialect
+std::unique_ptr<mlir::Pass> createPromoteToAffinePass();
+
+/// Convert `fir.do_loop` and `fir.if` to a CFG.  This
+/// conversion enables the `createLowerToCFGPass` to transform these to CFG
+/// form.
+std::unique_ptr<mlir::Pass> createFirToCfgPass();
+
+/// A pass to convert the FIR dialect from "Mem-SSA" form to "Reg-SSA"
+/// form. This pass is a port of LLVM's mem2reg pass, but modified for the FIR
+/// dialect as well as the restructuring of MLIR's representation to present PHI
+/// nodes as block arguments.
+std::unique_ptr<mlir::Pass> createMemToRegPass();
+
+/// Support for inlining on FIR.
+bool canLegallyInline(mlir::Operation *op, mlir::Region *reg,
+                      mlir::BlockAndValueMapping &map);
+
+inline void registerOptTransformPasses() {
+using mlir::Pass;
+// declarative passes
+#define GEN_PASS_REGISTRATION
+#include "flang/Optimizer/Transforms/Passes.h.inc"
+}
+
+} // namespace fir
+
+#endif // OPTIMIZER_TRANSFORMS_PASSES_H
