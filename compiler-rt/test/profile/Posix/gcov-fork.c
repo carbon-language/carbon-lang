1c6e2eceeb1fe9faa9f7e95424e2a7f1b1d0fb9a
Fangrui Song <maskray@google.com>
2020-07-03 17:06:54 -0700

    [gcov][test] Add `UNSUPPORTED: host-byteorder-big-endian` to gcov-fork.c
    This test strangely failed on ppc64be
    http://lab.llvm.org:8011/builders/clang-ppc64be-linux/builds/50913

--- compiler-rt/test/profile/Posix/gcov-fork.c
+++ compiler-rt/test/profile/Posix/gcov-fork.c
@@ -2,6 +2,8 @@
 /// fork/exec* so the lines before fork are counted once while succeeding
 /// lines are counted twice.
 // UNSUPPORTED: darwin
+/// FIXME: http://lab.llvm.org:8011/builders/clang-ppc64be-linux/builds/50913
+// UNSUPPORTED: host-byteorder-big-endian
 
 // RUN: mkdir -p %t.d && cd %t.d
 // RUN: %clang --coverage %s -o %t
@@ -16,7 +18,7 @@ void func1() {}                    // CHECK:      1: [[#@LINE]]:void func1()
 void func2() {}                    // CHECK-NEXT: 2: [[#@LINE]]:
 int main(void) {                   // CHECK-NEXT: 1: [[#@LINE]]:
   func1();                         // CHECK-NEXT: 1: [[#@LINE]]:
-  fork();                          // CHECK-NEXT: 1: [[#@LINE]]:
+  if (fork() == -1) return 1;      // CHECK-NEXT: 1: [[#@LINE]]:
   func2();                         // CHECK-NEXT: 2: [[#@LINE]]:
   return 0;                        // CHECK-NEXT: 2: [[#@LINE]]:
 }
