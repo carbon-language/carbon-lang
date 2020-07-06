3c7e8d6d0eb0660fb8fbae98c3e49ca059943416
Amara Emerson <amara@apple.com>
2020-07-06 15:12:12 -0700

    Fix sdk version test to use 99.99.99 as a max dummy version instead of 10.99.99.
    Was failing on macOS 11 hosts which is > 10.99.99

--- clang/test/Driver/darwin-sdk-vs-os-version.c
+++ clang/test/Driver/darwin-sdk-vs-os-version.c
@@ -2,9 +2,9 @@
 
 // Ensure that we never pick a version that's based on the SDK that's newer than
 // the system version:
-// RUN: rm -rf %t/SDKs/MacOSX10.99.99.sdk
-// RUN: mkdir -p %t/SDKs/MacOSX10.99.99.sdk
-// RUN: %clang -target x86_64-apple-darwin -isysroot %t/SDKs/MacOSX10.99.99.sdk %s -### 2>&1 \
+// RUN: rm -rf %t/SDKs/MacOSX99.99.99.sdk
+// RUN: mkdir -p %t/SDKs/MacOSX99.99.99.sdk
+// RUN: %clang -target x86_64-apple-darwin -isysroot %t/SDKs/MacOSX99.99.99.sdk %s -### 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-MACOSX-SYSTEM-VERSION %s
 
-// CHECK-MACOSX-SYSTEM-VERSION-NOT: 10.99.99"
+// CHECK-MACOSX-SYSTEM-VERSION-NOT: 99.99.99"
