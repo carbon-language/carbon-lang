efcb8a190395ad35dcd6931b16c7b0bd1ccd37cf
Yuanfang Chen <yuanfang.chen@sony.com>
2020-07-07 09:39:55 -0700

    [NFC] remove unneeded TargetLoweringObjectFile init after 85c30f3374d9

--- llvm/lib/CodeGen/LLVMTargetMachine.cpp
+++ llvm/lib/CodeGen/LLVMTargetMachine.cpp
@@ -196,18 +196,9 @@ bool LLVMTargetMachine::addPassesToEmitFile(
   if (!PassConfig)
     return true;
 
-  if (!TargetPassConfig::willCompleteCodeGenPipeline()) {
-    if (this->getTargetTriple().isOSAIX()) {
-      // On AIX, we might manifest MCSymbols during SDAG lowering. For MIR
-      // testing to be meaningful, we need to ensure that the symbols created
-      // are MCSymbolXCOFF variants, which requires that
-      // the TargetLoweringObjectFile instance has been initialized.
-      MCContext &Ctx = MMIWP->getMMI().getContext();
-      const_cast<TargetLoweringObjectFile &>(*this->getObjFileLowering())
-          .Initialize(Ctx, *this);
-    }
+  if (!TargetPassConfig::willCompleteCodeGenPipeline())
     PM.add(createPrintMIRPass(Out));
-  } else if (addAsmPrinter(PM, Out, DwoOut, FileType,
+  else if (addAsmPrinter(PM, Out, DwoOut, FileType,
                            MMIWP->getMMI().getContext()))
     return true;
 
