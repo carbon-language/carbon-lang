7a7d50e1f0d84c701fd2aa1b84a73a3e194fb91a
Bruno Ricci <riccibrun@gmail.com>
2020-07-08 16:36:48 +0100

    [clang][NFC] Also test for serialization in test/AST/ast-dump-APValue-*
    This does not actually exercise the serialization of APValue, but it
    will at least prevent a regression in the future. NFC.

--- clang/test/AST/ast-dump-APValue-todo.cpp
+++ clang/test/AST/ast-dump-APValue-todo.cpp
@@ -1,5 +1,13 @@
+// Test without serialization:
 // RUN: %clang_cc1 -triple x86_64-unknown-unknown -Wno-unused-value -std=gnu++17 \
 // RUN:            -ast-dump %s -ast-dump-filter Test \
+// RUN: | FileCheck --strict-whitespace --match-full-lines %s
+//
+// Test with serialization:
+// RUN: %clang_cc1 -triple x86_64-unknown-unknown -Wno-unused-value -std=gnu++17 -emit-pch -o %t %s
+// RUN: %clang_cc1 -x c++ -triple x86_64-unknown-unknown -Wno-unused-value -std=gnu++17 \
+// RUN:           -include-pch %t -ast-dump-all -ast-dump-filter Test /dev/null \
+// RUN: | sed -e "s/ <undeserialized declarations>//" -e "s/ imported//" \
 // RUN: | FileCheck --strict-whitespace --match-full-lines %s
 
 int i;
