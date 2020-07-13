9908a3b9f521c954cbf6adcec35b14b2f6c8da49
Davide Italiano <ditaliano@apple.com>
2020-07-13 12:59:16 -0700

    Revert "[llvm] Native size estimator for training -Oz inliner"
    This reverts commit 83080a294ad7d145d758821bcf4354ad0cb7d299 as
    it breaks the macOS modules build.

--- llvm/lib/Passes/PassBuilder.cpp
+++ llvm/lib/Passes/PassBuilder.cpp
@@ -35,7 +35,6 @@
 #include "llvm/Analysis/IVUsers.h"
 #include "llvm/Analysis/InlineAdvisor.h"
 #include "llvm/Analysis/InlineFeaturesAnalysis.h"
-#include "llvm/Analysis/InlineSizeEstimatorAnalysis.h"
 #include "llvm/Analysis/LazyCallGraph.h"
 #include "llvm/Analysis/LazyValueInfo.h"
 #include "llvm/Analysis/LoopAccessAnalysis.h"
