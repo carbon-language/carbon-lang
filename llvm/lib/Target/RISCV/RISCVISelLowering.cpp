31b52b4345e36b169a2b6a89eac44651f59889dd
lewis-revill <lewis.revill@embecosm.com>
2020-07-15 11:53:06 +0100

    [RISCV] Add matching of codegen patterns to RISCV Bit Manipulation Zbp asm instructions
    This patch provides optimization of bit manipulation operations by
    enabling the +experimental-b target feature.
    It adds matching of single block patterns of instructions to specific
    bit-manip instructions from the permutation subset (zbp subextension) of
    the experimental B extension of RISC-V.
    It adds also the correspondent codegen tests.
    
    This patch is based on Claire Wolf's proposal for the bit manipulation
    extension of RISCV:
    https://github.com/riscv/riscv-bitmanip/blob/master/bitmanip-0.92.pdf
    
    Differential Revision: https://reviews.llvm.org/D79871

--- llvm/lib/Target/RISCV/RISCVISelLowering.cpp
+++ llvm/lib/Target/RISCV/RISCVISelLowering.cpp
@@ -151,13 +151,18 @@ RISCVTargetLowering::RISCVTargetLowering(const TargetMachine &TM,
 
   setOperationAction(ISD::ROTL, XLenVT, Expand);
   setOperationAction(ISD::ROTR, XLenVT, Expand);
-  setOperationAction(ISD::BSWAP, XLenVT, Expand);
+
+  if (!Subtarget.hasStdExtZbp())
+    setOperationAction(ISD::BSWAP, XLenVT, Expand);
 
   if (!Subtarget.hasStdExtZbb()) {
     setOperationAction(ISD::CTTZ, XLenVT, Expand);
     setOperationAction(ISD::CTLZ, XLenVT, Expand);
     setOperationAction(ISD::CTPOP, XLenVT, Expand);
   }
+
+  if (Subtarget.hasStdExtZbp())
+    setOperationAction(ISD::BITREVERSE, XLenVT, Legal);
 
   ISD::CondCode FPCCToExtend[] = {
       ISD::SETOGT, ISD::SETOGE, ISD::SETONE, ISD::SETUEQ, ISD::SETUGT,
