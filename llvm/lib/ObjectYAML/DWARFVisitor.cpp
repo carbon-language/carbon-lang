dc65f57124a81511b085396dea621f585382299c
Xing GUO <higuoxing@gmail.com>
2020-07-17 11:28:29 +0800

    [DWARFYAML] Merge forms that use same encodings. NFC.

--- llvm/lib/ObjectYAML/DWARFVisitor.cpp
+++ llvm/lib/ObjectYAML/DWARFVisitor.cpp
@@ -118,6 +118,10 @@ template <typename T> Error DWARFYAML::VisitorImpl<T>::traverseDebugInfo() {
           case dwarf::DW_FORM_addrx:
           case dwarf::DW_FORM_rnglistx:
           case dwarf::DW_FORM_loclistx:
+          case dwarf::DW_FORM_udata:
+          case dwarf::DW_FORM_ref_udata:
+          case dwarf::DW_FORM_GNU_addr_index:
+          case dwarf::DW_FORM_GNU_str_index:
             onValue((uint64_t)FormVal->Value, /*LEB=*/true);
             break;
           case dwarf::DW_FORM_data1:
@@ -143,14 +147,11 @@ template <typename T> Error DWARFYAML::VisitorImpl<T>::traverseDebugInfo() {
           case dwarf::DW_FORM_data8:
           case dwarf::DW_FORM_ref8:
           case dwarf::DW_FORM_ref_sup8:
+          case dwarf::DW_FORM_ref_sig8:
             onValue((uint64_t)FormVal->Value);
             break;
           case dwarf::DW_FORM_sdata:
             onValue((int64_t)FormVal->Value, true);
-            break;
-          case dwarf::DW_FORM_udata:
-          case dwarf::DW_FORM_ref_udata:
-            onValue((uint64_t)FormVal->Value, true);
             break;
           case dwarf::DW_FORM_string:
             onValue(FormVal->CStr);
@@ -168,13 +169,6 @@ template <typename T> Error DWARFYAML::VisitorImpl<T>::traverseDebugInfo() {
           case dwarf::DW_FORM_line_strp:
           case dwarf::DW_FORM_strp_sup:
             onVariableSizeValue(FormVal->Value, getOffsetSize(Unit));
-            break;
-          case dwarf::DW_FORM_ref_sig8:
-            onValue((uint64_t)FormVal->Value);
-            break;
-          case dwarf::DW_FORM_GNU_addr_index:
-          case dwarf::DW_FORM_GNU_str_index:
-            onValue((uint64_t)FormVal->Value, true);
             break;
           default:
             break;
