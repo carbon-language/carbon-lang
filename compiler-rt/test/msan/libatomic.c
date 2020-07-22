33d239513c881d8c11c60d5710c55cf56cc309a5
Gui Andrade <guiand@google.com>
2020-07-22 16:34:55 +0000

    [MSAN] Instrument libatomic load/store calls
    These calls are neither intercepted by compiler-rt nor is libatomic.a
    naturally instrumented.
    
    This patch uses the existing libcall mechanism to detect a call
    to atomic_load or atomic_store, and instruments them much like
    the preexisting instrumentation for atomics.
    
    Calls to _load are modified to have at least Acquire ordering, and
    calls to _store at least Release ordering. Because this needs to be
    converted at runtime, msan injects a LUT (implemented as a vector
    with extractelement).
    
    Differential Revision: https://reviews.llvm.org/D83337

---
+++ compiler-rt/test/msan/libatomic.c
@@ -0,0 +1,37 @@
+// RUN: %clangxx_msan -fsanitize-memory-track-origins=2 -latomic -DTEST_STORE -O0 %s -o %t && %run %t 2>&1
+// RUN: %clangxx_msan -fsanitize-memory-track-origins=0 -latomic -DTEST_LOAD -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK
+// RUN: %clangxx_msan -fsanitize-memory-track-origins=2 -latomic -DTEST_LOAD -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s --check-prefix=CHECK --check-prefix=CHECK-SHADOW
+
+#include <sanitizer/msan_interface.h>
+#include <stdatomic.h>
+
+typedef struct __attribute((packed)) {
+  uint8_t val[3];
+} i24;
+
+void copy(i24 *dst, i24 *src);
+
+int main() {
+  i24 uninit;
+  i24 init = {0};
+
+  __msan_check_mem_is_initialized(&init, 3);
+  copy(&init, &uninit);
+  __msan_check_mem_is_initialized(&init, 3);
+}
+
+void copy(i24 *dst, i24 *src) {
+#ifdef TEST_LOAD
+  __atomic_load(src, dst, __ATOMIC_RELAXED);
+
+  // CHECK: MemorySanitizer: use-of-uninitialized-value
+  // CHECK: #0 {{0x[a-f0-9]+}} in main{{.*}}libatomic.c:[[@LINE-8]]
+
+  // CHECK-SHADOW: Uninitialized value was stored to memory at
+  // CHECK-SHADOW: #0 {{0x[a-f0-9]+}} in copy{{.*}}libatomic.c:[[@LINE-6]]
+#endif
+#ifdef TEST_STORE
+  // Store always writes a clean shadow
+  __atomic_store(src, dst, __ATOMIC_RELAXED);
+#endif
+}
