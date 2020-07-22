216b67e2023315ff30c2802c911a8ae0c7640c30
Tim Northover <t.p.northover@gmail.com>
2020-07-22 14:53:26 +0100

    AArch64: diagnose out of range relocation addends on MachO.
    MachO only has 24-bit addends for most relocations, small enough that it can
    overflow in semi-reasonable functions and cause insidious bugs if compiled
    without assertions enabled. Switch it to an actual error instead.
    
    The condition isn't quite identical because ld64 treats the addend as a signed
    number.

--- llvm/lib/Target/AArch64/MCTargetDesc/AArch64MachObjectWriter.cpp
+++ llvm/lib/Target/AArch64/MCTargetDesc/AArch64MachObjectWriter.cpp
@@ -373,7 +373,11 @@ void AArch64MachObjectWriter::recordRelocation(
        Type == MachO::ARM64_RELOC_PAGE21 ||
        Type == MachO::ARM64_RELOC_PAGEOFF12) &&
       Value) {
-    assert((Value & 0xff000000) == 0 && "Added relocation out of range!");
+    if (!isInt<24>(Value)) {
+      Asm.getContext().reportError(Fixup.getLoc(),
+                                   "addend too big for relocation");
+      return;
+    }
 
     MachO::any_relocation_info MRE;
     MRE.r_word0 = FixupOffset;
