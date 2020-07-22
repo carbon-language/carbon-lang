87d725473380652bbe845fd2fbd9c0507a55172f
Zequan Wu <zequanwu@google.com>
2020-07-21 18:01:19 -0700

    [Coverage] Fix coverage test cases.

--- compiler-rt/test/profile/coverage_comments.cpp
+++ compiler-rt/test/profile/coverage_comments.cpp
@@ -3,69 +3,69 @@
 // RUN: llvm-profdata merge -o %t.profdata %t.profraw
 // RUN: llvm-cov show %t -instr-profile %t.profdata -path-equivalence=/tmp,%S 2>&1 | FileCheck %s
 
-int main() {                           // CHECK:  [[# @LINE]]| 1|int main() {
-    /* comment */ int x = 0;           // CHECK:  [[# @LINE]]| 1|  /* comment */ int x = 0;
-    int y = 0; /* comment */           // CHECK:  [[# @LINE]]| 1|  int y = 0; /* comment */
-    int z = 0; // comment              // CHECK:  [[# @LINE]]| 1|  int z = 0; // comment
-    // comment                         // CHECK:  [[# @LINE]]|  |  // comment
-                                       // CHECK:  [[# @LINE]]|  |
-    x = 0; /*                          // CHECK:  [[# @LINE]]| 1|  x = 0; /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */                                 // CHECK:  [[# @LINE]]|  |    */
-                                       // CHECK:  [[# @LINE]]|  |
-    /*                                 // CHECK:  [[# @LINE]]|  |    /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */ x = 0;                          // CHECK:  [[# @LINE]]| 1|    */ x = 0;
-                                       // CHECK:  [[# @LINE]]|  |
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    z =                                // CHECK:  [[# @LINE]]| 1|    z =
-    x // comment                       // CHECK:  [[# @LINE]]| 1|    x // comment
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-    + /*                               // CHECK:  [[# @LINE]]| 1|    + /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */                                 // CHECK:  [[# @LINE]]|  |    */
-    /*                                 // CHECK:  [[# @LINE]]|  |    /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */y;                               // CHECK:  [[# @LINE]]| 1|    */y;
-                                       // CHECK:  [[# @LINE]]|  |
-    // Comments inside directives.     // CHECK:  [[# @LINE]]|  |    // Comments inside directives.
-    #if 0 //comment                    // CHECK:  [[# @LINE]]|  |    #if 0 //comment
-    /* comment */ x = 0;               // CHECK:  [[# @LINE]]|  |    /* comment */ x = 0;
-    y = 0; /* comment */               // CHECK:  [[# @LINE]]|  |    y = 0; /* comment */
-    z = 0; // comment                  // CHECK:  [[# @LINE]]|  |    z = 0; // comment
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-                                       // CHECK:  [[# @LINE]]|  |
-    x = 0; /*                          // CHECK:  [[# @LINE]]|  |    x = 0; /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */                                 // CHECK:  [[# @LINE]]|  |    */
-                                       // CHECK:  [[# @LINE]]|  | 
-    /*                                 // CHECK:  [[# @LINE]]|  |    /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */ x = 0;                          // CHECK:  [[# @LINE]]|  |    */ x = 0;
-                                       // CHECK:  [[# @LINE]]|  |
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    #endif // comment                  // CHECK:  [[# @LINE]]|  |    #endif // comment
-    #if 1 // comment                   // CHECK:  [[# @LINE]]| 1|    #if 1 // comment
-    /* comment */ x = 0;               // CHECK:  [[# @LINE]]| 1|    /* comment */ x = 0;
-    y = 0; /* comment */               // CHECK:  [[# @LINE]]| 1|    y = 0; /* comment */
-    z = 0; // comment                  // CHECK:  [[# @LINE]]| 1|    z = 0; // comment
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-                                       // CHECK:  [[# @LINE]]|  |
-    x = 0; /*                          // CHECK:  [[# @LINE]]| 1|    x = 0; /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */                                 // CHECK:  [[# @LINE]]|  |    */
-                                       // CHECK:  [[# @LINE]]|  |
-    /*                                 // CHECK:  [[# @LINE]]|  |    /*
-    comment                            // CHECK:  [[# @LINE]]|  |    comment
-    */ x = 0;                          // CHECK:  [[# @LINE]]| 1|    */ x = 0;
-                                       // CHECK:  [[# @LINE]]|  |
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    // comment                         // CHECK:  [[# @LINE]]|  |    // comment
-    /* comment */                      // CHECK:  [[# @LINE]]|  |    /* comment */
-    #endif //comment                   // CHECK:  [[# @LINE]]| 1|    #endif //comment
-    return 0;                          // CHECK:  [[# @LINE]]| 1|    return 0;
-}                                      // CHECK:  [[# @LINE]]| 1|}
+int main() {                           // CHECK:       [[# @LINE]]| 1|int main() {
+    /* comment */ int x = 0;           // CHECK-NEXT:  [[# @LINE]]| 1|
+    int y = 0; /* comment */           // CHECK-NEXT:  [[# @LINE]]| 1|
+    int z = 0; // comment              // CHECK-NEXT:  [[# @LINE]]| 1|
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    x = 0; /*                          // CHECK-NEXT:  [[# @LINE]]| 1|
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */                                 // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    /*                                 // CHECK-NEXT:  [[# @LINE]]|  |
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */ x = 0;                          // CHECK-NEXT:  [[# @LINE]]| 1|
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    z =                                // CHECK-NEXT:  [[# @LINE]]| 1|
+    x // comment                       // CHECK-NEXT:  [[# @LINE]]| 1|
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+    + /*                               // CHECK-NEXT:  [[# @LINE]]| 1|
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */                                 // CHECK-NEXT:  [[# @LINE]]|  |
+    /*                                 // CHECK-NEXT:  [[# @LINE]]|  |
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */y;                               // CHECK-NEXT:  [[# @LINE]]| 1|
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    // Comments inside directives.     // CHECK-NEXT:  [[# @LINE]]|  |
+    #if 0 //comment                    // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */ x = 0;               // CHECK-NEXT:  [[# @LINE]]|  |
+    y = 0; /* comment */               // CHECK-NEXT:  [[# @LINE]]|  |
+    z = 0; // comment                  // CHECK-NEXT:  [[# @LINE]]|  |
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    x = 0; /*                          // CHECK-NEXT:  [[# @LINE]]|  |
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */                                 // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  | 
+    /*                                 // CHECK-NEXT:  [[# @LINE]]|  |
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */ x = 0;                          // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    #endif // comment                  // CHECK-NEXT:  [[# @LINE]]|  |
+    #if 1 // comment                   // CHECK-NEXT:  [[# @LINE]]| 1|
+    /* comment */ x = 0;               // CHECK-NEXT:  [[# @LINE]]| 1|
+    y = 0; /* comment */               // CHECK-NEXT:  [[# @LINE]]| 1|
+    z = 0; // comment                  // CHECK-NEXT:  [[# @LINE]]| 1|
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    x = 0; /*                          // CHECK-NEXT:  [[# @LINE]]| 1|
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */                                 // CHECK-NEXT:  [[# @LINE]]|  |
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    /*                                 // CHECK-NEXT:  [[# @LINE]]|  |
+    comment                            // CHECK-NEXT:  [[# @LINE]]|  |
+    */ x = 0;                          // CHECK-NEXT:  [[# @LINE]]| 1|
+                                       // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    // comment                         // CHECK-NEXT:  [[# @LINE]]|  |
+    /* comment */                      // CHECK-NEXT:  [[# @LINE]]|  |
+    #endif //comment                   // CHECK-NEXT:  [[# @LINE]]| 1|
+    return 0;                          // CHECK-NEXT:  [[# @LINE]]| 1|
+}                                      // CHECK-NEXT:  [[# @LINE]]| 1|
