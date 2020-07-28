a5b89c285329dbf160d501bf81fcc3765fde219d
Jez Ng <jezng@fb.com>
2020-07-28 11:04:43 -0700

    [lld-macho] Fix no-filelist test on Windows

--- lld/test/MachO/invalid/no-filelist.s
+++ lld/test/MachO/invalid/no-filelist.s
@@ -1,7 +1,7 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-darwin %s -o %t.o
 # RUN: not lld -flavor darwinnew -Z -filelist nonexistent %t.o -o %t 2>&1 | FileCheck %s
-# CHECK: cannot open nonexistent: No such file or directory
+# CHECK: cannot open nonexistent: {{N|n}}o such file or directory
 
 .globl _main
 
