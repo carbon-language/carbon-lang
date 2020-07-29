cd7a5c3cd9a4891654b1b36f743a250135eb5c5a
Siva Chandra Reddy <sivachandra@google.com>
2020-07-28 17:03:16 -0700

    [libc][obvious] Add license headers to APIIndexer.[cpp|h].

--- libc/utils/LibcTableGenUtil/APIIndexer.h
+++ libc/utils/LibcTableGenUtil/APIIndexer.h
@@ -1,3 +1,13 @@
+//===-- A class to index libc API listed in tablegen files ------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_UTILS_LIBC_TABLE_GEN_UTILS_API_INDEXER_H
+#define LLVM_LIBC_UTILS_LIBC_TABLE_GEN_UTILS_API_INDEXER_H
 
 #include "llvm/ADT/StringRef.h"
 #include "llvm/TableGen/Record.h"
@@ -64,3 +74,5 @@ public:
 };
 
 } // namespace llvm_libc
+
+#endif // LLVM_LIBC_UTILS_LIBC_TABLE_GEN_UTILS_API_INDEXER_H
