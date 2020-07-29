71bf6dd682c03c8c29a365f602f5168d44757abe
Victor Campos <victor.campos@arm.com>
2020-07-29 14:32:51 +0100

    [Driver][ARM] Fix testcase that should only run on ARM
    Fix testcase introduced in d1a3396bfbc6fd6df927f2864c18d86e742cabff.

--- clang/test/CodeGen/arm-bf16-softfloat.c
+++ clang/test/CodeGen/arm-bf16-softfloat.c
@@ -1,3 +1,4 @@
+// REQUIRES: arm-registered-target
 // RUN: not %clang -target arm-arm-none-eabi -march=armv8-a+bf16 -mfloat-abi=soft -c %s -o %t 2>&1 | FileCheck %s
 // RUN: not %clang -target arm-arm-none-eabi -march=armv8-a+bf16 -mfpu=none -c %s -o %t 2>&1 | FileCheck %s
 // RUN: not %clang -target arm-arm-none-eabi -march=armv8-a+bf16+nofp -c %s -o %t 2>&1 | FileCheck %s
