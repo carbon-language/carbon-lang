926c14798d317a8c60592f420bb79cd80c150285
Francis Visoiu Mistrih <francisvm@yahoo.com>
2020-07-29 15:41:04 -0700

    Revert "build: always add support for assembly targets on Darwin"
    This reverts commit d8e8e32d85f1cbde2a6b67af010fba3c3a3c86e9.
    
    This breaks the build on GreenDragon:
    http://green.lab.llvm.org/green/job/clang-stage1-RA/13171/console

--- compiler-rt/cmake/Modules/AddCompilerRT.cmake
+++ compiler-rt/cmake/Modules/AddCompilerRT.cmake
@@ -110,7 +110,7 @@ endfunction()
 function(add_asm_sources output)
   set(${output} ${ARGN} PARENT_SCOPE)
   # Xcode will try to compile asm files as C ('clang -x c'), and that will fail.
-  if(CMAKE_SYSTEM_NAME STREQUAL Darwin)
+  if (${CMAKE_GENERATOR} STREQUAL "Xcode")
     enable_language(ASM)
   else()
     # Pass ASM file directly to the C++ compiler.
