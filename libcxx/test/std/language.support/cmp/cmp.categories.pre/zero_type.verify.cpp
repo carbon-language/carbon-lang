bf434a5f173eed4112a10e28e8a6236d48f9da07
Richard Smith <richard@metafoo.co.uk>
2020-07-31 15:03:21 -0700

    Improve the representation of <compare>'s zero-only type.
    * Use an empty struct instead of a member pointer to represent this
      type, so that we don't actually pass a zero member pointer at runtime.
    
    * Mark the constructor as consteval to ensure that no code is emitted
      for it whenever possible.
    
    * Add a honeypot constructor to reject all non-int arguments, so that
      the only argument that can arrive at the real constructor is the
      literal 0.
    
    This results in better generated code, and rejecting invalid comparisons
    against nullptr, 0L, and so on, while also rejecting invalid comparisons
    against (1-1) and similar that would be allowed if we required an
    integer constant expression with value 0.
    
    Differential Revision: https://reviews.llvm.org/D85051

---
+++ libcxx/test/std/language.support/cmp/cmp.categories.pre/zero_type.verify.cpp
@@ -0,0 +1,60 @@
+// -*- C++ -*-
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+
+// <compare>
+
+// Ensure we reject all cases where an argument other than a literal 0 is used
+// for a comparison against a comparison category type.
+
+#include <compare>
+
+#define TEST_OP(v, op)                                                         \
+  void(v op 0L);                                                               \
+  void(0L op v);                                                               \
+  void(v op nullptr);                                                          \
+  void(nullptr op v);                                                          \
+  void(v op(1 - 1));                                                           \
+  void((1 - 1) op v);
+
+template <typename T>
+void test_category(T v) {
+  TEST_OP(v, ==);  // expected-error 18 {{}}
+  TEST_OP(v, !=);  // expected-error 18 {{}}
+  TEST_OP(v, <);   // expected-error 18 {{}}
+  TEST_OP(v, <=);  // expected-error 18 {{}}
+  TEST_OP(v, >);   // expected-error 18 {{}}
+  TEST_OP(v, >=);  // expected-error 18 {{}}
+  TEST_OP(v, <=>); // expected-error 18 {{}}
+
+  void(v == 0);
+  void(0 == v);
+  void(v != 0);
+  void(0 != v);
+  void(v < 0);
+  void(0 < v);
+  void(v <= 0);
+  void(0 <= v);
+  void(v > 0);
+  void(0 > v);
+  void(v >= 0);
+  void(0 >= v);
+#ifndef _LIBCPP_HAS_NO_THREE_WAY_COMPARISON
+  void(v <=> 0); // expected-error 3 {{}}
+  void(0 <=> v); // expected-error 3 {{}}
+#endif
+}
+
+int main(int, char**) {
+  test_category(std::strong_ordering::equivalent);
+  test_category(std::weak_ordering::equivalent);
+  test_category(std::partial_ordering::equivalent);
+  return 0;
+}
