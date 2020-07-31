38d3e7533279fd4bfefcd88eac7d3b64f804c53a
Jaydeep Chauhan <jaydeepchauhan1494@gmail.com>
2020-07-31 20:22:22 +0100

    [clang] Use the location of the void parameters when complaining that only a single void parameter should be present.
    Fixes PR46417.
    
    Differential Revision: https://reviews.llvm.org/D84678
    
    Reviewed By: aaron.ballman

--- clang/lib/Sema/SemaType.cpp
+++ clang/lib/Sema/SemaType.cpp
@@ -5114,7 +5114,7 @@ static TypeSourceInfo *GetFullTypeForDeclarator(TypeProcessingState &state,
             // is an incomplete type (C99 6.2.5p19) and function decls cannot
             // have parameters of incomplete type.
             if (FTI.NumParams != 1 || FTI.isVariadic) {
-              S.Diag(DeclType.Loc, diag::err_void_only_param);
+              S.Diag(FTI.Params[i].IdentLoc, diag::err_void_only_param);
               ParamTy = Context.IntTy;
               Param->setType(ParamTy);
             } else if (FTI.Params[i].Ident) {
