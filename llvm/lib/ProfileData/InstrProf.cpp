f78f509c75861dc4e26f9a22ad12996bf8005a2e
Hiroshi Yamauchi <yamauchi@google.com>
2020-08-03 10:35:47 -0700

    [PGO] Extend the value profile buckets for mem op sizes.
    Extend the memop value profile buckets to be more flexible (could accommodate a
    mix of individual values and ranges) and to cover more value ranges (from 11 to
    22 buckets).
    
    Disabled behind a flag (to be enabled separately) and the existing code to be
    removed later.
    
    Differential Revision: https://reviews.llvm.org/D81682

--- llvm/lib/ProfileData/InstrProf.cpp
+++ llvm/lib/ProfileData/InstrProf.cpp
@@ -1112,6 +1112,8 @@ bool canRenameComdatFunc(const Function &F, bool CheckAddressTaken) {
   return true;
 }
 
+// FIXME: This is to be removed after switching to the new memop value
+// profiling.
 // Parse the value profile options.
 void getMemOPSizeRangeFromOption(StringRef MemOPSizeRange, int64_t &RangeStart,
                                  int64_t &RangeLast) {
