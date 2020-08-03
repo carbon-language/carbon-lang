c7be982c836cdaf7b1ef303d903e6a3de2eb4a34
Luboš Luňák <l.lunak@centrum.cz>
2020-08-03 07:09:03 +0200

    [lldb][gui] move TestGuiBasicDebug.py to lldb/test and update it
    Between the time it was created and it was pushed upstream,
    99451b4453688a94c6014cac233d371ab4cc342d has moved the existing
    gui gui tests to lldb/test, so move this one too.
    And update it to contain TestGuiBasic.py changes since the time
    when it was based on that test.
    
    Differential Revision: https://reviews.llvm.org/D85106

--- lldb/test/API/commands/gui/basicdebug/TestGuiBasicDebug.py
+++ lldb/test/API/commands/gui/basicdebug/TestGuiBasicDebug.py
@@ -11,7 +11,11 @@ class TestGuiBasicDebugCommandTest(PExpectTest):
 
     mydir = TestBase.compute_mydir(__file__)
 
+    # PExpect uses many timeouts internally and doesn't play well
+    # under ASAN on a loaded machine..
+    @skipIfAsan
     @skipIfCursesSupportMissing
+    @skipIfRemote # "run" command will not work correctly for remote debug
     def test_gui(self):
         self.build()
 
@@ -39,4 +43,8 @@ class TestGuiBasicDebugCommandTest(PExpectTest):
         self.child.send("n") # step over
         self.child.expect("return 0;[^\r\n]+<<< Thread 1: step over")
 
+        # Press escape to quit the gui
+        self.child.send(escape_key)
+
+        self.expect_prompt()
         self.quit()
