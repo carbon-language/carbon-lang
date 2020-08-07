be8b3f0c22048519f8a226e46f6a616806a8e558
Oliver Stannard <oliver.stannard@linaro.org>
2020-08-07 10:41:16 +0100

    [AArch64] Disable waitid.cpp test for AArch64
    This test is failing intermittently on the AArch64 build bots, disable
    it for now to keep the bots green while we investigate it.

--- compiler-rt/test/asan/TestCases/Posix/waitid.cpp
+++ compiler-rt/test/asan/TestCases/Posix/waitid.cpp
@@ -1,7 +1,7 @@
 // RUN: %clangxx_asan -O0 %s -o %t && not %run %t 2>&1 | FileCheck %s
 // RUN: %clangxx_asan -O3 %s -o %t && not %run %t 2>&1 | FileCheck %s
 
-// UNSUPPORTED: darwin
+// UNSUPPORTED: darwin, aarch64
 
 #include <assert.h>
 #include <sys/wait.h>
