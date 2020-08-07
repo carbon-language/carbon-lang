e3eb3cf5501c6b97b5baa2d66193f2d610492be6
Jonas Devlieghere <jonas@devlieghere.com>
2020-08-07 13:04:16 -0700

    [lldb] Only check for --apple-sdk argument on Darwin

--- lldb/packages/Python/lldbsuite/test/dotest.py
+++ lldb/packages/Python/lldbsuite/test/dotest.py
@@ -426,7 +426,7 @@ def parseOptionsAndInitTestdirs():
         configuration.lldb_platform_url = args.lldb_platform_url
     if args.lldb_platform_working_dir:
         configuration.lldb_platform_working_dir = args.lldb_platform_working_dir
-    if args.apple_sdk:
+    if platform_system == 'Darwin'  and args.apple_sdk:
         configuration.apple_sdk = args.apple_sdk
     if args.test_build_dir:
         configuration.test_build_dir = args.test_build_dir
