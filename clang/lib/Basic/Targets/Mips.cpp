24cca30f7f0de49e77af7e8c7719680daec2418e
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-08-07 11:20:44 +0100

    Remove unreachable return (PR47026)

--- clang/lib/Basic/Targets/Mips.cpp
+++ clang/lib/Basic/Targets/Mips.cpp
@@ -41,7 +41,6 @@ bool MipsTargetInfo::processorSupportsGPR64() const {
       .Case("octeon", true)
       .Case("octeon+", true)
       .Default(false);
-  return false;
 }
 
 static constexpr llvm::StringLiteral ValidCPUNames[] = {
