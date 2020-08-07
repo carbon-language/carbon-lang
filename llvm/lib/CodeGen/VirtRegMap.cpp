08803f0e62e82539756b94e2655dabdbdab39014
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2020-08-07 12:56:09 -0700

    Unbundle KILL bundles in VirtRegRewriter
    SplitKit forms invalid COPY subreg bundles without a leading
    BUNDLE instruction. That manifests itself in post-RA scheduler
    counting instruction and asserting on "Instruction count mismatch".
    
    The bundle shall be undone by VirtRegRewriter::expandCopyBundle(),
    but it does not because VirtRegRewriter::handleIdentityCopy() can
    turn COPY bundle into a KILL bundle.
    
    Process KILLs as well.
    
    Differential Revision: https://reviews.llvm.org/D85484

--- llvm/lib/CodeGen/VirtRegMap.cpp
+++ llvm/lib/CodeGen/VirtRegMap.cpp
@@ -400,18 +400,18 @@ void VirtRegRewriter::handleIdentityCopy(MachineInstr &MI) const {
 /// after processing the last in the bundle. Does not update LiveIntervals
 /// which we shouldn't need for this instruction anymore.
 void VirtRegRewriter::expandCopyBundle(MachineInstr &MI) const {
-  if (!MI.isCopy())
+  if (!MI.isCopy() && !MI.isKill())
     return;
 
   if (MI.isBundledWithPred() && !MI.isBundledWithSucc()) {
     SmallVector<MachineInstr *, 2> MIs({&MI});
 
-    // Only do this when the complete bundle is made out of COPYs.
+    // Only do this when the complete bundle is made out of COPYs and KILLs.
     MachineBasicBlock &MBB = *MI.getParent();
     for (MachineBasicBlock::reverse_instr_iterator I =
          std::next(MI.getReverseIterator()), E = MBB.instr_rend();
          I != E && I->isBundledWithSucc(); ++I) {
-      if (!I->isCopy())
+      if (!I->isCopy() && !I->isKill())
         return;
       MIs.push_back(&*I);
     }
