05d74dbc3bb1d943a029e4abea211288c920f559
Nick Desaulniers <ndesaulniers@google.com>
2020-08-10 15:25:20 -0700

    python bindings: fix DeprecationWarning
    Fixes observed warning running `ninja check-all`:
    llvm-project/clang/bindings/python/tests/cindex/test_diagnostics.py:100:
    DeprecationWarning: Please use assertRegex instead.
      self.assertRegexpMatches(children[0].spelling
    
    Looks like unittest.assertRegexpMatches has been deprecated in favor of
    unittest.assertRegex since Python 3.2, according to:
    https://docs.python.org/3/library/unittest.html#deprecated-aliases
    
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D85692

--- clang/bindings/python/tests/cindex/test_diagnostics.py
+++ clang/bindings/python/tests/cindex/test_diagnostics.py
@@ -41,7 +41,7 @@ class TestDiagnostics(unittest.TestCase):
         self.assertEqual(tu.diagnostics[0].severity, Diagnostic.Warning)
         self.assertEqual(tu.diagnostics[0].location.line, 1)
         self.assertEqual(tu.diagnostics[0].location.column, 26)
-        self.assertRegexpMatches(tu.diagnostics[0].spelling,
+        self.assertRegex(tu.diagnostics[0].spelling,
             'use of GNU old-style.*')
         self.assertEqual(len(tu.diagnostics[0].fixits), 1)
         self.assertEqual(tu.diagnostics[0].fixits[0].range.start.line, 1)
@@ -56,7 +56,7 @@ class TestDiagnostics(unittest.TestCase):
         self.assertEqual(tu.diagnostics[0].severity, Diagnostic.Warning)
         self.assertEqual(tu.diagnostics[0].location.line, 1)
         self.assertEqual(tu.diagnostics[0].location.column, 16)
-        self.assertRegexpMatches(tu.diagnostics[0].spelling,
+        self.assertRegex(tu.diagnostics[0].spelling,
             'incompatible pointer to.*')
         self.assertEqual(len(tu.diagnostics[0].fixits), 0)
         self.assertEqual(len(tu.diagnostics[0].ranges), 1)
@@ -97,7 +97,7 @@ class TestDiagnostics(unittest.TestCase):
         children = d.children
         self.assertEqual(len(children), 1)
         self.assertEqual(children[0].severity, Diagnostic.Note)
-        self.assertRegexpMatches(children[0].spelling,
+        self.assertRegex(children[0].spelling,
                 '.*declared here')
         self.assertEqual(children[0].location.line, 1)
         self.assertEqual(children[0].location.column, 6)
