740332b6cce3e59dca4e50d3e2fd0d008f5e9529
Georgii Rymar <grimar@accesssoftek.com>
2020-08-12 16:54:49 +0300

    [llvm-readobj/elf] - Refine testing of broken Android's packed relocation sections.
    This uses modern `split-file` tool to merge 5 `packed-relocs-error*.s` tests to a
    new `packed-relocs-errors.s` and adds testing for GNU style.
    
    Differential revision: https://reviews.llvm.org/D85835

--- llvm/test/tools/llvm-readobj/ELF/packed-relocs-error1.s
+++
@@ -1,8 +0,0 @@
-// REQUIRES: x86-registered-target
-// RUN: llvm-mc -filetype=obj -triple x86_64-pc-linux-gnu %s -o %t
-// RUN: llvm-readobj --relocations %t 2>&1 | FileCheck %s -DFILE=%t
-
-// CHECK: warning: '[[FILE]]': unable to read relocations from SHT_ANDROID_REL section with index 3: invalid packed relocation header
-
-.section .rela.dyn, "a", @0x60000001
-.ascii "APS9"
