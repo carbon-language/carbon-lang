11e2c253ac7993abbe347c048247453e0b423a32
Jez Ng <jezng@fb.com>
2020-08-12 20:57:42 -0700

    [lld-macho] Try to fix invalid-stub test on Windows
    Summary: Path separator issues...

--- lld/test/MachO/invalid/invalid-stub.s
+++ lld/test/MachO/invalid/invalid-stub.s
@@ -3,9 +3,9 @@
 # RUN: echo "--- !tapi-tbd-v3" > %t/libinvalidYAML.tbd
 # RUN: echo "invalid YAML" >> %t/libinvalidYAML.tbd
 # RUN: llvm-mc -filetype obj -triple x86_64-apple-darwin %s -o %t/test.o
-# RUN: not lld -flavor darwinnew -Z -L%t -linvalidYAML %t/test.o -o %t/test -Z 2>&1 | FileCheck %s -DDIR=%t
+# RUN: not lld -flavor darwinnew -Z -L%t -linvalidYAML %t/test.o -o %t/test -Z 2>&1 | FileCheck %s -DFILE=%t/libinvalidYAML.tbd
 
-# CHECK: could not load TAPI file at [[DIR]]/libinvalidYAML.tbd: malformed file
+# CHECK: could not load TAPI file at [[FILE]]: malformed file
 
 .globl _main
 _main:
