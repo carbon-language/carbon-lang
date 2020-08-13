c7191e318544ea0c30b5678c1f2e79e0c5be9628
Matt Arsenault <Matthew.Arsenault@amd.com>
2020-08-12 20:02:05 -0400

    DAG: Don't pass 0 alignment value to allowsMisalignedMemoryAccesses
    I think not unconditionally passing getDstAlign is broken, but leave
    that for another change.

--- llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
+++ llvm/lib/CodeGen/SelectionDAG/TargetLowering.cpp
@@ -250,7 +250,7 @@ bool TargetLowering::findOptimalMemOpLowering(
       bool Fast;
       if (NumMemOps && Op.allowOverlap() && NewVTSize < Size &&
           allowsMisalignedMemoryAccesses(
-              VT, DstAS, Op.isFixedDstAlign() ? Op.getDstAlign().value() : 0,
+              VT, DstAS, Op.isFixedDstAlign() ? Op.getDstAlign().value() : 1,
               MachineMemOperand::MONone, &Fast) &&
           Fast)
         VTSize = Size;
