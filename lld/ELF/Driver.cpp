b358daddea04ea3c52e0e5bd5e851cee47f7f27f
Fangrui Song <i@maskray.me>
2020-08-14 15:38:05 -0700

    [ELF] Re-initialize InputFile::isInGroup so that elf::link can be called more than once

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -1306,6 +1306,7 @@ void LinkerDriver::createFiles(opt::InputArgList &args) {
   std::vector<std::tuple<bool, bool, bool>> stack;
 
   // Iterate over argv to process input files and positional arguments.
+  InputFile::isInGroup = false;
   for (auto *arg : args) {
     switch (arg->getOption().getID()) {
     case OPT_library:
