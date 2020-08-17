32ebdc70f3af478f4f6a9c75b4bc47a453b1b933
George Rokos <georgios.rokos@intel.com>
2020-08-17 08:32:23 -0700

    [libomptarget][NFC] Sort list of plugins in chronological order
    Differential Revision: https://reviews.llvm.org/D86082

--- openmp/libomptarget/src/rtl.cpp
+++ openmp/libomptarget/src/rtl.cpp
@@ -23,12 +23,12 @@
 
 // List of all plugins that can support offloading.
 static const char *RTLNames[] = {
+    /* PowerPC target       */ "libomptarget.rtl.ppc64.so",
+    /* x86_64 target        */ "libomptarget.rtl.x86_64.so",
+    /* CUDA target          */ "libomptarget.rtl.cuda.so",
+    /* AArch64 target       */ "libomptarget.rtl.aarch64.so",
     /* SX-Aurora VE target  */ "libomptarget.rtl.ve.so",
-    /* PowerPC target */ "libomptarget.rtl.ppc64.so",
-    /* x86_64 target  */ "libomptarget.rtl.x86_64.so",
-    /* CUDA target    */ "libomptarget.rtl.cuda.so",
-    /* AArch64 target */ "libomptarget.rtl.aarch64.so",
-    /* AMDGPU target  */ "libomptarget.rtl.amdgpu.so",
+    /* AMDGPU target        */ "libomptarget.rtl.amdgpu.so",
 };
 
 RTLsTy *RTLs;
