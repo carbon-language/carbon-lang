9937872c02ba1b088a27a880301b6d816d971d16
Sourabh Singh Tomar <SourabhSingh.Tomar@amd.com>
2020-08-20 01:28:03 +0530

    Revert "[DebugInfo] Emit DW_OP_implicit_value for Floating point constants"
    This reverts commit 15801f16194a3d.
    arc's land messed up! It removed the new commit message and took it
    from revision.

--- llvm/lib/CodeGen/AsmPrinter/DwarfExpression.h
+++ llvm/lib/CodeGen/AsmPrinter/DwarfExpression.h
@@ -299,9 +299,6 @@ public:
   /// Emit an unsigned constant.
   void addUnsignedConstant(const APInt &Value);
 
-  /// Emit an floating point constant.
-  void addConstantFP(const APFloat &Value, const AsmPrinter &AP);
-
   /// Lock this down to become a memory location description.
   void setMemoryLocationKind() {
     assert(isUnknownLocation());
