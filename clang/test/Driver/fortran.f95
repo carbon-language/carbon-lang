33c554d8444a5ce1fb85def04de8c0ebfec643b9
Caroline Concatto <caroline.concatto@arm.com>
2020-08-19 17:53:29 +0100

    [clang][driver]Add quotation mark in test/fortran.f95  to avoid false positive
    If a folder's name, where the test fortran.f95 is running, has cc1 the test
    fails because of  CHECK-ASM-NOT: cc1.
    The solution used in this patch is to add quotation mark around cc1 and cc1as
    because the driver returns these flags with quotation marks ("")
    
    Reviewed By: DavidTruby, echristo
    
    Differential Revision: https://reviews.llvm.org/D86132

--- clang/test/Driver/fortran.f95
+++ clang/test/Driver/fortran.f95
@@ -6,14 +6,14 @@
 ! CHECK-OBJECT: gcc
 ! CHECK-OBJECT: "-c"
 ! CHECK-OBJECT: "-x" "f95"
-! CHECK-OBJECT-NOT: cc1as
+! CHECK-OBJECT-NOT: "-cc1as"
 
 ! RUN: %clang -target x86_64-unknown-linux-gnu -integrated-as -S %s -### 2>&1 \
 ! RUN:   | FileCheck --check-prefix=CHECK-ASM %s
 ! CHECK-ASM: gcc
 ! CHECK-ASM: "-S"
 ! CHECK-ASM: "-x" "f95"
-! CHECK-ASM-NOT: cc1
+! CHECK-ASM-NOT: "-cc1"
 
 ! RUN: %clang -Wall -target x86_64-unknown-linux-gnu -integrated-as %s -o %t -### 2>&1 | FileCheck --check-prefix=CHECK-WARN %s
 ! CHECK-WARN: gcc
