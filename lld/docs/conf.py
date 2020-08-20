7f009389752c758ccd9f7cc46867e991d482933e
Hans Wennborg <hans@chromium.org>
2020-08-20 16:44:14 +0200

    lld docs config: Use a list key in html_sidebars
    Otherwise the docs-lld-html target fails to build using recent Sphinx
    with the following not very helpful error message:
    
    An error happened in rendering the page index.
    Reason: TemplateNotFound()
    
    It turns out the values in the html_sidebars dictionary always need to be lists
    now. See https://github.com/sphinx-doc/sphinx/issues/6186

--- lld/docs/conf.py
+++ lld/docs/conf.py
@@ -134,7 +134,7 @@ html_last_updated_fmt = '%Y-%m-%d'
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-html_sidebars = {'index': 'indexsidebar.html'}
+html_sidebars = {'index': ['indexsidebar.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
