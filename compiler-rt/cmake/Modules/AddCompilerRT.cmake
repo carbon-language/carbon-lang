adf0b8cc70325f027d202139e3ff984c41896b57
Raphael Isemann <teemperor@gmail.com>
2020-08-20 15:23:50 +0200

    Revert "[compiler-rt] Compile assembly files as ASM not C"
    This reverts commit d58fd4e52197d55bf42ca446c8b0ed31b5c2ec1f. This broke
    compiler-rt compilation on macOS:
    
    codesign --sign - /Users/buildslave/jenkins/workspace/lldb-cmake/lldb-build/lib/clang/12.0.99/lib/darwin/libclang_rt.tsan_ios_dynamic.dylib
    ld: warning: ignoring file projects/compiler-rt/lib/tsan/CMakeFiles/clang_rt.tsan_ios_dynamic.dir/rtl/tsan_rtl_amd64.S.o, building for iOS-arm64 but attempting to link with file built for iOS Simulator-x86_64
    ld: warning: ignoring file projects/compiler-rt/lib/tsan/CMakeFiles/clang_rt.tsan_ios_dynamic.dir/rtl/tsan_rtl_aarch64.S.o, building for iOS-arm64 but attempting to link with file built for iOS Simulator-x86_64
    Undefined symbols for architecture arm64:
      "_wrap__setjmp", referenced from:
          substitution__setjmp in tsan_interceptors_posix.cpp.o
      "_wrap_setjmp", referenced from:
          substitution_setjmp in tsan_interceptors_posix.cpp.o
      "_wrap_sigsetjmp", referenced from:
          substitution_sigsetjmp in tsan_interceptors_posix.cpp.o
    ld: symbol(s) not found for architecture arm64

--- compiler-rt/cmake/Modules/AddCompilerRT.cmake
+++ compiler-rt/cmake/Modules/AddCompilerRT.cmake
@@ -109,11 +109,13 @@ endfunction()
 
 function(add_asm_sources output)
   set(${output} ${ARGN} PARENT_SCOPE)
-  # Make sure ASM language is available.
-  # We explicitly mark the source files as ASM, so they don't get passed to the
-  # C/CXX compiler and hopes that it recognizes them as assembly.
-  enable_language(ASM)
-  set_source_files_properties(${ARGN} PROPERTIES LANGUAGE ASM)
+  # Xcode will try to compile asm files as C ('clang -x c'), and that will fail.
+  if (${CMAKE_GENERATOR} STREQUAL "Xcode")
+    enable_language(ASM)
+  else()
+    # Pass ASM file directly to the C++ compiler.
+    set_source_files_properties(${ARGN} PROPERTIES LANGUAGE C)
+  endif()
 endfunction()
 
 macro(set_output_name output name arch)
