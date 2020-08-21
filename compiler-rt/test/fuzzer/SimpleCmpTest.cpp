9659b81b2ad8f3b4951ef46e2d90120bdf00fe6f
Dokyung Song <dokyungs@google.com>
2020-08-21 22:40:26 +0000

    [libFuzzer] Make msan.test:SimpleCmpTest succeed with less trials.
    Currently SimpleCmpTest passes after 9,831,994 trials on x86_64/Linux
    when the number of given trials is 10,000,000, just a little bigger than
    that. This patch modifies SimpleCmpTest.cpp so that the test passes with less
    trials, reducing its chances of future failures as libFuzzer evolves. More
    specifically, this patch changes a 32-bit equality check to a 8-bit equality
    check, making this test pass at 4,635,303 trials.
    
    Differential Revision: https://reviews.llvm.org/D86382

--- compiler-rt/test/fuzzer/SimpleCmpTest.cpp
+++ compiler-rt/test/fuzzer/SimpleCmpTest.cpp
@@ -18,20 +18,21 @@ bool PrintOnce(int Line) {
 }
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
-  if (Size != 24) return 0;
+  if (Size != 21)
+    return 0;
   uint64_t x = 0;
   int64_t  y = 0;
   int32_t z = 0;
-  uint32_t a = 0;
+  uint8_t a = 0;
   memcpy(&x, Data, 8);  // 8
   memcpy(&y, Data + 8, 8);  // 16
   memcpy(&z, Data + 16, sizeof(z));  // 20
-  memcpy(&a, Data + 20, sizeof(a));  // 24
+  memcpy(&a, Data + 20, sizeof(a));  // 21
   const bool k32bit = sizeof(void*) == 4;
 
   if ((k32bit || x > 1234567890) && PrintOnce(__LINE__) &&
       (k32bit || x < 1234567895) && PrintOnce(__LINE__) &&
-      a == 0x4242 && PrintOnce(__LINE__) &&
+      a == 0x42 && PrintOnce(__LINE__) &&
       (k32bit || y >= 987654321) && PrintOnce(__LINE__) &&
       (k32bit || y <= 987654325) && PrintOnce(__LINE__) &&
       z < -10000 && PrintOnce(__LINE__) &&
