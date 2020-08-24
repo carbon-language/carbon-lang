43465a43755498e11b14ceb46e278bd127b3b3d7
Craig Topper <craig.topper@intel.com>
2020-08-24 10:06:07 -0700

    [LegalizeTypes][X86] Add ROTL/ROTR to WidenVectorResult.
    We can widen these just like any other binary operation.
    
    Added test cases for v2i32 for X86 for coverage.
    
    Fixes failures seen after D77152.

--- llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
+++ llvm/lib/CodeGen/SelectionDAG/LegalizeVectorTypes.cpp
@@ -2812,6 +2812,8 @@ void DAGTypeLegalizer::WidenVectorResult(SDNode *N, unsigned ResNo) {
   case ISD::SSUBSAT:
   case ISD::SSHLSAT:
   case ISD::USHLSAT:
+  case ISD::ROTL:
+  case ISD::ROTR:
     Res = WidenVecRes_Binary(N);
     break;
 
