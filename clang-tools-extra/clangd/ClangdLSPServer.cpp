9d11e6789c477ce6104e29745ca70e13c9fafeb0
Kirill Bobyrev <kbobyrev@google.com>
2020-08-26 17:08:00 +0200

    [clangd] Use string[] for allCommitCharacters
    As per LSP specification, allCommitCharacters should be string[] instead of
    string:
    
    https://microsoft.github.io/language-server-protocol/specification#textDocument_completion
    
    Reviewed By: sammccall
    
    Differential Revision: https://reviews.llvm.org/D86604

--- clang-tools-extra/clangd/ClangdLSPServer.cpp
+++ clang-tools-extra/clangd/ClangdLSPServer.cpp
@@ -592,7 +592,10 @@ void ClangdLSPServer::onInitialize(const InitializeParams &Params,
             {"codeActionProvider", std::move(CodeActionProvider)},
             {"completionProvider",
              llvm::json::Object{
-                 {"allCommitCharacters", " \t()[]{}<>:;,+-/*%^&#?.=\"'|"},
+                 {"allCommitCharacters",
+                  {" ", "\t", "(", ")", "[", "]", "{",  "}", "<",
+                   ">", ":",  ";", ",", "+", "-", "/",  "*", "%",
+                   "^", "&",  "#", "?", ".", "=", "\"", "'", "|"}},
                  {"resolveProvider", false},
                  // We do extra checks, e.g. that > is part of ->.
                  {"triggerCharacters", {".", "<", ">", ":", "\"", "/"}},
