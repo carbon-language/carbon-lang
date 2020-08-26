642cb7865f35ad7dbac78d716fcddaff561e8639
Benjamin Kramer <benny.kra@googlemail.com>
2020-08-26 13:25:41 +0200

    Copy m_plan_is_for_signal_trap member.
    Otherwise it would stay uninitialized. Found by msan.

--- lldb/include/lldb/Symbol/UnwindPlan.h
+++ lldb/include/lldb/Symbol/UnwindPlan.h
@@ -393,6 +393,7 @@ public:
         m_plan_is_sourced_from_compiler(rhs.m_plan_is_sourced_from_compiler),
         m_plan_is_valid_at_all_instruction_locations(
             rhs.m_plan_is_valid_at_all_instruction_locations),
+        m_plan_is_for_signal_trap(rhs.m_plan_is_for_signal_trap),
         m_lsda_address(rhs.m_lsda_address),
         m_personality_func_addr(rhs.m_personality_func_addr) {
     m_row_list.reserve(rhs.m_row_list.size());
