0b342263043477ed2e2dc730468e9bfbcb375ac2
Alina Sbirlea <asbirlea@google.com>
2020-08-26 15:03:22 -0700

    Use properlyDominates in RDFLiveness when sorting on dominance.
    Summary:
    When looking for all reaching definitions, we sort basic blocks on dominance. When sorting looking for properlyDominates() handles the case A == B.
    
    Authored by: pranavb
    
    Differential Revision: https://reviews.llvm.org/D86661

--- llvm/lib/CodeGen/RDFLiveness.cpp
+++ llvm/lib/CodeGen/RDFLiveness.cpp
@@ -230,13 +230,12 @@ NodeList Liveness::getAllReachingDefs(RegisterRef RefRR,
     TmpBB.push_back(Bucket.first);
     if (Bucket.second.size() > 2)
       GetOrder(*Bucket.first);
-    std::sort(Bucket.second.begin(), Bucket.second.end(), Precedes);
+    llvm::sort(Bucket.second.begin(), Bucket.second.end(), Precedes);
   }
 
   // Sort the blocks with respect to dominance.
-  std::sort(TmpBB.begin(), TmpBB.end(), [this](auto A, auto B) {
-    return MDT.dominates(A, B);
-  });
+  llvm::sort(TmpBB.begin(), TmpBB.end(),
+             [this](auto A, auto B) { return MDT.properlyDominates(A, B); });
 
   std::vector<NodeId> TmpInst;
   for (auto I = TmpBB.rbegin(), E = TmpBB.rend(); I != E; ++I) {
