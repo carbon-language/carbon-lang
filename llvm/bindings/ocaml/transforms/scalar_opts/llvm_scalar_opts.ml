dd04fa17d794f73a4cf85827722988ab70239f71
Arthur Eubanks <aeubanks@google.com>
2020-08-27 09:29:17 -0700

    [OCaml] Remove add_constant_propagation
    After https://reviews.llvm.org/D85159.

--- llvm/bindings/ocaml/transforms/scalar_opts/llvm_scalar_opts.ml
+++ llvm/bindings/ocaml/transforms/scalar_opts/llvm_scalar_opts.ml
@@ -96,9 +96,6 @@ external add_lib_call_simplification
 external add_tail_call_elimination
   : [< Llvm.PassManager.any ] Llvm.PassManager.t -> unit
   = "llvm_add_tail_call_elimination"
-external add_constant_propagation
-  : [< Llvm.PassManager.any ] Llvm.PassManager.t -> unit
-  = "llvm_add_constant_propagation"
 external add_memory_to_register_demotion
   : [< Llvm.PassManager.any ] Llvm.PassManager.t -> unit
   = "llvm_add_demote_memory_to_register"
