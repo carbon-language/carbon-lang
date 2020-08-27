fce035eae980fd8f58a4f18ef95e2b2ee1f2bbcd
Anatoly Trosinenko <atrosinenko@accesssoftek.com>
2020-08-27 13:19:08 +0300

    [NFC][compiler-rt] Factor out __mulo[sdt]i4 implementations to .inc file
    The existing implementations are almost identical except for width of the
    integer type.
    
    Factor them out to int_mulo_impl.inc for better maintainability.
    
    This patch is almost identical to D86277.
    
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D86289

--- compiler-rt/lib/builtins/mulosi4.c
+++ compiler-rt/lib/builtins/mulosi4.c
@@ -10,40 +10,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "int_lib.h"
+#define fixint_t si_int
+#include "int_mulo_impl.inc"
 
 // Returns: a * b
 
 // Effects: sets *overflow to 1  if a * b overflows
 
 COMPILER_RT_ABI si_int __mulosi4(si_int a, si_int b, int *overflow) {
-  const int N = (int)(sizeof(si_int) * CHAR_BIT);
-  const si_int MIN = (si_int)1 << (N - 1);
-  const si_int MAX = ~MIN;
-  *overflow = 0;
-  si_int result = a * b;
-  if (a == MIN) {
-    if (b != 0 && b != 1)
-      *overflow = 1;
-    return result;
-  }
-  if (b == MIN) {
-    if (a != 0 && a != 1)
-      *overflow = 1;
-    return result;
-  }
-  si_int sa = a >> (N - 1);
-  si_int abs_a = (a ^ sa) - sa;
-  si_int sb = b >> (N - 1);
-  si_int abs_b = (b ^ sb) - sb;
-  if (abs_a < 2 || abs_b < 2)
-    return result;
-  if (sa == sb) {
-    if (abs_a > MAX / abs_b)
-      *overflow = 1;
-  } else {
-    if (abs_a > MIN / -abs_b)
-      *overflow = 1;
-  }
-  return result;
+  return __muloXi4(a, b, overflow);
 }
