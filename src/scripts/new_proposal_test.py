258f6ba706ab49aaaf088a1ac51d1d7f9da51b3b
jonmeow <46229924+jonmeow@users.noreply.github.com>
2020-09-01 16:17:09 -0700

    Tests

---
+++ src/scripts/new_proposal_test.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""Tests for new_proposal.py."""
+
+__copyright__ = """
+Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+Exceptions. See /LICENSE for license information.
+SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""
+
+import os
+import unittest
+from unittest import mock
+
+import new_proposal
