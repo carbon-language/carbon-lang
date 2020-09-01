c831a14aa16a74fa94c94a351a4bc7812a8bd166
Abhina Sreeskantharajan <Abhina.Sreeskantharajan@ibm.com>
2020-09-01 10:40:01 -0400

    [SystemZ][z/OS] Fix build break in z/OS toolchain
    Differential Revision: https://reviews.llvm.org/D86707

--- clang/lib/Driver/ToolChains/ZOS.cpp
+++ clang/lib/Driver/ToolChains/ZOS.cpp
@@ -12,8 +12,10 @@
 #include "clang/Driver/Options.h"
 #include "llvm/Option/ArgList.h"
 
+using namespace clang::driver;
 using namespace clang::driver::toolchains;
 using namespace llvm::opt;
+using namespace clang;
 
 ZOS::ZOS(const Driver &D, const llvm::Triple &Triple, const ArgList &Args)
     : ToolChain(D, Triple, Args) {}
