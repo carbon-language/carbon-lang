8df143c2cc0a3b49440cafe8a80b0f72cee6f166
Louis Dionne <ldionne@apple.com>
2020-09-01 11:13:44 -0400

    [libc++] Improve REQUIRES for _ExtInt test on recent Clangs
    The previous REQUIRES: would cause the test to run only on Clang-11, not
    even on following versions of Clang, which was mostly not the intent.

--- libcxx/test/libcxx/atomics/ext-int.verify.cpp
+++ libcxx/test/libcxx/atomics/ext-int.verify.cpp
@@ -1,4 +1,19 @@
-// REQUIRES: clang-11
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// <atomic>
+
+// Make sure that `std::atomic` doesn't work with `_ExtInt`. The intent is to
+// disable them for now until their behavior can be designed better later.
+// See https://reviews.llvm.org/D84049 for details.
+
+// UNSUPPORTED: clang-4, clang-5, clang-6, clang-7, clang-8, clang-9, clang-10
+// UNSUPPORTED: apple-clang-9, apple-clang-10, apple-clang-11, apple-clang-12
 
 #include <atomic>
 
