c2590de30df23ef0db39b496cdec62a83a61fbfa
Arthur Eubanks <aeubanks@google.com>
2020-08-31 18:36:11 -0700

    [docs][NewPM] Add docs for writing NPM passes
    As to not conflict with the legacy PM example passes under
    llvm/lib/Transforms/Hello, this is under HelloNew. This makes the
    CMakeLists.txt and general directory structure less confusing for people
    following the example.
    
    Much of the doc structure was taken from WritinAnLLVMPass.rst.
    
    This adds a HelloWorld pass which simply prints out each function name.
    
    More will follow after this, e.g. passes over different units of IR, analyses.
    https://llvm.org/docs/WritingAnLLVMPass.html contains a lot more.
    
    Reviewed By: ychen, asbirlea
    
    Differential Revision: https://reviews.llvm.org/D86979

---
+++ llvm/lib/Transforms/HelloNew/HelloWorld.cpp
@@ -0,0 +1,17 @@
+//===-- HelloWorld.cpp - Example Transformations --------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "llvm/Transforms/HelloNew/HelloWorld.h"
+
+using namespace llvm;
+
+PreservedAnalyses HelloWorldPass::run(Function &F,
+                                      FunctionAnalysisManager &AM) {
+  errs() << F.getName() << "\n";
+  return PreservedAnalyses::all();
+}
