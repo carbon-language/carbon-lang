db574fc7ba2a2faafdbf42f8759e206dac0fc4b2
John Brawn <john.brawn@arm.com>
2020-09-03 18:11:29 +0100

    [libc++] Make ext-int.verify.cpp test compatible with c++03
    Currently the libcxx/atomics/ext-int.verify.cpp test fails when run with
    -std=c++03 because there's an extra error due to using list initialization. Fix
    this by using parentheses instead.

--- libcxx/test/libcxx/atomics/ext-int.verify.cpp
+++ libcxx/test/libcxx/atomics/ext-int.verify.cpp
@@ -20,7 +20,7 @@
 int main(int, char**)
 {
   // expected-error@atomic:*1 {{_Atomic cannot be applied to integer type '_ExtInt(32)'}}
-  std::atomic<_ExtInt(32)> x {42};
+  std::atomic<_ExtInt(32)> x(42);
 
   return 0;
 }
