9933188c90615c9c264ebb69117f09726e909a25
Nico Weber <thakis@chromium.org>
2020-09-08 10:02:00 -0400

    StructPackAlignCheck: Fix a -Winconsistent-missing-override warning

--- clang-tools-extra/clang-tidy/altera/StructPackAlignCheck.h
+++ clang-tools-extra/clang-tidy/altera/StructPackAlignCheck.h
@@ -27,7 +27,7 @@ public:
     MaxConfiguredAlignment(Options.get("MaxConfiguredAlignment", 128)) {}
   void registerMatchers(ast_matchers::MatchFinder *Finder) override;
   void check(const ast_matchers::MatchFinder::MatchResult &Result) override;
-  void storeOptions(ClangTidyOptions::OptionMap &Opts);
+  void storeOptions(ClangTidyOptions::OptionMap &Opts) override;
 
 private:
   const unsigned MaxConfiguredAlignment;
