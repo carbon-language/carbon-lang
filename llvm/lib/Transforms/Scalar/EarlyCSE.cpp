41e68f7ee7b3bb33e9acb0502339a858806e8523
Michael Liao <michael.hliao@gmail.com>
2020-09-10 23:11:22 -0400

    [EarlyCSE] Fix and recommit the revised c9826829d74e637163fdb0351870b8204e62d6e6
    In addition to calculate hash consistently by swapping SELECT's
    operands, we also need to inverse the select pattern favor to match the
    original logic.
    
    [EarlyCSE] Equivalent SELECTs should hash equally
    
    DenseMap<SimpleValue> assumes that, if its isEqual method returns true
    for two elements, then its getHashValue method must return the same value
    for them. This invariant is broken when one SELECT node is a min/max
    operation, and the other can be transformed into an equivalent min/max by
    inverting its predicate and swapping its operands. This patch fixes an
    assertion failure that would occur intermittently while compiling the
    following IR:
    
        define i32 @t(i32 %i) {
          %cmp = icmp sle i32 0, %i
          %twin1 = select i1 %cmp, i32 %i, i32 0
          %cmpinv = icmp sgt i32 0, %i
          %twin2 = select i1 %cmpinv,  i32 0, i32 %i
          %sink = add i32 %twin1, %twin2
          ret i32 %sink
        }
    
    Differential Revision: https://reviews.llvm.org/D86843

--- llvm/lib/Transforms/Scalar/EarlyCSE.cpp
+++ llvm/lib/Transforms/Scalar/EarlyCSE.cpp
@@ -191,11 +191,26 @@ static bool matchSelectWithOptionalNotCond(Value *V, Value *&Cond, Value *&A,
     Pred = ICmpInst::getSwappedPredicate(Pred);
   }
 
+  // Check for inverted variants of min/max by swapping operands.
+  bool Inversed = false;
   switch (Pred) {
-  case CmpInst::ICMP_UGT: Flavor = SPF_UMAX; break;
-  case CmpInst::ICMP_ULT: Flavor = SPF_UMIN; break;
-  case CmpInst::ICMP_SGT: Flavor = SPF_SMAX; break;
-  case CmpInst::ICMP_SLT: Flavor = SPF_SMIN; break;
+  case CmpInst::ICMP_ULE:
+  case CmpInst::ICMP_UGE:
+  case CmpInst::ICMP_SLE:
+  case CmpInst::ICMP_SGE:
+    Pred = CmpInst::getInversePredicate(Pred);
+    std::swap(A, B);
+    Inversed = true;
+    break;
+  default:
+    break;
+  }
+
+  switch (Pred) {
+  case CmpInst::ICMP_UGT: Flavor = Inversed ? SPF_UMIN : SPF_UMAX; break;
+  case CmpInst::ICMP_ULT: Flavor = Inversed ? SPF_UMAX : SPF_UMIN; break;
+  case CmpInst::ICMP_SGT: Flavor = Inversed ? SPF_SMIN : SPF_SMAX; break;
+  case CmpInst::ICMP_SLT: Flavor = Inversed ? SPF_SMAX : SPF_SMIN; break;
   default: break;
   }
 
