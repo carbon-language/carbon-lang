1ec02efee9b1d01cde89f31ca9ba6a46b7662ac5
zoecarver <z.zoelec2@gmail.com>
2020-09-14 13:52:42 -0700

    [libc++] Make rotate a constexpr.
    This patch makes `std::rotate` a constexpr. In doing so, this patch also
    updates the internal `__move` and `__move_backward` funtions to be
    constexpr.
    
    Reviewed By: ldionne
    
    Differential Revision: https://reviews.llvm.org/D65721

--- libcxx/test/std/algorithms/alg.modifying.operations/alg.rotate/rotate.pass.cpp
+++ libcxx/test/std/algorithms/alg.modifying.operations/alg.rotate/rotate.pass.cpp
@@ -20,7 +20,7 @@
 #include "test_iterators.h"
 
 template <class Iter>
-void
+_LIBCPP_CONSTEXPR_AFTER_CXX17 bool
 test()
 {
     int ia[] = {0};
@@ -209,6 +209,8 @@ test()
     assert(ig[3] == 0);
     assert(ig[4] == 1);
     assert(ig[5] == 2);
+
+    return true;
 }
 
 #if TEST_STD_VER >= 11
@@ -434,6 +436,13 @@ int main(int, char**)
     test1<std::unique_ptr<int>*>();
 
 #endif
+
+#if TEST_STD_VER > 17
+    static_assert(test<forward_iterator<int*> >());
+    static_assert(test<bidirectional_iterator<int*> >());
+    static_assert(test<random_access_iterator<int*> >());
+    static_assert(test<int*>());
+#endif  // TEST_STD_VER > 17
 
   return 0;
 }
