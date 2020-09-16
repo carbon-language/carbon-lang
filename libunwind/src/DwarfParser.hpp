23bef7ee9923b1262326981960397e8cd95d6923
Daniel Kiss <daniel.kiss@arm.com>
2020-09-16 23:03:19 +0200

    [libunwind] Support for leaf function unwinding.
    Unwinding leaf function is useful in cases when the backtrace finds a
    leaf function for example when it caused a signal.
    This patch also add the support for the DW_CFA_undefined because it marks
    the end of the frames.
    
    Ryan Prichard provided code for the tests.
    
    Reviewed By: #libunwind, mstorsjo
    
    Differential Revision: https://reviews.llvm.org/D83573

--- libunwind/src/DwarfParser.hpp
+++ libunwind/src/DwarfParser.hpp
@@ -69,6 +69,7 @@ public:
   };
   enum RegisterSavedWhere {
     kRegisterUnused,
+    kRegisterUndefined,
     kRegisterInCFA,
     kRegisterOffsetFromCFA,
     kRegisterInRegister,
@@ -503,7 +504,7 @@ bool CFI_Parser<A>::parseInstructions(A &addressSpace, pint_t instructions,
                 "malformed DW_CFA_undefined DWARF unwind, reg too big");
         return false;
       }
-      results->setRegisterLocation(reg, kRegisterUnused, initialState);
+      results->setRegisterLocation(reg, kRegisterUndefined, initialState);
       _LIBUNWIND_TRACE_DWARF("DW_CFA_undefined(reg=%" PRIu64 ")\n", reg);
       break;
     case DW_CFA_same_value:
