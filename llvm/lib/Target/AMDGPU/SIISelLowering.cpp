7d593d0d6905b55ca1124fca5e4d1ebb17203138
Bogdan Graur <bgraur@google.com>
2020-09-17 18:04:21 +0200

    [amdgpu] Compilation fix for Release
    Reviewed By: bkramer
    
    Differential Revision: https://reviews.llvm.org/D87838

--- llvm/lib/Target/AMDGPU/SIISelLowering.cpp
+++ llvm/lib/Target/AMDGPU/SIISelLowering.cpp
@@ -11536,6 +11536,7 @@ static void lowerSGPRToVGPRCopy(MachineFunction &MF, MachineRegisterInfo &MRI,
       auto DstReg = MI.getOperand(0).getReg();
       auto MIB = BuildMI(MBB, MI, MI.getDebugLoc(), TII.get(Opcode), DstReg)
                      .add(MI.getOperand(1));
+      (void)MIB;
       LLVM_DEBUG(dbgs() << "        to: " << *MIB.getInstr());
       MI.eraseFromParent();
     }
