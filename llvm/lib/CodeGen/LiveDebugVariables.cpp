85ba2f16633638e55ebc8e84bfbd0aaaa2f72b7a
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-09-17 15:05:45 +0100

    LiveDebugVariables.cpp - remove unnecessary Compiler.h include. NFCI.
    Already included in LiveDebugVariables.h

--- llvm/lib/CodeGen/LiveDebugVariables.cpp
+++ llvm/lib/CodeGen/LiveDebugVariables.cpp
@@ -54,7 +54,6 @@
 #include "llvm/Pass.h"
 #include "llvm/Support/Casting.h"
 #include "llvm/Support/CommandLine.h"
-#include "llvm/Support/Compiler.h"
 #include "llvm/Support/Debug.h"
 #include "llvm/Support/raw_ostream.h"
 #include <algorithm>
