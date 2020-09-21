5b5e78a43124c0ced813f378195b36098f716c8f
Kazushi (Jam) Marukawa <jam@pobox.com>
2020-09-21 17:17:29 +0900

    [VE] Support register and frame-index pair correctly
    Support register and frame-index pair correctly as operands of
    generic load/store instrucitons, e.g. LD1BZXrri, STLrri, and etc.
    Add regression tests also.
    
    Differential Revision: https://reviews.llvm.org/D88779

--- llvm/lib/Target/VE/VEISelDAGToDAG.cpp
+++ llvm/lib/Target/VE/VEISelDAGToDAG.cpp
@@ -183,6 +183,14 @@ bool VEDAGToDAGISel::selectADDRrri(SDValue Addr, SDValue &Base, SDValue &Index,
     return false;
   }
   if (matchADDRrr(Addr, LHS, RHS)) {
+    // If the input is a pair of a frame-index and a register, move a
+    // frame-index to LHS.  This generates MI with following operands.
+    //    %dest, #FI, %reg, offset
+    // In the eliminateFrameIndex, above MI is converted to the following.
+    //    %dest, %fp, %reg, fi_offset + offset
+    if (dyn_cast<FrameIndexSDNode>(RHS))
+      std::swap(LHS, RHS);
+
     if (matchADDRri(RHS, Index, Offset)) {
       Base = LHS;
       return true;
