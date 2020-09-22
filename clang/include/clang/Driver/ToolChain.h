a594fd28e373cb7cd348cf01f6a90e055bf6cf6d
Petr Hosek <phosek@google.com>
2020-09-22 01:01:16 -0700

    [Driver] Move detectLibcxxIncludePath to ToolChain
    This helper method is useful even outside of Gnu toolchains, so move
    it to ToolChain so it can be reused in other toolchains such as Fuchsia.
    
    Differential Revision: https://reviews.llvm.org/D88452

--- clang/include/clang/Driver/ToolChain.h
+++ clang/include/clang/Driver/ToolChain.h
@@ -575,6 +575,9 @@ public:
   // given compilation arguments.
   virtual UnwindLibType GetUnwindLibType(const llvm::opt::ArgList &Args) const;
 
+  // Detect the highest available version of libc++ in base path.
+  virtual std::string detectLibcxxIncludePath(StringRef Base) const;
+
   /// AddClangCXXStdlibIncludeArgs - Add the clang -cc1 level arguments to set
   /// the include paths to use for the given C++ standard library type.
   virtual void
