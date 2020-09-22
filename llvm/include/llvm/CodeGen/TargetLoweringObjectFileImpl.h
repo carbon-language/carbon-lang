90242caca2074dab5a9b76e5bc36d9fafd2179a7
Reid Kleckner <rnk@google.com>
2020-09-22 13:45:22 -0700

    Revert "[CodeGen] emit CG profile for COFF object file"
    This reverts commit 91aed9bf975f1e4346cc8f4bdefc98436386ced2, it is
    causing link errors.

--- llvm/include/llvm/CodeGen/TargetLoweringObjectFileImpl.h
+++ llvm/include/llvm/CodeGen/TargetLoweringObjectFileImpl.h
@@ -35,6 +35,7 @@ class TargetLoweringObjectFileELF : public TargetLoweringObjectFile {
 protected:
   MCSymbolRefExpr::VariantKind PLTRelativeVariantKind =
       MCSymbolRefExpr::VK_None;
+  const TargetMachine *TM = nullptr;
 
 public:
   TargetLoweringObjectFileELF() = default;
