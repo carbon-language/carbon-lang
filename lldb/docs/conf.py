974551d37da8352c22fd849d19cb8fb1facff680
Jonas Devlieghere <jonas@devlieghere.com>
2020-09-28 13:50:22 -0700

    [lldb] Add print_function import

--- lldb/docs/conf.py
+++ lldb/docs/conf.py
@@ -10,8 +10,9 @@
 #
 # All configuration values have a default; values that are commented out
 # serve to show the default.
+from __future__ import print_function
 
-import sys, os
+import sys, os, re
 from datetime import date
 
 # If extensions (or modules to document with autodoc) are in another directory,
