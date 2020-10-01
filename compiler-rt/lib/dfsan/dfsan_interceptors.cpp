045a620c455d2f27a536d687ee6a0299b9e2c734
Jianzhou Zhao <jianzhouzh@google.com>
2020-10-01 18:05:34 +0000

    Release the shadow memory used by the mmap range at munmap
    When an application does a lot of pairs of mmap and munmap, if we did
    not release shadoe memory used by mmap addresses, this would increase
    memory usage.
    
    Reviewed-by: morehouse
    
    Differential Revision: https://reviews.llvm.org/D88686

--- compiler-rt/lib/dfsan/dfsan_interceptors.cpp
+++ compiler-rt/lib/dfsan/dfsan_interceptors.cpp
@@ -46,12 +46,25 @@ INTERCEPTOR(void *, mmap64, void *addr, SIZE_T length, int prot, int flags,
   return res;
 }
 
+INTERCEPTOR(int, munmap, void *addr, SIZE_T length) {
+  int res = REAL(munmap)(addr, length);
+  if (res != -1) {
+    uptr beg_shadow_addr = (uptr)__dfsan::shadow_for(addr);
+    void *end_addr =
+        (void *)((uptr)addr + RoundUpTo(length, GetPageSizeCached()));
+    uptr end_shadow_addr = (uptr)__dfsan::shadow_for(end_addr);
+    ReleaseMemoryPagesToOS(beg_shadow_addr, end_shadow_addr);
+  }
+  return res;
+}
+
 namespace __dfsan {
 void InitializeInterceptors() {
   CHECK(!interceptors_initialized);
 
   INTERCEPT_FUNCTION(mmap);
   INTERCEPT_FUNCTION(mmap64);
+  INTERCEPT_FUNCTION(munmap);
 
   interceptors_initialized = true;
 }
