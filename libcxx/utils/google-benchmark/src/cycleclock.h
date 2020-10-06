bd5fe7b010ea0d6c4b1e5e3740085f5eabf7def6
Min-Yih Hsu <minyihh@uci.edu>
2020-10-05 20:24:33 -0700

    [M680x0] Add google/benchmark's CycleTimer support for M68K
    This is a cherrypick of the upstream fix commit ffe1342 onto
    `llvm/utils/benchmark` and `libcxx/utils/google-benchmark`.
    This adds CycleTimer implementation for M680x0, which simply
    uses `gettimeofday` same as MIPS.
    
    Differential Review: https://reviews.llvm.org/D88868

--- libcxx/utils/google-benchmark/src/cycleclock.h
+++ libcxx/utils/google-benchmark/src/cycleclock.h
@@ -161,7 +161,7 @@ inline BENCHMARK_ALWAYS_INLINE int64_t Now() {
   struct timeval tv;
   gettimeofday(&tv, nullptr);
   return static_cast<int64_t>(tv.tv_sec) * 1000000 + tv.tv_usec;
-#elif defined(__mips__)
+#elif defined(__mips__) || defined(__m68k__)
   // mips apparently only allows rdtsc for superusers, so we fall
   // back to gettimeofday.  It's possible clock_gettime would be better.
   struct timeval tv;
