08924b54debcfd47bcf22a8213308cec7d22b975
Dave Lee <davelee.com@gmail.com>
2020-10-06 18:23:57 -0700

    [lldb] Remove unused code in GetVersion (NFC)
    Small cleanup to `lldb_private::GetVersion()`.
    
    Differential Revision: https://reviews.llvm.org/D88939

--- lldb/source/lldb.cpp
+++ lldb/source/lldb.cpp
@@ -33,12 +33,7 @@ static const char *GetLLDBRepository() {
 #endif
 }
 
-#define QUOTE(str) #str
-#define EXPAND_AND_QUOTE(str) QUOTE(str)
-
 const char *lldb_private::GetVersion() {
-  // On platforms other than Darwin, report a version number in the same style
-  // as the clang tool.
   static std::string g_version_str;
   if (g_version_str.empty()) {
     g_version_str += "lldb version ";
