f60686f35cc89504f3411f49cf16a651a74be6eb
Paula Toth <paulatoth@google.com>
2020-10-08 13:43:47 -0700

    [libc] Update buildbot worker version to 2.8.4.
    Tested locally by connecting to LLVM master. (:
    http://lab.llvm.org:8011/#/builders/78/builds/1
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D89069

--- libc/utils/buildbot/run.sh
+++ libc/utils/buildbot/run.sh
@@ -3,8 +3,8 @@
 # This serves as the entrypoint for docker to allow us to
 # run and start the buildbot while supplying the password
 # as an argument.
-buildslave create-slave --keepalive=200 "${WORKER_NAME}" \
+buildbot-worker create-worker --keepalive=200 "${WORKER_NAME}" \
   lab.llvm.org:9990 "${WORKER_NAME}" "$1"
 
-buildslave start "${WORKER_NAME}"
+buildbot-worker start "${WORKER_NAME}"
 tail -f ${WORKER_NAME}/twistd.log
