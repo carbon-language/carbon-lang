817cd3d191be44067c1d037729e0cdfe89e35a77
Matt Arsenault <Matthew.Arsenault@amd.com>
2020-10-08 20:05:18 -0400

    Fix missing c++ mode comment

--- llvm/include/llvm/LTO/Config.h
+++ llvm/include/llvm/LTO/Config.h
@@ -1,4 +1,4 @@
-//===-Config.h - LLVM Link Time Optimizer Configuration -------------------===//
+//===-Config.h - LLVM Link Time Optimizer Configuration ---------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
