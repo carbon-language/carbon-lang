d83cd73e9ddf9ba4cc5754560ef9a2abaaa30749
Michał Górny <mgorny@moritz.systems>
2020-10-10 09:23:15 +0200

    [lldb] [Process/FreeBSD] Mark methods override in RegisterContext*
    Differential Revision: https://reviews.llvm.org/D89181

--- lldb/source/Plugins/Process/FreeBSD/RegisterContextPOSIXProcessMonitor_arm64.h
+++ lldb/source/Plugins/Process/FreeBSD/RegisterContextPOSIXProcessMonitor_arm64.h
@@ -21,13 +21,13 @@ public:
       std::unique_ptr<RegisterInfoPOSIX_arm64> register_info);
 
 protected:
-  bool ReadGPR();
+  bool ReadGPR() override;
 
-  bool ReadFPR();
+  bool ReadFPR() override;
 
-  bool WriteGPR();
+  bool WriteGPR() override;
 
-  bool WriteFPR();
+  bool WriteFPR() override;
 
   // lldb_private::RegisterContext
   bool ReadRegister(const unsigned reg, lldb_private::RegisterValue &value);
@@ -36,39 +36,39 @@ protected:
                      const lldb_private::RegisterValue &value);
 
   bool ReadRegister(const lldb_private::RegisterInfo *reg_info,
-                    lldb_private::RegisterValue &value);
+                    lldb_private::RegisterValue &value) override;
 
   bool WriteRegister(const lldb_private::RegisterInfo *reg_info,
-                     const lldb_private::RegisterValue &value);
+                     const lldb_private::RegisterValue &value) override;
 
-  bool ReadAllRegisterValues(lldb::DataBufferSP &data_sp);
+  bool ReadAllRegisterValues(lldb::DataBufferSP &data_sp) override;
 
-  bool WriteAllRegisterValues(const lldb::DataBufferSP &data_sp);
+  bool WriteAllRegisterValues(const lldb::DataBufferSP &data_sp) override;
 
   uint32_t SetHardwareWatchpoint(lldb::addr_t addr, size_t size, bool read,
-                                 bool write);
+                                 bool write) override;
 
-  bool ClearHardwareWatchpoint(uint32_t hw_index);
+  bool ClearHardwareWatchpoint(uint32_t hw_index) override;
 
-  bool HardwareSingleStep(bool enable);
+  bool HardwareSingleStep(bool enable) override;
 
   // POSIXBreakpointProtocol
-  bool UpdateAfterBreakpoint();
+  bool UpdateAfterBreakpoint() override;
 
-  unsigned GetRegisterIndexFromOffset(unsigned offset);
+  unsigned GetRegisterIndexFromOffset(unsigned offset) override;
 
-  bool IsWatchpointHit(uint32_t hw_index);
+  bool IsWatchpointHit(uint32_t hw_index) override;
 
-  bool ClearWatchpointHits();
+  bool ClearWatchpointHits() override;
 
-  lldb::addr_t GetWatchpointAddress(uint32_t hw_index);
+  lldb::addr_t GetWatchpointAddress(uint32_t hw_index) override;
 
-  bool IsWatchpointVacant(uint32_t hw_index);
+  bool IsWatchpointVacant(uint32_t hw_index) override;
 
   bool SetHardwareWatchpointWithIndex(lldb::addr_t addr, size_t size, bool read,
-                                      bool write, uint32_t hw_index);
+                                      bool write, uint32_t hw_index) override;
 
-  uint32_t NumSupportedHardwareWatchpoints();
+  uint32_t NumSupportedHardwareWatchpoints() override;
 
 private:
   RegisterInfoPOSIX_arm64::GPR m_gpr_arm64; // 64-bit general purpose registers.
