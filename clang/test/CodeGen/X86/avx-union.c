bd05afcb3f4093fe76dd64fe29255550bfd894ec
Liu, Chen3 <chen3.liu@intel.com>
2020-10-14 10:12:59 +0800

    [X86][NFC] Fix RUN line bug in the testcase
    Testcase added in D78699 doesn't work because the wrong RUN line in the
    testcase.
    
    Differential Revision: https://reviews.llvm.org/D89361

--- clang/test/CodeGen/X86/avx-union.c
+++ clang/test/CodeGen/X86/avx-union.c
@@ -1,5 +1,5 @@
-// RUN: %clang_cc1 -w -ffreestanding -triple x86_64-linux-gnu -target-feature +avx -emit-llvm -o %t %s || FileCheck < %t %s --check-prefix=CHECK, AVX
-// RUN: %clang_cc1 -w -ffreestanding -triple x86_64-linux-gnu -target-feature +avx512f -emit-llvm -o %t %s || FileCheck < %t %s --check-prefix=CHECK, AVX512
+// RUN: %clang_cc1 -w -ffreestanding -triple x86_64-linux-gnu -target-feature +avx -emit-llvm -o - %s | FileCheck %s --check-prefix=CHECK --check-prefix=AVX
+// RUN: %clang_cc1 -w -ffreestanding -triple x86_64-linux-gnu -target-feature +avx512f -emit-llvm -o - %s | FileCheck %s --check-prefix=CHECK --check-prefix=AVX512
 // This tests verifies that a union parameter should pass by a vector regitster whose first eightbyte is SSE and the other eightbytes are SSEUP.
 
 typedef int __m256 __attribute__ ((__vector_size__ (32)));
@@ -19,8 +19,8 @@ extern void foo1(union M256 A);
 extern void foo2(union M512 A);
 union M256 m1;
 union M512 m2;
-// CHECK-LABEL: define dso_local void @test()
-// CHECK:       void @foo1(<4 x double>
+// CHECK-LABEL: define void @test()
+// CHECK:       call void @foo1(<4 x double>
 // AVX:         call void @foo2(%union.M512* byval(%union.M512) align 64
 // AVX512:      call void @foo2(<8 x double>
 void test() {
