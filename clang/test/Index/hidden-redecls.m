6030a075164c7016b29a67680ed5c9e7c5932109
Konstantin Schwarz <konstantin.schwarz@hightec-rt.com>
2020-10-15 11:57:19 +0200

    Fix hidden-redecls.m test for some environments
    This test was failing in our CI environment, because Jenkins mounts the workspaces into Docker containers using their full path, i.e. /home/jenkins/workspaces/llvm-build.
    We've seen permission denied errors because /home/jenkins is mounted with root permissions and the default cache directory under Linux is $HOME/.cache.
    
    The fix is to explicitly provide the -fmodules-cache-path, which the other tests already seem to provide.
    
    Reviewed By: akyrtzi
    
    Differential Revision: https://reviews.llvm.org/D89453

--- clang/test/Index/hidden-redecls.m
+++ clang/test/Index/hidden-redecls.m
@@ -7,6 +7,7 @@
 // p1_method in protocol P1 is hidden since module_redecls.sub hasn't been
 // imported yet. Check it is still indexed.
 
-// RUN: c-index-test -index-file-full %s -isystem %S/Inputs -fmodules -target x86_64-apple-macosx10.7 | FileCheck %s
+// RUN: rm -rf %t
+// RUN: c-index-test -index-file-full %s -isystem %S/Inputs -fmodules -fmodules-cache-path=%t -target x86_64-apple-macosx10.7 | FileCheck %s
 // CHECK: [indexDeclaration]: kind: objc-instance-method | name: p1_method | {{.*}} | loc: {{.*}}hidden-redecls-sub.h:2:9 | {{.*}} | isRedecl: 0
 // CHECK: [indexDeclaration]: kind: objc-instance-method | name: p1_method | {{.*}} | loc: {{.*}}hidden-redecls-sub.h:3:9 | {{.*}} | isRedecl: 1
