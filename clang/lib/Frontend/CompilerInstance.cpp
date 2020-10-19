4000c9ee18ecebe3ff0f197af8c1fb434ad986e5
Volodymyr Sapsai <vsapsai@apple.com>
2020-10-19 13:39:47 -0700

    Reland "[Modules] Add stats to measure performance of building and loading modules."
    Measure amount of high-level or fixed-cost operations performed during
    building/loading modules and during header search. High-level operations
    like building a module or processing a .pcm file are motivated by
    previous issues where clang was re-building modules or re-reading .pcm
    files unnecessarily. Fixed-cost operations like `stat` calls are tracked
    because clang cannot change how long each operation takes but it can
    perform fewer of such operations to improve the compile time.
    
    Also tracking such stats over time can help us detect compile-time
    regressions. Added stats are more stable than the actual measured
    compilation time, so expect the detected regressions to be less noisy.
    
    On relanding drop stats in MemoryBuffer.cpp as their value is pretty low
    but affects a lot of clients and many of those aren't interested in
    modules and header search.
    
    rdar://problem/55715134
    
    Reviewed By: aprantl, bruno
    
    Differential Revision: https://reviews.llvm.org/D86895

--- clang/lib/Frontend/CompilerInstance.cpp
+++ clang/lib/Frontend/CompilerInstance.cpp
@@ -55,6 +55,10 @@
 
 using namespace clang;
 
+#define DEBUG_TYPE "modules"
+
+ALWAYS_ENABLED_STATISTIC(NumCompiledModules, "Number of compiled modules.");
+
 CompilerInstance::CompilerInstance(
     std::shared_ptr<PCHContainerOperations> PCHContainerOps,
     InMemoryModuleCache *SharedModuleCache)
@@ -1063,6 +1067,7 @@ compileModuleImpl(CompilerInstance &ImportingInstance, SourceLocation ImportLoc,
                   llvm::function_ref<void(CompilerInstance &)> PostBuildStep =
                       [](CompilerInstance &) {}) {
   llvm::TimeTraceScope TimeScope("Module Compile", ModuleName);
+  ++NumCompiledModules;
 
   // Construct a compiler invocation for creating this module.
   auto Invocation =
