8041f13e62ae3dfd4b66e33d917aae45dfb61d2d
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2020-10-19 17:22:54 +0100

    [libc++] Skip tests using constexpr destructors with older clang versions
    It appears that the released version of clang that supports constexpr
    destructors is clang 10 and the oldest one that accepts -std=c++2a is 5,
    so mark these as UNSUPPORTED for clang-5 to clang-9.
    
    Reviewed By: #libc, ldionne
    
    Differential Revision: https://reviews.llvm.org/D89704

--- libcxx/test/std/utilities/memory/specialized.algorithms/specialized.construct/construct_at.pass.cpp
+++ libcxx/test/std/utilities/memory/specialized.algorithms/specialized.construct/construct_at.pass.cpp
@@ -7,6 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
+// constexpr destructors are only supported starting with clang 10
+// UNSUPPORTED: clang-5, clang-6, clang-7, clang-8, clang-9
 
 // Investigation needed
 // UNSUPPORTED: gcc
