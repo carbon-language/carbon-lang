cf9831b843b44baaf5dc01954ca381e2449e4670
Martin Storsjö <martin@martin.st>
2020-10-19 10:58:31 +0300

    [libcxx] [test] Add LIBCPP_ONLY() around another test for an implementation detail
    Differential Revision: https://reviews.llvm.org/D89675

--- libcxx/test/std/input.output/filesystems/fs.enum/enum.perm_options.pass.cpp
+++ libcxx/test/std/input.output/filesystems/fs.enum/enum.perm_options.pass.cpp
@@ -33,7 +33,7 @@ int main(int, char**) {
   typedef std::underlying_type<E>::type UT;
   static_assert(!std::is_convertible<E, UT>::value, "");
 
-  static_assert(std::is_same<UT, unsigned char >::value, ""); // Implementation detail
+  LIBCPP_ONLY(static_assert(std::is_same<UT, unsigned char >::value, "")); // Implementation detail
 
   typedef check_bitmask_type<E, E::replace, E::nofollow> BitmaskTester;
   assert(BitmaskTester::check());
