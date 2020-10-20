1ed506deaddb41870d22f5b48d52ba710e8d6c00
Michael Liao <michael.hliao@gmail.com>
2020-10-20 10:04:53 -0400

    [clang] Fix warnings on the missing of explicitly copy constructor on the base class. NFC.

--- clang/include/clang/Basic/PartialDiagnostic.h
+++ clang/include/clang/Basic/PartialDiagnostic.h
@@ -49,7 +49,8 @@ public:
   PartialDiagnostic(unsigned DiagID, DiagStorageAllocator &Allocator_)
       : StreamingDiagnostic(Allocator_), DiagID(DiagID) {}
 
-  PartialDiagnostic(const PartialDiagnostic &Other) : DiagID(Other.DiagID) {
+  PartialDiagnostic(const PartialDiagnostic &Other)
+      : StreamingDiagnostic(Other), DiagID(Other.DiagID) {
     Allocator = Other.Allocator;
     if (Other.DiagStorage) {
       DiagStorage = getStorage();
