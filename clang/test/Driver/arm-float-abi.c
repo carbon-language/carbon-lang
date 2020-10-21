ba60de5250ce1c4baa4a7bb7098ac67349f88a99
John Brawn <john.brawn@arm.com>
2020-10-21 17:34:47 +0100

    Use -### in arm-float-abi.c test
    This is needed to prevent the test from failing when llvm is
    configured so that the arm target is not present, which is the case
    for some buildbots.

--- clang/test/Driver/arm-float-abi.c
+++ clang/test/Driver/arm-float-abi.c
@@ -20,9 +20,9 @@
 // RUN:   | FileCheck --check-prefix=CHECK-ANDROID-ERROR %s
 // CHECK-ANDROID-ERROR: unsupported option '-mfloat-abi=hard' for target 'armv7-unknown-linux-android21'
 
-// RUN: %clang -target armv7-linux-androideabi21 %s -S -o - -mfloat-abi=soft 2>&1 \
+// RUN: %clang -target armv7-linux-androideabi21 %s -S -o - -mfloat-abi=soft -### 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-ANDROID-NOERROR %s
-// RUN: %clang -target armv7-linux-androideabi21 %s -S -o - -mfloat-abi=softfp 2>&1 \
+// RUN: %clang -target armv7-linux-androideabi21 %s -S -o - -mfloat-abi=softfp -### 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-ANDROID-NOERROR %s
 // CHECK-ANDROID-NOERROR-NOT: unsupported option
 
@@ -34,6 +34,6 @@
 // RUN:   | FileCheck --check-prefix=CHECK-WATCHOS-ERROR2 %s
 // CHECK-WATCHOS-ERROR2: unsupported option '-mfloat-abi=softfp' for target 'thumbv7-apple-watchos4'
 
-// RUN: %clang -target armv7-apple-watchos4 %s -S -o - -mfloat-abi=hard 2>&1 \
+// RUN: %clang -target armv7-apple-watchos4 %s -S -o - -mfloat-abi=hard -### 2>&1 \
 // RUN:   | FileCheck --check-prefix=CHECK-WATCHOS-NOERROR %s
 // CHECK-WATCHOS-NOERROR-NOT: unsupported option
