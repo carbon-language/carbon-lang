997d41cdec536a0b632505d9b0c4ab5dec5d7d12
Louis Dionne <ldionne@apple.com>
2020-10-21 11:11:45 -0400

    [libc++] Instantiate additional <iostream> members in the dylib
    This commit adds new explicit instantiations for some classes in <iostream>
    in the library. This is done after noticing that many programs that use
    streams end up containing weak definitions of these classes, which has a
    negative impact on both code size and load times (due to the need to
    resolve weak symbols at load time). Note that we are just adding the
    additional explicit instantiations for the `char` specializations, since
    the `wchar_t` specializations are not used as often, and as a result there
    wouldn't be a clear benefit.
    
    This change is not an ABI break, since we are just adding additional
    symbols.
    
    Differential Revision: https://reviews.llvm.org/D90677

--- libcxx/src/ios.instantiations.cpp
+++ libcxx/src/ios.instantiations.cpp
@@ -7,9 +7,11 @@
 //===----------------------------------------------------------------------===//
 
 #include "__config"
+#include "fstream"
 #include "ios"
 #include "istream"
 #include "ostream"
+#include "sstream"
 #include "streambuf"
 
 
@@ -25,6 +27,16 @@ template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_istream<wchar_t>;
 template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_ostream<char>;
 template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_ostream<wchar_t>;
 template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_iostream<char>;
+
+// Additional instantiations added later. Whether programs rely on these being
+// available is protected by _LIBCPP_ABI_ENABLE_ADDITIONAL_IOSTREAM_EXPLICIT_INSTANTIATIONS_1.
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_stringbuf<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_stringstream<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_ostringstream<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_istringstream<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_ifstream<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_ofstream<char>;
+template class _LIBCPP_CLASS_TEMPLATE_INSTANTIATION_VIS basic_filebuf<char>;
 
 // Add more here if needed...
 
