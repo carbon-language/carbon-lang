cae4067ec1cdf7846aa46dab13d3bc1f58b76016
George Mitenkov <georgemitenk0v@gmail.com>
2020-10-23 10:33:53 -0400

    [MLIR][mlir-spirv-cpu-runner] A pass to emulate a call to kernel in LLVM
    This patch introduces a pass for running
    `mlir-spirv-cpu-runner` - LowerHostCodeToLLVMPass.
    
    This pass emulates `gpu.launch_func` call in LLVM dialect and lowers
    the host module code to LLVM. It removes the `gpu.module`, creates a
    sequence of global variables that are later linked to the varables
    in the kernel module, as well as a series of copies to/from
    them to emulate the memory transfer to/from the host or to/from the
    device sides. It also converts the remaining Standard dialect into
    LLVM dialect, emitting C wrappers.
    
    Reviewed By: mravishankar
    
    Differential Revision: https://reviews.llvm.org/D86112

--- mlir/include/mlir/Conversion/SPIRVToLLVM/ConvertSPIRVToLLVMPass.h
+++ mlir/include/mlir/Conversion/SPIRVToLLVM/ConvertSPIRVToLLVMPass.h
@@ -20,6 +20,16 @@ class ModuleOp;
 template <typename T>
 class OperationPass;
 
+/// Creates a pass to emulate `gpu.launch_func` call in LLVM dialect and lower
+/// the host module code to LLVM.
+///
+/// This transformation creates a sequence of global variables that are later
+/// linked to the varables in the kernel module, and a series of copies to/from
+/// them to emulate the memory transfer from the host or to the device sides. It
+/// also converts the remaining Standard dialect into LLVM dialect, emitting C
+/// wrappers.
+std::unique_ptr<OperationPass<ModuleOp>> createLowerHostCodeToLLVMPass();
+
 /// Creates a pass to convert SPIR-V operations to the LLVMIR dialect.
 std::unique_ptr<OperationPass<ModuleOp>> createConvertSPIRVToLLVMPass();
 
