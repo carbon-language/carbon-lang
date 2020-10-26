4f98eaf655800720739867da7082edb829d84ffd
Puyan Lotfi <plotfi@fb.com>
2020-10-26 18:06:38 -0400

    [NFC] Fixing comment heading for MachineStableHash.h.
    Wrong filename and description.

--- llvm/include/llvm/CodeGen/MachineStableHash.h
+++ llvm/include/llvm/CodeGen/MachineStableHash.h
@@ -1,4 +1,4 @@
-//===------------ MIRVRegNamerUtils.h - MIR VReg Renaming Utilities -------===//
+//===------------ MachineStableHash.h - MIR Stable Hashing Utilities ------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
