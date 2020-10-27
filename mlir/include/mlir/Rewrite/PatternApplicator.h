c09d10437f9f00c1092c6034f80973fd0a2a984f
River Riddle <riddleriver@gmail.com>
2020-10-27 16:22:13 -0700

    [mlir][NFC] Fix incorrect header comments.
    Resolves missed comments in D89103

--- mlir/include/mlir/Rewrite/PatternApplicator.h
+++ mlir/include/mlir/Rewrite/PatternApplicator.h
@@ -1,4 +1,4 @@
-//===- PatternApplicator.h - PatternApplicator -------==---------*- C++ -*-===//
+//===- PatternApplicator.h - PatternApplicator ------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- mlir/include/mlir/Transforms/GreedyPatternRewriteDriver.h
+++ mlir/include/mlir/Transforms/GreedyPatternRewriteDriver.h
@@ -1,4 +1,4 @@
-//===- DialectConversion.h - MLIR dialect conversion pass -------*- C++ -*-===//
+//===- GreedyPatternRewriteDriver.h - Greedy Pattern Driver -----*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
