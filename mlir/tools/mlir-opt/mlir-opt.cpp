1142eaed9d4e31aa10584f2755a350970495002f
Geoffrey Martin-Noble <gcmn@google.com>
2020-10-30 13:32:02 -0700

    Revert "[MLIR][SPIRV] Start module combiner."
    This reverts commit 27324f28552d0c66e8b28efd9c15820e5f246619.
    
    Shared libs build is broken linking lib/libMLIRSPIRVModuleCombiner.so:
    
    ```
    ModuleCombiner.cpp:
      undefined reference to `mlir::spirv::ModuleOp::addressing_model()
    ```
    
    https://buildkite.com/mlir/mlir-core/builds/8988#e3d966b9-ea43-492e-a192-b28e71e9a15b

--- mlir/tools/mlir-opt/mlir-opt.cpp
+++ mlir/tools/mlir-opt/mlir-opt.cpp
@@ -79,7 +79,6 @@ void registerTestPrintNestingPass();
 void registerTestRecursiveTypesPass();
 void registerTestReducer();
 void registerTestSpirvEntryPointABIPass();
-void registerTestSpirvModuleCombinerPass();
 void registerTestSCFUtilsPass();
 void registerTestTraitsPass();
 void registerTestVectorConversions();
@@ -141,7 +140,6 @@ void registerTestPasses() {
   registerTestReducer();
   registerTestGpuParallelLoopMappingPass();
   registerTestSpirvEntryPointABIPass();
-  registerTestSpirvModuleCombinerPass();
   registerTestSCFUtilsPass();
   registerTestTraitsPass();
   registerTestVectorConversions();
