9f9077d7d1b436c75e493916c7c196a4c4937388
Vy Nguyen <vyng@google.com>
2020-11-05 00:19:38 -0500

    [sanitizers] Add missing definition
    Fix breakages from https://reviews.llvm.org/D90811
    
    http://lab.llvm.org:8011/#/builders/37/builds/471/steps/8/logs/stdio
    
    Differential Revision: https://reviews.llvm.org/D90818

--- compiler-rt/lib/sanitizer_common/sanitizer_common_nolibc.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_common_nolibc.cpp
@@ -10,9 +10,10 @@
 // libc in no-libcdep sources.
 //===----------------------------------------------------------------------===//
 
-#include "sanitizer_platform.h"
 #include "sanitizer_common.h"
+#include "sanitizer_flags.h"
 #include "sanitizer_libc.h"
+#include "sanitizer_platform.h"
 
 namespace __sanitizer {
 
@@ -29,6 +30,7 @@ void SleepForSeconds(int seconds) { internal_sleep(seconds); }
 
 #if !SANITIZER_WINDOWS && !SANITIZER_MAC
 void ListOfModules::init() {}
+void InitializePlatformCommonFlags(CommonFlags *cf) {}
 #endif
 
 }  // namespace __sanitizer
