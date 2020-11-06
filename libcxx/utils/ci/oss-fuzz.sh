1d53b55e18670df06802c4b5df187ef014e8fe7c
Louis Dionne <ldionne@apple.com>
2020-11-06 10:06:44 -0500

    [libc++] Try fixing the oss-fuzz build
    See https://github.com/google/oss-fuzz/issues/4586.

--- libcxx/utils/ci/oss-fuzz.sh
+++ libcxx/utils/ci/oss-fuzz.sh
@@ -9,11 +9,13 @@ if [[ ${SANITIZER} = *undefined* ]]; then
 fi
 
 for test in libcxx/test/libcxx/fuzzing/*.pass.cpp; do
+    exe="$(basename ${test})"
+    exe="${exe%.pass.cpp}"
     ${CXX} ${CXXFLAGS} \
         -std=c++14 \
         -DLIBCPP_OSS_FUZZ \
         -nostdinc++ -cxx-isystem libcxx/include \
-        -o "${OUT}/$(basename ${test})" \
+        -o "${OUT}/${exe}" \
         ${test} \
         ${LIB_FUZZING_ENGINE}
 done
