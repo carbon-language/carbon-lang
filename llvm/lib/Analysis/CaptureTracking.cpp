bac97993ca9351b71efbaac07632ef62e0fdb02a
Nikita Popov <nikita.ppv@gmail.com>
2020-11-07 10:13:49 +0100

    [CaptureTracking] Avoid duplicate shouldExplode() check (NFCI)
    We check shouldExplore() before adding uses to the worklist, so
    uses that should not be explored will not reach captured() in the
    first place.

--- llvm/lib/Analysis/CaptureTracking.cpp
+++ llvm/lib/Analysis/CaptureTracking.cpp
@@ -159,9 +159,6 @@ namespace {
       if (isa<ReturnInst>(U->getUser()) && !ReturnCaptures)
         return false;
 
-      if (!shouldExplore(U))
-        return false;
-
       Captured = true;
       return true;
     }
