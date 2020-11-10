defc59734a00cc0f476f6a93c35fd0b313ed587a
Simon Pilgrim <llvm-dev@redking.me.uk>
2020-11-10 10:17:46 +0000

    Add a llvm_unreachable to fix MSVC unhandled enum warning. NFCI.

--- clang/lib/AST/ItaniumMangle.cpp
+++ clang/lib/AST/ItaniumMangle.cpp
@@ -5028,6 +5028,8 @@ static bool isZeroInitialized(QualType T, const APValue &V) {
   case APValue::MemberPointer:
     return !V.getMemberPointerDecl();
   }
+
+  llvm_unreachable("Unhandled APValue::ValueKind enum");
 }
 
 void CXXNameMangler::mangleValueInTemplateArg(QualType T, const APValue &V) {
