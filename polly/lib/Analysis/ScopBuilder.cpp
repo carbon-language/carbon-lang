e408935bb5339e20035d84307c666fbdd15e99e0
Michael Kruse <llvm-project@meinersbur.de>
2020-11-10 02:37:35 -0600

    [Polly][ScopBuilder] Use only modeled instructions to compute statement granularity.
    ScopBuilder distributes independent instructions between statements.
    Only modeled (e.g. not synthesizable) instructions are represented.
    To compute independence, non-modeled instructions were used in some
    parts of determining instruction independence, which could lead to the
    re-introduction of non-model instructions.
    
    In particular, required invariant loads could be added to instruction
    list, which then led to redundant MemoryAccesses for such a load.
    
    This fixes llvm.org/PR48059.

--- polly/lib/Analysis/ScopBuilder.cpp
+++ polly/lib/Analysis/ScopBuilder.cpp
@@ -2132,11 +2132,11 @@ void ScopBuilder::buildEqivClassBlockStmts(BasicBlock *BB) {
   // The order of statements must be preserved w.r.t. their ordered
   // instructions. Without this explicit scan, we would also use non-ordered
   // instructions (whose order is arbitrary) to determine statement order.
-  for (Instruction &Inst : *BB) {
-    if (!isOrderedInstruction(&Inst))
+  for (Instruction *Inst : ModeledInsts) {
+    if (!isOrderedInstruction(Inst))
       continue;
 
-    auto LeaderIt = UnionFind.findLeader(&Inst);
+    auto LeaderIt = UnionFind.findLeader(Inst);
     if (LeaderIt == UnionFind.member_end())
       continue;
 
@@ -2146,15 +2146,15 @@ void ScopBuilder::buildEqivClassBlockStmts(BasicBlock *BB) {
 
   // Collect the instructions of all leaders. UnionFind's member iterator
   // unfortunately are not in any specific order.
-  for (Instruction &Inst : *BB) {
-    auto LeaderIt = UnionFind.findLeader(&Inst);
+  for (Instruction *Inst : ModeledInsts) {
+    auto LeaderIt = UnionFind.findLeader(Inst);
     if (LeaderIt == UnionFind.member_end())
       continue;
 
-    if (&Inst == MainInst)
+    if (Inst == MainInst)
       MainLeader = *LeaderIt;
     std::vector<Instruction *> &InstList = LeaderToInstList[*LeaderIt];
-    InstList.push_back(&Inst);
+    InstList.push_back(Inst);
   }
 
   // Finally build the statements.
