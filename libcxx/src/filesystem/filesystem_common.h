173b51169b838255aff4d4ff4eb6014b101a9687
Zbigniew Sarbinowski <zibi@ca.ibm.com>
2020-11-12 11:22:04 -0500

    [SystemZ][ZOS] Porting the time functions within libc++ to z/OS
    This patch is one part of many steps required to build libc++ and libc++abi libraries on z/OS.  This particular deals with time related functions and consists of the following 3 parts.
    
    1) Initialization of :timeval within libc++ library need to be adjusted to work on z/OS.
    The following is z/OS definition from time.h which includes additional aggregate member.
    typedef signed int suseconds_t;
    struct timeval {
    time_t tv_sec;
    char tv_usec_pad[4];
    suseconds_t tv_usec;
    };
    
    In contracts the following is definition from time.h on Linux.
    
    typedef long int __suseconds_t;
    struct timeval
    {
    __time_t tv_sec;
    __suseconds_t tv_usec;
    };
    
    2) In addition, retrieving ::timespec within libc++ library needs to be adjusted to compensate the difference of some of the members of ::stat depending of the target host.
    Here are the 2 members in conflict on z/OS extracted from stat.h.
    struct stat {
    ...
    time_t st_atime;
    time_t st_mtime;
    ...
    };
    In contract here is Linux equivalent from stat.h.
    struct stat
    {
    ...
    struct timespec st_atim;
    struct timespec st_mtim;
    ...
    };
    
    3) On Linux both members are of type timespec whereas on z/OS an object of type timespec need to be constructed first before retrieving it within libc++ library.
    
    The libc++ header file __threading_support calls nanosleep, which is not available on z/OS.
    The equivalent functionality will be implemented by using both sleep() and usleep().
    
    Reviewed By: ldionne, #libc
    
    Differential Revision: https://reviews.llvm.org/D87940

--- libcxx/src/filesystem/filesystem_common.h
+++ libcxx/src/filesystem/filesystem_common.h
@@ -198,7 +198,8 @@ private:
 using chrono::duration;
 using chrono::duration_cast;
 
-using TimeSpec = timespec;
+using TimeSpec = struct timespec;
+using TimeVal = struct timeval;
 using StatT = struct stat;
 
 template <class FileTimeT, class TimeT,
@@ -381,26 +382,38 @@ public:
 using fs_time = time_util<file_time_type, time_t, TimeSpec>;
 
 #if defined(__APPLE__)
-TimeSpec extract_mtime(StatT const& st) { return st.st_mtimespec; }
-TimeSpec extract_atime(StatT const& st) { return st.st_atimespec; }
+inline TimeSpec extract_mtime(StatT const& st) { return st.st_mtimespec; }
+inline TimeSpec extract_atime(StatT const& st) { return st.st_atimespec; }
+#elif defined(__MVS__)
+inline TimeSpec extract_mtime(StatT const& st) {
+  TimeSpec TS = {st.st_mtime, 0};
+  return TS;
+}
+inline TimeSpec extract_atime(StatT const& st) {
+  TimeSpec TS = {st.st_atime, 0};
+  return TS;
+}
 #else
-TimeSpec extract_mtime(StatT const& st) { return st.st_mtim; }
-TimeSpec extract_atime(StatT const& st) { return st.st_atim; }
+inline TimeSpec extract_mtime(StatT const& st) { return st.st_mtim; }
+inline TimeSpec extract_atime(StatT const& st) { return st.st_atim; }
 #endif
 
-// allow the utimes implementation to compile even it we're not going
-// to use it.
-
-bool posix_utimes(const path& p, std::array<TimeSpec, 2> const& TS,
-                  error_code& ec) {
+inline TimeVal make_timeval(TimeSpec const& ts) {
   using namespace chrono;
   auto Convert = [](long nsec) {
-    using int_type = decltype(std::declval< ::timeval>().tv_usec);
+    using int_type = decltype(std::declval<TimeVal>().tv_usec);
     auto dur = duration_cast<microseconds>(nanoseconds(nsec)).count();
     return static_cast<int_type>(dur);
   };
-  struct ::timeval ConvertedTS[2] = {{TS[0].tv_sec, Convert(TS[0].tv_nsec)},
-                                     {TS[1].tv_sec, Convert(TS[1].tv_nsec)}};
+  TimeVal TV = {};
+  TV.tv_sec = ts.tv_sec;
+  TV.tv_usec = Convert(ts.tv_nsec);
+  return TV;
+}
+
+inline bool posix_utimes(const path& p, std::array<TimeSpec, 2> const& TS,
+                  error_code& ec) {
+  TimeVal ConvertedTS[2] = {make_timeval(TS[0]), make_timeval(TS[1])};
   if (::utimes(p.c_str(), ConvertedTS) == -1) {
     ec = capture_errno();
     return true;
