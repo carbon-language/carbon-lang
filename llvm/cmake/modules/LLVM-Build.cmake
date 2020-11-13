f4a4c63588d4d784f42a94591c46b72bec64a415
serge-sans-paille <sguelton@redhat.com>
2020-11-13 15:49:27 +0100

    llvmbuildectomy - support disabled native target

--- llvm/cmake/modules/LLVM-Build.cmake
+++ llvm/cmake/modules/LLVM-Build.cmake
@@ -81,13 +81,21 @@ endfunction()
 
 # Resolve cross-component dependencies, for each available component.
 function(LLVMBuildResolveComponentsLink)
-  get_property(llvm_components GLOBAL PROPERTY LLVM_COMPONENT_LIBS)
-  get_property(llvm_has_jit_native TARGET ${LLVM_NATIVE_ARCH} PROPERTY LLVM_HAS_JIT)
+
+  # the native target may not be enabled when cross compiling
+  if(TARGET ${LLVM_NATIVE_ARCH})
+    get_property(llvm_has_jit_native TARGET ${LLVM_NATIVE_ARCH} PROPERTY LLVM_HAS_JIT)
+  else()
+    set(llvm_has_jit_native OFF)
+  endif()
+
   if(llvm_has_jit_native)
     set_property(TARGET Engine APPEND PROPERTY LLVM_LINK_COMPONENTS "MCJIT" "Native")
   else()
     set_property(TARGET Engine APPEND PROPERTY LLVM_LINK_COMPONENTS "Interpreter")
   endif()
+
+  get_property(llvm_components GLOBAL PROPERTY LLVM_COMPONENT_LIBS)
   foreach(llvm_component ${llvm_components})
     get_property(link_components TARGET ${llvm_component} PROPERTY LLVM_LINK_COMPONENTS)
     llvm_map_components_to_libnames(llvm_libs ${link_components})
