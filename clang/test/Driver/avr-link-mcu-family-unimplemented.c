bba96c99655579f5b94abb1e2d07bf2b4b497718
Ben Shi <powerman1st@163.com>
2020-11-17 23:28:41 +0800

    [clang][AVR] Improve avr-ld command line options
    Reviewed By: dylanmckay
    
    Differential Revision: https://reviews.llvm.org/D88410

--- clang/test/Driver/avr-link-mcu-family-unimplemented.c
+++ clang/test/Driver/avr-link-mcu-family-unimplemented.c
@@ -1,7 +1,10 @@
-// RUN: %clang -### -target avr -no-canonical-prefixes -save-temps -mmcu=attiny13a %s 2>&1 | FileCheck --check-prefix=WARN %s
+// RUN: %clang -### -target avr -no-canonical-prefixes -save-temps -mmcu=attiny102 %s 2>&1 | FileCheck --check-prefix=WARN0 %s
+// WARN0: warning: support for linking stdlibs for microcontroller 'attiny102' is not implemented
+// WARN0: warning: standard library not linked and so no interrupt vector table or compiler runtime routines will be linked
 
-// WARN: warning: support for linking stdlibs for microcontroller 'attiny13a' is not implemented
-// WARN: warning: standard library not linked and so no interrupt vector table or compiler runtime routines will be linked
+// RUN: %clang -### -target avr -no-canonical-prefixes -save-temps -mmcu=atxmega32x1 %s 2>&1 | FileCheck --check-prefix=WARN1 %s
+// WARN1: warning: support for linking stdlibs for microcontroller 'atxmega32x1' is not implemented
+// WARN1: warning: standard library not linked and so no interrupt vector table or compiler runtime routines will be linked
 
 int main() { return 0; }
 
