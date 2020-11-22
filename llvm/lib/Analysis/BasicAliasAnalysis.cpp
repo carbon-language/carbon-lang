6f5ef648a57aed3274118dcbd6467e8ac4f6ed1f
Nikita Popov <nikita.ppv@gmail.com>
2020-11-22 20:09:56 +0100

    [BasicAA] Avoid unnecessary cache update (NFC)
    If the final recursive query returns MayAlias as well, there is
    no need to update the cache (which already stores MayAlias).

--- llvm/lib/Analysis/BasicAliasAnalysis.cpp
+++ llvm/lib/Analysis/BasicAliasAnalysis.cpp
@@ -1785,7 +1785,11 @@ AliasResult BasicAAResult::aliasCheck(const Value *V1, LocationSize V1Size,
   // memory locations. We have already ensured that BasicAA has a MayAlias
   // cache result for these, so any recursion back into BasicAA won't loop.
   AliasResult Result = getBestAAResults().alias(Locs.first, Locs.second, AAQI);
-  return AAQI.updateResult(Locs, Result);
+  if (Result != MayAlias)
+    return AAQI.updateResult(Locs, Result);
+
+  // MayAlias is already in the cache.
+  return MayAlias;
 }
 
 /// Check whether two Values can be considered equivalent.
