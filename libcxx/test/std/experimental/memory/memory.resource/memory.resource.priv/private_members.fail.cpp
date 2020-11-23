0a20660c8fcb5bb8469e323585eeaecacea67c44
zoecarver <z.zoelec2@gmail.com>
2020-11-23 14:21:50 -0800

    [libcxx] Resolve LWG 2724 protected -> private.
    Fixes LWG issue 2724: "The protected virtual member functions of memory_resource should be private."
    
    Differential Revision: https://reviews.llvm.org/D66615

--- libcxx/test/std/experimental/memory/memory.resource/memory.resource.priv/private_members.fail.cpp
+++ libcxx/test/std/experimental/memory/memory.resource/memory.resource.priv/private_members.fail.cpp
@@ -10,9 +10,9 @@
 
 // <experimental/memory_resource>
 
-// memory_resource::do_allocate(size_t, size_t);          /* protected */
-// memory_resource::do_deallocate(void*, size_t, size_t); /* protected */
-// memory_resource::do_is_equal(memory_resource const&);  /* protected */
+// memory_resource::do_allocate(size_t, size_t);          /* private */
+// memory_resource::do_deallocate(void*, size_t, size_t); /* private */
+// memory_resource::do_is_equal(memory_resource const&);  /* private */
 
 #include <experimental/memory_resource>
 
@@ -20,9 +20,9 @@ namespace ex = std::experimental::pmr;
 
 int main(int, char**) {
     ex::memory_resource *m = ex::new_delete_resource();
-    m->do_allocate(0, 0); // expected-error{{'do_allocate' is a protected member}}
-    m->do_deallocate(nullptr, 0, 0); // expected-error{{'do_deallocate' is a protected member}}
-    m->do_is_equal(*m); // expected-error{{'do_is_equal' is a protected member}}
+    m->do_allocate(0, 0); // expected-error{{'do_allocate' is a private member}}
+    m->do_deallocate(nullptr, 0, 0); // expected-error{{'do_deallocate' is a private member}}
+    m->do_is_equal(*m); // expected-error{{'do_is_equal' is a private member}}
 
   return 0;
 }
