5641b1dfddff847f7f3edc484537f9314c283225
Marek Kurdej <marek.kurdej@gmail.com>
2020-11-26 12:40:50 +0100

    [libc++] Mark a few more tests as unsupported on gcc-8/9.
    This will fix remaining failures on gcc-9 buildbot: http://lab.llvm.org:8011/#/builders/101.
    gcc-8 and gcc-9 do not support constexpr destructors nor constexpr allocation.
    
    Fix gcc warnings: -Wconversion, -Wpragmas.

--- libcxx/test/std/utilities/memory/allocator.traits/allocator.traits.members/destroy.pass.cpp
+++ libcxx/test/std/utilities/memory/allocator.traits/allocator.traits.members/destroy.pass.cpp
@@ -7,6 +7,8 @@
 //===----------------------------------------------------------------------===//
 
 // UNSUPPORTED: clang-8
+// constexpr destructors are only supported starting with gcc 10
+// UNSUPPORTED: gcc-8, gcc-9
 
 // <memory>
 
