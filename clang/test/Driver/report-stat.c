979bcbd3a6f7ea784f2098ad4cf613fbd6b09e38
Zarko Todorovski <zarko@ca.ibm.com>
2020-11-27 15:54:37 -0500

    Add -fintegrated-as to clang invocation
    On platforms where the integrated as isn't called by default this
    test fails since the output is not what it expects.  Adding this
    option generates the expected output on those platforms as well.

--- clang/test/Driver/report-stat.c
+++ clang/test/Driver/report-stat.c
@@ -1,4 +1,4 @@
-// RUN: %clang -c -fproc-stat-report %s | FileCheck %s
+// RUN: %clang -c -fproc-stat-report -fintegrated-as %s | FileCheck %s
 // CHECK: clang{{.*}}: output={{.*}}.o, total={{[0-9.]+}} ms, user={{[0-9.]+}} ms, mem={{[0-9]+}} Kb
 
 // RUN: %clang -c -fproc-stat-report=%t %s
