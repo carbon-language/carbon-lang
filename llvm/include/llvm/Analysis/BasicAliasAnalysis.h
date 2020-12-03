17b195b632a780adf637432beda63c91eea2c106
Philip Reames <listmail@philipreames.com>
2020-12-03 13:08:20 -0800

    [BasicAA] Minor formatting improvements for printers

--- llvm/include/llvm/Analysis/BasicAliasAnalysis.h
+++ llvm/include/llvm/Analysis/BasicAliasAnalysis.h
@@ -132,7 +132,7 @@ private:
       OS << "(V=" << V->getName()
 	 << ", zextbits=" << ZExtBits
 	 << ", sextbits=" << SExtBits
-	 << ", scale=" << Scale << ")";
+	 << ", scale=" << Scale << ")\n";
     }
   };
 
@@ -152,14 +152,14 @@ private:
     void print(raw_ostream &OS) const {
       OS << "(DecomposedGEP Base=" << Base->getName()
 	 << ", Offset=" << Offset
-	 << ", VarIndices=[" << Offset;
+	 << ", VarIndices=[";
       for (size_t i = 0; i < VarIndices.size(); i++) {
        if (i != 0)
          OS << ", ";
        VarIndices[i].print(OS);
       }
       OS << "], HasCompileTimeConstantScale=" << HasCompileTimeConstantScale
-	 << ")";
+	 << ")\n";
     }
   };
 
