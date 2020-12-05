0003600aafd422ffb9dd86383d4828990476d45c
Nikita Popov <nikita.ppv@gmail.com>
2020-12-05 11:35:58 +0100

    [AA] Initialize Depth member
    Fix mistake introduced in f8afba5f7a25a69c12191d979d78d40fa6e5b684:
    I failed to initialize the Depth member to zero.

--- llvm/include/llvm/Analysis/AliasAnalysis.h
+++ llvm/include/llvm/Analysis/AliasAnalysis.h
@@ -815,7 +815,7 @@ private:
   std::vector<AnalysisKey *> AADeps;
 
   /// Query depth used to distinguish recursive queries.
-  unsigned Depth;
+  unsigned Depth = 0;
 
   friend class BatchAAResults;
 };
