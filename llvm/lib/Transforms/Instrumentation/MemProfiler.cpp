204d0d51b3cec91696f8b0b03f204cd0cf8d5ed4
Fangrui Song <i@maskray.me>
2020-12-05 21:36:31 -0800

    [MemProf] Make __memprof_shadow_memory_dynamic_address dso_local in static relocation model
    The x86-64 backend currently has a bug which uses a wrong register when for the GOTPCREL reference.
    The program will crash without the dso_local specifier.

--- llvm/lib/Transforms/Instrumentation/MemProfiler.cpp
+++ llvm/lib/Transforms/Instrumentation/MemProfiler.cpp
@@ -576,6 +576,8 @@ bool MemProfiler::insertDynamicShadowAtFunctionEntry(Function &F) {
   IRBuilder<> IRB(&F.front().front());
   Value *GlobalDynamicAddress = F.getParent()->getOrInsertGlobal(
       MemProfShadowMemoryDynamicAddress, IntptrTy);
+  if (F.getParent()->getPICLevel() == PICLevel::NotPIC)
+    dyn_cast<GlobalVariable>(GlobalDynamicAddress)->setDSOLocal(true);
   DynamicShadowOffset = IRB.CreateLoad(IntptrTy, GlobalDynamicAddress);
   return true;
 }
