7d04e70627aa0734ce820bec0dcb7fa28a9ea457
Oliver Stannard <oliver.stannard@linaro.org>
2020-12-07 09:04:26 +0000

    [Lit] Fix flaky test on heavily loaded bots
    On some of the slow or heavily-loaded bots, this test was failing
    intermittently because the infinite_loop.py script might not emit
    anything to stdout before the 1 second timeout, so the "Command Output"
    line isn't present in the output. That output isn't really important to
    this test, we just care that the process is killed, so we can just rmove
    that check line from  the test.
    
    Differential revision: https://reviews.llvm.org/D92563

--- llvm/utils/lit/tests/shtest-timeout.py
+++ llvm/utils/lit/tests/shtest-timeout.py
@@ -42,7 +42,6 @@
 
 # CHECK-OUT-COMMON: TIMEOUT: per_test_timeout :: infinite_loop.py
 # CHECK-OUT-COMMON: Timeout: Reached timeout of 1 seconds
-# CHECK-OUT-COMMON: Command {{([0-9]+ )?}}Output
 # CHECK-OUT-COMMON: Timed Out: 1
 
 
