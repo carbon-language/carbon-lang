c59ccc022265a6a3d70c86c4cf6391ebd70e6497
Jonas Devlieghere <jonas@devlieghere.com>
2020-12-08 19:30:10 -0800

    [lldb] Fix -Wformat warning in debugserver unit test
    RNBSocketTest.cpp:31:35: warning: format specifies type 'char *' but the
    argument has type 'const void *' [-Wformat]

--- lldb/unittests/debugserver/RNBSocketTest.cpp
+++ lldb/unittests/debugserver/RNBSocketTest.cpp
@@ -28,7 +28,7 @@ static void ServerCallbackv4(const void *baton, in_port_t port) {
   auto child_pid = fork();
   if (child_pid == 0) {
     char addr_buffer[256];
-    sprintf(addr_buffer, "%s:%d", baton, port);
+    sprintf(addr_buffer, "%s:%d", (const char *)baton, port);
     llvm::Expected<std::unique_ptr<Socket>> socket_or_err =
         Socket::TcpConnect(addr_buffer, false);
     ASSERT_THAT_EXPECTED(socket_or_err, llvm::Succeeded());
