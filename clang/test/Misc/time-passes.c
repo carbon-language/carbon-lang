754d1d3d52b7145acc9fd440e8ecc0c10d9e966d
Fangrui Song <i@maskray.me>
2020-12-09 17:17:28 -0800

    [test] Fix Misc/time-passes.c

--- clang/test/Misc/time-passes.c
+++ clang/test/Misc/time-passes.c
@@ -1,22 +1,22 @@
 // Check that legacy pass manager could only use -ftime-report
-// RUN: %clang_cc1 -fno-experimental-new-pass-manager -emit-obj -O1 \
+// RUN: %clang_cc1 -flegacy-pass-manager -emit-obj -O1 \
 // RUN:     -ftime-report %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=TIME,LPM
-// RUN: not %clang_cc1 -fno-experimental-new-pass-manager -emit-obj -O1 \
+// RUN: not %clang_cc1 -flegacy-pass-manager -emit-obj -O1 \
 // RUN:     -ftime-report=per-pass %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=ERROR
-// RUN: not %clang_cc1 -fno-experimental-new-pass-manager -emit-obj -O1 \
+// RUN: not %clang_cc1 -flegacy-pass-manager -emit-obj -O1 \
 // RUN:     -ftime-report=per-pass-run %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=ERROR
 
 // Check -ftime-report/-ftime-report= output for the new pass manager
-// RUN: %clang_cc1 -emit-obj -O1 -fexperimental-new-pass-manager \
+// RUN: %clang_cc1 -emit-obj -O1 -fno-legacy-pass-manager \
 // RUN:     -ftime-report %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=TIME,NPM
-// RUN: %clang_cc1 -emit-obj -O1 -fexperimental-new-pass-manager \
+// RUN: %clang_cc1 -emit-obj -O1 -fno-legacy-pass-manager \
 // RUN:     -ftime-report=per-pass %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=TIME,NPM
-// RUN: %clang_cc1 -emit-obj -O1 -fexperimental-new-pass-manager \
+// RUN: %clang_cc1 -emit-obj -O1 -fno-legacy-pass-manager \
 // RUN:     -ftime-report=per-pass-run %s -o /dev/null 2>&1 | \
 // RUN:     FileCheck %s --check-prefixes=TIME,NPM-PER-INVOKE
 
@@ -36,6 +36,6 @@
 // LPM-NOT: Pass execution timing report
 // NPM: Pass execution timing report
 
-// ERROR: error: invalid argument '-ftime-report={{.*}}' only allowed with '-fexperimental-new-pass-manager'
+// ERROR: error: invalid argument '-ftime-report={{.*}}' only allowed with '-fno-legacy-pass-manager'
 
 int foo(int x, int y) { return x + y; }
