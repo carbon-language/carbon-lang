1d3f1eb855fcfb03b6ddb92543c02694115b5739
Med Ismail Bennani <medismail.bennani@gmail.com>
2020-12-15 06:11:35 +0100

    Revert "[lldb/API] Expose Target::CreateBreakpoint(..., move_to_nearest_code) overload"
    This reverts commit 04696ff002e7d311887b7b7e6e171340a0623dd9.
    
    Exposing the LazyBool private type in SBTarget.h breaks some tests.

--- lldb/test/API/functionalities/breakpoint/breakpoint_by_line_and_column/TestBreakpointByLineAndColumn.py
+++ lldb/test/API/functionalities/breakpoint/breakpoint_by_line_and_column/TestBreakpointByLineAndColumn.py
@@ -42,30 +42,3 @@ class BreakpointByLineAndColumnTestCase(TestBase):
             self.assertEqual(b_loc.GetLine(), 11)
             in_condition |= b_loc.GetColumn() < 30
         self.assertTrue(in_condition)
-
-    ## Skip gcc version less 7.1 since it doesn't support -gcolumn-info
-    @skipIf(compiler="gcc", compiler_version=['<', '7.1'])
-    def testBreakpointByLineAndColumnNearestCode(self):
-        self.build()
-        exe = self.getBuildArtifact("a.out")
-
-        main_c = lldb.SBFileSpec("main.c")
-        line = line_number("main.c", "// Line 20.")
-        column = len("// Line 20") # should stop at the period.
-        indent = 2
-        module_list = lldb.SBFileSpecList()
-
-        # Create a target from the debugger.
-        target = self.dbg.CreateTarget(exe)
-        self.assertTrue(target, VALID_TARGET)
-
-        valid_bpt = target.BreakpointCreateByLocation(main_c, line, column,
-                                                      indent, module_list, True)
-        self.assertTrue(valid_bpt, VALID_BREAKPOINT)
-        self.assertEqual(valid_bpt.GetNumLocations(), 1)
-
-        invalid_bpt = target.BreakpointCreateByLocation(main_c, line, column,
-                                                      indent, module_list, False)
-        self.assertTrue(invalid_bpt, VALID_BREAKPOINT)
-        self.assertEqual(invalid_bpt.GetNumLocations(), 0)
-
