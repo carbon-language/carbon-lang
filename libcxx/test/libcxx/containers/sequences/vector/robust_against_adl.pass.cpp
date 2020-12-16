781c476ce09ed983477885e33b8acbb2220ad3a1
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2020-12-15 19:32:29 -0500

    [libc++] ADL-proof vector<bool> by adding _VSTD:: qualification on calls.
    This affects only vectors with weird/malicious allocators,
    the same corner case covered in D91708, but for `vector<bool>` this time.
    
    Also ADL-proof <__tree>, which affects only sets and maps with weird/malicious
    allocators where the ADL trap is in the *fancy pointer type*.
    
    Also drive-by _VSTD:: qualification in the guts of std::bind,
    std::packaged_task, std::condition_variable.
    
    Differential Revision: https://reviews.llvm.org/D93424

--- libcxx/test/libcxx/containers/sequences/vector/robust_against_adl.pass.cpp
+++ libcxx/test/libcxx/containers/sequences/vector/robust_against_adl.pass.cpp
@@ -20,14 +20,19 @@ template<class T> struct Holder { T t; };
 template<class T, class AdlTrap = Holder<Incomplete>>
 struct MyAlloc {
     using value_type = T;
+    MyAlloc() = default;
+    template<class U> MyAlloc(const MyAlloc<U>&) {}
     T *allocate(int n) { return std::allocator<T>().allocate(n); }
     void deallocate(T *p, int n) { return std::allocator<T>().deallocate(p, n); }
 };
 
 int main(int, char**)
 {
+    std::vector<bool, MyAlloc<bool>> vb;
+    std::vector<bool, MyAlloc<bool>> wb(100);
+
     std::vector<int, MyAlloc<int>> v;
-    std::vector<int, MyAlloc<int>> w;
+    std::vector<int, MyAlloc<int>> w(100);
     v.push_back(1);
     v.insert(v.end(), 2);
     v.insert(v.end(), w.begin(), w.end());
