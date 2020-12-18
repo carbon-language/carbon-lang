b4c63ef6dd90dba9af26a111c9a78b121c5284b1
Richard Smith <richard@metafoo.co.uk>
2020-12-18 13:10:05 -0800

    [c++20] Mark class type NTTPs as done and start defining the feature test macro.

--- clang/test/Lexer/cxx-features.cpp
+++ clang/test/Lexer/cxx-features.cpp
@@ -155,7 +155,7 @@
 #error "wrong value for __cpp_structured_bindings"
 #endif
 
-#if check(nontype_template_args, 0, 0, 0, 201411, 201411)
+#if check(nontype_template_args, 0, 0, 0, 201411, 201911)
 #error "wrong value for __cpp_nontype_template_args"
 #endif
 
