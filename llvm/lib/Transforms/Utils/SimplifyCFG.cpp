6a1617d67ccdf359d4250267be4431530308f351
Roman Lebedev <lebedev.ri@gmail.com>
2020-12-19 20:07:26 +0300

    [SimplifyCFG] Teach SimplifyCondBranchToTwoReturns() to preserve DomTree, part 2
    ... for the custom case returning void.

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -2631,6 +2631,10 @@ bool SimplifyCFGOpt::SimplifyCondBranchToTwoReturns(BranchInst *BI,
     FalseSucc->removePredecessor(BB);
     Builder.CreateRetVoid();
     EraseTerminatorAndDCECond(BI);
+    if (DTU) {
+      DTU->applyUpdatesPermissive({{DominatorTree::Delete, BB, TrueSucc},
+                                   {DominatorTree::Delete, BB, FalseSucc}});
+    }
     return true;
   }
 
