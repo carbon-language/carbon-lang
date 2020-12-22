8c85aae6c5b282eee1f58e67334e809016f04776
Fangrui Song <i@maskray.me>
2020-12-21 17:18:28 -0800

    [MC][test] Reorganize .cfi_* tests
    Delete tests which are covered by others.

--- llvm/test/MC/ELF/cfi-scope-errors2.s
+++ llvm/test/MC/ELF/cfi-scope-errors2.s
@@ -4,15 +4,10 @@
 # RUN: not llvm-mc %s -filetype=obj -triple=x86_64-unknown-linux -o /dev/null 2>&1 | FileCheck %s
 
 .text
-.globl proc_one
-proc_one:
  .cfi_startproc
- 
+
 .text
-.globl proc_two
-proc_two:
  .cfi_startproc
 # CHECK: [[#@LINE]]:1: error: starting new .cfi frame before finishing the previous one
 
  .cfi_endproc
-
