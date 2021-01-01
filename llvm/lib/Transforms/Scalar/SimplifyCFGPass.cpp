e08fea3b240994572a4dd6b34bd846aef023a123
Roman Lebedev <lebedev.ri@gmail.com>
2021-01-01 16:31:11 +0300

    [SimplifyCFGPass] Ensure that DominatorTreeWrapperPass is init'd before SimplifyCFG
    It's probably better than hoping that it will happen to be
    already initialized.

--- llvm/lib/Transforms/Scalar/SimplifyCFGPass.cpp
+++ llvm/lib/Transforms/Scalar/SimplifyCFGPass.cpp
@@ -357,6 +357,7 @@ INITIALIZE_PASS_BEGIN(CFGSimplifyPass, "simplifycfg", "Simplify the CFG", false,
                       false)
 INITIALIZE_PASS_DEPENDENCY(TargetTransformInfoWrapperPass)
 INITIALIZE_PASS_DEPENDENCY(AssumptionCacheTracker)
+INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
 INITIALIZE_PASS_END(CFGSimplifyPass, "simplifycfg", "Simplify the CFG", false,
                     false)
 
