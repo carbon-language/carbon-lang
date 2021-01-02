993d8ac5cb935b78fb136c25a7e4bae18852f429
Siva Chandra Reddy <sivachandra@google.com>
2021-01-01 21:19:31 -0800

    [libc] Add implementations of nearbyint[f|l].
    The implementation is exactly the same as rint* as even rint does not
    raise any floating point exceptions currently. [Note that the standards
    do not specify that floating point exceptions must be raised - they
    leave it up to the implementation to choose to raise FE_INEXACT when
    rounding non-integral values.]
    
    Reviewed By: lntue
    
    Differential Revision: https://reviews.llvm.org/D94112

---
+++ libc/src/math/nearbyintf.cpp
@@ -0,0 +1,18 @@
+//===-- Implementation of nearbyintf function -----------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/__support/common.h"
+#include "utils/FPUtil/NearestIntegerOperations.h"
+
+namespace __llvm_libc {
+
+float LLVM_LIBC_ENTRYPOINT(nearbyintf)(float x) {
+  return fputil::roundUsingCurrentRoundingMode(x);
+}
+
+} // namespace __llvm_libc
