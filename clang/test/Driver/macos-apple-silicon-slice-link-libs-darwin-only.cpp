305ac81e1d4bbd037587190175e4d0b5631300e1
Thomas Preud'homme <thomasp@graphcore.ai>
2021-01-06 23:22:12 +0000

    Fix macos target assumption in test
    Clang test Driver/macos-apple-silicon-slice-link-libs-darwin-only.cpp
    assumes the target is darwin when the host is darwin which is not
    necessarily the case, causing the test to fail when it is not. This
    commit adds a -triple argument to the clang invocation to ensure the
    target is darwin.
    
    Reviewed By: hans
    
    Differential Revision: https://reviews.llvm.org/D94396

--- clang/test/Driver/macos-apple-silicon-slice-link-libs-darwin-only.cpp
+++ clang/test/Driver/macos-apple-silicon-slice-link-libs-darwin-only.cpp
@@ -1,5 +1,5 @@
-// RUN: %clang -### -arch arm64 -mmacosx-version-min=10.7 %s 2>&1 | FileCheck -check-prefix=ARM64-10_7 %s
-// RUN: %clang -### -arch x86_64 -mmacosx-version-min=10.7 %s 2>&1 | FileCheck -check-prefix=x86_64-10_7 %s
+// RUN: %clang -### -target arm64-apple-darwin -arch arm64 -mmacosx-version-min=10.7 %s 2>&1 | FileCheck -check-prefix=ARM64-10_7 %s
+// RUN: %clang -### -target x86_64-apple-darwin10 -arch x86_64 -mmacosx-version-min=10.7 %s 2>&1 | FileCheck -check-prefix=x86_64-10_7 %s
 // REQUIRES: system-darwin
 
 // ARM64-10_7-NOT: -lcrt1.10.6.o
