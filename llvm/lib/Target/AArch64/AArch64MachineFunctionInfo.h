42635856ed3c9a05957640f9deb50cf865c03825
Bradley Smith <bradley.smith@arm.com>
2021-01-07 17:31:07 +0000

    [AArch64][SVE] Allow accesses to SVE stack objects to use frame pointer
    The layout of the stack frame for SVE means that using the frame pointer
    rather than the stack pointer for an access to an SVE stack object
    removes the need for an additional add to jump over the non-SVE objects.
    
    Likewise the opposite is true for non-SVE stack objects.
    
    This patch allows for the former to be done by having HasFP return true
    in the presence of both SVE and non-SVE stack objects, and also fixes a
    minor issue whereby the later would not be done for certain offsets.

--- llvm/lib/Target/AArch64/AArch64MachineFunctionInfo.h
+++ llvm/lib/Target/AArch64/AArch64MachineFunctionInfo.h
@@ -248,6 +248,10 @@ public:
     return getCalleeSavedStackSize();
   }
 
+  bool isCalleeSavedStackSizeComputed() const {
+    return HasCalleeSavedStackSize;
+  }
+
   unsigned getCalleeSavedStackSize() const {
     assert(HasCalleeSavedStackSize &&
            "CalleeSavedStackSize has not been calculated");
