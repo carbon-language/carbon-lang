aefeb5f136e7b72b251ce2881cb39a1fe8f0d76a
Siva Chandra Reddy <sivachandra@google.com>
2021-01-11 12:35:34 -0800

    [libc][NFC] Make __support/common.h an in tree header.
    It was previously a generated header. It can easily converted to a
    generated header if required in future.
    
    Reviewed By: michaelrj
    
    Differential Revision: https://reviews.llvm.org/D94445

--- libc/src/__support/common.h
+++ libc/src/__support/common.h
@@ -11,22 +11,22 @@
 
 #define LIBC_INLINE_ASM __asm__ __volatile__
 
-#define likely(x) __builtin_expect (!!(x), 1)
-#define unlikely(x) __builtin_expect (x, 0)
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(x, 0)
 #define UNUSED __attribute__((unused))
 
 #ifndef LLVM_LIBC_FUNCTION_ATTR
-  #define LLVM_LIBC_FUNCTION_ATTR
+#define LLVM_LIBC_FUNCTION_ATTR
 #endif
 
 #ifdef LLVM_LIBC_PUBLIC_PACKAGING
-#define LLVM_LIBC_FUNCTION(type, name, arglist) \
-  LLVM_LIBC_FUNCTION_ATTR decltype(__llvm_libc::name) __##name##_impl__ __asm__(#name); \
-  decltype(__llvm_libc::name) name [[gnu::alias(#name)]]; \
-  type __##name##_impl__ arglist                                        
+#define LLVM_LIBC_FUNCTION(type, name, arglist)                                \
+  LLVM_LIBC_FUNCTION_ATTR decltype(__llvm_libc::name)                          \
+      __##name##_impl__ __asm__(#name);                                        \
+  decltype(__llvm_libc::name) name [[gnu::alias(#name)]];                      \
+  type __##name##_impl__ arglist
 #else
-#define LLVM_LIBC_FUNCTION(type, name, arglist)\
-  type name arglist
+#define LLVM_LIBC_FUNCTION(type, name, arglist) type name arglist
 #endif
 
 #endif // LLVM_LIBC_SUPPORT_COMMON_H
