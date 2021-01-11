b8d28420885a42d16a57e02c28129d0eb92474a1
Fangrui Song <i@maskray.me>
2021-01-10 22:22:07 -0800

    CGDebugInfo: Delete unneeded UnwrapTypeForDebugInfo
    Tested with stage 2 -DCMAKE_BUILD_TYPE=Debug clang, byte identical.

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -3132,10 +3132,6 @@ static QualType UnwrapTypeForDebugInfo(QualType T, const ASTContext &C) {
 }
 
 llvm::DIType *CGDebugInfo::getTypeOrNull(QualType Ty) {
-
-  // Unwrap the type as needed for debug information.
-  Ty = UnwrapTypeForDebugInfo(Ty, CGM.getContext());
-
   auto It = TypeCache.find(Ty.getAsOpaquePtr());
   if (It != TypeCache.end()) {
     // Verify that the debug info still exists.
