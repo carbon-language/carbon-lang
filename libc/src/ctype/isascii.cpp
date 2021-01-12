04edcc02638bc70772baa50a74e582bb8e029872
Michael Jones <michaelrj@google.com>
2021-01-12 22:37:56 +0000

    [libc] add isascii and toascii implementations
    adding both at once since these are trivial functions.
    
    Reviewed By: sivachandra
    
    Differential Revision: https://reviews.llvm.org/D94558

---
+++ libc/src/ctype/isascii.cpp
@@ -0,0 +1,17 @@
+//===-- Implementation of isascii------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#include "src/ctype/isascii.h"
+
+#include "src/__support/common.h"
+
+namespace __llvm_libc {
+
+LLVM_LIBC_FUNCTION(int, isascii, (int c)) { return (c & (~0x7f)) == 0; }
+
+} // namespace __llvm_libc
