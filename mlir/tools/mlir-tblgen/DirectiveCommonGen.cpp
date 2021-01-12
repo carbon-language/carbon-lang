0bd9a1369112f7d0a8a3b94a050fd5ed37037e9b
Valentin Clement <clementval@gmail.com>
2021-01-12 09:42:25 -0500

    [mlir][openacc] Use TableGen information for default enum
    Use TableGen and information in ACC.td for the Default enum in the OpenACC dialect.
    This patch generalize what was done for OpenMP for directives.
    
    Follow up patch after D93576
    
    Reviewed By: kiranchandramohan
    
    Differential Revision: https://reviews.llvm.org/D93710

--- mlir/tools/mlir-tblgen/DirectiveCommonGen.cpp
+++ mlir/tools/mlir-tblgen/DirectiveCommonGen.cpp
@@ -1,4 +1,4 @@
-//===========- OpenMPCommonGen.cpp - OpenMP common info generator -===========//
+//===========- DirectiveCommonGen.cpp - Directive common info generator -=====//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -25,21 +25,26 @@ using llvm::RecordKeeper;
 using llvm::Twine;
 
 // LLVM has multiple places (Clang, Flang, MLIR) where information about
-// the OpenMP directives, and clauses are needed. It is good software
+// the directives (OpenMP/OpenACC), and clauses are needed. It is good software
 // engineering to keep the common information in a single place to avoid
 // duplication, reduce engineering effort and prevent mistakes.
-// Currently that common place is llvm/include/llvm/Frontend/OpenMP/OMP.td.
+// Currently that common place is llvm/include/llvm/Frontend/OpenMP/OMP.td for
+// OpenMP and llvm/include/llvm/Frontend/OpenACC/ACC.td for OpenACC.
 // We plan to use this tablegen source to generate all the required
 // declarations, functions etc.
 //
-// Some OpenMP clauses accept only a fixed set of values as inputs. These
-// can be represented as a String Enum Attribute (StrEnumAttr) in MLIR ODS.
-// The emitDecls function below currently generates these enumerations. The
+// Some OpenMP/OpenACC clauses accept only a fixed set of values as inputs.
+// These can be represented as a String Enum Attribute (StrEnumAttr) in MLIR
+// ODS. The emitDecls function below currently generates these enumerations. The
 // name of the enumeration is specified in the enumClauseValue field of
 // Clause record in OMP.td. This name can be used to specify the type of the
 // OpenMP operation's operand. The allowedClauseValues field provides the list
 // of ClauseValues which are part of the enumeration.
 static bool emitDecls(const RecordKeeper &recordKeeper, raw_ostream &os) {
+  const auto &directiveLanguages =
+      recordKeeper.getAllDerivedDefinitions("DirectiveLanguage");
+  assert(directiveLanguages.size() != 0 && "DirectiveLanguage missing.");
+
   const auto &clauses = recordKeeper.getAllDerivedDefinitions("Clause");
 
   for (const auto &r : clauses) {
@@ -73,16 +78,17 @@ static bool emitDecls(const RecordKeeper &recordKeeper, raw_ostream &os) {
         os << ",";
     }
     os << "]> {\n";
-    os << "    let cppNamespace = \"::mlir::omp\";\n";
+    os << "    let cppNamespace = \"::mlir::"
+       << directiveLanguages[0]->getValueAsString("cppNamespace") << "\";\n";
     os << "}\n";
   }
   return false;
 }
 
 // Registers the generator to mlir-tblgen.
-static mlir::GenRegistration
-    genDirectiveDecls("gen-directive-decl",
-                      "Generate declarations for directives (OpenMP etc.)",
-                      [](const RecordKeeper &records, raw_ostream &os) {
-                        return emitDecls(records, os);
-                      });
+static mlir::GenRegistration genDirectiveDecls(
+    "gen-directive-decl",
+    "Generate declarations for directives (OpenMP/OpenACC etc.)",
+    [](const RecordKeeper &records, raw_ostream &os) {
+      return emitDecls(records, os);
+    });
