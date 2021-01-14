8b09cf7956d8abc722fa736874e4cea667a9d3cb
Utkarsh Saxena <usx@google.com>
2021-01-14 18:38:42 +0100

    [clangd] Trivial: Documentation fix in ASTSignals.

--- clang-tools-extra/clangd/ASTSignals.cpp
+++ clang-tools-extra/clangd/ASTSignals.cpp
@@ -1,4 +1,4 @@
-//===--- ASTSignals.cpp - LSP server -----------------------------*- C++-*-===//
+//===--- ASTSignals.cpp ------------------------------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- clang-tools-extra/clangd/ASTSignals.h
+++ clang-tools-extra/clangd/ASTSignals.h
@@ -1,4 +1,4 @@
-//===--- ASTSignals.h - LSP server -------------------------------*- C++-*-===//
+//===--- ASTSignals.h --------------------------------------------*- C++-*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
