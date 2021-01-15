ead71a23edde5f8a1acfdd75bd01f1fa6e0c4014
Sean Fertile <sd.fertile@gmail.com>
2021-01-15 16:36:50 -0500

    [PowerPC][AIX]Do not emit xxspltd mnemonic on AIX.
    A bug in the system assembler can assemble the xxspltd extended
    menemonic into the wrong instruction (extracting the wrong element).
    Emit the full xxpermdi with all operands to work around the problem.
    
    Differential Revision: https://reviews.llvm.org/D94419

--- llvm/test/MC/PowerPC/modern-aix-as.s
+++ llvm/test/MC/PowerPC/modern-aix-as.s
@@ -12,5 +12,14 @@
             mtudscr 2
 # CHECK-BE: mfudscr 2                       # encoding: [0x7c,0x43,0x02,0xa6]
 # CHECK-LE: mfudscr 2                       # encoding: [0xa6,0x02,0x43,0x7c]
+# CHECK-OLD: instruction use requires an option to be enabled
             mfudscr 2
+# CHECK-BE: xxspltd 2, 3, 0                 # encoding: [0xf0,0x43,0x18,0x50]
+# CHECK-LE: xxspltd 2, 3, 0                 # encoding: [0x50,0x18,0x43,0xf0]
+# CHECK-OLD: instruction use requires an option to be enabled
+            xxspltd 2, 3, 0
 
+# CHECK-BE: xxspltd 2, 3, 1                 # encoding: [0xf0,0x43,0x1b,0x50]
+# CHECK-LE: xxspltd 2, 3, 1                 # encoding: [0x50,0x1b,0x43,0xf0]
+# CHECK-OLD: instruction use requires an option to be enabled
+            xxspltd 2, 3, 1
