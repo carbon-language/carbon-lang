6ef95056b9dce1aa64d975b70f059673484bed87
Paul Robinson <paul.robinson@sony.com>
2021-01-15 08:29:35 -0800

    [RGT][ADT] Remove test assertion that will not be executed
    Found by the Rotten Green Tests project.
    
    Differential Revision: https://reviews.llvm.org/D95255

--- llvm/unittests/ADT/ImmutableSetTest.cpp
+++ llvm/unittests/ADT/ImmutableSetTest.cpp
@@ -180,7 +180,6 @@ TEST_F(ImmutableSetTest, IterLongSetTest) {
 
   int i = 0;
   for (ImmutableSet<long>::iterator I = S.begin(), E = S.end(); I != E; ++I) {
-    ASSERT_EQ(i, *I);
     i++;
   }
   ASSERT_EQ(0, i);
