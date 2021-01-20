8dd58a509cc8b93a211c9b07b12e1548dc187fc3
Nicolas Vasilache <nicolas.vasilache@gmail.com>
2021-01-20 20:29:34 +0000

    [mlir][Linalg] NFC - Fully compose map and operands when creating AffineMin in tiling.
    This may simplify the composition of patterns but is otherwise NFC.

--- mlir/lib/Dialect/Linalg/Transforms/Tiling.cpp
+++ mlir/lib/Dialect/Linalg/Transforms/Tiling.cpp
@@ -34,7 +34,6 @@ using namespace mlir::edsc::intrinsics;
 using namespace mlir::linalg;
 using namespace mlir::scf;
 
-using folded_affine_min = FoldedValueBuilder<AffineMinOp>;
 
 #define DEBUG_TYPE "linalg-tiling"
 
@@ -292,8 +291,9 @@ makeTiledShapes(OpBuilder &b, Location loc, LinalgOp linalgOp,
                  getAffineDimExpr(/*position=*/2, b.getContext())},
             b.getContext());
         auto d = std_dim(shapedOp, r);
-        size =
-            affine_min(b.getIndexType(), minMap, ValueRange{size, d, offset});
+        SmallVector<Value, 4> operands{size, d, offset};
+        fullyComposeAffineMapAndOperands(&minMap, &operands);
+        size = affine_min(b.getIndexType(), minMap, operands);
       }
 
       sizes.push_back(size);
