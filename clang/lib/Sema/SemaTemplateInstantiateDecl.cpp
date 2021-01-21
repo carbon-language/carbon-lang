d462aa5a619ab9fdf8b024e48c19bc8820fe8781
Adam Czachorowski <adamcz@google.com>
2021-01-21 17:51:13 +0100

    [clang] Fix a nullptr dereference bug on invalid code
    When working with invalid code, we would try to dereference a nullptr
    while deducing template arguments in some dependend code operating on a
    lambda with invalid return type.
    
    Differential Revision: https://reviews.llvm.org/D95145

--- clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
+++ clang/lib/Sema/SemaTemplateInstantiateDecl.cpp
@@ -4189,6 +4189,9 @@ TemplateDeclInstantiator::SubstFunctionType(FunctionDecl *D,
       for (unsigned OldIdx = 0, NumOldParams = OldProtoLoc.getNumParams();
            OldIdx != NumOldParams; ++OldIdx) {
         ParmVarDecl *OldParam = OldProtoLoc.getParam(OldIdx);
+        if (!OldParam)
+          return nullptr;
+
         LocalInstantiationScope *Scope = SemaRef.CurrentInstantiationScope;
 
         Optional<unsigned> NumArgumentsInExpansion;
