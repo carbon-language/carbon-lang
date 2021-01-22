3a50ed84f4823fb0e7b385cc22fef12435dfd376
Jonas Devlieghere <jonas@devlieghere.com>
2021-01-22 15:04:11 -0800

    [lldb] FixFileSystem::GetExternalPath for VFS API change

--- lldb/source/Host/common/FileSystem.cpp
+++ lldb/source/Host/common/FileSystem.cpp
@@ -479,7 +479,7 @@ ErrorOr<std::string> FileSystem::GetExternalPath(const llvm::Twine &path) {
 
   // If VFS mapped we know the underlying FS is a RedirectingFileSystem.
   ErrorOr<vfs::RedirectingFileSystem::Entry *> E =
-      static_cast<vfs::RedirectingFileSystem &>(*m_fs).lookupPath(path);
+      static_cast<vfs::RedirectingFileSystem &>(*m_fs).lookupPath(path.str());
   if (!E) {
     if (E.getError() == llvm::errc::no_such_file_or_directory) {
       return path.str();
