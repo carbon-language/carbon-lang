6fe193bf271521c5dd1a50949e83b246a9820ce2
Fangrui Song <i@maskray.me>
2021-01-22 19:07:02 -0800

    [test] Add -mtriple

--- lld/test/ELF/exclude-libs-versym.s
+++ lld/test/ELF/exclude-libs-versym.s
@@ -2,8 +2,8 @@
 ## --exclude-libs can hide version symbols.
 
 # RUN: rm -rf %t && split-file %s %t
-# RUN: llvm-mc -filetype=obj %t/a.s -o %t/a.o
-# RUN: llvm-mc -filetype=obj %t/b.s -o %t/b.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %t/a.s -o %t/a.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %t/b.s -o %t/b.o
 # RUN: llvm-ar rc %t/b.a %t/b.o
 # RUN: ld.lld -shared %t/a.o %t/b.a --version-script=%t/ver -o %t.so
 # RUN: llvm-readelf --dyn-syms %t.so | FileCheck %s
