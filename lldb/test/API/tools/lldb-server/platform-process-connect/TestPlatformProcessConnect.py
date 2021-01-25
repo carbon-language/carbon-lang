e9a3fac76cf199d902a746c2b2fc308dfe0ade26
Muhammad Omair Javaid <omair.javaid@linaro.org>
2021-01-25 20:29:22 +0500

    [LLDB] Skip TestPlatformProcessConnect on arm/aarch64 buildbot
    TestPlatformProcessConnect is randomly failing on LLDB Arm/AArch64
    buildbot. I am disabling it temporarily untill problem is fixed.

--- lldb/test/API/tools/lldb-server/platform-process-connect/TestPlatformProcessConnect.py
+++ lldb/test/API/tools/lldb-server/platform-process-connect/TestPlatformProcessConnect.py
@@ -12,6 +12,7 @@ class TestPlatformProcessConnect(gdbremote_testcase.GdbRemoteTestCaseBase):
     @expectedFailureAll(hostoslist=["windows"], triple='.*-android')
     @skipIfWindows # lldb-server does not terminate correctly
     @skipIfDarwin # lldb-server not found correctly
+    @skipIf(oslist=["linux"], archs=["arm", "aarch64"])  # Fails randomly
     def test_platform_process_connect(self):
         self.build()
 
