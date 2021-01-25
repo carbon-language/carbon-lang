4a64d8fe392449b205e59031aad5424968cf7446
Akira Hatanaka <ahatanaka@apple.com>
2021-01-25 11:56:33 -0800

    [ObjC][ARC] Use operand bundle 'clang.arc.rv' instead of explicitly emitting retainRV or claimRV calls in the IR
    This reapplies 3fe3946d9a958b7af6130241996d9cfcecf559d4 without the
    changes made to lib/IR/AutoUpgrade.cpp, which was violating layering.
    
    Original commit message:
    
    Background:
    
    This patch makes changes to the front-end and middle-end that are
    needed to fix a longstanding problem where llvm breaks ARC's autorelease
    optimization (see the link below) by separating calls from the marker
    instructions or retainRV/claimRV calls. The backend changes are in
    https://reviews.llvm.org/D92569.
    
    https://clang.llvm.org/docs/AutomaticReferenceCounting.html#arc-runtime-objc-autoreleasereturnvalue
    
    What this patch does to fix the problem:
    
    - The front-end adds operand bundle "clang.arc.rv" to calls, which
      indicates the call is implicitly followed by a marker instruction and
      an implicit retainRV/claimRV call that consumes the call result. In
      addition, it emits a call to @llvm.objc.clang.arc.noop.use, which
      consumes the call result, to prevent the middle-end passes from changing
      the return type of the called function. This is currently done only when
      the target is arm64 and the optimization level is higher than -O0.
    
    - ARC optimizer temporarily emits retainRV/claimRV calls after the calls
      with the operand bundle in the IR and removes the inserted calls after
      processing the function.
    
    - ARC contract pass emits retainRV/claimRV calls after the call with the
      operand bundle. It doesn't remove the operand bundle on the call since
      the backend needs it to emit the marker instruction. The retainRV and
      claimRV calls are emitted late in the pipeline to prevent optimization
      passes from transforming the IR in a way that makes it harder for the
      ARC middle-end passes to figure out the def-use relationship between
      the call and the retainRV/claimRV calls (which is the cause of
      PR31925).
    
    - The function inliner removes an autoreleaseRV call in the callee if
      nothing in the callee prevents it from being paired up with the
      retainRV/claimRV call in the caller. It then inserts a release call if
      the call is annotated with claimRV since autoreleaseRV+claimRV is
      equivalent to a release. If it cannot find an autoreleaseRV call, it
      tries to transfer the operand bundle to a function call in the callee.
      This is important since ARC optimizer can remove the autoreleaseRV
      returning the callee result, which makes it impossible to pair it up
      with the retainRV/claimRV call in the caller. If that fails, it simply
      emits a retain call in the IR if the implicit call is a call to
      retainRV and does nothing if it's a call to claimRV.
    
    Future work:
    
    - Use the operand bundle on x86-64.
    
    - Fix the auto upgrader to convert call+retainRV/claimRV pairs into
      calls annotated with the operand bundles.
    
    rdar://71443534
    
    Differential Revision: https://reviews.llvm.org/D92808

--- llvm/include/llvm/IR/LLVMContext.h
+++ llvm/include/llvm/IR/LLVMContext.h
@@ -93,6 +93,7 @@ public:
     OB_cfguardtarget = 3, // "cfguardtarget"
     OB_preallocated = 4,  // "preallocated"
     OB_gc_live = 5,       // "gc-live"
+    OB_clang_arc_rv = 6,  // "clang.arc.rv"
   };
 
   /// getMDKindID - Return a unique non-zero ID for the specified metadata kind.
