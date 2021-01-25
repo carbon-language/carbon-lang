17c3538aef656178b342573043eff328f5cf2673
Xun Li <lxfind@gmail.com>
2021-01-25 08:37:18 -0800

    Revert "Fix unused variable in CoroFrame.cpp when building Release with GCC 10"
    This reverts commit ff5e896425577f445ed080d88b582aab0896fba0.

--- llvm/lib/Transforms/Coroutines/CoroFrame.cpp
+++ llvm/lib/Transforms/Coroutines/CoroFrame.cpp
@@ -1549,7 +1549,6 @@ static void rewritePHIs(BasicBlock &BB) {
     for (BasicBlock *Pred : Preds) {
       if (CatchSwitchInst *CS =
               dyn_cast<CatchSwitchInst>(Pred->getTerminator())) {
-        (void)CS;
         // CleanupPad with a CatchSwitch predecessor: therefore this is an
         // unwind destination that needs to be handle specially.
         assert(CS->getUnwindDest() == &BB);
