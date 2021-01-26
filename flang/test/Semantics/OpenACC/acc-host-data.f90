d2abd62b9d123cde5a55a92450bbe665b237a72d
Valentin Clement <clementval@gmail.com>
2021-01-26 14:53:50 -0500

    [flang][openacc][NFC] Organize clause validity tests by directive
    Split the tests from acc-clause-validity.f90 in dedicated files by directives.
    The file acc-clause-validity.f90 was getting too big to be correctly maintained.
    Tests are identical.
    
    Reviewed By: SouraVX
    
    Differential Revision: https://reviews.llvm.org/D95328

---
+++ flang/test/Semantics/OpenACC/acc-host-data.f90
@@ -0,0 +1,36 @@
+! RUN: %S/../test_errors.sh %s %t %f18 -fopenacc
+
+! Check OpenACC clause validity for the following construct and directive:
+!   2.8 host_data
+
+program openacc_host_data_validity
+
+  implicit none
+
+  integer, parameter :: N = 256
+  real(8), dimension(N, N) :: aa, bb
+  logical :: ifCondition = .TRUE.
+
+  !ERROR: At least one of USE_DEVICE clause must appear on the HOST_DATA directive
+  !$acc host_data
+  !$acc end host_data
+
+  !$acc host_data use_device(aa)
+  !$acc end host_data
+
+  !$acc host_data use_device(aa) if(.true.)
+  !$acc end host_data
+
+  !$acc host_data use_device(aa) if(ifCondition)
+  !$acc end host_data
+
+  !$acc host_data use_device(aa, bb) if_present
+  !$acc end host_data
+
+  !$acc host_data use_device(aa, bb) if(.true.) if_present
+  !$acc end host_data
+
+  !$acc host_data use_device(aa, bb) if(.true.) if(ifCondition)
+  !$acc end host_data
+
+end program openacc_host_data_validity
