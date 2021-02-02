d8c373815d35df1b8544784ce172ade68fb01f8f
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2021-02-02 18:26:05 +0000

    [mlir][NFC] Add missing include guards to MlirOptMain.h
    Reviewed By: mehdi_amini
    
    Differential Revision: https://reviews.llvm.org/D95533

--- mlir/include/mlir/Support/MlirOptMain.h
+++ mlir/include/mlir/Support/MlirOptMain.h
@@ -10,6 +10,9 @@
 //
 //===----------------------------------------------------------------------===//
 
+#ifndef MLIR_SUPPORT_MLIROPTMAIN_H
+#define MLIR_SUPPORT_MLIROPTMAIN_H
+
 #include "mlir/Support/LogicalResult.h"
 #include "llvm/ADT/StringRef.h"
 
@@ -59,3 +62,5 @@ LogicalResult MlirOptMain(int argc, char **argv, llvm::StringRef toolName,
                           bool preloadDialectsInContext = true);
 
 } // end namespace mlir
+
+#endif // MLIR_SUPPORT_MLIROPTMAIN_H
