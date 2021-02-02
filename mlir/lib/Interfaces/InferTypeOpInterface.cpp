95935849881c403e6e141da16b41d64dc4e537d3
Vladislav Vinogradov <vlad.vinogradov@intel.com>
2021-02-02 18:27:26 +0000

    [mlir] Print more verbose message in case of type inference error
    Include the types into the error message.
    
    Reviewed By: ftynse
    
    Differential Revision: https://reviews.llvm.org/D95854

--- mlir/lib/Interfaces/InferTypeOpInterface.cpp
+++ mlir/lib/Interfaces/InferTypeOpInterface.cpp
@@ -55,7 +55,9 @@ LogicalResult mlir::detail::verifyInferredResultTypes(Operation *op) {
     return failure();
   if (!retTypeFn.isCompatibleReturnTypes(inferredReturnTypes,
                                          op->getResultTypes()))
-    return op->emitOpError(
-        "inferred type incompatible with return type of operation");
+    return op->emitOpError("inferred type(s) ")
+           << inferredReturnTypes
+           << " are incompatible with return type(s) of operation "
+           << op->getResultTypes();
   return success();
 }
