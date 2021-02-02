9a5dc01e4b65b15c73751f5b3d9273ea73533b5d
Melanie Blower <melanie.blower@intel.com>
2021-02-02 07:06:25 -0800

    [clang][PATCH][NFC] Correct test case related to review D95482

--- clang/test/SemaCXX/lambda-expressions.cpp
+++ clang/test/SemaCXX/lambda-expressions.cpp
@@ -1,8 +1,6 @@
 // RUN: %clang_cc1 -std=c++14 -Wno-unused-value -fsyntax-only -verify -verify=expected-cxx14 -fblocks %s
 // RUN: %clang_cc1 -std=c++17 -Wno-unused-value -fsyntax-only -verify -fblocks %s
-// RUN: %clang_cc1 -std=c++17 -fblocks -DSHOW_MS -Wno-unused-value -fms-compatibility -fdelayed-template-parsing -fsyntax-only -verify %s
 
-#ifndef SHOW_MS
 namespace std { class type_info; };
 
 namespace ExplicitCapture {
@@ -666,22 +664,3 @@ void Test() {
 
 }
 };
-
-#else
-template <typename RT, typename ET>
-void Decider(const RT &sp, ET &ep) {
-  [=](auto i) { ep[i] = sp[i + j]; };
-    // expected-error@-1 {{use of undeclared identifier 'j'}}
-}
-
-template <typename EMT> void LS() {
-  int *ep;
-  Decider(5, ep);
-}
-
-void runChapter4()
-{
-  LS<int>(); // expected-note {{in instantiation of}}
-}
-
-#endif
