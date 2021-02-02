50c523a9d4402c69d59c0b2ecb383a763d16cde9
Jeroen Dobbelaere <jeroen.dobbelaere@synopsys.com>
2021-02-02 17:55:06 +0100

    [InlineFunction] Only update noalias scopes once for an instruction.
    Inlining sometimes maps different instructions to be inlined onto the same instruction.
    
    We must ensure to only remap the noalias scopes once. Otherwise the scope might disappear (at best).
    This patch ensures that we only replace scopes for which the mapping is known.
    
    This approach is preferred over tracking which instructions we already handled in a SmallPtrSet,
    as that one will need more memory.
    
    Reviewed By: nikic
    
    Differential Revision: https://reviews.llvm.org/D95862

--- llvm/lib/Transforms/Utils/InlineFunction.cpp
+++ llvm/lib/Transforms/Utils/InlineFunction.cpp
@@ -921,14 +921,20 @@ void ScopedAliasMetadataDeepCloner::remap(ValueToValueMapTy &VMap) {
     if (!I)
       continue;
 
+    // Only update scopes when we find them in the map. If they are not, it is
+    // because we already handled that instruction before. This is faster than
+    // tracking which instructions we already updated.
     if (MDNode *M = I->getMetadata(LLVMContext::MD_alias_scope))
-      I->setMetadata(LLVMContext::MD_alias_scope, MDMap[M]);
+      if (MDNode *MNew = MDMap.lookup(M))
+        I->setMetadata(LLVMContext::MD_alias_scope, MNew);
 
     if (MDNode *M = I->getMetadata(LLVMContext::MD_noalias))
-      I->setMetadata(LLVMContext::MD_noalias, MDMap[M]);
+      if (MDNode *MNew = MDMap.lookup(M))
+        I->setMetadata(LLVMContext::MD_noalias, MNew);
 
     if (auto *Decl = dyn_cast<NoAliasScopeDeclInst>(I))
-      Decl->setScopeList(MDMap[Decl->getScopeList()]);
+      if (MDNode *MNew = MDMap.lookup(Decl->getScopeList()))
+        Decl->setScopeList(MNew);
   }
 }
 
