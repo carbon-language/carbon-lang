4454a9cd77e7481f6a2d75b4986fe7fbc364204d
Nathan James <n.james93@hotmail.co.uk>
2021-02-03 05:18:00 +0000

    [NFC] Fix typo in comment

--- clang-tools-extra/clang-tidy/readability/IdentifierNamingCheck.cpp
+++ clang-tools-extra/clang-tidy/readability/IdentifierNamingCheck.cpp
@@ -352,7 +352,7 @@ static bool isParamInMainLikeFunction(const ParmVarDecl &ParmDecl,
     return false;
   if (FDecl->getAccess() != AS_public && FDecl->getAccess() != AS_none)
     return false;
-  // If the function doesn't have a name thats an identifier, can occur of the
+  // If the function doesn't have a name that's an identifier, can occur if the
   // function is an operator overload, bail out early.
   if (!FDecl->getDeclName().isIdentifier())
     return false;
