b0f4ffbfaa61ad88c7d9e901b539790bfa59fe65
James Henderson <james.henderson@sony.com>
2021-02-05 08:36:31 +0000

    [llvm-objdump] Fix missing first line of license in header file

--- llvm/tools/llvm-objdump/llvm-objdump.h
+++ llvm/tools/llvm-objdump/llvm-objdump.h
@@ -1,3 +1,4 @@
+//===--- llvm-objdump.h -----------------------------------------*- C++ -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
