5b8db127a30f583c5437579c8a9412d07745f2b1
Hongtao Yu <hoy@fb.com>
2021-02-05 17:56:45 -0800

    [ELF] Rewriting the path of sample profile file for --reproduce response.txt
    Rewritting the path of the sample profile file in response.txt to be relative to the repro tar.
    
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D96193

--- lld/ELF/DriverUtils.cpp
+++ lld/ELF/DriverUtils.cpp
@@ -186,6 +186,9 @@ std::string elf::createResponseFile(const opt::InputArgList &args) {
       // Strip directories to prevent the issue.
       os << "-o " << quote(path::filename(arg->getValue())) << "\n";
       break;
+    case OPT_lto_sample_profile:
+      os << arg->getSpelling() << quote(rewritePath(arg->getValue())) << "\n";
+      break;
     case OPT_call_graph_ordering_file:
     case OPT_dynamic_list:
     case OPT_just_symbols:
