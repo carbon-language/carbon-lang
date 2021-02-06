e6810cab09fcbc87b6e5e4d226de0810e2f2ea38
Fangrui Song <i@maskray.me>
2021-02-06 11:56:15 -0800

    [Polly] Fix build after AssumptionCache change (D96168)

--- polly/lib/Analysis/ScopBuilder.cpp
+++ polly/lib/Analysis/ScopBuilder.cpp
@@ -1527,7 +1527,7 @@ void ScopBuilder::addRecordedAssumptions() {
 void ScopBuilder::addUserAssumptions(
     AssumptionCache &AC, DenseMap<BasicBlock *, isl::set> &InvalidDomainMap) {
   for (auto &Assumption : AC.assumptions()) {
-    auto *CI = dyn_cast_or_null<CallInst>(Assumption);
+    auto *CI = dyn_cast_or_null<CallInst>(Assumption.getAssumeCI());
     if (!CI || CI->getNumArgOperands() != 1)
       continue;
 
