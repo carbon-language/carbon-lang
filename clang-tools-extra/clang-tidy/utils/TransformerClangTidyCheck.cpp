33f35a4b793bb53b830f8893110af57672e1dc79
Yitzhak Mandelbaum <yitzhakm@google.com>
2021-02-11 20:51:08 +0000

    [clang-tidy] Fix `TransformerClangTidyCheck`'s handling of include insertions.
    Currently, all include insertions are directed to the main file. However,
    Transformer rules can specify alternative destinations for include
    insertions. This patch fixes the code to associate the include with the correct
    file.
    
    This patch was tested manually. The clang tidy unit test framework does not
    support testing changes to header files. Given that this is a bug fix for a live
    bug, the patch relies on manual testing rather than blocking on upgrading the
    unit test framework.
    
    Differential Revision: https://reviews.llvm.org/D96542

--- clang-tools-extra/clang-tidy/utils/TransformerClangTidyCheck.cpp
+++ clang-tools-extra/clang-tidy/utils/TransformerClangTidyCheck.cpp
@@ -104,7 +104,8 @@ void TransformerClangTidyCheck::check(
       Diag << FixItHint::CreateReplacement(T.Range, T.Replacement);
       break;
     case transformer::EditKind::AddInclude:
-      Diag << Inserter.createMainFileIncludeInsertion(T.Replacement);
+      Diag << Inserter.createIncludeInsertion(
+          Result.SourceManager->getFileID(T.Range.getBegin()), T.Replacement);
       break;
     }
 }
