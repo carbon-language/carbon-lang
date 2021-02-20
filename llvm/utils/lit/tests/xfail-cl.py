4550fdff2b2eec15143fac536e41ce967e522a3a
David Zarzycki <dave@znu.io>
2021-02-20 09:43:36 -0500

    [lit testing] "END." not "END:"

--- llvm/utils/lit/tests/xfail-cl.py
+++ llvm/utils/lit/tests/xfail-cl.py
@@ -2,8 +2,8 @@
 #
 # RUN: %{lit} --xfail 'false.txt;false2.txt' %{inputs}/xfail-cl | FileCheck --check-prefix=CHECK-FILTER %s
 # RUN: env LIT_XFAIL='false.txt;false2.txt' %{lit} %{inputs}/xfail-cl | FileCheck --check-prefix=CHECK-FILTER %s
-# END:
+# END.
 # CHECK-FILTER: Testing: 3 tests, 3 workers
-# CHECK-FILTER-DAG: {{XFAIL}}: top-level-suite :: false.txt
-# CHECK-FILTER-DAG: {{XFAIL}}: top-level-suite :: false2.txt
+# CHECK-FILTER-DAG: XFAIL: top-level-suite :: false.txt
+# CHECK-FILTER-DAG: XFAIL: top-level-suite :: false2.txt
 # CHECK-FILTER-DAG: PASS: top-level-suite :: true.txt
