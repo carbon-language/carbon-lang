6c06b0aa5a5534a370532c287cf35a96eeb2146e
Michał Górny <mgorny@moritz.systems>
2021-02-23 13:43:40 +0100

    [lldb] [test] Un-XFAIL TestBuiltinTrap on FreeBSD/aarch64

--- lldb/test/API/linux/builtin_trap/TestBuiltinTrap.py
+++ lldb/test/API/linux/builtin_trap/TestBuiltinTrap.py
@@ -23,7 +23,7 @@ class BuiltinTrapTestCase(TestBase):
 
     # gcc generates incorrect linetable
     @expectedFailureAll(archs="arm", compiler="gcc", triple=".*-android")
-    @expectedFailureAll(archs=['aarch64'], oslist=no_match(['linux']))
+    @expectedFailureAll(archs=['aarch64'], oslist=no_match(['freebsd', 'linux']))
     @skipIfWindows
     def test_with_run_command(self):
         """Test that LLDB handles a function with __builtin_trap correctly."""
