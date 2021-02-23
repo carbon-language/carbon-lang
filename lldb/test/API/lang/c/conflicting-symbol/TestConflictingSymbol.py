2f75363a9e138429d5c80ca0a541247a5bc70614
Michał Górny <mgorny@moritz.systems>
2021-02-23 13:34:04 +0100

    [lldb] [test] Un-XFAIL a test that no longer fail on FreeBSD

--- lldb/test/API/lang/c/conflicting-symbol/TestConflictingSymbol.py
+++ lldb/test/API/lang/c/conflicting-symbol/TestConflictingSymbol.py
@@ -19,7 +19,6 @@ class TestConflictingSymbols(TestBase):
         lldbutil.mkdir_p(self.getBuildArtifact("Two"))
 
     @expectedFailureAll(oslist=["windows"], bugnumber="llvm.org/pr24489")
-    @expectedFailureAll(oslist=["freebsd"], bugnumber="llvm.org/pr48416")
     def test_conflicting_symbols(self):
         self.build()
         exe = self.getBuildArtifact("a.out")
