9ced8b3b614ceba7dcd4a8fc3d2fc0be7d09f650
Jez Ng <jezng@fb.com>
2021-02-24 09:42:52 -0500

    [lld-macho] Add REQUIRES to incompatible-arch-tapi test

--- lld/test/MachO/invalid/incompatible-arch-tapi.s
+++ lld/test/MachO/invalid/incompatible-arch-tapi.s
@@ -1,3 +1,4 @@
+# REQUIRES: x86
 # RUN: rm -rf %t; split-file %s %t
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-macosx %t/main.s -o %t/main.o
 # RUN: not %lld -arch x86_64 -lSystem %S/Inputs/libincompatible.tbd %t/main.o -o /dev/null 2>&1 | FileCheck %s --check-prefix=ARCH
