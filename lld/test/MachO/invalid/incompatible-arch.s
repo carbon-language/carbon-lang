91e31ebbbe0c5781d1c0d5b3470c2cb8932faf0b
Douglas Yung <douglas.yung@sony.com>
2021-02-24 00:02:21 -0800

    Add REQUIRES aarch64 to test incompatible-arch.s since it fails if that target is not built.
    This should fix the PS4 linux buildbot failure: http://lab.llvm.org:8011/#/builders/139/builds/63

--- lld/test/MachO/invalid/incompatible-arch.s
+++ lld/test/MachO/invalid/incompatible-arch.s
@@ -1,3 +1,4 @@
+# REQUIRES: aarch64
 # RUN: llvm-mc -filetype=obj -triple=arm64-apple-darwin %s -o %t.o
 # RUN: not %lld -arch x86_64 -lSystem %t.o -o /dev/null 2>&1 | FileCheck %s -DFILE=%t.o
 # CHECK: error: {{.*}}[[FILE]] has architecture arm64 which is incompatible with target architecture x86_64
