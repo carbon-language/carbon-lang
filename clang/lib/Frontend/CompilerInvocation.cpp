cb81135f94e58ce0a0e91d764841acee90eeb59f
Jan Svoboda <jan_svoboda@apple.com>
2021-02-25 13:20:59 +0100

    [clang][cli] Implement '-cuid=' marshalling
    This patch adds marshalling to the `-cuid=` option introduced in D95007.
    
    Reviewed By: Bigcheese
    
    Differential Revision: https://reviews.llvm.org/D97461

--- clang/lib/Frontend/CompilerInvocation.cpp
+++ clang/lib/Frontend/CompilerInvocation.cpp
@@ -3585,11 +3585,6 @@ bool CompilerInvocation::ParseLangArgs(LangOptions &Opts, ArgList &Args,
     }
   }
 
-
-  if (auto *A = Args.getLastArg(OPT_cuid_EQ)) {
-    Opts.CUID = std::string(A->getValue());
-  }
-
   if (Opts.ObjC) {
     if (Arg *arg = Args.getLastArg(OPT_fobjc_runtime_EQ)) {
       StringRef value = arg->getValue();
