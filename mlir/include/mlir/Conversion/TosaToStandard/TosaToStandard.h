16abacaea9db653b41808fc37277b68168438059
Rob Suderman <rob.suderman@gmail.com>
2021-02-25 18:08:29 -0800

    [MLIR][TOSA] Resubmit Tosa to Standard/SCF Lowerings (const, if, while)"
    Includes a lowering for tosa.const, tosa.if, and tosa.while to Standard/SCF dialects. TosaToStandard is
    used for constant lowerings and TosaToSCF handles the if/while ops.
    
    Resubmission of https://reviews.llvm.org/D97518 with ASAN fixes.
    
    Differential Revision: https://reviews.llvm.org/D97529

---
+++ mlir/include/mlir/Conversion/TosaToStandard/TosaToStandard.h
@@ -0,0 +1,32 @@
+//===-- TosaToStandard.h - TOSA optimization pass declarations --*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file declares the passes for the TOSA to Standard Dialect conversion.
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef MLIR_CONVERSION_TOSATOSTANDARD_TOSATOSTANDARD_H
+#define MLIR_CONVERSION_TOSATOSTANDARD_TOSATOSTANDARD_H
+
+#include "mlir/Pass/Pass.h"
+
+namespace mlir {
+namespace tosa {
+
+std::unique_ptr<Pass> createTosaToStandard();
+
+void populateTosaToStandardConversionPatterns(
+    MLIRContext *context, OwningRewritePatternList *patterns);
+
+/// Populates passes to convert from TOSA to Standard.
+void addTosaToStandardPasses(OpPassManager &pm);
+
+} // namespace tosa
+} // namespace mlir
+
+#endif // MLIR_CONVERSION_TOSATOSTANDARD_TOSATOSTANDARD_H
