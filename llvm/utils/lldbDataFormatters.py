8d1b6afcea6c30db7728d966d9281ab373eaa9e1
Dave Lee <davelee.com@gmail.com>
2021-02-26 07:45:04 -0800

    [llvm][utils] Rename lldb dict variables to internal_dict
    Most lldb scripts use `internal_dict`. Also, `dict` is a builtin constructor,
    it's good habit to avoid using it as a variable name.

--- llvm/utils/lldbDataFormatters.py
+++ llvm/utils/lldbDataFormatters.py
@@ -35,7 +35,7 @@ def __lldb_init_module(debugger, internal_dict):
 
 # Pretty printer for llvm::SmallVector/llvm::SmallVectorImpl
 class SmallVectorSynthProvider:
-    def __init__(self, valobj, dict):
+    def __init__(self, valobj, internal_dict):
         self.valobj = valobj;
         self.update() # initialize this provider
 
@@ -74,7 +74,7 @@ class SmallVectorSynthProvider:
 
 class ArrayRefSynthProvider:
     """ Provider for llvm::ArrayRef """
-    def __init__(self, valobj, dict):
+    def __init__(self, valobj, internal_dict):
         self.valobj = valobj;
         self.update() # initialize this provider
 
