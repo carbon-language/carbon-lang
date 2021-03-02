bbdb4c8c9bcef0e8db751630accc04ad874f54e7
Stefan Gränitz <stefan.graenitz@gmail.com>
2021-03-02 21:42:11 +0100

    [llvm-jitlink] Prevent missing symbols from JITLoaderGDB with MSVC mangling
    The issue came up on builder clang-x64-windows-msvc after 5182a7901a5d83dfd15021d01e8a1899910130ec

--- llvm/tools/llvm-jitlink/llvm-jitlink.cpp
+++ llvm/tools/llvm-jitlink/llvm-jitlink.cpp
@@ -852,8 +852,9 @@ Session::Session(std::unique_ptr<TargetProcessControl> TPC, Error &Err)
     ObjLayer.addPlugin(std::make_unique<EHFrameRegistrationPlugin>(
         ES, ExitOnErr(TPCEHFrameRegistrar::Create(*this->TPC))));
 
-  ObjLayer.addPlugin(std::make_unique<DebugObjectManagerPlugin>(
-      ES, ExitOnErr(createJITLoaderGDBRegistrar(*this->TPC))));
+  if (!this->TPC->getTargetTriple().isOSWindows())
+    ObjLayer.addPlugin(std::make_unique<DebugObjectManagerPlugin>(
+        ES, ExitOnErr(createJITLoaderGDBRegistrar(*this->TPC))));
 
   ObjLayer.addPlugin(std::make_unique<JITLinkSessionPlugin>(*this));
 
