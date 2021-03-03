ab280cbaa3daef375a6056c385986f8f38932e2c
David Green <david.green@arm.com>
2021-03-03 08:02:58 +0000

    [ARM] Ensure undef is propagated to CBZ/CBNZ flags
    In some rare circumstances we can be using an undef register for a
    compare. When folded into a CBZ/CBNZ the undef flags are lost, leading
    to machine verifier problems. This propagates the existing flags to the
    new instruction.

--- llvm/lib/Target/ARM/ARMConstantIslandPass.cpp
+++ llvm/lib/Target/ARM/ARMConstantIslandPass.cpp
@@ -1986,7 +1986,8 @@ bool ARMConstantIslands::optimizeThumb2Branches() {
     LLVM_DEBUG(dbgs() << "Fold: " << *Cmp.MI << " and: " << *Br.MI);
     MachineInstr *NewBR =
         BuildMI(*MBB, Br.MI, Br.MI->getDebugLoc(), TII->get(Cmp.NewOpc))
-            .addReg(Reg, getKillRegState(RegKilled))
+            .addReg(Reg, getKillRegState(RegKilled) |
+                             getRegState(Cmp.MI->getOperand(0)))
             .addMBB(DestBB, Br.MI->getOperand(0).getTargetFlags());
 
     Cmp.MI->eraseFromParent();
