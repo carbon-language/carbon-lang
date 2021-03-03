9403b59a7dbbf35714b5786df0e535281b926746
Daniel McIntosh <Daniel.McIntosh@ibm.com>
2021-03-03 10:29:37 -0500

    [test] Fix apparent typo in clang/test/Driver/std.c
    Currently the test on line 3 is identical to the test on line 1.
    Looking at the rest of the file (particularily the use of FOVERRIDE
    as the check-prefix), I think it's pretty clear that this line
    was supposed to use `-ftrigraphs` instead of `-trigraphs`.
    
    Reviewed By: thakis
    
    Differential Revision: https://reviews.llvm.org/D97796

--- clang/test/Driver/std.c
+++ clang/test/Driver/std.c
@@ -1,6 +1,6 @@
 // RUN: %clang -w -std=c99 -trigraphs -std=gnu99 %s -E -o - | FileCheck -check-prefix=OVERRIDE %s
 // OVERRIDE: ??(??)
-// RUN: %clang -w -std=c99 -trigraphs -std=gnu99 %s -E -o - | FileCheck -check-prefix=FOVERRIDE %s
+// RUN: %clang -w -std=c99 -ftrigraphs -std=gnu99 %s -E -o - | FileCheck -check-prefix=FOVERRIDE %s
 // FOVERRIDE: ??(??)
 // RUN: %clang -w -ansi %s -E -o - | FileCheck -check-prefix=ANSI %s
 // ANSI: []
