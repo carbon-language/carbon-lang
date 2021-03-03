5d9169845b8906186a4a4fb6b193d3e463d97618
Jez Ng <jezng@fb.com>
2021-03-03 17:31:03 -0500

    [lld-macho] Fix test breakage introduced by D97799

--- lld/test/MachO/invalid/stub-link.s
+++ lld/test/MachO/invalid/stub-link.s
@@ -6,7 +6,8 @@
 # RUN: mkdir -p %t
 #
 # RUN: llvm-mc -filetype obj -triple x86_64-apple-ios %s -o %t/test.o
-# RUN: not ld64.lld -o %t/test -syslibroot %S/../Inputs/iPhoneSimulator.sdk -lSystem %t/test.o 2>&1 | FileCheck %s
+# RUN: not ld64.lld -arch x86_64 -platform_version ios 14.0 15.0 -o %t/test \
+# RUN:   -syslibroot %S/../Inputs/iPhoneSimulator.sdk -lSystem %t/test.o 2>&1 | FileCheck %s
 
 # CHECK-DAG: error: undefined symbol: __cache_handle_memory_pressure_event
 # CHECK-DAG: error: undefined symbol: _from_non_reexported_tapi_dylib
