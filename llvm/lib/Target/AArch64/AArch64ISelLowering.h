8c3a70a78f7e15d1c8f474a9e5316f1d3b5a71d0
Florian Hahn <flo@fhahn.com>
2021-03-03 18:58:05 +0000

    [AArch64] Move CALL_RVMARKER definition after CALL.
    This is a NFC with respect to the generated code. But it fixes a crash
    when using -debug, because of the position in the enum CALL_RVMARKER
    nodes were treated as memops. That caused a crash when printing
    CALL_RVMARKER nodes.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.h
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.h
@@ -51,6 +51,10 @@ enum NodeType : unsigned {
   WrapperLarge, // 4-instruction MOVZ/MOVK sequence for 64-bit addresses.
   CALL,         // Function call.
 
+  // Pseudo for a OBJC call that gets emitted together with a special `mov
+  // x29, x29` marker instruction.
+  CALL_RVMARKER,
+
   // Produces the full sequence of instructions for getting the thread pointer
   // offset of a variable into X0, using the TLSDesc model.
   TLSDESC_CALLSEQ,
@@ -429,10 +433,6 @@ enum NodeType : unsigned {
   LDP,
   STP,
   STNP,
-
-  // Pseudo for a OBJC call that gets emitted together with a special `mov
-  // x29, x29` marker instruction.
-  CALL_RVMARKER
 };
 
 } // end namespace AArch64ISD
