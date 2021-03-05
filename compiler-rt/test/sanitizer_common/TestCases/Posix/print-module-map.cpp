51d8f598ad57d0b3581f167afe1ec404a58ff386
Emily Shi <code@emi.sh>
2021-03-05 14:39:18 -0800

    [sanitizers] run print module map test run on posix
    Previously, this test only ran for mac because platforms have different messaging. This diff enables the test for all posix
    
    rdar://75110847
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D98079

--- compiler-rt/test/sanitizer_common/TestCases/Posix/print-module-map.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/print-module-map.cpp
@@ -1,6 +1,11 @@
 // Checks that module map does not print at 0, prints once after aborting with 1,
 // and prints once before and after aborting with 2
 
+// mac header is "Process module map"
+// other posix header is "Process memory map follows"
+// windows header is "Dumping process modules" (ignored here)
+// we should consider unifying the message cross platform
+
 // RUN: %clangxx -DUSING_%tool_name %s -o %t -w
 
 // RUN: %env_tool_opts="print_module_map=0" not %run %t 2>&1 | FileCheck %s --check-prefixes=CHECK,CHECK-MM0
@@ -31,10 +36,10 @@ int main() {
 }
 
 // CHECK: SUMMARY:
-// CHECK-MM0-NOT: Process module map:
-// CHECK-MM1-NOT: Process module map:
-// CHECK-MM2: Process module map:
+// CHECK-MM0-NOT: {{Process .*map}}
+// CHECK-MM1-NOT: {{Process .*map}}
+// CHECK-MM2: {{Process (module|memory) map}}
 // CHECK: ABORTING
-// CHECK-MM0-NOT: Process module map:
-// CHECK-MM1-NEXT: Process module map:
-// CHECK-MM2-NEXT: Process module map:
+// CHECK-MM0-NOT: {{Process .*map}}
+// CHECK-MM1-NEXT: {{Process (module|memory) map}}
+// CHECK-MM2-NEXT: {{Process (module|memory) map}}
