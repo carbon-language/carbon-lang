714644a36c3095e1dffeb2fb42da3876a5919d21
Martin Storsjö <martin@martin.st>
2021-03-05 17:27:55 +0200

    [libcxx] [test] Move the is_<platform> functions down to subclasses
    If cross testing (and manually specifying a LIBCXX_TARGET_INFO in the
    cmake configuration, as the default is to match the build platform),
    we want the accessors for querying the target platform, is_windows,
    is_darwin, to return the right value depending on which target info
    class is used, not based on what platform is running the build and
    driving the tests.
    
    When LIBCXX_TARGET_INFO isn't defined, the right target info class
    is chosen automatically based on the platform one is running on, so
    this shouldn't make any practical difference for such setups.
    
    Differential Revision: https://reviews.llvm.org/D98045

--- libcxx/utils/libcxx/test/target_info.py
+++ libcxx/utils/libcxx/test/target_info.py
@@ -25,13 +25,13 @@ class DefaultTargetInfo(object):
         return sys.platform.lower().strip()
 
     def is_windows(self):
-        return self.platform() == 'win32'
+        return False
 
     def is_mingw(self):
         return False
 
     def is_darwin(self):
-        return self.platform() == 'darwin'
+        return False
 
     def add_cxx_flags(self, flags): pass
     def add_cxx_compile_flags(self, flags): pass
@@ -52,6 +52,9 @@ class DefaultTargetInfo(object):
 class DarwinLocalTI(DefaultTargetInfo):
     def __init__(self, full_config):
         super(DarwinLocalTI, self).__init__(full_config)
+
+    def is_darwin(self):
+        return True
 
     def is_host_macosx(self):
         name = lit.util.to_string(subprocess.check_output(['sw_vers', '-productName'])).strip()
@@ -186,6 +189,9 @@ class LinuxRemoteTI(LinuxLocalTI):
 class WindowsLocalTI(DefaultTargetInfo):
     def __init__(self, full_config):
         super(WindowsLocalTI, self).__init__(full_config)
+
+    def is_windows(self):
+        return True
 
 class MingwLocalTI(WindowsLocalTI):
     def __init__(self, full_config):
