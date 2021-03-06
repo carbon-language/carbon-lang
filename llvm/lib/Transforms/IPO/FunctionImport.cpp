fb2cf0dd609238931b0fc53ef0ea2172a99307ae
Fangrui Song <i@maskray.me>
2021-03-06 14:09:54 -0800

    [FunctionImport] Delete unneeded setLive. NFC
    ValueInfo's in Worklist are guaranteed to be live.

--- llvm/lib/Transforms/IPO/FunctionImport.cpp
+++ llvm/lib/Transforms/IPO/FunctionImport.cpp
@@ -914,7 +914,6 @@ void llvm::computeDeadSymbols(
   while (!Worklist.empty()) {
     auto VI = Worklist.pop_back_val();
     for (auto &Summary : VI.getSummaryList()) {
-      Summary->setLive(true);
       if (auto *AS = dyn_cast<AliasSummary>(Summary.get())) {
         // If this is an alias, visit the aliasee VI to ensure that all copies
         // are marked live and it is added to the worklist for further
