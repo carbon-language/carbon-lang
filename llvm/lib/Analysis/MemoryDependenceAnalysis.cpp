875891a10d50a791d3f076c9259e60af6c9af18c
William S. Moses <gh@wsmoses.com>
2021-03-09 11:55:19 -0500

    [MemoryDependence] Fix invariant group store
    Fix bug in MemoryDependence [and thus GVN] for invariant group.
    
    Previously MemDep didn't verify that the store was storing into a
    pointer rather than a store simply using a pointer.
    
    Differential Revision: https://reviews.llvm.org/D98267

--- llvm/lib/Analysis/MemoryDependenceAnalysis.cpp
+++ llvm/lib/Analysis/MemoryDependenceAnalysis.cpp
@@ -344,7 +344,9 @@ MemoryDependenceResults::getInvariantGroupPointerDependency(LoadInst *LI,
       // If we hit load/store with the same invariant.group metadata (and the
       // same pointer operand) we can assume that value pointed by pointer
       // operand didn't change.
-      if ((isa<LoadInst>(U) || isa<StoreInst>(U)) &&
+      if ((isa<LoadInst>(U) ||
+           (isa<StoreInst>(U) &&
+            cast<StoreInst>(U)->getPointerOperand() == Ptr)) &&
           U->hasMetadata(LLVMContext::MD_invariant_group))
         ClosestDependency = GetClosestDependency(ClosestDependency, U);
     }
