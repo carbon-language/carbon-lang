fd8f61d70c004d612212938de810b916953c6530
Dave Abrahams <dabrahams@google.com>
2021-03-09 15:33:43 -0800

    Working state.

--- executable_semantics/syntax/driver.cpp
+++ executable_semantics/syntax/driver.cpp
@@ -9,34 +9,6 @@
 
 #include "executable_semantics/tracing_flag.h"
 
-extern FILE* yyin;
-
-// Returns an abstract representation of the program contained in the
-// well-formed input file, or if the file was malformed, a description of the
-// problem.
-auto Carbon::SyntaxDriver::operator()()
-    -> std::variant<Carbon::AST, Carbon::SyntaxDriver::Error> {
-  yyin = fopen(inputFileName.c_str(), "r");
-  if (yyin == nullptr) {
-    std::cerr << "Error opening '" << inputFileName
-              << "': " << std::strerror(errno) << std::endl;
-    exit(1);
-  }
-
-  std::optional<Carbon::AST> parsedInput = std::nullopt;
-  auto syntaxErrorCode = yyparse(parsedInput, *this);
-  if (syntaxErrorCode != 0) {
-    return syntaxErrorCode;
-  }
-
-  if (parsedInput == std::nullopt) {
-    std::cerr << "Internal error: parser validated syntax yet didn't produce "
-                 "an AST.\n";
-    exit(1);
-  }
-  return *parsedInput;
-}
-
 // Writes a syntax error diagnostic, containing message, for the input file at
 // the given line, to standard error.
 auto Carbon::SyntaxDriver::PrintDiagnostic(const char* message, int line_num)
