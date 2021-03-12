154395536e3cd9b39141b306ebb6f41620d4f53f
Christopher Di Bella <cjdb.ns@gmail.com>
2021-03-11 19:49:27 -0800

    [libcxx] adds concept std::semiregular
    Implements parts of:
        - P0898R3 Standard Library Concepts
        - P1754 Rename concepts to standard_case for C++20, while we still can
    
    Depends on D97443
    
    Reviewed By: Quuxplusone, EricWF, #libc
    
    Differential Revision: https://reviews.llvm.org/D97911

--- libcxx/test/std/concepts/object/copyable.compile.pass.cpp
+++ libcxx/test/std/concepts/object/copyable.compile.pass.cpp
@@ -9,7 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // UNSUPPORTED: libcpp-no-concepts
 
-// template<class From>
+// template<class T>
 // concept copyable = see below;
 
 #include <concepts>
