5bb2757e218dfa06662e6f019c13c3378e36c925
Josh Berdine <josh@berdine.net>
2021-03-13 23:29:36 +0000

    [OCaml][test] Fix Bindings/OCaml/executionengine.ml test
    It seems that at some point it became necessary to pass `-thread` to
    the ocaml compiler for this test.
    
    Differential Revision: https://reviews.llvm.org/D98593

--- llvm/test/Bindings/OCaml/executionengine.ml
+++ llvm/test/Bindings/OCaml/executionengine.ml
@@ -1,7 +1,7 @@
 (* RUN: rm -rf %t && mkdir -p %t && cp %s %t/executionengine.ml
- * RUN: %ocamlc -g -w +A -package llvm.executionengine -linkpkg %t/executionengine.ml -o %t/executable
+ * RUN: %ocamlc -g -w +A -thread -package llvm.executionengine -linkpkg %t/executionengine.ml -o %t/executable
  * RUN: %t/executable
- * RUN: %ocamlopt -g -w +A -package llvm.executionengine -linkpkg %t/executionengine.ml -o %t/executable
+ * RUN: %ocamlopt -g -w +A -thread -package llvm.executionengine -linkpkg %t/executionengine.ml -o %t/executable
  * RUN: %t/executable
  * REQUIRES: native
  * XFAIL: vg_leak
