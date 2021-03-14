19740652c4c4329e2b9e77f96e5e31c360b4e8bb
Stephen Kelly <steveire@gmail.com>
2021-03-14 16:18:54 +0000

    [AST] Add generator for source location introspection
    Generate a json file containing descriptions of AST classes and their
    public accessors which return SourceLocation or SourceRange.
    
    Use the JSON file to generate a C++ API and implementation for accessing
    the source locations and method names for accessing them for a given AST
    node.
    
    This new API can be used to implement 'srcloc' output in clang-query:
    
      http://ce.steveire.com/z/m_kTIo
    
    The JSON file can also be used to generate bindings for other languages,
    such as Python and Javascript:
    
      https://steveire.wordpress.com/2019/04/30/the-future-of-ast-matching
    
    In this first version of this feature, only the accessors for Stmt
    classes are generated, not Decls, TypeLocs etc.  Those can be added
    after this change is reviewed, as this change is mostly about
    infrastructure of these code generators.
    
    Also in this version, the platforms/cmake configurations are excluded as
    much as possible so that support can be added iteratively.  Currently a
    break on any platform causes a revert of the entire feature.  This way,
    the `OR WIN32` can be removed in a future commit and if it breaks the
    buildbots, only that commit gets reverted, making the entire process
    easier to manage.
    
    Differential Revision: https://reviews.llvm.org/D93164

---
+++ clang/unittests/Introspection/IntrospectionTest.cpp
@@ -0,0 +1,80 @@
+//===- unittest/Introspection/IntrospectionTest.cpp ----------*- C++ -*---===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Tests for AST location API introspection.
+//
+//===----------------------------------------------------------------------===//
+
+#include "clang/AST/ASTContext.h"
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/NodeIntrospection.h"
+#include "clang/Tooling/Tooling.h"
+#include "gmock/gmock-matchers.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+using namespace clang;
+using namespace clang::ast_matchers;
+using namespace clang::tooling;
+
+using ::testing::Pair;
+using ::testing::UnorderedElementsAre;
+
+template<typename T, typename MapType>
+std::map<std::string, T>
+FormatExpected(const MapType &Accessors) {
+  std::map<std::string, T> Result;
+  llvm::transform(Accessors,
+                  std::inserter(Result, Result.end()),
+                  [](const auto &Accessor) {
+                    return std::make_pair(
+                        LocationCallFormatterCpp::format(Accessor.second.get()),
+                        Accessor.first);
+                  });
+  return Result;
+}
+
+TEST(Introspection, SourceLocations) {
+  auto AST = buildASTFromCode("void foo() {} void bar() { foo(); }", "foo.cpp",
+                              std::make_shared<PCHContainerOperations>());
+  auto &Ctx = AST->getASTContext();
+  auto &TU = *Ctx.getTranslationUnitDecl();
+
+  auto BoundNodes = ast_matchers::match(
+      decl(hasDescendant(
+          callExpr(callee(functionDecl(hasName("foo")))).bind("fooCall"))),
+      TU, Ctx);
+
+  EXPECT_EQ(BoundNodes.size(), 1u);
+
+  auto *FooCall = BoundNodes[0].getNodeAs<CallExpr>("fooCall");
+
+  auto Result = NodeIntrospection::GetLocations(FooCall);
+
+  if (Result.LocationAccessors.empty() && Result.RangeAccessors.empty())
+  {
+    return;
+  }
+
+  auto ExpectedLocations =
+    FormatExpected<SourceLocation>(Result.LocationAccessors);
+
+  EXPECT_THAT(
+      ExpectedLocations,
+      UnorderedElementsAre(Pair("getBeginLoc()", FooCall->getBeginLoc()),
+                           Pair("getEndLoc()", FooCall->getEndLoc()),
+                           Pair("getExprLoc()", FooCall->getExprLoc()),
+                           Pair("getRParenLoc()", FooCall->getRParenLoc())));
+
+  auto ExpectedRanges = FormatExpected<SourceRange>(Result.RangeAccessors);
+
+  EXPECT_THAT(ExpectedRanges,
+              UnorderedElementsAre(
+                  Pair("getSourceRange()", FooCall->getSourceRange())));
+}
