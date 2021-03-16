264f101ae6b4e35a0d377f3fb8bc35dbe9ba173e
Jeremy Morse <jeremy.morse@sony.com>
2021-03-16 16:50:49 +0000

    Tweak spelling of system-windows UNSUPPORTED line

--- llvm/utils/lit/tests/reorder.py
+++ llvm/utils/lit/tests/reorder.py
@@ -3,7 +3,7 @@
 # RUN: cp %{inputs}/reorder/.lit_test_times.txt %{inputs}/reorder/.lit_test_times.txt.orig
 # RUN: %{lit} -j1 %{inputs}/reorder | FileCheck %s
 # RUN: not diff %{inputs}/reorder/.lit_test_times.txt %{inputs}/reorder/.lit_test_times.txt.orig
-# UNSUPPORTED: windows
+# UNSUPPORTED: system-windows
 # END.
 
 # CHECK:     -- Testing: 3 tests, 1 workers --
