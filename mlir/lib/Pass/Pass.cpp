0524a09cc7e1a0797982feacf505825231efbee7
Sean Silva <silvasean@google.com>
2021-03-16 13:40:35 -0700

    [mlir] Tune error message for assertion.
    This assertion can fire in the case of different contexts as well, which
    is not difficult to do from Python bindings, for example.

--- mlir/lib/Pass/Pass.cpp
+++ mlir/lib/Pass/Pass.cpp
@@ -864,7 +864,8 @@ void PassManager::enableVerifier(bool enabled) { verifyPasses = enabled; }
 LogicalResult PassManager::run(Operation *op) {
   MLIRContext *context = getContext();
   assert(op->getName().getIdentifier() == getOpName(*context) &&
-         "operation has a different name than the PassManager");
+         "operation has a different name than the PassManager or is from a "
+         "different context");
 
   // Before running, make sure to coalesce any adjacent pass adaptors in the
   // pipeline.
