9de63b2e051cb3e79645cc20b83b4d33d132cba0
Martin Storsjö <martin@martin.st>
2021-03-18 14:08:10 +0200

    [lit] Pass the USERPROFILE variable through on Windows
    When running in a Windows Container, the Git for Windows Unix tools
    (C:\Program Files\Git\usr\bin) just hang if this variable isn't
    passed through.
    
    Currently, running the LLVM/clang tests in a Windows Container fails
    if that directory is added to the path, but succeeds after this change.
    (After this change, the previously used GnuWin tools can be left out
    entirely, too, as lit automatically picks up the Git for Windows tools
    if necessary.)
    
    Differential Revision: https://reviews.llvm.org/D98858

--- llvm/utils/lit/lit/TestingConfig.py
+++ llvm/utils/lit/lit/TestingConfig.py
@@ -33,6 +33,7 @@ class TestingConfig(object):
             pass_vars.append('INCLUDE')
             pass_vars.append('LIB')
             pass_vars.append('PATHEXT')
+            pass_vars.append('USERPROFILE')
             environment['PYTHONBUFFERED'] = '1'
 
         for var in pass_vars:
