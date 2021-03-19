d1757d99790722305ab8b40a941d8e05335006a8
Richard Smith <richard-llvm@metafoo.co.uk>
2021-03-18 21:05:54 -0700

    Add location information to diagnostics. (#385)
    Add simple mocking and testing of diagnostic emission to ensure this
    works.

--- lexer/numeric_literal_test.cpp
+++ lexer/numeric_literal_test.cpp
@@ -5,15 +5,22 @@
 #include "lexer/numeric_literal.h"
 
 #include <iterator>
+#include <memory>
+#include <vector>
 
 #include "diagnostics/diagnostic_emitter.h"
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
+#include "lexer/test_helpers.h"
 
 namespace Carbon {
 namespace {
 
 struct NumericLiteralTest : ::testing::Test {
+  std::vector<std::unique_ptr<Testing::SingleTokenDiagnosticTranslator>>
+      translators;
+  std::vector<std::unique_ptr<DiagnosticEmitter<const char*>>> emitters;
+
   auto Lex(llvm::StringRef text) -> LexedNumericLiteral {
     llvm::Optional<LexedNumericLiteral> result = LexedNumericLiteral::Lex(text);
     assert(result);
@@ -22,7 +29,11 @@ struct NumericLiteralTest : ::testing::Test {
   }
 
   auto Parse(llvm::StringRef text) -> LexedNumericLiteral::Parser {
-    return LexedNumericLiteral::Parser(ConsoleDiagnosticEmitter(), Lex(text));
+    translators.push_back(
+        std::make_unique<Testing::SingleTokenDiagnosticTranslator>(text));
+    emitters.push_back(std::make_unique<DiagnosticEmitter<const char*>>(
+        *translators.back(), ConsoleDiagnosticConsumer()));
+    return LexedNumericLiteral::Parser(*emitters.back(), Lex(text));
   }
 };
 
