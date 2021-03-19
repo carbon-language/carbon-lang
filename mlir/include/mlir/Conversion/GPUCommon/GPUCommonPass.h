74ffe8dc590c29f1895e7b9cabf13944ffef16cb
Christian Sigg <csigg@google.com>
2021-03-19 09:27:55 +0100

    [mlir] Remove ConvertKernelFuncToBlob
    All users have been converted to gpu::SerializeToBlobPass.
    
    Reviewed By: ftynse
    
    Differential Revision: https://reviews.llvm.org/D98928

--- mlir/include/mlir/Conversion/GPUCommon/GPUCommonPass.h
+++ mlir/include/mlir/Conversion/GPUCommon/GPUCommonPass.h
@@ -60,37 +60,6 @@ createGpuToLLVMConversionPass(StringRef gpuBinaryAnnotation = {});
 void populateGpuToLLVMConversionPatterns(LLVMTypeConverter &converter,
                                          OwningRewritePatternList &patterns,
                                          StringRef gpuBinaryAnnotation = {});
-
-/// Creates a pass to convert kernel functions into GPU target object blobs.
-///
-/// This transformation takes the body of each function that is annotated with
-/// the 'gpu.kernel' attribute, copies it to a new LLVM module, compiles the
-/// module with help of the GPU backend to target object and then invokes
-/// the provided blobGenerator to produce a binary blob. Such blob is then
-/// attached as a string attribute to the kernel function.
-///
-/// Following callbacks are to be provided by user:
-/// - loweringCallback : lower the module to an LLVM module.
-/// - blobGenerator : build a blob executable on target GPU.
-///
-/// Information wrt LLVM backend are to be supplied by user:
-/// - triple : target triple to be used.
-/// - targetChip : mcpu to be used.
-/// - features : target-specific features to be used.
-///
-/// Information about result attribute is to be specified by user:
-/// - gpuBinaryAnnotation : the name of the attribute which contains the blob.
-///
-/// After the transformation, the body of the kernel function is removed (i.e.,
-/// it is turned into a declaration).
-///
-/// A non-empty gpuBinaryAnnotation overrides the pass' command line option.
-std::unique_ptr<OperationPass<gpu::GPUModuleOp>>
-createConvertGPUKernelToBlobPass(LoweringCallback loweringCallback,
-                                 BlobGenerator blobGenerator, StringRef triple,
-                                 StringRef targetChip, StringRef features,
-                                 StringRef gpuBinaryAnnotation = {});
-
 } // namespace mlir
 
 #endif // MLIR_CONVERSION_GPUCOMMON_GPUCOMMONPASS_H_
