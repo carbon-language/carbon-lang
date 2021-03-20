bed9933a461e7b3d0c8c5a8fa770aa1b49802660
Fangrui Song <i@maskray.me>
2021-03-19 23:50:22 -0700

    [Driver][test] Fix gcc-toolchain.cpp on non-x86_64

--- clang/test/Driver/gcc-toolchain.cpp
+++ clang/test/Driver/gcc-toolchain.cpp
@@ -24,7 +24,8 @@
 // CHECK: "{{[^"]*}}ld{{(.exe)?}}"
 // CHECK-SAME: "{{[^"]*}}/usr/lib/gcc/x86_64-linux-gnu/4.8{{/|\\\\}}crtbegin.o"
 // CHECK-SAME: "-L[[TOOLCHAIN]]/usr/lib/gcc/x86_64-linux-gnu/4.8"
-// CHECK-SAME: "-L[[TOOLCHAIN]]/usr/lib/gcc/x86_64-linux-gnu/4.8/../../../x86_64-linux-gnu"
+/// On x86_64, there is an extra usr/lib/gcc/x86_64-linux-gnu/4.8/../../../x86_64-linux-gnu but we should not test it.
+// CHECK-SAME: "-L[[TOOLCHAIN]]/usr/lib/gcc/x86_64-linux-gnu/4.8/../../.."
 
 /// Test we don't detect GCC installation under -B.
 // RUN: %clangxx -no-canonical-prefixes %s -### -o %t 2>&1 \
