0776eca7a4e76bfadc311f3607be3a4f0c0e989a
Andrew Litteken <andrew.litteken@gmail.com>
2021-03-20 18:03:02 -0500

    Revert "[IRSim] Adding basic implementation of llvm-sim."
    Causing build errors on the Windows Buildbots.
    
    This reverts commit 5155dff2784a47583d432d796b7cf47a0bed9f20.

--- llvm/test/lit.cfg.py
+++ llvm/test/lit.cfg.py
@@ -162,7 +162,7 @@ tools.extend([
     'llvm-link', 'llvm-lto', 'llvm-lto2', 'llvm-mc', 'llvm-mca',
     'llvm-modextract', 'llvm-nm', 'llvm-objcopy', 'llvm-objdump',
     'llvm-pdbutil', 'llvm-profdata', 'llvm-ranlib', 'llvm-rc', 'llvm-readelf',
-    'llvm-readobj', 'llvm-rtdyld', 'llvm-sim', 'llvm-size', 'llvm-split', 'llvm-strings',
+    'llvm-readobj', 'llvm-rtdyld', 'llvm-size', 'llvm-split', 'llvm-strings',
     'llvm-strip', 'llvm-tblgen', 'llvm-undname', 'llvm-c-test', 'llvm-cxxfilt',
     'llvm-xray', 'yaml2obj', 'obj2yaml', 'yaml-bench', 'verify-uselistorder',
     'bugpoint', 'llc', 'llvm-symbolizer', 'opt', 'sancov', 'sanstats'])
