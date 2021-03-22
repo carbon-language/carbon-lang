b32fe2b5142e835597a15eb06c59191c4cf51b54
Roman Lebedev <lebedev.ri@gmail.com>
2021-03-22 13:14:29 +0300

    [NFC][lit] Add a test showing that timing data for tests not executed is lost
    I.e. when you first run lit on a directory, and then on a single test,
    the timing knowledge about anything else other than that single test
    is lost. This isn't right.

--- llvm/utils/lit/tests/reorder.py
+++ llvm/utils/lit/tests/reorder.py
@@ -1,13 +1,22 @@
 ## Check that we can reorder test runs.
 
 # RUN: cp %{inputs}/reorder/.lit_test_times.txt %{inputs}/reorder/.lit_test_times.txt.orig
-# RUN: %{lit} -j1 %{inputs}/reorder | FileCheck %s
-# RUN: not diff %{inputs}/reorder/.lit_test_times.txt %{inputs}/reorder/.lit_test_times.txt.orig
+# RUN: %{lit} -j1 %{inputs}/reorder > %t.out
+# RUN: cp %{inputs}/reorder/.lit_test_times.txt %{inputs}/reorder/.lit_test_times.txt.new
 # RUN: cp %{inputs}/reorder/.lit_test_times.txt.orig %{inputs}/reorder/.lit_test_times.txt
+# RUN: not diff %{inputs}/reorder/.lit_test_times.txt.new %{inputs}/reorder/.lit_test_times.txt.orig
+# RUN: FileCheck --check-prefix=TIMES --implicit-check-not=not-executed.txt < %{inputs}/reorder/.lit_test_times.txt.new %s
+# RUN: FileCheck < %t.out %s
 # END.
 
-# CHECK:     -- Testing: 3 tests, 1 workers --
+# TIMES: subdir/ccc.txt
+# TIMES-NEXT: bbb.txt
+# TIMES-NEXT: aaa.txt
+# TIMES-NEXT: new-test.txt
+
+# CHECK:     -- Testing: 4 tests, 1 workers --
 # CHECK-NEXT: PASS: reorder :: subdir/ccc.txt
 # CHECK-NEXT: PASS: reorder :: bbb.txt
 # CHECK-NEXT: PASS: reorder :: aaa.txt
-# CHECK:     Passed: 3
+# CHECK-NEXT: PASS: reorder :: new-test.txt
+# CHECK:     Passed: 4
