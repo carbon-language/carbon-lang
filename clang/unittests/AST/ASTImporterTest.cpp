274907c0a4d6dbdc8815f9a37ea2e444bdfee528
Raphael Isemann <teemperor@gmail.com>
2021-03-23 13:55:44 +0100

    [ASTImporter] Split out Objective-C related unit tests
    This moves the two tests we have for importing Objective-C nodes to their own
    file. The motivation is that this means I can add more Objective-C tests without
    making the compilation time of ASTImporterTest even longer. Also it seems nice
    to separate the Apple-specific stuff from the ASTImporter test.
    
    Reviewed By: martong
    
    Differential Revision: https://reviews.llvm.org/D99162

--- clang/unittests/AST/ASTImporterTest.cpp
+++ clang/unittests/AST/ASTImporterTest.cpp
@@ -5615,59 +5615,6 @@ TEST_P(ASTImporterOptionSpecificTestBase, ImportDefaultConstructibleLambdas) {
             2u);
 }
 
-TEST_P(ASTImporterOptionSpecificTestBase, ImplicitlyDeclareSelf) {
-  Decl *FromTU = getTuDecl(R"(
-                           __attribute__((objc_root_class))
-                           @interface Root
-                           @end
-                           @interface C : Root
-                             -(void)method;
-                           @end
-                           @implementation C
-                             -(void)method {}
-                           @end
-                           )",
-                           Lang_OBJCXX, "input.mm");
-  auto *FromMethod = LastDeclMatcher<ObjCMethodDecl>().match(
-      FromTU, namedDecl(hasName("method")));
-  ASSERT_TRUE(FromMethod);
-  auto ToMethod = Import(FromMethod, Lang_OBJCXX);
-  ASSERT_TRUE(ToMethod);
-
-  // Both methods should have their implicit parameters.
-  EXPECT_TRUE(FromMethod->getSelfDecl() != nullptr);
-  EXPECT_TRUE(ToMethod->getSelfDecl() != nullptr);
-}
-
-TEST_P(ASTImporterOptionSpecificTestBase, ObjPropertyNameConflict) {
-  // Tests that properties that share the same name are correctly imported.
-  // This is only possible with one instance and one class property.
-  Decl *FromTU = getTuDecl(R"(
-                           @interface DupProp{}
-                           @property (class) int prop;
-                           @property int prop;
-                           @end
-                           )",
-                           Lang_OBJCXX, "input.mm");
-  auto *FromClass = FirstDeclMatcher<ObjCInterfaceDecl>().match(
-      FromTU, namedDecl(hasName("DupProp")));
-  auto ToClass = Import(FromClass, Lang_OBJCXX);
-  ASSERT_TRUE(ToClass);
-  // We should have one class and one instance property.
-  ASSERT_EQ(
-      1, std::distance(ToClass->classprop_begin(), ToClass->classprop_end()));
-  ASSERT_EQ(1,
-            std::distance(ToClass->instprop_begin(), ToClass->instprop_end()));
-  for (clang::ObjCPropertyDecl *prop : ToClass->properties()) {
-    // All properties should have a getter and a setter.
-    ASSERT_TRUE(prop->getGetterMethodDecl());
-    ASSERT_TRUE(prop->getSetterMethodDecl());
-    // The getters/setters should be able to find the right associated property.
-    ASSERT_EQ(prop->getGetterMethodDecl()->findPropertyDecl(), prop);
-    ASSERT_EQ(prop->getSetterMethodDecl()->findPropertyDecl(), prop);
-  }
-}
-
 struct ImportAutoFunctions : ASTImporterOptionSpecificTestBase {};
 
 TEST_P(ImportAutoFunctions, ReturnWithTypedefDeclaredInside) {
