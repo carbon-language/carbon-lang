dee5787d3ee5467b38f614c50023af0f7d1850d7
Vy Nguyen <vyng@google.com>
2021-03-23 21:27:58 -0400

    Reland [lld-macho][nfc] minor clean up, follow up to D98559
    This reverts commit 77b4230ed9bea541fd3fb04707e35308c2f34347.
    
    New change: Fixed tests on windows
    
         Differential Revision: https://reviews.llvm.org/D99210

--- lld/test/MachO/dependency-info.s
+++ lld/test/MachO/dependency-info.s
@@ -1,7 +1,4 @@
 # REQUIRES: x86
-## FIXME: Paths on windows have both `\` and '/', as a result, they are in a different
-## order when sorted. Maybe create a separate test for that?
-# UNSUPPORTED: system-windows
 #
 # RUN: rm -rf %t
 # RUN: split-file %s %t
@@ -16,16 +13,17 @@
 # RUN: %python %S/Inputs/DependencyDump.py %t/deps_info.out | FileCheck %s
 
 # CHECK: lld-version: {{.*}}LLD {{.*}}
-# CHECK-DAG: input-file: {{.*}}/bar.a
-# CHECK-DAG: input-file: {{.*}}/libfoo.dylib
-# CHECK-DAG: input-file: {{.*}}/libSystem.tbd
-# CHECK-DAG: input-file: {{.*}}/main.o
+
+# CHECK-DAG: input-file: {{.*}}{{[/\]}}bar.a
+# CHECK-DAG: input-file: {{.*}}{{[/\]}}libfoo.dylib
+# CHECK-DAG: input-file: {{.*}}{{[/\]}}libSystem.tbd
+# CHECK-DAG: input-file: {{.*}}{{[/\]}}main.o
 # CHECK-DAG: input-file: {{.*}}bar.o
 
-# CHECK-NEXT: not-found: {{.*}}/libdyld.dylib
+# CHECK-NEXT: not-found: {{.*}}{{[/\]}}libdyld.{{.*}}
 ## There could be more not-found here but we are not checking those because it's brittle.
 
-# CHECK: output-file: {{.*}}/test.out
+# CHECK: output-file: {{.*}}{{[/\]}}test.out
 
 #--- foo.s
 .globl __Z3foo
