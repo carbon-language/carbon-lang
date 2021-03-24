85cbfe75afa734b9e141fb11c8fd3c63e815616b
Max Kazantsev <mkazantsev@azul.com>
2021-03-24 11:03:21 +0700

    [NFC] Fix comment describing what EdgeBundles is
    The original comment says the same thing twice, and does not mention that
    edges entering the block are also in the same bundle (which seems true from
    what the underlying code is doing).
    
    Differential Revision: https://reviews.llvm.org/D99144
    Reviewed By: RKSimon

--- llvm/include/llvm/CodeGen/EdgeBundles.h
+++ llvm/include/llvm/CodeGen/EdgeBundles.h
@@ -8,7 +8,7 @@
 //
 // The EdgeBundles analysis forms equivalence classes of CFG edges such that all
 // edges leaving a machine basic block are in the same bundle, and all edges
-// leaving a basic block are in the same bundle.
+// entering a machine basic block are in the same bundle.
 //
 //===----------------------------------------------------------------------===//
 
