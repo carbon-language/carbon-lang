275df61f043ccf86a9c17957379bff9434da1489
Bing1 Yu <bing1.yu@intel.com>
2021-03-24 16:53:12 +0800

    [X86] Pass to transform tdpbsud&tdpbusd&tdpbuud intrinsics to scalar operation
    Reviewed By: pengfei
    
    Differential Revision: https://reviews.llvm.org/D99244

--- llvm/lib/Target/X86/X86LowerAMXIntrinsics.cpp
+++ llvm/lib/Target/X86/X86LowerAMXIntrinsics.cpp
@@ -73,6 +73,9 @@ private:
                                   Value *Ptr, Value *Stride, Value *Tile);
   template <Intrinsic::ID IntrID>
   typename std::enable_if<IntrID == Intrinsic::x86_tdpbssd_internal ||
+                              IntrID == Intrinsic::x86_tdpbsud_internal ||
+                              IntrID == Intrinsic::x86_tdpbusd_internal ||
+                              IntrID == Intrinsic::x86_tdpbuud_internal ||
                               IntrID == Intrinsic::x86_tdpbf16ps_internal,
                           Value *>::type
   createTileDPLoops(BasicBlock *Start, BasicBlock *End, IRBuilderBase &B,
@@ -82,6 +85,9 @@ private:
   bool lowerTileLoadStore(Instruction *TileLoadStore);
   template <Intrinsic::ID IntrID>
   typename std::enable_if<IntrID == Intrinsic::x86_tdpbssd_internal ||
+                              IntrID == Intrinsic::x86_tdpbsud_internal ||
+                              IntrID == Intrinsic::x86_tdpbusd_internal ||
+                              IntrID == Intrinsic::x86_tdpbuud_internal ||
                               IntrID == Intrinsic::x86_tdpbf16ps_internal,
                           bool>::type
   lowerTileDP(Instruction *TileDP);
@@ -223,14 +229,33 @@ Value *X86LowerAMXIntrinsics::createTileLoadStoreLoops(
 
 template <Intrinsic::ID IntrID>
 typename std::enable_if<IntrID == Intrinsic::x86_tdpbssd_internal ||
+                            IntrID == Intrinsic::x86_tdpbsud_internal ||
+                            IntrID == Intrinsic::x86_tdpbusd_internal ||
+                            IntrID == Intrinsic::x86_tdpbuud_internal ||
                             IntrID == Intrinsic::x86_tdpbf16ps_internal,
                         Value *>::type
 X86LowerAMXIntrinsics::createTileDPLoops(BasicBlock *Start, BasicBlock *End,
                                          IRBuilderBase &B, Value *Row,
                                          Value *Col, Value *K, Value *Acc,
                                          Value *LHS, Value *RHS) {
-  std::string IntrinName =
-      IntrID == Intrinsic::x86_tdpbssd_internal ? "tiledpbssd" : "tdpbf16ps";
+  std::string IntrinName;
+  switch (IntrID) {
+  case Intrinsic::x86_tdpbssd_internal:
+    IntrinName = "tiledpbssd";
+    break;
+  case Intrinsic::x86_tdpbsud_internal:
+    IntrinName = "tiledpbsud";
+    break;
+  case Intrinsic::x86_tdpbusd_internal:
+    IntrinName = "tiledpbusd";
+    break;
+  case Intrinsic::x86_tdpbuud_internal:
+    IntrinName = "tiledpbuud";
+    break;
+  case Intrinsic::x86_tdpbf16ps_internal:
+    IntrinName = "tiledpbf16ps";
+    break;
+  }
   Loop *RowLoop = nullptr;
   Loop *ColLoop = nullptr;
   Loop *InnerLoop = nullptr;
@@ -329,7 +354,7 @@ X86LowerAMXIntrinsics::createTileDPLoops(BasicBlock *Start, BasicBlock *End,
       B.CreateAdd(B.CreateMul(CurrentInner, B.getInt16(16)), CurrentCol);
   Value *NewVecC = nullptr;
 
-  if (IntrID == Intrinsic::x86_tdpbssd_internal) {
+  if (IntrID != Intrinsic::x86_tdpbf16ps_internal) {
     // tiledpbssd.scalarize.inner.body:
     // calculate idxa, idxb
     // %eltc = extractelement <256 x i32> %vec.c.inner.phi, i16 %idxc
@@ -351,12 +376,30 @@ X86LowerAMXIntrinsics::createTileDPLoops(BasicBlock *Start, BasicBlock *End,
     Value *SubVecA = B.CreateBitCast(EltA, V4I8Ty);
     Value *EltB = B.CreateExtractElement(VecB, IdxB);
     Value *SubVecB = B.CreateBitCast(EltB, V4I8Ty);
-    Value *SEXTSubVecB = B.CreateSExt(SubVecB, V4I32Ty);
-    Value *SEXTSubVecA = B.CreateSExt(SubVecA, V4I32Ty);
+    Value *SEXTSubVecB = nullptr;
+    Value *SEXTSubVecA = nullptr;
+    switch (IntrID) {
+    case Intrinsic::x86_tdpbssd_internal:
+      SEXTSubVecB = B.CreateSExt(SubVecB, V4I32Ty);
+      SEXTSubVecA = B.CreateSExt(SubVecA, V4I32Ty);
+      break;
+    case Intrinsic::x86_tdpbsud_internal:
+      SEXTSubVecB = B.CreateZExt(SubVecB, V4I32Ty);
+      SEXTSubVecA = B.CreateSExt(SubVecA, V4I32Ty);
+      break;
+    case Intrinsic::x86_tdpbusd_internal:
+      SEXTSubVecB = B.CreateSExt(SubVecB, V4I32Ty);
+      SEXTSubVecA = B.CreateZExt(SubVecA, V4I32Ty);
+      break;
+    case Intrinsic::x86_tdpbuud_internal:
+      SEXTSubVecB = B.CreateZExt(SubVecB, V4I32Ty);
+      SEXTSubVecA = B.CreateZExt(SubVecA, V4I32Ty);
+      break;
+    }
     Value *SubVecR = B.CreateAddReduce(B.CreateMul(SEXTSubVecA, SEXTSubVecB));
     Value *ResElt = B.CreateAdd(EltC, SubVecR);
     NewVecC = B.CreateInsertElement(VecCPhi, ResElt, IdxC);
-  } else if (IntrID == Intrinsic::x86_tdpbf16ps_internal) {
+  } else {
     // tiledpbf16ps.scalarize.inner.body:
     // calculate idxa, idxb, idxc
     // %eltc = extractelement <256 x i32> %vec.c.inner.phi, i16 %idxc
@@ -418,6 +461,9 @@ X86LowerAMXIntrinsics::createTileDPLoops(BasicBlock *Start, BasicBlock *End,
 
 template <Intrinsic::ID IntrID>
 typename std::enable_if<IntrID == Intrinsic::x86_tdpbssd_internal ||
+                            IntrID == Intrinsic::x86_tdpbsud_internal ||
+                            IntrID == Intrinsic::x86_tdpbusd_internal ||
+                            IntrID == Intrinsic::x86_tdpbuud_internal ||
                             IntrID == Intrinsic::x86_tdpbf16ps_internal,
                         bool>::type
 X86LowerAMXIntrinsics::lowerTileDP(Instruction *TileDP) {
@@ -527,6 +573,9 @@ bool X86LowerAMXIntrinsics::visit() {
       if (auto *Inst = dyn_cast<IntrinsicInst>(&*II++)) {
         switch (Inst->getIntrinsicID()) {
         case Intrinsic::x86_tdpbssd_internal:
+        case Intrinsic::x86_tdpbsud_internal:
+        case Intrinsic::x86_tdpbusd_internal:
+        case Intrinsic::x86_tdpbuud_internal:
         case Intrinsic::x86_tileloadd64_internal:
         case Intrinsic::x86_tilestored64_internal:
         case Intrinsic::x86_tilezero_internal:
@@ -544,6 +593,15 @@ bool X86LowerAMXIntrinsics::visit() {
     switch (Inst->getIntrinsicID()) {
     case Intrinsic::x86_tdpbssd_internal:
       C = lowerTileDP<Intrinsic::x86_tdpbssd_internal>(Inst) || C;
+      break;
+    case Intrinsic::x86_tdpbsud_internal:
+      C = lowerTileDP<Intrinsic::x86_tdpbsud_internal>(Inst) || C;
+      break;
+    case Intrinsic::x86_tdpbusd_internal:
+      C = lowerTileDP<Intrinsic::x86_tdpbusd_internal>(Inst) || C;
+      break;
+    case Intrinsic::x86_tdpbuud_internal:
+      C = lowerTileDP<Intrinsic::x86_tdpbuud_internal>(Inst) || C;
       break;
     case Intrinsic::x86_tdpbf16ps_internal:
       C = lowerTileDP<Intrinsic::x86_tdpbf16ps_internal>(Inst) || C;
