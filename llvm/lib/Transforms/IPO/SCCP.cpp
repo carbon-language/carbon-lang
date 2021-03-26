62c41cfba15e5e4f4ba667877f9718cddc7894e3
Chris Lattner <clattner@nondot.org>
2021-03-26 15:34:04 -0700

    Add a missing file header comment, NFC.

--- llvm/lib/Transforms/IPO/SCCP.cpp
+++ llvm/lib/Transforms/IPO/SCCP.cpp
@@ -1,3 +1,15 @@
+//===-- SCCP.cpp ----------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// This file implements Interprocedural Sparse Conditional Constant Propagation.
+//
+//===----------------------------------------------------------------------===//
+
 #include "llvm/Transforms/IPO/SCCP.h"
 #include "llvm/Analysis/AssumptionCache.h"
 #include "llvm/Analysis/PostDominators.h"
