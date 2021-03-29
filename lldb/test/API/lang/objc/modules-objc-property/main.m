69e7b7457809bac02f3e6f7d643c4fb516b31616
Raphael Isemann <teemperor@gmail.com>
2021-03-29 12:28:48 +0200

    [lldb][NFC] Add a test case for Objective-C properties with conflicting names
    This is an LLDB test for the ASTImporter crash that got fixed in D99077.
    The test is using Clang modules for the properties as it seems the conflicting
    names are not actually correctly handled when generating debug information
    (only the first property is emitted and the second one is ignored in the current
    clang ToT).

---
+++ lldb/test/API/lang/objc/modules-objc-property/main.m
@@ -0,0 +1,8 @@
+@import Foundation;
+@import myModule;
+
+int main() {
+    MyClass *m = [[MyClass alloc] init];
+    int i = m.propConflict + MyClass.propConflict;
+    return i; // Set breakpoint here.
+}
