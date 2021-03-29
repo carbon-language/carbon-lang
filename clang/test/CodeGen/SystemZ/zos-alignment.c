bd8dd580ffd221dd38e28c609b30d9b6361efac7
Fanbo Meng <fanbo.meng@ibm.com>
2021-03-29 16:47:57 -0400

    [NFC] clang-formatting zos-alignment.c
    Reviewed By: abhina.sreeskantharajan
    
    Differential Revision: https://reviews.llvm.org/D99514

--- clang/test/CodeGen/SystemZ/zos-alignment.c
+++ clang/test/CodeGen/SystemZ/zos-alignment.c
@@ -13,8 +13,8 @@ int f0() { return v0 + v1 + v2 + v3; }
 // DECL-NEXT: @v3 {{.*}} align 32
 
 const struct cs0 {
-  unsigned long   :0;
-  long long        a;
+  unsigned long : 0;
+  long long a;
 } CS0 = {};
 // CHECK:              0 | struct cs0
 // CHECK-NEXT:       0:- |   unsigned long
@@ -22,8 +22,8 @@ const struct cs0 {
 // CHECK-NEXT:           | [sizeof=8, align=8]
 
 volatile struct vs0 {
-  long            :0;
-  short           a;
+  long : 0;
+  short a;
 } VS0;
 // CHECK:              0 | struct vs0
 // CHECK-NEXT:       0:- |   long
@@ -31,11 +31,11 @@ volatile struct vs0 {
 // CHECK-NEXT:           | [sizeof=2, align=2]
 
 struct s0 {
-  short a:3;
-  long b:5;
-  int c:1;
-  long d:10;
-  char e:5;
+  short a : 3;
+  long b : 5;
+  int c : 1;
+  long d : 10;
+  char e : 5;
 } S0;
 // CHECK:              0 | struct s0
 // CHECK-NEXT:     0:0-2 |   short a
@@ -46,9 +46,9 @@ struct s0 {
 // CHECK-NEXT:           | [sizeof=3, align=1]
 
 struct s1 {
-  char a:7;
-  long b:27;
-  int c:2;
+  char a : 7;
+  long b : 27;
+  int c : 2;
 } S1;
 // CHECK:              0 | struct s1
 // CHECK-NEXT:     0:0-6 |   char a
@@ -57,10 +57,10 @@ struct s1 {
 // CHECK-NEXT:           | [sizeof=5, align=1]
 
 struct s2 {
-  char a:7;
-  char  :0;
-  short :0;
-  short :0;
+  char a : 7;
+  char : 0;
+  short : 0;
+  short : 0;
 } S2;
 // CHECK:              0 | struct s2
 // CHECK-NEXT:     0:0-6 |   char a
@@ -71,9 +71,9 @@ struct s2 {
 
 struct s3 {
   int a;
-  int b:16;
-  char  :0;
-  char c:1;
+  int b : 16;
+  char : 0;
+  char c : 1;
 } S3;
 // CHECK:              0 | struct s3
 // CHECK-NEXT:         0 |   int a
@@ -83,7 +83,7 @@ struct s3 {
 // CHECK-NEXT:           | [sizeof=12, align=4]
 
 struct s4 {
- unsigned int __attribute__((aligned(32))) a;
+  unsigned int __attribute__((aligned(32))) a;
 } S4;
 // CHECK:              0 | struct s4
 // CHECK-NEXT:         0 |   unsigned int a
@@ -91,10 +91,10 @@ struct s4 {
 
 struct s5 {
   char a;
-  int  b:19 __attribute__((aligned(4)));
-  int  c:22 __attribute__((aligned(8)));
-  int  :0;
-  int  d:10;
+  int b : 19 __attribute__((aligned(4)));
+  int c : 22 __attribute__((aligned(8)));
+  int : 0;
+  int d : 10;
 } S5;
 // CHECK:              0 | struct s5
 // CHECK-NEXT:         0 |   char a
@@ -105,8 +105,8 @@ struct s5 {
 // CHECK-NEXT:           | [sizeof=16, align=8]
 
 struct s6 {
-  char * a;
-  char * b[];
+  char *a;
+  char *b[];
 } S6;
 // CHECK:              0 | struct s6
 // CHECK-NEXT:         0 |   char * a
@@ -114,7 +114,7 @@ struct s6 {
 // CHECK-NEXT:           | [sizeof=8, align=8]
 
 struct s7 {
-  long  :0;
+  long : 0;
   short a;
 } S7;
 // CHECK:              0 | struct s7
@@ -124,8 +124,8 @@ struct s7 {
 
 #pragma pack(2)
 struct s8 {
-  unsigned long       :0;
-  long long           a;
+  unsigned long : 0;
+  long long a;
 } S8;
 #pragma pack()
 // CHECK:              0 | struct s8
@@ -134,8 +134,8 @@ struct s8 {
 // CHECK-NEXT:           | [sizeof=8, align=2]
 
 struct s9 {
-  unsigned int   :0;
-  unsigned short :0;
+  unsigned int : 0;
+  unsigned short : 0;
 } S9;
 // CHECK:              0 | struct s9
 // CHECK-NEXT:       0:- |   unsigned int
@@ -143,7 +143,7 @@ struct s9 {
 // CHECK-NEXT:           | [sizeof=0, align=1]
 
 struct s10 {
- unsigned int __attribute__((aligned)) a;
+  unsigned int __attribute__((aligned)) a;
 } S10;
 // CHECK:              0 | struct s10
 // CHECK-NEXT:         0 |   unsigned int a
@@ -151,7 +151,7 @@ struct s10 {
 
 struct s11 {
   char a;
-  long :0;
+  long : 0;
   char b;
 } S11;
 // CHECK:              0 | struct s11
@@ -161,9 +161,9 @@ struct s11 {
 // CHECK-NEXT:           | [sizeof=16, align=8]
 
 union u0 {
-  unsigned short     d1 __attribute__((packed));
-  int                d2:10;
-  long               d3;
+  unsigned short d1 __attribute__((packed));
+  int d2 : 10;
+  long d3;
 } U0 __attribute__((aligned(8)));
 // CHECK:              0 | union u0
 // CHECK-NEXT:         0 |   unsigned short d1
@@ -172,8 +172,8 @@ union u0 {
 // CHECK-NEXT:           | [sizeof=8, align=8]
 
 union u1 {
-  unsigned int        :0;
-  short               a;
+  unsigned int : 0;
+  short a;
 } U1;
 // CHECK:              0 | union u1
 // CHECK-NEXT:       0:- |   unsigned int
@@ -181,8 +181,8 @@ union u1 {
 // CHECK-NEXT:           | [sizeof=4, align=4]
 
 union u2 {
-  long      :0;
-  short      a;
+  long : 0;
+  short a;
 } U2;
 // CHECK:              0 | union u2
 // CHECK-NEXT:       0:- |   long
@@ -190,8 +190,8 @@ union u2 {
 // CHECK-NEXT:           | [sizeof=8, align=8]
 
 union u3 {
-  unsigned char :0;
-  unsigned short :0;
+  unsigned char : 0;
+  unsigned short : 0;
 } U3;
 // CHECK:              0 | union u3
 // CHECK-NEXT:       0:- |   unsigned char
