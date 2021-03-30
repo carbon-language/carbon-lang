1cbba533ec93864caab8ad2f3fd4293a56723307
Raphael Isemann <teemperor@gmail.com>
2021-03-30 11:07:04 +0200

    [ObjC][CodeGen] Fix missing debug info in situations where an instance and class property have the same identifier
    Since the introduction of class properties in Objective-C it is possible to declare a class and an instance
    property with the same identifier in an interface/protocol.
    
    Right now Clang just generates debug information for whatever property comes first in the source file.
    The second property is ignored as it's filtered out by the set of already emitted properties (which is just
    using the identifier of the property to check for equivalence).  I don't think generating debug info in this case
    was never supported as the identifier filter is in place since 7123bca7fb6e1dde51be8329cfb523d2bb9ffadf
    (which precedes the introduction of class properties).
    
    This patch expands the filter to take in account identifier + whether the property is class/instance. This
    ensures that both properties are emitted in this special situation.
    
    Reviewed By: aprantl
    
    Differential Revision: https://reviews.llvm.org/D99512

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -2740,16 +2740,26 @@ llvm::DIType *CGDebugInfo::CreateTypeDefinition(const ObjCInterfaceType *Ty,
     EltTys.push_back(PropertyNode);
   };
   {
-    llvm::SmallPtrSet<const IdentifierInfo *, 16> PropertySet;
+    // Use 'char' for the isClassProperty bit as DenseSet requires space for
+    // empty/tombstone keys in the data type (and bool is too small for that).
+    typedef std::pair<char, const IdentifierInfo *> IsClassAndIdent;
+    /// List of already emitted properties. Two distinct class and instance
+    /// properties can share the same identifier (but not two instance
+    /// properties or two class properties).
+    llvm::DenseSet<IsClassAndIdent> PropertySet;
+    /// Returns the IsClassAndIdent key for the given property.
+    auto GetIsClassAndIdent = [](const ObjCPropertyDecl *PD) {
+      return std::make_pair(PD->isClassProperty(), PD->getIdentifier());
+    };
     for (const ObjCCategoryDecl *ClassExt : ID->known_extensions())
       for (auto *PD : ClassExt->properties()) {
-        PropertySet.insert(PD->getIdentifier());
+        PropertySet.insert(GetIsClassAndIdent(PD));
         AddProperty(PD);
       }
     for (const auto *PD : ID->properties()) {
       // Don't emit duplicate metadata for properties that were already in a
       // class extension.
-      if (!PropertySet.insert(PD->getIdentifier()).second)
+      if (!PropertySet.insert(GetIsClassAndIdent(PD)).second)
         continue;
       AddProperty(PD);
     }
