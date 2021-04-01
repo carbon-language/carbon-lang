846a530e7db28bdd50c9a9fb08cddc210134b579
Arnold Schwaighofer <aschwaighofer@apple.com>
2021-04-01 15:51:56 -0700

    Fix coro lowering of single predecessor phis
    Code assumes that uses of single predecessor phis are not live accross
    suspend points. Cleanup any single predecessor phis preceeding the code
    making this assumption.
    
    rdar://76020301
    
    Differential Revision: https://reviews.llvm.org/D105488

--- llvm/lib/Transforms/Coroutines/CoroFrame.cpp
+++ llvm/lib/Transforms/Coroutines/CoroFrame.cpp
@@ -1839,6 +1839,24 @@ static void rewritePHIsForCleanupPad(BasicBlock *CleanupPadBB,
   }
 }
 
+static void cleanupSinglePredPHIs(Function &F) {
+  SmallVector<PHINode *, 32> Worklist;
+  for (auto &BB : F) {
+    for (auto &Phi : BB.phis()) {
+      if (Phi.getNumIncomingValues() == 1) {
+        Worklist.push_back(&Phi);
+      } else
+        break;
+    }
+  }
+  while (!Worklist.empty()) {
+    auto *Phi = Worklist.back();
+    Worklist.pop_back();
+    auto *OriginalValue = Phi->getIncomingValue(0);
+    Phi->replaceAllUsesWith(OriginalValue);
+  }
+}
+
 static void rewritePHIs(BasicBlock &BB) {
   // For every incoming edge we will create a block holding all
   // incoming values in a single PHI nodes.
@@ -2587,6 +2605,10 @@ void coro::buildCoroutineFrame(Function &F, Shape &Shape) {
       splitAround(Call, "MustTailCall.Before.CoroEnd");
     }
   }
+
+  // Later code makes structural assumptions about single predecessors phis e.g
+  // that they are not live accross a suspend point.
+  cleanupSinglePredPHIs(F);
 
   // Transforms multi-edge PHI Nodes, so that any value feeding into a PHI will
   // never has its definition separated from the PHI by the suspend point.
