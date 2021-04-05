e5411854f36ecddf39acf946151f1ba01ff83808
Dave Abrahams <dabrahams@google.com>
2021-04-05 15:54:56 -0700

    Finish Tokenize commenting

--- Sources/Tokenize.swift
+++ Sources/Tokenize.swift
@@ -50,22 +50,25 @@ fileprivate let keywords: [String: TokenID] = [
 ]
 
 /// A mapping from regular expression pattern to either a coresponding token ID,
-/// or `nil` if the pattern is to be discarded.
+/// or `nil` if the pattern is to be discarded (e.g. for whitespace).
 fileprivate let patterns: [String: TokenID?] = [
   #"[A-Za-z_][A-Za-z0-9_]*"#: .Identifier,
   #"[0-9]+"#: .Integer_literal,
-  // 1-line comment: "//" followed by any number of non-newlines (See
+  
+  // "//" followed by any number of non-newlines (See
   // https://unicode-org.github.io/icu/userguide/strings/regexp.html
   //   #regular-expression-metacharacters
   // and https://www.unicode.org/reports/tr44/#BC_Values_Table).
-  #"//\P{Bidi_Class=B}*"#: nil,
-  #"\s+"#: nil
+  #"//\P{Bidi_Class=B}*"#: nil, // 1-line comment
+
+  #"\s+"#: nil // whitespace
 ]
 
 // ===== Lexical Analyzer Implementation ======
 
 /// A single regex pattern with alternatives for all the keywords
 fileprivate let keywordPattern = keywords.keys
+  // Put the longest ones first because regexps match alternatives eagerly.
   .sorted { $0.count > $1.count }
   .lazy.map { NSRegularExpression.escapedPattern(for: $0) }
   .joined(separator: "|")
@@ -76,7 +79,7 @@ fileprivate let keywordPattern = keywords.keys
 /// this list is treated specially.
 fileprivate let allPatterns = [(keywordPattern, nil)] + patterns
 
-/// A version of allPatterns with compiled regular expressions.
+/// A version of allPatterns with the patterns compiled to regexp matchers.
 fileprivate let matchers = allPatterns.map {
   try! (matcher: NSRegularExpression(pattern: $0, options: []), nonterminal: $1)
 }
@@ -93,18 +96,27 @@ extension String {
   }
 }
 
+/// The sequence of tokens in a source file.
+///
+/// An .ILLEGAL_CHARACTER token is produced for each character that isn't
+/// otherwise recognized.
 struct Tokens: Sequence {
+  /// Creates an instance that extracts the tokens from `sourceText`, labeling
+  /// each as having come from the given source file.
   init(in sourceText: String, from sourceFileName: String) {
     self.sourceText = sourceText
     self.sourceFileName = sourceFileName
   }
-  
+
+  /// Returns a new iteration state.
   func makeIterator() -> Iterator {
     .init(over: sourceText, from: sourceFileName)
   }
 
-  /// The token streams's iteration state and producer.
+  /// The token stream's iteration state and element producer.
   struct Iterator: IteratorProtocol {
+    /// Creates an instance producing the tokens from `sourceText`, labeling
+    /// each as having come from the given source file.
     init(over sourceText: String, from sourceFileName: String) {
       self.sourceText = sourceText
       textPosition = sourceText.startIndex
@@ -112,62 +124,80 @@ struct Tokens: Sequence {
       self.sourceFileName = sourceFileName
     }
 
+    /// Returns the next token in the source, or `nil` if the source is
+    /// exhausted.
     mutating func next() -> AST<Token>? {
-      while true {
+      // Repeat until a non-ignored pattern is matched.
+      while utf16Offset < sourceUTF16Length {
+        // NSRegularExpression matching region
         let remainingUTF16 = NSRange(
           location: utf16Offset, length: sourceUTF16Length - utf16Offset)
-        
-        if remainingUTF16.length == 0 { return nil }
-        
+
+        // UTF16 lengths matched by each matcher
         let matchUTF16Lengths = matchers.lazy.map { [sourceText] in
           $0.matcher.firstMatch(
             in: sourceText, options: .anchored, range: remainingUTF16
           )?.range.length ?? 0
         }
 
+        // Choose the longest matcher.
         let (bestMatchIndex, bestMatchUTF16Length)
-          = matchUTF16Lengths.enumerated().max(by: { $0.1 < $1.1 })!
+          = matchUTF16Lengths.enumerated().max(by: { $0.element < $1.element })!
 
         let tokenStart = textPosition
+        let remainingText = sourceText[tokenStart...]
         
+        // Advance past the recognized text, or the first character if nothing
+        // matched.
         textPosition = bestMatchUTF16Length == 0
-          ? sourceText[textPosition...].dropFirst().startIndex
-          : sourceText[textPosition...].utf16
-            .dropFirst(bestMatchUTF16Length).startIndex
+          ? remainingText.dropFirst(1).startIndex
+          : remainingText.utf16.dropFirst(bestMatchUTF16Length).startIndex
 
-        let tokenText = sourceText[tokenStart..<textPosition]
+        let tokenText = remainingText[..<textPosition]
         utf16Offset += tokenText.utf16.count
 
-        let tokenLocationStart = sourcePosition
+        let tokenRegionStart = sourcePosition
+        
+        // Adjust human-readable source position
         let tokenLines = tokenText.split(
           omittingEmptySubsequences: false, whereSeparator: \.isNewline)
         let newlineCount = tokenLines.count - 1
-        
         sourcePosition.line += newlineCount
         sourcePosition.column
           = (newlineCount == 0 ? sourcePosition.column : 1)
           + tokenLines.last!.count
 
-        let text = String(tokenText)
-        if let matchedKind = bestMatchUTF16Length == 0 ? .ILLEGAL_CHARACTER
-             : bestMatchIndex == 0 ? keywords[text]
+        let matchedText = String(tokenText)
+        if let matchedID = bestMatchUTF16Length == 0 ? .ILLEGAL_CHARACTER
+             : bestMatchIndex == 0 ? keywords[matchedText]
              : matchers[bestMatchIndex].nonterminal
         {
           return AST(
-            .init(matchedKind, text),
+            Token(matchedID, matchedText),
             SourceRegion(
-              fileName: sourceFileName,
-              tokenLocationStart..<sourcePosition))
+              fileName: sourceFileName, tokenRegionStart..<sourcePosition))
         }
       }
+      return nil
     }
+    
+    /// The complete text being matched
     private let sourceText: String
+    /// The name of the file embedded in each token's source region.
     private let sourceFileName: String
-    private var sourcePosition = SourcePosition.start
-    private var textPosition: String.Index
-    private var utf16Offset: Int = 0
+    /// The number of UTF-16 code units in `sourceText`.
     private let sourceUTF16Length: Int
+    
+    /// Where scanning for the next token will resume (human-readable form).
+    private var sourcePosition = SourcePosition.start
+    /// Where scanning for the next token will resume (string form).
+    private var textPosition: String.Index
+    /// Where scanning for the next token will resume (NSRegularExpression form).
+    private var utf16Offset: Int = 0
   }
+  
+  /// The complete text from which `self`'s tokens will be derived.
   private let sourceText: String
+    /// The name of the file embedded in each token's source region.
   private let sourceFileName: String
 }
