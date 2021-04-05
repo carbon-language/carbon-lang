5f0ac1ef78880f233dea2070ab55894c6bd2690c
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2021-04-05 13:01:44 -0700

    Set IgnoreLLVMUsed to false in CallGraph::addToCallGraph()
    clang++ uses llvm.compiler.used in certain cases to preserve
    symbol which is fully inlined. D96087 has resulted in undefined
    symbols in such cases. Set it to false by default to preserve
    old behavior but keep the option for specific uses where we
    want to ignore these (e.g. to detect a potential indirect call
    to a function).
    
    Differential Revision: https://reviews.llvm.org/D99897

--- llvm/lib/Analysis/CallGraph.cpp
+++ llvm/lib/Analysis/CallGraph.cpp
@@ -82,7 +82,7 @@ void CallGraph::addToCallGraph(Function *F) {
   if (!F->hasLocalLinkage() ||
       F->hasAddressTaken(nullptr, /*IgnoreCallbackUses=*/true,
                          /* IgnoreAssumeLikeCalls */ true,
-                         /* IgnoreLLVMUsed */ true))
+                         /* IgnoreLLVMUsed */ false))
     ExternalCallingNode->addCalledFunction(nullptr, Node);
 
   populateCallGraphNode(Node);
