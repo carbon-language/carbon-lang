3b2d3b96647815c3282689ddd20ea9ed0d8ff985
Dave Abrahams <dabrahams@google.com>
2021-04-05 20:31:37 -0700

    Comments for Token.swift

--- Sources/Token.swift
+++ Sources/Token.swift
@@ -8,19 +8,28 @@
 ///   be implied (e.g. for keywords, which only match one string.)
 typealias TokenID = CarbonParser.CitronTokenCode
 
+/// A symbol recognized by the lexical analyzer.
+typealias Token = AST<Token_>
+
+/// The body of a `Token`.
 struct Token_: Hashable {
-  init(_ kind: TokenID, _ text: String) {
+  /// Creates an instance of the given token kind and content.
+  init(_ kind: TokenID, _ content: String) {
     self.kind = kind
-    self.text = text
+    self.text = content
   }
+
+  /// The lexical analyzer pattern that matched this token.
   let kind: TokenID
+
+  /// The textual content of this token.
   let text: String
 }
 
 extension Token_: CustomStringConvertible {
+  /// A textual description of `self`.
   var description: String {
     "Token_(.\(kind), \(String(reflecting: text)))"
   }
 }
 
-typealias Token = AST<Token_>
