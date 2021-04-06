7dd8d1b20a6e209ba5557e5c8e64fd380600b1f9
Dave Abrahams <dabrahams@google.com>
2021-04-05 20:24:19 -0700

    Fix a comment

--- Sources/Token.swift
+++ Sources/Token.swift
@@ -2,7 +2,7 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-/// A token as recognized by the lexical analyzer.
+/// Identifies a textual pattern matched by the lexical analyzer.
 ///
 /// - Note: TokenID does not include the token's text, except inasmuch as it may
 ///   be implied (e.g. for keywords, which only match one string.)
