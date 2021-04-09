36b932d6a385bb97efe17818a7a47d29d2d8acf3
Evgeniy Brevnov <ybrevnov@azul.com>
2021-04-09 15:41:27 +0700

    [NARY] Don't optimize min/max if there are side uses
    Say we have
    %1=min(%a,%b)
    %2=min(%b,%c)
    %3=min(%2,%a)
    
    The optimization will try to reassociate the later one so that we can rewrite it to %3=min(%1, %c) and remove %2.
    But if %2 has another uses outside of %3 then we can't remove %2 and end up with:
    
    %1=min(%a,%b)
    %2=min(%b,%c)
    %3=min(%1, %c)
    
    This doesn't harm by itself except it is not profitable and changes IR for no good reason.
    What is bad it triggers next iteration which finds out that optimization is applicable to %2 and %3 and generates:
    
    %1=min(%a,%b)
    %2=min(%b,%c)
    %3=min(%1,%c)
    %4=min(%2,%a)
    
    and so on...
    
    The solution is to prevent optimization in the first place if intermediate result (%2) has side uses and
    known to be not removed.
    
    Reviewed By: mkazantsev
    
    Differential Revision: https://reviews.llvm.org/D100170

--- llvm/lib/Transforms/Scalar/NaryReassociate.cpp
+++ llvm/lib/Transforms/Scalar/NaryReassociate.cpp
@@ -592,7 +592,7 @@ Value *NaryReassociatePass::tryReassociateMinOrMax(Instruction *I,
   Value *A = nullptr, *B = nullptr;
   MaxMinT m_MaxMin(m_Value(A), m_Value(B));
   for (unsigned int i = 0; i < 2; ++i) {
-    if (match(LHS, m_MaxMin)) {
+    if (LHS->getNumUses() <= 2 && match(LHS, m_MaxMin)) {
       const SCEV *AExpr = SE->getSCEV(A), *BExpr = SE->getSCEV(B);
       const SCEV *RHSExpr = SE->getSCEV(RHS);
       for (unsigned int j = 0; j < 2; ++j) {
@@ -609,6 +609,12 @@ Value *NaryReassociatePass::tryReassociateMinOrMax(Instruction *I,
           // iteration.
           std::swap(AExpr, RHSExpr);
         }
+
+        // The optimization is profitable only if LHS can be removed in the end.
+        // In other words LHS should be used (directly or indirectly) by I only.
+        for (User *U : LHS->users())
+          if (U != I || !(U->hasOneUser() && *U->users().begin() == I))
+            continue;
 
         SCEVExpander Expander(*SE, *DL, "nary-reassociate");
         SmallVector<const SCEV *, 2> Ops1{ BExpr, AExpr };
