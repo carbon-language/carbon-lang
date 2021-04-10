58dad2e9b9329f3201d76d959552f94c11a3c311
Dave Abrahams <dabrahams@google.com>
2021-04-09 18:02:42 -0700

    Add a simple Stack.

---
+++ Sources/Stack.swift
@@ -0,0 +1,35 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+/// A last-in, first-out (LIFO) queue of T.
+struct Stack<T> {
+  /// Accesses the top element.
+  ///
+  /// - Requires: !isEmpty
+  var top: T {
+    get { elements.last! }
+    _modify { yield &elements[elements.count - 1] }
+    set { elements[elements.count - 1] = newValue }
+  }
+
+  /// The number of elements.
+  var count: Int { elements.count }
+
+  /// True iff `count == 0`.
+  var isEmpty: Bool { elements.isEmpty }
+
+  /// Pushes `x` onto the top of `self`.
+  mutating func push(_ x: T) { elements.append(x) }
+
+  /// Removes and returns the top element, or returns `nil` if `isEmpty`.
+  mutating func pop() -> T? { elements.popLast() }
+
+  /// Removes the top `n` elements.
+  ///
+  /// - Requires: `count >= n`.
+  mutating func pop(_ n: Int) { elements.removeLast(n) }
+
+  /// The elements, starting with the bottom element and ending with the top one.
+  private(set) var elements: [T]
+}
