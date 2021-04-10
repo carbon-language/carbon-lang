4da58275990c7c2a2bde33dda0bf8c947c0c4d0c
Dave Abrahams <dabrahams@google.com>
2021-04-10 15:06:27 -0700

    Interpreter WIP

--- Sources/Interpreter.swift
+++ Sources/Interpreter.swift
@@ -3,25 +3,35 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 protocol Action {
-  /// Updates the interpreter state and optionally returning an action to be
-  /// pushed onto its todo stack.
-  func run(on i: inout Interpreter) -> Action?
+  /// Updates the interpreter state, optionally returning an action to be
+  /// executed as a subpart of this action.
+  ///
+  /// If the result is non-nil, `self` will be run again after the resulting
+  /// action is completed.
+  mutating func run(on i: inout Interpreter) -> Action?
 }
 
+/// The address of a (global or local) symbol, or local temporary, with respect
+/// to the currently-executing function context.
 struct RelativeAddress: Hashable {
-  /// The lexical function nesting level of the function whose frame spans
-  /// the indicated address.
-  ///
-  /// 0 indicates that the address is a global variable, 1 indicates a top-level
-  /// function, 2 indicates a function or lambda nested in a top-level function,
-  /// etc.
-  var lexicalFrame: Int
-  /// The offset of the indicated address from the lexical frame's base address.
-  var offsetInFrame: Int
+  init(_ base: Context, _ offset: Int) {
+    self.base = base
+    self.offset = offset
+  }
 
+  /// The kind of lookup we are doing.
+  enum Context {
+    case global // The address of a global symbol.
+    case local  // The address of a parameter, local variable, or temporary.
+    // expect to add "this-relative" for lambdas and method access.
+  }
+  
+  let base: Context
+  let offset: Int
+  
   /// Returns the absolute address corresponding to `self` in `i`.
   func resolved(in i: Interpreter) -> Address {
-    return lexicalFrameBase[lexicalFrame] + offsetInFrame
+    (base == .global ? 0 : i.frameBase) + offset
   }
 }
 
@@ -36,36 +46,32 @@ struct Interpreter {
   /// Progress one step forward in the execution sequence, returning an exit
   /// code if the program terminated.
   mutating func step() -> ExitCode? {
-    // pop the top action, run it on `self`, and push any result that returns.
+    // pop the top action and run it on `self`
     if let e = earlyExit { return e }
-    guard let a = todo.pop() else { return 0 }
+    guard var a = todo.pop() else { return 0 }
     if let b = a.run(on: &self) {
+      // If a result was returned, keep running `a` but run `b` first.
+      todo.push(a)
       todo.push(b)
     }
     return nil
   }
 
-  /// The first address allocated to each lexical frame.
-  ///
-  /// This is not a call stack; it has one entry for the current lexical scope,
-  /// and one for each enclosing The frame address of the currently-executing
-  /// function is always `lexicalFrameAddress.last!`.  The frame where globals
-  /// live is always `lexicalFrameAddress[0]`; expect that value to be zero.
-  /// Intermediate lexical frames are represented by the chain of function
-  /// scopes inside which the current function scope is nested (for lambdas and
-  /// other nested functions).
-  var lexicalFrameBase: Stack<Address>
+  /// The address in memory of the first address allocated to the
+  /// currently-executing function's frame.
+  var frameBase: Address
 
   /// The stack of pending actions.
   private var todo: Stack<Action>
 }
 
 struct CallFunction: Action {
-  let callee: FunctionDefinition
+  let callee: Expression
+  let arguments: TupleLiteral
 
-  /// Updates the interpreter state and optionally returning an action to be
+  /// Updates the interpreter state, optionally returning an action to be
   /// pushed onto its todo stack.
   func run(on i: inout Interpreter) -> Action? {
-    if i.program.nestingLevel[callee] == i.
+    return nil
   }
 }
