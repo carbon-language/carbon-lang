13015ebd6f568f98e9b908564195f03f8f0d02a9
Stanislav Mekhanoshin <Stanislav.Mekhanoshin@amd.com>
2021-04-13 12:48:53 -0700

    [AMDGPU] Factor out predicate FmaakFmamkF32Insts
    Differential Revision: https://reviews.llvm.org/D100409

--- llvm/lib/Target/AMDGPU/GCNSubtarget.h
+++ llvm/lib/Target/AMDGPU/GCNSubtarget.h
@@ -838,6 +838,10 @@ public:
     return HasPackedFP32Ops;
   }
 
+  bool hasFmaakFmamkF32Insts() const {
+    return getGeneration() >= GFX10;
+  }
+
   bool hasExtendedImageInsts() const {
     return HasExtendedImageInsts;
   }
