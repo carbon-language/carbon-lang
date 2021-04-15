aa80955f638f26360a1a137d69f61d5ac4970295
Zequan Wu <zequanwu@google.com>
2021-04-15 16:23:33 -0700

    [lld-link] Warn on exported deleting dtor
    MSVC linker has this [[ https://docs.microsoft.com/en-us/cpp/error-messages/tool-errors/linker-tools-warning-lnk4102?view=msvc-160 | warning]], so lld-link should also warn on this.
    
    Differential Revision: https://reviews.llvm.org/D100606

--- lld/COFF/Writer.cpp
+++ lld/COFF/Writer.cpp
@@ -1078,6 +1078,10 @@ void Writer::createExportTable() {
     edataStart = edataSec->chunks.front();
     edataEnd = edataSec->chunks.back();
   }
+  // Warn on exported deleting destructor.
+  for (auto e : config->exports)
+    if (e.sym && e.sym->getName().startswith("??_G"))
+      warn("export of deleting dtor: " + toString(*e.sym));
 }
 
 void Writer::removeUnusedSections() {
