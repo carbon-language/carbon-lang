c2e452fb05a714a3826275f038d201f87e9ba2d5
Andrzej Warzynski <andrzej.warzynski@arm.com>
2021-04-16 15:23:47 +0000

    [flang][nfc] Port 2 tests to use the new driver when enabled
    This is similar to https://reviews.llvm.org/D100309, i.e. `%f18` is
    replaced with `%flang_new`.
    
    resolve105.f90 wasn't in tree when D100309 was worked on, so it's
    updated here instead.
    
    label14.f90 requires `-fsyntax-only`. I didn't notice that when
    submitting D100309, hence updating it now instead. `-fsyntax-only` is
    required to prevent `%f18` from calling an external compiler (which then
    fails and returns a non-zero exit code).
    
    Differential Revision: https://reviews.llvm.org/D100655

--- flang/test/Semantics/label14.f90
+++ flang/test/Semantics/label14.f90
@@ -2,7 +2,7 @@
 ! 11.1.4 - 4 It is permissible to branch to and end-block-stmt only withinh its
 !            Block Construct
 
-! RUN: not %f18 %s 2>&1 | FileCheck %s
+! RUN: %flang_fc1 -fsyntax-only %s 2>&1 | FileCheck %s
 ! CHECK: Label '20' is not in scope
 
 subroutine s1
