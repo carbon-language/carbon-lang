369c0e0f48dd3585566d251cc99955dbdeed84a1
Wael Yehia <wyehia@ca.ibm.com>
2021-04-19 16:01:19 +0000

    [AIX] Diagnose thinLTO usage in clang on AIX.
    Reviewed By: Xiangling Liao
    
    Differential Revision: https://reviews.llvm.org/D100350

--- clang/lib/Driver/Driver.cpp
+++ clang/lib/Driver/Driver.cpp
@@ -3998,10 +3998,13 @@ void Driver::BuildJobs(Compilation &C) const {
   }
 
   const llvm::Triple &RawTriple = C.getDefaultToolChain().getTriple();
-  if (RawTriple.isOSAIX())
+  if (RawTriple.isOSAIX()) {
     if (Arg *A = C.getArgs().getLastArg(options::OPT_G))
       Diag(diag::err_drv_unsupported_opt_for_target)
           << A->getSpelling() << RawTriple.str();
+    if (LTOMode == LTOK_Thin)
+      Diag(diag::err_drv_clang_unsupported) << "thinLTO on AIX";
+  }
 
   // Collect the list of architectures.
   llvm::StringSet<> ArchNames;
