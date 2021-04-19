00764c36edf88ae9806e8d57a6addb782e6ceae8
Raphael Isemann <teemperor@gmail.com>
2021-04-19 15:50:10 +0200

    [lldb] Add support for evaluating expressions in static member functions
    At the moment the expression parser doesn't support evaluating expressions in
    static member functions and just pretends the expression is evaluated within a
    non-member function. This causes that all static members are inaccessible when
    doing unqualified name lookup.
    
    This patch adds support for evaluating in static member functions. It
    essentially just does the same setup as what LLDB is already doing for
    non-static member functions (i.e., wrapping the expression in a fake member
    function) with the difference that we now mark the wrapping function as static
    (to prevent access to non-static members).
    
    Reviewed By: shafik, jarin
    
    Differential Revision: https://reviews.llvm.org/D81550

--- lldb/source/Plugins/ExpressionParser/Clang/ClangExpressionDeclMap.h
+++ lldb/source/Plugins/ExpressionParser/Clang/ClangExpressionDeclMap.h
@@ -608,8 +608,13 @@ private:
   ///
   /// \param[in] type
   ///     The type of the class that serves as the evaluation context.
-  void AddContextClassType(NameSearchContext &context,
-                           const TypeFromUser &type);
+  ///
+  /// \param[in] context_method
+  ///     The member function declaration in which the expression is being
+  ///     evaluated or null if the expression is not evaluated in the context
+  ///     of a member function.
+  void AddContextClassType(NameSearchContext &context, const TypeFromUser &type,
+                           clang::CXXMethodDecl *context_method = nullptr);
 
   /// Move a type out of the current ASTContext into another, but make sure to
   /// export all components of the type also.
