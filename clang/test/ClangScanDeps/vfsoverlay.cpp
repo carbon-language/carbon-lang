199c39748292cbc89cd148a0d8364ebb1014ec38
Alexandre Ganea <alexandre.ganea@ubisoft.com>
2021-04-19 17:43:24 -0400

    Revert "[clang-scan-deps] Add support for clang-cl"
    This reverts commit bb26fa8c286bf524ed9235c3e293ad22ecf3e984.

--- clang/test/ClangScanDeps/vfsoverlay.cpp
+++ clang/test/ClangScanDeps/vfsoverlay.cpp
@@ -2,7 +2,6 @@
 // RUN: rm -rf %t.cdb
 // RUN: mkdir -p %t.dir
 // RUN: cp %s %t.dir/vfsoverlay_input.cpp
-// RUN: cp %s %t.dir/vfsoverlay_input_clangcl.cpp
 // RUN: sed -e "s|DIR|%/t.dir|g" %S/Inputs/vfsoverlay.yaml > %t.dir/vfsoverlay.yaml
 // RUN: mkdir %t.dir/Inputs
 // RUN: cp %S/Inputs/header.h %t.dir/Inputs/header.h
@@ -15,8 +14,4 @@
 
 // CHECK: vfsoverlay_input.o
 // CHECK-NEXT: vfsoverlay_input.cpp
-// CHECK-NEXT: Inputs{{/|\\}}header.h
-
-// CHECK: vfsoverlay_input_clangcl.o
-// CHECK-NEXT: vfsoverlay_input_clangcl.cpp
 // CHECK-NEXT: Inputs{{/|\\}}header.h
