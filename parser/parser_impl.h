53c1580db2dcdfe7c326d5af816c8703891ae550
Richard Smith <richard@metafoo.co.uk>
2021-04-21 16:33:05 -0700

    Add convenience functions to inspect the kind of the next token.

--- parser/parser_impl.h
+++ parser/parser_impl.h
@@ -33,6 +33,20 @@ class ParseTree::Parser {
     return tokens.GetKind(*position) == TokenKind::EndOfFile();
   }
 
+  // Gets the kind of the next token to be consumed.
+  auto NextTokenKind() const -> TokenKind { return tokens.GetKind(*position); }
+
+  // Tests whether the next token to be consumed is of the specified kind.
+  auto NextTokenIs(TokenKind kind) const -> bool {
+    return NextTokenKind() == kind;
+  }
+
+  // Tests whether the next token to be consumed is of any of the specified
+  // kinds.
+  auto NextTokenIsOneOf(std::initializer_list<TokenKind> kinds) const -> bool {
+    return NextTokenKind().IsOneOf(kinds);
+  }
+
   // Requires (and asserts) that the current position matches the provide
   // `Kind`. Returns the current token and advances to the next position.
   auto Consume(TokenKind kind) -> TokenizedBuffer::Token;
