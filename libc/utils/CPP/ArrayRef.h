fa404ae43a6f7ed256a47257aeccc760d16dc0c9
Guillaume Chatelet <gchatelet@google.com>
2021-04-21 13:25:24 +0000

    [libc] Enhance ArrayRef + unittests
    This patch mostly adds unittests for `ArrayRef` and `MutableArrayRef`, additionnaly:
     - We mimic the behavior of `std::vector` and disallow CV qualified type (`ArrayRef<const X>` is not allowed).
       This is to make sure that the type traits are always valid (e.g. `value_type`, `pointer`, ...).
     - In the previous implementation `ArrayRef` would define `value_type` as `const T` but this is not correct, it should be `T` for both `MutableArrayRef` and `ArrayRef`.
     - We add the `equals` method to ease testing,
     - We define the constructor taking an `Array` outside of the base implementation to ensure we match `const Array<T>&` and not `Array<const T>&` in the case of `ArrayRef`.
    
    Differential Revision: https://reviews.llvm.org/D100732

--- libc/utils/CPP/ArrayRef.h
+++ libc/utils/CPP/ArrayRef.h
@@ -10,6 +10,7 @@
 #define LLVM_LIBC_UTILS_CPP_ARRAYREF_H
 
 #include "Array.h"
+#include "TypeTraits.h" //RemoveCVType
 
 #include <stddef.h> // For size_t.
 
@@ -21,81 +22,116 @@ namespace cpp {
 // llvm/ADT/ArrayRef.h. The implementations in this file are of a limited
 // functionality, but can be extended in an as needed basis.
 namespace internal {
-template <typename T> class ArrayRefBase {
+template <typename QualifiedT> class ArrayRefBase {
 public:
-  using iterator = T *;
-  using pointer = T *;
-  using reference = T &;
+  using value_type = RemoveCVType<QualifiedT>;
+  using pointer = value_type *;
+  using const_pointer = const value_type *;
+  using reference = value_type &;
+  using const_reference = const value_type &;
+  using iterator = const_pointer;
+  using const_iterator = const_pointer;
+  using size_type = size_t;
+  using difference_type = ptrdiff_t;
 
   ArrayRefBase() = default;
 
-  // From Array.
-  template <size_t N>
-  ArrayRefBase(Array<T, N> &Arr) : Data(Arr.Data), Length(N) {}
-
   // Construct an ArrayRefBase from a single element.
-  explicit ArrayRefBase(T &OneElt) : Data(&OneElt), Length(1) {}
+  explicit ArrayRefBase(QualifiedT &OneElt) : Data(&OneElt), Length(1) {}
 
   // Construct an ArrayRefBase from a pointer and length.
-  ArrayRefBase(pointer Data, size_t Length) : Data(Data), Length(Length) {}
+  ArrayRefBase(QualifiedT *Data, size_t Length) : Data(Data), Length(Length) {}
 
   // Construct an ArrayRefBase from a range.
-  ArrayRefBase(iterator Begin, iterator End)
+  ArrayRefBase(QualifiedT *Begin, QualifiedT *End)
       : Data(Begin), Length(End - Begin) {}
 
   // Construct an ArrayRefBase from a C array.
   template <size_t N>
-  constexpr ArrayRefBase(T (&Arr)[N]) : Data(Arr), Length(N) {}
+  constexpr ArrayRefBase(QualifiedT (&Arr)[N]) : Data(Arr), Length(N) {}
 
-  iterator begin() const { return Data; }
-  iterator end() const { return Data + Length; }
-
-  bool empty() const { return Length == 0; }
-
-  pointer data() const { return Data; }
-
+  QualifiedT *data() const { return Data; }
   size_t size() const { return Length; }
 
-  reference operator[](size_t Index) const { return Data[Index]; }
+  auto begin() const { return data(); }
+  auto end() const { return data() + size(); }
+
+  bool empty() const { return size() == 0; }
+
+  auto operator[](size_t Index) const { return data()[Index]; }
 
   // slice(n, m) - Chop off the first N elements of the array, and keep M
   // elements in the array.
-  ArrayRefBase<T> slice(size_t N, size_t M) const {
-    return ArrayRefBase<T>(data() + N, M);
-  }
+  auto slice(size_t N, size_t M) const { return ArrayRefBase(data() + N, M); }
   // slice(n) - Chop off the first N elements of the array.
-  ArrayRefBase<T> slice(size_t N) const { return slice(N, size() - N); }
+  auto slice(size_t N) const { return slice(N, size() - N); }
 
   // Drop the first \p N elements of the array.
-  ArrayRefBase<T> drop_front(size_t N = 1) const {
-    return slice(N, size() - N);
-  }
+  auto drop_front(size_t N = 1) const { return slice(N, size() - N); }
 
   // Drop the last \p N elements of the array.
-  ArrayRefBase<T> drop_back(size_t N = 1) const { return slice(0, size() - N); }
+  auto drop_back(size_t N = 1) const { return slice(0, size() - N); }
 
   // Return a copy of *this with only the first \p N elements.
-  ArrayRefBase<T> take_front(size_t N = 1) const {
+  auto take_front(size_t N = 1) const {
     if (N >= size())
       return *this;
     return drop_back(size() - N);
   }
 
   // Return a copy of *this with only the last \p N elements.
-  ArrayRefBase<T> take_back(size_t N = 1) const {
+  auto take_back(size_t N = 1) const {
     if (N >= size())
       return *this;
     return drop_front(size() - N);
   }
 
+  // equals - Check for element-wise equality.
+  bool equals(ArrayRefBase<QualifiedT> RHS) const {
+    if (Length != RHS.Length)
+      return false;
+    auto First1 = begin();
+    auto Last1 = end();
+    auto First2 = RHS.begin();
+    for (; First1 != Last1; ++First1, ++First2) {
+      if (!(*First1 == *First2)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 private:
-  pointer Data = nullptr;
+  QualifiedT *Data = nullptr;
   size_t Length = 0;
 };
 } // namespace internal
 
-template <typename T> using ArrayRef = internal::ArrayRefBase<const T>;
-template <typename T> using MutableArrayRef = internal::ArrayRefBase<T>;
+template <typename T> struct ArrayRef : public internal::ArrayRefBase<const T> {
+private:
+  static_assert(IsSameV<T, RemoveCVType<T>>,
+                "ArrayRef must have a non-const, non-volatile value_type");
+  using Impl = internal::ArrayRefBase<const T>;
+  using Impl::Impl;
+
+public:
+  // From Array.
+  template <size_t N> ArrayRef(const Array<T, N> &Arr) : Impl(Arr.Data, N) {}
+};
+
+template <typename T>
+struct MutableArrayRef : public internal::ArrayRefBase<T> {
+private:
+  static_assert(
+      IsSameV<T, RemoveCVType<T>>,
+      "MutableArrayRef must have a non-const, non-volatile value_type");
+  using Impl = internal::ArrayRefBase<T>;
+  using Impl::Impl;
+
+public:
+  // From Array.
+  template <size_t N> MutableArrayRef(Array<T, N> &Arr) : Impl(Arr.Data, N) {}
+};
 
 } // namespace cpp
 } // namespace __llvm_libc
