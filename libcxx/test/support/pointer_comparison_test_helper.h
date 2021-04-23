d7bd62a64e440366eaebde75f202f7f737c77507
zoecarver <z.zoelec2@gmail.com>
2021-04-23 12:37:47 -0700

    [libcxx][nfc] Add license to `pointer_comparison_test_helper.h`

--- libcxx/test/support/pointer_comparison_test_helper.h
+++ libcxx/test/support/pointer_comparison_test_helper.h
@@ -1,3 +1,10 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
 #ifndef POINTER_COMPARISON_TEST_HELPER_H
 #define POINTER_COMPARISON_TEST_HELPER_H
 
