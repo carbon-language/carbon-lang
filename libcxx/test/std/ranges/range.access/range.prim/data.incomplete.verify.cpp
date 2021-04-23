9db55b314b5b9e387ed36fe61b829efcc98b7c7b
zoecarver <z.zoelec2@gmail.com>
2021-04-23 15:22:18 -0700

    [libcxx][ranges] Add ranges::data CPO.
    This is the second to last one! Based on D101396. Depends on D100255. Refs D101079 and D101193.
    
    Differential Revision: https://reviews.llvm.org/D101476

---
+++ libcxx/test/std/ranges/range.access/range.prim/data.incomplete.verify.cpp
@@ -0,0 +1,56 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// UNSUPPORTED: libcpp-no-concepts
+// UNSUPPORTED: clang-10
+// UNSUPPORTED: gcc-10
+
+// std::ranges::data
+
+#include <ranges>
+
+struct Incomplete;
+
+void f(Incomplete arr[]) {
+  // expected-error@*:* {{is SFINAE-unfriendly on arrays of an incomplete type.}}
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f(Incomplete(&arr)[]) {
+  // expected-error@*:* {{is SFINAE-unfriendly on arrays of an incomplete type.}}
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f(Incomplete(&&arr)[]) {
+  // expected-error@*:* {{is SFINAE-unfriendly on arrays of an incomplete type.}}
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f2(Incomplete arr[2]) {
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f(Incomplete(&arr)[2]) {
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f(Incomplete(&&arr)[2]) {
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
+
+void f(Incomplete(&arr)[2][2]) {
+  // expected-error@*:* {{no matching function for call}}
+  std::ranges::data(arr);
+}
