a224bf8ec423b42eea251407e7a6cc8398a5edf4
Christopher Di Bella <cjdb.ns@gmail.com>
2021-04-23 16:08:58 -0700

    [libcxx] disables ranges for clang-cl
    clang-cl doesn't properly handle concepts right now and is failing CI.
    
    Differential Revision: https://reviews.llvm.org/D101205

--- libcxx/test/std/iterators/iterator.requirements/iterator.assoc.types/incrementable.traits/incrementable_traits.compile.pass.cpp
+++ libcxx/test/std/iterators/iterator.requirements/iterator.assoc.types/incrementable.traits/incrementable_traits.compile.pass.cpp
@@ -9,6 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // UNSUPPORTED: libcpp-no-concepts
 // UNSUPPORTED: gcc-10
+// XFAIL: msvc && clang
 
 // template<class T>
 // struct incrementable_traits;
--- libcxx/test/std/iterators/stream.iterators/ostreambuf.iterator/iterator_concept_conformance.compile.pass.cpp
+++ libcxx/test/std/iterators/stream.iterators/ostreambuf.iterator/iterator_concept_conformance.compile.pass.cpp
@@ -9,6 +9,7 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // UNSUPPORTED: libcpp-no-concepts
 // UNSUPPORTED: gcc-10
+// XFAIL: msvc && clang
 
 // ostreambuf_iterator
 
