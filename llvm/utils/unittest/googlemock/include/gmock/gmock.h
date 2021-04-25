504eee28fe0ff4cf582f623b2ee83e9940e33024
Nemanja Ivanovic <nemanjai@ca.ibm.com>
2021-04-25 10:37:25 -0700

    Disable deprecated-copy warnings on various LLVM code to bring the bot back to green
    Bootstrap with `-Werror` is currently broken due to D79714.
    This patch is required to bring the bootstrap bot back to green. The
    code will likely need to be fixed and the pragmas removed in due time,
    but for now we need to bring the bot back up.
    
    Bot that is currently failing:
    https://lab.llvm.org/buildbot/#/builders/36/builds/7680
    
    Differential Revision: https://reviews.llvm.org/D101214

--- llvm/utils/unittest/googlemock/include/gmock/gmock.h
+++ llvm/utils/unittest/googlemock/include/gmock/gmock.h
@@ -55,6 +55,11 @@
 //
 // where all clauses are optional and WillOnce() can be repeated.
 
+#ifdef __clang__
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-copy"
+#endif
+
 #include "gmock/gmock-actions.h"
 #include "gmock/gmock-cardinalities.h"
 #include "gmock/gmock-generated-actions.h"
@@ -91,4 +96,7 @@ GTEST_API_ void InitGoogleMock(int* argc, wchar_t** argv);
 
 }  // namespace testing
 
+#ifdef __clang__
+#pragma clang diagnostic pop
+#endif
 #endif  // GMOCK_INCLUDE_GMOCK_GMOCK_H_
