63cfab4f40390bf10a313039adf0b955db9706b9
RamNalamothu <VenkataRamanaiah.Nalamothu@amd.com>
2021-04-28 05:57:29 +0530

    [NFC] Refactor how CFI section types are represented in AsmPrinter
    In terms of readability, the `enum CFIMoveType` didn't better document what it
    intends to convey i.e. the type of CFI section that gets emitted.
    
    Reviewed By: dblaikie, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D76519

--- llvm/lib/CodeGen/AsmPrinter/ARMException.cpp
+++ llvm/lib/CodeGen/AsmPrinter/ARMException.cpp
@@ -39,13 +39,13 @@ void ARMException::beginFunction(const MachineFunction *MF) {
   if (Asm->MAI->getExceptionHandlingType() == ExceptionHandling::ARM)
     getTargetStreamer().emitFnStart();
   // See if we need call frame info.
-  AsmPrinter::CFIMoveType MoveType = Asm->needsCFIMoves();
-  assert(MoveType != AsmPrinter::CFI_M_EH &&
+  AsmPrinter::CFISection CFISecType = Asm->getFunctionCFISectionType(*MF);
+  assert(CFISecType != AsmPrinter::CFISection::EH &&
          "non-EH CFI not yet supported in prologue with EHABI lowering");
 
-  if (MoveType == AsmPrinter::CFI_M_Debug) {
+  if (CFISecType == AsmPrinter::CFISection::Debug) {
     if (!hasEmittedCFISections) {
-      if (Asm->needsOnlyDebugCFIMoves())
+      if (Asm->getModuleCFISectionType() == AsmPrinter::CFISection::Debug)
         Asm->OutStreamer->emitCFISections(false, true);
       hasEmittedCFISections = true;
     }
