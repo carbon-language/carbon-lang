1d4d6a96164ea9f7d24103ca6fa1ad500386aff6
Craig Topper <craig.topper@sifive.com>
2021-04-28 09:55:36 -0700

    [RISCV] Add explanatory comment to RISCVOp::OPERAND_AVL.

--- llvm/lib/Target/RISCV/MCTargetDesc/RISCVBaseInfo.h
+++ llvm/lib/Target/RISCV/MCTargetDesc/RISCVBaseInfo.h
@@ -118,6 +118,9 @@ enum OperandType : unsigned {
   OPERAND_UIMM20,
   OPERAND_UIMMLOG2XLEN,
   OPERAND_LAST_RISCV_IMM = OPERAND_UIMMLOG2XLEN,
+  // Operand is either a register or uimm5, this is used by V extension pseudo
+  // instructions to represent a value that be passed as AVL to either vsetvli
+  // or vsetivli.
   OPERAND_AVL,
 };
 } // namespace RISCVOp
