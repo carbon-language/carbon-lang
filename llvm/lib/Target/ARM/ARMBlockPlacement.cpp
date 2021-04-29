465df35355ec30ab8c60ef1c0c156a6b22bda7d4
David Green <david.green@arm.com>
2021-04-29 07:44:04 +0100

    [ARM] Use just ARM::t2B in ARMBlockPlacementPass
    The ARMConstantIsland pass will convert any t2B to tB if they are within
    range after it has added or moved any constant pools. They don't need to
    be deliberately converted beforehand, and it doesn't deal with needing
    to convert tB to t2B very well.

--- llvm/lib/Target/ARM/ARMBlockPlacement.cpp
+++ llvm/lib/Target/ARM/ARMBlockPlacement.cpp
@@ -237,10 +237,8 @@ void ARMBlockPlacement::moveBasicBlock(MachineBasicBlock *BB,
     if (!Terminator.isUnconditionalBranch()) {
       // The BB doesn't have an unconditional branch so it relied on
       // fall-through. Fix by adding an unconditional branch to the moved BB.
-      unsigned BrOpc =
-          BBUtils->isBBInRange(&Terminator, To, 254) ? ARM::tB : ARM::t2B;
       MachineInstrBuilder MIB =
-          BuildMI(From, Terminator.getDebugLoc(), TII->get(BrOpc));
+          BuildMI(From, Terminator.getDebugLoc(), TII->get(ARM::t2B));
       MIB.addMBB(To);
       MIB.addImm(ARMCC::CondCodes::AL);
       MIB.addReg(ARM::NoRegister);
