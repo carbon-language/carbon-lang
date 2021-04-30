a67a377014ceaaed55b2ba2259e080bc3fc42b43
David Stuttard <david.stuttard@amd.com>
2021-04-30 10:06:30 +0100

    [AMDGPU] Tidy up some simple expressions for clarity NFC
    Slight refactor for clarity.
    
    Change-Id: Ib25e7f4582c67a7c57f066cfd5382c1405d7d4c5
    
    Differential Revision: https://reviews.llvm.org/D101610

--- llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
+++ llvm/lib/Target/AMDGPU/SIInstrInfo.cpp
@@ -4244,7 +4244,7 @@ bool SIInstrInfo::verifyInstruction(const MachineInstr &MI,
       unsigned AddrComponents = (BaseOpcode->Coordinates ? Dim->NumCoords : 0) +
                                 (BaseOpcode->LodOrClampOrMip ? 1 : 0);
       if (IsA16)
-        AddrWords += (AddrComponents + 1) / 2;
+        AddrWords += divideCeil(AddrComponents, 2);
       else
         AddrWords += AddrComponents;
 
@@ -4252,7 +4252,7 @@ bool SIInstrInfo::verifyInstruction(const MachineInstr &MI,
         if (PackDerivatives)
           // There are two gradients per coordinate, we pack them separately.
           // For the 3d case, we get (dy/du, dx/du) (-, dz/du) (dy/dv, dx/dv) (-, dz/dv)
-          AddrWords += (Dim->NumGradients / 2 + 1) / 2 * 2;
+          AddrWords += alignTo<2>(Dim->NumGradients / 2);
         else
           AddrWords += Dim->NumGradients;
       }
