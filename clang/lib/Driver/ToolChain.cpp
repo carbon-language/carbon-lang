1971823ecb9eaa077554a5d268a44c7cb75eccce
Dan Liew <dliew@apple.com>
2021-04-30 19:10:18 -0700

    [Driver] Fix `ToolChain::getCompilerRTPath()` to return the correct path on Apple platforms.
    When the target triple was an Apple platform `ToolChain::getOSLibName()`
    (called by `getCompilerRTPath()`) would return the full OS name
    including the version number (e.g. `darwin20.3.0`). This is not correct
    because the library directory for all Apple platforms is `darwin`.
    
    This in turn caused
    
    * `-print-runtime-dir` to return a non-existant path.
    * `-print-file-name=<any compiler-rt library>` to return the filename
      instead of the full path to the library.
    
    Two regression tests are included.
    
    rdar://77417317
    
    Differential Revision: https://reviews.llvm.org/D101682

--- clang/lib/Driver/ToolChain.cpp
+++ clang/lib/Driver/ToolChain.cpp
@@ -387,6 +387,9 @@ static StringRef getArchNameForCompilerRTLib(const ToolChain &TC,
 }
 
 StringRef ToolChain::getOSLibName() const {
+  if (Triple.isOSDarwin())
+    return "darwin";
+
   switch (Triple.getOS()) {
   case llvm::Triple::FreeBSD:
     return "freebsd";
