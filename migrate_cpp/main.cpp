4f7b4b30ec31ee0907c85b9ebdaaa7a6f0e09a7d
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-05-03 18:03:37 +0000

    json

--- migrate_cpp/main.cpp
+++ migrate_cpp/main.cpp
@@ -7,34 +7,22 @@
 #include <iostream>
 
 #include "clang/Tooling/CompilationDatabase.h"
+#include "clang/Tooling/JSONCompilationDatabase.h"
 #include "clang/Tooling/Tooling.h"
 #include "llvm/Support/CommandLine.h"
 
 namespace cl = llvm::cl;
 using clang::tooling::ClangTool;
 using clang::tooling::CompilationDatabase;
-using clang::tooling::FixedCompilationDatabase;
+using clang::tooling::JSONCommandLineSyntax;
+using clang::tooling::JSONCompilationDatabase;
 
 auto GetAST(const std::unique_ptr<CompilationDatabase>& common_comp_db,
             const llvm::StringRef build_path, const llvm::StringRef filename)
     -> std::unique_ptr<clang::ASTUnit> {
   std::string err;
-  std::unique_ptr<CompilationDatabase> comp_db;
-  if (!common_comp_db) {
-    comp_db = CompilationDatabase::autoDetectFromSource(
-        build_path.empty() ? filename : build_path, err);
-    if (!comp_db) {
-      llvm::errs()
-          << "Error while trying to load a compilation database, running "
-             "without flags.\n"
-          << err;
-      comp_db = std::make_unique<clang::tooling::FixedCompilationDatabase>(
-          ".", std::vector<std::string>());
-    }
-  }
-  // addExtraArgs(Compilations);
   std::array<std::string, 1> files = {{std::string(filename)}};
-  ClangTool tool(comp_db ? *comp_db : *common_comp_db, files);
+  ClangTool tool(*common_comp_db, files);
   std::vector<std::unique_ptr<clang::ASTUnit>> asts;
   tool.buildASTs(asts);
   if (asts.size() != files.size()) {
@@ -46,18 +34,22 @@ auto GetAST(const std::unique_ptr<CompilationDatabase>& common_comp_db,
 int main(int argc, char* argv[]) {
   cl::opt<std::string> source_path(cl::Positional, cl::desc("<source>"),
                                    cl::Required);
+  cl::opt<std::string> common_comp_db_path("comp-db", cl::desc("<path>"),
+                                           cl::Required);
   cl::opt<std::string> build_path("p", cl::desc("Build path"), cl::init(""),
                                   cl::Optional);
 
-  std::string err;
-  std::unique_ptr<CompilationDatabase> common_comp_db =
-      FixedCompilationDatabase::loadFromCommandLine(argc, argv, err);
-  if (!common_comp_db && !err.empty()) {
-    llvm::errs() << err;
-  }
   if (!cl::ParseCommandLineOptions(argc, argv)) {
     cl::PrintOptionValues();
     return 1;
+  }
+
+  std::string err;
+  std::unique_ptr<CompilationDatabase> common_comp_db =
+      JSONCompilationDatabase::loadFromFile(common_comp_db_path, err,
+                                            JSONCommandLineSyntax::AutoDetect);
+  if (!common_comp_db && !err.empty()) {
+    llvm::errs() << err;
   }
   std::unique_ptr<clang::ASTUnit> source =
       GetAST(common_comp_db, build_path, source_path);
