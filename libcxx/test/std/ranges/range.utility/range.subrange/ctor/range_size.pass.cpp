9106047ee3dd93a250deb1ef34b890f486dee710
zoecarver <z.zoelec2@gmail.com>
2021-05-03 14:37:42 -0700

    [libcxx][ranges] Add range.subrange.
    Basically the title.
    
    Differential Revision: https://reviews.llvm.org/D102006

---
+++ libcxx/test/std/ranges/range.utility/range.subrange/ctor/range_size.pass.cpp
@@ -0,0 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11, c++14, c++17
+// UNSUPPORTED: libcpp-no-concepts
+// UNSUPPORTED: gcc-10
+
+// class std::ranges::subrange;
+
+#include <ranges>
+
+// Tested in pair_like_conv.pass.cpp.
+
+int main(int, char**) {
+  return 0;
+}
