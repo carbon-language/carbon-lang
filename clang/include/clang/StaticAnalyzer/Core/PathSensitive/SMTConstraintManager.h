d882750f1105b20d892545e7ebd96f82166dcb53
Ella Ma <alansnape3058@gmail.com>
2021-05-04 16:50:21 +0200

    [analyzer] Fix a crash for dereferencing an empty llvm::Optional variable in SMTConstraintManager.h.
    The first crash reported in the bug report 44338.
    
    Condition `!isSat.hasValue() || isNotSat.getValue()` here should be
    `!isNotSat.hasValue() || isNotSat.getValue()`.
    `getValue()` here crashed when we used the static analyzer to analyze
    postgresql-12.0.
    
    Patch By: OikawaKirie
    
    Reviewed By: steakhal, martong
    
    Differential Revision: https://reviews.llvm.org/D83660

--- clang/include/clang/StaticAnalyzer/Core/PathSensitive/SMTConstraintManager.h
+++ clang/include/clang/StaticAnalyzer/Core/PathSensitive/SMTConstraintManager.h
@@ -146,7 +146,7 @@ public:
       Solver->addConstraint(NotExp);
 
       Optional<bool> isNotSat = Solver->check();
-      if (!isSat.hasValue() || isNotSat.getValue())
+      if (!isNotSat.hasValue() || isNotSat.getValue())
         return nullptr;
 
       // This is the only solution, store it
