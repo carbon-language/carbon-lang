33ff3c20498ef5c2057730d91a9dbee21a027a31
Bjorn Pettersson <bjorn.a.pettersson@ericsson.com>
2021-05-04 19:07:05 +0200

    Revert "Make dependency between certain analysis passes transitive"
    This reverts commit 3655f0757f2b4b61419446b326410118658826ba.
    
    It caused assertion failures related to setLastUser in polly builds.

--- llvm/lib/Analysis/LoopAccessAnalysis.cpp
+++ llvm/lib/Analysis/LoopAccessAnalysis.cpp
@@ -2281,12 +2281,12 @@ bool LoopAccessLegacyAnalysis::runOnFunction(Function &F) {
 }
 
 void LoopAccessLegacyAnalysis::getAnalysisUsage(AnalysisUsage &AU) const {
-  AU.addRequiredTransitive<ScalarEvolutionWrapperPass>();
-  AU.addRequiredTransitive<AAResultsWrapperPass>();
-  AU.addRequiredTransitive<DominatorTreeWrapperPass>();
-  AU.addRequiredTransitive<LoopInfoWrapperPass>();
+    AU.addRequired<ScalarEvolutionWrapperPass>();
+    AU.addRequired<AAResultsWrapperPass>();
+    AU.addRequired<DominatorTreeWrapperPass>();
+    AU.addRequired<LoopInfoWrapperPass>();
 
-  AU.setPreservesAll();
+    AU.setPreservesAll();
 }
 
 char LoopAccessLegacyAnalysis::ID = 0;
