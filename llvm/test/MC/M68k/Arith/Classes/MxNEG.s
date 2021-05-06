f6d7fc801b4a513a5d5e04762e0e4c71414ae5ef
Min-Yih Hsu <minyihh@uci.edu>
2021-05-05 17:46:56 -0700

    [M68k][test][NFC] Scrubing some tests
    Remove unecessary labels and assembly directives. NFC.

--- llvm/test/MC/M68k/Arith/Classes/MxNEG.s
+++ llvm/test/MC/M68k/Arith/Classes/MxNEG.s
@@ -1,20 +1,16 @@
 ; RUN: llvm-mc -triple=m68k -show-encoding %s | FileCheck %s
 
-; CHECK-LABEL: MxNEG:
-MxNEG:
-	; CHECK:      neg.b  %d0
-	; CHECK-SAME: encoding: [0x44,0x00]
-	neg.b	%d0
-	; CHECK:      neg.l  %d0
-	; CHECK-SAME: encoding: [0x44,0x80]
-	neg.l	%d0
+; CHECK:      neg.b  %d0
+; CHECK-SAME: encoding: [0x44,0x00]
+neg.b	%d0
+; CHECK:      neg.l  %d0
+; CHECK-SAME: encoding: [0x44,0x80]
+neg.l	%d0
 
-; CHECK-LABEL: MxNEGX:
-MxNEGX:
-	; CHECK:      negx.b  %d0
-	; CHECK-SAME: encoding: [0x40,0x00]
-	negx.b	%d0
-	; CHECK:      negx.l  %d0
-	; CHECK-SAME: encoding: [0x40,0x80]
-	negx.l	%d0
+; CHECK:      negx.b  %d0
+; CHECK-SAME: encoding: [0x40,0x00]
+negx.b	%d0
+; CHECK:      negx.l  %d0
+; CHECK-SAME: encoding: [0x40,0x80]
+negx.l	%d0
 
--- llvm/test/MC/M68k/Bits/Classes/MxBTST_MI.s
+++ llvm/test/MC/M68k/Bits/Classes/MxBTST_MI.s
@@ -1,52 +1,37 @@
 ; RUN: llvm-mc -triple=m68k -show-encoding %s | FileCheck %s
-	.text
-	.globl	MxBTST_MI_PCI
-; CHECK-LABEL: MxBTST_MI_PCI:
-MxBTST_MI_PCI:
-	; CHECK:      btst  #0, (-1,%pc,%d1)
-	; CHECK-SAME: encoding: [0x08,0x3b,0x00,0x00,0x18,0xff]
-	btst	#0, (-1,%pc,%d1)
-	; CHECK:      btst  #1, (0,%pc,%d0)
-	; CHECK-SAME: encoding: [0x08,0x3b,0x00,0x01,0x08,0x00]
-	btst	#1, (0,%pc,%d0)
 
-	.globl	MxBTST_MI_PCD
-; CHECK-LABEL: MxBTST_MI_PCD:
-MxBTST_MI_PCD:
-	; CHECK:      btst  #0, (0,%pc)
-	; CHECK-SAME: encoding: [0x08,0x3a,0x00,0x00,0x00,0x00]
-	btst	#0, (0,%pc)
-	; CHECK:      btst  #-1, (-1,%pc)
-	; CHECK-SAME: encoding: [0x08,0x3a,0x00,0xff,0xff,0xff]
-	btst	#-1, (-1,%pc)
+; CHECK:      btst  #0, (-1,%pc,%d1)
+; CHECK-SAME: encoding: [0x08,0x3b,0x00,0x00,0x18,0xff]
+btst	#0, (-1,%pc,%d1)
+; CHECK:      btst  #1, (0,%pc,%d0)
+; CHECK-SAME: encoding: [0x08,0x3b,0x00,0x01,0x08,0x00]
+btst	#1, (0,%pc,%d0)
 
-	.globl	MxBTST_MI_ARII
-; CHECK-LABEL: MxBTST_MI_ARII:
-MxBTST_MI_ARII:
-	; CHECK:      btst  #0, (-1,%a1,%a0)
-	; CHECK-SAME: encoding: [0x08,0x31,0x00,0x00,0x88,0xff]
-	btst	#0, (-1,%a1,%a0)
-	; CHECK:      btst  #-1, (0,%a0,%a0)
-	; CHECK-SAME: encoding: [0x08,0x30,0x00,0xff,0x88,0x00]
-	btst	#-1, (0,%a0,%a0)
+; CHECK:      btst  #0, (0,%pc)
+; CHECK-SAME: encoding: [0x08,0x3a,0x00,0x00,0x00,0x00]
+btst	#0, (0,%pc)
+; CHECK:      btst  #-1, (-1,%pc)
+; CHECK-SAME: encoding: [0x08,0x3a,0x00,0xff,0xff,0xff]
+btst	#-1, (-1,%pc)
 
-	.globl	MxBTST_MI_ARID
-; CHECK-LABEL: MxBTST_MI_ARID:
-MxBTST_MI_ARID:
-	; CHECK:      btst  #0, (-1,%a1)
-	; CHECK-SAME: encoding: [0x08,0x29,0x00,0x00,0xff,0xff]
-	btst	#0, (-1,%a1)
-	; CHECK:      btst  #-1, (0,%a0)
-	; CHECK-SAME: encoding: [0x08,0x28,0x00,0xff,0x00,0x00]
-	btst	#-1, (0,%a0)
+; CHECK:      btst  #0, (-1,%a1,%a0)
+; CHECK-SAME: encoding: [0x08,0x31,0x00,0x00,0x88,0xff]
+btst	#0, (-1,%a1,%a0)
+; CHECK:      btst  #-1, (0,%a0,%a0)
+; CHECK-SAME: encoding: [0x08,0x30,0x00,0xff,0x88,0x00]
+btst	#-1, (0,%a0,%a0)
 
-	.globl	MxBTST_MI_ARI
-; CHECK-LABEL: MxBTST_MI_ARI:
-MxBTST_MI_ARI:
-	; CHECK:      btst  #0, (%a1)
-	; CHECK-SAME: encoding: [0x08,0x11,0x00,0x00]
-	btst	#0, (%a1)
-	; CHECK:      btst  #-1, (%a0)
-	; CHECK-SAME: encoding: [0x08,0x10,0x00,0xff]
-	btst	#-1, (%a0)
+; CHECK:      btst  #0, (-1,%a1)
+; CHECK-SAME: encoding: [0x08,0x29,0x00,0x00,0xff,0xff]
+btst	#0, (-1,%a1)
+; CHECK:      btst  #-1, (0,%a0)
+; CHECK-SAME: encoding: [0x08,0x28,0x00,0xff,0x00,0x00]
+btst	#-1, (0,%a0)
+
+; CHECK:      btst  #0, (%a1)
+; CHECK-SAME: encoding: [0x08,0x11,0x00,0x00]
+btst	#0, (%a1)
+; CHECK:      btst  #-1, (%a0)
+; CHECK-SAME: encoding: [0x08,0x10,0x00,0xff]
+btst	#-1, (%a0)
 
