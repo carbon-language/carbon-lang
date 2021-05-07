70cbc6dbef7048d3b1aa89a676d96c6ba075b41b
Sebastian Poeplau <poeplau@code-intelligence.com>
2021-05-07 08:00:33 -0700

    [libFuzzer] Fix stack overflow detection
    Address sanitizer can detect stack exhaustion via its SEGV handler, which is
    executed on a separate stack using the sigaltstack mechanism. When libFuzzer is
    used with address sanitizer, it installs its own signal handlers which defer to
    those put in place by the sanitizer before performing additional actions. In the
    particular case of a stack overflow, the current setup fails because libFuzzer
    doesn't preserve the flag for executing the signal handler on a separate stack:
    when we run out of stack space, the operating system can't run the SEGV handler,
    so address sanitizer never reports the issue. See the included test for an
    example.
    
    This commit fixes the issue by making libFuzzer preserve the SA_ONSTACK flag
    when installing its signal handlers; the dedicated signal-handler stack set up
    by the sanitizer runtime appears to be large enough to support the additional
    frames from the fuzzer.
    
    Reviewed By: morehouse
    
    Differential Revision: https://reviews.llvm.org/D101824

--- compiler-rt/lib/fuzzer/FuzzerUtilPosix.cpp
+++ compiler-rt/lib/fuzzer/FuzzerUtilPosix.cpp
@@ -77,10 +77,13 @@ static void SetSigaction(int signum,
       return;
   }
 
-  sigact = {};
-  sigact.sa_flags = SA_SIGINFO;
-  sigact.sa_sigaction = callback;
-  if (sigaction(signum, &sigact, 0)) {
+  struct sigaction new_sigact = {};
+  // Address sanitizer needs SA_ONSTACK (causing the signal handler to run on a
+  // dedicated stack) in order to be able to detect stack overflows; keep the
+  // flag if it's set.
+  new_sigact.sa_flags = SA_SIGINFO | (sigact.sa_flags & SA_ONSTACK);
+  new_sigact.sa_sigaction = callback;
+  if (sigaction(signum, &new_sigact, nullptr)) {
     Printf("libFuzzer: sigaction failed with %d\n", errno);
     exit(1);
   }
