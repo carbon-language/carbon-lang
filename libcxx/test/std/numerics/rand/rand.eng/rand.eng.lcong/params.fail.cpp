866b27950aaf2c38f4ecfc8a0f18945fff3b8542
Arthur O'Dwyer <arthur.j.odwyer@gmail.com>
2021-05-10 15:32:38 -0400

    [libc++] s/_VSTD::is_unsigned/is_unsigned/ in <random>. NFCI.

--- libcxx/test/std/numerics/rand/rand.eng/rand.eng.lcong/params.fail.cpp
+++ libcxx/test/std/numerics/rand/rand.eng/rand.eng.lcong/params.fail.cpp
@@ -24,7 +24,7 @@ int main(int, char**)
     // expected-error@random:* {{static_assert failed due to requirement '1ULL == 0 || 1ULL < 1ULL' "linear_congruential_engine invalid parameters"}}
     std::linear_congruential_engine<T, 0, 1, 1> e3;
     std::linear_congruential_engine<T, 1, 0, 1> e4;
-    // expected-error-re@random:* {{static_assert failed due to requirement 'std:{{.*}}:is_unsigned<int>::value' "_UIntType must be unsigned type"}}
+    // expected-error@random:* {{static_assert failed due to requirement 'is_unsigned<int>::value' "_UIntType must be unsigned type"}}
     std::linear_congruential_engine<int, 0, 0, 0> e5;
 
     return 0;
