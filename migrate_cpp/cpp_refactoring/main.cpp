d0f87760b088918ee20235093d8b94bd3872e1c6
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-05-11 17:15:41 +0000

    Iterating

--- migrate_cpp/cpp_refactoring/main.cpp
+++ migrate_cpp/cpp_refactoring/main.cpp
@@ -2,57 +2,61 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+#include "clang/ASTMatchers/ASTMatchFinder.h"
+#include "clang/ASTMatchers/ASTMatchers.h"
+#include "clang/Tooling/CommonOptionsParser.h"
 #include "clang/Tooling/Refactoring.h"
-#include "clang/lib/ASTMatchers/ASTMatchFinder.h"
 
-using clang::MatchFinder;
+using clang::IfStmt;
+using clang::Stmt;
+using clang::ast_matchers::MatchFinder;
+using clang::tooling::Replacement;
+using clang::tooling::Replacements;
 
 class IfStmtHandler : public MatchFinder::MatchCallback {
  public:
-  IfStmtHandler(Replacements* Replace) : Replace(Replace) {}
+  IfStmtHandler(std::map<std::string, Replacements>* in_replacements)
+      : replacements(in_replacements) {}
 
   virtual void run(const MatchFinder::MatchResult& Result) {
     // The matched 'if' statement was bound to 'ifStmt'.
     if (const IfStmt* IfS = Result.Nodes.getNodeAs<clang::IfStmt>("ifStmt")) {
       const Stmt* Then = IfS->getThen();
-      Replacement Rep(*(Result.SourceManager), Then->getLocStart(), 0,
+      Replacement Rep(*(Result.SourceManager), Then->getBeginLoc(), 0,
                       "// the 'if' part\n");
-      Replace->insert(Rep);
+      auto err = (*replacements)[std::string(Rep.getFilePath())].add(Rep);
+      if (err) {
+        llvm::errs() << err;
+      }
 
       if (const Stmt* Else = IfS->getElse()) {
-        Replacement Rep(*(Result.SourceManager), Else->getLocStart(), 0,
+        Replacement Rep(*(Result.SourceManager), Else->getBeginLoc(), 0,
                         "// the 'else' part\n");
-        Replace->insert(Rep);
+        auto err = (*replacements)[std::string(Rep.getFilePath())].add(Rep);
+        if (err) {
+          llvm::errs() << err;
+        }
       }
     }
   }
 
  private:
-  Replacements* Replace;
+  std::map<std::string, Replacements>* replacements;
 };
 
 int main(int argc, const char** argv) {
-  clang::CommonOptionsParser op(argc, argv, clang::ToolingSampleCategory);
-  clang::RefactoringTool Tool(op.getCompilations(), op.getSourcePathList());
+  llvm::cl::OptionCategory category("C++ refactoring options");
+  clang::tooling::CommonOptionsParser op(argc, argv, category);
+  clang::tooling::RefactoringTool tool(op.getCompilations(),
+                                       op.getSourcePathList());
 
   // Set up AST matcher callbacks.
-  IfStmtHandler HandlerForIf(&Tool.getReplacements());
+  IfStmtHandler HandlerForIf(&tool.getReplacements());
 
   MatchFinder Finder;
-  Finder.addMatcher(ifStmt().bind("ifStmt"), &HandlerForIf);
+  Finder.addMatcher(clang::ast_matchers::ifStmt().bind("ifStmt"),
+                    &HandlerForIf);
 
-  // Run the tool and collect a list of replacements. We could call
-  // runAndSave, which would destructively overwrite the files with
-  // their new contents. However, for demonstration purposes it's
-  // interesting to show the replacements.
-  if (int Result = Tool.run(newFrontendActionFactory(&Finder).get())) {
-    return Result;
-  }
-
-  llvm::outs() << "Replacements collected by the tool:\n";
-  for (auto& r : Tool.getReplacements()) {
-    llvm::outs() << r.toString() << "\n";
-  }
-
-  return 0;
+  return tool.runAndSave(
+      clang::tooling::newFrontendActionFactory(&Finder).get());
 }
