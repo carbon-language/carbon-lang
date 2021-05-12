7d101e0f6a38b95f512dc5d78bc9b508f4ee6da0
Vitaly Buka <vitalybuka@google.com>
2021-05-11 19:03:50 -0700

    [NFC][msan] Move setlocale test into sanitizer_common

--- compiler-rt/test/sanitizer_common/TestCases/setlocale.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/setlocale.cpp
@@ -1,11 +1,11 @@
-// RUN: %clangxx_msan -O0 %s -o %t && %run %t
+// RUN: %clangxx -O0 %s -o %t && %run %t
 
 #include <assert.h>
 #include <locale.h>
 #include <stdlib.h>
 
 int main(void) {
-  char *locale = setlocale (LC_ALL, "");
+  char *locale = setlocale(LC_ALL, "");
   assert(locale);
   if (locale[0])
     exit(0);
