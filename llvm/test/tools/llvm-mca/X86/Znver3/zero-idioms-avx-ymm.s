d2fb4bfba8913c81db770778f6b3105a43716b83
Roman Lebedev <lebedev.ri@gmail.com>
2021-05-14 18:48:14 +0300

    [NFC][X86][MCA] AMD Zen 3: add same-reg AVX YMM VPCMPGT{B,W,D,Q} tests

--- llvm/test/tools/llvm-mca/X86/Znver3/zero-idioms-avx-ymm.s
+++ llvm/test/tools/llvm-mca/X86/Znver3/zero-idioms-avx-ymm.s
@@ -71,6 +71,26 @@ vpsubusw %ymm0, %ymm0, %ymm0
 vpxor %ymm1, %ymm0, %ymm0
 # LLVM-MCA-END
 
+# LLVM-MCA-BEGIN
+vpcmpgtb %ymm0, %ymm0, %ymm0
+vpaddb %ymm0, %ymm0, %ymm0
+# LLVM-MCA-END
+
+# LLVM-MCA-BEGIN
+vpcmpgtw %ymm0, %ymm0, %ymm0
+vpaddw %ymm0, %ymm0, %ymm0
+# LLVM-MCA-END
+
+# LLVM-MCA-BEGIN
+vpcmpgtd %ymm0, %ymm0, %ymm0
+vpaddd %ymm0, %ymm0, %ymm0
+# LLVM-MCA-END
+
+# LLVM-MCA-BEGIN
+vpcmpgtq %ymm0, %ymm0, %ymm0
+vpaddq %ymm0, %ymm0, %ymm0
+# LLVM-MCA-END
+
 # CHECK:      [0] Code Region
 
 # CHECK:      Iterations:        10000
@@ -1344,3 +1364,367 @@ vpxor %ymm1, %ymm0, %ymm0
 # CHECK-NEXT: 0.     2     1.0    1.0    0.5       vpsubusw	%ymm0, %ymm0, %ymm0
 # CHECK-NEXT: 1.     2     2.0    0.0    0.0       vpxor	%ymm1, %ymm0, %ymm0
 # CHECK-NEXT:        2     1.5    0.5    0.3       <total>
+
+# CHECK:      [14] Code Region
+
+# CHECK:      Iterations:        10000
+# CHECK-NEXT: Instructions:      20000
+# CHECK-NEXT: Total Cycles:      20003
+# CHECK-NEXT: Total uOps:        20000
+
+# CHECK:      Dispatch Width:    6
+# CHECK-NEXT: uOps Per Cycle:    1.00
+# CHECK-NEXT: IPC:               1.00
+# CHECK-NEXT: Block RThroughput: 0.5
+
+# CHECK:      Instruction Info:
+# CHECK-NEXT: [1]: #uOps
+# CHECK-NEXT: [2]: Latency
+# CHECK-NEXT: [3]: RThroughput
+# CHECK-NEXT: [4]: MayLoad
+# CHECK-NEXT: [5]: MayStore
+# CHECK-NEXT: [6]: HasSideEffects (U)
+
+# CHECK:      [1]    [2]    [3]    [4]    [5]    [6]    Instructions:
+# CHECK-NEXT:  1      1     0.25                        vpcmpgtb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  1      1     0.25                        vpaddb	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Register File statistics:
+# CHECK-NEXT: Total number of mappings created:    20000
+# CHECK-NEXT: Max number of mappings used:         66
+
+# CHECK:      *  Register File #1 -- Zn3FpPRF:
+# CHECK-NEXT:    Number of physical registers:     160
+# CHECK-NEXT:    Total number of mappings created: 20000
+# CHECK-NEXT:    Max number of mappings used:      66
+
+# CHECK:      *  Register File #2 -- Zn3IntegerPRF:
+# CHECK-NEXT:    Number of physical registers:     192
+# CHECK-NEXT:    Total number of mappings created: 0
+# CHECK-NEXT:    Max number of mappings used:      0
+
+# CHECK:      Resources:
+# CHECK-NEXT: [0]   - Zn3AGU0
+# CHECK-NEXT: [1]   - Zn3AGU1
+# CHECK-NEXT: [2]   - Zn3AGU2
+# CHECK-NEXT: [3]   - Zn3ALU0
+# CHECK-NEXT: [4]   - Zn3ALU1
+# CHECK-NEXT: [5]   - Zn3ALU2
+# CHECK-NEXT: [6]   - Zn3ALU3
+# CHECK-NEXT: [7]   - Zn3BRU1
+# CHECK-NEXT: [8]   - Zn3FPP0
+# CHECK-NEXT: [9]   - Zn3FPP1
+# CHECK-NEXT: [10]  - Zn3FPP2
+# CHECK-NEXT: [11]  - Zn3FPP3
+# CHECK-NEXT: [12.0] - Zn3FPP45
+# CHECK-NEXT: [12.1] - Zn3FPP45
+# CHECK-NEXT: [13]  - Zn3FPSt
+# CHECK-NEXT: [14.0] - Zn3LSU
+# CHECK-NEXT: [14.1] - Zn3LSU
+# CHECK-NEXT: [14.2] - Zn3LSU
+# CHECK-NEXT: [15.0] - Zn3Load
+# CHECK-NEXT: [15.1] - Zn3Load
+# CHECK-NEXT: [15.2] - Zn3Load
+# CHECK-NEXT: [16.0] - Zn3Store
+# CHECK-NEXT: [16.1] - Zn3Store
+
+# CHECK:      Resource pressure per iteration:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1]
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50   0.50   0.50   0.50    -      -      -      -      -      -      -      -      -      -      -
+
+# CHECK:      Resource pressure by instruction:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1] Instructions:
+# CHECK-NEXT:  -      -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -     vpcmpgtb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -      -     vpaddb	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Timeline view:
+# CHECK-NEXT: Index     0123456
+
+# CHECK:      [0,0]     DeER ..   vpcmpgtb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [0,1]     D=eER..   vpaddb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,0]     D==eER.   vpcmpgtb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,1]     D===eER   vpaddb	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Average Wait times (based on the timeline view):
+# CHECK-NEXT: [0]: Executions
+# CHECK-NEXT: [1]: Average time spent waiting in a scheduler's queue
+# CHECK-NEXT: [2]: Average time spent waiting in a scheduler's queue while ready
+# CHECK-NEXT: [3]: Average time elapsed from WB until retire stage
+
+# CHECK:            [0]    [1]    [2]    [3]
+# CHECK-NEXT: 0.     2     2.0    0.5    0.0       vpcmpgtb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: 1.     2     3.0    0.0    0.0       vpaddb	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:        2     2.5    0.3    0.0       <total>
+
+# CHECK:      [15] Code Region
+
+# CHECK:      Iterations:        10000
+# CHECK-NEXT: Instructions:      20000
+# CHECK-NEXT: Total Cycles:      20003
+# CHECK-NEXT: Total uOps:        20000
+
+# CHECK:      Dispatch Width:    6
+# CHECK-NEXT: uOps Per Cycle:    1.00
+# CHECK-NEXT: IPC:               1.00
+# CHECK-NEXT: Block RThroughput: 0.5
+
+# CHECK:      Instruction Info:
+# CHECK-NEXT: [1]: #uOps
+# CHECK-NEXT: [2]: Latency
+# CHECK-NEXT: [3]: RThroughput
+# CHECK-NEXT: [4]: MayLoad
+# CHECK-NEXT: [5]: MayStore
+# CHECK-NEXT: [6]: HasSideEffects (U)
+
+# CHECK:      [1]    [2]    [3]    [4]    [5]    [6]    Instructions:
+# CHECK-NEXT:  1      1     0.25                        vpcmpgtw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  1      1     0.25                        vpaddw	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Register File statistics:
+# CHECK-NEXT: Total number of mappings created:    20000
+# CHECK-NEXT: Max number of mappings used:         66
+
+# CHECK:      *  Register File #1 -- Zn3FpPRF:
+# CHECK-NEXT:    Number of physical registers:     160
+# CHECK-NEXT:    Total number of mappings created: 20000
+# CHECK-NEXT:    Max number of mappings used:      66
+
+# CHECK:      *  Register File #2 -- Zn3IntegerPRF:
+# CHECK-NEXT:    Number of physical registers:     192
+# CHECK-NEXT:    Total number of mappings created: 0
+# CHECK-NEXT:    Max number of mappings used:      0
+
+# CHECK:      Resources:
+# CHECK-NEXT: [0]   - Zn3AGU0
+# CHECK-NEXT: [1]   - Zn3AGU1
+# CHECK-NEXT: [2]   - Zn3AGU2
+# CHECK-NEXT: [3]   - Zn3ALU0
+# CHECK-NEXT: [4]   - Zn3ALU1
+# CHECK-NEXT: [5]   - Zn3ALU2
+# CHECK-NEXT: [6]   - Zn3ALU3
+# CHECK-NEXT: [7]   - Zn3BRU1
+# CHECK-NEXT: [8]   - Zn3FPP0
+# CHECK-NEXT: [9]   - Zn3FPP1
+# CHECK-NEXT: [10]  - Zn3FPP2
+# CHECK-NEXT: [11]  - Zn3FPP3
+# CHECK-NEXT: [12.0] - Zn3FPP45
+# CHECK-NEXT: [12.1] - Zn3FPP45
+# CHECK-NEXT: [13]  - Zn3FPSt
+# CHECK-NEXT: [14.0] - Zn3LSU
+# CHECK-NEXT: [14.1] - Zn3LSU
+# CHECK-NEXT: [14.2] - Zn3LSU
+# CHECK-NEXT: [15.0] - Zn3Load
+# CHECK-NEXT: [15.1] - Zn3Load
+# CHECK-NEXT: [15.2] - Zn3Load
+# CHECK-NEXT: [16.0] - Zn3Store
+# CHECK-NEXT: [16.1] - Zn3Store
+
+# CHECK:      Resource pressure per iteration:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1]
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50   0.50   0.50   0.50    -      -      -      -      -      -      -      -      -      -      -
+
+# CHECK:      Resource pressure by instruction:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1] Instructions:
+# CHECK-NEXT:  -      -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -     vpcmpgtw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -      -     vpaddw	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Timeline view:
+# CHECK-NEXT: Index     0123456
+
+# CHECK:      [0,0]     DeER ..   vpcmpgtw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [0,1]     D=eER..   vpaddw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,0]     D==eER.   vpcmpgtw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,1]     D===eER   vpaddw	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Average Wait times (based on the timeline view):
+# CHECK-NEXT: [0]: Executions
+# CHECK-NEXT: [1]: Average time spent waiting in a scheduler's queue
+# CHECK-NEXT: [2]: Average time spent waiting in a scheduler's queue while ready
+# CHECK-NEXT: [3]: Average time elapsed from WB until retire stage
+
+# CHECK:            [0]    [1]    [2]    [3]
+# CHECK-NEXT: 0.     2     2.0    0.5    0.0       vpcmpgtw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: 1.     2     3.0    0.0    0.0       vpaddw	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:        2     2.5    0.3    0.0       <total>
+
+# CHECK:      [16] Code Region
+
+# CHECK:      Iterations:        10000
+# CHECK-NEXT: Instructions:      20000
+# CHECK-NEXT: Total Cycles:      20003
+# CHECK-NEXT: Total uOps:        20000
+
+# CHECK:      Dispatch Width:    6
+# CHECK-NEXT: uOps Per Cycle:    1.00
+# CHECK-NEXT: IPC:               1.00
+# CHECK-NEXT: Block RThroughput: 0.5
+
+# CHECK:      Instruction Info:
+# CHECK-NEXT: [1]: #uOps
+# CHECK-NEXT: [2]: Latency
+# CHECK-NEXT: [3]: RThroughput
+# CHECK-NEXT: [4]: MayLoad
+# CHECK-NEXT: [5]: MayStore
+# CHECK-NEXT: [6]: HasSideEffects (U)
+
+# CHECK:      [1]    [2]    [3]    [4]    [5]    [6]    Instructions:
+# CHECK-NEXT:  1      1     0.25                        vpcmpgtd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  1      1     0.25                        vpaddd	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Register File statistics:
+# CHECK-NEXT: Total number of mappings created:    20000
+# CHECK-NEXT: Max number of mappings used:         66
+
+# CHECK:      *  Register File #1 -- Zn3FpPRF:
+# CHECK-NEXT:    Number of physical registers:     160
+# CHECK-NEXT:    Total number of mappings created: 20000
+# CHECK-NEXT:    Max number of mappings used:      66
+
+# CHECK:      *  Register File #2 -- Zn3IntegerPRF:
+# CHECK-NEXT:    Number of physical registers:     192
+# CHECK-NEXT:    Total number of mappings created: 0
+# CHECK-NEXT:    Max number of mappings used:      0
+
+# CHECK:      Resources:
+# CHECK-NEXT: [0]   - Zn3AGU0
+# CHECK-NEXT: [1]   - Zn3AGU1
+# CHECK-NEXT: [2]   - Zn3AGU2
+# CHECK-NEXT: [3]   - Zn3ALU0
+# CHECK-NEXT: [4]   - Zn3ALU1
+# CHECK-NEXT: [5]   - Zn3ALU2
+# CHECK-NEXT: [6]   - Zn3ALU3
+# CHECK-NEXT: [7]   - Zn3BRU1
+# CHECK-NEXT: [8]   - Zn3FPP0
+# CHECK-NEXT: [9]   - Zn3FPP1
+# CHECK-NEXT: [10]  - Zn3FPP2
+# CHECK-NEXT: [11]  - Zn3FPP3
+# CHECK-NEXT: [12.0] - Zn3FPP45
+# CHECK-NEXT: [12.1] - Zn3FPP45
+# CHECK-NEXT: [13]  - Zn3FPSt
+# CHECK-NEXT: [14.0] - Zn3LSU
+# CHECK-NEXT: [14.1] - Zn3LSU
+# CHECK-NEXT: [14.2] - Zn3LSU
+# CHECK-NEXT: [15.0] - Zn3Load
+# CHECK-NEXT: [15.1] - Zn3Load
+# CHECK-NEXT: [15.2] - Zn3Load
+# CHECK-NEXT: [16.0] - Zn3Store
+# CHECK-NEXT: [16.1] - Zn3Store
+
+# CHECK:      Resource pressure per iteration:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1]
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50   0.50   0.50   0.50    -      -      -      -      -      -      -      -      -      -      -
+
+# CHECK:      Resource pressure by instruction:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1] Instructions:
+# CHECK-NEXT:  -      -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -     vpcmpgtd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -      -     vpaddd	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Timeline view:
+# CHECK-NEXT: Index     0123456
+
+# CHECK:      [0,0]     DeER ..   vpcmpgtd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [0,1]     D=eER..   vpaddd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,0]     D==eER.   vpcmpgtd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,1]     D===eER   vpaddd	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Average Wait times (based on the timeline view):
+# CHECK-NEXT: [0]: Executions
+# CHECK-NEXT: [1]: Average time spent waiting in a scheduler's queue
+# CHECK-NEXT: [2]: Average time spent waiting in a scheduler's queue while ready
+# CHECK-NEXT: [3]: Average time elapsed from WB until retire stage
+
+# CHECK:            [0]    [1]    [2]    [3]
+# CHECK-NEXT: 0.     2     2.0    0.5    0.0       vpcmpgtd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: 1.     2     3.0    0.0    0.0       vpaddd	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:        2     2.5    0.3    0.0       <total>
+
+# CHECK:      [17] Code Region
+
+# CHECK:      Iterations:        10000
+# CHECK-NEXT: Instructions:      20000
+# CHECK-NEXT: Total Cycles:      20003
+# CHECK-NEXT: Total uOps:        20000
+
+# CHECK:      Dispatch Width:    6
+# CHECK-NEXT: uOps Per Cycle:    1.00
+# CHECK-NEXT: IPC:               1.00
+# CHECK-NEXT: Block RThroughput: 0.5
+
+# CHECK:      Instruction Info:
+# CHECK-NEXT: [1]: #uOps
+# CHECK-NEXT: [2]: Latency
+# CHECK-NEXT: [3]: RThroughput
+# CHECK-NEXT: [4]: MayLoad
+# CHECK-NEXT: [5]: MayStore
+# CHECK-NEXT: [6]: HasSideEffects (U)
+
+# CHECK:      [1]    [2]    [3]    [4]    [5]    [6]    Instructions:
+# CHECK-NEXT:  1      1     0.25                        vpcmpgtq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  1      1     0.25                        vpaddq	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Register File statistics:
+# CHECK-NEXT: Total number of mappings created:    20000
+# CHECK-NEXT: Max number of mappings used:         66
+
+# CHECK:      *  Register File #1 -- Zn3FpPRF:
+# CHECK-NEXT:    Number of physical registers:     160
+# CHECK-NEXT:    Total number of mappings created: 20000
+# CHECK-NEXT:    Max number of mappings used:      66
+
+# CHECK:      *  Register File #2 -- Zn3IntegerPRF:
+# CHECK-NEXT:    Number of physical registers:     192
+# CHECK-NEXT:    Total number of mappings created: 0
+# CHECK-NEXT:    Max number of mappings used:      0
+
+# CHECK:      Resources:
+# CHECK-NEXT: [0]   - Zn3AGU0
+# CHECK-NEXT: [1]   - Zn3AGU1
+# CHECK-NEXT: [2]   - Zn3AGU2
+# CHECK-NEXT: [3]   - Zn3ALU0
+# CHECK-NEXT: [4]   - Zn3ALU1
+# CHECK-NEXT: [5]   - Zn3ALU2
+# CHECK-NEXT: [6]   - Zn3ALU3
+# CHECK-NEXT: [7]   - Zn3BRU1
+# CHECK-NEXT: [8]   - Zn3FPP0
+# CHECK-NEXT: [9]   - Zn3FPP1
+# CHECK-NEXT: [10]  - Zn3FPP2
+# CHECK-NEXT: [11]  - Zn3FPP3
+# CHECK-NEXT: [12.0] - Zn3FPP45
+# CHECK-NEXT: [12.1] - Zn3FPP45
+# CHECK-NEXT: [13]  - Zn3FPSt
+# CHECK-NEXT: [14.0] - Zn3LSU
+# CHECK-NEXT: [14.1] - Zn3LSU
+# CHECK-NEXT: [14.2] - Zn3LSU
+# CHECK-NEXT: [15.0] - Zn3Load
+# CHECK-NEXT: [15.1] - Zn3Load
+# CHECK-NEXT: [15.2] - Zn3Load
+# CHECK-NEXT: [16.0] - Zn3Store
+# CHECK-NEXT: [16.1] - Zn3Store
+
+# CHECK:      Resource pressure per iteration:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1]
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50   0.50   0.50   0.50    -      -      -      -      -      -      -      -      -      -      -
+
+# CHECK:      Resource pressure by instruction:
+# CHECK-NEXT: [0]    [1]    [2]    [3]    [4]    [5]    [6]    [7]    [8]    [9]    [10]   [11]   [12.0] [12.1] [13]   [14.0] [14.1] [14.2] [15.0] [15.1] [15.2] [16.0] [16.1] Instructions:
+# CHECK-NEXT:  -      -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -     vpcmpgtq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:  -      -      -      -      -      -      -      -     0.50    -     0.50    -      -      -      -      -      -      -      -      -      -      -      -     vpaddq	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Timeline view:
+# CHECK-NEXT: Index     0123456
+
+# CHECK:      [0,0]     DeER ..   vpcmpgtq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [0,1]     D=eER..   vpaddq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,0]     D==eER.   vpcmpgtq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: [1,1]     D===eER   vpaddq	%ymm0, %ymm0, %ymm0
+
+# CHECK:      Average Wait times (based on the timeline view):
+# CHECK-NEXT: [0]: Executions
+# CHECK-NEXT: [1]: Average time spent waiting in a scheduler's queue
+# CHECK-NEXT: [2]: Average time spent waiting in a scheduler's queue while ready
+# CHECK-NEXT: [3]: Average time elapsed from WB until retire stage
+
+# CHECK:            [0]    [1]    [2]    [3]
+# CHECK-NEXT: 0.     2     2.0    0.5    0.0       vpcmpgtq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT: 1.     2     3.0    0.0    0.0       vpaddq	%ymm0, %ymm0, %ymm0
+# CHECK-NEXT:        2     2.5    0.3    0.0       <total>
