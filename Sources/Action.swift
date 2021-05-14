301517c0f4c6bdac9e12c15b1dacc527d7601e47
Geoff Romer <gromer@google.com>
2021-05-14 11:12:31 -0700

    Apply suggestions from code review
    Co-authored-by: Dave Abrahams <dabrahams@google.com>

--- Sources/Action.swift
+++ Sources/Action.swift
@@ -91,7 +91,8 @@ struct EvaluateTupleLiteral: Action {
   }
 }
 
-struct NoopAction: Action {
+/// An action that just provides a reference-point for a scope.
+struct NoOpAction: Action {
   mutating func run(on state: inout Interpreter) -> Followup {
     return .done
   }
