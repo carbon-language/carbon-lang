2d1f2ba7d51689de231794cf3dd74257aeccd315
Stella Stamenova <stilis@microsoft.com>
2021-05-17 20:26:59 -0700

    Revert "[ADT] Add new type traits for type pack indexes"
    This reverts commit a6d3987b8ef3b7616f0835b89515c4264f2a7a64.

--- llvm/unittests/ADT/STLExtrasTest.cpp
+++ llvm/unittests/ADT/STLExtrasTest.cpp
@@ -711,40 +711,4 @@ TEST(STLExtras, MoveRange) {
   EXPECT_EQ(V4.size(), 4U);
   EXPECT_TRUE(llvm::all_of(V4, HasVal));
 }
-
-TEST(STLExtrasTest, TypesAreDistinct) {
-  EXPECT_TRUE((llvm::TypesAreDistinct<>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int>::value));
-  EXPECT_FALSE((llvm::TypesAreDistinct<int, int>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, float>::value));
-  EXPECT_FALSE((llvm::TypesAreDistinct<int, float, int>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, float, double>::value));
-  EXPECT_FALSE((llvm::TypesAreDistinct<int, float, double, float>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, int *>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, int &>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, int &&>::value));
-  EXPECT_TRUE((llvm::TypesAreDistinct<int, const int>::value));
-}
-
-TEST(STLExtrasTest, FirstIndexOfType) {
-  EXPECT_EQ((llvm::FirstIndexOfType<int, int>::value), 0u);
-  EXPECT_EQ((llvm::FirstIndexOfType<int, int, int>::value), 0u);
-  EXPECT_EQ((llvm::FirstIndexOfType<int, float, int>::value), 1u);
-  EXPECT_EQ((llvm::FirstIndexOfType<int const *, float, int, int const *,
-                                    const int>::value),
-            2u);
-}
-
-TEST(STLExtrasTest, TypeAtIndex) {
-  EXPECT_TRUE((std::is_same<int, llvm::TypeAtIndex<0, int>>::value));
-  EXPECT_TRUE((std::is_same<int, llvm::TypeAtIndex<0, int, float>>::value));
-  EXPECT_TRUE((std::is_same<float, llvm::TypeAtIndex<1, int, float>>::value));
-  EXPECT_TRUE(
-      (std::is_same<float, llvm::TypeAtIndex<1, int, float, double>>::value));
-  EXPECT_TRUE(
-      (std::is_same<float, llvm::TypeAtIndex<1, int, float, double>>::value));
-  EXPECT_TRUE(
-      (std::is_same<double, llvm::TypeAtIndex<2, int, float, double>>::value));
-}
-
 } // namespace
