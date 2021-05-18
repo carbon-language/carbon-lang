ef6e1213b1c0af387fe13754e02a14f3d31018a0
Lang Hames <lhames@gmail.com>
2021-05-18 09:22:07 -0700

    [ORC] Add a CPU getter to JITTargetMachineBuilder.

--- llvm/include/llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h
+++ llvm/include/llvm/ExecutionEngine/Orc/JITTargetMachineBuilder.h
@@ -79,6 +79,9 @@ public:
     return *this;
   }
 
+  /// Returns the CPU string.
+  const std::string &getCPU() const { return CPU; }
+
   /// Set the relocation model.
   JITTargetMachineBuilder &setRelocationModel(Optional<Reloc::Model> RM) {
     this->RM = std::move(RM);
