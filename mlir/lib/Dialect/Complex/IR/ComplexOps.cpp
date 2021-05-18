fa765a09440253fd16e92376b4cf132873afe84e
Adrian Kuegel <akuegel@google.com>
2021-05-18 10:40:49 +0200

    [mlir] Add folder for complex.ReOp and complex.ImOp.
    Now that complex constants are supported, we can also fold.
    
    Differential Revision: https://reviews.llvm.org/D102616

--- mlir/lib/Dialect/Complex/IR/ComplexOps.cpp
+++ mlir/lib/Dialect/Complex/IR/ComplexOps.cpp
@@ -17,3 +17,19 @@ using namespace mlir::complex;
 
 #define GET_OP_CLASSES
 #include "mlir/Dialect/Complex/IR/ComplexOps.cpp.inc"
+
+OpFoldResult ReOp::fold(ArrayRef<Attribute> operands) {
+  assert(operands.size() == 1 && "unary op takes 1 operand");
+  ArrayAttr arrayAttr = operands[0].dyn_cast_or_null<ArrayAttr>();
+  if (arrayAttr && arrayAttr.size() == 2)
+    return arrayAttr[0];
+  return {};
+}
+
+OpFoldResult ImOp::fold(ArrayRef<Attribute> operands) {
+  assert(operands.size() == 1 && "unary op takes 1 operand");
+  ArrayAttr arrayAttr = operands[0].dyn_cast_or_null<ArrayAttr>();
+  if (arrayAttr && arrayAttr.size() == 2)
+    return arrayAttr[1];
+  return {};
+}
