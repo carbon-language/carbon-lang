00f90e3fca79e8ba9bf44e7e3a0da0e6311af822
Roman Lebedev <lebedev.ri@gmail.com>
2021-05-19 11:49:16 +0300

    [NFCI][SimplifyCFG] simplifySingleResume(): use DeleteDeadBlock()

--- llvm/lib/Transforms/Utils/SimplifyCFG.cpp
+++ llvm/lib/Transforms/Utils/SimplifyCFG.cpp
@@ -4426,10 +4426,7 @@ bool SimplifyCFGOpt::simplifySingleResume(ResumeInst *RI) {
   }
 
   // The landingpad is now unreachable.  Zap it.
-  if (DTU)
-    DTU->deleteBB(BB);
-  else
-    BB->eraseFromParent();
+  DeleteDeadBlock(BB, DTU);
   return true;
 }
 
