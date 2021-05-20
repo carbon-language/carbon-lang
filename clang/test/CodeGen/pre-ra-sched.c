603818b97c795114f66a6fc13e8a5f0e54b49a13
Hubert Tong <hubert.reinterpretcast@gmail.com>
2021-05-20 09:50:33 -0400

    [test] Fix pre-ra-sched.c to check for error message from stderr
    The test previous accidentally passed because it was looking for a lack
    of specific input from the binary(!) output being sent to stdout.

--- clang/test/CodeGen/pre-ra-sched.c
+++ clang/test/CodeGen/pre-ra-sched.c
@@ -1,4 +1,4 @@
-// RUN: %clang %s -mllvm -pre-RA-sched=fast -c -o - | FileCheck %s
-// RUN: %clang %s -mllvm -pre-RA-sched=linearize -c -o - | FileCheck %s
+// RUN: %clang %s -mllvm -pre-RA-sched=fast -c -o %t-fast.o 2>&1 | FileCheck --allow-empty %s
+// RUN: %clang %s -mllvm -pre-RA-sched=linearize -c -o %t-linearize.o 2>&1 | FileCheck --allow-empty %s
 
 // CHECK-NOT: clang (LLVM option parsing): for the --pre-RA-sched option: Cannot find option named
