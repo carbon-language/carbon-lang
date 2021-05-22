5ff79f001feb6584e87173348a24f3f317e35984
Petr Hosek <phosek@google.com>
2021-05-22 15:49:46 -0700

    Revert "[Driver] Support libc++ in MSVC"
    This reverts commit b604301be3559fb85a11779db79fc9bda4b62bce since
    it caused compilation failure in sanitizer_unwind_win.cpp when using
    the runtimes build.

--- clang/test/Driver/msvc-libcxx.cpp
+++
@@ -1,7 +0,0 @@
-// RUN: %clangxx -### %s 2>&1 -stdlib=libc++ -fuse-ld=lld \
-// RUN:   --target=x86_64-pc-windows-msvc \
-// RUN:   -ccc-install-dir %S/Inputs/msvc_libcxx_tree/usr/bin \
-// RUN:   | FileCheck %s -check-prefix MSVC-LIBCXX
-// MSVC-LIBCXX: "-internal-isystem" "{{.*[/\\]}}include{{/|\\\\}}x86_64-pc-windows-msvc{{/|\\\\}}c++{{/|\\\\}}v1"
-// MSVC-LIBCXX: "-internal-isystem" "{{.*[/\\]}}include{{/|\\\\}}c++{{/|\\\\}}v1"
-// MSVC-LIBCXX: "-libpath:{{.*}}{{/|\\\\}}..{{/|\\\\}}lib{{/|\\\\}}x86_64-pc-windows-msvc"
