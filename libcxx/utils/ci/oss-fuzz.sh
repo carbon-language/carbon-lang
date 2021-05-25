d95a4b950dd38c745ef98e0f22ff22755196571d
Louis Dionne <ldionne.2@gmail.com>
2021-05-25 12:52:14 -0400

    [libc++] Try to fix the oss-fuzz failure

--- libcxx/utils/ci/oss-fuzz.sh
+++ libcxx/utils/ci/oss-fuzz.sh
@@ -8,6 +8,16 @@ if [[ ${SANITIZER} = *undefined* ]]; then
   CXXFLAGS="${CXXFLAGS} -fsanitize=unsigned-integer-overflow -fsanitize-trap=unsigned-integer-overflow"
 fi
 
+BUILD=cxx_build_dir
+INSTALL=cxx_install_dir
+
+mkdir ${BUILD}
+cmake -S ${PWD} -B ${BUILD} \
+      -DLLVM_ENABLE_PROJECTS="libcxx;libcxxabi" \
+      -DCMAKE_BUILD_TYPE=RelWithDebInfo \
+      -DCMAKE_INSTALL_PREFIX="${INSTALL}"
+cmake --build ${BUILD} --target install-cxx-headers
+
 for test in libcxx/test/libcxx/fuzzing/*.pass.cpp; do
     exe="$(basename ${test})"
     exe="${exe%.pass.cpp}"
@@ -15,7 +25,7 @@ for test in libcxx/test/libcxx/fuzzing/*.pass.cpp; do
         -std=c++14 \
         -DLIBCPP_OSS_FUZZ \
         -D_LIBCPP_HAS_NO_VENDOR_AVAILABILITY_ANNOTATIONS \
-        -nostdinc++ -cxx-isystem libcxx/include \
+        -nostdinc++ -cxx-isystem ${INSTALL}/include/c++/v1 \
         -lpthread -ldl \
         -o "${OUT}/${exe}" \
         ${test} \
