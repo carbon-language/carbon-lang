a96f875fe98de5eb5f44b0148baa442c998364ed
Fangrui Song <i@maskray.me>
2021-06-03 15:26:34 -0700

    [CMake][ELF] Add -fno-semantic-interposition for GCC and Clang>=13
    In a `-DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -DLLVM_LINK_LLVM_DYLIB=on -DCLANG_LINK_CLANG_DYLIB=on`
    build, libLLVM-13git.so is 2% smaller and libclang-cpp.so is 1% smaller (on top of -Wl,-Bsymbolic-functions).
    There may be some small performance improvement as well because GCC
    -fPIC suppresses interprocedural optimizations for non-inline
    definitions by default.
    
    Note: we cannot add -fno-semantic-interposition for Clang<13.  Clang<13's
    implementation additionally optimizes global variables, which is incompatible
    with unfortunate ELF -fno-pic default: direct access relocations for external
    data. If the executable has a -fno-pic object file referencing a global variable
    declared in a public header, the direct access relocation will cause a copy
    relocation. The executable and libLLVM.so/libclang-cpp.so will disagree on the
    address.
    
    Reviewed By: phosek
    
    Differential Revision: https://reviews.llvm.org/D102453

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -305,6 +305,17 @@ if( LLVM_ENABLE_PIC )
     # On Windows all code is PIC. MinGW warns if -fPIC is used.
   else()
     add_flag_or_print_warning("-fPIC" FPIC)
+    # Enable interprocedural optimizations for non-inline functions which would
+    # otherwise be disabled due to GCC -fPIC's default.
+    #
+    # Note: Clang allows IPO for -fPIC so this optimization is less effective.
+    # Older Clang may support -fno-semantic-interposition but it used local
+    # aliases to optimize global variables, which is incompatible with copy
+    # relocations due to -fno-pic.
+    if (CMAKE_COMPILER_IS_GNUCXX OR (CMAKE_CXX_COMPILER_ID MATCHES "Clang" AND
+        CMAKE_CXX_COMPILER_VERSION GREATER_EQUAL 13))
+      add_flag_if_supported("-fno-semantic-interposition" FNO_SEMANTIC_INTERPOSITION)
+    endif()
   endif()
   # GCC for MIPS can miscompile LLVM due to PR37701.
   if(CMAKE_COMPILER_IS_GNUCXX AND LLVM_NATIVE_ARCH STREQUAL "Mips" AND
