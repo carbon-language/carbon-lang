738abfdbea21acd2597d83ad3390daf5696b6d07
Arthur Eubanks <aeubanks@google.com>
2021-06-03 18:57:36 -0700

    [NFC] Remove checking pointee type for byval/preallocated type
    These currently always require a type parameter. The bitcode reader
    already upgrades old bitcode without the type parameter to use the
    pointee type.

--- llvm/include/llvm/IR/InstrTypes.h
+++ llvm/include/llvm/IR/InstrTypes.h
@@ -1729,13 +1729,13 @@ public:
   /// Extract the byval type for a call or parameter.
   Type *getParamByValType(unsigned ArgNo) const {
     Type *Ty = Attrs.getParamByValType(ArgNo);
-    return Ty ? Ty : getArgOperand(ArgNo)->getType()->getPointerElementType();
+    return Ty;
   }
 
   /// Extract the preallocated type for a call or parameter.
   Type *getParamPreallocatedType(unsigned ArgNo) const {
     Type *Ty = Attrs.getParamPreallocatedType(ArgNo);
-    return Ty ? Ty : getArgOperand(ArgNo)->getType()->getPointerElementType();
+    return Ty;
   }
 
   /// Extract the number of dereferenceable bytes for a call or
