927688a4cd9dcde92e8bf430e6ceefac6ab8c8a0
Kevin Athey <kda@google.com>
2021-06-04 20:21:01 -0700

    Update and improve compiler-rt tests for -mllvm -asan_use_after_return=(never|[runtime]|always).
    In addition:
      - optionally add global flag to capture compile intent for UAR:
        __asan_detect_use_after_return_always.
        The global is a SANITIZER_WEAK_ATTRIBUTE.
    
    for issue: https://github.com/google/sanitizers/issues/1394
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D103304

--- compiler-rt/test/asan/TestCases/Linux/uar_signals.cpp
+++ compiler-rt/test/asan/TestCases/Linux/uar_signals.cpp
@@ -1,6 +1,8 @@
 // This test checks that the implementation of use-after-return
 // is async-signal-safe.
 // RUN: %clangxx_asan -std=c++11 -O1 %s -o %t -pthread && %run %t
+// RUN: %clangxx_asan -std=c++11 -O1 %s -o %t -pthread -mllvm -asan-use-after-return=never && %run %t
+// RUN: %clangxx_asan -std=c++11 -O1 %s -o %t -pthread -mllvm -asan-use-after-return=always && %run %t
 // REQUIRES: stable-runtime
 #include <signal.h>
 #include <stdlib.h>
