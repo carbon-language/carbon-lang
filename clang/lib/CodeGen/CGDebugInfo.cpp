64b2fb7967a749b83f59656f0cd2f4d00501efaa
Hongtao Yu <hoy@fb.com>
2021-06-08 09:35:28 -0700

    [CSSPGO] Emit mangled dwarf names for line tables debug option under -fpseudo-probe-for-profiling
    Reviewed By: wenlei
    
    Differential Revision: https://reviews.llvm.org/D103909

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -3551,6 +3551,7 @@ void CGDebugInfo::collectFunctionDeclProps(GlobalDecl GD, llvm::DIFile *Unit,
   if (LinkageName == Name || (!CGM.getCodeGenOpts().EmitGcovArcs &&
                               !CGM.getCodeGenOpts().EmitGcovNotes &&
                               !CGM.getCodeGenOpts().DebugInfoForProfiling &&
+                              !CGM.getCodeGenOpts().PseudoProbeForProfiling &&
                               DebugKind <= codegenoptions::DebugLineTablesOnly))
     LinkageName = StringRef();
 
