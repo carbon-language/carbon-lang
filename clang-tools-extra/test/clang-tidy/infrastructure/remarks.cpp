47d138c93992f779a5dd0810b0e7402e043df61d
Ivan Murashko <ivanmurashko@fb.com>
2021-06-11 01:44:56 -0700

    [clang-tidy] LIT test fix for Remark diagnostic
    There is a followup fix for a unit test introduced at D102906. The test file was placed into a temp folder and test assumed that it would be visible without the full path specification.
    
    This behaviour can be changed in future and it would be good to specify full path to the file at the test.
    
    Test Plan:
    ```
    ninja check-clang-tools
    ```
    
    Reviewed By: DmitryPolukhin
    
    Differential Revision: https://reviews.llvm.org/D104021

--- clang-tools-extra/test/clang-tidy/infrastructure/remarks.cpp
+++ clang-tools-extra/test/clang-tidy/infrastructure/remarks.cpp
@@ -2,14 +2,13 @@
 // RUN: cp -r %S/Inputs/remarks %t
 // RUN: cp %s %t/t.cpp
 
-// RUN: clang-tidy -checks='-*,modernize-use-override,clang-diagnostic-module-import' t.cpp -- \
+// RUN: clang-tidy -checks='-*,modernize-use-override,clang-diagnostic-module-import' %t/t.cpp -- \
 // RUN:     -fmodules -fimplicit-module-maps -fmodules-cache-path=%t/cache \
 // RUN:     -fsyntax-only \
 // RUN:     -I%S/Inputs/remarks \
 // RUN:     -working-directory=%t \
-// RUN:     -Rmodule-build -Rmodule-import t.cpp 2>&1 |\
+// RUN:     -Rmodule-build -Rmodule-import 2>&1 |\
 // RUN: FileCheck %s -implicit-check-not "remark:"
 
 #include "A.h"
 // CHECK: remark: importing module 'A' from {{.*}} [clang-diagnostic-module-import]
-
