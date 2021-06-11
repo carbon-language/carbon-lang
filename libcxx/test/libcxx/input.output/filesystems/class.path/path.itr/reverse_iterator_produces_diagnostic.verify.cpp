f32f3db9fcbf43b1a603c5da882557ebed62f1c5
Louis Dionne <ldionne.2@gmail.com>
2021-06-11 09:55:11 -0400

    [libc++] Split the various iterator types out of <iterator>
    Differential Revision: https://reviews.llvm.org/D104669

--- libcxx/test/libcxx/input.output/filesystems/class.path/path.itr/reverse_iterator_produces_diagnostic.verify.cpp
+++ libcxx/test/libcxx/input.output/filesystems/class.path/path.itr/reverse_iterator_produces_diagnostic.verify.cpp
@@ -20,7 +20,7 @@ int main(int, char**) {
   using namespace fs;
   using RIt = std::reverse_iterator<path::iterator>;
 
-  // expected-error-re@iterator:* {{static_assert failed{{.*}} "The specified iterator type cannot be used with reverse_iterator; Using stashing iterators with reverse_iterator causes undefined behavior"}}
+  // expected-error-re@*:* {{static_assert failed{{.*}} "The specified iterator type cannot be used with reverse_iterator; Using stashing iterators with reverse_iterator causes undefined behavior"}}
   {
     RIt r;
     ((void)r);
