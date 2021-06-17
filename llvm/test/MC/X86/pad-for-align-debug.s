aa6e8e9572d54d8fdcd6e8789ea7076640011066
Florian Hahn <flo@fhahn.com>
2021-06-17 13:08:53 +0100

    [X86] Check using default in test added in 0bd5bbb31e0345ae.
    Make sure llvm-mc is invariant with respect to debug locations in the
    test (checks update to use the -x86-pad-for-align default value)

--- llvm/test/MC/X86/pad-for-align-debug.s
+++ llvm/test/MC/X86/pad-for-align-debug.s
@@ -1,7 +1,7 @@
 # RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos -x86-pad-for-align=true %S/Inputs/pad-align-with-debug.s | llvm-objdump -d - | FileCheck --check-prefix=CHECK --check-prefix=DEBUG %s
 # RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos -x86-pad-for-align=true %S/Inputs/pad-align-without-debug.s | llvm-objdump -d - | FileCheck --check-prefix=CHECK --check-prefix=NODEBUG %s
-# RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos -x86-pad-for-align=false %S/Inputs/pad-align-without-debug.s | llvm-objdump -d - | FileCheck --check-prefix=NOPAD %s
-# RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos -x86-pad-for-align=false %S/Inputs/pad-align-with-debug.s | llvm-objdump -d - | FileCheck --check-prefix=NOPAD %s
+# RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos %S/Inputs/pad-align-without-debug.s | llvm-objdump -d - | FileCheck --check-prefix=DEFAULT %s
+# RUN: llvm-mc -mcpu=skylake -filetype=obj -triple x86_64-apple-macos %S/Inputs/pad-align-with-debug.s | llvm-objdump -d - | FileCheck --check-prefix=DEFAULT %s
 
 ; Test case to show that -x86-pad-for-align causes binary differences in the
 ; presence of debug locations. Inputs/pad-align-with-debug.s and
@@ -51,31 +51,31 @@
 ; CHECK-NEXT:      50: c3                               retq
 ; CHECK-NEXT:      51: c3                               retq
 
-; NOPAD:            0: 48 89 f8                         movq    %rdi, %rax
-; NOPAD-NEXT:       3: 0f 1f 44 00 00                   nopl    (%rax,%rax)
-; NOPAD-NEXT:       8: 0f b6 10                         movzbl  (%rax), %edx
-; NOPAD-NEXT:       b: 80 fa 0a                         cmpb    $10, %dl
-; NOPAD-NEXT:       e: 74 30                            je  0x40 <foo+0x40>
-; NOPAD-NEXT:      10: 80 fa 64                         cmpb    $100, %dl
-; NOPAD-NEXT:      13: 74 31                            je  0x46 <foo+0x46>
-; NOPAD-NEXT:      15: 80 fa c8                         cmpb    $-56, %dl
-; NOPAD-NEXT:      18: 74 2c                            je  0x46 <foo+0x46>
-; NOPAD-NEXT:      1a: 80 fa 2c                         cmpb    $44, %dl
-; NOPAD-NEXT:      1d: 75 27                            jne 0x46 <foo+0x46>
-; NOPAD-NEXT:      1f: 90                               nop
-; NOPAD-NEXT:      20: 48 89 c2                         movq    %rax, %rdx
-; NOPAD-NEXT:      23: 48 ff c0                         incq    %rax
-; NOPAD-NEXT:      26: 80 38 1e                         cmpb    $30, (%rax)
-; NOPAD-NEXT:      29: 75 f5                            jne 0x20 <foo+0x20>
-; NOPAD-NEXT:      2b: 48 8d 42 02                      leaq    2(%rdx), %rax
-; NOPAD-NEXT:      2f: 0f b6 10                         movzbl  (%rax), %edx
-; NOPAD-NEXT:      32: 80 fa 5a                         cmpb    $90, %dl
-; NOPAD-NEXT:      35: 75 d9                            jne 0x10 <foo+0x10>
-; NOPAD-NEXT:      37: 66 0f 1f 84 00 00 00 00 00       nopw    (%rax,%rax)
-; NOPAD-NEXT:      40: 80 78 04 63                      cmpb    $99, 4(%rax)
-; NOPAD-NEXT:      44: 74 0b                            je  0x51 <foo+0x51>
-; NOPAD-NEXT:      46: 48 ff c0                         incq    %rax
-; NOPAD-NEXT:      49: eb bd                            jmp 0x8 <foo+0x8>
-; NOPAD-NEXT:      4b: 0f 1f 44 00 00                   nopl    (%rax,%rax)
-; NOPAD-NEXT:      50: c3                               retq
-; NOPAD-NEXT:      51: c3                               retq
+; DEFAULT:            0: 48 89 f8                         movq    %rdi, %rax
+; DEFAULT-NEXT:       3: 0f 1f 44 00 00                   nopl    (%rax,%rax)
+; DEFAULT-NEXT:       8: 0f b6 10                         movzbl  (%rax), %edx
+; DEFAULT-NEXT:       b: 80 fa 0a                         cmpb    $10, %dl
+; DEFAULT-NEXT:       e: 74 30                            je  0x40 <foo+0x40>
+; DEFAULT-NEXT:      10: 80 fa 64                         cmpb    $100, %dl
+; DEFAULT-NEXT:      13: 74 31                            je  0x46 <foo+0x46>
+; DEFAULT-NEXT:      15: 80 fa c8                         cmpb    $-56, %dl
+; DEFAULT-NEXT:      18: 74 2c                            je  0x46 <foo+0x46>
+; DEFAULT-NEXT:      1a: 80 fa 2c                         cmpb    $44, %dl
+; DEFAULT-NEXT:      1d: 75 27                            jne 0x46 <foo+0x46>
+; DEFAULT-NEXT:      1f: 90                               nop
+; DEFAULT-NEXT:      20: 48 89 c2                         movq    %rax, %rdx
+; DEFAULT-NEXT:      23: 48 ff c0                         incq    %rax
+; DEFAULT-NEXT:      26: 80 38 1e                         cmpb    $30, (%rax)
+; DEFAULT-NEXT:      29: 75 f5                            jne 0x20 <foo+0x20>
+; DEFAULT-NEXT:      2b: 48 8d 42 02                      leaq    2(%rdx), %rax
+; DEFAULT-NEXT:      2f: 0f b6 10                         movzbl  (%rax), %edx
+; DEFAULT-NEXT:      32: 80 fa 5a                         cmpb    $90, %dl
+; DEFAULT-NEXT:      35: 75 d9                            jne 0x10 <foo+0x10>
+; DEFAULT-NEXT:      37: 66 0f 1f 84 00 00 00 00 00       nopw    (%rax,%rax)
+; DEFAULT-NEXT:      40: 80 78 04 63                      cmpb    $99, 4(%rax)
+; DEFAULT-NEXT:      44: 74 0b                            je  0x51 <foo+0x51>
+; DEFAULT-NEXT:      46: 48 ff c0                         incq    %rax
+; DEFAULT-NEXT:      49: eb bd                            jmp 0x8 <foo+0x8>
+; DEFAULT-NEXT:      4b: 0f 1f 44 00 00                   nopl    (%rax,%rax)
+; DEFAULT-NEXT:      50: c3                               retq
+; DEFAULT-NEXT:      51: c3                               retq
