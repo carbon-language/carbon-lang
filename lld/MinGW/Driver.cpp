fdf54f5c506a12ca509b5af0967347a2848b3b77
Martin Storsjö <martin@martin.st>
2021-06-18 14:33:28 +0300

    [LLD] [MinGW] Print the lld-link command to stderr
    This is consistent with how clang prints its internal commands with
    -### and -v.
    
    When linking with -verbose, we get log messages from the actual
    linking written to stderr. By printing the command to the same stream,
    we make sure they appear in a sensible chronological order.
    
    Differential Revision: https://reviews.llvm.org/D104527

--- lld/MinGW/Driver.cpp
+++ lld/MinGW/Driver.cpp
@@ -428,7 +428,7 @@ bool mingw::link(ArrayRef<const char *> argsArr, bool canExitEarly,
     return false;
 
   if (args.hasArg(OPT_verbose) || args.hasArg(OPT__HASH_HASH_HASH))
-    lld::outs() << llvm::join(linkArgs, " ") << "\n";
+    lld::errs() << llvm::join(linkArgs, " ") << "\n";
 
   if (args.hasArg(OPT__HASH_HASH_HASH))
     return true;
