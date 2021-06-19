c9b241efd68c5a0f1f67e9250960ade454f3bc11
Nico Weber <thakis@chromium.org>
2021-06-18 20:22:15 -0400

    Revert "[lld-macho] Avoid force-loading the same archive twice"
    This reverts commit 24706cd73cd150543753a2e169c68a2c68da46a1.
    Test seems to fail flakily. See comments on https://reviews.llvm.org/D104353
    for a hypothesis for why.

--- lld/MachO/Driver.cpp
+++ lld/MachO/Driver.cpp
@@ -235,8 +235,6 @@ static std::vector<ArchiveMember> getArchiveMembers(MemoryBufferRef mb) {
   return v;
 }
 
-static DenseMap<StringRef, ArchiveFile *> loadedArchives;
-
 static InputFile *addFile(StringRef path, bool forceLoadArchive,
                           bool isExplicit = true,
                           bool isBundleLoader = false) {
@@ -249,13 +247,6 @@ static InputFile *addFile(StringRef path, bool forceLoadArchive,
   file_magic magic = identify_magic(mbref.getBuffer());
   switch (magic) {
   case file_magic::archive: {
-    // Avoid loading archives twice. If the archives are being force-loaded,
-    // loading them twice would create duplicate symbol errors. In the
-    // non-force-loading case, this is just a minor performance optimization.
-    ArchiveFile *&cachedFile = loadedArchives[path];
-    if (cachedFile)
-      return cachedFile;
-
     std::unique_ptr<object::Archive> file = CHECK(
         object::Archive::create(mbref), path + ": failed to parse archive");
 
@@ -295,7 +286,7 @@ static InputFile *addFile(StringRef path, bool forceLoadArchive,
       }
     }
 
-    newFile = cachedFile = make<ArchiveFile>(std::move(file));
+    newFile = make<ArchiveFile>(std::move(file));
     break;
   }
   case file_magic::macho_object:
