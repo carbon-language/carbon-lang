35cf5b109769ceb356a9013ca0c0f60fbd230080
Raphael Isemann <teemperor@gmail.com>
2021-06-22 11:55:36 +0200

    [lldb] Bumb Clang version requirement for TestBasicEntryValues.py to 11
    The test only passes with Clang>=11 so adjust the decorator.
    
    Failure output for Clang 10 is:
    
    --- FileCheck trace (code=1) ---
    FileCheck main.cpp -check-prefix=FUNC1-GNU
    
    FileCheck input:
          Address: a.out[0x0000000000401127] (a.out.PT_LOAD[1]..text + 263)
          Summary: a.out`func1(int&) + 23 at main.cpp:25:1
           Module: file = "functionalities/param_entry_vals/basic_entry_values/BasicEntryValues_GNU.test_dwo/a.out", arch = "x86_64"
      CompileUnit: id = {0x00000000}, file = "functionalities/param_entry_vals/basic_entry_values/main.cpp", language = "c++11"
         Function: id = {0x400000000000010a}, name = "func1(int&)", mangled = "_Z5func1Ri", range = [0x0000000000401110-0x0000000000401129)
         FuncType: id = {0x400000000000010a}, byte-size = 0, decl = main.cpp:13, compiler_type = "void (int &)"
           Blocks: id = {0x400000000000010a}, range = [0x00401110-0x00401129)
        LineEntry: [0x0000000000401127-0x0000000000401130): functionalities/param_entry_vals/basic_entry_values/main.cpp:25:1
           Symbol: id = {0x0000002c}, range = [0x0000000000401110-0x0000000000401129), name="func1(int&)", mangled="_Z5func1Ri"
    
    FileCheck output:
    
    functionalities/param_entry_vals/basic_entry_values/main.cpp:23:16: error: FUNC1-GNU: expected string not found in input
     // FUNC1-GNU: name = "sink", type = "int &", location = DW_OP_GNU_entry_value

--- lldb/test/API/functionalities/param_entry_vals/basic_entry_values/TestBasicEntryValues.py
+++ lldb/test/API/functionalities/param_entry_vals/basic_entry_values/TestBasicEntryValues.py
@@ -4,7 +4,7 @@ from lldbsuite.test import lldbplatformutil
 
 supported_archs = ["x86_64", "aarch64"]
 decorators = [skipIf(archs=no_match(supported_archs)),
-         skipIf(compiler="clang", compiler_version=['<', '10.0']),
+         skipIf(compiler="clang", compiler_version=['<', '11.0']),
          skipUnlessHasCallSiteInfo,
          skipIf(dwarf_version=['<', '4'])]
 
