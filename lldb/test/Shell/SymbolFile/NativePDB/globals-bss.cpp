9952d591ccc49cbcbf9c89d5191e6111c44703a6
Stella Stamenova <stilis@microsoft.com>
2021-06-29 13:39:18 -0700

    [lldb] Fix globals-bss.cpp which was broken in https://reviews.llvm.org/D105055
    -S replaced -s, so the test needs to be updated to use the new option

--- lldb/test/Shell/SymbolFile/NativePDB/globals-bss.cpp
+++ lldb/test/Shell/SymbolFile/NativePDB/globals-bss.cpp
@@ -4,7 +4,7 @@
 // Make sure we can read variables from BSS
 // RUN: %clang_cl --target=x86_64-windows-msvc -Od -Z7 -c /Fo%t.obj -- %s
 // RUN: lld-link -debug:full -nodefaultlib -entry:main %t.obj -out:%t.exe -pdb:%t.pdb
-// RUN: llvm-readobj -s %t.exe | FileCheck --check-prefix=BSS %s
+// RUN: llvm-readobj -S %t.exe | FileCheck --check-prefix=BSS %s
 // RUN: env LLDB_USE_NATIVE_PDB_READER=1 %lldb -f %t.exe -s \
 // RUN:     %p/Inputs/globals-bss.lldbinit 2>&1 | FileCheck %s
 
