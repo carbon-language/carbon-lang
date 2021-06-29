948308ef34dc7da8bb741a85eb9941cc2b05d227
Michael Liao <michael.hliao@gmail.com>
2021-06-28 22:48:26 -0400

    Fix `-Wunused-variable` warning. NFC.

--- clang/lib/CodeGen/CGCall.cpp
+++ clang/lib/CodeGen/CGCall.cpp
@@ -2173,7 +2173,7 @@ void CodeGenModule::ConstructAttributeList(StringRef Name,
   // Add "sample-profile-suffix-elision-policy" attribute for internal linkage
   // functions with -funique-internal-linkage-names.
   if (TargetDecl && CodeGenOpts.UniqueInternalLinkageNames) {
-    if (auto *Fn = dyn_cast<FunctionDecl>(TargetDecl)) {
+    if (isa<FunctionDecl>(TargetDecl)) {
       if (this->getFunctionLinkage(CalleeInfo.getCalleeDecl()) ==
           llvm::GlobalValue::InternalLinkage)
         FuncAttrs.addAttribute("sample-profile-suffix-elision-policy",
