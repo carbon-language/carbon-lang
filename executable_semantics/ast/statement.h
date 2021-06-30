5348b891999d2b01488a120701d13d852d43c64c
Geoff Romer <gromer@google.com>
2021-06-30 11:10:36 -0700

    Pass Expressions by value in Statement factories. (#611)

--- executable_semantics/ast/statement.h
+++ executable_semantics/ast/statement.h
@@ -80,25 +80,23 @@ struct Statement {
   StatementKind tag;
 
   // Constructors
-  static auto MakeExpStmt(int line_num, const Expression* exp)
+  static auto MakeExpStmt(int line_num, Expression exp) -> const Statement*;
+  static auto MakeAssign(int line_num, Expression lhs, Expression rhs)
       -> const Statement*;
-  static auto MakeAssign(int line_num, const Expression* lhs,
-                         const Expression* rhs) -> const Statement*;
-  static auto MakeVarDef(int line_num, const Expression* pat,
-                         const Expression* init) -> const Statement*;
-  static auto MakeIf(int line_num, const Expression* cond,
-                     const Statement* then_stmt, const Statement* else_stmt)
+  static auto MakeVarDef(int line_num, Expression pat, Expression init)
       -> const Statement*;
-  static auto MakeReturn(int line_num, const Expression* e) -> const Statement*;
+  static auto MakeIf(int line_num, Expression cond, const Statement* then_stmt,
+                     const Statement* else_stmt) -> const Statement*;
+  static auto MakeReturn(int line_num, Expression e) -> const Statement*;
   static auto MakeSeq(int line_num, const Statement* s1, const Statement* s2)
       -> const Statement*;
   static auto MakeBlock(int line_num, const Statement* s) -> const Statement*;
-  static auto MakeWhile(int line_num, const Expression* cond,
-                        const Statement* body) -> const Statement*;
+  static auto MakeWhile(int line_num, Expression cond, const Statement* body)
+      -> const Statement*;
   static auto MakeBreak(int line_num) -> const Statement*;
   static auto MakeContinue(int line_num) -> const Statement*;
   static auto MakeMatch(
-      int line_num, const Expression* exp,
+      int line_num, Expression exp,
       std::list<std::pair<const Expression*, const Statement*>>* clauses)
       -> const Statement*;
   // Returns an AST node for a continuation statement give its line number and
@@ -112,8 +110,7 @@ struct Statement {
   // Returns an AST node for a run statement give its line number and argument.
   //
   //     __run <argument>;
-  static auto MakeRun(int line_num, const Expression* argument)
-      -> const Statement*;
+  static auto MakeRun(int line_num, Expression argument) -> const Statement*;
   // Returns an AST node for an await statement give its line number.
   //
   //     __await;
