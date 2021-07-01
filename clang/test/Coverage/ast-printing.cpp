e95106f50190d7a25dfad57adf0317ca6cfa9a4f
Richard Smith <richard@metafoo.co.uk>
2021-06-30 17:25:55 -0700

    Add a target triple to fix failures on MS build bots.
    -ast-dump=json tries to mangle every declaration, and under the MS ABI
    there are still a few things we can't mangle, which caused this test to
    assert.

--- clang/test/Coverage/ast-printing.cpp
+++ clang/test/Coverage/ast-printing.cpp
@@ -4,7 +4,7 @@
 // RUN: diff %t.1.cpp %t.2.cpp
 // RUN: %clang_cc1 -std=c++20 -ast-dump %s
 // RUN: %clang_cc1 -std=c++20 -ast-dump-all %s
-// RUN: %clang_cc1 -std=c++20 -ast-dump=json %s
+// RUN: %clang_cc1 -std=c++20 -ast-dump=json -triple=x86_64-linux-gnu %s
 // RUN: %clang_cc1 -std=c++20 -fdump-record-layouts %s
 
 #include "cxx-language-features.inc"
