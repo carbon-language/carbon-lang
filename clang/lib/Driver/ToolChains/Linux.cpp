e34078f121a58b503d225cf715d1494117e7948b
Ilya Leoshkevich <iii@linux.ibm.com>
2021-07-02 02:43:00 +0200

    [TSan] Enable SystemZ support
    Enable building the runtime and enable -fsanitize=thread in clang.
    
    Reviewed By: dvyukov
    
    Differential Revision: https://reviews.llvm.org/D105629

--- clang/lib/Driver/ToolChains/Linux.cpp
+++ clang/lib/Driver/ToolChains/Linux.cpp
@@ -709,7 +709,7 @@ SanitizerMask Linux::getSupportedSanitizers() const {
   if (IsX86_64 || IsMIPS64 || IsAArch64 || IsX86 || IsArmArch || IsPowerPC64 ||
       IsRISCV64 || IsSystemZ)
     Res |= SanitizerKind::Leak;
-  if (IsX86_64 || IsMIPS64 || IsAArch64 || IsPowerPC64)
+  if (IsX86_64 || IsMIPS64 || IsAArch64 || IsPowerPC64 || IsSystemZ)
     Res |= SanitizerKind::Thread;
   if (IsX86_64)
     Res |= SanitizerKind::KernelMemory;
