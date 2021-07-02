a5c3f10b7527e1aa020d5d05703bb816a3e936fb
Jonas Devlieghere <jonas@devlieghere.com>
2021-07-02 15:33:46 -0700

    [lldb] Update shebang in heap.py and crashlog.py

--- lldb/examples/darwin/heap_find/heap.py
+++ lldb/examples/darwin/heap_find/heap.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #----------------------------------------------------------------------
 # This module is designed to live inside the "lldb" python package
--- lldb/examples/python/crashlog.py
+++ lldb/examples/python/crashlog.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 #----------------------------------------------------------------------
 # Be sure to add the python path that points to the LLDB shared library.
