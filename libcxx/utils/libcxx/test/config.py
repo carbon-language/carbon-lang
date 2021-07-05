f7d8754312bf3d10fb902d94f2bc84e3adf12ba4
Louis Dionne <ldionne.2@gmail.com>
2021-07-05 12:27:14 -0400

    [runtimes] Move enable_32bit to the DSL
    This is necessary for from-scratch configurations to support the 32-bit
    mode of the test suite.
    
    Differential Revision: https://reviews.llvm.org/D105435

--- libcxx/utils/libcxx/test/config.py
+++ libcxx/utils/libcxx/test/config.py
@@ -267,10 +267,6 @@ class Configuration(object):
         self.configure_compile_flags_header_includes()
         self.target_info.add_cxx_compile_flags(self.cxx.compile_flags)
         self.target_info.add_cxx_flags(self.cxx.flags)
-        # Configure feature flags.
-        enable_32bit = self.get_lit_bool('enable_32bit', False)
-        if enable_32bit:
-            self.cxx.flags += ['-m32']
         # Use verbose output for better errors
         self.cxx.flags += ['-v']
         sysroot = self.get_lit_conf('sysroot')
