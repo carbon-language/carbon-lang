2d68bb1765f978f0426f1face22e4ff37439a224
Michał Górny <mgorny@moritz.systems>
2021-07-06 18:10:50 +0200

    [compiler-rt] [test] Fix asan symbolize tests on py3.10
    Update the asan_symbolize_script for changes in argparse output
    in Python 3.10.  The parser output 'options' instead of 'optional
    arguments'.
    
    Differential Revision: https://reviews.llvm.org/D105489

--- compiler-rt/test/asan/TestCases/Posix/asan_symbolize_script/plugin_no_op_help_output.cpp
+++ compiler-rt/test/asan/TestCases/Posix/asan_symbolize_script/plugin_no_op_help_output.cpp
@@ -2,6 +2,6 @@
 // RUN: %asan_symbolize --log-level info --plugins %S/plugin_no_op.py --help 2>&1 | FileCheck %s
 // CHECK: Registering plugin NoOpPlugin
 // CHECK: Adding --unlikely-option-name-XXX option
-// CHECK: optional arguments:
+// CHECK: option{{al arguments|s}}:
 // CHECK: --unlikely-option-name-XXX
 
