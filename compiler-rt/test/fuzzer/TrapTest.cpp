9a0152799f8e4a59e0483728c9f11c8a7805616f
Roman Lebedev <lebedev.ri@gmail.com>
2021-07-09 14:56:18 +0300

    [compiler-rt] fuzzer: adjust tests to not expect that store to null traps and is not erase
    Instead, mostly just trap directly.

--- compiler-rt/test/fuzzer/TrapTest.cpp
+++ compiler-rt/test/fuzzer/TrapTest.cpp
@@ -5,11 +5,10 @@
 // Simple test for a fuzzer. The fuzzer must find the string "Hi!".
 #include <cstddef>
 #include <cstdint>
-#include <cstdlib>
 #include <cstdio>
+#include <cstdlib>
 
 static volatile int Sink;
-static volatile int *Null = 0;
 
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   if (Size > 0 && Data[0] == 'H') {
@@ -17,11 +16,10 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
     if (Size > 1 && Data[1] == 'i') {
       Sink = 2;
       if (Size > 2 && Data[2] == '!') {
-        printf("Found the target, dereferencing NULL\n");
-        *Null = 1;
+        printf("Found the target, trapping\n");
+        __builtin_trap();
       }
     }
   }
   return 0;
 }
-
