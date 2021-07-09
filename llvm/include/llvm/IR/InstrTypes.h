784e01abca65722df8969b56d2d240cf9ced9c85
Nikita Popov <nikita.ppv@gmail.com>
2021-07-09 22:56:53 +0200

    [IR] Require matching signature in getCalledFunction()
    With opaque pointers, it's possible to directly call a function with
    a different signature, without an intermediate bitcast. However,
    lot's of code using getCalledFunction() reasonably assumes that the
    signatures match (which is always true without opaque pointers).
    Add an explicit check to that effect.
    
    The test case is from D105313, where I ran into the problem, but on
    further investigation this also affects lots of other code, we just
    have little coverage with mismatching signatures. The change from
    D105313 is still desirable for other reasons, but this patch
    addresses the root problem when it comes to opaque pointers.
    
    Differential Revision: https://reviews.llvm.org/D105733

--- llvm/include/llvm/IR/InstrTypes.h
+++ llvm/include/llvm/IR/InstrTypes.h
@@ -1393,10 +1393,13 @@ public:
   const Use &getCalledOperandUse() const { return Op<CalledOperandOpEndIdx>(); }
   Use &getCalledOperandUse() { return Op<CalledOperandOpEndIdx>(); }
 
-  /// Returns the function called, or null if this is an
-  /// indirect function invocation.
+  /// Returns the function called, or null if this is an indirect function
+  /// invocation or the function signature does not match the call signature.
   Function *getCalledFunction() const {
-    return dyn_cast_or_null<Function>(getCalledOperand());
+    if (auto *F = dyn_cast_or_null<Function>(getCalledOperand()))
+      if (F->getValueType() == getFunctionType())
+        return F;
+    return nullptr;
   }
 
   /// Return true if the callsite is an indirect call.
