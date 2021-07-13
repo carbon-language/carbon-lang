fb8d87d515caa223bf3574a32e56f7d07bc1c603
Jon Meow <46229924+jonmeow@users.noreply.github.com>
2021-07-13 13:53:48 -0700

    Use a lambda for tag's visit (#645)
    Also shifts declaration order a little for [style](https://google.github.io/styleguide/cppguide.html#Declaration_Order); moving line_num down (data members after methods) and tag() down (factory functions before instance members).

--- executable_semantics/interpreter/value.h
+++ executable_semantics/interpreter/value.h
@@ -163,8 +163,6 @@ struct ContinuationValue {
 };
 
 struct Value {
-  auto tag() const -> ValKind;
-
   // Constructors
 
   // Return a first-class continuation represented by the
@@ -215,6 +213,10 @@ struct Value {
   auto GetStructType() const -> const StructType&;
   auto GetChoiceType() const -> const ChoiceType&;
   auto GetContinuationValue() const -> const ContinuationValue&;
+
+  inline auto tag() const -> ValKind {
+    return std::visit([](const auto& t) { return t.Kind; }, value);
+  }
 
  private:
   std::variant<IntValue, FunctionValue, PointerValue, BoolValue, StructValue,
