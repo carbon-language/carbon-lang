64785ac12ef8b94fe7281e2cbe2db68d64d55e4c
Jinsong Ji <jji@us.ibm.com>
2021-07-14 03:29:06 +0000

    [AIX] Update testcase to use aix triple
    We have implemented the basic MCAsmParser now, we can use the triple
    directly now.

--- llvm/test/MC/PowerPC/modern-aix-as.s
+++ llvm/test/MC/PowerPC/modern-aix-as.s
@@ -2,9 +2,8 @@
 # RUN: llvm-mc -triple powerpc64le-unknown-unknown -mattr=+modern-aix-as --show-encoding %s | FileCheck -check-prefix=CHECK-LE %s
 # RUN: llvm-mc -triple powerpc64le-unknown-unknown -mattr=-modern-aix-as --show-encoding %s | FileCheck -check-prefix=CHECK-LE %s
 # RUN: not llvm-mc -triple powerpc64le-unknown-unknown -mattr=+aix --show-encoding %s 2>&1 | FileCheck -check-prefix=CHECK-OLD %s
-##TODO: Replace above with following when implement createXCOFFAsmParser
-# UN: llvm-mc -triple powerpc-aix-ibm-xcoff -mattr=+modern-aix-as --show-encoding %s 2>&1 | FileCheck -check-prefix=CHECK-BE %s
-# UN: not llvm-mc -triple powerpc-aix-ibm-xcoff --show-encoding %s 2>&1 | FileCheck -check-prefix=CHECK-OLD %s
+# RUN: llvm-mc -triple powerpc-aix-ibm-xcoff -mattr=+modern-aix-as --show-encoding %s 2>&1 | FileCheck -check-prefix=CHECK-BE %s
+# RUN: not llvm-mc -triple powerpc-aix-ibm-xcoff --show-encoding %s 2>&1 | FileCheck -check-prefix=CHECK-OLD %s
 
 # CHECK-BE: mtudscr 2                       # encoding: [0x7c,0x43,0x03,0xa6]
 # CHECK-LE: mtudscr 2                       # encoding: [0xa6,0x03,0x43,0x7c]
