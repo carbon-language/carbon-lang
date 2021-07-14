d21772fa21de6a1c5d0f4792fbd175ff3d18c842
Alexander Shaposhnikov <alexshap@fb.com>
2021-07-14 04:33:09 -0700

    [lld][MachO] Code cleanup
    Make use of ArgList::getLastArgValue. NFC.
    
    Test plan: make check-lld-macho
    
    Differential revision: https://reviews.llvm.org/D105452

--- lld/MachO/Driver.cpp
+++ lld/MachO/Driver.cpp
@@ -1142,10 +1142,8 @@ bool macho::link(ArrayRef<const char *> argsArr, bool canExitEarly,
 
   config->mapFile = args.getLastArgValue(OPT_map);
   config->outputFile = args.getLastArgValue(OPT_o, "a.out");
-  if (const Arg *arg = args.getLastArg(OPT_final_output))
-    config->finalOutput = arg->getValue();
-  else
-    config->finalOutput = config->outputFile;
+  config->finalOutput =
+      args.getLastArgValue(OPT_final_output, config->outputFile);
   config->astPaths = args.getAllArgValues(OPT_add_ast_path);
   config->headerPad = args::getHex(args, OPT_headerpad, /*Default=*/32);
   config->headerPadMaxInstallNames =
