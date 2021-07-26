7d9a2c714c4c198b250b65b955619a169ed9400e
Mehdi Amini <joker.eph@gmail.com>
2021-07-26 00:20:24 +0000

    Remove the NotUnderValgrind caching flag
    The motivation for this caching wasn't clear, remove it in an effort to
    simplify the code and make libSupport free of global dynamic constructor.
    
    Reviewed By: dexonsmith
    
    Differential Revision: https://reviews.llvm.org/D106206

--- llvm/lib/Support/Valgrind.cpp
+++ llvm/lib/Support/Valgrind.cpp
@@ -19,26 +19,11 @@
 #if HAVE_VALGRIND_VALGRIND_H
 #include <valgrind/valgrind.h>
 
-static bool InitNotUnderValgrind() {
-  return !RUNNING_ON_VALGRIND;
-}
-
-// This bool is negated from what we'd expect because code may run before it
-// gets initialized.  If that happens, it will appear to be 0 (false), and we
-// want that to cause the rest of the code in this file to run the
-// Valgrind-provided macros.
-static const bool NotUnderValgrind = InitNotUnderValgrind();
-
 bool llvm::sys::RunningOnValgrind() {
-  if (NotUnderValgrind)
-    return false;
   return RUNNING_ON_VALGRIND;
 }
 
 void llvm::sys::ValgrindDiscardTranslations(const void *Addr, size_t Len) {
-  if (NotUnderValgrind)
-    return;
-
   VALGRIND_DISCARD_TRANSLATIONS(Addr, Len);
 }
 
