93fe84d32fea8e1213bf7207b45e66667d3217f3
Jon Chesterfield <jonathanchesterfield@gmail.com>
2021-07-26 09:54:30 +0100

    [libomptarget][nfc] Squash unused variable warning
    Suppress only current warning on openmp-clang-x86_64-linux-debian
    
    Reviewed By: jdoerfert
    
    Differential Revision: https://reviews.llvm.org/D106777

--- openmp/libomptarget/src/device.cpp
+++ openmp/libomptarget/src/device.cpp
@@ -90,6 +90,7 @@ int DeviceTy::associatePtr(void *HstPtrBegin, void *TgtPtrBegin, int64_t Size) {
      DPxPTR(newEntry.HstPtrBase), DPxPTR(newEntry.HstPtrBegin),
      DPxPTR(newEntry.HstPtrEnd), DPxPTR(newEntry.TgtPtrBegin),
      newEntry.refCountToStr().c_str());
+  (void)newEntry;
 
   DataMapMtx.unlock();
 
