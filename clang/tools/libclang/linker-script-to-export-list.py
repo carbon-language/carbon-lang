c7b3a91017d26266d7556b1ac7c49b06f0109b91
Tom Stellard <tstellar@redhat.com>
2021-07-26 16:36:32 -0700

    libclang.so: Make SONAME independent from LLVM version
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D105527

---
+++ clang/tools/libclang/linker-script-to-export-list.py
@@ -0,0 +1,10 @@
+import re
+import os
+
+input_file = open(sys.argv[1])
+output_file = open(sys.argv[2])
+
+for line in input_file:
+    m = re.search('clang_[^;]+', line)
+    if m:
+        output_file.write(m.group(0))
