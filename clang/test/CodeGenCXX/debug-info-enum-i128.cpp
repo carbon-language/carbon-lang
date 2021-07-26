a9b114c5dd68685e0e68dd9144454b44509c2f5f
Reid Kleckner <rnk@google.com>
2021-07-26 15:47:54 -0700

    Disable the new enum i128 test under ASan, it uncovers an existing leak
    See llvm.org/pr51221

--- clang/test/CodeGenCXX/debug-info-enum-i128.cpp
+++ clang/test/CodeGenCXX/debug-info-enum-i128.cpp
@@ -1,6 +1,9 @@
 // RUN: %clang_cc1 %s -triple x86_64-windows-msvc -gcodeview -debug-info-kind=limited -emit-llvm -o - | FileCheck %s
 // RUN: %clang_cc1 %s -triple x86_64-linux-gnu -debug-info-kind=limited -emit-llvm -o - | FileCheck %s
 
+// FIXME: llvm.org/pr51221, the APSInt leaks
+// UNSUPPORTED: asan
+
 enum class uns : __uint128_t { unsval = __uint128_t(1) << 64 };
 uns t1() { return uns::unsval; }
 
