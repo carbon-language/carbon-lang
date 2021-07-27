0300e16eb0a8e7bd980b6b14b6a3bae38786651d
Tom Stellard <tstellar@redhat.com>
2021-07-26 18:20:14 -0700

    libclang: Fixes for the python script that generates the export list
    This script was added in 0cf37a3b0617457daaed3224373ffa07724f8482.

--- clang/tools/libclang/linker-script-to-export-list.py
+++ clang/tools/libclang/linker-script-to-export-list.py
@@ -3,9 +3,9 @@ import os
 import sys
 
 input_file = open(sys.argv[1])
-output_file = open(sys.argv[2])
+output_file = open(sys.argv[2], 'w')
 
 for line in input_file:
-    m = re.search('clang_[^;]+', line)
+    m = re.search('^\s+(clang_[^;]+)', line)
     if m:
-        output_file.write(m.group(0))
+        output_file.write(m.group(1) + "\n")
