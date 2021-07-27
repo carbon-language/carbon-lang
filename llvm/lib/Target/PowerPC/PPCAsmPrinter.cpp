778932c673c28ce5fb75e551f1aa6bb5a542c563
Nemanja Ivanovic <nemanja.i.ibm@gmail.com>
2021-07-27 15:47:44 -0500

    [PowerPC] Turn deprecated altivec prefetch instrs to nops on AIX
    The dst/dstt/dstst/dststt instructions are nop's on all PowerPC
    cores that AIX supports. The AIX assembler also does not accept
    these mnemonics. Turn them into nop's on AIX (similar to dstall).

--- llvm/lib/Target/PowerPC/PPCAsmPrinter.cpp
+++ llvm/lib/Target/PowerPC/PPCAsmPrinter.cpp
@@ -2574,6 +2574,18 @@ void PPCAIXAsmPrinter::emitInstruction(const MachineInstr *MI) {
     if (MI->getOperand(0).isSymbol())
       report_fatal_error("Tail call for extern symbol not yet supported.");
     break;
+  case PPC::DST:
+  case PPC::DST64:
+  case PPC::DSTT:
+  case PPC::DSTT64:
+  case PPC::DSTST:
+  case PPC::DSTST64:
+  case PPC::DSTSTT:
+  case PPC::DSTSTT64:
+    EmitToStreamer(
+        *OutStreamer,
+        MCInstBuilder(PPC::ORI).addReg(PPC::R0).addReg(PPC::R0).addImm(0));
+    return;
   }
   return PPCAsmPrinter::emitInstruction(MI);
 }
