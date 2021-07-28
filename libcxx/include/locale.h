4e65688571221565cf1a5c1e4bb58ee2ab7c9c83
Mark de Wever <koraq@xs4all.nl>
2021-07-28 08:11:52 +0200

    [libc++][nfc] Improve error diagnostics.
    The error message for disabled filesystem and locale support is now done
    in the same fashion as ranges and format in D106763.
    
    Reviewed By: #libc, ldionne
    
    Differential Revision: https://reviews.llvm.org/D106935

--- libcxx/include/locale.h
+++ libcxx/include/locale.h
@@ -36,7 +36,8 @@ Functions:
 #include <__config>
 
 #if defined(_LIBCPP_HAS_NO_LOCALIZATION)
-#   error "Localization is not supported by this configuration of libc++"
+#error                                                                                                                 \
+    "The Localization library is not supported since libc++ has been configured with LIBCXX_ENABLE_LOCALIZATION disabled"
 #endif
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
