648844fd69fa304a04aab2e39dcb114c89d078bf
Adrian Prantl <aprantl@apple.com>
2021-07-29 16:23:00 -0700

    Make testcase more robust against codegen changes

--- lldb/test/API/commands/process/attach/main.cpp
+++ lldb/test/API/commands/process/attach/main.cpp
@@ -12,9 +12,8 @@ int main(int argc, char const *argv[]) {
     // Waiting to be attached by the debugger.
     temp = 0;
 
-    while (temp < 30) // Waiting to be attached...
-    {
-        std::this_thread::sleep_for(std::chrono::seconds(2));
+    while (temp < 30) {
+        std::this_thread::sleep_for(std::chrono::seconds(2)); // Waiting to be attached...
         temp++;
     }
 
