172a55e7a40d27c7882be2e86d515696d8e12427
Fangrui Song <i@maskray.me>
2021-07-29 09:57:10 -0700

    [lldb] Fix FunctionDecl::Create after D102343

--- lldb/source/Plugins/ExpressionParser/Clang/NameSearchContext.cpp
+++ lldb/source/Plugins/ExpressionParser/Clang/NameSearchContext.cpp
@@ -77,7 +77,7 @@ clang::NamedDecl *NameSearchContext::AddFunDecl(const CompilerType &type,
 
   clang::FunctionDecl *func_decl = FunctionDecl::Create(
       ast, context, SourceLocation(), SourceLocation(), decl_name, qual_type,
-      nullptr, SC_Extern, isInlineSpecified, hasWrittenPrototype,
+      nullptr, SC_Extern, /*UsesFPIntrin=*/false, isInlineSpecified, hasWrittenPrototype,
       isConstexprSpecified ? ConstexprSpecKind::Constexpr
                            : ConstexprSpecKind::Unspecified);
 
