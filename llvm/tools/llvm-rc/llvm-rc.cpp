46020f6f0c8aa134002208b2ecf0593b04c46d08
Martin Storsjö <martin@martin.st>
2021-08-02 14:10:22 +0300

    [llvm-rc] Allow specifying language with a leading 0x prefix
    This option is always interpreted strictly as a hexadecimal string,
    even if it has no prefix that indicates the number format, hence
    the existing call to StringRef::getAsInteger(16, ...).
    
    StringRef::getAsInteger(0, ...) consumes a leading "0x" prefix is
    present, but when the radix is specified, the radix shouldn't
    be included.
    
    Both MS rc.exe and GNU windres accept the language with that
    prefix.
    
    Also allow specifying the codepage to llvm-windres with a different
    radix, as GNU windres allows that (but MS rc.exe doesn't).
    
    This fixes https://llvm.org/PR51295.
    
    Differential Revision: https://reviews.llvm.org/D107263

--- llvm/tools/llvm-rc/llvm-rc.cpp
+++ llvm/tools/llvm-rc/llvm-rc.cpp
@@ -476,13 +476,14 @@ RcOptions parseWindresOptions(ArrayRef<const char *> ArgsArr,
   Opts.Params.CodePage = CpWin1252; // Different default
   if (InputArgs.hasArg(WINDRES_codepage)) {
     if (InputArgs.getLastArgValue(WINDRES_codepage)
-            .getAsInteger(10, Opts.Params.CodePage))
+            .getAsInteger(0, Opts.Params.CodePage))
       fatalError("Invalid code page: " +
                  InputArgs.getLastArgValue(WINDRES_codepage));
   }
   if (InputArgs.hasArg(WINDRES_language)) {
-    if (InputArgs.getLastArgValue(WINDRES_language)
-            .getAsInteger(16, Opts.LangId))
+    StringRef Val = InputArgs.getLastArgValue(WINDRES_language);
+    Val.consume_front_insensitive("0x");
+    if (Val.getAsInteger(16, Opts.LangId))
       fatalError("Invalid language id: " +
                  InputArgs.getLastArgValue(WINDRES_language));
   }
@@ -565,7 +566,9 @@ RcOptions parseRcOptions(ArrayRef<const char *> ArgsArr,
   }
   Opts.AppendNull = InputArgs.hasArg(OPT_add_null);
   if (InputArgs.hasArg(OPT_lang_id)) {
-    if (InputArgs.getLastArgValue(OPT_lang_id).getAsInteger(16, Opts.LangId))
+    StringRef Val = InputArgs.getLastArgValue(OPT_lang_id);
+    Val.consume_front_insensitive("0x");
+    if (Val.getAsInteger(16, Opts.LangId))
       fatalError("Invalid language id: " +
                  InputArgs.getLastArgValue(OPT_lang_id));
   }
