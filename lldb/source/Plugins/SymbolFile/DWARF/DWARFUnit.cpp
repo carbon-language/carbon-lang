0092dbcd80f208f7becec002e70bad5b35a081cd
Kim-Anh Tran <kimanh@chromium.org>
2021-08-04 09:16:10 +0200

    [lldb] Fix lookup of .debug_loclists with split-dwarf
    This patch fixes the lookup of locations in
    .debug_loclists, if they are split in a .dwp file.
    
    Mainly, we need to consider the cu index offsets.
    
    Reviewed By: jankratochvil
    
    Differential Revision: https://reviews.llvm.org/D107161

--- lldb/source/Plugins/SymbolFile/DWARF/DWARFUnit.cpp
+++ lldb/source/Plugins/SymbolFile/DWARF/DWARFUnit.cpp
@@ -453,6 +453,18 @@ ParseListTableHeader(const llvm::DWARFDataExtractor &data, uint64_t offset,
 }
 
 void DWARFUnit::SetLoclistsBase(dw_addr_t loclists_base) {
+  uint64_t offset = 0;
+  if (const llvm::DWARFUnitIndex::Entry *entry = m_header.GetIndexEntry()) {
+    const auto *contribution = entry->getContribution(llvm::DW_SECT_LOCLISTS);
+    if (!contribution) {
+      GetSymbolFileDWARF().GetObjectFile()->GetModule()->ReportError(
+          "Failed to find location list contribution for CU with DWO Id "
+          "0x%" PRIx64,
+          this->GetDWOId());
+      return;
+    }
+    offset += contribution->Offset;
+  }
   m_loclists_base = loclists_base;
 
   uint64_t header_size = llvm::DWARFListTableHeader::getHeaderSize(DWARF32);
@@ -460,13 +472,14 @@ void DWARFUnit::SetLoclistsBase(dw_addr_t loclists_base) {
     return;
 
   m_loclist_table_header.emplace(".debug_loclists", "locations");
-  uint64_t offset = loclists_base - header_size;
+  offset += loclists_base - header_size;
   if (llvm::Error E = m_loclist_table_header->extract(
           m_dwarf.GetDWARFContext().getOrLoadLocListsData().GetAsLLVM(),
           &offset)) {
     GetSymbolFileDWARF().GetObjectFile()->GetModule()->ReportError(
-        "Failed to extract location list table at offset 0x%" PRIx64 ": %s",
-        loclists_base, toString(std::move(E)).c_str());
+        "Failed to extract location list table at offset 0x%" PRIx64
+        " (location list base: 0x%" PRIx64 "): %s",
+        offset, loclists_base, toString(std::move(E)).c_str());
   }
 }
 
@@ -486,7 +499,8 @@ DWARFDataExtractor DWARFUnit::GetLocationData() const {
   const DWARFDataExtractor &data =
       GetVersion() >= 5 ? Ctx.getOrLoadLocListsData() : Ctx.getOrLoadLocData();
   if (const llvm::DWARFUnitIndex::Entry *entry = m_header.GetIndexEntry()) {
-    if (const auto *contribution = entry->getContribution(llvm::DW_SECT_EXT_LOC))
+    if (const auto *contribution = entry->getContribution(
+            GetVersion() >= 5 ? llvm::DW_SECT_LOCLISTS : llvm::DW_SECT_EXT_LOC))
       return DWARFDataExtractor(data, contribution->Offset,
                                 contribution->Length);
     return DWARFDataExtractor();
