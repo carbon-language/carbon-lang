3cebc874dc368a8aaa4ec3c12ce4500322258c8e
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-08-06 22:45:59 +0000

    Starting

--- executable_semantics/ast/statement.h
+++ executable_semantics/ast/statement.h
@@ -31,7 +31,17 @@ enum class StatementKind {
   Await,         // Pause execution of the continuation.
 };
 
-struct Statement;
+struct Statement {
+  void Print(llvm::raw_ostream& out) const { PrintDepth(-1, out); }
+  void PrintDepth(int depth, llvm::raw_ostream& out) const;
+  LLVM_DUMP_METHOD void Dump() const { Print(llvm::errs()); }
+
+  inline auto tag() const -> StatementKind {
+    return std::visit([](const auto& t) { return t.Kind; }, value);
+  }
+
+  int line_num;
+};
 
 struct ExpressionStatement {
   static constexpr StatementKind Kind = StatementKind::ExpressionStatement;
@@ -94,94 +104,30 @@ struct Match {
   std::list<std::pair<const Pattern*, const Statement*>>* clauses;
 };
 
+// A continuation statement.
+//
+//     __continuation <continuation_variable> {
+//       <body>
+//     }
 struct Continuation {
   static constexpr StatementKind Kind = StatementKind::Continuation;
   std::string continuation_variable;
   const Statement* body;
 };
 
+// A run statement.
+//
+//     __run <argument>;
 struct Run {
   static constexpr StatementKind Kind = StatementKind::Run;
   const Expression* argument;
 };
 
+// An await statement.
+//
+//    __await;
 struct Await {
   static constexpr StatementKind Kind = StatementKind::Await;
-};
-
-struct Statement {
-  // Constructors
-  static auto MakeExpressionStatement(int line_num, const Expression* exp)
-      -> const Statement*;
-  static auto MakeAssign(int line_num, const Expression* lhs,
-                         const Expression* rhs) -> const Statement*;
-  static auto MakeVariableDefinition(int line_num, const Pattern* pat,
-                                     const Expression* init)
-      -> const Statement*;
-  static auto MakeIf(int line_num, const Expression* cond,
-                     const Statement* then_stmt, const Statement* else_stmt)
-      -> const Statement*;
-  static auto MakeReturn(int line_num, const Expression* exp,
-                         bool is_omitted_exp) -> const Statement*;
-  static auto MakeSequence(int line_num, const Statement* s1,
-                           const Statement* s2) -> const Statement*;
-  static auto MakeBlock(int line_num, const Statement* s) -> const Statement*;
-  static auto MakeWhile(int line_num, const Expression* cond,
-                        const Statement* body) -> const Statement*;
-  static auto MakeBreak(int line_num) -> const Statement*;
-  static auto MakeContinue(int line_num) -> const Statement*;
-  static auto MakeMatch(
-      int line_num, const Expression* exp,
-      std::list<std::pair<const Pattern*, const Statement*>>* clauses)
-      -> const Statement*;
-  // Returns an AST node for a continuation statement give its line number and
-  // contituent parts.
-  //
-  //     __continuation <continuation_variable> {
-  //       <body>
-  //     }
-  static auto MakeContinuation(int line_num, std::string continuation_variable,
-                               const Statement* body) -> const Statement*;
-  // Returns an AST node for a run statement give its line number and argument.
-  //
-  //     __run <argument>;
-  static auto MakeRun(int line_num, const Expression* argument)
-      -> const Statement*;
-  // Returns an AST node for an await statement give its line number.
-  //
-  //     __await;
-  static auto MakeAwait(int line_num) -> const Statement*;
-
-  auto GetExpressionStatement() const -> const ExpressionStatement&;
-  auto GetAssign() const -> const Assign&;
-  auto GetVariableDefinition() const -> const VariableDefinition&;
-  auto GetIf() const -> const If&;
-  auto GetReturn() const -> const Return&;
-  auto GetSequence() const -> const Sequence&;
-  auto GetBlock() const -> const Block&;
-  auto GetWhile() const -> const While&;
-  auto GetBreak() const -> const Break&;
-  auto GetContinue() const -> const Continue&;
-  auto GetMatch() const -> const Match&;
-  auto GetContinuation() const -> const Continuation&;
-  auto GetRun() const -> const Run&;
-  auto GetAwait() const -> const Await&;
-
-  void Print(llvm::raw_ostream& out) const { PrintDepth(-1, out); }
-  void PrintDepth(int depth, llvm::raw_ostream& out) const;
-  LLVM_DUMP_METHOD void Dump() const { Print(llvm::errs()); }
-
-  inline auto tag() const -> StatementKind {
-    return std::visit([](const auto& t) { return t.Kind; }, value);
-  }
-
-  int line_num;
-
- private:
-  std::variant<ExpressionStatement, Assign, VariableDefinition, If, Return,
-               Sequence, Block, While, Break, Continue, Match, Continuation,
-               Run, Await>
-      value;
 };
 
 }  // namespace Carbon
