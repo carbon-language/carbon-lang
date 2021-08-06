a1b21ed3fb4a9f4b85d8fa29f95c31b737e0840f
Arthur Eubanks <aeubanks@google.com>
2021-08-05 21:18:53 -0700

    [GCov] Emit memset instead of stores in __llvm_gcov_reset
    For a very large module, __llvm_gcov_reset can become very large.
    __llvm_gcov_reset previously emitted stores to a bunch of globals in one
    huge basic block. MemCpyOpt would turn many of these stores into
    memsets, and updating MemorySSA would be extremely slow.
    
    Verified that this makes the compile time of certain files go down
    drastically (20min -> 5min).
    
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D107538

--- llvm/lib/Transforms/Instrumentation/GCOVProfiling.cpp
+++ llvm/lib/Transforms/Instrumentation/GCOVProfiling.cpp
@@ -1373,12 +1373,16 @@ Function *GCOVProfiler::insertReset(
 
   BasicBlock *Entry = BasicBlock::Create(*Ctx, "entry", ResetF);
   IRBuilder<> Builder(Entry);
+  LLVMContext &C = Entry->getContext();
 
   // Zero out the counters.
   for (const auto &I : CountersBySP) {
     GlobalVariable *GV = I.first;
-    Constant *Null = Constant::getNullValue(GV->getValueType());
-    Builder.CreateStore(Null, GV);
+    auto *GVTy = cast<ArrayType>(GV->getValueType());
+    Builder.CreateMemSet(GV, Constant::getNullValue(Type::getInt8Ty(C)),
+                         GVTy->getNumElements() *
+                             GVTy->getElementType()->getScalarSizeInBits() / 8,
+                         GV->getAlign());
   }
 
   Type *RetTy = ResetF->getReturnType();
