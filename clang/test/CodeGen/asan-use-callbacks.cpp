1397e19129ef7aa6c2ba6f6018ff172a5022a1eb
Thomas Preud'homme <thomasp@graphcore.ai>
2021-08-06 11:09:40 +0100

    Set supported target for asan-use-callbacks test
    Explicitely set x86_64-linux-gnu as a target for asan-use-callbacks
    clang test since some target do not support -fsanitize=address (e.g.
    i386-pc-openbsd). Also remove redundant -fsanitize=address and move
    -emit-llvm right after -S.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D107633

--- clang/test/CodeGen/asan-use-callbacks.cpp
+++ clang/test/CodeGen/asan-use-callbacks.cpp
@@ -1,7 +1,8 @@
-// RUN: %clang -S -fsanitize=address -emit-llvm -o - -fsanitize=address %s \
+// RUN: %clang -target x86_64-linux-gnu -S -emit-llvm -fsanitize=address \
+// RUN:     -o - %s \
 // RUN:     | FileCheck %s --check-prefixes=CHECK-NO-OUTLINE
-// RUN: %clang -S -fsanitize=address -emit-llvm -o - -fsanitize=address %s \
-// RUN:     -fsanitize-address-outline-instrumentation \
+// RUN: %clang -target x86_64-linux-gnu -S -emit-llvm -o - \
+// RUN:     -fsanitize=address %s -fsanitize-address-outline-instrumentation \
 // RUN:     | FileCheck %s --check-prefixes=CHECK-OUTLINE
 
 // CHECK-NO-OUTLINE-NOT: call{{.*}}@__asan_load4
