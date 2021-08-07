ab4b4684a26c31b1eb507a7eb6d2f4ac30764c3d
Dimitry Andric <dimitry@andric.com>
2021-08-07 21:55:19 +0200

    [mlir] Avoid including <alloca.h> on FreeBSD and NetBSD
    Instead, include `<cstdlib>` which is the canonical header containing
    the declaration of `alloca()`.
    
    Reviewed By: bondhugula
    
    Differential Revision: https://reviews.llvm.org/D107699

--- mlir/lib/ExecutionEngine/CRunnerUtils.cpp
+++ mlir/lib/ExecutionEngine/CRunnerUtils.cpp
@@ -15,7 +15,11 @@
 #include "mlir/ExecutionEngine/CRunnerUtils.h"
 
 #ifndef _WIN32
+#if defined(__FreeBSD__) || defined(__NetBSD__)
+#include <cstdlib>
+#else
 #include <alloca.h>
+#endif
 #include <sys/time.h>
 #else
 #include "malloc.h"
