b978df4af4c8a668550fa035b70795312bf41f44
Fangrui Song <i@maskray.me>
2021-08-09 20:25:33 -0700

    [Driver][test] Improve avr-toolchain.c
    Reviewed By: benshi001, mhjacobson
    
    Differential Revision: https://reviews.llvm.org/D107797

--- clang/test/Driver/avr-toolchain.c
+++ clang/test/Driver/avr-toolchain.c
@@ -1,13 +1,20 @@
+// UNSUPPORTED: system-windows
 // A basic clang -cc1 command-line.
 
-// RUN: %clang %s -### -no-canonical-prefixes -target avr 2>&1 | FileCheck -check-prefix=CC1 %s
+// RUN: %clang %s -### -target avr --sysroot %S/Inputs/basic_avr_tree -resource-dir=%S/Inputs/resource_dir 2>&1 | FileCheck --check-prefix=CHECK1 %s
+// CHECK1: clang{{.*}} "-cc1" "-triple" "avr"
+// CHECK1-SAME: "-resource-dir" "[[RESOURCE:[^"]+]]"
+// CHECK1-SAME: "-isysroot" "[[SYSROOT:[^"]+/basic_avr_tree]]"
+// CHECK1-SAME: "-internal-isystem"
+// CHECK1-SAME: {{^}} "[[SYSROOT]]/usr/lib/avr/include"
+// CHECK1-NOT:  "-L
+// CHECK1:      avr-ld"
+// CHECK1-SAME: "-o" "a.out"
+// CHECK1-SAME: {{^}} "--gc-sections"
+
+// RUN: %clang %s -### -target avr 2>&1 | FileCheck -check-prefix=CC1 %s
 // CC1: clang{{.*}} "-cc1" "-triple" "avr" {{.*}} "-fno-use-init-array"
 
-// RUN: %clang %s -### -no-canonical-prefixes -target avr --sysroot %S/Inputs/basic_avr_tree 2>&1 | FileCheck -check-prefix CC1A %s
-// CC1A: clang{{.*}} "-cc1" "-triple" "avr" {{.*}} "-internal-isystem" {{".*avr/include"}}
-
-// RUN: %clang %s -### -target avr --sysroot %S/Inputs/basic_avr_tree 2>&1 -nostdinc | FileCheck -check-prefix CC1B %s
-// CC1B-NOT: "-internal-isystem" {{".*avr/include"}}
-
-// RUN: %clang %s -### -target avr --sysroot %S/Inputs/basic_avr_tree 2>&1 -nostdlibinc | FileCheck -check-prefix CC1C %s
-// CC1C-NOT: "-internal-isystem" {{".*avr/include"}}
+// RUN: %clang %s -### -target avr --sysroot %S/Inputs/basic_avr_tree 2>&1 -nostdinc | FileCheck --check-prefix=NOSTDINC %s
+// RUN: %clang %s -### -target avr --sysroot %S/Inputs/basic_avr_tree 2>&1 -nostdlibinc | FileCheck --check-prefix=NOSTDINC %s
+// NOSTDINC-NOT: "-internal-isystem" {{".*avr/include"}}
