bf6000dc98df82d9b493e0b5d247538f51d6b9ac
Vitaly Buka <vitalybuka@google.com>
2021-08-12 15:37:07 -0700

    [sanitizer] Fix the test on Solaris
    On Solaris sem_open on the same name returns the same pointer, and
    then sem_close fails the call.

--- compiler-rt/test/sanitizer_common/TestCases/Posix/sem_open.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/sem_open.cpp
@@ -11,16 +11,10 @@
 
 int main() {
   char name[1024];
-  sprintf(name, "/sem_open_test_%d", getpid());
+  sprintf(name, "/sem_open_test_%zu", (size_t)getpid());
 
-  sem_t *s1 = sem_open(name, O_CREAT, 0644, 123);
-  assert(s1 != SEM_FAILED);
-
-  sem_t *s2 = sem_open(name, O_CREAT, 0644, 123);
-  assert(s2 != SEM_FAILED);
-
-  assert(sem_close(s1) == 0);
-  assert(sem_close(s2) == 0);
-
+  sem_t *s = sem_open(name, O_CREAT, 0644, 123);
+  assert(s != SEM_FAILED);
+  assert(sem_close(s) == 0);
   assert(sem_unlink(name) == 0);
 }
