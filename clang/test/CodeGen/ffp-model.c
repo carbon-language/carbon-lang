17d9560294eee1eae5e2d3ac1ab84f514318409e
Zahira Ammarguellat <zahira.ammarguellat@intel.com>
2021-08-12 16:13:16 -0400

    Making the code compliant to the documentation about Floating Point support default values for C/C++. FPP-MODEL=PRECISE enables FFP-CONTRACT FMA is enabled.
    Fix for https://bugs.llvm.org/show_bug.cgi?id=50222

---
+++ clang/test/CodeGen/ffp-model.c
@@ -0,0 +1,51 @@
+// RUN: %clang -S -emit-llvm -ffp-model=fast -emit-llvm %s -o - \
+// RUN: | FileCheck %s \
+// RUN: --check-prefixes=CHECK,CHECK-FAST
+
+// RUN: %clang -S -emit-llvm -ffp-model=precise -emit-llvm %s -o - \
+// RUN: | FileCheck %s \
+// RUN: --check-prefixes=CHECK,CHECK-PRECISE
+
+// RUN: %clang -S -emit-llvm -ffp-model=strict -emit-llvm %s -o - \
+// RUN: | FileCheck %s \
+// RUN: --check-prefixes=CHECK,CHECK-STRICT
+
+// RUN: %clang -S -emit-llvm -ffp-model=strict -ffast-math -emit-llvm \
+// RUN:  %s -o - | FileCheck %s \
+// RUN: --check-prefixes CHECK,CHECK-STRICT-FAST
+
+// RUN: %clang -S -emit-llvm -ffp-model=precise -ffast-math -emit-llvm \
+// RUN: %s -o - | FileCheck %s \
+// RUN: --check-prefixes CHECK,CHECK-FAST1
+
+float mymuladd(float x, float y, float z) {
+  // CHECK: define {{.*}} float @mymuladd(float %x, float %y, float %z)
+  return x * y + z;
+  // CHECK-FAST: fmul fast float
+
+  // CHECK-FAST: load float, float*
+  // CHECK-FAST: fadd fast float
+
+  // CHECK-PRECISE: load float, float*
+  // CHECK-PRECISE: load float, float*
+  // CHECK-PRECISE: load float, float*
+  // CHECK-PRECISE: call float @llvm.fmuladd.f32(float {{.*}}, float {{.*}}, float {{.*}})
+
+  // CHECK-STRICT: load float, float*
+  // CHECK-STRICT: load float, float*
+  // CHECK-STRICT: call float @llvm.experimental.constrained.fmul.f32(float {{.*}}, float {{.*}}, {{.*}})
+  // CHECK-STRICT: load float, float*
+  // CHECK-STRICT: call float @llvm.experimental.constrained.fadd.f32(float {{.*}}, float {{.*}}, {{.*}})
+
+  // CHECK-STRICT-FAST: load float, float*
+  // CHECK-STRICT-FAST: load float, float*
+  // CHECK-STRICT-FAST: call fast float @llvm.experimental.constrained.fmul.f32(float {{.*}}, float {{.*}}, {{.*}})
+  // CHECK-STRICT-FAST: load float, float*
+  // CHECK-STRICT-FAST: call fast float @llvm.experimental.constrained.fadd.f32(float {{.*}}, float {{.*}}, {{.*}}
+
+  // CHECK-FAST1: load float, float*
+  // CHECK-FAST1: load float, float*
+  // CHECK-FAST1: fmul fast float {{.*}}, {{.*}}
+  // CHECK-FAST1: load float, float* {{.*}}
+  // CHECK-FAST1: fadd fast float {{.*}}, {{.*}}
+}
