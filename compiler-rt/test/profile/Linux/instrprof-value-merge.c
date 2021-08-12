d54c62cc326e992c3dee020dc267f15284c7ea19
Fangrui Song <i@maskray.me>
2021-08-11 22:41:52 -0700

    [profile][test] Add -no-pie to make value profile merge work on Linux with default PIE
    Alpine enables PIE by default.

--- compiler-rt/test/profile/Linux/instrprof-value-merge.c
+++ compiler-rt/test/profile/Linux/instrprof-value-merge.c
@@ -1,4 +1,6 @@
-// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c
+/// Without PIE function addresses are the same across runs and their value
+/// sites can be merged, matching the test.
+// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c -no-pie
 // RUN: rm -rf %t.profdir
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
@@ -9,7 +11,7 @@
 
 /// -z start-stop-gc requires binutils 2.37. Don't test the option for now.
 /// TODO: Add -Wl,--gc-sections.
-// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c -fuse-ld=bfd -ffunction-sections -fdata-sections
+// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c -no-pie -fuse-ld=bfd -ffunction-sections -fdata-sections
 // RUN: rm -rf %t.profdir
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
@@ -18,7 +20,7 @@
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t 1
 // RUN: llvm-profdata show -counts -function=main -ic-targets -memop-sizes %t.profdir/default_*.profraw | FileCheck %S/Inputs/instrprof-value-merge.c
 
-// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c -fuse-ld=gold -ffunction-sections -fdata-sections
+// RUN: %clang_pgogen -o %t -O3 %S/Inputs/instrprof-value-merge.c -no-pie -fuse-ld=gold -ffunction-sections -fdata-sections
 // RUN: rm -rf %t.profdir
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
 // RUN: env LLVM_PROFILE_FILE=%t.profdir/default_%m.profraw %run %t
