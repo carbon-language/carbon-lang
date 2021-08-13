ef6e1945af77ac2e8422005a00b2014cacc7da0d
Dmitry Vyukov <dvyukov@google.com>
2021-08-13 07:36:56 +0200

    sanitizer_common: declare vars more locally in VSNPrintf
    No point in declaring variables separately before use.
    
    Depends on D107979.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D108015

--- compiler-rt/lib/sanitizer_common/sanitizer_printf.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_printf.cpp
@@ -162,17 +162,15 @@ int VSNPrintf(char *buff, int buff_length,
     cur += have_z;
     bool have_ll = !have_z && (cur[0] == 'l' && cur[1] == 'l');
     cur += have_ll * 2;
-    s64 dval;
-    u64 uval;
     const bool have_length = have_z || have_ll;
     const bool have_flags = have_width || have_length;
     // At the moment only %s supports precision and left-justification.
     CHECK(!((precision >= 0 || left_justified) && *cur != 's'));
     switch (*cur) {
       case 'd': {
-        dval = have_ll  ? va_arg(args, s64)
-               : have_z ? va_arg(args, sptr)
-                        : va_arg(args, int);
+        s64 dval = have_ll  ? va_arg(args, s64)
+                   : have_z ? va_arg(args, sptr)
+                            : va_arg(args, int);
         result += AppendSignedDecimal(&buff, buff_end, dval, width,
                                       pad_with_zero);
         break;
@@ -180,9 +178,9 @@ int VSNPrintf(char *buff, int buff_length,
       case 'u':
       case 'x':
       case 'X': {
-        uval = have_ll  ? va_arg(args, u64)
-               : have_z ? va_arg(args, uptr)
-                        : va_arg(args, unsigned);
+        u64 uval = have_ll  ? va_arg(args, u64)
+                   : have_z ? va_arg(args, uptr)
+                            : va_arg(args, unsigned);
         bool uppercase = (*cur == 'X');
         result += AppendUnsigned(&buff, buff_end, uval, (*cur == 'u') ? 10 : 16,
                                  width, pad_with_zero, uppercase);
