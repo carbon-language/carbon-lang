c6ebe0db67dfce2be8aa8a15c4264d6ad5950ae0
Jon Meow <46229924+jonmeow@users.noreply.github.com>
2021-08-17 13:37:29 -0700

    Add `Ptr` and `RawNew` for migration (#751)
    Co-authored-by: Geoff Romer <gromer@google.com>

---
+++ executable_semantics/common/ptr.h
@@ -0,0 +1,43 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef EXECUTABLE_SEMANTICS_COMMON_PTR_H_
+#define EXECUTABLE_SEMANTICS_COMMON_PTR_H_
+
+#include <memory>
+#include <vector>
+
+#include "common/check.h"
+
+namespace Carbon {
+
+// A non-nullable pointer. Written as `Ptr<T>` instead of `T*`.
+template <typename T>
+class Ptr {
+ public:
+  explicit Ptr(T* ptr) : ptr(ptr) { CHECK(ptr != nullptr); }
+
+  template <typename OtherT,
+            std::enable_if_t<std::is_convertible_v<OtherT*, T*>>* = nullptr>
+  Ptr(Ptr<OtherT> other) {
+    return Ptr<OtherT>(other.ptr);
+  }
+
+  Ptr(std::nullptr_t) = delete;
+
+  Ptr(const Ptr& other) = default;
+  Ptr& operator=(const Ptr& rhs) = default;
+
+  auto operator*() const -> T& { return *ptr; }
+  auto operator->() const -> T* { return ptr; }
+
+  T* Get() const { return ptr; }
+
+ private:
+  T* ptr;
+};
+
+}  // namespace Carbon
+
+#endif  // EXECUTABLE_SEMANTICS_COMMON_PTR_H_
--- executable_semantics/interpreter/value.cpp
+++ executable_semantics/interpreter/value.cpp
@@ -78,7 +78,8 @@ auto GetMember(const Value* v, const std::string& f, int line_num)
         FATAL_RUNTIME_ERROR(line_num)
             << "alternative " << f << " not in " << *v;
       }
-      return global_arena->New<AlternativeConstructorValue>(f, choice.Name());
+      return global_arena->RawNew<AlternativeConstructorValue>(f,
+                                                               choice.Name());
     }
     default:
       FATAL() << "field access not allowed for value " << *v;
@@ -122,7 +123,7 @@ auto SetFieldImpl(const Value* value,
       }
       it->value = SetFieldImpl(it->value, path_begin + 1, path_end, field_value,
                                line_num);
-      return global_arena->New<TupleValue>(elements);
+      return global_arena->RawNew<TupleValue>(elements);
     }
     default:
       FATAL() << "field access not allowed for value " << *value;
@@ -255,56 +256,56 @@ auto CopyVal(const Value* val, int line_num) -> const Value* {
         elements.push_back(
             {.name = element.name, .value = CopyVal(element.value, line_num)});
       }
-      return global_arena->New<TupleValue>(std::move(elements));
+      return global_arena->RawNew<TupleValue>(std::move(elements));
     }
     case Value::Kind::AlternativeValue: {
       const auto& alt = cast<AlternativeValue>(*val);
       const Value* arg = CopyVal(alt.Argument(), line_num);
-      return global_arena->New<AlternativeValue>(alt.AltName(),
-                                                 alt.ChoiceName(), arg);
+      return global_arena->RawNew<AlternativeValue>(alt.AltName(),
+                                                    alt.ChoiceName(), arg);
     }
     case Value::Kind::StructValue: {
       const auto& s = cast<StructValue>(*val);
       const Value* inits = CopyVal(s.Inits(), line_num);
-      return global_arena->New<StructValue>(s.Type(), inits);
+      return global_arena->RawNew<StructValue>(s.Type(), inits);
     }
     case Value::Kind::IntValue:
-      return global_arena->New<IntValue>(cast<IntValue>(*val).Val());
+      return global_arena->RawNew<IntValue>(cast<IntValue>(*val).Val());
     case Value::Kind::BoolValue:
-      return global_arena->New<BoolValue>(cast<BoolValue>(*val).Val());
+      return global_arena->RawNew<BoolValue>(cast<BoolValue>(*val).Val());
     case Value::Kind::FunctionValue: {
       const auto& fn_value = cast<FunctionValue>(*val);
-      return global_arena->New<FunctionValue>(fn_value.Name(), fn_value.Param(),
-                                              fn_value.Body());
+      return global_arena->RawNew<FunctionValue>(
+          fn_value.Name(), fn_value.Param(), fn_value.Body());
     }
     case Value::Kind::PointerValue:
-      return global_arena->New<PointerValue>(cast<PointerValue>(*val).Val());
+      return global_arena->RawNew<PointerValue>(cast<PointerValue>(*val).Val());
     case Value::Kind::ContinuationValue:
       // Copying a continuation is "shallow".
       return val;
     case Value::Kind::FunctionType: {
       const auto& fn_type = cast<FunctionType>(*val);
-      return global_arena->New<FunctionType>(fn_type.Deduced(),
-                                             CopyVal(fn_type.Param(), line_num),
-                                             CopyVal(fn_type.Ret(), line_num));
+      return global_arena->RawNew<FunctionType>(
+          fn_type.Deduced(), CopyVal(fn_type.Param(), line_num),
+          CopyVal(fn_type.Ret(), line_num));
     }
     case Value::Kind::PointerType:
-      return global_arena->New<PointerType>(
+      return global_arena->RawNew<PointerType>(
           CopyVal(cast<PointerType>(*val).Type(), line_num));
     case Value::Kind::IntType:
-      return global_arena->New<IntType>();
+      return global_arena->RawNew<IntType>();
     case Value::Kind::BoolType:
-      return global_arena->New<BoolType>();
+      return global_arena->RawNew<BoolType>();
     case Value::Kind::TypeType:
-      return global_arena->New<TypeType>();
+      return global_arena->RawNew<TypeType>();
     case Value::Kind::AutoType:
-      return global_arena->New<AutoType>();
+      return global_arena->RawNew<AutoType>();
     case Value::Kind::ContinuationType:
-      return global_arena->New<ContinuationType>();
+      return global_arena->RawNew<ContinuationType>();
     case Value::Kind::StringType:
-      return global_arena->New<StringType>();
+      return global_arena->RawNew<StringType>();
     case Value::Kind::StringValue:
-      return global_arena->New<StringValue>(cast<StringValue>(*val).Val());
+      return global_arena->RawNew<StringValue>(cast<StringValue>(*val).Val());
     case Value::Kind::VariableType:
     case Value::Kind::StructType:
     case Value::Kind::ChoiceType:
