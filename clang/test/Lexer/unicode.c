9f27364377a247cc779c11b1e1f220b20755982f
Aaron Ballman <aaron@aaronballman.com>
2021-08-18 09:30:36 -0400

    Use a more general test here.
    The interesting bit about that triple isn't the architecture, it's the
    fact that ps4 implies C99 as the standard rather than a newer C mode.
    Specify the language standard rather than the triple so the test is a
    bit more general.

--- clang/test/Lexer/unicode.c
+++ clang/test/Lexer/unicode.c
@@ -1,6 +1,6 @@
 // RUN: %clang_cc1 -fsyntax-only -verify -x c -std=c11 %s
 // RUN: %clang_cc1 -fsyntax-only -verify -x c++ -std=c++11 %s
-// RUN: %clang_cc1 -triple x86_64-scei-ps4 -E -DPP_ONLY=1 %s | FileCheck %s --strict-whitespace
+// RUN: %clang_cc1 -std=c99 -E -DPP_ONLY=1 %s | FileCheck %s --strict-whitespace
 // RUN: %clang_cc1 -E -DPP_ONLY=1 %s | FileCheck %s --strict-whitespace
 
 // This file contains Unicode characters; please do not "fix" them!
