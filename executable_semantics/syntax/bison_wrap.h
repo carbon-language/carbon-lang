155c380c5fd3509b79284e3d71e7c01645e6abf2
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-08-18 15:52:14 +0000

    Provide a BisonWrap for default construction

---
+++ executable_semantics/syntax/bison_wrap.h
@@ -0,0 +1,42 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef EXECUTABLE_SEMANTICS_SYNTAX_BISON_WRAP_H_
+#define EXECUTABLE_SEMANTICS_SYNTAX_BISON_WRAP_H_
+
+#include <optional>
+
+#include "common/check.h"
+
+namespace Carbon {
+
+// Bison requires that types be default initializable for use with its variant
+// semantics. This wraps arbitrary types to support a default constructor, while
+// still requiring they be properly initialized.
+template <typename T>
+class BisonWrap {
+ public:
+  // The default constructor leaves the value unset.
+  BisonWrap() {}
+
+  // Assigning a value initializes the wrapper.
+  BisonWrap& operator=(T&& rhs) {
+    val = std::move(rhs);
+    return *this;
+  }
+
+  // Support transparent conversion to the wrapped type, erroring if not
+  // initialized.
+  operator T() const {
+    CHECK(val);
+    return std::move(*val);
+  }
+
+ private:
+  std::optional<T> val;
+};
+
+}  // namespace Carbon
+
+#endif  // EXECUTABLE_SEMANTICS_SYNTAX_BISON_WRAP_H_
