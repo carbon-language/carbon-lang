ea8539111d444ffb93ab09c5865d9a1a0927cb2c
Keno Fischer <keno@juliacomputing.com>
2021-08-19 14:31:39 -0400

    [COFF] Force Symbols containing '.' to be quoted
    In D87099, the mangler learned to quote export directives that contain
    special characters. Only alhpanumerical characters as well as
    '_', '$', '.' and '@' were exmpt from this quoting. However, at least
    binutils considers an unquoted '.' to be syntax and object files
    containing such symbols will cause errors during linking. Fix that
    by removing '.' from the list of allowed exemptions.
    
    Differential Revision: https://reviews.llvm.org/D100359

--- llvm/lib/IR/Mangler.cpp
+++ llvm/lib/IR/Mangler.cpp
@@ -186,7 +186,7 @@ void Mangler::getNameWithPrefix(SmallVectorImpl<char> &OutName,
 
 // Check if the name needs quotes to be safe for the linker to interpret.
 static bool canBeUnquotedInDirective(char C) {
-  return isAlnum(C) || C == '_' || C == '$' || C == '.' || C == '@';
+  return isAlnum(C) || C == '_' || C == '@';
 }
 
 static bool canBeUnquotedInDirective(StringRef Name) {
