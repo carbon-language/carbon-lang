0afd10b4030dcca36c85f24942be8ea860df8d9e
Nikita Popov <nikita.ppv@gmail.com>
2021-08-19 20:56:09 +0200

    [LoopPassManager] Assert that MemorySSA is preserved if used
    Currently it's possible to silently use a loop pass that does not
    preserve MemorySSA in a loop-mssa pass manager, as we don't
    statically know which loop passes preserve MemorySSA (as was the
    case with the legacy pass manager).
    
    However, we can at least add a check after the fact that if
    MemorySSA is used, then it should also have been preserved.
    Hopefully this will reduce confusion as seen in
    https://bugs.llvm.org/show_bug.cgi?id=51020.
    
    Differential Revision: https://reviews.llvm.org/D108399

--- llvm/lib/Transforms/Scalar/LoopPassManager.cpp
+++ llvm/lib/Transforms/Scalar/LoopPassManager.cpp
@@ -285,6 +285,10 @@ PreservedAnalyses FunctionToLoopPassAdaptor::run(Function &F,
     else
       PI.runAfterPass<Loop>(*Pass, *L, PassPA);
 
+    if (LAR.MSSA && !PassPA.getChecker<MemorySSAAnalysis>().preserved())
+      report_fatal_error("Loop pass manager using MemorySSA contains a pass "
+                         "that does not preserve MemorySSA");
+
 #ifndef NDEBUG
     // LoopAnalysisResults should always be valid.
     // Note that we don't LAR.SE.verify() because that can change observed SE
