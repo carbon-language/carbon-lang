e9c8973f1c6879a4545329afceb66cc98447574f
Cullen Rhodes <cullen.rhodes@arm.com>
2021-08-24 08:13:57 +0000

    [AArch64][SME] Fix v8.6a bf16 NEON instruction predication
    In streaming mode on SME targets only the scalar BFCVT armv8.6-a
    instruction is legal, predicate the illegal instructions on NEON to
    disable them in streaming mode (see D107902). BFCVT is predicated on
    HasNEONorStreamingSVE.
    
    The reference can be found here:
    https://developer.arm.com/documentation/ddi0602/2021-06/SIMD-FP-Instructions
    
    Reviewed By: paulwalker-arm
    
    Differential Revision: https://reviews.llvm.org/D108279

--- llvm/test/MC/AArch64/SME/streaming-mode-neon-negative.s
+++ llvm/test/MC/AArch64/SME/streaming-mode-neon-negative.s
@@ -1,4 +1,4 @@
-// RUN: not llvm-mc -triple=aarch64 -show-encoding -mattr=+streaming-sve 2>&1 < %s| FileCheck %s
+// RUN: not llvm-mc -triple=aarch64 -show-encoding -mattr=+streaming-sve,+bf16 2>&1 < %s| FileCheck %s
 
 // ------------------------------------------------------------------------- //
 // Check FABD is illegal in streaming mode
@@ -6,4 +6,12 @@
 fabd s0, s1, s2
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: instruction requires: neon
 // CHECK-NEXT: fabd s0, s1, s2
+// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
+
+// ------------------------------------------------------------------------- //
+// Check non-scalar v8.6a BFloat16 instructions are illegal in streaming mode
+
+bfcvtn v5.4h, v5.4s
+// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: instruction requires: neon
+// CHECK-NEXT: bfcvtn v5.4h, v5.4s
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
