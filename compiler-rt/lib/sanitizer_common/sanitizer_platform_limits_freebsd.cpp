84013d7093c929061073c276a374c72ee77755f9
David Carlier <devnexen@gmail.com>
2021-08-27 23:58:06 +0100

    [Sanitizers] wordexp interceptor build fix on FreeBSD.
    Reviewers: vitalybuka
    
    Reviewed-By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D108838

--- compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_platform_limits_freebsd.cpp
@@ -170,6 +170,7 @@ uptr __sanitizer_in_addr_sz(int af) {
 unsigned struct_ElfW_Phdr_sz = sizeof(Elf_Phdr);
 int glob_nomatch = GLOB_NOMATCH;
 int glob_altdirfunc = GLOB_ALTDIRFUNC;
+const int wordexp_wrde_dooffs = WRDE_DOOFFS;
 
 unsigned path_max = PATH_MAX;
 
