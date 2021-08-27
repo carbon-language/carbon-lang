9d7a77c26d2f68e1c3b58b61d792d371bd3ed224
Fanbo Meng <fanbo.meng@ibm.com>
2021-08-27 10:26:51 -0400

    [MCParser][z/OS] Mark test as unsupported for the z/OS Target
    Marking test as unsupported for the same reason as https://reviews.llvm.org/D105204
    
    Reviewed By: abhina.sreeskantharajan
    
    Differential Revision: https://reviews.llvm.org/D108819

--- clang/test/Driver/as-version.s
+++ clang/test/Driver/as-version.s
@@ -1,5 +1,6 @@
 // Test version information.
 
+// UNSUPPORTED: -zos
 // RUN: %clang -Wa,--version -c -fintegrated-as %s -o /dev/null \
 // RUN:   | FileCheck --check-prefix=IAS %s
 // IAS: clang version
