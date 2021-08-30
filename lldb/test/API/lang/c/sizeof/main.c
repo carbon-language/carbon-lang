2ce889fa4e5cab75fc65d03a4dfae52784d57db9
Raphael Isemann <teemperor@gmail.com>
2021-08-30 15:16:18 +0200

    [lldb][NFC] Add size tests for empty records with alignment and with empty members
    This came up during the Windows bot failure discussing after D105471 . See
    also 3d9a9fa6911a5228ce799a7c639e94d322678934 .

--- lldb/test/API/lang/c/sizeof/main.c
+++ lldb/test/API/lang/c/sizeof/main.c
@@ -1,4 +1,7 @@
 struct Empty {};
+struct EmptyMember {
+  char i[0];
+};
 struct SingleMember {
   int i;
 };
@@ -9,13 +12,15 @@ struct PaddingMember {
 };
 
 const unsigned sizeof_empty = sizeof(struct Empty);
+const unsigned sizeof_empty_member = sizeof(struct EmptyMember);
 const unsigned sizeof_single = sizeof(struct SingleMember);
 const unsigned sizeof_padding = sizeof(struct PaddingMember);
 
 int main() {
   struct Empty empty;
+  struct EmptyMember empty_member;
   struct SingleMember single;
   struct PaddingMember padding;
   // Make sure globals are used.
-  return sizeof_empty + sizeof_single + sizeof_padding;
+  return sizeof_empty + sizeof_empty_member + sizeof_single + sizeof_padding;
 }
