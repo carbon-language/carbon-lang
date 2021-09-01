ceccbb8145c87d62c4a796d7b2a380083cf32331
Michał Górny <mgorny@moritz.systems>
2021-09-01 08:53:11 +0200

    Revert "[lldb] [gdb-remote client] Remove breakpoints throughout vfork"
    This reverts commit 199344d832c1bed1d75b1db2c1493e5a3e5c5544.
    It caused regressions on arm, as reported by lldb-arm-ubuntu buildbot.

--- lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.h
+++ lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.h
@@ -232,7 +232,6 @@ public:
 
   void DidFork(lldb::pid_t child_pid, lldb::tid_t child_tid) override;
   void DidVFork(lldb::pid_t child_pid, lldb::tid_t child_tid) override;
-  void DidVForkDone() override;
 
 protected:
   friend class ThreadGDBRemote;
@@ -296,8 +295,6 @@ protected:
   using FlashRangeVector = lldb_private::RangeVector<lldb::addr_t, size_t>;
   using FlashRange = FlashRangeVector::Entry;
   FlashRangeVector m_erased_flash_ranges;
-
-  bool m_vfork_in_progress;
 
   // Accessors
   bool IsRunning(lldb::StateType state) {
