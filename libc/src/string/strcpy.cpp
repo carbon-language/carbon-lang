0239adac4aa09900937d08ababf34e43bfca1442
Siva Chandra Reddy <sivachandra@google.com>
2021-09-01 06:52:12 +0000

    [libc] Mark return value of memcpy in strcpy as initialized for msan.
    Reviewed By: michaelrj
    
    Differential Revision: https://reviews.llvm.org/D109045

--- libc/src/string/strcpy.cpp
+++ libc/src/string/strcpy.cpp
@@ -11,13 +11,25 @@
 #include "src/string/string_utils.h"
 
 #include "src/__support/common.h"
+#include "src/__support/sanitizer.h"
 
 namespace __llvm_libc {
 
 LLVM_LIBC_FUNCTION(char *, strcpy,
                    (char *__restrict dest, const char *__restrict src)) {
-  return reinterpret_cast<char *>(
-      __llvm_libc::memcpy(dest, src, internal::string_length(src) + 1));
+  size_t size = internal::string_length(src) + 1;
+  char *result = reinterpret_cast<char *>(__llvm_libc::memcpy(dest, src, size));
+
+  // In many libc uses, we do not want memcpy to be instrumented. Hence,
+  // we mark the destination as initialized.
+  //
+  // We do not want memcpy to be instrumented because compilers can potentially
+  // generate calls to memcpy. If the sanitizer business logic ends up with a
+  // compiler generated call to memcpy which is instrumented, then it will
+  // break the sanitizers.
+  SANITIZER_MEMORY_INITIALIZED(result, size);
+
+  return result;
 }
 
 } // namespace __llvm_libc
