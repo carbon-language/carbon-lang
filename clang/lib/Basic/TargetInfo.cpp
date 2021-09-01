cc9260a0fb7c42ac8a8bb87eac1e11900c25ed20
Justas Janickas <Justas.Janickas@arm.com>
2021-09-01 09:14:07 +0100

    [OpenCL] Supports optional generic address space semantics in C++ for OpenCL 2021
    Adds support for a feature macro `__opencl_c_generic_adress_space`
    in C++ for OpenCL 2021 enabling a respective optional core feature
    from OpenCL 3.0. Testing is only performed in SemaOpenCL because
    generic address space functionality is yet to be implemented in
    C++ for OpenCL 2021.
    
    This change aims to achieve compatibility between C++ for OpenCL
    2021 and OpenCL 3.0.
    
    Differential Revision: https://reviews.llvm.org/D108461

--- clang/lib/Basic/TargetInfo.cpp
+++ clang/lib/Basic/TargetInfo.cpp
@@ -406,12 +406,13 @@ void TargetInfo::adjust(DiagnosticsEngine &Diags, LangOptions &Opts) {
     // for OpenCL C 2.0 but with no access to target capabilities. Target
     // should be immutable once created and thus these language options need
     // to be defined only once.
-    if (Opts.OpenCLVersion == 300) {
+    if (Opts.getOpenCLCompatibleVersion() == 300) {
       const auto &OpenCLFeaturesMap = getSupportedOpenCLOpts();
       Opts.OpenCLGenericAddressSpace = hasFeatureEnabled(
           OpenCLFeaturesMap, "__opencl_c_generic_address_space");
-      Opts.OpenCLPipes =
-          hasFeatureEnabled(OpenCLFeaturesMap, "__opencl_c_pipes");
+      if (Opts.OpenCLVersion == 300)
+        Opts.OpenCLPipes =
+            hasFeatureEnabled(OpenCLFeaturesMap, "__opencl_c_pipes");
     }
   }
 
