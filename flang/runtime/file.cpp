651f58bf63e6cb6926faf0f33200c57fdd591a85
Diana Picus <diana.picus@linaro.org>
2021-09-02 08:14:01 +0000

    [flang] Remove *- C++ -* incantation from runtime .cpp files. NFC
    We should only need to spell the language out in .h files.
    
    Differential Revision: https://reviews.llvm.org/D109138

--- flang/runtime/file.cpp
+++ flang/runtime/file.cpp
@@ -1,4 +1,4 @@
-//===-- runtime/file.cpp ----------------------------------------*- C++ -*-===//
+//===-- runtime/file.cpp --------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- flang/runtime/io-stmt.cpp
+++ flang/runtime/io-stmt.cpp
@@ -1,4 +1,4 @@
-//===-- runtime/io-stmt.cpp -------------------------------------*- C++ -*-===//
+//===-- runtime/io-stmt.cpp -----------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
