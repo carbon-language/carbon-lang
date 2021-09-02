7f544f7658357045bde67c2f897a088558310f16
Nico Weber <thakis@chromium.org>
2021-09-02 11:32:08 -0400

    Try to unbreak lldb build after 973519826edb76

--- lldb/include/lldb/Utility/Environment.h
+++ lldb/include/lldb/Utility/Environment.h
@@ -73,7 +73,7 @@ public:
     return insert(std::make_pair(Split.first, std::string(Split.second)));
   }
 
-  void insert(const_iterator first, const_iterator last);
+  void insert(iterator first, iterator last);
 
   Envp getEnvp() const { return Envp(*this); }
 
--- lldb/source/Utility/Environment.cpp
+++ lldb/source/Utility/Environment.cpp
@@ -41,7 +41,7 @@ Environment::Environment(const char *const *Env) {
     insert(*Env++);
 }
 
-void Environment::insert(const_iterator first, const_iterator last) {
+void Environment::insert(iterator first, iterator last) {
   while (first != last) {
     try_emplace(first->first(), first->second);
     ++first;
