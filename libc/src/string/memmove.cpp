b659b789c03ac339e28d7b91406b67bb887a426d
Cheng Wang <chennngwang@gmail.com>
2021-09-05 11:09:34 +0800

    [libc] Some clean work with memmove.
    - Replace `move_byte_forward()` with `memcpy`. In `memcpy` implementation,
    it copies bytes forward from beginning to end. Otherwise, `memmove` unit
    tests will break.
    - Make `memmove` unit tests work.
    
    Reviewed By: gchatelet
    
    Differential Revision: https://reviews.llvm.org/D109316

--- libc/src/string/memmove.cpp
+++ libc/src/string/memmove.cpp
@@ -15,52 +15,50 @@
 
 namespace __llvm_libc {
 
-static inline void move_byte_forward(char *dest_m, const char *src_m,
-                                     size_t count) {
-  for (size_t offset = 0; count; --count, ++offset)
-    dest_m[offset] = src_m[offset];
-}
-
-static inline void move_byte_backward(char *dest_m, const char *src_m,
+static inline void move_byte_backward(char *dst, const char *src,
                                       size_t count) {
   for (size_t offset = count - 1; count; --count, --offset)
-    dest_m[offset] = src_m[offset];
+    dst[offset] = src[offset];
+}
+
+static void memmove_impl(char *dst, const char *src, size_t count) {
+
+  // If the distance between `src` and `dst` is equal to or greater
+  // than count (integerAbs(src - dst) >= count), they would not overlap.
+  // e.g. greater     equal       overlapping
+  //      [12345678]  [12345678]  [12345678]
+  // src: [_ab_____]  [_ab_____]  [_ab_____]
+  // dst: [_____yz_]  [___yz___]  [__yz____]
+
+  // Call `memcpy` when `src` and `dst` do not overlap.
+  if (__llvm_libc::integerAbs(src - dst) >= static_cast<ptrdiff_t>(count))
+    __llvm_libc::memcpy(dst, src, count);
+
+  // Overlap cases.
+  // If `dst` starts before `src` (dst < src), copy forward from beginning to
+  // end. If `dst` starts after `src` (dst > src), copy backward from end to
+  // beginning. If `dst` and `src` start at the same address (dst == src), do
+  // nothing.
+  // e.g. forward      backward
+  //             *->    <-*
+  // src: [___abcde_]  [_abcde___]
+  // dst: [_abc--___]  [___--cde_]
+
+  // In `memcpy` implementation, it copies bytes forward from beginning to
+  // end. Otherwise, `memmove` unit tests will break.
+  if (dst < src)
+    __llvm_libc::memcpy(dst, src, count);
+
+  // TODO: Optimize `move_byte_xxx(...)` functions.
+  if (dst > src)
+    move_byte_backward(dst, src, count);
 }
 
 LLVM_LIBC_FUNCTION(void *, memmove,
-                   (void *dest, const void *src, size_t count)) {
-  char *dest_c = reinterpret_cast<char *>(dest);
-  const char *src_c = reinterpret_cast<const char *>(src);
-
-  // If the distance between src_c and dest_c is equal to or greater
-  // than count (integerAbs(src_c - dest_c) >= count), they would not overlap.
-  // e.g.   greater     equal       overlapping
-  //        [12345678]  [12345678]  [12345678]
-  // src_c: [_ab_____]  [_ab_____]  [_ab_____]
-  // dest_c:[_____yz_]  [___yz___]  [__yz____]
-
-  // Use memcpy if src_c and dest_c do not overlap.
-  if (__llvm_libc::integerAbs(src_c - dest_c) >= static_cast<ptrdiff_t>(count))
-    return __llvm_libc::memcpy(dest_c, src_c, count);
-
-  // Overlap cases.
-  // If dest_c starts before src_c (dest_c < src_c), copy forward(pointer add 1)
-  // from beginning to end.
-  // If dest_c starts after src_c (dest_c > src_c), copy backward(pointer add
-  // -1) from end to beginning.
-  // If dest_c and src_c start at the same address (dest_c == src_c),
-  // just return dest.
-  // e.g.    forward      backward
-  //             *-->        <--*
-  // src_c : [___abcde_]  [_abcde___]
-  // dest_c: [_abc--___]  [___--cde_]
-
-  // TODO: Optimize `move_byte_xxx(...)` functions.
-  if (dest_c < src_c)
-    move_byte_forward(dest_c, src_c, count);
-  if (dest_c > src_c)
-    move_byte_backward(dest_c, src_c, count);
-  return dest;
+                   (void *dst, const void *src, size_t count)) {
+  memmove_impl(reinterpret_cast<char *>(dst),
+               reinterpret_cast<const char *>(src), count);
+  return dst;
 }
 
 } // namespace __llvm_libc
