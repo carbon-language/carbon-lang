09dc454b00b8ed0a19f766f760fa19e86a0b9059
Justas Janickas <Justas.Janickas@arm.com>
2021-09-07 14:32:05 +0100

    [OpenCL] Enables .rgba vector extension in C++ for OpenCL 2021
    `.rgba` vector extension setting in C++ for OpenCL 2021 is now
    performed analogously to OpenCL C 3.0. Test case added.
    
    Differential Revision: https://reviews.llvm.org/D109370

--- clang/lib/Sema/SemaExprMember.cpp
+++ clang/lib/Sema/SemaExprMember.cpp
@@ -340,7 +340,8 @@ CheckExtVectorComponent(Sema &S, QualType baseType, ExprValueKind &VK,
 
     // Emit a warning if an rgba selector is used earlier than OpenCL C 3.0.
     if (HasRGBA || (*compStr && IsRGBA(*compStr))) {
-      if (S.getLangOpts().OpenCL && S.getLangOpts().OpenCLVersion < 300) {
+      if (S.getLangOpts().OpenCL &&
+          S.getLangOpts().getOpenCLCompatibleVersion() < 300) {
         const char *DiagBegin = HasRGBA ? CompName->getNameStart() : compStr;
         S.Diag(OpLoc, diag::ext_opencl_ext_vector_type_rgba_selector)
             << StringRef(DiagBegin, 1) << SourceRange(CompLoc);
