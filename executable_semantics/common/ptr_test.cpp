7cd4c0283546864af9d2f8f38983ebedb8289e69
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-09-10 18:07:50 +0000

    Test ->

--- executable_semantics/common/ptr_test.cpp
+++ executable_semantics/common/ptr_test.cpp
@@ -28,26 +28,31 @@ TEST(PtrTest, Constness) {
   // A non-const ptr returns a non-const T.
   Ptr<Stub> p(&i);
   static_assert(!IsConstRef<decltype(*p)>);
+  static_assert(!IsConstRef<decltype((p->i))>);
   static_assert(!IsConstPtr<decltype(p.Get())>);
 
   // A ptr to const returns a const T.
   Ptr<const Stub> p_const(&i);
   static_assert(IsConstRef<decltype(*p_const)>);
+  static_assert(IsConstRef<decltype((p_const->i))>);
   static_assert(IsConstPtr<decltype(p_const.Get())>);
 
   // A const ptr also returns a const T.
   const Ptr<Stub> const_p(&i);
   static_assert(IsConstRef<decltype(*const_p)>);
+  static_assert(IsConstRef<decltype((const_p->i))>);
   static_assert(IsConstPtr<decltype(const_p.Get())>);
 
   // A non-const vector of ptrs gets non-const ptr access.
   std::vector<Ptr<Stub>> v = {Ptr<Stub>(&i)};
   static_assert(!IsConstRef<decltype(*v[0])>);
+  static_assert(!IsConstRef<decltype((v[0]->i))>);
   static_assert(!IsConstPtr<decltype(v[0].Get())>);
 
   // Constness of the vector propagates to ptr access.
   const std::vector<Ptr<Stub>> const_v = v;
   static_assert(IsConstRef<decltype(*const_v[0])>);
+  static_assert(IsConstRef<decltype((const_v[0]->i))>);
   static_assert(IsConstPtr<decltype(const_v[0].Get())>);
 }
 
