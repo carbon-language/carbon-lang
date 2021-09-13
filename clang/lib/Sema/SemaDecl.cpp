6377426b4a326b52733065609a5d811afd2b8b1b
Andrew Savonichev <andrew.savonichev@gmail.com>
2021-09-13 15:34:21 +0300

    Revert "[clang] Check unsupported types in expressions"
    This reverts commit ec6c847179fd019acae4d97a18f9e7d3961a6fdf.
    
    Fails on check-openmp:
    
    /b/1/openmp-clang-x86_64-linux-debian/llvm.build/projects/openmp/runtime/test/lock/Output/omp_init_lock.c.tmp
    --
    Exit Code: -11

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9569,7 +9569,8 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
     }
   }
 
-  checkTypeSupport(NewFD->getType(), D.getBeginLoc(), NewFD);
+  if (LangOpts.SYCLIsDevice || (LangOpts.OpenMP && LangOpts.OpenMPIsDevice))
+    checkDeviceDecl(NewFD, D.getBeginLoc());
 
   if (!getLangOpts().CPlusPlus) {
     // Perform semantic checking on the function declaration.
