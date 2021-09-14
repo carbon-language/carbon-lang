3582828748282708ea33cadb587b637d9ffa8546
Cheng Wang <chennngwang@gmail.com>
2021-09-14 17:19:28 +0800

    [libc][Obvious] Some clean work with memmove.

--- libc/src/string/memmove.cpp
+++ libc/src/string/memmove.cpp
@@ -28,30 +28,30 @@ static inline void move_byte_backward(char *dest_m, const char *src_m,
 }
 
 LLVM_LIBC_FUNCTION(void *, memmove,
-                   (void *dest, const void *src, size_t count)) {
-  char *dest_c = reinterpret_cast<char *>(dest);
+                   (void *dst, const void *src, size_t count)) {
+  char *dest_c = reinterpret_cast<char *>(dst);
   const char *src_c = reinterpret_cast<const char *>(src);
 
-  // If the distance between src_c and dest_c is equal to or greater
-  // than count (integerAbs(src_c - dest_c) >= count), they would not overlap.
+  // If the distance between `src_c` and `dest_c` is equal to or greater
+  // than `count` (integerAbs(src_c - dest_c) >= count), they would not overlap.
   // e.g.   greater     equal       overlapping
   //        [12345678]  [12345678]  [12345678]
   // src_c: [_ab_____]  [_ab_____]  [_ab_____]
   // dest_c:[_____yz_]  [___yz___]  [__yz____]
 
-  // Use memcpy if src_c and dest_c do not overlap.
+  // Call `memcpy` if `src_c` and `dest_c` do not overlap.
   if (__llvm_libc::integerAbs(src_c - dest_c) >= static_cast<ptrdiff_t>(count))
     return __llvm_libc::memcpy(dest_c, src_c, count);
 
-  // Overlap cases.
-  // If dest_c starts before src_c (dest_c < src_c), copy forward(pointer add 1)
-  // from beginning to end.
-  // If dest_c starts after src_c (dest_c > src_c), copy backward(pointer add
-  // -1) from end to beginning.
-  // If dest_c and src_c start at the same address (dest_c == src_c),
+  // Overlapping cases.
+  // If `dest_c` starts before `src_c` (dest_c < src_c), copy
+  // forward(pointer add 1) from beginning to end.
+  // If `dest_c` starts after `src_c` (dest_c > src_c), copy
+  // backward(pointer add -1) from end to beginning.
+  // If `dest_c` and `src_c` start at the same address (dest_c == src_c),
   // just return dest.
   // e.g.    forward      backward
-  //             *-->        <--*
+  //                *->    <-*
   // src_c : [___abcde_]  [_abcde___]
   // dest_c: [_abc--___]  [___--cde_]
 
@@ -60,7 +60,7 @@ LLVM_LIBC_FUNCTION(void *, memmove,
     move_byte_forward(dest_c, src_c, count);
   if (dest_c > src_c)
     move_byte_backward(dest_c, src_c, count);
-  return dest;
+  return dst;
 }
 
 } // namespace __llvm_libc
