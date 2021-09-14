f78f613bb74ca837be36efcc72105c3c2d54b4f3
Nico Weber <thakis@chromium.org>
2021-09-14 08:58:59 -0400

    [llvm cmake] replace tabs with spaces in config-ix.cmake

--- llvm/cmake/config-ix.cmake
+++ llvm/cmake/config-ix.cmake
@@ -589,7 +589,7 @@ endif()
 
 find_program(GOLD_EXECUTABLE NAMES ${LLVM_DEFAULT_TARGET_TRIPLE}-ld.gold ld.gold ${LLVM_DEFAULT_TARGET_TRIPLE}-ld ld DOC "The gold linker")
 set(LLVM_BINUTILS_INCDIR "" CACHE PATH
-	"PATH to binutils/include containing plugin-api.h for gold plugin.")
+    "PATH to binutils/include containing plugin-api.h for gold plugin.")
 
 if(CMAKE_GENERATOR STREQUAL "Ninja")
   execute_process(COMMAND ${CMAKE_MAKE_PROGRAM} --version
