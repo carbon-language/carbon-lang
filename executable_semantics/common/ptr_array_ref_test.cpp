f4737041050cd71d411f003f4be9175c0fa71d23
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-09-14 19:39:25 +0000

    clean up tests

--- executable_semantics/common/ptr_array_ref_test.cpp
+++ executable_semantics/common/ptr_array_ref_test.cpp
@@ -21,43 +21,50 @@ inline constexpr bool IsConstRef = std::is_const_v<std::remove_reference_t<T>>;
 template <typename T>
 inline constexpr bool IsConstPtr = std::is_const_v<std::remove_pointer_t<T>>;
 
-TEST(PtrTest, Constness) {
+TEST(PtrArrayRefTest, MutableT) {
   // A non-const value to point at.
   Stub i;
 
-  // A non-const ptr returns a non-const T.
+  // The mutable Ptr is mutable.
   Ptr<Stub> p(&i);
   static_assert(!IsConstRef<decltype(*p)>);
   static_assert(!IsConstRef<decltype((p->i))>);
   static_assert(!IsConstPtr<decltype(p.Get())>);
 
-  // A ptr to const returns a const T.
-  Ptr<const Stub> p_const(&i);
-  static_assert(IsConstRef<decltype(*p_const)>);
-  static_assert(IsConstRef<decltype((p_const->i))>);
-  static_assert(IsConstPtr<decltype(p_const.Get())>);
-
-  // A non-const vector of ptrs gets non-const ptr access.
+  // Still mutable in a vector.
   std::vector<Ptr<Stub>> v = {p};
   static_assert(!IsConstRef<decltype(*v[0])>);
   static_assert(!IsConstRef<decltype((v[0]->i))>);
   static_assert(!IsConstPtr<decltype(v[0].Get())>);
 
+  // PtrArrayRef returns const references.
   PtrArrayRef<Stub> ref(v);
   static_assert(IsConstRef<decltype(*ref[0])>);
   static_assert(IsConstRef<decltype((ref[0]->i))>);
   static_assert(IsConstPtr<decltype(ref[0].Get())>);
+}
 
-  // Constness of the vector propagates to ptr access.
-  std::vector<Ptr<const Stub>> v_const = {p_const};
-  static_assert(IsConstRef<decltype(*v_const[0])>);
-  static_assert(IsConstRef<decltype((v_const[0]->i))>);
-  static_assert(IsConstPtr<decltype(v_const[0].Get())>);
+TEST(PtrArrayRefTest, ConstT) {
+  // A non-const value to point at.
+  Stub i;
 
-  PtrArrayRef<const Stub> ref_const(v_const);
-  static_assert(IsConstRef<decltype(*ref_const[0])>);
-  static_assert(IsConstRef<decltype((ref_const[0]->i))>);
-  static_assert(IsConstPtr<decltype(ref_const[0].Get())>);
+  // The const Ptr is const.
+  Ptr<const Stub> p(&i);
+  static_assert(IsConstRef<decltype(*p)>);
+  static_assert(IsConstRef<decltype((p->i))>);
+  static_assert(IsConstPtr<decltype(p.Get())>);
+
+  // Still const in a vector.
+  std::vector<Ptr<const Stub>> v = {p};
+  static_assert(IsConstRef<decltype(*v[0])>);
+  static_assert(IsConstRef<decltype((v[0]->i))>);
+  static_assert(IsConstPtr<decltype(v[0].Get())>);
+
+  // PtrArrayRef is happy to accept const.
+  PtrArrayRef<const Stub> ref(v);
+  static_assert(IsConstRef<decltype(*ref[0])>);
+  static_assert(IsConstRef<decltype((ref[0]->i))>);
+  static_assert(IsConstPtr<decltype(ref[0].Get())>);
 }
 
 }  // namespace
