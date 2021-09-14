7eb67748f9d7186419d678e807c01fc2a3811a80
Vedant Kumar <vsk@apple.com>
2021-09-14 16:58:40 -0700

    [MachCore] Report arm64 thread exception state
    A MachO userspace corefile may contain LC_THREAD commands which specify
    thread exception state.
    
    For arm64* only (for now), report a human-readable version of this state
    as the thread stop reason, instead of 'SIGSTOP'.
    
    As a follow-up, similar functionality can be implemented for x86 cores
    by translating the trapno/err exception registers.
    
    rdar://82898146
    
    Differential Revision: https://reviews.llvm.org/D109795

--- lldb/source/Plugins/Process/mach-core/ThreadMachCore.cpp
+++ lldb/source/Plugins/Process/mach-core/ThreadMachCore.cpp
@@ -9,7 +9,9 @@
 #include "ThreadMachCore.h"
 
 #include "lldb/Breakpoint/Watchpoint.h"
+#include "lldb/Host/SafeMachO.h"
 #include "lldb/Symbol/ObjectFile.h"
+#include "lldb/Target/AppleArm64ExceptionClass.h"
 #include "lldb/Target/Process.h"
 #include "lldb/Target/RegisterContext.h"
 #include "lldb/Target/StopInfo.h"
@@ -17,6 +19,7 @@
 #include "lldb/Target/Unwind.h"
 #include "lldb/Utility/ArchSpec.h"
 #include "lldb/Utility/DataExtractor.h"
+#include "lldb/Utility/RegisterValue.h"
 #include "lldb/Utility/State.h"
 #include "lldb/Utility/StreamString.h"
 
@@ -88,10 +91,55 @@ ThreadMachCore::CreateRegisterContextForFrame(StackFrame *frame) {
   return reg_ctx_sp;
 }
 
+static bool IsCrashExceptionClass(AppleArm64ExceptionClass EC) {
+  switch (EC) {
+  case AppleArm64ExceptionClass::ESR_EC_UNCATEGORIZED:
+  case AppleArm64ExceptionClass::ESR_EC_SVC_32:
+  case AppleArm64ExceptionClass::ESR_EC_SVC_64:
+    // In the ARM exception model, a process takes an exception when asking the
+    // kernel to service a system call. Don't treat this like a crash.
+    return false;
+  default:
+    return true;
+  }
+}
+
 bool ThreadMachCore::CalculateStopInfo() {
   ProcessSP process_sp(GetProcess());
   if (process_sp) {
-    SetStopInfo(StopInfo::CreateStopReasonWithSignal(*this, SIGSTOP));
+    StopInfoSP stop_info;
+    RegisterContextSP reg_ctx_sp = GetRegisterContext();
+
+    if (reg_ctx_sp) {
+      Target &target = process_sp->GetTarget();
+      const ArchSpec arch_spec = target.GetArchitecture();
+      const uint32_t cputype = arch_spec.GetMachOCPUType();
+
+      if (cputype == llvm::MachO::CPU_TYPE_ARM64 ||
+          cputype == llvm::MachO::CPU_TYPE_ARM64_32) {
+        const RegisterInfo *esr_info = reg_ctx_sp->GetRegisterInfoByName("esr");
+        const RegisterInfo *far_info = reg_ctx_sp->GetRegisterInfoByName("far");
+        RegisterValue esr, far;
+        if (reg_ctx_sp->ReadRegister(esr_info, esr) &&
+            reg_ctx_sp->ReadRegister(far_info, far)) {
+          const uint32_t esr_val = esr.GetAsUInt32();
+          const AppleArm64ExceptionClass exception_class =
+              getAppleArm64ExceptionClass(esr_val);
+          if (IsCrashExceptionClass(exception_class)) {
+            StreamString S;
+            S.Printf("%s (fault address: 0x%" PRIx64 ")",
+                     toString(exception_class), far.GetAsUInt64());
+            stop_info =
+                StopInfo::CreateStopReasonWithException(*this, S.GetData());
+          }
+        }
+      }
+    }
+
+    // Set a stop reason for crashing threads only so that they get selected
+    // preferentially.
+    if (stop_info)
+      SetStopInfo(stop_info);
     return true;
   }
   return false;
