07c8decd623d1e4bca677ccf7042d07e878a4234
Jon Meow <46229924+jonmeow@users.noreply.github.com>
2021-09-16 16:38:26 -0700

    Set issue report url (#833)
    Defaults to llvm.org, which we definitely don't want

--- executable_semantics/main.cpp
+++ executable_semantics/main.cpp
@@ -13,6 +13,10 @@
 #include "llvm/Support/InitLLVM.h"
 
 int main(int argc, char* argv[]) {
+  llvm::setBugReportMsg(
+      "Please report issues to "
+      "https://github.com/carbon-language/carbon-lang/issues and include the "
+      "crash backtrace.\n");
   llvm::InitLLVM(argc, argv);
 
   // Printing to stderr should flush stdout. This is most noticeable when stderr
