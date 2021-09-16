05ea321f716390abda1632cca71dc9441662cf62
Nico Weber <thakis@chromium.org>
2021-09-16 08:43:58 -0400

    [clang-cl] Fix test after 951f362e256 on systems where default target isn't x86_64

--- clang/test/Driver/cl-sysroot.cpp
+++ clang/test/Driver/cl-sysroot.cpp
@@ -1,8 +1,10 @@
 // RUN: rm -rf %t
 // RUN: split-file %s %t
 
-// RUN: %clang_cl -m64 /winsysroot %t -### -- %t/foo.cpp 2>&1 | FileCheck %s
-// RUN: %clang_cl -m64 \
+// RUN: %clang_cl --target=x86_64-windows \
+// RUN:     /winsysroot %t \
+// RUN:     -### -- %t/foo.cpp 2>&1 | FileCheck %s
+// RUN: %clang_cl --target=x86_64-windows \
 // RUN:     /diasdkdir "%t/DIA SDK" \
 // RUN:     /vctoolsdir %t/VC/Tools/MSVC/27.1828.18284 \
 // RUN:     /winsdkdir "%t/Windows Kits/10" \
