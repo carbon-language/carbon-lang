c3717b6858d32d64514a187ede1a77be8ba4e542
serge-sans-paille <sguelton@redhat.com>
2021-09-16 18:13:15 +0200

    Simplify handling of builtin with inline redefinition
    (This is a recommit of 3d6f49a56995b845 that should no longer break validation since
    bd379915de38a9af3d65e1).
    
    It is a common practice in glibc header to provide an inline redefinition of an
    existing function. It is especially the case for fortified function.
    
    Clang currently has an imperfect approach to the problem, using a combination of
    trivially recursive function detection and noinline attribute.
    
    Simplify the logic by suffixing these functions by `.inline` during codegen, so
    that they are not recognized as builtin by llvm.
    
    After that patch, clang passes all tests from https://github.com/serge-sans-paille/fortify-test-suite
    
    Differential Revision: https://reviews.llvm.org/D109967

--- clang/test/CodeGen/memcpy-nobuiltin.c
+++ clang/test/CodeGen/memcpy-nobuiltin.c
@@ -4,7 +4,8 @@
 //
 // CHECK-WITH-DECL-NOT: @llvm.memcpy
 // CHECK-NO-DECL: @llvm.memcpy
-// CHECK-SELF-REF-DECL: @llvm.memcpy
+// CHECK-SELF-REF-DECL-LABEL: define dso_local i8* @memcpy.inline
+// CHECK-SELF-REF-DECL:       @memcpy(
 //
 #include <memcpy-nobuiltin.inc>
 void test(void *dest, void const *from, size_t n) {
