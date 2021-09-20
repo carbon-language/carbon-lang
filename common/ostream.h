a94cc6b24cb1c694237070024bfe1b35426ecd53
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-09-20 21:03:53 +0000

    Remove body

--- common/ostream.h
+++ common/ostream.h
@@ -23,9 +23,7 @@ template <typename T, typename std::enable_if<std::is_member_function_pointer<
                           decltype(&T::Print)>::value>::type* = nullptr>
 __attribute__((unavailable(
     "Received a pointer to a printable type, are you missing a `*`?"))) auto
-operator<<(llvm::raw_ostream& out, const T* /*obj*/) -> llvm::raw_ostream& {
-  return out;
-}
+operator<<(llvm::raw_ostream& out, const T* /*obj*/) -> llvm::raw_ostream&;
 
 }  // namespace Carbon
 
