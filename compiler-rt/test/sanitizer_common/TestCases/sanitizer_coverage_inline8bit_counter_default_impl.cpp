11c533e1ea38381726ed04ab65937a03fbfb3e19
Kostya Serebryany <kcc@google.com>
2021-09-20 17:58:50 -0700

    [sanitizer coverage] write the pc-table at the process exit
    The current code writes the pc-table at the process startup,
    which may happen before the common_flags() are initialized.
    Move writing to the process end.
    This is consistent with how we write the counters and avoids the problem with the uninitalized flags.
    Add prints if verbosity>=1.
    
    Reviewed By: kostik
    
    Differential Revision: https://reviews.llvm.org/D110119

--- compiler-rt/test/sanitizer_common/TestCases/sanitizer_coverage_inline8bit_counter_default_impl.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/sanitizer_coverage_inline8bit_counter_default_impl.cpp
@@ -5,7 +5,7 @@
 
 // RUN: %clangxx -O0 %s -fsanitize-coverage=inline-8bit-counters,pc-table -o %t
 // RUN: rm -f %t-counters %t-pcs
-// RUN: env %tool_options="cov_8bit_counters_out=%t-counters cov_pcs_out=%t-pcs" %run %t 2>&1 | FileCheck %s
+// RUN: env %tool_options="cov_8bit_counters_out=%t-counters cov_pcs_out=%t-pcs verbosity=1" %run %t 2>&1 | FileCheck %s
 
 // Check the file sizes
 // RUN: wc -c %t-counters | grep "^2 "
@@ -19,4 +19,6 @@ int main() {
   foo();
   fprintf(stderr, "PASS\n");
   // CHECK: PASS
+  // CHECK: cov_8bit_counters_out: written {{.*}} bytes to {{.*}}-counter
+  // CHECK: cov_pcs_out: written {{.*}} bytes to {{.*}}-pcs
 }
