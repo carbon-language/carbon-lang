2ce73f13c98ad3bfb904ac991f5810ddff9e77e7
Lang Hames <lhames@gmail.com>
2021-09-22 11:16:09 +1000

    [ORC] Fix file header.

--- llvm/lib/ExecutionEngine/Orc/TargetProcess/SimpleExecutorMemoryManager.cpp
+++ llvm/lib/ExecutionEngine/Orc/TargetProcess/SimpleExecutorMemoryManager.cpp
@@ -1,4 +1,4 @@
-//===------- ResourceManager.cpp - Abstract executor resource mgmt --------===//
+//===- SimpleExecuorMemoryManagare.cpp - Simple executor-side memory mgmt -===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
