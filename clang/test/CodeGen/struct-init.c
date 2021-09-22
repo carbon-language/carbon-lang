d5631d49d070116ba2e23baad1ecf5f4cc08e6d1
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2021-09-22 11:29:35 +0100

    Drop REQUIRES: arm-registered-target from an IR-only test
    This works just fine even if the Arm backend is not built.

--- clang/test/CodeGen/struct-init.c
+++ clang/test/CodeGen/struct-init.c
@@ -1,5 +1,4 @@
-// REQUIRES: arm-registered-target
-// RUN: %clang_cc1 -S -triple armv7-apple-darwin -target-feature +neon %s -emit-llvm -o - | FileCheck %s
+// RUN: %clang_cc1 -triple armv7-apple-darwin -target-feature +neon %s -emit-llvm -o - | FileCheck %s
 
 typedef struct _zend_ini_entry zend_ini_entry;
 struct _zend_ini_entry {
