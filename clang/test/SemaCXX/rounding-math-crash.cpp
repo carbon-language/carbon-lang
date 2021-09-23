8ecf3660f2de3b88d10db0fd52d0bc80bda33dcc
Haojian Wu <hokein.wu@gmail.com>
2021-09-23 17:03:40 +0200

    Fix buildbot error.
    -frounding-math is not available for all targets.

--- clang/test/SemaCXX/rounding-math-crash.cpp
+++ clang/test/SemaCXX/rounding-math-crash.cpp
@@ -1,3 +1,3 @@
-// RUN: %clang_cc1 -fsyntax-only -frounding-math -verify %s
+// RUN: %clang_cc1 -triple x86_64-linux -fsyntax-only -frounding-math -verify %s
 
 template <class b> b::a() {}  // expected-error {{nested name specifier}}
