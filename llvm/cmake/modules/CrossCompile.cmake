4a45f95e7612bc5dd1d68db84df266cfcc62ff95
Nico Weber <thakis@chromium.org>
2021-09-24 18:48:08 -0400

    [llvm] Remove LLVM_CHECK_ENABLED_PROJECTS again
    This reverts commit 55f0b337087136554122f942fea951a357bc4a49 and
    follow-up reverts commit e9ea03c62ccc1ed4e3ed4f20e37640cfdd76cbcf.
    
    LLVM_EXTERNAL_PROJECTS is sufficient, see https://reviews.llvm.org/D110016

--- llvm/cmake/modules/CrossCompile.cmake
+++ llvm/cmake/modules/CrossCompile.cmake
@@ -75,7 +75,6 @@ function(llvm_create_cross_target project_name target_name toolchain buildtype)
         -DLLVM_EXPERIMENTAL_TARGETS_TO_BUILD="${experimental_targets_to_build_arg}"
         -DLLVM_DEFAULT_TARGET_TRIPLE="${TARGET_TRIPLE}"
         -DLLVM_TARGET_ARCH="${LLVM_TARGET_ARCH}"
-        -DLLVM_CHECK_ENABLED_PROJECTS="${LLVM_CHECK_ENABLED_PROJECTS}"
         -DLLVM_ENABLE_PROJECTS="${llvm_enable_projects_arg}"
         -DLLVM_EXTERNAL_PROJECTS="${llvm_external_projects_arg}"
         ${external_project_source_dirs}
