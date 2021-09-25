1c3859f31d0ef8e99c4aabbcfd11e5eea03e3c15
Nikita Popov <nikita.ppv@gmail.com>
2021-09-25 22:01:28 +0200

    [BasicAA] Don't consider Argument as escape source (NFCI)
    The case of an Argument and an identified function local is already
    handled earlier, because we don't care about captures in that case.
    As such, we don't need to additionally consider the combination of
    an Argument with a non-escaping identified function local.
    
    This ensures that isEscapeSource() only returns true for
    instructions, which is necessary for D110368.

--- llvm/lib/Analysis/BasicAliasAnalysis.cpp
+++ llvm/lib/Analysis/BasicAliasAnalysis.cpp
@@ -120,9 +120,6 @@ static bool isEscapeSource(const Value *V) {
   if (isa<CallBase>(V))
     return true;
 
-  if (isa<Argument>(V))
-    return true;
-
   // The load case works because isNonEscapingLocalObject considers all
   // stores to be escapes (it passes true for the StoreCaptures argument
   // to PointerMayBeCaptured).
