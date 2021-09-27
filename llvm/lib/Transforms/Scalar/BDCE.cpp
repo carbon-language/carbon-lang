9232ca4712cf3f3a99a8cd095165241daf0ddc4e
Adrian Prantl <aprantl@apple.com>
2021-09-27 10:51:04 -0700

    Improve the effectiveness of BDCE's debug info salvaging
    This patch improves the effectiveness of BDCE's debug info salvaging
    by processing the instructions in reverse order and delaying
    dropAllReferences until after debug info salvaging. This allows
    salvaging of entire chains of deleted instructions!
    
    Previously we would remove all references from an instruction, which
    would make it impossible to use that instruction to salvage a later
    instruction in the instruction stream, because its operands were
    already removed.
    
    This reapplies the previous patch with a fix for a use-after-free.
    
    Differential Revision: https://reviews.llvm.org/D110568

--- llvm/lib/Transforms/Scalar/BDCE.cpp
+++ llvm/lib/Transforms/Scalar/BDCE.cpp
@@ -106,9 +106,7 @@ static bool bitTrackingDCE(Function &F, DemandedBits &DB) {
         (I.getType()->isIntOrIntVectorTy() &&
          DB.getDemandedBits(&I).isNullValue() &&
          wouldInstructionBeTriviallyDead(&I))) {
-      salvageDebugInfo(I);
       Worklist.push_back(&I);
-      I.dropAllReferences();
       Changed = true;
       continue;
     }
@@ -153,6 +151,11 @@ static bool bitTrackingDCE(Function &F, DemandedBits &DB) {
       ++NumSimplified;
       Changed = true;
     }
+  }
+
+  for (Instruction *&I : llvm::reverse(Worklist)) {
+    salvageDebugInfo(*I);
+    I->dropAllReferences();
   }
 
   for (Instruction *&I : Worklist) {
