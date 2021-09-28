2c53215e99cbd7b24768d9f0915aff9fedfcaa4a
David Green <david.green@arm.com>
2021-09-28 14:58:13 +0100

    [ARM] Skip debug info in recomputeVPTBlockMask
    The ARMLowOverheadLoops pass recalculates VPT block masks when it
    converts VCMP's inside VPT blocks into VPT's. The function to do so
    doesn't seem to handle debug info though, leading to invalid block
    creation or asserts at compile time. Make sure the function skips any
    debug info between the MVE instructions it inspects.
    
    Differential Revision: https://reviews.llvm.org/D110564

--- llvm/lib/Target/ARM/Thumb2InstrInfo.cpp
+++ llvm/lib/Target/ARM/Thumb2InstrInfo.cpp
@@ -792,8 +792,12 @@ void llvm::recomputeVPTBlockMask(MachineInstr &Instr) {
   MachineBasicBlock::iterator Iter = ++Instr.getIterator(),
                               End = Instr.getParent()->end();
 
+  while (Iter != End && Iter->isDebugInstr())
+    ++Iter;
+
   // Verify that the instruction after the VPT/VPST is predicated (it should
   // be), and skip it.
+  assert(Iter != End && "Expected some instructions in any VPT block");
   assert(
       getVPTInstrPredicate(*Iter) == ARMVCC::Then &&
       "VPT/VPST should be followed by an instruction with a 'then' predicate!");
@@ -802,6 +806,10 @@ void llvm::recomputeVPTBlockMask(MachineInstr &Instr) {
   // Iterate over the predicated instructions, updating the BlockMask as we go.
   ARM::PredBlockMask BlockMask = ARM::PredBlockMask::T;
   while (Iter != End) {
+    if (Iter->isDebugInstr()) {
+      ++Iter;
+      continue;
+    }
     ARMVCC::VPTCodes Pred = getVPTInstrPredicate(*Iter);
     if (Pred == ARMVCC::None)
       break;
