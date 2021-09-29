f9b68304a22ef82d827d282dd9014efc2e7ac875
Jay Foad <jay.foad@amd.com>
2021-09-29 11:42:04 +0100

    [AMDGPU] Enable machine verification after AMDGPUISelDAGToDAG
    This was introduced in D32628 but it does not seem to be required any
    more. At least it does not show any problems in check-llvm in an
    LLVM_ENABLE_EXPENSIVE_CHECKS build.
    
    Differential Revision: https://reviews.llvm.org/D110692

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -1020,8 +1020,7 @@ bool AMDGPUPassConfig::addPreISel() {
 }
 
 bool AMDGPUPassConfig::addInstSelector() {
-  // Defer the verifier until FinalizeISel.
-  addPass(createAMDGPUISelDag(&getAMDGPUTargetMachine(), getOptLevel()), false);
+  addPass(createAMDGPUISelDag(&getAMDGPUTargetMachine(), getOptLevel()));
   return false;
 }
 
