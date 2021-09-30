0c4a75f193a7eab8aff06e84bb4cd90e2af3c014
Petr Hosek <phosek@google.com>
2021-09-30 13:41:57 -0700

    [CMake] Remove the LLD LTO check for Darwin
    LLD now supports LTO on Darwin.
    
    Differential Revision: https://reviews.llvm.org/D110881

--- llvm/cmake/modules/HandleLLVMOptions.cmake
+++ llvm/cmake/modules/HandleLLVMOptions.cmake
@@ -167,9 +167,6 @@ else()
 endif()
 
 if(APPLE)
-  if(LLVM_ENABLE_LLD AND LLVM_ENABLE_LTO)
-    message(FATAL_ERROR "lld does not support LTO on Darwin")
-  endif()
   # Darwin-specific linker flags for loadable modules.
   set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -Wl,-flat_namespace -Wl,-undefined -Wl,suppress")
 endif()
