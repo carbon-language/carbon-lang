93a6b49d38af236a964492ac6de71d71e93a89a6
Alex Zinenko <zinenko@google.com>
2021-09-30 15:09:30 +0200

    [mlir][python] provide bindings for ops from the sparse_tensor dialect
    Previously, the dialect was exposed for linking and pass management purposes,
    but we did not generate op classes for it. Generate them.
    
    Reviewed By: nicolasvasilache
    
    Differential Revision: https://reviews.llvm.org/D110819

--- mlir/python/mlir/dialects/sparse_tensor.py
+++ mlir/python/mlir/dialects/sparse_tensor.py
@@ -2,5 +2,6 @@
 #  See https://llvm.org/LICENSE.txt for license information.
 #  SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
+from ._sparse_tensor_ops_gen import *
 from .._mlir_libs._mlir.dialects.sparse_tensor import *
 from .._mlir_libs import _mlirSparseTensorPasses as _cextSparseTensorPasses
