cf1f5fbdfcf516c6ee2617a21b004b575c9c521b
Jean Perier <jperier@nvidia.com>
2021-09-30 23:04:03 +0200

    [flang][NFC] Fix header comments in some runtime headers
    Differential Revision: https://reviews.llvm.org/D110850

--- flang/include/flang/Runtime/entry-names.h
+++ flang/include/flang/Runtime/entry-names.h
@@ -1,4 +1,4 @@
-/*===-- include/flang/Runtime/entry-names.h -------------------------*- C -*-===
+/*===-- include/flang/Runtime/entry-names.h -------------------------*- C -*-=//
  *
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
