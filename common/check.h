9e33e78627de779b53a6335cbe1ac772ff850401
Chandler Carruth <chandlerc@gmail.com>
2021-09-30 08:08:46 +0000

    Rework exiting system to have better backtraces.
    Previously, the program exit was triggered by the destructor.
    Unfortunately, C++ doesn't make it precisely clear where the destructor
    is run, and Clang doesn't generate reliable debug information for that
    to give good backtraces. Among other things, when combining separate
    cleanup regions in Clang there may be no single canonical location.
    
    Instead, move the `ExitingStream` system to use an explicit
    low-precedence operator overload to flush the output and exit. This
    ensures the stream and other actions are completed first but then
    *immediately* exits the program in a way that has a definitive source
    location and produces reliable backtraces.
    
    I've tried to add comments and helpers to make this as clear as possible
    given that it is a subtle and surprising issue.

--- common/check.h
+++ common/check.h
@@ -14,23 +14,6 @@ namespace Carbon {
 // Wraps a stream and exiting for fatal errors.
 class ExitingStream {
  public:
-  [[noreturn]] ~ExitingStream() {
-    // Finish with a newline.
-    llvm::errs() << "\n";
-    if (treat_as_bug) {
-      std::abort();
-    } else {
-      std::exit(-1);
-    }
-  }
-
-  // Indicates that initial input is in, so this is where a ": " should be added
-  // before user input.
-  ExitingStream& AddSeparator() {
-    separator = true;
-    return *this;
-  }
-
   // Indicates that the program is exiting due to a bug in the program, rather
   // than, e.g., invalid input.
   ExitingStream& TreatAsBug() {
@@ -53,6 +36,42 @@ class ExitingStream {
     return *this;
   }
 
+  // A tag type that triggers a special overload when streaming, see below.
+  struct AddSeparator {};
+
+  // When the `AddSeparator` type is streamed, some initial input is in and this
+  // is where a ": " should be added if the user streams any further
+  // information. Primarily used when building macros around these streams.
+  ExitingStream& operator<<(AddSeparator /*unused*/) {
+    separator = true;
+    return *this;
+  }
+
+  // Below are implementation details used by the macros in this header.
+
+  // Internal type used in macros to dispatch to the `operator&` overload below.
+  struct Helper {};
+
+  // Low-precedence binary operator overload used in macros below to flush the
+  // output and exit the program. We do this in a binary operator rather than
+  // the destructor to ensure good debug info and backtraces for errors.
+  [[noreturn]] friend auto operator|(Helper /*unused*/, ExitingStream& rhs) {
+    // Finish with a newline.
+    llvm::errs() << "\n";
+    if (rhs.treat_as_bug) {
+      std::abort();
+    } else {
+      std::exit(-1);
+    }
+  }
+
+  [[noreturn]] ~ExitingStream() {
+    llvm_unreachable(
+        "Exiting streams should only be constructed with the below macros that "
+        "ensure the special operator| exits the program prior to their "
+        "destruction!");
+  }
+
  private:
   // Whether a separator should be printed if << is used again.
   bool separator = false;
@@ -61,23 +80,30 @@ class ExitingStream {
   bool treat_as_bug = false;
 };
 
+// Raw exiting stream. This should be used when building other forms of exiting
+// macros like those below. It evaluates to a temporary `ExitingStream` object
+// that can be manipulated, streamed into, and then will exit the program.
+#define RAW_EXITING_STREAM() \
+  Carbon::ExitingStream::Helper() | Carbon::ExitingStream()
+
 // Checks the given condition, and if it's false, prints a stack, streams the
 // error message, then exits. This should be used for unexpected errors, such as
 // a bug in the application.
 //
 // For example:
 //   CHECK(is_valid) << "Data is not valid!";
-#define CHECK(condition)                                                      \
-  (!(condition)) && (Carbon::ExitingStream() << "CHECK failure: " #condition) \
-                        .AddSeparator()                                       \
-                        .TreatAsBug()
+#define CHECK(condition)                            \
+  (condition) ? (void)0                             \
+              : RAW_EXITING_STREAM().TreatAsBug()   \
+                    << "CHECK failure: " #condition \
+                    << Carbon::ExitingStream::AddSeparator()
 
 // This is similar to CHECK, but is unconditional. Writing FATAL() is clearer
 // than CHECK(false) because it avoids confusion about control flow.
 //
 // For example:
 //   FATAL() << "Unreachable!";
-#define FATAL() Carbon::ExitingStream().TreatAsBug() << "FATAL: "
+#define FATAL() RAW_EXITING_STREAM().TreatAsBug() << "FATAL: "
 
 }  // namespace Carbon
 
