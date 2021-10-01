ea76cb87ee4022d8663a7c25943478fe3f64e21a
Roman Lebedev <lebedev.ri@gmail.com>
2021-10-01 16:52:51 +0300

    [X86][Costmodel] Load/store i32/f32 Stride=2 VF=32 interleaving costs
    The only sched models that for cpu's that support avx2
    but not avx512 are: haswell, broadwell, skylake, zen1-3
    
    Here for `store` pattern we are starting to have spilling,
    so accurate modelling may be problematic,
    although if i drop the spilling, the measurements don't change.
    
    For load we have:
    https://godbolt.org/z/1oTTnncbx - for intels `Block RThroughput: =16.0`; for ryzens, `Block RThroughput: <=8.0`
    So pick cost of `16`.
    
    For store we have:
    https://godbolt.org/z/1oTTnncbx - for intels `Block RThroughput: =16.0`; for ryzens, `Block RThroughput: =8.0`
    So pick cost of `16`.
    
    I'm directly using the shuffling asm the llc produced,
    without any manual fixups that may be needed
    to ensure sequential execution.
    
    Reviewed By: RKSimon
    
    Differential Revision: https://reviews.llvm.org/D110761

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -5079,6 +5079,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {2, MVT::v4i32, 2}, // (load 8i32 and) deinterleave into 2 x 4i32
       {2, MVT::v8i32, 4}, // (load 16i32 and) deinterleave into 2 x 8i32
       {2, MVT::v16i32, 8}, // (load 32i32 and) deinterleave into 2 x 16i32
+      {2, MVT::v32i32, 16}, // (load 64i32 and) deinterleave into 2 x 32i32
 
       {2, MVT::v4i64, 6}, // (load 8i64 and) deinterleave into 2 x 4i64
 
@@ -5127,6 +5128,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {2, MVT::v4i32, 2}, // interleave 2 x 4i32 into 8i32 (and store)
       {2, MVT::v8i32, 4}, // interleave 2 x 8i32 into 16i32 (and store)
       {2, MVT::v16i32, 8}, // interleave 2 x 16i32 into 32i32 (and store)
+      {2, MVT::v32i32, 16}, // interleave 2 x 32i32 into 64i32 (and store)
 
       {2, MVT::v4i64, 6}, // interleave 2 x 4i64 into 8i64 (and store)
 
