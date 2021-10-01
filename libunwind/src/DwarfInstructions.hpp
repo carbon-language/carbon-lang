532783f9e1e65c7bd48b1592d2376e9dd47c5a73
Daniel Kiss <daniel.kiss@arm.com>
2021-10-01 16:49:37 +0200

    [libunwind] Fix cfi_register for float registers.
    Fixes D110144.
    registers.getFloatRegister is not const in ARM therefor can't be called here.
    
    Reviewed By: mstorsjo, #libunwind
    
    Differential Revision: https://reviews.llvm.org/D110731

--- libunwind/src/DwarfInstructions.hpp
+++ libunwind/src/DwarfInstructions.hpp
@@ -115,10 +115,12 @@ double DwarfInstructions<A, R>::getSavedFloatRegister(
     return addressSpace.getDouble(
         evaluateExpression((pint_t)savedReg.value, addressSpace,
                             registers, cfa));
-  case CFI_Parser<A>::kRegisterInRegister:
-    return registers.getFloatRegister((int)savedReg.value);
   case CFI_Parser<A>::kRegisterUndefined:
     return 0.0;
+  case CFI_Parser<A>::kRegisterInRegister:
+#ifndef _LIBUNWIND_TARGET_ARM
+    return registers.getFloatRegister((int)savedReg.value);
+#endif
   case CFI_Parser<A>::kRegisterIsExpression:
   case CFI_Parser<A>::kRegisterUnused:
   case CFI_Parser<A>::kRegisterOffsetFromCFA:
