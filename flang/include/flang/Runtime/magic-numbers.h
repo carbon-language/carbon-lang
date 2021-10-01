33c526416058cfd41a57a7ff73103127b319d6d1
peter klausler <pklausler@nvidia.com>
2021-10-01 12:55:33 -0700

    [flang][NFC] Fix first line of magic-numbers.h
    The first line of flang/include/flang/Runtime/magic-numbers.h
    got split into two somehow; join it back up.
    
    Differential Revision: https://reviews.llvm.org/D110965

--- flang/include/flang/Runtime/magic-numbers.h
+++ flang/include/flang/Runtime/magic-numbers.h
@@ -1,5 +1,4 @@
-#if 0 /*===-- include/flang/Runtime/magic-numbers.h \
-         -----------------------===*/
+#if 0 /*===-- include/flang/Runtime/magic-numbers.h -----------------------===*/
 /*
  * Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
  * See https://llvm.org/LICENSE.txt for license information.
