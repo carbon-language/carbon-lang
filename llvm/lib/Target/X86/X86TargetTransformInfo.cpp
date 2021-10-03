6fe4cce55816863bbb2ca9628d103dfa2d431616
Roman Lebedev <lebedev.ri@gmail.com>
2021-10-03 23:23:03 +0300

    [X86][Costmodel] Load/store i8 Stride=6 VF=4 interleaving costs
    The only sched models that for cpu's that support avx2
    but not avx512 are: haswell, broadwell, skylake, zen1-3
    
    For load we have:
    https://godbolt.org/z/4sWhs396o - for intels `Block RThroughput: =14.0`; for ryzens, `Block RThroughput: <=7.0`
    So pick cost of `14`.
    
    For store we have:
    https://godbolt.org/z/4sWhs396o - for intels `Block RThroughput: =9.0`; for ryzens, `Block RThroughput: <=3.0`
    So pick cost of `9`.
    
    I'm directly using the shuffling asm the llc produced,
    without any manual fixups that may be needed
    to ensure sequential execution.
    
    Reviewed By: RKSimon
    
    Differential Revision: https://reviews.llvm.org/D111010

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -5115,6 +5115,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {4, MVT::v32i16, 150}, // (load 128i16 and) deinterleave into 4 x 32i16
 
       {6, MVT::v2i8, 6}, // (load 12i8 and) deinterleave into 6 x 2i8
+      {6, MVT::v4i8, 14}, // (load 24i8 and) deinterleave into 6 x 4i8
 
       {6, MVT::v2i16, 13}, // (load 12i16 and) deinterleave into 6 x 2i16
       {6, MVT::v4i16, 9}, // (load 24i16 and) deinterleave into 6 x 4i16
@@ -5167,6 +5168,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {4, MVT::v32i16, 64},  // interleave 4 x 32i16 into 128i16 (and store)
 
       {6, MVT::v2i8, 7},  // interleave 6 x 2i8 into 12i8 (and store)
+      {6, MVT::v4i8, 9},  // interleave 6 x 4i8 into 24i8 (and store)
 
       {6, MVT::v2i16, 10},  // interleave 6 x 2i16 into 12i16 (and store)
       {6, MVT::v4i16, 15},  // interleave 6 x 4i16 into 24i16 (and store)
