7d91037fd2f71f1253bd8751a887eb4b6ed7d2ec
Roman Lebedev <lebedev.ri@gmail.com>
2021-10-04 19:30:14 +0300

    [X86][Costmodel] Load/store i32/f32 Stride=4 VF=16 interleaving costs
    This one required quite a bit of assembly surgery, but the trend continues, so i think this is right.
    
    The only sched models that for cpu's that support avx2
    but not avx512 are: haswell, broadwell, skylake, zen1-3
    
    For load we have:
    https://godbolt.org/z/EKWdj8cKT - for intels `Block RThroughput: <=32.0`; for ryzens, `Block RThroughput: <=24.0`
    So could pick cost of `32`.
    
    For store we have:
    https://godbolt.org/z/zj4bb9P75 - for intels `Block RThroughput: =32.0`; for ryzens, `Block RThroughput: <=16.0`
    So we could pick cost of `32`.
    
    I'm directly using the shuffling asm the llc produced,
    without any manual fixups that may be needed
    to ensure sequential execution.
    
    Reviewed By: RKSimon
    
    Differential Revision: https://reviews.llvm.org/D111064

--- llvm/lib/Target/X86/X86TargetTransformInfo.cpp
+++ llvm/lib/Target/X86/X86TargetTransformInfo.cpp
@@ -5131,6 +5131,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {4, MVT::v2i32, 4}, // (load 8i32 and) deinterleave into 4 x 2i32
       {4, MVT::v4i32, 8}, // (load 16i32 and) deinterleave into 4 x 4i32
       {4, MVT::v8i32, 16}, // (load 32i32 and) deinterleave into 4 x 8i32
+      {4, MVT::v16i32, 32}, // (load 64i32 and) deinterleave into 4 x 16i32
 
       {6, MVT::v2i8, 6}, // (load 12i8 and) deinterleave into 6 x 2i8
       {6, MVT::v4i8, 14}, // (load 24i8 and) deinterleave into 6 x 4i8
@@ -5207,6 +5208,7 @@ InstructionCost X86TTIImpl::getInterleavedMemoryOpCostAVX2(
       {4, MVT::v2i32, 5},  // interleave 4 x 2i32 into 8i32 (and store)
       {4, MVT::v4i32, 6},  // interleave 4 x 4i32 into 16i32 (and store)
       {4, MVT::v8i32, 16},  // interleave 4 x 8i32 into 32i32 (and store)
+      {4, MVT::v16i32, 32},  // interleave 4 x 16i32 into 64i32 (and store)
 
       {6, MVT::v2i8, 7},  // interleave 6 x 2i8 into 12i8 (and store)
       {6, MVT::v4i8, 9},  // interleave 6 x 4i8 into 24i8 (and store)
--- llvm/test/Analysis/CostModel/X86/interleaved-store-f32-stride-4.ll
+++ llvm/test/Analysis/CostModel/X86/interleaved-store-f32-stride-4.ll
@@ -27,7 +27,7 @@ target triple = "x86_64-unknown-linux-gnu"
 ; AVX2: LV: Found an estimated cost of 6 for VF 2 For instruction:   store float %v3, float* %out3, align 4
 ; AVX2: LV: Found an estimated cost of 8 for VF 4 For instruction:   store float %v3, float* %out3, align 4
 ; AVX2: LV: Found an estimated cost of 20 for VF 8 For instruction:   store float %v3, float* %out3, align 4
-; AVX2: LV: Found an estimated cost of 152 for VF 16 For instruction:   store float %v3, float* %out3, align 4
+; AVX2: LV: Found an estimated cost of 40 for VF 16 For instruction:   store float %v3, float* %out3, align 4
 ;
 ; AVX512: LV: Found an estimated cost of 1 for VF 1 For instruction:   store float %v3, float* %out3, align 4
 ; AVX512: LV: Found an estimated cost of 5 for VF 2 For instruction:   store float %v3, float* %out3, align 4
