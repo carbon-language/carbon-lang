259390de9af659e2432c5b2af041ec6576c1b26d
Arthur Eubanks <aeubanks@google.com>
2021-10-06 14:32:08 -0700

    [LCG] Don't skip invalidation of LazyCallGraph if CFG analyses are preserved
    The CFG being changed and the overall call graph are not related, we can introduce/remove calls without changing the CFG.
    
    Resolves one of the issues in PR51946.
    
    Reviewed By: asbirlea
    
    Differential Revision: https://reviews.llvm.org/D111275

--- llvm/lib/Analysis/LazyCallGraph.cpp
+++ llvm/lib/Analysis/LazyCallGraph.cpp
@@ -220,8 +220,7 @@ bool LazyCallGraph::invalidate(Module &, const PreservedAnalyses &PA,
   // Check whether the analysis, all analyses on functions, or the function's
   // CFG have been preserved.
   auto PAC = PA.getChecker<llvm::LazyCallGraphAnalysis>();
-  return !(PAC.preserved() || PAC.preservedSet<AllAnalysesOn<Module>>() ||
-           PAC.preservedSet<CFGAnalyses>());
+  return !(PAC.preserved() || PAC.preservedSet<AllAnalysesOn<Module>>());
 }
 
 LazyCallGraph &LazyCallGraph::operator=(LazyCallGraph &&G) {
