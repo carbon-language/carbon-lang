14cb138b15adafe7d4a1cd7cb2e836c40a5957b5
Cullen Rhodes <cullen.rhodes@arm.com>
2021-10-07 08:29:51 +0000

    [AArch64][SME] Update DUP (predicate) instruction
    Changes in architecture revision 00eac1:
      * Renamed to PSEL.
      * Copies whole source register.
      * Element type suffix removed from destination.
      * Element index no longer optional and '#' prefix has been removed.
    
    The reference can be found here:
    https://developer.arm.com/documentation/ddi0602/2021-09
    
    Depends on D111212.
    
    Reviewed By: kmclaughlin
    
    Differential Revision: https://reviews.llvm.org/D111213

--- llvm/test/MC/AArch64/SME/psel-diagnostics.s
+++ llvm/test/MC/AArch64/SME/psel-diagnostics.s
@@ -3,56 +3,44 @@
 // ------------------------------------------------------------------------- //
 // Invalid predicate
 
-// wrong predication qualifier, expected /z.
-dup p0.b, p0/m, p0.b[w12]
-// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid operand for instruction
-// CHECK-NEXT: dup p0.b, p0/m, p0.b[w12]
-// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
-
-// mismatched element type
-dup p0.b, p0/z, p0.h[w12]
-// CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid predicate register.
-// CHECK-NEXT: dup p0.b, p0/z, p0.h[w12]
-// CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
-
 // missing element type suffix
-dup p0.b, p0/z, p0[w12]
+psel p0, p0, p0[w12]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: invalid predicate register.
-// CHECK-NEXT: dup p0.b, p0/z, p0[w12]
+// CHECK-NEXT: psel p0, p0, p0[w12]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
 // ------------------------------------------------------------------------- //
 // Invalid index base register register (w12-w15)
 
-dup p0.b, p0/z, p0.b[w11]
+psel p0, p0, p0.b[w11]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: operand must be a register in range [w12, w15]
-// CHECK-NEXT: dup  p0.b, p0/z, p0.b[w11]
+// CHECK-NEXT: psel p0, p0, p0.b[w11]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
-dup p0.b, p0/z, p0.b[w16]
+psel p0, p0, p0.b[w16]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: operand must be a register in range [w12, w15]
-// CHECK-NEXT: dup  p0.b, p0/z, p0.b[w16]
+// CHECK-NEXT: psel p0, p0, p0.b[w16]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
 // --------------------------------------------------------------------------//
 // Invalid immediates
 
-dup p0.b, p0/z, p0.b[w12, #16]
+psel p0, p0, p0.b[w12, #16]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: immediate must be an integer in range [0, 15].
-// CHECK-NEXT: dup  p0.b, p0/z, p0.b[w12, #16]
+// CHECK-NEXT: psel p0, p0, p0.b[w12, #16]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
-dup p0.h, p0/z, p0.h[w12, #8]
+psel p0, p0, p0.h[w12, #8]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: immediate must be an integer in range [0, 7].
-// CHECK-NEXT: dup  p0.h, p0/z, p0.h[w12, #8]
+// CHECK-NEXT: psel p0, p0, p0.h[w12, #8]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
-dup p0.s, p0/z, p0.s[w12, #4]
+psel p0, p0, p0.s[w12, #4]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: immediate must be an integer in range [0, 3].
-// CHECK-NEXT: dup  p0.s, p0/z, p0.s[w12, #4]
+// CHECK-NEXT: psel p0, p0, p0.s[w12, #4]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
 
-dup p0.d, p0/z, p0.d[w12, #2]
+psel p0, p0, p0.d[w12, #2]
 // CHECK: [[@LINE-1]]:{{[0-9]+}}: error: immediate must be an integer in range [0, 1].
-// CHECK-NEXT: dup  p0.d, p0/z, p0.d[w12, #2]
+// CHECK-NEXT: psel  p0, p0, p0.d[w12, #2]
 // CHECK-NOT: [[@LINE-1]]:{{[0-9]+}}:
