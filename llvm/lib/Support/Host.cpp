a706a5ef225125e4afd37c7709cf697514cd2187
Andreas Schwab <schwab@suse.de>
2021-10-08 10:08:39 -0700

    [Support] Define sys::getHostCPUName for RISC-V
    The RISCV target doesn't define a "generic" cpu, only "generic-rv32" and
    "generic-rv64".  Define sys::getHostCPUName for RISC-V that returns the
    correct cpu for the host.
    
    Reviewed By: craig.topper, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D105274

--- llvm/lib/Support/Host.cpp
+++ llvm/lib/Support/Host.cpp
@@ -1356,6 +1356,16 @@ StringRef sys::getHostCPUName() {
     return "generic";
   }
 }
+#elif defined(__riscv)
+StringRef sys::getHostCPUName() {
+#if __riscv_xlen == 64
+  return "generic-rv64";
+#elif __riscv_xlen == 32
+  return "generic-rv32";
+#else
+#error "Unhandled value of __riscv_xlen"
+#endif
+}
 #else
 StringRef sys::getHostCPUName() { return "generic"; }
 namespace llvm {
