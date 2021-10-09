ea12adc1699f53d7d299d3b216a97e7da88c534b
Nikita Popov <nikita.ppv@gmail.com>
2021-10-09 16:53:07 +0200

    [CanonicalizeFreeze] Drop IVUsers.h include (NFC)
    Looking for users of IVUsers, this was a false positive. Only LSR
    uses IVUsers.

--- llvm/lib/Transforms/Utils/CanonicalizeFreezeInLoops.cpp
+++ llvm/lib/Transforms/Utils/CanonicalizeFreezeInLoops.cpp
@@ -33,7 +33,6 @@
 #include "llvm/ADT/STLExtras.h"
 #include "llvm/ADT/SmallVector.h"
 #include "llvm/Analysis/IVDescriptors.h"
-#include "llvm/Analysis/IVUsers.h"
 #include "llvm/Analysis/LoopAnalysisManager.h"
 #include "llvm/Analysis/LoopInfo.h"
 #include "llvm/Analysis/LoopPass.h"
