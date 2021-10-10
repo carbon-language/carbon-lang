c26e53e129085b64f66f0b4cbc5fd4bfdf6575e6
Med Ismail Bennani <medismail.bennani@gmail.com>
2021-10-10 03:28:36 +0200

    [lldb/test] Disable 'TestScriptedProcess.py' on macOS
    This is disabling 'TestScriptedProcess.py' on macOS since it fails on
    Green Dragon: https://green.lab.llvm.org/green/view/LLDB/job/lldb-cmake/35974
    
    Signed-off-by: Med Ismail Bennani <medismail.bennani@gmail.com>

--- lldb/test/API/functionalities/scripted_process/TestScriptedProcess.py
+++ lldb/test/API/functionalities/scripted_process/TestScriptedProcess.py
@@ -88,6 +88,7 @@ class ScriptedProcesTestCase(TestBase):
         for idx, reg in enumerate(registers, start=1):
             self.assertEqual(idx, int(reg.value, 16))
 
+    @skipIfDarwin
     @skipUnlessDarwin
     def test_launch_scripted_process_stack_frames(self):
         """Test that we can launch an lldb scripted process from the command
@@ -113,11 +114,6 @@ class ScriptedProcesTestCase(TestBase):
         self.assertEqual(process.GetProcessID(), 42)
         self.assertEqual(process.GetNumThreads(), 1)
 
-        error = lldb.SBError()
-        hello_world = "Hello, world!"
-        memory_read = process.ReadCStringFromMemory(0x50000000000,
-                                                    len(hello_world) + 1, # NULL byte
-                                                    error)
         thread = process.GetSelectedThread()
         self.assertTrue(thread, "Invalid thread.")
         self.assertEqual(thread.GetThreadID(), 0x19)
