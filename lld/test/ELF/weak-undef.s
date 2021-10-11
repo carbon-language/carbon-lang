aaeba6483fdafa442b950427bfa3e6443034f78d
Ben Dunbobbin <Ben.Dunbobbin@sony.com>
2021-10-11 13:10:52 +0100

    [LLD] [TEST] Add test case for patching an absolute relocation to a weak undef
    I noticed that we had this case in our internal testsuite but couldn't find it in LLD's tests.
    
    This adds that case.
    
    Differential Revision: https://reviews.llvm.org/D110716

--- lld/test/ELF/weak-undef.s
+++ lld/test/ELF/weak-undef.s
@@ -1,33 +1,28 @@
 # REQUIRES: x86
 # RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o %t.o
 # RUN: ld.lld %t.o -o %t --export-dynamic
-# RUN: llvm-readobj --dyn-syms %t | FileCheck %s
+# RUN: llvm-readelf -r --dyn-syms --hex-dump=.data %t | \
+# RUN:   FileCheck %s --check-prefixes=NORELOC,COMMON
+
+# NORELOC: There are no relocations in this file.
 
 # RUN: llvm-mc -filetype=obj -triple=x86_64 %p/Inputs/dummy-shared.s -o %t1.o
 # RUN: ld.lld %t1.o -shared -o %t1.so
 # RUN: ld.lld %t.o -o %t %t1.so -pie
-# RUN: llvm-readobj --dyn-syms %t | FileCheck %s
+# RUN: llvm-readelf -r --dyn-syms --hex-dump=.data %t | \
+# RUN:   FileCheck %s --check-prefixes=RELOC,COMMON
 
-# CHECK:      DynamicSymbols [
-# CHECK-NEXT:   Symbol {
-# CHECK-NEXT:     Name:
-# CHECK-NEXT:     Value: 0x0
-# CHECK-NEXT:     Size: 0
-# CHECK-NEXT:     Binding: Local (0x0)
-# CHECK-NEXT:     Type: None (0x0)
-# CHECK-NEXT:     Other: 0
-# CHECK-NEXT:     Section: Undefined (0x0)
-# CHECK-NEXT:   }
-# CHECK-NEXT:   Symbol {
-# CHECK-NEXT:     Name: foo
-# CHECK-NEXT:     Value: 0x0
-# CHECK-NEXT:     Size: 0
-# CHECK-NEXT:     Binding: Weak
-# CHECK-NEXT:     Type: None
-# CHECK-NEXT:     Other: 0
-# CHECK-NEXT:     Section: Undefined
-# CHECK-NEXT:   }
-# CHECK-NEXT: ]
+# RELOC:      Relocation section '.rela.dyn' at offset {{.*}} contains 1 entries:
+# RELOC-NEXT: Offset Info Type Symbol's Value Symbol's Name + Addend
+# RELOC-NEXT: {{.*}} 0000000100000001 R_X86_64_64 0000000000000000 foo + 0
+
+# COMMON:       Symbol table '.dynsym' contains 2 entries:
+# COMMON-NEXT:  Num: Value Size Type Bind Vis Ndx Name
+# COMMON-NEXT:  0: 0000000000000000 0 NOTYPE LOCAL DEFAULT UND
+# COMMON-NEXT:  1: 0000000000000000 0 NOTYPE WEAK DEFAULT UND foo
+# COMMON:      Hex dump of section '.data':
+# COMMON-NEXT: {{.*}} 00000000 00000000 
+# COMMON-EMPTY:
 
 .weak foo
 
