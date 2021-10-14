fa639eda6535732a5fd79c3f551d5a667b810963
Pavel Labath <pavel@labath.sk>
2021-10-14 10:38:00 +0200

    [lldb] Fix TestStackCorefile.py for ca0ce99fc8

--- lldb/test/API/macosx/stack-corefile/main.c
+++ lldb/test/API/macosx/stack-corefile/main.c
@@ -6,8 +6,7 @@ int main() {
   int *heap_int = (int*) malloc(sizeof (int));
   *heap_int = 10;
 
-  char stack_str[80];
-  strcpy (stack_str, "stack string");
+  char stack_str[] = "stack string";
   char *heap_str = (char*) malloc(80);
   strcpy (heap_str, "heap string");
 
