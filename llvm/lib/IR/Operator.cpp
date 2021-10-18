b9ca73e1a8fd0c018b0b3eb313163da2b4ca4e09
Stephen Tozer <stephen.tozer@sony.com>
2021-10-18 11:56:06 +0100

    [DebugInfo] Correctly handle arrays with 0-width elements in GEP salvaging
    Fixes an issue where GEP salvaging did not properly account for GEP
    instructions which stepped over array elements of width 0 (effectively a
    no-op). This unnecessarily produced long expressions by appending
    `... + (x * 0)` and potentially extended the number of SSA values used
    in the dbg.value. This also erroneously triggered an assert in the
    salvage function that the element width would be strictly positive.
    These issues are resolved by simply ignoring these useless operands.
    
    Reviewed By: aprantl
    
    Differential Revision: https://reviews.llvm.org/D111809

--- llvm/lib/IR/Operator.cpp
+++ llvm/lib/IR/Operator.cpp
@@ -190,12 +190,14 @@ bool GEPOperator::collectOffset(
 
     if (STy || ScalableType)
       return false;
-    // Insert an initial offset of 0 for V iff none exists already, then
-    // increment the offset by IndexedSize.
-    VariableOffsets.insert({V, APInt(BitWidth, 0)});
     APInt IndexedSize =
         APInt(BitWidth, DL.getTypeAllocSize(GTI.getIndexedType()));
-    VariableOffsets[V] += IndexedSize;
+    // Insert an initial offset of 0 for V iff none exists already, then
+    // increment the offset by IndexedSize.
+    if (!IndexedSize.isZero()) {
+      VariableOffsets.insert({V, APInt(BitWidth, 0)});
+      VariableOffsets[V] += IndexedSize;
+    }
   }
   return true;
 }
