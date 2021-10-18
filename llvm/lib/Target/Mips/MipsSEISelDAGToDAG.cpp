f5755c0849a56543ef6938352784fdd7b4596e4c
Jessica Clarke <jrtc27@jrtc27.com>
2021-10-18 15:10:17 +0100

    [Mips] Add glue between CopyFromReg, CopyToReg and RDHWR nodes for TLS
    The MIPS ABI requires the thread pointer be accessed via rdhwr $3, $r29.
    This is currently represented by (CopyToReg $3, (RDHWR $29)) followed by
    a (CopyFromReg $3). However, there is no glue between these, meaning
    scheduling can break those apart. In particular, PR51691 is a report
    where PseudoSELECT_I was moved to between the CopyToReg and CopyFromReg,
    and since its expansion uses branches, it split the def and use of the
    physical register between two basic blocks, resulting in the def being
    eliminated and the use having no def. It also seems possible that a
    similar situation could arise splitting up the CopyToReg from the RDHWR,
    causing the RDHWR to use a destination register other than $3, violating
    the ABI requirement.
    
    Thus, add glue between all three nodes to ensure they aren't split up
    during instruction selection. No regression test is added since any test
    would be implictly relying on specific scheduling behaviour, so whilst
    it might be testing that glue is preventing reordering today, changes to
    scheduling behaviour could result in the test no longer being able to
    catch a regression here, as the reordering might no longer happen for
    other unrelated reasons.
    
    Fixes PR51691.
    
    Reviewed By: atanasyan, dim
    
    Differential Revision: https://reviews.llvm.org/D111967

--- llvm/lib/Target/Mips/MipsSEISelDAGToDAG.cpp
+++ llvm/lib/Target/Mips/MipsSEISelDAGToDAG.cpp
@@ -1027,12 +1027,13 @@ bool MipsSEDAGToDAGISel::trySelect(SDNode *Node) {
     }
 
     SDNode *Rdhwr =
-        CurDAG->getMachineNode(RdhwrOpc, DL, Node->getValueType(0),
+        CurDAG->getMachineNode(RdhwrOpc, DL, Node->getValueType(0), MVT::Glue,
                                CurDAG->getRegister(Mips::HWR29, MVT::i32),
                                CurDAG->getTargetConstant(0, DL, MVT::i32));
     SDValue Chain = CurDAG->getCopyToReg(CurDAG->getEntryNode(), DL, DestReg,
-                                         SDValue(Rdhwr, 0));
-    SDValue ResNode = CurDAG->getCopyFromReg(Chain, DL, DestReg, PtrVT);
+                                         SDValue(Rdhwr, 0), SDValue(Rdhwr, 1));
+    SDValue ResNode = CurDAG->getCopyFromReg(Chain, DL, DestReg, PtrVT,
+                                             Chain.getValue(1));
     ReplaceNode(Node, ResNode.getNode());
     return true;
   }
