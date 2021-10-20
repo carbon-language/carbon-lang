b8da594750762f811283820c19b02cedfb6632d4
Ben Langmuir <blangmuir@apple.com>
2021-10-20 10:37:32 -0700

    Reapply [ORC-RT] Configure the ORC runtime for more architectures and platforms
    Reapply 5692ed0cce8c95, but with the ORC runtime disabled explicitly on
    CrossWinToARMLinux to match the other compiler-rt runtime libraries.
    
    Differential Revision: https://reviews.llvm.org/D112229
    
    ---
    
    Enable building the ORC runtime for 64-bit and 32-bit ARM architectures,
    and for all Darwin embedded platforms (iOS, tvOS, and watchOS). This
    covers building the cross-platform code, but does not add TLV runtime
    support for the new architectures, which can be added independently.
    
    Incidentally, stop building the Mach-O TLS support file unnecessarily on
    other platforms.
    
    Differential Revision: https://reviews.llvm.org/D112111

--- compiler-rt/lib/orc/simple_packed_serialization.h
+++ compiler-rt/lib/orc/simple_packed_serialization.h
@@ -396,10 +396,12 @@ public:
     uint64_t Size;
     if (!SPSArgList<uint64_t>::deserialize(IB, Size))
       return false;
+    if (Size > std::numeric_limits<size_t>::max())
+      return false;
     Data = IB.data();
     if (!IB.skip(Size))
       return false;
-    S = {Data, Size};
+    S = {Data, static_cast<size_t>(Size)};
     return true;
   }
 };
