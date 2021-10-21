fdb66eda77a00d4ff0cb2730290f0fadf7840770
Arthur Eubanks <aeubanks@google.com>
2021-10-21 11:14:29 -0700

    [test] Make sure plugin actually runs in clear-ast-before-backend-plugins.c
    Reviewed By: dblaikie
    
    Differential Revision: https://reviews.llvm.org/D112253

--- clang/test/Misc/clear-ast-before-backend-plugins.c
+++ clang/test/Misc/clear-ast-before-backend-plugins.c
@@ -5,5 +5,6 @@
 
 // RUN: %clang_cc1 -mllvm -debug-only=codegenaction -clear-ast-before-backend -emit-obj -o /dev/null -load %llvmshlibdir/PrintFunctionNames%pluginext -add-plugin print-fns -plugin-arg-print-fns help %s 2>&1 | FileCheck %s --check-prefix=NO
 // NO-NOT: Clearing AST
+// NO: top-level-decl: "f"
 
 void f() {}
