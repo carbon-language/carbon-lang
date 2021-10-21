a5435844f0e67c70963a1a743fef41b1a8eac4d3
Yaxun (Sam) Liu <yaxun.liu@amd.com>
2021-10-21 12:59:49 -0400

    [HIP][OpenMP] Fix assertion in deferred diag
    Fix assertion in UsedDeclVisitor where clang is trying to look up a destructor
    for a forward declared class.
    
    Fixes: https://bugs.llvm.org/show_bug.cgi?id=52250
    
    Reviewed by: Artem Belevich, John McCall
    
    Differential Revision: https://reviews.llvm.org/D112235

--- clang/test/OpenMP/deferred-diags.cpp
+++ clang/test/OpenMP/deferred-diags.cpp
@@ -6,8 +6,6 @@
 // RUN:            -verify-ignore-unexpected=note \
 // RUN:            -fopenmp -o - %s
 
-// expected-no-diagnostics
-
 // Test no infinite recursion in DeferredDiagnosticEmitter.
 constexpr int foo(int *x) {
   return 0;
@@ -37,3 +35,14 @@ public:
      }
   }
 };
+
+// Test that deleting an incomplete class type doesn't cause an assertion.
+namespace TestDeleteIncompleteClassDefinition {
+struct a;
+struct b {
+  b() {
+    delete c; // expected-warning {{deleting pointer to incomplete type 'TestDeleteIncompleteClassDefinition::a' may cause undefined behavior}}
+  }
+  a *c;
+};
+} // namespace TestDeleteIncompleteClassDefinition
