2915889d74b124d3105d1e7583fbfb1cf1bad10d
Jay Foad <jay.foad@amd.com>
2021-10-22 12:21:57 +0100

    [ScheduleDAGInstrs] Call adjustSchedDependency in more cases
    This removes a condition and the corresponding FIXME comment, because
    the Hexagon assertion it refers to has apparently been fixed, probably
    by D76134.
    
    NFCI. This just gives targets the opportunity to adjust latencies that
    were set to 0 by the generic code because they involve "implicit pseudo"
    operands.
    
    Differential Revision: https://reviews.llvm.org/D112306

--- llvm/lib/CodeGen/ScheduleDAGInstrs.cpp
+++ llvm/lib/CodeGen/ScheduleDAGInstrs.cpp
@@ -271,15 +271,10 @@ void ScheduleDAGInstrs::addPhysRegDataDeps(SUnit *SU, unsigned OperIdx) {
       if (!ImplicitPseudoDef && !ImplicitPseudoUse) {
         Dep.setLatency(SchedModel.computeOperandLatency(SU->getInstr(), OperIdx,
                                                         RegUse, UseOp));
-        ST.adjustSchedDependency(SU, OperIdx, UseSU, UseOp, Dep);
       } else {
         Dep.setLatency(0);
-        // FIXME: We could always let target to adjustSchedDependency(), and
-        // remove this condition, but that currently asserts in Hexagon BE.
-        if (SU->getInstr()->isBundle() || (RegUse && RegUse->isBundle()))
-          ST.adjustSchedDependency(SU, OperIdx, UseSU, UseOp, Dep);
       }
-
+      ST.adjustSchedDependency(SU, OperIdx, UseSU, UseOp, Dep);
       UseSU->addPred(Dep);
     }
   }
