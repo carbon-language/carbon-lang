9c7ace60aa629ecb7bc836802a7f4e33ee1045f1
Ben Shi <powerman1st@163.com>
2021-10-26 08:39:51 +0000

    [CUDA][NFC] Fix inexact warning message about lambdas
    Reviewed By: tra
    
    Differential Revision: https://reviews.llvm.org/D112521

--- clang/test/Parser/lambda-attr.cu
+++ clang/test/Parser/lambda-attr.cu
@@ -6,36 +6,36 @@ __attribute__((host, device)) void hd_fn() {}
 
 __attribute__((device)) void device_attr() {
   ([]() __attribute__((device)) { device_fn(); })();
-  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([] __attribute__((device)) () { device_fn(); })();
   ([] __attribute__((device)) { device_fn(); })();
 
   ([&]() __attribute__((device)){ device_fn(); })();
-  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([&] __attribute__((device)) () { device_fn(); })();
   ([&] __attribute__((device)) { device_fn(); })();
 
   ([&](int) __attribute__((device)){ device_fn(); })(0);
-  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([&] __attribute__((device)) (int) { device_fn(); })(0);
 }
 
 __attribute__((host)) __attribute__((device)) void host_device_attrs() {
   ([]() __attribute__((host)) __attribute__((device)){ hd_fn(); })();
-  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after '()' in lambdas}}
-  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after the parameter list in lambdas}}
+  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([] __attribute__((host)) __attribute__((device)) () { hd_fn(); })();
   ([] __attribute__((host)) __attribute__((device)) { hd_fn(); })();
 
   ([&]() __attribute__((host)) __attribute__((device)){ hd_fn(); })();
-  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after '()' in lambdas}}
-  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after the parameter list in lambdas}}
+  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([&] __attribute__((host)) __attribute__((device)) () { hd_fn(); })();
   ([&] __attribute__((host)) __attribute__((device)) { hd_fn(); })();
 
   ([&](int) __attribute__((host)) __attribute__((device)){ hd_fn(); })(0);
-  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after '()' in lambdas}}
-  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after '()' in lambdas}}
+  // expected-warning@-1 {{nvcc does not allow '__host__' to appear after the parameter list in lambdas}}
+  // expected-warning@-2 {{nvcc does not allow '__device__' to appear after the parameter list in lambdas}}
   ([&] __attribute__((host)) __attribute__((device)) (int) { hd_fn(); })(0);
 }
 
