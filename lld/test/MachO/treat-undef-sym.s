d3ddd569eb78ae4e042502775704bd27c0c0a543
Jez Ng <jezng@fb.com>
2021-10-25 20:50:35 -0400

    [lld-macho] Fix incremental builds

--- lld/test/MachO/treat-undef-sym.s
+++ lld/test/MachO/treat-undef-sym.s
@@ -1,5 +1,5 @@
 # REQUIRES: x86
-# RUN: split-file %s %t
+# RUN: rm -rf %s; split-file %s %t
 
 # RUN: llvm-mc -filetype=obj -triple=x86_64-apple-macos %s -o %t/live.o
 # RUN: not %lld -undefined bogus -o /dev/null %t/live.o 2>&1 | \
