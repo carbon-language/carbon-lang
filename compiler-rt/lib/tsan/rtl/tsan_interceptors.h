d31b2dc235f1b1eff27c553012520792b7a0eb47
Dmitry Vyukov <dvyukov@google.com>
2021-10-28 13:00:09 +0200

    tsan: add debugging code for ptrace test failures
    Debugging of crashes on powerpc after commit:
    c80604f7a3 ("tsan: remove real func check from interceptors")
    Somehow replacing if with DCHECK leads to strange failures in:
    SanitizerCommon-tsan-powerpc64le-Linux :: Linux/ptrace.cpp
    https://lab.llvm.org/buildbot/#/builders/105
    https://lab.llvm.org/buildbot/#/builders/121
    https://lab.llvm.org/buildbot/#/builders/57
    
    The hypothesis is that something writes out-of-bounds
    into pt_regs on stack and that corrupts internal tsan state.
    
    Reviewed By: melver
    
    Differential Revision: https://reviews.llvm.org/D112703

--- compiler-rt/lib/tsan/rtl/tsan_interceptors.h
+++ compiler-rt/lib/tsan/rtl/tsan_interceptors.h
@@ -43,9 +43,26 @@ inline bool in_symbolizer() {
   ScopedInterceptor si(thr, #func, GET_CALLER_PC()); \
   UNUSED const uptr pc = GET_CURRENT_PC();
 
+#ifdef __powerpc64__
+// Debugging of crashes on powerpc after commit:
+// c80604f7a3 ("tsan: remove real func check from interceptors")
+// Somehow replacing if with DCHECK leads to strange failures in:
+// SanitizerCommon-tsan-powerpc64le-Linux :: Linux/ptrace.cpp
+// https://lab.llvm.org/buildbot/#/builders/105
+// https://lab.llvm.org/buildbot/#/builders/121
+// https://lab.llvm.org/buildbot/#/builders/57
+#  define CHECK_REAL_FUNC(func)                                          \
+    if (REAL(func) == 0) {                                               \
+      Report("FATAL: ThreadSanitizer: failed to intercept %s\n", #func); \
+      Die();                                                             \
+    }
+#else
+#  define CHECK_REAL_FUNC(func) DCHECK(REAL(func))
+#endif
+
 #define SCOPED_TSAN_INTERCEPTOR(func, ...)                                \
   SCOPED_INTERCEPTOR_RAW(func, __VA_ARGS__);                              \
-  DCHECK(REAL(func));                                                     \
+  CHECK_REAL_FUNC(func);                                                  \
   if (!thr->is_inited || thr->ignore_interceptors || thr->in_ignored_lib) \
     return REAL(func)(__VA_ARGS__);
 
