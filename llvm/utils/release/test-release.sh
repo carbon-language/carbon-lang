26a1830e39baf0f8844cda0ba6be72eb8a66981d
Louis Dionne <ldionne.2@gmail.com>
2021-10-28 11:53:48 -0400

    [release] Use the Bootstrapping build for building LLVM releases
    Instead of using the (now deprecated) Projects build for libcxx, libcxxabi,
    libunwind and compiler-rt, this patch uses the Bootstrapping build. This
    implies that Clang will be built from scratch, and then the runtimes will
    be built using that just-built Clang instead of the system compiler.
    This is the correct way of assembling a toolchain, since we don't want
    to ship runtimes that were built with a non-Clang compiler (or a
    potentially older Clang).
    
    Differential Revision: https://reviews.llvm.org/D112748

--- llvm/utils/release/test-release.sh
+++ llvm/utils/release/test-release.sh
@@ -236,16 +236,17 @@ fi
 
 # Projects list
 projects="llvm clang clang-tools-extra"
+runtimes=""
 if [ $do_rt = "yes" ]; then
-  projects="$projects compiler-rt"
+  runtimes="$runtimes compiler-rt"
 fi
 if [ $do_libs = "yes" ]; then
-  projects="$projects libcxx"
+  runtimes="$runtimes libcxx"
   if [ $do_libcxxabi = "yes" ]; then
-    projects="$projects libcxxabi"
+    runtimes="$runtimes libcxxabi"
   fi
   if [ $do_libunwind = "yes" ]; then
-    projects="$projects libunwind"
+    runtimes="$runtimes libunwind"
   fi
 fi
 case $do_test_suite in
@@ -377,6 +378,7 @@ function configure_llvmCore() {
     esac
 
     project_list=${projects// /;}
+    runtime_list=${runtimes// /;}
     echo "# Using C compiler: $c_compiler"
     echo "# Using C++ compiler: $cxx_compiler"
 
@@ -389,6 +391,7 @@ function configure_llvmCore() {
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
         -DLLVM_ENABLE_PROJECTS="$project_list" \
         -DLLVM_LIT_ARGS="-j $NumJobs" \
+        -DLLVM_ENABLE_RUNTIMES="$runtime_list" \
         $ExtraConfigureFlags $BuildDir/llvm-project/llvm \
         2>&1 | tee $LogDir/llvm.configure-Phase$Phase-$Flavor.log
     env CC="$c_compiler" CXX="$cxx_compiler" \
@@ -397,6 +400,7 @@ function configure_llvmCore() {
         -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
         -DLLVM_ENABLE_PROJECTS="$project_list" \
         -DLLVM_LIT_ARGS="-j $NumJobs" \
+        -DLLVM_ENABLE_RUNTIMES="$runtime_list" \
         $ExtraConfigureFlags $BuildDir/llvm-project/llvm \
         2>&1 | tee $LogDir/llvm.configure-Phase$Phase-$Flavor.log
 
