7ee5e7e97c1c9365c083f8ae456b5eda3c5540c0
Mark de Wever <koraq@xs4all.nl>
2021-10-28 17:41:35 +0200

    [libc++] Remove Clang-11 support.
    Since we no longer officially support Clang 11 remove the work-arounds
    for this version.
    
    Reviewed By: #libc, ldionne
    
    Differential Revision: https://reviews.llvm.org/D112727

--- libcxx/test/std/utilities/format/format.formatter/format.context/format.formatter.spec/formatter.const_char_array.pass.cpp
+++ libcxx/test/std/utilities/format/format.formatter/format.context/format.formatter.spec/formatter.const_char_array.pass.cpp
@@ -8,7 +8,6 @@
 // UNSUPPORTED: c++03, c++11, c++14, c++17
 // UNSUPPORTED: libcpp-no-concepts
 // UNSUPPORTED: libcpp-has-no-incomplete-format
-// UNSUPPORTED: clang-11
 // TODO FMT Evaluate gcc-11 status
 // UNSUPPORTED: gcc-11
 // UNSUPPORTED: apple-clang-12
