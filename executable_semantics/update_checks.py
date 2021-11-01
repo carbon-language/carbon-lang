3627bb38fa451f40ce9560d2b5eccfaf873d9197
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-11-01 21:47:24 +0000

    Test --trace directly

--- executable_semantics/update_checks.py
+++ executable_semantics/update_checks.py
@@ -12,16 +12,16 @@ from concurrent import futures
 import os
 import subprocess
 import sys
+from typing import Set
 
 _BINDIR = "./bazel-bin/executable_semantics"
 _TESTDATA = "executable_semantics/testdata"
 
-# TODO: Right now this is a static string used. In theory maybe we should use
-# the command; it's included for that flexibility.
-_AUTOUPDATE_MARKER = "// AUTOUPDATE: executable_semantics %s\n"
+# A prefix followed by a command to run for autoupdating checked output.
+_AUTOUPDATE_MARKER = "// AUTOUPDATE: "
 
 
-def _get_tests():
+def _get_tests() -> Set[str]:
     """Get the list of tests from the filesystem."""
     tests = set()
     for root, _, files in os.walk(_TESTDATA):
@@ -36,18 +36,39 @@ def _get_tests():
     return tests
 
 
-def _update_check(test):
-    """Updates the CHECK: lines for `test` by running executable_semantics."""
+def _update_check_once(test: str) -> bool:
+    """Updates the CHECK: lines for `test` by running executable_semantics.
+
+    Returns True if the number of lines changes.
+    """
     with open(test) as f:
         orig_lines = f.readlines()
-    if _AUTOUPDATE_MARKER not in orig_lines:
+
+    # Remove old OUT.
+    lines_without_check = [
+        x for x in orig_lines if not x.startswith("// CHECK")
+    ]
+    num_orig_check_lines = len(orig_lines) - len(lines_without_check)
+    autoupdate_index = None
+    for line_index, line in enumerate(lines_without_check):
+        if line.startswith(_AUTOUPDATE_MARKER):
+            autoupdate_index = line_index
+            autoupdate_cmd = line[len(_AUTOUPDATE_MARKER) :]
+    if autoupdate_index is None:
         raise ValueError("No autoupdate marker in %s" % test)
-    # Run executable_semantics to general output.
+
+    # Add executable_semantics to the PATH.
+    env = os.environ.copy()
+    env["PATH"] = "%s:%s" % (os.path.abspath(_BINDIR), env["PATH"])
+
+    # Run the autoupdate command to generate output.
     # (`bazel run` would serialize)
     p = subprocess.run(
-        ["%s/executable_semantics" % _BINDIR, test],
+        autoupdate_cmd % test,
+        shell=True,
         stdout=subprocess.PIPE,
         stderr=subprocess.STDOUT,
+        env=env,
     )
     out = p.stdout.decode("utf-8")
 
@@ -56,22 +77,33 @@ def _update_check(test):
     # TODO: Maybe revisit and see if lit can be convinced to give a
     # root-relative path.
     out = out.replace(test, "{{.*}}/%s" % test)
+    out_lines = out.splitlines()
 
-    # Remove old OUT.
-    lines_without_check = [
-        x for x in orig_lines if not x.startswith("// CHECK:")
-    ]
-    autoupdate_index = lines_without_check.index(_AUTOUPDATE_MARKER)
-    assert autoupdate_index >= 0
+    # Interleave the new CHECK: lines with the tested content.
     with open(test, "w") as f:
         f.writelines(lines_without_check[: autoupdate_index + 1])
-        f.writelines(["// CHECK: %s\n" % x for x in out.splitlines()])
+        for line in out_lines:
+            if line:
+                f.write("// CHECK: %s\n" % line)
+            else:
+                f.write("// CHECK-EMPTY:\n")
         f.writelines(lines_without_check[autoupdate_index + 1 :])
 
+    # Compares the number of CHECK: lines originally with the number added.
+    return num_orig_check_lines != len(out_lines)
+
+
+def _update_check(test: str) -> None:
+    """Wraps CHECK: updates for test files."""
+    if _update_check_once(test):
+        # If the number of output lines changes, run again because output can be
+        # line-specific. However, output should stabilize quickly.
+        if _update_check_once(test):
+            raise ValueError("The output of %s kept changing" % test)
     print(".", end="", flush=True)
 
 
-def _update_checks():
+def _update_checks() -> None:
     """Runs bazel to update CHECK: lines in lit tests."""
     # TODO: It may be helpful if a list of tests can be passed in args; would
     # want to use argparse for this.
@@ -92,7 +124,7 @@ def _update_checks():
     print("\nUpdated lit tests.")
 
 
-def main():
+def main() -> None:
     # Go to the repository root so that paths will match bazel's view.
     os.chdir(os.path.join(os.path.dirname(__file__), ".."))
 
