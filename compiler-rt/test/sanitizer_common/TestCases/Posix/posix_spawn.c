9213202abd275c26c51cc46e2a34c678051bd179
Vitaly Buka <vitalybuka@google.com>
2021-11-01 21:02:13 -0700

    [sanitizer] Fix PosixSpawnImpl which fails exitcode test

--- compiler-rt/test/sanitizer_common/TestCases/Posix/posix_spawn.c
+++ compiler-rt/test/sanitizer_common/TestCases/Posix/posix_spawn.c
@@ -1,7 +1,4 @@
 // RUN: %clang %s -o %t && %run %t 2>&1 | FileCheck %s
-//
-// Older versions of Android do not have certain posix_spawn* functions.
-// UNSUPPORTED: android
 
 #include <assert.h>
 #include <spawn.h>
@@ -16,21 +13,14 @@ int main(int argc, char **argv) {
     return 0;
   }
 
-  int s;
-
-  posix_spawnattr_t attr;
-  s = posix_spawnattr_init(&attr);
-  assert(!s);
-
-  posix_spawn_file_actions_t file_actions;
-  s = posix_spawn_file_actions_init(&file_actions);
-  assert(!s);
+  posix_spawnattr_t attr = {};
+  posix_spawn_file_actions_t file_actions = {};
 
   char *const args[] = {argv[0], "2", NULL};
   char *const env[] = {"A=B", NULL};
 
   pid_t pid;
-  s = posix_spawn(&pid, argv[0], &file_actions, &attr, args, env);
+  int s = posix_spawn(&pid, argv[0], &file_actions, &attr, args, env);
   assert(!s);
 
   waitpid(pid, &s, WUNTRACED | WCONTINUED);
