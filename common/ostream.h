6a7a75d038805f76c71ef24b994ccc0da9c88a0b
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-11-02 20:10:21 +0000

    using

--- common/ostream.h
+++ common/ostream.h
@@ -9,20 +9,20 @@
 
 namespace Carbon {
 
+template <typename T>
+using HasPrintMethod = typename std::enable_if_t<
+    std::is_member_function_pointer_v<decltype(&T::Print)>>;
+
 // Support ostream << for types which implement:
 //   void Print(llvm::raw_ostream& out) const;
-template <typename T,
-          typename std::enable_if_t<std::is_member_function_pointer_v<
-              decltype(&T::Print)>>* = nullptr>
+template <typename T, HasPrintMethod<T>* = nullptr>
 auto operator<<(llvm::raw_ostream& out, const T& obj) -> llvm::raw_ostream& {
   obj.Print(out);
   return out;
 }
 
 // Prevents ostream << for pointers to printable types.
-template <typename T,
-          typename std::enable_if_t<std::is_member_function_pointer_v<
-              decltype(&T::Print)>>* = nullptr>
+template <typename T, HasPrintMethod<T>* = nullptr>
 __attribute__((unavailable(
     "Received a pointer to a printable type, are you missing a `*`? "
     "To print as a pointer, cast to void*."))) auto
