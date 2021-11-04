8720149d9bb17f3059e18a8170dc0175104f7c19
Jamie Schmeiser <schmeise@ca.ibm.com>
2021-11-04 10:39:39 -0400

    Remove unused function from print-changed=dot-cfg code
    Summary:
    Remove unused function from print-changed=dot-cfg code to silence a
    gcc compiler warning.
    
    Author: Jamie Schmeiser <schmeise@ca.ibm.com>
    Reviewed By: uabelho(Mikael Holmen)
    Differential Revision: https://reviews.llvm.org/D113188

--- llvm/lib/Passes/StandardInstrumentations.cpp
+++ llvm/lib/Passes/StandardInstrumentations.cpp
@@ -1553,8 +1553,6 @@ public:
 protected:
   // Return the string surrounded by HTML to make it the appropriate colour.
   std::string colourize(std::string S, IRChangeDiffType T) const;
-  // Return the string containing the colour to use as a Dot attribute.
-  std::string attribute(IRChangeDiffType T) const;
 
   void createNode(StringRef Label, const BlockDataT<DCData> &BD,
                   IRChangeDiffType T) {
@@ -1644,10 +1642,6 @@ std::string DotCfgDiff::colourize(std::string S, IRChangeDiffType T) const {
   if (S.length() == 0)
     return S;
   return "<FONT COLOR=\"" + Colours[T] + "\">" + S + "</FONT>";
-}
-
-std::string DotCfgDiff::attribute(IRChangeDiffType T) const {
-  return "color=" + Colours[T];
 }
 
 std::string DotCfgDiffDisplayGraph::attribute(IRChangeDiffType T) const {
