3120cadac782cd51f88f52dd2e88a20a6aa77b4b
Jonas Devlieghere <jonas@devlieghere.com>
2021-11-04 09:43:31 -0700

    [debugserver] Fix typo in DNBArchImplARM64
    rdar://85020754

--- lldb/tools/debugserver/source/MacOSX/arm64/DNBArchImplARM64.cpp
+++ lldb/tools/debugserver/source/MacOSX/arm64/DNBArchImplARM64.cpp
@@ -159,7 +159,7 @@ kern_return_t DNBArchMachARM64::GetGPRState(bool force) {
         uint64_t log_fp = m_state.context.gpr.__fp;
         uint64_t log_lr = m_state.context.gpr.__lr;
         uint64_t log_sp = m_state.context.gpr.__sp;
-        uint64_t log_pc = m_state.context.gpr.__pc,
+        uint64_t log_pc = m_state.context.gpr.__pc;
 #endif
     DNBLogThreaded(
         "thread_get_state(0x%4.4x, %u, &gpr, %u) => 0x%8.8x (count = %u) regs"
