005456e5fc67f05a3df8f099f3856c6ac049304f
Fangrui Song <i@maskray.me>
2021-11-04 21:28:33 -0700

    [lld-macho] Fix an assertion failure when -u specifies an undefined section$start symbol
    This matches ld64. Also improve the test for `-dead_strip`.
    
    Reviewed By: #lld-macho, Jez Ng
    
    Differential Revision: https://reviews.llvm.org/D113147

--- lld/MachO/MarkLive.cpp
+++ lld/MachO/MarkLive.cpp
@@ -96,8 +96,7 @@ void markLive() {
   }
   // -u symbols
   for (Symbol *sym : config->explicitUndefineds)
-    if (auto *defined = dyn_cast<Defined>(sym))
-      addSym(defined);
+    addSym(sym);
   // local symbols explicitly marked .no_dead_strip
   for (const InputFile *file : inputFiles)
     if (auto *objFile = dyn_cast<ObjFile>(file))
