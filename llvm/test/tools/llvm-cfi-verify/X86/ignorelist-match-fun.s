c4396b77ae79d9273ab79c3615c2596940ade06d
Zarko Todorovski <zarko@ca.ibm.com>
2021-11-08 09:44:26 -0500

    [LLVM][llvm-cfi] Inclusive language: replace uses of blacklist with ignorelist
    Replace the description and file names for this argument. As far as I understand
    this is a positional argument and I don't believe this changes breaks any existing
    interfaces.
    
    Reviewed By: hctim, MaskRay
    
    Differential Revision: https://reviews.llvm.org/D113316

--- llvm/test/tools/llvm-cfi-verify/X86/ignorelist-match-fun.s
+++ llvm/test/tools/llvm-cfi-verify/X86/ignorelist-match-fun.s
@@ -1,11 +1,11 @@
 # RUN: llvm-mc %S/Inputs/unprotected-fullinfo.s -filetype obj \
 # RUN:         -triple x86_64-linux-elf -o %t.o
-# RUN: echo "fun:*main*" > %t.blacklist.txt
-# RUN: llvm-cfi-verify %t.o %t.blacklist.txt | FileCheck %s
+# RUN: echo "fun:*main*" > %t.ignorelist.txt
+# RUN: llvm-cfi-verify %t.o %t.ignorelist.txt | FileCheck %s
 
 # CHECK-LABEL: {{^Instruction: .* \(FAIL_BAD_CONDITIONAL_BRANCH\)}}
 # CHECK-NEXT: tiny.cc:11
-# CHECK-NEXT: {{^Blacklist Match:.*blacklist\.txt:1$}}
+# CHECK-NEXT: {{^Ignorelist Match:.*ignorelist\.txt:1$}}
 # CHECK-NEXT: ====> Expected Unprotected
 
 # CHECK: Expected Protected: 0 (0.00%)
@@ -13,5 +13,5 @@
 # CHECK: Expected Unprotected: 1 (100.00%)
 # CHECK: Unexpected Unprotected (BAD): 0 (0.00%)
 
-# Source: (blacklist.txt):
+# Source: (ignorelist.txt):
 #   fun:*main*
--- llvm/test/tools/llvm-cfi-verify/X86/ignorelist-unexpected-protected.s
+++ llvm/test/tools/llvm-cfi-verify/X86/ignorelist-unexpected-protected.s
@@ -1,11 +1,11 @@
 # RUN: llvm-mc %S/Inputs/protected-lineinfo.s -filetype obj \
 # RUN:         -triple x86_64-linux-elf -o %t.o
-# RUN: echo "src:*tiny*" > %t.blacklist.txt
-# RUN: llvm-cfi-verify %t.o %t.blacklist.txt | FileCheck %s
+# RUN: echo "src:*tiny*" > %t.ignorelist.txt
+# RUN: llvm-cfi-verify %t.o %t.ignorelist.txt | FileCheck %s
 
 # CHECK-LABEL: {{^Instruction: .* \(PROTECTED\)}}
 # CHECK-NEXT: tiny.cc:11
-# CHECK-NEXT: {{^Blacklist Match:.*blacklist\.txt:1$}}
+# CHECK-NEXT: {{^Ignorelist Match:.*ignorelist\.txt:1$}}
 # CHECK-NEXT: ====> Unexpected Protected
 
 # CHECK: Expected Protected: 0 (0.00%)
@@ -13,5 +13,5 @@
 # CHECK: Expected Unprotected: 0 (0.00%)
 # CHECK: Unexpected Unprotected (BAD): 0 (0.00%)
 
-# Source: (blacklist.txt):
+# Source: (ignorelist.txt):
 #   src:*tiny*
