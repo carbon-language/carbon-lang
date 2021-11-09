68a4d179c2ac4c882f2d242b81748ceed66827ff
Adrian Prantl <aprantl@apple.com>
2021-11-09 10:46:09 -0800

    Use yaml2obj instead of relying on invoking the Darwin system assembler.

--- lldb/test/Shell/SymbolFile/absolute-symbol.s
+++
@@ -1,8 +0,0 @@
-# REQUIRES: system-darwin
-# RUN: %clang %s -c -o %t.o
-# RUN: %lldb -b -o 'target modules lookup -s absolute_symbol' %t.o | FileCheck %s
-# CHECK: 1 symbols match 'absolute_symbol'
-# CHECK:   Address: 0x0000000012345678 (0x0000000012345678)
-# CHECK:   Summary: 0x0000000012345678
-.globl absolute_symbol
-absolute_symbol = 0x12345678
