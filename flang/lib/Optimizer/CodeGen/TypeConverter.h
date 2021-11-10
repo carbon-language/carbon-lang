a343b74f8532c8e04e600d5d6e1ba7959f950808
Diana Picus <diana.picus@linaro.org>
2021-11-10 11:26:15 +0000

    [fir] Add !fir.char type conversion
    This patch is part of the upstreaming effort from fir-dev.
    
    Differential Revision: https://reviews.llvm.org/D113560
    
    Co-authored-by: Eric Schweitz <eschweitz@nvidia.com>
    Co-authored-by: Jean Perier <jperier@nvidia.com>

--- flang/lib/Optimizer/CodeGen/TypeConverter.h
+++ flang/lib/Optimizer/CodeGen/TypeConverter.h
@@ -37,6 +37,8 @@ public:
 
     // Each conversion should return a value of type mlir::Type.
     addConversion([&](BoxType box) { return convertBoxType(box); });
+    addConversion(
+        [&](fir::CharacterType charTy) { return convertCharType(charTy); });
     addConversion([&](fir::LogicalType boolTy) {
       return mlir::IntegerType::get(
           &getContext(), kindMapping.getLogicalBitsize(boolTy.getFKind()));
@@ -148,6 +150,18 @@ public:
     return mlir::LLVM::LLVMPointerType::get(
         mlir::LLVM::LLVMStructType::getLiteral(&getContext(), dataDescFields,
                                                /*isPacked=*/false));
+  }
+
+  unsigned characterBitsize(fir::CharacterType charTy) {
+    return kindMapping.getCharacterBitsize(charTy.getFKind());
+  }
+
+  // fir.char<n>  -->  llvm<"ix*">   where ix is scaled by kind mapping
+  mlir::Type convertCharType(fir::CharacterType charTy) {
+    auto iTy = mlir::IntegerType::get(&getContext(), characterBitsize(charTy));
+    if (charTy.getLen() == fir::CharacterType::unknownLen())
+      return iTy;
+    return mlir::LLVM::LLVMArrayType::get(iTy, charTy.getLen());
   }
 
   // Use the target specifics to figure out how to map complex to LLVM IR. The
