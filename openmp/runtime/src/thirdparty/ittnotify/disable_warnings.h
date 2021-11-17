9b2b54983765abd365f6e43428c6a2697c8e4c03
Martin Storsjö <martin@martin.st>
2021-11-17 10:49:44 +0200

    [OpenMP] Silence build warnings when built with MinGW
    There's an attempt to upstream this change in
    https://github.com/intel/ittapi/pull/25 too.
    
    Differential Revision: https://reviews.llvm.org/D114069

--- openmp/runtime/src/thirdparty/ittnotify/disable_warnings.h
+++ openmp/runtime/src/thirdparty/ittnotify/disable_warnings.h
@@ -11,6 +11,8 @@
 
 #if ITT_PLATFORM == ITT_PLATFORM_WIN
 
+#if defined _MSC_VER
+
 #pragma warning(disable : 593) /* parameter "XXXX" was set but never used */
 #pragma warning(disable : 344) /* typedef name has already been declared (with \
                                   same type) */
@@ -18,6 +20,8 @@
 #pragma warning(disable : 4127) /* conditional expression is constant */
 #pragma warning(                                                               \
     disable : 4306) /* conversion from '?' to '?' of greater size */
+
+#endif /* _MSC_VER */
 
 #endif /* ITT_PLATFORM==ITT_PLATFORM_WIN */
 
