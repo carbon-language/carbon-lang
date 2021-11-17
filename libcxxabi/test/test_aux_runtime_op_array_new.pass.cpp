eb8650a75793b2bd079d0c8901ff066f129061da
Louis Dionne <ldionne.2@gmail.com>
2021-11-17 16:25:01 -0500

    [runtimes][NFC] Remove filenames at the top of the license notice
    We've stopped doing it in libc++ for a while now because these names
    would end up rotting as we move things around and copy/paste stuff.
    This cleans up all the existing files so as to stop the spreading
    as people copy-paste headers around.

--- libcxxabi/test/test_aux_runtime_op_array_new.pass.cpp
+++ libcxxabi/test/test_aux_runtime_op_array_new.pass.cpp
@@ -1,4 +1,4 @@
-//===-------------------------- test_aux_runtime_op_array_new.cpp ---------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
--- libunwind/src/libunwind_ext.h
+++ libunwind/src/libunwind_ext.h
@@ -1,4 +1,4 @@
-//===------------------------ libunwind_ext.h -----------------------------===//
+//===----------------------------------------------------------------------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
