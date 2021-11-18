cbb8b200a6566943d898e51b80769a648a47a04c
Adrian Prantl <aprantl@apple.com>
2021-11-18 15:07:08 -0800

    Revert "Increase gdbremote timeout."
    This reverts commit 6424dc21bf6b73645ef98a35417e0591b2a95939.

--- lldb/packages/Python/lldbsuite/test/tools/lldb-server/gdbremote_testcase.py
+++ lldb/packages/Python/lldbsuite/test/tools/lldb-server/gdbremote_testcase.py
@@ -62,8 +62,8 @@ class GdbRemoteTestCaseFactory(type):
 @add_metaclass(GdbRemoteTestCaseFactory)
 class GdbRemoteTestCaseBase(Base):
 
-    # Default time out in seconds. The timeout is increased fivefold under Asan.
-    DEFAULT_TIMEOUT =  60 * (5 if ('ASAN_OPTIONS' in os.environ) else 1)
+    # Default time out in seconds. The timeout is increased tenfold under Asan.
+    DEFAULT_TIMEOUT =  20 * (10 if ('ASAN_OPTIONS' in os.environ) else 1)
     # Default sleep time in seconds. The sleep time is doubled under Asan.
     DEFAULT_SLEEP   =  5  * (2  if ('ASAN_OPTIONS' in os.environ) else 1)
 
