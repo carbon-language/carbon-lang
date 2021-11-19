290cddcd139d668251821f11426f37481faf6d7f
Becca Royal-Gordon <broyalgordon@apple.com>
2021-11-19 12:10:15 -0800

    Allow __attribute__((swift_attr)) in attribute push pragmas
    This change allows SwiftAttr to be used with #pragma clang attribute push
    to add Swift attributes to large regions of header files.
    We plan to use this to annotate headers with concurrency information.
    
    Patch by: Becca Royal-Gordon
    
    Differential Revision: https://reviews.llvm.org/D112773

--- clang/test/AST/attr-swift_attr.m
+++ clang/test/AST/attr-swift_attr.m
@@ -4,5 +4,13 @@ __attribute__((swift_attr("@actor")))
 @interface View
 @end
 
-// CHECK: InterfaceDecl {{.*}} View
+// CHECK-LABEL: InterfaceDecl {{.*}} View
 // CHECK-NEXT: SwiftAttrAttr {{.*}} "@actor"
+
+#pragma clang attribute push(__attribute__((swift_attr("@sendable"))), apply_to=objc_interface)
+@interface Contact
+@end
+#pragma clang attribute pop
+
+// CHECK-LABEL: InterfaceDecl {{.*}} Contact
+// CHECK-NEXT: SwiftAttrAttr {{.*}} "@sendable"
