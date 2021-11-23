c075566c8df081c965b690997a06020e1abec962
Paul Robinson <paul.robinson@sony.com>
2021-11-23 08:42:16 -0800

    [PS4][TLI] Remove redundant line

--- llvm/lib/Analysis/TargetLibraryInfo.cpp
+++ llvm/lib/Analysis/TargetLibraryInfo.cpp
@@ -684,7 +684,6 @@ static void initialize(TargetLibraryInfoImpl &TLI, const Triple &T,
     TLI.setUnavailable(LibFunc_strcat_chk);
     TLI.setUnavailable(LibFunc_strcpy_chk);
     TLI.setUnavailable(LibFunc_strlcat_chk);
-    TLI.setUnavailable(LibFunc_strlcat_chk);
     TLI.setUnavailable(LibFunc_strlcpy_chk);
     TLI.setUnavailable(LibFunc_strlen_chk);
     TLI.setUnavailable(LibFunc_strncat_chk);
