2f07534263ca617420239c9d250dffd9ff527a7a
Jessica Clarke <jrtc27@jrtc27.com>
2021-11-26 03:58:36 +0000

    [NFC] Fix typo in 95875d246acb

--- llvm/include/llvm/Analysis/VectorUtils.h
+++ llvm/include/llvm/Analysis/VectorUtils.h
@@ -115,7 +115,7 @@ struct VFShape {
 
     return {EC, Parameters};
   }
-  /// Validiation check on the Parameters in the VFShape.
+  /// Validation check on the Parameters in the VFShape.
   bool hasValidParameterList() const;
 };
 
