f9a4d9aa0309ca353de909a19b648ab34f6c0c68
Fangrui Song <i@maskray.me>
2021-11-28 12:47:49 -0800

    [ELF] -z separate-*: Use max-page-size instead of common-page-size for text/non-SHF_ALLOC transition and writeTrapInstr
    For -z separate-code and -z separate-loadable-segments:
    
    When RW is present, the RX to RW transition is aligned with max-page-size.
    When RW is absent, the RX to non-SHF_ALLOC transition should use max-page-size as well.

--- lld/ELF/Writer.cpp
+++ lld/ELF/Writer.cpp
@@ -2608,7 +2608,7 @@ template <class ELFT> void Writer<ELFT>::assignFileOffsets() {
     // following section to avoid loading non-segments parts of the file.
     if (config->zSeparate != SeparateSegmentKind::None && lastRX &&
         lastRX->lastSec == sec)
-      off = alignTo(off, config->commonPageSize);
+      off = alignTo(off, config->maxPageSize);
   }
   for (OutputSection *sec : outputSections)
     if (!(sec->flags & SHF_ALLOC))
@@ -2886,9 +2886,9 @@ template <class ELFT> void Writer<ELFT>::writeTrapInstr() {
     for (PhdrEntry *p : part.phdrs)
       if (p->p_type == PT_LOAD && (p->p_flags & PF_X))
         fillTrap(Out::bufferStart + alignDown(p->firstSec->offset + p->p_filesz,
-                                              config->commonPageSize),
+                                              config->maxPageSize),
                  Out::bufferStart + alignTo(p->firstSec->offset + p->p_filesz,
-                                            config->commonPageSize));
+                                            config->maxPageSize));
 
     // Round up the file size of the last segment to the page boundary iff it is
     // an executable segment to ensure that other tools don't accidentally
@@ -2900,7 +2900,7 @@ template <class ELFT> void Writer<ELFT>::writeTrapInstr() {
 
     if (last && (last->p_flags & PF_X))
       last->p_memsz = last->p_filesz =
-          alignTo(last->p_filesz, config->commonPageSize);
+          alignTo(last->p_filesz, config->maxPageSize);
   }
 }
 
