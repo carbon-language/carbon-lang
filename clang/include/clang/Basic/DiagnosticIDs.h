3c32c568844c745e3fe7fa72ce3aa65340e545bc
Steven Wan <wanyu9511@gmail.com>
2021-11-29 14:11:40 -0500

    [NFC][clang]Increase the number of driver diagnostics
    We're close to hitting the limited number of driver diagnostics, increase `DIAG_SIZE_DRIVER` to accommodate more.
    
    Reviewed By: erichkeane
    
    Differential Revision: https://reviews.llvm.org/D114615

--- clang/include/clang/Basic/DiagnosticIDs.h
+++ clang/include/clang/Basic/DiagnosticIDs.h
@@ -30,7 +30,7 @@ namespace clang {
     // Size of each of the diagnostic categories.
     enum {
       DIAG_SIZE_COMMON        =  300,
-      DIAG_SIZE_DRIVER        =  250,
+      DIAG_SIZE_DRIVER        =  300,
       DIAG_SIZE_FRONTEND      =  150,
       DIAG_SIZE_SERIALIZATION =  120,
       DIAG_SIZE_LEX           =  400,
