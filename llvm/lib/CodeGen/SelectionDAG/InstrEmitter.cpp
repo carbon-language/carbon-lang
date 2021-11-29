a20987adf4f80e2657eb3032a5a91e13f58106a0
Jeremy Morse <jeremy.morse@sony.com>
2021-11-29 22:06:46 +0000

    [DebugInfo][InstrRef] Add indirection from dbg.declare in SelectionDAG
    Usually dbg.declares get translated into either entries in an MF
    side-table, or a DBG_VALUE on entry to the function with IsIndirect set
    (including in instruction referencing mode). Much rarer is a dbg.declare
    attached to a non-argument value, such as in the test added in this patch
    where there's a variable-length-array. Such dbg.declares become SDDbgValue
    nodes with InIndirect=true.
    
    As it happens, we weren't correctly emitting DBG_INSTR_REFs with the
    additional indirection. This patch adds the extra indirection, encoded as
    adding an additional DW_OP_deref to the expression.
    
    Differential Revision: https://reviews.llvm.org/D114440

--- llvm/lib/CodeGen/SelectionDAG/InstrEmitter.cpp
+++ llvm/lib/CodeGen/SelectionDAG/InstrEmitter.cpp
@@ -765,7 +765,7 @@ InstrEmitter::EmitDbgInstrRef(SDDbgValue *SD,
   assert(!SD->isVariadic());
   SDDbgOperand DbgOperand = SD->getLocationOps()[0];
   MDNode *Var = SD->getVariable();
-  MDNode *Expr = SD->getExpression();
+  DIExpression *Expr = (DIExpression*)SD->getExpression();
   DebugLoc DL = SD->getDebugLoc();
   const MCInstrDesc &RefII = TII->get(TargetOpcode::DBG_INSTR_REF);
 
@@ -774,6 +774,13 @@ InstrEmitter::EmitDbgInstrRef(SDDbgValue *SD,
   if (DbgOperand.getKind() == SDDbgOperand::FRAMEIX ||
       DbgOperand.getKind() == SDDbgOperand::CONST)
     return EmitDbgValueFromSingleOp(SD, VRBaseMap);
+
+  // Immediately fold any indirectness from the LLVM-IR intrinsic into the
+  // expression:
+  if (SD->isIndirect()) {
+    std::vector<uint64_t> Elts = {dwarf::DW_OP_deref};
+    Expr = DIExpression::append(Expr, Elts);
+  }
 
   // It may not be immediately possible to identify the MachineInstr that
   // defines a VReg, it can depend for example on the order blocks are
