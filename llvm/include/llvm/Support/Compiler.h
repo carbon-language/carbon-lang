71e97ad35b2abcc89cc8ff471a3eb404120cf208
Ben Langmuir <blangmuir@apple.com>
2021-12-10 11:49:04 -0800

    Only define LLVM_EXTERNAL_VISIBILITY when building libLLVM dylib
    When building LLVM static libraries, we should not make symbols more
    visible than CMAKE_CXX_VISIBILITY_PRESET, since the goal may be to have
    a purely hidden llvm embedded in another library. Instead, we only
    define LLVM_EXTERNAL_VISIBILITY for the dynamic library build (when
    LLVM_BUILD_LLVM_DYLIB=YES).
    
    Differential Revision: https://reviews.llvm.org/D113610

--- llvm/include/llvm/Support/Compiler.h
+++ llvm/include/llvm/Support/Compiler.h
@@ -121,12 +121,16 @@
 /// On PE/COFF targets, library visibility is the default, so this isn't needed.
 ///
 /// LLVM_EXTERNAL_VISIBILITY - classes, functions, and variables marked with
-/// this attribute will be made public and visible outside of any shared library
-/// they are linked in to.
+/// this attribute will be made public and visible outside the libLLVM dynamic
+/// library.
 #if __has_attribute(visibility) && !defined(__MINGW32__) &&                    \
     !defined(__CYGWIN__) && !defined(_WIN32)
 #define LLVM_LIBRARY_VISIBILITY __attribute__ ((visibility("hidden")))
-#define LLVM_EXTERNAL_VISIBILITY __attribute__ ((visibility("default")))
+#if defined(LLVM_BUILD_LLVM_DYLIB)
+#define LLVM_EXTERNAL_VISIBILITY __attribute__((visibility("default")))
+#else
+#define LLVM_EXTERNAL_VISIBILITY
+#endif
 #else
 #define LLVM_LIBRARY_VISIBILITY
 #define LLVM_EXTERNAL_VISIBILITY
