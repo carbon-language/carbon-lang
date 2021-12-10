edd2b99a57c127dc3d99fe7550d69a113de53eb0
Florian Mayer <fmayer@google.com>
2021-12-10 14:21:09 -0800

    [sanitizers] include build ids in stacks on linux.
    Reviewed By: eugenis
    
    Differential Revision: https://reviews.llvm.org/D114294

--- compiler-rt/lib/sanitizer_common/sanitizer_common.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_common.cpp
@@ -138,7 +138,15 @@ void LoadedModule::set(const char *module_name, uptr base_address,
   set(module_name, base_address);
   arch_ = arch;
   internal_memcpy(uuid_, uuid, sizeof(uuid_));
+  uuid_size_ = kModuleUUIDSize;
   instrumented_ = instrumented;
+}
+
+void LoadedModule::setUuid(const char *uuid, uptr size) {
+  if (size > kModuleUUIDSize)
+    size = kModuleUUIDSize;
+  internal_memcpy(uuid_, uuid, size);
+  uuid_size_ = size;
 }
 
 void LoadedModule::clear() {
