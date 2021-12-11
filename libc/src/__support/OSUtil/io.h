261c17c1d3434e005cbb24ab3ab4ce8a9f8bdd84
Siva Chandra Reddy <sivachandra@google.com>
2021-12-11 08:42:57 +0000

    [libc][NFC] Remove dependecies on entrypoints from loader tests.
    A small collection of utils called osutils has been added. These utils
    are now used in loader tests instead of the equivalent entrypoints.
    The loader can now be built and tested without depending on any entrypoint.
    In a follow up change, the relevent entrypoints will be updated to use the
    newly added osutils.
    
    Reviewed By: lntue, michaelrj
    
    Differential Revision: https://reviews.llvm.org/D115655

---
+++ libc/src/__support/OSUtil/io.h
@@ -0,0 +1,16 @@
+//===---------------- Implementation of IO utils ----------------*- C++ -*-===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+#ifndef LLVM_LIBC_SRC_SUPPORT_OSUTIL_IO_H
+#define LLVM_LIBC_SRC_SUPPORT_OSUTIL_IO_H
+
+#ifdef __unix__
+#include "linux/io.h"
+#endif
+
+#endif // LLVM_LIBC_SRC_SUPPORT_OSUTIL_IO_H
