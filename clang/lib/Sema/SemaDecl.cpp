0d1363e5614e24d164373e19e3f4b7102188d4e3
Yuanfang Chen <yuanfang.chen@sony.com>
2021-12-15 10:25:37 -0800

    Revert "[Sema] Mark explicit specialization declaration in a friend invalid"
    This reverts commit 8cb6ecbc4da2b0cfd8dcf04f612dc413716d27a1.
    
    Nothing wrong with the commit. It is missing Phabricator informations.

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -9202,7 +9202,6 @@ Sema::ActOnFunctionDeclarator(Scope *S, Declarator &D, DeclContext *DC,
             << Name << RemoveRange
             << FixItHint::CreateRemoval(RemoveRange)
             << FixItHint::CreateInsertion(InsertLoc, "<>");
-          Invalid = true;
         }
       }
     } else {
