770ef94097c02205b3ec9e902f1d6a9c99b5189c
Nico Weber <thakis@chromium.org>
2021-12-16 20:46:51 -0500

    Revert "[analyzer][ctu] Fix wrong 'multiple definitions' errors caused by space characters in lookup names when parsing the ctu index file"
    This reverts commit 333d66b09494b7ebc1a89f2befa79128a56f77e3.
    Breaks tests on macOS, see comments on https://reviews.llvm.org/D102669

--- clang/test/Analysis/func-mapping-test.cpp
+++ clang/test/Analysis/func-mapping-test.cpp
@@ -3,10 +3,10 @@
 int f(int) {
   return 0;
 }
-// CHECK-DAG: 9:c:@F@f#I#
+// CHECK-DAG: c:@F@f#I#
 
 extern const int x = 5;
-// CHECK-DAG: 4:c:@x
+// CHECK-DAG: c:@x
 
 // Non-const variables should not be collected.
 int y = 5;
@@ -18,45 +18,33 @@ struct S {
   int a;
 };
 extern S const s = {.a = 2};
-// CHECK-DAG: 4:c:@s
+// CHECK-DAG: c:@s
 
 struct SF {
   const int a;
 };
 SF sf = {.a = 2};
-// CHECK-DAG: 5:c:@sf
+// CHECK-DAG: c:@sf
 
 struct SStatic {
   static const int a = 4;
 };
 const int SStatic::a;
-// CHECK-DAG: 14:c:@S@SStatic@a
+// CHECK-DAG: c:@S@SStatic@a
 
 extern int const arr[5] = { 0, 1 };
-// CHECK-DAG: 6:c:@arr
+// CHECK-DAG: c:@arr
 
 union U {
   const int a;
   const unsigned int b;
 };
 U u = {.a = 6};
-// CHECK-DAG: 4:c:@u
+// CHECK-DAG: c:@u
 
 // No USR can be generated for this.
 // Check for no crash in this case.
 static union {
   float uf;
   const int ui;
-};
-
-void f(int (*)(char));
-void f(bool (*)(char));
-
-struct G {
-  G() {
-    f([](char) -> int { return 42; });
-    // CHECK-DAG: 41:c:@S@G@F@G#@Sa@F@operator int (*)(char)#1
-    f([](char) -> bool { return true; });
-    // CHECK-DAG: 42:c:@S@G@F@G#@Sa@F@operator bool (*)(char)#1
-  }
 };
