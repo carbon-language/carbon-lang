979d0ee8ab30a175220af3b39a6df7d56de9d2c8
Alex Lorenz <arphaman@gmail.com>
2021-12-20 12:26:50 -0800

    [clang] fix out of bounds access in an empty string when lexing a _Pragma with missing string token
    The lexer can attempt to lex a _Pragma and crash with an out of bounds string access when it's
    lexing a _Pragma whose string token is an invalid buffer, e.g. when a module header file from which the macro
    expansion for that token was deleted from the file system.
    
    Differential Revision: https://reviews.llvm.org/D116052

--- clang/lib/Lex/Pragma.cpp
+++ clang/lib/Lex/Pragma.cpp
@@ -263,7 +263,12 @@ void Preprocessor::Handle_Pragma(Token &Tok) {
   }
 
   SourceLocation RParenLoc = Tok.getLocation();
-  std::string StrVal = getSpelling(StrTok);
+  bool Invalid = false;
+  std::string StrVal = getSpelling(StrTok, &Invalid);
+  if (Invalid) {
+    Diag(PragmaLoc, diag::err__Pragma_malformed);
+    return;
+  }
 
   // The _Pragma is lexically sound.  Destringize according to C11 6.10.9.1:
   // "The string literal is destringized by deleting any encoding prefix,
