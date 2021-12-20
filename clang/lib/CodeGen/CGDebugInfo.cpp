18f087c21cdb08fec77adff47a528a5a335807d9
Esme-Yi <esme.yi@ibm.com>
2021-12-20 02:40:42 +0000

    [DebugInfo][Clang] record the access flag for class/struct/union types.
    Summary: This patch records the access flag for
    class/struct/union types in the clang part.
    
    The summary of binary size change and debug info size change due to the DW_AT_accessibility attribute are as the following table. They are built with flags of `clang -O0 -g` (no -gz).
    
    | section | before | after | change | % |
    | .debug_loc | 929821 | 929821 |0|0|
    |.debug_abbrev | 5885289 | 5971547 |+86258|+1.466%|
    |.debug_info | 497613455 | 498122074 |+508619|+0.102%|
    |.debug_ranges | 45731664 | 45731664 |0|0|
    |.debug_str | 233842595 | 233839388 |-3207| -0.001%|
    |.debug_line | 149773166 | 149764583 |-8583|-0.006%|
    |total (debug) |933775990 |934359077|+583087 |+0.062%|
    
    |total (binary) |1394617288 | 1395200024| +582736|+0.042%|
    
    Reviewed By: dblaikie, shchenz
    
    Differential Revision: https://reviews.llvm.org/D115503

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -3633,6 +3633,9 @@ llvm::DICompositeType *CGDebugInfo::CreateLimitedType(const RecordType *Ty) {
     // Record exports it symbols to the containing structure.
     if (CXXRD->isAnonymousStructOrUnion())
         Flags |= llvm::DINode::FlagExportSymbols;
+
+    Flags |= getAccessFlag(CXXRD->getAccess(),
+                           dyn_cast<CXXRecordDecl>(CXXRD->getDeclContext()));
   }
 
   llvm::DINodeArray Annotations = CollectBTFDeclTagAnnotations(D);
