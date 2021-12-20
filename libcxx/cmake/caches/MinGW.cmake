f68e89044a2ee14babd1af8fc406c9bab080992a
Martin Storsjö <martin@martin.st>
2021-12-21 01:19:34 +0200

    [libcxx] Add LIBCXX_EXTRA_SITE_DEFINES for adding extra defines in __config_site
    This is similar to the existing setting LIBCXX_ABI_DEFINES, with
    the difference that this also allows setting other defines than
    ones that start with "_LIBCPP_ABI_", and allows setting defines
    to a specific value.
    
    This allows avoiding using LIBCXX_TEST_COMPILER_FLAGS in two
    CI configurations.
    
    Differential Revision: https://reviews.llvm.org/D116109

--- libcxx/cmake/caches/MinGW.cmake
+++ libcxx/cmake/caches/MinGW.cmake
@@ -13,8 +13,6 @@ set(LIBUNWIND_USE_COMPILER_RT ON CACHE BOOL "")
 
 set(LIBCXX_TARGET_INFO "libcxx.test.target_info.MingwLocalTI" CACHE STRING "")
 
-# Without these flags, 'long double' (which is 80 bit on x86 mingw, but
+# Without this flag, 'long double' (which is 80 bit on x86 mingw, but
 # 64 bit in MSVC) isn't handled correctly in printf.
-set(CMAKE_C_FLAGS "-D__USE_MINGW_ANSI_STDIO=1" CACHE STRING "")
-set(CMAKE_CXX_FLAGS "-D__USE_MINGW_ANSI_STDIO=1" CACHE STRING "")
-set(LIBCXX_TEST_COMPILER_FLAGS "-D__USE_MINGW_ANSI_STDIO=1" CACHE STRING "")
+set(LIBCXX_EXTRA_SITE_DEFINES "__USE_MINGW_ANSI_STDIO=1" CACHE STRING "")
