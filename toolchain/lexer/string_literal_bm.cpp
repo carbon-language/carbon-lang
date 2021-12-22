01dbb7bab6ca4ffd60b7e92cd3f1c06df6f43583
jonmeow <46229924+jonmeow@users.noreply.github.com>
2021-12-22 23:55:15 +0000

    Checkpoint

--- toolchain/lexer/string_literal_bm.cpp
+++ toolchain/lexer/string_literal_bm.cpp
@@ -2,14 +2,15 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "toolchain/lexer/string_literal.h"
-
 #include <benchmark/benchmark.h>
+
+#include "toolchain/lexer/string_literal.h"
 
 namespace Carbon::Testing {
 namespace {
 
-static void BM_ValidString(benchmark::State& state, std::string_view introducer, std::string_view terminator) {
+static void BM_ValidString(benchmark::State& state, std::string_view introducer,
+                           std::string_view terminator) {
   std::string x(introducer);
   x.append(100000, 'a');
   x.append(terminator);
@@ -34,7 +35,9 @@ BENCHMARK(BM_ValidString_Simple);
 BENCHMARK(BM_ValidString_Multiline);
 BENCHMARK(BM_ValidString_Raw);
 
-static void BM_IncompleteWithRepeatedEscapes(benchmark::State& state, std::string_view introducer, std::string_view escape) {
+static void BM_IncompleteWithRepeatedEscapes(benchmark::State& state,
+                                             std::string_view introducer,
+                                             std::string_view escape) {
   std::string x(introducer);
   // Aim for about 100k to emphasize escape parsing issues.
   while (x.size() < 100000) {
@@ -67,5 +70,5 @@ BENCHMARK(BM_IncompleteWithEscapes_Simple);
 BENCHMARK(BM_IncompleteWithEscapes_Multiline);
 BENCHMARK(BM_IncompleteWithEscapes_Raw);
 
-} // namespace
+}  // namespace
 }  // namespace Carbon::Testing
