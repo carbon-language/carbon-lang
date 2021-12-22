edf8e3ea5ee74f7bf86651b6caa29e456e3119cd
Mircea Trofin <mtrofin@google.com>
2021-12-22 13:17:10 -0800

    [NFC][mlgo]Make the test model generator inlining-specific
    When looking at building the generator for regalloc, we realized we'd
    need quite a bit of custom logic, and that perhaps it'd be easier to
    just have each usecase (each kind of mlgo policy) have it's own
    stand-alone test generator.
    
    This patch just consolidates the old `config.py` and
    `generate_mock_model.py` into one file, and does away with
    subdirectories under Analysis/models.

--- llvm/lib/Analysis/models/gen-inline-oz-test-model.py
+++ llvm/lib/Analysis/models/gen-inline-oz-test-model.py
@@ -1,4 +1,13 @@
-"""Inlining Training config."""
+"""Generate a mock model for LLVM tests.
+
+The generated model is not a neural net - it is just a tf.function with the
+correct input and output parameters. By construction, the mock model will always
+output 1.
+"""
+
+import os
+import importlib.util
+import sys
 
 import tensorflow as tf
 
@@ -85,3 +94,49 @@ def get_output_signature():
 
 def get_output_spec():
   return POLICY_OUTPUT_SPEC
+
+def get_output_spec_path(path):
+  return os.path.join(path, 'output_spec.json')
+
+
+def build_mock_model(path, signature):
+  """Build and save the mock model with the given signature"""
+  module = tf.Module()
+
+  # We have to set this useless variable in order for the TF C API to correctly
+  # intake it
+  module.var = tf.Variable(0.)
+
+  def action(*inputs):
+    s = tf.reduce_sum([tf.cast(x, tf.float32) for x in tf.nest.flatten(inputs)])
+    return {signature['output']: float('inf') + s + module.var}
+
+  module.action = tf.function()(action)
+  action = {'action': module.action.get_concrete_function(signature['inputs'])}
+  tf.saved_model.save(module, path, signatures=action)
+
+  output_spec_path = get_output_spec_path(path)
+  with open(output_spec_path, 'w') as f:
+    print(f'Writing output spec to {output_spec_path}.')
+    f.write(signature['output_spec'])
+
+
+def get_signature():
+  return {
+      'inputs': get_input_signature(),
+      'output': get_output_signature(),
+      'output_spec': get_output_spec()
+  }
+
+
+def main(argv):
+  assert len(argv) == 2
+  model_path = argv[1]
+
+  print(f'Output model to: [{argv[1]}]')
+  signature = get_signature()
+  build_mock_model(model_path, signature)
+
+
+if __name__ == '__main__':
+  main(sys.argv)
