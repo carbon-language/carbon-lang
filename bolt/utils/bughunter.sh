9a689946f2477173c3e4a4988d6e230a8ede34e2
Maksim Panchenko <maks@fb.com>
2021-12-23 10:14:27 -0800

    [bughunter.sh][NFC] Fix license and file description
    Summary:
    Convert bughunter.sh to the new LLVM license and fix file-description
    comments.
    
    (cherry picked from FBD33298823)

--- bolt/utils/bughunter.sh
+++ bolt/utils/bughunter.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#===--------------- llvm/tools/llvm-bolt/utils/bughunter.sh ---------------===//
+##===- bolt/utils/bughunter.sh - Help locate BOLT bugs -------*- Script -*-===##
 #
-#                     The LLVM Compiler Infrastructure
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+# details.
 #
-# This file is distributed under the University of Illinois Open Source
-# License. See LICENSE.TXT for details.
-#
-#===-----------------------------------------------------------------------===//
+##===----------------------------------------------------------------------===##
 #
 # This script attempts to narrow down llvm-bolt bug to a single function in the
 # input binary.
@@ -55,6 +55,8 @@
 #
 #   VERBOSE           - if non-empty, set the script to echo mode.
 #
+##===----------------------------------------------------------------------===##
+
 BOLT=${BOLT:=llvm-bolt}
 
 ulimit -c 0
