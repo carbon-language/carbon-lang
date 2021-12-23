0f32a5e5e1c083aeb78410ec4e5859ef78652e35
Maksim Panchenko <maks@fb.com>
2021-12-22 23:34:49 -0800

    [BOLT][NFC] Clear HFSort copyright/license
    Summary:
    Remove the copyright/license message for the code originated from
    Facebook.
    
    (cherry picked from FBD33293101)

--- bolt/include/bolt/Passes/HFSort.h
+++ bolt/include/bolt/Passes/HFSort.h
@@ -6,6 +6,9 @@
 //
 //===----------------------------------------------------------------------===//
 //
+// Implementation of HFSort algorithm for function ordering:
+// https://research.fb.com/wp-content/uploads/2017/01/cgo2017-hfsort-final1.pdf
+//
 // Cluster functions by hotness.  There are four clustering algorithms:
 // 1. clusterize
 // 2. HFsort+
@@ -14,24 +17,6 @@
 //
 // See original code in hphp/utils/hfsort.[h,cpp]
 //===----------------------------------------------------------------------===//
-
-// TODO: copyright/license msg.
-
-/*
-   +----------------------------------------------------------------------+
-   | HipHop for PHP                                                       |
-   +----------------------------------------------------------------------+
-   | Copyright (c) 2010-2016 Facebook, Inc. (http://www.facebook.com)     |
-   +----------------------------------------------------------------------+
-   | This source file is subject to version 3.01 of the PHP license,      |
-   | that is bundled with this package in the file LICENSE, and is        |
-   | available through the world-wide-web at the following url:           |
-   | http://www.php.net/license/3_01.txt                                  |
-   | If you did not receive a copy of the PHP license and are unable to   |
-   | obtain it through the world-wide-web, please send a note to          |
-   | license@php.net so we can mail you a copy immediately.               |
-   +----------------------------------------------------------------------+
-*/
 
 #ifndef BOLT_PASSES_HFSORT_H
 #define BOLT_PASSES_HFSORT_H
