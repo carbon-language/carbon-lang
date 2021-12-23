be8180af5854806a343c3dd334d97ba2c4bfadfa
Nathan Chancellor <nathan@kernel.org>
2021-12-23 11:43:26 -0700

    [clang][driver] Warn when '-mno-outline-atomics' is used with a non-AArch64 triple
    The Linux kernel has a make macro called cc-option that invokes the
    compiler with an option in isolation to see if it is supported before
    adding it to CFLAGS. The exit code of the compiler is used to determine
    if the flag is supported and should be added to the compiler invocation.
    
    A call to cc-option with '-mno-outline-atomics' was added to prevent
    linking errors with newer GCC versions but this call succeeds with a
    non-AArch64 target because there is no warning from clang with
    '-mno-outline-atomics', just '-moutline-atomics'. Because the call
    succeeds and adds '-mno-outline-atomics' to the compiler invocation,
    there is a warning from LLVM because the 'outline-atomics target
    feature is only supported by the AArch64 backend.
    
    $ echo | clang -target x86_64 -moutline-atomics -Werror -x c -c -o /dev/null -
    clang-14: error: The 'x86_64' architecture does not support -moutline-atomics; flag ignored [-Werror,-Woption-ignored]
    
    $ echo $?
    1
    
    $ echo | clang -target x86_64 -mno-outline-atomics -Werror -x c -c -o /dev/null -
    '-outline-atomics' is not a recognized feature for this target (ignoring feature)
    
    $ echo $?
    0
    
    This does not match GCC's behavior, which errors when the flag is added
    to a non-AArch64 target.
    
    $ echo | gcc -moutline-atomics -x c -c -o /dev/null -
    gcc: error: unrecognized command-line option ‘-moutline-atomics’; did you mean ‘-finline-atomics’?
    
    $ echo | gcc -mno-outline-atomics -x c -c -o /dev/null -
    gcc: error: unrecognized command-line option ‘-mno-outline-atomics’; did you mean ‘-fno-inline-atomics’?
    
    $ echo | aarch64-linux-gnu-gcc -moutline-atomics -x c -c -o /dev/null -
    
    $ echo | aarch64-linux-gnu-gcc -mno-outline-atomics -x c -c -o /dev/null -
    
    To get closer to  GCC's behavior, issue a warning when
    '-mno-outline-atomics' is used without an AArch64 triple and do not add
    '{-,+}outline-atomic" to the list of target features in these cases.
    
    Link: https://github.com/ClangBuiltLinux/linux/issues/1552
    
    Reviewed By: melver, nickdesaulniers
    
    Differential Revision: https://reviews.llvm.org/D116128

--- clang/lib/Driver/ToolChains/Clang.cpp
+++ clang/lib/Driver/ToolChains/Clang.cpp
@@ -7007,18 +7007,18 @@ void Clang::ConstructJob(Compilation &C, const JobAction &JA,
 
   if (Arg *A = Args.getLastArg(options::OPT_moutline_atomics,
                                options::OPT_mno_outline_atomics)) {
-    if (A->getOption().matches(options::OPT_moutline_atomics)) {
-      // Option -moutline-atomics supported for AArch64 target only.
-      if (!Triple.isAArch64()) {
-        D.Diag(diag::warn_drv_moutline_atomics_unsupported_opt)
-            << Triple.getArchName();
-      } else {
+    // Option -moutline-atomics supported for AArch64 target only.
+    if (!Triple.isAArch64()) {
+      D.Diag(diag::warn_drv_moutline_atomics_unsupported_opt)
+          << Triple.getArchName() << A->getOption().getName();
+    } else {
+      if (A->getOption().matches(options::OPT_moutline_atomics)) {
         CmdArgs.push_back("-target-feature");
         CmdArgs.push_back("+outline-atomics");
+      } else {
+        CmdArgs.push_back("-target-feature");
+        CmdArgs.push_back("-outline-atomics");
       }
-    } else {
-      CmdArgs.push_back("-target-feature");
-      CmdArgs.push_back("-outline-atomics");
     }
   } else if (Triple.isAArch64() &&
              getToolChain().IsAArch64OutlineAtomicsDefault(Args)) {
