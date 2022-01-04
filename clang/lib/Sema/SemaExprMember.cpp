e902ffe6d7560f708b76edaa53d75edcb5d49a3f
Yuanfang Chen <yuanfang.chen@sony.com>
2022-01-04 12:11:33 -0800

    [Sema] Fix the assertion in Sema::ActOnDependentMemberExpr
    617007240cbfb97c introduced the use of ActOnDependentMemberExpr with
    variable template specialization. The assertion inside
    ActOnDependentMemberExpr should be adjusted accordingly.
    
    Fixes https://bugs.llvm.org/show_bug.cgi?id=47211
    
    Reviewed By: aaron.ballman
    
    Differential Revision: https://reviews.llvm.org/D113146

--- clang/lib/Sema/SemaExprMember.cpp
+++ clang/lib/Sema/SemaExprMember.cpp
@@ -504,9 +504,12 @@ Sema::ActOnDependentMemberExpr(Expr *BaseExpr, QualType BaseType,
     }
   }
 
-  assert(BaseType->isDependentType() ||
-         NameInfo.getName().isDependentName() ||
-         isDependentScopeSpecifier(SS));
+  assert(BaseType->isDependentType() || NameInfo.getName().isDependentName() ||
+         isDependentScopeSpecifier(SS) ||
+         (TemplateArgs && llvm::any_of(TemplateArgs->arguments(),
+                                       [](const TemplateArgumentLoc &Arg) {
+                                         return Arg.getArgument().isDependent();
+                                       })));
 
   // Get the type being accessed in BaseType.  If this is an arrow, the BaseExpr
   // must have pointer type, and the accessed type is the pointee.
