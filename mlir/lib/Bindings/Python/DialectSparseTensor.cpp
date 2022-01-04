bc1df1fabb578c7efb33fb59e8aec6b85720f863
Alex Zinenko <zinenko@google.com>
2022-01-04 18:38:30 +0100

    [mlir] Fix incorrect top-level comment in DialectSparseTensor.cpp

--- mlir/lib/Bindings/Python/DialectSparseTensor.cpp
+++ mlir/lib/Bindings/Python/DialectSparseTensor.cpp
@@ -1,4 +1,4 @@
-//===- DialectLinalg.cpp - 'sparse_tensor' dialect submodule --------------===//
+//===- DialectSparseTensor.cpp - 'sparse_tensor' dialect submodule --------===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
