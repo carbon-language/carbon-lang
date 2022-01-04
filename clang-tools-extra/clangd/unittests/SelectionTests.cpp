cb9ccd38c55f729a6bd7986bbdcb34755b774240
Haojian Wu <hokein.wu@gmail.com>
2022-01-04 12:07:37 +0100

    [clangd] Move the selection decltype hack to getSourceRange.
    Previously, it was in canSafelySkipNode, which is only used to decide
    whether we should descend into it and its children, and we still used
    the incomplete Decltypeloc.getSourceRange() to claim tokens, which will
    cause some tokens were not claimed correctly.
    
    Separate a change of https://reviews.llvm.org/D116536
    
    Reviewed By: sammccall
    
    Differential Revision: https://reviews.llvm.org/D116586

--- clang-tools-extra/clangd/unittests/SelectionTests.cpp
+++ clang-tools-extra/clangd/unittests/SelectionTests.cpp
@@ -385,6 +385,7 @@ TEST(SelectionTest, CommonAncestor) {
         decltype([[^a]] + a) b;
         )cpp",
           "DeclRefExpr"},
+      {"[[decltype]]^(1) b;", "DecltypeTypeLoc"}, // Not the VarDecl.
 
       // Objective-C nullability attributes.
       {
