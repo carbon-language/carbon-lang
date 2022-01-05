b77fd01a8f4423d2ed0868fb53e5588fe6791cd4
Peter Klausler <pklausler@nvidia.com>
2022-01-05 11:42:18 -0800

    [flang] Don't blank-fill remaining lines in internal output
    Internal writes to character arrays should not blank-fill
    records (elements) past the last one that was written to.
    
    Differential Revision: https://reviews.llvm.org/D117342

--- flang/runtime/internal-unit.cpp
+++ flang/runtime/internal-unit.cpp
@@ -38,15 +38,9 @@ InternalDescriptorUnit<DIR>::InternalDescriptorUnit(
 }
 
 template <Direction DIR> void InternalDescriptorUnit<DIR>::EndIoStatement() {
-  if constexpr (DIR == Direction::Output) { // blank fill
-    while (char *record{CurrentRecord()}) {
-      if (furthestPositionInRecord <
-          recordLength.value_or(furthestPositionInRecord)) {
-        std::fill_n(record + furthestPositionInRecord,
-            *recordLength - furthestPositionInRecord, ' ');
-      }
-      furthestPositionInRecord = 0;
-      ++currentRecordNumber;
+  if constexpr (DIR == Direction::Output) {
+    if (furthestPositionInRecord > 0) {
+      BlankFillOutputRecord();
     }
   }
 }
@@ -127,18 +121,24 @@ bool InternalDescriptorUnit<DIR>::AdvanceRecord(IoErrorHandler &handler) {
     handler.SignalEnd();
     return false;
   }
-  if constexpr (DIR == Direction::Output) { // blank fill
-    if (furthestPositionInRecord <
-        recordLength.value_or(furthestPositionInRecord)) {
-      char *record{CurrentRecord()};
-      RUNTIME_CHECK(handler, record != nullptr);
-      std::fill_n(record + furthestPositionInRecord,
-          *recordLength - furthestPositionInRecord, ' ');
-    }
+  if constexpr (DIR == Direction::Output) {
+    BlankFillOutputRecord();
   }
   ++currentRecordNumber;
   BeginRecord();
   return true;
+}
+
+template <Direction DIR>
+void InternalDescriptorUnit<DIR>::BlankFillOutputRecord() {
+  if constexpr (DIR == Direction::Output) {
+    if (furthestPositionInRecord <
+        recordLength.value_or(furthestPositionInRecord)) {
+      char *record{CurrentRecord()};
+      std::fill_n(record + furthestPositionInRecord,
+          *recordLength - furthestPositionInRecord, ' ');
+    }
+  }
 }
 
 template <Direction DIR>
