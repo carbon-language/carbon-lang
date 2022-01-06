cb0e12a144dea486b383876f138a8398e9c9386e
Jim Lin <jim@andestech.com>
2022-01-06 10:14:46 +0800

    [M68k][test][NFC] Add missing tests for MxCMP_RM
    Let each format of inst have two tests for it like other MxCMP
    testcases.

--- llvm/test/MC/M68k/Arith/Classes/MxCMP_RM.s
+++ llvm/test/MC/M68k/Arith/Classes/MxCMP_RM.s
@@ -9,10 +9,19 @@ cmp.b	(-1,%pc,%d1), %d0
 ; CHECK:      cmp.l  (0,%pc,%d1), %d0
 ; CHECK-SAME: encoding: [0xb0,0xbb,0x18,0x00]
 cmp.l	(0,%pc,%d1), %d0
+; CHECK:      cmp.l  (-1,%pc,%d1), %d0
+; CHECK-SAME: encoding: [0xb0,0xbb,0x18,0xff]
+cmp.l	(-1,%pc,%d1), %d0
 
 ; CHECK:      cmp.b  (0,%pc), %d0
 ; CHECK-SAME: encoding: [0xb0,0x3a,0x00,0x00]
 cmp.b	(0,%pc), %d0
+; CHECK:      cmp.b  (-1,%pc), %d0
+; CHECK-SAME: encoding: [0xb0,0x3a,0xff,0xff]
+cmp.b	(-1,%pc), %d0
+; CHECK:      cmp.l  (0,%pc), %d0
+; CHECK-SAME: encoding: [0xb0,0xba,0x00,0x00]
+cmp.l	(0,%pc), %d0
 ; CHECK:      cmp.l  (-1,%pc), %d0
 ; CHECK-SAME: encoding: [0xb0,0xba,0xff,0xff]
 cmp.l	(-1,%pc), %d0
@@ -33,6 +42,12 @@ cmp.l	(0,%a2,%a2), %d1
 ; CHECK:      cmp.b  (0,%a0), %d0
 ; CHECK-SAME: encoding: [0xb0,0x28,0x00,0x00]
 cmp.b	(0,%a0), %d0
+; CHECK:      cmp.b  (-1,%a1), %d0
+; CHECK-SAME: encoding: [0xb0,0x29,0xff,0xff]
+cmp.b	(-1,%a1), %d0
+; CHECK:      cmp.l  (0,%a0), %d0
+; CHECK-SAME: encoding: [0xb0,0xa8,0x00,0x00]
+cmp.l	(0,%a0), %d0
 ; CHECK:      cmp.l  (-1,%a1), %d0
 ; CHECK-SAME: encoding: [0xb0,0xa9,0xff,0xff]
 cmp.l	(-1,%a1), %d0
@@ -40,6 +55,12 @@ cmp.l	(-1,%a1), %d0
 ; CHECK:      cmp.b  (%a0), %d0
 ; CHECK-SAME: encoding: [0xb0,0x10]
 cmp.b	(%a0), %d0
+; CHECK:      cmp.b  (%a0), %d1
+; CHECK-SAME: encoding: [0xb2,0x10]
+cmp.b	(%a0), %d1
+; CHECK:      cmp.l  (%a1), %d2
+; CHECK-SAME: encoding: [0xb4,0x91]
+cmp.l	(%a1), %d2
 ; CHECK:      cmp.l  (%a1), %d3
 ; CHECK-SAME: encoding: [0xb6,0x91]
 cmp.l	(%a1), %d3
