2bcff220bf1e372e91491911fe0bb76c4c4bbef8
Dave Lee <davelee.com@gmail.com>
2022-01-06 19:38:31 -0800

    [lldb] Set result error state in 'frame variable'
    Ensure that errors in `frame variable` are reflected in result object.
    
    The statistics for `frame variable` show invocations as being successful, even
    when executing one of the error paths.
    
    This change replaces `result.GetErrorStream()` with `result.AppendError()`,
    which also sets the status to `eReturnStatusFailed`.
    
    Differential Revision: https://reviews.llvm.org/D116788
    
    (cherry picked from commit 2c7d10c41278181e3e45c68f28b501cd95193a8a)

--- lldb/test/API/lang/cpp/frame-var-anon-unions/TestFrameVariableAnonymousUnions.py
+++ lldb/test/API/lang/cpp/frame-var-anon-unions/TestFrameVariableAnonymousUnions.py
@@ -30,6 +30,9 @@ class FrameVariableAnonymousUnionsTestCase(TestBase):
 
         self.expect('frame variable c', substrs=["'A"])
 
-        self.expect('frame variable x', matching=False, substrs=['3'])
-        self.expect('frame variable y', matching=False, substrs=["'B'"])
-        self.expect('frame variable z', matching=False, substrs=['14'])
+        self.expect('frame variable x', error=True,
+                    substrs=["no variable named 'x' found"])
+        self.expect('frame variable y', error=True,
+                    substrs=["no variable named 'y' found"])
+        self.expect('frame variable z', error=True,
+                    substrs=["no variable named 'z' found"])
