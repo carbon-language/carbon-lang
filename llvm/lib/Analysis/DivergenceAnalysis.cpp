be7dbd674cb0cec2558e054c39be0eb098a99394
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-01-08 14:30:10 +0000

    [DivergenceAnalysis] Simplify inRegion test based on whether the RegionLoop pointer is null or not
    More closely matches the documentation
    
    Requested by @nikic

--- llvm/lib/Analysis/DivergenceAnalysis.cpp
+++ llvm/lib/Analysis/DivergenceAnalysis.cpp
@@ -130,8 +130,7 @@ bool DivergenceAnalysisImpl::inRegion(const Instruction &I) const {
 }
 
 bool DivergenceAnalysisImpl::inRegion(const BasicBlock &BB) const {
-  return (!RegionLoop && BB.getParent() == &F) ||
-         (RegionLoop && RegionLoop->contains(&BB));
+  return RegionLoop ? RegionLoop->contains(&BB) : (BB.getParent() == &F);
 }
 
 void DivergenceAnalysisImpl::pushUsers(const Value &V) {
