a60e83fe7cebbfb9f0a6a3ad7504c034f5d0e40f
River Riddle <riddleriver@gmail.com>
2022-01-07 17:41:06 -0800

    [mlir][Interfaces] Add a extraSharedClassDeclaration field
    This field allows for defining a code block that is placed in both the interface
    and trait declarations. This is very useful when defining a set of utilities to
    expose on both the Interface class and the derived attribute/operation/type.
    
    In non-static methods, `$_attr`/`$_op`/`$_type` (depending on the type of
    interface) may be used to refer to an instance of the IR entity. In the interface
    declaration, this is an instance of the interface class. In the trait declaration,
    this is an instance of the concrete entity class (e.g. `IntegerAttr`, `FuncOp`, etc.).
    
    Differential Revision: https://reviews.llvm.org/D116961

--- mlir/tools/mlir-tblgen/OpInterfacesGen.cpp
+++ mlir/tools/mlir-tblgen/OpInterfacesGen.cpp
@@ -104,6 +104,7 @@ protected:
   /// The format context to use for methods.
   tblgen::FmtContext nonStaticMethodFmt;
   tblgen::FmtContext traitMethodFmt;
+  tblgen::FmtContext extraDeclsFmt;
 };
 
 /// A specialized generator for attribute interfaces.
@@ -118,6 +119,7 @@ struct AttrInterfaceGenerator : public InterfaceGenerator {
     nonStaticMethodFmt.addSubst("_attr", castCode).withSelf(castCode);
     traitMethodFmt.addSubst("_attr",
                             "(*static_cast<const ConcreteAttr *>(this))");
+    extraDeclsFmt.addSubst("_attr", "(*this)");
   }
 };
 /// A specialized generator for operation interfaces.
@@ -132,6 +134,7 @@ struct OpInterfaceGenerator : public InterfaceGenerator {
         .withOp(castCode)
         .withSelf(castCode);
     traitMethodFmt.withOp("(*static_cast<ConcreteOp *>(this))");
+    extraDeclsFmt.withOp("(*this)");
   }
 };
 /// A specialized generator for type interfaces.
@@ -146,6 +149,7 @@ struct TypeInterfaceGenerator : public InterfaceGenerator {
     nonStaticMethodFmt.addSubst("_type", castCode).withSelf(castCode);
     traitMethodFmt.addSubst("_type",
                             "(*static_cast<const ConcreteType *>(this))");
+    extraDeclsFmt.addSubst("_type", "(*this)");
   }
 };
 } // namespace
@@ -415,6 +419,8 @@ void InterfaceGenerator::emitTraitDecl(Interface &interface,
   }
   if (auto extraTraitDecls = interface.getExtraTraitClassDeclaration())
     os << tblgen::tgfmt(*extraTraitDecls, &traitMethodFmt) << "\n";
+  if (auto extraTraitDecls = interface.getExtraSharedClassDeclaration())
+    os << tblgen::tgfmt(*extraTraitDecls, &traitMethodFmt) << "\n";
 
   os << "  };\n";
 }
@@ -469,6 +475,9 @@ void InterfaceGenerator::emitInterfaceDecl(Interface interface) {
   // Emit any extra declarations.
   if (Optional<StringRef> extraDecls = interface.getExtraClassDeclaration())
     os << *extraDecls << "\n";
+  if (Optional<StringRef> extraDecls =
+          interface.getExtraSharedClassDeclaration())
+    os << tblgen::tgfmt(*extraDecls, &extraDeclsFmt);
 
   os << "};\n";
 
