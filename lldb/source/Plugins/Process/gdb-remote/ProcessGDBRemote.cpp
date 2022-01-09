e69a3d18f48bc0d81b5dd12e735a2ec898ce64d4
Michał Górny <mgorny@moritz.systems>
2022-01-09 19:35:22 +0100

    [lldb] [gdb-remote] Support client fallback for servers without reg defs
    Provide minimal register definition defaults for working with servers
    that implement neither target.xml nor qRegisterInfo packets.  This is
    useful e.g. when interacting with FreeBSD's kernel minimal gdbserver
    that does not send target.xml but uses the same layout for its supported
    register subset as GDB.
    
    The prerequisite for this is the ability to determine the correct
    architecture, e.g. from the target executable.
    
    Differential Revision: https://reviews.llvm.org/D116896

--- lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.cpp
+++ lldb/source/Plugins/Process/gdb-remote/ProcessGDBRemote.cpp
@@ -72,6 +72,7 @@
 #include <thread>
 
 #include "GDBRemoteRegisterContext.h"
+#include "GDBRemoteRegisterFallback.h"
 #include "Plugins/Platform/MacOSX/PlatformRemoteiOS.h"
 #include "Plugins/Process/Utility/GDBRemoteSignals.h"
 #include "Plugins/Process/Utility/InferiorCallPOSIX.h"
@@ -394,6 +395,7 @@ void ProcessGDBRemote::BuildDynamicRegisterInfo(bool force) {
   //     2 - If the target definition doesn't have any of the info from the
   //     target.xml (registers) then proceed to read the target.xml.
   //     3 - Fall back on the qRegisterInfo packets.
+  //     4 - Use hardcoded defaults if available.
 
   FileSpec target_definition_fspec =
       GetGlobalPluginProperties().GetTargetDefinitionFile();
@@ -506,6 +508,9 @@ void ProcessGDBRemote::BuildDynamicRegisterInfo(bool force) {
       break;
     }
   }
+
+  if (registers.empty())
+    registers = GetFallbackRegisters(arch_to_use);
 
   AddRemoteRegisters(registers, arch_to_use);
 }
