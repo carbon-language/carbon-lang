aad27a890754fe13bd57a99a838307fee81496ee
Dave Lee <davelee.com@gmail.com>
2022-01-09 20:10:38 -0800

    [lldb] Check for arm64 in TestDisassembleRawData
    This test checks for `aarch64` but the lit config could also contain `arm64`.
    This change adds `arm64` to make the test pass in all cases.
    
    Differential Revision: https://reviews.llvm.org/D116912

--- lldb/test/API/python_api/disassemble-raw-data/TestDisassembleRawData.py
+++ lldb/test/API/python_api/disassemble-raw-data/TestDisassembleRawData.py
@@ -31,7 +31,7 @@ class DisassembleRawDataTestCase(TestBase):
         elif re.match("powerpc64le", arch):
             target = self.dbg.CreateTargetWithFileAndTargetTriple("", "powerpc64le")
             raw_bytes = bytearray([0x00, 0x00, 0x80, 0x38])
-        elif arch == "aarch64":
+        elif arch in ("aarch64", "arm64"):
             target = self.dbg.CreateTargetWithFileAndTargetTriple("", "aarch64")
             raw_bytes = bytearray([0x60, 0x0c, 0x80, 0x52])
         elif arch == "arm":
@@ -57,7 +57,7 @@ class DisassembleRawDataTestCase(TestBase):
         elif re.match("powerpc64le", arch):
             self.assertEqual(inst.GetMnemonic(target), "li")
             self.assertEqual(inst.GetOperands(target), "4, 0")
-        elif arch == "aarch64":
+        elif arch in ("aarch64", "arm64"):
             self.assertEqual(inst.GetMnemonic(target), "mov")
             self.assertEqual(inst.GetOperands(target), "w0, #0x63")
         elif arch == "arm":
