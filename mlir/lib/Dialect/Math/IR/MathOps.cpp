d2c547342c6d6a8a4b3f9111830e60b1f00f88a1
William S. Moses <gh@wsmoses.com>
2022-01-12 02:00:53 -0500

    Revert "[MLIR][Math] Enable constant folding of ops"
    This reverts commit 2f8b956ab6e63e57fbaa60749c9ba7752b071993.
    
    There is a linker error for mlir-nvidia as seen on
    https://lab.llvm.org/buildbot/#/builders/61/builds/19939.
    
    As it's late for me here, I'm oing to rever this for now to be
    investigated later.

--- mlir/lib/Dialect/Math/IR/MathOps.cpp
+++ mlir/lib/Dialect/Math/IR/MathOps.cpp
@@ -6,9 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "mlir/Dialect/Arithmetic/IR/Arithmetic.h"
 #include "mlir/Dialect/Math/IR/Math.h"
-#include "mlir/IR/Builders.h"
 
 using namespace mlir;
 using namespace mlir::math;
@@ -19,58 +17,3 @@ using namespace mlir::math;
 
 #define GET_OP_CLASSES
 #include "mlir/Dialect/Math/IR/MathOps.cpp.inc"
-
-//===----------------------------------------------------------------------===//
-// CeilOp folder
-//===----------------------------------------------------------------------===//
-
-OpFoldResult math::CeilOp::fold(ArrayRef<Attribute> operands) {
-  auto constOperand = operands.front();
-  if (!constOperand)
-    return {};
-
-  auto attr = constOperand.dyn_cast<FloatAttr>();
-  if (!attr)
-    return {};
-
-  APFloat sourceVal = attr.getValue();
-  sourceVal.roundToIntegral(llvm::RoundingMode::TowardPositive);
-
-  return FloatAttr::get(getType(), sourceVal);
-}
-
-//===----------------------------------------------------------------------===//
-// Log2Op folder
-//===----------------------------------------------------------------------===//
-
-OpFoldResult math::Log2Op::fold(ArrayRef<Attribute> operands) {
-  auto constOperand = operands.front();
-  if (!constOperand)
-    return {};
-
-  auto attr = constOperand.dyn_cast<FloatAttr>();
-  if (!attr)
-    return {};
-
-  auto FT = getType().cast<FloatType>();
-
-  APFloat APF = attr.getValue();
-
-  if (APF.isNegative())
-    return {};
-
-  if (FT.getWidth() == 64)
-    return FloatAttr::get(getType(), log2(APF.convertToDouble()));
-
-  if (FT.getWidth() == 32)
-    return FloatAttr::get(getType(), log2f(APF.convertToDouble()));
-
-  return {};
-}
-
-/// Materialize an integer or floating point constant.
-Operation *math::MathDialect::materializeConstant(OpBuilder &builder,
-                                                  Attribute value, Type type,
-                                                  Location loc) {
-  return builder.create<arith::ConstantOp>(loc, value, type);
-}
