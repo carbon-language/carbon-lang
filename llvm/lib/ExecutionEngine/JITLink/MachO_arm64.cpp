bfd0cbd4eb9fef85da96bb799038ba49a52d08f8
Lang Hames <lhames@gmail.com>
2022-01-13 12:06:53 +1100

    Reapply 253ce92844f "[JITLink][AArch64] Add support for splitting.." with fixes.
    This reapplies 253ce92844f72e3a6d0e423473f2765c2c5afd6a, which was reverted in
    66b2ed477f902329c916bd4389b2034dae09c02b due to bot failures.
    
    I have added the `-phony-externals` option added, which should fix the
    unresolved symbol errors.

--- llvm/lib/ExecutionEngine/JITLink/MachO_arm64.cpp
+++ llvm/lib/ExecutionEngine/JITLink/MachO_arm64.cpp
@@ -709,6 +709,13 @@ void link_MachO_arm64(std::unique_ptr<LinkGraph> G,
     Config.PrePrunePasses.push_back(
         CompactUnwindSplitter("__LD,__compact_unwind"));
 
+    // Add eh-frame passses.
+    // FIXME: Prune eh-frames for which compact-unwind is available once
+    // we support compact-unwind registration with libunwind.
+    Config.PrePrunePasses.push_back(EHFrameSplitter("__TEXT,__eh_frame"));
+    Config.PrePrunePasses.push_back(
+        EHFrameEdgeFixer("__TEXT,__eh_frame", 8, Delta64, Delta32, NegDelta32));
+
     // Add an in-place GOT/Stubs pass.
     Config.PostPrunePasses.push_back(
         PerGraphGOTAndPLTStubsBuilder_MachO_arm64::asPass);
