0e65d5021aa0aab4fbf67ce0a91abf4467b356a9
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-01-14 16:31:14 +0000

    [LTO] runNewPMPasses - remove check for TM != nullptr as we already dereference the pointer directly later on in the same code

--- llvm/lib/LTO/LTOBackend.cpp
+++ llvm/lib/LTO/LTOBackend.cpp
@@ -229,8 +229,7 @@ static void runNewPMPasses(const Config &Conf, Module &Mod, TargetMachine *TM,
     PGOOpt = PGOOptions("", "", "", PGOOptions::NoAction,
                         PGOOptions::NoCSAction, true);
   }
-  if (TM)
-    TM->setPGOOption(PGOOpt);
+  TM->setPGOOption(PGOOpt);
 
   LoopAnalysisManager LAM;
   FunctionAnalysisManager FAM;
