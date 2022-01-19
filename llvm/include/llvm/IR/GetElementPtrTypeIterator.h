93e8cd26857aa22c815ad553cbb631e06f2e662f
Nikita Popov <npopov@redhat.com>
2022-01-19 11:47:27 +0100

    [IR] Remove NumElements tracking from GEP type iterator
    After ed0cdb29397ecd7d03579b846360906081d80aea, this is no longer
    used by anything, and shouldn't be used by anything.

--- llvm/include/llvm/IR/GetElementPtrTypeIterator.h
+++ llvm/include/llvm/IR/GetElementPtrTypeIterator.h
@@ -32,8 +32,6 @@ class generic_gep_type_iterator {
 
   ItTy OpIt;
   PointerUnion<StructType *, Type *> CurTy;
-  enum : uint64_t { Unbounded = -1ull };
-  uint64_t NumElements = Unbounded;
 
   generic_gep_type_iterator() = default;
 
@@ -79,16 +77,11 @@ public:
 
   generic_gep_type_iterator &operator++() { // Preincrement
     Type *Ty = getIndexedType();
-    if (auto *ATy = dyn_cast<ArrayType>(Ty)) {
+    if (auto *ATy = dyn_cast<ArrayType>(Ty))
       CurTy = ATy->getElementType();
-      NumElements = ATy->getNumElements();
-    } else if (auto *VTy = dyn_cast<VectorType>(Ty)) {
+    else if (auto *VTy = dyn_cast<VectorType>(Ty))
       CurTy = VTy->getElementType();
-      if (isa<ScalableVectorType>(VTy))
-        NumElements = Unbounded;
-      else
-        NumElements = cast<FixedVectorType>(VTy)->getNumElements();
-    } else
+    else
       CurTy = dyn_cast<StructType>(Ty);
     ++OpIt;
     return *this;
@@ -122,15 +115,6 @@ public:
 
   StructType *getStructTypeOrNull() const {
     return CurTy.dyn_cast<StructType *>();
-  }
-
-  bool isBoundedSequential() const {
-    return isSequential() && NumElements != Unbounded;
-  }
-
-  uint64_t getSequentialNumElements() const {
-    assert(isBoundedSequential());
-    return NumElements;
   }
 };
 
