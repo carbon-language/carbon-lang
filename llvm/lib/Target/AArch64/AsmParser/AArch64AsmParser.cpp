6732c4389786bf12e5adc2c9fba19fef2fe93c86
David Spickett <david.spickett@linaro.org>
2022-01-19 16:56:41 +0000

    [llvm][AArch64] Accept armv8.8 "hbc" and "mops" in .arch_extension directive
    Reviewed By: lenary
    
    Differential Revision: https://reviews.llvm.org/D117693

--- llvm/lib/Target/AArch64/AsmParser/AArch64AsmParser.cpp
+++ llvm/lib/Target/AArch64/AsmParser/AArch64AsmParser.cpp
@@ -3284,6 +3284,8 @@ static const struct Extension {
     {"sme", {AArch64::FeatureSME}},
     {"sme-f64", {AArch64::FeatureSMEF64}},
     {"sme-i64", {AArch64::FeatureSMEI64}},
+    {"hbc", {AArch64::FeatureHBC}},
+    {"mops", {AArch64::FeatureMOPS}},
     // FIXME: Unsupported extensions
     {"lor", {}},
     {"rdma", {}},
