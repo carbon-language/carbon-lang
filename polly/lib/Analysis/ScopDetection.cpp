8bc66189429919d86d3ff3b4b5a3f63cfa6c35ca
serge-sans-paille <sguelton@redhat.com>
2022-01-21 16:04:19 +0100

    Add missing llvm/support/Regex.h include in polly/lib/Analysis/ScopDetection.cpp

--- polly/lib/Analysis/ScopDetection.cpp
+++ polly/lib/Analysis/ScopDetection.cpp
@@ -78,6 +78,7 @@
 #include "llvm/InitializePasses.h"
 #include "llvm/Pass.h"
 #include "llvm/Support/Debug.h"
+#include "llvm/Support/Regex.h"
 #include "llvm/Support/raw_ostream.h"
 #include <algorithm>
 #include <cassert>
