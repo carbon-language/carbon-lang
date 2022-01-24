c5590396d041e77a84101cdcc4249788403e4e40
Qiu Chaofan <qiucofan@cn.ibm.com>
2022-01-24 15:23:28 +0800

    [PowerPC] Emit warning for ieeelongdouble on older GNU toolchain
    GCC 12 should have proper support for IEEE-754 compliant 128-bit
    floating point in libstdc++. So warning is needed when linking against
    older libstdc++ versions or LLVM libc++.
    
    Glibc starts supporting float128 in both header and libraries since
    2.32.
    
    Reviewed By: jsji
    
    Differential Revision: https://reviews.llvm.org/D112906

--- clang/lib/Driver/ToolChains/PPCLinux.h
+++ clang/lib/Driver/ToolChains/PPCLinux.h
@@ -18,12 +18,15 @@ namespace toolchains {
 class LLVM_LIBRARY_VISIBILITY PPCLinuxToolChain : public Linux {
 public:
   PPCLinuxToolChain(const Driver &D, const llvm::Triple &Triple,
-                    const llvm::opt::ArgList &Args)
-      : Linux(D, Triple, Args) {}
+                    const llvm::opt::ArgList &Args);
 
   void
   AddClangSystemIncludeArgs(const llvm::opt::ArgList &DriverArgs,
                             llvm::opt::ArgStringList &CC1Args) const override;
+
+private:
+  bool SupportIEEEFloat128(const Driver &D, const llvm::Triple &Triple,
+                           const llvm::opt::ArgList &Args) const;
 };
 
 } // end namespace toolchains
