a09be08594a8b980f3cd0ad7e8aea1f9545517ae
David Tenty <daltenty@ibm.com>
2022-01-24 23:20:18 -0500

    [compiler-rt][profile][AIX] pass extra link opts for test
    The AIX linker doesn't export any symbols by default, so an export list is usually used. Since clang doesn't have the tools to auto-generate an export list yet, just pass the linker an extra opt to tell it to export everything. This is  generally not recommended for real shared libs, but is fine for the purpose of this test.
    
    Differential Revision: https://reviews.llvm.org/D118101

--- compiler-rt/test/profile/Posix/lit.local.cfg.py
+++ compiler-rt/test/profile/Posix/lit.local.cfg.py
@@ -7,3 +7,10 @@ root = getRoot(config)
 
 if root.host_os in ['Windows']:
   config.unsupported = True
+
+# AIX usually usually makes use of an explicit export list when linking a shared
+# object, but for the purposes of these tests just export all symbols.
+if root.host_os in ['AIX']:
+  config.substitutions.append(('%shared_linker_xopts', '-Wl,-bexpfull'))
+else:
+  config.substitutions.append(('%shared_linker_xopts', ''))
