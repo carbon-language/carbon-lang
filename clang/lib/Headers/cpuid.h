f6984b299afcbeddefe53db7ee1ce62e5d68cc4f
Liu, Chen3 <chen3.liu@intel.com>
2022-01-25 14:08:58 +0800

    Fix the wrong value of bit_AVXVNNI
    Differential Revision: https://reviews.llvm.org/D118103

--- clang/lib/Headers/cpuid.h
+++ clang/lib/Headers/cpuid.h
@@ -200,7 +200,7 @@
 #define bit_AMXINT8       0x02000000
 
 /* Features in %eax for leaf 7 sub-leaf 1 */
-#define bit_AVXVNNI       0x00000008
+#define bit_AVXVNNI       0x00000010
 #define bit_AVX512BF16    0x00000020
 #define bit_HRESET        0x00400000
 
