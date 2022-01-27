98fa46f870e402f225b60ab5d02487c36d79632b
Zixu Wang <zixu_wang@apple.com>
2022-01-26 16:22:08 -0800

    [FIX][clang] Fix unused private field in ExtractAPIVisitor
    Fix a build failure where an unused private field in ExtractAPIVisitor
    triggered a warning turned into error.

--- clang/lib/Frontend/ExtractAPIConsumer.cpp
+++ clang/lib/Frontend/ExtractAPIConsumer.cpp
@@ -9,21 +9,14 @@ using namespace clang;
 namespace {
 class ExtractAPIVisitor : public RecursiveASTVisitor<ExtractAPIVisitor> {
 public:
-  explicit ExtractAPIVisitor(ASTContext *Context) : Context(Context) {}
-
   bool VisitNamedDecl(NamedDecl *Decl) {
     llvm::outs() << Decl->getName() << "\n";
     return true;
   }
-
-private:
-  ASTContext *Context;
 };
 
 class ExtractAPIConsumer : public ASTConsumer {
 public:
-  explicit ExtractAPIConsumer(ASTContext *Context) : Visitor(Context) {}
-
   void HandleTranslationUnit(ASTContext &Context) override {
     Visitor.TraverseDecl(Context.getTranslationUnitDecl());
   }
@@ -35,5 +28,5 @@ private:
 
 std::unique_ptr<ASTConsumer>
 ExtractAPIAction::CreateASTConsumer(CompilerInstance &CI, StringRef InFile) {
-  return std::make_unique<ExtractAPIConsumer>(&CI.getASTContext());
+  return std::make_unique<ExtractAPIConsumer>();
 }
