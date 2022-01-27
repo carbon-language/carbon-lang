f17123831e47073f7850ea17560bc1f756b65298
Arthur Eubanks <aeubanks@google.com>
2022-01-27 14:17:58 -0800

    [NFC][Clang][OpaquePtr] Move away from deprecated Address constructor in CreateTempAlloca()
    Specify the Address element type, which is the bitcast destination type.
    (the whole bitcast won't be needed after opaque pointers)

--- clang/lib/CodeGen/CGExpr.cpp
+++ clang/lib/CodeGen/CGExpr.cpp
@@ -150,7 +150,7 @@ Address CodeGenFunction::CreateMemTemp(QualType Ty, CharUnits Align,
 
     Result = Address(
         Builder.CreateBitCast(Result.getPointer(), VectorTy->getPointerTo()),
-        Result.getAlignment());
+        VectorTy, Result.getAlignment());
   }
   return Result;
 }
