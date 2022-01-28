746e435ff76a12468103a978d46f7b1a5052e867
Philip Reames <listmail@philipreames.com>
2022-01-28 12:07:39 -0800

    Revert "[SLP] Add a clarifying assert in block scheduling [NFC]"
    This reverts commit db49a78900f5e4b59714565876b5dbb5e2dfe840.  The reasoning in the patch applied to a downstream branch, and I got myself confused when trying to split apart pieces.  Thankfully, the assert was simply weaker than the actual invariant currently upstream which is that ReadyInsts is not empty.

--- llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
+++ llvm/lib/Transforms/Vectorize/SLPVectorizer.cpp
@@ -7668,11 +7668,6 @@ void BoUpSLP::scheduleBlock(BlockScheduling *BS) {
   }
   BS->initialFillReadyList(ReadyInsts);
 
-  assert((!ReadyInsts.empty() ||
-          (VectorizableTree.size() == 1 &&
-           VectorizableTree[0]->State == TreeEntry::NeedToGather)) &&
-         "why no ready insts from scheduleable tree?");
-
   Instruction *LastScheduledInst = BS->ScheduleEnd;
 
   // Do the "real" scheduling.
