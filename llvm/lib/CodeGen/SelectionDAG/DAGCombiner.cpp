5d089d9a832c24c008ba0ecc0e28e82a7ad42297
Cullen Rhodes <cullen.rhodes@arm.com>
2022-01-28 16:28:40 +0000

    [DAGCombiner] Fix invalid size request in combineRepeatedFPDivisors
    If we have a vector FP division with a splatted divisor, use
    getVectorMinNumElements when scaling the num of uses by splat factor.
    
    For AArch64 the combine kicks in for the <vscale x 4 x float> case since it's
    above the fdiv threshold (3) when scaling num uses by splat factor, but the
    codegen is worse (splat + vector fdiv + vector fmul) than the <vscale x 2 x
    double> case (splat + vector fdiv).
    
    If the combine could be converted into a scalar FP division by
    scalarizeBinOpOfSplats it may be cheaper, but it looks like this is predicated
    on the isExtractVecEltCheap TLI function which is implemented for x86 but not
    AArch64. Perhaps for now combineRepeatedFPDivisors should only scale num uses
    by splat if the division can be converted into scalar op.
    
    Reviewed By: sdesmalen
    
    Differential Revision: https://reviews.llvm.org/D118343

--- llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
+++ llvm/lib/CodeGen/SelectionDAG/DAGCombiner.cpp
@@ -14580,7 +14580,7 @@ SDValue DAGCombiner::combineRepeatedFPDivisors(SDNode *N) {
   unsigned NumElts = 1;
   EVT VT = N->getValueType(0);
   if (VT.isVector() && DAG.isSplatValue(N1))
-    NumElts = VT.getVectorNumElements();
+    NumElts = VT.getVectorMinNumElements();
 
   if (!MinUses || (N1->use_size() * NumElts) < MinUses)
     return SDValue();
