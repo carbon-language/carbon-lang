94e97e668c95385b4bddc52403f7486be65fe03f
Fangrui Song <i@maskray.me>
2022-01-29 16:20:40 -0800

    [ELF] Reorder InputSectionBase::parent. NFC
    Move it before others.

--- lld/ELF/InputSection.h
+++ lld/ELF/InputSection.h
@@ -119,6 +119,12 @@ public:
   // its static type.
   InputFile *file;
 
+  // Input sections are part of an output section. Special sections
+  // like .eh_frame and merge sections are first combined into a
+  // synthetic section that is then added to an output section. In all
+  // cases this points one level up.
+  SectionBase *parent = nullptr;
+
   // Section index of the relocation section if exists.
   uint32_t relSecIdx = 0;
 
@@ -158,12 +164,6 @@ public:
       uncompress();
     return rawData;
   }
-
-  // Input sections are part of an output section. Special sections
-  // like .eh_frame and merge sections are first combined into a
-  // synthetic section that is then added to an output section. In all
-  // cases this points one level up.
-  SectionBase *parent = nullptr;
 
   // The next member in the section group if this section is in a group. This is
   // used by --gc-sections.
