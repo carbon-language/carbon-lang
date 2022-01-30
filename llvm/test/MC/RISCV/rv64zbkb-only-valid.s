bb495810d9e3b5029d5a79273c9443ebc9023b7c
Craig Topper <craig.topper@sifive.com>
2022-01-30 14:07:35 -0800

    [RISCV] Merge rv64zbkb-valid.s and rv64zbkb-only-valid.s. NFC
    Based on the existing naming "only" tests are used for rv32 instructions
    that don't exist in rv64. rv32 tests without "only" are for instructions
    that are in both rv32 and rv64. The rv64 tests are for instructions
    that are only in rv64.
    
    Both of these test files have instruction encodings that are only
    valid in rv64 so they can be the same file.

--- llvm/test/MC/RISCV/rv64zbkb-only-valid.s
+++
@@ -1,9 +0,0 @@
-# RUN: llvm-mc %s -triple=riscv64 -mattr=+zbkb -show-encoding \
-# RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
-# RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+zbkb < %s \
-# RUN:     | llvm-objdump --mattr=+zbkb -d -r - \
-# RUN:     | FileCheck --check-prefix=CHECK-ASM-AND-OBJ %s
-
-# CHECK-ASM-AND-OBJ: rev8 t0, t1
-# CHECK-ASM: encoding: [0x93,0x52,0x83,0x6b]
-rev8 t0, t1
--- llvm/test/MC/RISCV/rv64zbkb-valid.s
+++ llvm/test/MC/RISCV/rv64zbkb-valid.s
@@ -1,6 +1,12 @@
+# RUN: llvm-mc %s -triple=riscv64 -mattr=+zbkb -show-encoding \
+# RUN:     | FileCheck -check-prefixes=CHECK-ASM,CHECK-ASM-AND-OBJ %s
 # RUN: llvm-mc -filetype=obj -triple=riscv64 -mattr=+zbkb < %s \
 # RUN:     | llvm-objdump --mattr=+zbkb -d -r - \
 # RUN:     | FileCheck --check-prefix=CHECK-ASM-AND-OBJ %s
+
+# CHECK-ASM-AND-OBJ: rev8 t0, t1
+# CHECK-ASM: encoding: [0x93,0x52,0x83,0x6b]
+rev8 t0, t1
 
 # CHECK-ASM-AND-OBJ: rorw t0, t1, t2
 # CHECK-ASM: encoding: [0xbb,0x52,0x73,0x60]
