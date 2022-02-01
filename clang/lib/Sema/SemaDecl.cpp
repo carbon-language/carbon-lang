275c56226d7fbd6a4d554807374f78d323aa0c1c
David Blaikie <dblaikie@gmail.com>
2022-01-31 17:32:31 -0800

    Disable -Wmissing-prototypes for internal linkage functions that aren't explicitly marked "static"
    Some functions can end up non-externally visible despite not being
    declared "static" or in an unnamed namespace in C++ - such as by having
    parameters that are of non-external types.
    
    Such functions aren't mistakenly intended to be defining some function
    that needs a declaration. They could be maybe more legible (except for
    the `operator new` example) with an explicit static, but that's a
    stylistic thing outside what should be addressed by a warning.

--- clang/lib/Sema/SemaDecl.cpp
+++ clang/lib/Sema/SemaDecl.cpp
@@ -14196,6 +14196,9 @@ ShouldWarnAboutMissingPrototype(const FunctionDecl *FD,
   if (!FD->isGlobal())
     return false;
 
+  if (!FD->isExternallyVisible())
+    return false;
+
   // Don't warn about C++ member functions.
   if (isa<CXXMethodDecl>(FD))
     return false;
