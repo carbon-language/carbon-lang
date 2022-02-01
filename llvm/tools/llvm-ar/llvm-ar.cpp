dd6e7e0d5722d6396f8a0e94146f0b5e50dfa028
Fangrui Song <i@maskray.me>
2022-02-01 09:56:50 -0800

    [llvm-ar] Add --thin for creating a thin archive
    In GNU ar (since 2008), the modifier 'T' means creating a thin archive.
    In many other ar implementations (FreeBSD, macOS, elfutils, etc), -T
    means "allow filename truncation of extracted files", as specified by
    X/Open System Interface.
    
    For portability, 'T' with thin archive semantics should be avoided.
    
    See https://sourceware.org/bugzilla/show_bug.cgi?id=28759 binutils 2.38
    will deprecate 'T' (without diagnostic) and add --thin.
    
    Reviewed By: jhenderson
    
    Differential Revision: https://reviews.llvm.org/D116979

--- llvm/tools/llvm-ar/llvm-ar.cpp
+++ llvm/tools/llvm-ar/llvm-ar.cpp
@@ -90,6 +90,7 @@ OPTIONS:
   --rsp-quoting         - quoting style for response files
     =posix              -   posix
     =windows            -   windows
+  --thin                - create a thin archive
   --version             - print the version and exit
   @<file>               - read options from <file>
 
@@ -118,7 +119,7 @@ MODIFIERS:
   [P] - use full names when matching (implied for thin archives)
   [s] - create an archive index (cf. ranlib)
   [S] - do not build a symbol table
-  [T] - create a thin archive
+  [T] - deprecated, use --thin instead
   [u] - update only [files] newer than archive contents
   [U] - use actual timestamps and uids/gids
   [v] - be verbose about actions taken
@@ -390,8 +391,6 @@ static ArchiveOperation parseCommandLine() {
       break;
     case 'T':
       Thin = true;
-      // Thin archives store path names, so P should be forced.
-      CompareFullPath = true;
       break;
     case 'L':
       AddLibrary = true;
@@ -406,6 +405,10 @@ static ArchiveOperation parseCommandLine() {
       badUsage(std::string("unknown option ") + Options[i]);
     }
   }
+
+  // Thin archives store path names, so P should be forced.
+  if (Thin)
+    CompareFullPath = true;
 
   // At this point, the next thing on the command line must be
   // the archive name.
@@ -1199,6 +1202,11 @@ static int ar_main(int argc, char **argv) {
 
     if (strcmp(*ArgIt, "-M") == 0) {
       MRI = true;
+      continue;
+    }
+
+    if (strcmp(*ArgIt, "--thin") == 0) {
+      Thin = true;
       continue;
     }
 
