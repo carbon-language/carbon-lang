997f2a56dea16a8b0a8827ca00cbaa21fbcd2338
Shoaib Meenai <smeenai@fb.com>
2022-02-01 23:59:50 -0800

    [ELF] Avoid wrapping unreferenced lazy symbols
    There's a couple of motivations here:
    * LLD 12 (which I was originally testing with) was adding an undefined
      symbol to the symbol table if you attempted to wrap an unreferenced
      lazy symbol, which would later break `--no-allow-shlib-undefined`. LLD
      on main actually produces a weak undefined symbol, so this doesn't
      break anyway, but it's cleaner to not have the weak undefined symbol
      as well. The new behavior also matches bfd and gold.
    * PROVIDE in a linker script referencing a wrapped symbol would think
      that an otherwise-unreferenced lazy symbol which was wrapped was
      actually referenced, and therefore proceed with the definition, which
      goes against expectations. The new behavior also matches bfd and gold.
    
    Reviewed By: MaskRay
    
    Differential Revision: https://reviews.llvm.org/D118756

--- lld/ELF/Driver.cpp
+++ lld/ELF/Driver.cpp
@@ -2047,7 +2047,11 @@ static std::vector<WrappedSymbol> addWrappedSymbols(opt::InputArgList &args) {
       continue;
 
     Symbol *sym = symtab->find(name);
-    if (!sym)
+    // Avoid wrapping symbols that are lazy and unreferenced at this point, to
+    // not create undefined references. The isUsedInRegularObj check handles the
+    // case of a weak reference, which we still want to wrap even though it
+    // doesn't cause lazy symbols to be extracted.
+    if (!sym || (sym->isLazy() && !sym->isUsedInRegularObj))
       continue;
 
     Symbol *real = addUnusedUndefined(saver().save("__real_" + name));
