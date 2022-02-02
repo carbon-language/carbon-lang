cf70f7ce8b502a9bec33f9ff7c804b4cbe499c0f
River Riddle <riddleriver@gmail.com>
2022-02-02 15:04:19 -0800

    [mlir] Remove dangling reference to std-bufferize which got removed

--- mlir/test/Integration/Dialect/SparseTensor/taco/tools/mlir_pytaco_utils.py
+++ mlir/test/Integration/Dialect/SparseTensor/taco/tools/mlir_pytaco_utils.py
@@ -161,7 +161,7 @@ def compile_and_build_engine(
       f"convert-scf-to-std,"
       f"func-bufferize,"
       f"arith-bufferize,"
-      f"builtin.func(tensor-bufferize,std-bufferize,finalizing-bufferize),"
+      f"builtin.func(tensor-bufferize,finalizing-bufferize),"
       f"convert-vector-to-llvm{{reassociate-fp-reductions=1 enable-index-optimizations=1}},"
       f"lower-affine,"
       f"convert-memref-to-llvm,"
