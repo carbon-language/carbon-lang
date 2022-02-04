f62a400cdf114ff97fee99d74234c889142a34e7
Nikita Popov <npopov@redhat.com>
2022-02-04 14:37:02 +0100

    [Statepoint] Determine return type from elementtype attribute
    Based on the LangRef change in D117890, this uses the elementtype
    attribute rather than the pointer element type to determine the
    statepoint callee function type, making statepoints compatible
    with opaque pointers.

--- llvm/include/llvm/IR/Statepoint.h
+++ llvm/include/llvm/IR/Statepoint.h
@@ -121,9 +121,9 @@ public:
   /// Return the type of the value returned by the call underlying the
   /// statepoint.
   Type *getActualReturnType() const {
-    auto *CalleeTy =
-        getActualCalledOperand()->getType()->getPointerElementType();
-    return cast<FunctionType>(CalleeTy)->getReturnType();
+    auto *FT = cast<FunctionType>(
+        getAttributes().getParamElementType(CalledFunctionPos));
+    return FT->getReturnType();
   }
 
 
