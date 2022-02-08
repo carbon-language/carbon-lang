4db88a54b6d4bd38fe38dbe57ec2a156ff3c144e
Saiyedul Islam <Saiyedul.Islam@amd.com>
2022-02-08 16:30:09 +0000

    [OpenMP][Clang] Move partial support of reverse offload to a future version
    OpenMP Spec 5.2 requires unimplemented requires clauses to produce
    compile time error termination. Moving current partial support of
    reverse_offload to a distant future version 9.9 so that existing
    code can be tested and maintained until a complete implementation
    is available.
    
    Reviewed By: ABataev
    
    Differential Revision: https://reviews.llvm.org/D119256

--- clang/test/OpenMP/target_ast_print.cpp
+++ clang/test/OpenMP/target_ast_print.cpp
@@ -342,7 +342,18 @@ int main (int argc, char **argv) {
 // RUN: %clang_cc1 -DOMP5 -verify -fopenmp-simd -fopenmp-version=50 -ast-print %s | FileCheck %s --check-prefix OMP5
 // RUN: %clang_cc1 -DOMP5 -fopenmp-simd -fopenmp-version=50 -x c++ -std=c++11 -emit-pch -o %t %s
 // RUN: %clang_cc1 -DOMP5 -fopenmp-simd -fopenmp-version=50 -std=c++11 -include-pch %t -fsyntax-only -verify %s -ast-print | FileCheck %s --check-prefix OMP5
+
+// RUN: %clang_cc1 -DOMP5 -verify -fopenmp -fopenmp-version=99 -DOMP99 -ast-print %s | FileCheck %s --check-prefixes=OMP5,REV
+// RUN: %clang_cc1 -DOMP5 -fopenmp -fopenmp-version=99 -DOMP99 -x c++ -std=c++11 -emit-pch -o %t %s
+// RUN: %clang_cc1 -DOMP5 -fopenmp -fopenmp-version=99 -DOMP99 -std=c++11 -include-pch %t -fsyntax-only -verify %s -ast-print | FileCheck %s --check-prefixes=OMP5,REV
+
+// RUN: %clang_cc1 -DOMP5 -verify -fopenmp-simd -fopenmp-version=99 -DOMP99 -ast-print %s | FileCheck %s --check-prefixes=OMP5,REV
+// RUN: %clang_cc1 -DOMP5 -fopenmp-simd -fopenmp-version=99 -DOMP99 -x c++ -std=c++11 -emit-pch -o %t %s
+// RUN: %clang_cc1 -DOMP5 -fopenmp-simd -fopenmp-version=99 -DOMP99 -std=c++11 -include-pch %t -fsyntax-only -verify %s -ast-print | FileCheck %s --check-prefixes=OMP5,REV
+
+#ifdef OMP99
 #pragma omp requires reverse_offload
+#endif
 typedef void **omp_allocator_handle_t;
 extern const omp_allocator_handle_t omp_null_allocator;
 extern const omp_allocator_handle_t omp_default_mem_alloc;
@@ -370,8 +381,10 @@ T tmain(T argc, T *argv) {
   foo();
 #pragma omp target if (target:argc > 0) device(device_num: C)
   foo();
+#ifdef OMP99
 #pragma omp target if (C) device(ancestor: argc)
   foo();
+#endif
 #pragma omp target map(i)
   foo();
 #pragma omp target map(a[0:10], i)
@@ -475,8 +488,8 @@ T tmain(T argc, T *argv) {
 // OMP5-NEXT: foo();
 // OMP5-NEXT: #pragma omp target if(target: argc > 0) device(device_num: C)
 // OMP5-NEXT: foo()
-// OMP5-NEXT: #pragma omp target if(C) device(ancestor: argc)
-// OMP5-NEXT: foo()
+// REV: #pragma omp target if(C) device(ancestor: argc)
+// REV-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: i)
 // OMP5-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: a[0:10],i)
@@ -571,8 +584,8 @@ T tmain(T argc, T *argv) {
 // OMP5-NEXT: foo();
 // OMP5-NEXT: #pragma omp target if(target: argc > 0)
 // OMP5-NEXT: foo()
-// OMP5-NEXT: #pragma omp target if(5)
-// OMP5-NEXT: foo()
+// REV: #pragma omp target if(5)
+// REV-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: i)
 // OMP5-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: a[0:10],i)
@@ -667,8 +680,8 @@ T tmain(T argc, T *argv) {
 // OMP5-NEXT: foo();
 // OMP5-NEXT: #pragma omp target if(target: argc > 0) device(device_num: 1)
 // OMP5-NEXT: foo()
-// OMP5-NEXT: #pragma omp target if(1) device(ancestor: argc)
-// OMP5-NEXT: foo()
+// REV: #pragma omp target if(1) device(ancestor: argc)
+// REV-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: i)
 // OMP5-NEXT: foo()
 // OMP5-NEXT: #pragma omp target map(tofrom: a[0:10],i)
