13ac0392debf74875666207b98c114a359dbe918
Nikita Popov <npopov@redhat.com>
2022-02-08 09:33:55 +0100

    [Bitcode] Add missing includes in llvm-dis-fuzzer
    Probably missed as part of some recent header cleanup.

--- llvm/tools/llvm-dis-fuzzer/llvm-dis-fuzzer.cpp
+++ llvm/tools/llvm-dis-fuzzer/llvm-dis-fuzzer.cpp
@@ -11,6 +11,8 @@
 //===----------------------------------------------------------------------===//
 
 #include "llvm/Bitcode/BitcodeReader.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
 #include "llvm/Support/MemoryBuffer.h"
 
 using namespace llvm;
