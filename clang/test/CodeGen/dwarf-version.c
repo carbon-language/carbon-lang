6cf64b2d2858dc93c3ec93438bdaca2807847e9b
Martin Storsjö <martin@martin.st>
2022-02-09 15:15:25 +0200

    [clang] [MinGW] Default to DWARF 4
    Neither LLDB nor GDB seem to work with DWARF 5 debug information on
    Windows right now.
    
    This applies the same change as in
    9c6272861032f511a23784ce0c5cc8f6ac2f625b (Default to DWARFv4 on Windows)
    to the MinGW driver too.
    
    Differential Revision: https://reviews.llvm.org/D119326

--- clang/test/CodeGen/dwarf-version.c
+++ clang/test/CodeGen/dwarf-version.c
@@ -32,6 +32,11 @@
 //     Explicitly request both.
 // RUN: %clang -target i686-pc-windows-msvc -gdwarf -gcodeview -S -emit-llvm -o - %s \
 // RUN:     | FileCheck %s --check-prefixes=VER4,CODEVIEW
+
+// Check what version of dwarf is used for MinGW targets.
+// RUN: %clang -target i686-pc-windows-gnu -g -S -emit-llvm -o - %s | \
+// RUN:   FileCheck %s --check-prefixes=VER4
+
 // RUN: %clang -target powerpc-ibm-aix-xcoff -g -S -emit-llvm -o - %s | \
 // RUN:   FileCheck %s --check-prefix=VER3
 // RUN: %clang -target powerpc-ibm-aix-xcoff -gdwarf-2 -S -emit-llvm -o - %s | \
