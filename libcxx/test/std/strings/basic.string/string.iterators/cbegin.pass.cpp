c515b652a082bc7927fffbd6e6d40e4b82b7c60c
Nikolas Klauser <nikolasklauser@berlin.de>
2022-02-09 02:03:27 +0100

    [libc++] Prepare string.{contains, ends_with, iterators, require, starts_with} tests for constexpr
    Reviewed By: ldionne, #libc
    
    Spies: libcxx-commits
    
    Differential Revision: https://reviews.llvm.org/D119304

--- libcxx/test/std/strings/basic.string/string.iterators/cbegin.pass.cpp
+++ libcxx/test/std/strings/basic.string/string.iterators/cbegin.pass.cpp
@@ -17,7 +17,7 @@
 #include "min_allocator.h"
 
 template <class S>
-void
+TEST_CONSTEXPR_CXX20 void
 test(const S& s)
 {
     typename S::const_iterator cb = s.cbegin();
@@ -28,19 +28,28 @@ test(const S& s)
     assert(cb == s.begin());
 }
 
-int main(int, char**)
-{
-    {
+bool test() {
+  {
     typedef std::string S;
     test(S());
     test(S("123"));
-    }
+  }
 #if TEST_STD_VER >= 11
-    {
+  {
     typedef std::basic_string<char, std::char_traits<char>, min_allocator<char>> S;
     test(S());
     test(S("123"));
-    }
+  }
+#endif
+
+  return true;
+}
+
+int main(int, char**)
+{
+  test();
+#if TEST_STD_VER > 17
+  // static_assert(test());
 #endif
 
   return 0;
