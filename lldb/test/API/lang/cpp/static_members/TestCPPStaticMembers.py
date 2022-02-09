e0f2375b5262d0dd778ecaf0628f905d241da733
Andy Yankovsky <weratt@gmail.com>
2022-02-09 20:40:50 +0100

    [lldb] Disable failing test on Windows
    Test was introduced in https://reviews.llvm.org/D113498.

--- lldb/test/API/lang/cpp/static_members/TestCPPStaticMembers.py
+++ lldb/test/API/lang/cpp/static_members/TestCPPStaticMembers.py
@@ -42,6 +42,8 @@ class TestCase(TestBase):
         self.expect("expression s_c", error=True,
                     startstr="error: use of undeclared identifier 's_d'")
 
+    # We fail to lookup static members on Windows.
+    @expectedFailureAll(oslist=["windows"])
     def test_no_crash_in_IR_arithmetic(self):
         """
         Test that LLDB doesn't crash on evaluating specific expression involving
