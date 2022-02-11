e5739368b36693a8932c466f1c5b1b66f1faf744
Paul Kirth <paulkirth@google.com>
2022-02-11 22:53:19 +0000

    [sanitizers] Fix missing header for mac builds
    Differential Revision: https://reviews.llvm.org/D119598

--- compiler-rt/lib/sanitizer_common/sanitizer_mac.cpp
+++ compiler-rt/lib/sanitizer_common/sanitizer_mac.cpp
@@ -25,6 +25,7 @@
 #include "sanitizer_common.h"
 #include "sanitizer_file.h"
 #include "sanitizer_flags.h"
+#include "sanitizer_interface_internal.h"
 #include "sanitizer_internal_defs.h"
 #include "sanitizer_libc.h"
 #include "sanitizer_platform_limits_posix.h"
