1ec089763496821973333649546aa0ef28a9bf39
jonmeow <46229924+jonmeow@users.noreply.github.com>
2022-02-11 00:56:31 +0000

    buildifier and utils

---
+++ scripts/buildifier.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+"""Runs buildifier on passed-in BUILD files, mainly for pre-commit."""
+
+__copyright__ = """
+Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+Exceptions. See /LICENSE for license information.
+SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""
+
+import subprocess
+import sys
+
+import utils
+
+
+def main() -> None:
+    buildifier = utils.cache_release_file(utils.Release.BUILDIFIER)
+    subprocess.check_call([buildifier] + sys.argv[1:])
+
+
+if __name__ == "__main__":
+    main()
