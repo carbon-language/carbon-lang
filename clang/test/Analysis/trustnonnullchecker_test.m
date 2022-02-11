ce420820c815e806bab9c5f17cb3b829a616548a
Rashmi Mudduluru <rashmi@rashmis-mbp.lan>
2022-02-10 18:49:34 -0800

    [Analyzer] Re-enables trustnonnullchecker_test.m
    Differential review: https://reviews.llvm.org/D119270

--- clang/test/Analysis/trustnonnullchecker_test.m
+++ clang/test/Analysis/trustnonnullchecker_test.m
@@ -1,6 +1,3 @@
-// Temporarily disabling the test, it failes the "system is over-constrained"
-// assertion in *non* optimized builds.
-// REQUIRES: rdar44992170
 // RUN: %clang_analyze_cc1 -fblocks -analyze -analyzer-checker=core,nullability,apiModeling,debug.ExprInspection  -verify %s
 
 #include "Inputs/system-header-simulator-for-nullability.h"
