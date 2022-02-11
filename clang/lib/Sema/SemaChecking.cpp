bfb1a33bec7c88170b0809b8a7c9cb860e7cc19b
Anton Zabaznov <anton.zabaznov@intel.com>
2022-02-11 15:54:55 +0300

    [OpenCL] Adjust diagnostic for subgroup support.
    OpenCL C 3.0 __opencl_c_subgroups feature is slightly different
    then other equivalent features and extensions (fp64 and 3d image writes):
    OpenCL C 3.0 device can support the extension but not the feature.
    cl_khr_subgroups requires subgroup independent forward progress.
    
    This patch adjusts the check which is used when translating language
    builtins to check either the extension or feature is supported.
    
    Reviewed By: Anastasia
    
    Differential Revision: https://reviews.llvm.org/D118999

--- clang/lib/Sema/SemaChecking.cpp
+++ clang/lib/Sema/SemaChecking.cpp
@@ -1042,9 +1042,15 @@ static bool checkOpenCLBlockArgs(Sema &S, Expr *BlockArg) {
 }
 
 static bool checkOpenCLSubgroupExt(Sema &S, CallExpr *Call) {
-  if (!S.getOpenCLOptions().isSupported("cl_khr_subgroups", S.getLangOpts())) {
+  // OpenCL device can support extension but not the feature as extension
+  // requires subgroup independent forward progress, but subgroup independent
+  // forward progress is optional in OpenCL C 3.0 __opencl_c_subgroups feature.
+  if (!S.getOpenCLOptions().isSupported("cl_khr_subgroups", S.getLangOpts()) &&
+      !S.getOpenCLOptions().isSupported("__opencl_c_subgroups",
+                                        S.getLangOpts())) {
     S.Diag(Call->getBeginLoc(), diag::err_opencl_requires_extension)
-        << 1 << Call->getDirectCallee() << "cl_khr_subgroups";
+        << 1 << Call->getDirectCallee()
+        << "cl_khr_subgroups or __opencl_c_subgroups";
     return true;
   }
   return false;
