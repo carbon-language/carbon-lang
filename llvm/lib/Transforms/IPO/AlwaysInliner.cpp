1be1fd735d5abd669453c0ebc5d5fa780ceeb9c9
Dávid Bolvanský <david.bolvansky@gmail.com>
2022-02-14 09:32:41 +0100

    [AlwaysInliner] Check for callsite noinline attribute simplified

--- llvm/lib/Transforms/IPO/AlwaysInliner.cpp
+++ llvm/lib/Transforms/IPO/AlwaysInliner.cpp
@@ -59,16 +59,10 @@ PreservedAnalyses AlwaysInlinerPass::run(Module &M,
 
       for (User *U : F.users())
         if (auto *CB = dyn_cast<CallBase>(U))
-          if (CB->getCalledFunction() == &F) {
-            if (F.hasFnAttribute(Attribute::AlwaysInline)) {
-              // Avoid inlining if noinline call site attribute.
-              if (!CB->isNoInline())
-                Calls.insert(CB);
-            } else if (CB->hasFnAttr(Attribute::AlwaysInline)) {
-              // Ok, alwaysinline call site attribute.
+          if (CB->getCalledFunction() == &F &&
+                CB->hasFnAttr(Attribute::AlwaysInline) &&
+                !CB->getAttributes().hasFnAttr(Attribute::NoInline))
               Calls.insert(CB);
-            }
-          }
 
       for (CallBase *CB : Calls) {
         Function *Caller = CB->getCaller();
