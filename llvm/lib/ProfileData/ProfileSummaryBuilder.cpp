3940f1e2372c69e04f50f86bcfc1182a1549ec32
minglotus-6 <mingmingl@google.com>
2022-02-14 23:18:18 -0800

    [ProfData] Change type of options from int to uint64_t.
    - Reader uses option values to override uint64_t values.
    
    Differential Revision: https://reviews.llvm.org/D119810

--- llvm/lib/ProfileData/ProfileSummaryBuilder.cpp
+++ llvm/lib/ProfileData/ProfileSummaryBuilder.cpp
@@ -59,12 +59,12 @@ cl::opt<unsigned> ProfileSummaryLargeWorkingSetSizeThreshold(
 
 // The next two options override the counts derived from summary computation and
 // are useful for debugging purposes.
-cl::opt<int> ProfileSummaryHotCount(
+cl::opt<uint64_t> ProfileSummaryHotCount(
     "profile-summary-hot-count", cl::ReallyHidden, cl::ZeroOrMore,
     cl::desc("A fixed hot count that overrides the count derived from"
              " profile-summary-cutoff-hot"));
 
-cl::opt<int> ProfileSummaryColdCount(
+cl::opt<uint64_t> ProfileSummaryColdCount(
     "profile-summary-cold-count", cl::ReallyHidden, cl::ZeroOrMore,
     cl::desc("A fixed cold count that overrides the count derived from"
              " profile-summary-cutoff-cold"));
