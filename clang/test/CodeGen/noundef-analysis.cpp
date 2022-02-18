5333447a00ff568e5029483e1b46041ad1c9788f
hyeongyukim <gusrb406@snu.ac.kr>
2022-02-18 17:38:50 +0900

    [NFC] Fix a buildbot failure after b529744

--- clang/test/CodeGen/noundef-analysis.cpp
+++ clang/test/CodeGen/noundef-analysis.cpp
@@ -19,12 +19,12 @@ static int sink;
 static void examineValue(int x) { sink = x; }
 
 // ENABLED-LABEL: @main(
-// ENABLED:    [[CALL:%.*]] = call noundef i32 @_Z19indirect_callee_inti(i32 noundef 0)
+// ENABLED:    [[CALL:%.*]] = call noundef {{.*}}i32 @_Z19indirect_callee_inti(i32 noundef {{.*}}0)
 // ENABLED:    [[CALL1:%.*]] = call i32 @_Z21indirect_callee_union2u1(i32 {{.*}})
 // ENABLED:    [[CALL2:%.*]] = call noalias noundef nonnull i8* @_Znwm(i64 noundef 4) #[[ATTR4:[0-9]+]]
 // ENABLED:    call void @_ZL12examineValuei(i32 noundef {{.*}})
 // DISABLED-LABEL: @main(
-// DISABLED:    [[CALL:%.*]] = call i32 @_Z19indirect_callee_inti(i32 0)
+// DISABLED:    [[CALL:%.*]] = call {{.*}}i32 @_Z19indirect_callee_inti(i32 {{.*}}0)
 // DISABLED:    [[CALL1:%.*]] = call i32 @_Z21indirect_callee_union2u1(i32 {{.*}})
 // DISABLED:    [[CALL2:%.*]] = call noalias nonnull i8* @_Znwm(i64 4) #[[ATTR4:[0-9]+]]
 // DISABLED:    call void @_ZL12examineValuei(i32 {{.*}})
