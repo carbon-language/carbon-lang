4729a72acec31b46e167b16f50efacab9d16da9c
Michael Forster <forster@google.com>
2022-02-25 13:07:49 +0000

    Revert "[lldb/test] Fix TestProgressReporting.py race issue with the event listener"
    This reverts commit 3e3e79a9e4c378b59f5f393f556e6a84edcd8898.
    
    MemorySanitizer: use-of-uninitialized-value

--- lldb/test/API/functionalities/progress_reporting/TestProgressReporting.py
+++ lldb/test/API/functionalities/progress_reporting/TestProgressReporting.py
@@ -17,42 +17,41 @@ class TestProgressReporting(TestBase):
         TestBase.setUp(self)
         self.progress_events = []
 
-    def fetch_events(self):
+    def fetch_events(self, test_broadcaster):
+        listener = lldb.SBListener("lldb.progress.listener")
+        listener.StartListeningForEvents(test_broadcaster,
+                                         self.eBroadcastBitStopProgressThread)
+
+        progress_broadcaster = self.dbg.GetBroadcaster()
+        progress_broadcaster.AddListener(listener, lldb.SBDebugger.eBroadcastBitProgress)
+
         event = lldb.SBEvent()
 
         done = False
         while not done:
-            if self.listener.WaitForEvent(1, event):
+            if listener.WaitForEvent(1, event):
                 event_mask = event.GetType();
-                if event.BroadcasterMatchesRef(self.test_broadcaster):
+                if event.BroadcasterMatchesRef(test_broadcaster):
                     if event_mask & self.eBroadcastBitStopProgressThread:
                         done = True;
-                elif event.BroadcasterMatchesRef(self.progress_broadcaster):
-                    ret_args = lldb.SBDebugger().GetProgressFromEvent(event);
-                    self.assertGreater(len(ret_args), 1)
-
-                    message = ret_args[0]
+                elif event.BroadcasterMatchesRef(progress_broadcaster):
+                    message = lldb.SBDebugger().GetProgressFromEvent(event, 0, 0, 0, False);
                     if message:
                         self.progress_events.append((message, event))
 
+    @skipUnlessDarwin
     def test_dwarf_symbol_loading_progress_report(self):
         """Test that we are able to fetch dwarf symbol loading progress events"""
         self.build()
 
-        self.listener = lldb.SBListener("lldb.progress.listener")
-        self.test_broadcaster = lldb.SBBroadcaster('lldb.broadcaster.test')
-        self.listener.StartListeningForEvents(self.test_broadcaster,
-                                              self.eBroadcastBitStopProgressThread)
-
-        self.progress_broadcaster = self.dbg.GetBroadcaster()
-        self.progress_broadcaster.AddListener(self.listener, lldb.SBDebugger.eBroadcastBitProgress)
-
-        listener_thread = threading.Thread(target=self.fetch_events)
+        test_broadcaster = lldb.SBBroadcaster('lldb.broadcaster.test')
+        listener_thread = threading.Thread(target=self.fetch_events,
+                                           args=[test_broadcaster])
         listener_thread.start()
 
         lldbutil.run_to_source_breakpoint(self, 'break here', lldb.SBFileSpec('main.c'))
 
-        self.test_broadcaster.BroadcastEventByType(self.eBroadcastBitStopProgressThread)
+        test_broadcaster.BroadcastEventByType(self.eBroadcastBitStopProgressThread)
         listener_thread.join()
 
-        self.assertGreater(len(self.progress_events), 0)
+        self.assertTrue(len(self.progress_events) > 0)
