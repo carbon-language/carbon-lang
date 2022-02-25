54b909de682bfa4e3389b680b0916ab18c99952a
Amanieu d'Antras <amanieu@gmail.com>
2022-02-25 22:06:47 +0000

    [Mangler] Mangle aliases to fastcall/vectorcall functions correctly
    These aliases are produced by MergeFunctions and need to be mangled according to the calling convention of the function they are pointing to instead of defaulting to the C calling convention.
    
    Reviewed By: rnk
    
    Differential Revision: https://reviews.llvm.org/D120382

--- llvm/lib/IR/Mangler.cpp
+++ llvm/lib/IR/Mangler.cpp
@@ -144,7 +144,7 @@ void Mangler::getNameWithPrefix(raw_ostream &OS, const GlobalValue *GV,
 
   // Mangle functions with Microsoft calling conventions specially.  Only do
   // this mangling for x86_64 vectorcall and 32-bit x86.
-  const Function *MSFunc = dyn_cast<Function>(GV);
+  const Function *MSFunc = dyn_cast_or_null<Function>(GV->getAliaseeObject());
 
   // Don't add byte count suffixes when '\01' or '?' are in the first
   // character.
