1c8366f9f2aa436538949aa2b76f96980bb0de27
Stephen Tozer <stephen.tozer@sony.com>
2022-03-01 13:20:51 +0000

    [Dexter] Collate penalties of the same type into a single line for each
    Currently in Dexter, every step at which a DexExpectWatchValue/Type does
    not have the correct value is printed on a separate line. This patch
    reduces the size of the text output by instead printing each incorrect
    result (i.e. each incorrect value seen, 'Variable optimized out', and so
    on) on its own line, alongside a list of the steps at which that result
    was seen. This makes for much less spam in the output when watches are
    missing or wrong for many steps.
    
    Differential Revision: https://reviews.llvm.org/D120716

--- cross-project-tests/debuginfo-tests/dexter/feature_tests/subtools/test/address_printing.cpp
+++ cross-project-tests/debuginfo-tests/dexter/feature_tests/subtools/test/address_printing.cpp
@@ -30,8 +30,8 @@
 // CHECK-NEXT: address 'x_2' (0x[[X2_VAL]])
 // CHECK-NEXT: address 'y' (0x[[Y_VAL]])
 // CHECK: misordered result:
-// CHECK-NEXT: step 4 (0x[[Y_VAL]])
-// CHECK-NEXT: step 5 (0x[[X2_VAL]])
+// CHECK-NEXT: (0x[[Y_VAL]]): step 4
+// CHECK-NEXT: (0x[[X2_VAL]]): step 5
 
 int main() {
     int *x = new int(5);
