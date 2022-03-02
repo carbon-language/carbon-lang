0bd0f48e95413fa69d3b1cae1a9879247d89a0a5
jonmeow <46229924+jonmeow@users.noreply.github.com>
2022-03-02 22:16:48 +0000

    Do MOCK_METHOD TODO

--- toolchain/diagnostics/mocks.h
+++ toolchain/diagnostics/mocks.h
@@ -13,8 +13,7 @@ namespace Carbon::Testing {
 
 class MockDiagnosticConsumer : public DiagnosticConsumer {
  public:
-  // TODO: Use `MOCK_METHOD` once it's available.
-  MOCK_METHOD1(HandleDiagnostic, void(const Diagnostic& diagnostic));
+  MOCK_METHOD(void, HandleDiagnostic, (const Diagnostic& diagnostic));
 };
 
 // Matcher `DiagnosticAt` matches the location of a diagnostic.
