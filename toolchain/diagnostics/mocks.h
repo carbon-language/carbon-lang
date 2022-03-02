40db06b26bbebcc214c116284a98bbbf31c595e9
jonmeow <46229924+jonmeow@users.noreply.github.com>
2022-03-02 22:31:54 +0000

    Add override

--- toolchain/diagnostics/mocks.h
+++ toolchain/diagnostics/mocks.h
@@ -13,7 +13,8 @@ namespace Carbon::Testing {
 
 class MockDiagnosticConsumer : public DiagnosticConsumer {
  public:
-  MOCK_METHOD(void, HandleDiagnostic, (const Diagnostic& diagnostic));
+  MOCK_METHOD(void, HandleDiagnostic, (const Diagnostic& diagnostic),
+              (override));
 };
 
 // Matcher `DiagnosticAt` matches the location of a diagnostic.
