e8784289c0b5b1af6b9828ba0ce76c7c061d170c
Pavel Labath <pavel@labath.sk>
2022-03-02 15:44:22 +0100

    Revert "Remove a top-level "using namespace" from TargetTransformInfoImpl.h"
    Causing failures on many bots.
    
    This reverts commit 31efecfde988a55f70aea97fbc59681f28ef2652.

--- llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
+++ llvm/include/llvm/Analysis/TargetTransformInfoImpl.h
@@ -24,6 +24,8 @@
 #include "llvm/IR/PatternMatch.h"
 #include <utility>
 
+using namespace llvm::PatternMatch;
+
 namespace llvm {
 
 class Function;
@@ -958,8 +960,6 @@ public:
 
   InstructionCost getUserCost(const User *U, ArrayRef<const Value *> Operands,
                               TTI::TargetCostKind CostKind) {
-    using namespace llvm::PatternMatch;
-
     auto *TargetTTI = static_cast<T *>(this);
     // Handle non-intrinsic calls, invokes, and callbr.
     // FIXME: Unlikely to be true for anything but CodeSize.
