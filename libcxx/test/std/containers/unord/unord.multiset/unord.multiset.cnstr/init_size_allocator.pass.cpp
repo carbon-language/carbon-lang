6cefc30a7fc83b91889aac631382f115e230b75d
Ruslan Arutyunyan <ruslan.arutyunyan@intel.com>
2022-03-03 14:48:54 +0300

    [libcxx][test][NFC] Add tests for C++14 constructors of unordered sets
    Add tests for C++14 constructors in unordered_set and unordered_multiset:
    
    unordered_[multi]set(size_type sz, const allocator_type& alloc);
    unordered_[multi]set(size_type sz, const hasher& hash, const allocator_type& alloc);
    
    template <class InputIt>
    unordered_[multi]set(InputIt first, InputIt last, size_type sz, const allocator_type& alloc);
    
    template <class InputIt>
    unordered_[multi]set(InputIt first, InputIt last, size_type sz, const hasher& hash, const allocator_type& alloc);
    
    unordered_[multi]set(initializer_list<value_type> init, size_type sz, const allocator_type& alloc);
    unordered_[multi]set(initializer_list<value_type> init, size_type sz, const hasher& hash, const allocator_type& alloc);
    
    Reviewed By: ldionne, Quuxplusone, rarutyun, #libc
    
    Differential Revision: https://reviews.llvm.org/D119243

---
+++ libcxx/test/std/containers/unord/unord.multiset/unord.multiset.cnstr/init_size_allocator.pass.cpp
@@ -0,0 +1,72 @@
+//===----------------------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+
+// UNSUPPORTED: c++03, c++11
+
+// <unordered_set>
+
+// template <class Value, class Hash = hash<Value>, class Pred = equal_to<Value>,
+//           class Alloc = allocator<Value>>
+// class unordered_multiset
+
+// unordered_multiset(initializer_list<value_type> init, size_type n, const allocator_type& alloc);
+
+#include <unordered_set>
+#include <cassert>
+#include <cfloat>
+#include <cmath>
+#include <cstddef>
+#include <iterator>
+
+#include "test_macros.h"
+#include "../../../test_compare.h"
+#include "../../../test_hash.h"
+#include "test_allocator.h"
+#include "min_allocator.h"
+
+template <class Allocator>
+void test(const Allocator& alloc)
+{
+    typedef std::unordered_multiset<int,
+                                    test_hash<int>,
+                                    test_equal_to<int>,
+                                    Allocator> C;
+    C c({
+            1,
+            2,
+            3,
+            4,
+            1,
+            2
+        },
+        7,
+        alloc);
+
+    LIBCPP_ASSERT(c.bucket_count() == 7);
+    assert(c.size() == 6);
+    assert(c.count(1) == 2);
+    assert(c.count(2) == 2);
+    assert(c.count(3) == 1);
+    assert(c.count(4) == 1);
+    assert(c.hash_function() == test_hash<int>());
+    assert(c.key_eq() == test_equal_to<int>());
+    assert(c.get_allocator() == alloc);
+    assert(!c.empty());
+    assert(static_cast<std::size_t>(std::distance(c.begin(), c.end())) == c.size());
+    assert(std::fabs(c.load_factor() - (float)c.size()/c.bucket_count()) < FLT_EPSILON);
+    assert(c.max_load_factor() == 1);
+}
+
+int main(int, char**)
+{
+    test(test_allocator<int>(10));
+    test(min_allocator<int>());
+    test(explicit_allocator<int>());
+
+    return 0;
+}
