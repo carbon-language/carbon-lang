53abe3ff66a54117308352d379837c7d3229f8d6
Nikita Popov <npopov@redhat.com>
2022-03-04 11:13:40 +0100

    [MergeICmp] Make instruction move robust against empty block (NFCI)
    Use the overload that support moving into an empty block. I don't
    think that this situation can occur right now, but it can happen
    with the change from e7fb1c15cb85d748c1c4fdd5a2eb5613ec7bef1d,
    and the test is derived from the issue reported there.

--- llvm/lib/Transforms/Scalar/MergeICmps.cpp
+++ llvm/lib/Transforms/Scalar/MergeICmps.cpp
@@ -270,9 +270,8 @@ void BCECmpBlock::split(BasicBlock *NewParent, AliasAnalysis &AA) const {
   }
 
   // Do the actual spliting.
-  for (Instruction *Inst : reverse(OtherInsts)) {
-    Inst->moveBefore(&*NewParent->begin());
-  }
+  for (Instruction *Inst : reverse(OtherInsts))
+    Inst->moveBefore(*NewParent, NewParent->begin());
 }
 
 bool BCECmpBlock::canSplit(AliasAnalysis &AA) const {
