a0b4aaffac9d1206be67284ed6d17a9297225c06
River Riddle <riddleriver@gmail.com>
2022-03-04 19:45:00 -0800

    [mlir][NFC] Remove FuncOp overload of NestedPattern::match
    This method is redundant with the Operation* overload, and is an artifact of when
    FuncOp wasn't an operation.

--- mlir/include/mlir/Dialect/Affine/Analysis/NestedMatcher.h
+++ mlir/include/mlir/Dialect/Affine/Analysis/NestedMatcher.h
@@ -103,11 +103,6 @@ public:
     freeNested();
   }
 
-  /// Returns all the top-level matches in `func`.
-  void match(FuncOp func, SmallVectorImpl<NestedMatch> *matches) {
-    func.walk([&](Operation *op) { matchOne(op, matches); });
-  }
-
   /// Returns all the top-level matches in `op`.
   void match(Operation *op, SmallVectorImpl<NestedMatch> *matches) {
     op->walk([&](Operation *child) { matchOne(child, matches); });
