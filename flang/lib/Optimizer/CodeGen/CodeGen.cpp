3b7ec85a1e2cd3ba581e88543986fa5031f270b7
Jean Perier <jperier@nvidia.com>
2022-03-09 09:41:55 +0100

    [flang] Use unix logical representation for fir.logical
    The front-end and the runtime are currently using the unix logical
    representation, but lowering was not. These inconsistencies could
    caused issues.
    
    The only place that defines what the logical representation is in
    lowering is the translation from FIR to LLVM (FIR is agnostic to the
    actual representation). More precisely, the LLVM implementation of
    `fir.convert` between `i1` and `fir.logcial` is what defines the
    representation:
    - `fir.convert` from `i1` to `fir.logical` defines the `.true.` and `.false.`
    canonical representations
    - `fir.convert` from `fir.logical` to `i1` decides what the test for
    truth is.
    
    Unix representation is:
    - .true. canonical integer representation is 1
    - .false. canonical integer representation is 0
    - the test for truth is "integer representation != 0"
    
    For the record, the previous representation that was used was in
    codegen was:
    - .true. canonical integer representation is -1 (all bits 1)
    - .false. canonical integer representation is 0
    - the test for truth is "integer representation lowest bit == 1"
    
    Differential Revision: https://reviews.llvm.org/D121200

--- flang/lib/Optimizer/CodeGen/CodeGen.cpp
+++ flang/lib/Optimizer/CodeGen/CodeGen.cpp
@@ -727,8 +727,10 @@ struct ConvertOpConversion : public FIROpConversion<fir::ConvertOp> {
   mlir::LogicalResult
   matchAndRewrite(fir::ConvertOp convert, OpAdaptor adaptor,
                   mlir::ConversionPatternRewriter &rewriter) const override {
-    auto fromTy = convertType(convert.getValue().getType());
-    auto toTy = convertType(convert.getRes().getType());
+    auto fromFirTy = convert.getValue().getType();
+    auto toFirTy = convert.getRes().getType();
+    auto fromTy = convertType(fromFirTy);
+    auto toTy = convertType(toFirTy);
     mlir::Value op0 = adaptor.getOperands()[0];
     if (fromTy == toTy) {
       rewriter.replaceOp(convert, op0);
@@ -750,8 +752,7 @@ struct ConvertOpConversion : public FIROpConversion<fir::ConvertOp> {
       return rewriter.create<mlir::LLVM::FPExtOp>(loc, toTy, val);
     };
     // Complex to complex conversion.
-    if (fir::isa_complex(convert.getValue().getType()) &&
-        fir::isa_complex(convert.getRes().getType())) {
+    if (fir::isa_complex(fromFirTy) && fir::isa_complex(toFirTy)) {
       // Special case: handle the conversion of a complex such that both the
       // real and imaginary parts are converted together.
       auto zero = mlir::ArrayAttr::get(convert.getContext(),
@@ -773,6 +774,22 @@ struct ConvertOpConversion : public FIROpConversion<fir::ConvertOp> {
                                                              ic, one);
       return mlir::success();
     }
+
+    // Follow UNIX F77 convention for logicals:
+    // 1. underlying integer is not zero => logical is .TRUE.
+    // 2. logical is .TRUE. => set underlying integer to 1.
+    auto i1Type = mlir::IntegerType::get(convert.getContext(), 1);
+    if (fromFirTy.isa<fir::LogicalType>() && toFirTy == i1Type) {
+      mlir::Value zero = genConstantIndex(loc, fromTy, rewriter, 0);
+      rewriter.replaceOpWithNewOp<mlir::LLVM::ICmpOp>(
+          convert, mlir::LLVM::ICmpPredicate::ne, op0, zero);
+      return mlir::success();
+    }
+    if (fromFirTy == i1Type && toFirTy.isa<fir::LogicalType>()) {
+      rewriter.replaceOpWithNewOp<mlir::LLVM::ZExtOp>(convert, toTy, op0);
+      return mlir::success();
+    }
+
     // Floating point to floating point conversion.
     if (isFloatingPointTy(fromTy)) {
       if (isFloatingPointTy(toTy)) {
