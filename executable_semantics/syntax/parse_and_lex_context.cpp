24fbafb88b7333e6afacec05c8e6ba950cd37b62
Pavel Kobyakov <pkobyakov@google.com>
2022-03-09 08:54:13 -0500

    FatalComplationError() -> ParseAndLexContext::RecordLexerError(). Other usages of ERROR_TOKEN in lexer.lpp were actually supposed to be END_OF_FILE.

--- executable_semantics/syntax/parse_and_lex_context.cpp
+++ executable_semantics/syntax/parse_and_lex_context.cpp
@@ -10,4 +10,18 @@ auto ParseAndLexContext::RecordError(const std::string& message) -> void {
   error_messages_.push_back(message);
 }
 
+auto ParseAndLexContext::RecordLexerError(const std::string& message)
+    -> Parser::symbol_type {
+  // Adds a newline in trace mode because trace prints an incomplete line
+  // "Reading a token: " which can prevent LIT from finding expected patterns.
+  std::string full_message;
+  llvm::raw_string_ostream(full_message)
+      << (trace() ? "\n" : "") << "COMPILATION ERROR: " << source_loc() << ": "
+      << message;
+  RecordError(full_message);
+
+  // TODO: use `YYerror` token once bison is upgraded to at least 3.5.
+  return Parser::make_END_OF_FILE(current_token_position);
+}
+
 }  // namespace Carbon
