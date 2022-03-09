83b8878fbb0ede596bd85400e921422e45376016
Siva Chandra Reddy <sivachandra@google.com>
2022-03-09 21:30:22 +0000

    [libc] Use the constexpr constructor to initialize exit handlers mutex.
    Reviewed By: abrachet
    
    Differential Revision: https://reviews.llvm.org/D121334

--- libc/src/stdlib/atexit.cpp
+++ libc/src/stdlib/atexit.cpp
@@ -9,20 +9,13 @@
 #include "src/stdlib/atexit.h"
 #include "src/__support/CPP/vector.h"
 #include "src/__support/common.h"
-#include "src/threads/mtx_init.h"
-#include "src/threads/mtx_lock.h"
-#include "src/threads/mtx_unlock.h"
+#include "src/__support/threads/mutex.h"
 
 namespace __llvm_libc {
 
 namespace {
 
-mtx_t lock;
-// TODO need an easier way to use mtx_t internally, or use pthread_mutex_t
-// with PTHREAD_MUTEX_INITIALIZER when it lands.
-struct Init {
-  Init() { __llvm_libc::mtx_init(&lock, mtx_plain); }
-} init;
+Mutex handler_list_mtx(false, false, false);
 
 // TOOD should we make cpp::vector like llvm::SmallVector<T, N> where it will
 // allocate at least N before needing dynamic allocation?
@@ -33,21 +26,21 @@ static cpp::vector<void (*)(void)> handlers;
 namespace internal {
 
 void call_exit_handlers() {
-  __llvm_libc::mtx_lock(&lock);
+  handler_list_mtx.lock();
   // TODO: implement rbegin() + rend() for cpp::vector
   for (int i = handlers.size() - 1; i >= 0; i--) {
-    __llvm_libc::mtx_unlock(&lock);
+    handler_list_mtx.unlock();
     handlers[i]();
-    __llvm_libc::mtx_lock(&lock);
+    handler_list_mtx.lock();
   }
 }
 
 } // namespace internal
 
 LLVM_LIBC_FUNCTION(int, atexit, (void (*function)())) {
-  __llvm_libc::mtx_lock(&lock);
+  handler_list_mtx.lock();
   handlers.push_back(function);
-  __llvm_libc::mtx_unlock(&lock);
+  handler_list_mtx.unlock();
   return 0;
 }
 
