853beb55e14615ade795e18d218305c1dfdb5181
Nikita Popov <npopov@redhat.com>
2022-03-11 14:19:00 +0100

    [FuzzMutate] Support scalable shufflevector

--- llvm/lib/FuzzMutate/Operations.cpp
+++ llvm/lib/FuzzMutate/Operations.cpp
@@ -309,12 +309,12 @@ static SourcePred validShuffleVectorIndex() {
     return ShuffleVectorInst::isValidOperands(Cur[0], Cur[1], V);
   };
   auto Make = [](ArrayRef<Value *> Cur, ArrayRef<Type *> Ts) {
-    auto *FirstTy = cast<FixedVectorType>(Cur[0]->getType());
+    auto *FirstTy = cast<VectorType>(Cur[0]->getType());
     auto *Int32Ty = Type::getInt32Ty(Cur[0]->getContext());
     // TODO: It's straighforward to make up reasonable values, but listing them
     // exhaustively would be insane. Come up with a couple of sensible ones.
     return std::vector<Constant *>{UndefValue::get(
-        FixedVectorType::get(Int32Ty, FirstTy->getNumElements()))};
+        VectorType::get(Int32Ty, FirstTy->getElementCount()))};
   };
   return {Pred, Make};
 }
