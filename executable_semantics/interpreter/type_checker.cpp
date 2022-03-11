93842ad8786bf29d9a06e805123f3a24ddbef778
Geoff Romer <gromer@google.com>
2022-03-10 16:46:43 -0800

    Eliminate run-time errors from PatternMatch (#1126)

--- executable_semantics/interpreter/type_checker.cpp
+++ executable_semantics/interpreter/type_checker.cpp
@@ -840,6 +840,10 @@ void TypeChecker::TypeCheckPattern(
     }
     case PatternKind::BindingPattern: {
       auto& binding = cast<BindingPattern>(*p);
+      if (!GetBindings(binding.type()).empty()) {
+        FATAL_COMPILATION_ERROR(binding.type().source_loc())
+            << "The type of a binding pattern cannot contain bindings.";
+      }
       TypeCheckPattern(&binding.type(), std::nullopt, impl_scope);
       Nonnull<const Value*> type =
           InterpPattern(&binding.type(), arena_, trace_);
