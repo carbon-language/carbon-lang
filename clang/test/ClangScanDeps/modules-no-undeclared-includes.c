4d6835f96d610c59c6558f571214e6c42097fec9
Jan Svoboda <jan_svoboda@apple.com>
2022-03-12 12:09:10 +0100

    [clang][deps] Attempt to fix Windows test failure

--- clang/test/ClangScanDeps/modules-no-undeclared-includes.c
+++ clang/test/ClangScanDeps/modules-no-undeclared-includes.c
@@ -27,7 +27,7 @@ module User [no_undeclared_includes] { header "user.h" }
 // RUN: sed "s|DIR|%/t|g" %t/cdb.json.template > %t/cdb.json
 // RUN: clang-scan-deps -compilation-database %t/cdb.json -format experimental-full \
 // RUN:   -generate-modules-path-args -module-files-dir %t/build > %t/result.json
-// RUN: cat %t/result.json | sed 's:\\\\\?:/:g' | FileCheck %s -DPREFIX=%t
+// RUN: cat %t/result.json | sed 's:\\\\\?:/:g' | FileCheck %s -DPREFIX=%/t
 
 // CHECK:        {
 // CHECK-NEXT:   "modules": [
