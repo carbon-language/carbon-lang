5f016f5b7a775fbafe9b594958d7e4afb126e27a
Mark de Wever <koraq@xs4all.nl>
2022-03-12 12:07:54 +0100

    [libc++] Add a missing test case.

--- libcxx/test/libcxx/utilities/format/format.arguments/format.arg/arg_t.compile.pass.cpp
+++ libcxx/test/libcxx/utilities/format/format.arguments/format.arg/arg_t.compile.pass.cpp
@@ -36,3 +36,4 @@ static_assert(uint8_t(std::__format::__arg_t::__long_double) == 11);
 static_assert(uint8_t(std::__format::__arg_t::__const_char_type_ptr) == 12);
 static_assert(uint8_t(std::__format::__arg_t::__string_view) == 13);
 static_assert(uint8_t(std::__format::__arg_t::__ptr) == 14);
+static_assert(uint8_t(std::__format::__arg_t::__handle) == 15);
