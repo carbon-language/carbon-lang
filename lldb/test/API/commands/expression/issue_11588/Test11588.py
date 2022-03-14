8bd1988e74f212f65de21e25c9e44b42a4b97bd9
Stella Stamenova <stilis@microsoft.com>
2022-03-14 09:26:41 -0700

    [lldb] Skip Test11588 on Windows
    The test was marked as XFAIL on Windows, but recent changes have made it flaky instead

--- lldb/test/API/commands/expression/issue_11588/Test11588.py
+++ lldb/test/API/commands/expression/issue_11588/Test11588.py
@@ -16,7 +16,7 @@ class Issue11581TestCase(TestBase):
 
     mydir = TestBase.compute_mydir(__file__)
 
-    @expectedFailureAll(oslist=["windows"], bugnumber="llvm.org/pr24778")
+    @skipIfWindows #This test is now flaky on windows, see llvm.org/pr24778
     def test_11581_commands(self):
         # This is the function to remove the custom commands in order to have a
         # clean slate for the next test case.
