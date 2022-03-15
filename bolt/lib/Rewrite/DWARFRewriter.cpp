57f03db1953d81b0868d4c19ab30c93f5e90b3d3
Maksim Panchenko <maks@fb.com>
2022-03-15 12:33:55 -0700

    [BOLT][NFC] Remove unused function
    Reviewed By: yota9
    
    Differential Revision: https://reviews.llvm.org/D121729

--- bolt/lib/Rewrite/DWARFRewriter.cpp
+++ bolt/lib/Rewrite/DWARFRewriter.cpp
@@ -149,10 +149,6 @@ getDWOName(llvm::DWARFUnit &CU,
   return DWOName;
 }
 
-static bool isHighPcFormEightBytes(dwarf::Form DwarfForm) {
-  return DwarfForm == dwarf::DW_FORM_addr || DwarfForm == dwarf::DW_FORM_data8;
-}
-
 void DWARFRewriter::updateDebugInfo() {
   ErrorOr<BinarySection &> DebugInfo = BC.getUniqueSectionByName(".debug_info");
   if (!DebugInfo)
