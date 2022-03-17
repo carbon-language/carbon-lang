699172633f9a49144eccf0ef7e480094cf883a3b
Jon Meow <jperkins@google.com>
2022-03-17 13:27:25 -0700

    Sort diagnostic output from the driver by default (#1143)

---
+++ toolchain/diagnostics/sorting_diagnostic_consumer.h
@@ -0,0 +1,46 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef TOOLCHAIN_DIAGNOSTICS_SORTING_DIAGNOSTIC_CONSUMER_H_
+#define TOOLCHAIN_DIAGNOSTICS_SORTING_DIAGNOSTIC_CONSUMER_H_
+
+#include "common/check.h"
+#include "llvm/ADT/STLExtras.h"
+#include "toolchain/diagnostics/diagnostic_emitter.h"
+
+namespace Carbon {
+
+// Buffers incoming diagnostics for printing and sorting.
+class SortingDiagnosticConsumer : public DiagnosticConsumer {
+ public:
+  explicit SortingDiagnosticConsumer(DiagnosticConsumer& next_consumer)
+      : next_consumer_(&next_consumer) {}
+
+  ~SortingDiagnosticConsumer() override { Flush(); }
+
+  // Buffers the diagnostic.
+  auto HandleDiagnostic(const Diagnostic& diagnostic) -> void override {
+    diagnostics_.push_back(diagnostic);
+  }
+
+  // Sorts and flushes buffered diagnostics.
+  void Flush() override {
+    llvm::sort(diagnostics_, [](const Diagnostic& lhs, const Diagnostic& rhs) {
+      return std::tie(lhs.location.line_number, lhs.location.column_number) <
+             std::tie(rhs.location.line_number, rhs.location.column_number);
+    });
+    for (const auto& diagnostic : diagnostics_) {
+      next_consumer_->HandleDiagnostic(diagnostic);
+    }
+    diagnostics_.clear();
+  }
+
+ private:
+  llvm::SmallVector<Diagnostic, 0> diagnostics_;
+  DiagnosticConsumer* next_consumer_;
+};
+
+}  // namespace Carbon
+
+#endif  // TOOLCHAIN_DIAGNOSTICS_SORTING_DIAGNOSTIC_CONSUMER_H_
---
+++ toolchain/diagnostics/sorting_diagnostic_consumer_test.cpp
@@ -0,0 +1,86 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "toolchain/diagnostics/sorting_diagnostic_consumer.h"
+
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "llvm/ADT/StringRef.h"
+#include "llvm/Support/FormatVariadic.h"
+#include "toolchain/diagnostics/diagnostic_emitter.h"
+#include "toolchain/diagnostics/mocks.h"
+
+namespace Carbon::Testing {
+namespace {
+
+using ::testing::InSequence;
+
+struct FakeDiagnostic : DiagnosticBase<FakeDiagnostic> {
+  static constexpr llvm::StringLiteral ShortName = "fake-diagnostic";
+  // TODO: consider ways to put the Message into `format` to allow dynamic
+  // selection of the message.
+  static constexpr llvm::StringLiteral Message = "{0}";
+
+  auto Format() -> std::string {
+    // Work around a bug in Clang's unused const variable warning by marking it
+    // used here with a no-op.
+    static_cast<void>(ShortName);
+
+    return llvm::formatv(Message.data(), message).str();
+  }
+
+  std::string message;
+};
+
+struct FakeDiagnosticLocationTranslator
+    : DiagnosticLocationTranslator<Diagnostic::Location> {
+  auto GetLocation(Diagnostic::Location loc) -> Diagnostic::Location override {
+    return loc;
+  }
+};
+
+// Produces a location for the given line and column.
+static auto MakeLoc(int line, int col) -> Diagnostic::Location {
+  return {.file_name = "test", .line_number = line, .column_number = col};
+}
+
+TEST(SortedDiagnosticEmitterTest, SortErrors) {
+  FakeDiagnosticLocationTranslator translator;
+  Testing::MockDiagnosticConsumer consumer;
+  SortingDiagnosticConsumer sorting_consumer(consumer);
+  DiagnosticEmitter<Diagnostic::Location> emitter(translator, sorting_consumer);
+
+  emitter.EmitError<FakeDiagnostic>(MakeLoc(2, 1), {.message = "M1"});
+  emitter.EmitError<FakeDiagnostic>(MakeLoc(1, 1), {.message = "M2"});
+  emitter.EmitError<FakeDiagnostic>(MakeLoc(1, 3), {.message = "M3"});
+  emitter.EmitError<FakeDiagnostic>(MakeLoc(3, 4), {.message = "M4"});
+  emitter.EmitError<FakeDiagnostic>(MakeLoc(3, 2), {.message = "M5"});
+
+  InSequence s;
+  EXPECT_CALL(consumer, HandleDiagnostic(
+                            AllOf(DiagnosticLevel(Diagnostic::Error),
+                                  DiagnosticAt(1, 1), DiagnosticMessage("M2"),
+                                  DiagnosticShortName("fake-diagnostic"))));
+  EXPECT_CALL(consumer, HandleDiagnostic(
+                            AllOf(DiagnosticLevel(Diagnostic::Error),
+                                  DiagnosticAt(1, 3), DiagnosticMessage("M3"),
+                                  DiagnosticShortName("fake-diagnostic"))));
+  EXPECT_CALL(consumer, HandleDiagnostic(
+                            AllOf(DiagnosticLevel(Diagnostic::Error),
+                                  DiagnosticAt(2, 1), DiagnosticMessage("M1"),
+                                  DiagnosticShortName("fake-diagnostic"))));
+  EXPECT_CALL(consumer, HandleDiagnostic(
+                            AllOf(DiagnosticLevel(Diagnostic::Error),
+                                  DiagnosticAt(3, 2), DiagnosticMessage("M5"),
+                                  DiagnosticShortName("fake-diagnostic"))));
+  EXPECT_CALL(consumer, HandleDiagnostic(
+                            AllOf(DiagnosticLevel(Diagnostic::Error),
+                                  DiagnosticAt(3, 4), DiagnosticMessage("M4"),
+                                  DiagnosticShortName("fake-diagnostic"))));
+  sorting_consumer.Flush();
+}
+
+}  // namespace
+}  // namespace Carbon::Testing
