3b2e605e33bd9017ff2eff1493add07822f9d58b
Mark de Wever <koraq@xs4all.nl>
2022-03-20 14:15:00 +0100

    [libc++][test][NFC] Remove libcpp-no-concepts.
    This is no longer needed.
    
    Reviewed By: #libc, philnik
    
    Differential Revision: https://reviews.llvm.org/D122099

--- libcxx/test/std/algorithms/alg.sorting/alg.min.max/ranges.min.pass.cpp
+++ libcxx/test/std/algorithms/alg.sorting/alg.min.max/ranges.min.pass.cpp
@@ -9,7 +9,6 @@
 // <algorithm>
 
 // UNSUPPORTED: c++03, c++11, c++14, c++17
-// UNSUPPORTED: libcpp-no-concepts
 // UNSUPPORTED: libcpp-has-no-incomplete-ranges
 
 // template<class T, class Proj = identity,
