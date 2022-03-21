ca47011e73e6ff0451b6a9c8de3d3f3044acec72
Alisamar Husain <zrthxn@gmail.com>
2022-03-21 13:08:40 +0530

    [tests][intelpt] Fix outdated trace load test
    Differential Revision: https://reviews.llvm.org/D122114

--- lldb/test/API/commands/trace/TestTraceLoad.py
+++ lldb/test/API/commands/trace/TestTraceLoad.py
@@ -36,7 +36,9 @@ class TestTraceLoad(TraceIntelPTTestCaseBase):
         self.expect("thread trace dump info", substrs=['''Trace technology: intel-pt
 
 thread #1: tid = 3842849
-  Raw trace size: 4096 bytes'''])
+  Raw trace size: 4 KiB
+  Total number of instructions: 21
+  Total approximate memory usage: 5.38 KiB'''])
 
     def testLoadInvalidTraces(self):
         src_dir = self.getSourceDir()
