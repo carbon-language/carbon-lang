d6f00f88390702df32b93824f4f1a06bf3916472
Louis Dionne <ldionne.2@gmail.com>
2022-03-21 13:50:53 -0400

    [libc++] Trigger CI when cmake/ is modified

--- libcxx/utils/ci/buildkite-pipeline-premerge.sh
+++ libcxx/utils/ci/buildkite-pipeline-premerge.sh
@@ -17,8 +17,8 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-if ! git diff --name-only HEAD~1 | grep -q -E "libcxx/|libcxxabi/|libunwind/|runtimes/"; then
-  # libcxx/, libcxxabi/, libunwind/ or runtimes/ are not affected
+if ! git diff --name-only HEAD~1 | grep -q -E "libcxx/|libcxxabi/|libunwind/|runtimes/|cmake/"; then
+  # libcxx/, libcxxabi/, libunwind/, runtimes/ or cmake/ are not affected
   exit 0
 fi
 
