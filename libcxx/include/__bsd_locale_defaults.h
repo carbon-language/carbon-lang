cc82a1b02a29c1f3a839f388391c13217d361326
Louis Dionne <ldionne.2@gmail.com>
2022-03-23 13:11:04 -0400

    [libc++][NFC] Fix include guards and add a missing license header

--- libcxx/include/__bsd_locale_defaults.h
+++ libcxx/include/__bsd_locale_defaults.h
@@ -11,8 +11,8 @@
 // we will define the mapping from an internal macro to the real BSD symbol.
 //===----------------------------------------------------------------------===//
 
-#ifndef _LIBCPP_BSD_LOCALE_DEFAULTS_H
-#define _LIBCPP_BSD_LOCALE_DEFAULTS_H
+#ifndef _LIBCPP___BSD_LOCALE_DEFAULTS_H
+#define _LIBCPP___BSD_LOCALE_DEFAULTS_H
 
 #if !defined(_LIBCPP_HAS_NO_PRAGMA_SYSTEM_HEADER)
 #  pragma GCC system_header
@@ -33,4 +33,4 @@
 #define __libcpp_asprintf_l(...)                            asprintf_l(__VA_ARGS__)
 #define __libcpp_sscanf_l(...)                              sscanf_l(__VA_ARGS__)
 
-#endif // _LIBCPP_BSD_LOCALE_DEFAULTS_H
+#endif // _LIBCPP___BSD_LOCALE_DEFAULTS_H
