681b9466c9209ee6c7209761a309fb78c6fa1ef6
Matt Arsenault <Matthew.Arsenault@amd.com>
2022-03-26 17:25:34 -0400

    RegAllocGreedy: Remove redundant check for virtual registers
    The set of interfering virtual registers obviously only includes
    virtual registers.

--- llvm/lib/CodeGen/RegAllocGreedy.cpp
+++ llvm/lib/CodeGen/RegAllocGreedy.cpp
@@ -460,9 +460,6 @@ bool RAGreedy::canEvictInterferenceInRange(const LiveInterval &VirtReg,
       if (!Intf->overlaps(Start, End))
         continue;
 
-      // Cannot evict non virtual reg interference.
-      if (!Register::isVirtualRegister(Intf->reg()))
-        return false;
       // Never evict spill products. They cannot split or spill.
       if (ExtraInfo->getStage(*Intf) == RS_Done)
         return false;
