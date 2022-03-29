d0eeb0fba09f2eeebb2d71f70c8b491dc3c4f316
Dominic Chen <ddchen@apple.com>
2022-03-28 21:52:04 -0700

    [scudo] Fix definition of canCache() function
    The called member function has boolean type, change this function to match
    
    Differential Revision: https://reviews.llvm.org/D122631

--- compiler-rt/lib/scudo/standalone/secondary.h
+++ compiler-rt/lib/scudo/standalone/secondary.h
@@ -459,7 +459,7 @@ public:
     }
   }
 
-  uptr canCache(uptr Size) { return Cache.canCache(Size); }
+  bool canCache(uptr Size) { return Cache.canCache(Size); }
 
   bool setOption(Option O, sptr Value) { return Cache.setOption(O, Value); }
 
