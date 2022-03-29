95eac47260542fc51616bf6ee147a3b85342d72a
Ron Lieberman <ron.lieberman@amd.com>
2022-03-29 14:53:27 +0000

    [libomptarget] x86 offloading fails map_back_race.cpp intermittently
    Differential Revision: https://reviews.llvm.org/D122658

--- openmp/libomptarget/test/mapping/map_back_race.cpp
+++ openmp/libomptarget/test/mapping/map_back_race.cpp
@@ -2,6 +2,10 @@
 
 // Taken from https://github.com/llvm/llvm-project/issues/54216
 
+
+// UNSUPPORTED: x86_64-pc-linux-gnu
+// UNSUPPORTED: x86_64-pc-linux-gnu-newDriver
+
 #include <algorithm>
 #include <cstdlib>
 #include <iostream>
