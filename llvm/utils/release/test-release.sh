a30972fbd7a511f4a96d732fe390e046a29f139d
Tobias Hieta <tobias@hieta.se>
2022-03-31 08:34:30 +0200

    [test-release] Added -silent-log flag to test-release.sh
    This flag silents the build output of test-release.sh so that
    it can be used in CI systems a bit better. It will still log
    the build output to the log files but not echo it to stdout.
    
    Reviewed By: tstellar
    
    Differential Revision: https://reviews.llvm.org/D122146

--- llvm/utils/release/test-release.sh
+++ llvm/utils/release/test-release.sh
@@ -40,6 +40,7 @@ do_lldb="yes"
 do_polly="yes"
 do_mlir="yes"
 do_flang="yes"
+do_silent_log="no"
 BuildDir="`pwd`"
 ExtraConfigureFlags=""
 ExportBranch=""
@@ -75,6 +76,7 @@ function usage() {
     echo " -no-polly            Disable check-out & build Polly"
     echo " -no-mlir             Disable check-out & build MLIR"
     echo " -no-flang            Disable check-out & build Flang"
+    echo " -silent-log          Don't output build logs to stdout"
 }
 
 while [ $# -gt 0 ]; do
@@ -178,6 +180,9 @@ while [ $# -gt 0 ]; do
             ;;
         -no-flang )
             do_flang="no"
+            ;;
+        -silent-log )
+            do_silent_log="yes"
             ;;
         -help | --help | -h | --h | -\? )
             usage
@@ -421,16 +426,22 @@ function build_llvmCore() {
       Verbose="-v"
     fi
 
+    redir="/dev/stdout"
+    if [ $do_silent_log == "yes" ]; then
+      echo "# Silencing build logs because of -silent-log flag..."
+      redir="/dev/null"
+    fi
+
     cd $ObjDir
     echo "# Compiling llvm $Release-$RC $Flavor"
     echo "# ${MAKE} -j $NumJobs $Verbose"
     ${MAKE} -j $NumJobs $Verbose \
-        2>&1 | tee $LogDir/llvm.make-Phase$Phase-$Flavor.log
+        2>&1 | tee $LogDir/llvm.make-Phase$Phase-$Flavor.log > $redir
 
     echo "# Installing llvm $Release-$RC $Flavor"
     echo "# ${MAKE} install"
     DESTDIR="${DestDir}" ${MAKE} install \
-        2>&1 | tee $LogDir/llvm.install-Phase$Phase-$Flavor.log
+        2>&1 | tee $LogDir/llvm.install-Phase$Phase-$Flavor.log > $redir
     cd $BuildDir
 }
 
