c103eac9d8596cf154ccf5249f5c919badd831d6
Muhammad Omair Javaid <omair.javaid@linaro.org>
2022-04-01 13:58:16 +0500

    [LLDB] Skip TestJobControl.py AArch64/Arm Linux
    TestJobControl.py is randomly failing on AArch64/Arm Linux buildbots.
    I am marking it as skipped to make buildbot stable.

--- lldb/test/API/driver/job_control/TestJobControl.py
+++ lldb/test/API/driver/job_control/TestJobControl.py
@@ -10,7 +10,8 @@ from lldbsuite.test.lldbpexpect import PExpectTest
 class JobControlTest(PExpectTest):
 
     mydir = TestBase.compute_mydir(__file__)
-
+    
+    @skipIf(oslist=["linux"], archs=["arm", "aarch64"])
     def test_job_control(self):
         def post_spawn():
             self.child.expect("PID=([0-9]+)")
