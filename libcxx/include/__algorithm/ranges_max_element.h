e476df5629ee32b1180105c7a73e827bcc0d639b
Nikolas Klauser <nikolasklauser@berlin.de>
2022-04-03 09:17:01 +0200

    [libc++][ranges] Implement ranges::max
    Reviewed By: Mordante, var-const, #libc
    
    Spies: sstefan1, libcxx-commits, mgorny
    
    Differential Revision: https://reviews.llvm.org/D122002

--- libcxx/include/__algorithm/ranges_max_element.h
+++ libcxx/include/__algorithm/ranges_max_element.h
@@ -9,6 +9,7 @@
 #ifndef _LIBCPP___ALGORITHM_RANGES_MAX_ELEMENT_H
 #define _LIBCPP___ALGORITHM_RANGES_MAX_ELEMENT_H
 
+#include <__algorithm/ranges_min_element.h>
 #include <__config>
 #include <__functional/identity.h>
 #include <__functional/invoke.h>
@@ -30,31 +31,20 @@ _LIBCPP_BEGIN_NAMESPACE_STD
 namespace ranges {
 namespace __max_element {
 struct __fn {
-  template <class _Ip, class _Sp, class _Proj, class _Comp>
-  _LIBCPP_HIDE_FROM_ABI static constexpr
-  _Ip __go(_Ip __first, _Sp __last, _Comp& __comp, _Proj& __proj) {
-    if (__first == __last)
-      return __first;
-
-    _Ip __i = __first;
-    while (++__i != __last)
-      if (std::invoke(__comp, std::invoke(__proj, *__first), std::invoke(__proj, *__i)))
-        __first = __i;
-    return __first;
-  }
-
   template <forward_iterator _Ip, sentinel_for<_Ip> _Sp, class _Proj = identity,
             indirect_strict_weak_order<projected<_Ip, _Proj>> _Comp = ranges::less>
   _LIBCPP_HIDE_FROM_ABI constexpr
   _Ip operator()(_Ip __first, _Sp __last, _Comp __comp = {}, _Proj __proj = {}) const {
-    return __go(__first, __last, __comp, __proj);
+    auto __comp_lhs_rhs_swapped = [&](auto&& __lhs, auto&& __rhs) { return std::invoke(__comp, __rhs, __lhs); };
+    return __min_element_impl(__first, __last, __comp_lhs_rhs_swapped, __proj);
   }
 
   template <forward_range _Rp, class _Proj = identity,
             indirect_strict_weak_order<projected<iterator_t<_Rp>, _Proj>> _Comp = ranges::less>
   _LIBCPP_HIDE_FROM_ABI constexpr
   borrowed_iterator_t<_Rp> operator()(_Rp&& __r, _Comp __comp = {}, _Proj __proj = {}) const {
-    return __go(ranges::begin(__r), ranges::end(__r), __comp, __proj);
+    auto __comp_lhs_rhs_swapped = [&](auto&& __lhs, auto&& __rhs) { return std::invoke(__comp, __rhs, __lhs); };
+    return __min_element_impl(ranges::begin(__r), ranges::end(__r), __comp_lhs_rhs_swapped, __proj);
   }
 };
 } // namespace __max_element
