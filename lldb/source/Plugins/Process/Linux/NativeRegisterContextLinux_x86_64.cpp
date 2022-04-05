ed9a14f912c034c6a7177d4bc8a2d41a22b3e129
Jonas Devlieghere <jonas@devlieghere.com>
2022-04-05 14:24:32 -0700

    [lldb] Add missing const to NativeRegisterContextLinux_x86_64
    Fixes error: invalid conversion from ‘const uint8_t*’ {aka ‘const
    unsigned char*’} to ‘uint8_t*’ {aka ‘unsigned char*’}

--- lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_x86_64.cpp
+++ lldb/source/Plugins/Process/Linux/NativeRegisterContextLinux_x86_64.cpp
@@ -780,7 +780,7 @@ Status NativeRegisterContextLinux_x86_64::WriteAllRegisterValues(
     return error;
   }
 
-  uint8_t *src = data_sp->GetBytes();
+  const uint8_t *src = data_sp->GetBytes();
   if (src == nullptr) {
     error.SetErrorStringWithFormat("NativeRegisterContextLinux_x86_64::%s "
                                    "DataBuffer::GetBytes() returned a null "
