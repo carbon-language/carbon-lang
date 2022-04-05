0601a0dc0135fea422e2c794fb392efef8857b3a
Jean Perier <jperier@nvidia.com>
2022-04-05 17:26:53 +0200

    [flang] Fix fir.embox codegen with constant interior shape
    Fix https://github.com/flang-compiler/f18-llvm-project/issues/1416.
    
    The `constRows` variable was being decremented too soon, causing the
    last constant interior dimension extent being used to multiply the GEP
    offset. This lead to wrong address computation and caused segfaults.
    
    Note: also upstream fir.embox tests that can be upstreamed.
    
    Differential Revision: https://reviews.llvm.org/D123130

--- flang/lib/Optimizer/CodeGen/CodeGen.cpp
+++ flang/lib/Optimizer/CodeGen/CodeGen.cpp
@@ -1525,7 +1525,6 @@ struct XEmboxOpConversion : public EmboxCommonConversion<fir::cg::XEmboxOp> {
         auto ao = rewriter.create<mlir::LLVM::SubOp>(loc, i64Ty, off, adj);
         if (constRows > 0) {
           gepArgs.push_back(ao);
-          --constRows;
         } else {
           auto dimOff =
               rewriter.create<mlir::LLVM::MulOp>(loc, i64Ty, ao, prevPtrOff);
@@ -1584,6 +1583,8 @@ struct XEmboxOpConversion : public EmboxCommonConversion<fir::cg::XEmboxOp> {
       if (constRows == 0)
         prevPtrOff = rewriter.create<mlir::LLVM::MulOp>(loc, i64Ty, prevPtrOff,
                                                         outerExtent);
+      else
+        --constRows;
 
       // increment iterators
       ++shapeOffset;
