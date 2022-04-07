cc0f995e66cd052e0d3d53b1fbd64cf98ace75bf
jonmeow <46229924+jonmeow@users.noreply.github.com>
2022-04-07 19:41:12 +0000

    Fix include guard

--- executable_semantics/fuzzing/ast_to_proto.h
+++ executable_semantics/fuzzing/ast_to_proto.h
@@ -2,8 +2,8 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#ifndef THIRD_PARTY_CARBON_LANG_EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
-#define THIRD_PARTY_CARBON_LANG_EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
+#ifndef EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
+#define EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
 
 #include "common/fuzzing/carbon.pb.h"
 #include "executable_semantics/ast/ast.h"
@@ -15,4 +15,4 @@ auto AstToProto(const AST& ast) -> Fuzzing::CompilationUnit;
 
 }  // namespace Carbon
 
-#endif  // THIRD_PARTY_CARBON_LANG_EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
+#endif  // EXECUTABLE_SEMANTICS_FUZZING_AST_TO_PROTO_H_
