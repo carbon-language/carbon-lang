78a6f59c2cef58c01a13ba4e587b30cd1dbd6475
Paul Robinson <paul.robinson@sony.com>
2022-04-08 12:19:51 -0700

    [RGT] Use GTEST_SKIP instead of just returning

--- clang/unittests/Tooling/RecursiveASTVisitorTests/LambdaExpr.cpp
+++ clang/unittests/Tooling/RecursiveASTVisitorTests/LambdaExpr.cpp
@@ -88,7 +88,7 @@ TEST(RecursiveASTVisitor, VisitsLambdaExprAndImplicitClass) {
 
 TEST(RecursiveASTVisitor, VisitsAttributedLambdaExpr) {
   if (llvm::Triple(llvm::sys::getDefaultTargetTriple()).isPS4())
-    return; // PS4 does not support fastcall.
+    GTEST_SKIP(); // PS4 does not support fastcall.
   LambdaExprVisitor Visitor;
   Visitor.ExpectMatch("", 1, 12);
   EXPECT_TRUE(Visitor.runOver(
