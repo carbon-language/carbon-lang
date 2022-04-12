fdd424e37abf3fa411c07f1e08fee72cfe7bb25b
Fangrui Song <i@maskray.me>
2022-04-12 11:24:19 -0700

    [ubsan] Fix print_stacktrace=1:fast_unwind_on_fatal=0 to correctly fallback to fast unwinder
    ubsan_GetStackTrace (from 52b751088b11547e0f4ef0589ebbe5e57752c68c) called by
    ~ScopeReport leaves top/bottom zeroes in the
    `!WillUseFastUnwind(request_fast_unwind)` code path.
    When BufferedStackTrace::Unwind falls back to UnwindFast,
    `if (stack_top < 4096) return;` will return early, leaving just one frame in the stack trace.
    
    Fix this by always initializing top/bottom like 261d6e05d5574bec753ea6b7e9a7f99229927753.
    
    Reviewed By: eugenis, yln
    
    Differential Revision: https://reviews.llvm.org/D123562

---
+++ compiler-rt/test/ubsan/TestCases/Misc/Linux/diag-stacktrace.cpp
@@ -0,0 +1,22 @@
+/// Fast unwinder does not work with Thumb code
+// UNSUPPORTED: thumb
+
+// RUN: %clangxx -fsanitize=return %gmlt -O2 -fno-omit-frame-pointer -fasynchronous-unwind-tables %s -o %t
+// RUN: %env_ubsan_opts=print_stacktrace=1:fast_unwind_on_fatal=0 not %run %t 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=print_stacktrace=1:fast_unwind_on_fatal=1 not %run %t 2>&1 | FileCheck %s
+// RUN: %clangxx -fsanitize=return %gmlt -O2 -fno-omit-frame-pointer -fno-exceptions -fno-asynchronous-unwind-tables %s -o %t
+// RUN: %env_ubsan_opts=print_stacktrace=1:fast_unwind_on_fatal=0 not %run %t 2>&1 | FileCheck %s
+// RUN: %env_ubsan_opts=print_stacktrace=1:fast_unwind_on_fatal=1 not %run %t 2>&1 | FileCheck %s
+
+// CHECK:      runtime error: execution reached the end of a value-returning function without returning a value
+// CHECK-NEXT: #0 {{.*}}f() {{.*}}.cpp:[[#@LINE+1]]
+__attribute__((noinline)) int f() {}
+
+// CHECK-NEXT: #1 {{.*}}g() {{.*}}.cpp:[[#@LINE+1]]
+__attribute__((noinline)) void g() { f(); }
+
+// CHECK-NEXT: #2 {{.*}}h() {{.*}}.cpp:[[#@LINE+1]]
+__attribute__((noinline)) void h() { g(); }
+
+// CHECK-NEXT: #3 {{.*}}main {{.*}}.cpp:[[#@LINE+1]]
+int main() { h(); }
