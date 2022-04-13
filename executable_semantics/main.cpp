6542506cdc0aefefe2a6ad80f42ad8564595814d
Chandler Carruth <chandlerc@gmail.com>
2022-04-12 19:55:35 -0700

    Teach executable semantics to use the program path to locate data files. (#1179)
    This allows it to find the prelude when run out of a Bazel runfiles tree
    regardless of the current working directory. For example, this will
    allow local testing of compiler explorer.

--- executable_semantics/main.cpp
+++ executable_semantics/main.cpp
@@ -16,8 +16,13 @@
 #include "executable_semantics/interpreter/exec_program.h"
 #include "executable_semantics/syntax/parse.h"
 #include "executable_semantics/syntax/prelude.h"
+#include "llvm/ADT/SmallString.h"
 #include "llvm/Support/CommandLine.h"
+#include "llvm/Support/ErrorOr.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/Path.h"
+#include "llvm/Support/Program.h"
 
 // Prints an error message and returns error code value.
 auto PrintError(const Carbon::Error& error) -> int {
@@ -41,9 +46,29 @@ auto main(int argc, char* argv[]) -> int {
   opt<bool> trace_option("trace", desc("Enable tracing"));
   opt<std::string> input_file_name(llvm::cl::Positional, desc("<input file>"),
                                    llvm::cl::Required);
-  opt<std::string> prelude_file_name(
-      "prelude", desc("<prelude file>"),
-      llvm::cl::init("executable_semantics/data/prelude.carbon"));
+
+  // Find the path of the executable if possible and use that as a relative root
+  // for finding the prelude. FIXME: Currently, this assumes a Bazel-like
+  // runfiles tree rather than any kind of installation tree.
+  llvm::SmallString<256> exe_path(argv[0]);
+  if (!llvm::sys::fs::exists(exe_path)) {
+    // Try to lookup the program name in the `PATH`.
+    if (llvm::ErrorOr<std::string> path =
+            llvm::sys::findProgramByName(exe_path)) {
+      exe_path = *path;
+    }
+  }
+  // If we have a valid path, find the parent directory. Otherwise, just use an
+  // empty string to get the current working directory.
+  if (llvm::sys::fs::exists(exe_path)) {
+    llvm::sys::path::remove_filename(exe_path);
+  } else {
+    exe_path = "";
+  }
+  llvm::SmallString<256> prelude_path = exe_path;
+  llvm::sys::path::append(prelude_path, "data/prelude.carbon");
+  opt<std::string> prelude_file_name("prelude", desc("<prelude file>"),
+                                     llvm::cl::init(prelude_path.str().str()));
 
   llvm::cl::ParseCommandLineOptions(argc, argv);
 
