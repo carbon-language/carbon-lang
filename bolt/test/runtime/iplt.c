04641b212191e3cea73904d37c5a59b0f87f3247
Amir Ayupov <aaupov@fb.com>
2022-04-13 11:46:59 -0700

    [BOLT][TEST] Add -no-pie to two tests
    Missed these two tests in D123329 in a rebase.

--- bolt/test/runtime/iplt.c
+++ bolt/test/runtime/iplt.c
@@ -1,6 +1,6 @@
 // This test checks that the ifuncs works after bolt.
 
-// RUN: %clang %cflags %s -fuse-ld=lld \
+// RUN: %clang %cflags -no-pie %s -fuse-ld=lld \
 // RUN:    -o %t.exe -Wl,-q
 // RUN: llvm-bolt %t.exe -o %t.bolt.exe -use-old-text=0 -lite=0
 // RUN: %t.bolt.exe  | FileCheck %s
