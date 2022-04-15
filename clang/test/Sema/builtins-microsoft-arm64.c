8a9b4fb4aa6d2dde026d9ae08459aa9e7a1edb05
Pengxuan Zheng <pzheng@quicinc.com>
2022-04-15 14:57:40 -0700

    [COFF, ARM64] Add __break intrinsic
    https://docs.microsoft.com/en-us/cpp/intrinsics/arm64-intrinsics?view=msvc-170
    
    Reviewed By: rnk, mstorsjo
    
    Differential Revision: https://reviews.llvm.org/D124032

--- clang/test/Sema/builtins-microsoft-arm64.c
+++ clang/test/Sema/builtins-microsoft-arm64.c
@@ -3,6 +3,12 @@
 
 #include <intrin.h>
 
+void check__break(int x) {
+  __break(-1); // expected-error-re {{argument value {{.*}} is outside the valid range}}
+  __break(65536); // expected-error-re {{argument value {{.*}} is outside the valid range}}
+  __break(x); // expected-error {{argument to '__break' must be a constant integer}}
+}
+
 void check__getReg(void) {
   __getReg(-1); // expected-error-re {{argument value {{.*}} is outside the valid range}}
   __getReg(32); // expected-error-re {{argument value {{.*}} is outside the valid range}}
