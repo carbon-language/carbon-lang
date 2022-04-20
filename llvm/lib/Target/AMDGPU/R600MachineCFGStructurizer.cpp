bec8dff33e2bf68c72f24e80ee1a734036c4bbe0
Fangrui Song <i@maskray.me>
2022-04-19 22:36:58 -0700

    [AMDGPU] Fix -Wunused-variable in -DLLVM_ENABLE_ASSERTIONS=off builds

--- llvm/lib/Target/AMDGPU/R600MachineCFGStructurizer.cpp
+++ llvm/lib/Target/AMDGPU/R600MachineCFGStructurizer.cpp
@@ -741,6 +741,7 @@ bool R600MachineCFGStructurizer::run() {
     ++NumIter;
     LLVM_DEBUG(dbgs() << "numIter = " << NumIter
                       << ", numRemaintedBlk = " << NumRemainedBlk << "\n";);
+    (void)NumIter;
 
     SmallVectorImpl<MachineBasicBlock *>::const_iterator It =
         OrderedBlks.begin();
@@ -781,6 +782,7 @@ bool R600MachineCFGStructurizer::run() {
           LLVM_DEBUG(dbgs() << "Can't reduce SCC " << getSCCNum(MBB)
                             << ", sccNumIter = " << SccNumIter;
                      dbgs() << "doesn't make any progress\n";);
+          (void)SccNumIter;
           ContNextScc = true;
         } else if (sccRemainedNumBlk != 1 && sccRemainedNumBlk < SccNumBlk) {
           SccNumBlk = sccRemainedNumBlk;
