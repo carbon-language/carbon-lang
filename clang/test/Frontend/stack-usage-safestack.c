b8786413d8007f6864f0dc55d95a09cffff232e3
Paul Kirth <paulkirth@google.com>
2022-04-21 22:44:42 +0000

    Revert "[clang][safestack] Remove triple from stack usage test"
    This reverts commit 414f84ba29d96c8cbbe198cfc022146e4582cbef.
    
    Requires deirectives had already been added to the test, and CC1 tests
    should use a target triple.
    
    Differential Revision: https://reviews.llvm.org/D124210

--- clang/test/Frontend/stack-usage-safestack.c
+++ clang/test/Frontend/stack-usage-safestack.c
@@ -1,10 +1,12 @@
 /// Check that stack frame size warnings behave the same when safe stack is enabled
 
-// RUN: %clang_cc1 %s -fwarn-stack-size=48 -S -o - 2>&1 | FileCheck --check-prefix=REGULAR %s
-// RUN: %clang_cc1 %s -fwarn-stack-size=1060 -S -o - 2>&1 | FileCheck --check-prefix=IGNORE %s
+// REQUIRES: x86-registered-target
 
-// RUN: %clang_cc1 %s -fsanitize=safe-stack -fwarn-stack-size=48 -S -o - 2>&1 | FileCheck --check-prefix=SAFESTACK %s
-// RUN: %clang_cc1 %s -fsanitize=safe-stack -fwarn-stack-size=1060 -S -o - 2>&1 | FileCheck --check-prefix=IGNORE %s
+// RUN: %clang_cc1 %s -fwarn-stack-size=48 -S -o - -triple=i386-apple-darwin 2>&1 | FileCheck --check-prefix=REGULAR %s
+// RUN: %clang_cc1 %s -fwarn-stack-size=1060 -S -o - -triple=i386-apple-darwin 2>&1 | FileCheck --check-prefix=IGNORE %s
+
+// RUN: %clang_cc1 %s -fsanitize=safe-stack -fwarn-stack-size=48 -S -o - -triple=i386-apple-darwin 2>&1 | FileCheck --check-prefix=SAFESTACK %s
+// RUN: %clang_cc1 %s -fsanitize=safe-stack -fwarn-stack-size=1060 -S -o - -triple=i386-apple-darwin 2>&1 | FileCheck --check-prefix=IGNORE %s
 
 extern void init(char *buf, int size);
 extern int use_buf(char *buf, int size);
