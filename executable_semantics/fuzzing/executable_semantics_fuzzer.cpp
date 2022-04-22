d31c35e5798e906b809546ff854e2cc52a8ffb08
Pavel Kobyakov <pkobyakov@google.com>
2022-04-22 15:20:24 -0400

    code review

--- executable_semantics/fuzzing/executable_semantics_fuzzer.cpp
+++ executable_semantics/fuzzing/executable_semantics_fuzzer.cpp
@@ -5,15 +5,29 @@
 #include <google/protobuf/text_format.h>
 #include <libprotobuf_mutator/src/libfuzzer/libfuzzer_macro.h>
 
+#include <filesystem>
+
 #include "common/fuzzing/carbon.pb.h"
-#include "common/runfiles_util.h"
 #include "executable_semantics/fuzzing/fuzzer_util.h"
 #include "executable_semantics/interpreter/exec_program.h"
 #include "executable_semantics/syntax/parse.h"
 #include "executable_semantics/syntax/prelude.h"
+#include "llvm/Support/FileSystem.h"
 #include "llvm/Support/raw_ostream.h"
 
 namespace Carbon {
+
+// Determines runfiles dir to use.
+static auto GetRunfilesDir() -> std::string {
+  const char* test_src_dir = getenv("TEST_SRCDIR");
+  std::string runfiles_dir =
+      test_src_dir != nullptr
+          ? test_src_dir
+          : llvm::sys::fs::getMainExecutable(nullptr, nullptr) + ".runfiles";
+  CHECK(std::filesystem::exists(runfiles_dir))
+      << runfiles_dir << " doesn't exist";
+  return runfiles_dir;
+}
 
 // Parses and executes a fuzzer-generated program.
 void ParseAndExecute(const Fuzzing::CompilationUnit& compilation_unit) {
