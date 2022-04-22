ddfc84e63494a9689f7cc22cbc391d030ffdb9bb
Nikita Popov <npopov@redhat.com>
2022-04-22 10:19:54 +0200

    [fuzzer] Reduce size of large.test
    This halves the size of LargeTest, dropping time to compile this
    file locally from 14s to 5.5s. Hopefully this will also fix the
    persistent timeouts in pre-merge checks.
    
    Differential Revision: https://reviews.llvm.org/D124237

--- compiler-rt/test/fuzzer/LargeTest.cpp
+++ compiler-rt/test/fuzzer/LargeTest.cpp
@@ -24,8 +24,7 @@ static inline void break_optimization(const void *arg) {
 
 #define B do{A; A; A; A; A; A; A; A; A; A; A; A; A; A; A; A; A; A; }while(0)
 #define C do{B; B; B; B; B; B; B; B; B; B; B; B; B; B; B; B; B; B; }while(0)
-#define D do{C; C; C; C; C; C; C; C; C; C; C; C; C; C; C; C; C; C; }while(0)
-#define E do{D; D; D; D; D; D; D; D; D; D; D; D; D; D; D; D; D; D; }while(0)
+#define D do{C; C; C; C; C; C; C; C; C; }while(0)
 
 volatile int sink;
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
