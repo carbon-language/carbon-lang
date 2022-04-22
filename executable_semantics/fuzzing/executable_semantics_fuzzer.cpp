43f9ce718da9ae560216ce13cb45f52503728ca8
Pavel Kobyakov <pkobyakov@google.com>
2022-04-22 10:23:39 -0400

    getrunfiles

--- executable_semantics/fuzzing/executable_semantics_fuzzer.cpp
+++ executable_semantics/fuzzing/executable_semantics_fuzzer.cpp
@@ -5,6 +5,8 @@
 #include <google/protobuf/text_format.h>
 #include <libprotobuf_mutator/src/libfuzzer/libfuzzer_macro.h>
 
+#include <filesystem>
+
 #include "common/fuzzing/carbon.pb.h"
 #include "executable_semantics/fuzzing/fuzzer_util.h"
 #include "executable_semantics/interpreter/exec_program.h"
@@ -13,6 +15,12 @@
 #include "llvm/Support/raw_ostream.h"
 
 namespace Carbon {
+
+std::string GetRunfilesDir() {
+  const std::string program_name = std::filesystem::canonical("/proc/self/exe");
+  llvm::errs() << "### program name=" << program_name << "\n";
+  return program_name + ".runfiles";
+}
 
 // Parses and executes a fuzzer-generated program.
 void ParseAndExecute(const Fuzzing::CompilationUnit& compilation_unit) {
@@ -25,10 +33,11 @@ void ParseAndExecute(const Fuzzing::CompilationUnit& compilation_unit) {
     llvm::errs() << "Parsing failed: " << ast.error().message() << "\n";
     return;
   }
-  AddPrelude("executable_semantics/data/prelude.carbon", &arena,
-             &ast->declarations);
+  AddPrelude(
+      GetRunfilesDir() + "/carbon/executable_semantics/data/prelude.carbon",
+      &arena, &ast->declarations);
   const ErrorOr<int> result =
-      ExecProgram(&arena, *ast, /*trace_stream=*/nullptr);
+      ExecProgram(&arena, *ast, /*trace_stream=*/std::nullopt);
   if (!result.ok()) {
     llvm::errs() << "Execution failed: " << result.error().message() << "\n";
     return;
