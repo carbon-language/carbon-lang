16a4d3a85ca9805928c67da5d8b85db6d2b473a8
Fangrui Song <i@maskray.me>
2022-04-21 19:25:57 -0700

    [LegacyPM] Remove AddressSanitizerLegacyPass
    Using the legacy PM for the optimization pipeline was deprecated in 13.0.0.
    Following recent changes to remove non-core features of the legacy
    PM/optimization pipeline, remove AddressSanitizerLegacyPass,
    ModuleAddressSanitizerLegacyPass, and ASanGlobalsMetadataWrapperPass.
    
    MemorySanitizerLegacyPass was removed in D123894.
    
    Reviewed By: #sanitizers, vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D124216

--- llvm/bindings/go/llvm/InstrumentationBindings.cpp
+++ llvm/bindings/go/llvm/InstrumentationBindings.cpp
@@ -16,19 +16,10 @@
 #include "llvm/IR/Module.h"
 #include "llvm/Pass.h"
 #include "llvm/Transforms/Instrumentation.h"
-#include "llvm/Transforms/Instrumentation/AddressSanitizer.h"
 #include "llvm/Transforms/Instrumentation/MemorySanitizer.h"
 #include "llvm/Transforms/Instrumentation/ThreadSanitizer.h"
 
 using namespace llvm;
-
-void LLVMAddAddressSanitizerFunctionPass(LLVMPassManagerRef PM) {
-  unwrap(PM)->add(createAddressSanitizerFunctionPass());
-}
-
-void LLVMAddAddressSanitizerModulePass(LLVMPassManagerRef PM) {
-  unwrap(PM)->add(createModuleAddressSanitizerLegacyPassPass());
-}
 
 void LLVMAddThreadSanitizerPass(LLVMPassManagerRef PM) {
   unwrap(PM)->add(createThreadSanitizerLegacyPassPass());
--- llvm/include/llvm/Transforms/Instrumentation/AddressSanitizer.h
+++ llvm/include/llvm/Transforms/Instrumentation/AddressSanitizer.h
@@ -126,17 +126,6 @@ private:
   AsanDtorKind DestructorKind;
 };
 
-// Insert AddressSanitizer (address basic correctness checking) instrumentation
-FunctionPass *createAddressSanitizerFunctionPass(
-    bool CompileKernel = false, bool Recover = false,
-    bool UseAfterScope = false,
-    AsanDetectStackUseAfterReturnMode UseAfterReturn =
-        AsanDetectStackUseAfterReturnMode::Runtime);
-ModulePass *createModuleAddressSanitizerLegacyPassPass(
-    bool CompileKernel = false, bool Recover = false, bool UseGlobalsGC = true,
-    bool UseOdrIndicator = true,
-    AsanDtorKind DestructorKind = AsanDtorKind::Global);
-
 struct ASanAccessInfo {
   const int32_t Packed;
   const uint8_t AccessSizeIndex;
