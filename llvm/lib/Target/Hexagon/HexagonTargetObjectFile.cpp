3143840f216c72fcc7f7b9cd54b2ccb9619fdc58
Chris Bieneman <chris.bieneman@me.com>
2022-04-25 20:07:56 -0500

    NFC. Add missing DXILPointerTyID case
    This resolves -Werror hexigon build failures.

--- llvm/lib/Target/Hexagon/HexagonTargetObjectFile.cpp
+++ llvm/lib/Target/Hexagon/HexagonTargetObjectFile.cpp
@@ -332,6 +332,7 @@ unsigned HexagonTargetObjectFile::getSmallestAddressableSize(const Type *Ty,
   case Type::X86_MMXTyID:
   case Type::X86_AMXTyID:
   case Type::TokenTyID:
+  case Type::DXILPointerTyID:
     return 0;
   }
 
