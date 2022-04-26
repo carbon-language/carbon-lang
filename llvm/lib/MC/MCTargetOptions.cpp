e4479504837306ec997ad87a2ce2e0e81c62a539
Andrew Savonichev <andrew.savonichev@gmail.com>
2022-04-26 22:05:32 +0300

    [NFC] Fix order of initialization in MCTargetOptions
    This patch fixes a compiler warning after D121299:
    
    field 'MCUseDwarfDirectory' will be initialized after field
    'MCIncrementalLinkerCompatible'

--- llvm/lib/MC/MCTargetOptions.cpp
+++ llvm/lib/MC/MCTargetOptions.cpp
@@ -14,10 +14,10 @@ using namespace llvm;
 MCTargetOptions::MCTargetOptions()
     : MCRelaxAll(false), MCNoExecStack(false), MCFatalWarnings(false),
       MCNoWarn(false), MCNoDeprecatedWarn(false), MCNoTypeCheck(false),
-      MCSaveTempLabels(false), MCUseDwarfDirectory(DefaultDwarfDirectory),
-      MCIncrementalLinkerCompatible(false), ShowMCEncoding(false),
-      ShowMCInst(false), AsmVerbose(false), PreserveAsmComments(true),
-      Dwarf64(false) {}
+      MCSaveTempLabels(false), MCIncrementalLinkerCompatible(false),
+      ShowMCEncoding(false), ShowMCInst(false), AsmVerbose(false),
+      PreserveAsmComments(true), Dwarf64(false),
+      MCUseDwarfDirectory(DefaultDwarfDirectory) {}
 
 StringRef MCTargetOptions::getABIName() const {
   return ABIName;
