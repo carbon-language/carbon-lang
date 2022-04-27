18a1fc8459b7fa8ca639d53e4aa5e5f5d68e8407
Petr Hosek <phosek@google.com>
2022-04-26 23:23:53 -0700

    [CMake][compiler-rt] Provide a dedicated option for LLVM unwinder
    This allows configuring LLVM unwinder separately from the C++ library
    matching how we configure it in libcxx.
    
    This also applies changes made to libunwind+libcxxabi+libcxx in D113253
    to compiler-rt.
    
    Differential Revision: https://reviews.llvm.org/D115674

--- compiler-rt/cmake/config-ix.cmake
+++ compiler-rt/cmake/config-ix.cmake
@@ -4,8 +4,18 @@ include(CheckCCompilerFlag)
 include(CheckCXXCompilerFlag)
 include(CheckIncludeFiles)
 include(CheckLibraryExists)
+include(LLVMCheckCompilerLinkerFlag)
 include(CheckSymbolExists)
 include(TestBigEndian)
+
+# The compiler driver may be implicitly trying to link against libunwind.
+# This is normally ok (libcxx relies on an unwinder), but if libunwind is
+# built in the same cmake invocation as compiler-rt and we're using the
+# in tree version of runtimes, we'd be linking against the just-built
+# libunwind (and the compiler implicit -lunwind wouldn't succeed as the newly
+# built libunwind isn't installed yet). For those cases, it'd be good to
+# link with --uwnindlib=none. Check if that option works.
+llvm_check_compiler_linker_flag(C "--unwindlib=none" CXX_SUPPORTS_UNWINDLIB_NONE_FLAG)
 
 check_library_exists(c fopen "" COMPILER_RT_HAS_LIBC)
 if (COMPILER_RT_USE_BUILTINS_LIBRARY)
