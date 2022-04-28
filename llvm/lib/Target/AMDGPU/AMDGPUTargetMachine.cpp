3ff8ee2447eca4d33aa037b2b0782ae24b410d18
jeff <jrbyrnes1989@gmail.com>
2022-04-28 16:50:55 -0700

    [NFC] Fix typo
    Reviewed By: kerbowa
    
    Differential Revision: https://reviews.llvm.org/D124647

--- llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
+++ llvm/lib/Target/AMDGPU/AMDGPUTargetMachine.cpp
@@ -968,7 +968,7 @@ void AMDGPUPassConfig::addIRPasses() {
   addPass(createAlwaysInlinerLegacyPass());
   // We need to add the barrier noop pass, otherwise adding the function
   // inlining pass will cause all of the PassConfigs passes to be run
-  // one function at a time, which means if we have a nodule with two
+  // one function at a time, which means if we have a module with two
   // functions, then we will generate code for the first function
   // without ever running any passes on the second.
   addPass(createBarrierNoopPass());
