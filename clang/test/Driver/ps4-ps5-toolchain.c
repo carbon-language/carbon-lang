2ed0123b7fedd6360b0dc06cab9721f108123a98
Paul Robinson <paul.robinson@sony.com>
2022-05-02 11:03:08 -0700

    [PS5] Check for HasNativeLLVMSupport

--- clang/test/Driver/ps4-ps5-toolchain.c
+++ clang/test/Driver/ps4-ps5-toolchain.c
@@ -1,7 +1,13 @@
-// PS4/PS5 miscellaneous toolchain defaults.
+/// PS4/PS5 miscellaneous toolchain behavior.
 
 // RUN: %clang -c %s -### -target x86_64-scei-ps4 2>&1 | FileCheck %s
 // RUN: %clang -c %s -### -target x86_64-sie-ps5 2>&1 | FileCheck %s
 // CHECK-DAG: "-ffunction-sections"
 // CHECK-DAG: "-fdata-sections"
 // CHECK-DAG: "-fdeclspec"
+
+/// Verify LTO is enabled (no diagnostic).
+// RUN: %clang %s -### -target x86_64-scei-ps4 -flto 2>&1 | FileCheck %s --check-prefix=LTO
+// RUN: %clang %s -### -target x86_64-sie-ps5 -flto 2>&1 | FileCheck %s --check-prefix=LTO
+// LTO-NOT: error:
+// LTO-NOT: unable to pass LLVM bit-code
