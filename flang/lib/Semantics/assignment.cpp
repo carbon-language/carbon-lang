2472b6869a6eeb198a4e982fd7c3ffc89dd4f6f5
PeixinQiao <qiaopeixin@huawei.com>
2022-05-06 22:19:20 +0800

    [flang] Add one semantic check for masked array assignment
    As Fortran 2018 states, in each where-assignment-stmt, the mask-expr and
    the variable being defined shall be arrays of the same shape. The
    previous check does not consider checking if it is an array.
    
    Reviewed By: klausler
    
    Differential Revision: https://reviews.llvm.org/D125022

--- flang/lib/Semantics/assignment.cpp
+++ flang/lib/Semantics/assignment.cpp
@@ -216,10 +216,13 @@ bool AssignmentContext::CheckForPureContext(const SomeExpr &lhs,
   return true;
 }
 
-// 10.2.3.1(2) The masks and LHS of assignments must all have the same shape
+// 10.2.3.1(2) The masks and LHS of assignments must be arrays of the same shape
 void AssignmentContext::CheckShape(parser::CharBlock at, const SomeExpr *expr) {
   if (auto shape{evaluate::GetShape(foldingContext(), expr)}) {
     std::size_t size{shape->size()};
+    if (size == 0) {
+      Say(at, "The mask or variable must not be scalar"_err_en_US);
+    }
     if (whereDepth_ == 0) {
       whereExtents_.resize(size);
     } else if (whereExtents_.size() != size) {
