4750be4907bc22c1b7575fa53c3568628d6cbbe4
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-05-07 15:23:29 +0100

    [X86] Add 32-bit target test coverage to clean header tests

--- clang/test/Headers/x86-intrinsics-headers-clean.cpp
+++ clang/test/Headers/x86-intrinsics-headers-clean.cpp
@@ -1,7 +1,12 @@
 // Make sure the intrinsic headers compile cleanly with no warnings or errors.
 
-// RUN: %clang_cc1 -ffreestanding -triple x86_64-unknown-unknown -Wsystem-headers \
-// RUN:   -Wcast-qual -fsyntax-only -flax-vector-conversions=none -x c++ -verify %s
+// RUN: %clang_cc1 -ffreestanding -triple i386-unknown-unknown \
+// RUN:    -Werror -Wsystem-headers -Wcast-qual \
+// RUN:    -fsyntax-only -flax-vector-conversions=none -x c++ -verify %s
+
+// RUN: %clang_cc1 -ffreestanding -triple x86_64-unknown-unknown \
+// RUN:    -Werror -Wsystem-headers -Wcast-qual \
+// RUN:    -fsyntax-only -flax-vector-conversions=none -x c++ -verify %s
 
 // expected-no-diagnostics
 
