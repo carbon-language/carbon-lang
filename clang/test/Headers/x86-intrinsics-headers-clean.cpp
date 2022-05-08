72eb630207b812dced21fb820c6d65ecc2978c0a
Simon Pilgrim <llvm-dev@redking.me.uk>
2022-05-08 10:53:28 +0100

    [Headers][X86] Enable basic Wdocumentation testing on X86 headers
    First part of Issue #35297 - we want to enable Wdocumentation-pedantic as well, but need '\n' support first which Issue #55319 is addressing

--- clang/test/Headers/x86-intrinsics-headers-clean.cpp
+++ clang/test/Headers/x86-intrinsics-headers-clean.cpp
@@ -1,12 +1,12 @@
 // Make sure the intrinsic headers compile cleanly with no warnings or errors.
 
 // RUN: %clang_cc1 -ffreestanding -triple i386-unknown-unknown \
-// RUN:    -Wextra -Werror -Wsystem-headers -Wsign-conversion -Wcast-qual \
-// RUN:    -fsyntax-only -flax-vector-conversions=none -x c++ -verify %s
+// RUN:    -Wextra -Werror -Wsystem-headers -Wsign-conversion -Wcast-qual -Wdocumentation \
+// RUN:    -fsyntax-only -fretain-comments-from-system-headers -flax-vector-conversions=none -x c++ -verify %s
 
 // RUN: %clang_cc1 -ffreestanding -triple x86_64-unknown-unknown \
-// RUN:    -Wextra -Werror -Wsystem-headers -Wsign-conversion -Wcast-qual \
-// RUN:    -fsyntax-only -flax-vector-conversions=none -x c++ -verify %s
+// RUN:    -Wextra -Werror -Wsystem-headers -Wsign-conversion -Wcast-qual -Wdocumentation \
+// RUN:    -fsyntax-only -fretain-comments-from-system-headers -flax-vector-conversions=none -x c++ -verify %s
 
 // expected-no-diagnostics
 
