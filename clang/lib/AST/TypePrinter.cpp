b6572ad504753c6c27ba49c2ed595a091c977dcd
Erich Keane <erich.keane@intel.com>
2022-05-10 12:48:01 -0700

    [NFC] Add missing 'break' in a switch case

--- clang/lib/AST/TypePrinter.cpp
+++ clang/lib/AST/TypePrinter.cpp
@@ -283,6 +283,7 @@ bool TypePrinter::canPrefixQualifiers(const Type *T,
       // address_space attribute.
       const auto *AttrTy = cast<AttributedType>(UnderlyingType);
       CanPrefixQualifiers = AttrTy->getAttrKind() == attr::AddressSpace;
+      break;
     }
   }
 
