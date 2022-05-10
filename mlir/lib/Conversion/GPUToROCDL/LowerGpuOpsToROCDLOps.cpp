814b60509599614ba767cf0afef119cdbb7c4df8
Krzysztof Drewniak <Krzysztof.Drewniak@amd.com>
2022-05-10 15:37:53 +0000

    [mlir][AMDGPU] Add AMDGPU conversion patterns to ConvertGPUToROCDL
    This ensures that attributes such as the index bitwidth propagate
    correctly to the AMDGPUToROCDL patterns.
    
    Differential Revision: https://reviews.llvm.org/D125320

--- mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
+++ mlir/lib/Conversion/GPUToROCDL/LowerGpuOpsToROCDLOps.cpp
@@ -14,6 +14,7 @@
 #include "mlir/Conversion/ControlFlowToLLVM/ControlFlowToLLVM.h"
 #include "mlir/Conversion/GPUToROCDL/GPUToROCDLPass.h"
 
+#include "mlir/Conversion/AMDGPUToROCDL/AMDGPUToROCDL.h"
 #include "mlir/Conversion/ArithmeticToLLVM/ArithmeticToLLVM.h"
 #include "mlir/Conversion/FuncToLLVM/ConvertFuncToLLVM.h"
 #include "mlir/Conversion/LLVMCommon/ConversionTarget.h"
@@ -79,6 +80,7 @@ struct LowerGpuOpsToROCDLOpsPass
 
     mlir::arith::populateArithmeticToLLVMConversionPatterns(converter,
                                                             llvmPatterns);
+    populateAMDGPUToROCDLConversionPatterns(converter, llvmPatterns);
     populateVectorToLLVMConversionPatterns(converter, llvmPatterns);
     populateVectorToROCDLConversionPatterns(converter, llvmPatterns);
     cf::populateControlFlowToLLVMConversionPatterns(converter, llvmPatterns);
