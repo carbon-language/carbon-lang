eccf5efeccfab6b1028dd3bd7adf239d5f0a6381
Ben Shi <ben.shi@streamcomputing.com>
2022-05-10 02:15:13 +0000

    [RISCV][NFC] Add 'rv32izvfh' invalid arch test
    Reviewed By: craig.topper
    
    Differential Revision: https://reviews.llvm.org/D125210

--- llvm/test/MC/RISCV/attribute-arch-invalid.s
+++ llvm/test/MC/RISCV/attribute-arch-invalid.s
@@ -22,3 +22,6 @@
 
 .attribute arch, "rv32izbt"
 # CHECK: error: invalid arch name 'rv32izbt', experimental extension requires explicit version number `zbt`
+
+.attribute arch, "rv32izvfh"
+# CHECK: error: invalid arch name 'rv32izvfh', experimental extension requires explicit version number `zvfh`
