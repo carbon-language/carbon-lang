8527f32f0a16a77edb0b53a5ab8074e518eeff54
Ben Shi <ben.shi@streamcomputing.com>
2022-05-12 13:26:15 +0000

    [lld][ELF] Support BFD name elf32-avr
    Reviewed By: MaskRay
    
    differential Revision: https://reviews.llvm.org/D125544

--- lld/ELF/ScriptParser.cpp
+++ lld/ELF/ScriptParser.cpp
@@ -415,6 +415,7 @@ void ScriptParser::readOutputArch() {
 static std::pair<ELFKind, uint16_t> parseBfdName(StringRef s) {
   return StringSwitch<std::pair<ELFKind, uint16_t>>(s)
       .Case("elf32-i386", {ELF32LEKind, EM_386})
+      .Case("elf32-avr", {ELF32LEKind, EM_AVR})
       .Case("elf32-iamcu", {ELF32LEKind, EM_IAMCU})
       .Case("elf32-littlearm", {ELF32LEKind, EM_ARM})
       .Case("elf32-x86-64", {ELF32LEKind, EM_X86_64})
