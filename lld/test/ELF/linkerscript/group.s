82482e709f028d78c584be884b29f8e55d5aeb44
Fangrui Song <i@maskray.me>
2022-05-13 11:53:03 -0700

    [ELF][test] Clean up linkerscript/{filename-spec.s,group.s}

--- lld/test/ELF/linkerscript/group.s
+++ lld/test/ELF/linkerscript/group.s
@@ -1,64 +1,64 @@
 # REQUIRES: x86
 # UNSUPPORTED: system-windows
 
-# RUN: mkdir -p %t.dir
-# RUN: rm -f %t.dir/libxyz.a
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux %s -o %t
-# RUN: llvm-mc -filetype=obj -triple=x86_64-unknown-linux \
-# RUN:   %p/Inputs/libsearch-st.s -o %t2.o
-# RUN: llvm-ar rcs %t.dir/libxyz.a %t2.o
+# RUN: rm -rf %t.dir && mkdir %t.dir
+# RUN: rm -rf %t && split-file %s %t && cd %t
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %s -o a.o
+# RUN: llvm-mc -filetype=obj -triple=x86_64 %p/Inputs/libsearch-st.s -o b.o
+# RUN: llvm-ar rc %t.dir/libxyz.a b.o
 
-# RUN: echo "GROUP(\"%t\")" > %t.script
-# RUN: ld.lld -o %t2 %t.script
+# RUN: echo 'GROUP("a.o")' > %t.t
+# RUN: ld.lld -o %t2 %t.t
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "INPUT(\"%t\")" > %t.script
-# RUN: ld.lld -o %t2 %t.script
+# RUN: echo 'INPUT("a.o")' > %t.t
+# RUN: ld.lld -o %t2 %t.t
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "GROUP(\"%t\" libxyz.a )" > %t.script
-# RUN: not ld.lld -o /dev/null %t.script 2>/dev/null
-# RUN: ld.lld -o %t2 %t.script -L%t.dir
+# RUN: echo 'GROUP("a.o" libxyz.a )' > %t.t
+# RUN: not ld.lld -o /dev/null %t.t 2>/dev/null
+# RUN: ld.lld -o %t2 %t.t -L%t.dir
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "GROUP(\"%t\" =libxyz.a )" > %t.script
-# RUN: not ld.lld -o /dev/null %t.script  2>/dev/null
-# RUN: ld.lld -o %t2 %t.script --sysroot=%t.dir
+# RUN: echo 'GROUP("a.o" =libxyz.a )' > %t.t
+# RUN: not ld.lld -o /dev/null %t.t  2>/dev/null
+# RUN: ld.lld -o %t2 %t.t --sysroot=%t.dir
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "GROUP(\"%t\" -lxyz )" > %t.script
-# RUN: not ld.lld -o /dev/null %t.script  2>/dev/null
-# RUN: ld.lld -o %t2 %t.script -L%t.dir
+# RUN: echo 'GROUP("a.o" -lxyz )' > %t.t
+# RUN: not ld.lld -o /dev/null %t.t  2>/dev/null
+# RUN: ld.lld -o %t2 %t.t -L%t.dir
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "GROUP(\"%t\" libxyz.a )" > %t.script
-# RUN: not ld.lld -o /dev/null %t.script  2>/dev/null
-# RUN: ld.lld -o %t2 %t.script -L%t.dir
+# RUN: echo 'GROUP("a.o" libxyz.a )' > %t.t
+# RUN: not ld.lld -o /dev/null %t.t  2>/dev/null
+# RUN: ld.lld -o %t2 %t.t -L%t.dir
 # RUN: llvm-readobj %t2 > /dev/null
 
-# RUN: echo "GROUP(\"%t\" /libxyz.a )" > %t.script
-# RUN: echo "GROUP(\"%t\" /libxyz.a )" > %t.dir/xyz.script
-# RUN: not ld.lld -o /dev/null %t.script 2>&1 | FileCheck %s --check-prefix=CANNOT_OPEN -DFILE=/libxyz.a
-# RUN: not ld.lld -o /dev/null %t.script --sysroot=%t.dir 2>&1 | FileCheck %s --check-prefix=CANNOT_OPEN -DFILE=/libxyz.a
+# RUN: echo 'GROUP("a.o" /libxyz.a )' > %t.t
+# RUN: echo 'GROUP("%t/a.o" /libxyz.a )' > %t.dir/xyz.t
+# RUN: not ld.lld -o /dev/null %t.t 2>&1 | FileCheck %s --check-prefix=CANNOT_OPEN -DFILE=/libxyz.a
+# RUN: not ld.lld -o /dev/null %t.t --sysroot=%t.dir 2>&1 | FileCheck %s --check-prefix=CANNOT_OPEN -DFILE=/libxyz.a
 
 ## Since %t.dir/%t does not exist, report an error, instead of falling back to %t
 ## without the syroot prefix.
-# RUN: not ld.lld -o /dev/null %t.dir/xyz.script --sysroot=%t.dir 2>&1 | FileCheck %s --check-prefix=CANNOT_FIND_SYSROOT -DTMP=%t
+# RUN: not ld.lld -o /dev/null %t.dir/xyz.t --sysroot=%t.dir 2>&1 | FileCheck %s --check-prefix=CANNOT_FIND_SYSROOT -DTMP=%t/a.o
 
-# CANNOT_FIND_SYSROOT:      error: {{.*}}xyz.script:1: cannot find [[TMP]] inside [[TMP]].dir
+# CANNOT_FIND_SYSROOT:      error: {{.*}}xyz.t:1: cannot find [[TMP]] inside {{.*}}.dir
 # CANNOT_FIND_SYSROOT-NEXT: >>> GROUP({{.*}}
 
-# RUN: echo "GROUP(\"%t.script2\")" > %t.script1
-# RUN: echo "GROUP(\"%t\")" > %t.script2
-# RUN: ld.lld -o %t2 %t.script1
-# RUN: llvm-readobj %t2 > /dev/null
+# RUN: echo 'GROUP("2.t")' > 1.t
+# RUN: echo 'GROUP("a.o")' > 2.t
+# RUN: ld.lld 1.t
+# RUN: llvm-readobj a.out > /dev/null
 
-# RUN: echo "GROUP(AS_NEEDED(\"%t\"))" > %t.script
-# RUN: ld.lld -o %t2 %t.script
-# RUN: llvm-readobj %t2 > /dev/null
+# RUN: echo 'GROUP(AS_NEEDED("a.o"))' > 1.t
+# RUN: ld.lld 1.t
+# RUN: llvm-readobj a.out > /dev/null
 
 # CANNOT_OPEN: error: cannot open [[FILE]]: {{.*}}
 
+#--- a.s
 .globl _start
 _start:
   ret
