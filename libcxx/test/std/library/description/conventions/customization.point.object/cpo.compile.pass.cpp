496e135b034d7d9a9f06853899b204644a0bfb1a
Joe Loser <joeloser93@gmail.com>
2022-05-14 16:00:44 -0600

    [libc++][test] Verify std::views::drop and std::views::join are CPOs
    `std::views::drops` and `std::views::join` have been implemented, but the tests
    verifying the CPOs for them are still commented out. Uncomment the tests.
    
    Differential Revision: https://reviews.llvm.org/D125618

--- libcxx/test/std/library/description/conventions/customization.point.object/cpo.compile.pass.cpp
+++ libcxx/test/std/library/description/conventions/customization.point.object/cpo.compile.pass.cpp
@@ -42,7 +42,7 @@ constexpr bool test(CPO& o, Args&&...) {
 }
 
 int a[10];
-//int arrays[10][10];
+int arrays[10][10];
 //std::pair<int, int> pairs[10];
 
 // [concept.swappable]
@@ -86,11 +86,11 @@ static_assert(test(std::views::single, 4));
 static_assert(test(std::views::all, a));
 static_assert(test(std::views::common, a));
 static_assert(test(std::views::counted, a, 10));
-//static_assert(test(std::views::drop, a, 10));
+static_assert(test(std::views::drop, a, 10));
 //static_assert(test(std::views::drop_while, a, [](int x){ return x < 10; }));
 //static_assert(test(std::views::elements<0>, pairs));
 static_assert(test(std::views::filter, a, [](int x){ return x < 10; }));
-//static_assert(test(std::views::join, arrays));
+static_assert(test(std::views::join, arrays));
 //static_assert(test(std::views::split, a, 4));
 static_assert(test(std::views::lazy_split, a, 4));
 static_assert(test(std::views::reverse, a));
