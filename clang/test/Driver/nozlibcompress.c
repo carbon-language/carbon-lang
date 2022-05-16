0b69b8384d9b92898ec29f78a2364bec2277e516
Fangrui Song <i@maskray.me>
2022-05-16 10:42:43 -0700

    [Driver] Change "zlib not installed" diagnostic to "zlib not enabled"
    The former is a bit misleading and a user may try installing zlib which
    will not help.

--- clang/test/Driver/nozlibcompress.c
+++ clang/test/Driver/nozlibcompress.c
@@ -3,5 +3,5 @@
 // RUN: %clang -### -fintegrated-as -gz -c %s 2>&1 | FileCheck %s -check-prefix CHECK-WARN
 // RUN: %clang -### -fintegrated-as -gz=none -c %s 2>&1 | FileCheck -allow-empty -check-prefix CHECK-NOWARN %s
 
-// CHECK-WARN: warning: cannot compress debug sections (zlib not installed)
-// CHECK-NOWARN-NOT: warning: cannot compress debug sections (zlib not installed)
+// CHECK-WARN: warning: cannot compress debug sections (zlib not enabled)
+// CHECK-NOWARN-NOT: warning: cannot compress debug sections (zlib not enabled)
