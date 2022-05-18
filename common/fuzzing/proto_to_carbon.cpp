495907ff7e38c77b264524e5f86860dd8b2b9ff8
Pavel Kobyakov <pkobyakov@google.com>
2022-05-18 10:41:41 -0400

    removed __Fm<space> hack

--- common/fuzzing/proto_to_carbon.cpp
+++ common/fuzzing/proto_to_carbon.cpp
@@ -199,7 +199,7 @@ static auto ExpressionToCarbon(const Fuzzing::Expression& expression,
 
     case Fuzzing::Expression::kFunctionType: {
       const auto& fun_type = expression.function_type();
-      out << "__Fn ";
+      out << "__Fn";
       ExpressionToCarbon(fun_type.parameter(), out);
       out << " -> ";
       ExpressionToCarbon(fun_type.return_type(), out);
