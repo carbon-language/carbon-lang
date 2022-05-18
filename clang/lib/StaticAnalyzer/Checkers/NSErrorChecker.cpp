dd7233bc67e45106c4625df7d20391798f31d4a7
Usama Hameed <usamahameed@g.ucla.edu>
2022-05-18 14:26:40 -0700

    [Analyzer] Remove extra space from NSErrorChecker message.
    Differential Revision: https://reviews.llvm.org/D125840

--- clang/lib/StaticAnalyzer/Checkers/NSErrorChecker.cpp
+++ clang/lib/StaticAnalyzer/Checkers/NSErrorChecker.cpp
@@ -266,7 +266,7 @@ void NSOrCFErrorDerefChecker::checkEvent(ImplicitNullDerefEvent event) const {
   SmallString<128> Buf;
   llvm::raw_svector_ostream os(Buf);
 
-  os << "Potential null dereference.  According to coding standards ";
+  os << "Potential null dereference. According to coding standards ";
   os << (isNSError
          ? "in 'Creating and Returning NSError Objects' the parameter"
          : "documented in CoreFoundation/CFError.h the parameter");
