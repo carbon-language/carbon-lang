df25f0d5202bbd9e2e909dccb1aa16f8ebad5e7b
Sheng <ox59616e@gmail.com>
2022-05-19 21:17:29 +0800

    [M68k] Fix a bug in disassembler
    Sorry for my reckless patch. In some cases `RoundUp` is less than
    the bit width of APInt. We need to check this before we do zext.

--- llvm/lib/Target/M68k/Disassembler/M68kDisassembler.cpp
+++ llvm/lib/Target/M68k/Disassembler/M68kDisassembler.cpp
@@ -120,7 +120,8 @@ DecodeStatus M68kDisassembler::getInstruction(MCInst &Instr, uint64_t &Size,
   auto MakeUp = [&](APInt &Insn, unsigned InstrBits) {
     unsigned Idx = Insn.getBitWidth() >> 3;
     unsigned RoundUp = alignTo(InstrBits, Align(16));
-    Insn = Insn.zext(RoundUp);
+    if (RoundUp > Insn.getBitWidth())
+      Insn = Insn.zext(RoundUp);
     RoundUp = RoundUp >> 3;
     for (; Idx < RoundUp; Idx += 2) {
       Insn.insertBits(support::endian::read16be(&Bytes[Idx]), Idx * 8, 16);
