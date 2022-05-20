b369762beb70dfef22c7e793aed79b94d7dc0757
Jim Ingham <jingham@apple.com>
2022-05-20 15:14:26 -0700

    Convert the test file for TestIgnoredExceptions.py to the mach_vm API.
    The previous version of this test uses mprotect, and that seemed to be
    flakey on older systems.  I converted the test to use the underlying
    mach_vm API's.  The test only runs on Darwin anyway, so this is not a
    real limitation, and I'm hoping the lower level API's work more
    consistently.

--- lldb/test/API/macosx/ignore_exceptions/main.c
+++ lldb/test/API/macosx/ignore_exceptions/main.c
@@ -3,25 +3,34 @@
 #include <stdlib.h>
 #include <sys/mman.h>
 #include <stdio.h>
+#include <mach/mach.h>
+#include <mach/mach_vm.h>
 
-int g_ints[] = {10, 20, 30, 40, 50, 60};
+int *g_int_ptr = NULL;
+size_t g_size = 10*sizeof(int);
 
 void
 saction_handler(int signo, siginfo_t info, void *baton) {
-  printf("Got into handler.\n");
-  mprotect(g_ints, sizeof(g_ints), PROT_READ|PROT_WRITE); // stop here in the signal handler
-  g_ints[0] = 20;
+  printf("Got into handler.\n");   // stop here in the signal handler
+  kern_return_t success
+      = mach_vm_protect(mach_task_self(), g_int_ptr,
+                        g_size, 0, VM_PROT_READ|VM_PROT_WRITE);
+  g_int_ptr[1] = 20;
 }
 int
 main()
 {
-  mprotect(g_ints, 10*sizeof(int) , PROT_NONE);
+  kern_return_t vm_result = vm_allocate(mach_task_self(), &g_int_ptr, g_size, VM_FLAGS_ANYWHERE);
+  for (int i = 0; i < 10; i++)
+    g_int_ptr[i] = i * 10;
+  
+  vm_result = mach_vm_protect(mach_task_self(), g_int_ptr, g_size, 0, VM_PROT_NONE);
   struct sigaction my_action;
   sigemptyset(&my_action.sa_mask);
   my_action.sa_handler = (void (*)(int)) saction_handler;
   my_action.sa_flags = SA_SIGINFO;
 
   sigaction(SIGBUS, &my_action, NULL); // Stop here to get things going.
-  int local_value = g_ints[1];
+  int local_value = g_int_ptr[1];
   return local_value; // Break here to make sure we got past the signal handler
 }
