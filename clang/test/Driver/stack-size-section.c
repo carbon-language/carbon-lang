efebb27b745a0d677ad2ea9aefff242c12aef29c
Paul Robinson <paul.robinson@sony.com>
2022-05-24 09:59:57 -0700

    Revert "[PS5] Verify defaults to -fno-stack-size-section"
    This reverts commit 28432b0f655641df7f9d079cf69ba235038d6340.
    
    Caused some unexpected buildbot failures.

--- clang/test/Driver/stack-size-section.c
+++ clang/test/Driver/stack-size-section.c
@@ -1,15 +1,9 @@
 // RUN: %clang -target x86_64-unknown %s -### 2>&1 | FileCheck %s --check-prefix=CHECK-ABSENT
-// RUN: %clang -target x86_64-sie-ps5 %s -### 2>&1 | FileCheck %s --check-prefix=CHECK-ABSENT
 // RUN: %clang -target x86_64-scei-ps4 -fno-stack-size-section %s -### 2>&1 | FileCheck %s --check-prefix=CHECK-ABSENT
 // CHECK-ABSENT-NOT: -fstack-size-section
 
 // RUN: %clang -target x86_64-unknown -fstack-size-section -### 2>&1 | FileCheck %s --check-prefix=CHECK-PRESENT
 // RUN: %clang -target x86_64-scei-ps4 %s -### 2>&1 | FileCheck %s --check-prefix=CHECK-PRESENT
 // CHECK-PRESENT: -fstack-size-section
-
-// RUN: %clang -target x86_64-unknown -fstack-size-section -fno-stack-size-section -### 2>&1 \
-// RUN:     | FileCheck %s --check-prefix=CHECK-ABSENT
-// RUN: %clang -target x86_64-unknown -fnostack-size-section -fstack-size-section -### 2>&1 \
-// RUN:     | FileCheck %s --check-prefix=CHECK-PRESENT
 
 int foo() { return 42; }
