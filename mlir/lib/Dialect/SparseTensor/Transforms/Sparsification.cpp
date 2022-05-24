5799f843a22029bd51d45edcd773e3c8662a0a08
Aart Bik <ajcbik@google.com>
2022-05-24 11:49:47 -0700

    [mlir][sparse] add new complex ops to reduction recognition
    Reviewed By: bixia
    
    Differential Revision: https://reviews.llvm.org/D126318

--- mlir/lib/Dialect/SparseTensor/Transforms/Sparsification.cpp
+++ mlir/lib/Dialect/SparseTensor/Transforms/Sparsification.cpp
@@ -378,11 +378,14 @@ static vector::CombiningKind getCombiningKind(Reduction kind) {
 static Reduction getReduction(Kind kind) {
   switch (kind) {
   case Kind::kAddF:
+  case Kind::kAddC:
   case Kind::kAddI:
   case Kind::kSubF:
+  case Kind::kSubC:
   case Kind::kSubI:
     return kSum;
   case Kind::kMulF:
+  case Kind::kMulC:
   case Kind::kMulI:
     return kProduct;
   case Kind::kAndI:
