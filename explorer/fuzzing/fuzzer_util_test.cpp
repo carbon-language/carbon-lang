34cdde7689b25c15191d07dd9f375c7c34ddafb8
Pavel Kobyakov <pkobyakov@google.com>
2022-05-25 21:10:19 -0400

    Changed fuzzer_util_test to not process the full fuzzer corpus, but instead just run on a single file, to minimize friction when making fuzzer proto changes

--- explorer/fuzzing/fuzzer_util_test.cpp
+++ explorer/fuzzing/fuzzer_util_test.cpp
@@ -15,30 +15,50 @@
 namespace Carbon::Testing {
 namespace {
 
-static std::vector<llvm::StringRef>* carbon_files = nullptr;
-
 // A workaround for https://github.com/carbon-language/carbon-lang/issues/1208.
-TEST(FuzzerUtilTest, RunFuzzerOnCorpus) {
-  int file_count = 0;
-  for (const llvm::StringRef f : *carbon_files) {
-    llvm::outs() << "Processing " << f << "\n";
-    std::ifstream file(f.str(), std::ios::in);
-    ASSERT_TRUE(file.is_open());
-    std::stringstream contents;
-    contents << file.rdbuf();
-    const ErrorOr<Fuzzing::Carbon> carbon_proto =
-        ParseCarbonTextProto(contents.str());
-    ASSERT_TRUE(carbon_proto.ok()) << "couldn't parse text proto in " << f;
-    ParseAndExecute(carbon_proto->compilation_unit());
-    ++file_count;
-  }
-  EXPECT_GT(file_count, 0);
+// Runs fuzzer logic on a sample input to make sure it works.
+TEST(FuzzerUtilTest, FuzzerSmokeTest) {
+  const std::string carbon_source = R"(
+    compilation_unit {
+      package_statement {
+        package_name: "P"
+      }
+      is_api: true
+      declarations {
+        function {
+          name: "Main"
+          param_pattern {
+          }
+          return_term {
+            kind: Expression
+            type {
+              int_type_literal {
+              }
+            }
+          }
+          body {
+            statements {
+              return_statement {
+                expression {
+                  int_literal {
+                    value: 0
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    })";
+  const ErrorOr<Fuzzing::Carbon> carbon_proto =
+      ParseCarbonTextProto(carbon_source);
+  ASSERT_TRUE(carbon_proto.ok()) << "couldn't parse text proto";
+  ParseAndExecute(carbon_proto->compilation_unit());
 }
 
 TEST(FuzzerUtilTest, GetRunfilesFile) {
-  EXPECT_THAT(*Internal::GetRunfilesFile(
-                  "carbon/explorer/fuzzing/fuzzer_corpus/empty.textproto"),
-              testing::EndsWith("/empty.textproto"));
+  EXPECT_THAT(*Internal::GetRunfilesFile("carbon/explorer/data/prelude.carbon"),
+              testing::EndsWith("/prelude.carbon"));
   EXPECT_THAT(Internal::GetRunfilesFile("nonexistent-file").error().message(),
               testing::EndsWith("doesn't exist"));
 }
@@ -63,10 +83,3 @@ TEST(FuzzerUtilTest, ParseCarbonTextProtoWithUnknownField) {
 
 }  // namespace
 }  // namespace Carbon::Testing
-
-auto main(int argc, char** argv) -> int {
-  ::testing::InitGoogleTest(&argc, argv);
-  Carbon::Testing::carbon_files =
-      new std::vector<llvm::StringRef>(&argv[1], &argv[argc]);
-  return RUN_ALL_TESTS();
-}
