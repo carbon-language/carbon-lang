4dc3893eeb47bef9298c34cdc993165af88721a5
Alex Brachet <abrachet@google.com>
2022-05-27 17:03:32 +0000

    Revert "[Clang] Extend -gen-reproducer flag"
    This reverts commit 684c080108766b4f112f172fed4a49059484614d.

--- clang/include/clang/Driver/Driver.h
+++ clang/include/clang/Driver/Driver.h
@@ -276,6 +276,11 @@ private:
   unsigned ProbePrecompiled : 1;
 
 public:
+  /// Force clang to emit reproducer for driver invocation. This is enabled
+  /// indirectly by setting FORCE_CLANG_DIAGNOSTICS_CRASH environment variable
+  /// or when using the -gen-reproducer driver flag.
+  unsigned GenReproducer : 1;
+
   // getFinalPhase - Determine which compilation mode we are in and record
   // which option we used to determine the final phase.
   // TODO: Much of what getFinalPhase returns are not actually true compiler
@@ -499,32 +504,6 @@ public:
       Compilation &C, const Command &FailingCommand,
       StringRef AdditionalInformation = "",
       CompilationDiagnosticReport *GeneratedReport = nullptr);
-
-  enum class CommandStatus {
-    Crash = 1,
-    Error,
-    Ok,
-  };
-
-  enum class ReproLevel {
-    Off = 0,
-    OnCrash = static_cast<int>(CommandStatus::Crash),
-    OnError = static_cast<int>(CommandStatus::Error),
-    Always = static_cast<int>(CommandStatus::Ok),
-  };
-
-  bool maybeGenerateCompilationDiagnostics(
-      CommandStatus CS, ReproLevel Level, Compilation &C,
-      const Command &FailingCommand, StringRef AdditionalInformation = "",
-      CompilationDiagnosticReport *GeneratedReport = nullptr) {
-    if (static_cast<int>(CS) > static_cast<int>(Level))
-      return false;
-    // Hack to ensure that diagnostic notes get emitted.
-    Diags.setLastDiagnosticIgnored(false);
-    generateCompilationDiagnostics(C, FailingCommand, AdditionalInformation,
-                                   GeneratedReport);
-    return true;
-  }
 
   /// @}
   /// @name Helper Methods
