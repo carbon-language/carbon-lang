858e6273d938cc4d08ee053ddff3fe7b19eb302a
Nikita Popov <npopov@redhat.com>
2022-05-31 15:40:46 +0200

    [Clang] Always set opaque pointers mode
    Always set the opaque pointers mode, to make sure that
    -no-opaque-pointers continues working when the default on the LLVM
    side is flipped.

--- clang/lib/CodeGen/CodeGenAction.cpp
+++ clang/lib/CodeGen/CodeGenAction.cpp
@@ -1017,8 +1017,7 @@ CodeGenAction::CreateASTConsumer(CompilerInstance &CI, StringRef InFile) {
   if (BA != Backend_EmitNothing && !OS)
     return nullptr;
 
-  if (CI.getCodeGenOpts().OpaquePointers)
-    VMContext->setOpaquePointers(true);
+  VMContext->setOpaquePointers(CI.getCodeGenOpts().OpaquePointers);
 
   // Load bitcode modules to link with, if we need to.
   if (LinkModules.empty())
