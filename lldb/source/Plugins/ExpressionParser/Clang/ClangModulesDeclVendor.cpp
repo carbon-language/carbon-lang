ca73de43744503a557b1f3709c0ff4751798702f
Adrian Prantl <aprantl@apple.com>
2022-05-31 13:54:58 -0700

    Adapt LLDB for D120540.
    In https://reviews.llvm.org/D120540 the -fcxx-modules flag changed
    semantics and specifying it explicitly seems to no longer be what we
    want here.

--- lldb/source/Plugins/ExpressionParser/Clang/ClangModulesDeclVendor.cpp
+++ lldb/source/Plugins/ExpressionParser/Clang/ClangModulesDeclVendor.cpp
@@ -604,7 +604,6 @@ ClangModulesDeclVendor::Create(Target &target) {
       "clang",
       "-fmodules",
       "-fimplicit-module-maps",
-      "-fcxx-modules",
       "-fsyntax-only",
       "-femit-all-decls",
       "-target",
