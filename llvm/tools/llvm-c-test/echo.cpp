8bfd69ca33dd27c199652d5efa4e8a570c096edc
Nikita Popov <npopov@redhat.com>
2022-06-01 12:54:58 +0200

    [llvm-c-test] Always set opaque pointers mode
    Avoid a behavior change when opaque pointers are enabled by default.

--- llvm/tools/llvm-c-test/echo.cpp
+++ llvm/tools/llvm-c-test/echo.cpp
@@ -1401,8 +1401,7 @@ int llvm_echo(bool OpaquePointers) {
   size_t ModuleIdentLen;
   const char *ModuleName = LLVMGetModuleIdentifier(Src, &ModuleIdentLen);
   LLVMContextRef Ctx = LLVMContextCreate();
-  if (OpaquePointers)
-    LLVMContextSetOpaquePointers(Ctx, true);
+  LLVMContextSetOpaquePointers(Ctx, OpaquePointers);
   LLVMModuleRef M = LLVMModuleCreateWithNameInContext(ModuleName, Ctx);
 
   LLVMSetSourceFileName(M, SourceFileName, SourceFileLen);
