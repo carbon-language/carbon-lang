1a729bce86173c9019545599c8a0771d0419ce9e
Kevin Athey <kda@google.com>
2022-06-02 14:18:00 -0700

    [MSAN] add dn_expand intercept.
    This interceptor only addresses writes to inputs.  Reads of inputs are not checked.
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D126851

---
+++ compiler-rt/test/msan/Linux/dn_expand.cpp
@@ -0,0 +1,42 @@
+// RUN: %clangxx_msan -O0 %s -o %t -lresolv && %run %t
+
+#include <assert.h>
+#include <resolv.h>
+#include <string.h>
+
+#include <sanitizer/msan_interface.h>
+
+void testWrite() {
+  char unsigned input[] = {0xff, 0xc5, 0xf7, 0xff, 0x00, 0x00, 0xff, 0x0a, 0x00,
+                           0x00, 0x00, 0x01, 0x00, 0x00, 0x02, 0x00, 0x01, 0x00,
+                           0x10, 0x01, 0x05, 0x00, 0x01, 0x0a, 0x67, 0x6f, 0x6f,
+                           0x67, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, 0x00};
+  char output[1024];
+
+  int res = dn_expand(input, input + sizeof(input), input + 23, output,
+                      sizeof(output));
+
+  assert(res >= 0);
+  __msan_check_mem_is_initialized(output, res);
+}
+
+void testWriteZeroLength() {
+  char unsigned input[] = {
+      0xff, 0xc5, 0xf7, 0xff, 0x00, 0x00, 0xff, 0x0a, 0x00, 0x00, 0x00, 0x01,
+      0x00, 0x00, 0x02, 0x00, 0x01, 0x00, 0x10, 0x01, 0x05, 0x00, 0x01, 0x00,
+  };
+  char output[1024];
+
+  int res = dn_expand(input, input + sizeof(input), input + 23, output,
+                      sizeof(output));
+
+  assert(res >= 0);
+  __msan_check_mem_is_initialized(output, res);
+}
+
+int main(int iArgc, const char *szArgv[]) {
+  testWrite();
+  testWriteZeroLength();
+
+  return 0;
+}
---
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/dn_expand.cpp
@@ -0,0 +1,42 @@
+// RUN: %clangxx %s -o %t -lresolv && %run %t %p
+
+#include <assert.h>
+#include <resolv.h>
+#include <string.h>
+
+#include <sanitizer/msan_interface.h>
+
+void testWrite() {
+  char unsigned input[] = {0xff, 0xc5, 0xf7, 0xff, 0x00, 0x00, 0xff, 0x0a, 0x00,
+                           0x00, 0x00, 0x01, 0x00, 0x00, 0x02, 0x00, 0x01, 0x00,
+                           0x10, 0x01, 0x05, 0x00, 0x01, 0x0a, 0x67, 0x6f, 0x6f,
+                           0x67, 0x6c, 0x65, 0x2e, 0x63, 0x6f, 0x6d, 0x00};
+  char output[1024];
+
+  int res = dn_expand(input, input + sizeof(input), input + 23, output,
+                      sizeof(output));
+
+  assert(res == 12);
+  assert(strcmp(output, "google\\.com") == 0);
+}
+
+void testWriteZeroLength() {
+  char unsigned input[] = {
+      0xff, 0xc5, 0xf7, 0xff, 0x00, 0x00, 0xff, 0x0a, 0x00, 0x00, 0x00, 0x01,
+      0x00, 0x00, 0x02, 0x00, 0x01, 0x00, 0x10, 0x01, 0x05, 0x00, 0x01, 0x00,
+  };
+  char output[1024];
+
+  int res = dn_expand(input, input + sizeof(input), input + 23, output,
+                      sizeof(output));
+
+  assert(res == 1);
+  assert(strcmp(output, "") == 0);
+}
+
+int main(int iArgc, const char *szArgv[]) {
+  testWrite();
+  testWriteZeroLength();
+
+  return 0;
+}
