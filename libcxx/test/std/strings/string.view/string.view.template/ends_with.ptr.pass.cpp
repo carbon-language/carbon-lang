4be36dc77fc96766988344b54f6ab5aade86e1c6
Joe Loser <joeloser93@gmail.com>
2022-06-02 12:11:06 -0600

    [libc++][test] Fix unused variable warning in string_view tests
    In 6423a9f0ec8ba70049ea76e7bcfc9a9d1a54e826, I accidentally thought this was
    getting tested, but these variables are unused. Just remove the lines instead of
    leaving them commented out.
    
    Differential Revision: https://reviews.llvm.org/D126901

--- libcxx/test/std/strings/string.view/string.view.template/ends_with.ptr.pass.cpp
+++ libcxx/test/std/strings/string.view/string.view.template/ends_with.ptr.pass.cpp
@@ -26,9 +26,6 @@ int main(int, char**)
     SV  sv0 {};
     SV  sv1 { s + 4, 1 };
     SV  sv2 { s + 3, 2 };
-    SV  sv3 { s + 2, 3 };
-    SV  sv4 { s + 1, 4 };
-    SV  sv5 { s    , 5 };
     SV  svNot {"def", 3 };
 
     LIBCPP_ASSERT_NOEXCEPT(sv0.ends_with(""));
@@ -68,9 +65,6 @@ int main(int, char**)
     constexpr SV  sv0 {};
     constexpr SV  sv1 { s + 4, 1 };
     constexpr SV  sv2 { s + 3, 2 };
-//     constexpr SV  sv3 { s + 2, 3 };
-//     constexpr SV  sv4 { s + 1, 4 };
-//     constexpr SV  sv5 { s,     5 };
     constexpr SV  svNot {"def", 3 };
 
     static_assert ( sv0.ends_with(""), "" );
--- libcxx/test/std/strings/string.view/string.view.template/starts_with.ptr.pass.cpp
+++ libcxx/test/std/strings/string.view/string.view.template/starts_with.ptr.pass.cpp
@@ -26,9 +26,6 @@ int main(int, char**)
     SV  sv0 {};
     SV  sv1 { s, 1 };
     SV  sv2 { s, 2 };
-    SV  sv3 { s, 3 };
-    SV  sv4 { s, 4 };
-    SV  sv5 { s, 5 };
     SV  svNot {"def", 3 };
 
     LIBCPP_ASSERT_NOEXCEPT(sv0.starts_with(""));
@@ -68,9 +65,6 @@ int main(int, char**)
     constexpr SV  sv0 {};
     constexpr SV  sv1 { s, 1 };
     constexpr SV  sv2 { s, 2 };
-//     constexpr SV  sv3 { s, 3 };
-//     constexpr SV  sv4 { s, 4 };
-//     constexpr SV  sv5 { s, 5 };
     constexpr SV  svNot {"def", 3 };
 
     static_assert ( sv0.starts_with(""), "" );
