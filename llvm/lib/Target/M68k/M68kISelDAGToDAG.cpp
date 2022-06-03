78b16ccf2b5c4ba4ebd7f1e2a2fc1dcad0e1a343
Douglas Chen <dougpuob@gmail.com>
2022-06-03 13:19:03 +0800

    [M68k] Instruction selection to choose neg x when mul x -1 (Fix issue 48588)
    This patch is trying to fix issue 48588(https://github.com/llvm/llvm-project/issues/48588)
    
    I found the results of Instruction Selection between SelectionDAG and FastISEL for the `%mul = mul i32 %A, 4294967295`:
    (seldag-isel) mul --> sub --> SUB32dp
    (fast-isel)   mul --> sub --> NEG32d
    
    My patch to fix this issue is by overriding a virtual function M68kDAGToDAGISel::IsProfitableToFold(). Return `false` when it was trying to match with SUB, then it will match with NEG.
    
    Reviewed By: myhsu
    
    Differential Revision: https://reviews.llvm.org/D116886

--- llvm/lib/Target/M68k/M68kISelDAGToDAG.cpp
+++ llvm/lib/Target/M68k/M68kISelDAGToDAG.cpp
@@ -181,6 +181,7 @@ public:
   }
 
   bool runOnMachineFunction(MachineFunction &MF) override;
+  bool IsProfitableToFold(SDValue N, SDNode *U, SDNode *Root) const override;
 
 private:
   /// Keep a pointer to the M68kSubtarget around so that we can
@@ -310,6 +311,33 @@ private:
   SDNode *getGlobalBaseReg();
 };
 } // namespace
+
+bool M68kDAGToDAGISel::IsProfitableToFold(SDValue N, SDNode *U,
+                                          SDNode *Root) const {
+  if (OptLevel == CodeGenOpt::None)
+    return false;
+
+  if (U == Root) {
+    switch (U->getOpcode()) {
+    default:
+      return true;
+    case M68kISD::SUB:
+    case ISD::SUB:
+      // Prefer NEG instruction when zero subtracts a value.
+      // e.g.
+      //   move.l	#0, %d0
+      //   sub.l	(4,%sp), %d0
+      // vs.
+      //   move.l	(4,%sp), %d0
+      //   neg.l	%d0
+      if (llvm::isNullConstant(U->getOperand(0)))
+        return false;
+      break;
+    }
+  }
+
+  return true;
+}
 
 bool M68kDAGToDAGISel::runOnMachineFunction(MachineFunction &MF) {
   Subtarget = &MF.getSubtarget<M68kSubtarget>();
