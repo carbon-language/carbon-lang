aa9b3389ffd85533b4e7d27f2dac242046f41e8e
Yuki Okushi <jtitor@2k36.org>
2022-06-05 12:30:50 +0900

    [clang] Remove some `U+C2AD`s in `__cpp_multidimensional_subscript`
    The `Builder.defineMacro("__cpp_multidimensional_subscript", "202110L");` line has
    some `U+C2AD`s that shouldn't necessary here. So removed them.
    
    Differential Revision: https://reviews.llvm.org/D127066

--- clang/lib/Frontend/InitPreprocessor.cpp
+++ clang/lib/Frontend/InitPreprocessor.cpp
@@ -690,7 +690,7 @@ static void InitializeCPlusPlusFeatureTestMacros(const LangOptions &LangOpts,
     Builder.defineMacro("__cpp_implicit_move", "202011L");
     Builder.defineMacro("__cpp_size_t_suffix", "202011L");
     Builder.defineMacro("__cpp_if_consteval", "202106L");
-    Builder.defineMacro("__cpp_­multidimensional_­subscript", "202110L");
+    Builder.defineMacro("__cpp_multidimensional_subscript", "202110L");
   }
   if (LangOpts.Char8)
     Builder.defineMacro("__cpp_char8_t", "201811L");
