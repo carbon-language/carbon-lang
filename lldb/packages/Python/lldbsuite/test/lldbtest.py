d3d9bbd104368850d5e362ce8c5982f468272ae8
Muhammad Omair Javaid <omair.javaid@linaro.org>
2022-06-07 17:47:50 +0400

    [LLDB] Fix TestBase.generateSource for AArch64/Windows
    This patch adds a minor fix in lldbtest.py TestBase.generateSource
    function. Generated Python source with directory paths was not being
    escaped properly. This fix makes sure we treat dir path as raw string.

--- lldb/packages/Python/lldbsuite/test/lldbtest.py
+++ lldb/packages/Python/lldbsuite/test/lldbtest.py
@@ -1775,7 +1775,7 @@ class TestBase(Base):
         if self.hasDarwinFramework():
             include_stmt = "'#include <%s>' % os.path.join('LLDB', header)"
         else:
-            include_stmt = "'#include <%s>' % os.path.join('" + public_api_dir + "', header)"
+            include_stmt = "'#include <%s>' % os.path.join(r'" + public_api_dir + "', header)"
         list = [eval(include_stmt) for header in public_headers if (
             header.startswith("SB") and header.endswith(".h"))]
         includes = '\n'.join(list)
