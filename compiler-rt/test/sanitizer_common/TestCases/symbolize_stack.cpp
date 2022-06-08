56eb8dcf1e00efca01cd6b41c7a85ab2c39b71eb
Paul Kirth <paulkirth@google.com>
2022-06-08 18:59:27 +0000

    [compiler-rt][test] Restore original symbolize_stack test
    In D126580 we updated the test to reflect that there should always
    be a full trace. However, some executions do not have symbolizer
    information, so we will restore the original test until we can formulate
    a more robust test.
    
    Reviewed By: leonardchan
    
    Differential Revision: https://reviews.llvm.org/D127334

--- compiler-rt/test/sanitizer_common/TestCases/symbolize_stack.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/symbolize_stack.cpp
@@ -26,6 +26,6 @@ __attribute__((noinline)) void A<0>::RecursiveTemplateFunction(const T &) {
 }
 
 int main() {
-  // CHECK: {{#[0-9]+.*A<0>.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector<.*vector.*symbolize_stack.cpp:25}}
+  // CHECK: {{vector<.*vector<.*vector<.*vector<.*vector<}}
   A<10>().RecursiveTemplateFunction(0);
 }
