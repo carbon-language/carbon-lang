7b1aa20a767cdb8a44745456d1986fe501fcac54
Kevin Athey <kda@google.com>
2022-06-08 09:45:05 -0700

    [MSAN] Add result printing for failed call in pthread_getaffinity_np.
    Will be reverted when test failure is diagnosed.
    
    Depends on: https://reviews.llvm.org/D127185
    
    Reviewed By: vitalybuka
    
    Differential Revision: https://reviews.llvm.org/D127320

--- compiler-rt/test/sanitizer_common/TestCases/Linux/pthread_getaffinity_np.cpp
+++ compiler-rt/test/sanitizer_common/TestCases/Linux/pthread_getaffinity_np.cpp
@@ -4,6 +4,7 @@
 
 #include <assert.h>
 #include <pthread.h>
+#include <stdio.h>
 #include <sys/sysinfo.h>
 
 #include <sanitizer/msan_interface.h>
@@ -11,6 +12,8 @@
 int main() {
   cpu_set_t set_x;
   int res = pthread_getaffinity_np(pthread_self(), sizeof(set_x), &set_x);
+  if (res != 0)
+    printf("res: %d\n", res);
   assert(res == 0);
   assert(CPU_COUNT_S(sizeof(set_x), &set_x) == get_nprocs());
 
