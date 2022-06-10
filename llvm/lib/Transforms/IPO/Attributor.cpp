db68a25ca90e0da46c9c33b027fa83260073bd28
Mitch Phillips <31459023+hctim@users.noreply.github.com>
2022-06-10 13:56:29 -0700

    Revert "[Attributor] Ensure to use the proper liveness AA"
    This reverts commit a3273c0c06003b9e5f53a856844fee42a17e6b7b.
    
    Reason: Broke the ASan buildbots with a memory leak. See
    https://reviews.llvm.org/rG94841c713fdd2bce3276015d1e946d414bb74ee8 for
    more information.

--- llvm/lib/Transforms/IPO/Attributor.cpp
+++ llvm/lib/Transforms/IPO/Attributor.cpp
@@ -1188,14 +1188,16 @@ bool Attributor::isAssumedDead(const Instruction &I,
   if (ManifestAddedBlocks.contains(I.getParent()))
     return false;
 
-  if (!FnLivenessAA || FnLivenessAA->getAnchorScope() != I.getFunction())
-    FnLivenessAA = &getOrCreateAAFor<AAIsDead>(
-        IRPosition::function(*I.getFunction(), CBCtx), QueryingAA,
-        DepClassTy::NONE);
+  if (!FnLivenessAA)
+    FnLivenessAA =
+        lookupAAFor<AAIsDead>(IRPosition::function(*I.getFunction(), CBCtx),
+                              QueryingAA, DepClassTy::NONE);
 
   // If we have a context instruction and a liveness AA we use it.
-  if (CheckBBLivenessOnly ? FnLivenessAA->isAssumedDead(I.getParent())
-                          : FnLivenessAA->isAssumedDead(&I)) {
+  if (FnLivenessAA &&
+      FnLivenessAA->getIRPosition().getAnchorScope() == I.getFunction() &&
+      (CheckBBLivenessOnly ? FnLivenessAA->isAssumedDead(I.getParent())
+                           : FnLivenessAA->isAssumedDead(&I))) {
     if (QueryingAA)
       recordDependence(*FnLivenessAA, *QueryingAA, DepClass);
     if (!FnLivenessAA->isKnownDead(&I))
@@ -1266,9 +1268,9 @@ bool Attributor::isAssumedDead(const BasicBlock &BB,
                                const AbstractAttribute *QueryingAA,
                                const AAIsDead *FnLivenessAA,
                                DepClassTy DepClass) {
-  if (!FnLivenessAA || FnLivenessAA->getAnchorScope() != BB.getParent())
-    FnLivenessAA = &getOrCreateAAFor<AAIsDead>(
-        IRPosition::function(*BB.getParent()), QueryingAA, DepClassTy::NONE);
+  if (!FnLivenessAA)
+    FnLivenessAA = lookupAAFor<AAIsDead>(IRPosition::function(*BB.getParent()),
+                                         QueryingAA, DepClassTy::NONE);
   if (FnLivenessAA->isAssumedDead(&BB)) {
     if (QueryingAA)
       recordDependence(*FnLivenessAA, *QueryingAA, DepClass);
