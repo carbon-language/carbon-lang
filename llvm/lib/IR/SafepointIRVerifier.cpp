3b54aa9eab823f8edc8eaae4fba8e6f646ff5f91
Danila Malyutin <dmalyutin@azul.com>
2022-06-14 17:32:15 +0300

    freeze does not change the constant property
    Traverse through freeze when checking if base is constant
    
    Differential Revision: https://reviews.llvm.org/D127744

--- llvm/lib/IR/SafepointIRVerifier.cpp
+++ llvm/lib/IR/SafepointIRVerifier.cpp
@@ -363,6 +363,11 @@ static enum BaseType getBaseType(const Value *Val) {
       Worklist.push_back(GCRelocate->getDerivedPtr());
       continue;
     }
+    if (const auto *FI = dyn_cast<FreezeInst>(V)) {
+      // Freeze does not change null-ness or constant-ness of the value.
+      Worklist.push_back(FI->getOperand(0));
+      continue;
+    }
     if (isa<Constant>(V)) {
       // We found at least one base pointer which is non-null, so this derived
       // pointer is not exclusively derived from null.
