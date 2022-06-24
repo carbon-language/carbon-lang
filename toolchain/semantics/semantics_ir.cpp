4e712807e9036a87da09ebdbc80c3c12f6d5a215
jonmeow <jperkins@google.com>
2022-06-24 23:45:50 +0000

    Refactor instructions

--- toolchain/semantics/semantics_ir.cpp
+++ toolchain/semantics/semantics_ir.cpp
@@ -10,100 +10,28 @@
 
 namespace Carbon {
 
-void SemanticsIR::Print(llvm::raw_ostream& out, ParseTree::Node node) const {
-  out << parse_tree_->GetNodeText(node);
-}
-
 void SemanticsIR::Print(llvm::raw_ostream& out,
-                        Semantics::Declaration decl) const {
-  switch (decl.kind()) {
-    case Semantics::DeclarationKind::Function:
-      Print(out, declarations_.Get<Semantics::Function>(decl));
+                        Semantics::NodeRef node_ref) const {
+  switch (node_ref.kind()) {
+    case Semantics::NodeKind::BinaryOperator:
+      nodes_.Get<Semantics::BinaryOperator>(node_ref).Print(out);
       return;
-    case Semantics::DeclarationKind::Invalid:
-      CARBON_FATAL() << "Invalid declaration type";
-  }
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        Semantics::Statement stmt) const {
-  switch (stmt.kind()) {
-    case Semantics::StatementKind::InfixOperator:
-      Print(out, statements_.Get<Semantics::InfixOperator>(stmt));
+    case Semantics::NodeKind::Function:
+      nodes_.Get<Semantics::Function>(node_ref).Print(
+          out, [&](Semantics::NodeRef other) { Print(out, other); });
       return;
-    case Semantics::StatementKind::Literal:
-      Print(out, statements_.Get<Semantics::Literal>(stmt));
+    case Semantics::NodeKind::IntegerLiteral:
+      nodes_.Get<Semantics::IntegerLiteral>(node_ref).Print(out);
       return;
-    case Semantics::StatementKind::Return:
-      Print(out, statements_.Get<Semantics::Return>(stmt));
+    case Semantics::NodeKind::Return:
+      nodes_.Get<Semantics::Return>(node_ref).Print(out);
       return;
-    case Semantics::StatementKind::Invalid:
-      CARBON_FATAL() << "Invalid expression type";
+    case Semantics::NodeKind::SetName:
+      nodes_.Get<Semantics::SetName>(node_ref).Print(out);
+      return;
+    case Semantics::NodeKind::Invalid:
+      CARBON_FATAL() << "Invalid NodeRef kind";
   }
-  out << ";";
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::DeclaredName& name) const {
-  Print(out, name.node());
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::Function& function) const {
-  out << "fn ";
-  Print(out, function.name());
-  out << "(";
-  llvm::ListSeparator sep;
-  for (const auto& param : function.params()) {
-    out << sep;
-    Print(out, param);
-  }
-  out << ")";
-  if (function.return_expr()) {
-    out << " -> ";
-    Print(out, *function.return_expr());
-  }
-  Print(out, function.body());
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::InfixOperator& op) const {
-  Print(out, op.lhs());
-  out << " ";
-  Print(out, op.node());
-  out << " ";
-  Print(out, op.rhs());
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::Literal& literal) const {
-  Print(out, literal.node());
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::PatternBinding& binding) const {
-  Print(out, binding.name());
-  out << ": ";
-  Print(out, binding.type());
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::Return& ret) const {
-  out << "return";
-  if (ret.expression()) {
-    out << " ";
-    Print(out, *ret.expression());
-  }
-}
-
-void SemanticsIR::Print(llvm::raw_ostream& out,
-                        const Semantics::StatementBlock& block) const {
-  out << " { ";
-  for (const auto& statement : block.nodes()) {
-    Print(out, statement);
-    out << "; ";
-  }
-  out << "}";
 }
 
 }  // namespace Carbon
--- toolchain/semantics/semantics_ir_test_helpers.h
+++ toolchain/semantics/semantics_ir_test_helpers.h
@@ -11,43 +11,17 @@
 #include "common/check.h"
 #include "common/ostream.h"
 #include "llvm/ADT/StringExtras.h"
-#include "toolchain/semantics/nodes/declared_name_test_matchers.h"
+#include "toolchain/semantics/nodes/binary_operator_test_matchers.h"
 #include "toolchain/semantics/nodes/function_test_matchers.h"
-#include "toolchain/semantics/nodes/infix_operator_test_matchers.h"
-#include "toolchain/semantics/nodes/literal_test_matchers.h"
-#include "toolchain/semantics/nodes/pattern_binding_test_matchers.h"
+#include "toolchain/semantics/nodes/integer_literal_test_matchers.h"
 #include "toolchain/semantics/nodes/return_test_matchers.h"
+#include "toolchain/semantics/nodes/set_name_test_matchers.h"
 #include "toolchain/semantics/semantics_ir_for_test.h"
 
 namespace Carbon::Testing {
 
-// TODO: Relocate these matchers.
-
-inline auto MappedNode(::testing::Matcher<std::string> key,
-                       ::testing::Matcher<Semantics::Declaration> value)
-    -> ::testing::Matcher<llvm::StringMapEntry<Semantics::Declaration>> {
-  return ::testing::AllOf(
-      ::testing::Property(
-          "key", &llvm::StringMapEntry<Semantics::Declaration>::getKey, key),
-      ::testing::Property(
-          "value", &llvm::StringMapEntry<Semantics::Declaration>::getValue,
-          value));
-}
-
 // Avoids gtest confusion of how to print llvm::None.
 MATCHER(IsNone, "is llvm::None") { return arg == llvm::None; }
-
-inline auto StatementBlock(
-    ::testing::Matcher<llvm::ArrayRef<Semantics::Statement>> nodes_matcher,
-    ::testing::Matcher<llvm::StringMap<Semantics::Statement>>
-        name_lookup_matcher) -> ::testing::Matcher<Semantics::StatementBlock> {
-  return ::testing::AllOf(
-      ::testing::Property("nodes", &Semantics::StatementBlock::nodes,
-                          nodes_matcher),
-      ::testing::Property("name_lookup",
-                          &Semantics::StatementBlock::name_lookup,
-                          name_lookup_matcher));
-}
 
 }  // namespace Carbon::Testing
 
