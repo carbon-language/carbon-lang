52e6d238d509bc05e7835c20e31109ec48572d4e
Patryk Gutowski <37260994+pacion@users.noreply.github.com>
2022-07-24 13:25:26 +0200

    Better readability format

--- installers/local/carbon.cpp
+++ installers/local/carbon.cpp
@@ -1,7 +1,3 @@
-// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
-// Exceptions. See /LICENSE for license information.
-// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 #include "explorer/main.h"
 #include "llvm/ADT/SmallString.h"
 #include "llvm/Support/FileSystem.h"
@@ -12,16 +8,19 @@ namespace path = llvm::sys::path;
 
 auto main(int argc, char** argv) -> int {
   llvm::StringRef bin = path::filename(argv[0]);
+
   if (bin == "carbon-explorer") {
-    static int static_for_main_addr;
+    static int static_for_main_address;
+
     std::string exe = fs::getMainExecutable(
-        argv[0], static_cast<void*>(&static_for_main_addr));
+            argv[0], static_cast<void *>(&static_for_main_address)
+    );
     llvm::StringRef install_path = path::parent_path(exe);
     llvm::SmallString<256> prelude_file(install_path);
     path::append(prelude_file, "data", "prelude.carbon");
+    
     return Carbon::ExplorerMain(prelude_file, argc, argv);
   } else {
-    fprintf(stderr, "Unrecognized Carbon binary requested: %s", argv[0]);
-    return 1;
+    return fprintf(stderr, "Unrecognized Carbon binary requested: %s", argv[0]), 1;
   }
 }
