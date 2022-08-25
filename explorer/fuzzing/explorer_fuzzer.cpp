98d95cd188d1c72ffd5e263a5e0664475a8a22f0
pk19604014 <95385881+pk19604014@users.noreply.github.com>
2022-08-25 12:06:45 -0400

    Cleanup: since explorer_fuzzer is now a standard cc_fuzz_test, and runs on all files in the corpus, there's no need for fuzzer_util_test to do the same manually
    Before:
    ```
    //explorer/fuzzing:fuzzer_util_test                                      PASSED in 18.5s
    ```
    
    After:
    ```
    //explorer/fuzzing:fuzzer_util_test                                      PASSED in 0.3s
    ```

--- explorer/fuzzing/explorer_fuzzer.cpp
+++ explorer/fuzzing/explorer_fuzzer.cpp
@@ -4,8 +4,15 @@
 
 #include <libprotobuf_mutator/src/libfuzzer/libfuzzer_macro.h>
 
+#include "common/error.h"
 #include "explorer/fuzzing/fuzzer_util.h"
+#include "llvm/Support/raw_ostream.h"
 
 DEFINE_TEXT_PROTO_FUZZER(const Carbon::Fuzzing::Carbon& input) {
-  Carbon::ParseAndExecute(input.compilation_unit());
+  const auto result = Carbon::ParseAndExecute(input.compilation_unit());
+  if (result.ok()) {
+    llvm::outs() << "Executed OK: " << *result << "\n";
+  } else {
+    llvm::errs() << "Execution failed: " << result.error() << "\n";
+  }
 }
