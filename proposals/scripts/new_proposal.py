carbon-language/carbon-lang

commit e489b14de0678f4b810a9c5bdcd3c530bd8ed815

Fix `new_proposal.py` to include "Abstract" section (#2139)
"Proposal" was hard-coded as the first section and so it was deleting the "Abstract" section along with the "TODO" section.

proposals/scripts/new_proposal.py
@@ -98,7 +98,7 @@ def _fill_template(template_path: str, title: str, pr_num: int) -> str:
         r"\g<1>%d" % pr_num,
         content,
     )
-    content = re.sub(r"## TODO(?:.|\n)*(## Problem)", r"\1", content)
+    content = re.sub(r"\n## TODO(?:|\n)*?(\n## )", r"\1", content)
     return content
 
 

proposals/scripts/new_proposal_test.py
@@ -50,6 +50,7 @@ class TestNewProposal(unittest.TestCase):
             "pull/123)" in content,
             content,
         )
+        self.assertTrue("\n## Abstract\n\n" in content, content)
 
     def test_run_success(self):
         new_proposal._run(["true"])

commit e941b90ac953df6056fd2ed7b85fe78bc07bc0e1

Correctly remove `## TODO` section from proposals (#2207)
Fix bug introduced in #2139

proposals/scripts/new_proposal.py
@@ -98,7 +98,7 @@ def _fill_template(template_path: str, title: str, pr_num: int) -> str:
         r"\g<1>%d" % pr_num,
         content,
     )
-    content = re.sub(r"\n## TODO(?:|\n)*?(\n## )", r"\1", content)
+    content = re.sub(r"\n## TODO(?:.|\n)*?(\n## )", r"\1", content)
     return content
 
 

proposals/scripts/new_proposal_test.py
@@ -50,7 +50,9 @@ class TestNewProposal(unittest.TestCase):
             "pull/123)" in content,
             content,
         )
-        self.assertTrue("\n## Abstract\n\n" in content, content)
+        self.assertTrue(
+            "<!-- tocstop -->\n\n## Abstract\n\n" in content, content
+        )
 
     def test_run_success(self):
         new_proposal._run(["true"])