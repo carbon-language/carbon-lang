1a41ad86566fb01193241adb87a241a1adcff254
jonmeow <jperkins@google.com>
2022-10-07 23:41:01 +0000

    del

--- toolchain/semantics/node_kind.h
+++ toolchain/semantics/node_kind.h
@@ -13,13 +13,9 @@ namespace Carbon::Semantics {
 
 // Type-safe storage of Node IDs.
 struct NodeId {
-  static auto Invalid() -> NodeId { return NodeId(-1); }
-
   explicit NodeId(int32_t id) : id(id) {}
 
   void Print(llvm::raw_ostream& out) const { out << "%" << id; }
-
-  bool is_invalid() { return id != -1; }
 
   int32_t id;
 };
