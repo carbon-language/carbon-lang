8739d09b9c807cd0fe81bee9747731ae73aaeefa
jonmeow <jperkins@google.com>
2022-10-14 16:29:03 +0000

    update

--- toolchain/semantics/semantics_ir_factory.cpp
+++ toolchain/semantics/semantics_ir_factory.cpp
@@ -49,14 +49,6 @@ struct TraversalStackEntry {
   llvm::Optional<Semantics::NodeId> result_id;
 };
 
-// Provides a standard check that a node has no children.
-static auto RequireNodeEmpty(ParseTree::Node node, ParseNodeKind kind,
-                             int subtree_size) -> void {
-  CARBON_CHECK(subtree_size == 1)
-      << "At index " << node.index() << ", expected " << kind
-      << " would have subtree_size of 1, but was " << subtree_size;
-}
-
 // Converts a TokenKind to a BinaryOperator operator.
 static auto GetBinaryOp(TokenKind kind) -> Semantics::BinaryOperator::Op {
   switch (kind) {
@@ -67,72 +59,51 @@ static auto GetBinaryOp(TokenKind kind) -> Semantics::BinaryOperator::Op {
   }
 }
 
-static auto PopFromStack(const ParseTree& parse_tree,
-                         llvm::SmallVector<TraversalStackEntry>& node_stack,
-                         int& subtree_size) -> void {
-  subtree_size -= parse_tree.node_subtree_size(node_stack.back().parse_node);
-  CARBON_CHECK(subtree_size >= 1);
-  node_stack.pop_back();
-}
-
 void SemanticsIRFactory::Build() {
   llvm::SmallVector<TraversalStackEntry> node_stack;
   auto range = parse_tree().postorder();
   for (auto it = range.begin();; ++it) {
     auto parse_node = *it;
-    int32_t subtree_size = parse_tree().node_subtree_size(parse_node);
     switch (auto parse_kind = parse_tree().node_kind(parse_node)) {
-      case ParseNodeKind::CodeBlock(): {
-        // Just merges children.
-        llvm::SmallVector<Semantics::NodeRef> sem_ir;
-        while (subtree_size > 1) {
-          sem_ir.insert(sem_ir.begin(), node_stack.back().sem_ir.begin(),
-                        node_stack.back().sem_ir.end());
-          PopFromStack(parse_tree(), node_stack, subtree_size);
-        }
-        CARBON_CHECK(subtree_size == 1);
-        node_stack.push_back(
-            TraversalStackEntry(parse_node, std::move(sem_ir)));
-        break;
-      }
       case ParseNodeKind::DeclaredName(): {
-        RequireNodeEmpty(parse_node, parse_kind, subtree_size);
         // DeclaredNames will be acted upon as part of the declaring construct.
         node_stack.push_back(TraversalStackEntry(parse_node));
         break;
       }
-      case ParseNodeKind::FunctionDeclaration(): {
-        // Currently we only have definitions, so this is a CodeBlock.
-        llvm::SmallVector<Semantics::NodeRef> body =
-            std::move(node_stack.back().sem_ir);
-        PopFromStack(parse_tree(), node_stack, subtree_size);
+      case ParseNodeKind::FunctionDefinition(): {
+        // Merges code block children up under the FunctionDefinitionStart.
+        llvm::SmallVector<Semantics::NodeRef> body;
+        while (parse_tree().node_kind(node_stack.back().parse_node) !=
+               ParseNodeKind::FunctionDefinitionStart()) {
+          body.insert(body.begin(), node_stack.back().sem_ir.begin(),
+                      node_stack.back().sem_ir.end());
+          node_stack.pop_back();
+        }
 
-        // Next is the FunctionSignature.
+        // Next is the FunctionDefinitionStart.
         llvm::SmallVector<Semantics::NodeRef> sig =
             std::move(node_stack.back().sem_ir);
-        PopFromStack(parse_tree(), node_stack, subtree_size);
-        CARBON_CHECK(subtree_size == 1);
+        node_stack.pop_back();
 
         // TODO: This replacement is in particular why I want to change
         // the IR setup now, but for now I want to just produce output that
         // satisfies tests without changes.
         auto orig_function = semantics_.nodes_.Get<Semantics::Function>(sig[0]);
         auto orig_set_name = semantics_.nodes_.Get<Semantics::SetName>(sig[1]);
-        llvm::SmallVector<Semantics::NodeRef> sem_ir;
+        llvm::SmallVector<Semantics::NodeRef> function_sem_ir;
         auto function_id = next_id();
-        sem_ir.push_back(semantics_.nodes_.Store(
+        function_sem_ir.push_back(semantics_.nodes_.Store(
             Semantics::Function(orig_function.node(), function_id, body)));
-        sem_ir.push_back(semantics_.nodes_.Store(Semantics::SetName(
+        function_sem_ir.push_back(semantics_.nodes_.Store(Semantics::SetName(
             orig_set_name.node(), orig_set_name.name(), function_id)));
 
         node_stack.push_back(
-            TraversalStackEntry(parse_node, std::move(sem_ir)));
+            TraversalStackEntry(parse_node, std::move(function_sem_ir)));
         break;
       }
-      /*
-      case ParseNodeKind::FunctionSignature(): {
+      case ParseNodeKind::FunctionDefinitionStart(): {
         // TODO: Skip over the parameter list for now.
-        PopFromStack(parse_tree(), node_stack, subtree_size);
+        node_stack.pop_back();
 
         // TODO: At this point, it should be possible to forward-declare the
         // function so that it can be called from its code block. For now, we
@@ -146,16 +117,16 @@ void SemanticsIRFactory::Build() {
         auto name_node = node_stack.back().parse_node;
         sem_ir.push_back(semantics_.nodes_.Store(Semantics::SetName(
             name_node, parse_tree().GetNodeText(name_node), function_id)));
-        PopFromStack(parse_tree(), node_stack, subtree_size);
-        CARBON_CHECK(subtree_size == 1);
+        node_stack.pop_back();
+
+        // Do nothing with the `fn`.
+        node_stack.pop_back();
 
         node_stack.push_back(
             TraversalStackEntry(parse_node, std::move(sem_ir)));
         break;
       }
-      */
       case ParseNodeKind::FileEnd(): {
-        CARBON_CHECK(subtree_size == 1);
         ++it;
         CARBON_CHECK(it == range.end())
             << "FileEnd should always be last, found "
@@ -173,14 +144,12 @@ void SemanticsIRFactory::Build() {
         sem_ir.insert(sem_ir.begin(), node_stack.back().sem_ir.begin(),
                       node_stack.back().sem_ir.end());
         auto rhs_id = *node_stack.back().result_id;
-        PopFromStack(parse_tree(), node_stack, subtree_size);
+        node_stack.pop_back();
 
         sem_ir.insert(sem_ir.begin(), node_stack.back().sem_ir.begin(),
                       node_stack.back().sem_ir.end());
         auto lhs_id = *node_stack.back().result_id;
-        PopFromStack(parse_tree(), node_stack, subtree_size);
-
-        CARBON_CHECK(subtree_size == 1);
+        node_stack.pop_back();
 
         // Figure out the operator for the token.
         auto token = parse_tree().node_token(parse_node);
@@ -195,7 +164,6 @@ void SemanticsIRFactory::Build() {
         break;
       }
       case ParseNodeKind::Literal(): {
-        RequireNodeEmpty(parse_node, parse_kind, subtree_size);
         auto literal_id = next_id();
 
         llvm::SmallVector<Semantics::NodeRef> sem_ir;
@@ -216,9 +184,10 @@ void SemanticsIRFactory::Build() {
       case ParseNodeKind::ReturnStatement(): {
         // TODO: For now just blindly pop what should be StatementEnd.
         node_stack.pop_back();
-        --subtree_size;
 
-        if (subtree_size == 1) {
+        // TODO: Restructure ReturnStatement so that we can do this without
+        // looking at the subtree size.
+        if (parse_tree().node_subtree_size(parse_node) == 2) {
           node_stack.push_back(TraversalStackEntry(
               parse_node, {semantics_.nodes_.Store(
                               Semantics::Return(parse_node, llvm::None))}));
@@ -227,8 +196,7 @@ void SemanticsIRFactory::Build() {
           llvm::SmallVector<Semantics::NodeRef> sem_ir =
               std::move(node_stack.back().sem_ir);
           Semantics::NodeId result_id = *node_stack.back().result_id;
-          PopFromStack(parse_tree(), node_stack, subtree_size);
-          CARBON_CHECK(subtree_size == 1);
+          node_stack.pop_back();
           sem_ir.push_back(semantics_.nodes_.Store(
               Semantics::Return(parse_node, result_id)));
           node_stack.push_back(
@@ -239,18 +207,18 @@ void SemanticsIRFactory::Build() {
       case ParseNodeKind::ParameterList(): {
         // TODO: This should transform into a usable parameter list. For now
         // it's unused and only stored so that node counts match.
-        while (subtree_size > 1) {
-          PopFromStack(parse_tree(), node_stack, subtree_size);
-        }
-        CARBON_CHECK(subtree_size == 1);
+        // TODO: Reorder with ParameterListStart so that we can traverse without
+        // subtree_size.
+        CARBON_CHECK(parse_tree().node_kind(node_stack.back().parse_node) ==
+                     ParseNodeKind::ParameterListEnd());
+        node_stack.pop_back();
         node_stack.push_back(TraversalStackEntry(parse_node));
         break;
       }
-      case ParseNodeKind::CodeBlockStart():
+      case ParseNodeKind::FunctionIntroducer():
       case ParseNodeKind::ParameterListEnd():
       case ParseNodeKind::StatementEnd(): {
-        // The token is ignored, but we track it for consistency.
-        RequireNodeEmpty(parse_node, parse_kind, subtree_size);
+        // The token has no action, but we still track it for the stack.
         node_stack.push_back(TraversalStackEntry(parse_node));
         break;
       }
