14f5562d67e60ca607d60e46f2871b14b61eb5b2
jonmeow <jperkins@google.com>
2022-10-26 18:46:35 +0000

    tmpl

--- toolchain/semantics/semantics_node.cpp
+++ toolchain/semantics/semantics_node.cpp
@@ -9,29 +9,14 @@ namespace Carbon {
 static auto PrintArgs(llvm::raw_ostream& /*out*/,
                       const SemanticsNode::NoArgs /*no_args*/) {}
 
-static auto PrintArgs(llvm::raw_ostream& out, SemanticsNodeId arg0) {
-  out << arg0;
-}
-
-static auto PrintArgs(llvm::raw_ostream& out,
-                      SemanticsIdentifierId identifier) {
-  out << identifier;
-}
-
-static auto PrintArgs(llvm::raw_ostream& out,
-                      SemanticsIntegerLiteralId integer_literal) {
-  out << integer_literal;
-}
-
-static auto PrintArgs(llvm::raw_ostream& out, SemanticsNodeBlockId node_block) {
-  out << node_block;
+template <typename T>
+static auto PrintArgs(llvm::raw_ostream& out, T arg) {
+  out << arg;
 }
 
 template <typename T0, typename T1>
 static auto PrintArgs(llvm::raw_ostream& out, std::pair<T0, T1> args) {
-  PrintArgs(out, args.first);
-  out << ", ";
-  PrintArgs(out, args.second);
+  out << args.first << ", " << args.second;
 }
 
 void SemanticsNode::Print(llvm::raw_ostream& out) const {
