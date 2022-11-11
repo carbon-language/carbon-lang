bf05bde9ea0673c40380da3e612f2a906a2338f1
jonmeow <jperkins@google.com>
2022-11-11 18:13:53 +0000

    x

---
+++ common/vlog.h
@@ -0,0 +1,50 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef CARBON_COMMON_VLOG_H_
+#define CARBON_COMMON_VLOG_H_
+
+#include "common/vlog_internal.h"
+
+namespace Carbon {
+
+// Raw exiting stream. This should be used when building other forms of exiting
+// macros like those below. It evaluates to a temporary `ExitingStream` object
+// that can be manipulated, streamed into, and then will exit the program.
+#define CARBON_RAW_EXITING_STREAM() \
+  Carbon::Internal::ExitingStream::Helper() | Carbon::Internal::ExitingStream()
+
+// Checks the given condition, and if it's false, prints a stack, streams the
+// error message, then exits. This should be used for unexpected errors, such as
+// a bug in the application.
+//
+// For example:
+//   CARBON_CHECK(is_valid) << "Data is not valid!";
+#define CARBON_CHECK(...)                                                   \
+  (__VA_ARGS__) ? (void)0                                                   \
+                : CARBON_RAW_EXITING_STREAM()                               \
+                      << "CHECK failure at " << __FILE__ << ":" << __LINE__ \
+                      << ": " #__VA_ARGS__                                  \
+                      << Carbon::Internal::ExitingStream::AddSeparator()
+
+// DCHECK calls CHECK in debug mode, and does nothing otherwise.
+#ifndef NDEBUG
+#define CARBON_DCHECK(...) CARBON_CHECK(__VA_ARGS__)
+#else
+#define CARBON_DCHECK(...) CARBON_CHECK(true || (__VA_ARGS__))
+#endif
+
+// This is similar to CHECK, but is unconditional. Writing CARBON_FATAL() is
+// clearer than CARBON_CHECK(false) because it avoids confusion about control
+// flow.
+//
+// For example:
+//   CARBON_FATAL() << "Unreachable!";
+#define CARBON_FATAL()        \
+  CARBON_RAW_EXITING_STREAM() \
+      << "FATAL failure at " << __FILE__ << ":" << __LINE__ << ": "
+
+}  // namespace Carbon
+
+#endif  // CARBON_COMMON_VLOG_H_
---
+++ common/vlog_internal.h
@@ -0,0 +1,73 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef CARBON_COMMON_VLOG_INTERNAL_H_
+#define CARBON_COMMON_VLOG_INTERNAL_H_
+
+#include <cstdlib>
+
+#include "llvm/Support/raw_ostream.h"
+
+namespace Carbon::Internal {
+
+// Wraps a stream and exiting for fatal errors. Should only be used by check.h
+// macros.
+class ExitingStream {
+ public:
+  // A tag type that renders as ": " in an ExitingStream, but only if it is
+  // followed by additional output. Otherwise, it renders as "". Primarily used
+  // when building macros around these streams.
+  struct AddSeparator {};
+
+  // Internal type used in macros to dispatch to the `operator|` overload.
+  struct Helper {};
+
+  ExitingStream()
+      // Prefix the buffer with the current bug report message.
+      : buffer_(buffer_str_) {}
+
+  // Never called.
+  [[noreturn]] ~ExitingStream();
+
+  // If the bool cast occurs, it's because the condition is false. This supports
+  // && short-circuiting the creation of ExitingStream.
+  explicit operator bool() const { return true; }
+
+  // Forward output to llvm::errs.
+  template <typename T>
+  auto operator<<(const T& message) -> ExitingStream& {
+    if (separator_) {
+      buffer_ << ": ";
+      separator_ = false;
+    }
+    buffer_ << message;
+    return *this;
+  }
+
+  auto operator<<(AddSeparator /*add_separator*/) -> ExitingStream& {
+    separator_ = true;
+    return *this;
+  }
+
+  // Low-precedence binary operator overload used in check.h macros to flush the
+  // output and exit the program. We do this in a binary operator rather than
+  // the destructor to ensure good debug info and backtraces for errors.
+  [[noreturn]] friend auto operator|(Helper /*helper*/, ExitingStream& stream)
+      -> void {
+    stream.Done();
+  }
+
+ private:
+  [[noreturn]] auto Done() -> void;
+
+  // Whether a separator should be printed if << is used again.
+  bool separator_ = false;
+
+  std::string buffer_str_;
+  llvm::raw_string_ostream buffer_;
+};
+
+}  // namespace Carbon::Internal
+
+#endif  // CARBON_COMMON_VLOG_INTERNAL_H_
