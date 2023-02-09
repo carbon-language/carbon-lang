c69bee0b20313ff0e4510d7405ef337cc512315e
jonmeow <jperkins@google.com>
2023-02-09 19:05:49 +0000

    codegen framework

---
+++ toolchain/codegen/codegen.h
@@ -0,0 +1,14 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef CARBON_TOOLCHAIN_CODEGEN_CODEGEN_H_
+#define CARBON_TOOLCHAIN_CODEGEN_CODEGEN_H_
+
+namespace Carbon {
+
+// TODO
+
+}  // namespace Carbon
+
+#endif  // CARBON_TOOLCHAIN_CODEGEN_CODEGEN_H_
