c573a1dfe1f6696e253979fa4702c58398acbd2f
Jon Ross-Perkins <jperkins@google.com>
2023-03-01 12:25:41 -0800

    Update explorer/fuzzing/ast_to_proto_test.cpp
    Co-authored-by: Richard Smith <richard@metafoo.co.uk>

--- explorer/fuzzing/ast_to_proto_test.cpp
+++ explorer/fuzzing/ast_to_proto_test.cpp
@@ -124,8 +124,7 @@ TEST(AstToProtoTest, SetsAllProtoFields) {
 auto main(int argc, char** argv) -> int {
   ::testing::InitGoogleTest(&argc, argv);
   // gtest should remove flags, leaving just input files.
-  auto carbon_files =
-      std::make_unique<std::vector<llvm::StringRef>>(&argv[1], &argv[argc]);
-  Carbon::Testing::carbon_files = carbon_files.get();
+  std::vector<llvm::StringRef> carbon_files(&argv[1], &argv[argc]);
+  Carbon::Testing::carbon_files = &carbon_files;
   return RUN_ALL_TESTS();
 }
