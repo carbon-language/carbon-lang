dbcba33c0a7e7d18e4753b3f3300d698bc02794a
jonmeow <jperkins@google.com>
2023-04-19 16:51:52 +0000

    x

--- explorer/fuzzing/explorer_fuzzer.cpp
+++ explorer/fuzzing/explorer_fuzzer.cpp
@@ -10,5 +10,5 @@
 
 DEFINE_TEXT_PROTO_FUZZER(const Carbon::Fuzzing::Carbon& input) {
   // Only verifying it doesn't crash.
-  Carbon::ParseAndExecute(input.compilation_unit());
+  (void)Carbon::ParseAndExecute(input.compilation_unit());
 }
