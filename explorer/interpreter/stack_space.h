5d998e4021229e571c8106a2ebe5a2ae26eab36c
jonmeow <jperkins@google.com>
2023-04-26 20:18:09 +0000

    comments

--- explorer/interpreter/stack_space.h
+++ explorer/interpreter/stack_space.h
@@ -11,10 +11,18 @@
 namespace Carbon {
 
 namespace Internal {
+
+// Returns true if a new thread should be started for more stack space.
 auto IsStackSpaceNearlyExhausted() -> bool;
+
+// Starts a thread to run the function.
 auto RunWithStackSpaceHelper(std::function<void()> fn) -> void;
+
 }  // namespace Internal
 
+// Starts a new thread with initialized stack space. This should be called
+// before RunWithStackSpace so that execution is occurring with known stack
+// space.
 template <typename ReturnType>
 auto InitStackSpace(std::function<ReturnType()> fn) -> ReturnType {
   std::optional<ReturnType> result;
@@ -22,6 +30,7 @@ auto InitStackSpace(std::function<ReturnType()> fn) -> ReturnType {
   return std::move(*result);
 }
 
+// Runs a function. May start a thread if more stack space is desirable.
 template <typename ReturnType>
 auto RunWithStackSpace(std::function<ReturnType()> fn) -> ReturnType {
   if (Internal::IsStackSpaceNearlyExhausted()) {
