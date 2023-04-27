c0f8686c54fa61d2be99c690caa40c7cd6aecdec
jonmeow <jperkins@google.com>
2023-04-27 18:01:11 +0000

    Return type

--- explorer/parse_and_execute.cpp
+++ explorer/parse_and_execute.cpp
@@ -17,14 +17,12 @@ namespace Carbon {
 // Returns a scope exit function for printing the timing of a step on scope
 // exit. Note the use prints step timings in reverse order.
 static auto PrintTimingOnExit(TraceStream* trace_stream, const char* label,
-                              std::chrono::steady_clock::time_point* cursor)
-    -> ErrorOr<decltype(llvm::make_scope_exit(std::function<void()>()))> {
+                              std::chrono::steady_clock::time_point* cursor) {
   auto end = std::chrono::steady_clock::now();
   auto duration = end - *cursor;
   *cursor = end;
 
-  return llvm::make_scope_exit(std::function<void()>([trace_stream, label,
-                                                      duration]() {
+  return llvm::make_scope_exit([trace_stream, label, duration]() {
     if (trace_stream->is_enabled()) {
       *trace_stream << "Time elapsed in " << label << ": "
                     << std::chrono::duration_cast<std::chrono::milliseconds>(
@@ -32,7 +30,7 @@ static auto PrintTimingOnExit(TraceStream* trace_stream, const char* label,
                            .count()
                     << "ms\n";
     }
-  }));
+  });
 }
 
 static auto ParseAndExecuteHelper(std::function<ErrorOr<AST>(Arena*)> parse,
