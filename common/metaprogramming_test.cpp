6af5ce93bf0c927a04a23c3c89cc347e08956b26
CanftIn <wwc7033@gmail.com>
2023-06-08 14:46:27 +0800

    Add RequiresPrintMethod test of metaprogramming

--- common/metaprogramming_test.cpp
+++ common/metaprogramming_test.cpp
@@ -8,13 +8,23 @@
 
 #include <string>
 
+#include "llvm/Support/raw_ostream.h"
+
 namespace Carbon::Testing {
 namespace {
 
-TEST(MetaProgrammingTest, RequiresTest) {
-  auto lambda = [](int a, int b) { return a + b; };
+TEST(MetaProgrammingTest, RequiresBasic) {
+  bool result = Requires<int, int>([](int a, int b) { return a + b; });
+  EXPECT_TRUE(result);
+}
 
-  bool result = Requires<int, int>(lambda);
+struct TypeWithPrint {
+  void Print(llvm::raw_ostream& os) const { os << "Test"; }
+};
+
+TEST(MetaProgrammingTest, RequiresPrintMethod) {
+  bool result = Requires<const TypeWithPrint, llvm::raw_ostream>(
+      [](auto&& t, auto&& out) -> decltype(t.Print(out)) {});
   EXPECT_TRUE(result);
 }
 
