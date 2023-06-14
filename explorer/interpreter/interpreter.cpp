ded8d7355892d2254c7535bb095a9b30f39f45b6
prabhatexit0 <prabhatexit0@gmail.com>
2023-06-14 10:57:02 +0530

    added kind - string

--- explorer/interpreter/interpreter.cpp
+++ explorer/interpreter/interpreter.cpp
@@ -579,7 +579,8 @@ auto Interpreter::EvalRecursively(std::unique_ptr<Action> action)
     -> ErrorOr<Nonnull<const Value*>> {
   const auto act_source_loc = action->source_loc();
   if (trace_stream_->is_enabled(act_source_loc)) {
-    *trace_stream_ << "--- recursive eval for `";
+    *trace_stream_ << "--- recursive eval for `" << action->kind_string()
+                   << "` `";
     action->Print(trace_stream_->stream());
     *trace_stream_ << "` --->\n";
     TraceState();
@@ -2479,7 +2480,8 @@ auto Interpreter::RunAllSteps(std::unique_ptr<Action> action)
     -> ErrorOr<Success> {
   auto act_source_loc = action->source_loc();
   if (trace_stream_->is_enabled(act_source_loc)) {
-    *trace_stream_ << "--- running all steps for `";
+    *trace_stream_ << "--- running all steps for `" << action->kind_string()
+                   << "` `";
     action->Print(trace_stream_->stream());
     *trace_stream_ << "` --->\n";
     TraceState();
