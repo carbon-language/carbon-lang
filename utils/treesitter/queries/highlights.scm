b9408c267b52f8364bd3e8d8b1f64ce5731b3f88
maan2003 <manmeetmann2003@gmail.com>
2023-06-16 02:33:33 +0530

    add hightlight queries

---
+++ utils/treesitter/queries/highlights.scm
@@ -0,0 +1,107 @@
+(comment) @comment
+(builtin_type) @type.builtin
+(sized_type_literal) @type.builtin
+(bool_literal) @constant.builtin
+(string_literal) @string
+(integer_literal) @constant.builtin
+(float_literal) @constant.builtin
+
+; function declaration or call expression => function
+(function_declaration (declared_name (ident) @function))
+(call_expression (ident) @function)
+
+; upper case => type
+((ident) @type
+  (#match? @type "^[A-Z]"))
+
+; lower case => variable
+((ident) @variable
+  (#match? @variable "^[a-z_]"))
+
+[
+  "("
+  ")"
+  "{"
+  "}"
+  "["
+  "]"
+] @punctuation.bracket
+
+[
+  "."
+  ";"
+  ","
+  ":"
+  ":!"
+  "=>"
+] @punctuation.delimiter
+
+"->" @punctuation
+
+[
+  "and"
+  "or"
+  "&"
+  "|"
+  "^"
+  "=="
+  "!="
+  "<"
+  "<="
+  ">"
+  ">="
+  ">>"
+  "<<"
+  "+"
+  "-"
+  (binary_star)
+  (prefix_star)
+  (postfix_star)
+  "/"
+  "%"
+  "not"
+  "-"
+  "+"
+  "&"
+  "++"
+  "--"
+] @operator
+
+[
+  "addr"
+  "auto"
+  "template"
+  "var"
+  "if"
+  "then"
+  "else"
+  "impls"
+  "base"
+  "where"
+  "var"
+  "let"
+  "case"
+  "default"
+  "returned"
+  "match"
+  "while"
+  "break"
+  "continue"
+  "for"
+  "in"
+  "return"
+  "abstract"
+  "virtual"
+  "impl"
+  "namespace"
+  "fn"
+  "alias"
+  "interface"
+  "extends"
+  "constraint"
+  "class"
+  "package"
+  "import"
+  "api"
+  "library"
+] @keyword
