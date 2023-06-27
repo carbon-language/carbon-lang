a5dc5b2197dd5cb30603cc3d8707b07bad59cb1a
maan2003 <manmeetmann2003@gmail.com>
2023-06-28 03:08:59 +0530

    update test-toolchain.sh to use testdata symlink

--- utils/treesitter/test-toolchain.sh
+++ utils/treesitter/test-toolchain.sh
@@ -9,8 +9,8 @@ set -euo pipefail
 ROOT="$(git rev-parse --show-toplevel)"
 cd "$ROOT/utils/treesitter"
 
-FILES=$(find "$ROOT/toolchain/parser/testdata" -not -name "fail_*" -name "*.carbon" -type f -print)
-FILES_FAIL=$(find "$ROOT/toolchain/parser/testdata" -name "fail_*.carbon" -type f -print)
+FILES=$(find "$PWD/testdata/" -not -name "fail_*" -name "*.carbon" -type f -print)
+FILES_FAIL=$(find "$PWD/testdata/" -name "fail_*.carbon" -type f -print)
 
 OUTPUT="$(for f in $FILES; do bazel run "//utils/treesitter:treesitter_carbon_tester" "$f" 1>/dev/null 2>&1 || echo "$f" ; done)"
 FAILED=$(echo -n "$OUTPUT" | grep -c '^')
