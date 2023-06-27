0881f04da2f69ce9bcf986ec3eab39839ea8b5e1
maan2003 <manmeetmann2003@gmail.com>
2023-06-28 02:58:22 +0530

    use bazel in test-toolchain.sh

--- utils/treesitter/test-toolchain.sh
+++ utils/treesitter/test-toolchain.sh
@@ -9,12 +9,10 @@ set -euo pipefail
 ROOT="$(git rev-parse --show-toplevel)"
 cd "$ROOT/utils/treesitter"
 
-./build.sh
-
 FILES=$(find "$ROOT/toolchain/parser/testdata" -not -name "fail_*" -name "*.carbon" -type f -print)
 FILES_FAIL=$(find "$ROOT/toolchain/parser/testdata" -name "fail_*.carbon" -type f -print)
 
-OUTPUT="$(for f in $FILES; do tree-sitter parse "$f" 1>/dev/null || echo "$f" ; done)"
+OUTPUT="$(for f in $FILES; do bazel run "//utils/treesitter:treesitter_carbon_tester" "$f" 1>/dev/null 2>&1 || echo "$f" ; done)"
 FAILED=$(echo -n "$OUTPUT" | grep -c '^')
 TOTAL=$(echo -n "$FILES" | grep -c '^')
 
@@ -22,7 +20,7 @@ echo "$FAILED/$TOTAL failed"
 echo "$OUTPUT"
 echo
 
-OUTPUT_FAIL="$(for f in $FILES_FAIL; do tree-sitter parse "$f" 1>/dev/null && echo "$f" || true ; done)"
+OUTPUT_FAIL="$(for f in $FILES_FAIL; do bazel run "//utils/treesitter:treesitter_carbon_tester" "$f" 1>/dev/null 2>&1 && echo "$f" || true ; done)"
 PASSED=$(echo -n "$OUTPUT_FAIL" | grep -c '^')
 TOTAL=$(echo -n "$FILES_FAIL" | grep -c '^')
 echo "$PASSED/$TOTAL false positive"
