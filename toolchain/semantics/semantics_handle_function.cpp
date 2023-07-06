b2b570415f2ed61c8a9c23c8fde75ee543b17def
Richard Smith <richard@metafoo.co.uk>
2023-07-06 15:18:04 -0700

    Basic support for non-defining declarations of functions.
    This doesn't support redeclaration, so this isn't very useful right now.

--- toolchain/semantics/semantics_handle_function.cpp
+++ toolchain/semantics/semantics_handle_function.cpp
@@ -6,9 +6,43 @@
 
 namespace Carbon {
 
+static auto HandleFunctionDeclarationOrDefinition(
+    SemanticsContext& context, ParseTree::Node /*parse_node*/)
+    -> SemanticsNodeId {
+  SemanticsTypeId return_type_id = SemanticsTypeId::Invalid;
+  if (context.parse_tree().node_kind(context.node_stack().PeekParseNode()) ==
+      ParseNodeKind::ReturnType) {
+    return_type_id =
+        context.node_stack().Pop<SemanticsTypeId>(ParseNodeKind::ReturnType);
+  } else {
+    // Canonicalize the empty tuple for the implicit return.
+    context.CanonicalizeType(SemanticsNodeId::BuiltinEmptyTupleType);
+  }
+  auto param_refs_id = context.node_stack().Pop<SemanticsNodeBlockId>(
+      ParseNodeKind::ParameterList);
+  auto name_context = context.PopDeclarationName();
+  auto fn_node = context.node_stack().PopForSoloParseNode(
+      ParseNodeKind::FunctionIntroducer);
+
+  // TODO: Support out-of-line definitions, which will have a resolved
+  // name_context. Right now, those become errors in AddNameToLookup.
+
+  // Add the callable.
+  auto function_id = context.semantics_ir().AddFunction(
+      {.name_id = name_context.unresolved_name_id,
+       .param_refs_id = param_refs_id,
+       .return_type_id = return_type_id,
+       .body_block_ids = {}});
+  auto decl_id = context.AddNode(
+      SemanticsNode::FunctionDeclaration::Make(fn_node, function_id));
+  context.AddNameToLookup(name_context, decl_id);
+  return decl_id;
+}
+
 auto SemanticsHandleFunctionDeclaration(SemanticsContext& context,
                                         ParseTree::Node parse_node) -> bool {
-  return context.TODO(parse_node, "HandleFunctionDeclaration");
+  HandleFunctionDeclarationOrDefinition(context, parse_node);
+  return true;
 }
 
 auto SemanticsHandleFunctionDefinition(SemanticsContext& context,
@@ -31,57 +65,36 @@ auto SemanticsHandleFunctionDefinition(SemanticsContext& context,
     }
   }
 
-  context.return_scope_stack().pop_back();
   context.PopScope();
   context.node_block_stack().Pop();
+  context.return_scope_stack().pop_back();
   return true;
 }
 
 auto SemanticsHandleFunctionDefinitionStart(SemanticsContext& context,
                                             ParseTree::Node parse_node)
     -> bool {
-  SemanticsTypeId return_type_id = SemanticsTypeId::Invalid;
-  if (context.parse_tree().node_kind(context.node_stack().PeekParseNode()) ==
-      ParseNodeKind::ReturnType) {
-    return_type_id =
-        context.node_stack().Pop<SemanticsTypeId>(ParseNodeKind::ReturnType);
-  } else {
-    // Canonicalize the empty tuple for the implicit return.
-    context.CanonicalizeType(SemanticsNodeId::BuiltinEmptyTupleType);
-  }
-  auto param_refs_id = context.node_stack().Pop<SemanticsNodeBlockId>(
-      ParseNodeKind::ParameterList);
-  auto name_context = context.PopDeclarationName();
-  auto fn_node = context.node_stack().PopForSoloParseNode(
-      ParseNodeKind::FunctionIntroducer);
+  // Process the declaration portion of the function.
+  auto decl_id = HandleFunctionDeclarationOrDefinition(context, parse_node);
+  auto function_id =
+      context.semantics_ir().GetNode(decl_id).GetAsFunctionDeclaration();
+  const auto& function = context.semantics_ir().GetFunction(function_id);
 
-  // Create the entry block.
-  auto outer_block = context.node_block_stack().PeekForAdd();
+  // Create the function scope and the entry block.
+  context.return_scope_stack().push_back(decl_id);
   context.node_block_stack().Push();
-
-  // TODO: Support out-of-line definitions, which will have a resolved
-  // name_context. Right now, those become errors in AddNameToLookup.
-
-  // Add the callable.
-  auto function_id = context.semantics_ir().AddFunction(
-      {.name_id = name_context.unresolved_name_id,
-       .param_refs_id = param_refs_id,
-       .return_type_id = return_type_id,
-       .body_block_ids = {context.node_block_stack().PeekForAdd()}});
-  auto decl_id = context.AddNodeToBlock(
-      outer_block,
-      SemanticsNode::FunctionDeclaration::Make(fn_node, function_id));
-  context.AddNameToLookup(name_context, decl_id);
-
   context.PushScope();
-  for (auto ref_id : context.semantics_ir().GetNodeBlock(param_refs_id)) {
+  context.AddCurrentCodeBlockToFunction();
+
+  // Bring the parameters into scope.
+  for (auto ref_id :
+       context.semantics_ir().GetNodeBlock(function.param_refs_id)) {
     auto ref = context.semantics_ir().GetNode(ref_id);
     auto [name_id, target_id] = ref.GetAsBindName();
     context.AddNameToLookup(ref.parse_node(), name_id, target_id);
   }
-  context.return_scope_stack().push_back(decl_id);
-  context.node_stack().Push(parse_node, function_id);
 
+  context.node_stack().Push(parse_node, function_id);
   return true;
 }
 
