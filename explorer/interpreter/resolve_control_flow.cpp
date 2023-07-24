8a5ee6e04a988229ce147a8276d8f2b4f6090cab
prabhatexit0 <prabhatexit0@gmail.com>
2023-07-24 17:22:45 +0530

    add trace for control flow resolution

--- explorer/interpreter/resolve_control_flow.cpp
+++ explorer/interpreter/resolve_control_flow.cpp
@@ -32,10 +32,13 @@ struct FunctionData {
 // belongs to, and that information may be updated by this call. `function`
 // can be nullopt if `statement` does not belong to a function body, for
 // example if it is part of a continuation body instead.
-static auto ResolveControlFlow(Nonnull<Statement*> statement,
+static auto ResolveControlFlow(Nonnull<TraceStream*> trace_stream,
+                               Nonnull<Statement*> statement,
                                std::optional<Nonnull<const Statement*>> loop,
                                std::optional<Nonnull<FunctionData*>> function)
     -> ErrorOr<Success> {
+  SetFileContext set_file_ctx(*trace_stream, statement->source_loc());
+
   switch (statement->kind()) {
     case StatementKind::ReturnVar:
     case StatementKind::ReturnExpression: {
@@ -73,6 +76,12 @@ static auto ResolveControlFlow(Nonnull<Statement*> statement,
         }
       }
 
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved return statement `" << *statement
+                      << "` for `" << PrintAsID(*((*function)->declaration))
+                      << "` (" << statement->source_loc() << ")\n";
+      }
+
       return Success();
     }
     case StatementKind::Break:
@@ -81,6 +90,12 @@ static auto ResolveControlFlow(Nonnull<Statement*> statement,
                << "break is not within a loop body";
       }
       cast<Break>(*statement).set_loop(*loop);
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved break statement `" << *statement
+                      << "` for `" << PrintAsID(**loop) << "`\n";
+      }
+
       return Success();
     case StatementKind::Continue:
       if (!loop.has_value()) {
@@ -88,39 +103,76 @@ static auto ResolveControlFlow(Nonnull<Statement*> statement,
                << "continue is not within a loop body";
       }
       cast<Continue>(*statement).set_loop(*loop);
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved continue statement `" << *statement
+                      << "` for `" << PrintAsID(**loop) << "`\n";
+      }
+
       return Success();
     case StatementKind::If: {
       auto& if_stmt = cast<If>(*statement);
-      CARBON_RETURN_IF_ERROR(
-          ResolveControlFlow(&if_stmt.then_block(), loop, function));
+      CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+          trace_stream, &if_stmt.then_block(), loop, function));
       if (if_stmt.else_block().has_value()) {
-        CARBON_RETURN_IF_ERROR(
-            ResolveControlFlow(*if_stmt.else_block(), loop, function));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+            trace_stream, *if_stmt.else_block(), loop, function));
       }
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved if statement `" << PrintAsID(*statement)
+                      << "` (" << statement->source_loc() << ")\n";
+      }
+
       return Success();
     }
     case StatementKind::Block: {
       auto& block = cast<Block>(*statement);
       for (auto* block_statement : block.statements()) {
         CARBON_RETURN_IF_ERROR(
-            ResolveControlFlow(block_statement, loop, function));
+            ResolveControlFlow(trace_stream, block_statement, loop, function));
       }
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved block statement `"
+                      << PrintAsID(*statement) << "` ("
+                      << statement->source_loc() << ")\n";
+      }
+
       return Success();
     }
     case StatementKind::For: {
-      CARBON_RETURN_IF_ERROR(ResolveControlFlow(&cast<For>(*statement).body(),
-                                                statement, function));
+      CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+          trace_stream, &cast<For>(*statement).body(), statement, function));
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved for statement `" << PrintAsID(*statement)
+                      << "` (" << statement->source_loc() << ")\n";
+      }
+
       return Success();
     }
     case StatementKind::While:
-      CARBON_RETURN_IF_ERROR(ResolveControlFlow(&cast<While>(*statement).body(),
-                                                statement, function));
+      CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+          trace_stream, &cast<While>(*statement).body(), statement, function));
+
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved while statement `"
+                      << PrintAsID(*statement) << "` ("
+                      << statement->source_loc() << ")\n";
+      }
+
       return Success();
     case StatementKind::Match: {
       auto& match = cast<Match>(*statement);
       for (Match::Clause& clause : match.clauses()) {
-        CARBON_RETURN_IF_ERROR(
-            ResolveControlFlow(&clause.statement(), loop, function));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+            trace_stream, &clause.statement(), loop, function));
+      }
+      if (trace_stream->is_enabled()) {
+        *trace_stream << "--- resolved match statement `"
+                      << PrintAsID(*statement) << "` ("
+                      << statement->source_loc() << ")\n";
       }
       return Success();
     }
@@ -132,29 +184,30 @@ static auto ResolveControlFlow(Nonnull<Statement*> statement,
   }
 }
 
-auto ResolveControlFlow(Nonnull<Declaration*> declaration) -> ErrorOr<Success> {
+auto ResolveControlFlow(Nonnull<TraceStream*> trace_stream,
+                        Nonnull<Declaration*> declaration) -> ErrorOr<Success> {
   switch (declaration->kind()) {
     case DeclarationKind::DestructorDeclaration:
     case DeclarationKind::FunctionDeclaration: {
       auto& callable = cast<CallableDeclaration>(*declaration);
       if (callable.body().has_value()) {
         FunctionData data = {.declaration = &callable};
-        CARBON_RETURN_IF_ERROR(
-            ResolveControlFlow(*callable.body(), std::nullopt, &data));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(
+            trace_stream, *callable.body(), std::nullopt, &data));
       }
       break;
     }
     case DeclarationKind::ClassDeclaration: {
       auto& class_decl = cast<ClassDeclaration>(*declaration);
       for (Nonnull<Declaration*> member : class_decl.members()) {
-        CARBON_RETURN_IF_ERROR(ResolveControlFlow(member));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, member));
       }
       break;
     }
     case DeclarationKind::MixinDeclaration: {
       auto& mixin_decl = cast<MixinDeclaration>(*declaration);
       for (Nonnull<Declaration*> member : mixin_decl.members()) {
-        CARBON_RETURN_IF_ERROR(ResolveControlFlow(member));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, member));
       }
       break;
     }
@@ -162,21 +215,21 @@ auto ResolveControlFlow(Nonnull<Declaration*> declaration) -> ErrorOr<Success> {
     case DeclarationKind::ConstraintDeclaration: {
       auto& iface_decl = cast<ConstraintTypeDeclaration>(*declaration);
       for (Nonnull<Declaration*> member : iface_decl.members()) {
-        CARBON_RETURN_IF_ERROR(ResolveControlFlow(member));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, member));
       }
       break;
     }
     case DeclarationKind::ImplDeclaration: {
       auto& impl_decl = cast<ImplDeclaration>(*declaration);
       for (Nonnull<Declaration*> member : impl_decl.members()) {
-        CARBON_RETURN_IF_ERROR(ResolveControlFlow(member));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, member));
       }
       break;
     }
     case DeclarationKind::MatchFirstDeclaration: {
       auto& match_first_decl = cast<MatchFirstDeclaration>(*declaration);
       for (Nonnull<Declaration*> impl : match_first_decl.impl_declarations()) {
-        CARBON_RETURN_IF_ERROR(ResolveControlFlow(impl));
+        CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, impl));
       }
       break;
     }
@@ -196,9 +249,10 @@ auto ResolveControlFlow(Nonnull<Declaration*> declaration) -> ErrorOr<Success> {
   return Success();
 }
 
-auto ResolveControlFlow(AST& ast) -> ErrorOr<Success> {
+auto ResolveControlFlow(Nonnull<TraceStream*> trace_stream, AST& ast)
+    -> ErrorOr<Success> {
   for (auto* declaration : ast.declarations) {
-    CARBON_RETURN_IF_ERROR(ResolveControlFlow(declaration));
+    CARBON_RETURN_IF_ERROR(ResolveControlFlow(trace_stream, declaration));
   }
   return Success();
 }
