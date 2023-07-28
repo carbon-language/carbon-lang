e5fd90bf395c778099bfd535c3f61a0e988be1bb
Farzana Ahmed Siddique <fasiddique@google.com>
2023-07-28 18:07:09 +0000

    Changed diagnostic type

--- toolchain/parser/parser_handle_index_expression.cpp
+++ toolchain/parser/parser_handle_index_expression.cpp
@@ -14,14 +14,7 @@ auto ParserHandleIndexExpression(ParserContext& context) -> void {
   context.AddNode(ParseNodeKind::IndexExpressionStart,
                   context.ConsumeChecked(TokenKind::OpenSquareBracket),
                   state.subtree_start, state.has_error);
-  if (context.PositionIs(TokenKind::CloseSquareBracket)) {
-    CARBON_DIAGNOSTIC(ExpectedExpressionInside, Error,
-                      "Expected expression inside the square bracket.", );
-    context.emitter().Emit(*context.position(), ExpectedExpressionInside);
-    context.ReturnErrorOnState();
-  } else {
-    context.PushState(ParserState::Expression);
-  }
+  context.PushState(ParserState::Expression);
 }
 
 auto ParserHandleIndexExpressionFinish(ParserContext& context) -> void {
