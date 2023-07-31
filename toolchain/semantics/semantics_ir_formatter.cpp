af8035db82430c7b06254e1e70a0684ffe889a84
Richard Smith <richard@metafoo.co.uk>
2023-07-31 15:39:59 -0700

    Node and label naming.

--- toolchain/semantics/semantics_ir_formatter.cpp
+++ toolchain/semantics/semantics_ir_formatter.cpp
@@ -3,11 +3,96 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
 #include "toolchain/semantics/semantics_ir_formatter.h"
+#include "llvm/ADT/StringExtras.h"
 
 namespace Carbon {
 
+namespace {
+class NodeNamer {
+ public:
+  auto GetNameFor(SemanticsNodeId node_id) -> std::string {
+    auto it = names.find(node_id);
+    if (it == names.end()) {
+      // This should not happen in valid IR.
+      return "<noderef " + llvm::itostr(node_id.index) + ">";
+    }
+    return it->second;
+  }
+
+  auto GetLabelFor(SemanticsNodeBlockId block_id) -> std::string {
+    auto it = labels.find(block_id);
+    if (it == labels.end()) {
+      // This should not happen in valid IR.
+      return "<nodeblockref " + llvm::itostr(block_id.index) + ">";
+    }
+    return it->second;
+  }
+
+  auto AddBlockLabel(SemanticsNodeBlockId block_id) -> void {
+    if (label_count == 0) {
+      labels[block_id] = "!entry";
+    } else {
+      labels[block_id] = "!" + llvm::itostr(label_count - 1);
+    }
+    ++label_count;
+  }
+
+  auto CollectNamesInBlock(const SemanticsIR& semantics_ir,
+                           SemanticsNodeBlockId block_id) -> void {
+    // Use bound names where available.
+    for (auto node_id : semantics_ir.GetNodeBlock(block_id)) {
+      auto node = semantics_ir.GetNode(node_id);
+      if (node.kind() == SemanticsNodeKind::BindName) {
+        auto [name_id, named_node_id] = node.GetAsBindName();
+        names[named_node_id] = ("%" + semantics_ir.GetString(name_id)).str();
+      }
+    }
+
+    // Sequentially number all remaining typed values.
+    for (auto node_id : semantics_ir.GetNodeBlock(block_id)) {
+      auto node = semantics_ir.GetNode(node_id);
+      if (node.kind().type_field_kind() == SemanticsTypeFieldKind::Type) {
+        auto& name = names[node_id];
+        if (name.empty()) {
+          names[node_id] = "%" + llvm::itostr(unnamed_count++);
+        }
+      }
+    }
+  }
+
+ private:
+  llvm::DenseMap<SemanticsNodeId, std::string> names;
+  llvm::DenseMap<SemanticsNodeBlockId, std::string> labels;
+  int unnamed_count = 0;
+  int label_count = 0;
+};
+}
+
 class SemanticsIRFormatter {
  public:
+  class NodeNameScope {
+   public:
+    NodeNameScope(SemanticsIRFormatter& parent) : parent_(parent) {
+      CARBON_CHECK(!parent_.node_name_scope) << "multiple name scopes at once";
+      parent_.node_name_scope = &namer_;
+    }
+    ~NodeNameScope() {
+      parent_.node_name_scope = nullptr;
+    }
+
+    auto AddBlockLabel(SemanticsNodeBlockId block_id) -> void {
+      namer_.AddBlockLabel(block_id);
+    }
+
+    auto CollectNamesInBlock(SemanticsNodeBlockId block_id) -> void {
+      namer_.CollectNamesInBlock(parent_.semantics_ir_, block_id);
+    }
+
+   private:
+    SemanticsIRFormatter& parent_;
+    NodeNamer namer_;
+  };
+
   explicit SemanticsIRFormatter(const SemanticsIR& semantics_ir,
                                 llvm::raw_ostream& out)
       : semantics_ir_(semantics_ir), out_(out) {}
@@ -18,7 +103,11 @@ class SemanticsIRFormatter {
     // TODO: Handle the case where there are multiple top-level node blocks.
     // For example, there may be branching in the initializer of a global or a
     // type expression.
-    FormatCodeBlock(semantics_ir_.top_node_block_id());
+    {
+      NodeNameScope package_scope(*this);
+      package_scope.CollectNamesInBlock(semantics_ir_.top_node_block_id());
+      FormatCodeBlock(semantics_ir_.top_node_block_id());
+    }
     out_ << "}\n";
 
     for (int i = 0; i != semantics_ir_.functions_size(); ++i) {
@@ -35,13 +124,21 @@ class SemanticsIRFormatter {
     FormatGlobalName(fn.name_id);
     out_ << "(";
 
+    // Assign names to values and blocks in this function.
+    NodeNameScope function_scope(*this);
+    function_scope.CollectNamesInBlock(fn.param_refs_id);
+    for (auto block_id : fn.body_block_ids) {
+      function_scope.AddBlockLabel(block_id);
+      function_scope.CollectNamesInBlock(block_id);
+    }
+
     llvm::ListSeparator sep;
     for (const SemanticsNodeId param_id :
          semantics_ir_.GetNodeBlock(fn.param_refs_id)) {
       out_ << sep;
       auto param = semantics_ir_.GetNode(param_id);
       auto [name_id, node_id] = param.GetAsBindName();
-      FormatBindingName(name_id);
+      FormatNodeName(node_id);
       out_ << ": ";
       FormatType(param.type_id());
     }
@@ -52,10 +149,17 @@ class SemanticsIRFormatter {
     }
 
     if (!fn.body_block_ids.empty()) {
-      out_ << " {\n";
+      out_ << " {";
+
       for (auto block_id : fn.body_block_ids) {
+        out_ << "\n";
+
+        FormatLabel(block_id);
+        out_ << ":\n";
+
         FormatCodeBlock(block_id);
       }
+
       out_ << "}\n";
     } else {
       out_ << ";";
@@ -65,41 +169,114 @@ class SemanticsIRFormatter {
   auto FormatCodeBlock(SemanticsNodeBlockId block_id) -> void {
     for (const SemanticsNodeId node_id : semantics_ir_.GetNodeBlock(block_id)) {
       FormatInstruction(node_id);
-      out_ << "\n";
     }
   }
 
   auto FormatInstruction(SemanticsNodeId node_id) -> void {
-    out_ << "  ";
     if (!node_id.is_valid()) {
-      out_ << "InvalidInstruction";
+      out_ << "  InvalidInstruction\n";
       return;
     }
 
-    auto node = semantics_ir_.GetNode(node_id);
+    FormatInstruction(node_id, semantics_ir_.GetNode(node_id));
+  }
 
+  auto FormatInstruction(SemanticsNodeId node_id, SemanticsNode node) -> void {
+    switch (node.kind()) {
+#define CARBON_SEMANTICS_NODE_KIND(Name)                         \
+  case SemanticsNodeKind::Name:                                  \
+    FormatInstruction<SemanticsNode::Name>(node_id, node, #Name, \
+                                           node.GetAs##Name());  \
+    break;
+#include "toolchain/semantics/semantics_node_kind.def"
+    }
+  }
+
+  template <typename Kind>
+  auto FormatInstruction(SemanticsNodeId node_id, SemanticsNode node,
+                         llvm::StringRef name, decltype(Kind::Get(node)) args)
+      -> void {
+    out_ << "  ";
+    FormatInstructionLHS(node_id, node);
+    out_ << name;
+    if (node.kind().type_field_kind() == SemanticsTypeFieldKind::Argument) {
+      FormatArgs(std::pair(node.type_id(), args));
+    } else {
+      FormatArgs(args);
+    }
+    out_ << "\n";
+  }
+
+  auto FormatInstructionLHS(SemanticsNodeId node_id, SemanticsNode node)
+      -> void {
     if (node.kind().type_field_kind() == SemanticsTypeFieldKind::Type) {
-      out_ << "%" << node_id.index;
+      FormatNodeName(node_id);
       out_ << ": ";
       FormatType(node.type_id());
       out_ << " = ";
     }
-    FormatInstruction(node);
   }
 
-  auto FormatInstruction(SemanticsNode insn) -> void {
-    switch (insn.kind()) {
-#define CARBON_SEMANTICS_NODE_KIND(Name)                                     \
-  case SemanticsNodeKind::Name:                                              \
-    out_ << #Name;                                                           \
-    if (insn.kind().type_field_kind() == SemanticsTypeFieldKind::Argument) { \
-      FormatArgs(std::pair(insn.type_id(), insn.GetAs##Name()));             \
-    } else {                                                                 \
-      FormatArgs(insn.GetAs##Name());                                        \
-    }                                                                        \
-    break;
-#include "toolchain/semantics/semantics_node_kind.def"
+  // BindName is handled by the NodeNamer and doesn't appear in the output.
+  template <>
+  auto FormatInstruction<SemanticsNode::BindName>(
+      SemanticsNodeId, SemanticsNode, llvm::StringRef,
+      std::pair<SemanticsStringId, SemanticsNodeId>) -> void {}
+
+  template <>
+  auto FormatInstruction<SemanticsNode::BlockArg>(SemanticsNodeId node_id,
+                                                      SemanticsNode node,
+                                                      llvm::StringRef,
+                                                      SemanticsNodeBlockId self)
+      -> void {
+    out_ << "  ";
+    FormatInstructionLHS(node_id, node);
+    out_ << "block_arg ";
+    FormatLabel(self);
+    out_ << "\n";
+  }
+
+  template <>
+  auto FormatInstruction<SemanticsNode::BranchIf>(
+      SemanticsNodeId, SemanticsNode, llvm::StringRef,
+      std::pair<SemanticsNodeBlockId, SemanticsNodeId> args) -> void {
+    if (!in_terminator_sequence) {
+      out_ << "  ";
     }
+    out_ << "if ";
+    FormatNodeName(args.second);
+    out_ << " br ";
+    FormatLabel(args.first);
+    out_ << " else ";
+    in_terminator_sequence = true;
+  }
+
+  template <>
+  auto FormatInstruction<SemanticsNode::BranchWithArg>(
+      SemanticsNodeId, SemanticsNode, llvm::StringRef,
+      std::pair<SemanticsNodeBlockId, SemanticsNodeId> args) -> void {
+    if (!in_terminator_sequence) {
+      out_ << "  ";
+    }
+    out_ << "br ";
+    FormatLabel(args.first);
+    out_ << "(";
+    FormatNodeName(args.second);
+    out_ << ")\n";
+    in_terminator_sequence = false;
+  }
+
+  template <>
+  auto FormatInstruction<SemanticsNode::Branch>(
+      SemanticsNodeId, SemanticsNode, llvm::StringRef,
+      SemanticsNodeBlockId target) -> void {
+    if (!in_terminator_sequence) {
+      out_ << "  ";
+    }
+    out_ << "br ";
+    FormatLabel(target);
+    out_ << "\n";
+    in_terminator_sequence = false;
   }
 
   auto FormatArgs(SemanticsNode::NoArgs) -> void {}
@@ -133,7 +310,7 @@ class SemanticsIRFormatter {
   }
 
   auto FormatArg(SemanticsNodeId id) -> void {
-    out_ << '%' << id.index;
+    out_ << node_name_scope->GetNameFor(id);
   }
 
   auto FormatArg(SemanticsNodeBlockId id) -> void {
@@ -172,6 +349,14 @@ class SemanticsIRFormatter {
     out_ << '}';
   }
 
+  auto FormatNodeName(SemanticsNodeId id) -> void {
+    out_ << node_name_scope->GetNameFor(id);
+  }
+
+  auto FormatLabel(SemanticsNodeBlockId id) -> void {
+    out_ << node_name_scope->GetLabelFor(id);
+  }
+
   auto FormatString(SemanticsStringId id) -> void {
     out_ << semantics_ir_.GetString(id);
   }
@@ -185,11 +370,6 @@ class SemanticsIRFormatter {
     }
   }
 
-  auto FormatBindingName(SemanticsStringId id) -> void {
-    out_ << '%';
-    FormatString(id);
-  }
-
   auto FormatType(SemanticsTypeId id) -> void {
     if (!id.is_valid()) {
       out_ << "invalid";
@@ -199,8 +379,10 @@ class SemanticsIRFormatter {
   }
 
  private:
-  const SemanticsIR &semantics_ir_;
-  llvm::raw_ostream &out_;
+  const SemanticsIR& semantics_ir_;
+  llvm::raw_ostream& out_;
+  NodeNamer* node_name_scope = nullptr;
+  bool in_terminator_sequence = false;
 };
 
 auto FormatSemanticsIR(const SemanticsIR& ir, llvm::raw_ostream& out) -> void {
