c99bec7b49f5447b93c051ccb4230af2d009f456
Richard Smith <richard@metafoo.co.uk>
2023-08-10 17:55:04 -0700

    Add forgotten file.

---
+++ toolchain/lowering/lowering_handle_expression_category.cpp
@@ -0,0 +1,27 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "toolchain/lowering/lowering_function_context.h"
+
+namespace Carbon {
+
+auto LoweringHandleInitializeFrom(LoweringFunctionContext& context,
+                                  SemanticsNodeId node_id, SemanticsNode node)
+    -> void {
+  context.SetLocal(node_id, context.GetLocal(node.GetAsInitializeFrom()));
+}
+
+auto LoweringHandleMaterializeTemporary(LoweringFunctionContext& context,
+                                        SemanticsNodeId node_id,
+                                        SemanticsNode node) -> void {
+  context.SetLocal(node_id, context.GetLocal(node.GetAsMaterializeTemporary()));
+}
+
+auto LoweringHandleValueBinding(LoweringFunctionContext& context,
+                                SemanticsNodeId node_id, SemanticsNode node)
+    -> void {
+  context.SetLocal(node_id, context.GetLocalLoaded(node.GetAsValueBinding()));
+}
+
+}  // namespace Carbon
