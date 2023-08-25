bd94535ba3ed002a1baa85918d2bd2c3016f9a75
Richard Smith <richard@metafoo.co.uk>
2023-08-25 14:16:04 -0700

    Take the value representation into account in BindValue lowering. (#3148)
    For types using a pointer value representation, emit the address of the
    source rather than loading the value.

--- toolchain/lowering/lowering_handle_expression_category.cpp
+++ toolchain/lowering/lowering_handle_expression_category.cpp
@@ -8,7 +8,26 @@ namespace Carbon {
 
 auto LoweringHandleBindValue(LoweringFunctionContext& context,
                              SemIR::NodeId node_id, SemIR::Node node) -> void {
-  context.SetLocal(node_id, context.GetLocalLoaded(node.GetAsBindValue()));
+  switch (auto rep = SemIR::GetValueRepresentation(context.semantics_ir(),
+                                                   node.type_id());
+          rep.kind) {
+    case SemIR::ValueRepresentation::None:
+      // Nothing should use this value, but StubReference needs a value to
+      // propagate.
+      context.SetLocal(node_id,
+                       llvm::PoisonValue::get(context.GetType(node.type_id())));
+      break;
+    case SemIR::ValueRepresentation::Copy:
+      context.SetLocal(node_id, context.builder().CreateLoad(
+                                    context.GetType(node.type_id()),
+                                    context.GetLocal(node.GetAsBindValue())));
+      break;
+    case SemIR::ValueRepresentation::Pointer:
+      context.SetLocal(node_id, context.GetLocal(node.GetAsBindValue()));
+      break;
+    case SemIR::ValueRepresentation::Custom:
+      CARBON_FATAL() << "TODO: Add support for BindValue with custom value rep";
+  }
 }
 
 auto LoweringHandleMaterializeTemporary(LoweringFunctionContext& context,
