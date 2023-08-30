742b13bc52b8e415ae646c1605910e6a79b624d0
Richard Smith <richard@metafoo.co.uk>
2023-08-30 14:50:36 -0700

    Lowering support for prior change.

--- toolchain/lowering/lowering_handle_expression_category.cpp
+++ toolchain/lowering/lowering_handle_expression_category.cpp
@@ -40,6 +40,7 @@ auto HandleMaterializeTemporary(FunctionContext& context, SemIR::NodeId node_id,
 auto HandleTemporary(FunctionContext& context, SemIR::NodeId node_id,
                      SemIR::Node node) -> void {
   auto [temporary_id, init_id] = node.GetAsTemporary();
+  context.FinishInitialization(node.type_id(), temporary_id, init_id);
   context.SetLocal(node_id, context.GetLocal(temporary_id));
 }
 
