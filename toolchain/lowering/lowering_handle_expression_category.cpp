19f7e1926384c9a1ad88e297eac7e52456170084
Richard Smith <richard@metafoo.co.uk>
2023-08-30 16:07:38 -0700

    Alphabetically sort temporary handle functions.
    Co-authored-by: Jon Ross-Perkins <jperkins@google.com>

--- toolchain/lowering/lowering_handle_expression_category.cpp
+++ toolchain/lowering/lowering_handle_expression_category.cpp
@@ -30,18 +30,18 @@ auto HandleBindValue(FunctionContext& context, SemIR::NodeId node_id,
   }
 }
 
-auto HandleTemporaryStorage(FunctionContext& context, SemIR::NodeId node_id,
-                            SemIR::Node node) -> void {
-  context.SetLocal(
-      node_id, context.builder().CreateAlloca(context.GetType(node.type_id()),
-                                              nullptr, "temp"));
-}
-
 auto HandleTemporary(FunctionContext& context, SemIR::NodeId node_id,
                      SemIR::Node node) -> void {
   auto [temporary_id, init_id] = node.GetAsTemporary();
   context.FinishInitialization(node.type_id(), temporary_id, init_id);
   context.SetLocal(node_id, context.GetLocal(temporary_id));
+}
+
+auto HandleTemporaryStorage(FunctionContext& context, SemIR::NodeId node_id,
+                            SemIR::Node node) -> void {
+  context.SetLocal(
+      node_id, context.builder().CreateAlloca(context.GetType(node.type_id()),
+                                              nullptr, "temp"));
 }
 
 }  // namespace Carbon::Lower
