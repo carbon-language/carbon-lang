d4ff32ad9ba647700234b5d15613c149ba5e3dbb
Chandler Carruth <chandlerc@gmail.com>
2023-09-03 10:37:22 +0000

    refactor running

--- scripts/bazel_diff.py
+++ scripts/bazel_diff.py
@@ -18,6 +18,7 @@ import tempfile
 import os
 import sys
 from pathlib import Path
+from typing import List
 
 import scripts_utils
 
@@ -26,20 +27,45 @@ def log(s: str) -> None:
     print(s, file=sys.stderr)
 
 
+def quiet_run(args: List[str]) -> None:
+    try:
+        subprocess.run(
+            args,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.PIPE,
+            check=True,
+            encoding="utf-8",
+        )
+    except subprocess.CalledProcessError as err:
+        log(err.stderr)
+        raise
+
+
+def quiet_run_output(args: List[str]) -> str:
+    try:
+        p = subprocess.run(
+            args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            check=True,
+            encoding="utf-8",
+        )
+        return p.stdout
+    except subprocess.CalledProcessError as err:
+        log(err.stderr)
+        raise
+
+
 def make_bazel_diff_script(bazel: Path, tmpdir: Path) -> Path:
     bazel_diff_path = tmpdir / "bazel_diff"
-    args = [
-        str(bazel),
-        "run",
-        f"--script_path={bazel_diff_path}",
-        "//bazel/diff:bazel-diff",
-    ]
-    p = subprocess.run(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8"
+    quiet_run(
+        [
+            str(bazel),
+            "run",
+            f"--script_path={bazel_diff_path}",
+            "//bazel/diff:bazel-diff",
+        ]
     )
-    if p.returncode != 0:
-        log(p.stderr)
-        exit(f"Bazel run returned {p.returncode}")
     return bazel_diff_path
 
 
@@ -47,38 +73,29 @@ def compute_hashes(
     bazel: Path, bazel_diff: Path, tmpdir: Path, prefix: str
 ) -> Path:
     hashes_path = tmpdir / f"{prefix}_hashes"
-    args = [
-        str(bazel_diff),
-        "generate-hashes",
-        f"-b={bazel}",
-        f"-w={os.getcwd()}",
-        str(hashes_path),
-    ]
-    p = subprocess.run(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8"
+    quiet_run(
+        [
+            str(bazel_diff),
+            "generate-hashes",
+            f"-b={bazel}",
+            f"-w={os.getcwd()}",
+            str(hashes_path),
+        ]
     )
-    if p.returncode != 0:
-        log(p.stderr)
-        exit(f"Bazel diff returned {p.returncode}")
     return hashes_path
 
 
 def impacted_targets(
     bazel_diff: Path, baseline_hashes: Path, current_hashes: Path
 ) -> str:
-    args = [
-        str(bazel_diff),
-        "get-impacted-targets",
-        f"-sh={baseline_hashes}",
-        f"-fh={current_hashes}",
-    ]
-    p = subprocess.run(
-        args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, encoding="utf-8"
+    return quiet_run_output(
+        [
+            str(bazel_diff),
+            "get-impacted-targets",
+            f"-sh={baseline_hashes}",
+            f"-fh={current_hashes}",
+        ]
     )
-    if p.returncode != 0:
-        log(p.stderr)
-        exit(f"Bazel diff returned {p.returncode}")
-    return p.stdout
 
 
 def filter_targets(bazel: Path, targets: str) -> str:
@@ -87,21 +104,13 @@ def filter_targets(bazel: Path, targets: str) -> str:
             f"let t = set({targets}) in "
             "kind(rule, $t) except attr(tags, manual, $t)\n"
         )
-        args = [
-            str(bazel),
-            "query",
-            f"--query_file={tmp.name}",
-        ]
-        p = subprocess.run(
-            args,
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-            encoding="utf-8",
+        return quiet_run_output(
+            [
+                str(bazel),
+                "query",
+                f"--query_file={tmp.name}",
+            ]
         )
-        if p.returncode != 0:
-            log(p.stderr)
-            exit(f"Bazel run returned {p.returncode}")
-        return p.stdout
 
 
 def git_is_dirty() -> bool:
@@ -161,14 +170,19 @@ def main() -> None:
         bazel_diff = make_bazel_diff_script(bazel, tmpdir)
         log(f"Wrote bazel-diff script: {bazel_diff}")
 
-        git_checkout(baseline)
-        log(f"Checked out commit: {baseline}")
+        # Checkout the baseline revision to compute those hashes. But if we hit
+        # any errors, make sure to re-checkout the current state.
+        try:
+            log(f"Checking out commit: {baseline}")
+            git_checkout(baseline)
 
-        baseline_hashes = compute_hashes(bazel, bazel_diff, tmpdir, "baseline")
-        log(f"Baseline hashes: {baseline_hashes}")
-
-        git_checkout(current)
-        log(f"Checking out original commit: {current}")
+            baseline_hashes = compute_hashes(
+                bazel, bazel_diff, tmpdir, "baseline"
+            )
+            log(f"Baseline hashes: {baseline_hashes}")
+        finally:
+            log(f"Checking out original commit: {current}")
+            git_checkout(current)
 
         current_hashes = compute_hashes(bazel, bazel_diff, tmpdir, "current")
         log(f"Current hashes: {current_hashes}")
