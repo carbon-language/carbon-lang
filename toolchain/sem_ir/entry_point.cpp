d2379ab17219d09a45f32cab09f64636391b9ed9
Richard Smith <richard@metafoo.co.uk>
2023-10-05 11:05:17 -0700

    Autoupdate and fix test failure.

--- toolchain/sem_ir/entry_point.cpp
+++ toolchain/sem_ir/entry_point.cpp
@@ -15,7 +15,8 @@ auto IsEntryPoint(const SemIR::File& file, SemIR::FunctionId function_id)
   // TODO: Check if `file` is in the `Main` package.
   auto& function = file.GetFunction(function_id);
   // TODO: Check if `function` is in a namespace.
-  return file.GetString(function.name_id) == EntryPointFunction;
+  return function.name_id.is_valid() &&
+         file.GetString(function.name_id) == EntryPointFunction;
 }
 
 }  // namespace Carbon::SemIR
