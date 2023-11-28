bc7c4670f5fcf9fc2d3e536c29b21e80353ec746
Jacob <jacobschneiderjgs@gmail.com>
2023-11-27 18:43:05 -0800

    fully underline parse nodes in diagnostics

--- toolchain/parse/tree_node_location_translator.h
+++ toolchain/parse/tree_node_location_translator.h
@@ -25,7 +25,40 @@ class NodeLocationTranslator : public DiagnosticLocationTranslator<Node> {
     if (!node.is_valid()) {
       return {.file_name = filename_};
     }
-    return token_translator_.GetLocation(parse_tree_->node_token(node));
+
+    // Retrieve all tokens that descend from this node (including the root).
+    // This will always represent a contiguous chunk of source b/c XXXX. Then
+    // construct a location that represents this range.
+    DiagnosticLocation start_loc =
+        token_translator_.GetLocation(parse_tree_->node_token(node));
+    DiagnosticLocation end_loc = start_loc;
+    for (Node descendant : parse_tree_->postorder(node)) {
+      Lex::Token token = parse_tree_->node_token(descendant);
+      if (!token.is_valid()) {
+        continue;
+      }
+      DiagnosticLocation loc = token_translator_.GetLocation(token);
+      if (loc.line_number < start_loc.line_number ||
+          (loc.line_number == start_loc.line_number &&
+           loc.column_number < start_loc.column_number)) {
+        start_loc = loc;
+      } else if (loc.line_number > end_loc.line_number ||
+                 (loc.line_number == end_loc.line_number &&
+                  loc.column_number > end_loc.column_number)) {
+        end_loc = loc;
+      }
+    }
+
+    // For multiline locations we simply return the rest of the line for now
+    // since true multiline locations are not yet supported.
+    if (start_loc.line_number != end_loc.line_number) {
+      start_loc.length = start_loc.line.size() - start_loc.column_number + 1;
+    } else {
+      if (start_loc.column_number != end_loc.column_number) {
+        start_loc.length = end_loc.column_number - start_loc.column_number + 1;
+      }
+    }
+    return start_loc;
   }
 
  private:
