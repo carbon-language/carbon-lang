82ce532dcfe35c0ba86b3ab08ef641fb507c85a0
Richard Smith <richard@metafoo.co.uk>
2023-12-01 18:36:34 -0800

    Another binary that needs to properly initialize LLVM

--- testing/base/gtest_main.cpp
+++ testing/base/gtest_main.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 
 #include "llvm/Support/InitLLVM.h"
+#include "llvm/Support/TargetSelect.h"
 
 auto main(int argc, char** argv) -> int {
   testing::InitGoogleTest(&argc, argv);
@@ -13,5 +14,10 @@ auto main(int argc, char** argv) -> int {
       "https://github.com/carbon-language/carbon-lang/issues and include the "
       "crash backtrace.\n");
   llvm::InitLLVM init_llvm(argc, argv);
+  llvm::InitializeAllTargetInfos();
+  llvm::InitializeAllTargets();
+  llvm::InitializeAllTargetMCs();
+  llvm::InitializeAllAsmParsers();
+  llvm::InitializeAllAsmPrinters();
   return RUN_ALL_TESTS();
 }
