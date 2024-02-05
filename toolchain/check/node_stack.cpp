eaeae6359e1a778bcc1f3de77e80ac25a979168b
Richard Smith <richard@metafoo.co.uk>
2024-02-05 21:23:35 +0000

    Switch to X macro for ID printing.

--- toolchain/check/node_stack.cpp
+++ toolchain/check/node_stack.cpp
@@ -11,32 +11,28 @@
 namespace Carbon::Check {
 
 auto NodeStack::PrintForStackDump(llvm::raw_ostream& output) const -> void {
-  static_assert(static_cast<std::size_t>(Id::Kind::Invalid) ==
-                Id::NumValidKinds);
-  static_assert(static_cast<std::size_t>(Id::Kind::None) ==
-                Id::NumValidKinds + 1);
-  using PrintIdFn = auto(llvm::raw_ostream & output, Id id)->void;
-  using PrintIdFnsType = std::array<PrintIdFn*, Id::NumValidKinds + 2>;
-  static constexpr PrintIdFnsType PrintIdFns =
-      []<std::size_t... Is>(std::index_sequence<Is...>) -> PrintIdFnsType {
-    return {[](llvm::raw_ostream& output, Id id) {
-      constexpr auto IdKind = static_cast<Id::Kind>(Is);
-      if constexpr (IdKind == Id::Kind::None) {
-        output << " -> no value";
-      } else if constexpr (IdKind == Id::Kind::Invalid) {
-        CARBON_FATAL() << "Should not be in node stack";
-      } else {
-        output << id.As<IdKind>();
-      }
-    }...};
-  }(std::make_index_sequence<Id::NumValidKinds + 2>());
+  auto print_id = [&]<Id::Kind Kind>(Id id) {
+    if constexpr (Kind == Id::Kind::None) {
+      output << " -> no value";
+    } else if constexpr (Kind == Id::Kind::Invalid) {
+      CARBON_FATAL() << "Should not be in node stack";
+    } else {
+      output << id.As<Kind>();
+    }
+  };
 
   output << "NodeStack:\n";
   for (auto [i, entry] : llvm::enumerate(stack_)) {
     auto parse_node_kind = parse_tree_->node_kind(entry.parse_node);
     output << "\t" << i << ".\t" << parse_node_kind;
-    PrintIdFns[static_cast<std::size_t>(
-        ParseNodeKindToIdKind(parse_node_kind))](output, entry.id);
+    switch (parse_node_kind) {
+#define CARBON_PARSE_NODE_KIND(Kind)                                   \
+  case Parse::NodeKind::Kind:                                          \
+    print_id.operator()<ParseNodeKindToIdKind(Parse::NodeKind::Kind)>( \
+        entry.id);                                                     \
+    break;
+#include "toolchain/parse/node_kind.def"
+    }
     output << "\n";
   }
 }
