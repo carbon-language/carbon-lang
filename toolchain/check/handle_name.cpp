8ad0c70f9f97ba36caa2d257ce78ce91f26325c8
Jon Ross-Perkins <jperkins@google.com>
2024-02-22 14:29:06 -0800

    Add ClassDecl/Type import functionality. (#3709)
    I believe this PR is sufficient to pull in all current class features,
    including the current bits of inheritance which have been implemented.
    Because a class declaration can reference its own type, this creates an
    incomplete type prior to constant loading.
    
    Right now, the object representation is imported proactively, but
    individual fields are left as ImportRefUnused. This means that member
    functions and similar will only be imported if called.
    
    This also adjusts how function parameters are being handled, to match
    the expectations of Self param structure.
    
    When formatting, I'm starting to look into constants. Otherwise we get
    "unexpected instref".
    
    Overall, there are a few things that may be worth further discussion:
    
    - The lack of a constant corresponding to the ClassType on ClassDecl is
    inconvenient -- I'd like to see how zygoloid feels about trying to
    restructure this. i.e., I'm setting a constant in order to be able to
    track things down later, it'd be nice if the normal IR did this simply
    for consistency, or if we were able to combine these rather than having
    separate instructions.
    - Should we shift the parse node tracking further, and go with a setup
    wherein imports can embed import references into that? e.g., negative
    values go to another array which includes a ImportIRId for printing
    diagnostics, replacing the invalid NodeId.
    - Can the formatter switch to a more general scan of instructions for
    naming, to eliminate the ImportRef constant approach added here?
    - GetExprValueForLookupResult special-casing instructions felt
    surprising, I might see if there's a way to restructure to avoid that.
    
    But I think these issues are things that can be separated out.

--- toolchain/check/handle_name.cpp
+++ toolchain/check/handle_name.cpp
@@ -49,21 +49,40 @@ static auto GetExprValueForLookupResult(Context& context,
     -> SemIR::InstId {
   // If lookup finds a class declaration, the value is its `Self` type.
   auto lookup_result = context.insts().Get(lookup_result_id);
-  if (auto class_decl = lookup_result.TryAs<SemIR::ClassDecl>()) {
-    return context.types().GetInstId(
-        context.classes().Get(class_decl->class_id).self_type_id);
+  switch (lookup_result.kind()) {
+    case SemIR::ClassDecl::Kind: {
+      auto class_decl = lookup_result.As<SemIR::ClassDecl>();
+      return context.types().GetInstId(
+          context.classes().Get(class_decl.class_id).self_type_id);
+    }
+    case SemIR::InterfaceDecl::Kind: {
+      auto interface_decl = lookup_result.As<SemIR::InterfaceDecl>();
+      return TryEvalInst(context, SemIR::InstId::Invalid,
+                         SemIR::InterfaceType{SemIR::TypeId::TypeType,
+                                              interface_decl.interface_id})
+          .inst_id();
+    }
+    case SemIR::ImportRefUsed::Kind: {
+      auto import_ref = lookup_result.As<SemIR::ImportRefUsed>();
+      const auto* import_ir = context.import_irs().Get(import_ref.ir_id);
+      auto import_kind = import_ir->insts().Get(import_ref.inst_id).kind();
+      // For a declared type, recurse to get the appropriate type value.
+      // Otherwise, the ImportRefUsed is sufficient.
+      if (import_kind == SemIR::ClassDecl::Kind ||
+          import_kind == SemIR::InterfaceDecl::Kind) {
+        return GetExprValueForLookupResult(
+            context, context.constant_values().Get(lookup_result_id).inst_id());
+      } else {
+        return lookup_result_id;
+      }
+    }
+    default:
+      // Anything else should be a typed value already.
+      CARBON_CHECK(lookup_result.kind().value_kind() ==
+                   SemIR::InstValueKind::Typed)
+          << "Unexpected kind for lookup result, " << lookup_result;
+      return lookup_result_id;
   }
-  if (auto interface_decl = lookup_result.TryAs<SemIR::InterfaceDecl>()) {
-    return TryEvalInst(context, SemIR::InstId::Invalid,
-                       SemIR::InterfaceType{SemIR::TypeId::TypeType,
-                                            interface_decl->interface_id})
-        .inst_id();
-  }
-
-  // Anything else should be a typed value already.
-  CARBON_CHECK(lookup_result.kind().value_kind() == SemIR::InstValueKind::Typed)
-      << "Unexpected kind for lookup result, " << lookup_result;
-  return lookup_result_id;
 }
 
 static auto GetClassElementIndex(Context& context, SemIR::InstId element_id)
