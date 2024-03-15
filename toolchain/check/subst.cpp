254be5570c52820e99023b6d2d3f5f670483e39f
Richard Smith <richard@metafoo.co.uk>
2024-03-15 21:42:09 +0000

    Switch to using factored out facilities.

--- toolchain/check/subst.cpp
+++ toolchain/check/subst.cpp
@@ -5,6 +5,7 @@
 #include "toolchain/check/subst.h"
 
 #include "toolchain/sem_ir/ids.h"
+#include "toolchain/sem_ir/cow_block.h"
 #include "toolchain/check/eval.h"
 
 namespace Carbon::Check {
@@ -42,66 +43,30 @@ class Worklist {
   llvm::SmallVector<WorklistItem, 512> worklist;
 };
 
-enum class ArgKind : std::uint8_t {
-  Unimportant,
-  InstId,
-  TypeId,
-  InstBlockId,
-  TypeBlockId,
-};
-
 }  // namespace
 
-// TODO: Move this into SemIR and refactor with `inst_profile.cpp`.
-template<typename InstT, int N>
-static constexpr auto GetArgKind() -> ArgKind {
-  if constexpr (N >= SemIR::Internal::InstLikeTypeInfo<InstT>::NumArgs) {
-    // This argument is not used by this instruction; don't profile it.
-    return ArgKind::Unimportant;
-  } else {
-    using ArgT = SemIR::Internal::InstLikeTypeInfo<InstT>::template ArgType<N>;
-    if (std::is_same_v<ArgT, SemIR::InstId>) {
-      return ArgKind::InstId;
-    } else if (std::is_same_v<ArgT, SemIR::TypeId>) {
-      return ArgKind::TypeId;
-    } else if (std::is_same_v<ArgT, SemIR::InstBlockId>) {
-      return ArgKind::InstBlockId;
-    } else if (std::is_same_v<ArgT, SemIR::TypeBlockId>) {
-      return ArgKind::TypeBlockId;
-    } else {
-      return ArgKind::Unimportant;
-    }
-  }
-}
-
-static constexpr std::pair<ArgKind, ArgKind> ArgKinds[] = {
-#define CARBON_SEM_IR_INST_KIND(KindName) \
-  {GetArgKind<SemIR::KindName, 0>(), GetArgKind<SemIR::KindName, 1>()},
-#include "toolchain/sem_ir/inst_kind.def"
-};
-
 // Pushes the specified operand onto the worklist.
-static auto PushOperand(Context& context, Worklist& worklist, ArgKind kind,
+static auto PushOperand(Context& context, Worklist& worklist, SemIR::IdKind kind,
                         int32_t arg) -> void {
   switch (kind) {
-    case ArgKind::Unimportant:
-      break;
-    case ArgKind::InstId:
+    case SemIR::IdKind::For<SemIR::InstId>:
       worklist.Push(static_cast<SemIR::InstId>(arg));
       break;
-    case ArgKind::TypeId:
+    case SemIR::IdKind::For<SemIR::TypeId>:
       worklist.Push(context.types().GetInstId(static_cast<SemIR::TypeId>(arg)));
       break;
-    case ArgKind::InstBlockId:
+    case SemIR::IdKind::For<SemIR::InstBlockId>:
       for (auto inst_id :
            context.inst_blocks().Get(static_cast<SemIR::InstBlockId>(arg))) {
         worklist.Push(inst_id);
       }
       break;
-    case ArgKind::TypeBlockId:
+    case SemIR::IdKind::For<SemIR::TypeBlockId>:
       for (auto type_id : context.type_blocks().Get(static_cast<SemIR::TypeBlockId>(arg))) {
         worklist.Push(context.types().GetInstId(type_id));
       }
+      break;
+    default:
       break;
   }
 }
@@ -111,81 +76,42 @@ static auto PushOperand(Context& context, Worklist& worklist, ArgKind kind,
 static auto ExpandOperands(Context& context, Worklist& worklist,
                            SemIR::InstId inst_id) -> void {
   auto inst = context.insts().Get(inst_id);
-  auto kinds = ArgKinds[inst.kind().AsInt()];
+  auto kinds = inst.ArgKinds();
   PushOperand(context, worklist, kinds.first, inst.arg0());
   PushOperand(context, worklist, kinds.second, inst.arg1());
 }
 
-// TODO: Copied from convert.cpp. Deduplicate!
-namespace {
-// A handle to a new block that may be modified, with copy-on-write semantics.
-//
-// The constructor is given the ID of an existing block that provides the
-// initial contents of the new block. The new block is lazily allocated; if no
-// modifications have been made, the `id()` function will return the original
-// block ID.
-//
-// This is intended to avoid an unnecessary block allocation in the case where
-// the new block ends up being exactly the same as the original block.
-template<typename BlockIdType, auto (SemIR::File::*ValueStore)()>
-class CopyOnWriteBlock {
- public:
-  // Constructs the block. If `source_id` is valid, it is used as the initial
-  // value of the block. Otherwise, uninitialized storage for `size` elements
-  // is allocated.
-  CopyOnWriteBlock(SemIR::File& file, BlockIdType source_id)
-      : file_(file), source_id_(source_id) {}
-
-  auto id() const -> BlockIdType { return id_; }
-
-  auto Set(int i, typename BlockIdType::ElementType value) -> void {
-    if (source_id_.is_valid() && (file_.*ValueStore)().Get(id_)[i] == value) {
-      return;
-    }
-    if (id_ == source_id_) {
-      id_ = (file_.*ValueStore)().Add((file_.*ValueStore)().Get(source_id_));
-    }
-    (file_.*ValueStore)().Get(id_)[i] = value;
-  }
-
- private:
-  SemIR::File& file_;
-  BlockIdType source_id_;
-  BlockIdType id_ = source_id_;
-};
-}  // namespace
-
 // Pops the specified operand from the worklist and returns it.
-static auto PopOperand(Context& context, Worklist& worklist, ArgKind kind,
+static auto PopOperand(Context& context, Worklist& worklist, SemIR::IdKind kind,
                        int32_t arg) -> int32_t {
   switch (kind) {
-    case ArgKind::Unimportant:
-      return arg;
-    case ArgKind::InstId:
+    case SemIR::IdKind::For<SemIR::InstId>:
       return worklist.Pop().index;
-    case ArgKind::TypeId:
+    case SemIR::IdKind::For<SemIR::TypeId>:
       return context.GetTypeIdForTypeInst(worklist.Pop()).index;
-    case ArgKind::InstBlockId: {
+    case SemIR::IdKind::For<SemIR::InstBlockId>: {
       auto old_inst_block_id = static_cast<SemIR::InstBlockId>(arg);
       auto size = context.inst_blocks().Get(old_inst_block_id).size();
-      CopyOnWriteBlock<SemIR::InstBlockId, &SemIR::File::inst_blocks>
-          new_inst_block(context.sem_ir(), old_inst_block_id);
+      SemIR::CopyOnWriteInstBlock new_inst_block(context.sem_ir(),
+                                                 old_inst_block_id);
       for (auto i : llvm::index_range(0, size)) {
         new_inst_block.Set(size - i - 1, worklist.Pop());
       }
       return new_inst_block.id().index;
     }
-    case ArgKind::TypeBlockId: {
+    case SemIR::IdKind::For<SemIR::TypeBlockId>: {
       auto old_type_block_id = static_cast<SemIR::TypeBlockId>(arg);
       auto size = context.type_blocks().Get(old_type_block_id).size();
-      CopyOnWriteBlock<SemIR::TypeBlockId, &SemIR::File::type_blocks>
-          new_type_block(context.sem_ir(), old_type_block_id);
+      SemIR::CopyOnWriteTypeBlock new_type_block(context.sem_ir(),
+                                                 old_type_block_id);
       for (auto i : llvm::index_range(0, size)) {
         new_type_block.Set(size - i - 1,
                            context.GetTypeIdForTypeInst(worklist.Pop()));
       }
       return new_type_block.id().index;
     }
+    default:
+      return arg;
   }
 }
 
@@ -194,7 +120,7 @@ static auto PopOperand(Context& context, Worklist& worklist, ArgKind kind,
 static auto Rebuild(Context& context, Worklist& worklist,
                     SemIR::InstId inst_id) -> SemIR::InstId {
   auto inst = context.insts().Get(inst_id);
-  auto kinds = ArgKinds[inst.kind().AsInt()];
+  auto kinds = inst.ArgKinds();
 
   // Note that we pop in reverse order because we pushed them in forwards order.
   int32_t arg1 = PopOperand(context, worklist, kinds.second, inst.arg1());
