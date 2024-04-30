b4e56449b6a1842accc13b9735602b3b38da20a8
Josh L <josh11b@users.noreply.github.com>
2024-04-30 23:08:36 +0000

    Apply suggestion

--- toolchain/check/inst_block_stack.h
+++ toolchain/check/inst_block_stack.h
@@ -124,15 +124,15 @@ class InstBlockStack {
 
   std::vector<SemIR::InstId> init_block_;
 
+  // Current global init block to push.
+  SemIR::InstBlockId init_block_id_ = SemIR::InstBlockId::GlobalInit;
+
   // The actual stack.
   llvm::SmallVector<StackEntry> stack_;
 
   // The size of the stack. Entries after this in `stack_` are kept around so
   // that we can reuse the allocated buffer for their content.
   int size_ = 0;
-
-  // Current global init block to push.
-  SemIR::InstBlockId init_block_id_ = SemIR::InstBlockId::GlobalInit;
 };
 
 }  // namespace Carbon::Check
