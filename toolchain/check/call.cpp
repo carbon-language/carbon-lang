72d6fbc408234ad7c8a2e34e05f168075598db72
Richard Smith <richard@metafoo.co.uk>
2024-05-07 16:36:24 -0700

    Reflow comment
    Co-authored-by: Carbon Infra Bot <carbon-external-infra@google.com>

--- toolchain/check/call.cpp
+++ toolchain/check/call.cpp
@@ -12,7 +12,8 @@
 
 namespace Carbon::Check {
 
-// Performs a call where the callee is the name of a generic class, such as `Vector(i32)`.
+// Performs a call where the callee is the name of a generic class, such as
+// `Vector(i32)`.
 static auto PerformCallToGenericClass(Context& context, Parse::NodeId node_id,
                                       SemIR::ClassId class_id,
                                       llvm::ArrayRef<SemIR::InstId> arg_ids)
