abbea462dcfdfb0066da5aa5880b50d887b68a9b
jonmeow <jperkins@google.com>
2024-05-17 08:01:32 -0700

    Handling for cross-package imports of exports.

--- toolchain/check/handle_export.cpp
+++ toolchain/check/handle_export.cpp
@@ -38,6 +38,20 @@ auto HandleExportDecl(Context& context, Parse::ExportDeclId node_id) -> bool {
     return true;
   }
 
+  auto inst = context.insts().Get(inst_id);
+
+  if (inst.Is<SemIR::ExportDecl>()) {
+    CARBON_DIAGNOSTIC(ExportRedundant, Warning,
+                      "`export` matches previous `export`.");
+    CARBON_DIAGNOSTIC(ExportPrevious, Note, "Previous `export` here.");
+    context.emitter()
+        .Build(node_id, ExportRedundant)
+        // Use the location of the export itself, not the exported instruction.
+        .Note(context.insts().GetLocId(inst_id), ExportPrevious)
+        .Emit();
+    return true;
+  }
+
   auto import_ref = context.insts().TryGetAs<SemIR::ImportRefLoaded>(inst_id);
   if (!import_ref) {
     CARBON_DIAGNOSTIC(ExportNotImportedEntity, Error,
@@ -56,6 +70,14 @@ auto HandleExportDecl(Context& context, Parse::ExportDeclId node_id) -> bool {
                                   .bind_name_id = import_ref->bind_name_id,
                                   .value_id = inst_id}});
   context.AddExport(export_id);
+
+  // Replace the ImportRef in name lookup, both for the above duplicate
+  // diagnostic and so that cross-package imports can find it easily.
+  auto bind_name = context.bind_names().Get(import_ref->bind_name_id);
+  auto& names = context.name_scopes().Get(bind_name.enclosing_scope_id).names;
+  auto it = names.find(bind_name.name_id);
+  CARBON_CHECK(it->second == inst_id);
+  it->second = export_id;
 
   return true;
 }
