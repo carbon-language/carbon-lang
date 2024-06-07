7fab6fd70ddbb6e076f51c7af2da755f0516ff22
Chandler Carruth <chandlerc@gmail.com>
2024-06-07 09:20:03 +0000

    more review feedback

--- common/raw_hashtable_metadata_group_benchmark.cpp
+++ common/raw_hashtable_metadata_group_benchmark.cpp
@@ -65,6 +65,8 @@ class BenchmarkSIMDMetadataGroup : public MetadataGroup {
 
 namespace {
 
+// The number of metadata groups we use when benchmarking a particular scenario
+// of matching within a group.
 constexpr ssize_t BenchSize = 256;
 
 #if CARBON_NEON_SIMD_SUPPORT || CARBON_X86_SIMD_SUPPORT
@@ -73,10 +75,15 @@ using SIMDGroup = BenchmarkSIMDMetadataGroup;
 #endif
 
 struct BenchMetadata {
+  // The metadata for benchmarking, arranged in `BenchSize` groups, each one
+  // `GroupSize` in length. As a consequence, the size of this array will always
+  // be `BenchSize * GroupSize`.
   llvm::MutableArrayRef<uint8_t> metadata;
 
-  // For random byte metadata, also store the specific byte to search for in
-  // each group here. For other kinds, this is empty.
+  // For benchmarking random matches in the metadata, each byte here is the tag
+  // that should be matched against the corresponding group of the metadata.
+  // Because this array parallels the *groups* of the metadata array, its size
+  // will be `BenchSize`. For other kinds, this is empty.
   llvm::ArrayRef<uint8_t> bytes;
 };
 
@@ -86,11 +93,29 @@ enum class BenchKind : uint8_t {
   Deleted,
 };
 
+// This routine should only be called once per `BenchKind` as the initializer of
+// a global variable below. It returns an `ArrayRef` pointing into
+// function-local static storage that provides our benchmark metadata.
+//
+// The returned array will have exactly `GroupSize` elements, each of
+// `BenchMetadata`. For the `BenchMetadata` at index `i`, there will be `i+1`
+// matches of that kind within each group of the metadata. This let's us
+// benchmark each of the possible match-counts for a group.
 template <BenchKind Kind = BenchKind::Random>
 static auto BuildBenchMetadata() -> llvm::ArrayRef<BenchMetadata> {
+  // We build `GroupSize` elements of `BenchMetadata` below, and so we need
+  // `GroupSize` copies of each of these arrays to serve as inputs to it.
+  //
+  // The first storage is of `BenchSize` groups of metadata.
   static uint8_t metadata_storage[GroupSize][BenchSize * GroupSize];
+  // When `Kind` is `Random`, each group above will have a *different* byte that
+  // matches in that group. This array stores those bytes for teh benchmark to
+  // match against the group.
   static uint8_t bytes_storage[GroupSize][BenchSize];
+
+  // The backing storage for the returned `ArrayRef`.
   static BenchMetadata bm_storage[GroupSize];
+
   absl::BitGen gen;
   for (auto [bm_index, bm] : llvm::enumerate(bm_storage)) {
     int match_count = bm_index + 1;
@@ -157,17 +182,37 @@ template <BenchKind Kind>
 // NOLINTNEXTLINE(google-readability-casting): False positive clang-tidy bug.
 const auto bench_metadata = BuildBenchMetadata<Kind>();
 
+// Benchmark that simulates the dynamic execution pattern when we match exactly
+// one entry in the group, typically then using the index of the matching byte
+// to index into an element of a group of entries. But notably, the *first*
+// match is sufficient, and we never have to find the *next* match within the
+// group.
 template <BenchKind Kind, typename GroupT = MetadataGroup>
 static void BM_LoadMatch(benchmark::State& s) {
   BenchMetadata bm = bench_metadata<Kind>[0];
-  ssize_t nonce_data[GroupSize];
-  for (ssize_t& n : nonce_data) {
+
+  // We want to make the index used by the next iteration of the benchmark have
+  // a data dependency on the result of matching. A match produces an index into
+  // the group of metadata. To consume this match in a way that is
+  // representative of how it will be used in a hashtable (indexing into an
+  // array of entries), while establishing that dependence, we keep a
+  // group-sized array of the value `1` in memory that we can index into to
+  // increment to the next step of the loop. We do have to hide the contents of
+  // the loop from the optimizer by clobbering the memory.
+  ssize_t all_ones[GroupSize];
+  for (ssize_t& n : all_ones) {
     n = 1;
   }
-  // Now hide the contents of `nonce_data`.
   benchmark::ClobberMemory();
+
+  // We don't want the optimizer to peel iterations off of this loop, so hide
+  // the starting index.
   ssize_t i = 0;
   benchmark::DoNotOptimize(i);
+
+  // This loop looks *really* attractive to unroll to the compiler. However,
+  // that can easily overlap some of the memory operations and generally makes
+  // it harder to analyze the exact operation sequence we care about.
 #pragma clang loop unroll(disable)
   for (auto _ : s) {
     auto g = GroupT::Load(bm.metadata.data(), i * GroupSize);
@@ -180,8 +225,18 @@ static void BM_LoadMatch(benchmark::State& s) {
       static_assert(Kind == BenchKind::Random);
       matches = static_cast<MetadataGroup::MatchIndex>(g.Match(bm.bytes[i]));
     }
+    // Despite not being a DCHECK, this is fine for benchmarking. In an actual
+    // hashtable, we expect to have a test for empty of the match prior to using
+    // it to index an array, and that test is expected to be strongly predicted.
+    // That exactly matches how the `CARBON_CHECK` macro works, and so this
+    // serves as both a good correctness test and replication of hashtable usage
+    // of a match.
     CARBON_CHECK(matches);
-    i = (i + nonce_data[matches.index()]) & (BenchSize - 1);
+
+    // Now do the data-dependent increment by indexing our "all ones" array. The
+    // index into `all_ones` is analogous to the index into a group of hashtable
+    // entries.
+    i = (i + all_ones[matches.index()]) & (BenchSize - 1);
   }
 }
 BENCHMARK(BM_LoadMatch<BenchKind::Random>);
@@ -196,27 +251,67 @@ BENCHMARK(BM_LoadMatch<BenchKind::Empty, SIMDGroup>);
 BENCHMARK(BM_LoadMatch<BenchKind::Deleted, SIMDGroup>);
 #endif
 
+// Benchmark that measures the speed of a match that is only found after at
+// least one miss. Because the first match doesn't work, this covers
+// incrementing to the next match, with a number of increments taken from the
+// `Step` template parameter.
 template <BenchKind Kind, ssize_t Steps>
 static void BM_LoadMatchMissSteps(benchmark::State& s) {
   static_assert(Steps > 0);
   static_assert(Steps <= GroupSize);
+
+  // We pick the benchmark metadata at index `Steps - 1`, which will have
+  // `Steps` matches within each group.
   BenchMetadata bm = bench_metadata<Kind>[Steps - 1];
-  ssize_t nonce_data[GroupSize];
-  for (ssize_t& n : nonce_data) {
+
+  // We want to make the index used by the next iteration of the benchmark have
+  // a data dependency on the result of matching. A match produces an index into
+  // the group of metadata. To consume this match in a way that is
+  // representative of how it will be used in a hashtable (indexing into an
+  // array of entries), while establishing that dependence, we keep a
+  // group-sized array of the value `1` in memory that we can index into to
+  // increment to the next step of the loop. We do have to hide the contents of
+  // the loop from the optimizer by clobbering the memory.
+  ssize_t all_ones[GroupSize];
+  for (ssize_t& n : all_ones) {
     n = 1;
   }
-  // Now hide the contents of `nonce_data`.
   benchmark::ClobberMemory();
+
+  // We don't want the optimizer to peel iterations off of this loop, so hide
+  // the starting index.
   ssize_t i = 0;
   benchmark::DoNotOptimize(i);
+
+  // This loop looks *really* attractive to unroll to the compiler. However,
+  // that can easily overlap some of the memory operations and generally makes
+  // it harder to analyze the exact operation sequence we care about.
 #pragma clang loop unroll(disable)
   for (auto _ : s) {
     auto g = MetadataGroup::Load(bm.metadata.data(), i * GroupSize);
     auto matched_range = g.Match(bm.bytes[i]);
+
+    // We don't use a `CARBON_CHECK` here as the loop below will test the range
+    // to see if the loop should be skipped, replicating the test that we also
+    // expect in hashtable usage. 
+
+    // We want to simulate the code sequence a hashtable would produce when
+    // matching indices are "misses" in the hashtable, but only the aspects of
+    // those that reflect on the specific *match* implementation's generated
+    // code and performance. For each index in the match, we locate it in the
+    // `matched_range`, extract it as an index, and use that to index a
+    // group-sized array. We read memory from that array to increment `indices`,
+    // establishing data dependencies on each match index. This loop will run
+    // exactly `Steps` times.
     ssize_t indices = 0;
     for (ssize_t index : matched_range) {
-      indices += nonce_data[index];
+      indices += all_ones[index];
     }
+
+    // We want to propagate the data dependencies accumulated into `indices`
+    // into the next value of `i`, and we know exactly how many increments were
+    // done in the loop, so subtract that constant and add one to arrive back at
+    // an increment of 1.
     i = (i + (indices - Steps + 1)) & (BenchSize - 1);
   }
 }
