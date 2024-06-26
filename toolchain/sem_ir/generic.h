25a5059d554d9430ebd00a09a7fcf40f0ced6340
Richard Smith <richard@metafoo.co.uk>
2024-06-26 07:05:35 +0000

    Remove placeholder stuff from Generic.

--- toolchain/sem_ir/generic.h
+++ toolchain/sem_ir/generic.h
@@ -12,17 +12,6 @@ namespace Carbon::SemIR {
 // Information for a generic entity, such as a generic class, a generic
 // interface, or generic function.
 struct Generic : public Printable<Generic> {
-  // A portion of the generic corresponding to either the declaration or the
-  // definition. These are tracked separately because they're built and resolved
-  // at different times.
-  struct Fragment {
-    // A block containing symbolic constants that are used in this fragment.
-    InstBlockId symbolic_constants_id = InstBlockId::Invalid;
-    // TODO: Also track:
-    // - Types required to be complete in this generic.
-    // - Template-dependent instructions in this generic.
-  };
-
   auto Print(llvm::raw_ostream& out) const -> void {
     out << "{decl: " << decl_id << ", bindings: " << bindings_id << "}";
   }
@@ -35,11 +24,6 @@ struct Generic : public Printable<Generic> {
   // A block containing the IDs of compile time bindings in this generic scope.
   // The index in this block will match the `bind_index` of the instruction.
   InstBlockId bindings_id;
-
-  // The portion of the generic corresponding to the declaration of the entity.
-  Fragment decl;
-  // The portion of the generic corresponding to the definition of the entity.
-  Fragment definition;
 };
 
 }  // namespace Carbon::SemIR
