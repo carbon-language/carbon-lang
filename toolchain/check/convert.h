dbe8380553ec87618ca2d4bf873fcb5105622653
jonmeow <jperkins@google.com>
2024-07-24 16:02:30 -0700

    pre-commit

--- toolchain/check/convert.h
+++ toolchain/check/convert.h
@@ -95,8 +95,10 @@ auto ConvertForExplicitAs(Context& context, Parse::NodeId as_node,
 auto ConvertCallArgs(Context& context, SemIR::LocId call_loc_id,
                      SemIR::InstId self_id,
                      llvm::ArrayRef<SemIR::InstId> arg_refs,
-                     SemIR::InstId return_storage_id, const SemIR::EntityWithParamsBase& callee,
-                     SemIR::GenericInstanceId callee_specific_id) -> SemIR::InstBlockId;
+                     SemIR::InstId return_storage_id,
+                     const SemIR::EntityWithParamsBase& callee,
+                     SemIR::GenericInstanceId callee_specific_id)
+    -> SemIR::InstBlockId;
 
 // Converts an expression for use as a type.
 auto ExprAsType(Context& context, SemIR::LocId loc_id, SemIR::InstId value_id)
