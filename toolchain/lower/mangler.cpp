b879dc75832a1f6c1ee78cc2e1f0e20146b31e75
David Blaikie <dblaikie@gmail.com>
2024-08-28 21:46:24 +0000

    Namespace mangling

--- toolchain/lower/mangler.cpp
+++ toolchain/lower/mangler.cpp
@@ -18,7 +18,24 @@ auto Mangler::Mangle(SemIR::FunctionId function_id) -> std::string {
   auto name = sem_ir_.names().GetAsStringIfIdentifier(function.name_id);
   CARBON_CHECK(name) << "Unexpected special name for function: "
                      << function.name_id;
-  return "_C" + name->str();
+  SemIR::NameScopeId parent_scope_id = function.parent_scope_id;
+  std::string result = "_C" + name->str();
+  while (parent_scope_id.is_valid()) {
+    const auto& parent = sem_ir_.name_scopes().Get(parent_scope_id);
+    if (parent.name_id == SemIR::NameId::PackageNamespace) {
+      break;
+    }
+    auto name = sem_ir_.names().GetAsStringIfIdentifier(parent.name_id);
+    if (!name) {
+      break;
+    }
+    CARBON_CHECK(name) << "Unexpected special name for function scope: "
+                       << function.name_id;
+    result += '.';
+    result += *name;
+    parent_scope_id = parent.parent_scope_id;
+  }
+  return result;
 }
 
 }  // namespace Carbon::Lower
