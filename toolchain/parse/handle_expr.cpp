5566f6065031742856aec4ba063b1cb33e67b988
Josh L <josh11b@users.noreply.github.com>
2024-09-04 22:05:11 +0000

    Checkpoint progress.

--- toolchain/parse/handle_expr.cpp
+++ toolchain/parse/handle_expr.cpp
@@ -5,6 +5,7 @@
 #include "toolchain/lex/token_kind.h"
 #include "toolchain/parse/context.h"
 #include "toolchain/parse/handle.h"
+#include "toolchain/parse/handle_requirement.h"
 
 namespace Carbon::Parse {
 
@@ -217,73 +218,6 @@ auto HandleExprInPostfix(Context& context) -> void {
       break;
     }
   }
-}
-
-static auto BeginRequirement(Context& context) -> void {
-  // context.PushState(State::RequirementAnd);
-  context.PushState(State::RequirementOperator);
-  context.PushStateForExpr(PrecedenceGroup::ForRequirements());
-}
-
-auto HandleRequirementOperator(Context& context) -> void {
-  auto state = context.PopState();
-  auto kind = context.PositionKind();
-  state.token = context.Consume();
-
-  switch (kind) {
-    case Lex::TokenKind::Impls: {
-      break;
-    }
-    case Lex::TokenKind::Equal: {
-      break;
-    }
-    case Lex::TokenKind::EqualEqual: {
-      break;
-    }
-    default: {
-      CARBON_DIAGNOSTIC(
-          ExpectedRequirementOperator, Error,
-          "requirement should use `impls`, `=`, or `==` operator.");
-      context.emitter().Emit(state.token, ExpectedRequirementOperator);
-      context.ReturnErrorOnState();
-      return;
-    }
-  }
-  context.PushState(state, State::RequirementOperatorFinish);
-  context.PushStateForExpr(PrecedenceGroup::ForRequirements());
-}
-
-auto HandleRequirementOperatorFinish(Context& context) -> void {
-  auto state = context.PopState();
-
-  switch (auto token_kind = context.tokens().GetKind(state.token)) {
-    case Lex::TokenKind::Impls: {
-      context.AddNode(NodeKind::RequirementImpls, state.token, state.has_error);
-      break;
-    }
-    case Lex::TokenKind::Equal: {
-      context.AddNode(NodeKind::RequirementAssign, state.token,
-                      state.has_error);
-      break;
-    }
-    case Lex::TokenKind::EqualEqual: {
-      context.AddNode(NodeKind::RequirementEquals, state.token,
-                      state.has_error);
-      break;
-    }
-    default:
-      // RequirementOperatorFinish state is only pushed in
-      // HandleRequirementOperator on one of the three requirement operator
-      // tokens.
-      CARBON_FATAL() << "Unexpected token kind for requirement operator: "
-                     << token_kind;
-      return;
-  }
-}
-
-auto HandleWhereFinish(Context& context) -> void {
-  auto state = context.PopState();
-  context.AddNode(NodeKind::WhereExpr, state.token, state.has_error);
 }
 
 auto HandleExprInPostfixLoop(Context& context) -> void {
---
+++ toolchain/parse/handle_requirement.cpp
@@ -0,0 +1,80 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "toolchain/parse/handle_requirement.h"
+
+#include "toolchain/lex/token_kind.h"
+#include "toolchain/parse/context.h"
+#include "toolchain/parse/handle.h"
+
+namespace Carbon::Parse {
+
+auto BeginRequirement(Context& context) -> void {
+  // context.PushState(State::RequirementAnd);
+  context.PushState(State::RequirementOperator);
+  context.PushStateForExpr(PrecedenceGroup::ForRequirements());
+}
+
+auto HandleRequirementOperator(Context& context) -> void {
+  auto state = context.PopState();
+  auto kind = context.PositionKind();
+  state.token = context.Consume();
+
+  switch (kind) {
+    case Lex::TokenKind::Impls: {
+      break;
+    }
+    case Lex::TokenKind::Equal: {
+      break;
+    }
+    case Lex::TokenKind::EqualEqual: {
+      break;
+    }
+    default: {
+      CARBON_DIAGNOSTIC(
+          ExpectedRequirementOperator, Error,
+          "requirement should use `impls`, `=`, or `==` operator.");
+      context.emitter().Emit(state.token, ExpectedRequirementOperator);
+      context.ReturnErrorOnState();
+      return;
+    }
+  }
+  context.PushState(state, State::RequirementOperatorFinish);
+  context.PushStateForExpr(PrecedenceGroup::ForRequirements());
+}
+
+auto HandleRequirementOperatorFinish(Context& context) -> void {
+  auto state = context.PopState();
+
+  switch (auto token_kind = context.tokens().GetKind(state.token)) {
+    case Lex::TokenKind::Impls: {
+      context.AddNode(NodeKind::RequirementImpls, state.token, state.has_error);
+      break;
+    }
+    case Lex::TokenKind::Equal: {
+      context.AddNode(NodeKind::RequirementAssign, state.token,
+                      state.has_error);
+      break;
+    }
+    case Lex::TokenKind::EqualEqual: {
+      context.AddNode(NodeKind::RequirementEquals, state.token,
+                      state.has_error);
+      break;
+    }
+    default:
+      // RequirementOperatorFinish state is only pushed in
+      // HandleRequirementOperator on one of the three requirement operator
+      // tokens.
+      CARBON_FATAL() << "Unexpected token kind for requirement operator: "
+                     << token_kind;
+      return;
+  }
+}
+
+auto HandleWhereFinish(Context& context) -> void {
+  auto state = context.PopState();
+  context.AddNode(NodeKind::WhereExpr, state.token, state.has_error);
+}
+
+}  // namespace Carbon::Parse
