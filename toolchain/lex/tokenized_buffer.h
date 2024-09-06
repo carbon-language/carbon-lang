c43fa3a8a523a4fd68163fd89068b8d12e304f23
Chandler Carruth <chandlerc@gmail.com>
2024-09-06 09:22:36 -0700

    Bit-pack the lexer's token info (#4270)
    This makes each token info consist of 8 bytes of data:
    - 1 byte of the kind
    - 1 bit for whitespace tracking
    - 23 bits of payload
    - 32 bits for byte offset in the file
    
    This builds directly on representing the location of the token as
    a single 32-bit offset, now compressing the rest of the data into
    a single 32-bit bitfield.
    
    This adds some implementation limits: we can no longer lex more than
    2^23 tokens in a single source file. Nor can we have more than 2^23
    string literals, integer literals, real literals, or identifiers. Only
    the first of these is even close to an issue, and even then seems
    unlikely to ever be a problem in practice.
    
    The memory efficiency here is great and the motivating goal. But to make
    this work well, we also need to streamline how we create the tokens.
    Otherwise, all the bit fiddling can end up erasing our gains. This PR
    adds a number of APIs to manage creating and accessing the now
    significantly more complex storage of token infos to try and help with
    this.
    
    One big change required to simplify the writes here is to switch from
    computing whether a token has trailing space after-the-fact to
    pre-computing whether a token will have leading space. That lets us have
    the leading space information available immediately when forming the
    token, and avoids doing a single bit flip afterward.
    
    Another change that helps with this representation is to minimize the
    updating of groups after-the-fact. The code now tries to set the opening
    index directly when creating the closing token and only updates the
    opening group afterward. Because of the bit packing, this is a reduction
    of 0.5% of dynamic instructions in the compile benchmark, and has
    dramatic improvements for the grouping symbol focused benchmarks.
    
    All combined, this is a significant improvement on the lexer-focused
    benchmarks despite the added complexity, and a significant win on our
    compile time benchmarks due to both the lexer improvements and
    downstream memory density improvements: 5-12% reduction in lex time,
    growing larger as files get larger. About a 4.5% reduction in parse
    time, and even a 1-2% reduction in total check time. =D
    
    ---------
    
    Co-authored-by: Jon Ross-Perkins <jperkins@google.com>
    Co-authored-by: Geoff Romer <gromer@google.com>

--- toolchain/lex/tokenized_buffer.h
+++ toolchain/lex/tokenized_buffer.h
@@ -260,33 +260,161 @@ class TokenizedBuffer : public Printable<TokenizedBuffer> {
     int indent;
   };
 
-  struct TokenInfo {
-    TokenKind kind;
+  // Storage for the information about a specific token in the buffer.
+  //
+  // This provides a friendly accessor API to the carefully space-optimized
+  // storage model of the information we associated with each token.
+  //
+  // There are four pieces of information stored here:
+  // - The kind of the token.
+  // - Whether that token has leading whitespace before it.
+  // - A kind-specific payload that can be compressed into a small integer.
+  //   - This class provides dedicated accessors for each different form of
+  //     payload that check the kind and payload correspond correctly.
+  // - A 32-bit byte offset of the token within the source text.
+  //
+  // These are compressed and stored in 8-bytes for each token.
+  //
+  // Note that while the class provides some limited setters for payloads and
+  // mutating methods, setters on this type may be unexpectedly expensive due to
+  // the bit-packed representation and should be avoided. As such, only the
+  // minimal necessary setters are provided.
+  //
+  // TODO: It might be worth considering a struct-of-arrays data layout in order
+  // to move the byte offset to a separate array from the rest as it is only hot
+  // during lexing, and then cold during parsing and semantic analysis. However,
+  // a trivial approach to that adds more overhead than it saves due to tracking
+  // two separate vectors and their growth. Making this profitable would likely
+  // at least require a highly specialized single vector that manages the growth
+  // once and then provides separate storage areas for the two arrays.
+  class TokenInfo {
+   public:
+    // The kind for this token.
+    auto kind() const -> TokenKind { return TokenKind::Make(kind_); }
 
-    // Whether the token has trailing whitespace.
-    bool has_trailing_space = false;
+    // Whether this token is preceded by whitespace. We only store the preceding
+    // state, and look at the next token to check for trailing whitespace.
+    auto has_leading_space() const -> bool { return has_leading_space_; }
 
-    // Whether the token was injected artificially during error recovery.
-    bool is_recovery = false;
+    // A collection of methods to access the specific payload included with
+    // particular kinds of tokens. Only the specific payload accessor below may
+    // be used for an info entry of a token with a particular kind, and these
+    // check that the kind is valid. Some tokens do not include a payload at all
+    // and none of these methods may be called.
+    auto ident_id() const -> IdentifierId {
+      CARBON_DCHECK(kind() == TokenKind::Identifier);
+      return IdentifierId(token_payload_);
+    }
+    auto set_ident_id(IdentifierId ident_id) -> void {
+      CARBON_DCHECK(kind() == TokenKind::Identifier);
+      CARBON_DCHECK(ident_id.index < (2 << PayloadBits));
+      token_payload_ = ident_id.index;
+    }
 
-    // Zero-based byte offset of the token within the file.
-    int32_t byte_offset;
+    auto string_literal_id() const -> StringLiteralValueId {
+      CARBON_DCHECK(kind() == TokenKind::StringLiteral);
+      return StringLiteralValueId(token_payload_);
+    }
 
-    // We may have up to 32 bits of payload, based on the kind of token.
-    union {
-      static_assert(
-          sizeof(TokenIndex) <= sizeof(int32_t),
-          "Unable to pack token and identifier index into the same space!");
+    auto int_id() const -> IntId {
+      CARBON_DCHECK(kind() == TokenKind::IntLiteral ||
+                    kind() == TokenKind::IntTypeLiteral ||
+                    kind() == TokenKind::UnsignedIntTypeLiteral ||
+                    kind() == TokenKind::FloatTypeLiteral);
+      return IntId(token_payload_);
+    }
 
-      IdentifierId ident_id = IdentifierId::Invalid;
-      StringLiteralValueId string_literal_id;
-      IntId int_id;
-      RealId real_id;
-      TokenIndex closing_token;
-      TokenIndex opening_token;
-      int32_t error_length;
-    };
+    auto real_id() const -> RealId {
+      CARBON_DCHECK(kind() == TokenKind::RealLiteral);
+      return RealId(token_payload_);
+    }
+
+    auto closing_token_index() const -> TokenIndex {
+      CARBON_DCHECK(kind().is_opening_symbol());
+      return TokenIndex(token_payload_);
+    }
+    auto set_closing_token_index(TokenIndex closing_index) -> void {
+      CARBON_DCHECK(kind().is_opening_symbol());
+      CARBON_DCHECK(closing_index.index < (2 << PayloadBits));
+      token_payload_ = closing_index.index;
+    }
+
+    auto opening_token_index() const -> TokenIndex {
+      CARBON_DCHECK(kind().is_closing_symbol());
+      return TokenIndex(token_payload_);
+    }
+    auto set_opening_token_index(TokenIndex opening_index) -> void {
+      CARBON_DCHECK(kind().is_closing_symbol());
+      CARBON_DCHECK(opening_index.index < (2 << PayloadBits));
+      token_payload_ = opening_index.index;
+    }
+
+    auto error_length() const -> int {
+      CARBON_DCHECK(kind() == TokenKind::Error);
+      return token_payload_;
+    }
+
+    // Zero-based byte offset of the token within the file. This can be combined
+    // with the buffer's line information to locate the line and column of the
+    // token as well.
+    auto byte_offset() const -> int32_t { return byte_offset_; }
+
+    // Transforms the token into an error token of the given length but at its
+    // original position and with the same whitespace adjacency.
+    auto ResetAsError(int error_length) -> void {
+      // Construct a fresh token to establish any needed invariants and replace
+      // this token with it.
+      TokenInfo error(TokenKind::Error, has_leading_space(), error_length,
+                      byte_offset());
+      *this = error;
+    }
+
+   private:
+    friend class Lexer;
+
+    static constexpr int PayloadBits = 23;
+
+    // Constructor for a TokenKind that carries no payload, or where the payload
+    // will be set later.
+    //
+    // Only used by the lexer which enforces only the correct kinds are used.
+    //
+    // When the payload is not being set, we leave it uninitialized. At least in
+    // some cases, this will allow MSan to correctly detect erroneous attempts
+    // to access the payload, as it works to track uninitialized memory
+    // bit-for-bit specifically to handle complex cases like bitfields.
+    TokenInfo(TokenKind kind, bool has_leading_space, int32_t byte_offset)
+        : kind_(kind),
+          has_leading_space_(has_leading_space),
+          byte_offset_(byte_offset) {}
+
+    // Constructor for a TokenKind that carries a payload.
+    //
+    // Only used by the lexer which enforces the correct kind and payload types.
+    TokenInfo(TokenKind kind, bool has_leading_space, int payload,
+              int32_t byte_offset)
+        : kind_(kind),
+          has_leading_space_(has_leading_space),
+          token_payload_(payload),
+          byte_offset_(byte_offset) {
+      CARBON_DCHECK(payload >= 0 && payload < (2 << PayloadBits))
+          << "Payload won't fit into unsigned bit pack: " << payload;
+    }
+
+    // A bitfield that encodes the token's kind, the leading space flag, and the
+    // remaining bits in a payload. These are encoded together as a bitfield for
+    // density and because these are the hottest fields of tokens for consumers
+    // after lexing.
+    TokenKind::RawEnumType kind_ : sizeof(TokenKind) * 8;
+    bool has_leading_space_ : 1;
+    unsigned token_payload_ : PayloadBits;
+
+    // Separate storage for the byte offset, this is hot while lexing but then
+    // generally cold.
+    int32_t byte_offset_;
   };
+  static_assert(sizeof(TokenInfo) == 8,
+                "Expected `TokenInfo` to pack to an 8-byte structure.");
 
   struct LineInfo {
     explicit LineInfo(int32_t start) : start(start), indent(0) {}
@@ -333,6 +461,11 @@ class TokenizedBuffer : public Printable<TokenizedBuffer> {
   int expected_parse_tree_size_ = 0;
 
   bool has_errors_ = false;
+
+  // A vector of flags for recovery tokens. If empty, there are none. When doing
+  // token recovery, this will be extended to be indexable by token indices and
+  // contain true for the tokens that were synthesized for recovery.
+  llvm::BitVector recovery_tokens_;
 };
 
 // A diagnostic emitter that uses positions within a source buffer's text as
