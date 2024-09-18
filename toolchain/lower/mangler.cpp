d5ee110b824441dbe0eddb9fe3264f2b490bd6fb
David Blaikie <dblaikie@gmail.com>
2024-09-18 18:38:07 +0000

    Check the enclosing name scope is a package before stopping

--- toolchain/lower/mangler.cpp
+++ toolchain/lower/mangler.cpp
@@ -89,7 +89,8 @@ auto Mangler::MangleInverseQualifiedNameScope(llvm::raw_ostream& os,
         CARBON_FATAL("Attempting to mangle unsupported SemIR.");
         break;
     }
-    if (!name_scope.is_closed_import) {
+    if (!name_scope.is_closed_import ||
+        name_scope.parent_scope_id != SemIR::NameScopeId::Package) {
       names_to_render.push_back(
           {.name_scope_id = name_scope.parent_scope_id, .prefix = '.'});
     }
