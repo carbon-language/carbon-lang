c28c94d586b7b50f11a2c0a5743e32a935e8ece6
Josh L <josh11b@users.noreply.github.com>
2024-09-19 21:45:08 +0000

    Checkpoint progress.

--- toolchain/check/handle_where.cpp
+++ toolchain/check/handle_where.cpp
@@ -15,27 +15,20 @@ auto HandleParseNode(Context& context, Parse::WhereOperandId /*node_id*/)
   // `MyInterface where .Member = i32`.
   auto [self_node, self_id] = context.node_stack().PopExprWithNodeId();
   auto self_type_id = ExprAsType(context, self_node, self_id);
-  // TODO: Do this once `WhereExpr` is ready to do something with this:
+  // TODO: Do this instead once `WhereExpr` is ready to consume this:
   // context.node_stack().Push(node_id, self_type_id);
   context.node_stack().Push(self_node, self_id);
 
   // Introduce a name scope so that we can remove the `.Self` entry we are
   // adding to name lookup at the end of the `where` expression.
-  // FIXME: is there a declaration that should be used as the InstId here?
-  auto scope_id =
-      context.name_scopes().Add(SemIR::InstId::Invalid, SemIR::NameId::Invalid,
-                                context.decl_name_stack().PeekParentScopeId());
-  // FIXME: specify any of the arguments instead of using defaults?
-  // Where would I get scope_inst_id or specific_id?
-  context.scope_stack().Push(/*scope_inst_id=*/SemIR::InstId::Invalid,
-                             scope_id);
+  context.scope_stack().Push();
   // Introduce `.Self` as a symbolic binding. Its type is the value of the
   // expression to the left of `where`, so `MyInterface` in the example above.
   // Because there is no equivalent non-symbolic value, we use `Invalid` as
   // the `value_id` on the `BindSymbolicName`.
   auto entity_name_id = context.entity_names().Add(
       {.name_id = SemIR::NameId::PeriodSelf,
-       .parent_scope_id = scope_id,
+       .parent_scope_id = context.decl_name_stack().PeekParentScopeId(),
        .bind_index = context.scope_stack().AddCompileTimeBinding()});
   // FIXME: should this have a location associated with `node_id`?
   auto inst_id =
@@ -44,8 +37,10 @@ auto HandleParseNode(Context& context, Parse::WhereOperandId /*node_id*/)
            .entity_name_id = entity_name_id,
            .value_id = SemIR::InstId::Invalid}));
   context.scope_stack().PushCompileTimeBinding(inst_id);
-  context.name_scopes().AddRequiredName(scope_id, SemIR::NameId::PeriodSelf,
-                                        inst_id);
+  auto existing =
+      context.scope_stack().LookupOrAddName(SemIR::NameId::PeriodSelf, inst_id);
+  // Shouldn't have any names in newly created scope.
+  CARBON_CHECK(!existing.is_valid());
   return true;
 }
 
