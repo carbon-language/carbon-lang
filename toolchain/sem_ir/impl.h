ec14cc44193b541c59f79514c11dab8973c2320f
Richard Smith <richard@metafoo.co.uk>
2024-09-27 02:16:22 +0000

    Use more compact storage for impl lookup buckets.
    If the bucket is of size zero or one, which is expected to be the common
    case, then store it directly. For the remaining cases, use a side table
    of lookup buckets.

--- toolchain/sem_ir/impl.h
+++ toolchain/sem_ir/impl.h
@@ -57,22 +57,110 @@ struct Impl : public EntityWithParamsBase,
 // A collection of `Impl`s, which can be accessed by the self type and
 // constraint implemented.
 class ImplStore {
- public:
-  // TODO: Switch to something like TinyPtrVector. We expect it to be rare for
-  // there to be more than one ImplId per bucket.
-  using LookupBucket = llvm::SmallVector<ImplId, 1>;
+ private:
+  // An ID of either a single impl or a lookup bucket.
+  class ImplOrLookupBucketId : public IdBase {
+   public:
+    using IdBase::IdBase;
 
-  // Returns the lookup bucket containing the list of impls with this self type
-  // and constraint, or adds a new bucket if this is the first time we've seen
-  // an impl of this kind. The lookup bucket should only include impls from the
-  // current file and its API file; impls from other files should not be added
-  // to it.
+    // An explicitly invalid ID, corresponding to to ImplId::Invalid.
+    static const ImplOrLookupBucketId Invalid;
+
+    static auto ForImplId(ImplId impl_id) -> ImplOrLookupBucketId {
+      return ImplOrLookupBucketId(impl_id.index);
+    }
+
+    static auto ForBucket(int bucket) -> ImplOrLookupBucketId {
+      return ImplOrLookupBucketId(ImplId::InvalidIndex - bucket - 1);
+    }
+
+    // Returns whether this ID represents a bucket index, rather than an ImplId.
+    // An invalid ID is not a bucket index.
+    auto is_bucket() const { return index < ImplId::InvalidIndex; }
+
+    // Returns the bucket index represented by this ID. Requires is_bucket().
+    auto bucket() const -> int {
+      CARBON_CHECK(is_bucket());
+      return ImplId::InvalidIndex - index - 1;
+    }
+
+    // Returns the ImplId index represented by this ID. Requires !is_bucket().
+    auto impl_id() const -> ImplId {
+      CARBON_CHECK(!is_bucket());
+      return ImplId(index);
+    }
+  };
+
+ public:
+  // A reference to an impl lookup bucket. This represents a list of impls with
+  // the same self and constraint type.
+  //
+  // The bucket is held indirectly as an `ImplOrLookupBucketId`, in one of three
+  // states:
+  //
+  //   - An invalid `ImplId` represents an empty bucket.
+  //   - A valid `ImplId` represents a bucket with exactly one impl. This is
+  //     expected to be by far the most common case.
+  //   - A lookup bucket index represents an index within the `ImplStore`'s
+  //     array of variable-sized lookup buckets.
+  class LookupBucketRef {
+   public:
+    LookupBucketRef(ImplStore& store, ImplOrLookupBucketId& id)
+        : store_(&store), id_(&id), single_id_storage_(ImplId::Invalid) {
+      if (!id.is_bucket()) {
+        single_id_storage_ = id.impl_id();
+      }
+    }
+
+    auto begin() const -> const ImplId* {
+      if (id_->is_bucket()) {
+        return store_->lookup_buckets_[id_->bucket()].begin();
+      }
+      return &single_id_storage_;
+    }
+
+    auto end() const -> const ImplId* {
+      if (id_->is_bucket()) {
+        return store_->lookup_buckets_[id_->bucket()].end();
+      }
+      return &single_id_storage_ + (id_->is_valid() ? 1 : 0);
+    }
+
+    // Adds an impl to this lookup bucket. Only impls from the current file and
+    // its API file should be added in this way. Impls from other files do not
+    // need to be found by impl redeclaration lookup so should not be added.
+    auto push_back(ImplId impl_id) -> void {
+      if (!id_->is_valid()) {
+        *id_ = ImplOrLookupBucketId::ForImplId(impl_id);
+        single_id_storage_ = impl_id;
+      } else if (!id_->is_bucket()) {
+        auto first_id = id_->impl_id();
+        *id_ = ImplOrLookupBucketId::ForBucket(store_->lookup_buckets_.size());
+        store_->lookup_buckets_.push_back({first_id, impl_id});
+      } else {
+        store_->lookup_buckets_[id_->bucket()].push_back(impl_id);
+      }
+    }
+
+   private:
+    ImplStore* store_;
+    ImplOrLookupBucketId* id_;
+    // Storage for a single ImplId. Used to support iteration over the contents
+    // of the bucket when it contains a single ImplId.
+    ImplId single_id_storage_;
+  };
+
+  // Returns a reference to the lookup bucket containing the list of impls with
+  // this self type and constraint, or adds a new bucket if this is the first
+  // time we've seen an impl of this kind. The lookup bucket reference remains
+  // valid until this function is called again.
   auto GetOrAddLookupBucket(TypeId self_id, TypeId constraint_id)
-      -> LookupBucket& {
-    return lookup_
-        .Insert(std::pair{self_id, constraint_id},
-                [] { return LookupBucket(); })
-        .value();
+      -> LookupBucketRef {
+    return LookupBucketRef(
+        *this, lookup_
+                   .Insert(std::pair{self_id, constraint_id},
+                           [] { return ImplOrLookupBucketId::Invalid; })
+                   .value());
   }
 
   // Adds the specified impl to the store. Does not add it to impl lookup.
@@ -100,8 +188,12 @@ class ImplStore {
 
  private:
   ValueStore<ImplId> values_;
-  Map<std::pair<TypeId, TypeId>, LookupBucket> lookup_;
+  Map<std::pair<TypeId, TypeId>, ImplOrLookupBucketId> lookup_;
+  llvm::SmallVector<llvm::SmallVector<ImplId, 2>> lookup_buckets_;
 };
+
+constexpr inline ImplStore::ImplOrLookupBucketId
+    ImplStore::ImplOrLookupBucketId::Invalid(InvalidIndex);
 
 }  // namespace Carbon::SemIR
 
