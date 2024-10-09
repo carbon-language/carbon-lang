f80eee7f3446c96c9a5897c4e7f336a5b70d7886
jonmeow <jperkins@google.com>
2024-10-09 15:27:33 -0700

    Move TODOs

--- toolchain/format/formatter.h
+++ toolchain/format/formatter.h
@@ -14,21 +14,21 @@ namespace Carbon::Format {
 
 // Implements Format(); see format.h. It's intended to be constructed and
 // `Run()` once, then destructed.
+//
+// TODO: This will probably need to work less linearly in the future, for
+// example to handle smart wrapping of arguments. This is a simple
+// implementation that only handles simple code. Before adding too much more
+// complexity, it should be rewritten.
+//
+// TODO: Add retention of blank lines between original code.
+//
+// TODO: Add support for formatting line ranges (will need flags too).
 class Formatter {
  public:
   explicit Formatter(const Lex::TokenizedBuffer* tokens, llvm::raw_ostream* out)
       : tokens_(tokens), out_(out) {}
 
   // See class comments.
-  //
-  // TODO: This will probably need to work less linearly in the future, for
-  // example to handle smart wrapping of arguments. This is a simple
-  // implementation that only handles simple code. Before adding too much more
-  // complexity, it should be rewritten.
-  //
-  // TODO: Add retention of blank lines between original code.
-  //
-  // TODO: Add support for formatting line ranges (will need flags too).
   auto Run() -> bool;
 
  private:
