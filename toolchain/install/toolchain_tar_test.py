6eac63d63881d9e5559f6a5925f70a71e8268c23
jonmeow <jperkins@google.com>
2024-10-30 14:14:38 -0700

    Switch tar verification to a manifest comparison.

--- toolchain/install/toolchain_tar_test.py
+++ toolchain/install/toolchain_tar_test.py
@@ -9,9 +9,19 @@ SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 """
 
 import argparse
-import sys
 from pathlib import Path
+import re
 import tarfile
+
+
+def expect_empty_set(filename: str, file_set: set[str]) -> bool:
+    """Prints and returns false when the set has entries."""
+    if file_set:
+        print(f"error: files only in `{filename}`:")
+        for f in file_set:
+            print(f"  - ${f}")
+        return False
+    return True
 
 
 def main() -> None:
@@ -22,42 +32,41 @@ def main() -> None:
         help="The tar file to test.",
     )
     parser.add_argument(
-        "install_marker",
+        "install_data_manifest",
         type=Path,
-        help="The path of the install marker in a prefix root to test against.",
+        help="The install data manifest file.",
     )
     args = parser.parse_args()
 
-    # Locate the prefix root from the install marker.
-    if not args.install_marker.exists():
-        sys.exit("ERROR: No install marker: " + args.install_marker)
-    prefix_root_path = args.install_marker.parents[2]
+    with open(args.install_data_manifest) as manifest:
+        # Remove everything up to and including `prefix_root`.
+        install_files = set(
+            [
+                re.sub("^.*/prefix_root/", "", entry.strip())
+                for entry in manifest.readlines()
+            ]
+        )
+    assert len(install_files), f"`{args.install_data_manifest}` is empty."
 
     # First check that every file and directory in the tar file exists in our
     # prefix root, and build a set of those paths.
-    installed_paths = set()
     with tarfile.open(args.tar_file) as tar:
-        for tarinfo in tar:
-            relative_path = Path(*Path(tarinfo.name).parts[1:])
-            installed_paths.add(relative_path)
-            if not prefix_root_path.joinpath(relative_path).exists():
-                sys.exit(
-                    "ERROR: File `{0}` is not in prefix root: `{1}`".format(
-                        tarinfo.name, prefix_root_path
-                    )
-                )
+        # Remove the first path component.
+        tar_files = set(
+            [
+                str(Path(*Path(tarinfo.name).parts[1:]))
+                for tarinfo in tar
+                if not tarinfo.isdir()
+            ]
+        )
+    assert len(tar_files), f"`{args.tar_file}` is empty."
 
-    # If we found an empty tar file, it's always an error.
-    if len(installed_paths) == 0:
-        sys.exit("ERROR: Tar file `{0}` was empty.".format(args.tar_file))
-
-    # Now check that every file and directory in the prefix root is in that set.
-    for prefix_path in prefix_root_path.glob("**/*"):
-        relative_path = prefix_path.relative_to(prefix_root_path)
-        if relative_path not in installed_paths:
-            sys.exit(
-                "ERROR: File `{0}` is not in tar file.".format(relative_path)
-            )
+    tar_okay = expect_empty_set(args.tar_file, tar_files - install_files)
+    install_okay = expect_empty_set(
+        args.install_data_manifest, install_files - tar_files
+    )
+    if not (tar_okay and install_okay):
+        exit("error: tar and install data did not match.")
 
 
 if __name__ == "__main__":
