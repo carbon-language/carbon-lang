d1382be01f698c918c0923a0baf04a613cbe260f
Josh L <josh11b@users.noreply.github.com>
2024-11-06 01:24:47 +0000

    Checkpoint progress.

--- toolchain/sem_ir/stringify_type.cpp
+++ toolchain/sem_ir/stringify_type.cpp
@@ -137,7 +137,7 @@ auto StringifyTypeExpr(const SemIR::File& outer_sem_ir, InstId outer_inst_id)
         }
         // TODO: also output other restrictions from facet_type_info.
         if (step.index + 1 >= static_cast<int>(facet_type_info.impls.size()) &&
-            !facet_type_info.requirement_block_id.invalid()) {
+            facet_type_info.requirement_block_id.is_valid()) {
           out << " where...";
         }
         break;
