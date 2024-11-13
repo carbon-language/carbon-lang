da3101f99cb4b6d6cf70f3f1abf3340842af2ca8
jonmeow <jperkins@google.com>
2024-11-13 14:00:37 -0800

    Fix up busybox detection

--- toolchain/install/busybox_main.cpp
+++ toolchain/install/busybox_main.cpp
@@ -5,7 +5,6 @@
 #include <unistd.h>
 
 #include <cstdlib>
-#include <filesystem>
 
 #include "common/bazel_working_dir.h"
 #include "common/error.h"
@@ -15,58 +14,29 @@
 #include "llvm/ADT/StringRef.h"
 #include "llvm/Support/LLVMDriver.h"
 #include "toolchain/driver/driver.h"
+#include "toolchain/install/busybox_info.h"
 #include "toolchain/install/install_paths.h"
 
 namespace Carbon {
 
-namespace {
-struct BusyboxInfo {
-  // The path to `carbon-busybox`.
-  std::filesystem::path bin_path;
-  // The mode, such as `carbon` or `clang`.
-  std::optional<std::string> mode;
-};
-}  // namespace
-
-// Returns the busybox information, given argv[0]. This primarily handles
-// resolving symlinks that point at the busybox.
-static auto GetBusyboxInfo(llvm::StringRef argv0) -> ErrorOr<BusyboxInfo> {
-  BusyboxInfo info = BusyboxInfo{argv0.str(), std::nullopt};
-  while (true) {
-    std::string filename = info.bin_path.filename();
-    if (filename == "carbon-busybox") {
-      return info;
-    }
-    std::error_code ec;
-    auto symlink_target = std::filesystem::read_symlink(info.bin_path, ec);
-    if (ec) {
-      return ErrorBuilder()
-             << "expected carbon-busybox symlink at `" << info.bin_path << "`";
-    }
-    info.mode = filename;
-    info.bin_path = symlink_target;
-  }
-}
-
 // The actual `main` implementation. Can return an exit code or an `Error`
 // (which causes EXIT_FAILRUE).
 static auto Main(int argc, char** argv) -> ErrorOr<int> {
-  Carbon::InitLLVM init_llvm(argc, argv);
+  InitLLVM init_llvm(argc, argv);
 
   // Start by resolving any symlinks.
-  CARBON_ASSIGN_OR_RETURN(auto busybox_info, Carbon::GetBusyboxInfo(argv[0]));
+  CARBON_ASSIGN_OR_RETURN(auto busybox_info, GetBusyboxInfo(argv[0]));
 
   auto fs = llvm::vfs::getRealFileSystem();
 
   // Resolve paths before calling SetWorkingDirForBazel.
-  std::string exe_path =
-      Carbon::FindExecutablePath(busybox_info.bin_path.string());
-  const auto install_paths = Carbon::InstallPaths::MakeExeRelative(exe_path);
+  std::string exe_path = FindExecutablePath(busybox_info.bin_path.string());
+  const auto install_paths = InstallPaths::MakeExeRelative(exe_path);
   if (install_paths.error()) {
     return Error(*install_paths.error());
   }
 
-  Carbon::SetWorkingDirForBazel();
+  SetWorkingDirForBazel();
 
   llvm::SmallVector<llvm::StringRef> args;
   args.reserve(argc + 1);
@@ -75,7 +45,7 @@ static auto Main(int argc, char** argv) -> ErrorOr<int> {
   }
   args.append(argv + 1, argv + argc);
 
-  Carbon::Driver driver(*fs, &install_paths, llvm::outs(), llvm::errs());
+  Driver driver(*fs, &install_paths, llvm::outs(), llvm::errs());
   bool success = driver.RunCommand(args).success;
   return success ? EXIT_SUCCESS : EXIT_FAILURE;
 }
