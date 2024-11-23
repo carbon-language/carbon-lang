43d96a06d6ddd8df5876fefe8237a2f1be6a32df
David Blaikie <dblaikie@gmail.com>
2024-11-23 00:28:08 +0000

    Error if a function is marked `impl` but there is no base class.

--- toolchain/check/handle_function.cpp
+++ toolchain/check/handle_function.cpp
@@ -212,8 +212,11 @@ static auto BuildFunctionDecl(Context& context,
       parent_scope_inst) {
     if (auto class_decl = parent_scope_inst->TryAs<SemIR::ClassDecl>()) {
       auto& class_info = context.classes().Get(class_decl->class_id);
-      CARBON_CHECK(virtual_modifier != SemIR::Function::VirtualModifier::Impl ||
-                   class_info.is_dynamic);
+      if (virtual_modifier == SemIR::Function::VirtualModifier::Impl &&
+          !class_info.base_id.is_valid()) {
+        CARBON_DIAGNOSTIC(ImplWithoutBase, Error, "impl without base class");
+        context.emitter().Build(node_id, ImplWithoutBase).Emit();
+      }
       class_info.is_dynamic = true;
     }
   }
