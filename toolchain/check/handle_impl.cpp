4ede8b7f228422314e6b4a697816518babfd3325
Richard Smith <richard@metafoo.co.uk>
2024-12-11 01:54:58 +0000

    Pacify CHECK failure on invalid code.
    Found by fuzzer.

--- toolchain/check/handle_impl.cpp
+++ toolchain/check/handle_impl.cpp
@@ -202,12 +202,18 @@ static auto PopImplIntroducerAndParamsAsNameComponent(
       context.node_stack().PopWithNodeIdIf<Parse::NodeKind::ImplForall>();
 
   if (implicit_param_patterns_id) {
-    // Emit the `forall` match. This shouldn't produce any `Call` params,
+    // Emit the `forall` match. This shouldn't produce any valid `Call` params,
     // because `impl`s are never actually called at runtime.
     auto call_params_id =
         CalleePatternMatch(context, *implicit_param_patterns_id,
                            SemIR::InstBlockId::Invalid, SemIR::InstId::Invalid);
-    CARBON_CHECK(call_params_id == SemIR::InstBlockId::Empty);
+    CARBON_CHECK(call_params_id == SemIR::InstBlockId::Empty ||
+                 context.inst_blocks()
+                     .Get(call_params_id)
+                     .drop_while([](SemIR::InstId inst_id) {
+                       return inst_id == SemIR::ErrorInst::SingletonInstId;
+                     })
+                     .empty());
   }
 
   Parse::NodeId first_param_node_id =
