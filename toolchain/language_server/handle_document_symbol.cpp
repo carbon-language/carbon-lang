8b1f3161aeb6da917c4b962eb5efe9bfcfea7e01
jonmeow <jperkins@google.com>
2024-12-19 16:03:34 -0800

    Restore handle_*

--- toolchain/language_server/handle_document_symbol.cpp
+++ toolchain/language_server/handle_document_symbol.cpp
@@ -2,10 +2,10 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "toolchain/language_server/server.h"
-
 #include "toolchain/base/shared_value_stores.h"
 #include "toolchain/diagnostics/null_diagnostics.h"
+#include "toolchain/language_server/context.h"
+#include "toolchain/language_server/handler_registry.h"
 #include "toolchain/lex/lex.h"
 #include "toolchain/parse/node_kind.h"
 #include "toolchain/parse/parse.h"
@@ -13,92 +13,6 @@
 #include "toolchain/source/source_buffer.h"
 
 namespace Carbon::LanguageServer {
-
-Server::Server(std::FILE* input_stream, llvm::raw_ostream& output_stream)
-    : transport_(clang::clangd::newJSONTransport(input_stream, output_stream,
-                                                 /*InMirror=*/nullptr,
-                                                 /*Pretty=*/true)),
-      binder_(handlers_, *this) {
-  binder_.notification("textDocument/didOpen", this,
-                       &Server::OnDidOpenTextDocument);
-  binder_.notification("textDocument/didChange", this,
-                       &Server::OnDidChangeTextDocument);
-  binder_.method("initialize", this, &Server::OnInitialize);
-  binder_.method("textDocument/documentSymbol", this,
-                 &Server::OnDocumentSymbol);
-}
-
-auto Server::Run() -> ErrorOr<Success> {
-  llvm::Error err = transport_->loop(*this);
-  if (err) {
-    std::string str;
-    llvm::raw_string_ostream out(str);
-    out << err;
-    return Error(str);
-  } else {
-    return Success();
-  }
-}
-
-void Server::OnDidOpenTextDocument(
-    clang::clangd::DidOpenTextDocumentParams const& params) {
-  files_.emplace(params.textDocument.uri.file(), params.textDocument.text);
-}
-
-void Server::OnDidChangeTextDocument(
-    clang::clangd::DidChangeTextDocumentParams const& params) {
-  // Full text is sent if full sync is specified in capabilities.
-  CARBON_CHECK(params.contentChanges.size() == 1);
-  std::string file = params.textDocument.uri.file().str();
-  files_[file] = params.contentChanges[0].text;
-}
-
-void Server::OnInitialize(
-    clang::clangd::NoParams const& /*client_capabilities*/,
-    clang::clangd::Callback<llvm::json::Object> cb) {
-  llvm::json::Object capabilities{{"documentSymbolProvider", true},
-                                  {"textDocumentSync", /*Full=*/1}};
-
-  llvm::json::Object reply{{"capabilities", std::move(capabilities)}};
-  cb(reply);
-}
-
-auto Server::onNotify(llvm::StringRef method, llvm::json::Value value) -> bool {
-  if (method == "exit") {
-    return false;
-  }
-  if (auto handler = handlers_.NotificationHandlers.find(method);
-      handler != handlers_.NotificationHandlers.end()) {
-    handler->second(std::move(value));
-  } else {
-    clang::clangd::log("unhandled notification {0}", method);
-  }
-
-  return true;
-}
-
-auto Server::onCall(llvm::StringRef method, llvm::json::Value params,
-                    llvm::json::Value id) -> bool {
-  if (auto handler = handlers_.MethodHandlers.find(method);
-      handler != handlers_.MethodHandlers.end()) {
-    // TODO: Improve this if add threads.
-    handler->second(std::move(params),
-                    [&](llvm::Expected<llvm::json::Value> reply) {
-                      transport_->reply(id, std::move(reply));
-                    });
-  } else {
-    transport_->reply(
-        id, llvm::make_error<clang::clangd::LSPError>(
-                "method not found", clang::clangd::ErrorCode::MethodNotFound));
-  }
-
-  return true;
-}
-
-auto Server::onReply(llvm::json::Value /*id*/,
-                     llvm::Expected<llvm::json::Value> /*result*/) -> bool {
-  return true;
-}
 
 // Returns the text of first child of kind Parse::NodeKind::IdentifierName.
 static auto GetIdentifierName(const SharedValueStores& value_stores,
@@ -117,16 +31,21 @@ static auto GetIdentifierName(const SharedValueStores& value_stores,
   return std::nullopt;
 }
 
-void Server::OnDocumentSymbol(
-    clang::clangd::DocumentSymbolParams const& params,
-    clang::clangd::Callback<std::vector<clang::clangd::DocumentSymbol>> cb) {
+// Provides information about document symbols.
+static auto HandleDocumentSymbol(
+    Context& context, const clang::clangd::DocumentSymbolParams& params,
+    llvm::function_ref<
+        void(llvm::Expected<std::vector<clang::clangd::DocumentSymbol>>)>
+        on_done) -> void {
   SharedValueStores value_stores;
   llvm::vfs::InMemoryFileSystem vfs;
-  auto file = params.textDocument.uri.file().str();
-  vfs.addFile(file, /*mtime=*/0,
-              llvm::MemoryBuffer::getMemBufferCopy(files_.at(file)));
+  auto lookup = context.files().Lookup(params.textDocument.uri.file());
+  CARBON_CHECK(lookup);
+  vfs.addFile(lookup.key(), /*mtime=*/0,
+              llvm::MemoryBuffer::getMemBufferCopy(lookup.value()));
 
-  auto buf = SourceBuffer::MakeFromFile(vfs, file, NullDiagnosticConsumer());
+  auto buf =
+      SourceBuffer::MakeFromFile(vfs, lookup.key(), NullDiagnosticConsumer());
   auto lexed = Lex::Lex(value_stores, *buf, NullDiagnosticConsumer());
   auto parsed = Parse::Parse(lexed, NullDiagnosticConsumer(), nullptr);
   Parse::TreeAndSubtrees tree_and_subtrees(lexed, parsed);
@@ -168,7 +87,10 @@ void Server::OnDocumentSymbol(
       result.push_back(symbol);
     }
   }
-  cb(result);
+  on_done(result);
 }
+
+static RegisterCallHandler<HandleDocumentSymbol> register_call(
+    "textDocument/didChange", "");
 
 }  // namespace Carbon::LanguageServer
