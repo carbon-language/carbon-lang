d50ed05ca142acfdd8d65178da4a9917cc579d1c
danakj <danakj@orodu.net>
2025-01-06 14:24:30 -0500

    Add function_id comment

--- toolchain/sem_ir/typed_insts.h
+++ toolchain/sem_ir/typed_insts.h
@@ -397,6 +397,9 @@ struct BoundMethod {
   // `self`, or whose address will be used to initialize `self` for an `addr
   // self` parameter.
   InstId object_id;
+  // The instruction from which to find the function being bound to an object.
+  // The actual function (e.g. FunctionType) comes from the this instruction's
+  // type id.
   InstId function_id;
 };
 
