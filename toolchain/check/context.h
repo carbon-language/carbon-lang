a3f1c8d2b3699f39f910f59c39232353ff95d452
danakj <danakj@orodu.net>
2025-02-03 12:42:15 -0500

    Improve comments on conversion from type value to type id
    The GetTypeIdForTypeConstant() and GetTypeIdForTypeInst() methods
    convert from a type value to the type id of that value. Expand the
    comments to explain why they can not be called with facet values, which
    would be of type `FacetType`.
    
    Change the CHECK inside to disallow being called with facet values as
    well. No tests cause this to occur anymore.

--- toolchain/check/context.h
+++ toolchain/check/context.h
@@ -365,11 +365,12 @@ class Context {
   // Returns whether the current position in the current block is reachable.
   auto is_current_position_reachable() -> bool;
 
-  // Returns the type ID for a constant of type `type`.
+  // Returns the type ID for a constant is a type value, i.e. it is a value of
+  // type `TypeType` or `FacetType`.
   auto GetTypeIdForTypeConstant(SemIR::ConstantId constant_id) -> SemIR::TypeId;
 
-  // Returns the type ID for an instruction whose constant value is of type
-  // `type`.
+  // Returns the type ID for an instruction whose constant value is a type
+  // value, i.e. it is a value of type `TypeType` or `FacetType`.
   auto GetTypeIdForTypeInst(SemIR::InstId inst_id) -> SemIR::TypeId {
     return GetTypeIdForTypeConstant(constant_values().Get(inst_id));
   }
