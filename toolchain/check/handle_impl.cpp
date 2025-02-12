af74a2f89b35d123519a9906ff54d29763a43e0b
Geoff Romer <gromer@google.com>
2025-02-12 13:57:26 -0800

    Apply suggestions from code review
    Co-authored-by: Jon Ross-Perkins <jperkins@google.com>

--- toolchain/check/handle_impl.cpp
+++ toolchain/check/handle_impl.cpp
@@ -41,6 +41,7 @@ auto HandleParseNode(Context& context, Parse::ImplIntroducerId node_id)
 }
 
 auto HandleParseNode(Context& context, Parse::ForallId /*node_id*/) -> bool {
+  // Push a pattern block for the signature of the `forall`.
   context.pattern_block_stack().Push();
   context.full_pattern_stack().PushFullPattern(
       FullPatternStack::Kind::ImplicitParamList);
