67057ee31e58f4f38ddbcbb5725999a5094f5339
Alina Sbirlea <asbirlea@google.com>
2025-02-27 21:05:31 +0000

    Add nolint for for loop WAI.

--- toolchain/lower/file_context.cpp
+++ toolchain/lower/file_context.cpp
@@ -88,8 +88,9 @@ auto FileContext::Run() -> std::unique_ptr<llvm::Module> {
   }
 
   // Lower function definitions for generics.
-  // TODO: this cannot be a range-based loop, as new definitions can be added
-  // while building.
+  // This cannot be a range-based loop, as new definitions can be added
+  // while building other definitions.
+  // NOLINTNEXTLINE
   for (size_t i = 0; i != specific_function_definitions_.size(); ++i) {
     auto [function_id, specific_id] = specific_function_definitions_[i];
     BuildFunctionDefinition(function_id, specific_id);
