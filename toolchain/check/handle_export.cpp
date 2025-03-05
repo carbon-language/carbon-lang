a9ee6609999c33e8dfdead76f851a93a37691908
jonmeow <jperkins@google.com>
2025-03-05 12:57:49 -0800

    Fix crash when exporting a poisoned name

--- toolchain/check/handle_export.cpp
+++ toolchain/check/handle_export.cpp
@@ -37,7 +37,10 @@ auto HandleParseNode(Context& context, Parse::ExportDeclId node_id) -> bool {
     return true;
   }
 
-  auto inst_id = name_context.prev_inst_id();
+  auto inst_id =
+      name_context.state == DeclNameStack::NameContext::State::Poisoned
+          ? SemIR::InstId::None
+          : name_context.prev_inst_id();
   if (!inst_id.has_value()) {
     DiagnoseNameNotFound(context, node_id, name_context.name_id_for_new_inst());
     return true;
