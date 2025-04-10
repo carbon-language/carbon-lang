128fbc86d0d41b6f436774937a734805070e4780
danakj <danakj@orodu.net>
2025-04-10 17:37:45 -0400

    CheckConstantTypeIsTypeType

--- toolchain/sem_ir/type.cpp
+++ toolchain/sem_ir/type.cpp
@@ -9,8 +9,7 @@
 namespace Carbon::SemIR {
 
 // Verify that the constant value's type is `TypeType` (or an error).
-static void CheckConstantValueHasTypeOfTypeType(File& file,
-                                                ConstantId constant_id) {
+static void CheckConstantTypeIsTypeType(File& file, ConstantId constant_id) {
   CARBON_CHECK(constant_id.is_constant(),
                "Canonicalizing non-constant type: {0}", constant_id);
   auto type_id =
@@ -23,13 +22,13 @@ static void CheckConstantValueHasTypeOfTypeType(File& file,
 
 auto TypeStore::GetTypeIdForTypeConstantId(ConstantId constant_id) const
     -> TypeId {
-  CheckConstantValueHasTypeOfTypeType(*file_, constant_id);
+  CheckConstantTypeIsTypeType(*file_, constant_id);
   return TypeId::ForTypeConstant(constant_id);
 }
 
 auto TypeStore::GetTypeIdForTypeInstId(InstId inst_id) const -> TypeId {
   auto constant_id = file_->constant_values().Get(inst_id);
-  CheckConstantValueHasTypeOfTypeType(*file_, constant_id);
+  CheckConstantTypeIsTypeType(*file_, constant_id);
   return TypeId::ForTypeConstant(constant_id);
 }
 
@@ -40,7 +39,7 @@ auto TypeStore::GetTypeIdForTypeInstId(TypeInstId inst_id) const -> TypeId {
 
 auto TypeStore::GetAsTypeInstId(InstId inst_id) const -> TypeInstId {
   auto constant_id = file_->constant_values().Get(inst_id);
-  CheckConstantValueHasTypeOfTypeType(*file_, constant_id);
+  CheckConstantTypeIsTypeType(*file_, constant_id);
   return TypeInstId::UnsafeMake(inst_id);
 }
 
