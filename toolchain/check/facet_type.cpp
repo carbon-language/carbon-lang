0b346d4357809aa9647c0960fa474bee97716fb6
danakj <danakj@orodu.net>
2025-04-10 18:04:44 -0400

    wip-the-table-is-missing

--- toolchain/check/facet_type.cpp
+++ toolchain/check/facet_type.cpp
@@ -78,9 +78,7 @@ auto InitialFacetTypeImplWitness(
   if (!is_definition && facet_type_info.rewrite_constraints.empty()) {
     auto witness_table_inst_id = AddInst<SemIR::ImplWitnessTable>(
         context, witness_loc_id,
-        {.type_id =
-             GetSingletonType(context, SemIR::WitnessType::SingletonInstId),
-         .elements_id = context.inst_blocks().AddPlaceholder(),
+        {.elements_id = context.inst_blocks().AddPlaceholder(),
          .impl_id = SemIR::ImplId::None});
     return AddInst<SemIR::ImplWitness>(
         context, witness_loc_id,
@@ -122,10 +120,7 @@ auto InitialFacetTypeImplWitness(
 
     auto witness_table_inst_id = AddInst<SemIR::ImplWitnessTable>(
         context, witness_loc_id,
-        {.type_id =
-             GetSingletonType(context, SemIR::WitnessType::SingletonInstId),
-         .elements_id = elements_id,
-         .impl_id = SemIR::ImplId::None});
+        {.elements_id = elements_id, .impl_id = SemIR::ImplId::None});
 
     witness_inst_id = AddInst<SemIR::ImplWitness>(
         context, witness_loc_id,
