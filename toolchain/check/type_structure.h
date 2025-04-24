bb06fac94ee5182058fb259a86cd72f6a589dc58
Boaz Brickner <brickner@google.com>
2025-04-24 16:58:46 +0200

    Fix clang-tidy: use a ranges version of this algorithm [modernize-use-ranges,-warnings-as-errors]

--- toolchain/check/type_structure.h
+++ toolchain/check/type_structure.h
@@ -35,9 +35,8 @@ class TypeStructure : public Printable<TypeStructure> {
   // runners).
   friend auto operator<(const TypeStructure& lhs, const TypeStructure& rhs)
       -> bool {
-    return std::lexicographical_compare(
-        lhs.symbolic_type_indices_.begin(), lhs.symbolic_type_indices_.end(),
-        rhs.symbolic_type_indices_.begin(), rhs.symbolic_type_indices_.end(),
+    return std::ranges::lexicographical_compare(
+        lhs.symbolic_type_indices_, rhs.symbolic_type_indices_,
         [](int lhs_index, int rhs_index) {
           // A higher symbolic type index is a better match, so we need to
           // reverse the order.
