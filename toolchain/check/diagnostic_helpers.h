a850db0671e756355ea67f0ccf0183b891252efd
Dana Jansens <danakj@orodu.net>
2025-04-28 12:00:57 -0400

    rewording
    Co-authored-by: Jon Ross-Perkins <jperkins@google.com>

--- toolchain/check/diagnostic_helpers.h
+++ toolchain/check/diagnostic_helpers.h
@@ -13,7 +13,7 @@
 
 namespace Carbon::Check {
 
-// The `Diagnostics::Emitter` for `check/` is templated on this type so that
+// The `DiagnosticEmitterBase` is templated on this type so that
 // diagnostics can be passed an `InstId` as a location, without having to
 // explicitly construct a `LocId` from it first.
 class LocIdForDiagnostics {
