0d90a821e8d7a04db886d672748a9200979d6f6b
jonmeow <jperkins@google.com>
2025-04-28 13:10:45 -0700

    Still nolint

--- toolchain/sem_ir/formatter.cpp
+++ toolchain/sem_ir/formatter.cpp
@@ -24,6 +24,11 @@
 #include "toolchain/sem_ir/name_scope.h"
 #include "toolchain/sem_ir/typed_insts.h"
 
+// TODO: Consider addressing recursion here, although it's not critical because
+// the formatter isn't required to work on arbitrary code. Still, it may help
+// in the future to debug complex code.
+// NOLINTBEGIN(misc-no-recursion)
+
 namespace Carbon::SemIR {
 
 Formatter::Formatter(const File* sem_ir,
@@ -1285,3 +1290,5 @@ auto Formatter::GetImportIRLabel(ImportIRId id) -> std::string {
 }
 
 }  // namespace Carbon::SemIR
+
+// NOLINTEND(misc-no-recursion)
