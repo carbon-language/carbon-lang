72045c0773f4f79d9ae7bbbe2cf3e78c38aa51a8
Ivana Ivanovska <iivanovska@google.com>
2025-05-05 13:35:57 +0000

    Apply review comments

--- toolchain/check/import_cpp.cpp
+++ toolchain/check/import_cpp.cpp
@@ -310,8 +310,8 @@ static auto MakeIntType(Context& context, IntId size_id) -> TypeExpr {
   return ExprAsType(context, Parse::NodeId::None, type_inst_id);
 }
 
-// Maps a C++ type to a Carbon type. Currently only 32-bit `int` and `short` are
-// supported.
+// Maps a C++ type to a Carbon type. Currently only 32-bit `int` and 16-bit
+// `short` are supported.
 // TODO: Support more types.
 static auto MapType(Context& context, clang::QualType type) -> TypeExpr {
   const auto* builtin_type = dyn_cast<clang::BuiltinType>(type);
@@ -321,16 +321,20 @@ static auto MapType(Context& context, clang::QualType type) -> TypeExpr {
   }
   switch (builtin_type->getKind()) {
     case clang::BuiltinType::Short:
-      return MakeIntType(context, context.ints().Add(16));
+      if (context.ast_context().getTypeSize(type) == 16) {
+        return MakeIntType(context, context.ints().Add(16));
+      }
+      break;
     case clang::BuiltinType::Int:
       if (context.ast_context().getTypeSize(type) == 32) {
         return MakeIntType(context, context.ints().Add(32));
       }
-      [[fallthrough]];
+      break;
     default:
-      return {.inst_id = SemIR::ErrorInst::TypeInstId,
-              .type_id = SemIR::ErrorInst::TypeId};
+      break;
   }
+  return {.inst_id = SemIR::ErrorInst::TypeInstId,
+          .type_id = SemIR::ErrorInst::TypeId};
 }
 
 // Returns a block id for the explicit parameters of the given function
