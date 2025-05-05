7397e892d4f4c50680230a3c4c6e296616b5d3e3
Dana Jansens <danakj@orodu.net>
2025-05-05 13:03:17 -0400

    comment
    Co-authored-by: Jon Ross-Perkins <jperkins@google.com>

--- toolchain/check/type_structure.h
+++ toolchain/check/type_structure.h
@@ -88,7 +88,9 @@ class TypeStructure : public Printable<TypeStructure> {
     // the nested and is paired with a ConcreteCloseParen at the end of the
     // nested types.
     ConcreteOpenParen,
+    //
     // Closes a ConcreteOpenParen for a concrete type with nested types.
+    // Does not have its own concrete type.
     ConcreteCloseParen,
     // A symbolic element in the type structure. When matching type structures,
     // it represents a wildcard that matches against either a single `Concrete`
