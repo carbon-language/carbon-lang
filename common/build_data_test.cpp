526f89175fd30ebe16078998d3dc97ab61074add
jonmeow <jperkins@google.com>
2025-05-08 16:42:22 -0700

    comments

--- common/build_data_test.cpp
+++ common/build_data_test.cpp
@@ -17,7 +17,11 @@ using ::testing::HasSubstr;
 TEST(BuildDataTest, Values) {
   EXPECT_FALSE(BuildData::Platform.empty());
   // Can't really test BuildCoverageEnabled.
+
+  // This doesn't require `//`, for a bit of incremental robustness.
   EXPECT_THAT(BuildData::TargetName, EndsWith("/common:build_data_test"));
+
+  // This doesn't examine the path, for platform robustness (e.g. `.exe`).
   EXPECT_THAT(BuildData::BuildTarget, HasSubstr("build_data_test"));
 }
 
