c581684a0b4b93fc0d3f00753326eb85df457224
danakj <danakj@orodu.net>
2025-05-15 10:13:51 -0400

    reform-comment

--- toolchain/check/impl_validation.cpp
+++ toolchain/check/impl_validation.cpp
@@ -247,8 +247,8 @@ static auto ValidateImplsForInterface(
                                   });
 
   // All `impl`s we look at here have the same `InterfaceId` (though different
-  // `specific_id`s in their `SpecificInterface`s). So we can grab the ImportIRId for
-  // the interface a single time up front.
+  // `SpecificId`s in their `SpecificInterface`s). So we can grab the
+  // `ImportIRId` for the interface a single time up front.
   auto interface_decl_id =
       context.interfaces()
           .Get(impls_and_interface[0].interface.interface_id)
