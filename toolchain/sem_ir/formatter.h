892ca3847702966f3c0c0e095b21257844ee2067
jonmeow <jperkins@google.com>
2025-05-19 15:28:48 -0700

    comment

--- toolchain/sem_ir/formatter.h
+++ toolchain/sem_ir/formatter.h
@@ -230,6 +230,8 @@ class Formatter {
   // no such arguments.
   auto FormatPendingConstantValue(AddSpace space_where) -> void;
 
+  // Formats `<name>[: <type>] = `. Skips unnamed instructions. Typed
+  // instructions must be named.
   auto FormatInstLhs(InstId inst_id, Inst inst) -> void;
 
   template <typename InstT>
