ee2c20bf3a281d0f2d8ceec68ba73e17ab4d661b
jonmeow <jperkins@google.com>
2025-05-22 09:12:57 -0700

    comment

--- toolchain/sem_ir/formatter.cpp
+++ toolchain/sem_ir/formatter.cpp
@@ -538,6 +538,7 @@ auto Formatter::FormatSpecificRegion(const Generic& generic,
 auto Formatter::FormatSpecific(SpecificId id) -> void {
   const auto& specific = sem_ir_->specifics().Get(id);
   const auto& generic = sem_ir_->generics().Get(specific.generic_id);
+  // We don't know that this is a definition start node; be conservative.
   if (!ShouldFormatEntity(generic.decl_id, /*is_definition_start=*/false)) {
     // Omit specifics if we also omitted the generic.
     return;
