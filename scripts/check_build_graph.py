41cb0ac5eb96fa49f1236fc3022fbc1e5e54f502
Geoffrey Romer <gromer@google.com>
2025-06-12 12:08:03 -0700

    Add pre-commit check for bad build graphs

---
+++ scripts/check_build_graph.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+"""Verify that the bazel build graph is in a valid state, for pre-commit."""
+
+__copyright__ = """
+Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+Exceptions. See /LICENSE for license information.
+SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+"""
+
+import subprocess
+import sys
+
+import scripts_utils
+
+
+def main() -> int:
+    scripts_utils.chdir_repo_root()
+    bazel = scripts_utils.locate_bazel()
+    sys.exit(subprocess.run([bazel, "build", "--nobuild", "//..."]).returncode)
+
+
+if __name__ == "__main__":
+    main()
