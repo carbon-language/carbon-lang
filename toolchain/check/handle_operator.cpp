73b9858d689e1e8cb2fd80e05d7770df84613bc7
David Blaikie <dblaikie@gmail.com>
2025-06-26 22:31:59 +0000

    Add TODO for partial-applied-to-non-base/abstract

--- toolchain/check/handle_operator.cpp
+++ toolchain/check/handle_operator.cpp
@@ -314,6 +314,7 @@ auto HandleParseNode(Context& context, Parse::PrefixOperatorPartialId node_id)
     -> bool {
   auto value_id = context.node_stack().PopExpr();
   auto inner_type = ExprAsType(context, node_id, value_id);
+  // TODO: Add diagnostics for partial applied to non-base/abstract types.
   AddInstAndPush<SemIR::PartialType>(
       context, node_id,
       {.type_id = SemIR::TypeType::TypeId, .inner_id = inner_type.inst_id});
