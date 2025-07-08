08acf0f6d7c47617cb0bff584444817d4f4eaae4
Richard Smith <richard@metafoo.co.uk>
2025-07-08 21:22:00 +0000

    Track the location of the Cpp import for use in Clang diagnostics.

--- toolchain/check/import_cpp.cpp
+++ toolchain/check/import_cpp.cpp
@@ -44,6 +44,16 @@ static auto GenerateCppIncludesHeaderCode(
   std::string code;
   llvm::raw_string_ostream code_stream(code);
   for (const Parse::Tree::PackagingNames& import : imports) {
+    // Add a line marker directive pointing at the location of the `import Cpp`
+    // declaration in the Carbon source file. This will cause Clang's
+    // diagnostics machinery to track and report the location in Carbon code
+    // where the import was written.
+    auto import_loc = context.tokens().TokenToDiagnosticLoc(
+        context.parse_tree().node_token(import.node_id));
+    code_stream << "# " << import_loc.loc.line_number << " \"";
+    code_stream.write_escaped(import_loc.loc.filename);
+    code_stream << "\"\n";
+
     code_stream << "#include \""
                 << FormatEscaped(
                        context.string_literal_values().Get(import.library_id))
@@ -108,6 +118,7 @@ class CarbonClangDiagnosticConsumer : public clang::DiagnosticConsumer {
     // TODO: Consider allowing setting `DiagnosticOptions` or use
     // `ASTUnit::getDiagnostics().getLangOptions().getDiagnosticOptions()`.
     clang::DiagnosticOptions diagnostic_options;
+    diagnostic_options.ShowPresumedLoc = true;
     clang::TextDiagnostic text_diagnostic(diagnostics_stream, lang_options,
                                           diagnostic_options);
     text_diagnostic.emitDiagnostic(
@@ -139,8 +150,6 @@ class CarbonClangDiagnosticConsumer : public clang::DiagnosticConsumer {
         case clang::DiagnosticsEngine::Warning:
         case clang::DiagnosticsEngine::Error:
         case clang::DiagnosticsEngine::Fatal: {
-          // TODO: Parse the message to select the relevant C++ import and add
-          // that information to the location.
           CARBON_DIAGNOSTIC(CppInteropParseWarning, Warning, "{0}",
                             std::string);
           CARBON_DIAGNOSTIC(CppInteropParseError, Error, "{0}", std::string);
