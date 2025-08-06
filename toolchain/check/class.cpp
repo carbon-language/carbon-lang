c9f1b39f23907de84683245859b3dfbe3129ecc5
jonmeow <jperkins@google.com>
2025-08-06 14:32:51 -0700

    Use GetStructType when building object repr structs

--- toolchain/check/class.cpp
+++ toolchain/check/class.cpp
@@ -314,16 +314,13 @@ static auto CheckCompleteClassType(
                                    .specific_id = self_specific_id});
   }
 
-  auto struct_type_inst_id = AddTypeInst<SemIR::StructType>(
-      context, node_id,
-      {.type_id = SemIR::TypeType::TypeId,
-       .fields_id =
-           AddStructTypeFields(context, struct_type_fields, field_decls)});
+  auto struct_type_id = GetStructType(
+      context, AddStructTypeFields(context, struct_type_fields, field_decls));
 
   return AddInst<SemIR::CompleteTypeWitness>(
       context, node_id,
       {.type_id = GetSingletonType(context, SemIR::WitnessType::TypeInstId),
-       .object_repr_type_inst_id = struct_type_inst_id});
+       .object_repr_type_inst_id = context.types().GetInstId(struct_type_id)});
 }
 
 auto ComputeClassObjectRepr(Context& context, Parse::ClassDefinitionId node_id,
