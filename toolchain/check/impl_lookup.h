0a08c9f6eaa9e57cdd59aeb207deae08e448257d
evoxtorm <evostorm96@gmail.com>
2025-09-06 15:50:57 +0530

    feat: suggested changes

--- toolchain/check/impl_lookup.h
+++ toolchain/check/impl_lookup.h
@@ -49,10 +49,9 @@ auto LookupMatchesImpl(Context& context, SemIR::LocId loc_id,
 
 // The result of EvalLookupSingleImplWitness(). It can be one of:
 // - No value. Lookup failed to find an impl declaration.
-// - A constant. Lookup found a concrete impl declaration that can be
+// - A concrete value. Lookup found a concrete impl declaration that can be
 //   used definitively.
-// - A symbolic value. Lookup found an impl but it is not returned since
-// lookup
+// - A symbolic value. Lookup found an impl but it is not returned since lookup
 //   will need to be done again with a more specific query to look for
 //   specializations.
 class [[nodiscard]] EvalImplLookupResult {
