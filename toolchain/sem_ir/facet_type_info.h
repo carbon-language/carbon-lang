4a02fdf4f523cc59d9b583a7d0135d96cecd185c
Jon Ross-Perkins <jperkins@google.com>
2025-09-15 08:56:30 -0700

    Update toolchain/sem_ir/facet_type_info.h
    Co-authored-by: Dana Jansens <danakj@orodu.net>

--- toolchain/sem_ir/facet_type_info.h
+++ toolchain/sem_ir/facet_type_info.h
@@ -25,7 +25,7 @@ CARBON_DEFINE_RAW_ENUM_MASK(BuiltinConstraintMask, uint32_t) {
 // Constraints that are produced by builtin functions.
 //
 // Note these constraints are not treated as full interfaces, and behave
-// somewhat similarly to `type` as an API. Similarly, `impl C as
+// somewhat similarly to `type where .Self impls <builtin>` as an API. Similarly, `impl C as
 // <BuiltinConstraint>` will be invalid because `impl` requires at least one
 // extended interface.
 class BuiltinConstraintMask
