64548c51b8d4da4c2ee22e003eede784d311cdb3
danakj <danakj@orodu.net>
2025-10-07 12:38:45 -0400

    import-eval

--- toolchain/check/import_ref.cpp
+++ toolchain/check/import_ref.cpp
@@ -3650,12 +3650,24 @@ auto LoadImportRef(Context& context, SemIR::InstId inst_id) -> void {
   auto [indirect_insts, load_type_id] =
       GetInstForLoad(context, inst->import_ir_inst_id);
 
+  // Loading an import ref, if its a generic, can re-evaluate its eval block
+  // against a new self specific. This can involve doing impl lookup, which can
+  // introduce new conversion instructions. Those instructions should be treated
+  // as part of the import, not becoming part of any enclosing generic.
+  context.inst_block_stack().Push();
+  context.generic_region_stack().Push(
+      {.generic_id = SemIR::GenericId::None,
+       .region = SemIR::GenericInstIndex::Declaration});
+
   // The last indirect instruction is the one to resolve. Pop it here because
   // Resolve will assign the constant.
   auto load_ir_inst = indirect_insts.pop_back_val();
   ImportRefResolver resolver(&context, load_ir_inst.ir_id());
   auto type_id = resolver.ResolveType(load_type_id);
   auto constant_id = resolver.Resolve(load_ir_inst.inst_id());
+
+  context.generic_region_stack().Pop();
+  context.inst_block_stack().PopAndDiscard();
 
   // Replace the ImportRefUnloaded instruction with ImportRefLoaded. This
   // doesn't use ReplacePlaceholderImportedInst because it would trigger
