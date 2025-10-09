68ee44b4a5dd9fb5fd02690f35feda9229dabf92
jonmeow <jperkins@google.com>
2025-10-09 14:20:01 -0700

    Add special-casing for tidy with boost_unordered

---
+++ scripts/deps_for_clangd_tidy.cpp
@@ -0,0 +1,8 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+// This file is only validating expected includes exist. See the BUILD target
+// for more information.
+
+#include <boost/unordered/unordered_flat_map.hpp>
