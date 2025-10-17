1d7bd156b456870dee9e5448bf4644bec2eb469d
David Blaikie <dblaikie@gmail.com>
2025-10-17 22:25:15 +0000

    Sort alphabetically

--- toolchain/sem_ir/inst_namer.h
+++ toolchain/sem_ir/inst_namer.h
@@ -61,12 +61,12 @@ class InstNamer {
       index = sem_ir_->functions().GetRawIndex(id);
     } else if constexpr (std::is_same_v<IdT, ImplId>) {
       index = sem_ir_->impls().GetRawIndex(id);
+    } else if constexpr (std::is_same_v<IdT, InterfaceId>) {
+      index = sem_ir_->interfaces().GetRawIndex(id);
     } else if constexpr (std::is_same_v<IdT, SpecificInterfaceId>) {
       index = sem_ir_->specific_interfaces().GetRawIndex(id);
     } else if constexpr (std::is_same_v<IdT, VtableId>) {
       index = sem_ir_->vtables().GetRawIndex(id);
-    } else if constexpr (std::is_same_v<IdT, InterfaceId>) {
-      index = sem_ir_->interfaces().GetRawIndex(id);
     }
     return static_cast<ScopeId>(GetScopeIdOffset(ScopeIdTypeEnum::For<IdT>) +
                                 index);
