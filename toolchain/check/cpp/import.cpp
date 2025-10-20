9dad5ab93c1c0fa8e6d2064d89aa0c610f325050
Boaz Brickner <brickner@google.com>
2025-10-20 14:21:28 +0200

    Use `UnresolvedSetIterator::getPair()`.

--- toolchain/check/cpp/import.cpp
+++ toolchain/check/cpp/import.cpp
@@ -2019,8 +2019,7 @@ auto ImportCppFunctionDecl(Context& context, SemIR::LocId loc_id,
 // Calculates the access given `UnresolvedSetIterator`.
 static auto MapAccess(clang::UnresolvedSetIterator iterator)
     -> SemIR::AccessKind {
-  return ConvertCppAccess(
-      clang::DeclAccessPair::make(*iterator, iterator.getAccess()));
+  return ConvertCppAccess(iterator.getPair());
 }
 
 // Imports a Clang declaration into Carbon and adds that name into the
