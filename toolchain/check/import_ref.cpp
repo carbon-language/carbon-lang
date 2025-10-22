baca1caddd297cd818fe8c93b7ae5029873673da
David Blaikie <dblaikie@gmail.com>
2025-10-22 23:42:54 +0000

    Correct format string specifier to match number of parameters

--- toolchain/check/import_ref.cpp
+++ toolchain/check/import_ref.cpp
@@ -60,7 +60,7 @@ static auto SetSpecialImportIR(Context& context, SemIR::ImportIR import_ir,
     ir_id = context.import_irs().Add(import_ir);
   }
   CARBON_CHECK(ir_id == expected_import_ir_id,
-               "Actual ImportIRId ($0) != Expected ImportIRId ({1})", ir_id,
+               "Actual ImportIRId ({0}) != Expected ImportIRId ({1})", ir_id,
                expected_import_ir_id);
 }
 
