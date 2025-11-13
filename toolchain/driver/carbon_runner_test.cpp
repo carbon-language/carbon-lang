92ae949188c4825677776a2ab7c48f043290cbe0
aw-andre <aw.andregerard@gmail.com>
2025-11-12 20:23:01 -0500

    Add extra tests for cached and cross-compiled core libs

--- toolchain/driver/carbon_runner_test.cpp
+++ toolchain/driver/carbon_runner_test.cpp
@@ -45,13 +45,11 @@ class CarbonRunnerTest : public ::testing::Test {
       *Runtimes::Cache::MakeSystem(*driver_env_.installation);
 };
 
-TEST_F(CarbonRunnerTest, BuildCoreLibraries) {
+TEST_F(CarbonRunnerTest, BuildCoreLibrariesX8664Linux) {
   CarbonRunner runner(&driver_env_);
 
-  // Note that we can't test arbitrary targets here as we need to be able
-  // to compile the builtin functions for the target. We use the default
-  // target as the most likely to pass.
-  std::string target = llvm::sys::getDefaultTargetTriple();
+  // Build Core libs for x86_64-unknown-linux-gnu.
+  std::string target = "x86_64-unknown-linux-gnu";
   llvm::Triple target_triple(target);
   Runtimes::Cache::Features features = {.target = target};
   auto runtimes = *runtimes_cache_.Lookup(features);
@@ -71,8 +69,74 @@ TEST_F(CarbonRunnerTest, BuildCoreLibraries) {
   }));
 
   // Check that we found a symbol from the core libraries.
-  EXPECT_THAT(out, HasSubstr(target_triple.isMacOSX() ? "T __CRange.Core\n"
-                                                      : "T _CRange.Core\n"));
+  EXPECT_THAT(out, HasSubstr("T _CRange.Core\n"));
+
+  EXPECT_THAT(test_output_stream_.TakeStr(), StrEq(""));
+  EXPECT_THAT(test_error_stream_.TakeStr(), StrEq(""));
+}
+
+TEST_F(CarbonRunnerTest, BuildCoreLibrariesAarch64Darwin) {
+  CarbonRunner runner(&driver_env_);
+
+  // Build Core libs for aarch64-apple-darwin.
+  std::string target = "aarch64-apple-darwin";
+  llvm::Triple target_triple(target);
+  Runtimes::Cache::Features features = {.target = target};
+  auto runtimes = *runtimes_cache_.Lookup(features);
+  auto build_result = runner.BuildCoreLibraries(features, runtimes);
+  ASSERT_TRUE(build_result.ok()) << build_result.error();
+  std::filesystem::path archive_path = std::move(*build_result);
+
+  // Check that the Core archive exists and contains a relevant symbol by
+  // running the `llvm-nm` tool over it. Using `nm` rather than directly
+  // inspecting the objects is a bit awkward, but lets us easily ignore the
+  // wrapping in an archive file.
+  LLVMRunner llvm_runner(driver_env_.installation, &llvm::errs());
+  std::string out;
+  std::string err;
+  EXPECT_TRUE(Testing::CallWithCapturedOutput(out, err, [&] {
+    return llvm_runner.Run(LLVMTool::Nm, {archive_path.native()});
+  }));
+
+  // Check that we found a symbol from the core libraries.
+  EXPECT_THAT(out, HasSubstr("T __CRange.Core\n"));
+
+  EXPECT_THAT(test_output_stream_.TakeStr(), StrEq(""));
+  EXPECT_THAT(test_error_stream_.TakeStr(), StrEq(""));
+}
+
+TEST_F(CarbonRunnerTest, RepeatedBuild) {
+  // Repeated "builds" (one from scratch and one cached) should point to the
+  // same file.
+  std::filesystem::path archive_path1;
+  std::filesystem::path archive_path2;
+
+  // Two runners should be able to point to the same driver_env.
+  {
+    CarbonRunner runner(&driver_env_);
+
+    std::string target = llvm::sys::getDefaultTargetTriple();
+    llvm::Triple target_triple(target);
+    Runtimes::Cache::Features features = {.target = target};
+    auto runtimes = *runtimes_cache_.Lookup(features);
+    auto build_result = runner.BuildCoreLibraries(features, runtimes);
+    ASSERT_TRUE(build_result.ok()) << build_result.error();
+    archive_path1 = std::move(*build_result);
+  }
+  {
+    CarbonRunner runner(&driver_env_);
+
+    std::string target = llvm::sys::getDefaultTargetTriple();
+    llvm::Triple target_triple(target);
+    Runtimes::Cache::Features features = {.target = target};
+    auto runtimes = *runtimes_cache_.Lookup(features);
+    auto build_result = runner.BuildCoreLibraries(features, runtimes);
+    ASSERT_TRUE(build_result.ok()) << build_result.error();
+    archive_path2 = std::move(*build_result);
+  }
+
+  ASSERT_TRUE(archive_path1 == archive_path2)
+      << "Different paths: " << archive_path1 << ", " << archive_path2 << "\n";
 
   EXPECT_THAT(test_output_stream_.TakeStr(), StrEq(""));
   EXPECT_THAT(test_error_stream_.TakeStr(), StrEq(""));
