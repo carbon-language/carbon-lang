926bca85881b0e5f06082e0144d40f9a022c9ccc
Chandler Carruth <chandlerc@gmail.com>
2025-11-20 02:35:14 +0000

    Extract the CC1 logic to `third_party` location
    This clarifies that the CC1 logic is directly extracted from Clang.
    There are probably some other places in the toolchain we should extract
    code like this where we're replicating and customizing logic from LLVM,
    but wanted to start here.

---
+++ third_party/llvm/clang_cc1.h
@@ -0,0 +1,29 @@
+// Part of the Carbon Language project, under the Apache License v2.0 with LLVM
+// Exceptions. See /LICENSE for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#ifndef CARBON_THIRD_PARTY_LLVM_CLANG_CC1_H_
+#define CARBON_THIRD_PARTY_LLVM_CLANG_CC1_H_
+
+#include "llvm/ADT/IntrusiveRefCntPtr.h"
+#include "llvm/ADT/SmallVector.h"
+#include "llvm/Support/VirtualFileSystem.h"
+#include "toolchain/install/install_paths.h"
+
+namespace Carbon {
+
+// Emulates Clang's `cc1_main` but in a way that doesn't assume it is running
+// in the main thread and can more easily fit into library calls to do
+// compiles.
+//
+// TODO: Much of the logic here should be factored out of the CC1
+// implementation in Clang's driver and into a reusable part of its libraries.
+// That should allow reducing the code here to a minimal amount.
+auto RunClangCC1(const InstallPaths& installation,
+                 llvm::IntrusiveRefCntPtr<llvm::vfs::FileSystem> fs,
+                 llvm::SmallVectorImpl<const char*>& cc1_args,
+                 bool enable_leaking) -> int;
+
+}  // namespace Carbon
+
+#endif  // CARBON_THIRD_PARTY_LLVM_CLANG_CC1_H_
