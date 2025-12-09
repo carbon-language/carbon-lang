660f8c1661d1f57c852e46fd2a5f53e5a6696300
danakj <danakj@orodu.net>
2025-12-09 13:51:26 -0500

    propagate-errors-extend-require

--- toolchain/check/handle_require.cpp
+++ toolchain/check/handle_require.cpp
@@ -246,6 +246,7 @@ auto HandleParseNode(Context& context, Parse::RequireDeclId node_id) -> bool {
   auto introducer =
       context.decl_introducer_state_stack().Pop<Lex::TokenKind::Require>();
   LimitModifiersOnDecl(context, introducer, KeywordModifierSet::Extend);
+  bool extend = introducer.modifier_set.HasAnyOf(KeywordModifierSet::Extend);
 
   auto scope_inst_id =
       context.node_stack().Pop<Parse::NodeKind::RequireIntroducer>();
@@ -253,6 +254,13 @@ auto HandleParseNode(Context& context, Parse::RequireDeclId node_id) -> bool {
   auto validated = ValidateRequire(context, node_id, self_inst_id,
                                    constraint_inst_id, scope_inst_id);
   if (!validated) {
+    // In an `extend` decl, errors get propageted into the parent scope just as
+    // names do.
+    if (extend) {
+      context.name_scopes()
+          .Get(context.scope_stack().PeekNameScopeId())
+          .set_has_error();
+    }
     DiscardGenericDecl(context);
     return true;
   }
@@ -260,13 +268,10 @@ auto HandleParseNode(Context& context, Parse::RequireDeclId node_id) -> bool {
   auto [constraint_type_id, identified_facet_type] = *validated;
   if (identified_facet_type->required_interfaces().empty()) {
     // A `require T impls type` adds no actual constraints, so nothing to do.
+    // This is not an error though.
     DiscardGenericDecl(context);
     return true;
   }
-
-  // TODO: When extend is true, any errors should propagate up to the parent
-  // scope.
-  bool extend = introducer.modifier_set.HasAnyOf(KeywordModifierSet::Extend);
 
   auto require_impls_decl =
       SemIR::RequireImplsDecl{// To be filled in after.
