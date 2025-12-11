5d18ae10bb59113384c10a01ab858f4eb97c1cfc
Richard Smith <richard@metafoo.co.uk>
2025-12-11 01:08:20 +0000

    Keep the parser alive until we're done with the TU too.
    Reuse the same parser for macro parsing.

--- toolchain/check/cpp/generate_ast.cpp
+++ toolchain/check/cpp/generate_ast.cpp
@@ -369,14 +369,17 @@ class GenerateASTAction : public clang::ASTFrontendAction {
 
   auto ExecuteAction() -> void override {
     clang::CompilerInstance& clang_instance = getCompilerInstance();
-    clang_instance.createSema(getTranslationUnitKind(), /*CompletionConsumer=*/nullptr);
+    clang_instance.createSema(getTranslationUnitKind(),
+                              /*CompletionConsumer=*/nullptr);
 
     // Parse the imports and inline C++ fragments. This is notionally very
     // similar to `ParseAST`, but doesn't parse C++20 modules and stops just
     // before reaching the end of the translation unit.
-    clang::Parser parser(clang_instance.getPreprocessor(),
-                         clang_instance.getSema(),
-                         /*SkipFunctionBodies=*/false);
+
+    context_->cpp_context()->set_parser(std::make_unique<clang::Parser>(
+        clang_instance.getPreprocessor(), clang_instance.getSema(),
+        /*SkipFunctionBodies=*/false));
+    auto& parser = context_->cpp_context()->parser();
 
     clang_instance.getPreprocessor().EnterMainSourceFile();
     if (auto* source = clang_instance.getASTContext().getExternalSource()) {
