5fcce7efb9db5884371e9abc1497f0ba0a8628c0
Geoffrey Romer <gromer@google.com>
2026-01-13 12:45:10 -0800

    Respond to reviewer comments

--- toolchain/sem_ir/expr_info.cpp
+++ toolchain/sem_ir/expr_info.cpp
@@ -174,9 +174,6 @@ auto FindReturnSlotArgForInitializer(const File& sem_ir, InstId init_id,
         return init.dest_id;
       }
       case CARBON_KIND(InPlaceInit init): {
-        if (!InitRepr::ForType(sem_ir, init.type_id).MightBeInPlace()) {
-          return InstId::None;
-        }
         return init.dest_id;
       }
       case CARBON_KIND(Call call): {
