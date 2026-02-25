8ca2765eef14406cb4adac61c69d5bf9534558db
David Blaikie <dblaikie@gmail.com>
2026-02-25 23:47:01 +0000

    Initial Reverse Interop implementation
    Add a clang::ExternalASTSource to begin exposing Carbon entities to
    Clang - initially only a single `Carbon` top level namespace.
    
    Subsequent work will add Carbon entities to this namespace.
    
    Likely this CarbonExternalASTSource will be refactored into another
    file, tie into/reference SemIR::File and CppFile, etc eventually - but
    that'll wait for future patches.
    
    If there's mechanical problems with the current implementation - how I'm
    creating the new NamespaceDecl, etc - I'm all ears. It's very much in
    the "it seems to work" state, not much more than that.
    
    This does break Clang Modules (header modules, C++20 modules,
    precompiled headers, etc) since they're implemented as an
    ExternalASTSource as well, and Clang's ASTContext only supports one
    ExternalASTSource at a time. To fix that regression we'll need to
    implement some kind of ExternalASTSource multiplexing support - either
    in Clang or Carbon (unclear which).
    
    This regression of modules support can be observed by the following:
    `a.h`
    ```
    inline void f1() { }
    ```
    `module.modulemap`
    ```
    module A {
      header "A.h"
      export *
    }
    ```
    `test.carbon`
    ```
    import Cpp inline '''
    // Hardcode the pragma to ensure this isn't silently falling back to
    // textual inclusion.
    void f2() {
      f1();
    }
    ''';
    ```
    ```
    carbon compile test.carbon -- -I . -fmodules -fimplicit-modules -fmodules-cache-path=module_cache
    ```
    
    I wrote a `file_test` test for this, but it doesn't /quite/ work because
    `file_test` provides an in-memory filesystem for tests to make them more
    hermetic, but Clang's Filesystem abstrtaction is for reading only - so
    the module that's written out successfully can't be found when it needs
    to be read back in - so the test doesn't pass as a baseline. Clang does
    have support for `llvm::vfs::OutputBackend` which allows virtualizing
    output - which I guess we could tie together with the InMemoryFilesystem
    we use for input to make such a test work. But I guess that's not worth
    the effort here?

--- testing/file_test/run_test.cpp
+++ testing/file_test/run_test.cpp
@@ -53,7 +53,8 @@ static auto DoArgReplacements(llvm::SmallVector<std::string>& test_args,
         it = test_args.erase(it);
         for (const auto& split : split_files) {
           const std::string& filename = split->filename;
-          if (filename == StdinFilename || filename.ends_with(".h")) {
+          if (filename == StdinFilename || filename.ends_with(".h") ||
+              filename.ends_with(".modulemap")) {
             continue;
           }
           it = test_args.insert(it, filename);
