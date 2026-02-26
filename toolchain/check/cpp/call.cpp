a4dd342d09c0085f6e011cc71ea591a408c4ae17
nicholasbishop <nicholasbishop@google.com>
2026-02-26 15:03:29 -0500

    Support float template params

--- toolchain/check/cpp/call.cpp
+++ toolchain/check/cpp/call.cpp
@@ -189,6 +189,14 @@ static auto ConvertArgToTemplateArg(
                                                param_type);
           return clang::TemplateArgumentLoc(template_arg, template_loc);
         }
+      } else if (param_type->isFloatingType()) {
+        if (auto float_value =
+                context.insts().TryGetAs<SemIR::FloatValue>(const_inst_id)) {
+          const auto& ap_float = context.floats().Get(float_value->float_id);
+          clang::TemplateArgument template_arg(
+              context.ast_context(), param_type, clang::APValue(ap_float));
+          return clang::TemplateArgumentLoc(template_arg, template_loc);
+        }
       }
     }
 
