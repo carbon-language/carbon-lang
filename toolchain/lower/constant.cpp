abb4163f48e96e97f88b34de992483caf23b14ab
nicholasbishop <nicholasbishop@google.com>
2026-03-30 14:12:20 -0400

    Handle null inst_namer by falling back to "const" name

--- toolchain/lower/constant.cpp
+++ toolchain/lower/constant.cpp
@@ -100,8 +100,8 @@ class ConstantContext {
     return file_context_->llvm_module();
   }
   auto sem_ir() const -> const SemIR::File& { return file_context_->sem_ir(); }
-  auto inst_namer() const -> const SemIR::InstNamer& {
-    return *file_context_->inst_namer();
+  auto inst_namer() const -> const SemIR::InstNamer* {
+    return file_context_->inst_namer();
   }
 
  private:
@@ -304,7 +304,10 @@ static auto EmitAsConstant(ConstantContext& context, SemIR::Temporary inst)
     -> llvm::Constant* {
   auto* const_value = context.GetConstant(inst.init_id);
 
-  auto const_name = context.inst_namer().GetUnscopedNameFor(inst.init_id);
+  llvm::StringRef const_name;
+  if (context.inst_namer()) {
+    const_name = context.inst_namer()->GetUnscopedNameFor(inst.init_id);
+  }
   if (const_name.empty()) {
     const_name = "const";
   }
