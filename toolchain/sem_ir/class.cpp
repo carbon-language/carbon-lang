b94cbdacb363e203acd531990579448cddf35080
nicholasbishop <nicholasbishop@google.com>
2026-06-10 14:36:30 -0400

    Move GetAsCarbonOwnedClass to sem_ir

--- toolchain/sem_ir/class.cpp
+++ toolchain/sem_ir/class.cpp
@@ -4,6 +4,7 @@
 
 #include "toolchain/sem_ir/class.h"
 
+#include "clang/AST/Decl.h"
 #include "toolchain/base/value_store_impl.h"
 #include "toolchain/sem_ir/file.h"
 #include "toolchain/sem_ir/generic.h"
@@ -67,6 +68,50 @@ auto Class::GetStructTypeFields(const File& sem_ir) const
   auto struct_type =
       sem_ir.types().GetAs<SemIR::StructType>(object_repr_type_id);
   return sem_ir.struct_type_fields().Get(struct_type.fields_id);
+}
+
+auto GetAsCarbonOwnedClass(const File& sem_ir, const clang::TagDecl* tag_decl)
+    -> std::optional<std::pair<SemIR::TypeId, SemIR::ClassType>> {
+  // Quickly check whether we could possibly own this class.
+  // TODO: Once we multiplex with the ASTReader, handle
+  // ASTReader::completeVisibleDeclsMap setting this to `false`.
+  if (!tag_decl->hasExternalVisibleStorage()) {
+    return std::nullopt;
+  }
+
+  auto key = SemIR::ClangDeclKey::ForNonFunctionDecl(
+      const_cast<clang::TagDecl*>(tag_decl->getFirstDecl()));
+  auto clang_decl_id = sem_ir.clang_decls().LookupId(key);
+  if (!clang_decl_id.has_value()) {
+    return std::nullopt;
+  }
+
+  auto inst_id = sem_ir.clang_decls().Get(clang_decl_id).inst_id;
+  auto const_id = sem_ir.constant_values().Get(inst_id);
+  if (!const_id.has_value()) {
+    return std::nullopt;
+  }
+
+  auto class_type =
+      sem_ir.constant_values().TryGetInstAs<SemIR::ClassType>(const_id);
+  if (!class_type) {
+    return std::nullopt;
+  }
+
+  // Determine whether this class was imported from C++.
+  // TODO: This currently can't happen, because only Carbon classes have
+  // external lexical storage, but will happen once we support importing C++
+  // classes from AST files. Add a test once that is supported.
+  // TODO: Consider setting `extern_library_id` on classes imported from C++ to
+  // indicate the current file does not own them.
+  const auto& class_info = sem_ir.classes().Get(class_type->class_id);
+  if (class_info.parent_scope_id.has_value() &&
+      sem_ir.name_scopes().Get(class_info.parent_scope_id).is_cpp_scope()) {
+    return std::nullopt;
+  }
+
+  auto class_type_id = sem_ir.types().GetTypeIdForTypeConstantId(const_id);
+  return std::make_pair(class_type_id, *class_type);
 }
 
 auto LookupClassFieldByStructField(const File& sem_ir,
