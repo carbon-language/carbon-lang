1673cef3febe08a3bc302bd80a131be24fff4c22
Richard Smith <richard@metafoo.co.uk>
2026-06-23 00:05:54 +0000

    Use plain Apache 2.0 license for mirrors.
    Our mirror repositories contain vim scripts and textmate grammars,
    neither of which have any need for the LLVM exception, so we can use the
    base Apache 2.0 license there with no loss of relevant permissions.
    This is a compatible license, so providing these repositories under
    these license terms is valid.
    
    Other than removing the LLVM exception, the new license file has one
    other change compared to LICENSE: the amount of whitespace on some lines
    is reduced from four spaces to three. This makes the new license
    *exactly* match the Apache 2.0 license, byte-for-byte.
    
    This is important because GitHub's linguist project doesn't recognize
    Apache-2.0-with-LLVM-exception as an acceptable license, but does allow
    plain Apache-2.0.

--- scripts/sync_repos.sh
+++ scripts/sync_repos.sh
@@ -47,8 +47,11 @@ for dir in "${!MIRRORS[@]}"; do
   # Copy the basic framework from the origin repository.
   cp "$ORIGIN_DIR/.gitignore" \
     "$ORIGIN_DIR/CODE_OF_CONDUCT.md" \
-    "$ORIGIN_DIR/LICENSE" \
     .
+
+  # The LLVM exception is not relevant to the mirrors, so use the simplified
+  # but compatible Apache 2.0 license.
+  cp "$ORIGIN_DIR/LICENSE.no-llvm-exception" LICENSE
 
   # Copy the mirrored directory. We use `rsync` to get a more reliable way of
   # handling the mirroring of the contents of a directory. We also make this
