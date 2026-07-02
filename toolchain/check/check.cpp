9c229deed01fa80f0574ed045d916a99aa84776f
Lucile Rose Nihlen <carbon@lucile.tech>
2026-07-02 16:47:25 +0000

    fix prelude import for min_prelude

--- toolchain/check/check.cpp
+++ toolchain/check/check.cpp
@@ -459,17 +459,18 @@ auto CheckParseTrees(
     // Add the prelude import. It's added to explicit_import_map so that it can
     // conflict with an explicit import of the prelude.
     if (options.prelude_import) {
-      auto prelude_id =
-          unit_info.unit->value_stores->string_literal_values().Add("prelude");
-
       // We add the prelude to every Carbon unit except for the prelude itself.
       // Adding the prelude to the non-prelude parts of Core requires different
       // semantics because it is not valid to mention the name of the package
       // for a library import from the same package.
       if (packaging && (packaging->names.package_id == PackageNameId::Core)) {
-        if (!unit_info.unit->value_stores->string_literal_values()
+        if (packaging->names.library_id == StringLiteralValueId::None ||
+            !unit_info.unit->value_stores->string_literal_values()
                  .Get(packaging->names.library_id)
                  .starts_with("prelude")) {
+          auto prelude_id =
+              unit_info.unit->value_stores->string_literal_values().Add(
+                  "prelude");
           // Add the prelude to a non-prelude unit within Core.
           TrackImport(api_map, &explicit_import_map, unit_info,
                       {.node_id = Parse::NoneNodeId(),
@@ -478,6 +479,9 @@ auto CheckParseTrees(
                       options.fuzzing);
         }
       } else {
+        auto prelude_id =
+            unit_info.unit->value_stores->string_literal_values().Add(
+                "prelude");
         // Add the prelude to a non-Core unit.
         TrackImport(api_map, &explicit_import_map, unit_info,
                     {.node_id = Parse::NoneNodeId(),
