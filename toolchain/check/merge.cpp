252dc974c94f110c49c7b0f82a967fe521648a50
nicholasbishop <nicholasbishop@google.com>
2026-07-08 11:53:55 -0400

    Fix a few proposal URLs that were missed in the great renaming

--- toolchain/check/merge.cpp
+++ toolchain/check/merge.cpp
@@ -484,7 +484,7 @@ static auto CheckRedeclParamSyntax(Context& context,
       // redeclaration matching rule from proposal #3763.
       //
       // Skip difference if it is `Self as` vs. `as` in an `impl` declaration.
-      // https://github.com/carbon-language/carbon-lang/blob/trunk/proposals/p003763.md#redeclarations
+      // https://github.com/carbon-language/carbon-lang/blob/trunk/proposals/p003763-matching-redeclarations.md#redeclarations
       if (new_node_kind == Parse::NodeKind::ImplDefaultSelfAs &&
           prev_node_kind == Parse::NodeKind::SelfTypeNameExpr &&
           context.parse_tree().node_kind(prev_iter[1]) ==
