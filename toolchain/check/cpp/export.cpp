25a745c5b6ab7aceb69b917f849dac42dbbbec84
nicholasbishop <nicholasbishop@google.com>
2026-07-20 13:54:33 -0400

    Remove mangling from BuildCppFunctionDeclForGenericCarbonFn

--- toolchain/check/cpp/export.cpp
+++ toolchain/check/cpp/export.cpp
@@ -555,13 +555,6 @@ static auto BuildCppFunctionDeclForGenericCarbonFn(
   }
   function_decl->setParams(param_var_decls);
 
-  // Mangle the function name and attach it to the `FunctionDecl`.
-  SemIR::Mangler m(context.sem_ir(), context.total_ir_count(),
-                   context.mangle_string_fingerprint());
-  std::string mangled_name = m.Mangle(function_id, SemIR::SpecificId::None);
-  function_decl->addAttr(
-      clang::AsmLabelAttr::Create(context.ast_context(), mangled_name));
-
   return function_decl;
 }
 
