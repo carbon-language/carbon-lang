cf01ea5256d320cb2b8455f01ae7c995cf36250f
ozguronsoy <ozguronsoy@gmail.com>
2026-09-04 01:21:15 +0300

    diagnose wrong scope

--- toolchain/check/handle_positional_param_expr.cpp
+++ toolchain/check/handle_positional_param_expr.cpp
@@ -5,19 +5,30 @@
 #include "toolchain/check/context.h"
 #include "toolchain/check/handle.h"
 #include "toolchain/check/inst.h"
+#include "toolchain/diagnostics/diagnostic.h"
 #include "toolchain/parse/node_ids.h"
 #include "toolchain/sem_ir/typed_insts.h"
 
 namespace Carbon::Check {
 auto HandleParseNode(Context& context, Parse::PositionalParamExprId node_id)
     -> bool {
+  if (!context.scope_stack().IsInFunctionScope()) {
+    CARBON_DIAGNOSTIC(PositionalParamInWrongScope, Error,
+                      "positional parameters can only be used in a function");
+    context.emitter().Emit(node_id, PositionalParamInWrongScope);
+  }
+
   auto positional_param_id = AddInstInNoBlock<SemIR::PositionalParam>(
       context, node_id,
       {.type_id = SemIR::TypeType::TypeId,
        .int_id = context.tokens().GetDollarIntLiteral(
            context.parse_tree().node_token(node_id))});
-  context.args_type_info_stack().AddInstId(positional_param_id);
-  context.inst_block_stack().AddInstId(positional_param_id);
+  if (!context.args_type_info_stack().empty()) {
+    context.args_type_info_stack().AddInstId(positional_param_id);
+  }
+  if (!context.inst_block_stack().empty()) {
+    context.inst_block_stack().AddInstId(positional_param_id);
+  }
   context.node_stack().Push(node_id, positional_param_id);
   return true;
 }
