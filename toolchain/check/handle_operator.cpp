95da0e6d61640364e978b3baddd92cebaae249bb
nicholasbishop <nicholasbishop@google.com>
2026-09-10 10:51:55 -0400

    Drop unused include

--- toolchain/check/handle_operator.cpp
+++ toolchain/check/handle_operator.cpp
@@ -2,7 +2,6 @@
 // Exceptions. See /LICENSE for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "toolchain/check/action.h"
 #include "toolchain/check/context.h"
 #include "toolchain/check/control_flow.h"
 #include "toolchain/check/convert.h"
