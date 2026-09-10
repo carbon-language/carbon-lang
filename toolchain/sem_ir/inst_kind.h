5c4a09e83b2414ee623cb5d7236884b14dfc7907
danakj <danakj@orodu.net>
2026-09-10 17:53:05 -0400

    add-link

--- toolchain/sem_ir/inst_kind.h
+++ toolchain/sem_ir/inst_kind.h
@@ -483,7 +483,8 @@ namespace Internal {
 
 // Storage for `internal_allowed_node_kinds` where there's a list of kinds.
 template <Parse::NodeKind::RawEnumType... T>
-// NOLINTNEXTLINE(google-readability-casting): Initializer list is not a cast.
+// False positive: https://github.com/llvm/llvm-project/issues/222793
+// NOLINTNEXTLINE(google-readability-casting)
 constexpr std::array<Parse::NodeKind::RawEnumType, sizeof...(T)> Kinds = {T...};
 
 // `NoneNodeId` uses should never have a node associated; it's mainly for
