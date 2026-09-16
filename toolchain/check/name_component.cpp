21d25030281167709d12c16712a12c36781ad504
Lucile Rose Nihlen <carbon@lucile.tech>
2026-09-16 19:55:56 +0000

    repair bug in previous

--- toolchain/check/name_component.cpp
+++ toolchain/check/name_component.cpp
@@ -13,7 +13,6 @@ auto PopNameComponent(Context& context, SemIR::InstId return_pattern_id)
     -> NameComponent {
   Parse::NodeId first_param_node_id = Parse::NoneNodeId();
   Parse::NodeId last_param_node_id = Parse::NoneNodeId();
-  auto call_param_default_values_id = SemIR::InstBlockId::None;
 
   // Explicit params.
   auto [params_node_id, param_patterns_id] =
@@ -24,13 +23,6 @@ auto PopNameComponent(Context& context, SemIR::InstId return_pattern_id)
         context.node_stack()
             .PopForSoloNodeId<Parse::NodeKind::ExplicitParamListStart>();
     last_param_node_id = params_node_id;
-    if (!context.full_pattern_stack().GetConvertedDefaultValues().empty()) {
-      CARBON_CHECK(
-          context.full_pattern_stack().GetRawDefaultValues().size() ==
-          context.full_pattern_stack().GetConvertedDefaultValues().size());
-      call_param_default_values_id = context.inst_blocks().Add(
-          context.full_pattern_stack().GetConvertedDefaultValues());
-    }
   } else {
     param_patterns_id = SemIR::InstBlockId::None;
   }
@@ -53,6 +45,7 @@ auto PopNameComponent(Context& context, SemIR::InstId return_pattern_id)
   }
 
   auto call_param_patterns_id = SemIR::InstBlockId::None;
+  auto call_param_default_values_id = SemIR::InstBlockId::None;
   auto call_params_id = SemIR::InstBlockId::None;
   auto param_ranges = SemIR::Function::CallParamIndexRanges::Empty;
   auto pattern_block_id = SemIR::InstBlockId::None;
@@ -64,6 +57,14 @@ auto PopNameComponent(Context& context, SemIR::InstId return_pattern_id)
     call_param_patterns_id = results.call_param_patterns_id;
     call_params_id = results.call_params_id;
     param_ranges = results.param_ranges;
+    if (param_patterns_id->has_value() &&
+        !context.full_pattern_stack().GetRawDefaultValues().empty()) {
+      CARBON_CHECK(
+          context.full_pattern_stack().GetRawDefaultValues().size() ==
+          context.full_pattern_stack().GetConvertedDefaultValues().size());
+      call_param_default_values_id = context.inst_blocks().Add(
+          context.full_pattern_stack().GetConvertedDefaultValues());
+    }
     pattern_block_id = context.pattern_block_stack().Pop();
     context.full_pattern_stack().PopFullPattern();
   }
