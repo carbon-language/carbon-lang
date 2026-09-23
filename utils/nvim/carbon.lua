5402aead7a4265cb5835af8d2595c534df4e9270
Nicole Mazzuca <nicole@strega-nil.co>
2026-09-23 23:43:19 +0200

    Update nvim/carbon.lua for modern neovim
    LSP support is now built in, so no need to depend on lspconfig (and
    lspconfig doesn't really do anything anymore either).
    
    Additionally, fix syntax highlighting by actually making sure to enable
    treesitter.

--- utils/nvim/carbon.lua
+++ utils/nvim/carbon.lua
@@ -7,22 +7,20 @@ vim.filetype.add({
     carbon = 'carbon',
   },
 })
-
 vim.treesitter.language.add("carbon")
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'carbon' },
+  callback = function() vim.treesitter.start() end,
+})
 
 -- LSP
-local lsp = require 'lspconfig'
-local configs = require 'lspconfig.configs'
-local util = require 'lspconfig.util'
-
--- Check if the config is already defined (useful when reloading this file)
-if not configs.carbon then
-  configs.carbon = {
-    default_config = {
-      cmd = { "./bazel-bin/toolchain/carbon language-server" },
-      filetypes = { "carbon" },
-      root_dir = util.find_git_ancestor,
-    }
-  }
+function find_git_ancestor(startpath)
+  return vim.fs.dirname(vim.fs.find('.git', { path = startpath, upward = true })[1])
 end
-lsp.carbon.setup {}
+local root = find_git_ancestor(vim.env.PWD);
+
+vim.lsp.config("carbon", {
+  cmd = { root .. "/bazel-bin/toolchain/carbon", "language-server" },
+  filetypes = { "carbon" },
+})
+vim.lsp.enable("carbon")
